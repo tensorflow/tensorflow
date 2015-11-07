fddaed524622417900d745fe8f115562c55ac49a
Vijay Vasudevan <vrv@google.com>
2015-11-07 13:58:24 -0800

    TensorFlow: Upstream commits to git.
    Changes:
    - More documentation edits, fixes to anchors,
      fixes to mathjax, new images, etc.
    - Add rnn models to pip install package.
    
    Base CL: 107312343

---
+++ tensorflow/models/rnn/__init__.py
@@ -0,0 +1,12 @@
+"""Libraries to build Recurrent Neural Networks.
+
+This file helps simplify the import process:
+
+import tensorflow.python.platform
+from tensorflow.models.rnn import rnn
+from tensorflow.models.rnn import rnn_cell
+...
+"""
+from tensorflow.models.rnn import rnn
+from tensorflow.models.rnn import rnn_cell
+from tensorflow.models.rnn import seq2seq
