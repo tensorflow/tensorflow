6bbc2c42059660a7ff1ae6d90767d9837f3ff0b6
Illia Polosukhin <ilblackdragon@gmail.com>
2015-11-25 10:52:08 -0800

    moved installation for travis into a script

---
+++ .travis_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create virtual env
+deactivate
+virtualenv --system-site-packages testvenv
+source testenv/bin/activate
+
+# Install dependencies
+pip install sklearn scipy numpy
+pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+
+# Install test tools
+pip install nose
+
+# Install skflow
+python setup.py install
+
