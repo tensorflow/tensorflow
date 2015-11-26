0a166322f1fecbd9bb588ce3d1078619a49ecbaf
Illia Polosukhin <ilblackdragon@gmail.com>
2015-11-25 16:02:21 -0800

    moved scipy & numpy installation to apt and added lapack/blas

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Create virtual env
+# Create virtual env using system numpy and scipy
 deactivate
 virtualenv --system-site-packages testenv
 source testenv/bin/activate
 
 # Install dependencies
-pip install numpy scipy
 pip install sklearn
 pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
 
