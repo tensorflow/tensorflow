371863aabe84e4d059bb7f28367fa71cc9a7f23f
Illia Polosukhin <ilblackdragon@gmail.com>
2015-11-27 20:48:16 -0800

    Fixes #13: Fixed readme to use skflow.models.logistic_regerssion and also created example in examples/ folder with this code to make sure it runs.

--- README.md
+++ README.md
@@ -96,7 +96,7 @@ iris = datasets.load_iris()
 def my_model(X, y):
     """This is DNN with 10, 20, 10 hidden layers, and dropout of 0.5 probability."""
     layers = skflow.ops.dnn(X, [10, 20, 10], keep_prob=0.5)
-    return skflow.ops.logistic_classifier(layers, y)
+    return skflow.models.logistic_regression(layers, y)
 
 classifier = skflow.TensorFlowEstimator(model_fn=my_model, n_classes=3)
 classifier.fit(iris.data, iris.target)
---
+++ examples/iris_custom_model.py
@@ -0,0 +1,33 @@
+#  Copyright 2015 Google Inc. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import random
+
+import skflow
+from sklearn import datasets, metrics
+
+iris = datasets.load_iris()
+
+random.seed(42)
+
+def my_model(X, y):
+    """This is DNN with 10, 20, 10 hidden layers, and dropout of 0.5 probability."""
+    layers = skflow.ops.dnn(X, [10, 20, 10], keep_prob=0.5)
+    return skflow.models.logistic_regression(layers, y)
+
+classifier = skflow.TensorFlowEstimator(model_fn=my_model, n_classes=3)
+classifier.fit(iris.data, iris.target)
+score = metrics.accuracy_score(classifier.predict(iris.data), iris.target)
+print("Accuracy: %f" % score)
+
