add82f747fdbf89b0888b4c61ee59579630a86fe
Illia Polosukhin <ilblackdragon@gmail.com>
2015-11-30 18:07:07 -0800

    Updated travis config to run on both osx and linux

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,12 +6,23 @@ virtualenv --system-site-packages testenv
 source testenv/bin/activate
 
 # Install dependencies
-pip install sklearn
-pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+sudo pip install --upgrade pip
+sudo pip install numpy
+sudo pip install scipy
+sudo pip install pandas
+sudo pip install sklearn
+
+# Install TensorFlow
+if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+    sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+fi
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+    sudo pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.5.0-py2-none-any.whl
+fi
 
 # Install test tools
-pip install nose
+sudo pip install nose
 
 # Install skflow
-python setup.py install
+sudo python setup.py install
 
