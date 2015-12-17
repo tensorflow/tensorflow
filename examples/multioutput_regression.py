484b83a828bfa2360eff3c50a1febabe67644425
Illia Polosukhin <ilblackdragon@gmail.com>
2015-12-17 11:49:38 -0800

    Changed a bit example to run custom DNN model with tanh activations

--- examples/multioutput_regression.py
+++ examples/multioutput_regression.py
@@ -31,27 +31,41 @@ rng = np.random.RandomState(1)
 X = np.sort(200 * rng.rand(100, 1) - 100, axis=0)
 y = np.array([np.pi * np.sin(X).ravel(), np.pi * np.cos(X).ravel()]).T
 
-# Fit regression DNN model.
-regressor = skflow.TensorFlowDNNRegressor(hidden_units=[5, 5])
-regressor.fit(X, y)
-score = mean_squared_error(regressor.predict(X), y)
-print("Mean Squared Error: {0:f}".format(score))
+# Fit regression DNN models.
+regressors = []
+options = [[2], [10, 10], [20, 20]]
+for hidden_units in options:
+    def tanh_dnn(X, y):
+        features = skflow.ops.dnn(X, hidden_units=hidden_units,
+          activation=skflow.tf.tanh)
+        return skflow.models.linear_regression(features, y)
+
+    regressor = skflow.TensorFlowEstimator(model_fn=tanh_dnn, n_classes=0,
+        steps=500, learning_rate=0.1, batch_size=100)
+    regressor.fit(X, y)
+    score = mean_squared_error(regressor.predict(X), y)
+    print("Mean Squared Error for {0}: {1:f}".format(str(hidden_units), score))
+    regressors.append(regressor)
 
 # Predict on new random Xs.
-X_test = np.arange(-100.0, 100.0, 0.01)[:, np.newaxis]
-y_1 = regr_1.predict(X_test)
-y_2 = regr_2.predict(X_test)
-y_3 = regr_3.predict(X_test)
+X_test = np.arange(-100.0, 100.0, 0.1)[:, np.newaxis]
+y_1 = regressors[0].predict(X_test)
+y_2 = regressors[1].predict(X_test)
+y_3 = regressors[2].predict(X_test)
 
 # Plot the results
 plt.figure()
 plt.scatter(y[:, 0], y[:, 1], c="k", label="data")
-plt.scatter(y_1[:, 0], y_1[:, 1], c="g", label="max_depth=2")
-plt.scatter(y_2[:, 0], y_2[:, 1], c="r", label="max_depth=5")
-plt.scatter(y_3[:, 0], y_3[:, 1], c="b", label="max_depth=8")
+plt.scatter(y_1[:, 0], y_1[:, 1], c="g",
+    label="hidden_units{}".format(str(options[0])))
+plt.scatter(y_2[:, 0], y_2[:, 1], c="r",
+    label="hidden_units{}".format(str(options[1])))
+plt.scatter(y_3[:, 0], y_3[:, 1], c="b",
+    label="hidden_units{}".format(str(options[2])))
 plt.xlim([-6, 6])
 plt.ylim([-6, 6])
 plt.xlabel("data")
 plt.ylabel("target")
 plt.title("Multi-output DNN Regression")
-
+plt.legend()
+plt.show()
