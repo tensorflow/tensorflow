1d811884f7a70cedd730c0799e9b2682c84dc0aa
Benoit Steiner <benoit.steiner.goog@gmail.com>
2015-12-17 17:27:28 -0800

    Leverage Eigen IndexList to speedup reductions on GPU Change: 110503632

--- tensorflow/core/kernels/reduction_ops_common.h
+++ tensorflow/core/kernels/reduction_ops_common.h
@@ -40,7 +40,6 @@ namespace tensorflow {
 typedef Eigen::ThreadPoolDevice CPUDevice;
 typedef Eigen::GpuDevice GPUDevice;
 
-#if !defined(EIGEN_HAS_INDEX_LIST)
 template <typename Device>
 struct Constants {
   // Derive Index type. int (32-bit) or long (64-bit) depending on the
@@ -59,10 +58,9 @@ struct Constants {
   }
 };
 
-#else
-
-template <typename Device>
-struct Constants {
+#if defined(EIGEN_HAS_INDEX_LIST)
+template <>
+struct Constants<CPUDevice> {
   const Eigen::IndexList<Eigen::type2index<0>> kZero;
   const Eigen::IndexList<Eigen::type2index<1>> kOne;
   const Eigen::IndexList<Eigen::type2index<0>, Eigen::type2index<2>> kZeroTwo;
