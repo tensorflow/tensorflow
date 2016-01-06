94ce694c294b23b4e3fdcc6bf39c1b0456d09b4a
Josh Levenberg <josh11b@tensorflow.org>
2016-01-06 12:55:02 -0800

    #include third_party/tensorflow/core/platform/macros.h directly so we can drop it from port.h. Change: 111528649

--- tensorflow/core/framework/op.h
+++ tensorflow/core/framework/op.h
@@ -24,6 +24,7 @@ limitations under the License.
 #include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/lib/strings/strcat.h"
 #include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/macros.h"
 #include "tensorflow/core/platform/port.h"
 #include "tensorflow/core/platform/thread_annotations.h"
 #include "tensorflow/core/public/status.h"
