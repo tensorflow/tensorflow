2c64510a149bbcd9a46fa9ba5ad13faa4d5e7d73
Josh Levenberg <josh11b@tensorflow.org>
2016-01-06 12:53:10 -0800

    Drop host_info.h from port.h and #include it directly where needed. Change: 111476610

--- tensorflow/core/common_runtime/direct_session.cc
+++ tensorflow/core/common_runtime/direct_session.cc
@@ -42,6 +42,7 @@ limitations under the License.
 #include "tensorflow/core/lib/strings/numbers.h"
 #include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/lib/strings/strcat.h"
+#include "tensorflow/core/platform/host_info.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/port.h"
 #include "tensorflow/core/public/status.h"
--- tensorflow/core/util/events_writer.cc
+++ tensorflow/core/util/events_writer.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include "tensorflow/core/lib/io/path.h"
 #include "tensorflow/core/lib/strings/strcat.h"
 #include "tensorflow/core/lib/strings/stringprintf.h"
+#include "tensorflow/core/platform/host_info.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/port.h"
 #include "tensorflow/core/public/env.h"
