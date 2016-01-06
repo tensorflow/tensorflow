4123115f596cb4e96a06792befe034ae595c6ff2
A. Unique TensorFlower <nobody@tensorflow.org>
2016-01-06 12:51:57 -0800

    Explicitly setting shape in grayscale_to_rgb and rgb_to_grayscale.
    Fixes shape inference following a previous change that generalized these two members of image_ops. The more general implementation, however, does not allow shape inference. This change explicitly fixes the shape with set_shape, following array_grad._TileGrad.
    
    Adding tests for expected shape inference behavior.
    Change: 111459347

--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -133,6 +133,31 @@ class GrayscaleToRGBTest(test_util.TensorFlowTestCase):
       y_tf = y.eval()
       self.assertAllEqual(y_tf, y_np)
 
+  def testShapeInference(self):
+    # Shape inference works and produces expected output where possible
+    rgb_shape = [7, None, 19, 3]
+    gray_shape = rgb_shape[:-1] + [1]
+    with self.test_session():
+      rgb_tf = array_ops.placeholder(dtypes.uint8, shape=rgb_shape)
+      gray = image_ops.rgb_to_grayscale(rgb_tf)
+      self.assertEqual(gray_shape, gray.get_shape().as_list())
+
+    with self.test_session():
+      gray_tf = array_ops.placeholder(dtypes.uint8, shape=gray_shape)
+      rgb = image_ops.grayscale_to_rgb(gray_tf)
+      self.assertEqual(rgb_shape, rgb.get_shape().as_list())
+
+    # Shape inference does not break for unknown shapes
+    with self.test_session():
+      rgb_tf_unknown = array_ops.placeholder(dtypes.uint8)
+      gray_unknown = image_ops.rgb_to_grayscale(rgb_tf_unknown)
+      self.assertFalse(gray_unknown.get_shape())
+
+    with self.test_session():
+      gray_tf_unknown = array_ops.placeholder(dtypes.uint8)
+      rgb_unknown = image_ops.grayscale_to_rgb(gray_tf_unknown)
+      self.assertFalse(rgb_unknown.get_shape())
+
 
 class AdjustHueTest(test_util.TensorFlowTestCase):
 
