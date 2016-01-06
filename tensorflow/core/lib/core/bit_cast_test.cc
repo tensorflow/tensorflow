16a671c0c5977f3d04c36811a72412cd0d74296f
Josh Levenberg <josh11b@tensorflow.org>
2016-01-06 12:51:38 -0800

    Execute TODO to make #include more precise. Change: 111457566

--- tensorflow/core/lib/core/bit_cast_test.cc
+++ tensorflow/core/lib/core/bit_cast_test.cc
@@ -15,9 +15,10 @@ limitations under the License.
 
 // Unit test for bit_cast template.
 
-#include "tensorflow/core/lib/core/casts.h"
 #include <gtest/gtest.h>
+#include "tensorflow/core/lib/core/casts.h"
 #include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/macros.h"
 
 namespace tensorflow {
 
