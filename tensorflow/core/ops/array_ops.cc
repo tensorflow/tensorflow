831718549c51d57479bef0f65d22539505c2b4b5
Dongjoon Hyun <dongjoon@apache.org>
2016-01-06 16:09:31 -0800

    Fix documentation typos in ops.
    Since this PR does not include the fixes on generated docs,
    the corresponding md files need to be regenerated after merging this PR.

--- tensorflow/core/ops/array_ops.cc
+++ tensorflow/core/ops/array_ops.cc
@@ -456,7 +456,7 @@ Computes the inverse permutation of a tensor.
 
 This operation computes the inverse of an index permutation. It takes a 1-D
 integer tensor `x`, which represents the indices of a zero-based array, and
-swaps each value with its index position. In other words, for an ouput tensor
+swaps each value with its index position. In other words, for an output tensor
 `y` and an input tensor `x`, this operation computes the following:
 
 `y[x[i]] = i for i in [0, 1, ..., len(x) - 1]`
