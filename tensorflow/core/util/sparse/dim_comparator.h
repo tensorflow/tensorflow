36b25e83bf022ad6b67bb22ac5adb4c0243a78a7
Josh Levenberg <josh11b@tensorflow.org>
2016-01-12 16:55:12 -0800

    Make .h includes more comprehensive in tensorflow/core/. Change: 111999765

--- tensorflow/core/util/sparse/dim_comparator.h
+++ tensorflow/core/util/sparse/dim_comparator.h
@@ -17,8 +17,10 @@ limitations under the License.
 #define TENSORFLOW_UTIL_SPARSE_DIM_COMPARATOR_H_
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/lib/gtl/array_slice.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/port.h"
+#include "tensorflow/core/platform/types.h"
 
 namespace tensorflow {
 namespace sparse {
