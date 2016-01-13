9e03a7b62f75cf56766df6e22a79c6fbd1828538
Geoffrey Irving <geoffreyi@google.com>
2016-01-13 14:05:27 -0800

    Add comparison operators for remaining realnumbertypes
    uint8, int8, and int16 were missing for =, !=, <, >, <=, >=.
    Change: 112066815

--- tensorflow/core/kernels/cwise_op_gpu_equal_to.cu.cc
+++ tensorflow/core/kernels/cwise_op_gpu_equal_to.cu.cc
@@ -19,7 +19,7 @@ limitations under the License.
 
 namespace tensorflow {
 namespace functor {
-DEFINE_BINARY4(equal_to, float, double, int64, complex64);
+DEFINE_BINARY7(equal_to, float, double, uint8, int8, int16, int64, complex64);
 }  // namespace functor
 }  // namespace tensorflow
 
