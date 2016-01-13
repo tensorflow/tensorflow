Fix fill gradient test for legacy scalar strictness Change: 112005548

tensorflow/python/kernel_tests/constant_op_test.py
@@ -507,7 +507,7 @@ class FillTest(tf.test.TestCase):
       in_v = tf.constant(5.0)
       out_shape = [3, 2]
       out_filled = tf.fill(out_shape, in_v)
-      err = tf.test.compute_gradient_error(in_v, [1],
+      err = tf.test.compute_gradient_error(in_v, [],
                                            out_filled, out_shape)
     self.assertLess(err, 1e-3)
 

tensorflow/python/ops/array_ops.py
@@ -335,9 +335,8 @@ def _ConcatShape(op):
         value.get_shape().assert_has_rank(rank)
       else:
         rank = value.get_shape().ndims
-    # TODO(irving): Remove once !kAllowLegacyScalars.
-    if rank is not None:
-      rank = max(rank, 1)
+    if rank == 0:
+      raise ValueError("Can't concatenate scalars (use tf.pack instead)")
     return [tensor_shape.unknown_shape(ndims=rank)]
 
   else:
@@ -345,19 +344,11 @@ def _ConcatShape(op):
     # output shape.
     concat_dim = int(concat_dim)
     output_shape = op.inputs[1].get_shape()
-    # TODO(irving): Remove once !kAllowLegacyScalars.
-    if output_shape.ndims == 0:
-      output_shape = tensor_shape.TensorShape([1])
     for value in op.inputs[2:]:
       value_shape = value.get_shape()
       if value_shape.ndims is not None and concat_dim >= value_shape.ndims:
-        if value_shape.ndims == 0 and concat_dim == 0:
-          # Let concat handle scalars
-          # TODO(irving): Remove once !kAllowLegacyScalars.
-          value_shape = tensor_shape.TensorShape([1])
-        else:
-          raise ValueError("concat_dim is out of range (values rank = %d)" %
-                           value_shape.ndims)
+        raise ValueError("concat_dim is out of range (values rank = %d)" %
+                         value_shape.ndims)
       before = output_shape[:concat_dim].merge_with(value_shape[:concat_dim])
       at = output_shape[concat_dim] + value_shape[concat_dim]
       after = output_shape[
@@ -968,11 +959,7 @@ def _PadShape(op):
   """
   paddings_shape = op.inputs[1].get_shape().with_rank(2)
   input_shape = op.inputs[0].get_shape()
-  if input_shape.ndims == 0 and paddings_shape[0].value == 1:
-    # TODO(irving): Remove once !kAllowLegacyScalars.
-    input_shape = tensor_shape.TensorShape([1])
-  else:
-    input_shape = input_shape.with_rank(paddings_shape[0].value)
+  input_shape = input_shape.with_rank(paddings_shape[0].value)
   paddings_shape = paddings_shape.merge_with(
       tensor_shape.matrix(input_shape.ndims, 2))
   paddings = tensor_util.ConstantValue(op.inputs[1])