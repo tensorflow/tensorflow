e7183d14233ffd07c33b08d893a7edf548ac89c6
Jan Prach <jendap@google.com>
2016-01-19 14:36:37 -0800

    ci_build: add support for python3 build

--- tensorflow/tools/ci_build/install/install_deb_packages.sh
+++ tensorflow/tools/ci_build/install/install_deb_packages.sh
@@ -26,6 +26,9 @@ apt-get install -y \
     python-dev \
     python-numpy \
     python-pip \
+    python3-dev \
+    python3-numpy \
+    python3-pip \
     software-properties-common \
     swig \
     unzip \
