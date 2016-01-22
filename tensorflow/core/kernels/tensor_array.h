e14090d217fc4e7e49ac04ccbc50acdba8b9f120
Eugene Brevdo <ebrevdo@gmail.com>
2016-01-21 16:00:25 -0800

    Add TensorArray data_flow_ops wrapper and TensorArray gradients.
    These are undocumented features and the API can and will change.
    Change: 112733605

--- tensorflow/core/kernels/tensor_array.h
+++ tensorflow/core/kernels/tensor_array.h
@@ -69,10 +69,11 @@ TF_CALL_GPU_NUMBER_TYPES(TENSOR_ARRAY_WRITE_OR_ADD_GPU);
 class TensorArray : public ResourceBase {
  public:
   TensorArray(const DataType& dtype, const Tensor& handle, int32 N)
-      : dtype_(dtype), handle_(handle), tensor_array_(N) {}
+      : dead_(false), dtype_(dtype), handle_(handle), tensor_array_(N) {}
 
   Status Write(const int32 index, const PersistentTensor& value) {
     mutex_lock l(mu_);
+    TF_RETURN_IF_ERROR(LockedReturnIfDead());
     if (index < 0 || index >= tensor_array_.size()) {
       return errors::InvalidArgument("Tried to write to index ", index,
                                      " but array size is: ",
@@ -91,6 +92,7 @@ class TensorArray : public ResourceBase {
   Status WriteOrAdd(OpKernelContext* ctx, const int32 index,
                     PersistentTensor value) {
     mutex_lock l(mu_);
+    TF_RETURN_IF_ERROR(LockedReturnIfDead());
     if (index < 0 || index >= tensor_array_.size()) {
       return errors::InvalidArgument("Tried to write to index ", index,
                                      " but array size is: ",
@@ -116,6 +118,7 @@ class TensorArray : public ResourceBase {
 
   Status Read(const int32 index, PersistentTensor* value) {
     mutex_lock l(mu_);
+    TF_RETURN_IF_ERROR(LockedReturnIfDead());
     if (index < 0 || index >= tensor_array_.size()) {
       return errors::InvalidArgument("Tried to read from index ", index,
                                      " but array size is: ",
@@ -132,20 +135,39 @@ class TensorArray : public ResourceBase {
 
   inline int32 Size() {
     mutex_lock l(mu_);
+    DCHECK(!dead_);
     return tensor_array_.size();
+  }
+
+  inline Status LockedReturnIfDead() const {
+    if (dead_) {
+      return errors::InvalidArgument("Tensor ", handle_.vec<string>()(1),
+                                     " has already been closed.");
+    }
+    return Status::OK();
   }
 
   DataType ElemType() { return dtype_; }
 
   string DebugString() override {
     mutex_lock l(mu_);
+    DCHECK(!dead_);
     return strings::StrCat("TensorArray[", tensor_array_.size(), "]");
+  }
+
+  inline bool IsDead() const { return dead_; }
+
+  void ClearAndMarkDead() {
+    mutex_lock l(mu_);
+    tensor_array_.clear();
+    dead_ = true;
   }
 
   mutex* mu() { return &mu_; }
   Tensor* handle() { return &handle_; }
 
  private:
+  bool dead_;  // Marks that the tensor_array_ has been cleared.
   mutex mu_;
   DataType dtype_;
   Tensor handle_;
--- tensorflow/python/ops/data_flow_ops.py
+++ tensorflow/python/ops/data_flow_ops.py
@@ -22,12 +22,14 @@ from __future__ import print_function
 import collections
 import re
 
+from tensorflow.python.framework import dtypes as _dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import random_seed
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_util
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import common_shapes
+from tensorflow.python.ops import constant_op
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import gen_data_flow_ops
 # pylint: disable=wildcard-import
@@ -535,6 +537,114 @@ def initialize_all_tables(name="init_all_tables"):
   return control_flow_ops.no_op(name=name)
 
 
+class TensorArray(object):
+  """Class wrapping dynamic-sized, per-time-step, write-once Tensor arrays.
+
+  This class is meant to be used with dynamic iteration primitives such as
+  `While` loops, and supports gradient back-propagation via special "flow"
+  control flow dependencies.
+
+  @@handle
+  @@flow
+
+  @@read
+  @@unpack
+
+  @@write
+  @@pack
+
+  @@grad
+  """
+
+  def __init__(
+      self, dtype, size, tensor_array_name=None, handle=None, name=None):
+    """Construct a new TensorArray or wrap an existing TensorArray handle.
+
+    Args:
+      dtype: (required) data type of the TensorArray.
+      size: (required) int32 scalar `Tensor`: the size of the TensorArray.
+      tensor_array_name: (optional) Python string: the name of the TensorArray.
+        This is used when creating the TensorArray handle.  If this value is
+        set, handle should be None.
+      handle: (optional) A `Tensor` handle to an existing TensorArray.  If this
+        is set, tensor_array_name should be None.
+      name: A name for the operation (optional).
+
+    Raises:
+      ValueError: if both handle and tensor_array_name are provided.
+    """
+    if handle and tensor_array_name:
+      raise ValueError(
+          "Cannot construct with both handle and tensor_array_name")
+
+    with ops.op_scope([handle, size], name, "TensorArray") as scope:
+      if handle:
+        self._handle = handle
+      else:
+        self._handle = gen_data_flow_ops._tensor_array(
+            dtype=dtype, size=size, tensor_array_name=tensor_array_name,
+            name=scope)
+
+      self._flow = constant_op.constant(0, dtype=_dtypes.float32)
+      self._dtype = dtype
+      self._gradient_add = False
+
+  @property
+  def flow(self):
+    """The flow `Tensor` forcing ops leading to this TensorArray state."""
+    return self._flow
+
+  @property
+  def handle(self):
+    """The reference to the TensorArray."""
+    return self._handle
+
+  def grad(self):
+    g = TensorArray(
+        dtype=self._dtype,
+        size=-1,
+        handle=gen_data_flow_ops._tensor_array_grad(self._handle))
+    g._gradient_add = True
+    return g
+
+  def read(self, index):
+    """Read the value at location `index` in the TensorArray."""
+    value = gen_data_flow_ops._tensor_array_read(
+        handle=self._handle, index=index, flow_in=self._flow, dtype=self._dtype)
+    return value
+
+  def write(self, index, value):
+    """Write `value` into index `index` of the TensorArray."""
+    flow_out = gen_data_flow_ops._tensor_array_write(
+        handle=self._handle, index=index, value=value, flow_in=self._flow,
+        gradient_add=self._gradient_add)
+    # Size below is ignored
+    ta = TensorArray(dtype=self._dtype, size=-1, handle=self._handle)
+    ta._gradient_add = self._gradient_add
+    ta._flow = flow_out
+    return ta
+
+  def pack(self):
+    """Return the values in the TensorArray as a packed `Tensor`."""
+    value = gen_data_flow_ops._tensor_array_pack(
+        handle=self._handle, flow_in=self._flow, dtype=self._dtype)
+    return value
+
+  def unpack(self, value):
+    """Packs the values of a `Tensor` in the TensorArray."""
+    flow_out = gen_data_flow_ops._tensor_array_unpack(
+        handle=self._handle, value=value, flow_in=self._flow,
+        gradient_add=self._gradient_add)
+    ta = TensorArray(dtype=self._dtype, size=-1, handle=self._handle)
+    ta._gradient_add = self._gradient_add
+    ta._flow = flow_out
+    return ta
+
+  def close(self):
+    """Close the current TensorArray."""
+    return gen_data_flow_ops._tensor_array_close(handle=self._handle)
+
+
 ops.NoGradient("LookupTableFind")
 ops.NoGradient("LookupTableSize")
 ops.NoGradient("HashTable")
@@ -576,18 +686,22 @@ def _TensorArrayShape(op):
 
 @ops.RegisterShape("TensorArrayRead")
 def _TensorArrayReadShape(op):
-  # handle, index, value
+  # handle, index, flow_in
   op.inputs[0].get_shape().merge_with(tensor_shape.vector(2))
   op.inputs[1].get_shape().merge_with(tensor_shape.scalar())
+  op.inputs[2].get_shape().merge_with(tensor_shape.scalar())
+  # value
   return [tensor_shape.unknown_shape()]
 
 
 @ops.RegisterShape("TensorArrayWrite")
 def _TensorArrayWriteShape(op):
-  # handle, index
+  # handle, index, value, flow_in
   op.inputs[0].get_shape().merge_with(tensor_shape.vector(2))
   op.inputs[1].get_shape().merge_with(tensor_shape.scalar())
-  return []
+  op.inputs[3].get_shape().merge_with(tensor_shape.scalar())
+  # flow_out
+  return [tensor_shape.scalar()]
 
 
 @ops.RegisterShape("TensorArrayClose")
@@ -606,14 +720,20 @@ def _TensorArrayGradShape(op):
 
 @ops.RegisterShape("TensorArrayPack")
 def _TensorArrayPackShape(op):
+  # handle, flow_in
   op.inputs[0].get_shape().merge_with(tensor_shape.vector(2))
+  op.inputs[1].get_shape().merge_with(tensor_shape.scalar())
+  # value
   return [tensor_shape.unknown_shape()]
 
 
 @ops.RegisterShape("TensorArrayUnpack")
 def _TensorArrayUnpackShape(op):
+  # handle, value, flow_in
   op.inputs[0].get_shape().merge_with(tensor_shape.vector(2))
-  return []
+  op.inputs[2].get_shape().merge_with(tensor_shape.scalar())
+  # flow_out
+  return [tensor_shape.scalar()]
 
 
 @ops.RegisterShape("DynamicPartition")
