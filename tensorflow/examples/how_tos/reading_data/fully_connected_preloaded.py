ebe109b76d3a8fbdfadee8fa1e6e642563c88034
Yaroslav Bulatov <yaroslavvb@google.com>
2016-01-22 16:21:34 -0800

    Pin constant holding data to CPU. This makes GPU version 50-100x faster per step. Fixes #837 Change: 112829709

--- tensorflow/examples/how_tos/reading_data/fully_connected_preloaded.py
+++ tensorflow/examples/how_tos/reading_data/fully_connected_preloaded.py
@@ -65,9 +65,10 @@ def run_training():
   # Tell TensorFlow that the model will be built into the default Graph.
   with tf.Graph().as_default():
     with tf.name_scope('input'):
-      # Input data
-      input_images = tf.constant(data_sets.train.images)
-      input_labels = tf.constant(data_sets.train.labels)
+      # Input data, pin to CPU because rest of pipeline is CPU-only
+      with tf.device('/cpu:0'):
+        input_images = tf.constant(data_sets.train.images)
+        input_labels = tf.constant(data_sets.train.labels)
 
       image, label = tf.train.slice_input_producer(
           [input_images, input_labels], num_epochs=FLAGS.num_epochs)
