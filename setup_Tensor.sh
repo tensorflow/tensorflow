a463c10f1b38e1c345f861c66438efaa7de5a922
bellaj <bellaj.badr@gmail.com>
2016-01-25 11:35:06 +0000

    Update setup_Tensor.sh

--- setup_Tensor.sh
+++ setup_Tensor.sh
@@ -1,18 +1,55 @@
 #!/bin/bash  
 # original instructions are located in https://github.com/tensorflow/tensorflow/blob/master/tensorflow/g3doc/get_started/os_setup.md
-# this is a bash file to help you install tenserflow 
-#run this file from a shell   
-#Make sure you use a capital V, or you will launch Python in verbose mode;
+# this is a bash file to help newbies to install tenserflow 
+# run this file from a shell   cd shell location and ./setup_tensor.sh
+echo "Tensofrlow installation has started"
 
-echo "please check if you have python version <3, otherwise follow these instructions https://github.com/tensorflow/tensorflow/blob/master/tensorflow/g3doc/get_started/os_setup.md
-"
-python -V 
+
+echo "choose your package installer yum(CentOS, Fedora, OpenSuse, RedHat), zypper, apt-get(Debian,ubuntu...), yast(opensuse),....."
+read installer
+echo "TensorFlow is available only for 32bit architechture"
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; 
+then
+ echo "Your Proc architecture is x86_64" 
+else
+ echo "you have a 32-bit processor, TensorFlow could not be installed" 
+ exit 0
+
+fi
+
+#**************************************
+version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
+if [[ -z "$version" ]]
+then
+    echo "No Python!" 
+    pythonV=0
+    sudo $installer install python
+    ech "python is insallaed, please resatrt this script"
+    exit 0
+
+else
+    pythonV=$version
+fi
+
+parsedVersion=$(echo "${version//./}")
+if [[ "$parsedVersion" -lt "300" ]] #python version <3
+then 
+    pythonV=2
+else
+    pythonV=3
+fi
+
+echo "python $version is installed"
+#********************************************/
+ 
 
 #Install pip (or pip3 for python3) if it is not already installed:
 # Ubuntu/Linux 64-bit
 echo "Installing PIP and Virtual Environment"
 
-sudo apt-get install python-pip python-dev python-virtualenv
+sudo $installer install python-pip python-dev python-virtualenv
 
 echo "do you like to use python virtual environment(1) or not(0)?"
 read env
@@ -26,10 +63,17 @@ read choice
 if [ $choice -eq 0 ]; 
 then
 # Ubuntu/Linux 64-bit, CPU only:
- sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
-elif [ $choice -eq 1 ]; then
-# Ubuntu/Linux 64-bit, GPU enabled:
-sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+   if [ "$pythonV" -lt 3 ] 
+    then sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+    else sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+   fi
+elif [ $choice -eq 1 ]; 
+then
+# Ubuntu/Linux 64-bit, CPU only:
+   if [ "$pythonV" -lt 3 ] 
+    then sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+    else sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+   fi
 else "bad choice, restart"
 fi
 echo "--------------------------------------------------------------"
@@ -65,27 +109,31 @@ read choice
 if [ $choice -eq 0 ]; 
 then
 # Ubuntu/Linux 64-bit, CPU only:
- sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+ if [ "$pythonV" -lt 3 ] 
+    then sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+
+    else sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+   fi
+
 elif [ $choice -eq 1 ]; then
-# Ubuntu/Linux 64-bit, GPU enabled:
-sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+ if [ "$pythonV" -lt 3 ] 
+    then sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+
+    else sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+   fi
 else "bad choice, restart"
 fi
-echo "--------------------------------------------------------------"
+echo "********************************"
 ###############
 deactivate
 
 fi
-
-echo "--------------------------------------------------------------"
-echo ". "
-
+ 
 echo "To use TensorFlow later you will have to activate the Virtualenv environment again:"
-
 echo "$ source ~/tensorflow/bin/activate   If using bash."
 echo "$ source ~/tensorflow/bin/activate.csh   If using csh."
 echo "(tensorflow)$   Your prompt should change."
 echo "# Run Python programs that use TensorFlow."
 #...
-echo "# When you are done using TensorFlow, deactivate the environment."
+echo "# When you finish using TensorFlow, deactivate the environment."
 echo "(tensorflow)$ deactivate"
