f6db6bfc342003a3ce4c73246b9350a3e37296df
alexggmatthews <alexggmatthews@googlemail.com>
2016-01-28 17:04:53 +0000

    Matches matlab implementation.

--- tensorflow/core/user_ops/python_chol_rev.py
+++ tensorflow/core/user_ops/python_chol_rev.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+def chol_rev( L, L_bar ):
+    N = L.shape[0]
+    NB = 2
+    A_bar = np.tril( L_bar )
+    for Ji in range( (N-NB+1), (1-NB+1)-1, -NB ):
+        J = max(1, Ji);
+        JB = NB - (J - Ji); # corrected block-size        
+        A_bar[ (J+JB-1):, (J-1):(J+JB-1) ] = np.linalg.solve( L[(J-1):(J+JB-1), (J-1):(J+JB-1)].T, A_bar[ (J+JB-1):, (J-1):(J+JB-1)].T ).T
+        A_bar[ (J-1):(J+JB-1), (J-1):(J+JB-1)] = A_bar[(J-1):J+JB-1, (J-1):J+JB-1] - np.tril( np.dot( A_bar[(J+JB-1):, (J-1):J+JB-1].T, L[(J+JB-1):, (J-1):J+JB-1] ) )
+        A_bar[ (J+JB-1):N, 0:(J-1)]  = A_bar[(J+JB-1):N, 0:J-1] - np.dot( A_bar[(J+JB-1):N, (J-1):J+JB-1], L[(J-1):J+JB-1, 0:J-1] )
+        A_bar[ (J-1):(J+JB-1), 0:(J-1) ] = A_bar[ (J-1):J+JB-1, 0:J-1 ] - np.dot( A_bar[ (J+JB-1):N, (J-1):J+JB-1].T , L[(J+JB-1):N, 0:J-1] )
+        A_bar[ (J-1):(J+JB-1), (J-1):(J+JB-1) ] = chol_rev_unblocked(L[(J-1):(J+JB-1), (J-1):(J+JB-1)], A_bar[(J-1):(J+JB-1), (J-1):(J+JB-1)] )
+        A_bar[ (J-1):(J+JB-1), 0:(J-1) ] = A_bar[ (J-1):(J+JB-1), 0:(J-1) ] - np.dot( (A_bar[(J-1):(J+JB-1), (J-1):(J+JB-1)] + A_bar[(J-1):(J+JB-1), (J-1):(J+JB-1)].T ),L[(J-1):(J+JB-1), 0:(J-1) ] );
+    return A_bar
+        
 def chol_rev_unblocked( L, L_bar ):
     N = L.shape[0]
     A_bar = np.tril( L_bar )
@@ -17,4 +32,4 @@ if __name__ == "__main__":
     b = np.tril( np.array( range( 1, N**2+1, 1) ).reshape( N, N ).T *2. )
     print "a ", a
     print "b ", b
-    print "chol_rev_unblocked( a , b ) ", chol_rev_unblocked( a , b )
+    print "chol_rev( a , b ) ", chol_rev( a , b )
