ce05f30bff67aeb12605a5a12f66ec2fbbb9ae18
alexggmatthews <alexggmatthews@googlemail.com>
2016-01-28 17:51:09 +0000

    Using Murray style.

--- tensorflow/core/user_ops/chol_grad.cc
+++ tensorflow/core/user_ops/chol_grad.cc
@@ -7,8 +7,25 @@
 
 #include "tensorflow/core/framework/op.h"
 
-REGISTER_OP("CholeskyGrad").Input("l: T").Input("f: T").Output("g: T").Attr( "T: {float, double}").Doc("Cholesky backpropagation where l is output of Cholesky algorithm and f is gradient of some loss wrt l");
+REGISTER_OP("CholeskyGrad").Input("l: T").Input("l_bar: T").Output("a_bar: T").Attr( "T: {float, double}").Doc("Cholesky backpropagation where l is output of Cholesky algorithm and f is gradient of some loss wrt l");
 
+/*
+CHOL_REV_UNBLOCKED push derivatives back through a Cholesky decomposition
+
+     A_bar = chol_rev_unblocked(L, L_bar)
+
+ Back-propagate derivatives of some function f, through the Cholesky
+ decomposition:
+ A_bar = tril(df/dA), when L = chol(A, 'lower') and L_bar = df/dL.
+
+ Inputs:
+          L NxN lower-triangular matrix, resulting from chol(A, 'lower'),
+                where A is a symmetric +ve definite matrix
+      L_bar NxN df/dL for some scalar function f
+
+ Outputs:
+      A_bar NxN tril(df/dA)
+*/
 
 #include "tensorflow/core/framework/op_kernel.h"
 
@@ -27,29 +44,28 @@ class CholeskyGrad : public OpKernel {
     using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
     using ConstMatrixMap = Eigen::Map<const Matrix>;
     using MatrixMap = Eigen::Map<Matrix>;
-    
+
     void Compute(OpKernelContext* context) override {
 
-    //Grab the input tensors.
     const Tensor& input_tensor_l = context->input(0); ;
-    const Tensor& input_tensor_f = context->input(1); 
-    
+    const Tensor& input_tensor_l_bar = context->input(1); 
+
     //Check that input tensors represent a matrix.
     OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_l.shape()), errors::InvalidArgument("In[0] is not a matrix"));
-    OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_f.shape()), errors::InvalidArgument("In[1] is not a matrix"));
+    OP_REQUIRES(context, TensorShapeUtils::IsMatrix(input_tensor_l_bar.shape()), errors::InvalidArgument("In[1] is not a matrix"));
 
     //Check that input tensors are square.
     OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_l.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
-    OP_REQUIRES(context, input_tensor_f.dim_size(0) == input_tensor_f.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
-    
+    OP_REQUIRES(context, input_tensor_l_bar.dim_size(0) == input_tensor_l_bar.dim_size(1), errors::InvalidArgument("Input matrix must be square."));
+
     //Check that input tensors are of same size.
-    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_f.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));
-    
+    OP_REQUIRES(context, input_tensor_l.dim_size(0) == input_tensor_l_bar.dim_size(0), errors::InvalidArgument("Input matrices must be of same size."));    
+
     // Create an output tensor
     Tensor* output_tensor = NULL;
     
-    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor_f.shape(), &output_tensor));
-    
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor_l_bar.shape(), &output_tensor));
+
     if (output_tensor->NumElements() == 0) {
       // the output shape is a 0-element matrix, so there is nothing to do.
       return;
@@ -57,26 +73,37 @@ class CholeskyGrad : public OpKernel {
     
     //The next three lines are necessary to get Eigen matrix behaviour.
     const ConstMatrixMap input_matrix_l(input_tensor_l.flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1));
-    const ConstMatrixMap input_matrix_f(input_tensor_f.flat<T>().data(), input_tensor_f.dim_size(0), input_tensor_f.dim_size(1));
-    MatrixMap output_matrix(output_tensor->template flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1) );
-    
+    const ConstMatrixMap input_matrix_l_bar(input_tensor_l_bar.flat<T>().data(), input_tensor_l_bar.dim_size(0), input_tensor_l_bar.dim_size(1));
+    MatrixMap output_matrix(output_tensor->template flat<T>().data(), input_tensor_l.dim_size(0), input_tensor_l.dim_size(1) );    
+
     const int N = input_matrix_l.rows();
 
-    output_matrix = input_matrix_f.template triangularView<Eigen::Lower>();
+    output_matrix = input_matrix_l_bar.template triangularView<Eigen::Lower>();
+
+    /*
+    for J = N:-1:1
+        L_bar(J,J) = L_bar(J,J) - L(J+1:N,J)'*L_bar(J+1:N,J) / L(J,J);
+        L_bar(J:N,J) = L_bar(J:N,J) / L(J,J);
+        L_bar(J,1:J-1) = L_bar(J,1:J-1) - L_bar(J:N,J)'*L(J:N,1:J-1);
+        L_bar(J+1:N,1:J-1) = L_bar(J+1:N,1:J-1) - L_bar(J+1:N,J)*L(J,1:J-1);
+        L_bar(J,J) = 0.5 * L_bar(J,J); % can take out of loop if like.
+    end
+    */
     
-    output_matrix(N-1,N-1) /= 2 * input_matrix_l(N-1,N-1);
+    //TODO: what to do if input_matrix_l isn't lower triangular?
     
-    for ( int k = N-2; k>=0; k--)
+    for ( int k = N-1; k>=0; k--)
     {
-        output_matrix.block( k+1, k, N-k-1, 1) -= output_matrix.block( k+1,k+1, N-k-1, N-k-1 ).template selfadjointView<Eigen::Lower>() * input_matrix_l.block( k+1, k, N-k-1, 1);
-    	output_matrix.block( k+1, k, N-k-1, 1) -= output_matrix.diagonal().block( k+1, 0, N-k-1 , 1).cwiseProduct( input_matrix_l.block( k+1, k, N-k-1, 1) );  
-    	output_matrix.block( k+1, k, N-k-1, 1) /= input_matrix_l(k,k);
-    	output_matrix(k,k) -= (output_matrix.block( k+1, k, N-k-1, 1).adjoint()*input_matrix_l.block( k+1, k, N-k-1, 1))(0,0);
-    	output_matrix(k,k) /= 2* input_matrix_l(k,k);
+        output_matrix(k,k) -= (input_matrix_l.block( k+1,k , N-(k+1), 1 ).adjoint() * output_matrix.block( k+1,k , N-(k+1), 1 ) )(0,0) / input_matrix_l(k,k);
+        output_matrix.block(k,k,N-k,1) = output_matrix.block(k,k,N-k,1) / input_matrix_l( k,k) ;
+        output_matrix.block(k,0,1,k) -=output_matrix.block(k,k,N-k,1).adjoint() * input_matrix_l.block(k,0,N-k,k);
+        output_matrix.block(k+1,0,N-(k+1),k) -= output_matrix.block(k+1, k , N-(k+1), 1 ) * input_matrix_l.block( k, 0, 1, k ) ; 
+        output_matrix(k,k) *= 0.5;
     }
-    
+
     //This symmetrizes the result effectively assigning equal contribution to the gradient for the two symmetric halves.
-    output_matrix = (0.5 * ( output_matrix +  output_matrix.transpose() )).eval();
+    //output_matrix = (0.5 * ( output_matrix +  output_matrix.transpose() )).eval();
+    //output_matrix.template triangularView<Eigen::Upper>() =  output_matrix.template triangularView<Eigen::Lower>().adjoint() ; 
 
     }
 };
