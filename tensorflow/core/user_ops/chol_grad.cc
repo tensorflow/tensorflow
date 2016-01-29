bb1501a88d801d45bb2c8e776ad2c7e2d487b3cc
alexggmatthews <alexggmatthews@googlemail.com>
2016-01-29 14:23:14 +0000

    Pulling out into separate function.

--- tensorflow/core/user_ops/chol_grad.cc
+++ tensorflow/core/user_ops/chol_grad.cc
@@ -6,26 +6,9 @@
 //and implementation from AutoGrad by Dougal Maclaurin, David Duvenaud and Matt Johnson, 2015.
 
 #include "tensorflow/core/framework/op.h"
+#include "third_party/eigen3/Eigen/Core"
 
 REGISTER_OP("CholeskyGrad").Input("l: T").Input("l_bar: T").Output("a_bar: T").Attr( "T: {float, double}").Doc("Cholesky backpropagation where l is output of Cholesky algorithm and f is gradient of some loss wrt l");
-
-/*
-CHOL_REV_UNBLOCKED push derivatives back through a Cholesky decomposition
-
-     A_bar = chol_rev_unblocked(L, L_bar)
-
- Back-propagate derivatives of some function f, through the Cholesky
- decomposition:
- A_bar = tril(df/dA), when L = chol(A, 'lower') and L_bar = df/dL.
-
- Inputs:
-          L NxN lower-triangular matrix, resulting from chol(A, 'lower'),
-                where A is a symmetric +ve definite matrix
-      L_bar NxN df/dL for some scalar function f
-
- Outputs:
-      A_bar NxN tril(df/dA)
-*/
 
 #include "tensorflow/core/framework/op_kernel.h"
 
@@ -44,6 +27,8 @@ class CholeskyGrad : public OpKernel {
     using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
     using ConstMatrixMap = Eigen::Map<const Matrix>;
     using MatrixMap = Eigen::Map<Matrix>;
+    using ConstRef = Eigen::Ref<const Matrix>;
+    using Ref = Eigen::Ref<Matrix>;
 
     void Compute(OpKernelContext* context) override {
 
@@ -80,32 +65,30 @@ class CholeskyGrad : public OpKernel {
 
     output_matrix = input_matrix_l_bar.template triangularView<Eigen::Lower>();
 
-    /*
-    for J = N:-1:1
-        L_bar(J,J) = L_bar(J,J) - L(J+1:N,J)'*L_bar(J+1:N,J) / L(J,J);
-        L_bar(J:N,J) = L_bar(J:N,J) / L(J,J);
-        L_bar(J,1:J-1) = L_bar(J,1:J-1) - L_bar(J:N,J)'*L(J:N,1:J-1);
-        L_bar(J+1:N,1:J-1) = L_bar(J+1:N,1:J-1) - L_bar(J+1:N,J)*L(J,1:J-1);
-        L_bar(J,J) = 0.5 * L_bar(J,J); % can take out of loop if like.
-    end
-    */
+    chol_rev_unblocked( input_matrix_l, output_matrix );
     
     //TODO: what to do if input_matrix_l isn't lower triangular?
     
-    for ( int k = N-1; k>=0; k--)
-    {
-        output_matrix(k,k) -= (input_matrix_l.block( k+1,k , N-(k+1), 1 ).adjoint() * output_matrix.block( k+1,k , N-(k+1), 1 ) )(0,0) / input_matrix_l(k,k);
-        output_matrix.block(k,k,N-k,1) /= input_matrix_l( k,k) ;
-        output_matrix.block(k,0,1,k) -=output_matrix.block(k,k,N-k,1).adjoint() * input_matrix_l.block(k,0,N-k,k);
-        output_matrix.block(k+1,0,N-(k+1),k) -= output_matrix.block(k+1, k , N-(k+1), 1 ) * input_matrix_l.block( k, 0, 1, k ) ; 
-        output_matrix(k,k) *= 0.5;
-    }
-
     //This symmetrizes the result effectively assigning equal contribution to the gradient for the two symmetric halves.
     //output_matrix = (0.5 * ( output_matrix +  output_matrix.transpose() )).eval();
     //output_matrix.template triangularView<Eigen::Upper>() =  output_matrix.template triangularView<Eigen::Lower>().adjoint() ; 
 
     }
+    
+    void chol_rev_unblocked(const ConstRef l_block, Ref l_bar_block)
+    {
+        const int N = l_block.rows();
+        
+        for ( int k = N-1; k>=0; k--)
+        {
+            l_bar_block(k,k) -= (l_block.block( k+1,k , N-(k+1), 1 ).adjoint() * l_bar_block.block( k+1,k , N-(k+1), 1 ) )(0,0) / l_block(k,k);
+            l_bar_block.block(k,k,N-k,1) /= l_block( k,k) ;
+            l_bar_block.block(k,0,1,k) -=l_bar_block.block(k,k,N-k,1).adjoint() * l_block.block(k,0,N-k,k);
+            l_bar_block.block(k+1,0,N-(k+1),k) -= l_bar_block.block(k+1, k , N-(k+1), 1 ) * l_block.block( k, 0, 1, k ) ; 
+            l_bar_block(k,k) *= 0.5;
+        }
+    }
+    
 };
 
 REGISTER_KERNEL_BUILDER(
