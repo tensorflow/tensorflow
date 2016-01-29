ca255fdd2446980384acda685cd0e419d32ed13b
Shanqing Cai <cais@google.com>
2016-01-29 10:47:33 -0500

    Minor topy fix

--- tensorflow/tools/ci_build/builds/test_on_install.sh
+++ tensorflow/tools/ci_build/builds/test_on_install.sh
@@ -4,7 +4,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#q
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
