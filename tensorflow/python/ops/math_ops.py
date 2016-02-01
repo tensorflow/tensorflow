8525dbdc02b0c45c38ba16cfba26856eece2b851
Pete Warden <pete@petewarden.com>
2016-02-01 14:59:09 -0800

    Fix for MatMul statistics transpose error, fixes #899. Change: 113571550

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -965,9 +965,9 @@ def _calc_mat_mul_flops(graph, node):
   a_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
   a_shape.assert_is_fully_defined()
   if transpose_a:
-    k = int(a_shape[1])
-  else:
     k = int(a_shape[0])
+  else:
+    k = int(a_shape[1])
   output_shape = graph_util.tensor_shape_from_node_def_name(graph, node.name)
   output_shape.assert_is_fully_defined()
   output_count = np.prod(output_shape.as_list())
