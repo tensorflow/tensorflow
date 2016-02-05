31521d500ca42dc17f0fea9d7328723d88d1e5e2
terrytangyuan <terrytangyuan@gmail.com>
2016-02-04 22:07:38 -0600

    * Refactored batch_normalize

--- examples/resnet.py
+++ examples/resnet.py
@@ -29,93 +29,29 @@ import skflow
 from collections import namedtuple
 from math import sqrt
 
-
-"""Batch Normalization for TensorFlow.
-Parag K. Mital, Jan 2016."""
-
-class batch_normalize(object):
-    """Basic usage from: http://stackoverflow.com/a/33950177
-
-    Parag K. Mital, Jan 2016
-
-    Attributes
-    ----------
-    batch_size : int
-        Size of the batch.  Set to -1 to fit to current net.
-    beta : Tensor
-        A 1D beta Tensor with size matching the last dimension of t.
-        An offset to be added to the normalized tensor.
-    ema : tf.train.ExponentialMovingAverage
-        For computing the moving average.
-    epsilon : float
-        A small float number to avoid dividing by 0.
-    gamma : Tensor
-        If "scale_after_normalization" is true, this tensor will be multiplied
-        with the normalized tensor.
-    momentum : float
-        The decay to use for the moving average.
-    name : str
-        The variable scope for all variables under batch normalization.
-    """
-
-    def __init__(self, batch_size, epsilon=1e-5,
-                 momentum=0.1, name="batch_norm"):
-        """Summary
-
-        Parameters
-        ----------
+def batch_normalize(X, batch_size, epsilon=1e-5, momentum=0.1):
+    """Batch Normalization
+    Args:
+        X: Input Tensor
         batch_size : int
             Size of the batch, or -1 for size to fit.
         epsilon : float, optional
             A small float number to avoid dividing by 0.
         momentum : float, optional
             Decay to use for the moving average.
-        name : str, optional
-            Variable scope will be under this prefix.
-        """
-        with tf.variable_scope(name) as scope:
-            self.epsilon = epsilon
-            self.momentum = momentum
-            self.batch_size = batch_size
-            self.ema = tf.train.ExponentialMovingAverage(decay=self.momentum)
-            self.name = name
+    """
+    shape = X.get_shape().as_list()
 
-    def __call__(self, x, train=True):
-        """Applies/updates the BN to the input Tensor.
-
-        Parameters
-        ----------
-        x : Tensor
-            The input tensor to normalize.
-        train : bool, optional
-            Whether or not to train parameters.
-
-        Returns
-        -------
-        x_normed : Tensor
-            The normalized Tensor.
-        """
-        shape = x.get_shape().as_list()
-
-        # Using a variable scope means any new variables
-        # will be prefixed with "variable_scope/", e.g.:
-        # "variable_scope/new_variable".  Also, using
-        # TensorBoard, this will make everything very
-        # nicely grouped.
-        with tf.variable_scope(self.name) as scope:
-            self.gamma = tf.get_variable(
-                "gamma", [shape[-1]],
-                initializer=tf.random_normal_initializer(1., 0.02))
-            self.beta = tf.get_variable(
-                "beta", [shape[-1]],
-                initializer=tf.constant_initializer(0.))
-
-            mean, variance = tf.nn.moments(x, [0, 1, 2])
-
-            return tf.nn.batch_norm_with_global_normalization(
-                x, mean, variance, self.beta, self.gamma, self.epsilon,
-                scale_after_normalization=True)
-
+    with tf.variable_scope("batch_norm"):
+        ema = tf.train.ExponentialMovingAverage(decay=momentum)
+        gamma = tf.get_variable("gamma", [shape[-1]],
+                                initializer=tf.random_normal_initializer(1., 0.02))
+        beta = tf.get_variable("beta", [shape[-1]],
+                                initializer=tf.constant_initializer(0.))
+        mean, variance = tf.nn.moments(X, [0, 1, 2])
+        return tf.nn.batch_norm_with_global_normalization(
+            X, mean, variance, beta, gamma, epsilon,
+            scale_after_normalization=True)
 
 def conv2d(x, n_filters,
            k_h=5, k_w=5,
@@ -170,8 +106,7 @@ def conv2d(x, n_filters,
                 initializer=tf.truncated_normal_initializer(stddev=stddev))
             conv = conv + b
         if batch_norm:
-            norm = batch_normalize(-1)
-            conv = norm(conv)
+            conv = batch_normalize(X=conv, batch_size=-1)
         return conv
 
 def res_net(x, y, activation=tf.nn.relu):
