434b79ec286c13bc0fb8b65ff824cf7ea3ee0c3e
Manjunath Kudlur <keveman@gmail.com>
2016-02-05 08:45:20 -0800

    Remove vocab_size argument for model_with_buckets. Change: 113952458

--- tensorflow/models/rnn/translate/seq2seq_model.py
+++ tensorflow/models/rnn/translate/seq2seq_model.py
@@ -147,7 +147,7 @@ class Seq2SeqModel(object):
     else:
       self.outputs, self.losses = seq2seq.model_with_buckets(
           self.encoder_inputs, self.decoder_inputs, targets,
-          self.target_weights, buckets, self.target_vocab_size,
+          self.target_weights, buckets,
           lambda x, y: seq2seq_f(x, y, False),
           softmax_loss_function=softmax_loss_function)
 
