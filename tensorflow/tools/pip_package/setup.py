0fa3312cb9c1f32e7c79ec956490781ee9d9e41d
panmari <smmuzi@gmail.com>
2016-02-08 17:22:40 +0100

    Fixed tensorboard installation for pip
    Broke when backend was moved to subfolder. See also #1000

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -43,7 +43,7 @@ else:
 
 # pylint: disable=line-too-long
 CONSOLE_SCRIPTS = [
-    'tensorboard = tensorflow.tensorboard.tensorboard:main',
+    'tensorboard = tensorflow.tensorboard.backend.tensorboard:main',
 ]
 # pylint: enable=line-too-long
 
