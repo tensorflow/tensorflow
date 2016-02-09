df66b9fe049cb17e58454f309b662bdaf0d14fdb
A. Unique TensorFlower <nobody@tensorflow.org>
2016-02-08 16:25:37 -0800

    Reimplementation of TensorShape to significantly reduce its memory footprint for the common cases (not very many dimensions, dimension sizes fairly small).
    The old TensorShape implementation was 56 bytes, while this new
    implementation is 24 bytes in the common cases (and Tensor objects
    have a contained TensorShape object).
    
    The implementation has a 16 byte buffer, plus an int64 num_elements_ field.
    It switches between three representations that use the 16 byte buffer
    in different ways.
    
    The last two bytes of the 16 bytes are always a tag byte, indicating
    which representation is in use, and a number of dimensions byte that
    indicates the number of dimensions in the shape.  This change also
    introduces a new restriction that tensor shapes can be at most 256
    distinct dimensions.
    
    The different representations are:
    
    REP16: up to 7 dimensions stored inline, where each dimension value is
    less than 32768.
    
    REP32: up to 3 dimensions stored inline, where each dimension value is
    less than 2^31 - 1.
    
    REP_OUT_OF_LINE: the fallback case, where the inline bytes are used to
    store a pointer to a heap allocated gtl::InlinedVector<int64, 4> that
    holds the shape dimensions.
    
    Changed the signature of TensorShape::dim_sizes() to return an actual
    gtl::InlinedVector<int64, 4> rather than an ArraySlice<int64> (the old
    signature was problematic because it placed unnecessary requirements
    on the implementation, namely that it store the dimensions as a
    contiguous array of in64 values, which the new representation does not
    do for REP16 and REP32).
    
    Preserved the old implementation by renaming it TensorShapeOld and
    moving it to the tensor_shape_test.cc unittest, where it used for a
    randomized test to ensure that the behavior is the same.
    
    Most of the benefit is that the TensorShape object is smaller.  Assignment
    is also generally faster, except for the rare REP_OUT_OF_LINE case, which
    is a bit slower (benchmark case /4):
    
    Run on machine with (40 X 2801 MHz CPUs); 2016/01/29-11:46:08
    CPU: Intel Ivybridge with HyperThreading (20 cores) dL1:32KB dL2:256KB dL3:25MB
    Benchmark                    Time(ns)    CPU(ns) Iterations
    -----------------------------------------------------------
    BM_TensorShape_Assign/0             1          1  580669339
    BM_TensorShape_Assign/1             1          1  588612006
    BM_TensorShape_Assign/2             1          1  586977731
    BM_TensorShape_Assign/3             1          1  564029390
    BM_TensorShape_Assign/4            61         62   10000000
    BM_TensorShapeOld_Assign/0          4          4  183910667
    BM_TensorShapeOld_Assign/1          6          6  100000000
    BM_TensorShapeOld_Assign/2          6          6  100000000
    BM_TensorShapeOld_Assign/3          8          8   88330961
    BM_TensorShapeOld_Assign/4         39         39   17701395
    Change: 114162871

--- tensorflow/core/framework/tensor_shape_test.cc
+++ tensorflow/core/framework/tensor_shape_test.cc
@@ -15,7 +15,11 @@ limitations under the License.
 
 #include "tensorflow/core/framework/tensor_shape.h"
 
+#include "tensorflow/core/lib/random/simple_philox.h"
+#include "tensorflow/core/lib/strings/str_util.h"
+#include "tensorflow/core/lib/strings/strcat.h"
 #include "tensorflow/core/platform/test.h"
+#include "tensorflow/core/platform/test_benchmark.h"
 
 namespace tensorflow {
 namespace {
@@ -97,6 +101,402 @@ TEST(TensorShapeTest, AppendShape64BitIndices) {
   EXPECT_EQ(10, s2.dim_size(0));
   EXPECT_EQ(2147483648, s2.dim_size(1));
 }
+
+// -----------------------------------------------------------------------
+// An old implementation of TensorShape using a different representation,
+// preserved here in the unittest to allow us to have a randomized unittest
+// that makes sure the behavior of TensorShape and TensorShapeOld are
+// the same.
+class TensorShapeIterOld;  // Declared below
+
+/// Manages the dimensions of a Tensor and their sizes.
+class TensorShapeOld {
+ public:
+  /// \brief Construct a `TensorShape` from the provided sizes.
+  /// REQUIRES: `dim_sizes[i] >= 0`
+  explicit TensorShapeOld(gtl::ArraySlice<int64> dim_sizes);
+  TensorShapeOld(std::initializer_list<int64> dim_sizes)
+      : TensorShapeOld(gtl::ArraySlice<int64>(dim_sizes)) {}
+
+  /// REQUIRES: `IsValid(proto)`
+  explicit TensorShapeOld(const TensorShapeProto& proto);
+
+  /// Create a tensor shape with no dimensions and one element, which you can
+  /// then call `AddDim()` on.
+  TensorShapeOld();
+
+  /// Returns `true` iff `proto` is a valid tensor shape.
+  static bool IsValid(const TensorShapeProto& proto);
+
+  /// Returns `OK` iff `proto` is a valid tensor shape, and a descriptive error
+  /// status otherwise.
+  static Status IsValidShape(const TensorShapeProto& proto);
+
+  /// Clear a tensor shape
+  void Clear();
+
+  /// \brief Add a dimension to the end ("inner-most").
+  /// REQUIRES: `size >= 0`
+  void AddDim(int64 size);
+
+  /// Appends all the dimensions from `shape`.
+  void AppendShape(const TensorShapeOld& shape);
+
+  /// \brief Insert a dimension somewhere in the `TensorShape`.
+  /// REQUIRES: `0 <= d <= dims()`
+  /// REQUIRES: `size >= 0`
+  void InsertDim(int d, int64 size);
+
+  /// \brief Modifies the size of the dimension `d` to be `size`
+  /// REQUIRES: `0 <= d < dims()`
+  /// REQUIRES: `size >= 0`
+  void set_dim(int d, int64 size);
+
+  /// \brief Removes dimension `d` from the `TensorShape`.
+  /// REQUIRES: `0 <= d < dims()`
+  void RemoveDim(int d);
+
+  /// Return the number of dimensions in the tensor.
+  int dims() const { return dim_sizes_.size(); }
+
+  /// \brief Returns the number of elements in dimension `d`.
+  /// REQUIRES: `0 <= d < dims()`
+  // TODO(touts): Rename to `dimension()` to match
+  // `Eigen::Tensor::dimension()`?
+  int64 dim_size(int d) const {
+    DCHECK_GE(d, 0);
+    DCHECK_LT(d, dims());
+    return dim_sizes_[d];
+  }
+
+  /// Returns sizes of all dimensions.
+  gtl::ArraySlice<int64> dim_sizes() const { return dim_sizes_; }
+
+  /// \brief Returns the number of elements in the tensor.
+  ///
+  /// We use `int64` and not `size_t` to be compatible with `Eigen::Tensor`
+  /// which uses `ptrdiff_t`.
+  int64 num_elements() const { return num_elements_; }
+
+  /// Returns true if `*this` and `b` have the same sizes. Ignores
+  /// dimension names.
+  bool IsSameSize(const TensorShapeOld& b) const;
+  bool operator==(const TensorShapeOld& b) const { return IsSameSize(b); }
+
+  /// Fill `*proto` from `*this`.
+  void AsProto(TensorShapeProto* proto) const;
+
+  /// Fill `*dsizes` from `*this`.
+  template <int NDIMS>
+  Eigen::DSizes<Eigen::DenseIndex, NDIMS> AsEigenDSizes() const;
+
+  /// Same as `AsEigenDSizes()` but allows for `NDIMS > dims()` -- in
+  /// which case we pad the rest of the sizes with 1.
+  template <int NDIMS>
+  Eigen::DSizes<Eigen::DenseIndex, NDIMS> AsEigenDSizesWithPadding() const;
+
+  /// For iterating through the dimensions.
+  TensorShapeIterOld begin() const;
+  TensorShapeIterOld end() const;
+
+  /// For error messages.
+  string DebugString() const;
+
+  /// Same as `TensorShape(proto).DebugString()` but doesn't crash for
+  /// invalid protos.
+  static string DebugString(const TensorShapeProto& proto);
+
+ private:
+  // Recalculates the dimensions of this tensor after they are modified.
+  void recompute_dims();
+
+  // TODO(josh11b): Maybe use something from the Eigen Tensor library
+  // for the sizes.
+  gtl::InlinedVector<int64, 4> dim_sizes_;
+
+  // total number of elements (avoids recomputing it each time).
+  int64 num_elements_;
+};
+
+struct TensorShapeDimOld {
+  explicit TensorShapeDimOld(int64 s) : size(s) {}
+  int64 size;
+};
+
+class TensorShapeIterOld {
+ public:
+  TensorShapeIterOld(const TensorShapeOld* shape, int d)
+      : shape_(shape), d_(d) {}
+  bool operator==(const TensorShapeIterOld& rhs) {
+    DCHECK(shape_ == rhs.shape_);
+    return d_ == rhs.d_;
+  }
+  bool operator!=(const TensorShapeIterOld& rhs) {
+    DCHECK(shape_ == rhs.shape_);
+    return d_ != rhs.d_;
+  }
+  void operator++() { ++d_; }
+  TensorShapeDimOld operator*() {
+    return TensorShapeDimOld(shape_->dim_size(d_));
+  }
+
+ private:
+  const TensorShapeOld* shape_;
+  int d_;
+};
+
+// An upper limit of the total number of elements in a tensor.
+static const int64 kMaxElements = (1LL << 40);
+
+bool TensorShapeOld::IsValid(const TensorShapeProto& proto) {
+  int64 num_elements = 1;
+  for (const auto& d : proto.dim()) {
+    if (d.size() < 0) return false;
+    num_elements *= d.size();
+    if (num_elements > kMaxElements) return false;
+  }
+  return true;
+}
+
+Status TensorShapeOld::IsValidShape(const TensorShapeProto& proto) {
+  int64 num_elements = 1;
+  for (const auto& d : proto.dim()) {
+    if (d.size() < 0) {
+      return errors::InvalidArgument("Shape ", DebugString(proto),
+                                     " has negative dimensions");
+    }
+    num_elements *= d.size();
+    if (num_elements > kMaxElements) {
+      return errors::InvalidArgument("Shape ", DebugString(proto),
+                                     " is too large (more than ", kMaxElements,
+                                     " entries)");
+    }
+  }
+  return Status::OK();
+}
+
+TensorShapeOld::TensorShapeOld(const TensorShapeProto& proto) {
+  dim_sizes_.reserve(proto.dim_size());
+  num_elements_ = 1;
+  for (const auto& d : proto.dim()) {
+    AddDim(d.size());
+  }
+}
+
+TensorShapeOld::TensorShapeOld(gtl::ArraySlice<int64> dim_sizes) {
+  dim_sizes_.reserve(dim_sizes.size());
+  num_elements_ = 1;
+  for (auto s : dim_sizes) {
+    AddDim(s);
+  }
+}
+
+TensorShapeOld::TensorShapeOld() : num_elements_(1) {}
+
+void TensorShapeOld::Clear() {
+  dim_sizes_.clear();
+  num_elements_ = 1;
+}
+
+void TensorShapeOld::AddDim(int64 size) {
+  CHECK_GE(size, 0);
+  dim_sizes_.push_back(size);
+  num_elements_ *= size;
+  CHECK_LE(0, num_elements_);
+  CHECK_LE(num_elements_, kMaxElements);
+}
+
+void TensorShapeOld::AppendShape(const TensorShapeOld& shape) {
+  for (auto d : shape) AddDim(d.size);
+}
+
+void TensorShapeOld::InsertDim(int d, int64 size) {
+  CHECK_GE(d, 0);
+  CHECK_LE(d, dims());
+  CHECK_GE(size, 0);
+  dim_sizes_.insert(dim_sizes_.begin() + d, size);
+  num_elements_ *= size;
+  CHECK_LE(0, num_elements_);
+  CHECK_LE(num_elements_, kMaxElements);
+}
+
+void TensorShapeOld::set_dim(int d, int64 size) {
+  CHECK_GE(d, 0);
+  CHECK_LT(d, dims());
+  CHECK_GE(size, 0);
+
+  // Update the number of elements. num_elements_ is int64.
+  dim_sizes_[d] = size;
+  recompute_dims();
+}
+
+void TensorShapeOld::RemoveDim(int d) {
+  CHECK_GE(d, 0);
+  CHECK_LT(d, dims());
+
+  // Update the number of elements and remove the dimension from the
+  // sizes.
+  dim_sizes_.erase(dim_sizes_.begin() + d);
+  recompute_dims();
+}
+
+void TensorShapeOld::recompute_dims() {
+  num_elements_ = 1;
+  for (auto s : dim_sizes_) {
+    num_elements_ *= s;
+    CHECK_LE(0, num_elements_);
+    CHECK_LE(num_elements_, kMaxElements);
+  }
+}
+
+bool TensorShapeOld::IsSameSize(const TensorShapeOld& b) const {
+  if (b.dims() != dims()) return false;
+  for (int d = 0; d < dims(); d++) {
+    if (dim_size(d) != b.dim_size(d)) return false;
+  }
+  return true;
+}
+
+void TensorShapeOld::AsProto(TensorShapeProto* proto) const {
+  proto->Clear();
+  for (size_t d = 0; d < dim_sizes_.size(); ++d) {
+    auto* dim = proto->add_dim();
+    dim->set_size(dim_sizes_[d]);
+  }
+}
+
+TensorShapeIterOld TensorShapeOld::begin() const {
+  return TensorShapeIterOld(this, 0);
+}
+
+TensorShapeIterOld TensorShapeOld::end() const {
+  return TensorShapeIterOld(this, dims());
+}
+
+string TensorShapeOld::DebugString() const {
+  return strings::StrCat(
+      "[", str_util::Join(gtl::ArraySlice<int64>(dim_sizes_), ","), "]");
+}
+
+string TensorShapeOld::DebugString(const TensorShapeProto& proto) {
+  string s = "[";
+  bool first = true;
+  for (const auto& d : proto.dim()) {
+    strings::StrAppend(&s, first ? "" : ",", d.size());
+    first = false;
+  }
+  strings::StrAppend(&s, "]");
+  return s;
+}
+// End of old implementation
+// ------------------------------------------------------------------------
+
+static int64 SkewedSize(random::SimplePhilox* gen, int64 current_elements) {
+  int64 result = 0;
+  do {
+    if (current_elements < 100) {
+      result = gen->Uniform(100000);
+    } else {
+      result = gen->Uniform(2);
+    }
+  } while ((result * current_elements >= 1uLL << 34) ||
+           (result * current_elements < 0));
+  return result;
+}
+
+TEST(TensorShapeTest, Randomized) {
+  // We do a randomized test to verify that the behavior of the
+  // TensorShape implementation (which changes representations depending
+  // on the values) is identical to our older, more straightforward (but
+  // more memory hungry) implementation (TensorShapeOld).
+  random::PhiloxRandom philox(7, 7);
+  random::SimplePhilox gen(&philox);
+  TensorShape s;
+  TensorShapeOld sold;
+  TensorShapeProto sp;
+  TensorShapeProto spold;
+  LOG(INFO) << "Sizes: " << sizeof(TensorShape) << " vs "
+            << sizeof(TensorShapeOld);
+  for (int i = 0; i < 100000; i++) {
+    s.AsProto(&sp);
+    sold.AsProto(&spold);
+    EXPECT_EQ(sp.DebugString(), spold.DebugString());
+    if ((i % 1000) == 0) {
+      fprintf(stderr, "ITERATION %d: %s\n", i, sp.DebugString().c_str());
+    }
+    EXPECT_EQ(s.num_elements(), sold.num_elements());
+    int64 ne = sold.num_elements();
+    int r = gen.Uniform(100);
+    if (r < 10) {
+      int64 sz = SkewedSize(&gen, sold.num_elements());
+      s.AddDim(sz);
+      sold.AddDim(sz);
+    } else if (r < 15) {
+      s.Clear();
+      sold.Clear();
+    } else if (r < 35 && s.dims() > 0 && ne > 0 && ne < 100000000) {
+      int dim = gen.Uniform(s.dims());
+      s.RemoveDim(dim);
+      sold.RemoveDim(dim);
+    } else if (r < 50 && ne > 0 && ne < 100000000) {
+      int dim = gen.Uniform(s.dims() + 1);
+      int64 sz = SkewedSize(&gen, sold.num_elements());
+      s.InsertDim(dim, sz);
+      sold.InsertDim(dim, sz);
+    } else {
+      std::vector<int64> sizes;
+      const int N = (gen.Uniform(4) == 0) ? gen.Uniform(10) : gen.Uniform(3);
+      int64 num_elements = 1;
+      for (int i = 0; i < N; i++) {
+        int64 sz = SkewedSize(&gen, num_elements);
+        sizes.push_back(sz);
+        num_elements *= max<int64>(1, sz);
+      }
+
+      s = TensorShape(sizes);
+      sold = TensorShapeOld(sizes);
+    }
+  }
+}
+
+// A few different test cases for tensor sizes for benchmarks
+static std::vector<int64> MakeSizes(int arg) {
+  std::vector<int64> sizes;
+  switch (arg) {
+    case 0:
+      sizes = {100};
+      break;
+    case 1:
+      sizes = {100, 1000};
+      break;
+    case 2:
+      sizes = {100, 1000000};
+      break;
+    case 3:
+      sizes = {100, 256, 192, 3};
+      break;
+    case 4:
+      sizes = {1, 2, 1ll << 34, 1, 1, 1};
+      break;
+  }
+  return sizes;
+}
+
+static void BM_TensorShape_Assign(int iters, int arg) {
+  TensorShape s(MakeSizes(arg));
+  while (--iters > 0) {
+    TensorShape s2 = s;
+  }
+}
+BENCHMARK(BM_TensorShape_Assign)->Arg(0)->Arg(1)->Arg(2)->Arg(3)->Arg(4);
+
+static void BM_TensorShapeOld_Assign(int iters, int arg) {
+  TensorShapeOld sold(MakeSizes(arg));
+  while (--iters > 0) {
+    TensorShapeOld sold2 = sold;
+  }
+}
+BENCHMARK(BM_TensorShapeOld_Assign)->Arg(0)->Arg(1)->Arg(2)->Arg(3)->Arg(4);
 
 }  // namespace
 }  // namespace tensorflow
