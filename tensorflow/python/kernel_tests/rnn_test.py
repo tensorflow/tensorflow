45197e07c0dc410bde203462992841b457e0f61c
Eugene Brevdo <ebrevdo@gmail.com>
2016-02-09 13:28:15 -0800

    Add fully dynamic RNN op.  Unofficial version (not yet documented).
    Current benchmarking results (speed, not memory): calculate forward
    and gradients w.r.t. variables
    
    Compiled -c opt --config=cuda
    CPU: Intel Ivybridge with HyperThreading (6 cores) dL1:32KB dL2:256KB dL3:12MB
    Tesla K40c
    
    Graph Creation: Static Unroll vs. Dynamic Unroll LSTM
    max_t    dt(static)      dt(dynamic)     dt(dynamic)/dt(static)
    1        0.713889        1.657085        2.321208
    25       13.108961       1.784326        0.136115
    50       25.887796       2.009615        0.077628
    
    Calculation: Static Unroll with Dynamic Flow LSTM vs. Dynamic Unroll LSTM
    batch    max_t   units   gpu     dt(static)      dt(dynamic)     dt(dynamic)/dt(static)
    256      50      512     False   2.098613        1.995831        0.951024
    256      50      512     True    0.193702        0.218444        1.127730
    256      50      256     False   0.630918        0.638142        1.011451
    256      50      256     True    0.091755        0.119900        1.306731
    256      50      128     False   0.278201        0.280812        1.009385
    256      50      128     True    0.065415        0.093180        1.424446
    256      100     512     False   4.252444        4.155129        0.977115
    256      100     512     True    0.388840        0.459850        1.182622
    256      100     256     False   1.247995        1.318395        1.056411
    256      100     256     True    0.183765        0.238367        1.297130
    256      100     128     False   0.558911        0.577376        1.033039
    256      100     128     True    0.132579        0.192326        1.450656
    512      50      512     False   3.682583        4.108835        1.115748
    512      50      512     True    0.328870        0.378585        1.151168
    512      50      256     False   1.129227        1.178769        1.043872
    512      50      256     True    0.135209        0.163431        1.208724
    512      50      128     False   0.435205        0.444609        1.021608
    512      50      128     True    0.079627        0.106961        1.343268
    512      100     512     False   7.183316        7.332909        1.020825
    512      100     512     True    0.682864        0.722224        1.057641
    512      100     256     False   2.033514        2.169560        1.066902
    512      100     256     True    0.269665        0.338253        1.254347
    512      100     128     False   0.776487        0.985389        1.269036
    512      100     128     True    0.157085        0.207062        1.318155
    Change: 114253188

--- tensorflow/python/kernel_tests/rnn_test.py
+++ tensorflow/python/kernel_tests/rnn_test.py
@@ -20,12 +20,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.python.platform
+import tensorflow.python.platform as platform
 
 import numpy as np
+import sys
 import tensorflow as tf
+import time
+import timeit
+
+from tensorflow.python.client import graph_util
 
 from six.moves import xrange  # pylint: disable=redefined-builtin
+
+
+def _flatten(list_of_lists):
+  return [x for y in list_of_lists for x in y]
 
 
 class Plus1RNNCell(tf.nn.rnn_cell.RNNCell):
@@ -514,6 +523,153 @@ class LSTMTest(tf.test.TestCase):
       for out0, out1 in zip(outputs0_values, outputs1_values):
         self.assertAllEqual(out0, out1)
 
+  def _testDynamicEquivalentToStaticRNN(self, use_gpu):
+    time_steps = 8
+    num_units = 3
+    num_proj = 4
+    input_size = 5
+    batch_size = 2
+
+    input_values = np.random.randn(time_steps, batch_size, input_size)
+
+    sequence_length = np.random.randint(0, time_steps, size=batch_size)
+
+    ########### Step 1: Run static graph and generate readouts
+    with self.test_session(use_gpu=use_gpu, graph=tf.Graph()) as sess:
+      concat_inputs = tf.placeholder(tf.float32,
+                                     shape=(time_steps, batch_size, input_size))
+      inputs = tf.unpack(concat_inputs)
+      initializer = tf.random_uniform_initializer(-0.01, 0.01, seed=self._seed)
+
+      cell = tf.nn.rnn_cell.LSTMCell(
+          num_units, input_size, use_peepholes=True,
+          initializer=initializer, num_proj=num_proj)
+
+      with tf.variable_scope("dynamic_scope"):
+        outputs_static, state_static = tf.nn.rnn(
+            cell, inputs, sequence_length=sequence_length, dtype=tf.float32)
+
+      feeds = {concat_inputs: input_values}
+
+      # Initialize
+      tf.initialize_all_variables().run(feed_dict=feeds)
+
+      # Generate gradients of sum of outputs w.r.t. inputs
+      static_gradients = tf.gradients(
+          outputs_static + [state_static], [concat_inputs])
+
+      # Generate gradients of individual outputs w.r.t. inputs
+      static_individual_gradients = _flatten([
+          tf.gradients(y, [concat_inputs])
+          for y in [outputs_static[0],
+                    outputs_static[-1],
+                    state_static]])
+
+      # Generate gradients of individual variables w.r.t. inputs
+      trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+      assert len(trainable_variables) > 1, (
+          "Count of trainable variables: %d" % len(trainable_variables))
+      # pylint: disable=bad-builtin
+      static_individual_variable_gradients = _flatten([
+          tf.gradients(y, trainable_variables)
+          for y in [outputs_static[0],
+                    outputs_static[-1],
+                    state_static]])
+
+      # Test forward pass
+      values_static = sess.run(outputs_static, feed_dict=feeds)
+      (state_value_static,) = sess.run((state_static,), feed_dict=feeds)
+
+      # Test gradients to inputs and variables w.r.t. outputs & final state
+      static_grad_values = sess.run(static_gradients, feed_dict=feeds)
+
+      static_individual_grad_values = sess.run(
+          static_individual_gradients, feed_dict=feeds)
+
+      static_individual_var_grad_values = sess.run(
+          static_individual_variable_gradients, feed_dict=feeds)
+
+    ########## Step 2: Run dynamic graph and generate readouts
+    with self.test_session(use_gpu=use_gpu, graph=tf.Graph()) as sess:
+      concat_inputs = tf.placeholder(tf.float32,
+                                     shape=(time_steps, batch_size, input_size))
+      inputs = tf.unpack(concat_inputs)
+      initializer = tf.random_uniform_initializer(-0.01, 0.01, seed=self._seed)
+
+      cell = tf.nn.rnn_cell.LSTMCell(
+          num_units, input_size, use_peepholes=True,
+          initializer=initializer, num_proj=num_proj)
+
+      with tf.variable_scope("dynamic_scope"):
+        outputs_dynamic, state_dynamic = tf.nn.dynamic_rnn(
+            cell, inputs=concat_inputs, sequence_length=sequence_length,
+            time_major=True, dtype=tf.float32)
+        split_outputs_dynamic = tf.unpack(outputs_dynamic, time_steps)
+
+      feeds = {concat_inputs: input_values}
+
+      # Initialize
+      tf.initialize_all_variables().run(feed_dict=feeds)
+
+      # Generate gradients of sum of outputs w.r.t. inputs
+      dynamic_gradients = tf.gradients(
+          split_outputs_dynamic + [state_dynamic], [concat_inputs])
+
+      # Generate gradients of several individual outputs w.r.t. inputs
+      dynamic_individual_gradients = _flatten([
+          tf.gradients(y, [concat_inputs])
+          for y in [split_outputs_dynamic[0],
+                    split_outputs_dynamic[-1],
+                    state_dynamic]])
+
+      # Generate gradients of individual variables w.r.t. inputs
+      trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+      assert len(trainable_variables) > 1, (
+          "Count of trainable variables: %d" % len(trainable_variables))
+      dynamic_individual_variable_gradients = _flatten([
+          tf.gradients(y, trainable_variables)
+          for y in [split_outputs_dynamic[0],
+                    split_outputs_dynamic[-1],
+                    state_dynamic]])
+
+      # Test forward pass
+      values_dynamic = sess.run(split_outputs_dynamic, feed_dict=feeds)
+      (state_value_dynamic,) = sess.run(
+          (state_dynamic,), feed_dict=feeds)
+
+      # Test gradients to inputs and variables w.r.t. outputs & final state
+      dynamic_grad_values = sess.run(dynamic_gradients, feed_dict=feeds)
+
+      dynamic_individual_grad_values = sess.run(
+          dynamic_individual_gradients, feed_dict=feeds)
+
+      dynamic_individual_var_grad_values = sess.run(
+          dynamic_individual_variable_gradients, feed_dict=feeds)
+
+    ######### Step 3: Comparisons
+    self.assertEqual(len(values_static), len(values_dynamic))
+    for (value_static, value_dynamic) in zip(values_static, values_dynamic):
+      self.assertAllEqual(value_static, value_dynamic)
+    self.assertAllEqual(state_value_static, state_value_dynamic)
+
+    self.assertAllEqual(static_grad_values, dynamic_grad_values)
+
+    self.assertEqual(len(static_individual_grad_values),
+                     len(dynamic_individual_grad_values))
+    self.assertEqual(len(static_individual_var_grad_values),
+                     len(dynamic_individual_var_grad_values))
+
+    for i, (a, b) in enumerate(zip(static_individual_grad_values,
+                                   dynamic_individual_grad_values)):
+      tf.logging.info("Comparing individual gradients iteration %d" % i)
+      self.assertAllEqual(a, b)
+
+    for i, (a, b) in enumerate(zip(static_individual_var_grad_values,
+                                   dynamic_individual_var_grad_values)):
+      tf.logging.info(
+          "Comparing individual variable gradients iteraiton %d" % i)
+      self.assertAllEqual(a, b)
+
   def testNoProjNoShardingSimpleStateSaver(self):
     self._testNoProjNoShardingSimpleStateSaver(use_gpu=False)
     self._testNoProjNoShardingSimpleStateSaver(use_gpu=True)
@@ -549,6 +705,10 @@ class LSTMTest(tf.test.TestCase):
   def testDoubleInputWithDropoutAndDynamicCalculation(self):
     self._testDoubleInputWithDropoutAndDynamicCalculation(use_gpu=False)
     self._testDoubleInputWithDropoutAndDynamicCalculation(use_gpu=True)
+
+  def testDynamicEquivalentToStaticRNN(self):
+    self._testDynamicEquivalentToStaticRNN(use_gpu=False)
+    self._testDynamicEquivalentToStaticRNN(use_gpu=True)
 
 
 class BidirectionalRNNTest(tf.test.TestCase):
@@ -670,5 +830,153 @@ class BidirectionalRNNTest(tf.test.TestCase):
     self._testBidirectionalRNNWithoutSequenceLength(use_gpu=True,
                                                     use_shape=True)
 
+
+######### Benchmarking RNN code
+
+def _static_vs_dynamic_rnn_benchmark_static(inputs_list_t, sequence_length):
+  (_, input_size) = inputs_list_t[0].get_shape().as_list()
+  initializer = tf.random_uniform_initializer(-0.01, 0.01, seed=127)
+  cell = tf.nn.rnn_cell.LSTMCell(
+      num_units=input_size, input_size=input_size, use_peepholes=True,
+      initializer=initializer)
+  outputs, final_state = tf.nn.rnn(
+      cell, inputs_list_t, sequence_length=sequence_length, dtype=tf.float32)
+
+  trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+  gradients = tf.gradients(outputs + [final_state], trainable_variables)
+
+  return outputs + [final_state] + gradients
+
+
+def _static_vs_dynamic_rnn_benchmark_dynamic(inputs_t, sequence_length):
+  (unused_0, unused_1, input_size) = inputs_t.get_shape().as_list()
+  initializer = tf.random_uniform_initializer(-0.01, 0.01, seed=127)
+  cell = tf.nn.rnn_cell.LSTMCell(
+      num_units=input_size, input_size=input_size, use_peepholes=True,
+      initializer=initializer)
+  outputs, final_state = tf.nn.dynamic_rnn(
+      cell, inputs_t, sequence_length=sequence_length, dtype=tf.float32)
+
+  trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+  gradients = tf.gradients([outputs, final_state], trainable_variables)
+
+  return [outputs, final_state] + gradients
+
+
+def graph_creation_static_vs_dynamic_rnn_benchmark(max_time):
+  config = tf.ConfigProto()
+  config.allow_soft_placement = True
+
+  # These parameters don't matter
+  batch_size = 512
+  num_units = 512
+
+  # Set up sequence lengths
+  np.random.seed([127])
+  sequence_length = np.random.randint(0, max_time, size=batch_size)
+  inputs_list = [
+      np.random.randn(batch_size, num_units).astype(np.float32)
+      for _ in range(max_time)]
+  inputs = np.dstack(inputs_list).transpose([0, 2, 1])  # batch x time x depth
+
+  def _create_static_rnn():
+    with tf.Session(config=config, graph=tf.Graph()) as sess:
+      inputs_list_t = [tf.constant(x) for x in inputs_list]
+      ops = _static_vs_dynamic_rnn_benchmark_static(
+          inputs_list_t, sequence_length)
+
+  def _create_dynamic_rnn():
+    with tf.Session(config=config, graph=tf.Graph()) as sess:
+      inputs_t = tf.constant(inputs)
+      ops = _static_vs_dynamic_rnn_benchmark_dynamic(
+          inputs_t, sequence_length)
+
+  delta_static = timeit.timeit(_create_static_rnn, number=5)
+  delta_dynamic = timeit.timeit(_create_dynamic_rnn, number=5)
+
+  print("%d \t %f \t %f \t %f" %
+        (max_time, delta_static, delta_dynamic, delta_dynamic/delta_static))
+
+
+def static_vs_dynamic_rnn_benchmark(batch_size, max_time, num_units, use_gpu):
+  config = tf.ConfigProto()
+  config.allow_soft_placement = True
+
+  def _timer(sess, ops):
+    # Warm in
+    for _ in range(2):
+      sess.run(ops)
+
+    # Timing run
+    start = time.time()
+    for _ in range(10):
+      sess.run(ops)
+    end = time.time()
+
+    return (end - start)/10.0  # Average runtime per iteration
+
+  # Set up sequence lengths
+  np.random.seed([127])
+  sequence_length = np.random.randint(0, max_time, size=batch_size)
+  inputs_list = [
+      np.random.randn(batch_size, num_units).astype(np.float32)
+      for _ in range(max_time)]
+  inputs = np.dstack(inputs_list).transpose([0, 2, 1])  # batch x time x depth
+
+  # Using rnn()
+  with tf.Session(config=config, graph=tf.Graph()) as sess:
+    if not use_gpu:
+      with tf.device(graph_util.pin_to_cpu):
+        inputs_list_t = [tf.constant(x) for x in inputs_list]
+        ops = _static_vs_dynamic_rnn_benchmark_static(
+            inputs_list_t, sequence_length)
+    else:
+      inputs_list_t = [tf.constant(x) for x in inputs_list]
+      ops = _static_vs_dynamic_rnn_benchmark_static(
+          inputs_list_t, sequence_length)
+    tf.initialize_all_variables().run()
+    delta_static = _timer(sess, ops)
+
+  # Using dynamic_rnn()
+  with tf.Session(config=config, graph=tf.Graph()) as sess:
+    if not use_gpu:
+      with tf.device(graph_util.pin_to_cpu):
+        inputs_t = tf.constant(inputs)
+        ops = _static_vs_dynamic_rnn_benchmark_dynamic(
+            inputs_t, sequence_length)
+    else:
+      inputs_t = tf.constant(inputs)
+      ops = _static_vs_dynamic_rnn_benchmark_dynamic(
+          inputs_t, sequence_length)
+    tf.initialize_all_variables().run()
+    delta_dynamic = _timer(sess, ops)
+
+  print("%d \t %d \t %d \t %s \t %f \t %f \t %f" %
+        (batch_size, max_time, num_units, use_gpu, delta_static,
+         delta_dynamic, delta_dynamic/delta_static))
+
+
+def main(_):
+  print("Graph Creation: Static Unroll vs. Dynamic Unroll LSTM")
+  print("max_t \t dt(static) \t dt(dynamic) \t dt(dynamic)/dt(static)")
+  for max_time in (1, 25, 50):
+    graph_creation_static_vs_dynamic_rnn_benchmark(max_time)
+
+  print("Calculation: Static Unroll with Dynamic Flow LSTM "
+        "vs. Dynamic Unroll LSTM")
+  print("batch \t max_t \t units \t gpu \t dt(static) \t dt(dynamic) "
+        "\t dt(dynamic)/dt(static)")
+  for batch_size in (256, 512):
+    for max_time in (50, 100):
+      for num_units in (512, 256, 128):
+        for use_gpu in (False, True):
+          static_vs_dynamic_rnn_benchmark(
+              batch_size, max_time, num_units, use_gpu)
+
+
 if __name__ == "__main__":
-  tf.test.main()
+  if "--benchmarks" in sys.argv:
+    sys.argv.remove("--benchmarks")
+    tf.app.run()
+  else:
+    tf.test.main()
