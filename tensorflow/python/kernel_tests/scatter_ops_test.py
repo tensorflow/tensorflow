243de8ecb4c58cd6f2dd63297e4224ed3152fcce
Shanqing Cai <cais@google.com>
2016-02-09 14:44:42 -0500

    Fixing Python3 test failures

--- tensorflow/python/kernel_tests/scatter_ops_test.py
+++ tensorflow/python/kernel_tests/scatter_ops_test.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
+
 import tensorflow.python.platform
 
 import numpy as np
