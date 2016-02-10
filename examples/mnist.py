86462105f53a1d0ef1eb7f6503ffb247f6e52db0
terrytangyuan <terrytangyuan@gmail.com>
2016-02-09 23:11:42 -0600

    + Comments for mnist example

--- examples/mnist.py
+++ examples/mnist.py
@@ -45,19 +45,26 @@ def max_pool_2x2(tensor_in):
         padding='SAME')
 
 def conv_model(X, y):
+    # reshape X to 4d tensor with 2nd and 3rd dimensions being image width and height
+    # final dimension being the number of color channels
     X = tf.reshape(X, [-1, 28, 28, 1])
+    # first conv layer will compute 32 features for each 5x5 patch
     with tf.variable_scope('conv_layer1'):
         h_conv1 = skflow.ops.conv2d(X, n_filters=32, filter_shape=[5, 5], 
                                     bias=True, activation=tf.nn.relu)
         h_pool1 = max_pool_2x2(h_conv1)
+    # second conv layer will compute 64 features for each 5x5 patch
     with tf.variable_scope('conv_layer2'):
         h_conv2 = skflow.ops.conv2d(h_pool1, n_filters=64, filter_shape=[5, 5], 
                                     bias=True, activation=tf.nn.relu)
         h_pool2 = max_pool_2x2(h_conv2)
+        # reshape tensor into a batch of vectors
         h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    # densely connected layer with 1024 neurons
     h_fc1 = skflow.ops.dnn(h_pool2_flat, [1024], activation=tf.nn.relu, keep_prob=0.5)
     return skflow.models.logistic_regression(h_fc1, y)
 
+# Training and predicting
 classifier = skflow.TensorFlowEstimator(
     model_fn=conv_model, n_classes=10, batch_size=100, steps=20000,
     learning_rate=0.001)
