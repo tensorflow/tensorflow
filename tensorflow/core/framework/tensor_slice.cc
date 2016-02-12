4392eee66fe9e452e5b9ce8188347a58476f56f3
A. Unique TensorFlower <nobody@tensorflow.org>
2016-02-11 18:48:12 -0800

    Add missing std namespace to min and max calls in tensor_slice.cc. Change: 114500524

--- tensorflow/core/framework/tensor_slice.cc
+++ tensorflow/core/framework/tensor_slice.cc
@@ -203,8 +203,8 @@ void TensorSlice::UpdateToCover(const TensorSlice& other) {
         starts_[d] = 0;
         lengths_[d] = kFullExtent;
       } else {
-        const auto new_end = max(end(d), other.end(d));
-        set_start(d, min(start(d), other.start(d)));
+        const auto new_end = std::max(end(d), other.end(d));
+        set_start(d, std::min(start(d), other.start(d)));
         set_length(d, new_end - start(d));
       }
     }
