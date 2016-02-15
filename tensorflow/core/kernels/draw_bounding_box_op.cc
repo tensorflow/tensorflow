64d4ac74902786bf0604367921f8303619a2fe1a
Martin Wicke <wicke@google.com>
2016-02-14 22:18:58 -0800

    Add tf.image.draw_bounding_box and tf.image.sample_distorted_bounding_box. Change: 114664952

---
+++ tensorflow/core/kernels/draw_bounding_box_op.cc
@@ -0,0 +1,150 @@
+/* Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+// See ../ops/image_ops.cc for details.
+#define EIGEN_USE_THREADS
+
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/framework/types.h"
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/platform/logging.h"
+
+namespace tensorflow {
+
+class DrawBoundingBoxesOp : public OpKernel {
+ public:
+  explicit DrawBoundingBoxesOp(OpKernelConstruction* context)
+      : OpKernel(context) {
+    OP_REQUIRES_OK(context,
+                   context->MatchSignature({DT_FLOAT, DT_FLOAT}, {DT_FLOAT}));
+  }
+
+  void Compute(OpKernelContext* context) override {
+    const Tensor& images = context->input(0);
+    const Tensor& boxes = context->input(1);
+
+    OP_REQUIRES(context, images.dims() == 4,
+                errors::InvalidArgument("The rank of the images should be 4"));
+    OP_REQUIRES(
+        context, boxes.dims() == 3,
+        errors::InvalidArgument("The rank of the boxes tensor should be 3"));
+
+    OP_REQUIRES(context, images.dim_size(0) == boxes.dim_size(0),
+                errors::InvalidArgument("The batch sizes should be the same"));
+
+    const int64 batch_size = images.dim_size(0);
+    const int64 height = images.dim_size(1);
+    const int64 width = images.dim_size(2);
+    const int64 depth = images.dim_size(3);
+
+    Tensor* output;
+    OP_REQUIRES_OK(
+        context,
+        context->allocate_output(
+            0, TensorShape({batch_size, height, width, depth}), &output));
+
+    output->tensor<float, 4>() = images.tensor<float, 4>();
+    auto canvas = output->tensor<float, 4>();
+
+    for (int64 b = 0; b < batch_size; ++b) {
+      const int64 num_boxes = boxes.dim_size(1);
+
+      for (int64 bb = 0; bb < num_boxes; ++bb) {
+        const int64 min_box_row =
+            (height - 1) * boxes.tensor<float, 3>()(b, bb, 0);
+        const int64 min_box_row_clamp =
+            std::max<int64>(min_box_row, 0);
+        const int64 max_box_row =
+            (height - 1) * boxes.tensor<float, 3>()(b, bb, 2);
+        const int64 max_box_row_clamp =
+            std::min<int64>(max_box_row, height - 1);
+        const int64 min_box_col =
+            (width - 1) * boxes.tensor<float, 3>()(b, bb, 1);
+        const int64 min_box_col_clamp =
+            std::max<int64>(min_box_col, 0);
+        const int64 max_box_col =
+            (width - 1) * boxes.tensor<float, 3>()(b, bb, 3);
+        const int64 max_box_col_clamp =
+            std::min<int64>(max_box_col, width - 1);
+
+        if (min_box_row > max_box_row || min_box_col > max_box_col) {
+          LOG(WARNING) << "Bounding box (" << min_box_row
+                       << "," << min_box_col
+                       << "," << max_box_row
+                       << "," << max_box_col
+                       << ") is inverted and will not be drawn.";
+          continue;
+        }
+        if (min_box_row >= height || max_box_row < 0 ||
+            min_box_col >= width || max_box_col < 0) {
+          LOG(WARNING) << "Bounding box (" << min_box_row
+                       << "," << min_box_col
+                       << "," << max_box_row
+                       << "," << max_box_col
+                       << ") is completely outside the image"
+                       << " and will not be drawn.";
+          continue;
+        }
+
+        // At this point, {min,max}_box_{row,col}_clamp are inside the
+        // image.
+        CHECK_GE(min_box_row_clamp, 0);
+        CHECK_GE(max_box_row_clamp, 0);
+        CHECK_LT(min_box_row_clamp, height);
+        CHECK_LT(max_box_row_clamp, height);
+        CHECK_GE(min_box_col_clamp, 0);
+        CHECK_GE(max_box_col_clamp, 0);
+        CHECK_LT(min_box_col_clamp, width);
+        CHECK_LT(max_box_col_clamp, width);
+
+        // At this point, the min_box_row and min_box_col are either
+        // in the image or above/left of it, and max_box_row and
+        // max_box_col are either in the image or below/right or it.
+        CHECK_LT(min_box_row, height);
+        CHECK_GE(max_box_row, 0);
+        CHECK_LT(min_box_col, width);
+        CHECK_GE(max_box_col, 0);
+
+        // Draw top line.
+        if (min_box_row >= 0) {
+          for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
+            canvas(b, min_box_row, j, 0) = nanf("");
+        }
+        // Draw bottom line.
+        if (max_box_row < height) {
+          for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
+            canvas(b, max_box_row, j, 0) = nanf("");
+        }
+        // Draw left line.
+        if (min_box_col >= 0) {
+          for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
+            canvas(b, i, min_box_col, 0) = nanf("");
+        }
+        // Draw right line.
+        if (max_box_col < width) {
+          for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
+            canvas(b, i, max_box_col, 0) = nanf("");
+        }
+      }
+    }
+  }
+};
+
+REGISTER_KERNEL_BUILDER(Name("DrawBoundingBoxes").Device(DEVICE_CPU),
+                        DrawBoundingBoxesOp);
+
+}  // namespace tensorflow
--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -556,6 +556,148 @@ class PadToBoundingBoxTest(test_util.TensorFlowTestCase):
       self.assertAllEqual(y_tf, y_np)
 
 
+class SelectDistortedCropBoxTest(test_util.TensorFlowTestCase):
+
+  def _testSampleDistortedBoundingBox(self, image, bounding_box,
+                                      min_object_covered, aspect_ratio_range,
+                                      area_range):
+    original_area = float(np.prod(image.shape))
+    bounding_box_area = float((bounding_box[3] - bounding_box[1]) *
+                              (bounding_box[2] - bounding_box[0]))
+
+    image_size_np = np.array(image.shape, dtype=np.int32)
+    bounding_box_np = (np.array(bounding_box, dtype=np.float32)
+                       .reshape([1, 1, 4]))
+
+    aspect_ratios = []
+    area_ratios = []
+
+    fraction_object_covered = []
+
+    num_iter = 1000
+    with self.test_session():
+      image_tf = constant_op.constant(image,
+                                      shape=image.shape)
+      image_size_tf = constant_op.constant(image_size_np,
+                                           shape=image_size_np.shape)
+      bounding_box_tf = constant_op.constant(bounding_box_np,
+                                             dtype=dtypes.float32,
+                                             shape=bounding_box_np.shape)
+      begin, end, _ = image_ops.sample_distorted_bounding_box(
+          image_size=image_size_tf,
+          bounding_boxes=bounding_box_tf,
+          min_object_covered=min_object_covered,
+          aspect_ratio_range=aspect_ratio_range,
+          area_range=area_range)
+      y = array_ops.slice(image_tf, begin, end)
+
+      for _ in xrange(num_iter):
+        y_tf = y.eval()
+        crop_height = y_tf.shape[0]
+        crop_width = y_tf.shape[1]
+        aspect_ratio = float(crop_width) / float(crop_height)
+        area = float(crop_width * crop_height)
+
+        aspect_ratios.append(aspect_ratio)
+        area_ratios.append(area / original_area)
+        fraction_object_covered.append(float(np.sum(y_tf)) / bounding_box_area)
+
+    # Ensure that each entry is observed within 3 standard deviations.
+    # num_bins = 10
+    # aspect_ratio_hist, _ = np.histogram(aspect_ratios,
+    #                                     bins=num_bins,
+    #                                     range=aspect_ratio_range)
+    # mean = np.mean(aspect_ratio_hist)
+    # stddev = np.sqrt(mean)
+    # TODO(wicke, shlens, dga): Restore this test so that it is no longer flaky.
+    # TODO(irving): Since the rejection probability is not independent of the
+    # aspect ratio, the aspect_ratio random value is not exactly uniformly
+    # distributed in [min_aspect_ratio, max_aspect_ratio).  This test should be
+    # fixed to reflect the true statistical property, then tightened to enforce
+    # a stricter bound.  Or, ideally, the sample_distorted_bounding_box Op
+    # be fixed to not use rejection sampling and generate correctly uniform
+    # aspect ratios.
+    # self.assertAllClose(aspect_ratio_hist,
+    #                     [mean] * num_bins, atol=3.6 * stddev)
+
+    # The resulting crop will not be uniformly distributed in area. In practice,
+    # we find that the area skews towards the small sizes. Instead, we perform
+    # a weaker test to ensure that the area ratios are merely within the
+    # specified bounds.
+    self.assertLessEqual(max(area_ratios), area_range[1])
+    self.assertGreaterEqual(min(area_ratios), area_range[0])
+
+    # For reference, here is what the distribution of area ratios look like.
+    area_ratio_hist, _ = np.histogram(area_ratios, bins=10, range=area_range)
+    print('area_ratio_hist ', area_ratio_hist)
+
+    # Ensure that fraction_object_covered is satisfied.
+    # TODO(wicke, shlens, dga): Restore this test so that it is no longer flaky.
+    # self.assertGreaterEqual(min(fraction_object_covered), min_object_covered)
+
+  def testWholeImageBoundingBox(self):
+    height = 40
+    width = 50
+    image_size = [height, width, 1]
+    bounding_box = [0.0, 0.0, 1.0, 1.0]
+    image = np.arange(0, np.prod(image_size),
+                      dtype=np.int32).reshape(image_size)
+    self._testSampleDistortedBoundingBox(image,
+                                         bounding_box,
+                                         min_object_covered=0.1,
+                                         aspect_ratio_range=(0.75, 1.33),
+                                         area_range=(0.05, 1.0))
+
+  def testWithBoundingBox(self):
+    height = 40
+    width = 50
+    x_shape = [height, width, 1]
+    image = np.zeros(x_shape, dtype=np.int32)
+
+    # Create an object with 1's in a region with area A and require that
+    # the total pixel values >= 0.1 * A.
+    min_object_covered = 0.1
+
+    xmin = 2
+    ymin = 3
+    xmax = 12
+    ymax = 13
+    for x in np.arange(xmin, xmax + 1, 1):
+      for y in np.arange(ymin, ymax + 1, 1):
+        image[x, y] = 1
+
+    # Bounding box is specified as (ymin, xmin, ymax, xmax) in
+    # relative coordinates.
+    bounding_box = (float(ymin) / height, float(xmin) / width,
+                    float(ymax) / height, float(xmax) / width)
+
+    self._testSampleDistortedBoundingBox(image,
+                                         bounding_box=bounding_box,
+                                         min_object_covered=min_object_covered,
+                                         aspect_ratio_range=(0.75, 1.33),
+                                         area_range=(0.05, 1.0))
+
+  def testSampleDistortedBoundingBoxShape(self):
+    with self.test_session():
+      image_size = constant_op.constant([40, 50, 1],
+                                        shape=[3],
+                                        dtype=dtypes.int32)
+      bounding_box = constant_op.constant([0.0, 0.0, 1.0, 1.0],
+                                          shape=[4],
+                                          dtype=dtypes.float32,)
+      begin, end, bbox_for_drawing = image_ops.sample_distorted_bounding_box(
+          image_size=image_size,
+          bounding_boxes=bounding_box,
+          min_object_covered=0.1,
+          aspect_ratio_range=(0.75, 1.33),
+          area_range=(0.05, 1.0))
+
+      # Test that the shapes are correct.
+      self.assertAllEqual([3], begin.get_shape().as_list())
+      self.assertAllEqual([3], end.get_shape().as_list())
+      self.assertAllEqual([1, 1, 4], bbox_for_drawing.get_shape().as_list())
+
+
 class ResizeImagesTest(test_util.TensorFlowTestCase):
 
   OPTIONS = [image_ops.ResizeMethod.BILINEAR,
