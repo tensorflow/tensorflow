a965380cc2c19877c314d71d48fe786c51c8373a
Martin Wicke <wicke@google.com>
2016-02-14 23:45:55 -0800

    Fix build failure (missing include for int64) Change: 114667596

--- tensorflow/core/kernels/sparse_tensor_dense_matmul_op.h
+++ tensorflow/core/kernels/sparse_tensor_dense_matmul_op.h
@@ -18,6 +18,7 @@ limitations under the License.
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/framework/types.h"
 
 namespace tensorflow {
 
