bf08a419e5855c57665e7bc21dc7d4df374dc89c
Vijay Vasudevan <vrv@google.com>
2016-02-15 16:50:56 -0800

    TensorFlow: add gfile.Copy implementation and test.   Fixes #1102. Change: 114712488

--- tensorflow/python/platform/default/_gfile.py
+++ tensorflow/python/platform/default/_gfile.py
@@ -420,3 +420,21 @@ def Stat(path):   # pylint: disable=invalid-name
   filestat = collections.namedtuple('FileStat', ['mtime'])
   filestat.mtime = statinfo.st_mtime
   return filestat
+
+
+def Copy(oldpath, newpath, overwrite=False):
+  """Copy a file.
+
+  Args:
+    oldpath: string; a pathname of a file.
+    newpath: string; a pathname to which the file will be copied.
+    overwrite: boolean; if false, it is an error for newpath to be
+      occupied by an existing file.
+
+  Raises:
+    OSError: If "newpath" is occupied by an existing file and overwrite=False,
+             or any error thrown by shutil.copy.
+  """
+  if not overwrite and Exists(newpath):
+    raise OSError(errno.EEXIST, os.strerror(errno.EEXIST), newpath)
+  shutil.copy(oldpath, newpath)
