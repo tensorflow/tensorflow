250369c18bae02a2a6a855cca480e87d95d782f8
Yuan Yu <yuanbyu@google.com>
2016-02-16 09:52:47 -0800

    Remove some trivial tests. Tidy up the code. Change: 114763128

--- tensorflow/python/ops/control_flow_grad.py
+++ tensorflow/python/ops/control_flow_grad.py
@@ -144,9 +144,12 @@ def _ExitGrad(op, grad):
   grad_ctxt = op.grad_state.grad_context
   grad_ctxt.AddName(grad.name)
   enter_fn = control_flow_ops._Enter  # pylint: disable=protected-access
-  return enter_fn(grad, grad_ctxt.name, is_constant=False,
-                  parallel_iterations=grad_ctxt.parallel_iterations,
-                  name="b_exit")
+  grad_ctxt.Enter()
+  result = enter_fn(grad, grad_ctxt.name, is_constant=False,
+                    parallel_iterations=grad_ctxt.parallel_iterations,
+                    name="b_exit")
+  grad_ctxt.Exit()
+  return result
 
 
 ops.RegisterGradient("RefExit")(_ExitGrad)
