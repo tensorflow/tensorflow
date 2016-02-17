53c3570545a9f6c32dd3be70c036ad701703ec04
Igor Babuschkin <igor@babuschk.in>
2016-02-17 18:17:58 +0000

    Enable GPU reduction for doubles

--- tensorflow/core/kernels/reduction_ops_sum.cc
+++ tensorflow/core/kernels/reduction_ops_sum.cc
@@ -41,6 +41,7 @@ REGISTER_KERNEL_BUILDER(
           .HostMemory("reduction_indices"), \
       ReductionOp<GPUDevice, type, Eigen::internal::SumReducer<type>>);
 REGISTER_GPU_KERNELS(float);
+REGISTER_GPU_KERNELS(double);
 #undef REGISTER_GPU_KERNELS
 
 REGISTER_KERNEL_BUILDER(
