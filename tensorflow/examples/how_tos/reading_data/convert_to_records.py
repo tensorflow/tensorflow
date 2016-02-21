d25821376669ee193363d6098e127096029110af
e3 <eerwitt@gmail.com>
2016-02-21 11:44:07 -0800

    TFRecordWriter explicitly calling close in mnist example.

--- tensorflow/examples/how_tos/reading_data/convert_to_records.py
+++ tensorflow/examples/how_tos/reading_data/convert_to_records.py
@@ -68,6 +68,7 @@ def convert_to(images, labels, name):
         'label': _int64_feature(int(labels[index])),
         'image_raw': _bytes_feature(image_raw)}))
     writer.write(example.SerializeToString())
+  writer.close()
 
 
 def main(argv):
