497606904be87f7a4078ed7ee0784afaa094b258
Geoffrey Irving <geoffreyi@google.com>
2016-02-24 14:46:43 -0800

    Fix build issue with safety fix to gather and scatter Change: 115495726

--- tensorflow/python/kernel_tests/scatter_ops_test.py
+++ tensorflow/python/kernel_tests/scatter_ops_test.py
@@ -128,11 +128,11 @@ class ScatterTest(tf.test.TestCase):
 
         # Test some out of range errors.
         indices = np.array([-1, 0, 5])
-        with self.assertRaisesOpError('indices is out of range'):
+        with self.assertRaisesOpError(r'indices\[0\] = -1 is not in \[0, 6\)'):
           op(ref, indices, updates).eval()
 
         indices = np.array([2, 0, 6])
-        with self.assertRaisesOpError('indices is out of range'):
+        with self.assertRaisesOpError(r'indices\[2\] = 6 is not in \[0, 6\)'):
           op(ref, indices, updates).eval()
 
   # TODO(fpmc): Re-enable this test when gpu_pip test actually runs on a GPU.
