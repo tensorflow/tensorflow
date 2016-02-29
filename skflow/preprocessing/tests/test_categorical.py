7b11cedae8d9547fe5401538bb22b973e4361abc
Illia Polosukhin <ilblackdragon@gmail.com>
2016-02-28 17:26:19 -0800

    Fixing changes in tests due to reordering after trimming

--- skflow/preprocessing/tests/test_categorical.py
+++ skflow/preprocessing/tests/test_categorical.py
@@ -30,8 +30,8 @@ class CategoricalTest(tf.test.TestCase):
             [["0"], [1], [float('nan')], 
             ["C"], ["C"], [1], ["0"], [np.nan], [3]])
         self.assertAllEqual(list(X), [
-            [1], [2], [0], [3], 
-            [3], [2], [1], [0],
+            [1], [3], [0], [2], 
+            [2], [3], [1], [0],
             [0]])
 
     def testSingleCategoricalProcessorPandasSingleDF(self):
