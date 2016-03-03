b3a193de22c103a3e5189d5f548c1f9a9d10e3a0
Derek Murray <mrry@google.com>
2016-03-02 16:14:56 -0800

    Adds an in-process gRPC server with support for clean shutdown.
    This CL refactors the `grpc_server_lib` library such that it exports a
    `ServerInterface` class with `Start()`, `Stop()`, and `Join()`
    methods; and a factory method that takes a new `ServerDef` proto for
    configuring the server.
    
    Also fixes a bug in "master.cc" whereby a `~Master()` would hang if GC
    was disabled.
    
    TODO(mrry): Add a SWIG wrapper so that this can be instantiated in Python.
    Change: 116194792

--- tensorflow/core/distributed_runtime/master.cc
+++ tensorflow/core/distributed_runtime/master.cc
@@ -63,17 +63,20 @@ Master::Master(MasterEnv* env, double session_gc_seconds)
   CHECK(!env->local_devices.empty());
 
   if (session_gc_seconds_ > 0.0) {
-    SchedClosure([this]() { GC(); });
+    gc_thread_ = env_->env->StartThread(ThreadOptions(), "TF_master_GC",
+                                        [this]() { GC(); });
+  } else {
+    gc_thread_ = nullptr;
   }
 }
 
 Master::~Master() {
-  {
+  if (gc_thread_) {
     mutex_lock l(mu_);
     shutdown_ = true;
     shutdown_cv_.notify_all();
+    delete gc_thread_;
   }
-  gc_stopped_.WaitForNotification();
 }
 
 void Master::GC() {
@@ -104,7 +107,6 @@ void Master::GC() {
     }
     for (const auto& handle : handles) sessions_.erase(handle);
   }
-  gc_stopped_.Notify();
 }
 
 class DeviceFinder {
