31757e11afd319639c1da9c19ca367d51dcd1940
A. Unique TensorFlower <nobody@tensorflow.org>
2016-03-03 10:59:19 -0800

    Only expose documented symbols from the tf package, plus current whitelisted exceptions.
    Qualified names removed by this change:
      tf.DESCRIPTOR
      tf.absolute_import
      tf.division
      tf.print_function
      tf.xrange
    Change: 116268941

--- tensorflow/python/util/all_util.py
+++ tensorflow/python/util/all_util.py
@@ -25,27 +25,33 @@ import sys
 _reference_pattern = re.compile(r'^@@(\w+)$', flags=re.MULTILINE)
 
 
-def make_all(module_name, doc_string_module_name=None):
-  """Generate `__all__` from a module's docstring.
+def make_all(module_name, doc_string_modules=None):
+  """Generate `__all__` from the docstring of one or more modules.
 
-  Usage: `make_all(__name__)`.  The caller module must have a docstring,
-  and `__all__` will contain all symbols with `@@` references, where
-  that symbol currently exists in the module named `module_name`.
+  Usage: `make_all(__name__)` or
+  `make_all(__name__, [sys.modules(__name__), other_module])`. The doc string
+  modules must each a docstring, and `__all__` will contain all symbols with
+  `@@` references, where that symbol currently exists in the module named
+  `module_name`.
 
   Args:
     module_name: The name of the module (usually `__name__`).
-    doc_string_module_name: the name of the module whose docstring should
-        be used to find symbols. If None, then `module_name` is used.
+    doc_string_modules: a list of modules from which to take docstring.
+    If None, then a list containing only the module named `module_name` is used.
 
   Returns:
     A list suitable for use as `__all__`.
   """
-  if doc_string_module_name is None: doc_string_module_name = module_name
-  doc = sys.modules[doc_string_module_name].__doc__
+  if doc_string_modules is None: doc_string_modules = [sys.modules[module_name]]
   cur_members = set([name for name, _
                      in inspect.getmembers(sys.modules[module_name])])
-  return [m.group(1) for m in _reference_pattern.finditer(doc)
-          if m.group(1) in cur_members]
+
+  results = set()
+  for doc_module in doc_string_modules:
+    results.update([m.group(1)
+                    for m in _reference_pattern.finditer(doc_module.__doc__)
+                    if m.group(1) in cur_members])
+  return list(results)
 
 
 __all__ = ['make_all']
