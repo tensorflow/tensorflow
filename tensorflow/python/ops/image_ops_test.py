698b6e9dbd1a59a91ab4d660821444d61b2a94a0
panmari <smmuzi@gmail.com>
2016-03-04 11:37:17 +0100

    Also testing align_corners feature when comparing CPU with GPU implementation.

--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -977,18 +977,21 @@ class ResizeImagesTest(test_util.TensorFlowTestCase):
     input_shape = [1, 5, 5, 3]
     target_height = target_width = 10
     for nptype in [np.float]:
-      img_np = np.arange(0, np.prod(input_shape), dtype=nptype).reshape(input_shape)
-      with self.test_session(use_gpu=True):
-        image = constant_op.constant(img_np, shape=input_shape)
-        out_op = image_ops.resize_images(image, target_height, target_width,
-                                         image_ops.ResizeMethod.NEAREST_NEIGHBOR)
-        gpu_val = out_op.eval()
-      with self.test_session(use_gpu=False):
-        image = constant_op.constant(img_np, shape=input_shape)
-        out_op = image_ops.resize_images(image, target_height, target_width,
-                                         image_ops.ResizeMethod.NEAREST_NEIGHBOR)
-        cpu_val = out_op.eval()
-      self.assertAllClose(cpu_val, gpu_val, rtol=1e-5, atol=1e-5)
+      for align_corners in [True, False]:
+        img_np = np.arange(0, np.prod(input_shape), dtype=nptype).reshape(input_shape)
+        with self.test_session(use_gpu=True):
+          image = constant_op.constant(img_np, shape=input_shape)
+          out_op = image_ops.resize_images(image, target_height, target_width,
+                                           image_ops.ResizeMethod.NEAREST_NEIGHBOR,
+                                           align_corners=align_corners)
+          gpu_val = out_op.eval()
+        with self.test_session(use_gpu=False):
+          image = constant_op.constant(img_np, shape=input_shape)
+          out_op = image_ops.resize_images(image, target_height, target_width,
+                                           image_ops.ResizeMethod.NEAREST_NEIGHBOR,
+                                           align_corners=align_corners)
+          cpu_val = out_op.eval()
+        self.assertAllClose(cpu_val, gpu_val, rtol=1e-5, atol=1e-5)
 
 
 class ResizeImageWithCropOrPadTest(test_util.TensorFlowTestCase):
