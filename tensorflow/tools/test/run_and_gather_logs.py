6f1acd3af3493de4cb7900629ce065d66c8c28bc
Eugene Brevdo <ebrevdo@gmail.com>
2016-03-15 12:22:04 -0800

    A few more tweaks to test_log.proto to reflect what's accessible from bazel build.
    Also some updates to run_and_gather_logs.
    
    Reduce the set of tests that run_and_gather_logs_test runs to speed up the tests.
    Change: 117271064

--- tensorflow/tools/test/run_and_gather_logs.py
+++ tensorflow/tools/test/run_and_gather_logs.py
@@ -13,17 +13,35 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Library for getting system information during TensorFlow tests."""
+"""Test runner for TensorFlow tests."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
+import shlex
+import sys
 
 import tensorflow as tf
 
+# pylint: disable=g-import-not-at-top
+# pylint: disable=g-bad-import-order
+# pylint: disable=unused-import
+# Note: cpuinfo and psutil are not installed for you in the TensorFlow
+# OSS tree.  They are installable via pip.
+try:
+  import cpuinfo
+  import psutil
+except ImportError as e:
+  tf.logging.error("\n\n\nERROR: Unable to import necessary library: {}.  "
+                   "Issuing a soft exit.\n\n\n".format(e.message))
+  sys.exit(0)
+# pylint: enable=g-bad-import-order
+# pylint: enable=unused-import
+
 from google.protobuf import text_format
+from tensorflow.core.util import test_log_pb2
 from tensorflow.tools.test import run_and_gather_logs_lib
 
 FLAGS = tf.app.flags.FLAGS
@@ -36,6 +54,21 @@ tf.app.flags.DEFINE_string(
 tf.app.flags.DEFINE_bool(
     "test_log_output_use_tmpdir", False,
     """Store the log output into tmpdir?.""")
+tf.app.flags.DEFINE_string(
+    "compilation_mode", "", """Mode used during this build (e.g. opt, dbg).""")
+tf.app.flags.DEFINE_string(
+    "cc_flags", "", """CC flags used during this build.""")
+
+
+def gather_build_configuration():
+  build_config = test_log_pb2.BuildConfiguration()
+  build_config.mode = FLAGS.compilation_mode
+  # Include all flags except includes
+  cc_flags = [
+      flag for flag in shlex.split(FLAGS.cc_flags)
+      if not flag.startswith("-i")]
+  build_config.cc_flags.extend(cc_flags)
+  return build_config
 
 
 def main(unused_args):
@@ -43,6 +76,10 @@ def main(unused_args):
   test_args = FLAGS.test_args
   test_results, _ = run_and_gather_logs_lib.run_and_gather_logs(
       test_name, test_args)
+
+  # Additional bits we receive from bazel
+  test_results.build_configuration.CopyFrom(gather_build_configuration())
+
   serialized_test_results = text_format.MessageToString(test_results)
 
   if not FLAGS.test_log_output:
