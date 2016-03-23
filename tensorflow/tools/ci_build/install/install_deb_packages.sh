3ad7d3e289aa8e7af4ac98f7f076a8637dc7ecf8
A. Unique TensorFlower <nobody@tensorflow.org>
2016-03-23 09:19:51 -0800

    Adding an FFmpeg install to the Ubuntu Jenkins test machines. This change will allow ops to be added that use FFmpeg. Note that OSX machines will be used to validate TensorFlow without FFmpeg installed. Change: 117942636

--- tensorflow/tools/ci_build/install/install_deb_packages.sh
+++ tensorflow/tools/ci_build/install/install_deb_packages.sh
@@ -16,6 +16,11 @@
 
 set -e
 
+# Install FFmpeg from an alternate repository.
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:mc3man/trusty-media
+apt-get update
+
 # Install dependencies from ubuntu deb repository.
 apt-get update
 apt-get install -y \
@@ -23,6 +28,7 @@ apt-get install -y \
     build-essential \
     cmake \
     curl \
+    ffmpeg \
     git \
     openjdk-8-jdk \
     openjdk-8-jre-headless \
