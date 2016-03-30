115344555592bd08eaf175694a165d1dba5db8dc
Illia Polosukhin <ilblackdragon@gmail.com>
2016-03-30 13:22:14 -0700

    Fixes import errors in the examples for skflow.

--- tensorflow/contrib/skflow/__init__.py
+++ tensorflow/contrib/skflow/__init__.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from python import *
+from tensorflow.contrib.skflow.python.skflow import *
