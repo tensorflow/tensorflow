838c47b173c733f770e2ac799f1bc1faa0a3128e
A. Unique TensorFlower <nobody@tensorflow.org>
2016-04-06 15:02:45 -0800

    Add SparseTensor.eval. Change: 119212533

--- tensorflow/python/framework/ops_test.py
+++ tensorflow/python/framework/ops_test.py
@@ -58,10 +58,22 @@ class TensorTest(test_util.TensorFlowTestCase):
 class SparseTensorTest(test_util.TensorFlowTestCase):
 
   def testPythonConstruction(self):
-    sp = ops.SparseTensor([[1, 2], [2, 0], [3, 4]], ["a", "b", "c"], [4, 5])
+    indices = [[1, 2], [2, 0], [3, 4]]
+    values = ["a", "b", "c"]
+    shape = [4, 5]
+    sp = ops.SparseTensor(indices, values, shape)
     self.assertEqual(sp.indices.dtype, dtypes.int64)
     self.assertEqual(sp.values.dtype, dtypes.string)
     self.assertEqual(sp.shape.dtype, dtypes.int64)
+    with self.test_session() as sess:
+      value = sp.eval()
+      self.assertAllEqual(indices, value.indices)
+      self.assertAllEqual(values, value.values)
+      self.assertAllEqual(shape, value.shape)
+      sess_run_value = sess.run(sp)
+      self.assertAllEqual(sess_run_value.indices, value.indices)
+      self.assertAllEqual(sess_run_value.values, value.values)
+      self.assertAllEqual(sess_run_value.shape, value.shape)
 
 
 class IndexedSlicesTest(test_util.TensorFlowTestCase):
