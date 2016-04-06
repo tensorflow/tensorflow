e44c0f6339888e97d7c07a5e4e4a4d101e9473b7
A. Unique TensorFlower <nobody@tensorflow.org>
2016-04-06 08:54:07 -0800

    Remove NumericParse32 from str_util.h, replacing calls with safe_strto32 from numbers.h.
    This removes the duplicate code, but has a small behavior change - safe_strto32
    strips leading and trailing whitespace, while NumericParse does not. But this
    seems okay.
    
    Also changed the flag parsing command_line_flags.cc to template on int32 instead of int.
    Change: 119170865

--- tensorflow/core/lib/strings/str_util.cc
+++ tensorflow/core/lib/strings/str_util.cc
@@ -14,8 +14,10 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/core/lib/strings/str_util.h"
+
 #include <ctype.h>
 #include <vector>
+#include "tensorflow/core/lib/strings/numbers.h"
 
 namespace tensorflow {
 namespace str_util {
@@ -206,17 +208,6 @@ bool CUnescape(StringPiece source, string* dest, string* error) {
   return true;
 }
 
-bool NumericParse32(const string& text, int32* val) {
-  // Slow, but this code is not performance critical, and this
-  // doesn't bring in any new dependencies
-  char junk;
-  if (sscanf(text.c_str(), "%d%c", val, &junk) == 1) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 void StripTrailingWhitespace(string* s) {
   string::size_type i;
   for (i = s->size(); i > 0 && isspace((*s)[i - 1]); --i) {
@@ -337,7 +328,7 @@ bool SplitAndParseAsInts(StringPiece text, char delim,
   std::vector<string> num_strings = Split(text, delim);
   for (const auto& s : num_strings) {
     int32 num;
-    if (!NumericParse32(s, &num)) return false;
+    if (!strings::safe_strto32(s, &num)) return false;
     result->push_back(num);
   }
   return true;
