9b6354f7492ea6c881fd977053efac4a49b8a764
Illia Polosukhin <ilblackdragon@gmail.com>
2016-04-06 21:58:23 -0700

    Added support for unsupervised data feeder (only X given)

--- tensorflow/contrib/skflow/python/skflow/io/data_feeder.py
+++ tensorflow/contrib/skflow/python/skflow/io/data_feeder.py
@@ -37,6 +37,8 @@ def _get_in_out_shape(x_shape, y_shape, n_classes, batch_size):
     """Returns shape for input and output of the data feeder."""
     x_shape = list(x_shape[1:]) if len(x_shape) > 1 else [1]
     input_shape = [batch_size] + x_shape
+    if y_shape is None:
+        return input_shape, None
     y_shape = list(y_shape[1:]) if len(y_shape) > 1 else []
     # Skip first dimention if it is 1.
     if y_shape and y_shape[0] == 1:
@@ -52,10 +54,12 @@ def _data_type_filter(X, y):
     """Filter data types into acceptable format"""
     if HAS_DASK:
         X = extract_dask_data(X)
-        y = extract_dask_labels(y)
+        if y is not None:
+            y = extract_dask_labels(y)
     if HAS_PANDAS:
         X = extract_pandas_data(X)
-        y = extract_pandas_labels(y)
+        if y is not None:
+            y = extract_pandas_labels(y)
     return X, y
 
 
@@ -80,7 +84,8 @@ def setup_train_data_feeder(X, y, n_classes, batch_size):
     if HAS_DASK:
         import dask.dataframe as dd
         allowed_classes = (dd.Series, dd.DataFrame)
-        if isinstance(X, allowed_classes) and isinstance(y, allowed_classes):
+        if (isinstance(X, allowed_classes) and 
+            (y is None or isinstance(y, allowed_classes))):
             data_feeder_cls = DaskDataFeeder
         else:
             data_feeder_cls = DataFeeder
@@ -88,7 +93,7 @@ def setup_train_data_feeder(X, y, n_classes, batch_size):
         data_feeder_cls = DataFeeder
 
     if _is_iterable(X):
-        if not _is_iterable(y):
+        if y is not None and not _is_iterable(y):
             raise ValueError("Both X and y should be iterators for "
                              "streaming learning to work.")
         data_feeder_cls = StreamingDataFeeder
@@ -151,7 +156,7 @@ class DataFeeder(object):
         X: feature Nd numpy matrix of shape [n_samples, n_features, ...].
         y: target vector, either floats for regression or class id for
             classification. If matrix, will consider as a sequence
-            of targets.
+            of targets. Can be None for unsupervised setting.
         n_classes: number of classes, 0 and 1 are considered regression.
         batch_size: mini batch size to accumulate.
         random_state: numpy RandomState object to reproduce sampling.
@@ -172,11 +177,15 @@ class DataFeeder(object):
         y_dtype = np.int64 if n_classes > 1 else np.float32
         self.X = check_array(X, ensure_2d=False,
                              allow_nd=True, dtype=x_dtype)
-        self.y = check_array(y, ensure_2d=False, dtype=y_dtype)
+        self.y = (None if y is None else check_array(y, ensure_2d=False,
+                                                     dtype=y_dtype))
         self.n_classes = n_classes
         self.batch_size = batch_size
         self.input_shape, self.output_shape = _get_in_out_shape(
-            self.X.shape, self.y.shape, n_classes, batch_size)
+            self.X.shape,
+            None if self.y is None else self.y.shape,
+            n_classes,
+            batch_size)
         # Input dtype matches dtype of X.
         self.input_dtype = self.X.dtype
         # Output dtype always float32 (because for classification we use
@@ -188,16 +197,32 @@ class DataFeeder(object):
         self.epoch = 0
 
     def input_builder(self):
-        """Builds inputs in the graph."""
+        """Builds inputs in the graph.
+        
+        Returns:
+            Two placeholders for inputs and outputs.
+        """
         input_shape = [None] + self.input_shape[1:]
-        output_shape = [None] + self.output_shape[1:]
         self._input_placeholder = array_ops.placeholder(dtypes.as_dtype(self.input_dtype), input_shape,
             name="input")
-        self._output_placeholder = array_ops.placeholder(dtypes.as_dtype(self.output_dtype), output_shape,
-            name="output")
+        if self.output_shape is None:
+            self._output_placeholder = None
+        else:
+            output_shape = [None] + self.output_shape[1:]
+            self._output_placeholder = array_ops.placeholder(dtypes.as_dtype(self.output_dtype), output_shape,
+                name="output")
         return self._input_placeholder, self._output_placeholder
 
     def set_placeholders(self, input_placeholder, output_placeholder):
+        """Sets placeholders for this data feeder.
+
+        Args:
+            input_placeholder: Placeholder for `X` variable. Should match shape
+                               of the examples in the X dataset.
+            output_placeholder: Placeholder for `y` variable. Should match
+                                shape of the examples in the y dataset. Can be
+                                None.
+        """
         self._input_placeholder = input_placeholder
         self._output_placeholder = output_placeholder
 
@@ -232,6 +257,9 @@ class DataFeeder(object):
             inp = np.array(self.X[batch_indices]).reshape((batch_indices.shape[0], 1)) \
                 if len(self.X.shape) == 1 else self.X[batch_indices]
 
+            if self._output_placeholder is None:
+                return {self._input_placeholder.name: inp}
+
             # assign labels from random indices
             self.output_shape[0] = batch_indices.shape[0]
             out = np.zeros(self.output_shape, dtype=self.output_dtype)
@@ -253,7 +281,8 @@ class DataFeeder(object):
                 self.offset = 0
                 self.epoch += 1
 
-            return {self._input_placeholder.name: inp, self._output_placeholder.name: out}
+            return {self._input_placeholder.name: inp,
+            self._output_placeholder.name: out}
         return _feed_dict_fn
 
 
