9671b6c17f6d4809cc37c3218e27f885084b6253
Manjunath Kudlur <keveman@gmail.com>
2016-04-06 20:25:15 -0800

    Implement a file factory mechanism to handle network file systems. - Env dispatches to a FileSystem interface - FileSystemFactory is used to look up the correct FileSystem implementation based on the prefix of the filename - Provide a registration mechanism to register different factories Change: 119233977

--- tensorflow/core/platform/env.cc
+++ tensorflow/core/platform/env.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "tensorflow/core/platform/env.h"
 #include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/gtl/map_util.h"
 #include "tensorflow/core/lib/gtl/stl_util.h"
 #include "tensorflow/core/platform/protobuf.h"
 
@@ -22,9 +23,92 @@ namespace tensorflow {
 
 Env::~Env() {}
 
-RandomAccessFile::~RandomAccessFile() {}
+Status Env::GetFileSystemForFile(const string& fname, FileSystem** result) {
+  string scheme = GetSchemeFromURI(fname);
+  FileSystem* file_system = GlobalFileSystemRegistry()->Lookup(scheme);
+  if (!file_system) {
+    return errors::Unimplemented("File system scheme ", scheme,
+                                 " not implemented");
+  }
+  *result = file_system;
+  return Status::OK();
+}
 
-WritableFile::~WritableFile() {}
+Status Env::NewRandomAccessFile(const string& fname,
+                                RandomAccessFile** result) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->NewRandomAccessFile(fname, result);
+}
+
+Status Env::NewWritableFile(const string& fname, WritableFile** result) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->NewWritableFile(fname, result);
+}
+
+Status Env::NewAppendableFile(const string& fname, WritableFile** result) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->NewAppendableFile(fname, result);
+}
+
+Status Env::NewReadOnlyMemoryRegionFromFile(const string& fname,
+                                            ReadOnlyMemoryRegion** result) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->NewReadOnlyMemoryRegionFromFile(fname, result);
+}
+
+bool Env::FileExists(const string& fname) {
+  FileSystem* fs;
+  if (!GetFileSystemForFile(fname, &fs).ok()) {
+    return false;
+  }
+  return fs->FileExists(fname);
+}
+
+Status Env::GetChildren(const string& dir, std::vector<string>* result) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(dir, &fs));
+  return fs->GetChildren(dir, result);
+}
+
+Status Env::DeleteFile(const string& fname) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->DeleteFile(fname);
+}
+
+Status Env::CreateDir(const string& dirname) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(dirname, &fs));
+  return fs->CreateDir(dirname);
+}
+
+Status Env::DeleteDir(const string& dirname) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(dirname, &fs));
+  return fs->DeleteDir(dirname);
+}
+
+Status Env::GetFileSize(const string& fname, uint64* file_size) {
+  FileSystem* fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(fname, &fs));
+  return fs->GetFileSize(fname, file_size);
+}
+
+Status Env::RenameFile(const string& src, const string& target) {
+  FileSystem* src_fs;
+  FileSystem* target_fs;
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(src, &src_fs));
+  TF_RETURN_IF_ERROR(GetFileSystemForFile(target, &target_fs));
+  if (src_fs != target_fs) {
+    return errors::Unimplemented("Renaming ", src, " to ", target,
+                                 " not implemented");
+  }
+  return src_fs->RenameFile(src, target);
+}
 
 Thread::~Thread() {}
 
