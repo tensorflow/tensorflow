5a009ced208a07c9b899b8f7d9031ec3ee441cab
hunkim <hunkim@gmail.com>
2016-04-10 21:46:32 +0800

    Let reshape guess the dim.

--- tensorflow/models/image/cifar10/cifar10.py
+++ tensorflow/models/image/cifar10/cifar10.py
@@ -221,11 +221,8 @@ def inference(images):
   # local3
   with tf.variable_scope('local3') as scope:
     # Move everything into depth so we can perform a single matrix multiply.
-    dim = 1
-    for d in pool2.get_shape()[1:].as_list():
-      dim *= d
-    reshape = tf.reshape(pool2, [FLAGS.batch_size, dim])
-
+    reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+    dim = reshape.get_shape()[1].value
     weights = _variable_with_weight_decay('weights', shape=[dim, 384],
                                           stddev=0.04, wd=0.004)
     biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
