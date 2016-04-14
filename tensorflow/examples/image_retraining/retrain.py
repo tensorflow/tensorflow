d4bf5e072478c92cae2bd71b96e10e77e229dfba
SeongJae Park <sj38.park@gmail.com>
2016-04-15 01:52:54 +0900

    image_retraining: fix trivial typos (#1908)
    This commit fix trivial typos under image_retraining/ directory.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- tensorflow/examples/image_retraining/retrain.py
+++ tensorflow/examples/image_retraining/retrain.py
@@ -345,7 +345,7 @@ def maybe_download_and_extract():
                                              _progress)
     print()
     statinfo = os.stat(filepath)
-    print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
+    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
   tarfile.open(filepath, 'r:gz').extractall(dest_directory)
 
 
@@ -469,11 +469,11 @@ def get_random_cached_bottlenecks(sess, image_lists, how_many, category,
     bottleneck_tensor: The bottleneck output layer of the CNN graph.
 
   Returns:
-    List of bottleneck arrays and their corresponding ground truthes.
+    List of bottleneck arrays and their corresponding ground truths.
   """
   class_count = len(image_lists.keys())
   bottlenecks = []
-  ground_truthes = []
+  ground_truths = []
   for unused_i in range(how_many):
     label_index = random.randrange(class_count)
     label_name = list(image_lists.keys())[label_index]
@@ -485,8 +485,8 @@ def get_random_cached_bottlenecks(sess, image_lists, how_many, category,
     ground_truth = np.zeros(class_count, dtype=np.float32)
     ground_truth[label_index] = 1.0
     bottlenecks.append(bottleneck)
-    ground_truthes.append(ground_truth)
-  return bottlenecks, ground_truthes
+    ground_truths.append(ground_truth)
+  return bottlenecks, ground_truths
 
 
 def get_random_distorted_bottlenecks(
@@ -514,11 +514,11 @@ def get_random_distorted_bottlenecks(
     bottleneck_tensor: The bottleneck output layer of the CNN graph.
 
   Returns:
-    List of bottleneck arrays and their corresponding ground truthes.
+    List of bottleneck arrays and their corresponding ground truths.
   """
   class_count = len(image_lists.keys())
   bottlenecks = []
-  ground_truthes = []
+  ground_truths = []
   for unused_i in range(how_many):
     label_index = random.randrange(class_count)
     label_name = image_lists.keys()[label_index]
@@ -539,8 +539,8 @@ def get_random_distorted_bottlenecks(
     ground_truth = np.zeros(class_count, dtype=np.float32)
     ground_truth[label_index] = 1.0
     bottlenecks.append(bottleneck)
-    ground_truthes.append(ground_truth)
-  return bottlenecks, ground_truthes
+    ground_truths.append(ground_truth)
+  return bottlenecks, ground_truths
 
 
 def should_distort_images(flip_left_right, random_crop, random_scale,
@@ -734,7 +734,7 @@ def main(_):
   sess = tf.Session()
 
   if do_distort_images:
-    # We will be applying distortions, so set upthe operations we'll need.
+    # We will be applying distortions, so setup the operations we'll need.
     distorted_jpeg_data_tensor, distorted_image_tensor = add_input_distortions(
         FLAGS.flip_left_right, FLAGS.random_crop, FLAGS.random_scale,
         FLAGS.random_brightness)
