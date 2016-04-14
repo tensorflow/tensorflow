bf1f8ee213a60aa2db79046cfd9d015cadfef602
Dan Aloni <alonid@gmail.com>
2016-04-14 19:53:20 +0300

    tv.Variable doc: fix typo (#1907)
    'b' does not exist in the example, probably 'y' was meant.

--- tensorflow/python/ops/variables.py
+++ tensorflow/python/ops/variables.py
@@ -56,7 +56,7 @@ class Variable(object):
   y = tf.matmul(w, ...another variable or tensor...)
 
   # The overloaded operators are available too.
-  z = tf.sigmoid(w + b)
+  z = tf.sigmoid(w + y)
 
   # Assign a new value to the variable with `assign()` or a related method.
   w.assign(w + 1.0)
