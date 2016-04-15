43f1c2633797040881b1cf99c785d044f30d9582
Dan Mané <danmane@gmail.com>
2016-04-15 14:29:33 -0800

    Add TensorBoard auto-reload behavior.
    Auto-reloading defaults on and is controlled by a setting.
    
    To have a place to put that setting, also add a settings panel.
    
    The settings panel uses paper-dialog and is opened by clicking a settings icon button in the global top right.
    
    Minor changes to how the toolbar is laid out by CSS to ensure that the settings all show up in a line and don't bunch up together.
    
    Also, the "TensorBoard" message on the left no longer takes a fixed pixel width (everything is flex), so the toolbar will use space more appropriately when the window size is small.
    Change: 119997005

--- tensorflow/tensorboard/components/tf-tensorboard/test/tensorboardTests.ts
+++ tensorflow/tensorboard/components/tf-tensorboard/test/tensorboardTests.ts
@@ -1,10 +1,25 @@
+/* Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 describe('tf-tensorboard tests', () => {
   window.HTMLImports.whenReady(() => {
     let assert = chai.assert;
     let demoRouter = TF.Backend.router('data', true);
     function makeTensorBoard() {
-      let tensorboard: any = fixture('testElementFixture');
+      let tensorboard: any = fixture('tensorboardFixture');
       tensorboard.router = demoRouter;
+      tensorboard.autoReloadEnabled = false;
       return tensorboard;
     }
     var tensorboard;
@@ -62,6 +77,42 @@ describe('tf-tensorboard tests', () => {
             assert.isFalse(called, `reload was not called`);
             done();
           });
+    });
+
+    describe('top right global icons', function() {
+      it('Clicking the reload button will call reload', function() {
+        var called = false;
+        tensorboard.reload = function() { called = true; };
+        tensorboard.$$('#reload-button').click();
+        assert.isTrue(called);
+      });
+
+      it('settings pane is hidden', function() {
+        assert.equal(tensorboard.$.settings.style['display'], 'none');
+      });
+
+      it('settings icon button opens the settings pane', function(done) {
+        tensorboard.$$('#settings-button').click();
+        setTimeout(function() {  // async, give it a moment
+          assert.notEqual(tensorboard.$.settings.style['display'], 'none');
+          done();
+        });
+      });
+
+      it('Autoreload checkbox toggle works', function() {
+        var checkbox = tensorboard.$$('#auto-reload-checkbox');
+        assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
+        var oldValue = checkbox.checked;
+        checkbox.click();
+        assert.notEqual(oldValue, checkbox.checked);
+        assert.equal(checkbox.checked, tensorboard.autoReloadEnabled);
+      });
+
+      it('Autoreload checkbox contains correct interval info', function() {
+        var checkbox = tensorboard.$$('#auto-reload-checkbox');
+        var timeInSeconds = tensorboard.autoReloadIntervalSecs + 's';
+        assert.include(checkbox.innerText, timeInSeconds);
+      });
     });
   });
 });
