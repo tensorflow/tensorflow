af91b35a0175759674d76dce1179eb82879d5ff8
A. Unique TensorFlower <nobody@tensorflow.org>
2016-04-15 08:32:40 -0800

    Renamed parameters in GPU ApplyGradientDescent for parity with the CPU code. Change: 119962541

--- tensorflow/core/kernels/training_ops_gpu.cu.cc
+++ tensorflow/core/kernels/training_ops_gpu.cu.cc
@@ -28,12 +28,12 @@ namespace functor {
 template <typename T>
 struct ApplyGradientDescent<GPUDevice, T> {
   void operator()(const GPUDevice& d, typename TTypes<T>::Flat var,
-                  typename TTypes<T>::ConstScalar alpha,
-                  typename TTypes<T>::ConstFlat delta) {
+                  typename TTypes<T>::ConstScalar lr,
+                  typename TTypes<T>::ConstFlat grad) {
     Eigen::array<typename TTypes<T>::Tensor::Index, 1> bcast;
-    bcast[0] = delta.dimension(0);
+    bcast[0] = grad.dimension(0);
     Eigen::Sizes<1> single;
-    var.device(d) -= alpha.reshape(single).broadcast(bcast) * delta;
+    var.device(d) -= lr.reshape(single).broadcast(bcast) * grad;
   }
 };
 
