06af3c8bff0da448f9e1d4b23e8278f430a0b250
A. Unique TensorFlower <nobody@tensorflow.org>
2016-04-28 17:20:12 -0800

    Make skflow pass linter, part 2. Change: 121078769

--- tensorflow/contrib/learn/python/learn/trainer.py
+++ tensorflow/contrib/learn/python/learn/trainer.py
@@ -17,7 +17,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from six.moves import xrange   # pylint: disable=redefined-builtin
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import init_ops
@@ -29,36 +29,47 @@ from tensorflow.python.ops import variable_scope as vs
 from tensorflow.contrib.layers import optimizers
 
 
-def train(session, train_op, loss, global_step, feed_dict_fn, steps, monitor,
-          summary_writer=None, summaries=None,
+def train(session,
+          train_op,
+          loss,
+          global_step,
+          feed_dict_fn,
+          steps,
+          monitor,
+          summary_writer=None,
+          summaries=None,
           feed_params_fn=None):
-    """Trains a model for given number of steps, given feed_dict function.
+  """Trains a model for given number of steps, given feed_dict function.
 
-    Args:
-        session: Session object.
-        train: Tensor, trains model.
-        loss: Tensor, loss value.
-        global_step: Tensor, global step of the model.
-        feed_dict_fn: Function that will return a feed dictionary.
-        summary_writer: SummaryWriter object to use for writing summaries.
-        steps: Number of steps to run.
-        monitor: Monitor object to track training progress and induce early stopping
-        summaries: Joined object of all summaries that should be ran.
-    """
-    for step in xrange(steps):
-        feed_dict = feed_dict_fn()
-        if summaries is not None:
-            global_step_value, loss_value, summ, _ = session.run(
-                [global_step, loss, summaries, train_op],
-                feed_dict=feed_dict)
-        else:
-            global_step_value, loss_value, _ = session.run(
-                [global_step, loss, train_op],
-                feed_dict=feed_dict)
-        monitor.update(step, global_step_value, loss_value, session,
-                       feed_params_fn, loss_expression_tensor=loss)
-        if summaries is not None and summary_writer and summ is not None:
-            summary_writer.add_summary(summ, global_step_value)
-        if monitor.monitor_inducing_stop():
-            break
-
+  Args:
+    session: Session object.
+    train: Tensor, trains model.
+    loss: Tensor, loss value.
+    global_step: Tensor, global step of the model.
+    feed_dict_fn: Function that will return a feed dictionary.
+    summary_writer: SummaryWriter object to use for writing summaries.
+    steps: Number of steps to run.
+    monitor: Monitor object to track training progress and induce early
+      stopping
+    summaries: Joined object of all summaries that should be ran.
+  """
+  for step in xrange(steps):
+    feed_dict = feed_dict_fn()
+    if summaries is not None:
+      global_step_value, loss_value, summ, _ = session.run(
+          [global_step, loss, summaries, train_op],
+          feed_dict=feed_dict)
+    else:
+      global_step_value, loss_value, _ = session.run(
+          [global_step, loss, train_op],
+          feed_dict=feed_dict)
+    monitor.update(step,
+                   global_step_value,
+                   loss_value,
+                   session,
+                   feed_params_fn,
+                   loss_expression_tensor=loss)
+    if summaries is not None and summary_writer and summ is not None:
+      summary_writer.add_summary(summ, global_step_value)
+    if monitor.monitor_inducing_stop():
+      break
