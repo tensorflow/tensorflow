1a80ebeac51dd5cc2736b4f0bc68906aa0c1df4e
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-05-08 09:48:55 +0200

    Removed files that should have been ignored.

--- build.sh
+++
@@ -1,4 +0,0 @@
-git submodule update --init
-bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-pip2 install --upgrade /tmp/tensorflow_pkg/tensorflow-0.7.1-py2-none-any.whl
