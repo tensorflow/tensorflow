4780d9d9de7c8bb202154f3022512fdcd822dc20
William Chan <williamchan@cmu.edu>
2016-05-12 17:06:27 -0700

    apache lic text

--- tensorflow/python/kernel_tests/sampling_ops_test.py
+++ tensorflow/python/kernel_tests/sampling_ops_test.py
@@ -1,3 +1,19 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Tests for tensorflow.ops.sampling_ops."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -5,7 +21,6 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import sampling_ops
-from tensorflow.python.ops import gen_sampling_ops
 
 
 class BernoulliSampleTest(tf.test.TestCase):
@@ -16,13 +31,13 @@ class BernoulliSampleTest(tf.test.TestCase):
       a = np.random.randn(10)
       b = np.random.randn(10)
 
-      output = gen_sampling_ops.bernoulli_sample(p, a, b).eval()
+      output = sampling_ops.bernoulli_sample(p, a, b).eval()
 
       self.assertAllEqual(a, output)
 
       p = 1.0
 
-      output = gen_sampling_ops.bernoulli_sample(p, a, b).eval()
+      output = sampling_ops.bernoulli_sample(p, a, b).eval()
 
       self.assertAllEqual(b, output)
 
@@ -34,7 +49,7 @@ class SampleDistributionIndexTest(tf.test.TestCase):
       batch_size = 10
       vocab_size = 100
       dist = np.random.randn(batch_size, vocab_size)
-      idx = gen_sampling_ops.sample_distribution_index(dist).eval()
+      idx = sampling_ops.sample_distribution_index(dist).eval()
 
       for b in range(batch_size):
         self.assertGreaterEqual(idx[b], 0)
@@ -48,7 +63,7 @@ class SampleDistributionIndexTest(tf.test.TestCase):
       hot_idx = np.random.randint(0, vocab_size, size=batch_size)
       dist[range(batch_size), hot_idx] = 1.0
 
-      idx = gen_sampling_ops.sample_distribution_index(dist).eval()
+      idx = sampling_ops.sample_distribution_index(dist).eval()
       self.assertAllEqual(idx, hot_idx)
 
 if __name__ == '__main__':
--- tensorflow/python/ops/sampling_ops.py
+++ tensorflow/python/ops/sampling_ops.py
@@ -1,3 +1,23 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""## Random Sampling Operators.
+
+TensorFlow provides you functions to sample from distributions.
+
+"""
+
 import sys
 import numpy as np
 
@@ -5,8 +25,7 @@ from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_util
 # pylint: disable=wildcard-import
-# 'Constant' gets imported in the module 'array_ops'.
-from tensorflow.python.ops import gen_sampling_ops
+from tensorflow.python.ops.gen_sampling_ops import *
 
 
 @ops.RegisterShape("BernoulliSample")
