e9f3892db7025a4ad43b7f474e19061c42d8464c
Victor Melo <victorhcmelo@gmail.com>
2016-05-15 20:41:11 -0300

    Enable password support for jupyter nb

--- tensorflow/tools/docker/jupyter_notebook_config.py
+++ tensorflow/tools/docker/jupyter_notebook_config.py
@@ -17,3 +17,10 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python2'
+
+# sets a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+    import os
+    from IPython.lib import passwd
+    c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+    del os.environ['PASSWORD']
