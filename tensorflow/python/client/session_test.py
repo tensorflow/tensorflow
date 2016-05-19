be8a3e2cf7738442f2b03c35b2ef9113b178855c
Yuan Yu <yuanbyu@google.com>
2016-05-18 19:25:50 -0800

    Add the ability to return the cost model to the client as part of run metadata. Change: 122696039

--- tensorflow/python/client/session_test.py
+++ tensorflow/python/client/session_test.py
@@ -1125,5 +1125,24 @@ class SessionTest(test_util.TensorFlowTestCase):
       # Avoid lint error regarding 'unused' var a.
       self.assertTrue(a == a)
 
+  def testBuildCostModel(self):
+    run_options = config_pb2.RunOptions()
+    config = config_pb2.ConfigProto(
+        allow_soft_placement=True,
+        graph_options=config_pb2.GraphOptions(build_cost_model=100))
+    with session.Session(config=config) as sess:
+      with ops.device('/gpu:0'):
+        a = array_ops.placeholder(dtypes.float32, shape=[])
+        b = math_ops.add(a, a)
+        c = array_ops.identity(b)
+        d = math_ops.mul(c, c)
+      for step in xrange(120):
+        run_metadata = config_pb2.RunMetadata()
+        sess.run(d, feed_dict={a: 1.0}, options=run_options, run_metadata=run_metadata)
+        if step == 99:
+          self.assertTrue(run_metadata.HasField('cost_graph'))
+        else:
+          self.assertFalse(run_metadata.HasField('cost_graph'))
+
 if __name__ == '__main__':
   googletest.main()
