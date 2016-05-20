cff1bd666e4e9b5750e29eb6454ebf93ff2d28bf
A. Unique TensorFlower <nobody@tensorflow.org>
2016-05-20 00:14:36 -0800

    Adding statistics support for DepthwiseConv2dNative Change: 122810336

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -752,6 +752,41 @@ def _calc_conv_weight_params(graph, node):
                                            filter_in_depth * filter_out_depth))
 
 
+@ops.RegisterStatistics("DepthwiseConv2dNative", "flops")
+def _calc_depthwise_conv_flops(graph, node):
+  """Calculates the compute resources needed for DepthwiseConv2dNative."""
+  input_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
+  input_shape.assert_is_fully_defined()
+  filter_shape = graph_util.tensor_shape_from_node_def_name(graph,
+                                                            node.input[1])
+  filter_shape.assert_is_fully_defined()
+  output_shape = graph_util.tensor_shape_from_node_def_name(graph, node.name)
+  output_shape.assert_is_fully_defined()
+  filter_height = int(filter_shape[0])
+  filter_width = int(filter_shape[1])
+  output_count = np.prod(output_shape.as_list())
+  return ops.OpStats("flops", (output_count * filter_height * filter_width * 2))
+
+
+@ops.RegisterStatistics("DepthwiseConv2dNative", "weight_parameters")
+def _calc_depthwise_conv_weight_params(graph, node):
+  """Calculates the on-disk size of the weights for DepthwiseConv2dNative."""
+  input_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
+  input_shape.assert_is_fully_defined()
+  filter_shape = graph_util.tensor_shape_from_node_def_name(graph,
+                                                            node.input[1])
+  filter_shape.assert_is_fully_defined()
+  output_shape = graph_util.tensor_shape_from_node_def_name(graph, node.name)
+  output_shape.assert_is_fully_defined()
+  filter_height = int(filter_shape[0])
+  filter_width = int(filter_shape[1])
+  filter_in_depth = int(filter_shape[2])
+  filter_channel_multiplier = int(filter_shape[3])
+  return ops.OpStats("weight_parameters", (filter_height * filter_width *
+                                           filter_in_depth *
+                                           filter_channel_multiplier))
+
+
 @ops.RegisterShape("Conv3D")
 def _Conv3DShape(op):
   """Shape function for Conv3D."""
