3b2989f941095df649683a88d62775cf746cfa34
snakecharmer1024 <snakecharmr1024@gmail.com>
2016-05-24 17:14:28 -0700

    Updated padding docstrings to link to padding discussion.

--- tensorflow/python/ops/nn.py
+++ tensorflow/python/ops/nn.py
@@ -490,7 +490,7 @@ def depthwise_conv2d(input, filter, strides, padding, name=None):
     strides: 1-D of size 4.  The stride of the sliding window for each
       dimension of `input`.
     padding: A string, either `'VALID'` or `'SAME'`.  The padding algorithm.
-      See the [comment here](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/nn.py)
+      See the [comment here](https://www.tensorflow.org/api_docs/python/nn.html#convolution)
     name: A name for this operation (optional).
 
   Returns:
@@ -556,7 +556,7 @@ def separable_conv2d(input, depthwise_filter, pointwise_filter, strides,
     strides: 1-D of size 4.  The strides for the depthwise convolution for
       each dimension of `input`.
     padding: A string, either `'VALID'` or `'SAME'`.  The padding algorithm.
-      See the [comment here](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/nn.py)
+      See the [comment here](https://www.tensorflow.org/api_docs/python/nn.html#convolution)
     name: A name for this operation (optional).
 
   Returns:
