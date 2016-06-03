2c033b7b14f665f031cf07c837ec4dd1c4561eb5
Jesper Steen Møller <jesper@selskabet.org>
2016-06-03 10:11:01 +0200

    Work on strings instead of binary in Python3 (#2622)

--- tensorflow/examples/tutorials/word2vec/word2vec_basic.py
+++ tensorflow/examples/tutorials/word2vec/word2vec_basic.py
@@ -51,7 +51,7 @@ filename = maybe_download('text8.zip', 31344016)
 def read_data(filename):
   """Extract the first file enclosed in a zip file as a list of words"""
   with zipfile.ZipFile(filename) as f:
-    data = f.read(f.namelist()[0]).split()
+    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
   return data
 
 words = read_data(filename)
