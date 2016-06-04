89cb0b301f7a5d3174405abc83f185200bed5eb9
ErnestG <me@ernestg.com>
2016-06-04 14:42:06 -0400

    Added a simple permission check and request to prevent crashing on api 23+

--- tensorflow/examples/android/src/org/tensorflow/demo/CameraActivity.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/CameraActivity.java
@@ -16,22 +16,72 @@
 
 package org.tensorflow.demo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class CameraActivity extends Activity {
+  private static final int PERMISSIONS_REQUEST = 1;
+
+  private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+  private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     setContentView(R.layout.activity_camera);
-    if (null == savedInstanceState) {
-      getFragmentManager()
-          .beginTransaction()
-          .replace(R.id.container, CameraConnectionFragment.newInstance())
-          .commit();
+
+    if (hasPermission()) {
+      if (null == savedInstanceState) {
+        setFragment();
+      }
+    } else {
+      getPermission();
     }
+
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    switch (requestCode) {
+      case PERMISSIONS_REQUEST: {
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+          setFragment();
+        } else {
+          getPermission();
+        }
+      }
+    }
+  }
+
+  private boolean hasPermission() {
+    if (ContextCompat.checkSelfPermission(this, PERMISSION_CAMERA) == PackageManager.PERMISSION_DENIED || ContextCompat.checkSelfPermission(this, PERMISSION_STORAGE) == PackageManager.PERMISSION_DENIED) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  private void getPermission() {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(this, PERMISSION_CAMERA) || ActivityCompat.shouldShowRequestPermissionRationale(this, PERMISSION_STORAGE)) {
+      Toast.makeText(CameraActivity.this, "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
+    }
+    ActivityCompat.requestPermissions(this, new String[]{PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
+  }
+
+  private void setFragment() {
+    getFragmentManager()
+            .beginTransaction()
+            .replace(R.id.container, CameraConnectionFragment.newInstance())
+            .commit();
   }
 }
