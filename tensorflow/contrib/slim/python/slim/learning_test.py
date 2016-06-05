65ba9e502af7dc7ca3cde837e93f48982953c8fb
A. Unique TensorFlower <nobody@tensorflow.org>
2016-06-04 19:17:37 -0800

    Adding slim to contrib includes (enabling tf.contrib.slim). Change: 124064568

--- tensorflow/contrib/slim/python/slim/learning_test.py
+++ tensorflow/contrib/slim/python/slim/learning_test.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2016 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import numpy as np
 from numpy import testing as np_testing
 import tensorflow as tf
 
-import tensorflow.contrib.slim as slim
+slim = tf.contrib.slim
 
 
 class ClipGradientNormsTest(tf.test.TestCase):
