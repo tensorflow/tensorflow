8034d8250bb7926a93202c8f53d6af96cf5ed7d0
Pete Warden <petewarden@google.com>
2016-06-08 15:24:12 -0700

    Updated Eigen version in makefile

--- tensorflow/contrib/makefile/download_dependencies.sh
+++ tensorflow/contrib/makefile/download_dependencies.sh
@@ -18,7 +18,7 @@ DOWNLOADS_DIR=tensorflow/contrib/makefile/downloads
 
 mkdir ${DOWNLOADS_DIR}
 
-EIGEN_HASH=d02e6a705c30
+EIGEN_HASH=62a2305d5734
 curl "https://bitbucket.org/eigen/eigen/get/${EIGEN_HASH}.tar.gz" \
 -o /tmp/eigen-${EIGEN_HASH}.tar.gz
 tar xzf /tmp/eigen-${EIGEN_HASH}.tar.gz -C ${DOWNLOADS_DIR}
