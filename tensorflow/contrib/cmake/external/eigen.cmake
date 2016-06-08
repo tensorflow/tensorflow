a607538ae791bfc6ed042a19ac8e3bc738cbf51d
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-06-08 10:30:32 -0800

    Enable the vectorization of adds and mult on fp16s. This improves the performance of the toy mnist training by 1 order of magnitude Change: 124374286

--- tensorflow/contrib/cmake/external/eigen.cmake
+++ tensorflow/contrib/cmake/external/eigen.cmake
@@ -7,7 +7,7 @@
 
 include (ExternalProject)
 
-set(eigen_archive_hash "62a2305d5734")
+set(eigen_archive_hash "5f86b31739cd")
 
 set(eigen_INCLUDE_DIRS
     ${CMAKE_CURRENT_BINARY_DIR}
@@ -16,7 +16,7 @@ set(eigen_INCLUDE_DIRS
     ${tensorflow_source_dir}/third_party/eigen3
 )
 set(eigen_URL https://bitbucket.org/eigen/eigen/get/${eigen_archive_hash}.tar.gz)
-set(eigen_HASH SHA256=d5da5c60f7225bc2f104f3494323b929e68e3a188ccf01dcee61df32ff536888)
+set(eigen_HASH SHA256=e1101422f198a5d8c07e008eb801aeac385bf3022d88a0f041acc9f77e09d26e)
 set(eigen_BUILD ${CMAKE_CURRENT_BINARY_DIR}/eigen/src/eigen)
 set(eigen_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/eigen/install)
 
