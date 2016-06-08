e51c62d1a183d351d77de9028cf0e5de26a21f39
Mustafa Ispir <mustafa.ispir@gmail.com>
2016-06-08 13:21:48 -0800

    Added gradient clip to LinearRegressor and DnnClassifier. Set a default for gradient clib. Change: 124392980

--- tensorflow/contrib/learn/python/learn/estimators/dnn.py
+++ tensorflow/contrib/learn/python/learn/estimators/dnn.py
@@ -84,6 +84,10 @@ class DNNClassifier(dnn_linear_combined.DNNLinearCombinedClassifier):
     activation_fn: Activation function applied to each layer. If `None`, will
       use `tf.nn.relu`.
     dropout: When not None, the probability we will drop out a given coordinate.
+    gradient_clip_norm: A float > 0. If provided, gradients are clipped
+      to their global norm with this clipping ratio. See tf.clip_by_global_norm
+      for more details.
+    config: RunConfig object to configure the runtime settings.
   """
 
   def __init__(self,
@@ -95,6 +99,7 @@ class DNNClassifier(dnn_linear_combined.DNNLinearCombinedClassifier):
                optimizer=None,
                activation_fn=nn.relu,
                dropout=None,
+               gradient_clip_norm=None,
                config=None):
     super(DNNClassifier, self).__init__(model_dir=model_dir,
                                         n_classes=n_classes,
@@ -104,6 +109,7 @@ class DNNClassifier(dnn_linear_combined.DNNLinearCombinedClassifier):
                                         dnn_hidden_units=hidden_units,
                                         dnn_activation_fn=activation_fn,
                                         dnn_dropout=dropout,
+                                        gradient_clip_norm=gradient_clip_norm,
                                         config=config)
 
   def _get_train_ops(self, features, targets):
