08af604fa88c57ab6b0e54a6f468d7c268e640e1
Micael Carvalho <MicaelCarvalho@users.noreply.github.com>
2016-06-08 18:28:29 +0200

    Fixing MNIST downloader
    Fixing incompatibility with the tutorial, which states: For your convenience, we've included a script which automatically downloads and imports the MNIST dataset. It will create a directory 'MNIST_data' in which to store the data files.

--- tensorflow/examples/tutorials/mnist/input_data.py
+++ tensorflow/examples/tutorials/mnist/input_data.py
@@ -26,4 +26,6 @@ import numpy
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
