7c3141c44c07f377a65c625a9240cda0bd4e6efe
Clayton Mellina <clayton@yahoo-inc.com>
2016-06-13 23:07:18 -0700

    add FlipGradientOp as user op

---
+++ tensorflow/core/user_ops/flip_gradient.cc
@@ -0,0 +1,33 @@
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+// #include "tensorflow/core/kernels/identity_op.h"
+
+using namespace tensorflow;
+
+// TODO: replace with IdentityOp
+class FlipGradientOp : public OpKernel {
+ public:
+  explicit FlipGradientOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    if (IsRefType(context->input_dtype(0))) {
+      context->forward_ref_input_to_ref_output(0, 0);
+    } else {
+      context->set_output(0, context->input(0));
+    }
+  }
+
+  bool IsExpensive() override { return false; }
+};
+
+REGISTER_OP("FlipGradient")
+    .Input("input: T")
+    .Output("output: T")
+    .Attr("T: type")
+    .Doc(R"Doc(
+Return a tensor with the same shape and contents as the input tensor or value,
+but flip gradients for gradient computation.
+)Doc");
+
+REGISTER_KERNEL_BUILDER(Name("FlipGradient").Device(DEVICE_CPU), FlipGradientOp);
+// REGISTER_KERNEL_BUILDER(Name("FlipGradient").Device(DEVICE_CPU), IdentityOp);
