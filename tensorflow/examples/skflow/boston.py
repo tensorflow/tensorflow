06a0062f88de018963bf9a89bed0edfdefb78c85
A. Unique TensorFlower <gardener@tensorflow.org>
2016-06-13 17:33:27 -0800

    Added a dropout layer to the iris custom model, as well as some tests and more docstrings for example boston, iris, and iris_custom_model. Change: 124794201

--- tensorflow/examples/skflow/boston.py
+++ tensorflow/examples/skflow/boston.py
@@ -11,12 +11,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Example of DNNRegressor for Housing dataset."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from sklearn import cross_validation, metrics
+from sklearn import datasets, cross_validation, metrics
 from sklearn import preprocessing
 import tensorflow as tf
 from tensorflow.contrib import learn
@@ -24,7 +22,7 @@ from tensorflow.contrib import learn
 
 def main(unused_argv):
   # Load dataset
-  boston = learn.datasets.load_dataset('boston')
+  boston = datasets.load_boston()
   x, y = boston.data, boston.target
 
   # Split dataset into train / test
