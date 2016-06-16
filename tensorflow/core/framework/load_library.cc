6a22672fa32ed170f4c7b2c9cc5b1c96fd5a3691
Derek Murray <mrry@google.com>
2016-06-16 10:35:11 -0800

    Remove UNIX-specific include from platform-independent code. Change: 125083184

--- tensorflow/core/framework/load_library.cc
+++ tensorflow/core/framework/load_library.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <dlfcn.h>
 #include <memory>
 
 #include "tensorflow/core/framework/op.h"
