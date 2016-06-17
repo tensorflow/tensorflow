ac7e6f38666240350e51b2d8d44a6c2a201c8866
Shanqing Cai <cais@google.com>
2016-06-17 09:34:08 -0800

    Python3 fix in monitors_test Change: 125178681

--- tensorflow/contrib/learn/python/learn/tests/monitors_test.py
+++ tensorflow/contrib/learn/python/learn/tests/monitors_test.py
@@ -20,6 +20,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 from tensorflow.contrib import testing
