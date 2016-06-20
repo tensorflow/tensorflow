e199301e2d1df78d2be2aa9930286c9d2ab97df5
A. Unique TensorFlower <gardener@tensorflow.org>
2016-06-20 12:56:04 -0800

    Add shape functions in math_ops for unchanged shapes and segment reduction shapes.
    Also restored math_ops_test and shape_inference_testutil_test in BUILD file
    that were dropped from original change somehow.
    Change: 125376193

--- tensorflow/core/ops/math_ops_test.cc
+++ tensorflow/core/ops/math_ops_test.cc
@@ -41,4 +41,84 @@ TEST(MathOpsTest, AddN_ShapeFn) {
               "AddN", "?;[1,2];?;[1,2,3]");
 }
 
+TEST(MathOpsTest, UnchangedShape_ShapeFn) {
+  INFER_OK("Cast", "?", "in0");
+  INFER_OK("Cast", "[?]", "in0");
+  INFER_OK("Cast", "[1,?,3,4]", "in0");
+}
+
+TEST(MathOpsTest, FFT) {
+  for (const auto* op : {"FFT", "IFFT"}) {
+    INFER_OK(op, "?", "[?]");
+    INFER_ERROR("Shape must be rank 1 but is rank 2", op, "[1,2]");
+    INFER_OK(op, "[?]", "in0");
+    INFER_OK(op, "[1]", "in0");
+  }
+
+  for (const auto* op : {"FFT2D", "IFFT2D"}) {
+    INFER_OK(op, "?", "[?,?]");
+    INFER_ERROR("Shape must be rank 2 but is rank 1", op, "[1]");
+    INFER_OK(op, "[?,1]", "in0");
+    INFER_OK(op, "[1,2]", "in0");
+  }
+
+  for (const auto* op : {"FFT3D", "IFFT3D"}) {
+    INFER_OK(op, "?", "[?,?,?]");
+    INFER_ERROR("Shape must be rank 3 but is rank 2", op, "[1,2]");
+    INFER_OK(op, "[?,1,?]", "in0");
+    INFER_OK(op, "[1,2,3]", "in0");
+  }
+
+  for (const auto* op : {"BatchFFT", "BatchIFFT"}) {
+    INFER_OK(op, "?", "?");
+    INFER_ERROR("Shape must be at least rank 1 but is rank 0", op, "[]");
+    INFER_OK(op, "[?]", "in0");
+    INFER_OK(op, "[1]", "in0");
+    INFER_OK(op, "[1,2,3,4,5,6,7]", "in0");
+  }
+
+  for (const auto* op : {"BatchFFT2D", "BatchIFFT2D"}) {
+    INFER_OK(op, "?", "?");
+    INFER_ERROR("Shape must be at least rank 2 but is rank 1", op, "[1]");
+    INFER_OK(op, "[?,1]", "in0");
+    INFER_OK(op, "[1,2]", "in0");
+    INFER_OK(op, "[1,2,3,4,5,6,7]", "in0");
+  }
+
+  for (const auto* op : {"BatchFFT3D", "BatchIFFT3D"}) {
+    INFER_OK(op, "?", "?");
+    INFER_ERROR("Shape must be at least rank 3 but is rank 2", op, "[1,2]");
+    INFER_OK(op, "[?,1,?]", "in0");
+    INFER_OK(op, "[1,2,3]", "in0");
+    INFER_OK(op, "[1,2,3,4,5,6,7]", "in0");
+  }
+}
+
+TEST(MathOpsTest, Segment) {
+  // Tests SegmentReductionShapeFn.
+  for (const auto* op : {"SegmentMax", "SegmentMean", "SegmentMin",
+                         "SegmentProd", "SegmentSum"}) {
+    INFER_OK(op, "?;?", "?");
+    INFER_OK(op, "?;[100]", "?");
+
+    // Data shape with single dimension.
+    INFER_OK(op, "[?];?", "[?]");
+    INFER_OK(op, "[?];[100]", "[?]");
+    INFER_OK(op, "[1];?", "[?]");
+    INFER_OK(op, "[1];[100]", "[?]");
+
+    // Data shape with multiple dimensions.
+    INFER_OK(op, "[?,?];?", "[?,d0_1]");
+    INFER_OK(op, "[?,2];[100]", "[?,d0_1]");
+    INFER_OK(op, "[?,2,?,4];[100]", "[?,d0_1,d0_2,d0_3]");
+    INFER_OK(op, "[1,?];?", "[?,d0_1]");
+    INFER_OK(op, "[1,2];[100]", "[?,d0_1]");
+    INFER_OK(op, "[1,2,?,4];[100]", "[?,d0_1,d0_2,d0_3]");
+
+    // Error cases.
+    INFER_ERROR("Shape must be rank 1 but is rank 2", op, "?;[1,2]");
+    INFER_ERROR("Shape must have rank >= 1, but is 0", op, "[];[1]");
+  }
+}
+
 }  // end namespace tensorflow
