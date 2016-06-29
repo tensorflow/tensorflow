bdb6e00b08b5023825eb9acd0e4e4fd1cbc6d01e
Adria Puigdomenech <adriap@google.com>
2016-06-29 06:50:54 -0800

    Support nested inputs and outputs in rnn containers. Change: 126191028

--- tensorflow/python/kernel_tests/rnn_test.py
+++ tensorflow/python/kernel_tests/rnn_test.py
@@ -78,6 +78,26 @@ class DummyMultiDimensionalLSTM(tf.nn.rnn_cell.RNNCell):
     return (input_ + 1, (h + 1, c + 1))
 
 
+class NestedRNNCell(tf.nn.rnn_cell.RNNCell):
+  """RNN Cell generating (output, new_state) = (input + 1, state + 1).
+
+  The input, output and state of this cell is a tuple of two tensors.
+  """
+
+  @property
+  def output_size(self):
+    return (5, 5)
+
+  @property
+  def state_size(self):
+    return (6, 6)
+
+  def __call__(self, input_, state, scope=None):
+    h, c = state
+    x, y = input_
+    return ((x + 1, y + 1), (h + 1, c + 1))
+
+
 class TestStateSaver(object):
 
   def __init__(self, batch_size, state_size):
@@ -1228,11 +1248,108 @@ class MultiDimensionalLSTMTest(tf.test.TestCase):
       state_dynamic_v = sess.run(
           state_dynamic, feed_dict={inputs[0]: input_value})
       state_bid_fw_v = sess.run(
-          state_bid_fw, feed_dict={inputs[0]: input_value})
+          state_bid_fw, feed_dict={inputs_using_dim[0]: input_value})
       state_bid_bw_v = sess.run(
-          state_bid_bw, feed_dict={inputs[0]: input_value})
+          state_bid_bw, feed_dict={inputs_using_dim[0]: input_value})
       state_sav_v = sess.run(
-          state_sav, feed_dict={inputs[0]: input_value})
+          state_sav, feed_dict={inputs_using_dim[0]: input_value})
+      self.assertAllEqual(
+          np.hstack(state_static_v), np.hstack(state_dynamic_v))
+      self.assertAllEqual(
+          np.hstack(state_static_v), np.hstack(state_sav_v))
+      self.assertAllEqual(
+          np.hstack(state_static_v), np.hstack(state_bid_fw_v))
+      self.assertAllEqual(
+          np.hstack(state_static_v), np.hstack(state_bid_bw_v))
+
+
+class NestedLSTMTest(tf.test.TestCase):
+
+  def setUp(self):
+    self._seed = 23489
+    np.random.seed(self._seed)
+
+  def testNestedIOLSTMAllRNNContainers(self):
+    input_size = 5
+    batch_size = 2
+    state_size = 6
+    max_length = 8
+    sequence_length = [4, 6]
+    with self.test_session(graph=tf.Graph()) as sess:
+      state_saver = TestStateSaver(batch_size, state_size)
+      single_input = (tf.placeholder(tf.float32, shape=(None, input_size)),
+                      tf.placeholder(tf.float32, shape=(None, input_size)))
+      inputs = max_length * [single_input]
+      inputs_c = (tf.pack([input_[0] for input_ in inputs]),
+                  tf.pack([input_[1] for input_ in inputs]))
+      single_input_using_dim = (
+          tf.placeholder(tf.float32, shape=(batch_size, input_size)),
+          tf.placeholder(tf.float32, shape=(batch_size, input_size)))
+      inputs_using_dim = max_length * [single_input_using_dim]
+
+      # Create a cell for the whole test. This is fine because the cell has no
+      # variables.
+      cell = NestedRNNCell()
+      outputs_dynamic, state_dynamic = tf.nn.dynamic_rnn(
+          cell, inputs_c, dtype=tf.float32, time_major=True,
+          sequence_length=sequence_length)
+      outputs_static, state_static = tf.nn.rnn(
+          cell, inputs, dtype=tf.float32,
+          sequence_length=sequence_length)
+      outputs_bid, state_bid_fw, state_bid_bw = tf.nn.bidirectional_rnn(
+          cell, cell, inputs_using_dim, dtype=tf.float32,
+          sequence_length=sequence_length)
+      outputs_sav, state_sav = tf.nn.state_saving_rnn(
+          cell, inputs_using_dim, sequence_length=sequence_length,
+          state_saver=state_saver, state_name=("h", "c"))
+
+      def _assert_same_shape(input1, input2, double=False):
+        flat_input1 = nest.flatten(input1)
+        flat_input2 = nest.flatten(input2)
+        for inp1, inp2 in zip(flat_input1, flat_input2):
+          input_shape = inp1.get_shape().as_list()
+          if double:
+            input_shape[1] *= 2
+          self.assertEqual(input_shape, inp2.get_shape().as_list())
+
+      _assert_same_shape(inputs_c, outputs_dynamic)
+      _assert_same_shape(inputs, outputs_static)
+      _assert_same_shape(inputs_using_dim, outputs_sav)
+      _assert_same_shape(inputs_using_dim, outputs_bid, double=True)
+
+      tf.initialize_all_variables().run()
+
+      input_total_size = (batch_size, input_size)
+      input_value = (np.random.randn(*input_total_size),
+                     np.random.randn(*input_total_size))
+      outputs_dynamic_v = sess.run(
+          outputs_dynamic, feed_dict={single_input: input_value})
+      outputs_static_v = sess.run(
+          outputs_static, feed_dict={single_input: input_value})
+      outputs_sav_v = sess.run(
+          outputs_sav, feed_dict={single_input_using_dim: input_value})
+      outputs_bid_v = sess.run(
+          outputs_bid, feed_dict={single_input_using_dim: input_value})
+
+      self.assertAllEqual(outputs_static_v,
+                          np.transpose(outputs_dynamic_v, (1, 0, 2, 3)))
+      self.assertAllEqual(outputs_static_v, outputs_sav_v)
+      outputs_static_array = np.array(outputs_static_v)
+      outputs_static_array_double = np.concatenate(
+          (outputs_static_array, outputs_static_array), axis=3)
+      outputs_bid_array = np.array(outputs_bid_v)
+      self.assertAllEqual(outputs_static_array_double, outputs_bid_array)
+
+      state_dynamic_v = sess.run(
+          state_dynamic, feed_dict={single_input: input_value})
+      state_static_v = sess.run(
+          state_static, feed_dict={single_input: input_value})
+      state_bid_fw_v = sess.run(
+          state_bid_fw, feed_dict={single_input_using_dim: input_value})
+      state_bid_bw_v = sess.run(
+          state_bid_bw, feed_dict={single_input_using_dim: input_value})
+      state_sav_v = sess.run(
+          state_sav, feed_dict={single_input_using_dim: input_value})
       self.assertAllEqual(
           np.hstack(state_static_v), np.hstack(state_dynamic_v))
       self.assertAllEqual(
