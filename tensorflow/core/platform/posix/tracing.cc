d9cfbd63aefbeb48200d65841a401291b572d35b
Derek Murray <mrry@google.com>
2016-06-30 16:44:14 -0800

    Move POSIX-dependent tracing function from platform/default -> platform/posix. Change: 126364522

--- tensorflow/core/platform/posix/tracing.cc
+++ tensorflow/core/platform/posix/tracing.cc
@@ -1,4 +1,4 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,21 +15,26 @@ limitations under the License.
 
 #include "tensorflow/core/platform/tracing.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+
 namespace tensorflow {
 namespace port {
 
-void Tracing::RegisterEvent(EventCategory id, const char* name) {
-  // TODO(opensource): implement
+static bool TryGetEnv(const char* name, const char** value) {
+  *value = getenv(name);
+  return *value != nullptr && (*value)[0] != '\0';
 }
 
-void Tracing::Initialize() {}
-
-static bool DoInit() {
-  Tracing::Initialize();
-  return true;
+const char* Tracing::LogDir() {
+  const char* dir;
+  if (TryGetEnv("TEST_TMPDIR", &dir)) return dir;
+  if (TryGetEnv("TMP", &dir)) return dir;
+  if (TryGetEnv("TMPDIR", &dir)) return dir;
+  dir = "/tmp";
+  if (access(dir, R_OK | W_OK | X_OK) == 0) return dir;
+  return ".";  // Default to current directory.
 }
-
-static const bool dummy = DoInit();
 
 }  // namespace port
 }  // namespace tensorflow
