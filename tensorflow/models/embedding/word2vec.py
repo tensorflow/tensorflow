42c042cb6dee91b16b17496a2ce826e964cce5ca
raix852 <raix852@gmail.com>
2016-07-10 22:20:35 +0800

    Fix word2vec excepction when words contain unicode.

--- tensorflow/models/embedding/word2vec.py
+++ tensorflow/models/embedding/word2vec.py
@@ -378,7 +378,7 @@ class Word2Vec(object):
     opts = self._options
     with open(os.path.join(opts.save_path, "vocab.txt"), "w") as f:
       for i in xrange(opts.vocab_size):
-        f.write("%s %d\n" % (tf.compat.as_text(opts.vocab_words[i]),
+        f.write("%s %d\n" % (tf.compat.as_text(opts.vocab_words[i]).encode('utf-8'),
                              opts.vocab_counts[i]))
 
   def _train_thread_body(self):
