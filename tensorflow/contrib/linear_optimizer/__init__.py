ecc874be2739b9c901f0e452065f2a5719524605
A. Unique TensorFlower <gardener@tensorflow.org>
2016-07-13 15:23:30 -0800

    Move SDCAOptimizer from contrib/learn to contrib/linear_optimizer Change: 127373128

--- tensorflow/contrib/linear_optimizer/__init__.py
+++ tensorflow/contrib/linear_optimizer/__init__.py
@@ -23,3 +23,4 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.contrib.linear_optimizer.python.ops.sdca_ops import SdcaModel
+from tensorflow.contrib.linear_optimizer.python.sdca_optimizer import SDCAOptimizer
