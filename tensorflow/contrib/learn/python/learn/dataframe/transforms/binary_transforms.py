366fbf19779f6871c2f0b74efd73d2a4032564c7
A. Unique TensorFlower <gardener@tensorflow.org>
2016-07-28 09:21:49 -0800

    Generalize comparison transforms to all elementwise binary transforms. Add division, "true" division, floor division, multiplication and mod.
    Add Difference (subtraction) transform.
    Change: 128712913

--- tensorflow/contrib/learn/python/learn/dataframe/transforms/binary_transforms.py
+++ tensorflow/contrib/learn/python/learn/dataframe/transforms/binary_transforms.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-"""Transforms for comparing pairs of `Series`."""
+"""Transforms that wrap binary TensorFlow operations."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -27,25 +26,24 @@ from tensorflow.python.ops import math_ops
 # Each entry is a mapping from registered_name to operation. Each operation is
 # wrapped in a transform and then registered as a member function
 # `Series`.registered_name().
-COMPARISON_TRANSFORMS = [("__eq__", math_ops.equal),
-                         ("__gt__", math_ops.greater),
-                         ("__ge__", math_ops.greater_equal),
-                         ("__lt__", math_ops.less),
-                         ("__le__", math_ops.less_equal)]
+BINARY_TRANSFORMS = [("__eq__", math_ops.equal),
+                     ("__gt__", math_ops.greater),
+                     ("__ge__", math_ops.greater_equal),
+                     ("__lt__", math_ops.less),
+                     ("__le__", math_ops.less_equal),
+                     ("__mul__", math_ops.mul),
+                     ("__div__", math_ops.div),
+                     ("__truediv__", math_ops.truediv),
+                     ("__floordiv__", math_ops.floordiv),
+                     ("__mod__", math_ops.mod),
+                     ("pow", math_ops.pow)]
 
-SERIES_DOC_FORMAT_STRING = (
-    "A `Transform` that uses `{0}` to compare two Series. "
-    "Documentation for `{0}`: \n\n {1}"
-)
-
-SCALAR_DOC_FORMAT_STRING = (
-    "A `Transform` that uses `{0}` to compare a Series and a scalar. "
-    "Documentation for `{0}`: \n\n {1}"
-)
+_DOC_FORMAT_STRING = ("A `Transform` that wraps `{0}`. "
+                      "Documentation for `{0}`: \n\n {1}")
 
 
-class SeriesComparisonTransform(transform.Transform):
-  """Parent class for `Transform`s that compare `Series` elementwise."""
+class SeriesBinaryTransform(transform.Transform):
+  """Parent class for `Transform`s that operate on two `Series`."""
 
   @property
   def input_valency(self):
@@ -56,31 +54,30 @@ class SeriesComparisonTransform(transform.Transform):
     return "output",
 
   def _apply_transform(self, input_tensors):
-    # TODO(jamieas): consider supporting sparse comparisons.
+    # TODO(jamieas): consider supporting sparse inputs.
     if isinstance(input_tensors[0], ops.SparseTensor) or isinstance(
         input_tensors[1], ops.SparseTensor):
-      raise TypeError("{} does not support SparseTensors".format(type(
-          self).__name__))
+      raise TypeError("{} does not support SparseTensors".format(
+          type(self).__name__))
 
     # pylint: disable=not-callable
-    return self.return_type(self._compare(input_tensors[0], input_tensors[1]))
+    return self.return_type(self._apply_op(input_tensors[0], input_tensors[1]))
 
 
-class ScalarComparisonTransform(transform.Transform):
-  """Parent class for `Transform`s that compare `Series` to a scalar."""
+class ScalarBinaryTransform(transform.Transform):
+  """Parent class for `Transform`s that combine `Series` to a scalar."""
 
-  def __init__(self, threshold):
-    if isinstance(threshold, series.Series):
-      raise ValueError(
-          "{} is used to compare Series with scalars. It was called with "
-          "another Series.".format(
-              type(self).__name__))
-    super(ScalarComparisonTransform, self).__init__()
-    self._threshold = threshold
+  def __init__(self, scalar):
+    if isinstance(scalar, series.Series):
+      raise ValueError("{} takes a Series and a scalar. "
+                       "It was called with another Series.".format(
+                           type(self).__name__))
+    super(ScalarBinaryTransform, self).__init__()
+    self._scalar = scalar
 
   @transform.parameter
-  def threshold(self):
-    return self._threshold
+  def scalar(self):
+    return self._scalar
 
   @property
   def input_valency(self):
@@ -94,63 +91,62 @@ class ScalarComparisonTransform(transform.Transform):
     input_tensor = input_tensors[0]
     if isinstance(input_tensor, ops.SparseTensor):
       result = ops.SparseTensor(input_tensor.indices,
-                                self._compare(input_tensor.values),
+                                self._apply_op(input_tensor.values),
                                 input_tensor.shape)
     else:
-      result = self._compare(input_tensor)
+      result = self._apply_op(input_tensor)
 
     # pylint: disable=not-callable
     return self.return_type(result)
 
 
 # pylint: disable=unused-argument
-def register_comparison_ops(method_name, operation):
-  """Registers `Series` member functions for comparisons.
+def register_binary_op(method_name, operation):
+  """Registers `Series` member functions for binary operations.
 
   Args:
     method_name: the name of the method that will be created in `Series`.
-    operation: TensorFlow operation used for comparison.
+    operation: underlying TensorFlow operation.
   """
 
-  # Define series-series comparison `Transform`.
+  # Define series-series `Transform`.
   @property
   def series_name(self):
     return operation.__name__
 
-  series_doc = SERIES_DOC_FORMAT_STRING.format(operation.__name__,
-                                               operation.__doc__)
-  def series_compare(self, x, y):
+  series_doc = _DOC_FORMAT_STRING.format(operation.__name__, operation.__doc__)
+
+  def series_apply_op(self, x, y):
     return operation(x, y)
 
   series_transform_cls = type("scalar_{}".format(operation.__name__),
-                              (SeriesComparisonTransform,),
+                              (SeriesBinaryTransform,),
                               {"name": series_name,
                                "__doc__": series_doc,
-                               "_compare": series_compare})
+                               "_apply_op": series_apply_op})
 
-  # Define series-scalar comparison `Transform`.
+  # Define series-scalar `Transform`.
   @property
   def scalar_name(self):
     return "scalar_{}".format(operation.__name__)
 
-  scalar_doc = SCALAR_DOC_FORMAT_STRING.format(operation.__name__,
-                                               operation.__doc__)
+  scalar_doc = _DOC_FORMAT_STRING.format(operation.__name__, operation.__doc__)
 
-  def scalar_compare(self, x):
-    return operation(x, self.threshold)
+  def scalar_apply_op(self, x):
+    return operation(x, self.scalar)
 
   scalar_transform_cls = type("scalar_{}".format(operation.__name__),
-                              (ScalarComparisonTransform,),
+                              (ScalarBinaryTransform,),
                               {"name": scalar_name,
                                "__doc__": scalar_doc,
-                               "_compare": scalar_compare})
+                               "_apply_op": scalar_apply_op})
 
   # Define function that delegates to the two `Transforms`.
-  def _comparison_fn(self, other, *args, **kwargs):
+  def _fn(self, other, *args, **kwargs):
     # pylint: disable=not-callable,abstract-class-instantiated
     if isinstance(other, series.Series):
       return series_transform_cls(*args, **kwargs)([self, other])[0]
     return scalar_transform_cls(other, *args, **kwargs)([self])[0]
 
   # Register new member function of `Series`.
-  setattr(series.Series, method_name, _comparison_fn)
+  setattr(series.Series, method_name, _fn)
