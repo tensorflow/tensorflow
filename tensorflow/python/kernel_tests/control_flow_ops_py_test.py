cf8d866b3f2f76b349432ac197a62ca11dac6ceb
Yuan Yu <yuanbyu@google.com>
2016-08-01 08:33:31 -0800

    Fix a bug in distributed execution of while loop.
    When a while loop is partitioned on multiple devices, control edges are added to control the recv nodes. There are two partitioning phases, one at the worker level and the other at the device level within a worker. We didn't handle properly when an op has two remote inputs, one on a different worker and the other on a different device of the same worker.
    Change: 128993033

--- tensorflow/python/kernel_tests/control_flow_ops_py_test.py
+++ tensorflow/python/kernel_tests/control_flow_ops_py_test.py
@@ -716,10 +716,11 @@ class ControlFlowTest(tf.test.TestCase):
   def testWhileWithControl_3(self):
     with self.test_session() as sess:
       b = tf.placeholder(tf.bool)
-      c = tf.constant(0)
+      c = tf.constant(1)
+      x0 = tf.constant(0)
       with tf.control_dependencies([b]):
-        c = tf.while_loop(lambda x: x < 10, lambda x: x + 1, [c])
-      self.assertEqual(10, sess.run(c, {b: True}))
+        r = tf.while_loop(lambda x: x < 10, lambda x: x + c, [x0])
+      self.assertEqual(10, sess.run(r, {b: True}))
 
   def testWhileWithControl_4(self):
     with self.test_session() as sess:
