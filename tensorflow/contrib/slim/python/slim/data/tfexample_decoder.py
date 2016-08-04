f261f1a572140296a1dbed2f01e1271d7505e97f
A. Unique TensorFlower <gardener@tensorflow.org>
2016-08-03 16:33:21 -0800

    Allow Tensor decoder to take multiple shape_keys for reshaping.
    If shape_keys is a list of N keys, decoder will reshape the Tensor to [k1, k2,
    .., kN]. If shape_keys is a single key, then the decoded tensor is used as
    shape.
    Change: 129279506

--- tensorflow/contrib/slim/python/slim/data/tfexample_decoder.py
+++ tensorflow/contrib/slim/python/slim/data/tfexample_decoder.py
@@ -99,47 +99,55 @@ class ItemHandlerCallback(ItemHandler):
 class Tensor(ItemHandler):
   """An ItemHandler that returns a parsed Tensor."""
 
-  def __init__(self, tensor_key, shape_key=None, shape=None, default_value=0):
+  def __init__(self, tensor_key, shape_keys=None, shape=None, default_value=0):
     """Initializes the Tensor handler.
 
     Tensors are, by default, returned without any reshaping. However, there are
-    two mechanisms which allow reshaping to occur at load time. If `shape_key`
-    is provided, both the `Tensor` corresponding to `tensor_key` and `shape_key`
-    is loaded and the former `Tensor` is reshaped with the values of the latter.
-    Alternatively, if a fixed `shape` is provided, the `Tensor` corresponding to
-    `tensor_key` is loaded and reshape appropriately. If neither `shape_key` nor
-    `shape` are provided, the `Tensor` will be returned without any reshaping.
+    two mechanisms which allow reshaping to occur at load time. If `shape_keys`
+    is provided, both the `Tensor` corresponding to `tensor_key` and
+    `shape_keys` is loaded and the former `Tensor` is reshaped with the values
+    of the latter. Alternatively, if a fixed `shape` is provided, the `Tensor`
+    corresponding to `tensor_key` is loaded and reshape appropriately.
+    If neither `shape_keys` nor `shape` are provided, the `Tensor` will be
+    returned without any reshaping.
 
     Args:
       tensor_key: the name of the `TFExample` feature to read the tensor from.
-      shape_key: Optional name of the TF-Example feature in which the tensor
-        shape is stored.
+      shape_keys: Optional name or list of names of the TF-Example feature in
+        which the tensor shape is stored. If a list, then each corresponds to
+        one dimension of the shape.
       shape: Optional output shape of the `Tensor`. If provided, the `Tensor` is
         reshaped accordingly.
       default_value: The value used when the `tensor_key` is not found in a
         particular `TFExample`.
 
     Raises:
-      ValueError: if both `shape_key` and `shape` are specified.
+      ValueError: if both `shape_keys` and `shape` are specified.
     """
-    if shape_key and shape is not None:
-      raise ValueError('Cannot specify both shape_key and shape parameters.')
+    if shape_keys and shape is not None:
+      raise ValueError('Cannot specify both shape_keys and shape parameters.')
+    if shape_keys and not isinstance(shape_keys, list):
+      shape_keys = [shape_keys]
     self._tensor_key = tensor_key
-    self._shape_key = shape_key
+    self._shape_keys = shape_keys
     self._shape = shape
     self._default_value = default_value
     keys = [tensor_key]
-    if shape_key:
-      keys.append(shape_key)
+    if shape_keys:
+      keys.extend(shape_keys)
     super(Tensor, self).__init__(keys)
 
   def tensors_to_item(self, keys_to_tensors):
     tensor = keys_to_tensors[self._tensor_key]
     shape = self._shape
-    if self._shape_key:
-      shape = keys_to_tensors[self._shape_key]
-      if isinstance(shape, ops.SparseTensor):
-        shape = sparse_ops.sparse_tensor_to_dense(shape)
+    if self._shape_keys:
+      shape_dims = []
+      for k in self._shape_keys:
+        shape_dim = keys_to_tensors[k]
+        if isinstance(shape_dim, ops.SparseTensor):
+          shape_dim = sparse_ops.sparse_tensor_to_dense(shape_dim)
+        shape_dims.append(shape_dim)
+      shape = array_ops.squeeze(array_ops.pack(shape_dims))
     if isinstance(tensor, ops.SparseTensor):
       if shape is not None:
         tensor = sparse_ops.sparse_reshape(tensor, shape)
