d2589654ddc7bffc2f51eb5bebba2eb78f48a9a2
Eugene Brevdo <ebrevdo@google.com>
2016-08-04 16:32:31 -0800

    Fix tf.Example parsing when the Example feature name is provided, but no value exists.  In this case, if no default is given then the error should be correct.  If a default *is* provided, it should be used.
    Also, reformatted the parsing_ops_test.py file (which doesn't lose history,
    since it hasn't changed much since my original version).
    Change: 129393762

--- tensorflow/python/framework/test_util.py
+++ tensorflow/python/framework/test_util.py
@@ -502,23 +502,26 @@ class TensorFlowTestCase(googletest.TestCase):
                                      expected_err_re_or_predicate):
     """Returns a context manager to enclose code expected to raise an exception.
 
+    If the exception is an OpError, the op stack is also included in the message
+    predicate search.
+
     Args:
       exception_type: The expected type of exception that should be raised.
       expected_err_re_or_predicate: If this is callable, it should be a function
-        of one argument that inspects the passed-in OpError exception and
+        of one argument that inspects the passed-in exception and
         returns True (success) or False (please fail the test). Otherwise, the
         error message is expected to match this regular expression partially.
 
     Returns:
       A context manager to surround code that is expected to raise an
-      errors.OpError exception.
+      exception.
     """
     if callable(expected_err_re_or_predicate):
       predicate = expected_err_re_or_predicate
     else:
       def predicate(e):
-        err_str = e.message
-        op = e.op
+        err_str = e.message if isinstance(e, errors.OpError) else str(e)
+        op = e.op if isinstance(e, errors.OpError) else None
         while op is not None:
           err_str += "\nCaused by: " + op.name
           op = op._original_op
