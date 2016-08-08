e28e3ba54c6c2776bfc1444d650a1c0a901d061f
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2016-08-08 16:36:11 +0900

    Added code to raise ValueError ifparent directory of  path Saver.save() should use doesn't exist. This give user a much more readable error message than previous NotFoundError
    Added code to raise ValueError ifparent directory of  path Saver.save() should use doesn't exist. This give user a much more readable error message than previous NotFoundError
    
    Added some comments
    
    Removed cached file

--- tensorflow/python/training/saver.py
+++ tensorflow/python/training/saver.py
@@ -1070,6 +1070,10 @@ class Saver(object):
     if not isinstance(sess, session.SessionInterface):
       raise TypeError("'sess' must be a Session; %s" % sess)
 
+    # Note a few lines above save_path was set to os.path.dirname(save_path)
+    if not os.path.exists(save_path):
+      raise ValueError("Parent directory {} doesn't exist, can't save.".format(save_path))
+
     model_checkpoint_path = sess.run(
         self.saver_def.save_tensor_name,
         {self.saver_def.filename_tensor_name: checkpoint_file})
