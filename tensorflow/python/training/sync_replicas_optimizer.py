416faf742b9fe9ca0956e40104dadbc8070a46ca
Zongheng Yang <zongheng@google.com>
2016-08-09 10:07:54 -0800

    Typo fix in sync_replicas_optimizer.py. Change: 129769850

--- tensorflow/python/training/sync_replicas_optimizer.py
+++ tensorflow/python/training/sync_replicas_optimizer.py
@@ -118,7 +118,7 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
   chief_queue_runner and get_init_tokens_op generated from this optimizer.
 
   ```python
-  # After the session is created by the superviser and before the main while
+  # After the session is created by the Supervisor and before the main while
   # loop:
   if is_chief and FLAGS.sync_replicas:
     sv.start_queue_runners(sess, [chief_queue_runner])
