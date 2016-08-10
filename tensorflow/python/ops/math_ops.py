fddaa6b035492776f761e16003685b45ee9e6c60
A. Unique TensorFlower <gardener@tensorflow.org>
2016-08-10 10:11:54 -0800

    Move _BroadcastShape to a separate function in tensor_shape. Change: 129887373

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -218,7 +218,6 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-import six.moves
 
 from tensorflow.python.framework import common_shapes
 from tensorflow.python.framework import constant_op
@@ -1763,47 +1762,9 @@ ops.RegisterShape("Cumprod")(common_shapes.unchanged_shape)
 @ops.RegisterShape("SquaredDifference")
 def _BroadcastShape(op):
   """Common shape function for binary operators that broadcast their inputs."""
-  shape_x = op.inputs[0].get_shape()
-  shape_y = op.inputs[1].get_shape()
-  if shape_x.ndims is None or shape_y.ndims is None:
-    return [tensor_shape.unknown_shape()]
-
-  # To compute the broadcasted dimensions, we zip together shape_x and shape_y,
-  # and pad with 1 to make them the same length.
-  broadcasted_dims = reversed(list(six.moves.zip_longest(
-      reversed(shape_x.dims),
-      reversed(shape_y.dims),
-      fillvalue=tensor_shape.Dimension(1))))
-  # Next we combine the dimensions according to the numpy broadcasting rules.
-  # http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
-  return_dims = []
-  for (dim_x, dim_y) in broadcasted_dims:
-    if dim_x.value is None or dim_y.value is None:
-      # One or both dimensions is unknown. If either dimension is greater than
-      # 1, we assume that the program is correct, and the other dimension will
-      # be broadcast to match it.
-      # TODO(mrry): If we eliminate the shape checks in C++, we must still
-      # assert that the unknown dim is either 1 or the same as the known dim.
-      if dim_x.value is not None and dim_x.value > 1:
-        return_dims.append(dim_x)
-      elif dim_y.value is not None and dim_y.value > 1:
-        return_dims.append(dim_y)
-      else:
-        return_dims.append(None)
-    elif dim_x.value == 1:
-      # We will broadcast dim_x to dim_y.
-      return_dims.append(dim_y)
-    elif dim_y.value == 1:
-      # We will broadcast dim_y to dim_x.
-      return_dims.append(dim_x)
-    elif dim_x.value == dim_y.value:
-      # The dimensions are compatible, so output is the same size in that
-      # dimension.
-      return_dims.append(dim_x.merge_with(dim_y))
-    else:
-      raise ValueError("Incompatible shapes for broadcasting: %s and %s"
-                       % (shape_x, shape_y))
-  return [tensor_shape.TensorShape(return_dims)]
+  return [common_shapes.broadcast_shape(
+      op.inputs[0].get_shape(),
+      op.inputs[1].get_shape())]
 
 
 @ops.RegisterShape("SparseDenseCwiseMul")
