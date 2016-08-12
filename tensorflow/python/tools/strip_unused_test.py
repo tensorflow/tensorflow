1c8a2b0e5f5c1ac22d8db8526f48ea3113d49486
Daniel Castro Chin <dcastro@google.com>
2016-08-11 18:03:12 -0700

    Minor comment change.

--- tensorflow/python/tools/strip_unused_test.py
+++ tensorflow/python/tools/strip_unused_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests the graph freezing tool."""
+"""Tests the node stripping tool."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
