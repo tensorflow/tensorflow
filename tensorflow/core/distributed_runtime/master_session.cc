1ef8d7444dab59b627daf508bd4d7fd8963dc33a
Derek Murray <derek.murray@gmail.com>
2016-08-12 09:49:36 -0700

    Remove accidentally-added dead code

--- tensorflow/core/distributed_runtime/master_session.cc
+++ tensorflow/core/distributed_runtime/master_session.cc
@@ -607,37 +607,6 @@ Status MasterSession::ReffedClientGraph::RunPartitions(
   return status;
 }
 
-class BlockingCounter {
- public:
-  BlockingCounter(int initial_count) : count_(initial_count) {
-    CHECK_GE(count_, 0);
-  }
-
-  ~BlockingCounter() {}
-
-  inline void DecrementCount() {
-    mutex_lock l(mu_);
-    --count_;
-    CHECK(count_ >= 0);
-    if (count_ == 0) {
-      cond_var_.notify_all();
-    }
-  }
-
-  inline void Wait() {
-    mutex_lock l(mu_);
-    while (count_ > 0) {
-      cond_var_.wait(l);
-    }
-  }
-
- private:
-  int count_;
-  mutex mu_;
-  condition_variable cond_var_;
-};
-
-
 namespace {
 
 class CleanupBroadcastHelper {
