eec92db0eae46f3241be8a7472555854b8fa3b17
Pieter de Rijk <pieter@de-rijk.com>
2016-08-21 12:01:26 +0200

    Modified the output message, so if FLAGS.host is set to 0.0.0.0 it will display the correct IP-address on the prompt

--- tensorflow/tensorboard/tensorboard.py
+++ tensorflow/tensorboard/tensorboard.py
@@ -139,9 +139,8 @@ def main(unused_argv=None):
   status_bar.SetupStatusBarInsideGoogle('TensorBoard %s' % tag, FLAGS.port)
   print('Starting TensorBoard %s on port %d' % (tag, FLAGS.port))
 
-  hostname = socket.gethostname()
   if FLAGS.host == "0.0.0.0":
-    print('(You can navigate to http://%s:%d)' % (socket.gethostname(), FLAGS.port))
+    print('(You can navigate to http://%s:%d)' % (socket.gethostbyname(socket.gethostname()), FLAGS.port))
   else:
     print('(You can navigate to http://%s:%d)' % (FLAGS.host, FLAGS.port))
 
