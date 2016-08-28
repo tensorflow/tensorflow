8af3cb915cd51149d0db10f86d8405aea4eeec8c
Jingtian Peng <pjt73651@gmail.com>
2016-08-28 11:49:24 +0800

    Fix a miss word in the comment

--- tensorflow/tools/dist_test/python/mnist_replica.py
+++ tensorflow/tools/dist_test/python/mnist_replica.py
@@ -58,7 +58,7 @@ flags.DEFINE_integer("task_index", None,
                      "initialization ")
 flags.DEFINE_integer("num_gpus", 1,
                      "Total number of gpus for each machine."
-                     "If you don't   GPU, please set it to '0'")
+                     "If you don't use GPU, please set it to '0'")
 flags.DEFINE_integer("replicas_to_aggregate", None,
                      "Number of replicas to aggregate before parameter update"
                      "is applied (For sync_replicas mode only; default: "
