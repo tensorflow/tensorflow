987128f2e1a908bb148386d0406f789c7bdb03e1
Martin Englund <martin@englund.nu>
2016-09-02 16:52:22 -0700

    clarify error message

--- tensorflow/examples/image_retraining/retrain.py
+++ tensorflow/examples/image_retraining/retrain.py
@@ -268,7 +268,7 @@ def get_image_path(image_lists, label_name, index, image_dir, category):
     tf.logging.fatal('Category does not exist %s.', category)
   category_list = label_lists[category]
   if not category_list:
-    tf.logging.fatal('Category %s has no images - %s.', label_name, category)
+    tf.logging.fatal('Label %s has no images in the category %s.', label_name, category)
   mod_index = index % len(category_list)
   base_name = category_list[mod_index]
   sub_dir = label_lists['dir']
