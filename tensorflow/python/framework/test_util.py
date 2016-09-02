Fix gradients of numerous complex ops
There were bugs in sqrt, rsqrt, log, div, and pow.  These have been fixed, and
tests have been extended to cover them.

There were also bugs in erf, erfc, lgamma, digamma, zeta, polygamma, tan, asin,
acos, and atan.  These ops don't have registered complex kernels, so the bugs
were benign at the moment but would turn into hard-to-find bugs if kernels were
registered.  I've fixed the Python gradients to use conj even though it's
always a nop for now.

tf.conj and tf.real are now nops for real tensors.
Change: 132028111

tensorflow/python/framework/test_util.py
@@ -432,7 +432,8 @@ class TensorFlowTestCase(googletest.TestCase):
       # absolute difference atol are added together to compare against
       # the absolute difference between a and b.  Here, we want to
       # print out which elements violate such conditions.
-      cond = np.abs(a - b) > atol + rtol * np.abs(b)
+      cond = np.logical_or(
+          np.abs(a - b) > atol + rtol * np.abs(b), np.isnan(a) != np.isnan(b))
       if a.ndim:
         x = a[np.where(cond)]
         y = b[np.where(cond)]
@@ -444,6 +445,7 @@ class TensorFlowTestCase(googletest.TestCase):
       print("not close rhs = ", y)
       print("not close dif = ", np.abs(x - y))
       print("not close tol = ", atol + rtol * np.abs(y))
+      print("dtype = %s, shape = %s" % (a.dtype, a.shape))
       np.testing.assert_allclose(a, b, rtol=rtol, atol=atol)
 
   def assertAllCloseAccordingToType(self, a, b, rtol=1e-6, atol=1e-6):

tensorflow/python/ops/math_grad.py
@@ -245,7 +245,7 @@ def _AbsGrad(op, grad):
 @ops.RegisterGradient("Neg")
 def _NegGrad(_, grad):
   """Returns -grad."""
-  return - grad
+  return -grad
 
 
 @ops.RegisterGradient("Inv")
@@ -260,8 +260,7 @@ def _SquareGrad(op, grad):
   x = op.inputs[0]
   # Added control dependencies to prevent 2*x from being computed too early.
   with ops.control_dependencies([grad.op]):
-    if x.dtype.is_complex:
-      x = math_ops.conj(x)
+    x = math_ops.conj(x)
     return grad * (2.0 * x)
 
 
@@ -274,9 +273,10 @@ def _SqrtGrad(op, grad):
 @ops.RegisterGradient("SqrtGrad")
 def _SqrtGradGrad(op, grad):
   a = op.inputs[0]
-  y = op.outputs[0]  # y = 0.5 * b / a
+  y = op.outputs[0]  # y = 0.5 * b / conj(a)
   with ops.control_dependencies([grad.op]):
-    return -grad * y / a, 0.5 * grad / a
+    ga = grad / a
+    return -math_ops.conj(ga) * y, 0.5 * ga
 
 
 @ops.RegisterGradient("Rsqrt")
@@ -290,8 +290,7 @@ def _ExpGrad(op, grad):
   """Returns grad * exp(x)."""
   y = op.outputs[0]  # y = e^x
   with ops.control_dependencies([grad.op]):
-    if y.dtype.is_complex:
-      y = math_ops.conj(y)
+    y = math_ops.conj(y)
     return grad * y
 
 
@@ -300,6 +299,7 @@ def _LogGrad(op, grad):
   """Returns grad * (1/x)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     return grad * math_ops.inv(x)
 
 
@@ -308,8 +308,7 @@ def _TanhGrad(op, grad):
   """Returns grad * (1 - tanh(x) * tanh(x))."""
   y = op.outputs[0]  # y = tanh(x)
   with ops.control_dependencies([grad.op]):
-    if y.dtype.is_complex:
-      y = math_ops.conj(y)
+    y = math_ops.conj(y)
     return gen_math_ops._tanh_grad(y, grad)
 
 
@@ -319,7 +318,8 @@ def _ErfGrad(op, grad):
   x = op.inputs[0]
   two_over_root_pi = constant_op.constant(2 / np.sqrt(np.pi), dtype=grad.dtype)
   with ops.control_dependencies([grad.op]):
-    return  grad * two_over_root_pi * math_ops.exp(-math_ops.square(x))
+    x = math_ops.conj(x)
+    return grad * two_over_root_pi * math_ops.exp(-math_ops.square(x))
 
 
 @ops.RegisterGradient("Erfc")
@@ -329,7 +329,8 @@ def _ErfcGrad(op, grad):
   minus_two_over_root_pi = constant_op.constant(-2 / np.sqrt(np.pi),
                                                 dtype=grad.dtype)
   with ops.control_dependencies([grad.op]):
-    return  grad * minus_two_over_root_pi * math_ops.exp(-math_ops.square(x))
+    x = math_ops.conj(x)
+    return grad * minus_two_over_root_pi * math_ops.exp(-math_ops.square(x))
 
 
 @ops.RegisterGradient("Lgamma")
@@ -337,6 +338,7 @@ def _LgammaGrad(op, grad):
   """Returns grad * digamma(x)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     return grad * math_ops.digamma(x)
 
 
@@ -345,6 +347,7 @@ def _DigammaGrad(op, grad):
   """Compute gradient of the digamma function with respect to its argument."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     return grad * math_ops.polygamma(array_ops.constant(1, dtype=x.dtype), x)
 
 
@@ -383,6 +386,8 @@ def _ZetaGrad(op, grad):
   unused_rx, rq = gen_array_ops._broadcast_gradient_args(sx, sq)
   # Evaluate gradient
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
+    q = math_ops.conj(q)
     partial_q = -x * math_ops.zeta(x + 1, q)
     return (None,
             array_ops.reshape(math_ops.reduce_sum(partial_q * grad, rq), sq))
@@ -400,6 +405,8 @@ def _PolygammaGrad(op, grad):
   unused_rn, rx = gen_array_ops._broadcast_gradient_args(sn, sx)
   # Evaluate gradient
   with ops.control_dependencies([grad.op]):
+    n = math_ops.conj(n)
+    x = math_ops.conj(x)
     partial_x = math_ops.polygamma(n + 1, x)
     return (None,
             array_ops.reshape(math_ops.reduce_sum(partial_x * grad, rx), sx))
@@ -410,8 +417,7 @@ def _SigmoidGrad(op, grad):
   """Returns grad * sigmoid(x) * (1 - sigmoid(x))."""
   y = op.outputs[0]  # y = sigmoid(x)
   with ops.control_dependencies([grad.op]):
-    if y.dtype.is_complex:
-      y = math_ops.conj(y)
+    y = math_ops.conj(y)
     return gen_math_ops._sigmoid_grad(y, grad)
 
 
@@ -427,8 +433,7 @@ def _SinGrad(op, grad):
   """Returns grad * cos(x)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
-    if x.dtype.is_complex:
-      x = math_ops.conj(x)
+    x = math_ops.conj(x)
     return grad * math_ops.cos(x)
 
 
@@ -437,8 +442,7 @@ def _CosGrad(op, grad):
   """Returns grad * -sin(x)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
-    if x.dtype.is_complex:
-      x = math_ops.conj(x)
+    x = math_ops.conj(x)
     return -grad * math_ops.sin(x)
 
 
@@ -447,6 +451,7 @@ def _TanGrad(op, grad):
   """Returns grad * 1/sec^2(x)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     secx = math_ops.inv(math_ops.cos(x))
     secx2 = math_ops.square(secx)
     return grad * secx2
@@ -457,6 +462,7 @@ def _AsinGrad(op, grad):
   """Returns grad * 1/sqrt(1-x^2)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     x2 = math_ops.square(x)
     one = constant_op.constant(1, dtype=grad.dtype)
     den = math_ops.sqrt(math_ops.sub(one, x2))
@@ -469,6 +475,7 @@ def _AcosGrad(op, grad):
   """Returns grad * -1/sqrt(1-x^2)."""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     x2 = math_ops.square(x)
     one = constant_op.constant(1, dtype=grad.dtype)
     den = math_ops.sqrt(math_ops.sub(one, x2))
@@ -481,6 +488,7 @@ def _AtanGrad(op, grad):
   """Returns grad * 1/ (1 + x^2)"""
   x = op.inputs[0]
   with ops.control_dependencies([grad.op]):
+    x = math_ops.conj(x)
     x2 = math_ops.square(x)
     one = constant_op.constant(1, dtype=grad.dtype)
     inv = math_ops.inv(math_ops.add(one, x2))
@@ -525,9 +533,8 @@ def _MulGrad(op, grad):
   sx = array_ops.shape(x)
   sy = array_ops.shape(y)
   rx, ry = gen_array_ops._broadcast_gradient_args(sx, sy)
-  if x.dtype.is_complex:
-    x = math_ops.conj(x)
-    y = math_ops.conj(y)
+  x = math_ops.conj(x)
+  y = math_ops.conj(y)
   return (array_ops.reshape(math_ops.reduce_sum(grad * y, rx), sx),
           array_ops.reshape(math_ops.reduce_sum(x * grad, ry), sy))
 
@@ -539,6 +546,8 @@ def _DivGrad(op, grad):
   sx = array_ops.shape(x)
   sy = array_ops.shape(y)
   rx, ry = gen_array_ops._broadcast_gradient_args(sx, sy)  # pylint: disable=protected-access
+  x = math_ops.conj(x)
+  y = math_ops.conj(y)
   return (array_ops.reshape(math_ops.reduce_sum(grad / y, rx), sx),
           array_ops.reshape(math_ops.reduce_sum(grad *
                                          (-x / math_ops.square(y)), ry), sy))
@@ -553,6 +562,9 @@ def _PowGrad(op, grad):
   sx = array_ops.shape(x)
   sy = array_ops.shape(y)
   rx, ry = gen_array_ops._broadcast_gradient_args(sx, sy)
+  x = math_ops.conj(x)
+  y = math_ops.conj(y)
+  z = math_ops.conj(z)
   gx = array_ops.reshape(
       math_ops.reduce_sum(grad * y * math_ops.pow(x, y - 1), rx), sx)
   # Avoid false singularity at x = 0

tensorflow/python/ops/math_ops.py
@@ -523,16 +523,20 @@ def real(input, name=None):
   tf.real(input) ==> [-2.25, 3.25]
   ```
 
+  If `input` is already real, it is returned unchanged.
+
   Args:
-    input: A `Tensor`. Must be one of the following types: `complex64`,
-         `complex128`.
+    input: A `Tensor`. Must have numeric type.
     name: A name for the operation (optional).
 
   Returns:
     A `Tensor` of type `float32` or `float64`.
   """
   with ops.name_scope(name, "Real", [input]) as name:
-    return gen_math_ops.real(input, Tout=input.dtype.real_dtype, name=name)
+    real_dtype = input.dtype.real_dtype
+    if input.dtype.base_dtype == real_dtype:
+      return input
+    return gen_math_ops.real(input, Tout=real_dtype, name=name)
 
 
 def imag(input, name=None):
@@ -1739,6 +1743,43 @@ def cumprod(x, axis=0, exclusive=False, reverse=False, name=None):
     x = ops.convert_to_tensor(x, name="x")
     return gen_math_ops.cumprod(
         x, axis, exclusive=exclusive, reverse=reverse, name=name)
+
+
+def conj(x, name=None):
+  r"""Returns the complex conjugate of a complex number.
+
+  Given a tensor `input` of complex numbers, this operation returns a tensor of
+  complex numbers that are the complex conjugate of each element in `input`. The
+  complex numbers in `input` must be of the form \\(a + bj\\), where *a* is the
+  real part and *b* is the imaginary part.
+
+  The complex conjugate returned by this operation is of the form \\(a - bj\\).
+
+  For example:
+
+      # tensor 'input' is [-2.25 + 4.75j, 3.25 + 5.75j]
+      tf.conj(input) ==> [-2.25 - 4.75j, 3.25 - 5.75j]
+
+  If `x` is real, it is returned unchanged.
+
+  Args:
+    x: `Tensor` to conjugate.  Must have numeric type.
+    name: A name for the operation (optional).
+
+  Returns:
+    A `Tensor` that is the conjugate of `x` (with the same type).
+
+  Raises:
+    TypeError: If `x` is not a numeric tensor.
+  """
+  with ops.name_scope(name, "Conj", [x]) as name:
+    x = ops.convert_to_tensor(x, name="x")
+    if x.dtype.is_complex:
+      return gen_math_ops._conj(x, name=name)
+    elif x.dtype.is_floating or x.dtype.is_integer:
+      return x
+    else:
+      raise TypeError("Expected numeric tensor, got dtype %r" % x.dtype)
 
 
 ops.RegisterShape("Abs")(common_shapes.unchanged_shape)