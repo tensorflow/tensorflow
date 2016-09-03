897e2bcf65641a12c001ce63ec1271a637df0b57
Rohan Jain <rohanj@google.com>
2016-09-03 09:51:19 -0800

    Changing ZlibInputBuffer to ZlibInputStream and making it so that it implements InputStreamInterface. Change: 132153154

--- tensorflow/core/lib/io/zlib_inputstream.h
+++ tensorflow/core/lib/io/zlib_inputstream.h
@@ -13,11 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_LIB_IO_COMPRESSED_INPUTBUFFER_H_
-#define TENSORFLOW_LIB_IO_COMPRESSED_INPUTBUFFER_H_
+#ifndef TENSORFLOW_LIB_IO_ZLIB_INPUTSTREAM_H_
+#define TENSORFLOW_LIB_IO_ZLIB_INPUTSTREAM_H_
 
 #include <string>
 #include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/core/lib/io/inputstream_interface.h"
 #include "tensorflow/core/lib/io/zlib_compression_options.h"
 #include "tensorflow/core/platform/env.h"
 #include "tensorflow/core/platform/macros.h"
@@ -34,47 +35,48 @@ limitations under the License.
 namespace tensorflow {
 namespace io {
 
-// An ZlibInputBuffer provides support for reading from a file compressed using
-// zlib (http://www.zlib.net/).
+// An ZlibInputStream provides support for reading from a stream compressed
+// using zlib (http://www.zlib.net/). Buffers the contents of the file.
 //
-// A given instance of an ZlibInputBuffer is NOT safe for concurrent use
+// A given instance of an ZlibInputStream is NOT safe for concurrent use
 // by multiple threads
-class ZlibInputBuffer {
+class ZlibInputStream : public InputStreamInterface {
  public:
-  // Create a ZlibInputBuffer for `file` with a buffer of size
-  // `input_buffer_bytes` bytes for reading contents from `file` and another
-  // buffer with size `output_buffer_bytes` for caching decompressed contents.
-  // Does *not* take ownership of "file".
-  ZlibInputBuffer(RandomAccessFile* file, size_t input_buffer_bytes,
+  // Create a ZlibInputBuffer for `input_stream` with a buffer of size
+  // `input_buffer_bytes` bytes for reading contents from `input_stream` and
+  // another buffer with size `output_buffer_bytes` for caching decompressed
+  // contents. Does *not* take ownership of "input_stream".
+  ZlibInputStream(InputStreamInterface* input_stream, size_t input_buffer_bytes,
                   size_t output_buffer_bytes,
                   const ZlibCompressionOptions& zlib_options);
 
-  ~ZlibInputBuffer();
+  ~ZlibInputStream();
 
   // Reads bytes_to_read bytes into *result, overwriting *result.
   //
   // Return Status codes:
   // OK:           If successful.
   // OUT_OF_RANGE: If there are not enough bytes to read before
-  //               the end of the file.
+  //               the end of the stream.
   // ABORTED:      If inflate() fails, we return the error code with the
   //               error message in `z_stream_->msg`.
-  // others:       If reading from file failed.
-  Status ReadNBytes(int64 bytes_to_read, string* result);
+  // others:       If reading from stream failed.
+  Status ReadNBytes(int64 bytes_to_read, string* result) override;
+
+  int64 Tell() const override;
 
  private:
-  RandomAccessFile* file_;         // Not owned
-  int64 file_pos_;                 // Next position to read from in `file_`
-  size_t input_buffer_capacity_;   // Size of `z_stream_input_`
-  size_t output_buffer_capacity_;  // Size of `z_stream_output_`
-  char* next_unread_byte_;         // Next unread byte in `z_stream_output_`
+  InputStreamInterface* input_stream_;  // Not owned
+  size_t input_buffer_capacity_;        // Size of z_stream_input_
+  size_t output_buffer_capacity_;       // Size of z_stream_output_
+  char* next_unread_byte_;              // Next unread byte in z_stream_output_
 
-  // Buffer for storing contents read from compressed file.
+  // Buffer for storing contents read from compressed stream.
   // TODO(srbs): Consider using circular buffers. That would greatly simplify
   // the implementation.
   std::unique_ptr<Bytef[]> z_stream_input_;
 
-  // Buffer for storing inflated contents of `file_`.
+  // Buffer for storing inflated contents of `input_stream_`.
   std::unique_ptr<Bytef[]> z_stream_output_;
 
   ZlibCompressionOptions const zlib_options_;
@@ -92,8 +94,8 @@ class ZlibInputBuffer {
   //   Number of free bytes available at write location.
   std::unique_ptr<z_stream> z_stream_;
 
-  // Reads data from `file_` and tries to fill up `z_stream_input_` if enough
-  // unread data is left in `file_`.
+  // Reads data from `input_stream_` and tries to fill up `z_stream_input_` if
+  // enough unread data is left in `input_stream_`.
   //
   // Looks up z_stream_->next_in to check how much data in z_stream_input_
   // has already been read. The used data is removed and new data is added to
@@ -102,10 +104,10 @@ class ZlibInputBuffer {
   // and z_stream_->avail_in stores the number of readable bytes in
   // z_stream_input_.
   //
-  // Returns OutOfRange error if NO data could be read from file. Note that this
-  // won't return an OutOfRange if there wasn't sufficient data in file to
-  // completely fill up z_stream_input_.
-  Status ReadFromFile();
+  // Returns OutOfRange error if NO data could be read from stream. Note that
+  // this won't return an OutOfRange if there wasn't sufficient data in stream
+  // to completely fill up z_stream_input_.
+  Status ReadFromStream();
 
   // Calls `inflate()` and returns DataLoss Status if it failed.
   Status Inflate();
@@ -131,10 +133,10 @@ class ZlibInputBuffer {
   // Returns the size of [next_unread_byte_, z_stream_->next_out)
   size_t NumUnreadBytes() const;
 
-  TF_DISALLOW_COPY_AND_ASSIGN(ZlibInputBuffer);
+  TF_DISALLOW_COPY_AND_ASSIGN(ZlibInputStream);
 };
 
 }  // namespace io
 }  // namespace tensorflow
 
-#endif  // TENSORFLOW_LIB_IO_CompressedInputBuffer_H_
+#endif  // TENSORFLOW_LIB_IO_ZLIB_INPUTSTREAM_H_
