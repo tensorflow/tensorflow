e98f46a30b58e086bf2dacaf371acf7e5a5b469c
A. Unique TensorFlower <gardener@tensorflow.org>
2016-09-06 09:09:59 -0800

    Fix AddBackPropIndexedSlicesAccumulator for partially defined shapes and non-float32 dtypes. Change: 132333359

--- tensorflow/python/ops/control_flow_ops_test.py
+++ tensorflow/python/ops/control_flow_ops_test.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 from tensorflow.core.framework import graph_pb2
 from tensorflow.core.framework import node_def_pb2
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework.test_util import TensorFlowTestCase
 from tensorflow.python.ops import control_flow_ops
@@ -172,55 +173,57 @@ class SwitchTestCase(TensorFlowTestCase):
         self.assertAllEqual(*sess.run([static_grads, dynamic_grads]))
 
   def testIndexedSlicesWithShapeGradientInWhileLoop(self):
-    with self.test_session() as sess:
-      num_steps = 9
+    for dtype in [dtypes.float32, dtypes.float64]:
+      with self.test_session() as sess:
+        num_steps = 9
 
-      inputs = tf.placeholder(dtype="float32", shape=[num_steps])
-      initial_outputs = tf.TensorArray(dtype="float32", size=num_steps)
-      initial_i = tf.constant(0, dtype="int32")
+        inputs = tf.placeholder(dtype=dtype, shape=[num_steps])
+        initial_outputs = tf.TensorArray(dtype=dtype, size=num_steps)
+        initial_i = tf.constant(0, dtype=dtypes.int32)
 
-      def Cond(i, _):
-        return i < num_steps
+        def Cond(i, _):
+          return i < num_steps  # pylint: disable=cell-var-from-loop
 
-      def Body(i, outputs):
-        x = tf.gather(inputs, i)
-        outputs = outputs.write(i, x)
-        return i + 1, outputs
+        def Body(i, outputs):
+          x = tf.gather(inputs, i)  # pylint: disable=cell-var-from-loop
+          outputs = outputs.write(i, x)
+          return i + 1, outputs
 
-      _, outputs = tf.while_loop(Cond, Body, [initial_i, initial_outputs])
+        _, outputs = tf.while_loop(Cond, Body, [initial_i, initial_outputs])
 
-      outputs = tf.reduce_sum(outputs.pack())
-      r = tf.gradients([outputs], [inputs])[0]
-      grad_wr_inputs = ops.convert_to_tensor(r)
-      o, grad = sess.run([outputs, grad_wr_inputs],
-                         feed_dict={inputs: [4, 6, 0, 7, 0, 0, 1, 2, 0]})
-      self.assertEquals(o, 20)
-      self.assertAllEqual(grad, [1] * num_steps)
+        outputs = tf.reduce_sum(outputs.pack())
+        r = tf.gradients([outputs], [inputs])[0]
+        grad_wr_inputs = ops.convert_to_tensor(r)
+        o, grad = sess.run([outputs, grad_wr_inputs],
+                           feed_dict={inputs: [4, 6, 0, 7, 0, 0, 1, 2, 0]})
+        self.assertEquals(o, 20)
+        self.assertAllEqual(grad, [1] * num_steps)
 
   def testIndexedSlicesWithDynamicShapeGradientInWhileLoop(self):
-    with self.test_session() as sess:
-      inputs = tf.placeholder(dtype="float32")
-      initial_outputs = tf.TensorArray(dtype="float32", dynamic_size=True,
-                                       size=1)
-      initial_i = tf.constant(0, dtype="int32")
+    for dtype in [dtypes.float32, dtypes.float64]:
+      with self.test_session() as sess:
+        inputs = tf.placeholder(dtype=dtype)
+        initial_outputs = tf.TensorArray(dtype=dtype, dynamic_size=True,
+                                         size=1)
+        initial_i = tf.constant(0, dtype=dtypes.int32)
 
-      def Cond(i, _):
-        return i < tf.size(inputs)
+        def Cond(i, _):
+          return i < tf.size(inputs)  # pylint: disable=cell-var-from-loop
 
-      def Body(i, outputs):
-        x = tf.gather(inputs, i)
-        outputs = outputs.write(i, x)
-        return i + 1, outputs
+        def Body(i, outputs):
+          x = tf.gather(inputs, i)  # pylint: disable=cell-var-from-loop
+          outputs = outputs.write(i, x)
+          return i + 1, outputs
 
-      _, outputs = tf.while_loop(Cond, Body, [initial_i, initial_outputs])
+        _, outputs = tf.while_loop(Cond, Body, [initial_i, initial_outputs])
 
-      outputs = tf.reduce_sum(outputs.pack())
-      r = tf.gradients([outputs], [inputs])[0]
-      grad_wr_inputs = ops.convert_to_tensor(r)
-      o, grad = sess.run([outputs, grad_wr_inputs],
-                         feed_dict={inputs: [1, 3, 2]})
-      self.assertEquals(o, 6)
-      self.assertAllEqual(grad, [1] * 3)
+        outputs = tf.reduce_sum(outputs.pack())
+        r = tf.gradients([outputs], [inputs])[0]
+        grad_wr_inputs = ops.convert_to_tensor(r)
+        o, grad = sess.run([outputs, grad_wr_inputs],
+                           feed_dict={inputs: [1, 3, 2]})
+        self.assertEquals(o, 6)
+        self.assertAllEqual(grad, [1] * 3)
 
 
 if __name__ == "__main__":
