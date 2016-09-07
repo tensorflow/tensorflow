cc1100a48b20e25512343fea37537370c796e359
Jingtian Peng <pjt73651@gmail.com>
2016-09-07 19:07:11 +0800

    Fix a bug #3994

--- tensorflow/python/training/optimizer.py
+++ tensorflow/python/training/optimizer.py
@@ -27,7 +27,7 @@ from tensorflow.python.ops import gradients
 from tensorflow.python.ops import state_ops
 from tensorflow.python.ops import variables
 from tensorflow.python.training import slot_creator
-import tensorflow as tf
+
 
 class Optimizer(object):
   """Base class for optimizers.
@@ -283,38 +283,36 @@ class Optimizer(object):
     # by most optimizers.  It relies on the subclass implementing the following
     # methods: _create_slots(), _prepare(), _apply_dense(), and _apply_sparse().
 
-    grads_to_tensors_list = []
-    vars_list = []
     grads_and_vars = tuple(grads_and_vars)  # Make sure repeat iteration works
+    converted_grads_and_vars = []
     for g, v in grads_and_vars:
-      # Check if gradient is Variable. If so, convert it to tensor
-      if isinstance(g, (variables.Variable)):
-        g = tf.convert_to_tensor(g)
+      if g is not None:
+        try:
+          # Convert the grad to Tensor or IndexedSlices if necessary
+          g = ops.convert_to_tensor_or_indexed_slices(g)
+        except TypeError:
+          raise TypeError(
+              "Gradient must be convertible to a Tensor or IndexedSlices, or None: %s" %g)  
       if not isinstance(g, (ops.Tensor, ops.IndexedSlices, type(None))):
         raise TypeError(
             "Gradient must be a Tensor, IndexedSlices, or None: %s" % g)
       if not isinstance(v, variables.Variable):
         raise TypeError(
             "Variable must be a tf.Variable: %s" % v)
-      if g is not None:
-        self._assert_valid_dtypes([g, v])
-      # If all is well, append g and v to corresponding list separately 
-      grads_to_tensors_list.append(g)
-      vars_list.append(v) 
 
-    # Remake the grads_and_vars after checking and converting 
-    grads_and_vars = tuple(zip(grads_to_tensors_list, vars_list))
+      converted_grads_and_vars.append((g,v))
     
-    var_list = [v for g, v in grads_and_vars if g is not None]
+    converted_grads_and_vars = tuple(converted_grads_and_vars)
+    var_list = [v for g, v in converted_grads_and_vars if g is not None]
     if not var_list:
       raise ValueError("No gradients provided for any variable: %s" %
-                       (grads_and_vars,))
+                       (converted_grads_and_vars,))
     with ops.control_dependencies(None):
       self._create_slots(var_list)
     update_ops = []
     with ops.name_scope(name, self._name) as name:
       self._prepare()
-      for grad, var in grads_and_vars:
+      for grad, var in converted_grads_and_vars:
         if grad is None:
           continue
         # We colocate all ops created in _apply_dense or _apply_sparse
