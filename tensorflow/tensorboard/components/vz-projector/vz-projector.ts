f12bcfa89ba07e39d2025ebf02656742094bad4f
Nikhil Thorat <nsthorat@google.com>
2016-09-07 14:50:20 -0800

    Separate the clickedPoint from the selectedPoints. Change: 132490364

--- tensorflow/tensorboard/components/vz-projector/vz-projector.ts
+++ tensorflow/tensorboard/components/vz-projector/vz-projector.ts
@@ -94,6 +94,9 @@ export class Projector extends ProjectorPolymer {
   private dim: number;
   private selectedDistance: (a: number[], b: number[]) => number;
   private highlightedPoints: {index: number, color: string}[];
+  // The index of a point that has been individually clicked.
+  private clickedPoint: number;
+  // The index of all selected points.
   private selectedPoints: number[];
   private centroidValues: any;
   private centroids: Centroids;
@@ -117,6 +120,7 @@ export class Projector extends ProjectorPolymer {
     this.hasPcaZ = true;
     this.selectedDistance = vector.cosDistNorm;
     this.highlightedPoints = [];
+    this.clickedPoint = null;
     this.selectedPoints = [];
     this.centroidValues = {xLeft: null, xRight: null, yUp: null, yDown: null};
     this.centroids = {xLeft: null, xRight: null, yUp: null, yDown: null};
@@ -347,7 +351,8 @@ export class Projector extends ProjectorPolymer {
           this.showTab('inspector');
           let neighbors = this.findNeighbors(indices[0]);
           if (indices.length === 1) {
-            this.scatter.clickOnPoint(indices[0]);
+            this.clickedPoint = indices[0];
+            this.scatter.clickOnPoint(this.clickedPoint);
           }
           this.selectedPoints = indices;
           this.updateNNList(neighbors);
@@ -473,17 +478,20 @@ export class Projector extends ProjectorPolymer {
   private updateSelection(selectedPoints: number[]) {
     // If no points are selected, unselect everything.
     if (!selectedPoints.length) {
+      this.clickedPoint = null;
       this.selectedPoints = [];
       this.updateNNList([]);
     } else if (selectedPoints.length === 1) {
       // If only one point is selected, we want to get its nearest neighbors
       // and change the UI accordingly.
-      let selectedPoint = selectedPoints[0];
+      this.clickedPoint = selectedPoints[0];
       this.showTab('inspector');
-      let neighbors = this.findNeighbors(selectedPoint);
-      this.selectedPoints = [selectedPoint].concat(neighbors.map(n => n.index));
+      let neighbors = this.findNeighbors(this.clickedPoint);
+      this.selectedPoints =
+          [this.clickedPoint].concat(neighbors.map(n => n.index));
       this.updateNNList(neighbors);
     } else {
+      this.clickedPoint = null;
       // Otherwise, select all points and hide nearest neighbors list.
       this.selectedPoints = selectedPoints as number[];
       this.highlightedPoints = [];
@@ -577,8 +585,8 @@ export class Projector extends ProjectorPolymer {
     metadataContainerElement.selectAll('*').remove();
 
     let display = false;
-    if (this.selectedPoints.length >= 1) {
-      let selectedPoint = this.points[this.selectedPoints[0]];
+    if (this.clickedPoint != null) {
+      let selectedPoint = this.points[this.clickedPoint];
 
       for (let metadataKey in selectedPoint.metadata) {
         if (!selectedPoint.metadata.hasOwnProperty(metadataKey)) {
@@ -662,7 +670,7 @@ export class Projector extends ProjectorPolymer {
       return;
     }
 
-    let selectedPoint = this.points[this.selectedPoints[0]];
+    let selectedPoint = this.points[this.clickedPoint];
     this.dom.select('#nn-title')
         .text(selectedPoint != null ? selectedPoint.metadata['label'] : '');
 
