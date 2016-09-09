6d7797701bace00acb3a871183ebab0093e41ae8
A. Unique TensorFlower <gardener@tensorflow.org>
2016-09-09 10:28:12 -0800

    Single-machine benchmark for kmeans: tensorflow vs. sklearn. Tensorflow seemed to go 2x faster for the larger sized problems, but slightly slower on the smaller ones. Change: 132696567

--- tensorflow/contrib/factorization/python/ops/kmeans_test.py
+++ tensorflow/contrib/factorization/python/ops/kmeans_test.py
@@ -19,12 +19,18 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+import time
+
+
 import numpy as np
+from sklearn.cluster import KMeans as SklearnKMeans
 import tensorflow as tf
 
 from tensorflow.contrib.factorization.python.ops import kmeans as kmeans_ops
 from tensorflow.contrib.factorization.python.ops.kmeans import KMeansClustering as KMeans
 from tensorflow.contrib.learn.python.learn.estimators import run_config
+from tensorflow.python.platform import benchmark
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -37,6 +43,21 @@ def cosine_similarity(x, y):
   return np.dot(normalize(x), np.transpose(normalize(y)))
 
 
+def make_random_centers(num_centers, num_dims, center_norm=500):
+  return np.round(np.random.rand(num_centers, num_dims).astype(np.float32) *
+                  center_norm)
+
+
+def make_random_points(centers, num_points, max_offset=20):
+  num_centers, num_dims = centers.shape
+  assignments = np.random.choice(num_centers, num_points)
+  offsets = np.round(np.random.randn(num_points, num_dims).astype(np.float32) *
+                     max_offset)
+  return (centers[assignments] + offsets,
+          assignments,
+          np.add.reduce(offsets * offsets, 1))
+
+
 class KMeansTest(tf.test.TestCase):
 
   def setUp(self):
@@ -44,11 +65,9 @@ class KMeansTest(tf.test.TestCase):
     self.num_centers = 5
     self.num_dims = 2
     self.num_points = 10000
-    self.true_centers = self.make_random_centers(self.num_centers,
-                                                 self.num_dims)
-    self.points, _, self.scores = self.make_random_points(
-        self.true_centers,
-        self.num_points)
+    self.true_centers = make_random_centers(self.num_centers, self.num_dims)
+    self.points, _, self.scores = make_random_points(self.true_centers,
+                                                     self.num_points)
     self.true_score = np.add.reduce(self.scores)
 
     self.kmeans = KMeans(self.num_centers,
@@ -68,22 +87,6 @@ class KMeansTest(tf.test.TestCase):
   @property
   def use_mini_batch(self):
     return False
-
-  @staticmethod
-  def make_random_centers(num_centers, num_dims):
-    return np.round(np.random.rand(num_centers,
-                                   num_dims).astype(np.float32) * 500)
-
-  @staticmethod
-  def make_random_points(centers, num_points, max_offset=20):
-    num_centers, num_dims = centers.shape
-    assignments = np.random.choice(num_centers, num_points)
-    offsets = np.round(np.random.randn(
-        num_points,
-        num_dims).astype(np.float32) * max_offset)
-    return (centers[assignments] + offsets,
-            assignments,
-            np.add.reduce(offsets * offsets, 1))
 
   def test_clusters(self):
     kmeans = self.kmeans
@@ -131,8 +134,7 @@ class KMeansTest(tf.test.TestCase):
     clusters = kmeans.clusters()
 
     # Make a small test set
-    points, true_assignments, true_offsets = self.make_random_points(clusters,
-                                                                     10)
+    points, true_assignments, true_offsets = make_random_points(clusters, 10)
     # Test predict
     assignments = kmeans.predict(points, batch_size=self.batch_size)
     self.assertAllEqual(assignments, true_assignments)
@@ -290,6 +292,92 @@ class MiniBatchKMeansTest(KMeansTest):
   @property
   def use_mini_batch(self):
     return True
+
+
+class KMeansBenchmark(benchmark.Benchmark):
+  """Base class for benchmarks."""
+
+  def SetUp(self, dimension=50, num_clusters=50, points_per_cluster=10000,
+            center_norm=500, cluster_width=20):
+    np.random.seed(123456)
+    self.num_clusters = num_clusters
+    self.num_points = num_clusters * points_per_cluster
+    self.centers = make_random_centers(self.num_clusters, dimension,
+                                       center_norm=center_norm)
+    self.points, _, scores = make_random_points(self.centers, self.num_points,
+                                                max_offset=cluster_width)
+    self.score = float(np.sum(scores))
+
+  def _report(self, num_iters, start, end, scores):
+    print(scores)
+    self.report_benchmark(iters=num_iters, wall_time=(end - start) / num_iters,
+                          extras={'true_sum_squared_distances': self.score,
+                                  'fit_scores': scores})
+
+  def _fit(self, num_iters=10):
+    pass
+
+  def benchmark_01_2dim_5center_500point(self):
+    self.SetUp(dimension=2, num_clusters=5, points_per_cluster=100)
+    self._fit()
+
+  def benchmark_02_20dim_20center_10kpoint(self):
+    self.SetUp(dimension=20, num_clusters=20, points_per_cluster=500)
+    self._fit()
+
+  def benchmark_03_100dim_50center_50kpoint(self):
+    self.SetUp(dimension=100, num_clusters=50, points_per_cluster=1000)
+    self._fit()
+
+  def benchmark_03_100dim_50center_50kpoint_unseparated(self):
+    self.SetUp(dimension=100, num_clusters=50, points_per_cluster=1000,
+               cluster_width=250)
+    self._fit()
+
+  def benchmark_04_100dim_500center_500kpoint(self):
+    self.SetUp(dimension=100, num_clusters=500, points_per_cluster=1000)
+    self._fit(num_iters=4)
+
+  def benchmark_05_100dim_500center_500kpoint_unseparated(self):
+    self.SetUp(dimension=100, num_clusters=500, points_per_cluster=1000,
+               cluster_width=250)
+    self._fit(num_iters=4)
+
+
+class TensorflowKMeansBenchmark(KMeansBenchmark):
+
+  def _fit(self, num_iters=10):
+    scores = []
+    start = time.time()
+    for i in range(num_iters):
+      print('Starting tensorflow KMeans: %d' % i)
+      tf_kmeans = KMeans(self.num_clusters,
+                         initial_clusters=kmeans_ops.KMEANS_PLUS_PLUS_INIT,
+                         kmeans_plus_plus_num_retries=int(
+                             math.log(self.num_clusters) + 2),
+                         random_seed=i * 42,
+                         config=run_config.RunConfig(tf_random_seed=3))
+      tf_kmeans.fit(x=self.points, batch_size=self.num_points, steps=50,
+                    relative_tolerance=1e-6)
+      _ = tf_kmeans.clusters()
+      scores.append(tf_kmeans.score(self.points))
+    self._report(num_iters, start, time.time(), scores)
+
+
+class SklearnKMeansBenchmark(KMeansBenchmark):
+
+  def _fit(self, num_iters=10):
+    scores = []
+    start = time.time()
+    for i in range(num_iters):
+      print('Starting sklearn KMeans: %d' % i)
+      sklearn_kmeans = SklearnKMeans(n_clusters=self.num_clusters,
+                                     init='k-means++',
+                                     max_iter=50, n_init=1, tol=1e-4,
+                                     random_state=i * 42)
+      sklearn_kmeans.fit(self.points)
+      scores.append(sklearn_kmeans.inertia_)
+    self._report(num_iters, start, time.time(), scores)
 
 
 if __name__ == '__main__':
