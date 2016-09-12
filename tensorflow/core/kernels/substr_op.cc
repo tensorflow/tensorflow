c2ffd19d9fb585a847548c6c8b1c7a44cc219825
Sam Abrahams <sjabraha@gmail.com>
2016-09-12 13:06:25 -0700

    Remove unnecessary header includes

--- tensorflow/core/kernels/substr_op.cc
+++ tensorflow/core/kernels/substr_op.cc
@@ -19,11 +19,8 @@ limitations under the License.
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor_shape.h"
-#include "tensorflow/core/framework/tensor_types.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/lib/core/errors.h"
-#include "tensorflow/core/lib/core/status.h"
-#include "tensorflow/core/platform/logging.h"
 
 namespace tensorflow {
 
