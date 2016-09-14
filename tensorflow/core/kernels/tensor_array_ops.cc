035f3ec26f855471a4aa373f71952fb35356d3db
Derek Murray <mrry@google.com>
2016-09-14 10:35:04 -0800

    Include what you use: <numeric> for std::iota(). Change: 133151870

--- tensorflow/core/kernels/tensor_array_ops.cc
+++ tensorflow/core/kernels/tensor_array_ops.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #define EIGEN_USE_THREADS
 
 #include <limits>
+#include <numeric>
 #include <vector>
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
