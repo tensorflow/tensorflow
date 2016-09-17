3f2e3252943f4d6beee1500235730dbb4dc8671d
Asim Shankar <ashankar@google.com>
2016-09-16 19:31:47 -0800

    graph_constructor: Cosmetic changes.
    My intent is to be able to re-use most of GraphConstructor to implement the
    equivalent of python's import_graph_def() in C++. Breaking that intent
    down into a series of smaller changes that morph GraphConstructor into
    a form that will work for that, as well as continue to serve the purpose
    it does today.
    
    This particular change should not change behavior in any way.
    Change: 133456212

--- tensorflow/core/graph/graph_constructor.cc
+++ tensorflow/core/graph/graph_constructor.cc
@@ -37,44 +37,51 @@ namespace {
 inline bool IsMerge(const NodeDef& node_def) {
   return node_def.op() == "Merge" || node_def.op() == "RefMerge";
 }
-}  // namespace
 
-namespace {
+bool IsValidNodeName(StringPiece s, bool allow_internal_ops) {
+  using ::tensorflow::strings::Scanner;
+  return Scanner(s)
+      .One(allow_internal_ops ? Scanner::LETTER_DIGIT_DOT_UNDERSCORE
+                              : Scanner::LETTER_DIGIT_DOT)
+      .Any(Scanner::LETTER_DIGIT_DASH_DOT_SLASH_UNDERSCORE)
+      .Eos()
+      .GetResult();
+}
 
 class GraphConstructor {
  public:
-  GraphConstructor(const GraphConstructorOptions& opts, const GraphDef* gdef,
-                   Graph* g, Status* status)
-      : opts_(opts), gdef_(gdef), g_(g), status_(status) {
-    *status =
-        CheckVersions(gdef->versions(), TF_GRAPH_DEF_VERSION,
-                      TF_GRAPH_DEF_VERSION_MIN_PRODUCER, "GraphDef", "graph");
-    if (!status->ok()) return;
+  static Status Construct(const GraphConstructorOptions& opts,
+                          const GraphDef* gdef, Graph* g) {
+    TF_RETURN_IF_ERROR(CheckVersions(gdef->versions(), TF_GRAPH_DEF_VERSION,
+                                     TF_GRAPH_DEF_VERSION_MIN_PRODUCER,
+                                     "GraphDef", "graph"));
+    GraphConstructor c(opts, gdef, g);
     g->set_versions(gdef->versions());
-    BuildNodeIndex();
-    InitFromEdges();
-    Convert();
+    TF_RETURN_IF_ERROR(c.BuildNodeIndex());
+    TF_RETURN_IF_ERROR(c.InitFromEdges());
+    TF_RETURN_IF_ERROR(c.Convert());
+    TF_RETURN_IF_ERROR(c.AddBackEdges());
+    FixupSourceAndSinkEdges(g);
+    return Status::OK();
   }
 
  private:
-  void SetError(const string& error);
-  void SetNodeError(const NodeDef& node_def, const StringPiece& message) {
-    SetError(strings::StrCat("Node '", node_def.name(), "': ", message));
-  }
-  void BuildNodeIndex();
-  void InitFromEdges();
-  Node* MakeNode(const NodeDef& node_def);
-  void Convert();
-  // Calls SetError() and returns false if the type of the output of
-  // the source of the edge can't be consumed by destination of the edge.
-  // REQUIRES: edge must be a data edge, not a control edge.
-  bool TypeValidateEdge(const Edge* edge);
+  GraphConstructor(const GraphConstructorOptions& opts, const GraphDef* gdef,
+                   Graph* g)
+      : opts_(opts), gdef_(gdef), g_(g) {}
+
+  Status BuildNodeIndex();
+  Status InitFromEdges();
+  Status Convert();
+  Status AddBackEdges();
+
+  Status MakeNode(const NodeDef& node_def, Node** node);
+  Status MakeEdge(Node* src, int output_index, Node* dst, int input_index);
 
   // From constructor
   const GraphConstructorOptions opts_;
   const GraphDef* gdef_;
   Graph* g_;
-  Status* status_;
 
   // Mapping from node name to the index within gdef_
   struct NodeInfo {
@@ -119,51 +126,39 @@ class GraphConstructor {
     Node* dst_node;
     int dst_index;
   };
+  std::vector<EdgeInfo> back_edges_;
 };
 
-void GraphConstructor::SetError(const string& error) {
-  status_->Update(errors::InvalidArgument(error));
-}
-
-bool IsValidNodeName(StringPiece s, bool allow_internal_ops) {
-  using ::tensorflow::strings::Scanner;
-  return Scanner(s)
-      .One(allow_internal_ops ? Scanner::LETTER_DIGIT_DOT_UNDERSCORE
-                              : Scanner::LETTER_DIGIT_DOT)
-      .Any(Scanner::LETTER_DIGIT_DASH_DOT_SLASH_UNDERSCORE)
-      .Eos()
-      .GetResult();
-}
-
-void GraphConstructor::BuildNodeIndex() {
+Status GraphConstructor::BuildNodeIndex() {
   // Validate the node names and add them to name_index_.
   for (int n = 0; n < gdef_->node_size(); ++n) {
     const NodeDef& node_def(gdef_->node(n));
     if (!IsValidNodeName(node_def.name(), opts_.allow_internal_ops)) {
-      SetNodeError(node_def, "Node name contains invalid characters");
-      return;
+      return errors::InvalidArgument(
+          "Node '", node_def.name(),
+          "': Node name contains invalid characters");
     }
     if (!name_index_
              .insert(std::make_pair(StringPiece(node_def.name()), NodeInfo(n)))
              .second) {
-      SetNodeError(node_def, "Node name is not unique");
-      return;
+      return errors::InvalidArgument("Node '", node_def.name(),
+                                     "' is not unique");
     }
     // Validate the operation's type.
     if (node_def.op().empty()) {
-      SetNodeError(node_def, "Does not specify a type");
-      return;
+      return errors::InvalidArgument("Node '", node_def.name(),
+                                     "' does not specify an operation");
     }
     if (opts_.expect_device_spec && node_def.device().empty()) {
-      SetNodeError(node_def, strings::StrCat("Missing device specification."));
-      return;
+      return errors::InvalidArgument("Node '", node_def.name(),
+                                     "' is missing a device specification");
     }
   }
+  return Status::OK();
 }
 
-void GraphConstructor::InitFromEdges() {
+Status GraphConstructor::InitFromEdges() {
   const int num_nodes = gdef_->node_size();
-  ready_.reserve(num_nodes);
   pending_count_.reserve(num_nodes);
   outputs_.resize(num_nodes);
 
@@ -175,7 +170,7 @@ void GraphConstructor::InitFromEdges() {
       int32 num_control_edges = 0;
       for (int i = 0; i < node_def.input_size(); ++i) {
         StringPiece input_name(node_def.input(i));
-        if (StringPiece(input_name).starts_with("^")) {
+        if (input_name.starts_with("^")) {
           num_control_edges++;
         }
       }
@@ -189,35 +184,34 @@ void GraphConstructor::InitFromEdges() {
     }
     for (int i = 0; i < node_def.input_size(); ++i) {
       StringPiece input_name = node_def.input(i);
-      if (input_name.starts_with("^")) {
-        // Control dependence
-        input_name.remove_prefix(1);
-      }
+      input_name.Consume("^");  // In case this is a control dependence.
       TensorId id(ParseTensorName(input_name));
       auto iter = name_index_.find(id.first);
       if (iter == name_index_.end()) {
-        SetNodeError(node_def,
-                     strings::StrCat("Unknown input node ", node_def.input(i)));
-        return;
+        return errors::InvalidArgument("Node '", node_def.name(),
+                                       "': Unknown input node '",
+                                       node_def.input(i), "'");
       }
       outputs_[iter->second.gdef_index].push_back(n);
     }
   }
+  return Status::OK();
 }
 
-Node* GraphConstructor::MakeNode(const NodeDef& node_def) {
+Status GraphConstructor::MakeNode(const NodeDef& node_def, Node** node) {
   // Add the node to the graph.
-  Node* node = g_->AddNode(node_def, status_);
-  if (node == nullptr) return nullptr;
+  Status status;
+  *node = g_->AddNode(node_def, &status);
+  if (!status.ok()) return status;
   if (opts_.expect_device_spec) {
-    node->set_assigned_device_name(node_def.device());
+    (*node)->set_assigned_device_name(node_def.device());
   }
-  name_index_[node_def.name()].node = node;
-  return node;
+  name_index_[node_def.name()].node = *node;
+  return Status::OK();
 }
 
 // Return the number of nodes in "g"
-static int CountNodes(Graph* g) {
+int CountNodes(Graph* g) {
   int nodes = 0;
   for (Node* node : g->nodes()) {
     VLOG(3) << node;  // Dummy use to avoid compiler warning
@@ -226,9 +220,8 @@ static int CountNodes(Graph* g) {
   return nodes;
 }
 
-void GraphConstructor::Convert() {
+Status GraphConstructor::Convert() {
   std::vector<InputInfo> inputs;
-  std::vector<EdgeInfo> back_edges;
   int processed = 0;
   // Process the NodeDefs in topological order.
   while (!ready_.empty()) {
@@ -241,18 +234,12 @@ void GraphConstructor::Convert() {
     bool has_data_back_edge = false;
     for (int i = 0; i < node_def.input_size(); ++i) {
       StringPiece input_name(node_def.input(i));
-      if (StringPiece(input_name).starts_with("^")) {
-        // A control dependence
+      if (input_name.Consume("^")) {
         in_control_dependence = true;
-        input_name.remove_prefix(1);
-      } else {
-        if (in_control_dependence) {
-          SetNodeError(node_def, strings::StrCat(
-                                     "Control dependencies must come after ",
-                                     "regular dependencies: input ", input_name,
-                                     " of source node ", node_def.name()));
-          return;
-        }
+      } else if (in_control_dependence) {
+        return errors::InvalidArgument(
+            "Node '", node_def.name(),
+            "': Control dependencies must come after regular dependencies");
       }
       TensorId id(ParseTensorName(input_name));
       auto iter = name_index_.find(id.first);
@@ -266,40 +253,35 @@ void GraphConstructor::Convert() {
           inputs.push_back(InputInfo(id.first, src_node, id.second));
         } else {
           if (id.second >= src_node->num_outputs()) {
-            SetNodeError(
-                node_def,
-                strings::StrCat("Connecting to invalid output ", id.second,
-                                " of source node ", id.first, " which has ",
-                                src_node->num_outputs(), " outputs"));
-            return;
+            return errors::InvalidArgument(
+                "Node '", node_def.name(), "': Connecting to invalid output ",
+                id.second, " of source node ", id.first, " which has ",
+                src_node->num_outputs(), " outputs");
           }
           inputs.push_back(InputInfo(id.first, src_node, id.second));
         }
       }
     }
     if (has_data_back_edge && !IsMerge(node_def)) {
-      SetError(strings::StrCat(
-          node_def.name(),
-          " had a back edge. But only Merge can have back edges."));
-      return;
+      return errors::InvalidArgument(
+          "Node '", node_def.name(),
+          "' had a back edge, but only Merge nodes can have back edges.");
     }
 
-    Node* node = MakeNode(node_def);
-    if (node == nullptr) return;
+    Node* node;
+    TF_RETURN_IF_ERROR(MakeNode(node_def, &node));
 
     // Add edges from inputs to *node to the graph.
     for (size_t i = 0; i < inputs.size(); ++i) {
       if (inputs[i].node == nullptr) {
         // Record this back edge, which will be added after all nodes
         // are created.
-        back_edges.push_back(
+        back_edges_.push_back(
             EdgeInfo(inputs[i].name, inputs[i].index, node, i));
       } else if (inputs[i].index == -1) {
         g_->AddControlEdge(inputs[i].node, node);
       } else {
-        const Edge* edge =
-            g_->AddEdge(inputs[i].node, inputs[i].index, node, i);
-        if (!TypeValidateEdge(edge)) return;
+        TF_RETURN_IF_ERROR(MakeEdge(inputs[i].node, inputs[i].index, node, i));
       }
     }
 
@@ -313,44 +295,42 @@ void GraphConstructor::Convert() {
     }
   }
 
+  if (processed < gdef_->node_size()) {
+    return errors::InvalidArgument(gdef_->node_size() - processed,
+                                   " nodes in a cycle");
+  }
+  return Status::OK();
+}
+
+Status GraphConstructor::AddBackEdges() {
   // Add the back edges after all nodes are created.
-  for (auto e : back_edges) {
+  for (auto e : back_edges_) {
     Node* src_node = name_index_[e.src_name].node;
     if (e.src_index == -1) {
       g_->AddControlEdge(src_node, e.dst_node);
     } else {
-      const Edge* edge =
-          g_->AddEdge(src_node, e.src_index, e.dst_node, e.dst_index);
-      if (!TypeValidateEdge(edge)) return;
+      TF_RETURN_IF_ERROR(
+          MakeEdge(src_node, e.src_index, e.dst_node, e.dst_index));
     }
 
     VLOG(2) << "Add back edge: " << src_node->name() << " -> "
             << e.dst_node->name();
   }
-
-  if (processed < gdef_->node_size()) {
-    SetError(
-        strings::StrCat(gdef_->node_size() - processed, " nodes in a cycle"));
-    return;
-  }
-
-  if (status_->ok()) {
-    FixupSourceAndSinkEdges(g_);
-  }
+  return Status::OK();
 }
 
-bool GraphConstructor::TypeValidateEdge(const Edge* edge) {
-  DataType src_out = edge->src()->output_type(edge->src_output());
-  DataType dst_in = edge->dst()->input_type(edge->dst_input());
+Status GraphConstructor::MakeEdge(Node* src, int output_index, Node* dst,
+                                  int input_index) {
+  DataType src_out = src->output_type(output_index);
+  DataType dst_in = dst->input_type(input_index);
   if (!TypesCompatible(dst_in, src_out)) {
-    SetError(strings::StrCat(
-        "Input ", edge->dst_input(), " of node ", edge->dst()->name(),
-        " was passed ", DataTypeString(src_out), " from ", edge->src()->name(),
-        ":", edge->src_output(), " incompatible with expected ",
-        DataTypeString(dst_in), "."));
-    return false;
+    return errors::InvalidArgument(
+        "Input ", input_index, " of node ", dst->name(), " was passed ",
+        DataTypeString(src_out), " from ", src->name(), ":", output_index,
+        " incompatible with expected ", DataTypeString(dst_in), ".");
   }
-  return true;
+  g_->AddEdge(src, output_index, dst, input_index);
+  return Status::OK();
 }
 
 }  // namespace
@@ -367,9 +347,7 @@ GraphConstructorOptions::GraphConstructorOptions() {}
 
 Status ConvertGraphDefToGraph(const GraphConstructorOptions& opts,
                               const GraphDef& gdef, Graph* g) {
-  Status status;
-  GraphConstructor constructor(opts, &gdef, g, &status);
-  return status;
+  return GraphConstructor::Construct(opts, &gdef, g);
 }
 
 // ----------------------------------------------------------------------------
