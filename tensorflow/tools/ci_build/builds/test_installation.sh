2f53824587dc10efc8f4ad130d985519126f10c1
A. Unique TensorFlower <gardener@tensorflow.org>
2016-09-19 11:52:11 -0800

    Follow-up from cl/133420080, remove reference to deleted test. Change: 133619612

--- tensorflow/tools/ci_build/builds/test_installation.sh
+++ tensorflow/tools/ci_build/builds/test_installation.sh
@@ -92,7 +92,6 @@ PY_TEST_BLACKLIST="${PY_TEST_BLACKLIST}:"\
 "tensorflow/python/util/protobuf/compare_test.py:"\
 "tensorflow/python/framework/device_test.py:"\
 "tensorflow/python/framework/file_system_test.py:"\
-"tensorflow/contrib/learn/python/learn/tests/early_stopping_test.py:"\
 "tensorflow/contrib/quantization/python/dequantize_op_test.py:"\
 "tensorflow/contrib/quantization/python/quantized_conv_ops_test.py:"\
 "tensorflow/contrib/quantization/tools/quantize_graph_test.py:"\
@@ -101,8 +100,7 @@ PY_TEST_BLACKLIST="${PY_TEST_BLACKLIST}:"\
 "tensorflow/python/platform/default/_resource_loader_test.py:"\
 "tensorflow/python/platform/default/flags_test.py:"\
 "tensorflow/python/platform/default/logging_test.py:"\
-"tensorflow/contrib/learn/nonlinear_test.py:"\
-"tensorflow/contrib/learn/python/learn/tests/nonlinear_test.py"
+"tensorflow/contrib/learn/nonlinear_test.py:"
 
 # Test blacklist: GPU-only
 PY_TEST_GPU_BLACKLIST="${PY_TEST_GPU_BLACKLIST}:"\
