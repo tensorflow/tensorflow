7a5169037dd8f364cbd35da783cd00bce28a0a6d
Jingtian Peng <pengjingtian@huawei.com>
2016-09-21 13:08:24 +0800

    imports alphabetically

--- tensorflow/contrib/metrics/python/ops/histogram_ops.py
+++ tensorflow/contrib/metrics/python/ops/histogram_ops.py
@@ -28,9 +28,9 @@ from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import histogram_ops
+from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.ops import variable_scope
