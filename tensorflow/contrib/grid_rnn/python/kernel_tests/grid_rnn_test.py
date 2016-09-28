21b6551135d7993459057ef070f0e0c1a6515d57
Vu Pham <phvu225@gmail.com>
2016-09-28 15:45:08 +0200

    added state_is_tuple and output_is_tuple, 5 tests passed, 14 to go

--- tensorflow/contrib/grid_rnn/python/kernel_tests/grid_rnn_test.py
+++ tensorflow/contrib/grid_rnn/python/kernel_tests/grid_rnn_test.py
@@ -25,96 +25,148 @@ import tensorflow as tf
 class GridRNNCellTest(tf.test.TestCase):
 
   def testGrid2BasicLSTMCell(self):
-    with self.test_session() as sess:
+    with self.test_session(use_gpu=False) as sess:
       with tf.variable_scope(
           'root', initializer=tf.constant_initializer(0.2)) as root_scope:
         x = tf.zeros([1, 3])
-        m = tf.zeros([1, 8])
+        m = ((tf.zeros([1, 2]), tf.zeros([1, 2])),
+             (tf.zeros([1, 2]), tf.zeros([1, 2])))
         cell = tf.contrib.grid_rnn.Grid2BasicLSTMCell(2)
-        self.assertEqual(cell.state_size, 8)
+        self.assertEqual(cell.state_size, ((2, 2), (2, 2)))
 
         g, s = cell(x, m)
-        self.assertEqual(g.get_shape(), (1, 2))
-        self.assertEqual(s.get_shape(), (1, 8))
+        self.assertIsInstance(g, tuple)
+        self.assertEqual(len(g), 1)
+        self.assertEqual(g[0].get_shape(), (1, 2))
+        self.assertIsInstance(s, tuple)
+        self.assertEqual(len(s), 2)
+        self.assertEqual(len(s[0]), 2)
+        self.assertEqual(len(s[1]), 2)
+        self.assertEqual(s[0][0].get_shape(), (1, 2))
+        self.assertEqual(s[0][1].get_shape(), (1, 2))
+        self.assertEqual(s[1][0].get_shape(), (1, 2))
+        self.assertEqual(s[1][1].get_shape(), (1, 2))
 
         sess.run([tf.initialize_all_variables()])
-        res = sess.run(
+        res_g, res_s = sess.run(
             [g, s], {x: np.array([[1., 1., 1.]]),
-                     m: np.array([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]])})
-        self.assertEqual(res[0].shape, (1, 2))
-        self.assertEqual(res[1].shape, (1, 8))
-        self.assertAllClose(res[0], [[0.36617181, 0.36617181]])
-        self.assertAllClose(res[1], [[0.71053141, 0.71053141, 0.36617181,
-                                      0.36617181, 0.72320831, 0.80555487,
-                                      0.39102408, 0.42150158]])
+                     m: ((np.array([[0.1, 0.2]]), np.array([[0.3, 0.4]])),
+                         (np.array([[0.5, 0.6]]), np.array([[0.7, 0.8]])))})
+        self.assertEqual(res_g[0].shape, (1, 2))
+        self.assertAllClose(res_g[0], [[0.36617181, 0.36617181]])
+
+        self.assertAllClose(res_s, (([[0.71053141, 0.71053141]],
+                                     [[0.36617181, 0.36617181]]),
+                                    ([[0.72320831, 0.80555487]],
+                                     [[0.39102408, 0.42150158]])))
+        self.assertAllClose(res_s[0][0].shape, (1, 2))
+        self.assertAllClose(res_s[0][1].shape, (1, 2))
+        self.assertAllClose(res_s[1][0].shape, (1, 2))
+        self.assertAllClose(res_s[1][1].shape, (1, 2))
 
         # emulate a loop through the input sequence,
         # where we call cell() multiple times
         root_scope.reuse_variables()
         g2, s2 = cell(x, m)
-        self.assertEqual(g2.get_shape(), (1, 2))
-        self.assertEqual(s2.get_shape(), (1, 8))
+        self.assertIsInstance(g2, tuple)
+        self.assertEqual(len(g2), 1)
+        self.assertEqual(g2[0].get_shape(), (1, 2))
+        self.assertIsInstance(s2, tuple)
+        self.assertEqual(len(s2), 2)
+        self.assertEqual(len(s2[0]), 2)
+        self.assertEqual(len(s2[1]), 2)
+        self.assertEqual(s2[0][0].get_shape(), (1, 2))
+        self.assertEqual(s2[0][1].get_shape(), (1, 2))
+        self.assertEqual(s2[1][0].get_shape(), (1, 2))
+        self.assertEqual(s2[1][1].get_shape(), (1, 2))
 
-        res = sess.run([g2, s2], {x: np.array([[2., 2., 2.]]), m: res[1]})
-        self.assertEqual(res[0].shape, (1, 2))
-        self.assertEqual(res[1].shape, (1, 8))
-        self.assertAllClose(res[0], [[0.58847463, 0.58847463]])
-        self.assertAllClose(res[1], [[1.40469193, 1.40469193, 0.58847463,
-                                      0.58847463, 0.97726452, 1.04626071,
-                                      0.4927212, 0.51137757]])
+        res_g2, res_s2 = sess.run([g2, s2],
+                                  {x: np.array([[2., 2., 2.]]), m: res_s})
+        self.assertEqual(res_g2[0].shape, (1, 2))
+        self.assertAllClose(res_g2[0], [[0.58847463, 0.58847463]])
+        self.assertAllClose(res_s2, (([[1.40469193, 1.40469193]],
+                                     [[0.58847463, 0.58847463]]),
+                                    ([[0.97726452, 1.04626071]],
+                                     [[0.4927212, 0.51137757]])))
+        self.assertAllClose(res_s2[0][0].shape, (1, 2))
+        self.assertAllClose(res_s2[0][1].shape, (1, 2))
+        self.assertAllClose(res_s2[1][0].shape, (1, 2))
+        self.assertAllClose(res_s2[1][1].shape, (1, 2))
 
   def testGrid2BasicLSTMCellTied(self):
-    with self.test_session() as sess:
+    with self.test_session(use_gpu=False) as sess:
       with tf.variable_scope('root', initializer=tf.constant_initializer(0.2)):
         x = tf.zeros([1, 3])
-        m = tf.zeros([1, 8])
+        m = ((tf.zeros([1, 2]), tf.zeros([1, 2])),
+             (tf.zeros([1, 2]), tf.zeros([1, 2])))
         cell = tf.contrib.grid_rnn.Grid2BasicLSTMCell(2, tied=True)
-        self.assertEqual(cell.state_size, 8)
+        self.assertEqual(cell.state_size, ((2, 2), (2, 2)))
 
         g, s = cell(x, m)
-        self.assertEqual(g.get_shape(), (1, 2))
-        self.assertEqual(s.get_shape(), (1, 8))
+        self.assertIsInstance(g, tuple)
+        self.assertEqual(len(g), 1)
+        self.assertEqual(g[0].get_shape(), (1, 2))
+        self.assertIsInstance(s, tuple)
+        self.assertEqual(len(s), 2)
+        self.assertEqual(len(s[0]), 2)
+        self.assertEqual(len(s[1]), 2)
+        self.assertEqual(s[0][0].get_shape(), (1, 2))
+        self.assertEqual(s[0][1].get_shape(), (1, 2))
+        self.assertEqual(s[1][0].get_shape(), (1, 2))
+        self.assertEqual(s[1][1].get_shape(), (1, 2))
 
         sess.run([tf.initialize_all_variables()])
-        res = sess.run(
+        res_g, res_s = sess.run(
             [g, s], {x: np.array([[1., 1., 1.]]),
-                     m: np.array([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]])})
-        self.assertEqual(res[0].shape, (1, 2))
-        self.assertEqual(res[1].shape, (1, 8))
-        self.assertAllClose(res[0], [[0.36617181, 0.36617181]])
-        self.assertAllClose(res[1], [[0.71053141, 0.71053141, 0.36617181,
-                                      0.36617181, 0.72320831, 0.80555487,
-                                      0.39102408, 0.42150158]])
+                     m: ((np.array([[0.1, 0.2]]), np.array([[0.3, 0.4]])),
+                         (np.array([[0.5, 0.6]]), np.array([[0.7, 0.8]])))})
+        self.assertEqual(res_g[0].shape, (1, 2))
+        self.assertAllClose(res_g[0], [[0.36617181, 0.36617181]])
 
-        res = sess.run([g, s], {x: np.array([[1., 1., 1.]]), m: res[1]})
-        self.assertEqual(res[0].shape, (1, 2))
-        self.assertEqual(res[1].shape, (1, 8))
-        self.assertAllClose(res[0], [[0.36703536, 0.36703536]])
-        self.assertAllClose(res[1], [[0.71200621, 0.71200621, 0.36703536,
-                                      0.36703536, 0.80941606, 0.87550586,
-                                      0.40108523, 0.42199609]])
+        self.assertAllClose(res_s, (([[0.71053141, 0.71053141]],
+                                     [[0.36617181, 0.36617181]]),
+                                    ([[0.72320831, 0.80555487]],
+                                     [[0.39102408, 0.42150158]])))
+        self.assertAllClose(res_s[0][0].shape, (1, 2))
+        self.assertAllClose(res_s[0][1].shape, (1, 2))
+        self.assertAllClose(res_s[1][0].shape, (1, 2))
+        self.assertAllClose(res_s[1][1].shape, (1, 2))
+
+        res_g, res_s = sess.run([g, s], {x: np.array([[1., 1., 1.]]), m: res_s})
+        self.assertEqual(res_g[0].shape, (1, 2))
+        self.assertAllClose(res_g[0], [[0.36703536, 0.36703536]])
+        self.assertAllClose(res_s, (([[0.71200621, 0.71200621]],
+                                     [[0.36703536, 0.36703536]]),
+                                    ([[0.80941606, 0.87550586]],
+                                     [[0.40108523, 0.42199609]])))
 
   def testGrid2BasicLSTMCellWithRelu(self):
     with self.test_session() as sess:
       with tf.variable_scope('root', initializer=tf.constant_initializer(0.2)):
         x = tf.zeros([1, 3])
-        m = tf.zeros([1, 4])
+        m = ((tf.zeros([1, 2]), tf.zeros([1, 2])),)
         cell = tf.contrib.grid_rnn.Grid2BasicLSTMCell(
             2, tied=False, non_recurrent_fn=tf.nn.relu)
-        self.assertEqual(cell.state_size, 4)
+        self.assertEqual(cell.state_size, ((2, 2), ))
 
         g, s = cell(x, m)
-        self.assertEqual(g.get_shape(), (1, 2))
-        self.assertEqual(s.get_shape(), (1, 4))
+        self.assertIsInstance(g, tuple)
+        self.assertEqual(len(g), 1)
+        self.assertEqual(g[0].get_shape(), (1, 2))
+        self.assertIsInstance(s, tuple)
+        self.assertEqual(len(s), 1)
+        self.assertEqual(len(s[0]), 2)
+        self.assertEqual(s[0][0].get_shape(), (1, 2))
+        self.assertEqual(s[0][1].get_shape(), (1, 2))
 
         sess.run([tf.initialize_all_variables()])
-        res = sess.run([g, s], {x: np.array([[1., 1., 1.]]),
-                                m: np.array([[0.1, 0.2, 0.3, 0.4]])})
-        self.assertEqual(res[0].shape, (1, 2))
-        self.assertEqual(res[1].shape, (1, 4))
-        self.assertAllClose(res[0], [[0.31667367, 0.31667367]])
-        self.assertAllClose(res[1],
-                            [[0.29530135, 0.37520045, 0.17044567, 0.21292259]])
+        res_g, res_s = sess.run(
+          [g, s], {x: np.array([[1., 1., 1.]]),
+                   m: ((np.array([[0.1, 0.2]]), np.array([[0.3, 0.4]])), )})
+        self.assertEqual(res_g[0].shape, (1, 2))
+        self.assertAllClose(res_g[0], [[0.31667367, 0.31667367]])
+        self.assertAllClose(res_s, (([[0.29530135, 0.37520045]],
+                                     [[0.17044567, 0.21292259]]), ))
 
   """LSTMCell
   """
@@ -522,6 +574,37 @@ class GridRNNCellTest(tf.test.TestCase):
       for v in values:
         self.assertTrue(np.all(np.isfinite(v)))
 
+  # test with input of None (https://github.com/tensorflow/tensorflow/issues/4296)
+
+  # test for state_size and output_size
+
+
+  def testGrid2LSTMCellLegacy(self):
+    """Test for legacy case (when state_is_tuple=False)
+    """
+    with self.test_session() as sess:
+      with tf.variable_scope('root', initializer=tf.constant_initializer(0.5)):
+        x = tf.zeros([1, 3])
+        m = tf.zeros([1, 8])
+        cell = tf.contrib.grid_rnn.Grid2LSTMCell(2, use_peepholes=True,
+                                                 state_is_tuple=False,
+                                                 output_is_tuple=False)
+        self.assertEqual(cell.state_size, 8)
+
+        g, s = cell(x, m)
+        self.assertEqual(g.get_shape(), (1, 2))
+        self.assertEqual(s.get_shape(), (1, 8))
+
+        sess.run([tf.initialize_all_variables()])
+        res = sess.run(
+            [g, s], {x: np.array([[1., 1., 1.]]),
+                     m: np.array([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]])})
+        self.assertEqual(res[0].shape, (1, 2))
+        self.assertEqual(res[1].shape, (1, 8))
+        self.assertAllClose(res[0], [[0.95686918, 0.95686918]])
+        self.assertAllClose(res[1], [[2.41515064, 2.41515064, 0.95686918,
+                                      0.95686918, 1.38917875, 1.49043763,
+                                      0.83884692, 0.86036491]])
 
 if __name__ == '__main__':
   tf.test.main()
