bdf16f94fc402dd49eef0bdef5dcad5f7d39dea0
Dan Smilkov <smilkov@google.com>
2016-10-13 09:04:22 -0800

    TensorBoard: Fix IOError message when file is not found.
    Let the user of the load_resource method decide whether to log or not.
    Change: 136052944
    
    (cherry picked from commit 9352b2ee9157baf76c2e382a9dab7fc717c4b89d)

--- tensorflow/python/platform/resource_loader.py
+++ tensorflow/python/platform/resource_loader.py
@@ -23,8 +23,6 @@ import inspect
 import os.path
 import sys
 
-from tensorflow.python.platform import tf_logging as logging
-
 
 def load_resource(path):
   """Load the resource at given path, where path is relative to tensorflow/.
@@ -43,12 +41,8 @@ def load_resource(path):
           os.path.dirname(__file__), os.pardir, os.pardir))
   path = os.path.join(tensorflow_root, path)
   path = os.path.abspath(path)
-  try:
-    with open(path, 'rb') as f:
-      return f.read()
-  except IOError as e:
-    logging.warning('IOError %s on path %s', e, path)
-    raise e
+  with open(path, 'rb') as f:
+    return f.read()
 
 
 # pylint: disable=protected-access
