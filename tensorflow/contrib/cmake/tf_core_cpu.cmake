4921b340bc08365a4889db87d370f4de968e81d1
Guenther Schmuelling <guschmue@microsoft.com>
2016-10-18 10:39:57 -0700

    cmake to enable gpu support on windows

--- tensorflow/contrib/cmake/tf_core_cpu.cmake
+++ tensorflow/contrib/cmake/tf_core_cpu.cmake
@@ -21,13 +21,27 @@ file(GLOB_RECURSE tf_core_cpu_exclude_srcs
     "${tensorflow_source_dir}/tensorflow/core/common_runtime/session_factory.cc"
     "${tensorflow_source_dir}/tensorflow/core/common_runtime/session_options.cc"
 )
-
 list(REMOVE_ITEM tf_core_cpu_srcs ${tf_core_cpu_exclude_srcs}) 
+
 # We need to include stubs for the GPU tracer, which are in the exclude glob.
 list(APPEND tf_core_cpu_srcs
      "${tensorflow_source_dir}/tensorflow/core/common_runtime/gpu/gpu_tracer.cc"
      "${tensorflow_source_dir}/tensorflow/core/common_runtime/gpu/gpu_tracer.h"
 )
+
+if (tensorflow_ENABLE_GPU)
+  file(GLOB_RECURSE tf_core_gpu_srcs
+    "${tensorflow_source_dir}/tensorflow/core/common_runtime/gpu/*.cc"
+    "${tensorflow_source_dir}/tensorflow/core/platform/default/gpu/cupti_wrapper.cc"
+    "${tensorflow_source_dir}/tensorflow/core/common_runtime/gpu_device_factory.cc"
+  )
+  file(GLOB_RECURSE tf_core_gpu_exclude_srcs
+     "${tensorflow_source_dir}/tensorflow/core/*test*.cc"
+     "${tensorflow_source_dir}/tensorflow/core/*test*.cc"
+  )
+  list(REMOVE_ITEM tf_core_gpu_srcs ${tf_core_gpu_exclude_srcs})
+  list(APPEND tf_core_cpu_srcs ${tf_core_gpu_srcs})
+endif()
 
 add_library(tf_core_cpu OBJECT ${tf_core_cpu_srcs})
 add_dependencies(tf_core_cpu tf_core_framework)
