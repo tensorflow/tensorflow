6804656a28f7be18c5b6da9d3bb9f51c4a7d0d09
thuyenvn <vanthuyen@gmail.com>
2016-10-20 14:25:03 -0700

    replace get_available_gpus() with is_gpu_available and add use_gpu=True

--- tensorflow/python/kernel_tests/conv2d_transpose_test.py
+++ tensorflow/python/kernel_tests/conv2d_transpose_test.py
@@ -158,128 +158,118 @@ class Conv2DTransposeTest(tf.test.TestCase):
     err_tolerance = 0.0005
     self.assertLess(err, err_tolerance)
 
-  def get_available_gpus(self):
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
-
   def testConv2DTransposeSingleStrideNCHW(self):
     # `NCHW` data fomat is only supported for `GPU` device.
-    if len(self.get_available_gpus()) < 1:
-      return
+    if tf.test.is_gpu_available():
+      with self.test_session(use_gpu=True):
+        strides = [1, 1, 1, 1]
 
-    strides = [1, 1, 1, 1]
+        # Input, output: [batch, depth, height, width, depth]
+        x_shape = [2, 3, 6, 4]
+        y_shape = [2, 2, 6, 4]
 
-    # Input, output: [batch, depth, height, width, depth]
-    x_shape = [2, 3, 6, 4]
-    y_shape = [2, 2, 6, 4]
+        # Filter: [kernel_height, kernel_width, output_depth, input_depth]
+        f_shape = [3, 3, 2, 3]
 
-    # Filter: [kernel_height, kernel_width, output_depth, input_depth]
-    f_shape = [3, 3, 2, 3]
+        x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
+        f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
 
-    x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
-    f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
+        output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
+                                     padding="SAME", data_format='NCHW')
 
-    output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
-                                 padding="SAME", data_format='NCHW')
-
-    with self.test_session() as sess:
-      value = output.eval()
-      for n in xrange(x_shape[0]):
-        for k in xrange(f_shape[2]):
-          for w in xrange(y_shape[3]):
-            for h in xrange(y_shape[2]):
-              target = 4 * 3.0
-              h_in = h > 0 and h < y_shape[2] - 1
-              w_in = w > 0 and w < y_shape[3] - 1
-              if h_in and w_in:
-                target += 5 * 3.0
-              elif h_in or w_in:
-                target += 2 * 3.0
-              self.assertAllClose(target, value[n, k, h, w])
+        value = output.eval()
+        for n in xrange(x_shape[0]):
+          for k in xrange(f_shape[2]):
+            for w in xrange(y_shape[3]):
+              for h in xrange(y_shape[2]):
+                target = 4 * 3.0
+                h_in = h > 0 and h < y_shape[2] - 1
+                w_in = w > 0 and w < y_shape[3] - 1
+                if h_in and w_in:
+                  target += 5 * 3.0
+                elif h_in or w_in:
+                  target += 2 * 3.0
+                self.assertAllClose(target, value[n, k, h, w])
 
   def testConv2DTransposeSameNCHW(self):
     # `NCHW` data fomat is only supported for `GPU` device.
-    if len(self.get_available_gpus()) < 1:
-      return
+    if tf.test.is_gpu_available():
+      with self.test_session(use_gpu=True):
+        strides = [1, 1, 2, 2]
 
-    strides = [1, 1, 2, 2]
+        # Input, output: [batch, depth, height, width]
+        x_shape = [2, 3, 6, 4]
+        y_shape = [2, 2, 12, 8]
 
-    # Input, output: [batch, depth, height, width]
-    x_shape = [2, 3, 6, 4]
-    y_shape = [2, 2, 12, 8]
+        # Filter: [kernel_height, kernel_width, output_depth, input_depth]
+        f_shape = [3, 3, 2, 3]
 
-    # Filter: [kernel_height, kernel_width, output_depth, input_depth]
-    f_shape = [3, 3, 2, 3]
+        x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
+        f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
 
-    x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
-    f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
+        output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
+                                          padding="SAME", data_format='NCHW')
 
-    output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
-                                      padding="SAME", data_format='NCHW')
-
-    with self.test_session():
-      value = output.eval()
-      for n in xrange(x_shape[0]):
-        for k in xrange(f_shape[2]):
-          for w in xrange(y_shape[3]):
-            for h in xrange(y_shape[2]):
-              target = 3.0
-              # We add a case for locations divisible by the stride.
-              h_in = h % strides[2] == 0 and h > 0 and h < y_shape[2] - 1
-              w_in = w % strides[3] == 0 and w > 0 and w < y_shape[3] - 1
-              if h_in and w_in:
-                target += 9.0
-              elif h_in or w_in:
-                target += 3.0
-              self.assertAllClose(target, value[n, k, h, w])
+        value = output.eval()
+        for n in xrange(x_shape[0]):
+          for k in xrange(f_shape[2]):
+            for w in xrange(y_shape[3]):
+              for h in xrange(y_shape[2]):
+                target = 3.0
+                # We add a case for locations divisible by the stride.
+                h_in = h % strides[2] == 0 and h > 0 and h < y_shape[2] - 1
+                w_in = w % strides[3] == 0 and w > 0 and w < y_shape[3] - 1
+                if h_in and w_in:
+                  target += 9.0
+                elif h_in or w_in:
+                  target += 3.0
+                self.assertAllClose(target, value[n, k, h, w])
 
   def testConv2DTransposeValidNCHW(self):
     # `NCHW` data fomat is only supported for `GPU` device.
-    if len(self.get_available_gpus()) < 1:
-      return
+    if tf.test.is_gpu_available():
+      with self.test_session(use_gpu=True):
+        strides = [1, 1, 2, 2]
 
-    strides = [1, 1, 2, 2]
+        # Input, output: [batch, depth, height, width]
+        x_shape = [2, 3, 6, 4]
+        y_shape = [2, 2, 13, 9]
 
-    # Input, output: [batch, depth, height, width]
-    x_shape = [2, 3, 6, 4]
-    y_shape = [2, 2, 13, 9]
+        # Filter: [kernel_height, kernel_width, output_depth, input_depth]
+        f_shape = [3, 3, 2, 3]
 
-    # Filter: [kernel_height, kernel_width, output_depth, input_depth]
-    f_shape = [3, 3, 2, 3]
+        x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
+        f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
+        output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
+                                        padding="VALID", data_format='NCHW')
 
-    x = tf.constant(1.0, shape=x_shape, name="x", dtype=tf.float32)
-    f = tf.constant(1.0, shape=f_shape, name="filter", dtype=tf.float32)
-    output = tf.nn.conv2d_transpose(x, f, y_shape, strides=strides,
-                                    padding="VALID", data_format='NCHW')
+        value = output.eval()
+        cache_values = np.zeros(y_shape, dtype=np.float32)
+        # The amount of padding added
+        pad = 1
+        for n in xrange(x_shape[0]):
+          for k in xrange(f_shape[2]):
+            for w in xrange(pad, y_shape[3] - pad):
+              for h in xrange(pad, y_shape[2] - pad):
+                target = 3.0
+                # We add a case for locations divisible by the stride.
+                h_in = h % strides[
+                    2] == 0 and h > pad and h < y_shape[2] - 1 - pad
+                w_in = w % strides[
+                    3] == 0 and w > pad and w < y_shape[3] - 1 - pad
+                if h_in and w_in:
+                  target += 9.0
+                elif h_in or w_in:
+                  target += 3.0
+                cache_values[n, k, h, w] = target
 
-    with self.test_session():
-      value = output.eval()
-      cache_values = np.zeros(y_shape, dtype=np.float32)
-      # The amount of padding added
-      pad = 1
-      for n in xrange(x_shape[0]):
-        for k in xrange(f_shape[2]):
-          for w in xrange(pad, y_shape[3] - pad):
-            for h in xrange(pad, y_shape[2] - pad):
-              target = 3.0
-              # We add a case for locations divisible by the stride.
-              h_in = h % strides[
-                  2] == 0 and h > pad and h < y_shape[2] - 1 - pad
-              w_in = w % strides[
-                  3] == 0 and w > pad and w < y_shape[3] - 1 - pad
-              if h_in and w_in:
-                target += 9.0
-              elif h_in or w_in:
-                target += 3.0
-              cache_values[n, k, h, w] = target
+            # copy values in the border
+            cache_values[n, k, :, 0] = cache_values[n, k, :, 1]
+            cache_values[n, k, :, -1] = cache_values[n, k, :, -2]
+            cache_values[n, k, 0, :] = cache_values[n, k, 1, :]
+            cache_values[n, k, -1, :] = cache_values[n, k, -2, :]
 
-          # copy values in the border
-          cache_values[n, k, :, 0] = cache_values[n, k, :, 1]
-          cache_values[n, k, :, -1] = cache_values[n, k, :, -2]
-          cache_values[n, k, 0, :] = cache_values[n, k, 1, :]
-          cache_values[n, k, -1, :] = cache_values[n, k, -2, :]
-
-    self.assertAllClose(cache_values, value)
+        self.assertAllClose(cache_values, value)
 
 
 if __name__ == "__main__":
