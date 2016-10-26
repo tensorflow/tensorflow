d41fb6f1b1412e1f20642f353b1a1f085ff903f2
Tom Nickson <tom.nickson@gmail.com>
2016-10-26 09:32:57 +0100

    removed erroneous tab character

--- tensorflow/core/kernels/matrix_triangular_solve_op.cc
+++ tensorflow/core/kernels/matrix_triangular_solve_op.cc
@@ -77,7 +77,7 @@ class MatrixTriangularSolveOp : public LinearAlgebraOp<Scalar> {
   int64 GetCostPerUnit(const TensorShapes& input_matrix_shapes) const final {
     double rows = static_cast<double>(input_matrix_shapes[0].dim_size(0));
     double num_rhss = static_cast<double>(input_matrix_shapes[1].dim_size(1));
-        double cost = rows * rows * num_rhss * 
+    double cost = rows * rows * num_rhss * 
           (Eigen::TensorOpCost::AddCost<Scalar>() + 
            Eigen::TensorOpCost::MulCost<Scalar>());
     return cost >= static_cast<double>(kint64max) ? kint64max
