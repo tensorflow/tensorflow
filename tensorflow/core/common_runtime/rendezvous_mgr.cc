0ad69511e18b6d87eef5ae76ff9332021d2cd955
luke iwanski <luke@codeplay.com>
2016-10-27 07:12:20 +0100

    Quick copy can be used for host buffers when using sycl.

--- tensorflow/core/common_runtime/rendezvous_mgr.cc
+++ tensorflow/core/common_runtime/rendezvous_mgr.cc
@@ -65,10 +65,10 @@ void IntraProcessRendezvous::SameWorkerRecvDone(
     StatusCallback done) {
   // Do a quick copy (sharing the underlying buffer) if both tensors
   // are on host memory.
-  const bool src_host =
-      (send_args.alloc_attrs.on_host() || parsed.src.type == "CPU");
-  const bool dst_host =
-      (recv_args.alloc_attrs.on_host() || parsed.dst.type == "CPU");
+  const bool src_host = (send_args.alloc_attrs.on_host() ||
+                         parsed.src.type == "CPU" || parsed.src.type == "SYCL");
+  const bool dst_host = (recv_args.alloc_attrs.on_host() ||
+                         parsed.dst.type == "CPU" || parsed.dst.type == "SYCL");
   if (src_host && dst_host) {
     *out = in;
     done(Status::OK());
