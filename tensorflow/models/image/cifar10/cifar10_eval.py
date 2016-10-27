8851c39b1694d154ed3f783fe5ea131d77c08780
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-10-27 18:48:59 +0200

    Revert prior indentation

--- tensorflow/models/image/cifar10/cifar10_eval.py
+++ tensorflow/models/image/cifar10/cifar10_eval.py
@@ -81,36 +81,36 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
       print('No checkpoint file found')
       return
 
-      # Start the queue runners.
-      coord = tf.train.Coordinator()
-      try:
-        threads = []
-        for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
-          threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
-                                           start=True))
+    # Start the queue runners.
+    coord = tf.train.Coordinator()
+    try:
+      threads = []
+      for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+        threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+                                         start=True))
 
-        num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
-        true_count = 0  # Counts the number of correct predictions.
-        total_sample_count = num_iter * FLAGS.batch_size
-        step = 0
-        while step < num_iter and not coord.should_stop():
-          predictions = sess.run([top_k_op])
-          true_count += np.sum(predictions)
-          step += 1
+      num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+      true_count = 0  # Counts the number of correct predictions.
+      total_sample_count = num_iter * FLAGS.batch_size
+      step = 0
+      while step < num_iter and not coord.should_stop():
+        predictions = sess.run([top_k_op])
+        true_count += np.sum(predictions)
+        step += 1
 
-        # Compute precision @ 1.
-        precision = true_count / total_sample_count
-        print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
+      # Compute precision @ 1.
+      precision = true_count / total_sample_count
+      print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
 
-        summary = tf.Summary()
-        summary.ParseFromString(sess.run(summary_op))
-        summary.value.add(tag='Precision @ 1', simple_value=precision)
-        summary_writer.add_summary(summary, global_step)
-      except Exception as e:  # pylint: disable=broad-except
-        coord.request_stop(e)
+      summary = tf.Summary()
+      summary.ParseFromString(sess.run(summary_op))
+      summary.value.add(tag='Precision @ 1', simple_value=precision)
+      summary_writer.add_summary(summary, global_step)
+    except Exception as e:  # pylint: disable=broad-except
+      coord.request_stop(e)
 
-      coord.request_stop()
-      coord.join(threads, stop_grace_period_secs=10)
+    coord.request_stop()
+    coord.join(threads, stop_grace_period_secs=10)
 
 
 def evaluate():
