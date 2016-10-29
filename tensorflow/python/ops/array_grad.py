3bb585a7b3592763ef5c6b11a897c1a8ff99ea81
A. Unique TensorFlower <gardener@tensorflow.org>
2016-10-28 17:45:51 -0800

    Automated rollback of change 137564676 Change: 137576487

--- tensorflow/python/ops/array_grad.py
+++ tensorflow/python/ops/array_grad.py
@@ -301,12 +301,8 @@ def _GatherGrad(op, grad):
 
 
 @ops.RegisterGradient("GatherNd")
-def _GatherNdGrad(op, grad):
-  ref = op.inputs[0]
-  ref_shape = array_ops.shape(ref)
-  indices = op.inputs[1]
-  ref_grad = array_ops.scatter_nd(indices, grad, ref_shape)
-  return [ref_grad, None]
+def _GatherNdGrad(unused_op, unused_grad):
+  raise NotImplementedError("Gradient for gather_nd is not implemented.")
 
 
 @ops.RegisterGradient("CheckNumerics")
@@ -570,10 +566,3 @@ def _ExtractImagePatchesGrad(op, grad):
   grad_out = array_ops.transpose(grad_out, (2, 0, 1, 3))
 
   return [grad_out]
-
-
-@ops.RegisterGradient("ScatterNd")
-def _ScatterNdGrad(op, grad):
-  indices = op.inputs[0]
-  updates_grad = array_ops.gather_nd(grad, indices)
-  return [None, updates_grad, None]
