2e6e800975c06ccc31479a22e3ddeb7ee6d188f0
Sam Abrahams <sjabraha@gmail.com>
2016-10-30 11:37:06 -0700

    Add examples to documentation in string_ops.cc

--- tensorflow/core/ops/string_ops.cc
+++ tensorflow/core/ops/string_ops.cc
@@ -325,6 +325,62 @@ Op creation.
 broadcasting
 [here](http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html)
 
+Examples:
+
+Using scalar `pos` and `len`:
+
+```
+input = [b"Hello", b"World"]
+position = 1
+length = 3
+
+output = [b"ell", b"orl"]
+```
+
+Using `pos` and `len` with same shape as `input`:
+
+```
+input = [[b"ten", b"eleven", b"twelve"],
+         [b"thirteen", b"fourteen", b"fifteen"],
+         [b"sixteen", b"seventeen", b"eighteen"]]
+position = [[1, 2, 3],
+            [1, 2, 3],
+            [1, 2, 3]]
+length =   [[2, 3, 4],
+            [4, 3, 2],
+            [5, 5, 5]]
+
+output = [[b"en", b"eve", b"lve"],
+          [b"hirt", b"urt", b"te"],
+          [b"ixtee", b"vente", b"hteen"]]
+```
+
+Broadcasting `pos` and `len` onto `input`:
+
+```
+input = [[b"ten", b"eleven", b"twelve"],
+         [b"thirteen", b"fourteen", b"fifteen"],
+         [b"sixteen", b"seventeen", b"eighteen"],
+         [b"nineteen", b"twenty", b"twentyone"]]
+position = [1, 2, 3]
+length =   [1, 2, 3]
+
+output = [[b"e", b"ev", b"lve"],
+          [b"h", b"ur", b"tee"],
+          [b"i", b"ve", b"hte"],
+          [b"i", b"en", b"nty"]]
+```
+
+Broadcasting `input` onto `pos` and `len`:
+
+```
+input = b"thirteen"
+position = [1, 5, 7]
+length =   [3, 2, 1]
+
+output = [b"hir", b"ee", b"n"]
+```
+
 input: Tensor of strings 
 pos: Scalar defining the position of first character in each substring
 len: Scalar defining the number of characters to include in each substring
