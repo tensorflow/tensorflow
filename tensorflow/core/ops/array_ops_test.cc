5a8af550174de742eedde4288be1ba4bbb1042d1
A. Unique TensorFlower <gardener@tensorflow.org>
2016-10-31 14:17:32 -0800

    Switch FakeQuant and Dequantize ops in array_ops to use C++ shape functions. Change: 137756007

--- tensorflow/core/ops/array_ops_test.cc
+++ tensorflow/core/ops/array_ops_test.cc
@@ -1493,4 +1493,36 @@ TEST(ArrayOpsTest, StridedSliceGrad_ShapeFn) {
   INFER_OK(op, "[4];?;?;?;?", "[1,2,3,4]");
 }
 
+TEST(ArrayOpsTest, UnchangedWithQuantizationScalars_ShapeFn) {
+  for (const char* op_name : {"Dequantize", "FakeQuantWithMinMaxVars"}) {
+    ShapeInferenceTestOp op(op_name);
+
+    INFER_OK(op, "?;?;?", "in0");
+    INFER_OK(op, "[1,?,3];[];[]", "in0");
+
+    // Rank check scalars.
+    INFER_ERROR("be rank 0", op, "[1,?,3];[1];[]");
+    INFER_ERROR("be rank 0", op, "[1,?,3];[];[1]");
+  }
+}
+
+TEST(ArrayOpsTest, FakeQuantWithMinMaxVarsPerChannel) {
+  ShapeInferenceTestOp op("FakeQuantWithMinMaxVarsPerChannel");
+
+  INFER_OK(op, "?;?;?", "?");
+  INFER_OK(op, "[?];?;?", "in0");
+  INFER_OK(op, "[1,?,3];[3];[3]", "in0");
+  INFER_OK(op, "[3];[3];[3]", "in0");
+
+  // Rank check vectors.
+  INFER_ERROR("be rank 1", op, "[1,?,3];[1];[]");
+  INFER_ERROR("be rank 1", op, "[1,?,3];[];[1]");
+
+  // Vectors must match each other, and match last dim of input.
+  INFER_ERROR("must be equal", op, "[1,?,3];[2];[?]");
+  INFER_ERROR("must be equal", op, "[1,?,3];[?];[2]");
+  INFER_ERROR("must be equal", op, "[1,?,?];[1];[2]");
+  INFER_ERROR("must be equal", op, "[5];[4];[?]");
+}
+
 }  // end namespace tensorflow
