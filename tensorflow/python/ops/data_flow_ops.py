281e7468a2f1e68dad66068f7ed09eef1fd7729e
SriramRamesh <grksriram321@gmail.com>
2016-11-01 02:34:37 +0530

    Add dynamic_pad flag to shuffle_batch

--- tensorflow/python/ops/data_flow_ops.py
+++ tensorflow/python/ops/data_flow_ops.py
@@ -622,6 +622,92 @@ class RandomShuffleQueue(QueueBase):
 
     super(RandomShuffleQueue, self).__init__(dtypes, shapes, names, queue_ref)
 
+class PaddingRandomShuffleQueue(QueueBase):
+  """A RandomShuffleQueue that supports batching variable-sized tensors by padding.
+
+  A `PaddingRandomShuffleQueue` may contain components with dynamic shape, while also
+  supporting `dequeue_many`.  See the constructor for more details.
+
+  See [`tf.QueueBase`](#QueueBase) for a description of the methods on
+  this class.
+
+  @@__init__
+  """
+  def __init__(self, capacity, min_after_dequeue, dtypes, shapes,
+               names=None, seed=None, shared_name=None,
+               name="padding_random_shuffle_queue"):
+               """Create a queue that dequeues elements in a random order.
+
+    A `PaddingRandomShuffleQueue` has bounded capacity; supports multiple
+    concurrent producers and consumers; and provides exactly-once
+    delivery.
+
+    A `PaddingRandomShuffleQueue` holds a list of up to `capacity`
+    elements. Each element is a fixed-length tuple of tensors whose
+    dtypes are described by `dtypes`, and whose shapes are described by the `shapes` argument.
+
+     The `shapes` argument must be specified; each component of a queue
+    element must have the respective shape.  Shapes of fixed
+    rank but variable size are allowed by setting any shape dimension to None.
+    In this case, the inputs' shape may vary along the given dimension, and
+    `dequeue_many` will pad the given dimension with zeros up to the maximum
+    shape of all elements in the given batch.
+
+    The `min_after_dequeue` argument allows the caller to specify a
+    minimum number of elements that will remain in the queue after a
+    `dequeue` or `dequeue_many` operation completes, to ensure a
+    minimum level of mixing of elements. This invariant is maintained
+    by blocking those operations until sufficient elements have been
+    enqueued. The `min_after_dequeue` argument is ignored after the
+    queue has been closed.
+
+    Args:
+      capacity: An integer. The upper bound on the number of elements
+        that may be stored in this queue.
+      min_after_dequeue: An integer (described above).
+      dtypes:  A list of `DType` objects. The length of `dtypes` must equal
+        the number of tensors in each queue element.
+      shapes: A list of fully-defined `TensorShape` objects
+        with the same length as `dtypes`.Any dimension in the `TensorShape` containing value `None` is dynamic and allows values to be enqueued with
+         variable size in that dimension.
+      names: (Optional.) A list of string naming the components in the queue
+        with the same length as `dtypes`, or `None`.  If specified the dequeue
+        methods return a dictionary with the names as keys.
+      seed: A Python integer. Used to create a random seed. See
+        [`set_random_seed`](../../api_docs/python/constant_op.md#set_random_seed)
+        for behavior.
+      shared_name: (Optional.) If non-empty, this queue will be shared under
+        the given name across multiple sessions.
+      name: Optional name for the queue operation.
+    Raises:
+      ValueError: If shapes is not a list of shapes, or the lengths of dtypes
+        and shapes do not match, or if names is specified and the lengths of
+        dtypes and names do not match.
+    """
+
+    dtypes = _as_type_list(dtypes)
+    shapes = _as_shape_list(shapes, dtypes,unknown_dim_allowed=True)
+    names = _as_name_list(names, dtypes)
+    if len(dtypes) != len(shapes):
+      raise ValueError("Shapes must be provided for all components, "
+                       "but received %d dtypes and %d shapes."
+                       % (len(dtypes), len(shapes)))
+    seed1, seed2 = random_seed.get_seed(seed)
+    if seed1 is None and seed2 is None:
+      seed1, seed2 = 0, 0
+    elif seed is None and shared_name is not None:
+      # This means that graph seed is provided but op seed is not provided.
+      # If shared_name is also provided, make seed2 depend only on the graph
+      # seed and shared_name. (seed2 from get_seed() is generally dependent on
+      # the id of the last op created.)
+      string = (str(seed1) + shared_name).encode("utf-8")
+      seed2 = int(hashlib.md5(string).hexdigest()[:8], 16) & 0x7FFFFFFF
+    queue_ref = gen_data_flow_ops._padding_random_shuffle_queue(
+        component_types=dtypes, shapes=shapes, capacity=capacity,
+        min_after_dequeue=min_after_dequeue, seed=seed1, seed2=seed2,
+        shared_name=shared_name, name=name)
+
+    super(PaddingRandomShuffleQueue, self).__init__(dtypes, shapes, names, queue_ref)
 
 class FIFOQueue(QueueBase):
   """A queue implementation that dequeues elements in first-in first-out order.
