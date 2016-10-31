711a3125a1b1d5ae8fdf8597839cc96721c1e6e5
Sam Abrahams <sjabraha@gmail.com>
2016-10-31 10:13:39 -0700

    Reorder python imports to be alphabetical

--- tensorflow/python/kernel_tests/substr_op_test.py
+++ tensorflow/python/kernel_tests/substr_op_test.py
@@ -19,8 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 
 class SubstrOpTest(tf.test.TestCase):
