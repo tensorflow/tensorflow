d59b364d8b480a62661318de4afeab463fae044c
Zafar Takhirov <cc.rafaz@zafar.cc>
2016-11-05 16:11:48 -0400

    Implemented CLs to resolve #5314

--- tensorflow/contrib/learn/python/learn/datasets/synthetic.py
+++ tensorflow/contrib/learn/python/learn/datasets/synthetic.py
@@ -25,14 +25,15 @@ import numpy as np
 
 from tensorflow.contrib.learn.python.learn.datasets.base import Dataset
 
-def circles(n_samples=100, noise=None, factor=0.8, n_classes=2, *args, **kwargs):
+def circles(n_samples=100, noise=None, seed=None, factor=0.8, n_classes=2, *args, **kwargs):
   """Create circles separated by some value
 
   Args:
-    n_samples: Number of datapoints to generate (optional, int, default=100)
-    noise: Standard deviation of the Gaussian noise added (optional, float or None, default=None)
-    factor: size factor of the inner circles with respect to the outer ones (optional, float, default=0.8)
-    n_classes: Number of classes to generate (optional, int, default=2)
+    n_samples: int, number of datapoints to generate
+    noise: float or None, standard deviation of the Gaussian noise added
+    seed: int or None, seed for the noise
+    factor: float, size factor of the inner circles with respect to the outer ones
+    n_classes: int, number of classes to generate
 
   Returns:
     Shuffled features and labels for 'circles' synthetic dataset of type `base.Dataset`
@@ -52,6 +53,8 @@ def circles(n_samples=100, noise=None, factor=0.8, n_classes=2, *args, **kwargs)
     G. Louppe, 
     J. Nothman
   """
+  if seed is not None:
+    np.random.seed(seed)
   # Algo: 1) Generate initial circle, 2) For ever class generate a smaller radius circle
   linspace = np.linspace(0, 2*np.pi, n_samples // n_classes)
   circ_x = np.empty(0, dtype=np.int32)
