62c94ea3b8df5be2a7d4498ad5f6b83f78527a81
Charles Nicholson <nicholsonc@google.com>
2016-11-07 12:44:30 -0800

    Remove 'projectedPoint' from DataSet. ProjectorScatterPlotAdapter creates a Float32Array of data point positions, and passes it into onPointPositionsChanged. Overhaul the visualizer interface, change 'onRecreateScene' to 'setScene', change 'removeAll' to 'dispose', change 'onUpdate' to 'onPointPositionsChanged', delete 'onDataSetChanged' entirely.
    Remove 'sceneIs3D' and 'backgroundColor' from onRecreateScene, they never really belonged there. The scene is /always/ 3D, the visualizers are really interested in the camera projection method, so now they query that. backgroundColor moved into the RenderContext, where it probably always belonged.
    
    Sprite visualizer now observes new point arrays and creates the WebGL representation only if one doesn't already exist, or its buffers are too small to hold the current point array.
    
    ScatterPlot directly uses THREE.AxesHelper, delete scatterPlotVisualizerAxes. (we might add it back later when scatter plot becomes more of a general-purpose reusable component, but it will need lots of work anyway).
    
    Add 'util.vector3FromPackedArray', which loads array[i], array[i+1], array[i+2] into a THREE.Vector3 and replaces 'util.getProjectedPointFromIndex'.
    
    Projector no longer automatically renders the scatter plot when the positions / attributes change.
    
    Break the trace visualizer's dependency on the selection context, it was causing problems when dataset filtering was being canceled. Add various Float32Array buffers for opacity and line width, generate them in the scatter plot adapter, and pass them into the render context with the rest of the 'attribute' data.
    
    When switching between sprites + labels mode, always add the trace visualizer.
    Change: 138426641

--- tensorflow/tensorboard/components/vz_projector/projectorScatterPlotAdapter.ts
+++ tensorflow/tensorboard/components/vz_projector/projectorScatterPlotAdapter.ts
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-import {DataSet} from './data';
+import {DataSet, PointAccessors3D} from './data';
 import {NearestEntry} from './knn';
 import {LabelRenderParams} from './renderContext';
 
@@ -44,11 +44,66 @@ const TRACE_END_HUE = 360;
 const TRACE_SATURATION = 1;
 const TRACE_LIGHTNESS = .3;
 
+const TRACE_DEFAULT_OPACITY = .2;
+const TRACE_DEFAULT_LINEWIDTH = 2;
+const TRACE_SELECTED_OPACITY = .9;
+const TRACE_SELECTED_LINEWIDTH = 3;
+const TRACE_DESELECTED_OPACITY = .05;
+
+const SCATTER_PLOT_CUBE_LENGTH = 2;
+
 /**
  * Interprets projector events and assembes the arrays and commands necessary
  * to use the ScatterPlot to render the current projected data set.
  */
 export class ProjectorScatterPlotAdapter {
+  generatePointPositionArray(ds: DataSet, pointAccessors: PointAccessors3D):
+      Float32Array {
+    if (ds == null) {
+      return new Float32Array(0);
+    }
+
+    const xScaler: d3.scale.Linear<number, number> = d3.scale.linear();
+    const yScaler: d3.scale.Linear<number, number> = d3.scale.linear();
+    let zScaler: d3.scale.Linear<number, number> = null;
+    {
+      // Determine max and min of each axis of our data.
+      const xExtent = d3.extent(ds.points, (p, i) => pointAccessors[0](i));
+      const yExtent = d3.extent(ds.points, (p, i) => pointAccessors[1](i));
+
+      const range =
+          [-SCATTER_PLOT_CUBE_LENGTH / 2, SCATTER_PLOT_CUBE_LENGTH / 2];
+
+      xScaler.domain(xExtent).range(range);
+      yScaler.domain(yExtent).range(range);
+
+      if (pointAccessors[2] != null) {
+        const zExtent = d3.extent(ds.points, (p, i) => pointAccessors[2](i));
+        zScaler = d3.scale.linear();
+        zScaler.domain(zExtent).range(range);
+      }
+    }
+
+    const positions = new Float32Array(ds.points.length * 3);
+    let dst = 0;
+
+    ds.points.forEach((d, i) => {
+      positions[dst++] = xScaler(pointAccessors[0](i));
+      positions[dst++] = yScaler(pointAccessors[1](i));
+      positions[dst++] = 0.0;
+    });
+
+    if (zScaler) {
+      dst = 2;
+      ds.points.forEach((d, i) => {
+        positions[dst] = zScaler(pointAccessors[2](i));
+        dst += 3;
+      });
+    }
+
+    return positions;
+  }
+
   generateVisibleLabelRenderParams(
       ds: DataSet, selectedPointIndices: number[],
       neighborsOfFirstPoint: NearestEntry[],
@@ -186,6 +241,36 @@ export class ProjectorScatterPlotAdapter {
     }
 
     return traceColorArrayMap;
+  }
+
+  generateLineSegmentOpacityArray(ds: DataSet, selectedPoints: number[]):
+      Float32Array {
+    if (ds == null) {
+      return new Float32Array(0);
+    }
+    const opacities = new Float32Array(ds.traces.length);
+    if (selectedPoints.length > 0) {
+      opacities.fill(TRACE_DESELECTED_OPACITY);
+      const i = ds.points[selectedPoints[0]].traceIndex;
+      opacities[i] = TRACE_SELECTED_OPACITY;
+    } else {
+      opacities.fill(TRACE_DEFAULT_OPACITY);
+    }
+    return opacities;
+  }
+
+  generateLineSegmentWidthArray(ds: DataSet, selectedPoints: number[]):
+      Float32Array {
+    if (ds == null) {
+      return new Float32Array(0);
+    }
+    const widths = new Float32Array(ds.traces.length);
+    widths.fill(TRACE_DEFAULT_LINEWIDTH);
+    if (selectedPoints.length > 0) {
+      const i = ds.points[selectedPoints[0]].traceIndex;
+      widths[i] = TRACE_SELECTED_LINEWIDTH;
+    }
+    return widths;
   }
 
   private getDefaultPointInTraceColor(index: number, totalPoints: number):
