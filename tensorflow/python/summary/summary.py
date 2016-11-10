bf7f32408b6ddb4c886f4a0196f0edb3518b063b
Dan Mané <danmane@google.com>
2016-11-09 16:33:34 -0800

    Alias summary-related protos as tf.summary.Event, tf.summary.Summary, etc
    This is part of the general Summary-related API reorganization leading up to the TensorFlow 1.0 release.
    We're moving all Summary-related functionality to the tf.summary submodule.
    
    After this change, we will have TensorFlow use the new endpoint, and deprecate tf.Summary, tf.Event, etc.
    
    Full list of aliases created:
    tf.summary.Summary
    tf.summary.SummaryDescription
    tf.summary.SessionLog
    tf.summary.TaggedRunMetadata
    tf.summary.Event
    
    I updated the usage in a few tests, just to verify the change works.
    Change: 138701324

--- tensorflow/python/summary/summary.py
+++ tensorflow/python/summary/summary.py
@@ -39,7 +39,15 @@ from __future__ import print_function
 import re as _re
 
 from google.protobuf import json_format as _json_format
-from tensorflow.core.framework import summary_pb2 as _summary_pb2
+# exports Summary, SummaryDescription, Event, TaggedRunMetadata, SessionLog
+# pylint: disable=unused-import
+from tensorflow.core.framework.summary_pb2 import Summary
+from tensorflow.core.framework.summary_pb2 import SummaryDescription
+from tensorflow.core.util.event_pb2 import Event
+from tensorflow.core.util.event_pb2 import SessionLog
+from tensorflow.core.util.event_pb2 import TaggedRunMetadata
+# pylint: enable=unused-import
+
 from tensorflow.python.framework import dtypes as _dtypes
 from tensorflow.python.framework import ops as _ops
 from tensorflow.python.ops import gen_logging_ops as _gen_logging_ops
@@ -317,9 +325,13 @@ def get_summary_description(node_def):
   if node_def.op != 'TensorSummary':
     raise ValueError("Can't get_summary_description on %s" % node_def.op)
   description_str = _compat.as_str_any(node_def.attr['description'].s)
-  summary_description = _summary_pb2.SummaryDescription()
+  summary_description = SummaryDescription()
   _json_format.Parse(description_str, summary_description)
   return summary_description
 
 
-remove_undocumented(__name__, [])
+_allowed_symbols = [
+    'Summary', 'SummaryDescription', 'Event', 'TaggedRunMetadata', 'SessionLog'
+]
+
+remove_undocumented(__name__, _allowed_symbols)
