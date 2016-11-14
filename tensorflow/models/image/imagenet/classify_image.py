1fff09e7721608cc67e39e5ecdadaa5478f8d4d0
leoybkim <leoybkim@gmail.com>
2016-11-13 19:49:39 -0800

    Remove argv argument from tf.app.run()

--- tensorflow/models/image/imagenet/classify_image.py
+++ tensorflow/models/image/imagenet/classify_image.py
@@ -224,4 +224,4 @@ if __name__ == '__main__':
       help='Display this many predictions.'
   )
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=main)
