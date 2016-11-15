632650e9631a74733ed7579af3ac607c7fa5d446
Corey Wharton <corey@cwharton.com>
2016-11-15 10:04:39 -0800

    Add support for decoding RGBA images in tfexample_decoder. (#5571)
    * Add support for decoding RGBA images in tfexample_decoder.
    
    * Remove redundant reshape op.
    
    * Don't support JPEG image format when the number of channels is >3.

--- tensorflow/contrib/slim/python/slim/data/tfexample_decoder_test.py
+++ tensorflow/contrib/slim/python/slim/data/tfexample_decoder_test.py
@@ -168,7 +168,7 @@ class TFExampleDecoderTest(tf.test.TestCase):
       self.assertEqual(tf_decoded_image.get_shape().ndims, 3)
 
   def testDecodeExampleWithPngEncoding(self):
-    test_image_channels = [1, 3]
+    test_image_channels = [1, 3, 4]
     for channels in test_image_channels:
       image_shape = (2, 3, channels)
       image, serialized_example = self.GenerateImage(
@@ -183,7 +183,7 @@ class TFExampleDecoderTest(tf.test.TestCase):
       self.assertAllClose(image, decoded_image, atol=0)
 
   def testDecodeExampleWithPNGEncoding(self):
-    test_image_channels = [1, 3]
+    test_image_channels = [1, 3, 4]
     for channels in test_image_channels:
       image_shape = (2, 3, channels)
       image, serialized_example = self.GenerateImage(
