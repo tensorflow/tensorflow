957269a88f7ce4d2890a87a2bb38499b98050945
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2016-11-14 21:02:28 -0800

    A "for comprehensions" changed to a "any" wherever is possible.

--- tensorflow/python/framework/tensor_util.py
+++ tensorflow/python/framework/tensor_util.py
@@ -221,13 +221,13 @@ def _NotNone(v):
 
 def _FilterTuple(v):
   if not isinstance(v, (list, tuple)):
-      return v
+    return v
   if isinstance(v, tuple):
-      if not True in [isinstance(x, (list, tuple)) for x in v]:
-          return None
+    if not any(isinstance(x, (list, tuple)) for x in v):
+      return None
   if isinstance(v, list):
-      if not True in [isinstance(x, (list, tuple)) for x in v]:
-          return _FirstNotNone([None if isinstance(x, (list, tuple)) else x for x in v])
+    if not any(isinstance(x, (list, tuple)) for x in v):
+      return _FirstNotNone([None if isinstance(x, (list, tuple)) else x for x in v])
   return _FirstNotNone([_FilterTuple(x) for x in v])
 
 
