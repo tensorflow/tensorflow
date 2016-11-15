f09cae541f915e458b68ec1bf9a1b51c7fd196e1
Darren Garvey <darren.garvey@gmail.com>
2016-11-15 20:15:49 +0000

    word2vec: Create directories for saving summaries, if necessary. (#5542)
    * word2vec: Create directories for saving summaries, if necessary.
    
    * Fix bad indentation.

--- tensorflow/models/embedding/word2vec_optimized.py
+++ tensorflow/models/embedding/word2vec_optimized.py
@@ -126,6 +126,8 @@ class Options(object):
 
     # Where to write out summaries.
     self.save_path = FLAGS.save_path
+    if not os.path.exists(self.save_path):
+      os.makedirs(self.save_path)
 
     # Eval options.
 
