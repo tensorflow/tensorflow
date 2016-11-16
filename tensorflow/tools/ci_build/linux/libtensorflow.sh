d045fa7d8c06b1a1ad62bed1b9772d664cba7265
Asim Shankar <ashankar@google.com>
2016-11-16 12:38:09 -0800

    Scripts to produce a tarball containing the TensorFlow C-library and C API header file.
    These scripts are intended to be run with every release to
    produce libtensorflow.tar.gz for CPU and GPU on Linux and OS X
    for x86_64 architecture machines.
    (Eventually there will be other operating systems and architectures).
    
    These binary releases are then intended to make use of other language
    bindings (such as https://github.com/tensorflow/rust,
    https://github.com/tensorflow/haskell,
    https://godoc.org/github.com/tensorflow/tensorflow/tensorflow/go) easier
    as the common case would be to download the binary C-library release and
    avoid the need to build TensorFlow from source (and all the time and
    external dependencies doing so entails).

---
+++ tensorflow/tools/ci_build/linux/libtensorflow.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+#
+# Script to produce a tarball release of the C-library and associated C API
+# header file. Intended to be run inside a docker container. See
+# libtensorflow_docker.sh
+
+set -ex
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# See comments at the top of this file for details.
+source "${SCRIPT_DIR}/../builds/libtensorflow.sh"
+
+SUFFIX="-linux-cpu-"
+if [ "${TF_NEED_CUDA}" == "1" ]; then
+  SUFFIX="-linux-gpu-"
+fi
+
+build_libtensorflow_tarball "${SUFFIX}$(uname -m)"
