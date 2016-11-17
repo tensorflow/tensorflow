16408372a6411c7f5a44139502d90bd7111d86a4
A. Unique TensorFlower <gardener@tensorflow.org>
2016-11-16 17:26:08 -0800

    Removing call to Variable.ref() which was missed in earlier change. Change: 139402578

--- tensorflow/python/training/sync_replicas_optimizer.py
+++ tensorflow/python/training/sync_replicas_optimizer.py
@@ -838,7 +838,7 @@ class SyncReplicasOptimizer(optimizer.Optimizer):
           # step so the replicas can fetch them to start the next step.
           # Note that ref() is used to avoid reading from the identity with old
           # the step.
-          tokens = array_ops.fill([self._tokens_per_step], global_step.ref())
+          tokens = array_ops.fill([self._tokens_per_step], global_step._ref())  # pylint: disable=protected-access
           sync_op = sync_token_queue.enqueue_many((tokens,))
 
         if self._variable_averages is not None:
