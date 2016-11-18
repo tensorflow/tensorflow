229ad7a8945f8e036fe3d3458df4d38db4806b40
kamilhryniewicz <kamil.hryniewicz@gmail.com>
2016-11-18 13:39:47 +0100

    Replace squeeze/split with unpack

--- tensorflow/models/rnn/ptb/ptb_word_lm.py
+++ tensorflow/models/rnn/ptb/ptb_word_lm.py
@@ -130,8 +130,7 @@ class PTBModel(object):
     #
     # The alternative version of the code below is:
     #
-    # inputs = [tf.squeeze(input_step, [1])
-    #           for input_step in tf.split(1, num_steps, inputs)]
+    # inputs = tf.unpack(inputs, num_steps, 1)
     # outputs, state = tf.nn.rnn(cell, inputs, initial_state=self._initial_state)
     outputs = []
     state = self._initial_state
