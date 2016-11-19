d94457f55b23dcc8eedb7c236a923031d7b51409
Suharsh Sivakumar <suharshs@google.com>
2016-11-18 18:04:45 -0800

    Partial run support for GRPC runtime.
    Tests for distributed partial run added in session_test.py.
    Change: 139644597

--- tensorflow/core/distributed_runtime/rpc/grpc_remote_master.cc
+++ tensorflow/core/distributed_runtime/rpc/grpc_remote_master.cc
@@ -52,6 +52,15 @@ class GrpcRemoteMaster : public MasterInterface {
     return FromGrpcStatus(stub_->ExtendSession(&ctx, *request, response));
   }
 
+  Status PartialRunSetup(CallOptions* call_options,
+                         const PartialRunSetupRequest* request,
+                         PartialRunSetupResponse* response) override {
+    ::grpc::ClientContext ctx;
+    ctx.set_fail_fast(false);
+    SetDeadline(&ctx, call_options->GetTimeout());
+    return FromGrpcStatus(stub_->PartialRunSetup(&ctx, *request, response));
+  }
+
   Status RunStep(CallOptions* call_options, const RunStepRequest* request,
                  RunStepResponse* response) override {
     ::grpc::ClientContext ctx;
