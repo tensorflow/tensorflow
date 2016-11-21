7e289460de0b8c2ef4513968fd81ea04657bcbdb
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2016-11-21 10:47:16 +0800

    Update mnist_softmax.py
    Correct some bug.

--- tensorflow/examples/tutorials/mnist/mnist_softmax.py
+++ tensorflow/examples/tutorials/mnist/mnist_softmax.py
@@ -64,10 +64,10 @@ def main(_):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-    # Test trained model
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+  # Test trained model
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
 if __name__ == '__main__':
