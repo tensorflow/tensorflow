d88995c6ab1ef5b45f2fb5175d28904cc710c626
Jonathan Hseu <jhseu@google.com>
2016-11-28 14:00:34 -0800

    Add tests for expected OUT_OF_RANGE semantics in the default filesystem. Change: 140398862

--- tensorflow/core/util/tensor_bundle/tensor_bundle_test.cc
+++ tensorflow/core/util/tensor_bundle/tensor_bundle_test.cc
@@ -511,12 +511,7 @@ TEST(TensorBundleTest, TruncatedTensorContents) {
   BundleReader reader(env, Prefix("end"));
   TF_ASSERT_OK(reader.status());
   Tensor val(DT_FLOAT, TensorShape({2, 3}));
-#if defined(PLATFORM_GOOGLE)
-  EXPECT_EQ("Data loss: Requested 24 bytes but read 23 bytes.",
-            reader.Lookup("key", &val).ToString());
-#else
   EXPECT_TRUE(errors::IsOutOfRange(reader.Lookup("key", &val)));
-#endif
 }
 
 TEST(TensorBundleTest, HeaderEntry) {
