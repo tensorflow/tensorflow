d760c8aa3dea769c84641a907684dc8aeacc9216
Chad Kennedy <chadkennedyonline@users.noreply.github.com>
2016-12-02 10:48:14 -0600

    Corrected missed argument mismatch

--- tensorflow/contrib/learn/python/learn/datasets/base.py
+++ tensorflow/contrib/learn/python/learn/datasets/base.py
@@ -186,8 +186,8 @@ def _is_retriable(e):
 
 
 @retry(initial_delay=1.0, max_delay=16.0, is_retriable=_is_retriable)
-def urlretrieve_with_retry(url, filename):
-  return urllib.request.urlretrieve(url, filename)
+def urlretrieve_with_retry(url):
+  return urllib.request.urlretrieve(url)
 
 
 def maybe_download(filename, work_directory, source_url):
