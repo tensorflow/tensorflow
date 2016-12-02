0097b314da70dd24ad0aa367feadd77874282d6f
Derek Murray <derek.murray@gmail.com>
2016-12-02 14:18:28 -0800

    [Windows/CMake] Add tensorflow.models.* to the PIP package. (#6029)
    * [Windows/CMake] Add tensorflow.models.* to the PIP package.
    
    Also runs the few tests in there, and builds the word2vec kernels.
    
    * Add missing op registration for the Word2Vec ops.
    
    * Avoid requiring shape functions for w2v ops.

--- tensorflow/contrib/cmake/tf_core_ops.cmake
+++ tensorflow/contrib/cmake/tf_core_ops.cmake
@@ -46,7 +46,7 @@ GENERATE_CONTRIB_OP_LIBRARY(cudnn_rnn "${tensorflow_source_dir}/tensorflow/contr
 GENERATE_CONTRIB_OP_LIBRARY(factorization_clustering "${tensorflow_source_dir}/tensorflow/contrib/factorization/ops/clustering_ops.cc")
 GENERATE_CONTRIB_OP_LIBRARY(factorization_factorization "${tensorflow_source_dir}/tensorflow/contrib/factorization/ops/factorization_ops.cc")
 GENERATE_CONTRIB_OP_LIBRARY(framework_variable "${tensorflow_source_dir}/tensorflow/contrib/framework/ops/variable_ops.cc")
-
+GENERATE_CONTRIB_OP_LIBRARY(word2vec "${tensorflow_source_dir}/tensorflow/models/embedding/word2vec_ops.cc")
 
 ########################################################
 # tf_user_ops library
@@ -67,6 +67,7 @@ file(GLOB_RECURSE tf_core_ops_srcs
     "${tensorflow_source_dir}/tensorflow/core/ops/*.cc"
     "${tensorflow_source_dir}/tensorflow/core/user_ops/*.h"
     "${tensorflow_source_dir}/tensorflow/core/user_ops/*.cc"
+    "${tensorflow_source_dir}/tensorflow/models/embedding/word2vec_ops.cc"
 )
 
 file(GLOB_RECURSE tf_core_ops_exclude_srcs
