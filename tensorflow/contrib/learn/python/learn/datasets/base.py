90a60f261f22cfd5c38358649f9583d1b77303e7
Chad Kennedy <chadkennedyonline@users.noreply.github.com>
2016-12-02 11:15:04 -0600

    Revert urlretrieve_with_retry() to support "filename" argument
    This allows urlretrieve_with_retry() to be called with or without filename. Mock tests can continue to pass filename and remain unchanged, since they do not create a temp file using NamedTemporaryFile. maybe_download() will work properly since it will use the temp file created within urlretrieve()

--- tensorflow/contrib/learn/python/learn/datasets/base.py
+++ tensorflow/contrib/learn/python/learn/datasets/base.py
@@ -186,8 +186,8 @@ def _is_retriable(e):
 
 
 @retry(initial_delay=1.0, max_delay=16.0, is_retriable=_is_retriable)
-def urlretrieve_with_retry(url):
-  return urllib.request.urlretrieve(url)
+def urlretrieve_with_retry(url, filename=None):
+  return urllib.request.urlretrieve(url, filename)
 
 
 def maybe_download(filename, work_directory, source_url):
