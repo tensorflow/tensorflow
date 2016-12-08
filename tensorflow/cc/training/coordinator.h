8daaae4c89a7da16caec240db582fea6d4d4b512
Benoit Steiner <bsteiner@google.com>
2016-12-08 14:25:56 -0800

    Added a new AllRunnersStopped() to check if all the runners have been stopped. Change: 141484062

--- tensorflow/cc/training/coordinator.h
+++ tensorflow/cc/training/coordinator.h
@@ -32,6 +32,10 @@ class RunnerInterface {
  public:
   virtual ~RunnerInterface() {}
   virtual Status Join() = 0;
+
+  // Returns true iff the runner is running, i.e. if it is trying to populate
+  // its queue.
+  virtual bool IsRunning() const = 0;
 };
 
 // Coordinator class manages the termination of a collection of QueueRunners.
@@ -74,6 +78,9 @@ class Coordinator {
   // supposed to be in running state when they are registered here.
   Status RegisterRunner(std::unique_ptr<RunnerInterface> runner);
 
+  // Returns true iff all the registered runners have been stopped.
+  bool AllRunnersStopped();
+
   // Requests all running threads to stop.
   Status RequestStop();
 
@@ -106,6 +113,8 @@ class Coordinator {
   mutex runners_lock_;
   std::vector<std::unique_ptr<RunnerInterface>> runners_
       GUARDED_BY(runners_lock_);
+
+  std::atomic<int> num_runners_to_cancel_;
 
   TF_DISALLOW_COPY_AND_ASSIGN(Coordinator);
 };
