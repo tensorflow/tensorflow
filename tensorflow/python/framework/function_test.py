2d00e6f17df644077af331e5bcb47a0e8a0fa1b7
A. Unique TensorFlower <gardener@tensorflow.org>
2016-12-08 16:40:38 -0800

    Re-enable the NodeDef version of functions, fixing issue where Python functions create signatures with duplicate output argument names. Change: 141500446

--- tensorflow/python/framework/function_test.py
+++ tensorflow/python/framework/function_test.py
@@ -54,6 +54,23 @@ class FunctionTest(tf.test.TestCase):
       with tf.Session() as sess:
         self.assertAllEqual([5.0], sess.run(call))
 
+  def testDefineFunctionDuplicateOutputs(self):
+
+    @function.Defun(tf.float32, func_name="Duplicate")
+    def Duplicate(a):
+      b = a + 1.0
+      return b, b
+
+    g = tf.Graph()
+    with g.as_default():
+      Duplicate([3.0])
+      func_sig = g.as_graph_def().library.function[0].signature
+      # The names given to both outputs should be different
+      # even though the same tensor is emitted to both.
+      out_names = [a.name for a in func_sig.output_arg]
+      self.assertEqual(2, len(out_names))
+      self.assertNotEqual(out_names[0], out_names[1])
+
   def testGradientFunc(self):
 
     @function.Defun(tf.float32, func_name="XSquarePlusOneFn")
