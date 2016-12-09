936ae38243fbb58f5d12515a7209be134e87ee17
gunan <gunan@google.com>
2016-12-08 19:19:04 -0800

    Fix tutorials failures. (#6200)
    * Fix ptb_word_lm tutorial.
    
    * Fix failures in cifar10_train example.
    
    * Update cifar10_input.py
    
    Correct shape for read_input.label

--- tensorflow/models/rnn/ptb/reader.py
+++ tensorflow/models/rnn/ptb/reader.py
@@ -115,6 +115,8 @@ def ptb_producer(raw_data, batch_size, num_steps, name=None):
     i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
     x = tf.strided_slice(data, [0, i * num_steps],
                          [batch_size, (i + 1) * num_steps])
+    x.set_shape([batch_size, num_steps])
     y = tf.strided_slice(data, [0, i * num_steps + 1],
                          [batch_size, (i + 1) * num_steps + 1])
+    y.set_shape([batch_size, num_steps])
     return x, y
