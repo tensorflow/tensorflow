44e903fab1f88ec33fc243a033fc3bd8b9de6af8
Andrew Harp <andrew.harp@gmail.com>
2016-12-09 19:51:55 -0500

    Remove duplicate extract method

--- tensorflow/contrib/learn/python/learn/estimators/estimator_test.py
+++ tensorflow/contrib/learn/python/learn/estimators/estimator_test.py
@@ -90,15 +90,6 @@ def boston_eval_fn():
   return tf.concat_v2([features, features], 0), tf.concat_v2([labels, labels],
                                                              0)
 
-
-def extract(data, key):
-  if isinstance(data, dict):
-    assert key in data
-    return data[key]
-  else:
-    return data
-
-
 def extract(data, key):
   if isinstance(data, dict):
     assert key in data
