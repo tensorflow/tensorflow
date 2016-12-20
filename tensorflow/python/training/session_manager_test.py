92eac95e9d3f6a083ce36aaaf3e33cff16b81dc8
Mustafa Ispir <ispir@google.com>
2016-12-20 11:03:36 -0800

    Added specific checkpoint file argument to the session_manager. Change: 142575145

--- tensorflow/python/training/session_manager_test.py
+++ tensorflow/python/training/session_manager_test.py
@@ -127,6 +127,29 @@ class SessionManagerTest(test.TestCase):
           variables.is_variable_initialized(
               sess.graph.get_tensor_by_name("v:0")).eval(session=sess))
 
+  def _test_recovered_variable(self,
+                               checkpoint_dir=None,
+                               checkpoint_filename_with_path=None):
+    # Create a new Graph and SessionManager and recover from a checkpoint.
+    with ops.Graph().as_default():
+      v = variables.Variable(2, name="v")
+      with session_lib.Session():
+        self.assertEqual(False, variables.is_variable_initialized(v).eval())
+      sm2 = session_manager.SessionManager(
+          ready_op=variables.report_uninitialized_variables())
+      saver = saver_lib.Saver({"v": v})
+      sess, initialized = sm2.recover_session(
+          "",
+          saver=saver,
+          checkpoint_dir=checkpoint_dir,
+          checkpoint_filename_with_path=checkpoint_filename_with_path)
+      self.assertTrue(initialized)
+      self.assertEqual(
+          True,
+          variables.is_variable_initialized(
+              sess.graph.get_tensor_by_name("v:0")).eval(session=sess))
+      self.assertEquals(1, sess.run(v))
+
   def testRecoverSession(self):
     # Create a checkpoint.
     checkpoint_dir = os.path.join(self.get_temp_dir(), "recover_session")
@@ -148,22 +171,16 @@ class SessionManagerTest(test.TestCase):
       self.assertEquals(1, sess.run(v))
       saver.save(sess,
                  os.path.join(checkpoint_dir, "recover_session_checkpoint"))
-    # Create a new Graph and SessionManager and recover.
-    with ops.Graph().as_default():
-      v = variables.Variable(2, name="v")
-      with self.test_session():
-        self.assertEqual(False, variables.is_variable_initialized(v).eval())
-      sm2 = session_manager.SessionManager(
-          ready_op=variables.report_uninitialized_variables())
-      saver = saver_lib.Saver({"v": v})
-      sess, initialized = sm2.recover_session(
-          "", saver=saver, checkpoint_dir=checkpoint_dir)
-      self.assertTrue(initialized)
-      self.assertEqual(
-          True,
-          variables.is_variable_initialized(
-              sess.graph.get_tensor_by_name("v:0")).eval(session=sess))
-      self.assertEquals(1, sess.run(v))
+    self._test_recovered_variable(checkpoint_dir=checkpoint_dir)
+    self._test_recovered_variable(
+        checkpoint_filename_with_path=saver_lib.latest_checkpoint(
+            checkpoint_dir))
+    # Cannot set both checkpoint_dir and checkpoint_filename_with_path.
+    with self.assertRaises(ValueError):
+      self._test_recovered_variable(
+          checkpoint_dir=checkpoint_dir,
+          checkpoint_filename_with_path=saver_lib.latest_checkpoint(
+              checkpoint_dir))
 
   def testWaitForSessionReturnsNoneAfterTimeout(self):
     with ops.Graph().as_default():
