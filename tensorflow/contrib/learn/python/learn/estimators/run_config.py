f793d1544706ad8e5d2e3b40c3a2a4a604fc2174
Yota Toyama <raviqqe@gmail.com>
2016-12-22 01:42:06 +0900

    Fix TF_CONFIG example in ClusterConfig documentation (#6430)

--- tensorflow/contrib/learn/python/learn/estimators/run_config.py
+++ tensorflow/contrib/learn/python/learn/estimators/run_config.py
@@ -84,7 +84,7 @@ class ClusterConfig(object):
                  'worker': ['host3:2222', 'host4:2222', 'host5:2222']}
       os.environ['TF_CONFIG'] = json.dumps({
           {'cluster': cluster,
-           'task_id': {'type': 'worker', 'index': 1}}})
+           'task': {'type': 'worker', 'index': 1}}})
       config = ClusterConfig()
       assert config.master == 'host4:2222'
       assert config.task_id == 1
