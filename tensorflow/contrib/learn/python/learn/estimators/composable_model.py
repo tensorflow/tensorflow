115b3f8a267fad943f2591c71cb5f9d1fcb1cfdc
Chikanaga Tomoyuki <nagachika@ruby-lang.org>
2017-01-06 16:55:40 +0900

    Eliminate ':' in summary name of contrib.learn.estimators. The character ':' is illegal in summary name and replaced by '_'.

--- tensorflow/contrib/learn/python/learn/estimators/composable_model.py
+++ tensorflow/contrib/learn/python/learn/estimators/composable_model.py
@@ -334,8 +334,8 @@ class DNNComposableModel(_ComposableModel):
 
   def _add_hidden_layer_summary(self, value, tag):
     # TODO(zakaria): Move this code to tf.learn and add test.
-    summary.scalar("%s:fraction_of_zero_values" % tag, nn.zero_fraction(value))
-    summary.histogram("%s:activation" % tag, value)
+    summary.scalar("%s/fraction_of_zero_values" % tag, nn.zero_fraction(value))
+    summary.histogram("%s/activation" % tag, value)
 
   def build_model(self, features, feature_columns, is_training):
     """See base class."""
