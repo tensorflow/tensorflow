a245f73c7d9dea4aeb5c524e290f05ef6d5efdab
A. Unique TensorFlower <gardener@tensorflow.org>
2017-01-06 12:35:32 -0800

    Allow empty tf.Features in the FeatureLists for sparse sequence examples. Change: 143802382

--- tensorflow/python/kernel_tests/parsing_ops_test.py
+++ tensorflow/python/kernel_tests/parsing_ops_test.py
@@ -1027,6 +1027,40 @@ class ParseSequenceExampleTest(test.TestCase):
         },
         expected_feat_list_values=expected_feature_list_output)
 
+  def testSequenceExampleWithEmptyFeatureInFeatureLists(self):
+    original = sequence_example(feature_lists=feature_lists({
+        "st_a":
+            feature_list([
+                float_feature([3.0, 4.0]),
+                feature(),
+                float_feature([5.0]),
+            ]),
+    }))
+
+    serialized = original.SerializeToString()
+
+    expected_st_a = (
+        np.array(
+            [[0, 0], [0, 1], [2, 0]], dtype=np.int64),  # indices
+        np.array(
+            [3.0, 4.0, 5.0], dtype=np.float32),  # values
+        np.array(
+            [3, 2], dtype=np.int64))  # shape: num_time = 3, max_feat = 2
+
+    expected_feature_list_output = {
+        "st_a": expected_st_a,
+    }
+
+    self._test(
+        {
+            "example_name": "in1",
+            "serialized": ops.convert_to_tensor(serialized),
+            "sequence_features": {
+                "st_a": parsing_ops.VarLenFeature(dtypes.float32),
+            }
+        },
+        expected_feat_list_values=expected_feature_list_output)
+
   def testSequenceExampleListWithInconsistentDataFails(self):
     original = sequence_example(feature_lists=feature_lists({
         "a": feature_list([int64_feature([-1, 0]), float_feature([2, 3])])
