4b868aee1e32faa8cb4285c5a9fe7e71d7dff34a
Vijay Vasudevan <vrv@google.com>
2017-01-09 18:36:11 -0800

    More flags->argparse conversions. Change: 144034332

--- tensorflow/contrib/session_bundle/example/export_half_plus_two.py
+++ tensorflow/contrib/session_bundle/example/export_half_plus_two.py
@@ -29,17 +29,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import argparse
+import sys
+
 import tensorflow as tf
+
 from tensorflow.contrib.session_bundle import exporter
 
-tf.app.flags.DEFINE_string("export_dir", "/tmp/half_plus_two",
-                           "Directory where to export inference model.")
-tf.flags.DEFINE_bool("use_checkpoint_v2", False,
-                     "If true, write v2 checkpoint files.")
-FLAGS = tf.flags.FLAGS
+FLAGS = None
 
 
-def Export():
+def Export(export_dir, use_checkpoint_v2):
   with tf.Session() as sess:
     # Make model parameters a&b variables instead of constants to
     # exercise the variable reloading mechanisms.
@@ -66,7 +66,7 @@ def Export():
             "b": b
         },
         sharded=True,
-        write_version=tf.train.SaverDef.V2 if FLAGS.use_checkpoint_v2 else
+        write_version=tf.train.SaverDef.V2 if use_checkpoint_v2 else
         tf.train.SaverDef.V1)
 
     # asset_path contains the base directory of assets used in training (e.g.
@@ -134,12 +134,29 @@ def Export():
         named_graph_signatures=named_graph_signature,
         assets_collection=tf.get_collection(tf.GraphKeys.ASSET_FILEPATHS),
         assets_callback=CopyAssets)
-    export.export(FLAGS.export_dir, global_step_tensor, sess)
+    export.export(export_dir, global_step_tensor, sess)
 
 
 def main(_):
-  Export()
+  Export(FLAGS.export_dir, FLAGS.use_checkpoint_v2)
 
 
 if __name__ == "__main__":
-  tf.app.run()
+  parser = argparse.ArgumentParser()
+  parser.register("type", "bool", lambda v: v.lower() == "true")
+  parser.add_argument(
+      "--export_dir",
+      type=str,
+      default="/tmp/half_plus_two",
+      help="Directory where to export inference model."
+  )
+  parser.add_argument(
+      "--use_checkpoint_v2",
+      "bool",
+      nargs="?",
+      const=True,
+      default=False,
+      help="If true, write v2 checkpoint files."
+  )
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
