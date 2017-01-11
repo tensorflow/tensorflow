37af1b8790d633b9002ab04a0e664ca3c1dbe508
Sergio Guadarrama <sguada@google.com>
2017-01-11 14:37:45 -0800

    Now that moments are numerically stable by default, not need to use moving_mean. Change: 144251474

--- tensorflow/python/layers/normalization.py
+++ tensorflow/python/layers/normalization.py
@@ -178,16 +178,13 @@ class BatchNormalization(base._Layer):  # pylint: disable=protected-access
         broadcast_gamma = None
 
     if training_value is not False:
-      # Use a copy of moving_mean as a shift to compute more reliable moments.
-      shift = math_ops.add(self.moving_mean, 0)
       if needs_broadcasting:
-        shift = array_ops.reshape(shift, broadcast_shape)
         broadcast_mean, broadcast_variance = nn.moments(
-            inputs, reduction_axes, shift=shift, keep_dims=True)
+            inputs, reduction_axes, keep_dims=True)
         mean = array_ops.reshape(broadcast_mean, [-1])
         variance = array_ops.reshape(broadcast_variance, [-1])
       else:
-        mean, variance = nn.moments(inputs, reduction_axes, shift=shift)
+        mean, variance = nn.moments(inputs, reduction_axes)
 
       # Prepare updates if necessary.
       if not self.updates:
