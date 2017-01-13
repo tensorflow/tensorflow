018583b064f40fa1f3cabb7eef0a5ac6b169275d
Shanqing Cai <cais@google.com>
2017-01-13 04:43:59 +0000

    Make tfdbg tests pass in Windows Bazel build (#6821)

--- tensorflow/python/debug/debug_data_test.py
+++ tensorflow/python/debug/debug_data_test.py
@@ -133,7 +133,7 @@ class HasNanOrInfTest(test_util.TensorFlowTestCase):
     a = np.array([1j, 3j, 3j, 7j], dtype=np.complex128)
     self.assertFalse(debug_data.has_inf_or_nan(self._dummy_datum, a))
 
-    b = np.array([1j, 3j, 3j, 7j, np.nan], dtype=np.complex256)
+    b = np.array([1j, 3j, 3j, 7j, np.nan], dtype=np.complex128)
     self.assertTrue(debug_data.has_inf_or_nan(self._dummy_datum, b))
 
   def testDTypeIntegerWorks(self):
