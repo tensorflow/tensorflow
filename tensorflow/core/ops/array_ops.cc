58dcfa4f8b33298b9c6b4b55a55fbcea868feb2f
A. Unique TensorFlower <gardener@tensorflow.org>
2017-01-17 13:19:10 -0800

    Simplifications and cleanups in the implementation of ParallelConcat Change: 144747557

--- tensorflow/core/ops/array_ops.cc
+++ tensorflow/core/ops/array_ops.cc
@@ -1226,11 +1226,9 @@ Equivalent to np.full
 
 // --------------------------------------------------------------------------
 REGISTER_OP("_ParallelConcatStart")
-    .Input("shape: Tshape")
     .Output("output: dtype")
+    .Attr("shape: shape")
     .Attr("dtype: type")
-    .Attr("Tshape: {int32, int64} = DT_INT32")
-    .Attr("init: bool = false")
     .SetIsStateful()
     .SetShapeFn([](InferenceContext* c) {
       ShapeHandle out;
@@ -1246,44 +1244,27 @@ conjunction with inplace operations.
 
 shape: 1-D `Tensor` indicating the shape of the output.
 dtype: The element type of the returned tensor.
-init: `bool` indicating whether or not to zero the allocated memory.
 output: An empty Tensor of the specified type.
 )doc");
 
 // --------------------------------------------------------------------------
 REGISTER_OP("_ParallelConcatUpdate")
     .Input("value: T")
-    .Input("loc: Tshape")
     .Input("update: T")
     .Output("output: T")
     .Attr("T: type")
-    .Attr("Tshape: {int32, int64} = DT_INT32")
+    .Attr("loc: int")
     .SetShapeFn(shape_inference::UnchangedShape)
     .Doc(R"doc(
 Updates input `value` at `loc` with `update`.
-
-If `loc` is None, `value` and `update` must be the same size.
-```
-value = update
-```
-
-If `loc` is a scalar, `value` has rank 1 higher than `update`
-```
-value[i, :] = update
-```
-
-If `loc` is a vector, `value` has the same rank as `update`
-```
-value[loc, :] = update
-```
 
 If you use this function you will almost certainly want to add
 a control dependency as done in the implementation of parallel_stack to
 avoid race conditions.
 
 value: A `Tensor` object that will be updated in-place.
-loc: A scalar or 1-D `Tensor` indicating the indices of the first dimension
-     such that value[loc, :] is updated.
+loc: A scalar indicating the index of the first dimension such that
+         value[loc, :] is updated.
 update: A `Tensor` of rank one less than `value` if `loc` is a scalar,
         otherwise of rank equal to `value` that contains the new values
         for `value`.
