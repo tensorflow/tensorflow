d1906839a628e69f8aa0cab97656d79e6877cbfe
A. Unique TensorFlower <gardener@tensorflow.org>
2017-01-18 17:36:39 -0800

    Deal with parallel concat as a registered graph optimization pass. Change: 144905301

--- tensorflow/core/common_runtime/graph_optimizer.cc
+++ tensorflow/core/common_runtime/graph_optimizer.cc
@@ -22,84 +22,6 @@ limitations under the License.
 #include "tensorflow/core/graph/optimizer_cse.h"
 
 namespace tensorflow {
-namespace {
-
-// Replaces occurrences of parallel_concat with the implementation based on
-// unsafe ops. Sets removed_any to true if any parallel_concats were removed;
-// leaves it untouched otherwise.
-Status RemoveParallelConcat(bool* removed_any, Graph* g) {
-  gtl::InlinedVector<Node*, 2> matches;
-  for (Node* n : g->nodes()) {
-    if (n->type_string() == "ParallelConcat") {
-      matches.push_back(n);
-    }
-  }
-  for (Node* n : matches) {
-    AttrSlice n_attrs(n->def());
-    auto make_node = [n, g, &n_attrs](string op) {
-      NodeBuilder node_builder(g->NewName(n->name()), op);
-      node_builder.Device(n->def().device());
-      string colo;
-      if (GetNodeAttr(n_attrs, "_class", &colo).ok()) {
-        node_builder.Attr("_class", colo);
-      }
-      return node_builder;
-    };
-    DataType dtype;
-    TF_RETURN_IF_ERROR(GetNodeAttr(n_attrs, "T", &dtype));
-    TensorShapeProto shape;
-    TF_RETURN_IF_ERROR(GetNodeAttr(n_attrs, "shape", &shape));
-
-    // Add the start node
-    Node* start;
-    TF_RETURN_IF_ERROR(make_node("_ParallelConcatStart")
-                           .Attr("shape", shape)
-                           .Attr("dtype", dtype)
-                           .Finalize(g, &start));
-
-    // Add all the inplace_updates.
-    std::vector<Node*> control_nodes;
-    int64 i = 0;
-    for (const Edge* input_edge : n->in_edges()) {
-      if (input_edge->IsControlEdge()) {
-        g->AddControlEdge(input_edge->src(), start);
-        continue;
-      }
-
-      Node* update;
-      TF_RETURN_IF_ERROR(make_node("_ParallelConcatUpdate")
-                             .Attr("loc", i)
-                             .Input(start)
-                             .Input(input_edge->src(), input_edge->src_output())
-                             .Finalize(g, &update));
-      control_nodes.push_back(update);
-
-      ++i;
-    }
-
-    // Add the final identity.
-    NodeBuilder identity_def = make_node("Identity");
-    identity_def.Input(start, 0);
-    for (Node* s : control_nodes) {
-      identity_def.ControlInput(s);
-    }
-    Node* identity_node;
-    TF_RETURN_IF_ERROR(identity_def.Finalize(g, &identity_node));
-
-    // Remove the node and redirect edges.
-    for (auto* e : n->out_edges()) {
-      if (e->IsControlEdge()) {
-        g->AddControlEdge(identity_node, e->dst());
-      } else {
-        g->AddEdge(identity_node, 0, e->dst(), e->dst_input());
-      }
-    }
-    g->RemoveNode(n);
-    *removed_any = true;
-  }
-  return Status::OK();
-}
-}
 
 GraphOptimizer::GraphOptimizer(const OptimizerOptions& opts) : opts_(opts) {
   if (opts_.opt_level() >= OptimizerOptions::L1) {
@@ -122,11 +44,6 @@ void GraphOptimizer::Optimize(FunctionLibraryRuntime* runtime, Env* env,
     if (RemoveListArrayConverter(g)) {
       DumpGraph("RemoveListArrayConverter", g);
       changed = true;
-    }
-    auto s = RemoveParallelConcat(&changed, g);
-    if (!s.ok()) {
-      // TODO(apassos): figure out how to halt here.
-      LOG(WARNING) << s;
     }
     if (opts_.do_function_inlining() && RemoveDeadNodes(g)) {
       DumpGraph("RemoveDeadNodes", g);
