3a4849e169e9394849857254e65cff8696e0c9dc
David Soergel <soergel@google.com>
2017-01-19 08:39:42 -0800

    Automated rollback of change 144922485 Change: 144966157

--- tensorflow/contrib/learn/python/learn/export_strategy.py
+++ tensorflow/contrib/learn/python/learn/export_strategy.py
@@ -13,8 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-"""ExportStrategy class that provides strategies to export model so later it
-can be used for TensorFlow serving."""
+"""ExportStrategy class represents different flavors of model export."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -26,8 +25,38 @@ __all__ = ['ExportStrategy']
 
 
 class ExportStrategy(collections.namedtuple('ExportStrategy',
-                                            ['name', 'export_fn'])):
+                                            ['name',
+                                             'export_fn',
+                                             'end_fn'])):
+  """A class representing a type of model export.
+
+  Typically constructed by a utility function specific to the exporter, such as
+  `saved_model_export_utils.make_export_strategy()`.
+
+  The fields are:
+    name: The directory name under the export base directory where exports of
+      this type will be written.
+    export_fn: A function that writes an export, given an estimator and a
+      destination path.  This may be run repeatedly during continuous training.
+    end_fn: A function to be run at the end of training, taking a single
+      argument naming the ExportStrategy-specific export directory.  This is
+      typically used to take some action regarding the most recent export, such
+      as copying it to another location.
+  """
+
+  def __new__(cls,
+              name,
+              export_fn,
+              end_fn=None):
+
+    return super(ExportStrategy, cls).__new__(cls,
+                                              name=name,
+                                              export_fn=export_fn,
+                                              end_fn=end_fn)
 
   def export(self, estimator, export_path):
     return self.export_fn(estimator, export_path)
+
+  def end(self, export_path):
+    return self.end_fn(export_path)
 
