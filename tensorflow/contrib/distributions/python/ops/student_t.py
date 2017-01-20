4a070c0ba310672fab25aa3dbaa46d83cfd11fe0
Eugene Brevdo <ebrevdo@google.com>
2017-01-19 21:02:57 -0800

    Bugfix: StudentT distribution is not reparameterizable. Change: 145046188

--- tensorflow/contrib/distributions/python/ops/student_t.py
+++ tensorflow/contrib/distributions/python/ops/student_t.py
@@ -142,7 +142,7 @@ class StudentT(distribution.Distribution):
     super(StudentT, self).__init__(
         dtype=self._sigma.dtype,
         is_continuous=True,
-        reparameterization_type=distribution.FULLY_REPARAMETERIZED,
+        reparameterization_type=distribution.NOT_REPARAMETERIZED,
         validate_args=validate_args,
         allow_nan_stats=allow_nan_stats,
         parameters=parameters,
