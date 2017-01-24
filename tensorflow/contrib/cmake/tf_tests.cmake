d1cc162253641d0c9229dfdc8efc8af366d88e54
Gunhan Gulsoy <gunan@google.com>
2017-01-24 10:36:26 -0800

    Fix: Rename the reference to http_test in tf_tests.cmake. Change: 145436800

--- tensorflow/contrib/cmake/tf_tests.cmake
+++ tensorflow/contrib/cmake/tf_tests.cmake
@@ -165,7 +165,7 @@ if (tensorflow_BUILD_PYTHON_TESTS)
       "${tensorflow_source_dir}/tensorflow/python/kernel_tests/array_ops_test.py"  # depends on python/framework/test_ops
       # Broken TensorBoard tests due to different paths in windows
       "${tensorflow_source_dir}/tensorflow/tensorboard/backend/application_test.py"
-      "${tensorflow_source_dir}/tensorflow/tensorboard/lib/python/http_test.py"
+      "${tensorflow_source_dir}/tensorflow/tensorboard/lib/python/http_util_test.py"
       # tensor_forest tests (also note that we exclude the hybrid tests for now)
       "${tensorflow_source_dir}/tensorflow/contrib/tensor_forest/python/kernel_tests/count_extremely_random_stats_op_test.py"  # Results in wrong order.
       "${tensorflow_source_dir}/tensorflow/contrib/tensor_forest/python/kernel_tests/sample_inputs_op_test.py"  # Results in wrong order.
