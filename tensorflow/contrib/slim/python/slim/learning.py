665d811195a3d8ab84f4ca598bbd717ea51431cf
A. Unique TensorFlower <gardener@tensorflow.org>
2017-01-25 13:48:10 -0800

    Use a scalar summary for gradient norms Change: 145591369

--- tensorflow/contrib/slim/python/slim/learning.py
+++ tensorflow/contrib/slim/python/slim/learning.py
@@ -359,8 +359,8 @@ def add_gradients_summaries(grads_and_vars):
       summaries.append(
           summary.histogram(var.op.name + '/gradient', grad_values))
       summaries.append(
-          summary.histogram(var.op.name + '/gradient_norm',
-                            clip_ops.global_norm([grad_values])))
+          summary.scalar(var.op.name + '/gradient_norm',
+                         clip_ops.global_norm([grad_values])))
     else:
       logging.info('Var %s has no gradient', var.op.name)
 
