357b10acf57f5cd1abf1a3bd41ea518c5624b6b9
Dan Ringwalt <ringwalt@google.com>
2017-01-25 12:18:50 -0800

    Support a scalar or vector of globs in tf.matching_files and related functions.
    This makes it more convenient to pass in multiple globs, e.g. for training data in multiple directories.
    Change: 145580313

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -250,13 +250,13 @@ def write_string_to_file(filename, file_content):
 
 
 def get_matching_files(filename):
-  """Returns a list of files that match the given pattern.
+  """Returns a list of files that match the given pattern(s).
 
   Args:
-    filename: string, the pattern
+    filename: string or iterable of strings. The glob pattern(s).
 
   Returns:
-    Returns a list of strings containing filenames that match the given pattern.
+    A list of strings containing filenames that match the given pattern(s).
 
   Raises:
     errors.OpError: If there are filesystem / directory listing errors.
@@ -264,9 +264,20 @@ def get_matching_files(filename):
   with errors.raise_exception_on_not_ok_status() as status:
     # Convert each element to string, since the return values of the
     # vector of string should be interpreted as strings, not bytes.
-    return [compat.as_str_any(matching_filename)
-            for matching_filename in pywrap_tensorflow.GetMatchingFiles(
-                compat.as_bytes(filename), status)]
+    if hasattr(filename, "__iter__"):
+      glob_patterns = filename
+      return [
+          compat.as_str_any(matching_filename)
+          for pattern in glob_patterns
+          for matching_filename in pywrap_tensorflow.GetMatchingFiles(
+              compat.as_bytes(pattern), status)
+      ]
+    else:
+      return [
+          compat.as_str_any(matching_filename)
+          for matching_filename in pywrap_tensorflow.GetMatchingFiles(
+              compat.as_bytes(filename), status)
+      ]
 
 
 def create_dir(dirname):
