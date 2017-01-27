557c5395dbdc7e87137549b355944e53fb6030d8
A. Unique TensorFlower <gardener@tensorflow.org>
2017-01-27 00:33:42 -0800

    Use glob.glob instead of gfile.Glob in system_info_lib.py. Change: 145768535

--- tensorflow/tools/test/system_info_lib.py
+++ tensorflow/tools/test/system_info_lib.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import glob
 import multiprocessing
 import platform
 import re
@@ -104,7 +105,7 @@ def gather_cpu_info():
   try:
     cpu_governors = set([
         gfile.GFile(f, 'r').readline().rstrip()
-        for f in gfile.Glob(
+        for f in glob.glob(
             '/sys/devices/system/cpu/cpu*/cpufreq/scaling_governor')
     ])
     if cpu_governors:
