BREAKING CHANGE: Use `Covariance` where appropriate. BUGFIX: Fix broadcasting in DirichletMultinomial.mean. Change: 145851311

tensorflow/contrib/distributions/python/kernel_tests/multinomial_test.py
@@ -191,18 +191,18 @@ class MultinomialTest(test.TestCase):
       self.assertEqual((3,), dist.mean().get_shape())
       self.assertAllClose(expected_means, dist.mean().eval())
 
-  def testMultinomialVariance(self):
+  def testMultinomialCovariance(self):
     with self.test_session():
       n = 5.
       p = [0.1, 0.2, 0.7]
       dist = ds.Multinomial(total_count=n, probs=p)
-      expected_variances = [[9. / 20, -1 / 10, -7 / 20],
-                            [-1 / 10, 4 / 5, -7 / 10],
-                            [-7 / 20, -7 / 10, 21 / 20]]
-      self.assertEqual((3, 3), dist.variance().get_shape())
-      self.assertAllClose(expected_variances, dist.variance().eval())
+      expected_covariances = [[9. / 20, -1 / 10, -7 / 20],
+                              [-1 / 10, 4 / 5, -7 / 10],
+                              [-7 / 20, -7 / 10, 21 / 20]]
+      self.assertEqual((3, 3), dist.covariance().get_shape())
+      self.assertAllClose(expected_covariances, dist.covariance().eval())
 
-  def testMultinomialVarianceBatch(self):
+  def testMultinomialCovarianceBatch(self):
     with self.test_session():
       # Shape [2]
       n = [5.] * 2
@@ -212,11 +212,11 @@ class MultinomialTest(test.TestCase):
       # Shape [2, 2]
       inner_var = [[9. / 20, -9 / 20], [-9 / 20, 9 / 20]]
       # Shape [4, 2, 2, 2]
-      expected_variances = [[inner_var, inner_var]] * 4
-      self.assertEqual((4, 2, 2, 2), dist.variance().get_shape())
-      self.assertAllClose(expected_variances, dist.variance().eval())
+      expected_covariances = [[inner_var, inner_var]] * 4
+      self.assertEqual((4, 2, 2, 2), dist.covariance().get_shape())
+      self.assertAllClose(expected_covariances, dist.covariance().eval())
 
-  def testVarianceMultidimensional(self):
+  def testCovarianceMultidimensional(self):
     # Shape [3, 5, 4]
     p = np.random.dirichlet([.25, .25, .25, .25], [3, 5]).astype(np.float32)
     # Shape [6, 3, 3]
@@ -229,10 +229,52 @@ class MultinomialTest(test.TestCase):
       dist = ds.Multinomial(ns, p)
       dist2 = ds.Multinomial(ns2, p2)
 
-      variance = dist.variance()
-      variance2 = dist2.variance()
-      self.assertEqual((3, 5, 4, 4), variance.get_shape())
-      self.assertEqual((6, 3, 3, 3), variance2.get_shape())
+      covariance = dist.covariance()
+      covariance2 = dist2.covariance()
+      self.assertEqual((3, 5, 4, 4), covariance.get_shape())
+      self.assertEqual((6, 3, 3, 3), covariance2.get_shape())
+
+  def testCovarianceFromSampling(self):
+    # We will test mean, cov, var, stddev on a DirichletMultinomial constructed
+    # via broadcast between alpha, n.
+    theta = np.array([[1., 2, 3],
+                      [2.5, 4, 0.01]], dtype=np.float32)
+    theta /= np.sum(theta, 1)[..., None]
+    # Ideally we'd be able to test broadcasting but, the multinomial sampler
+    # doesn't support different total counts.
+    n = np.float32(5)
+    with self.test_session() as sess:
+      dist = ds.Multinomial(n, theta)  # batch_shape=[2], event_shape=[3]
+      x = dist.sample(int(250e3), seed=1)
+      sample_mean = math_ops.reduce_mean(x, 0)
+      x_centered = x - sample_mean[None, ...]
+      sample_cov = math_ops.reduce_mean(math_ops.matmul(
+          x_centered[..., None], x_centered[..., None, :]), 0)
+      sample_var = array_ops.matrix_diag_part(sample_cov)
+      sample_stddev = math_ops.sqrt(sample_var)
+      [
+          sample_mean_,
+          sample_cov_,
+          sample_var_,
+          sample_stddev_,
+          analytic_mean,
+          analytic_cov,
+          analytic_var,
+          analytic_stddev,
+      ] = sess.run([
+          sample_mean,
+          sample_cov,
+          sample_var,
+          sample_stddev,
+          dist.mean(),
+          dist.covariance(),
+          dist.variance(),
+          dist.stddev(),
+      ])
+      self.assertAllClose(sample_mean_, analytic_mean, atol=0., rtol=0.01)
+      self.assertAllClose(sample_cov_, analytic_cov, atol=0., rtol=0.01)
+      self.assertAllClose(sample_var_, analytic_var, atol=0., rtol=0.01)
+      self.assertAllClose(sample_stddev_, analytic_stddev, atol=0., rtol=0.01)
 
   def testSampleUnbiasedNonScalarBatch(self):
     with self.test_session() as sess:
@@ -255,7 +297,7 @@ class MultinomialTest(test.TestCase):
           sample_mean,
           sample_covariance,
           dist.mean(),
-          dist.variance(),
+          dist.covariance(),
       ])
       self.assertAllEqual([4, 3, 2], sample_mean.get_shape())
       self.assertAllClose(actual_mean_, sample_mean_, atol=0., rtol=0.07)
@@ -283,7 +325,7 @@ class MultinomialTest(test.TestCase):
           sample_mean,
           sample_covariance,
           dist.mean(),
-          dist.variance(),
+          dist.covariance(),
       ])
       self.assertAllEqual([4], sample_mean.get_shape())
       self.assertAllClose(actual_mean_, sample_mean_, atol=0., rtol=0.07)