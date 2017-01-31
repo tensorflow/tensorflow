35b8855146617eb848ae3de8a878e4aa1bba87bb
Gunhan Gulsoy <gunan@google.com>
2017-01-30 17:46:24 -0800

    Fix python3 failures caused by change 145590579. Change: 146063650

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -228,20 +228,25 @@ def delete_file(filename):
     pywrap_tensorflow.DeleteFile(compat.as_bytes(filename), status)
 
 
-def read_file_to_string(filename):
+def read_file_to_string(filename, binary_mode=False):
   """Reads the entire contents of a file to a string.
 
   Args:
     filename: string, path to a file
+    binary_mode: whether to open the file in binary mode or not. This changes
+        the type of the object returned.
 
   Returns:
-    contents of the file as a string
+    contents of the file as a string or bytes.
 
   Raises:
     errors.OpError: Raises variety of errors that are subtypes e.g.
     NotFoundError etc.
   """
-  f = FileIO(filename, mode="r")
+  if binary_mode:
+    f = FileIO(filename, mode="rb")
+  else:
+    f = FileIO(filename, mode="r")
   return f.read()
 
 
