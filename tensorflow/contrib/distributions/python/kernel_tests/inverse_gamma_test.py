BREAKING CHANGE: Standardize "concentration", "rate", "total_count" distribution arguments. BUGFIX: Correct undefined mode in dirichlet.mode. BUGFIX: Correct broadcasting in dirichletmultinomial.mean. Change: 146187147

tensorflow/contrib/distributions/python/kernel_tests/inverse_gamma_test.py
@@ -33,7 +33,7 @@ class InverseGammaTest(test.TestCase):
     with self.test_session():
       alpha = constant_op.constant([3.0] * 5)
       beta = constant_op.constant(11.0)
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
 
       self.assertEqual(inv_gamma.batch_shape_tensor().eval(), (5,))
       self.assertEqual(inv_gamma.batch_shape,
@@ -50,7 +50,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = 2.0
       beta_v = 3.0
       x = np.array([2.5, 2.5, 4.0, 0.1, 1.0, 2.0], dtype=np.float32)
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
       expected_log_pdf = stats.invgamma.logpdf(x, alpha_v, scale=beta_v)
       log_pdf = inv_gamma.log_prob(x)
       self.assertEqual(log_pdf.get_shape(), (6,))
@@ -68,7 +68,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([2.0, 4.0])
       beta_v = np.array([3.0, 4.0])
       x = np.array([[2.5, 2.5, 4.0, 0.1, 1.0, 2.0]], dtype=np.float32).T
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
       expected_log_pdf = stats.invgamma.logpdf(x, alpha_v, scale=beta_v)
       log_pdf = inv_gamma.log_prob(x)
       log_pdf_values = log_pdf.eval()
@@ -88,7 +88,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([2.0, 4.0])
       beta_v = 3.0
       x = np.array([[2.5, 2.5, 4.0, 0.1, 1.0, 2.0]], dtype=np.float32).T
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
       expected_log_pdf = stats.invgamma.logpdf(x, alpha_v, scale=beta_v)
       log_pdf = inv_gamma.log_prob(x)
       log_pdf_values = log_pdf.eval()
@@ -109,7 +109,7 @@ class InverseGammaTest(test.TestCase):
       beta = constant_op.constant([beta_v] * batch_size)
       x = np.array([2.5, 2.5, 4.0, 0.1, 1.0, 2.0], dtype=np.float32)
 
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
       expected_cdf = stats.invgamma.cdf(x, alpha_v, scale=beta_v)
 
       cdf = inv_gamma.cdf(x)
@@ -120,7 +120,7 @@ class InverseGammaTest(test.TestCase):
     with self.test_session():
       alpha_v = np.array([5.5, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha_v, beta=beta_v)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha_v, rate=beta_v)
       expected_modes = beta_v / (alpha_v + 1)
       self.assertEqual(inv_gamma.mode().get_shape(), (3,))
       self.assertAllClose(inv_gamma.mode().eval(), expected_modes)
@@ -129,7 +129,7 @@ class InverseGammaTest(test.TestCase):
     with self.test_session():
       alpha_v = np.array([5.5, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha_v, beta=beta_v)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha_v, rate=beta_v)
       expected_means = stats.invgamma.mean(alpha_v, scale=beta_v)
       self.assertEqual(inv_gamma.mean().get_shape(), (3,))
       self.assertAllClose(inv_gamma.mean().eval(), expected_means)
@@ -140,7 +140,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([1.0, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, allow_nan_stats=False)
+          concentration=alpha_v, rate=beta_v, allow_nan_stats=False)
       with self.assertRaisesOpError("x < y"):
         inv_gamma.mean().eval()
 
@@ -150,7 +150,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([0.5, 1.0, 3.0, 2.5])
       beta_v = np.array([1.0, 2.0, 4.0, 5.0])
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, allow_nan_stats=True)
+          concentration=alpha_v, rate=beta_v, allow_nan_stats=True)
       expected_means = beta_v / (alpha_v - 1)
       expected_means[0] = np.nan
       expected_means[1] = np.nan
@@ -161,7 +161,7 @@ class InverseGammaTest(test.TestCase):
     with self.test_session():
       alpha_v = np.array([7.0, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha_v, beta=beta_v)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha_v, rate=beta_v)
       expected_variances = stats.invgamma.var(alpha_v, scale=beta_v)
       self.assertEqual(inv_gamma.variance().get_shape(), (3,))
       self.assertAllClose(inv_gamma.variance().eval(), expected_variances)
@@ -171,7 +171,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([1.5, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, allow_nan_stats=False)
+          concentration=alpha_v, rate=beta_v, allow_nan_stats=False)
       with self.assertRaisesOpError("x < y"):
         inv_gamma.variance().eval()
 
@@ -180,7 +180,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([1.5, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, allow_nan_stats=True)
+          concentration=alpha_v, rate=beta_v, allow_nan_stats=True)
       expected_variances = stats.invgamma.var(alpha_v, scale=beta_v)
       expected_variances[0] = np.nan
       self.assertEqual(inv_gamma.variance().get_shape(), (3,))
@@ -191,7 +191,7 @@ class InverseGammaTest(test.TestCase):
       alpha_v = np.array([1.0, 3.0, 2.5])
       beta_v = np.array([1.0, 4.0, 5.0])
       expected_entropy = stats.invgamma.entropy(alpha_v, scale=beta_v)
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha_v, beta=beta_v)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha_v, rate=beta_v)
       self.assertEqual(inv_gamma.entropy().get_shape(), (3,))
       self.assertAllClose(inv_gamma.entropy().eval(), expected_entropy)
 
@@ -202,7 +202,7 @@ class InverseGammaTest(test.TestCase):
       alpha = constant_op.constant(alpha_v)
       beta = constant_op.constant(beta_v)
       n = 100000
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha, beta=beta)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha, rate=beta)
       samples = inv_gamma.sample(n, seed=137)
       sample_values = samples.eval()
       self.assertEqual(samples.get_shape(), (n,))
@@ -222,7 +222,7 @@ class InverseGammaTest(test.TestCase):
     with session.Session():
       alpha_v = np.array([np.arange(3, 103, dtype=np.float32)])  # 1 x 100
       beta_v = np.array([np.arange(1, 11, dtype=np.float32)]).T  # 10 x 1
-      inv_gamma = inverse_gamma.InverseGamma(alpha=alpha_v, beta=beta_v)
+      inv_gamma = inverse_gamma.InverseGamma(concentration=alpha_v, rate=beta_v)
       n = 10000
       samples = inv_gamma.sample(n, seed=137)
       sample_values = samples.eval()
@@ -257,7 +257,9 @@ class InverseGammaTest(test.TestCase):
 
   def testInverseGammaPdfOfSampleMultiDims(self):
     with session.Session() as sess:
-      inv_gamma = inverse_gamma.InverseGamma(alpha=[7., 11.], beta=[[5.], [6.]])
+      inv_gamma = inverse_gamma.InverseGamma(
+          concentration=[7., 11.],
+          rate=[[5.], [6.]])
       num = 50000
       samples = inv_gamma.sample(num, seed=137)
       pdfs = inv_gamma.prob(samples)
@@ -294,24 +296,26 @@ class InverseGammaTest(test.TestCase):
       alpha_v = constant_op.constant(0.0, name="alpha")
       beta_v = constant_op.constant(1.0, name="beta")
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, validate_args=True)
+          concentration=alpha_v, rate=beta_v, validate_args=True)
       with self.assertRaisesOpError("alpha"):
         inv_gamma.mean().eval()
       alpha_v = constant_op.constant(1.0, name="alpha")
       beta_v = constant_op.constant(0.0, name="beta")
       inv_gamma = inverse_gamma.InverseGamma(
-          alpha=alpha_v, beta=beta_v, validate_args=True)
+          concentration=alpha_v, rate=beta_v, validate_args=True)
       with self.assertRaisesOpError("beta"):
         inv_gamma.mean().eval()
 
-  def testInverseGammaWithSoftplusAlphaBeta(self):
+  def testInverseGammaWithSoftplusConcentrationRate(self):
     with self.test_session():
       alpha = constant_op.constant([-0.1, -2.9], name="alpha")
       beta = constant_op.constant([1.0, -4.8], name="beta")
-      inv_gamma = inverse_gamma.InverseGammaWithSoftplusAlphaBeta(
-          alpha=alpha, beta=beta, validate_args=True)
-      self.assertAllClose(nn_ops.softplus(alpha).eval(), inv_gamma.alpha.eval())
-      self.assertAllClose(nn_ops.softplus(beta).eval(), inv_gamma.beta.eval())
+      inv_gamma = inverse_gamma.InverseGammaWithSoftplusConcentrationRate(
+          concentration=alpha, rate=beta, validate_args=True)
+      self.assertAllClose(nn_ops.softplus(alpha).eval(),
+                          inv_gamma.concentration.eval())
+      self.assertAllClose(nn_ops.softplus(beta).eval(),
+                          inv_gamma.rate.eval())
 
 
 if __name__ == "__main__":

tensorflow/contrib/distributions/python/kernel_tests/quantized_distribution_test.py
@@ -201,7 +201,7 @@ class QuantizedDistributionTest(test.TestCase):
     # integers.  Hence, F(X) (see below) will not be uniform exactly.
     with self.test_session():
       qdist = distributions.QuantizedDistribution(
-          distribution=distributions.Exponential(lam=0.01))
+          distribution=distributions.Exponential(rate=0.01))
       # X ~ QuantizedExponential
       x = qdist.sample(10000, seed=42)
       # Z = F(X), should be Uniform.
@@ -224,7 +224,7 @@ class QuantizedDistributionTest(test.TestCase):
     # Make an exponential with mean 5.
     with self.test_session():
       qdist = distributions.QuantizedDistribution(
-          distribution=distributions.Exponential(lam=0.2))
+          distribution=distributions.Exponential(rate=0.2))
       # Standard error should be less than 1 / (2 * sqrt(n_samples))
       n_samples = 10000
       stddev_err_bound = 1 / (2 * np.sqrt(n_samples))

tensorflow/contrib/distributions/python/kernel_tests/wishart_test.py
@@ -67,10 +67,13 @@ class WishartCholeskyTest(test.TestCase):
     with self.test_session():
 
       def entropy_alt(w):
-        return (w.log_normalizing_constant() - 0.5 * (w.df - w.dimension - 1.) *
-                w.mean_log_det() + 0.5 * w.df * w.dimension).eval()
+        return (
+            w.log_normalization()
+            - 0.5 * (w.df - w.dimension - 1.) * w.mean_log_det()
+            + 0.5 * w.df * w.dimension).eval()
 
-      w = distributions.WishartCholesky(df=4, scale=chol(make_pd(1., 2)))
+      w = distributions.WishartCholesky(df=4,
+                                        scale=chol(make_pd(1., 2)))
       self.assertAllClose(w.entropy().eval(), entropy_alt(w))
 
       w = distributions.WishartCholesky(df=5, scale=[[1.]])
@@ -204,7 +207,9 @@ class WishartCholeskyTest(test.TestCase):
 
       # This test checks that batches don't interfere with correctness.
       w = distributions.WishartCholesky(
-          df=[2, 3, 4, 5], scale=chol_x, cholesky_input_output_matrices=True)
+          df=[2, 3, 4, 5],
+          scale=chol_x,
+          cholesky_input_output_matrices=True)
       self.assertAllClose(log_prob_df_seq, w.log_prob(chol_x).eval())
 
       # Now we test various constructions of Wishart with different sample
@@ -221,10 +226,15 @@ class WishartCholeskyTest(test.TestCase):
           -20.951582705289454,
       ])
 
-      for w in (distributions.WishartCholesky(
-          df=4, scale=chol_x[0], cholesky_input_output_matrices=False),
-                distributions.WishartFull(
-                    df=4, scale=x[0], cholesky_input_output_matrices=False)):
+      for w in (
+          distributions.WishartCholesky(
+              df=4,
+              scale=chol_x[0],
+              cholesky_input_output_matrices=False),
+          distributions.WishartFull(
+              df=4,
+              scale=x[0],
+              cholesky_input_output_matrices=False)):
         self.assertAllEqual((2, 2), w.event_shape_tensor().eval())
         self.assertEqual(2, w.dimension.eval())
         self.assertAllClose(log_prob[0], w.log_prob(x[0]).eval())
@@ -238,10 +248,15 @@ class WishartCholeskyTest(test.TestCase):
         self.assertAllEqual((2, 2),
                             w.log_prob(np.reshape(x, (2, 2, 2, 2))).get_shape())
 
-      for w in (distributions.WishartCholesky(
-          df=4, scale=chol_x[0], cholesky_input_output_matrices=True),
-                distributions.WishartFull(
-                    df=4, scale=x[0], cholesky_input_output_matrices=True)):
+      for w in (
+          distributions.WishartCholesky(
+              df=4,
+              scale=chol_x[0],
+              cholesky_input_output_matrices=True),
+          distributions.WishartFull(
+              df=4,
+              scale=x[0],
+              cholesky_input_output_matrices=True)):
         self.assertAllEqual((2, 2), w.event_shape_tensor().eval())
         self.assertEqual(2, w.dimension.eval())
         self.assertAllClose(log_prob[0], w.log_prob(chol_x[0]).eval())
@@ -315,7 +330,9 @@ class WishartCholeskyTest(test.TestCase):
       with self.assertRaisesRegexp(errors_impl.InvalidArgumentError,
                                    "cannot be less than"):
         chol_w = distributions.WishartCholesky(
-            df=df_deferred, scale=chol_scale_deferred, validate_args=True)
+            df=df_deferred,
+            scale=chol_scale_deferred,
+            validate_args=True)
         sess.run(chol_w.log_prob(np.asarray(
             x, dtype=np.float32)),
                  feed_dict={df_deferred: 2.,
@@ -336,7 +353,9 @@ class WishartCholeskyTest(test.TestCase):
 
       # Ensure no assertions.
       chol_w = distributions.WishartCholesky(
-          df=df_deferred, scale=chol_scale_deferred, validate_args=False)
+          df=df_deferred,
+          scale=chol_scale_deferred,
+          validate_args=False)
       sess.run(chol_w.log_prob(np.asarray(
           x, dtype=np.float32)),
                feed_dict={df_deferred: 4,

tensorflow/contrib/distributions/python/ops/dirichlet_multinomial.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""The Dirichlet Multinomial distribution class."""
+"""The DirichletMultinomial distribution class."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -30,52 +30,76 @@ from tensorflow.python.ops import random_ops
 from tensorflow.python.ops import special_math_ops
 
 
-_dirichlet_multinomial_prob_note = """
-For each batch of counts `[n_1,...,n_k]`, `P[counts]` is the probability
-that after sampling `n` draws from this Dirichlet Multinomial
-distribution, the number of draws falling in class `j` is `n_j`.  Note that
-different sequences of draws can result in the same counts, thus the
-probability includes a combinatorial coefficient.
+__all__ = [
+    "DirichletMultinomial",
+]
 
-Note that input, "counts", must be a non-negative tensor with dtype `dtype`
-and whose shape can be broadcast with `self.alpha`.  For fixed leading
-dimensions, the last dimension represents counts for the corresponding
-Dirichlet Multinomial distribution in `self.alpha`. `counts` is only legal if
-it sums up to `n` and its components are equal to integer values.
-"""
+
+_dirichlet_multinomial_sample_note = """For each batch of counts,
+`value = [n_0, ... ,n_{k-1}]`, `P[value]` is the probability that after sampling
+`self.total_count` draws from this Dirichlet-Multinomial distribution, the
+number of draws falling in class `j` is `n_j`. Since this definition is
+[exchangeable]( https://en.wikipedia.org/wiki/Exchangeable_random_variables);
+different sequences have the same counts so the probability includes a
+combinatorial coefficient.
+
+Note: `value` must be a non-negative tensor with dtype `self.dtype`, have no
+fractional components, and such that
+`tf.reduce_sum(value, -1) = self.total_count`. Its shape must be broadcastable
+with `self.concentration` and `self.total_count`."""
 
 
 class DirichletMultinomial(distribution.Distribution):
-  """DirichletMultinomial mixture distribution.
+  """Dirichlet-Multinomial compound distribution.
 
-  This distribution is parameterized by a vector `alpha` of concentration
-  parameters for `k` classes and `n`, the counts per each class..
+  The Dirichlet-Multinomial distribution is parameterized by a (batch of)
+  length-`k` `concentration` vectors (`k > 1`) and a `total_count` number of
+  trials, i.e., the number of trials per draw from the DirichletMultinomial. It
+  is defined over a (batch of) length-`k` vector `counts` such that
+  `tf.reduce_sum(counts, -1) = total_count`. The Dirichlet-Multinomial is
+  identically the Beta-Binomial distribution when `k = 2`.
 
-  #### Mathematical details
+  #### Mathematical Details
 
-  The Dirichlet Multinomial is a distribution over k-class count data, meaning
-  for each k-tuple of non-negative integer `counts = [c_1,...,c_k]`, we have a
-  probability of these draws being made from the distribution.  The distribution
-  has hyperparameters `alpha = (alpha_1,...,alpha_k)`, and probability mass
-  function (pmf):
+  The Dirichlet-Multinomial is a distribution over `k`-class counts, i.e., a
+  length-`k` vector of non-negative integer `counts = n = [n_0, ..., n_{k-1}]`.
 
-  ```pmf(counts) = N! / (n_1!...n_k!) * Beta(alpha + c) / Beta(alpha)```
+  The probability mass function (pmf) is,
 
-  where above `N = sum_j n_j`, `N!` is `N` factorial, and
-  `Beta(x) = prod_j Gamma(x_j) / Gamma(sum_j x_j)` is the multivariate beta
-  function.
+  ```none
+  pmf(n; alpha, N) = Beta(alpha + n) / (prod_j n_j!) / Z
+  Z = Beta(alpha) / N!
+  ```
 
-  This is a mixture distribution in that `M` samples can be produced by:
-    1. Choose class probabilities `p = (p_1,...,p_k) ~ Dir(alpha)`
-    2. Draw integers `m = (n_1,...,n_k) ~ Multinomial(N, p)`
+  where:
 
-  This class provides methods to create indexed batches of Dirichlet
-  Multinomial distributions.  If the provided `alpha` is rank 2 or higher, for
-  every fixed set of leading dimensions, the last dimension represents one
-  single Dirichlet Multinomial distribution.  When calling distribution
-  functions (e.g. `dist.prob(counts)`), `alpha` and `counts` are broadcast to
-  the same shape (if possible).  In all cases, the last dimension of
-  alpha/counts represents single Dirichlet Multinomial distributions.
+  * `concentration = alpha = [alpha_0, ..., alpha_{k-1}]`, `alpha_j > 0`,
+  * `total_count = N`, `N` a positive integer,
+  * `N!` is `N` factorial, and,
+  * `Beta(x) = prod_j Gamma(x_j) / Gamma(sum_j x_j)` is the
+    [multivariate beta function](
+    https://en.wikipedia.org/wiki/Beta_function#Multivariate_beta_function),
+    and,
+  * `Gamma` is the [gamma function](
+    https://en.wikipedia.org/wiki/Gamma_function).
+
+  Dirichlet-Multinomial is a [compound distribution](
+  https://en.wikipedia.org/wiki/Compound_probability_distribution), i.e., its
+  samples are generated as follows.
+
+    1. Choose class probabilities:
+       `probs = [p_0,...,p_{k-1}] ~ Dir(concentration)`
+    2. Draw integers:
+       `counts = [n_0,...,n_{k-1}] ~ Multinomial(total_count, probs)`
+
+  The last `concentration` dimension parametrizes a single Dirichlet-Multinomial
+  distribution. When calling distribution functions (e.g., `dist.prob(counts)`),
+  `concentration`, `total_count` and `counts` are broadcast to the same shape.
+  The last dimension of of `counts` corresponds single Dirichlet-Multinomial
+  distributions.
+
+  Distribution parameters are automatically broadcast in all functions; see
+  examples for details.
 
   #### Examples
 
@@ -116,116 +140,102 @@ class DirichletMultinomial(distribution.Distribution):
 
   """
 
-  # TODO(b/27419586) Change docstring for dtype of alpha once int allowed.
+  # TODO(b/27419586) Change docstring for dtype of concentration once int
+  # allowed.
   def __init__(self,
-               n,
-               alpha,
+               total_count,
+               concentration,
                validate_args=False,
                allow_nan_stats=True,
                name="DirichletMultinomial"):
     """Initialize a batch of DirichletMultinomial distributions.
 
     Args:
-      n:  Non-negative floating point tensor, whose dtype is the same as
-        `alpha`. The shape is broadcastable to `[N1,..., Nm]` with `m >= 0`.
-        Defines this as a batch of `N1 x ... x Nm` different Dirichlet
-        multinomial distributions. Its components should be equal to integer
-        values.
-      alpha: Positive floating point tensor, whose dtype is the same as
-        `n` with shape broadcastable to `[N1,..., Nm, k]` `m >= 0`.  Defines
-        this as a batch of `N1 x ... x Nm` different `k` class Dirichlet
+      total_count:  Non-negative floating point tensor, whose dtype is the same
+        as `concentration`. The shape is broadcastable to `[N1,..., Nm]` with
+        `m >= 0`.  Defines this as a batch of `N1 x ... x Nm` different
+        Dirichlet multinomial distributions. Its components should be equal to
+        integer values.
+      concentration: Positive floating point tensor, whose dtype is the
+        same as `n` with shape broadcastable to `[N1,..., Nm, k]` `m >= 0`.
+        Defines this as a batch of `N1 x ... x Nm` different `k` class Dirichlet
         multinomial distributions.
-      validate_args: `Boolean`, default `False`.  Whether to assert valid
-        values for parameters `alpha` and `n`, and `x` in `prob` and
-        `log_prob`.  If `False`, correct behavior is not guaranteed.
-      allow_nan_stats: `Boolean`, default `True`.  If `False`, raise an
-        exception if a statistic (e.g. mean/mode/etc...) is undefined for any
-        batch member.  If `True`, batch members with valid parameters leading to
-        undefined statistics will return NaN for this statistic.
-      name: The name to prefix Ops created by this distribution class.
-
-    Examples:
-
-    ```python
-    # Define 1-batch of 2-class Dirichlet multinomial distribution,
-    # also known as a beta-binomial.
-    dist = DirichletMultinomial(2.0, [1.1, 2.0])
-
-    # Define a 2-batch of 3-class distributions.
-    dist = DirichletMultinomial([3., 4], [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-    ```
-
+      validate_args: Python `Boolean`, default `False`. When `True` distribution
+        parameters are checked for validity despite possibly degrading runtime
+        performance. When `False` invalid inputs may silently render incorrect
+        outputs.
+      allow_nan_stats: Python `Boolean`, default `True`. When `True`, statistics
+        (e.g., mean, mode, variance) use the value "`NaN`" to indicate the
+        result is undefined.  When `False`, an exception is raised if one or
+        more of the statistic's batch members are undefined.
+      name: `String` name prefixed to Ops created by this class.
     """
     parameters = locals()
     parameters.pop("self")
-    with ops.name_scope(name, values=[n, alpha]) as ns:
+    with ops.name_scope(name, values=[total_count, concentration]) as ns:
       # Broadcasting works because:
       # * The broadcasting convention is to prepend dimensions of size [1], and
-      #   we use the last dimension for the distribution, wherease
+      #   we use the last dimension for the distribution, whereas
       #   the batch dimensions are the leading dimensions, which forces the
       #   distribution dimension to be defined explicitly (i.e. it cannot be
       #   created automatically by prepending).  This forces enough
-      #   explicitivity.
-      #   * All calls involving `counts` eventually require a broadcast between
-      #   `counts` and alpha.
-      self._alpha = self._assert_valid_alpha(alpha, validate_args)
-      self._n = self._assert_valid_n(n, validate_args)
-      self._alpha_sum = math_ops.reduce_sum(
-          self._alpha, reduction_indices=[-1], keep_dims=False)
+      #   explicitness.
+      # * All calls involving `counts` eventually require a broadcast between
+      #  `counts` and concentration.
+      self._total_count = self._maybe_assert_valid_total_count(
+          ops.convert_to_tensor(total_count, name="total_count"),
+          validate_args)
+      self._concentration = self._maybe_assert_valid_concentration(
+          ops.convert_to_tensor(concentration,
+                                name="concentration"),
+          validate_args)
+      self._total_concentration = math_ops.reduce_sum(self._concentration, -1)
     super(DirichletMultinomial, self).__init__(
-        dtype=self._alpha.dtype,
-        is_continuous=False,
-        reparameterization_type=distribution.NOT_REPARAMETERIZED,
+        dtype=self._concentration.dtype,
         validate_args=validate_args,
         allow_nan_stats=allow_nan_stats,
+        is_continuous=False,
+        reparameterization_type=distribution.NOT_REPARAMETERIZED,
         parameters=parameters,
-        graph_parents=[self._alpha, self._n, self._alpha_sum],
+        graph_parents=[self._total_count,
+                       self._concentration],
         name=ns)
 
   @property
-  def n(self):
-    """Parameter defining this distribution."""
-    return self._n
+  def total_count(self):
+    """Number of trials used to construct a sample."""
+    return self._total_count
 
   @property
-  def alpha(self):
-    """Parameter defining this distribution."""
-    return self._alpha
+  def concentration(self):
+    """Concentration parameter; expected prior counts for that coordinate."""
+    return self._concentration
 
   @property
-  def alpha_sum(self):
-    """Summation of alpha parameter."""
-    return self._alpha_sum
+  def total_concentration(self):
+    """Sum of last dim of concentration parameter."""
+    return self._total_concentration
 
   def _batch_shape_tensor(self):
-    return array_ops.shape(self.alpha_sum)
+    return array_ops.shape(self.total_concentration)
 
   def _batch_shape(self):
-    return self.alpha_sum.get_shape()
+    return self.total_concentration.get_shape()
 
   def _event_shape_tensor(self):
-    return array_ops.reverse_v2(array_ops.shape(self.alpha), [0])[0]
+    return array_ops.shape(self.concentration)[-1:]
 
   def _event_shape(self):
-    # Event shape depends only on alpha, not "n".
-    return self.alpha.get_shape().with_rank_at_least(1)[-1:]
+    # Event shape depends only on total_concentration, not "n".
+    return self.concentration.get_shape().with_rank_at_least(1)[-1:]
 
   def _sample_n(self, n, seed=None):
-    n_draws = math_ops.cast(self.n, dtype=dtypes.int32)
-    if self.n.get_shape().ndims is not None:
-      if self.n.get_shape().ndims != 0:
-        raise NotImplementedError(
-            "Sample only supported for scalar number of draws.")
-    elif self.validate_args:
-      is_scalar = check_ops.assert_rank(
-          n_draws, 0,
-          message="Sample only supported for scalar number of draws.")
-      n_draws = control_flow_ops.with_dependencies([is_scalar], n_draws)
+    n_draws = math_ops.cast(self.total_count, dtype=dtypes.int32)
     k = self.event_shape_tensor()[0]
     unnormalized_logits = array_ops.reshape(
         math_ops.log(random_ops.random_gamma(
             shape=[n],
-            alpha=self.alpha,
+            alpha=self.concentration,
             dtype=self.dtype,
             seed=seed)),
         shape=[-1, k])
@@ -233,40 +243,41 @@ class DirichletMultinomial(distribution.Distribution):
         logits=unnormalized_logits,
         num_samples=n_draws,
         seed=distribution_util.gen_new_seed(seed, salt="dirichlet_multinomial"))
-    x = math_ops.reduce_sum(array_ops.one_hot(draws, depth=k),
-                            reduction_indices=-2)
+    x = math_ops.reduce_sum(array_ops.one_hot(draws, depth=k), -2)
     final_shape = array_ops.concat([[n], self.batch_shape_tensor(), [k]], 0)
     return array_ops.reshape(x, final_shape)
 
-  @distribution_util.AppendDocstring(_dirichlet_multinomial_prob_note)
+  @distribution_util.AppendDocstring(_dirichlet_multinomial_sample_note)
   def _log_prob(self, counts):
-    counts = self._assert_valid_counts(counts)
-    ordered_prob = (special_math_ops.lbeta(self.alpha + counts) -
-                    special_math_ops.lbeta(self.alpha))
-    log_prob = ordered_prob + distribution_util.log_combinations(
-        self.n, counts)
-    return log_prob
+    counts = self._maybe_assert_valid_sample(counts)
+    ordered_prob = (
+        special_math_ops.lbeta(self.concentration + counts)
+        - special_math_ops.lbeta(self.concentration))
+    return ordered_prob + distribution_util.log_combinations(
+        self.total_count, counts)
 
-  @distribution_util.AppendDocstring(_dirichlet_multinomial_prob_note)
+  @distribution_util.AppendDocstring(_dirichlet_multinomial_sample_note)
   def _prob(self, counts):
     return math_ops.exp(self._log_prob(counts))
 
   def _mean(self):
-    return self.n * (self.alpha / self.alpha_sum[..., None])
+    return self.total_count * (self.concentration /
+                               self.total_concentration[..., None])
 
   @distribution_util.AppendDocstring(
       """The covariance for each batch member is defined as the following:
 
-      ```
+      ```none
       Var(X_j) = n * alpha_j / alpha_0 * (1 - alpha_j / alpha_0) *
       (n + alpha_0) / (1 + alpha_0)
       ```
 
-      where `alpha_0 = sum_j alpha_j`.
+      where `concentration = alpha` and
+      `total_concentration = alpha_0 = sum_j alpha_j`.
 
       The covariance between elements in a batch is defined as:
 
-      ```
+      ```none
       Cov(X_i, X_j) = -n * alpha_i * alpha_j / alpha_0 ** 2 *
       (n + alpha_0) / (1 + alpha_0)
       ```
@@ -280,40 +291,55 @@ class DirichletMultinomial(distribution.Distribution):
   def _variance(self):
     scale = self._variance_scale_term()
     x = scale * self._mean()
-    return x * (self.n * scale - x)
+    return x * (self.total_count * scale - x)
 
   def _variance_scale_term(self):
     """Helper to `_covariance` and `_variance` which computes a shared scale."""
     # We must take care to expand back the last dim whenever we use the
-    # alpha_sum.
-    c0 = self.alpha_sum[..., None]
-    return math_ops.sqrt((1. + c0 / self.n) / (1. + c0))
+    # total_concentration.
+    c0 = self.total_concentration[..., None]
+    return math_ops.sqrt((1. + c0 / self.total_count) / (1. + c0))
 
-  def _assert_valid_counts(self, counts):
+  def _maybe_assert_valid_concentration(self, concentration, validate_args):
+    """Checks the validity of the concentration parameter."""
+    if not validate_args:
+      return concentration
+    return control_flow_ops.with_dependencies([
+        check_ops.assert_positive(
+            concentration,
+            message="Concentration parameter must be positive."),
+        check_ops.assert_rank_at_least(
+            concentration, 1,
+            message="Concentration parameter must have >=1 dimensions."),
+        check_ops.assert_less(
+            1, array_ops.shape(concentration)[-1],
+            message="Concentration parameter must have event_size >= 2."),
+    ], concentration)
+
+  def _maybe_assert_valid_total_count(self, total_count, validate_args):
+    if not validate_args:
+      return total_count
+    return control_flow_ops.with_dependencies([
+        check_ops.assert_non_negative(
+            total_count,
+            message="total_count must be non-negative."),
+        distribution_util.assert_integer_form(
+            total_count,
+            message="total_count cannot contain fractional values."),
+    ], total_count)
+
+  def _maybe_assert_valid_sample(self, counts):
     """Check counts for proper shape, values, then return tensor version."""
-    counts = ops.convert_to_tensor(counts, name="counts")
     if not self.validate_args:
       return counts
-    candidate_n = math_ops.reduce_sum(counts, reduction_indices=[-1])
     return control_flow_ops.with_dependencies([
-        check_ops.assert_non_negative(counts),
+        check_ops.assert_non_negative(
+            counts,
+            message="counts must be non-negative."),
         check_ops.assert_equal(
-            self._n, candidate_n,
-            message="counts do not sum to n"),
-        distribution_util.assert_integer_form(counts)], counts)
-
-  def _assert_valid_alpha(self, alpha, validate_args):
-    alpha = ops.convert_to_tensor(alpha, name="alpha")
-    if not validate_args:
-      return alpha
-    return control_flow_ops.with_dependencies(
-        [check_ops.assert_rank_at_least(alpha, 1),
-         check_ops.assert_positive(alpha)], alpha)
-
-  def _assert_valid_n(self, n, validate_args):
-    n = ops.convert_to_tensor(n, name="n")
-    if not validate_args:
-      return n
-    return control_flow_ops.with_dependencies(
-        [check_ops.assert_non_negative(n),
-         distribution_util.assert_integer_form(n)], n)
+            self.total_count, math_ops.reduce_sum(counts, -1),
+            message="counts last-dimension must sum to `self.total_count`"),
+        distribution_util.assert_integer_form(
+            counts,
+            message="counts cannot contain fractional components."),
+    ], counts)

tensorflow/contrib/distributions/python/ops/wishart.py
@@ -38,6 +38,12 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import random_ops
 
 
+__all__ = [
+    "WishartCholesky",
+    "WishartFull",
+]
+
+
 class _WishartOperatorPD(distribution.Distribution):
   """The matrix Wishart distribution on positive definite matrices.
 
@@ -45,22 +51,22 @@ class _WishartOperatorPD(distribution.Distribution):
   an instance of `OperatorPDBase`, which provides matrix-free access to a
   symmetric positive definite operator, which defines the scale matrix.
 
-  #### Mathematical details.
+  #### Mathematical Details
 
-  The PDF of this distribution is,
+  The probability density function (pdf) is,
 
-  ```
-  f(X) = det(X)^(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / B(scale, df)
-  ```
-
-  where `df >= k` denotes the degrees of freedom, `scale` is a symmetric, pd,
-  `k x k` matrix, and the normalizing constant `B(scale, df)` is given by:
-
-  ```
-  B(scale, df) = 2^(0.5 df k) |det(scale)|^(0.5 df) Gamma_k(0.5 df)
+  ```none
+  pdf(X; df, scale) = det(X)**(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / Z
+  Z = 2**(0.5 df k) |det(scale)|**(0.5 df) Gamma_k(0.5 df)
   ```
 
-  where `Gamma_k` is the multivariate Gamma function.
+  where:
+
+  * `df >= k` denotes the degrees of freedom,
+  * `scale` is a symmetric, positive definite, `k x k` matrix,
+  * `Z` is the normalizing constant, and,
+  * `Gamma_k` is the [multivariate Gamma function](
+    https://en.wikipedia.org/wiki/Multivariate_gamma_function).
 
   #### Examples
 
@@ -86,19 +92,21 @@ class _WishartOperatorPD(distribution.Distribution):
         Cholesky factored matrix. Example `log_prob` input takes a Cholesky and
         `sample_n` returns a Cholesky when
         `cholesky_input_output_matrices=True`.
-      validate_args: `Boolean`, default `False`.  Whether to validate input with
-        asserts. If `validate_args` is `False`, and the inputs are invalid,
-        correct behavior is not guaranteed.
-      allow_nan_stats: `Boolean`, default `True`. If `False`, raise an
-        exception if a statistic (e.g., mean, mode) is undefined for any batch
-        member. If True, batch members with valid parameters leading to
-        undefined statistics will return `NaN` for this statistic.
-      name: The name to give Ops created by the initializer.
+      validate_args: Python `Boolean`, default `False`. When `True` distribution
+        parameters are checked for validity despite possibly degrading runtime
+        performance. When `False` invalid inputs may silently render incorrect
+        outputs.
+      allow_nan_stats: Python `Boolean`, default `True`. When `True`, statistics
+        (e.g., mean, mode, variance) use the value "`NaN`" to indicate the
+        result is undefined.  When `False`, an exception is raised if one or
+        more of the statistic's batch members are undefined.
+      name: `String` name prefixed to Ops created by this class.
 
     Raises:
       TypeError: if scale is not floating-type
       TypeError: if scale.dtype != df.dtype
-      ValueError: if df < k, where scale operator event shape is `(k, k)`
+      ValueError: if df < k, where scale operator event shape is
+        `(k, k)`
     """
     parameters = locals()
     parameters.pop("self")
@@ -111,7 +119,9 @@ class _WishartOperatorPD(distribution.Distribution):
               scale_operator_pd.dtype)
         self._scale_operator_pd = scale_operator_pd
         self._df = ops.convert_to_tensor(
-            df, dtype=scale_operator_pd.dtype, name="df")
+            df,
+            dtype=scale_operator_pd.dtype,
+            name="df")
         contrib_tensor_util.assert_same_float_dtype(
             (self._df, self._scale_operator_pd))
         if (self._scale_operator_pd.get_shape().ndims is None or
@@ -127,19 +137,22 @@ class _WishartOperatorPD(distribution.Distribution):
         dim_val = tensor_util.constant_value(self._dimension)
         if df_val is not None and dim_val is not None:
           df_val = np.asarray(df_val)
-          if not df_val.shape: df_val = (df_val,)
+          if not df_val.shape:
+            df_val = [df_val]
           if any(df_val < dim_val):
             raise ValueError(
-                "Degrees of freedom (df = %s) cannot be less than dimension of "
-                "scale matrix (scale.dimension = %s)"
+                "Degrees of freedom (df = %s) cannot be less than "
+                "dimension of scale matrix (scale.dimension = %s)"
                 % (df_val, dim_val))
         elif validate_args:
           assertions = check_ops.assert_less_equal(
               self._dimension, self._df,
-              message=("Degrees of freedom (df = %s) cannot be less than "
-                       "dimension of scale matrix (scale.dimension = %s)" %
+              message=("Degrees of freedom (df = %s) cannot be "
+                       "less than dimension of scale matrix "
+                       "(scale.dimension = %s)" %
                        (self._dimension, self._df)))
-          self._df = control_flow_ops.with_dependencies([assertions], self._df)
+          self._df = control_flow_ops.with_dependencies(
+              [assertions], self._df)
     super(_WishartOperatorPD, self).__init__(
         dtype=self._scale_operator_pd.dtype,
         validate_args=validate_args,
@@ -321,7 +334,7 @@ class _WishartOperatorPD(distribution.Distribution):
     # Complexity: O(nbk^2)
     log_prob = ((self.df - self.dimension - 1.) * half_log_det_x -
                 0.5 * trace_scale_inv_x -
-                self.log_normalizing_constant())
+                self.log_normalization())
 
     # Set shape hints.
     # Try to merge what we know from the input then what we know from the
@@ -349,7 +362,8 @@ class _WishartOperatorPD(distribution.Distribution):
 
   def _mean(self):
     if self.cholesky_input_output_matrices:
-      return math_ops.sqrt(self.df) * self.scale_operator_pd.sqrt_to_dense()
+      return (math_ops.sqrt(self.df)
+              * self.scale_operator_pd.sqrt_to_dense())
     return self.df * self.scale_operator_pd.to_dense()
 
   def _variance(self):
@@ -384,7 +398,7 @@ class _WishartOperatorPD(distribution.Distribution):
               self.dimension * math.log(2.) +
               self.scale_operator_pd.log_det())
 
-  def log_normalizing_constant(self, name="log_normalizing_constant"):
+  def log_normalization(self, name="log_normalization"):
     """Computes the log normalizing constant, log(Z)."""
     with self._name_scope(name):
       return (self.df * self.scale_operator_pd.sqrt_log_det() +
@@ -429,22 +443,21 @@ class WishartCholesky(_WishartOperatorPD):
   another O(nbk^3) operation since most uses of Wishart will also use the
   Cholesky factorization.
 
-  #### Mathematical details.
+  #### Mathematical Details
 
-  The PDF of this distribution is,
+  The probability density function (pdf) is,
 
-  ```
-  f(X) = det(X)^(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / B(scale, df)
-  ```
-
-  where `df >= k` denotes the degrees of freedom, `scale` is a symmetric, pd,
-  `k x k` matrix, and the normalizing constant `B(scale, df)` is given by:
-
-  ```
-  B(scale, df) = 2^(0.5 df k) |det(scale)|^(0.5 df) Gamma_k(0.5 df)
+  ```none
+  pdf(X; df, scale) = det(X)**(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / Z
+  Z = 2**(0.5 df k) |det(scale)|**(0.5 df) Gamma_k(0.5 df)
   ```
 
-  where `Gamma_k` is the multivariate Gamma function.
+  where:
+  * `df >= k` denotes the degrees of freedom,
+  * `scale` is a symmetric, positive definite, `k x k` matrix,
+  * `Z` is the normalizing constant, and,
+  * `Gamma_k` is the [multivariate Gamma function](
+    https://en.wikipedia.org/wiki/Multivariate_gamma_function).
 
 
   #### Examples
@@ -499,14 +512,15 @@ class WishartCholesky(_WishartOperatorPD):
         Cholesky factored matrix. Example `log_prob` input takes a Cholesky and
         `sample_n` returns a Cholesky when
         `cholesky_input_output_matrices=True`.
-      validate_args: `Boolean`, default `False`.  Whether to validate input
-        with asserts. If `validate_args` is `False`, and the inputs are invalid,
-        correct behavior is not guaranteed.
-      allow_nan_stats: `Boolean`, default `True`. If `False`, raise an
-        exception if a statistic (e.g., mean, mode) is undefined for any batch
-        member. If True, batch members with valid parameters leading to
-        undefined statistics will return `NaN` for this statistic.
-      name: The name scope to give class member ops.
+      validate_args: Python `Boolean`, default `False`. When `True` distribution
+        parameters are checked for validity despite possibly degrading runtime
+        performance. When `False` invalid inputs may silently render incorrect
+        outputs.
+      allow_nan_stats: Python `Boolean`, default `True`. When `True`, statistics
+        (e.g., mean, mode, variance) use the value "`NaN`" to indicate the
+        result is undefined.  When `False`, an exception is raised if one or
+        more of the statistic's batch members are undefined.
+      name: `String` name prefixed to Ops created by this class.
     """
     parameters = locals()
     parameters.pop("self")
@@ -531,22 +545,21 @@ class WishartFull(_WishartOperatorPD):
   Evaluation of the pdf, determinant, and sampling are all `O(k^3)` operations
   where `(k, k)` is the event space shape.
 
-  #### Mathematical details.
+  #### Mathematical Details
 
-  The PDF of this distribution is,
+  The probability density function (pdf) is,
 
-  ```
-  f(X) = det(X)^(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / B(scale, df)
-  ```
-
-  where `df >= k` denotes the degrees of freedom, `scale` is a symmetric, pd,
-  `k x k` matrix, and the normalizing constant `B(scale, df)` is given by:
-
-  ```
-  B(scale, df) = 2^(0.5 df k) |det(scale)|^(0.5 df) Gamma_k(0.5 df)
+  ```none
+  pdf(X; df, scale) = det(X)**(0.5 (df-k-1)) exp(-0.5 tr[inv(scale) X]) / Z
+  Z = 2**(0.5 df k) |det(scale)|**(0.5 df) Gamma_k(0.5 df)
   ```
 
-  where `Gamma_k` is the multivariate Gamma function.
+  where:
+  * `df >= k` denotes the degrees of freedom,
+  * `scale` is a symmetric, positive definite, `k x k` matrix,
+  * `Z` is the normalizing constant, and,
+  * `Gamma_k` is the [multivariate Gamma function](
+    https://en.wikipedia.org/wiki/Multivariate_gamma_function).
 
   #### Examples
 
@@ -600,14 +613,15 @@ class WishartFull(_WishartOperatorPD):
         Cholesky factored matrix. Example `log_prob` input takes a Cholesky and
         `sample_n` returns a Cholesky when
         `cholesky_input_output_matrices=True`.
-      validate_args: `Boolean`, default `False`.  Whether to validate input with
-        asserts. If `validate_args` is `False`, and the inputs are invalid,
-        correct behavior is not guaranteed.
-      allow_nan_stats: `Boolean`, default `True`. If `False`, raise an
-        exception if a statistic (e.g., mean, mode) is undefined for any batch
-        member. If True, batch members with valid parameters leading to
-        undefined statistics will return `NaN` for this statistic.
-      name: The name scope to give class member ops.
+      validate_args: Python `Boolean`, default `False`. When `True` distribution
+        parameters are checked for validity despite possibly degrading runtime
+        performance. When `False` invalid inputs may silently render incorrect
+        outputs.
+      allow_nan_stats: Python `Boolean`, default `True`. When `True`, statistics
+        (e.g., mean, mode, variance) use the value "`NaN`" to indicate the
+        result is undefined.  When `False`, an exception is raised if one or
+        more of the statistic's batch members are undefined.
+      name: `String` name prefixed to Ops created by this class.
     """
     parameters = locals()
     parameters.pop("self")