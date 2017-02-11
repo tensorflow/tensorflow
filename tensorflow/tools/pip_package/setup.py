7c851683c332ececfaf0dc44e7f9e6b0493c7eb0
Yifei Feng <yifeif@google.com>
2017-02-10 21:52:28 -0800

    Update version string for 1.0.0.

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -29,7 +29,7 @@ from setuptools.dist import Distribution
 # This version string is semver compatible, but incompatible with pip.
 # For pip, we will remove all '-' characters from this string, and use the
 # result for pip.
-_VERSION = '1.0.0-rc2'
+_VERSION = '1.0.0'
 
 REQUIRED_PACKAGES = [
     'numpy >= 1.11.0',
