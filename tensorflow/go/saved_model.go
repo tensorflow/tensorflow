2b35f66be3f648cedf4b0530c7d3712a6662de2c
Asim Shankar <ashankar@google.com>
2017-02-15 06:39:08 -0800

    Go: Add blank line to avoid Copyright notice from appearing as package doc. Change: 147590973

--- tensorflow/go/saved_model.go
+++ tensorflow/go/saved_model.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package tensorflow
 
 import (
