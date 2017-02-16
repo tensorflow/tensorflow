1598a35266add3128eedeb64f847691af38d09d5
Rahul Kavi <leorahul16@gmail.com>
2017-02-15 18:18:49 -0800

    fixed tabs; added missing fclose(infile);

--- tensorflow/contrib/pi_examples/label_image/label_image.cc
+++ tensorflow/contrib/pi_examples/label_image/label_image.cc
@@ -102,7 +102,7 @@ Status LoadJpegFile(string file_name, std::vector<tensorflow::uint8>* data,
   cinfo.client_data = &jpeg_jmpbuf;
   jerr.error_exit = CatchError;
   if (setjmp(jpeg_jmpbuf)) {
-		fclose(infile);
+    fclose(infile);
     return tensorflow::errors::Unknown("JPEG decoding failed");
   }
   
