bc082fb6fb54722cd6340f4f9b2f868c1989c79c
Yuan Yu <yuanbyu@google.com>
2017-02-17 15:03:28 -0800

    Made tensors produced in a while loop unfeedable so we will give a more informative error message. Use queues or TensorArray to feed tensors into a while loop. Change: 147884461

--- tensorflow/python/kernel_tests/control_flow_ops_py_test.py
+++ tensorflow/python/kernel_tests/control_flow_ops_py_test.py
@@ -95,6 +95,16 @@ def all_fetchables():
   return tensor_names
 
 
+def all_feedables():
+  feedable_tensors = []
+  graph = ops.get_default_graph()
+  for op in graph.get_operations():
+    for t in op.inputs:
+      if graph.is_feedable(t):
+        feedable_tensors.append(t)
+  return feedable_tensors
+
+
 def opt_cfg():
   return config_pb2.ConfigProto(
       allow_soft_placement=True,
@@ -322,6 +332,23 @@ class ControlFlowTest(test.TestCase):
       tensor_names = all_fetchables()
       for name in tensor_names:
         sess.run(name, feed_dict={x: 3})
+
+  def testFeedable(self):
+    with self.test_session() as sess:
+      c = constant_op.constant(2)
+      i0 = constant_op.constant(0)
+      r = control_flow_ops.while_loop(lambda i: i < 1000,
+                                      lambda i: math_ops.square(c) + i, [i0])
+      self.assertEqual(1000, r.eval(feed_dict={i0: 0}))
+      feedable_tensors = all_feedables()
+      for t in feedable_tensors:
+        sess.run(r, feed_dict={t: 3})
+      graph = ops.get_default_graph()
+      for op in graph.get_operations():
+        for t in op.inputs:
+          if t not in feedable_tensors and t.dtype is dtypes.int32:
+            with self.assertRaisesRegexp(ValueError, "may not be fed"):
+              sess.run(r, feed_dict={t: 3})
 
   def testCondIndexedSlices(self):
     with self.test_session():
@@ -2616,7 +2643,6 @@ class AssertTest(test.TestCase):
         self.assertLess(0, len(unguarded_memcpy_nodestat_names))
       # No copy was performed for the guarded assert
       self.assertEqual([], guarded_memcpy_nodestat_names)
-
 
 if __name__ == "__main__":
   test.main()
