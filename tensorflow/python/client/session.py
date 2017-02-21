d6f24e33dfbe37b50b7ec3eb77fd9bf7a34d5fbb
Derek Murray <mrry@google.com>
2017-02-21 12:04:21 -0800

    Clarify documentation of `tf.Session.as_default()` wrt default graphs. Change: 148126366

--- tensorflow/python/client/session.py
+++ tensorflow/python/client/session.py
@@ -605,9 +605,8 @@ class BaseSession(SessionInterface):
     """Returns a context manager that makes this object the default session.
 
     Use with the `with` keyword to specify that calls to
-    @{tf.Operation.run} or
-    @{tf.Tensor.eval} should be
-    executed in this session.
+    @{tf.Operation.run} or @{tf.Tensor.eval} should be executed in
+    this session.
 
     ```python
     c = tf.constant(..)
@@ -618,9 +617,7 @@ class BaseSession(SessionInterface):
       print(c.eval())
     ```
 
-    To get the current default session, use
-    @{tf.get_default_session}.
-
+    To get the current default session, use @{tf.get_default_session}.
 
     *N.B.* The `as_default` context manager *does not* close the
     session when you exit the context, and you must close the session
@@ -642,14 +639,19 @@ class BaseSession(SessionInterface):
     session that is automatically closed on exiting the context,
     including when an uncaught exception is raised.
 
-    *N.B.* The default graph is a property of the current thread. If you
+    *N.B.* The default session is a property of the current thread. If you
     create a new thread, and wish to use the default session in that
     thread, you must explicitly add a `with sess.as_default():` in that
     thread's function.
 
+    *N.B.* Entering a `with sess.as_default():` block does not affect
+    the current default graph. If you are using multiple graphs, and
+    `sess.graph` is different from the value of @{tf.get_default_graph},
+    you must explicitly enter a `with sess.graph.as_default():` block
+    to make `sess.graph` the default graph.
+
     Returns:
       A context manager using this session as the default session.
-
     """
     return ops.default_session(self)
 
