79098e13efe58ef3e56025c93761f4d7bb02dfbe
A. Unique TensorFlower <gardener@tensorflow.org>
2017-02-27 12:01:00 -0800

    Fix a bug where queue_parsed_examples fails for single tensors. Change: 148676738

--- tensorflow/contrib/learn/python/learn/learn_io/graph_io_test.py
+++ tensorflow/contrib/learn/python/learn/learn_io/graph_io_test.py
@@ -34,6 +34,7 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 from tensorflow.contrib.learn.python.learn.learn_io import graph_io
 from tensorflow.contrib.learn.python.learn.learn_io.graph_io import _read_keyed_batch_examples_shared_queue
 from tensorflow.python.client import session as session_lib
+from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes as dtypes_lib
 from tensorflow.python.framework import errors
 from tensorflow.python.framework import ops
@@ -740,6 +741,17 @@ class GraphIOTest(test.TestCase):
       with self.assertRaises(errors.OutOfRangeError):
         session.run((keys, inputs))
 
+      coord.request_stop()
+      coord.join(threads)
+
+  def test_queue_parsed_features_single_tensor(self):
+    with ops.Graph().as_default() as g, self.test_session(graph=g) as session:
+      features = {"test": constant_op.constant([1, 2, 3])}
+      _, queued_features = graph_io.queue_parsed_features(features)
+      coord = coordinator.Coordinator()
+      threads = queue_runner_impl.start_queue_runners(session, coord=coord)
+      out_features = session.run(queued_features["test"])
+      self.assertAllEqual([1, 2, 3], out_features)
       coord.request_stop()
       coord.join(threads)
 
