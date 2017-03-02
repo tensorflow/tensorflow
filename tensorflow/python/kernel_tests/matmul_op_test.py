Fix bug in matmul gradients for complex types. Clean up matmul gradient test code and improve test coverage. Change: 149057186

tensorflow/python/kernel_tests/matmul_op_test.py
@@ -21,7 +21,6 @@ from __future__ import print_function
 import numpy as np
 
 from tensorflow.python.framework import constant_op
-from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import array_ops
@@ -39,6 +38,15 @@ def _AddTest(test, op_name, testcase_name, fn):
   setattr(test, test_name, fn)
 
 
+def _GetTransposedMatrices(x, x_name, kwargs):
+  if kwargs["transpose_" + x_name] is True:
+    return x.T
+  elif kwargs["adjoint_" + x_name] is True:
+    return np.conj(x.T)
+  else:
+    return x
+
+
 class MatMulTest(test_lib.TestCase):
   pass  # Filled in below
 
@@ -46,25 +54,12 @@ class MatMulTest(test_lib.TestCase):
 def _GetMatMulTest(a_np_, b_np_, use_static_shape_, **kwargs_):
 
   def Test(self):
+    # TODO(rmlarsen): Re-enable this test when we have fixed the failure on
+    # Windows.
+    if not use_static_shape_ and a_np_.dtype is np.int32:
+      self.skipTest("Skipping test to avoid failure on Windows.")
+
     np_val = np.matrix(a_np_) * np.matrix(b_np_)
-
-    # Transpose and possibly conjugate a and b according to the attributes
-    # such that tf.matmul(effective_a_np, effective_b_np, **kwargs) results in
-    # a valid matrix multiplication and produces the same result as
-    # np.matrix(a_np_) * np.matrix(b_np_)
-    if kwargs_["transpose_a"] is True or kwargs_["adjoint_a"] is True:
-      effective_a_np = a_np_.T
-      if kwargs_["adjoint_a"] is True:
-        effective_a_np = np.conj(effective_a_np)
-    else:
-      effective_a_np = a_np_
-
-    if kwargs_["transpose_b"] is True or kwargs_["adjoint_b"] is True:
-      effective_b_np = b_np_.T
-      if kwargs_["adjoint_b"] is True:
-        effective_b_np = np.conj(effective_b_np)
-    else:
-      effective_b_np = b_np_
 
     use_gpu = True
     if a_np_.dtype is np.float16 and (
@@ -72,6 +67,12 @@ def _GetMatMulTest(a_np_, b_np_, use_static_shape_, **kwargs_):
       use_gpu = False
       print("Built without fp16 matmul support for Cuda, running test on CPU.")
 
+    # Transpose and possibly conjugate a_np_ and b_np_ according to the
+    # attributes such that tf.matmul(effective_a_np, effective_b_np, **kwargs)
+    # results in a valid matrix multiplication and produces the same result as
+    # np.matrix(a_np_) * np.matrix(b_np_)
+    effective_a_np = _GetTransposedMatrices(a_np_, "a", kwargs_)
+    effective_b_np = _GetTransposedMatrices(b_np_, "b", kwargs_)
     with self.test_session(use_gpu=use_gpu) as sess:
       if use_static_shape_:
         a = constant_op.constant(effective_a_np)
@@ -82,7 +83,7 @@ def _GetMatMulTest(a_np_, b_np_, use_static_shape_, **kwargs_):
         a = array_ops.placeholder(a_np_.dtype)
         b = array_ops.placeholder(b_np_.dtype)
         res = math_ops.matmul(a, b, **kwargs_)
-        tf_val = sess.run(res, {a: effective_a_np, b: effective_b_np})
+        tf_val = sess.run(res, feed_dict={a: effective_a_np, b: effective_b_np})
 
     self.assertAllCloseAccordingToType(
         tf_val,
@@ -95,98 +96,40 @@ def _GetMatMulTest(a_np_, b_np_, use_static_shape_, **kwargs_):
   return Test
 
 
-# TODO(zhifengc): Figures out how to test matmul gradients on GPU.
 class MatMulGradientTest(test_lib.TestCase):
+  pass  # Will be filled in below.
 
-  def testGradientInput0(self):
-    with self.test_session(use_gpu=False):
-      x = constant_op.constant(
-          [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-          shape=[3, 2],
-          dtype=dtypes.float64,
-          name="x")
-      y = constant_op.constant(
-          [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7],
-          shape=[2, 4],
-          dtype=dtypes.float64,
-          name="y")
-      m = math_ops.matmul(x, y, name="matmul")
-      err = gradient_checker.compute_gradient_error(x, [3, 2], m, [3, 4])
-    print("matmul input0 gradient err = ", err)
-    self.assertLess(err, 1e-10)
 
-  def testGradientInput1(self):
-    with self.test_session(use_gpu=False):
-      x = constant_op.constant(
-          [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-          shape=[3, 2],
-          dtype=dtypes.float64,
-          name="x")
-      y = constant_op.constant(
-          [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7],
-          shape=[2, 4],
-          dtype=dtypes.float64,
-          name="y")
-      m = math_ops.matmul(x, y, name="matmul")
-      err = gradient_checker.compute_gradient_error(y, [2, 4], m, [3, 4])
-    print("matmul input1 gradient err = ", err)
-    self.assertLess(err, 1e-10)
+def _GetMatMulGradientTest(a_np_, b_np_, use_static_shape_, **kwargs_):
 
-  def _VerifyInput0(self, transpose_a, transpose_b):
-    shape_x = [3, 2]
-    shape_y = [2, 4]
-    if transpose_a:
-      shape_x = list(reversed(shape_x))
-    if transpose_b:
-      shape_y = list(reversed(shape_y))
-    with self.test_session(use_gpu=False):
-      x = constant_op.constant(
-          [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-          shape=shape_x,
-          dtype=dtypes.float64,
-          name="x")
-      y = constant_op.constant(
-          [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7],
-          shape=shape_y,
-          dtype=dtypes.float64,
-          name="y")
-      m = math_ops.matmul(x, y, transpose_a, transpose_b, name="matmul")
-      err = gradient_checker.compute_gradient_error(x, shape_x, m, [3, 4])
-    print("matmul input0 gradient err = ", err)
-    self.assertLess(err, 1e-10)
+  def Test(self):
+    if not use_static_shape_ or a_np_.dtype in (np.int32, np.float16):
+      self.skipTest("Skipping infeasible gradient test.")
 
-  def testGradientInput0WithTranspose(self):
-    self._VerifyInput0(transpose_a=True, transpose_b=False)
-    self._VerifyInput0(transpose_a=False, transpose_b=True)
-    self._VerifyInput0(transpose_a=True, transpose_b=True)
+    # Transpose and possibly conjugate a_np_ and b_np_ according to the
+    # attributes such that tf.matmul(effective_a_np, effective_b_np, **kwargs)
+    # results in a valid matrix multiplication and produces the same result as
+    # np.matrix(a_np_) * np.matrix(b_np_)
+    effective_a_np = _GetTransposedMatrices(a_np_, "a", kwargs_)
+    effective_b_np = _GetTransposedMatrices(b_np_, "b", kwargs_)
 
-  def _VerifyInput1(self, transpose_a, transpose_b):
-    shape_x = [3, 2]
-    shape_y = [2, 4]
-    if transpose_a:
-      shape_x = list(reversed(shape_x))
-    if transpose_b:
-      shape_y = list(reversed(shape_y))
-    with self.test_session(use_gpu=False):
-      x = constant_op.constant(
-          [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-          shape=shape_x,
-          dtype=dtypes.float64,
-          name="x")
-      y = constant_op.constant(
-          [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7],
-          shape=shape_y,
-          dtype=dtypes.float64,
-          name="y")
-      m = math_ops.matmul(x, y, transpose_a, transpose_b, name="matmul")
-      err = gradient_checker.compute_gradient_error(y, shape_y, m, [3, 4])
-    print("matmul input1 gradient err = ", err)
-    self.assertLess(err, 1e-10)
+    epsilon = np.finfo(a_np_.dtype).eps
+    delta = epsilon**(1.0 / 3.0)
+    tol = 20 * delta
+    with self.test_session(use_gpu=True):
+      a = constant_op.constant(effective_a_np)
+      b = constant_op.constant(effective_b_np)
+      res = math_ops.matmul(a, b, **kwargs_)
+      for x, x_init in [a, effective_a_np], [b, effective_b_np]:
+        theoretical, numerical = gradient_checker.compute_gradient(
+            x,
+            x_init.shape,
+            res, [a_np_.shape[0], b_np_.shape[1]],
+            x_init_value=x_init,
+            delta=delta)
+        self.assertAllClose(theoretical, numerical, rtol=tol, atol=tol)
 
-  def testGradientInput1WithTranspose(self):
-    self._VerifyInput1(transpose_a=True, transpose_b=False)
-    self._VerifyInput1(transpose_a=False, transpose_b=True)
-    self._VerifyInput1(transpose_a=True, transpose_b=True)
+  return Test
 
 
 class MatMulStatsTest(test_lib.TestCase):
@@ -248,4 +191,14 @@ if __name__ == "__main__":
                              transpose_a=transpose_a,
                              adjoint_b=adjoint_b,
                              transpose_b=transpose_b))
+                _AddTest(MatMulGradientTest, "MatMulGradientTest", name,
+                         _GetMatMulGradientTest(
+                             a_np,
+                             b_np,
+                             use_static_shape,
+                             adjoint_a=adjoint_a,
+                             transpose_a=transpose_a,
+                             adjoint_b=adjoint_b,
+                             transpose_b=transpose_b))
+
   test_lib.main()