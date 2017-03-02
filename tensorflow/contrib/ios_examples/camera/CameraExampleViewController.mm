860056b1638b4f9d01d0ad6181eae850f93a6bb7
Tomer Gafner <tomergafner@gmail.com>
2017-03-02 01:19:02 -0500

    [ios camera app] replace deprecated stringWithCString

--- tensorflow/contrib/ios_examples/camera/CameraExampleViewController.mm
+++ tensorflow/contrib/ios_examples/camera/CameraExampleViewController.mm
@@ -327,7 +327,7 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
         const float predictionValue = predictions(index);
         if (predictionValue > 0.05f) {
           std::string label = labels[index % predictions.size()];
-          NSString *labelObject = [NSString stringWithCString:label.c_str()];
+          NSString *labelObject = [NSString stringWithUTF8String:label.c_str()];
           NSNumber *valueObject = [NSNumber numberWithFloat:predictionValue];
           [newValues setObject:valueObject forKey:labelObject];
         }
