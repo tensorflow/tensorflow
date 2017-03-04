9752e45e33c51fcd0532c6cd0591178849a6424e
wangg12 <guwang12@gmail.com>
2017-03-04 12:49:50 +0800

    change learn.metric_spec.MetricSpec to learn.MetricSpec due to API change in version 1.0

--- tensorflow/examples/tutorials/monitors/iris_monitors.py
+++ tensorflow/examples/tutorials/monitors/iris_monitors.py
@@ -39,17 +39,17 @@ def main(unused_argv):
 
   validation_metrics = {
       "accuracy":
-          tf.contrib.learn.metric_spec.MetricSpec(
+          tf.contrib.learn.MetricSpec(
               metric_fn=tf.contrib.metrics.streaming_accuracy,
               prediction_key=tf.contrib.learn.prediction_key.PredictionKey.
               CLASSES),
       "precision":
-          tf.contrib.learn.metric_spec.MetricSpec(
+          tf.contrib.learn.MetricSpec(
               metric_fn=tf.contrib.metrics.streaming_precision,
               prediction_key=tf.contrib.learn.prediction_key.PredictionKey.
               CLASSES),
       "recall":
-          tf.contrib.learn.metric_spec.MetricSpec(
+          tf.contrib.learn.MetricSpec(
               metric_fn=tf.contrib.metrics.streaming_recall,
               prediction_key=tf.contrib.learn.prediction_key.PredictionKey.
               CLASSES)
