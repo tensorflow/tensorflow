96c0d2c9146ebb5f31ae82ebc206643fc8049ea0
A. Unique TensorFlower <gardener@tensorflow.org>
2017-03-06 16:00:32 -0800

    Fixes the treatment of collections=[] to be consistent with the documentation, so GLOBAL_VARIABLES is used only when collections==None. Change: 149357125

--- tensorflow/contrib/framework/python/ops/variables.py
+++ tensorflow/contrib/framework/python/ops/variables.py
@@ -195,7 +195,8 @@ def variable(name, shape=None, dtype=None, initializer=None,
   Returns:
     The created or existing variable.
   """
-  collections = list(collections or [ops.GraphKeys.GLOBAL_VARIABLES])
+  collections = list(collections if collections is not None
+                     else [ops.GraphKeys.GLOBAL_VARIABLES])
 
   # Remove duplicates
   collections = set(collections)
