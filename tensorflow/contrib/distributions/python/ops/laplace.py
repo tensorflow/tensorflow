b6c0b14c8f1c00d50fb7cb8d7bd121efd17cca75
Ian Langmore <langmore@google.com>
2017-03-13 15:37:41 -0800

    More distributions:  Use init arg "name" for the name, rather than name_scope. These should have been done as part of an earlier change, but was missed. Using name_scope means self.name = the_initialization_name + "/", which makes it hard to set  a distribution name, then know what the name is later. Change: 150008411

--- tensorflow/contrib/distributions/python/ops/laplace.py
+++ tensorflow/contrib/distributions/python/ops/laplace.py
@@ -217,11 +217,11 @@ class LaplaceWithSoftplusScale(Laplace):
                allow_nan_stats=True,
                name="LaplaceWithSoftplusScale"):
     parameters = locals()
-    with ops.name_scope(name, values=[loc, scale]) as ns:
+    with ops.name_scope(name, values=[loc, scale]):
       super(LaplaceWithSoftplusScale, self).__init__(
           loc=loc,
           scale=nn.softplus(scale, name="softplus_scale"),
           validate_args=validate_args,
           allow_nan_stats=allow_nan_stats,
-          name=ns)
+          name=name)
     self._parameters = parameters
