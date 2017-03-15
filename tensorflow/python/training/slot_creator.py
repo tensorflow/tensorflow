8f75f24a52a7645a4ef93cbe244d45d30d5e4a3f
Peter Hawkins <phawkins@google.com>
2017-03-15 09:43:19 -0800

    Add a function to create training variable slots from an initializer. Use it from the Adagrad optimizer. Change: 150210582

--- tensorflow/python/training/slot_creator.py
+++ tensorflow/python/training/slot_creator.py
@@ -115,6 +115,40 @@ def create_slot(primary, val, name, colocate_with_primary=True):
       return _create_slot_var(primary, val, "", validate_shape, None, None)
 
 
+def create_slot_with_initializer(primary, initializer, shape, dtype, name,
+                                 colocate_with_primary=True):
+  """Creates a slot initialized using an `Initializer`.
+
+  The type of the slot is determined by the given value.
+
+  Args:
+    primary: The primary `Variable` or `Tensor`.
+    initializer: An `Initializer`.  The initial value of the slot.
+    shape: Shape of the initial value of the slot.
+    dtype: Type of the value of the slot.
+    name: Name to use for the slot variable.
+    colocate_with_primary: Boolean.  If True the slot is located
+      on the same device as `primary`.
+
+  Returns:
+    A `Variable` object.
+  """
+  # Scope the slot name in the namespace of the primary variable.
+  # Set "primary.op.name + '/' + name" as default name, so the scope name of
+  # optimizer can be shared when reuse is True. Meanwhile when reuse is False
+  # and the same name has been previously used, the scope name will add '_N'
+  # as suffix for unique identifications.
+  validate_shape = shape.is_fully_defined()
+  with variable_scope.variable_scope(None, primary.op.name + "/" + name):
+    if colocate_with_primary:
+      with ops.colocate_with(primary):
+        return _create_slot_var(primary, initializer, "", validate_shape, shape,
+                                dtype)
+    else:
+      return _create_slot_var(primary, initializer, "", validate_shape, shape,
+                              dtype)
+
+
 def create_zeros_slot(primary, name, dtype=None, colocate_with_primary=True):
   """Create a slot initialized to 0 with same shape as the primary object.
 
@@ -134,16 +168,11 @@ def create_zeros_slot(primary, name, dtype=None, colocate_with_primary=True):
   slot_shape = (slot_shape if slot_shape.is_fully_defined()
                 else array_ops.shape(primary.initialized_value()))
   if slot_shape.is_fully_defined():
-    val = init_ops.zeros_initializer(dtype)
-    validate_shape = True
-    shape = slot_shape
+    initializer = init_ops.zeros_initializer(dtype)
+    return create_slot_with_initializer(
+        primary, initializer, slot_shape, dtype, name,
+        colocate_with_primary=colocate_with_primary)
   else:
     val = array_ops.zeros(slot_shape, dtype=dtype)
-    validate_shape = False
-    shape = None
-  with variable_scope.variable_scope(None, primary.op.name + "/" + name):
-    if colocate_with_primary:
-      with ops.colocate_with(primary):
-        return _create_slot_var(primary, val, "", validate_shape, shape, dtype)
-    else:
-      return _create_slot_var(primary, val, "", validate_shape, shape, dtype)
+    return create_slot(primary, val, name,
+                       colocate_with_primary=colocate_with_primary)
