d09c94623272a0f83de714b10ff630a85da0d173
Karan Desai <karandesai_96@live.com>
2017-03-16 13:20:01 +0530

    Add functional interface for conv3d transpose layer.

--- tensorflow/python/layers/convolutional.py
+++ tensorflow/python/layers/convolutional.py
@@ -1355,6 +1355,43 @@ class Conv3DTranspose(Conv3D):
       return self.activation(outputs)
     return outputs
 
+
+def conv3d_transpose(inputs,
+                     filters,
+                     kernel_size,
+                     strides=(1, 1, 1),
+                     padding='valid',
+                     data_format='channels_last',
+                     activation=None,
+                     use_bias=True,
+                     kernel_initializer=None,
+                     bias_initializer=init_ops.zeros_initializer(),
+                     kernel_regularizer=None,
+                     bias_regularizer=None,
+                     activity_regularizer=None,
+                     trainable=True,
+                     name=None,
+                     reuse=None):
+  layer = Conv3DTranspose(
+      filters=filters,
+      kernel_size=kernel_size,
+      strides=strides,
+      padding=padding,
+      data_format=data_format,
+      activation=activation,
+      use_bias=use_bias,
+      kernel_initializer=kernel_initializer,
+      bias_initializer=bias_initializer,
+      kernel_regularizer=kernel_regularizer,
+      bias_regularizer=bias_regularizer,
+      activity_regularizer=activity_regularizer,
+      trainable=trainable,
+      name=name,
+      _reuse=reuse,
+      _scope=name)
+  return layer.apply(inputs)
+
+
 # Aliases
 
 Convolution1D = Conv1D
