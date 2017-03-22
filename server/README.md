5e4d48593fc21fcb1ee8245fde56e326dea26048
Ovidio Henriquez <odejesush@tamu.edu>
2017-03-21 22:15:22 -0500

    The image uploading is working; files are saved according to the set that they are a part of

--- server/README.md
+++ server/README.md
@@ -1,5 +1,23 @@
 # README
 
+### Prerequisites
+
+Install the virtual environment:
+```
+sudo pip install virtualenv
+```
+
+If needed, install the setup tools:
+```
+pip install --upgrade pip setuptools
+```
+
+Change the permissions of the server folder to allow the web server to write to the disk:
+```
+chmod 777 ./server
+```
+(If necessary replace ./server with the actual path of the server folder)
+
 ### Run server
 
 To run the server, execute the following command in the terminal:
@@ -7,3 +25,4 @@ To run the server, execute the following command in the terminal:
 . /venv/bin/activate
 export FLASK_APP=web_server.py
 flask run
+```
--- server/web_server.py
+++ server/web_server.py
@@ -3,37 +3,57 @@
 that will be used for training. """
 
 import os
-from flask import Flask, request, redirect, url_for
+from os import listdir
+from os.path import isfile, join
+from flask import Flask, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'user_images'
+UPLOAD_FOLDER = './user_images'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'bmp'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+APP = Flask(__name__)
+APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
+    """ Check if the attached file contains the valid image extensions defined
+    in ALLOWED_EXTENSIONS """
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
+@APP.route('/')
 def hello_world():
+    """ Index page. TODO: Replace with a dashboard or something to help manage the server """
     return 'Hello world!'
 
-# Training API
-#@app.route('/images', methods=['GET'])
-#def images():
-#    if request.method == 'GET':
-#        
 
-@app.route('/images', defaults={'set_name': None})
-@app.route('/images?set_name=<set_name>', methods=['GET', 'POST'])
-def image_upload(set_name):
+# Training API
+# Receive images and upload them to the set folder
+@APP.route('/images', methods=['GET', 'POST'])
+def images():
+    """ Handles the training set images
+
+    GET: Displays the image upload page if no set_name argument is specified
+    in the URL or displays all the images of a given set if set_name is specified
+    in the URL
+
+    POST: Upload the given image to store under the UPLOAD_FOLDER/set_name, where
+    set_name is specified in the form. """
     if request.method == 'GET':
         # Display all the images stored under the set_name folder
-        if set_name != None:
-            return 'Display images from %s' % set_name
+        if request.args.get('set_name') != None:
+            #return 'Display images from %s' % request.args.get('set_name')
+            set_directory = UPLOAD_FOLDER + '/' + request.args.get('set_name')
+            files = [f for f in listdir(set_directory) if isfile(join(set_directory, f))]
+            imageHTML = """
+                <!doctype html>
+                <title>""" + request.args.get('set_name') + \
+                """ Set</title> """
+            for image in files:
+                imageHTML = imageHTML + '<img src="' + request.args.get('set_name') + \
+                    '/' + image + '">'
+            return imageHTML
         else:
+        # Display the file upload form
             return '''
             <!doctype html>
             <title>Upload new image</title>
@@ -49,13 +69,26 @@ def image_upload(set_name):
         if 'file' not in request.files:
             flash('No files attached')
             return redirect(request.url)
-        file = request.files['file']
+        img = request.files['file']
+
         # if user does not select file, browser also
         # submit a empty part without a filename
-        if file.filename == '':
+        if img.filename == '':
             flash('No file selected')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], set_name, filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+
+        if img and allowed_file(img.filename):
+            filename = secure_filename(img.filename)
+            directory = os.path.join(APP.config['UPLOAD_FOLDER'], request.form['set_name'])
+
+            # create the file path if it does not exist
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            # save the image in the directory
+            img.save(os.path.join(directory, filename))
+            return redirect('/images?set_name=' + request.form['set_name'])
+
+@APP.route('/<set_name>/<image>')
+def serve_image(set_name, image):
+    return APP.send_static_file(UPLOAD_FOLDER + '/' + set_name + '/' + image)
