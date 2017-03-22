317366a203b09e038ce5a0eb4dc20adb3842b673
mazecreator <mazecreator@mazecreator.com>
2017-03-22 18:30:07 -0400

    Clean up code formatting

--- tensorflow/contrib/image/ops/single_image_random_dot_stereograms_ops.cc
+++ tensorflow/contrib/image/ops/single_image_random_dot_stereograms_ops.cc
@@ -22,7 +22,7 @@ namespace tensorflow {
 using shape_inference::InferenceContext;
 
 REGISTER_OP("SingleImageRandomDotStereograms")
-    .Attr("T: {double,float,int64,int32}")   
+    .Attr("T: {double,float,int64,int32}")
     .Input("depth_values: T")
     .Output("image: uint8")
     .Attr("hidden_surface_removal: bool = true")
@@ -34,27 +34,25 @@ REGISTER_OP("SingleImageRandomDotStereograms")
     .Attr("normalize_max: float = -100.0")
     .Attr("normalize_min: float = 100.0")
     .Attr("boarder_level: float = 0.0")
-    .Attr("number_colors: int = 256")    
+    .Attr("number_colors: int = 256")
     .Attr("generation_mode: string = 'SIRDS'")
-    .Attr("output_image_shape: shape = { dim {size:1024} dim {size: 768} dim {size: 1}}")
+    .Attr(
+        "output_image_shape: shape = { dim {size:1024} dim {size: 768} dim "
+        "{size: 1}}")
     .Attr("output_data_window: shape = { dim {size:1022} dim {size: 757}}")
-    // TODO(Mazecreator):  Add "SetShapeFN" to reflect proper output shape & Rank 2 error checking on Input
-    // TODO(Mazecreator):  Add better error handling for output shapes / windows settings
-    // .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-    //   ::tensorflow::shape_inference::ShapeHandle input;
-    //   ::tensorflow::shape_inference::ShapeHandle output;
-    //   TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 2, &input));
-    //   TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 2, &output));
-    //   c->set_output(0,output);
-    //   return Status::OK();
-    // })
     .Doc(R"doc(
-Output a RandomDotStereogram Tensor of shape "output_image_shape" for export via encode_PNG or encode_JPG OP.
+Outputs a single image random dot stereogram for export via encode_PNG/JPG OP.
 
-Based upon:
+Given the 2-D tensor 'depth_values' with encoded Z values, this operation will 
+encode 3-D data into a 2-D image.  The output of this Op is suitable for the
+encode_PNG/JPG ops.  Be careful with image compression as this may corrupt the
+encode 3-D data witin the image.
+
+This Op is based upon:
 'http://www.learningace.com/doc/4331582/b6ab058d1e206d68ab60e4e1ead2fe6e/sirds-paper'
 
 Example use which outputs a SIRDS image as picture_out.png:
+```python
 img=[[1,2,3,3,2,1],
      [1,2,3,4,5,2],
      [1,2,3,4,5,3],
@@ -71,27 +69,28 @@ png = tf.image.encode_png(out).eval()
 
 with open('picture_out.png', 'wb') as f:
     f.write(png)
+```
 
+depth_values: Z values of data to encode into 'output_data_window' window, 
+  lower values are further away {0.0 floor(far), 1.0 ceiling(near) after normalization}, must be 2-D tensor
+hidden_surface_removal: Activate hidden surface removal
+convergence_dots_size: Black dot size in pixels to help view converge image, drawn on bottom of image
+dots_per_inch: Output device in dots/inch
+eye_separation: Separation between eyes in inches
+mu: Depth of field, Fraction of viewing distance (eg. 1/3 = .3333)
+normalize: Normalize input data to [0.0, 1.0] 
+normalize_max: Fix MAX value for Normalization - if < MIN, autoscale
+normalize_min: Fix MIN value for Normalization - if > MAX, autoscale
+boarder_level: Value of board in depth 0.0 {far} to 1.0 {near}
+number_colors: 2 (Black & White),256 (grayscale), and Numbers > 256 (Full Color) are all that are supported currently
+generation_mode: Mode for Stereogram
+  SIRDS - single image random dot stereogram (Default)
+output_image_shape: Output size of returned image in X,Y, Channels 1-grayscale, 3 color (1024, 768, 1),
+  channels will be updated to 3 if 'number_colors' > 256
+output_data_window: Size of "DATA" window, must be equal to or smaller than 'output_image_shape', will be centered
+  and use 'convergence_dots_size' for best fit to avoid overlap if possible
 
-depth_values:           Z values of data to encode into "output_data_window" window, lower further away {0.0 floor(far), 1.0 ceiling(near) after normalization}, must be rank 2
-hidden_surface_removal: Activate hidden surface removal (True)
-convergence_dots_size:  Black dot size in pixels to help view converge image, drawn on bottom of image (8 pixels)
-dots_per_inch:	        Output device in dots/inch (72 default)
-eye_separation:         Separation between eyes in inches (2.5 inchs)
-mu:                     Depth of field, Fraction of viewing distance (1/3 = .3333)
-normalize:              Normalize input data to [0.0, 1.0] (True)
-normalize_max:          Fix MAX value for Normalization (0.0) - if < MIN, autoscale
-normalize_min:          Fix MIN value for Normalization (0.0) - if > MAX, autoscale
-boarder_level:          Value of board in depth 0.0 {far} to 1.0 {near} (0.0)
-number_colors:          2 (Black & White),256 (grayscale), and Numbers > 256 (Full Color) are all that are supported currently
-generation_mode:        Mode for Stereogram
-                            SIRDS - 2 color stereogram (Default)
-output_image_shape:     Output size of returned image in X,Y, Channels 1-grayscale, 3 color (1024, 768, 1), channels will be updated to 3 if number_colors > 256
-output_data_window:     Size of "DATA" window, must be equal to or smaller than output_image_shape, will be centered
-                          and use convergence_dots_size for best fit to avoid overlap if possible
-
-image:                  returns a Tensor of size output_image_shape with depth_values encoded into image
-
+image:= A tensor of size 'output_image_shape' with the encloded 'depth_values'
 )doc");
 
 }  // namespace tensorflow
--- tensorflow/contrib/image/python/ops/single_image_random_dot_stereograms.py
+++ tensorflow/contrib/image/python/ops/single_image_random_dot_stereograms.py
@@ -18,11 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.contrib.util import loader
-from tensorflow.python.framework import common_shapes
-from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import resource_loader
 
 file =   resource_loader.get_path_to_datafile("_single_image_random_dot_stereograms.so")
@@ -41,65 +37,71 @@ def single_image_random_dot_stereograms(depth_values,
                                         generation_mode=None,
                                         output_image_shape=None,
                                         output_data_window=None):
-  r"""Output a RandomDotStereogram Tensor of shape "output_image_shape" for export via encode_PNG or encode_JPG OP.
+    """Output a RandomDotStereogram Tensor for export via encode_PNG/JPG OP.
 
-  Based upon:
-  'http://www.learningace.com/doc/4331582/b6ab058d1e206d68ab60e4e1ead2fe6e/sirds-paper'
+    Given the 2-D tensor 'depth_values' with encoded Z values, this operation will 
+    encode 3-D data into a 2-D image.  The output of this Op is suitable for the
+    encode_PNG/JPG ops.  Be careful with image compression as this may corrupt the
+    encode 3-D data witin the image.
 
-  Example use which outputs a SIRDS image as picture_out.png:
-  img=[[1,2,3,3,2,1],
-       [1,2,3,4,5,2],
-       [1,2,3,4,5,3],
-       [1,2,3,4,5,4],
-       [6,5,4,4,5,5]]
+    Based upon:
+    'http://www.learningace.com/doc/4331582/b6ab058d1e206d68ab60e4e1ead2fe6e/sirds-paper'
 
-  session = tf.InteractiveSession()
+    Example use which outputs a SIRDS image as picture_out.png:
 
-  sirds = single_image_random_dot_stereograms(img,convergence_dots_size=8,number_colors=256,normalize=True)
+    ```python
+    img=[[1,2,3,3,2,1],
+          [1,2,3,4,5,2],
+          [1,2,3,4,5,3],
+          [1,2,3,4,5,4],
+          [6,5,4,4,5,5]]
+    session = tf.InteractiveSession()
+    sirds = single_image_random_dot_stereograms(img,
+        convergence_dots_size=8,
+        number_colors=256,normalize=True)
+      
+    out = sirds.eval()
+    png = tf.image.encode_png(out).eval()
+    with open('picture_out.png', 'wb') as f:
+        f.write(png)
+    ```
 
-  out = sirds.eval()
+    Args:
+      depth_values: A `Tensor`. Must be one of the following types: 
+        `float64`, `float32`, `int64`, `int32`.  Z values of data to encode into 'output_data_window'
+        window, lower further away {0.0 floor(far), 1.0 ceiling(near) after norm}, must be 2-D tensor
+      hidden_surface_removal: An optional `bool`. Defaults to `True`.
+        Activate hidden surface removal
+      convergence_dots_size: An optional `int`. Defaults to `8`.
+        Black dot size in pixels to help view converge image, drawn on bottom of image
+      dots_per_inch: An optional `int`. Defaults to `72`.
+        Output device in dots/inch
+      eye_separation: An optional `float`. Defaults to `2.5`.
+        Separation between eyes in inches
+      mu: An optional `float`. Defaults to `0.3333`.
+        Depth of field, Fraction of viewing distance (eg. 1/3 = 0.3333)
+      normalize: An optional `bool`. Defaults to `True`.
+        Normalize input data to [0.0, 1.0] 
+      normalize_max: An optional `float`. Defaults to `-100`.
+        Fix MAX value for Normalization (0.0) - if < MIN, autoscale
+      normalize_min: An optional `float`. Defaults to `100`.
+        Fix MIN value for Normalization (0.0) - if > MAX, autoscale
+      boarder_level: An optional `float`. Defaults to `0`.
+        Value of board in depth 0.0 {far} to 1.0 {near} 
+      number_colors: An optional `int`. Defaults to `256`.
+        2 (Black & White),256 (grayscale), and Numbers > 256 (Full Color) are supported
+      generation_mode: An optional `string`. Defaults to `"SIRDS"`.
+        SIRDS - single image random dot stereogram (Default)
+      output_image_shape: An optional `tf.TensorShape` or list of `ints`. 
+        Defaults to shape `[1024, 768, 1]`.
+        Defines output shape of returned image in '[X,Y, Channels]' 1-grayscale, 3 color
+        channels will be updated to 3 if number_colors > 256
+      output_data_window: An optional `tf.TensorShape` or list of `ints`. Defaults to `[1022, 757]`.
+        Size of "DATA" window, must be equal to or smaller than 'output_image_shape', will be
+        centered and use convergence_dots_size for best fit to avoid overlap if possible
 
-  png = tf.image.encode_png(out).eval()
-
-  with open('picture_out.png', 'wb') as f:
-      f.write(png)
-
-  Args:
-    depth_values: A `Tensor`. Must be one of the following types: `float64`, `float32`, `int64`, `int32`.
-      Z values of data to encode into "output_data_window" window, lower further away {0.0 floor(far), 1.0 ceiling(near) after normalization}, must be rank 2
-    hidden_surface_removal: An optional `bool`. Defaults to `True`.
-      Activate hidden surface removal (True)
-    convergence_dots_size: An optional `int`. Defaults to `8`.
-      Black dot size in pixels to help view converge image, drawn on bottom of image (8 pixels)
-    dots_per_inch: An optional `int`. Defaults to `72`.
-      Output device in dots/inch (72 default)
-    eye_separation: An optional `float`. Defaults to `2.5`.
-      Separation between eyes in inches (2.5 inchs)
-    mu: An optional `float`. Defaults to `0.3333`.
-      Depth of field, Fraction of viewing distance (1/3 = .3333)
-    normalize: An optional `bool`. Defaults to `True`.
-      Normalize input data to [0.0, 1.0] (True)
-    normalize_max: An optional `float`. Defaults to `-100`.
-      Fix MAX value for Normalization (0.0) - if < MIN, autoscale
-    normalize_min: An optional `float`. Defaults to `100`.
-      Fix MIN value for Normalization (0.0) - if > MAX, autoscale
-    boarder_level: An optional `float`. Defaults to `0`.
-      Value of board in depth 0.0 {far} to 1.0 {near} (0.0)
-    number_colors: An optional `int`. Defaults to `256`.
-      2 (Black & White),256 (grayscale), and Numbers > 256 (Full Color) are all that are supported currently
-    generation_mode: An optional `string`. Defaults to `"SIRDS"`.
-      Mode for Stereogram
-      SIRDS - 2 color stereogram (Default)
-    output_image_shape: An optional `tf.TensorShape` or list of `ints`. Defaults to `[1024, 768, 1]`.
-      Output size of returned image in X,Y, Channels 1-grayscale, 3 color (1024, 768, 1), channels will be updated to 3 if number_colors > 256
-    output_data_window: An optional `tf.TensorShape` or list of `ints`. Defaults to `[1022, 757]`.
-      Size of "DATA" window, must be equal to or smaller than output_image_shape, will be centered
-      and use convergence_dots_size for best fit to avoid overlap if possible
-    name: A name for the operation (optional).
-
-  Returns:
-    A `Tensor` of type `uint8`.
-    returns a Tensor of size output_image_shape with depth_values encoded into image
+    Returns:
+      A `Tensor` of type `uint8` of shape 'output_image_shape' with encoded 'depth_values'
   """
   result = _sirds_ops.single_image_random_dot_stereograms( depth_values=depth_values,
                                 hidden_surface_removal=hidden_surface_removal,
@@ -115,6 +117,5 @@ def single_image_random_dot_stereograms(depth_values,
                                 output_image_shape=output_image_shape,
                                 output_data_window=output_data_window)
   return result
-
 
 ops.NotDifferentiable("SingleImageRandomDotStereograms")
