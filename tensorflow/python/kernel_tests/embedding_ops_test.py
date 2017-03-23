5164984cf22aa3ddaa234331a2e448463304232a
Ziming Dong <dzm1016397507@gmail.com>
2017-03-23 03:32:43 -0400

    modify unit test

--- tensorflow/python/kernel_tests/embedding_ops_test.py
+++ tensorflow/python/kernel_tests/embedding_ops_test.py
@@ -600,9 +600,9 @@ class EmbeddingLookupSparseTest(test.TestCase):
     grouped_ignored_weights = self._GroupByBatchEntry(
         np.ones(np.sum(vals_per_batch_entry)), vals_per_batch_entry)
 
-    for num_shards, combiner, dtype, ignore_weights in itertools.product(
-        [1, 5], ["sum", "mean", "sqrtn"], [dtypes.float32, dtypes.float64],
-        [True, False]):
+    for num_shards, combiner, dtype, ignore_weights, use_aggregation in \
+        itertools.product([1, 5], ["sum", "mean", "sqrtn"],
+            [dtypes.float32, dtypes.float64], [True, False], [True, False]):
 
       with self.test_session():
         p, params, feed_dict = _EmbeddingParams(
@@ -639,9 +639,9 @@ class EmbeddingLookupSparseTest(test.TestCase):
     sp_ids, sp_weights, _, _, _ = (
         self._RandomIdsAndWeights(batch_size, vocab_size))
 
-    for num_shards, combiner, dtype, ignore_weights in itertools.product(
-        [1, 3], ["sum", "mean", "sqrtn"], [dtypes.float32, dtypes.float64],
-        [True, False]):
+    for num_shards, combiner, dtype, ignore_weights, use_aggregation in \
+        itertools.product([1, 3], ["sum", "mean", "sqrtn"],
+            [dtypes.float32, dtypes.float64], [True, False], [True, False]):
       with self.test_session():
         x, params, _ = _EmbeddingParams(
             num_shards, vocab_size, shape=param_shape, dtype=dtype)
