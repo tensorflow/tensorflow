dbca4f78380d56c0b144daa058decaea2bbf55ff
Yong Tang <yong.tang.github@outlook.com>
2017-03-25 14:03:33 -0700

    Remove pandas imports, and use convert_to_tensor, as suggested in the review.
    This fix removes pandas imports, and use convert_to_tensor,
    as suggested in the review.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/framework/tensor_util_test.py
+++ tensorflow/python/framework/tensor_util_test.py
@@ -19,11 +19,11 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-import pandas as pd
 import sys
 
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_util
 from tensorflow.python.ops import array_ops
@@ -671,61 +671,13 @@ class TensorUtilTest(test.TestCase):
       def __array__(self, dtype=None):
         return np.asarray(self.array, dtype)
 
-    ma = MockArray(np.array([10, 20, 30]))
-    t = tensor_util.make_tensor_proto(ma)
-    if sys.byteorder == "big":  
-      self.assertProtoEquals("""  
-        dtype: DT_INT64  
-        tensor_shape { dim { size: 3 } }  
-        tensor_content: "\000\000\000\000\000\000\000\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036"  
-        """, t)  
-    else:  
-      self.assertProtoEquals("""
-        dtype: DT_INT64
-        tensor_shape { dim { size: 3 } }
-        tensor_content: "\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036\000\000\000\000\000\000\000"
-        """, t)
-    a = tensor_util.MakeNdarray(t)
-    self.assertEquals(np.int64, a.dtype)
-    self.assertAllClose(np.array([10, 20, 30], dtype=np.int64), a)
+    with self.test_session() as sess:
+      ma = MockArray(np.array([10, 20, 30]))
+      t = ops.convert_to_tensor(ma)
+      a = sess.run(t)
+      self.assertEquals(np.int64, a.dtype)
+      self.assertAllClose(np.array([10, 20, 30], dtype=np.int64), a)
 
-  def testSeries(self):
-    df = pd.Series([10, 20, 30])
-    t = tensor_util.make_tensor_proto(df)
-    if sys.byteorder == "big":  
-      self.assertProtoEquals("""  
-        dtype: DT_INT64  
-        tensor_shape { dim { size: 3 } }  
-        tensor_content: "\000\000\000\000\000\000\000\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036"  
-        """, t)  
-    else:  
-      self.assertProtoEquals("""
-        dtype: DT_INT64
-        tensor_shape { dim { size: 3 } }
-        tensor_content: "\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036\000\000\000\000\000\000\000"
-        """, t)
-    a = tensor_util.MakeNdarray(t)
-    self.assertEquals(np.int64, a.dtype)
-    self.assertAllClose(np.array([10, 20, 30], dtype=np.int64), a)
-
-  def testDataFrame(self):
-    df = pd.DataFrame({'A': [10], 'B': [20], 'C': [30]})
-    t = tensor_util.make_tensor_proto(df)
-    if sys.byteorder == "big":  
-      self.assertProtoEquals("""  
-        dtype: DT_INT64  
-        tensor_shape { dim { size: 1 } dim { size: 3 } }  
-        tensor_content: "\000\000\000\000\000\000\000\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036"  
-        """, t)  
-    else: 
-      self.assertProtoEquals("""
-        dtype: DT_INT64
-        tensor_shape { dim { size: 1 } dim { size: 3 } }
-        tensor_content: "\\n\000\000\000\000\000\000\000\024\000\000\000\000\000\000\000\036\000\000\000\000\000\000\000"
-        """, t)
-    a = tensor_util.MakeNdarray(t)
-    self.assertEquals(np.int64, a.dtype)
-    self.assertAllClose(np.array([[10, 20, 30]], dtype=np.int64), a)
 
 class ConstantValueTest(test.TestCase):
 
