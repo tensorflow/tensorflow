160acfa65ec910ac43240bdc8c1868d821d35a8f
Agis Oikonomou <agis@trueai.io>
2017-03-27 20:18:23 +0100

    moved _GeneratorFeedFn to contrib

--- tensorflow/contrib/learn/python/learn/dataframe/queues/feeding_functions.py
+++ tensorflow/contrib/learn/python/learn/dataframe/queues/feeding_functions.py
@@ -18,11 +18,56 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import random
 
 # pylint: disable=unused-import
 from tensorflow.python.estimator.inputs.queues.feeding_functions import _ArrayFeedFn
 from tensorflow.python.estimator.inputs.queues.feeding_functions import _enqueue_data as enqueue_data
 from tensorflow.python.estimator.inputs.queues.feeding_functions import _OrderedDictNumpyFeedFn
-from tensorflow.python.estimator.inputs.queues.feeding_functions import _GeneratorFeedFn
 from tensorflow.python.estimator.inputs.queues.feeding_functions import _PandasFeedFn
+from tensorflow.python.estimator.inputs.queues.feeding_functions import errors
 # pylint: enable=unused-import
+
+
+class _GeneratorFeedFn(object):
+    """Creates feed dictionaries from `Generator` of `dicts` of numpy arrays."""
+
+    def __init__(self,
+                 placeholders,
+                 generator,
+                 batch_size,
+                 random_start=False,
+                 seed=None,
+                 num_epochs=None):
+        first_sample = next(generator())
+        if len(placeholders) != len(first_sample):
+            raise ValueError("Expected {} placeholders; got {}.".format(
+                len(first_sample), len(placeholders)))
+        self._col_placeholders = placeholders
+        self._generator_function = generator
+        self._iterator = generator()
+        self._batch_size = batch_size
+        self._num_epochs = num_epochs
+        self._epoch = 0
+        random.seed(seed)
+
+    def __call__(self):
+        if self._num_epochs and self._epoch >= self._num_epochs:
+            raise errors.OutOfRangeError(None, None,
+                                         "Already emitted %s epochs." % self._epoch)
+        list_dict = {}
+        list_dict_size = 0
+        while list_dict_size < self._batch_size:
+            try:
+                data_row = next(self._iterator)
+            except StopIteration:
+                self._epoch += 1
+                self._iterator = self._generator_function()
+                data_row = next(self._iterator)
+            for index, key in enumerate(sorted(data_row.keys())):
+                list_dict.setdefault(
+                    self._col_placeholders[index], list()).append(data_row[key])
+            list_dict_size += 1
+        feed_dict = {key: np.asarray(item) for key, item
+                         in list(list_dict.items())}
+        return feed_dict
--- tensorflow/python/estimator/inputs/queues/feeding_functions.py
+++ tensorflow/python/estimator/inputs/queues/feeding_functions.py
@@ -34,6 +34,7 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.summary import summary
 from tensorflow.python.training import queue_runner
+from tensorflow.contrib.learn.python.learn.dataframe.queues.feeding_functions import _GeneratorFeedFn
 
 try:
   # pylint: disable=g-import-not-at-top
@@ -218,50 +219,6 @@ class _PandasFeedFn(object):
     feed_dict = dict(zip(self._col_placeholders, cols))
     feed_dict[self._index_placeholder] = result.index.values
     return feed_dict
-
-
-class _GeneratorFeedFn(object):
-    """Creates feed dictionaries from `Generator` of `dicts` of numpy arrays."""
-
-    def __init__(self,
-                 placeholders,
-                 generator,
-                 batch_size,
-                 random_start=False,
-                 seed=None,
-                 num_epochs=None):
-        first_sample = next(generator())
-        if len(placeholders) != len(first_sample):
-            raise ValueError("Expected {} placeholders; got {}.".format(
-                len(first_sample), len(placeholders)))
-        self._col_placeholders = placeholders
-        self._generator_function = generator
-        self._iterator = generator()
-        self._batch_size = batch_size
-        self._num_epochs = num_epochs
-        self._epoch = 0
-        random.seed(seed)
-
-    def __call__(self):
-        if self._num_epochs and self._epoch >= self._num_epochs:
-            raise errors.OutOfRangeError(None, None,
-                                         "Already emitted %s epochs." % self._epoch)
-        list_dict = {}
-        list_dict_size = 0
-        while list_dict_size < self._batch_size:
-            try:
-                data_row = next(self._iterator)
-            except StopIteration:
-                self._epoch += 1
-                self._iterator = self._generator_function()
-                data_row = next(self._iterator)
-            for index, key in enumerate(sorted(data_row.keys())):
-                list_dict.setdefault(
-                    self._col_placeholders[index], list()).append(data_row[key])
-            list_dict_size += 1
-        feed_dict = {key: np.asarray(item) for key, item
-                         in list(list_dict.items())}
-        return feed_dict
 
 
 def _enqueue_data(data,
