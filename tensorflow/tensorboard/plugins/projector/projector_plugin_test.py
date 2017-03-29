b1d6de28c5600e179d979cc635c812aca955d046
Dan Smilkov <smilkov@google.com>
2017-03-29 10:41:04 -0800

    Add plugin assets for the Embedding Projector and expose the API via tf.contrib.tensorboard.plugins.projector Change: 151603470

--- tensorflow/tensorboard/plugins/projector/projector_plugin_test.py
+++ tensorflow/tensorboard/plugins/projector/projector_plugin_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,17 +27,19 @@ import numpy as np
 
 from werkzeug import test as werkzeug_test
 from werkzeug import wrappers
-
 from google.protobuf import text_format
-from tensorflow.contrib.tensorboard.plugins.projector.projector_config_pb2 import ProjectorConfig
+from tensorflow.contrib.tensorboard.plugins.projector import projector_config_pb2
 from tensorflow.core.protobuf import saver_pb2
 from tensorflow.python.client import session
 from tensorflow.python.framework import ops
+from tensorflow.python.ops import image_ops
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import variables
 from tensorflow.python.platform import gfile
 from tensorflow.python.platform import test
+from tensorflow.python.summary import plugin_asset
+from tensorflow.python.summary.writer import writer
 from tensorflow.python.training import saver as saver_lib
 from tensorflow.tensorboard.backend import application
 from tensorflow.tensorboard.backend.event_processing import event_multiplexer
@@ -100,9 +103,9 @@ class ProjectorAppTest(test.TestCase):
     multiplexer = event_multiplexer.EventMultiplexer(
         size_guidance=application.DEFAULT_SIZE_GUIDANCE,
         purge_orphaned_data=True)
-    projector = projector_plugin.ProjectorPlugin()
-    projector.get_plugin_apps(multiplexer, self.log_dir)
-    plugins = {'projector': projector}
+    plugin = projector_plugin.ProjectorPlugin()
+    plugin.get_plugin_apps(multiplexer, self.log_dir)
+    plugins = {'projector': plugin}
     wsgi_app = application.TensorBoardWSGIApp(
         self.log_dir, plugins, multiplexer, reload_interval=0)
     self.server = werkzeug_test.Client(wsgi_app, wrappers.BaseResponse)
@@ -119,7 +122,7 @@ class ProjectorAppTest(test.TestCase):
 
   def _GenerateProjectorTestData(self):
     config_path = os.path.join(self.log_dir, 'projector_config.pbtxt')
-    config = ProjectorConfig()
+    config = projector_config_pb2.ProjectorConfig()
     embedding = config.embeddings.add()
     # Add an embedding by its canonical tensor name.
     embedding.tensor_name = 'var1:0'
@@ -139,6 +142,277 @@ class ProjectorAppTest(test.TestCase):
       saver = saver_lib.Saver(write_version=saver_pb2.SaverDef.V1)
       saver.save(sess, checkpoint_path)
 
+
+class MetadataColumnsTest(test.TestCase):
+
+  def testLengthDoesNotMatch(self):
+    metadata = projector_plugin.EmbeddingMetadata(10)
+
+    with self.assertRaises(ValueError):
+      metadata.add_column('Labels', [''] * 11)
+
+  def testValuesNot1D(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    values = np.array([[1, 2, 3]])
+
+    with self.assertRaises(ValueError):
+      metadata.add_column('Labels', values)
+
+  def testMultipleColumnsRetrieval(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('Sizes', [1, 2, 3])
+    metadata.add_column('Labels', ['a', 'b', 'c'])
+    self.assertEqual(metadata.column_names, ['Sizes', 'Labels'])
+    self.assertEqual(metadata.name_to_values['Labels'], ['a', 'b', 'c'])
+    self.assertEqual(metadata.name_to_values['Sizes'], [1, 2, 3])
+
+  def testValuesAreListofLists(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    values = [[1, 2, 3], [4, 5, 6]]
+    with self.assertRaises(ValueError):
+      metadata.add_column('Labels', values)
+
+  def testStringListRetrieval(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('Labels', ['a', 'B', 'c'])
+    self.assertEqual(metadata.name_to_values['Labels'], ['a', 'B', 'c'])
+    self.assertEqual(metadata.column_names, ['Labels'])
+
+  def testNumericListRetrieval(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('Labels', [1, 2, 3])
+    self.assertEqual(metadata.name_to_values['Labels'], [1, 2, 3])
+
+  def testNumericNdArrayRetrieval(self):
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('Labels', np.array([1, 2, 3]))
+    self.assertEqual(metadata.name_to_values['Labels'].tolist(), [1, 2, 3])
+
+  def testStringNdArrayRetrieval(self):
+    metadata = projector_plugin.EmbeddingMetadata(2)
+    metadata.add_column('Labels', np.array(['a', 'b']))
+    self.assertEqual(metadata.name_to_values['Labels'].tolist(), ['a', 'b'])
+
+  def testDuplicateColumnName(self):
+    metadata = projector_plugin.EmbeddingMetadata(2)
+    metadata.add_column('Labels', np.array(['a', 'b']))
+    with self.assertRaises(ValueError):
+      metadata.add_column('Labels', np.array(['a', 'b']))
+
+
+class ProjectorPluginAssetTest(test.TestCase):
+
+  def testNoAssets(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    self.assertEqual(manager.assets(), {'projector_config.pbtxt': ''})
+
+  def testAddEmbeddingNoMetadata(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    manager.add_embedding('test', np.array([[1, 2, 3.1]]))
+
+    config = projector_config_pb2.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = 'test'
+    embedding.tensor_shape.extend([1, 3])
+    embedding.tensor_path = 'test_values.tsv'
+    expected_config_pbtxt = text_format.MessageToString(config)
+
+    self.assertEqual(manager.assets(), {
+        'projector_config.pbtxt': expected_config_pbtxt,
+        'test_values.tsv': b'1\t2\t3.1\n'
+    })
+
+  def testAddEmbeddingIncorrectRank(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    with self.assertRaises(ValueError):
+      manager.add_embedding('test', np.array([1, 2, 3.1]))
+
+  def testAddEmbeddingWithTwoMetadataColumns(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('labels', ['a', 'b', 'друг јазик'])
+    metadata.add_column('sizes', [10, 20, 30])
+    manager.add_embedding('test', np.array([[1], [2], [3]]), metadata)
+
+    config = projector_config_pb2.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = 'test'
+    embedding.tensor_shape.extend([3, 1])
+    embedding.tensor_path = 'test_values.tsv'
+    embedding.metadata_path = 'test_metadata.tsv'
+    expected_config_pbtxt = text_format.MessageToString(config)
+
+    self.assertEqual(manager.assets(), {
+        'projector_config.pbtxt': expected_config_pbtxt,
+        'test_values.tsv': b'1\n2\n3\n',
+        'test_metadata.tsv': 'labels\tsizes\na\t10\nb\t20\nдруг јазик\t30\n'
+    })
+
+  def testAddEmbeddingWithOneMetadataColumn(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('labels', ['a', 'b', 'c'])
+    manager.add_embedding('test', np.array([[1], [2], [3]]), metadata)
+
+    config = projector_config_pb2.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = 'test'
+    embedding.tensor_shape.extend([3, 1])
+    embedding.tensor_path = 'test_values.tsv'
+    embedding.metadata_path = 'test_metadata.tsv'
+    expected_config_pbtxt = text_format.MessageToString(config)
+
+    self.assertEqual(manager.assets(), {
+        'projector_config.pbtxt': expected_config_pbtxt,
+        'test_values.tsv': b'1\n2\n3\n',
+        'test_metadata.tsv': 'a\nb\nc\n'
+    })
+
+  def testAddEmbeddingWithThumbnails(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    image1 = np.array([[[1, 2, 3], [4, 5, 6]],
+                       [[7, 8, 9], [10, 11, 12]]])
+    image2 = np.array([[[10, 20, 30], [40, 50, 60]],
+                       [[70, 80, 90], [100, 110, 120]]])
+    manager.add_embedding(
+        'test',
+        np.array([[1], [2], [3]]),
+        thumbnails=[image1, image2],
+        thumbnail_dim=[2, 2])
+
+    assets = manager.assets()
+
+    config = projector_config_pb2.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = 'test'
+    embedding.tensor_shape.extend([3, 1])
+    embedding.tensor_path = 'test_values.tsv'
+    embedding.sprite.image_path = 'test_sprite.png'
+    embedding.sprite.single_image_dim.extend([2, 2])
+    expected_config_pbtxt = text_format.MessageToString(config)
+
+    self.assertEqual(assets['projector_config.pbtxt'], expected_config_pbtxt)
+    self.assertEqual(assets['test_values.tsv'], b'1\n2\n3\n')
+
+    png_bytes = assets['test_sprite.png']
+    with ops.Graph().as_default():
+      s = session.Session()
+      image_array = image_ops.decode_png(png_bytes).eval(session=s).tolist()
+    expected_master_image = [
+        [[1, 2, 3], [4, 5, 6], [10, 20, 30], [40, 50, 60]],
+        [[7, 8, 9], [10, 11, 12], [70, 80, 90], [100, 110, 120]],
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    ]
+    self.assertEqual(image_array, expected_master_image)
+
+  def testAddEmbeddingWithSpriteImageButNoThumbnailDim(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    thumbnails = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    with self.assertRaises(ValueError):
+      manager.add_embedding(
+          'test', np.array([[1], [2], [3]]), thumbnails=thumbnails)
+
+  def testAddEmbeddingThumbnailDimNotAList(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    thumbnails = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    with self.assertRaises(ValueError):
+      manager.add_embedding(
+          'test', np.array([[1], [2], [3]]), thumbnails=thumbnails,
+          thumbnail_dim=4)
+
+  def testAddEmbeddingThumbnailDimNotOfLength2(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    thumbnails = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    with self.assertRaises(ValueError):
+      manager.add_embedding(
+          'test', np.array([[1], [2], [3]]), thumbnails=thumbnails,
+          thumbnail_dim=[4])
+
+  def testAddEmbeddingWithMetadataOfIncorrectLength(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('labels', ['a', 'b', 'c'])
+    # values has length 2, while metadata has length 3.
+    values = np.array([[1], [2]])
+
+    with self.assertRaises(ValueError):
+      manager.add_embedding('test', values, metadata)
+
+  def testAddMetadataForVariableButNoColumns(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    with self.assertRaises(ValueError):
+      manager.add_metadata_for_embedding_variable('test', metadata)
+
+  def testAddMetadataForVariable(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    metadata = projector_plugin.EmbeddingMetadata(3)
+    metadata.add_column('Labels', ['a', 'b', 'c'])
+    manager.add_metadata_for_embedding_variable('test', metadata)
+
+    config = projector_config_pb2.ProjectorConfig()
+    embedding = config.embeddings.add()
+    embedding.tensor_name = 'test'
+    embedding.metadata_path = 'test_metadata.tsv'
+    expected_config_pbtxt = text_format.MessageToString(config)
+
+    self.assertEqual(manager.assets(), {
+        'projector_config.pbtxt': expected_config_pbtxt,
+        'test_metadata.tsv': 'a\nb\nc\n'
+    })
+
+  def testAddMetadataForVariableAtLeastOneParamIsRequired(self):
+    manager = plugin_asset.get_plugin_asset(
+        projector_plugin.ProjectorPluginAsset)
+    with self.assertRaises(ValueError):
+      manager.add_metadata_for_embedding_variable('test')
+
+  def testNoAssetsProperSerializationOnDisk(self):
+    logdir = self.get_temp_dir()
+    plugin_dir = os.path.join(logdir, writer._PLUGINS_DIR,
+                              projector_plugin.ProjectorPluginAsset.plugin_name)
+
+    with ops.Graph().as_default() as g:
+      plugin_asset.get_plugin_asset(projector_plugin.ProjectorPluginAsset)
+      fw = writer.FileWriter(logdir)
+      fw.add_graph(g)
+
+    with gfile.Open(os.path.join(plugin_dir, 'projector_config.pbtxt')) as f:
+      content = f.read()
+    self.assertEqual(content, '')
+
+  def testNoReferenceToPluginNoSerializationOnDisk(self):
+    logdir = self.get_temp_dir()
+    plugin_dir = os.path.join(logdir, writer._PLUGINS_DIR,
+                              projector_plugin.ProjectorPluginAsset.plugin_name)
+
+    with ops.Graph().as_default() as g:
+      fw = writer.FileWriter(logdir)
+      fw.add_graph(g)
+
+    self.assertFalse(
+        gfile.Exists(plugin_dir),
+        'The projector plugin directory should not exist.')
 
 if __name__ == '__main__':
   test.main()
