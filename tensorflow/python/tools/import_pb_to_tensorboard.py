3172f84d1eefbdbb57c859396da7b0e9a1303d87
Jamie Cooke <jamie@iamjamiecooke.com>
2017-03-31 12:38:53 +0100

    Create import_pb_to_tensorboard.py
    In response to issue #8851, a quick tool to put a .pb model into tensorboard for visualization until a more friendly means appears.

---
+++ tensorflow/python/tools/import_pb_to_tensorboard.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+from tensorflow.python.framework.ops import reset_default_graph
+
+def import_to_tensorboard(modeldir,logdir):
+# import_to_tensorboard(modeldir,logdir) provides a quick way to visualize 
+# a .pb model in Tensorboard.
+# Created in response to issue #8851
+# Args:
+# 	modeldir: The location of the .pb model to visualize
+#	logdir: The location for the Tensorboard log to begin visualisation from.
+# Usage:
+# 	Call this function with your model location and desired log directory.
+#	Launch Tensorboard by pointing it to the log directory.
+#	View your imported .pb model as a graph.
+#
+#
+  reset_default_graph()
+  with tf.Session() as sess:
+    model_filename =modeldir
+    with gfile.FastGFile(model_filename, 'rb') as f:
+      graph_def = tf.GraphDef()
+      graph_def.ParseFromString(f.read())
+      g_in = tf.import_graph_def(graph_def)
+
+  pbVisualWriter = tf.summary.FileWriter(logdir)
+  pbVisualWriter.add_graph(sess.graph)
+  print("Model Imported. Visualize by running > tensorboard --logdir={}".format(logdir))
