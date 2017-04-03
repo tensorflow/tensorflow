01f03299baa4cb1f1b59ee7e370dc938aaba7a20
Petros Mol <pmol@google.com>
2017-04-03 14:18:28 -0800

    Add kernel_methods package to contrib Change: 152064888

--- tensorflow/contrib/__init__.py
+++ tensorflow/contrib/__init__.py
@@ -35,6 +35,7 @@ from tensorflow.contrib import image
 from tensorflow.contrib import input_pipeline
 from tensorflow.contrib import integrate
 from tensorflow.contrib import keras
+from tensorflow.contrib import kernel_methods
 from tensorflow.contrib import labeled_tensor
 from tensorflow.contrib import layers
 from tensorflow.contrib import learn
