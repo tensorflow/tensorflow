84d063709800034c217793aebd5ce4bf21c9adc1
Till Hoffmann <till@spotify.com>
2017-04-05 13:17:29 +0200

    Move poll_zmq to contrib.

---
+++ tensorflow/contrib/io/ops/io_ops.cc
@@ -0,0 +1,52 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/framework/common_shape_fns.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+
+namespace tensorflow {
+
+using shape_inference::DimensionHandle;
+using shape_inference::InferenceContext;
+using shape_inference::ShapeHandle;
+
+namespace {
+
+Status ScalarInputsAndOutputs(InferenceContext* c) {
+  ShapeHandle unused;
+  for (int i = 0; i < c->num_inputs(); ++i) {
+    TF_RETURN_IF_ERROR(c->WithRank(c->input(i), 0, &unused));
+  }
+  for (int i = 0; i < c->num_outputs(); ++i) {
+    c->set_output(i, c->Scalar());
+  }
+  return Status::OK();
+}
+
+}  // namespace
+
+REGISTER_OP("PollZmq")
+    .Input("request: string")
+    .Output("reply: string")
+    .Attr("address: string")
+    .Attr("timeout: int = -1")
+    .SetIsStateful()
+    .SetShapeFn(ScalarInputsAndOutputs)
+    .Doc(R"doc(
+Sends a message using ZeroMQ and returns the reply.
+)doc");
+
+}  // namespace tensorflow
---
+++ tensorflow/contrib/io/python/kernel_tests/io_ops_test.py
@@ -0,0 +1,53 @@
+﻿# -*- coding: utf-8 -*-
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for tensorflow.contrib.io."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import zmq
+
+from tensorflow.python.platform import test
+from tensorflow.python.util import compat
+from tensorflow.contrib import io as io_ops
+
+
+class IoOpsTest(test.TestCase):
+  def testPollZmq(self):
+    def _listener():
+      context = zmq.Context()
+      socket = context.socket(zmq.REP)
+      socket.bind("tcp://*:5555")
+
+      message = socket.recv()
+      socket.send(b"World")
+      self.assertEqual(message, b"Hello")
+
+    # Start a listener thread
+    thread = self.checkedThread(_listener)
+    thread.start()
+
+    with self.test_session() as sess:
+      response = io_ops.poll_zmq("Hello", "tcp://localhost:5555")
+      response_val = sess.run(response)
+      self.assertEqual(response_val, b"World")
+
+    thread.join()
+
+
+if __name__ == '__main__':
+  test.main()
