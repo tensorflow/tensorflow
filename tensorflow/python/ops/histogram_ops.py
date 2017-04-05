93521b575f0e33d1b43c4b7531353f297400386e
zhengjiajin <393926893@qq.com>
2017-04-06 03:37:16 +0800

    normalize code

--- tensorflow/python/ops/histogram_ops.py
+++ tensorflow/python/ops/histogram_ops.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 # pylint: disable=g-short-docstring-punctuation
 """Histograms.
 
