046bab08e91a4cd4834c92aae26e72ac307a16fb
drpngx <drpngx@users.noreply.github.com>
2017-04-10 17:49:29 -0700

    Add period to comply with style.

--- tensorflow/examples/learn/iris_custom_decay_dnn.py
+++ tensorflow/examples/learn/iris_custom_decay_dnn.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""Example of DNNClassifier for Iris plant dataset, with exponential decay"""
+"""Example of DNNClassifier for Iris plant dataset, with exponential decay."""
 
 from __future__ import absolute_import
 from __future__ import division
