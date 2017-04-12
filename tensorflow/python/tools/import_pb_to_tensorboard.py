44112bbab3eeaf73e811f35a97f5cfe5a684a46e
Jamie Cooke <jamie@iamjamiecooke.com>
2017-04-12 11:54:54 +0100

    Improve formatting and style guideline adherence
    Action the suggested changes

--- tensorflow/python/tools/import_pb_to_tensorboard.py
+++ tensorflow/python/tools/import_pb_to_tensorboard.py
@@ -3,12 +3,10 @@ from tensorflow.python.platform import gfile
 from tensorflow.python.framework.ops import reset_default_graph
 
 def import_to_tensorboard(modeldir,logdir):
-# import_to_tensorboard(modeldir,logdir) provides a quick way to visualize 
-# a .pb model in Tensorboard.
-# Created in response to issue #8854
+# View an imported protobuf model (.pb file) as a graph in Tensorboard
 # Args:
-# 	modeldir: The location of the .pb model to visualize
-#	logdir: The location for the Tensorboard log to begin visualisation from.
+# 	modeldir: The location of the protobuf (ph) model to visualize
+#	  logdir: The location for the Tensorboard log to begin visualisation from.
 # Usage:
 # 	Call this function with your model location and desired log directory.
 #	Launch Tensorboard by pointing it to the log directory.
@@ -17,12 +15,12 @@ def import_to_tensorboard(modeldir,logdir):
 #
   reset_default_graph()
   with tf.Session() as sess:
-    model_filename =modeldir
+    model_filename = modeldir
     with gfile.FastGFile(model_filename, 'rb') as f:
       graph_def = tf.GraphDef()
       graph_def.ParseFromString(f.read())
       g_in = tf.import_graph_def(graph_def)
 
-  pbVisualWriter = tf.summary.FileWriter(logdir)
-  pbVisualWriter.add_graph(sess.graph)
+  pb_visual_writer = tf.summary.FileWriter(logdir)
+  pb_visual_writer.add_graph(sess.graph)
   print("Model Imported. Visualize by running > tensorboard --logdir={}".format(logdir))
