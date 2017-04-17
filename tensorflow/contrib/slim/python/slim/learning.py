8382abdd54ae12ad5563c3df93107b7daecf3e04
Sergio Guadarrama <sguada@google.com>
2017-04-16 18:35:01 -0800

    Increase logging global step time decimals. Change: 153311513

--- tensorflow/contrib/slim/python/slim/learning.py
+++ tensorflow/contrib/slim/python/slim/learning.py
@@ -502,7 +502,7 @@ def train_step(sess, train_op, global_step, train_step_kwargs):
 
   if 'should_log' in train_step_kwargs:
     if sess.run(train_step_kwargs['should_log']):
-      logging.info('global step %d: loss = %.4f (%.2f sec/step)',
+      logging.info('global step %d: loss = %.4f (%.3f sec/step)',
                    np_global_step, total_loss, time_elapsed)
 
   # TODO(nsilberman): figure out why we can't put this into sess.run. The
