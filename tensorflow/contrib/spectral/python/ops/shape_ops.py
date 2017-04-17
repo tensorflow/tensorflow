a8a42868c5d68355024a5b6650b80f7b38349ec9
Androbin <robin.richtsfeld@gmail.com>
2017-04-17 20:44:08 +0200

    Update shape_ops.py

--- tensorflow/contrib/spectral/python/ops/shape_ops.py
+++ tensorflow/contrib/spectral/python/ops/shape_ops.py
@@ -63,7 +63,7 @@ def frames(signal, frame_length, frame_step, name="frames"):
     pad_length = (num_frames - 1) * frame_step + frame_length
     pad_signal = array_ops.pad(signal, [[0, 0], [0, pad_length - signal_length]])
     
-    indices_frames = array_ops.tile(aray_ops.expand_dims(math_ops.range(0, frame_length), 0), [num_frames, 1])
+    indices_frames = array_ops.tile(array_ops.expand_dims(math_ops.range(0, frame_length), 0), [num_frames, 1])
     indices_steps = array_ops.transpose(array_ops.tile(array_ops.expand_dims(math_ops.range(0, num_frames * frame_step, frame_step), 0), [frame_length, 1]))
     indices = indices_frames + indices_steps
     
