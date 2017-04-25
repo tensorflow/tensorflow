546befc40895749bc81b8d59c24fdf375a9a5034
A. Unique TensorFlower <gardener@tensorflow.org>
2017-04-25 14:25:14 -0800

    Go: Update generated wrapper functions for TensorFlow ops. Change: 154229675

--- tensorflow/go/op/wrappers.go
+++ tensorflow/go/op/wrappers.go
@@ -3284,7 +3284,7 @@ func Enter(scope *Scope, data tf.Output, frame_name string, optional ...EnterAtt
 // `Merge` waits for at least one of the tensors in `inputs` to become available.
 // It is usually combined with `Switch` to implement branching.
 //
-// `Merge` forwards the first tensor for become available to `output`, and sets
+// `Merge` forwards the first tensor to become available to `output`, and sets
 // `value_index` to its index in `inputs`.
 //
 // Arguments:
