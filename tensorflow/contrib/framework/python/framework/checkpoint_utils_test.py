0777da4faf9e5a1bed1c29356a7d129ec1d33340
A. Unique TensorFlower <gardener@tensorflow.org>
2017-04-25 14:23:39 -0800

    Bug fix to checkpoint_utils.init_from_checkpoint().
    Prior to this change, attempting to restore variables under a specific scope
    would also restore variables under other scopes whose names shared the same
    prefix, mangling the names slightly. For example, if a client tried to restore
    variables under 'my_scope/' and there was a variable named 'my_scope_1/foo' then
    the function would try to restore a variable with the mangled name 'my_scope/1/foo'.
    Change: 154229485

--- tensorflow/contrib/framework/python/framework/checkpoint_utils_test.py
+++ tensorflow/contrib/framework/python/framework/checkpoint_utils_test.py
@@ -144,6 +144,25 @@ class CheckpointsTest(test.TestCase):
         # Check that tensors are not explicitly in the graph.
         self.assertLess(len(str(session.graph.as_graph_def())), 27000)
 
+  def testInitWithScopeDoesNotCaptureSuffixes(self):
+    checkpoint_dir = self.get_temp_dir()
+    with self.test_session() as session:
+      _, _, _, v4 = _create_checkpoints(session, checkpoint_dir)
+
+    with ops.Graph().as_default() as g:
+      with variable_scope.variable_scope("useful_scope"):
+        my4 = variable_scope.get_variable("var4", [9, 9])
+      with variable_scope.variable_scope("useful_scope_1"):
+        my5_init = [[1.0, 2.0], [3.0, 4.0]]
+        my5 = variable_scope.get_variable("var5", initializer=my5_init)
+
+      checkpoint_utils.init_from_checkpoint(checkpoint_dir,
+                                            {"useful_scope/": "useful_scope/"})
+      with self.test_session(graph=g) as session:
+        session.run(variables.global_variables_initializer())
+        self.assertAllEqual(my4.eval(session), v4)
+        self.assertAllEqual(my5.eval(session), my5_init)
+
   def testInitFromRootCheckpoint(self):
     checkpoint_dir = self.get_temp_dir()
     with self.test_session() as session:
