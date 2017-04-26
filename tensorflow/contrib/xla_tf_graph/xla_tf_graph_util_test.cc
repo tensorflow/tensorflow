4f0dacd1f1995414b6656f9398aca133cda1c860
A. Unique TensorFlower <gardener@tensorflow.org>
2017-04-26 12:36:21 -0800

    Add a conversion from xla session module to a list of node structure as a step of xla computation transformation Change: 154337650

--- tensorflow/contrib/xla_tf_graph/xla_tf_graph_util_test.cc
+++ tensorflow/contrib/xla_tf_graph/xla_tf_graph_util_test.cc
@@ -17,6 +17,7 @@ limitations under the License.
 #include "tensorflow/cc/framework/scope.h"
 #include "tensorflow/cc/ops/function_ops.h"
 #include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/compiler/tf2xla/xla_op_registry.h"
 #include "tensorflow/compiler/xla/client/client_library.h"
 #include "tensorflow/core/platform/test.h"
 
@@ -27,6 +28,7 @@ static std::unique_ptr<Graph> BuildAddGraph() {
   Scope scope = Scope::NewRootScope().ExitOnError();
   auto a = ops::_Arg(scope.WithOpName("A"), DT_INT32, 0);
   auto b = ops::_Arg(scope.WithOpName("B"), DT_INT32, 1);
+  // See tf2xla/kernels/binary_ops.cc
   auto c = ops::Add(scope.WithOpName("C"), a, b);
   auto d = ops::_Retval(scope.WithOpName("D"), c, 0);
   std::unique_ptr<Graph> graph(new Graph(OpRegistry::Global()));
@@ -34,16 +36,75 @@ static std::unique_ptr<Graph> BuildAddGraph() {
   return graph;
 }
 
-TEST(XlaTfGraphUtil, ConvertTfGraphToHloModule) {
+static std::vector<XlaCompiler::Argument> BuildAddGraphArguments() {
   // Builds a description of the arguments.
   std::vector<XlaCompiler::Argument> args(2);
   args[0].kind = XlaCompiler::Argument::kParameter;
   args[0].type = DT_INT32;
-  args[0].shape = TensorShape({2});
+  // Difference of dimension will add extra broadcast_dimensions.
+  // broadcast_dimension generates an additional HloInstruction
+  // in user_computation.cc
+  args[0].shape = TensorShape({2, 2});
   args[1].kind = XlaCompiler::Argument::kParameter;
   args[1].type = DT_INT32;
   args[1].shape = TensorShape({2});
+  return args;
+}
 
+// CAVEAT: Debug purpose only.
+// This function dumps a protobuf string format of HloModule.
+static void DumpHloGraphForDebug(const std::vector<XlaCompiler::Argument>& args,
+                                 std::unique_ptr<Graph> graph) {
+  std::unique_ptr<FunctionLibraryDefinition> flib_def;
+  std::unique_ptr<FunctionLibraryRuntime> flr;
+  std::unique_ptr<XlaCompiler> compiler;
+
+  xla::Client* client = xla::ClientLibrary::LocalClientOrDie();
+  XlaOpRegistry::RegisterCompilationKernels();
+
+  FunctionDefLibrary flib;
+  flib_def.reset(new FunctionLibraryDefinition(OpRegistry::Global(), flib));
+
+  // Compiles the graph.
+  XlaCompiler::Options options;
+  options.device_type = DeviceType("XLA_CPU_JIT");
+  options.client = client;
+  compiler.reset(new XlaCompiler(options));
+
+  flr.reset(NewFunctionLibraryRuntime(compiler->device_mgr(), /*env=*/nullptr,
+                                      compiler->device(), TF_GRAPH_DEF_VERSION,
+                                      flib_def.get(), OptimizerOptions(),
+                                      /*custom_kernel_creator=*/nullptr));
+
+  // Compile graph
+  XlaCompiler::CompilationResult result;
+  TF_CHECK_OK(compiler->CompileGraph("dump", std::move(graph), flr.get(), args,
+                                     &result));
+
+  // Convert to hlo
+  xla::Computation& computation = result.computation;
+
+  xla::Service* service(
+      static_cast<xla::Service*>(xla::ClientLibrary::GetXlaService(
+          static_cast<xla::LocalClient*>(client)->platform())));
+  const xla::ComputationTracker& computation_tracker =
+      service->computation_tracker();
+
+  auto user_computation_status =
+      computation_tracker.Resolve(computation.handle());
+  TF_CHECK_OK(user_computation_status.status());
+  auto user_computation = user_computation_status.ConsumeValueOrDie();
+  xla::VersionedComputationHandle versioned_handle =
+      user_computation->GetVersionedHandle();
+  std::unique_ptr<xla::HloModule> hlo_module = std::move(
+      computation_tracker.BuildHloModule(versioned_handle).ValueOrDie());
+  VLOG(1) << "--- DUMP HLO ---";
+  VLOG(1) << hlo_module->ToString();
+}
+
+TEST(XlaTfGraphUtil, ConvertTfGraphToSessionModule) {
+  // Builds a description of the arguments.
+  std::vector<XlaCompiler::Argument> args = BuildAddGraphArguments();
   std::unique_ptr<Graph> graph = BuildAddGraph();
 
   TF_ASSIGN_OR_ASSERT_OK(
@@ -51,6 +112,21 @@ TEST(XlaTfGraphUtil, ConvertTfGraphToHloModule) {
       ConvertTfGraphToXlaSessionModule(args, std::move(graph)));
 
   ASSERT_EQ(5, session_module->entry().requests_size());
+
+  VLOG(1) << "--- DUMP ---";
+  VLOG(1) << session_module->DebugString();
+  DumpHloGraphForDebug(args, BuildAddGraph());
+}
+
+TEST(XlaTfGraphUtil, ConvertXlaSessionModuleToXlaNodes) {
+  std::vector<XlaCompiler::Argument> args = BuildAddGraphArguments();
+  std::unique_ptr<Graph> graph = BuildAddGraph();
+  TF_ASSIGN_OR_ASSERT_OK(
+      std::unique_ptr<xla::SessionModule> session_module,
+      ConvertTfGraphToXlaSessionModule(args, std::move(graph)));
+  TF_ASSIGN_OR_ASSERT_OK(auto xla_nodes,
+                         ConvertXlaSessionModuleToXlaNodes(*session_module));
+  EXPECT_EQ(session_module->entry().requests_size(), xla_nodes.size());
 }
 
 }  // namespace xla_tf_graph
