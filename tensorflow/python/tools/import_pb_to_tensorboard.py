f45ee5045515019bd2fa5f333f836b52f760e250
Vijay Vasudevan <vrv@google.com>
2017-05-01 16:18:24 -0700

    Fix code on behalf of user

--- tensorflow/python/tools/import_pb_to_tensorboard.py
+++ tensorflow/python/tools/import_pb_to_tensorboard.py
@@ -1,26 +1,24 @@
 import tensorflow as tf
-from tensorflow.python.platform import gfile
-from tensorflow.python.framework.ops import reset_default_graph
 
-def import_to_tensorboard(modeldir,logdir):
-# View an imported protobuf model (.pb file) as a graph in Tensorboard
-# Args:
-# 	modeldir: The location of the protobuf (ph) model to visualize
-#	  logdir: The location for the Tensorboard log to begin visualisation from.
-# Usage:
-# 	Call this function with your model location and desired log directory.
-#	Launch Tensorboard by pointing it to the log directory.
-#	View your imported .pb model as a graph.
-#
-#
-  reset_default_graph()
-  with tf.Session() as sess:
-    model_filename = modeldir
-    with gfile.FastGFile(model_filename, 'rb') as f:
+def import_to_tensorboard(model_dir, log_dir):
+  """View an imported protobuf model (.pb file) as a graph in Tensorboard.
+  
+  Args:
+  	model_dir: The location of the protobuf (pb) model to visualize
+    log_dir: The location for the Tensorboard log to begin visualisation from.
+    
+  Usage:
+  	Call this function with your model location and desired log directory.
+    Launch Tensorboard by pointing it to the log directory.
+    View your imported .pb model as a graph.
+  """
+  with tf.Session(graph=tf.Graph()) as sess:
+    with tf.gfile.FastGFile(model_dir, 'rb') as f:
       graph_def = tf.GraphDef()
       graph_def.ParseFromString(f.read())
       g_in = tf.import_graph_def(graph_def)
 
-  pb_visual_writer = tf.summary.FileWriter(logdir)
-  pb_visual_writer.add_graph(sess.graph)
-  print("Model Imported. Visualize by running > tensorboard --logdir={}".format(logdir))
+    pb_visual_writer = tf.summary.FileWriter(log_dir)
+    pb_visual_writer.add_graph(sess.graph)
+    print("Model Imported. Visualize by running: "
+          "> tensorboard --logdir={}".format(log_dir))
