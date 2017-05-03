27aaf4a653aa36e1c742fd859426f046ff6cf7af
Benoit Steiner <bsteiner@google.com>
2017-05-03 07:52:23 -0800

    Made sure that both queue shapes and types are updated in one single pass instead of two. Change: 154960772

--- tensorflow/core/grappler/costs/graph_properties.cc
+++ tensorflow/core/grappler/costs/graph_properties.cc
@@ -84,7 +84,7 @@ Status GraphProperties::InferStatically() {
           }
         }
       }
-      if (qctx->set_output_handle_dtype(0, queue_type) ||
+      if (qctx->set_output_handle_dtype(0, queue_type) |
           qctx->MergeOutputHandleShape(0, queue_shp)) {
         new_shapes.push(qnode);
       }
