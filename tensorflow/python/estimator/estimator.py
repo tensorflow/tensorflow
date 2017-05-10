8e667204cef6a5ed3c321ffeca1f1c7ce1d326cc
terrytangyuan <terrytangyuan@gmail.com>
2017-05-10 12:32:11 -0400

    Remove self from arg instead of changing a const

--- tensorflow/python/estimator/estimator.py
+++ tensorflow/python/estimator/estimator.py
@@ -737,7 +737,7 @@ def _model_fn_args(fn):
 
 def _verify_model_fn_args(model_fn, params):
   """Verifies model fn arguments."""
-  args = _model_fn_args(model_fn)
+  args = set(_model_fn_args(model_fn))
   if 'features' not in args:
     raise ValueError('model_fn (%s) must include features argument.' % model_fn)
   if 'labels' not in args:
@@ -751,8 +751,9 @@ def _verify_model_fn_args(model_fn, params):
                     'argument, but params are not passed to Estimator.',
                     model_fn)
   if tf_inspect.ismethod(model_fn):
-    _VALID_MODEL_FN_ARGS.add("self")
-  non_valid_args = list(set(args) - _VALID_MODEL_FN_ARGS)
+    if 'self' in args:
+      args.remove('self')
+  non_valid_args = list(args - _VALID_MODEL_FN_ARGS)
   if non_valid_args:
     raise ValueError('model_fn (%s) has following not expected args: %s' %
                      (model_fn, non_valid_args))
