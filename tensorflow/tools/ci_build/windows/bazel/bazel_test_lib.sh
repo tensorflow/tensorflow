9db7514c68bfa459dc53428d025162f1974af74f
Laszlo Csomor <laszlocsomor@google.com>
2017-05-12 10:51:15 +0200

    bazel_test_lib.sh: add envvars to CPU build config

--- tensorflow/tools/ci_build/windows/bazel/bazel_test_lib.sh
+++ tensorflow/tools/ci_build/windows/bazel/bazel_test_lib.sh
@@ -178,6 +178,10 @@ function run_configure_for_cpu_build {
   if [ -z "$TF_NEED_MKL" ]; then
     export TF_NEED_MKL=0
   fi
+  export TF_NEED_VERBS=0
+  export TF_NEED_GCP=0
+  export TF_NEED_HDFS=0
+  export TF_NEED_OPENCL=0
   echo "" | ./configure
 }
 
