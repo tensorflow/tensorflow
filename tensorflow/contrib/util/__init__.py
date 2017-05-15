5d628d2cecdbf1f95565d70e32a8d90ac8150e47
Dan Ringwalt <ringwalt@google.com>
2017-05-15 10:59:58 -0700

    Automated g4 rollback of changelist 155868794
    PiperOrigin-RevId: 156075609

--- tensorflow/contrib/util/__init__.py
+++ tensorflow/contrib/util/__init__.py
@@ -18,7 +18,6 @@
 See @{$python/contrib.util} guide.
 
 @@constant_value
-@@create_example
 @@make_tensor_proto
 @@make_ndarray
 @@ops_used_by_graph_def
@@ -31,11 +30,11 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=unused-import
-from tensorflow.contrib.util.create_example import create_example
 from tensorflow.python.framework.meta_graph import ops_used_by_graph_def
 from tensorflow.python.framework.meta_graph import stripped_op_list_for_graph
 from tensorflow.python.framework.tensor_util import constant_value
 from tensorflow.python.framework.tensor_util import make_tensor_proto
 from tensorflow.python.framework.tensor_util import MakeNdarray as make_ndarray
+# pylint: disable=unused_import
 from tensorflow.python.util.all_util import remove_undocumented
 remove_undocumented(__name__)
