2b90ef158b9dd07b71453f3254a6a8a16501cdfa
A. Unique TensorFlower <gardener@tensorflow.org>
2017-05-15 04:03:31 -0700

    Fix importing a scoped metagraph into a graph that has an active name scope.
    PiperOrigin-RevId: 156038375

--- tensorflow/python/framework/meta_graph_test.py
+++ tensorflow/python/framework/meta_graph_test.py
@@ -335,6 +335,20 @@ class ScopedMetaGraphTest(test.TestCase):
     for a, b in zip(orig_meta_graphs, new_meta_graphs):
       test_util.assert_meta_graph_protos_equal(self, a, b)
 
+  def testScopedImportUnderNameScope(self):
+    graph = ops.Graph()
+    with graph.as_default():
+      variables.Variable(initial_value=1.0, trainable=True, name="myvar")
+    meta_graph_def, _ = meta_graph.export_scoped_meta_graph(graph=graph)
+
+    graph = ops.Graph()
+    with graph.as_default():
+      with ops.name_scope("foo"):
+        imported_variables = meta_graph.import_scoped_meta_graph(
+            meta_graph_def, import_scope="bar")
+        self.assertEqual(len(imported_variables), 1)
+        self.assertEqual(imported_variables.values()[0].name, "foo/bar/myvar:0")
+
   def testScopedImportWithSelectedCollections(self):
     meta_graph_filename = os.path.join(
         _TestDir("selected_collections_import"), "meta_graph.pb")
