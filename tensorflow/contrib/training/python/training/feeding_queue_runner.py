40255434c66b19ccabf23c9a451dd727512a2dd6
Martin Wicke <wicke@google.com>
2017-05-17 11:47:42 -0700

    Deprecate contrib/learn/dataframe. To be removed June 15.
    PiperOrigin-RevId: 156333930

--- tensorflow/contrib/training/python/training/feeding_queue_runner.py
+++ tensorflow/contrib/training/python/training/feeding_queue_runner.py
@@ -19,12 +19,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.python.estimator.inputs.queues.feeding_queue_runner import _FeedingQueueRunner
-from tensorflow.python.util.deprecation import deprecated
-
-
-class FeedingQueueRunner(_FeedingQueueRunner):
-
-  @deprecated('2017-06-15', 'Moved to tf.contrib.training.FeedingQueueRunner.')
-  def __init__(self, *args, **kwargs):
-    super(FeedingQueueRunner, self).__init__(*args, **kwargs)
+# pylint: disable=unused-import
+from tensorflow.python.estimator.inputs.queues.feeding_queue_runner import _FeedingQueueRunner as FeedingQueueRunner
+# pylint: enable=unused-import
