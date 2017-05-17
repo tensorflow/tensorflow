931fd84bb72df0500f512d5d92ec0bef2ea461be
Fei Sun <sunfei.china@outlook.com>
2017-05-18 02:00:48 +0800

    fix numpy.prod and GatherGrad overflow for large tensors inside windows (#9780)
    * fix numpy.prod overflow for windows
    
    * fix GatherGrad large shape exception for windows
    
    * add __long__ for Dimension; modify comment according to review

--- tensorflow/python/ops/array_grad.py
+++ tensorflow/python/ops/array_grad.py
@@ -354,9 +354,15 @@ def _PreventGradientGrad(op, _):
 def _GatherGrad(op, grad):
   """Gradient for Gather op."""
   # params can be large, so colocate the shape calculation with it.
+  #
+  # params can be very large for sparse model, array_ops.shape raises
+  # exception on the Windows platform when any dimension is larger than
+  # int32. params_shape is not used in optimizer apply_sparse gradients,
+  # so it's fine to convert it back to int32 regardless of truncation.
   params = op.inputs[0]
   with ops.colocate_with(params):
-    params_shape = array_ops.shape(params)
+    params_shape = array_ops.shape(params, out_type=ops.dtypes.int64)
+    params_shape = math_ops.to_int32(params_shape)
 
   # Build appropriately shaped IndexedSlices
   indices = op.inputs[1]
