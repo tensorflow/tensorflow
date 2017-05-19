d2e4eb64729d0b05992e6f552b863635137872f7
Simon Perkins <simon.perkins@gmail.com>
2017-05-19 20:39:25 +0200

    Add #include <numeric> for windows builds

--- tensorflow/core/kernels/map_stage_op.cc
+++ tensorflow/core/kernels/map_stage_op.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 
 #include <map>
+#include <numeric>
 #include <unordered_map>
 #include <vector>
 
