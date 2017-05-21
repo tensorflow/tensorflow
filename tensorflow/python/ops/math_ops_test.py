c2c98be69607b301f9fe0a5231b5226d7eb60f8b
Xiang Gao <qasdfgtyuiop@gmail.com>
2017-05-21 01:29:14 -0400

    fix test

--- tensorflow/python/ops/math_ops_test.py
+++ tensorflow/python/ops/math_ops_test.py
@@ -450,7 +450,7 @@ class ReduceSliceTest(test_util.TensorFlowTestCase):
     x = np.array([[[1, 2], [3, 4]], [[50, 60], [70, 80]], [[600, 700], [800, 900]]], dtype=np.int32)
     indices = np.array([[0, 1], [0, 3], [1, 2], [1, 3], [0,2]], dtype=np.int32)
     result = np.array([[[1, 2], [3, 4]],
-                       [[651, 762], [873, 983]],
+                       [[651, 762], [873, 984]],
                        [[50, 60], [70, 80]],
                        [[650, 760], [870, 980]],
                        [[51, 62], [73, 84]]], dtype=np.int32)
@@ -471,7 +471,7 @@ class ReduceSliceTest(test_util.TensorFlowTestCase):
     indices = np.array([[0, 1], [0, 3], [1, 2], [1, 3], [0,2]], dtype=np.int32)
     result = np.array([[1, 2, 3], [28, 80, 162], [4, 5, 6], [28, 40, 54], [4, 10, 18]], dtype=np.int32)
     with self.test_session(use_gpu=True):
-      y_tf = math_ops.partial_prod(x,indices).eval()
+      y_tf = math_ops.reduce_slice_prod(x,indices).eval()
       self.assertAllEqual(y_tf, result)
 
   def testReduceSliceMax(self):
