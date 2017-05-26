Fix attention score padding. Use -inf as default instead of 0 for softmax.
PiperOrigin-RevId: 157214300

tensorflow/contrib/seq2seq/python/ops/attention_wrapper.py
@@ -112,6 +112,18 @@ def _prepare_memory(memory, memory_sequence_length, check_inner_dims_defined):
   return nest.map_structure(lambda m: _maybe_mask(m, seq_len_mask), memory)
 
 
+def _maybe_mask_score(score, memory_sequence_length, score_mask_value):
+  if memory_sequence_length is None:
+    return score
+  message = ("All values in memory_sequence_length must greater than zero.")
+  with ops.control_dependencies(
+      [check_ops.assert_positive(memory_sequence_length, message=message)]):
+    score_mask = array_ops.sequence_mask(
+        memory_sequence_length, maxlen=array_ops.shape(score)[1])
+    score_mask_values = score_mask_value * array_ops.ones_like(score)
+    return array_ops.where(score_mask, score, score_mask_values)
+
+
 class _BaseAttentionMechanism(AttentionMechanism):
   """A base AttentionMechanism class providing common functionality.
 
@@ -127,6 +139,7 @@ class _BaseAttentionMechanism(AttentionMechanism):
                memory_sequence_length=None,
                memory_layer=None,
                check_inner_dims_defined=True,
+               score_mask_value=float("-inf"),
                name=None):
     """Construct base AttentionMechanism class.
 
@@ -149,6 +162,9 @@ class _BaseAttentionMechanism(AttentionMechanism):
       check_inner_dims_defined: Python boolean.  If `True`, the `memory`
         argument's shape is checked to ensure all but the two outermost
         dimensions are fully defined.
+      score_mask_value: (optional): The mask value for score before passing into
+        `probability_fn`. The default is -inf. Only used if
+        `memory_sequence_length` is not None.
       name: Name to use when creating ops.
     """
     if (query_layer is not None
@@ -164,7 +180,10 @@ class _BaseAttentionMechanism(AttentionMechanism):
     if not callable(probability_fn):
       raise TypeError("probability_fn must be callable, saw type: %s" %
                       type(probability_fn).__name__)
-    self._probability_fn = probability_fn
+    self._probability_fn = lambda score, prev: (  # pylint:disable=g-long-lambda
+        probability_fn(
+            _maybe_mask_score(score, memory_sequence_length, score_mask_value),
+            prev))
     with ops.name_scope(
         name, "BaseAttentionMechanismInit", nest.flatten(memory)):
       self._values = _prepare_memory(
@@ -245,6 +264,7 @@ class LuongAttention(_BaseAttentionMechanism):
                memory_sequence_length=None,
                scale=False,
                probability_fn=None,
+               score_mask_value=float("-inf"),
                name="LuongAttention"):
     """Construct the AttentionMechanism mechanism.
 
@@ -260,6 +280,9 @@ class LuongAttention(_BaseAttentionMechanism):
         probabilities.  The default is @{tf.nn.softmax}. Other options include
         @{tf.contrib.seq2seq.hardmax} and @{tf.contrib.sparsemax.sparsemax}.
         Its signature should be: `probabilities = probability_fn(score)`.
+      score_mask_value: (optional): The mask value for score before passing into
+        `probability_fn`. The default is -inf. Only used if
+        `memory_sequence_length` is not None.
       name: Name to use when creating ops.
     """
     # For LuongAttention, we only transform the memory layer; thus
@@ -274,6 +297,7 @@ class LuongAttention(_BaseAttentionMechanism):
         memory=memory,
         probability_fn=wrapped_probability_fn,
         memory_sequence_length=memory_sequence_length,
+        score_mask_value=score_mask_value,
         name=name)
     self._num_units = num_units
     self._scale = scale
@@ -362,6 +386,7 @@ class BahdanauAttention(_BaseAttentionMechanism):
                memory_sequence_length=None,
                normalize=False,
                probability_fn=None,
+               score_mask_value=float("-inf"),
                name="BahdanauAttention"):
     """Construct the Attention mechanism.
 
@@ -377,6 +402,9 @@ class BahdanauAttention(_BaseAttentionMechanism):
         probabilities.  The default is @{tf.nn.softmax}. Other options include
         @{tf.contrib.seq2seq.hardmax} and @{tf.contrib.sparsemax.sparsemax}.
         Its signature should be: `probabilities = probability_fn(score)`.
+      score_mask_value: (optional): The mask value for score before passing into
+        `probability_fn`. The default is -inf. Only used if
+        `memory_sequence_length` is not None.
       name: Name to use when creating ops.
     """
     if probability_fn is None:
@@ -390,6 +418,7 @@ class BahdanauAttention(_BaseAttentionMechanism):
         memory=memory,
         probability_fn=wrapped_probability_fn,
         memory_sequence_length=memory_sequence_length,
+        score_mask_value=score_mask_value,
         name=name)
     self._num_units = num_units
     self._normalize = normalize