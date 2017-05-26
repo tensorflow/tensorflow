eae0b5f53d0c7a13308e616135a75f6228bddb1a
A. Unique TensorFlower <gardener@tensorflow.org>
2017-05-26 16:25:36 -0700

    Remove unused using-declarations
    PiperOrigin-RevId: 157276276

--- tensorflow/contrib/image/kernels/single_image_random_dot_stereograms_ops.cc
+++ tensorflow/contrib/image/kernels/single_image_random_dot_stereograms_ops.cc
@@ -19,8 +19,6 @@ limitations under the License.
 
 namespace tensorflow {
 
-using shape_inference::InferenceContext;
-
 template <typename T>
 class SingleImageRandomDotStereogramsOp : public OpKernel {
  private:
