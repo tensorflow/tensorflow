060d67b34adf0058c3005db70ad4faeec0474992
Taehoon Lee <taehoonlee@snu.ac.kr>
2017-05-28 21:38:56 +0900

    Fix typos

--- tensorflow/contrib/keras/python/keras/applications/resnet50.py
+++ tensorflow/contrib/keras/python/keras/applications/resnet50.py
@@ -172,7 +172,7 @@ def ResNet50(include_top=True,
       input_shape: optional shape tuple, only to be specified
           if `include_top` is False (otherwise the input shape
           has to be `(224, 224, 3)` (with `channels_last` data format)
-          or `(3, 224, 244)` (with `channels_first` data format).
+          or `(3, 224, 224)` (with `channels_first` data format).
           It should have exactly 3 inputs channels,
           and width and height should be no smaller than 197.
           E.g. `(200, 200, 3)` would be one valid value.
