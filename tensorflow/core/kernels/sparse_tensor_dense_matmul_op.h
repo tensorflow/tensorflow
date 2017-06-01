af05777ad9026726c715bbee476f37d35fe4a190
zycdragonball <zycdragonball@gmail.com>
2017-06-01 19:52:13 -0400

    Update according to rmlarsen's suggestions

--- tensorflow/core/kernels/sparse_tensor_dense_matmul_op.h
+++ tensorflow/core/kernels/sparse_tensor_dense_matmul_op.h
@@ -36,28 +36,28 @@ struct SparseTensorDenseMatMulFunctor {
       typename TTypes<T, NDIM>::ConstTensor b);
 };
 
-template <typename MATRIX, bool ADJ, int NDIM>
+template <typename TENSOR, bool ADJ, int NDIM>
 class MaybeAdjoint;
 
-template <typename MATRIX, int NDIM>
-class MaybeAdjoint<MATRIX, false, NDIM> {
+template <typename TENSOR, int NDIM>
+class MaybeAdjoint<TENSOR, false, NDIM> {
  public:
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE MaybeAdjoint(MATRIX m) : m_(m) {}
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename MATRIX::Scalar operator()(
-      const typename MATRIX::Index i, const typename MATRIX::Index j) const {
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE MaybeAdjoint(TENSOR m) : m_(m) {}
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename TENSOR::Scalar operator()(
+      const typename TENSOR::Index i, const typename TENSOR::Index j) const {
     return m_(i, j);
   }
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename MATRIX::Scalar operator()(
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename TENSOR::Scalar operator()(
       const Eigen::array<Eigen::Index, NDIM> indices) const {
     return m_(indices);
   }
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename MATRIX::Scalar operator()(
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename TENSOR::Scalar operator()(
       const Eigen::array<int, NDIM> indices) const {
     return m_(indices);
   }
 
  private:
-  const MATRIX m_;
+  const TENSOR m_;
 };
 
 template <typename T>
@@ -65,15 +65,15 @@ EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE T MaybeConj(T v) {
   return v;
 }
 
-template <typename MATRIX, int NDIM>
-class MaybeAdjoint<MATRIX, true, NDIM> {
+template <typename TENSOR, int NDIM>
+class MaybeAdjoint<TENSOR, true, NDIM> {
  public:
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE MaybeAdjoint(MATRIX m) : m_(m) {}
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename MATRIX::Scalar operator()(
-      const typename MATRIX::Index i, const typename MATRIX::Index j) const {
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE MaybeAdjoint(TENSOR m) : m_(m) {}
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE typename TENSOR::Scalar operator()(
+      const typename TENSOR::Index i, const typename TENSOR::Index j) const {
     return Eigen::numext::conj(m_(j, i));
   }
-  typename MATRIX::Scalar operator()(
+  typename TENSOR::Scalar operator()(
       const Eigen::array<Eigen::Index, NDIM> indices) const {
     Eigen::array<Eigen::Index, NDIM> swapped(indices);
     Eigen::Index tmp = swapped[NDIM - 2];
@@ -81,7 +81,7 @@ class MaybeAdjoint<MATRIX, true, NDIM> {
     swapped[NDIM - 1] = tmp;
     return Eigen::numext::conj(m_(swapped));
   }
-  typename MATRIX::Scalar operator()(
+  typename TENSOR::Scalar operator()(
       const Eigen::array<int, NDIM> indices) const {
     Eigen::array<int, NDIM> swapped(indices);
     Eigen::Index tmp = swapped[NDIM - 2];
@@ -91,7 +91,7 @@ class MaybeAdjoint<MATRIX, true, NDIM> {
   }
 
  private:
-  const MATRIX m_;
+  const TENSOR m_;
 };
 
 }  // end namespace functor
