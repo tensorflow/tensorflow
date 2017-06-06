3e43a4422b99bf6d79547e12c8b6afbfbb06f61e
Simon Perkins <simon.perkins@gmail.com>
2017-06-06 10:37:15 +0200

    Improve imports
    Avoid importing base tensorflow package

--- tensorflow/python/kernel_tests/map_stage_op_test.py
+++ tensorflow/python/kernel_tests/map_stage_op_test.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow import errors
+from tensorflow.python.framework import errors
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
--- tensorflow/python/kernel_tests/stage_op_test.py
+++ tensorflow/python/kernel_tests/stage_op_test.py
@@ -16,7 +16,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
