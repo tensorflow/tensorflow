379aa9911f97b4bb2b493e31ad7227e63907508b
A. Unique TensorFlower <gardener@tensorflow.org>
2017-06-07 15:22:24 -0700

    Go: Update generated wrapper functions for TensorFlow ops.
    PiperOrigin-RevId: 158325855

--- tensorflow/go/op/wrappers.go
+++ tensorflow/go/op/wrappers.go
@@ -3797,7 +3797,7 @@ func TensorArrayWriteV3(scope *Scope, handle tf.Output, index tf.Output, value t
 //
 // TensorArray gradient calls use an accumulator TensorArray object.  If
 // multiple gradients are calculated and run in the same session, the multiple
-// gradient nodes may accidentally flow throuth the same accumulator TensorArray.
+// gradient nodes may accidentally flow through the same accumulator TensorArray.
 // This double counts and generally breaks the TensorArray gradient flow.
 //
 // The solution is to identify which gradient call this particular
