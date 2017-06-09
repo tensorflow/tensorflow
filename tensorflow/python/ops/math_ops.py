79deaeebe99712a6ef467f57db3a7fdec94ae6a1
Lakshay Garg <lakshay.garg.1996@gmail.com>
2017-06-09 21:46:17 +0530

    Fixes python doc for tanh (Resolves #10376)

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -2057,12 +2057,11 @@ def tanh(x, name=None):
 
   Args:
     x: A Tensor or SparseTensor with type `float`, `double`, `int32`,
-      `complex64`, `int64`, or `qint32`.
+      `complex64`, or `int64`.
     name: A name for the operation (optional).
 
   Returns:
-    A Tensor or SparseTensor respectively with the same type as `x` if
-    `x.dtype != qint32` otherwise the return type is `quint8`.
+    A Tensor or SparseTensor respectively with the same type as `x`.
   """
   with ops.name_scope(name, "Tanh", [x]) as name:
     if isinstance(x, sparse_tensor.SparseTensor):
