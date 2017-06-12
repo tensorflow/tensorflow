Fix incorrect results from Eigen-based IRFFT kernel when input dimensions exceed fft_length.
Also fixes fft_ops_test msan failures for rank > 1 IRFFTs. The temporary buffer used for computing the (rank - 1) outer IFFTs is not fully initialized. Instead, only compute the (rank - 1) outer IFFTs over the valid positions of the temporary buffer.

PiperOrigin-RevId: 158772209

tensorflow/python/kernel_tests/fft_ops_test.py
@@ -351,20 +351,13 @@ class RFFTOpsTest(BaseFFTOpsTest):
             # Test truncation (FFT size < dimensions).
             fft_length = (size - 2,) * rank
             self._CompareForward(r2c.astype(np.float32), rank, fft_length)
-            if test.is_gpu_available():
-              # TODO(rjryan): figure out why eigen kernel gives wrong answer.
-              self._CompareBackward(c2r.astype(np.complex64), rank, fft_length)
+            self._CompareBackward(c2r.astype(np.complex64), rank, fft_length)
 
             # Confirm it works with unknown shapes as well.
             self._CompareForward(r2c.astype(np.float32), rank, fft_length,
                                  use_placeholder=True)
-            if test.is_gpu_available():
-              # TODO(rjryan): figure out why eigen kernel gives wrong answer.
-              self._CompareBackward(
-                  c2r.astype(np.complex64),
-                  rank,
-                  fft_length,
-                  use_placeholder=True)
+            self._CompareBackward(c2r.astype(np.complex64), rank, fft_length,
+                                  use_placeholder=True)
 
             # Test padding (FFT size > dimensions).
             fft_length = (size + 2,) * rank