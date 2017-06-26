f0f5f6a6798e81a8100f31cfeed2991b7998411f
drpngx <drpngx@users.noreply.github.com>
2017-06-26 11:32:09 -0700

    Add from __future__ as per style guide.

--- tensorflow/examples/label_image/label_image.py
+++ tensorflow/examples/label_image/label_image.py
@@ -13,6 +13,10 @@
 # limitations under the License.
 # ==============================================================================
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import argparse
 import sys
 
