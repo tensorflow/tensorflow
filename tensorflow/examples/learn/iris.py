a132b8330039e7ed326d090cdae35c97561f68b1
A. Unique TensorFlower <gardener@tensorflow.org>
2017-06-27 08:35:47 -0700

    Updates some more examples in examples/learn.
    PiperOrigin-RevId: 160278757

--- tensorflow/examples/learn/iris.py
+++ tensorflow/examples/learn/iris.py
@@ -25,6 +25,9 @@ from sklearn import model_selection
 import tensorflow as tf
 
 
+X_FEATURE = 'x'  # Name of the input feature.
+
+
 def main(unused_argv):
   # Load dataset.
   iris = datasets.load_iris()
@@ -33,18 +36,19 @@ def main(unused_argv):
 
   # Build 3 layer DNN with 10, 20, 10 units respectively.
   feature_columns = [
-      tf.feature_column.numeric_column('x', shape=np.array(x_train).shape[1:])]
+      tf.feature_column.numeric_column(
+          X_FEATURE, shape=np.array(x_train).shape[1:])]
   classifier = tf.estimator.DNNClassifier(
       feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=3)
 
   # Train.
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={'x': x_train}, y=y_train, num_epochs=None, shuffle=True)
+      x={X_FEATURE: x_train}, y=y_train, num_epochs=None, shuffle=True)
   classifier.train(input_fn=train_input_fn, steps=200)
 
   # Predict.
   test_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={'x': x_test}, y=y_test, num_epochs=1, shuffle=False)
+      x={X_FEATURE: x_test}, y=y_test, num_epochs=1, shuffle=False)
   predictions = classifier.predict(input_fn=test_input_fn)
   y_predicted = np.array(list(p['class_ids'] for p in predictions))
   y_predicted = y_predicted.reshape(np.array(y_test).shape)
