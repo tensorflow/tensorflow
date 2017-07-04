a8a10b68d70121dce5a2901c1c2cb535dc95bd28
Luke Iwanski <luke@codeplay.com>
2017-07-04 04:31:08 +0100

    [OpenCL] Fixes warning caused by half type (#11177)
    * [OpenCL] Fixes warning caused by half type
    
      - half requires opencl extension that might not be avaiable on all platforms
      - extends cast operation to cover int8, int16, uint8, uint16
    
    * Fixed typo
    
    * Fixes formatting

--- tensorflow/core/kernels/cast_op_impl_int64.cc
+++ tensorflow/core/kernels/cast_op_impl_int64.cc
@@ -38,9 +38,9 @@ GetGpuCastFromInt64(DataType dst_dtype) {
 typedef Eigen::SyclDevice SYCLDevice;
 std::function<void(OpKernelContext*, const Tensor&, Tensor*)>
 GetSyclCastFromInt64(DataType dst_dtype) {
-  CURRY_TYPES3(CAST_CASE, SYCLDevice, int64);
+  CURRY_TYPES3_NO_HALF(CAST_CASE, SYCLDevice, int64);
   return nullptr;
 }
-#endif // TENSORFLOW_USE_SYCL
+#endif  // TENSORFLOW_USE_SYCL
 
 }  // namespace tensorflow
