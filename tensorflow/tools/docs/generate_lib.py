4d1c87f1a28d3b43010f486aeea9b537764aa191
Mark Daoust <markdaoust@google.com>
2017-07-06 04:53:14 -0700

    Upgrade the python doc generator's `ReferenceResolver` to handle errors consistently.
    Report all errors, and the name of the file they occurred in.
    
    The errors are not serialized with the reference resolver.
    
    PiperOrigin-RevId: 161066611

--- tensorflow/tools/docs/generate_lib.py
+++ tensorflow/tools/docs/generate_lib.py
@@ -91,6 +91,7 @@ def write_docs(output_dir, parser_config, yaml_toc):
 
   # Parse and write Markdown pages, resolving cross-links (@{symbol}).
   for full_name, py_object in six.iteritems(parser_config.index):
+    parser_config.reference_resolver.current_doc_full_name = full_name
 
     if full_name in parser_config.duplicate_of:
       continue
@@ -391,6 +392,9 @@ def _other_docs(src_dir, output_dir, reference_resolver):
         print('Skipping excluded file %s...' % base_name)
         continue
       full_in_path = os.path.join(dirpath, base_name)
+
+      reference_resolver.current_doc_full_name = full_in_path
+
       suffix = os.path.relpath(path=full_in_path, start=src_dir)
       full_out_path = os.path.join(output_dir, suffix)
       if not base_name.endswith('.md'):
@@ -508,7 +512,6 @@ class DocGenerator(object):
     write_docs(output_dir, parser_config, yaml_toc=self.yaml_toc)
     _other_docs(flags.src_dir, flags.output_dir, reference_resolver)
 
-    if parser.all_errors:
-      print('Errors during processing:\n  ' + '\n  '.join(parser.all_errors))
-      return 1
-    return 0
+    parser_config.reference_resolver.log_errors()
+
+    return parser_config.reference_resolver.num_errors()
