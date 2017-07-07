982db6094843d23cfae4a2cf4b70cf229140a18f
Dhruv <printdhruv@yahoo.com>
2017-07-06 19:42:15 -0700

    removed summary duplicate import
    Have to check test on Linux and mac-OS

--- tensorflow/contrib/learn/python/learn/estimators/head.py
+++ tensorflow/contrib/learn/python/learn/estimators/head.py
@@ -36,8 +36,7 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import sparse_tensor
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import control_flow_ops
-from tensorflow.python import summary 
+from tensorflow.python.ops import control_flow_ops 
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import sparse_ops
