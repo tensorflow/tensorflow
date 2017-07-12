b720d4f41ef390952514693e6879c9b34267b91b
Karl Lessard <karllessard@users.noreply.github.com>
2017-07-12 14:10:53 -0400

    [Java] Add base classes and utilities for operation wrappers. (#11188)
    * Add base classes and utilities for operation wrappers.
    
    * Rename Input interface to Operand
    
    * Introduce changes after code review

--- tensorflow/java/src/main/java/org/tensorflow/Operand.java
+++ tensorflow/java/src/main/java/org/tensorflow/Operand.java
@@ -21,20 +21,20 @@ package org.tensorflow;
  * <p>Example usage:
  *
  * <pre>{@code
- * // The "decodeJpeg" operation can be used as input to the "cast" operation
- * Input decodeJpeg = ops.image().decodeJpeg(...);
+ * // The "decodeJpeg" operation can be used as an operand to the "cast" operation
+ * Operand decodeJpeg = ops.image().decodeJpeg(...);
  * ops.math().cast(decodeJpeg, DataType.FLOAT);
  *
- * // The output "y" of the "unique" operation can be used as input to the "cast" operation
+ * // The output "y" of the "unique" operation can be used as an operand to the "cast" operation
  * Output y = ops.array().unique(...).y();
  * ops.math().cast(y, DataType.FLOAT);
  *
- * // The "split" operation can be used as input list to the "concat" operation
- * Iterable<? extends Input> split = ops.array().split(...);
+ * // The "split" operation can be used as operand list to the "concat" operation
+ * Iterable<? extends Operand> split = ops.array().split(...);
  * ops.array().concat(0, split);
  * }</pre>
  */
-public interface Input {
+public interface Operand {
 
   /**
    * Returns the symbolic handle of a tensor.
