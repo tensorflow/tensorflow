d4babb3b087edd4294bb7b144afd6aaae37a0ea3
A. Unique TensorFlower <gardener@tensorflow.org>
2017-07-12 16:47:31 -0700

    Allow HashTableOP to map int64 to float.
    PiperOrigin-RevId: 161738207

--- tensorflow/core/kernels/lookup_table_op.cc
+++ tensorflow/core/kernels/lookup_table_op.cc
@@ -790,6 +790,7 @@ REGISTER_KERNEL(string, int32);
 REGISTER_KERNEL(string, int64);
 REGISTER_KERNEL(int64, string);
 REGISTER_KERNEL(int64, int64);
+REGISTER_KERNEL(int64, float);
 REGISTER_KERNEL(string, string);
 REGISTER_KERNEL(string, bool);
 
