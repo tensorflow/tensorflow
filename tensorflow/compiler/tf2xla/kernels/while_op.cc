b66ddcbf04ca5f9b6e62ad9ef46304c3152242ab
DavidNorman <davidn@graphcore.ai>
2017-07-13 11:17:39 +0100

    Update to latest public repo

--- tensorflow/compiler/tf2xla/kernels/while_op.cc
+++ tensorflow/compiler/tf2xla/kernels/while_op.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include "tensorflow/compiler/tf2xla/xla_op_kernel.h"
 #include "tensorflow/compiler/tf2xla/xla_op_registry.h"
 #include "tensorflow/compiler/xla/client/computation_builder.h"
+#include "tensorflow/compiler/xla/literal_util.h"
 #include "tensorflow/core/framework/function.h"
 #include "tensorflow/core/framework/op_kernel.h"
 
@@ -55,6 +56,9 @@ Status MakeXlaCompilerArgumentsFromInputs(
           break;
         case XlaResource::kTensorArray:
           arg.kind = XlaCompiler::Argument::kTensorArray;
+          break;
+        case XlaResource::kStack:
+          arg.kind = XlaCompiler::Argument::kStack;
           break;
         case XlaResource::kInvalid:
           CHECK(false);
--- tensorflow/java/src/main/java/org/tensorflow/Output.java
+++ tensorflow/java/src/main/java/org/tensorflow/Output.java
@@ -22,10 +22,10 @@ import java.util.Objects;
  * <p>An Output is a symbolic handle to a tensor. The value of the Tensor is computed by executing
  * the {@link Operation} in a {@link Session}.
  *
- * <p>By implementing the {@link Input} interface, instances of this class could also be passed
- * directly in input to an operation.
+ * <p>By implementing the {@link Operand} interface, instances of this class also act as operands to
+ * {@link org.tensorflow.op.Op Op} instances.
  */
-public final class Output implements Input {
+public final class Output implements Operand {
 
   /** Handle to the idx-th output of the Operation {@code op}. */
   public Output(Operation op, int idx) {
