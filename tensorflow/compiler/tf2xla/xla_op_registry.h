39f9cd47fde2816387c5a2d16d1be730fff8a7d7
David Norman <DavidNorman@users.noreply.github.com>
2017-07-18 21:32:39 +0100

    [XLA] Add DT_HALF to the list of floating point types (#11386)
    * Add DT_HALF to the list of floating point types
    
    * Add DT_HALF to the list of generic numerical types too

--- tensorflow/compiler/tf2xla/xla_op_registry.h
+++ tensorflow/compiler/tf2xla/xla_op_registry.h
@@ -45,9 +45,10 @@ extern const char* const DEVICE_XLA_CPU;
 extern const char* const DEVICE_XLA_GPU;
 
 constexpr std::array<DataType, 2> kIntTypes = {{DT_INT32, DT_INT64}};
-constexpr std::array<DataType, 2> kFloatTypes = {{DT_FLOAT, DT_DOUBLE}};
-constexpr std::array<DataType, 4> kNumericTypes = {
-    {DT_INT32, DT_INT64, DT_FLOAT, DT_DOUBLE}};
+constexpr std::array<DataType, 3> kFloatTypes = {
+    {DT_HALF, DT_FLOAT, DT_DOUBLE}};
+constexpr std::array<DataType, 5> kNumericTypes = {
+    {DT_INT32, DT_INT64, DT_HALF, DT_FLOAT, DT_DOUBLE}};
 
 constexpr std::array<DataType, 5> kCpuAllTypes = {
     {DT_INT32, DT_INT64, DT_FLOAT, DT_DOUBLE, DT_BOOL}};
