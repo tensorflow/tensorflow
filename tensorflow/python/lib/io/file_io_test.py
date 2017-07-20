3210e17c7874208f38df8b69b2481ca823aec2ad
NORTHAMERICA\hecli <hecli@microsoft.com>
2017-07-20 14:26:48 -0700

    specify the utf-8 encoding in the python file

--- tensorflow/python/lib/io/file_io_test.py
+++ tensorflow/python/lib/io/file_io_test.py
@@ -1,3 +1,4 @@
+# This Python file uses the following encoding: utf-8
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
