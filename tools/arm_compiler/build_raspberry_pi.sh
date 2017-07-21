0ca23796fb9981b419940495dd6b612696af1e3c
Eugene Brevdo <ebrevdo@google.com>
2017-07-21 15:06:42 -0700

    Add build_raspberry_pi.sh with some documentation.

---
+++ tools/arm_compiler/build_raspberry_pi.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+# To be able to build any of the python packages, it is assumed that
+# you have a python directory in /usr/include/arm-linux-gnueabihf/.
+
+# To install the architecture includes for python on ubuntu trusty;
+# run:
+#  sudo dpkg --add-architecture armhf
+#  echo "deb [arch=armhf] http://ports.ubuntu.com/ trusty main universe" \
+#    sudo tee -a /etc/apt/sources.list.d/armhf.list
+#  # Ignore errors about missing armhf packages in other repos.
+#  sudo apt-get update
+#  sudo apt-get install libpython-all-dev:armhf
+#
+
+yes '' | ./configure
+
+bazel build -c opt --copt=-march=armv6 --copt=-mfpu=vfp \
+  --copt=-funsafe-math-optimizations --copt=-ftree-vectorize \
+  --copt=-fomit-frame-pointer --cpu=armeabi \
+  --crosstool_top=@local_config_arm_compiler//:toolchain \
+  --verbose_failures \
+  tensorflow/tools/benchmark:benchmark_model \
+  //tensorflow/tools/pip_package:build_pip_package
+
+TMPDIR=$(mktemp -d -t tmp.XXXXXXXXXX)
+echo "Final outputs will go to ${TMPDIR}"
+
+# Build a universal wheel.
+BDIST_OPTS="--universal" \
+  bazel-bin/tensorflow/tools/pip_package/build_pip_package "${TMPDIR}"
+
+OLD_FN=$(ls "${TMPDIR}" | grep \.whl)
+SUB='s/tensorflow-([^-]+)-([^-]+)-.*/tensorflow-\1-\2-none-any.whl/; print'
+NEW_FN=$(echo "${OLD_FN}" | \
+  perl -ne "${SUB}")
+mv "${TMPDIR}/${OLD_FN}" "${TMPDIR}/${NEW_FN}"
+cp bazel-bin/tensorflow/tools/benchmark/benchmark_model "${TMPDIR}"
+
+echo "Output may be found here:"
+find "${TMPDIR}"
+
+
