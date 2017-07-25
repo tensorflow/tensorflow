b65b44ddb6ecd5f9b8616456d95fd47dae179828
Li Jing <jingli9111@gmail.com>
2017-07-24 23:42:52 -0400

    modrelu and test

---
+++ tensorflow/contrib/nn/python/ops/modrelu.py
@@ -0,0 +1,54 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""ModRelu."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import nn
+
+
+def modrelu(inputs, 
+			biases,
+			name=None):
+	"""Mod ReLU.
+
+	ModReLU is a modified ReLU that applied to complex number.
+	ModRelu applies ReLU to the magnitude of complex numbers and keep the phases.
+	Source: [Tunable Efficient Unitary Neural Networks (EUNN) and their application to RNNs. L. Jing, et al.](https://arxiv.org/abs/1612.05231)
+	
+	Args:
+		inputs: A `Tensor` with type `complex64` or `complex128`.
+		biases: A 1-D `Tensor` with size matching the last dimension of `inputs`.
+			Must have type `float` or `double`.
+		name: A name for the operation (optional).
+
+	Returns:
+		A `Tensor` with the same type as `inputs`.
+	"""
+	with ops.name_scope(name, "modRelu", [inputs]) as name:
+		inputs = ops.convert_to_tensor(inputs, name="inputs")
+		inputs_norm = math_ops.sqrt(math_ops.square(math_ops.real(inputs)) + math_ops.square(math_ops.imag(inputs))) + 0.000001
+		magnitudes = math_ops.complex(nn_ops.relu(nn_ops.bias_add(inputs_norm, biases)), array_ops.zeros_like(inputs_norm))
+		phases = inputs/math_ops.complex(inputs_norm, array_ops.zeros_like(inputs_norm))
+		return magnitudes * phases
+
+
---
+++ tensorflow/contrib/nn/python/ops/modrelu_test.py
@@ -0,0 +1,69 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	 http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for modrelu.py."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import modrelu
+# from tensorflow.contrib.nn.python.ops import modrelu
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.platform import test
+import numpy as np
+
+class ModreluTest(test.TestCase):
+	def testTensor(self):
+		with self.test_session() as sess:
+			inp_re = constant_op.constant(
+					[
+						[2.5, 0.5, 0.3, 0.4],
+						[0.7, 0, -0.3, 0.2]
+					]
+				)
+			inp_im = constant_op.constant(
+					[
+						[0.2, 0.0, 0.5, 0.7],
+						[0.5, -2.5, 0.0, 0.8]
+					]
+				)
+			inp = math_ops.complex(inp_re, inp_im)
+			biases = constant_op.constant([0.0, 1.0, 0.0, -1.0])
+
+			target_re = constant_op.constant(
+					[
+						[2.5, 1.5, 0.3, 0.0],
+						[0.7, 0, -0.3, 0.0]
+					]
+				)
+			target_im = constant_op.constant(
+					[
+						[0.2, 0.0, 0.5, 0.0],
+						[0.5, -3.5, 0.0, 0.0]
+					]
+				)
+			target = math_ops.complex(target_re, target_im)
+
+			out = sess.run(modrelu.modrelu(inp, biases))
+			tar = sess.run(target)
+
+			self.assertAllClose(out, tar)
+
+if __name__ == '__main__':
+	test.main()
+
