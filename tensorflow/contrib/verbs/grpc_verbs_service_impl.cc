ef140339c949f00b6b5dde1fa6cf24013c605931
Jonathan Hseu <jhseu@google.com>
2017-07-26 12:27:45 -0700

    Fix verbs

--- tensorflow/contrib/verbs/grpc_verbs_service_impl.cc
+++ tensorflow/contrib/verbs/grpc_verbs_service_impl.cc
@@ -43,7 +43,8 @@ VerbsService::Stub::Stub(
     const std::shared_ptr< ::grpc::ChannelInterface>& channel)
     : channel_(channel),
       rpcmethod_GetRemoteAddress_(grpcVerbsService_method_names[0],
-                                  ::grpc::RpcMethod::NORMAL_RPC, channel) {}
+                                  ::grpc::internal::RpcMethod::NORMAL_RPC,
+                                  channel) {}
 
 ::grpc::Status VerbsService::Stub::GetRemoteAddress(
     ::grpc::ClientContext* context, const GetRemoteAddressRequest& request,
@@ -54,9 +55,10 @@ VerbsService::Stub::Stub(
 
 VerbsService::AsyncService::AsyncService() {
   for (int i = 0; i < 1; ++i) {
-    AddMethod(new ::grpc::RpcServiceMethod(grpcVerbsService_method_names[i],
-                                           ::grpc::RpcMethod::NORMAL_RPC,
-                                           nullptr));
+    AddMethod(new ::grpc::internal::RpcServiceMethod(
+        grpcVerbsService_method_names[i],
+        ::grpc::internal::RpcMethod::NORMAL_RPC,
+        nullptr));
     ::grpc::Service::MarkMethodAsync(i);
   }
 }
