d09304fca478b8db5c651eec6fb2559844a73373
Jonathan Hseu <vomjom@vomjom.net>
2017-07-27 10:40:16 -0700

    Upgrade gRPC (#11768)
    * BUILD rule modifications
    
    * More build fixes
    
    * Code changes
    
    * More code fixes
    
    * Working tests
    
    * CMake build
    
    * Fix pprof
    
    * Fix header includes
    
    * CMake fix test
    
    * Bazel clean
    
    * Fix verbs
    
    * More verbs fixes
    
    * bazel clean for XLA
    
    * Windows build fix test
    
    * Add openssl/rand.h
    
    * New cmake build command
    
    * --config Release

--- tensorflow/core/distributed_runtime/rpc/grpc_worker_service_impl.cc
+++ tensorflow/core/distributed_runtime/rpc/grpc_worker_service_impl.cc
@@ -58,9 +58,9 @@ namespace grpc {
 
 WorkerService::AsyncService::AsyncService() {
   for (int i = 0; i < kGrpcNumWorkerMethods; ++i) {
-    AddMethod(new ::grpc::RpcServiceMethod(
+    AddMethod(new ::grpc::internal::RpcServiceMethod(
         GrpcWorkerMethodName(static_cast<GrpcWorkerMethod>(i)),
-        ::grpc::RpcMethod::NORMAL_RPC, nullptr));
+        ::grpc::internal::RpcMethod::NORMAL_RPC, nullptr));
     ::grpc::Service::MarkMethodAsync(i);
   }
 }
