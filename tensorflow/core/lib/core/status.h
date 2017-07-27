bdf6e1b5f1bccc4c320fd79fa67951139a720679
liu.guangcong <liu.guangcong@zte.com.cn>
2017-07-27 17:55:10 +0800

    fix memory leak, and remove duplicated implements

--- tensorflow/core/lib/core/status.h
+++ tensorflow/core/lib/core/status.h
@@ -118,19 +118,22 @@ std::ostream& operator<<(std::ostream& os, const Status& x);
 
 typedef std::function<void(const Status&)> StatusCallback;
 
-extern tensorflow::string* TfCheckOpHelperOutOfLine(
+extern tensorflow::string TfCheckOpHelperOutOfLine(
     const ::tensorflow::Status& v, const char* msg);
-inline tensorflow::string* TfCheckOpHelper(::tensorflow::Status v,
+
+inline tensorflow::string TfCheckOpHelper(::tensorflow::Status v,
                                            const char* msg) {
-  if (v.ok()) return nullptr;
+  if (v.ok()) return "";
   return TfCheckOpHelperOutOfLine(v, msg);
 }
-#define TF_CHECK_OK(val)                                             \
-  while (::tensorflow::string* _result = TfCheckOpHelper(val, #val)) \
-  LOG(FATAL) << *(_result)
-#define TF_QCHECK_OK(val)                                            \
-  while (::tensorflow::string* _result = TfCheckOpHelper(val, #val)) \
-  LOG(QFATAL) << *(_result)
+
+#define TF_DO_CHECK_OK(val，level)                                  \
+  while (::tensorflow::string _result = TfCheckOpHelper(val, #val)) \
+    if (!_result.empty())                                           \
+      LOG(level) << _result
+
+#define TF_CHECK_OK(val)  TF_DO_CHECK_OK(val, FATAL)
+#define TF_QCHECK_OK(val) TF_DO_CHECK_OK(val, QFATAL)
 
 // DEBUG only version of TF_CHECK_OK.  Compiler still parses 'val' even in opt
 // mode.
