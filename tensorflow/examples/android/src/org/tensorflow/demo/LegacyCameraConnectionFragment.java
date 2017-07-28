977ad7060be370125994b0635982b5b5bd267c88
Vijay Vasudevan <vrv@google.com>
2017-07-28 14:32:18 -0700

    spelling

--- tensorflow/examples/android/src/org/tensorflow/demo/LegacyCameraConnectionFragment.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/LegacyCameraConnectionFragment.java
@@ -35,7 +35,7 @@ import android.hardware.Camera.CameraInfo;
 
 import org.tensorflow.demo.env.Logger;
 
-// Eplicit import needed for internal Google builds.
+// Explicit import needed for internal Google builds.
 import org.tensorflow.demo.R;
 
 public class LegacyCameraConnectionFragment extends Fragment {
