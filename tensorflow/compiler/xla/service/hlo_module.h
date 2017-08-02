047ab471366ad7210f7b906be24506d5e5370974
Jeffrey A. Dean <jeff@google.com>
2017-08-02 15:50:44 -0700

    Assign unique ids at the HloModule level to each HloInstruction object. Use these when doing DFS over a graph in order to store the visited bits using an array of two-bit values (in the dfs_hlo_visitor.{h,cc} module), rather than a significantly larger and more expensive hash table to store this state.
    Ids are initially -1 and are assigned when unique names are assigned to
    the HloInstruction objects.
    
    Speeds up compilation of a convolutional image model by ~5.3%
    
    PiperOrigin-RevId: 164050902

--- tensorflow/compiler/xla/service/hlo_module.h
+++ tensorflow/compiler/xla/service/hlo_module.h
@@ -127,6 +127,13 @@ class HloModule {
   // Returns the NameUniquer for uniquing instruction names in this module.
   NameUniquer& instruction_name_uniquer() { return instruction_name_uniquer_; }
 
+  // Assign a new unique dense id for an instruction
+  int NewUniqueInstructionId() {
+    int result = next_unique_id_;
+    next_unique_id_++;
+    return result;
+  }
+
  private:
   HloComputation* AddComputationInternal(
       std::unique_ptr<HloComputation> computation);
@@ -151,6 +158,7 @@ class HloModule {
   // unique per module.
   NameUniquer computation_name_uniquer_{/*separator=*/"."};
   NameUniquer instruction_name_uniquer_{/*separator=*/"."};
+  int next_unique_id_ = 0;
 };
 
 }  // namespace xla
