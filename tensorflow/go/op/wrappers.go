a95d66c10a2b5be9cf7acb8e12915d4d177a2454
A. Unique TensorFlower <gardener@tensorflow.org>
2017-08-02 10:57:46 -0700

    Go: Update generated wrapper functions for TensorFlow ops.
    PiperOrigin-RevId: 164007279

--- tensorflow/go/op/wrappers.go
+++ tensorflow/go/op/wrappers.go
@@ -15316,6 +15316,11 @@ func ResizeAreaAlignCorners(value bool) ResizeAreaAttr {
 //
 // Input images can be of different types but output images are always float.
 //
+// Each output pixel is computed by first transforming the pixel's footprint into
+// the input tensor and then averaging the pixels that intersect the footprint. An
+// input pixel's contribution to the average is weighted by the fraction of its
+// area that intersects the footprint.  This is the same as OpenCV's INTER_AREA.
+//
 // Arguments:
 //	images: 4-D with shape `[batch, height, width, channels]`.
 //	size: = A 1-D int32 Tensor of 2 elements: `new_height, new_width`.  The
