36725ea1dcc80e9953a88076b499b44296ff74d8
A. Unique TensorFlower <gardener@tensorflow.org>
2017-08-03 20:03:34 -0700

    Add a configuration option for code size.
    This change adds a CPU-specific flag: xla_cpu_optimize_for_size
    
    When this flag is passed, it changes the optimizers to run
    more or less analogously to LLVM's -Os optimizations.
    
    There are two things that turning on the code size optimization option
    controls:
    * the internal settings of some optimization passes (which is mostly
      controlled through a function attribute)
    * the passes that get run (which is decided by the pass manager)
    
    This change also refactors the code by reorganizing the way
    that CPU backend specific flags are queried, as well as some
    other minor refactoring.
    
    PiperOrigin-RevId: 164218771

--- tensorflow/compiler/xla/service/cpu/cpu_compiler.cc
+++ tensorflow/compiler/xla/service/cpu/cpu_compiler.cc
@@ -50,6 +50,7 @@ limitations under the License.
 #include "tensorflow/compiler/xla/service/cpu/conv_canonicalization.h"
 #include "tensorflow/compiler/xla/service/cpu/cpu_executable.h"
 #include "tensorflow/compiler/xla/service/cpu/cpu_instruction_fusion.h"
+#include "tensorflow/compiler/xla/service/cpu/cpu_options.h"
 #include "tensorflow/compiler/xla/service/cpu/cpu_parallelization_preparation.h"
 #include "tensorflow/compiler/xla/service/cpu/disassembler.h"
 #include "tensorflow/compiler/xla/service/cpu/ir_emission_utils.h"
@@ -154,8 +155,6 @@ CpuCompiler::CpuCompiler() {
 
 namespace {
 
-const char* kXlaParallelCpuOption = "xla_cpu_parallel";
-
 // LLVM makes certain options configurable only through its command-line
 // options; it provide the ParseCommandLineOptions function that lets us set
 // flags at runtime. However, since these flags are global we want to avoid
@@ -188,14 +187,6 @@ void InitializeLLVMCommandLineOptions(const HloModuleConfig& config) {
     }
     llvm::cl::ParseCommandLineOptions(fake_argv.size(), &fake_argv[0]);
   }
-}
-
-// Helps determine whether the parallel CPU backend was requested in the options
-// of this module configuration.
-bool CpuParallelBackendRequested(const HloModuleConfig& config) {
-  const auto& extra_options_map =
-      config.debug_options().xla_backend_extra_options();
-  return extra_options_map.count(kXlaParallelCpuOption) > 0;
 }
 
 // This visitor records which HLO instructions should have profiling information
@@ -307,7 +298,7 @@ Status CpuCompiler::RunHloPasses(HloModule* module) {
       module->config().intra_op_parallelism_threads() > 0
           ? module->config().intra_op_parallelism_threads()
           : tensorflow::port::NumSchedulableCPUs();
-  if (CpuParallelBackendRequested(module->config())) {
+  if (options::CpuParallelBackendRequested(module->config())) {
     pipeline.AddPass<ParallelizationPreparation>(max_parallelism,
                                                  ShapeSizeBytesFunction());
   }
@@ -319,7 +310,7 @@ Status CpuCompiler::RunHloPasses(HloModule* module) {
   // with the rewrites.
   pipeline.AddPass<HloDCE>();
   pipeline.AddPass<CopyInsertion>();
-  if (CpuParallelBackendRequested(module->config())) {
+  if (options::CpuParallelBackendRequested(module->config())) {
     // Re-run the outlining, in case any copies were inserted into the entry
     // computation.
     pipeline.AddPass<ParallelizationPreparation>(max_parallelism,
@@ -430,10 +421,11 @@ StatusOr<std::unique_ptr<Executable>> CpuCompiler::Compile(
   auto llvm_context = MakeUnique<llvm::LLVMContext>();
   auto llvm_module =
       MakeUnique<llvm::Module>("__compute_module", *llvm_context);
-  auto jit = MakeUnique<SimpleOrcJIT>(CompilerTargetOptions(module->config()),
-                                      CodeGenOptLevel(module->config()),
-                                      pre_optimization_ir_dump_hook,
-                                      post_optimization_ir_dump_hook);
+  auto jit = MakeUnique<SimpleOrcJIT>(
+      CompilerTargetOptions(module->config()),
+      CodeGenOptLevel(module->config()),
+      options::OptimizeForSizeRequested(module->config()),
+      pre_optimization_ir_dump_hook, post_optimization_ir_dump_hook);
   llvm_module->setDataLayout(jit->data_layout());
   llvm_module->setTargetTriple(jit->target_triple().getTriple());
 
@@ -456,7 +448,7 @@ StatusOr<std::unique_ptr<Executable>> CpuCompiler::Compile(
   const string dump_debug_json_to =
       module->config().debug_options().xla_dump_debug_json_to();
 
-  if (CpuParallelBackendRequested(module->config())) {
+  if (options::CpuParallelBackendRequested(module->config())) {
     VLOG(1) << "Using parallel cpu backend";
 
     // Run buffer analysis on the HLO graph. This analysis figures out which
@@ -785,6 +777,7 @@ CpuCompiler::CompileAheadOfTime(std::vector<std::unique_ptr<HloModule>> modules,
     Disassembler disassembler(*target_machine);
     CompilerFunctor compiler_functor(
         target_machine.get(), &disassembler, opt_level,
+        options::OptimizeForSizeRequested(module->config()),
         CompilerFunctor::AllIntrinsics(), pre_optimization_ir_dump_hook,
         post_optimization_ir_dump_hook);
     llvm::object::OwningBinary<llvm::object::ObjectFile> object_file =
