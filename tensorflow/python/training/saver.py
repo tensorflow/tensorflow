a3fa1b5b07374abc1bf335226eb56af8002a4c92
Anna R <annarev@google.com>
2017-08-04 13:52:28 -0700

    Automated g4 rollback of changelist 164279921
    PiperOrigin-RevId: 164305108

--- tensorflow/python/training/saver.py
+++ tensorflow/python/training/saver.py
@@ -673,10 +673,8 @@ class BaseSaverBuilder(object):
 
     with ops.name_scope(name, "save",
                         [saveable.op for saveable in saveables]) as name:
-      # Add a placeholder string tensor for the filename.
-      # Keep the name "Const" for backwards compatibility.
-      filename_tensor = array_ops.placeholder_with_default(
-          filename or "model", shape=(), name="Const")
+      # Add the Constant string tensor for the filename.
+      filename_tensor = constant_op.constant(filename or "model")
 
       # Add the save ops.
       if sharded:
