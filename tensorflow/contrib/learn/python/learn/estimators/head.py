ac5102c29efbe79e60bd96234912f27e65ac4ba3
Alan Yee <alyee@ucsd.edu>
2017-08-08 13:41:55 -0700

    Update head.py
    Put back needed import statement

--- tensorflow/contrib/learn/python/learn/estimators/head.py
+++ tensorflow/contrib/learn/python/learn/estimators/head.py
@@ -35,6 +35,7 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import sparse_tensor
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import sparse_ops
