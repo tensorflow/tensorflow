09f58382b6550ebb1f47d0710d0b33386d3af872
Yong Tang <yong.tang.github@outlook.com>
2017-08-09 00:11:59 +0000

    Fix the expected error message in the tests.
    This fix sorts the list so that error message is repeatable.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/special_math_ops.py
+++ tensorflow/python/ops/special_math_ops.py
@@ -156,7 +156,7 @@ def einsum(equation, *inputs, **kwargs):
   if kwargs:
     raise TypeError("invalid keyword arguments for this function: " +
                     ", ".join([format(key)
-                               for key in reversed(list(kwargs.keys()))]))
+                               for key in sorted(list(kwargs.keys()))]))
   with ops.name_scope(name, "einsum", [equation, inputs]) as name:
     if '...' in equation:
       raise ValueError('Subscripts with ellipses are not yet supported.')
