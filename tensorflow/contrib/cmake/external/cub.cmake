501cdd40543cc8060e14d843ef332924db336501
Amit Patankar <amitpatankar@google.com>
2017-08-11 16:39:37 -0700

    Updating the cmake hash.

--- tensorflow/contrib/cmake/external/cub.cmake
+++ tensorflow/contrib/cmake/external/cub.cmake
@@ -15,7 +15,7 @@
 include (ExternalProject)
 
 set(cub_URL https://github.com/NVlabs/cub/archive/1.6.4.zip)
-set(cub_HASH SHA256=966d0c4f41e2bdc81aebf9ccfbf0baffaac5a74f00b826b06f4dee79b2bb8cee)
+set(cub_HASH SHA256=b35162e70fdfee4af4926b46dc3fbb89e06c57bb5b2e72bf6cc47e1f58cc2c75)
 set(cub_BUILD ${CMAKE_CURRENT_BINARY_DIR}/cub/src/cub)
 set(cub_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/cub/src/cub)
 set(cub_ARCHIVE_DIR ${CMAKE_CURRENT_BINARY_DIR}/external/cub_archive)
