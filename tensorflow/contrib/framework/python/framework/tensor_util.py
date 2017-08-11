bce67813e2c94bec12eb34380ca94661afa60c3b
Alan Yee <alyee@ucsd.edu>
2017-08-11 11:28:35 -0700

    Update tensor_util.py
    Added deprecated note

--- tensorflow/contrib/framework/python/framework/tensor_util.py
+++ tensorflow/contrib/framework/python/framework/tensor_util.py
@@ -28,6 +28,7 @@ from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import check_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.util.deprecation import deprecated
 
 
 __all__ = [
@@ -76,7 +77,8 @@ def reduce_sum_n(tensors, name=None):
       return tensors[0]
     return math_ops.add_n(tensors, name=name_scope)
 
-
+@deprecated(None,
+    'Please switch to tf.confusion_matrix.remove_squeezable_dimensions')
 def remove_squeezable_dimensions(predictions, labels, name=None):
   """Squeeze last dim if ranks of `predictions` and `labels` differ by 1.
 
