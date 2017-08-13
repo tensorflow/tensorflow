2278188a38f193ba61a125a133fb0667cb56a81f
Alan Yee <alyee@ucsd.edu>
2017-08-12 21:47:38 -0700

    Update tensor_util.py and fix metric_ops.py (#12240)
    * Update tensor_util.py
    
    Added deprecated note
    
    * Update tensor_util.py
    
    Add more details to deprecated note
    
    * Fix metric_ops
    
    Corrected ordering of returned tuple

--- tensorflow/contrib/framework/python/framework/tensor_util.py
+++ tensorflow/contrib/framework/python/framework/tensor_util.py
@@ -28,6 +28,7 @@ from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import check_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.util.deprecation import deprecated
 
 
 __all__ = [
@@ -76,7 +77,10 @@ def reduce_sum_n(tensors, name=None):
       return tensors[0]
     return math_ops.add_n(tensors, name=name_scope)
 
-
+@deprecated(None,
+    "Please switch to tf.confusion_matrix.remove_squeezable_dimensions. Note "
+    "that order of the inputs and ouputs of labels and predictions have also "
+    "been switched.")
 def remove_squeezable_dimensions(predictions, labels, name=None):
   """Squeeze last dim if ranks of `predictions` and `labels` differ by 1.
 
