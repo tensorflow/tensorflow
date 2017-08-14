f25fea8cf6abc4da12d5850afc7e44ac6f76452c
JoshuaBradbury <joshua.bradbury@protonmail.com>
2017-08-14 11:11:05 +0100

    Fixed formatting issue

--- tensorflow/contrib/learn/python/learn/datasets/emnist.py
+++ tensorflow/contrib/learn/python/learn/datasets/emnist.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2017 Joshua Bradbury. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,155 +11,299 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# =============================================================================
+# This is a copy of the MNIST data set from tensorflow that is
+# designed to support the EMNIST data set outlined in the paper
+#
+# https://arxiv.org/pdf/1702.05373v1.pdf
 
-"""A deep MNIST classifier using convolutional layers.
-
-See extensive documentation at
-https://www.tensorflow.org/get_started/mnist/pros
-"""
-# Disable linter warnings to maintain consistency with tutorial.
-# pylint: disable=invalid-name
-# pylint: disable=g-bad-import-order
+"""Functions for downloading and reading EMNIST data."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
-import sys
+import gzip
+import zipfile
+import os
 
-from tensorflow.examples.tutorials.mnist import input_data
-from emnist import read_data_sets
-from emnist import EMNIST
+import numpy
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
-import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import random_seed
 
-FLAGS = None
+def enum(*sequential, **named):
+  enums = dict(zip(sequential, range(len(sequential))), **named)
+  reverse = dict((value, key) for key, value in enums.iteritems())
+  enums['name'] = reverse
+  return type('Enum', (), enums)
 
-num_of_classes = 47
+EMNIST = enum('BALANCED', 'BY_CLASS', 'BY_MERGE', 'DIGITS', 'LETTERS', 'MNIST')
 
-def deepnn(x):
-	"""deepnn builds the graph for a deep net for classifying digits.
-
-	Args:
-		x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-		number of pixels in a standard MNIST image.
-
-	Returns:
-		A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
-		equal to the logits of classifying the digit into one of 10 classes (the
-		digits 0-9). keep_prob is a scalar placeholder for the probability of
-		dropout.
-	"""
-	# Reshape to use within a convolutional neural net.
-	# Last dimension is for "features" - there is only one here, since images are
-	# grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
-	x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-	# First convolutional layer - maps one grayscale image to 32 feature maps.
-	W_conv1 = weight_variable([5, 5, 1, 32])
-	b_conv1 = bias_variable([32])
-	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-
-	# Pooling layer - downsamples by 2X.
-	h_pool1 = max_pool_2x2(h_conv1)
-
-	# Second convolutional layer -- maps 32 feature maps to 64.
-	W_conv2 = weight_variable([5, 5, 32, 64])
-	b_conv2 = bias_variable([64])
-	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-
-	# Second pooling layer.
-	h_pool2 = max_pool_2x2(h_conv2)
-
-	# Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
-	# is down to 7x7x64 feature maps -- maps this to 1024 features.
-	W_fc1 = weight_variable([7 * 7 * 64, 1024])
-	b_fc1 = bias_variable([1024])
-
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-	# Dropout - controls the complexity of the model, prevents co-adaptation of
-	# features.
-	keep_prob = tf.placeholder(tf.float32)
-	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-	# Map the 1024 features to 10 classes, one for each digit
-	W_fc2 = weight_variable([1024, num_of_classes])
-	b_fc2 = bias_variable([num_of_classes])
-
-	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-	return y_conv, keep_prob
+def _read32(bytestream):
+  dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+  return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
 
 
-def conv2d(x, W):
-	"""conv2d returns a 2d convolution layer with full stride."""
-	print(x)
-	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def extract_images(f):
+  """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
+
+  Args:
+    f: A file object that can be passed into a gzip reader.
+
+  Returns:
+    data: A 4D uint8 numpy array [index, y, x, depth].
+
+  Raises:
+    ValueError: If the bytestream does not start with 2051.
+
+  """
+  print('Extracting', f.name)
+  with gzip.GzipFile(fileobj=f) as bytestream:
+    magic = _read32(bytestream)
+    if magic != 2051:
+      raise ValueError('Invalid magic number %d in MNIST image file: %s' %
+                       (magic, f.name))
+    num_images = _read32(bytestream)
+    rows = _read32(bytestream)
+    cols = _read32(bytestream)
+    buf = bytestream.read(rows * cols * num_images)
+    data = numpy.frombuffer(buf, dtype=numpy.uint8)
+    data = data.reshape(num_images, rows, cols, 1)
+    return data
 
 
-def max_pool_2x2(x):
-	"""max_pool_2x2 downsamples a feature map by 2X."""
-	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def dense_to_one_hot(labels_dense, num_classes):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = numpy.arange(num_labels) * num_classes
+  labels_one_hot = numpy.zeros((num_labels, num_classes))
+
+  indexes = index_offset + labels_dense.ravel()
+  if num_labels * num_classes in indexes:
+    indexes = numpy.delete(indexes, [len(indexes) - 1])
+
+  labels_one_hot.flat[indexes] = 1
+  return labels_one_hot
 
 
-def weight_variable(shape):
-	"""weight_variable generates a weight variable of a given shape."""
-	initial = tf.truncated_normal(shape, stddev=0.1)
-	return tf.Variable(initial)
+def extract_labels(f, one_hot=False, emnist_type=EMNIST.BALANCED):
+  """Extract the labels into a 1D uint8 numpy array [index].
+
+  Args:
+    f: A file object that can be passed into a gzip reader.
+    one_hot: Does one hot encoding for the result.
+    num_classes: Number of classes for the one hot encoding.
+
+  Returns:
+    labels: a 1D uint8 numpy array.
+
+  Raises:
+    ValueError: If the bystream doesn't start with 2049.
+  """
+  print('Extracting', f.name)
+  with gzip.GzipFile(fileobj=f) as bytestream:
+    magic = _read32(bytestream)
+    if magic != 2049:
+      raise ValueError('Invalid magic number %d in MNIST label file: %s' %
+                       (magic, f.name))
+    num_items = _read32(bytestream)
+    buf = bytestream.read(num_items)
+    labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+    if one_hot:
+      if emnist_type == EMNIST.BALANCED or emnist_type == EMNIST.BY_MERGE:
+        num_classes = 47
+      elif emnist_type == EMNIST.BY_CLASS:
+        num_classes = 62
+      elif emnist_type == EMNIST.LETTERS:
+        num_classes = 26
+      else:
+        num_classes = 10
+      return dense_to_one_hot(labels, num_classes)
+    return labels
 
 
-def bias_variable(shape):
-	"""bias_variable generates a bias variable of a given shape."""
-	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial)
+class DataSet(object):
+
+  def __init__(self,
+               images,
+               labels,
+               emnist_type=EMNIST.BALANCED,
+               fake_data=False,
+               one_hot=False,
+               dtype=dtypes.float32,
+               reshape=True,
+               seed=None):
+    """Construct a DataSet.
+    one_hot arg is used only if fake_data is true.  `dtype` can be either
+    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+    `[0, 1]`.  Seed arg provides for convenient deterministic testing.
+    """
+    seed1, seed2 = random_seed.get_seed(seed)
+    # If op level seed is not set, use whatever graph level seed is returned
+    numpy.random.seed(seed1 if seed is None else seed2)
+    dtype = dtypes.as_dtype(dtype).base_dtype
+    if dtype not in (dtypes.uint8, dtypes.float32):
+      raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                      dtype)
+    if fake_data:
+      self._num_examples = 10000
+      self.one_hot = one_hot
+    else:
+      assert images.shape[0] == labels.shape[0], (
+          'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+      self._num_examples = images.shape[0]
+
+      # Convert shape from [num examples, rows, columns, depth]
+      # to [num examples, rows*columns] (assuming depth == 1)
+      if reshape:
+        assert images.shape[3] == 1
+        images = images.reshape(images.shape[0],
+                                images.shape[1] * images.shape[2])
+      if dtype == dtypes.float32:
+        # Convert from [0, 255] -> [0.0, 1.0].
+        images = images.astype(numpy.float32)
+        images = numpy.multiply(images, 1.0 / 255.0)
+    self._images = images
+    self._labels = labels
+    self._emnist_type = emnist_type
+    self._epochs_completed = 0
+    self._index_in_epoch = 0
+
+  @property
+  def images(self):
+    return self._images
+
+  @property
+  def labels(self):
+    return self._labels
+
+  @property
+  def num_examples(self):
+    return self._num_examples
+
+  @property
+  def epochs_completed(self):
+    return self._epochs_completed
+
+  @property
+  def emnist_type(self):
+    return self._emnist_type
+
+  def next_batch(self, batch_size, fake_data=False, shuffle=True):
+    """Return the next `batch_size` examples from this data set."""
+    if fake_data:
+      fake_image = [1] * 784
+      if self.one_hot:
+        fake_label = [1] + [0] * 9
+      else:
+        fake_label = 0
+      return [fake_image for _ in xrange(batch_size)], [
+          fake_label for _ in xrange(batch_size)
+      ]
+    start = self._index_in_epoch
+    # Shuffle for the first epoch
+    if self._epochs_completed == 0 and start == 0 and shuffle:
+      perm0 = numpy.arange(self._num_examples)
+      numpy.random.shuffle(perm0)
+      self._images = self.images[perm0]
+      self._labels = self.labels[perm0]
+    # Go to the next epoch
+    if start + batch_size > self._num_examples:
+      # Finished epoch
+      self._epochs_completed += 1
+      # Get the rest examples in this epoch
+      rest_num_examples = self._num_examples - start
+      images_rest_part = self._images[start:self._num_examples]
+      labels_rest_part = self._labels[start:self._num_examples]
+      # Shuffle the data
+      if shuffle:
+        perm = numpy.arange(self._num_examples)
+        numpy.random.shuffle(perm)
+        self._images = self.images[perm]
+        self._labels = self.labels[perm]
+      # Start next epoch
+      start = 0
+      self._index_in_epoch = batch_size - rest_num_examples
+      end = self._index_in_epoch
+      images_new_part = self._images[start:end]
+      labels_new_part = self._labels[start:end]
+      return numpy.concatenate((images_rest_part, images_new_part), axis=0), \
+             numpy.concatenate((labels_rest_part, labels_new_part), axis=0)
+    else:
+      self._index_in_epoch += batch_size
+      end = self._index_in_epoch
+      return self._images[start:end], self._labels[start:end]
 
 
-def main(_):
-	# Import data
-	mnist = read_data_sets(FLAGS.data_dir, emnist_type=EMNIST.BALANCED, one_hot=True)
+def read_data_sets(train_dir,
+                   emnist_type=EMNIST.BALANCED,
+                   fake_data=False,
+                   one_hot=False,
+                   dtype=dtypes.float32,
+                   reshape=True,
+                   validation_size=5000,
+                   seed=None):
+  if fake_data:
 
-	# Create the model
-	x = tf.placeholder(tf.float32, [None, 784])
+    def fake():
+      return DataSet(
+          [], [], fake_data=True, one_hot=one_hot, dtype=dtype, seed=seed)
 
-	# Define loss and optimizer
-	y_ = tf.placeholder(tf.float32, [None, num_of_classes])
+    train = fake()
+    validation = fake()
+    test = fake()
+    return base.Datasets(train=train, validation=validation, test=test)
 
-	# Build the graph for the deep net
-	y_conv, keep_prob = deepnn(x)
+  #temporary until a better hosting solution can be achieved
+  base.maybe_download('gzip.zip', train_dir,
+                      'http://biometrics.nist.gov/cs_links/EMNIST/gzip.zip')
 
-	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-	correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  with zipfile.ZipFile(train_dir + '/gzip.zip') as zf:
 
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		for i in range(10000):
-			batch = mnist.train.next_batch(50)
-			if i % 100 == 0:
-				train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-				print('step %d, training accuracy %g' % (i, train_accuracy))
-			train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    for name in zf.namelist():
+      if os.path.isfile(train_dir + "/" + name):
+        zf.extract(name, train_dir)
 
-		start = 0
-		probSum = 0.0
-		count = 0
-		i = 0
-		while start < len(mnist.test.images):
-			i = start + 10000
-			if i > len(mnist.test.images):
-				i = len(mnist.test.images)
-			probSum += accuracy.eval(feed_dict={x: mnist.test.images[start:i], y_: mnist.test.labels[start:i], keep_prob: 1.0})
-			start = i
-			count += 1
+    emnist_nice_name = EMNIST.name[emnist_type].lower().replace("_", "")
 
-		print("test accuracy %g" % (probSum / count))
+    with open("{}/gzip/emnist-{}-train-images-idx3-ubyte.gz"
+              .format(train_dir, emnist_nice_name), 'rb') as f:
+      train_images = extract_images(f)
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data', help='Directory for storing input data')
-	FLAGS, unparsed = parser.parse_known_args()
-	tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    with open("{}/gzip/emnist-{}-train-labels-idx1-ubyte.gz"
+              .format(train_dir, emnist_nice_name), 'rb') as f:
+      train_labels = extract_labels(f, emnist_type=emnist_type, one_hot=one_hot)
+
+    with open("{}/gzip/emnist-{}-test-images-idx3-ubyte.gz"
+              .format(train_dir, emnist_nice_name), 'rb') as f:
+      test_images = extract_images(f)
+
+    with open("{}/gzip/emnist-{}-test-labels-idx1-ubyte.gz"
+              .format(train_dir, emnist_nice_name), 'rb') as f:
+      test_labels = extract_labels(f, emnist_type=emnist_type, one_hot=one_hot)
+
+  if not 0 <= validation_size <= len(train_images):
+    raise ValueError(
+        'Validation size should be between 0 and {}. Received: {}.'
+        .format(len(train_images), validation_size))
+
+  validation_images = train_images[:validation_size]
+  validation_labels = train_labels[:validation_size]
+  train_images = train_images[validation_size:]
+  train_labels = train_labels[validation_size:]
+
+  train = DataSet(train_images, train_labels, dtype=dtype, reshape=reshape,
+                  seed=seed)
+  validation = DataSet(validation_images, validation_labels, dtype=dtype,
+                       reshape=reshape, seed=seed)
+  test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape,
+                 seed=seed)
+
+  return base.Datasets(train=train, validation=validation, test=test)
+
+
+def load_emnist(train_dir='EMNIST-data'):
+  return read_data_sets(train_dir)
