473fc81737c8ae8c9eef11ab8f94cabea3857a35
Jonathan DEKHTIAR <DEKHTIARJonathan@users.noreply.github.com>
2017-08-23 23:34:43 +0200

    Main setup.py modified - Py3.4 and P3.5 flagged

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -227,6 +227,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
