6863a29f234cc8a7b5b2c56c61985a7a9bd78434
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-08-26 06:24:28 +0800

    CLN: put back imports on the top

--- tensorflow/python/framework/tensor_shape.py
+++ tensorflow/python/framework/tensor_shape.py
@@ -18,6 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.core.framework import tensor_shape_pb2
+from tensorflow.python.util import compat
 
 
 class Dimension(object):
@@ -25,8 +26,6 @@ class Dimension(object):
 
   def __init__(self, value):
     """Creates a new Dimension with the given value."""
-    from tensorflow.python.util import compat
-
     if value is None:
       self._value = None
     else:
@@ -429,8 +428,6 @@ class TensorShape(object):
       TypeError: If dims cannot be converted to a list of dimensions.
     """
     # TODO(irving): Eliminate the single integer special case.
-    from tensorflow.python.util import compat
-
     if dims is None:
       self._dims = None
     elif isinstance(dims, compat.bytes_or_text_types):
