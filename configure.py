ab635bcafaa42f599e49ceb6ed324e32996d8b45
Penghao Cen <scorpiocph@gmail.com>
2017-08-25 12:50:48 +0800

    Double check after regex search for cudnn_path

--- configure.py
+++ configure.py
@@ -685,7 +685,9 @@ def set_tf_cunn_version(environ_cp):
       ldconfig_bin = which('ldconfig') or '/sbin/ldconfig'
       cudnn_path_from_ldconfig = run_shell([ldconfig_bin, '-p'])
       cudnn_path_from_ldconfig = re.search('.*libcudnn.so .* => (.*)',
-                                           cudnn_path_from_ldconfig).group(1)
+                                           cudnn_path_from_ldconfig)
+      if cudnn_path_from_ldconfig != None:
+        cudnn_path_from_ldconfig = cudnn_path_from_ldconfig.group(1)
       if os.path.exists('%s.%s' % (cudnn_path_from_ldconfig, tf_cudnn_version)):
         cudnn_install_path = os.path.dirname(cudnn_path_from_ldconfig)
         break
