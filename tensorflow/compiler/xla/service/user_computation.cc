eedb16e4d503bb55392b95eaefab69a9f4a72a95
DavidNorman <davidn@graphcore.ai>
2017-08-26 13:46:53 +0100

    Add a check to the HLO verifier for badly formatted Broadcasts.
    Also fix up the two cases where the code was creating invalid
    broadcasts.

--- tensorflow/compiler/xla/service/user_computation.cc
+++ tensorflow/compiler/xla/service/user_computation.cc
@@ -2472,7 +2472,7 @@ HloInstruction* ComputationLowerer::ImplicitBroadcastToExplicitBroadcast(
   // Do explicit broadcast for scalar.
   if (ShapeUtil::IsScalar(operand->shape())) {
     return hlo_builder_.AddInstruction(HloInstruction::CreateBroadcast(
-        broadcast_shape, operand, AsInt64Slice(broadcast_shape.dimensions())));
+        broadcast_shape, operand, {}));
   }
   // Do explicit broadcast for degenerate broadcast.
   std::vector<int64> broadcast_dimensions;
