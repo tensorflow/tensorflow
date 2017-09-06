b866e0e54b31aedc1c92f57dffe7a5d14f363745
SG Vogel <sabine.g.vogel@gmail.com>
2017-09-05 19:19:18 -0700

    mmap interface

--- tensorflow/contrib/android/java/org/tensorflow/contrib/android/TensorFlowInferenceInterface.java
+++ tensorflow/contrib/android/java/org/tensorflow/contrib/android/TensorFlowInferenceInterface.java
@@ -15,21 +15,13 @@ limitations under the License.
 
 package org.tensorflow.contrib.android;
 
+import android.content.Context;
 import android.content.res.AssetManager;
-import android.os.Trace;
 import android.os.Build.VERSION;
+import android.os.Trace;
 import android.text.TextUtils;
 import android.util.Log;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.DoubleBuffer;
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.nio.LongBuffer;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.tensorflow.DataType;
 import org.tensorflow.Graph;
 import org.tensorflow.Operation;
@@ -37,39 +29,65 @@ import org.tensorflow.Session;
 import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Wrapper over the TensorFlow API ({@link Graph}, {@link Session}) providing a smaller API surface
  * for inference.
- *
+ * <p>
  * <p>See tensorflow/examples/android/src/org/tensorflow/demo/TensorFlowImageClassifier.java for an
  * example usage.
  */
 public class TensorFlowInferenceInterface {
-  private static final String TAG = "TensorFlowInferenceInterface";
-  private static final String ASSET_FILE_PREFIX = "file:///android_asset/";
+  //TAG should be <=23 chars
+  private static final String TAG = "TensorFlowInferenceInt";
+  //@todo remove
+  static File mModelFile = null;
 
-  /*
-   * Load a TensorFlow model from the AssetManager or from disk if it is not an asset file.
-   *
-   * @param assetManager The AssetManager to use to load the model file.
-   * @param model The filepath to the GraphDef proto representing the model.
-   */
+  // @todo refactor
+  public static TensorFlowInferenceInterface createTensorFlowInferenceInterface(Context ctx, String model) {
+    mModelFile = ctx.getFileStreamPath(model);
+    return new TensorFlowInferenceInterface(null, model);
+  }
+
+    /* Load a TensorFlow model from the AssetManager or from disk if it is not an asset file.
+    *
+    * @param assetManager The AssetManager to use to load the model file.
+    * @param model The filepath to the GraphDef proto representing the model.`
+    */
+
   public TensorFlowInferenceInterface(AssetManager assetManager, String model) {
-    Log.i(TAG, "Checking to see if TensorFlow native methods are already loaded");
+    //@todo update usages-- include the copy function here instead of init?
+    //@todo or just remove the am
+    //@todo cache object
+    //@todo add permsission for external storage
+
+    Log.i(TAG, "Checking if TensorFlow native methods are already loaded");
     try {
       // Hack to see if the native libraries have been loaded.
       new RunStats();
       Log.i(TAG, "TensorFlow native methods already loaded");
     } catch (UnsatisfiedLinkError e1) {
       Log.i(
-          TAG, "TensorFlow native methods not found, attempting to load via tensorflow_inference");
+              TAG, "TensorFlow native methods not found, attempting to load via tensorflow_inference");
       try {
         System.loadLibrary("tensorflow_inference");
         Log.i(TAG, "Successfully loaded TensorFlow native methods (RunStats error may be ignored)");
       } catch (UnsatisfiedLinkError e2) {
         throw new RuntimeException(
-            "Native TF methods not found; check that the correct native"
-                + " libraries are present in the APK.");
+                "Native TF methods not found; check that the correct native"
+                        + " libraries are present in the APK.");
       }
     }
 
@@ -78,36 +96,67 @@ public class TensorFlowInferenceInterface {
     this.sess = new Session(g);
     this.runner = sess.runner();
 
-    final boolean hasAssetPrefix = model.startsWith(ASSET_FILE_PREFIX);
-    InputStream is = null;
+    MappedByteBuffer buffer = null;
+    FileChannel fileChannel = null;
+    RandomAccessFile raf = null;
     try {
-      String aname = hasAssetPrefix ? model.split(ASSET_FILE_PREFIX)[1] : model;
-      is = assetManager.open(aname);
+      raf = new RandomAccessFile(mModelFile, "r");
+      fileChannel = raf.getChannel();
+      buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
     } catch (IOException e) {
-      if (hasAssetPrefix) {
-        throw new RuntimeException("Failed to load model from '" + model + "'", e);
-      }
-      // Perhaps the model file is not an asset but is on disk.
-      try {
-        is = new FileInputStream(model);
-      } catch (IOException e2) {
-        throw new RuntimeException("Failed to load model from '" + model + "'", e);
-      }
+      throw new RuntimeException("Failed to load model from: " + mModelFile, e);
+
     }
     try {
-      loadGraph(is, g);
-      is.close();
-      Log.i(TAG, "Successfully loaded model from '" + model + "'");
+      //buffer.array() is not written to work with mem map
+      //load () can cause IO ops to kick off
+      if (!buffer.isLoaded()) {
+        buffer.load();
+      }
+      //no guarantee but can be faster than normal IO reads
+      byte[] graphDef = new byte[buffer.limit()];
+      //for (int i = 0; i < buffer.limit(); i++) {
+      //     graphDef[i] = buffer.get();
+      //}
+      //bulk method
+      buffer.get(graphDef);
+      loadGraph(graphDef, g);
+
+      Log.i(TAG, "Successfully loaded model from map '" + model + "'");
     } catch (IOException e) {
       throw new RuntimeException("Failed to load model from '" + model + "'", e);
+    } finally {
+      try {
+
+        raf.close();
+      } catch (IOException e) {
+        throw new RuntimeException("Failed to close FileChannel '" + fileChannel + "'", e);
+      }
+      try {
+        //@todo individual exceptions
+        fileChannel.close();
+      } catch (IOException e) {
+        throw new RuntimeException("Failed to close RAF '" + fileChannel + "'", e);
+      }
+      if (buffer != null) {
+        buffer.clear();
+        buffer = null;
+      }
+            /*@todo maybe leave this out--only 4 cached objs, assumes this must only be called once per type
+            if (mModelFile != null) {
+                mModelFile.delete();
+            }*/
+
+
     }
+
   }
 
   /**
    * Runs inference between the previously registered input nodes (via feed*) and the requested
    * output nodes. Output nodes can then be queried with the fetch* methods.
    *
-   * @param outputNames A list of output nodes which should be filled by the inference pass.
+   * @param outputNames A zlist of output nodes which should be filled by the inference pass.
    */
   public void run(String[] outputNames) {
     run(outputNames, false);
@@ -147,12 +196,12 @@ public class TensorFlowInferenceInterface {
       // Ideally the exception would have been let through, but since this interface predates the
       // TensorFlow Java API, must return -1.
       Log.e(
-          TAG,
-          "Failed to run TensorFlow inference with inputs:["
-              + TextUtils.join(", ", feedNames)
-              + "], outputs:["
-              + TextUtils.join(", ", fetchNames)
-              + "]");
+              TAG,
+              "Failed to run TensorFlow inference with inputs:["
+                      + TextUtils.join(", ", feedNames)
+                      + "], outputs:["
+                      + TextUtils.join(", ", fetchNames)
+                      + "]");
       throw e;
     } finally {
       // Always release the feeds (to save resources) and reset the runner, this run is
@@ -162,7 +211,9 @@ public class TensorFlowInferenceInterface {
     }
   }
 
-  /** Returns a reference to the Graph describing the computation run during inference. */
+  /**
+   * Returns a reference to the Graph describing the computation run during inference.
+   */
   public Graph graph() {
     return g;
   }
@@ -171,12 +222,14 @@ public class TensorFlowInferenceInterface {
     final Operation operation = g.operation(operationName);
     if (operation == null) {
       throw new RuntimeException(
-          "Node '" + operationName + "' does not exist in model '" + modelName + "'");
+              "Node '" + operationName + "' does not exist in model '" + modelName + "'");
     }
     return operation;
   }
 
-  /** Returns the last stat summary string if logging is enabled. */
+  /**
+   * Returns the last stat summary string if logging is enabled.
+   */
   public String getStatString() {
     return (runStats == null) ? "" : runStats.summary();
   }
@@ -409,7 +462,7 @@ public class TensorFlowInferenceInterface {
     getTensor(outputName).writeTo(dst);
   }
 
-  private void loadGraph(InputStream is, Graph g) throws IOException {
+  private void loadGraph(byte[] graphDef, Graph g) throws IOException {
     final long startMs = System.currentTimeMillis();
 
     if (VERSION.SDK_INT >= 18) {
@@ -418,15 +471,7 @@ public class TensorFlowInferenceInterface {
     }
 
     // TODO(ashankar): Can we somehow mmap the contents instead of copying them?
-    byte[] graphDef = new byte[is.available()];
-    final int numBytesRead = is.read(graphDef);
-    if (numBytesRead != graphDef.length) {
-      throw new IOException(
-          "read error: read only "
-              + numBytesRead
-              + " of the graph, expected to read "
-              + graphDef.length);
-    }
+    //yes
 
     if (VERSION.SDK_INT >= 18) {
       Trace.endSection(); // readGraphDef.
@@ -446,8 +491,8 @@ public class TensorFlowInferenceInterface {
 
     final long endMs = System.currentTimeMillis();
     Log.i(
-        TAG,
-        "Model load took " + (endMs - startMs) + "ms, TensorFlow version: " + TensorFlow.version());
+            TAG,
+            "Model load took " + (endMs - startMs) + "ms, TensorFlow version: " + TensorFlow.version());
   }
 
   private void addFeed(String inputName, Tensor t) {
@@ -493,7 +538,7 @@ public class TensorFlowInferenceInterface {
       ++i;
     }
     throw new RuntimeException(
-        "Node '" + outputName + "' was not provided to run(), so it cannot be read");
+            "Node '" + outputName + "' was not provided to run(), so it cannot be read");
   }
 
   private void closeFeeds() {
