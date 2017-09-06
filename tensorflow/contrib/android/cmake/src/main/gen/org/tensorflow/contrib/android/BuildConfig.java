19334a71b5985cec940d9a3b5a3ce64d1e990c6e
SG Vogel <sabine.g.vogel@gmail.com>
2017-09-05 19:08:22 -0700

    Revert "prelim changes"
    This reverts commit e1a17108a5227c1a626dbf9785e0ecc969fb63a6.

--- tensorflow/contrib/android/cmake/src/main/gen/org/tensorflow/contrib/android/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.tensorflow.contrib.android;
-
-/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
-public final class BuildConfig {
-  public final static boolean DEBUG = Boolean.parseBoolean(null);
-}
