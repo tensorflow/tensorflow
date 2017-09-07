2e8a79e15d267bba8bc087f92d4fc8292a8fa485
Amit Kushwaha <amit_kushwaha@outlook.com>
2017-09-07 07:34:32 +0530

    Undo import changes in the utils_test.py

--- tensorflow/python/layers/utils_test.py
+++ tensorflow/python/layers/utils_test.py
@@ -18,9 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.python.platform import test
-
 from tensorflow.python.layers import utils
+from tensorflow.python.platform import test
 
 
 class ConvUtilsTest(test.TestCase):
