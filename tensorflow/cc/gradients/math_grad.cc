2c3964177ab5f51326eb82f9ba3c749ec2cb1d70
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-09-14 13:59:57 +0800

    CLN: remove useless dependency in LgmmaGrad

--- tensorflow/cc/gradients/math_grad.cc
+++ tensorflow/cc/gradients/math_grad.cc
@@ -707,8 +707,7 @@ Status LgammaGrad(const Scope& scope, const Operation& op,
                   const std::vector<Output>& grad_inputs,
                   std::vector<Output>* grad_outputs) {
   auto grad = grad_inputs[0];
-  Scope grad_scope = scope.WithControlDependencies(grad);
-  auto x = ConjugateHelper(grad_scope, op.input(0));
+  auto x = ConjugateHelper(scope, op.input(0));
   auto dx = Mul(scope, grad, Digamma(scope, x));
   grad_outputs->push_back(dx);
   return scope.status();
