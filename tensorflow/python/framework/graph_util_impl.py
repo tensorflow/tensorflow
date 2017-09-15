74f042fc4a23b2f14f746d9dba06d340c60c0413
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-09-15 13:30:34 +0800

    CLN: use warning

--- tensorflow/python/framework/graph_util_impl.py
+++ tensorflow/python/framework/graph_util_impl.py
@@ -126,7 +126,7 @@ def extract_sub_graph(graph_def, dest_nodes):
   # Github 13047, cast single string to list
   if isinstance(dest_nodes, str):
     dest_nodes = [dest_nodes]
-    logging.warn("dest_nodes should be a list.")
+    logging.warning("dest_nodes should be a list.")
 
   edges = {}  # Keyed by the dest node name.
   name_to_node_map = {}  # Keyed by node name.
