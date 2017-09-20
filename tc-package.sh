fbbc5342f5c7dce4464fe9e1b6a8343b103d7da5
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2017-09-20 16:40:10 +0200

    Switch to heavy OSX worker
    Relying on multiple generic-worker on OSX will need us to stop using the
    hack of /Users/build-user/TaskCluster/Tasks/task-<SHA1>/ and use the
    proper task's directory which is defined at
    /Users/build-user/TaskCluster/Tasks/task_<TIME>/, with the downside that
    "client-side" use of home.tar.xz on OSX will need to rewrite the paths
    since task dir will include the worker's instance, following that
    pattern, X being the instance number:
    /Users/build-user/TaskCluster/(Heavy|Light)Tasks/X/

--- tc-package.sh
+++ tc-package.sh
@@ -9,6 +9,19 @@ mkdir -p ${TASKCLUSTER_ARTIFACTS} || true
 cp ${DS_ROOT_TASK}/DeepSpeech/tf/bazel-bin/tensorflow/libtensorflow_cc.so ${TASKCLUSTER_ARTIFACTS}
 cp ${DS_ROOT_TASK}/DeepSpeech/tf/bazel-bin/tensorflow/tools/graph_transforms/transform_graph ${TASKCLUSTER_ARTIFACTS}
 
-artifact_root_dir=$(dirname "${DS_ROOT_TASK}")
-artifact_tar_dir=$(basename "${DS_ROOT_TASK}")
-tar -C ${artifact_root_dir} -cf - ${artifact_tar_dir} | pixz -9 > ${TASKCLUSTER_ARTIFACTS}/home.tar.xz
+# It seems that bsdtar and gnutar are behaving a bit differently on the way
+# they deal with --exclude="./public/*" ; this caused ./DeepSpeech/tensorflow/core/public/
+# to be ditched when we just wanted to get rid of ./public/ on OSX.
+# Switching to gnutar (already needed for the --transform on DeepSpeech tasks)
+# does the trick.
+TAR=tar
+TAR_EXCLUDE="--exclude=./dls/*"
+if [ "${OS}" = "Darwin" ]; then
+    TAR=gtar
+    TAR_EXCLUDE="--exclude=./dls/* --exclude=./public/* --exclude=./generic-worker/* --exclude=./homebrew/* --exclude=./homebrew.cache/*"
+fi;
+
+# Make a tar of
+#  - /home/build-user/ (linux
+#  - /Users/build-user/TaskCluster/HeavyTasks/X/ (OSX)
+${TAR} -C ${DS_ROOT_TASK} ${TAR_EXCLUDE} -cf - . | pixz -9 > ${TASKCLUSTER_ARTIFACTS}/home.tar.xz
