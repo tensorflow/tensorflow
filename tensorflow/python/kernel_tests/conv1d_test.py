bbc15523e83325dbb57ece18abb18d083742f266
Yong Tang <yong.tang.github@outlook.com>
2017-09-29 04:04:25 +0000

    Fix xrange issue in Python 3
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/conv1d_test.py
+++ tensorflow/python/kernel_tests/conv1d_test.py
@@ -18,6 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
