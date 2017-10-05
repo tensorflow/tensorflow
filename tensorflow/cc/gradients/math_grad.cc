dbceda103f61ca2c1431483d8f5c664c3af1da7b
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-10-05 10:07:12 +0800

    ENH: import M_PI

--- tensorflow/cc/gradients/math_grad.cc
+++ tensorflow/cc/gradients/math_grad.cc
@@ -13,6 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "tensorflow/cc/ops/array_ops_internal.h"
 #include "tensorflow/cc/ops/math_ops_internal.h"
 #include "tensorflow/cc/ops/standard_ops.h"
