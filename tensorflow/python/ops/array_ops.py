253f5386cb6478dba6d9b99286775c6cbbe86a9a
Asim Shankar <ashankar@google.com>
2017-10-10 12:09:42 -0700

    eager: Fix an issue with tf.identity.
    Like with graph execution, tf.identity should accept an input that
    is not a Tensor instance but can be converted to one.
    
    PiperOrigin-RevId: 171714919

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -124,7 +124,13 @@ def identity(input, name=None):  # pylint: disable=redefined-builtin
   if context.in_graph_mode():
     return gen_array_ops.identity(input, name=name)
   else:
-    if context.context().device_name != input.device:
+    try:
+      in_device = input.device
+    except AttributeError:
+      input = ops.convert_to_tensor(input)
+      in_device = input.device
+    # TODO(ashankar): Does 'identity' need to invoke execution callbacks?
+    if context.context().device_name != in_device:
       return input._copy()  # pylint: disable=protected-access
     return input
 
