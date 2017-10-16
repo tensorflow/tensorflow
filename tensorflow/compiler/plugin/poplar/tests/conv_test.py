d53f4d6e350ddf0e47683a54dfef9a7fea0186be
DavidNorman <davidn@graphcore.ai>
2017-10-16 14:13:29 +0100

    Include coonv op tests again - use smaller tensors

--- tensorflow/compiler/plugin/poplar/tests/conv_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv_test.py
@@ -15,38 +15,38 @@ import numpy as np
 
 class IpuXlaConvTest(test_util.TensorFlowTestCase):
 
-    # def testConv3x3_Pad1x1(self):
-    #     with tf.device("/device:XLA_IPU:0"):
-    #         with tf.Session() as sess:
-    #             pa = tf.placeholder(tf.float32, [1,112,112,64], name="a")
-    #             pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
-    #             output = nn_ops.convolution(pa, pb, padding="SAME")
-    #
-    #             fd = {
-    #                 pa: np.zeros([1,112,112,64]),
-    #                 pb: np.zeros([3,3,64,128])
-    #             }
-    #             result = sess.run(output, fd)
-    #             self.assertAllClose(result,
-    #                                 np.zeros([1,112,112,128]))
+    def testConv3x3_Pad1x1(self):
+        with tf.device("/device:XLA_IPU:0"):
+            with tf.Session() as sess:
+                pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
+                pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
+                output = nn_ops.convolution(pa, pb, padding="SAME")
 
-    # def testConv3x3_WithBias(self):
-    #     with tf.device("/device:XLA_IPU:0"):
-    #         with tf.Session() as sess:
-    #             pa = tf.placeholder(tf.float32, [1,112,112,64], name="a")
-    #             pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
-    #             bi = tf.placeholder(tf.float32, [128], name="b")
-    #             output = nn_ops.convolution(pa, pb, padding="SAME")
-    #             output = nn_ops.bias_add(output, bi)
-    #
-    #             fd = {
-    #                 pa: np.zeros([1,112,112,64]),
-    #                 pb: np.zeros([3,3,64,128]),
-    #                 bi: np.zeros([128]),
-    #             }
-    #             result = sess.run(output, fd)
-    #             self.assertAllClose(result,
-    #                                 np.zeros([1,112,112,128]))
+                fd = {
+                    pa: np.zeros([1,14,14,64]),
+                    pb: np.zeros([3,3,64,128])
+                }
+                result = sess.run(output, fd)
+                self.assertAllClose(result,
+                                    np.zeros([1,14,14,128]))
+
+    def testConv3x3_WithBias(self):
+        with tf.device("/device:XLA_IPU:0"):
+            with tf.Session() as sess:
+                pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
+                pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
+                bi = tf.placeholder(tf.float32, [128], name="b")
+                output = nn_ops.convolution(pa, pb, padding="SAME")
+                output = nn_ops.bias_add(output, bi)
+
+                fd = {
+                    pa: np.zeros([1,14,14,64]),
+                    pb: np.zeros([3,3,64,128]),
+                    bi: np.zeros([128]),
+                }
+                result = sess.run(output, fd)
+                self.assertAllClose(result,
+                                    np.zeros([1,14,14,128]))
 
 
     def testDepthwiseConv3x2(self):
