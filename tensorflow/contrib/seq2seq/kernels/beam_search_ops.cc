18f89c81d288f191abd56501ec6f86fe29265bdd
Eugene Brevdo <ebrevdo@google.com>
2017-10-17 08:48:29 -0700

    [tf.contrib.seq2seq] Bugfixes to BeamSearchDecoder and GatherTree.
    1. Begin the gather tree at the maximum sequence length across all beams (within the batch).
    2. Take a second pass starting from t=0 and mask out any beam ids past the *first* beam occurrence of end_token.
    3. Update the final sequence lengths to include the first <eos> token in the beam.
    4. Update dynamic_decode to allow the BeamSearchDecoder to keep track of its own "finished" states, as the shuffling in the decoder confused the tracking mechanism in dynamic_decode.  This fixes a bug where beam search decoding stops early.
    5. Cap sequence length used in GatherTree to min(max_time, max_seq_len(b)) to avoid accessing memory outside the dimensions of input matrices.
    
    Bugs caught by @bdaskalov on github and Pavel Sountsov.  Proper solution and analysis thanks to Rui Zhao.  Thanks all!
    
    Fixes #13536.
    
    PiperOrigin-RevId: 172471462

--- tensorflow/contrib/seq2seq/kernels/beam_search_ops.cc
+++ tensorflow/contrib/seq2seq/kernels/beam_search_ops.cc
@@ -49,40 +49,46 @@ class GatherTreeOp : public OpKernel {
     const Device& device = ctx->eigen_device<Device>();
     const Tensor& step_ids = ctx->input(0);
     const Tensor& parent_ids = ctx->input(1);
-    const Tensor& sequence_length = ctx->input(2);
+    const Tensor& max_sequence_lengths = ctx->input(2);
+    const Tensor& end_token = ctx->input(3);
     const TensorShape& step_ids_shape = step_ids.shape();
     OP_REQUIRES(
         ctx, step_ids_shape.dims() == 3,
         errors::InvalidArgument("step_ids must be a 3-tensor, saw shape: ",
                                 step_ids_shape.DebugString()));
+    OP_REQUIRES(ctx, TensorShapeUtils::IsVector(max_sequence_lengths.shape()),
+                errors::InvalidArgument(
+                    "max_sequence_lengths must be a vector, saw shape: ",
+                    max_sequence_lengths.shape().DebugString()));
     OP_REQUIRES(
-        ctx, TensorShapeUtils::IsMatrix(sequence_length.shape()),
-        errors::InvalidArgument("sequence_length must be a matrix, saw shape: ",
-                                sequence_length.shape().DebugString()));
-    OP_REQUIRES(ctx, sequence_length.dim_size(0) == step_ids_shape.dim_size(1),
-                errors::InvalidArgument(
-                    "Inconsistent batch sizes: sequence_length.shape[0] (",
-                    sequence_length.dim_size(0), ") != ", "step_ids.shape[1] (",
-                    step_ids_shape.dim_size(1), ")"));
-    OP_REQUIRES(ctx, sequence_length.dim_size(1) == step_ids_shape.dim_size(2),
-                errors::InvalidArgument(
-                    "Inconsistent batch sizes: sequence_length.shape[1] (",
-                    sequence_length.dim_size(1), ") != ", "step_ids.shape[2] (",
-                    step_ids_shape.dim_size(2), ")"));
+        ctx, TensorShapeUtils::IsScalar(end_token.shape()),
+        errors::InvalidArgument("end_token must be a scalar, saw shape: ",
+                                end_token.shape().DebugString()));
     OP_REQUIRES(
         ctx, step_ids_shape == parent_ids.shape(),
         errors::InvalidArgument(
             "step_ids.shape must match parent_ids.shape.  but shapes are: ",
             step_ids_shape.DebugString(), " and ",
             parent_ids.shape().DebugString()));
+    OP_REQUIRES(
+        ctx,
+        step_ids_shape.dim_size(1) == max_sequence_lengths.shape().dim_size(0),
+        errors::InvalidArgument("batch size dimensions step_ids.shape[1] and "
+                                "max_seqeuence_lengths.shape[0] must match.  "
+                                "but shapes are: ",
+                                step_ids_shape.DebugString(), " and ",
+                                max_sequence_lengths.shape().DebugString()));
     Tensor* beams;
     OP_REQUIRES_OK(ctx, ctx->allocate_output(0, step_ids_shape, &beams));
     typename TTypes<T, 3>::ConstTensor step_ids_t = step_ids.tensor<T, 3>();
     typename TTypes<T, 3>::ConstTensor parent_ids_t = parent_ids.tensor<T, 3>();
-    typename TTypes<T>::ConstMatrix seq_len_t = sequence_length.matrix<T>();
+    typename TTypes<int32>::ConstVec max_seq_lens_t =
+        max_sequence_lengths.vec<int32>();
+    typename TTypes<T>::ConstScalar end_token_t = end_token.scalar<T>();
     typename TTypes<T, 3>::Tensor beams_t = beams->tensor<T, 3>();
+    const T end_token_value = end_token_t();
     functor::GatherTree<Device, T>()(ctx, device, step_ids_t, parent_ids_t,
-                                     seq_len_t, beams_t);
+                                     max_seq_lens_t, end_token_value, beams_t);
   }
 };
 
@@ -99,27 +105,29 @@ namespace functor {
 template <>
 struct GatherTree<CPUDevice, int32> {
   void operator()(OpKernelContext* ctx, const CPUDevice& d,
-                  typename TTypes<int32, 3>::ConstTensor step_ids,
-                  typename TTypes<int32, 3>::ConstTensor parent_ids,
-                  typename TTypes<int32>::ConstMatrix sequence_length,
-                  typename TTypes<int32, 3>::Tensor beams) {
-    const int64 max_time = parent_ids.dimension(0);
-    const int64 batch_size = parent_ids.dimension(1);
-    const int64 beam_width = parent_ids.dimension(2);
+                  TTypes<int32, 3>::ConstTensor step_ids,
+                  TTypes<int32, 3>::ConstTensor parent_ids,
+                  TTypes<int32>::ConstVec max_sequence_lengths,
+                  const int32 end_token, TTypes<int32, 3>::Tensor beams) {
+    const int32 max_time = parent_ids.dimension(0);
+    const int32 batch_size = parent_ids.dimension(1);
+    const int32 beam_width = parent_ids.dimension(2);
     beams.setConstant(-1);
 
-    auto DoWork = [&, ctx](int start_batch_beam, int limit_batch_beam) {
+    auto DoWork = [&, ctx, end_token](int start_batch_beam,
+                                      int limit_batch_beam) {
       for (int32 i = start_batch_beam; i < limit_batch_beam; ++i) {
         const int32 batch = i / beam_width;
         const int32 beam = i % beam_width;
-        int32 seq_len_b = sequence_length(batch, beam);
-        if (seq_len_b <= 0) {
+        const int32 max_seq_len_b =
+            Eigen::numext::mini(max_time, max_sequence_lengths(batch));
+        if (max_seq_len_b <= 0) {
           continue;
         }
-        beams(seq_len_b - 1, batch, beam) =
-            step_ids(seq_len_b - 1, batch, beam);
-        int32 parent = parent_ids(seq_len_b - 1, batch, beam);
-        for (int32 level = seq_len_b - 2; level >= 0; --level) {
+        beams(max_seq_len_b - 1, batch, beam) =
+            step_ids(max_seq_len_b - 1, batch, beam);
+        int32 parent = parent_ids(max_seq_len_b - 1, batch, beam);
+        for (int32 level = max_seq_len_b - 2; level >= 0; --level) {
           if (parent < 0 || parent > beam_width) {
             ctx->SetStatus(
                 errors::InvalidArgument("Saw invalid parent id ", parent,
@@ -130,6 +138,14 @@ struct GatherTree<CPUDevice, int32> {
           beams(level, batch, beam) = step_ids(level, batch, parent);
           parent = parent_ids(level, batch, parent);
         }
+        bool finished = false;
+        for (int32 time = 0; time < max_seq_len_b; ++time) {
+          if (finished) {
+            beams(time, batch, beam) = -1;
+          } else if (beams(time, batch, beam) == end_token) {
+            finished = true;
+          }
+        }
       }
     };
     // Guesstimate of cost; ~5 lookup/store/compare per inner beam
@@ -137,7 +153,7 @@ struct GatherTree<CPUDevice, int32> {
     const int64 batch_beam_cost =
         Eigen::TensorOpCost::DivCost<int32>() +
         6 * Eigen::TensorOpCost::AddCost<int32>() +
-        max_time * (5 * Eigen::TensorOpCost::AddCost<int32>());
+        2 * max_time * (5 * Eigen::TensorOpCost::AddCost<int32>());
     auto worker_threads = *(ctx->device()->tensorflow_cpu_worker_threads());
     Shard(worker_threads.num_threads, worker_threads.workers,
           batch_size * beam_width, batch_beam_cost, DoWork);
@@ -148,24 +164,26 @@ struct GatherTree<CPUDevice, int32> {
 
 #if GOOGLE_CUDA
 namespace functor {
-#define DECLARE_GPU_SPEC(T)                            \
-  template <>                                          \
-  void GatherTree<GPUDevice, T>::operator()(           \
-      OpKernelContext* ctx, const GPUDevice& d,        \
-      typename TTypes<T, 3>::ConstTensor step_ids,     \
-      typename TTypes<T, 3>::ConstTensor parent_ids,   \
-      typename TTypes<T>::ConstMatrix sequence_length, \
-      typename TTypes<T, 3>::Tensor beams);            \
+#define DECLARE_GPU_SPEC(T)                                            \
+  template <>                                                          \
+  void GatherTree<GPUDevice, T>::operator()(                           \
+      OpKernelContext* ctx, const GPUDevice& d,                        \
+      typename TTypes<T, 3>::ConstTensor step_ids,                     \
+      typename TTypes<T, 3>::ConstTensor parent_ids,                   \
+      TTypes<int32>::ConstVec max_sequence_lengths, const T end_token, \
+      typename TTypes<T, 3>::Tensor beams);                            \
   extern template struct GatherTree<GPUDevice, T>;
 
 DECLARE_GPU_SPEC(int32);
 #undef DECLARE_GPU_SPEC
 }  // end namespace functor
 
-#define REGISTER_GPU_KERNEL(T)                                      \
-  REGISTER_KERNEL_BUILDER(                                          \
-      Name("GatherTree").Device(DEVICE_GPU).TypeConstraint<T>("T"), \
-      GatherTreeOp<GPUDevice, T>);
+#define REGISTER_GPU_KERNEL(T)                          \
+  REGISTER_KERNEL_BUILDER(Name("GatherTree")            \
+                              .Device(DEVICE_GPU)       \
+                              .TypeConstraint<T>("T")   \
+                              .HostMemory("end_token"), \
+                          GatherTreeOp<GPUDevice, T>);
 
 REGISTER_GPU_KERNEL(int32);
 #undef REGISTER_GPU_KERNEL
