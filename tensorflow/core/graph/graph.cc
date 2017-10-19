2878bd1e1162b78458447e02596cfec35367d988
Anthony Platanios <e.a.platanios@gmail.com>
2017-10-18 20:33:23 -0400

    Made a small requested change.

--- tensorflow/core/graph/graph.cc
+++ tensorflow/core/graph/graph.cc
@@ -456,9 +456,10 @@ const Edge* Graph::AddControlEdge(Node* source, Node* dest,
 void Graph::RemoveControlEdge(const Edge* e) {
   if (!e->src_->IsSource() && !e->dst_->IsSink()) {
     e->dst_->MaybeCopyOnWrite();
+    std::string e_src_name = strings::StrCat("^", e->src_->name());
     auto* inputs = e->dst_->props_->node_def.mutable_input();
     for (auto it = inputs->begin(); it != inputs->end(); ++it) {
-      if (*it == strings::StrCat("^", e->src_->name())) {
+      if (*it == e_src_name) {
         inputs->erase(it);
         break;
       }
