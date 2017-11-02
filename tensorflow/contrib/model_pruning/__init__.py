83621c7ec59a400d83de0dd3e7b45ec670c02893
A. Unique TensorFlower <gardener@tensorflow.org>
2017-11-01 17:11:25 -0700

    Bug fix: Expose get_pruning_hparams function
    PiperOrigin-RevId: 174260120

--- tensorflow/contrib/model_pruning/__init__.py
+++ tensorflow/contrib/model_pruning/__init__.py
@@ -28,6 +28,7 @@ from tensorflow.contrib.model_pruning.python.learning import train
 from tensorflow.contrib.model_pruning.python.pruning import apply_mask
 from tensorflow.contrib.model_pruning.python.pruning import get_masked_weights
 from tensorflow.contrib.model_pruning.python.pruning import get_masks
+from tensorflow.contrib.model_pruning.python.pruning import get_pruning_hparams
 from tensorflow.contrib.model_pruning.python.pruning import get_thresholds
 from tensorflow.contrib.model_pruning.python.pruning import get_weight_sparsity
 from tensorflow.contrib.model_pruning.python.pruning import get_weights
@@ -39,8 +40,8 @@ from tensorflow.python.util.all_util import remove_undocumented
 _allowed_symbols = [
     'masked_convolution', 'masked_conv2d', 'masked_fully_connected',
     'MaskedBasicLSTMCell', 'MaskedLSTMCell', 'train', 'apply_mask',
-    'get_masked_weights', 'get_masks', 'get_thresholds', 'get_weights',
-    'get_weight_sparsity', 'Pruning'
+    'get_masked_weights', 'get_masks', 'get_pruning_hparams', 'get_thresholds',
+    'get_weights', 'get_weight_sparsity', 'Pruning'
 ]
 
 remove_undocumented(__name__, _allowed_symbols)
