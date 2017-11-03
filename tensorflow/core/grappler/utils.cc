29afeb8183597ee732c28ce0f7d97c4f343036da
Taehoon Lee <me@taehoonlee.com>
2017-11-03 17:07:12 +0900

    Fix typos

--- tensorflow/core/grappler/utils.cc
+++ tensorflow/core/grappler/utils.cc
@@ -59,7 +59,7 @@ const std::set<NodeDef*>& NodeMap::GetOutputs(const string& node_name) const {
 void NodeMap::AddNode(const string& name, NodeDef* node) {
   auto ret = nodes_.insert(std::make_pair(name, node));
   CHECK(ret.second) << "Pair (" << name << "," << node
-                    << ") is not inserted because a same key already exists.";
+                    << ") is not inserted because the same key already exists.";
 }
 
 void NodeMap::AddOutput(const string& node_name, const string& output_name) {
