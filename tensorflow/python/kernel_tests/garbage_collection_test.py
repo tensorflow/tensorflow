86d491e9047bd663bc707ba31348025449e255aa
Allen Lavoie <allenl@google.com>
2017-11-06 15:05:13 -0800

    More thoroughly disable the should_use_result decorator when executing eagerly.
    It was creating reference cycles.
    
    Adds a test that TensorArrays create no reference cycles in eager mode.
    
    PiperOrigin-RevId: 174768765

--- tensorflow/python/kernel_tests/garbage_collection_test.py
+++ tensorflow/python/kernel_tests/garbage_collection_test.py
@@ -24,8 +24,10 @@ from __future__ import print_function
 import gc
 
 from tensorflow.python.eager import context
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import resource_variable_ops
+from tensorflow.python.ops import tensor_array_ops
 from tensorflow.python.platform import test
 
 
@@ -58,6 +60,28 @@ class NoReferenceCycleTests(test_util.TensorFlowTestCase):
   def testEagerResourceVariables(self):
     with context.eager_mode():
       resource_variable_ops.ResourceVariable(1.0, name="a")
+
+  @assert_no_garbage_created
+  def testTensorArrays(self):
+    with context.eager_mode():
+      ta = tensor_array_ops.TensorArray(
+          dtype=dtypes.float32,
+          tensor_array_name="foo",
+          size=3,
+          infer_shape=False)
+
+      w0 = ta.write(0, [[4.0, 5.0]])
+      w1 = w0.write(1, [[1.0]])
+      w2 = w1.write(2, -3.0)
+
+      r0 = w2.read(0)
+      r1 = w2.read(1)
+      r2 = w2.read(2)
+
+      d0, d1, d2 = self.evaluate([r0, r1, r2])
+      self.assertAllEqual([[4.0, 5.0]], d0)
+      self.assertAllEqual([[1.0]], d1)
+      self.assertAllEqual(-3.0, d2)
 
 
 if __name__ == "__main__":
