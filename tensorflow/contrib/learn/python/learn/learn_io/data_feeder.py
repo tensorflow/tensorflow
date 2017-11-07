ca3653a4926823631fcedec254d87d13cabd0101
Martin Wicke <martin.wicke@gmail.com>
2017-11-07 11:46:49 -0800

    Import tensor_util and use it.

--- tensorflow/contrib/learn/python/learn/learn_io/data_feeder.py
+++ tensorflow/contrib/learn/python/learn/learn_io/data_feeder.py
@@ -29,13 +29,13 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_util
 from tensorflow.python.ops import array_ops
 from tensorflow.python.platform import tf_logging as logging
 
 # pylint: disable=g-multiple-import,g-bad-import-order
 from .pandas_io import HAS_PANDAS, extract_pandas_data, extract_pandas_matrix, extract_pandas_labels
 from .dask_io import HAS_DASK, extract_dask_data, extract_dask_labels
-
 # pylint: enable=g-multiple-import,g-bad-import-order
 
 
@@ -363,7 +363,7 @@ class DataFeeder(object):
 
     if x_is_dict:
       num_samples = list(self._x.values())[0].shape[0]
-    elif is_tensor(self._x):
+    elif tensor_util.is_tensor(self._x):
       num_samples = self._x.shape[0].value  # shape will be a Dimension, extract an int
     else:
       num_samples = self._x.shape[0]
