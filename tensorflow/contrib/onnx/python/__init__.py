8b4c705562b60d07885da55e2b8e9797d3ea1540
Tian Jin <tian.jin1@ibm.com>
2017-11-07 00:20:43 -0500

    fix typos

--- tensorflow/contrib/onnx/python/__init__.py
+++ tensorflow/contrib/onnx/python/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Module for variants of ops in tf.nn."""
+"""Module for variants of ops in tf.onnx."""
 
 from __future__ import absolute_import
 from __future__ import division
