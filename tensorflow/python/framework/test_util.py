5d36e0d638c41e4d4c3d1c29d508d3a6439c8010
Yong Tang <yong.tang.github@outlook.com>
2017-11-08 12:41:03 -0800

    Add half-precision support for `DepthwiseConv2dNative` (#12976)
    * Add float16 support for `DepthwiseConv2dNative`
    This fix tries to address the request raised in 12327
    so that float16 is supported for `DepthwiseConv2dNative`
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Add test cases for float16 support of `DepthwiseConv2dNative`
    
    A couple of tests for float16 have been disabled as error is
    large.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Add float16 support for `DepthwiseConv2dNative` on GPU
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    Add Eigen::half support for DepthwiseConv2dNative on GPU
    
    This commit add Eigen::half support for DepthwiseConv2dNative on GPU
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Disable float16 support for the gradient of DepthwiseConv2dNative
    
    The accuracy is not high enough for float16 of gradient.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/framework/test_util.py
+++ tensorflow/python/framework/test_util.py
@@ -984,8 +984,9 @@ class TensorFlowTestCase(googletest.TestCase):
       err: A float value.
       msg: An optional string message to append to the failure message.
     """
+    # f1 == f2 is needed here as we might have: f1, f2 = inf, inf
     self.assertTrue(
-        math.fabs(f1 - f2) <= err,
+        f1 == f2 or math.fabs(f1 - f2) <= err,
         "%f != %f +/- %f%s" % (f1, f2, err, " (%s)" % msg
                                if msg is not None else ""))
 
