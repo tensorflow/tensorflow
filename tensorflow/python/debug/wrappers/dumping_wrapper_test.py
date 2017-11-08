15605b68f368753ae65476fbb172d293d997ec7d
Shanqing Cai <cais@google.com>
2017-11-08 07:36:56 -0800

    tfdbg: Fix a test bug hidden in a child thread
    PiperOrigin-RevId: 175004323

--- tensorflow/python/debug/wrappers/dumping_wrapper_test.py
+++ tensorflow/python/debug/wrappers/dumping_wrapper_test.py
@@ -364,12 +364,14 @@ class DumpingDebugWrapperSessionTest(test_util.TensorFlowTestCase):
         thread_name_filter=r"MainThread$")
 
     self.assertAllClose(1.0, sess.run(self.delta))
+    child_thread_result = []
     def child_thread_job():
-      sess.run(sess.run(self.eta))
+      child_thread_result.append(sess.run(self.eta))
 
     thread = threading.Thread(name="ChildThread", target=child_thread_job)
     thread.start()
     thread.join()
+    self.assertAllClose([-1.4], child_thread_result)
 
     dump_dirs = glob.glob(os.path.join(self.session_root, "run_*"))
     self.assertEqual(1, len(dump_dirs))
