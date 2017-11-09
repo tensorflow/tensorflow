d45f27d4586ef2d2dcc405eaac97b1515dad9671
PW486 <ooqwe486@gmail.com>
2017-11-09 23:05:38 +0900

    Fixed typos, comments

--- tensorflow/core/util/strided_slice_op.cc
+++ tensorflow/core/util/strided_slice_op.cc
@@ -218,8 +218,8 @@ Status ValidateStridedSliceOp(
 
   // Step 2: Make a sparse spec into a full index spec
   //
-  // The sparse spec does not corresponds to the number of dimensions
-  // Make a dense spec that corresponds to the number of dimensions
+  // The sparse spec does not correspond to the number of dimensions
+  // Make a dense spec that correspond to the number of dimensions
   //
   // For example suppose foo[...,3:] on foo.shape=(2,2,3) then
   // we need to produce the missing begin_mask for the first two
