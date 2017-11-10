ec482438aeec42bcc402fd231c349afccc678120
Christoph Boeddeker <boeddeker@users.noreply.github.com>
2017-11-10 13:48:34 +0100

    PrefetchDataset with buffer_size==0 results in deadlock

--- tensorflow/python/data/ops/dataset_ops.py
+++ tensorflow/python/data/ops/dataset_ops.py
@@ -1604,6 +1604,7 @@ class PrefetchDataset(Dataset):
 
   def __init__(self, input_dataset, buffer_size):
     """See `Dataset.prefetch()` for details."""
+    assert buffer_size > 0, "The buffer_size ({}) has to be > 0.".format(buffer_size)
     super(PrefetchDataset, self).__init__()
     self._input_dataset = input_dataset
     self._buffer_size = ops.convert_to_tensor(buffer_size, dtype=dtypes.int64,
