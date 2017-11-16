0dfcc34c954513ff26d20729712baade9dda93ed
A. Unique TensorFlower <gardener@tensorflow.org>
2017-11-15 18:11:54 -0800

    Cast offsets into int64 to allow for cases where tag_indices is of type int64 (e.g. the output of a call to tf.lookup.*).
    PiperOrigin-RevId: 175912063

--- tensorflow/contrib/crf/python/ops/crf.py
+++ tensorflow/contrib/crf/python/ops/crf.py
@@ -193,6 +193,9 @@ def crf_unary_score(tag_indices, sequence_lengths, inputs):
   offsets = array_ops.expand_dims(
       math_ops.range(batch_size) * max_seq_len * num_tags, 1)
   offsets += array_ops.expand_dims(math_ops.range(max_seq_len) * num_tags, 0)
+  # Use int32 or int64 based on tag_indices' dtype.
+  if tag_indices.dtype == dtypes.int64:
+    offsets = math_ops.to_int64(offsets)
   flattened_tag_indices = array_ops.reshape(offsets + tag_indices, [-1])
 
   unary_scores = array_ops.reshape(
@@ -305,7 +308,7 @@ def viterbi_decode(score, transition_params):
 
   Returns:
     viterbi: A [seq_len] list of integers containing the highest scoring tag
-        indicies.
+        indices.
     viterbi_score: A float containing the score for the Viterbi sequence.
   """
   trellis = np.zeros_like(score)
@@ -385,7 +388,7 @@ class CrfDecodeBackwardRnnCell(rnn_cell.RNNCell):
     """Initialize the CrfDecodeBackwardRnnCell.
 
     Args:
-      num_tags
+      num_tags: The number of tags.
     """
     self._num_tags = num_tags
 
@@ -434,7 +437,7 @@ def crf_decode(potentials, transition_params, sequence_length):
 
   Returns:
     decode_tags: A [batch_size, max_seq_len] tensor, with dtype tf.int32.
-                Contains the highest scoring tag indicies.
+                Contains the highest scoring tag indices.
     best_score: A [batch_size] tensor, containing the score of decode_tags.
   """
   # For simplicity, in shape comments, denote:
