45430ffb2190c74016d63eb0ceb2c388cdc3a59c
apantykhin <apantykhin@gmail.com>
2017-11-16 21:06:02 +0400

    simplify imports

--- tensorflow/python/ops/standard_ops.py
+++ tensorflow/python/ops/standard_ops.py
@@ -41,13 +41,7 @@ from tensorflow.python.ops.clip_ops import *
 from tensorflow.python.ops.special_math_ops import *
 # TODO(vrv): Switch to import * once we're okay with exposing the module.
 from tensorflow.python.ops.confusion_matrix import confusion_matrix
-from tensorflow.python.ops.control_flow_ops import Assert
-from tensorflow.python.ops.control_flow_ops import group
-from tensorflow.python.ops.control_flow_ops import no_op
-from tensorflow.python.ops.control_flow_ops import tuple
-from tensorflow.python.ops.control_flow_ops import cond
-from tensorflow.python.ops.control_flow_ops import case
-from tensorflow.python.ops.control_flow_ops import while_loop
+from tensorflow.python.ops.control_flow_ops import *
 from tensorflow.python.ops.data_flow_ops import *
 from tensorflow.python.ops.functional_ops import *
 from tensorflow.python.ops.gradients import *
