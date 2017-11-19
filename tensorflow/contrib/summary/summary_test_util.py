5c5d4bce31730ddb6a3fb8ebde43f336ed93dfb9
Gunhan Gulsoy <gunan@google.com>
2017-11-18 23:52:32 -0800

    Fix pip package tests. (#14685)
    * Disable contrib/summary tests from pip runs.
    
    They depend on a testonly package, so they break pip test runs for
    nightly and releases.
    
    * Disable reader_ops_test from pip tests.
    
    It depends on a testonly module, which breaks nightly and release tests.
    
    * Instead of disabling tests, merge test_internal package into test_util.
    
    * Add import for sqlite3 to summary_test_util.
    
    * Add functools import to summary_test_util.

--- tensorflow/contrib/summary/summary_test_util.py
+++ tensorflow/contrib/summary/summary_test_util.py
@@ -19,11 +19,36 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import functools
 import os
+import sqlite3
 
+from tensorflow.contrib.summary import summary_ops
 from tensorflow.core.util import event_pb2
+from tensorflow.python.framework import test_util
 from tensorflow.python.lib.io import tf_record
 from tensorflow.python.platform import gfile
+
+
+class SummaryDbTest(test_util.TensorFlowTestCase):
+  """Helper for summary database testing."""
+
+  def setUp(self):
+    super(SummaryDbTest, self).setUp()
+    self.db_path = os.path.join(self.get_temp_dir(), 'DbTest.sqlite')
+    if os.path.exists(self.db_path):
+      os.unlink(self.db_path)
+    self.db = sqlite3.connect(self.db_path)
+    self.create_summary_db_writer = functools.partial(
+        summary_ops.create_summary_db_writer,
+        db_uri=self.db_path,
+        experiment_name='experiment',
+        run_name='run',
+        user_name='user')
+
+  def tearDown(self):
+    self.db.close()
+    super(SummaryDbTest, self).tearDown()
 
 
 def events_from_file(filepath):
@@ -60,3 +85,15 @@ def events_from_logdir(logdir):
   files = gfile.ListDirectory(logdir)
   assert len(files) == 1, "Found not exactly one file in logdir: %s" % files
   return events_from_file(os.path.join(logdir, files[0]))
+
+
+def get_one(db, q, *p):
+  return db.execute(q, p).fetchone()[0]
+
+
+def get_all(db, q, *p):
+  return unroll(db.execute(q, p).fetchall())
+
+
+def unroll(list_of_tuples):
+  return sum(list_of_tuples, ())
