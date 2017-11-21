7fd8d4ef1edc75bb1689e8299aef6d5b2cffb20a
DavidNorman <davidn@graphcore.ai>
2017-11-21 08:59:13 +0000

    Remove temporary compile fix

--- tensorflow/compiler/tf2xla/xla_cpu_backend.cc
+++ tensorflow/compiler/tf2xla/xla_cpu_backend.cc
@@ -16,8 +16,6 @@ limitations under the License.
 #include "tensorflow/compiler/tf2xla/xla_op_registry.h"
 #include "tensorflow/core/framework/kernel_def.pb.h"
 
-#include "tensorflow/core/framework/kernel_def.pb.h"
-
 namespace tensorflow {
 
 bool CpuOpFilter(KernelDef* kdef) {
