3dbcc9485e11d5a0643baf1cfc9ccd2e6d407c06
A. Unique TensorFlower <gardener@tensorflow.org>
2017-11-22 23:19:07 -0800

    Registers StridedSlice for int64 tensor on GPUs
    PiperOrigin-RevId: 176737730

--- tensorflow/core/kernels/strided_slice_op.cc
+++ tensorflow/core/kernels/strided_slice_op.cc
@@ -427,6 +427,7 @@ REGISTER_STRIDED_SLICE(bfloat16);
 TF_CALL_GPU_NUMBER_TYPES(REGISTER_GPU);
 TF_CALL_complex64(REGISTER_GPU);
 TF_CALL_complex128(REGISTER_GPU);
+TF_CALL_int64(REGISTER_GPU);
 
 // A special GPU kernel for int32.
 // TODO(b/25387198): Also enable int32 in device memory. This kernel
