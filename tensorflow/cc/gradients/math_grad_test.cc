172dcec11e8a20b47d78d597291e833161329096
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-11-24 19:19:16 +0800

    TST: add test case

--- tensorflow/cc/gradients/math_grad_test.cc
+++ tensorflow/cc/gradients/math_grad_test.cc
@@ -865,5 +865,15 @@ TEST_F(NaryGradTest, Minimum) {
   RunTest(x, x_init_value, y, shape);
 }
 
+TEST_F(NaryGradTest, Select) {
+  TensorShape x_shape({3, 4});
+  TensorShape y_shape({3, 4});
+  auto x1 = Placeholder(scope_, DT_FLOAT, Placeholder::Shape(x_shape));
+  auto x2 = Placeholder(scope_, DT_FLOAT, Placeholder::Shape(x_shape));
+  auto condition = Greater(scope_, x1, Const(scope_, 0.f));
+  auto y = Where3(scope_, condition, x1, x2);
+  RunTest({x1, x2}, {x_shape, x_shape}, {y}, {y_shape});
+}
+
 }  // namespace
 }  // namespace tensorflow
