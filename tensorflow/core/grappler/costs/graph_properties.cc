2167eb04446ebce76ab2abecd53463240e5320b2
Benoit Steiner <bsteiner@google.com>
2017-11-29 12:06:55 -0800

    Made sure the unknown shapes of placeholders always propagate to their fanouts
    PiperOrigin-RevId: 177344207

--- tensorflow/core/grappler/costs/graph_properties.cc
+++ tensorflow/core/grappler/costs/graph_properties.cc
@@ -313,15 +313,17 @@ class SymbolicShapeRefiner {
   Status UpdateNode(const Node* node, bool relax, bool* refined) {
     return shape_refiner_->UpdateNode(node, relax, refined);
   }
-  Status SetShape(const Node* node, int output_port,
-                  shape_inference::ShapeHandle shape) {
-    return shape_refiner_->SetShape(node, output_port, shape);
-  }
   Status SetUnknownShape(const Node* node, int output_port) {
     shape_inference::ShapeHandle shape =
         GetUnknownOutputShape(node, output_port);
-    return shape_refiner_->SetShape(node, output_port, shape);
+    InferenceContext* ctx = GetContext(node);
+    if (ctx == nullptr) {
+      return errors::InvalidArgument("Missing context");
+    }
+    ctx->set_output(output_port, shape);
+    return Status::OK();
   }
+
   struct ShapeId {
     const Node* node;
     int port_id;
