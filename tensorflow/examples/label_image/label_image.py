66225bd432e8a9465611b9b919465bf3652f4d97
Koan-Sin Tan <koansin.tan@gmail.com>
2017-12-01 12:16:23 +0800

    remove extraneous semicolons

--- tensorflow/examples/label_image/label_image.py
+++ tensorflow/examples/label_image/label_image.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
 
   input_name = "import/" + input_layer
   output_name = "import/" + output_layer
-  input_operation = graph.get_operation_by_name(input_name);
-  output_operation = graph.get_operation_by_name(output_name);
+  input_operation = graph.get_operation_by_name(input_name)
+  output_operation = graph.get_operation_by_name(output_name)
 
   with tf.Session(graph=graph) as sess:
     results = sess.run(output_operation.outputs[0],
