8b2f6ceeb02f5fe8b0d63a41e3119127c76907b6
Brennan Saeta <saeta@google.com>
2017-12-01 16:32:49 -0800

    Log curl request state when requests hang.
    PiperOrigin-RevId: 177657178

--- tensorflow/core/platform/cloud/curl_http_request.h
+++ tensorflow/core/platform/cloud/curl_http_request.h
@@ -208,6 +208,8 @@ class LibCurl {
   virtual void curl_slist_free_all(curl_slist* list) = 0;
   virtual char* curl_easy_escape(CURL* curl, const char* str, int length) = 0;
   virtual void curl_free(void* p) = 0;
+
+  virtual const char* curl_easy_strerror(CURLcode errornum) = 0;
 };
 
 }  // namespace tensorflow
