bd65f17656471aaabd1fc6951869516cd960f492
JxKing <jinxin900924@gmail.com>
2017-12-05 09:07:51 +0800

    Update elastic_averaging_optimizer_test.py

--- tensorflow/contrib/opt/python/training/elastic_averaging_optimizer_test.py
+++ tensorflow/contrib/opt/python/training/elastic_averaging_optimizer_test.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for ElasticAverageOptimizer."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import portpicker
 from tensorflow.python.framework import constant_op
