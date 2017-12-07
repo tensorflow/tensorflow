659684e8f2043c010b0445bf4dd08640cf49fee8
Andrew Harp <andrewharp@users.noreply.github.com>
2017-12-07 12:32:23 -0500

    add paren for clarity

--- tensorflow/examples/android/src/org/tensorflow/demo/CameraActivity.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/CameraActivity.java
@@ -336,7 +336,7 @@ public abstract class CameraActivity extends Activity
         // Fallback to camera1 API for internal cameras that don't have full support.
         // This should help with legacy situations where using the camera2 API causes
         // distorted or otherwise broken previews.
-        useCamera2API = facing == CameraCharacteristics.LENS_FACING_EXTERNAL
+        useCamera2API = (facing == CameraCharacteristics.LENS_FACING_EXTERNAL)
             || isHardwareLevelSupported(characteristics, 
                                         CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL);
         LOGGER.i("Camera API lv2?: %s", useCamera2API);
