4ff2e5d5d20622fc155c3e56fa7dd09711a4f5f2
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-12-09 20:51:05 +0800

    TST: add test case

--- tensorflow/python/keras/_impl/keras/layers/lstm_test.py
+++ tensorflow/python/keras/_impl/keras/layers/lstm_test.py
@@ -39,6 +39,22 @@ class LSTMLayerTest(test.TestCase):
                   'return_sequences': True},
           input_shape=(num_samples, timesteps, embedding_dim))
 
+  def test_static_shape_inference_LSTM(self):
+    # Github issue: 15165
+    num_samples = 2
+    timesteps = 3
+    embedding_dim = 4
+    units = 2
+    with self.test_session():
+      inputs = keras.layers.Dense(embedding_dim,
+                                  input_shape=(timesteps, embedding_dim))
+      layer = keras.layers.LSTM(units, return_sequences=True)
+      model = keras.models.Sequential()
+      model.add(inputs)
+      model.add(layer)
+      sequences_shape = model.layers[-1].output.get_shape()
+      self.assertEquals(sequences_shape.as_list(), [None, timesteps, units])
+
   def test_dynamic_behavior_LSTM(self):
     num_samples = 2
     timesteps = 3
