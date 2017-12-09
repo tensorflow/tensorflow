826784467ecd0fed8e44645ef1032496fed88ba5
Changming Sun <chasun@microsoft.com>
2017-12-09 10:03:43 +0800

    Enable GCS filesystem for Windows (#14856)

--- tensorflow/tools/ci_build/windows/bazel/bazel_test_lib.sh
+++ tensorflow/tools/ci_build/windows/bazel/bazel_test_lib.sh
@@ -111,7 +111,7 @@ function run_configure_for_cpu_build {
     export TF_NEED_MKL=0
   fi
   export TF_NEED_VERBS=0
-  export TF_NEED_GCP=0
+  export TF_NEED_GCP=1
   export TF_NEED_HDFS=0
   export TF_NEED_OPENCL_SYCL=0
   echo "" | ./configure
