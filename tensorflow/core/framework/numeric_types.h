10d45f9ca118ed37b190140f9310e58f95d4d52c
A. Unique TensorFlower <gardener@tensorflow.org>
2017-12-13 10:01:47 -0800

    Make bfloat16 works with complex
    PiperOrigin-RevId: 178917043

--- tensorflow/core/framework/numeric_types.h
+++ tensorflow/core/framework/numeric_types.h
@@ -58,6 +58,14 @@ struct bfloat16 {
 #endif
   }
 
+  // Following the convention of numpy, converting between complex and
+  // float will lead to loss of imag value.
+  explicit EIGEN_DEVICE_FUNC bfloat16(const complex64& val)
+      : bfloat16(val.real()) {}
+
+  explicit EIGEN_DEVICE_FUNC bfloat16(const complex128& val)
+      : bfloat16(static_cast<float>(val.real())) {}
+
   template <class T>
   explicit EIGEN_DEVICE_FUNC bfloat16(const T& val)
       : bfloat16(static_cast<float>(val)) {}
@@ -127,6 +135,14 @@ struct bfloat16 {
 
   EIGEN_DEVICE_FUNC explicit operator double() const {
     return static_cast<double>(float(*this));
+  }
+
+  EIGEN_DEVICE_FUNC explicit operator complex64() const {
+    return complex64(float(*this), float(0.0));
+  }
+
+  EIGEN_DEVICE_FUNC explicit operator complex128() const {
+    return complex128(double(*this), double(0.0));
   }
 
   static bfloat16 epsilon() {
