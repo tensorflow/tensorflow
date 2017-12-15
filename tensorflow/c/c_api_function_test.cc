98f060d0e069e1fb8d290fb290b1ae3c0766878a
Shahid <shahid@us.ibm.com>
2017-12-15 15:17:04 +0530

    Correcting code to conform to Google C++ Style Guide
    Correcting as per the changes suggested by "clang-format".

--- tensorflow/c/c_api_function_test.cc
+++ tensorflow/c/c_api_function_test.cc
@@ -1462,11 +1462,11 @@ TEST_F(CApiFunctionTest, AppendHash) {
                  /*append_hash=*/true);
   tensorflow::FunctionDef fdef;
   ASSERT_TRUE(GetFunctionDef(func_, &fdef));
-  #if ((__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__) && defined(__s390x__))
+#if ((__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__) && defined(__s390x__))
   ASSERT_EQ(string("func_name_base_ZpgUD4x8oqk"), fdef.signature().name());
-  #else
+#else
   ASSERT_EQ(string("func_name_base_qaJ8jA8UmGY"), fdef.signature().name());
-  #endif
+#endif
 }
 
 TEST_F(CApiFunctionTest, GetOpDef) {
