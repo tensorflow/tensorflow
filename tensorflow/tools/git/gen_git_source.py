8d3bd70a547fa843ba3e3da6f1379c4fdd836f2b
Alex Sergeev <alexander.sergeev@live.com>
2017-12-15 08:59:53 -0800

    Support --config=monolithic in tf.sysconfig.get_link_flags() (#15139)
    * Don't add -ltensorflow_framework to sysconfig LFLAGS if the build is monolithic

--- tensorflow/tools/git/gen_git_source.py
+++ tensorflow/tools/git/gen_git_source.py
@@ -180,6 +180,13 @@ const int tf_cxx11_abi_flag() {
   return 0;
 #endif
 }
+const int tf_monolithic_build() {
+#ifdef TENSORFLOW_MONOLITHIC_BUILD
+  return 1;
+#else
+  return 0;
+#endif
+}
 """ % git_version
   open(filename, "w").write(contents)
 
