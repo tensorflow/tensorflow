e7d579ec59469380f8513975f0caf7d7f5075369
Amit Patankar <amitpatankar@google.com>
2017-12-27 13:32:39 -0800

    Trying something else for sqlite.

--- tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
@@ -26,7 +26,7 @@ add-apt-repository -y ppa:fkrull/deadsnakes
 # Install python dep
 apt-get install python-dev
 # Install bz2 dep
-# apt-get install libbz2-dev bzip2-devel
+apt-get install libbz2-dev bzip2-devel
 # Install curses dep
 apt-get install libncurses5 libncurses5-dev libncursesw5
 # Install readline dep
@@ -37,14 +37,6 @@ apt-get install libsqlite3-dev
 apt-get update
 
 set -e
-# Install bz2 dep
-wget http://bzip.org/1.0.6/bzip2-1.0.6.tar.gz
-tar xpzf bzip2-1.0.6.tar.gz
-cd bzip2-1.0.6
-make
-make -f Makefile-libbz2_so
-make install PREFIX=/path/to/local # /usr/local by default
-cd ..
 
 # Install Python 3.6 and dev library
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
