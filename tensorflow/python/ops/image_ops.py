0482f5eb0c062e875866398335e04eefbdd39a9f
Paweł Kapica <pawel.kapica@gmail.com>
2018-01-03 09:49:47 +0100

    image colorspace conversion using tensordot op

--- tensorflow/python/ops/image_ops.py
+++ tensorflow/python/ops/image_ops.py
@@ -49,6 +49,10 @@ See the @{$python/image} guide.
 @@grayscale_to_rgb
 @@hsv_to_rgb
 @@rgb_to_hsv
+@@rgb_to_yiq
+@@yiq_to_rgb
+@@rgb_to_yuv
+@@yuv_to_rgb
 @@convert_image_dtype
 @@adjust_brightness
 @@random_brightness
