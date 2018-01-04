9ad5d74b3bc869260b6c65e6152b266a4e392123
Skye Wanderman-Milne <skyewm@google.com>
2018-01-03 16:08:49 -0800

    Fix bug with imported while loops with C API enabled.
    Specifically, make control_flow_util.GetOutputContext robust to
    imported exit nodes (which don't have control flow contexts). This was
    a bug prior to the C API being enabled in that imported exit nodes
    would not have contexts, but it happened to not be exposed. Note that
    importing a metagraph will add the contexts back after doing the
    initial import, but there's still a window where no contexts are
    assigned.
    
    PiperOrigin-RevId: 180730785

--- tensorflow/python/ops/control_flow_util.py
+++ tensorflow/python/ops/control_flow_util.py
@@ -88,7 +88,10 @@ def GetLoopConstantEnter(value):
 def GetOutputContext(op):
   """Return the control flow context for the output of an op."""
   ctxt = op._get_control_flow_context()  # pylint: disable=protected-access
-  if IsLoopExit(op):
+  # Exit nodes usually have a control flow context, except in the case where the
+  # exit node was imported via import_graph_def (in which case no nodes have
+  # control flow contexts).
+  if ctxt is not None and IsLoopExit(op):
     ctxt = ctxt.outer_context
   return ctxt
 
