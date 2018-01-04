e2b1176ba09fb0da2f89961eb1de6c5556fd5159
Yong Tang <yong.tang.github@outlook.com>
2018-01-04 05:41:25 +0000

    Add axis support for tf.nn.crelu
    This fix tries to address the issue raised in 15619
    where it was not possible to specify an `axis` for
    `tf.nn.crelu`. By default, `axis=-1` was used for concatenation
    implicitly.
    
    This fix adds the support of `axis` for tf.nn.crelu,
    and adds test cases for it.
    
    This fix fixes 15619.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -1498,7 +1498,7 @@ def bias_add_v1(value, bias, name=None):
     return gen_nn_ops._bias_add_v1(value, bias, name=name)
 
 
-def crelu(features, name=None):
+def crelu(features, name=None, axis=-1):
   """Computes Concatenated ReLU.
 
   Concatenates a ReLU which selects only the positive part of the activation
@@ -1510,13 +1510,14 @@ def crelu(features, name=None):
     features: A `Tensor` with type `float`, `double`, `int32`, `int64`, `uint8`,
       `int16`, or `int8`.
     name: A name for the operation (optional).
+    axis: The axis that the output values are concatenated along. Default is -1.
 
   Returns:
     A `Tensor` with the same type as `features`.
   """
   with ops.name_scope(name, "CRelu", [features]) as name:
     features = ops.convert_to_tensor(features, name="features")
-    c = array_ops.concat([features, -features], -1, name=name)
+    c = array_ops.concat([features, -features], axis, name=name)
     return gen_nn_ops.relu(c)
 
 
