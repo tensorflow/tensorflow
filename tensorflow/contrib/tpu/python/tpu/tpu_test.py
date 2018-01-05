dbb0feec02a6c34c457ebe83ea307043939b0fc3
Akshay Agrawal <akshayka@google.com>
2018-01-05 11:41:07 -0800

    Make it possible to wrap Layer's `call` method in `tfe.defun`.
    This change:
      (1) wraps Layer's `build` method in an `init_scope`, which in turn makes it
          possible to compile the `call` method into a graph function by wrapping
          it in `tfe.defun` because the `init_scope` lifts all ops created in
          `build` out of function-building graphs;
    
      (2) defers the creation of regularizers, constructing them after `build`
          exits and thereby ensuring that they are not created inside an
          `init_scope`.
    
    PiperOrigin-RevId: 180954866

--- tensorflow/contrib/tpu/python/tpu/tpu_test.py
+++ tensorflow/contrib/tpu/python/tpu/tpu_test.py
@@ -20,8 +20,14 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.contrib.tpu.python.tpu import tpu
+from tensorflow.contrib.tpu.python.tpu import tpu_feed
+from tensorflow.contrib.tpu.python.tpu import training_loop
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.layers import convolutional
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import control_flow_util
+from tensorflow.python.ops import math_ops
 
 from tensorflow.python.platform import test
 
@@ -37,6 +43,37 @@ class TPUContextTest(test.TestCase):
     context.Exit()
     self.assertFalse(control_flow_util.IsInXLAContext(z1.op))
     self.assertTrue(control_flow_util.IsInXLAContext(z2.op))
+
+
+class TPULayerRewriteTest(test.TestCase):
+
+  def testUsingInfeedQueueWithRegularizer(self):
+    """Test that Layer regularizers can reference data created in loops."""
+
+    def make_regularizer(scale):
+      return lambda inputs: scale * math_ops.reduce_sum(math_ops.square(inputs))
+
+    def training_step(inputs, scale):
+      outputs = convolutional.conv2d(
+          inputs,
+          filters=16,
+          kernel_size=(3, 3),
+          data_format="channels_first",
+          kernel_regularizer=make_regularizer(scale))
+      loss = math_ops.reduce_mean(math_ops.square(outputs))
+      return loss.op
+
+    inputs = array_ops.zeros(shape=(128, 32, 32, 16))
+    scale = array_ops.ones(shape=())
+    infeed = tpu_feed.InfeedQueue(
+        tuple_types=[dtypes.float32, dtypes.float32],
+        tuple_shapes=[inputs.shape, scale.shape])
+
+    def loop():
+      return training_loop.repeat(5, training_step, infeed_queue=infeed)
+
+    # This should not throw an error.
+    tpu.rewrite(loop)
 
 
 if __name__ == "__main__":
