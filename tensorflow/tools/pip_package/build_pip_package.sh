07c4b72dfa16492b298b536621cf27d15c183e81
Amit Patankar <amitpatankar@google.com>
2018-01-10 17:09:48 -0800

    Adding brackets.

--- tensorflow/tools/pip_package/build_pip_package.sh
+++ tensorflow/tools/pip_package/build_pip_package.sh
@@ -27,8 +27,8 @@ function cp_external() {
   for f in `find "$src_dir" -maxdepth 1 -mindepth 1 ! -name '*local_config_cuda*' ! -name '*org_tensorflow*'`; do
     cp -R "$f" "$dest_dir"
   done
-  mkdir -p "$dest_dir/local_config_cuda/cuda/cuda/"
-  cp "${src_dir}/local_config_cuda/cuda/cuda/cuda_config.h" "$dest_dir/local_config_cuda/cuda/cuda/"
+  mkdir -p "${dest_dir}/local_config_cuda/cuda/cuda/"
+  cp "${src_dir}/local_config_cuda/cuda/cuda/cuda_config.h" "${dest_dir}/local_config_cuda/cuda/cuda/"
 }
 
 PLATFORM="$(uname -s | tr 'A-Z' 'a-z')"
