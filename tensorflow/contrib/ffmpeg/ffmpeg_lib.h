612b176cf3f8f751743b35438b24dadfb1668db2
Yong Tang <yong.tang.github@outlook.com>
2018-01-13 15:48:42 +0000

    Add stream selection support for `tf.contrib.ffmpeg.decode_audio`
    This fix tries to address the issue raised in 16073 where it
    was not possible to selectively decode a perticular stream
    with `tf.contrib.ffmpeg.decode_audio`.
    This fix adds an additional attribute `stream` which could
    be used to specify the stream to decode. By default stream=''
    which leaves the decision to ffmpeg.
    
    This fix fixes 16073.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/contrib/ffmpeg/ffmpeg_lib.h
+++ tensorflow/contrib/ffmpeg/ffmpeg_lib.h
@@ -42,7 +42,7 @@ Status WriteFile(const string& filename, tensorflow::StringPiece contents);
 // contain a separate sample for each channel. Frames are ordered by time.
 Status ReadAudioFile(const string& filename, const string& audio_format_id,
                      int32 samples_per_second, int32 channel_count,
-                     std::vector<float>* output_samples);
+                     const string& stream, std::vector<float>* output_samples);
 
 // Creates an audio file using ffmpeg in a specific format. The samples are in
 // [-1.0, 1.0]. If there are multiple channels in the audio then each frame will
