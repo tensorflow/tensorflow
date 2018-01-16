6620a8b9fad489029d3818d534e9bd91b13fff79
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-01-16 11:00:42 +0100

    tf.alphas() and tf.alphas_like functions added to the repository

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -1633,7 +1633,97 @@ def ones_like(tensor, dtype=None, name=None, optimize=True):
     if context.in_graph_mode():
       ret.set_shape(tensor.get_shape())
     return ret
+    
+def alphas_like(tensor, alpha_value, name=None, optimize=True):
+  """Creates a tensor with all elements set to the value contained in `alpha_value`.
+  Given a single tensor (`tensor`), this operation returns a tensor of the same
+  shape as `tensor` with all elements set to `alpha_value`.
+  
+  For example:
+  ```python
+  tensor = tf.constant([[1, 2, 3], [4, 5, 6]])
+  tf.alphas_like(tensor, 0.123456)  # [[0.123456, 0.123456, 0.123456], [0.123456, 0.123456, 0.123456]]
+  ```
+  Args:
+  tensor: A `Tensor`.
+  alpha_value: A custom value to be set in the new tensor. Must be `float32`, `float64`,
+    `int8`, `uint8`, `int16`, `uint16`, int32`, `int64`, `complex64`, `complex128` or `bool`.
+  name: A name for the operation (optional).
+  optimize: if true, attempt to statically determine the shape of 'tensor' and encode it as a constant.
+  Returns:
+  A `Tensor` with all elements set to `alpha_value`.
+  """
+  with ops.name_scope(name, "alphas_like", [tensor]) as name:
+  tensor = ops.convert_to_tensor(tensor, name="tensor")
 
+  if context.in_eager_mode():
+    ret = alphas(
+      shape_internal(tensor, optimize=optimize),
+      alpha_value=alpha_value,
+      name=name
+    )
+
+  else: # if context.in_graph_mode():
+
+    if (optimize and tensor.shape.is_fully_defined()):
+      # We can produce a tensor independent of the value of 'tensor',
+      # since the shape is known statically.
+      ret = alphas(tensor.shape, alpha_value=alpha_value, name=name)
+
+    else:
+      ret = alphas(
+        shape_internal(tensor, optimize=optimize),
+        alpha_value=alpha_value,
+        name=name
+      )
+
+    ret.set_shape(tensor.get_shape())
+
+  return ret
+
+
+def alphas(shape, alpha_value, name=None):
+  """Creates a tensor with all elements set to `alpha_value`.
+  This operation returns a tensor with shape `shape` and all elements set to `alpha_value`.
+  For example:
+  ```python
+  tf.alphas([2, 3], alpha_value=0.123456)  # [[0.123456, 0.123456, 0.123456], [0.123456, 0.123456, 0.123456]]
+  ```
+  Args:
+  shape: A list of integers, a tuple of integers, or a 1-D `Tensor` of type
+    `int32`.  
+  alpha_value: A custom value to be set in the new tensor. Must be `float32`, `float64`,
+    `int8`, `uint8`, `int16`, `uint16`, int32`, `int64`, `complex64`, `complex128` or `bool`.
+  name: A name for the operation (optional).
+  Returns:
+  A `Tensor` with all elements set to `alpha_value`.
+  """
+
+  with ops.name_scope(name, "alphas", [shape]) as name:
+
+    alpha_tensor = convert_to_tensor(alpha_value)
+    alpha_dtype = dtypes.as_dtype(alpha_tensor.dtype).base_dtype
+
+    if not isinstance(shape, ops.Tensor):
+      try:
+        shape = constant_op._tensor_shape_tensor_conversion_function(tensor_shape.TensorShape(shape))
+      except (TypeError, ValueError):
+        shape = ops.convert_to_tensor(shape, dtype=dtypes.int32)
+
+    if not shape._shape_tuple():
+      shape = reshape(shape, [-1])  # Ensure it's a vector
+
+
+    try:
+      output = constant(alpha_value, shape=shape, dtype=alpha_dtype, name=name)
+
+    except (TypeError, ValueError) as e:
+      output = fill(shape, constant(alpha_value, dtype=alpha_dtype), name=name)
+
+    assert output.dtype.base_dtype == alpha_dtype
+
+    return output
+    
 
 def ones(shape, dtype=dtypes.float32, name=None):
   """Creates a tensor with all elements set to 1.
