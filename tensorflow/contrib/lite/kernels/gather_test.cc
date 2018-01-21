94744a4dbab22a9ddf08d35cf2379505eb25e013
Shaurya Sharma <shaur141@gmail.com>
2018-01-21 12:52:10 +0100

    fix booboo from previous commit

--- tensorflow/contrib/lite/kernels/gather_test.cc
+++ tensorflow/contrib/lite/kernels/gather_test.cc
@@ -1,10 +1,10 @@
 /* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0f (the "License");
+Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0f
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
