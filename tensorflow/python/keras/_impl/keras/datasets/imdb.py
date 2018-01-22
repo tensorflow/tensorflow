f02efd91973a596469333f562146b6b3c0e5c83a
Anna R <annarev@google.com>
2018-01-22 14:59:19 -0800

    Adding tf_export decorators/calls to TensorFlow functions and constants.
    PiperOrigin-RevId: 182841644

--- tensorflow/python/keras/_impl/keras/datasets/imdb.py
+++ tensorflow/python/keras/_impl/keras/datasets/imdb.py
@@ -24,8 +24,10 @@ import numpy as np
 from six.moves import zip  # pylint: disable=redefined-builtin
 
 from tensorflow.python.keras._impl.keras.utils.data_utils import get_file
+from tensorflow.python.util.tf_export import tf_export
 
 
+@tf_export('keras.datasets.imdb.load_data')
 def load_data(path='imdb.npz',
               num_words=None,
               skip_top=0,
@@ -133,6 +135,7 @@ def load_data(path='imdb.npz',
   return (x_train, y_train), (x_test, y_test)
 
 
+@tf_export('keras.datasets.imdb.get_word_index')
 def get_word_index(path='imdb_word_index.json'):
   """Retrieves the dictionary mapping word indices back to words.
 
