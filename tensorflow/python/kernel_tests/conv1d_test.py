15dea86e4528ce46a47c1123acd1bbbc210b6cce
Yong Tang <yong.tang.github@outlook.com>
2018-01-22 18:23:01 +0000

    Update docstring with TODO.
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/conv1d_test.py
+++ tensorflow/python/kernel_tests/conv1d_test.py
@@ -31,7 +31,7 @@ class Conv1DTest(test.TestCase):
 
   def testBasic(self):
     """Test that argument passing to conv1d is handled properly."""
-    # TODO: dtypes.float64 can only be enabled once conv2d support
+    # TODO(yongtang): dtypes.float64 can only be enabled once conv2d support
     # dtypes.float64, as conv1d implicitly calls conv2d after expand_dims.
     for dtype in [dtypes.float16, dtypes.float32]:
       x = constant_op.constant([1, 2, 3, 4], dtype=dtype)
