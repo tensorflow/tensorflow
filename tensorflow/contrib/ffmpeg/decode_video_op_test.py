a5f54d9a72176804deacc512c3e9e03fb8361514
Rajendra arora <rajendraarora16@yahoo.com>
2018-01-23 20:14:07 +0530

    Removing os.path

--- tensorflow/contrib/ffmpeg/decode_video_op_test.py
+++ tensorflow/contrib/ffmpeg/decode_video_op_test.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os.path
+import os
 
 import six  # pylint: disable=unused-import
 
