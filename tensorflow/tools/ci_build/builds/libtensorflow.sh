d1020bfdedadc3da7c89a69651c45cc790922b69
Gunhan Gulsoy <gunan@google.com>
2018-01-23 11:09:26 -0800

    Run TF opensource presubmits with sanybridge CPU optimizations.
    PiperOrigin-RevId: 182961789

--- tensorflow/tools/ci_build/builds/libtensorflow.sh
+++ tensorflow/tools/ci_build/builds/libtensorflow.sh
@@ -51,8 +51,8 @@ function build_libtensorflow_tarball() {
   rm -rf ${DIR}
 
   TARBALL_SUFFIX="${1}"
-  BAZEL="bazel --bazelrc ./tensorflow/tools/ci_build/install/.bazelrc"
   BAZEL_OPTS="-c opt"
+  export CC_OPT_FLAGS='-mavx'
   if [ "${TF_NEED_CUDA}" == "1" ]; then
     BAZEL_OPTS="${BAZEL_OPTS} --config=cuda"
   fi
