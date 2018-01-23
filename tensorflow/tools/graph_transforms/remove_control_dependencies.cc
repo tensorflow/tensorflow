df7c1e534f53c9c9173d07947a85531f69efb081
Thomas Deegan <thomas.deegan@getcruise.com>
2018-01-22 21:31:47 -0800

    add remove control deps transform

---
+++ tensorflow/tools/graph_transforms/remove_control_dependencies.cc
@@ -0,0 +1,34 @@
+#include <tensorflow/core/common_runtime/constant_folding.h>
+#include <tensorflow/core/graph/graph_constructor.h>
+#include <tensorflow/core/graph/node_builder.h>
+#include <tensorflow/core/graph/subgraph.h>
+#include <tensorflow/core/platform/init_main.h>
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/core/util/command_line_flags.h>
+#include <tensorflow/tools/graph_transforms/transform_utils.h>
+
+namespace tensorflow {
+namespace graph_transforms {
+
+// Changes the op type of a specified op.
+Status RemoveControlDependencies(const GraphDef& input_graph_def,
+                const TransformFuncContext& context,
+                GraphDef* output_graph_def) {
+    output_graph_def->Clear();
+    for (const NodeDef& node : input_graph_def.node()) {
+        NodeDef* new_node = output_graph_def->mutable_node()->Add();
+        *new_node = node;
+        new_node->clear_input();
+        for (const auto& input : node.input()) {
+            if (input[0] != '^') {
+                new_node->add_input(input);
+            }
+        }
+    }
+    return Status::OK();
+}
+
+REGISTER_GRAPH_TRANSFORM("remove_control_dependencies", RemoveControlDependencies);
+
+}  // namespace graph_transforms
+}  // namespace tensorflow
