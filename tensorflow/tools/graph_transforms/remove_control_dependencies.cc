31b2e402aea0c4785bd90518cfe7512f2ea7c52a
Thomas Deegan <thomas.deegan@getcruise.com>
2018-01-23 12:28:00 -0800

    remove unnecessary includes

--- tensorflow/tools/graph_transforms/remove_control_dependencies.cc
+++ tensorflow/tools/graph_transforms/remove_control_dependencies.cc
@@ -1,10 +1,5 @@
-#include <tensorflow/core/common_runtime/constant_folding.h>
 #include <tensorflow/core/graph/graph_constructor.h>
 #include <tensorflow/core/graph/node_builder.h>
-#include <tensorflow/core/graph/subgraph.h>
-#include <tensorflow/core/platform/init_main.h>
-#include <tensorflow/core/public/session.h>
-#include <tensorflow/core/util/command_line_flags.h>
 #include <tensorflow/tools/graph_transforms/transform_utils.h>
 
 namespace tensorflow {
