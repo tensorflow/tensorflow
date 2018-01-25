49a61d82b6529e43dea1da395b0e2342bc49a62d
Rajendra Arora <rajendraarora161992@gmail.com>
2018-01-25 17:27:44 +0530

    Changing from single quotes to double.

--- tensorflow/contrib/factorization/python/ops/constants.py
+++ tensorflow/contrib/factorization/python/ops/constants.py
@@ -22,28 +22,28 @@ import numpy as np
 
 # Euclidean distance between vectors U and V is defined as ||U - V||_F which is
 # the square root of the sum of the absolute squares of the elements difference.
-SQUARED_EUCLIDEAN_DISTANCE = 'squared_euclidean'
+SQUARED_EUCLIDEAN_DISTANCE = "squared_euclidean"
 # Cosine distance between vectors U and V is defined as
 # 1 - (U \dot V) / (||U||_F ||V||_F)
-COSINE_DISTANCE = 'cosine'
+COSINE_DISTANCE = "cosine"
 
-RANDOM_INIT = 'random'
-KMEANS_PLUS_PLUS_INIT = 'kmeans_plus_plus'
-KMC2_INIT = 'kmc2'
+RANDOM_INIT = "random"
+KMEANS_PLUS_PLUS_INIT = "kmeans_plus_plus"
+KMC2_INIT = "kmc2"
 
 # The name of the variable holding the cluster centers. Used by the Estimator.
-CLUSTERS_VAR_NAME = 'clusters'
+CLUSTERS_VAR_NAME = "clusters"
 
-SCALAR_INIT = 'scalar'
-LIST_INIT = 'list'
-WORKER_INIT = 'worker_init'
+SCALAR_INIT = "scalar"
+LIST_INIT = "list"
+WORKER_INIT = "worker_init"
 
 # Machine epsilon.
 MEPS = np.finfo(float).eps
-FULL_COVARIANCE = 'full'
-DIAG_COVARIANCE = 'diag'
+FULL_COVARIANCE = "full"
+DIAG_COVARIANCE = "diag"
 
 #Tensorflow Gaussian mixture model clusters
-CLUSTERS_WEIGHT = 'alphas'
-CLUSTERS_VARIABLE = 'clusters'
-CLUSTERS_COVS_VARIABLE = 'clusters_covs'
+CLUSTERS_WEIGHT = "alphas"
+CLUSTERS_VARIABLE = "clusters"
+CLUSTERS_COVS_VARIABLE = "clusters_covs"
