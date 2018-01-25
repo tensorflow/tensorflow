10eab61afb2292b4b8ad82426b7650c77e483d7a
Jerome <jvgonzalvo@up.edu.ph>
2018-01-25 09:21:51 +0800

    Created dense_to_sparse in contrib.layers (#16119)
    * Added ctc_loss_dense_labels. This does the conversion of dense labels into sparse ones to be passed into the core ctc_loss function.
    
    * Removed constant_op from the import.
    
    * Matched ctc_loss_dense_labels with the other layers ops.
    
    * Added ctc_loss_dense_labels to contrib.layers __init__.py file
    
    * Added missing comma to list of ops.
    
    * Reordred arguments for ctc_loss_dense_labels
    
    Labels should be first then inputs for ctc_loss.
    
    * Removed ctc_loss_dense_labels.
    
    Replaced it with dense_to_sparse instead so that there'll be only one ctc_loss function.
    
    * Replaced ctc_loss_dense_labels with dense_to_sparse
    
    * Fixed dense_to_sparse. Some of the names of the variables did not match with that of the parameters.
    
    * Updated documentation for dense_to_sparse since it can accept a tensor of any shape.
    
    * Added test case for dense_to_sparse.
    
    * Updated documentation. Dense to sparse accepts int tensors.
    
    * Fixed testDenseFromConstantToSparse.
    
    The sparse_to_dense order of arguments in the test are wrong and the expected constant should be of int64.

--- tensorflow/contrib/layers/python/layers/layers.py
+++ tensorflow/contrib/layers/python/layers/layers.py
@@ -29,6 +29,7 @@ from tensorflow.contrib.framework.python.ops import variables
 from tensorflow.contrib.layers.python.layers import initializers
 from tensorflow.contrib.layers.python.layers import utils
 from tensorflow.python.eager import context
+from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import function
 from tensorflow.python.framework import ops
@@ -58,7 +59,8 @@ __all__ = [
     'avg_pool2d', 'avg_pool3d', 'batch_norm', 'bias_add', 'conv2d', 'conv3d',
     'conv2d_in_plane', 'conv2d_transpose', 'conv3d_transpose', 'convolution',
     'convolution2d', 'convolution2d_in_plane', 'convolution2d_transpose',
-    'convolution3d', 'convolution3d_transpose', 'dropout', 'elu', 'flatten',
+    'convolution3d', 'convolution3d_transpose', 'dense_to_sparse',
+           'dropout', 'elu', 'flatten',
     'fully_connected', 'GDN', 'gdn', 'layer_norm', 'linear', 'pool',
     'max_pool2d', 'max_pool3d', 'one_hot_encoding', 'relu', 'relu6', 'repeat',
     'scale_gradient', 'separable_conv2d', 'separable_convolution2d', 'softmax',
@@ -1397,6 +1399,29 @@ def convolution3d_transpose(
 
     if activation_fn is not None:
       outputs = activation_fn(outputs)
+    return utils.collect_named_outputs(outputs_collections, sc.name, outputs)
+
+
+@add_arg_scope
+def dense_to_sparse(tensor, eos_token=0, outputs_collections=None, scope=None):
+  """Converts a dense tensor into a sparse tensor.
+  An example use would be to convert dense labels to sparse ones
+  so that they can be fed to the ctc_loss.
+
+  Args:
+     tensor: An `int` `Tensor` to be converted to a `Sparse`.
+     eos_token: An integer.
+       It is part of the target label that signfies the end of a sentence.
+     outputs_collections: Collection to add the outputs.
+     scope: Optional scope for name_scope.
+  """
+  with variable_scope.variable_scope(
+      scope, 'dense_to_sparse', [tensor]) as sc:
+    tensor = ops.convert_to_tensor(tensor)
+    indices = array_ops.where(math_ops.not_equal(tensor, constant_op.constant(eos_token, tensor.dtype)))
+    values = array_ops.gather_nd(tensor, indices)
+    shape = array_ops.shape(tensor, out_type=dtypes.int64)
+    outputs = sparse_tensor.SparseTensor(indices, values, shape)
     return utils.collect_named_outputs(outputs_collections, sc.name, outputs)
 
 
