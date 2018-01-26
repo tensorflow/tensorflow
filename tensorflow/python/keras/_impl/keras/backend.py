94ac09b1f974aee7074fc26e0ad1fbf6496beb52
Kern Handa <kernhanda@users.noreply.github.com>
2018-01-26 13:25:29 -0800

    Fixing hard_sigmoid's documentation to match impl

--- tensorflow/python/keras/_impl/keras/backend.py
+++ tensorflow/python/keras/_impl/keras/backend.py
@@ -3267,8 +3267,8 @@ def hard_sigmoid(x):
   """Segment-wise linear approximation of sigmoid.
 
   Faster than sigmoid.
-  Returns `0.` if `x < -2.5`, `1.` if `x > 2.5`.
-  In `-2.5 <= x <= 2.5`, returns `0.2 * x + 0.5`.
+  Returns `0.` if `x < 0`, `1.` if `x > 1`.
+  In `0 <= x <= 1`, returns `0.2 * x + 0.5`.
 
   Arguments:
       x: A tensor or variable.
