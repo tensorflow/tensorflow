ac2631e97f5645530df42521f722fea80677eb5b
jackyko <jackkykokoko@gmail.com>
2018-01-31 14:39:49 +0800

    remove perl, go, asm requirement

--- tensorflow/contrib/cmake/external/grpc.cmake
+++ tensorflow/contrib/cmake/external/grpc.cmake
@@ -14,46 +14,6 @@
 # ==============================================================================
 include (ExternalProject)
 
-# # perl
-# find_package(Perl REQUIRED)
-
-# # nasm
-# if(NOT CMAKE_ASM_NASM_COMPILER)
-#   if (WIN32)  
-#     find_program(CMAKE_ASM_NASM_COMPILER 
-#         NAMES nasm
-#         PATHS "$ENV{ProgramFiles}/NASM" 
-#         DOC "path to NASM compiler")
-#   else()
-#     find_program(CMAKE_ASM_NASM_COMPILER 
-#       NAMES nasm
-#       PATHS "/usr/bin" 
-#       DOC "path to NASM compiler")
-#   endif()
-# endif()
-
-# if(NOT CMAKE_ASM_NASM_COMPILER)
-# 	message(FATAL_ERROR "NASM not found!")
-# endif()
-
-# # go
-# if(NOT GO_EXECUTABLE)
-#   if (WIN32)  
-#     find_program(GO_EXECUTABLE 
-#         NAMES go
-#         PATHS "$ENV{ProgramFiles}/Go/bin"
-#         DOC "path to go compiler")
-#   else()
-#     find_program(GO_EXECUTABLE 
-#         NAMES go
-#         PATHS "/usr/bin" "/usr/local/bin" "/usr/local/go"
-#         DOC "path to go compiler")
-#   endif()
-# endif()
-# if(NOT GO_EXECUTABLE)
-#   message(FATAL_ERROR "Go not found!")
-# endif()
-
 set(GRPC_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc/include)
 set(GRPC_URL https://github.com/grpc/grpc.git)
 set(GRPC_BUILD ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc)
