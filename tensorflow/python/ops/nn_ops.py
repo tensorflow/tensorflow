11f67fef8b22226f7dbdfa9632b27c5600fb3420
jerome <jvgonzalvo@up.edu.ph>
2018-01-31 11:29:23 +0800

    Refactored nn_ops.py
    Extracted duplicate code into a method.

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -2147,10 +2147,14 @@ def _calc_conv_flops(graph, node):
 @ops.RegisterStatistics("DepthwiseConv2dNative", "flops")
 def _calc_depthwise_conv_flops(graph, node):
   """Calculates the compute resources needed for DepthwiseConv2dNative."""
+  return _initialize_flops(graph, node)
+
+
+def _initialize_flops(graph, node):
   input_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
   input_shape.assert_is_fully_defined()
   filter_shape = graph_util.tensor_shape_from_node_def_name(
-      graph, node.input[1])
+    graph, node.input[1])
   filter_shape.assert_is_fully_defined()
   output_shape = graph_util.tensor_shape_from_node_def_name(graph, node.name)
   output_shape.assert_is_fully_defined()
@@ -2522,17 +2526,7 @@ def conv1d_transpose(
 @ops.RegisterStatistics("Dilation2D", "flops")
 def _calc_dilation2d_flops(graph, node):
   """Calculates the compute resources needed for Dilation2D."""
-  input_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
-  input_shape.assert_is_fully_defined()
-  filter_shape = graph_util.tensor_shape_from_node_def_name(
-      graph, node.input[1])
-  filter_shape.assert_is_fully_defined()
-  output_shape = graph_util.tensor_shape_from_node_def_name(graph, node.name)
-  output_shape.assert_is_fully_defined()
-  filter_height = int(filter_shape[0])
-  filter_width = int(filter_shape[1])
-  output_count = np.prod(output_shape.as_list())
-  return ops.OpStats("flops", (output_count * filter_height * filter_width * 2))
+  return _initialize_flops(graph, node)
 
 
 @tf_export("nn.erosion2d")
