07ec52c0ee57f10cd0e261c498a536877e439799
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-02-09 00:59:08 +0800

    Fix static shape inference for keras.layers.LSTM (#15234)
    * TST: add test case
    
    * BUG: fix static shape inference
    
    * TST: clean code
    
    * BUG: support dims > 3

--- tensorflow/python/keras/_impl/keras/backend.py
+++ tensorflow/python/keras/_impl/keras/backend.py
@@ -2781,6 +2781,7 @@ def rnn(step_function,
   ndim = len(inputs.get_shape())
   if ndim < 3:
     raise ValueError('Input should be at least 3D.')
+  inputs_shape = inputs.get_shape()
   axes = [1, 0] + list(range(2, ndim))
   inputs = array_ops.transpose(inputs, (axes))
 
@@ -2965,6 +2966,13 @@ def rnn(step_function,
 
   axes = [1, 0] + list(range(2, len(outputs.get_shape())))
   outputs = array_ops.transpose(outputs, axes)
+
+  # Static shape inference: (samples, time, ...)
+  outputs_shape = outputs.get_shape().as_list()
+  outputs_shape[0] = inputs_shape[0]
+  outputs_shape[1] = inputs_shape[1]
+  outputs.set_shape(outputs_shape)
+
   last_output._uses_learning_phase = uses_learning_phase
   return last_output, outputs, new_states
 
