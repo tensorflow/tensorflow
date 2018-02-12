be903d84071c0162fab8b14e9ebf25d41d78111b
Yifei Feng <fengyifei2026@gmail.com>
2018-02-12 11:09:12 -0800

    TESTING DO NOT MERGE

--- configure.py
+++ configure.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# =============================================================================
 """configure script to get build parameters from user."""
 
 from __future__ import absolute_import
