71878e136473e4ea2d85593171fcf221d3bced2a
Thomas Deegan <tadeegan@gmail.com>
2018-02-15 16:38:46 -0800

    Update remove_control_dependencies.cc

--- tensorflow/tools/graph_transforms/remove_control_dependencies.cc
+++ tensorflow/tools/graph_transforms/remove_control_dependencies.cc
@@ -12,9 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <tensorflow/core/graph/graph_constructor.h>
-#include <tensorflow/core/graph/node_builder.h>
-#include <tensorflow/tools/graph_transforms/transform_utils.h>
+#include "tensorflow/core/graph/graph_constructor.h"
+#include "tensorflow/core/graph/node_builder.h"
+#include "tensorflow/tools/graph_transforms/transform_utils.h"
 
 namespace tensorflow {
 namespace graph_transforms {
