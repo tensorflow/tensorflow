Fix a bug in tf.multiply documentation In this commit, a bug that caused "NOTE: `tf.multiplytiply` supports broadcasting." to appear on tf.multiply documentation page was fixed. See https://www.tensorflow.org/api_docs/python/tf/multiply

tensorflow/python/ops/math_ops.py
@@ -331,7 +331,7 @@ def multiply(x, y, name=None):
   return gen_math_ops._mul(x, y, name)
 
 
-multiply.__doc__ = gen_math_ops._mul.__doc__.replace("Mul", "`tf.multiply`")
+multiply.__doc__ = gen_math_ops._mul.__doc__.replace("`Mul`", "`tf.multiply`")
 
 
 # TODO(aselle): put deprecation in after another round of global code changes