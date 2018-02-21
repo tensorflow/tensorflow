37832aeec330a3ad60635c1b1c8196b722349b3c
Bo Wang <david.b.wang@gmail.com>
2018-02-20 16:05:17 -0800

    Fix various format issues

--- tensorflow/contrib/bilinear_initializer/bilinear_initializer_op.py
+++ tensorflow/contrib/bilinear_initializer/bilinear_initializer_op.py
@@ -1,11 +1,25 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+"""Initialize with bilinear interpolation."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-
 import tensorflow as tf
-from tensorflow.python.framework import common_shapes
 from tensorflow.python.framework import errors
 from tensorflow.python.framework import load_library
 from tensorflow.python.framework import ops
@@ -14,13 +28,13 @@ from tensorflow.python.platform import tf_logging as logging
 
 library = None
 
+
 def bilinear_initializer(shape, dtype=tf.float32, partition_info=None):
-  """
-  Bilinear Initializer Operation
-  
+  """Bilinear Initializer Operation.
+
   Computes batch filters with bilinear weights. Result is a
   4D tensor of dimension [H, W, C, N] where:
-  
+
   (1) H = height of kernel
   (2) W = width of kernel
   (3) C = number of input channels
@@ -29,11 +43,13 @@ def bilinear_initializer(shape, dtype=tf.float32, partition_info=None):
   """
   return library.bilinear_initializer(shape)
 
+
 ops.NotDifferentiable('BilinearInitializer')
 
 
 def _load_library(name, op_list=None):
   """Loads a .so file containing the specified operators.
+
   Args:
     name: The name of the .so file to load.
     op_list: A list of names of operators that the library should have. If None
@@ -50,7 +66,7 @@ def _load_library(name, op_list=None):
           break
         else:
           raise NameError('Could not find operator %s in dynamic library %s' %
-                  (expected_op, name))
+                          (expected_op, name))
     return library
   except errors.NotFoundError:
     logging.warning('%s file could not be loaded.', name)
--- tensorflow/contrib/bilinear_initializer/bilinear_initializer_test.py
+++ tensorflow/contrib/bilinear_initializer/bilinear_initializer_test.py
@@ -1,55 +1,81 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+"""Tests for third_party.tensorflow.contrib.bilinear_initializer_op."""
+
 import numpy as np
 import skimage.transform as sktr
 import tensorflow as tf
-from tensorflow.contrib.bilinear_initializer.bilinear_initializer_op import bilinear_initializer
+from tensorflow.contrib.bilinear_initializer.bilinear_initializer_op import \
+  bilinear_initializer
+
 
 class BilinearInitializerTest(tf.test.TestCase):
+
   def testBilinearInitializer(self):
     with self.test_session() as sess:
-      # Define parameters
+      # Define parameters.
       height, width, channel = 3, 3, 3
       factor = 2
       kernel_width = 2 * factor - factor % 2
       num_output = 3
 
-      # Generate input image
-      img = self.generate_imput_img(height, width, channel)
+      # Generate input image.
+      img = self.generate_input_img(height, width, channel)
 
-      # Generate benchmark image (ski)
+      # Generate benchmark image (ski).
       img_benchmark = self.ski_upsample(factor, img)
 
-      # Generate test image (tensorflow)
+      # Generate test image (tensorflow).
       with tf.Session() as sess:
         dims = [kernel_width, kernel_width, channel, num_output]
         tf_filter = bilinear_initializer(dims).eval()
         new_h = factor * height
         new_w = factor * width
-        img_test = self.tf_upsample(sess, tf_filter, new_h, new_w, channel, factor, img)
+        img_test = self.tf_upsample(sess,
+                                    tf_filter,
+                                    new_h,
+                                    new_w,
+                                    channel,
+                                    factor,
+                                    img)
         self.assertTrue(np.allclose(img_benchmark, img_test))
 
-  def generate_imput_img(self, height, width, channel):
-    """Generate input image with a given height, width, and number of channels
+  def generate_input_img(self, height, width, channel):
+    """Generate input image with a given height, width, and number of channels.
     """
     x, y = np.ogrid[:height, :width]
-    return np.repeat((x + y)[..., np.newaxis], channel, 2) / float(height * width)
+    img = np.repeat((x + y)[..., np.newaxis], channel, 2)
+    return img / float(height * width)
 
   def ski_upsample(self, factor, input_img):
-    """Benchmark for testing. Use skikit learn library
-    order = 1 means bilinear initializer
+    """Benchmark for testing. Use skikit learn library.
+       order = 1 means bilinear initializer
     """
-    return sktr.rescale(input_img, factor, 
-      mode='constant', cval=0, order=1)
+    return sktr.rescale(input_img, factor, mode='constant', cval=0, order=1)
 
-  def tf_upsample(self, sess, tf_filter, new_h, new_w, channel, factor, input_img):
+  def tf_upsample(self, sess, tf_filter, new_h, new_w, channel, factor,
+                  input_img):
     expanded_img = np.expand_dims(input_img, axis=0)
     filt = tf.placeholder(tf.float32)
     img_in = tf.placeholder(tf.float32)
-    res = tf.nn.conv2d_transpose(img_in, filt,
-      output_shape=[1, new_h, new_w, channel],
-      strides=[1, factor, factor, 1])
+    res = tf.nn.conv2d_transpose(img_in,
+                                 filt,
+                                 output_shape=[1, new_h, new_w, channel],
+                                 strides=[1, factor, factor, 1])
     final_result = sess.run(res,
-            feed_dict={filt: tf_filter,
-                   img_in: expanded_img})
+                            feed_dict={filt: tf_filter, img_in: expanded_img})
     return final_result.squeeze()
 
 
