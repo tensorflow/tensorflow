26ecbe6c060ead22373e6c069f3c14433423b3a6
Yifei Feng <yifeif@google.com>
2018-02-23 11:03:14 -0800

    Remove repeated defination due to auto-merge.

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -2214,30 +2214,6 @@ def xw_plus_b_v1(x, weights, biases, name=None):  # pylint: disable=invalid-name
     mm = math_ops.matmul(x, weights)
     return bias_add_v1(mm, biases, name=name)
 
-def _get_noise_shape(x, noise_shape):
-  # If noise_shape is none return immediately.
-  if noise_shape is None:
-    return array_ops.shape(x)
-
-  try:
-    # Best effort to figure out the intended shape.
-    # If not possible, let the op to handle it.
-    # In eager mode exception will show up.
-    noise_shape_ = tensor_shape.as_shape(noise_shape)
-  except (TypeError, ValueError):
-    return noise_shape
-
-  if (x.shape.dims is not None and
-      len(x.shape.dims) == len(noise_shape_.dims)):
-    new_dims = []
-    for i, dim in enumerate(x.shape.dims):
-      if noise_shape_.dims[i].value is None and dim.value is not None:
-        new_dims.append(dim.value)
-      else:
-        new_dims.append(noise_shape_.dims[i].value)
-    return tensor_shape.TensorShape(new_dims)
-
-  return noise_shape
 
 def _get_noise_shape(x, noise_shape):
   # If noise_shape is none return immediately.
