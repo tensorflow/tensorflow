44bec5d15f656d054df5c61e3eb70d5fbe8bb77a
A. Unique TensorFlower <gardener@tensorflow.org>
2018-02-23 17:22:37 -0800

    Add another utility that captures a function's namespace as a mapping from symbol names to actual values. Update getmethodclass with a hopefully more robust method.
    PiperOrigin-RevId: 186847003

--- tensorflow/contrib/py2tf/pyct/inspect_utils_test.py
+++ tensorflow/contrib/py2tf/pyct/inspect_utils_test.py
@@ -20,6 +20,8 @@ from __future__ import print_function
 
 from functools import wraps
 
+import six
+
 from tensorflow.contrib.py2tf.pyct import inspect_utils
 from tensorflow.python.platform import test
 
@@ -76,6 +78,10 @@ def free_function():
   pass
 
 
+def factory():
+  return free_function
+
+
 def free_factory():
   def local_function():
     pass
@@ -83,6 +89,43 @@ def free_factory():
 
 
 class InspectUtilsTest(test.TestCase):
+
+  def test_getnamespace_globals(self):
+    ns = inspect_utils.getnamespace(factory)
+    self.assertEqual(ns['free_function'], free_function)
+
+  def test_getnamespace_hermetic(self):
+
+    # Intentionally hiding the global function to make sure we don't overwrite
+    # it in the global namespace.
+    free_function = object()  # pylint:disable=redefined-outer-name
+
+    def test_fn():
+      return free_function
+
+    ns = inspect_utils.getnamespace(test_fn)
+    globs = six.get_function_globals(test_fn)
+    self.assertTrue(ns['free_function'] is free_function)
+    self.assertFalse(globs['free_function'] is free_function)
+
+  def test_getnamespace_locals(self):
+
+    def called_fn():
+      return 0
+
+    closed_over_list = []
+    closed_over_primitive = 1
+
+    def local_fn():
+      closed_over_list.append(1)
+      local_var = 1
+      return called_fn() + local_var + closed_over_primitive
+
+    ns = inspect_utils.getnamespace(local_fn)
+    self.assertEqual(ns['called_fn'], called_fn)
+    self.assertEqual(ns['closed_over_list'], closed_over_list)
+    self.assertEqual(ns['closed_over_primitive'], closed_over_primitive)
+    self.assertTrue('local_var' not in ns)
 
   def test_getcallargs_constructor(self):
 
@@ -123,48 +166,47 @@ class InspectUtilsTest(test.TestCase):
   def test_getmethodclass(self):
 
     self.assertEqual(
-        inspect_utils.getmethodclass(free_function, {}), None)
+        inspect_utils.getmethodclass(free_function), None)
     self.assertEqual(
-        inspect_utils.getmethodclass(free_factory(), {}), None)
+        inspect_utils.getmethodclass(free_factory()), None)
 
-    ns = {'TestClass': TestClass}
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.member_function, ns),
+        inspect_utils.getmethodclass(TestClass.member_function),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.decorated_member, ns),
+        inspect_utils.getmethodclass(TestClass.decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.fn_decorated_member, ns),
+        inspect_utils.getmethodclass(TestClass.fn_decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.wrap_decorated_member, ns),
+        inspect_utils.getmethodclass(TestClass.wrap_decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.static_method, ns),
+        inspect_utils.getmethodclass(TestClass.static_method),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(TestClass.class_method, ns),
+        inspect_utils.getmethodclass(TestClass.class_method),
         TestClass)
 
     test_obj = TestClass()
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.member_function, ns),
+        inspect_utils.getmethodclass(test_obj.member_function),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.fn_decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.fn_decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.wrap_decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.wrap_decorated_member),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.static_method, ns),
+        inspect_utils.getmethodclass(test_obj.static_method),
         TestClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.class_method, ns),
+        inspect_utils.getmethodclass(test_obj.class_method),
         TestClass)
 
   def test_getmethodclass_locals(self):
@@ -190,34 +232,33 @@ class InspectUtilsTest(test.TestCase):
         pass
 
     self.assertEqual(
-        inspect_utils.getmethodclass(local_function, {}), None)
+        inspect_utils.getmethodclass(local_function), None)
 
-    ns = {'LocalClass': LocalClass}
     self.assertEqual(
-        inspect_utils.getmethodclass(LocalClass.member_function, ns),
+        inspect_utils.getmethodclass(LocalClass.member_function),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(LocalClass.decorated_member, ns),
+        inspect_utils.getmethodclass(LocalClass.decorated_member),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(LocalClass.fn_decorated_member, ns),
+        inspect_utils.getmethodclass(LocalClass.fn_decorated_member),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(LocalClass.wrap_decorated_member, ns),
+        inspect_utils.getmethodclass(LocalClass.wrap_decorated_member),
         LocalClass)
 
     test_obj = LocalClass()
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.member_function, ns),
+        inspect_utils.getmethodclass(test_obj.member_function),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.decorated_member),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.fn_decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.fn_decorated_member),
         LocalClass)
     self.assertEqual(
-        inspect_utils.getmethodclass(test_obj.wrap_decorated_member, ns),
+        inspect_utils.getmethodclass(test_obj.wrap_decorated_member),
         LocalClass)
 
 
