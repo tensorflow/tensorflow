908ef1ed81d18a7bb90bf78bf4391700cf103c6b
DavidNorman <davidn@graphcore.ai>
2018-02-27 21:51:38 +0000

    Ensure that only the compute sets which are expected are present

--- tensorflow/compiler/plugin/poplar/tests/conv_test.py
+++ tensorflow/compiler/plugin/poplar/tests/conv_test.py
@@ -10,107 +10,159 @@ import tensorflow as tf
 from tensorflow.python.platform import googletest
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import nn_ops
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.compiler.plugin.poplar.ops import gen_ipu_ops
 
 import numpy as np
+import contextlib
+import re
+
+@contextlib.contextmanager
+def ipu_session():
+  opts = config_pb2.IPUOptions()
+  dev = opts.device_config.add()
+  dev.type = config_pb2.IPUOptions.DeviceConfig.IPU_MODEL
+  dev.enable_profile = True
+  with tf.Session(config=tf.ConfigProto(ipu_options=opts)) as sess:
+    yield sess
+
+def get_compute_sets_from_report(report):
+  lines = report.split('\n')
+  return [x for x in lines if re.search('(\d+ execution.?)', x)]
+
+def check_all_compute_sets_in_list(cs_list, whitelist):
+  for cs in cs_list:
+    if len([x for x in whitelist if cs.startswith(x)]) == 0:
+      return False
+  return True
 
 class IpuXlaConvTest(test_util.TensorFlowTestCase):
 
   def testConv1x1_Stride2x1_In1x5(self):
     with tf.device("/device:IPU:0"):
-      with tf.Session() as sess:
-        pa = tf.placeholder(tf.float32, [1,1,5,1], name="a")
-        pb = tf.placeholder(tf.float32, [1,1,1,1], name="b")
-        output = nn_ops.convolution(pa, pb, strides=[1,2], padding="VALID")
+      pa = tf.placeholder(tf.float32, [1,1,5,1], name="a")
+      pb = tf.placeholder(tf.float32, [1,1,1,1], name="b")
+      output = nn_ops.convolution(pa, pb, strides=[1,2], padding="VALID")
 
-        fd = {
-          pa: [[[[1], [2], [3], [4], [5]]]],
-          pb: [[[[10]]]]
-        }
-        result = sess.run(output, fd)
-        self.assertAllClose(result,
-                            [[[[10], [30], [50]]]])
+    with tf.Session() as sess:
+      fd = {
+        pa: [[[[1], [2], [3], [4], [5]]]],
+        pb: [[[[10]]]]
+      }
+      result = sess.run(output, fd)
+      self.assertAllClose(result, [[[[10], [30], [50]]]])
 
   def testConv3x3_Pad1x1(self):
-      with tf.device("/device:IPU:0"):
-          with tf.Session() as sess:
-              pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
-              pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
-              output = nn_ops.convolution(pa, pb, padding="SAME")
+    with tf.device("/device:IPU:0"):
+      pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
+      pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
+      output = nn_ops.convolution(pa, pb, padding="SAME")
 
-              fd = {
-                  pa: np.zeros([1,14,14,64]),
-                  pb: np.zeros([3,3,64,128])
-              }
-              result = sess.run(output, fd)
-              self.assertAllClose(result,
-                                  np.zeros([1,14,14,128]))
+    with tf.Session() as sess:
+      fd = {
+        pa: np.zeros([1,14,14,64]),
+        pb: np.zeros([3,3,64,128])
+      }
+      result = sess.run(output, fd)
+      self.assertAllClose(result, np.zeros([1,14,14,128]))
 
   def testConv3x3_WithBias(self):
-      with tf.device("/device:IPU:0"):
-          with tf.Session() as sess:
-              pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
-              pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
-              bi = tf.placeholder(tf.float32, [128], name="b")
-              output = nn_ops.convolution(pa, pb, padding="SAME")
-              output = nn_ops.bias_add(output, bi)
+    with tf.device("/device:IPU:0"):
+      pa = tf.placeholder(tf.float32, [1,14,14,64], name="a")
+      pb = tf.placeholder(tf.float32, [3,3,64,128], name="b")
+      bi = tf.placeholder(tf.float32, [128], name="b")
+      output = nn_ops.convolution(pa, pb, padding="SAME")
+      output = nn_ops.bias_add(output, bi)
 
-              fd = {
-                  pa: np.zeros([1,14,14,64]),
-                  pb: np.zeros([3,3,64,128]),
-                  bi: np.zeros([128]),
-              }
-              result = sess.run(output, fd)
-              self.assertAllClose(result,
-                                  np.zeros([1,14,14,128]))
+    with tf.device('cpu'):
+      report = gen_ipu_ops.ipu_summary()
+
+    with ipu_session() as sess:
+      fd = {
+        pa: np.zeros([1,14,14,64]),
+        pb: np.zeros([3,3,64,128]),
+        bi: np.zeros([128]),
+      }
+      result = sess.run(output, fd)
+      self.assertAllClose(result, np.zeros([1,14,14,128]))
+
+      result = sess.run(report)
+      self.assertTrue(len(result) == 1)
+      cs_list = get_compute_sets_from_report(result[0])
+
+      ok = ['convolution.clone/Conv_3x3',
+            'Copy_{<const>,arg0_input}',
+            'call/addToChannel']
+      self.assertTrue(check_all_compute_sets_in_list(cs_list, ok))
 
   def testConv8x8_WithBias(self):
     with tf.device("/device:IPU:0"):
-      with tf.Session() as sess:
-        inp = tf.placeholder(tf.float32, [1,84,84,4], name="inp")
-        wei = tf.placeholder(tf.float32, [8,8,4,16], name="wei")
-        bia = tf.placeholder(tf.float32, [16], name="bia")
-        output = nn_ops.conv2d(inp, wei, strides=[1,4,4,1], padding="VALID")
-        output = nn_ops.bias_add(output, bia)
+      inp = tf.placeholder(tf.float32, [1,84,84,4], name="inp")
+      wei = tf.placeholder(tf.float32, [8,8,4,16], name="wei")
+      bia = tf.placeholder(tf.float32, [16], name="bia")
+      output = nn_ops.conv2d(inp, wei, strides=[1,4,4,1], padding="VALID")
+      output = nn_ops.bias_add(output, bia)
 
-        fd = {
-          inp: np.zeros([1,84,84,4]),
-          wei: np.zeros([8,8,4,16]),
-          bia: np.zeros([16]),
-        }
-        result = sess.run(output, fd)
-        self.assertAllClose(result,
-                            np.zeros([1, 20, 20, 16]))
+    with tf.device('cpu'):
+      report = gen_ipu_ops.ipu_summary()
+
+    with ipu_session() as sess:
+      fd = {
+        inp: np.zeros([1,84,84,4]),
+        wei: np.zeros([8,8,4,16]),
+        bia: np.zeros([16]),
+      }
+      result = sess.run(output, fd)
+      self.assertAllClose(result, np.zeros([1, 20, 20, 16]))
+
+      result = sess.run(report)
+      self.assertTrue(len(result) == 1)
+      cs_list = get_compute_sets_from_report(result[0])
+
+      ok = ['convolution.clone/Conv_8x8_stride4x4',
+            'call/addToChannel']
+      self.assertTrue(check_all_compute_sets_in_list(cs_list, ok))
 
 
   def testDepthwiseConv3x2(self):
-      with tf.device("/device:IPU:0"):
-          with tf.Session() as sess:
-              pa = tf.placeholder(tf.float32, [1,2,2,3], name="a")
-              pb = tf.placeholder(tf.float32, [1,1,3,2], name="b")
-              pc = tf.placeholder(tf.float32, [1,2,2,6], name="c")
-              c = tf.nn.depthwise_conv2d(pa, pb,
-                                        strides=[1,1,1,1],
-                                        padding="SAME")
-              output = c + pc
+    with tf.device("/device:IPU:0"):
+      pa = tf.placeholder(tf.float32, [1,2,2,3], name="a")
+      pb = tf.placeholder(tf.float32, [1,1,3,2], name="b")
+      pc = tf.placeholder(tf.float32, [1,2,2,6], name="c")
+      c = tf.nn.depthwise_conv2d(pa, pb, strides=[1,1,1,1], padding="SAME")
+      output = c + pc
 
-              fd = {
-                  pa: [[[[1,2,3],
-                         [4,5,6]],
-                        [[7,8,9],
-                         [10,11,12]]]],
-                  pb: [[[[6,5],
-                         [4,3],
-                         [2,1]]]],
-                  pc: [[[[1,1,1,1,1,1],
-                         [1,1,1,1,1,1]],
-                        [[1,1,1,1,1,1],
-                         [1,1,1,1,1,1]]]]
-              }
-              result = sess.run(output, fd)
-              self.assertAllClose(result, [[[[7, 6, 9, 7, 7, 4],
-                                             [25, 21, 21, 16, 13, 7]],
-                                            [[43, 36, 33, 25, 19, 10],
-                                             [61, 51, 45, 34, 25, 13]]]])
+    with tf.device('cpu'):
+      report = gen_ipu_ops.ipu_summary()
+
+    with ipu_session() as sess:
+      fd = {
+        pa: [[[[1,2,3],
+               [4,5,6]],
+              [[7,8,9],
+               [10,11,12]]]],
+        pb: [[[[6,5],
+               [4,3],
+               [2,1]]]],
+        pc: [[[[1,1,1,1,1,1],
+               [1,1,1,1,1,1]],
+              [[1,1,1,1,1,1],
+               [1,1,1,1,1,1]]]]
+      }
+      result = sess.run(output, fd)
+      self.assertAllClose(result, [[[[7, 6, 9, 7, 7, 4],
+                                     [25, 21, 21, 16, 13, 7]],
+                                    [[43, 36, 33, 25, 19, 10],
+                                     [61, 51, 45, 34, 25, 13]]]])
+
+      result = sess.run(report)
+      self.assertTrue(len(result) == 1)
+      cs_list = get_compute_sets_from_report(result[0])
+
+      ok = ['call.1.clone/Conv_1x1',
+            'Copy_partials_to_call',
+            'call/addToChannel']
+      self.assertTrue(check_all_compute_sets_in_list(cs_list, ok))
 
 
 
