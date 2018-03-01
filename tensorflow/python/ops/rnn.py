91c3d94743674f8a26ca73022f13fa11c0943f38
DavidNorman <davidn@graphcore.ai>
2018-03-01 14:07:13 +0000

    Remove apparently unnecessary dynamic_rnn change

--- tensorflow/python/ops/rnn.py
+++ tensorflow/python/ops/rnn.py
@@ -833,8 +833,7 @@ def _dynamic_rnn_loop(cell,
 
   # Unpack final output if not using output tuples.
   if in_graph_mode:
-    final_outputs = tuple(
-      ta.gather(range(const_batch_size)) for ta in output_final_ta)
+    final_outputs = tuple(ta.stack() for ta in output_final_ta)
     # Restore some shape information
     for output, output_size in zip(final_outputs, flat_output_size):
       shape = _concat(
