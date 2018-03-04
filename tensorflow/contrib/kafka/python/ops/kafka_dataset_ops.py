45583d9d70d374d728b945c36ee727b6b4622a64
Yong Tang <yong.tang.github@outlook.com>
2018-03-04 22:18:01 +0000

    Change `import readers.Dataset` to `import dataset_ops.Dataset`,
    due to the changes in some other places.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/contrib/kafka/python/ops/kafka_dataset_ops.py
+++ tensorflow/contrib/kafka/python/ops/kafka_dataset_ops.py
@@ -18,7 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.contrib.kafka.python.ops import gen_kafka_ops
-from tensorflow.python.data.ops.readers import Dataset
+from tensorflow.python.data.ops.dataset_ops import Dataset
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
