00bbe6aaa84089ade597b3807f692923f8865a16
Anjali Sridhar <anjalisridhar@google.com>
2018-03-06 10:35:56 -0800

    Add mask keyword to ensure that we don't pass masks in place of training.
    PiperOrigin-RevId: 188043473

--- tensorflow/python/keras/_impl/keras/engine/network.py
+++ tensorflow/python/keras/_impl/keras/engine/network.py
@@ -396,7 +396,7 @@ class Network(base_layer.Layer):
     if cache_key in self._output_mask_cache:
       return self._output_mask_cache[cache_key]
     else:
-      _, output_masks = self._run_internal_graph(inputs, masks)
+      _, output_masks = self._run_internal_graph(inputs, mask=masks)
       return output_masks
 
   @property
