381f000714b3ecd4be79f057c7a230d5152fa3dd
Skye Wanderman-Milne <skyewm@google.com>
2018-03-09 14:00:23 -0800

    Add smart_case that calls smart_cond.
    PiperOrigin-RevId: 188534066

--- tensorflow/python/framework/smart_cond.py
+++ tensorflow/python/framework/smart_cond.py
@@ -92,3 +92,32 @@ def smart_constant_value(pred):
     raise TypeError("`pred` must be a Tensor, or a Python bool, or 1 or 0. "
                     "Found instead: %s" % pred)
   return pred_value
+
+
+def smart_case(pred_fn_pairs, default=None, exclusive=False, name="smart_case"):
+  """Like tf.case, except attempts to statically evaluate predicates.
+
+  If any predicate in `pred_fn_pairs` is a bool or has a constant value, the
+  associated callable will be called or omitted depending on its value.
+  Otherwise this functions like tf.case.
+
+  Args:
+    pred_fn_pairs: Dict or list of pairs of a boolean scalar tensor and a
+                   callable which returns a list of tensors.
+    default: Optional callable that returns a list of tensors.
+    exclusive: True iff at most one predicate is allowed to evaluate to `True`.
+    name: A name for this operation (optional).
+
+  Returns:
+    The tensors returned by the first pair whose predicate evaluated to True, or
+    those returned by `default` if none does.
+
+  Raises:
+    TypeError: If `pred_fn_pairs` is not a list/dictionary.
+    TypeError: If `pred_fn_pairs` is a list but does not contain 2-tuples.
+    TypeError: If `fns[i]` is not callable for any i, or `default` is not
+               callable.
+  """
+  return control_flow_ops._case_helper(  # pylint: disable=protected-access
+      smart_cond, pred_fn_pairs, default, exclusive, name,
+      allow_python_preds=True)
--- tensorflow/python/framework/smart_cond_test.py
+++ tensorflow/python/framework/smart_cond_test.py
@@ -28,6 +28,10 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import googletest
 
 
+def raise_exception():
+  raise RuntimeError("did not expect to be called")
+
+
 @test_util.with_c_api
 class SmartCondTest(test_util.TensorFlowTestCase):
 
@@ -68,8 +72,6 @@ class SmartCondTest(test_util.TensorFlowTestCase):
         y = constant_op.constant(2)
         # x * y > 0 can be evaluated at graph construction time, so the false
         # branch shouldn't be evaluated at all.
-        def raise_exception():
-          raise RuntimeError("did not expect to be called")
         z = smart_cond.smart_cond(x * y > 0, lambda: constant_op.constant(1),
                                   raise_exception)
         self.assertEqual(z.eval(feed_dict={x: 1}), 1)
@@ -96,6 +98,50 @@ class SmartCondTest(test_util.TensorFlowTestCase):
         x = constant_op.constant(1)
         with self.assertRaises(TypeError):
           smart_cond.smart_cond(True, lambda: x)
+
+
+@test_util.with_c_api
+class SmartCaseTest(test_util.TensorFlowTestCase):
+
+  def testTrue(self):
+    x = array_ops.placeholder(dtype=dtypes.int32, shape=[])
+    conditions = [(True, lambda: constant_op.constant(1)),
+                  (x == 0, raise_exception)]
+    y = smart_cond.smart_case(conditions, default=raise_exception,
+                              exclusive=False)
+    z = smart_cond.smart_case(conditions, default=raise_exception,
+                              exclusive=True)
+    with session.Session() as sess:
+      # No feed_dict necessary
+      self.assertEqual(sess.run(y), 1)
+      self.assertEqual(sess.run(z), 1)
+
+  def testFalse(self):
+    conditions = [(False, raise_exception)]
+    y = smart_cond.smart_case(conditions,
+                              default=lambda: constant_op.constant(1),
+                              exclusive=False)
+    z = smart_cond.smart_case(conditions,
+                              default=lambda: constant_op.constant(1),
+                              exclusive=True)
+    with session.Session() as sess:
+      self.assertEqual(sess.run(y), 1)
+      self.assertEqual(sess.run(z), 1)
+
+  def testMix(self):
+    # Constant expression evaluation only works with the C API enabled.
+    if not ops._USE_C_API: return
+
+    x = array_ops.placeholder(dtype=dtypes.int32, shape=[])
+    y = constant_op.constant(10)
+    conditions = [(x > 1, lambda: constant_op.constant(1)),
+                  (y < 1, raise_exception),
+                  (False, raise_exception),
+                  (True, lambda: constant_op.constant(3))]
+    z = smart_cond.smart_case(conditions, default=raise_exception)
+    with session.Session() as sess:
+      self.assertEqual(sess.run(z, feed_dict={x: 2}), 1)
+      self.assertEqual(sess.run(z, feed_dict={x: 0}), 3)
 
 
 if __name__ == "__main__":
