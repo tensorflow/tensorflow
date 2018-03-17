85087076d1e0a50a96870643e1413720ba77403f
Derek Murray <mrry@google.com>
2018-03-16 17:10:16 -0700

    Automated g4 rollback of changelist 189228094
    PiperOrigin-RevId: 189416074

--- tensorflow/python/client/session_test.py
+++ tensorflow/python/client/session_test.py
@@ -22,7 +22,6 @@ import os
 import sys
 import threading
 import time
-import warnings
 
 import numpy as np
 import six
@@ -65,10 +64,6 @@ ops.RegisterShape('ConstructionFails')(common_shapes.unknown_shape)
 # TODO(skyewm): reenable when this works with _USE_C_SHAPES=False
 # @test_util.with_c_api
 class SessionTest(test_util.TensorFlowTestCase):
-
-  def setUp(self):
-    super(SessionTest, self).setUp()
-    warnings.simplefilter('always')
 
   def testUseExistingGraph(self):
     with ops.Graph().as_default() as g, ops.device('/cpu:0'):
@@ -1194,32 +1189,6 @@ class SessionTest(test_util.TensorFlowTestCase):
       e = math_ops.matmul(c, d)
       self.assertAllEqual([[24.0]], e.eval())
       sess.close()
-
-  def testMultipleInteractiveSessionsWarning(self):
-    # Reinitialize the global state to ensure that the expected warnings will
-    # be emitted.
-    session.InteractiveSession._active_session_count = 0  # pylint: disable=protected-access
-
-    sess = session.InteractiveSession()
-    sess.close()
-    # Opening and closing interactive sessions serially should not warn.
-    with warnings.catch_warnings(record=True) as w:
-      sess = session.InteractiveSession()
-      sess.close()
-    self.assertEqual(0, len(w))
-
-    with warnings.catch_warnings(record=True) as w:
-      sess = session.InteractiveSession()
-    self.assertEqual(0, len(w))
-    with warnings.catch_warnings(record=True) as w:
-      sess2 = session.InteractiveSession()
-    self.assertEqual(1, len(w))
-    self.assertTrue('An interactive session is already active. This can cause '
-                    'out-of-memory errors in some cases. You must explicitly '
-                    'call `InteractiveSession.close()` to release resources '
-                    'held by the other session(s).' in str(w[0].message))
-    sess2.close()
-    sess.close()
 
   def testInteractivePlacePrunedGraph(self):
     sess = session.InteractiveSession()
