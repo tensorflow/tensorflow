b1cb65ab5218c13eb9d0f55b7f169cd676e032f3
Fanjin Zeng <Fnjn@users.noreply.github.com>
2018-03-19 10:29:45 -0700

    Fix related doc clarification request on tf.contrib.lookup.MutableHashTable insert operation #17835
    Make the doc example executable, and explicitly suggests that MutableDenseHashTable.insert is an operation rather than in-place computation.

--- tensorflow/contrib/lookup/lookup_ops.py
+++ tensorflow/contrib/lookup/lookup_ops.py
@@ -494,7 +494,7 @@ class MutableDenseHashTable(LookupInterface):
                                                   value_dtype=tf.int64,
                                                   default_value=-1,
                                                   empty_key=0)
-  table.insert(keys, values)
+  sess.run(table.insert(keys, values))
   out = table.lookup(query_keys)
   print(out.eval())
   ```
