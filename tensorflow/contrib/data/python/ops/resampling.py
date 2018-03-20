198bf16225b3224e9af6bafd9f1b4c1433557281
joel-shor <joelshor@google.com>
2018-03-20 13:18:13 +0200

    Fix dataset resampling bug introduced by a bug in datasets itself.
    Fixes github issue #16606.
    
    The core issue is that in the case of certain random Tensors, the
    following two lines aren't the same:
    
    ```
    rand_0s_and_1s_ds = ...
    gather_ds = rand_0s_and_1s_ds.map(lambda i: tf.gather([0, 1], i))
    tup_ds = tf.data.Dataset.zip(gather_ds, rand_0s_and_1s_ds)
    
    ```
    
    ```
    rand_0s_and_1s_ds = ...
    tup_ds = rand_0s_and_1s_ds.map(lambda i: (tf.gather([0, 1], i), i))
    ```
    
    Note that this does NOT fix the underlying issue of drawing multiple
    sampes from the underlying distribution.
    
    Tested:
    With the new test, `bazel test :resample_test` fails before and succeeds
    after.

--- tensorflow/contrib/data/python/ops/resampling.py
+++ tensorflow/contrib/data/python/ops/resampling.py
@@ -101,11 +101,11 @@ def rejection_resample(class_func, target_dist, initial_dist=None, seed=None):
                                                    initial_dist_ds))
                           .map(maybe_warn_on_large_rejection))
 
-    current_probabilities_ds = dataset_ops.Dataset.zip(
-        (acceptance_dist_ds, class_values_ds)).map(array_ops.gather)
-    filtered_ds = (
-        dataset_ops.Dataset.zip((class_values_ds, current_probabilities_ds,
-                                 dataset))
+    def _gather_and_copy(class_val, acceptance_prob, data):
+        return (class_val, array_ops.gather(acceptance_prob, class_val), data)
+    current_probabilities_and_class_and_data_ds = dataset_ops.Dataset.zip(
+        (class_values_ds, acceptance_dist_ds, dataset)).map(_gather_and_copy)
+    filtered_ds = (current_probabilities_and_class_and_data_ds\
         .filter(lambda _1, p, _2: random_ops.random_uniform([], seed=seed) < p))
     return filtered_ds.map(lambda class_value, _, data: (class_value, data))
 
