053d6aa43c3ff49c27d635a478666b7c8646e0c4
Maximilian Mitchell <max@maxis.me>
2018-03-23 18:30:34 +0000

    Fix parameters

--- tensorflow/contrib/learn/python/learn/estimators/linear.py
+++ tensorflow/contrib/learn/python/learn/estimators/linear.py
@@ -244,7 +244,7 @@ def sdca_model_fn(features, labels, mode, params):
   parent_scope = "linear"
 
   with variable_scope.variable_scope(
-      features.values(), parent_scope) as scope:
+          values=features.values(), name_or_scope=parent_scope) as scope:
     features = features.copy()
     features.update(layers.transform_features(features, feature_columns))
     logits, columns_to_variables, bias = (
