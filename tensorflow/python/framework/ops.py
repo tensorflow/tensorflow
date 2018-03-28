d76ea97359821edb35e689ca705ede91d418e02f
Carl Thomé <carlthome@gmail.com>
2018-03-28 20:06:25 +0200

    Use nest.flatten in control_dependencies

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -55,6 +55,7 @@ from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.util import compat
 from tensorflow.python.util import decorator_utils
 from tensorflow.python.util import tf_contextlib
+from tensorflow.python.util import nest
 from tensorflow.python.util.tf_export import tf_export
 
 
@@ -4480,9 +4481,7 @@ class Graph(object):
     #   redundant control inputs.
     control_ops = []
     current = self._current_control_dependencies()
-    if type(control_inputs[0]) is list:
-      control_inputs = control_inputs[0]
-    for c in control_inputs:
+    for c in nest.flatten(control_inputs):
       if isinstance(c, IndexedSlices):
         c = c.op
       c = self.as_graph_element(c)
