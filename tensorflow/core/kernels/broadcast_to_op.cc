613ea20d2034224fbbe57081dedb5293f2fb7b52
Yong Tang <yong.tang.github@outlook.com>
2018-04-03 02:55:08 +0000

    Enable GPU kernel for BroadcastTo
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/core/kernels/broadcast_to_op.cc
+++ tensorflow/core/kernels/broadcast_to_op.cc
@@ -15,17 +15,20 @@ limitations under the License.
 
 #define EIGEN_USE_THREADS
 
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/types.h"
-#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/kernels/broadcast_to_op.h"
 
 namespace tensorflow {
 
 typedef Eigen::ThreadPoolDevice CPUDevice;
+typedef Eigen::GpuDevice GPUDevice;
 
-template <typename T>
+template <typename Device, typename T>
 class BroadcastToOp : public OpKernel {
  public:
   explicit BroadcastToOp(OpKernelConstruction* ctx) : OpKernel(ctx) {}
@@ -43,186 +46,9 @@ class BroadcastToOp : public OpKernel {
     Tensor* output_tensor = nullptr;
     OP_REQUIRES_OK(ctx, ctx->allocate_output(0, output_shape, &output_tensor));
 
-#define BROADCAST_SHAPE(broadcast, reshape, NDIMS, input_shape, output_shape) \
-  for (int i = 0; i < NDIMS; i++) {                                           \
-    OP_REQUIRES(ctx, (broadcast[i] % reshape[i] == 0),                        \
-                errors::InvalidArgument("invalid shape to broadcast from ",   \
-                                        input_shape.DebugString(), " to ",    \
-                                        output_shape.DebugString()));         \
-    broadcast[i] = broadcast[i] / reshape[i];                                 \
-  }
-
-    switch (output_shape.dims()) {
-      case 1: {
-        auto reshape = AsEigenDSizesWithPrefix<1>(input_shape);
-        auto broadcast = output_shape.AsEigenDSizes<1>();
-
-        BROADCAST_SHAPE(broadcast, reshape, 1, input_shape, output_shape);
-
-        auto output = output_tensor->tensor<T, 1>();
-        switch (input_shape.dims()) {
-          case 0: {
-            output.setConstant(input_tensor.scalar<T>()());
-          } break;
-          case 1: {
-            auto input = input_tensor.tensor<T, 1>();
-            output = input.broadcast(broadcast);
-          } break;
-          default:
-            ctx->CtxFailure(errors::InvalidArgument(
-                "invalid shape to broadcast from ", input_shape.DebugString(),
-                " to ", output_shape.DebugString()));
-            break;
-        }
-      } break;
-      case 2: {
-        auto reshape = AsEigenDSizesWithPrefix<2>(input_shape);
-        auto broadcast = output_shape.AsEigenDSizes<2>();
-
-        BROADCAST_SHAPE(broadcast, reshape, 2, input_shape, output_shape);
-
-        auto output = output_tensor->tensor<T, 2>();
-        switch (input_shape.dims()) {
-          case 0: {
-            output.setConstant(input_tensor.scalar<T>()());
-          } break;
-          case 1: {
-            auto input = input_tensor.tensor<T, 1>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 2: {
-            auto input = input_tensor.tensor<T, 2>();
-            output = input.broadcast(broadcast);
-          } break;
-          default:
-            ctx->CtxFailure(errors::InvalidArgument(
-                "invalid shape to broadcast from ", input_shape.DebugString(),
-                " to ", output_shape.DebugString()));
-            break;
-        }
-      } break;
-      case 3: {
-        auto reshape = AsEigenDSizesWithPrefix<3>(input_shape);
-        auto broadcast = output_shape.AsEigenDSizes<3>();
-
-        BROADCAST_SHAPE(broadcast, reshape, 3, input_shape, output_shape);
-
-        auto output = output_tensor->tensor<T, 3>();
-        switch (input_shape.dims()) {
-          case 0: {
-            output.setConstant(input_tensor.scalar<T>()());
-          } break;
-          case 1: {
-            auto input = input_tensor.tensor<T, 1>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 2: {
-            auto input = input_tensor.tensor<T, 2>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 3: {
-            auto input = input_tensor.tensor<T, 3>();
-            output = input.broadcast(broadcast);
-          } break;
-          default:
-            ctx->CtxFailure(errors::InvalidArgument(
-                "invalid shape to broadcast from ", input_shape.DebugString(),
-                " to ", output_shape.DebugString()));
-            break;
-        }
-      } break;
-      case 4: {
-        auto reshape = AsEigenDSizesWithPrefix<4>(input_shape);
-        auto broadcast = output_shape.AsEigenDSizes<4>();
-
-        BROADCAST_SHAPE(broadcast, reshape, 4, input_shape, output_shape);
-
-        auto output = output_tensor->tensor<T, 4>();
-        switch (input_shape.dims()) {
-          case 0: {
-            output.setConstant(input_tensor.scalar<T>()());
-          } break;
-          case 1: {
-            auto input = input_tensor.tensor<T, 1>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 2: {
-            auto input = input_tensor.tensor<T, 2>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 3: {
-            auto input = input_tensor.tensor<T, 3>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 4: {
-            auto input = input_tensor.tensor<T, 4>();
-            output = input.broadcast(broadcast);
-          } break;
-          default:
-            ctx->CtxFailure(errors::InvalidArgument(
-                "invalid shape to broadcast from ", input_shape.DebugString(),
-                " to ", output_shape.DebugString()));
-            break;
-        }
-      } break;
-      case 5: {
-        auto reshape = AsEigenDSizesWithPrefix<5>(input_shape);
-        auto broadcast = output_shape.AsEigenDSizes<5>();
-
-        BROADCAST_SHAPE(broadcast, reshape, 5, input_shape, output_shape);
-
-        auto output = output_tensor->tensor<T, 5>();
-        switch (input_shape.dims()) {
-          case 0: {
-            output.setConstant(input_tensor.scalar<T>()());
-          } break;
-          case 1: {
-            auto input = input_tensor.tensor<T, 1>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 2: {
-            auto input = input_tensor.tensor<T, 2>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 3: {
-            auto input = input_tensor.tensor<T, 3>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 4: {
-            auto input = input_tensor.tensor<T, 4>();
-            output = input.reshape(reshape).broadcast(broadcast);
-          } break;
-          case 5: {
-            auto input = input_tensor.tensor<T, 5>();
-            output = input.broadcast(broadcast);
-          } break;
-          default:
-            ctx->CtxFailure(errors::InvalidArgument(
-                "invalid shape to broadcast from ", input_shape.DebugString(),
-                " to ", output_shape.DebugString()));
-            break;
-        }
-      } break;
-      default:
-        ctx->CtxFailure(errors::InvalidArgument(
-            "invalid shape to broadcast from ", input_shape.DebugString(),
-            " to ", output_shape.DebugString()));
-        break;
-    }
-  }
-
- private:
-  template <int NDIMS>
-  Eigen::DSizes<Eigen::DenseIndex, NDIMS> AsEigenDSizesWithPrefix(
-      const TensorShape& shape) const {
-    Eigen::DSizes<Eigen::DenseIndex, NDIMS> dsizes;
-    for (int d = 0; d < NDIMS - shape.dims(); d++) {
-      dsizes[d] = 1;
-    }
-    for (int d = NDIMS - shape.dims(); d < NDIMS; d++) {
-      dsizes[d] = shape.dim_size(d - (NDIMS - shape.dims()));
-    }
-    return dsizes;
+    const Device& d = ctx->eigen_device<Device>();
+    functor::BroadcastTo<Device, T>()(d, ctx, *output_tensor, output_shape,
+                                      input_tensor, input_shape);
   }
 };
 
@@ -231,9 +57,35 @@ class BroadcastToOp : public OpKernel {
 #define REGISTER_KERNEL(type)                                           \
   REGISTER_KERNEL_BUILDER(                                              \
       Name("BroadcastTo").Device(DEVICE_CPU).TypeConstraint<type>("T"), \
-      BroadcastToOp<type>);
+      BroadcastToOp<CPUDevice, type>);
 
 TF_CALL_ALL_TYPES(REGISTER_KERNEL);
 #undef REGISTER_KERNEL
+
+#if GOOGLE_CUDA
+
+namespace functor {
+#define DECLARE_GPU_TEMPLATE(Type)                              \
+  template <>                                                   \
+  void BroadcastTo<GPUDevice, Type>::operator()(                \
+      const GPUDevice& d, OpKernelContext* ctx, Tensor& output, \
+      const TensorShape& output_shape, const Tensor& input,     \
+      const TensorShape& input_shape);                          \
+  extern template struct BroadcastTo<GPUDevice, Type>;
+
+TF_CALL_GPU_ALL_TYPES(DECLARE_GPU_TEMPLATE);
+#undef DECLARE_GPU_KERNEL
+}  // namespace functor
+
+#define REGISTER_KERNEL(type)                            \
+  REGISTER_KERNEL_BUILDER(Name("BroadcastTo")            \
+                              .Device(DEVICE_GPU)        \
+                              .TypeConstraint<type>("T") \
+                              .HostMemory("shape"),      \
+                          BroadcastToOp<GPUDevice, type>);
+
+TF_CALL_GPU_ALL_TYPES(REGISTER_KERNEL);
+#undef REGISTER_KERNEL
+#endif
 
 }  // namespace tensorflow
