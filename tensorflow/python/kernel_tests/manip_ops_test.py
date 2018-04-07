8252dbd1d1d92e2c481931fe089efb220c8e54d4
Yong Tang <yong.tang.github@outlook.com>
2018-04-07 21:14:41 +0000

    Add additional test cases
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/manip_ops_test.py
+++ tensorflow/python/kernel_tests/manip_ops_test.py
@@ -91,6 +91,10 @@ class RollTest(test_util.TensorFlowTestCase):
   def testNegativeAxis(self):
     self._testAll(np.random.randint(-100, 100, (5)).astype(np.int32), 3, -1)
     self._testAll(np.random.randint(-100, 100, (4, 4)).astype(np.int32), 3, -2)
+    # Make sure negative axis shoudl be 0 <= axis + dims < dims
+    with self.test_session():
+      with self.assertRaisesRegexp(errors_impl.InvalidArgumentError, "is out of range"):
+        manip_ops.roll(np.random.randint(-100, 100, (4, 4)).astype(np.int32), 3, -10).eval()
 
   def testRollInputMustVectorHigherRaises(self):
     tensor = 7
