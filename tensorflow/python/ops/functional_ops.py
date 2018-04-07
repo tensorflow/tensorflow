3d1fc0c0f7fb0746b8b109c085e82269bb2b187e
Yong Tang <yong.tang.github@outlook.com>
2018-04-07 20:01:09 +0000

    Add scanr support for tensorflow
    This fix tries to address the issue raised in 17387 where
    unlike `tf.foldl` vs `tf.foldr`, there is no `scanr` support
    which is the opposite of `tf.scan` (`scanl`).
    
    This fix adds the `scanr` support and add additional test cases.
    
    This fix fixes 17387.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/functional_ops.py
+++ tensorflow/python/ops/functional_ops.py
@@ -681,6 +681,130 @@ def scan(fn, elems, initializer=None, parallel_iterations=10, back_prop=True,
     return output_pack(results_flat)
 
 
+def scanr(fn, elems, initializer=None, parallel_iterations=10, back_prop=True,
+          swap_memory=False, infer_shape=True, name=None):
+  if not callable(fn):
+    raise TypeError("fn must be callable.")
+
+  input_is_sequence = nest.is_sequence(elems)
+  input_flatten = lambda x: nest.flatten(x) if input_is_sequence else [x]
+  def input_pack(x):
+    return nest.pack_sequence_as(elems, x) if input_is_sequence else x[0]
+
+  if initializer is None:
+    output_is_sequence = input_is_sequence
+    output_flatten = input_flatten
+    output_pack = input_pack
+  else:
+    output_is_sequence = nest.is_sequence(initializer)
+    output_flatten = lambda x: nest.flatten(x) if output_is_sequence else [x]
+    def output_pack(x):
+      return (nest.pack_sequence_as(initializer, x)
+              if output_is_sequence else x[0])
+
+  elems_flat = input_flatten(elems)
+
+  in_graph_mode = not context.executing_eagerly()
+  with ops.name_scope(name, "scan", elems_flat):
+    # TODO(akshayka): Remove the in_graph_mode check once caching devices are
+    # supported in Eager
+    if in_graph_mode:
+      # Any get_variable calls in fn will cache the first call locally
+      # and not issue repeated network I/O requests for each iteration.
+      varscope = vs.get_variable_scope()
+      varscope_caching_device_was_none = False
+      if varscope.caching_device is None:
+        # TODO(ebrevdo): Change to using colocate_with here and in other
+        # methods.
+        varscope.set_caching_device(lambda op: op.device)
+        varscope_caching_device_was_none = True
+
+    # Convert elems to tensor array.
+    elems_flat = [
+        ops.convert_to_tensor(elem, name="elem") for elem in elems_flat]
+
+    # Convert elems to tensor array. n may be known statically.
+    n = elems_flat[0].shape[0].value or array_ops.shape(elems_flat[0])[0]
+
+    # TensorArrays are always flat
+    elems_ta = [
+        tensor_array_ops.TensorArray(dtype=elem.dtype, size=n,
+                                     dynamic_size=False,
+                                     infer_shape=True)
+        for elem in elems_flat]
+    # Unpack elements
+    elems_ta = [
+        elem_ta.unstack(elem) for elem_ta, elem in zip(elems_ta, elems_flat)]
+
+    if initializer is None:
+      i = n - 1
+      a_flat = [elem.read(i) for elem in elems_ta]
+    else:
+      i = n
+      initializer_flat = output_flatten(initializer)
+      a_flat = [ops.convert_to_tensor(init) for init in initializer_flat]
+
+    # Create a tensor array to store the intermediate values.
+    accs_ta = [
+        tensor_array_ops.TensorArray(
+            dtype=init.dtype, size=n,
+            element_shape=init.shape if infer_shape else None,
+            dynamic_size=False,
+            infer_shape=infer_shape)
+        for init in a_flat]
+
+    if initializer is None:
+      accs_ta = [acc_ta.write(i, a) for (acc_ta, a) in zip(accs_ta, a_flat)]
+
+    def compute(i, a_flat, tas):
+      """The loop body of scan.
+
+      Args:
+        i: the loop counter.
+        a_flat: the accumulator value(s), flattened.
+        tas: the output accumulator TensorArray(s), flattened.
+
+      Returns:
+        [i - 1, a_flat, tas]: the updated counter + new accumulator values +
+          updated TensorArrays
+
+      Raises:
+        TypeError: if initializer and fn() output structure do not match
+        ValueType: if initializer and fn() output lengths do not match
+      """
+      i -= 1
+      packed_elems = input_pack([elem_ta.read(i) for elem_ta in elems_ta])
+      packed_a = output_pack(a_flat)
+      a_out = fn(packed_a, packed_elems)
+      nest.assert_same_structure(
+          elems if initializer is None else initializer, a_out)
+      flat_a_out = output_flatten(a_out)
+      tas = [ta.write(i, value) for (ta, value) in zip(tas, flat_a_out)]
+      return (i, flat_a_out, tas)
+
+    _, _, r_a = control_flow_ops.while_loop(
+        lambda i, _1, _2: i > 0, compute, (i, a_flat, accs_ta),
+        parallel_iterations=parallel_iterations,
+        back_prop=back_prop, swap_memory=swap_memory,
+        maximum_iterations=n)
+
+    results_flat = [r.stack() for r in r_a]
+
+    n_static = elems_flat[0].get_shape().with_rank_at_least(1)[0]
+    for elem in elems_flat[1:]:
+      n_static.merge_with(elem.get_shape().with_rank_at_least(1)[0])
+    for r in results_flat:
+      r.set_shape(tensor_shape.TensorShape(n_static).concatenate(
+          r.get_shape()[1:]))
+
+    # TODO(akshayka): Remove the in_graph_mode check once caching devices are
+    # supported in Eager
+    if in_graph_mode and varscope_caching_device_was_none:
+      varscope.set_caching_device(None)
+
+    return output_pack(results_flat)
+
+
 # pylint: disable=invalid-name
 def If(cond, inputs, then_branch, else_branch, name=None):
   r"""output = Cond(inputs) ? then_branch(inputs) : else_branch(inputs).
