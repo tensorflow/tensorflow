da0fed895c2cb8d8f16d0a8083bb635f623cfa75
Yong Tang <yong.tang.github@outlook.com>
2018-04-11 17:51:41 -0700

    Add deprecated_args decoration to expand_dims (#18419)
    * Add deprecated_args decoration to expand_dims
    
    This fix adds deprecated_args decoration to expand_dims
    as `dims` has been deprecated and in favor of `axis`
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Enhance deprecated args with deprecation.deprecated_argument_lookup
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -144,6 +144,7 @@ def identity(input, name=None):  # pylint: disable=redefined-builtin
 
 # pylint: disable=redefined-builtin,protected-access
 @tf_export("expand_dims")
+@deprecation.deprecated_args(None, "Use the `axis` argument instead", "dim")
 def expand_dims(input, axis=None, name=None, dim=None):
   """Inserts a dimension of 1 into a tensor's shape.
 
@@ -193,11 +194,7 @@ def expand_dims(input, axis=None, name=None, dim=None):
   Raises:
     ValueError: if both `dim` and `axis` are specified.
   """
-  # TODO(aselle): Remove argument dim
-  if dim is not None:
-    if axis is not None:
-      raise ValueError("can't specify both 'dim' and 'axis'")
-    axis = dim
+  axis = deprecation.deprecated_argument_lookup("axis", axis, "dim", dim)
   return gen_array_ops.expand_dims(input, axis, name)
 
 
