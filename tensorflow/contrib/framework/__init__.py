27210f41427645bda64699aad4273f697b8a408c
A. Unique TensorFlower <gardener@tensorflow.org>
2018-04-13 20:59:14 -0700

    Adding 1d and 3d orthogonal kernels convolutions.
    PiperOrigin-RevId: 192861809

--- tensorflow/contrib/framework/__init__.py
+++ tensorflow/contrib/framework/__init__.py
@@ -72,7 +72,9 @@ See the @{$python/contrib.framework} guide.
 @@variable
 @@VariableDeviceChooser
 @@convolutional_delta_orthogonal
+@@convolutional_orthogonal_1d
 @@convolutional_orthogonal_2d
+@@convolutional_orthogonal_3d
 @@zero_initializer
 
 @@load_checkpoint
@@ -117,7 +119,9 @@ from tensorflow.python.framework.smart_cond import smart_constant_value
 from tensorflow.python.framework.tensor_spec import BoundedTensorSpec
 from tensorflow.python.framework.tensor_spec import TensorSpec
 from tensorflow.python.ops.init_ops import convolutional_delta_orthogonal
+from tensorflow.python.ops.init_ops import convolutional_orthogonal_1d
 from tensorflow.python.ops.init_ops import convolutional_orthogonal_2d
+from tensorflow.python.ops.init_ops import convolutional_orthogonal_3d
 from tensorflow.python.util.all_util import remove_undocumented
 
 _allowed_symbols = ['nest']
