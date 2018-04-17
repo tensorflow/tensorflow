dc70fe423965be1efdbf6747aa73ff7738c91308
Yong Tang <yong.tang.github@outlook.com>
2018-04-17 21:36:21 +0000

    Clean up remaining issues.
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/feature_column/feature_column.py
+++ tensorflow/python/feature_column/feature_column.py
@@ -161,7 +161,6 @@ from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.training import checkpoint_utils
 from tensorflow.python.util import nest
 from tensorflow.python.util.tf_export import tf_export
-from tensorflow.python.util.tf_export import tf_export
 
 
 def _internal_input_layer(features,
