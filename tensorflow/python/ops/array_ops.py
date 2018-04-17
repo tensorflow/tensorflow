7248f1ff4a61491f22639e16db31f9545e291a6b
imsheridan <xiaoyudong0512@gmail.com>
2018-04-17 20:30:13 +0800

    fix typo to keep consistence

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -2690,8 +2690,8 @@ reverse.__doc__ = gen_array_ops.reverse_v2.__doc__
 
 # pylint: disable=redefined-builtin
 @tf_export("reverse_sequence")
-@deprecation.deprecated_args(None, "Use the `seq_axis` argument instead", "seq_dim")
-@deprecation.deprecated_args(None, "Use the `batch_axis` argument instead", "batch_dim")
+@deprecation.deprecated_args(None, "seq_dim is deprecated, use seq_axis instead", "seq_dim")
+@deprecation.deprecated_args(None, "batch_dim is deprecated, use batch_axis instead", "batch_dim")
 def reverse_sequence(input,
                      seq_lengths,
                      seq_axis=None,
