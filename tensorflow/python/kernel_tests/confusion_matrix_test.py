a2e4a7e362504e9bb83e90da0feeb9fab57fc3fa
Yong Tang <yong.tang.github@outlook.com>
2018-04-17 01:23:00 +0000

    Disable pylint: disable=redefined-builtin
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/confusion_matrix_test.py
+++ tensorflow/python/kernel_tests/confusion_matrix_test.py
@@ -19,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-from six.moves import xrange
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
