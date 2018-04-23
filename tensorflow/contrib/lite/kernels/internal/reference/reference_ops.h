105c7df01b12b77bc17909cfb4a0d0c0aff87571
A. Unique TensorFlower <gardener@tensorflow.org>
2018-04-23 13:44:57 -0700

    More relaxed size checking for TransposeConv, and miscellaneous bug fixes.
    PiperOrigin-RevId: 193977375

--- tensorflow/contrib/lite/kernels/internal/reference/reference_ops.h
+++ tensorflow/contrib/lite/kernels/internal/reference/reference_ops.h
@@ -3065,6 +3065,9 @@ inline void Pad(const T* input_data, const Dims<4>& input_dims,
                 const std::vector<int>& left_paddings,
                 const std::vector<int>& right_paddings, T* output_data,
                 const Dims<4>& output_dims, const int32_t pad_value) {
+  TFLITE_DCHECK_EQ(left_paddings.size(), 4);
+  TFLITE_DCHECK_EQ(right_paddings.size(), 4);
+
   const int output_batch = ArraySize(output_dims, 3);
   const int output_height = ArraySize(output_dims, 2);
   const int output_width = ArraySize(output_dims, 1);
