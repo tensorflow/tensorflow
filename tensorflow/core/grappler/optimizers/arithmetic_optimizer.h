9c7e819352581bf5a97509b1fa5dc71dffa26500
A. Unique TensorFlower <gardener@tensorflow.org>
2018-04-24 10:24:26 -0700

    Enable all arithmetic optimizations by default.
    PiperOrigin-RevId: 194106835

--- tensorflow/core/grappler/optimizers/arithmetic_optimizer.h
+++ tensorflow/core/grappler/optimizers/arithmetic_optimizer.h
@@ -57,9 +57,9 @@ class ArithmeticOptimizer : public GraphOptimizer {
     // TODO(ezhulenev): flag do disable TrySimplifyAndReplaceUses in tests.
     // Remove when all optimizers will be migrated to separate stages.
     bool enable_try_simplify_and_replace = true;
-    bool combine_add_to_addn = false;
+    bool combine_add_to_addn = true;
     bool hoist_common_factor_out_of_aggregation = true;
-    bool minimize_broadcasts = false;
+    bool minimize_broadcasts = true;
     bool remove_identity_transpose = true;
     bool remove_redundant_bitcast = true;
     bool remove_redundant_cast = true;
@@ -70,11 +70,6 @@ class ArithmeticOptimizer : public GraphOptimizer {
     static ArithmeticOptimizerOptions Default(
         RewriterConfig::Toggle opt_level) {
       ArithmeticOptimizerOptions options;
-      // TODO(ezhulenev): enable by default after 1.8 release cut
-      if (opt_level == RewriterConfig::AGGRESSIVE) {
-        options.combine_add_to_addn = true;
-        options.minimize_broadcasts = true;
-      }
       return options;
     }
   };
