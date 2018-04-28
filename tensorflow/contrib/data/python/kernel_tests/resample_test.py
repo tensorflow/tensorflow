b384c339ee7d8440b6d4e39c09202c19f900aebe
joel-shor <joelshor@google.com>
2018-04-29 01:16:16 +0300

    [tf.data] Possible bug fix to fix Winsows build.

--- tensorflow/contrib/data/python/kernel_tests/resample_test.py
+++ tensorflow/contrib/data/python/kernel_tests/resample_test.py
@@ -60,9 +60,9 @@ class ResampleTest(test.TestCase, parameterized.TestCase):
 
   @parameterized.named_parameters(
       ("InitialDistributionKnown", True),
-      ("InitialDistributionUnknown", True))  # THIS IS TO TEST THE WINDOWS BUILD DONT SUBMIT
+      ("InitialDistributionUnknown", False))
   def testDistribution(self, initial_known):
-    classes = np.random.randint(5, size=(20000,))  # Uniformly sampled
+    classes = np.random.randint(5, size=(20000,), dtype=np.int64)
     target_dist = [0.9, 0.05, 0.05, 0.0, 0.0]
     initial_dist = [0.2] * 5 if initial_known else None
     dataset = dataset_ops.Dataset.from_tensor_slices(classes).shuffle(
