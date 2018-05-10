11569894f10243fda5f827510cc30a9e12fc1e3a
A. Unique TensorFlower <gardener@tensorflow.org>
2018-05-10 11:35:22 -0700

    Extracts PartialAssocOpConstFolding into a method.
    PiperOrigin-RevId: 196146716

--- tensorflow/core/grappler/optimizers/constant_folding.cc
+++ tensorflow/core/grappler/optimizers/constant_folding.cc
@@ -2294,80 +2294,9 @@ Status ConstantFolding::SimplifyGraph(GraphDef* optimized_graph,
       }
     }
 
-    // Partial constant folding for associative operators:
-    // Split AddN/AccumulateNV2 to enable partial
-    // folding of ops when more than one but not all inputs are constant.
-    // For AddN and AccumulateNV2, we may furthermore reorder inputs, since
-    // addition is commutative.
-    const int num_non_control_inputs = NumNonControlInputs(*node);
-    if (IsAggregate(*node) && IsCommutative(*node) &&
-        num_non_control_inputs > 2) {
-      const int num_control_inputs =
-          node->input_size() - num_non_control_inputs;
-      std::vector<int> const_inputs;
-      std::vector<int> nonconst_inputs;
-      for (int i = 0; i < node->input_size(); ++i) {
-        const string& input = node->input(i);
-        const NodeDef* input_node = node_map_->GetNode(NodeName(input));
-        CHECK(input_node != nullptr) << input;
-        if (!IsControlInput(input) && IsReallyConstant(*input_node)) {
-          const_inputs.push_back(i);
-        } else {
-          // Non-const and control inputs.
-          nonconst_inputs.push_back(i);
-        }
-      }
-      // Promote AccumulateNV2 with all constant inputs to AddN, since it is
-      // a fake node that cannot be constant folded by itself.
-      if (const_inputs.size() == num_non_control_inputs &&
-          node->op() == "AccumulateNV2") {
-        node->set_op("AddN");
-        node->mutable_attr()->erase("shape");
-        graph_modified_ = true;
-        continue;
-      }
-      const string new_node_name = OptimizedNodeName(
-          *node, strings::StrCat("_partial_split_", const_inputs.size()));
-      if (1 < const_inputs.size() &&
-          const_inputs.size() < num_non_control_inputs &&
-          !node_map_->NodeExists(new_node_name)) {
-        NodeDef* added_node = optimized_graph->add_node();
-        *added_node = *node;
-        // Always use AddN for the constant node, since AccumulateNV2 is a fake
-        // node that cannot be constant folded, since it does not have a kernel.
-        added_node->set_op("AddN");
-        added_node->mutable_attr()->erase("shape");
-        added_node->set_name(new_node_name);
-        node_map_->AddNode(added_node->name(), added_node);
-        added_node->clear_input();
-        for (int i : const_inputs) {
-          added_node->add_input(node->input(i));
-          node_map_->UpdateOutput(NodeName(node->input(i)), node->name(),
-                                  added_node->name());
-        }
-
-        // Overwrite the first const input with the added node.
-        node->set_input(const_inputs[0], added_node->name());
-        node_map_->AddOutput(added_node->name(), node->name());
-        nonconst_inputs.push_back(const_inputs[0]);
-        // Compact the remaining inputs to the original node.
-        std::sort(nonconst_inputs.begin(), nonconst_inputs.end());
-        int idx = 0;
-        for (int i : nonconst_inputs) {
-          if (idx != i) {
-            node->set_input(idx, node->input(i));
-          }
-          ++idx;
-        }
-        node->mutable_input()->DeleteSubrange(nonconst_inputs.size(),
-                                              const_inputs.size() - 1);
-        (*node->mutable_attr())["N"].set_i(node->input_size() -
-                                           num_control_inputs);
-        properties->ClearInputProperties(node->name());
-        (*added_node->mutable_attr())["N"].set_i(const_inputs.size());
-        graph_modified_ = true;
-        continue;
-      }
+    if (PartialAssocOpConstFolding(optimized_graph, properties, node)) {
+      graph_modified_ = true;
+      continue;
     }
 
     if (PartialConcatConstFolding(optimized_graph, properties, node)) {
@@ -2377,6 +2306,84 @@ Status ConstantFolding::SimplifyGraph(GraphDef* optimized_graph,
   }
 
   return Status::OK();
+}
+
+bool ConstantFolding::PartialAssocOpConstFolding(GraphDef* optimized_graph,
+                                                 GraphProperties* properties,
+                                                 NodeDef* node) {
+  // Partial constant folding for associative operators:
+  // Split AddN/AccumulateNV2 to enable partial
+  // folding of ops when more than one but not all inputs are constant.
+  // For AddN and AccumulateNV2, we may furthermore reorder inputs, since
+  // addition is commutative.
+  const int num_non_control_inputs = NumNonControlInputs(*node);
+  if (IsAggregate(*node) && IsCommutative(*node) &&
+      num_non_control_inputs > 2) {
+    const int num_control_inputs = node->input_size() - num_non_control_inputs;
+    std::vector<int> const_inputs;
+    std::vector<int> nonconst_inputs;
+    for (int i = 0; i < node->input_size(); ++i) {
+      const string& input = node->input(i);
+      const NodeDef* input_node = node_map_->GetNode(NodeName(input));
+      CHECK(input_node != nullptr) << input;
+      if (!IsControlInput(input) && IsReallyConstant(*input_node)) {
+        const_inputs.push_back(i);
+      } else {
+        // Non-const and control inputs.
+        nonconst_inputs.push_back(i);
+      }
+    }
+    // Promote AccumulateNV2 with all constant inputs to AddN, since it is
+    // a fake node that cannot be constant folded by itself.
+    if (const_inputs.size() == num_non_control_inputs &&
+        node->op() == "AccumulateNV2") {
+      node->set_op("AddN");
+      node->mutable_attr()->erase("shape");
+      return true;
+    }
+    const string new_node_name = OptimizedNodeName(
+        *node, strings::StrCat("_partial_split_", const_inputs.size()));
+    if (1 < const_inputs.size() &&
+        const_inputs.size() < num_non_control_inputs &&
+        !node_map_->NodeExists(new_node_name)) {
+      NodeDef* added_node = optimized_graph->add_node();
+      *added_node = *node;
+      // Always use AddN for the constant node, since AccumulateNV2 is a fake
+      // node that cannot be constant folded, since it does not have a kernel.
+      added_node->set_op("AddN");
+      added_node->mutable_attr()->erase("shape");
+      added_node->set_name(new_node_name);
+      node_map_->AddNode(added_node->name(), added_node);
+      added_node->clear_input();
+      for (int i : const_inputs) {
+        added_node->add_input(node->input(i));
+        node_map_->UpdateOutput(NodeName(node->input(i)), node->name(),
+                                added_node->name());
+      }
+
+      // Overwrite the first const input with the added node.
+      node->set_input(const_inputs[0], added_node->name());
+      node_map_->AddOutput(added_node->name(), node->name());
+      nonconst_inputs.push_back(const_inputs[0]);
+      // Compact the remaining inputs to the original node.
+      std::sort(nonconst_inputs.begin(), nonconst_inputs.end());
+      int idx = 0;
+      for (int i : nonconst_inputs) {
+        if (idx != i) {
+          node->set_input(idx, node->input(i));
+        }
+        ++idx;
+      }
+      node->mutable_input()->DeleteSubrange(nonconst_inputs.size(),
+                                            const_inputs.size() - 1);
+      (*node->mutable_attr())["N"].set_i(node->input_size() -
+                                         num_control_inputs);
+      properties->ClearInputProperties(node->name());
+      (*added_node->mutable_attr())["N"].set_i(const_inputs.size());
+      return true;
+    }
+  }
+  return false;
 }
 
 bool ConstantFolding::PartialConcatConstFolding(GraphDef* optimized_graph,
