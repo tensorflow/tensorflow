4e822f04388f22ab87f47262f04eb42220461df4
Vijay Vasudevan <vrv@google.com>
2018-05-10 23:17:51 -0700

    add "as name" to end of name_scope line to match convention in file

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -1596,7 +1596,7 @@ def leaky_relu(features, alpha=0.2, name=None):
   Returns:
     The activation value.
   """
-  with ops.name_scope(name, "LeakyRelu", [features, alpha]):
+  with ops.name_scope(name, "LeakyRelu", [features, alpha]) as name:
     features = ops.convert_to_tensor(features, name="features")
     if features.dtype.is_integer:
       features = math_ops.to_float(features)
