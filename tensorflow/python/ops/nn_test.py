07f912c04d8b094ecb05105dab2e1f9b1ca81d6c
Jesse Gumz <jesse@jwplayer.com>
2018-05-14 14:30:58 -0400

    address pylint issues with line length

--- tensorflow/python/ops/nn_test.py
+++ tensorflow/python/ops/nn_test.py
@@ -964,9 +964,12 @@ class LeakyReluTest(test_lib.TestCase):
 
   def testName(self):
     np_values = np.array([-2, -1, 0, 1, 2], dtype=np.float64)
-    outputs_with_name_set = nn_ops.leaky_relu(constant_op.constant(np_values), name='test_relu_op')
+    outputs_with_name_set = nn_ops.leaky_relu(
+        constant_op.constant(np_values),
+        name='test_relu_op')
     self.assertEqual(outputs_with_name_set.name, 'test_relu_op:0')
-    outputs_without_name_set = nn_ops.leaky_relu(constant_op.constant(np_values))
+    outputs_without_name_set = nn_ops.leaky_relu(
+        constant_op.constant(np_values))
     self.assertEqual(outputs_without_name_set.name, 'LeakyRelu:0')
 
 
