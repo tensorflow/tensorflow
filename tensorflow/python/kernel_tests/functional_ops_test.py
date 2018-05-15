103638433f16f31dbde3480504c4c0a33273cc64
Alexandre Passos <apassos@google.com>
2018-05-15 13:07:49 -0700

    Allows scan to work in reverse as well as forward
    PiperOrigin-RevId: 196716758

--- tensorflow/python/kernel_tests/functional_ops_test.py
+++ tensorflow/python/kernel_tests/functional_ops_test.py
@@ -329,6 +329,23 @@ class FunctionalOpsTest(test.TestCase):
       # pylint: enable=unnecessary-lambda
 
   @test_util.run_in_graph_and_eager_modes()
+  def testScan_Reverse(self):
+    with self.test_session():
+      elems = constant_op.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], name="data")
+      v = constant_op.constant(2.0, name="v")
+
+      # pylint: disable=unnecessary-lambda
+      r = functional_ops.scan(lambda a, x: math_ops.multiply(a, x), elems,
+                              reverse=True)
+      self.assertAllEqual([720., 720., 360., 120., 30., 6.], self.evaluate(r))
+      r = functional_ops.scan(
+          lambda a, x: math_ops.multiply(a, x), elems, initializer=v,
+          reverse=True)
+      self.assertAllEqual([1440., 1440., 720., 240., 60., 12.],
+                          self.evaluate(r))
+      # pylint: enable=unnecessary-lambda
+
+  @test_util.run_in_graph_and_eager_modes()
   def testScan_SingleInputMultiOutput(self):
     with self.test_session():
       elems = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
