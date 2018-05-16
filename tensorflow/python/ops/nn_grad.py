9da3091216faa7d008378fd3aa6c1ad03429bd5a
zero-impact <lawsonfulton@gmail.com>
2018-05-16 15:51:14 -0400

    Fixing ELU and SELU GradGrad

--- tensorflow/python/ops/nn_grad.py
+++ tensorflow/python/ops/nn_grad.py
@@ -364,7 +364,7 @@ def _ReluGrad(op, grad):
 @ops.RegisterGradient("EluGrad")
 def _EluGradGrad(op, grad):
   elu_x = op.inputs[1]
-  return (gen_nn_ops.elu_grad(grad, op.outputs[0]),
+  return (gen_nn_ops.elu_grad(grad, elu_x),
           array_ops.where(elu_x < 0, grad * op.inputs[0],
                           array_ops.zeros(
                               shape=array_ops.shape(elu_x), dtype=elu_x.dtype)))
@@ -374,7 +374,7 @@ def _EluGradGrad(op, grad):
 def _SeluGradGrad(op, grad):
   x = op.inputs[1]
   scale_alpha = 1.7580993408473768599402175208123
-  return (gen_nn_ops.elu_grad(grad, op.outputs[0]),
+  return (gen_nn_ops.elu_grad(grad, x),
           array_ops.where(x < 0.,
                           gen_nn_ops.elu_grad(grad,
                                               op.outputs[0] + scale_alpha),
