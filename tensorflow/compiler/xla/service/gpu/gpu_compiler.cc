24c6a2186b888dd9035aa629ef49bc48518c920a
A. Unique TensorFlower <gardener@tensorflow.org>
2018-05-17 17:11:47 -0700

    Do not force default layout when there is no need to. Allow the inner computations to negotiate a root and parameter layouts different from default. END_PUBLIC
    RELNOTES: n/a
    
    ---------------------
    BEGIN_PUBLIC
    Automated g4 rollback of changelist 194293187
    
    PiperOrigin-RevId: 197076025

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
@@ -247,7 +247,7 @@ Status OptimizeHloModule(HloModule* hlo_module, se::StreamExecutor* stream_exec,
   {
     HloPassPipeline pipeline("layout_assignment");
     pipeline.AddPass<GpuLayoutAssignment>(
-        hlo_module->device_entry_computation_layout());
+        hlo_module->mutable_device_entry_computation_layout());
 
     // The LayoutAssignment pass may leave behind kCopy instructions which are
     // duplicate or NOPs, so remove them with algebraic simplification and CSE.
