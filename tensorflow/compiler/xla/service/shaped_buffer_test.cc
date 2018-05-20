3a297e69662e4149c32b983f34053e4829cce53b
Justin Lebar <jlebar@google.com>
2018-05-20 14:50:49 -0700

    [XLA] Fix memory leak in ScopedShapedBuffer's move-assignment operator.
    PiperOrigin-RevId: 197331197

--- tensorflow/compiler/xla/service/shaped_buffer_test.cc
+++ tensorflow/compiler/xla/service/shaped_buffer_test.cc
@@ -17,9 +17,11 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/service/device_memory_allocator.h"
 #include "tensorflow/compiler/xla/service/platform_util.h"
+#include "tensorflow/compiler/xla/shape_util.h"
 #include "tensorflow/compiler/xla/test.h"
 #include "tensorflow/core/util/ptr_util.h"
 
+namespace xla {
 namespace {
 
 TEST(ShapedBufferTest, ScopedShapeBufferAsShapedBufferB71629047) {
@@ -38,4 +40,71 @@ TEST(ShapedBufferTest, ScopedShapeBufferAsShapedBufferB71629047) {
   buffer = nullptr;
 }
 
-}  // namespace
+class TestAllocator : public DeviceMemoryAllocator {
+ public:
+  TestAllocator()
+      : DeviceMemoryAllocator(PlatformUtil::GetDefaultPlatform().ValueOrDie()) {
+  }
+
+  ~TestAllocator() override {
+    if (!allocations_.empty()) {
+      ADD_FAILURE() << "Some allocations not freed!";
+    }
+  }
+
+  // Pull in two-arg overload of Allocate.
+  using DeviceMemoryAllocator::Allocate;
+
+  StatusOr<OwningDeviceMemory> Allocate(int device_ordinal, uint64 size,
+                                        bool /*retry_on_failure*/) override {
+    // By contract, we must return null if size == 0.
+    if (size == 0) {
+      return OwningDeviceMemory();
+    }
+    void* buf = malloc(size);
+    allocations_.insert({device_ordinal, buf});
+    return OwningDeviceMemory(se::DeviceMemoryBase(buf, size), device_ordinal,
+                              this);
+  }
+
+  Status Deallocate(int device_ordinal, se::DeviceMemoryBase mem) override {
+    if (mem.is_null()) {
+      return Status::OK();
+    }
+
+    auto it = allocations_.find({device_ordinal, mem.opaque()});
+    if (it == allocations_.end()) {
+      ADD_FAILURE() << "Allocation not found (double free?)";
+    } else {
+      free(mem.opaque());
+      allocations_.erase(it);
+    }
+    return Status::OK();
+  }
+
+  bool AllowsAsynchronousDeallocation() const override { return false; }
+
+ private:
+  std::set<std::pair</*device_ordinal*/ int64, void*>> allocations_;
+};
+
+TEST(ScopedShapedBufferTest, TestMoveAssignmentOperator) {
+  Shape s = ShapeUtil::MakeShape(F32, {1});
+  TestAllocator allocator;
+  ScopedShapedBuffer sb1(s, s, &allocator, /*device_ordinal=*/0);
+  sb1.set_buffer(
+      allocator.Allocate(/*device_ordinal=*/0, /*size=*/42).ValueOrDie(),
+      /*index=*/{});
+
+  ScopedShapedBuffer sb2(s, s, &allocator, /*device_ordinal=*/1);
+  sb2.set_buffer(
+      allocator.Allocate(/*device_ordinal=*/1, /*size=*/10).ValueOrDie(),
+      /*index=*/{});
+
+  sb1 = std::move(sb2);
+
+  // TestAllocator's destructor checks that all memory was freed.
+}
+
+}  // anonymous namespace
+}  // namespace xla
