b5611ff9e7d28dcc50bf8e7a584ca43d6e2fe675
Gunhan Gulsoy <gunan@google.com>
2018-05-22 14:10:32 -0700

    Expose 2 more symbols.

--- tensorflow/contrib/cmake/tools/create_def_file.py
+++ tensorflow/contrib/cmake/tools/create_def_file.py
@@ -57,6 +57,8 @@ INCLUDEPRE_RE = re.compile(r"google::protobuf::internal::ExplicitlyConstructed|"
                            r"tensorflow::strings::internal::AppendPieces|"
                            r"tensorflow::strings::internal::CatPieces|"
                            r"tensorflow::errors::Internal|"
+                           r"tensorflow::Tensor::CopyFromInternal|"
+                           r"tensorflow::kernel_factory::OpKernelRegistrar::InitInternal|"
                            r"tensorflow::io::internal::JoinPathImpl")
 
 # Include if matched after exclude
