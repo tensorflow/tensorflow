a02c6c5dc2815b8f50e38f8e248927d0822434d6
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-05-23 16:20:42 -0500

    Replace hipBLAS with rocBLAS. Along the way also enable more BLAS features implemented in rocBLAS.

--- tensorflow/stream_executor/rocm/rocm_blas.h
+++ tensorflow/stream_executor/rocm/rocm_blas.h
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-// ROCM-specific support for BLAS functionality -- this wraps the HIPBLAS library
+// ROCM-specific support for BLAS functionality -- this wraps the rocBLAS library
 // capabilities, and is only included into ROCM implementation code -- it will
 // not introduce rocm headers into other code.
 
@@ -33,18 +33,18 @@ class Stream;
 
 namespace rocm {
 
-// Opaque and unique identifier for the HIPBLAS plugin.
-extern const PluginId kHipBlasPlugin;
+// Opaque and unique identifier for the rocBLAS plugin.
+extern const PluginId kRocBlasPlugin;
 
 class ROCMExecutor;
 
-// BLAS plugin for ROCM platform via HIPBLAS library.
+// BLAS plugin for ROCM platform via rocBLAS library.
 //
 // This satisfies the platform-agnostic BlasSupport interface.
 //
-// Note that the HIPBLAS handle that this encapsulates is implicitly tied to the
+// Note that the rocBLAS handle that this encapsulates is implicitly tied to the
 // context (and, as a result, the device) that the parent ROCMExecutor is tied
-// to. This simply happens as an artifact of creating the HIPBLAS handle when a
+// to. This simply happens as an artifact of creating the rocBLAS handle when a
 // ROCM context is active.
 //
 // Thread-safe post-initialization.
@@ -52,49 +52,49 @@ class ROCMBlas : public blas::BlasSupport {
  public:
   explicit ROCMBlas(ROCMExecutor *parent);
 
-  // Allocates a HIPBLAS handle.
+  // Allocates a rocBLAS handle.
   bool Init();
 
-  // Releases the HIPBLAS handle, if present.
+  // Releases the rocBLAS handle, if present.
   ~ROCMBlas() override;
 
   TENSORFLOW_STREAM_EXECUTOR_GPU_BLAS_SUPPORT_OVERRIDES
 
  private:
-  // Tells HIPBLAS to enqueue the BLAS operation onto a particular Stream.
+  // Tells rocBLAS to enqueue the BLAS operation onto a particular Stream.
   //
-  // HIPBLAS is stateful, and only be associated with one stream (in order to
+  // rocBLAS is stateful, and only be associated with one stream (in order to
   // enqueue dispatch) at a given time. As a result, this generally must be
-  // invoked before calling into HIPBLAS.
+  // invoked before calling into rocBLAS.
   bool SetStream(Stream *stream) EXCLUSIVE_LOCKS_REQUIRED(mu_);
 
-  // A helper function that calls the real HIPBLAS function together with error
+  // A helper function that calls the real rocBLAS function together with error
   // handling.
   //
-  // hipblas_func:        HIPBLAS function pointer.
-  // hipblas_name:        HIPBLAS function name.
+  // rocblas_func:       rocBLAS function pointer.
+  // rocblas_name:       rocBLAS function name.
   // stream:             Stream to enqueue the BLAS operation onto.
   // pointer_mode_host:  Indicate if the pointer to a scalar value is from host
   //                     (true) or device (false).
-  // err_on_failure:     Whether to print an error if the hipblas function fails.
-  // args:               Arguments of HIPBLAS function.
+  // err_on_failure:     Whether to print an error if the rocBLAS function fails.
+  // args:               Arguments of rocBLAS function.
   template <typename FuncT, typename... Args>
-  bool DoBlasInternalImpl(FuncT hipblas_func, Stream *stream,
+  bool DoBlasInternalImpl(FuncT rocblas_func, Stream *stream,
                           bool pointer_mode_host, bool err_on_failure,
                           Args... args);
 
   // Convenience functions that call DoBlasInternalImpl with different values
   // for err_on_failure.
   template <typename FuncT, typename... Args>
-  bool DoBlasInternal(FuncT hipblas_func, Stream *stream, bool pointer_mode_host,
+  bool DoBlasInternal(FuncT rocblas_func, Stream *stream, bool pointer_mode_host,
                       Args... args) {
-    return DoBlasInternalImpl(hipblas_func, stream, pointer_mode_host,
+    return DoBlasInternalImpl(rocblas_func, stream, pointer_mode_host,
                               /*err_on_failure=*/true, args...);
   }
   template <typename FuncT, typename... Args>
-  bool DoBlasInternalFailureOK(FuncT hipblas_func, Stream *stream,
+  bool DoBlasInternalFailureOK(FuncT rocblas_func, Stream *stream,
                                bool pointer_mode_host, Args... args) {
-    return DoBlasInternalImpl(hipblas_func, stream, pointer_mode_host,
+    return DoBlasInternalImpl(rocblas_func, stream, pointer_mode_host,
                               /*err_on_failure=*/false, args...);
   }
 
@@ -102,7 +102,7 @@ class ROCMBlas : public blas::BlasSupport {
   // types.
   template <typename T, typename FuncT>
   port::Status DoBlasGemmBatchedInternal(
-      FuncT hipblas_func, Stream *stream, blas::Transpose transa,
+      FuncT rocblas_func, Stream *stream, blas::Transpose transa,
       blas::Transpose transb, uint64 m, uint64 n, uint64 k, T alpha,
       const port::ArraySlice<DeviceMemory<T> *> &a_array, int lda,
       const port::ArraySlice<DeviceMemory<T> *> &b_array, int ldb, T beta,
@@ -113,7 +113,7 @@ class ROCMBlas : public blas::BlasSupport {
   //
   // We take alpha and beta by const reference because T might be Eigen::half,
   // and we want to avoid pulling in a dependency on Eigen.  When we pass the
-  // references to hipblas, we essentially reinterpret_cast to __half, which is
+  // references to rocBLAS, we essentially reinterpret_cast to __half, which is
   // safe because Eigen::half inherits from __half.
   template <typename InT, typename OutT, typename CompT>
   bool DoBlasGemmWithAlgorithmImpl(
@@ -141,15 +141,15 @@ class ROCMBlas : public blas::BlasSupport {
                                    const T &beta, DeviceMemory<T> *y, int incy,
                                    blas::ProfileResult *output_profile_result);
 
-  // mutex that guards the HIPBLAS handle for this device.
+  // mutex that guards the rocBLAS handle for this device.
   mutex mu_;
 
   // ROCMExecutor which instantiated this ROCMBlas.
   // Immutable post-initialization.
   ROCMExecutor *parent_;
 
-  // HIPBLAS library handle on the device.
-  hipblasHandle_t blas_ GUARDED_BY(mu_);
+  // rocBLAS library handle on the device.
+  rocblas_handle blas_ GUARDED_BY(mu_);
 
   SE_DISALLOW_COPY_AND_ASSIGN(ROCMBlas);
 };
