68430112b2ca5c160db6dd412d43f572ec69e72f
Alexandre Passos <apassos@google.com>
2018-05-25 13:20:13 -0700

    Public API to switch between eager execution and graph building.
    Now, after tf.enable_eager_execution() has been executed, entering the context
    manager of a tf.Graph will enable graph mode. So, for example
    
    ```
    tf.enable_eager_execution()
    with tf.Graph().as_default():
      c = tf.constant(1.0)  # this is a graph tensor
    c2 = tf.constant(1.0)  # this is an eager tensor
    ```
    
    The main use-case of this is allowing documentation writers to make a single
    notebook which starts with eager execution and seamlessly transitions to
    building graphs.
    
    This also makes many explicit enablings of graph mode in the code redundant
    (a cleanup cl will follow).
    
    PiperOrigin-RevId: 198092991

--- tensorflow/python/ops/variables.py
+++ tensorflow/python/ops/variables.py
@@ -259,7 +259,7 @@ class Variable(checkpointable.CheckpointableBase):
           constraint=constraint)
 
   def __repr__(self):
-    if context.executing_eagerly():
+    if context.executing_eagerly() and not self._in_graph_mode:
       return "<tf.Variable '%s' shape=%s dtype=%s, numpy=%s>" % (
           self.name, self.get_shape(), self.dtype.name,
           ops.numpy_text(self.read_value(), is_repr=True))
