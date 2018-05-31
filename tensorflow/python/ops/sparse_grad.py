86191c9f267fbd157c199c410e8d46574d034782
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-05-31 12:27:39 +0800

    DOC: add document

--- tensorflow/python/ops/sparse_grad.py
+++ tensorflow/python/ops/sparse_grad.py
@@ -120,19 +120,18 @@ def _SparseReduceSumGrad(op, out_grad):
 def _SparseSliceGrad(op, *grads):
   """The backward operator for the SparseSlice op.
 
-  The SparseAdd op calculates A + B, where A, B, and the sum are all represented
-  as `SparseTensor` objects.  This op takes in the upstream gradient w.r.t.
-  non-empty values of the sum, and outputs the gradients w.r.t. the non-empty
-  values of A and B.
+  This op takes in the upstream gradient w.r.t. non-empty values of
+  the sliced `SparseTensor`, and outputs the gradients w.r.t.
+  the non-empty values of input `SparseTensor`.
 
   Args:
-    op: the SparseAdd op
+    op: the SparseSlice op
     *grads: the incoming gradients, one element per output of `op`
 
   Returns:
-    Gradient for each of the 6 input tensors of SparseAdd:
-      (a_indices, a_values, a_shape, b_indices, b_values, b_shape, thresh)
-    The gradients for the indices, shapes, and the threshold are None.
+    Gradient for each of the 5 input tensors of SparseSlice:
+      (indices, values, shape, start, size)
+    The gradients for the indices, shapes, start and the size are None.
   """
   backprop_val_grad = grads[1]
   input_indices = op.inputs[0]
