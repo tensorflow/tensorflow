16c42f0d4826b12a5359281997ee3f8e27fd5a87
JxKing <jinxin900924@gmail.com>
2018-05-31 19:24:19 +0800

    fix "workers share local variables" error

--- tensorflow/contrib/opt/python/training/elastic_average_optimizer.py
+++ tensorflow/contrib/opt/python/training/elastic_average_optimizer.py
@@ -102,7 +102,12 @@ class ElasticAverageCustomGetter(object):
     else:
       kwargs['trainable'] = trainable
       kwargs['collections'] = collections
-      return getter(name, *args, **kwargs)
+      if ops.GraphKeys.LOCAL_VARIABLES in collections:
+        with ops.device(self._worker_device):
+          return getter(name, *args, **kwargs)
+      else:
+        return getter(name, *args, **kwargs)
+
 
 
 class ElasticAverageOptimizer(optimizer.Optimizer):
