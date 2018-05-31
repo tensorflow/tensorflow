c47442d74edf0de11cad2975662a21bd27b9bf68
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-05-31 10:45:28 +0800

    ENH: add gradient in python side

--- tensorflow/python/ops/sparse_grad.py
+++ tensorflow/python/ops/sparse_grad.py
@@ -116,6 +116,36 @@ def _SparseReduceSumGrad(op, out_grad):
           None, None)
 
 
+@ops.RegisterGradient("SparseSlice")
+def _SparseSliceGrad(op, *grads):
+  """The backward operator for the SparseSlice op.
+
+  The SparseAdd op calculates A + B, where A, B, and the sum are all represented
+  as `SparseTensor` objects.  This op takes in the upstream gradient w.r.t.
+  non-empty values of the sum, and outputs the gradients w.r.t. the non-empty
+  values of A and B.
+
+  Args:
+    op: the SparseAdd op
+    *grads: the incoming gradients, one element per output of `op`
+
+  Returns:
+    Gradient for each of the 6 input tensors of SparseAdd:
+      (a_indices, a_values, a_shape, b_indices, b_values, b_shape, thresh)
+    The gradients for the indices, shapes, and the threshold are None.
+  """
+  backprop_val_grad = grads[1]
+  input_indices = op.inputs[0]
+  input_start = op.inputs[3]
+  output_indices = op.outputs[0]
+
+  val_grad = gen_sparse_ops.sparse_slice_grad(
+    backprop_val_grad, input_indices, input_start, output_indices)
+  val_grad.set_shape(op.inputs[1].get_shape())
+  # (indices, values, shape, start, size)
+  return (None, val_grad, None, None, None)
+
+
 @ops.RegisterGradient("SparseTensorDenseMatMul")
 def _SparseTensorDenseMatMulGrad(op, grad):
   """Gradients for the dense tensor in the SparseTensorDenseMatMul op.
