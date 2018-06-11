ca300754967812a6a7d9f5ecfa62ca7e94be5a37
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-06-11 16:22:24 -0500

    Do not use problematic branch on ROCm. nn_test now works for me.

--- tensorflow/core/kernels/reduction_gpu_kernels.cu.h
+++ tensorflow/core/kernels/reduction_gpu_kernels.cu.h
@@ -695,9 +695,13 @@ void LaunchColumnReduction(OpKernelContext* ctx, OUT_T out, IN_T in,
   if (extent_y <= 16) {
     LaunchColumnReduction_LTE16Cols(ctx, out, in, extent_x, extent_y, op, init,
                                     cu_stream);
+#ifdef GOOGLE_CUDA
+// FIXME on ROCm
   } else if (extent_y <= 4096) {
     LaunchColumnReduction_LTE4096Cols<T, Op, OUT_T, IN_T>(ctx, out, in, extent_x, extent_y, op,
+
                                       init, cu_stream);
+#endif
   } else {
     int threads_per_block = 128;
     int num_blocks = Eigen::divup(extent_y, threads_per_block);
