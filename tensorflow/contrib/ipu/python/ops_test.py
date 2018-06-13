391f5c92a0c0783d5a815cb3913a4d46925d430e
DavidNorman <davidn@graphcore.ai>
2018-06-13 22:23:13 +0100

    Eliminate the tensorflow_py dependency to improve test times
    Summary:
    This change prevents the need for a lot more compiling in order
    to run the tests in the module
    
    Test Plan: all existing tests ok
    
    Reviewers: georgep
    
    Reviewed By: georgep
    
    Differential Revision: https://phabricator.sourcevertex.net/D4712

--- tensorflow/contrib/ipu/python/ops_test.py
+++ tensorflow/contrib/ipu/python/ops_test.py
@@ -2,26 +2,36 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import numpy as np
 
 from tensorflow.compiler.plugin.poplar.ops import gen_ipu_ops
-
-from tensorflow.python.platform import googletest
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.python.client import session as sl
 from tensorflow.python.framework import test_util
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import rnn
+from tensorflow.python.ops import rnn_cell
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import variables
+from tensorflow.python.ops.losses import losses
+from tensorflow.python.platform import googletest
+from tensorflow.contrib import ipu
+from tensorflow.python.training import gradient_descent
 
 
 class ContribIpuOpsTest(test_util.TensorFlowTestCase):
 
   def testSummary(self):
-    with tf.device("/device:IPU:0"):
-      a = tf.placeholder(tf.float32, [1], name="a")
-      b = tf.placeholder(tf.float32, [1], name="b")
+    with ops.device("/device:IPU:0"):
+      a = array_ops.placeholder(np.float32, [1], name="a")
+      b = array_ops.placeholder(np.float32, [1], name="b")
       out = a + b
 
-    summary = tf.contrib.ipu.ops.ipu_compile_summary('comp', out)
+    summary = ipu.ops.ipu_compile_summary('comp', out)
 
-    cfg = tf.contrib.ipu.utils.create_ipu_config(True)
-    with tf.Session(config=tf.ConfigProto(ipu_options=cfg)) as sess:
+    cfg = ipu.utils.create_ipu_config(True)
+    with sl.Session(config=config_pb2.ConfigProto(ipu_options=cfg)) as sess:
       fd = {
         a: [1.0],
         b: [2.0],
@@ -31,15 +41,15 @@ class ContribIpuOpsTest(test_util.TensorFlowTestCase):
       self.assertTrue(len(s) > 100)
 
   def testEventFetchAndStringDecode(self):
-    with tf.device("/device:IPU:0"):
-      a = tf.placeholder(tf.float32, [1], name="a")
-      b = tf.placeholder(tf.float32, [1], name="b")
+    with ops.device("/device:IPU:0"):
+      a = array_ops.placeholder(np.float32, [1], name="a")
+      b = array_ops.placeholder(np.float32, [1], name="b")
       out = a + b
 
     events = gen_ipu_ops.ipu_event_trace()
 
-    cfg = tf.contrib.ipu.utils.create_ipu_config(True)
-    with tf.Session(config=tf.ConfigProto(ipu_options=cfg)) as sess:
+    cfg = ipu.utils.create_ipu_config(True)
+    with sl.Session(config=config_pb2.ConfigProto(ipu_options=cfg)) as sess:
       # Discard any existing events
       sess.run(events)
 
@@ -54,7 +64,7 @@ class ContribIpuOpsTest(test_util.TensorFlowTestCase):
       e = sess.run(events)
       self.assertEqual(len(e), 4)
 
-      dump = tf.contrib.ipu.utils.extract_all_strings_from_event_trace(e);
+      dump = ipu.utils.extract_all_strings_from_event_trace(e);
       self.assertTrue(len(dump) > 100)
 
   def testIpuScope(self):
@@ -62,34 +72,33 @@ class ContribIpuOpsTest(test_util.TensorFlowTestCase):
     # 2: variables are resource variables
     # 3: training a while_loop is possible
     def RNN(x):
-      lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(1, forget_bias=1.0)
-      outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
+      lstm_cell = rnn_cell.BasicLSTMCell(1, forget_bias=1.0)
+      outputs, states = rnn.dynamic_rnn(lstm_cell, x, dtype=np.float32)
       return outputs[-1]
 
-    with tf.contrib.ipu.ops.ipu_scope("/device:IPU:0"):
+    with ipu.ops.ipu_scope("/device:IPU:0"):
 
-      a = tf.placeholder(tf.float32, [1,8,1], name="a")
-      b = tf.placeholder(tf.float32, [1,8,1], name="b")
+      a = array_ops.placeholder(np.float32, [1,8,1], name="a")
+      b = array_ops.placeholder(np.float32, [1,8,1], name="b")
 
-      c = tf.get_variable('c', shape=[1],
-                          initializer=tf.constant_initializer(1.0))
+      c = variable_scope.get_variable('c', initializer=[1.0])
 
       logits = RNN(a) * c
 
-      res = tf.reshape(logits, [1,8,1])
+      res = array_ops.reshape(logits, [1,8,1])
 
-      l = tf.losses.mean_squared_error(res, b)
+      l = losses.mean_squared_error(res, b)
 
-      optimizer = tf.train.GradientDescentOptimizer(0.1)
+      optimizer = gradient_descent.GradientDescentOptimizer(0.1)
       train = optimizer.minimize(l)
 
     self.assertTrue("ResourceVariable" in str(type(c)))
     self.assertEqual(logits.device, "/device:IPU:0")
 
-    cfg = tf.contrib.ipu.utils.create_ipu_config(True)
-    with tf.Session(config=tf.ConfigProto(ipu_options=cfg)) as sess:
+    cfg = ipu.utils.create_ipu_config(True)
+    with sl.Session(config=config_pb2.ConfigProto(ipu_options=cfg)) as sess:
       # Initialize and then discard events relating to initialization
-      sess.run(tf.global_variables_initializer())
+      sess.run(variables.global_variables_initializer())
 
       fd = {
         a: [[[1.],[1.],[1.],[1.],[1.],[1.],[1.],[1.]]],
