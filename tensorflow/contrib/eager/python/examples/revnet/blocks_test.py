ee3ecdfde04591366eadbb4e79b8885b47f274cb
A. Unique TensorFlower <gardener@tensorflow.org>
2018-06-13 11:18:46 -0700

    Reversible residual network example with manually built gradient computation in TensorFlow eager mode execution.
    PiperOrigin-RevId: 200422481

---
+++ tensorflow/contrib/eager/python/examples/revnet/blocks_test.py
@@ -0,0 +1,346 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for basic building blocks used in eager mode RevNet."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow.contrib.eager.python.examples.revnet import blocks
+
+
+def _validate_block_call_channels_last(block_factory, test):
+  """Generic testing function for `channels_last` data format.
+
+  Completes a set of tests varying data format, stride, and batch normalization
+  configured train vs test time.
+  Args:
+    block_factory: constructor of one of blocks.InitBlock, blocks.FinalBlock,
+      blocks._ResidualInner
+    test: tf.test.TestCase object
+  """
+  with tf.device("/cpu:0"):  # NHWC format
+    input_shape = (224, 224, 32)
+    data_shape = (16,) + input_shape
+    x = tf.random_normal(shape=data_shape)
+
+    # Stride 1
+    block = block_factory(
+        filters=64,
+        strides=(1, 1),
+        input_shape=input_shape,
+        data_format="channels_last")
+    y_tr, y_ev = block(x, training=True), block(x, training=False)
+    test.assertEqual(y_tr.shape, y_ev.shape)
+    test.assertEqual(y_ev.shape, (16, 224, 224, 64))
+    test.assertNotAllClose(y_tr, y_ev)
+
+    # Stride of 2
+    block = block_factory(
+        filters=64,
+        strides=(2, 2),
+        input_shape=input_shape,
+        data_format="channels_last")
+    y_tr, y_ev = block(x, training=True), block(x, training=False)
+    test.assertEqual(y_tr.shape, y_ev.shape)
+    test.assertEqual(y_ev.shape, (16, 112, 112, 64))
+    test.assertNotAllClose(y_tr, y_ev)
+
+
+def _validate_block_call_channels_first(block_factory, test):
+  """Generic testing function for `channels_first` data format.
+
+  Completes a set of tests varying data format, stride, and batch normalization
+  configured train vs test time.
+  Args:
+    block_factory: constructor of one of blocks.InitBlock, blocks.FinalBlock,
+      blocks._ResidualInner
+    test: tf.test.TestCase object
+  """
+  if not tf.test.is_gpu_available():
+    test.skipTest("GPU not available")
+
+  with tf.device("/gpu:0"):  # Default NCHW format
+    input_shape = (32, 224, 224)
+    data_shape = (16,) + input_shape
+    x = tf.random_normal(shape=data_shape)
+
+    # Stride of 1
+    block = block_factory(filters=64, strides=(1, 1), input_shape=input_shape)
+    y_tr, y_ev = block(x, training=True), block(x, training=False)
+    test.assertEqual(y_tr.shape, y_ev.shape)
+    test.assertEqual(y_ev.shape, (16, 64, 224, 224))
+    test.assertNotAllClose(y_tr, y_ev)
+
+    # Stride of 2
+    block = block_factory(filters=64, strides=(2, 2), input_shape=input_shape)
+    y_tr, y_ev = block(x, training=True), block(x, training=False)
+    test.assertEqual(y_tr.shape, y_ev.shape)
+    test.assertEqual(y_ev.shape, (16, 64, 112, 112))
+    test.assertNotAllClose(y_tr, y_ev)
+
+
+class RevBlockTest(tf.test.TestCase):
+
+  def test_call_channels_first(self):
+    """Test `call` function with `channels_first` data format."""
+    if not tf.test.is_gpu_available():
+      self.skipTest("GPU not available")
+
+    with tf.device("/gpu:0"):  # Default NCHW format
+      input_shape = (32, 224, 224)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+
+      # Stride of 1
+      block = blocks.RevBlock(
+          n_res=3, filters=64, strides=(1, 1), input_shape=input_shape)
+      y_tr, y_ev = block(x, training=True), block(x, training=False)
+      self.assertEqual(y_tr.shape, y_ev.shape)
+      self.assertEqual(y_ev.shape, (16, 64, 224, 224))
+      self.assertNotAllClose(y_tr, y_ev)
+
+      # Stride of 2
+      block = blocks.RevBlock(
+          n_res=3, filters=64, strides=(2, 2), input_shape=input_shape)
+      y_tr, y_ev = block(x, training=True), block(x, training=False)
+      self.assertEqual(y_tr.shape, y_ev.shape)
+      self.assertEqual(y_ev.shape, [16, 64, 112, 112])
+      self.assertNotAllClose(y_tr, y_ev)
+
+  def test_call_channels_last(self):
+    """Test `call` function with `channels_last` data format."""
+    with tf.device("/cpu:0"):  # NHWC format
+      input_shape = (224, 224, 32)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+
+      # Stride 1
+      block = blocks.RevBlock(
+          n_res=3,
+          filters=64,
+          strides=(1, 1),
+          input_shape=input_shape,
+          data_format="channels_last")
+      y_tr, y_ev = block(x, training=True), block(x, training=False)
+      self.assertEqual(y_tr.shape, y_ev.shape)
+      self.assertEqual(y_ev.shape, (16, 224, 224, 64))
+      self.assertNotAllClose(y_tr, y_ev)
+
+      # Stride of 2
+      block = blocks.RevBlock(
+          n_res=3,
+          filters=64,
+          strides=(2, 2),
+          input_shape=input_shape,
+          data_format="channels_last")
+      y_tr, y_ev = block(x, training=True), block(x, training=False)
+      self.assertEqual(y_tr.shape, y_ev.shape)
+      self.assertEqual(y_ev.shape, (16, 112, 112, 64))
+      self.assertNotAllClose(y_tr, y_ev)
+
+  def test_backward_grads_and_vars_channels_first(self):
+    """Test `backward` function with `channels_first` data format."""
+    if not tf.test.is_gpu_available():
+      self.skipTest("GPU not available")
+
+    with tf.device("/gpu:0"):  # Default NCHW format
+      input_shape = (32, 224, 224)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+
+      # Stride 1
+      y = tf.random_normal(shape=data_shape)
+      dy = tf.random_normal(shape=data_shape)
+      block = blocks.RevBlock(
+          n_res=3, filters=32, strides=(1, 1), input_shape=input_shape)
+      dy, grads, vars_ = block.backward_grads_and_vars(x, y, dy)
+      self.assertEqual(dy.shape, x.shape)
+      self.assertTrue(isinstance(grads, list))
+      self.assertTrue(isinstance(vars_, list))
+
+      # Stride 2
+      y = tf.random_normal(shape=(16, 32, 112, 112))
+      dy = tf.random_normal(shape=(16, 32, 112, 112))
+      block = blocks.RevBlock(
+          n_res=3, filters=32, strides=(2, 2), input_shape=input_shape)
+      dy, grads, vars_ = block.backward_grads_and_vars(x, y, dy)
+      self.assertEqual(dy.shape, x.shape)
+      self.assertTrue(isinstance(grads, list))
+      self.assertTrue(isinstance(vars_, list))
+
+  def test_backward_grads_and_vars_channels_last(self):
+    """Test `backward` function with `channels_last` data format."""
+    with tf.device("/cpu:0"):  # NHWC format
+      input_shape = (224, 224, 32)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+
+      # Stride 1
+      y = tf.random_normal(shape=data_shape)
+      dy = tf.random_normal(shape=data_shape)
+      block = blocks.RevBlock(
+          n_res=3,
+          filters=32,
+          strides=(1, 1),
+          input_shape=input_shape,
+          data_format="channels_last")
+      dy, grads, vars_ = block.backward_grads_and_vars(x, y, dy)
+      self.assertEqual(dy.shape, x.shape)
+      self.assertTrue(isinstance(grads, list))
+      self.assertTrue(isinstance(vars_, list))
+
+      # Stride 2
+      y = tf.random_normal(shape=(16, 112, 112, 32))
+      dy = tf.random_normal(shape=(16, 112, 112, 32))
+      block = blocks.RevBlock(
+          n_res=3,
+          filters=32,
+          strides=(2, 2),
+          input_shape=input_shape,
+          data_format="channels_last")
+      dy, grads, vars_ = block.backward_grads_and_vars(x, y, dy)
+      self.assertEqual(dy.shape, x.shape)
+      self.assertTrue(isinstance(grads, list))
+      self.assertTrue(isinstance(vars_, list))
+
+
+class _ResidualTest(tf.test.TestCase):
+
+  def test_call(self):
+    """Test `call` function.
+
+    Varying downsampling and data format options.
+    """
+
+    _validate_block_call_channels_first(blocks._Residual, self)
+    _validate_block_call_channels_last(blocks._Residual, self)
+
+  def test_backward_channels_first(self):
+    """Test `backward` function with `channels_first` data format."""
+    if not tf.test.is_gpu_available():
+      self.skipTest("GPU not available")
+
+    with tf.device("/gpu:0"):  # Default NCHW format
+      input_shape = (16, 224, 224)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+      residual = blocks._Residual(
+          filters=16, strides=(1, 1), input_shape=input_shape)
+      y_tr, y_ev = residual(x, training=True), residual(x, training=False)
+      x_ = residual.backward(y_tr, training=True)
+      # The numerical loss is alarming; reconstructed inputs could differ from
+      # the original inputs often by more than 1e-3
+      self.assertAllClose(x, x_, rtol=1e-01, atol=1e-01)
+      x_ = residual.backward(y_ev, training=False)
+      self.assertAllClose(x, x_, rtol=1e-01, atol=1e-01)
+
+  def test_backward_channels_last(self):
+    """Test `backward` function with `channels_last` data format."""
+    with tf.device("/cpu:0"):  # NHWC format
+      input_shape = (224, 224, 16)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+      residual = blocks._Residual(
+          filters=16,
+          strides=(1, 1),
+          input_shape=input_shape,
+          data_format="channels_last")
+      y_tr, y_ev = residual(x, training=True), residual(x, training=False)
+      x_ = residual.backward(y_tr, training=True)
+      # Egregious numerical error
+      self.assertAllClose(x, x_, rtol=1e-01, atol=1e-01)
+      x_ = residual.backward(y_ev, training=False)
+      self.assertAllClose(x, x_, rtol=1e-01, atol=1e-01)
+
+  def test_backward_grads_and_vars_channels_first(self):
+    """Test `backward_grads` function with `channels_first` data format."""
+    if not tf.test.is_gpu_available():
+      self.skipTest("GPU not available")
+
+    with tf.device("/gpu:0"):  # Default NCHW format
+      input_shape = (16, 224, 224)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+      dy = tf.random_normal(shape=data_shape)
+      residual = blocks._Residual(
+          filters=16, strides=(1, 1), input_shape=input_shape)
+      dx_tr, grads_tr, vars_tr = residual.backward_grads_and_vars(
+          x, dy=dy, training=True)
+      dx_ev, grads_ev, vars_ev = residual.backward_grads_and_vars(
+          x, dy=dy, training=False)
+      self.assertNotAllClose(dx_tr, dx_ev)
+      self.assertTrue(isinstance(grads_tr, list))
+      self.assertTrue(isinstance(grads_ev, list))
+      self.assertTrue(isinstance(vars_tr, list))
+      self.assertTrue(isinstance(vars_ev, list))
+      for grad_tr, var_tr, grad_ev, var_ev in zip(grads_tr, vars_tr, grads_ev,
+                                                  vars_ev):
+        if grad_tr is not None:  # Batch norm moving mean, var gives None grad
+          self.assertEqual(grad_tr.shape, grad_ev.shape)
+          self.assertEqual(var_tr.shape, var_ev.shape)
+          self.assertEqual(grad_tr.shape, var_tr.shape)
+
+  def test_backward_grads_and_vars_channels_last(self):
+    """Test `backward_grads` function with `channels_last` data format."""
+    with tf.device("/cpu:0"):  # NHWC format
+      input_shape = (224, 224, 16)
+      data_shape = (16,) + input_shape
+      x = tf.random_normal(shape=data_shape)
+      dy = tf.random_normal(shape=data_shape)
+      residual = blocks._Residual(
+          filters=16,
+          strides=(1, 1),
+          input_shape=input_shape,
+          data_format="channels_last")
+      dx_tr, grads_tr, vars_tr = residual.backward_grads_and_vars(
+          x, dy=dy, training=True)
+      dx_ev, grads_ev, vars_ev = residual.backward_grads_and_vars(
+          x, dy=dy, training=False)
+      self.assertNotAllClose(dx_tr, dx_ev)
+      self.assertTrue(isinstance(grads_tr, list))
+      self.assertTrue(isinstance(grads_ev, list))
+      self.assertTrue(isinstance(vars_tr, list))
+      self.assertTrue(isinstance(vars_ev, list))
+      for grad_tr, var_tr, grad_ev, var_ev in zip(grads_tr, vars_tr, grads_ev,
+                                                  vars_ev):
+        if grad_tr is not None:  # Batch norm moving mean, var gives None grad
+          self.assertEqual(grad_tr.shape, grad_ev.shape)
+          self.assertEqual(var_tr.shape, var_ev.shape)
+          self.assertEqual(grad_tr.shape, var_tr.shape)
+
+
+class _ResidualInnerTest(tf.test.TestCase):
+
+  def test_call(self):
+    """Test `call` function."""
+
+    _validate_block_call_channels_first(blocks._ResidualInner, self)
+    _validate_block_call_channels_last(blocks._ResidualInner, self)
+
+
+class _BottleneckResidualInner(tf.test.TestCase):
+
+  def test_call(self):
+    """Test `call` function."""
+
+    _validate_block_call_channels_first(blocks._BottleneckResidualInner, self)
+    _validate_block_call_channels_last(blocks._BottleneckResidualInner, self)
+
+
+if __name__ == "__main__":
+  tf.enable_eager_execution()
+  tf.test.main()
---
+++ tensorflow/contrib/eager/python/examples/revnet/revnet.py
@@ -0,0 +1,263 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Reversible residual network compatible with eager execution.
+
+Code for main model.
+
+Reference [The Reversible Residual Network: Backpropagation
+Without Storing Activations](https://arxiv.org/pdf/1707.04585.pdf)
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+import operator
+
+import tensorflow as tf
+from tensorflow.contrib.eager.python.examples.revnet import blocks
+
+
+# Global Conventions:
+# 1) Default data format is NCWH, targeting GPU
+# 2) Each block has attribute axis, inferred from data_format
+# 3) Default training option to True for batch normalization
+class RevNet(tf.keras.Model):
+  """RevNet that depends on all the blocks."""
+
+  def __init__(self, config):
+    """Initialize RevNet with building blocks.
+
+    Args:
+      config: tf.contrib.training.HParams object; specifies hyperparameters
+    """
+    super(RevNet, self).__init__()
+    self.axis = 1 if config.data_format == "channels_first" else 3
+    self.config = config
+
+    self._init_block = self._construct_init_block()
+    self._block_list = self._construct_intermediate_blocks()
+    self._final_block = self._construct_final_block()
+
+  def _construct_init_block(self):
+    init_block = tf.keras.Sequential(
+        [
+            tf.keras.layers.Conv2D(
+                filters=self.config.init_filters,
+                kernel_size=self.config.init_kernel,
+                strides=(self.config.init_stride, self.config.init_stride),
+                data_format=self.config.data_format,
+                use_bias=False,
+                padding="SAME",
+                input_shape=self.config.input_shape),
+            tf.keras.layers.BatchNormalization(
+                axis=self.axis, fused=self.config.fused),
+            tf.keras.layers.LeakyReLU(alpha=0.)
+        ],
+        name="init")
+    if self.config.init_max_pool:
+      init_block.add(
+          tf.keras.layers.MaxPooling2D(
+              pool_size=(3, 3),
+              strides=(2, 2),
+              padding="SAME",
+              data_format=self.config.data_format))
+    return init_block
+
+  def _construct_final_block(self):
+    f = self.config.filters[-1]  # Number of filters
+    r = functools.reduce(operator.mul, self.config.strides, 1)  # Reduce ratio
+    r *= self.config.init_stride
+    if self.config.init_max_pool:
+      r *= 2
+
+    if self.config.data_format == "channels_first":
+      w, h = self.config.input_shape[1], self.config.input_shape[2]
+      input_shape = (f, w // r, h // r)
+    elif self.config.data_format == "channels_last":
+      w, h = self.config.input_shape[0], self.config.input_shape[1]
+      input_shape = (w // r, h // r, f)
+    else:
+      raise ValueError("Data format should be either `channels_first`"
+                       " or `channels_last`")
+
+    final_block = tf.keras.Sequential(
+        [
+            tf.keras.layers.BatchNormalization(
+                axis=self.axis,
+                input_shape=input_shape,
+                fused=self.config.fused),
+            tf.keras.layers.LeakyReLU(alpha=0.),  # Vanilla ReLU
+            tf.keras.layers.GlobalAveragePooling2D(
+                data_format=self.config.data_format),
+            tf.keras.layers.Dense(self.config.n_classes)
+        ],
+        name="final")
+    return final_block
+
+  def _construct_intermediate_blocks(self):
+    # Precompute input shape after initial block
+    stride = self.config.init_stride
+    if self.config.init_max_pool:
+      stride *= 2
+    if self.config.data_format == "channels_first":
+      w, h = self.config.input_shape[1], self.config.input_shape[2]
+      input_shape = (self.config.init_filters, w // stride, h // stride)
+    else:
+      w, h = self.config.input_shape[0], self.config.input_shape[1]
+      input_shape = (w // stride, h // stride, self.config.init_filters)
+
+    # Aggregate intermediate blocks
+    block_list = tf.contrib.checkpoint.List()
+    for i in range(self.config.n_rev_blocks):
+      # RevBlock configurations
+      n_res = self.config.n_res[i]
+      filters = self.config.filters[i]
+      if filters % 2 != 0:
+        raise ValueError("Number of output filters must be even to ensure"
+                         "correct partitioning of channels")
+      stride = self.config.strides[i]
+      strides = (self.config.strides[i], self.config.strides[i])
+
+      # Add block
+      rev_block = blocks.RevBlock(
+          n_res,
+          filters,
+          strides,
+          input_shape,
+          batch_norm_first=(i != 0),  # Only skip on first block
+          data_format=self.config.data_format,
+          bottleneck=self.config.bottleneck,
+          fused=self.config.fused)
+      block_list.append(rev_block)
+
+      # Precompute input shape for the next block
+      if self.config.data_format == "channels_first":
+        w, h = input_shape[1], input_shape[2]
+        input_shape = (filters, w // stride, h // stride)
+      else:
+        w, h = input_shape[0], input_shape[1]
+        input_shape = (w // stride, h // stride, filters)
+
+    return block_list
+
+  def call(self, inputs, training=True):
+    """Forward pass."""
+
+    # Only store hidden states during training
+    if training:
+      saved_hidden = [inputs]
+
+    h = self._init_block(inputs, training=training)
+    if training:
+      saved_hidden.append(h)
+
+    for block in self._block_list:
+      h = block(h, training=training)
+      if training:
+        saved_hidden.append(h)
+
+    logits = self._final_block(h, training=training)
+
+    return (logits, saved_hidden) if training else (logits, None)
+
+  def compute_loss(self, logits, labels):
+    """Compute cross entropy loss."""
+
+    cross_ent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=logits, labels=labels)
+
+    return tf.reduce_mean(cross_ent)
+
+  def compute_gradients(self, inputs, labels, training=True):
+    """Manually computes gradients.
+
+    Args:
+      inputs: Image tensor, either NHWC or NCHW, conforming to `data_format`
+      labels: One-hot labels for classification
+      training: for batch normalization
+
+    Returns:
+      list of tuple each being (grad, var) for optimizer use
+    """
+
+    # Forward pass record hidden states before downsampling
+    _, saved_hidden = self.call(inputs, training=training)
+
+    grads_all = []
+    vars_all = []
+
+    # Manually backprop through last block
+    x = saved_hidden[-1]
+    with tf.GradientTape() as tape:
+      tape.watch(x)
+      logits = self._final_block(x, training=training)
+      cost = self.compute_loss(logits, labels)
+
+    grads_combined = tape.gradient(cost, [x] + self._final_block.variables)
+    dy, grads_ = grads_combined[0], grads_combined[1:]
+    grads_all += grads_
+    vars_all += self._final_block.variables
+
+    # Manually backprop through intermediate blocks
+    for block in reversed(self._block_list):
+      y = saved_hidden.pop()
+      x = saved_hidden[-1]
+      dy, grads, vars_ = block.backward_grads_and_vars(
+          x, y, dy, training=training)
+      grads_all += grads
+      vars_all += vars_
+
+    # Manually backprop through first block
+    saved_hidden.pop()
+    x = saved_hidden.pop()
+    assert not saved_hidden  # Cleared after backprop
+
+    with tf.GradientTape() as tape:
+      y = self._init_block(x, training=training)  # Recomputing
+
+    grads_all += tape.gradient(
+        y, self._init_block.variables, output_gradients=[dy])
+    vars_all += self._init_block.variables
+
+    return grads_all, vars_all
+
+  def train_step(self,
+                 inputs,
+                 labels,
+                 optimizer,
+                 global_step=None,
+                 report=False):
+    """Train for one iteration."""
+
+    grads_all, vars_all = self.compute_gradients(inputs, labels, training=True)
+    optimizer.apply_gradients(zip(grads_all, vars_all), global_step=global_step)
+
+    if report:
+      logits, _ = self.call(inputs, training=True)
+      loss = self.compute_loss(logits, labels)
+
+      return loss
+
+  def eval_step(self, inputs, labels):
+    """Evaluate."""
+
+    logits, _ = self.call(inputs, training=False)
+    preds = tf.cast(tf.argmax(logits, axis=1), tf.int32)
+    corrects = tf.cast(tf.equal(preds, labels), tf.float32)
+    accuracy = tf.reduce_mean(corrects)
+
+    return accuracy
