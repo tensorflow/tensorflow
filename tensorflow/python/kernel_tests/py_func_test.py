4d8a66c5b29428b709f4f54b566a44902ea8173e
Asim Shankar <ashankar@google.com>
2018-06-15 16:26:35 -0700

    [py_func]: Fix #20021
    * EagerPyFunc now validates its assumption that returned tensors are backed by memory on the same device that the EagerPyFunc kernel executed on.
    * Make the Python trampolining mechanism ensure that this requirement of the kernel is met.
    * Allow tf.contrib.eager.py_func to execute correctly on devices other than CPU and GPU:0.
    
    Prior to this change, tf.contrib.eager.py_func() would copy data from CPU to GPU:0 if necessary, but not the other way around. As a result, the assumptions made by the EagerPyFunc kernel implementation about the placement of returned tensors would be violated.
    
    The test added in py_func_test.py, when executed on a machine with a GPU will:
    
    - Fail with a segmentation fault (dereferencing GPU memory) without the changes to py_func.cc and script_ops.py
    - Fail with an error message with the change to py_func.cc but without the change to script_ops.py
    - Pass with changes to py_func.cc and script_ops.py
    
    PiperOrigin-RevId: 200792057

--- tensorflow/python/kernel_tests/py_func_test.py
+++ tensorflow/python/kernel_tests/py_func_test.py
@@ -599,6 +599,25 @@ class PyFuncTest(test.TestCase):
       self.assertEqual(y, 1.0)
       self.assertEqual(dy_dx, 2.0)
 
+  def testEagerRespectsDevicePlacmentOfOp(self):
+
+    def f(x):
+      return math_ops.square(x)
+
+    def g(x):
+      return math_ops.add(x, x)
+
+    with ops.device("/CPU:0"):
+      # Explicitly ask for the py_funcs to execute on CPU, even if
+      # a GPU is available.
+      x = array_ops.placeholder(dtypes.float32)
+      y = script_ops.eager_py_func(func=f, inp=[x], Tout=dtypes.float32)
+      z = script_ops.eager_py_func(func=g, inp=[y], Tout=dtypes.float32)
+
+    with self.test_session(use_gpu=True) as sess:
+      output = sess.run(z, feed_dict={x: 3.0})
+      self.assertEqual(output, 18.0)
+
 
 if __name__ == "__main__":
   test.main()
