03ac7e07b9db83d9ec79717a1b9fe9debc10b310
Yong Tang <yong.tang.github@outlook.com>
2018-06-19 20:36:45 +0000

    Remove tensorflow_ENABLE_GCS_SUPPORT
    As GCS requires curl so it is not possible to have GCS
    support now.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/contrib/cmake/tf_core_framework.cmake
+++ tensorflow/contrib/cmake/tf_core_framework.cmake
@@ -233,14 +233,6 @@ if(WIN32)
   list(APPEND tf_core_lib_srcs ${tf_core_platform_windows_srcs})
 endif(WIN32)
 
-if(tensorflow_ENABLE_GCS_SUPPORT)
-  file(GLOB tf_core_platform_cloud_srcs
-      "${tensorflow_source_dir}/tensorflow/core/platform/cloud/*.h"
-      "${tensorflow_source_dir}/tensorflow/core/platform/cloud/*.cc"
-  )
-  list(APPEND tf_core_lib_srcs ${tf_core_platform_cloud_srcs})
-endif()
-
 if (tensorflow_ENABLE_HDFS_SUPPORT)
   list(APPEND tf_core_platform_hdfs_srcs
       "${tensorflow_source_dir}/tensorflow/core/platform/hadoop/hadoop_file_system.cc"
