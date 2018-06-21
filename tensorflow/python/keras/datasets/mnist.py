dde16faf1f6676c3e7d3dcf997aff8c8492b328d
Vikram <vikramtheone1@gmail.com>
2018-06-20 22:58:04 -0700

    Update mnist.py
    using `with` to be consistent with other code in datasets
    
    might also be useful in case file is not present due to corruption of something else

--- tensorflow/python/keras/datasets/mnist.py
+++ tensorflow/python/keras/datasets/mnist.py
@@ -47,8 +47,8 @@ def load_data(path='mnist.npz'):
       path,
       origin=origin_folder + 'mnist.npz',
       file_hash='8a61469f7ea1b51cbae51d4f78837e45')
-  f = np.load(path)
-  x_train, y_train = f['x_train'], f['y_train']
-  x_test, y_test = f['x_test'], f['y_test']
-  f.close()
+  with np.load(path) as f:
+    x_train, y_train = f['x_train'], f['y_train']
+    x_test, y_test = f['x_test'], f['y_test']
+    
   return (x_train, y_train), (x_test, y_test)
