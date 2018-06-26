6f251e4a8b6b5997dafbd178e7b8dac274008688
gracehoney <31743510+aaroey@users.noreply.github.com>
2018-06-26 11:26:39 -0700

    Fix tf_trt_integration_test.py when running in python3.

--- tensorflow/contrib/tensorrt/test/tf_trt_integration_test.py
+++ tensorflow/contrib/tensorrt/test/tf_trt_integration_test.py
@@ -186,10 +186,10 @@ class TfTrtIntegrationTest(test_util.TensorFlowTestCase):
       # Defaults to 2 runs to verify result across multiple runs is same.
       for _ in range(num_runs):
         new_val = sess.run(out, {inp: input_data})
-        self.assertEquals(TEST_GRAPHS[graph_key].expected_output_dims,
-                          new_val.shape)
+        self.assertEqual(TEST_GRAPHS[graph_key].expected_output_dims,
+                         new_val.shape)
         if val is not None:
-          self.assertAllEqual(new_val, val)
+          self.assertAllEqual(val, new_val)
         val = new_val
     return val
 
@@ -220,19 +220,18 @@ class TfTrtIntegrationTest(test_util.TensorFlowTestCase):
     for n in gdef.node:
       if n.op == "TRTEngineOp":
         num_engines += 1
-        self.assertNotEqual("", n.attr["serialized_segment"].s)
-        self.assertNotEqual("", n.attr["segment_funcdef_name"].s)
-        self.assertEquals(n.attr["precision_mode"].s, precision_mode)
-        self.assertEquals(n.attr["static_engine"].b, not dynamic_engine)
+        self.assertNotEqual(b"", n.attr["serialized_segment"].s)
+        self.assertNotEqual(b"", n.attr["segment_funcdef_name"].s)
+        self.assertEqual(to_bytes(precision_mode), n.attr["precision_mode"].s)
+        self.assertEqual(not dynamic_engine, n.attr["static_engine"].b)
         if precision_mode == MODE_INT8 and is_calibrated:
-          self.assertNotEqual("", n.attr["calibration_data"].s)
+          self.assertNotEqual(b"", n.attr["calibration_data"].s)
         else:
-          self.assertEquals("", n.attr["calibration_data"].s)
-    if precision_mode is None:
-      self.assertEquals(num_engines, 0)
+          self.assertEqual(b"", n.attr["calibration_data"].s)
+    if precision_mode is None:  # This is for original input GraphDef.
+      self.assertEqual(0, num_engines)
     else:
-      self.assertEquals(num_engines,
-                        TEST_GRAPHS[graph_key].num_expected_engines)
+      self.assertEqual(TEST_GRAPHS[graph_key].num_expected_engines, num_engines)
 
   def _RunTest(self, graph_key, use_optimizer, precision_mode,
                dynamic_infer_engine, dynamic_calib_engine):
