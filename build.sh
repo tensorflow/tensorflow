b9b16406021aed03301dfa6a2d144a1fdfb360a9
Anton Dmitriev <dmitrievanthony@gmail.com>
2018-07-05 12:08:07 +0300

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 bazel build --config=opt --copt="-mtune=generic" //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-sudo /home/ubuntu/.local/bin/pip3 uninstall tensorflow
-sudo /home/ubuntu/.local/bin/pip3 install /tmp/tensorflow_pkg/tensorflow-1.9.0rc0-cp35-cp35m-linux_x86_64.whl
+cp /tmp/tensorflow_pkg/tensorflow-1.9.0rc0-cp35-cp35m-linux_x86_64.whl /tensorflow/tensorflow-1.9.0rc0-py3-none-linux_x86_64.whl
+#sudo /home/ubuntu/.local/bin/pip3 uninstall tensorflow
+#sudo /home/ubuntu/.local/bin/pip3 install /tmp/tensorflow_pkg/tensorflow-1.9.0rc0-cp35-cp35m-linux_x86_64.whl
