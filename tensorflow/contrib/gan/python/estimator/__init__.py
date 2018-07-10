47dea684efa41981e10299c2737317c504ce41af
A. Unique TensorFlower <gardener@tensorflow.org>
2018-07-10 02:34:40 -0700

    [tfgan] Deprecate GANHead and simplify GANEstimator.
    PiperOrigin-RevId: 203914111

--- tensorflow/contrib/gan/python/estimator/__init__.py
+++ tensorflow/contrib/gan/python/estimator/__init__.py
@@ -25,16 +25,13 @@ from __future__ import print_function
 # Collapse `estimator` into a single namespace.
 # pylint: disable=unused-import,wildcard-import
 from tensorflow.contrib.gan.python.estimator.python import gan_estimator
-from tensorflow.contrib.gan.python.estimator.python import head
 
 from tensorflow.contrib.gan.python.estimator.python.gan_estimator import *
-from tensorflow.contrib.gan.python.estimator.python.head import *
 # pylint: enable=unused-import,wildcard-import
 
 from tensorflow.python.util.all_util import remove_undocumented
 
 _allowed_symbols = [
     'gan_estimator',
-    'head',
-] + gan_estimator.__all__ + head.__all__
+] + gan_estimator.__all__
 remove_undocumented(__name__, _allowed_symbols)
