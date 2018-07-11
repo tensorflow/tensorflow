308612f702d6eb885a6244bea998e100f7d5ae35
Jiri Simsa <jsimsa@google.com>
2018-07-11 11:40:37 -0700

    [tf.data] Remove outdated comment.
    PiperOrigin-RevId: 204160548

--- tensorflow/core/kernels/data/optimize_dataset_op.cc
+++ tensorflow/core/kernels/data/optimize_dataset_op.cc
@@ -92,8 +92,6 @@ class OptimizeDatasetOp : public UnaryDatasetOpKernel {
       TF_RETURN_IF_ERROR(ImportGraphDef({}, graph_def, &graph, nullptr));
       std::vector<Tensor> outputs;
       GraphRunner graph_runner(ctx->function_library()->device());
-      // Once rewrites that add/modify functions are introduced, we will need
-      // persist the results in a function library runtime.
       TF_RETURN_IF_ERROR(graph_runner.Run(&graph, ctx->function_library(), {},
                                           {output_node}, &outputs));
       TF_RETURN_IF_ERROR(GetDatasetFromVariantTensor(outputs[0], &input_));
