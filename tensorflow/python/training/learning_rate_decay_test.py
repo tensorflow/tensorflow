83bb94b25cfdde0cba1bc544072a6f44d2859acd
Mahmoud Aslan <mahmoudaslan@outlook.com>
2018-07-13 19:23:40 +0300

    Add functional test for cyclic_learning_rate
    Added functional tests for the three policies of cyclic_learning_rate:
    1- test_triangular
    2- test_triangular2
    3- test_exp_range

--- tensorflow/python/training/learning_rate_decay_test.py
+++ tensorflow/python/training/learning_rate_decay_test.py
@@ -29,6 +29,68 @@ from tensorflow.python.platform import googletest
 from tensorflow.python.training import learning_rate_decay
 
 
+class CyclicLearningRateTest(test_util.TensorFlowTestCase):
+  """Functional test for Cyclic Learning Rate"""
+  
+  def np_cyclic_learning_rate(self, step, lr, max_lr, step_size, mode):
+    cycle = math.floor(1. + step / (2. * step_size))
+    x = math.fabs(step / step_size - 2. * cycle + 1.)
+    clr = (max_lr - lr) * max(0., 1. - x)
+    if mode == 'triangular2':
+      clr = clr/(math.pow(2, (cycle - 1)))
+    if mode == 'exp_range':
+      clr = clr * math.pow(.99994, step)
+    return clr + lr
+
+  @test_util.run_in_graph_and_eager_modes()
+  def test_triangular(self):
+    step = 5
+    lr = 0.01
+    max_lr = 0.1
+    step_size = 20.
+    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                         lr, max_lr,
+                                                         step_size,
+                                                         mode='triangular')
+    expected = self.np_cyclic_learning_rate(step,
+                                            lr, max_lr,
+                                            step_size,
+                                            mode='triangular')
+    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+
+  @test_util.run_in_graph_and_eager_modes()
+  def test_triangular2(self):
+    step = 5
+    lr = 0.01
+    max_lr = 0.1
+    step_size = 20.
+    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                         lr, max_lr,
+                                                         step_size,
+                                                         mode='triangular2')
+    expected = self.np_cyclic_learning_rate(step,
+                                            lr, max_lr,
+                                            step_size,
+                                            mode='triangular2')
+    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+
+  @test_util.run_in_graph_and_eager_modes()
+  def test_exp_range(self):
+    step = 5
+    lr = 0.01
+    max_lr = 0.1
+    step_size = 20.
+    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                         lr, max_lr,
+                                                         step_size,
+                                                         mode='exp_range')
+    expected = self.np_cyclic_learning_rate(step,
+                                            lr, max_lr,
+                                            step_size,
+                                            mode='exp_range')
+    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+
+
 class LRDecayTest(test_util.TensorFlowTestCase):
 
   @test_util.run_in_graph_and_eager_modes
