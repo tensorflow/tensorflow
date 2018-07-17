e209107920f8d6fe63e2f410d55be40a4c7a2d94
A. Unique TensorFlower <gardener@tensorflow.org>
2018-07-17 09:16:33 -0700

    Quick clarification about the stack nature of the default graph context.
    PiperOrigin-RevId: 204921538

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -3558,9 +3558,13 @@ class Graph(object):
     This method should be used if you want to create multiple graphs
     in the same process. For convenience, a global default graph is
     provided, and all ops will be added to this graph if you do not
-    create a new graph explicitly. Use this method with the `with` keyword
-    to specify that ops created within the scope of a block should be
-    added to this graph.
+    create a new graph explicitly.
+
+    Use this method with the `with` keyword to specify that ops created within
+    the scope of a block should be added to this graph. In this case, once
+    the scope of the `with` is exited, the previous default graph is set again
+    as default. There is a stack, so it's ok to have multiple nested levels
+    of `as_default` calls.
 
     The default graph is a property of the current thread. If you
     create a new thread, and wish to use the default graph in that
