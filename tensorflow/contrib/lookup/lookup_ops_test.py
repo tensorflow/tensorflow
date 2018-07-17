54653810d18ea28c4d9fb82736b3529e84d1128c
Yong Tang <yong.tang.github@outlook.com>
2018-07-17 15:06:49 +0000

    Add test case for int32/string(k/v) of tf.contrib.lookup.HashTable
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/contrib/lookup/lookup_ops_test.py
+++ tensorflow/contrib/lookup/lookup_ops_test.py
@@ -280,6 +280,21 @@ class HashTableOpTest(test.TestCase):
       table.init.run()
       self.assertAllEqual(3, table.size().eval())
 
+  def testHashTableInt32String(self):
+    with self.test_session():
+      default_val = "n/a"
+      keys = constant_op.constant([0, 1, 2], dtypes.int32)
+      values = constant_op.constant(["brain", "salad", "surgery"])
+      table = lookup.HashTable(
+          lookup.KeyValueTensorInitializer(keys, values), default_val)
+      table.init.run()
+
+      input_tensor = constant_op.constant([0, 1, -1])
+      output = table.lookup(input_tensor)
+
+      result = output.eval()
+      self.assertAllEqual(["brain", "salad", "n/a"], result)
+
 
 class MutableHashTableOpTest(test.TestCase):
 
