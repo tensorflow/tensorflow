8ab438afcd1ef9de838871c2a03c2d9876d398f7
Mark Heffernan <meheff@google.com>
2018-07-17 18:01:52 -0700

    Add single-sided host send and receive operations. Adds a bit on kSend/kReceive instructions and their Done variants indicated whether the operations communicates with the host or another device (the default). Host send/recv operations are single-sided without a complementary recv/send instruction in another module.
    Host send/recv operations are exposed in the XLA builder API as SendToHost and RecvFromHost.
    
    PiperOrigin-RevId: 205008138

--- tensorflow/compiler/xla/service/hlo_verifier.h
+++ tensorflow/compiler/xla/service/hlo_verifier.h
@@ -102,10 +102,6 @@ class ShapeVerifier : public DfsHloVisitor {
   Status CheckTernaryShape(const HloInstruction* instruction);
   Status CheckVariadicShape(const HloInstruction* instruction);
 
-  // Checks if the given two instructions share the same channel id.
-  Status CheckSameChannel(const HloInstruction* instr1,
-                          const HloInstruction* instr2);
-
  private:
   // Whether the inputs and output of an instruction can contain both F32s and
   // BF16s. Tuples that include both F32s and BF16s are allowed regardless of
