9a3afbae51ac2b28cba0b75e3a8a2e3f6d4ea2fa
A. Unique TensorFlower <gardener@tensorflow.org>
2018-07-19 13:48:50 -0700

    Improve Windows build process
    After this change, the windows build steps should be like:
    https://docs.google.com/document/d/1oVYzPJVv8r5N9PecqwG74rY_QbqPH70IxBouBHdq5EI/edit?usp=sharing
    
    PiperOrigin-RevId: 205295588

--- tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
@@ -67,15 +67,11 @@ for ARG in "$@"; do
 done
 
 if [[ "$release_build" != 1 ]]; then
-  # --define=override_eigen_strong_inline=true speeds up the compiling of conv_grad_ops_3d.cc and conv_ops_3d.cc
+  # Overriding eigen strong inline speeds up the compiling of conv_grad_ops_3d.cc and conv_ops_3d.cc
   # by 20 minutes. See https://github.com/tensorflow/tensorflow/issues/10521
-  # Because this hurts the performance of TF, we don't enable it in release build.
-  echo "build --define=override_eigen_strong_inline=true" >> "${TMP_BAZELRC}"
+  # Because this hurts the performance of TF, we don't override it in release build.
+  export TF_OVERRIDE_EIGEN_STRONG_INLINE=0
 fi
-
-# The host and target platforms are the same in Windows build. So we don't have
-# to distinct them. This helps avoid building the same targets twice.
-echo "build --distinct_host_configuration=false" >> "${TMP_BAZELRC}"
 
 # Enable short object file path to avoid long path issue on Windows.
 echo "startup --output_user_root=${TMPDIR}" >> "${TMP_BAZELRC}"
