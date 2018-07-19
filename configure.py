8997c7cc0f2a97e46687d7442eba9d8183716c23
Reed Wanderman-Milne <reedwm@google.com>
2018-07-19 15:52:35 -0700

    Update minimum bazel version to 0.13.0.
    I was using bazel 0.12.0 and was getting the error:
    
    file '@bazel_tools//tools/cpp:windows_cc_configure.bzl' does not contain symbol 'setup_vc_env_vars'
    
    PiperOrigin-RevId: 205316270

--- configure.py
+++ configure.py
@@ -1451,7 +1451,7 @@ def main():
   # environment variables.
   environ_cp = dict(os.environ)
 
-  check_bazel_version('0.10.0')
+  check_bazel_version('0.13.0')
 
   reset_tf_configure_bazelrc(args.workspace)
   cleanup_makefile()
