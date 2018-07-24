885206514f1ea0681418692603f3853917de07b5
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-07-24 11:09:44 +0800

    TST: add test case

--- tensorflow/python/training/saver_test.py
+++ tensorflow/python/training/saver_test.py
@@ -768,6 +768,30 @@ class SaverTest(test.TestCase):
       self.assertEqual(20.0, v1.eval())
       save.save(sess, save_path)
 
+  def testSaveRestoreAndValidateVariableDtype(self):
+    for variable_op in [variables.Variable,
+                        resource_variable_ops.ResourceVariable]:
+      save_path = os.path.join(self.get_temp_dir(), "basic_save_restore")
+
+      # Build the first session.
+      with self.test_session(graph=ops_lib.Graph()) as sess:
+        v0 = variable_op(10.0, name="v0", dtype=dtypes.float32)
+
+        if not context.executing_eagerly():
+          self.evaluate([variables.global_variables_initializer()])
+
+        save = saver_module.Saver({"v0": v0})
+        save.save(sess, save_path)
+
+      # Start a second session.
+      with self.test_session(graph=ops_lib.Graph()) as sess:
+        v0_wrong_dtype = variable_op(1, name="v0", dtype=dtypes.int32)
+        # Restore the saved value with different dtype
+        # in the parameter nodes.
+        save = saver_module.Saver({"v0": v0_wrong_dtype})
+        with self.assertRaises(errors.InvalidArgumentError):
+          save.restore(sess, save_path)
+
 
 class SaveRestoreShardedTest(test.TestCase):
 
