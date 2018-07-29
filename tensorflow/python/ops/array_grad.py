c22b5c678a42474fbc9aab59345ac09eeb685c37
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-07-29 17:20:45 +0800

    CLN: remove unused import

--- tensorflow/python/ops/array_grad.py
+++ tensorflow/python/ops/array_grad.py
@@ -18,9 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from math import ceil
-import numpy as np
-
 from tensorflow.python import pywrap_tensorflow
 from tensorflow.python.eager import context
 from tensorflow.python.framework import constant_op
