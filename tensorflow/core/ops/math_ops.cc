19eeef040a393b0b4e32b23a4d1525fb3cba0979
Yong Tang <yong.tang.github@outlook.com>
2018-07-30 20:54:29 +0000

    Also enables int64 support for BatchMatMul,
    as test cases uses BatchMatMul as well
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/core/ops/math_ops.cc
+++ tensorflow/core/ops/math_ops.cc
@@ -65,7 +65,9 @@ REGISTER_OP("BatchMatMul")
     .Input("x: T")
     .Input("y: T")
     .Output("output: T")
-    .Attr("T: {bfloat16, half, float, double, int32, complex64, complex128}")
+    .Attr(
+        "T: {bfloat16, half, float, double, int32, int64, complex64, "
+        "complex128}")
     .Attr("adj_x: bool = false")
     .Attr("adj_y: bool = false")
     .SetShapeFn([](InferenceContext* c) {
