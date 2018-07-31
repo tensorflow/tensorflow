4451c5795a35ad416b4f80f058cd1d69ef3d072d
Jacques Pienaar <jpienaar@google.com>
2018-07-31 15:40:09 -0700

    Add . to bare-id to allow custom ops such as tf.add
    PiperOrigin-RevId: 206840659

--- lib/Parser/Lexer.cpp
+++ lib/Parser/Lexer.cpp
@@ -155,13 +155,13 @@ Token Lexer::lexComment() {
 
 /// Lex a bare identifier or keyword that starts with a letter.
 ///
-///   bare-id ::= (letter|[_]) (letter|digit|[_$])*
+///   bare-id ::= (letter|[_]) (letter|digit|[_$.])*
 ///   integer-type ::= `i[1-9][0-9]*`
 ///
 Token Lexer::lexBareIdentifierOrKeyword(const char *tokStart) {
-  // Match the rest of the identifier regex: [0-9a-zA-Z_$]*
+  // Match the rest of the identifier regex: [0-9a-zA-Z_.$]*
   while (isalpha(*curPtr) || isdigit(*curPtr) || *curPtr == '_' ||
-         *curPtr == '$')
+         *curPtr == '$' || *curPtr == '.')
     ++curPtr;
 
   // Check to see if this identifier is a keyword.
