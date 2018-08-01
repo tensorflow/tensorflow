4b6061d7c0984614da436287ee2bbf7f64f01f26
Peng <peng.sun@amd.com>
2018-08-01 16:35:08 -0500

    Add dnn_linear_combined_test to whitelist

--- tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
@@ -42,6 +42,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     //tensorflow/... -//tensorflow/compiler/... -//tensorflow/contrib/... \
     -//tensorflow/python/distribute:distribute_coordinator_test \
     -//tensorflow/python/eager:pywrap_tfe_test \
+    -//tensorflow/python/estimator:dnn_linear_combined_test \
     -//tensorflow/python/estimator:estimator_test \
     -//tensorflow/python/keras:activations_test \
     -//tensorflow/python/keras:cudnn_recurrent_test \
