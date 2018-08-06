a0c653b684afa859e7c468fea4b5e600271eec0f
Piotr Padlewski <prazek@google.com>
2018-08-06 16:01:45 -0700

    Map and filter fusion
    This patch introduces MapAndFilterFusion optimization, e.g:
    map(map_fun).filter(filter_fun)
    
    It moves computation of filter_fun to the map_fun, which returns the computed element and computed predicate. Filter is replaced with FilterByLastComponentDataset, which
    filters by the boolean, resulting in:
    
    map(fused_fun).FilterByLastComponent()
    where:
      fused_fun(x):
        r = map_function(x)
        return (r, filter_fun(r))
    
    The micro benchmark shows around 68% improvement (chain of 50 map-filter)
    
    PiperOrigin-RevId: 207624667

--- tensorflow/contrib/data/python/kernel_tests/optimize_dataset_op_test.py
+++ tensorflow/contrib/data/python/kernel_tests/optimize_dataset_op_test.py
@@ -23,7 +23,10 @@ from tensorflow.contrib.data.python.kernel_tests import stats_dataset_test_base
 from tensorflow.contrib.data.python.ops import optimization
 from tensorflow.contrib.data.python.ops import stats_ops
 from tensorflow.python.data.ops import dataset_ops
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
+from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import test
 
 
@@ -48,8 +51,7 @@ class OptimizeDatasetTest(test.TestCase, parameterized.TestCase):
       with self.assertRaisesRegexp(
           errors.InvalidArgumentError,
           "Asserted Whoops transformation at offset 0 but encountered "
-          "Map transformation instead."
-      ):
+          "Map transformation instead."):
         sess.run(get_next)
 
   def testAssertSuffixShort(self):
@@ -125,19 +127,30 @@ class OptimizeDatasetTest(test.TestCase, parameterized.TestCase):
 
     functions = [identity, increment, increment_and_square]
     tests = []
-
-    for fun1 in functions:
-      for fun2 in functions:
-        tests.append(([fun1, fun2],))
-        for fun3 in functions:
-          tests.append(([fun1, fun2, fun3],))
+    for i, fun1 in enumerate(functions):
+      for j, fun2 in enumerate(functions):
+        tests.append((
+            "test_{}_{}".format(i, j),
+            [fun1, fun2],
+        ))
+        for k, fun3 in enumerate(functions):
+          tests.append((
+              "test_{}_{}_{}".format(i, j, k),
+              [fun1, fun2, fun3],
+          ))
 
     swap = lambda x, n: (n, x)
-    tests.append(([lambda x: (x, 42), swap],))
-    tests.append(([lambda x: (x, 42), swap, swap],))
+    tests.append((
+        "swap1",
+        [lambda x: (x, 42), swap],
+    ))
+    tests.append((
+        "swap2",
+        [lambda x: (x, 42), swap, swap],
+    ))
     return tuple(tests)
 
-  @parameterized.parameters(*map_functions.__func__())
+  @parameterized.named_parameters(*map_functions.__func__())
   def testMapFusion(self, functions):
     dataset = dataset_ops.Dataset.range(5).apply(
         optimization.assert_next(["Map", "Prefetch"]))
@@ -160,6 +173,79 @@ class OptimizeDatasetTest(test.TestCase, parameterized.TestCase):
 
       with self.assertRaises(errors.OutOfRangeError):
         sess.run(get_next)
+
+  @staticmethod
+  def map_and_filter_functions():
+    identity = lambda x: x
+    increment = lambda x: x + 1
+    minus_five = lambda x: x - 5
+
+    def increment_and_square(x):
+      y = x + 1
+      return y * y
+
+    take_all = lambda x: constant_op.constant(True)
+    is_zero = lambda x: math_ops.equal(x, 0)
+    is_odd = lambda x: math_ops.equal(x % 2, 0)
+    greater = lambda x: math_ops.greater(x + 5, 0)
+
+    functions = [identity, increment, minus_five, increment_and_square]
+    filters = [take_all, is_zero, is_odd, greater]
+    tests = []
+
+    for x, fun in enumerate(functions):
+      for y, predicate in enumerate(filters):
+        tests.append(("mixed_{}_{}".format(x, y), fun, predicate))
+
+    # Multi output
+    tests.append(("multiOne", lambda x: (x, x),
+                  lambda x, y: constant_op.constant(True)))
+    tests.append(
+        ("multiTwo", lambda x: (x, 2),
+         lambda x, y: math_ops.equal(x * math_ops.cast(y, dtypes.int64), 0)))
+    return tuple(tests)
+
+  @parameterized.named_parameters(*map_and_filter_functions.__func__())
+  def testMapFilterFusion(self, function, predicate):
+    dataset = dataset_ops.Dataset.range(10).apply(
+        optimization.assert_next(
+            ["Map",
+             "FilterByLastComponent"])).map(function).filter(predicate).apply(
+                 optimization.optimize(["map_and_filter_fusion"]))
+    self._testMapAndFilter(dataset, function, predicate)
+
+  def _testMapAndFilter(self, dataset, function, predicate):
+    iterator = dataset.make_one_shot_iterator()
+    get_next = iterator.get_next()
+    with self.test_session() as sess:
+      for x in range(10):
+        r = function(x)
+        if isinstance(r, tuple):
+          b = predicate(*r)  # Pass tuple as multiple arguments.
+        else:
+          b = predicate(r)
+        if sess.run(b):
+          result = sess.run(get_next)
+          self.assertAllEqual(r, result)
+      with self.assertRaises(errors.OutOfRangeError):
+        sess.run(get_next)
+
+  def testAdditionalInputs(self):
+    a = constant_op.constant(3, dtype=dtypes.int64)
+    b = constant_op.constant(4, dtype=dtypes.int64)
+    some_tensor = math_ops.mul(a, b)
+    function = lambda x: x * x
+
+    def predicate(y):
+      return math_ops.less(math_ops.cast(y, dtypes.int64), some_tensor)
+
+    # We are currently not supporting functions with additional inputs.
+    dataset = dataset_ops.Dataset.range(10).apply(
+        optimization.assert_next(
+            ["Map", "Filter"])).map(function).filter(predicate).apply(
+                optimization.optimize(["map_and_filter_fusion"]))
+
+    self._testMapAndFilter(dataset, function, predicate)
 
 
 class OptimizeStatsDatasetTest(stats_dataset_test_base.StatsDatasetTestBase):
