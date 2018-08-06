7a80fea3adb983f9d4c92f70c17d1a3a4664f4ab
Anna R <annarev@google.com>
2018-08-06 16:32:37 -0700

    Add a way to generate compat.v* modules in create_python_api.py. We need to generate V2 and V1 in one step since V2's "compat" module (e.g. "tf.compat.as_bytes") overlaps with "compat.v1". That is, some of the __init__.py files are *shared*. So, we need to run create_python_api.py just once to generate all. A few additional changes: - Split out a few functions to prevent them from growing too large. - Added output_init_files_test.py to test consistency of api_init_files.bzl and   api_init_files_v1.bzl. Before, this check was done in create_python_api.py   but now that we have different files for V1 and V2 this is getting   cumbersome, so I split it in a separete test.
    PiperOrigin-RevId: 207629449

--- tensorflow/python/tools/api/generator/create_python_api_test.py
+++ tensorflow/python/tools/api/generator/create_python_api_test.py
@@ -26,7 +26,7 @@ from tensorflow.python.tools.api.generator import create_python_api
 from tensorflow.python.util.tf_export import tf_export
 
 
-@tf_export('test_op', 'test_op1')
+@tf_export('test_op', 'test_op1', 'test.test_op2')
 def test_op():
   pass
 
@@ -72,6 +72,9 @@ class CreatePythonApiTest(test.TestCase):
     self.assertTrue(
         expected_import in str(imports),
         msg='%s not in %s' % (expected_import, str(imports)))
+    # Also check that compat.v1 is not added to imports.
+    self.assertFalse('compat.v1' in imports,
+                     msg='compat.v1 in %s' % str(imports.keys()))
 
   def testClassImportIsAdded(self):
     imports = create_python_api.get_api_init_text(
@@ -93,6 +96,18 @@ class CreatePythonApiTest(test.TestCase):
                 'import _TEST_CONSTANT')
     self.assertTrue(expected in str(imports),
                     msg='%s not in %s' % (expected, str(imports)))
+
+  def testCompatModuleIsAdded(self):
+    imports = create_python_api.get_api_init_text(
+        package=create_python_api._DEFAULT_PACKAGE,
+        output_package='tensorflow',
+        api_name='tensorflow',
+        api_version=2,
+        compat_api_versions=[1])
+    self.assertTrue('compat.v1' in imports,
+                    msg='compat.v1 not in %s' % str(imports.keys()))
+    self.assertTrue('compat.v1.test' in imports,
+                    msg='compat.v1.test not in %s' % str(imports.keys()))
 
 
 if __name__ == '__main__':
