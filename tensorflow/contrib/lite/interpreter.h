54d92a58cad8619460889bd1b1ef34df89d1b612
Nupur Garg <nupurgarg@google.com>
2018-08-08 13:33:49 -0700

    Minor fixes to Eager delegate.
    PiperOrigin-RevId: 207937525

--- tensorflow/contrib/lite/interpreter.h
+++ tensorflow/contrib/lite/interpreter.h
@@ -527,12 +527,13 @@ class Interpreter {
                                              TfLiteRegistration** registration);
 
   // WARNING: This is an experimental interface that is subject to change.
-  // Gets an TfLiteIntArray* representing the execution plan. The caller owns
-  // this memory and must free it with TfLiteIntArrayFree().
+  // Gets an TfLiteIntArray* representing the execution plan. The interpreter
+  // owns this memory and it is only guaranteed to exist during the invocation
+  // of the delegate prepare.
   TfLiteStatus GetExecutionPlan(TfLiteIntArray** execution_plan);
 
   // WARNING: This is an experimental interface that is subject to change.
-  // Entry point for C node plugin API to get the execution plan
+  // Entry point for C node plugin API to get the execution plan.
   static TfLiteStatus GetExecutionPlan(struct TfLiteContext* context,
                                        TfLiteIntArray** execution_plan);
 
