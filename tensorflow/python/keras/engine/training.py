38e507f7666f6be34e6eb58bac3fb9140544f925
Tiezhen WANG <wangtz@google.com>
2018-08-07 19:58:35 -0700

    update documentation Calling the parent constructor is needed, otherwise it will fail with the following error: RuntimeError: It looks like you are subclassing `Model` and you forgot to call `super(YourClass, self).__init__()`. Always start with this line. PiperOrigin-RevId: 207823589

--- tensorflow/python/keras/engine/training.py
+++ tensorflow/python/keras/engine/training.py
@@ -74,6 +74,7 @@ class Model(Network):
   class MyModel(tf.keras.Model):
 
     def __init__(self):
+      super(MyModel, self).__init__()
       self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
       self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
 
@@ -94,6 +95,7 @@ class Model(Network):
   class MyModel(tf.keras.Model):
 
     def __init__(self):
+      super(MyModel, self).__init__()
       self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
       self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
       self.dropout = tf.keras.layers.Dropout(0.5)
