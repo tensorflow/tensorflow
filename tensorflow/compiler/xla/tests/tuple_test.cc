4ad236bf12a6737ea9f7f47a2a78b7d211219072
Kay Zhu <kayzhu@google.com>
2018-08-08 22:12:54 -0700

    [XLA] Fix a couple CPU test failures due to use-of-unintialized-value by using CreateFromShape instead to initialize literal.
    PiperOrigin-RevId: 207995946

--- tensorflow/compiler/xla/tests/tuple_test.cc
+++ tensorflow/compiler/xla/tests/tuple_test.cc
@@ -586,7 +586,7 @@ XLA_TEST_F(TupleHloTest,
           }));
   auto expected =
       LiteralUtil::MakeTupleOwned(LiteralUtil::CreateR1<float>({2, 3}));
-  auto literal = MakeUnique<Literal>(expected->shape());
+  auto literal = Literal::CreateFromShape(expected->shape());
   TF_EXPECT_OK(backend().transfer_manager()->TransferLiteralFromOutfeed(
       backend().default_stream_executor(), expected->shape(), *literal));
   EXPECT_TRUE(LiteralTestUtil::Equal(*expected, *literal));
