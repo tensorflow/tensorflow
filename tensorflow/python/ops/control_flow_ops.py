09071384471a4e4d481061eaaefe3e454287007e
Jacques Pienaar <jpienaar@google.com>
2018-08-11 09:36:49 -0700

    Guard zeros by ZerosLikeOutsideLoop by pivot instead of switch.
    The constant should be dependent on the then/else edge of the switch instead of the switch.
    
    PiperOrigin-RevId: 208342636

--- tensorflow/python/ops/control_flow_ops.py
+++ tensorflow/python/ops/control_flow_ops.py
@@ -1449,14 +1449,17 @@ def ZerosLikeOutsideLoop(op, index):
       pred = op_ctxt.pred
       branch = op_ctxt.branch
       switch_val = switch(op.inputs[0], pred)[1 - branch]
+      # A op is created along the branch taken as control dependencies are on
+      # the whole op and not on the tensor output.
+      pivot = array_ops.identity(switch_val)
       if val.dtype == dtypes.resource:
-        with ops.control_dependencies([switch_val]):
+        with ops.control_dependencies([pivot]):
           return array_ops.zeros(
               gen_resource_variable_ops.variable_shape(switch_val))
       zeros_shape = array_ops.shape_internal(switch_val, optimize=False)
       # Ensure ops created within array_ops.zeros are dominated by switch in
       # cond context.
-      with ops.control_dependencies([switch_val]):
+      with ops.control_dependencies([pivot]):
         return array_ops.zeros(zeros_shape, dtype=val.dtype)
     else:
       return array_ops.zeros_like(val, optimize=False)
