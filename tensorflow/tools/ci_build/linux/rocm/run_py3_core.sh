8a6ee60b3e96db838cfccc26c7a066769f5d89ec
Deven Desai <deven.desai.amd@gmail.com>
2018-08-13 23:58:04 +0000

    adding //tensorflow/python/estimator:linear_test to the whitelist as it is flaky

--- tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
@@ -43,6 +43,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     -//tensorflow/python/distribute:distribute_coordinator_test \
     -//tensorflow/python/eager:pywrap_tfe_test \
     -//tensorflow/python/estimator:dnn_linear_combined_test \
+    -//tensorflow/python/estimator:linear_test \
     -//tensorflow/python/keras:cudnn_recurrent_test \
     -//tensorflow/python/keras:pooling_test \
     -//tensorflow/python/keras:model_subclassing_test \
