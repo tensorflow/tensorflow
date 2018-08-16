2b4ec9a8c140ed15caaf334d4027cbc95694bfae
Peng Yu <peng.yu@shopify.com>
2018-08-16 17:17:46 -0400

    add code

--- tensorflow/python/estimator/canned/tensor_forest.py
+++ tensorflow/python/estimator/canned/tensor_forest.py
@@ -35,6 +35,217 @@ VARIANCE_PREDICTION_KEY = 'prediction_variance'
 EPSILON = 0.000001
 
 
+class RandomForestGraphs(object):
+  """Builds TF graphs for random forest training and inference."""
+
+  def __init__(self,
+               params,
+               configs,
+               tree_configs=None,
+               tree_stats=None,
+               training=True):
+    self.params = params
+    self.configs = configs
+    self.device_assigner = framework_variables.VariableDeviceChooser()
+    logging.info('Constructing forest with params = ')
+    logging.info(self.params.__dict__)
+    self.variables = ForestVariables(
+        self.params,
+        device_assigner=self.device_assigner,
+        training=training,
+        tree_configs=tree_configs,
+        tree_stats=tree_stats)
+
+    self.decision_trees = [
+        RandomDecisionTreeGraphs(self.variables[i], self.params, i)
+        for i in range(self.params.num_trees)
+    ]
+
+  def get_all_resource_handles(self):
+    return ([self.variables[i].tree for i in range(len(self.decision_trees))] +
+            [self.variables[i].stats for i in range(len(self.decision_trees))])
+
+  def training_graph(self,
+                     input_data,
+                     input_labels,
+                     num_trainers=1,
+                     trainer_id=0,
+                     **tree_kwargs):
+    """Constructs a TF graph for training a random forest.
+
+    Args:
+      input_data: A tensor or dict of string->Tensor for input data.
+      input_labels: A tensor or placeholder for labels associated with
+        input_data.
+      num_trainers: Number of parallel trainers to split trees among.
+      trainer_id: Which trainer this instance is.
+      **tree_kwargs: Keyword arguments passed to each tree's training_graph.
+
+    Returns:
+      The last op in the random forest training graph.
+
+    Raises:
+      NotImplementedError: If trying to use bagging with sparse features.
+    """
+    tree_graphs = []
+    trees_per_trainer = self.params.num_trees / num_trainers
+    tree_start = int(trainer_id * trees_per_trainer)
+    tree_end = int((trainer_id + 1) * trees_per_trainer)
+    seed = self.configs.tf_random_seed
+
+    for i in range(tree_start, tree_end):
+      with ops.device(self.variables.device_dummies[i].device):
+        if seed is not None:
+          seed += i
+        tree_graphs.append(self.decision_trees[i].training_graph(
+            input_data,
+            label,
+            seed,
+            **tree_kwargs))
+
+    return control_flow_ops.group(*tree_graphs, name='train')
+
+  def inference_graph(self, input_data, **inference_args):
+    """Constructs a TF graph for evaluating a random forest.
+
+    Args:
+      input_data: A tensor or dict of string->Tensor for the input data.
+                  This input_data must generate the same spec as the
+                  input_data used in training_graph:  the dict must have
+                  the same keys, for example, and all tensors must have
+                  the same size in their first dimension.
+      **inference_args: Keyword arguments to pass through to each tree.
+
+    Returns:
+      A tuple of (probabilities, tree_paths, variance).
+
+    Raises:
+      NotImplementedError: If trying to use feature bagging with sparse
+        features.
+    """
+    probabilities = []
+    paths = []
+    for i in range(self.params.num_trees):
+      with ops.device(self.variables.device_dummies[i].device):
+        probs, path = self.decision_trees[i].inference_graph(
+            input_data,
+            **inference_args)
+        probabilities.append(probs)
+        paths.append(path)
+    with ops.device(self.variables.device_dummies[0].device):
+      # shape of all_predict should be [batch_size, num_trees, num_outputs]
+      all_predict = array_ops.stack(probabilities, axis=1)
+      average_values = math_ops.div(
+          math_ops.reduce_sum(all_predict, 1),
+          self.params.num_trees,
+          name='probabilities')
+      tree_paths = array_ops.stack(paths, axis=1)
+
+      expected_squares = math_ops.div(
+          math_ops.reduce_sum(all_predict * all_predict, 1),
+          self.params.num_trees)
+      regression_variance = math_ops.maximum(
+          0., expected_squares - average_values * average_values)
+      return average_values, tree_paths, regression_variance
+
+  def average_size(self):
+    """Constructs a TF graph for evaluating the average size of a forest.
+
+    Returns:
+      The average number of nodes over the trees.
+    """
+    sizes = []
+    for i in range(self.params.num_trees):
+      with ops.device(self.variables.device_dummies[i].device):
+        sizes.append(self.decision_trees[i].size())
+    return math_ops.reduce_mean(math_ops.to_float(array_ops.stack(sizes)))
+
+
+class RandomDecisionTreeGraphs(object):
+  """Builds TF graphs for random tree training and inference."""
+
+  def __init__(self, variables, params, tree_num):
+    self.variables = variables
+    self.params = params
+    self.tree_num = tree_num
+
+  def training_graph(self,
+                     input_data,
+                     input_labels,
+                     random_seed,
+                     data_spec,
+                     sparse_features=None,
+                     input_weights=None):
+    """Constructs a TF graph for training a random tree.
+
+    Args:
+      input_data: A tensor or placeholder for input data.
+      input_labels: A tensor or placeholder for labels associated with
+        input_data.
+      random_seed: The random number generator seed to use for this tree.  0
+        means use the current time as the seed.
+      data_spec: A data_ops.TensorForestDataSpec object specifying the
+        original feature/columns of the data.
+      sparse_features: A tf.SparseTensor for sparse input data.
+      input_weights: A float tensor or placeholder holding per-input weights,
+        or None if all inputs are to be weighted equally.
+
+    Returns:
+      The last op in the random tree training graph.
+    """
+    leaf_ids = model_ops.traverse_tree_v4(
+        self.variables.tree,
+        input_data,
+        params=self.params.serialized_params_proto)
+
+    update_model = model_ops.update_model_v4(
+        self.variables.tree,
+        leaf_ids,
+        input_labels,
+        params=self.params.serialized_params_proto)
+
+    finished_nodes = stats_ops.process_input_v4(
+        self.variables.tree,
+        self.variables.stats,
+        input_data,
+        input_labels,
+        leaf_ids,
+        random_seed=random_seed,
+        params=self.params.serialized_params_proto)
+
+    with ops.control_dependencies([update_model]):
+      return stats_ops.grow_tree_v4(
+          self.variables.tree,
+          self.variables.stats,
+          finished_nodes,
+          params=self.params.serialized_params_proto)
+
+  def inference_graph(self, input_data):
+    """Constructs a TF graph for evaluating a random tree.
+
+    Args:
+      input_data: A tensor or placeholder for input data.
+      data_spec: A TensorForestDataSpec proto specifying the original
+        input columns.
+      sparse_features: A tf.SparseTensor for sparse input data.
+
+    Returns:
+      A tuple of (probabilities, tree_paths).
+    """
+    return model_ops.tree_predictions_v4(
+        self.variables.tree,
+        input_data,
+        params=self.params.serialized_params_proto)
+
+  def size(self):
+    """Constructs a TF graph for evaluating the current number of nodes.
+
+    Returns:
+      The current number of nodes in the tree.
+    """
+    return tensor_forest_ops.tree_size(self.variables.tree)
+
+
 def _ensure_logits(head, logits):
     # For binary classification problems, convert probabilities to logits.
     # Includes hack to get around the fact that a probability might be 0 or 1.
