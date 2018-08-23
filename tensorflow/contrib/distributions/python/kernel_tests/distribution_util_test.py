d63812f1f4a37d8a03779ee5caa062978c7176e8
Asim Shankar <ashankar@google.com>
2018-08-23 11:21:30 -0700

    contrib/distributions: Test cleanup.
    - Don't need a test_session() when using
      @test_util.run_in_graph_and_eager_modes
    - Can use @test_util.run_all_in_graph_and_eager_modes when all tests in the
      TestCase should be run in both modes.
    
    PiperOrigin-RevId: 209968609

--- tensorflow/contrib/distributions/python/kernel_tests/distribution_util_test.py
+++ tensorflow/contrib/distributions/python/kernel_tests/distribution_util_test.py
@@ -542,9 +542,9 @@ class PadDynamicTest(_PadTest, test.TestCase):
     return False
 
 
+@test_util.run_all_in_graph_and_eager_modes
 class TestMoveDimension(test.TestCase):
 
-  @test_util.run_in_graph_and_eager_modes
   def test_move_dimension_static_shape(self):
 
     x = random_ops.random_normal(shape=[200, 30, 4, 1, 6])
@@ -561,7 +561,6 @@ class TestMoveDimension(test.TestCase):
     x_perm = distribution_util.move_dimension(x, 4, 2)
     self.assertAllEqual(x_perm.shape.as_list(), [200, 30, 6, 4, 1])
 
-  @test_util.run_in_graph_and_eager_modes
   def test_move_dimension_dynamic_shape(self):
 
     x_ = random_ops.random_normal(shape=[200, 30, 4, 1, 6])
