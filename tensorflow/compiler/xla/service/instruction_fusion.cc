acfacd18ae8c9531a0d08489b9684a75485b5f26
David Majnemer <majnemer@google.com>
2018-08-29 14:51:13 -0700

    [XLA] Update InstructionFusion::EffectivelyAtMostUnary for kIota
    It should behave like kBroadcast with respect to it being "effectively unary."
    
    PiperOrigin-RevId: 210795483

--- tensorflow/compiler/xla/service/instruction_fusion.cc
+++ tensorflow/compiler/xla/service/instruction_fusion.cc
@@ -172,7 +172,8 @@ bool InstructionFusion::EffectivelyAtMostUnary(HloInstruction* hlo) {
       });
   return std::count_if(hlo->operands().begin(), hlo->operands().end(),
                        [output_rank](HloInstruction* operand) {
-                         if (operand->opcode() == HloOpcode::kBroadcast) {
+                         if (operand->opcode() == HloOpcode::kBroadcast ||
+                             operand->opcode() == HloOpcode::kIota) {
                            return false;
                          }
                          if (operand->opcode() == HloOpcode::kConstant &&
