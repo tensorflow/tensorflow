ca475cb603210b7c9f09d2e4da5071f5bbd74e27
Pan Daoxin <daoxin.pan@intel.com>
2018-09-04 16:18:40 +0800

    Add MklSlice op.

--- tensorflow/core/ops/array_ops.cc
+++ tensorflow/core/ops/array_ops.cc
@@ -1648,6 +1648,60 @@ REGISTER_OP("Slice")
       return Status::OK();
     });
 
+#ifdef INTEL_MKL
+REGISTER_OP("_MklSlice")
+    .Input("input: T")
+    .Input("begin: Index")
+    .Input("size: Index")
+    .Input("mkl_input: uint8")
+    .Input("mkl_begin: uint8")
+    .Input("mkl_size: uint8")
+    .Output("output: T")
+    .Output("mkl_output: uint8")
+    .Attr("T: type")
+    .Attr("Index: {int32,int64}")
+    .SetShapeFn([](InferenceContext* c) {
+      ShapeHandle input = c->input(0);
+      ShapeHandle begin_shape;
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 1, &begin_shape));
+      ShapeHandle sizes_shape;
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(2), 1, &sizes_shape));
+
+      // Merge to check compatibility of begin and sizes tensors.
+      TF_RETURN_IF_ERROR(c->Merge(begin_shape, sizes_shape, &begin_shape));
+
+      DimensionHandle ndims = c->Dim(begin_shape, 0);
+      if (c->ValueKnown(ndims)) {
+        TF_RETURN_IF_ERROR(c->WithRank(input, c->Value(ndims), &input));
+      }
+
+      // NOTE(mrry): Use MakeShapeFromShapeTensor to handle partially-known
+      // values, even though the `begin` value does not represent a shape.
+      ShapeHandle begin_value;
+      TF_RETURN_IF_ERROR(c->MakeShapeFromShapeTensor(1, &begin_value));
+
+      // NOTE(mrry): We can't use `MakeShapeFromShapeTensor` for `sizes` because
+      // it might contain -1, which can't be represented -1 in the ShapeHandle
+      // would meqan "unknown".
+      const Tensor* sizes_value = c->input_tensor(3);
+
+      if (sizes_value != nullptr) {
+        TF_RETURN_IF_ERROR(
+            c->WithRank(begin_value, sizes_value->NumElements(), &begin_value));
+        std::vector<DimensionHandle> dims;
+        // If the begin and sizes tensors are available, then
+        // we can be precise about the shape of the output.
+        if (sizes_value->dtype() == DT_INT64) {
+          TF_RETURN_IF_ERROR(
+              SliceHelper<int64>(c, begin_value, sizes_value, &dims));
+        } else {
+          TF_RETURN_IF_ERROR(
+              SliceHelper<int32>(c, begin_value, sizes_value, &dims));
+        }
+      }
+    });
+#endif
+
 REGISTER_OP("StridedSlice")
     .Input("input: T")
     .Input("begin: Index")
