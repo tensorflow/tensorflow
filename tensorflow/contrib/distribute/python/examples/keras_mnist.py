f5e4e61d3f03421c190b0dc5a9b4d17e7616d5c1
Priya Gupta <priyag@google.com>
2018-09-05 15:00:09 -0700

    Experimental work-in-progress support for TPUStrategy in keras.
    PiperOrigin-RevId: 211705274

--- tensorflow/contrib/distribute/python/examples/keras_mnist.py
+++ tensorflow/contrib/distribute/python/examples/keras_mnist.py
@@ -58,13 +58,13 @@ def get_input_datasets():
   train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
   train_ds = train_ds.repeat()
   train_ds = train_ds.shuffle(100)
-  train_ds = train_ds.batch(64)
+  train_ds = train_ds.batch(64, drop_remainder=True)
 
   # eval dataset
   eval_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
   eval_ds = eval_ds.repeat()
   eval_ds = eval_ds.shuffle(100)
-  eval_ds = eval_ds.batch(64)
+  eval_ds = eval_ds.batch(64, drop_remainder=True)
 
   return train_ds, eval_ds, input_shape
 
