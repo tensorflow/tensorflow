406d9b79103983f84b11946c5a55b2a11f6fc974
Yuanzhong Xu <yuanzx@google.com>
2018-09-06 16:40:47 -0700

    [XLA] Handle kDomain in HloCostAnalysis.
    PiperOrigin-RevId: 211891325

--- tensorflow/compiler/xla/service/hlo_cost_analysis.cc
+++ tensorflow/compiler/xla/service/hlo_cost_analysis.cc
@@ -227,6 +227,14 @@ Status HloCostAnalysis::HandleCopy(const HloInstruction*) {
   return Status::OK();
 }
 
+Status HloCostAnalysis::HandleDomain(const HloInstruction* domain) {
+  // Domain does not have any computation or data transfer.
+  current_should_compute_bottleneck_time_ = false;
+  current_properties_[kBytesAccessedKey] = 0;
+  current_properties_[kOptimalSecondsKey] = 0;
+  return Status::OK();
+}
+
 Status HloCostAnalysis::HandleDot(const HloInstruction* dot) {
   const Shape& lhs_shape = dot->operand(0)->shape();
   const Shape& rhs_shape = dot->operand(1)->shape();
