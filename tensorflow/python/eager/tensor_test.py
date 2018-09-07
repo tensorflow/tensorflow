d57908f2d65f84381d2891ce05b29e5ddadf0838
Akshay Modi <akshaym@users.noreply.github.com>
2018-09-07 13:37:44 -0700

    Add PyMemberDef for __dict__ on eager tensors. (#22152)
    This fixes dir() calls on instances of eager tensors so that it correctly
    accesses the __dict__ of EagerTensorType.
    
    Earlier it would fail due to an infinite "loop" in subtype_dict: https://github.com/python/cpython/blob/7e610bcdf128f61b925654e4fa80fbac83537d0e/Objects/typeobject.c#L2145
    
    get_builtin_base_with_dict will return the same type (though I'm not sure this is reasonable behavior given its name).
    The __dict__ getter for the type is subtype_dict creating an infinite tail recursion.
    
    PiperOrigin-RevId: 212020695

--- tensorflow/python/eager/tensor_test.py
+++ tensorflow/python/eager/tensor_test.py
@@ -31,6 +31,7 @@ from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import test_util
+from tensorflow.python.ops import array_ops
 
 
 def _create_tensor(value, device=None, dtype=None):
@@ -331,6 +332,19 @@ class TFETensorUtilTest(test_util.TensorFlowTestCase):
         r"Slice dimension \(0\) must be smaller than rank of all tensors, "
         "but tensor at index 2 has rank 0"):
       pywrap_tensorflow.TFE_Py_TensorShapeSlice([t2, t1, t3], 0)
+
+  @test_util.assert_no_new_pyobjects_executing_eagerly
+  def testTensorDir(self):
+    t = array_ops.zeros(1)
+    t.test_attr = "Test"
+
+    instance_dir = dir(t)
+    type_dir = dir(ops.EagerTensor)
+
+    # Monkey patched attributes should show up in dir(t)
+    self.assertIn("test_attr", instance_dir)
+    instance_dir.remove("test_attr")
+    self.assertEqual(instance_dir, type_dir)
 
 
 if __name__ == "__main__":
