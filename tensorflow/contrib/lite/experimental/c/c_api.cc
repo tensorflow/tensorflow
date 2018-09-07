dfd6aba381a7867906ba2d173c1720bb38dec6a8
Jared Duke <jdduke@google.com>
2018-09-07 11:01:45 -0700

    Add TFL_TensorName() to C API
    PiperOrigin-RevId: 212002349

--- tensorflow/contrib/lite/experimental/c/c_api.cc
+++ tensorflow/contrib/lite/experimental/c/c_api.cc
@@ -133,6 +133,8 @@ void* TFL_TensorData(const TFL_Tensor* tensor) {
   return static_cast<void*>(tensor->data.raw);
 }
 
+const char* TFL_TensorName(const TFL_Tensor* tensor) { return tensor->name; }
+
 TFL_Status TFL_TensorCopyFromBuffer(TFL_Tensor* tensor, const void* input_data,
                                     size_t input_data_size) {
   if (tensor->bytes != input_data_size) {
