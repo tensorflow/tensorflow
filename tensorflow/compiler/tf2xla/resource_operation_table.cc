6ace17e9910335a2778280ead6dca475bfa5d4c7
Austin Anderson <angerson@google.com>
2018-09-07 17:33:57 -0700

    Revert "More fixes for bad pick of rollback (#22150)"
    This reverts commit 5a720679efc205cef0e4cf305faafc70b87a212a.

--- tensorflow/compiler/tf2xla/resource_operation_table.cc
+++ tensorflow/compiler/tf2xla/resource_operation_table.cc
@@ -110,10 +110,10 @@ GetStaticResourceOpInfoMap() {
   return *op_info_map;
 }
 
-const XlaResourceOpInfo* GetResourceOpInfoForOp(StringPiece op) {
+const XlaResourceOpInfo* GetResourceOpInfoForOp(absl::string_view op) {
   const gtl::FlatMap<StringPiece, XlaResourceOpInfo>& op_infos =
       GetStaticResourceOpInfoMap();
-  auto it = op_infos.find(op);
+  auto it = op_infos.find(StringPiece(op.data(), op.length()));
   return it == op_infos.end() ? nullptr : &it->second;
 }
 
