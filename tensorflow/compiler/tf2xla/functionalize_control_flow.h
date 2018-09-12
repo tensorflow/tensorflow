37ddb13ece32500bf87af5d8b8493be1c77781de
Tong Shen <endlessroad@google.com>
2018-09-12 10:03:32 -0700

    Roll forward change "Move control flow functionalization as a graph optimization pass, instead of a step in XlaCompiler.".
    PiperOrigin-RevId: 212657932

--- tensorflow/compiler/tf2xla/functionalize_control_flow.h
+++ tensorflow/compiler/tf2xla/functionalize_control_flow.h
@@ -17,6 +17,7 @@ limitations under the License.
 #define TENSORFLOW_COMPILER_TF2XLA_FUNCTIONALIZE_CONTROL_FLOW_H_
 
 #include "tensorflow/compiler/xla/status_macros.h"
+#include "tensorflow/core/common_runtime/optimization_registry.h"
 #include "tensorflow/core/framework/function.h"
 #include "tensorflow/core/graph/graph.h"
 
@@ -31,6 +32,18 @@ Status FunctionalizeControlFlow(Graph* graph,
 Status FunctionalizeControlFlow(const FunctionLibraryDefinition* lookup_library,
                                 Graph* graph,
                                 FunctionLibraryDefinition* library);
+
+// This pass looks at the graph and all associated FunctionDefs, and turns
+// traditional control flow structure (Switch/Merge/etc.) into functional
+// control flow structure (XlaIf/XlaWhile).
+//
+// Notice that control flow structure marked with _xla_outside_compilation are
+// skipped, because they need to be executed on host with regular TF executor,
+// which does not support XlaIf/XlaWhile.
+class FunctionalizeControlFlowPass : public GraphOptimizationPass {
+ public:
+  Status Run(const GraphOptimizationPassOptions& options) override;
+};
 
 }  // namespace tensorflow
 
