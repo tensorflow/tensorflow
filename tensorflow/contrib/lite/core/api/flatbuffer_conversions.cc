1e7235c78e35292e620363da536d54af30851ecc
Suharsh Sivakumar <suharshs@google.com>
2018-09-13 15:01:08 -0700

    Dilated Depthwise Conv reference implementations.
    PiperOrigin-RevId: 212884951

--- tensorflow/contrib/lite/core/api/flatbuffer_conversions.cc
+++ tensorflow/contrib/lite/core/api/flatbuffer_conversions.cc
@@ -216,6 +216,9 @@ TfLiteStatus ParseOpData(const Operator* op, BuiltinOperator op_type,
         params->depth_multiplier = conv_params->depth_multiplier();
         params->activation =
             parse_activation(conv_params->fused_activation_function());
+
+        params->dilation_width_factor = conv_params->dilation_w_factor();
+        params->dilation_height_factor = conv_params->dilation_h_factor();
       }
       *builtin_data = reinterpret_cast<void*>(params);
       break;
