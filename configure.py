a9a1d5a673ad085777e6a8b14cbe39a427493e51
Anna R <annarev@google.com>
2018-09-14 12:44:31 -0700

    Add --config=v2 option to the .bazelrc file.
    PiperOrigin-RevId: 213027176

--- configure.py
+++ configure.py
@@ -1572,6 +1572,9 @@ def main():
   if is_windows():
     set_windows_build_flags(environ_cp)
 
+  # Add a config option to build TensorFlow 2.0 API.
+  write_to_bazelrc('build:v2 --define=tf_api_version=2')
+
   if get_var(
       environ_cp, 'TF_SET_ANDROID_WORKSPACE', 'android workspace',
       False,
