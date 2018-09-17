789c06d5db3117a572d8657491bca62311c302d3
Deven Desai <deven.desai.amd@gmail.com>
2018-09-17 17:53:39 +0000

    skipping tests that require 5D tensor support in MIOpen

--- tensorflow/python/keras/layers/convolutional_test.py
+++ tensorflow/python/keras/layers/convolutional_test.py
@@ -260,6 +260,10 @@ class Conv3DTransposeTest(test.TestCase):
 
   @tf_test_util.run_in_graph_and_eager_modes
   def test_conv3dtranspose(self):
+
+    if test.is_built_with_rocm():
+      self.skipTest("5D tensors are not yet supported in ROCm")
+
     kwargs = {
         'filters': 2,
         'kernel_size': (3, 3, 3),
@@ -479,6 +483,10 @@ class Conv3DTest(test.TestCase):
 
   @tf_test_util.run_in_graph_and_eager_modes
   def test_conv3d(self):
+
+    if test.is_built_with_rocm():
+      self.skipTest("5D tensors are not yet supported in ROCm")
+
     kwargs = {
         'filters': 2,
         'kernel_size': (3, 3, 3),
