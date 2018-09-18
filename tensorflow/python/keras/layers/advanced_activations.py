cc3a7a847f0c73ae3de99f6b56ef02f56644ea67
Yan Facai (颜发才) <facai.yan@gmail.com>
2018-09-18 13:06:51 +0800

    CLN: minor changes

--- tensorflow/python/keras/layers/advanced_activations.py
+++ tensorflow/python/keras/layers/advanced_activations.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from tensorflow.python.keras import activations
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras import constraints
 from tensorflow.python.keras import initializers
