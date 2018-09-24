6995db405617abc90da3331094aa8af5e6b57fd1
A. Unique TensorFlower <gardener@tensorflow.org>
2018-09-24 15:06:55 -0700

    Disable PinToHostOptimizer for any TPU graphs.
    PiperOrigin-RevId: 214338297

--- tensorflow/core/grappler/optimizers/pin_to_host_optimizer.cc
+++ tensorflow/core/grappler/optimizers/pin_to_host_optimizer.cc
@@ -157,6 +157,16 @@ string TryFindHostDevice(const gtl::FlatSet<string>& devices,
   return device;
 }
 
+bool IsTPUGraphDef(const GraphDef& def) {
+  for (auto node : def.node()) {
+    if (node.op() == "TPUCompile" || node.op() == "TPUExecute" ||
+        node.op() == "TPUPartitionedCall") {
+      return true;
+    }
+  }
+  return false;
+}
+
 // All the nodes that should be blacklisted and not swapped.
 bool IsBlacklisted(const NodeDef& node) { return IsCollective(node); }
 }  // end namespace internal
@@ -164,6 +174,11 @@ bool IsBlacklisted(const NodeDef& node) { return IsCollective(node); }
 Status PinToHostOptimizer::Optimize(Cluster* cluster, const GrapplerItem& item,
                                     GraphDef* optimized_graph) {
   *optimized_graph = item.graph;
+
+  // Skip all TPU graphs.
+  if (internal::IsTPUGraphDef(*optimized_graph)) {
+    return Status::OK();
+  }
 
   GraphProperties properties(item);
   bool has_properties = false;
