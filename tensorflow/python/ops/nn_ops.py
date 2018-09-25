54cb27a867a0c54f968800dbdffdf127eacce356
Cao Zongyan <zongyan.cao@alibaba-inc.com>
2018-09-25 09:59:17 +0800

    Fix build dependencies in tensorflow/cc/BUILD.

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -1601,7 +1601,7 @@ def leaky_relu(features, alpha=0.2, name=None):
     features = ops.convert_to_tensor(features, name="features")
     if features.dtype.is_integer:
       features = math_ops.to_float(features)
-    if compat.forward_compatible(2018, 10, 1):
+    if compat.forward_compatible(2018, 11, 1):
       return gen_nn_ops.leaky_relu(features, alpha=alpha, name=name)
     alpha = ops.convert_to_tensor(alpha, dtype=features.dtype, name="alpha")
     return math_ops.maximum(alpha * features, features, name=name)
