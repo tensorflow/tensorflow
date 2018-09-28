96f3428e33e18477661b8d8cf78f2db457c8881b
A. Unique TensorFlower <gardener@tensorflow.org>
2018-09-27 18:43:55 -0700

    Let feature columns correctly handle rank-1 sparse tensors from an empty batch.
    reshape can't determine the size of the last dimension when reshaping
    shape (0) to (0, 1).
    
    PiperOrigin-RevId: 214872677

--- tensorflow/python/feature_column/feature_column_v2.py
+++ tensorflow/python/feature_column/feature_column_v2.py
@@ -2341,7 +2341,7 @@ class FeatureTransformationCache(object):
       # Input_tensor must have rank 1.
       if isinstance(input_tensor, sparse_tensor_lib.SparseTensor):
         return sparse_ops.sparse_reshape(
-            input_tensor, [array_ops.shape(input_tensor)[0], -1])
+            input_tensor, [array_ops.shape(input_tensor)[0], 1])
       else:
         return array_ops.expand_dims(input_tensor, -1)
 
