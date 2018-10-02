3e43275f929f4de442021ba2be743403a8f1027e
Gautam <gautamrbharadwaj@gmail.com>
2018-10-02 12:32:37 +0530

    Update math_ops.py
    adding erfniv in reference to # TODO(phawkins): implement erfinv

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -386,6 +386,27 @@ def erf(x, name=None):
     else:
       return gen_math_ops.erf(x, name=name)
 
+    
+@tf_export("math.erfinv", "erfinv")
+@deprecation.deprecated_endpoints("erfinv")    
+def erfinv(x, name="erfinv"):
+  """The inverse function for erf, the error function.
+  Args:
+    x: `Tensor` of type `float32`, `float64`.
+    name: Python string. A name for the operation (default="erfinv").
+  Returns:
+    x: `Tensor` with `dtype=x.dtype`.
+  Raises:
+    TypeError: if `x` is not floating-type.
+  """
+
+  with ops.name_scope(name, values=[x]):
+    x = ops.convert_to_tensor(x, name="x")
+    if x.dtype.as_numpy_dtype not in [np.float32, np.float64]:
+      raise TypeError(
+          "x.dtype=%s is not handled, see docstring for supported types."
+          % x.dtype)
+    return ndtri((x + 1.0) / 2.0) / np.sqrt(2)
 
 @tf_export("math.scalar_mul", "scalar_mul")
 def scalar_mul(scalar, x):
