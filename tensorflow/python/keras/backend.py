648e423619ddaacb37f4a665721ee7d7afe9c14f
Gautam <gautamrbharadwaj@gmail.com>
2018-10-02 11:56:06 +0530

    Update backend.py
    Adding missing import files in the commented examples. When trying out that particular example in commented section the TensorFlow and bumpy imports are missing

--- tensorflow/python/keras/backend.py
+++ tensorflow/python/keras/backend.py
@@ -773,6 +773,8 @@ def is_keras_tensor(x):
 
   Examples:
   ```python
+      >>> import tensorflow as tf
+      >>> import numpy
       >>> from keras import backend as K
       >>> from keras.layers import Input, Dense
       >>> np_var = numpy.array([1, 2])
