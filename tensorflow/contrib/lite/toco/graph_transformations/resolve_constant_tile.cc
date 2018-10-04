e1a8f4b03df2ef84538c01788b6043eb723cd046
A. Unique TensorFlower <gardener@tensorflow.org>
2018-10-04 11:04:41 -0700

    Automated rollback of commit 8dc7bc7764150253c03a666eee84fc48f867d6a2
    PiperOrigin-RevId: 215768310

--- tensorflow/contrib/lite/toco/graph_transformations/resolve_constant_tile.cc
+++ tensorflow/contrib/lite/toco/graph_transformations/resolve_constant_tile.cc
@@ -105,13 +105,6 @@ bool ResolveConstantTile::Run(Model* model, std::size_t op_index) {
   }
   const auto* op = static_cast<const TensorFlowTileOperator*>(base_op);
 
-  // If the output of this op is a non-discardable array such as an input_array
-  // or a state array of the model, then this is a job for RemoveUnusedOp, not
-  // for constants-propagation.
-  if (!IsDiscardableArray(*model, op->outputs[0])) {
-    return false;
-  }
-
   CHECK_GE(op->inputs.size(), 2);
   CHECK_EQ(op->outputs.size(), 1);
   auto& output_array = model->GetArray(op->outputs[0]);
