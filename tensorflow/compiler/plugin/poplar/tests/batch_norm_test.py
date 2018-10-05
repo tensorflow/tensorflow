b20018f6e80c31b1cfdc869d4e08f1007b00c78a
David Norman <davidn@graphcore.ai>
2018-10-05 09:25:04 +0100

    Progress towards FP16 batchnorm
    Summary:
    Add tests to check that batchnorm will work in fp16, and fix up
    both normal batchnorm and fused batchnorm so that FP16 works.
    
    note that the 'layers' batchnorm still uses 32 bit stats variables
    independent of the type of the input.
    
    Test Plan: all existing tests ok
    
    Reviewers: jakeh, georgep
    
    Reviewed By: georgep
    
    Differential Revision: https://phabricator.sourcevertex.net/D6722

--- tensorflow/compiler/plugin/poplar/tests/batch_norm_test.py
+++ tensorflow/compiler/plugin/poplar/tests/batch_norm_test.py
@@ -14,41 +14,154 @@ from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import variables
+from tensorflow.python.layers import normalization as layers_norm
 
 import numpy as np
 
+
 class IpuXlaBatchNormTest(test_util.TensorFlowTestCase):
 
-    def testBatchNormalize1(self):
+  def testBatchNormalize(self):
+    x = array_ops.placeholder(np.float32, [4, 64, 64, 4], name="a")
 
-        with ops.device("/device:IPU:0"):
-            with session_lib.Session() as sess:
-                with variable_scope.variable_scope("ascope", use_resource=True):
-                    x = array_ops.placeholder(np.float32, [1,64,64,4], name="a")
+    with ops.device("/device:IPU:0"):
+      with variable_scope.variable_scope("", use_resource=True):
 
-                    beta = variable_scope.get_variable("x", shape=[4],
-                            dtype=np.float32,
-                            initializer=init_ops.constant_initializer(0.0))
-                    gamma = variable_scope.get_variable("y", shape=[4],
-                            dtype=np.float32,
-                            initializer=init_ops.constant_initializer(1.0))
+        beta = variable_scope.get_variable(
+          "x", dtype=np.float32, shape=[4],
+          initializer=init_ops.constant_initializer(0.0))
+        gamma = variable_scope.get_variable(
+          "y", dtype=np.float32, shape=[4],
+        initializer=init_ops.constant_initializer(1.0))
 
-                    b_mean, b_var = nn.moments(x, [0,1,2], name='moments')
+        b_mean, b_var = nn.moments(x, [0, 1, 2], name='moments')
 
-                    normed = nn.batch_normalization(x,
-                                                       b_mean, b_var,
-                                                       beta, gamma,
-                                                       1e-3)
+        normed = nn.batch_normalization(x, b_mean, b_var, beta, gamma, 1e-3)
 
-                    fd = {
-                        x: np.zeros([1,64,64,4])
-                    }
+    with session_lib.Session() as sess:
+      sess.run(variables.global_variables_initializer())
+      result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+      self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
-                    sess.run(variables.global_variables_initializer())
 
-                    result = sess.run(normed, fd)
-                    self.assertAllClose(result,
-                                        np.zeros([1,64,64,4]))
+  def testBatchNormalizeFp16(self):
+    x = array_ops.placeholder(np.float16, [4, 64, 64, 4], name="a")
+
+    with ops.device("/device:IPU:0"):
+      with variable_scope.variable_scope("", use_resource=True):
+
+        beta = variable_scope.get_variable(
+          "x", dtype=np.float16, shape=[4],
+          initializer=init_ops.constant_initializer(0.0))
+        gamma = variable_scope.get_variable(
+          "y", dtype=np.float16, shape=[4],
+        initializer=init_ops.constant_initializer(1.0))
+
+        b_mean, b_var = nn.moments(x, [0, 1, 2], name='moments')
+
+        normed = nn.batch_normalization(x, b_mean, b_var, beta, gamma, 1e-3)
+
+      with session_lib.Session() as sess:
+        sess.run(variables.global_variables_initializer())
+        result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+        self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+
+  def testBatchNormalizeFused(self):
+    x = array_ops.placeholder(np.float32, [4, 64, 64, 4], name="a")
+
+    with ops.device("/device:IPU:0"):
+      with variable_scope.variable_scope("", use_resource=True):
+
+        beta = variable_scope.get_variable(
+          "x", dtype=np.float32, shape=[4],
+          initializer=init_ops.constant_initializer(0.0))
+        gamma = variable_scope.get_variable(
+          "y", dtype=np.float32, shape=[4],
+        initializer=init_ops.constant_initializer(1.0))
+
+        b_mean, b_var = nn.moments(x, [0, 1, 2], name='moments')
+
+        normed = nn.fused_batch_norm(x, gamma, beta, b_mean, b_var,
+                                     is_training=False)
+
+    with session_lib.Session() as sess:
+      sess.run(variables.global_variables_initializer())
+      result, _, _ = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+      self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+
+  def testBatchNormalizeFusedFp16(self):
+    with ops.device("/device:IPU:0"):
+        with variable_scope.variable_scope("", use_resource=True):
+          x = array_ops.placeholder(np.float16, [4, 64, 64, 4], name="a")
+
+          beta = variable_scope.get_variable(
+            "x", dtype=np.float16, shape=[4],
+            initializer=init_ops.constant_initializer(0.0))
+          gamma = variable_scope.get_variable(
+            "y", dtype=np.float16, shape=[4],
+          initializer=init_ops.constant_initializer(1.0))
+
+          b_mean, b_var = nn.moments(x, [0, 1, 2], name='moments')
+
+          normed = nn.fused_batch_norm(x, gamma, beta, b_mean, b_var,
+                                       is_training=False)
+
+    with session_lib.Session() as sess:
+          sess.run(variables.global_variables_initializer())
+          result, _, _ = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+          self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+
+  def testBatchNormalizeLayer(self):
+    with ops.device("/device:IPU:0"):
+        with variable_scope.variable_scope("", use_resource=True):
+          x = array_ops.placeholder(np.float32, [4, 64, 64, 4], name="a")
+
+          normed = layers_norm.batch_normalization(x, fused=False)
+
+    with session_lib.Session() as sess:
+          sess.run(variables.global_variables_initializer())
+          result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+          self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+
+  def testBatchNormalizeFusedLayer(self):
+    with ops.device("/device:IPU:0"):
+        with variable_scope.variable_scope("", use_resource=True):
+          x = array_ops.placeholder(np.float32, [4, 64, 64, 4], name="a")
+
+          normed = layers_norm.batch_normalization(x, fused=True)
+
+    with session_lib.Session() as sess:
+          sess.run(variables.global_variables_initializer())
+          result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+          self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+  def testBatchNormalizeLayerFp16(self):
+    with ops.device("/device:IPU:0"):
+        with variable_scope.variable_scope("", use_resource=True):
+          x = array_ops.placeholder(np.float16, [4, 64, 64, 4], name="a")
+
+          normed = layers_norm.batch_normalization(x, fused=False)
+
+    with session_lib.Session() as sess:
+          sess.run(variables.global_variables_initializer())
+          result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+          self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
+
+  def testBatchNormalizeLayerFusedFp16(self):
+    with ops.device("/device:IPU:0"):
+        with variable_scope.variable_scope("", use_resource=True):
+          x = array_ops.placeholder(np.float16, [4, 64, 64, 4], name="a")
+
+          normed = layers_norm.batch_normalization(x, fused=True)
+
+    with session_lib.Session() as sess:
+      sess.run(variables.global_variables_initializer())
+      result = sess.run(normed, { x: np.zeros([4, 64, 64, 4]) })
+      self.assertAllClose(result, np.zeros([4, 64, 64, 4]))
 
 if __name__ == "__main__":
-    googletest.main()
+  googletest.main()
