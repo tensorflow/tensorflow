a09eeb46814252d8846b22a54b40e708f8b0f212
A. Unique TensorFlower <gardener@tensorflow.org>
2018-10-10 13:23:26 -0700

    Don't use wildcard reshaping when dimensions can be unknown.
    PiperOrigin-RevId: 216582137

--- tensorflow/python/feature_column/feature_column_v2.py
+++ tensorflow/python/feature_column/feature_column_v2.py
@@ -4110,7 +4110,10 @@ class SequenceCategoricalColumn(
     # combined during embedding lookup. If the tensor is already 3D, leave
     # as-is.
     shape = array_ops.shape(id_tensor)
-    target_shape = [shape[0], shape[1], -1]
+    # Compute the third dimension explicitly instead of setting it to -1, as
+    # that doesn't work for dynamically shaped tensors with 0-length at runtime.
+    # This happens for empty sequences.
+    target_shape = [shape[0], shape[1], math_ops.reduce_prod(shape[2:])]
     id_tensor = sparse_ops.sparse_reshape(id_tensor, target_shape)
     if weight_tensor is not None:
       weight_tensor = sparse_ops.sparse_reshape(weight_tensor, target_shape)
