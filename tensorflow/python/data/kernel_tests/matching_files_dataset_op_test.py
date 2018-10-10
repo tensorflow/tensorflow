ffd1840ecb1e2b9bb06638ecb51c3238f1d70f26
Fei Hu <hufei68@gmail.com>
2018-10-10 13:38:06 -0700

    Revise the NestedDirectories test working for both Windows and Linux

--- tensorflow/python/data/kernel_tests/matching_files_dataset_op_test.py
+++ tensorflow/python/data/kernel_tests/matching_files_dataset_op_test.py
@@ -136,23 +136,23 @@ class MatchingFilesDatasetTest(test_base.DatasetTestBase):
         new_base = os.path.join(self.tmp_dir, str(i),
                                 *[str(dir_name) for dir_name in range(j)])
         os.makedirs(new_base)
-        for f in ['a.txt', 'b.py', 'c.pyc']:
+        child_files = ['a.py', 'b.pyc'] if j < depth - 1 else ['c.txt', 'd.log']
+        for f in child_files:
           filename = os.path.join(new_base, f)
           filenames.append(filename)
           open(filename, 'w').close()
 
-    patterns = []
-    for i in range(depth):
-      pattern = os.path.join(
-          self.tmp_dir, os.path.join(*['**' for _ in range(i + 1)]), '*.txt')
-      patterns.append(pattern)
+    patterns = [
+        os.path.join(self.tmp_dir, os.path.join(*['**' for _ in range(depth)]),
+                     suffix) for suffix in ['*.txt', '*.log']]
 
     dataset = MatchingFilesDataset(patterns)
     with self.cached_session() as sess:
       next_element = dataset.make_one_shot_iterator().get_next()
       expected_filenames = [compat.as_bytes(filename)
                             for filename in filenames
-                            if filename.endswith('.txt')]
+                            if filename.endswith('.txt')
+                            or filename.endswith('.log')]
       actual_filenames = []
       while True:
         try:
@@ -173,19 +173,15 @@ class MatchingFilesDatasetBenchmark(test.Benchmark):
       for j in range(depth):
         new_base = os.path.join(tmp_dir, str(i),
                                 *[str(dir_name) for dir_name in range(j)])
-        if not os.path.exists(new_base):
-          os.makedirs(new_base)
-        for f in ['a.txt', 'b.py', 'c.pyc']:
+        os.makedirs(new_base)
+        child_files = ['a.py', 'b.pyc'] if j < depth - 1 else ['c.txt', 'd.log']
+        for f in child_files:
           filename = os.path.join(new_base, f)
           open(filename, 'w').close()
 
-    patterns = []
-    for i in range(depth):
-      pattern = os.path.join(tmp_dir,
-                             os.path.join(*['**' for _ in range(i + 1)]),
-                             '*.txt')
-
-      patterns.append(pattern)
+    patterns = [
+        os.path.join(tmp_dir, os.path.join(*['**' for _ in range(depth)]),
+                     suffix) for suffix in ['*.txt', '*.log']]
 
     deltas = []
     iters = 3
