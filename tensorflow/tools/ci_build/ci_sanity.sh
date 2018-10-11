d4a3578b46b477bcedee2600b39ff094b355eec3
Francois Chollet <fchollet@google.com>
2018-10-10 20:50:21 -0700

    Part 3/4 of the update of tf.keras to the 2.2.4 API.
    PiperOrigin-RevId: 216639755

--- tensorflow/tools/ci_build/ci_sanity.sh
+++ tensorflow/tools/ci_build/ci_sanity.sh
@@ -104,7 +104,8 @@ do_pylint() {
 "^tensorflow/python/keras/callbacks\.py.*\[E1133.*not-an-iterable "\
 "^tensorflow/python/keras/engine/base_layer.py.*\[E0203.*access-member-before-definition "\
 "^tensorflow/python/keras/layers/recurrent\.py.*\[E0203.*access-member-before-definition "\
-"^tensorflow/python/kernel_tests/constant_op_eager_test.py.*\[E0303.*invalid-length-returned"
+"^tensorflow/python/kernel_tests/constant_op_eager_test.py.*\[E0303.*invalid-length-returned "\
+"^tensorflow/python/keras/utils/data_utils.py.*\[E1102.*not-callable"
 
   echo "ERROR_WHITELIST=\"${ERROR_WHITELIST}\""
 
