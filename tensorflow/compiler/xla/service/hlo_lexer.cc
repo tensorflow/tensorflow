444eb1c66327eac00d0d6b6acd68b92d8e38413a
Michael Kuperstein <mkuper@google.com>
2018-10-12 14:13:31 -0700

    [XLA] Allow whitespace in shapes and layouts in HLO text.
    PiperOrigin-RevId: 216919890

--- tensorflow/compiler/xla/service/hlo_lexer.cc
+++ tensorflow/compiler/xla/service/hlo_lexer.cc
@@ -204,7 +204,7 @@ TokKind HloLexer::LexIdentifier() {
     auto consumable = RegexpStringPieceFromPointers(token_start_, buf_.end());
     // 'consumable' will be advanced iff its prefix matches the pattern.
     static LazyRE2 shape_pattern = {
-        R"(^(\w*\d*)\[([\d,]*)\](?:(dense|sparse)?{([\d,]+)})?)"};
+        R"(^(\w*\d*)\[([\d,\s]*)\](?:(dense|sparse)?{([\d,\s]+)})?)"};
     if (RE2::Consume(&consumable, *shape_pattern)) {
       auto status_or_shape = ShapeUtil::ParseShapeString(
           StringPieceFromPointers(token_start_, consumable.begin()));
