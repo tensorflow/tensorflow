732bfdc7da667a57dabc0b85d47ff98bf52caca6
Alexandre Passos <apassos@google.com>
2018-10-15 12:45:30 -0700

    Automated rollback of commit 6e16d34172ba14b6afc5fac6ce50913343769f40. Revert #22533.
    PiperOrigin-RevId: 217190256

--- tensorflow/python/training/adam.py
+++ tensorflow/python/training/adam.py
@@ -20,9 +20,10 @@ from __future__ import print_function
 
 from tensorflow.python.eager import context
 from tensorflow.python.framework import ops
-from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import resource_variable_ops
+from tensorflow.python.ops import state_ops
 from tensorflow.python.training import optimizer
 from tensorflow.python.training import training_ops
 from tensorflow.python.util.tf_export import tf_export
@@ -173,7 +174,7 @@ class AdamOptimizer(optimizer.Optimizer):
         math_ops.cast(self._epsilon_t, grad.dtype.base_dtype),
         grad, use_locking=self._use_locking)
 
-  def _apply_sparse_shared(self, grad, var, indices):
+  def _apply_sparse_shared(self, grad, var, indices, scatter_add):
     beta1_power, beta2_power = self._get_beta_accumulators()
     beta1_power = math_ops.cast(beta1_power, var.dtype.base_dtype)
     beta2_power = math_ops.cast(beta2_power, var.dtype.base_dtype)
@@ -185,27 +186,37 @@ class AdamOptimizer(optimizer.Optimizer):
     # m_t = beta1 * m + (1 - beta1) * g_t
     m = self.get_slot(var, "m")
     m_scaled_g_values = grad * (1 - beta1_t)
-    m_gathered = array_ops.gather(m, indices)
-    m_t_gathered = m_gathered * beta1_t + m_scaled_g_values
-    m_t = m.scatter_update(ops.IndexedSlices(m_t_gathered, indices))
+    m_t = state_ops.assign(m, m * beta1_t,
+                           use_locking=self._use_locking)
+    with ops.control_dependencies([m_t]):
+      m_t = scatter_add(m, indices, m_scaled_g_values)
     # v_t = beta2 * v + (1 - beta2) * (g_t * g_t)
     v = self.get_slot(var, "v")
     v_scaled_g_values = (grad * grad) * (1 - beta2_t)
-    v_gathered = array_ops.gather(v, indices)
-    v_t_gathered = v_gathered * beta2_t + v_scaled_g_values
-    v_t = v.scatter_update(ops.IndexedSlices(v_t_gathered, indices))
-
-    v_sqrt_gathered = math_ops.sqrt(v_t_gathered)
-    var_update = var.scatter_sub(
-        ops.IndexedSlices(lr * m_t_gathered / (v_sqrt_gathered + epsilon_t),
-                          indices))
+    v_t = state_ops.assign(v, v * beta2_t, use_locking=self._use_locking)
+    with ops.control_dependencies([v_t]):
+      v_t = scatter_add(v, indices, v_scaled_g_values)
+    v_sqrt = math_ops.sqrt(v_t)
+    var_update = state_ops.assign_sub(var,
+                                      lr * m_t / (v_sqrt + epsilon_t),
+                                      use_locking=self._use_locking)
     return control_flow_ops.group(*[var_update, m_t, v_t])
 
   def _apply_sparse(self, grad, var):
-    return self._apply_sparse_shared(grad.values, var, grad.indices)
+    return self._apply_sparse_shared(
+        grad.values, var, grad.indices,
+        lambda x, i, v: state_ops.scatter_add(  # pylint: disable=g-long-lambda
+            x, i, v, use_locking=self._use_locking))
+
+  def _resource_scatter_add(self, x, i, v):
+    with ops.control_dependencies(
+        [resource_variable_ops.resource_scatter_add(
+            x.handle, i, v)]):
+      return x.value()
 
   def _resource_apply_sparse(self, grad, var, indices):
-    return self._apply_sparse_shared(grad, var, indices)
+    return self._apply_sparse_shared(
+        grad, var, indices, self._resource_scatter_add)
 
   def _finish(self, update_ops, name_scope):
     # Update the power accumulators.
