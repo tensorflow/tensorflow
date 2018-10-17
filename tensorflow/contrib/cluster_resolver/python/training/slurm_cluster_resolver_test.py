6320670ec92aeb4a860c5c4ba1493c7debe3a168
steven <wdchien@kth.se>
2018-10-17 13:59:57 +0200

    fixed extra }

--- tensorflow/contrib/cluster_resolver/python/training/slurm_cluster_resolver_test.py
+++ tensorflow/contrib/cluster_resolver/python/training/slurm_cluster_resolver_test.py
@@ -101,8 +101,8 @@ class SlurmClusterResolverTest(test.TestCase):
     expected_proto = """
     job { name: 'ps' tasks { value: 't02n13:8888' } }
     job { name: 'worker' tasks { key: 0 value: 't02n13:8889' }
-                         tasks { key: 1 value: 't02n41:8888' } }
-                         tasks { key: 2 value: 't02n41:8889' } }
+                         tasks { key: 1 value: 't02n41:8888' }
+                         tasks { key: 2 value: 't02n41:8889' }
                          tasks { key: 3 value: 't02n43:8888' } }
     """
 
@@ -127,8 +127,8 @@ class SlurmClusterResolverTest(test.TestCase):
     expected_proto = """
     job { name: 'ps' tasks { value: 't02n13:8888' } }
     job { name: 'worker' tasks { key: 0 value: 't02n13:8889' }
-                         tasks { key: 1 value: 't02n41:8888' } }
-                         tasks { key: 2 value: 't02n41:8889' } }
+                         tasks { key: 1 value: 't02n41:8888' }
+                         tasks { key: 2 value: 't02n41:8889' }
                          tasks { key: 3 value: 't02n43:8888' } }
     """
 
