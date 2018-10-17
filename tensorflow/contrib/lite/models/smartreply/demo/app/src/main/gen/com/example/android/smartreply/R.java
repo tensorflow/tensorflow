6993dfbd78bcf3aa048abc00e45790229b098e85
Florian Raudies <florian.raudies@gmail.com>
2018-10-17 11:21:31 -0700

    Cleanup scope of files -- unsure when these sneaked into this diff - Remove generated code from diff to avoid clutter in PR - Remove README.md from diff

--- tensorflow/contrib/lite/models/smartreply/demo/app/src/main/gen/com/example/android/smartreply/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.example.android.smartreply;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
