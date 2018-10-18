13f6c51f579dfd1012e3ff488302a9278e8c77e6
A. Unique TensorFlower <gardener@tensorflow.org>
2018-10-17 22:33:34 -0700

    Step 1 of "tower" -> "replica" terminology transition for DistributionStrategy.
    PiperOrigin-RevId: 217645293

--- tensorflow/python/training/distribution_strategy_context.py
+++ tensorflow/python/training/distribution_strategy_context.py
@@ -30,29 +30,29 @@ distribute_lib = LazyLoader(
 
 # ------------------------------------------------------------------------------
 # Internal API for setting the current thread mode as being either in a
-# tower or cross-tower context for a particular distribution strategy.
+# replica or cross-replica context for a particular distribution strategy.
 
 
 class _ThreadMode(object):
 
-  def __init__(self, dist, cross, tower):
+  def __init__(self, dist, cross, replica):
     self.distribution_strategy = dist
-    self.cross_tower_context = cross
-    self.tower_context = tower
+    self.cross_replica_context = cross
+    self.replica_context = replica
 
 
-class _CrossTowerThreadMode(_ThreadMode):
+class _CrossReplicaThreadMode(_ThreadMode):
 
   def __init__(self, distribution_strategy):
     _ThreadMode.__init__(
         self, distribution_strategy, distribution_strategy, None)
 
 
-class _InTowerThreadMode(_ThreadMode):
+class _InReplicaThreadMode(_ThreadMode):
 
-  def __init__(self, tower_ctx):
+  def __init__(self, replica_ctx):
     _ThreadMode.__init__(
-        self, tower_ctx.distribution_strategy, None, tower_ctx)
+        self, replica_ctx.distribution_strategy, None, replica_ctx)
 
 
 def _push_per_thread_mode(context):
@@ -63,7 +63,7 @@ def _pop_per_thread_mode():
   ops.get_default_graph()._distribution_strategy_stack.pop(-1)  # pylint: disable=protected-access
 
 
-class _DefaultTowerThreadMode(_ThreadMode):
+class _DefaultReplicaThreadMode(_ThreadMode):
   """Type of default value returned by `_get_per_thread_mode()`.
 
   Used when the thread-local stack is empty.
@@ -71,95 +71,96 @@ class _DefaultTowerThreadMode(_ThreadMode):
 
   def __init__(self):
     _ThreadMode.__init__(self, _get_default_distribution_strategy(), None,
-                         _get_default_tower_context())
+                         _get_default_replica_context())
 
 
 def _get_per_thread_mode():
   try:
     return ops.get_default_graph()._distribution_strategy_stack[-1]  # pylint: disable=protected-access
   except (AttributeError, IndexError):
-    return _get_default_tower_mode()
+    return _get_default_replica_mode()
 
 
 # ------------------------------------------------------------------------------
 # Public API for accessing the current thread mode
 
 
-def get_tower_context():
-  """Returns the current TowerContext or None if in a cross-tower context.
+def get_replica_context():
+  """Returns the current ReplicaContext or None if in a cross-replica context.
 
   Note that execution:
 
-  1. starts in the default (single-tower) tower context (this function
-     will return the default TowerContext object);
-  2. switches to cross-tower context (in which case this will return
+  1. starts in the default (single-replica) replica context (this function
+     will return the default ReplicaContext object);
+  2. switches to cross-replica context (in which case this will return
      None) when entering a `with DistributionStrategy.scope():` block;
-  3. switches to a (non-default) tower context inside
-     `call_for_each_tower(fn, ...)`;
-  4. if `fn` calls `get_tower_context()->merge_call(merge_fn, ...)`, then
-     inside `merge_fn` you are back in the cross-tower context (and again
+  3. switches to a (non-default) replica context inside
+     `call_for_each_replica(fn, ...)`;
+  4. if `fn` calls `get_replica_context()->merge_call(merge_fn, ...)`, then
+     inside `merge_fn` you are back in the cross-replica context (and again
      this function will return None).
 
   Note that you can also go directly from step 1 to 4 to switch to a
-  cross-tower context for the default `DistributionStrategy`. You may
-  also switch from the cross-tower context of 4 to a tower context by
-  calling `call_for_each_tower()`, jumping back to step 3.
+  cross-replica context for the default `DistributionStrategy`. You may
+  also switch from the cross-replica context of 4 to a replica context by
+  calling `call_for_each_replica()`, jumping back to step 3.
 
   Most `DistributionStrategy` methods may only be executed in
-  a cross-tower context, in a tower context you should use the
-  `TowerContext` API instead.
+  a cross-replica context, in a replica context you should use the
+  `ReplicaContext` API instead.
 
   Returns:
-    The current `TowerContext` object when in a tower context scope, else None.
+    The current `ReplicaContext` object when in a replica context scope,
+    else None.
 
-    Exactly one of `get_tower_context()` and `get_cross_tower_context()`
+    Exactly one of `get_replica_context()` and `get_cross_replica_context()`
     will return None in a particular block.
   """
-  return _get_per_thread_mode().tower_context
+  return _get_per_thread_mode().replica_context
 
 
-def get_cross_tower_context():
-  """Returns the current DistributionStrategy if in a cross-tower context.
+def get_cross_replica_context():
+  """Returns the current DistributionStrategy if in a cross-replica context.
 
   Note that execution:
 
-  1. starts in the default (single-tower) tower context;
-  2. switches to cross-tower context when entering a
+  1. starts in the default (single-replica) replica context;
+  2. switches to cross-replica context when entering a
      `with DistributionStrategy.scope():` block;
-  3. switches to a (non-default) tower context inside
-     `call_for_each_tower(fn, ...)`;
-  4. if `fn` calls `get_tower_context()->merge_call(merge_fn, ...)`, then
-     inside `merge_fn` you are back in the cross-tower context.
+  3. switches to a (non-default) replica context inside
+     `call_for_each_replica(fn, ...)`;
+  4. if `fn` calls `get_replica_context()->merge_call(merge_fn, ...)`, then
+     inside `merge_fn` you are back in the cross-replica context.
 
   Note that you can also go directly from step 1 to 4 to switch to a
-  cross-tower context for the default `DistributionStrategy`. You may
-  also switch from the cross-tower context of 4 to a tower context by
-  calling `call_for_each_tower()`, jumping back to step 3.
+  cross-replica context for the default `DistributionStrategy`. You may
+  also switch from the cross-replica context of 4 to a replica context by
+  calling `call_for_each_replica()`, jumping back to step 3.
 
   Most `DistributionStrategy` methods may only be executed in
-  a cross-tower context.
+  a cross-replica context.
 
   Returns:
-    Returns the current `DistributionStrategy` object in a cross-tower
+    Returns the current `DistributionStrategy` object in a cross-replica
     context, or None.
 
-    Exactly one of `get_tower_context()` and `get_cross_tower_context()`
+    Exactly one of `get_replica_context()` and `get_cross_replica_context()`
     will return None in a particular block.
   """
-  return _get_per_thread_mode().cross_tower_context
+  return _get_per_thread_mode().cross_replica_context
 
 
 def get_distribution_strategy():
   """Returns the current `DistributionStrategy` object.
 
-  Prefer to use `get_tower_context()` or `get_cross_tower_context()`
+  Prefer to use `get_replica_context()` or `get_cross_replica_context()`
   instead when possible.
 
   Returns:
     A `DistributionStrategy` object. Inside a
     `with distribution_strategy.scope()` block, it returns
     `distribution_strategy`, otherwise it returns the default
-    (single-tower) `DistributionStrategy` object.
+    (single-replica) `DistributionStrategy` object.
   """
   return _get_per_thread_mode().distribution_strategy
 
@@ -180,8 +181,8 @@ def has_distribution_strategy():
 
 _defaults = {
     "distribution_strategy": None,
-    "tower_context": None,
-    "tower_mode": None
+    "replica_context": None,
+    "replica_mode": None
 }
 
 
@@ -192,14 +193,25 @@ def _get_default_distribution_strategy():
   return _defaults["distribution_strategy"]
 
 
-def _get_default_tower_context():
-  if _defaults["tower_context"] is None:
-    _defaults["tower_context"] = distribute_lib.TowerContext(
-        _get_default_distribution_strategy(), tower_id=0)
-  return _defaults["tower_context"]
+def _get_default_replica_context():
+  if _defaults["replica_context"] is None:
+    _defaults["replica_context"] = distribute_lib.ReplicaContext(
+        _get_default_distribution_strategy(), replica_id=0)
+  return _defaults["replica_context"]
 
 
-def _get_default_tower_mode():
-  if _defaults["tower_mode"] is None:
-    _defaults["tower_mode"] = _DefaultTowerThreadMode()
-  return _defaults["tower_mode"]
+def _get_default_replica_mode():
+  if _defaults["replica_mode"] is None:
+    _defaults["replica_mode"] = _DefaultReplicaThreadMode()
+  return _defaults["replica_mode"]
+
+
+#-------------------------------------------------------------------------------
+# For compatibility during the tower -> replica transistion.
+_CrossTowerThreadMode = _CrossReplicaThreadMode
+_InTowerThreadMode = _InReplicaThreadMode
+_DefaultTowerThreadMode = _DefaultReplicaThreadMode
+get_tower_context = get_replica_context
+get_cross_tower_context = get_cross_replica_context
+_get_default_tower_context = _get_default_replica_context
+_get_default_tower_mode = _get_default_replica_mode
