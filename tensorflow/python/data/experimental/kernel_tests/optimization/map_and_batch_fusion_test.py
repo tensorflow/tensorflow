67732a5e16e11e309749aa609d37ad45077de35a
Jiri Simsa <jsimsa@google.com>
2018-10-19 11:09:52 -0700

    [tf.data] Internal cleanup of optimization tests.
    PiperOrigin-RevId: 217898414

--- tensorflow/python/data/experimental/kernel_tests/optimization/map_and_batch_fusion_test.py
+++ tensorflow/python/data/experimental/kernel_tests/optimization/map_and_batch_fusion_test.py
@@ -12,47 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for the `LatencyAllEdges` optimization."""
+"""Tests for the `MapAndBatchFusion` optimization."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.python.data.experimental.kernel_tests import stats_dataset_test_base
 from tensorflow.python.data.experimental.ops import optimization
-from tensorflow.python.data.experimental.ops import stats_ops
+from tensorflow.python.data.kernel_tests import test_base
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.framework import errors
 from tensorflow.python.platform import test
 
 
-class LatencyAllEdgesTest(stats_dataset_test_base.StatsDatasetTestBase):
+class MapAndBatchFusionTest(test_base.DatasetTestBase):
 
-  def testLatencyStatsOptimization(self):
-    stats_aggregator = stats_ops.StatsAggregator()
-    dataset = dataset_ops.Dataset.from_tensors(1).apply(
+  def testMapAndBatchFusion(self):
+    dataset = dataset_ops.Dataset.range(10).apply(
         optimization.assert_next(
-            ["LatencyStats", "Map", "LatencyStats", "Prefetch",
-             "LatencyStats"])).map(lambda x: x * x).prefetch(1).apply(
-                 stats_ops.set_stats_aggregator(stats_aggregator))
+            ["MapAndBatch"])).map(lambda x: x * x).batch(10)
     options = dataset_ops.Options()
-    options.experimental_latency_all_edges = True
+    options.experimental_map_and_batch_fusion = True
     dataset = dataset.with_options(options)
-    iterator = dataset.make_initializable_iterator()
+    iterator = dataset.make_one_shot_iterator()
     get_next = iterator.get_next()
-    summary_t = stats_aggregator.get_summary()
 
     with self.cached_session() as sess:
-      sess.run(iterator.initializer)
-      self.assertEqual(1, sess.run(get_next))
+      self.assertAllEqual([x * x for x in range(10)], sess.run(get_next))
       with self.assertRaises(errors.OutOfRangeError):
         sess.run(get_next)
-      summary_str = sess.run(summary_t)
-      self._assertSummaryHasCount(summary_str,
-                                  "record_latency_TensorDataset/_1", 1)
-      self._assertSummaryHasCount(summary_str, "record_latency_MapDataset/_4",
-                                  1)
-      self._assertSummaryHasCount(summary_str,
-                                  "record_latency_PrefetchDataset/_6", 1)
 
 
 if __name__ == "__main__":
