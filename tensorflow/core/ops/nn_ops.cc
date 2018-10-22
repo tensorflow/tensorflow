8d4805d8a3d2b2b48c69d9cd7f358996cb9a87a1
mbhuiyan <mohammad.ashraf.bhuiyan@intel.com>
2018-10-22 11:09:58 -0700

    cleaning up MKL ML code in LRN kernel

--- tensorflow/core/ops/nn_ops.cc
+++ tensorflow/core/ops/nn_ops.cc
@@ -2172,11 +2172,7 @@ REGISTER_OP("_MklLRN")
     .Input("input: T")
     .Input("mkl_input: uint8")
     .Output("output: T")
-#ifdef INTEL_MKL_ML_ONLY
-    .Output("workspace: T")
-#else
     .Output("workspace: uint8")
-#endif
     .Output("mkl_output: uint8")
     .Output("mkl_workspace: uint8")
     .Attr("depth_radius: int = 5")
@@ -2200,11 +2196,7 @@ REGISTER_OP("_MklLRNGrad")
     .Input("input_grads: T")
     .Input("input_image: T")
     .Input("output_image: T")
-#ifdef INTEL_MKL_ML_ONLY
-    .Input("workspace: T")
-#else
     .Input("workspace: uint8")
-#endif
     .Input("mkl_input_grads: uint8")
     .Input("mkl_input_image: uint8")
     .Input("mkl_output_image: uint8")
