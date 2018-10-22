45137afd38ac86c1bb65f371ad89739b915329aa
A. Unique TensorFlower <gardener@tensorflow.org>
2018-10-22 12:45:57 -0700

    [TF:XLA] Test uint64 and int64 of bitcast op.
    PiperOrigin-RevId: 218215409

--- tensorflow/compiler/tests/unary_ops_test.py
+++ tensorflow/compiler/tests/unary_ops_test.py
@@ -724,6 +724,15 @@ class UnaryOpsTest(xla_test.XLATestCase):
         lambda x: array_ops.bitcast(x, dtypes.int32),
         np.array([1e-45, 1.0], np.float32),
         expected=np.array([1, 0x3f800000], np.int32))
+    if np.int64 in self.numeric_types:
+      self._assertOpOutputMatchesExpected(
+          lambda x: array_ops.bitcast(x, dtypes.int64),
+          np.array([1, 0x100000003f800000], np.uint64),
+          expected=np.array([1, 0x100000003f800000], np.int64))
+      self._assertOpOutputMatchesExpected(
+          lambda x: array_ops.bitcast(x, dtypes.uint64),
+          np.array([1, 0x100000003f800000], np.int64),
+          expected=np.array([1, 0x100000003f800000], np.uint64))
 
   def testInvertPermutation(self):
     self._assertOpOutputMatchesExpected(
