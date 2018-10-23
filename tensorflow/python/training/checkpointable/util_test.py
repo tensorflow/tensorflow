ec1d53dc794e010812f3e078eb04b445002c93ad
Julian Niedermeier <jpniedermeier@gmail.com>
2018-10-23 22:30:18 +0200

    Added unit test

--- tensorflow/python/training/checkpointable/util_test.py
+++ tensorflow/python/training/checkpointable/util_test.py
@@ -1324,6 +1324,21 @@ class CheckpointingTests(test.TestCase):
       train_fn()
       self.assertEqual(42., self.evaluate(optimizer.variables()[0]))
 
+  @test_util.run_in_graph_and_eager_modes
+  def test_restore_after_adding_empty_checkpointable_data_structure(self):
+    with ops.Graph().as_default(), self.session(graph=ops.get_default_graph()):
+      model = NonLayerCheckpointable()
+      checkpoint = checkpointable_utils.Checkpoint(model=model)
+      checkpoint.restore(None).initialize_or_restore()
+      checkpoint_directory = self.get_temp_dir()
+      checkpoint_prefix = os.path.join(checkpoint_directory, "ckpt")
+      save_path = checkpoint.save(checkpoint_prefix)
+    with ops.Graph().as_default(), self.session(graph=ops.get_default_graph()):
+      model = NonLayerCheckpointable()
+      model.dict = {"a": 1}
+      model.list = {"b": 1}
+      checkpoint.restore(save_path).assert_consumed().run_restore_ops()
+
 
 class _ManualScope(tracking.Checkpointable):
 
