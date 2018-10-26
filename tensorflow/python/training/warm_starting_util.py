901c10f6df188cb2154458021557b67c7945129d
Eddie Zhou <eddz@google.com>
2018-10-26 10:18:48 -0700

    Fix formatting for WarmStartSettings documentation, and add some clarifying notes (mirrored for warm_starting_util.warm_start).
    PiperOrigin-RevId: 218872678

--- tensorflow/python/training/warm_starting_util.py
+++ tensorflow/python/training/warm_starting_util.py
@@ -380,23 +380,32 @@ def warm_start(ckpt_to_initialize_from,
 
       - A regular expression (string) that captures which variables to
         warm-start (see tf.get_collection).  This expression will only consider
-        variables in the TRAINABLE_VARIABLES collection.
-      - A list of Variables to warm-start.
-      - A list of strings, each representing a full variable name to warm-start.
+        variables in the TRAINABLE_VARIABLES collection -- if you need to
+        warm-start non_TRAINABLE vars (such as optimizer accumulators or batch
+        norm statistics), please use the below option.
+      - A list of Variables to warm-start.  If you do not have access to the
+        `Variable` objects at the call site, please use the below option.
+      - A list of strings, each a regex scope provided to tf.get_collection with
+        GLOBAL_VARIABLES (please see tf.get_collection).  For backwards
+        compatibility reasons, this is separate from the single-string argument
+        type.
       - `None`, in which case only variables specified in
         `var_name_to_vocab_info` will be warm-started.
 
       Defaults to `'.*'`, which warm-starts all variables in the
-      TRAINABLE_VARIABLES collection.  Note that this excludes variables such as
-      accumulators and moving statistics from batch norm.
+      TRAINABLE_VARIABLES collection.  Note that this excludes variables such
+      as accumulators and moving statistics from batch norm.
     var_name_to_vocab_info: [Optional] Dict of variable names (strings) to
-      VocabInfo. The variable names should be "full" variables, not the names
-      of the partitions.  If not explicitly provided, the variable is assumed to
-      have no vocabulary.
+      `tf.estimator.VocabInfo`. The variable names should be "full" variables,
+      not the names of the partitions.  If not explicitly provided, the variable
+      is assumed to have no (changes to) vocabulary.
     var_name_to_prev_var_name: [Optional] Dict of variable names (strings) to
       name of the previously-trained variable in `ckpt_to_initialize_from`. If
       not explicitly provided, the name of the variable is assumed to be same
-      between previous checkpoint and current model.
+      between previous checkpoint and current model.  Note that this has no
+      effect on the set of variables that is warm-started, and only controls
+      name mapping (use `vars_to_warm_start` for controlling what variables to
+      warm-start).
   Raises:
     ValueError: If the WarmStartSettings contains prev_var_name or VocabInfo
       configuration for variable names that are not used.  This is to ensure
