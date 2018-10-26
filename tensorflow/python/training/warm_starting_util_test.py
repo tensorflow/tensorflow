1d41938333996ec5325faf0d0359463785847259
Eddie Zhou <eddz@google.com>
2018-10-26 10:18:48 -0700

    Fix formatting for WarmStartSettings documentation, and add some clarifying notes (mirrored for warm_starting_util.warm_start).
    PiperOrigin-RevId: 218872678

--- tensorflow/python/training/warm_starting_util_test.py
+++ tensorflow/python/training/warm_starting_util_test.py
@@ -71,6 +71,22 @@ class WarmStartingUtilTest(test.TestCase):
           var = var._get_variable_list()
         return var, sess.run(var)
 
+  def _create_prev_run_vars(self,
+                            var_names,
+                            shapes,
+                            initializers):
+    with ops.Graph().as_default() as g:
+      with self.session(graph=g) as sess:
+        all_vars = []
+        for var_name, shape, initializer in zip(var_names, shapes,
+                                                initializers):
+          all_vars.append(variable_scope.get_variable(
+              var_name,
+              shape=shape,
+              initializer=initializer))
+        self._write_checkpoint(sess)
+        return [sess.run(var) for var in all_vars]
+
   def _create_dummy_inputs(self):
     return {
         "sc_int": array_ops.sparse_placeholder(dtypes.int32),
@@ -463,6 +479,46 @@ class WarmStartingUtilTest(test.TestCase):
         sess.run(variables.global_variables_initializer())
         # Verify weights were correctly warm-started (init overridden to ones).
         self.assertAllEqual(var.eval(), prev_int_val)
+
+  def testWarmStart_ListOfRegexes(self):
+    # Save checkpoint from which to warm-start.
+    [prev_v1_val, prev_v1_momentum_val,
+     prev_v2_val, _] = self._create_prev_run_vars(
+         var_names=["v1", "v1/Momentum", "v2", "v2/Momentum"],
+         shapes=[[10, 1]] * 4,
+         initializers=[ones()] * 4)
+
+    # New graph, new session with warm-starting.
+    with ops.Graph().as_default() as g:
+      with self.session(graph=g) as sess:
+        # Initialize with zeros.
+        v1 = variable_scope.get_variable(
+            "v1",
+            shape=[10, 1],
+            initializer=zeros())
+        v1_momentum = variable_scope.get_variable(
+            "v1/Momentum",
+            shape=[10, 1],
+            initializer=zeros())
+        v2 = variable_scope.get_variable(
+            "v2",
+            shape=[10, 1],
+            initializer=zeros())
+        v2_momentum = variable_scope.get_variable(
+            "v2/Momentum",
+            shape=[10, 1],
+            initializer=zeros())
+        ws_util.warm_start(self.get_temp_dir(),
+                           # This warm-starts both v1 and v1/Momentum, but only
+                           # v2 (and not v2/Momentum).
+                           vars_to_warm_start=["v1", "v2[^/]"])
+        sess.run(variables.global_variables_initializer())
+        # Verify the selection of weights were correctly warm-started (init
+        # overridden to ones).
+        self.assertAllEqual(v1.eval(), prev_v1_val)
+        self.assertAllEqual(v1_momentum.eval(), prev_v1_momentum_val)
+        self.assertAllEqual(v2.eval(), prev_v2_val)
+        self.assertAllEqual(v2_momentum.eval(), np.zeros([10, 1]))
 
   def testWarmStart_SparseColumnIntegerized(self):
     # Create feature column.
