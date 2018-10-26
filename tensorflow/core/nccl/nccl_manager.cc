fc6cd33c334f88759ce637e29e1586733076e094
A. Unique TensorFlower <gardener@tensorflow.org>
2018-10-26 13:54:44 -0700

    Move contrib/nccl to core/nccl.
    PiperOrigin-RevId: 218908694

--- tensorflow/core/nccl/nccl_manager.cc
+++ tensorflow/core/nccl/nccl_manager.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/contrib/nccl/kernels/nccl_manager.h"
+#include "tensorflow/core/nccl/nccl_manager.h"
 
 #include <utility>
 
