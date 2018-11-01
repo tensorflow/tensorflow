3852e79f606d883dafb394afd0ce709a75da30cc
Allen Lavoie <allenl@google.com>
2018-11-01 10:19:21 -0700

    Add variable support to 2.x SavedModel export
    This is still focused on exporting to Session-based import APIs. Should be ready to add to the 2.x preview after this and possibly a few follow up fixes (for the reference cycle issue in the skipped unit test).
    
    Maps resources which are owned by the eager context to new resources in the Graph being exported.
    
    PiperOrigin-RevId: 219655229

--- tensorflow/python/saved_model/export.py
+++ tensorflow/python/saved_model/export.py
@@ -22,16 +22,19 @@ import collections
 import os
 
 from tensorflow.core.protobuf import saved_model_pb2
+from tensorflow.python.eager import context
 from tensorflow.python.eager import def_function
 from tensorflow.python.eager import function
 from tensorflow.python.framework import ops
 from tensorflow.python.lib.io import file_io
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import resource_variable_ops
 from tensorflow.python.saved_model import constants
 from tensorflow.python.saved_model import signature_constants
 from tensorflow.python.saved_model import signature_def_utils
-from tensorflow.python.saved_model import utils
-from tensorflow.python.training.checkpointable import base as checkpointable
+from tensorflow.python.saved_model import utils_impl
+from tensorflow.python.training.checkpointable import base
+from tensorflow.python.training.checkpointable import util
 from tensorflow.python.util import compat
 from tensorflow.python.util import nest
 
@@ -109,17 +112,134 @@ def _normalize_outputs(outputs, function_name, signature_key):
 
 
 def _tensor_dict_to_tensorinfo(tensor_dict):
-  return {key: utils.build_tensor_info(value)
+  return {key: utils_impl.build_tensor_info(value)
           for key, value in tensor_dict.items()}
 
 
-def _generate_signatures(signature_functions):
+def _map_captured_resources_to_created_resources(
+    original_captures, resource_map):
+  """Maps eager resources captured by a function to Graph resources for export.
+
+  Args:
+    original_captures: A dictionary mapping from resource tensors captured by
+      the function to interior placeholders for those resources (inside the
+      function body).
+    resource_map: A dictionary mapping from resource tensors owned by the eager
+      context to resource tensors in the exported graph.
+
+  Returns:
+    A dictionary mapping from interior placeholders in the function body to
+    exterior stand-in resource tensors which belong to the exported graph.
+
+  Raises:
+    AssertionError: If the function references a resource which is not part of
+      `resource_map`.
+  """
+  export_captures = {}
+  for exterior, interior in original_captures.items():
+    mapped_resource = resource_map.get(exterior, None)
+    if mapped_resource is None:
+      raise AssertionError(
+          ("Tried to export a function which references untracked stateful "
+           "object {}. Stateful TensorFlow objects (e.g. tf.Variable) must "
+           "be tracked by the main object. Objects may be tracked by "
+           "assigning them to an attribute of another tracked object, or to "
+           "an attribute of the main object directly.")
+          .format(interior))
+    export_captures[interior] = mapped_resource
+  return export_captures
+
+
+def _map_function_inputs_to_created_inputs(
+    function_inputs, export_captures, signature_key, function_name):
+  """Creates exterior placeholders in the exported graph for function inputs.
+
+  Functions have two types of inputs: tensors captured from the outside (eager)
+  context, and arguments to the function which we expect to receive from the
+  user at each call. `_map_captured_resources_to_created_resources` replaces
+  captured tensors with stand-ins (typically these are resource dtype tensors
+  associated with variables). `_map_function_inputs_to_created_inputs` runs over
+  every input, either captured or argument. For captures, it uses the mapped
+  resource from `export_captures`. For arguments, it creates a new placeholder
+  which will belong to the exported graph rather than the function body.
+
+  Args:
+    function_inputs: A list of all placeholders in the function body.
+    export_captures: A dictionary mapping from interior placeholders in the
+      function body to exterior stand-in resource tensors which belong to the
+      exported graph (see `_map_captured_resources_to_created_resources`).
+    signature_key: The name of the signature being exported, for error messages.
+    function_name: The name of the function, for error messages.
+
+  Returns:
+    A tuple of (mapped_inputs, exterior_placeholders)
+      mapped_inputs: A list with entries corresponding to `function_inputs`
+        containing all of the inputs of the function gathered from the exported
+        graph (both captured resources and arguments).
+      exterior_argument_placeholders: A dictionary mapping from argument names
+        to placeholders in the exported graph, containing the explicit arguments
+        to the function which a user is expected to provide.
+
+  Raises:
+    ValueError: If argument names are not unique.
+  """
+  # `exterior_argument_placeholders` holds placeholders which are outside the
+  # function body, directly contained in a MetaGraph of the SavedModel. The
+  # function body itself contains nearly identical placeholders used when
+  # running the function, but these exterior placeholders allow Session-based
+  # APIs to call the function using feeds and fetches which name Tensors in the
+  # MetaGraph.
+  exterior_argument_placeholders = {}
+  mapped_inputs = []
+  for placeholder in function_inputs:
+    mapped_resource_tensor = export_captures.get(placeholder, None)
+    if mapped_resource_tensor is not None:
+      # This is a captured resource.
+      mapped_inputs.append(mapped_resource_tensor)
+      continue
+    # `export_captures` contains an exhaustive set of captures, so if we don't
+    # find the input there then we now know we have an argument.
+    user_input_name = compat.as_str_any(
+        placeholder.op.get_attr("_user_specified_name"))
+    # If the internal placeholders for a function have names which were
+    # uniquified by TensorFlow, then a single user-specified argument name
+    # must refer to multiple Tensors. The resulting signatures would be
+    # confusing to call. Instead, we throw an exception telling the user to
+    # specify explicit names.
+    if user_input_name != placeholder.op.name:
+      # This should be unreachable, since concrete functions may not be
+      # generated with non-unique argument names.
+      raise ValueError(
+          ("Got non-flat/non-unique argument names for SavedModel "
+           "signature '{}': more than one argument to '{}' was named '{}'. "
+           "Signatures have one Tensor per named input, so to have "
+           "predictable names Python functions used to generate these "
+           "signatures should avoid *args and Tensors in nested "
+           "structures unless unique names are specified for each. Use "
+           "tf.TensorSpec(..., name=...) to provide a name for a Tensor "
+           "input.")
+          .format(signature_key, compat.as_str_any(function_name),
+                  user_input_name))
+    arg_placeholder = array_ops.placeholder(
+        shape=placeholder.shape,
+        dtype=placeholder.dtype,
+        name="{}_{}".format(signature_key, user_input_name))
+    exterior_argument_placeholders[user_input_name] = arg_placeholder
+    mapped_inputs.append(arg_placeholder)
+  return mapped_inputs, exterior_argument_placeholders
+
+
+def _generate_signatures(signature_functions, resource_map):
   """Validates and calls `signature_functions` in the default graph.
 
   Args:
     signature_functions: A dictionary mapping string keys to concrete TensorFlow
       functions (e.g. from `_canonicalize_signatures`) which will be used to
       generate SignatureDefs.
+    resource_map: A dictionary mapping from resource tensors in the eager
+      context to resource tensors in the Graph being exported. This dictionary
+      is used to re-bind resources captured by functions to tensors which will
+      exist in the SavedModel.
 
   Returns:
     Each function in the `signature_functions` dictionary is called with
@@ -139,59 +259,88 @@ def _generate_signatures(signature_functions):
   """
   signatures = {}
   for signature_key, func in sorted(signature_functions.items()):
-    func.add_to_graph(register_gradient_functions=True)
-    # `exterior_placeholders` holds placeholders which are outside the function
-    # body, directly contained in a MetaGraph of the SavedModel. The function
-    # body itself contains nearly identical placeholders used when running the
-    # function, but these exterior placeholders allow Session-based APIs to call
-    # the function using feeds and fetches which name Tensors in the MetaGraph.
-    exterior_placeholders = {}
-    kwargs = {}
-    for placeholder in func.inputs:
-      user_input_name = compat.as_str_any(
-          placeholder.op.get_attr("_user_specified_name"))
-      # If the internal placeholders for a function have names which were
-      # uniquified by TensorFlow, then a single user-specified argument name
-      # must refer to multiple Tensors. The resulting signatures would be
-      # confusing to call. Instead, we throw an exception telling the user to
-      # specify explicit names.
-      if user_input_name != placeholder.op.name:
-        # This should be unreachable, since concrete functions may not be
-        # generated with non-unique argument names.
-        raise ValueError(
-            ("Got non-flat/non-unique argument names for SavedModel "
-             "signature '{}': more than one argument to '{}' was named '{}'. "
-             "Signatures have one Tensor per named input, so to have "
-             "predictable names Python functions used to generate these "
-             "signatures should avoid *args and Tensors in nested "
-             "structures unless unique names are specified for each. Use "
-             "tf.TensorSpec(..., name=...) to provide a name for a Tensor "
-             "input.")
-            .format(signature_key, compat.as_str_any(func.name),
-                    user_input_name))
-      arg_placeholder = array_ops.placeholder(
-          shape=placeholder.shape,
-          dtype=placeholder.dtype,
-          name="{}_{}".format(signature_key, user_input_name))
-      exterior_placeholders[user_input_name] = arg_placeholder
-      kwargs[user_input_name] = arg_placeholder
+    # Register the inference function for this signature in the exported
+    # graph. There is no direct use for the gradient of this function, so we
+    # don't generate/register a gradient function here (but may end up with one
+    # if another function relies on it). Users can still take symbolic gradients
+    # of the function on import, the gradient just won't be in the saved
+    # graph. When exporting a signature which already computes gradients, this
+    # stops us from taking needless second-order gradients.
+    func.add_to_graph(register_gradient_functions=False)
+    export_captures = _map_captured_resources_to_created_resources(
+        func.graph.captures, resource_map)
+    mapped_inputs, exterior_argument_placeholders = (
+        _map_function_inputs_to_created_inputs(
+            func.inputs, export_captures, signature_key, func.name))
+    # Calls the function quite directly, since we have new captured resource
+    # tensors we need to feed in which weren't part of the original function
+    # definition.
+    # pylint: disable=protected-access
     outputs = _normalize_outputs(
-        func(**kwargs), func.name, signature_key)
+        func._build_call_outputs(
+            func._inference_function.call(context.context(), mapped_inputs)),
+        func.name, signature_key)
+    # pylint: enable=protected-access
     signatures[signature_key] = signature_def_utils.build_signature_def(
-        _tensor_dict_to_tensorinfo(exterior_placeholders),
+        _tensor_dict_to_tensorinfo(exterior_argument_placeholders),
         _tensor_dict_to_tensorinfo(outputs))
   return signatures
 
 
-def _make_graph_def(signature_functions):
+def _map_resources(accessible_objects):
+  """Makes new resource handle ops corresponding to existing resource tensors.
+
+  Creates resource handle ops in the current default graph, whereas
+  `accessible_objects` will be from an eager context. Resource mapping adds
+  resource handle ops to the main GraphDef of a SavedModel, which allows the C++
+  loader API to interact with variables.
+
+  Args:
+    accessible_objects: A list of objects, some of which may contain resources,
+      to create replacements for.
+
+  Returns:
+    A tuple of (object_map, resource_map):
+      object_map: A dictionary mapping from object in `accessible_objects` to
+        replacement objects created to hold the new resource tensors.
+      resource_map: A dictionary mapping from resource tensors extracted from
+        `accessible_objects` to newly created resource tensors.
+  """
+  # TODO(allenl, rohanj): Map generic resources rather than just variables.
+  # TODO(allenl): Handle MirroredVariables and other types of variables which
+  # may need special casing.
+  object_map = {}
+  resource_map = {}
+  for obj in accessible_objects:
+    if resource_variable_ops.is_resource_variable(obj):
+      new_variable = resource_variable_ops.copy_to_graph_uninitialized(obj)
+      object_map[obj] = new_variable
+      resource_map[obj.handle] = new_variable.handle
+  return object_map, resource_map
+
+
+def _make_graph_def(root, signature_functions, object_saver):
   """Generates and exports call ops for `signature_functions`."""
-  # TODO(allenl): Handle variables
   signatures = {}
+  # List objects from the eager context to make sure Optimizers give us the
+  # right Graph-dependent variables.
+  accessible_objects = util.list_objects(root)
   exported_graph = ops.Graph()
   with exported_graph.as_default():
-    signatures = _generate_signatures(signature_functions)
+    object_map, resource_map = _map_resources(accessible_objects)
+  # Saving an object-based checkpoint again gathers variables. We need to do the
+  # gathering from the eager context so Optimizers save the right set of
+  # variables, but want any operations associated with the save/restore to be in
+  # the exported graph (thus the `to_graph` argument).
+  saver = object_saver.freeze(object_map=object_map, to_graph=exported_graph)
+  with exported_graph.as_default():
+    signatures = _generate_signatures(signature_functions, resource_map)
+    saver_def = saver.to_proto()
   graph_def = exported_graph.as_graph_def(add_shapes=True)
-  return graph_def, signatures
+  # Clean reference cycles so repeated export()s don't make work for the garbage
+  # collector.
+  ops.dismantle_graph(exported_graph)
+  return graph_def, signatures, saver_def
 
 
 def export(obj, export_dir, signatures=None):
@@ -244,6 +393,14 @@ def export(obj, export_dir, signatures=None):
           tf.TensorSpec(shape=[None, 3], dtype=tf.float32, name="inp")))
   ```
 
+  Variables must be tracked by assigning them to an attribute of a tracked
+  object or to an attribute of `obj` directly. TensorFlow objects (e.g. layers
+  from `tf.keras.layers`, optimizers from `tf.train`) track their variables
+  automatically. This is the same tracking scheme that `tf.train.Checkpoint`
+  uses, and an exported `Checkpoint` object may be restored as a training
+  checkpoint by pointing `tf.train.Checkpoint.restore` to the SavedModel's
+  "variables/" subdirectory.
+
   Args:
     obj: A checkpointable object to export.
     export_dir: A directory in which to write the SavedModel.
@@ -261,22 +418,27 @@ def export(obj, export_dir, signatures=None):
     ValueError: If `obj` is not checkpointable.
   """
   # pylint: enable=line-too-long
-  if not isinstance(obj, checkpointable.CheckpointableBase):
+  if not isinstance(obj, base.CheckpointableBase):
     raise ValueError(
         "Expected a Checkpointable object for export, got {}.".format(obj))
+  object_saver = util.CheckpointableSaver(obj)
+  utils_impl.get_or_create_variables_dir(export_dir)
+  object_saver.save(utils_impl.get_variables_path(export_dir))
+
   signatures = _canonicalize_signatures(signatures)
-  graph_def, signatures = _make_graph_def(signatures)
+  graph_def, signatures, saver_def = _make_graph_def(
+      obj, signatures, object_saver)
   saved_model = saved_model_pb2.SavedModel()
   saved_model.saved_model_schema_version = (
       constants.SAVED_MODEL_SCHEMA_VERSION)
   meta_graph_def = saved_model.meta_graphs.add()
+  meta_graph_def.saver_def.CopyFrom(saver_def)
   # TODO(allenl): Factor out some subset of SavedModelBuilder which is 2.x
   # compatible (no sessions) and share it with this export API rather than
   # making a SavedModel proto and writing it directly.
   meta_graph_def.graph_def.MergeFrom(graph_def)
   for signature_key, signature in signatures.items():
     meta_graph_def.signature_def[signature_key].MergeFrom(signature)
-  file_io.recursive_create_dir(export_dir)
   path = os.path.join(
       compat.as_bytes(export_dir),
       compat.as_bytes(constants.SAVED_MODEL_FILENAME_PB))
--- tensorflow/python/saved_model/export_test.py
+++ tensorflow/python/saved_model/export_test.py
@@ -20,16 +20,42 @@ from __future__ import print_function
 
 import os
 
+from tensorflow.python.eager import backprop
 from tensorflow.python.eager import def_function
 from tensorflow.python.eager import test
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_spec
+from tensorflow.python.framework import test_util
+from tensorflow.python.keras.engine import training
+from tensorflow.python.keras.layers import core
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import variables
 from tensorflow.python.saved_model import export
 from tensorflow.python.saved_model import loader
 from tensorflow.python.saved_model import signature_constants
+from tensorflow.python.training import adam
 from tensorflow.python.training.checkpointable import tracking
+
+
+class _ModelWithOptimizer(training.Model):
+
+  def __init__(self):
+    super(_ModelWithOptimizer, self).__init__()
+    self.dense = core.Dense(1)
+    self.optimizer = adam.AdamOptimizer(0.01)
+
+  @def_function.function(
+      input_signature=(tensor_spec.TensorSpec([None, 2], dtypes.float32),
+                       tensor_spec.TensorSpec([None], dtypes.float32)))
+  def call(self, x, y):
+    with backprop.GradientTape() as tape:
+      loss = math_ops.reduce_mean((self.dense(x) - y) ** 2.)
+    trainable_variables = self.trainable_variables
+    gradients = tape.gradient(loss, trainable_variables)
+    self.optimizer.apply_gradients(zip(gradients, trainable_variables))
+    return {"loss": loss}
 
 
 class ExportTest(test.TestCase):
@@ -123,6 +149,48 @@ class ExportTest(test.TestCase):
     with self.assertRaisesRegexp(
         ValueError, "dictionary containing non-Tensor value"):
       export.export(root, export_dir, to_export)
+
+  def test_variable(self):
+    root = tracking.Checkpointable()
+    root.v1 = variables.Variable(3.)
+    root.v2 = variables.Variable(2.)
+    root.f = def_function.function(
+        lambda x: root.v1 * root.v2 * x)
+    root.f(constant_op.constant(1.))
+    to_export = root.f.get_concrete_function(constant_op.constant(1.))
+    export_dir = os.path.join(self.get_temp_dir(), "saved_model")
+    export.export(root, export_dir, to_export)
+    self.assertAllEqual({"output_0": 12.},
+                        self._import_and_infer(export_dir, {"x": 2.}))
+
+  def test_optimizer(self):
+    x = constant_op.constant([[3., 4.]])
+    y = constant_op.constant([2.])
+    model = _ModelWithOptimizer()
+    first_loss = model(x, y)
+    export_dir = os.path.join(self.get_temp_dir(), "saved_model")
+    export.export(model, export_dir, model.call)
+    second_loss = model(x, y)
+    self.assertNotEqual(first_loss, second_loss)
+    self.assertAllClose(
+        second_loss,
+        self._import_and_infer(export_dir, {"x": [[3., 4.]], "y": [2.]}))
+
+
+class MemoryTests(test.TestCase):
+
+  def setUp(self):
+    self._model = _ModelWithOptimizer()
+
+  @test_util.assert_no_garbage_created
+  def test_no_reference_cycles(self):
+    # TODO(allenl): Fix remaining reference cycles
+    self.skipTest("Not working")
+    x = constant_op.constant([[3., 4.]])
+    y = constant_op.constant([2.])
+    self._model(x, y)
+    export_dir = os.path.join(self.get_temp_dir(), "saved_model")
+    export.export(self._model, export_dir, self._model.call)
 
 
 if __name__ == "__main__":
