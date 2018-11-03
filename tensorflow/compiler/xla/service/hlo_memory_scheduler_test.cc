0d2c953b3adb53c6b73074df6ff4f20baee183ae
Mark Heffernan <meheff@google.com>
2018-11-02 17:45:02 -0700

    Clone schedule when cloning an HLO module. Also, add trivial scheduler to HloMemoryScheduler which creates a naive schedule based on a DFS walk without the need for a size function or points-to-analysis.
    PiperOrigin-RevId: 219890103

--- tensorflow/compiler/xla/service/hlo_memory_scheduler_test.cc
+++ tensorflow/compiler/xla/service/hlo_memory_scheduler_test.cc
@@ -309,5 +309,40 @@ TEST_F(HloSchedulingTest, HeapSimulatorAccountsForSubcomputations) {
                     .ValueOrDie());
 }
 
+TEST_F(HloSchedulingTest, TrivialScheduler) {
+  const char* const hlo_string = R"(
+HloModule ModuleWithWhile
+
+body {
+  param.b = (s32[], s32[]) parameter(0)
+  gte.0 = s32[] get-tuple-element(param.b), index=0
+  gte.1 = s32[] get-tuple-element(param.b), index=1
+  add = s32[] add(gte.0, gte.1)
+  ROOT tuple = (s32[], s32[]) tuple(gte.0, add)
+}
+
+cond {
+  param.c = (s32[], s32[]) parameter(0)
+  ROOT constant = pred[] constant(true)
+}
+
+ENTRY main {
+  init = (s32[], s32[]) parameter(0)
+  ROOT while = (s32[], s32[]) while(init), condition=cond, body=body
+}
+)";
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          ParseHloString(hlo_string));
+  EXPECT_FALSE(module->has_schedule());
+  TF_ASSERT_OK(HloTrivialScheduler().Run(module.get()).status());
+  ASSERT_TRUE(module->has_schedule());
+  TF_ASSERT_OK(module->schedule().Verify());
+
+  // Verify that a clone of the module also has a schedule.
+  std::unique_ptr<HloModule> clone = module->Clone();
+  ASSERT_TRUE(clone->has_schedule());
+  TF_ASSERT_OK(clone->schedule().Verify());
+}
+
 }  // namespace
 }  // namespace xla
