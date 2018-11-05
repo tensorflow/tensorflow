c8e28ff62cee8e4894bae8c4c98f39b23e75cefc
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-11-05 22:09:33 +0000

    Fix failed tests on ROCm path

--- tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_py3_core.sh
@@ -43,6 +43,7 @@ bazel test --test_sharding_strategy=disabled --config=rocm --test_tag_filters=-n
     -//tensorflow/python/distribute:distribute_coordinator_test \
     -//tensorflow/python/feature_column:feature_column_v2_test \
     -//tensorflow/python/keras:cudnn_recurrent_test \
+    -//tensorflow/python/keras:cudnn_recurrent_test_gpu \
     -//tensorflow/python/kernel_tests:batch_matmul_op_test \
     -//tensorflow/python/kernel_tests:batch_matmul_op_test_gpu \
     -//tensorflow/python/kernel_tests:conv1d_test \
