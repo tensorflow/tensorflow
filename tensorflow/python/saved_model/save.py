648f754428b9bd6c85645b9e3de8791356525d83
Allen Lavoie <allenl@google.com>
2018-11-05 16:11:41 -0800

    Add tf.saved_model.save to the 2.x API (tf.saved_model.experimental.save in 1.x)
    Renamed to "save" from "export" since "import" is a reserved keyword.
    
    Exports SavedModels, currently targeting only serving. This CL includes some extra documentation and some tangential changes to make the example unit test work.
    
    PiperOrigin-RevId: 220191697

--- tensorflow/python/saved_model/save.py
+++ tensorflow/python/saved_model/save.py
@@ -25,6 +25,7 @@ from tensorflow.core.protobuf import saved_model_pb2
 from tensorflow.python.eager import context
 from tensorflow.python.eager import def_function
 from tensorflow.python.eager import function
+from tensorflow.python.framework import meta_graph
 from tensorflow.python.framework import ops
 from tensorflow.python.lib.io import file_io
 from tensorflow.python.ops import array_ops
@@ -37,6 +38,7 @@ from tensorflow.python.training.checkpointable import base
 from tensorflow.python.training.checkpointable import util
 from tensorflow.python.util import compat
 from tensorflow.python.util import nest
+from tensorflow.python.util.tf_export import tf_export
 
 
 def _find_function_to_export(root):
@@ -51,7 +53,7 @@ def _find_function_to_export(root):
   # TODO(allenl): Automatically infer signatures for Keras functional models?
   if not functions:
     raise ValueError(
-        ("Exporting an object with no tf.saved_model_save(..., signatures=...) "
+        ("Exporting an object with no tf.saved_model.save(..., signatures=...) "
          "argument specified, and with no @tf.function-decorated methods "
          "attached to it. In the future this will be a supported use-case for "
          "Python re-import, but at the moment saving a SavedModel without "
@@ -60,7 +62,7 @@ def _find_function_to_export(root):
          "explicitly."))
   elif len(functions) > 1:
     raise ValueError(
-        ("Exporting an object with no tf.saved_model_save(..., signatures=...) "
+        ("Exporting an object with no tf.saved_model.save(..., signatures=...) "
          "argument specified, and with more than one @tf.function-decorated "
          "method attached to it: {}. The signature keys for these functions "
          "are ambiguous. Specify signature functions explicitly.").format(
@@ -370,46 +372,66 @@ def _make_graph_def(root, signature_functions, object_saver):
   return graph_def, signatures, saver_def
 
 
-def export(obj, export_dir, signatures=None):
+@tf_export("saved_model.save", v1=["saved_model.experimental.save"])
+def save(obj, export_dir, signatures=None):
   # pylint: disable=line-too-long
   """Exports the Checkpointable object `obj` to [SavedModel format](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/saved_model/README.md).
 
-  The `signatures` argument indicates TensorFlow functions which will be
+  Example usage:
+
+  ```python
+  class Adder(tf.train.Checkpoint):
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=None, dtype=tf.float32)])
+    def add(self, x):
+      return x + x + 1.
+
+  to_export = Adder()
+  tf.saved_model.save(to_export, '/tmp/adder')
+  ```
+
+  The resulting SavedModel is then servable with an input named "x", its value
+  having any shape and dtype float32.
+
+  The optional `signatures` argument controls which methods in `obj` will be
   available to programs which consume `SavedModel`s, for example serving
   APIs. Python functions may be decorated with
   `@tf.function(input_signature=...)` and passed as signatures directly, or
-  created without a signature using `@tf.function` and then converted to a
-  concrete TensorFlow function using `f.get_concrete_function(...)`.
+  lazily with a call to `get_concrete_function` on the method decorated with
+  `@tf.function`.
 
-  In either case, `Tensor` inputs to `signatures` functions which are not
-  associated with a unique Python argument name must have names explicitly
-  specified in their `tf.TensorSpec` objects. Cases where this is necessary
-  include positional arguments passed through variadic `*args` and multiple
-  `Tensor` inputs which are part of the same nested structure.
+  If the `signatures` argument is omitted, `obj` will be searched for
+  `@tf.function`-decorated methods. If exactly one `@tf.function` is found, that
+  method will be used as the default signature for the SavedModel. This behavior
+  is expected to change in the future, when a corresponding
+  `tf.saved_model.load` symbol is added. At that point signatures will be
+  completely optional, and any `@tf.function` attached to `obj` or its
+  dependencies will be exported for use with `load`.
+
+  When invoking a signature in an exported SavedModel, `Tensor` arguments are
+  identified by name. These names will come from the Python function's argument
+  names by default. They may be overridden by specifying a `name=...` argument
+  in the corresponding `tf.TensorSpec` object. Explicit naming is required if
+  multiple `Tensor`s are passed through a single argument to the Python
+  function.
 
   The outputs of functions used as `signatures` must either be flat lists, in
   which case outputs will be numbered, or a dictionary mapping string keys to
-  Tensors, in which case the string keys will be used to name outputs.
+  `Tensor`, in which case the keys will be used to name outputs.
 
-  Exporting with a signature specified:
+  Since `tf.keras.Model` objects are also Checkpointable, this function can be
+  used to export Keras models. For example, exporting with a signature
+  specified:
 
   ```python
   class Model(tf.keras.Model):
 
-    @tf.function(input_signature=tf.TensorSpec(shape=[None], dtype=tf.string))
-    def serve(serialized):
+    @tf.function(input_signature=[tf.TensorSpec(shape=[None], dtype=tf.string)])
+    def serve(self, serialized):
       ...
 
   m = Model()
-  tf.saved_model.export(m, '/tmp/saved_model/', signatures=m.serve)
-  ```
-
-  The `signatures` argument may be omitted if only one method of the exported
-  object is decorated with `tf.function` and that method has an input signature
-  specified.
-
-  ```python
-  tf.saved_model.export(m, '/tmp/saved_model/')
+  tf.saved_model.save(m, '/tmp/saved_model/')
   ```
 
   Exporting from a function without a fixed signature:
@@ -418,13 +440,13 @@ def export(obj, export_dir, signatures=None):
   class Model(tf.keras.Model):
 
     @tf.function
-    def compute(x):
+    def call(self, x):
       ...
 
   m = Model()
-  tf.saved_model.export(
+  tf.saved_model.save(
       m, '/tmp/saved_model/',
-      signatures=m.compute.get_concrete_function(
+      signatures=m.call.get_concrete_function(
           tf.TensorSpec(shape=[None, 3], dtype=tf.float32, name="inp")))
   ```
 
@@ -434,14 +456,47 @@ def export(obj, export_dir, signatures=None):
   automatically. This is the same tracking scheme that `tf.train.Checkpoint`
   uses, and an exported `Checkpoint` object may be restored as a training
   checkpoint by pointing `tf.train.Checkpoint.restore` to the SavedModel's
-  "variables/" subdirectory.
+  "variables/" subdirectory. Currently variables are the only stateful objects
+  supported by `tf.saved_model.save`, but others (e.g. tables) will be supported
+  in the future.
+
+  `tf.function` does not hard-code device annotations from outside the function
+  body, instead using the calling context's device. This means for example that
+  exporting a model which runs on a GPU and serving it on a CPU will generally
+  work, with some exceptions. `tf.device` annotations inside the body of the
+  function will be hard-coded in the exported model; this type of annotation is
+  discouraged. Device-specific operations, e.g. with "cuDNN" in the name or with
+  device-specific layouts, may cause issues. Currently a `DistributionStrategy`
+  is another exception: active distribution strategies will cause device
+  placements to be hard-coded in a function. Exporting a single-device
+  computation and importing under a `DistributionStrategy` is not currently
+  supported, but may be in the future.
+
+  SavedModels exported with `tf.saved_model.save` [strip default-valued
+  attributes](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/saved_model/README.md#stripping-default-valued-attributes)
+  automatically, which removes one source of incompatibilities when the consumer
+  of a SavedModel is running an older TensorFlow version than the
+  producer. There are however other sources of incompatibilities which are not
+  handled automatically, such as when the exported model contains operations
+  which the consumer does not have definitions for.
+
+  The current implementation of `tf.saved_model.save` targets serving use-cases,
+  but omits information which will be necessary for the planned future
+  implementation of `tf.saved_model.load`. Exported models using the current
+  `save` implementation, and other existing SavedModels, will not be compatible
+  with `tf.saved_model.load` when it is implemented. Further, `save` will in the
+  future attempt to export `@tf.function`-decorated methods which it does not
+  currently inspect, so some objects which are exportable today will raise
+  exceptions on export in the future (e.g. due to complex/non-serializable
+  default arguments). Such backwards-incompatible API changes are expected only
+  prior to the TensorFlow 2.0 release.
 
   Args:
     obj: A checkpointable object to export.
     export_dir: A directory in which to write the SavedModel.
     signatures: Optional, either a `tf.function` with an input signature
       specified or the result of `f.get_concrete_function` on a
-      `tf.function`-decorated function `f`, in which case `f` will be used to
+      `@tf.function`-decorated function `f`, in which case `f` will be used to
       generate a signature for the SavedModel under the default serving
       signature key. `signatures` may also be a dictionary, in which case it
       maps from signature keys to either `tf.function` instances with input
@@ -456,12 +511,14 @@ def export(obj, export_dir, signatures=None):
   if not isinstance(obj, base.CheckpointableBase):
     raise ValueError(
         "Expected a Checkpointable object for export, got {}.".format(obj))
+  if signatures is None:
+    # Note that we run this before saving the checkpoint, since looping over
+    # attributes may have the side effect of creating variables in some cases.
+    signatures = _find_function_to_export(obj)
   object_saver = util.CheckpointableSaver(obj)
   utils_impl.get_or_create_variables_dir(export_dir)
   object_saver.save(utils_impl.get_variables_path(export_dir))
 
-  if signatures is None:
-    signatures = _find_function_to_export(obj)
   signatures = _canonicalize_signatures(signatures)
   graph_def, signatures, saver_def = _make_graph_def(
       obj, signatures, object_saver)
@@ -476,6 +533,7 @@ def export(obj, export_dir, signatures=None):
   meta_graph_def.graph_def.MergeFrom(graph_def)
   for signature_key, signature in signatures.items():
     meta_graph_def.signature_def[signature_key].MergeFrom(signature)
+  meta_graph.strip_graph_default_valued_attrs(meta_graph_def)
   path = os.path.join(
       compat.as_bytes(export_dir),
       compat.as_bytes(constants.SAVED_MODEL_FILENAME_PB))
