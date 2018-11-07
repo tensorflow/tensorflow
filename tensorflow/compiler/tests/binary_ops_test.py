5ead90123c0c93752ac4caffae026da7af0dbaef
Peter Hawkins <phawkins@google.com>
2018-11-07 11:27:38 -0800

    [TF:XLA] Fix wrong output for Pad with int64 pad amounts.
    Re-enable some long-disabled cases in randomized test for Pad, which oddly enough turned out to still trigger bugs. Change test harness to form padding quantities by subtraction rather than addition, which stops it from trying to build unreasonably large tensors.
    
    PiperOrigin-RevId: 220498750

--- tensorflow/compiler/tests/binary_ops_test.py
+++ tensorflow/compiler/tests/binary_ops_test.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import itertools
+
 import numpy as np
 
 from tensorflow.compiler.tests import xla_test
@@ -996,13 +998,14 @@ class BinaryOpsTest(xla_test.XLATestCase):
           expected=np.array([[[[1], [2]], [[3], [4]]]], dtype=dtype))
 
   def testPad(self):
-    for dtype in self.numeric_types:
+    for dtype, pad_type in itertools.product(
+        self.numeric_types, [np.int32, np.int64]):
       self._testBinary(
           array_ops.pad,
           np.array(
               [[1, 2, 3], [4, 5, 6]], dtype=dtype),
           np.array(
-              [[1, 2], [2, 1]], dtype=np.int32),
+              [[1, 2], [2, 1]], dtype=pad_type),
           expected=np.array(
               [[0, 0, 0, 0, 0, 0],
                [0, 0, 1, 2, 3, 0],
@@ -1016,7 +1019,7 @@ class BinaryOpsTest(xla_test.XLATestCase):
           np.array(
               [[1, 2, 3], [4, 5, 6]], dtype=dtype),
           np.array(
-              [[0, 3], [2, 1]], dtype=np.int32),
+              [[0, 3], [2, 1]], dtype=pad_type),
           expected=np.array(
               [[7, 7, 1, 2, 3, 7],
                [7, 7, 4, 5, 6, 7],
