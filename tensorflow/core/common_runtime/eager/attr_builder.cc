ab93388bc56034bff6210c409c6fa3805a553351
Igor Ganichev <iga@google.com>
2018-11-07 17:30:11 -0800

    Fix a bug in eager FingerprintCat128
    The buggy version was ignoring the high 64 bits.
    
    PiperOrigin-RevId: 220558690

--- tensorflow/core/common_runtime/eager/attr_builder.cc
+++ tensorflow/core/common_runtime/eager/attr_builder.cc
@@ -184,7 +184,7 @@ namespace {
 inline tensorflow::Fprint128 FingerprintCat128(const tensorflow::Fprint128& a,
                                                const tensorflow::Fprint128& b) {
   return {tensorflow::FingerprintCat64(a.low64, b.low64),
-          tensorflow::FingerprintCat64(a.low64, b.low64)};
+          tensorflow::FingerprintCat64(a.high64, b.high64)};
 }
 
 void CombineUnordered(const tensorflow::Fprint128& a,
