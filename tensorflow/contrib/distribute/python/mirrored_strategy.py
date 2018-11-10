45c1df9b8caaec56ffba107629a38c87b7dc375c
A. Unique TensorFlower <gardener@tensorflow.org>
2018-11-09 23:21:58 -0800

    Remove prefetch_on_device argument to MirroredStrategy and OneDeviceStrategy.
    PiperOrigin-RevId: 220911217

--- tensorflow/contrib/distribute/python/mirrored_strategy.py
+++ tensorflow/contrib/distribute/python/mirrored_strategy.py
@@ -348,8 +348,6 @@ class MirroredStrategy(distribute_lib.DistributionStrategy):
       specified.
     cross_device_ops: optional, a descedant of `CrossDeviceOps`. If this is not
       set, the `configure` method will try to find the best one.
-    prefetch_on_device: optional boolean to specify whether to prefetch input
-      data to devices.
     auto_shard_dataset: whether to auto-shard the dataset when there are
       multiple workers.
     cross_tower_ops: Deprecated alias for `cross_device_ops`.
@@ -360,14 +358,12 @@ class MirroredStrategy(distribute_lib.DistributionStrategy):
                num_gpus=None,
                num_gpus_per_worker=None,
                cross_device_ops=None,
-               prefetch_on_device=None,
                auto_shard_dataset=False,
                cross_tower_ops=None):
     super(MirroredStrategy, self).__init__()
 
     assert not (cross_device_ops and cross_tower_ops)
     self._cross_tower_ops = cross_device_ops or cross_tower_ops
-    self._prefetch_on_device = prefetch_on_device
     self._auto_shard_dataset = auto_shard_dataset
     # Remember num GPUs which might be needed by `configure` method.
     if num_gpus is not None and num_gpus_per_worker is not None:
@@ -492,11 +488,10 @@ class MirroredStrategy(distribute_lib.DistributionStrategy):
     if self._cluster_spec:
       return values.MultiWorkerDataset(
           partial(self._call_dataset_fn, dataset_fn), self._worker_devices,
-          self._prefetch_on_device, self._auto_shard_dataset)
+          auto_shard=self._auto_shard_dataset)
     else:
       return values.PerReplicaDataset(
-          self._call_dataset_fn(dataset_fn), self._devices,
-          self._prefetch_on_device)
+          self._call_dataset_fn(dataset_fn), self._devices)
 
   # TODO(priyag): Deal with OutOfRange errors once b/111349762 is fixed.
   def _run_steps_on_dataset(self, fn, iterator, iterations,
