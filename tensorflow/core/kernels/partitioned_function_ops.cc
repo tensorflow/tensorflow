c806b163c4d52cf80daecf2d63e3b76c7dc696e6
Nutti <nutti.metro@gmail.com>
2018-11-10 10:41:09 +0900

    OptimizationPass::POST_REWRITE_FOR_EXEC after Grappler optimization in PartitionedCallOp

--- tensorflow/core/kernels/partitioned_function_ops.cc
+++ tensorflow/core/kernels/partitioned_function_ops.cc
@@ -166,12 +166,6 @@ class PartitionedCallOp : public AsyncOpKernel {
             OptimizationPassRegistry::Global()->RunGrouping(
                 OptimizationPassRegistry::POST_PLACEMENT, optimization_options),
             done);
-        OP_REQUIRES_OK_ASYNC(
-            ctx,
-            OptimizationPassRegistry::Global()->RunGrouping(
-                OptimizationPassRegistry::POST_REWRITE_FOR_EXEC,
-                optimization_options),
-            done);
 
         Device* cpu_device;
         OP_REQUIRES_OK_ASYNC(
@@ -183,6 +177,13 @@ class PartitionedCallOp : public AsyncOpKernel {
                              OptimizeGraph(ctx, fbody->ret_nodes, overlay_lib,
                                            device_set, cpu_device, &graph),
                              done);
+
+        OP_REQUIRES_OK_ASYNC(
+            ctx,
+            OptimizationPassRegistry::Global()->RunGrouping(
+                OptimizationPassRegistry::POST_REWRITE_FOR_EXEC,
+                optimization_options),
+            done);
 
         std::unordered_map<string, std::unique_ptr<Graph>> subgraphs;
         OP_REQUIRES_OK_ASYNC(
