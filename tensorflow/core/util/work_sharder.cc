a5ae34b8af14d564d9f99271225e69f75ef53c40
Alon Daks <adaks@butterflynetinc.com>
2018-11-13 16:38:21 -0500

    fix build

--- tensorflow/core/util/work_sharder.cc
+++ tensorflow/core/util/work_sharder.cc
@@ -20,7 +20,9 @@ limitations under the License.
 
 namespace tensorflow {
 
-/* ABSL_CONST_INIT */ thread_local int per_thread_max_parallism = 1000000;
+
+// Changed according to tensorflow issue #18356
+/* ABSL_CONST_INIT */ __thread int per_thread_max_parallism = 1000000;
 
 void SetPerThreadMaxParallelism(int max_parallelism) {
   CHECK_LE(0, max_parallelism);
