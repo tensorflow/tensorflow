82776ecfceffb0ef664096779de169338b8b7e80
Eugene Brevdo <ebrevdo@google.com>
2018-11-14 09:46:07 -0800

    [TF Grappler] MetaOptimizer now takes a ConfigProto.
    This will allow us to start reasoning about more global config options in
    Grappler.  For example, it will allow us to move XLA functionalization rewrites
    into Grappler, because we'll know when JIT optimization has been turned on.
    
    We'll also be able to selectively disable particular grappler rewrites when
    JIT optimization is on.
    
    Additional changes:
    
    * PartitionedCall now accepts a ConfigProto instead of a RewriterConfig
    * Eager context now passes the entire ConfigProto through.
    
    PiperOrigin-RevId: 221460335

--- tensorflow/python/kernel_tests/signal/test_util.py
+++ tensorflow/python/kernel_tests/signal/test_util.py
@@ -18,12 +18,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.core.protobuf import config_pb2
 from tensorflow.python.grappler import tf_optimizer
 from tensorflow.python.training import saver
 
 
-def grappler_optimize(graph, fetches=None, rewriter_config=None):
+def grappler_optimize(graph, fetches=None, config_proto=None):
   """Tries to optimize the provided graph using grappler.
 
   Args:
@@ -31,17 +31,17 @@ def grappler_optimize(graph, fetches=None, rewriter_config=None):
     fetches: An optional list of `Tensor`s to fetch (i.e. not optimize away).
       Grappler uses the 'train_op' collection to look for fetches, so if not
       provided this collection should be non-empty.
-    rewriter_config: An optional `tf.RewriterConfig` to use when rewriting the
+    config_proto: An optional `tf.ConfigProto` to use when rewriting the
       graph.
 
   Returns:
     A `tf.GraphDef` containing the rewritten graph.
   """
-  if rewriter_config is None:
-    rewriter_config = rewriter_config_pb2.RewriterConfig()
-    rewriter_config.min_graph_nodes = -1
+  if config_proto is None:
+    config_proto = config_pb2.ConfigProto()
+    config_proto.graph_options.rewrite_options.min_graph_nodes = -1
   if fetches is not None:
     for fetch in fetches:
       graph.add_to_collection('train_op', fetch)
   metagraph = saver.export_meta_graph(graph_def=graph.as_graph_def())
-  return tf_optimizer.OptimizeGraph(rewriter_config, metagraph)
+  return tf_optimizer.OptimizeGraph(config_proto, metagraph)
