fbd2c76afc0d884161845e256c7191dc915ec8c3
Jacky Ko <jackkykokoko@gmail.com>
2018-11-14 23:01:27 +0800

    remove change on pip setup

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -45,9 +45,7 @@ DOCLINES = __doc__.split('\n')
 # This version string is semver compatible, but incompatible with pip.
 # For pip, we will remove all '-' characters from this string, and use the
 # result for pip.
-
 _VERSION = '1.12.0-rc0'
-
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.1.6',
@@ -60,8 +58,7 @@ REQUIRED_PACKAGES = [
     'protobuf >= 3.6.1',
     'tensorboard >= 1.12.0, < 1.13.0',
     'tensorflow_estimator >= 1.10.0',
-    'termcolor >= 1.1.0',
-    'absl-py >= 0.1.9'
+    'termcolor >= 1.1.0'
 ]
 
 if sys.byteorder == 'little':
