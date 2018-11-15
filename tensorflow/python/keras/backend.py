b5eee621fe11fae8f1038eea617c3e011ae0a2b1
A. Unique TensorFlower <gardener@tensorflow.org>
2018-11-15 11:54:36 -0800

    export keras.backend.tile
    PiperOrigin-RevId: 221663541

--- tensorflow/python/keras/backend.py
+++ tensorflow/python/keras/backend.py
@@ -2547,7 +2547,7 @@ def arange(start, stop=None, step=1, dtype='int32'):
     result = cast(result, dtype)
   return result
 
-
+@tf_export('keras.backend.tile')
 def tile(x, n):
   """Creates a tensor by tiling `x` by `n`.
 
