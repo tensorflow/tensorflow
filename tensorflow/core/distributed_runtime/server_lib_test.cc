9dd9c9497c1fb8aac578b301be82a70591c2f3b4
James Keeling <jtkeeling@google.com>
2018-11-15 07:44:18 -0800

    Improve coverage of server_lib.cc testing
    We add one more test method to cover the successful case. Testing now covers all lines except the LOG(ERROR) statement.
    
    PiperOrigin-RevId: 221622246

--- tensorflow/core/distributed_runtime/server_lib_test.cc
+++ tensorflow/core/distributed_runtime/server_lib_test.cc
@@ -14,10 +14,31 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/core/distributed_runtime/server_lib.h"
+#include "tensorflow/core/lib/core/status_test_util.h"
 #include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
+
+class TestServerFactory : public ServerFactory {
+ public:
+  bool AcceptsOptions(const ServerDef& server_def) override {
+    return server_def.protocol() == "test_protocol";
+  }
+
+  Status NewServer(const ServerDef& server_def,
+                   std::unique_ptr<ServerInterface>* out_server) override {
+    return Status::OK();
+  }
+};
+
+TEST(ServerLibTest, NewServerFactoryAccepts) {
+  ServerFactory::Register("TEST_SERVER", new TestServerFactory());
+  ServerDef server_def;
+  server_def.set_protocol("test_protocol");
+  std::unique_ptr<ServerInterface> server;
+  TF_EXPECT_OK(NewServer(server_def, &server));
+}
 
 TEST(ServerLibTest, NewServerNoFactoriesAccept) {
   ServerDef server_def;
