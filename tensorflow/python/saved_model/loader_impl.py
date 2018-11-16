28bb75b696bbfea014c2c9c62a09cad987720d8f
A. Unique TensorFlower <gardener@tensorflow.org>
2018-11-16 09:55:23 -0800

    Renamed maybe_saved_model_directory to contains_saved_model for TF 2.0
    PiperOrigin-RevId: 221809031

--- tensorflow/python/saved_model/loader_impl.py
+++ tensorflow/python/saved_model/loader_impl.py
@@ -145,12 +145,11 @@ def _get_main_op_tensor(
   return main_op_tensor
 
 
-@tf_export(
+@tf_export(v1=[
+    "saved_model.contains_saved_model",
     "saved_model.maybe_saved_model_directory",
-    v1=[
-        "saved_model.maybe_saved_model_directory",
-        "saved_model.loader.maybe_saved_model_directory"
-    ])
+    "saved_model.loader.maybe_saved_model_directory"
+])
 @deprecation.deprecated_endpoints(
     "saved_model.loader.maybe_saved_model_directory")
 def maybe_saved_model_directory(export_dir):
@@ -171,6 +170,25 @@ def maybe_saved_model_directory(export_dir):
   txt_path = os.path.join(export_dir, constants.SAVED_MODEL_FILENAME_PBTXT)
   pb_path = os.path.join(export_dir, constants.SAVED_MODEL_FILENAME_PB)
   return file_io.file_exists(txt_path) or file_io.file_exists(pb_path)
+
+
+@tf_export("saved_model.contains_saved_model", v1=[])
+def contains_saved_model(export_dir):
+  """Checks whether the provided export directory could contain a SavedModel.
+
+  Note that the method does not load any data by itself. If the method returns
+  `false`, the export directory definitely does not contain a SavedModel. If the
+  method returns `true`, the export directory may contain a SavedModel but
+  provides no guarantee that it can be loaded.
+
+  Args:
+    export_dir: Absolute string path to possible export location. For example,
+                '/my/foo/model'.
+
+  Returns:
+    True if the export directory contains SavedModel files, False otherwise.
+  """
+  return maybe_saved_model_directory(export_dir)
 
 
 @tf_export(v1=["saved_model.load", "saved_model.loader.load"])
