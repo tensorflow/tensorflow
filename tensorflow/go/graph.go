07f6ed9896bcf6f193f794a6c22398c3a6bcada6
Cibifang <cifang_bifang@hotmail.com>
2018-11-20 18:48:51 +0800

    Fix comment for AddGradients in graph.go

--- tensorflow/go/graph.go
+++ tensorflow/go/graph.go
@@ -147,7 +147,7 @@ func (g *Graph) Operations() []Operation {
 	return ops
 }
 
-// AddGradients add operations to compute the partial derivatives of the sum of tensors in y
+// AddGradients adds operations to compute the partial derivatives of the sum of tensors in y
 // with respect to tensors in x, i.e., d(y[0] + y[1] + ...) / d x[0], d(y[0] + y[1] + ... ) / d x[1] etc.
 //
 // prefix, if non-empty, is the name prefix used for all operations added to the graph to compute
