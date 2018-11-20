68e5275401e2d13ae5bf5093bb9cb62dc9768b6d
Derek Murray <mrry@google.com>
2018-11-20 14:13:20 -0800

    Automated rollback of commit a3650434746cba6b78d9846fbde3b7f31f6a33c8
    PiperOrigin-RevId: 222304840

--- tensorflow/python/data/ops/optional_ops.py
+++ tensorflow/python/data/ops/optional_ops.py
@@ -183,13 +183,19 @@ class OptionalStructure(structure.Structure):
     return OptionalStructure(value.value_structure)
 
   def _to_legacy_output_types(self):
-    return self
+    raise NotImplementedError("The `output_types` property is not supported on "
+                              "structured objects containing an `Optional`. "
+                              "Use the corresponding `structure` property.")
 
   def _to_legacy_output_shapes(self):
-    return self
+    raise NotImplementedError("The `output_shapes` property is not supported on"
+                              " structured objects containing an `Optional`. "
+                              "Use the corresponding `structure` property.")
 
   def _to_legacy_output_classes(self):
-    return self
+    raise NotImplementedError("The `output_classes` property is not supported "
+                              "on structured objects containing an `Optional`. "
+                              "Use the corresponding `structure` property.")
 
 
 # pylint: disable=protected-access
