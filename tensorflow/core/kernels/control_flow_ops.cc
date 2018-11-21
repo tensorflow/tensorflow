6fc1a1872f69569dc6ff58e14c485c65dbc6bccc
Fei Hu <hufei68@gmail.com>
2018-11-20 22:25:51 -0500

    Raise an error when loop execution is cancelled

--- tensorflow/core/kernels/control_flow_ops.cc
+++ tensorflow/core/kernels/control_flow_ops.cc
@@ -602,12 +602,8 @@ LoopCondOp::~LoopCondOp() = default;
 void LoopCondOp::Compute(OpKernelContext* context) {
   CancellationManager* cm = context->cancellation_manager();
   bool already_cancelled = cm->IsCancelled();
-
-  if (already_cancelled) {
-    Tensor continue_running(false);
-    context->set_output(0, continue_running);
-    return;
-  }
+  OP_REQUIRES(context, !already_cancelled,
+              errors::Cancelled("Loop execution was cancelled."));
 
   context->set_output(0, context->input(0));
 }
