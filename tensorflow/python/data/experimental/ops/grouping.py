e191edc894569560ea886e5525d9bb289fb8e4aa
I-Hong Jhuo <ihjhuo@ibm.com>
2018-11-27 16:30:05 -0800

    typos

--- tensorflow/python/data/experimental/ops/grouping.py
+++ tensorflow/python/data/experimental/ops/grouping.py
@@ -36,7 +36,7 @@ from tensorflow.python.util.tf_export import tf_export
 def group_by_reducer(key_func, reducer):
   """A transformation that groups elements and performs a reduction.
 
-  This transformation maps each element of a dataset to a key using `key_func` and
+  This transformation maps element of a dataset to a key using `key_func` and
   groups the elements by key. The `reducer` is used to process each group; its
   `init_func` is used to initialize state for each group when it is created, the
   `reduce_func` is used to update the state every time an element is mapped to
