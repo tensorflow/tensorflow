29fe1b96c32b88951cb6ee9a9c3df62306caf64e
Trevor Morris <tmorris@nvidia.com>
2018-11-28 15:17:45 -0800

    Remove unused imports

--- tensorflow/contrib/tensorrt/test/multi_connection_neighbor_engine_test.py
+++ tensorflow/contrib/tensorrt/test/multi_connection_neighbor_engine_test.py
@@ -25,8 +25,6 @@ from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import gen_math_ops
-from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.platform import test
 
