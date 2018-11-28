8af921c29445020c1419cc6f2463a303e3316e9d
Yong Tang <yong.tang.github@outlook.com>
2018-11-28 02:51:56 +0000

    Pylint fix
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/sparse_concat_op_test.py
+++ tensorflow/python/kernel_tests/sparse_concat_op_test.py
@@ -339,9 +339,11 @@ class SparseConcatTest(test.TestCase):
 
   def testConcatShape(self):
     # Test case for GitHub 21964.
-    x = sparse_tensor.SparseTensor(indices=[[0,0],[1,1]], values=[1,2], dense_shape=[2,2])
-    y = sparse_tensor.SparseTensor(indices=[[0,0],[1,1]], values=[1,2], dense_shape=[2,2])
-    z = sparse_ops.sparse_concat(-1, [x,y])
+    x = sparse_tensor.SparseTensor(
+        indices=[[0, 0], [1, 1]], values=[1, 2], dense_shape=[2, 2])
+    y = sparse_tensor.SparseTensor(
+        indices=[[0, 0], [1, 1]], values=[1, 2], dense_shape=[2, 2])
+    z = sparse_ops.sparse_concat(-1, [x, y])
     self.assertEqual(z.get_shape().as_list(), [2, 4])
 
 
