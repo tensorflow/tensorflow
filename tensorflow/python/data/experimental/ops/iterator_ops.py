8a7c96bcf906af68255d2bba4d7e84058a67752b
I-Hong Jhuo <ihjhuo@ibm.com>
2018-11-28 09:51:57 -0800

    typos

--- tensorflow/python/data/experimental/ops/iterator_ops.py
+++ tensorflow/python/data/experimental/ops/iterator_ops.py
@@ -29,7 +29,7 @@ from tensorflow.python.util.tf_export import tf_export
 
 @tf_export("data.experimental.make_saveable_from_iterator")
 def make_saveable_from_iterator(iterator):
-  """Returns a SaveableObject for saving/restore iterator state using Saver.
+  """Returns a SaveableObject for saving/restoring iterator state using Saver.
 
   Args:
     iterator: Iterator.
@@ -87,11 +87,12 @@ class _Saveable(saver_lib.BaseSaverBuilder.SaveableObject):
 class CheckpointInputPipelineHook(session_run_hook.SessionRunHook):
   """Checkpoints input pipeline state every N steps or seconds.
 
-  This hook saves the state of the iterators in the `Graph` so that when
-  training is resumed the input pipeline continues from where it left off.
+  This hook saves the state of the iterators in the `Graph` so that
+  the input pipeline can continue from where it left off when training
+  is resumed.
   This could potentially avoid overfitting in certain pipelines where the
   number of training steps per eval are small compared to the dataset
-  size or if the training pipeline is pre-empted.
+  size or if the training pipeline is preempted.
 
   Differences from `CheckpointSaverHook`:
   1. Saves only the input pipelines in the "iterators" collection and not the
@@ -113,19 +114,19 @@ class CheckpointInputPipelineHook(session_run_hook.SessionRunHook):
       break
   ```
 
-  This hook should be used if the input pipeline state needs to be saved
-  separate from the model checkpoint. Doing so may be useful for a few reasons:
+  This hook should be used if the input pipeline state needs to be saved and
+  separates from the model checkpoint. Doing so may be useful for a few reasons:
   1. The input pipeline checkpoint may be large, if there are large shuffle
      or prefetch buffers for instance, and may bloat the checkpoint size.
   2. If the input pipeline is shared between training and validation, restoring
      the checkpoint during validation may override the validation input
      pipeline.
 
-  For saving the input pipeline checkpoint alongside the model weights use
+  For saving the input pipeline checkpoint alongside, the model weights use
   `tf.data.experimental.make_saveable_from_iterator` directly to create a
   `SaveableObject` and add to the `SAVEABLE_OBJECTS` collection. Note, however,
   that you will need to be careful not to restore the training iterator during
-  eval. You can do that by not adding the iterator to the SAVEABLE_OBJECTS
+  eval. You can do that without adding the iterator to the SAVEABLE_OBJECTS
   collector when building the eval graph.
   """
 
