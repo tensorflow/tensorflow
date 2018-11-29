f48173d1b14f78bd179e5e7be2da70535d73c618
Yong Tang <yong.tang.github@outlook.com>
2018-11-29 20:27:32 +0000

    Fix broken sparse_ops_test
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/sparse_ops_test.py
+++ tensorflow/python/kernel_tests/sparse_ops_test.py
@@ -968,7 +968,7 @@ class SparsePlaceholderTest(test.TestCase):
 
   def testPartialShapePlaceholder(self):
     foo = array_ops.sparse_placeholder(dtypes.float32, shape=(None, 47))
-    self.assertAllEqual([None, None], foo.get_shape().as_list())
+    self.assertAllEqual([None, 47], foo.get_shape().as_list())
     self.assertAllEqual([None, 2], foo.indices.get_shape().as_list())
 
   def testNoShapePlaceholder(self):
