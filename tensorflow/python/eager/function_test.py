5261e4dc3fed1ba6b209f203335767cf50797b42
Sergio Guadarrama <sguada@google.com>
2018-11-29 16:10:20 -0800

    Add testNestOutputs to tf.function
    PiperOrigin-RevId: 223429927

--- tensorflow/python/eager/function_test.py
+++ tensorflow/python/eager/function_test.py
@@ -147,6 +147,24 @@ class FunctionTest(test.TestCase, parameterized.TestCase):
     out = a_times_b(pair({'a': t}, {'b': t}))
     self.assertAllEqual(out, math_ops.matmul(t, t).numpy())
 
+  def testNestedOutputsGraphMode(self):
+    matmul = def_function.function(math_ops.matmul)
+
+    pair = collections.namedtuple('pair', ['a', 'b'])
+
+    # TODO(b/120222989) remove autograph=False.
+    @def_function.function(autograph=False)
+    def pairs_mul(pair_a, pair_b):
+      return pair(matmul(pair_a.a, pair_b.a), matmul(pair_a.b, pair_b.b))
+
+    a = constant_op.constant([[1.0, 2.0], [1.0, 2.0]])
+    b = constant_op.constant([[3.0, 4.0], [3.0, 4.0]])
+
+    out = pairs_mul(pair(a, b), pair(b, a))
+    expected = pair(math_ops.matmul(a, b).numpy(),
+                    math_ops.matmul(b, a).numpy())
+    self.assertAllClose(out, expected)
+
   def testGraphEagerIsolation(self):
 
     @function.defun
