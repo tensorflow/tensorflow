a680cc42a84f784a3687f588e86be6bfaf3b53db
A. Unique TensorFlower <gardener@tensorflow.org>
2018-11-30 14:30:37 -0800

    Automated rollback of commit b17d53c0cd07bbae46a55dc9c12d3f48a1c19604
    PiperOrigin-RevId: 223576240

--- tensorflow/python/ops/parallel_for/control_flow_ops_test.py
+++ tensorflow/python/ops/parallel_for/control_flow_ops_test.py
@@ -1133,7 +1133,7 @@ class TensorArrayTest(PForTest):
     # y = x * x. Hence dy/dx = 2 * x.
     actual_grad = 2.0 * x
     with session.Session() as sess:
-      actual_grad, computed_grad = self.evaluate([t1, actual_grad])
+      actual_grad, computed_grad = sess.run([t1, actual_grad])
       self.assertAllClose(actual_grad, computed_grad)
 
 
@@ -1287,7 +1287,7 @@ class ControlFlowTest(PForTest):
     expected_output = array_ops.transpose(expected_output, [1, 0])
 
     with session.Session() as sess:
-      out, expected = self.evaluate([out, expected_output])
+      out, expected = sess.run([out, expected_output])
       self.assertAllClose(expected, out)
 
   def test_tensor_array_as_loop_variable(self):
@@ -1475,7 +1475,7 @@ class Benchmarks(test.Benchmark):
     sess = session.Session()
     with sess:
       init = variables.global_variables_initializer()
-      self.evaluate(init)
+      sess.run(init)
       run_fn = sess.make_callable(targets)
       run_fn()  # Warm up
       begin = time.time()
