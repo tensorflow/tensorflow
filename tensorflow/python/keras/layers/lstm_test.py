64d96c3df527cc44c92653afa2a75692ead7016d
Scott Zhu <scottzhu@google.com>
2018-12-03 12:43:17 -0800

    Replace deprecated method assertEquals() to assertEqual() in all keras py tests.
    PiperOrigin-RevId: 223844581

--- tensorflow/python/keras/layers/lstm_test.py
+++ tensorflow/python/keras/layers/lstm_test.py
@@ -57,7 +57,7 @@ class LSTMLayerTest(test.TestCase, parameterized.TestCase):
     layer = keras.layers.LSTM(units, return_sequences=True)
     model.add(layer)
     outputs = model.layers[-1].output
-    self.assertEquals(outputs.get_shape().as_list(), [None, timesteps, units])
+    self.assertEqual(outputs.get_shape().as_list(), [None, timesteps, units])
 
   def test_dynamic_behavior_LSTM(self):
     num_samples = 2
