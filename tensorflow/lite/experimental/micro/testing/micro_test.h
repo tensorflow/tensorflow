50d5166bc12924315ec69bf8c09a923ff35d0ee9
William D. Irons <wdirons@us.ibm.com>
2018-12-04 10:28:56 -0600

    Fix seg fault in micro_speech_test on ppc64le.
    Building micro_speech_test with the same compiler options as on x86, leads to the warnings:
    ... /micro/testing/micro_test.h:95:51: warning: ISO C++ forbids converting a string constant to 'va_list {aka char*}'
    ... /micro/micro_error_reporter_test.cc:24:56: warning: ISO C++ forbids converting a string constant to 'va_list {aka char*}'
    
    And running the test program leads to a seg fault, for what I understand the standard for c++11
    doesn't allow for a constant string to be used with %s substitution.
    
    Another fix was to add the "-lm" link option when compiling on ppc64le, this avoided compile errors like:
    undefined reference to symbol 'cos@@GLIBC_2.17'
    
    This fixes several failing unit testcases on ppc64le, and causes no regression
    errors on x86.
    
    Fixes #24148

--- tensorflow/lite/experimental/micro/testing/micro_test.h
+++ tensorflow/lite/experimental/micro/testing/micro_test.h
@@ -92,7 +92,7 @@ extern tflite::ErrorReporter* reporter;
 
 // TODO(petewarden): I'm going to hell for what I'm doing to this poor for loop.
 #define TF_LITE_MICRO_TEST(name)                                           \
-  micro_test::reporter->Report("Testing %s", #name);                       \
+  micro_test::reporter->Report("Testing " #name);                          \
   for (micro_test::is_test_complete = false,                               \
       micro_test::did_test_fail = false;                                   \
        !micro_test::is_test_complete; micro_test::is_test_complete = true, \
