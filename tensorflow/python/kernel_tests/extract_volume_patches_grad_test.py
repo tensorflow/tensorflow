cecdc921b27be9e46a832daa6e52b0e1adfadbfa
Niels Ole Salscheider <niels_ole@salscheider-online.de>
2018-12-05 16:00:27 +0100

    Add gradient for tf.extract_volume_patches
    This implementation is based on tf.extract_image_patches.

---
+++ tensorflow/python/kernel_tests/extract_volume_patches_grad_test.py
@@ -0,0 +1,109 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for ExtractVolumePatches gradient."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import random_seed as random_seed_lib
+from tensorflow.python.framework import test_util
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import gradient_checker
+from tensorflow.python.ops import gradients_impl
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.platform import test
+
+
+class ExtractVolumePatchesGradTest(test.TestCase):
+  """Gradient-checking for ExtractVolumePatches op."""
+
+  _TEST_CASES = [
+      {
+          'in_shape': [2, 5, 5, 5, 3],
+          'ksizes': [1, 1, 1, 1, 1],
+          'strides': [1, 2, 3, 4, 1],
+      },
+      {
+          'in_shape': [2, 7, 7, 7, 3],
+          'ksizes': [1, 3, 3, 3, 1],
+          'strides': [1, 1, 1, 1, 1],
+      },
+      {
+          'in_shape': [2, 5, 7, 6, 3],
+          'ksizes': [1, 3, 2, 2, 1],
+          'strides': [1, 1, 1, 1, 1],
+      },
+      {
+          'in_shape': [2, 7, 8, 6, 3],
+          'ksizes': [1, 2, 3, 2, 1],
+          'strides': [1, 2, 4, 3, 1],
+      },
+  ]
+
+  @test_util.run_deprecated_v1
+  def testGradient(self):
+    # Set graph seed for determinism.
+    random_seed = 42
+    random_seed_lib.set_random_seed(random_seed)
+
+    with self.cached_session():
+      for test_case in self._TEST_CASES:
+        np.random.seed(random_seed)
+        in_shape = test_case['in_shape']
+        in_val = constant_op.constant(
+            np.random.random(in_shape), dtype=dtypes.float32)
+
+        for padding in ['VALID', 'SAME']:
+          out_val = array_ops.extract_volume_patches(in_val,
+                                                    test_case['ksizes'],
+                                                    test_case['strides'],
+                                                    padding)
+          out_shape = out_val.get_shape().as_list()
+
+          err = gradient_checker.compute_gradient_error(in_val, in_shape,
+                                                        out_val, out_shape)
+
+          print('extract_volume_patches gradient err: %.4e' % err)
+          self.assertLess(err, 1e-4)
+
+  @test_util.run_deprecated_v1
+  def testConstructGradientWithLargeVolumess(self):
+    batch_size = 4
+    planes = 8
+    height = 32
+    width = 32
+    ksize = 5
+    volumes = variable_scope.get_variable('inputs',
+                                          (batch_size, planes,
+                                           height, width, 1))
+    patches = array_ops.extract_volume_patches(volumes,
+                                               ksizes=[1, ksize,
+                                               ksize, ksize, 1],
+                                               strides=[1, 1, 1, 1, 1],
+                                               padding='SAME')
+    # Github issue: #20146
+    # tf.extract_volume_patches() gradient very slow at graph construction time
+    gradients = gradients_impl.gradients(patches, volumes)
+    # Won't time out.
+    self.assertIsNotNone(gradients)
+
+
+if __name__ == '__main__':
+  test.main()
--- tensorflow/python/ops/array_grad.py
+++ tensorflow/python/ops/array_grad.py
@@ -795,6 +795,73 @@ def _ExtractImagePatchesGrad(op, grad):
   return [grad_out]
 
 
+@ops.RegisterGradient("ExtractVolumePatches")
+def _ExtractVolumePatchesGrad(op, grad):
+  batch_size, planes_in, rows_in, cols_in, channels = [
+      dim.value for dim in op.inputs[0].shape.dims
+  ]
+  input_bphwc = array_ops.shape(op.inputs[0])
+  batch_size = input_bphwc[0]
+  channels = input_bphwc[4]
+
+  # Create indices matrix for input tensor.
+  # Note that 0 is preserved for padding location,
+  # so indices for input start from 1 to 1 + rows_in * cols_in.
+  input_indices_num = 1 + planes_in * rows_in * cols_in
+  input_idx = array_ops.reshape(math_ops.range(1, input_indices_num,
+                                               dtype=ops.dtypes.int64),
+                                (1, planes_in, rows_in, cols_in, 1))
+  input_idx_patched = gen_array_ops.extract_volume_patches(
+      input_idx,
+      op.get_attr("ksizes"),
+      op.get_attr("strides"),
+      op.get_attr("padding"))
+
+  # Create indices matrix for output tensor.
+  _, planes_out, rows_out, cols_out, _ = [dim.value for dim in
+                                          op.outputs[0].shape.dims]
+  _, ksize_p, ksize_r, ksize_c, _ = op.get_attr("ksizes")
+  # Indices for output start from 0.
+  prc_indices_num = planes_out * rows_out * cols_out
+  output_indices_num = prc_indices_num * ksize_p * ksize_r * ksize_c
+  output_idx = array_ops.reshape(math_ops.range(output_indices_num,
+                                                dtype=ops.dtypes.int64),
+                                 (1, planes_out, rows_out, cols_out,
+                                  ksize_p * ksize_r * ksize_c))
+
+  # Construct mapping table for indices: (input -> output).
+  idx_matrix = array_ops.concat(
+      [array_ops.expand_dims(input_idx_patched, axis=-1),
+       array_ops.expand_dims(output_idx, axis=-1)],
+      axis=-1)
+  idx_map = array_ops.reshape(idx_matrix, (-1, 2))
+
+  sp_shape = (input_indices_num, output_indices_num)
+  sp_mat_full = sparse_tensor.SparseTensor(
+      idx_map,
+      array_ops.ones([output_indices_num], dtype=grad.dtype),
+      sp_shape)
+  # Remove all padding locations [0, :].
+  sp_mat = sparse_ops.sparse_slice(sp_mat_full,
+                                   (1, 0),
+                                   (input_indices_num - 1, output_indices_num))
+
+  grad_expanded = array_ops.transpose(
+      array_ops.reshape(
+          grad, (batch_size, planes_out, rows_out, cols_out, ksize_p, ksize_r,
+                 ksize_c, channels)),
+      (1, 2, 3, 4, 5, 6, 0, 7))
+  grad_flat = array_ops.reshape(grad_expanded, (-1, batch_size * channels))
+
+  jac = sparse_ops.sparse_tensor_dense_matmul(sp_mat, grad_flat)
+
+  grad_out = array_ops.reshape(jac, (planes_in, rows_in, cols_in, batch_size,
+                                     channels))
+  grad_out = array_ops.transpose(grad_out, (3, 0, 1, 2, 4))
+
+  return [grad_out]
+
+
 @ops.RegisterGradient("ScatterNd")
 def _ScatterNdGrad(op, grad):
   indices = op.inputs[0]
