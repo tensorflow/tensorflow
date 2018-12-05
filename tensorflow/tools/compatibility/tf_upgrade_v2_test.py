15233264eb6d8412c6f853652513521757055f8d
Alexandre Passos <apassos@google.com>
2018-12-05 14:12:13 -0800

    Tests that all symbols the converter outputs actually exist in tf v1.
    Also some fixes to make this a reality. This will help ensure code can be
    v1-and-v2-agnostic.
    
    PiperOrigin-RevId: 224219069

--- tensorflow/tools/compatibility/tf_upgrade_v2_test.py
+++ tensorflow/tools/compatibility/tf_upgrade_v2_test.py
@@ -162,6 +162,35 @@ class TestUpgrade(test_util.TensorFlowTestCase):
     visitor.private_map["tf.compat"] = ["v1", "v2"]
     traverse.traverse(tf.compat.v1, visitor)
 
+  def testAllAPIV1(self):
+    collect = True
+    v1_symbols = set([])
+
+    # Converts all symbols in the v1 namespace to the v2 namespace, raising
+    # an error if the target of the conversion is not in the v1 namespace.
+    def conversion_visitor(unused_path, unused_parent, children):
+      for child in children:
+        _, attr = tf_decorator.unwrap(child[1])
+        api_names = get_v1_names(attr)
+        for name in api_names:
+          if collect:
+            v1_symbols.add("tf." + name)
+          else:
+            _, _, _, text = self._upgrade("tf." + name)
+            if (text and
+                not text.startswith("tf.compat.v1") and
+                text not in v1_symbols):
+              self.assertFalse(
+                  True, "Symbol %s generated from %s not in v1 API" % (
+                      text, name))
+
+    visitor = public_api.PublicAPIVisitor(conversion_visitor)
+    visitor.do_not_descend_map["tf"].append("contrib")
+    visitor.private_map["tf.compat"] = ["v1", "v2"]
+    traverse.traverse(tf.compat.v1, visitor)
+    collect = False
+    traverse.traverse(tf.compat.v1, visitor)
+
   def testKeywordArgNames(self):
     if not hasattr(tf.compat, "v2"):
       return
