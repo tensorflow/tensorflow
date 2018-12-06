6b4f9d9de1bfc7b40d423b3dd730c4f2c852c63d
A. Unique TensorFlower <gardener@tensorflow.org>
2018-12-06 10:52:09 -0800

    Disabling flaky tests in LossWeightingTest.
    PiperOrigin-RevId: 224367330

--- tensorflow/python/keras/engine/training_test.py
+++ tensorflow/python/keras/engine/training_test.py
@@ -793,7 +793,8 @@ class TestExceptionsAndWarnings(keras_parameterized.TestCase):
 class LossWeightingTest(keras_parameterized.TestCase):
 
   @keras_parameterized.run_all_keras_modes
-  def test_class_weights(self):
+  # TODO(b/120562577): Test failing with assertion error.
+  def DISABLED_test_class_weights(self):
     num_classes = 5
     batch_size = 5
     epochs = 5
@@ -960,7 +961,8 @@ class LossWeightingTest(keras_parameterized.TestCase):
       self.assertTrue(msg_found)
 
   @keras_parameterized.run_all_keras_modes
-  def test_temporal_sample_weights(self):
+  # TODO(b/120562577): Test failing with assertion error.
+  def DISABLED_test_temporal_sample_weights(self):
     num_classes = 5
     batch_size = 5
     epochs = 5
