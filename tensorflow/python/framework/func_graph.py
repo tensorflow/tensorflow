9fffd26041e40bb3c89eabebfd35f92fe37b2c2a
Igor Ganichev <iga@google.com>
2018-12-07 12:39:30 -0800

    Support XLA_CPU/GPU devices in eager runtime
    Compiling a defun can be done with:
    
      @function.defun
      def f(x):
        return x + 1
    
      with tf.device("XLA_GPU"):
        f(5)
    
    PiperOrigin-RevId: 224560868

--- tensorflow/python/framework/func_graph.py
+++ tensorflow/python/framework/func_graph.py
@@ -122,7 +122,9 @@ class FuncGraph(ops.Graph):
     # restored.
     if context.executing_eagerly():
       self.seed = context.global_seed()
-      self._xla_compile = (context.context().device_spec.device_type == "TPU")
+      device_type = context.context().device_spec.device_type
+      self._xla_compile = (device_type == "TPU" or device_type == "XLA_GPU"
+                           or device_type == "XLA_CPU")
       if self._distribution_strategy_stack or self._xla_compile:
         self._add_device_to_stack(context.context().device_name)
     else:
