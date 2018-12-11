46eac5c06bcbe788ce8f9ac09241b62ac278c045
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-12-11 16:11:01 +0000

    Misc terminology change: reinstate tf_cuda_ related symbols
    For existing build logic in upstream TensorFlow, reinstante tf_cuda_ symbols to
    ease maintainence efforts in our downstream fork.
    
    For ROCm-specific tests, use tf_gpu_ symbols.

--- tensorflow/contrib/makefile/build_all_linux.sh
+++ tensorflow/contrib/makefile/build_all_linux.sh
@@ -43,6 +43,6 @@ tensorflow/contrib/makefile/compile_linux_protobuf.sh
 
 # Build TensorFlow.
 make -j"${JOB_COUNT}" -f tensorflow/contrib/makefile/Makefile \
-  OPTFLAGS="-O3 -march=haswell" \
+  OPTFLAGS="-O3 -march=native" \
   HOST_CXXFLAGS="--std=c++11 -march=native" \
   MAKEFILE_DIR=$SCRIPT_DIR
