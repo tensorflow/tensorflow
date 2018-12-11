5478c41e32d7ee455741fdee9473e60fa8e40a21
Justin Lebar <jlebar@google.com>
2018-12-10 19:13:04 -0800

    [XLA] Don't pass XLA_FLAGS down to fake computations created by replay_computation.
    When you pass XLA_FLAGS to replay_computation, you very likely want that only
    to apply to the actual computation(s) being run, not to the XLA computations
    that replay_computation synthesizes to generate fake data for the "real" ones'
    arguments.
    
    PiperOrigin-RevId: 224927003

--- tensorflow/compiler/xla/client/lib/testing.h
+++ tensorflow/compiler/xla/client/lib/testing.h
@@ -29,14 +29,19 @@ namespace xla {
 // Generates fake data of the given shape on the device or dies. The fake data
 // is created by performing a computation on the device rather than transferring
 // data from the host to the device.
-std::unique_ptr<GlobalData> MakeFakeDataOrDie(const Shape& shape,
-                                              Client* client);
+//
+// The optional DebugOptions are used when generating fake data on the device.
+std::unique_ptr<GlobalData> MakeFakeDataOrDie(
+    const Shape& shape, Client* client, DebugOptions* debug_opts = nullptr);
 
 // Returns vector of GlobalData handles of fake data (created using
 // MakeFakeDataOrDie) that are correctly shaped arguments for the given
 // xla computation.
+//
+// The optional DebugOptions are used when generating fake data on the device.
 std::vector<std::unique_ptr<GlobalData>> MakeFakeArgumentsOrDie(
-    const XlaComputation& computation, Client* client);
+    const XlaComputation& computation, Client* client,
+    DebugOptions* debug_opts = nullptr);
 
 }  // namespace xla
 
