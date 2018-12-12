b4c28561416e97f8029ad0c009cbe15e4fb75563
Skye Wanderman-Milne <skyewm@google.com>
2018-12-11 23:14:27 -0800

    Make AddWhileInputHack handle control inputs correctly.
    PiperOrigin-RevId: 225131361

--- tensorflow/core/graph/graph.cc
+++ tensorflow/core/graph/graph.cc
@@ -555,7 +555,13 @@ Status Graph::AddWhileInputHack(Node* new_src, int new_src_index, Node* dst) {
         dst->DebugString());
   }
   TF_RETURN_IF_ERROR(IsValidOutputTensor(new_src, new_src_index));
-  int dst_index = dst->in_edges().size();
+  // Find the current number of data inputs. We'll add the new edge to the next
+  // missing data input.
+  int dst_index = 0;
+  for (const Edge* edge : dst->in_edges()) {
+    if (edge->IsControlEdge()) continue;
+    ++dst_index;
+  }
   TF_RETURN_IF_ERROR(IsValidInputTensor(dst, dst_index));
   AddEdge(new_src, new_src_index, dst, dst_index);
   dst->MaybeCopyOnWrite();
