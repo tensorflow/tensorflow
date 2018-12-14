06f311a596b515de8f81a9cf73f0914b5421def1
Gunhan Gulsoy <gunan@google.com>
2018-12-14 05:04:43 -0800

    Update the default cuda version windows GPU builds default to.
    PiperOrigin-RevId: 225524673

--- tensorflow/tools/ci_build/windows/bazel/common_env.sh
+++ tensorflow/tools/ci_build/windows/bazel/common_env.sh
@@ -55,9 +55,9 @@ export PATH="/c/Program Files/Git/cmd:$PATH"
 export PATH="/c/${PYTHON_BASE_PATH}/Scripts:$PATH"
 
 # Setting default values to CUDA related environment variables
-export TF_CUDA_VERSION=${TF_CUDA_VERSION:-9.0}
+export TF_CUDA_VERSION=${TF_CUDA_VERSION:-10.0}
 export TF_CUDNN_VERSION=${TF_CUDNN_VERSION:-7}
-export TF_CUDA_COMPUTE_CAPABILITIES=${TF_CUDA_COMPUTE_CAPABILITIES:-3.7}
+export TF_CUDA_COMPUTE_CAPABILITIES=${TF_CUDA_COMPUTE_CAPABILITIES:-6.0}
 export CUDA_TOOLKIT_PATH=${CUDA_TOOLKIT_PATH:-"C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v${TF_CUDA_VERSION}"}
 export CUDNN_INSTALL_PATH=${CUDNN_INSTALL_PATH:-"C:/tools/cuda"}
 
