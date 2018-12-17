87dd3aee6d6eb25e2d6c3d2bc76afbdd891cde76
Akshay Modi <nareshmodi@google.com>
2018-12-17 13:24:50 -0800

    Minor speedup of the sequential model run eagerly
    PiperOrigin-RevId: 225878747

--- tensorflow/python/keras/engine/sequential.py
+++ tensorflow/python/keras/engine/sequential.py
@@ -103,6 +103,8 @@ class Sequential(Model):
     self._build_input_shape = None
     self._compute_output_and_mask_jointly = True
 
+    self._layer_call_argspecs = {}
+
     # Add to the model any layers passed to the constructor.
     if layers:
       for layer in layers:
@@ -192,6 +194,8 @@ class Sequential(Model):
     if self._layers:
       self._track_layers(self._layers)
 
+    self._layer_call_argspecs[layer] = tf_inspect.getfullargspec(layer.call)
+
   @checkpointable.no_automatic_dependency_tracking
   def pop(self):
     """Removes the last layer in the model.
@@ -202,7 +206,8 @@ class Sequential(Model):
     if not self.layers:
       raise TypeError('There are no layers in the model.')
 
-    self._layers.pop()
+    layer = self._layers.pop()
+    self._layer_call_argspecs.pop(layer)
     if not self.layers:
       self.outputs = None
       self.inputs = None
@@ -240,9 +245,10 @@ class Sequential(Model):
     x = inputs
     for layer in self.layers:
       kwargs = {}
-      if 'mask' in tf_inspect.getfullargspec(layer.call).args:
+      argspec = self._layer_call_argspecs[layer].args
+      if 'mask' in argspec:
         kwargs['mask'] = mask
-      if 'training' in tf_inspect.getfullargspec(layer.call).args:
+      if 'training' in argspec:
         kwargs['training'] = training
 
       if isinstance(layer, Network) and layer._compute_output_and_mask_jointly:
