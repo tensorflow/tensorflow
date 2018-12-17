fed6109d74e51720872ec5abd0de267118cd80eb
Think tank of Sichuan University <wangzichaochaochao@gmail.com>
2018-12-17 15:07:25 +0800

    make_one_shot_iterator API have changed
     tf.compat.v1.data.make_one_shot_iterator(dataset)  to  tf.data.Dataset.make_one_shot_iterator(dataset)

--- tensorflow/examples/how_tos/reading_data/fully_connected_reader.py
+++ tensorflow/examples/how_tos/reading_data/fully_connected_reader.py
@@ -126,7 +126,7 @@ def inputs(train, batch_size, num_epochs):
     dataset = dataset.repeat(num_epochs)
     dataset = dataset.batch(batch_size)
 
-    iterator = tf.compat.v1.data.make_one_shot_iterator(dataset)
+    iterator = tf.data.Dataset.make_one_shot_iterator(dataset)
   return iterator.get_next()
 
 
