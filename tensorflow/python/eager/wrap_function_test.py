be93ec7939d5db5b91da1137c3e7cf7b7507e3f6
Allen Lavoie <allenl@google.com>
2018-12-18 10:43:57 -0800

    Allow fetching operations using tf.compat.v1.wrap_function.prune()
    Some related tweaking to avoid creating two sets of input placeholders and to re-structure outputs. Like session.run, fetched Operations turn into None.
    
    PiperOrigin-RevId: 226021852

--- tensorflow/python/eager/wrap_function_test.py
+++ tensorflow/python/eager/wrap_function_test.py
@@ -80,7 +80,7 @@ class WrapFunctionTest(test.TestCase):
     f_wrapped = wrap_function.wrap_function(f, [])
     self.assertAllEqual(1.0, f_wrapped())
 
-  def testCaptures(self):
+  def testPruneCaptures(self):
 
     v1 = variables.Variable(2.)
 
@@ -94,6 +94,45 @@ class WrapFunctionTest(test.TestCase):
         feeds=(),
         fetches=f_wrapped.graph.get_tensor_by_name('fetch:0'))
     self.assertAllEqual(6.0, pruned())
+
+  def testPruneOperations(self):
+
+    v = variables.Variable(0)
+
+    def f():
+      v.assign_add(1, name='increment', read_value=False)
+
+    f_wrapped = wrap_function.wrap_function(f, [])
+    pruned = f_wrapped.prune(
+        feeds=(),
+        fetches=(f_wrapped.graph.get_operation_by_name('increment'),))
+    self.assertEqual((None,), pruned())
+    self.assertEqual(1, self.evaluate(v))
+
+    del f, f_wrapped
+
+    def f1():
+      v.assign_add(
+          array_ops.placeholder(shape=[], dtype=dtypes.int32, name='step'),
+          name='increment', read_value=False)
+      return constant_op.constant(1, name='other')
+
+    f_wrapped = wrap_function.wrap_function(f1, [])
+    increments = f_wrapped.prune(
+        feeds=(f_wrapped.graph.get_tensor_by_name('step:0')),
+        fetches=(f_wrapped.graph.get_operation_by_name('increment'),
+                 f_wrapped.graph.get_tensor_by_name('other:0')))
+    first_output, second_output = increments(constant_op.constant(2))
+    self.assertEqual(['Placeholder:0', 'Placeholder_1:0'],
+                     [t.name for t in increments.inputs])
+    self.assertIs(None, first_output)
+    self.assertEqual(1, second_output.numpy())
+    self.assertEqual(3, v.numpy())
+    does_not_increment = f_wrapped.prune(
+        feeds=(f_wrapped.graph.get_tensor_by_name('step:0')),
+        fetches=f_wrapped.graph.get_tensor_by_name('other:0'))
+    self.assertEqual(1, does_not_increment(constant_op.constant(3)).numpy())
+    self.assertEqual(3, v.numpy())
 
 
 if __name__ == '__main__':
