6fd5dbeaece011c01961a10b4d75431cc6912b37
Rachel Lim <rachelim@google.com>
2018-12-18 11:30:52 -0800

    [tf.data] Limit the number of elements in the shuffle buffer to restrict memory consumption
    PiperOrigin-RevId: 226030086

--- tensorflow/python/data/experimental/kernel_tests/shuffle_and_repeat_test.py
+++ tensorflow/python/data/experimental/kernel_tests/shuffle_and_repeat_test.py
@@ -108,6 +108,20 @@ class ShuffleAndRepeatTest(test_base.DatasetTestBase):
     get_next = self.getNext(ds)
     self.evaluate(get_next())
 
+  def testVeryLargeBufferSize(self):
+    num_epochs = 1000 * 1000
+    # Each element being shuffled and repeated has shape (100,). This will OOM
+    # or timeout if we actually load everything into the buffer.
+    ds = dataset_ops.Dataset.range(500).batch(100).apply(
+        shuffle_ops.shuffle_and_repeat(
+            buffer_size=5 * num_epochs, count=num_epochs))
+    # Verify two epochs worth of output.
+    output = self._gen_outputs(lambda: ds, 2 * 5, verify_exhausted=False)
+    for i in range(2):
+      sorted_epoch = sorted(
+          output[i * 5:(i + 1) * 5], key=lambda batch: batch[0])
+      self.assertAllEqual(sorted_epoch, np.arange(500).reshape([5, 100]))
+
 
 if __name__ == "__main__":
   test.main()
