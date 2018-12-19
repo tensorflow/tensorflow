82a6986f5c9771757f190913fd590e7e3fab2eac
Andrew Selle <aselle@google.com>
2018-12-19 13:11:07 -0800

    Handle Python 2 and Python 3 compatibility by forcing to bytes.
    This had previously been done but not uniformly. Change to remove
    #!/usr/bin/env python in October caused this problems in latest release.
    
    PiperOrigin-RevId: 226221579

--- tensorflow/tools/git/gen_git_source.py
+++ tensorflow/tools/git/gen_git_source.py
@@ -159,12 +159,14 @@ def get_git_version(git_base_path, git_tag_override):
   """
   unknown_label = b"unknown"
   try:
+    # Force to bytes so this works on python 2 and python 3
     val = bytes(subprocess.check_output([
         "git", str("--git-dir=%s/.git" % git_base_path),
         str("--work-tree=" + git_base_path), "describe", "--long", "--tags"
     ]).strip())
+    version_separator = b"-"
     if git_tag_override and val:
-      split_val = val.split("-")
+      split_val = val.split(version_separator)
       if len(split_val) < 3:
         raise Exception(
             ("Expected git version in format 'TAG-COMMITS AFTER TAG-HASH' "
@@ -173,7 +175,7 @@ def get_git_version(git_base_path, git_tag_override):
       # two "-" are those inserted by the git describe command.
       abbrev_commit = split_val[-1]
       val = bytes(
-          "-".join([git_tag_override, "0", abbrev_commit]))
+          version_separator.join([git_tag_override, "0", abbrev_commit]))
     return val if val else unknown_label
   except (subprocess.CalledProcessError, OSError):
     return unknown_label
