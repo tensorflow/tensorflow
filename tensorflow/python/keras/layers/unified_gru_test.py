9946c1fe24dc7854fed0750b73cf971f5188b9c8
Scott Zhu <scottzhu@google.com>
2018-12-20 11:53:54 -0800

    Fix flakiness in the test case.
    PiperOrigin-RevId: 226370519

--- tensorflow/python/keras/layers/unified_gru_test.py
+++ tensorflow/python/keras/layers/unified_gru_test.py
@@ -176,8 +176,8 @@ class UnifiedGRUTest(keras_parameterized.TestCase):
       cudnn_model.fit(x_train, y_train)
       y_4 = cudnn_model.predict(x_train)
 
-      self.assertAllClose(y_1, y_3)
-      self.assertAllClose(y_2, y_4)
+      self.assertAllClose(y_1, y_3, rtol=1e-5, atol=1e-5)
+      self.assertAllClose(y_2, y_4, rtol=1e-5, atol=1e-5)
 
   @parameterized.named_parameters(
       # test_name, use_bias, bias_initializer, activation
