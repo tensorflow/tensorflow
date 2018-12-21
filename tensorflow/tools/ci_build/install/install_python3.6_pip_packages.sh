2114498920ee34ad83f72686a151f1ab434ff9ad
Andrew Selle <aselle@google.com>
2018-12-20 17:45:52 -0800

    Make estimator part of pip install
    PiperOrigin-RevId: 226422176

--- tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
@@ -107,4 +107,7 @@ pip3 install --upgrade h5py==2.8.0
 pip3 install keras_applications==1.0.6
 pip3 install keras_preprocessing==1.0.5
 
+# Estimator
+pip3 install tf-estimator-nightly==1.12.0.dev20181203 --no-deps
+
 # LINT.ThenChange(//tensorflow/tools/ci_build/install/install_python3.5_pip_packages.sh)
