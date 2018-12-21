ef1fd4ab6cb076aee9af7801f5b66fb66a768fc4
Mahmoud Abuzaina <mahmoud.abuzaina@intel.com>
2018-12-21 09:37:24 -0800

    Moved #endif of MKL

--- tensorflow/core/ops/nn_ops.cc
+++ tensorflow/core/ops/nn_ops.cc
@@ -2515,6 +2515,7 @@ NOTE Do not invoke this operator directly in Python. Graph rewrite pass is
 expected to invoke these operators.
 )doc");
 
+#endif  // INTEL_MKL
 REGISTER_OP("QuantizedConv2DAndRequantize")
     .Input("input: Tinput")
     .Input("filter: Tfilter")
@@ -2851,6 +2852,5 @@ REGISTER_OP("QuantizedConv2DWithBiasSignedSumAndReluAndRequantize")
       return Status::OK();
     });
 
-#endif  // INTEL_MKL
 
 }  // namespace tensorflow
