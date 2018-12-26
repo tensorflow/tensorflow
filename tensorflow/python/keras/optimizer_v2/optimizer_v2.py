f150f5ddd30806ed83c1e9d1f7af375a6d96489e
Jianwei Xie <xiejw@google.com>
2018-12-26 11:03:02 -0800

    Automated rollback of commit 2696015d8539ea68f27a519ac908ed538f8085ae
    PiperOrigin-RevId: 226934586

--- tensorflow/python/keras/optimizer_v2/optimizer_v2.py
+++ tensorflow/python/keras/optimizer_v2/optimizer_v2.py
@@ -443,12 +443,11 @@ class OptimizerV2(checkpointable.CheckpointableBase):
             initializer, shape=var.shape, dtype=var.dtype)
       else:
         initial_value = initializer
-      with ops._colocate_with_for_gradient(var, None):  # pylint: disable=protected-access
-        weight = tf_variables.Variable(
-            name="%s/%s" % (var._shared_name, slot_name),  # pylint: disable=protected-access
-            dtype=var.dtype,
-            trainable=False,
-            initial_value=initial_value)
+      weight = tf_variables.Variable(
+          name="%s/%s" % (var._shared_name, slot_name),  # pylint: disable=protected-access
+          dtype=var.dtype,
+          trainable=False,
+          initial_value=initial_value)
       backend.track_variable(weight)
       slot_dict[slot_name] = weight
       self._restore_slot_variable(
