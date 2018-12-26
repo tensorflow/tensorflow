a3048ca3452026e99f6d651d67a4830ab8c793cb
Yong Tang <yong.tang.github@outlook.com>
2018-12-26 21:00:10 +0000

    Added missing dtypes import
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/keras/utils/losses_utils.py
+++ tensorflow/python/keras/utils/losses_utils.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.keras import backend as K
 from tensorflow.python.ops import array_ops
