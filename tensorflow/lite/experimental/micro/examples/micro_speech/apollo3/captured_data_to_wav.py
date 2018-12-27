906e741c78557433994684f3ad79b346b29c96fa
Pete Warden <petewarden@google.com>
2018-12-27 12:25:21 -0800

    Fixed Python lint errors

--- tensorflow/lite/experimental/micro/examples/micro_speech/apollo3/captured_data_to_wav.py
+++ tensorflow/lite/experimental/micro/examples/micro_speech/apollo3/captured_data_to_wav.py
@@ -1,11 +1,11 @@
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,29 +13,31 @@
 # limitations under the License.
 # ==============================================================================
 
-import numpy as np
-import re
+"""Converts values pulled from the microcontroller into audio files."""
+
 import struct
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import numpy as np
 import soundfile as sf
 
+
 def new_data_to_array(fn):
-    vals = []
-    with open(fn) as f:
-        for n, line in enumerate(f):
-            if n is not 0:
-                vals.extend([int(v, 16) for v in line.split()])
-    b = ''.join(map(chr, vals))
-    y = struct.unpack('<'+'h'*int(len(b)/2), b)
-            
-    return y
+  vals = []
+  with open(fn) as f:
+    for n, line in enumerate(f):
+      if n is not 0:
+        vals.extend([int(v, 16) for v in line.split()])
+  b = ''.join(map(chr, vals))
+  y = struct.unpack('<' + 'h' * int(len(b) / 2), b)
 
-        
-data = 'captured_data.txt'            
-vals = np.array(new_data_to_array(data)).astype(float)
+  return y
 
-#plt.plot(vals, 'o-')
-#plt.show(block=False)
 
-wav = vals/np.max(np.abs(vals))
+data = 'captured_data.txt'
+values = np.array(new_data_to_array(data)).astype(float)
+
+# plt.plot(values, 'o-')
+# plt.show(block=False)
+
+wav = values / np.max(np.abs(values))
 sf.write('captured_data.wav', wav, 16000)
