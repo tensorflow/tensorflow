b9abbc5424e6e883a1ec481344e3b080a29c49b8
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-01 09:05:30 -0800

    More `TFGAN` to `TF-GAN`.
    PiperOrigin-RevId: 227437819

--- tensorflow/contrib/gan/python/eval/python/classifier_metrics_test.py
+++ tensorflow/contrib/gan/python/eval/python/classifier_metrics_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for TFGAN classifier_metrics."""
+"""Tests for TF-GAN classifier_metrics."""
 
 from __future__ import absolute_import
 from __future__ import division
