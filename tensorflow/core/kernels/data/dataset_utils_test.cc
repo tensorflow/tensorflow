0762324b237eca697b376e3e5d067d761717230e
Fei Hu <hufei68@gmail.com>
2019-01-03 22:37:28 -0800

    Add the test cases for VariantTensorDataWriter and VariantTensorDataReader

--- tensorflow/core/kernels/data/dataset_utils_test.cc
+++ tensorflow/core/kernels/data/dataset_utils_test.cc
@@ -13,15 +13,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include "tensorflow/core/framework/variant.h"
 #include "tensorflow/core/kernels/data/dataset_utils.h"
-
+#include "tensorflow/core/lib/core/status_test_util.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
 namespace data {
 namespace {
 
-TEST(DatasetUtils, ComputeMoveVector) {
+TEST(DatasetUtilsTest, ComputeMoveVector) {
   struct TestCase {
     std::vector<int> indices;
     std::vector<bool> expected;
@@ -39,6 +40,45 @@ TEST(DatasetUtils, ComputeMoveVector) {
   for (auto& test_case : test_cases) {
     EXPECT_EQ(test_case.expected, ComputeMoveVector(test_case.indices));
   }
+}
+
+TEST(DatasetUtilsTest, VariantTensorData_Writer_Reader) {
+  VariantTensorData data;
+
+  // Basic test cases.
+  VariantTensorDataWriter writer(&data);
+  TF_ASSERT_OK(writer.WriteScalar("Int64", 24));
+  TF_ASSERT_OK(writer.WriteScalar("", "Empty_Key"));
+  Tensor input_tensor(DT_FLOAT, {1});
+  input_tensor.flat<float>()(0) = 2.0f;
+  TF_ASSERT_OK(writer.WriteTensor("Tensor", input_tensor));
+  TF_ASSERT_OK(writer.Flush());
+
+  VariantTensorDataReader reader(&data);
+  EXPECT_OK(reader.status());
+  int64 val_int64;
+  TF_ASSERT_OK(reader.ReadScalar("Int64", &val_int64));
+  EXPECT_EQ(val_int64, 24);
+  string val_string;
+  TF_ASSERT_OK(reader.ReadScalar("", &val_string));
+  EXPECT_EQ(val_string, "Empty_Key");
+  Tensor val_tensor;
+  TF_ASSERT_OK(reader.ReadTensor("Tensor", &val_tensor));
+  EXPECT_EQ(input_tensor.NumElements(), val_tensor.NumElements());
+  EXPECT_EQ(input_tensor.flat<float>()(0), val_tensor.flat<float>()(0));
+
+  // Test the non-existing key for VariantTensorDataReader.
+  EXPECT_EQ(error::NOT_FOUND,
+            reader.ReadScalar("Non_Existing_Key", &val_int64).code());
+  EXPECT_EQ(error::NOT_FOUND,
+            reader.ReadScalar("Non_Existing_Key", &val_string).code());
+  EXPECT_EQ(error::NOT_FOUND,
+            reader.ReadTensor("Non_Existing_Key", &val_tensor).code());
+
+  // Test the invalid parameter for the constructor of VariantTensorDataReader.
+  data.metadata_ = "Invalid Metadata";
+  VariantTensorDataReader reader2(&data);
+  EXPECT_EQ(error::INTERNAL, reader2.status().code());
 }
 
 }  // namespace
