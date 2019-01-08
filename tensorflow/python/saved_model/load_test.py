0122b072b77aca876e8d3e065280afa4c663d94f
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-08 04:37:51 -0800

    Small clean up in in saved_model/load_test.py
    Temporary `function` in checkpointable under test is not needed in most examples
    as `cycle` is passing signatures explicitly.
    
    PiperOrigin-RevId: 228309220

--- tensorflow/python/saved_model/load_test.py
+++ tensorflow/python/saved_model/load_test.py
@@ -42,9 +42,6 @@ class LoadTest(test.TestCase):
 
   def test_structure_import(self):
     root = tracking.Checkpointable()
-    root.f = def_function.function(
-        lambda x: 2. * x,
-        input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
     root.dep_one = tracking.Checkpointable()
     root.dep_two = tracking.Checkpointable()
     root.dep_two.dep = tracking.Checkpointable()
@@ -52,19 +49,25 @@ class LoadTest(test.TestCase):
     imported = self.cycle(root)
     self.assertIs(imported.dep_three, imported.dep_two.dep)
     self.assertIsNot(imported.dep_one, imported.dep_two)
-    self.assertEqual(4., imported.f(constant_op.constant(2.)).numpy())
 
   def test_variables(self):
     root = tracking.Checkpointable()
     root.v1 = variables.Variable(1.)
     root.v2 = variables.Variable(2.)
-    root.f = def_function.function(
-        lambda x: root.v2 * x,
-        input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
     imported = self.cycle(root)
     self.assertEquals(imported.v1.numpy(), 1.0)
     self.assertEquals(imported.v2.numpy(), 2.0)
+
+  def test_capture_variables(self):
+    root = tracking.Checkpointable()
+    root.weights = variables.Variable(2.)
+    root.f = def_function.function(
+        lambda x: root.weights * x,
+        input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
+    imported = self.cycle(root)
     self.assertEqual(4., imported.f(constant_op.constant(2.)).numpy())
+    imported.weights.assign(4.0)
+    self.assertEqual(8., imported.f(constant_op.constant(2.)).numpy())
 
   def _make_asset(self, contents):
     filename = tempfile.mktemp(prefix=self.get_temp_dir())
@@ -72,19 +75,16 @@ class LoadTest(test.TestCase):
       f.write(contents)
     return filename
 
-  def test_assets_import(self):
+  def test_assets(self):
     file1 = self._make_asset("contents 1")
     file2 = self._make_asset("contents 2")
 
     root = tracking.Checkpointable()
-    root.f = def_function.function(
-        lambda x: 2. * x,
-        input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
     root.asset1 = tracking.TrackableAsset(file1)
     root.asset2 = tracking.TrackableAsset(file2)
 
     save_dir = os.path.join(self.get_temp_dir(), "save_dir")
-    save.save(root, save_dir)
+    save.save(root, save_dir, signatures={})
 
     file_io.delete_file(file1)
     file_io.delete_file(file2)
@@ -110,18 +110,12 @@ class LoadTest(test.TestCase):
     with open(imported_output, "r") as f:
       self.assertEquals("contents", f.read())
 
-  def test_assets_dedup(self):
+  def test_dedup_assets(self):
     vocab = self._make_asset("contents")
     root = tracking.Checkpointable()
-    root.f = def_function.function(
-        lambda x: 2. * x,
-        input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
-
     root.asset1 = tracking.TrackableAsset(vocab)
     root.asset2 = tracking.TrackableAsset(vocab)
-
     imported = self.cycle(root)
-
     self.assertEqual(imported.asset1.asset_path.numpy(),
                      imported.asset2.asset_path.numpy())
 
@@ -154,7 +148,7 @@ class LoadTest(test.TestCase):
     imported = self.cycle(root)
     self.assertEqual(4., imported.f(constant_op.constant(2.0)).numpy())
 
-  def test_nested_func(self):
+  def test_nested_functions(self):
     f = def_function.function(
         lambda x: x*2.0,
         input_signature=[tensor_spec.TensorSpec(None, dtypes.float32)])
