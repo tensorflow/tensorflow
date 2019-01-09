e4bf78a4261468db1c4979be91c92d8b2d40918b
Zhenyu Tan <tanzheny@google.com>
2019-01-09 11:39:26 -0800

    Internal Cleanup.
    PiperOrigin-RevId: 228557086

--- tensorflow/python/keras/optimizer_v2/nadam.py
+++ tensorflow/python/keras/optimizer_v2/nadam.py
@@ -84,8 +84,7 @@ class Nadam(optimizer_v2.OptimizerV2):
     """
 
     # Backwards compatiblity with keras NAdam optimizer.
-    if 'schedule_decay' in kwargs:
-      kwargs['decay'] = kwargs.pop('schedule_decay', 0.004)
+    kwargs['decay'] = kwargs.pop('schedule_decay', 0.004)
     super(Nadam, self).__init__(name, **kwargs)
     self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
     self._set_hyper('decay', self._initial_decay)
