2fc0a7ac5df7dacae9eea571e04c4dc0f22e2aea
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-09 14:52:30 -0800

    This change makes errors more informative in case of functions. We now report two additional things: 1. The line number/file name of the culprit node inside the function. 2. The function calling stack
    For eg, consider the following code:
    
    import tensorflow as tf
    tf.enable_eager_execution()
    
    @tf.contrib.eager.defun
    def fn3(x):
      return x + 2
    
    @tf.contrib.eager.defun
    def fn2(x):
      tf.assert_equal(fn3(x), 3)
      return 2
    
    @tf.contrib.eager.defun
    def fn(x):
      return fn2(x)
    
    @tf.contrib.eager.defun
    def main(argv):
      x = fn(2)
      print(x)
    
    if __name__ == '__main__':
      tf.app.run()
    
    In this, the error message would contain the following:
    InvalidArgumentError: assertion failed: [] [Condition x == y did not hold element-wise:] [x (PartitionedCall:0) = ] [4] [y (assert_equal/y:0) = ] [3]
    	 [[node assert_equal/Assert/Assert (defined at filename:line_num) ]]
    	 [[StatefulPartitionedCall]]
    	 [[StatefulPartitionedCall]] [Op:StatefulPartitionedCall]
    
    Function calling stack:
    main -> fn -> fn2
    
    PiperOrigin-RevId: 228593653

--- tensorflow/python/eager/function.py
+++ tensorflow/python/eager/function.py
@@ -41,6 +41,8 @@ from tensorflow.python.framework import c_api_util
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import device as pydev
 from tensorflow.python.framework import dtypes as dtypes_module
+from tensorflow.python.framework import error_interpolation
+from tensorflow.python.framework import errors
 from tensorflow.python.framework import func_graph as func_graph_module
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_spec
@@ -129,6 +131,46 @@ def _parse_func_attrs(attributes):
       raise ValueError("Unsupported attribute type for %s with type %s" %
                        (key, type(value)))
   return attrs
+
+
+class _InterpolateFunctionError(object):
+  """Context Manager that interpolates the exception from 'top_level_func'."""
+
+  def __init__(self, top_level_func):
+    self._func = top_level_func
+
+  def __enter__(self):
+    pass
+
+  def __exit__(self, typ, exc, tb):
+    if not exc or not isinstance(exc, errors.OpError):
+      return False
+    message = compat.as_text(exc.message)
+    _, tags = error_interpolation.parse_message(message)
+    g = None
+    func_stack = []
+    # pylint: disable=protected-access
+    for t in tags:
+      if t.type == "function_node":
+        if t.name == compat.as_str(self._func.name):
+          g = self._func._graph
+        elif g:
+          next_func = g._get_function(t.name)
+          if next_func is not None and isinstance(next_func,
+                                                  _EagerDefinedFunction):
+            g = next_func._graph
+        if g:
+          func_stack.append(g.name)
+        else:
+          func_stack.append("<unknown>")
+    # pylint: enable=protected-access
+    if g:
+      message = error_interpolation.interpolate(message, g)
+      message += "\n\nFunction call stack:\n"
+      message += " -> ".join(func_stack)
+      message += "\n"
+      exc._message = message  # pylint: disable=protected-access
+    return False
 
 
 def _forward_name(n):
@@ -279,13 +321,14 @@ class _EagerDefinedFunction(object):
             "Arguments and signature arguments do not match: %s %s " %
             (len(args), len(list(self.signature.input_arg))))
       function_call_options = ctx.get_function_call_options()
-      outputs = functional_ops.partitioned_call(
-          args=args,
-          f=self,
-          tout=self._output_types,
-          executing_eagerly=executing_eagerly,
-          config=function_call_options.config_proto_serialized,
-          executor_type=function_call_options.executor_type)
+      with _InterpolateFunctionError(self):
+        outputs = functional_ops.partitioned_call(
+            args=args,
+            f=self,
+            tout=self._output_types,
+            executing_eagerly=executing_eagerly,
+            config=function_call_options.config_proto_serialized,
+            executor_type=function_call_options.executor_type)
 
     if executing_eagerly:
       return outputs
@@ -401,8 +444,8 @@ class Function(object):
     """
     return self._call_flat(
         (t for t in nest.flatten((args, kwargs))
-         if isinstance(
-             t, (ops.Tensor, resource_variable_ops.ResourceVariable))))
+         if isinstance(t, (ops.Tensor,
+                           resource_variable_ops.ResourceVariable))))
 
   def _call_flat(self, args):
     """Executes the wrapped function.
