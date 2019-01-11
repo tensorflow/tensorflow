2057f1b34a8568287358911bb12dfd3a02cbf3b7
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-11 14:24:48 -0800

    Start device tracer when running eager profiler.
    PiperOrigin-RevId: 228945120

--- tensorflow/c/eager/c_api_experimental_test.cc
+++ tensorflow/c/eager/c_api_experimental_test.cc
@@ -16,9 +16,9 @@ limitations under the License.
 #include "tensorflow/c/eager/c_api_experimental.h"
 
 #include <string.h>
-#include "absl/strings/match.h"
 #include "tensorflow/c/eager/c_api_test_util.h"
 #include "tensorflow/cc/profiler/profiler.h"
+#include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/protobuf.h"
 #include "tensorflow/core/platform/test.h"
@@ -27,7 +27,14 @@ limitations under the License.
 
 using tensorflow::string;
 
+namespace tensorflow {
 namespace {
+
+static bool HasSubstr(absl::string_view base, absl::string_view substr) {
+  bool ok = str_util::StrContains(base, substr);
+  EXPECT_TRUE(ok) << base << ", expected substring " << substr;
+  return ok;
+}
 
 void ExecuteWithProfiling(bool async) {
   TF_Status* status = TF_NewStatus();
@@ -42,6 +49,16 @@ void ExecuteWithProfiling(bool async) {
   TFE_Op* matmul = MatMulOp(ctx, m, m);
   TFE_TensorHandle* retvals[1] = {nullptr};
   int num_retvals = 1;
+
+  // Run op on GPU if it is present.
+  string gpu_device_name;
+  if (GetDeviceName(ctx, &gpu_device_name, "GPU")) {
+    TFE_OpSetDevice(matmul, gpu_device_name.c_str(), status);
+    ASSERT_TRUE(TF_GetCode(status) == TF_OK) << TF_Message(status);
+    const char* device_name = TFE_OpGetDevice(matmul, status);
+    ASSERT_TRUE(strstr(device_name, "GPU:0") != nullptr);
+  }
+
   TFE_Execute(matmul, &retvals[0], &num_retvals, status);
   EXPECT_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
   TFE_DeleteOp(matmul);
@@ -57,6 +74,13 @@ void ExecuteWithProfiling(bool async) {
   EXPECT_TRUE(profile_proto.ParseFromString(
       {reinterpret_cast<const char*>(profiler_result->data),
        profiler_result->length}));
+  string profile_proto_str = profile_proto.DebugString();
+  if (!gpu_device_name.empty()) {
+    EXPECT_TRUE(HasSubstr(profile_proto_str, "gpu:0"));
+    // device name with "stream:all" is collected by Device Tracer.
+    EXPECT_TRUE(HasSubstr(profile_proto_str, "stream:all"));
+  }
+  EXPECT_TRUE(HasSubstr(profile_proto_str, "cpu:0"));
   TF_DeleteBuffer(profiler_result);
 
   TF_Tensor* t = TFE_TensorHandleResolve(retvals[0], status);
@@ -77,3 +101,4 @@ TEST(CAPI, ExecuteWithTracing) { ExecuteWithProfiling(false); }
 TEST(CAPI, ExecuteWithTracingAsync) { ExecuteWithProfiling(true); }
 
 }  // namespace
+}  // namespace tensorflow
