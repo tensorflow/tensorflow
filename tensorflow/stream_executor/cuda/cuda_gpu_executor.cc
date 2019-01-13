d07bfe0b94ddfef5650e07b3613066c8ccf17bdf
Kashif Rasul <kashif.rasul@gmail.com>
2019-01-13 13:35:32 +0100

    removed deprecated cuda driver API calls

--- tensorflow/stream_executor/cuda/cuda_gpu_executor.cc
+++ tensorflow/stream_executor/cuda/cuda_gpu_executor.cc
@@ -1043,18 +1043,24 @@ DeviceDescription *CUDAExecutor::PopulateDeviceDescription() const {
     builder.set_numa_node(numa_node);
   }
 
-  CUdevprop prop;
-  if (CUDADriver::GetDeviceProperties(&prop, device_ordinal_)) {
-    builder.set_threads_per_block_limit(prop.maxThreadsPerBlock);
+  {
+    builder.set_threads_per_block_limit(
+      CUDADriver::GetDeviceAttribute(
+          CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK, device_)
+          .ValueOrDie());
 
     ThreadDim thread_dim_limit;
-    thread_dim_limit.x = prop.maxThreadsDim[0];
-    thread_dim_limit.y = prop.maxThreadsDim[1];
-    thread_dim_limit.z = prop.maxThreadsDim[2];
+    thread_dim_limit.x = CUDADriver::GetDeviceAttribute(
+        CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X, device_).ValueOrDie();
+    thread_dim_limit.y = CUDADriver::GetDeviceAttribute(
+        CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y, device_).ValueOrDie();
+    thread_dim_limit.z = CUDADriver::GetDeviceAttribute(
+        CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z, device_).ValueOrDie();
     builder.set_thread_dim_limit(thread_dim_limit);
 
-    float clock_rate_ghz = static_cast<float>(prop.clockRate) / 1e6;
-    builder.set_clock_rate_ghz(clock_rate_ghz);
+    int clockRate = CUDADriver::GetDeviceAttribute(
+        CU_DEVICE_ATTRIBUTE_CLOCK_RATE, device_).ValueOrDie();
+    builder.set_clock_rate_ghz(static_cast<float>(clockRate) / 1e6);
   }
 
   {
