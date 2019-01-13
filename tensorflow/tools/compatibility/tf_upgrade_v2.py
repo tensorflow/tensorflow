6f9355a52ce67a0e82d1a2186e31867e3531e97a
Yong Tang <yong.tang.github@outlook.com>
2019-01-13 19:47:00 +0000

    Add parentheses around new_value.test to cover `d if (a if t else b) else d`
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/tools/compatibility/tf_upgrade_v2.py
+++ tensorflow/tools/compatibility/tf_upgrade_v2.py
@@ -1277,10 +1277,11 @@ class TFAPIChangeSpec(ast_edits.APIChangeSpec):
       # This copies the prefix and suffix on old_value to new_value.
       pasta.ast_utils.replace_child(parent, old_value, new_value)
       ast.copy_location(new_value, old_value)
-      # Put parentheses around noise.value (and remove the old prefix/
-      # suffix, they should only be around new_value).
-      pasta.base.formatting.set(new_value, "prefix", "(")
-      pasta.base.formatting.set(new_value, "suffix", ")")
+      # Put parentheses around noise.value.test (and remove the old prefix/
+      # suffix, they should only be around new_value.test), so that:
+      # "uniform" if (a if b else c) else "gaussian" is valid.
+      pasta.base.formatting.set(new_value.test, "prefix", "(")
+      pasta.base.formatting.set(new_value.test, "suffix", ")")
 
     # Check if we have a uniform_noise keyword arg
     for uniform_noise in node.keywords:
@@ -1294,14 +1295,9 @@ class TFAPIChangeSpec(ast_edits.APIChangeSpec):
         _replace_uniform_noise_node(uniform_noise, uniform_noise.value)
         return node
 
-    # Maybe it was a positional arg
-    if len(node.args) < 5:
-      logs.append((ast_edits.ERROR, node.lineno, node.col_offset,
-                   "tf.image.extract_glimpse called without "
-                   "arguments, so automatic fix was disabled. "
-                   "tf.image.extract_glimpse has changed the semantics "
-                   "of the sxth argument."))
-    else:
+    # Since `noise`/`uniform_noise` is optional arg, nothing needs to be
+    # done if len(node.args) < 5.
+    if len(node.args) >= 5:
       _replace_uniform_noise_node(node, node.args[5])
       logs.append((ast_edits.INFO, node.lineno, node.col_offset,
                    "Changing uniform_noise arg of tf.image.extract_glimpse to "
