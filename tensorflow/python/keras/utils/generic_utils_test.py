e4b2c2cc66695cb99c2e63752ab10996c95b18b5
Dayananda-V <dayanandasiet@gmail.com>
2019-01-15 16:32:26 +0530

    TF Keras generic_utils_test missing test cases add
    test case added for
    1-func_dump
    2-func_load

--- tensorflow/python/keras/utils/generic_utils_test.py
+++ tensorflow/python/keras/utils/generic_utils_test.py
@@ -18,7 +18,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import marshal
+import numpy as np
+
+from absl.testing import parameterized
+
 from tensorflow.python import keras
+from tensorflow.python.keras.utils import generic_utils
 from tensorflow.python.platform import test
 
 
@@ -37,20 +43,13 @@ class HasArgTest(test.TestCase):
       _ = kwargs
       return x
 
-    self.assertTrue(keras.utils.generic_utils.has_arg(
-        f_x, 'x', accept_all=False))
-    self.assertFalse(keras.utils.generic_utils.has_arg(
-        f_x, 'y', accept_all=False))
-    self.assertTrue(keras.utils.generic_utils.has_arg(
-        f_x_args, 'x', accept_all=False))
-    self.assertFalse(keras.utils.generic_utils.has_arg(
-        f_x_args, 'y', accept_all=False))
-    self.assertTrue(keras.utils.generic_utils.has_arg(
-        f_x_kwargs, 'x', accept_all=False))
-    self.assertFalse(keras.utils.generic_utils.has_arg(
-        f_x_kwargs, 'y', accept_all=False))
-    self.assertTrue(keras.utils.generic_utils.has_arg(
-        f_x_kwargs, 'y', accept_all=True))
+    self.assertTrue(generic_utils.has_arg(f_x, 'x', accept_all=False))
+    self.assertFalse(generic_utils.has_arg(f_x, 'y', accept_all=False))
+    self.assertTrue(generic_utils.has_arg(f_x_args, 'x', accept_all=False))
+    self.assertFalse(generic_utils.has_arg(f_x_args, 'y', accept_all=False))
+    self.assertTrue(generic_utils.has_arg(f_x_kwargs, 'x', accept_all=False))
+    self.assertFalse(generic_utils.has_arg(f_x_kwargs, 'y', accept_all=False))
+    self.assertTrue(generic_utils.has_arg(f_x_kwargs, 'y', accept_all=True))
 
 
 class TestCustomObjectScope(test.TestCase):
@@ -63,7 +62,7 @@ class TestCustomObjectScope(test.TestCase):
     class CustomClass(object):
       pass
 
-    with keras.utils.generic_utils.custom_object_scope(
+    with generic_utils.custom_object_scope(
         {'CustomClass': CustomClass, 'custom_fn': custom_fn}):
       act = keras.activations.get('custom_fn')
       self.assertEqual(act, custom_fn)
@@ -74,11 +73,57 @@ class TestCustomObjectScope(test.TestCase):
 class SerializeKerasObjectTest(test.TestCase):
 
   def test_serialize_none(self):
-    serialized = keras.utils.generic_utils.serialize_keras_object(None)
+    serialized = generic_utils.serialize_keras_object(None)
     self.assertEqual(serialized, None)
-    deserialized = keras.utils.generic_utils.deserialize_keras_object(
-        serialized)
+    deserialized = generic_utils.deserialize_keras_object(serialized)
     self.assertEqual(deserialized, None)
+
+
+class FuncDumpAndLoadTest(test.TestCase, parameterized.TestCase):
+  @parameterized.parameters(['simple_function', 'closured_function'])
+  def test_func_dump_and_load(self, test_function_type):
+    if test_function_type == 'simple_function':
+      def test_func():
+        return r'\u'
+
+    elif test_function_type == 'closured_function':
+      def get_test_func():
+        x = r'\u'
+
+        def test_func():
+          return x
+        return test_func
+      test_func = get_test_func()
+    else:
+      raise Exception('Unknown test case for test_func_dump_and_load')
+
+    serialized = generic_utils.func_dump(test_func)
+    deserialized = generic_utils.func_load(serialized)
+    self.assertEqual(deserialized.__code__, test_func.__code__)
+    self.assertEqual(deserialized.__closure__, test_func.__closure__)
+    self.assertEqual(deserialized.__defaults__, test_func.__defaults__)
+
+  def test_func_dump_and_load_closure(self):
+    y = 0
+    test_func = lambda x: x + y
+    serialized, _, closure = generic_utils.func_dump(test_func)
+    deserialized = generic_utils.func_load(serialized, closure=closure)
+    self.assertEqual(deserialized.__code__, test_func.__code__)
+    self.assertEqual(deserialized.__closure__, test_func.__closure__)
+    self.assertEqual(deserialized.__defaults__, test_func.__defaults__)
+
+
+  @parameterized.parameters(
+      [keras.activations.softmax, np.argmax, lambda x: x**2, lambda x: x])
+  def test_func_dump_and_load_backwards_compat(self, test_func):
+    serialized = marshal.dumps(test_func.__code__).decode(
+        'raw_unicode_escape')
+
+    deserialized = generic_utils.func_load(
+        serialized, defaults=test_func.__defaults__)
+    self.assertEqual(deserialized.__code__, test_func.__code__)
+    self.assertEqual(deserialized.__closure__, test_func.__closure__)
+    self.assertEqual(deserialized.__defaults__, test_func.__defaults__)
 
 
 if __name__ == '__main__':
