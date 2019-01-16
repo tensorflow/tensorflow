ff12131917fb3f9c789063e3a78e1d8a41e49776
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-16 10:17:33 -0800

    Automated rollback of commit 8154ec0b4a893657e48b838c06e1f988aeac4115
    PiperOrigin-RevId: 229579722

--- tensorflow/core/grappler/optimizers/function_optimizer_test.cc
+++ tensorflow/core/grappler/optimizers/function_optimizer_test.cc
@@ -14,8 +14,6 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/core/grappler/optimizers/function_optimizer.h"
-
-#include "absl/algorithm/container.h"
 #include "tensorflow/cc/ops/functional_ops.h"
 #include "tensorflow/cc/ops/standard_ops.h"
 #include "tensorflow/core/framework/function_testlib.h"
@@ -1182,126 +1180,6 @@ TEST_F(FunctionOptimizerTest, InlineIndirectFunctionWithMergedDeadTensors) {
   ASSERT_EQ(tensors.size(), 1);
 
   test::ExpectTensorEqual<float>(tensors[0], tensors_expected[0]);
-}
-
-TEST_F(FunctionOptimizerTest, InlineIndirectFunctionWithFunctionalControlFlow) {
-  using test::function::NDef;
-  using FDH = FunctionDefHelper;
-
-  FunctionOptimizer optimizer(RewriterConfig::AGGRESSIVE);
-
-  FunctionDef add_func = FunctionDefHelper::Create(
-      "MyAdd", {"x:T", "y:T"}, {"z:T"}, {"T: {float, double}"},
-      {{{"add"}, "Add", {"x", "y"}, {{"T", "$T"}}}},
-      /* Mapping between function returns and function node outputs. */
-      {{"z", "add:z:0"}});
-
-  FunctionDef mul_func = FunctionDefHelper::Create(
-      "MyMul", {"x:T", "y:T"}, {"z:T"}, {"T: {float, double}"},
-      {{{"mul"}, "Mul", {"x", "y"}, {{"T", "$T"}}}},
-      /* Mapping between function returns and function node outputs. */
-      {{"z", "mul:z:0"}});
-
-  // Compute: return cond ? a + b : a * b
-  FunctionDef add_or_mul_func = FunctionDefHelper::Create(
-      "AddOrMul", {"cond:bool", "x:float", "y:float"}, {"z:float"}, {},
-      {
-          {{"if_node"},
-           "If",
-           {"cond", "x", "y"},
-           {
-               {"Tcond", DT_BOOL},
-               {"Tin", DataTypeSlice{DT_FLOAT, DT_FLOAT}},
-               {"Tout", DataTypeSlice{DT_FLOAT}},
-               {"then_branch", FDH::FunctionRef("MyAdd", {{"T", DT_FLOAT}})},
-               {"else_branch", FDH::FunctionRef("MyMul", {{"T", DT_FLOAT}})},
-               {"_lower_using_switch_merge", true},
-           }},
-      },
-      /* Mapping between function returns and function node outputs. */
-      {{"z", "if_node:output:0"}});
-
-  // Build a computation graph for:
-  //   is_add: bool
-  //   a: float
-  //   b: float
-  //   c = AddOrMul(is_add, a, b)  # is_add ? a + b : a * b
-  //   d = Identity(c)
-  //   return d
-
-  // c = MyMul(a, b)
-  GrapplerItem item;
-  item.fetch = {"d"};
-  item.graph = test::function::GDef(
-      {NDef("is_add", "Placeholder", {}, {{"dtype", DT_BOOL}}, kDevice),
-       NDef("a", "Placeholder", {}, {{"dtype", DT_FLOAT}}, kDevice),
-       NDef("b", "Placeholder", {}, {{"dtype", DT_FLOAT}}, kDevice),
-
-       NDef("c", "PartitionedCall", {"is_add", "a", "b"},
-            {{"Tin", DataTypeSlice{DT_BOOL, DT_FLOAT, DT_FLOAT}},
-             {"Tout", DataTypeSlice{DT_FLOAT}},
-             {"f", FDH::FunctionRef("AddOrMul")}},
-            kDevice),
-
-       NDef("d", "Identity", {"c"}, {{"T", DT_FLOAT}}, kDevice)},
-      // Function library.
-      {add_or_mul_func, add_func, mul_func});
-
-  GraphDef optimized_graph;
-  TF_EXPECT_OK(optimizer.Optimize(nullptr, item, &optimized_graph));
-
-  const auto count_nodes_with_op = [&](const string& op) {
-    return absl::c_count_if(optimized_graph.node(), [&](const NodeDef& node) {
-      return node.op() == op;
-    });
-  };
-
-  // All `PartitionedCall` nodes in the optimized graph must be inlined, and
-  // `If` node must be lowered to `Switch` and `Merge` nodes.
-  EXPECT_EQ(count_nodes_with_op("PartitionedCallOp"), 0);
-  EXPECT_EQ(count_nodes_with_op("If"), 0);
-  EXPECT_EQ(count_nodes_with_op("Switch"), 3);
-  EXPECT_EQ(count_nodes_with_op("Merge"), 1);
-
-  GrapplerItem optimized = item.WithGraph(std::move(optimized_graph));
-
-  Tensor one = test::AsScalar<float>(1.0);
-  Tensor two = test::AsScalar<float>(2.0);
-  Tensor three = test::AsScalar<float>(3.0);
-
-  const auto feed_args = [&](bool is_add) {
-    std::vector<std::pair<string, Tensor>> feed;
-    feed.emplace_back("a", one);
-    feed.emplace_back("b", two);
-    feed.emplace_back("is_add", test::AsScalar<bool>(is_add));
-    return feed;
-  };
-
-  {  // Check 'is_add == true': a + b
-    item.feed = feed_args(true);
-    optimized.feed = feed_args(true);
-
-    auto tensors_expected = EvaluateFetchNodes(item);
-    ASSERT_EQ(tensors_expected.size(), 1);
-    test::ExpectTensorEqual<float>(tensors_expected[0], three);
-
-    auto tensors = EvaluateFetchNodes(optimized);
-    ASSERT_EQ(tensors.size(), tensors_expected.size());
-    test::ExpectTensorEqual<float>(tensors_expected[0], tensors[0]);
-  }
-
-  {  // Check 'is_add == false': a * b
-    item.feed = feed_args(false);
-    optimized.feed = feed_args(false);
-
-    auto tensors_expected = EvaluateFetchNodes(item);
-    ASSERT_EQ(tensors_expected.size(), 1);
-    test::ExpectTensorEqual<float>(tensors_expected[0], two);
-
-    auto tensors = EvaluateFetchNodes(optimized);
-    ASSERT_EQ(tensors.size(), tensors_expected.size());
-    test::ExpectTensorEqual<float>(tensors_expected[0], tensors[0]);
-  }
 }
 
 TEST_F(FunctionOptimizerTest, SpecializeFunctionXTimesTwo) {
