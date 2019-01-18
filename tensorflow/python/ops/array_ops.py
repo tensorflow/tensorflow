2d6e9d561261a795912ad65d55679f9906296599
Yanan Cao <ycao@google.com>
2019-01-18 11:44:34 -0800

    Add back a missing arg documentation for validate_indices in tf.gather
    PiperOrigin-RevId: 229966279

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -3266,7 +3266,7 @@ def gather(params,
            validate_indices=None,
            name=None,
            axis=None,
-           batch_dims=0):  # pylint: disable=g-doc-args
+           batch_dims=0):
   r"""Gather slices from params axis axis according to indices.
 
   Gather slices from params axis axis according to indices.  `indices` must be
@@ -3320,13 +3320,14 @@ def gather(params,
       `axis + 1`.
     indices: The index `Tensor`.  Must be one of the following types: `int32`,
       `int64`. Must be in range `[0, params.shape[axis])`.
+    validate_indices: Deprecated, does nothing.
+    name: A name for the operation (optional).
     axis: A `Tensor`. Must be one of the following types: `int32`, `int64`. The
       `axis` in `params` to gather `indices` from. Must be greater than or equal
       to `batch_dims`.  Defaults to the first non-batch dimension. Supports
       negative indexes.
     batch_dims: An `integer`.  The number of batch dimensions.  Must be less
       than `ndims(inices)`.
-    name: A name for the operation (optional).
 
   Returns:
     A `Tensor`. Has the same type as `params`.
