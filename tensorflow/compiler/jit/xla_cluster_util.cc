483c34e957a2ad0b83cab0ac477524d840efda33
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-22 14:16:37 -0800

    Update references to bounds_checks.h
    PiperOrigin-RevId: 230408522

--- tensorflow/compiler/jit/xla_cluster_util.cc
+++ tensorflow/compiler/jit/xla_cluster_util.cc
@@ -19,9 +19,9 @@ limitations under the License.
 
 #include "absl/strings/str_cat.h"
 #include "tensorflow/compiler/jit/resource_operation_safety_analysis.h"
+#include "tensorflow/core/framework/bounds_check.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/graph/control_flow.h"
-#include "tensorflow/core/kernels/bounds_check.h"
 #include "tensorflow/core/util/device_name_utils.h"
 
 namespace tensorflow {
--- tensorflow/core/kernels/cwise_ops.h
+++ tensorflow/core/kernels/cwise_ops.h
@@ -22,9 +22,9 @@ limitations under the License.
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 
+#include "tensorflow/core/framework/bounds_check.h"
 #include "tensorflow/core/framework/numeric_types.h"
 #include "tensorflow/core/framework/tensor_types.h"
-#include "tensorflow/core/kernels/bounds_check.h"
 
 namespace Eigen {
 namespace internal {
