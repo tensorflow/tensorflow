986a88d706e661d1b999f9be6939de0998c52b4b
Guangda Lai <laigd@google.com>
2019-01-22 14:11:24 -0800

    Move TF-TRT C++ implementations to compiler/tf2tensorrt.
    PiperOrigin-RevId: 230407626

--- tensorflow/compiler/tf2tensorrt/python/ops/trt_ops.py
+++ tensorflow/compiler/tf2tensorrt/python/ops/trt_ops.py
@@ -12,19 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
-"""Exposes the python wrapper for TensorRT graph transforms."""
+"""Exposes the Python wrapper of TRTEngineOp."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# pylint: disable=unused-import,line-too-long
-from tensorflow.compiler.tf2tensorrt.python.ops import trt_ops
-from tensorflow.contrib.tensorrt.python.trt_convert import add_test_value
-from tensorflow.contrib.tensorrt.python.trt_convert import calib_graph_to_infer_graph
-from tensorflow.contrib.tensorrt.python.trt_convert import clear_test_values
-from tensorflow.contrib.tensorrt.python.trt_convert import create_inference_graph
-from tensorflow.contrib.tensorrt.python.trt_convert import enable_test_value
-from tensorflow.contrib.tensorrt.python.trt_convert import get_test_value
-from tensorflow.contrib.tensorrt.python.trt_convert import is_tensorrt_enabled
-# pylint: enable=unused-import,line-too-long
+import platform
+
+if platform.system() != "Windows":
+  # pylint: disable=wildcard-import,unused-import,g-import-not-at-top
+  from tensorflow.compiler.tf2tensorrt.ops.gen_trt_ops import *
+
+  from tensorflow.python.framework import load_library
+  from tensorflow.python.platform import resource_loader
+  # pylint: enable=wildcard-import,unused-import,g-import-not-at-top
+
+  _trt_ops = load_library.load_op_library(
+      resource_loader.get_path_to_datafile("_trt_ops.so"))
+else:
+  raise RuntimeError("Windows platforms are not supported")
