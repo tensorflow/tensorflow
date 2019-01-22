833e66e9ee7602444e8898741a15e9b96d5b927b
Saurabh Saxena <srbs@google.com>
2019-01-22 10:13:12 -0800

    Handle fetching uninitialized tensors from TensorLists.
    This is needed to support use-cases of the form:
    
    c = constant([1., 2.])
    l = tensor_list_from_tensor(c, [])
    t = tensor_list_get_item(l, 0)
    dt = gradients(t, c)
    
    In the above example gradient of the TensorListFromTensor op, a TensorListStack op, would attempt to stack a list with DT_INVALID type tensors. Since we only read index 0, the gradient list has only index 0 set.
    
    This change replaces the DT_INVALID tensors with zeros of the appropriate shape and type. This change adds an `element_shape` input to TensorListGetItem, TensorListPopBack, TensorListStack and TensorListGather ops. This is the shape used to build a zeros tensor for a DT_INVALID tensor in the list. Returning zeros for DT_VARIANT (nested lists) type tensors is not supported yet.
    
    I will add support for TensorListConcat in a follow-up change.
    
    PiperOrigin-RevId: 230362520

--- tensorflow/core/kernels/fill_functor.cu.cc
+++ tensorflow/core/kernels/fill_functor.cu.cc
@@ -88,6 +88,12 @@ struct SetZeroFunctor<GPUDevice, T> {
   }
 };
 
+template <>
+void SetZeroFunctor<GPUDevice, Variant>::operator()(
+    const GPUDevice& d, typename TTypes<Variant>::Flat out) {
+  // TODO(b/123028789): Implement this.
+}
+
 #define DEFINE_SETZERO_GPU(T) template struct SetZeroFunctor<GPUDevice, T>;
 TF_CALL_NUMBER_TYPES(DEFINE_SETZERO_GPU);
 TF_CALL_bool(DEFINE_SETZERO_GPU);
