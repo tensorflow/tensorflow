f29f2c5b3989fef0f2fc3a58e92514d7d2cd5d9e
A. Unique TensorFlower <gardener@tensorflow.org>
2019-01-25 01:02:49 -0800

    compat: Update forward compatibility horizon to 2019-01-25
    PiperOrigin-RevId: 230863315

--- tensorflow/python/compat/compat.py
+++ tensorflow/python/compat/compat.py
@@ -27,7 +27,7 @@ import datetime
 from tensorflow.python.util import tf_contextlib
 from tensorflow.python.util.tf_export import tf_export
 
-_FORWARD_COMPATIBILITY_HORIZON = datetime.date(2019, 1, 24)
+_FORWARD_COMPATIBILITY_HORIZON = datetime.date(2019, 1, 25)
 
 
 @tf_export("compat.forward_compatible")
