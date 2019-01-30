73d391da457e23d19ab3d6040cfd3069433f929a
nammbash <niroopshankar.ammbashankar@intel.com>
2019-01-29 17:43:46 -0800

    unit test for _MklQuantizedConv2DPerChannel op

--- tensorflow/core/kernels/mkl_quantized_conv_ops_perchannel_test.cc
+++ tensorflow/core/kernels/mkl_quantized_conv_ops_perchannel_test.cc
@@ -33,15 +33,12 @@ limitations under the License.
 
 namespace tensorflow {
 
-// TODO(bhavanis): Move ConvMklToTF to mkl_test_util.h as it is used by both FP32 and
-// int8 kernels.
-
-// Helper class for converting MKL tensors to TF tensors and comparing to
-// expected values
-
+// Some helper constants
 static const uint8 dummy_tensor[] = {0, 0, 0, 0, 0, 0, 0, 0};
 static const TensorShape dummy_shape({8});
 
+// Helper class for converting MKL tensors to TF tensors and comparing to
+// expected values
 class ConvMklToTF : public OpsTestBase {
  public:
   template <typename T>
@@ -75,32 +72,31 @@ TEST_F(QuantizedConv2DPerchannelTest, Small) {
                    .Input(FakeInput(DT_FLOAT))
                    .Input(FakeInput(DT_FLOAT))
                    .Input(FakeInput(DT_FLOAT))
-                   //  MKL metadata tensors //
+                    // MKL metadata tensors 
                    .Input(FakeInput(DT_UINT8))
                    .Input(FakeInput(DT_UINT8))
                    .Input(FakeInput(DT_UINT8))
                    .Input(FakeInput(DT_UINT8))
                    .Input(FakeInput(DT_UINT8))
                    .Input(FakeInput(DT_UINT8))
-                   ///////////////////////////
+                    // Attributes
                    .Attr("Tinput", DataTypeToEnum<quint8>::v())
                    .Attr("Tfilter", DataTypeToEnum<qint8>::v())
                    .Attr("T", DataTypeToEnum<quint8>::v())
                    .Attr("out_type", DataTypeToEnum<qint32>::v())
-                   // TODO(bhavanis): need to add `data_format` as well?
                    .Attr("strides", {1, stride, stride, 1})
-                   //.Attr("is_filter_const: bool = false")
                    .Attr("padding", "SAME")
                    .Attr("_kernel", "QuantizedMklOp")
                    .Finalize(node_def()));
   TF_ASSERT_OK(InitOp());
   
-  const int channels = 2;
-  const int image_width = 4;
-  const int image_height = 3;
+  // Image shape
   const int image_batch_count = 1;
+  const int image_height = 3;
+  const int image_width = 4;
+  const int image_channel = 1;
 
-  // Image -> uint8
+  // Image is uint8
   const float image_min = 0.0f;
   const float image_max = 255.0f;
 
@@ -109,29 +105,36 @@ TEST_F(QuantizedConv2DPerchannelTest, Small) {
   // |  5 |  6 |  7 |  8 |
   // |  9 | 10 | 11 | 12 |
   Tensor image_float(DT_FLOAT,
-                     {image_batch_count, image_height, image_width, channels});
+                     {image_batch_count, image_height, image_width, image_channel});
   test::FillValues<float>(&image_float,
-                          {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12});
+                          {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+  // Create image tensor
   Tensor image_quantized =
       FloatTensorToQuantized<quint8>(image_float, image_min, image_max);
+  
+  // Filter shape
+  const int filter_height = 3;
+  const int filter_width = 3;
+  const int filter_channel = 1;
+  const int filter_count = 2;
 
-  const int filter_size = 3;
-  const int filter_count = 1;
-
-  // Filter -> int8
+  // Filter is int8
   const float filter_min = -128.0f;
   const float filter_max = 127.0f;
 
-  // The filter matrix is:
+  // The filter matrix is for each channel:
   // | 1 | 4 | 7 |
   // | 2 | 5 | 8 |
   // | 3 | 6 | 9 |
   Tensor filter_float(DT_FLOAT,
-                      {filter_size, filter_size, channels, filter_count});
+                      {filter_height, filter_width, filter_channel, filter_count});
   test::FillValues<float>(&filter_float, {1, 1, 4, 4, 7, 7, 2, 2, 5, 5, 8, 8, 3, 3, 6, 6, 9, 9});
+
+  // create filter tensor 
   Tensor filter_quantized =
       FloatTensorToQuantized<qint8>(filter_float, filter_min, filter_max);
 
+  // Add the tensors as input to the current Op.
   AddInputFromArray<quint8>(image_quantized.shape(),
                             image_quantized.flat<quint8>());
   AddInputFromArray<qint8>(filter_quantized.shape(),
@@ -147,10 +150,27 @@ TEST_F(QuantizedConv2DPerchannelTest, Small) {
   AddInputFromArray<uint8>(dummy_shape, dummy_tensor);
   AddInputFromArray<uint8>(dummy_shape, dummy_tensor);
 
+  // Run the Op Kernel.  
   TF_ASSERT_OK(RunOpKernel());
 
+  // Get the output
+  const Tensor& output = *GetOutput(0);
+  const Tensor& output_mkl_metadata = *GetOutput(3);
+
+  // Convert the output tensor in MKL to TF .  
+  ConvMklToTF conv_comp;
+  Tensor output_quantized;
+  conv_comp.ConvertMKL2TF<qint32>(DT_QINT32, output, output_mkl_metadata,
+                                  output_quantized);
+
+  const float output_min = GetOutput(1)->flat<float>()(0);
+  const float output_max = GetOutput(2)->flat<float>()(0);
+  Tensor output_float =
+      QuantizedTensorToFloat<qint32>(output_quantized, output_min, output_max);
+
+  // Get the Expected Output tensor.  
   // We're sliding the 3x3 filter across the 3x4 image, with accesses outside
-  // the input set to zero because we're using the 'SAME' padding mode.
+  // the input dimensions set to zero because we're using the 'SAME' padding mode.
   // The calculations behind the expected output are:
   // (1*0)+(4*0)+(7*0)+(2*0)+(5*1)+(8*2)+(3*0)+(6*5)+(9*6)=105
   // (1*0)+(4*0)+(7*0)+(2*1)+(5*2)+(8*3)+(3*5)+(6*6)+(9*7)=150
@@ -164,33 +184,27 @@ TEST_F(QuantizedConv2DPerchannelTest, Small) {
   // (1*5)+(4*6)+(7*7)+(2*9)+(5*10)+(8*11)+(3*0)+(6*0)+(9*0)=234
   // (1*6)+(4*7)+(7*8)+(2*10)+(5*11)+(8*12)+(3*0)+(6*0)+(9*0)=261
   // (1*7)+(4*11)+(7*0)+(2*8)+(5*12)+(8*0)+(3*0)+(6*0)+(9*0)=121
-  // This means we should end up with this matrix:
+  
+  // This means we should end up with this matrix for each channel:
   // |  105  |  150  |  183  |   95  |
   // |  235  |  312  |  357  |  178  |
   // |  187  |  234  |  261  |  121  |
 
+  // Output size expected at  N x IH X IW x filter_count
+  // create the expectation tensor
   const int expected_width = image_width;
-  const int expected_height = image_height * filter_count * channels;
+  const int expected_height = image_height;
+  
   Tensor expected_float(
       DT_FLOAT, TensorShape({image_batch_count, expected_height, expected_width,
                              filter_count}));
+  
   test::FillValues<float>(&expected_float, {105, 105, 150, 150, 183, 183, 95, 95, 235, 235, 312, 312, 357, 357,
                                             178, 178, 187, 187, 234, 234, 261, 261, 121, 121});
-
-  const Tensor& output = *GetOutput(0);
-  const Tensor& output_mkl_metadata = *GetOutput(3);
-
-  ConvMklToTF conv_comp;
-  Tensor output_quantized;
-  conv_comp.ConvertMKL2TF<qint32>(DT_QINT32, output, output_mkl_metadata,
-                                  output_quantized);
-
-  const float output_min = GetOutput(1)->flat<float>()(0);
-  const float output_max = GetOutput(2)->flat<float>()(0);
-  Tensor output_float =
-      QuantizedTensorToFloat<qint32>(output_quantized, output_min, output_max);
-
-  //test::ExpectTensorNear<float>(expected_float, output_float, 1.0);
+  
+  
+  // test whether the values are as expected. 
+  test::ExpectTensorNear<float>(expected_float, output_float, 2.0);
   
 }
 
