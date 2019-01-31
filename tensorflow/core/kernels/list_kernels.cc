db820306defa458e38edc55c715d2c7fcbe71041
Saurabh Saxena <srbs@google.com>
2019-01-31 15:08:28 -0800

    Replace uninitialized tensors with zeros in TensorListConcat. In TensorListGetItem, infer the shape of the requested entry from other list elements if element_shape is not fully defined.
    PiperOrigin-RevId: 231862507

--- tensorflow/core/kernels/list_kernels.cc
+++ tensorflow/core/kernels/list_kernels.cc
@@ -148,6 +148,7 @@ Status TensorShapeFromTensor(const Tensor& t, PartialTensorShape* out) {
   if (t.shape() == TensorShape({})) {
     if ((t.dtype() == DT_INT32 && t.scalar<int32>()() == -1) ||
         (t.dtype() == DT_INT64 && t.scalar<int64>()() == -1)) {
+      *out = PartialTensorShape();
       return Status::OK();
     }
     return errors::InvalidArgument(
@@ -633,6 +634,10 @@ REGISTER_KERNEL_BUILDER(Name("TensorListConcatLists").Device(DEVICE_GPU),
                               .Device(DEVICE_CPU),                \
                           TensorListGather<CPUDevice, T>)         \
   REGISTER_KERNEL_BUILDER(Name("TensorListConcat")                \
+                              .TypeConstraint<T>("element_dtype") \
+                              .Device(DEVICE_CPU),                \
+                          TensorListConcat<CPUDevice, T>)         \
+  REGISTER_KERNEL_BUILDER(Name("TensorListConcatV2")              \
                               .TypeConstraint<T>("element_dtype") \
                               .Device(DEVICE_CPU),                \
                           TensorListConcat<CPUDevice, T>)         \
