fac4249a529bf2e0a8ef93b44868d729c51e4f07
Loo Rong Jie <loorongjie@gmail.com>
2019-02-02 10:03:20 +0800

    Fix Windows build due to winsock2.h conflicts

--- configure.py
+++ configure.py
@@ -1508,6 +1508,8 @@ def set_windows_build_flags(environ_cp):
   write_to_bazelrc('build --config monolithic')
   # Suppress warning messages
   write_to_bazelrc('build --copt=-w --host_copt=-w')
+  # Fix winsock2.h conflicts
+  write_to_bazelrc('build --copt=-DWIN32_LEAN_AND_MEAN --host_copt=-DWIN32_LEAN_AND_MEAN')
   # Output more verbose information when something goes wrong
   write_to_bazelrc('build --verbose_failures')
   # The host and target platforms are the same in Windows build. So we don't
