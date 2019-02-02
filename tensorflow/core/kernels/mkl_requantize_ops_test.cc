33a704aad006ee80c6165a79297f6c3aba19576f
Penporn Koanantakool <38085909+penpornk@users.noreply.github.com>
2019-02-01 21:56:54 -0800

    Update tensorflow/core/kernels/mkl_requantize_ops_test.cc
    Co-Authored-By: nammbash <38869685+nammbash@users.noreply.github.com>

--- tensorflow/core/kernels/mkl_requantize_ops_test.cc
+++ tensorflow/core/kernels/mkl_requantize_ops_test.cc
@@ -265,7 +265,7 @@ TEST_F(MklRequantizatedOpsTest, RequantizePerChannelTest_Basic) {
                             input_tensor_qint32.flat<qint32>());
 
   // Calculate the Min and max from the ranges
-  float ch1_min = -1.0 * range_weights_ch1;
+  float ch1_min = -range_weights_ch1;
   float ch1_max = range_weights_ch1;
   float ch2_min = -range_weights_ch2;
   float ch2_max = range_weights_ch2;
