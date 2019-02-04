0a62b8b72441d6a8a12261e72e354dcccc658bdf
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-04 15:06:51 -0800

    Add a version of SerializeToStringDeterministic that serializes to a pre-allocated buffer. In the common case, this avoids a double write into the string and is in some cases about 30% faster on large protobufs.
    Benchmark:
    
    Run on *** (72 X 2993 MHz CPUs); 2019-02-01T17:30:47.860707104-08:00
    CPU: Intel Skylake Xeon with HyperThreading (36 cores) dL1:32KB dL2:1024KB dL3:24MB
    Benchmark                           Time(ns)        CPU(ns)     Iterations
    --------------------------------------------------------------------------
    BM_ProtoSerializationToString/1           99.1           99.1      7063926
    BM_ProtoSerializationToString/8          450            450        1557551
    BM_ProtoSerializationToString/64        3154           3154         222251
    BM_ProtoSerializationToString/512      24947          24949          28043
    BM_ProtoSerializationToString/4k      224626         224626           2995
    BM_ProtoSerializationToString/10k     563273         563262           1000
    BM_ProtoSerializationToBuffer/1           77.7           77.8      9015139
    BM_ProtoSerializationToBuffer/8          351            351        1996330
    BM_ProtoSerializationToBuffer/64        2511           2511         278283
    BM_ProtoSerializationToBuffer/512      19846          19849          35234
    BM_ProtoSerializationToBuffer/4k      184208         184262           3502
    BM_ProtoSerializationToBuffer/10k     427867         427938           1622
    PiperOrigin-RevId: 232374729

---
+++ tensorflow/core/lib/strings/proto_serialization_test.cc
@@ -0,0 +1,66 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/lib/strings/proto_serialization.h"
+
+#include <string>
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/node_def.pb.h"
+#include "tensorflow/core/lib/strings/strcat.h"
+#include "tensorflow/core/platform/test.h"
+#include "tensorflow/core/platform/test_benchmark.h"
+
+namespace tensorflow {
+
+static void BM_ProtoSerializationToString(int iters, int num_nodes) {
+  testing::StopTiming();
+  GraphDef graph_def;
+  for (int i = 0; i < num_nodes; ++i) {
+    NodeDef* node = graph_def.add_node();
+    node->set_name(strings::StrCat("node", i));
+    node->set_op(strings::StrCat("op", i % 10));
+  }
+  testing::StartTiming();
+  for (int i = 0; i < iters; ++i) {
+    string serialized;
+    testing::DoNotOptimize(
+        SerializeToStringDeterministic(graph_def, &serialized));
+  }
+  testing::StopTiming();
+}
+BENCHMARK(BM_ProtoSerializationToString)->Range(1, 10000);
+
+static void BM_ProtoSerializationToBuffer(int iters, int num_nodes) {
+  testing::StopTiming();
+  GraphDef graph_def;
+  for (int i = 0; i < num_nodes; ++i) {
+    NodeDef* node = graph_def.add_node();
+    node->set_name(strings::StrCat("node", i));
+    node->set_op(strings::StrCat("op", i % 10));
+  }
+  testing::StartTiming();
+  int size = graph_def.ByteSizeLong();
+  for (int i = 0; i < iters; ++i) {
+    char* buf = new char[size];
+    testing::DoNotOptimize(
+        SerializeToBufferDeterministic(graph_def, buf, size));
+    delete[] buf;
+  }
+  testing::StopTiming();
+}
+
+BENCHMARK(BM_ProtoSerializationToBuffer)->Range(1, 10000);
+
+}  // namespace tensorflow
