2d46ea894f03ee8aa391982f054912f0860b04d4
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-05 13:56:25 -0800

    Remove unused inclusions and add missing inclusions
    PiperOrigin-RevId: 232552217

--- tensorflow/compiler/xla/client/lib/matrix_test.cc
+++ tensorflow/compiler/xla/client/lib/matrix_test.cc
@@ -24,7 +24,6 @@ limitations under the License.
 #include "tensorflow/compiler/xla/tests/client_library_test_base.h"
 #include "tensorflow/compiler/xla/tests/test_macros.h"
 #include "tensorflow/compiler/xla/types.h"
-#include "tensorflow/compiler/xla/xla_data.pb.h"
 
 namespace xla {
 namespace {
