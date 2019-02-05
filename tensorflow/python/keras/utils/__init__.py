adeee2dc176565a445e04158b6d9fa7456ef6d69
Jan Zikes <zikes@spaceknow.com>
2019-02-05 18:10:06 +0100

    Enable keras.utils.metrics_utils AUCCurve api

--- tensorflow/python/keras/utils/__init__.py
+++ tensorflow/python/keras/utils/__init__.py
@@ -36,6 +36,7 @@ from tensorflow.python.keras.utils.layer_utils import convert_all_kernels_in_mod
 from tensorflow.python.keras.utils.layer_utils import get_source_inputs
 from tensorflow.python.keras.utils.layer_utils import print_summary
 from tensorflow.python.keras.utils.losses_utils import squeeze_or_expand_dimensions
+from tensorflow.python.keras.utils.metrics_utils import AUCCurve
 from tensorflow.python.keras.utils.multi_gpu_utils import multi_gpu_model
 from tensorflow.python.keras.utils.np_utils import normalize
 from tensorflow.python.keras.utils.np_utils import to_categorical
