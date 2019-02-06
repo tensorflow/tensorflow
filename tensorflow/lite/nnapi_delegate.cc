0c4f5dfea4ceb3d7c0b46fc04828420a344f7598
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-06 12:56:47 -0800

    Add new op schema for TFlite equivalent of tf.gather_nd.
    PiperOrigin-RevId: 232728420

--- tensorflow/lite/nnapi_delegate.cc
+++ tensorflow/lite/nnapi_delegate.cc
@@ -665,6 +665,7 @@ TfLiteStatus AddOpsAndParams(
       case tflite::BuiltinOperator_CEIL:
       case tflite::BuiltinOperator_REVERSE_V2:
       case tflite::BuiltinOperator_ADD_N:
+      case tflite::BuiltinOperator_GATHER_ND:
         logError("Op code %d is currently not delegated to NNAPI", builtin);
         return kTfLiteError;
         break;
