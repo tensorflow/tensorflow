be3d1dc8354f9edde67bb0071b73e4cf22d15348
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-06 19:46:25 -0800

    Make the split op work with int32 in tf-lite.
    PiperOrigin-RevId: 232796544

--- tensorflow/lite/toco/tflite/operator.cc
+++ tensorflow/lite/toco/tflite/operator.cc
@@ -1267,9 +1267,11 @@ class Split
   int GetVersion(const OperatorSignature& op_signature) const override {
     const string& input_name = op_signature.op->inputs[0];
     const Array& input_array = op_signature.model->GetArray(input_name);
-    // If the op take int8 input, it is version 2.
+    // If the op take int8 input, it is version 2, for int32 it's version 3.
     if (input_array.data_type == ArrayDataType::kInt8) {
       return 2;
+    } else if (input_array.data_type == ArrayDataType::kInt32) {
+      return 3;
     }
     return 1;
   }
