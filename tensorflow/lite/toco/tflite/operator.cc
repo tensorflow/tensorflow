81e366d58a252b8a3a292c6e9c52dc31e3043b2b
Jian Li <jianlijianli@google.com>
2019-02-08 10:48:29 -0800

    Create int8 reduce min and reduce max.
    PiperOrigin-RevId: 233087624

--- tensorflow/lite/toco/tflite/operator.cc
+++ tensorflow/lite/toco/tflite/operator.cc
@@ -1115,6 +1115,12 @@ class ReduceMax
   }
 
   int GetVersion(const OperatorSignature& op_signature) const override {
+    const string& input_name = op_signature.op->inputs[0];
+    const Array& input_array = op_signature.model->GetArray(input_name);
+    // If the op take int8 input, it is version 2.
+    if (input_array.data_type == ArrayDataType::kInt8) {
+      return 2;
+    }
     return 1;
   }
 };
@@ -1136,6 +1142,12 @@ class ReduceMin
   }
 
   int GetVersion(const OperatorSignature& op_signature) const override {
+    const string& input_name = op_signature.op->inputs[0];
+    const Array& input_array = op_signature.model->GetArray(input_name);
+    // If the op take int8 input, it is version 2.
+    if (input_array.data_type == ArrayDataType::kInt8) {
+      return 2;
+    }
     return 1;
   }
 };
