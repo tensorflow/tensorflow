d17c8b75dc852de18926f4c82dbac35369ea0f1f
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-02-11 23:24:01 +0000

    Updated license date, refactored repeated commands into functions adding NVIDIA CUDA version and GPU model and bazel version

--- tools/tf_env_collect.sh
+++ tools/tf_env_collect.sh
@@ -34,56 +34,79 @@ python_bin_path=$(which python || which python3 || die "Cannot find Python binar
 
 get_os() {
   uname -a
-}
-
-get_gpu() {
-  nvidia-smi
-  if [ $? -eq 127 ]
-  then
-     echo "This machine does not have an NVIDIA CUDA GPU."
-  fi
-}
-
-{
-  echo
-  echo '== cat /etc/issue ==============================================='
-  uname -a
   uname=`uname -s`
   if [ "$(uname)" == "Darwin" ]; then
     echo Mac OS X `sw_vers -productVersion`
   elif [ "$(uname)" == "Linux" ]; then
     cat /etc/*release | grep VERSION
   fi
+}
 
-  echo
-  echo '== in docker container ? ========================================'
-  num=`cat /proc/1/cgroup | grep docker | wc -l`;
+check_docker () {
+  num=`cat /proc/1/cgroup 2>/dev/null | grep docker | wc -l`;
   if [ $num -ge 1 ]; then
     echo "Yes"
   else
     echo "No"
   fi
+}
+
+check_cuda_gpu() {
+  # Check for CUDA
+  echo "CUDA Compiler version:"
+  if ! nvcc --version 2>/dev/null
+  then
+    echo "This machine does not have CUDA installed."
+  fi
+  # Check for GPU
+  echo "NVIDIA GPU:"
+  if ! nvidia-smi 2>/dev/null
+  then
+    echo "This machine does not have an NVIDIA CUDA GPU."
+  fi
+}
+
+check_bazel() {
+  if ! bazel version 2>/dev/null
+  then
+    echo "This machine does not have bazel installed."
+  fi
+}
+
+check_pips() {
+  if ! pip list 2>&1 | grep "proto\|numpy\|tensorflow"
+  then
+    echo "This machine does not have pip configured."
+  fi
+}
+
+{
+  # Getting the operating system name
+  echo
+  echo '== OS ==========================================================='
+  get_os
+
+  # Checking whether in docker container
+  echo
+  echo '== in docker container ? ========================================'
+  check_docker
 
   # Getting C++ compiler version
   echo
-  echo '== compiler ====================================================='
+  echo '== c++ compiler ================================================='
   c++ --version 2>&1
-
-  # Getting the operating system name
-  echo
-  echo '== uname -a ====================================================='
-  get_os
 
   # Getting the Bazel version
   echo
   echo '== bazel ========================================================'
-  bazel version
+  check_bazel
 
+  # Checking if numpy, proto and tensorflow installed through pip
   echo
   echo '== check pips ==================================================='
-  pip list 2>&1 | grep "proto\|numpy\|tensorflow"
+  check_pips
 
-
+  # Check for virtual environments
   echo
   echo '== check for virtualenv ========================================='
   ${python_bin_path} -c "import sys;print(hasattr(sys, \"real_prefix\"))"
@@ -123,18 +146,20 @@ DEBUG_LD=libs ${python_bin_path} -c "import tensorflow"  2>>${OUTPUT_FILE} > /tm
   # Get Bazel version
   echo
   echo '== bazel ========================================================'
-  bazel version
+  check_bazel
 
+  # Get NVIDIA GPU model and CUDA compiler version
   echo
   echo '== nvidia-smi ==================================================='
-  nvidia-smi 2>&1
+  check_cuda_gpu
 
+  # Get CUDA libraries (cuBLAS, cuDNN, cuFFT etc.)
   echo
   echo '== cuda libs  ==================================================='
 } >> ${OUTPUT_FILE}
 
-find /usr/local -type f -name 'libcudart*'  2>/dev/null | grep cuda |  grep -v "\\.cache" >> ${OUTPUT_FILE}
-find /usr/local -type f -name 'libudnn*'  2>/dev/null | grep cuda |  grep -v "\\.cache" >> ${OUTPUT_FILE}
+find /usr/local -type f -name 'libcudart*' 2>/dev/null | grep cuda |  grep -v "\\.cache" >> ${OUTPUT_FILE}
+find /usr/local -type f -name 'libudnn*' 2>/dev/null | grep cuda |  grep -v "\\.cache" >> ${OUTPUT_FILE}
 
 # Remove any words with google.
 mv $OUTPUT_FILE old-$OUTPUT_FILE
