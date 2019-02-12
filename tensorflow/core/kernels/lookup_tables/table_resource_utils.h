965c80bab5acc5dc1654e7f9df279ebb0396ea9c
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-12 12:03:12 -0800

    Add opkernel templates for common table operations.
    PiperOrigin-RevId: 233652472

--- tensorflow/core/kernels/lookup_tables/table_resource_utils.h
+++ tensorflow/core/kernels/lookup_tables/table_resource_utils.h
@@ -33,22 +33,8 @@ template <typename HeterogeneousKeyType, typename ValueType>
 class LookupTableWithSynchronization
     : public LookupTableInterface<HeterogeneousKeyType, ValueType> {
  public:
-  // By convention, it is assumed that the OpKernel which creates this
-  // resource is bound to an op whose first input is an in64 tensor list with a
-  // first element whose boolean value indicates whether a mutex should be
-  // exported to synchronize access to state. If the tensor list is empty,
-  // the status in ctx is set to InvalidArgument and this object is in an
-  // undefined state.
-  LookupTableWithSynchronization(OpKernelContext* ctx, OpKernel* kernel) {
-    OpInputList table_int64_args;
-    OP_REQUIRES_OK(ctx, ctx->input_list("table_int64_args", &table_int64_args));
-    if (table_int64_args.size() == 0) {
-      ctx->SetStatus(errors::InvalidArgument(
-          "table_int64_args should not be empty. Set the first element "
-          "to 1 to enable synchronized table use and to 0 otherwise."));
-      return;
-    }
-    if (ctx->input(0).scalar<int64>()() != 0) {
+  LookupTableWithSynchronization(bool enable_synchronization) {
+    if (enable_synchronization) {
       mutex_ = absl::make_unique<mutex>();
     }
   }
@@ -68,20 +54,20 @@ class LookupTableWithSynchronization
 // two-stage lookups. If the first key lookup fails, clients can choose
 // to perform a fallback lookup using an externally supplied table.
 template <typename HeterogeneousKeyType, typename ValueType,
-          typename FallbackTableRegisteredType =
+          typename FallbackTableBaseType =
               LookupTableInterface<HeterogeneousKeyType, ValueType>>
 class LookupTableWithFallbackInterface
     : public LookupTableWithSynchronization<HeterogeneousKeyType, ValueType> {
  public:
-  LookupTableWithFallbackInterface(OpKernelContext* ctx, OpKernel* kernel,
-                                   FallbackTableRegisteredType* fallback_table)
-      : LookupTableWithSynchronization<HeterogeneousKeyType, ValueType>(ctx,
-                                                                        kernel),
+  LookupTableWithFallbackInterface(bool enable_synchronization,
+                                   const FallbackTableBaseType* fallback_table)
+      : LookupTableWithSynchronization<HeterogeneousKeyType, ValueType>(
+            enable_synchronization),
         fallback_table_(fallback_table) {}
 
   // Clients are required to fail when ctx is set to a not-OK status in
   // the constructor so this dereference is safe.
-  const FallbackTableRegisteredType& fallback_table() const {
+  const FallbackTableBaseType& fallback_table() const {
     return *fallback_table_;
   }
 
@@ -92,7 +78,7 @@ class LookupTableWithFallbackInterface
   }
 
  private:
-  FallbackTableRegisteredType* fallback_table_;
+  const FallbackTableBaseType* fallback_table_;
 };
 
 }  // namespace tables
