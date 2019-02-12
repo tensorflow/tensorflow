6332189a67b99b1caeb35ca750c0d6b434c386f0
Andrew Selle <aselle@andyselle.com>
2019-02-12 10:23:59 -0800

    Revert "Raise an error when loop execution is cancelled"
    This reverts commit fc5392dedec126f988788a597edb55021fb07b60.

--- tensorflow/core/kernels/control_flow_ops.cc
+++ tensorflow/core/kernels/control_flow_ops.cc
@@ -603,8 +603,12 @@ LoopCondOp::~LoopCondOp() = default;
 void LoopCondOp::Compute(OpKernelContext* context) {
   CancellationManager* cm = context->cancellation_manager();
   bool already_cancelled = cm->IsCancelled();
-  OP_REQUIRES(context, !already_cancelled,
-              errors::Cancelled("Loop execution was cancelled."));
+
+  if (already_cancelled) {
+    Tensor continue_running(false);
+    context->set_output(0, continue_running);
+    return;
+  }
 
   context->set_output(0, context->input(0));
 }
