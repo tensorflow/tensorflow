83a85e2b531e92e19aa774818afd6478d125f867
Michael Käufl <michael-k@users.noreply.github.com>
2019-02-13 11:32:51 +0100

    Add Python 3.7 to classifiers
    Python 3.7 compatibility seems fixed in 1.13, see
    eg. tensorflow/tensorflow#20517 and tensorflow/tensorflow#17022.

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -284,6 +284,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
