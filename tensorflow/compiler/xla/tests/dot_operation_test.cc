2e6167178c16b137486c98019426060556471078
Blake Hechtman <blakehechtman@google.com>
2019-02-13 15:48:36 -0800

    [XLA] Simplify dot operations that have only batch and contracting dimension on the lhs or rhs.
    PiperOrigin-RevId: 233845773

--- tensorflow/compiler/xla/tests/dot_operation_test.cc
+++ tensorflow/compiler/xla/tests/dot_operation_test.cc
@@ -1188,6 +1188,8 @@ std::vector<EinsumParamType> GetEinsumTestCases() {
       p{v{8, 55, 11, 3}, v{55, 11, 3, 29}, "mkBC,kBCn->BCnm"},
       p{v{5, 6}, v{6, 7}, "ab,cd->dcba"},
       p{v{6}, v{6, 7}, "b,bc->c"},
+      p{v{5, 6, 7}, v{5, 6, 7}, "abc,abc->ab"},
+      p{v{5, 6, 7}, v{7, 6, 5}, "abc,cba->ca"},
       p{v{77}, v{77}, "a,a->a"},
       p{v{77}, v{77, 55}, "a,ab->ba"},
       p{v{2, 3, 77}, v{77, 2, 3, 55}, "ija,aijb->baij"},
