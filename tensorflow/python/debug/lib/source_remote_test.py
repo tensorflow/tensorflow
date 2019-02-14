95fd0fa9f2eed0beda9923f27d37270e0b5d9353
Shanqing Cai <cais@google.com>
2019-02-13 18:42:52 -0800

    tfdbg: de-flake and re-enable a test in source_remote_test
    For some reason, the debug grpc server startup is sometimes
    delayed in Python3. Adding polling of test server to fix that.
    
    PiperOrigin-RevId: 233873150

--- tensorflow/python/debug/lib/source_remote_test.py
+++ tensorflow/python/debug/lib/source_remote_test.py
@@ -48,7 +48,8 @@ class SendTracebacksTest(test_util.TensorFlowTestCase):
     test_util.TensorFlowTestCase.setUpClass()
     (cls._server_port, cls._debug_server_url, cls._server_dump_dir,
      cls._server_thread,
-     cls._server) = grpc_debug_test_server.start_server_on_separate_thread()
+     cls._server) = grpc_debug_test_server.start_server_on_separate_thread(
+         poll_server=True)
     cls._server_address = "localhost:%d" % cls._server_port
     (cls._server_port_2, cls._debug_server_url_2, cls._server_dump_dir_2,
      cls._server_thread_2,
@@ -201,8 +202,7 @@ class SendTracebacksTest(test_util.TensorFlowTestCase):
           with self.assertRaises(ValueError):
             self._server.query_source_file_line(tf_trace_file_path, 0)
 
-  # TODO(b/124381153): Re-enable this test when not flaky.
-  def DISABLED_testSendEagerTracebacksToSingleDebugServer(self):
+  def testSendEagerTracebacksToSingleDebugServer(self):
     this_func_name = "testSendEagerTracebacksToSingleDebugServer"
     send_traceback = traceback.extract_stack()
     send_lineno = line_number_above()
