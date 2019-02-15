35c0f45cef65cfc1a4357028eabbf7acb0a03b1b
Lucas Hendren <lhendre2@gmail.com>
2019-02-15 01:37:47 -0500

    Updated test cases

--- tensorflow/python/ops/special_math_ops.py
+++ tensorflow/python/ops/special_math_ops.py
@@ -237,11 +237,10 @@ def einsum(equation, *inputs, **kwargs):
 
       output_axis_labels = ''.join(
           sorted(ax for ax in indices if counts[ax] == 1))
-    print('TEST: ',axis_labels,input_axis_labels,inputs,equation)
     for a in axis_labels:
       for input_labels in input_axis_labels:
         if (len(input_axis_labels) == 1 and input_labels.count(a) == 2
-            and input_labels == input_labels[::-1]):
+            and input_labels == input_labels[::-1] and '->' not in equation):
           return math_ops.trace(inputs[0])
         if input_labels.count(a) > 1:
           raise ValueError(
