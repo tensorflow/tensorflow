c8b9b363d6beb90f089643f5ec8cd31404e23da2
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2019-02-17 14:33:48 -0800

    Add support for cudnn's group convolution.

--- tensorflow/python/kernel_tests/conv_ops_test.py
+++ tensorflow/python/kernel_tests/conv_ops_test.py
@@ -693,6 +693,74 @@ class Conv2DTest(test.TestCase):
         dilations=[2, 3],
         test_grappler_layout_optimizer=True)
 
+  def _VerifyGroupConvFwd(self, tensor_in_sizes, filter_in_sizes, dilations,
+                          strides, padding, data_format, dtype):
+    """
+    Verify the output of group convolution is equal to a
+    for-loop implementation.
+
+    Args:
+      tensor_in_sizes: Input tensor dimensions in
+        [batch, input_rows, input_cols, input_depth].
+      filter_in_sizes: Filter tensor dimensions in
+        [kernel_rows, kernel_cols, input_depth, output_depth].
+      dilations: Dilated rate: [col_dilation, row_dilation]
+      strides: Stride: [col_stride, row_stride]
+      padding: Padding type.
+      data_format: Format of the data tensors.
+      dtype: Data type for inputs and outputs.
+    """
+    tensor_in = self._CreateNumpyTensor(tensor_in_sizes)
+    filter_in = self._CreateNumpyTensor(filter_in_sizes)
+    num_groups = tensor_in_sizes[3] // filter_in_sizes[2]
+    assert num_groups > 1 and \
+        filter_in_sizes[2] * num_groups == tensor_in_sizes[3]
+    with test_util.device(True):
+      t1 = constant_op.constant(tensor_in, dtype=dtype)
+      t2 = constant_op.constant(filter_in, dtype=dtype)
+      strides = [1] + strides + [1]
+      dilations = [1] + dilations + [1]
+      if data_format == "NCHW":
+        t1 = test_util.NHWCToNCHW(t1)
+        strides = test_util.NHWCToNCHW(strides)
+        dilations = test_util.NHWCToNCHW(dilations)
+        t1_splits = array_ops.split(t1, num_groups, axis=1)
+      else:
+        t1_splits = array_ops.split(t1, num_groups, axis=3)
+      t2_splits = array_ops.split(t2, num_groups, axis=3)
+
+      group_conv = nn_ops.conv2d(
+          t1,
+          t2,
+          dilations=dilations,
+          strides=strides,
+          padding=padding,
+          data_format=data_format)
+      group_conv_loop = array_ops.concat(
+          [nn_ops.conv2d(t1s, t2s,
+                         dilations=dilations,
+                         strides=strides,
+                         padding=padding,
+                         data_format=data_format)
+           for t1s, t2s in zip(t1_splits, t2_splits)],
+          axis=1 if data_format == "NCHW" else 3)
+      results = self.evaluate([group_conv, group_conv_loop])
+      tol_to_use = 1e-5
+      self.assertAllClose(results[0], results[1],
+                          atol=tol_to_use, rtol=tol_to_use)
+
+  @test_util.run_in_graph_and_eager_modes
+  @test_util.run_cuda_only
+  def testConv2DGroupConvFwd(self):
+    for data_format in ["NHWC", "NCHW"]:
+      for dilation in [1, 2]:
+        for stride in [1, 2]:
+          self._VerifyGroupConvFwd([10, 32, 32, 16], [3, 3, 4, 8],
+                                   dilations=[dilation, dilation],
+                                   strides=[stride, stride], padding="SAME",
+                                   data_format=data_format,
+                                   dtype=dtypes.float32)
+
   # TODO(yzhwang): this currently fails.
   # self._VerifyValues(tensor_in_sizes=[1, 8, 8, 1],
   #                   filter_in_sizes=[2, 2, 1, 1],
@@ -2274,6 +2342,16 @@ class Conv2DTest(test.TestCase):
               dtypes.float32, shape=[4, 4, 2, 2]),
           strides=[1, 1, 1, 1],
           padding="SAME")
+
+    # Input depth divisible by filter depth (group convolution).
+    # No exceptions should appear.
+    nn_ops.conv2d(
+        array_ops.placeholder(
+            dtypes.float32, shape=[32, 20, 20, 8]),
+        array_ops.placeholder(
+            dtypes.float32, shape=[4, 4, 2, 16]),
+        strides=[1, 1, 1, 1],
+        padding="SAME")
 
     # Negative padding.
     with self.assertRaises(ValueError):
