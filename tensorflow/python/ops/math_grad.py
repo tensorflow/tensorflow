a35c95c3b7479d55467d804d92b6735d9bd5a56f
Yong Tang <yong.tang.github@outlook.com>
2019-02-17 19:39:40 +0000

    Fix Pylint Line too long (83/80) issue
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/math_grad.py
+++ tensorflow/python/ops/math_grad.py
@@ -960,10 +960,11 @@ def _DivGrad(op, grad):
   rx, ry = gen_array_ops.broadcast_gradient_args(sx, sy)
   x = math_ops.conj(x)
   y = math_ops.conj(y)
-  return (array_ops.reshape(math_ops.reduce_sum(math_ops.divide(grad, y), rx), sx),
-          array_ops.reshape(
-              math_ops.reduce_sum(grad * math_ops.divide(math_ops.divide(-x, y), y),
-                                  ry), sy))
+  return (
+      array_ops.reshape(math_ops.reduce_sum(math_ops.divide(grad, y), rx), sx),
+      array_ops.reshape(
+          math_ops.reduce_sum(
+              grad * math_ops.divide(math_ops.divide(-x, y), y), ry), sy))
 
 
 @ops.RegisterGradient("FloorDiv")
