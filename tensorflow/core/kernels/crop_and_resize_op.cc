4b1d5dc8fe5fca2c1ab9ba14e0da77502ff76702
Pan Daoxin <daoxin.pan@intel.com>
2019-02-18 09:33:06 +0800

    Modify calculation costs.

--- tensorflow/core/kernels/crop_and_resize_op.cc
+++ tensorflow/core/kernels/crop_and_resize_op.cc
@@ -537,10 +537,9 @@ struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
              ? depth * (Eigen::TensorOpCost::AddCost<float>() * 4 +
                         Eigen::TensorOpCost::MulCost<float>() * 6 +
                         Eigen::TensorOpCost::CastCost<T, float>() * 4)
+               + Eigen::TensorOpCost::AddCost<float>() * 4
              : depth * (Eigen::TensorOpCost::AddCost<float>() +
-                        Eigen::TensorOpCost::CastCost<T, float>())) +
-        (Eigen::TensorOpCost::AddCost<float>() * 2 +
-         Eigen::TensorOpCost::AddCost<float>() * 3);
+                        Eigen::TensorOpCost::CastCost<T, float>()));
     const double cost_per_box = crop_height * crop_width * cost_per_pixel;
 
     const DeviceBase::CpuWorkerThreads& worker_threads =
