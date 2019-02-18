0edcdb8786c49aeca61dd9927e5883b57cfe8e50
Pan Daoxin <daoxin.pan@intel.com>
2019-02-18 11:16:50 +0800

    Change function splitting method.

--- tensorflow/core/kernels/crop_and_resize_op.cc
+++ tensorflow/core/kernels/crop_and_resize_op.cc
@@ -409,14 +409,9 @@ class CropAndResizeGradImageOp : public AsyncOpKernel {
       const Tensor& grads = context->input(0);
       const Tensor& boxes = context->input(1);
       const Tensor& box_index = context->input(2);
-      const bool status =
-          (method_ == "bilinear")
-              ? functor::CropAndResizeBackpropImage<Device, T, true>()(
+      const bool status = functor::CropAndResizeBackpropImage<Device, T>()(
                     context, grads.tensor<float, 4>(), boxes.tensor<float, 2>(),
-                    box_index.tensor<int32, 1>(), output->tensor<T, 4>())
-              : functor::CropAndResizeBackpropImage<Device, T, false>()(
-                    context, grads.tensor<float, 4>(), boxes.tensor<float, 2>(),
-                    box_index.tensor<int32, 1>(), output->tensor<T, 4>());
+                    box_index.tensor<int32, 1>(), output->tensor<T, 4>(), method_);
 
       if (!status) {
         context->SetStatus(errors::Internal(
@@ -435,15 +430,14 @@ class CropAndResizeGradImageOp : public AsyncOpKernel {
 
 // Partial specialization of CropAndResizeBackpropImage functor for a CPUDevice.
 namespace functor {
-// Split two methods' code to avoid branch mis-prediction.
-// By template isBilinear variable.
-template <typename T, bool isBilinear>
-struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
+template <typename T>
+struct CropAndResizeBackpropImage<CPUDevice, T> {
   bool operator()(const OpKernelContext* context,
                   typename TTypes<float, 4>::ConstTensor grads,
                   typename TTypes<float, 2>::ConstTensor boxes,
                   typename TTypes<int32, 1>::ConstTensor box_index,
-                  typename TTypes<T, 4>::Tensor grads_image) {
+                  typename TTypes<T, 4>::Tensor grads_image,
+                  const string& method_name) {
     const int batch_size = grads_image.dimension(0);
     const int image_height = grads_image.dimension(1);
     const int image_width = grads_image.dimension(2);
@@ -457,7 +451,8 @@ struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
 
     // Parallelize the loop using Eigen thread pool.
     // Assign ranges of boxes into different threads.
-    auto CropAndResizeBackImgPerBox = [&](int start_box, int limit_box) {
+    // Split two methods' code to avoid branch mis-prediction.
+    auto CropAndResizeBackImgPerBoxBilinear = [&](int start_box, int limit_box) {
       for (int b = start_box; b < limit_box; ++b) {
         const float y1 = boxes(b, 0);
         const float x1 = boxes(b, 1);
@@ -493,37 +488,75 @@ struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
               continue;
             }
 
-            if (isBilinear) {
-              const int top_y_index = floorf(in_y);
-              const int bottom_y_index = ceilf(in_y);
-              const float y_lerp = in_y - top_y_index;
-              const float one_y_lerp = 1 - y_lerp;
+            const int top_y_index = floorf(in_y);
+            const int bottom_y_index = ceilf(in_y);
+            const float y_lerp = in_y - top_y_index;
+            const float one_y_lerp = 1 - y_lerp;
 
-              const int left_x_index = floorf(in_x);
-              const int right_x_index = ceilf(in_x);
-              const float x_lerp = in_x - left_x_index;
-              const float one_x_lerp = 1 - x_lerp;
+            const int left_x_index = floorf(in_x);
+            const int right_x_index = ceilf(in_x);
+            const float x_lerp = in_x - left_x_index;
+            const float one_x_lerp = 1 - x_lerp;
 
-              for (int d = 0; d < depth; ++d) {
-                const float dtop = one_y_lerp * grads(b, y, x, d);
-                grads_image(b_in, top_y_index, left_x_index, d) +=
-                    static_cast<T>(one_x_lerp * dtop);
-                grads_image(b_in, top_y_index, right_x_index, d) +=
-                    static_cast<T>(x_lerp * dtop);
-                const float dbottom = y_lerp * grads(b, y, x, d);
-                grads_image(b_in, bottom_y_index, left_x_index, d) +=
-                    static_cast<T>(one_x_lerp * dbottom);
-                grads_image(b_in, bottom_y_index, right_x_index, d) +=
-                    static_cast<T>(x_lerp * dbottom);
-              }
-            } else {  // method_name == "nearest"
-              const int closest_x_index = roundf(in_x);
-              const int closest_y_index = roundf(in_y);
+            for (int d = 0; d < depth; ++d) {
+              const float dtop = one_y_lerp * grads(b, y, x, d);
+              grads_image(b_in, top_y_index, left_x_index, d) +=
+                  static_cast<T>(one_x_lerp * dtop);
+              grads_image(b_in, top_y_index, right_x_index, d) +=
+                  static_cast<T>(x_lerp * dtop);
+              const float dbottom = y_lerp * grads(b, y, x, d);
+              grads_image(b_in, bottom_y_index, left_x_index, d) +=
+                  static_cast<T>(one_x_lerp * dbottom);
+              grads_image(b_in, bottom_y_index, right_x_index, d) +=
+                  static_cast<T>(x_lerp * dbottom);
+            }
+          }
+        }
+      }
+    };
 
-              for (int d = 0; d < depth; ++d) {
-                grads_image(b_in, closest_y_index, closest_x_index, d) +=
-                    static_cast<T>(grads(b, y, x, d));
-              }
+    auto CropAndResizeBackImgPerBoxNearest = [&](int start_box, int limit_box) {
+      for (int b = start_box; b < limit_box; ++b) {
+        const float y1 = boxes(b, 0);
+        const float x1 = boxes(b, 1);
+        const float y2 = boxes(b, 2);
+        const float x2 = boxes(b, 3);
+
+        const int32 b_in = box_index(b);
+        if (!FastBoundsCheck(b_in, batch_size)) {
+          continue;
+        }
+
+        const float height_scale =
+            (crop_height > 1)
+                ? (y2 - y1) * (image_height - 1) / (crop_height - 1)
+                : 0;
+        const float width_scale =
+            (crop_width > 1) ? (x2 - x1) * (image_width - 1) / (crop_width - 1)
+                             : 0;
+
+        for (int y = 0; y < crop_height; ++y) {
+          const float in_y = (crop_height > 1)
+                                 ? y1 * (image_height - 1) + y * height_scale
+                                 : 0.5 * (y1 + y2) * (image_height - 1);
+          if (in_y < 0 || in_y > image_height - 1) {
+            continue;
+          }
+
+          for (int x = 0; x < crop_width; ++x) {
+            const float in_x = (crop_width > 1)
+                                   ? x1 * (image_width - 1) + x * width_scale
+                                   : 0.5 * (x1 + x2) * (image_width - 1);
+            if (in_x < 0 || in_x > image_width - 1) {
+              continue;
+            }
+
+            const int closest_x_index = roundf(in_x);
+            const int closest_y_index = roundf(in_y);
+
+            for (int d = 0; d < depth; ++d) {
+              grads_image(b_in, closest_y_index, closest_x_index, d) +=
+                  static_cast<T>(grads(b, y, x, d));
             }
           }
         }
@@ -533,7 +566,7 @@ struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
     // A rough estimation of the cost for each cropped box.
     // Including calculation cost in the depth loop and pixel loop.
     const double cost_per_pixel =
-        (isBilinear
+        (method_name == "bilinear"
              ? depth * (Eigen::TensorOpCost::AddCost<float>() * 4 +
                         Eigen::TensorOpCost::MulCost<float>() * 6 +
                         Eigen::TensorOpCost::CastCost<T, float>() * 4)
@@ -544,8 +577,12 @@ struct CropAndResizeBackpropImage<CPUDevice, T, isBilinear> {
 
     const DeviceBase::CpuWorkerThreads& worker_threads =
         *(context->device()->tensorflow_cpu_worker_threads());
+    method_name == "bilinear" ?
     Shard(worker_threads.num_threads, worker_threads.workers, num_boxes,
-          cost_per_box, CropAndResizeBackImgPerBox);
+          cost_per_box, CropAndResizeBackImgPerBoxBilinear)
+    : Shard(worker_threads.num_threads, worker_threads.workers, num_boxes,
+          cost_per_box, CropAndResizeBackImgPerBoxNearest);
+
     return true;
   }
 };
