2fef4af30171a83faa7f5782f781703d2ae0ccac
Aurelien Geron <aurelien.geron@gmail.com>
2019-02-21 09:30:21 +0800

    Make deserialize_keras_object() support lists, and use losses.deserialize() and metrics.deserialize() to load losses and metrics, instead of convert_custom_objects(). Fixes #25938

--- tensorflow/python/keras/saving/hdf5_format.py
+++ tensorflow/python/keras/saving/hdf5_format.py
@@ -27,6 +27,8 @@ from six.moves import zip  # pylint: disable=redefined-builtin
 
 from tensorflow.python.framework import ops
 from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import metrics as metrics_lib
 from tensorflow.python.keras import optimizers
 from tensorflow.python.keras.saving import model_config as model_config_lib
 from tensorflow.python.keras.utils import conv_utils
@@ -174,31 +176,6 @@ def load_model(filepath, custom_objects=None, compile=True):  # pylint: disable=
   if not custom_objects:
     custom_objects = {}
 
-  def convert_custom_objects(obj):
-    """Handles custom object lookup.
-
-    Arguments:
-        obj: object, dict, or list.
-
-    Returns:
-        The same structure, where occurrences
-            of a custom object name have been replaced
-            with the custom object.
-    """
-    if isinstance(obj, list):
-      deserialized = []
-      for value in obj:
-        deserialized.append(convert_custom_objects(value))
-      return deserialized
-    if isinstance(obj, dict):
-      deserialized = {}
-      for key, value in obj.items():
-        deserialized[key] = convert_custom_objects(value)
-      return deserialized
-    if obj in custom_objects:
-      return custom_objects[obj]
-    return obj
-
   opened_new_file = not isinstance(filepath, h5py.File)
   if opened_new_file:
     f = h5py.File(filepath, mode='r')
@@ -231,10 +208,13 @@ def load_model(filepath, custom_objects=None, compile=True):  # pylint: disable=
           optimizer_config, custom_objects=custom_objects)
 
       # Recover loss functions and metrics.
-      loss = convert_custom_objects(training_config['loss'])
-      metrics = convert_custom_objects(training_config['metrics'])
-      weighted_metrics = convert_custom_objects(
-          training_config.get('weighted_metrics', None))
+      loss = losses.deserialize(
+          training_config['loss'], custom_objects=custom_objects)
+      metrics = metrics_lib.deserialize(
+          training_config['metrics'], custom_objects=custom_objects)
+      weighted_metrics = metrics_lib.deserialize(
+          training_config.get('weighted_metrics', None),
+          custom_objects=custom_objects)
       sample_weight_mode = training_config['sample_weight_mode']
       loss_weights = training_config['loss_weights']
 
