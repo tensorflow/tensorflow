6aa83398ab03bfae822f36772757097bcb98b6ed
Justin Lebar <jlebar@google.com>
2019-02-23 16:51:42 -0800

    [SE] Fix function signature of stream_executor::CalculateDimensionality.
    PiperOrigin-RevId: 235369844

--- tensorflow/stream_executor/device_description.cc
+++ tensorflow/stream_executor/device_description.cc
@@ -148,8 +148,8 @@ uint64 DivideCeil(uint64 x, uint64 y) {
 }
 
 void CalculateDimensionality(const DeviceDescription &device_description,
-                             uint64 element_count, uint64 *threads_per_block,
-                             uint64 *block_count) {
+                             int64 element_count, int64 *threads_per_block,
+                             int64 *block_count) {
   *threads_per_block = device_description.threads_per_block_limit();
   *block_count = port::MathUtil::CeilOfRatio(element_count, *threads_per_block);
   if (*block_count == 1) {
