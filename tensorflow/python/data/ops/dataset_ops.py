d25c2c5dd8ebaa016b19bab1d80f758ae9ca26af
Mark Daoust <markdaoust@google.com>
2019-02-25 09:46:35 -0800

    Reworded for clarity.

--- tensorflow/python/data/ops/dataset_ops.py
+++ tensorflow/python/data/ops/dataset_ops.py
@@ -699,8 +699,11 @@ class DatasetV2(tracking_base.Trackable, composite_tensor.CompositeTensor):
   def prefetch(self, buffer_size):
     """Creates a `Dataset` that prefetches elements from this dataset.
 
-    NOTE: If prefetch after batch, such as: dataset.batch(20).prefetch(2),
-    which means prefetch 2 batches, totally 40 samples.
+    Note: Like other `Dataset` methods, prefetch operates on the elements of
+    the dataset. It has no concept of examples vs. batches. `examples.prefetch(2)`
+    will prefetch two elements (2 examples), while `examples.batch(20).prefetch(2)`
+    will prefetch 2 elements (2 batches, of 20 examples each). The typical
+    use-case is to `prefetch` batches.
 
     Args:
       buffer_size: A `tf.int64` scalar `tf.Tensor`, representing the maximum
