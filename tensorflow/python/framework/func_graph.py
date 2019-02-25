63b2013884a46c6e95ecf8a8e1c6852bf2f336b0
Alexandre Passos <apassos@google.com>
2019-02-25 15:46:20 -0800

    Supporting external control dependencies in func_graph, defun, and control flow v2.
    PiperOrigin-RevId: 235617033

--- tensorflow/python/framework/func_graph.py
+++ tensorflow/python/framework/func_graph.py
@@ -125,6 +125,8 @@ class FuncGraph(ops.Graph):
       inputs coming first.
     outputs: Tensors that will be returned by this function. The tensors are in
       this FuncGraph.
+    control_outputs: Operations that must be executed before the function
+      represented by this graph can be said to have been executed.
     structured_input_signature: A tuple of (args, kwargs), which are both
       possibly-nested python objects that were received by this function. Note
       that these structures might contain Python `None`s.
@@ -136,6 +138,8 @@ class FuncGraph(ops.Graph):
       or the global default Graph.
     captures: Maps external tensor -> internal tensor (i.e. input placeholder).
       The entries are in the order they were captured.
+    control_captures: Set of external ops on which this graph has a control
+      dependency.
     seed: The graph-level random seed.
     capture_by_value: If True, the func graph will capture Variables by value
       instead of reference.
@@ -166,6 +170,7 @@ class FuncGraph(ops.Graph):
     self.inputs = []
     self.outputs = []
     self.control_outputs = []
+    self.control_captures = set()
     self.structured_input_signature = None
     self.structured_outputs = None
     self._weak_variables = []
@@ -213,6 +218,47 @@ class FuncGraph(ops.Graph):
 
   def __str__(self):
     return "FuncGraph(name=%s, id=%s)" % (self.name, id(self))
+
+  def control_dependencies(self, control_inputs):
+    """Handles control dependencies.
+
+    FuncGraph wraps Graph's control_dependencies logic by first filtering out
+    any external tensors / operations and storing them in the graph's
+    control_captures member. Any consumers of this function graph must then
+    decide how to handle the control captures.
+
+    Args:
+      control_inputs: A list of `Operation` or `Tensor` objects which
+        must be executed or computed before running the operations
+        defined in the context.  Can also be `None` to clear the control
+        dependencies.
+
+    Returns:
+     A context manager that specifies control dependencies for all
+     operations constructed within the context.
+
+    Raises:
+      TypeError: If `control_inputs` is not a list of `Operation` or
+        `Tensor` objects.
+    """
+    if control_inputs is None:
+      return super(FuncGraph, self).control_dependencies(control_inputs)
+
+    filtered_control_inputs = []
+    for c in control_inputs:
+      # Check for _UnreadVariable
+      if (isinstance(c, ops.IndexedSlices) or
+          (hasattr(c, "_handle") and hasattr(c, "op"))):
+        c = c.op
+      graph_element = ops._as_graph_element(c)  # pylint: disable=protected-access
+      if graph_element is None:
+        graph_element = c
+      if graph_element is not None and getattr(
+          graph_element, "graph", None) is not self:
+        self.control_captures.add(graph_element)
+      else:
+        filtered_control_inputs.append(graph_element)
+    return super(FuncGraph, self).control_dependencies(filtered_control_inputs)
 
   def as_default(self):
     outer_cm = super(FuncGraph, self).as_default()
