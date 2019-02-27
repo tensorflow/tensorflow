bc174b5505f649fd93870a640eed12069b0ccb78
A. Unique TensorFlower <gardener@tensorflow.org>
2019-02-27 08:23:18 -0800

    Add support for mixed quantization parameters in NN API delegate for Concatenation.
    PiperOrigin-RevId: 235923098

--- tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
@@ -696,9 +696,10 @@ class NNAPIDelegateKernel {
         if (version == 1 &&
             reinterpret_cast<TfLiteConcatenationParams*>(node->builtin_data)
                     ->activation == kTfLiteActNone) {
-          if (context->tensors[node->inputs->data[0]].type == kTfLiteUInt8) {
-            // NNAPI only support concatenating quantized tensor of the same
-            // scale and offset.
+          if (context->tensors[node->inputs->data[0]].type == kTfLiteUInt8 &&
+              android_sdk_version < kMinSdkVersionForNNAPI12) {
+            // NNAPI 1.0-1 only supported concatenating quantized tensor of the
+            // same scale and offset.
             auto first_param = context->tensors[node->inputs->data[0]].params;
             for (int i = 1; i < node->inputs->size; i++) {
               auto curr_param = context->tensors[node->inputs->data[i]].params;
