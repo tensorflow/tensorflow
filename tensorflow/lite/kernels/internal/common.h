d9d5ab289d08da9b3fa0a4feccf8dfa87a7b669e
Pete Warden <petewarden@google.com>
2019-02-27 14:03:00 -0800

    Add command response mechanism
    PiperOrigin-RevId: 235987570

--- tensorflow/lite/kernels/internal/common.h
+++ tensorflow/lite/kernels/internal/common.h
@@ -169,7 +169,9 @@ inline std::int16_t SaturatingSub(std::int16_t a, std::int16_t b) {
   std::int32_t a32 = a;
   std::int32_t b32 = b;
   std::int32_t diff = a32 - b32;
-  return static_cast<std::int16_t>(std::min(32767, std::max(-32768, diff)));
+  return static_cast<std::int16_t>(
+      std::min(static_cast<int32_t>(32767),
+               std::max(static_cast<int32_t>(-32768), diff)));
 }
 
 template <>
