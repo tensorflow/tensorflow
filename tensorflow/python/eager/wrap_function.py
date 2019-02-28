d39f5bfe02a6bf95d4144e5548ba074466c7fd48
Allen Lavoie <allenl@google.com>
2019-02-27 16:20:50 -0800

    Add a utility to create a function from a GraphDef
    PiperOrigin-RevId: 236014680

--- tensorflow/python/eager/wrap_function.py
+++ tensorflow/python/eager/wrap_function.py
@@ -25,6 +25,7 @@ from tensorflow.python.eager import def_function
 from tensorflow.python.eager import function
 from tensorflow.python.eager import lift_to_graph
 from tensorflow.python.framework import func_graph
+from tensorflow.python.framework import importer
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import resource_variable_ops
@@ -256,3 +257,26 @@ def wrap_function(fn, signature, name=None):
           collections={}),
       variable_holder=holder,
       signature=signature)
+
+
+def function_from_graph_def(graph_def, inputs, outputs):
+  """Creates a ConcreteFunction from a GraphDef.
+
+  Args:
+    graph_def: A GraphDef to make a function out of.
+    inputs: A Tensor name or nested structure of names in `graph_def` which
+      should be inputs to the function.
+    outputs: A Tensor name or nested structure of names in `graph_def` which
+      should be outputs of the function.
+
+  Returns:
+    A ConcreteFunction.
+  """
+  def _imports_graph_def():
+    importer.import_graph_def(graph_def, name="")
+
+  wrapped_import = wrap_function(_imports_graph_def, [])
+  import_graph = wrapped_import.graph
+  return wrapped_import.prune(
+      nest.map_structure(import_graph.as_graph_element, inputs),
+      nest.map_structure(import_graph.as_graph_element, outputs))
