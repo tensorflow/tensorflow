34853c4204090b8e32f417ca05148e88667f31b0
Mark Daoust <markdaoust@google.com>
2019-02-28 10:59:10 -0800

    Prevent validation `tf.data.Datasets` from overwriting the progress bar target.
    Fixes this:
    
    ```
    history = model.fit(
        train_batches.take(10),
        validation_data=validation_batches.take(5),
        epochs=EPOCHS)
    ```
    
    ```
    
    Epoch 1/15
    10/10==============================] - 62s 6s/step - loss: 0.6939 - acc: 0.4844 - val_loss: 0.0000e+00 - val_acc: 0.0000e+00
    Epoch 2/15
    10/5============================================================] - 58s 6s/step - loss: 0.6921 - acc: 0.5156 - val_loss: 0.6919 - val_acc: 0.5250
    ```
    
    PiperOrigin-RevId: 236154811

--- tensorflow/python/keras/engine/training_dataset_test.py
+++ tensorflow/python/keras/engine/training_dataset_test.py
@@ -19,8 +19,10 @@ from __future__ import division
 from __future__ import print_function
 
 import logging
+import sys
 
 import numpy as np
+import six
 
 from tensorflow.python import keras
 from tensorflow.python.data.experimental.ops import cardinality
@@ -428,6 +430,54 @@ class TestTrainingWithDataset(keras_parameterized.TestCase):
 
     batch_counter = BatchCounterCallback()
     history = model.fit(dataset, epochs=2, verbose=1, callbacks=[batch_counter])
+
+    self.assertLen(history.history['loss'], 2)
+    self.assertEqual(batch_counter.batch_count, 20)
+    model.evaluate(dataset)
+    out = model.predict(dataset)
+    self.assertEqual(out.shape[0], 100)
+
+  @keras_parameterized.run_with_all_model_types
+  @keras_parameterized.run_all_keras_modes(always_skip_v1=True)
+  def test_finite_dataset_unknown_cardinality_no_step_with_train_and_val(self):
+
+    class CaptureStdout(object):
+
+      def __enter__(self):
+        self._stdout = sys.stdout
+        string_io = six.StringIO()
+        sys.stdout = string_io
+        self._stringio = string_io
+        return self
+
+      def __exit__(self, *args):
+        self.output = self._stringio.getvalue()
+        sys.stdout = self._stdout
+
+    model = testing_utils.get_small_mlp(1, 4, input_dim=3)
+    model.compile(
+        'rmsprop', 'mse', run_eagerly=testing_utils.should_run_eagerly())
+
+    inputs = np.zeros((100, 3), dtype=np.float32)
+    targets = np.random.randint(0, 4, size=100, dtype=np.int32)
+    dataset = dataset_ops.Dataset.from_tensor_slices((inputs, targets))
+    dataset = dataset.filter(lambda x, y: True).batch(10)
+    self.assertEqual(
+        keras.backend.get_value(cardinality.cardinality(dataset)),
+        cardinality.UNKNOWN)
+
+    batch_counter = BatchCounterCallback()
+    with CaptureStdout() as capture:
+      history = model.fit(
+          dataset,
+          epochs=2,
+          callbacks=[batch_counter],
+          validation_data=dataset.take(3))
+
+    lines = capture.output.splitlines()
+
+    self.assertIn('1/Unknown', lines[2])
+    self.assertIn('10/10', lines[-1])
 
     self.assertLen(history.history['loss'], 2)
     self.assertEqual(batch_counter.batch_count, 20)
