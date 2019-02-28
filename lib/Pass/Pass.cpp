bf00f8a23828be3bee5b815970e24fc569a331fd
River Riddle <riverriddle@google.com>
2019-02-27 17:49:51 -0800

    Move the PassExecutor and ModuleToFunctionPassAdaptor classes from PassManager.h to Pass.cpp. This allows for us to remove a dependency on Pass.h from PassManager.h.
    PiperOrigin-RevId: 236029339

--- lib/Pass/Pass.cpp
+++ lib/Pass/Pass.cpp
@@ -24,6 +24,11 @@
 #include "mlir/Pass/PassManager.h"
 
 using namespace mlir;
+using namespace mlir::detail;
+
+//===----------------------------------------------------------------------===//
+// Pass
+//===----------------------------------------------------------------------===//
 
 /// Out of line virtual method to ensure vtables and metadata are emitted to a
 /// single .o file.
@@ -46,6 +51,79 @@ PassResult ModulePassBase::run(Module *module) {
   /// Invoke the virtual runOnModule function.
   return runOnModule();
 }
+
+//===----------------------------------------------------------------------===//
+// PassExecutor
+//===----------------------------------------------------------------------===//
+
+namespace mlir {
+namespace detail {
+/// The abstract base pass executor class.
+class PassExecutor {
+public:
+  enum Kind { FunctionExecutor, ModuleExecutor };
+  explicit PassExecutor(Kind kind) : kind(kind) {}
+
+  /// Get the kind of this executor.
+  Kind getKind() const { return kind; }
+
+private:
+  /// The kind of executor this object is.
+  Kind kind;
+};
+
+/// A pass executor that contains a list of passes over a function.
+class FunctionPassExecutor : public PassExecutor {
+public:
+  FunctionPassExecutor() : PassExecutor(Kind::FunctionExecutor) {}
+  FunctionPassExecutor(FunctionPassExecutor &&) = default;
+
+  // TODO(riverriddle) Allow copying.
+  FunctionPassExecutor(const FunctionPassExecutor &) = delete;
+  FunctionPassExecutor &operator=(const FunctionPassExecutor &) = delete;
+
+  /// Run the executor on the given function.
+  bool run(Function *function);
+
+  /// Add a pass to the current executor. This takes ownership over the provided
+  /// pass pointer.
+  void addPass(FunctionPassBase *pass) { passes.emplace_back(pass); }
+
+  static bool classof(const PassExecutor *pe) {
+    return pe->getKind() == Kind::FunctionExecutor;
+  }
+
+private:
+  std::vector<std::unique_ptr<FunctionPassBase>> passes;
+};
+
+/// A pass executor that contains a list of passes over a module unit.
+class ModulePassExecutor : public PassExecutor {
+public:
+  ModulePassExecutor() : PassExecutor(Kind::ModuleExecutor) {}
+  ModulePassExecutor(ModulePassExecutor &&) = default;
+
+  // Don't allow copying.
+  ModulePassExecutor(const ModulePassExecutor &) = delete;
+  ModulePassExecutor &operator=(const ModulePassExecutor &) = delete;
+
+  /// Run the executor on the given module.
+  bool run(Module *module);
+
+  /// Add a pass to the current executor. This takes ownership over the provided
+  /// pass pointer.
+  void addPass(ModulePassBase *pass) { passes.emplace_back(pass); }
+
+  static bool classof(const PassExecutor *pe) {
+    return pe->getKind() == Kind::ModuleExecutor;
+  }
+
+private:
+  /// Set of passes to run on the given module.
+  std::vector<std::unique_ptr<ModulePassBase>> passes;
+};
+} // end namespace detail
+} // end namespace mlir
 
 /// Run all of the passes in this manager over the current function.
 bool detail::FunctionPassExecutor::run(Function *function) {
@@ -72,9 +150,39 @@ bool detail::ModulePassExecutor::run(Module *module) {
   return false;
 }
 
+//===----------------------------------------------------------------------===//
+// ModuleToFunctionPassAdaptor
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// An adaptor module pass used to run function passes over all of the
+/// non-external functions of a module.
+class ModuleToFunctionPassAdaptor
+    : public ModulePass<ModuleToFunctionPassAdaptor> {
+public:
+  ModuleToFunctionPassAdaptor() = default;
+  ModuleToFunctionPassAdaptor(ModuleToFunctionPassAdaptor &&) = default;
+
+  // TODO(riverriddle) Allow copying.
+  ModuleToFunctionPassAdaptor(const ModuleToFunctionPassAdaptor &) = delete;
+  ModuleToFunctionPassAdaptor &
+  operator=(const ModuleToFunctionPassAdaptor &) = delete;
+
+  /// run the held function pipeline over all non-external functions within the
+  /// module.
+  PassResult runOnModule() override;
+
+  /// Returns the function pass executor for this adaptor.
+  FunctionPassExecutor &getFunctionExecutor() { return fpe; }
+
+private:
+  FunctionPassExecutor fpe;
+};
+} // end anonymous namespace
+
 /// Execute the held function pass over all non-external functions within the
 /// module.
-PassResult detail::ModuleToFunctionPassAdaptor::runOnModule() {
+PassResult ModuleToFunctionPassAdaptor::runOnModule() {
   for (auto &func : getModule()) {
     // Skip external functions.
     if (func.isExternal())
@@ -86,3 +194,53 @@ PassResult detail::ModuleToFunctionPassAdaptor::runOnModule() {
   }
   return success();
 }
+
+//===----------------------------------------------------------------------===//
+// PassManager
+//===----------------------------------------------------------------------===//
+
+PassManager::PassManager() : mpe(new ModulePassExecutor()) {}
+
+PassManager::~PassManager() {}
+
+/// Add an opaque pass pointer to the current manager. This takes ownership
+/// over the provided pass pointer.
+void PassManager::addPass(Pass *pass) {
+  switch (pass->getKind()) {
+  case Pass::Kind::FunctionPass:
+    addPass(cast<FunctionPassBase>(pass));
+    break;
+  case Pass::Kind::ModulePass:
+    addPass(cast<ModulePassBase>(pass));
+    break;
+  }
+}
+
+/// Add a module pass to the current manager. This takes ownership over the
+/// provided pass pointer.
+void PassManager::addPass(ModulePassBase *pass) {
+  nestedExecutorStack.clear();
+  mpe->addPass(pass);
+}
+
+/// Add a function pass to the current manager. This takes ownership over the
+/// provided pass pointer. This will automatically create a function pass
+/// executor if necessary.
+void PassManager::addPass(FunctionPassBase *pass) {
+  detail::FunctionPassExecutor *fpe;
+  if (nestedExecutorStack.empty()) {
+    /// Create an executor adaptor for this pass.
+    auto *adaptor = new ModuleToFunctionPassAdaptor();
+    mpe->addPass(adaptor);
+
+    /// Add the executor to the stack.
+    fpe = &adaptor->getFunctionExecutor();
+    nestedExecutorStack.push_back(fpe);
+  } else {
+    fpe = cast<detail::FunctionPassExecutor>(nestedExecutorStack.back());
+  }
+  fpe->addPass(pass);
+}
+
+/// Run the passes within this manager on the provided module.
+bool PassManager::run(Module *module) { return mpe->run(module); }
