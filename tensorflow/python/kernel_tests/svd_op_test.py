Fix SVD gradient bug on batches of matrices
Fix a newly introduced bug in dealing with a gradient of SVD applied to a batch of matrices.
s is a batch of vectors, not matrices, so we should use shape[:-1] to get the batch shape.

PiperOrigin-RevId: 236134183

tensorflow/python/kernel_tests/svd_op_test.py
@@ -330,7 +330,7 @@ if __name__ == "__main__":
       dtypes = ([np.float32, np.float64]
                 + [np.complex64, np.complex128] * (not compute_uv))
       for dtype in dtypes:
-        mat_shapes = [(10, 11), (11, 10), (11, 11)]
+        mat_shapes = [(10, 11), (11, 10), (11, 11), (2, 2, 2, 3)]
         if not full_matrices or not compute_uv:
           mat_shapes += [(5, 11), (11, 5)]
         for mat_shape in mat_shapes: