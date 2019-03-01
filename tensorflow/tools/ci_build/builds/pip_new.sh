df8f1ab92a9e70d75251b48ed9b4021c690788bc
A. Unique TensorFlower <gardener@tensorflow.org>
2019-03-01 11:02:11 -0800

    Quick test on python version retrieval.
    PiperOrigin-RevId: 236341880

--- tensorflow/tools/ci_build/builds/pip_new.sh
+++ tensorflow/tools/ci_build/builds/pip_new.sh
@@ -209,6 +209,11 @@ if [[ -z "$PYTHON_BIN_PATH" ]]; then
 fi
 # Get python version for configuring pip later in installation.
 PYTHON_VER_CFG=$(${PYTHON_BIN_PATH} -V 2>&1 | awk '{print $NF}' | cut -d. -f-2)
+if [[ $OS_TYPE == "macos" ]]; then
+  echo "PYTHON_VER_CFG (-V): $(${PYTHON_BIN_PATH} -V 2>&1)"
+  echo "PYTHON_VER_CFG (--version): $(${PYTHON_BIN_PATH} --version 2>&1)"
+  PYTHON_VER_CFG=$(${PYTHON_BIN_PATH} --version 2>&1 | awk '{print $NF}' | cut -d. -f-2)
+fi
 echo "PYTHON_BIN_PATH: ${PYTHON_BIN_PATH} (version: ${PYTHON_VER_CFG})"
 
 # Default values for optional global variables in case they are not user
