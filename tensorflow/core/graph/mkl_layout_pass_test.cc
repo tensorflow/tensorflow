07f6b58e642ff943d963b380094ac7e29a151435
Siju <siju.samuel@huawei.com>
2019-03-01 09:39:03 +0530

    Update mkl_layout_pass_test.cc

--- tensorflow/core/graph/mkl_layout_pass_test.cc
+++ tensorflow/core/graph/mkl_layout_pass_test.cc
@@ -572,7 +572,7 @@ TEST_F(MklLayoutPassTest, Input_ControlEdge_PadWithConv2D_Positive) {
 // Test if output control edges does not duplicate after merge.
 // If both the merging ops have output control edge to a common op,
 // then after merge, the merged op will have only one control edge
-// to that commom op.
+// to that common op.
 // padding is VALID type
 // A = input(image), B = input(paddings), C= Pad = input of conv2D,
 // D=input(filter), E = Conv2D, Z = Zeta
@@ -1501,7 +1501,7 @@ TEST_F(MklLayoutPassTest, Input_ControlEdge_PadWithFusedConv2D_Positive) {
 // ts that there are no duplicate output control edges after merge.
 // If both the merging ops have output control edge to a common op,
 // then after merge, the merged op will have only one control edge
-// to that commom op. This test only add additional output control edge check
+// to that common op. This test only add additional output control edge check
 // based on the previous test NodeMerge_PadWithFusedConv2D_Positive1
 // padding is VALID type
 // A = input(image), B = input(paddings), C = Pad(A, B) = input of conv2D,
