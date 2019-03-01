5f2f5fa7f48c2792e6e4bef535a133464f6030c9
Sanjoy Das <sanjoy@google.com>
2019-02-28 16:32:05 -0800

    Reapply "[TF:XLA] Create XLA clusters with heterogeneous device assignment"
    Fixed an internal timeout by adding an optimization that avoids creating
    clusters that would have never made it to XLA.
    
    PiperOrigin-RevId: 236220238

--- tensorflow/python/kernel_tests/zero_division_test.py
+++ tensorflow/python/kernel_tests/zero_division_test.py
@@ -51,8 +51,10 @@ class ZeroDivisionTest(test.TestCase):
             # means 32 bits set, so we allow 0xffffffff as well.  This isn't
             # very portable, so we may need to expand this list if other GPUs
             # do different things.
+            #
+            # XLA constant folds integer division by zero to 1.
             self.assertTrue(test.is_gpu_available())
-            self.assertIn(result, (-1, 0xff, 0xffffffff))
+            self.assertIn(result, (-1, 1, 0xff, 0xffffffff))
 
 
 if __name__ == '__main__':
