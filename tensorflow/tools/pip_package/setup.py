622976e9835cf87fb12ef07162cb71f562d8d010
Mihai Maruseac <mihaimaruseac@google.com>
2019-03-07 06:57:37 -0800

    Virtual pip package for tensorflow and tensorflow_core.
    PiperOrigin-RevId: 237239663

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -134,8 +134,8 @@ class InstallCommand(InstallCommandBase):
 
   def finalize_options(self):
     ret = InstallCommandBase.finalize_options(self)
-    self.install_headers = os.path.join(self.install_purelib,
-                                        'tensorflow', 'include')
+    self.install_headers = os.path.join(self.install_purelib, 'tensorflow_core',
+                                        'include')
     return ret
 
 
@@ -172,14 +172,14 @@ class InstallHeaders(Command):
     # directories for -I
     install_dir = re.sub('/google/protobuf_archive/src', '', install_dir)
 
-    # Copy external code headers into tensorflow/include.
+    # Copy external code headers into tensorflow_core/include.
     # A symlink would do, but the wheel file that gets created ignores
     # symlink within the directory hierarchy.
     # NOTE(keveman): Figure out how to customize bdist_wheel package so
     # we can do the symlink.
     external_header_locations = [
-        'tensorflow/include/external/eigen_archive/',
-        'tensorflow/include/external/com_google_absl/',
+        'tensorflow_core/include/external/eigen_archive/',
+        'tensorflow_core/include/external/com_google_absl/',
     ]
     for location in external_header_locations:
       if location in install_dir:
