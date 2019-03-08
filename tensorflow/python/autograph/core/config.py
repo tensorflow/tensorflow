61f5a61d25f2d8ef422f14331584fb49af97e493
Smit Hinsu <hinsu@google.com>
2019-03-07 18:11:23 -0800

    Automated rollback of commit fc85579a2b4b689aa9c56d0434c3433479750d23
    PiperOrigin-RevId: 237367210

--- tensorflow/python/autograph/core/config.py
+++ tensorflow/python/autograph/core/config.py
@@ -37,11 +37,7 @@ def _internal_name(name):
   # If the TF module is foo.tensorflow, then all other modules
   # are then assumed to be prefixed by 'foo'.
 
-  # TODO(mihaimaruseac): Fix this hack once estimator move is finished
-  core_reference_root = 'tensorflow_core.'
-
-  if reference_name.startswith(reference_root) or \
-      reference_name.startswith(core_reference_root):
+  if reference_name.startswith(reference_root):
     return name
 
   reference_begin = reference_name.find('.' + reference_root)
