9b93b43909c102eafa9fc4a37565f11db4f921d0
Nathan Luehr <nluehr@nvidia.com>
2019-03-08 11:09:13 -0600

    Remove cuGraph APIs from cuda_stub

--- tensorflow/stream_executor/cuda/cuda_stub.cc
+++ tensorflow/stream_executor/cuda/cuda_stub.cc
@@ -93,11 +93,7 @@ typedef struct CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS_st
     CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS;
 typedef void(CUDA_CB* CUhostFn)(void* userData);
 
-// We only need one stub implementation. Calling a function that is not yet
-// available in the given CUDA version will return
-// CUDA_ERROR_SHARED_OBJECT_INIT_FAILED.
-#if CUDA_VERSION < 10010
-#include "tensorflow/stream_executor/cuda/cuda_10_0.inc"
-#else
-#include "tensorflow/stream_executor/cuda/cuda_10_1.inc"
-#endif
+// For now only one stub implementation is needed.
+// Calling a function that is not yet available in the given CUDA version will
+// return CUDA_ERROR_SHARED_OBJECT_INIT_FAILED.
+#include "tensorflow/stream_executor/cuda/cuda_10_X.inc"
