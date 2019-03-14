fb58b872957a7411679cdfe0deefab68104c9de7
Haoliang Zhang <haoliang@google.com>
2019-03-14 08:33:16 -0700

    In tensor_name function, don't drop the suffix behind colon if it's not the first tensor. For example, it should make sure: tensor:0 -> tensor tensor:1 -> tensor:1 tensor:2 -> tensor:2
    PiperOrigin-RevId: 238447941

--- tensorflow/lite/python/convert.py
+++ tensorflow/lite/python/convert.py
@@ -214,7 +214,17 @@ def toco_convert_protos(model_flags_str, toco_flags_str, input_data_str):
 
 
 def tensor_name(x):
-  return x.name.split(":")[0]
+  """Returns name of the input tensor."""
+  parts = x.name.split(":")
+  if len(parts) > 2:
+    raise ValueError("Tensor name invalid. Expect 0 or 1 colon, got {0}".format(
+        len(parts) - 1))
+
+  # To be consistent with the tensor naming scheme in tensorflow, we need
+  # drop the ':0' suffix for the first tensor.
+  if len(parts) > 1 and parts[1] != "0":
+    return x.name
+  return parts[0]
 
 
 # Don't expose these for now.
