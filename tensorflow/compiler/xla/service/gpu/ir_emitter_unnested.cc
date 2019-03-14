a4512cfbdebc06c7a41980918fbfd9564d9d6d62
George Karpenkov <cheshire@google.com>
2019-03-14 14:45:22 -0700

    NFC: Change all helper functions in `tuple_ops` not to require `llvm::Module` argument, as it can be derived from the already provided `llvm::IRBuilder`. This makes functions easier to code and the calls less verbose, and additionally enables making calls from helper classes where `llvm::Module*` might not be available. PiperOrigin-RevId: 238523747

--- tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
+++ tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
@@ -2201,7 +2201,7 @@ Status IrEmitterUnnested::EmitTargetElementLoopInThunk(
   // kernel *anyway*.
   std::vector<IrArray> output_arrays = ConstructIrArrayForOutputs(hlo);
   KernelSupportLibrary{&b_}.If("emit_mof_tuple", IsBlock0Thread0(&b_), [&] {
-    llvm_ir::EmitTuple(GetIrArray(hlo, hlo), output_arrays, &b_, module_);
+    llvm_ir::EmitTuple(GetIrArray(hlo, hlo), output_arrays, &b_);
   });
 
   // For multioutput fusion, we need to emit each operand and the root.
@@ -3103,8 +3103,7 @@ LaunchDimensions IrEmitterUnnested::EmitKernel(
   if (!reduction_info && unnested_hlo->IsMultiOutputFusion()) {
     KernelSupportLibrary{&b_}.If("emit_mof_tuple", IsBlock0Thread0(&b_), [&] {
       llvm_ir::EmitTuple(GetIrArray(*unnested_hlo, *unnested_hlo),
-                         ConstructIrArrayForOutputs(*unnested_hlo), &b_,
-                         module_);
+                         ConstructIrArrayForOutputs(*unnested_hlo), &b_);
     });
   }
 
