65f25df5d4fbc64ab887339e7a0a74e35e255753
Yong Tang <yong.tang.github@outlook.com>
2019-03-15 21:34:44 +0000

    Add the reference of lookup.TextFileIndexer to lookup.TextFileInitializer
    based on review feedback.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/lookup_ops.py
+++ tensorflow/python/ops/lookup_ops.py
@@ -423,6 +423,8 @@ class KeyValueTensorInitializer(TableInitializerBase):
 class TextFileIndex(object):
   """The key and value content to get from each line.
 
+  This class defines the key and value used for tf.lookup.TextFileInitializer.
+
   The key and value content to get from each line is specified either
   by the following, or a value `>=0`.
   * `TextFileIndex.LINE_NUMBER` means use the line number starting from zero,
