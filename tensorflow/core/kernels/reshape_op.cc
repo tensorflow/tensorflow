475f0c317ffbfccccf69503d38001fbb6af26969
Jeffrey Poznanovic <Jeffrey.Poznanovic@amd.com>
2019-03-15 20:23:37 +0000

    Add ROCm support for the reshape op

--- tensorflow/core/kernels/reshape_op.cc
+++ tensorflow/core/kernels/reshape_op.cc
@@ -86,7 +86,7 @@ REGISTER_KERNEL_BUILDER(Name("Reshape")
 #undef REGISTER_SYCL_KERNEL
 #endif  // TENSORFLOW_USE_SYCL
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 // A special GPU kernel for int32.
 // TODO(b/25387198): Also enable int32 in device memory. This kernel
 // registration requires all int32 inputs and outputs to be in host memory.
