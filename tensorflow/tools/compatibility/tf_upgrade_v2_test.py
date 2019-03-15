25b44710c52fcf158eeecf52cc3d8d5cac0dc9ae
Allen Lavoie <allenl@google.com>
2019-03-15 14:51:53 -0700

    Remove tf.train.sdca_* from the v2 API. They're already in tf.raw_ops.Sdca*.
    I believe they were only kept in tf.train since they weren't exposed elsewhere.
    
    PiperOrigin-RevId: 238715335

--- tensorflow/tools/compatibility/tf_upgrade_v2_test.py
+++ tensorflow/tools/compatibility/tf_upgrade_v2_test.py
@@ -1520,6 +1520,33 @@ def _log_prob(self, x):
     _, _, _, new_text = self._upgrade(text)
     self.assertEqual(expected_text, new_text)
 
+  def test_sdca_to_raw_ops(self):
+    text = "tf.train.sdca_fprint(input_tensor)"
+    expected_text = "tf.raw_ops.SdcaFprint(input=input_tensor)"
+    _, _, _, new_text = self._upgrade(text)
+    self.assertEqual(expected_text, new_text)
+
+    text = "tf.train.sdca_fprint(input, name=n)"
+    expected_text = "tf.raw_ops.SdcaFprint(input=input, name=n)"
+    _, _, _, new_text = self._upgrade(text)
+    self.assertEqual(expected_text, new_text)
+
+    text = "tf.train.sdca_shrink_l1(w, l, ll)"
+    expected_text = "tf.raw_ops.SdcaShrinkL1(weights=w, l1=l, l2=ll)"
+    _, _, _, new_text = self._upgrade(text)
+    self.assertEqual(expected_text, new_text)
+
+    text = (
+        "tf.train.sdca_optimizer(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o)")
+    expected_text = (
+        "tf.raw_ops.SdcaOptimizer(sparse_example_indices=a, "
+        "sparse_feature_indices=b, sparse_feature_values=c, dense_features=d, "
+        "example_weights=e, example_labels=f, sparse_indices=g, "
+        "sparse_weights=h, dense_weights=i, example_state_data=j, loss_type=k, "
+        "l1=l, l2=m, num_loss_partitions=n, num_inner_iterations=o)")
+    _, _, _, new_text = self._upgrade(text)
+    self.assertEqual(expected_text, new_text)
+
 
 class TestUpgradeFiles(test_util.TensorFlowTestCase):
 
