937a02ae3d560f7c8f92d61a8f2feb6533c113b3
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2019-03-18 14:35:03 +0530

    Added example to tf.argsort and tf.sort

--- tensorflow/python/ops/sort_ops.py
+++ tensorflow/python/ops/sort_ops.py
@@ -36,7 +36,17 @@ from tensorflow.python.util.tf_export import tf_export
 @tf_export('sort')
 def sort(values, axis=-1, direction='ASCENDING', name=None):
   """Sorts a tensor.
+  
+  Usage:
 
+  ```python
+  import tensorflow as tf
+  a = [1, 10, 26.9, 2.8, 166.32, 62.3]
+  b = tf.sort(a,axis=-1,direction='ASCENDING',name=None)
+  c = tf.keras.backend.eval(b)
+  # Here, c = [  1.     2.8   10.    26.9   62.3  166.32]
+  ```
+  
   Args:
     values: 1-D or higher numeric `Tensor`.
     axis: The axis along which to sort. The default is -1, which sorts the last
@@ -65,7 +75,6 @@ def argsort(values, axis=-1, direction='ASCENDING', stable=False, name=None):
   `values`, but along the given axis, values represent the index of the sorted
   element in that slice of the tensor at the given position.
   
-  For example:
   Usage:
 
   ```python
