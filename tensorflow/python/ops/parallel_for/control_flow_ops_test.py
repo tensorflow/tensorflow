a2cb112e407d6e5480a31abc57f08eb80807cd29
Deven Desai <deven.desai.amd@gmail.com>
2019-03-21 21:20:22 +0000

    skipping subtests that require 5d tensors

--- tensorflow/python/ops/parallel_for/control_flow_ops_test.py
+++ tensorflow/python/ops/parallel_for/control_flow_ops_test.py
@@ -317,6 +317,10 @@ class NNTest(PForTestCase):
     self._test_loop_fn(loop_fn, 3, loop_fn_dtypes=[dtypes.float32] * 3)
 
   def test_max_pool3d(self):
+
+    if test_lib.is_built_with_rocm():
+      self.skipTest("5D tensors are not yet supported in ROCm")
+
     with backprop.GradientTape(persistent=True) as g:
       x = random_ops.random_uniform([3, 3, 2, 12, 12, 3])
       g.watch(x)
