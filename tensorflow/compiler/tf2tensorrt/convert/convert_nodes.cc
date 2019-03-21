283fcfe75c9dde7a14e3613356007f974ccc140f
Trevor Morris <tmorris@nvidia.com>
2019-03-21 16:01:17 -0700

    Basic split and unstack converters.
    Squeeze output for Unstack
    
    Add Unstack, Split to converter list. Sort converter list. Combine Reduce op list
    
    Add unit tests for Split
    
    Fix Unstack and Split
    
    Fixes
    
    Fix Half tests and some formatting
    
    0-D support
    
    Fix unit tests
    
    Undo cuda 10.1 fix
    
    Formatting
    
    Remove ConvertVector
    
    Fix bug
    
    Formatting
    
    Undo thing

--- tensorflow/compiler/tf2tensorrt/convert/convert_nodes.cc
+++ tensorflow/compiler/tf2tensorrt/convert/convert_nodes.cc
@@ -3492,6 +3492,105 @@ Status ConvertPad(OpConverterParams* params) {
   return Status::OK();
 }
 
+Status ConvertSplitHelper(OpConverterParams* params,
+                          const TRT_TensorOrWeights& input, int tf_axis,
+                          int num_splits, bool squeeze_after) {
+  const auto& node_def = params->node_def;
+  const nvinfer1::Dims dims = input.GetTrtDims();
+  // Convert axis.
+  int trt_axis;
+  TF_RETURN_IF_ERROR(
+      ConvertAxis(tf_axis, dims.nbDims, node_def.name(), &trt_axis));
+  // Dimension must equal num_splits for Unstack (when squeeze_after is true)
+  if (squeeze_after && dims.d[trt_axis] != num_splits) {
+    return errors::InvalidArgument(
+        "Dimension ", tf_axis, " has size ", dims.d[trt_axis],
+        " which is not equal to num of ", num_splits, ", at ", node_def.name());
+  }
+  // Dimension must be evenly divisible by num_splits.
+  if (dims.d[trt_axis] % num_splits != 0) {
+    return errors::InvalidArgument(
+        "Dimension ", tf_axis, " of size ", dims.d[trt_axis],
+        " is not evenly divisble by ", num_splits, ", at ", node_def.name());
+  }
+
+  // Create parameters for StridedSliceHelper.
+  // Slice will begin on zero for all dims, except the one being split which
+  // will change.
+  std::vector<int> begin(dims.nbDims, 0);
+  // Determine size of split. Slice will get the full length of all dims, except
+  // the one being split.
+  std::vector<int> size(dims.d, dims.d + dims.nbDims);
+  const int split_size_on_axis = dims.d[trt_axis] / num_splits;
+  size[trt_axis] = split_size_on_axis;
+  // Stride will always be 1
+  std::vector<int> stride(dims.nbDims, 1);
+  // Add dummy batch dimension
+  begin.insert(begin.begin(), 0);
+  size.insert(size.begin(), 1);
+  stride.insert(stride.begin(), 1);
+
+  // Slice the input. ConvertStridedSliceHelper will push the outputs onto
+  // params->outputs.
+  for (int i = 0; i < num_splits; ++i) {
+    begin[trt_axis + 1] = i * split_size_on_axis;
+    TF_RETURN_IF_ERROR(
+        ConvertStridedSliceHelper(params, input, begin, size, stride));
+  }
+  if (params->validation_only) return Status::OK();
+
+  // For Unpack/Unstack, remove axis that we split upon.
+  if (squeeze_after) {
+    // Create the new shape.
+    size.erase(size.begin() + trt_axis + 1);
+    nvinfer1::Dims new_dims;
+    TF_RETURN_IF_ERROR(
+        TensorShapeArrayToTrtDims(size, &new_dims, /*ignore_frst_dim=*/true));
+    // Reshape each slice.
+    for (int i = 0; i < params->outputs->size(); i++) {
+      nvinfer1::ITensor* output_tensor = nullptr;
+      TF_RETURN_IF_ERROR(params->converter->PrepareTensorForShape(
+          params->outputs->at(i), new_dims, /*validation_only=*/false,
+          &output_tensor));
+      (*params->outputs)[i] = TRT_TensorOrWeights(output_tensor);
+    }
+  }
+  return Status::OK();
+}
+
+Status ConvertSplit(OpConverterParams* params) {
+  const auto& inputs = params->inputs;
+  const auto& node_def = params->node_def;
+  TF_RETURN_IF_ERROR(
+      CheckInputsWeights(*params, {{"axis", true}, {"value", false}}));
+  TF_RETURN_IF_ERROR(AllowDataTypes(
+      *params, {DataType::DT_FLOAT, DataType::DT_HALF, DataType::DT_INT32}));
+  int tf_axis = inputs.at(0).weights().GetSpan<int>()[0];
+  TFAttrs attrs(node_def);
+  const int num_split = attrs.get<int64>("num_split");
+
+  return ConvertSplitHelper(params, inputs.at(1), tf_axis, num_split, false);
+}
+
+Status ConvertUnpack(OpConverterParams* params) {
+  const auto& inputs = params->inputs;
+  const auto& node_def = params->node_def;
+  TF_RETURN_IF_ERROR(CheckInputsWeights(*params, {{"value", false}}));
+  TF_RETURN_IF_ERROR(AllowDataTypes(
+      *params, {DataType::DT_FLOAT, DataType::DT_HALF, DataType::DT_INT32}));
+  // Input must be rank 1 or higher, since we can't unpack on axis 0.
+  if (inputs.at(0).GetTrtDims().nbDims == 0) {
+    return errors::Unimplemented(
+        "Input \"value\" for Unpack must be rank 2 or greater, at ",
+        node_def.name());
+  }
+  TFAttrs attrs(node_def);
+  const int tf_axis = attrs.get<int64>("axis");
+  const int num = attrs.get<int64>("num");
+
+  return ConvertSplitHelper(params, inputs.at(0), tf_axis, num, true);
+}
+
 Status ConvertConcat(OpConverterParams* params) {
   const auto& inputs = params->inputs;
   const auto& node_def = params->node_def;
@@ -4128,6 +4227,7 @@ Status ConvertCombinedNMS(OpConverterParams* params) {
 
 static void RegisterValidatableOpConverters(
     std::unordered_map<string, OpConverter>* registration) {
+  (*registration)["BatchMatMul"] = ConvertBatchMatMul;
   (*registration)["BiasAdd"] = ConvertBiasAdd;
 #if IS_TRT_VERSION_GE(5, 1, 0, 0)
   (*registration)["CombinedNonMaxSuppression"] = ConvertCombinedNMS;
@@ -4139,6 +4239,7 @@ static void RegisterValidatableOpConverters(
   (*registration)["DepthwiseConv2dNative"] = ConvertConv2DDepthwise;
   (*registration)["ExpandDims"] = ConvertExpandDims;
   (*registration)["GatherV2"] = ConvertGather;
+  (*registration)["Identity"] = ConvertIdentity;  // Identity should be removed
   (*registration)["LeakyRelu"] = ConvertLeakyRelu;
   (*registration)["MatMul"] = ConvertMatMul;
   (*registration)["Pad"] = ConvertPad;
@@ -4146,23 +4247,15 @@ static void RegisterValidatableOpConverters(
   (*registration)["Reshape"] = ConvertReshape;
   (*registration)["Rsqrt"] = ConvertRsqrt;
   (*registration)["Slice"] = ConvertSlice;
+  (*registration)["Snapshot"] = ConvertIdentity;  // Snapshot should be removed
+  (*registration)["Softmax"] = ConvertSoftmax;
+  (*registration)["Split"] = ConvertSplit;
   (*registration)["Square"] = ConvertSquare;
   (*registration)["Squeeze"] = ConvertSqueeze;
   (*registration)["StridedSlice"] = ConvertStridedSlice;
-  (*registration)["Transpose"] = ConvertTranspose;
   (*registration)["TopKV2"] = ConvertTopK;
-
-  // TODO(ben,jie): this is a temp hack.
-  (*registration)["Identity"] = ConvertIdentity;  // Identity should be removed
-  (*registration)["Snapshot"] = ConvertIdentity;  // Snapshot should be removed
-
-  (*registration)["Sum"] = ConvertReduce;
-  (*registration)["Prod"] = ConvertReduce;
-  (*registration)["Max"] = ConvertReduce;
-  (*registration)["Min"] = ConvertReduce;
-  (*registration)["Mean"] = ConvertReduce;
-  (*registration)["Softmax"] = ConvertSoftmax;
-  (*registration)["BatchMatMul"] = ConvertBatchMatMul;
+  (*registration)["Transpose"] = ConvertTranspose;
+  (*registration)["Unpack"] = ConvertUnpack;
 
   for (auto quantization_op_type :
        {"QuantizeAndDequantizeV2", "QuantizeAndDequantizeV3",
@@ -4184,6 +4277,9 @@ static void RegisterValidatableOpConverters(
   }
   for (auto unary_op_pair : *UnaryOperationMap()) {
     (*registration)[unary_op_pair.first] = ConvertUnary;
+  }
+  for (auto reduce_op_type : {"Sum", "Prod", "Max", "Min", "Mean"}) {
+    (*registration)[reduce_op_type] = ConvertReduce;
   }
 }
 
