0fae906792e8bfa298c5d29ab125a354e4966182
A. Unique TensorFlower <gardener@tensorflow.org>
2019-03-22 16:07:15 -0700

    Removes Foundation imports from Swift files.
    PiperOrigin-RevId: 239883149

--- tensorflow/lite/experimental/swift/Sources/Model.swift
+++ tensorflow/lite/experimental/swift/Sources/Model.swift
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
 import TensorFlowLiteC
 
 /// A TensorFlow Lite model used by the 'Interpreter` to perform inference.
