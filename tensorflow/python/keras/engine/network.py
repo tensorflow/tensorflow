5d46ea5c8612000e7ce2b0d0e365e20f92494d76
Thomas O'Malley <omalleyt@google.com>
2019-03-22 11:41:36 -0700

    Fix Functional API `get_config` compat wi/ keras-team/keras: `inbound_nodes` property.
    PiperOrigin-RevId: 239834276

--- tensorflow/python/keras/engine/network.py
+++ tensorflow/python/keras/engine/network.py
@@ -1085,6 +1085,8 @@ class Network(base_layer.Layer):
                   tf_utils.ListWrapper(
                       [inbound_layer.name, new_node_index, tensor_id, kwargs]))
             node_data = nest.pack_sequence_as(node.input_tensors, node_data)
+            if not nest.is_sequence(node_data):
+              node_data = [node_data]
             # Convert ListWrapper to list for backwards compatible configs.
             node_data = tf_utils.convert_inner_node_data(node_data)
             filtered_inbound_nodes.append(node_data)
