3ae29805c6570a2f03c606f5ecc3ea7d34924a2b
Siju Samuel <siju.samuel@huawei.com>
2019-03-22 11:43:39 +0530

    make_round_test updated based on latest code

--- tensorflow/lite/testing/generate_examples_lib.py
+++ tensorflow/lite/testing/generate_examples_lib.py
@@ -3527,6 +3527,31 @@ def make_ceil_tests(options):
 
   make_zip_of_tests(options, test_parameters, build_graph, build_inputs)
 
+@register_make_test_function
+def make_round_tests(zip_path):
+  """Build the round op testing graph."""
+
+  test_parameters = [{
+      "input_dtype": [tf.float32],
+      "input_shape": [[], [1], [1, 2], [5, 6, 7, 8], [3, 4, 5, 6]],
+  }]
+
+  def build_graph(parameters):
+    """Build the round op testing graph."""
+    input_value = tf.placeholder(
+        dtype=parameters["input_dtype"],
+        name="input1",
+        shape=parameters["input_shape"])
+    out = tf.round(input_value)
+    return [input_value], [out]
+
+  def build_inputs(parameters, sess, inputs, outputs):
+    input_value = create_tensor_data(parameters["input_dtype"],
+                                     parameters["input_shape"])
+    return [input_value], sess.run(
+        outputs, feed_dict={inputs[0]: input_value})
+
+  make_zip_of_tests(zip_path, test_parameters, build_graph, build_inputs)
 
 @register_make_test_function
 def make_neg_tests(options):
