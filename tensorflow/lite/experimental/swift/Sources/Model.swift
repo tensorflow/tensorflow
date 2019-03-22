0a9084203d48424bd5b50ebb431daed71163b419
A. Unique TensorFlower <gardener@tensorflow.org>
2019-03-22 00:25:36 -0700

    Adds new podspecs for the TensorFlow Lite iOS libraries.
    PiperOrigin-RevId: 239748653

--- tensorflow/lite/experimental/swift/Sources/Model.swift
+++ tensorflow/lite/experimental/swift/Sources/Model.swift
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import Foundation
-import TensorFlowLiteCAPI
+import TensorFlowLiteC
 
 /// A TensorFlow Lite model used by the 'Interpreter` to perform inference.
 final class Model {
