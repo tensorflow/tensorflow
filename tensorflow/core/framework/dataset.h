3281d964b4d940a799f39ddde5cacca2420ee77e
Shivani Agrawal <shivaniagrawal@google.com>
2019-03-25 15:27:05 -0700

    [tf.data] Check if node_ is present before returning num_elemets for iterator.
    PiperOrigin-RevId: 240230437

--- tensorflow/core/framework/dataset.h
+++ tensorflow/core/framework/dataset.h
@@ -529,7 +529,10 @@ class IteratorBase {
   }
 
   // Returns the number of elements produced by this itertaor.
-  int64 num_elements() const { return node_->num_elements(); }
+  int64 num_elements() const {
+    if (node_) return node_->num_elements();
+    return 0;
+  }
 
  private:
   friend class DatasetBase;  // for access to `AddCleanupFunction`
