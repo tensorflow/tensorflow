b8ab8777eb4268ca5bca94d5eaae49dd0fe67fab
shashvat <ambesh1969@gmail.com>
2019-03-25 12:25:13 +0530

    added float16 dtype

--- tensorflow/core/ops/math_ops.cc
+++ tensorflow/core/ops/math_ops.cc
@@ -460,7 +460,7 @@ REGISTER_OP("DivNoNan")
     .Input("x: T")
     .Input("y: T")
     .Output("z: T")
-    .Attr("T: {float, double}")
+    .Attr("T: {bfloat16, half, float, double}")
     .SetShapeFn(shape_inference::BroadcastBinaryOpShapeFn);
 
 REGISTER_OP("FloorDiv")
