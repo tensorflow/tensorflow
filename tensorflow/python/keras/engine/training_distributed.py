b3be250ccf0177e5f5dac1340044fe8479173dae
A. Unique TensorFlower <gardener@tensorflow.org>
2019-03-27 15:37:52 -0700

    Fixed TPU distribution to be compatible with python3.
    PiperOrigin-RevId: 240655994

--- tensorflow/python/keras/engine/training_distributed.py
+++ tensorflow/python/keras/engine/training_distributed.py
@@ -513,7 +513,7 @@ def experimental_tpu_test_loop(model,
       # workaround until new metrics are in place.
       reduce_op = ds_reduce_util.ReduceOp.MEAN
     output_tensors[label] = current_strategy.reduce(reduce_op, output)
-  test_op = control_flow_ops.group(output_tensors.values())
+  test_op = control_flow_ops.group(list(output_tensors.values()))
 
   if verbose >= 1:
     progbar = Progbar(target=steps)
