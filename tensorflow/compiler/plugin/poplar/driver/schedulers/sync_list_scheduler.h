628745e69c62a60acd16b2b4f491f05154d4bb4a
Jake Hall <jakeh@graphcore.ai>
2019-03-29 16:33:41 +0000

    Add custom scheduler that schedules all-reduce ops as late as possible
    Summary:
    Based on the TF list scheduler.
    Currently waits until there are 64k of syncs waiting...
    
    Resolves T7643
    
    Test Plan: Added a test that demonstrates delaying the all-reduce ops
    
    Reviewers: georgep, davidn
    
    Reviewed By: georgep
    
    Maniphest Tasks: T7643
    
    Differential Revision: https://phabricator.sourcevertex.net/D10363

---
+++ tensorflow/compiler/plugin/poplar/driver/schedulers/sync_list_scheduler.h
@@ -0,0 +1,43 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_SCHEDULERS_SYNC_LIST_SCHEDULER_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_SCHEDULERS_SYNC_LIST_SCHEDULER_H_
+
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "tensorflow/compiler/xla/service/hlo_instruction.h"
+#include "tensorflow/compiler/xla/service/hlo_memory_scheduler.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
+#include "tensorflow/compiler/xla/service/hlo_ordering.h"
+#include "tensorflow/compiler/xla/service/hlo_pass_interface.h"
+#include "tensorflow/compiler/xla/service/hlo_schedule.h"
+#include "tensorflow/compiler/xla/service/logical_buffer.h"
+#include "tensorflow/compiler/xla/service/tuple_points_to_analysis.h"
+#include "tensorflow/compiler/xla/statusor.h"
+#include "tensorflow/compiler/xla/types.h"
+
+namespace xla {
+namespace poplarplugin {
+
+// Sync aware list scheduler based on the TF ListMemoryScheduler from
+// hlo_memory_scheduler
+MemorySchedulerAlgorithm CreateSyncListMemoryScheduler(
+    int64 maxmimum_all_reduce_buffer_size = 0);
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif
