9f955aefca4eb6fc2fdbd061933958cd8892a71e
Dayananda V <dayananda.v1@huawei.com>
2019-03-29 12:06:33 +0530

    TF Lite zeros_like_test death test case added Unsupported validation test case added for zeros_like operator

---
+++ git_email_change.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="dayanandasiet@gmail.com"
+CORRECT_NAME="Dayananda V"
+CORRECT_EMAIL="dayananda.v1@huawei.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
--- tensorflow/lite/kernels/zeros_like_test.cc
+++ tensorflow/lite/kernels/zeros_like_test.cc
@@ -68,6 +68,22 @@ TEST(ZerosLikeOpModel, ZerosLikeInt64) {
   EXPECT_THAT(m.GetTensorShape(m.output()), ElementsAreArray({1, 2, 2, 1}));
 }
 
+#ifdef GTEST_HAS_DEATH_TEST
+TEST(ZerosLikeOpModel, InvalidTypeTest) {
+  ZerosLikeOpModel m_uint8({TensorType_UINT8, {1, 1}});
+  EXPECT_DEATH(m_uint8.Invoke(),
+               "ZerosLike only currently supports int64, int32, and float32");
+  ZerosLikeOpModel m_int16({TensorType_INT16, {1, 1}});
+  EXPECT_DEATH(m_int16.Invoke(),
+               "ZerosLike only currently supports int64, int32, and float32");
+  ZerosLikeOpModel m_complex({TensorType_COMPLEX64, {1, 1}});
+  EXPECT_DEATH(m_complex.Invoke(),
+               "ZerosLike only currently supports int64, int32, and float32");
+  ZerosLikeOpModel m_int8({TensorType_INT8, {1, 1}});
+  EXPECT_DEATH(m_int8.Invoke(),
+               "ZerosLike only currently supports int64, int32, and float32");
+}
+#endif
 }  // namespace
 }  // namespace tflite
 
