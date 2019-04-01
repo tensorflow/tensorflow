fad7651026efb5dcbfe42a6b0c146118432cf340
Jared Duke <jdduke@google.com>
2019-04-01 09:10:20 -0700

    Disable flaky NNAPI NNAPIDelegate.SqueezeSimpleTest
    PiperOrigin-RevId: 241336295

--- tensorflow/lite/delegates/nnapi/nnapi_delegate_test.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate_test.cc
@@ -882,7 +882,8 @@ class SqueezeOpModel : public SingleOpModelWithNNAPI {
   int output_;
 };
 
-TEST(NNAPIDelegate, SqueezeSimpleTest) {
+// TODO(b/215935381): Enable after resolving issues with flakiness.
+TEST(NNAPIDelegate, DISABLED_SqueezeSimpleTest) {
   std::initializer_list<float> data = {
       1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0,  10.0, 11.0, 12.0,
       13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0};
