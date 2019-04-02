6f6740c655760bd6f21e0bfacda49101326473ff
Blake Hechtman <blakehechtman@google.com>
2019-04-02 11:01:40 -0700

    [XLA:CLIENT] Support numpy style implicit broadcasting in xla::BatchDot
    PiperOrigin-RevId: 241562264

--- tensorflow/compiler/xla/client/lib/matrix.cc
+++ tensorflow/compiler/xla/client/lib/matrix.cc
@@ -258,23 +258,66 @@ XlaOp BatchDot(XlaOp x, XlaOp y, PrecisionConfig::Precision precision) {
     TF_ASSIGN_OR_RETURN(Shape x_shape, builder->GetShape(x));
     TF_ASSIGN_OR_RETURN(Shape y_shape, builder->GetShape(y));
 
-    // The batch dimensions must be equal and the matrix dimensions must be
-    // valid.
-    std::vector<int64> batch_dimension_numbers;
-    const int ndims = x_shape.rank();
-    batch_dimension_numbers.reserve(ndims - 2);
+    // The batch dimensions must be broadcast-compatible and the matrix
+    // dimensions must be valid.
+    std::vector<int64> x_config;
+    std::vector<int64> y_config;
+    std::vector<int64> output_config;
+
+    std::vector<int64> x_implicit_broadcast;
+    std::vector<int64> y_implicit_broadcast;
+
+    const int64 ndims = std::max(y_shape.rank(), x_shape.rank());
+    // If X and Y have unequal ranks, the major dimensions of the higher rank
+    // shape are broadcasted.
+    //
+    // A dimension of size 1 can be implicitly broadcasted to any other
+    // dimension.
+    const int64 x_offset = std::max<int64>(0, y_shape.rank() - x_shape.rank());
+    const int64 y_offset = std::max<int64>(0, x_shape.rank() - y_shape.rank());
     for (int i = 0; i < ndims - 2; ++i) {
-      batch_dimension_numbers.push_back(i);
+      const int64 x_dim = i - x_offset;
+      const int64 y_dim = i - y_offset;
+      output_config.push_back(i);
+      if (x_dim < 0) {
+        y_config.push_back(i);
+      } else if (y_dim < 0) {
+        x_config.push_back(i);
+      } else if (x_shape.dimensions(x_dim) == y_shape.dimensions(y_dim)) {
+        y_config.push_back(i);
+        x_config.push_back(i);
+      } else if (x_shape.dimensions(x_dim) == 1) {
+        y_config.push_back(i);
+        x_implicit_broadcast.push_back(x_dim);
+      } else if (y_shape.dimensions(y_dim) == 1) {
+        x_config.push_back(i);
+        y_implicit_broadcast.push_back(y_dim);
+      } else {
+        return InvalidArgument("Expected batch dot dimension to be equal or 1");
+      }
     }
-    std::vector<int64> x_config = batch_dimension_numbers;
     x_config.push_back(ndims - 2);
     x_config.push_back(ndims);
-    std::vector<int64> y_config = batch_dimension_numbers;
     y_config.push_back(ndims);
     y_config.push_back(ndims - 1);
-    std::vector<int64> output_config = batch_dimension_numbers;
     output_config.push_back(ndims - 2);
     output_config.push_back(ndims - 1);
+    if (!x_implicit_broadcast.empty()) {
+      x_shape = ShapeUtil::FilterDimensions(
+          [&](int64 dim) {
+            return !absl::c_linear_search(x_implicit_broadcast, dim);
+          },
+          x_shape);
+      x = Reshape(x, x_shape.dimensions());
+    }
+    if (!y_implicit_broadcast.empty()) {
+      y_shape = ShapeUtil::FilterDimensions(
+          [&](int64 dim) {
+            return !absl::c_linear_search(y_implicit_broadcast, dim);
+          },
+          y_shape);
+      y = Reshape(y, y_shape.dimensions());
+    }
     return Einsum(x, x_config, y, y_config, output_config, precision);
   });
 }
