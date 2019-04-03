34fc260230814187d8d43393e3e178f89e9693ac
Saurabh Saxena <srbs@google.com>
2019-04-03 16:35:43 -0700

    Do not wrap Resource tensors inside optionals in cond_v2. This makes it consistent with while_v2 and avoids weird issues with copying Resources wrapped in Optionals across devices.
    PiperOrigin-RevId: 241830574

--- tensorflow/python/ops/cond_v2.py
+++ tensorflow/python/ops/cond_v2.py
@@ -652,31 +652,44 @@ class _CondGradFuncGraph(util.CondBranchFuncGraph):
     if captured_tensor is not None:
       return captured_tensor
 
-    # 'tensor' is an uncaptured intermediate in the forward graph. We wrap it in
-    # an optional in the forward graph and capture the optional normally. We
-    # then unwrap the captured optional value in the gradient graph to get the
-    # raw intermediate value.
+    # 'tensor' is an uncaptured intermediate in the forward graph.
+    # If it is not a resource, we wrap it in an optional in the forward graph
+    # and capture the optional normally. We then unwrap the captured optional
+    # value in the gradient graph to get the raw intermediate value.
+    # If it is a resource, we trace the resource upto the input in the forward
+    # graph and capture that.
 
-    if tensor not in self._wrapped_intermediates:
-      # If the gradient has already been computed for this If op, 'tensor' may
-      # already be wrapped.
-      for consumer in tensor.consumers():
-        if (consumer.type == "OptionalFromValue"
-            and consumer.outputs[0] in self._forward_graph.outputs):
-          optional = consumer.outputs[0]
-          break
-      else:
-        # 'tensor' hasn't been wrapped, do it now.
-        with self._forward_graph.as_default():
-          optional = gen_dataset_ops.optional_from_value([tensor])
-        self.if_op_needs_rewrite = True
+    if tensor.dtype == dtypes.resource:
+      # Index of the forward graph input corresponding to the resource tensor.
+      index = util.resource_input_index(
+          tensor.name, [t.name for t in self._forward_graph.inputs],
+          {op.name: op.node_def for op in self._forward_graph.get_operations()},
+          self._forward_graph._functions)
+      # This gets mapped to the corresponding If op input in
+      # `_resolve_grad_inputs`.
+      captured_tensor = super(_CondGradFuncGraph, self)._capture_helper(
+          self._forward_graph.inputs[index], name)
+    else:
+      if tensor not in self._wrapped_intermediates:
+        # If the gradient has already been computed for this If op, 'tensor' may
+        # already be wrapped.
+        for consumer in tensor.consumers():
+          if (consumer.type == "OptionalFromValue" and
+              consumer.outputs[0] in self._forward_graph.outputs):
+            optional = consumer.outputs[0]
+            break
+        else:
+          # 'tensor' hasn't been wrapped, do it now.
+          with self._forward_graph.as_default():
+            optional = gen_dataset_ops.optional_from_value([tensor])
+          self.if_op_needs_rewrite = True
+        self._wrapped_intermediates[tensor] = optional
 
-      self._wrapped_intermediates[tensor] = optional
+      optional = self._wrapped_intermediates[tensor]
+      captured_optional = super(_CondGradFuncGraph,
+                                self)._capture_helper(optional, name)
+      captured_tensor = gen_dataset_ops.optional_get_value(
+          captured_optional, [tensor.dtype], [tensor.shape])[0]
 
-    optional = self._wrapped_intermediates[tensor]
-    captured_optional = super(_CondGradFuncGraph, self)._capture_helper(
-        optional, name)
-    captured_tensor = gen_dataset_ops.optional_get_value(
-        captured_optional, [tensor.dtype], [tensor.shape])[0]
     self._indirect_captures[tensor] = captured_tensor
     return captured_tensor
