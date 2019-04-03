a3b4de0f6e9e120ba0c6c89599b432ccb22226ad
Alex Zinenko <zinenko@google.com>
2019-04-03 06:11:03 -0700

    Drop MLIREmitter-based version of the EDSC
        This version has been deprecated and can now be removed completely since the
        last remaining user (Python bindings) migrated to declarative builders.
        Several functions in lib/EDSC/Types.cpp construct core IR objects for the C
        bindings.  Move these functions into lib/EDSC/CoreAPIs.cpp until we decide
        where they should live.
    
        This completes the migration from the delayed-construction EDSC to Declarative
        Builders.
    
    --
    
    PiperOrigin-RevId: 241716729

--- include/mlir-c/Core.h
+++ include/mlir-c/Core.h
@@ -38,18 +38,6 @@ typedef const void *mlir_type_t;
 typedef void *mlir_func_t;
 /// Opaque C type for mlir::Attribute.
 typedef const void *mlir_attr_t;
-/// Opaque C type for mlir::edsc::MLIREmiter.
-typedef void *edsc_mlir_emitter_t;
-/// Opaque C type for mlir::edsc::Expr.
-typedef void *edsc_expr_t;
-/// Opaque C type for mlir::edsc::MaxExpr.
-typedef void *edsc_max_expr_t;
-/// Opaque C type for mlir::edsc::MinExpr.
-typedef void *edsc_min_expr_t;
-/// Opaque C type for mlir::edsc::Stmt.
-typedef void *edsc_stmt_t;
-/// Opaque C type for mlir::edsc::Block.
-typedef void *edsc_block_t;
 
 /// Simple C lists for non-owning mlir Opaque C types.
 /// Recommended usage is construction from the `data()` and `size()` of a scoped
@@ -66,31 +54,6 @@ typedef struct {
   mlir_type_t *types;
   uint64_t n;
 } mlir_type_list_t;
-
-typedef struct {
-  edsc_expr_t *exprs;
-  uint64_t n;
-} edsc_expr_list_t;
-
-typedef struct {
-  edsc_stmt_t *stmts;
-  uint64_t n;
-} edsc_stmt_list_t;
-
-typedef struct {
-  edsc_expr_t base;
-  edsc_expr_list_t indices;
-} edsc_indexed_t;
-
-typedef struct {
-  edsc_indexed_t *list;
-  uint64_t n;
-} edsc_indexed_list_t;
-
-typedef struct {
-  edsc_block_t *list;
-  uint64_t n;
-} edsc_block_list_t;
 
 typedef struct {
   const char *name;
@@ -140,18 +103,6 @@ mlir_attr_t makeBoolAttr(mlir_context_t context, bool value);
 /// Returns the arity of `function`.
 unsigned getFunctionArity(mlir_func_t function);
 
-/// Returns a new opaque mlir::edsc::Expr that is bound into `emitter` with a
-/// constant of the specified type.
-edsc_expr_t bindConstantBF16(edsc_mlir_emitter_t emitter, double value);
-edsc_expr_t bindConstantF16(edsc_mlir_emitter_t emitter, float value);
-edsc_expr_t bindConstantF32(edsc_mlir_emitter_t emitter, float value);
-edsc_expr_t bindConstantF64(edsc_mlir_emitter_t emitter, double value);
-edsc_expr_t bindConstantInt(edsc_mlir_emitter_t emitter, int64_t value,
-                            unsigned bitwidth);
-edsc_expr_t bindConstantIndex(edsc_mlir_emitter_t emitter, int64_t value);
-edsc_expr_t bindConstantFunction(edsc_mlir_emitter_t emitter,
-                                 mlir_func_t function);
-
 /// Returns the rank of the `function` argument at position `pos`.
 /// If the argument is of MemRefType, this returns the rank of the MemRef.
 /// Otherwise returns `0`.
@@ -160,166 +111,6 @@ unsigned getRankOfFunctionArgument(mlir_func_t function, unsigned pos);
 
 /// Returns an opaque mlir::Type of the `function` argument at position `pos`.
 mlir_type_t getTypeOfFunctionArgument(mlir_func_t function, unsigned pos);
-
-/// Returns an opaque mlir::edsc::Expr that has been bound to the `pos` argument
-/// of `function`.
-edsc_expr_t bindFunctionArgument(edsc_mlir_emitter_t emitter,
-                                 mlir_func_t function, unsigned pos);
-
-/// Fills the preallocated list `result` with opaque mlir::edsc::Expr that have
-/// been bound to each argument of `function`.
-///
-/// Prerequisites:
-///   - `result` must have been preallocated with space for exactly the number
-///     of arguments of `function`.
-void bindFunctionArguments(edsc_mlir_emitter_t emitter, mlir_func_t function,
-                           edsc_expr_list_t *result);
-
-/// Returns the rank of `boundMemRef`. This API function is provided to more
-/// easily compose with `bindFunctionArgument`. A similar function could be
-/// provided for an mlir_type_t of type MemRefType but it is expected that users
-/// of this API either:
-///   1. construct the MemRefType explicitly, in which case they already have
-///      access to the rank and shape of the MemRefType;
-///   2. access MemRefs via mlir_function_t *values* in which case they would
-///      pass edsc_expr_t bound to an edsc_emitter_t.
-///
-/// Prerequisites:
-///   - `boundMemRef` must be an opaque edsc_expr_t that has alreay been bound
-///     in `emitter`.
-unsigned getBoundMemRefRank(edsc_mlir_emitter_t emitter,
-                            edsc_expr_t boundMemRef);
-
-/// Fills the preallocated list `result` with opaque mlir::edsc::Expr that have
-/// been bound to each dimension of `boundMemRef`.
-///
-/// Prerequisites:
-///   - `result` must have been preallocated with space for exactly the rank of
-///     `boundMemRef`;
-///   - `boundMemRef` must be an opaque edsc_expr_t that has alreay been bound
-///     in `emitter`. This is because symbolic MemRef shapes require an SSAValue
-///     that can only be recovered from `emitter`.
-void bindMemRefShape(edsc_mlir_emitter_t emitter, edsc_expr_t boundMemRef,
-                     edsc_expr_list_t *result);
-
-/// Fills the preallocated lists `resultLbs`, `resultUbs` and `resultSteps` with
-/// opaque mlir::edsc::Expr that have been bound to proper values to traverse
-/// each dimension of `memRefType`.
-/// At the moment:
-///   - `resultsLbs` are always bound to the constant index `0`;
-///   - `resultsUbs` are always bound to the shape of `memRefType`;
-///   - `resultsSteps` are always bound to the constant index `1`.
-/// In the future, this will allow non-contiguous MemRef views.
-///
-/// Prerequisites:
-///   - `resultLbs`, `resultUbs` and `resultSteps` must have each been
-///     preallocated with space for exactly the rank of `boundMemRef`;
-///   - `boundMemRef` must be an opaque edsc_expr_t that has alreay been bound
-///     in `emitter`. This is because symbolic MemRef shapes require an SSAValue
-///     that can only be recovered from `emitter`.
-void bindMemRefView(edsc_mlir_emitter_t emitter, edsc_expr_t boundMemRef,
-                    edsc_expr_list_t *resultLbs, edsc_expr_list_t *resultUbs,
-                    edsc_expr_list_t *resultSteps);
-
-/// Returns an opaque expression for an mlir::edsc::Expr.
-edsc_expr_t makeBindable(mlir_type_t type);
-
-/// Returns an opaque expression for an mlir::edsc::Stmt.
-edsc_stmt_t makeStmt(edsc_expr_t e);
-
-/// Returns an opaque expression for an mlir::edsc::Indexed.
-edsc_indexed_t makeIndexed(edsc_expr_t expr);
-
-/// Returns an opaque expression that will emit an abstract operation identified
-/// by its name.
-edsc_expr_t Op(mlir_context_t context, const char *name, mlir_type_t resultType,
-               edsc_expr_list_t arguments, edsc_block_list_t successors,
-               mlir_named_attr_list_t attrs);
-
-/// Returns an opaque expression that will emit an mlir::LoadOp.
-edsc_expr_t Load(edsc_indexed_t indexed, edsc_expr_list_t indices);
-
-/// Returns an opaque statement for an mlir::StoreOp.
-edsc_stmt_t Store(edsc_expr_t value, edsc_indexed_t indexed,
-                  edsc_expr_list_t indices);
-
-/// Returns an opaque statement for an mlir::SelectOp.
-edsc_expr_t Select(edsc_expr_t cond, edsc_expr_t lhs, edsc_expr_t rhs);
-
-/// Returns an opaque constant integer expression of the specified type.  The
-/// type may be i* or index.
-edsc_expr_t ConstantInteger(mlir_type_t type, int64_t value);
-
-/// Returns an opaque statement for an mlir::ReturnOp.
-edsc_stmt_t Return(edsc_expr_list_t values);
-
-/// Returns an opaque expression for an mlir::edsc::StmtBlock containing the
-/// given list of statements.
-edsc_block_t Block(edsc_expr_list_t arguments, edsc_stmt_list_t enclosedStmts);
-
-/// Set the body of the block to the given statements and return the block.
-edsc_block_t BlockSetBody(edsc_block_t, edsc_stmt_list_t stmts);
-
-/// Returns an opaque statement branching to `destination` and passing
-/// `arguments` as block arguments.
-edsc_stmt_t Branch(edsc_block_t destination, edsc_expr_list_t arguments);
-
-/// Returns an opaque statement that redirects the control flow to either
-/// `trueDestination` or `falseDestination` depending on whether the
-/// `condition` expression is true or false.  The caller may pass expressions
-/// as arguments to the destination blocks using `trueArguments` and
-/// `falseArguments`, respectively.
-edsc_stmt_t CondBranch(edsc_expr_t condition, edsc_block_t trueDestination,
-                       edsc_expr_list_t trueArguments,
-                       edsc_block_t falseDestination,
-                       edsc_expr_list_t falseArguments);
-
-/// Returns an opaque statement for an mlir::AffineForOp with `enclosedStmts`
-/// nested below it.
-edsc_stmt_t For(edsc_expr_t iv, edsc_expr_t lb, edsc_expr_t ub,
-                edsc_expr_t step, edsc_stmt_list_t enclosedStmts);
-
-/// Returns an opaque statement for a perfectly nested set of mlir::AffineForOp
-/// with `enclosedStmts` nested below it.
-edsc_stmt_t ForNest(edsc_expr_list_t iv, edsc_expr_list_t lb,
-                    edsc_expr_list_t ub, edsc_expr_list_t step,
-                    edsc_stmt_list_t enclosedStmts);
-
-/// Returns an opaque 'max' expression that can be used only inside a for loop.
-edsc_max_expr_t Max(edsc_expr_list_t args);
-
-/// Returns an opaque 'min' expression that can be used only inside a for loop.
-edsc_min_expr_t Min(edsc_expr_list_t args);
-
-/// Returns an opaque statement for an mlir::AffineForOp with the lower bound
-/// `max(lbs)` and the upper bound `min(ubs)`, and with `enclosedStmts` nested
-/// below it.
-edsc_stmt_t MaxMinFor(edsc_expr_t iv, edsc_max_expr_t lb, edsc_min_expr_t ub,
-                      edsc_expr_t step, edsc_stmt_list_t enclosedStmts);
-
-/// Returns an opaque expression for the corresponding Binary operation.
-edsc_expr_t Add(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Sub(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Mul(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Div(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Rem(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t LT(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t LE(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t GT(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t GE(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t EQ(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t NE(edsc_expr_t e1, edsc_expr_t e2);
-
-edsc_expr_t FloorDiv(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t CeilDiv(edsc_expr_t e1, edsc_expr_t e2);
-
-edsc_expr_t And(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Or(edsc_expr_t e1, edsc_expr_t e2);
-edsc_expr_t Negate(edsc_expr_t e);
-
-edsc_expr_t Call0(edsc_expr_t callee, edsc_expr_list_t args);
-edsc_expr_t Call1(edsc_expr_t callee, mlir_type_t result,
-                  edsc_expr_list_t args);
 
 #ifdef __cplusplus
 } // end extern "C"
