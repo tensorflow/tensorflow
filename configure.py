2b9c299716afdc07a1cc5a9e1651f31eb7e4b4fc
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-04 10:30:19 -0700

    Bump max bazel version to 0.24.1
    Bazel 0.24.1 is now released: https://github.com/bazelbuild/bazel/releases/tag/0.24.1
    
    PiperOrigin-RevId: 241955827

--- configure.py
+++ configure.py
@@ -1637,7 +1637,7 @@ def main():
   # environment variables.
   environ_cp = dict(os.environ)
 
-  current_bazel_version = check_bazel_version('0.22.0', '0.24.0')
+  current_bazel_version = check_bazel_version('0.22.0', '0.24.1')
   _TF_CURRENT_BAZEL_VERSION = convert_version_to_int(current_bazel_version)
 
   reset_tf_configure_bazelrc()
