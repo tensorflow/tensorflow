82477a6470f4d5dccd8074026bb84abc432e3b7c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-04-04 20:11:49 +0900

    Move include header to cc

--- tensorflow/lite/delegates/gpu/gl/egl_context.cc
+++ tensorflow/lite/delegates/gpu/gl/egl_context.cc
@@ -15,6 +15,9 @@ limitations under the License.
 
 #include "tensorflow/lite/delegates/gpu/gl/egl_context.h"
 
+#include <vector>
+#include <cstring>
+
 #include "tensorflow/lite/delegates/gpu/common/status.h"
 #include "tensorflow/lite/delegates/gpu/gl/gl_call.h"
 #include "tensorflow/lite/delegates/gpu/gl/gl_errors.h"
