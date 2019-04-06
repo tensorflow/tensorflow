e700bac4f1d72b041fae74944aa050d47788ff45
Brad Huang <bradhuang1999@yahoo.com>
2019-04-06 14:02:53 -0700

    Formatting: Change formatting of source paper

--- tensorflow/python/ops/nn_impl.py
+++ tensorflow/python/ops/nn_impl.py
@@ -1192,7 +1192,6 @@ def batch_normalization(x,
                         name=None):
   r"""Batch normalization.
 
-  As described in http://arxiv.org/abs/1502.03167.
   Normalizes a tensor by `mean` and `variance`, and applies (optionally) a
   `scale` \\(\gamma\\) to it, as well as an `offset` \\(\beta\\):
 
@@ -1217,6 +1216,9 @@ def batch_normalization(x,
       `mean` and `variance` in this case would typically be the outputs of
       `tf.nn.moments(..., keep_dims=False)` during training, or running averages
       thereof during inference.
+
+  See Source [Batch Normalization: Accelerating Deep Network Training by Reducing 
+  Internal Covariate Shift, S. Ioffe, C. Szegedy](http://arxiv.org/abs/1502.03167).
 
   Args:
     x: Input `Tensor` of arbitrary dimensionality.
@@ -1255,7 +1257,8 @@ def fused_batch_norm(
     name=None):
   r"""Batch normalization.
 
-  As described in http://arxiv.org/abs/1502.03167.
+  See Source [Batch Normalization: Accelerating Deep Network Training by Reducing 
+  Internal Covariate Shift, S. Ioffe, C. Szegedy](http://arxiv.org/abs/1502.03167).
 
   Args:
     x: Input `Tensor` of 4 dimensions.
