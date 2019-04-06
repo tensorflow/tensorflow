32bf042be99e05664ae42291dddfd11e3b5d1582
Elaine Crutchley <44687684+elainecrutchley@users.noreply.github.com>
2019-04-06 12:42:03 -0700

    Additional info from Keras.io

--- tensorflow/python/keras/applications/__init__.py
+++ tensorflow/python/keras/applications/__init__.py
@@ -88,3 +88,37 @@ from tensorflow.python.keras.applications.xception import Xception
 del absolute_import
 del division
 del print_function
+
+
+"""
+`keras.applications.mobilenet.MobileNet(input_shape=None, alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=True, weights='imagenet', input_tensor=None, pooling=None, classes=1000)`
+
+MobileNet model, with weights pre-trained on ImageNet.
+
+Note that this model only supports the data format `'channels_last'` (height, width, channels).
+
+The default input size for this model is 224x224.
+
+Arguments
+
+* input_shape: optional shape tuple, only to be specified if `include_top` is `False` (otherwise the input shape has to be `(224, 224, 3)` (with `'channels_last'` data format) or `(3, 224, 224`) (with `'channels_first'` data format). It should have exactly 3 inputs channels, and width and height should be no smaller than 32. E.g. `(200, 200, 3)` would be one valid value.
+* alpha: controls the width of the network.
+** If `alpha` < 1.0, proportionally decreases the number of filters in each layer.
+** If `alpha` > 1.0, proportionally increases the number of filters in each layer.
+** If `alpha` = 1, default number of filters from the paper are used at each layer.
+* depth_multiplier: depth multiplier for depthwise convolution (also called the resolution multiplier)
+* dropout: dropout rate
+* include_top: whether to include the fully-connected layer at the top of the network.
+* weights: `None` (random initialization) or  `'imagenet'` (ImageNet weights)
+* input_tensor: optional Keras tensor (i.e. output of `layers.Input()`) to use as image input for the model.
+* pooling: Optional pooling mode for feature extraction when `include_top` is `False`.
+** `None` means that the output of the model will be the 4D tensor output of the last convolutional layer.
+** `'avg'` means that global average pooling will be applied to the output of the last convolutional layer, and thus the output of the model will be a 2D tensor.
+** `'max'` means that global max pooling will be applied.
+** classes: optional number of classes to classify images into, only to be specified if `include_top` is `True`, and if no `weights` argument is specified.
+
+Returns
+
+A Keras `Model` instance.
+
+"""
