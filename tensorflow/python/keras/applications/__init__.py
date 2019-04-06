430da5dbb5d3657f8d4ad20cbb7f52764942574f
Elaine Crutchley <44687684+elainecrutchley@users.noreply.github.com>
2019-04-06 12:34:36 -0700

    Added additional information from Keras.io

--- tensorflow/python/keras/applications/__init__.py
+++ tensorflow/python/keras/applications/__init__.py
@@ -88,3 +88,23 @@ from tensorflow.python.keras.applications.xception import Xception
 del absolute_import
 del division
 del print_function
+
+"""
+`keras.applications.inception_resnet_v2.InceptionResNetV2(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)`
+
+Inception-ResNet V2 model, with weights pre-trained on ImageNet.
+
+This model and can be built both with `'channels_first'` data format (channels, height, width) or `'channels_last'` data format (height, width, channels).
+
+The default input size for this model is 299x299.
+
+Arguments: 
+* include_top: whether to include the fully-connected layer at the top of the network.
+* weights: one of `None` (random initialization) or `'imagenet'` (pre-training on ImageNet).
+* input_tensor: optional Keras tensor (i.e. output of `layers.Input()`) to use as image input for the model.
+* input_shape: optional shape tuple, only to be specified if `include_top` is `False` (otherwise the input shape has to be `(299, 299, 3)` (with `'channels_last'` data format) or `(3, 299, 299)` (with `'channels_first'` data format). It should have exactly 3 inputs channels, and width and height should be no smaller than 75. E.g. `(150, 150, 3)` would be one valid value.
+* pooling: Optional pooling mode for feature extraction when `include_top` is `False`.
+** `None` means that the output of the model will be the 4D tensor output of the last convolutional layer.
+** `'avg'` means that global average pooling will be applied to the output of the last convolutional layer, and thus the output of the model will be a 2D tensor.
+** `'max'` means that global max pooling will be applied.
+* classes: optional number of classes to classify images into, only to be specified if `include_top` is `True`, and if no `weights` argument is specified.
