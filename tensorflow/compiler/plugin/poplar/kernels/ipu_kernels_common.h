e60b2a85d39c65e3e77893a825221ee81da3ee39
George Grzegorz Pawelczak <grzegorzp@graphcore.ai>
2019-04-09 17:21:09 +0100

    Remove dead code
    Test Plan: all tests pass.
    
    Reviewers: jakeh, davidn
    
    Reviewed By: jakeh
    
    Differential Revision: https://phabricator.sourcevertex.net/D10584

--- tensorflow/compiler/plugin/poplar/kernels/ipu_kernels_common.h
+++ tensorflow/compiler/plugin/poplar/kernels/ipu_kernels_common.h
@@ -35,26 +35,6 @@ class IpuOpKernel {
  protected:
   IpuOpKernel() = default;
 
-  // Allocating indexes used by the Allocation Finder - op specific.
-  virtual const absl::flat_hash_set<int64> AllocatingIndexes() = 0;
-
-  // Layout dependent indexes used by the Forward Allocation Finder - op
-  // specific.
-  // Example - custom op has 3 inputs a(0), b(1), c(2) - a is an input, and b
-  // and c are dependent on allocation of a, then
-  // this map would look as follows:
-  // { {1, 0}, {2, 0} }
-  // Note that the dependent allocation cannot be an Allocating index or another
-  // layout dependency.
-  virtual const absl::flat_hash_map<int64, int64> LayoutDependencies() = 0;
-
-  // Return how many of the first n operands are updated in place. If 0, the op
-  // is treated as NotInplace.
-  virtual const uint64 NumberOfInplaceOperands() = 0;
-
-  // Adds all the required attributes to the map.
-  void AddRequiredAttributesToMap();
-
   xla::poplarplugin::IPUCustomKernelsUtil::AttributeMap attribute_map_;
 };
 }  // namespace tensorflow
