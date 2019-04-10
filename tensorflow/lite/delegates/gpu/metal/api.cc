8a2ff8ec40a097d2a55352a16c4cbf65b809787a
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-09 17:30:39 -0700

    New version of convolution 1x1 for A9/A10 (PowerVR) for FP16 precision with FP32 accumulator. On PowerVR in iPhones we have the same time for FP16 accumulator and FP32 accumulator.
    PiperOrigin-RevId: 242773387

--- tensorflow/lite/delegates/gpu/metal/api.cc
+++ tensorflow/lite/delegates/gpu/metal/api.cc
@@ -51,10 +51,22 @@ namespace {
 std::vector<ComputeTaskDescriptorPtr> SelectConvolution(
     const GraphFloat32& graph, int id, ValueId input_id, ValueId output_id,
     const Convolution2DAttributes& attr, const metal::RuntimeOptions& options) {
+  // Special precise version, in case we cover dst_shape poorly with standard
+  // work group size.
   const auto dst_shape = graph.FindOutputs(id)[0]->tensor.shape;
-  if (GetAppleSocVersion() >= 12 &&
-      GetThreadsRatioUsualToPreciseConvolution(dst_shape) >= 1.2f) {
-    return ConvolutionPrecise(id, input_id, output_id, attr, options);
+  if (GetThreadsRatioUsualToPreciseConvolution(dst_shape) >= 1.2f) {
+    // Special version for PowerVR >= IPhone6S/SE
+    // Metal has bad driver for PowerVR in IPhone6, so for Iphone6 we should use
+    // default kernel with shared memory.
+    if ((GetAppleSocVersion() == 9 || GetAppleSocVersion() == 10) &&
+        CheckConvolutionPrecise1x1Support(attr)) {
+      return ConvolutionPrecise1x1PowerVR(id, input_id, output_id, attr,
+                                          options);
+    }
+    if (GetAppleSocVersion() >= 11 &&
+        GetThreadsRatioUsualToPreciseConvolution(dst_shape) >= 1.2f) {
+      return ConvolutionPrecise(id, input_id, output_id, attr, options);
+    }
   }
   if (GetAppleSocVersion() >= 11) {
     if (CheckConvolution1x1Support(attr)) {
