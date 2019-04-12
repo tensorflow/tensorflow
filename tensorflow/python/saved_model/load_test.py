02b60049d00d179e54713ceb250849c74486ab45
Vojtech Bardiovsky <vbardiovsky@google.com>
2019-04-12 00:53:03 -0700

    Roll forward of the https://github.com/tensorflow/tensorflow/commit/4e4943edc3d2409bffb5776f99b941987d6eda82.
    Added a condition in tf_inspect, that we don't attempt to set default for keywords that are not in args.
    
    This means that we are not in fact representing the function with ArgSpec. However the same is true for Python3 implementation of inspect.getfullargspec:
    
    def f(*args, **kwargs):
      return args
    p = functools.partial(f, 2, b=7)
    
    inspect.getfullargspec(p)
    
    "FullArgSpec(args=[], varargs='args', varkw='kwargs', defaults=None, kwonlyargs=[], kwonlydefaults=None, annotations={})"
    
    Also added one test in load_test.py.
    
    PiperOrigin-RevId: 243218910

--- tensorflow/python/saved_model/load_test.py
+++ tensorflow/python/saved_model/load_test.py
@@ -1177,9 +1177,6 @@ class LoadTest(test.TestCase, parameterized.TestCase):
     self.assertAllEqual(4, root.f(constant_op.constant(3)).numpy())
 
   def test_partial(self, cycles):
-    # TODO(b/124441704): Figure out the story for FunctionSpec vs partial.
-    self.skipTest("Partial does not work for serialization.")
-
     def f(x, y):
       return x + y
 
@@ -1194,7 +1191,8 @@ class LoadTest(test.TestCase, parameterized.TestCase):
     self.assertAllEqual(root.f(), [1.0])
 
   def test_partial_with_non_tensor_defaults(self, cycles):
-    def f(x, y):
+
+    def f(x, y=3):
       return x + y
 
     func = def_function.function(functools.partial(f, y=5))
@@ -1207,9 +1205,6 @@ class LoadTest(test.TestCase, parameterized.TestCase):
     self.assertAllEqual(root.f(1), 6)
 
   def test_partial_with_positional(self, cycles):
-    # TODO(b/124441704): Figure out the story for FunctionSpec vs partial.
-    self.skipTest("Partial does not work for serialization.")
-
     def f(x, y):
       return x + y
 
@@ -1222,9 +1217,59 @@ class LoadTest(test.TestCase, parameterized.TestCase):
     root = self.cycle(root, cycles)
     self.assertAllEqual(root.f(1), 6)
 
+  def test_partial_with_positional_captured_tensors(self, cycles):
+
+    def f(x, y):
+      return x + y
+
+    tensor = constant_op.constant(5) + constant_op.constant(7)
+    func = def_function.function(functools.partial(f, tensor))
+
+    root = tracking.AutoTrackable()
+    root.f = func
+    self.assertAllEqual(root.f(1), 13)
+
+    root = self.cycle(root, cycles)
+    self.assertAllEqual(root.f(1), 13)
+
+  def test_partial_keyword_hiding_default(self, cycles):
+
+    def f(x=3, training=True, y=7):
+      if training:
+        return x + y
+      else:
+        return x + y + 2
+
+    func = def_function.function(functools.partial(f, y=6))
+
+    root = tracking.AutoTrackable()
+    root.f = func
+    self.assertEqual(root.f().numpy(), 9)
+    self.assertEqual(root.f(training=False).numpy(), 11)
+
+    root = self.cycle(root, cycles)
+    self.assertEqual(root.f().numpy(), 9)
+    self.assertEqual(root.f(training=False).numpy(), 11)
+
+  def test_partial_with_kwargs(self, cycles):
+
+    def f(a, b, *args, **kwargs):
+      args_sum = sum(args)
+      return a + b + kwargs["some_tensor"] * kwargs["learning_rate"] + args_sum
+
+    constant_tensor = constant_op.constant(10)
+    func = def_function.function(
+        functools.partial(
+            f, 7, 1, 2, learning_rate=3, some_tensor=constant_tensor))
+
+    root = tracking.AutoTrackable()
+    root.f = func
+    self.assertEqual(root.f(constant_op.constant(4)).numpy(), 44)
+
+    root = self.cycle(root, cycles)
+    self.assertEqual(root.f(constant_op.constant(5)).numpy(), 45)
+
   def test_partial_with_passed_fn_as_default(self, cycles):
-    # TODO(b/124441704): Figure out the story for FunctionSpec vs partial.
-    self.skipTest("Partial does not work for serialization.")
 
     def f(x, y):
       return x(3) + y
