5ce3f026ee23c13c80e431ba9a59fcfa53cf29f1
shashvatshahi1998 <dracshahi@gmail.com>
2019-04-13 07:25:48 +0530

    Added some minor changes

--- tensorflow/python/ops/manip_ops.py
+++ tensorflow/python/ops/manip_ops.py
@@ -27,7 +27,7 @@ from tensorflow.python.util.tf_export import tf_export
 @tf_export('roll', v1=['roll', 'manip.roll'])
 @deprecation.deprecated_endpoints('manip.roll')
 def roll(input, shift, axis, name=None):  # pylint: disable=redefined-builtin
-  return _gen_manip_ops.roll(input, shift, axis)
+  return _gen_manip_ops.roll(input, shift, axis, name)
 
 
 roll.__doc__ = _gen_manip_ops.roll.__doc__
