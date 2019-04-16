bdb5c9a16f84cfe3909568ae91a0315937909811
Guangda Lai <31743510+aaroey@users.noreply.github.com>
2019-04-16 15:33:28 -0700

    Fix more formatting stuff.

--- tensorflow/python/compiler/tensorrt/trt_convert.py
+++ tensorflow/python/compiler/tensorrt/trt_convert.py
@@ -447,11 +447,11 @@ class GraphConverter(object):
             "Not able to save to a SavedModel since input is a GraphDef")
 
       def _restore_collections(dest_graph, src_meta_graph_def, collections):
-        scope = None
-        # Restores collections that we need to keep.
+        """Restores collections that we need to keep."""
+        scope = ""
         for key in collections:
-          col_def = src_meta_graph_def.collection_def[key]
-          kind = col_def.WhichOneof("kind")
+          collection_def = src_meta_graph_def.collection_def[key]
+          kind = collection_def.WhichOneof("kind")
           if kind is None:
             tf_logging.error(
                 "Cannot identify data type for collection %s. Skipping.", key)
@@ -460,7 +460,7 @@ class GraphConverter(object):
           if from_proto and kind == "bytes_list":
             proto_type = ops.get_collection_proto_type(key)
             # It is assumed that there are no Variables Keys in collections
-            for value in col_def.bytes_list.value:
+            for value in collection_def.bytes_list.value:
               proto = proto_type()
               proto.ParseFromString(value)
               try:
@@ -469,20 +469,21 @@ class GraphConverter(object):
                 continue
               dest_graph.add_to_collection(key, new_value)
           else:
-            field = getattr(col_def, kind)
+            field = getattr(collection_def, kind)
             if kind == "node_list":
               for value in field.value:
                 name = ops.prepend_name_scope(value, scope)
-                # since the graph has been optimized, the node may no longer exists
+                # Since the graph has been optimized, the node may no longer
+                # exists
                 try:
                   col_op = dest_graph.as_graph_element(name)
                 except (TypeError, ValueError, KeyError) as e:
                   continue
                 dest_graph.add_to_collection(key, col_op)
             elif kind == "int64_list":
-              # NOTE(opensource): This force conversion is to work around the fact
-              # that Python2 distinguishes between int and long, while Python3 has
-              # only int.
+              # NOTE(opensource): This force conversion is to work around the
+              # fact that Python2 distinguishes between int and long, while
+              # Python3 has only int.
               for value in field.value:
                 dest_graph.add_to_collection(key, int(value))
             else:
@@ -497,7 +498,7 @@ class GraphConverter(object):
         _restore_collections(
             ops.get_default_graph(), self._grappler_meta_graph_def,
             self._collections_to_keep(
-                self._grappler_meta_graph_def.collection_def), "")
+                self._grappler_meta_graph_def.collection_def))
         # We don't use any specific converter here.
         with session.Session(config=self._session_config) as sess:
           saved_model_builder.add_meta_graph_and_variables(
