b52146dcee89d3afcd73ad204f852a48e450f467
ANSHUMAN TRIPATHY <Anshuman.T@huawei.com>
2019-04-16 10:57:43 +0530

    Lite: Unpack quantization support added

--- tensorflow/lite/toco/graph_transformations/quantize.cc
+++ tensorflow/lite/toco/graph_transformations/quantize.cc
@@ -64,6 +64,7 @@ bool SupportsQuantization(const Operator& op) {
          type == OperatorType::kRelu1 || type == OperatorType::kRelu6 ||
          type == OperatorType::kLeakyRelu || type == OperatorType::kShape ||
          type == OperatorType::kExpandDims || type == OperatorType::kPack ||
+         type == OperatorType::kUnpack ||
          type == OperatorType::kTopK_V2 ||
          type == OperatorType::kRandomUniform ||
          type == OperatorType::kResizeNearestNeighbor ||
