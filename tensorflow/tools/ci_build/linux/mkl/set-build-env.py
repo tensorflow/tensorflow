eeba14f8d2dd7a75c65bfe6094adbb4d7bf751ef
Clayne Robison <clayne.b.robison@intel.com>
2019-04-17 14:48:07 -0700

    Change license to TensorFlow Apache 2.0 license

--- tensorflow/tools/ci_build/linux/mkl/set-build-env.py
+++ tensorflow/tools/ci_build/linux/mkl/set-build-env.py
@@ -1,26 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2019 Intel Corporation
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# SPDX-License-Identifier: EPL-2.0
-#
-# This script will detect the installed verion of gcc, calculate the bazel
-# build flags based on the --disable-mkl and --platform parameters, and
-# then write the build options to the --bazelrc-file bazelrc file in the
-# container environment
+# ==============================================================================
 
 from __future__ import absolute_import
 from __future__ import division
