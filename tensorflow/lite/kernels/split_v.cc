16dd0d4e718ccfb980cb9d97a54e21a295a9c66d
Josh Beal <jbeal@pinterest.com>
2019-04-17 11:48:55 -0700

    Add Int32 and Int64 value support to SplitV.

--- tensorflow/lite/kernels/split_v.cc
+++ tensorflow/lite/kernels/split_v.cc
@@ -126,7 +126,9 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   auto input_type = op_context.input->type;
   TF_LITE_ENSURE(context, input_type == kTfLiteFloat32 ||
                               input_type == kTfLiteUInt8 ||
-                              input_type == kTfLiteInt16);
+                              input_type == kTfLiteInt16 ||
+                              input_type == kTfLiteInt32 ||
+                              input_type == kTfLiteInt64);
   for (int i = 0; i < NumOutputs(node); ++i) {
     GetOutput(context, node, i)->type = input_type;
   }
@@ -180,6 +182,14 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
     }
     case kTfLiteInt16: {
       TF_LITE_SPLIT_V(int16_t);
+      break;
+    }
+    case kTfLiteInt32: {
+      TF_LITE_SPLIT_V(int32_t);
+      break;
+    }
+    case kTfLiteInt64: {
+      TF_LITE_SPLIT_V(int64_t);
       break;
     }
     default:
