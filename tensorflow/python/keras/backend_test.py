5956c7e9c44e23cd1a006df872ae468201fdb600
Taylor Robie <taylorrobie@google.com>
2019-04-18 16:59:15 -0700

    Properly clear the keras backend graph in tf 2.
    PiperOrigin-RevId: 244286438

--- tensorflow/python/keras/backend_test.py
+++ tensorflow/python/keras/backend_test.py
@@ -92,6 +92,21 @@ def compare_two_inputs_op_to_numpy(keras_op,
                          str(keras_output))
 
 
+class BackendResetTest(test.TestCase, parameterized.TestCase):
+
+  # We can't use the normal parameterized decorator because the test session
+  # will block graph clearing.
+  @parameterized.named_parameters(('_v1', context.graph_mode),
+                                  ('_v2', context.eager_mode))
+  def test_new_graph(self, test_context):
+    with test_context():
+      g_old = keras.backend.get_graph()
+      keras.backend.clear_session()
+      g = keras.backend.get_graph()
+
+      assert g_old is not g
+
+
 @test_util.run_all_in_graph_and_eager_modes
 class BackendUtilsTest(test.TestCase):
 
