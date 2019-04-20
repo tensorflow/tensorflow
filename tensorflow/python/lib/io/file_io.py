40c03adc258fdbf8c2fd5915d05ddef2c3b0465a
Yong Tang <yong.tang.github@outlook.com>
2019-04-20 17:27:16 +0000

    Remove property from seekable method of FileIO
    This fix is a follow up to address the issue in
    27276 where seekable with property causes the following
    error with Python 3.7.:
    ```
      File "/usr/lib/python3.7/_collections_abc.py", line 744, in __iter__
        yield (key, self._mapping[key])
      File "/usr/local/lib/python3.7/dist-packages/numpy/lib/npyio.py", line 251, in __getitem__
        bytes = self.zip.open(key)
      File "/usr/lib/python3.7/zipfile.py", line 1540, in open
        return ZipExtFile(zef_file, mode, zinfo, zd, True)
      File "/usr/lib/python3.7/zipfile.py", line 821, in __init__
        if fileobj.seekable():
    TypeError: 'bool' object is not callable
    ```
    
    This fix removed property annotation of the seekable methods.
    
    This fix fixes 27276.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -241,7 +241,6 @@ class FileIO(object):
         pywrap_tensorflow.Set_TF_Status_from_Status(status, ret_status)
     self._writable_file = None
 
-  @property
   def seekable(self):
     """Returns True as FileIO supports random access ops of seek()/tell()"""
     return True
