12ee3420367f9a0f728c6b2e0493f5fb150eea47
Anna R <annarev@google.com>
2019-04-21 22:09:47 -0700

    Automated rollback of commit 93b440f7935ec889a0e60f10107fc7b9495ee5b0
    PiperOrigin-RevId: 244614266

--- tensorflow/compat_template_v1.__init__.py
+++ tensorflow/compat_template_v1.__init__.py
@@ -31,13 +31,15 @@ from tensorflow.python.tools import module_util as _module_util
 _current_module = _sys.modules[__name__]
 try:
   from tensorflow_estimator.python.estimator.api._v1 import estimator
-  _current_module.__path__.append(_module_util.get_parent_dir(estimator))
+  _current_module.__path__ = (
+      [_module_util.get_parent_dir(estimator)] + _current_module.__path__)
 except ImportError:
   pass
 
 try:
   from tensorflow.python.keras.api._v1 import keras
-  _current_module.__path__.append(_module_util.get_parent_dir(keras))
+  _current_module.__path__ = (
+      [_module_util.get_parent_dir(keras)] + _current_module.__path__)
 except ImportError:
   pass
 
