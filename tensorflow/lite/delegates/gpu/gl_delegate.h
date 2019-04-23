560cdcedcb8b433bf70e6369604b5bf38ec28d48
Joe Bowser <jbowser@adobe.com>
2019-04-23 10:22:12 -0700

    Add export symbol to gl_delegate so we can destroy the delegate, name the library for compilation

--- tensorflow/lite/delegates/gpu/gl_delegate.h
+++ tensorflow/lite/delegates/gpu/gl_delegate.h
@@ -88,7 +88,7 @@ TFL_CAPI_EXPORT TfLiteDelegate* TfLiteGpuDelegateCreate(
     const TfLiteGpuDelegateOptions* options);
 
 // Destroys a delegate created with `TfLiteGpuDelegateCreate` call.
-void TfLiteGpuDelegateDelete(TfLiteDelegate* delegate);
+TFL_CAPI_EXPORT void TfLiteGpuDelegateDelete(TfLiteDelegate* delegate);
 
 // Binds GL shader storage object to an input or an output tensor in the
 // initialized delegate.  Bound buffer should have sufficient storage to
