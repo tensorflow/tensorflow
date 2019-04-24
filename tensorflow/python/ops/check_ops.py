0dac6eaffc118d824142db8fbf03ba0c2eb89105
bodin-e <mail@erikbodin.com>
2019-04-24 14:50:35 +0100

    Update tf_export assert_shapes

--- tensorflow/python/ops/check_ops.py
+++ tensorflow/python/ops/check_ops.py
@@ -1564,7 +1564,60 @@ def _is_symbol_for_unspecified_dims(symbol):
   return symbol in [Ellipsis, '*']
 
 
-@tf_export('debugging.assert_shapes', 'assert_shapes')
+@tf_export('debugging.assert_shapes', v1=[])
+def assert_shapes_v2(shapes, data=None, summarize=None,
+                     message=None, name=None):
+  """Assert tensor shapes and dimension size relationships between tensors.
+
+  This Op checks that a collection of tensors shape relationships
+  satisfies given constraints.
+
+  Example:
+
+  ```python
+  tf.assert_shapes({
+    x: ('N', 'Q'),
+    y: ('N', 'D'),
+    param: ('Q',),
+    scalar: ()
+  })
+  ```
+
+  If `x`, `y`, `param` or `scalar` does not have a shape that satisfies
+  all specified constraints, `message`, as well as the first `summarize` entries
+  of the first encountered violating tensor are printed, and
+  `InvalidArgumentError` is raised.
+
+  Size entries in the specified shapes are checked against other entries by
+  their __hash__, except:
+    - a size entry is interpreted as an explicit size if it can be parsed as an
+      integer primitive.
+    - a size entry is interpreted as *any* size if it is None or '.'.
+
+  If the first entry of a shape is `...` (type `Ellipsis`) or '*' that indicates
+  a variable number of outer dimensions of unspecified size, i.e. the constraint
+  applies to the inner-most dimensions only.
+
+  Scalar tensors and specified shapes of length zero (excluding the 'inner-most'
+  prefix) are both treated as having a single dimension of size one.
+
+  Args:
+    shapes: dictionary with (`Tensor` to shape) items. A shape must be an
+    iterable.
+    data: The tensors to print out if the condition is False.  Defaults to
+    error message and first few entries of the violating tensor.
+    summarize: Print this many entries of the tensor.
+    message: A string to prefix to the default message.
+    name: A name for this operation (optional).  Defaults to "assert_shapes".
+
+  Raises:
+    ValueError:  If static checks determine any shape constraint is violated.
+  """
+  assert_shapes(shapes, data=data, summarize=summarize,
+                message=message, name=name)
+
+
+@tf_export(v1=['debugging.assert_shapes'])
 def assert_shapes(shapes, data=None, summarize=None,
                   message=None, name=None):
   """Assert tensor shapes and dimension size relationships between tensors.
@@ -1581,6 +1634,13 @@ def assert_shapes(shapes, data=None, summarize=None,
     param: ('Q',),
     scalar: ()
   })
+  ```
+
+  Example of adding a dependency to an operation:
+
+  ```python
+  with tf.control_dependencies([tf.assert_shapes(shapes)]):
+    output = tf.matmul(x, y, transpose_a=True)
   ```
 
   If `x`, `y`, `param` or `scalar` does not have a shape that satisfies
@@ -1644,8 +1704,8 @@ def assert_shapes(shapes, data=None, summarize=None,
     for x in shapes:
       symbolic_shape = shapes[x]
       is_iterable = (
-        hasattr(symbolic_shape, "__iter__") or
-        hasattr(symbolic_shape, "__getitem__") # For Python 2 compat.
+          hasattr(symbolic_shape, "__iter__") or
+          hasattr(symbolic_shape, "__getitem__") # For Python 2 compat.
       )
       if not is_iterable:
         raise ValueError(
