0a8930bbafa56bd9739c43686d3a748ad8c78494
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-24 12:44:06 -0700

    Use clang builtin to get warp lane instead of inline ptx.
    PiperOrigin-RevId: 245096831

--- tensorflow/core/util/gpu_device_functions.h
+++ tensorflow/core/util/gpu_device_functions.h
@@ -118,7 +118,11 @@ const unsigned kCudaWarpAll = 0xffffffff;
 __device__ inline unsigned CudaLaneId() {
   unsigned int lane_id;
 #if GOOGLE_CUDA
+#if __clang__
+  return __nvvm_read_ptx_sreg_laneid();
+#else   // __clang__
   asm("mov.u32 %0, %%laneid;" : "=r"(lane_id));
+#endif  // __clang__
 #elif TENSORFLOW_USE_ROCM
   land_id = __lane_id();
 #endif
