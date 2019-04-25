a9edfc7ae48378f0f8045192f86516afa5594fb4
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-25 09:10:45 -0700

    Add virtual destructor to PluginFactoryTensorRT, which has a virtual method inherited from IPluginFactory
    This shows up as an error with stricter clang checks (-Wnon-virtual-dtor)
    
    tensorrt is closed source, so fixing IPluginFactory directly is not an option at the moment
    
    PiperOrigin-RevId: 245246720

--- tensorflow/compiler/tf2tensorrt/plugin/trt_plugin_factory.h
+++ tensorflow/compiler/tf2tensorrt/plugin/trt_plugin_factory.h
@@ -34,6 +34,13 @@ namespace tensorrt {
 
 class PluginFactoryTensorRT : public nvinfer1::IPluginFactory {
  public:
+  // TODO(b/131313301): Delete this when IPluginFactory is fixed upstream.
+  // IPluginFactory defines virtual methods and no virtual destructor. To avoid
+  // a non-virtual-dtor error, we need to add a virtual destructor here. Do not
+  // use a pointer to IPluginFactory because deleting through such a pointer
+  // results in undefined behavior.
+  virtual ~PluginFactoryTensorRT() {}
+
   // TODO(aaroey): this static method has to be inlined to make the singleton a
   // unique global symbol. Find a way to fix it.
   static PluginFactoryTensorRT* GetInstance() {
