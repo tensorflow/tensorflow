5d3aa3663a7a07e9f6494ec86fee5be44aa00597
A. Unique TensorFlower <gardener@tensorflow.org>
2019-04-26 11:15:55 -0700

    No behavior change. Prevent breakages from protobuf
    PiperOrigin-RevId: 245453729

--- tensorflow/python/framework/graph_io.py
+++ tensorflow/python/framework/graph_io.py
@@ -68,7 +68,8 @@ def write_graph(graph_or_graph_def, logdir, name, as_text=True):
   path = os.path.join(logdir, name)
   if as_text:
     file_io.atomic_write_string_to_file(path,
-                                        text_format.MessageToString(graph_def))
+                                        text_format.MessageToString(
+                                            graph_def, float_format=''))
   else:
     file_io.atomic_write_string_to_file(path, graph_def.SerializeToString())
   return path
