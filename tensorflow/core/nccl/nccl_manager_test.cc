fba4056bc84e51fc9b5721fec93f5e174ed2ec2d
Ayush Dubey <ayushd@google.com>
2019-05-01 18:10:47 -0700

    Add NCCL-related logging to some tests.
    PiperOrigin-RevId: 246236010

--- tensorflow/core/nccl/nccl_manager_test.cc
+++ tensorflow/core/nccl/nccl_manager_test.cc
@@ -66,6 +66,7 @@ class NcclManagerTest : public ::testing::Test {
   static void SetUpTestCase() {
     setenv("NCCL_DEBUG", "INFO", 1 /* replace */);
     setenv("NCCL_LAUNCH_MODE", "PARALLEL", 1 /* replace */);
+    setenv("TF_CPP_VMODULE", "nccl_manager=2", 1 /* replace */);
     devices_ = new std::vector<std::unique_ptr<BaseGPUDevice>>(GetGPUDevices());
     LOG(INFO) << "Running test with " << devices_->size() << " gpus";
   }
