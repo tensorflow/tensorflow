f5625dc11284d5419eb553ef0784de49d91ed7b0
frreiss <frreiss@us.ibm.com>
2019-05-03 11:13:10 -0700

    Address review comments

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -2069,7 +2069,7 @@ tf_export(
     'image.decode_image',
     v1=['io.decode_image', 'image.decode_image'])
 def decode_image(contents, channels=None, dtype=dtypes.uint8, 
-                 expand_animations=True, name=None):
+                 name=None, expand_animations=True):
   """Function for `decode_bmp`, `decode_gif`, `decode_jpeg`, and `decode_png`.
 
   Detects whether an image is a BMP, GIF, JPEG, or PNG, and performs the
@@ -2089,16 +2089,16 @@ def decode_image(contents, channels=None, dtype=dtypes.uint8,
     channels: An optional `int`. Defaults to `0`. Number of color channels for
       the decoded image.
     dtype: The desired DType of the returned `Tensor`.
-    expand_animations: Controls the shape of the returned op's output. If
-      `True`, the returned op will produce a 3-D tensor for PNG, JPEG, and 
+    name: A name for the operation (optional)
+    expand_animations: Controls the shape of the returned op's output.
+      If `True`, the returned op will produce a 3-D tensor for PNG, JPEG, and
       BMP files; and a 4-D tensor for all GIFs, whether animated or not.
       If, `False`, the returned op will produce a 3-D tensor for all file 
       types and will truncate animated GIFs to the first frame.
-    name: A name for the operation (optional)
 
   Returns:
     `Tensor` with type `dtype` and a 3- or 4-dimensional shape, depending on
-    the file type and the value of the `num_returned_dims` parameter.
+    the file type and the value of the `expand_animations` parameter.
 
   Raises:
     ValueError: On incorrect number of channels.
