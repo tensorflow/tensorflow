4b0eca97bcada35b2be038313cd5da8a0fbb95e3
Peter Hawkins <phawkins@google.com>
2019-05-07 07:02:51 -0700

    [XLA:Python] Enable asynchronous dispatch on the CPU backend.
    This has the effect of running compute kernels asynchronously in a separate thread, returning control to Python as soon as operations are dispatched.
    
    PiperOrigin-RevId: 247012747

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -159,10 +159,18 @@ class LocalBackend(Backend):
     return executable.ExecutePerReplica(per_replica_args)
 
 
+def _cpu_backend_factory():
+  return LocalBackend(platform='cpu', xla_platform_id='Host', asynchronous=True)
+
+
+def _gpu_backend_factory():
+  return LocalBackend(platform='gpu', xla_platform_id='CUDA')
+
+
 # Backend factories, keyed by user-visible name, in increasing priority order.
 _local_backend_factories = collections.OrderedDict([
-    ('cpu', lambda: LocalBackend(platform='cpu', xla_platform_id='Host')),
-    ('gpu', lambda: LocalBackend(platform='gpu', xla_platform_id='CUDA')),
+    ('cpu', _cpu_backend_factory),
+    ('gpu', _gpu_backend_factory),
 ])
 
 
