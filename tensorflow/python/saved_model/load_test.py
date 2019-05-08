f9bce1358cc36d9a81792f0e90b408d24cc129f0
Allen Lavoie <allenl@google.com>
2019-05-08 13:54:43 -0700

    Do not save get_config in checkpoints by default.
    This code has caused way too many problems to be worth it. If someone comes up with a use-case they can add it back.
    
    Fixes an issue with tf.saved_model.save + Keras Models in 1.x-style graph mode.
    
    PiperOrigin-RevId: 247283369

--- tensorflow/python/saved_model/load_test.py
+++ tensorflow/python/saved_model/load_test.py
@@ -1490,6 +1490,7 @@ class LoadTest(test.TestCase, parameterized.TestCase):
         3 * (1 + 4 + 9 + 16),
         root(constant_op.constant(3, dtype=dtypes.int64)).numpy())
 
+  @test_util.run_in_graph_and_eager_modes
   def test_dense_features_layer(self, cycles):
     columns = [feature_column_v2.numeric_column("x"),
                feature_column_v2.numeric_column("y")]
@@ -1497,7 +1498,7 @@ class LoadTest(test.TestCase, parameterized.TestCase):
     model = sequential.Sequential([layer])
     model_input = {"x": constant_op.constant([[1.]]),
                    "y": constant_op.constant([[2.]])}
-    self.assertAllClose([[1., 2.]], model.predict(model_input))
+    self.assertAllClose([[1., 2.]], model.predict(model_input, steps=1))
     loaded = self.cycle(model, cycles)
     output, = loaded._default_save_signature(model_input).values()
     self.assertAllClose([[1., 2.]], output)
