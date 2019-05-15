ea3ce16971c465fc8a069883081db2b9ece29e6f
I-Hong <ihjhuo@ibm.com>
2019-05-14 17:15:34 -0700

    clean up TFOptimizer.get_updates()

--- tensorflow/python/keras/optimizers.py
+++ tensorflow/python/keras/optimizers.py
@@ -728,29 +728,26 @@ class TFOptimizer(Optimizer, trackable.Trackable):
     return self.optimizer.compute_gradients(loss, params)
 
   def get_updates(self, loss, params):
+    # Updates list starts out empty because the iterations variable is
+    # incremented in optimizer.apply_gradients()
+    self.updates = []
     if distribution_strategy_context.has_strategy():
-      self.updates = []
-
       if not params:
         # After the model vars have been created, the second call to get_updates
         # is called with params as an empty list. This ensures that we call
         # compute_gradients with params=None.
-        grads = self.optimizer.compute_gradients(loss)
-      else:
-        grads = self.optimizer.compute_gradients(loss, params)
+        params = None
+
       global_step = training_util.get_global_step()
-      opt_update = self.optimizer.apply_gradients(grads, global_step)
     else:
       if not params:
         self.updates = [state_ops.assign_add(self.iterations, 1)]
         return self.updates
 
-      # Updates list starts out empty because the iterations variable is
-      # incremented in optimizer.apply_gradients()
-      self.updates = []
-      grads = self.optimizer.compute_gradients(loss, params)
-      opt_update = self.optimizer.apply_gradients(
-          grads, global_step=self.iterations)
+      global_step=self.iterations
+
+    grads = self.optimizer.compute_gradients(loss, params)
+    opt_update = self.optimizer.apply_gradients(grads, global_step)
 
     self.updates.append(opt_update)
     return self.updates
