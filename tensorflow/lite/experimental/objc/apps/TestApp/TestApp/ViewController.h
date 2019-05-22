2be59a5191a2d32c3e2a1e630b6335910135d7d0
A. Unique TensorFlower <gardener@tensorflow.org>
2019-05-21 22:57:55 -0700

    Add a test app for TFLite ObjC API.
    PiperOrigin-RevId: 249390128

--- tensorflow/lite/experimental/objc/apps/TestApp/TestApp/ViewController.h
+++ tensorflow/lite/experimental/objc/apps/TestApp/TestApp/ViewController.h
@@ -12,16 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
+#import <UIKit/UIKit.h>
 
-extension Data {
-  /// Creates a new buffer by copying the buffer pointer of the given array.
-  ///
-  /// - Warning: The given array's element type `T` must be trivial in that it can be copied bit
-  ///     for bit with no indirection or reference-counting operations; otherwise, reinterpreting
-  ///     data from the resulting buffer has undefined behavior.
-  /// - Parameter array: An array with elements of type `T`.
-  init<T>(copyingBufferOf array: [T]) {
-    self = array.withUnsafeBufferPointer(Data.init)
-  }
-}
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+@end
+
+NS_ASSUME_NONNULL_END
