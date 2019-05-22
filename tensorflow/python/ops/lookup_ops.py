8ddfc1cae1efb75e163cb7b5b350c0c27754ceed
A. Unique TensorFlower <gardener@tensorflow.org>
2019-05-22 11:35:05 -0700

    Automated rollback of commit 8976c6b0a48f5e9daa0e393886b7a93002edc6aa
    PiperOrigin-RevId: 249487674

--- tensorflow/python/ops/lookup_ops.py
+++ tensorflow/python/ops/lookup_ops.py
@@ -21,7 +21,6 @@ from __future__ import print_function
 import collections
 import functools
 import six
-import uuid
 
 from tensorflow.python.compat import compat as fwd_compat
 from tensorflow.python.eager import context
@@ -273,12 +272,6 @@ class StaticHashTable(InitializableLookupTableBase):
     self._initializer = initializer
     self._default_value = default_value
     self._shared_name = self._initializer._shared_name  # pylint: disable=protected-access
-    if not self._shared_name:
-      # Force using a shared name so that StaticHashTable resources can be
-      # shared across different kernels. If no "shared_name" is set and
-      # "use_node_name_sharing" is False, then each kernel gets its own local
-      # resource.
-      self._shared_name = "table_%s" % (str(uuid.uuid4()),)
     self._name = name or "hash_table"
     self._table_name = None
     super(StaticHashTable, self).__init__(default_value, initializer)
@@ -426,8 +419,7 @@ class KeyValueTensorInitializer(TableInitializerBase):
         # To maintain forward compatibiltiy, use the old implementation.
         init_op = gen_lookup_ops.initialize_table_v2(table.resource_handle,
                                                      self._keys, self._values)
-    if init_op is not None:
-      ops.add_to_collection(ops.GraphKeys.TABLE_INITIALIZERS, init_op)
+    ops.add_to_collection(ops.GraphKeys.TABLE_INITIALIZERS, init_op)
     return init_op
 
 
@@ -609,8 +601,7 @@ class TextFileInitializer(TableInitializerBase):
       init_op = gen_lookup_ops.initialize_table_from_text_file_v2(
           table.resource_handle, filename, self._key_index, self._value_index,
           -1 if self._vocab_size is None else self._vocab_size, self._delimiter)
-    if init_op is not None:
-      ops.add_to_collection(ops.GraphKeys.TABLE_INITIALIZERS, init_op)
+    ops.add_to_collection(ops.GraphKeys.TABLE_INITIALIZERS, init_op)
     # If the filename tensor is anything other than a string constant (e.g.,
     # if it is a placeholder) then it does not make sense to track it as an
     # asset.
