d5b930900d2c4af0350502c4394fbf99a14f003a
Yanan Cao <ycao@google.com>
2019-05-23 17:43:15 -0700

    Split allow_slow_and_inaccurate_ops knob into slow_ops and inaccurate_ops knobs
    PiperOrigin-RevId: 249749337

--- tensorflow/compiler/jit/mark_for_compilation_pass.cc
+++ tensorflow/compiler/jit/mark_for_compilation_pass.cc
@@ -1475,7 +1475,8 @@ bool IsCompilable(FunctionLibraryRuntime* flr, const NodeDef& ndef) {
   op_filter.allow_control_trigger = true;
   op_filter.allow_eliding_assert_and_checknumerics_ops = true;
   op_filter.allow_ops_producing_or_consuming_variant = true;
-  op_filter.allow_slow_and_inaccurate_ops = true;
+  op_filter.allow_slow_ops = true;
+  op_filter.allow_inaccurate_ops = true;
 
   return RecursiveCompilabilityChecker{&op_filter, &jit_device_type}
       .IsCompilableCall(ndef, flr);
--- tensorflow/compiler/tf2xla/xla_op_registry.h
+++ tensorflow/compiler/tf2xla/xla_op_registry.h
@@ -116,9 +116,12 @@ class XlaOpRegistry {
     // If we should cluster operations returning DT_VARIANT.
     bool cluster_variant_ops = false;
 
-    // Whether ops known to be slow or to have correctness issues should be
+    // Whether ops known to be slow should be auto-clustered.
+    bool cluster_slow_ops = false;
+
+    // Whether ops known to have numerical accuracy issues should be
     // auto-clustered.
-    bool cluster_slow_and_inaccurate_ops = false;
+    bool cluster_inaccurate_ops = false;
   };
 
   // Registers an XLA backend. `compilation_device_name` is the name of the
