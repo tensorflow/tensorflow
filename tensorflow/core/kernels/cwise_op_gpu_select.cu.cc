acbe182872ee23796ce1eac0464ac085252b8b48
Jeff Daily <jeff.daily@amd.com>
2019-05-31 19:13:19 +0000

    Add ROCm support for most cwise ops

--- tensorflow/core/kernels/cwise_op_gpu_select.cu.cc
+++ tensorflow/core/kernels/cwise_op_gpu_select.cu.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
 #define EIGEN_USE_GPU
 
@@ -129,4 +129,4 @@ SELECT_FUNCTOR(complex128);
 }  // namespace functor
 }  // namespace tensorflow
 
-#endif  // GOOGLE_CUDA
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
