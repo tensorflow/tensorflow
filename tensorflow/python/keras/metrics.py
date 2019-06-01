6119919663c763687860947ff7d285bd311982cd
Zhiheng Xiao <siauhenry@gmail.com>
2019-06-01 15:04:41 -0700

    highlight codes in the description
    of function interpolate_pr_auc

--- tensorflow/python/keras/metrics.py
+++ tensorflow/python/keras/metrics.py
@@ -1705,37 +1705,37 @@ class AUC(Metric):
 
     Note here we derive & use a closed formula not present in the paper
     as follows:
-
+      ```
       Precision = TP / (TP + FP) = TP / P
-
-    Modeling all of TP (true positive), FP (false positive) and their sum
-    P = TP + FP (predicted positive) as varying linearly within each interval
-    [A, B] between successive thresholds, we get
-
+      ```
+    Modeling all of `TP` (true positive), `FP` (false positive) and their sum
+    `P = TP + FP` (predicted positive) as varying linearly within each interval
+    `[A, B]` between successive thresholds, we get
+      ```
       Precision slope = dTP / dP
                       = (TP_B - TP_A) / (P_B - P_A)
                       = (TP - TP_A) / (P - P_A)
       Precision = (TP_A + slope * (P - P_A)) / P
-
-    The area within the interval is (slope / total_pos_weight) times
-
+      ```
+    The area within the interval is `(slope / total_pos_weight)` times
+      ```
       int_A^B{Precision.dP} = int_A^B{(TP_A + slope * (P - P_A)) * dP / P}
       int_A^B{Precision.dP} = int_A^B{slope * dP + intercept * dP / P}
-
-    where intercept = TP_A - slope * P_A = TP_B - slope * P_B, resulting in
-
+      ```
+    where `intercept = TP_A - slope * P_A = TP_B - slope * P_B`, resulting in
+      ```
       int_A^B{Precision.dP} = TP_B - TP_A + intercept * log(P_B / P_A)
-
-    Bringing back the factor (slope / total_pos_weight) we'd put aside, we get
-
+      ```
+    Bringing back the factor `(slope / total_pos_weight)` we'd put aside, we get
+      ```
       slope * [dTP + intercept *  log(P_B / P_A)] / total_pos_weight
+      ```
+    where `dTP == TP_B - TP_A`.
 
-    where dTP == TP_B - TP_A.
-
-    Note that when P_A == 0 the above calculation simplifies into
-
+    Note that when `P_A == 0` the above calculation simplifies into
+      ```
       int_A^B{Precision.dTP} = int_A^B{slope * dTP} = slope * (TP_B - TP_A)
-
+      ```
     which is really equivalent to imputing constant precision throughout the
     first bucket having >0 true positives.
 
