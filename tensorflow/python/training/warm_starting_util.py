e13539d56223f3153e62976ca7d2c28bee0ce442
Elroy Ashtian Jr <eashtianjr@users.noreply.github.com>
2019-06-01 19:49:41 -0700

    Change Parameter(s) section heading

--- tensorflow/python/training/warm_starting_util.py
+++ tensorflow/python/training/warm_starting_util.py
@@ -48,7 +48,7 @@ class VocabInfo(
   See `tf.estimator.WarmStartSettings` for examples of using
   VocabInfo to warm-start.
 
-  Attributes:
+  Args:
     new_vocab: [Required] A path to the new vocabulary file (used with the
       model to be trained).
     new_vocab_size: [Required] An integer indicating how many entries of the new
