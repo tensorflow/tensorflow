8c65ee1de5ebcb85485cb0f7e5bc008134a394e7
Thomas O'Malley <omalleyt@google.com>
2019-06-03 10:52:58 -0700

    Fix check for explicitly passed `mask` in `__call__`.
    PiperOrigin-RevId: 251265634

--- tensorflow/python/keras/engine/base_layer_utils.py
+++ tensorflow/python/keras/engine/base_layer_utils.py
@@ -18,6 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 import threading
+
 import enum
 
 from tensorflow.python.distribute import distribution_strategy_context
@@ -612,3 +613,9 @@ def mark_as_return(outputs, acd):
     # pylint: enable=protected-access
 
   return nest.map_structure(_mark_as_return, outputs)
+
+
+def default(method):
+  """Decorates a method to detect overrides in subclasses."""
+  method._is_default = True  # pylint: disable=protected-access
+  return method
