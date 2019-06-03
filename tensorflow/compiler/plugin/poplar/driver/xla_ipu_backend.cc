eb20c241128e94f8f144e375d3c39adf62ccdfb2
David Norman <davidn@graphcore.ai>
2019-06-03 14:18:45 +0100

    Don't support Qt and NonMaxSuppression

--- tensorflow/compiler/plugin/poplar/driver/xla_ipu_backend.cc
+++ tensorflow/compiler/plugin/poplar/driver/xla_ipu_backend.cc
@@ -39,6 +39,8 @@ static bool OpFilter(KernelDef* kdef) {
   if (kdef->op() == "MaxPoolGradGrad") return false;
   if (kdef->op() == "MaxPool3DGradGrad") return false;
   if (kdef->op() == "Multinomial") return false;
+  if (kdef->op() == "NonMaxSuppressionV4") return false;
+  if (kdef->op() == "Qr") return false;
   if (kdef->op() == "Real") return false;
 
   if (kdef->op() == "Const") {
