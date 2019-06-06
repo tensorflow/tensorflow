7348ee578693bb5aacdb45a1be746f0c933fc65f
A. Unique TensorFlower <gardener@tensorflow.org>
2019-06-06 00:53:20 -0700

    Add Pfor converter for SelectV2.
    PiperOrigin-RevId: 251801227

--- tensorflow/python/ops/parallel_for/pfor.py
+++ tensorflow/python/ops/parallel_for/pfor.py
@@ -2381,6 +2381,16 @@ def _convert_select(pfor_input):
   return [wrap(out, True) for x in outputs]
 
 
+@RegisterPFor("SelectV2")
+def _convert_selectv2(pfor_input):
+  pfor_input.expanddim_inputs_for_broadcast()
+  cond = pfor_input.input(0)[0]
+  t = pfor_input.input(1)[0]
+  e = pfor_input.input(2)[0]
+  out = array_ops.where_v2(cond, t, e)
+  return wrap(out, True)
+
+
 # random_ops
 
 
