806884f59d3e8c39c204d1486ffd84c5bd338d90
eashtian3 <e.ashtianjr@gatech.edu>
2019-06-06 22:04:02 -0700

    Add python syntax formatting

--- tensorflow/python/training/warm_starting_util.py
+++ tensorflow/python/training/warm_starting_util.py
@@ -70,8 +70,8 @@ class VocabInfo(
       linear weights for binary classification / regression).  An axis of 1
       could be used for warm-starting output layers with class vocabularies.
 
-      For example:
-
+      Example Usage:
+```python
       embeddings_vocab_info = tf.VocabInfo(
           new_vocab='embeddings_vocab',
           new_vocab_size=100,
@@ -100,7 +100,8 @@ class VocabInfo(
           backup_initializer=tf.compat.v1.zeros_initializer(),
           axis=0)
 
-      Currently, only axis=0 and axis=1 are supported.
+      #Currently, only axis=0 and axis=1 are supported.
+  ```
   """
 
   def __new__(cls,
