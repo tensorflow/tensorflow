011f60efb270fac5316738715767843b48d3d90b
kbhute-ibm <Koumudini.Bhute@ibm.com>
2019-06-07 14:41:42 +0530

    Update tfexample_decoder_test.py

--- tensorflow/contrib/slim/python/slim/data/tfexample_decoder_test.py
+++ tensorflow/contrib/slim/python/slim/data/tfexample_decoder_test.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+import sys
 
 from tensorflow.contrib.slim.python.slim.data import tfexample_decoder
 from tensorflow.core.example import example_pb2
