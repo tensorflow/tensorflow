e7acb1a6c621e3f033e2d15e79105d841b15ea1e
Yong Tang <yong.tang.github@outlook.com>
2019-06-09 21:43:10 +0000

    Also updates tf/keras/datasets/boston_housing.py
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/keras/datasets/boston_housing.py
+++ tensorflow/python/keras/datasets/boston_housing.py
@@ -45,7 +45,7 @@ def load_data(path='boston_housing.npz', test_split=0.2, seed=113):
       origin=origin_folder + 'boston_housing.npz',
       file_hash=
       'f553886a1f8d56431e820c5b82552d9d95cfcb96d1e678153f8839538947dff5')
-  with np.load(path) as f:
+  with np.load(path, allow_pickle=True) as f:
     x = f['x']
     y = f['y']
 
