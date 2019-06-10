8ae0ca9d0f16ad6f2ce3f364c1084469b9cf2ff8
Jiri Simsa <jsimsa@google.com>
2019-06-10 13:01:15 -0700

    [tf.data] Adding support for datasets as inputs to `from_tensors` and `from_tensor_slices` and batching and unbatching of nested datasets.
    In addition, this CL moves legacy implementation of `_RestructuredDataset` back to contrib, replacing it with one based on `_element_structure`.
    
    PiperOrigin-RevId: 252466265

--- tensorflow/python/framework/type_spec.py
+++ tensorflow/python/framework/type_spec.py
@@ -492,6 +492,21 @@ def _type_spec_from_value(value):
   if isinstance(value, composite_tensor.CompositeTensor):
     return value._type_spec  # pylint: disable=protected-access
 
+  # If `value` is a list and all of its elements can be represented by the same
+  # batchable type spec, then we can represent the entire list using a single
+  # type spec that captures the type accurately (unlike the `convert_to_tensor`
+  # fallback).
+  if isinstance(value, list) and value:
+    subspecs = [_type_spec_from_value(v) for v in value]
+    if isinstance(subspecs[0], BatchableTypeSpec):
+      merged_subspec = subspecs[0]
+      try:
+        for subspec in subspecs[1:]:
+          merged_subspec = merged_subspec.most_specific_compatible_type(subspec)
+        return merged_subspec._batch(len(subspecs))  # pylint: disable=protected-access
+      except (ValueError, TypeError):
+        pass  # incompatible subspecs
+
   for entry in reversed(_TYPE_CONVERSION_FUNCTION_REGISTRY):
     type_object, converter_fn, allow_subclass = entry
     if ((type(value) is type_object) or  # pylint: disable=unidiomatic-typecheck
