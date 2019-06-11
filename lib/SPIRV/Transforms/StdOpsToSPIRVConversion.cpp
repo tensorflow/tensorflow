fd34efe4788a43eae3db30569bdc1106889fa7d1
Mahesh Ravishankar <ravishankarm@google.com>
2019-06-11 10:47:06 -0700

    [spirv] Add a skeleton to translate standard ops into SPIR-V dialect
    PiperOrigin-RevId: 252651994

---
+++ lib/SPIRV/Transforms/StdOpsToSPIRVConversion.cpp
@@ -0,0 +1,56 @@
+//===- StdOpsToSPIRVLowering.cpp - Std Ops to SPIR-V dialect conversion ---===//
+//
+// Copyright 2019 The MLIR Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+//
+// This file implements a pass to convert MLIR standard ops into the SPIR-V
+// dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/SPIRV/Passes.h"
+#include "mlir/SPIRV/SPIRVOps.h"
+
+namespace mlir {
+#include "mlir/SPIRV/Transforms/StdOpsToSPIRVConversion.cpp.inc"
+} // namespace mlir
+
+using namespace mlir;
+
+namespace {
+/// A pass converting MLIR Standard operations into the SPIR-V dialect.
+class StdOpsToSPIRVConversionPass
+    : public FunctionPass<StdOpsToSPIRVConversionPass> {
+  void runOnFunction() override;
+};
+} // namespace
+
+void StdOpsToSPIRVConversionPass::runOnFunction() {
+  OwningRewritePatternList patterns;
+  auto &func = getFunction();
+
+  populateWithGenerated(func.getContext(), &patterns);
+  applyPatternsGreedily(func, std::move(patterns));
+}
+
+FunctionPassBase *mlir::spirv::createStdOpsToSPIRVConversionPass() {
+  return new StdOpsToSPIRVConversionPass();
+}
+
+static PassRegistration<StdOpsToSPIRVConversionPass>
+    pass("std-to-spirv", "Convert Standard Ops to SPIR-V dialect");
---
+++ test/SPIRV/standard_ops_to_spirv.mlir
@@ -0,0 +1,46 @@
+// RUN: mlir-opt -std-to-spirv %s -o - | FileCheck %s
+
+// CHECK-LABEL: @fmul_scalar
+func @fmul_scalar(%arg: f32) -> f32 {
+  // CHECK: spv.FMul
+  %0 = mulf %arg, %arg : f32
+  return %0 : f32
+}
+
+// CHECK-LABEL: @fmul_vector2
+func @fmul_vector2(%arg: vector<2xf32>) -> vector<2xf32> {
+  // CHECK: spv.FMul
+  %0 = mulf %arg, %arg : vector<2xf32>
+  return %0 : vector<2xf32>
+}
+
+// CHECK-LABEL: @fmul_vector3
+func @fmul_vector3(%arg: vector<3xf32>) -> vector<3xf32> {
+  // CHECK: spv.FMul
+  %0 = mulf %arg, %arg : vector<3xf32>
+  return %0 : vector<3xf32>
+}
+
+// CHECK-LABEL: @fmul_vector4
+func @fmul_vector4(%arg: vector<4xf32>) -> vector<4xf32> {
+  // CHECK: spv.FMul
+  %0 = mulf %arg, %arg : vector<4xf32>
+  return %0 : vector<4xf32>
+}
+
+// CHECK-LABEL: @fmul_vector5
+func @fmul_vector5(%arg: vector<5xf32>) -> vector<5xf32> {
+  // Vector length of only 2, 3, and 4 is valid for SPIR-V
+  // CHECK: mulf
+  %0 = mulf %arg, %arg : vector<5xf32>
+  return %0 : vector<5xf32>
+}
+
+// CHECK-LABEL: @fmul_tensor
+func @fmul_tensor(%arg: tensor<4xf32>) -> tensor<4xf32> {
+  // For tensors mulf cannot be lowered directly to spv.FMul
+  // CHECK: mulf
+  %0 = mulf %arg, %arg : tensor<4xf32>
+  return %0 : tensor<4xf32>
+}
+
