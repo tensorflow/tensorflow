12f84c5499da1dd751e902c4cf5ddbc1c385e951
Vishnuvardhan Janapati <46058173+jvishnuvardhan@users.noreply.github.com>
2019-06-13 12:10:10 -0700

    Added couple of examples for tf.zeros_like

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -2255,7 +2255,15 @@ def zeros_like_v2(
 
   ```python
   tensor = tf.constant([[1, 2, 3], [4, 5, 6]])
-  tf.zeros_like(tensor)  # [[0, 0, 0], [0, 0, 0]]
+  tf.zeros_like(tensor)  # [[0, 0, 0], [0, 0, 0]] with dtype=int32
+  
+  If dtype of input `tensor` is `float32`, then the output is also of `float32`
+  tensor = tf.constant([[1.0, 2.0, 3.0], [4, 5, 6]])
+  tf.zeros_like(tensor)  # [[0., 0., 0.], [0., 0., 0.]] with dtype=floa32
+  
+  If you want to specify desired dtype of output `tensor`, then specify it in the op 
+  tensor = tf.constant([[1.0, 2.0, 3.0], [4, 5, 6]])
+  tf.zeros_like(tensor,dtype=tf.int32)  # [[0, 0, 0], [0, 0, 0]] with dtype=int32
   ```
 
   Args:
