52ff45ab004e9fd795713207c70fd7a1c1ada911
Niels Ole Salscheider <niels_ole@salscheider-online.de>
2019-06-15 02:00:53 +0200

    grappler: Also recompute LeakyRelu

--- tensorflow/core/grappler/optimizers/memory_optimizer.cc
+++ tensorflow/core/grappler/optimizers/memory_optimizer.cc
@@ -59,11 +59,11 @@ const char* kRecomputeHint = "_recompute_hint";
 // TODO(allenl): Replace this list with a cost model.
 std::unordered_set<string> GetCheapToRecomputeOps() {
   std::unordered_set<string> cheap_ops = {
-      "Add",      "AddN",       "BiasAdd",        "Cast",   "Fill",
-      "FloorDiv", "FloorMod",   "FusedBatchNorm", "Mul",    "Neg",
-      "RealDiv",  "Reciprocal", "Relu",           "Relu6",  "Reshape",
-      "Rsqrt",    "Sigmoid",    "Sqrt",           "Square", "SquaredDifference",
-      "Sub",      "Tile",       "Transpose"};
+      "Add",               "AddN",     "BiasAdd",        "Cast",      "Fill",
+      "FloorDiv",          "FloorMod", "FusedBatchNorm", "LeakyRelu", "Mul",
+      "Neg",               "RealDiv",  "Reciprocal",     "Relu",      "Relu6",
+      "Reshape",           "Rsqrt",    "Sigmoid",        "Sqrt",      "Square",
+      "SquaredDifference", "Sub",      "Tile",           "Transpose"};
   return cheap_ops;
 }
 
