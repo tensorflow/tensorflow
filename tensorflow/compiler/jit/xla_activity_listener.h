726ef878b70b8252edc084a971d48c94a241b5cd
Sanjoy Das <sanjoy@google.com>
2019-06-18 11:44:25 -0700

    Delete the global process id and associated complexity
    We don't need it right now, and we can add it back later when necessary.
    
    PiperOrigin-RevId: 253834326

--- tensorflow/compiler/jit/xla_activity_listener.h
+++ tensorflow/compiler/jit/xla_activity_listener.h
@@ -47,26 +47,6 @@ class XlaActivityListener {
 // Registers an `XlaActivityListener`, which will be invoked on all subsequent
 // `BroadcastXlaActivity` calls.
 void RegisterXlaActivityListener(std::unique_ptr<XlaActivityListener> listener);
-
-using GlobalProcessIdMaker = std::function<std::string()>;
-
-// Installs `global_process_id_maker` as a "global process id" maker.
-//
-// The value returned by the global process ID maker, if one is installed, is
-// stored in the global_process_id field of the Xla*Activity messages before
-// they're fed to the registered activity listeners.  If no ID maker is
-// installed then global_process_id is set to "unknown".
-//
-// `global_process_id_maker` must be thread safe.
-//
-// The global process id maker is used to tag *Activity messages to so that the
-// broadcasting process can be uniquely identified.  Therefore the global
-// process id maker
-//
-//  - Must always return the same value within the same process.
-//  - Cannot be set or changed after we have broadcasted any XLA activity.
-void SetGlobalProcessIdMaker(GlobalProcessIdMaker global_process_id_maker);
-
 }  // namespace tensorflow
 
 #endif  // TENSORFLOW_COMPILER_JIT_XLA_ACTIVITY_LISTENER_H_
