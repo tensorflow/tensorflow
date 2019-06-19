bea971a0e0aa318d6945d03ca9fc9107cd0c7fef
Roman Derco <romand@graphcore.ai>
2019-06-19 10:12:31 +0100

    memoization of convolution parameters
    Test Plan: run all tests.
    
    Reviewers: davidn, georgep, stephenm, jakeh
    
    Reviewed By: davidn, georgep, jakeh
    
    Maniphest Tasks: T6423
    
    Differential Revision: https://phabricator.sourcevertex.net/D12086

---
+++ tensorflow/compiler/plugin/poplar/driver/tools/convolution_preplanning.cc
@@ -0,0 +1,71 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/compiler/plugin/poplar/driver/tools/convolution_preplanning.h"
+#include <poplar/Target.hpp>
+#include "tensorflow/compiler/plugin/poplar/driver/ops/conv_graph_caching.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/matcher_predicates.h"
+#include "tensorflow/compiler/plugin/poplar/driver/tools/util.h"
+
+namespace xla {
+namespace poplarplugin {
+
+/*
+ * Visit all non-fused operations in the whole module looking for convolutions,
+ * and add the parameters and the options for that convolution to the set
+ *  of things to pass to the poplibs convolution pre-planner.
+ */
+
+Status ConvolutionPreplanning::Plan(const HloModule* module,
+                                    CompilerResources& resources) {
+  preplan_convs.clear();
+  option_flags_store.clear();
+
+  for (auto* comp : module->computations()) {
+    if (!IsPopOpsFusion(comp)) {
+      for (HloInstruction* inst : comp->instructions()) {
+        if (inst->opcode() == HloOpcode::kConvolution) {
+          TF_RETURN_IF_ERROR(StorePreplanConv(inst, resources, 0, 1));
+        } else if (IsPopOpsConvolution(inst)) {
+          TF_RETURN_IF_ERROR(StorePreplanConv(inst, resources, 0, 1));
+        } else if (IsPopOpsFusion(inst, "conv_scaled_inplace")) {
+          TF_RETURN_IF_ERROR(StorePreplanConv(inst, resources, 1, 2));
+        }
+      }
+    }
+  }
+
+  poplin::preplanConvolutions(preplan_convs, resources.convolution_cache);
+
+  return Status::OK();
+}
+
+Status ConvolutionPreplanning::StorePreplanConv(
+    const HloInstruction* inst, const CompilerResources& resources,
+    int64 input_index, int64 kernel_index) {
+  auto& target = resources.main_graph.getTarget();
+  TF_ASSIGN_OR_RETURN(
+      poplin::ConvParams conv_params,
+      GetConvolutionParameters(inst, input_index, kernel_index));
+  auto conv_type = GetConvClassificationType(inst, resources.annotations);
+  poplar::OptionFlags option_flags = resources.default_conv_options;
+  option_flags.set("pass", ConvClassificationTypeToString(conv_type));
+  option_flags_store.emplace_back(option_flags);
+  preplan_convs.insert(
+      std::make_tuple(&target, conv_params, &option_flags_store.back()));
+  return Status::OK();
+}
+
+}  // namespace poplarplugin
+}  // namespace xla
---
+++ tensorflow/compiler/plugin/poplar/driver/tools/convolution_preplanning.h
@@ -0,0 +1,53 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_CONVOLUTION_PREPLANNING_H_
+#define TENSORFLOW_COMPILER_PLUGIN_POPLAR_DRIVER_TOOLS_CONVOLUTION_PREPLANNING_H_
+
+#include <list>
+#include <set>
+#include <tuple>
+
+#include "tensorflow/compiler/plugin/poplar/driver/compiler_resources.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
+
+namespace xla {
+namespace poplarplugin {
+
+/**
+ * Memoization of convolution parameters.
+ */
+class ConvolutionPreplanning {
+ public:
+  Status Plan(const HloModule* module, CompilerResources& resources);
+
+ private:
+  // Store convolution parameters.
+  std::set<std::tuple<const poplar::Target*, const poplin::ConvParams,
+                      const poplar::OptionFlags*>>
+      preplan_convs;
+
+  // OptionsFlags storage location.
+  std::list<poplar::OptionFlags> option_flags_store;
+
+  Status StorePreplanConv(const HloInstruction* inst,
+                          const CompilerResources& resources, int64 input_index,
+                          int64 kernel_index);
+};
+
+}  // namespace poplarplugin
+}  // namespace xla
+
+#endif
