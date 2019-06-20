3c8189eddd07b70b8af3f98c048c0ebe5e7415fe
Lukas Folle <lukas.folle@fau.de>
2019-06-20 10:39:52 +0200

    Added two more examples.

--- tensorflow/python/keras/backend.py
+++ tensorflow/python/keras/backend.py
@@ -2831,6 +2831,17 @@ def flatten(x):
 
   Returns:
       A tensor, reshaped into 1-D
+
+  Example:
+      ```python
+        >>> b = tf.constant([[1, 2], [3, 4]])
+        >>> b
+        <tf.Tensor: id=102, shape=(2, 2), dtype=int32, numpy=
+        array([[1, 2],
+               [3, 4]], dtype=int32)>
+        >>> tf.keras.backend.flatten(b)
+        <tf.Tensor: id=105, shape=(4,), dtype=int32, numpy=array([1, 2, 3, 4], dtype=int32)>
+      ```
   """
   return array_ops.reshape(x, [-1])
 
@@ -2992,6 +3003,19 @@ def stack(x, axis=0):
 
   Returns:
       A tensor.
+
+  Example:
+      ```python
+        >>> a = tf.constant([[1, 2],[3, 4]])
+        >>> b = tf.constant([[10, 20],[30, 40]])
+        >>> tf.keras.backend.stack((a, b))
+        <tf.Tensor: id=146, shape=(2, 2, 2), dtype=int32, numpy=
+        array([[[ 1,  2],
+                [ 3,  4]],
+        
+               [[10, 20],
+                [30, 40]]], dtype=int32)>
+      ```
   """
   return array_ops.stack(x, axis=axis)
 
