5dffeea3bc693b55e6a2337a86275cd7a3aa5df0
saishruthi <saishruthi.tn@ibm.com>
2019-06-19 23:07:26 -0700

    pylint update

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -3040,8 +3040,10 @@ def bincount(arr,
   ```
   Vector length = Maximum element in vector `values` is 5. Adding 1, which is 6
                   will be the vector length.
-  Each bin value in the output indicates number of occurrences of the particular index.
-  Here, index 1 in output has a value 2. This indicates value 1 occurs two times in `values`.
+
+  Each bin value in the output indicates number of occurrences of the particular
+  index. Here, index 1 in output has a value 2. This indicates value 1 occurs
+  two times in `values`.
 
   ```python
   values = tf.constant([1,1,2,3,2,4,4,5])
@@ -3049,8 +3051,8 @@ def bincount(arr,
   tf.math.bincount(values, weights=weights) #[0 6 0 1 9 5]
   ```
   Bin will be incremented by the corresponding weight instead of 1.
-  Here, index 1 in output has a value 6. This is the summation of weights corresponding
-  to the value in `values`.
+  Here, index 1 in output has a value 6. This is the summation of weights
+  corresponding to the value in `values`.
 
   Args:
     arr: An int32 tensor of non-negative values.
