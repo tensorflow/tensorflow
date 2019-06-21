d1381b788c0593a35b6169d5637bb6679f07de55
David Norman <davidn@graphcore.ai>
2019-06-21 15:58:26 +0100

    Fix ordering out output of sort operations

--- tensorflow/compiler/plugin/poplar/driver/ops/sort_ops.cc
+++ tensorflow/compiler/plugin/poplar/driver/ops/sort_ops.cc
@@ -8,6 +8,7 @@
 
 #include "tensorflow/compiler/xla/service/hlo_casting_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_instructions.h"
+#include "tensorflow/compiler/xla/service/hlo_query.h"
 
 #include <popops/ElementWise.hpp>
 #include <popops/Sort.hpp>
@@ -15,10 +16,49 @@
 namespace xla {
 namespace poplarplugin {
 
+namespace {
+bool IsSimpleComparison(const HloInstruction* inst) {
+  HloInstruction* root(inst->to_apply()->root_instruction());
+  if (!hlo_query::AllOperandsAreParameters(*root)) {
+    return false;
+  }
+
+  if (root->opcode() != HloOpcode::kCompare) {
+    return false;
+  }
+
+  switch (root->comparison_direction()) {
+    case ComparisonDirection::kGe:
+    case ComparisonDirection::kGt:
+    case ComparisonDirection::kLe:
+    case ComparisonDirection::kLt:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool ReverseSortOutput(const HloInstruction* inst) {
+  HloInstruction* root(inst->to_apply()->root_instruction());
+  switch (root->comparison_direction()) {
+    case ComparisonDirection::kGe:
+    case ComparisonDirection::kGt:
+      return true;
+    default:
+      return false;
+  }
+}
+}
+
 StatusOr<poplar::program::Program> CreateSort(CompilerResources& res,
                                               const HloInstruction* inst,
                                               TensorMap& tensor_map) {
   const HloSortInstruction* sort = Cast<HloSortInstruction>(inst);
+
+  if (!IsSimpleComparison(inst)) {
+    return xla::Unimplemented(
+        "Current Sort implementation only supports GT/LT/GE/LE comparisons");
+  }
 
   poplar::Graph& graph = GetGraph(res, inst);
 
@@ -33,6 +73,10 @@ StatusOr<poplar::program::Program> CreateSort(CompilerResources& res,
 
     popops::sortInPlace(graph, to_sort, sort->dimensions(0), prog);
 
+    if (ReverseSortOutput(inst)) {
+      TF_ASSIGN_OR_RETURN(to_sort, ReverseTensor(to_sort, sort->dimensions()));
+    }
+
     TF_CHECK_OK(AddOutputTensor(tensor_map, inst, 0, to_sort));
   } else if (sort->operand_count() == 2) {
     CHECK_EQ(inputs.size(), 2);
@@ -42,6 +86,11 @@ StatusOr<poplar::program::Program> CreateSort(CompilerResources& res,
     poplar::Tensor value = inputs[1][0];
 
     popops::sortKeyValueInPlace(graph, key, value, sort->dimensions(0), prog);
+
+    if (ReverseSortOutput(inst)) {
+      TF_ASSIGN_OR_RETURN(key, ReverseTensor(key, sort->dimensions()));
+      TF_ASSIGN_OR_RETURN(value, ReverseTensor(value, sort->dimensions()));
+    }
 
     TF_CHECK_OK(AddOutputTensor(tensor_map, inst, 0, key));
 
