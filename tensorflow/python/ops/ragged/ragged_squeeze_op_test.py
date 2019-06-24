9962e5a163ef8972fb6fd2fcd0798662f9510607
Mehrdad Khatir <khatir@google.com>
2019-06-24 08:55:14 -0700

    fix the name in the docstring.
    PiperOrigin-RevId: 254767039

--- tensorflow/python/ops/ragged/ragged_squeeze_op_test.py
+++ tensorflow/python/ops/ragged/ragged_squeeze_op_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for ragged.size."""
+"""Tests for ragged.squeeze."""
 
 from __future__ import absolute_import
 from __future__ import division
