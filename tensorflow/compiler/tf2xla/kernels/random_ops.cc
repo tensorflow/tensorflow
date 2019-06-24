4b175e77a5176b798fa7a71a62bc92d01f03d7e6
David Norman <davidn@graphcore.ai>
2019-06-24 10:13:32 +0100

    Add half as supported type for TruncatedNormal op definition

--- tensorflow/compiler/tf2xla/kernels/random_ops.cc
+++ tensorflow/compiler/tf2xla/kernels/random_ops.cc
@@ -293,6 +293,7 @@ class TruncatedNormalOp : public XlaOpKernel {
 
 REGISTER_XLA_OP(Name("TruncatedNormal")
                     .CompileTimeConstantInput("shape")
+                    .TypeConstraint("dtype", DT_HALF)
                     .TypeConstraint("dtype", DT_FLOAT),
                 TruncatedNormalOp);
 
