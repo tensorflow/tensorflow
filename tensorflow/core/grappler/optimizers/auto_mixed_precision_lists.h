a5c1e0f86fab69709f48d5a060eb927d609fa15f
Ben Barsdell <bbarsdell@nvidia.com>
2019-06-28 16:00:18 -0700

    Add BatchMatMulV2 to auto_mixed_precision whitelist

--- tensorflow/core/grappler/optimizers/auto_mixed_precision_lists.h
+++ tensorflow/core/grappler/optimizers/auto_mixed_precision_lists.h
@@ -63,6 +63,7 @@ class AutoMixedPrecisionLists {
     auto list = gtl::FlatSet<string> {
 #if CUDA_VERSION >= 9010  // Fp16 BatchMatMul is slow before CUDA 9.1.
       "BatchMatMul",
+      "BatchMatMulV2",
 #endif
           "BlockLSTM", "BlockLSTMGrad", "Conv2D", "Conv2DBackpropFilter",
           "Conv2DBackpropInput",
