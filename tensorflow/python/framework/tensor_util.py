c4f40aea1d4f916aa3dfeb79f024c495ac609106
Martin Wicke <wicke@google.com>
2019-07-02 05:53:30 -0700

    Fix shape tensor conversion for shapes containing both v1 and v2 dimensions. This can occur in partially converted code, and will currently lead to a conversion error.
    Move the previously private utility function doing (known) shape to Tensor conversion from random_ops to tensor_util.py, and add a test.
    
    PiperOrigin-RevId: 256148834

--- tensorflow/python/framework/tensor_util.py
+++ tensorflow/python/framework/tensor_util.py
@@ -959,3 +959,18 @@ def is_tensor(x):  # pylint: disable=invalid-name
   return (isinstance(x, tensor_like._TensorLike) or  # pylint: disable=protected-access
           ops.is_dense_tensor_like(x) or
           getattr(x, "is_tensor_like", False))
+
+
+def shape_tensor(shape):  # pylint: disable=invalid-name
+  """Convert to an int32 or int64 tensor, defaulting to int32 if empty."""
+  dtype = None
+  if isinstance(shape, (tuple, list)):
+    if not shape:
+      dtype = dtypes.int32
+    else:
+      # If there are Dimension objects in the shape, unwrap them. This can be a
+      # problem if v1 and v2 TensorShape objects get mixed up in partial
+      # conversions, leading to shapes such as (1, 2, Dimension(5)), which are
+      # not convertible to Tensors becasue of mixed content.
+      shape = tuple(map(tensor_shape.dimension_value, shape))
+  return ops.convert_to_tensor(shape, dtype=dtype, name="shape")
