227049934318e0edec13f65ee3fe558b02e2b82a
Wen-Heng (Jack) Chung <whchung@gmail.com>
2019-07-02 19:33:58 +0000

    Use the name of nvptx_backend_lib for now

--- tensorflow/compiler/xla/service/gpu/amdgpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/amdgpu_compiler.cc
@@ -57,7 +57,7 @@ limitations under the License.
 #include "tensorflow/compiler/xla/service/gpu/ir_emission_utils.h"
 #include "tensorflow/compiler/xla/service/gpu/ir_emitter_context.h"
 #include "tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.h"
-#include "tensorflow/compiler/xla/service/gpu/llvm_gpu_backend/gpu_backend_lib.h"
+#include "tensorflow/compiler/xla/service/gpu/llvm_gpu_backend/nvptx_backend_lib.h"
 #include "tensorflow/compiler/xla/service/gpu/miopen_conv_algorithm_picker.h"
 #include "tensorflow/compiler/xla/service/gpu/multi_output_fusion.h"
 #include "tensorflow/compiler/xla/service/gpu/partition_assignment.h"
