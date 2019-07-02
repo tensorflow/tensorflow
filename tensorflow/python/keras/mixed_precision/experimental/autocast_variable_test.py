1ec90f6a7b8b4aa6a8ed9a596c0803406b4b7460
Reed Wanderman-Milne <reedwm@google.com>
2019-07-01 17:30:06 -0700

    Fix issue where AutoCastVariables wouldn't cast in tf.functions.
    The problem is we previously used an attribute on the default. graph to specify the dtype AutoCastVariables would be cast to. But, tf.functions introduce a new default graph. We could have modified tf.function to inherit the attribute from the default graph, which is what DistributionStrategy does. But instead I chose to remove the graph attribute, and add a "_read_dtype" attribute to AutoCastVariable.
    
    I had to move autocast_context_manager from base_layer_utils.py to the newly-added file casting_utils.py in order to avoid a circular dependency. autocast_context_manager now uses AutoCastVariable, which also causes loss_scale_optimizer.py to be imported due to the __init__.py file, which imports optimizer_v2.py, which imports base_layer_utils.py.
    
    PiperOrigin-RevId: 256067954

--- tensorflow/python/keras/mixed_precision/experimental/autocast_variable_test.py
+++ tensorflow/python/keras/mixed_precision/experimental/autocast_variable_test.py
@@ -25,7 +25,6 @@ import numpy as np
 from tensorflow.python.distribute import mirrored_strategy
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import ops
 from tensorflow.python.framework import test_util
 from tensorflow.python.keras.mixed_precision.experimental import autocast_variable
 
@@ -80,47 +79,20 @@ class AutoCastVariableTest(test.TestCase, parameterized.TestCase):
       x = get_autocast_var(x, distribute)
       self.evaluate(x.initializer)
 
-      # outside of auto cast scope.
+      # _read_dtype is same dtype as variable
       self.assertEqual(x.dtype, dtypes.float32)
+      self.assertEqual(x.true_dtype, dtypes.float32)
       self.assertEqual(x.value().dtype, dtypes.float32)
       self.assertEqual(x.read_value().dtype, dtypes.float32)
       self.assertEqual(array_ops.identity(x).dtype, dtypes.float32)
 
-      # within auto cast scope of different dtype
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        self.assertEqual(x.dtype, dtypes.float16)
-        self.assertEqual(x.value().dtype, dtypes.float16)
-        self.assertEqual(x.read_value().dtype, dtypes.float16)
-        self.assertEqual(array_ops.identity(x).dtype, dtypes.float16)
-
-      # within auto cast scope of same dtype
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float32):
-        self.assertEqual(x.dtype, dtypes.float32)
-        self.assertEqual(x.value().dtype, dtypes.float32)
-        self.assertEqual(x.read_value().dtype, dtypes.float32)
-        self.assertEqual(array_ops.identity(x).dtype, dtypes.float32)
-
-  @parameterized.named_parameters(*TESTCASES)
-  def test_read_nested_scopes(self, distribute):
-    with get_distribute_scope(distribute):
-      x = get_var(1., dtypes.float32)
-      x = get_autocast_var(x, distribute)
-      self.evaluate(x.initializer)
-
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        self.assertEqual(x.dtype, dtypes.float16)
-        self.assertEqual(x.read_value().dtype, dtypes.float16)
-
-        with ops.get_default_graph()._enable_auto_casting_variables(
-            dtypes.float32):
-          self.assertEqual(x.dtype, dtypes.float32)
-          self.assertEqual(x.read_value().dtype, dtypes.float32)
-
-        self.assertEqual(x.dtype, dtypes.float16)
-        self.assertEqual(x.read_value().dtype, dtypes.float16)
+      # Setting _read_dtype to a different dtype
+      x._read_dtype = dtypes.float16
+      self.assertEqual(x.dtype, dtypes.float16)
+      self.assertEqual(x.true_dtype, dtypes.float32)
+      self.assertEqual(x.value().dtype, dtypes.float16)
+      self.assertEqual(x.read_value().dtype, dtypes.float16)
+      self.assertEqual(array_ops.identity(x).dtype, dtypes.float16)
 
   @parameterized.named_parameters(*TESTCASES)
   def test_operator_overloads(self, distribute):
@@ -140,16 +112,15 @@ class AutoCastVariableTest(test.TestCase, parameterized.TestCase):
     if not distribute:
       self.assertEqual(self.evaluate(x + v1), 3.)
 
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        self.assertEqual(self.evaluate(x + v2), 3.)
+      x._read_dtype = dtypes.float16
+      self.assertEqual(self.evaluate(x + v2), 3.)
 
     # Test variable as the RHS
+    x._read_dtype = dtypes.float32
     self.assertEqual(self.evaluate(v1 + x), 3.)
 
-    with ops.get_default_graph()._enable_auto_casting_variables(
-        dtypes.float16):
-      self.assertEqual(self.evaluate(v2 + x), 3.)
+    x._read_dtype = dtypes.float16
+    self.assertEqual(self.evaluate(v2 + x), 3.)
 
   @parameterized.named_parameters(*TESTCASES)
   def test_assign(self, distribute):
@@ -158,7 +129,6 @@ class AutoCastVariableTest(test.TestCase, parameterized.TestCase):
       x = get_autocast_var(x, distribute)
       self.evaluate(x.initializer)
 
-      # outside of auto cast scope.
       v1 = constant_op.constant(3.14, dtype=dtypes.float32)
       v2 = constant_op.constant(3.14, dtype=dtypes.float16)
 
@@ -190,37 +160,37 @@ class AutoCastVariableTest(test.TestCase, parameterized.TestCase):
       run_and_check()
       # reset x
       self.evaluate(x.assign(0.))
-      # within auto cast scope.
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        # assign still expect float32 value even if in float16 scope
-        run_and_check()
+      x._read_dtype = dtypes.float16
+      # assign still expect float32 value even _read_dtype is float16
+      run_and_check()
 
   @parameterized.named_parameters(*TESTCASES)
   def test_assign_stays_in_true_dtype(self, distribute):
     with get_distribute_scope(distribute):
       x = get_var(1., dtypes.float32)
       x = get_autocast_var(x, distribute)
+      x._read_dtype = dtypes.float16
       self.evaluate(x.initializer)
       # small_val is a value such that 1.0 + small_val == 1.0 in fp16, but not
       # in fp32
       small_val = np.finfo('float16').eps / 2
       small_tensor = constant_op.constant(small_val, dtype=dtypes.float32)
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        # Variable should be increased, despite it appearing to be the same
-        # float16 value.
-        self.assertEqual(1. + small_val,
-                         self.evaluate(x.assign(1. + small_tensor)))
-        self.assertEqual(1., self.evaluate(x.value()))
+      # Variable should be increased, despite it appearing to be the same
+      # float16 value.
+      self.assertEqual(1. + small_val,
+                       self.evaluate(x.assign(1. + small_tensor)))
+      self.assertEqual(1., self.evaluate(x.value()))
+
+      x._read_dtype = dtypes.float32
       self.assertEqual(1. + small_val, self.evaluate(x.value()))
 
+      x._read_dtype = dtypes.float16
       self.evaluate(x.assign(1.))
-      with ops.get_default_graph()._enable_auto_casting_variables(
-          dtypes.float16):
-        self.assertEqual(1. + small_val,
-                         self.evaluate(x.assign_add(small_tensor)))
-        self.assertEqual(1., self.evaluate(x.value()))
+      self.assertEqual(1. + small_val,
+                       self.evaluate(x.assign_add(small_tensor)))
+      self.assertEqual(1., self.evaluate(x.value()))
+
+      x._read_dtype = dtypes.float32
       self.assertEqual(1. + small_val, self.evaluate(x.value()))
 
   @parameterized.named_parameters(*TESTCASES)
