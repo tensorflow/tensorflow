1d1a7a3425fd558a02c93bc49a1bb392f0871c40
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-02 07:56:32 -0700

    Add unidirectional sequence LSTM support to the NNAPI delegate
    PiperOrigin-RevId: 256164309

--- tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
@@ -154,7 +154,8 @@ bool IsHybridOperator(const TfLiteContext* context, int builtin_code,
       const TfLiteType filter_type = context->tensors[filter_id].type;
       return IsFloat(input_type) && IsQuantized(filter_type);
     }
-    case kTfLiteBuiltinLstm: {
+    case kTfLiteBuiltinLstm:
+    case kTfLiteBuiltinUnidirectionalSequenceLstm: {
       const int input_id = node->inputs->data[0];
       // Input #1 is optional so use #2 to determine if hybrid.
       const int weights_id = node->inputs->data[2];
@@ -1123,6 +1124,47 @@ class NNAPIDelegateKernel {
                 builtin->num_squeeze_dims ? builtin->squeeze_dims : nullptr,
                 static_cast<uint32_t>(builtin->num_squeeze_dims));
             return ANEURALNETWORKS_SQUEEZE;
+          };
+        }
+        break;
+      case kTfLiteBuiltinUnidirectionalSequenceLstm:
+        if (version == 1 && android_sdk_version >= kMinSdkVersionForNNAPI12) {
+          if (IsHybridOperator(context, builtin_code, node)) {
+            // Hybrid version of this op is not supported by NN API.
+            return nullptr;
+          }
+          if (node->inputs->size != 20 && node->inputs->size != 24) {
+            return nullptr;
+          }
+          return [](const NNAPIOpMappingArgs& mapping_args)
+                     -> ANeuralNetworksOperationType {
+            auto builtin =
+                reinterpret_cast<TfLiteUnidirectionalSequenceLSTMParams*>(
+                    mapping_args.node->builtin_data);
+            mapping_args.builder->AddScalarInt32Operand(builtin->activation);
+            mapping_args.builder->AddScalarFloat32Operand(builtin->cell_clip);
+            mapping_args.builder->AddScalarFloat32Operand(builtin->proj_clip);
+            mapping_args.builder->AddScalarBoolOperand(builtin->time_major);
+            const bool hybrid_op = IsHybridOperator(
+                mapping_args.context, kTfLiteBuiltinUnidirectionalSequenceLstm,
+                mapping_args.node);
+            if (mapping_args.node->inputs->size == 24) {
+              // Add layer normalization tensors if they are provided.
+              for (int i = 20; i < 24; ++i) {
+                const int input_index = mapping_args.node->inputs->data[i];
+                if (input_index != kOptionalTensor) {
+                  mapping_args.builder->AddTensorInput(input_index, hybrid_op);
+                } else {
+                  mapping_args.builder->AddVectorFloat32Operand(nullptr, 0);
+                }
+              }
+            } else {
+              for (int i = 0; i < 4; ++i) {
+                mapping_args.builder->AddVectorFloat32Operand(nullptr, 0);
+              }
+            }
+
+            return ANEURALNETWORKS_UNIDIRECTIONAL_SEQUENCE_LSTM;
           };
         }
         break;
@@ -2184,6 +2226,19 @@ class NNAPIDelegateKernel {
           // normalization weights are the last four inputs of the LSTM op in
           // NNAPI.
           continue;
+        }
+        if (reg->builtin_code == kTfLiteBuiltinUnidirectionalSequenceLstm) {
+          if (input_pos >= 20) {
+            // Skip layer normalization weights. They are added in the Map
+            // function (after all the other inputs added there) since layer
+            // normalization weights are the last four inputs of the
+            // unidirectional sequence LSTM op in NNAPI.
+            continue;
+          }
+          if (input_index == kOptionalTensor) {
+            TF_LITE_ENSURE_STATUS(builder.AddVectorFloat32Operand(nullptr, 0));
+            continue;
+          }
         }
         // Pad and Padv2 have an optional parameter for a pad value which has
         // to be converted to a scalar type in NN API.
