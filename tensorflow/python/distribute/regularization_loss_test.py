00f9929d25887e65b187594d6773c47866486040
wangsiyu <siyu.wsy@gmail.com>
2019-07-04 17:15:33 +0800

    Code refine

--- tensorflow/python/distribute/regularization_loss_test.py
+++ tensorflow/python/distribute/regularization_loss_test.py
@@ -30,21 +30,6 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import test
 
 
-def _create_checkpoints(sess, checkpoint_dir):
-  checkpoint_prefix = os.path.join(checkpoint_dir, "model")
-  checkpoint_state_name = "checkpoint"
-  v1 = variable_scope.get_variable("var1", [1, 10])
-  v2 = variable_scope.get_variable("var2", [10, 10])
-  sess.run(variables.global_variables_initializer())
-  v1_value, v2_value = sess.run([v1, v2])
-  saver = saver_lib.Saver()
-  saver.save(
-      sess,
-      checkpoint_prefix,
-      global_step=0,
-      latest_filename=checkpoint_state_name)
-  return v1_value, v2_value
-
 class RegularizationWithDistributionStrategyTest(
     test.TestCase, parameterized.TestCase):
 
@@ -58,30 +43,20 @@ class RegularizationWithDistributionStrategyTest(
           distribution=[
               strategy_combinations.default_strategy,
               strategy_combinations.one_device_strategy,
-          ],
-          mode=["graph"]))
-  def testOneReplicaWithLayers(self, distribution):
-    with distribution.scope():
-      distribution.extended.call_for_each_replica(self.model_fn)
-      reg_list = ops.get_collection(ops.GraphKeys.REGULARIZATION_LOSSES)
-      self.assertEqual(len(reg_list), 1)
-      for reg_loss in reg_list:
-        self.assertTrue(isinstance(reg_loss, ops.Tensor))
-
-  @combinations.generate(
-      combinations.combine(
-          distribution=[
               strategy_combinations.mirrored_strategy_with_gpu_and_cpu,
               strategy_combinations.mirrored_strategy_with_two_gpus,
           ],
           mode=["graph"]))
-  def testMoreThanOneReplicasWithLayers(self, distribution):
+  def testWithLayers(self, distribution):
     with distribution.scope():
       distribution.extended.call_for_each_replica(self.model_fn)
       reg_list = ops.get_collection(ops.GraphKeys.REGULARIZATION_LOSSES)
       self.assertEqual(len(reg_list), 1)
       for reg_loss in reg_list:
-        self.assertTrue(isinstance(reg_loss, value_lib.Mirrored))
+        if distribution.num_replicas_in_sync > 1:
+          self.assertTrue(isinstance(reg_loss, value_lib.Mirrored))
+        else:
+          self.assertTrue(isinstance(reg_loss, ops.Tensor))
 
 
 if __name__ == "__main__":
