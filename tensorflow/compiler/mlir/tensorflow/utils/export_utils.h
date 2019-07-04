028628376ac6175213db2c8dca350b44137ac976
Koan-Sin Tan <koansin.tan@gmail.com>
2019-07-04 10:35:32 +0800

    revert "mlir/StandardOps/Ops.h" chagnes
    remove additional "mlir/StandardOps/Ops.h" chagnes since there is
    no boolean problem anymore

--- tensorflow/compiler/mlir/tensorflow/utils/export_utils.h
+++ tensorflow/compiler/mlir/tensorflow/utils/export_utils.h
@@ -27,7 +27,6 @@ limitations under the License.
 #include "mlir/IR/Location.h"  // TF:local_config_mlir
 #include "mlir/IR/Operation.h"  // TF:local_config_mlir
 #include "mlir/IR/Types.h"  // TF:local_config_mlir
-#include "mlir/StandardOps/Ops.h"  // TF:local_config_mlir
 #include "tensorflow/core/framework/attr_value.pb.h"
 #include "tensorflow/core/framework/function.h"
 #include "tensorflow/core/framework/node_def.pb.h"
