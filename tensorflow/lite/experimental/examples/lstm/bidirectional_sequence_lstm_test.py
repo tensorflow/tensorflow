5f48f2e883c67f52af676a8649d13935f697e55a
Siju Samuel <siju.samuel@huawei.com>
2019-07-08 11:07:35 +0530

    Deprecated tf.random_normal to tf.random.normal in bidirectional_sequence_lstm_test

--- tensorflow/lite/experimental/examples/lstm/bidirectional_sequence_lstm_test.py
+++ tensorflow/lite/experimental/examples/lstm/bidirectional_sequence_lstm_test.py
@@ -86,8 +86,8 @@ class BidirectionalSequenceLstmTest(test_util.TensorFlowTestCase):
     """
     # Weights and biases for output softmax layer.
     out_weights = tf.Variable(
-        tf.random_normal([self.num_units * 2, self.n_classes]))
-    out_bias = tf.Variable(tf.random_normal([self.n_classes]))
+        tf.random.normal([self.num_units * 2, self.n_classes]))
+    out_bias = tf.Variable(tf.random.normal([self.n_classes]))
 
     # input image placeholder
     x = tf.placeholder(
