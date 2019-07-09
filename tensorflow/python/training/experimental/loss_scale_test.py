45925ad3d0c749db7a08abee38c0200a14728c6c
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-09 09:29:29 -0700

    Add single tensor gradient support for DynamicLossScale
    PiperOrigin-RevId: 257209230

--- tensorflow/python/training/experimental/loss_scale_test.py
+++ tensorflow/python/training/experimental/loss_scale_test.py
@@ -264,6 +264,15 @@ class DynamicLossScaleTest(test.TestCase, parameterized.TestCase):
       expected_outputs = [2, 2, 4, 4, 2, 2, 1, 1, 2, 2, 1]
       self._test_helper(inputs, expected_outputs, init_loss_scale)
 
+  @parameterized.named_parameters(*TESTCASES)
+  @test_util.run_in_graph_and_eager_modes
+  def test_single_tensor_gradient(self, strategy_fn):
+    with strategy_fn().scope():
+      loss_scale = loss_scale_module.DynamicLossScale()
+      grad = constant_op.constant(4.0)
+      _, should_apply = loss_scale.update(grad)
+      self.assertTrue(self.evaluate(should_apply))
+
   @test_util.run_in_graph_and_eager_modes
   def test_serialization(self):
     loss_scale = loss_scale_module.DynamicLossScale(
