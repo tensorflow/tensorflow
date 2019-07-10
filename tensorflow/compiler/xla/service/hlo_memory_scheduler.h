114b8e76197285077819df046f9ed08311bf05c6
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-10 05:16:51 -0700

    [XLA] Refactor memory scheduler to return the peak memory.
    PiperOrigin-RevId: 257383333

--- tensorflow/compiler/xla/service/hlo_memory_scheduler.h
+++ tensorflow/compiler/xla/service/hlo_memory_scheduler.h
@@ -35,13 +35,16 @@ namespace xla {
 // A memory scheduler computes an execution sequence for the HLO instructions in
 // 'computation' that minimizes peak memory, given a points-to analysis result
 // that describes buffer aliasing, together with a target-specific size function
-// that maps a tensor's logical size to its padded size.
+// that maps a tensor's logical size to its padded size. peak_memory (may be
+// nullptr) is set to the peak memory of the resulting schedule according to the
+// HeapSimulator.
 //
 // TODO(yunxing): Cleanup usage of TuplePointsToAnalysis.
 typedef std::function<StatusOr<HloInstructionSequence>(
     HloComputation*, const TuplePointsToAnalysis&, const HloAliasAnalysis&,
     const LogicalBuffer::SizeFunction&,
-    const absl::flat_hash_map<const HloComputation*, int64>&)>
+    const absl::flat_hash_map<const HloComputation*, int64>&,
+    /*peak_memory*/ int64*)>
     MemorySchedulerAlgorithm;
 
 // List scheduler
@@ -51,7 +54,8 @@ StatusOr<HloInstructionSequence> ListMemoryScheduler(
     const HloAliasAnalysis& alias_analysis,
     const LogicalBuffer::SizeFunction& size_function,
     const absl::flat_hash_map<const HloComputation*, int64>&
-        memory_by_computation);
+        memory_by_computation,
+    int64* peak_memory);
 
 // DFS-order scheduler
 StatusOr<HloInstructionSequence> DFSMemoryScheduler(
@@ -60,7 +64,8 @@ StatusOr<HloInstructionSequence> DFSMemoryScheduler(
     const HloAliasAnalysis& alias_analysis,
     const LogicalBuffer::SizeFunction& size_function,
     const absl::flat_hash_map<const HloComputation*, int64>&
-        memory_by_computation);
+        memory_by_computation,
+    int64* peak_memory);
 
 // Naive Post Order scheduler
 StatusOr<HloInstructionSequence> PostOrderMemoryScheduler(
@@ -69,25 +74,30 @@ StatusOr<HloInstructionSequence> PostOrderMemoryScheduler(
     const HloAliasAnalysis& alias_analysis,
     const LogicalBuffer::SizeFunction& size_function,
     const absl::flat_hash_map<const HloComputation*, int64>&
-        memory_by_computation);
+        memory_by_computation,
+    int64* peak_memory);
 
-// The default scheduling algorithm. Runs both the list scheduler
-// and the DFS scheduler, and chooses whichever returns a lower min-memory,
-// not accounting for fragmentation.
+// The default scheduling algorithm. Runs the list scheduler, the DFS scheduler,
+// and the post-order scheduler and chooses whichever returns a lower min-
+// memory, not accounting for fragmentation. peak_memory (may be nullptr) is set
+// to the peak memory of the resulting schedule according to the HeapSimulator.
 StatusOr<HloInstructionSequence> DefaultMemoryScheduler(
     HloComputation* computation,
     const TuplePointsToAnalysis& points_to_analysis,
     const HloAliasAnalysis& alias_analysis,
     const LogicalBuffer::SizeFunction& size_function,
     const absl::flat_hash_map<const HloComputation*, int64>&
-        memory_by_computation);
+        memory_by_computation,
+    int64* peak_memory);
 
-// Returns an HloSchedule which seeks to minimize the memory required for
-// the computation. size_function is the function returning the number of bytes
-// required for a LogicalBuffer.
+// Returns an HloSchedule which seeks to minimize the memory required for the
+// computation. size_function is the function returning the number of bytes
+// required for a LogicalBuffer. peak_memory (if not nullptr) is set to the peak
+// memory of the resulting schedule according to the HeapSimulator.
 StatusOr<HloSchedule> ScheduleModule(
     HloModule* module, const LogicalBuffer::SizeFunction& size_function,
-    const MemorySchedulerAlgorithm& algorithm = {});
+    const MemorySchedulerAlgorithm& algorithm = {},
+    int64* peak_memory = nullptr);
 
 // Computes the schedule for a single computation.
 // Currently only used by the GPU backend.
