c5ac4e8bb0cb63cfcd49ccfc83731a7035fcd20e
Yong Tang <yong.tang.github@outlook.com>
2019-07-10 14:38:12 +0000

    Pretty print error message for mod op of Dimesion
    This fix tries to address the issue raised in 30526 where
    the erorr message of mod with Dimension was not clear:
    ```
    Traceback (most recent call last):
      File "v.py", line 12, in <module>
        print(z.shape[0] % (-3))
    TypeError: unsupported operand type(s) for %: 'Dimension' and 'int'
    ```
    
    The issue was that when TypeErorr or ValueError was thrown, they were
    rethrown as NotImplemented.
    
    This fix expose the error message so that the error is clear:
    ```
    Traceback (most recent call last):
      File "v.py", line 12, in <module>
        print(z.shape[0] % (-3))
      File "/usr/local/lib/python2.7/dist-packages/tensorflow_core/python/framework/tensor_shape.py", line 579, in __mod__
        other = as_dimension(other)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow_core/python/framework/tensor_shape.py", line 720, in as_dimension
        return Dimension(value)
      File "/usr/local/lib/python2.7/dist-packages/tensorflow_core/python/framework/tensor_shape.py", line 197, in __init__
        raise ValueError("Dimension %d must be >= 0" % self._value)
    ValueError: Dimension -3 must be >= 0
    ```
    
    This fix fixes 30526.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/framework/tensor_shape.py
+++ tensorflow/python/framework/tensor_shape.py
@@ -576,10 +576,7 @@ class Dimension(object):
     Returns:
       A Dimension whose value is `self` modulo `other`.
     """
-    try:
-      other = as_dimension(other)
-    except (TypeError, ValueError):
-      return NotImplemented
+    other = as_dimension(other)
     if self._value is None or other.value is None:
       return Dimension(None)
     else:
