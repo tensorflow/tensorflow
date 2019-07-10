Formula fix, added tests.

tensorflow/python/ops/image_grad_test.py
@@ -29,7 +29,8 @@ from tensorflow.python.ops import gradients_impl
 from tensorflow.python.ops import image_ops
 from tensorflow.python.ops import gen_image_ops
 from tensorflow.python.platform import test
-
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import array_ops
 
 @test_util.for_all_test_methods(test_util.disable_xla,
                                 'align_corners=False not supported by XLA')
@@ -477,19 +478,71 @@ class RGBToHSVOpTest(test.TestCase):
         hsv_out = self.evaluate(hsv_out)
       self.assertEqual(out_shape, list(hsv_out.shape))
 
-  def testRGBToHSVGrad(self):
-    in_shape = [2, 20, 30, 3]
+  def testRGBToHSVGradSimpleCase(self):
+
     def f(x):
       return gen_image_ops.rgb_to_hsv(x)
 
-    x = np.random.rand(2, 20, 30, 3).astype(np.float32)
-    rgb_input_tensor = constant_op.constant(x, shape=in_shape)
+    # Building a simple input tensor to avoid any discontinuity
+    x = np.array(
+        [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]).astype(np.float32)
+    rgb_input_tensor = constant_op.constant(x, shape=x.shape)
+    # Computing Analytical and Numerical gradients of f(x)
     analytical, numerical = gradient_checker_v2.compute_gradient(
         f, [rgb_input_tensor])
+    self.assertAllClose(numerical, analytical, atol=1e-4)
+
+  def testRGBToHSVGradRandomCase(self):
+
+    def f(x):
+      return gen_image_ops.rgb_to_hsv(x)
+    np.random.seed(0)
+    # Building a simple input tensor to avoid any discontinuity
+    x = np.random.rand(1, 5, 5, 3).astype(np.float32)
+    rgb_input_tensor = constant_op.constant(x, shape=x.shape)
+    # Computing Analytical and Numerical gradients of f(x)
     self.assertLess(
-        gradient_checker_v2.max_error(analytical, numerical), 1e-2)
+        gradient_checker_v2.max_error(*gradient_checker_v2.compute_gradient(
+            f, [rgb_input_tensor])), 1e-4)
 
+  def testRGBToHSVGradSpecialCaseRGreatest(self):
+    # This test tests a specific subset of the input space
+    # with a dummy function implemented with native TF operations.
+    in_shape = [2, 10, 20, 3]
+    def f(x):
+      return gen_image_ops.rgb_to_hsv(x)
 
+    def f_dummy(x):
+      # This dummy function is a implementation of RGB to HSV using
+      # primitive TF functions for one particular case when R>G>B.
+      r = x[..., 0]
+      g = x[..., 1]
+      b = x[..., 2]
+      # Since MAX = r and MIN = b, we get the following h,s,v values.
+      v = r
+      s = 1 - math_ops.div_no_nan(b, r)
+      h = 60 * math_ops.div_no_nan(g - b, r - b)
+      h = h/360
+      return array_ops.stack([h, s, v], axis=-1)
+
+    # Building a custom input tensor where R>G>B
+    x_reds = np.ones((in_shape[0], in_shape[1], in_shape[2])).astype(np.float32)
+    x_greens = 0.5 * np.ones(
+        (in_shape[0], in_shape[1], in_shape[2])).astype(np.float32)
+    x_blues = 0.2 * np.ones(
+        (in_shape[0], in_shape[1], in_shape[2])).astype(np.float32)
+    x = np.stack([x_reds, x_greens, x_blues], axis=-1)
+    rgb_input_tensor = constant_op.constant(x, shape=in_shape)
+
+    # Computing Analytical and Numerical gradients of f(x)
+    analytical, numerical = gradient_checker_v2.compute_gradient(
+        f, [rgb_input_tensor])
+    # Computing Analytical and Numerical gradients of f_dummy(x)
+    analytical_dummy, numerical_dummy = gradient_checker_v2.compute_gradient(
+        f_dummy, [rgb_input_tensor])
+    self.assertAllClose(numerical, analytical, atol=1e-4)
+    self.assertAllClose(analytical_dummy, analytical, atol=1e-4)
+    self.assertAllClose(numerical_dummy, numerical, atol=1e-4)
 
 if __name__ == "__main__":
   test.main()