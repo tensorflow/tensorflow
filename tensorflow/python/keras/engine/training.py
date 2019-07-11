74e01e8cd5dcc5c72101bc63af38d6c1c8d9c066
Pavithra Vijay <psv@google.com>
2019-07-11 11:44:21 -0700

    Fix issue - in multi-output model if class weight is specified just for some outputs the weights get incorrectly mapped to placeholders
    PiperOrigin-RevId: 257650645

--- tensorflow/python/keras/engine/training.py
+++ tensorflow/python/keras/engine/training.py
@@ -1415,14 +1415,19 @@ class Model(network.Network):
   def _update_sample_weight_modes(self, sample_weights=None):
     """Updates sample weight modes based on training/eval inputs.
 
+    Sample weight placeholders will be created for all or no outputs
+    based on whether sample_weight is provided for any output.
+
     If model contains `_sample_weight_modes` we check if the input
     `sample_weights` corresponds to the sample weight modes.
-      1. If sample weight mode for output i is 'temporal', we do not
-        change it as the `temporal` mode has been set by the user.
-      2. Set sample weight mode to be 'samplewise' for output i if sample
-        weight mode was not set before and sample weight inputs are given.
+      1. Set sample weight mode to be 'temporal' for output i, if `compile`
+        sample_weight_mode was set to `temporal` and sample weight inputs
+        are given for one or more outputs.
+      2. Set sample weight mode to be 'samplewise' for output i, if `compile`
+        sample_weight_mode was not set and sample weight inputs are given for
+        one or more outputs.
       3. Reset sample weight mode to None for output i if sample weight mode
-        was set to 'samplewise' but there is no sample weight input.
+        was set but there is no sample weight input.
 
     Args:
       sample_weights: List of sample weights of the same length as model outputs
@@ -1430,21 +1435,11 @@ class Model(network.Network):
     """
     if not self._is_compiled:
       return
-    if not sample_weights:
-      sample_weights = [None] * len(self._training_endpoints)
-    for endpoint, sample_weight in zip(self._training_endpoints,
-                                       sample_weights):
-      if endpoint.sample_weight_mode == 'temporal':
-        # If sample weight mode for endpoint is 'temporal', do nothing.
-        continue
-      if endpoint.sample_weight_mode is None and sample_weight is not None:
-        # Set sample weight mode to be 'samplewise' for output i if sample
-        # weight mode was not set before and sample weight inputs are given.
-        endpoint.sample_weight_mode = 'samplewise'
-      elif (endpoint.sample_weight_mode == 'samplewise' and
-            sample_weight is None):
-        # Reset sample weight mode to None for output i if sample weight mode
-        # was set to 'samplewise' but there is no sample weight input.
+    if sample_weights and any([s is not None for s in sample_weights]):
+      for endpoint in self._training_endpoints:
+        endpoint.sample_weight_mode = self.sample_weight_mode or 'samplewise'
+    else:
+      for endpoint in self._training_endpoints:
         endpoint.sample_weight_mode = None
 
   def _recompile_weights_loss_and_weighted_metrics(self):
