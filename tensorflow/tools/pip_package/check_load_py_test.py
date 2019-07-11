08370f31ad948a9508df5f9630611c28c699e5e6
Mihai Maruseac <mihaimaruseac@google.com>
2019-07-10 17:19:06 -0700

    Temporarily add two bazel flags to test with an older version of bazel. This will be rolled back in at most 1 hour since submission
    PiperOrigin-RevId: 257515069

--- tensorflow/tools/pip_package/check_load_py_test.py
+++ tensorflow/tools/pip_package/check_load_py_test.py
@@ -47,6 +47,8 @@ def main():
   try:
     targets = subprocess.check_output([
         'bazel', 'query',
+        "--incompatible_package_name_is_a_function=false",
+        "--incompatible_remove_native_http_archive=false",
         'kind(py_test, //tensorflow/contrib/... + '
         '//tensorflow/python/... - '
         '//tensorflow/contrib/tensorboard/...)']).strip()
