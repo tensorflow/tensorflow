e457064999d35546c764d5c2d433d656ef322a3e
Wen-Heng (Jack) Chung <whchung@gmail.com>
2019-07-12 19:26:43 +0000

    Cope with the fact GpuVersion is yet merged in TF mainline.
    Use incumbent CompileToPtx interface.

--- tensorflow/compiler/xla/service/gpu/nvptx_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/nvptx_compiler.cc
@@ -361,7 +361,8 @@ NVPTXCompiler::CompileTargetBinary(const HloModule* module,
     XLA_SCOPED_LOGGING_TIMER(
         "NVPTXCompiler::CompileTargetBinary - CompileToPtx");
     TF_ASSIGN_OR_RETURN(
-        ptx, nvptx::CompileToPtx(llvm_module, gpu_version,
+        ptx, nvptx::CompileToPtx(llvm_module,
+                                 absl::get<std::pair<int, int>>(gpu_version),
                                  module->config(), libdevice_dir));
   }
 
