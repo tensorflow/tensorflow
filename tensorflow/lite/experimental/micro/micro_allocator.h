179e0454bb4e9ba9357ff12d549c54965d6808a5
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-12 14:40:39 -0700

    Split tensor allocator out of interpreter.
    Add a method to the allocator to re-use an existing input instead of re-allocating.  This is beneficial when a platform needs to control the life cycle of an input buffer (for example for DMA), and performance (avoiding a copy) or memory footprint is important.
    
    PiperOrigin-RevId: 257871435

--- tensorflow/lite/experimental/micro/micro_allocator.h
+++ tensorflow/lite/experimental/micro/micro_allocator.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,31 +12,26 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#ifndef TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_INTERPRETER_H_
-#define TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_INTERPRETER_H_
+#ifndef TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_ALLOCATOR_H_
+#define TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_ALLOCATOR_H_
 
 #include "tensorflow/lite/c/c_api_internal.h"
 #include "tensorflow/lite/core/api/error_reporter.h"
-#include "tensorflow/lite/core/api/op_resolver.h"
-#include "tensorflow/lite/experimental/micro/micro_allocator.h"
 #include "tensorflow/lite/experimental/micro/simple_tensor_allocator.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
 namespace tflite {
 
-class MicroInterpreter {
+// Allocator responsible for allocating memory for all intermediate tensors
+// necessary to invoke a model.
+class MicroAllocator {
  public:
-  // The lifetime of the model, op resolver, tensor arena, and error reporter
-  // must be at least as long as that of the interpreter object, since the
-  // interpreter may need to access them at any time. This means that you should
-  // usually create them with the same scope as each other, for example having
-  // them all allocated on the stack as local variables through a top-level
-  // function.
-  // The interpreter doesn't do any deallocation of any of the pointed-to
-  // objects, ownership remains with the caller.
-  MicroInterpreter(const Model* model, const OpResolver& op_resolver,
-                   uint8_t* tensor_arena, size_t tensor_arena_size,
-                   ErrorReporter* error_reporter);
+  // The lifetime of the model, tensor allocator and error reporter must be at
+  // least as long as that of the allocator object, since the allocator needs
+  // them to be accessible during its entire lifetime.
+  MicroAllocator(TfLiteContext* context, const Model* model,
+                 uint8_t* tensor_arena, size_t arena_size,
+                 ErrorReporter* error_reporter);
 
   // Specify a particular tensor as pre-allocated.  This means that this tensor
   // will internally point to the supplied buffer, and no new memory will be
@@ -54,36 +49,16 @@ class MicroInterpreter {
   // registerPreallocatedInput.
   TfLiteStatus AllocateTensors();
 
-  TfLiteStatus Invoke();
-
-  size_t tensors_size() const { return context_.tensors_size; }
-  TfLiteTensor* tensor(int tensor_index);
-
-  TfLiteTensor* input(int index);
-  size_t inputs_size() const { return subgraph_->inputs()->Length(); }
-
-  TfLiteTensor* output(int index);
-  size_t outputs_size() const { return subgraph_->outputs()->Length(); }
-
-  TfLiteStatus initialization_status() const { return initialization_status_; }
-
-  ErrorReporter* error_reporter() { return error_reporter_; }
-
  private:
-
   const Model* model_;
-  const OpResolver& op_resolver_;
+  SimpleTensorAllocator tensor_allocator_;
   ErrorReporter* error_reporter_;
-  TfLiteContext context_;
-  MicroAllocator allocator_;
-
-  TfLiteStatus initialization_status_;
-  const flatbuffers::Vector<flatbuffers::Offset<Tensor>>* tensors_;
-  const flatbuffers::Vector<flatbuffers::Offset<Operator>>* operators_;
+  TfLiteContext* context_;
 
   const SubGraph* subgraph_;
+  const flatbuffers::Vector<flatbuffers::Offset<Operator>>* operators_;
+  const flatbuffers::Vector<flatbuffers::Offset<Tensor>>* tensors_;
 };
 
 }  // namespace tflite
-
-#endif  // TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_INTERPRETER_H_
+#endif  // TENSORFLOW_LITE_EXPERIMENTAL_MICRO_MICRO_ALLOCATOR_H_
