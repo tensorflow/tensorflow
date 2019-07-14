19a2ffc5f3945d13a22d396d612b38d486126c48
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-14 02:02:13 -0700

    Update GraphDef version to 96.
    PiperOrigin-RevId: 258018576

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 95  // Updated: 2019/7/13
+#define TF_GRAPH_DEF_VERSION 96  // Updated: 2019/7/14
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
