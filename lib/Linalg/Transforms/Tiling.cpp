cd76d29652c73606439508090ed2b052bc9b09e5
Nicolas Vasilache <ntv@google.com>
2019-07-15 06:27:21 -0700

    Extend linalg transformations to allow value operands that are not views
    This CL extends the linalg ops that can be tiled and fused to operations that take either views, scalar or vector operands.
    
    PiperOrigin-RevId: 258149291

--- lib/Linalg/Transforms/Tiling.cpp
+++ lib/Linalg/Transforms/Tiling.cpp
@@ -383,8 +383,6 @@ mlir::linalg::tileLinalgOp(LinalgOp op, ArrayRef<Value *> tileSizes,
     auto b = ScopedContext::getBuilder();
     auto loc = ScopedContext::getLocation();
     SmallVector<Value *, 4> ivValues(ivs.begin(), ivs.end());
-    // If/when the assertion below becomes false, templatize `makeTiledViews`.
-    assert(op.getNumInputsAndOutputs() == op.getOperation()->getNumOperands());
     auto views =
         makeTiledViews(b, loc, op, ivValues, tileSizes, viewSizes, folder);
 
@@ -394,6 +392,8 @@ mlir::linalg::tileLinalgOp(LinalgOp op, ArrayRef<Value *> tileSizes,
                                                  viewsToPromote.end()),
                                 [](bool b) { return b; });
     if (!promote) {
+      auto operands = getAssumedNonViewOperands(op);
+      views.append(operands.begin(), operands.end());
       res = op.create(b, loc, views, op.getAttrs());
       return;
     }
@@ -424,6 +424,8 @@ mlir::linalg::tileLinalgOp(LinalgOp op, ArrayRef<Value *> tileSizes,
         opViews[i] = views[i];
       }
     }
+    auto operands = getAssumedNonViewOperands(op);
+    opViews.append(operands.begin(), operands.end());
     res = op.create(b, loc, opViews, op.getAttrs());
 
     // 6. Emit write-back for the promoted output views: copy the partial view.
