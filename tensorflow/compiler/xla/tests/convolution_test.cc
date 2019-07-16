cc93b7d7d5a066c5dbc597a28760cbd0cc2eb73c
Wen-Heng (Jack) Chung <whchung@gmail.com>
2019-07-16 10:25:27 -0500

    Remove unused DISABLED_ON_CPU macro from tests.

--- tensorflow/compiler/xla/tests/convolution_test.cc
+++ tensorflow/compiler/xla/tests/convolution_test.cc
@@ -1950,7 +1950,7 @@ class ConvolutionHloTest : public HloTestBase {};
 
 // double datatype is not yet supported in ROCm
 XLA_TEST_F(ConvolutionHloTest,
-           DISABLED_ON_GPU_ROCM(DISABLED_ON_CPU(ConvolveF64Forward))) {
+           DISABLED_ON_GPU_ROCM(ConvolveF64Forward)) {
   constexpr char kHlo[] = R"(
 HloModule TestModule
 
@@ -1976,7 +1976,7 @@ ENTRY Test {
 
 // double datatype is not yet supported in ROCm
 XLA_TEST_F(ConvolutionHloTest,
-           DISABLED_ON_GPU_ROCM(DISABLED_ON_CPU(ConvolveF64BackwardFilter))) {
+           DISABLED_ON_GPU_ROCM(ConvolveF64BackwardFilter)) {
   constexpr char kHlo[] = R"(
 HloModule TestModule
 
@@ -1990,7 +1990,7 @@ ENTRY Test {
 
 // double datatype is not yet supported in ROCm
 XLA_TEST_F(ConvolutionHloTest,
-           DISABLED_ON_GPU_ROCM(DISABLED_ON_CPU(ConvolveF64BackwardInput))) {
+           DISABLED_ON_GPU_ROCM(ConvolveF64BackwardInput)) {
   constexpr char kHlo[] = R"(
 HloModule TestModule
 
