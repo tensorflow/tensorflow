e08e7bc8e5dee69c48bbcf7d41c02f9c1a095a08
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2019-07-19 11:02:45 -0700

    Add docstring for reciprocal_no_nan().

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -4006,6 +4006,28 @@ def polyval(coeffs, x, name=None):
 
 @tf_export("math.reciprocal_no_nan", "reciprocal_no_nan")
 def reciprocal_no_nan(x, name=None):
+  """Performs a safe reciprocal operation, element wise. 
+  If a particular element is zero, the reciprocal for that element is 
+  also set to zero.
+
+  For example:
+  ```python
+  x = tf.constant([2.0, 0.5, 0, 1], dtype=tf.float32)
+  tf.math.reciprocal_no_nan(x)  # [ 0.5, 2, 0.0, 1.0 ]
+  ```
+
+  Args:
+    x: A `Tensor` of type `float16`, `float32`, `float64`
+       `complex64` or `complex128`.
+    name: A name for the operation (optional).
+
+  Returns:
+    A `Tensor` of same shape and type as `x`.
+  
+  Raises: 
+    TypeError: x must be of a valid dtype.
+
+  """
   with ops.name_scope(name, "reciprocal_no_nan", [x]) as scope:
     x = ops.convert_to_tensor(x, name="x")
     one = constant_ops.constant(1, dtype=x.dtype, name="one")
