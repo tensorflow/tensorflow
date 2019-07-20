8faa8bc20eae5f2b53a35dea7a56501d4e371870
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-19 17:27:24 -0700

    [tracing] unify internal and external ScopedAnnotation implementation.
    PiperOrigin-RevId: 259067560

--- tensorflow/core/profiler/internal/scoped_annotation_test.cc
+++ tensorflow/core/profiler/internal/scoped_annotation_test.cc
@@ -13,21 +13,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-/*
- * bazel run -c opt --config cuda --dynamic_mode=off \
- * --define tf_use_oss_timeline_nonprod=1 \
- * third_party/tensorflow/core/profiler/internal:scoped_annotation_test \
- * -- --benchmarks=all
- */
-
 #include "absl/strings/str_cat.h"
+#include "tensorflow/core/platform/annotation.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/core/platform/test_benchmark.h"
 #include "tensorflow/core/platform/tracing.h"
-#include "tensorflow/core/profiler/lib/profiler_session.h"
 
 namespace tensorflow {
 namespace {
+
+TEST(ScopedAnnotation, Simple) {
+  {
+    tracing::ScopedAnnotation trace("blah");
+    EXPECT_EQ(Annotation::CurrentAnnotation(), "");  // not enabled
+  }
+
+  {
+    tracing::ScopedAnnotation::Enable(true);
+    tracing::ScopedAnnotation trace("blah");
+    EXPECT_EQ(Annotation::CurrentAnnotation(), "blah");  // enabled
+    tracing::ScopedAnnotation::Enable(false);
+  }
+  {
+    tracing::ScopedAnnotation::Enable(true);
+    tracing::ScopedAnnotation outer("foo");
+    tracing::ScopedAnnotation inner("bar");
+    EXPECT_EQ(Annotation::CurrentAnnotation(), "foo::bar");  // enabled
+    tracing::ScopedAnnotation::Enable(false);
+  }
+
+  EXPECT_EQ(Annotation::CurrentAnnotation(), "");  // not enabled
+}
 
 std::string GenerateRandomString(int length) {
   return std::string(length, 'a');
@@ -48,13 +64,13 @@ BENCHMARK(BM_ScopedAnnotationDisabled)->Arg(8)->Arg(32)->Arg(128);
 void BM_ScopedAnnotationEnabled(int iters, int annotation_size) {
   testing::StopTiming();
   std::string annotation = GenerateRandomString(annotation_size);
-  auto profiler_session =
-      tensorflow::ProfilerSession::Create(/*ProfilerContext*/ nullptr);
+  tracing::ScopedAnnotation::Enable(true);
   testing::StartTiming();
   for (int i = 0; i < iters; i++) {
     tracing::ScopedAnnotation trace(annotation);
   }
   testing::StopTiming();
+  tracing::ScopedAnnotation::Enable(false);
 }
 
 BENCHMARK(BM_ScopedAnnotationEnabled)->Arg(8)->Arg(32)->Arg(128);
@@ -62,13 +78,13 @@ BENCHMARK(BM_ScopedAnnotationEnabled)->Arg(8)->Arg(32)->Arg(128);
 void BM_ScopedAnnotationEnabled_TwoParts(int iters, int annotation_size) {
   testing::StopTiming();
   std::string annotation = GenerateRandomString(annotation_size);
-  auto profiler_session =
-      tensorflow::ProfilerSession::Create(/*ProfilerContext*/ nullptr);
+  tracing::ScopedAnnotation::Enable(true);
   testing::StartTiming();
   for (int i = 0; i < iters; i++) {
     tracing::ScopedAnnotation trace(annotation, annotation);
   }
   testing::StopTiming();
+  tracing::ScopedAnnotation::Enable(false);
 }
 
 BENCHMARK(BM_ScopedAnnotationEnabled_TwoParts)->Arg(8)->Arg(32)->Arg(128);
@@ -76,31 +92,66 @@ BENCHMARK(BM_ScopedAnnotationEnabled_TwoParts)->Arg(8)->Arg(32)->Arg(128);
 void BM_ScopedAnnotationEnabled_Nested(int iters, int annotation_size) {
   testing::StopTiming();
   std::string annotation = GenerateRandomString(annotation_size);
-  auto profiler_session =
-      tensorflow::ProfilerSession::Create(/*ProfilerContext*/ nullptr);
+  tracing::ScopedAnnotation::Enable(true);
   testing::StartTiming();
   for (int i = 0; i < iters; i++) {
     tracing::ScopedAnnotation trace(annotation);
     { tracing::ScopedAnnotation trace(annotation); }
   }
   testing::StopTiming();
+  tracing::ScopedAnnotation::Enable(false);
 }
 
 BENCHMARK(BM_ScopedAnnotationEnabled_Nested)->Arg(8)->Arg(32)->Arg(128);
 
 void BM_ScopedAnnotationEnabled_Adhoc(int iters, int annotation_size) {
   testing::StopTiming();
-  auto profiler_session =
-      tensorflow::ProfilerSession::Create(/*ProfilerContext*/ nullptr);
+  tracing::ScopedAnnotation::Enable(true);
   testing::StartTiming();
   for (int i = 0; i < iters; i++) {
     // generate the annotation on the fly.
     tracing::ScopedAnnotation trace(absl::StrCat(i, "-", i * i));
   }
   testing::StopTiming();
+  tracing::ScopedAnnotation::Enable(false);
 }
 
 BENCHMARK(BM_ScopedAnnotationEnabled_Adhoc)->Arg(8)->Arg(32)->Arg(128);
+
+void BM_ScopedAnnotationDisabled_Lambda(int iters, int annotation_size) {
+  for (int i = 0; i < iters; i++) {
+    tracing::ScopedAnnotation trace(
+        [&]() { return absl::StrCat(i, "-", i * i); });
+  }
+}
+
+BENCHMARK(BM_ScopedAnnotationDisabled_Lambda)->Arg(8)->Arg(32)->Arg(128);
+
+void BM_ScopedAnnotationEnabled_Adhoc_Lambda(int iters, int annotation_size) {
+  tracing::ScopedAnnotation::Enable(true);
+  for (int i = 0; i < iters; i++) {
+    tracing::ScopedAnnotation trace(
+        [&]() { return absl::StrCat(i, "-", i * i); });
+  }
+  tracing::ScopedAnnotation::Enable(false);
+}
+
+BENCHMARK(BM_ScopedAnnotationEnabled_Adhoc_Lambda)->Arg(8)->Arg(32)->Arg(128);
+
+void BM_ScopedAnnotationEnabled_TwoPartsLambda(int iters, int annotation_size) {
+  testing::StopTiming();
+  std::string annotation = GenerateRandomString(annotation_size);
+  tracing::ScopedAnnotation::Enable(true);
+  testing::StartTiming();
+  for (int i = 0; i < iters; i++) {
+    tracing::ScopedAnnotation trace(
+        [&]() { return absl::StrCat(annotation, ":", annotation); });
+  }
+  testing::StopTiming();
+  tracing::ScopedAnnotation::Enable(false);
+}
+
+BENCHMARK(BM_ScopedAnnotationEnabled_TwoPartsLambda)->Arg(8)->Arg(32)->Arg(128);
 
 }  // namespace
 }  // namespace tensorflow
