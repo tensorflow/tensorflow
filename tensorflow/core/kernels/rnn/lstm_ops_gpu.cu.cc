ac406cf0eaa519d06f72c4e985878554df898ec3
jerryyin <zhuoryin@amd.com>
2019-07-23 14:49:09 +0000

    Adding ROCm support to rnn ops

--- tensorflow/core/kernels/rnn/lstm_ops_gpu.cu.cc
+++ tensorflow/core/kernels/rnn/lstm_ops_gpu.cu.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
 #define EIGEN_USE_GPU
 
@@ -460,4 +460,4 @@ DEFINE_GPU_SPECS(Eigen::half);
 
 }  // end namespace functor
 }  // end namespace tensorflow
-#endif  // GOOGLE_CUDA
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
