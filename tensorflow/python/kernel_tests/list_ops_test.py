7e0ac8e44001a3fe2d1af5753d19dc3acb9209c2
Saurabh Saxena <srbs@google.com>
2019-07-23 17:19:03 -0700

    Fix device to device copy of nested variants.
    PiperOrigin-RevId: 259644840

--- tensorflow/python/kernel_tests/list_ops_test.py
+++ tensorflow/python/kernel_tests/list_ops_test.py
@@ -1582,6 +1582,31 @@ class ListOpsTest(test_util.TensorFlowTestCase, parameterized.TestCase):
         tensor_list, 0, element_dtype=dtypes.float32)
     self.assertAllEqual(element.shape.as_list(), [])
 
+  @test_util.run_gpu_only
+  def testNestedListDevicetoDeviceCopy(self):
+    if context.num_gpus() < 2:
+      self.skipTest("Need at least 2 GPUs for this test, found %d" %
+                    context.num_gpus())
+    with ops.device("gpu:0"):
+      t = constant_op.constant([1.0, 2.0, 3.0])
+      inner_l = list_ops.tensor_list_from_tensor(t, element_shape=[])
+      outer_l = list_ops.empty_tensor_list(
+          element_dtype=dtypes.variant, element_shape=[])
+      outer_l = list_ops.tensor_list_push_back(outer_l, inner_l)
+
+    # Stress test.
+    for _ in range(1024):
+      with ops.device("gpu:1"):
+        outer_l = array_ops.identity(outer_l)
+      with ops.device("gpu:0"):
+        outer_l = array_ops.identity(outer_l)
+
+    with ops.device("gpu:1"):
+      _, inner_l = list_ops.tensor_list_pop_back(
+          outer_l, element_dtype=dtypes.variant)
+      t = list_ops.tensor_list_stack(inner_l, element_dtype=dtypes.float32)
+      self.assertAllEqual(t, [1.0, 2.0, 3.0])
+
 
 if __name__ == "__main__":
   test.main()
