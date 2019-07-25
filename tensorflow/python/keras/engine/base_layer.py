12a9859437cd8db105701e64cfbd60961184bcba
Pavithra Vijay <psv@google.com>
2019-07-24 17:13:55 -0700

    Fix autograph comment in base layer.
    PiperOrigin-RevId: 259849617

--- tensorflow/python/keras/engine/base_layer.py
+++ tensorflow/python/keras/engine/base_layer.py
@@ -684,8 +684,9 @@ class Layer(module.Module):
           self._maybe_build(inputs)
 
           # Wrapping `call` function in autograph to allow for dynamic control
-          # dependencies in call. We are limiting this to subclassed layers as
-          # autograph is strictly needed only for subclassed layers and models.
+          # flow and control dependencies in call. We are limiting this to
+          # subclassed layers as autograph is strictly needed only for
+          # subclassed layers and models.
           # tf_convert will respect the value of autograph setting in the
           # enclosing tf.function, if any.
           if base_layer_utils.is_subclassed(self):
