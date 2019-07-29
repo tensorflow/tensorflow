9e1264260af6e5373735090a915e618452027c54
Tong Shen <endlessroad@google.com>
2019-07-29 15:28:40 -0700

    Fix control inputs for while loop's Enter nodes.
    Before this change we keep all external control inputs for Enter nodes. If Enter node has control input from outside its outer WhileContext, Enter node will have inputs from both its outer WhileContext (the data input) and outside its outer WhileContext, which is considered invalid by BuildControlFlowInfo().
    
    So only keep external control inputs which are also within outer WhileContext.
    
    PiperOrigin-RevId: 260593003

--- tensorflow/python/ops/control_flow_ops.py
+++ tensorflow/python/ops/control_flow_ops.py
@@ -760,15 +760,6 @@ class ControlFlowContext(object):
       return self._outer_context.GetWhileContext()
     return None
 
-  def _IsInOuterContext(self, op):
-    op_ctxt = util.GetOutputContext(op)
-    outer_ctxt = self.outer_context
-    while outer_ctxt != op_ctxt:
-      if outer_ctxt is None:
-        return False
-      outer_ctxt = outer_ctxt.outer_context
-    return True
-
   def _RemoveExternalControlEdges(self, op):
     """Remove any external control dependency on this op."""
     while_ctxt = self.GetWhileContext()
@@ -2281,9 +2272,22 @@ class WhileContext(ControlFlowContext):
       for x in xs:
         inp_op = x.op.inputs[0].op
         control_inputs = graph._control_dependencies_for_inputs([inp_op])
-        outer_control_inputs = [
-            op for op in control_inputs if self._IsInOuterContext(op)
-        ]
+        outer_control_inputs = []
+        for op in control_inputs:
+          # We need to keep control inputs that are in any ancestor
+          # ControlFlowContext, and within outer WhileContext.
+          keep_as_control_input = True
+          op_ctxt = util.GetOutputContext(op)
+          outer_ctxt = self.outer_context
+          outer_while_context = (None if outer_ctxt is None else
+                                 outer_ctxt.GetWhileContext())
+          while outer_ctxt != op_ctxt:
+            if outer_ctxt is None or outer_ctxt == outer_while_context:
+              keep_as_control_input = False
+              break
+            outer_ctxt = outer_ctxt.outer_context
+          if keep_as_control_input:
+            outer_control_inputs.append(op)
         x.op._set_control_flow_context(self)
         x.op._add_control_inputs(outer_control_inputs)
         graph._record_op_seen_by_control_dependencies(x.op)
