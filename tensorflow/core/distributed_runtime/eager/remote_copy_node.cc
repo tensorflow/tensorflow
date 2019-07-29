296d899180329f0ab3d8862c23d5883a6bac43ec
Igor Ganichev <iga@google.com>
2019-07-29 16:21:49 -0700

    Don't start remote Recv if Send has failed
    PiperOrigin-RevId: 260603584

--- tensorflow/core/distributed_runtime/eager/remote_copy_node.cc
+++ tensorflow/core/distributed_runtime/eager/remote_copy_node.cc
@@ -195,15 +195,18 @@ Status RemoteCopyNode::RunRemoteRecv(EagerOperation* op) {
     return status;
   }
 
-  EnqueueResponse* response = new EnqueueResponse;
-
   // Don't issue the recv until send has completed.
   //  - local send will complete very quickly.
   //  - remote send will take some time, but remote->remote copy is
   //    probably rare enough that we don't care much.
   // Blocks until send has completed.
   Status send_status = captured_state_->GetSendStatus();
+  if (!send_status.ok()) {
+    captured_state_->dst()->Poison(send_status);
+    return send_status;
+  }
 
+  EnqueueResponse* response = new EnqueueResponse;
   const std::shared_ptr<CapturedSharedState>& captured_state = captured_state_;
   Device* recv_device = recv_device_;
   Notification n;
