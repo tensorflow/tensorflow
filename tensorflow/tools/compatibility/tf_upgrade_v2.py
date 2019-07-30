8b0f14320b05955894e9fc99911779ab9241a1f4
Anna R <annarev@google.com>
2019-07-30 12:06:17 -0700

    Automated rollback of commit a819c8d454b03aef284dc6d728a1de5047c94a58
    PiperOrigin-RevId: 260767023

--- tensorflow/tools/compatibility/tf_upgrade_v2.py
+++ tensorflow/tools/compatibility/tf_upgrade_v2.py
@@ -2024,7 +2024,7 @@ def _add_loss_reduction_transformer(parent, node, full_name, name, logs):
 
   Default value for tf.estimator.*Classifier and tf.estimator.*Regressor
   loss_reduction argument changed to SUM_OVER_BATCH_SIZE. So, we update
-  existing calls to use the old default value `tf.keras.losses.Reduction.SUM`.
+  existing calls to use the old default value `tf.losses.Reduction.SUM`.
 
   Note: to apply this transformation, symbol must be added
   to reordered_function_names above.
@@ -2032,7 +2032,9 @@ def _add_loss_reduction_transformer(parent, node, full_name, name, logs):
   for keyword_arg in node.keywords:
     if keyword_arg.arg == "loss_reduction":
       return node
-  default_value = "tf.keras.losses.Reduction.SUM"
+  # TODO(annarev): this should be updated to tf.keras.losses.Reduction.SUM
+  # once b/125525822 is fixed.
+  default_value = "tf.compat.v1.losses.Reduction.SUM"
   # Parse with pasta instead of ast to avoid emitting a spurious trailing \n.
   ast_value = pasta.parse(default_value)
   node.keywords.append(ast.keyword(arg="loss_reduction", value=ast_value))
