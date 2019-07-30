e7206a7d8efceda25bce5a530fc4e79a5582be13
Jiri Simsa <jsimsa@google.com>
2019-07-30 12:01:13 -0700

    [tf.data] Extending the TF 2.0 support for `shuffle(..., reshuffle_each_iteration=True)` to work across different Python iterators for the same dataset.
    To achieve its objective, this CL creates a `RandomSeedGenerator` resource and ops for creating and deleting the resource which is used to manage state for seeding the shuffle order of different Python iterators for the same dataset.
    
    Note that the new functionality is not yet supported for (multi-worker) distribution strategies that clone the input pipeline graph created by user programs. To support this use case, we need a mechanism to clone the `RandomSeedGenerator` resource (and in general other resources).
    
    Fixes: #27680
    PiperOrigin-RevId: 260765646

--- tensorflow/core/kernels/data/dataset_utils.h
+++ tensorflow/core/kernels/data/dataset_utils.h
@@ -15,12 +15,66 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_KERNELS_DATA_DATASET_UTILS_H_
 #define TENSORFLOW_CORE_KERNELS_DATA_DATASET_UTILS_H_
 
+#include "tensorflow/core/common_runtime/function.h"
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/function.h"
+#include "tensorflow/core/framework/resource_mgr.h"
 #include "tensorflow/core/framework/tensor.h"
 
 namespace tensorflow {
 namespace data {
+
+template <typename T>
+class AnonymousResourceOp : public OpKernel {
+ public:
+  static std::atomic<int64> resource_id_counter_;
+
+  explicit AnonymousResourceOp(OpKernelConstruction* context)
+      : OpKernel(context) {}
+
+  void Compute(OpKernelContext* ctx) override {
+    FunctionLibraryRuntime* lib;
+    std::unique_ptr<FunctionLibraryDefinition> flib_def(nullptr);
+    std::unique_ptr<ProcessFunctionLibraryRuntime> pflr(nullptr);
+    OP_REQUIRES_OK(
+        ctx, ctx->function_library()->Clone(&flib_def, &pflr, &lib, true));
+    T* resource;
+    OP_REQUIRES_OK(ctx, CreateResource(ctx, std::move(flib_def),
+                                       std::move(pflr), lib, &resource));
+
+    string container_name = name();
+    string unique_name =
+        strings::StrCat(container_name, resource_id_counter_.fetch_add(1));
+    ResourceMgr* mgr = ctx->resource_manager();
+    OP_REQUIRES_OK(ctx, mgr->Create<T>(container_name, unique_name, resource));
+
+    Tensor* handle_t;
+    OP_REQUIRES_OK(ctx, ctx->allocate_output(0, TensorShape({}), &handle_t));
+    ResourceHandle handle = MakeResourceHandle(ctx, container_name, unique_name,
+                                               MakeTypeIndex<T>());
+    handle_t->scalar<ResourceHandle>()() = handle;
+
+    if (create_deleter_) {
+      Tensor* deleter_t;
+      OP_REQUIRES_OK(ctx, ctx->allocate_output(1, TensorShape({}), &deleter_t));
+      deleter_t->scalar<Variant>()() =
+          ResourceDeleter(handle, ctx->resource_manager());
+    }
+  }
+
+ protected:
+  virtual string name() = 0;
+
+  virtual Status CreateResource(
+      OpKernelContext* ctx, std::unique_ptr<FunctionLibraryDefinition> flib_def,
+      std::unique_ptr<ProcessFunctionLibraryRuntime> pflr,
+      FunctionLibraryRuntime* lib, T** resource) = 0;
+
+  bool create_deleter_ = true;
+};
+
+template <typename T>
+std::atomic<int64> AnonymousResourceOp<T>::resource_id_counter_;
 
 // Returns a GraphDef representation of the given dataset.
 Status AsGraphDef(OpKernelContext* ctx, const DatasetBase* dataset,
--- tensorflow/core/kernels/data/experimental/auto_shard_dataset_op.cc
+++ tensorflow/core/kernels/data/experimental/auto_shard_dataset_op.cc
@@ -28,6 +28,7 @@ namespace experimental {
 /* static */ constexpr const char* const AutoShardDatasetOp::kOutputTypes;
 /* static */ constexpr const char* const AutoShardDatasetOp::kOutputShapes;
 
+constexpr char kMakeStateless[] = "make_stateless";
 constexpr char kOptimizerName[] = "tf_auto_shard";
 
 AutoShardDatasetOp::AutoShardDatasetOp(OpKernelConstruction* ctx)
@@ -63,17 +64,21 @@ RewriterConfig AutoShardDatasetOp::CreateConfig(int64 num_workers,
                                                 int64 index) {
   RewriterConfig rewriter_config;
   rewriter_config.set_fail_on_optimizer_errors(true);
-  rewriter_config.add_optimizers(kOptimizerName);
   rewriter_config.set_meta_optimizer_iterations(RewriterConfig::ONE);
+
+  rewriter_config.add_optimizers(kMakeStateless);
   auto custom_optimizer = rewriter_config.add_custom_optimizers();
-  custom_optimizer->set_name(kOptimizerName);
+  custom_optimizer->set_name(kMakeStateless);
+
+  rewriter_config.add_optimizers(kOptimizerName);
+  auto custom_optimizer2 = rewriter_config.add_custom_optimizers();
+  custom_optimizer2->set_name(kOptimizerName);
   AttrValue num_workers_attr;
   num_workers_attr.set_i(num_workers);
-  (*custom_optimizer->mutable_parameter_map())[kNumWorkers] = num_workers_attr;
-
+  (*custom_optimizer2->mutable_parameter_map())[kNumWorkers] = num_workers_attr;
   AttrValue index_attr;
   index_attr.set_i(index);
-  (*custom_optimizer->mutable_parameter_map())[kIndex] = index_attr;
+  (*custom_optimizer2->mutable_parameter_map())[kIndex] = index_attr;
 
   return rewriter_config;
 }
