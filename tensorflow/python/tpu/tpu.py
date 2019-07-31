7519f196e409306891aa52fb45002004423b0e6c
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-31 14:27:48 -0700

    Add tpu.initialize_system_for_tpu_embedding().
    It is helpful as often user needs to initialize the tpu system before embedding config is available.
    
    PiperOrigin-RevId: 260998607

--- tensorflow/python/tpu/tpu.py
+++ tensorflow/python/tpu/tpu.py
@@ -107,6 +107,32 @@ def initialize_system(embedding_config=None, job=None):
     return tpu_ops.configure_distributed_tpu(embedding_config=config_string)
 
 
+def initialize_system_for_tpu_embedding(embedding_config, job=None):
+  """Initializes a distributed TPU Embedding system for use with TensorFlow.
+
+  The following two are equivalent:
+  1. initialize_system() with embedding_config.
+  2. initialize_system() without embedding_config, then
+     initialize_system_for_tpu_embedding().
+  initialize_system() should not be called with embedding_config if
+  initialize_system_for_tpu_embedding() is meant to be called later.
+
+  Args:
+    embedding_config: a `TPUEmbeddingConfiguration` proto describing the desired
+      configuration of the hardware embedding lookup tables.
+    job: The job (the XXX in TensorFlow device specification /job:XXX) that
+      contains the TPU devices that will be initialized. If job=None it is
+      assumed there is only one job in the TensorFlow flock, and an error will
+      be returned if this assumption does not hold.
+
+  Returns:
+    A no-op.
+  """
+  config_string = embedding_config.SerializeToString()
+  with ops.device(_tpu_system_device_name(job)):
+    return tpu_ops.configure_tpu_embedding(config=config_string)
+
+
 @tf_export(v1=["tpu.shutdown_system"])
 def shutdown_system(job=None):
   """Shuts down a running a distributed TPU system.
