2b20ee5f226977f8ed3b83ab47e2a3a0e88910cd
A. Unique TensorFlower <gardener@tensorflow.org>
2019-07-31 15:28:09 -0700

    Rewrote Exhaustive Test to take templated values.
    ExhaustiveOpTestBase class now takes a primitive type when instantiated. As a
    result, its member functions no longer take templated types.
    
    ExhaustoveOpTetsBase class now also takes a size_t length when instantiated.
    This represents the number specified primitive types the function takes (i.e.
    N=2 represents a binary op test case.)
    
    ExpectNear and Run has been refactored into the ExhaustiveOpTestBase class.
    
    ErrorSpec is now a function pointer, as opposed to std::function objects.
    
    ErrorSpec functions now take in complex numbers as inputs for complex primitive
    type templates.
    
    Subclasses of Exhaustive Op Tests no longer require a primitive type in its
    ::testing::WithParamInterface templat class (since the primitive type is
    specified as a template).
    
    Exhaustive32BitOrLessUnaryTests that use different primitive types are now
    separate classes. As a result, their XLA_TEST_P functions are now separate. A
    macro has been added to generate all 32-bit or less tests simultaniously for
    when their implementations are the same.
    
    PiperOrigin-RevId: 261010596

--- tensorflow/compiler/xla/tests/exhaustive_op_test_utils.cc
+++ tensorflow/compiler/xla/tests/exhaustive_op_test_utils.cc
@@ -26,76 +26,207 @@ namespace xla {
 //   ceil(1 + S * log_10(2)) ~= ceil(1 + S * 0.30103).
 //
 // See https://people.eecs.berkeley.edu/~wkahan/Math128/BinDecBin.pdf.)
-/*static*/
-string ExhaustiveOpTestBase::StringifyNum(double x) {
-  return absl::StrFormat("%0.17g (0x%016x)", x, BitCast<uint64>(x));
-}
+namespace {
+template <typename T>
+struct ComponentStringifyFormat {};
+
+template <>
+struct ComponentStringifyFormat<double> {
+  static constexpr absl::string_view value = "%0.17g (0x%16x)";
+};
+
+template <>
+struct ComponentStringifyFormat<float> {
+  static constexpr absl::string_view value = "%0.8g (0x%08x)";
+};
+
+template <>
+struct ComponentStringifyFormat<Eigen::half> {
+  static constexpr absl::string_view value = "%0.5g (0x%04x)";
+};
+
+template <>
+struct ComponentStringifyFormat<bfloat16> {
+  static constexpr absl::string_view value = "%0.4g (0x%04x)";
+};
+}  // namespace
 
 /*static*/
-string ExhaustiveOpTestBase::StringifyNum(float x) {
-  return absl::StrFormat("%0.9g (0x%08x)", x, BitCast<uint32>(x));
+template <PrimitiveType T, size_t N>
+string ExhaustiveOpTestBase<T, N>::StringifyNum(
+    typename ExhaustiveOpTestBase<T, N>::ComponentNativeT x) {
+  typedef typename ExhaustiveOpTestBase<T, N>::ComponentNativeT ComponentType;
+  typedef typename ExhaustiveOpTestBase<T, N>::ComponentIntegralNativeT
+      IntegralType;
+  return absl::StrFormat(ComponentStringifyFormat<ComponentType>::value,
+                         static_cast<double>(x), BitCast<IntegralType>(x));
 }
 
-/*static*/
-string ExhaustiveOpTestBase::StringifyNum(half x) {
-  return absl::StrFormat("%0.5g (0x%04x)", static_cast<float>(x),
-                         BitCast<uint16>(x));
-}
-
-/*static*/
-string ExhaustiveOpTestBase::StringifyNum(bfloat16 x) {
-  return absl::StrFormat("%0.4g (0x%04x)", static_cast<float>(x),
-                         BitCast<uint16>(x));
-}
-
-/*static*/
-std::vector<std::pair<int64, int64>>
-ExhaustiveOpTestBase::CreateExhaustiveF32Ranges() {
-  // We break up the 2^32-element space into small'ish chunks to keep peak
-  // memory usage low.
-  std::vector<std::pair<int64, int64>> result;
-  const int64 step = 1 << 25;
-  for (int64 i = 0; i < (1l << 32); i += step) {
-    result.push_back({i, i + step});
+template <PrimitiveType T, size_t N>
+void ExhaustiveOpTestBase<T, N>::ExpectNear(const InputLiterals& input_literals,
+                                            const Literal& result_literal,
+                                            EvaluateOp evaluate_op,
+                                            ErrorSpecGen error_spec_gen) {
+  // Cache for when all components are subnormal testing values.
+  std::vector<NativeRefT> pure_subnormal_cache;
+  pure_subnormal_cache.reserve(GetMaxCacheSize());
+  for (int i = 0; i < GetMaxCacheSize(); ++i) {
+    pure_subnormal_cache.push_back(
+        CallOperation(evaluate_op, FromCacheLocation(i)));
   }
-  return result;
+
+  NativeInputsList inputs_arr;
+  for (int i = 0; i < N; ++i) {
+    const Literal& literal = input_literals[i];
+    inputs_arr[i] = literal.data<NativeT>();
+  }
+
+  absl::Span<const NativeT> result_arr = result_literal.data<NativeT>();
+
+  int64 mismatches = 0;
+
+  for (int64 i = 0; i < result_arr.size(); ++i) {
+    NativeInputs inputs;
+    NativeRefInputs inputs_ref_ty;
+
+    for (int j = 0; j < N; ++j) {
+      inputs[j] = inputs_arr[j][i];
+      inputs_ref_ty[j] = static_cast<NativeRefT>(inputs[j]);
+    }
+
+    NativeT actual = result_arr[i];
+    NativeT expected =
+        static_cast<NativeT>(CallOperation(evaluate_op, inputs_ref_ty));
+    ErrorSpec error_spec = CallErrorSpec(error_spec_gen, inputs);
+
+    if (IsClose(static_cast<NativeRefT>(expected),
+                static_cast<NativeRefT>(actual), error_spec)) {
+      continue;
+    }
+
+    std::vector<NativeRefInputs> subnormal_test_inputs =
+        GetTestValuesWithSubnormalSubstitutions(inputs_ref_ty);
+
+    // Easy case: If `input` is not subnormal and !IsClose(expected, actual,
+    // error_spec), print an error.
+    if (subnormal_test_inputs.size() == 1) {
+      PrintMismatch(&mismatches, [&] {
+        return absl::StrFormat("Mismatch on %s. Expected %s, but got %s.",
+                               StringifyNum(inputs), StringifyNum(expected),
+                               StringifyNum(actual));
+      });
+      continue;
+    }
+
+    // Otherwise, we need to test the additional subnormal test values.
+    std::vector<NativeRefT> subnormal_test_results;
+    subnormal_test_results.reserve(subnormal_test_inputs.size());
+    bool passed_subnormal_test = false;
+
+    for (NativeRefInputs test_value : subnormal_test_inputs) {
+      NativeRefT result;
+      int cache_loc = GetCacheLocation(test_value);
+      if (cache_loc == kInvalidCacheIndex) {
+        result = CallOperation(evaluate_op, test_value);
+      } else {
+        result = pure_subnormal_cache[cache_loc];
+      }
+
+      if (IsClose(result, static_cast<NativeRefT>(actual), error_spec)) {
+        passed_subnormal_test = true;
+        break;
+      }
+      subnormal_test_results.push_back(std::move(result));
+    }
+
+    if (passed_subnormal_test) {
+      continue;
+    }
+
+    std::string mismatch = absl::StrFormat(
+        "Mismatch on subnormal value %s.  Expected one of:\n"
+        "  %10s (evaluated at full-precision value)\n",
+        StringifyNum(inputs), StringifyNum(expected));
+
+    CHECK_EQ(subnormal_test_inputs.size(), subnormal_test_results.size());
+    for (int i = 0; i < subnormal_test_inputs.size(); ++i) {
+      absl::StrAppend(
+          &mismatch,
+          absl::StrFormat("  %10s (evaluated at %s)\n",
+                          StringifyNum(subnormal_test_results[i]),
+                          GetSubnormalDescription(subnormal_test_inputs[i],
+                                                  inputs_ref_ty)));
+    }
+    absl::StrAppend(&mismatch,
+                    absl::StrFormat("but got %s", StringifyNum(actual)));
+
+    PrintMismatch(&mismatches, [mismatch] { return mismatch; });
+  }
+  EXPECT_EQ(mismatches, 0);
 }
 
 namespace {
-ExhaustiveOpTestBase::ErrorSpec DefaultF64SpecGenerator(float) {
-  return ExhaustiveOpTestBase::ErrorSpec(0.0001, 0.0001);
+template <PrimitiveType T, size_t N>
+inline typename ExhaustiveOpTestBase<T, N>::ErrorSpec DefaultSpecGenerator(
+    typename ExhaustiveOpTestBase<T, N>::NativeT) {
+  LOG(FATAL) << "Unhandled Type";
+}
+template <PrimitiveType T, size_t N>
+inline typename ExhaustiveOpTestBase<T, N>::ErrorSpec DefaultSpecGenerator(
+    typename ExhaustiveOpTestBase<T, N>::NativeT,
+    typename ExhaustiveOpTestBase<T, N>::NativeT) {
+  LOG(FATAL) << "Unhandled Type";
 }
 
-ExhaustiveOpTestBase::ErrorSpec DefaultF32SpecGenerator(float) {
-  return ExhaustiveOpTestBase::ErrorSpec(0.0001, 0.0001);
+template <>
+inline ExhaustiveOpTestBase<C128, 1>::ErrorSpec DefaultSpecGenerator<C128, 1>(
+    complex128) {
+  return ExhaustiveOpTestBase<C128, 1>::ErrorSpec{0.0001, 0.0001};
 }
 
-ExhaustiveOpTestBase::ErrorSpec DefaultF16SpecGenerator(float) {
-  return ExhaustiveOpTestBase::ErrorSpec(0.001, 0.001);
+template <>
+inline ExhaustiveOpTestBase<C64, 1>::ErrorSpec DefaultSpecGenerator<C64, 1>(
+    complex64) {
+  return ExhaustiveOpTestBase<C64, 1>::ErrorSpec{0.0001, 0.0001};
 }
 
-ExhaustiveOpTestBase::ErrorSpec DefaultBF16SpecGenerator(float) {
-  return ExhaustiveOpTestBase::ErrorSpec(0.002, 0.02);
+template <>
+inline ExhaustiveOpTestBase<F64, 1>::ErrorSpec DefaultSpecGenerator<F64, 1>(
+    double) {
+  return ExhaustiveOpTestBase<F64, 1>::ErrorSpec{0.0001, 0.0001};
+}
+
+template <>
+inline ExhaustiveOpTestBase<F32, 1>::ErrorSpec DefaultSpecGenerator<F32, 1>(
+    float) {
+  return ExhaustiveOpTestBase<F32, 1>::ErrorSpec{0.0001, 0.0001};
+}
+
+template <>
+inline ExhaustiveOpTestBase<F16, 1>::ErrorSpec DefaultSpecGenerator<F16, 1>(
+    Eigen::half) {
+  return ExhaustiveOpTestBase<F16, 1>::ErrorSpec{0.001, 0.001};
+}
+
+template <>
+inline ExhaustiveOpTestBase<BF16, 1>::ErrorSpec DefaultSpecGenerator<BF16, 1>(
+    bfloat16) {
+  return ExhaustiveOpTestBase<BF16, 1>::ErrorSpec{0.002, 0.02};
 }
 }  // namespace
 
 /*static*/
-std::function<ExhaustiveOpTestBase::ErrorSpec(float)>
-ExhaustiveOpTestBase::GetDefaultSpecGenerator(PrimitiveType ty) {
-  switch (ty) {
-    case C128:
-    case F64:
-      return DefaultF64SpecGenerator;
-    case C64:
-    case F32:
-      return DefaultF32SpecGenerator;
-    case F16:
-      return DefaultF16SpecGenerator;
-    case BF16:
-      return DefaultBF16SpecGenerator;
-    default:
-      LOG(FATAL) << "Unhandled Type";
-  }
+template <PrimitiveType T, size_t N>
+typename ExhaustiveOpTestBase<T, N>::ErrorSpecGen
+ExhaustiveOpTestBase<T, N>::GetDefaultSpecGenerator() {
+  return DefaultSpecGenerator<T, N>;
 }
+
+template class ExhaustiveOpTestBase<C128, 1>;
+template class ExhaustiveOpTestBase<C64, 1>;
+template class ExhaustiveOpTestBase<F64, 1>;
+template class ExhaustiveOpTestBase<F32, 1>;
+template class ExhaustiveOpTestBase<F16, 1>;
+template class ExhaustiveOpTestBase<BF16, 1>;
 
 }  // namespace xla
