541417b44f82b18d9796175c9c9428b0a85912fc
Jiri Simsa <jsimsa@google.com>
2019-08-01 16:42:09 -0700

    [tf.data] Store a handle to user-defined Python function in StructuredFunctionWrapper so that anonymous resources created by its captured arguments are not deleted after the function is traced.
    PiperOrigin-RevId: 261223930

--- tensorflow/core/grappler/optimizers/data/auto_shard.cc
+++ tensorflow/core/grappler/optimizers/data/auto_shard.cc
@@ -446,7 +446,6 @@ Status AutoShard::OptimizeAndCollectStats(Cluster* /* cluster */,
                                           GraphDef* output,
                                           OptimizationStats* stats) {
   *output = item.graph;
-  VLOG(2) << "auto_shard";
   TF_RETURN_IF_ERROR(OptimizeGraph(item, num_workers_, index_, output));
   stats->num_changes++;
   return Status::OK();
