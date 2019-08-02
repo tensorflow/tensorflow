da5091bf507b8282c798343cdff87e1e23a6549b
fwcore <47165210+fwcore@users.noreply.github.com>
2019-08-02 11:53:51 +0800

    fix changes during last commit
    mistakenly removed proposed change during resolving merge conflict

--- tensorflow/examples/speech_commands/input_data.py
+++ tensorflow/examples/speech_commands/input_data.py
@@ -240,7 +240,7 @@ class AudioProcessor(object):
       statinfo = os.stat(filepath)
       tf.compat.v1.logging.info('Successfully downloaded %s (%d bytes)',
                                 filename, statinfo.st_size)
-    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+      tarfile.open(filepath, 'r:gz').extractall(dest_directory)
 
   def prepare_data_index(self, silence_percentage, unknown_percentage,
                          wanted_words, validation_percentage,
