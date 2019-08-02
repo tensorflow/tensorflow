a6c60966ebaeb56f43343645ced03b3b8b44a5ed
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2019-08-02 09:44:55 +0200

    Fixed cuda runtime deprecation warning
    In cuda 10.0 `memoryType` attribute is deprecated in favour of `type`.

--- tensorflow/stream_executor/cuda/cuda_driver.cc
+++ tensorflow/stream_executor/cuda/cuda_driver.cc
@@ -178,7 +178,7 @@ void CheckPointerIsValid(const PtrT ptr, absl::string_view name) {
   // If we failed, reset cuda error status to avoid poisoning cuda streams.
   if (err != cudaSuccess) cudaGetLastError();
   bool points_to_host_memory = (err == cudaErrorInvalidValue ||
-                                attributes.memoryType != cudaMemoryTypeDevice);
+                                attributes.type != cudaMemoryTypeDevice);
   CHECK_EQ(is_host_ptr, points_to_host_memory) << absl::StreamFormat(
       "%s pointer is not actually on %s: %p", name, is_host_ptr ? "CPU" : "GPU",
       reinterpret_cast<const void*>(ptr));
