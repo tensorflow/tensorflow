e9174365e6e5a37902f61bafd9fa425afbe77b94
A. Unique TensorFlower <gardener@tensorflow.org>
2019-08-05 11:57:04 -0700

    Change _DictFetchMapper to not use __setitem__ on a Mapping, since these may be immutable.
    PiperOrigin-RevId: 261735810

--- tensorflow/python/client/session.py
+++ tensorflow/python/client/session.py
@@ -413,10 +413,9 @@ class _DictFetchMapper(_FetchMapper):
     return self._unique_fetches
 
   def build_results(self, values):
-    results = self._fetch_type()
-    for k, m, vi in zip(self._keys, self._mappers, self._value_indices):
-      results[k] = m.build_results([values[j] for j in vi])
-    return results
+    return self._fetch_type(
+        (k, m.build_results([values[j] for j in vi]))
+        for k, m, vi in zip(self._keys, self._mappers, self._value_indices))
 
 
 class _AttrsFetchMapper(_FetchMapper):
