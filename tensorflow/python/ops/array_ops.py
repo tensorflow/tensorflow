6c5124ea6b4a73d2e5bfcae9501541d5e6f49ddd
I-Hong <ihjhuo@ibm.com>
2019-08-05 14:27:48 -0700

    pylint check

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -308,42 +308,45 @@ listdiff.__doc__ = gen_array_ops.list_diff.__doc__ + "\n" + listdiff.__doc__
                         "Please switch to tf.sets.difference().")
 @tf_export(v1=["setdiff1d"])
 def setdiff1d(x, y, index_dtype=dtypes.int32, name=None):
-  """Computes the difference between two lists of numbers or strings.
+    """Computes the difference between two lists of numbers or strings.
 
-  Given a list x and a list y, this operation returns a list out that represents
-  all values that are in x but not in y. The returned list out is sorted in the
-  same order that the numbers appear in x (duplicates are preserved). This operation
-  also returns a list idx that represents the position of each out element in x.
-  In other words:
+    Given a list x and a list y, this operation returns a list out that
+    represents all values that are in x but not in y. The returned list
+    out is sorted in the same order that the numbers appear in x
+    (duplicates are preserved). This operation also returns a list idx
+    that represents the position of each out element in x.
 
-  ```python
-  out[i] = x[idx[i]] for i in [0, 1, ..., len(out) - 1]
-  ```
+    In other words:
 
-  For example, given this input:
-  ```python
-  x = [1, 2, 3, 4, 5, 6]
-  y = [1, 3, 5]
-  ```
+    ```python
+    out[i] = x[idx[i]] for i in [0, 1, ..., len(out) - 1]
+    ```
 
-  This operation would return:
+    For example, given this input:
+    ```python
+    x = [1, 2, 3, 4, 5, 6]
+    y = [1, 3, 5]
+    ```
 
-  ```python
-  out ==> [2, 4, 6]
-  idx ==> [1, 3, 5]
-  ```
+    This operation would return:
 
-  Args:
-    x: A Tensor. 1-D. Values to keep.
-    y: A Tensor. Must have the same type as x. 1-D. Values to remove.
-    out_idx: An optional tf.DType from: tf.int32, tf.int64. Defaults to tf.int32.
-    name: A name for the operation (optional).
+    ```python
+    out ==> [2, 4, 6]
+    idx ==> [1, 3, 5]
+    ```
 
-  Returns:
-    A tuple of Tensor objects (out, idx).
-      out: A Tensor. Has the same type as x.
-      idx: A Tensor of type out_idx.
-  """
+    Args:
+      x: A Tensor. 1-D. Values to keep.
+      y: A Tensor. Must have the same type as x. 1-D. Values to remove.
+      out_idx: An optional tf.DType from: tf.int32, tf.int64. Defaults
+      to tf.int32.
+      name: A name for the operation (optional).
+
+    Returns:
+      A tuple of Tensor objects (out, idx).
+        out: A Tensor. Has the same type as x.
+        idx: A Tensor of type out_idx.
+    """
   return gen_array_ops.list_diff(x, y, index_dtype, name)
 
 
