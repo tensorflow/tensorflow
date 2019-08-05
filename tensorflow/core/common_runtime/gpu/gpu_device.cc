b801282158c9461fbcf02493bdf043f1af5e511d
Jeff Daily <jeff.daily@amd.com>
2019-08-05 23:33:51 +0000

    force initialization of compute and nccl streams
    This causes the underlying device queue creation so that the compute
    and nccl streams are immediate siblings, for optimal performance.

--- tensorflow/core/common_runtime/gpu/gpu_device.cc
+++ tensorflow/core/common_runtime/gpu/gpu_device.cc
@@ -254,6 +254,13 @@ class BaseGPUDevice::StreamGroupFactory {
       VLOG(2) << "Created nccl_stream[" << stream_group_within_gpu
               << "] = " << group->nccl;
 
+      // ROCm streams are lightweight and will not necessarily trigger device
+      // queue init until they are first used. For optimal performance,
+      // compute and nccl streams must be immediate siblings.
+      // Force underlying resource creation now.
+      group->compute->ThenWaitFor(group->nccl);
+      group->nccl->ThenWaitFor(group->compute);
+
       group->host_to_device = new se::Stream(executor);
       group->host_to_device->Init();
       VLOG(2) << "Created host_to_device_stream[" << stream_group_within_gpu
