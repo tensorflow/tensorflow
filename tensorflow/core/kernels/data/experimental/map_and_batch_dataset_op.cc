ae8a2f79452b84474dd1da257bb00db7ac4ce2b8
Mihai Maruseac <mihaimaruseac@google.com>
2019-08-06 09:35:40 -0700

    Add one include file so that compile would succeed.
    Without
    
    ```c
    ```
    
    compilation will crash with
    
    ```
    ERROR: /tmpfs/src/github/tensorflow/tensorflow/core/kernels/data/experimental/BUILD:360:1: C++ compilation of rule '//tensorflow/core/kernels/data/experimental:take_while_dataset_op' failed (Exit 1)
    tensorflow/core/kernels/data/experimental/take_while_dataset_op.cc:203:36: error: expected constructor, destructor, or type conversion before '(' token
     REGISTER_INPUT_COLOCATION_EXEMPTION("TakeWhileDataset");
                                        ^
    tensorflow/core/kernels/data/experimental/take_while_dataset_op.cc:204:36: error: expected constructor, destructor, or type conversion before '(' token
     REGISTER_INPUT_COLOCATION_EXEMPTION("ExperimentalTakeWhileDataset");
                                        ^
    Target //tensorflow/tools/pip_package:build_pip_package failed to build
    Use --verbose_failures to see the command lines of failed build steps.
    INFO: Elapsed time: 1158.013s, Critical Path: 245.65s
    INFO: 8464 processes: 8464 local.
    FAILED: Build did NOT complete successfully
    FAILED: Build did NOT complete successfully
    ```

--- tensorflow/core/kernels/data/experimental/map_and_batch_dataset_op.cc
+++ tensorflow/core/kernels/data/experimental/map_and_batch_dataset_op.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <utility>
 
 #include "tensorflow/core/common_runtime/function.h"
+#include "tensorflow/core/common_runtime/input_colocation_exemption_registry.h"
 #include "tensorflow/core/common_runtime/metrics.h"
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/partial_tensor_shape.h"
