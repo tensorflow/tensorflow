cc51b10289b15c9eaf64bfc64c21dda319ce8fc1
Anna R <annarev@google.com>
2019-08-07 11:23:28 -0700

    Fixing / disabling a few tests failing when we run with TF v2.

--- tensorflow/examples/adding_an_op/zero_out_1_test.py
+++ tensorflow/examples/adding_an_op/zero_out_1_test.py
@@ -36,7 +36,8 @@ class ZeroOut1Test(tf.test.TestCase):
 
   def testLoadTwice(self):
     zero_out_loaded_again = tf.load_op_library(os.path.join(
-        tf.resource_loader.get_data_files_path(), 'zero_out_op_kernel_1.so'))
+        tf.compat.v1.resource_loader.get_data_files_path(),
+        'zero_out_op_kernel_1.so'))
     self.assertEqual(zero_out_loaded_again, zero_out_op_1._zero_out_module)
 
 
