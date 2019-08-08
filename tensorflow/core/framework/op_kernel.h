8984bd30b49837893b95e44357264f5b4ee95118
Xiao Yu <fishx@google.com>
2019-08-08 13:03:15 -0700

    Only use cancellation_manager to cancel recv_op_ in eager mode.
    There is a race condition in TensorFlow 1.x if assigning a cancellation_manager to recv_op . Because both cancellation_manager.Cancel() and rendezvous::Abort() will be called if a session gets an error.
    
    PiperOrigin-RevId: 262415758

--- tensorflow/core/framework/op_kernel.h
+++ tensorflow/core/framework/op_kernel.h
@@ -621,6 +621,9 @@ class OpKernelContext {
     // The step being executed.
     int64 step_id = 0;
 
+    // True if the op is created by eager runtime.
+    bool is_eager = false;
+
     // The op kernel being computed.
     OpKernel* op_kernel = nullptr;
 
@@ -737,6 +740,8 @@ class OpKernelContext {
   Env* env() const { return params_->device->env(); }
 
   int64 step_id() const { return params_->step_id; }
+
+  bool is_eager() const { return params_->is_eager; }
 
   const OpKernel& op_kernel() const { return *params_->op_kernel; }
 
