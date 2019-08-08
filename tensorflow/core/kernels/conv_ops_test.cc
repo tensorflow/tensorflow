fb2f10e281582739ed272cde89a989210cbfa88a
Deven Desai <deven.desai.amd@gmail.com>
2019-08-08 20:38:52 +0000

    disabling subtests that test features not yet supported on the ROCm platform

--- tensorflow/core/kernels/conv_ops_test.cc
+++ tensorflow/core/kernels/conv_ops_test.cc
@@ -1001,6 +1001,10 @@ class FusedConv2DWithBatchNormOpTest : public FusedConv2DOpTest<T> {};
 TYPED_TEST_SUITE_P(FusedConv2DWithBiasOpTest);
 TYPED_TEST_SUITE_P(FusedConv2DWithBatchNormOpTest);
 
+// ROCm does not yet support the _FusedConv2D op,
+// Therefore disable tests that check _FusedConv2D, when building with ROCm
+
+#ifndef TENSORFLOW_USE_ROCM
 // -------------------------------------------------------------------------- //
 // Conv2D + BiasAdd + {Activation}                                            //
 // -------------------------------------------------------------------------- //
@@ -1165,4 +1169,5 @@ using FusedBatchNormDataTypes = ::testing::Types<float>;
 INSTANTIATE_TYPED_TEST_SUITE_P(Test, FusedConv2DWithBatchNormOpTest,
                                FusedBatchNormDataTypes);
 
+#endif // TENSORFLOW_USE_ROCM
 }  // namespace tensorflow
