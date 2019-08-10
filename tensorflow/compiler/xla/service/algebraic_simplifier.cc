90169feba2d0b082e26c58f7264c353778ffe361
Blake Hechtman <blakehechtman@google.com>
2019-08-10 08:13:00 -0700

    [XLA] Turn gathers of very small vectors into many selects.
    PiperOrigin-RevId: 262726250

--- tensorflow/compiler/xla/service/algebraic_simplifier.cc
+++ tensorflow/compiler/xla/service/algebraic_simplifier.cc
@@ -210,6 +210,8 @@ class AlgebraicSimplifierVisitor : public DfsHloRewriteVisitor {
 
   Status HandleDot(HloInstruction* dot) override;
 
+  Status HandleGather(HloInstruction* gather) override;
+
   Status HandleGetTupleElement(HloInstruction* get_tuple_element) override;
 
   Status HandleLog(HloInstruction* log) override;
@@ -1885,6 +1887,51 @@ Status AlgebraicSimplifierVisitor::HandleDot(HloInstruction* dot) {
         dot, HloInstruction::CreateTranspose(dot->shape(), new_dot, {1, 0}));
   }
 
+  return Status::OK();
+}
+
+Status AlgebraicSimplifierVisitor::HandleGather(HloInstruction* gather) {
+  const Shape& operand_shape = gather->operand(0)->shape();
+  // If the operand of a gather is very small, it is easier to fuse a
+  // sequence of selects.
+  if (operand_shape.rank() == 1 &&
+      operand_shape.dimensions(0) <= options_.very_small_gather_size() &&
+      gather->gather_dimension_numbers().index_vector_dim() ==
+          gather->operand(1)->shape().rank() &&
+      gather->gather_dimension_numbers().collapsed_slice_dims_size() == 1) {
+    const Shape& index_shape = gather->operand(1)->shape();
+    const int64 operand_elements = operand_shape.dimensions(0);
+    auto get_value = [&](int64 i) {
+      auto slice = computation_->AddInstruction(HloInstruction::CreateSlice(
+          ShapeUtil::MakeShape(operand_shape.element_type(), {1}),
+          gather->mutable_operand(0), {i}, {i + 1}, {1}));
+      auto scalar = computation_->AddInstruction(HloInstruction::CreateReshape(
+          ShapeUtil::MakeShape(operand_shape.element_type(), {}), slice));
+      return computation_->AddInstruction(
+          HloInstruction::CreateBroadcast(gather->shape(), scalar, {}));
+    };
+    auto result = get_value(0);
+    auto one = computation_->AddInstruction(HloInstruction::CreateConstant(
+        LiteralUtil::One(index_shape.element_type())));
+    auto index = one;
+    auto pred_shape = ShapeUtil::ChangeElementType(gather->shape(), PRED);
+    auto iter_shape = ShapeUtil::ChangeElementType(gather->shape(),
+                                                   index_shape.element_type());
+    for (int64 i = 1; i < operand_elements; ++i) {
+      auto broadcasted_index = computation_->AddInstruction(
+          HloInstruction::CreateBroadcast(iter_shape, index, {}));
+      auto index_mask =
+          computation_->AddInstruction(HloInstruction::CreateCompare(
+              pred_shape, gather->mutable_operand(1), broadcasted_index,
+              ComparisonDirection::kGe));
+      result = computation_->AddInstruction(
+          HloInstruction::CreateTernary(gather->shape(), HloOpcode::kSelect,
+                                        index_mask, get_value(i), result));
+      index = computation_->AddInstruction(HloInstruction::CreateBinary(
+          index->shape(), HloOpcode::kAdd, index, one));
+    }
+    return ReplaceInstruction(gather, result);
+  }
   return Status::OK();
 }
 
