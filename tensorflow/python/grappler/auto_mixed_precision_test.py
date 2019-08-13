7bd79f21445b0081a29083a58bab133580a38fd6
Ben Barsdell <bbarsdell@nvidia.com>
2019-08-13 11:04:36 -0700

    Fix auto_mixed_precision ingraph_train_loop test

--- tensorflow/python/grappler/auto_mixed_precision_test.py
+++ tensorflow/python/grappler/auto_mixed_precision_test.py
@@ -643,10 +643,11 @@ class AutoMixedPrecisionTest(test.TestCase):
       def body(_, i):
         i += 1
         x, yt = it.get_next()
-        y = layers.Dense(nclass)(x)
+        dense = layers.Dense(nclass)
+        y = dense(x)
         loss = losses.sparse_softmax_cross_entropy(yt, y)
         opt = adam.AdamOptimizer()
-        train_op = opt.minimize(loss)
+        train_op = opt.minimize(loss, var_list=dense.trainable_weights)
         with ops.control_dependencies([train_op]):
           loss = array_ops.identity(loss)
         return loss, i
