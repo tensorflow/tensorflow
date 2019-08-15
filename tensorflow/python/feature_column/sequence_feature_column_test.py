a29555a8fed1b2a1eb495e220eca7b3e3161ea26
A. Unique TensorFlower <gardener@tensorflow.org>
2019-08-15 01:39:51 -0700

    Work around feature column name collision during deserialization
    Otherwise the name collides during deserialization cache lookup, causing e.g. SequenceCategoricalColumn to be deserialized as the column it wraps instead.
    
    This is a workaround rather than just changing name() as the latter would break checkpoints.
    
    PiperOrigin-RevId: 263519137

--- tensorflow/python/feature_column/sequence_feature_column_test.py
+++ tensorflow/python/feature_column/sequence_feature_column_test.py
@@ -759,6 +759,42 @@ class SequenceCategoricalColumnWithIdentityTest(
     _assert_sparse_tensor_value(
         self, expected, self.evaluate(id_weight_pair.id_tensor))
 
+  def test_serialization(self):
+    """Tests that column can be serialized."""
+    parent = sfc.sequence_categorical_column_with_identity(
+        'animal', num_buckets=4)
+    animal = fc.indicator_column(parent)
+
+    config = animal._get_config()
+    self.assertEqual(
+        {
+            'categorical_column': {
+                'class_name': 'SequenceCategoricalColumn',
+                'config': {
+                    'categorical_column': {
+                        'class_name': 'IdentityCategoricalColumn',
+                        'config': {
+                            'default_value': None,
+                            'key': 'animal',
+                            'number_buckets': 4
+                        }
+                    }
+                }
+            }
+        }, config)
+
+    new_animal = fc.IndicatorColumn._from_config(config)
+    self.assertEqual(animal, new_animal)
+    self.assertIsNot(parent, new_animal.categorical_column)
+
+    new_animal = fc.IndicatorColumn._from_config(
+        config,
+        columns_by_name={
+            serialization._column_name_with_class_name(parent): parent
+        })
+    self.assertEqual(animal, new_animal)
+    self.assertIs(parent, new_animal.categorical_column)
+
 
 @test_util.run_all_in_graph_and_eager_modes
 class SequenceCategoricalColumnWithHashBucketTest(
