b363a41e623732009f5abd03d04370c5a6a59f31
Gaurav Jain <gjn@google.com>
2019-08-15 10:20:42 -0700

    Fix issues when tensor equality is enabled
    PiperOrigin-RevId: 263589274
    (cherry picked from commit 8651257d360e06697566b9ee7c859fa7c383e3da)

--- tensorflow/python/kernel_tests/check_ops_test.py
+++ tensorflow/python/kernel_tests/check_ops_test.py
@@ -1605,7 +1605,7 @@ class AssertShapesTest(test.TestCase):
       for shape in shapes:
         regex = (r"Tensor .* must have rank %d.  Received rank %d" %
                  (correct_rank, actual_rank))
-        self.raises_static_error(shapes={x: shape}, regex=regex)
+        self.raises_static_error(shapes=[(x, shape)], regex=regex)
 
     raises_static_rank_error(
         rank_two_shapes, array_ops.ones([1]), correct_rank=2, actual_rank=1)
@@ -1731,18 +1731,18 @@ class AssertShapesTest(test.TestCase):
   def test_dim_size_specified_as_unknown(self):
     x = array_ops.ones([1, 2, 3], name="x")
     y = array_ops.ones([2, 1], name="y")
-    a1 = check_ops.assert_shapes({
+    a1 = check_ops.assert_shapes([
         (x, (None, 2, None)),
         (y, (None, 1)),
-    })
-    a2 = check_ops.assert_shapes({
+    ])
+    a2 = check_ops.assert_shapes([
         (x, (".", 2, ".")),
         (y, (".", 1)),
-    })
-    a3 = check_ops.assert_shapes({
+    ])
+    a3 = check_ops.assert_shapes([
         (x, ".2."),
         (y, ".1"),
-    })
+    ])
     with ops.control_dependencies([a1, a2, a3]):
       out = array_ops.identity(x)
     self.evaluate(out)
