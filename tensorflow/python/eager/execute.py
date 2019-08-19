7b9689b4d1557902258af617b5fa17484ed44c21
Shanqing Cai <cais@google.com>
2019-08-19 11:09:38 -0700

    Add op_callback for instrumentation of eager execution and tf.functions
    op_callback allows access to ops' output tensors as they are
    executed eagerly or constructed in graphs.
    
    Also in this CL:
    - Eager context's `post_execution_callbacks` is made thread-local.
      This should be okay because `add_execution_callback()` is currently
      not a documented part of TF's core API.
    
    See design RFC (approved) at: https://github.com/tensorflow/community/blob/master/rfcs/20190815-tfdbg-v2-callbacks.md
    
    RELNOTES: N/A
    PiperOrigin-RevId: 264196920

--- tensorflow/python/eager/execute.py
+++ tensorflow/python/eager/execute.py
@@ -137,7 +137,7 @@ def execute_with_callbacks(op_name, num_outputs, inputs, attrs, ctx, name=None):
   """Monkey-patch to execute to enable execution callbacks."""
   tensors = quick_execute(op_name, num_outputs, inputs, attrs, ctx, name)
   for callback in ctx.post_execution_callbacks:
-    callback(op_name, inputs, attrs, tensors, name)
+    callback(op_name, tuple(inputs), attrs, tensors, name)
 
   return tensors
 
