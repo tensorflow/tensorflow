e888a37430ea8ec305470f666b4536a1d606d8d7
wenxizhu <wenxi.zhu@intel.com>
2019-08-20 15:40:35 +0800

    Feed test3Dimension with fixed input and filters.

--- tensorflow/python/kernel_tests/slice_op_test.py
+++ tensorflow/python/kernel_tests/slice_op_test.py
@@ -150,22 +150,27 @@ class SliceTest(test.TestCase):
   def test3Dimension(self):
     with self.session() as sess:
       input_shape = [8, 16, 16, 16, 8]
-      inputs = random_ops.random_normal(input_shape,
-                                        dtype=dtypes.float32,
-                                        seed=0)
-      filter_shape = [1, 1, 1, 8, 8]
-      filters = random_ops.random_normal(filter_shape,
-                                         dtype=dtypes.float32,
-                                         seed=0)
+      total_input_size = 1
+      for s in input_shape:
+        total_input_size *= s
+      inputs = [i * 1.0 / total_input_size for i in range(1, total_input_size + 1)]
+      a = constant_op.constant(inputs, shape=input_shape, dtype=dtypes.float32)
 
-      conv_t = nn_ops.conv3d(inputs,
-                             filter=filters,
+      filter_shape = [1, 1, 1, 8, 8]
+      total_filter_size = 1
+      for s in filter_shape:
+        total_filter_size *= s
+      filters = [i * 1.0 / total_filter_size for i in range(1, total_filter_size + 1)]
+      f = constant_op.constant(filters, shape=filter_shape, dtype=dtypes.float32)
+
+      conv_t = nn_ops.conv3d(a,
+                             filter=f,
                              strides=[1, 1, 1, 1, 1],
                              padding="VALID")
       slice_t = array_ops.slice(conv_t, [0, 1, 1, 1, 0], [1, 1, 1, 1, 8])
       result = self.evaluate(slice_t)
-      expected = [6.047066, 1.1073351, -1.4765838, -4.126741,
-                  7.0414743, 4.248739, 0.9407949, -3.58128]
+      expected = [0.03028321, 0.03132677, 0.03237033, 0.03341389,
+                  0.03445745, 0.035501, 0.03654456, 0.03758812]
       self.assertAllClose(expected, result.flatten(), rtol=1e-6)
 
   @test_util.run_deprecated_v1
