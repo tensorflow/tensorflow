1655c6ce203532b21eeec9b913eac9e51dbc5eac
Paul Andrey <paul.andrey@hotmail.fr>
2019-08-21 09:10:14 +0200

    Fixed indentations in noise.py
    Previous commits to fix issue #30834 introduced some 4-spaces indentation, contradicting the 2-spaces indentation norm enforced in the rest of the file. This commit fixes the former.

--- tensorflow/python/keras/layers/noise.py
+++ tensorflow/python/keras/layers/noise.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Layers that operate regularization via the addition of noise.
-"""
+"""Layers that operate regularization via the addition of noise."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -65,8 +65,8 @@ class GaussianNoise(Layer):
 
     def noised():
       return inputs + K.random_normal(
-          shape=array_ops.shape(inputs), mean=0., stddev=self.stddev,
-          dtype=inputs.dtype
+        shape=array_ops.shape(inputs), mean=0., stddev=self.stddev,
+        dtype=inputs.dtype
       )
 
     return K.in_train_phase(noised, inputs, training=training)
@@ -117,8 +117,8 @@ class GaussianDropout(Layer):
       def noised():
         stddev = np.sqrt(self.rate / (1.0 - self.rate))
         return inputs * K.random_normal(
-            shape=array_ops.shape(inputs), mean=1.0, stddev=stddev,
-            dtype=inputs.dtype
+          shape=array_ops.shape(inputs), mean=1.0, stddev=stddev,
+          dtype=inputs.dtype
         )
 
       return K.in_train_phase(noised, inputs, training=training)
@@ -184,7 +184,8 @@ class AlphaDropout(Layer):
         alpha_p = -alpha * scale
 
         kept_idx = math_ops.greater_equal(
-            K.random_uniform(noise_shape, seed=seed), rate)
+          K.random_uniform(noise_shape, seed=seed), rate
+        )
         kept_idx = math_ops.cast(kept_idx, K.floatx())
 
         # Get affine transformation params
