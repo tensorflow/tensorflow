dcb8aa7d83cad848473b99f66146b48e8eb26d50
A. Unique TensorFlower <gardener@tensorflow.org>
2019-08-22 14:18:06 -0700

    Create an XlaLaunchOp kernel directly instead of relying on custom_kernel_creator, if a function needs to be compiled with XLA. After this change, experimental_compile=True no longer requires XLA_CPU/XLA_GPU device dependency when running on CPU/GPU.
    PiperOrigin-RevId: 264917626

--- tensorflow/python/eager/def_function_xla_jit_test.py
+++ tensorflow/python/eager/def_function_xla_jit_test.py
@@ -17,17 +17,31 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from tensorflow.python.eager import backprop
 from tensorflow.python.eager import def_function
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import errors
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import resource_variable_ops
 from tensorflow.python.platform import test
 
 
 class DefFunctionTest(test.TestCase):
 
-  def testCompileFunctionWithXLA(self):
+  def testBasic(self):
+
+    def fn(x, a):
+      return x + a
+
+    func = def_function.function(fn, experimental_compile=False)
+    xla_func = def_function.function(fn, experimental_compile=True)
+
+    inputs = constant_op.constant([1, 2, 2, 3, 3])
+    self.assertAllClose([2, 3, 3, 4, 4], func(inputs, 1))
+    self.assertAllClose([2, 3, 3, 4, 4], xla_func(inputs, 1))
+
+  def testUnsupportedOps(self):
 
     def fn(x):
       return array_ops.unique(x).y  # Unique is not supported by XLA
@@ -37,9 +51,30 @@ class DefFunctionTest(test.TestCase):
 
     inputs = constant_op.constant([1, 2, 2, 3, 3])
     self.assertAllClose([1, 2, 3], func(inputs))
-    with self.assertRaisesRegexp(errors.InvalidArgumentError,
-                                 'node is not compilable'):
+    with self.assertRaisesRegexp(errors.InvalidArgumentError, 'not compilable'):
       xla_func(inputs)
+
+  def testFunctionGradient(self):
+    v = resource_variable_ops.ResourceVariable(2.0)
+
+    def fn(x):
+      return v * x
+
+    func = def_function.function(fn, experimental_compile=False)
+    xla_func = def_function.function(fn, experimental_compile=True)
+
+    x = constant_op.constant(3.0)
+    with backprop.GradientTape() as tape_1:
+      y_1 = func(x)
+    with backprop.GradientTape() as tape_2:
+      y_2 = xla_func(x)
+    dy_1 = tape_1.gradient(y_1, v)
+    dy_2 = tape_2.gradient(y_2, v)
+
+    self.assertAllClose(6.0, y_1)
+    self.assertAllClose(6.0, y_2)
+    self.assertAllClose(3.0, dy_1)
+    self.assertAllClose(3.0, dy_2)
 
 
 if __name__ == '__main__':
