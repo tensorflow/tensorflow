f279c0c000be956e2a25d8a215855a28c0e39649
Derek Murray <mrry@google.com>
2019-08-23 08:04:52 -0700

    [tf.data] Split Grappler-using utilities out from dataset_utils.cc.
    This simplifies the task of building tf.data on platforms that cannot build all of the Grappler dependencies.
    
    PiperOrigin-RevId: 265056563

--- tensorflow/core/kernels/data/experimental/snapshot_dataset_op.cc
+++ tensorflow/core/kernels/data/experimental/snapshot_dataset_op.cc
@@ -19,7 +19,7 @@ limitations under the License.
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor.pb.h"  // NOLINT
 #include "tensorflow/core/grappler/graph_view.h"
-#include "tensorflow/core/kernels/data/dataset_utils.h"
+#include "tensorflow/core/kernels/data/rewrite_utils.h"
 #include "tensorflow/core/lib/core/coding.h"
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/lib/core/raw_coding.h"
