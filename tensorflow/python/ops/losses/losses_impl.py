a9973552637dda57a202d2fe2cb083875c886535
tambulkar <tanish.ambulkar99@gmail.com>
2019-08-24 15:39:30 -0700

    added brier loss

--- tensorflow/python/ops/losses/losses_impl.py
+++ tensorflow/python/ops/losses/losses_impl.py
@@ -255,6 +255,57 @@ def absolute_difference(
     return compute_weighted_loss(
         losses, weights, scope, loss_collection, reduction=reduction)
 
+@tf_export(v1=["losses.brier_score_loss"])
+def brier_score_loss(labels, predictions, weights=1.0, scope=None,
+             loss_collection=ops.GraphKeys.LOSSES,
+             reduction=Reduction.MEAN):
+  """Adds a Brier Score Loss term to the training procedure.
+
+  `weights` acts as a coefficient for the loss. If a scalar is provided, then
+  the loss is simply scaled by the given value. If `weights` is a tensor of size
+  `[batch_size]`, then the total loss for each sample of the batch is rescaled
+  by the corresponding element in the `weights` vector. If the shape of
+  `weights` matches the shape of `predictions`, then the loss of each
+  measurable element of `predictions` is scaled by the corresponding value of
+  `weights`.
+
+  Args:
+    labels: The ground truth output tensor, same dimensions as 'predictions'.
+    predictions: The predicted outputs.
+    weights: Optional `Tensor` whose rank is either 0, or the same rank as
+      `labels`, and must be broadcastable to `labels` (i.e., all dimensions must
+      be either `1`, or the same as the corresponding `losses` dimension).
+    scope: The scope for the operations performed in computing the loss.
+    loss_collection: collection to which the loss will be added.
+    reduction: Type of reduction to apply to loss.
+
+  Returns:
+    Weighted loss float `Tensor`. If `reduction` is `NONE`, this has the same
+    shape as `labels`; otherwise, it is scalar.
+
+  Raises:
+    ValueError: If the shape of `predictions` doesn't match that of `labels` or
+      if the shape of `weights` is invalid.  Also if `labels` or `predictions`
+      is None.
+
+  @compatibility(eager)
+  The `loss_collection` argument is ignored when executing eagerly. Consider
+  holding on to the return value or collecting losses via a `tf.keras.Model`.
+  @end_compatibility
+  """
+  if labels is None:
+    raise ValueError("labels must not be None.")
+  if predictions is None:
+    raise ValueError("predictions must not be None.")
+  with ops.name_scope(scope, "brier_score_loss",
+                      (predictions, labels, weights)) as scope:
+    predictions = math_ops.cast(predictions, dtype=dtypes.float32)
+    labels = math_ops.cast(labels, dtype=dtypes.float32)
+    predictions.get_shape().assert_is_compatible_with(labels.get_shape())
+    losses = math_ops.square(math_ops.subtract(
+        labels, predictions))
+    return compute_weighted_loss(
+        losses, weights, scope, loss_collection, reduction=reduction)
 
 @tf_export(v1=["losses.cosine_distance"])
 @deprecated_args(None, "dim is deprecated, use axis instead", "dim")
