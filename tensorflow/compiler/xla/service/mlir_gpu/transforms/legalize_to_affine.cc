7b8f795debae67f4557939bc4b212f4f52554a79
Alexander Belyaev <pifon@google.com>
2019-09-02 02:15:04 -0700

    Fix function definition/declaration mismatch.
    PiperOrigin-RevId: 266741764

--- tensorflow/compiler/xla/service/mlir_gpu/transforms/legalize_to_affine.cc
+++ tensorflow/compiler/xla/service/mlir_gpu/transforms/legalize_to_affine.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 // This file implements logic for lowering LHLO dialect to Affine dialect.
 
+#include "tensorflow/compiler/xla/service/mlir_gpu/transforms/legalize_to_affine.h"
+
 #include "absl/memory/memory.h"
 #include "mlir/Dialect/AffineOps/AffineOps.h"  // TF:local_config_mlir
 #include "mlir/Dialect/StandardOps/Ops.h"  // TF:local_config_mlir
@@ -187,7 +189,7 @@ struct LegalizeToAffine : public ::mlir::FunctionPass<LegalizeToAffine> {
 
 }  // namespace
 
-std::unique_ptr<mlir::FunctionPassBase> createLegalizeAffinePass() {
+std::unique_ptr<::mlir::FunctionPassBase> createLegalizeAffinePass() {
   return absl::make_unique<LegalizeToAffine>();
 }
 
