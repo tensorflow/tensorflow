db7e43192d405973c6c50f6e60e831a198bb4a49
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-03 15:23:42 -0700

    Automated rollback of commit 2ccea0c7f85fc9a8dbf12f997bbffe7b593358a8
    PiperOrigin-RevId: 267026184

--- tensorflow/python/util/module_wrapper.py
+++ tensorflow/python/util/module_wrapper.py
@@ -90,8 +90,6 @@ class TFModuleWrapper(types.ModuleType):
       deprecation=True,
       has_lite=False):  # pylint: enable=super-on-old-class
     super(TFModuleWrapper, self).__init__(wrapped.__name__)
-    # A cache for all members which do not print deprecations (any more).
-    self._tfmw_attr_map = {}
     self.__dict__.update(wrapped.__dict__)
     # Prefix all local attributes with _tfmw_ so that we can
     # handle them differently in attribute access methods.
@@ -138,8 +136,6 @@ class TFModuleWrapper(types.ModuleType):
               'From %s: The name %s is deprecated. Please use %s instead.\n',
               _call_location(), full_name, rename)
           self._tfmw_warning_count += 1
-          return True
-    return False
 
   def _tfmw_import_module(self, name):
     symbol_loc_info = self._tfmw_public_apis[name]
@@ -153,37 +149,25 @@ class TFModuleWrapper(types.ModuleType):
     return attr
 
   def __getattribute__(self, name):  # pylint: disable=super-on-old-class
-    attr_map = object.__getattribute__(self, '_tfmw_attr_map')
-    try:
-      # Use cached attrs if available
-      return attr_map[name]
-    except KeyError:
-      # Make sure we do not import from tensorflow/lite/__init__.py
-      if name == 'lite':
-        if self._tfmw_has_lite:
-          attr = self._tfmw_import_module(name)
-          setattr(self._tfmw_wrapped_module, 'lite', attr)
-          attr_map[name] = attr
-          return attr
-
-      attr = super(TFModuleWrapper, self).__getattribute__(name)
-
-      # Return and cache dunders and our own members.
-      if name.startswith('__') or name.startswith('_tfmw_'):
-        attr_map[name] = attr
+    # Workaround to make sure we do not import from tensorflow/lite/__init__.py
+    if name == 'lite':
+      if self._tfmw_has_lite:
+        attr = self._tfmw_import_module(name)
+        setattr(self._tfmw_wrapped_module, 'lite', attr)
         return attr
 
-      # Print deprecations, only cache functions after deprecation warnings have
-      # stopped.
-      if not (self._tfmw_print_deprecation_warnings and
-              self._tfmw_add_deprecation_warning(name, attr)):
-        attr_map[name] = attr
+    attr = super(TFModuleWrapper, self).__getattribute__(name)
+    if name.startswith('__') or name.startswith('_tfmw_'):
       return attr
+
+    if self._tfmw_print_deprecation_warnings:
+      self._tfmw_add_deprecation_warning(name, attr)
+    return attr
 
   def __getattr__(self, name):
     try:
       attr = getattr(self._tfmw_wrapped_module, name)
-    except AttributeError:
+    except AttributeError as e:
       if not self._tfmw_public_apis:
         raise
       if name not in self._tfmw_public_apis:
@@ -200,8 +184,6 @@ class TFModuleWrapper(types.ModuleType):
       self.__dict__[arg] = val
       if arg not in self.__all__ and arg != '__all__':
         self.__all__.append(arg)
-      if arg in self._tfmw_attr_map:
-        self._tfmw_attr_map[arg] = val
     super(TFModuleWrapper, self).__setattr__(arg, val)
 
   def __dir__(self):
