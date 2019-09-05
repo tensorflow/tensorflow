0aaead1030f9bba85b0341c95a7780601787bdfd
saishruthi <saishruthi.tn@ibm.com>
2019-09-04 17:41:33 -0700

    addressing comments

--- tensorflow/python/keras/metrics.py
+++ tensorflow/python/keras/metrics.py
@@ -2857,6 +2857,7 @@ def get(identifier):
     raise ValueError('Could not interpret '
                      'metric function identifier: %s' % identifier)
 
+
 class FBetaScore(Metric):
     """Computes F-Beta Score.
 
@@ -2914,35 +2915,44 @@ class FBetaScore(Metric):
        imbalance into account.
 
     Usage:
+
     ```python
     actuals = tf.constant([[1, 1, 0],[1, 0, 0]],
               dtype=tf.int32)
     predis = tf.constant([[1, 0, 0],[1, 0, 1]],
              dtype=tf.int32)
     # F-Beta Micro
-    fb_score = tfa.metrics.FBetaScore(num_classes=3,
+    fb_score = tf.keras.metrics.FBetaScore(num_classes=3,
                 beta=0.4, average='micro')
     fb_score.update_state(actuals, preds)
     print('F1-Beta Score is: ',
            fb_score.result().numpy()) # 0.6666666
     # F-Beta Macro
-    fb_score = tfa.metrics.FBetaScore(num_classes=3,
+    fb_score = tf.keras.metrics.FBetaScore(num_classes=3,
            beta=0.4, average='macro')
     fb_score.update_state(actuals, preds)
     print('F1-Beta Score is: ',
           fb_score.result().numpy()) # 0.33333334
     # F-Beta Weighted
-    fb_score = tfa.metrics.FBetaScore(num_classes=3,
+    fb_score = tf.keras.metrics.FBetaScore(num_classes=3,
                beta=0.4, average='weighted')
     fb_score.update_state(actuals, preds)
     print('F1-Beta Score is: ',
           fb_score.result().numpy()) # 0.6666667
     # F-Beta score for each class (average=None).
-    fb_score = tfa.metrics.FBetaScore(num_classes=3,
+    fb_score = tf.keras.metrics.FBetaScore(num_classes=3,
                beta=0.4, average=None)
     fb_score.update_state(actuals, preds)
     print('F1-Beta Score is: ',
          fb_score.result().numpy()) # [1. 0. 0.]
+    ```
+
+    Usage with tf.keras API:
+
+    ```python
+    model = tf.keras.Model(inputs, outputs)
+    model.compile('sgd', loss='mse', metrics=[tf.keras.metrics.FBetaScore(
+                   num_classes=3, beta=0.4, average='weighted')])
     ```
     """
 
@@ -2951,20 +2961,20 @@ class FBetaScore(Metric):
                  average=None,
                  beta=1.0,
                  name='fbeta_score',
-                 dtype=tf.float32):
+                 dtype=dtypes.float32):
         super(FBetaScore, self).__init__(name=name)
         self.num_classes = num_classes
         # type check
-        if not isinstance(beta, float) and beta.dtype != tf.float32:
-            raise TypeError("The value of beta should be float")
+        if not isinstance(beta, float) and beta.dtype != dtypes.float32:
+            raise TypeError('The value of beta should be float')
         # value check
         if beta <= 0.0:
-            raise ValueError("beta value should be greater than zero")
+            raise ValueError('beta value should be greater than zero')
         else:
             self.beta = beta
         if average not in (None, 'micro', 'macro', 'weighted'):
-            raise ValueError("Unknown average type. Acceptable values "
-                             "are: [None, micro, macro, weighted]")
+            raise ValueError('Unknown average type. Acceptable values '
+                             'are: [None, micro, macro, weighted]')
         else:
             self.average = average
             if self.average == 'micro':
@@ -3011,54 +3021,57 @@ class FBetaScore(Metric):
 
     # TO DO SSaishruthi: Add sample weight option
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_true = tf.cast(y_true, tf.int32)
-        y_pred = tf.cast(y_pred, tf.int32)
+        y_true = math_ops.cast(y_true, dtypes.int32)
+        y_pred = math_ops.cast(y_pred, dtypes.int32)
 
         # true positive
         self.true_positives.assign_add(
-            tf.cast(
-                tf.math.count_nonzero(y_pred * y_true, axis=self.axis),
+            math_ops.cast(
+                math_ops.count_nonzero(y_pred * y_true, axis=self.axis),
                 self.dtype))
         # false positive
         self.false_positives.assign_add(
-            tf.cast(
-                tf.math.count_nonzero(y_pred * (y_true - 1), axis=self.axis),
+            math_ops.cast(
+                math_ops.count_nonzero(y_pred * (y_true - 1), axis=self.axis),
                 self.dtype))
         # false negative
         self.false_negatives.assign_add(
-            tf.cast(
-                tf.math.count_nonzero((y_pred - 1) * y_true, axis=self.axis),
+            math_ops.cast(
+                math_ops.count_nonzero((y_pred - 1) * y_true, axis=self.axis),
                 self.dtype))
         if self.average == 'weighted':
             # variable to hold intermediate weights
             self.weights_intermediate.assign_add(
-                tf.cast(tf.reduce_sum(y_true, axis=self.axis), self.dtype))
+                math_ops.cast(math_ops.reduce_sum(y_true, axis=self.axis),
+                              self.dtype))
 
     def result(self):
-        p_sum = tf.cast(self.true_positives + self.false_positives, self.dtype)
+        p_sum = math_ops.cast(self.true_positives + self.false_positives,
+                              self.dtype)
         # calculate precision
-        precision = tf.math.divide_no_nan(self.true_positives, p_sum)
+        precision = math_ops.divide_no_nan(self.true_positives, p_sum)
 
-        r_sum = tf.cast(self.true_positives + self.false_negatives, self.dtype)
+        r_sum = math_ops.cast(self.true_positives + self.false_negatives,
+                              self.dtype)
         # calculate recall
-        recall = tf.math.divide_no_nan(self.true_positives, r_sum)
+        recall = math_ops.divide_no_nan(self.true_positives, r_sum)
         # intermediate calculations
         mul_value = precision * recall
-        add_value = (tf.math.square(self.beta) * precision) + recall
-        f1_int = (1 + tf.math.square(self.beta)) * (tf.math.divide_no_nan(
+        add_value = (math_ops.square(self.beta) * precision) + recall
+        f1_int = (1 + math_ops.square(self.beta)) * (math_ops.divide_no_nan(
             mul_value, add_value))
         # f1 score
         if self.average is not None:
-            f1_score = tf.reduce_mean(f1_int)
+            f1_score = math_ops.reduce_mean(f1_int)
         else:
             f1_score = f1_int
         # condition for weighted f1 score
         if self.average == 'weighted':
-            f1_int_weights = tf.math.divide_no_nan(
+            f1_int_weights = math_ops.divide_no_nan(
                 self.weights_intermediate,
-                tf.reduce_sum(self.weights_intermediate))
+                math_ops.reduce_sum(self.weights_intermediate))
             # weighted f1 score calculation
-            f1_score = tf.reduce_sum(f1_int * f1_int_weights)
+            f1_score = math_ops.reduce_sum(f1_int * f1_int_weights)
 
         return f1_score
 
@@ -3143,33 +3156,41 @@ class F1Score(FBetaScore):
     preds = tf.constant([[1, 0, 0],[1, 0, 1]],
               dtype=tf.int32)
     # F1 Micro
-    output = tfa.metrics.F1Score(num_classes=3,
+    output = tf.keras.metrics.F1Score(num_classes=3,
               average='micro')
     output.update_state(actuals, preds)
     print('F1 Micro score is: ',
             output.result().numpy()) # 0.6666667
     # F1 Macro
-    output = tfa.metrics.F1Score(num_classes=3,
+    output = tf.keras.metrics.F1Score(num_classes=3,
                 average='macro')
     output.update_state(actuals, preds)
     print('F1 Macro score is: ',
             output.result().numpy()) # 0.33333334
     # F1 weighted
-    output = tfa.metrics.F1Score(num_classes=3,
+    output = tf.keras.metrics.F1Score(num_classes=3,
               average='weighted')
     output.update_state(actuals, preds)
     print('F1 Weighted score is: ',
             output.result().numpy()) # 0.6666667
     # F1 score for each class (average=None).
-    output = tfa.metrics.F1Score(num_classes=3)
+    output = tf.keras.metrics.F1Score(num_classes=3)
     output.update_state(actuals, preds)
     print('F1 score is: ',
             output.result().numpy()) # [1. 0. 0.]
     ```
+
+    Usage with tf.keras API:
+
+    ```python
+    model = tf.keras.Model(inputs, outputs)
+    model.compile('sgd', loss='mse', metrics=[tf.keras.metrics.F1Score(
+                   num_classes=3, average='weighted')])
+    ```
     """
 
     def __init__(self, num_classes, average, name='f1_score',
-                 dtype=tf.float32):
+                 dtype=dtypes.float32):
         super(F1Score, self).__init__(
             num_classes, average, 1.0, name=name, dtype=dtype)
 
