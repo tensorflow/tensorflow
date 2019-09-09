a19506fda0e9547d1b4c23dbfc2f74a6d7c4de2c
Benjamin Kramer <kramerb@google.com>
2019-09-09 06:38:57 -0700

    [TF:XLA] Sync einsum supported types with matmul supported types
    This is currently not that interesting as there is no way of calling
    XlaEinsum with these types, but will become more important when
    TF starts using XLA's einsum directly.
    
    PiperOrigin-RevId: 267985223

--- tensorflow/compiler/tf2xla/kernels/einsum_op.cc
+++ tensorflow/compiler/tf2xla/kernels/einsum_op.cc
@@ -25,8 +25,8 @@ limitations under the License.
 namespace tensorflow {
 namespace {
 
-constexpr std::array<DataType, 3> kEinsumTypes = {
-    {DT_COMPLEX64, DT_BFLOAT16, DT_FLOAT}};
+constexpr std::array<DataType, 6> kEinsumTypes = {
+    {DT_HALF, DT_BFLOAT16, DT_FLOAT, DT_DOUBLE, DT_COMPLEX64, DT_COMPLEX128}};
 
 class EinsumOp : public XlaOpKernel {
  public:
@@ -39,8 +39,6 @@ class EinsumOp : public XlaOpKernel {
   void Compile(XlaOpKernelContext* ctx) override {
     xla::XlaOp lhs = ctx->Input(0);
     xla::XlaOp rhs = ctx->Input(1);
-    const TensorShape a_shape = ctx->InputShape(0);
-    const TensorShape b_shape = ctx->InputShape(1);
     ctx->SetOutput(0, xla::Einsum(lhs, rhs, equation_));
   }
 
