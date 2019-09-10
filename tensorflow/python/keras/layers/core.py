c71c58beb262152d4b486eb92ff29b917584c201
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-10 14:47:19 -0700

    Add support for passive serialization in Keras. This lets users pass in any object that supports Keras serialization to any config, and ensures that the resulting to/from config is fully JSON-compatible.
    PiperOrigin-RevId: 268314367

--- tensorflow/python/keras/layers/core.py
+++ tensorflow/python/keras/layers/core.py
@@ -1007,7 +1007,8 @@ class Dense(Layer):
 
     super(Dense, self).__init__(
         activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
-    self.units = int(units)
+
+    self.units = int(units) if not isinstance(units, int) else units
     self.activation = activations.get(activation)
     self.use_bias = use_bias
     self.kernel_initializer = initializers.get(kernel_initializer)
