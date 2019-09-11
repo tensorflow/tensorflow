97b65731acbb5e05992c63dee3131a79f903679b
Hye Soo Yang <hyey@google.com>
2019-09-11 13:03:03 -0700

    Automated rollback of commit b2c58f3225fb5260b0c055a2b8f226c39a18f828
    PiperOrigin-RevId: 268523903

--- tensorflow/python/__init__.py
+++ tensorflow/python/__init__.py
@@ -48,7 +48,6 @@ import numpy as np
 
 from tensorflow.python import pywrap_tensorflow
 from tensorflow.python import _pywrap_utils
-from tensorflow.python import _pywrap_tfprof
 
 # Protocol buffers
 from tensorflow.core.framework.graph_pb2 import *
