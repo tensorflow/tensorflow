c7d1f70237708112b8a4af3d92ee8730359ea479
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-13 11:30:31 -0700

    Added function to check upfront that tensor can be created.
    PiperOrigin-RevId: 268946070

--- tensorflow/lite/delegates/gpu/cl/tensor.h
+++ tensorflow/lite/delegates/gpu/cl/tensor.h
@@ -148,6 +148,10 @@ class TensorBHWC : public Tensor {
 
 using TensorPtr = std::shared_ptr<Tensor>;
 
+bool CanCreateTensorWithShape(const CLContext& context, const CLDevice& device,
+                              const BHWC& shape,
+                              const TensorDescriptor& descriptor);
+
 Status AllocateTensorMemory(const CLContext& context, const CLDevice& device,
                             int width, int height, int channels,
                             DataType data_type, TensorStorageType storage_type,
