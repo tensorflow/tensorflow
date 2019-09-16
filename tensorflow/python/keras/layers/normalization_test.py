4f833ea9b81fa3da1d8a3bd03f26ad5dcafe6e9a
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2019-09-16 17:45:24 +0530

    Changed input shape to include batch dim.
    Changed input shape as requested to (2, 8, 8, 3).

--- tensorflow/python/keras/layers/normalization_test.py
+++ tensorflow/python/keras/layers/normalization_test.py
@@ -542,7 +542,7 @@ class LayerNormalizationTest(keras_parameterized.TestCase):
     testing_utils.layer_test(
         keras.layers.LayerNormalization,
         kwargs={'axis': (-3, -2, -1)},
-        input_shape=(64, 64, 3))
+        input_shape=(2, 8, 8, 3))
 
   @tf_test_util.run_in_graph_and_eager_modes
   def test_layernorm_weights(self):
