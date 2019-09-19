86c400385169d8c2837dfce6319d017fb0c39fd0
Edward Loper <edloper@google.com>
2019-09-19 06:12:40 -0700

    Updated tf.io.parse_example and tf.io.parse_single_example to support ragged features.
    * Added tf.io.RaggedFeature
    * Updated tf.io.parse_example and tf.io.parse_single_example to accept RaggedFeature.
    
    PiperOrigin-RevId: 270024281

--- tensorflow/python/ops/parallel_for/pfor.py
+++ tensorflow/python/ops/parallel_for/pfor.py
@@ -3258,6 +3258,38 @@ def _convert_parse_single_example(pfor_input):
   return [wrap(t, True, True) for t in nest.flatten(output)]
 
 
+@RegisterPFor("ParseExampleV2")
+def _convert_parse_example_v2(pfor_input):
+  serialized = pfor_input.stacked_input(0)
+  sparse_keys = pfor_input.unstacked_input(2)
+  dense_keys = pfor_input.unstacked_input(3)
+  ragged_keys = pfor_input.unstacked_input(4)
+  dense_defaults = [
+      pfor_input.unstacked_input(i) for i in range(5, pfor_input.num_inputs)
+  ]
+  num_sparse = pfor_input.get_attr("num_sparse")
+  sparse_types = pfor_input.get_attr("sparse_types")
+  ragged_value_types = pfor_input.get_attr("ragged_value_types")
+  ragged_split_types = pfor_input.get_attr("ragged_split_types")
+  dense_shapes = pfor_input.get_attr("dense_shapes")
+  if serialized.shape.ndims not in (None, 1):
+    raise ValueError("ParseExampleV2 can only be converted if `serialized` "
+                     "is scalar.")
+  output = gen_parsing_ops.parse_example_v2(
+      serialized=serialized,
+      names=[],
+      sparse_keys=sparse_keys,
+      dense_keys=dense_keys,
+      ragged_keys=ragged_keys,
+      dense_defaults=dense_defaults,
+      num_sparse=num_sparse,
+      sparse_types=sparse_types,
+      ragged_value_types=ragged_value_types,
+      ragged_split_types=ragged_split_types,
+      dense_shapes=dense_shapes)
+  return [wrap(t, True, True) for t in nest.flatten(output)]
+
+
 # functional_ops
 
 
