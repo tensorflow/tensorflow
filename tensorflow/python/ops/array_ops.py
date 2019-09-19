636ebac2f54a017f44fa5aa1482004ff59b93f70
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-19 14:27:24 -0700

    Update the docstring of tf.reshape.
    PiperOrigin-RevId: 270125266

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -59,70 +59,133 @@ _BaseSlice = slice
 def reshape(tensor, shape, name=None):  # pylint: disable=redefined-outer-name
   r"""Reshapes a tensor.
 
-  Given `tensor`, this operation returns a tensor that has the same values
-  as `tensor` with shape `shape`.
+  Given `tensor`, this operation returns a new `tf.Tensor` that has the same
+  values as `tensor` in the same order, except with a new shape given by
+  `shape`.
+
+  >>> t1 = [[1, 2, 3],
+  ...       [4, 5, 6]]
+  >>> print(tf.shape(t1).numpy())
+  [2 3]
+  >>> t2 = tf.reshape(t1, [6])
+  >>> t2
+  <tf.Tensor: id=394, shape=(6,), dtype=int32,
+    numpy=array([1, 2, 3, 4, 5, 6], dtype=int32)>
+  >>> tf.reshape(t2, [3, 2])
+  <tf.Tensor: id=396, shape=(3, 2), dtype=int32, numpy=
+    array([[1, 2],
+           [3, 4],
+           [5, 6]], dtype=int32)>
+
+  The `tf.reshape` does not change the order of or the total number of elements
+  in the tensor, and so it can reuse the underlying data buffer. This makes it
+  a fast operation independent of how big of a tensor it is operating on.
+
+  >>> tf.reshape([1, 2, 3], [2, 2])
+  Traceback (most recent call last):
+  ...
+  InvalidArgumentError: Input to reshape is a tensor with 3 values, but the
+  requested shape has 4
+
+  To instead reorder the data to rearrange the dimensions of a tensor, see
+  `tf.transpose`.
+
+  >>> t = [[1, 2, 3],
+  ...      [4, 5, 6]]
+  >>> tf.reshape(t, [3, 2]).numpy()
+  array([[1, 2],
+         [3, 4],
+         [5, 6]], dtype=int32)
+  >>> tf.transpose(t, perm=[1, 0]).numpy()
+  array([[1, 4],
+         [2, 5],
+         [3, 6]], dtype=int32)
 
   If one component of `shape` is the special value -1, the size of that
   dimension is computed so that the total size remains constant.  In particular,
   a `shape` of `[-1]` flattens into 1-D.  At most one component of `shape` can
   be -1.
 
-  If `shape` is 1-D or higher, then the operation returns a tensor with shape
-  `shape` filled with the values of `tensor`. In this case, the number of
-  elements implied by `shape` must be the same as the number of elements in
-  `tensor`.
+  >>> t = [[1, 2, 3],
+  ...      [4, 5, 6]]
+  >>> tf.reshape(t, [-1])
+  <tf.Tensor: id=407, shape=(6,), dtype=int32,
+    numpy=array([1, 2, 3, 4, 5, 6], dtype=int32)>
+  >>> tf.reshape(t, [3, -1])
+  <tf.Tensor: id=410, shape=(3, 2), dtype=int32, numpy=
+    array([[1, 2],
+           [3, 4],
+           [5, 6]], dtype=int32)>
+  >>> tf.reshape(t, [-1, 2])
+  <tf.Tensor: id=413, shape=(3, 2), dtype=int32, numpy=
+    array([[1, 2],
+           [3, 4],
+           [5, 6]], dtype=int32)>
 
-  For example:
+  `tf.reshape(t, [])` reshapes a tensor `t` with one element to a scalar.
 
-  ```
-  # tensor 't' is [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  # tensor 't' has shape [9]
-  reshape(t, [3, 3]) ==> [[1, 2, 3],
-                          [4, 5, 6],
-                          [7, 8, 9]]
+  >>> tf.reshape([7], []).numpy()
+  7
 
-  # tensor 't' is [[[1, 1], [2, 2]],
-  #                [[3, 3], [4, 4]]]
-  # tensor 't' has shape [2, 2, 2]
-  reshape(t, [2, 4]) ==> [[1, 1, 2, 2],
-                          [3, 3, 4, 4]]
+  More examples:
 
-  # tensor 't' is [[[1, 1, 1],
-  #                 [2, 2, 2]],
-  #                [[3, 3, 3],
-  #                 [4, 4, 4]],
-  #                [[5, 5, 5],
-  #                 [6, 6, 6]]]
-  # tensor 't' has shape [3, 2, 3]
-  # pass '[-1]' to flatten 't'
-  reshape(t, [-1]) ==> [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6]
+  >>> t = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  >>> print(tf.shape(t).numpy())
+  [9]
+  >>> tf.reshape(t, [3, 3])
+  <tf.Tensor: id=421, shape=(3, 3), dtype=int32, numpy=
+    array([[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]], dtype=int32)>
 
-  # -1 can also be used to infer the shape
+  >>> t = [[[1, 1], [2, 2]],
+  ...      [[3, 3], [4, 4]]]
+  >>> print(tf.shape(t).numpy())
+  [2 2 2]
+  >>> tf.reshape(t, [2, 4])
+  <tf.Tensor: id=426, shape=(2, 4), dtype=int32, numpy=
+    array([[1, 1, 2, 2],
+           [3, 3, 4, 4]], dtype=int32)>
 
-  # -1 is inferred to be 9:
-  reshape(t, [2, -1]) ==> [[1, 1, 1, 2, 2, 2, 3, 3, 3],
-                           [4, 4, 4, 5, 5, 5, 6, 6, 6]]
-  # -1 is inferred to be 2:
-  reshape(t, [-1, 9]) ==> [[1, 1, 1, 2, 2, 2, 3, 3, 3],
-                           [4, 4, 4, 5, 5, 5, 6, 6, 6]]
-  # -1 is inferred to be 3:
-  reshape(t, [ 2, -1, 3]) ==> [[[1, 1, 1],
-                                [2, 2, 2],
-                                [3, 3, 3]],
-                               [[4, 4, 4],
-                                [5, 5, 5],
-                                [6, 6, 6]]]
-
-  # tensor 't' is [7]
-  # shape `[]` reshapes to a scalar
-  reshape(t, []) ==> 7
-  ```
+  >>> t = [[[1, 1, 1],
+  ...       [2, 2, 2]],
+  ...      [[3, 3, 3],
+  ...       [4, 4, 4]],
+  ...      [[5, 5, 5],
+  ...       [6, 6, 6]]]
+  >>> print(tf.shape(t).numpy())
+  [3 2 3]
+  >>> # Pass '[-1]' to flatten 't'.
+  >>> tf.reshape(t, [-1])
+  <tf.Tensor: id=431, shape=(18,), dtype=int32,
+    numpy=array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6],
+    dtype=int32)>
+  >>> # -- Using -1 to infer the shape --
+  >>> # Here -1 is inferred to be 9:
+  >>> tf.reshape(t, [2, -1])
+  <tf.Tensor: id=434, shape=(2, 9), dtype=int32, numpy=
+    array([[1, 1, 1, 2, 2, 2, 3, 3, 3],
+           [4, 4, 4, 5, 5, 5, 6, 6, 6]], dtype=int32)>
+  >>> # -1 is inferred to be 2:
+  >>> tf.reshape(t, [-1, 9])
+  <tf.Tensor: id=437, shape=(2, 9), dtype=int32, numpy=
+    array([[1, 1, 1, 2, 2, 2, 3, 3, 3],
+           [4, 4, 4, 5, 5, 5, 6, 6, 6]], dtype=int32)>
+  >>> # -1 is inferred to be 3:
+  >>> tf.reshape(t, [ 2, -1, 3])
+  <tf.Tensor: id=440, shape=(2, 3, 3), dtype=int32, numpy=
+    array([[[1, 1, 1],
+            [2, 2, 2],
+            [3, 3, 3]],
+           [[4, 4, 4],
+            [5, 5, 5],
+            [6, 6, 6]]], dtype=int32)>
 
   Args:
     tensor: A `Tensor`.
     shape: A `Tensor`. Must be one of the following types: `int32`, `int64`.
       Defines the shape of the output tensor.
-    name: A name for the operation (optional).
+    name: Optional string. A name for the operation.
 
   Returns:
     A `Tensor`. Has the same type as `tensor`.
