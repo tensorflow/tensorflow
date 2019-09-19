d7496b17b72023e79fc1f392b704c20dcf355add
Ashutosh Malve <ashutoshmalve@hotmail.com>
2019-09-19 20:24:50 +0530

    Update file_io_test.py

--- tensorflow/python/lib/io/file_io_test.py
+++ tensorflow/python/lib/io/file_io_test.py
@@ -3,7 +3,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License at 
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
