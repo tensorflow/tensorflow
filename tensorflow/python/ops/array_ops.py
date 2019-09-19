520358458eade1c635dca16304186a425423d8c9
Adam Wood <adamwood@google.com>
2019-09-19 15:19:57 -0700

    - added testable doc strings to tf.zeros  - clarified args list
    PiperOrigin-RevId: 270137064

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -2366,17 +2366,17 @@ def zeros(shape, dtype=dtypes.float32, name=None):
   This operation returns a tensor of type `dtype` with shape `shape` and
   all elements set to zero.
 
-  For example:
-
-  ```python
-  tf.zeros([3, 4], tf.int32)  # [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-  ```
+  >>> tf.zeros([3, 4], tf.int32)
+  <tf.Tensor: id=..., shape=(3, 4), dtype=int32, numpy=
+  array([[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]], dtype=int32)>
 
   Args:
-    shape: A list of integers, a tuple of integers, or a 1-D `Tensor` of type
-      `int32`.
-    dtype: The type of an element in the resulting `Tensor`.
-    name: A name for the operation (optional).
+    shape: A `list` of integers, a `tuple` of integers, or
+      a 1-D `Tensor` of type `int32`.
+    dtype: The DType of an element in the resulting `Tensor`.
+    name: Optional string. A name for the operation.
 
   Returns:
     A `Tensor` with all elements set to zero.
