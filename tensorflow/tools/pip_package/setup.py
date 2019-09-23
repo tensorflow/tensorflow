3aded2100b8458c10925bc03f933579d31bc9643
Mihai Maruseac <mihaimaruseac@google.com>
2019-09-23 09:50:25 -0700

    Pin Estimator version to newly released 2.0.0

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -63,7 +63,7 @@ REQUIRED_PACKAGES = [
     'six >= 1.10.0',
     'protobuf >= 3.6.1',
     'tensorboard >= 2.0.0, < 2.1.0',
-    'tf-estimator-nightly >= 1.14.0.dev2019080601, < 1.14.0.dev2019080602',
+    'tensorflow_estimator >= 2.0.0, < 2.1.0',
     'termcolor >= 1.1.0',
     'wrapt >= 1.11.1',
 ]
