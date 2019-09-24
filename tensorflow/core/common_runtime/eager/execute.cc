4d374435ea9b9037291bf59318f69ddf569fc8e7
Xiao Yu <fishx@google.com>
2019-09-24 12:10:51 -0700

    Internal change
    PiperOrigin-RevId: 270958373

--- tensorflow/core/common_runtime/eager/execute.cc
+++ tensorflow/core/common_runtime/eager/execute.cc
@@ -484,7 +484,7 @@ Status EagerLocalExecute(EagerOperation* op, TensorHandle** retvals,
                                profiler::TraceMeLevel::kInfo);
     input_dev_ptrs.reserve(op->Inputs().size());
     // All inputs need to be on local devices.
-    // TODO(b/122851476): This is a limitation of the current code base (but
+    // TODO(b/134094971): This is a limitation of the current code base (but
     // should be possible to get around).
     // Code changes will need to be made to pass input objects to the
     // function library runtime instead of just "Tensor"s.
