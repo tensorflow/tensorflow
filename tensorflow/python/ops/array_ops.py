0d2a032f738ef5830a47380b001f1bc6e172a563
autoih <ihjhuo@ibm.com>
2019-09-26 11:16:43 -0700

    doctest

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -3940,49 +3940,19 @@ def reverse_sequence(input,
   input slice `i`, with the first `seq_lengths[i]` slices along
   dimension `seq_axis` reversed.
 
-  For example:
+  Example usage:
 
-  ```python
-  # Given this:
-  batch_dim = 0
-  seq_dim = 1
-  input.dims = (4, 8, ...)
-  seq_lengths = [7, 2, 3, 5]
-
-  # then slices of input are reversed on seq_dim, but only up to seq_lengths:
-  output[0, 0:7, :, ...] = input[0, 7:0:-1, :, ...]
-  output[1, 0:2, :, ...] = input[1, 2:0:-1, :, ...]
-  output[2, 0:3, :, ...] = input[2, 3:0:-1, :, ...]
-  output[3, 0:5, :, ...] = input[3, 5:0:-1, :, ...]
-
-  # while entries past seq_lens are copied through:
-  output[0, 7:, :, ...] = input[0, 7:, :, ...]
-  output[1, 2:, :, ...] = input[1, 2:, :, ...]
-  output[2, 3:, :, ...] = input[2, 3:, :, ...]
-  output[3, 2:, :, ...] = input[3, 2:, :, ...]
-  ```
-
-  In contrast, if:
-
-  ```python
-  # Given this:
-  batch_dim = 2
-  seq_dim = 0
-  input.dims = (8, ?, 4, ...)
-  seq_lengths = [7, 2, 3, 5]
-
-  # then slices of input are reversed on seq_dim, but only up to seq_lengths:
-  output[0:7, :, 0, :, ...] = input[7:0:-1, :, 0, :, ...]
-  output[0:2, :, 1, :, ...] = input[2:0:-1, :, 1, :, ...]
-  output[0:3, :, 2, :, ...] = input[3:0:-1, :, 2, :, ...]
-  output[0:5, :, 3, :, ...] = input[5:0:-1, :, 3, :, ...]
-
-  # while entries past seq_lens are copied through:
-  output[7:, :, 0, :, ...] = input[7:, :, 0, :, ...]
-  output[2:, :, 1, :, ...] = input[2:, :, 1, :, ...]
-  output[3:, :, 2, :, ...] = input[3:, :, 2, :, ...]
-  output[2:, :, 3, :, ...] = input[2:, :, 3, :, ...]
-  ```
+  >>> seq_lengths = [7, 2, 3, 5]
+  >>> input = [[1, 2, 3, 4, 5, 0, 0, 0], [1, 2, 0, 0, 0, 0, 0, 0],
+  ... [1, 2, 3, 4, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8]]
+  >>> output = reverse_sequence(input, seq_lens, seq_dim=1, batch_dim=0)
+  <tf.Tensor 'ReverseSequence_3:0' shape=(4, 8) dtype=int32>
+  >>> sess = tf.Session()
+  >>> print(sess.run(output))
+  [[0 0 5 4 3 2 1 0]
+  [2 1 0 0 0 0 0 0]
+  [3 2 1 4 0 0 0 0]
+  [5 4 3 2 1 6 7 8]]
 
   Args:
     `input`: A `Tensor`. The input to reverse.
