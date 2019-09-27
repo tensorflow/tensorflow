1d2a7658ed92841f42f9b94cf59cd4c3f735585e
Jiri Simsa <jsimsa@google.com>
2019-09-27 09:50:45 -0700

    [tf.data] Support tf.data optimizations for input pipelines with `cache` and `shuffle` transformations inside of tf.functions.
    PiperOrigin-RevId: 271590712

--- tensorflow/python/data/ops/dataset_ops.py
+++ tensorflow/python/data/ops/dataset_ops.py
@@ -1609,10 +1609,7 @@ class DatasetV2(tracking_base.Trackable, composite_tensor.CompositeTensor):
     reduce_func = wrapped_func.function
     reduce_func.add_to_graph(ops.get_default_graph())
 
-    # TODO(b/141256846): Apply options once optimizing stateful input pipelines
-    # in tf.functions is supported.
-    # dataset = self._apply_options()
-    dataset = self
+    dataset = self._apply_options()
 
     # pylint: disable=protected-access
     return structure.from_compatible_tensor_list(
