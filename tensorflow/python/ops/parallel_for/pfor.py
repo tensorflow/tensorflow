1119e57ab00a601bbe6de15e818c22075f77874d
A. Unique TensorFlower <gardener@tensorflow.org>
2019-09-27 13:04:41 -0700

    Clean up parallel_for tests to avoid explict loop_fn_dtype argument. Also fixes a bug in conversion of ShapeN.
    PiperOrigin-RevId: 271631238

--- tensorflow/python/ops/parallel_for/pfor.py
+++ tensorflow/python/ops/parallel_for/pfor.py
@@ -2513,8 +2513,8 @@ def _convert_shape(pfor_input):
 def _convert_shape_n(pfor_input):
   out_type = pfor_input.get_attr("out_type")
   shapes = [
-      array_ops.shape(x, out_type=out_type)[1:]
-      if stacked else array_ops.shape(x) for x, stacked, _ in pfor_input.inputs
+      array_ops.shape(x, out_type=out_type)[1:] if stacked else array_ops.shape(
+          x, out_type=out_type) for x, stacked, _ in pfor_input.inputs
   ]
   return [wrap(x, False) for x in shapes]
 
