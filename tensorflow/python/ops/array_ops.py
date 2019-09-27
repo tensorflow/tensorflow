fee9409fb7154b21e71cbba488c8ec3c28fbd65c
autoih <ihjhuo@ibm.com>
2019-09-27 11:41:10 -0700

    print

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -3944,8 +3944,9 @@ def reverse_sequence(input,
 
   >>> seq_lengths = [7, 2, 3, 5]
   >>> input = [[1, 2, 3, 4, 5, 0, 0, 0], [1, 2, 0, 0, 0, 0, 0, 0],
-  ... [1, 2, 3, 4, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8]]
+  ...          [1, 2, 3, 4, 0, 0, 0, 0], [1, 2, 3, 4, 5, 6, 7, 8]]
   >>> output = reverse_sequence(input, seq_lens, seq_dim=1, batch_dim=0)
+  >>> print(output)
   <tf.Tensor: id=5, shape=(4, 8), dtype=int32, numpy=
   array([[0, 0, 5, 4, 3, 2, 1, 0],
        [2, 1, 0, 0, 0, 0, 0, 0],
