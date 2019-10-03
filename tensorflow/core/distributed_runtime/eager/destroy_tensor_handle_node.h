ff650ad4c46ad7d7cf47b335f3698f66fee13edc
Xiao Yu <fishx@google.com>
2019-10-02 18:28:56 -0700

    Improve TF 2.0 error message: 1. Avoid appending "Encountered when executing an operation..." at the end of each error. 2. Avoid redundant "W 0916 11:48:32.515855  41375 eager_client_thread destroy_tensor_handle_node.h:48] Ignoring an error encountered when deleting..." when client is crashed.
    PiperOrigin-RevId: 272565152

--- tensorflow/core/distributed_runtime/eager/destroy_tensor_handle_node.h
+++ tensorflow/core/distributed_runtime/eager/destroy_tensor_handle_node.h
@@ -16,8 +16,10 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_EAGER_DESTROY_TENSOR_HANDLE_NODE_H_
 #define TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_EAGER_DESTROY_TENSOR_HANDLE_NODE_H_
 
+#include "tensorflow/core/common_runtime/eager/context.h"
 #include "tensorflow/core/common_runtime/eager/eager_executor.h"
 #include "tensorflow/core/distributed_runtime/eager/eager_client.h"
+#include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/protobuf/eager_service.pb.h"
 
 namespace tensorflow {
@@ -28,29 +30,63 @@ namespace eager {
 class DestroyTensorHandleNode : public tensorflow::AsyncEagerNode {
  public:
   DestroyTensorHandleNode(std::unique_ptr<EnqueueRequest> request,
-                          EagerClient* eager_client, bool ready)
+                          EagerContext* ctx, const string& remote_task,
+                          bool ready)
       : tensorflow::AsyncEagerNode(),
         request_(std::move(request)),
-        eager_client_(eager_client),
-        ready_(ready) {}
+        ctx_(ctx),
+        remote_task_(remote_task),
+        ready_(ready) {
+    ctx_->Ref();
+  }
+
+  ~DestroyTensorHandleNode() override { ctx_->Unref(); }
 
   void RunAsync(StatusCallback done) override {
+    auto context_id = request_->context_id();
+    if (ctx_->GetContextId() != context_id) {
+      // This means that this tensor was pointing to a remote device, which
+      // has been changed out from under us. Simply return since there is
+      // nothing we can do.
+      done(Status::OK());
+      return;
+    }
+
+    eager::EagerClient* eager_client;
+    Status status = ctx_->GetClient(remote_task_, &eager_client);
+    if (!status.ok()) {
+      LOG(INFO) << "Unable to destroy remote tensor handle because the target "
+                << remote_task_ << " is no longer available.";
+      done(Status::OK());
+      return;
+    }
+
     EnqueueResponse* response = new EnqueueResponse;
     bool ready = ready_;
+    auto context = ctx_;
+    context->Ref();
     // NOTE(fishx): Don't use StreamingEnqueueAsync here. When a
     // StreamingEnqueueAsync request fails all following requests will fail as
     // well. We don't want this request poison following requests since it is
     // safe to ignore a failing destroy tensor handle request.
-    eager_client_->EnqueueAsync(
+    eager_client->EnqueueAsync(
         request_.get(), response,
-        [response, ready, done](const tensorflow::Status& s) {
-          if (!s.ok() && ready) {
+        [response, ready, context, context_id,
+         done](const tensorflow::Status& s) {
+          // Omit the warning if:
+          // 1. The remote tensor isn't ready.
+          // 2. ServerDef has been reset.
+          // 3. Lost connection to remote worker. In this case client will
+          //    crash. We don't want to spam user with redundant warning logs.
+          if (!s.ok() && ready && context->GetContextId() == context_id &&
+              s.code() != errors::Code::UNAVAILABLE) {
             LOG(WARNING) << "Ignoring an error encountered when deleting "
                             "remote tensors handles: "
                          << s.ToString();
           }
-          done(Status::OK());
+          context->Unref();
           delete response;
+          done(Status::OK());
         });
   }
 
@@ -64,7 +100,8 @@ class DestroyTensorHandleNode : public tensorflow::AsyncEagerNode {
 
  private:
   std::unique_ptr<EnqueueRequest> request_;
-  EagerClient* eager_client_;  // Not owned, and must outlive this node.
+  EagerContext* ctx_;
+  const string remote_task_;
   bool ready_;
 };
 
