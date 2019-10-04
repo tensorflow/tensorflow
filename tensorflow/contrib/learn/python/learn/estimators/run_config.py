tensorflow/estimator

Expose session_creation_timeout_secs in estimator RunConfig
PiperOrigin-RevId: 267290523

tensorflow_estimator/python/estimator/run_config.py
@@ -54,6 +54,7 @@ _DEFAULT_REPLACEABLE_LIST = [
     'eval_distribute',
     'experimental_distribute',
     'experimental_max_worker_delay_secs',
+    'session_creation_timeout_secs',
 ]
 
 _SAVE_CKPT_ERR = (
@@ -300,6 +301,10 @@ def _validate_properties(run_config):
   _validate('experimental_max_worker_delay_secs', _validate_delay,
             message='experimental_max_worker_delay_secs must be an integer if'
             ' set.')
+  _validate(
+      'session_creation_timeout_secs',
+      lambda timeout_secs: timeout_secs > 0,
+      message='session_creation_timeout_secs should be > 0')
 
   _validate('device_fn', lambda device_fn: six.callable(device_fn) and
             set(function_utils.fn_args(device_fn)) == _VALID_DEVICE_FN_ARGS,
@@ -349,7 +354,8 @@ class RunConfig(object):
                protocol=None,
                eval_distribute=None,
                experimental_distribute=None,
-               experimental_max_worker_delay_secs=None):
+               experimental_max_worker_delay_secs=None,
+               session_creation_timeout_secs=7200):
     """Constructs a RunConfig.
 
     All distributed training related properties `cluster_spec`, `is_chief`,
@@ -502,6 +508,11 @@ class RunConfig(object):
         the weights of a randomly initialized model. Users who warm-start their
         models and train them for short durations (a few minutes or less) should
         consider reducing this default to improve training times.
+      session_creation_timeout_secs: Max time workers should wait for a session
+        to become available (on initialization or when recovering a session)
+        with MonitoredTrainingSession. Defaults to 7200 seconds, but users may
+        want to set a lower value to detect problems with variable / session
+        (re)-initialization more quickly.
 
     Raises:
       ValueError: If both `save_checkpoints_steps` and `save_checkpoints_secs`
@@ -543,7 +554,8 @@ class RunConfig(object):
         protocol=protocol,
         eval_distribute=eval_distribute,
         experimental_distribute=experimental_distribute,
-        experimental_max_worker_delay_secs=experimental_max_worker_delay_secs)
+        experimental_max_worker_delay_secs=experimental_max_worker_delay_secs,
+        session_creation_timeout_secs=session_creation_timeout_secs)
 
     # TODO(frankchn,priyag): Eventually use distributed coordinator for TPUs.
     if ((train_distribute and
@@ -804,6 +816,10 @@ class RunConfig(object):
   @property
   def keep_checkpoint_max(self):
     return self._keep_checkpoint_max
+
+  @property
+  def session_creation_timeout_secs(self):
+    return self._session_creation_timeout_secs
 
   @property
   def keep_checkpoint_every_n_hours(self):

tensorflow/tensorflow

tensorflow/contrib/learn/python/learn/estimators/run_config.py
@@ -243,7 +243,8 @@ class RunConfig(ClusterConfig, core_run_config.RunConfig):
                protocol=None,
                evaluation_master='',
                model_dir=None,
-               session_config=None):
+               session_config=None,
+               session_creation_timeout_secs=7200):
     """Constructor.
 
     The superclass `ClusterConfig` may set properties like `cluster_spec`,
@@ -282,6 +283,8 @@ class RunConfig(ClusterConfig, core_run_config.RunConfig):
         the feature.
       log_step_count_steps: The frequency, in number of global steps, that the
         global step/sec will be logged during training.
+      protocol: An optional argument which specifies the protocol used when
+        starting server. None means default to grpc.
       evaluation_master: the master on which to perform evaluation.
       model_dir: directory where model parameters, graph etc are saved. If
         `None`, will use `model_dir` property in `TF_CONFIG` environment
@@ -290,8 +293,11 @@ class RunConfig(ClusterConfig, core_run_config.RunConfig):
       session_config: a ConfigProto used to set session parameters, or None.
         Note - using this argument, it is easy to provide settings which break
         otherwise perfectly good models. Use with care.
-      protocol: An optional argument which specifies the protocol used when
-        starting server. None means default to grpc.
+      session_creation_timeout_secs: Max time workers should wait for a session
+        to become available (on initialization or when recovering a session)
+        with MonitoredTrainingSession. Defaults to 7200 seconds, but users may
+        want to set a lower value to detect problems with variable / session
+        (re)-initialization more quickly.
     """
     # Neither parent class calls super().__init__(), so here we have to
     # manually call their __init__() methods.
@@ -332,6 +338,7 @@ class RunConfig(ClusterConfig, core_run_config.RunConfig):
     self._keep_checkpoint_max = keep_checkpoint_max
     self._keep_checkpoint_every_n_hours = keep_checkpoint_every_n_hours
     self._model_dir = _get_model_dir(model_dir)
+    self._session_creation_timeout_secs = session_creation_timeout_secs
 
   @experimental
   def uid(self, whitelist=None):
@@ -407,6 +414,10 @@ class RunConfig(ClusterConfig, core_run_config.RunConfig):
   @property
   def log_step_count_steps(self):
     return self._log_step_count_steps
+
+  @property
+  def session_creation_timeout_secs(self):
+    return self._session_creation_timeout_secs
 
 
 def _count_ps(cluster_spec):