3e3640c2e1bc4d5630ecae67c9d0757f228a3ddf
Karim Nosir <karimnosseir@google.com>
2019-10-04 07:57:46 -0700

    Fix failing test in OSS
    PiperOrigin-RevId: 272876246

--- tensorflow/lite/python/tflite_convert_test.py
+++ tensorflow/lite/python/tflite_convert_test.py
@@ -46,7 +46,7 @@ class TestModels(test_util.TensorFlowTestCase):
 
   def _run(self, flags_str, should_succeed):
     output_file = os.path.join(self.get_temp_dir(), 'model.tflite')
-    tflite_bin = resource_loader.get_path_to_datafile('tflite_convert.par')
+    tflite_bin = resource_loader.get_path_to_datafile('tflite_convert')
     cmdline = '{0} --output_file={1} {2}'.format(tflite_bin, output_file,
                                                  flags_str)
 
