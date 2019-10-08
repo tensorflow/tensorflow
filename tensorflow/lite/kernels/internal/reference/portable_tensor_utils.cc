8c7479aadb77eadf3ee0d31b41ebc7d5737af02f
Jian Li <jianlijianli@google.com>
2019-10-08 07:47:21 -0700

    Optimize portable MatrixScalarMultiply method.
    PiperOrigin-RevId: 273517762

--- tensorflow/lite/kernels/internal/reference/portable_tensor_utils.cc
+++ tensorflow/lite/kernels/internal/reference/portable_tensor_utils.cc
@@ -281,9 +281,11 @@ void PortableMatrixScalarMultiplyAccumulate(const int8_t* matrix,
                                             int32_t scalar, int32_t n_row,
                                             int32_t n_col, int32_t* output) {
   for (int i = 0; i < n_row; ++i) {
+    int32_t row_sum = 0;
     for (int j = 0; j < n_col; ++j) {
-      output[i] += matrix[i * n_col + j] * scalar;
+      row_sum += *matrix++;
     }
+    output[i] += row_sum * scalar;
   }
 }
 
