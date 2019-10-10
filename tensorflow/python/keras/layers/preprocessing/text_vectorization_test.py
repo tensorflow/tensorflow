402ccb1803480d5596c507ecada11c68214b5d55
Scott Zhu <scottzhu@google.com>
2019-10-10 11:25:09 -0700

    Remove the __init__ content for keras/saving.
    All the direct dependencies has been changed to use explicit import, rather than rely on the __init__ shortcut.
    
    PiperOrigin-RevId: 274001008

--- tensorflow/python/keras/layers/preprocessing/text_vectorization_test.py
+++ tensorflow/python/keras/layers/preprocessing/text_vectorization_test.py
@@ -31,9 +31,9 @@ from tensorflow.python import keras
 from tensorflow.python.eager import context
 from tensorflow.python.framework import dtypes
 from tensorflow.python.keras import keras_parameterized
-from tensorflow.python.keras import saving
 from tensorflow.python.keras import testing_utils
 from tensorflow.python.keras.layers.preprocessing import preprocessing_test_utils
+from tensorflow.python.keras.saving import saved_model_experimental as saving
 from tensorflow.python.keras.utils.generic_utils import CustomObjectScope
 from tensorflow.python.platform import test
 
