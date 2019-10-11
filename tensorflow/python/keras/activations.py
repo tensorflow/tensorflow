894b0394f9886ec558e9f3bf31c0649c0aeef07f
Lukas Geiger <lukas.geiger94@gmail.com>
2019-10-11 15:03:58 +0100

    Use tf.nn.selu in keras activations

--- tensorflow/python/keras/activations.py
+++ tensorflow/python/keras/activations.py
@@ -153,9 +153,7 @@ def selu(x):
       [Self-Normalizing Neural Networks (Klambauer et al, 2017)]
       (https://arxiv.org/abs/1706.02515)
   """
-  alpha = 1.6732632423543772848170429916717
-  scale = 1.0507009873554804934193349852946
-  return scale * K.elu(x, alpha)
+  return nn.selu(x)
 
 
 @keras_export('keras.activations.softplus')
