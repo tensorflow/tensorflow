9f547cfa39ce1b04efd6f2431bb41cf3ad07db86
A. Unique TensorFlower <gardener@tensorflow.org>
2019-10-16 11:52:33 -0700

    Makes spv.module generated by GPU->SPIRV conversion spec compliant
    Makes the spv.module generated by the GPU to SPIR-V conversion SPIR-V
    spec compliant (validated using spirv-val from Vulkan tools).
    
    1) Separate out the VulkanLayoutUtils from
    DecorateSPIRVCompositeTypeLayoutPass to make it reusable within the
    Type converter in SPIR-V lowering infrastructure. This is used to
    compute the layout of the !spv.struct used in global variable type
    description.
    2) Set the capabilities of the spv.module to Shader (needed for use of
    Logical Memory Model, and the extensions to
    SPV_KHR_storage_buffer_storage_class for use of Storage Buffer)
    
    PiperOrigin-RevId: 275081486
    Change-Id: I4dcc7241c64bb4cbf95d45ed4efe754902e9b841

--- third_party/mlir/lib/Dialect/SPIRV/Transforms/DecorateSPIRVCompositeTypeLayoutPass.cpp
+++ third_party/mlir/lib/Dialect/SPIRV/Transforms/DecorateSPIRVCompositeTypeLayoutPass.cpp
@@ -22,202 +22,13 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Dialect/SPIRV/LayoutUtils.h"
 #include "mlir/Dialect/SPIRV/Passes.h"
 #include "mlir/Dialect/SPIRV/SPIRVDialect.h"
 #include "mlir/Dialect/SPIRV/SPIRVOps.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 using namespace mlir;
-
-/// According to the Vulkan spec "14.5.4. Offset and Stride Assignment":
-/// "There are different alignment requirements depending on the specific
-/// resources and on the features enabled on the device."
-///
-/// There are 3 types of alignment: scalar, base, extended.
-/// See the spec for details.
-///
-/// Note: Even if scalar alignment is supported, it is generally more
-/// performant to use the base alignment. So here the calculation is based on
-/// base alignment.
-///
-/// The memory layout must obey the following rules:
-/// 1. The Offset decoration of any member must be a multiple of its alignment.
-/// 2. Any ArrayStride or MatrixStride decoration must be a multiple of the
-/// alignment of the array or matrix as defined above.
-///
-/// According to the SPIR-V spec:
-/// "The ArrayStride, MatrixStride, and Offset decorations must be large
-/// enough to hold the size of the objects they affect (that is, specifying
-/// overlap is invalid)."
-namespace {
-class VulkanLayoutUtils {
-public:
-  using Alignment = uint64_t;
-
-  /// Returns a new type with layout info. Assigns the type size in bytes to the
-  /// `size`. Assigns the type alignment in bytes to the `alignment`.
-  static Type decorateType(spirv::StructType structType,
-                           spirv::StructType::LayoutInfo &size,
-                           Alignment &alignment);
-  /// Checks whether a type is legal in terms of Vulkan layout info
-  /// decoration. A type is dynamically illegal if it's a composite type in the
-  /// StorageBuffer, PhysicalStorageBuffer, Uniform, and PushConstant Storage
-  /// Classes without layout informtation.
-  static bool isLegalType(Type type);
-
-private:
-  static Type decorateType(Type type, spirv::StructType::LayoutInfo &size,
-                           Alignment &alignment);
-  static Type decorateType(VectorType vectorType,
-                           spirv::StructType::LayoutInfo &size,
-                           Alignment &alignment);
-  static Type decorateType(spirv::ArrayType arrayType,
-                           spirv::StructType::LayoutInfo &size,
-                           Alignment &alignment);
-  /// Calculates the alignment for the given scalar type.
-  static Alignment getScalarTypeAlignment(Type scalarType);
-};
-
-Type VulkanLayoutUtils::decorateType(spirv::StructType structType,
-                                     spirv::StructType::LayoutInfo &size,
-                                     VulkanLayoutUtils::Alignment &alignment) {
-  if (structType.getNumElements() == 0) {
-    return structType;
-  }
-
-  llvm::SmallVector<Type, 4> memberTypes;
-  llvm::SmallVector<spirv::StructType::LayoutInfo, 4> layoutInfo;
-  llvm::SmallVector<spirv::StructType::MemberDecorationInfo, 4>
-      memberDecorations;
-
-  spirv::StructType::LayoutInfo structMemberOffset = 0;
-  VulkanLayoutUtils::Alignment maxMemberAlignment = 1;
-
-  for (uint32_t i = 0, e = structType.getNumElements(); i < e; ++i) {
-    spirv::StructType::LayoutInfo memberSize = 0;
-    VulkanLayoutUtils::Alignment memberAlignment = 1;
-
-    auto memberType = VulkanLayoutUtils::decorateType(
-        structType.getElementType(i), memberSize, memberAlignment);
-    structMemberOffset = llvm::alignTo(structMemberOffset, memberAlignment);
-    memberTypes.push_back(memberType);
-    layoutInfo.push_back(structMemberOffset);
-    // According to the Vulkan spec:
-    // "A structure has a base alignment equal to the largest base alignment of
-    // any of its members."
-    structMemberOffset += memberSize;
-    maxMemberAlignment = std::max(maxMemberAlignment, memberAlignment);
-  }
-
-  // According to the Vulkan spec:
-  // "The Offset decoration of a member must not place it between the end of a
-  // structure or an array and the next multiple of the alignment of that
-  // structure or array."
-  size = llvm::alignTo(structMemberOffset, maxMemberAlignment);
-  alignment = maxMemberAlignment;
-  structType.getMemberDecorations(memberDecorations);
-  return spirv::StructType::get(memberTypes, layoutInfo, memberDecorations);
-}
-
-Type VulkanLayoutUtils::decorateType(Type type,
-                                     spirv::StructType::LayoutInfo &size,
-                                     VulkanLayoutUtils::Alignment &alignment) {
-  if (spirv::SPIRVDialect::isValidScalarType(type)) {
-    alignment = VulkanLayoutUtils::getScalarTypeAlignment(type);
-    // Vulkan spec does not specify any padding for a scalar type.
-    size = alignment;
-    return type;
-  }
-
-  switch (type.getKind()) {
-  case spirv::TypeKind::Struct:
-    return VulkanLayoutUtils::decorateType(type.cast<spirv::StructType>(), size,
-                                           alignment);
-  case spirv::TypeKind::Array:
-    return VulkanLayoutUtils::decorateType(type.cast<spirv::ArrayType>(), size,
-                                           alignment);
-  case StandardTypes::Vector:
-    return VulkanLayoutUtils::decorateType(type.cast<VectorType>(), size,
-                                           alignment);
-  default:
-    llvm_unreachable("unhandled SPIR-V type");
-  }
-}
-
-Type VulkanLayoutUtils::decorateType(VectorType vectorType,
-                                     spirv::StructType::LayoutInfo &size,
-                                     VulkanLayoutUtils::Alignment &alignment) {
-  const auto numElements = vectorType.getNumElements();
-  auto elementType = vectorType.getElementType();
-  spirv::StructType::LayoutInfo elementSize = 0;
-  VulkanLayoutUtils::Alignment elementAlignment = 1;
-
-  auto memberType = VulkanLayoutUtils::decorateType(elementType, elementSize,
-                                                    elementAlignment);
-  // According to the Vulkan spec:
-  // 1. "A two-component vector has a base alignment equal to twice its scalar
-  // alignment."
-  // 2. "A three- or four-component vector has a base alignment equal to four
-  // times its scalar alignment."
-  size = elementSize * numElements;
-  alignment = numElements == 2 ? elementAlignment * 2 : elementAlignment * 4;
-  return VectorType::get(numElements, memberType);
-}
-
-Type VulkanLayoutUtils::decorateType(spirv::ArrayType arrayType,
-                                     spirv::StructType::LayoutInfo &size,
-                                     VulkanLayoutUtils::Alignment &alignment) {
-  const auto numElements = arrayType.getNumElements();
-  auto elementType = arrayType.getElementType();
-  spirv::ArrayType::LayoutInfo elementSize = 0;
-  VulkanLayoutUtils::Alignment elementAlignment = 1;
-
-  auto memberType = VulkanLayoutUtils::decorateType(elementType, elementSize,
-                                                    elementAlignment);
-  // According to the Vulkan spec:
-  // "An array has a base alignment equal to the base alignment of its element
-  // type."
-  size = elementSize * numElements;
-  alignment = elementAlignment;
-  return spirv::ArrayType::get(memberType, numElements, elementSize);
-}
-
-VulkanLayoutUtils::Alignment
-VulkanLayoutUtils::getScalarTypeAlignment(Type scalarType) {
-  // According to the Vulkan spec:
-  // 1. "A scalar of size N has a scalar alignment of N."
-  // 2. "A scalar has a base alignment equal to its scalar alignment."
-  // 3. "A scalar, vector or matrix type has an extended alignment equal to its
-  // base alignment."
-  auto bitWidth = scalarType.getIntOrFloatBitWidth();
-  if (bitWidth == 1)
-    return 1;
-  return bitWidth / 8;
-}
-
-bool VulkanLayoutUtils::isLegalType(Type type) {
-  auto ptrType = type.dyn_cast<spirv::PointerType>();
-  if (!ptrType) {
-    return true;
-  }
-
-  auto storageClass = ptrType.getStorageClass();
-  auto structType = ptrType.getPointeeType().dyn_cast<spirv::StructType>();
-  if (!structType) {
-    return true;
-  }
-
-  switch (storageClass) {
-  case spirv::StorageClass::Uniform:
-  case spirv::StorageClass::StorageBuffer:
-  case spirv::StorageClass::PushConstant:
-  case spirv::StorageClass::PhysicalStorageBuffer:
-    return structType.hasLayout() || !structType.getNumElements();
-  default:
-    return true;
-  }
-}
-} // namespace
 
 namespace {
 class SPIRVGlobalVariableOpLayoutInfoDecoration
@@ -228,7 +39,7 @@ public:
   PatternMatchResult matchAndRewrite(spirv::GlobalVariableOp op,
                                      PatternRewriter &rewriter) const override {
     spirv::StructType::LayoutInfo structSize = 0;
-    VulkanLayoutUtils::Alignment structAlignment = 1;
+    VulkanLayoutUtils::Size structAlignment = 1;
     SmallVector<NamedAttribute, 4> globalVarAttrs;
 
     auto ptrType = op.type().cast<spirv::PointerType>();
