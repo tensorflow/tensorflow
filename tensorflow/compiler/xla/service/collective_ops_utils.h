4cd92b561749ec3775102b98ac995430f03be444
George Karpenkov <cheshire@google.com>
2019-10-17 10:30:41 -0700

    [XLA] [NFC] Move RendezvousKey and ParticipantData collective_ops_utils
    RendezvousKey implementation can be reused across different collective ops on
    different backends.
    
    PiperOrigin-RevId: 275287636
    Change-Id: Ia11701e0df16977678fd75ca61d87f324d479b0a

--- tensorflow/compiler/xla/service/collective_ops_utils.h
+++ tensorflow/compiler/xla/service/collective_ops_utils.h
@@ -18,8 +18,10 @@ limitations under the License.
 
 #include <vector>
 
+#include "tensorflow/compiler/xla/executable_run_options.h"
 #include "tensorflow/compiler/xla/service/computation_placer.h"
 #include "tensorflow/compiler/xla/service/hlo_instruction.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
 #include "tensorflow/compiler/xla/statusor.h"
 
 namespace xla {
@@ -29,6 +31,105 @@ namespace xla {
 StatusOr<std::vector<int64>> GetParticipatingReplicas(
     int64 device_ordinal, const HloInstruction* instr,
     int64 total_replica_count, const DeviceAssignment& device_assn);
+
+// Key that identifies a particular Rendezvous object in our global hashtable.
+// This determines which calls to ExecuteOnStream communicate with each other.
+// The rules are as follows.
+//
+// * Only ops with the same RunId can communicate with each other. (This is the
+//   whole purpose of RunId).
+//
+// * Only ops with the same set of participating replicas can communicate with
+//   each other.  This is how we separate out different replica groups (e.g. a
+//   single AllReduce HLO might do two reductions, between say GPUs {0,2} and
+//   {1,3}).
+//
+// * Only ops with the same opcode can communicate with each other.  At the
+//   moment we only support kAllReduce, so we don't check for this explicitly.
+//
+// * For cross-module all-reduces (i.e. instr->channel_id().has_value()),
+//   only ops with the same value for channel_id() can communicate with each
+//   other.
+//
+// * For cross-replica (i.e. same-module) all-reduces (i.e.
+//   !channel_id().has_value()), only ops from the same module (as
+//   identified by its unique_id()) can communicate with each other.
+//
+struct RendezvousKey {
+  enum CollectiveOpKind {
+    kCrossModule,
+    kCrossReplica,
+  };
+
+  explicit RendezvousKey(const RunId& run_id,
+                         std::vector<int64> participating_replicas,
+                         const HloInstruction* instr)
+      : run_id(run_id), participating_replicas(participating_replicas) {
+    std::tie(collective_op_kind, op_id) =
+        instr->channel_id().has_value()
+            ? std::make_pair(kCrossModule, instr->channel_id().value())
+            : std::make_pair(
+                  kCrossReplica,
+                  static_cast<int64>(instr->GetModule()->unique_id()));
+  }
+
+  int num_participants() const { return participating_replicas.size(); }
+
+  template <typename H>
+  friend H AbslHashValue(H h, const RendezvousKey& k) {
+    return H::combine(std::move(h), k.run_id, k.participating_replicas,
+                      static_cast<int>(k.collective_op_kind), k.op_id);
+  }
+  friend bool operator==(const RendezvousKey& a, const RendezvousKey& b) {
+    return a.run_id == b.run_id &&
+           a.participating_replicas == b.participating_replicas &&
+           a.collective_op_kind == b.collective_op_kind &&  //
+           a.op_id == b.op_id;
+  }
+  friend bool operator!=(const RendezvousKey& a, const RendezvousKey& b) {
+    return !(a == b);
+  }
+
+  string ToString() const {
+    return absl::StrFormat(
+        "RendezvousKey{run_id=%s, participating_replicas=[%s], "
+        "collective_op_kind=%d, op_id=%d}",
+        run_id.ToString(), absl::StrJoin(participating_replicas, ","),
+        static_cast<int>(collective_op_kind), op_id);
+  }
+
+  RunId run_id;
+  std::vector<int64> participating_replicas;
+  CollectiveOpKind collective_op_kind;
+  int64 op_id;
+};
+
+// Encapsulates parameters to Rendezvous::SubmitParticipant.
+struct ParticipantData {
+  explicit ParticipantData(RendezvousKey rendezvous_key)
+      : rendezvous_key(rendezvous_key) {}
+
+  int64 element_count;
+  int64 device_ordinal;
+  RendezvousKey rendezvous_key;
+
+  // TODO(b/125951860): We should vet that we're buffer allocating such that
+  // source_buffer == destination_buffer if that avoids a NCCL copy (will depend
+  // on how well the NCCL in-place implementation performs vs the out-of-place
+  // implementation).
+  se::DeviceMemoryBase source_data;
+  se::DeviceMemoryBase destination_data;
+  se::Stream* stream;
+
+  int num_participants() const { return rendezvous_key.num_participants(); }
+
+  string ToString() const {
+    return absl::StrFormat(
+        "ParticipantData{element_count=%d, rendezvous_key=%s, "
+        "device_ordinal=%d, stream=%p}",
+        element_count, rendezvous_key.ToString(), device_ordinal, stream);
+  }
+};
 
 }  // end namespace xla
 
