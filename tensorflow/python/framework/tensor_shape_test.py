52354aaa7f2693162098623aa58c73dea1149cc0
Sergei Lebedev <slebedev@google.com>
2019-10-23 12:33:12 -0700

    Do not mask errors in Dimension.{is_compatible_with,merge_with}
    PiperOrigin-RevId: 276328746
    Change-Id: I6d046d460332ebaee7453176202cf75221414af9

--- tensorflow/python/framework/tensor_shape_test.py
+++ tensorflow/python/framework/tensor_shape_test.py
@@ -169,6 +169,20 @@ class DimensionTest(test_util.TensorFlowTestCase):
     self.assertIsNone(tensor_shape.Dimension(None) != None)  # pylint: disable=g-equals-none
     self.assertNotEqual(tensor_shape.Dimension(12), 12.99)
 
+  def testIsCompatibleWithError(self):
+    with self.assertRaisesRegex(TypeError, "must be integer or None"):
+      tensor_shape.Dimension(42).is_compatible_with([])
+
+    with self.assertRaisesRegex(ValueError, "must be >= 0"):
+      tensor_shape.Dimension(42).is_compatible_with(-1)
+
+  def testMergeWithError(self):
+    with self.assertRaisesRegex(TypeError, "must be integer or None"):
+      tensor_shape.Dimension(42).merge_with([])
+
+    with self.assertRaisesRegex(ValueError, "must be >= 0"):
+      tensor_shape.Dimension(42).merge_with(-1)
+
   def testRepr(self):
     self.assertEqual(repr(tensor_shape.Dimension(7)), "Dimension(7)")
     self.assertEqual(repr(tensor_shape.Dimension(None)), "Dimension(None)")
