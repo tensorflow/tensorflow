c8eeb8ad22c357bbb9f2914784259370a394bdc4
Tim Shen <timshen@google.com>
2019-10-24 14:49:43 -0700

    [XLA/MLIR_GPU] Rework on handling initial shapes. Specifically: * For all buffers, use identity AffineMap. Move their original affine maps to their uses (loads and stores). This is because when lowering buffers to LLVM, MemRefs are reuiqred to have identity affine maps. * Move the creation of the generated MLIR function into conv_emitter.h, so that the logic that peeks into HloInstruction is in one place. * Rework on permuting shapes.
    PiperOrigin-RevId: 276570801
    Change-Id: Iad69d681f95bc369953e6baee21b813a53af4f8e

--- tensorflow/compiler/xla/service/mlir_gpu/experimental/conv_emitter/conv_emitter.cc
+++ tensorflow/compiler/xla/service/mlir_gpu/experimental/conv_emitter/conv_emitter.cc
@@ -27,16 +27,17 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/service/mlir_gpu/experimental/conv_emitter/conv_emitter.h"
 
-#include "mlir/Dialect/AffineOps/AffineOps.h"
-#include "mlir/Dialect/StandardOps/Ops.h"
 #include "absl/types/span.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/Support/raw_ostream.h"
+#include "mlir/Dialect/AffineOps/AffineOps.h"  // TF:local_config_mlir
+#include "mlir/Dialect/StandardOps/Ops.h"  // TF:local_config_mlir
 #include "mlir/IR/AffineExpr.h"  // TF:local_config_mlir
 #include "mlir/IR/AffineMap.h"  // TF:local_config_mlir
 #include "mlir/IR/StandardTypes.h"  // TF:local_config_mlir
 #include "mlir/Transforms/LoopUtils.h"  // TF:local_config_mlir
 #include "mlir/Transforms/RegionUtils.h"  // TF:local_config_mlir
+#include "tensorflow/compiler/xla/service/llvm_ir/llvm_util.h"
 #include "tensorflow/compiler/xla/window_util.h"
 
 namespace xla {
@@ -63,57 +64,68 @@ class NameLookupTable {
   absl::flat_hash_map<mlir::Value*, std::string> names_;
 };
 
-std::vector<int64_t> GetLogicalToPhysicalPermutationOrDie(
-    mlir::AffineMap affine_map) {
-  CHECK_EQ(0, affine_map.getNumSymbols());
-  CHECK_EQ(affine_map.getNumDims(), affine_map.getNumResults());
+// Various extracted information for input shapes.
+struct ShapeInfo {
+  // Buffer dimensions in the order of NCHW.
+  std::vector<int64_t> nchw_dimensions;
 
-  std::vector<int64_t> permutation(affine_map.getNumResults(), -1);
-  int count = 0;
-  for (auto expr : affine_map.getResults()) {
-    int64_t dim = expr.cast<mlir::AffineDimExpr>().getPosition();
-    CHECK_GE(dim, 0);
-    CHECK_EQ(-1, std::exchange(permutation[dim], count));
-    count++;
-  }
-  return permutation;
-}
+  // Buffer dimensions in the order of major to minor;
+  std::vector<int64_t> physical_dimensions;
 
-std::vector<int64_t> GetLogicalDimensions(mlir::MemRefType type) {
-  CHECK_EQ(1, type.getAffineMaps().size());
+  // The affine map that takes NCHW indices, and maps to the physical order.
+  mlir::AffineMap affine_map;
 
-  auto affine_map = type.getAffineMaps()[0];
-  CHECK(affine_map.isPermutation());
+  mlir::Type element_type;
+};
 
-  std::vector<int64_t> permutation =
-      GetLogicalToPhysicalPermutationOrDie(affine_map);
-  std::vector<int64_t> dimensions(type.getShape().size());
-  for (int i = 0; i < type.getShape().size(); i++) {
-    dimensions[i] = type.getShape()[permutation[i]];
-  }
-  return dimensions;
-}
+ShapeInfo GetShapeInfo(const Shape& shape, int64 n_dim, int64 c_dim,
+                       absl::Span<const int64> spatial_dims,
+                       mlir::Builder builder) {
+  ShapeInfo shape_info;
 
-mlir::Value* NormalizeMlirShapeToLogicalNchw(
-    int64 n_dim, int64 c_dim, absl::Span<const int64> spatial_dims,
-    mlir::Value* buffer, mlir::OpBuilder builder) {
-  mlir::MemRefType type = buffer->getType().cast<mlir::MemRefType>();
-  CHECK_EQ(1, type.getAffineMaps().size());
+  std::vector<int64> physical_to_logical(
+      shape.layout().minor_to_major().rbegin(),
+      shape.layout().minor_to_major().rend());
 
-  std::vector<mlir::AffineExpr> exprs;
-  exprs.push_back(builder.getAffineDimExpr(n_dim));
-  exprs.push_back(builder.getAffineDimExpr(c_dim));
+  std::vector<int64> nchw_to_logical;
+
+  nchw_to_logical.push_back(n_dim);
+  nchw_to_logical.push_back(c_dim);
   for (int64 dim : spatial_dims) {
-    exprs.push_back(builder.getAffineDimExpr(dim));
+    nchw_to_logical.push_back(dim);
   }
 
-  return builder.createOrFold<mlir::MemRefCastOp>(
-      builder.getUnknownLoc(),
-      mlir::MemRefType::get(
-          type.getShape(), type.getElementType(),
-          mlir::AffineMap(type.getAffineMaps()[0])
-              .compose(mlir::AffineMap::get(exprs.size(), 0, exprs))),
-      buffer);
+  for (int64 dim : nchw_to_logical) {
+    shape_info.nchw_dimensions.push_back(shape.dimensions(dim));
+  }
+
+  for (int64 dim : physical_to_logical) {
+    shape_info.physical_dimensions.push_back(shape.dimensions(dim));
+  }
+
+  std::vector<mlir::AffineExpr> affine_exprs;
+  // We want physical to nchw order.
+  for (int64 dim : ComposePermutations(InversePermutation(nchw_to_logical),
+                                       physical_to_logical)) {
+    affine_exprs.push_back(builder.getAffineDimExpr(dim));
+  }
+
+  shape_info.affine_map = mlir::AffineMap::get(
+      /*dimCount=*/2 + spatial_dims.size(), /*symbolCount=*/0, affine_exprs);
+
+  shape_info.element_type = [&] {
+    switch (shape.element_type()) {
+      case xla::F16:
+        return builder.getF16Type();
+      case xla::F32:
+        return builder.getF32Type();
+      default:
+        break;
+    }
+    CHECK(false);
+  }();
+
+  return shape_info;
 }
 
 void PrintExprWithBindings(mlir::AffineExpr expr,
@@ -593,43 +605,22 @@ struct InitialMlirConvAnchors {
 //     output[...] = output_acc[]
 //   }
 StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
-    HloInstruction* custom_call, mlir::Value* input, mlir::Value* filter,
-    mlir::Value* output, mlir::OpBuilder builder, NameLookupTable* names) {
-  TF_RETURN_IF_ERROR(ConvIsImplemented(custom_call));
-
-  const auto& dim_nums = custom_call->convolution_dimension_numbers();
-  input = NormalizeMlirShapeToLogicalNchw(
-      dim_nums.input_batch_dimension(), dim_nums.input_feature_dimension(),
-      dim_nums.input_spatial_dimensions(), input, builder);
-
-  filter = NormalizeMlirShapeToLogicalNchw(
-      dim_nums.kernel_output_feature_dimension(),
-      dim_nums.kernel_input_feature_dimension(),
-      dim_nums.kernel_spatial_dimensions(), filter, builder);
-
-  output = NormalizeMlirShapeToLogicalNchw(
-      dim_nums.output_batch_dimension(), dim_nums.output_feature_dimension(),
-      dim_nums.output_spatial_dimensions(), output, builder);
+    mlir::Value* input, mlir::Value* filter, mlir::Value* output,
+    const ShapeInfo& input_shape_info, const ShapeInfo& filter_shape_info,
+    const ShapeInfo& output_shape_info, const Window& window,
+    mlir::OpBuilder builder, NameLookupTable* names) {
+  CHECK(input_shape_info.element_type == builder.getF16Type());
+  CHECK(filter_shape_info.element_type == builder.getF16Type());
+  CHECK(output_shape_info.element_type == builder.getF16Type());
 
   auto location = mlir::UnknownLoc::get(builder.getContext());
-  auto input_shape = input->getType().cast<mlir::MemRefType>();
-  auto filter_shape = filter->getType().cast<mlir::MemRefType>();
-  auto output_shape = output->getType().cast<mlir::MemRefType>();
-
-  CHECK(input_shape.hasStaticShape());
-  CHECK(filter_shape.hasStaticShape());
-  CHECK(output_shape.hasStaticShape());
-
-  CHECK(input_shape.getElementType() == builder.getF16Type());
-  CHECK(filter_shape.getElementType() == builder.getF16Type());
-  CHECK(output_shape.getElementType() == builder.getF16Type());
 
   names->Insert(input, "input");
   names->Insert(filter, "filter");
   names->Insert(output, "output");
 
   std::vector<mlir::AffineForOp> cartesian_product_loops =
-      CreateNestedSimpleLoops(GetLogicalDimensions(output_shape), builder);
+      CreateNestedSimpleLoops(output_shape_info.nchw_dimensions, builder);
   // batch dimension
   names->Insert(cartesian_product_loops[0].getInductionVar(), "n");
   // output feature dimension
@@ -647,15 +638,12 @@ StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
   builder.create<mlir::AffineStoreOp>(
       location,
       builder.create<mlir::ConstantOp>(
-          location, builder.getFloatAttr(builder.getF32Type(), 0)),
+          location, mlir::FloatAttr::get(builder.getF32Type(), 0)),
       output_acc, llvm::ArrayRef<mlir::Value*>());
 
   std::vector<mlir::AffineForOp> reduction_loops;
-  {
-    std::vector<int64_t> dims = GetLogicalDimensions(filter_shape);
-    reduction_loops =
-        CreateNestedSimpleLoops(absl::MakeSpan(dims).subspan(1), builder);
-  }
+  reduction_loops = CreateNestedSimpleLoops(
+      absl::MakeSpan(filter_shape_info.nchw_dimensions).subspan(1), builder);
   // input feature dimension
   names->Insert(reduction_loops[0].getInductionVar(), "c");
   // filter height index
@@ -690,11 +678,11 @@ StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
     //
     // TODO(timshen): guard out-of-bound loads and stores brought by padding.
     for (int i = 0; i < num_spatial_dims; i++) {
-      const WindowDimension& window = custom_call->window().dimensions(i);
+      const WindowDimension& window_dim = window.dimensions(i);
       input_indices.push_back(
-          builder.getAffineDimExpr(i + 2) * window.stride() +
+          builder.getAffineDimExpr(i + 2) * window_dim.stride() +
           builder.getAffineDimExpr(2 + num_spatial_dims + i) -
-          window.padding_low());
+          window_dim.padding_low());
     }
     std::vector<mlir::Value*> input_vars;
     input_vars.push_back(loop_n.getInductionVar());
@@ -708,7 +696,10 @@ StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
         location,
         builder.createOrFold<mlir::AffineLoadOp>(
             location, input,
-            mlir::AffineMap::get(2 + num_spatial_dims * 2, 0, input_indices),
+            mlir::AffineMap(input_shape_info.affine_map)
+                .compose(
+                    mlir::AffineMap::get(/*dimCount=*/2 + num_spatial_dims * 2,
+                                         /*symbolCount=*/0, input_indices)),
             input_vars),
         builder.getF32Type());
   }();
@@ -722,7 +713,8 @@ StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
 
     return builder.create<mlir::FPExtOp>(
         location,
-        builder.createOrFold<mlir::AffineLoadOp>(location, filter, filter_vars),
+        builder.createOrFold<mlir::AffineLoadOp>(
+            location, filter, filter_shape_info.affine_map, filter_vars),
         builder.getF32Type());
   }();
 
@@ -747,7 +739,7 @@ StatusOr<InitialMlirConvAnchors> CreateNaiveMlirConv(
             location,
             builder.createOrFold<mlir::AffineLoadOp>(location, output_acc),
             builder.getF16Type()),
-        output, output_vars);
+        output, output_shape_info.affine_map, output_vars);
   }
 
   return InitialMlirConvAnchors{cartesian_product_loops, reduction_loops,
@@ -875,15 +867,58 @@ StatusOr<TransformedMlirConvAnchors> TransformMlirConv(
 
 }  // namespace
 
-Status EmitConvolutionForwardAsMlir(HloInstruction* custom_call,
-                                    mlir::Value* input, mlir::Value* filter,
-                                    mlir::Value* output,
-                                    mlir::OpBuilder builder) {
+StatusOr<mlir::FuncOp> EmitConvolutionForwardAsMlir(
+    HloInstruction* conv, absl::string_view function_name,
+    mlir::MLIRContext* context) {
+  mlir::OpBuilder builder(context);
+
+  const auto& dim_nums = conv->convolution_dimension_numbers();
+  ShapeInfo input_shape_info =
+      GetShapeInfo(conv->operand(0)->shape(), dim_nums.input_batch_dimension(),
+                   dim_nums.input_feature_dimension(),
+                   dim_nums.input_spatial_dimensions(), builder);
+
+  ShapeInfo filter_shape_info = GetShapeInfo(
+      conv->operand(1)->shape(), dim_nums.kernel_output_feature_dimension(),
+      dim_nums.kernel_input_feature_dimension(),
+      dim_nums.kernel_spatial_dimensions(), builder);
+
+  ShapeInfo output_shape_info = GetShapeInfo(
+      conv->shape().tuple_shapes(0), dim_nums.output_batch_dimension(),
+      dim_nums.output_feature_dimension(), dim_nums.output_spatial_dimensions(),
+      builder);
+
+  auto function = mlir::FuncOp::create(
+      mlir::UnknownLoc::get(builder.getContext()),
+      llvm_ir::AsStringRef(function_name),
+      builder.getFunctionType(
+          {mlir::MemRefType::get(output_shape_info.physical_dimensions,
+                                 output_shape_info.element_type, {}),
+           mlir::MemRefType::get(input_shape_info.physical_dimensions,
+                                 input_shape_info.element_type, {}),
+           mlir::MemRefType::get(filter_shape_info.physical_dimensions,
+                                 filter_shape_info.element_type, {})},
+          {}));
+
+  auto* entry_block = function.addEntryBlock();
+  builder.setInsertionPointToStart(entry_block);
+  builder.create<mlir::ReturnOp>(builder.getUnknownLoc());
+  builder.setInsertionPointToStart(entry_block);
+
+  mlir::Value* input = entry_block->getArgument(1);
+  mlir::Value* filter = entry_block->getArgument(2);
+  mlir::Value* output = entry_block->getArgument(0);
+
+  TF_RETURN_IF_ERROR(ConvIsImplemented(conv));
+
   NameLookupTable names;
 
   TF_ASSIGN_OR_RETURN(
       InitialMlirConvAnchors initial_anchors,
-      CreateNaiveMlirConv(custom_call, input, filter, output, builder, &names));
+      CreateNaiveMlirConv(input, filter, output, input_shape_info,
+                          filter_shape_info, output_shape_info, conv->window(),
+                          builder, &names));
+  VLOG(1) << ToString(initial_anchors.cartesian_product_loops[0], names);
 
   TF_ASSIGN_OR_RETURN(TransformedMlirConvAnchors transformed_anchors,
                       TransformMlirConv(initial_anchors));
@@ -897,18 +932,17 @@ Status EmitConvolutionForwardAsMlir(HloInstruction* custom_call,
 
   VLOG(1) << ToString(transformed_anchors.cartesian_product_loops[0], names);
 
-  return Status::OK();
+  return function;
 }
 
-Status ConvIsImplemented(const HloInstruction* custom_call) {
-  if (custom_call->feature_group_count() != 1 ||
-      custom_call->batch_group_count() != 1) {
+Status ConvIsImplemented(const HloInstruction* conv) {
+  if (conv->feature_group_count() != 1 || conv->batch_group_count() != 1) {
     return Unimplemented("group count is not implemented.");
   }
-  if (window_util::HasWindowReversal(custom_call->window())) {
+  if (window_util::HasWindowReversal(conv->window())) {
     return Unimplemented("Window reversal is not implemented.");
   }
-  if (window_util::HasDilation(custom_call->window())) {
+  if (window_util::HasDilation(conv->window())) {
     return Unimplemented("Dilation is not implemented.");
   }
   return Status::OK();
--- tensorflow/compiler/xla/service/mlir_gpu/experimental/conv_emitter/conv_emitter_test.cc
+++ tensorflow/compiler/xla/service/mlir_gpu/experimental/conv_emitter/conv_emitter_test.cc
@@ -18,14 +18,9 @@ limitations under the License.
 #include <vector>
 
 #include "llvm/Support/raw_ostream.h"
-#include "mlir/IR/AffineExpr.h"  // TF:local_config_mlir
-#include "mlir/IR/AffineMap.h"  // TF:local_config_mlir
-#include "mlir/IR/Builders.h"  // TF:local_config_mlir
-#include "mlir/IR/Function.h"  // TF:local_config_mlir
 #include "mlir/IR/Location.h"  // TF:local_config_mlir
 #include "mlir/IR/MLIRContext.h"  // TF:local_config_mlir
 #include "mlir/IR/Module.h"  // TF:local_config_mlir
-#include "mlir/IR/StandardTypes.h"  // TF:local_config_mlir
 #include "tensorflow/compiler/xla/service/hlo_parser.h"
 #include "tensorflow/compiler/xla/tests/filecheck.h"
 #include "tensorflow/compiler/xla/tests/verified_hlo_module.h"
@@ -34,32 +29,6 @@ limitations under the License.
 namespace xla {
 namespace gpu_mlir {
 namespace {
-
-mlir::Type ShapeToMlirType(const xla::Shape& shape, mlir::Builder* builder) {
-  CHECK(shape.IsArray());
-  mlir::Type element_type = [&] {
-    switch (shape.element_type()) {
-      case xla::F16:
-        return builder->getF16Type();
-      case xla::F32:
-        return builder->getF32Type();
-      default:
-        break;
-    }
-    CHECK(false);
-  }();
-  std::vector<int64_t> physical_layout;
-  std::vector<mlir::AffineExpr> major_to_minor;
-  for (int i = shape.layout().minor_to_major_size() - 1; i >= 0; i--) {
-    major_to_minor.push_back(
-        builder->getAffineDimExpr(shape.layout().minor_to_major(i)));
-    physical_layout.push_back(
-        shape.dimensions(shape.layout().minor_to_major(i)));
-  }
-  return mlir::MemRefType::get(
-      physical_layout, element_type,
-      {mlir::AffineMap::get(major_to_minor.size(), 0, major_to_minor)});
-}
 
 std::string CompileHloConvAndGetMlir(absl::string_view hlo_text) {
   xla::HloModuleConfig hlo_config;
@@ -72,29 +41,14 @@ std::string CompileHloConvAndGetMlir(absl::string_view hlo_text) {
       hlo_module.entry_computation()->root_instruction();
 
   mlir::MLIRContext context;
-  mlir::Location location = mlir::UnknownLoc::get(&context);
-  mlir::OwningModuleRef mlir_module(mlir::ModuleOp::create(location));
-  mlir::OpBuilder builder(&context);
+  mlir::OwningModuleRef mlir_module(
+      mlir::ModuleOp::create(mlir::UnknownLoc::get(&context)));
 
-  xla::Shape input_shape = conv->operand(0)->shape();
-  xla::Shape filter_shape = conv->operand(1)->shape();
-  xla::Shape output_shape = conv->shape().tuple_shapes(0);
+  mlir::FuncOp function =
+      xla::mlir_gpu::EmitConvolutionForwardAsMlir(conv, "Conv", &context)
+          .ValueOrDie();
 
-  auto function = mlir::FuncOp::create(
-      location, "Conv",
-      builder.getFunctionType({ShapeToMlirType(output_shape, &builder),
-                               ShapeToMlirType(input_shape, &builder),
-                               ShapeToMlirType(filter_shape, &builder)},
-                              {builder.getNoneType()}));
   mlir_module->push_back(function);
-
-  auto* entry_block = function.addEntryBlock();
-  builder.setInsertionPointToStart(entry_block);
-
-  TF_CHECK_OK(xla::mlir_gpu::EmitConvolutionForwardAsMlir(
-      conv, entry_block->getArgument(1), entry_block->getArgument(2),
-      entry_block->getArgument(0), builder));
-
   mlir_module->verify();
 
   std::string mlir_text;
@@ -114,8 +68,7 @@ ENTRY %TestComputation {
 
   std::string expected_mlir_pattern =
       R"(
-CHECK: func @Conv(%arg0: memref<128x112x112x64xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>, %arg1: memref<128x224x224x4xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>, %arg2: memref<64x7x7x4xf16, (d0, d1, d2, d3) -> (d2, d0, d1, d3)>) -> none {
-CHECK-NEXT:   %0 = memref_cast %arg2 : memref<64x7x7x4xf16, (d0, d1, d2, d3) -> (d2, d0, d1, d3)> to memref<64x7x7x4xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>
+CHECK: func @Conv(%arg0: memref<128x112x112x64xf16>, %arg1: memref<128x224x224x4xf16>, %arg2: memref<64x7x7x4xf16>) {
 CHECK-NEXT:   "affine.for"() ( {
 CHECK-NEXT:   ^bb0(%arg3: index):
 CHECK-NEXT:     "affine.for"() ( {
@@ -124,13 +77,13 @@ CHECK-NEXT:       "affine.for"() ( {
 CHECK-NEXT:       ^bb0(%arg5: index):
 CHECK-NEXT:         "affine.for"() ( {
 CHECK-NEXT:         ^bb0(%arg6: index):
-CHECK-NEXT:           %1 = alloc() : memref<32x16xf32>
+CHECK-NEXT:           %0 = alloc() : memref<32x16xf32>
 CHECK-NEXT:           "affine.for"() ( {
 CHECK-NEXT:           ^bb0(%arg7: index):
 CHECK-NEXT:             "affine.for"() ( {
 CHECK-NEXT:             ^bb0(%arg8: index):
 CHECK-NEXT:               %cst = constant 0.000000e+00 : f32
-CHECK-NEXT:               "affine.store"(%cst, %1, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (f32, memref<32x16xf32>, index, index) -> ()
+CHECK-NEXT:               "affine.store"(%cst, %0, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (f32, memref<32x16xf32>, index, index) -> ()
 CHECK-NEXT:               "affine.terminator"() : () -> ()
 CHECK-NEXT:             }) {lower_bound = () -> (0), step = 1 : index, upper_bound = () -> (16)} : () -> ()
 CHECK-NEXT:             "affine.terminator"() : () -> ()
@@ -147,14 +100,14 @@ CHECK-NEXT:                   "affine.for"() ( {
 CHECK-NEXT:                   ^bb0(%arg11: index):
 CHECK-NEXT:                     "affine.for"() ( {
 CHECK-NEXT:                     ^bb0(%arg12: index):
-CHECK-NEXT:                       %2 = "affine.load"(%arg1, %arg3, %arg9, %arg5, %arg6, %arg10, %arg11, %arg12, %arg8) {map = (d0, d1, d2, d3, d4, d5, d6, d7) -> (d0, d1 * 4 + d6, d2 * 2 + d4 - 3, (d3 * 16 + d7) * 2 + d5 - 3)} : (memref<128x224x224x4xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>, index, index, index, index, index, index, index, index) -> f16
-CHECK-NEXT:                       %3 = fpext %2 : f16 to f32
-CHECK-NEXT:                       %4 = "affine.load"(%0, %arg4, %arg9, %arg10, %arg11, %arg12, %arg7) {map = (d0, d1, d2, d3, d4, d5) -> (d0 * 32 + d5, d1 * 4 + d4, d2, d3)} : (memref<64x7x7x4xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>, index, index, index, index, index, index) -> f16
-CHECK-NEXT:                       %5 = fpext %4 : f16 to f32
-CHECK-NEXT:                       %6 = "affine.load"(%1, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (memref<32x16xf32>, index, index) -> f32
-CHECK-NEXT:                       %7 = mulf %3, %5 : f32
-CHECK-NEXT:                       %8 = addf %6, %7 : f32
-CHECK-NEXT:                       "affine.store"(%8, %1, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (f32, memref<32x16xf32>, index, index) -> ()
+CHECK-NEXT:                       %1 = "affine.load"(%arg1, %arg3, %arg9, %arg5, %arg6, %arg10, %arg11, %arg12, %arg8) {map = (d0, d1, d2, d3, d4, d5, d6, d7) -> (d0, d2 * 2 + d4 - 3, (d3 * 16 + d7) * 2 + d5 - 3, d1 * 4 + d6)} : (memref<128x224x224x4xf16>, index, index, index, index, index, index, index, index) -> f16
+CHECK-NEXT:                       %2 = fpext %1 : f16 to f32
+CHECK-NEXT:                       %3 = "affine.load"(%arg2, %arg4, %arg9, %arg10, %arg11, %arg12, %arg7) {map = (d0, d1, d2, d3, d4, d5) -> (d0 * 32 + d5, d2, d3, d1 * 4 + d4)} : (memref<64x7x7x4xf16>, index, index, index, index, index, index) -> f16
+CHECK-NEXT:                       %4 = fpext %3 : f16 to f32
+CHECK-NEXT:                       %5 = "affine.load"(%0, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (memref<32x16xf32>, index, index) -> f32
+CHECK-NEXT:                       %6 = mulf %2, %4 : f32
+CHECK-NEXT:                       %7 = addf %5, %6 : f32
+CHECK-NEXT:                       "affine.store"(%7, %0, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (f32, memref<32x16xf32>, index, index) -> ()
 CHECK-NEXT:                       "affine.terminator"() : () -> ()
 CHECK-NEXT:                     }) {lower_bound = () -> (0), step = 1 : index, upper_bound = () -> (4)} : () -> ()
 CHECK-NEXT:                     "affine.terminator"() : () -> ()
@@ -171,9 +124,9 @@ CHECK-NEXT:           "affine.for"() ( {
 CHECK-NEXT:           ^bb0(%arg7: index):
 CHECK-NEXT:             "affine.for"() ( {
 CHECK-NEXT:             ^bb0(%arg8: index):
-CHECK-NEXT:               %2 = "affine.load"(%1, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (memref<32x16xf32>, index, index) -> f32
-CHECK-NEXT:               %3 = fptrunc %2 : f32 to f16
-CHECK-NEXT:               "affine.store"(%3, %arg0, %arg3, %arg4, %arg5, %arg6, %arg7, %arg8) {map = (d0, d1, d2, d3, d4, d5) -> (d0, d1 * 32 + d4, d2, d3 * 16 + d5)} : (f16, memref<128x112x112x64xf16, (d0, d1, d2, d3) -> (d0, d2, d3, d1)>, index, index, index, index, index, index) -> ()
+CHECK-NEXT:               %1 = "affine.load"(%0, %arg7, %arg8) {map = (d0, d1) -> (d0, d1)} : (memref<32x16xf32>, index, index) -> f32
+CHECK-NEXT:               %2 = fptrunc %1 : f32 to f16
+CHECK-NEXT:               "affine.store"(%2, %arg0, %arg3, %arg4, %arg5, %arg6, %arg7, %arg8) {map = (d0, d1, d2, d3, d4, d5) -> (d0, d2, d3 * 16 + d5, d1 * 32 + d4)} : (f16, memref<128x112x112x64xf16>, index, index, index, index, index, index) -> ()
 CHECK-NEXT:               "affine.terminator"() : () -> ()
 CHECK-NEXT:             }) {lower_bound = () -> (0), step = 1 : index, upper_bound = () -> (16)} : () -> ()
 CHECK-NEXT:             "affine.terminator"() : () -> ()
@@ -186,6 +139,7 @@ CHECK-NEXT:       "affine.terminator"() : () -> ()
 CHECK-NEXT:     }) {lower_bound = () -> (0), step = 1 : index, upper_bound = () -> (2)} : () -> ()
 CHECK-NEXT:     "affine.terminator"() : () -> ()
 CHECK-NEXT:   }) {lower_bound = () -> (0), step = 1 : index, upper_bound = () -> (128)} : () -> ()
+CHECK-NEXT:   return
 CHECK-NEXT: }
 )";
 
