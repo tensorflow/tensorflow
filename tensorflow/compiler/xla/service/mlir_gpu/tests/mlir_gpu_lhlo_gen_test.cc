8b036e0cfb7e9bb1c016860cd7fe79752b97443c
Alexander Belyaev <pifon@google.com>
2019-10-24 02:12:48 -0700

    Enable ExpOp test in `mlir_gpu_lhlo_gen_test`.
    PiperOrigin-RevId: 276444490
    Change-Id: I8c8f0215f13e32f9ba4c2c7de30316a18181ec08

--- tensorflow/compiler/xla/service/mlir_gpu/tests/mlir_gpu_lhlo_gen_test.cc
+++ tensorflow/compiler/xla/service/mlir_gpu/tests/mlir_gpu_lhlo_gen_test.cc
@@ -84,7 +84,20 @@ ENTRY %Select (p: pred[2,2], x: f32[2,2], y: f32[2,2]) -> f32[2,2] {
       )");
 }
 
-// TODO(pifon): Re-enable the test with ExpOp.
+TEST_F(LhloGenTest, Exp) {
+  CompileAndVerifyIr(R"(
+HloModule Exp
+
+ENTRY %Exp (x: f32[2,2]) -> f32[2,2] {
+  %x = f32[2,2]{1,0} parameter(0)
+  ROOT %exp = f32[2,2]{1,0} exponential(f32[2,2]{1,0} %x)
+})",
+                     R"(
+;CHECK: func @exponential(%[[ARG0:.*]]: [[TYPE:.*]], %[[ARG1:.*]]: [[TYPE]]) {
+;CHECK:   "xla_lhlo.exp"(%[[ARG0]], %[[ARG1]]) {name = "exponential"} : ([[TYPE]], [[TYPE]]) -> ()
+;CHECK: }
+      )");
+}
 
 TEST_F(LhloGenTest, AddInGPUDialect) {
   CompileAndVerifyIr(R"(
