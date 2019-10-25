e461fa5725a932122c24b703860b8e1ee61d35e7
Yanhua Sun <yanhuasun@google.com>
2019-10-25 14:32:40 -0700

    Add a import_rename flag to allow to explicitly rename import to "compat.v2"
    PiperOrigin-RevId: 276761939
    Change-Id: I6e8baad28745a6fdd06e5b0a797aea08a7ad95fc

--- tensorflow/tools/compatibility/tf_upgrade_v2_test.py
+++ tensorflow/tools/compatibility/tf_upgrade_v2_test.py
@@ -117,10 +117,11 @@ class TestUpgrade(test_util.TensorFlowTestCase, parameterized.TestCase):
       visitor.private_map["tf.compat"] = ["v1", "v2"]
       traverse.traverse(tf.compat.v1, visitor)
 
-  def _upgrade(self, old_file_text):
+  def _upgrade(self, old_file_text, import_rename=False):
     in_file = six.StringIO(old_file_text)
     out_file = six.StringIO()
-    upgrader = ast_edits.ASTCodeUpgrader(tf_upgrade_v2.TFAPIChangeSpec())
+    upgrader = ast_edits.ASTCodeUpgrader(
+        tf_upgrade_v2.TFAPIChangeSpec(import_rename))
     count, report, errors = (
         upgrader.process_opened_file("test.py", in_file,
                                      "test_out.py", out_file))
@@ -2170,6 +2171,68 @@ def _log_prob(self, x):
         "tf.train.load_variable(ckpt_dir_or_file='a')")
     _, _, _, new_text = self._upgrade(text)
     self.assertEqual(expected_text, new_text)
+
+  def test_import_rename_analysis(self):
+    old_symbol = "tf.conj(a)"
+    new_symbol = "tf.math.conj(a)"
+
+    import_header = "import tensorflow as tf\n"
+    text = import_header + old_symbol
+    expected_text = "import tensorflow.compat.v2 as tf\n" + new_symbol
+    _, unused_report, unused_errors, new_text = self._upgrade(
+        text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = "import tensorflow as tf, other_import as y\n"
+    text = import_header + old_symbol
+    new_import_header = "import tensorflow.compat.v2 as tf, other_import as y\n"
+    expected_text = new_import_header + new_symbol
+    _, unused_report, unused_errors, new_text = self._upgrade(
+        text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = ("import tensorflow as tf\n"
+                     "import tensorflow.compat.v1 as tf_v1\n"
+                     "import tensorflow.compat.v2 as tf_v2\n")
+    text = import_header + old_symbol
+    expected_header = ("import tensorflow.compat.v2 as tf\n"
+                       "import tensorflow.compat.v1 as tf_v1\n"
+                       "import tensorflow.compat.v2 as tf_v2\n")
+    expected_text = expected_header + new_symbol
+    _, _, _, new_text = self._upgrade(text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = "from tensorflow import foo\n"
+    text = import_header + old_symbol
+    expected_text = "from tensorflow.compat.v2 import foo\n" + new_symbol
+    _, unused_report, unused_errors, new_text = self._upgrade(
+        text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = "from tensorflow import *\n"
+    text = import_header + old_symbol
+    expected_text = "from tensorflow.compat.v2 import *\n" + new_symbol
+    _, unused_report, unused_errors, new_text = self._upgrade(
+        text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = "from tensorflow.foo import bar\n"
+    text = import_header + old_symbol
+    expected_text = "from tensorflow.compat.v2.foo import bar\n" + new_symbol
+    _, unused_report, unused_errors, new_text = self._upgrade(
+        text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
+
+    import_header = ("from tensorflow import foo as tf\n"
+                     "from tensorflow.compat import v1 as tf_v1\n"
+                     "from tensorflow.compat import v2 as tf_v2\n")
+    text = import_header + old_symbol
+    expected_header = ("from tensorflow.compat.v2 import foo as tf\n"
+                       "from tensorflow.compat import v1 as tf_v1\n"
+                       "from tensorflow.compat import v2 as tf_v2\n")
+    expected_text = expected_header + new_symbol
+    _, _, _, new_text = self._upgrade(text, import_rename=True)
+    self.assertEqual(new_text, expected_text)
 
   def test_import_analysis(self):
     old_symbol = "tf.conj(a)"
