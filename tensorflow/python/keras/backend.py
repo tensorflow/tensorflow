973d805834ed1f4a4bc415d1e58cfa6dbd0005f7
Paige Bailey <webpaige@google.com>
2019-10-26 20:13:52 -0700

    Support tensor arguments in clip.
    Adding support for tensor arguments in clip, to align with the [semantics](https://github.com/keras-team/keras/pull/11442/files) of `backend.clip` in stand-alone Keras. See #24827.

--- tensorflow/python/keras/backend.py
+++ tensorflow/python/keras/backend.py
@@ -2289,18 +2289,20 @@ def clip(x, min_value, max_value):
 
   Arguments:
       x: Tensor or variable.
-      min_value: Python float or integer.
-      max_value: Python float or integer.
+      min_value: Python float, integer, or tensor.
+      max_value: Python float, integer, or tensor.
 
   Returns:
       A tensor.
   """
-  if max_value is not None and max_value < min_value:
-    max_value = min_value
+  if (isinstance(min_value, (int, float)) and
+      isinstance(max_value, (int, float))):
+    if max_value < min_value:
+      max_value = min_value
+  if min_value is None:
+    min_value = -np.inf
   if max_value is None:
     max_value = np.inf
-  min_value = _constant_to_tensor(min_value, x.dtype.base_dtype)
-  max_value = _constant_to_tensor(max_value, x.dtype.base_dtype)
   return clip_ops.clip_by_value(x, min_value, max_value)
 
 
