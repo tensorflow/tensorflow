cf97f642f5801263554f29ca62294de17b1d45f1
oclyke <owen.lyke@sparkfun.com>
2019-10-29 14:15:36 -0600

    fix sed error on mac

--- tensorflow/lite/experimental/micro/tools/make/download_and_extract.sh
+++ tensorflow/lite/experimental/micro/tools/make/download_and_extract.sh
@@ -53,13 +53,13 @@ patch_am_sdk() {
   sed -ir -E $'s/    with serial\.Serial\(args\.port, args\.baud, timeout=12\) as ser:/    with serial.Serial(args.port, args.baud, timeout=12) as ser:\\\n        # Patched.\\\n        import time\\\n        time.sleep(0.25)\\\n        # End patch./g' "${am_dir}/tools/apollo3_scripts/uart_wired_update.py"
 
   # Add CPP include guards to "am_hal_iom.h"
-  sed -i '57a\
+  sed -i -e '57a\
   #ifdef __cplusplus // Patch\
   extern "C" {\
   #endif // End patch
   ' "${am_dir}/mcu/apollo3/hal/am_hal_iom.h"
 
-  sed -i '836a\
+  sed -i -e '836a\
   #ifdef __cplusplus // Patch\
   }\
   #endif // End patch
