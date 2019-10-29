833e16a8c9d873977ad7a64aa400ef0289f1660b
Smit Hinsu <hinsu@google.com>
2019-10-28 21:53:16 -0700

    Lower TensorFlow CrossReplicaSum op to HLO
    Convert i32 elements types to i64 by using the conversion utility from hlo_ops.cc after moving it to hlo_utils
    
    PiperOrigin-RevId: 277211052
    Change-Id: Icc6d54011803b6a06a7b7b4d1b58a50d200951ff

--- tensorflow/compiler/mlir/xla/transforms/legalize_tf.cc
+++ tensorflow/compiler/mlir/xla/transforms/legalize_tf.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "mlir/Transforms/DialectConversion.h"  // TF:local_config_mlir
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
 #include "tensorflow/compiler/mlir/tensorflow/transforms/lower_tf.h"
+#include "tensorflow/compiler/mlir/xla/hlo_utils.h"
 #include "tensorflow/compiler/mlir/xla/ir/hlo_ops.h"
 #include "tensorflow/compiler/mlir/xla/transforms/passes.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
