24321279739330a8898a577460620f6cafd34030
Gaurav Jain <gjn@google.com>
2019-10-30 13:59:39 -0700

    Add support for watching complex types under tape
    PiperOrigin-RevId: 277578704
    Change-Id: I9d77db4a187ccfaf91f33150abd643533f409838

--- tensorflow/python/kernel_tests/py_func_test.py
+++ tensorflow/python/kernel_tests/py_func_test.py
@@ -598,7 +598,16 @@ class PyFuncTest(test.TestCase):
       tape.watch(x)
       y = script_ops.eager_py_func(f, inp=[x], Tout=dtypes.float32)
     dy_dx = tape.gradient(y, x)
-    self.assertEqual(self.evaluate(dy_dx), 6.0)
+    self.assertAllClose(self.evaluate(dy_dx), 6.0)
+
+    # Test complex values
+    x = constant_op.constant(3.0 + 3.0j)
+    with backprop.GradientTape() as tape:
+      tape.watch(x)
+      y = script_ops.eager_py_func(f, inp=[x], Tout=dtypes.complex128)
+    dy_dx = tape.gradient(y, x)
+    # Gradient of complex will be the conj
+    self.assertAllClose(self.evaluate(dy_dx), 6.0 - 6.0j)
 
   @test_util.run_v1_only("b/120545219")
   def testEagerGradientGraph(self):
