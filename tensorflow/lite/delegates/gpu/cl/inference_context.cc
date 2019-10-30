df1485e2316073b2e2c84266c21ba67f4ba4baab
A. Unique TensorFlower <gardener@tensorflow.org>
2019-10-30 09:31:26 -0700

    Improved batch detection.
    PiperOrigin-RevId: 277521282
    Change-Id: I37da201c82a4f5d97002923be50a4cb472932c56

--- tensorflow/lite/delegates/gpu/cl/inference_context.cc
+++ tensorflow/lite/delegates/gpu/cl/inference_context.cc
@@ -305,12 +305,15 @@ Status InferenceContext::ConvertOperations(
 
     OperationDef op_def;
     op_def.precision = precision_;
-    op_def.batch_support = outputs[0]->tensor.shape.b != 1;
     for (int j = 0; j < inputs.size(); ++j) {
+      op_def.batch_support =
+          op_def.batch_support || inputs[j]->tensor.shape.b != 1;
       op_def.src_tensors.push_back(
           tensor_reserver_.Get(inputs[j]->id).descriptor);
     }
     for (int j = 0; j < outputs.size(); ++j) {
+      op_def.batch_support =
+          op_def.batch_support || outputs[j]->tensor.shape.b != 1;
       op_def.dst_tensors.push_back(
           tensor_reserver_.Get(outputs[j]->id).descriptor);
     }
