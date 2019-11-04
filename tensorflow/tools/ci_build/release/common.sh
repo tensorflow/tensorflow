4d3cbd05a0ab1a1dd4adc88411b96e2590f2900e
Hye Soo Yang <hyey@google.com>
2019-11-04 10:27:40 -0800

    Downgrade bazel to expected version.

--- tensorflow/tools/ci_build/release/common.sh
+++ tensorflow/tools/ci_build/release/common.sh
@@ -17,7 +17,7 @@
 
 # Keep in sync with tensorflow_estimator and configure.py.
 # LINT.IfChange
-LATEST_BAZEL_VERSION=0.29.1
+LATEST_BAZEL_VERSION=0.26.1
 # LINT.ThenChange(
 #   //tensorflow/opensource_only/configure.py,
 #   //tensorflow_estimator/google/kokoro/common.sh,
