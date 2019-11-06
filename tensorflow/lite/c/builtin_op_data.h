d1e7c13f43cf0a95fce552651a5b336b68dff3c9
Yu-Cheng Ling <ycling@google.com>
2019-11-06 14:22:54 -0800

    Ignore depthwise_multiplier in DepthwisConv CPU kernel.
    PiperOrigin-RevId: 278939670
    Change-Id: If88fbd4e342055bd47c9a5d22001443f283235da

--- tensorflow/lite/c/builtin_op_data.h
+++ tensorflow/lite/c/builtin_op_data.h
@@ -96,6 +96,16 @@ typedef struct {
   TfLitePadding padding;
   int stride_width;
   int stride_height;
+  // `depth_multiplier` is redundant. It's used by CPU kernels in
+  // TensorFlow 2.0 or below, but ignored in versions above.
+  //
+  // The information can be deduced from the shape of input and the shape of
+  // weights. Since the TFLiteConverter toolchain doesn't support partially
+  // specificed shapes, relying on `depth_multiplier` stops us from supporting
+  // graphs with dynamic shape tensors.
+  //
+  // Note: Some of the delegates (e.g. NNAPI, GPU) are still relying on this
+  // field.
   int depth_multiplier;
   TfLiteFusedActivation activation;
   // Parameters for DepthwiseConv version 2 or above.
