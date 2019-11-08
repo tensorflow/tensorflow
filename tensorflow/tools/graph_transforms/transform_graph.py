cbd346126c577c71891f1a90336a074829692e84
frreiss <frreiss@us.ibm.com>
2019-11-07 17:24:22 -0800

    Fix linter warning

--- tensorflow/tools/graph_transforms/transform_graph.py
+++ tensorflow/tools/graph_transforms/transform_graph.py
@@ -19,7 +19,6 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.core.framework import graph_pb2
-from tensorflow.python.framework import errors
 from tensorflow.python.pywrap_tensorflow import TransformGraphWithStringInputs
 from tensorflow.python.util import compat
 
