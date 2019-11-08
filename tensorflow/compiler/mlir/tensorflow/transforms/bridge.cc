19aed023f6ce6410008b693cdcf9f44b2f4c3f8f
Andy Ly <lyandy@google.com>
2019-11-08 10:16:59 -0800

    Add ReplicateInvariantOpHoisting and ReplicateToIsland passes to TPU bridge pipeline.
    PiperOrigin-RevId: 279339580
    Change-Id: Ibdf8c01f546dbdf6205581c2c355ffa1db5efcb0

--- tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
@@ -33,7 +33,10 @@ void createTPUBridge(OpPassManager &pm) {
 
   pm.addPass(TFDevice::CreateClusterOutliningPass());
   pm.addPass(CreateTPURewritePass());
+  pm.addNestedPass<FuncOp>(TFDevice::CreateReplicateInvariantOpHoistingPass());
   pm.addNestedPass<FuncOp>(CreateFunctionalToExecutorDialectConversionPass());
+  pm.addNestedPass<FuncOp>(CreateBreakUpIslandsPass());
+  pm.addNestedPass<FuncOp>(TFDevice::CreateReplicateToIslandPass());
   pm.addNestedPass<FuncOp>(CreateBreakUpIslandsPass());
   pm.addNestedPass<FuncOp>(createCanonicalizerPass());
 }
