32e546a6b3ed2f56bfc94c5d95cb4e21a240aa14
Lei Zhang <antiagainst@google.com>
2019-11-12 10:33:36 -0800

    [spirv] Properly return when finding error in serialization
    PiperOrigin-RevId: 280001339
    Change-Id: Ie476c7959c68cadca224ecfa3cdda6a842ef99c3

--- third_party/mlir/tools/mlir-tblgen/SPIRVUtilsGen.cpp
+++ third_party/mlir/tools/mlir-tblgen/SPIRVUtilsGen.cpp
@@ -127,10 +127,9 @@ static void emitOperandSerialization(const Operator &op, ArrayRef<SMLoc> loc,
       os << tabs << "    auto argID = getValueID(arg);\n";
       os << tabs << "    if (!argID) {\n";
       os << tabs
-         << formatv(
-                "      emitError({0}.getLoc(), \"operand {1} has a use before "
-                "def\");\n",
-                opVar, operandNum);
+         << formatv("      return emitError({0}.getLoc(), "
+                    "\"operand #{1} has a use before def\");\n",
+                    opVar, operandNum);
       os << tabs << "    }\n";
       os << tabs << formatv("    {0}.push_back(argID);\n", operands);
       os << "    }\n";
