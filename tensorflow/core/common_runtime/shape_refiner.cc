7d5b56ed0e049ad92a47b8212ff58c1039ed3d90
Alexandre Passos <apassos@google.com>
2019-11-13 10:46:09 -0800

    Replacing DCHECKs with status in TF's shape inference constant folding.
    PiperOrigin-RevId: 280231067
    Change-Id: I23109c518a3a1c1f6ee0644ba51a10b3bdaffb42

--- tensorflow/core/common_runtime/shape_refiner.cc
+++ tensorflow/core/common_runtime/shape_refiner.cc
@@ -406,15 +406,19 @@ Status ShapeRefiner::EvaluateConstantIntScalarEdge(const Node* node,
   TF_RETURN_IF_ERROR(
       EvaluateConstantTensorForEdge(node, dst_idx, evaluated, &scalar));
   if (*evaluated) {
-    DCHECK_EQ(scalar.NumElements(), 1)
-        << "EvaluateConstantIntScalarEdge called on non-scalar edge: "
-        << scalar.NumElements();
+    if (scalar.NumElements() != 1) {
+      return errors::InvalidArgument(
+          "EvaluateConstantIntScalarEdge called on non-scalar edge: ",
+          scalar.NumElements());
+    }
     if (scalar.dtype() == DT_INT32) {
       *result = scalar.scalar<int32>()();
     } else {
-      DCHECK_EQ(scalar.dtype(), DT_INT64)
-          << "EvaluateConstantIntScalarEdge called on non-integer edge: "
-          << scalar.dtype();
+      if (scalar.dtype() != DT_INT64) {
+        return errors::InvalidArgument(
+            "EvaluateConstantIntScalarEdge called on non-integer edge: ",
+            scalar.dtype());
+      }
       *result = scalar.scalar<int64>()();
     }
   }
