4e4d996d184c5864b85f89954160a691d83b324d
Brandon Satrom <brandon@particle.io>
2019-11-13 11:22:28 -0800

    added Particle debug_log implementation

---
+++ tensorflow/lite/experimental/micro/particle/debug_log.cc
@@ -0,0 +1,29 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/lite/experimental/micro/debug_log.h"
+
+#include "Particle.h"
+
+// On Particle platforms, we set up a serial port and write to it for debug
+// logging.
+extern "C" void DebugLog(const char* s) {
+  static bool is_initialized = false;
+  if (!is_initialized) {
+    Serial.begin(9600);
+    is_initialized = true;
+  }
+  Serial.printlnf("%s", s);
+}
