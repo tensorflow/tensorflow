fae0c66c77dd4d660474185335a3d8e2c55e7d8b
bono1567 <bono10200807@hotmail.com>
2019-11-14 19:03:55 +0530

    Fix override build input-shape issue

--- tensorflow/python/keras/engine/base_layer.py
+++ tensorflow/python/keras/engine/base_layer.py
@@ -60,7 +60,6 @@ from tensorflow.python.keras.utils import tf_utils
 from tensorflow.python.keras.utils.generic_utils import to_snake_case  # pylint: disable=unused-import
 from tensorflow.python.keras.utils.tf_utils import is_tensor_or_tensor_list  # pylint: disable=unused-import
 from tensorflow.python.module import module
-from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import resource_variable_ops
