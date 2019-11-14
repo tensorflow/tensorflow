05fd94380719ba0370b0e85d35c3b08e7974263c
fsx950223 <fsx950223@outlook.com>
2019-11-14 17:55:37 +0800

    Format code

--- tensorflow/python/kernel_tests/string_split_op_test.py
+++ tensorflow/python/kernel_tests/string_split_op_test.py
@@ -279,11 +279,11 @@ class StringSplitV2OpTest(test_util.TensorFlowTestCase, parameterized.TestCase):
       expected_sparse = self.evaluate(expected_ragged.to_sparse())
       actual_sparse_v2 = string_ops.string_split_v2(input, **kwargs)
       self.assertEqual(expected_sparse.indices.tolist(),
-                        self.evaluate(actual_sparse_v2.indices).tolist())
+                       self.evaluate(actual_sparse_v2.indices).tolist())
       self.assertEqual(expected_sparse.values.tolist(),
-                        self.evaluate(actual_sparse_v2.values).tolist())
+                       self.evaluate(actual_sparse_v2.values).tolist())
       self.assertEqual(expected_sparse.dense_shape.tolist(),
-                        self.evaluate(actual_sparse_v2.dense_shape).tolist())
+                       self.evaluate(actual_sparse_v2.dense_shape).tolist())
 
   @parameterized.named_parameters([
       {"testcase_name": "Simple",
@@ -370,16 +370,15 @@ class StringSplitV2OpTest(test_util.TensorFlowTestCase, parameterized.TestCase):
       input = ragged_factory_ops.constant(input, dtype=dtypes.string)
     else:
       input = constant_op.constant(input, dtype=dtypes.string)
-    
+
     expected_ragged = ragged_factory_ops.constant(
-    expected)
+        expected)
     actual_ragged_v1 = ragged_string_ops.strings_split_v1(
         input, result_type="RaggedTensor", **kwargs)
     actual_ragged_v1_input_kwarg = ragged_string_ops.strings_split_v1(
         input=input, result_type="RaggedTensor", **kwargs)
     actual_ragged_v1_source_kwarg = ragged_string_ops.strings_split_v1(
         source=input, result_type="RaggedTensor", **kwargs)
-    
     self.assertAllEqual(expected_ragged, actual_ragged_v1)
     self.assertAllEqual(expected_ragged, actual_ragged_v1_input_kwarg)
     self.assertAllEqual(expected_ragged, actual_ragged_v1_source_kwarg)
@@ -387,11 +386,11 @@ class StringSplitV2OpTest(test_util.TensorFlowTestCase, parameterized.TestCase):
     actual_sparse_v1 = ragged_string_ops.strings_split_v1(
         input, result_type="SparseTensor", **kwargs)
     self.assertEqual(expected_sparse.indices.tolist(),
-                      self.evaluate(actual_sparse_v1.indices).tolist())
+                     self.evaluate(actual_sparse_v1.indices).tolist())
     self.assertEqual(expected_sparse.values.tolist(),
-                      self.evaluate(actual_sparse_v1.values).tolist())
+                     self.evaluate(actual_sparse_v1.values).tolist())
     self.assertEqual(expected_sparse.dense_shape.tolist(),
-                      self.evaluate(actual_sparse_v1.dense_shape).tolist())
+                     self.evaluate(actual_sparse_v1.dense_shape).tolist())
 
   def _py_split(self, strings, **kwargs):
     if isinstance(strings, compat.bytes_or_text_types):
