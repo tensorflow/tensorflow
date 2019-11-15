82ffe368896b5f360aa7afcdf613beff49a22bbe
Peng Wu <pengwu100@gmail.com>
2019-11-15 18:54:49 -0500

    rename variables

--- tensorflow/python/ops/collective_ops_test.py
+++ tensorflow/python/ops/collective_ops_test.py
@@ -349,10 +349,8 @@ class CollectiveOpTest(test.TestCase):
 
   @test_util.run_deprecated_v1
   def testCollectiveGatherPolymorphicShape(self):
-    t0 = [0, 1, 2, 3, 4, 5, 6, 7]
-    t1 = [10, 11, 12, 13, 14, 15, 16, 17]
-    t01 = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17]
-    t01_ = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17]
+    input0 = [0, 1, 2, 3, 4, 5, 6, 7]
+    input1 = [10, 11, 12, 13, 14, 15, 16, 17]
     group_size = 2
     group_key = 1
     instance_key = 123
@@ -365,13 +363,16 @@ class CollectiveOpTest(test.TestCase):
       with ops.device('/CPU:1'):
         in1 = array_ops.placeholder(dtype=dtypes.int32, shape=[None])
         c1 = collective_ops.all_gather(in1, group_size, group_key, instance_key)
-      results = sess.run([c0, c1], feed_dict={in0: t0, in1: t1})
-      self.assertAllClose(results[0], t01, rtol=1e-5, atol=1e-5)
-      self.assertAllClose(results[1], t01, rtol=1e-5, atol=1e-5)
 
-      results_ = sess.run([c0, c1], feed_dict={in0: t0[1:], in1: t1[1:]})
-      self.assertAllClose(results_[0], t01_, rtol=1e-5, atol=1e-5)
-      self.assertAllClose(results_[1], t01_, rtol=1e-5, atol=1e-5)
+      results = sess.run([c0, c1], feed_dict={in0: input0, in1: input1})
+      expected_output = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17]
+      self.assertAllClose(results[0], expected_output, rtol=1e-5, atol=1e-5)
+      self.assertAllClose(results[1], expected_output, rtol=1e-5, atol=1e-5)
+
+      results_ = sess.run([c0, c1], feed_dict={in0: input0[1:], in1: input1[1:]})
+      expected_output_ = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17]
+      self.assertAllClose(results_[0], expected_output_, rtol=1e-5, atol=1e-5)
+      self.assertAllClose(results_[1], expected_output_, rtol=1e-5, atol=1e-5)
 
   @test_util.run_v2_only
   def testCollectiveGroupSizeMismatch(self):
