69db3280882ee93f99b59efa5ef830d75aaeca5e
Peng Wu <pengwu100@gmail.com>
2019-11-15 19:26:04 -0500

    another line too long :)

--- tensorflow/python/ops/collective_ops_test.py
+++ tensorflow/python/ops/collective_ops_test.py
@@ -349,8 +349,8 @@ class CollectiveOpTest(test.TestCase):
 
   @test_util.run_deprecated_v1
   def testCollectiveGatherPolymorphicShape(self):
-    input0 = [0, 1, 2, 3, 4, 5, 6, 7]
-    input1 = [10, 11, 12, 13, 14, 15, 16, 17]
+    t0 = [0, 1, 2, 3, 4, 5, 6, 7]
+    t1 = [10, 11, 12, 13, 14, 15, 16, 17]
     group_size = 2
     group_key = 1
     instance_key = 123
@@ -364,12 +364,12 @@ class CollectiveOpTest(test.TestCase):
         in1 = array_ops.placeholder(dtype=dtypes.int32, shape=[None])
         c1 = collective_ops.all_gather(in1, group_size, group_key, instance_key)
 
-      results = sess.run([c0, c1], feed_dict={in0: input0, in1: input1})
+      results = sess.run([c0, c1], feed_dict={in0: t0, in1: t1})
       expected_output = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17]
       self.assertAllClose(results[0], expected_output, rtol=1e-5, atol=1e-5)
       self.assertAllClose(results[1], expected_output, rtol=1e-5, atol=1e-5)
 
-      results_ = sess.run([c0, c1], feed_dict={in0: input0[1:], in1: input1[1:]})
+      results_ = sess.run([c0, c1], feed_dict={in0: t0[1:], in1: t1[1:]})
       expected_output_ = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17]
       self.assertAllClose(results_[0], expected_output_, rtol=1e-5, atol=1e-5)
       self.assertAllClose(results_[1], expected_output_, rtol=1e-5, atol=1e-5)
