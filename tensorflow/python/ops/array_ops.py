94277bbacb1e83b7cca45923727c4ee1ec808d48
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2019-11-18 23:27:54 +0530

    Made minor changes to make code more suitable for GCI challenge. Refer #34005
    The changes have been made in order to make the code suitable for a Google Code-In Documentation and Research challenge.
    The task now is to clean the documentation so that it is more clear. There are too many code blocks in-between the text.
    Some examples are placed in the args section for illustration. A better way to handle that would be to add those examples in a separate "Examples" section (consistent with other docs) and add pointers in the "Args" section.
    Similarly, some other code elements could be eliminated from the text by adding pointers.
    Clarify the crops part in the "Args" section further.

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -3537,20 +3537,20 @@ def batch_to_space_v2(input, block_shape, crops, name=None):  # pylint: disable=
   defined by the spatial dimensions `[1, ..., M]`, to obtain a result with the
   same rank as the input.  The spatial dimensions of this intermediate result
   are then optionally cropped according to `crops` to produce the output.  This
-  is the reverse of SpaceToBatch.  See below for a precise description.
+  is the reverse of SpaceToBatch (see `tf.space_to_batch`).
 
   Args:
-    input: A `Tensor`. N-D with shape `input_shape = [batch] + spatial_shape +
-      remaining_shape`, where spatial_shape has M dimensions.
-    block_shape: A `Tensor`. Must be one of the following types: `int32`,
-      `int64`. 1-D with shape `[M]`, all values must be >= 1. For backwards
-      compatibility with TF 1.0, this parameter may be an int, in which case it
-      is converted to `numpy.array([block_shape, block_shape],
+    input: A N-D `Tensor` with shape `input_shape = [batch] + spatial_shape +
+      remaining_shape`, where `spatial_shape` has M dimensions.
+    block_shape: A 1-D `Tensor` with shape [M]. Must be one of the following types: `int32`,
+      `int64`. All values must be >= 1. For backwards compatibility with TF 1.0,
+      this parameter may be an int, in which case it is converted to
+      `numpy.array([block_shape, block_shape],
       dtype=numpy.int64)`.
-    crops: A `Tensor`. Must be one of the following types: `int32`, `int64`. 2-D
-      with shape `[M, 2]`, all values must be >= 0. `crops[i] = [crop_start,
-      crop_end]` specifies the amount to crop from input dimension `i + 1`,
-      which corresponds to spatial dimension `i`.  It is required that
+    crops: A  2-D `Tensor` with shape `[M, 2]`. Must be one of the 
+      following types: `int32`, `int64`. All values must be >= 0. 
+      `crops[i] = [crop_start, crop_end]` specifies the amount to crop from input dimension 
+      `i + 1`, which corresponds to spatial dimension `i`. It is required that
       `crop_start[i] + crop_end[i] <= block_shape[i] * input_shape[i + 1]`.
       This operation is equivalent to the following steps:
       1. Reshape `input` to `reshaped` of shape: [block_shape[0], ...,
@@ -3571,31 +3571,35 @@ def batch_to_space_v2(input, block_shape, crops, name=None):  # pylint: disable=
            block_shape[0] - crops[0,0] - crops[0,1], ..., input_shape[M] *
            block_shape[M-1] - crops[M-1,0] - crops[M-1,1],  input_shape[M+1],
            ..., input_shape[N-1]]
-      Some examples:  (1) For the following input of shape `[4, 1, 1, 1]`,
-          `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` [[[[1]]],
-            [[[2]]], [[[3]]], [[[4]]]] ```
-      The output tensor has shape `[1, 2, 2, 1]` and value:  ``` x = [[[[1],
-        [2]], [[3], [4]]]] ```  (2) For the following input of shape `[4, 1, 1,
-        3]`,
-          `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` [[[1, 2,
-            3]], [[4, 5, 6]], [[7, 8, 9]], [[10, 11, 12]]] ```
-      The output tensor has shape `[1, 2, 2, 3]` and value:  ``` x = [[[[1, 2,
-        3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]] ```  (3) For the following
-        input of shape `[4, 2, 2, 1]`,
-          `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` x =
-            [[[[1], [3]], [[9], [11]]], [[[2], [4]], [[10], [12]]], [[[5], [7]],
-            [[13], [15]]], [[[6], [8]], [[14], [16]]]] ```
-      The output tensor has shape `[1, 4, 4, 1]` and value:  ``` x = [[[1], [2],
-        [3],  [4]], [[5],   [6],  [7],  [8]], [[9],  [10], [11],  [12]], [[13],
-        [14], [15],  [16]]] ```  (4) For the following input of shape `[8, 1, 3,
-        1]`,
+      Some Examples:
+      (1) For the following input of shape `[4, 1, 1, 1]`,
+         `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` [[[[1]]],
+         [[[2]]], [[[3]]], [[[4]]]] ```
+         The output tensor has shape `[1, 2, 2, 1]` and value:  ``` x = [[[[1],
+         [2]], [[3], [4]]]] ```  
+      (2) For the following input of shape `[4, 1, 1,
+         3]`,
+         `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` [[[1, 2,
+         3]], [[4, 5, 6]], [[7, 8, 9]], [[10, 11, 12]]] ```
+         The output tensor has shape `[1, 2, 2, 3]` and value:  ``` x = [[[[1, 2,
+         3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]] ```
+      (3) For the following
+         input of shape `[4, 2, 2, 1]`,
+         `block_shape = [2, 2]`, and `crops = [[0, 0], [0, 0]]`:  ``` x =
+         [[[[1], [3]], [[9], [11]]], [[[2], [4]], [[10], [12]]], [[[5], [7]],
+         [[13], [15]]], [[[6], [8]], [[14], [16]]]] ```
+         The output tensor has shape `[1, 4, 4, 1]` and value:  ``` x = [[[1],
+         [2],  [3],  [4]], [[5],   [6],  [7],  [8]], [[9],  [10], [11],  [12]],
+         [[13], [14], [15],  [16]]] ```
+       (4) For the following input of shape
+          `[8, 1, 3, 1]`,
           `block_shape = [2, 2]`, and `crops = [[0, 0], [2, 0]]`:  ``` x =
-            [[[[0], [1], [3]]], [[[0], [9], [11]]], [[[0], [2], [4]]], [[[0],
-            [10], [12]]], [[[0], [5], [7]]], [[[0], [13], [15]]], [[[0], [6],
-            [8]]], [[[0], [14], [16]]]] ```
-      The output tensor has shape `[2, 2, 4, 1]` and value:  ``` x = [[[[1],
-        [2],  [3],  [4]], [[5],   [6],  [7],  [8]]], [[[9],  [10], [11],  [12]],
-        [[13], [14], [15],  [16]]]] ```
+          [[[[0], [1], [3]]], [[[0], [9], [11]]], [[[0], [2], [4]]], [[[0],
+          [10], [12]]], [[[0], [5], [7]]], [[[0], [13], [15]]], [[[0], [6],
+          [8]]], [[[0], [14], [16]]]] ```
+          The output tensor has shape `[2, 2, 4, 1]` and value:  ``` x = [[[[1],
+          [2],  [3],  [4]], [[5],   [6],  [7],  [8]]], [[[9],  [10], [11],  [12]],
+          [[13], [14], [15],  [16]]]] ```
     name: A name for the operation (optional).
 
   Returns:
