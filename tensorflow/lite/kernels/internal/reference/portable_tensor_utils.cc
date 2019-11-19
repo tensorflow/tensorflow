ee85e6d230278e763a2784ba86acc747abdb2242
Robert David <lrdx@google.com>
2019-11-18 16:25:05 -0800

    Use the numerically stable two-pass algorithm to calculate variance in MeanStddevNormalization.
    Add an extra test case with large mean and large variance.
    
    PiperOrigin-RevId: 281179296
    Change-Id: Ib2a5c3a0b0870670c3c41afc15b9ea3e13fa1f8c

--- tensorflow/lite/kernels/internal/reference/portable_tensor_utils.cc
+++ tensorflow/lite/kernels/internal/reference/portable_tensor_utils.cc
@@ -624,13 +624,16 @@ void PortableMeanStddevNormalization(const float* input_vector,
                                      int n_batch) {
   for (int batch = 0; batch < n_batch; ++batch) {
     float sum = 0.0f;
-    float sum_sq = 0.0f;
     for (int i = 0; i < v_size; ++i) {
       sum += input_vector[i];
-      sum_sq += input_vector[i] * input_vector[i];
     }
     const float mean = sum / v_size;
-    const float variance = sum_sq / v_size - mean * mean;
+    float sum_diff_sq = 0.0f;
+    for (int i = 0; i < v_size; ++i) {
+      const float diff = input_vector[i] - mean;
+      sum_diff_sq += diff * diff;
+    }
+    const float variance = sum_diff_sq / v_size;
     constexpr float kNormalizationConstant = 1e-8f;
     const float stddev_inv =
         1.0f / std::sqrt(variance + kNormalizationConstant);
