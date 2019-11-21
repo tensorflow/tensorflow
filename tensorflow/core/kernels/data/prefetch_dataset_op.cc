93a68feb340aca45b3c30fddc02229fc1d667993
Rachel Lim <rachelim@google.com>
2019-11-21 15:51:57 -0800

    [tf.data] Fix deadlock with Prefetch+ParallelMap
    PiperOrigin-RevId: 281851149
    Change-Id: I1b776edb68b45eabc9a0e931135470cae1b6e8f1

--- tensorflow/core/kernels/data/prefetch_dataset_op.cc
+++ tensorflow/core/kernels/data/prefetch_dataset_op.cc
@@ -208,14 +208,17 @@ class PrefetchDatasetOp::Dataset : public DatasetBase {
       }
 
       mutex_lock parent_l(*parent_mu_);
-      mutex_lock l(*mu_);
-      if (stats_aggregator) {
-        stats_aggregator->AddScalar(
-            stats_utils::BufferSizeScalarName(dataset()->node_name()),
-            static_cast<float>(buffer_.size()), num_elements());
-        stats_aggregator->AddScalar(
-            stats_utils::BufferCapacityScalarName(dataset()->node_name()),
-            static_cast<float>(buffer_limit()), num_elements());
+      {
+        mutex_lock l(*mu_);
+        if (stats_aggregator) {
+          stats_aggregator->AddScalar(
+              stats_utils::BufferSizeScalarName(dataset()->node_name()),
+              static_cast<float>(buffer_.size()), num_elements());
+          stats_aggregator->AddScalar(
+              stats_utils::BufferCapacityScalarName(dataset()->node_name()),
+              static_cast<float>(buffer_limit()), num_elements());
+        }
+        // Release mu_
       }
       return input_impl_->GetNext(ctx, out_tensors, end_of_sequence);
     }
@@ -477,6 +480,7 @@ class PrefetchDatasetOp::Dataset : public DatasetBase {
 
     // This mutex is used to ensure exclusivity between multiple threads
     // reading/writing this iterator's local state.
+    // Note: We should never call GetNext on the input while holding this.
     const std::shared_ptr<mutex> mu_;
     // This mutex is used to ensure exclusivity between multiple threads
     // accessing the parent iterator. We keep this separate from `mu_` to
