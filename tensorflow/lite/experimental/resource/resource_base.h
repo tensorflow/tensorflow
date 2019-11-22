160a61ef50154855018876b2c80c217cdfa13915
Jaesung Chung <jaesung@google.com>
2019-11-21 23:08:54 -0800

    Add hash table resource implementation into TFLite
    PiperOrigin-RevId: 281909097
    Change-Id: I1498a20749c85e42d75cbd73ee1035c66db6e369

---
+++ tensorflow/lite/experimental/resource/resource_base.h
@@ -0,0 +1,43 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_LITE_EXPERIMENTAL_RESOURCE_RESOURCE_BASE_H_
+#define TENSORFLOW_LITE_EXPERIMENTAL_RESOURCE_RESOURCE_BASE_H_
+
+#include <memory>
+#include <unordered_map>
+
+#include "tensorflow/lite/kernels/internal/compatibility.h"
+
+namespace tflite {
+namespace resource {
+
+// ResourceBase is an abstract base class for resources.
+/// WARNING: Experimental interface, subject to change.
+class ResourceBase {
+ public:
+  explicit ResourceBase() {}
+  virtual ~ResourceBase() {}
+
+  // Returns true if it is initialized.
+  virtual bool IsInitialized() = 0;
+};
+
+/// WARNING: Experimental interface, subject to change.
+using ResourceMap = std::unordered_map<int32, std::unique_ptr<ResourceBase>>;
+
+}  // namespace resource
+}  // namespace tflite
+
+#endif  // TENSORFLOW_LITE_EXPERIMENTAL_RESOURCE_RESOURCE_BASE_H_
