80813e6ed87db3386157937d6fe66d6bf8dcaab6
Kaixi Hou <kaixih@nvidia.com>
2019-11-21 17:51:50 -0800

    Update comments for the function IsCudnnSupportedFilterSize

--- tensorflow/core/util/use_cudnn.h
+++ tensorflow/core/util/use_cudnn.h
@@ -39,6 +39,10 @@ FP16ConvMode CudnnConvComputeMode();
 bool DebugCudnnRnn();
 bool DebugCudnnRnnUseTensorOps();
 int64 DebugCudnnRnnAlgo();
+
+// Return true if the filter is 1x1, 3x3, 5x5 or 7x7. This function is used
+// before calling the CuDNN depthwise convolution. See cudnn release note 7.6.3.
+// (https://docs.nvidia.com/deeplearning/sdk/cudnn-release-notes/rel_763.html)
 bool IsCudnnSupportedFilterSize(const int32 filter_rows,
                                 const int32 filter_cols);
 }  // namespace tensorflow
