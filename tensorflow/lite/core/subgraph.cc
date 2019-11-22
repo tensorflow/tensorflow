a36c12b7c70e750ef4bb8828855c3675e9519dc9
Mihai Maruseac <mihaimaruseac@google.com>
2019-11-22 10:39:18 -0800

    Add hash table resource implementation into TFLite
    PiperOrigin-RevId: 281997222
    Change-Id: I8680e454fd4de99d4a2d51ef651df97d52253f0b

--- tensorflow/lite/core/subgraph.cc
+++ tensorflow/lite/core/subgraph.cc
@@ -158,13 +158,13 @@ class InterpreterInfo : public GraphInfo {
 Subgraph::Subgraph(ErrorReporter* error_reporter,
                    TfLiteExternalContext** external_contexts,
                    std::vector<std::unique_ptr<Subgraph>>* subgraphs,
-                   resource::ResourceMap* resources)
+                   ResourceVariableMap* resource_variables)
     : external_contexts_(external_contexts),
       error_reporter_(error_reporter),
       next_execution_plan_index_to_prepare_(0),
       next_execution_plan_index_to_plan_allocation_(0),
       subgraphs_(subgraphs),
-      resources_(resources) {
+      resource_variables_(resource_variables) {
   context_.impl_ = static_cast<void*>(this);
   context_.ResizeTensor = ResizeTensor;
   context_.ReportError = ReportErrorC;
