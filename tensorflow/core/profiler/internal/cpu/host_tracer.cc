883b5becaced22f7dd9e3c23d9d259f55e087cb5
Jose Baiocchi <jbaiocchi@google.com>
2019-11-23 06:55:47 -0800

    Implement CollectData(XSpace*) in HostTracer
    PiperOrigin-RevId: 282135185
    Change-Id: I630d96c949dafa7c17d59ca3299a142d9a3263da

--- tensorflow/core/profiler/internal/cpu/host_tracer.cc
+++ tensorflow/core/profiler/internal/cpu/host_tracer.cc
@@ -13,15 +13,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include <utility>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/strings/str_split.h"
 #include "tensorflow/core/common_runtime/step_stats_collector.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/env_time.h"
+#include "tensorflow/core/profiler/internal/parse_annotation.h"
 #include "tensorflow/core/profiler/internal/profiler_interface.h"
 #include "tensorflow/core/profiler/internal/traceme_recorder.h"
 #include "tensorflow/core/profiler/protobuf/xplane.pb.h"
+#include "tensorflow/core/profiler/utils/xplane_builder.h"
 #include "tensorflow/core/protobuf/config.pb.h"
 #include "tensorflow/core/util/env_var.h"
 
@@ -29,6 +32,7 @@ namespace tensorflow {
 namespace profiler {
 namespace cpu {
 namespace {
+
 // Controls TraceMeRecorder and converts TraceMeRecorder::Events into
 // RunMetadata messages.
 //
@@ -50,16 +54,21 @@ class HostTracer : public ProfilerInterface {
 
   Status CollectData(XSpace* space) override;
 
-  profiler::DeviceType GetDeviceType() override {
-    return profiler::DeviceType::kCpu;
-  }
+  DeviceType GetDeviceType() override { return DeviceType::kCpu; }
 
  private:
+  // Combine events created by TraceMe::ActivityStart and TraceMe::ActivityEnd,
+  // which can be paired up by their activity_id.
+  void MakeCompleteEvents();
+
   // Level of host tracing.
   const int host_trace_level_;
 
   // True if currently recording.
   bool recording_ = false;
+
+  // Timestamp at the start of tracing.
+  uint64 start_timestamp_ns_ = 0;
 
   // Container of all traced events.
   TraceMeRecorder::Events events_;
@@ -78,6 +87,7 @@ Status HostTracer::Start() {
   if (!recording_) {
     return Status(error::INTERNAL, "Failed to start TraceMeRecorder");
   }
+  start_timestamp_ns_ = EnvTime::NowNanos();
   return Status::OK();
 }
 
@@ -90,33 +100,57 @@ Status HostTracer::Stop() {
   return Status::OK();
 }
 
-constexpr char kUserMetadataMarker = '#';
-
-Status HostTracer::CollectData(RunMetadata* run_metadata) {
-  if (recording_) {
-    return Status(error::INTERNAL, "TraceMeRecorder not stopped");
-  }
-  // Pair up start and end events, and add complete events to trace_entries.
-  absl::flat_hash_map<uint64, uint64> end_times;
-  for (const auto& thread : events_) {
-    for (const auto& event : thread.events) {
-      if (event.end_time && !event.start_time) {
-        end_times.emplace(event.activity_id, event.end_time);
+void HostTracer::MakeCompleteEvents() {
+  // Track events create by ActivityStart and copy their data to events created
+  // by ActivityEnd. TraceME records events in its destructor, so this results
+  // in complete events sorted by their end_time in the thread they ended.
+  // Within the same thread, the record created by ActivityStart must appear
+  // before the record created by ActivityEnd. Cross-thread events must be
+  // processed in a separate pass. A single map can be used because the
+  // activity_id is globally unique.
+  absl::flat_hash_map<uint64, TraceMeRecorder::Event*> start_events;
+  std::vector<TraceMeRecorder::Event*> end_events;
+  for (auto& thread : events_) {
+    for (auto& event : thread.events) {
+      if (event.start_time && !event.end_time) {  // ActivityStart
+        start_events.emplace(event.activity_id, &event);
+      } else if (!event.start_time && event.end_time) {  // ActivityEnd
+        auto iter = start_events.find(event.activity_id);
+        if (iter != start_events.end()) {  // same thread
+          auto* start_event = iter->second;
+          event.name = std::move(start_event->name);
+          event.start_time = start_event->start_time;
+          start_events.erase(iter);
+        } else {  // cross-thread
+          end_events.push_back(&event);
+        }
       }
     }
   }
+  for (auto* event : end_events) {  // cross-thread
+    auto iter = start_events.find(event->activity_id);
+    if (iter != start_events.end()) {
+      auto* start_event = iter->second;
+      event->name = std::move(start_event->name);
+      event->start_time = start_event->start_time;
+      start_events.erase(iter);
+    }
+  }
+}
 
+Status HostTracer::CollectData(RunMetadata* run_metadata) {
+  if (recording_) {
+    return errors::Internal("TraceMeRecorder not stopped");
+  }
+  MakeCompleteEvents();
   StepStatsCollector step_stats_collector(run_metadata->mutable_step_stats());
 
+  constexpr char kUserMetadataMarker = '#';
   const string cpu_name = "/host:CPU";
   for (auto& thread : events_) {
     step_stats_collector.SaveThreadName(cpu_name, thread.thread.tid,
                                         thread.thread.name);
     for (auto& event : thread.events) {
-      if (!event.end_time) {
-        auto it = end_times.find(event.activity_id);
-        if (it != end_times.end()) event.end_time = it->second;
-      }
       if (event.start_time && event.end_time) {
         NodeExecStats* ns = new NodeExecStats;
         if (event.name.back() != kUserMetadataMarker) {
@@ -136,7 +170,6 @@ Status HostTracer::CollectData(RunMetadata* run_metadata) {
         ns->set_all_end_rel_micros((event.end_time - event.start_time) /
                                    EnvTime::kMicrosToNanos);
         ns->set_thread_id(thread.thread.tid);
-        // TODO(fishx): Add thread name to RunMetadata
         step_stats_collector.Save(cpu_name, ns);
       }
     }
@@ -147,7 +180,48 @@ Status HostTracer::CollectData(RunMetadata* run_metadata) {
 }
 
 Status HostTracer::CollectData(XSpace* space) {
-  return errors::Unimplemented("Collect data into XSpace not yet implemented");
+  if (recording_) {
+    return errors::Internal("TraceMeRecorder not stopped");
+  }
+  MakeCompleteEvents();
+  XPlaneBuilder xplane(space->add_planes());
+  xplane.SetName("Host Threads");
+  absl::flat_hash_map<string, XEventMetadata*> xevent_metadata_by_name;
+  absl::flat_hash_map<string, XStatMetadata*> xstat_metadata_by_name;
+  for (const auto& thread : events_) {
+    XLineBuilder xline = xplane.AddLine();
+    xline.SetId(thread.thread.tid);
+    xline.SetName(thread.thread.name);
+    xline.SetTimestampNs(start_timestamp_ns_);
+    xline.ReserveEvents(thread.events.size());
+    for (const auto& event : thread.events) {
+      if (event.start_time && event.end_time) {
+        Annotation annotation = ParseAnnotation(event.name);
+        XEventMetadata*& xevent_metadata =
+            xevent_metadata_by_name[annotation.name];
+        if (xevent_metadata == nullptr) {
+          xevent_metadata =
+              xplane.GetOrCreateEventMetadata(xevent_metadata_by_name.size());
+          xevent_metadata->set_name(string(annotation.name));
+        }
+        XEventBuilder xevent = xline.AddEvent(*xevent_metadata);
+        xevent.SetTimestampNs(event.start_time);
+        xevent.SetEndTimestampNs(event.end_time);
+        xevent.ReserveStats(annotation.metadata.size());
+        for (const auto& metadata : annotation.metadata) {
+          XStatMetadata*& xstat_metadata = xstat_metadata_by_name[metadata.key];
+          if (xstat_metadata == nullptr) {
+            xstat_metadata =
+                xplane.GetOrCreateStatMetadata(xstat_metadata_by_name.size());
+            xstat_metadata->set_name(string(metadata.key));
+          }
+          xevent.ParseAndAddStatValue(*xstat_metadata, metadata.value);
+        }
+      }
+    }
+  }
+  events_.clear();
+  return Status::OK();
 }
 
 }  // namespace
@@ -161,7 +235,6 @@ std::unique_ptr<ProfilerInterface> CreateHostTracer(
 
 auto register_host_tracer_factory = [] {
   bool enable;
-
   TF_CHECK_OK(ReadBoolFromEnvVar("TF_ENABLE_OSS_CPU_PROFILER", true, &enable));
   if (enable) {
     RegisterProfilerFactory(&CreateHostTracer);
