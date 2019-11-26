9e8a730f08d5d9275dfed767d4ae15a44639b950
Akshay Modi <nareshmodi@google.com>
2019-11-26 13:52:58 -0800

    Add a wrapper around encode_png which converts to tensor before calling into the gen ops code.
    This way if there is uint16 np array, we'll use uint16 as the dtype correctly
    instead of using uint8 (which would happen before due to EncodePng having a
    default type of uint8).
    
    PiperOrigin-RevId: 282633227
    Change-Id: I46d5bda1f94fda3e0a4b15c57591ef86dd1649a4

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -2232,6 +2232,35 @@ tf_export(
         gen_image_ops.extract_jpeg_shape)
 
 
+@tf_export('image.encode_png')
+def encode_png(image, compression=-1, name=None):
+  r"""PNG-encode an image.
+
+  `image` is a 3-D uint8 or uint16 Tensor of shape `[height, width, channels]`
+  where `channels` is:
+
+  *   1: for grayscale.
+  *   2: for grayscale + alpha.
+  *   3: for RGB.
+  *   4: for RGBA.
+
+  The ZLIB compression level, `compression`, can be -1 for the PNG-encoder
+  default or a value from 0 to 9.  9 is the highest compression level,
+  generating the smallest output, but is slower.
+
+  Args:
+    image: A `Tensor`. Must be one of the following types: `uint8`, `uint16`.
+      3-D with shape `[height, width, channels]`.
+    compression: An optional `int`. Defaults to `-1`. Compression level.
+    name: A name for the operation (optional).
+
+  Returns:
+    A `Tensor` of type `string`.
+  """
+  return gen_image_ops.encode_png(
+      ops.convert_to_tensor(image), compression, name)
+
+
 @tf_export(
     'io.decode_image',
     'image.decode_image',
