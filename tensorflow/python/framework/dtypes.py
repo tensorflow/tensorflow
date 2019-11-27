39029c26b932efdc503ba762b0616d5942400784
Amit Patankar <amitpatankar@google.com>
2019-11-27 10:35:12 -0800

    Fix the MacOS convert_test.
    PiperOrigin-RevId: 282795396
    Change-Id: I4207d1200e6326767a1879b627017916942a811e

--- tensorflow/python/framework/dtypes.py
+++ tensorflow/python/framework/dtypes.py
@@ -21,8 +21,11 @@ import numpy as np
 from six.moves import builtins
 
 from tensorflow.core.framework import types_pb2
-from tensorflow.python import _dtypes
+# pywrap_tensorflow must be imported prior to _dtypes for the MacOS linker
+# to resolve the protobufs properly.
+# pylint: disable=unused-import,g-bad-import-order
 from tensorflow.python import pywrap_tensorflow
+from tensorflow.python import _dtypes
 from tensorflow.python.util.tf_export import tf_export
 
 _np_bfloat16 = pywrap_tensorflow.TF_bfloat16_type()
