fc7a817dbd4a931bad458fa5896c8ebdba15d60f
Brian Zhao <bmzhao@google.com>
2019-12-02 13:27:38 -0800

    Change the client_secret that looks like a "private key" to something more obviously fake.
    PiperOrigin-RevId: 283404936
    Change-Id: Ide7c9816e11c7f18debce9f3eca504e79c1ce47b

--- tensorflow/core/platform/cloud/oauth_client_test.cc
+++ tensorflow/core/platform/cloud/oauth_client_test.cc
@@ -64,7 +64,7 @@ TEST(OAuthClientTest, GetTokenFromRefreshTokenJson) {
   const string credentials_json = R"(
       {
         "client_id": "test_client_id",
-        "client_secret": "test_client_secret",
+        "client_secret": "@@@test_client_secret@@@",
         "refresh_token": "test_refresh_token",
         "type": "authorized_user"
       })";
@@ -75,7 +75,7 @@ TEST(OAuthClientTest, GetTokenFromRefreshTokenJson) {
   std::vector<HttpRequest*> requests({new FakeHttpRequest(
       "Uri: https://www.googleapis.com/oauth2/v3/token\n"
       "Post body: client_id=test_client_id&"
-      "client_secret=test_client_secret&"
+      "client_secret=@@@test_client_secret@@@&"
       "refresh_token=test_refresh_token&grant_type=refresh_token\n",
       kTokenJson)});
   FakeEnv env;
