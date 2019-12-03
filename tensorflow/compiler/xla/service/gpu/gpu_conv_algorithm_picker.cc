vibrancy/tensorflow-upstream

Changing GetMIOpenAlgorithms routine to return a vector for "ProfileResult"s instead of "AlgorithmDesc"s. ProfileResult datastructure has scratch_size and execution time information, in addition to the AlgorithmDesc. This then allows us to enable a further optimization in the TF kernel layer. When the number of "algorithms" returned by GetMIOpenAlgorithms is one, we can skip doing a profile run on that algorithm, and directly use it as the "best result".

tensorflow/compiler/xla/service/gpu/gpu_conv_algorithm_picker.cc
@@ -118,12 +118,12 @@ std::vector<AlgorithmDesc> GetAlgorithms(CudnnConvKind kind,
   return algorithms;
 }
 
-StatusOr<std::vector<AlgorithmDesc>> GetAlgorithms(
+StatusOr<std::vector<se::dnn::ProfileResult>> GetAlgorithms(
     const HloCustomCallInstruction* conv,
     absl::Span<se::DeviceMemoryBase> operand_buffers,
     se::DeviceMemoryBase result_buffer, se::StreamExecutor* stream_exec,
     se::Stream* stream) {
-  std::vector<AlgorithmDesc> algorithms;
+  std::vector<se::dnn::ProfileResult> algorithms;
 
   TF_ASSIGN_OR_RETURN(se::dnn::ConvolutionKind kind,
                       GetDnnConvolutionKind(conv));
@@ -641,50 +641,64 @@ GpuConvAlgorithmPicker::PickBestAlgorithmNoCacheRocm(
           ShapeUtil::ByteSizeOf(instr.shape().tuple_shapes(0))));
   initialize_buffer(result_buffer);
 
-  TF_ASSIGN_OR_RETURN(std::vector<AlgorithmDesc> algorithms,
+  TF_ASSIGN_OR_RETURN(std::vector<se::dnn::ProfileResult> algorithms,
                       GetAlgorithms(&instr, absl::MakeSpan(operand_buffers),
                                     result_buffer, stream_exec_, stream));
 
   std::vector<AutotuneResult> profile_results;
 
-  for (const AlgorithmDesc& alg : algorithms) {
-    XLA_SCOPED_LOGGING_TIMER_LEVEL(
-        absl::StrCat("CudnnConvAlgorithmPicker::PickBestAlgorithm algo ",
-                     AlgorithmToString(alg)),
-        2);
-
-    ScratchAllocator scratch_allocator(device_ordinal, allocator);
-    se::dnn::ProfileResult profile_result;
-    VLOG(3) << "Trying algorithm " << AlgorithmToString(alg) << " for "
-            << instr.ToString();
-
-    // Use assignment instead of brace-list to make GCC 4.9 happy.
-    RunConvOptions options;
-    options.profile_result = &profile_result;
-    options.algo_override = alg;
-    Status launch_status =
-        RunCudnnConv(&instr, absl::MakeSpan(operand_buffers), result_buffer,
-		     &scratch_allocator, stream, options);
-
-    if (!launch_status.ok()) {
-      continue;
-    }
-
-    if (!profile_result.is_valid()) {
-      continue;
-    }
-
+  std::vector<tensorflow::AutotuneResult> results;
+  if (algorithms.size() == 1) {
+    auto profile_result = algorithms[0];
     profile_results.emplace_back();
-    AutotuneResult& result = profile_results.back();
-    result.mutable_conv()->set_algorithm(alg.algo_id());
-    result.mutable_conv()->set_tensor_ops_enabled(alg.tensor_ops_enabled());
+    auto& result = results.back();
+    result.mutable_conv()->set_algorithm(profile_result.algorithm().algo_id());
+    result.mutable_conv()->set_tensor_ops_enabled(
+        profile_result.algorithm().tensor_ops_enabled());
 
-    int64 scratch_bytes_used = scratch_allocator.TotalAllocatedBytes();
-    result.set_scratch_bytes(scratch_bytes_used);
+    result.set_scratch_bytes(profile_result.scratch_size());
     *result.mutable_run_time() = tensorflow::proto_utils::ToDurationProto(
         absl::Milliseconds(profile_result.elapsed_time_in_ms()));
-  }
+  } else {
+    for (const auto& miopen_alg : algorithms) {
+      const auto& alg = miopen_alg.algorithm();
+      XLA_SCOPED_LOGGING_TIMER_LEVEL(
+          absl::StrCat("CudnnConvAlgorithmPicker::PickBestAlgorithm algo ",
+                       AlgorithmToString(alg)),
+          2);
 
+      ScratchAllocator scratch_allocator(device_ordinal, allocator);
+      se::dnn::ProfileResult profile_result;
+      VLOG(3) << "Trying algorithm " << AlgorithmToString(alg) << " for "
+              << instr.ToString();
+
+      // Use assignment instead of brace-list to make GCC 4.9 happy.
+      RunConvOptions options;
+      options.profile_result = &profile_result;
+      options.algo_override = alg;
+      Status launch_status =
+          RunCudnnConv(&instr, absl::MakeSpan(operand_buffers), result_buffer,
+                       &scratch_allocator, stream, options);
+
+      if (!launch_status.ok()) {
+        continue;
+      }
+
+      if (!profile_result.is_valid()) {
+        continue;
+      }
+
+      profile_results.emplace_back();
+      AutotuneResult& result = profile_results.back();
+      result.mutable_conv()->set_algorithm(alg.algo_id());
+      result.mutable_conv()->set_tensor_ops_enabled(alg.tensor_ops_enabled());
+
+      int64 scratch_bytes_used = scratch_allocator.TotalAllocatedBytes();
+      result.set_scratch_bytes(scratch_bytes_used);
+      *result.mutable_run_time() = tensorflow::proto_utils::ToDurationProto(
+          absl::Milliseconds(profile_result.elapsed_time_in_ms()));
+    }
+  }
   const auto& best_result = absl::c_min_element(
       profile_results,
       [&](const AutotuneResult& lhs, const AutotuneResult& rhs) {

tensorflow/tensorflow

tensorflow/compiler/xla/service/gpu/gpu_conv_algorithm_picker.cc
@@ -118,12 +118,12 @@ std::vector<AlgorithmDesc> GetAlgorithms(CudnnConvKind kind,
   return algorithms;
 }
 
-StatusOr<std::vector<AlgorithmDesc>> GetAlgorithms(
+StatusOr<std::vector<se::dnn::ProfileResult>> GetAlgorithms(
     const HloCustomCallInstruction* conv,
     absl::Span<se::DeviceMemoryBase> operand_buffers,
     se::DeviceMemoryBase result_buffer, se::StreamExecutor* stream_exec,
     se::Stream* stream) {
-  std::vector<AlgorithmDesc> algorithms;
+  std::vector<se::dnn::ProfileResult> algorithms;
 
   TF_ASSIGN_OR_RETURN(se::dnn::ConvolutionKind kind,
                       GetDnnConvolutionKind(conv));
@@ -640,50 +640,63 @@ GpuConvAlgorithmPicker::PickBestAlgorithmNoCacheRocm(
           ShapeUtil::ByteSizeOf(instr->shape().tuple_shapes(0))));
   initialize_buffer(result_buffer);
 
-  TF_ASSIGN_OR_RETURN(std::vector<AlgorithmDesc> algorithms,
+  TF_ASSIGN_OR_RETURN(std::vector<se::dnn::ProfileResult> algorithms,
                       GetAlgorithms(instr, absl::MakeSpan(operand_buffers),
                                     result_buffer, stream_exec_, stream));
 
   std::vector<AutotuneResult> profile_results;
 
-  for (const AlgorithmDesc& alg : algorithms) {
-    XLA_SCOPED_LOGGING_TIMER_LEVEL(
-        absl::StrCat("CudnnConvAlgorithmPicker::PickBestAlgorithm algo ",
-                     AlgorithmToString(alg)),
-        2);
-
-    ScratchAllocator scratch_allocator(device_ordinal, allocator);
-    se::dnn::ProfileResult profile_result;
-    VLOG(3) << "Trying algorithm " << AlgorithmToString(alg) << " for "
-            << instr->ToString();
-
-    // Use assignment instead of brace-list to make GCC 4.9 happy.
-    RunConvOptions options;
-    options.profile_result = &profile_result;
-    options.algo_override = alg;
-    Status launch_status =
-        RunGpuConv(instr, absl::MakeSpan(operand_buffers), result_buffer,
-                   &scratch_allocator, stream, options);
-
-    if (!launch_status.ok()) {
-      continue;
-    }
-
-    if (!profile_result.is_valid()) {
-      continue;
-    }
-
+  if (algorithms.size() == 1) {
+    auto profile_result = algorithms[0];
     profile_results.emplace_back();
-    AutotuneResult& result = profile_results.back();
-    result.mutable_conv()->set_algorithm(alg.algo_id());
-    result.mutable_conv()->set_tensor_ops_enabled(alg.tensor_ops_enabled());
+    auto& result = profile_results.back();
+    result.mutable_conv()->set_algorithm(profile_result.algorithm().algo_id());
+    result.mutable_conv()->set_tensor_ops_enabled(
+        profile_result.algorithm().tensor_ops_enabled());
 
-    int64 scratch_bytes_used = scratch_allocator.TotalAllocatedBytes();
-    result.set_scratch_bytes(scratch_bytes_used);
+    result.set_scratch_bytes(profile_result.scratch_size());
     *result.mutable_run_time() = tensorflow::proto_utils::ToDurationProto(
         absl::Milliseconds(profile_result.elapsed_time_in_ms()));
-  }
+  } else {
+    for (const auto& miopen_alg : algorithms) {
+      const auto& alg = miopen_alg.algorithm();
+      XLA_SCOPED_LOGGING_TIMER_LEVEL(
+          absl::StrCat("CudnnConvAlgorithmPicker::PickBestAlgorithm algo ",
+                       AlgorithmToString(alg)),
+          2);
 
+      ScratchAllocator scratch_allocator(device_ordinal, allocator);
+      se::dnn::ProfileResult profile_result;
+      VLOG(3) << "Trying algorithm " << AlgorithmToString(alg) << " for "
+              << instr->ToString();
+
+      // Use assignment instead of brace-list to make GCC 4.9 happy.
+      RunConvOptions options;
+      options.profile_result = &profile_result;
+      options.algo_override = alg;
+      Status launch_status =
+          RunGpuConv(instr, absl::MakeSpan(operand_buffers), result_buffer,
+		     &scratch_allocator, stream, options);
+
+      if (!launch_status.ok()) {
+        continue;
+      }
+
+      if (!profile_result.is_valid()) {
+        continue;
+      }
+
+      profile_results.emplace_back();
+      AutotuneResult& result = profile_results.back();
+      result.mutable_conv()->set_algorithm(alg.algo_id());
+      result.mutable_conv()->set_tensor_ops_enabled(alg.tensor_ops_enabled());
+
+      int64 scratch_bytes_used = scratch_allocator.TotalAllocatedBytes();
+      result.set_scratch_bytes(scratch_bytes_used);
+      *result.mutable_run_time() = tensorflow::proto_utils::ToDurationProto(
+          absl::Milliseconds(profile_result.elapsed_time_in_ms()));
+    }
+  }
   const auto& best_result = absl::c_min_element(
       profile_results,
       [&](const AutotuneResult& lhs, const AutotuneResult& rhs) {