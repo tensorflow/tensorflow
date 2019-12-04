610fdfba520a30f0c81b2f6ad2cac92641379e86
Pooya Davoodi <pdavoodi@nvidia.com>
2019-12-04 10:07:40 -0800

    Sort targets in BUILD and shorten statement for pylint

--- tensorflow/python/compiler/tensorrt/test/quantization_mnist_test.py
+++ tensorflow/python/compiler/tensorrt/test/quantization_mnist_test.py
@@ -265,7 +265,8 @@ class QuantizationAwareTrainingMNISTTest(test_util.TensorFlowTestCase):
   def testEval(self):
     if not is_tensorrt_enabled():
       return
-    model_dir = test.test_src_dir_path('python/compiler/tensorrt/test/testdata/mnist')
+    model_dir = test.test_src_dir_path(
+        'python/compiler/tensorrt/test/testdata/mnist')
 
     accuracy_tf_native = self._Run(
         is_training=False,
