c628dee8acfb9593e61f2fa1603f277c917a7c25
Rehan Guha <rehanguha29@gmail.com>
2019-12-11 12:30:36 +0530

    Adding- "COntinuous COin Betting" Optimization Algorithm
    Adding a new optimization algorithm named- COntinuous COin Betting(https://arxiv.org/pdf/1705.07795.pdf)
    This algorithm gives a faster convergence rate compared to some of the other algorithms.

--- tensorflow/python/keras/optimizers.py
+++ tensorflow/python/keras/optimizers.py
@@ -708,6 +708,53 @@ class Nadam(Optimizer):
     return dict(list(base_config.items()) + list(config.items()))
 
 
+class COCOB(Optimizer):
+  """Coin Betting Optimizer from the paper:
+    https://arxiv.org/pdf/1705.07795.pdf
+  """
+  def __init__(self, alpha=100, **kwargs):
+    """
+    Initialize COCOB Optimizer
+    Args:
+      alpha: Refer to paper.
+    """
+    super(COCOB, self).__init__(**kwargs)
+    self._alpha = alpha
+    with K.name_scope(self.__class__.__name__):
+      self.iterations = K.variable(0, dtype='int64', name='iterations')
+  def get_updates(self, params, loss, contraints=None):
+    self.updates = [K.update_add(self.iterations, 1)]
+    grads = self.get_gradients(loss, params)
+    shapes = [K.int_shape(p) for p in params]
+    L = [K.variable(np.full(fill_value=1e-8, shape=shape)) for shape in shapes]
+    reward = [K.zeros(shape) for shape in shapes]
+    tilde_w = [K.zeros(shape) for shape in shapes]
+    gradients_sum = [K.zeros(shape) for shape in shapes]
+    gradients_norm_sum = [K.zeros(shape) for shape in shapes]
+    for p, g, li, ri, twi, gsi, gns in zip(params, grads, L, reward,
+                       tilde_w,gradients_sum,
+                         gradients_norm_sum):
+      grad_sum_update = gsi + g
+      grad_norm_sum_update = gns + K.abs(g)
+      l_update = K.maximum(li, K.abs(g))
+      reward_update = K.maximum(ri - g * twi, 0)
+      new_w = - grad_sum_update / (l_update * (K.maximum(grad_norm_sum_update + l_update, self._alpha * l_update))) * (reward_update + l_update)
+      param_update = p - twi + new_w
+      tilde_w_update = new_w
+      self.updates.append(K.update(gsi, grad_sum_update))
+      self.updates.append(K.update(gns, grad_norm_sum_update))
+      self.updates.append(K.update(li, l_update))
+      self.updates.append(K.update(ri, reward_update))
+      self.updates.append(K.update(p, param_update))
+      self.updates.append(K.update(twi, tilde_w_update))
+    return self.updates
+  def get_config(self):
+      config = {'alpha': float(K.get_value(self._alpha)) }
+      base_config = super(COCOB, self).get_config()
+      return dict(list(base_config.items()) + list(config.items()))  
+  
+  
+
 class TFOptimizer(Optimizer, trackable.Trackable):
   """Wrapper class for native TensorFlow optimizers."""
 
@@ -775,7 +822,7 @@ adadelta = Adadelta
 adam = Adam
 adamax = Adamax
 nadam = Nadam
-
+cocob = COCOB
 
 @keras_export('keras.optimizers.serialize')
 def serialize(optimizer):
@@ -807,6 +854,7 @@ def deserialize(config, custom_objects=None):
       'sgd': gradient_descent_v2.SGD,
       'ftrl': ftrl.Ftrl,
       'lossscaleoptimizer': loss_scale_optimizer.LossScaleOptimizer,
+      'cocob': COCOB,
   }
 
   # Make deserialization case-insensitive for built-in optimizers.
