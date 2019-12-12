54ad00b25e88ddbefdc3f9a29c09423b9bcbe4c3
Brandon Satrom <brandon@particle.io>
2019-12-12 14:36:19 -0500

    added magic_wand output handler

--- tensorflow/lite/experimental/micro/examples/magic_wand/particle/output_handler.cc
+++ tensorflow/lite/experimental/micro/examples/magic_wand/particle/output_handler.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "output_handler.h"
+#include "tensorflow/lite/experimental/micro/examples/magic_wand/output_handler.h"
 
 #include "Particle.h"
 
