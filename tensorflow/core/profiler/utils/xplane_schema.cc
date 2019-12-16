708e5729bb02a1ff17c71ee6dc2ad531a6b18d50
A. Unique TensorFlower <gardener@tensorflow.org>
2019-12-16 12:13:43 -0800

    Add HostEventType to xplane_schema.
    PiperOrigin-RevId: 285822505
    Change-Id: Ieb1d4af1df36016e6ecb919c32334c84d2327453

--- tensorflow/core/profiler/utils/xplane_schema.cc
+++ tensorflow/core/profiler/utils/xplane_schema.cc
@@ -15,15 +15,54 @@ limitations under the License.
 
 #include "tensorflow/core/profiler/utils/xplane_schema.h"
 
+#include "absl/strings/string_view.h"
+
 namespace tensorflow {
 namespace profiler {
 
 const absl::string_view kHostThreads = "Host Threads";
 
-const int kNumStatTypes = static_cast<int>(StatType::kHloModule) + 1;
+constexpr int kNumHostEventTypes =
+    HostEventType::kLastHostEventType - HostEventType::kFirstHostEventType + 1;
 
-static const absl::string_view kStatTypeStrMap[kNumStatTypes] = {
-    "unknown",         "id",
+constexpr int kNumStatTypes =
+    StatType::kLastStatType - StatType::kFirstStatType + 1;
+
+static const absl::string_view kHostEventTypeMetadataMap[] = {
+    "UnknownHostEventType",
+    "TraceContext",
+    "SessionRun",
+    "FunctionRun",
+    "RunGraph",
+    "ExecutorState::Process",
+    "ExecutorDoneCallback",
+    // tf data captured function events.
+    "InstantiatedCapturedFunction::Run",
+    "InstantiatedCapturedFunction::RunWithBorrowedArgs",
+    "InstantiatedCapturedFunction::RunInstantiated",
+    "InstantiatedCapturedFunction::RunAsync",
+    // Functional ops.
+    "CallOp",
+    "ParallelForOp",
+    "ForeverOp",
+    "NumericalGradientOp-EvalRight",
+    "NumericalGradientOp-EvalLeft",
+    "SymbolicGradientOp",
+    "RemoteCallOp",
+    "IfOp",
+    "CaseOp",
+    "WhileOp-EvalCond",
+    "WhileOp-StartBody",
+    "ForOp",
+    "PartitionedCallOp",
+};
+
+static_assert(sizeof(kHostEventTypeMetadataMap) / sizeof(absl::string_view) ==
+                  kNumHostEventTypes,
+              "Mismatch between enum and string map.");
+
+static const absl::string_view kStatTypeStrMap[] = {
+    "UnknownStatType", "id",
     "parent_step_id",  "function_step_id",
     "device_ordinal",  "chip_ordinal",
     "node_ordinal",    "model_id",
@@ -38,6 +77,14 @@ static const absl::string_view kStatTypeStrMap[kNumStatTypes] = {
     "tf_op",           "hlo_op",
     "hlo_module",
 };
+
+static_assert(sizeof(kStatTypeStrMap) / sizeof(absl::string_view) ==
+                  kNumStatTypes,
+              "Mismatch between enum and string map.");
+
+absl::Span<const absl::string_view> GetHostEventTypeStrMap() {
+  return absl::MakeConstSpan(kHostEventTypeMetadataMap, kNumHostEventTypes);
+}
 
 absl::Span<const absl::string_view> GetStatTypeStrMap() {
   return absl::MakeConstSpan(kStatTypeStrMap, kNumStatTypes);
