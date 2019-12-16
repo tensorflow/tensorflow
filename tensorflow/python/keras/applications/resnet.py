a0f2c04bc06da196b6fa047ba673877fb433eaf2
Francois Chollet <fchollet@google.com>
2019-12-15 17:35:43 -0800

    Fix typo that prevented ResNet weight loading
    PiperOrigin-RevId: 285684337
    Change-Id: Ie610d4d2c2e1ebcf767298ce1c891010594bb7d8

--- tensorflow/python/keras/applications/resnet.py
+++ tensorflow/python/keras/applications/resnet.py
@@ -30,7 +30,7 @@ from tensorflow.python.util.tf_export import keras_export
 
 
 BASE_WEIGHTS_PATH = (
-    'https://storage.googleapis.com/tensorflow/keras-applications/resnet')
+    'https://storage.googleapis.com/tensorflow/keras-applications/resnet/')
 WEIGHTS_HASHES = {
     'resnet50': ('2cb95161c43110f7111970584f804107',
                  '4d473c1dd8becc155b73f8504c6f6626'),
