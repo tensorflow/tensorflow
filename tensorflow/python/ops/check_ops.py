99273e3f1eea3d97f08953d495c4efe5faf575ff
William Zhang <williamhyzhang@gmail.com>
2019-12-17 18:42:05 -0500

    remove tf.print doctests

--- tensorflow/python/ops/check_ops.py
+++ tensorflow/python/ops/check_ops.py
@@ -1968,10 +1968,10 @@ def is_strictly_increasing(x, name=None):
   See also:  `is_non_decreasing`
 
   >>> x1 = tf.constant([1.0, 2.0, 3.0])
-  >>> tf.print(tf.math.is_strictly_increasing(x1))
+  >>> tf.math.is_strictly_increasing(x1)
   <tf.Tensor: shape=(), dtype=bool, numpy=True>
   >>> x2 = tf.constant([3.0, 1.0, 2.0])
-  >>> tf.print(tf.math.is_strictly_increasing(x2))
+  >>> tf.math.is_strictly_increasing(x2)
   <tf.Tensor: shape=(), dtype=bool, numpy=False>
 
   Args:
