992723f8471e90699d2a9c4b9ad9af7ac79de2da
Jeff Daily <jeff.daily@amd.com>
2019-12-17 22:30:21 +0000

    skip unsupported tests on ROCm platform

--- tensorflow/python/keras/layers/lstm_test.py
+++ tensorflow/python/keras/layers/lstm_test.py
@@ -26,6 +26,7 @@ from tensorflow.python.eager import context
 from tensorflow.python.framework import test_util as tf_test_util
 from tensorflow.python.keras import keras_parameterized
 from tensorflow.python.keras import testing_utils
+from tensorflow.python.platform import build_info
 from tensorflow.python.platform import test
 from tensorflow.python.training import adam
 from tensorflow.python.training import gradient_descent
@@ -144,6 +145,8 @@ class LSTMLayerTest(keras_parameterized.TestCase):
 
   @parameterized.parameters([True, False])
   def test_with_masking_layer_LSTM(self, unroll):
+    if build_info.is_rocm_build:
+      self.skipTest("Skipping the test as ROCm MIOpen does not support padded input yet.")
     layer_class = keras.layers.LSTM
     inputs = np.random.random((2, 3, 4))
     targets = np.abs(np.random.random((2, 3, 5)))
@@ -383,6 +386,8 @@ class LSTMLayerTest(keras_parameterized.TestCase):
       self.assertEqual(len(layer.get_losses_for(x)), 1)
 
   def test_statefulness_LSTM(self):
+    if build_info.is_rocm_build:
+      self.skipTest("Skipping the test as ROCm MIOpen does not support padded input yet.")
     num_samples = 2
     timesteps = 3
     embedding_dim = 4
