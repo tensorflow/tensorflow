c345f83efd593217e660c7e55a5db6e2415bca23
Owen L - SFE <owen.lyke@sparkfun.com>
2019-12-18 15:21:59 -0700

    change ambiqsuite sdk references to release 2.2.0

--- tensorflow/lite/micro/examples/magic_wand/sparkfun_edge/output_handler.cc
+++ tensorflow/lite/micro/examples/magic_wand/sparkfun_edge/output_handler.cc
@@ -15,11 +15,11 @@ limitations under the License.
 
 #include "tensorflow/lite/micro/examples/magic_wand/output_handler.h"
 
-#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.0.0/boards/SparkFun_TensorFlow_Apollo3_BSP/bsp/am_bsp.h"
-#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.0.0/boards/SparkFun_TensorFlow_Apollo3_BSP/examples/example1_edge_test/src/tf_accelerometer/tf_accelerometer.h"
-#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.0.0/boards/SparkFun_TensorFlow_Apollo3_BSP/examples/example1_edge_test/src/tf_adc/tf_adc.h"
-#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.0.0/mcu/apollo3/am_mcu_apollo.h"
-#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.0.0/utils/am_util.h"
+#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/boards/SparkFun_TensorFlow_Apollo3_BSP/bsp/am_bsp.h"
+#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/boards/SparkFun_TensorFlow_Apollo3_BSP/examples/example1_edge_test/src/tf_accelerometer/tf_accelerometer.h"
+#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/boards/SparkFun_TensorFlow_Apollo3_BSP/examples/example1_edge_test/src/tf_adc/tf_adc.h"
+#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/mcu/apollo3/am_mcu_apollo.h"
+#include "tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/utils/am_util.h"
 
 void HandleOutput(tflite::ErrorReporter* error_reporter, int kind) {
   // The first time this method runs, set up our LEDs correctly
