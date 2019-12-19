ecaadd6ea54fe7938be4b54b70f6ab872628ad0f
Owen L - SFE <owen.lyke@sparkfun.com>
2019-12-19 13:53:02 -0700

    standardize to use of am_devices for led control

--- tensorflow/lite/micro/examples/magic_wand/sparkfun_edge/output_handler.cc
+++ tensorflow/lite/micro/examples/magic_wand/sparkfun_edge/output_handler.cc
@@ -23,43 +23,40 @@ void HandleOutput(tflite::ErrorReporter* error_reporter, int kind) {
   // The first time this method runs, set up our LEDs correctly
   static bool is_initialized = false;
   if (!is_initialized) {
-    am_hal_gpio_pinconfig(AM_BSP_GPIO_LED_RED, g_AM_HAL_GPIO_OUTPUT_12);
-    am_hal_gpio_pinconfig(AM_BSP_GPIO_LED_BLUE, g_AM_HAL_GPIO_OUTPUT_12);
-    am_hal_gpio_pinconfig(AM_BSP_GPIO_LED_GREEN, g_AM_HAL_GPIO_OUTPUT_12);
-    am_hal_gpio_pinconfig(AM_BSP_GPIO_LED_YELLOW, g_AM_HAL_GPIO_OUTPUT_12);
+    // Setup LED's as outputs
+#ifdef AM_BSP_NUM_LEDS
+    am_devices_led_array_init(am_bsp_psLEDs, AM_BSP_NUM_LEDS);
+    am_devices_led_array_out(am_bsp_psLEDs, AM_BSP_NUM_LEDS, 0x00000000);
+#endif
     is_initialized = true;
   }
+
   // Toggle the yellow LED every time an inference is performed
-  static int count = 0;
-  ++count;
-  if (count & 1) {
-    am_hal_gpio_output_set(AM_BSP_GPIO_LED_YELLOW);
-  } else {
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_YELLOW);
-  }
+  am_devices_led_toggle(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
+
   // Set the LED color and print a symbol (red: wing, blue: ring, green: slope)
   if (kind == 0) {
     error_reporter->Report(
         "WING:\n\r*         *         *\n\r *       * *       "
         "*\n\r  *     *   *     *\n\r   *   *     *   *\n\r    * *       "
         "* *\n\r     *         *\n\r");
-    am_hal_gpio_output_set(AM_BSP_GPIO_LED_RED);
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_BLUE);
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_GREEN);
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_RED);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_BLUE);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_GREEN);
   } else if (kind == 1) {
     error_reporter->Report(
         "RING:\n\r          *\n\r       *     *\n\r     *         *\n\r "
         "   *           *\n\r     *         *\n\r       *     *\n\r      "
         "    *\n\r");
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_RED);
-    am_hal_gpio_output_set(AM_BSP_GPIO_LED_BLUE);
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_GREEN);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_RED);
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_BLUE);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_GREEN);
   } else if (kind == 2) {
     error_reporter->Report(
         "SLOPE:\n\r        *\n\r       *\n\r      *\n\r     *\n\r    "
         "*\n\r   *\n\r  *\n\r * * * * * * * *\n\r");
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_RED);
-    am_hal_gpio_output_clear(AM_BSP_GPIO_LED_BLUE);
-    am_hal_gpio_output_set(AM_BSP_GPIO_LED_GREEN);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_RED);
+    am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_BLUE);
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_GREEN);
   }
 }
