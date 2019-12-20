3dbe4fb4ae9f210b540f7a9668e4dda10d17ff6b
Haoyu Zhang <haoyuzhang@google.com>
2019-12-20 11:34:09 -0800

    Minor tweaks on logging format and verbosity in process function library runtime.
    PiperOrigin-RevId: 286613321
    Change-Id: Ifd2c509243daba2be3be035c198ff84ec0378b9a

--- tensorflow/core/common_runtime/process_function_library_runtime.cc
+++ tensorflow/core/common_runtime/process_function_library_runtime.cc
@@ -843,7 +843,7 @@ Status ProcessFunctionLibraryRuntime::InstantiateMultiDevice(
       auto attrs = AttrSlice(&shard.attr());
       VLOG(1) << "Start instantiating component function " << unique_name
               << " on device " << target;
-      VLOG(2) << DebugString(shard);
+      VLOG(4) << DebugString(shard);
 
       auto* component_handle = new FunctionLibraryRuntime::Handle;
       auto done = [this, status, unique_name, comp_data, component_handle,
@@ -851,7 +851,7 @@ Status ProcessFunctionLibraryRuntime::InstantiateMultiDevice(
         status->Update(s);
 
         VLOG(1) << "Finished instantiating component function " << unique_name
-                << "with handle " << *component_handle << " status: " << s;
+                << " with handle " << *component_handle << " status: " << s;
         if (status->ok()) {
           {
             mutex_lock l(mu_);
