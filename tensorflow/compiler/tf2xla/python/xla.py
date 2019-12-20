a5a0ad4300224479823a17181fb138db2c4fa10f
A. Unique TensorFlower <gardener@tensorflow.org>
2019-12-20 06:36:47 -0800

    Expose XLA Gather/Scatter to python via the tf2xla bridge
    The semantics of these operations are a lot more flexible then tf.gather
    and tf.scatter so this change enables the usage of these more powerful
    APIs.
    
    This API is *not* stable so any user have to be prepared for potential
    breaking changes.
    
    PiperOrigin-RevId: 286569124
    Change-Id: If1690d749deb5d08bb89532842fecaad2c2921d4

--- tensorflow/compiler/tf2xla/python/xla.py
+++ tensorflow/compiler/tf2xla/python/xla.py
@@ -416,3 +416,27 @@ sort = gen_xla_ops.xla_sort
 key_value_sort = gen_xla_ops.xla_key_value_sort
 while_loop = gen_xla_ops.xla_while
 dequantize = gen_xla_ops.xla_dequantize
+
+
+def gather(operand, start_indices, dimension_numbers, slice_sizes,
+           indices_are_sorted=False, name=None):
+  return gen_xla_ops.xla_gather(
+      operand,
+      start_indices,
+      slice_sizes=slice_sizes,
+      dimension_numbers=dimension_numbers.SerializeToString(),
+      indices_are_sorted=indices_are_sorted,
+      name=name)
+
+
+def scatter(operand, scatter_indices, updates, update_computation,
+            dimension_numbers, indices_are_sorted=False, name=None):
+  return gen_xla_ops.xla_scatter(
+      operand,
+      scatter_indices,
+      updates,
+      update_computation=update_computation,
+      dimension_numbers=dimension_numbers.SerializeToString(),
+      indices_are_sorted=indices_are_sorted,
+      name=name)
+
