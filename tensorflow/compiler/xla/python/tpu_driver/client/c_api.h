71dffc1368ce6a619b894f8ae3bdd34769294e4f
Henry Tan <henrytan@google.com>
2019-12-20 15:04:31 -0800

    Adding CompileProgram/LoadProgram/UnloadProgram/ExecuteProgram/AllocateTuple scaffolding to Experimental C API.
    PiperOrigin-RevId: 286646527
    Change-Id: Iec56834e9cf9cdc9fbbdda86843d7550873d915d

--- tensorflow/compiler/xla/python/tpu_driver/client/c_api.h
+++ tensorflow/compiler/xla/python/tpu_driver/client/c_api.h
@@ -32,16 +32,65 @@ typedef struct TpuEvent TpuEvent;
 
 typedef struct TpuBufferHandleInternal TpuBufferHandleInternal;
 
+typedef struct TpuCompiledProgramHandleInternal
+    TpuCompiledProgramHandleInternal;
+
+typedef struct TpuLoadedProgramHandleInternal TpuLoadedProgramHandleInternal;
+
 typedef struct TpuBufferHandle {
   TpuBufferHandleInternal* internal_handle;
   TpuEvent* event;
 } TpuBufferHandle;
+
+typedef struct TpuCompiledProgramHandle {
+  TpuCompiledProgramHandleInternal* internal_handle;
+  TpuEvent* event;
+} TpuCompiledProgramHandle;
+
+typedef struct TpuLoadedProgramHandle {
+  TpuLoadedProgramHandleInternal* internal_handle;
+  TpuEvent* event;
+} TpuLoadedProgramHandle;
+
+typedef struct HloProto {
+  // TODO(b/146662059): this is a temp plug for xla::HloProto
+} HloProto;
+
+typedef struct DeviceAssignmentProto {
+  // TODO(b/146662059): this is a temp plug for xla::DeviceAssignmentProto
+} DeviceAssignmentProto;
 
 typedef void(PrototypeTpuDriver_Initialize)(struct TpuDriverFn* driver_fn);
 typedef struct TpuDriver*(PrototypeTpuDriver_Open)(const char* worker);
 typedef void(PrototypeTpuDriver_Close)(struct TpuDriver* driver);
 
 const int32_t MemoryRegion_HBM = 1;
+
+typedef struct TpuCompiledProgramHandle*(PrototypeTpuDriver_CompileProgram)(
+    struct TpuDriver* driver, const struct HloProto& source,
+    int32_t num_replicas, int32_t eventc, struct TpuEvent** eventv);
+
+typedef struct TpuLoadedProgramHandle*(PrototypeTpuDriver_LoadProgram)(
+    struct TpuDriver* driver, int32_t core_id,
+    const struct TpuCompiledProgramHandle* handle, int32_t eventc,
+    struct TpuEvent** eventv);
+
+typedef struct TpuLoadedProgramHandle*(PrototypeTpuDriver_UnloadProgram)(
+    struct TpuDriver* driver, int32_t core_id,
+    struct TpuLoadedProgramHandle* handle, int32_t eventc,
+    struct TpuEvent** eventv);
+
+typedef struct TpuLoadedProgramHandle*(PrototypeTpuDriver_ExecuteProgram)(
+    struct TpuDriver* driver, struct TpuLoadedProgramHandle* handle,
+    int32_t inputc, struct TpuBufferHandle** input_buffer_handle,
+    int32_t outputc, struct TpuBufferHandle** output_buffer_handle,
+    const DeviceAssignmentProto& device_assignment, int32_t eventc,
+    struct TpuEvent** eventv);
+
+typedef struct TpuBufferHandle*(PrototypeTpuDriver_AllocateTuple)(
+    struct TpuDriver* driver, int32_t core_id, int32_t memory_region,
+    int64_t num_bytes, int32_t bufferc, struct TpuBufferHandle** buffer_handle,
+    int32_t eventc, struct TpuEvent** eventv);
 
 typedef struct TpuBufferHandle*(PrototypeTpuDriver_Allocate)(
     struct TpuDriver* driver, int32_t core_id, int32_t memory_region,
@@ -70,6 +119,16 @@ typedef const char*(PrototypeTpuDriver_Version)();
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Initialize TpuDriver_Initialize;
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Open TpuDriver_Open;
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Close TpuDriver_Close;
+TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_CompileProgram
+    TpuDriver_CompileProgram;
+TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_LoadProgram
+    TpuDriver_LoadProgram;
+TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_UnloadProgram
+    TpuDriver_UnloadProgram;
+TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_ExecuteProgram
+    TpuDriver_ExecuteProgram;
+TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_AllocateTuple
+    TpuDriver_AllocateTuple;
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Allocate TpuDriver_Allocate;
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Deallocate TpuDriver_Deallocate;
 TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_TransferToDevice
@@ -88,6 +147,11 @@ TPUDRIVER_CAPI_EXPORT extern PrototypeTpuDriver_Version TpuDriver_Version;
 struct TpuDriverFn {
   PrototypeTpuDriver_Open* TpuDriver_Open;                          // NOLINT
   PrototypeTpuDriver_Close* TpuDriver_Close;                        // NOLINT
+  PrototypeTpuDriver_CompileProgram* TpuDriver_CompileProgram;      // NOLINT
+  PrototypeTpuDriver_LoadProgram* TpuDriver_LoadProgram;            // NOLINT
+  PrototypeTpuDriver_UnloadProgram* TpuDriver_UnloadProgram;        // NOLINT
+  PrototypeTpuDriver_ExecuteProgram* TpuDriver_ExecuteProgram;      // NOLINT
+  PrototypeTpuDriver_AllocateTuple* TpuDriver_AllocateTuple;        // NOLINT
   PrototypeTpuDriver_Allocate* TpuDriver_Allocate;                  // NOLINT
   PrototypeTpuDriver_Deallocate* TpuDriver_Deallocate;              // NOLINT
   PrototypeTpuDriver_TransferToDevice* TpuDriver_TransferToDevice;  // NOLINT
