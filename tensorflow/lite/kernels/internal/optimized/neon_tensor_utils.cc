8a7097eb9b5168d6f21b3c8d6789fd7ce03ba343
Artem Belevich <tra@google.com>
2019-12-20 20:48:14 -0800

    Fixed TFLite build on windows w/ AVX enabled.
    PiperOrigin-RevId: 286679971
    Change-Id: I7cb05244983c005949524a0101077e5771e2f556

--- tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
+++ tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include <sys/types.h>
-#include <unistd.h>
 
 #include <algorithm>
 #include <cmath>
