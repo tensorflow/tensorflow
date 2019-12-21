59fe23fc3dfd9897ed9181a931b5eaf6f7910d7f
Owen L - SFE <owen.lyke@sparkfun.com>
2019-12-20 17:08:48 -0700

    add edge support in arduino project
    This is an ugly solution - I had a hard time determining how to manipulate the makefile and 'transform arduino source' python script to allow separation of implementations. I am eager to have input from someone who would know how to better oranize this.
    
    (but in a strictly technical sense this works and does not break support for the Nano 33 BLE Sense)

--- tensorflow/lite/micro/examples/person_detection/arduino/detection_responder.cc
+++ tensorflow/lite/micro/examples/person_detection/arduino/detection_responder.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#ifdef ARDUINO_ARDUINO_NANO33BLE
+
 #include "tensorflow/lite/micro/examples/person_detection/detection_responder.h"
 
 #include "Arduino.h"
@@ -54,3 +56,46 @@ void RespondToDetection(tflite::ErrorReporter* error_reporter,
   error_reporter->Report("Person score: %d No person score: %d", person_score,
                          no_person_score);
 }
+
+#endif // ARDUINO_ARDUINO_NANO33BLE
+
+
+
+#ifdef ARDUINO_SFE_EDGE
+
+#include "tensorflow/lite/micro/examples/person_detection/detection_responder.h"
+
+#include "am_bsp.h"  // NOLINT
+
+// This implementation will light up LEDs on the board in response to the
+// inference results.
+void RespondToDetection(tflite::ErrorReporter* error_reporter,
+                        uint8_t person_score, uint8_t no_person_score) {
+  static bool is_initialized = false;
+  if (!is_initialized) {
+    // Setup LED's as outputs.  Leave red LED alone since that's an error
+    // indicator for sparkfun_edge in image_provider.
+    am_devices_led_init((am_bsp_psLEDs + AM_BSP_LED_BLUE));
+    am_devices_led_init((am_bsp_psLEDs + AM_BSP_LED_GREEN));
+    am_devices_led_init((am_bsp_psLEDs + AM_BSP_LED_YELLOW));
+    is_initialized = true;
+  }
+
+  // Toggle the blue LED every time an inference is performed.
+  am_devices_led_toggle(am_bsp_psLEDs, AM_BSP_LED_BLUE);
+
+  // Turn on the green LED if a person was detected.  Turn on the yellow LED
+  // otherwise.
+  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
+  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_GREEN);
+  if (person_score > no_person_score) {
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_GREEN);
+  } else {
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
+  }
+
+  error_reporter->Report("Person score: %d No person score: %d", person_score,
+                         no_person_score);
+}
+
+#endif // ARDUINO_SFE_EDGE
