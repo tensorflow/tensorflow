016013a52a2b21fda9d02f9ae6358177b5b94ffd
Mbah-Javis <jelvismbah18@gmail.com>
2019-12-23 07:41:16 +0100

    Update tensorflow/python/ops/image_ops_impl.py
    Co-Authored-By: Kilaru Yasaswi Sri Chandra Gandhi <yasaswisrichandragandhi@gmail.com>

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -644,7 +644,7 @@ def transpose(image, name=None):
   Usage Example:
     ```python
     >>> x = tf.random.normal(shape=(256, 256, 3))
-    tf.image.transpose(x) 
+    >>> tf.image.transpose(x) 
   """
   with ops.name_scope(name, 'transpose', [image]):
     image = ops.convert_to_tensor(image, name='image')
