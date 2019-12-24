484467dc957750de6075dc4bef800c97954ed03d
Yong Tang <yong.tang.github@outlook.com>
2019-12-24 05:02:28 +0000

    Add complex64 and complex128 GPU support for tf.unstack as well
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/unstack_op_test.py
+++ tensorflow/python/kernel_tests/unstack_op_test.py
@@ -167,6 +167,23 @@ class UnstackOpTest(test.TestCase):
     y = self.evaluate(array_ops.unstack(x, axis=1)[0])
     self.assertEqual(y.shape, (0, 2))
 
+  def testComplexGpu(self):
+    if not test_util.is_gpu_available():
+      self.skipTest('No GPU available')
+
+    np.random.seed(7)
+    with test_util.force_gpu():
+      for shape in (2,), (3,), (2, 3), (3, 2), (4, 3, 2):
+        for dtype in [np.complex64, np.complex128]:
+          data = np.random.randn(*shape).astype(dtype)
+          # Convert data to a single tensorflow tensor
+          x = constant_op.constant(data)
+          # Unstack into a list of tensors
+          cs = array_ops.unstack(x, num=shape[0])
+          self.assertEqual(type(cs), list)
+          self.assertEqual(len(cs), shape[0])
+          cs = [self.evaluate(c) for c in cs]
+          self.assertAllEqual(cs, data)
 
 if __name__ == '__main__':
   test.main()
