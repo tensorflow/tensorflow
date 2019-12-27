fa6f349a9779a0a05a4c1847e35b7f3ce6a7dc14
Richard Xiao <52942327+RichardXiao13@users.noreply.github.com>
2019-12-27 12:12:50 -0600

    Update image_ops_impl.py

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -791,7 +791,7 @@ def pad_to_bounding_box(image, offset_height, offset_width, target_height,
 Usage Example:
 >>> x = tf.random.normal(shape=(256, 256, 3))
 >>> x = tf.image.pad_to_bounding_box(x, 2, 2, 260, 260)
-<x.shape = TensorShape([Dimension(260), Dimension(260), Dimension(3)])>
+<x.shape = TensorShape([260, 260, 3])>
 
   Raises:
     ValueError: If the shape of `image` is incompatible with the `offset_*` or
