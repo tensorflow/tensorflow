fc47bc4ef1e4b6fa209d9d8746bad9bdf47511db
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2019-12-28 05:46:21 +0700

    Fixed indentation and reverting the output back

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -2936,7 +2936,8 @@ def rgb_to_yiq(images):
 
   >>> x = tf.constant([[[1.0, 2.0, 3.0]]])
   >>> tf.image.rgb_to_yiq(x)
-  array([[[ 1.815     , -0.91724455,  0.09962624]]], dtype=float32)
+  <tf.Tensor: shape=(1, 1, 3), dtype=float32,
+  numpy=array([[[ 1.815     , -0.91724455,  0.09962624]]], dtype=float32)>
 
   Args:
     images: 2-D or higher rank. Image data to convert. Last dimension must be
