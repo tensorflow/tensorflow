ab01c26d864fb54b80718f1ae6bada2d302329b4
Ananya Gangavarapu <anigasan637@gmail.com>
2019-12-27 19:57:06 -0500

    Update math_ops.py

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -345,7 +345,9 @@ def multiply(x, y, name=None):
   
   Args:
     'x': A 'Tensor'. Must be one of the following types: 
-    'bfloat16', 'half', 'float32', 'float64', 'uint8', 'int8', 'uint16', 'int16', 'int32', 'int64', 'complex64', 'complex128'.
+      'bfloat16', 'half', 'float32', 'float64', 
+      'uint8', 'int8', 'uint16', 'int16',
+      'int32', 'int64', 'complex64', 'complex128'.
     'y': A 'Tensor'. Must have the same type as 'x'.
     'name': A name for the operation (optional).
     
