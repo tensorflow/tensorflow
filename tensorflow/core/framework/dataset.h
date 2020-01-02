96de19babd874ea6be7674520e0d8351ca89a749
Rohan Jain <rohanj@google.com>
2020-01-02 11:36:25 -0800

    Editing IteratorStateReader and IteratorStateWriter documentation to indicate their thread safeness.
    PiperOrigin-RevId: 287867779
    Change-Id: Icab5c37e13fc730250264888dab42ff574477da3

--- tensorflow/core/framework/dataset.h
+++ tensorflow/core/framework/dataset.h
@@ -70,7 +70,7 @@ class DatasetBase;
 class SerializationContext;
 
 // Interface for reading values from a key-value store.
-// Used for restoring iterator state.
+// Used for restoring iterator state. This class is thread safe.
 // Please see comment on IteratorStateWriter for guidance around using the
 // Read*(key, val) vs Read*(name, key, val).
 class IteratorStateReader {
@@ -91,7 +91,7 @@ class IteratorStateReader {
 };
 
 // Interface for writing values to a key-value store.
-// Used for saving iterator state.
+// Used for saving iterator state. Not thread safe.
 // The IteratorStateWriter creates a tensor for each unique iterator name it
 // sees. For the Write*(key, val) API's the key is expected to encode this
 // name as keys are required to be produced using the full_name() method.
