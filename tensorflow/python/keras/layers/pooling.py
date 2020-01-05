101325cf1d69c63b061aeafd2cf4dbf5f45c8955
Arjun A Rastogi <43435971+generationXcode@users.noreply.github.com>
2020-01-05 11:40:55 +0530

    Update pooling.py

--- tensorflow/python/keras/layers/pooling.py
+++ tensorflow/python/keras/layers/pooling.py
@@ -447,7 +447,6 @@ class MaxPooling2D(Pooling2D):
 class AveragePooling2D(Pooling2D):
   """Average pooling operation for spatial data.
   
-  >>> import numpy as np
   >>> x = tf.keras.Input(shape=(3, 3, 1), name='x')
   >>> average_pooling_layer = tf.keras.layers.AveragePooling2D(pool_size = (1,1),strides = (1,1),padding = "same")(x)
   <tf.Tensor 'average_pooling2d_3/Identity:0' shape=(None, 3, 3, 1) dtype=float32>
