ebc6577281722dcf02c2a7cecbe368bdd8ddea4d
Benoit Jacob <benoitjacob@google.com>
2020-01-06 13:26:45 -0800

    Fix compilation broken by cl/288340160.
    PiperOrigin-RevId: 288363368
    Change-Id: I45a3d26b8b2fe937270e0beb989a7cb82bd81103

--- tensorflow/lite/experimental/ruy/detect_arm.cc
+++ tensorflow/lite/experimental/ruy/detect_arm.cc
@@ -42,7 +42,7 @@ limitations under the License.
 
 #include "tensorflow/lite/experimental/ruy/detect_arm.h"
 
-#ifdef __linux__
+#if defined __linux__ && defined __aarch64__
 #include <sys/auxv.h>
 #endif
 
