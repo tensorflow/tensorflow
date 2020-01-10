fede1f2256e6bfee4880b6b2486380e65e855432
Evan Cheung <evan.cl.wh@gmail.com>
2020-01-10 15:41:05 +0800

    Revert "TensorFlow: Improve performance of Alexnet"
    This reverts commit 9c3043ff3bf31a6a81810b4ce9e87ef936f1f529.

--- tensorflow/core/kernels/conv_ops_gpu_3.cu.cc
+++ tensorflow/core/kernels/conv_ops_gpu_3.cu.cc
@@ -1,18 +1,3 @@
-/* Copyright 2015 Google Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 #if GOOGLE_CUDA
 
 #define EIGEN_USE_GPU
@@ -94,7 +79,7 @@ __global__ void SwapDimension0And2InTensor3(int nthreads, const T* input,
 
     int input_index = TensorIndexToFlat(input_tensor_index, input_dims);
 
-    output[output_index] = __ldg(input + input_index);
+    output[output_index] = input[input_index];
   }
 }
 
@@ -119,7 +104,7 @@ __global__ void SwapDimension1And2InTensor3(int nthreads, const T* input,
 
     int input_index = TensorIndexToFlat(input_tensor_index, input_dims);
 
-    output[output_index] = __ldg(input + input_index);
+    output[output_index] = input[input_index];
   }
 }
 
