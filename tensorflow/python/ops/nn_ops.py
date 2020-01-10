85428623e40007f1ff7486171f4f3189748bdb10
boron <31139873+boronhub@users.noreply.github.com>
2020-01-10 14:53:32 +0530

    Update nn_ops.py

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -1855,7 +1855,7 @@ def conv2d_v2(input,  # pylint: disable=redefined-builtin
   >>> kernel_in = np.array([
   ...  [ [[2, 0.1]], [[3, 0.2]] ],
   ...  [ [[0, 0.3]],[[1, 0.4]] ], ])
-  >>> x = tf.Variable(shape=tf.TensorShape(1, 5, 5, 1))
+  >>> x = tf.compat.v1.placeholder(dtype=tf.float32, shape=[1, 5, 5, 1])
   >>> kernel = tf.constant(kernel_in, dtype=tf.float32)
   <tf.Tensor 'Conv2D_1:0' shape=(1, 4, 4, 2) dtype=float32>
 
