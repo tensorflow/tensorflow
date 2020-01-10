4eb92dafaf6222e77fdf0dfcbc8d9c359fe7a823
Deven Desai <deven.desai.amd@gmail.com>
2020-01-10 22:36:02 +0000

    Syncing the CSB script with the one in the upstream repo

--- tensorflow/tools/ci_build/linux/rocm/run_csb_tests.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_csb_tests.sh
@@ -38,33 +38,11 @@ yes "" | $PYTHON_BIN_PATH configure.py
 bazel test \
       --config=rocm \
       -k \
-      --test_tag_filters=gpu,-no_gpu,-no_rocm,-v1only \
-      --jobs=${N_JOBS} \
-      --local_test_jobs=1 \
+      --test_tag_filters=gpu,-no_gpu,-no_rocm,-benchmark-test,-no_oss,-oss_serial,-rocm_multi_gpu, \
       --test_timeout 600,900,2400,7200 \
       --test_output=errors \
-      --test_sharding_strategy=disabled \
-      -- \
-      //tensorflow/python/kernel_tests:conv_ops_test_gpu \
-&& bazel test \
-      --config=rocm \
-      -k \
-      --test_tag_filters=gpu,-no_gpu,-no_rocm,-v1only \
-      --jobs=${N_JOBS} \
-      --local_test_jobs=1 \
-      --test_timeout 600,900,2400,7200 \
-      --test_output=errors \
-      --test_sharding_strategy=disabled \
-      -- \
-      //tensorflow/core/nccl:nccl_manager_test \
-&& bazel test \
-      --config=rocm \
-      -k \
-      --test_tag_filters=gpu,-no_gpu,-no_rocm,-v1only,-benchmark-test,-no_oss,-oss_serial,-rocm_multi_gpu \
       --jobs=${N_JOBS} \
       --local_test_jobs=${TF_GPU_COUNT} \
-      --test_timeout 600,900,2400,7200 \
-      --test_output=errors \
       --test_sharding_strategy=disabled \
       --run_under=//tensorflow/tools/ci_build/gpu_build:parallel_gpu_execute \
       -- \
@@ -72,5 +50,15 @@ bazel test \
       -//tensorflow/compiler/... \
       -//tensorflow/lite/... \
       -//tensorflow/python/compiler/tensorrt/... \
-      -//tensorflow/python/kernel_tests:conv_ops_test_gpu
+&& bazel test \
+      --config=rocm \
+      -k \
+      --test_tag_filters=gpu \
+      --test_timeout 600,900,2400,7200 \
+      --test_output=errors \
+      --jobs=${N_JOBS} \
+      --local_test_jobs=1 \
+      --test_sharding_strategy=disabled \
+      -- \
+      //tensorflow/core/nccl:nccl_manager_test
 
