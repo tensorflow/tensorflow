6e7be1f6a55fe35f82b7c8c94014b31811cfad88
Deven Desai <deven.desai.amd@gmail.com>
2020-01-11 01:41:52 +0000

    adding no_rocm tag to tests/subtests that are currently failing on the ROCm platform

--- tensorflow/compiler/tests/matrix_triangular_solve_op_test.py
+++ tensorflow/compiler/tests/matrix_triangular_solve_op_test.py
@@ -87,6 +87,12 @@ class MatrixTriangularSolveOpTest(xla_test.XLATestCase):
       self._VerifyTriangularSolveCombo(a.astype(dtype), b.astype(dtype))
 
   def testBasicComplexDtypes(self):
+
+    if xla_test.test.is_built_with_rocm():
+      # The folowing subtest invokes the call to "BlasTrsm"
+      # That operation is currently not supported on the ROCm platform
+      self.skipTest("BlasTrsm op for complex types is not supported in ROCm")
+
     rng = np.random.RandomState(0)
     a = np.tril(rng.randn(5, 5) + rng.randn(5, 5) * 1j)
     b = rng.randn(5, 7) + rng.randn(5, 7) * 1j
