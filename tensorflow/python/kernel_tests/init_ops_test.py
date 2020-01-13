5097ebda4743297518f2384cced64ea03920f70c
Yong Tang <yong.tang.github@outlook.com>
2020-01-13 15:49:31 +0000

    Add test case for GitHub issue 35710
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/init_ops_test.py
+++ tensorflow/python/kernel_tests/init_ops_test.py
@@ -537,6 +537,11 @@ class RangeTest(test.TestCase):
         math_ops.range(
             0, 0, 1, dtype=dtypes.float64).dtype, dtypes.float64)
 
+  def testMixedDType(self):
+    # Test case for GitHub issue 35710
+    tf_ans = math_ops.range(constant_op.constant(4, dtype=dtypes.int32), dtype=dtypes.int64)
+    self.assertAllEqual(self.evaluate(tf_ans), np.array([0, 1, 2, 3]))
+
 
 # TODO(vrv): move to sequence_ops_test?
 class LinSpaceTest(test.TestCase):
