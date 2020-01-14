171ba06f5e52078e0aa2112797b5a4227370bbd5
Andrew Selle <aselle@google.com>
2020-01-13 16:47:00 -0800

    Bring back overflow detection with dummy expect implementation for windows. Automated g4 rollback of changelist 288577610.
    PiperOrigin-RevId: 289548448
    Change-Id: Ic2c0d5b33d4cb5ed4c8e8e7f83278c99acc34bbd

--- tensorflow/lite/core/subgraph.h
+++ tensorflow/lite/core/subgraph.h
@@ -23,6 +23,7 @@ limitations under the License.
 #include "tensorflow/lite/allocation.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/core/api/profiler.h"
+#include "tensorflow/lite/core/macros.h"
 #include "tensorflow/lite/delegates/nnapi/nnapi_delegate.h"
 #include "tensorflow/lite/experimental/resource/resource_base.h"
 #include "tensorflow/lite/memory_planner.h"
