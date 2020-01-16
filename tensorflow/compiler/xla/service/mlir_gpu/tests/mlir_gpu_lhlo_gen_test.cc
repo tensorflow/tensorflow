4d9ae5942fd8bc968e4bcc2559e5f9051ca6e473
Alexander Belyaev <pifon@google.com>
2020-01-16 04:54:58 -0800

    [[XLA:GPU]][[MLIR]] Emit MemRefs with specified permutations out of HLO types.
    PiperOrigin-RevId: 290043773
    Change-Id: I2fa880cab1f07e578c5a8fb224f67a10abf059cc

--- tensorflow/compiler/xla/service/mlir_gpu/tests/mlir_gpu_lhlo_gen_test.cc
+++ tensorflow/compiler/xla/service/mlir_gpu/tests/mlir_gpu_lhlo_gen_test.cc
@@ -88,14 +88,15 @@ TEST_F(LhloGenTest, Copy) {
   CompileAndVerifyIr(R"(
 HloModule Copy
 
-ENTRY %Copy (x: f32[2,2]) -> f32[2,2] {
-  %x = f32[2,2]{1,0} parameter(0)
-  ROOT %copy = f32[2,2]{1,0} copy(f32[2,2]{1,0} %x)
+ENTRY %Copy (x: f32[2,4,8]) -> f32[2,4,8] {
+  %x = f32[2,4,8]{1,0,2} parameter(0)
+  ROOT %copy = f32[2,4,8]{2,0,1} copy(f32[2,4,8]{1,0,2} %x)
 })",
                      R"(
-;CHECK: func @copy(%[[OPERAND:.*]]: [[TYPE:.*]], %[[RESULT:.*]]: [[TYPE]]) {
-;CHECK:   "xla_lhlo.copy"(%[[OPERAND]], %[[RESULT]]) : ([[TYPE]], [[TYPE]]) -> ()
-;CHECK: }
+;CHECK: #[[MAP0:.*]] = affine_map<(d0, d1, d2) -> (d2, d0, d1)>
+;CHECK: #[[MAP1:.*]] = affine_map<(d0, d1, d2) -> (d1, d0, d2)>
+;CHECK: func @copy(%[[OPERAND:.*]]: memref<2x4x8xf32, #[[MAP0]]>, %[[RESULT:.*]]: memref<2x4x8xf32, #[[MAP1]]>) {
+;CHECK:   "xla_lhlo.copy"(%[[OPERAND]], %[[RESULT]]) : (memref<2x4x8xf32, #[[MAP0]]>, memref<2x4x8xf32, #[[MAP1]]>) -> ()
       )");
 }
 
