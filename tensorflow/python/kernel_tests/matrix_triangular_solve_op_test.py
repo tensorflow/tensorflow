b1b7f38c258f28bd6c85b34aeaf690b108d52d52
Srinivas Vasudevan <srvasude@google.com>
2020-01-15 17:00:18 -0800

    Add Broadcasted Matrix Triangular Solve.
    Add Numpy-style broadcasting in the batch dimensions for tf.linalg.triangular_solve op. The last two dimensions of both operands constitute the matrix dimensions. The dimensions beyond these are broadcasted to form a common output shape with the standard NumPy broadcasting rules. (https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html)
    Note: This implementation differs from Numpy's behavior in that vectors (rank-1 Tensors) are not promoted to matrices (rank-2 Tensors) by appending/prepending dimensions.
    PiperOrigin-RevId: 289966825
    Change-Id: Ib276b9ed1f4b7d10c25617d7ba5f1564b2077610

--- tensorflow/python/kernel_tests/matrix_triangular_solve_op_test.py
+++ tensorflow/python/kernel_tests/matrix_triangular_solve_op_test.py
@@ -20,7 +20,6 @@ from __future__ import print_function
 
 import numpy as np
 
-from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import linalg_ops
@@ -68,31 +67,32 @@ class MatrixTriangularSolveOpTest(test.TestCase):
       else:
         a_np = a
       if adjoint:
-        a_np = np.conj(np.transpose(a_np))
+        axes = list(range(len(a_np.shape)))
+        axes[-2] = -1
+        axes[-1] = -2
+        a_np = np.conj(np.transpose(a_np, axes=axes))
 
       if batch_dims is not None:
         a = np.tile(a, batch_dims + [1, 1])
         a_np = np.tile(a_np, batch_dims + [1, 1])
         b = np.tile(b, batch_dims + [1, 1])
 
-      with self.cached_session(use_gpu=True) as sess:
-        if use_placeholder:
-          a_tf = array_ops.placeholder(a.dtype)
-          b_tf = array_ops.placeholder(b.dtype)
-          tf_ans = linalg_ops.matrix_triangular_solve(
-              a_tf, b_tf, lower=lower, adjoint=adjoint)
-          tf_val = sess.run(tf_ans, feed_dict={a_tf: a, b_tf: b})
-          np_ans = np.linalg.solve(a_np, b)
-        else:
-          a_tf = constant_op.constant(a)
-          b_tf = constant_op.constant(b)
-          tf_ans = linalg_ops.matrix_triangular_solve(
-              a_tf, b_tf, lower=lower, adjoint=adjoint)
-          tf_val = self.evaluate(tf_ans)
-          np_ans = np.linalg.solve(a_np, b)
-          self.assertEqual(np_ans.shape, tf_ans.get_shape())
-        self.assertEqual(np_ans.shape, tf_val.shape)
-        self.assertAllClose(np_ans, tf_val)
+      def broadcast(a, b):
+        b1 = b + np.zeros(a.shape[:-2] + (1, 1), dtype=b.dtype)
+        return a, b1
+
+      a_tf = a
+      b_tf = b
+      if use_placeholder:
+        a_tf = array_ops.placeholder_with_default(a_tf, shape=None)
+        b_tf = array_ops.placeholder_with_default(b_tf, shape=None)
+      tf_ans = linalg_ops.matrix_triangular_solve(
+          a_tf, b_tf, lower=lower, adjoint=adjoint)
+      tf_val = self.evaluate(tf_ans)
+      a_np, b = broadcast(a_np, b)
+      np_ans = np.linalg.solve(a_np, b)
+      self.assertEqual(np_ans.shape, tf_val.shape)
+      self.assertAllClose(np_ans, tf_val)
 
   @test_util.run_deprecated_v1
   def testSolve(self):
@@ -135,6 +135,50 @@ class MatrixTriangularSolveOpTest(test.TestCase):
     self._verifySolveAllWaysReal(matrix, rhs, batch_dims=[2, 3])
     # Batch of 3x2x2x2 matrices, 3x2x2x3 right-hand sides.
     self._verifySolveAllWaysReal(matrix, rhs, batch_dims=[3, 2])
+
+  @test_util.run_deprecated_v1
+  @test_util.disable_xla("XLA cannot broadcast triangular solve.")
+  def testSolveBatchBroadcast(self):
+    # 2 x 2 x 2
+    matrix = np.array([[[1., 0.], [3., 4.]], [[1., 0.], [2., 1.]]])
+    # 2 x 3
+    rhs = np.array([[1., 0., 1.], [0., 1., 1.]])
+    # 2 x 2 x 3
+    self._verifySolveAllWaysReal(matrix, rhs)
+    # 2 x 2 x 2
+    matrix2 = np.array([[[1., 0.], [3., 4.]], [[2., 0.], [1., 6.3]]])
+    # 1 x 2 x 3
+    rhs = np.array([[[1., 0., 1.], [0., 1., 1.]]])
+    # 2 x 2 x 3
+    self._verifySolveAllWaysReal(matrix2, rhs)
+
+  @test_util.run_deprecated_v1
+  @test_util.disable_xla("XLA cannot broadcast triangular solve.")
+  def testSolveBatchBroadcastLargerBatches(self):
+    # 1 x 10 x 10
+    matrix = np.random.uniform(low=1, high=2., size=[1, 10, 10])
+    # 10 x 1
+    rhs = np.random.uniform(size=[10, 1])
+    # 1 x 10 x 1
+    self._verifySolveAllWaysReal(matrix, rhs)
+
+    # 2 x 10 x 10
+    matrix = np.random.uniform(low=1, high=2., size=[2, 10, 10])
+    # 10 x 1
+    rhs = np.random.uniform(size=[10, 1])
+    # 2 x 10 x 1
+    self._verifySolveAllWaysReal(matrix, rhs)
+
+    # 2 x 257 x 257
+    matrix = np.random.uniform(low=1, high=2., size=[2, 257, 257])
+    # Also ensure the matrix is well conditioned by making it diagonally
+    # dominant.
+    np.fill_diagonal(matrix[0, ...], 257 * 2)
+    np.fill_diagonal(matrix[1, ...], 257 * 2)
+    # 257 x 1
+    rhs = np.random.uniform(size=[257, 1])
+    # 2 x 257 x 1
+    self._verifySolveAllWaysReal(matrix, rhs)
 
   @test_util.run_deprecated_v1
   def testSolveBatchComplex(self):
--- tensorflow/python/ops/linalg_ops.py
+++ tensorflow/python/ops/linalg_ops.py
@@ -80,6 +80,67 @@ def _RegularizedGramianCholesky(matrix, l2_regularizer, first_kind):
 
 
 @tf_export(
+    'linalg.triangular_solve',
+    v1=['linalg.triangular_solve', 'matrix_triangular_solve'])
+def matrix_triangular_solve(matrix, rhs, lower=True, adjoint=False, name=None):
+  """Solve systems of linear equations with upper or lower triangular matrices.
+
+  `matrix` is a tensor of shape `[..., M, M]` whose inner-most 2 dimensions form
+  square matrices. If `lower` is `True` then the strictly upper triangular part
+  of each inner-most matrix is assumed to be zero and not accessed. If `lower`
+  is `False` then the strictly lower triangular part of each inner-most matrix
+  is assumed to be zero and not accessed. `rhs` is a tensor of shape
+  `[..., M, N]`.
+
+  The output is a tensor of shape `[..., M, N]`. If `adjoint` is `True` then the
+  innermost matrices in output satisfy matrix equations `matrix[..., i, k] *
+  output[..., k, j] = rhs[..., i, j]`. If `adjoint` is `False` then the
+  innermost matrices in output satisfy matrix equations
+  `adjoint(matrix[..., i, k]) * output[..., k, j] = rhs[..., i, j]`.
+
+  Example:
+
+  >>> a = tf.constant([[3,  0,  0,  0],
+  ...   [2,  1,  0,  0],
+  ...   [1,  0,  1,  0],
+  ...   [1,  1,  1,  1]], dtype=tf.float32)
+
+  >>> b = tf.constant([[4], [2], [4], [2]], dtype=tf.float32)
+  >>> x = tf.linalg.triangular_solve(a, b, lower=True)
+  >>> x
+  <tf.Tensor: shape=(4, 1), dtype=float32, numpy=
+  array([[ 1.3333334 ],
+         [-0.66666675],
+         [ 2.6666665 ],
+         [-1.3333331 ]], dtype=float32)>
+  >>> tf.matmul(a, x)
+  <tf.Tensor: shape=(4, 1), dtype=float32, numpy=
+  array([[4.],
+         [2.],
+         [4.],
+         [2.]], dtype=float32)>
+
+  Args:
+    matrix: A `Tensor`. Must be one of the following types: `float64`,
+      `float32`, `half`, `complex64`, `complex128`. Shape is `[..., M, M]`.
+    rhs: A `Tensor`. Must have the same type as `matrix`. Shape is `[..., M,
+      N]`.
+    lower: An optional `bool`. Defaults to `True`. Boolean indicating whether
+      the innermost matrices in matrix are lower or upper triangular.
+    adjoint: An optional `bool`. Defaults to `False`. Boolean indicating whether
+      to solve with matrix or its (block-wise) adjoint.
+    name: A name for the operation (optional).
+
+  Returns:
+    A `Tensor`. Has the same type as matrix, and shape is `[..., M, N]`.
+
+  """
+  with ops.name_scope(name, 'triangular_solve', [matrix, rhs]):
+    return gen_linalg_ops.matrix_triangular_solve(
+        matrix, rhs, lower=lower, adjoint=adjoint)
+
+
+@tf_export(
     'linalg.cholesky_solve', v1=['linalg.cholesky_solve', 'cholesky_solve'])
 @deprecation.deprecated_endpoints('cholesky_solve')
 def cholesky_solve(chol, rhs, name=None):
