9361cd6fe21c78fea9260935d5121c9c9cd76f93
Scott Zhu <scottzhu@google.com>
2020-01-16 23:10:34 -0800

    Update docstring for keras.layers.ReLU.
    PiperOrigin-RevId: 290215220
    Change-Id: Iff1321a25f7ee3c7a25c9725de8bbfcb7b65434c

--- tensorflow/python/keras/layers/advanced_activations.py
+++ tensorflow/python/keras/layers/advanced_activations.py
@@ -276,22 +276,42 @@ class ReLU(Layer):
   With default values, it returns element-wise `max(x, 0)`.
 
   Otherwise, it follows:
-  `f(x) = max_value` for `x >= max_value`,
-  `f(x) = x` for `threshold <= x < max_value`,
-  `f(x) = negative_slope * (x - threshold)` otherwise.
+  $$f(x) = max_value if x >= max_value$$
+  $$f(x) = x if threshold <= x < max_value$$
+  $$f(x) = negative_slope * (x - threshold) otherwise$$
+
+  Usage:
+
+  >>> layer = tf.keras.layers.ReLU()
+  >>> output = layer([-3.0, -1.0, 0.0, 2.0])
+  >>> list(output.numpy())
+  [0.0, 0.0, 0.0, 2.0]
+  >>> layer = tf.keras.layers.ReLU(max_value=1.0)
+  >>> output = layer([-3.0, -1.0, 0.0, 2.0])
+  >>> list(output.numpy())
+  [0.0, 0.0, 0.0, 1.0]
+  >>> layer = tf.keras.layers.ReLU(negative_slope=1.0)
+  >>> output = layer([-3.0, -1.0, 0.0, 2.0])
+  >>> list(output.numpy())
+  [-3.0, -1.0, 0.0, 2.0]
+  >>> layer = tf.keras.layers.ReLU(threshold=1.5)
+  >>> output = layer([-3.0, -1.0, 1.0, 2.0])
+  >>> list(output.numpy())
+  [0.0, 0.0, 0.0, 2.0]
 
   Input shape:
     Arbitrary. Use the keyword argument `input_shape`
-    (tuple of integers, does not include the samples axis)
+    (tuple of integers, does not include the batch axis)
     when using this layer as the first layer in a model.
 
   Output shape:
     Same shape as the input.
 
   Arguments:
-    max_value: Float >= 0. Maximum activation value.
-    negative_slope: Float >= 0. Negative slope coefficient.
-    threshold: Float. Threshold value for thresholded activation.
+    max_value: Float >= 0. Maximum activation value. Default to None, which
+      means unlimited.
+    negative_slope: Float >= 0. Negative slope coefficient. Default to 0.
+    threshold: Float. Threshold value for thresholded activation. Default to 0.
   """
 
   def __init__(self, max_value=None, negative_slope=0, threshold=0, **kwargs):
