c300154e6137578bfeb9018a4fa046214aead9b8
Scott Zhu <scottzhu@google.com>
2020-01-17 00:26:32 -0800

    Update docsting for keras.layers.LeakyReLU.
    PiperOrigin-RevId: 290221773
    Change-Id: I1fefe79538943067baf631a4b8b64595a76302f3

--- tensorflow/python/keras/layers/advanced_activations.py
+++ tensorflow/python/keras/layers/advanced_activations.py
@@ -34,19 +34,30 @@ class LeakyReLU(Layer):
   """Leaky version of a Rectified Linear Unit.
 
   It allows a small gradient when the unit is not active:
-  `f(x) = alpha * x for x < 0`,
-  `f(x) = x for x >= 0`.
+  $$f(x) = alpha * x if x < 0$$
+  $$f(x) = x if x >= 0$$
+
+  Usage:
+
+  >>> layer = tf.keras.layers.LeakyReLU()
+  >>> output = layer([-3.0, -1.0, 0.0, 2.0])
+  >>> list(output.numpy())
+  [-0.9, -0.3, 0.0, 2.0]
+  >>> layer = tf.keras.layers.LeakyReLU(alpha=0.1)
+  >>> output = layer([-3.0, -1.0, 0.0, 2.0])
+  >>> list(output.numpy())
+  [-0.3, -0.1, 0.0, 2.0]
 
   Input shape:
     Arbitrary. Use the keyword argument `input_shape`
-    (tuple of integers, does not include the samples axis)
+    (tuple of integers, does not include the batch axis)
     when using this layer as the first layer in a model.
 
   Output shape:
     Same shape as the input.
 
   Arguments:
-    alpha: Float >= 0. Negative slope coefficient.
+    alpha: Float >= 0. Negative slope coefficient. Default to 0.3.
 
   """
 
