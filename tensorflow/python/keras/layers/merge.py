38da9f48f9a6a755cb5ae3b6f99b3315a4a502d0
Yanhui Liang <yhliang@google.com>
2020-01-17 21:40:28 -0800

    Fix keras API docs.
    PiperOrigin-RevId: 290392438
    Change-Id: If70b8787e09a92c2ab63742d00db1599e43bcb9c

--- tensorflow/python/keras/layers/merge.py
+++ tensorflow/python/keras/layers/merge.py
@@ -225,18 +225,24 @@ class Add(_Merge):
 
   Examples:
 
-  ```python
-      import keras
+  >>> input_shape = (2, 3, 4)
+  >>> x1 = tf.random.normal(input_shape)
+  >>> x2 = tf.random.normal(input_shape)
+  >>> y = tf.keras.layers.Add()([x1, x2])
+  >>> print(y.shape)
+  (2, 3, 4)
 
-      input1 = keras.layers.Input(shape=(16,))
-      x1 = keras.layers.Dense(8, activation='relu')(input1)
-      input2 = keras.layers.Input(shape=(32,))
-      x2 = keras.layers.Dense(8, activation='relu')(input2)
-      # equivalent to `added = keras.layers.add([x1, x2])`
-      added = keras.layers.Add()([x1, x2])
-      out = keras.layers.Dense(4)(added)
-      model = keras.models.Model(inputs=[input1, input2], outputs=out)
-  ```
+  Used in a functional model:
+
+  >>> input1 = tf.keras.layers.Input(shape=(16,))
+  >>> x1 = tf.keras.layers.Dense(8, activation='relu')(input1)
+  >>> input2 = tf.keras.layers.Input(shape=(32,))
+  >>> x2 = tf.keras.layers.Dense(8, activation='relu')(input2)
+  >>> # equivalent to `added = tf.keras.layers.add([x1, x2])`
+  >>> added = tf.keras.layers.Add()([x1, x2])
+  >>> out = tf.keras.layers.Dense(4)(added)
+  >>> model = tf.keras.models.Model(inputs=[input1, input2], outputs=out)
+
   """
 
   def _merge_function(self, inputs):
@@ -592,29 +598,34 @@ class Dot(_Merge):
 
 @keras_export('keras.layers.add')
 def add(inputs, **kwargs):
-  """Functional interface to the `Add` layer.
+  """Functional interface to the `tf.keras.layers.Add` layer.
 
   Arguments:
-      inputs: A list of input tensors (at least 2).
+      inputs: A list of input tensors (at least 2) with the same shape.
       **kwargs: Standard layer keyword arguments.
 
   Returns:
-      A tensor, the sum of the inputs.
+      A tensor as the sum of the inputs. It has the same shape as the inputs.
 
   Examples:
 
-  ```python
-      import keras
+  >>> input_shape = (2, 3, 4)
+  >>> x1 = tf.random.normal(input_shape)
+  >>> x2 = tf.random.normal(input_shape)
+  >>> y = tf.keras.layers.add([x1, x2])
+  >>> print(y.shape)
+  (2, 3, 4)
 
-      input1 = keras.layers.Input(shape=(16,))
-      x1 = keras.layers.Dense(8, activation='relu')(input1)
-      input2 = keras.layers.Input(shape=(32,))
-      x2 = keras.layers.Dense(8, activation='relu')(input2)
-      added = keras.layers.add([x1, x2])
+  Used in a functiona model:
 
-      out = keras.layers.Dense(4)(added)
-      model = keras.models.Model(inputs=[input1, input2], outputs=out)
-  ```
+  input1 = tf.keras.layers.Input(shape=(16,))
+  x1 = tf.keras.layers.Dense(8, activation='relu')(input1)
+  input2 = tf.keras.layers.Input(shape=(32,))
+  x2 = tf.keras.layers.Dense(8, activation='relu')(input2)
+  added = tf.keras.layers.add([x1, x2])
+  out = tf.keras.layers.Dense(4)(added)
+  model = tf.keras.models.Model(inputs=[input1, input2], outputs=out)
+
   """
   return Add(**kwargs)(inputs)
 
