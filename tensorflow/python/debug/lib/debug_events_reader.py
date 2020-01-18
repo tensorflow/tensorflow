4d2383d00c24b9c58b0eb00a3bdd379fcd46a852
Shanqing Cai <cais@google.com>
2020-01-17 19:07:14 -0800

    [tfdbg2] Ensure that op_callbacks capture Placeholders for tf.functions
    - The Placeholder ops created for input args to tf.functions use a separate
      code path from the one currently covered by op_callbacks. The code path is
      in graph_only_ops.py. This CL adds the op_callbacks invocation in that module.
    - Unit tests are added.
    - Some existing unit tests are to accommodate the newly-tracked Placeholder ops.
    
    PiperOrigin-RevId: 290381708
    Change-Id: Ic0266b193218997bd0a8a70da386a04968d4d873

--- tensorflow/python/debug/lib/debug_events_reader.py
+++ tensorflow/python/debug/lib/debug_events_reader.py
@@ -399,7 +399,10 @@ class DebuggedGraph(object):
       graph_op_creation_digest: A GraphOpCreationDigest data object describing
         the creation of an op inside this graph.
     """
-    assert graph_op_creation_digest.op_name not in self._op_by_name
+    if graph_op_creation_digest.op_name in self._op_by_name:
+      raise ValueError(
+          "Duplicate op name: %s (op type: %s)" %
+          (graph_op_creation_digest.op_name, graph_op_creation_digest.op_type))
     self._op_by_name[
         graph_op_creation_digest.op_name] = graph_op_creation_digest
 
