42dd663fd2707285ad02f07f167e25f64b257e1a
Rick Wierenga <rick_wierenga@icloud.com>
2020-01-19 23:21:53 +0100

    Use static tensor in usage example

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -627,12 +627,12 @@ def transpose(image, name=None):
   """Transpose image(s) by swapping the height and width dimension.
   
   Usage Example:
-    >>> image = tf.random.uniform((1920, 1080, 3))
+    >>> image = tf.convert_to_tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]])
     >>> print(image.shape)
-    (1920, 1080, 3)
+    (3, 2, 2)
     >>> transposed_img = tf.image.transpose(image)
     >>> print(transposed_img.shape)
-    (1080, 1920, 3)
+    (2, 3, 2)
 
   Args:
     image: 4-D Tensor of shape `[batch, height, width, channels]` or 3-D Tensor
