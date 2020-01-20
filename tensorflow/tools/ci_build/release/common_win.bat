c1990bf38109cb8cc7ff2aba77682f2bddaabf11
Mihai Maruseac <mihaimaruseac@google.com>
2020-01-19 17:25:52 -0800

    Pin estimator and tensorboard in `common.sh` instead of in build files

--- tensorflow/tools/ci_build/release/common_win.bat
+++ tensorflow/tools/ci_build/release/common_win.bat
@@ -28,8 +28,8 @@ SET PATH=%PATH%;C:\%PYTHON_DIRECTORY%
 
 %PIP_EXE% install setuptools --upgrade
 %PIP_EXE% install future>=0.17.1 --no-deps
-%PIP_EXE% install tf-estimator-nightly --no-deps
-%PIP_EXE% install tb-nightly --no-deps
+%PIP_EXE% install tf-estimator-nightly==1.14.0.dev2019072801 --no-deps
+%PIP_EXE% install tb-nightly==2.1.0a20190927 --no-deps
 %PIP_EXE% install numpy --upgrade --no-deps
 %PIP_EXE% install opt_einsum --upgrade
 %PIP_EXE% install pandas --upgrade --no-deps
