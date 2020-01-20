c954406a1eaa6c9f1d28134e4229f8aaad636fac
Jeff Daily <jeff.daily@amd.com>
2020-01-20 20:36:09 +0000

    NcclManager sort first by global rank, not device ID

--- tensorflow/core/nccl/nccl_manager.cc
+++ tensorflow/core/nccl/nccl_manager.cc
@@ -250,14 +250,14 @@ string NcclManager::GenerateCommunicatorKey() {
 
 Status NcclManager::GetCommunicator(NcclManager::Collective* collective,
                                     NcclManager::Communicator** communicator) {
-  // Sort by device ID to make ordering of participants deterministic.
+  // Sort by global rank to make ordering of participants deterministic.
   std::sort(collective->participants.begin(), collective->participants.end(),
             [](const std::unique_ptr<Participant>& a,
                const std::unique_ptr<Participant>& b) {
-              if (a->gpu_device_id == b->gpu_device_id) {
+              if (a->global_rank == b->global_rank) {
                 return a->executor < b->executor;
               }
-              return a->gpu_device_id < b->gpu_device_id;
+              return a->global_rank < b->global_rank;
             });
 
   mutex_lock l(mu_);
