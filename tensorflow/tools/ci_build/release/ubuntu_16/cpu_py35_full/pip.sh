090532665648eb8cbdf975ded8c8e60c7b1cc994
Mihai Maruseac <mihaimaruseac@google.com>
2020-01-20 20:03:50 -0800

    Remove `//tensorflow/tools/...` from linux builds.
    These were not tested in 1.15 on the release of 1.15.0, so no need to
    have them green on 1.15.1.

--- tensorflow/tools/ci_build/release/ubuntu_16/cpu_py35_full/pip.sh
+++ tensorflow/tools/ci_build/release/ubuntu_16/cpu_py35_full/pip.sh
@@ -42,7 +42,7 @@ source tensorflow/tools/ci_build/build_scripts/PRESUBMIT_BUILD_TARGETS.sh
 # Export optional variables for running pip.sh
 export TF_BUILD_FLAGS="--config=opt --crosstool_top=//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010-nvcc-cuda10.0:toolchain"
 export TF_TEST_FLAGS="--define=no_tensorflow_py_deps=true --test_lang_filters=py --test_output=errors --verbose_failures=true --keep_going"
-export TF_TEST_TARGETS="${DEFAULT_BAZEL_TARGETS} -//tensorflow/lite/... -//tensorflow/python:contrib_test -//tensorflow/examples/..."
+export TF_TEST_TARGETS="${DEFAULT_BAZEL_TARGETS} -//tensorflow/lite/... -//tensorflow/python:contrib_test -//tensorflow/examples/... -//tensorflow/tools/..."
 export TF_PIP_TESTS="test_pip_virtualenv_non_clean test_pip_virtualenv_clean"
 export TF_TEST_FILTER_TAGS='-no_oss,-oss_serial,-no_oss_py35'
 export IS_NIGHTLY=0 # Not nightly
