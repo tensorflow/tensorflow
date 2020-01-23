4a7069416442a82270494ec5ad26955e3ff85758
Jacques Pienaar <jpienaar@google.com>
2020-01-23 09:56:32 -0800

    Construct InferenceContext with AttrSlice instead of NodeDef
    The entire NodeDef isn't requires to construct InferenceContext as only the
    AttrSlice view of NodeDef is used for anything other than error info (where summary of AttrSlice is used instead which includes the node name if constructed via NodeDef). Change InferenceContext to be constructed with/contain a AttrSlice instead.
    
    PiperOrigin-RevId: 291188012
    Change-Id: I9777b45a56a04fdd5a460a21de6294ea31d3cdcf

--- tensorflow/core/util/padding.h
+++ tensorflow/core/util/padding.h
@@ -22,6 +22,7 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include "tensorflow/core/framework/node_def_util.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/util/tensor_format.h"
 
@@ -60,7 +61,7 @@ string GetPaddingAttrStringWithExplicit();
 string GetExplicitPaddingsAttrString();
 
 // Specialization to parse an attribute directly into a Padding enum.
-Status GetNodeAttr(const NodeDef& node_def, StringPiece attr_name,
+Status GetNodeAttr(const AttrSlice& attrs, StringPiece attr_name,
                    Padding* value);
 
 // Sets padding value based on the given string padding value.
