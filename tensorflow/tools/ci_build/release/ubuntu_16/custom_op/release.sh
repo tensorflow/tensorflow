3ab94ef0f3a2a16d5904426c729d484c7380a621
Yifei Feng <yifeif@google.com>
2020-01-22 17:29:57 -0800

    Add release builds to custom op docker files so that it matches up with TF release.
    Also remove cuda 10.0 from release as now head supports 10.1.
    
    PiperOrigin-RevId: 291067152
    Change-Id: I98fe5411d5aa5378919cfd43ad20a05b5d918c30

---
+++ tensorflow/tools/ci_build/release/ubuntu_16/custom_op/release.sh
@@ -0,0 +1,47 @@
+!/bin/bash
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+set -e
+set -x
+
+if [[ -z "${TF_VERSION}" ]]; then
+  echo "TF_VERSION needs to be set, for example 2.1.0rc0."
+  exit
+fi
+
+UBUNTU16_CPU_IMAGE="tensorflow/tensorflow:custom-op-ubuntu16"
+UBUNTU16_GPU_IMAGE="tensorflow/tensorflow:custom-op-gpu-ubuntu16"
+VERSIONED_UBUNTU16_CPU_IMAGE="tensorflow/tensorflow:${TF_VERSION}-custom-op-ubuntu16"
+VERSIONED_UBUNTU16_GPU_IMAGE="tensorflow/tensorflow:${TF_VERSION}-custom-op-gpu-ubuntu16"
+
+# Build the docker image
+cd tensorflow/tools/ci_build
+docker build --build-arg TF_PACKAGE_VERSION=${TF_VERSION} --no-cache -t "${VERSIONED_UBUNTU16_CPU_IMAGE}" -f Dockerfile.custom_op_ubuntu_16 .
+docker build --build-arg TF_PACKAGE_VERSION=${TF_VERSION} --no-cache -t "${VERSIONED_UBUNTU16_GPU_IMAGE}" -f Dockerfile.custom_op_ubuntu_16_cuda10.1 .
+
+# Log into docker hub, push the image and log out
+docker login -u "${TF_DOCKER_USERNAME}" -p "${TF_DOCKER_PASSWORD}"
+
+docker push "${VERSIONED_UBUNTU16_CPU_IMAGE}"
+docker push "${VERSIONED_UBUNTU16_GPU_IMAGE}"
+
+# Tag and push the default images
+docker tag "${VERSIONED_UBUNTU16_CPU_IMAGE}" "${UBUNTU16_CPU_IMAGE}"
+docker push "${UBUNTU16_CPU_IMAGE}"
+
+docker tag "${VERSIONED_UBUNTU16_GPU_IMAGE}" "${UBUNTU16_GPU_IMAGE}"
+docker push "${UBUNTU16_GPU_IMAGE}"
+
+docker logout#!/bin/bash
