61d81f82697dbb4f119ac7d60cebd10a4e078cbf
Akshay Modi <nareshmodi@google.com>
2020-01-23 13:13:25 -0800

    Allow switching the graph collection from post to pre optimized
    PiperOrigin-RevId: 291229123
    Change-Id: I836811802623ec533e3544340a34bc42e78f6d5b

--- tensorflow/python/eager/context.py
+++ tensorflow/python/eager/context.py
@@ -2039,8 +2039,8 @@ def export_run_metadata():
 
 
 @contextlib.contextmanager
-def collect_optimized_graphs():
-  """Collects a flat list of post-optimization graphs.
+def collect_graphs(optimized=True):
+  """Collects a flat list of pre- or post-optimization graphs.
 
   The collected graphs include device placements, which can be useful for
   testing.
@@ -2052,13 +2052,15 @@ def collect_optimized_graphs():
   def f(x):
     return x + constant_op.constant(1.)
 
-  with context.collect_optimized_graphs() as graphs:
+  with context.collect_graphs() as graphs:
     with ops.device("CPU:0"):
       f(constant_op.constant(1.))
 
   graph, = graphs  # `graph` contains a single GraphDef for inspection
   ```
 
+  Args:
+    optimized: whether to collect optimized graphs or non-optimized graphs
   Yields:
     A list of GraphDefs, populated when the context manager exits.
   """
@@ -2071,7 +2073,10 @@ def collect_optimized_graphs():
   finally:
     ctx.disable_graph_collection()
   for graph in metadata.function_graphs:
-    graphs.append(graph.post_optimization_graph)
+    if optimized:
+      graphs.append(graph.post_optimization_graph)
+    else:
+      graphs.append(graph.pre_optimization_graph)
 
 
 def get_server_def():
