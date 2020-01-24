8fbd517aa3c23b98300bd1970af627c00e4c02b6
nikochiko <kaustubh299792458@gmail.com>
2020-01-24 11:10:33 +0530

    Fix typo

--- tensorflow/python/keras/saving/save.py
+++ tensorflow/python/keras/saving/save.py
@@ -26,7 +26,7 @@ from tensorflow.python.keras.saving import hdf5_format
 from tensorflow.python.keras.saving.saved_model import load as saved_model_load
 from tensorflow.python.keras.saving.saved_model import save as saved_model_save
 from tensorflow.python.saved_model import loader_impl
-from tensroflow.python.util.lazy_loader import LazyLoader
+from tensorflow.python.util.lazy_loader import LazyLoader
 from tensorflow.python.util.tf_export import keras_export
 
 network = LazyLoader(
