07448678cf5cde0bb5da32de4bb5bcc2beb108a9
Benoit Jacob <benoitjacob@google.com>
2020-01-24 14:00:11 -0800

    Add a RUY_OPTIMIZE_FOR_MATMUL_BENCHMARK compile-time control allowing to set the default RUY_OPT_SET to what helps GEMM benchmarks as opposed to the default ruy behavior of doing what helps real applications the most. Unfortunately, some specific optimizations needed for real applications are counterproductive in GEMM benchmarks. In GEMM benchmarking contexts, measuring performance against other libraries more openly optimized for GEMM benchmarks, it makes sense to disable such optimizations that are counterproductive in such settings.
    PiperOrigin-RevId: 291437737
    Change-Id: I248f2142dc0aa37fe05fb37649c63352b34fedc7

--- tensorflow/lite/experimental/ruy/opt_set.h
+++ tensorflow/lite/experimental/ruy/opt_set.h
@@ -21,11 +21,6 @@ limitations under the License.
 // compiler command line.
 //
 // Each bit in RUY_OPT_SET controls a particular optimization done in Ruy.
-#if !defined(RUY_OPT_SET)
-// Default to all optimizations.
-#define RUY_OPT_SET 0x7ff
-#endif
-
 #define RUY_OPT_INTRINSICS 0x1
 #define RUY_OPT_ASM 0x2
 #define RUY_OPT_TUNING 0x4
@@ -37,6 +32,15 @@ limitations under the License.
 #define RUY_OPT_MAX_STREAMING 0x100
 #define RUY_OPT_PREFETCH 0x200
 #define RUY_OPT_PACK_AHEAD 0x400
+
+#if !defined(RUY_OPT_SET)
+#ifdef RUY_OPTIMIZE_FOR_MATMUL_BENCHMARK
+#define RUY_OPT_SET (~RUY_OPT_PREFETCH)
+#else
+// Default to all optimizations.
+#define RUY_OPT_SET (~0)
+#endif
+#endif
 
 #define RUY_OPT_ENABLED(ruy_opt) ((RUY_OPT_SET & ruy_opt) != 0)
 
