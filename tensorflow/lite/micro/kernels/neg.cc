62c88a74a00a8c23b37ecb588d6a3a28e9d54f57
Lamar <rlamarrr@gmail.com>
2020-01-24 01:47:53 +0100

    Added Zero Point Sum Computation to TfLite Micro's Eval

--- tensorflow/lite/micro/kernels/neg.cc
+++ tensorflow/lite/micro/kernels/neg.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/lite/kernels/internal/reference/neg.h"
+
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/reference/integer_ops/neg.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
@@ -26,6 +27,41 @@ namespace neg {
 
 constexpr int kInputTensor = 0;
 constexpr int kOutputTensor = 0;
+
+struct OpDataInt8 {
+  // sum of input and output zero points, accumulated on int16
+  // highest possible value: 127 + 127 = 254
+  // lowest possible value: -128 + - 128 = -256
+  // thus, accumulate on int16
+  int16_t zero_points_sum;
+};
+
+TfLiteStatus CalculateOpDataInt8(TfLiteContext* context,
+                                 const TfLiteTensor* input,
+                                 const TfLiteTensor* output, OpDataInt8* data) {
+  constexpr auto kI8Min =
+      static_cast<int16_t>(std::numeric_limits<int8_t>::min());
+  constexpr auto kI8Max =
+      static_cast<int16_t>(std::numeric_limits<int8_t>::max());
+
+  // using EQ attempts to cast to int via the %d format specifier and gives
+  // incorrect value and since some hardware platforms do not support float
+  // formatting by default, I did a direct equality check instead
+  TF_LITE_ENSURE(context, input->params.scale == output->params.scale);
+
+  // within: [-128, 127]
+  TF_LITE_ENSURE(context, input->params.zero_point <= kI8Max);
+  TF_LITE_ENSURE(context, input->params.zero_point >= kI8Min);
+
+  // within: [-128, 127]
+  TF_LITE_ENSURE(context, output->params.zero_point <= kI8Max);
+  TF_LITE_ENSURE(context, output->params.zero_point >= kI8Min);
+
+  data->zero_points_sum = static_cast<int16_t>(input->params.zero_point) +
+                          static_cast<int16_t>(output->params.zero_point);
+
+  return kTfLiteOk;
+}
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   const TfLiteTensor* input = GetInput(context, node, kInputTensor);
@@ -44,16 +80,15 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   const TfLiteTensor* input = GetInput(context, node, kInputTensor);
   TfLiteTensor* output = GetOutput(context, node, kOutputTensor);
   switch (input->type) {
-    case kTfLiteInt8:
-      // using EQ attempts to cast to int via the %d format specifier and gives
-      // incorrect value and since some hardware platforms do not support float
-      // formatting by default, I did a direct equality check instead
-      TF_LITE_ENSURE(context, input->params.scale == output->params.scale);
+    case kTfLiteInt8: {
+      OpDataInt8 op_data;
+      TF_LITE_ENSURE_STATUS(
+          CalculateOpDataInt8(context, input, output, &op_data));
       reference_integer_ops::Negate(
           GetTensorShape(input), GetTensorData<int8_t>(input),
-          input->params.zero_point, GetTensorShape(output),
-          GetTensorData<int8_t>(output), output->params.zero_point);
-      break;
+          GetTensorShape(output), GetTensorData<int8_t>(output),
+          op_data.zero_points_sum);
+    } break;
 
     case kTfLiteFloat32:
       reference_ops::Negate(GetTensorShape(input), GetTensorData<float>(input),
