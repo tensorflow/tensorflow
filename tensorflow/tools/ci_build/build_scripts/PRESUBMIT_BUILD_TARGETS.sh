5ffb02b3fbf61e8c43629f193fc25618cb348146
Anna R <annarev@google.com>
2020-01-23 18:42:12 -0800

    Enable OSS testing for XLA.
    PiperOrigin-RevId: 291290360
    Change-Id: Ieb7d895027a9047415ff7544a4f077877003319a

--- tensorflow/tools/ci_build/build_scripts/PRESUBMIT_BUILD_TARGETS.sh
+++ tensorflow/tools/ci_build/build_scripts/PRESUBMIT_BUILD_TARGETS.sh
@@ -15,4 +15,4 @@
 #!/bin/bash
 set -x
 
-DEFAULT_BAZEL_TARGETS="//tensorflow/...  //tensorflow/compiler/xla/python/tpu_driver/... -//tensorflow/compiler/tests/..."
+DEFAULT_BAZEL_TARGETS="//tensorflow/... -//tensorflow/compiler/... //tensorflow/compiler/xla/python/tpu_driver/..."
