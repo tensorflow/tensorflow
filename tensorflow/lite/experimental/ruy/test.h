04394e5c1fabdd4cd3eafa1fca075a0228ee0ef7
Benoit Jacob <benoitjacob@google.com>
2020-01-24 14:18:38 -0800

    Fix PMU-querying code to properly count child threads. There were 2 issues:  (1) We needed to set the 'inherit' flag.  (2) That only counts child threads created after perf_event_open. The only way to make this work consistently was thus to ensure that we call perf_event_open once, for each event to be counted, at the time that we create the ruy Context, before we create any child thread.
    PiperOrigin-RevId: 291441723
    Change-Id: I87633463762b8029df4930f98c9c84f0fc173df5

--- tensorflow/lite/experimental/ruy/test.h
+++ tensorflow/lite/experimental/ruy/test.h
@@ -548,7 +548,17 @@ struct TestSet final {
   bool benchmark_prepack_rhs = false;
 };
 
+inline PmuEvents& GlobalPmuEvents() {
+  static PmuEvents pmu;
+  return pmu;
+}
+
 inline Context& GlobalContext() {
+  // Ensure that GlobalPmuEvents is constructed before we create any context.
+  // This ensures that pmu counters are opened before we create any worker
+  // thread, which is necessary to count events from worker threads.
+  GlobalPmuEvents();
+
   static Context context;
   return context;
 }
@@ -1867,7 +1877,7 @@ void TestSet<LhsScalar, RhsScalar, SpecType>::Benchmark(
   float backend_stall_rate = std::numeric_limits<float>::infinity();
 
   for (int repeat = 0; repeat < repeats; repeat++) {
-    PmuEvents pmu_events;
+    auto& pmu_events = GlobalPmuEvents();
     if (record_pmu) {
       pmu_events.StartRecording();
     }
