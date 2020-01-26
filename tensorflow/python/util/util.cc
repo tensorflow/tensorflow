acf7733f2c4fbaf7773ec50ecdb68a2030d5baf8
Puneeth K <puneethk.2899@gmail.com>
2020-01-26 12:27:08 +0530

    Change IsMappingHelper to IsNestCompatibleMappingHelper

--- tensorflow/python/util/util.cc
+++ tensorflow/python/util/util.cc
@@ -213,9 +213,9 @@ int IsInstanceOfRegisteredType(PyObject* obj, const char* type_name) {
 // Returns 1 if `o` is considered a mapping for the purposes of Flatten().
 // Returns 0 otherwise.
 // Returns -1 if an error occurred.
-int IsMappingHelper(PyObject* o) {
+int IsNestCompatibleMappingHelper(PyObject* o) {
   static auto* const check_cache = new CachedTypeCheck([](PyObject* to_check) {
-    return IsInstanceOfRegisteredType(to_check, "Mapping");
+    return IsInstanceOfRegisteredType(to_check, "MutableMapping");
   });
   if (PyDict_Check(o)) return true;
   return check_cache->CachedLookup(o);
