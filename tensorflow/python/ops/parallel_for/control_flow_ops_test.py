dd5e3ca703ef9c38ec73bccda1936bea9569fc75
Rasul Karimov <yoatom96@hotmail.com>
2020-01-27 20:19:54 +0300

    fix test

--- tensorflow/python/ops/parallel_for/control_flow_ops_test.py
+++ tensorflow/python/ops/parallel_for/control_flow_ops_test.py
@@ -539,7 +539,7 @@ class NNTest(PForTestCase):
     def loop_fn(i):
       logits_i = array_ops.gather(logits, i)
       labels_i = array_ops.gather(labels, i)
-      loss = nn.softmax_cross_entropy_with_logits(
+      loss = nn.sparse_softmax_cross_entropy_with_logits(
         labels=labels_i, logits=logits_i)
       total_loss = math_ops.reduce_sum(loss)
       return loss
