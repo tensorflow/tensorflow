afce0531ead9c26e1d64a214498b1c70b5b5db14
Keith Moulton <keithm@xmos.com>
2020-01-27 14:39:48 -0500

    no longer calling conv2d_boggle_shift_scale

--- tensorflow/lite/micro/kernels/xcore/xcore_conv.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_conv.cc
@@ -8,22 +8,23 @@ namespace micro {
 namespace xcore {
 namespace conv {
 
-    static constexpr int unpadded_shape_array_len = 4;
-
-    typedef struct {
-        padding_mode_t padding_mode;
-        int32_t unpadded_shape[unpadded_shape_array_len]; // values represent [C_out, K_h, K_w, C_in]
-        nn_conv2d_sido_params_t kernel_params;
-    } SIDO_UserData;
+    typedef union ParamsPtrUnion {
+        nn_conv2d_sido_params_t *sido;
+        nn_conv2d_dido_params_t *dido;
+    } ParamsUnion;
 
 
     typedef struct {
         padding_mode_t padding_mode;
-        nn_conv2d_dido_params_t kernel_params;
-    } DIDO_UserData;
+        int32_t C_in;
+        int32_t C_out;
+        int32_t K_h;
+        int32_t K_w;
+        ParamsPtrUnion params;
+    } UserData;
 
 
-    static void parse_sido_options(const char* buffer, size_t length, SIDO_UserData *data)
+    static void parse_options(const char* buffer, size_t length, UserData *data)
     {
         const uint8_t* buffer_t = reinterpret_cast<const uint8_t*>(buffer);
         // std::cout << flexbuffers::GetRoot(buffer_t, length).ToString() << std::endl;
@@ -31,7 +32,7 @@ namespace conv {
 
         auto keys = map.Keys();
         auto values = map.Values();
-        for (int i=0; i<map.size(); ++i)
+        for (int i = 0; i < map.size(); ++i)
         {
             std::string key(keys[i].ToString());
             
@@ -45,35 +46,15 @@ namespace conv {
             }
             else if (key.compare("unpadded_shape") == 0)
             {
-                auto vec = values[i].AsVector();
-                for (int j=0; j<unpadded_shape_array_len; ++j)
-                    data->unpadded_shape[j] = vec[j].AsInt32();
+                auto vec = values[i].AsVector(); // values represent [C_out, K_h, K_w, C_in]
+                data->C_out = vec[0].AsInt32();
+                data->K_h = vec[1].AsInt32();
+                data->K_w = vec[2].AsInt32();
+                data->C_in = vec[3].AsInt32();
             }
         }
     }
 
-
-    static void parse_dido_options(const char* buffer, size_t length, DIDO_UserData *data)
-    {
-        const uint8_t* buffer_t = reinterpret_cast<const uint8_t*>(buffer);
-        auto map = flexbuffers::GetRoot(buffer_t, length).AsMap();
-
-        auto keys = map.Keys();
-        auto values = map.Values();
-        for (int i=0; i<map.size(); ++i)
-        {
-            std::string key(keys[i].ToString());
-            
-            if (key.compare("padding") == 0)
-            {
-                std::string padding_mode_str = values[i].ToString();
-                if (padding_mode_str.compare("VALID") == 0) 
-                    data->padding_mode = PADDING_VALID;
-                else 
-                    data->padding_mode = PADDING_SAME;
-            }
-        }
-    }
 
     //**************************************
     //**************************************
@@ -84,19 +65,19 @@ namespace conv {
     //**************************************
     void* Init_SIDO(TfLiteContext* context, const char* buffer, size_t length) 
     {
-        auto* user_data = new SIDO_UserData();
+        auto* user_data = new UserData();
+        user_data->params.sido = new nn_conv2d_sido_params_t;
 
         if (buffer)
-            parse_sido_options(buffer, length, user_data);
+            parse_options(buffer, length, user_data);
 
         return user_data;
     }
 
     void Free_SIDO(TfLiteContext* context, void* buffer) {
-        auto* user_data = reinterpret_cast<SIDO_UserData*>(buffer);
-        nn_conv2d_sido_params_t* kernel_params = (nn_conv2d_sido_params_t*) &user_data->kernel_params;
+        auto* user_data = reinterpret_cast<UserData*>(buffer);
 
-        conv2d_shallowin_deepout_deinit(kernel_params);
+        conv2d_shallowin_deepout_deinit(user_data->params.sido);
         delete user_data;
     }
 
@@ -110,13 +91,12 @@ namespace conv {
         const TfLiteTensor* biases = GetInput(context, node, 2);
         const TfLiteTensor* output = GetOutput(context, node, 0);
 
-        auto* user_data = reinterpret_cast<SIDO_UserData*>(node->user_data);
-        nn_conv2d_sido_params_t* kernel_params = (nn_conv2d_sido_params_t*) &user_data->kernel_params;
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
 
-        if (kernel_params)
+        if (user_data->params.sido)
         {
             // need to deinit first because something significant has changes with the operator
-            conv2d_shallowin_deepout_deinit(kernel_params);
+            conv2d_shallowin_deepout_deinit(user_data->params.sido);
         }
 
         nn_conv2d_init_params_t init_params;
@@ -124,10 +104,10 @@ namespace conv {
 
         init_params.X_height = input->dims->data[1];
         init_params.X_width = input->dims->data[2];
-        init_params.K_h = user_data->unpadded_shape[1];
-        init_params.K_w = user_data->unpadded_shape[2];
-        init_params.C_in = input->dims->data[3];
-        init_params.C_out = user_data->unpadded_shape[0];
+        init_params.K_h = user_data->K_h;
+        init_params.K_w = user_data->K_w;
+        init_params.C_in = input->dims->data[3]; // number of channels after padding
+        init_params.C_out = user_data->C_out;
         init_params.pad_mode = user_data->padding_mode;
         init_params.zero_point = input->params.zero_point;
 
@@ -139,7 +119,7 @@ namespace conv {
         // NOTE: There is a comment in common.h about TfLiteQuantization quantization being added to 
         //         TfLiteTensor to replace TfLiteQuantizationParams params
         conv2d_shallowin_deepout_init(
-            kernel_params,
+            user_data->params.sido,
             &init_params,
             &region_params,
             weights->data.int8,
@@ -156,18 +136,11 @@ namespace conv {
         const TfLiteTensor* shift_scale = GetInput(context, node, 3);
         TfLiteTensor* output = GetOutput(context, node, 0);
 
-        auto* user_data = reinterpret_cast<SIDO_UserData*>(node->user_data);
-        const nn_conv2d_sido_params_t *kernel_params = &user_data->kernel_params;
-        unsigned block_count = kernel_params->block_count;
-        int32_t C_out = user_data->unpadded_shape[0];
-
-        // this call is only necessary util the shifts & scales are merged
-        //   by tflite2xcore
-        conv2d_boggle_shift_scale((int16_t*) shift_scale->data.i16, C_out, nullptr);
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
 
         conv2d_shallowin_deepout(
             output->data.int8, // Y
-            kernel_params,
+            user_data->params.sido,
             input->data.int8, // X,
             weights->data.int8, // K
             (int16_t*) shift_scale->data.i16 // shifts & scales
@@ -186,18 +159,19 @@ namespace conv {
 
     void* Init_DIDO(TfLiteContext* context, const char* buffer, size_t length) 
     {
-        auto* user_data = new DIDO_UserData();
+        auto* user_data = new UserData();
+        user_data->params.dido = new nn_conv2d_dido_params_t;
+
         if (buffer)
-            parse_dido_options(buffer, length, user_data);
+            parse_options(buffer, length, user_data);
 
         return user_data;
     }
 
     void Free_DIDO(TfLiteContext* context, void* buffer) {
-        auto* user_data = reinterpret_cast<DIDO_UserData*>(buffer);
-        nn_conv2d_dido_params_t* kernel_params = (nn_conv2d_dido_params_t*) &user_data->kernel_params;
+        auto* user_data = reinterpret_cast<UserData*>(buffer);
 
-        conv2d_deepin_deepout_deinit(kernel_params);
+        conv2d_deepin_deepout_deinit(user_data->params.dido);
         delete user_data;
     }
 
@@ -210,13 +184,18 @@ namespace conv {
         const TfLiteTensor* biases = GetInput(context, node, 2);
         const TfLiteTensor* output = GetOutput(context, node, 0);
 
-        auto* user_data = reinterpret_cast<DIDO_UserData*>(node->user_data);
-        nn_conv2d_dido_params_t* kernel_params = (nn_conv2d_dido_params_t*) &user_data->kernel_params;
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
 
-        if (kernel_params)
+        // set param values not parsed from custom options
+        user_data->K_h = weights->dims->data[1];
+        user_data->K_w = weights->dims->data[2];
+        user_data->C_in = weights->dims->data[3] * weights->dims->data[5];
+        user_data->C_out = weights->dims->data[0] * weights->dims->data[4];
+
+        if (user_data->params.dido)
         {
             // need to deinit first because something significant has changes with the operator
-            conv2d_deepin_deepout_deinit(kernel_params);
+            conv2d_deepin_deepout_deinit(user_data->params.dido);
         }
 
         nn_conv2d_init_params_t init_params;
@@ -224,10 +203,10 @@ namespace conv {
 
         init_params.X_height = input->dims->data[1];
         init_params.X_width = input->dims->data[2];
-        init_params.K_h = weights->dims->data[1];
-        init_params.K_w = weights->dims->data[2];
-        init_params.C_in = weights->dims->data[3] * weights->dims->data[5];
-        init_params.C_out = weights->dims->data[0] * weights->dims->data[4];
+        init_params.K_h = user_data->K_h;
+        init_params.K_w = user_data->K_w;
+        init_params.C_in = user_data->C_in;
+        init_params.C_out = user_data->C_out;
         init_params.pad_mode = user_data->padding_mode;
         init_params.zero_point = input->params.zero_point;
 
@@ -239,7 +218,7 @@ namespace conv {
         // NOTE: There is a comment in common.h about TfLiteQuantization quantization being added to 
         //         TfLiteTensor to replace TfLiteQuantizationParams params
         conv2d_deepin_deepout_init(
-            kernel_params,
+            user_data->params.dido,
             &init_params,
             &region_params,
             weights->data.int8,
@@ -255,18 +234,11 @@ namespace conv {
         const TfLiteTensor* shift_scale = GetInput(context, node, 3);
         TfLiteTensor* output = GetOutput(context, node, 0);
 
-        auto* user_data = reinterpret_cast<DIDO_UserData*>(node->user_data);
-        const nn_conv2d_dido_params_t *kernel_params = &user_data->kernel_params;
-        unsigned block_count = kernel_params->block_count;
-        int32_t C_out = weights->dims->data[0] * weights->dims->data[4];
-
-        // this call is only necessary util the shifts & scales are merged
-        //   by tflite2xcore
-        conv2d_boggle_shift_scale((int16_t*) shift_scale->data.i16, C_out, nullptr);
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
 
         conv2d_deepin_deepout(
             output->data.int8, // Y
-            kernel_params,
+            user_data->params.dido,
             input->data.int8, // X,
             weights->data.int8, // K
             (int16_t*) shift_scale->data.i16 // shifts & scales
