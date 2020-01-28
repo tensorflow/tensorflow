48a0b665b831c30e4bdf8c2ddfd70ad09b95d302
T.J. Alumbaugh <talumbau@google.com>
2020-01-27 17:07:47 -0800

    Fix build of neon_tensor_utils for android_x86
    PiperOrigin-RevId: 291833076
    Change-Id: I93398a8f46101708d1d010573f1216473fbdee16

--- tensorflow/lite/kernels/internal/reference/portable_tensor_utils_impl.h
+++ tensorflow/lite/kernels/internal/reference/portable_tensor_utils_impl.h
@@ -69,6 +69,12 @@ void PortableMatrixBatchVectorMultiplyAccumulate(
 
 void PortableMatrixBatchVectorMultiplyAccumulate(
     const int8_t* __restrict__ matrix, const int m_rows, const int m_cols,
+    const int8_t* __restrict__ vector, const float* scaling_factors,
+    int n_batch, int32_t* scratch, float* __restrict__ result,
+    int result_stride, CpuBackendContext* context);
+
+void PortableMatrixBatchVectorMultiplyAccumulate(
+    const int8_t* __restrict__ matrix, const int m_rows, const int m_cols,
     const int8_t* __restrict__ vectors, const float* scaling_factors,
     int n_batch, float* __restrict__ result, int result_stride,
     const float* per_channel_scale, const int32_t* input_offset);
