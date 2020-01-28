1e88b0758519d9e3537b5def8baa1b5d20487047
Scott Zhu <scottzhu@google.com>
2020-01-28 10:08:03 -0800

    Adding the enable_caching_device to the JSON config of the RNN layer.
    PiperOrigin-RevId: 291957731
    Change-Id: Ieb7175aa67387c21b8f50d1593ba0fa108a50ed6

--- tensorflow/python/keras/layers/recurrent.py
+++ tensorflow/python/keras/layers/recurrent.py
@@ -1381,6 +1381,7 @@ class SimpleRNNCell(DropoutRNNCellMixin, Layer):
         'recurrent_dropout':
             self.recurrent_dropout
     }
+    config.update(_config_for_enable_caching_device(self))
     base_config = super(SimpleRNNCell, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
 
@@ -1498,6 +1499,11 @@ class SimpleRNN(RNN):
       logging.warning('The `implementation` argument '
                       'in `SimpleRNN` has been deprecated. '
                       'Please remove it from your layer call.')
+    if 'enable_caching_device' in kwargs:
+      cell_kwargs = {'enable_caching_device':
+                     kwargs.pop('enable_caching_device')}
+    else:
+      cell_kwargs = {}
     cell = SimpleRNNCell(
         units,
         activation=activation,
@@ -1514,7 +1520,8 @@ class SimpleRNN(RNN):
         dropout=dropout,
         recurrent_dropout=recurrent_dropout,
         dtype=kwargs.get('dtype'),
-        trainable=kwargs.get('trainable', True))
+        trainable=kwargs.get('trainable', True),
+        **cell_kwargs)
     super(SimpleRNN, self).__init__(
         cell,
         return_sequences=return_sequences,
@@ -1621,6 +1628,7 @@ class SimpleRNN(RNN):
             self.recurrent_dropout
     }
     base_config = super(SimpleRNN, self).get_config()
+    config.update(_config_for_enable_caching_device(self.cell))
     del base_config['cell']
     return dict(list(base_config.items()) + list(config.items()))
 
@@ -1902,6 +1910,7 @@ class GRUCell(DropoutRNNCellMixin, Layer):
         'implementation': self.implementation,
         'reset_after': self.reset_after
     }
+    config.update(_config_for_enable_caching_device(self))
     base_config = super(GRUCell, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
 
@@ -2032,6 +2041,11 @@ class GRU(RNN):
       logging.warning('`implementation=0` has been deprecated, '
                       'and now defaults to `implementation=1`.'
                       'Please update your layer call.')
+    if 'enable_caching_device' in kwargs:
+      cell_kwargs = {'enable_caching_device':
+                     kwargs.pop('enable_caching_device')}
+    else:
+      cell_kwargs = {}
     cell = GRUCell(
         units,
         activation=activation,
@@ -2051,7 +2065,8 @@ class GRU(RNN):
         implementation=implementation,
         reset_after=reset_after,
         dtype=kwargs.get('dtype'),
-        trainable=kwargs.get('trainable', True))
+        trainable=kwargs.get('trainable', True),
+        **cell_kwargs)
     super(GRU, self).__init__(
         cell,
         return_sequences=return_sequences,
@@ -2175,6 +2190,7 @@ class GRU(RNN):
         'reset_after':
             self.reset_after
     }
+    config.update(_config_for_enable_caching_device(self.cell))
     base_config = super(GRU, self).get_config()
     del base_config['cell']
     return dict(list(base_config.items()) + list(config.items()))
@@ -2464,6 +2480,7 @@ class LSTMCell(DropoutRNNCellMixin, Layer):
         'implementation':
             self.implementation
     }
+    config.update(_config_for_enable_caching_device(self))
     base_config = super(LSTMCell, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
 
@@ -2670,6 +2687,11 @@ class LSTM(RNN):
       logging.warning('`implementation=0` has been deprecated, '
                       'and now defaults to `implementation=1`.'
                       'Please update your layer call.')
+    if 'enable_caching_device' in kwargs:
+      cell_kwargs = {'enable_caching_device':
+                     kwargs.pop('enable_caching_device')}
+    else:
+      cell_kwargs = {}
     cell = LSTMCell(
         units,
         activation=activation,
@@ -2689,7 +2711,8 @@ class LSTM(RNN):
         recurrent_dropout=recurrent_dropout,
         implementation=implementation,
         dtype=kwargs.get('dtype'),
-        trainable=kwargs.get('trainable', True))
+        trainable=kwargs.get('trainable', True),
+        **cell_kwargs)
     super(LSTM, self).__init__(
         cell,
         return_sequences=return_sequences,
@@ -2813,6 +2836,7 @@ class LSTM(RNN):
         'implementation':
             self.implementation
     }
+    config.update(_config_for_enable_caching_device(self.cell))
     base_config = super(LSTM, self).get_config()
     del base_config['cell']
     return dict(list(base_config.items()) + list(config.items()))
@@ -2973,3 +2997,23 @@ def _caching_device(rnn_cell):
     return None
   # Cache the value on the device that access the variable.
   return lambda op: op.device
+
+
+def _config_for_enable_caching_device(rnn_cell):
+  """Return the dict config for RNN cell wrt to enable_caching_device field.
+
+  Since enable_caching_device is a internal implementation detail for speed up
+  the RNN variable read when running on the multi remote worker setting, we
+  don't want this config to be serialized constantly in the JSON. We will only
+  serialize this field when a none default value is used to create the cell.
+  Args:
+    rnn_cell: the RNN cell for serialize.
+
+  Returns:
+    A dict which contains the JSON config for enable_caching_device value or
+    empty dict if the enable_caching_device value is same as the default value.
+  """
+  default_enable_caching_device = ops.executing_eagerly_outside_functions()
+  if rnn_cell._enable_caching_device != default_enable_caching_device:
+    return {'enable_caching_device': rnn_cell._enable_caching_device}
+  return {}
