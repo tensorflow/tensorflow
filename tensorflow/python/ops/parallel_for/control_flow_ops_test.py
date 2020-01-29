414d61699b5a8bcae21d87946647bfa0dc427ce6
Rasul Karimov <yoatom96@hotmail.com>
2020-01-29 04:26:18 +0300

    fix pylint (2)

--- tensorflow/python/ops/parallel_for/control_flow_ops_test.py
+++ tensorflow/python/ops/parallel_for/control_flow_ops_test.py
@@ -534,13 +534,14 @@ class NNTest(PForTestCase):
 
   def test_sparse_softmax_cross_entropy_with_logits(self):
     logits = random_ops.random_uniform([3, 2, 4])
-    labels = random_ops.random_uniform(shape=[3, 2], maxval=4, dtype=dtypes.int32)
+    labels = random_ops.random_uniform(shape=[3, 2], maxval=4,
+                                       dtype=dtypes.int32)
 
     def loop_fn(i):
       logits_i = array_ops.gather(logits, i)
       labels_i = array_ops.gather(labels, i)
       loss = nn.sparse_softmax_cross_entropy_with_logits(
-        labels=labels_i, logits=logits_i)
+          labels=labels_i, logits=logits_i)
       return loss
 
     self._test_loop_fn(loop_fn, 3)
