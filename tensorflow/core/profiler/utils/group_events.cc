809e32ddf4c141897fad9b186badcf49216aca08
Jiho Choi <jihochoi@google.com>
2020-01-30 19:34:19 -0800

    Automated rollback of commit 484d9e71e38a1f71d03bd7afa95464a2c94d02df
    PiperOrigin-RevId: 292471780
    Change-Id: I94aa254da0e50fc85719acbf3ba67fdaa643afa3

--- tensorflow/core/profiler/utils/group_events.cc
+++ tensorflow/core/profiler/utils/group_events.cc
@@ -126,9 +126,9 @@ bool EventNode::IsNestedIn(EventNode* parent) {
   return parent && IsNested(GetEvent(), parent->GetEvent());
 }
 
-void ConnectIntraThread(const XPlaneVisitor& visitor, XPlane* host_trace,
+void ConnectIntraThread(const XPlaneVisitor& visitor, XPlane* plane,
                         EventNodeMap* event_node_map) {
-  for (auto& line : *host_trace->mutable_lines()) {
+  for (auto& line : *plane->mutable_lines()) {
     std::vector<EventNode*> parent_nodes;
     for (auto& event : *line.mutable_events()) {
       auto cur_node = absl::make_unique<EventNode>(&visitor, &event);
@@ -203,11 +203,13 @@ void CreateEventGroup(const std::vector<int64 /*EventType*/>& root_event_types,
         if (root_event_node->GetGroupId()) continue;
         int64 group_id = next_group_id++;
         root_event_node->PropagateGroupId(group_id);
-        (*event_group_name_map)[group_id] = root_event_node->GetGroupName();
-        // Add step_name stat if it is a TraceContext event.
-        // TODO(jihochoi): change event name instead.
-        if (root_event_type == HostEventType::kTraceContext) {
-          root_event_node->AddStepName((*event_group_name_map)[group_id]);
+        if (event_group_name_map) {
+          (*event_group_name_map)[group_id] = root_event_node->GetGroupName();
+          // Add step_name stat if it is a TraceContext event.
+          // TODO(jihochoi): change event name instead.
+          if (root_event_type == HostEventType::kTraceContext) {
+            root_event_node->AddStepName((*event_group_name_map)[group_id]);
+          }
         }
       }
     }
@@ -215,27 +217,20 @@ void CreateEventGroup(const std::vector<int64 /*EventType*/>& root_event_types,
 }
 
 void GroupEvents(const std::vector<InterThreadConnectInfo>& connect_info_list,
-                 const std::vector<int64>& root_event_types, XPlane* host_trace,
-                 const std::vector<XPlane*>& device_traces,
+                 const std::vector<int64>& root_event_types, XSpace* space,
                  EventGroupNameMap* event_group_name_map) {
-  if (!host_trace) return;
   EventNodeMap event_node_map;
-  XPlaneVisitor host_plane_visitor = CreateTfXPlaneVisitor(host_trace);
-  ConnectIntraThread(host_plane_visitor, host_trace, &event_node_map);
-  std::vector<XPlaneVisitor> device_plane_visitors;
-  device_plane_visitors.reserve(device_traces.size());
-  for (XPlane* device_trace : device_traces) {
-    device_plane_visitors.push_back(CreateTfXPlaneVisitor(device_trace));
-    ConnectIntraThread(device_plane_visitors.back(), device_trace,
-                       &event_node_map);
+  std::vector<XPlaneVisitor> visitors;
+  visitors.reserve(space->planes_size());
+  for (auto& plane : *space->mutable_planes()) {
+    visitors.push_back(CreateTfXPlaneVisitor(&plane));
+    ConnectIntraThread(visitors.back(), &plane, &event_node_map);
   }
   ConnectInterThread(event_node_map, connect_info_list);
   CreateEventGroup(root_event_types, event_node_map, event_group_name_map);
 }
 
-void GroupTfEvents(XPlane* host_trace,
-                   const std::vector<XPlane*>& device_traces,
-                   EventGroupNameMap* event_group_name_map) {
+void GroupTfEvents(XSpace* space, EventGroupNameMap* event_group_name_map) {
   std::vector<InterThreadConnectInfo> connect_info_list(
       {{HostEventType::kFunctionRun,
         HostEventType::kExecutorStateProcess,
@@ -249,8 +244,7 @@ void GroupTfEvents(XPlane* host_trace,
   const std::vector<int64 /*EventType*/> root_event_types(
       {HostEventType::kTraceContext, HostEventType::kFunctionRun,
        HostEventType::kSessionRun});
-  GroupEvents(connect_info_list, root_event_types, host_trace, device_traces,
-              event_group_name_map);
+  GroupEvents(connect_info_list, root_event_types, space, event_group_name_map);
 }
 
 }  // namespace profiler
