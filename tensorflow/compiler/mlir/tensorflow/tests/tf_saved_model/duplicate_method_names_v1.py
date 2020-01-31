ac5f09fc2b29dcbc5b3ec5a403b3a6d53757a542
Michael Gester <mgester@google.com>
2020-01-31 11:14:05 -0800

    Automated rollback of commit 49e6878a436f7a5e629b9ee04d15fac53a5ca4b8
    PiperOrigin-RevId: 292580159
    Change-Id: Iea26ede1883139b3db8d7a2b09f768664a100cd9

--- tensorflow/compiler/mlir/tensorflow/tests/tf_saved_model/duplicate_method_names_v1.py
+++ tensorflow/compiler/mlir/tensorflow/tests/tf_saved_model/duplicate_method_names_v1.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-# RUN: %p/shared_variable_v1 | FileCheck %s
+# RUN: %p/duplicate_method_names_v1 | FileCheck %s
 
 # pylint: disable=missing-docstring,line-too-long
 from __future__ import absolute_import
@@ -23,42 +23,32 @@ from __future__ import print_function
 import tensorflow.compat.v1 as tf
 from tensorflow.compiler.mlir.tensorflow.tests.tf_saved_model import common_v1
 
-# CHECK: "tf_saved_model.global_tensor"() {is_mutable, sym_name = "[[VAR:[a-zA-Z_0-9]+]]", type = tensor<1x3xf32>, value = {{.*}} : tensor<1x3xf32>} : () -> ()
+# Tests different SignatureDef's with identical method_name string
 
 # CHECK:      func {{@[a-zA-Z_0-9]+}}(
-# CHECK-SAME:   [[ARG0:%.*]]: tensor<3x1xf32> {tf_saved_model.index_path = ["x"]},
-# CHECK-SAME:   [[ARG1:%.*]]: tensor<!tf.resource<tensor<1x3xf32>>> {tf_saved_model.bound_input = @[[VAR]]})
-# CHECK-SAME:             -> (tensor<3x3xf32> {tf_saved_model.index_path = ["r"]})
+# CHECK-SAME:   {{.*}})
 # CHECK-SAME: attributes {{.*}} tf_saved_model.exported_names = ["key"]
 
 # CHECK:      func {{@[a-zA-Z_0-9]+}}(
-# CHECK-SAME:   [[ARG2:%.*]]: tensor<3x1xf32> {tf_saved_model.index_path = ["x"]},
-# CHECK-SAME:   [[ARG3:%.*]]: tensor<!tf.resource<tensor<1x3xf32>>> {tf_saved_model.bound_input = @[[VAR]]})
-# CHECK-SAME:             -> (tensor<3x3xf32> {tf_saved_model.index_path = ["r"]})
+# CHECK-SAME:   {{.*}})
 # CHECK-SAME: attributes {{.*}} tf_saved_model.exported_names = ["key2"]
 
 
 def Test():
 
-  x = tf.constant([[1.0], [1.0], [1.0]])
-  y = tf.get_variable(
-      name='y',
-      shape=(1, 3),
-      initializer=tf.random_normal_initializer(),
-      trainable=True)
-  r = tf.matmul(x, y)
+  x = tf.constant(1.0, shape=(3, 3))
+  y = tf.constant(1.0, shape=(3, 3))
 
-  tensor_info_x = tf.saved_model.utils.build_tensor_info(x)
-  tensor_info_r = tf.saved_model.utils.build_tensor_info(r)
+  s = tf.transpose(x)
+  t = tf.transpose(y)
+
+  tensor_info_s = tf.compat.v1.saved_model.utils.build_tensor_info(s)
+  tensor_info_t = tf.compat.v1.saved_model.utils.build_tensor_info(t)
 
   signature_def = tf.saved_model.signature_def_utils.build_signature_def(
-      inputs={'x': tensor_info_x},
-      outputs={'r': tensor_info_r},
-      method_name='some_function')
+      inputs=None, outputs={'s': tensor_info_s}, method_name='some_function')
   signature_def2 = tf.saved_model.signature_def_utils.build_signature_def(
-      inputs={'x': tensor_info_x},
-      outputs={'r': tensor_info_r},
-      method_name='some_other_function')
+      inputs=None, outputs={'t': tensor_info_t}, method_name='some_function')
 
   # Create two signatures that share the same variable.
   return {'key': signature_def, 'key2': signature_def2}
