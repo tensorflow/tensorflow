2d663113d923de31e02e9d30db926f68a39a13e1
Edward Loper <edloper@google.com>
2020-02-05 19:46:37 -0800

    Update `tf.embedding_lookup` to use `partition_strategy` and `max_norm` when `ids` is ragged.  (Prior to this change, these two parameters were ignored for ragged `ids` values.)
    PiperOrigin-RevId: 293507108
    Change-Id: Id5879023d81286cbe79d829d03a303cfca0e1df2

--- tensorflow/python/ops/nn_test.py
+++ tensorflow/python/ops/nn_test.py
@@ -1590,6 +1590,34 @@ class RaggedEmbeddingTest(test_lib.TestCase):
         ValueError, "The values contained by the inputs have type*"):
       nn.embedding_lookup_ragged(weights, ragged_ids)
 
+  def testMaxNormForEmbeddings(self):
+    weights = constant_op.constant([[0, 0, 0, 0], [1, 1, 1, 1],
+                                    [2, 2, 2, 2], [3, 3, 3, 3]],
+                                   dtype=dtypes.float32)
+    ragged_ids = ragged_factory_ops.constant([[1, 2, 3], [0], [1, 2]],
+                                             ragged_rank=1)
+
+    actual_embeddings = [
+        nn.embedding_lookup(weights, ragged_ids, max_norm=max_norm)
+        for max_norm in [1, 2, 5]]
+
+    expected_embeddings = (
+        # max_norm = 1
+        [[[.5, .5, .5, .5], [.5, .5, .5, .5], [.5, .5, .5, .5]],
+         [[0, 0, 0, 0]], [[.5, .5, .5, .5], [.5, .5, .5, .5]]],
+        # max_norm = 2
+        [[[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]],
+         [[0, 0, 0, 0]], [[1, 1, 1, 1], [1, 1, 1, 1]]],
+        # max_norm = 5
+        [[[1, 1, 1, 1], [2, 2, 2, 2], [2.5, 2.5, 2.5, 2.5]],
+         [[0, 0, 0, 0]], [[1, 1, 1, 1], [2, 2, 2, 2]]],
+        )
+
+    for expected, actual in zip(expected_embeddings, actual_embeddings):
+      self.assertAllClose(
+          ragged_factory_ops.constant(expected, dtype=float, ragged_rank=1),
+          actual)
+
 
 if __name__ == "__main__":
   test_lib.main()
