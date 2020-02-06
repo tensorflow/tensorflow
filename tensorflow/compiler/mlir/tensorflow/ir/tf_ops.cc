355cf9273bba4a031676f94e24f710aa8b043433
Sean Silva <silvasean@google.com>
2020-02-06 14:16:59 -0800

    Add tf.ToBool op and use it in functional-control-flow-to-cfg.
    This op represents the truthiness comparison used by tf.If/While. The semantics are non-trivial (e.g. involve control flow in the general case), so it is best to encapsulate this in an op.
    
    Also, add a corresponding TF op to keep things in sync.
    
    PiperOrigin-RevId: 293674401
    Change-Id: I797c5e440e8946ebb5f77fc00dff9b7f7c22d751

--- tensorflow/compiler/mlir/tensorflow/ir/tf_ops.cc
+++ tensorflow/compiler/mlir/tensorflow/ir/tf_ops.cc
@@ -2526,6 +2526,33 @@ static LogicalResult Verify(TopKV2Op op) {
 }
 
 //===----------------------------------------------------------------------===//
+// ToBoolOp
+//===----------------------------------------------------------------------===//
+
+namespace {
+// If the input to ToBoolOp is a `tensor<i1>`, then the ToBoolOp is an identity
+// function and can be removed.
+class ToBoolOfZeroDBoolTensor : public OpRewritePattern<ToBoolOp> {
+  using OpRewritePattern<ToBoolOp>::OpRewritePattern;
+  PatternMatchResult matchAndRewrite(ToBoolOp op,
+                                     PatternRewriter &rewriter) const override {
+    if (auto type = op.getOperand().getType().dyn_cast<RankedTensorType>()) {
+      if (type.getRank() == 0 && type.getElementType().isInteger(1)) {
+        rewriter.replaceOp(op, op.getOperand());
+        return matchSuccess();
+      }
+    }
+    return matchFailure();
+  }
+};
+}  // namespace
+
+void ToBoolOp::getCanonicalizationPatterns(OwningRewritePatternList &results,
+                                           MLIRContext *context) {
+  results.insert<ToBoolOfZeroDBoolTensor>(context);
+}
+
+//===----------------------------------------------------------------------===//
 // TransposeOp
 //===----------------------------------------------------------------------===//
 
