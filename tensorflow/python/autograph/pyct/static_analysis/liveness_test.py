92b66929070da69a40a0bfc78dfc1ac10bbf26d8
Dan Moldovan <mdan@google.com>
2020-02-07 06:18:19 -0800

    Handle Python corner case: the name of a variable caught in an except block (i.e. `e` in `except Name as e` is not visible outside the except block itself.
    PiperOrigin-RevId: 293801105
    Change-Id: I76faf7b0957170c18f739f531c44123b4dca3835

--- tensorflow/python/autograph/pyct/static_analysis/liveness_test.py
+++ tensorflow/python/autograph/pyct/static_analysis/liveness_test.py
@@ -245,6 +245,23 @@ class LivenessAnalyzerTest(LivenessAnalyzerTestBase):
     self.assertHasLiveIn(fn_body[0].body[0], ('b', 'c'))
     self.assertHasLiveIn(fn_body[1], ('x',))
 
+  def test_live_out_except_variable(self):
+
+    def test_fn(x, a):
+      try:
+        pass
+      except a as b:
+        raise b
+      return x
+
+    node = self._parse_and_analyze(test_fn)
+    fn_body = node.body
+
+    # Note: 'a' is not live because there is no raise statement inside the
+    # try, and we discount the possibility of other code in the try block
+    # raising an error.
+    self.assertHasLiveIn(fn_body[0], ('b', 'x'))
+
   def test_live_in_return_statement(self):
 
     def test_fn(x, a, b, c):  # pylint:disable=unused-argument
