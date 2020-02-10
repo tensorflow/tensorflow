f026e707d34db8c8f06e48eba699a2cb6bf2ecde
Deven Desai <deven.desai.amd@gmail.com>
2020-02-10 20:37:15 +0000

    Skipping unsupported subtests within the tests lstm_test and gru_test

--- tensorflow/python/keras/layers/lstm_test.py
+++ tensorflow/python/keras/layers/lstm_test.py
@@ -26,6 +26,7 @@ from tensorflow.python.eager import context
 from tensorflow.python.framework import test_util as tf_test_util
 from tensorflow.python.keras import keras_parameterized
 from tensorflow.python.keras import testing_utils
+from tensorflow.python.platform import build_info
 from tensorflow.python.platform import test
 from tensorflow.python.training import adam
 from tensorflow.python.training import gradient_descent
@@ -47,6 +48,8 @@ class LSTMLayerTest(keras_parameterized.TestCase):
 
   @tf_test_util.run_v2_only
   def test_float64_LSTM(self):
+    if test.is_built_with_rocm:
+      self.skipTest("Double type is yet not supported in ROCm")
     num_samples = 2
     timesteps = 3
     embedding_dim = 4
@@ -142,6 +145,9 @@ class LSTMLayerTest(keras_parameterized.TestCase):
 
   @parameterized.parameters([True, False])
   def test_with_masking_layer_LSTM(self, unroll):
+    if build_info.is_rocm_build:
+      self.skipTest(
+        "Skipping the test as ROCm MIOpen does not support padded input yet.")
     layer_class = keras.layers.LSTM
     inputs = np.random.random((2, 3, 4))
     targets = np.abs(np.random.random((2, 3, 5)))
@@ -381,6 +387,9 @@ class LSTMLayerTest(keras_parameterized.TestCase):
       self.assertEqual(len(layer.get_losses_for(x)), 1)
 
   def test_statefulness_LSTM(self):
+    if build_info.is_rocm_build:
+      self.skipTest(
+        "Skipping the test as ROCm MIOpen does not support padded input yet.")
     num_samples = 2
     timesteps = 3
     embedding_dim = 4
