36618b1465c96d620d98ce276399e5d0460af837
Mehdi Amini <aminim@google.com>
2020-02-10 17:54:18 -0800

    Add a pass that implements a dedicated and limited island coarsening targetting TPU clusters
    This is intended to target V1 compatibility, where we operate on an unpruned
    graph.
    
    PiperOrigin-RevId: 294346674
    Change-Id: I0146660028c652ad4f1e37f9279cc12ac3acdbe5

--- tensorflow/compiler/mlir/tensorflow/transforms/passes.h
+++ tensorflow/compiler/mlir/tensorflow/transforms/passes.h
@@ -101,6 +101,10 @@ std::unique_ptr<OpPassBase<FuncOp>> CreateSwitchFoldPass();
 // Creates a pass to merge IslandOps from TFExecutor dialect.
 std::unique_ptr<OpPassBase<FuncOp>> CreateTFExecutorIslandCoarseningPass();
 
+// Creates a pass to merge IslandOps for operation marked for execution on TPU.
+// This is a V1 backward compatibility.
+std::unique_ptr<OpPassBase<FuncOp>> CreateTFExecutorTPUV1IslandCoarseningPass();
+
 // Creates a pass to outlining TPU clusters from single IslandOp into a nested
 // module suitable for being processed as-if it was a V2 module.
 // This is a V1 backward compatibility.
