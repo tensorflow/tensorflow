31806aec0334da12cac5f85bebbc7884e52911af
Keith Moulton <keithm@xmos.com>
2020-02-12 11:09:13 -0500

    integrated fully_connected_16

--- tensorflow/lite/micro/kernels/xcore/xcore_fully_connected.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_fully_connected.cc
@@ -6,91 +6,70 @@ namespace micro {
 namespace xcore {
 namespace fully_connected {
 
-    // TfLiteStatus Prepare_16(TfLiteContext* context, TfLiteNode* node) {
-    //     TF_LITE_ENSURE_EQ(context, NumInputs(node), 3);
-    //     TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+    typedef struct {
+        nn_fully_connected_plan_t plan;
+    } UserData;
 
-    //     return kTfLiteOk;
-    // }
+    void* Init_16(TfLiteContext* context, const char* buffer, size_t length) 
+    {
+        auto* user_data = new UserData();
 
+        return user_data;
+    }
 
-    // TfLiteStatus Eval_16(TfLiteContext* context, TfLiteNode* node) {
-    //     const TfLiteTensor* input = GetInput(context, node, 0);
-    //     const TfLiteTensor* weights = GetInput(context, node, 1);
-    //     const TfLiteTensor* bias_shift_scale = GetInput(context, node, 2);
+    void Free_16(TfLiteContext* context, void* buffer) {
+        auto* user_data = reinterpret_cast<UserData*>(buffer);
 
-    //     TfLiteTensor* output = GetOutput(context, node, 0);
-
-    //     int32_t C_in = weights->dims->data[1];
-    //     int32_t C_out = weights->dims->data[0];
-
-    //     fully_connected_16(
-    //         output->data.i16,
-    //         weights->data.int8,
-    //         input->data.int8,
-    //         (data16_t*) bias_shift_scale->data.i16,
-    //         C_in,
-    //         C_out
-    //     );
-
-    //     return kTfLiteOk;
-    // }
+        delete user_data;
+    }
 
 
-    TfLiteStatus Prepare_DIAO(TfLiteContext* context, TfLiteNode* node) {
-        TF_LITE_ENSURE_EQ(context, NumInputs(node), 4);
+    TfLiteStatus Prepare_16(TfLiteContext* context, TfLiteNode* node) {
+        TF_LITE_ENSURE_EQ(context, NumInputs(node), 3);
         TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+
+        const TfLiteTensor* weights = GetInput(context, node, 1);
+        int32_t C_in = weights->dims->data[1];
+        int32_t C_out = weights->dims->data[0];
+
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
+
+        fully_connected_init(&user_data->plan, C_in, C_out);
 
         return kTfLiteOk;
     }
 
 
-    TfLiteStatus Eval_DIAO(TfLiteContext* context, TfLiteNode* node) {
+    TfLiteStatus Eval_16(TfLiteContext* context, TfLiteNode* node) {
         const TfLiteTensor* input = GetInput(context, node, 0);
         const TfLiteTensor* weights = GetInput(context, node, 1);
-        const TfLiteTensor* biases = GetInput(context, node, 2);
-        const TfLiteTensor* shift_scale = GetInput(context, node, 3);
-
-        int32_t C_out = weights->dims->data[0];
-        int32_t C_in = weights->dims->data[1];
-        int32_t scales_offset = C_out;
+        const TfLiteTensor* bias_shift_scale = GetInput(context, node, 2);
 
         TfLiteTensor* output = GetOutput(context, node, 0);
 
-        fc_deepin_shallowout_16(
-            weights->data.int8,
-            biases->data.i32,
-            input->data.int8,
+        auto* user_data = reinterpret_cast<UserData*>(node->user_data);
+
+        fully_connected_16(
             output->data.i16,
-            C_out,
-            C_in,
-            (uint16_t*) &shift_scale->data.i16[0],
-            (int16_t*) &shift_scale->data.i16[scales_offset]
+            weights->data.int8,
+            input->data.int8,
+            (data16_t*) bias_shift_scale->data.i16,
+            &user_data->plan
         );
 
         return kTfLiteOk;
     }
 
+
 }  // namespace fully_connected
 
 
-// TfLiteRegistration* Register_FullyConnected_16() {
-//     static TfLiteRegistration r = {
-//         nullptr,
-//         nullptr,
-//         fully_connected::Prepare_16,
-//         fully_connected::Eval_16
-//     };
-//     return &r;
-// }
-
-
-TfLiteRegistration* Register_FullyConnected_DIAO() {
+TfLiteRegistration* Register_FullyConnected_16() {
     static TfLiteRegistration r = {
-        nullptr,
-        nullptr,
-        fully_connected::Prepare_DIAO,
-        fully_connected::Eval_DIAO
+        fully_connected::Init_16,
+        fully_connected::Free_16,
+        fully_connected::Prepare_16,
+        fully_connected::Eval_16
     };
     return &r;
 }
--- tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.cc
@@ -64,8 +64,7 @@ XcoreOpsResolver::XcoreOpsResolver() {
     AddCustom("XC_argmax_16", Register_ArgMax_16());
     AddCustom("XC_maxpool2d_deep", Register_MaxPool());
     AddCustom("XC_avgpool2d_deep", Register_AvgPool());
-    //AddCustom("XC_fc_deepin_anyout", Register_FullyConnected_16());
-    AddCustom("XC_fc_deepin_anyout", Register_FullyConnected_DIAO());
+    AddCustom("XC_fc_deepin_anyout", Register_FullyConnected_16());
     AddCustom("XC_conv2d_shallowin_deepout_relu", Register_Conv2D_SIDO());
     AddCustom("XC_conv2d_deepin_deepout_relu", Register_Conv2D_DIDO());
     AddCustom("XC_requantize_16_to_8", Register_Requantize_16_to_8());
