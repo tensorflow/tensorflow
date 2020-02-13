862b62a562c75dba46d6d6ef2aa5a069faa72f0f
Jiho Choi <jihochoi@google.com>
2020-02-13 14:38:35 -0800

    Create events for TF name scope.
    PiperOrigin-RevId: 295003367
    Change-Id: Id4fc85f9ad35f968ff4b8b8730c2383e4c8ec214

--- tensorflow/core/profiler/utils/trace_utils.h
+++ tensorflow/core/profiler/utils/trace_utils.h
@@ -23,10 +23,12 @@ namespace profiler {
 // First derived stream/thread id.
 constexpr int kThreadIdDerivedMin = 0xdeadbeef;
 constexpr int kThreadIdStepInfo = kThreadIdDerivedMin;
-constexpr int kThreadIdTfOp = kThreadIdDerivedMin + 1;
-constexpr int kThreadIdHloModule = kThreadIdDerivedMin + 2;
-constexpr int kThreadIdHloOp = kThreadIdDerivedMin + 3;
-constexpr int kThreadIdOverhead = kThreadIdDerivedMin + 4;
+constexpr int kThreadIdTfNameScope = kThreadIdDerivedMin + 1;
+constexpr int kThreadIdTfOp = kThreadIdDerivedMin + 2;
+constexpr int kThreadIdHloModule = kThreadIdDerivedMin + 3;
+constexpr int kThreadIdHloOp = kThreadIdDerivedMin + 4;
+constexpr int kThreadIdOverhead = kThreadIdDerivedMin + 5;
+
 // Last derived stream/thread id.
 constexpr int kThreadIdDerivedMax = kThreadIdOverhead;
 
