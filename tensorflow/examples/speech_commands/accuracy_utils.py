e390647355548744b278f8f8dfd86eef4094b8e4
Judd <foldl@outlook.com>
2020-02-14 16:06:02 +0800

    Update accuracy_utils.py
    fix pylint warning/errors.

--- tensorflow/examples/speech_commands/accuracy_utils.py
+++ tensorflow/examples/speech_commands/accuracy_utils.py
@@ -144,7 +144,8 @@ class StreamingAccuracyStats(object):
       correct_match_percentage = self._how_many_c / self._how_many_gt * 100
       wrong_match_percentage = self._how_many_w / self._how_many_gt * 100
       false_positive_percentage = self._how_many_fp / self._how_many_gt * 100
-      tf.compat.v1.logging.info('{:.1f}% matched, {:.1f}% correct, {:.1f}% wrong, '
-                      '{:.1f}% false positive'.format(
-                          any_match_percentage, correct_match_percentage,
-                          wrong_match_percentage, false_positive_percentage))
+      tf.compat.v1.logging.info(
+          '{:.1f}% matched, {:.1f}% correct, {:.1f}% wrong, '
+          '{:.1f}% false positive'.format(
+              any_match_percentage, correct_match_percentage,
+              wrong_match_percentage, false_positive_percentage))
