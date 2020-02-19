1c978d506559b9e1aee78a83ee3499b3b925beab
lyonguyen8697 <mr.locasdfg@gmail.com>
2020-02-19 17:58:56 +0700

    fix build failures

--- tensorflow/python/autograph/operators/py_builtins.py
+++ tensorflow/python/autograph/operators/py_builtins.py
@@ -481,13 +481,15 @@ def _tf_sorted(iterable, key, reverse):
     if mapped.shape.rank is not None and mapped.shape.rank != 1:
       raise ValueError('sort only supports only 1D tensors')
     with ops.control_dependencies(
-        [check_ops.assert_rank_v2(mapped, 1, 'sort only supports only 1D tensors')]):
+        [check_ops.assert_rank_v2(
+            mapped, 1, 'sort only supports only 1D tensors')]):
       order = sort_ops.argsort(mapped, direction=direction)
       return array_ops.gather_v2(iterable, order)
   if iterable.shape.rank is not None and iterable.shape.rank != 1:
     raise ValueError('sort only supports only 1D tensors')
   with ops.control_dependencies(
-      [check_ops.assert_rank_v2(iterable, 1, 'sort only supports only 1D tensors')]):
+      [check_ops.assert_rank_v2(
+          iterable, 1, 'sort only supports only 1D tensors')]):
     return sort_ops.sort(iterable, direction=direction)
 
 
