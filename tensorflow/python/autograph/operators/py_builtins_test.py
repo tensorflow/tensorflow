346cd079735de432391800382eed9fe9e9111340
lyonguyen8697 <mr.locasdfg@gmail.com>
2020-02-19 17:58:56 +0700

    fix build failures

--- tensorflow/python/autograph/operators/py_builtins_test.py
+++ tensorflow/python/autograph/operators/py_builtins_test.py
@@ -397,7 +397,7 @@ class PyBuiltinsTest(test.TestCase):
         py_builtins.sorted_(iterable_1, reverse=True))), [3, 2, 1])
     self.assertListEqual(list(self.evaluate(
         py_builtins.sorted_(iterable_1, key=lambda x: -x, reverse=True))),
-        [1, 2, 3])
+                         [1, 2, 3])
 
     iterable_2 = constant_op.constant([[4, 3], [2, 1]])
     with self.assertRaises(ValueError):
@@ -406,7 +406,7 @@ class PyBuiltinsTest(test.TestCase):
       py_builtins.sorted_(iterable_2, key=lambda x: -x)
     self.assertListEqual(list(self.evaluate(
         py_builtins.sorted_(iterable_2, key=lambda x: math_ops.reduce_sum(x)))),
-        [[2, 1], [4, 3]])
+                         [[2, 1], [4, 3]])
 
 
 if __name__ == '__main__':
