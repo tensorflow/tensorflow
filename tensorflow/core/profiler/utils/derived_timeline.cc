75cce895f56524e5514ce2cd0d300ab6c0a5b972
A. Unique TensorFlower <gardener@tensorflow.org>
2020-02-19 08:50:06 -0800

    Add "Launch Activities" derived line. And add stats for each step , such as how many kernel/memcpy are launched, what's maximum/avg launch time.
    PiperOrigin-RevId: 295978584
    Change-Id: Ib2c418ecf034283613e960dd26ddf488ed5ba1bb

--- tensorflow/core/profiler/utils/derived_timeline.cc
+++ tensorflow/core/profiler/utils/derived_timeline.cc
@@ -14,11 +14,14 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/core/profiler/utils/derived_timeline.h"
 
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/match.h"
 #include "absl/strings/str_split.h"
 #include "tensorflow/core/lib/gtl/map_util.h"
 #include "tensorflow/core/profiler/protobuf/xplane.pb.h"
 #include "tensorflow/core/profiler/utils/tf_op_utils.h"
 #include "tensorflow/core/profiler/utils/tf_xplane_visitor.h"
+#include "tensorflow/core/profiler/utils/timespan.h"
 #include "tensorflow/core/profiler/utils/trace_utils.h"
 #include "tensorflow/core/profiler/utils/xplane_builder.h"
 #include "tensorflow/core/profiler/utils/xplane_schema.h"
@@ -113,6 +116,7 @@ const absl::string_view kDerivedLineTensorFlowNameScope =
 const absl::string_view kDerivedLineTensorFlowOps = "TensorFlow Ops";
 const absl::string_view kDerivedLineXlaModules = "XLA Modules";
 const absl::string_view kDerivedLineXlaOps = "XLA Ops";
+const absl::string_view kDerivedLineKernelLaunch = "Launch Stats";
 const absl::string_view kAnnotationDelimiter = "::";
 
 void ProcessTfOpEvent(const XEventVisitor& event,
@@ -229,6 +233,99 @@ void DeriveEventsFromAnnotations(const SymbolResolver& symbol_resolver,
     }
   }
   RemoveEmptyLines(device_trace);
+}
+
+void DeriveEventsFromHostTrace(const XPlane* host_trace,
+                               const EventGroupNameMap& event_group_name_map,
+                               std::vector<XPlane*> device_traces) {
+  struct GroupLaunchInfo {  // "Group" normally means step.
+    Timespan timespan;
+    int32 num_launches = 0;
+    uint64 max_launch_time_ps = 0ULL;
+    uint64 total_launch_time_ps = 0ULL;
+  };
+  typedef absl::flat_hash_map<uint64 /*group_id*/, GroupLaunchInfo>
+      DeviceLaunchInfo;
+
+  int num_devices = device_traces.size();
+  std::vector<DeviceLaunchInfo> per_device_launch_info(num_devices);
+
+  XPlaneVisitor host_plane = CreateTfXPlaneVisitor(host_trace);
+  host_plane.ForEachLine([&](const XLineVisitor& line) {
+    if (IsDerivedThreadId(line.Id())) return;
+    line.ForEachEvent([&](const XEventVisitor& event) {
+      absl::optional<int64> group_id;
+      absl::optional<int64> device_id;
+      absl::optional<int64> correlation_id;
+      // Filter out API calls for cuEventRecord/cuEventQuery/cuCtxSynchronize
+      // etc for now. TODO: find a better way to filter out only the memcpy and
+      // kernel launch events.
+      if (absl::StartsWith(event.Name(), "cu")) return;
+      event.ForEachStat([&](const XStatVisitor& stat) {
+        if (stat.Type() == StatType::kGroupId) {
+          group_id = stat.IntValue();
+        } else if (stat.Type() == StatType::kDeviceId) {
+          device_id = stat.IntValue();
+        } else if (stat.Type() == StatType::kCorrelationId) {
+          correlation_id = stat.IntValue();
+        }
+      });
+      if (group_id && device_id && correlation_id && *device_id >= 0 &&
+          *device_id < num_devices) {
+        // This is a launch event on a known device.
+        GroupLaunchInfo& group_launch_info =
+            per_device_launch_info[*device_id][*group_id];
+        Timespan& group_span = group_launch_info.timespan;
+        Timespan event_span = event.GetTimespan();
+        if (group_launch_info.num_launches) {  // Existing group.
+          uint64 begin_ps =
+              std::min(group_span.begin_ps(), event_span.begin_ps());
+          uint64 end_ps = std::max(group_span.end_ps(), event_span.end_ps());
+          group_span = Timespan::FromEndPoints(begin_ps, end_ps);
+        } else {
+          group_span = event_span;
+        }
+        ++group_launch_info.num_launches;
+        group_launch_info.max_launch_time_ps = std::max(
+            group_launch_info.max_launch_time_ps, event_span.duration_ps());
+        group_launch_info.total_launch_time_ps += event_span.duration_ps();
+      }
+    });
+  });
+
+  uint64 host_plane_start = GetStartTimestampNs(*host_trace);
+  for (int i = 0; i < num_devices; ++i) {
+    if (per_device_launch_info[i].empty()) continue;
+    uint64 device_plane_start = GetStartTimestampNs(*device_traces[i]);
+    XPlaneBuilder device_plane(device_traces[i]);
+    XLineBuilder launch_line =
+        device_plane.GetOrCreateLine(kThreadIdKernelLaunch);
+    launch_line.SetName(kDerivedLineKernelLaunch);
+    launch_line.SetTimestampNs(std::min(device_plane_start, host_plane_start));
+    for (const auto& [group_id, group_info] : per_device_launch_info[i]) {
+      if (auto group_name = gtl::FindOrNull(event_group_name_map, group_id)) {
+        XEventBuilder device_event =
+            launch_line.AddEvent(*device_plane.GetOrCreateEventMetadata(
+                absl::StrCat("Launch Stats for ", *group_name)));
+        device_event.SetTimestampNs(
+            host_plane_start + PicosToNanos(group_info.timespan.begin_ps()));
+        device_event.SetDurationPs(group_info.timespan.duration_ps());
+        device_event.AddStatValue(*device_plane.GetOrCreateStatMetadata(
+                                      GetStatTypeStr(StatType::kGroupId)),
+                                  group_id);
+        device_event.AddStatValue(
+            *device_plane.GetOrCreateStatMetadata("num_launches"),
+            group_info.num_launches);
+        device_event.AddStatValue(
+            *device_plane.GetOrCreateStatMetadata("max_launch_time_us"),
+            PicosToMicros(group_info.max_launch_time_ps));
+        device_event.AddStatValue(
+            *device_plane.GetOrCreateStatMetadata("avg_launch_time_us"),
+            PicosToMicros(group_info.total_launch_time_ps /
+                          group_info.num_launches));
+      }
+    }
+  }
 }
 
 void GenerateDerivedTimeLines(const EventGroupNameMap& event_group_name_map,
