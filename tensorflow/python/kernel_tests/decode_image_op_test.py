5fc1ad961b83dd36941aa2b447a4a602b622e2c9
Brian Atkinson <bca@google.com>
2020-02-20 13:27:14 -0800

    Use resource_loader to reference in-tree resources.
    PiperOrigin-RevId: 296283401
    Change-Id: I8531d318a672c1d496f334932ec7355b1d343adf

--- tensorflow/python/kernel_tests/decode_image_op_test.py
+++ tensorflow/python/kernel_tests/decode_image_op_test.py
@@ -27,9 +27,10 @@ from tensorflow.python.framework import test_util
 from tensorflow.python.ops import image_ops
 from tensorflow.python.ops import io_ops
 import tensorflow.python.ops.nn_grad  # pylint: disable=unused-import
+from tensorflow.python.platform import resource_loader
 from tensorflow.python.platform import test
 
-prefix_path = "tensorflow/core/lib"
+prefix_path = resource_loader.get_path_to_datafile("../../core/lib")
 
 
 class DecodeImageOpTest(test.TestCase):
