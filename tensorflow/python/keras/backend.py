bc32e4b92b71c49f7b31b7bc3bc8f9df327e73eb
Scott Zhu <scottzhu@google.com>
2020-02-21 12:04:03 -0800

    Update docstring for keras.backend.std.
    PiperOrigin-RevId: 296483794
    Change-Id: I346088fdae64452668a98ed8399f7cd6432134ce

--- tensorflow/python/keras/backend.py
+++ tensorflow/python/keras/backend.py
@@ -2080,16 +2080,22 @@ def var(x, axis=None, keepdims=False):
 def std(x, axis=None, keepdims=False):
   """Standard deviation of a tensor, alongside the specified axis.
 
+  It is an alias to `tf.math.reduce_std`.
+
   Arguments:
-      x: A tensor or variable.
-      axis: An integer, the axis to compute the standard deviation.
+      x: A tensor or variable. It should have numerical dtypes. Boolean type
+        inputs will be converted to float.
+      axis: An integer, the axis to compute the standard deviation. If `None`
+        (the default), reduces all dimensions. Must be in the range
+        `[-rank(x), rank(x))`.
       keepdims: A boolean, whether to keep the dimensions or not.
           If `keepdims` is `False`, the rank of the tensor is reduced
-          by 1. If `keepdims` is `True`,
-          the reduced dimension is retained with length 1.
+          by 1. If `keepdims` is `True`, the reduced dimension is retained with
+          length 1.
 
   Returns:
-      A tensor with the standard deviation of elements of `x`.
+      A tensor with the standard deviation of elements of `x` with same dtype.
+      Boolean type input will be converted to float.
   """
   if x.dtype.base_dtype == dtypes_module.bool:
     x = math_ops.cast(x, floatx())
