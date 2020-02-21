45bcaf16a6bfe9afb75ba6d8cddff92c232ff391
Elena Zhelezina <elena.zhelezina@arm.com>
2020-02-21 17:47:40 +0000

    Added quantization support for SPLIT_V, int8. Test added.

--- tensorflow/lite/tools/optimize/operator_property.cc
+++ tensorflow/lite/tools/optimize/operator_property.cc
@@ -92,6 +92,12 @@ OperatorProperty GetOperatorProperty(const ModelT* model, int subgraph_index,
       property.restrict_same_input_output_scale = true;
       property.version = 2;
       break;
+    case BuiltinOperator_SPLIT_V:
+      property.inputs = {{0, {}}};
+      property.outputs = {{0, {}}};
+      property.restrict_same_input_output_scale = true;
+      property.version = 1;
+      break;
     case BuiltinOperator_CONCATENATION:
       property.arbitrary_inputs = true;
       property.outputs = {{0, {}}};
