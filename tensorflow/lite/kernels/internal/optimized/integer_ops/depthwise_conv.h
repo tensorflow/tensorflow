8aca7a7815307f1369c64be746b7ed206efab240
Alex Stark <starka@google.com>
2020-02-24 08:26:58 -0800

    Depthwise convolution 3x3 per-channel int8 for dot-product ARM (17).
    Enable dot-product path properly.
    
    PiperOrigin-RevId: 296896819
    Change-Id: Idf592418c031453a3b30cacd613e594bc21fa808

--- tensorflow/lite/kernels/internal/optimized/integer_ops/depthwise_conv.h
+++ tensorflow/lite/kernels/internal/optimized/integer_ops/depthwise_conv.h
@@ -1823,13 +1823,16 @@ inline void DepthwiseConvWithRounding(
     DotProduct3x3KernelType kernel_type =
         optimized_ops::depthwise_conv::CategorizeDotProductKernel<
             optimized_ops::depthwise_conv::QuantizationType::kPerChannelInt8>(
-            input_shape, filter_shape, output_shape, params);
+            input_shape, filter_shape, output_shape, params, output_shift);
     if (kernel_type != DotProduct3x3KernelType::kNone) {
       ruy::profiler::ScopeLabel specialized_label(
           "DepthwiseConvInt8/8bit/3x3XDotProduct");
+      DepthwiseParams params_copy = params;
+      params_copy.output_shift_per_channel = output_shift;
+      params_copy.output_multiplier_per_channel = output_multiplier;
       optimized_ops::depthwise_conv::DepthwiseConvDotProduct3x3PerChannel<
           DepthwiseConvImplementation::kUseNeon3x3DotProduct>(
-          params, input_shape, input_data, filter_shape, filter_data,
+          params_copy, input_shape, input_data, filter_shape, filter_data,
           bias_shape, bias_data, output_shape, output_data, thread_start,
           thread_end, thread_dim);
       return;
