8748e84c5e38ecf4c740609c0b2d4cd4b7f7f76c
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-02-24 15:37:19 +0530

    Modified reduce_variance for extra optimization

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -2111,8 +2111,8 @@ def reduce_variance(input_tensor, axis=None, keepdims=False, name=None):
   with ops.name_scope(name):
     square_of_input = gen_math_ops.square(input_tensor)
     mean_of_square = reduce_mean(square_of_input,
-    	                         axis=axis,
-    	                         keepdims=keepdims)
+    	                           axis=axis,
+    	                           keepdims=keepdims)
     mean = reduce_mean(input_tensor, axis=axis, keepdims=keepdims)
     square_of_mean = gen_math_ops.square(mean)
     return mean_of_square - square_of_mean
