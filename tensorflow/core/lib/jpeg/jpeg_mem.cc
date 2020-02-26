d6e79390a5ab7ce86833becddfcc7009840118d3
Gaurav Singh <gaurav1086@gmail.com>
2020-02-26 10:02:44 -0500

    Fix compilation errors

--- tensorflow/core/lib/jpeg/jpeg_mem.cc
+++ tensorflow/core/lib/jpeg/jpeg_mem.cc
@@ -593,7 +593,7 @@ bool GetImageInfo(const void* srcdata, int datasize, int* width, int* height,
 namespace {
 bool CompressInternal(const uint8* srcdata, int width, int height,
                       const CompressFlags& flags, tstring* output) {
-  if (output == nullptr)
+  if (output == nullptr) {
     LOG(ERROR) << "Output buffer is null: ";
     return false;
   }
