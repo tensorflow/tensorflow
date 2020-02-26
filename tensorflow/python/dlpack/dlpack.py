dab3c26d038c980cc54a5754a6b69ce508e7d89b
VoVAllen <jz1749@nyu.edu>
2020-02-26 15:16:27 +0000

    add comment

--- tensorflow/python/dlpack/dlpack.py
+++ tensorflow/python/dlpack/dlpack.py
@@ -22,12 +22,41 @@ from tensorflow.python import pywrap_tfe
 from tensorflow.python.util.tf_export import tf_export
 
 
-# tf.dlpack.to_dlpack/from_dlpack doesn't work. How to fix?
 @tf_export("experimental.dlpack.to_dlpack", v1=[])
 def to_dlpack(tf_tensor):
+  """Returns the dlpack capsule representing the tensor. This operation
+   ensures the underlying data memory is ready when returns.
+
+    ```python
+    a = tf.tensor([1, 10])
+    dlcapsule = tf.experimental.dlpack.to_dlpack(a)
+    # dlcapsule represents the dlpack data structure
+    ```
+
+    Args:
+      tf_tensor: Tensorflow eager tensor, to be converted to dlpack capsule.
+
+    Returns:
+      A PyCapsule named as dltensor, which shares the underlying memory to other
+       framework. This PyCapsule can be consumed only once. 
+  """
   return pywrap_tfe.TFE_ToDlpackCapsule(tf_tensor)
 
 
 @tf_export("experimental.dlpack.from_dlpack", v1=[])
 def from_dlpack(dlcapsule):
+  """Returns the Tensorflow eager tensor, which uses the memory shared by
+   dlpack capsules from other framework.
+
+    ```python
+    a = tf.experimental.dlpack.from_dlpack(dlcapsule)
+    # `a` uses the memory shared by dlpack
+    ```
+
+    Args:
+      dlcapsule: A PyCapsule named as dltensor
+
+    Returns:
+      A Tensorflow eager tensor 
+  """
   return pywrap_tfe.TFE_FromDlpackCapsule(dlcapsule)
