1a9dcb0b4844007f0943581f4fbeaa7fc8628bd6
Pavithra Vijay <psv@google.com>
2020-02-26 11:33:39 -0800

    Keras API docs fixit: Keras applications preprocess_input and decode_predictions doc.
    PiperOrigin-RevId: 297408596
    Change-Id: I58fac397853a2661b4461465395625ec9704c2ad

--- tensorflow/python/keras/applications/inception_resnet_v2.py
+++ tensorflow/python/keras/applications/inception_resnet_v2.py
@@ -56,6 +56,9 @@ def InceptionResNetV2(include_top=True,
   Note that the data format convention used by the model is
   the one specified in your Keras config at `~/.keras/keras.json`.
 
+  Caution: Be sure to properly pre-process your inputs to the application.
+  Please see `applications.inception_resnet_v2.preprocess_input` for an example.
+
   Arguments:
     include_top: whether to include the fully-connected
       layer at the top of the network.
@@ -401,3 +404,8 @@ def decode_predictions(preds, top=5):
     ValueError: In case of invalid shape of the `preds` array (must be 2D).
   """
   return imagenet_utils.decode_predictions(preds, top=top)
+
+
+preprocess_input.__doc__ = imagenet_utils.PREPROCESS_INPUT_DOC.format(
+    mode='', ret=imagenet_utils.PREPROCESS_INPUT_RET_DOC_TF)
+decode_predictions.__doc__ = imagenet_utils.decode_predictions.__doc__
