1af853e07f8f6000afd52d401003a4b7b89689c2
Lucy Fox <lucyfox@google.com>
2020-02-26 21:19:58 -0800

    Propagate MLIR parsing error diagnostics up when compiling serialized MLIR.
    For instance, this changes the error message "Compilation failure: could not parse MLIR module" into "Compilation failure: could not parse MLIR module<stdin>: error: elements hex data size is invalid for provided type: 'tensor<1x1x64x64xbf16>'".
    
    PiperOrigin-RevId: 297508213
    Change-Id: Ie07c967298b5b07ece81784dea74cab08123a596

--- tensorflow/compiler/mlir/tensorflow/utils/compile_mlir_util.cc
+++ tensorflow/compiler/mlir/tensorflow/utils/compile_mlir_util.cc
@@ -49,10 +49,15 @@ Status ParseMlirModule(llvm::StringRef mlir_module_string,
       << "unexpected empty serialized MLIR module string";
   TF_RET_CHECK(mlir_module) << "unexpected null MLIR module pointer";
 
+  // Make sure we catch any error reported by MLIR and forward it to the TF
+  // error reporting system.
+  mlir::StatusScopedDiagnosticHandler error_handler(mlir_context);
+
   // Parse the module.
   *mlir_module = mlir::parseSourceString(mlir_module_string, mlir_context);
   if (!*mlir_module) {
-    return errors::InvalidArgument("could not parse MLIR module");
+    return error_handler.Combine(
+        errors::InvalidArgument("could not parse MLIR module"));
   }
 
   return Status::OK();
