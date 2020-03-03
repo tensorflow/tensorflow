82f4a2e82f574f90eae059b46c36a597673131f0
fsx950223 <fsx950223@outlook.com>
2020-03-03 10:05:47 +0800

    refact test case

--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -2753,16 +2753,15 @@ class ResizeImagesV2Test(test_util.TensorFlowTestCase):
 
   @test_util.run_deprecated_v1
   def testPreserveAspectRatioMultipleImages(self):
-    x_shape = [10, 100, 100, 10]
-    x = np.random.uniform(size=x_shape)
-
-    self._assertResizeCheckShape(
-        x, x_shape, [250, 250], [10, 250, 250, 10], preserve_aspect_ratio=False)
-
     x_shape = [10, 100, 80, 10]
     x = np.random.uniform(size=x_shape)
-    self._assertResizeCheckShape(
-        x, x_shape, [250, 250], [10, 250, 200, 10], preserve_aspect_ratio=True)
+    for preserve_aspect_ratio in [True, False]:
+      with self.subTest(preserve_aspect_ratio=preserve_aspect_ratio):
+        expect_shape = [10, 250, 200, 10] if preserve_aspect_ratio \
+            else [10, 250, 250, 10]
+        self._assertResizeCheckShape(
+            x, x_shape, [250, 250], expect_shape,
+            preserve_aspect_ratio=preserve_aspect_ratio)
 
   @test_util.run_deprecated_v1
   def testPreserveAspectRatioNoOp(self):
