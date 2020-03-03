188c063dfa16013a1fda3b227307e980a075dccf
Renjie Liu <renjieliu@google.com>
2020-03-02 17:10:19 -0800

    [NFC] Refactor graphdef_to_tfl_flatbuffer logic: factor out populate_quantization_specs, register_custom_ops, convert_mlir_to_tflite_flatbuffer logic (so they can be shared with saved_model_importer).
    This is in prepare for the saved_model import (which is needed for ops fusion).
    
    PiperOrigin-RevId: 298484338
    Change-Id: I0153dbfb987b22390a95f91ee8432fc4cb697a1c

--- tensorflow/compiler/mlir/lite/python/graphdef_to_tfl_flatbuffer.cc
+++ tensorflow/compiler/mlir/lite/python/graphdef_to_tfl_flatbuffer.cc
@@ -33,6 +33,7 @@ limitations under the License.
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/platform/status.h"
 #include "tensorflow/core/protobuf/graph_debug_info.pb.h"
 #include "tensorflow/lite/toco/model_flags.pb.h"
 #include "tensorflow/lite/toco/toco_flags.pb.h"
@@ -197,60 +198,60 @@ Status RegisterCustomBuiltinOps(const std::vector<string> extra_tf_opdefs) {
   return Status::OK();
 }
 
-}  // namespace
+Status RegisterAllCustomOps(const toco::TocoFlags& toco_flags) {
+  // Register any custom OpDefs.
+  std::vector<string> extra_tf_opdefs(toco_flags.custom_opdefs().begin(),
+                                      toco_flags.custom_opdefs().end());
+  extra_tf_opdefs.push_back(kDetectionPostProcessOp);
+  extra_tf_opdefs.push_back(kUnidirectionalSequenceLstmOp);
+  extra_tf_opdefs.push_back(kUnidirectionalSequenceRnnOp);
+  return RegisterCustomBuiltinOps(extra_tf_opdefs);
+}
 
-Status ConvertGraphDefToTFLiteFlatBuffer(const toco::ModelFlags& model_flags,
-                                         const toco::TocoFlags& toco_flags,
-                                         const GraphDebugInfo& debug_info,
-                                         const GraphDef& input,
-                                         string* result) {
-  mlir::MLIRContext context;
-  GraphImportConfig specs;
-  mlir::TFL::QuantizationSpecs quant_specs;
-
-  // Parse input arrays.
-  std::vector<string> node_names;
-  std::vector<string> node_dtypes;
-  std::vector<std::vector<int>> node_shapes;
-  std::vector<double> node_mins;
-  std::vector<double> node_maxs;
-  quant_specs.inference_input_type =
+Status PopulateQuantizationSpecs(const toco::ModelFlags& model_flags,
+                                 const toco::TocoFlags& toco_flags,
+                                 mlir::TFL::QuantizationSpecs* quant_specs,
+                                 std::vector<string>* node_names,
+                                 std::vector<string>* node_dtypes,
+                                 std::vector<std::vector<int>>* node_shapes,
+                                 std::vector<double>* node_mins,
+                                 std::vector<double>* node_maxs) {
+  quant_specs->inference_input_type =
       ConvertIODataTypeToDataType(toco_flags.inference_input_type());
   tensorflow::DataType inference_type =
       ConvertIODataTypeToDataType(toco_flags.inference_type());
   // Use non-float flag `inference_input_type` to override the `inference_type`
   // because we have to apply quantization to satisfy that.
-  if (quant_specs.inference_input_type != tensorflow::DT_FLOAT) {
-    inference_type = quant_specs.inference_input_type;
+  if (quant_specs->inference_input_type != tensorflow::DT_FLOAT) {
+    inference_type = quant_specs->inference_input_type;
   }
 
   for (auto& flag : model_flags.input_arrays()) {
-    node_names.push_back(flag.name());
+    node_names->push_back(flag.name());
     // TOCO doesn't required `data_type` to be filled for every input.
     // If it's not filled, make it an empty string so the importer will use
     // the data type in the NodeDef.
     auto toco_data_type = flag.data_type();
     if (toco_data_type == ::toco::IODataType::IO_DATA_TYPE_UNKNOWN) {
-      node_dtypes.push_back("");
+      node_dtypes->push_back("");
     } else {
-      node_dtypes.push_back(
+      node_dtypes->push_back(
           DataType_Name(ConvertIODataTypeToDataType(toco_data_type)));
     }
-    node_shapes.push_back(std::vector<int>(flag.shape().dims().begin(),
-                                           flag.shape().dims().end()));
+    node_shapes->push_back(std::vector<int>(flag.shape().dims().begin(),
+                                            flag.shape().dims().end()));
     // Currently, only UINT8 and INT8 require inputs stats
     if (inference_type == DT_QINT8 || inference_type == DT_QUINT8) {
       TF_ASSIGN_OR_RETURN(
           auto min_max, InputStatsToMinMax(flag.mean_value(), flag.std_value(),
                                            inference_type));
-      node_mins.push_back(min_max.first);
-      node_maxs.push_back(min_max.second);
+      node_mins->push_back(min_max.first);
+      node_maxs->push_back(min_max.second);
     }
   }
-  TF_RETURN_IF_ERROR(tensorflow::ParseInputArrayInfo(
-      node_names, node_dtypes, node_shapes, &specs.inputs));
-  if (mlir::TFL::GetInputNodeQuantSpecs(node_names, node_mins, node_maxs,
-                                        inference_type, &quant_specs)) {
+
+  if (mlir::TFL::GetInputNodeQuantSpecs(*node_names, *node_mins, *node_maxs,
+                                        inference_type, quant_specs)) {
     return errors::InvalidArgument("Failed to get input quant spec.");
   }
 
@@ -258,49 +259,34 @@ Status ConvertGraphDefToTFLiteFlatBuffer(const toco::ModelFlags& model_flags,
   // quantization is enabled, `inference_type` and `inference_input_type` are
   // not used by MLIR passes.
   if (toco_flags.post_training_quantize()) {
-    quant_specs.weight_quantization = true;
+    quant_specs->weight_quantization = true;
     if (toco_flags.quantize_to_float16()) {
-      quant_specs.inference_type = tensorflow::DT_HALF;
-      quant_specs.inference_input_type = tensorflow::DT_HALF;
+      quant_specs->inference_type = tensorflow::DT_HALF;
+      quant_specs->inference_input_type = tensorflow::DT_HALF;
     } else {
-      quant_specs.inference_type = tensorflow::DT_QINT8;
-      quant_specs.inference_input_type = tensorflow::DT_QINT8;
+      quant_specs->inference_type = tensorflow::DT_QINT8;
+      quant_specs->inference_input_type = tensorflow::DT_QINT8;
     }
   }
 
-  // Parse output arrays.
-  std::vector<string> output_arrays(model_flags.output_arrays().begin(),
-                                    model_flags.output_arrays().end());
-  TF_RETURN_IF_ERROR(
-      tensorflow::ParseOutputArrayInfo(output_arrays, &specs.outputs));
-
   // Other flags.
   if (toco_flags.has_default_ranges_min()) {
-    quant_specs.default_ranges.first = toco_flags.default_ranges_min();
+    quant_specs->default_ranges.first = toco_flags.default_ranges_min();
   }
   if (toco_flags.has_default_ranges_max()) {
-    quant_specs.default_ranges.second = toco_flags.default_ranges_max();
+    quant_specs->default_ranges.second = toco_flags.default_ranges_max();
   }
 
+  return ::tensorflow::Status::OK();
+}
+
+Status ConvertMLIRToTFLiteFlatBuffer(const toco::TocoFlags& toco_flags,
+                                     mlir::OwningModuleRef module,
+                                     mlir::TFL::QuantizationSpecs quant_specs,
+                                     string* result) {
   bool emit_builtin_tflite_ops = !toco_flags.force_select_tf_ops();
   bool emit_select_tf_ops = toco_flags.enable_select_tf_ops();
   bool emit_custom_ops = toco_flags.allow_custom_ops();
-  specs.prune_unused_nodes = true;
-  specs.convert_legacy_fed_inputs = true;
-  specs.graph_as_function = false;
-  specs.upgrade_legacy = true;
-  WarningUnusedFlags(model_flags, toco_flags);
-
-  // Register any custom OpDefs.
-  std::vector<string> extra_tf_opdefs(toco_flags.custom_opdefs().begin(),
-                                      toco_flags.custom_opdefs().end());
-  extra_tf_opdefs.push_back(kDetectionPostProcessOp);
-  extra_tf_opdefs.push_back(kUnidirectionalSequenceLstmOp);
-  extra_tf_opdefs.push_back(kUnidirectionalSequenceRnnOp);
-  TF_RETURN_IF_ERROR(RegisterCustomBuiltinOps(extra_tf_opdefs));
-
-  TF_ASSIGN_OR_RETURN(
-      auto module, ConvertGraphdefToMlir(input, debug_info, specs, &context));
 
   if (toco_flags.has_dump_graphviz_dir()) {
     TF_RETURN_IF_ERROR(DumpOpGraphToFile(
@@ -332,6 +318,54 @@ Status ConvertGraphDefToTFLiteFlatBuffer(const toco::ModelFlags& model_flags,
   }
 
   return status;
+}
+
+}  // namespace
+
+Status ConvertGraphDefToTFLiteFlatBuffer(const toco::ModelFlags& model_flags,
+                                         const toco::TocoFlags& toco_flags,
+                                         const GraphDebugInfo& debug_info,
+                                         const GraphDef& input,
+                                         string* result) {
+  mlir::MLIRContext context;
+  GraphImportConfig specs;
+  mlir::TFL::QuantizationSpecs quant_specs;
+
+  // Parse input arrays.
+  std::vector<string> node_names;
+  std::vector<string> node_dtypes;
+  std::vector<std::vector<int>> node_shapes;
+  std::vector<double> node_mins;
+  std::vector<double> node_maxs;
+
+  // Populate quantization specs.
+  TF_RETURN_IF_ERROR(PopulateQuantizationSpecs(
+      model_flags, toco_flags, &quant_specs, &node_names, &node_dtypes,
+      &node_shapes, &node_mins, &node_maxs));
+
+  TF_RETURN_IF_ERROR(tensorflow::ParseInputArrayInfo(
+      node_names, node_dtypes, node_shapes, &specs.inputs));
+
+  // Parse output arrays.
+  std::vector<string> output_arrays(model_flags.output_arrays().begin(),
+                                    model_flags.output_arrays().end());
+  TF_RETURN_IF_ERROR(
+      tensorflow::ParseOutputArrayInfo(output_arrays, &specs.outputs));
+
+  specs.prune_unused_nodes = true;
+  specs.convert_legacy_fed_inputs = true;
+  specs.graph_as_function = false;
+  specs.upgrade_legacy = true;
+  WarningUnusedFlags(model_flags, toco_flags);
+
+  // Register all custom ops, including user-specified custom ops.
+  TF_RETURN_IF_ERROR(RegisterAllCustomOps(toco_flags));
+
+  TF_ASSIGN_OR_RETURN(
+      auto module, ConvertGraphdefToMlir(input, debug_info, specs, &context));
+
+  return ConvertMLIRToTFLiteFlatBuffer(toco_flags, std::move(module),
+                                       quant_specs, result);
 }
 
 }  // namespace tensorflow
