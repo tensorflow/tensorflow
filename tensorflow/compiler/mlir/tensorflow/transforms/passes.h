39893efde123e86672aae348e3336ab67387dbc6
A. Unique TensorFlower <gardener@tensorflow.org>
2020-03-03 16:36:16 -0800

    Add an add-hoc pass to perform some fusion that are at the moment focused on GPU targets
    It supports only a simple pattern and is only intended to support a demo.
    
    PiperOrigin-RevId: 298723079
    Change-Id: I6cac67ae157d510de6fef5f356d36896602f4d82

--- tensorflow/compiler/mlir/tensorflow/transforms/passes.h
+++ tensorflow/compiler/mlir/tensorflow/transforms/passes.h
@@ -49,9 +49,6 @@ std::unique_ptr<OpPassBase<FuncOp>> CreateUnrollBatchMatMulPassPass();
 // Optimizes Tensorflow graph.
 std::unique_ptr<OpPassBase<FuncOp>> CreateTFOptimizePass();
 
-// Performs specific fusion for GPU targets.
-std::unique_ptr<OpPassBase<FuncOp>> CreateGpuOpFusionPass();
-
 struct LayoutOptimizationPipelineOptions
     : public PassPipelineOptions<LayoutOptimizationPipelineOptions> {
   Option<std::string> force_data_format{
