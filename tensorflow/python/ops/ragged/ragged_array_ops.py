90d563c8b7059f6fc6bfba9ec99f2337f04f5921
Edward Loper <edloper@google.com>
2020-03-04 11:01:10 -0800

    Add tests for tf.one_hot() ragged tensor handling. Fix bug for tf.one_hot() when indices is a ragged_tensor and axis >= 0.
    PiperOrigin-RevId: 298890935
    Change-Id: Idfb890f66f55d40a763bffb549b29750dbe662f9

--- tensorflow/python/ops/ragged/ragged_array_ops.py
+++ tensorflow/python/ops/ragged/ragged_array_ops.py
@@ -517,14 +517,18 @@ def ragged_one_hot(indices,
                    dtype=None,
                    name=None):
   """Applies tf.one_hot along the values of a RaggedTensor."""
-  with ops.name_scope(name, 'RaggedOneHot', [indices]):
+  # Get the adjusted axis value for the call to array_ops.one_hot.
+  # Note: the only negative `axis` value supported by array_ops.one_hot is -1.
+  if isinstance(axis, int) and axis >= 0:
+    if axis <= indices.ragged_rank:
+      raise ValueError('axis (%d) must be greater than indices.ragged_rank '
+                       '(%d).' % (axis, indices.ragged_rank))
+    axis -= indices.ragged_rank
+
+  with ops.name_scope(name, 'RaggedOneHot',
+                      [indices, depth, on_value, off_value, axis]):
     indices = ragged_tensor.convert_to_tensor_or_ragged_tensor(
         indices, name='indices')
-    if axis is not None:
-      axis = array_ops.get_positive_axis(
-          axis, indices.shape.ndims, ndims_name='rank(indices)')
-      if axis < indices.ragged_rank:
-        raise ValueError('axis may not be less than indices.ragged_rank.')
     return indices.with_flat_values(
         array_ops.one_hot(indices.flat_values, depth, on_value, off_value, axis,
                           dtype, name))
