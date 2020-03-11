bba651011d8c641c7937d1830221063a413facd1
Edward Loper <edloper@google.com>
2020-03-11 10:48:20 -0700

    Use public API to construct RowPartitions in ragged_tensor_test.py.
    PiperOrigin-RevId: 300359617
    Change-Id: I6f83aeb24db9c3fae4dfec3bac00ea26c7213da8

--- tensorflow/python/ops/ragged/ragged_tensor_test.py
+++ tensorflow/python/ops/ragged/ragged_tensor_test.py
@@ -36,7 +36,6 @@ from tensorflow.python.ops.ragged import ragged_factory_ops
 from tensorflow.python.ops.ragged import ragged_math_ops
 from tensorflow.python.ops.ragged import ragged_tensor
 from tensorflow.python.ops.ragged import ragged_tensor_value
-from tensorflow.python.ops.ragged import row_partition
 from tensorflow.python.ops.ragged.ragged_tensor import RaggedTensor
 from tensorflow.python.ops.ragged.ragged_tensor import RaggedTensorSpec
 from tensorflow.python.ops.ragged.row_partition import RowPartition
@@ -140,8 +139,7 @@ class RaggedTensorTest(test_util.TensorFlowTestCase, parameterized.TestCase):
   def testRaggedTensorConstruction(self):
     values = constant_op.constant(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
     row_splits = constant_op.constant([0, 2, 2, 5, 6, 7], dtypes.int64)
-    rp = RowPartition(row_splits=row_splits,
-                      internal=row_partition._row_partition_factory_key)
+    rp = RowPartition.from_row_splits(row_splits)
     rt = RaggedTensor(values=values, row_partition=rp, internal=True)
 
     self.assertAllEqual(rt,
@@ -150,8 +148,7 @@ class RaggedTensorTest(test_util.TensorFlowTestCase, parameterized.TestCase):
   def testRaggedTensorConstructionErrors(self):
     values = constant_op.constant(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
     row_splits = constant_op.constant([0, 2, 2, 5, 6, 7], dtypes.int64)
-    rp = RowPartition(row_splits=row_splits,
-                      internal=row_partition._row_partition_factory_key)
+    rp = RowPartition.from_row_splits(row_splits)
 
     with self.assertRaisesRegexp(ValueError,
                                  'RaggedTensor constructor is private'):
