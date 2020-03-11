ed056399ad7ff3b9a7beb3eb313bec7a33343eb1
Raman Sarokin <sorokin@google.com>
2020-03-11 12:25:47 -0700

    Improved block size selection in ConvolutionTransposed for Mali.
    PiperOrigin-RevId: 300381966
    Change-Id: I792a184a7cece13aefdcd6a65c5d346b52df94b2

--- tensorflow/lite/delegates/gpu/cl/cl_device.h
+++ tensorflow/lite/delegates/gpu/cl/cl_device.h
@@ -89,6 +89,13 @@ struct MaliInfo {
   MaliInfo() = default;
   explicit MaliInfo(const std::string& device_name);
   MaliGPU gpu_version;
+
+  bool IsMaliT6xx() const;
+  bool IsMaliT7xx() const;
+  bool IsMaliT8xx() const;
+  bool IsMidgard() const;
+  bool IsBifrost() const;
+  bool IsValhall() const;
 };
 
 struct DeviceInfo {
