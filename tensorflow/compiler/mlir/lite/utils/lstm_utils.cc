d8fdc31ebaef3ef913bb3dd0f76b4b54e10ddd0f
Renjie Liu <renjieliu@google.com>
2020-03-12 22:42:33 -0700

    update time major: we won't transpose inputs & outputs if it's not time-majored but only set the time_majored attribute. This will make bidi-lstm fusion easier.
    PiperOrigin-RevId: 300694225
    Change-Id: Id5fcf958b076ded7e87e8bd5b7077a88eaed8a1f

--- tensorflow/compiler/mlir/lite/utils/lstm_utils.cc
+++ tensorflow/compiler/mlir/lite/utils/lstm_utils.cc
@@ -650,26 +650,21 @@ LogicalResult ConvertKerasLSTMLayer(mlir::FuncOp func_op, OpBuilder* builder) {
 
   auto final_inputs = input;
   auto final_input_type = input_type;
-  // We will transpose the inputs.
-  if (!time_majored) {
-    SmallVector<int32_t, 4> perm = {1, 0, 2};
-    final_inputs =
-        Transpose(builder, final_inputs, perm, input_type, func_op.getLoc());
-    final_input_type = final_inputs.getType().dyn_cast<RankedTensorType>();
-  }
 
   // Handle go_backwards:
   // LSTM in Keras semantic will reverse the input sequence if it's go_backwards
   auto go_backwards_attr = func_op.getAttrOfType<BoolAttr>("tf.go_backwards");
 
   if (go_backwards_attr != nullptr && go_backwards_attr.getValue()) {
-    // We assume input is already in {time, batch, size} layout.
-    final_inputs =
-        Reverse(builder, final_inputs, 0, final_input_type, func_op.getLoc());
+    int time_dim = time_majored ? 0 : 1;
+    final_inputs = Reverse(builder, final_inputs, time_dim, final_input_type,
+                           func_op.getLoc());
   }
 
-  int batch = final_input_type.getDimSize(1);
-  int time = final_input_type.getDimSize(0);
+  int batch = time_majored ? final_input_type.getDimSize(1)
+                           : final_input_type.getDimSize(0);
+  int time = time_majored ? final_input_type.getDimSize(0)
+                          : final_input_type.getDimSize(1);
 
   // Setup correct weights.
   RankedTensorType weight_type =
@@ -710,7 +705,12 @@ LogicalResult ConvertKerasLSTMLayer(mlir::FuncOp func_op, OpBuilder* builder) {
     return failure();
 
   // Build the lstm op.
-  SmallVector<int64_t, 3> output_shape = {time, batch, n_output};
+  SmallVector<int64_t, 3> output_shape;
+  if (time_majored) {
+    output_shape = {time, batch, n_output};
+  } else {
+    output_shape = {batch, time, n_output};
+  }
   auto result_type = mlir::RankedTensorType::get(
       output_shape,
       final_inputs.getType().cast<RankedTensorType>().getElementType());
@@ -743,15 +743,9 @@ LogicalResult ConvertKerasLSTMLayer(mlir::FuncOp func_op, OpBuilder* builder) {
       /*cell_layer_norm_coefficients=*/none,
       /*output_layer_norm_coefficients=*/none, builder->getStringAttr("TANH"),
       builder->getF32FloatAttr(10.0), builder->getF32FloatAttr(0.0),
-      builder->getBoolAttr(true));
+      builder->getBoolAttr(time_majored));
 
   auto final_output_full_sequences = lstm.getResult();
-  if (!time_majored) {
-    SmallVector<int32_t, 4> perm = {1, 0, 2};
-    final_output_full_sequences =
-        Transpose(builder, final_output_full_sequences, perm, result_type,
-                  func_op.getLoc());
-  }
 
   // Populate the last output: last output is sliced from the full sequences.
   // If time_major: last_output = outputs[-1, :, :]
