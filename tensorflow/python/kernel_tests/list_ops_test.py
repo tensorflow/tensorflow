eaa1729a52952f2a541491a1ce2c34af7ab66fc8
Eugene Zhulenev <ezhulenev@google.com>
2020-03-18 13:03:19 -0700

    Force CPU placement for ops that has DT_VARIANT inputs with host-only underlying data type.
    Fix for #28007
    
    PiperOrigin-RevId: 301650148
    Change-Id: I47fa9c1b0b7a7d56c5a519095687f36651892644

--- tensorflow/python/kernel_tests/list_ops_test.py
+++ tensorflow/python/kernel_tests/list_ops_test.py
@@ -26,6 +26,7 @@ from tensorflow.python.client import session
 from tensorflow.python.eager import backprop
 from tensorflow.python.eager import context
 from tensorflow.python.eager import def_function
+from tensorflow.python.eager import function
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
@@ -1632,14 +1633,37 @@ class ListOpsTest(test_util.TensorFlowTestCase, parameterized.TestCase):
       self.assertAllEqual(t, [1.0, 2.0, 3.0])
 
   def testTensorListStrings(self):
-    self.skipTest("b/150742232")
-
     @def_function.function
     def f():
       return map_fn.map_fn(string_ops.string_upper,
                            constant_op.constant(["a", "b", "c"]))
 
-    self.assertAllEqual(f(), ["A", "B", "C"])
+    self.assertAllEqual(f(), [b"A", b"B", b"C"])
+
+  def testTensorListStringsNoInline(self):
+    # Generator function output type is a variant with a host-only underlying
+    # data type. "ColocationGraph::AddHostOnlyDataTypesConstraints" needs to
+    # have "deep op inspection" to be able to correctly place the while loop
+    # generated from map_fn.
+    self.skipTest("b/150742232")
+
+    @function.defun_with_attributes(attributes={"_noinline": True})
+    def generator():
+      c = constant_op.constant(["a", "b", "c"])
+      return list_ops.tensor_list_from_tensor(c, element_shape=[])
+
+    @def_function.function
+    def f():
+      l = generator()
+
+      def upper(i):
+        e = list_ops.tensor_list_get_item(l, i, element_dtype=dtypes.string)
+        return string_ops.string_upper(e)
+
+      return map_fn.map_fn(
+          upper, constant_op.constant([0, 1, 2]), dtype=dtypes.string)
+
+    self.assertAllEqual(f(), [b"A", b"B", b"C"])
 
 
 if __name__ == "__main__":
