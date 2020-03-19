d7ca6adbd14349253ea8f7b50d817937afdbd4b4
Robert David <lrdx@google.com>
2020-03-19 14:17:03 -0700

    Change the scratch tensor a stack array like for float SVDF.
    Similar to integer SVDF. This makes TFL and TFLM models compatible.
    
    PiperOrigin-RevId: 301893460
    Change-Id: I6b279d359ca0985bcc7cfeae908a83214ae8a2e0

--- tensorflow/lite/micro/kernels/svdf.cc
+++ tensorflow/lite/micro/kernels/svdf.cc
@@ -116,14 +116,11 @@ static inline void ApplyTimeWeightsBiasAndActivation(
   }
 }
 
-inline void EvalFloatSVDF(TfLiteContext* context, TfLiteNode* node,
-                          const TfLiteTensor* input,
-                          const TfLiteTensor* weights_feature,
-                          const TfLiteTensor* weights_time,
-                          const TfLiteTensor* bias,
-                          const TfLiteSVDFParams* params, TfLiteTensor* scratch,
-                          TfLiteTensor* activation_state,
-                          TfLiteTensor* output) {
+inline void EvalFloatSVDF(
+    TfLiteContext* context, TfLiteNode* node, const TfLiteTensor* input,
+    const TfLiteTensor* weights_feature, const TfLiteTensor* weights_time,
+    const TfLiteTensor* bias, const TfLiteSVDFParams* params,
+    TfLiteTensor* activation_state, TfLiteTensor* output) {
   const int rank = params->rank;
   const int batch_size = input->dims->data[0];
   const int input_size = input->dims->data[1];
@@ -137,7 +134,11 @@ inline void EvalFloatSVDF(TfLiteContext* context, TfLiteNode* node,
   const float* input_ptr = GetTensorData<float>(input);
 
   float* state_ptr = GetTensorData<float>(activation_state);
-  float* scratch_ptr = GetTensorData<float>(scratch);
+
+  // TODO(b/132070898): Move this temp variable to the new scratch buffer API
+  // when ready.
+  float scratch_tensor[kScratchTensorMaxSize];
+  float* scratch_ptr = scratch_tensor;
 
   float* output_ptr = GetTensorData<float>(output);
 
@@ -421,7 +422,7 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
     // Validate output tensor:
     TF_LITE_ENSURE_EQ(context, output->type, kTfLiteInt8);
   } else {
-    TF_LITE_ENSURE_EQ(context, node->inputs->size, 6);
+    TF_LITE_ENSURE_EQ(context, node->inputs->size, 5);
 
     // Validate Input Tensor dtypes:
     TF_LITE_ENSURE_EQ(context, weights_feature->type, kTfLiteFloat32);
@@ -436,15 +437,8 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
     // [0] = Holds dot-product of time-forward calculations in
     //       ApplyTimeWeightsBiasAndActivation():
     //         float/int32, {2, batch_size, num_filters}
-    // TODO(b/132070898): Use input tensor as variable until scratch tensor
-    // allocation has been implemented (b/132070898) TfLiteTensor*
-    // scratch_tensor = GetTemporary(context, node, 0);
-    TfLiteTensor* scratch_tensor = &context->tensors[node->inputs->data[5]];
-    TF_LITE_ENSURE_EQ(context, scratch_tensor->type, kTfLiteFloat32);
-
-    TF_LITE_ENSURE_EQ(context, NumDimensions(scratch_tensor), 2);
-    TF_LITE_ENSURE_EQ(context, scratch_tensor->dims->data[0], batch_size);
-    TF_LITE_ENSURE_EQ(context, scratch_tensor->dims->data[1], num_filters);
+    // TODO(b/132070898): Scratch values are used as stack variables in
+    // EvalIntegerSVDF().
 
     // Full-float SVDF only uses the one shared scratch tensor (see above for
     // usage).
@@ -475,11 +469,10 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   switch (weights_feature->type) {
     case kTfLiteFloat32: {
       // TODO(b/132070898): Use input tensor as variable until scratch tensor
-      // allocation has been implemented. TfLiteTensor* scratch =
-      // GetTemporary(context, node, /*index=*/0);
-      TfLiteTensor* scratch = &context->tensors[node->inputs->data[5]];
+      // allocation has been implemented.
+      // TfLiteTensor* scratch = GetTemporary(context, node, /*index=*/0);
       EvalFloatSVDF(context, node, input, weights_feature, weights_time, bias,
-                    params, scratch, activation_state, output);
+                    params, activation_state, output);
       return kTfLiteOk;
       break;
     }
