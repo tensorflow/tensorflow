61581a3b7c76aa5feea1c0db8b06064378d7cd47
A. Unique TensorFlower <gardener@tensorflow.org>
2020-03-19 22:30:55 -0700

    Intenral Copybara change.
    PiperOrigin-RevId: 301964706
    Change-Id: I39757e487623e2b728654446ec39d11f997317a3

--- tensorflow/core/kernels/eigen_contraction_kernel.cc
+++ tensorflow/core/kernels/eigen_contraction_kernel.cc
@@ -28,9 +28,7 @@ limitations under the License.
 // the configuration through the environment variable.
 //
 // Example:
-//   bazel test \
-//     --test_env=TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL=false \
-//     //path/to:test
+//   bazel test --test_env=TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL=false //test
 
 #if defined(TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL)
 
@@ -39,11 +37,7 @@ namespace internal {
 
 // TODO(ezhulenev): This is a temporary workaround for disabling custom kernels
 // at runtime in tests. We should always rely on compile time flags for that.
-//
-// Example:
-//   bazel test \
-//     --test_env=TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL=false \
-//     //path/to:test
+// Example: ... --test_env=TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL=false //test
 EIGEN_DEVICE_FUNC EIGEN_DONT_INLINE bool UseCustomContractionKernels() {
   static bool use_custom_contraction_kernel = true;
 
