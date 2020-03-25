ae851304f3915ac0221689e8ab3ad81a87209260
Manish <manisharadwad@gmail.com>
2020-03-25 22:49:24 +0530

    Mentioned tf.keras.losses.cosine_similarity() as a real number

--- tensorflow/python/keras/losses.py
+++ tensorflow/python/keras/losses.py
@@ -1681,10 +1681,10 @@ def poisson(y_true, y_pred):
 def cosine_similarity(y_true, y_pred, axis=-1):
   """Computes the cosine similarity between labels and predictions.
 
-  Note that it is a negative quantity between -1 and 0, where 0 indicates
-  orthogonality and values closer to -1 indicate greater similarity. This makes
-  it usable as a loss function in a setting where you try to maximize the
-  proximity between predictions and targets. If either `y_true` or `y_pred` 
+  Note that it is a real number. When it is a negative number between -1 and 0, 0
+  indicates orthogonality and values closer to -1 indicate greater similarity.
+  This makes it usable as a loss function in a setting where you try to maximize
+  the proximity between predictions and targets. If either `y_true` or `y_pred` 
   is a zero vector, cosine similarity will be 0 regardless of the proximity
   between predictions and targets.
 
