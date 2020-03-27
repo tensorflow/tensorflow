e0782d289012833c7b3881c683dddbfa81a683ea
Andrew Cavanaugh <andrewc@xmos.com>
2020-03-27 10:18:11 -0400

    Workaround for a matching ambiguity on some compilers in an overloaded function.

--- tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc
+++ tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc
@@ -331,7 +331,7 @@ void GreedyMemoryPlanner::PrintMemoryPlan(ErrorReporter* error_reporter) {
       }
     }
     line[kLineWidth] = 0;
-    TF_LITE_REPORT_ERROR(error_reporter, "%s", line);
+    TF_LITE_REPORT_ERROR(error_reporter, "%s", (const char *)line);
   }
 }
 
