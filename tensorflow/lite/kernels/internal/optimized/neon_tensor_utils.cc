d0a0a22b983782fa76397c667ac0be2446a19599
Benoit Jacob <benoitjacob@google.com>
2020-03-27 18:58:51 -0700

    Move ruy's code to a ruy/ subdirectory.
    The motivation is that having source files in the repository root runs into a number of corner cases with copybara setups and with external CMake build systems, so enclosing all code in ruy/ avoids that while generally making our setup much more similar to that of other related projects (TensorFlow, IREE).
    
    PiperOrigin-RevId: 303448881
    Change-Id: Id099055cba4d3c50ae60171d645614b57a8d3ee7

--- tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
+++ tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
@@ -23,8 +23,8 @@ limitations under the License.
 #include <limits>
 #include <utility>
 
-#include "tensorflow/lite/experimental/ruy/detect_arm.h"
-#include "tensorflow/lite/experimental/ruy/ruy.h"
+#include "tensorflow/lite/experimental/ruy/ruy/detect_arm.h"
+#include "tensorflow/lite/experimental/ruy/ruy/ruy.h"
 #include "tensorflow/lite/kernels/cpu_backend_context.h"
 #include "tensorflow/lite/kernels/cpu_backend_gemm.h"
 #include "tensorflow/lite/kernels/cpu_backend_gemm_params.h"
--- tensorflow/lite/kernels/lstm_eval.cc
+++ tensorflow/lite/kernels/lstm_eval.cc
@@ -19,7 +19,7 @@ limitations under the License.
 
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/experimental/ruy/profiler/instrumentation.h"
+#include "tensorflow/lite/experimental/ruy/ruy/profiler/instrumentation.h"
 #include "tensorflow/lite/kernels/cpu_backend_context.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
 #include "tensorflow/lite/kernels/internal/kernel_utils.h"
