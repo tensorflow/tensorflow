711b1b64ba6506fff9b6ff195fe800d331d5f204
Rohan Reddy <43641071+rohanreddych@users.noreply.github.com>
2020-03-29 10:11:43 +0530

    Update activations.py
    improve documentation of sigmoid function

--- tensorflow/python/keras/activations.py
+++ tensorflow/python/keras/activations.py
@@ -274,6 +274,8 @@ def sigmoid(x):
 
   >>> a = tf.constant([-20, -1.0, 0.0, 1.0, 20], dtype = tf.float32)
   >>> b = tf.keras.activations.sigmoid(a)
+  >>> b.numpy()
+  [2.0611537e-09, 2.6894143e-01, 5.0000000e-01, 7.3105860e-01, 1.0000000e+00]
   >>> b.numpy() >= 0.0
   array([ True,  True,  True,  True,  True])
 
