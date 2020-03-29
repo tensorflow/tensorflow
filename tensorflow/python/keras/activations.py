815bb2b5c66250a5d3d55e45f1016c3773dc3b83
Rohan Reddy <43641071+rohanreddych@users.noreply.github.com>
2020-03-29 10:25:01 +0530

    Update activations.py
    added usage example for softplus activation

--- tensorflow/python/keras/activations.py
+++ tensorflow/python/keras/activations.py
@@ -159,6 +159,14 @@ def selu(x):
 @keras_export('keras.activations.softplus')
 def softplus(x):
   """Softplus activation function.
+  
+  Example Usage:
+  
+  >>> a = tf.constant([-20, -1.0, 0.0, 1.0, 20], dtype = tf.float32)
+  >>> b = tf.keras.activations.softplus(a) 
+  >>> b.numpy()
+  [2.0611537e-09, 3.1326166e-01, 6.9314718e-01, 1.3132616e+00, 2.0000000e+01]
+  
 
   Arguments:
       x: Input tensor.
