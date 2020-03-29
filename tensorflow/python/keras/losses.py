03073f441377ab846e7802c5d6e8b17f361fb16e
Manish <manisharadwad@gmail.com>
2020-03-29 10:34:20 +0530

    Edited docstring example

--- tensorflow/python/keras/losses.py
+++ tensorflow/python/keras/losses.py
@@ -1693,8 +1693,8 @@ def cosine_similarity(y_true, y_pred, axis=-1):
 
   Usage:
 
-  >>> y_true = [[0., 1.], [1., 1.]]
-  >>> y_pred =[[1., 0.], [1., 1.]]
+  >>> y_true = [[0., 1.], [1., 1.], [1., 1.]]
+  >>> y_pred = [[1., 0.], [1., 1.], [-1., -1.]]
   >>> loss = tf.keras.losses.cosine_similarity(y_true, y_pred, axis=1)
   >>> # l2_norm(y_true) = [[0., 1.], [1./1.414], 1./1.414]]]
   >>> # l2_norm(y_pred) = [[1., 0.], [1./1.414], 1./1.414]]]
@@ -1702,7 +1702,7 @@ def cosine_similarity(y_true, y_pred, axis=-1):
   >>> # loss = -sum(l2_norm(y_true) . l2_norm(y_pred), axis=1)
   >>> #       = -[0. + 0., 0.5 + 0.5]
   >>> loss.numpy()
-  array([-0., -0.999], dtype=float32)
+  array([-0., -0.999, 0.999], dtype=float32)
 
   Args:
     y_true: Tensor of true targets.
