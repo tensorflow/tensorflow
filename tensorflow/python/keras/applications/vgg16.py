6b40dc713650c4c06a8111741f8d5319f4efc1f9
Byambaa <Byambaa0325@gmail.com>
2020-03-30 19:57:10 +0800

    Add Examples to vgg16

--- tensorflow/python/keras/applications/vgg16.py
+++ tensorflow/python/keras/applications/vgg16.py
@@ -98,6 +98,33 @@ def VGG16(
   Returns:
     A `keras.Model` instance.
 
+  Example:
+
+  ```python
+  #Extract image features with VGG16
+  from keras.applications.vgg16 import VGG16
+  from keras.preprocessing import image
+  from keras.applications.vgg16 import preprocess_input
+  import numpy as np
+
+  #create a VGG16 model pre-trained on imagenet
+  model = VGG16(weights='imagenet', include_top=False)
+
+  #process the input
+  img_path = 'elephant_example.jpg'
+  img = image.load_img(img_path, target_size=(224, 224))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  x = preprocess_input(x)
+
+  #extract the features
+  features = model.predict(x)
+  ```
+
+  >>>model = VGG16(weights = None)
+  >>>model.name
+  'vgg16'
+
   Raises:
     ValueError: in case of invalid argument for `weights`,
       or invalid input shape.
