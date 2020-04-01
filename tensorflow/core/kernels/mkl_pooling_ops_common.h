f5aad2df13d54c498623458bdd24e46060a8d339
Niranjan Hasabnis <niranjan.hasabnis@intel.com>
2020-04-01 10:54:55 -0700

    [Intel MKL] Fix incorrect DNNL1.2 integration in pooling backprop
    This PR removes the performance degradation caused by incorrect pooling
    backprop integration in Intel-MKL CPU backend.

--- tensorflow/core/kernels/mkl_pooling_ops_common.h
+++ tensorflow/core/kernels/mkl_pooling_ops_common.h
@@ -49,12 +49,18 @@ struct MklPoolingParams {
   mkldnn::prop_kind prop_kind;
   MEMORY_FORMAT src_format;
   memory::desc src_md;
+  memory::desc diff_dst_md;
 
   MklPoolingParams(memory::dims src_dims, memory::dims dst_dims,
                    memory::dims filter_dims, memory::dims strides,
                    memory::dims padding_left, memory::dims padding_right,
                    mkldnn::algorithm alg_kind, mkldnn::prop_kind prop_kind,
+#ifdef ENABLE_MKLDNN_V1
+                   MEMORY_FORMAT src_format, memory::desc src_md,
+                   memory::desc diff_dst_md = memory::desc())
+#else
                    MEMORY_FORMAT src_format, memory::desc src_md)
+#endif  // ENABLE_MKLDNN_V1
       : src_dims(src_dims),
         dst_dims(dst_dims),
         filter_dims(filter_dims),
@@ -64,7 +70,9 @@ struct MklPoolingParams {
         alg_kind(alg_kind),
         prop_kind(prop_kind),
         src_format(src_format),
-        src_md(src_md) {}
+        src_md(src_md),
+        diff_dst_md(diff_dst_md) {
+  }
 };
 
 template <typename T>
