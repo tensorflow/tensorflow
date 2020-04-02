1286b957c21eea2e956a51fcd758653ed7e7cb36
Francois Chollet <fchollet@google.com>
2020-04-01 18:14:17 -0700

    Small fixes in model.save / load_model docstrings.
    PiperOrigin-RevId: 304301887
    Change-Id: I71a1654ca3efa7b92b1cff6ea0e6177d7d2686a0

--- tensorflow/python/keras/callbacks.py
+++ tensorflow/python/keras/callbacks.py
@@ -552,28 +552,14 @@ class Callback(object):
           (eg. verbosity, batch size, number of epochs...).
       model: instance of `keras.models.Model`.
           Reference of the model being trained.
-      validation_data: Deprecated. Do not use.
 
   The `logs` dictionary that callback methods
   take as argument will contain keys for quantities relevant to
-  the current batch or epoch.
-
-  Currently, the `.fit()` method of the `Model` class
-  will include the following quantities in the `logs` that
-  it passes to its callbacks:
-
-      on_epoch_end: logs include `acc` and `loss`, and
-          optionally include `val_loss`
-          (if validation is enabled in `fit`), and `val_acc`
-          (if validation and accuracy monitoring are enabled).
-      on_batch_begin: logs include `size`,
-          the number of samples in the current batch.
-      on_batch_end: logs include `loss`, and optionally `acc`
-          (if accuracy monitoring is enabled).
+  the current batch or epoch (see method-specific docstrings).
   """
 
   def __init__(self):
-    self.validation_data = None
+    self.validation_data = None  # pylint: disable=g-missing-from-attributes
     self.model = None
     # Whether this Callback should only run on the chief worker in a
     # Multi-Worker setting.
