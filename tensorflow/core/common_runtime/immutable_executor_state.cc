1d6944b94524621c15bc3e5942651fbf74dcbaa6
Derek Murray <mrry@google.com>
2020-04-04 12:29:32 -0700

    Rolling forward "[SimplePropagatorState] Use atomic pending counts for each node in the graph."
    The previous code was correct, but failed during TSAN tests because of a limitation in TSAN's happens-before relation. To address this problem, we conditionally add a read of the atomic pending count in `GetInputTensors()`, which establishes that the write to `input_tensors_` in `PropagateOutputs()` happens before the read in `PrepareInputs()`.
    
    PiperOrigin-RevId: 304810349
    Change-Id: I9058c55898079cb3ab6011513b4468ddb293f59f

--- tensorflow/core/common_runtime/immutable_executor_state.cc
+++ tensorflow/core/common_runtime/immutable_executor_state.cc
@@ -327,6 +327,13 @@ void ImmutableExecutorState::InitializePending(const Graph* graph,
     finfo->pending_counts =
         absl::make_unique<PendingCounts>(finfo->pending_counts_layout);
   }
+
+  if (!requires_control_flow_) {
+    atomic_pending_counts_.reset(new std::atomic<int32>[gview_.num_nodes()]);
+    std::fill(atomic_pending_counts_.get(),
+              atomic_pending_counts_.get() + gview_.num_nodes(), 0);
+  }
+
   for (const Node* n : graph->nodes()) {
     if (IsSink(n)) continue;
     const int id = n->id();
@@ -335,6 +342,9 @@ void ImmutableExecutorState::InitializePending(const Graph* graph,
     GetMaxPendingCounts(n, &max_pending, &max_dead);
     auto& counts = EnsureFrameInfo(name)->pending_counts;
     counts->set_initial_count(pending_ids_[id], max_pending);
+    if (!requires_control_flow_) {
+      atomic_pending_counts_[id] = max_pending;
+    }
   }
 }
 }  // namespace tensorflow
