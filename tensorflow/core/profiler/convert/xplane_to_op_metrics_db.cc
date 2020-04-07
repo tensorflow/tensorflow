d5ce0d8a5dadc31b2d92e6584d035c6c2db63741
A. Unique TensorFlower <gardener@tensorflow.org>
2020-04-06 17:28:58 -0700

    remove callsite where str_value is directly used in case that it use ref_value. also don't compare XStat::metadata_id() which is not an invariant.
    PiperOrigin-RevId: 305153479
    Change-Id: I62e35cf3df603a7fa8870a68ed1914fda7f79614

--- tensorflow/core/profiler/convert/xplane_to_op_metrics_db.cc
+++ tensorflow/core/profiler/convert/xplane_to_op_metrics_db.cc
@@ -208,19 +208,21 @@ OpMetricsDb ConvertDeviceTraceXPlaneToOpMetricsDb(
       first_op_offset_ps = std::min(first_op_offset_ps, event.OffsetPs());
       last_op_offset_ps = std::max(last_op_offset_ps, event.EndOffsetPs());
 
-      const XStat* stat = event.GetStats(StatType::kLevel0);
-      if (!stat) return;
-      absl::string_view tf_op_fullname = stat->str_value();
-      if (tf_op_fullname.empty()) return;
-      TfOp tf_op = ParseTfOpFullname(tf_op_fullname);
-      TfOpRoofLineCostEstimator::OpRoofLineStats costs;
-      if (tf_op.category != Category::kUnknown) {
-        costs = op_level_cost_estimator.Predict(event);
-      }
-      device_op_metrics_db_builder.EnterOp(
-          /*program_id=*/0, tf_op.name, tf_op.type, tf_op_fullname,
-          /*occurrences=*/1, event.DurationPs(),
-          /*children_time_ps=*/0, costs.flops, costs.bytes_accessed);
+      event.ForEachStat([&](const XStatVisitor& stat) {
+        if (stat.Type() == StatType::kLevel0) {
+          auto tf_op_fullname = stat.ToString();
+          if (tf_op_fullname.empty()) return;
+          TfOp tf_op = ParseTfOpFullname(tf_op_fullname);
+          TfOpRoofLineCostEstimator::OpRoofLineStats costs;
+          if (tf_op.category != Category::kUnknown) {
+            costs = op_level_cost_estimator.Predict(event);
+          }
+          device_op_metrics_db_builder.EnterOp(
+              /*program_id=*/0, tf_op.name, tf_op.type, tf_op_fullname,
+              /*occurrences=*/1, event.DurationPs(),
+              /*children_time_ps=*/0, costs.flops, costs.bytes_accessed);
+        }
+      });
     });
   });
   result.set_total_time_ps(last_op_offset_ps - first_op_offset_ps);
