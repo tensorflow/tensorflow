cc08b5dff7346b2e2cd9fb80409c8f09bfebc089
Jens Elofsson <jens.elofsson@arm.com>
2020-04-07 11:20:02 +0200

    Update network_tester.
    New features:
    Multiple inputs
    Multiple outputs
    Output in json format
    Can call invoke() more than once
    
    Updated README

--- tensorflow/lite/micro/examples/network_tester/network_model.h
+++ tensorflow/lite/micro/examples/network_tester/network_model.h
@@ -1,8 +1,11 @@
 /* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -64,4 +67,4 @@ const unsigned char network_model[] = {
     0x08, 0x00, 0x07, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11};
 const unsigned int network_model_len = 576;
 
-#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_NETWORK_TESTER_NETWORK_MODEL_H_
+#endif
