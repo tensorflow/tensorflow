ROCmSoftwarePlatform/tensorflow-upstream

Updating the convolution kernel implementation(s) to ensure that the AlgorithmConfig::scratch_size field is always populated correctly before it is passed as an argument to the ThenConvolve* routine(s)

tensorflow/core/kernels/gpu_utils.cc
@@ -238,6 +238,7 @@ Status BestCudnnConvAlgorithm(absl::Span<const AutotuneResult> results,
 
   algo->set_algorithm({best_result->conv().algorithm(),
                        best_result->conv().tensor_ops_enabled()});
+  algo->set_scratch_size(best_result->scratch_bytes());
   if (best_result_no_scratch != filtered_results.end() &&
       best_result_no_scratch->scratch_bytes() == 0) {
     algo->set_algorithm_no_scratch(

tensorflow/tensorflow

tensorflow/core/kernels/gpu_utils.cc
@@ -270,6 +270,7 @@ Status BestCudnnConvAlgorithm(absl::Span<const AutotuneResult> results,
 
   algo->set_algorithm({selected_result->conv().algorithm(),
                        selected_result->conv().tensor_ops_enabled()});
+  algo->set_scratch_size(selected_result->scratch_bytes());
   if (selected_result_no_scratch != filtered_results_no_scratch.end()) {
     algo->set_algorithm_no_scratch(
         {selected_result_no_scratch->conv().algorithm(),