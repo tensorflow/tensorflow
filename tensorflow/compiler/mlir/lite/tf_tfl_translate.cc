1734e0c64e2554182f1a87a7421daf00d37a7986
A. Unique TensorFlower <gardener@tensorflow.org>
2020-04-09 18:23:28 -0700

    Add missing #include <iostream>
    PiperOrigin-RevId: 305801895
    Change-Id: I5927fdb927e8d54559c4372add8046b9e5cf0d7d

--- tensorflow/compiler/mlir/lite/tf_tfl_translate.cc
+++ tensorflow/compiler/mlir/lite/tf_tfl_translate.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <iostream>
+
 #include "absl/strings/str_split.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/Support/CommandLine.h"
