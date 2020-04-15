bde5a92016d1ca1c928a4bcf1ae4dc9f9c9be622
Benoit Jacob <benoitjacob@google.com>
2020-04-15 12:45:19 -0700

    Re-add a superfluous #include that was removed by cl/306682916 but that many existing files were relying on, until we can fix all of these.
    PiperOrigin-RevId: 306700172
    Change-Id: I89c563deab0062a8d36c16c9f6a6da3c53b65150

--- tensorflow/lite/kernels/internal/optimized/cpu_check.h
+++ tensorflow/lite/kernels/internal/optimized/cpu_check.h
@@ -17,6 +17,12 @@ limitations under the License.
 
 #include "ruy/detect_arm.h"  // from @ruy
 
+// This include is superfluous. However, it's been here for a while, and a
+// number of files have been relying on it to include neon_check.h for them.
+// This should be removed, but with a global run of presubmits to catch
+// any such issues. This requires running more than just TFLite presubmits.
+#include "tensorflow/lite/kernels/internal/optimized/neon_check.h"
+
 namespace tflite {
 
 struct CpuFlags {
