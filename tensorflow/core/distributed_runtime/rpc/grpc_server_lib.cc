966ed1cafc770e81e6a56be3f5715e0fe257b742
Fei Sun <sunfei.china@gmail.com>
2020-04-16 18:41:20 +0800

    Use provided host name/ip instead of localhost if possible

--- tensorflow/core/distributed_runtime/rpc/grpc_server_lib.cc
+++ tensorflow/core/distributed_runtime/rpc/grpc_server_lib.cc
@@ -132,8 +132,9 @@ GrpcServer::~GrpcServer() {
 void GrpcServer::MaybeMutateBuilder(::grpc::ServerBuilder* builder) {}
 
 // Look up the port that has been requested for this task in `server_def`.
-Status GrpcServer::GetPort(const ServerDef& server_def, int* port) const {
+Status GrpcServer::GetHostAndPort(const ServerDef& server_def, string* host_name, int* port) const {
   *port = -1;
+  *host_name = "localhost";
   for (const auto& job : server_def.cluster().job()) {
     if (job.name() == server_def.job_name()) {
       auto iter = job.tasks().find(server_def.task_index());
@@ -152,6 +153,10 @@ Status GrpcServer::GetPort(const ServerDef& server_def, int* port) const {
           return errors::InvalidArgument(
               "Could not parse port for local server from \"", iter->second,
               "\".");
+        }
+
+        if (colon_index != string::npos && !iter->second.substr(0, colon_index).empty()) {
+          *host_name = iter->second.substr(0, colon_index);
         }
       }
       break;
@@ -175,7 +180,9 @@ Status GrpcServer::Init(const GrpcServerOptions& opts) {
   // otherwise if 'task_index=-1' the program will abort.
 
   int requested_port;
-  TF_RETURN_IF_ERROR(GetPort(server_def_, &requested_port));
+  string host_name;
+  TF_RETURN_IF_ERROR(GetHostAndPort(server_def_, &host_name, &requested_port));
+  host_name_ = host_name;
 
   SessionOptions sess_opts;
   ConfigProto config = server_def_.default_session_config();
@@ -325,7 +332,7 @@ Status GrpcServer::ParseChannelSpec(const WorkerCacheFactoryOptions& options,
                                        task.second);
       }
       if (job.name() == *options.job_name && task.first == options.task_index) {
-        host_port = strings::StrCat("localhost:", bound_port_);
+        host_port = strings::StrCat(host_name_, ":", bound_port_);
       } else {
         host_port = task.second;
       }
@@ -478,7 +485,7 @@ Status GrpcServer::Join() {
 }
 
 const string GrpcServer::target() const {
-  return strings::StrCat("grpc://localhost:", bound_port_);
+  return strings::StrCat("grpc://", host_name_, ":", bound_port_);
 }
 
 std::shared_ptr<::grpc::ServerCredentials> GrpcServer::GetServerCredentials(
