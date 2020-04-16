1228f4e49249c2b33dc4f6088fb4aa1bf6d3895f
A. Unique TensorFlower <gardener@tensorflow.org>
2020-04-16 11:33:22 -0700

    Fixes an asan error in spacetobatch_op_test_cpu
    PiperOrigin-RevId: 306888134
    Change-Id: I2c915ba4b57a12ef79ecbd401b23134561eee333

--- tensorflow/core/ops/array_ops.cc
+++ tensorflow/core/ops/array_ops.cc
@@ -2164,8 +2164,10 @@ namespace {
 template <typename InputType>
 std::vector<int64> GetFlatInt64(const Tensor& t) {
   std::vector<int64> output(t.shape().num_elements());
-  auto eigen_vec = t.flat<InputType>();
-  std::copy_n(&eigen_vec(0), output.size(), output.begin());
+  if (t.shape().num_elements() > 0) {
+    auto eigen_vec = t.flat<InputType>();
+    std::copy_n(&eigen_vec(0), output.size(), output.begin());
+  }
   return output;
 }
 
