482e77f3cd81cd9d7c8427831a824369134066d5
Tom Hennigan <tomhennigan@google.com>
2020-04-20 04:42:07 -0700

    Extend special casing of NumPy arrays to anything supporting __array__.
    If a non-tf class (np.ndarray subclass or not) has the __array__ method it is
    used in combination with tf.constant to convert the input array to a tf.Tensor
    before the users function or a cached graph function is called.
    
    Note to match current tested behavior I have special cased instances of np.str_
    to *not* be automatically converted to tensor.
    
    PiperOrigin-RevId: 307375562
    Change-Id: I9a84efc1dfdbb916c9894f138739e9b7e16f757e

--- tensorflow/python/eager/function_test.py
+++ tensorflow/python/eager/function_test.py
@@ -762,11 +762,34 @@ class FunctionTest(test.TestCase, parameterized.TestCase):
     # shouldn't trigger another function definition.
     self.assertLen(total_function_cache(defined), 1)
 
+    np_ones = numpy.ones([], numpy.float32)
+    np_zeros = numpy.zeros([], numpy.float32)
+    tf_ones = array_ops.ones([])
+    tf_zeros = array_ops.zeros([])
+
     # Test that the numpy array is properly an argument to the graph function.
-    self.assertEqual(1., defined(numpy.ones([])).numpy())
-    self.assertEqual(0., defined(numpy.zeros([])).numpy())
-    self.assertEqual(1., defined(array_ops.ones([])).numpy())
-    self.assertEqual(0., defined(array_ops.zeros([])).numpy())
+    self.assertEqual(1., defined(np_ones).numpy())
+    self.assertLen(total_function_cache(defined), 2)
+    self.assertEqual(0., defined(np_zeros).numpy())
+    self.assertEqual(1., defined(tf_ones).numpy())
+    self.assertEqual(0., defined(tf_zeros).numpy())
+    self.assertLen(total_function_cache(defined), 2)
+
+    # Test that mutable inputs are supported.
+    mutable = numpy.ones([], numpy.float32)
+    self.assertEqual(1., defined(mutable).numpy())
+    mutable.fill(0)
+    self.assertEqual(0., defined(mutable).numpy())
+
+    class MyNdarray(numpy.ndarray):
+      pass
+
+     # Test that the subclasses of ndarray are converted too.
+    self.assertEqual(1., defined(np_ones.view(MyNdarray)).numpy())
+    self.assertEqual(0., defined(np_zeros.view(MyNdarray)).numpy())
+
+    # We should not have triggered any re-tracing of the python function.
+    self.assertLen(total_function_cache(defined), 2)
 
   def testDefunNumpyArraysConvertedToTensorsInKwargs(self):
 
