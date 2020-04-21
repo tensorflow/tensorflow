e97fdb5539becc7425e3e72c666c80454bd3a83a
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2020-04-20 18:57:26 -0700

    Changing TF_TRT_BLACKLIST_OP to TF_TRT_BLACKLIST_OPS

--- tensorflow/compiler/tf2tensorrt/segment/segment.cc
+++ tensorflow/compiler/tf2tensorrt/segment/segment.cc
@@ -436,7 +436,7 @@ Status SegmentGraph(const Graph* tf_graph,
   // Getting the nodes blacklisted for conversion
   string tftrt_node_blacklist_str;
   TF_CHECK_OK(ReadStringFromEnvVar(
-    "TF_TRT_BLACKLIST_OP", "", &tftrt_node_blacklist_str
+    "TF_TRT_BLACKLIST_OPS", "", &tftrt_node_blacklist_str
   ));
 
   auto tftrt_node_blacklist = gtl::FlatSet<string>{};
@@ -472,7 +472,7 @@ Status SegmentGraph(const Graph* tf_graph,
         LOG(WARNING) << "Blacklisted as TF-TRT candidate, "
                 << "(Op type: " << node->tf_node()->type_string() << "), "
                 << "(Op name: " << node->name() << "), "
-                << "(Reason: Blacklisted with the env var TF_TRT_BLACKLIST_OP)";
+                << "(Reason: Blacklisted with the env var TF_TRT_BLACKLIST_OPS)";
         unsupported_ops.emplace(node->tf_node()->type_string());
         num_unsupported_ops++;
         node = nullptr;
