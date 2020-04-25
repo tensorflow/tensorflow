2a4de77fac13a11b00131736b683292b37bfc094
Eugene Zhulenev <ezhulenev@google.com>
2020-04-24 23:59:13 -0700

    [Grappler] Do not rewrite nodes in a feed set in arithmetic and function optimizers
    PiperOrigin-RevId: 308388362
    Change-Id: I40c88b26b5f30f4e39540476c539a37605b53542

--- tensorflow/core/grappler/optimizers/arithmetic_optimizer.cc
+++ tensorflow/core/grappler/optimizers/arithmetic_optimizer.cc
@@ -1273,7 +1273,7 @@ class RemoveRedundantBitcastStage : public ArithmeticOptimizerStage {
     TF_RETURN_IF_ERROR(GetNodeAttr(attrs, "T", &input_type));
     DataType output_type;
     TF_RETURN_IF_ERROR(GetNodeAttr(attrs, "type", &output_type));
-    if (input_type == output_type) {
+    if ((input_type == output_type) && !IsInPreserveSet(*node)) {
       *simplified_node_name = node->input(0);
       return Status::OK();
     }
@@ -1283,7 +1283,7 @@ class RemoveRedundantBitcastStage : public ArithmeticOptimizerStage {
     NodeDef* operand;
     TF_RETURN_IF_ERROR(GetInputNode(node->input(0), &operand));
 
-    if (IsBitcast(*operand)) {
+    if (IsBitcast(*operand) && !IsInPreserveSet(*operand)) {
       AttrSlice operand_attrs(*operand);
       DataType operand_input_type;
       TF_RETURN_IF_ERROR(GetNodeAttr(operand_attrs, "T", &operand_input_type));
@@ -2007,6 +2007,7 @@ class ReorderCastLikeAndValuePreserving : public ArithmeticOptimizerStage {
         ((producer_is_cast && IsValuePreserving(*consumer)) ||
          (IsValuePreserving(*producer) && IsCastLike(*consumer)));
     if (!can_optimize || IsControlFlow(*producer) ||
+        IsInPreserveSet(*producer) ||
         producer->device() != consumer->device()) {
       return Status::OK();
     }
