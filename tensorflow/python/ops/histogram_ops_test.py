30adc877170ce07ddd78648484b329292030a1bb
Yong Tang <yong.tang.github@outlook.com>
2020-04-25 22:03:47 +0000

    Add test case for exception handling in `tf.histogram_fixed_width_bins`
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/histogram_ops_test.py
+++ tensorflow/python/ops/histogram_ops_test.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import numpy as np
 
 from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import errors_impl
 from tensorflow.python.framework import test_util
 from tensorflow.python.framework import constant_op
 from tensorflow.python.ops import array_ops
@@ -78,6 +79,16 @@ class BinValuesFixedWidth(test.TestCase):
           values, value_range, nbins=5)
       self.assertEqual(dtypes.int32, bins.dtype)
       self.assertAllClose(expected_bins, self.evaluate(bins))
+
+  def test_range_overlap(self):
+    # GitHub issue 29661
+    value_range = np.float32([0.0, 0.0])
+    values = np.float32([-1.0, 0.0, 1.5, 2.0, 5.0, 15])
+    expected_bins = [0, 0, 4, 4, 4, 4]
+    with self.assertRaises(errors_impl.InvalidArgumentError):
+      with self.cached_session():
+        _ = histogram_ops.histogram_fixed_width_bins(
+            values, value_range, nbins=5)
 
 
 class HistogramFixedWidthTest(test.TestCase):
