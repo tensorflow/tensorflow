3ee0a7150fc1e02fcf91beb82a1fc4d20abed869
Deven Desai <deven.desai.amd@gmail.com>
2020-04-28 22:29:08 +0000

    dumping the event correlation id in the OnEventsDropped message that gets output when an activity record event shows up out of sequence

--- tensorflow/core/profiler/internal/gpu/device_tracer_rocm.cc
+++ tensorflow/core/profiler/internal/gpu/device_tracer_rocm.cc
@@ -167,7 +167,8 @@ class RocmTraceCollectorImpl : public profiler::RocmTraceCollector {
           // the activity records, then they will show up during the subsequent
           // call to enable, and we will end up here!
           OnEventsDropped(
-              "Activity event encountered before a corresponding API event", 1);
+              "Activity event encountered before a corresponding API event",
+              event.correlation_id);
           break;
       }
     }
