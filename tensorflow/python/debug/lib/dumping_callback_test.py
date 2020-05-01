71336da702d4f39ef23cd94f2476b7462a87739f
Shanqing Cai <cais@google.com>
2020-05-01 08:58:04 -0700

    [tfdbg2] Fix debugger-generated names for Placeholder and Const ops
    to facilitate input tracing in the parts of a Graph that contains those ops.
    
    1. tfdbg2's dumping callback renames Const and
       Placeholder/PlaceholderWithDefault ops to avoid duplicate (e.g., None)
       op names in the same graph. It turns out a simpler way to solve it is to
       use the output tensor name of those ops, which are not duplicate in a graph.
    2. tfdbg2 adds Identity ops in the graph under the v1 graph mode. To allow
       tracing through these Identity ops, we create a new dict called
       `_tensor_aliases` to map the names of those debugger-generated Identity ops
       to the names of the original instrumented graph tensors.
    
    PiperOrigin-RevId: 309414953
    Change-Id: I73c086e8faba849562142a6a3d246938ebd25811

--- tensorflow/python/debug/lib/dumping_callback_test.py
+++ tensorflow/python/debug/lib/dumping_callback_test.py
@@ -1386,6 +1386,51 @@ class DumpingCallbackTest(
       # The Mul and Sub ops are from the same innermost context.
       self.assertEqual(mul_op_digest.graph_id, sub_op_digest.graph_id)
 
+  @parameterized.named_parameters(
+      ("NoTensor", "NO_TENSOR"),
+      ("Shape", "SHAPE"),
+      ("FullTensor", "FULL_TENSOR"),
+  )
+  @test_util.run_in_graph_and_eager_modes
+  def testGraphInputTracingWorksWithConstAndPlaceholderTensors(
+      self, tensor_debug_mode):
+    writer = dumping_callback.enable_dump_debug_info(
+        self.dump_root, tensor_debug_mode=tensor_debug_mode)
+
+    @def_function.function
+    def func(x):
+      return (x + constant_op.constant(4.0)) / x
+
+    x = constant_op.constant(2.0)
+    self.assertAllClose(self.evaluate(func(x)), 3.0)
+    writer.FlushNonExecutionFiles()
+    writer.FlushExecutionFiles()
+
+    with debug_events_reader.DebugDataReader(self.dump_root) as reader:
+      reader.update()
+      graph_op_digests = reader.graph_op_digests()
+      placeholder_op_name = None
+      const_op_name = None
+      add_op_name = None
+      div_op_name = None
+      for op_digest in graph_op_digests:
+        if op_digest.op_type == "Placeholder":
+          placeholder_op_name = op_digest.op_name
+        elif op_digest.op_type == "Const":
+          const_op_name = op_digest.op_name
+        elif op_digest.op_type == "AddV2":
+          add_op_name = op_digest.op_name
+          self.assertLen(op_digest.input_names, 2)
+          self.assertEqual(op_digest.input_names[0], placeholder_op_name + ":0")
+          self.assertEqual(op_digest.input_names[1], const_op_name + ":0")
+        elif op_digest.op_type == "RealDiv":
+          div_op_name = op_digest
+          self.assertLen(op_digest.input_names, 2)
+          self.assertEqual(op_digest.input_names[0], add_op_name + ":0")
+          self.assertEqual(op_digest.input_names[1], placeholder_op_name + ":0")
+      self.assertTrue(add_op_name)
+      self.assertTrue(div_op_name)
+
 
 if __name__ == "__main__":
   ops.enable_eager_execution()
