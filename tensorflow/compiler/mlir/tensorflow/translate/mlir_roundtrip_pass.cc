3993d8c6e22cfd666d411e63e009628e8b4dca52
A. Unique TensorFlower <gardener@tensorflow.org>
2020-05-03 03:34:54 -0700

    Integrate LLVM at https://github.com/llvm/llvm-project/commit/57818885be51
    PiperOrigin-RevId: 309620434
    Change-Id: Iba11b8e7edd02c1152399cbbba2b541fa3667140

--- tensorflow/compiler/mlir/tensorflow/translate/mlir_roundtrip_pass.cc
+++ tensorflow/compiler/mlir/tensorflow/translate/mlir_roundtrip_pass.cc
@@ -15,9 +15,9 @@ limitations under the License.
 
 #include "tensorflow/compiler/mlir/tensorflow/translate/mlir_roundtrip_pass.h"
 
-#include "mlir/Analysis/Verifier.h"  // from @llvm-project
 #include "mlir/IR/MLIRContext.h"  // from @llvm-project
 #include "mlir/IR/Module.h"  // from @llvm-project
+#include "mlir/IR/Verifier.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/tensorflow/translate/export_graphdef.h"
 #include "tensorflow/compiler/mlir/tensorflow/translate/import_model.h"
 #include "tensorflow/compiler/mlir/tensorflow/translate/mlir_roundtrip_flags.h"
