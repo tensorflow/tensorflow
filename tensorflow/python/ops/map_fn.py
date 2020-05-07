ad4323e93479caf12a11b6e089174be2a7ec7462
Yong Tang <yong.tang.github@outlook.com>
2020-05-07 17:34:50 +0000

    Update tf.map_fn to specify that at least one tensor must be present
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    Typo fix
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/ops/map_fn.py
+++ tensorflow/python/ops/map_fn.py
@@ -267,7 +267,7 @@ def map_fn(fn,
     elems: A tensor or (possibly nested) sequence of tensors, each of which will
       be unstacked along their first dimension.  `fn` will be applied to the
       nested sequence of the resulting slices.  `elems` may include ragged and
-      sparse tensors.
+      sparse tensors. `elems` must consist of at least one tensor.
     dtype: Deprecated: Equivalent to `fn_output_signature`.
     parallel_iterations: (optional) The number of iterations allowed to run in
       parallel. When graph building, the default value is 10. While executing
@@ -296,7 +296,7 @@ def map_fn(fn,
     TypeError: if `fn` is not callable or the structure of the output of
       `fn` and `fn_output_signature` do not match.
     ValueError: if the lengths of the output of `fn` and `fn_output_signature`
-      do not match.
+      do not match, or if the `elems` does not contain any tensor.
 
   Examples:
 
