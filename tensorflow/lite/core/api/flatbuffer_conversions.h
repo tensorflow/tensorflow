55b36215c8b8b2223b079522938bbdde695bcaf5
Advait Jain <advaitjain@google.com>
2020-05-15 15:59:38 -0700

    include-what-you-use for lite/core/api:api
    PiperOrigin-RevId: 311817318
    Change-Id: If003542599d8901465dcafc605fd4df1d0c65add

--- tensorflow/lite/core/api/flatbuffer_conversions.h
+++ tensorflow/lite/core/api/flatbuffer_conversions.h
@@ -19,9 +19,12 @@ limitations under the License.
 // flatbuffer serialization format into in-memory values that are used by the
 // runtime API and interpreter.
 
+#include <cstddef>
+#include <new>
+#include <type_traits>
+
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/core/api/error_reporter.h"
-#include "tensorflow/lite/core/api/op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
 namespace tflite {
--- tensorflow/lite/core/api/tensor_utils.cc
+++ tensorflow/lite/core/api/tensor_utils.cc
@@ -17,6 +17,8 @@ limitations under the License.
 
 #include <string.h>
 
+#include "tensorflow/lite/c/common.h"
+
 namespace tflite {
 
 TfLiteStatus ResetVariableTensor(TfLiteTensor* tensor) {
