d3cd2a76cc1984b7c1aa6efca74e4e26c359f460
A. Unique TensorFlower <gardener@tensorflow.org>
2020-05-21 11:54:42 -0700

    Internal change
    PiperOrigin-RevId: 312712086
    Change-Id: Iba2311e8ac40ebe73765f273ef48f5550d76fcfc

--- tensorflow/python/kernel_tests/conv_ops_test.py
+++ tensorflow/python/kernel_tests/conv_ops_test.py
@@ -456,58 +456,6 @@ class Conv2DTest(test.TestCase):
         self.evaluate(conv2).reshape(conv1.shape))
 
   @test_util.run_in_graph_and_eager_modes
-  def testConvolutionClass2DExpandedBatch(self):
-    tensor_in_sizes_batch = [10, 2, 3, 3]
-    tensor_in_sizes_expanded_batch = [2, 5, 2, 3, 3]
-    filter_in_sizes = [1, 1, 3, 3]
-    filter_in = self._CreateNumpyTensor(filter_in_sizes)
-    x1 = self._CreateNumpyTensor(tensor_in_sizes_batch)
-    x2 = x1.reshape(tensor_in_sizes_expanded_batch)
-    convolver1 = nn_ops.Convolution(
-        input_shape=x1.shape,
-        filter_shape=filter_in.shape,
-        strides=[1, 1],
-        padding="VALID")
-    self.assertEqual(convolver1.num_batch_dims, 1)
-    convolver2 = nn_ops.Convolution(
-        input_shape=x2.shape,
-        filter_shape=filter_in.shape,
-        strides=[1, 1],
-        padding="VALID")
-    self.assertEqual(convolver2.num_batch_dims, 2)
-    conv1 = convolver1(x1, filter_in)
-    conv2 = convolver2(x2, filter_in)
-    self.assertEqual(conv1.shape, tensor_in_sizes_batch)
-    self.assertEqual(conv2.shape, tensor_in_sizes_expanded_batch)
-    self.assertAllEqual(
-        conv1,
-        self.evaluate(conv2).reshape(conv1.shape))
-
-  @test_util.run_in_graph_and_eager_modes
-  def testConvolutionWith2SpatialDimensionsAndExpandedBatch(self):
-    tensor_in_sizes_batch = [10, 2, 3, 3]
-    tensor_in_sizes_expanded_batch = [2, 5, 2, 3, 3]
-    filter_in_sizes = [1, 1, 3, 3]
-    filter_in = self._CreateNumpyTensor(filter_in_sizes)
-    x1 = self._CreateNumpyTensor(tensor_in_sizes_batch)
-    x2 = x1.reshape(tensor_in_sizes_expanded_batch)
-    conv1 = nn_ops.convolution(
-        x1,
-        filter_in,
-        strides=[1, 1],
-        padding="VALID")
-    conv2 = nn_ops.convolution(
-        x2,
-        filter_in,
-        strides=[1, 1],
-        padding="VALID")
-    self.assertEqual(conv1.shape, tensor_in_sizes_batch)
-    self.assertEqual(conv2.shape, tensor_in_sizes_expanded_batch)
-    self.assertAllEqual(
-        conv1,
-        self.evaluate(conv2).reshape(conv1.shape))
-
-  @test_util.run_in_graph_and_eager_modes
   def testConv2D2x2Filter2x1Dilation(self):
     self._VerifyDilatedConvValues(
         tensor_in_sizes=[1, 4, 4, 1],
