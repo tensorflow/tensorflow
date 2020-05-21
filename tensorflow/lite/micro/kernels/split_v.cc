9ac3c5e5c97c4305f935a5eb35b206eeefb47b1d
yair_ehrenwald <yair.ehrenwald@ceva-dsp.com>
2020-05-21 08:44:10 +0300

    Removed unnecessary #include

--- tensorflow/lite/micro/kernels/split_v.cc
+++ tensorflow/lite/micro/kernels/split_v.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <vector>
+
 
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
