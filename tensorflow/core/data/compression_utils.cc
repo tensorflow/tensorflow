6b715d723897fd1a1f26893143b7e7781d99f42c
Andrew Audibert <aaudibert@google.com>
2020-05-21 18:08:24 -0700

    Remove superfluous tracemes in compression_utils.
    Now that we execute compression as a tensorflow op, a traceme will automatically be generated for the op, so we don't need a second traceme inside the compression util.
    
    PiperOrigin-RevId: 312776419
    Change-Id: I709c89b6a7fafaf41dbc8a64c8c28025cd7cd287

--- tensorflow/core/data/compression_utils.cc
+++ tensorflow/core/data/compression_utils.cc
@@ -17,16 +17,12 @@ limitations under the License.
 #include "tensorflow/core/common_runtime/dma_helper.h"
 #include "tensorflow/core/framework/tensor.pb.h"
 #include "tensorflow/core/platform/snappy.h"
-#include "tensorflow/core/profiler/lib/traceme.h"
 
 namespace tensorflow {
 namespace data {
 
 Status CompressElement(const std::vector<Tensor>& element,
                        CompressedElement* out) {
-  tensorflow::profiler::TraceMe activity(
-      "CompressElement", tensorflow::profiler::TraceMeLevel::kInfo);
-
   // Step 1: Determine the total uncompressed size. This requires serializing
   // non-memcopyable tensors, which we save to use again later.
   std::vector<TensorProto> non_memcpy_components;
@@ -79,8 +75,6 @@ Status CompressElement(const std::vector<Tensor>& element,
 
 Status UncompressElement(const CompressedElement& compressed,
                          std::vector<Tensor>* out) {
-  tensorflow::profiler::TraceMe activity(
-      "UncompressElement", tensorflow::profiler::TraceMeLevel::kInfo);
   int num_components = compressed.component_metadata_size();
   out->clear();
   out->reserve(num_components);
