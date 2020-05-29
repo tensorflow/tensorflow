efa880137ed3e00d7b1178abc95ec58f2108749a
Akshay Modi <nareshmodi@google.com>
2020-05-29 13:47:23 -0700

    Move linspace tests to their own file.
    PiperOrigin-RevId: 313843608
    Change-Id: Ifdae2ac60a721795151124a6a2ee643cb0e527ec

--- tensorflow/python/ops/math_ops_test.py
+++ tensorflow/python/ops/math_ops_test.py
@@ -17,9 +17,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import distutils
-import itertools
-
 import numpy as np
 
 from tensorflow.python.eager import backprop
@@ -822,33 +819,6 @@ class RangeTest(test_util.TensorFlowTestCase):
     tensor = ops.convert_to_tensor(values)
     self.assertAllEqual((5,), tensor.get_shape().as_list())
     self.assertAllEqual(values, self.evaluate(tensor))
-
-
-@test_util.run_all_in_graph_and_eager_modes
-class LinspaceTest(test_util.TensorFlowTestCase):
-
-  def testLinspaceBroadcasts(self):
-    if distutils.version.LooseVersion(
-        np.version.version) < distutils.version.LooseVersion("1.16.0"):
-      self.skipTest("numpy doesn't support axes before version 1.16.0")
-
-    shapes = [(), (2,), (2, 2)]
-
-    types = [np.float64, np.int64]
-
-    for start_shape, stop_shape in itertools.product(shapes, repeat=2):
-      for num in [0, 1, 2, 20]:
-        ndims = max(len(start_shape), len(stop_shape))
-        for axis in range(-ndims, ndims):
-          for dtype in types:
-            start = np.ones(start_shape, dtype)
-            stop = 10 * np.ones(stop_shape, dtype)
-
-            np_ans = np.linspace(start, stop, num, axis=axis)
-            tf_ans = self.evaluate(
-                math_ops.linspace_nd(start, stop, num, axis=axis))
-
-            self.assertAllClose(np_ans, tf_ans)
 
 
 if __name__ == "__main__":
