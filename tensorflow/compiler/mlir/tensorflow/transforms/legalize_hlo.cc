45b2bbc9a32362b36de8cc14e96b5b886c04c179
A. Unique TensorFlower <gardener@tensorflow.org>
2020-05-29 11:37:25 -0700

    [TF/MLIR] Adds legalization from xla_hlo.dot to tf.MatMul.
    PiperOrigin-RevId: 313819546
    Change-Id: I88ed2c455e80cf66bd6d09404b5ae6b3383f186a

--- tensorflow/compiler/mlir/tensorflow/transforms/legalize_hlo.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/legalize_hlo.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <memory>
 #include <vector>
 
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/raw_ostream.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"  // from @llvm-project
 #include "mlir/IR/Attributes.h"  // from @llvm-project
@@ -25,6 +26,7 @@ limitations under the License.
 #include "mlir/IR/Operation.h"  // from @llvm-project
 #include "mlir/IR/PatternMatch.h"  // from @llvm-project
 #include "mlir/IR/StandardTypes.h"  // from @llvm-project
+#include "mlir/IR/Value.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
 #include "mlir/Support/LogicalResult.h"  // from @llvm-project
@@ -104,6 +106,59 @@ ConstantOp ShapeToConst(PatternRewriter &rewriter, Value value) {
   return rewriter.create<ConstantOp>(value.getLoc(), attr_type, attr);
 }
 
+// Converts xla_hlo.dot to tf.MatMul. Reshape ops will be inserted when
+// necessary.
+Value ConvertDotOp(PatternRewriter &rewriter, Operation *old_op) {
+  auto dot_op = cast<xla_hlo::DotOp>(old_op);
+  const mlir::Location loc = dot_op.getLoc();
+  // Normalizes a ShapedType to 2d if the ShapedType is less than 2d by
+  // inserting dummy 1-element dimensions in the begining. Does nothing if the
+  // old shape is already 2d or higher. This is necessary because tf.MatMul
+  // requires input tensors to be at least 2d.
+  const auto normalize_rank = [](ShapedType type) -> ShapedType {
+    if (type.getRank() >= 2) {
+      return type;
+    }
+
+    const int rank = type.getRank();
+    llvm::SmallVector<int64_t, 2> shape_2d(type.getShape().begin(),
+                                           type.getShape().end());
+    for (int i = 0; i < 2 - rank; ++i) {
+      shape_2d.insert(shape_2d.begin(), 1);
+    }
+    return RankedTensorType::get(shape_2d, type.getElementType());
+  };
+
+  // Reshapes a tensor value to 2d if it is 1d or scalar. Otherwise does
+  // nothing.
+  const auto reshape_to_2d = [&rewriter, &loc,
+                              &normalize_rank](mlir::Value input) {
+    const auto input_type = input.getType().cast<ShapedType>();
+    if (input_type.getRank() >= 2) {
+      return input;
+    }
+
+    auto reshape = rewriter.create<xla_hlo::ReshapeOp>(
+        loc, normalize_rank(input_type), input);
+    return reshape.getResult();
+  };
+
+  // Reshapes both operand to be 2d for tf.MatMul op.
+  auto a = reshape_to_2d(dot_op.lhs());
+  auto b = reshape_to_2d(dot_op.rhs());
+  // Operand `b` needs to be transposed if it is 1d. This is because dot op will
+  // contract on the only dimension if rhs is 1d.
+  auto b_old_type = dot_op.rhs().getType().cast<ShapedType>();
+  BoolAttr transpose_b = rewriter.getBoolAttr(b_old_type.getRank() == 1);
+  auto output_type = dot_op.getResult().getType().cast<ShapedType>();
+  auto matmul = rewriter.create<TF::MatMulOp>(
+      loc, normalize_rank(output_type), a, b,
+      /*transpose_a=*/rewriter.getBoolAttr(false), transpose_b);
+  auto reshape =
+      rewriter.create<xla_hlo::ReshapeOp>(loc, output_type, matmul.product());
+  return reshape.getResult();
+}
+
 #include "tensorflow/compiler/mlir/tensorflow/transforms/generated_legalize_hlo.inc"
 
 /// Performs the lowering to XLA dialect.
@@ -118,8 +173,10 @@ void LegalizeHloToTf::runOnFunction() {
   ConversionTarget target(context);
   target.addLegalDialect<TensorFlowDialect>();
   target.addLegalOp<CallOp, ConstantOp>();
-  if (failed(applyPartialConversion(getFunction(), target, patterns)))
+  if (failed(applyPartialConversion(getFunction(), target, patterns))) {
+    getFunction().emitError("xla_hlo to TF legalization failed.");
     signalPassFailure();
+  }
 }
 
 static PassRegistration<LegalizeHloToTf> pass(
