0482d18b52bdd0cda27e84962b487acce7a2e5e0
Anudhyan Boral <anudhyan@google.com>
2020-06-01 23:39:22 -0700

    Mostly internal changes.
    PiperOrigin-RevId: 314275429
    Change-Id: I75d2431bee12a4ebc92b5ab291451ecce805f09a

--- tensorflow/compiler/tests/binary_ops_test.py
+++ tensorflow/compiler/tests/binary_ops_test.py
@@ -1017,7 +1017,8 @@ class BinaryOpsTest(xla_test.XLATestCase):
           math_ops.matmul,
           np.array([[3.1415926535897932]], dtype=dtype),
           np.array([[2.7182818284590452]], dtype=dtype),
-          expected=np.array([[8.5397342226735668]], dtype=dtype))
+          expected=np.array([[8.5397342226735668]], dtype=dtype),
+          rtol=1e-14)
 
       # Edge case with a large range of exponent. Not supported by float16.
       if dtype != np.float16:
@@ -1025,7 +1026,8 @@ class BinaryOpsTest(xla_test.XLATestCase):
             math_ops.matmul,
             np.array([[9.4039548065783000e-38]], dtype=dtype),
             np.array([[4.5070591730234615e37]], dtype=dtype),
-            expected=np.array([[4.2384180773686798]], dtype=dtype))
+            expected=np.array([[4.2384180773686798]], dtype=dtype),
+            rtol=1e-14)
 
   # TODO(phawkins): failing on GPU, no registered kernel.
   def DISABLED_testSparseMatMul(self):
