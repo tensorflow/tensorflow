b79a1ae8d7f78462f84a31a5a6789f603ca930fd
Xiaoquan Kong <u1mail2me@gmail.com>
2020-06-03 14:53:56 +0800

    stylefix: fix code style

--- tensorflow/python/keras/regularizers.py
+++ tensorflow/python/keras/regularizers.py
@@ -34,18 +34,18 @@ def _check_penalty_number(x):
   """check penalty number availability, raise ValueError if failed"""
   if not isinstance(x, (float, int)):
     raise ValueError(
-      (
-          "Value: {} is not a valid regularization penalty number, "
-          "expected an int or float value"
-      ).format(x)
+        (
+            "Value: {} is not a valid regularization penalty number, "
+            "expected an int or float value"
+        ).format(x)
     )
 
   if math.isinf(x) or math.isnan(x):
     raise ValueError(
-      (
-          "Value: {} is not a valid regularization penalty number, "
-          "a positive/negative infinity or NaN is not a property value"
-      ).format(x)
+        (
+            "Value: {} is not a valid regularization penalty number, "
+            "a positive/negative infinity or NaN is not a property value"
+        ).format(x)
     )
 
 
