1b8ff1534a031fab5e19ae8e2b6365ef8b641316
Dan Riedler <dan.riedler@silabs.com>
2020-06-08 10:56:25 -0400

    TF-Lite Micro: port Silicon Labs STK3701A to Micro Speech example

---
+++ tensorflow/lite/micro/examples/micro_speech/silabs/command_responder.cc
@@ -0,0 +1,70 @@
+/***************************************************************************//**
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ ******************************************************************************/
+#include <string.h>
+
+#include "tensorflow/lite/micro/examples/micro_speech/command_responder.h"
+
+
+#include "bsp.h"
+
+
+
+
+
+// The default implementation writes out the name of the recognized command
+// to the error console. Real applications will want to take some custom
+// action instead, and should implement their own versions of this function.
+void RespondToCommand(tflite::ErrorReporter* error_reporter,
+                      int32_t current_time, const char* found_command,
+                      uint8_t score, bool is_new_command) {
+    static bool initialized = false;
+
+    if(!initialized)
+    {
+        initialized = true;
+        BSP_LedsInit();
+    }
+
+   //error_reporter->Report("%s %d", found_command, score);
+
+    if (is_new_command) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Heard %s (%d) @%dms", found_command,
+                         score, current_time);
+
+
+        if(strcmp(found_command, "no") == 0)
+        {
+            BSP_LedSet(0);
+            BSP_LedClear(1);
+        }
+        else if(strcmp(found_command, "yes") == 0)
+        {
+            BSP_LedClear(0);
+            BSP_LedSet(1);
+        }
+  }
+}
