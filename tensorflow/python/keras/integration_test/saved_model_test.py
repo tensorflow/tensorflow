f2306d9f25e0f3987391d42add002613ac7cff79
Scott Zhu <scottzhu@google.com>
2020-06-10 09:17:33 -0700

    Move keras related saved_model test to keras integration test and unit test.
    The memory test couldn't be convert to integration test since the anntation for eager garbage collection is not publicly visible.
    
    PiperOrigin-RevId: 315702765
    Change-Id: I4c8d54b074364d3884af64a2b4d00ad615ef319d

--- tensorflow/python/keras/integration_test/saved_model_test.py
+++ tensorflow/python/keras/integration_test/saved_model_test.py
@@ -16,6 +16,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import tempfile
 
 from absl.testing import parameterized
@@ -38,6 +39,79 @@ def cycle(obj, cycles, signatures=None):
       loaded = tf.saved_model.load(path)
     to_save = loaded
   return loaded
+
+
+class _ModelWithOptimizer(tf.train.Checkpoint):
+
+  def __init__(self):
+    self.dense = tf.keras.layers.Dense(1)
+    self.optimizer = tf.keras.optimizers.Adam(0.01)
+
+  @tf.function(
+      input_signature=(tf.TensorSpec([None, 2], tf.float32),
+                       tf.TensorSpec([None], tf.float32)))
+  def call(self, x, y):
+    with tf.GradientTape() as tape:
+      loss = tf.math.reduce_mean((self.dense(x) - y) ** 2.)
+    trainable_variables = self.dense.trainable_variables
+    gradients = tape.gradient(loss, trainable_variables)
+    self.optimizer.apply_gradients(zip(gradients, trainable_variables))
+    return {"loss": loss}
+
+
+def _import_and_infer(save_dir, inputs, signature_key="serving_default"):
+  """Import a SavedModel into a TF 1.x-style graph and run `signature_key`."""
+  graph = tf.Graph()
+  with graph.as_default(), tf.compat.v1.Session() as session:
+    model = tf.compat.v1.saved_model.load(session, ["serve"], save_dir)
+    return _run_signature(session, model, inputs, signature_key)
+
+
+def _run_signature(session, meta_graph_def, inputs, signature_key):
+  signature = meta_graph_def.signature_def[signature_key]
+  assert set(inputs.keys()) == set(signature.inputs.keys())
+  feed_dict = {}
+  for arg_name in inputs.keys():
+    input_tensor = session.graph.get_tensor_by_name(
+        signature.inputs[arg_name].name)
+    feed_dict[input_tensor] = inputs[arg_name]
+  output_dict = {}
+  for output_name, output_tensor_info in signature.outputs.items():
+    output_dict[output_name] = session.graph.get_tensor_by_name(
+        output_tensor_info.name)
+  return session.run(output_dict, feed_dict=feed_dict)
+
+
+class SaveTest(tf.test.TestCase):
+
+  def test_unbuilt_model_does_not_prevent_saving(self):
+    root = tf.train.Checkpoint(
+        model=tf.keras.Sequential([tf.keras.layers.Dense(2)]))
+    tf.saved_model.save(root, os.path.join(self.get_temp_dir(), "saved_model"))
+
+  def test_optimizer(self):
+    x = tf.constant([[3., 4.]])
+    y = tf.constant([2.])
+    model = _ModelWithOptimizer()
+    first_loss = model.call(x, y)
+    save_dir = os.path.join(self.get_temp_dir(), "saved_model")
+    tf.saved_model.save(model, save_dir, model.call)
+    second_loss = model.call(x, y)
+    self.assertNotEqual(first_loss, second_loss)
+    self.assertAllClose(
+        second_loss,
+        _import_and_infer(save_dir, {"x": [[3., 4.]], "y": [2.]}))
+
+  def test_single_method_default_signature(self):
+    model = _ModelWithOptimizer()
+    x = tf.constant([[3., 4.]])
+    y = tf.constant([2.])
+    model.call(x, y)
+    save_dir = os.path.join(self.get_temp_dir(), "saved_model")
+    tf.saved_model.save(model, save_dir)
+    self.assertIn("loss",
+                  _import_and_infer(save_dir,
+                                    {"x": [[3., 4.]], "y": [2.]}))
 
 
 @parameterized.named_parameters(
