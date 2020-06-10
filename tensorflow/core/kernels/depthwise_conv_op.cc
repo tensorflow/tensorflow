f0322d6c68450b34db932a32da63e11d50996541
Kaixi Hou <kaixih@nvidia.com>
2020-06-10 13:04:20 -0700

    Change comments

--- tensorflow/core/kernels/depthwise_conv_op.cc
+++ tensorflow/core/kernels/depthwise_conv_op.cc
@@ -303,11 +303,15 @@ class DepthwiseConv2dNativeOp : public BinaryOp<T> {
     cudnn_use_autotune_ = CudnnUseAutotune();
     dtype_ = DataTypeToEnum<T>::value;
 #if CUDNN_VERSION >= 8000
-    // See cudnn release note 8.0. (https://docs.nvidia.com/deeplearning/sdk/cud
-    // nn-release-notes/rel_8.html#rel_8)
+    // From the cuDNN release note 8.0: We’ve extended the fprop and dgrad
+    // NHWC depthwise kernels to support more combinations (filter
+    // sizes/strides) such as 5x5/1x1, 5x5/2x2, 7x7/1x1, 7x7/2x2 (in addition
+    // to what we already have, 1x1/1x1, 3x3/1x1, 3x3/2x2), which provides
+    // good performance. (https://docs.nvidia.com/deeplearning/sdk/cudnn-
+    // release-notes/rel_8.html#rel_8)
     use_cudnn_grouped_conv_ = dtype_ == DT_HALF &&
-        (data_format_ == FORMAT_NCHW || data_format_ == FORMAT_NHWC &&
-         stride_ == stride_w && (stride_ == 1 || stride_ == 2));
+        (data_format_ == FORMAT_NCHW || (data_format_ == FORMAT_NHWC &&
+         stride_ == stride_w && (stride_ == 1 || stride_ == 2)));
 #elif CUDNN_VERSION >= 7603
     // Use CuDNN grouped conv only when input/output is NCHW and float16(half).
     // See cudnn release note 7.6.3. (https://docs.nvidia.com/deeplearning/sdk/c
