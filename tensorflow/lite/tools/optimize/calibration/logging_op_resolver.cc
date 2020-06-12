49633b387b531eacd07f60061c5a9b9d603759e9
Jaesung Chung <jaesung@google.com>
2020-06-12 16:45:24 -0700

    Support flex ops in calibration optimization
    This CL makes the tool generate a user-friendly error message as well.
    
    PiperOrigin-RevId: 316201706
    Change-Id: I03f705164db053f3b106d5d5644a983a4bd6b13c

--- tensorflow/lite/tools/optimize/calibration/logging_op_resolver.cc
+++ tensorflow/lite/tools/optimize/calibration/logging_op_resolver.cc
@@ -15,6 +15,10 @@ limitations under the License.
 #include "tensorflow/lite/tools/optimize/calibration/logging_op_resolver.h"
 
 #include "absl/memory/memory.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/lite/util.h"
 
 namespace tflite {
 namespace optimize {
@@ -24,9 +28,17 @@ LoggingOpResolver::LoggingOpResolver(
     const BuiltinOpsSet& builtin_ops_to_replace,
     const CustomOpsSet& custom_ops_to_replace, const OpResolver& base_resolver,
     KernelEvalFuncPtr logging_eval_fn) {
+  std::vector<std::string> unresolved_builtin_ops;
+  std::vector<std::string> unresolved_custom_ops;
+
   for (const auto& op_and_version : builtin_ops_to_replace) {
     const TfLiteRegistration* base_registration =
         base_resolver.FindOp(op_and_version.first, op_and_version.second);
+    if (!base_registration) {
+      unresolved_builtin_ops.push_back(
+          EnumNameBuiltinOperator(op_and_version.first));
+      continue;
+    }
     BuiltinOperatorKey key = op_and_version;
     builtin_op_evalfn_map_[key] = base_registration->invoke;
     auto logging_registration =
@@ -37,12 +49,30 @@ LoggingOpResolver::LoggingOpResolver(
   for (const auto& op_and_version : custom_ops_to_replace) {
     const TfLiteRegistration* base_registration = base_resolver.FindOp(
         op_and_version.first.c_str(), op_and_version.second);
+    if (!base_registration) {
+      if (!IsFlexOp(op_and_version.first.c_str()))
+        unresolved_custom_ops.push_back(op_and_version.first.c_str());
+      continue;
+    }
     CustomOperatorKey key = op_and_version;
     custom_op_evalfn_map_[key] = base_registration->invoke;
     auto logging_registration =
         absl::make_unique<TfLiteRegistration>(*base_registration);
     logging_registration->invoke = logging_eval_fn;
     custom_op_registration_map_[key] = std::move(logging_registration);
+  }
+
+  if (!unresolved_builtin_ops.empty() || !unresolved_custom_ops.empty()) {
+    std::string error_message =
+        "Failed to initialize op resolver for calibration:";
+    if (!unresolved_builtin_ops.empty())
+      absl::StrAppend(&error_message, "\nThere are unresolved builtin ops: [",
+                      absl::StrJoin(unresolved_builtin_ops, ", "), "]");
+    if (!unresolved_custom_ops.empty()) {
+      absl::StrAppend(&error_message, "\nThere are unresolved custom ops: [",
+                      absl::StrJoin(unresolved_builtin_ops, ", "), "]");
+    }
+    LOG(ERROR) << error_message;
   }
 }
 
