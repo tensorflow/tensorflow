52e1dba6b14da82ddd30344526e13557cf33cc32
Tare Gaskin <taregaskin@google.com>
2020-06-16 04:02:50 +0000

    getting rid of pesky lingering commits

--- tensorflow/compiler/mlir/lite/quantization/quantization_driver.cc
+++ tensorflow/compiler/mlir/lite/quantization/quantization_driver.cc
@@ -294,7 +294,7 @@ class QuantizationDriver {
         return;
       if (current_op == op) llvm::errs() << "===>>>";
       llvm::errs() << op->getName() << " : (";
-      for (int i = 0, e = op->getNumOperands(); i < e; ++i) {
+      for (auto i = 0; i < op->getNumOperands(); ++i) {
         if (auto params = GetOperandQuantState(op, i).params)
           params.print(llvm::errs());
         else
@@ -303,7 +303,7 @@ class QuantizationDriver {
         llvm::errs() << ",";
       }
       llvm::errs() << ") -> (";
-      for (int i = 0, e = op->getNumResults(); i < e; ++i) {
+      for (auto i = 0; i < op->getNumResults(); ++i) {
         if (auto params = GetResultQuantState(op, i).params)
           params.print(llvm::errs());
         else
