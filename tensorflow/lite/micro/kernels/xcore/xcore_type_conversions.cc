dad76ba7b74a32f2516183d632c207973d892a12
Keith Moulton <keithm@xmos.com>
2020-06-16 17:01:57 -0400

    added parallel support to xcore_type_conversions

--- tensorflow/lite/micro/kernels/xcore/xcore_type_conversions.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_type_conversions.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+
+#include "lib_ops/api/type_conversions.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
-
-#include "lib_ops/api/type_conversions.h"
+#include "tensorflow/lite/micro/kernels/xcore/xcore_custom_options.h"
 
 namespace tflite {
 namespace ops {
@@ -12,17 +13,28 @@ namespace xcore {
 namespace type_conversions {
 
 void* Init(TfLiteContext* context, const char* buffer, size_t length) {
-  void *data = nullptr;
-  context->AllocatePersistentBuffer(context, sizeof(::xcore::type_conversions::Requantize_16_to_8), &data);
-    ::xcore::type_conversions::Requantize_16_to_8* op =
-      new (data) ::xcore::type_conversions::Requantize_16_to_8();
-  
+  ::xcore::ExecutionPlan execution_plan;
+  if (buffer) parse_custom_options(buffer, length, &execution_plan);
+
+  void* data = nullptr;
+  context->AllocatePersistentBuffer(
+      context, sizeof(::xcore::type_conversions::Requantize_16_to_8), &data);
+  ::xcore::type_conversions::Requantize_16_to_8* op =
+      new (data)::xcore::type_conversions::Requantize_16_to_8(execution_plan);
+
   return op;
 }
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_ENSURE_EQ(context, NumInputs(node), 1);
   TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+  const TfLiteTensor* input = GetInput(context, node, 0);
+  int32_t length = input->bytes / sizeof(int16_t);
+
+  auto* op = reinterpret_cast<::xcore::type_conversions::Requantize_16_to_8*>(
+      node->user_data);
+
+  op->Init(length);
 
   return kTfLiteOk;
 }
@@ -35,7 +47,7 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   auto* op = reinterpret_cast<::xcore::type_conversions::Requantize_16_to_8*>(
       node->user_data);
 
-  op->Eval(output->data.int8, input->data.i16, length);
+  op->Eval(output->data.int8, input->data.i16);
 
   return kTfLiteOk;
 }
