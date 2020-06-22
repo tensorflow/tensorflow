090f88dcb4451aabcf2792b3caf7022e673baccb
A. Unique TensorFlower <gardener@tensorflow.org>
2020-06-22 15:22:24 -0700

    Add tests for code snippets in README.
    PiperOrigin-RevId: 317746384
    Change-Id: I437965917d7d392b906cecd145f87ced4635a592

--- tensorflow/python/ops/numpy_ops/np_interop_test.py
+++ tensorflow/python/ops/numpy_ops/np_interop_test.py
@@ -18,44 +18,73 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 import numpy as onp
+import tensorflow.compat.v2 as tf
+
+import tensorflow.python.ops.numpy_ops as np
 
 
-from tensorflow.python.distribute import distribution_strategy_context
-from tensorflow.python.distribute import mirrored_strategy
-from tensorflow.python.distribute import reduce_util
-from tensorflow.python.eager import backprop
-from tensorflow.python.eager import context
-from tensorflow.python.eager import def_function
-from tensorflow.python.framework import config
-from tensorflow.python.framework import constant_op
-from tensorflow.python.framework import ops
-from tensorflow.python.ops import control_flow_ops
-from tensorflow.python.ops import math_ops
-from tensorflow.python.ops.numpy_ops import np_array_ops
-from tensorflow.python.ops.numpy_ops import np_arrays
-from tensorflow.python.ops.numpy_ops import np_math_ops
-from tensorflow.python.platform import test
+# Tests for code snippet put in README.md
+class ReadmeTest(tf.test.TestCase):
+
+  def testBroadcastAdd(self):
+    x_np = np.ones([2, 1]) + np.ones([1, 2])
+    x_onp = onp.ones([2, 1]) + onp.ones([1, 2])
+    self.assertAllClose(x_onp, x_np)
+
+  def testTypePromotion(self):
+    x_np = np.ones([1, 2], dtype=np.int16) + np.ones([2, 1], dtype=np.uint8)
+    x_onp = np.ones([1, 2], dtype=np.int16) + np.ones([2, 1], dtype=np.uint8)
+    self.assertEqual(x_onp.dtype, x_np.dtype)
+    self.assertAllClose(x_onp, x_np)
+
+  def testTFInterop(self):
+    x_np = np.sum(np.ones([1, 2]) + tf.ones([2, 1]))
+    x_onp = onp.sum(onp.ones([1, 2]) + onp.ones([2, 1]))
+    self.assertAllClose(x_onp, x_np)
+
+  def testOnpInterop(self):
+    x_np = onp.sum(np.ones([1, 2]) + onp.ones([2, 1]))
+    x_onp = onp.sum(onp.ones([1, 2]) + onp.ones([2, 1]))
+    self.assertAllClose(x_onp, x_np)
+
+  def testDevice(self):
+    if tf.test.is_gpu_available():
+      with tf.device('GPU:0'):
+        x = np.ones([1, 2])
+      self.assertIn('GPU', tf.convert_to_tensor(x).device)
+    with tf.device('CPU:0'):
+      x = np.ones([1, 2])
+    self.assertIn('CPU', tf.convert_to_tensor(x).device)
+
+  def testFunction(self):
+
+    @tf.function
+    def f(x, y):
+      return np.sum(x + y)
+
+    x_np = f(np.ones([1, 2]), tf.ones([2, 1]))
+    x_onp = onp.sum(onp.ones([1, 2]) + onp.ones([2, 1]))
+    self.assertAllClose(x_onp, x_np)
 
 
-class InteropTest(test.TestCase):
+class InteropTest(tf.test.TestCase):
 
   def setUp(self):
     super(InteropTest, self).setUp()
-    physical_devices = config.list_physical_devices('CPU')
-    configs = config.get_logical_device_configuration(physical_devices[0])
+    physical_devices = tf.config.list_physical_devices('CPU')
+    configs = tf.config.get_logical_device_configuration(physical_devices[0])
     if configs is None:
       logical_devices = [
-          context.LogicalDeviceConfiguration() for _ in range(3)
+          tf.config.LogicalDeviceConfiguration() for _ in range(3)
       ]
-      config.set_logical_device_configuration(physical_devices[0],
-                                              logical_devices)
+      tf.config.set_logical_device_configuration(physical_devices[0],
+                                                 logical_devices)
 
   def testGradientTapeInterop(self):
-    with backprop.GradientTape() as t:
-      x = np_array_ops.asarray(3.0)
-      y = np_array_ops.asarray(2.0)
+    with tf.GradientTape() as t:
+      x = np.asarray(3.0)
+      y = np.asarray(2.0)
 
       t.watch([x, y])
 
@@ -71,30 +100,30 @@ class InteropTest(test.TestCase):
     self.assertAllClose(dy, 3.0)
 
   def testFunctionInterop(self):
-    x = np_array_ops.asarray(3.0)
-    y = np_array_ops.asarray(2.0)
+    x = np.asarray(3.0)
+    y = np.asarray(2.0)
 
     add = lambda x, y: x + y
-    add_fn = def_function.function(add)
+    add_fn = tf.function(add)
 
     raw_result = add(x, y)
     fn_result = add_fn(x, y)
 
-    self.assertIsInstance(raw_result, np_arrays.ndarray)
-    self.assertIsInstance(fn_result, np_arrays.ndarray)
+    self.assertIsInstance(raw_result, np.ndarray)
+    self.assertIsInstance(fn_result, np.ndarray)
     self.assertAllClose(raw_result, fn_result)
 
   def testCondInterop(self):
-    x = np_array_ops.asarray(3.0)
+    x = np.asarray(3.0)
 
     def fn(x):
-      x_plus_1 = control_flow_ops.cond(x > 0, lambda: x+1, lambda: x+2)
-      x_plus_2 = control_flow_ops.cond(x < 0, lambda: x+1, lambda: x+2)
+      x_plus_1 = tf.cond(x > 0, lambda: x + 1, lambda: x + 2)
+      x_plus_2 = tf.cond(x < 0, lambda: x + 1, lambda: x + 2)
 
       return x_plus_1, x_plus_2
 
     raw_x_plus_1, raw_x_plus_2 = fn(x)
-    fn_x_plus_1, fn_x_plus_2 = def_function.function(fn)(x)
+    fn_x_plus_1, fn_x_plus_2 = tf.function(fn)(x)
 
     self.assertAllClose(raw_x_plus_1, x + 1)
     self.assertAllClose(raw_x_plus_2, x + 2)
@@ -103,44 +132,45 @@ class InteropTest(test.TestCase):
     self.assertAllClose(fn_x_plus_2, x + 2)
 
   def testWhileInterop(self):
+
     def fn():
-      x = np_array_ops.asarray(0)
+      x = np.asarray(0)
       c = lambda x: x < 10000
       b = lambda x: [x + 1]
-      return control_flow_ops.while_loop_v2(c, b, [x], parallel_iterations=20)
+      return tf.while_loop(c, b, [x], parallel_iterations=20)
 
     self.assertEqual(10000, fn()[0])
-    self.assertEqual(10000, def_function.function(fn)()[0])
+    self.assertEqual(10000, tf.function(fn)()[0])
 
   def testTensorTFNPArrayInterop(self):
-    arr = np_array_ops.asarray(0.)
-    t = constant_op.constant(10.)
+    arr = np.asarray(0.)
+    t = tf.constant(10.)
 
     arr_plus_t = arr + t
     t_plus_arr = t + arr
 
-    self.assertIsInstance(arr_plus_t, ops.Tensor)
-    self.assertIsInstance(t_plus_arr, ops.Tensor)
+    self.assertIsInstance(arr_plus_t, tf.Tensor)
+    self.assertIsInstance(t_plus_arr, tf.Tensor)
     self.assertEqual(10., arr_plus_t.numpy())
     self.assertEqual(10., t_plus_arr.numpy())
 
   def testTensorTFNPOp(self):
-    t = constant_op.constant(10.)
+    t = tf.constant(10.)
 
-    sq = np_math_ops.square(t)
-    self.assertIsInstance(sq, np_arrays.ndarray)
+    sq = np.square(t)
+    self.assertIsInstance(sq, np.ndarray)
     self.assertEqual(100., sq)
 
   def testTFNPArrayTFOpInterop(self):
-    arr = np_array_ops.asarray(10.)
+    arr = np.asarray(10.)
 
     # TODO(nareshmodi): Test more ops.
-    sq = math_ops.square(arr)
-    self.assertIsInstance(sq, ops.Tensor)
+    sq = tf.square(arr)
+    self.assertIsInstance(sq, tf.Tensor)
     self.assertEqual(100., sq.numpy())
 
   def testTFNPArrayNPOpInterop(self):
-    arr = np_array_ops.asarray([10.])
+    arr = np.asarray([10.])
 
     # TODO(nareshmodi): Test more ops.
     sq = onp.square(arr)
@@ -149,47 +179,49 @@ class InteropTest(test.TestCase):
 
     # TODO(nareshmodi): Fails since the autopacking code doesn't use
     # nest.flatten.
+
+
 #   def testAutopacking(self):
-#     arr1 = np_array_ops.asarray(1.)
-#     arr2 = np_array_ops.asarray(2.)
-#     arr3 = np_array_ops.asarray(3.)
+#     arr1 = np.asarray(1.)
+#     arr2 = np.asarray(2.)
+#     arr3 = np.asarray(3.)
 #     t = ops.convert_to_tensor_v2([arr1, arr2, arr3])
 
 #     self.assertEqual(t.numpy(), [1., 2., 3.])
 
   def testDistStratInterop(self):
-    strategy = mirrored_strategy.MirroredStrategy(
+    strategy = tf.distribute.MirroredStrategy(
         devices=['CPU:0', 'CPU:1', 'CPU:2'])
 
-    multiplier = np_array_ops.asarray(5.)
+    multiplier = np.asarray(5.)
 
     with strategy.scope():
-      @def_function.function
+
+      @tf.function
       def run():
-        ctx = distribution_strategy_context.get_replica_context()
-        val = np_array_ops.asarray(ctx.replica_id_in_sync_group)
+        ctx = tf.distribute.get_replica_context()
+        val = np.asarray(ctx.replica_id_in_sync_group)
         return val * multiplier
 
       distributed_values = strategy.run(run)
-      reduced = strategy.reduce(reduce_util.ReduceOp.SUM,
-                                distributed_values, axis=None)
+      reduced = strategy.reduce(
+          tf.distribute.ReduceOp.SUM, distributed_values, axis=None)
 
     values = distributed_values.values
 
     # Note that this should match the number of virtual CPUs.
     self.assertLen(values, 3)
-    self.assertIsInstance(values[0], np_arrays.ndarray)
-    self.assertIsInstance(values[1], np_arrays.ndarray)
-    self.assertIsInstance(values[2], np_arrays.ndarray)
+    self.assertIsInstance(values[0], np.ndarray)
+    self.assertIsInstance(values[1], np.ndarray)
+    self.assertIsInstance(values[2], np.ndarray)
     self.assertAllClose(values[0], 0)
     self.assertAllClose(values[1], 5)
     self.assertAllClose(values[2], 10)
 
     # "strategy.reduce" doesn't rewrap in ndarray.
-    # self.assertIsInstance(reduced, np_arrays.ndarray)
+    # self.assertIsInstance(reduced, np.ndarray)
     self.assertAllClose(reduced, 15)
 
-
 if __name__ == '__main__':
-  ops.enable_eager_execution()
-  test.main()
+  tf.compat.v1.enable_eager_execution()
+  tf.test.main()
