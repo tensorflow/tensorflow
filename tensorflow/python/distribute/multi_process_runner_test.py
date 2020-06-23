b07691301fa26602c35531aa6349e95136c9ef3d
Smit Hinsu <hinsu@google.com>
2020-06-23 10:05:49 -0700

    MultiProcessRunner: Add UnexpectedSubprocessExitError to be raised if the exit code from a subprocess in unexpected. This results in subprocess having segfault failing the test, which would not have before this change.
    PiperOrigin-RevId: 317888643
    Change-Id: Id1c372ec695e8478bbb9c0888516ca9af4bb9799

--- tensorflow/python/distribute/multi_process_runner_test.py
+++ tensorflow/python/distribute/multi_process_runner_test.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import ctypes
 import json
 import os
 import threading
@@ -299,40 +298,6 @@ class MultiProcessRunnerTest(test.TestCase):
     self.assertTrue(
         any('something printed' in line for line in list_to_assert))
 
-  def test_seg_fault_raises_error(self):
-
-    def proc_func_expected_to_seg_fault():
-      ctypes.string_at(0)  # Intentionally made seg fault.
-
-    with self.assertRaises(
-        multi_process_runner.UnexpectedSubprocessExitError) as cm:
-      multi_process_runner.run(
-          proc_func_expected_to_seg_fault,
-          multi_worker_test_base.create_cluster_spec(num_workers=1),
-          list_stdout=True)
-    self.assertIn('Missing status(es) from 1 subprocess(es).',
-                  str(cm.exception))
-    list_to_assert = cm.exception.mpr_result.stdout
-    self.assertTrue(any('SIGSEGV' in line for line in list_to_assert))
-
-  def test_seg_fault_in_chief_raises_error(self):
-
-    def proc_func_expected_to_seg_fault():
-      if multi_worker_test_base.get_task_type() == 'worker':
-        time.sleep(10000)
-      ctypes.string_at(0)  # Intentionally made seg fault.
-
-    with self.assertRaises(
-        multi_process_runner.UnexpectedSubprocessExitError) as cm:
-      multi_process_runner.run(
-          proc_func_expected_to_seg_fault,
-          multi_worker_test_base.create_cluster_spec(
-              has_chief=True, num_workers=1),
-          list_stdout=True)
-    self.assertIn('Subprocess chief-0 exited with exit code',
-                  str(cm.exception))
-    list_to_assert = cm.exception.mpr_result.stdout
-    self.assertTrue(any('SIGSEGV' in line for line in list_to_assert))
 
 if __name__ == '__main__':
   multi_process_runner.test_main()
