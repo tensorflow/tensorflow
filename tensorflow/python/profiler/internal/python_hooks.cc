06a954905bfa5ff23cd23330880d7598558ffcb0
A. Unique TensorFlower <gardener@tensorflow.org>
2020-06-23 15:07:18 -0700

    Internal change
    PiperOrigin-RevId: 317946737
    Change-Id: I39415c323b1d612d14f7a165ce6b188c4b84143d

--- tensorflow/python/profiler/internal/python_hooks.cc
+++ tensorflow/python/profiler/internal/python_hooks.cc
@@ -200,8 +200,12 @@ void PythonHooks::ClearProfilerInAllThreads() {
 void PythonHooks::EnableTraceMe(bool enable) {
   const char* kModuleName =
       "tensorflow.python.profiler.trace";
-  auto trace_module = py::module::import(kModuleName);
-  trace_module.attr("enabled") = py::bool_(enable);
+  try {
+    auto trace_module = py::module::import(kModuleName);
+    trace_module.attr("enabled") = py::bool_(enable);
+  } catch (const py::error_already_set& e) {
+    LOG(ERROR) << "Can't import " << kModuleName;
+  }
 }
 
 }  // namespace profiler
