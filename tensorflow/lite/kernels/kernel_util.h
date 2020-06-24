65d5f172c7bf7a8f968de7c39cda89309b17ef25
Nick Kreeger <kreeger@google.com>
2020-06-24 07:50:56 -0700

    Reduce the size of TfLiteNode for the TF Micro runtime.
    This change uses the existing micro-specific build flag (TF_LITE_MICRO_STATIC_MEMORY) to reduce the size of TfLiteNode. In this build setting, only the minimum number of fields required for running TFLM are used. This build define is opt-in only for internal builds and continues to be enabled by default in Makefile builds.
    
    All TFLM internal targets can be built with this flag by adding '--copt=-DTF_LITE_STATIC_MEMORY'.
    
    This change reduces the sizeof(TfLiteNode) to 48 bytes (64bit systems) down from 72 bytes (64 bit systems) (1.5x reduction or ~33%).
    
    Keyword model total allocation reduced by: 368 bytes (~2% savings)
    Keyword model tail allocation reduced by: 368 bytes (~2% savings)
    
    Test Conv model total allocation reduced by: 176 bytes (~1.6% savings)
    Test Conv model tail allocation reduced by: 176 bytes (~5.5% savings)
    
    PiperOrigin-RevId: 318062703
    Change-Id: Ibbe860ae25e18686bc60a6694998719509779a8e

--- tensorflow/lite/kernels/kernel_util.h
+++ tensorflow/lite/kernels/kernel_util.h
@@ -44,6 +44,7 @@ inline TfLiteTensor* GetOutput(TfLiteContext* context, const TfLiteNode* node,
                                int index) {
   return &context->tensors[node->outputs->data[index]];
 }
+#ifndef TF_LITE_STATIC_MEMORY
 inline TfLiteTensor* GetTemporary(TfLiteContext* context,
                                   const TfLiteNode* node, int index) {
   return &context->tensors[node->temporaries->data[index]];
@@ -52,11 +53,12 @@ inline const TfLiteTensor* GetIntermediates(TfLiteContext* context,
                                             const TfLiteNode* node, int index) {
   return &context->tensors[node->intermediates->data[index]];
 }
-inline int NumInputs(const TfLiteNode* node) { return node->inputs->size; }
-inline int NumOutputs(const TfLiteNode* node) { return node->outputs->size; }
 inline int NumIntermediates(const TfLiteNode* node) {
   return node->intermediates->size;
 }
+#endif  // TF_LITE_STATIC_MEMORY
+inline int NumInputs(const TfLiteNode* node) { return node->inputs->size; }
+inline int NumOutputs(const TfLiteNode* node) { return node->outputs->size; }
 
 inline int64_t NumElements(const TfLiteIntArray* dims) {
   int64_t count = 1;
