b9347647b8558ee18bc73d3a26a7e760e702168c
Geeta Chavan <geetac@google.com>
2020-06-25 13:08:54 -0700

    Update version to master
    PiperOrigin-RevId: 318333129
    Change-Id: Ide2e10bc27cc0bfd0536944c556a34c8e49d1fba

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -49,7 +49,7 @@ from setuptools.dist import Distribution
 # result for pip.
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
-_VERSION = '2.2.0'
+_VERSION = '2.4.0'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.7.0',
