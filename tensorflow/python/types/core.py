030fae94de1a0dbf8c410b840124ce33e74d7e1a
rahul-kamat <rahulkamat@gmail.com>
2020-06-28 04:46:17 +0000

    Create TensorLike to be a Union of function input types

--- tensorflow/python/types/core.py
+++ tensorflow/python/types/core.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from typing import Union
+from tensorflow.python.util.tf_export import tf_export
 
 # TODO(mdan): Consider adding ABC once the dependence on isinstance is reduced.
 # TODO(mdan): Add type annotations.
@@ -58,3 +60,6 @@ class Value(Tensor):
 
   def numpy(self):
     pass
+
+# TODO(rahulkamat): add complete set of types to TensorLike and add tf.export
+TensorLike = Union[Tensor, int, float, bool, str, tuple]
