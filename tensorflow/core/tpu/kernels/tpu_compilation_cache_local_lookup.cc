69e0a88ffd28d2fdb15bc590353b1e13d78f640d
Henry Tan <henrytan@google.com>
2020-06-29 15:45:25 -0700

    Splitting TpuCompilationCacheLookup interface and TpuCompilationCacheLocalLookup.
    PiperOrigin-RevId: 318909518
    Change-Id: I76de6a69723d70f542935c23ed05e1ea4e07f52d

--- tensorflow/core/tpu/kernels/tpu_compilation_cache_local_lookup.cc
+++ tensorflow/core/tpu/kernels/tpu_compilation_cache_local_lookup.cc
@@ -12,14 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/core/tpu/kernels/tpu_compilation_cache_lookup.h"
-
-#include "tensorflow/core/profiler/lib/traceme.h"
-#include "tensorflow/core/tpu/kernels/tpu_compilation_cache_external.h"
+#include "tensorflow/core/tpu/kernels/tpu_compilation_cache_local_lookup.h"
 
 namespace tensorflow {
 namespace tpu {
-
 namespace {
 class CompilationCacheFetchTargetUtility {
  public:
