8b552681d9a9b451743eb7e2a2278c576111b9d9
Deven Desai <deven.desai.amd@gmail.com>
2020-06-29 14:53:36 +0000

    Revert "Provide ldexp float overload for HIP, it's missing in their headers."
    This reverts commit 655ce09f679a90ecd561538227c703b42d0fc5fa.

--- tensorflow/core/kernels/rnn/blas_gemm.h
+++ tensorflow/core/kernels/rnn/blas_gemm.h
@@ -25,11 +25,6 @@ limitations under the License.
 #include "tensorflow/core/kernels/eigen_contraction_kernel.h"
 #endif
 
-#ifdef __HIP_DEVICE_COMPILE__
-// Provide ldexp float overload for HIP, it's missing in their headers.
-__device__ inline float ldexp(float x, int exp) { return ldexpf(x, exp); }
-#endif
-
 namespace tensorflow {
 class OpKernelContext;
 namespace functor {
