35bd17b6717384cf587aa7cc271d693a111d4a11
rahul-kamat <rahulkamat@gmail.com>
2020-06-29 16:24:23 +0000

    Remove TensorLike and changes to types/core.py

--- tensorflow/python/types/core.py
+++ tensorflow/python/types/core.py
@@ -18,8 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from typing import Union
-from tensorflow.python.util.tf_export import tf_export
 
 # TODO(mdan): Consider adding ABC once the dependence on isinstance is reduced.
 # TODO(mdan): Add type annotations.
@@ -60,6 +58,3 @@ class Value(Tensor):
 
   def numpy(self):
     pass
-
-# TODO(rahulkamat): add complete set of types to TensorLike and add tf.export
-TensorLike = Union[Tensor, int, float, bool, str, tuple]
