a3e749b9007186b8c71d802c919ddb92408adaf8
Raman Sarokin <sorokin@google.com>
2020-06-29 09:49:07 -0700

    ConvBuffer1x1 updated to new style. Added ability to operate on tensor elements of X2 or X4 size for Buffer tensor.
    PiperOrigin-RevId: 318833564
    Change-Id: I4ca1fef6e2c136245777267b4e8f223daca78413

--- tensorflow/lite/delegates/gpu/cl/tensor.cc
+++ tensorflow/lite/delegates/gpu/cl/tensor.cc
@@ -180,7 +180,11 @@ absl::Status Tensor::GetGPUResources(const GPUObjectDescriptor* obj_ptr,
   }
   if (descriptor_.HasAxis(Axis::WIDTH)) {
     resources->ints.push_back({"width", Width()});
+    resources->ints.push_back({"width_div2", Width() / 2});
+    resources->ints.push_back({"width_div4", Width() / 4});
     resources->ints.push_back({"width_batched", Width() * Batch()});
+    resources->ints.push_back({"width_batched_div2", Width() * Batch() / 2});
+    resources->ints.push_back({"width_batched_div4", Width() * Batch() / 4});
   }
   if (descriptor_.HasAxis(Axis::HEIGHT)) {
     resources->ints.push_back({"height", Height()});
