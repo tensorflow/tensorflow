2296107175c8c07ba1ca6f59d2ff2a77ca437cba
rahul-kamat <rahulkamat@gmail.com>
2020-06-30 00:28:12 +0000

    Add TensorLike with tf_export

--- tensorflow/python/types/core.py
+++ tensorflow/python/types/core.py
@@ -18,6 +18,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from typing import Union
+import numpy as np
+
+from tensorflow.python.util.tf_export import tf_export
 
 # TODO(mdan): Consider adding ABC once the dependence on isinstance is reduced.
 # TODO(mdan): Add type annotations.
@@ -58,3 +62,7 @@ class Value(Tensor):
 
   def numpy(self):
     pass
+
+
+TensorLike = Union[Tensor, int, float, bool, str, tuple, list, np.ndarray]
+tf_export("TensorLike").export_constant(__name__, "TensorLike")
