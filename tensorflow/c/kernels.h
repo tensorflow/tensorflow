2f861a79758545f3a6357814c79d7dcace920a3b
Daniel Nguyen <nguyendaniel@google.com>
2020-06-30 23:51:04 +0000

    merge with master

--- tensorflow/c/kernels.h
+++ tensorflow/c/kernels.h
@@ -194,10 +194,6 @@ TF_CAPI_EXPORT TF_Tensor* TF_AllocateOutput(TF_OpKernelContext* context,
                                             int64_t* dims, int num_dims,
                                             size_t len, TF_Status* status);
 
-<<<<<<< HEAD
-TF_CAPI_EXPORT extern TF_Tensor* TF_AllocateTemp(TF_OpKernelContext* context, TF_DataType dtype, 
-                     int64_t* dims, int num_dims, TF_Status* Status);
-=======
 // Allocates a temporary Tensor of the specified type and shape. Devices 
 // such as GPUs that enqueue Ops for lazy execution may retain references 
 // to the temporary tensors after the Op's Compute method has run. 
@@ -205,8 +201,6 @@ TF_CAPI_EXPORT extern TF_Tensor* TF_AllocateTemp(TF_OpKernelContext* context, TF
 // num_dims must equal the size of array dims 
 TF_CAPI_EXPORT extern TF_Tensor* TF_AllocateTemp(TF_OpKernelContext* context, 
     TF_DataType dtype, int64_t* dims, int num_dims, TF_Status* status);
->>>>>>> e2187cd137f929eee06bd82a067564c4cdac2fa3
-
 
 #ifdef __cplusplus
 } /* end extern "C" */
