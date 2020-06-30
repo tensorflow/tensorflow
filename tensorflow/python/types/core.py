5817699a27210a317a198a829cea634e547f3515
rahul-kamat <rahulkamat@gmail.com>
2020-06-30 18:21:07 +0000

    Add complex type, Update the goldens for api_test

--- tensorflow/python/types/core.py
+++ tensorflow/python/types/core.py
@@ -64,5 +64,5 @@ class Value(Tensor):
     pass
 
 
-TensorLike = Union[Tensor, int, float, bool, str, tuple, list, np.ndarray]
-tf_export("TensorLike").export_constant(__name__, "TensorLike")
+TensorLike = Union[Tensor, int, float, bool, str, complex, tuple, list, np.ndarray]
+tf_export("experimental.TensorLike").export_constant(__name__, "TensorLike")
