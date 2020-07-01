1b4085d652870db166197c66a16c817ecec8e2e4
amturati <amturati@google.com>
2020-07-01 16:01:57 +0000

    fixed memory leaks and added matrix addition test

--- tensorflow/c/eager/c_api_unified_experimental_test.cc
+++ tensorflow/c/eager/c_api_unified_experimental_test.cc
@@ -245,6 +245,90 @@ TEST_P(UnifiedCAPI, TestBasicEagerMatMul2) {
   TF_DeleteExecutionContext(ctx);
 }
 
+// MatAdd
+TEST_P(UnifiedCAPI, TestBasicEagerMatAdd) {
+  std::unique_ptr<TF_Status, decltype(&TF_DeleteStatus)> status(
+      TF_NewStatus(), TF_DeleteStatus);
+  TFE_ContextOptions* opts = TFE_NewContextOptions();
+  TF_ExecutionContext* ctx = TF_NewEagerExecutionContext(opts, status.get());
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+  TFE_DeleteContextOptions(opts);
+
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  /* Want to test simple MatMul example with abstract tensors: 
+
+    [ [1,2] ,   +   [ [5,6] ,   =   [ [6,8],
+      [3,4] ]         [7,8] ]         [10,12] ]
+
+  */
+
+  // Build 1st Matrix.
+  int64_t dims [] = {2,2}; // Matrices will be 2 x 2   
+  int num_dims = sizeof(dims)/sizeof(dims[0]);
+
+  float vals1 [] = {1.0f,2.0f,3.0f,4.0f};
+  TFE_Context* eager_ctx = TF_ExecutionContextGetTFEContext(ctx);
+  TFE_TensorHandle* t1 = TestMatrixTensorHandleWithInput(eager_ctx, vals1, dims, num_dims);
+  
+  TF_AbstractTensor* at1 =
+      TF_CreateAbstractTensorFromEagerTensor(t1, status.get()); // get abstract tensor
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  // Build 2nd Matrix.
+  float vals2 [] = {5.0f,6.0f,7.0f,8.0f};
+  TFE_TensorHandle* t2 = TestMatrixTensorHandleWithInput(eager_ctx, vals2, dims, num_dims);
+  
+  TF_AbstractTensor* at2 =
+      TF_CreateAbstractTensorFromEagerTensor(t2, status.get()); // get abstract tensor
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  // Build an abstract operation.
+  auto* op = TF_NewAbstractOp(ctx);
+  TF_AbstractOpSetOpType(op, "Add", status.get());
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  // Build inputs and outputs.
+  TF_AbstractTensor* inputs[2] = {at1, at2};
+  TF_OutputList* o = TF_NewOutputList();
+  TF_OutputListSetNumOutputs(o, 1, status.get());
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  // Execute.
+  TF_ExecuteOperation(op, 2, inputs, o, ctx, status.get());
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+
+  // Clean up operation and inputs.
+  TF_DeleteAbstractOp(op);
+  TF_DeleteAbstractTensor(at1);
+  TF_DeleteAbstractTensor(at2);
+
+  // Verify the results.
+  ASSERT_EQ(1, TF_OutputListNumOutputs(o));
+  TF_AbstractTensor* result = TF_OutputListGet(o, 0);
+  TFE_TensorHandle* result_t =
+      TF_AbstractTensorGetEagerTensor(result, status.get());
+  ASSERT_EQ(TF_OK, TF_GetCode(status.get())) << TF_Message(status.get());
+  
+  TF_Tensor* result_tensor = TFE_TensorHandleResolve(result_t, status.get());
+
+  // Copy Tensor data into array.
+  float result_data[4] = {0};
+  memcpy(&result_data[0], TF_TensorData(result_tensor), TF_TensorByteSize(result_tensor));
+
+  // Build expected result & verify.
+  float e_vals [] = {6.0f,8.0f,10.0f,12.0f};//{19.0f,22.0f,43.0f,50.0f};
+
+  int data_len = 4; // length of e_vals
+  for(int i = 0; i < data_len; i++){ 
+    EXPECT_EQ(result_data[i], e_vals[i]);
+  }
+
+  TF_DeleteTensor(result_tensor);
+  TF_DeleteAbstractTensor(result);
+  TF_DeleteOutputList(o);
+  TF_DeleteExecutionContext(ctx);
+}
 
 TEST_P(UnifiedCAPI, TestBasicGraph) {
   std::unique_ptr<TF_Status, decltype(&TF_DeleteStatus)> status(
@@ -324,6 +408,7 @@ TEST_P(UnifiedCAPI, TestBasicGraph) {
   TF_DeleteOutputList(add_outputs);
   TF_DeleteAbstractOp(fn_op);
   TF_DeleteAbstractTensor(input_t);
+  TF_DeleteAbstractTensor(placeholder_t);
   TF_DeleteAbstractTensor(final_result);
   TF_DeleteTensor(f_t);
   TF_DeleteAbstractFunction(func);
@@ -426,10 +511,11 @@ TEST_P(UnifiedCAPI, TestBasicGraphMatMul) {
     ASSERT_EQ(result_data[i], 2.0f);
   }
 
+  TF_DeleteAbstractTensor(final_result);
   TF_DeleteOutputList(mm_outputs);
+  TF_DeleteAbstractTensor(placeholder_t);
   TF_DeleteAbstractOp(fn_op);
   TF_DeleteAbstractTensor(input_t);
-  TF_DeleteAbstractTensor(final_result);
   TF_DeleteTensor(f_t);
   TF_DeleteAbstractFunction(func);
 
