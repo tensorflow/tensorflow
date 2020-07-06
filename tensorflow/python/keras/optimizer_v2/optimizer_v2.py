322c39cbfe3b4d27da7d0f187f86273ed604dca4
Rick Chao <rchao@google.com>
2020-07-06 16:48:33 -0700

    In optimizer_v2, adds a comment saying the check for `callable` covers the usage when `value` is a `LearningRateSchedule`.
    PiperOrigin-RevId: 319882549
    Change-Id: I4c2111df937d07dfed3cee8f9ee8dce75a041c35

--- tensorflow/python/keras/optimizer_v2/optimizer_v2.py
+++ tensorflow/python/keras/optimizer_v2/optimizer_v2.py
@@ -813,6 +813,9 @@ class OptimizerV2(trackable.Trackable):
       for name, value in sorted(self._hyper.items()):
         if isinstance(value,
                       (ops.Tensor, tf_variables.Variable)) or callable(value):
+          # The check for `callable` covers the usage when `value` is a
+          # `LearningRateSchedule`, in which case it does not need to create a
+          # variable.
           continue
         else:
           self._hyper[name] = self.add_weight(
