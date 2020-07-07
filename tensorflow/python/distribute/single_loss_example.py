25528fe1d7ca284840a51f1fe4ef7874a78769a3
Scott Zhu <scottzhu@google.com>
2020-07-07 10:59:13 -0700

    Update the single_loss_example to not rely on Keras.
    Change the instance type check to the method signature check for optimizer.minimize().
    
    PiperOrigin-RevId: 320018058
    Change-Id: I3e066f37b72d23cf4e5c798775e6ca83c769362f

--- tensorflow/python/distribute/single_loss_example.py
+++ tensorflow/python/distribute/single_loss_example.py
@@ -17,15 +17,16 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.distribute import step_fn
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import ops
-from tensorflow.python.keras.optimizer_v2 import optimizer_v2
 from tensorflow.python.layers import core
 from tensorflow.python.layers import normalization
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.util import tf_inspect
 
 
 def single_loss_example(optimizer_fn, distribution, use_bias=False,
@@ -68,7 +69,7 @@ def minimize_loss_example(optimizer, use_bias=False, use_callable_loss=True):
       y = array_ops.reshape(layer(x), []) - constant_op.constant(1.)
       return y * y
 
-    if isinstance(optimizer, optimizer_v2.OptimizerV2):
+    if _is_optimizer_v2_instance(optimizer):
       return optimizer.minimize(loss_fn, lambda: layer.trainable_variables)
     elif use_callable_loss:
       return optimizer.minimize(loss_fn)
@@ -111,10 +112,17 @@ def batchnorm_example(optimizer_fn,
       # `x` and `y` will be fetched by the gradient computation, but not `loss`.
       return loss
 
-    if isinstance(optimizer, optimizer_v2.OptimizerV2):
+    if _is_optimizer_v2_instance(optimizer):
       return optimizer.minimize(loss_fn, lambda: layer.trainable_variables)
 
     # Callable loss.
     return optimizer.minimize(loss_fn)
 
   return model_fn, dataset_fn, batchnorm
+
+
+def _is_optimizer_v2_instance(optimizer):
+  # For a optimizer instance, the v2 implementation has var_list as a required
+  # argument.
+  arg_spec = tf_inspect.getfullargspec(optimizer.minimize)
+  return 'var_list' in arg_spec.args[:-len(arg_spec.defaults)]
