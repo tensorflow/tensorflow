b1a57d8f9b79835781d25c824de827d7fd9556bc
A. Unique TensorFlower <gardener@tensorflow.org>
2020-07-09 13:59:00 -0700

    Fix build error on Linux platforms caused by unavailable vasprintf in SwiftGlibc.
    PiperOrigin-RevId: 320468534
    Change-Id: I2a87856475eca8e94237ce2bd82556b9cd8159a3

--- tensorflow/lite/experimental/swift/Sources/Interpreter.swift
+++ tensorflow/lite/experimental/swift/Sources/Interpreter.swift
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if os(Linux)
+import SwiftGlibc
+#else
+import Darwin
+#endif
+
 import Foundation
 import TensorFlowLiteC
 
@@ -331,8 +337,19 @@ extension String {
   ///   - cFormat: The format C array as a template for substituting values.
   ///   - arguments: A C pointer to a `va_list` of arguments to substitute into `cFormat`.
   init?(cFormat: UnsafePointer<CChar>, arguments: CVaListPointer) {
+#if os(Linux)
+    let length = vsnprintf(nil, 0, cFormat, arguments) + 1 // null terminator
+    guard length > 0 else { return nil }
+    var buffer = UnsafeMutablePointer<CChar>.allocate(capacity: length)
+    defer {
+      buffer.deallocate()
+    }
+    guard vsnprintf(buffer, length, cFormat, arguments) == length - 1 else { return nil }
+    self.init(validatingUTF8: buffer)
+#else
     var buffer: UnsafeMutablePointer<CChar>?
     guard vasprintf(&buffer, cFormat, arguments) != 0, let cString = buffer else { return nil }
     self.init(validatingUTF8: cString)
+#endif
   }
 }
