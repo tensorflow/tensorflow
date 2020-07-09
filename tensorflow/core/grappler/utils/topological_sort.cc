ab230eded9f439ed0ea99881d35170f2723b24ce
tg-at-google <taregaskin@google.com>
2020-07-09 14:54:28 -0400

    Update topological_sort.cc

--- tensorflow/core/grappler/utils/topological_sort.cc
+++ tensorflow/core/grappler/utils/topological_sort.cc
@@ -96,7 +96,7 @@ Status ComputeTopologicalOrder(
                  "at node = "
               << graph.node(back).DebugString();
       for (int i = 0; i < graph_view.num_nodes(); ++i) {
-        int graph_view_GetFanin_i_size = graph_view.GetFanin(i).size();
+        const int graph_view_GetFanin_i_size = graph_view.GetFanin(i).size();
         if (num_ready_inputs[i] != graph_view_GetFanin_i_size) {
           VLOG(1) << "Node not ready: " << graph.node(i).DebugString();
         }
