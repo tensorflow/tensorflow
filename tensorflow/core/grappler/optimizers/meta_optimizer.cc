2a3f88d4c8a1595936ff5d90f783fa043b88fa09
Yimei Sun <yimei.sun@intel.com>
2020-07-10 20:37:17 -0700

    Add auto_mixed_precision_mkl to run-once optimizer list

--- tensorflow/core/grappler/optimizers/meta_optimizer.cc
+++ tensorflow/core/grappler/optimizers/meta_optimizer.cc
@@ -88,7 +88,8 @@ int NumIterations(const RewriterConfig& cfg) {
 // Check if optimizer is allowed to run only once.
 bool IsRunOnceOptimizer(const string& name) {
   return name == "layout" || name == "memory_optimizer" ||
-         name == "loop_optimizer" || name == "auto_mixed_precision";
+         name == "loop_optimizer" || name == "auto_mixed_precision" ||
+         name == "auto_mixed_precision_mkl";
 }
 
 bool IsTFDataFunction(const FunctionDef& func) {
