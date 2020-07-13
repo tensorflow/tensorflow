1ed264d86be06ee7ac7cabfe5c3806666323f700
Teng Lu <teng.lu@intel.com>
2020-07-14 01:44:45 +0800

    Fix wrong skip issue of double/int64.

--- tensorflow/core/kernels/random_op_cpu.h
+++ tensorflow/core/kernels/random_op_cpu.h
@@ -86,7 +86,13 @@ struct FillPhiloxRandomTask<Distribution, false> {
                   int64 start_group, int64 limit_group, Distribution dist) {
     const int kGroupSize = Distribution::kResultElementCount;
 
-    gen.Skip(start_group * kGroupSize / gen.kResultElementCount);
+    // Decide skip strides according to different kResultElementCount:
+    // * `1 = (4 + 3) / 4` for normal Distribution.
+    // * `1 = (2 + 3) / 4` for double/int64 Distribution.
+    // * `4 = (16 + 3) / 4` for vecotorized float/bfloat16 Distribution.
+    const int skip_strides =
+        (kGroupSize + gen.kResultElementCount - 1) / gen.kResultElementCount;
+    gen.Skip(start_group * skip_strides);
     int64 offset = start_group * kGroupSize;
 
     // First fill all the full-size groups
