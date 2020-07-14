2bcdcf38caeba1c836ed02d0da5e451c9e824275
WindQAQ <windqaq@gmail.com>
2020-07-13 19:39:05 -0700

    Add example and more doc

--- tensorflow/python/keras/activations.py
+++ tensorflow/python/keras/activations.py
@@ -305,16 +305,34 @@ def relu(x, alpha=0., max_value=None, threshold=0):
 @keras_export('keras.activations.gelu')
 @dispatch.add_dispatch_support
 def gelu(x, approximate=True):
-  """Gaussian Error Linear Unit.
+  """Applies the Gaussian error linear unit (GELU) activation function.
+
+  Gaussian error linear unit (GELU) computes
+  `x * P(X <= x)`, where `P(X) ~ N(0, 1)`.
+  The (GELU) nonlinearity weights inputs by their value, rather than gates
+  inputs by their sign as in ReLU.
+
+  For example:
+
+  >>> x = tf.constant([-3.0, -1.0, 0.0, 1.0, 3.0], dtype=tf.float32)
+  >>> y = tf.keras.activations.gelu(x)
+  >>> y.numpy()
+  array([-0.00363752, -0.158808  ,  0.        ,  0.841192  ,  2.9963627 ],
+      dtype=float32)
+  >>> y = tf.keras.activations.gelu(x, approximate=False)
+  >>> y.numpy()
+  array([-0.00404951, -0.15865529,  0.        ,  0.8413447 ,  2.9959507 ],
+      dtype=float32)
 
   Arguments:
       x: Input tensor.
+      approximate: A `bool`, whether to enable approximation.
 
   Returns:
       The gaussian error linear activation:
       `0.5 * x * (1 + tanh(sqrt(2 / pi) * (x + 0.044715 * x^3)))`
       if `approximate` is `True` or
-      `x * P(X <= x) = 0.5 * x * (1 + erf(x / sqrt(2)))`, where P(X) ~ N(0, 1),
+      `x * P(X <= x) = 0.5 * x * (1 + erf(x / sqrt(2)))`, where `P(X) ~ N(0, 1)`,
       if `approximate` is `False`.
 
   Reference:
--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -3498,6 +3498,23 @@ def leaky_relu(features, alpha=0.2, name=None):
 def gelu(features, approximate=True, name=None):
   """Compute the Gaussian Error Linear Unit (GELU) activation function.
 
+  Gaussian error linear unit (GELU) computes
+  `x * P(X <= x)`, where `P(X) ~ N(0, 1)`.
+  The (GELU) nonlinearity weights inputs by their value, rather than gates
+  inputs by their sign as in ReLU.
+
+  For example:
+
+  >>> x = tf.constant([-3.0, -1.0, 0.0, 1.0, 3.0], dtype=tf.float32)
+  >>> y = tf.keras.activations.gelu(x)
+  >>> y.numpy()
+  array([-0.00363752, -0.158808  ,  0.        ,  0.841192  ,  2.9963627 ],
+      dtype=float32)
+  >>> y = tf.keras.activations.gelu(x, approximate=False)
+  >>> y.numpy()
+  array([-0.00404951, -0.15865529,  0.        ,  0.8413447 ,  2.9959507 ],
+      dtype=float32)
+
   Args:
     features: A `Tensor` representing preactivation values.
     approximate: An optional `bool`. Defaults to `True`.
@@ -3513,10 +3530,9 @@ def gelu(features, approximate=True, name=None):
   with ops.name_scope(name, "Gelu", [features]):
     features = ops.convert_to_tensor(features, name="features")
     if approximate:
-      pi = math_ops.cast(np.pi, features.dtype)
       coeff = math_ops.cast(0.044715, features.dtype)
       return 0.5 * features * (
-          1.0 + math_ops.tanh(math_ops.sqrt(2.0 / pi) * (
+          1.0 + math_ops.tanh(0.7978845608028654 * (
               features + coeff * math_ops.pow(features, 3))))
     else:
       return 0.5 * features * (
