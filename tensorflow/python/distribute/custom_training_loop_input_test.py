452b4d88d929daba8f0610af7fc3b3c14b8be496
Ruoxin Sang <rxsang@google.com>
2020-07-14 11:34:12 -0700

    Fix X64 outputs with dynamic shapes.
    PiperOrigin-RevId: 321199821
    Change-Id: I2de7814bcf1071a87e313210abbd505e3c295283

--- tensorflow/python/distribute/custom_training_loop_input_test.py
+++ tensorflow/python/distribute/custom_training_loop_input_test.py
@@ -536,6 +536,31 @@ class InputIterationTest(test.TestCase, parameterized.TestCase,
   @combinations.generate(
       combinations.combine(
           distribution=strategy_combinations.multidevice_strategies,
+          mode=["eager"]))
+  def testDynamicOutputsWithX64(self, distribution):
+    dataset = get_dataset_from_tensor_slices(
+        [5]).map(lambda x: math_ops.cast(x, dtypes.int64)).batch(2)
+    input_iterator = iter(distribution.experimental_distribute_dataset(dataset))
+
+    @def_function.function
+    def run(iterator):
+
+      def computation(x):
+        return math_ops.add(x, x)
+
+      inputs = next(iterator)
+      outputs = distribution.experimental_local_results(
+          distribution.run(computation, args=(inputs,)))
+      return outputs
+
+    # This assumes that there are exactly 2 replicas
+    result = run(input_iterator)
+    self.assertAllEqual([10], result[0])
+    self.assertAllEqual([], result[1])
+
+  @combinations.generate(
+      combinations.combine(
+          distribution=strategy_combinations.multidevice_strategies,
           mode=["eager"]
       ))
   def testDynamicShapesWithGetNextOutsideFunction(self, distribution):
