f8c8a778032db1685af4c746cef63fdcd39abf97
Yong Tang <yong.tang.github@outlook.com>
2020-07-15 16:11:17 +0000

    Add qint8 and qint16 support for FillOp
    This PR tries to address the issue raised in 26069 where
    qint8 and qint16 were not supported for FillOp.
    
    This PR add qint8 and qint16 support for FillOp.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/kernel_tests/constant_op_test.py
+++ tensorflow/python/kernel_tests/constant_op_test.py
@@ -456,6 +456,18 @@ class ZerosTest(test.TestCase):
         self.assertFalse(np.any(z_value))
         self.assertEqual((2, 3), z_value.shape)
 
+  def testQintDtype(self):
+    for dtype in [
+        dtypes_lib.qint8, dtypes_lib.quint8,
+        dtypes_lib.qint16, dtypes_lib.quint16]:
+      z = array_ops.zeros([2, 3], dtype=dtype)
+      self.assertEqual(z.dtype, dtype)
+      self.assertEqual([2, 3], z.get_shape())
+      # cast to int32 so that it can be compred with numpy
+      # where [qint|quint][8|16] are not available.
+      z_value = self.evaluate(math_ops.cast(z, dtypes_lib.int32))
+      self.assertFalse(np.any(z_value))
+
 
 class ZerosLikeTest(test.TestCase):
 
