02ff9707d75b5e9d481aeea395f68741fbfab64b
danielyou0230 <danielyou0230@gmail.com>
2020-07-16 10:12:16 -0700

    TFLM magic_wand: changed macro ZEPHYR_DEBUG to ZEPHYR_RISCV_PROFILE

--- tensorflow/lite/micro/examples/magic_wand/zephyr_riscv/src/main_functions.cc
+++ tensorflow/lite/micro/examples/magic_wand/zephyr_riscv/src/main_functions.cc
@@ -26,6 +26,8 @@ limitations under the License.
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 
+// #define ZEPHYR_RISCV_PROFILE
+
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
 tflite::ErrorReporter* error_reporter = nullptr;
@@ -105,35 +107,35 @@ void setup() {
 
 void loop() {
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   unsigned int cycle = 0;
   static unsigned int count = 0;
   static const unsigned int CLEAR = 0;
 
   // Reset cycle count for ReadAccelerometer()
   __asm__ __volatile__("csrw mcycle, %0" : : "r"(CLEAR));
-#endif /* ZEPHYR_DEBUG */
+#endif /* ZEPHYR_RISCV_PROFILE */
 
   // Attempt to read new data from the accelerometer.
   bool got_data =
       ReadAccelerometer(error_reporter, model_input->data.f, input_length);
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   __asm__ __volatile__("csrr %0, mcycle" : "=r"(cycle));
   // Accumulate cycle count till enough data is collected
   count += cycle;
-#endif /* ZEPHYR_DEBUG */
+#endif /* ZEPHYR_RISCV_PROFILE */
 
   // If there was no new data, wait until next time.
   if (!got_data) return;
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   TF_LITE_REPORT_ERROR(error_reporter,
-                       "[ZEPHYR_DEBUG] Before Invoke(): %d cycles\n", count);
+                       "[ZEPHYR_RISCV_PROFILE] Before Invoke(): %d cycles\n", count);
 
   // Reset cycle count for Invoke()
   __asm__ __volatile__("csrw mcycle, %0" : : "r"(CLEAR));
-#endif /* ZEPHYR_DEBUG */
+#endif /* ZEPHYR_RISCV_PROFILE */
 
   // Run inference, and report any error.
   TfLiteStatus invoke_status = interpreter->Invoke();
@@ -144,33 +146,33 @@ void loop() {
     return;
   }
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   __asm__ __volatile__("csrr %0, mcycle" : "=r"(cycle));
   TF_LITE_REPORT_ERROR(error_reporter,
-                       "[ZEPHYR_DEBUG] Invoke(): %d cycles\n", (int)cycle);
+                       "[ZEPHYR_RISCV_PROFILE] Invoke(): %d cycles\n", (int)cycle);
 
   // Reset cycle count for PredictGesture()
   __asm__ __volatile__("csrw mcycle, %0" : : "r"(CLEAR));
-#endif /* ZEPHYR_DEBUG */
+#endif /* ZEPHYR_RISCV_PROFILE */
 
   // Analyze the results to obtain a prediction
   int gesture_index = PredictGesture(interpreter->output(0)->data.f);
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   __asm__ __volatile__("csrr %0, mcycle" : "=r"(cycle));
   TF_LITE_REPORT_ERROR(error_reporter,
-                       "[ZEPHYR_DEBUG] PredictGesture(): %d cycles\n", (int)cycle);
+                       "[ZEPHYR_RISCV_PROFILE] PredictGesture(): %d cycles\n", (int)cycle);
 
   // Reset cycle count for HandleOutput()
   __asm__ __volatile__("csrw mcycle, %0" : : "r"(CLEAR));
-#endif /* ZEPHYR_DEBUG */
+#endif /* ZEPHYR_RISCV_PROFILE */
 
   // Produce an output
   HandleOutput(error_reporter, gesture_index);
 
-#ifdef ZEPHYR_DEBUG
+#ifdef ZEPHYR_RISCV_PROFILE
   __asm__ __volatile__("csrr %0, mcycle" : "=r"(cycle));
   TF_LITE_REPORT_ERROR(error_reporter,
-                       "[ZEPHYR_DEBUG] HandleOutput(): %d cycles\n", (int)cycle);
-#endif /* ZEPHYR_DEBUG */
+                       "[ZEPHYR_RISCV_PROFILE] HandleOutput(): %d cycles\n", (int)cycle);
+#endif /* ZEPHYR_RISCV_PROFILE */
 }
