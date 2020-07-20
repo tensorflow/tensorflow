8fbbcbfd02459902a2fbca314a6855a9ba623bbe
Gaurav Jain <gjn@google.com>
2020-07-20 14:01:06 -0700

    Reduce memory requirement for test
    PiperOrigin-RevId: 322220008
    Change-Id: If4940e9c4aa0b783cea1d112deb7f4cadbcf96fb

--- tensorflow/python/kernel_tests/reshape_op_test.py
+++ tensorflow/python/kernel_tests/reshape_op_test.py
@@ -202,7 +202,7 @@ class ReshapeTest(test.TestCase):
     self.assertEqual([100, 1], y.get_shape().as_list())
 
   def testInt64Shape(self):
-    x = array_ops.zeros([50000, 50000])
+    x = array_ops.zeros([50000, 50000], dtype=dtypes.bool)
     # Provide dimension larger than int32
     y = array_ops.reshape(x, [50000**2])
     self.assertEqual([50000**2], y.get_shape().as_list())
