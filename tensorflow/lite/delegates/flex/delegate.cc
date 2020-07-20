19a8ce8888bd451bc856d8a9c50d271bceb22130
Terry Heo <terryheo@google.com>
2020-07-19 18:02:25 -0700

    tflite: Refactor core/macros.h, c/c_api.h and c/common.h
    - Move TFL_CAPI_EXPORT to c/common.h to make them available for delegates.
    - Create lite:macros module to make core/macros.h available for delegates.
    
    PiperOrigin-RevId: 322061366
    Change-Id: I671631687775c4d86b0056ccd5a20ca368874a58

--- tensorflow/lite/delegates/flex/delegate.cc
+++ tensorflow/lite/delegates/flex/delegate.cc
@@ -19,7 +19,9 @@ limitations under the License.
 
 #include "absl/strings/str_cat.h"
 #include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/context_util.h"
+#include "tensorflow/lite/core/macros.h"
 #include "tensorflow/lite/delegates/flex/buffer_map.h"
 #include "tensorflow/lite/delegates/flex/kernel.h"
 #include "tensorflow/lite/delegates/flex/util.h"
@@ -30,7 +32,7 @@ limitations under the License.
 namespace tflite {
 
 // Corresponding weak declaration found in lite/interpreter_builder.cc.
-#if !defined(_WIN32)
+#if TFLITE_HAS_ATTRIBUTE_WEAK
 // If weak symbol is not supported (Windows), it can use
 // TF_AcquireFlexDelegate() path instead.
 TfLiteDelegateUniquePtr AcquireFlexDelegate() {
@@ -145,10 +147,7 @@ TfLiteStatus FlexDelegate::CopyFromBufferHandle(
 // interpreter_build.cc. To export the function name globally, the function name
 // must be matched with patterns in tf_version_script.lds
 extern "C" {
-#if defined(_WIN32)
-__declspec(dllexport)
-#endif
-    tflite::TfLiteDelegateUniquePtr TF_AcquireFlexDelegate() {
+TFL_CAPI_EXPORT tflite::TfLiteDelegateUniquePtr TF_AcquireFlexDelegate() {
   return tflite::FlexDelegate::Create();
 }
 }  // extern "C"
