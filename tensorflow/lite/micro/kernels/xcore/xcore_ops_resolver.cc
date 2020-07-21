e0e9d29c1801d236df379d25710c7fff63cfc6e0
Keith Moulton <keithm@xmos.com>
2020-07-21 16:37:41 -0400

    now conforms to ops resolver convention of a function to add each operator

--- tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.cc
+++ tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.cc
@@ -1,35 +1,26 @@
 #include "tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.h"
 
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
-#include "tensorflow/lite/micro/kernels/xcore/xcore_ops.h"
 
 namespace tflite {
 namespace ops {
 namespace micro {
 namespace xcore {
 
-void add_builtin_ops(MicroMutableOpResolver<num_xcore_ops> *resolver) {
-  resolver->AddSoftmax();
-  resolver->AddPad();
-}
-
-void add_custom_ops(MicroMutableOpResolver<num_xcore_ops> *resolver) {
-  resolver->AddCustom("XC_argmax_16", Register_ArgMax_16());
-  resolver->AddCustom("XC_maxpool2d", Register_MaxPool2D());
-  resolver->AddCustom("XC_avgpool2d", Register_AvgPool2D());
-  resolver->AddCustom("XC_avgpool2d_global", Register_AvgPool2D_Global());
-  resolver->AddCustom("XC_fc_deepin_anyout", Register_FullyConnected_16());
-  resolver->AddCustom("XC_conv2d_shallowin", Register_Conv2D_Shallow());
-  resolver->AddCustom("XC_conv2d_deep", Register_Conv2D_Deep());
-  resolver->AddCustom("XC_conv2d_1x1", Register_Conv2D_1x1());
-  resolver->AddCustom("XC_conv2d_depthwise", Register_Conv2D_Depthwise());
-  resolver->AddCustom("XC_requantize_16_to_8", Register_Requantize_16_to_8());
-  resolver->AddCustom("XC_lookup_8", Register_Lookup_8());
-}
-
-XcoreOpsResolver::XcoreOpsResolver() {
-  add_builtin_ops(this);
-  add_custom_ops(this);
+void XcoreOpsResolver::AddXC() {
+  AddSoftmax();
+  AddPad();
+  AddXC_ArgMax();
+  AddXC_MaxPool2D();
+  AddXC_AvgPool2D();
+  AddXC_AvgPoolGlobal();
+  AddXC_FullyConnected();
+  AddXC_Conv2D_Shallow();
+  AddXC_Conv2D_Deep();
+  AddXC_Conv2D_1x1();
+  AddXC_Conv2D_Deepthwise();
+  AddXC_Requantize_16_to_8();
+  AddXC_Lookup8();
 }
 
 }  // namespace xcore
--- tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.h
+++ tensorflow/lite/micro/kernels/xcore/xcore_ops_resolver.h
@@ -3,6 +3,8 @@
 
 #include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/compatibility.h"
+#include "tensorflow/lite/micro/kernels/micro_ops.h"
+#include "tensorflow/lite/micro/kernels/xcore/xcore_ops.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 
 namespace tflite {
@@ -12,11 +14,54 @@ namespace xcore {
 
 constexpr int num_xcore_ops = 13;
 
-void add_custom_ops(MicroMutableOpResolver<num_xcore_ops> *resolver);
-
 class XcoreOpsResolver : public MicroMutableOpResolver<num_xcore_ops> {
  public:
-  XcoreOpsResolver();
+  XcoreOpsResolver() {}
+  void AddXC();
+
+  TfLiteStatus AddXC_ArgMax() {
+    return AddCustom("XC_argmax_16", Register_ArgMax_16());
+  }
+
+  TfLiteStatus AddXC_MaxPool2D() {
+    return AddCustom("XC_maxpool2d", Register_MaxPool2D());
+  }
+
+  TfLiteStatus AddXC_AvgPool2D() {
+    return AddCustom("XC_avgpool2d", Register_AvgPool2D());
+  }
+
+  TfLiteStatus AddXC_AvgPoolGlobal() {
+    return AddCustom("XC_avgpool2d_global", Register_AvgPool2D_Global());
+  }
+
+  TfLiteStatus AddXC_FullyConnected() {
+    return AddCustom("XC_fc_deepin_anyout", Register_FullyConnected_16());
+  }
+
+  TfLiteStatus AddXC_Conv2D_Shallow() {
+    return AddCustom("XC_conv2d_shallowin", Register_Conv2D_Shallow());
+  }
+
+  TfLiteStatus AddXC_Conv2D_Deep() {
+    return AddCustom("XC_conv2d_deep", Register_Conv2D_Deep());
+  }
+
+  TfLiteStatus AddXC_Conv2D_1x1() {
+    return AddCustom("XC_conv2d_1x1", Register_Conv2D_1x1());
+  }
+
+  TfLiteStatus AddXC_Conv2D_Deepthwise() {
+    return AddCustom("XC_conv2d_depthwise", Register_Conv2D_Depthwise());
+  }
+
+  TfLiteStatus AddXC_Requantize_16_to_8() {
+    return AddCustom("XC_requantize_16_to_8", Register_Requantize_16_to_8());
+  }
+
+  TfLiteStatus AddXC_Lookup8() {
+    return AddCustom("XC_lookup_8", Register_Lookup_8());
+  }
 
  private:
   TF_LITE_REMOVE_VIRTUAL_DELETE
