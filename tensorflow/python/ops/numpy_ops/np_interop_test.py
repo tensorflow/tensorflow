d57541184dae517b32fb8a20a2de8f861db2645e
Akshay Modi <nareshmodi@google.com>
2020-07-22 14:21:35 -0700

    Wrap/rewrap ndarrays in batch_jacobian
    PiperOrigin-RevId: 322654079
    Change-Id: I1ac1d2154c3b82f0714b460b3f002fb68e6b7ca5

--- tensorflow/python/ops/numpy_ops/np_interop_test.py
+++ tensorflow/python/ops/numpy_ops/np_interop_test.py
@@ -300,6 +300,22 @@ class InteropTest(tf.test.TestCase):
     self.assertIsInstance(jacobian[1], np.ndarray)
     self.assertAllClose(jacobian, answer)
 
+  def testBatchJacobian(self):
+    with tf.GradientTape() as g:
+      x = np.asarray([[1., 2.], [3., 4.]])
+      y = np.asarray([[3., 4.], [5., 6.]])
+      g.watch(x)
+      g.watch(y)
+      z = x * x * y
+
+    batch_jacobian = g.batch_jacobian(z, x)
+    answer = tf.stack(
+        [tf.linalg.diag(2 * x[0] * y[0]),
+         tf.linalg.diag(2 * x[1] * y[1])])
+
+    self.assertIsInstance(batch_jacobian, np.ndarray)
+    self.assertAllClose(batch_jacobian, answer)
+
 
 class FunctionTest(InteropTest):
 
