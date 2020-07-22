e3be70aa9d472acaeeeea161bc609dd959decc9b
Tomer Kaftan <kaftan@google.com>
2020-07-22 14:30:00 -0700

    Add a `convert_to_tensor` to the start of Tensor.__getitem__ (_slice_helper) to make sure it dispatches directly, rather than letting the nested tf.strided_slice trigger dispatching.
    This is important because `tensor.__getitem__` does some input arg manipulation before getting to the `tf.strided_slice`. So, when we try to run the traced code using the args provided to `strided_slice` (e.g. for KerasTensors), we lose information about constants that TPUs need to compile graphs involving shape manipulation. Tracing `__getitem__` and its input args directly does not seem to run into this problem.
    
    (Note: this TPU situation is separate from the shape value inferring we do in KerasTensors during Functional API construction/tracing time. This happens at model run-time when running the already-traced code)
    
    To get this all to work correctly in practice when dispatching KerasTensors + serializing/deserializing Keras models, this CL also has to:
    
    * Add special KerasTensor dispatchers for APIs that may take `slices` as inputs, to make sure they can trigger dispatching & serialize/deserialize correctly. This specialized dispatcher makes sure to unpack any `slices` in the args/kwargs into a namedtuple, before passing it to a specialized Keras TFOpLambda subclass that re-packs any slices.
    
    * Add serialization/deserialization support for `ellipsis` objects in Keras
    
    ------------------------
    Other considered alternatives to get the dispatching/serialization to work correctly for KerasTensors:
    
    * add flatten/pack support for slices to `tf.nest`/`tree`. This can be revisited in the future (especially re: dispatchv2), but tree is critical path code and it's not obvious if we should always be flattening/packing slices or not.
    
    * Make the dispatched __operators__.getitem method expect slices to have already been unwrapped, and add a step to the __getitem__ overriding that unwraps the slices. This would be somewhat clunky in practice because there are other TF apis that take `slice`s in their args as well, and it might be surprising to dispatch users that the __operators__.getitem dispatch doesn't actually match the standard __getitem__ api. Likewise it's unclear what the performance implication of doing extra packing/unpacking even when not dispatching would be.
    
    PiperOrigin-RevId: 322655930
    Change-Id: I35417577199393c016f753be685bf2926d62e753

--- tensorflow/python/keras/saving/saved_model/json_utils.py
+++ tensorflow/python/keras/saving/saved_model/json_utils.py
@@ -70,11 +70,14 @@ def decode(json_string):
 
 
 def _decode_helper(obj):
+  """A decoding helper that is TF-object aware."""
   if isinstance(obj, dict) and 'class_name' in obj:
     if obj['class_name'] == 'TensorShape':
       return tensor_shape.TensorShape(obj['items'])
     elif obj['class_name'] == '__tuple__':
       return tuple(_decode_helper(i) for i in obj['items'])
+    elif obj['class_name'] == '__ellipsis__':
+      return Ellipsis
   return obj
 
 
@@ -121,6 +124,9 @@ def get_json_type(obj):
 
   if isinstance(obj, collections_abc.Mapping):
     return dict(obj)
+
+  if obj is Ellipsis:
+    return {'class_name': '__ellipsis__'}
 
   if isinstance(obj, wrapt.ObjectProxy):
     return obj.__wrapped__
