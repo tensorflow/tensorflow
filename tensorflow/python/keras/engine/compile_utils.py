eeffaf8faa203c27667d04a1e1785d080e07f06c
bhack <bhack@users.noreply.github.com>
2020-07-27 12:08:48 +0000

    Linting

--- tensorflow/python/keras/engine/compile_utils.py
+++ tensorflow/python/keras/engine/compile_utils.py
@@ -420,11 +420,13 @@ class MetricsContainer(Container):
     """Convert user-supplied metrics to `Metric` objects."""
     metrics = nest.flatten(metrics)
     if self._loss_container:
-      losses = nest.map_structure(self._loss_container._get_loss_object, self._loss_container._losses)
+      losses = nest.map_structure(self._loss_container._get_loss_object,
+                                  self._loss_container._losses)
       losses = nest.flatten(losses)
-      return [self._get_metric_object(m, losses[idx], y_t, y_p) for idx, m in enumerate(metrics)]
+      return [self._get_metric_object(m, losses[idx], y_t, y_p) 
+              for idx, m in enumerate(metrics)]
     else:
-      return [self._get_metric_object(m, None , y_t, y_p) for m in metrics]
+      return [self._get_metric_object(m, None, y_t, y_p) for m in metrics]
   def _get_metric_object(self, metric, loss, y_t, y_p):
     """Converts user-supplied metric to a `Metric` object.
 
