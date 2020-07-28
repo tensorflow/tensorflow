3a7368d06f05757aa3e8764fa8428280903cb62b
Yong Tang <yong.tang.github@outlook.com>
2020-07-28 02:13:16 +0000

    Add qint8/qint16/quint8/quint16 support for tf.math.equal/tf/math.not_equal
    This PR add qint8/qint16/quint8/quint16 support for tf.math.equal/tf/math.not_equal,
    as was requested in 26069.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/core/ops/math_ops.cc
+++ tensorflow/core/ops/math_ops.cc
@@ -708,10 +708,7 @@ REGISTER_OP("GreaterEqual").COMPARISON();
       .Input("y: T")                                                       \
       .Output("z: bool")                                                   \
       .SetIsCommutative()                                                  \
-      .Attr(                                                               \
-          "T: {bfloat16, half, float, double, uint8, int8, int16, int32, " \
-          "int64, uint16, uint32, uint64, complex64, "                     \
-          "quint8, qint8, qint32, string, bool, complex128}")              \
+      .Attr("T: type")                                                     \
       .Attr("incompatible_shape_error: bool = true")                       \
       .SetShapeFn([](InferenceContext* c) {                                \
         ShapeHandle x = c->input(0);                                       \
