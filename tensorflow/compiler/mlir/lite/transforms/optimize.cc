02f600eb92dce044c75a89dddfede9e6da2c0f44
tg-at-google <taregaskin@google.com>
2020-07-29 16:18:42 -0400

    Update tensorflow/compiler/mlir/lite/transforms/optimize.cc
    Co-authored-by: Mihai Maruseac <mihai.maruseac@gmail.com>

--- tensorflow/compiler/mlir/lite/transforms/optimize.cc
+++ tensorflow/compiler/mlir/lite/transforms/optimize.cc
@@ -198,6 +198,7 @@ DenseElementsAttr GetShape(Value output_val) {
   auto output_type = output_val.getType().cast<RankedTensorType>();
   auto shape_vector = output_type.getShape();
   std::vector<int32_t> shape;
+  shape.reserve(shape_vector.size());
   for (auto shape_object : shape_vector) {
     shape.push_back(shape_object);
   }
