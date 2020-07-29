774441f2b9c8b5a6688bf552dbd8a5faf985dc8d
tg-at-google <taregaskin@google.com>
2020-07-28 21:05:23 -0400

    Update op_stats_to_overview_page.cc

--- tensorflow/core/profiler/convert/op_stats_to_overview_page.cc
+++ tensorflow/core/profiler/convert/op_stats_to_overview_page.cc
@@ -287,10 +287,10 @@ std::string TfFunctionRecommendationHtml(const TfFunctionDb& tf_function_db) {
   // Sorts candidates in descending order of expensive_call_percent.
   absl::c_sort(candidates, cmp);
   std::string expensive_functions = "";
-  const int64 num_functions_shown = std::min(
+  auto num_functions_shown = std::min(
       static_cast<decltype(candidates)::size_type>(3), candidates.size());
 
-  for (int64 i = 0; i < num_functions_shown; i++) {
+  for (int64 i = 0, end = num_functions_shown; i < end; i++) {
     if (i > 0) absl::StrAppend(&expensive_functions, ", ");
     absl::StrAppend(&expensive_functions, "\"", candidates[i].function_name,
                     "\"");
