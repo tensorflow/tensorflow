72107afe98becf1b764e7bbc3890057b144df065
pillowtann <pillowtann@gmail.com>
2020-07-30 18:09:40 +0800

    Resolved issue when recursively trying to create sub directories, will hit NotFoundError: Failed to create a directory

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -471,6 +471,7 @@ def recursive_create_dir_v2(path):
   """Creates a directory and all parent/intermediate directories.
 
   It succeeds if path already exists and is writable.
+  If RecursivelyCreateDir method does not work it tries os method.
 
   Args:
     path: string, name of the directory to be created
@@ -478,7 +479,11 @@ def recursive_create_dir_v2(path):
   Raises:
     errors.OpError: If the operation fails.
   """
-  _pywrap_file_io.RecursivelyCreateDir(compat.path_to_bytes(path))
+  try:
+    pywrap_tensorflow.RecursivelyCreateDir(compat.as_bytes(path))
+  except:
+    if not os.path.exists(path):
+      os.makedirs(path)
 
 
 @tf_export(v1=["gfile.Copy"])
