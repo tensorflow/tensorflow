ae10b73d6b70e2174613d83c3cbada09df4d4c48
A. Unique TensorFlower <gardener@tensorflow.org>
2020-07-31 08:07:11 -0700

    Optimize RearrangeWeights for fully connected to be cache efficient while reading weights.
    PiperOrigin-RevId: 324209359
    Change-Id: Ifa6dfff91b216e2e007bd4cefb8f3d814971d598

--- tensorflow/lite/delegates/gpu/cl/kernels/fully_connected_test.cc
+++ tensorflow/lite/delegates/gpu/cl/kernels/fully_connected_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include "tensorflow/lite/delegates/gpu/common/operations.h"
 #include "tensorflow/lite/delegates/gpu/common/status.h"
 
+using ::testing::ElementsAreArray;
 using ::testing::FloatNear;
 using ::testing::Pointwise;
 
@@ -59,6 +60,60 @@ TEST_F(OpenCLOperationTest, FullyConnected) {
       EXPECT_THAT(dst_tensor.data, Pointwise(FloatNear(eps), {14.5f, 37.5f}));
     }
   }
+}
+
+TEST_F(OpenCLOperationTest, RearrageWeights) {
+  tflite::gpu::Tensor<OHWI, DataType::FLOAT32> weights;
+  weights.shape = OHWI(8, 1, 1, 8);
+  weights.data = {0.0,  1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  10.0, 11.0,
+                  12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 20.0, 21.0, 22.0, 23.0,
+                  24.0, 25.0, 26.0, 27.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0,
+                  36.0, 37.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0,
+                  50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 60.0, 61.0,
+                  62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 70.0, 71.0, 72.0, 73.0,
+                  74.0, 75.0, 76.0, 77.0};
+
+  std::vector<float> expected_rearranged_data = {
+      0.0,  1.0,  2.0,  3.0,  10.0, 11.0, 12.0, 13.0, 20.0, 21.0, 22.0,
+      23.0, 30.0, 31.0, 32.0, 33.0, 40.0, 41.0, 42.0, 43.0, 50.0, 51.0,
+      52.0, 53.0, 60.0, 61.0, 62.0, 63.0, 70.0, 71.0, 72.0, 73.0, 4.0,
+      5.0,  6.0,  7.0,  14.0, 15.0, 16.0, 17.0, 24.0, 25.0, 26.0, 27.0,
+      34.0, 35.0, 36.0, 37.0, 44.0, 45.0, 46.0, 47.0, 54.0, 55.0, 56.0,
+      57.0, 64.0, 65.0, 66.0, 67.0, 74.0, 75.0, 76.0, 77.0,
+  };
+
+  std::vector<float> data(8 * 8);
+  float4* data_ptr = static_cast<float4*>(static_cast<void*>(data.data()));
+  RearrangeFCWeightsToIOO4I4(weights, absl::MakeSpan(data_ptr, 8 * 8 / 4));
+
+  EXPECT_THAT(data, ElementsAreArray(expected_rearranged_data));
+}
+
+TEST_F(OpenCLOperationTest, RearrageWeightsWhenPaddingIsRequired) {
+  tflite::gpu::Tensor<OHWI, DataType::FLOAT32> weights;
+  weights.shape = OHWI(7, 1, 1, 7);
+  weights.data = {
+      0.0,  1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  10.0, 11.0, 12.0,
+      13.0, 14.0, 15.0, 16.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0,
+      26.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 40.0, 41.0,
+      42.0, 43.0, 44.0, 45.0, 46.0, 50.0, 51.0, 52.0, 53.0, 54.0,
+      55.0, 56.0, 60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0,
+  };
+
+  std::vector<float> expected_rearranged_data = {
+      0.0,  1.0,  2.0,  3.0,  10.0, 11.0, 12.0, 13.0, 20.0, 21.0, 22.0,
+      23.0, 30.0, 31.0, 32.0, 33.0, 40.0, 41.0, 42.0, 43.0, 50.0, 51.0,
+      52.0, 53.0, 60.0, 61.0, 62.0, 63.0, 0.0,  0.0,  0.0,  0.0,  4.0,
+      5.0,  6.0,  0.0,  14.0, 15.0, 16.0, 0.0,  24.0, 25.0, 26.0, 0.0,
+      34.0, 35.0, 36.0, 0.0,  44.0, 45.0, 46.0, 0.0,  54.0, 55.0, 56.0,
+      0.0,  64.0, 65.0, 66.0, 0.0,  0.0,  0.0,  0.0,  0.0,
+  };
+
+  std::vector<float> data(8 * 8);
+  float4* data_ptr = static_cast<float4*>(static_cast<void*>(data.data()));
+  RearrangeFCWeightsToIOO4I4(weights, absl::MakeSpan(data_ptr, 8 * 8 / 4));
+
+  EXPECT_THAT(data, ElementsAreArray(expected_rearranged_data));
 }
 
 }  // namespace
