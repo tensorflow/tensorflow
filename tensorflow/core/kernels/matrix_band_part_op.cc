6453d1cf2412769ce07a471fcf927e24709d00e7
A. Unique TensorFlower <gardener@tensorflow.org>
2020-07-31 18:10:40 -0700

    Break up core/kernels/BUILD (part 1 of N):
    Move linear algebra kernels to subdirectory tensorflow/core/kernels/linalg with its own BUILD file.
    
    PiperOrigin-RevId: 324329298
    Change-Id: Id7ca60219a3a7c83ef37f0e62a11a48cff2b7679

---
+++ tensorflow/core/kernels/matrix_band_part_op.cc
@@ -0,0 +1,242 @@
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+// See docs in ../ops/array_ops.cc.
+
+#define EIGEN_USE_THREADS
+
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+#define EIGEN_USE_GPU
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+
+#include "tensorflow/core/kernels/matrix_band_part_op.h"
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/framework/types.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/macros.h"
+
+namespace tensorflow {
+
+typedef Eigen::ThreadPoolDevice CPUDevice;
+typedef Eigen::GpuDevice GPUDevice;
+
+template <typename Device, typename T>
+class MatrixBandPartOp : public OpKernel {
+ public:
+  explicit MatrixBandPartOp(OpKernelConstruction* context)
+      : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    const Tensor& input = context->input(0);
+    const TensorShape& input_shape = input.shape();
+    // Preliminary validation of sizes.
+    OP_REQUIRES(context, TensorShapeUtils::IsMatrixOrHigher(input_shape),
+                errors::InvalidArgument(
+                    "input must be at least 2-dim, received shape: ",
+                    input.shape().DebugString()));
+    auto input_reshaped = input.flat_inner_dims<T, 3>();
+
+    const Tensor& num_lower_in = context->input(1);
+    OP_REQUIRES(context, TensorShapeUtils::IsScalar(num_lower_in.shape()),
+                errors::InvalidArgument("num_lower must be scalar, got shape ",
+                                        num_lower_in.shape().DebugString()));
+
+    auto as_int64_scalar = [](const Tensor& tensor) -> int64 {
+      if (tensor.dtype() == DT_INT32) {
+        return tensor.scalar<int32>()();
+      } else {
+        return tensor.scalar<int64>()();
+      }
+    };
+    const int64 num_lower = as_int64_scalar(num_lower_in);
+    OP_REQUIRES(
+        context, num_lower <= input_reshaped.dimension(1),
+        errors::InvalidArgument(
+            "num_lower must be negative or less or equal to number of rows (",
+            input_reshaped.dimension(1), ") got: ", num_lower));
+
+    const Tensor& num_upper_in = context->input(2);
+    OP_REQUIRES(context, TensorShapeUtils::IsScalar(num_upper_in.shape()),
+                errors::InvalidArgument("num_upper must be scalar, got shape ",
+                                        num_upper_in.shape().DebugString()));
+    const int64 num_upper = as_int64_scalar(num_upper_in);
+    OP_REQUIRES(context, num_upper <= input_reshaped.dimension(2),
+                errors::InvalidArgument("num_upper must be negative or less or "
+                                        "equal to number of columns (",
+                                        input_reshaped.dimension(2),
+                                        ") got: ", num_upper));
+
+    if (input.NumElements() == 0 ||
+        ((num_lower < 0 || num_lower == input_reshaped.dimension(1)) &&
+         (num_upper < 0 || num_upper == input_reshaped.dimension(2)))) {
+      // This is a no-op.
+      context->set_output(0, input);
+      return;
+    }
+
+    Tensor* output = nullptr;
+    OP_REQUIRES_OK(context, context->forward_input_or_allocate_output(
+                                {0}, 0, input_shape, &output));
+    auto output_reshaped = output->flat_inner_dims<T, 3>();
+    functor::MatrixBandPartFunctor<Device, T> fn;
+    fn(context, context->eigen_device<Device>(), num_lower, num_upper,
+       input_reshaped, output_reshaped);
+  }
+
+ private:
+  TF_DISALLOW_COPY_AND_ASSIGN(MatrixBandPartOp);
+};
+
+#define REGISTER_MATRIX_BAND_PART(type)                                    \
+  REGISTER_KERNEL_BUILDER(                                                 \
+      Name("MatrixBandPart").Device(DEVICE_CPU).TypeConstraint<type>("T"), \
+      MatrixBandPartOp<CPUDevice, type>);
+TF_CALL_POD_TYPES(REGISTER_MATRIX_BAND_PART);
+#undef REGISTER_MATRIX_BAND_PART
+
+// Registration of the deprecated kernel.
+// Delete after 10mar2017.
+#define REGISTER_BATCH_MATRIX_BAND_PART(type)             \
+  REGISTER_KERNEL_BUILDER(Name("BatchMatrixBandPart")     \
+                              .Device(DEVICE_CPU)         \
+                              .TypeConstraint<type>("T"), \
+                          MatrixBandPartOp<CPUDevice, type>);
+TF_CALL_NUMBER_TYPES(REGISTER_BATCH_MATRIX_BAND_PART);
+#undef REGISTER_BATCH_MATRIX_BAND_PART
+
+// Implementation of the functor specialization for CPU.
+namespace functor {
+
+// CPU implementation of BandPartFunctor.
+typedef Eigen::ThreadPoolDevice CPUDevice;
+
+template <typename Scalar>
+struct MatrixBandPartFunctor<CPUDevice, Scalar> {
+  void operator()(OpKernelContext* context, const CPUDevice& device,
+                  int num_lower_diags, int num_upper_diags,
+                  typename TTypes<Scalar, 3>::ConstTensor input,
+                  typename TTypes<Scalar, 3>::Tensor output) {
+    const int64 b = input.dimension(0);
+    const int64 m = input.dimension(1);
+    const int64 n = input.dimension(2);
+    auto thread_pool =
+        context->device()->tensorflow_cpu_worker_threads()->workers;
+    const int64 total_rows = b * m;
+    const int64 row_cost = 10 * n;
+    const bool in_place = input.data() == output.data();
+    auto compute_shard = [=, &input, &output](int64 begin, int64 end) {
+      if (!in_place) {
+        std::fill(output.data() + begin * n, output.data() + end * n,
+                  Scalar(0));
+      }
+      const int64 batch_begin = begin / m;
+      const int64 batch_end = (end + m - 1) / m;
+      for (int64 batch = batch_begin; batch < batch_end; ++batch) {
+        const int64 row_begin = begin > batch * m ? begin % m : 0;
+        const int64 row_end = end < (batch + 1) * m ? end % m : m;
+        for (int64 row = row_begin; row < row_end; ++row) {
+          const int64 band_start =
+              num_lower_diags < 0
+                  ? 0
+                  : std::min(n, std::max(int64{0}, row - num_lower_diags));
+          const int64 band_end =
+              num_upper_diags < 0
+                  ? n
+                  : std::min(static_cast<int64>(n), row + num_upper_diags + 1);
+          if (in_place) {
+            if (band_start > 0) {
+              std::fill(&output(batch, row, 0), &output(batch, row, band_start),
+                        Scalar(0));
+            }
+            if (band_end < n) {
+              std::fill(&output(batch, row, band_end), &output(batch, row, n),
+                        Scalar(0));
+            }
+          } else {
+            if (band_start < band_end) {
+              const Eigen::DSizes<Eigen::DenseIndex, 3> indices(batch, row,
+                                                                band_start);
+              const Eigen::DSizes<Eigen::DenseIndex, 3> sizes(
+                  1, 1, band_end - band_start);
+              output.slice(indices, sizes) = input.slice(indices, sizes);
+            }
+          }
+        }
+      }
+    };
+    thread_pool->ParallelFor(total_rows, row_cost, std::move(compute_shard));
+  }
+};
+
+#define DEFINE_CPU_SPEC(T) template struct MatrixBandPartFunctor<CPUDevice, T>;
+TF_CALL_POD_TYPES(DEFINE_CPU_SPEC);
+#undef DEFINE_CPU_SPEC
+
+}  // namespace functor
+
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+
+// Forward declarations of the functor specializations for GPU.
+namespace functor {
+#define DECLARE_GPU_SPEC(T)                                            \
+  template <>                                                          \
+  struct MatrixBandPartFunctor<GPUDevice, T> {                         \
+    void operator()(OpKernelContext* context, const GPUDevice& device, \
+                    int num_upper_diags, int num_lower_diags,          \
+                    typename TTypes<T, 3>::ConstTensor input,          \
+                    typename TTypes<T, 3>::Tensor output);             \
+  };                                                                   \
+  extern template struct MatrixBandPartFunctor<GPUDevice, T>;
+
+TF_CALL_GPU_ALL_TYPES(DECLARE_GPU_SPEC);
+#undef DECLARE_GPU_SPEC
+}  // namespace functor
+
+// Registration of the GPU implementations.
+#define REGISTER_MATRIX_BAND_PART_GPU(type)              \
+  REGISTER_KERNEL_BUILDER(Name("MatrixBandPart")         \
+                              .Device(DEVICE_GPU)        \
+                              .TypeConstraint<type>("T") \
+                              .HostMemory("num_lower")   \
+                              .HostMemory("num_upper"),  \
+                          MatrixBandPartOp<GPUDevice, type>);
+TF_CALL_GPU_ALL_TYPES(REGISTER_MATRIX_BAND_PART_GPU);
+#undef REGISTER_MATRIX_BAND_PART_GPU
+
+// Registration of the deprecated kernel.
+// Delete after 10mar2017.
+#define REGISTER_BATCH_MATRIX_BAND_PART_GPU(type)        \
+  REGISTER_KERNEL_BUILDER(Name("BatchMatrixBandPart")    \
+                              .Device(DEVICE_GPU)        \
+                              .TypeConstraint<type>("T") \
+                              .HostMemory("num_lower")   \
+                              .HostMemory("num_upper"),  \
+                          MatrixBandPartOp<GPUDevice, type>);
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_BATCH_MATRIX_BAND_PART_GPU);
+#undef REGISTER_BATCH_MATRIX_BAND_PART_GPU
+
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+
+}  // namespace tensorflow
---
+++ tensorflow/core/kernels/svd_op_gpu.cu.cc
@@ -0,0 +1,439 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+// See docs in ../ops/linalg_ops.cc.
+// TODO(shamanDevel): Enable complex inputs. This will require a specialization
+//                    of Gesvd for complex inputs as well as a new kernel
+//                    definition to output the singular values as reals
+//                    instead of complex values. The current CPU implementation
+//                    outputs the singular values as complex values and then
+//                    casts them to reals in the python wrapper.
+// TODO(rmlarsen/shamanDevel): This could use a bit of cleanup. We don't need to
+// pass quite as many raw pointers around. Would also be nice to reduce code
+// duplication.
+
+#if GOOGLE_CUDA
+#define EIGEN_USE_GPU
+
+#include <algorithm>
+#include <vector>
+
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/framework/kernel_def_builder.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/register_types.h"
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/framework/types.h"
+#include "tensorflow/core/kernels/cuda_solvers.h"
+#include "tensorflow/core/kernels/eye_functor.h"
+#include "tensorflow/core/kernels/linalg_ops_common.h"
+#include "tensorflow/core/kernels/transpose_functor.h"
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/stream_executor.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/util/gpu_kernel_helper.h"
+
+namespace tensorflow {
+
+static const char kErrMsg[] =
+    "Singular Value Decomposition was not successful. The input might not be "
+    "valid.";
+
+typedef Eigen::GpuDevice GPUDevice;
+
+namespace {
+// This kernel computes the reduction
+// V' = sum_i (M_i * U_i,1 * S_i).
+// The result is stored in V[batch] and has the same sign as the
+// real value of V (which should be computed)
+template <class Scalar>
+__global__ void ComputeValueOfVKernel(Gpu2DLaunchConfig config, int64 m,
+                                      int64 ldu, const Scalar* __restrict__ M,
+                                      const Scalar* __restrict__ U,
+                                      const Scalar* __restrict__ S,
+                                      Scalar* __restrict__ V) {
+  GPU_AXIS_KERNEL_LOOP(batch, config.virtual_thread_count.x, X) {
+    GPU_AXIS_KERNEL_LOOP(i, config.virtual_thread_count.y, Y) {
+      Scalar v = M[i + m * batch] * U[ldu * (i + m * batch)] * S[batch];
+      GpuAtomicAdd(V + batch, v);
+    }
+  }
+}
+
+// Extracts the sign of V
+// V[i] = V[i]>=0 ? 1 : 0
+template <class Scalar>
+__global__ void ExtractSignOfVKernel(GpuLaunchConfig config,
+                                     Scalar* __restrict__ V) {
+  GPU_1D_KERNEL_LOOP(i, config.virtual_thread_count) {
+    V[i] = V[i] >= 0 ? Scalar(1) : Scalar(-1);
+  }
+}
+}  // namespace
+
+// Scalar: The input scalar type (can be complex)
+template <class Scalar>
+class SvdOpGpu : public AsyncOpKernel {
+ public:
+  using RealScalar = typename Eigen::NumTraits<Scalar>::Real;
+
+  explicit SvdOpGpu(OpKernelConstruction* context) : AsyncOpKernel(context) {
+    OP_REQUIRES_OK(context, context->GetAttr("compute_uv", &compute_uv_));
+    OP_REQUIRES_OK(context, context->GetAttr("full_matrices", &full_matrices_));
+  }
+
+  void RunSVD(OpKernelContext* context, DoneCallback done, int64 m, int64 n,
+              int64 p, Tensor& M_copy, Tensor* S, Tensor* U, Tensor* V,
+              std::unique_ptr<CudaSolver> solver) {
+    // Compute U S V* = M.
+    // 1. cuSolver works in column-major rather than row-major.
+    // 2. Gesvd returns V*. GesvdjBatched returns V.
+    // 3. Hence M should be transposed before input and
+    //    a) U (rather than V) should be transposed on output with Gesvd.
+    //    b) U and V should be transposed on output with GesvdjBatched.
+
+    // get the pointers to input data
+    Scalar* input_ptr;
+    RealScalar* outputS_ptr;
+    auto input_reshaped = M_copy.template flat_inner_dims<Scalar, 3>();
+    input_ptr = input_reshaped.data();
+    const int64 batch_size =
+        M_copy.dims() > 2 ? input_reshaped.dimension(0) : 1;
+    // Gesvdjbatched handles matrices up to 32x32.
+    // TODO(jamessspencer): if not full_matrices, compute full U and V matrices
+    // using Gesvdjbatched and return slices.
+    const bool batched =
+        m <= 32 && n <= 32 && batch_size > 1 && (full_matrices_ || m == n);
+
+    // Copies of U and V if required so can take transposes after SVD.
+    Tensor u_copy, v_copy;
+    Scalar* outputU_ptr = NULL;
+    Scalar* outputV_ptr = NULL;
+    if (compute_uv_ || batched) {
+      TensorShape u_shape, v_shape;
+      if (batched) {
+        // Gesvdjbatched seems to require U and V matrices even if the vectors
+        // aren't computed.
+        TensorShape shapeRaw = M_copy.shape();
+        shapeRaw.RemoveLastDims(2);
+        u_shape = shapeRaw;
+        u_shape.AddDim(m);
+        u_shape.AddDim(m);
+        v_shape = shapeRaw;
+        v_shape.AddDim(n);
+        v_shape.AddDim(n);
+      } else if (full_matrices_) {
+        u_shape = U->shape();
+        v_shape = V->shape();
+      } else {
+        TensorShape shapeRaw = M_copy.shape();
+        shapeRaw.RemoveLastDims(2);
+        u_shape = shapeRaw;
+        u_shape.AddDim(p);
+        u_shape.AddDim(m);
+        v_shape = shapeRaw;
+        v_shape.AddDim(p);
+        v_shape.AddDim(n);
+      }
+      OP_REQUIRES_OK_ASYNC(
+          context, solver->allocate_scoped_tensor(U->dtype(), u_shape, &u_copy),
+          done);
+      if (batched) {
+        OP_REQUIRES_OK_ASYNC(
+            context,
+            solver->allocate_scoped_tensor(V->dtype(), v_shape, &v_copy), done);
+      }
+      outputU_ptr = u_copy.template flat_inner_dims<Scalar, 3>().data();
+      if (batched) {
+        outputV_ptr = v_copy.template flat_inner_dims<Scalar, 3>().data();
+      } else {
+        outputV_ptr = V->template flat_inner_dims<Scalar, 3>().data();
+      }
+    }
+
+    outputS_ptr = S->template flat_inner_dims<RealScalar, 2>().data();
+    std::vector<DeviceLapackInfo> dev_info;
+    dev_info.push_back(solver->GetDeviceLapackInfo(batch_size, "gesvd"));
+    int* dev_info_ptr = dev_info.back().mutable_data();
+
+    // Save the input matrix
+    // Needed for the n=1 fix, see below, since SVD destroys the input
+    Tensor input_copy;
+    if (compute_uv_ && n == 1) {
+      OP_REQUIRES_OK_ASYNC(context,
+                           solver->allocate_scoped_tensor(
+                               DataTypeToEnum<Scalar>::v(),
+                               TensorShape({batch_size, m}), &input_copy),
+                           done);
+      const GPUDevice& d = context->eigen_device<GPUDevice>();
+      d.memcpy(input_copy.flat<Scalar>().data(), input_ptr,
+               batch_size * m * sizeof(Scalar));
+    }
+
+    if (batched) {
+      cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_NOVECTOR;
+      if (compute_uv_) jobz = CUSOLVER_EIG_MODE_VECTOR;
+      OP_REQUIRES_OK_ASYNC(
+          context,
+          solver->GesvdjBatched(jobz, m, n, input_ptr, m, outputS_ptr,
+                                outputU_ptr, m, outputV_ptr, n, dev_info_ptr,
+                                batch_size),
+          done);
+    } else {
+      for (int64 batch = 0; batch < batch_size; ++batch) {
+        Scalar* input = input_ptr + batch * m * n;
+        RealScalar* outputS = outputS_ptr + batch * p;
+        Scalar* outputU = NULL;
+        Scalar* outputVT = NULL;
+        char jobu = 'N';
+        char jobvt = 'N';
+
+        if (compute_uv_) {
+          if (full_matrices_) {
+            outputU = outputU_ptr + batch * m * m;
+            outputVT = outputV_ptr + batch * n * n;
+            jobu = 'A';
+            jobvt = 'A';
+          } else {
+            outputU = outputU_ptr + batch * m * p;
+            outputVT = outputV_ptr + batch * n * p;
+            jobu = 'S';
+            jobvt = 'S';
+          }
+        }
+
+        OP_REQUIRES_OK_ASYNC(
+            context,
+            solver->Gesvd(jobu, jobvt, m, n, input, m, outputS, outputU, m,
+                          outputVT, n, dev_info_ptr + batch),
+            done);
+      }
+    }
+
+    // This is a bug in cuSolver:
+    // If n is one, then outputVT only contains zeros instead of ones.
+    // Hence, I need to fill outputVT manually
+    // The question is: +1 or -1?
+    // -> Compute U*S and compare sign against M
+    // But because S is zero except for the first entry, the multiplication
+    // simplifies a lot.
+    // However, what happens if M contains zeros? At these indices, it is
+    // impossible to determine the value of V.
+    // -> Compute V for all rows in M to cope for zeros.
+    // 1. V' = sum_i (M_i * U_i,1 * S_i)
+    // 2. V = {1, V'>=0, -1, V'<0}
+    // TODO: what is with complex values?
+    if (compute_uv_ && n == 1) {
+      // 1. compute the (batched) sum
+      const GPUDevice& d = context->eigen_device<GPUDevice>();
+      d.memset(outputV_ptr, 0, batch_size * sizeof(Scalar));
+      Gpu2DLaunchConfig cfg2D = GetGpu2DLaunchConfig(batch_size, m, d);
+      TF_CHECK_OK(GpuLaunchKernel(ComputeValueOfVKernel<Scalar>,
+                                  cfg2D.block_count, cfg2D.thread_per_block, 0,
+                                  d.stream(), cfg2D, m, full_matrices_ ? m : p,
+                                  input_copy.flat<Scalar>().data(), outputU_ptr,
+                                  outputS_ptr, outputV_ptr));
+      // 2. clamp V to -1 or +1
+      GpuLaunchConfig cfg1D = GetGpuLaunchConfig(batch_size, d);
+      TF_CHECK_OK(GpuLaunchKernel(ExtractSignOfVKernel<Scalar>,
+                                  cfg1D.block_count, cfg1D.thread_per_block, 0,
+                                  d.stream(), cfg1D, outputV_ptr));
+    }
+
+    if (compute_uv_) {
+      auto device = context->eigen_device<GPUDevice>();
+      OP_REQUIRES_OK_ASYNC(context, DoMatrixTranspose(device, u_copy, U), done);
+      if (batched) {
+        OP_REQUIRES_OK_ASYNC(context, DoMatrixTranspose(device, v_copy, V),
+                             done);
+      }
+    }
+
+    CheckResult(context, std::move(done), dev_info, std::move(solver));
+  }
+
+  void CheckResult(OpKernelContext* context, DoneCallback done,
+                   const std::vector<DeviceLapackInfo>& dev_info,
+                   std::unique_ptr<CudaSolver> solver) {
+    auto info_checker = [context, done](
+                            const Status& status,
+                            const std::vector<HostLapackInfo>& /* unused */) {
+      Status full_status = status;
+      if (!full_status.ok()) {
+        full_status.Update(errors::InvalidArgument(kErrMsg));
+      }
+      OP_REQUIRES_OK_ASYNC(context, full_status, done);
+      done();
+    };
+
+    CudaSolver::CheckLapackInfoAndDeleteSolverAsync(std::move(solver), dev_info,
+                                                    std::move(info_checker));
+  }
+
+  // The SVD if m >= n
+  // TODO: can the two cases (MgeqN and MlessN) be simplified,
+  //   common boilerplate be reduced, or even combined in one method?
+  void PerformSVD_MgeqN(OpKernelContext* context, DoneCallback done, int64 m,
+                        int64 n, int64 p, const Tensor& M, Tensor* S, Tensor* U,
+                        Tensor* V) {
+    // Transpose M, because cuSolver expects it to be column-major
+    TensorShape shapeRaw = M.shape();
+    shapeRaw.RemoveLastDims(2);
+    TensorShape input_shape = shapeRaw;
+    input_shape.AddDim(n);
+    input_shape.AddDim(m);
+    Tensor input_copy;
+    // TODO(rmlarsen): Convert to std::make_unique when available.
+    std::unique_ptr<CudaSolver> solver(new CudaSolver(context));
+    OP_REQUIRES_OK_ASYNC(
+        context,
+        solver->allocate_scoped_tensor(M.dtype(), input_shape, &input_copy),
+        done);
+    auto device = context->eigen_device<GPUDevice>();
+    OP_REQUIRES_OK_ASYNC(context, DoMatrixTranspose(device, M, &input_copy),
+                         done);
+
+    // Call the SVD: compute U S V* = M.
+    RunSVD(context, done, m, n, p, input_copy, S, U, V, std::move(solver));
+  }
+
+  // The SVD if m < n
+  void PerformSVD_MlessN(OpKernelContext* context, DoneCallback done, int64 m,
+                         int64 n, int64 p, const Tensor& M, Tensor* S,
+                         Tensor* U, Tensor* V) {
+    // Perform the SVD on M'. cuSolver works column major so don't need to
+    // transpose M.
+
+    // Reuse the input buffer or make a copy for the SVD depending on whether
+    // this op owns the input buffer exclusively. This is needed because the
+    // SVD modifies the input
+    // TODO(rmlarsen): Convert to std::make_unique when available.
+    std::unique_ptr<CudaSolver> solver(new CudaSolver(context));
+    Tensor input_copy;
+    OP_REQUIRES_OK_ASYNC(
+        context,
+        solver->forward_input_or_allocate_scoped_tensor(
+            {0}, DataTypeToEnum<Scalar>::value, M.shape(), &input_copy),
+        done);
+
+    if (!M.SharesBufferWith(input_copy)) {
+      const GPUDevice& d = context->eigen_device<GPUDevice>();
+      d.memcpy(input_copy.flat<Scalar>().data(), M.flat<Scalar>().data(),
+               M.NumElements() * sizeof(Scalar));
+    }
+
+    // Call the SVD: compute V S U* = M*.
+    // Note (m, n) and (U, V) are swapped accordingly.
+    RunSVD(context, done, n, m, p, input_copy, S, V, U, std::move(solver));
+  }
+
+  void ComputeAsync(OpKernelContext* context, DoneCallback done) final {
+    const Tensor& input = context->input(0);
+    const int ndims = input.dims();
+    const int64 m = input.dim_size(ndims - 2);
+    const int64 n = input.dim_size(ndims - 1);
+    const int64 p = std::min(m, n);
+
+    // Validate inputs.
+    OP_REQUIRES_ASYNC(
+        context, ndims >= 2,
+        errors::InvalidArgument("Input must have rank >= 2, got ", ndims),
+        done);
+
+    // output tensors.
+    Tensor* outputU = NULL;
+    Tensor* outputS = NULL;
+    Tensor* outputV = NULL;
+
+    // compute  shapes
+    TensorShape shapeRaw = input.shape();
+    shapeRaw.RemoveLastDims(2);
+    TensorShape shapeS = shapeRaw;
+    TensorShape shapeU = shapeRaw;
+    TensorShape shapeV = shapeRaw;
+    shapeS.AddDim(p);
+    if (compute_uv_) {
+      if (full_matrices_) {
+        shapeU.AddDim(m);
+        shapeU.AddDim(m);
+        shapeV.AddDim(n);
+        shapeV.AddDim(n);
+      } else {
+        shapeU.AddDim(m);
+        shapeU.AddDim(p);
+        shapeV.AddDim(n);
+        shapeV.AddDim(p);
+      }
+    } else {
+      shapeU = TensorShape({0});
+      shapeV = TensorShape({0});
+    }
+
+    // allocate output
+    OP_REQUIRES_OK_ASYNC(context, context->allocate_output(0, shapeS, &outputS),
+                         done);
+    OP_REQUIRES_OK_ASYNC(context, context->allocate_output(1, shapeU, &outputU),
+                         done);
+    OP_REQUIRES_OK_ASYNC(context, context->allocate_output(2, shapeV, &outputV),
+                         done);
+
+    if (n == 0 || m == 0) {
+      if (n == m || !compute_uv_ || !full_matrices_) {
+        // S, U, and V are all empty. Nothing to do.
+        done();
+        return;
+      }
+      auto device = context->eigen_device<GPUDevice>();
+      functor::EyeFunctor<GPUDevice, Scalar> eye;
+      if (m > 0) {
+        // Return a full canonical basis for the column space.
+        auto outputU_reshaped = outputU->flat_inner_dims<Scalar, 3>();
+        eye(device, outputU_reshaped);
+      } else if (n > 0) {
+        // Return a full canonical basis for the row space.
+        auto outputV_reshaped = outputV->flat_inner_dims<Scalar, 3>();
+        eye(device, outputV_reshaped);
+      }
+      done();
+      return;
+    }
+
+    // call implementations
+    if (m >= n) {
+      PerformSVD_MgeqN(context, done, m, n, p, input, outputS, outputU,
+                       outputV);
+    } else {
+      PerformSVD_MlessN(context, done, m, n, p, input, outputS, outputU,
+                        outputV);
+    }
+  }
+
+ private:
+  bool compute_uv_;
+  bool full_matrices_;
+};
+
+// TODO: add support for complex types
+REGISTER_LINALG_OP_GPU("Svd", (SvdOpGpu<float>), float);
+REGISTER_LINALG_OP_GPU("Svd", (SvdOpGpu<double>), double);
+
+// Deprecated kernels.
+REGISTER_LINALG_OP_GPU("BatchSvd", (SvdOpGpu<float>), float);
+REGISTER_LINALG_OP_GPU("BatchSvd", (SvdOpGpu<double>), double);
+
+}  // namespace tensorflow
+
+#endif  // GOOGLE_CUDA
