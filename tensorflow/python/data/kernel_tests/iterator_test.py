21507a97c18c72406511ec0cbaf4b7ee46b2ddc0
Ilya Persky <ilya.persky@gmail.com>
2020-08-02 11:23:43 +0300

    Fixing old "spec" term to new "signature"

--- tensorflow/python/data/kernel_tests/iterator_test.py
+++ tensorflow/python/data/kernel_tests/iterator_test.py
@@ -946,9 +946,9 @@ class IteratorTest(test_base.DatasetTestBase, parameterized.TestCase):
 
     @def_function.function
     def fn():
-      output_spec = tensor_spec.TensorSpec((), dtypes.int64)
+      output_signature = tensor_spec.TensorSpec((), dtypes.int64)
       dataset = dataset_ops._GeneratorDataset(1, init_fn, next_fn, finalize_fn,
-                                              output_spec)
+                                              output_signature)
       iterator = iter(dataset)
       next(iterator)
 
