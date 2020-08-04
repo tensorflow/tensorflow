db7f4c409c1fe6a6c9855b57b3321fd3c621ca30
Ilya Persky <ilya.persky@gmail.com>
2020-08-04 12:48:53 +0300

    Minor fixed to testFromGeneratorRaggedTensor

--- tensorflow/python/data/kernel_tests/from_generator_test.py
+++ tensorflow/python/data/kernel_tests/from_generator_test.py
@@ -457,20 +457,18 @@ class FromGeneratorTest(test_base.DatasetTestBase, parameterized.TestCase):
   def testFromGeneratorRaggedTensor(self):
 
     def generator():
-      yield ragged_factory_ops.constant([[1, 2], [3]],
-                                        dtype=dtypes.int64,
-                                        ragged_rank=1)
+      yield ragged_factory_ops.constant([[1, 2], [3]])
 
     dataset = dataset_ops.Dataset.from_generator(
         generator,
         output_signature=ragged_tensor.RaggedTensorSpec(
-            shape=(2, None), dtype=dtypes.int64))
+            shape=(2, None), dtype=dtypes.int32))
     get_next = self.getNext(dataset)
 
     ret = get_next()
 
     self.assertIsInstance(ret, ragged_tensor.RaggedTensor)
-    self.assertAllEqual([1, 2, 3], ret.values)
+    self.assertAllEqual([[1, 2], [3]], ret)
 
   @combinations.generate(test_base.default_test_combinations())
   def testFromGeneratorSparseTensor(self):
