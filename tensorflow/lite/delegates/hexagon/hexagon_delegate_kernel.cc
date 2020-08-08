902abbe41e198d58446896dacc4f00bc5e7bac7d
A. Unique TensorFlower <gardener@tensorflow.org>
2020-08-07 20:12:44 -0700

    Add cache for const nodes in hexagon delegate. On few test models this reduces the size of const nodes by half, which will reduce graph preparation time. Bug fix for sometimes wrong casting. Remove some redundant const nodes.
    PiperOrigin-RevId: 325554729
    Change-Id: I805afc4151d99a1ed2f739accc7f86d6336b14b7

--- tensorflow/lite/delegates/hexagon/hexagon_delegate_kernel.cc
+++ tensorflow/lite/delegates/hexagon/hexagon_delegate_kernel.cc
@@ -264,9 +264,8 @@ TfLiteStatus HexagonDelegateKernel::BuildGraph(
       if (tensor_id == -1) continue;
       const auto& input_tensor = context->tensors[tensor_id];
       if (input_tensor.allocation_type == kTfLiteMmapRo) {
-        builder_->AddConstNodeWithData(
-            tensor_id, input_tensor,
-            /*int8_to_uint8*/ (input_tensor.type == kTfLiteInt8));
+        builder_->AddConstNodeWithData(tensor_id, input_tensor,
+                                       /*int8_to_uint8*/ true);
       }
     }
     auto* op_builder =
