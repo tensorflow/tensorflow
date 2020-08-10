145b3526825d5185d25f561976b9c1a29a4ee8a9
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-08-10 23:12:18 +1000

    Fix  cuda_libdevice_path.cc

--- tensorflow/core/platform/default/cuda_libdevice_path.cc
+++ tensorflow/core/platform/default/cuda_libdevice_path.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "absl/strings/match.h"
+#include "absl/strings/str_split.h"
 #include "tensorflow/core/platform/cuda_libdevice_path.h"
 
 #include <stdlib.h>
