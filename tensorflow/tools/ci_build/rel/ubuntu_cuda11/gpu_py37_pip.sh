a5d7f8181004d585838a1e047c397e2e7ad99f0e
Pankaj Kanwar <pkanwar@google.com>
2020-08-11 13:17:40 -0700

    Change test file for cuda11 for pip build.
    PiperOrigin-RevId: 326087677
    Change-Id: I40cd3e172128431f881912565d20c68cd9665167

--- tensorflow/tools/ci_build/rel/ubuntu_cuda11/gpu_py37_pip.sh
+++ tensorflow/tools/ci_build/rel/ubuntu_cuda11/gpu_py37_pip.sh
@@ -36,13 +36,23 @@ source tensorflow/tools/ci_build/build_scripts/DEFAULT_TEST_TARGETS.sh
 
 # Export optional variables for running pip.sh
 export TF_TEST_FILTER_TAGS='gpu,requires-gpu,-no_gpu,-no_oss,-oss_serial,-no_oss_py37'
-export TF_BUILD_FLAGS="--config=release_gpu_linux "
+# TODO (pkanwar): Revert this CL (cl/326069644) once the cuda 11 migration is complete.
+export TF_BUILD_FLAGS="--config=release_common "
 export TF_TEST_FLAGS="--test_tag_filters=${TF_TEST_FILTER_TAGS} --build_tag_filters=${TF_TEST_FILTER_TAGS} \
 --distinct_host_configuration=false \
 --action_env=TF_CUDA_VERSION=11 --action_env=TF_CUDNN_VERSION=8 --test_env=TF2_BEHAVIOR=1 \
 --config=cuda --test_output=errors --local_test_jobs=4 --test_lang_filters=py \
 --verbose_failures=true --keep_going --define=no_tensorflow_py_deps=true \
---run_under=//tensorflow/tools/ci_build/gpu_build:parallel_gpu_execute "
+--run_under=//tensorflow/tools/ci_build/gpu_build:parallel_gpu_execute \
+--config=cuda \
+--config=tensorrt \
+--action_env=CUDA_TOOLKIT_PATH=/usr/local/cuda-11.0 --action_env=TF_NEED_TENSORRT=1 \
+--action_env=TF_CUDA_COMPUTE_CAPABILITIES=sm_35,sm_37,sm_52,sm_60,sm_61,compute_70 \
+--action_env=TENSORRT_INSTALL_PATH=/usr/local/tensorrt \
+--action_env=LD_LIBRARY_PATH=/usr/local/cuda:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/tensorrt/lib \
+--action_env=GCC_HOST_COMPILER_PATH=/usr/bin/gcc-5 \
+--config=avx_linux \
+--crosstool_top=//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010-nvcc-cuda11:toolchain"
 export TF_TEST_TARGETS="${DEFAULT_BAZEL_TARGETS} -//tensorflow/lite/... "
 export TF_PIP_TESTS="test_pip_virtualenv_non_clean test_pip_virtualenv_clean"
 #export IS_NIGHTLY=0 # Not nightly; uncomment if building from tf repo.
