706c737689c7a25de5eea9cdda35d76c0bff1d99
Leon Shams <52867365+LeonShams@users.noreply.github.com>
2020-08-12 14:53:02 -0700

    Update core_test.py

--- tensorflow/python/keras/layers/core_test.py
+++ tensorflow/python/keras/layers/core_test.py
@@ -541,6 +541,57 @@ class CoreLayersTest(keras_parameterized.TestCase):
     self.assertEqual(layer.kernel.constraint, k_constraint)
     self.assertEqual(layer.bias.constraint, b_constraint)
 
+  def test_noisy_dense(self):
+    testing_utils.layer_test(
+        keras.layers.NoisyDense, kwargs={'units': 3}, input_shape=(3, 2))
+
+    testing_utils.layer_test(
+        keras.layers.NoisyDense, kwargs={'units': 3}, input_shape=(3, 4, 2))
+
+    testing_utils.layer_test(
+        keras.layers.NoisyDense, kwargs={'units': 3}, input_shape=(None, None, 2))
+
+    testing_utils.layer_test(
+        keras.layers.NoisyDense, kwargs={'units': 3}, input_shape=(3, 4, 5, 2))
+
+  def test_noisy_dense_dtype(self):
+    inputs = ops.convert_to_tensor_v2(
+        np.random.randint(low=0, high=7, size=(2, 2)))
+    layer = keras.layers.NoisyDense(5, dtype='float32')
+    outputs = layer(inputs)
+    self.assertEqual(outputs.dtype, 'float32')
+
+  def test_noisy_dense_with_policy(self):
+    inputs = ops.convert_to_tensor_v2(
+        np.random.randint(low=0, high=7, size=(2, 2)))
+    layer = keras.layers.NoisyDense(5, dtype=policy.Policy('mixed_float16'))
+    outputs = layer(inputs)
+    output_signature = layer.compute_output_signature(
+        tensor_spec.TensorSpec(dtype='float16', shape=(2, 2)))
+    self.assertEqual(output_signature.dtype, dtypes.float16)
+    self.assertEqual(output_signature.shape, (2, 5))
+    self.assertEqual(outputs.dtype, 'float16')
+    self.assertEqual(layer.kernel.dtype, 'float32')
+
+  def test_noisy_dense_regularization(self):
+    layer = keras.layers.NoisyDense(
+        3,
+        kernel_regularizer=keras.regularizers.l1(0.01),
+        bias_regularizer='l1',
+        activity_regularizer='l2',
+        name='noisy_dense_reg')
+    layer(keras.backend.variable(np.ones((2, 4))))
+    self.assertEqual(3, len(layer.losses))
+
+  def test_noisy_dense_constraints(self):
+    k_constraint = keras.constraints.max_norm(0.01)
+    b_constraint = keras.constraints.max_norm(0.01)
+    layer = keras.layers.NoisyDense(
+        3, kernel_constraint=k_constraint, bias_constraint=b_constraint)
+    layer(keras.backend.variable(np.ones((2, 4))))
+    self.assertEqual(layer.kernel.constraint, k_constraint)
+    self.assertEqual(layer.bias.constraint, b_constraint)
+
   def test_activity_regularization(self):
     layer = keras.layers.ActivityRegularization(l1=0.1)
     layer(keras.backend.variable(np.ones((2, 4))))
