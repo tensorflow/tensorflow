f6b3dec1b034299f7c1d6d8c7a7139c3e0a07ad2
Eugene Brevdo <ebrevdo@google.com>
2020-08-14 09:26:52 -0700

    [TF DistStrat] Add proper __deepcopy__ support for all DistributedVariable objects (eager mode only).
    PiperOrigin-RevId: 326669797
    Change-Id: I3f7ec350efdd2c25456fce920ea43a2aa8ba373d

--- tensorflow/python/distribute/values.py
+++ tensorflow/python/distribute/values.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import copy
 
 from tensorflow.python.distribute import device_util
 from tensorflow.python.distribute import distribute_lib
@@ -471,6 +472,41 @@ class DistributedVariable(DistributedDelegate, variables_lib.Variable,
     # Set a VariablePolicy which decides how we replicate/aggregate the given
     # variable.
     self._policy = var_policy
+
+  def __deepcopy__(self, memo):
+    """Perform a deepcopy of the `DistributedVariable`.
+
+    Unlike the deepcopy of a regular tf.Variable, this keeps the original
+    strategy and devices of the `DistributedVariable`.  To avoid confusion
+    with the behavior of deepcopy on a regular `Variable` (which does
+    copy into new devices), we only allow a deepcopy of a `DistributedVariable`
+    within its originating strategy scope.
+
+    Args:
+      memo: The memoization object for `deepcopy`.
+
+    Returns:
+      A deep copy of the current `DistributedVariable`.
+
+    Raises:
+      RuntimeError: If trying to deepcopy into a different strategy.
+    """
+    with ds_context.enter_or_assert_strategy(self._distribute_strategy):
+      new_values = []
+
+      for value in self._values:
+        with ops.device(value.device):
+          new_values.append(copy.deepcopy(value, memo))
+
+    copied_variable = type(self)(
+        strategy=self._distribute_strategy,
+        values=new_values,
+        aggregation=self._aggregation,
+        var_policy=copy.deepcopy(self._policy, memo))
+
+    memo[id(self)] = copied_variable
+
+    return copied_variable
 
   def _use_packed_variable(self):
     # Don't use packed variable when under a SaveContext to avoid explicit
--- tensorflow/python/distribute/values_test.py
+++ tensorflow/python/distribute/values_test.py
@@ -533,6 +533,34 @@ class DistributedVariableTest(test.TestCase, parameterized.TestCase):
     # In replica context.
     distribution.run(assert_is_tensor_like, args=(v,))
 
+  def testDeepCopy(self, distribution, synchronization,
+                   aggregation):
+    if not context.executing_eagerly():
+      self.skipTest("deepcopy only supported in eager mode")
+
+    with distribution.scope():
+      v = variables_lib.Variable(
+          0., synchronization=synchronization, aggregation=aggregation)
+      in_dist_copy = copy.deepcopy(v)
+
+    out_dist_copy = copy.deepcopy(v)
+
+    def assert_is_deep_copy(v1, v2):
+      self.assertIsInstance(v2, type(v1))
+      self.assertEqual(v1.aggregation, v2.aggregation)
+      self.assertEqual(v1.distribute_strategy, v2.distribute_strategy)
+      self.assertEqual(v1._policy, v2._policy)  # pylint: disable=protected-access
+      self.assertEqual(len(v1.values), len(v2.values))
+      for (v1v, v2v) in zip(v1.values, v2.values):
+        self.assertEqual(v1v.device, v2v.device)
+        self.assertNotEqual(id(v1v), id(v2v))
+      self.assertAllEqual(self.evaluate(v1.values), self.evaluate(v2.values))
+
+    self.evaluate(variables_lib.global_variables_initializer())
+    if not isinstance(distribution.extended, tpu_strategy.TPUExtended):
+      distribution.run(assert_is_deep_copy, args=(v, in_dist_copy))
+      distribution.run(assert_is_deep_copy, args=(v, out_dist_copy))
+
   def testAssignSignature(self, distribution, synchronization, aggregation):
     # This test verifies assign*() can be called in the same way as normal
     # variables.
