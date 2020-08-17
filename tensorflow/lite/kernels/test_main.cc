dc65286f124daca51ecd82be7ccf277850c71570
Koan-Sin Tan <koansin.tan@gmail.com>
2020-08-17 13:10:45 +0800

    per review comments

--- tensorflow/lite/kernels/test_main.cc
+++ tensorflow/lite/kernels/test_main.cc
@@ -31,12 +31,10 @@ void InitKernelTest(int* argc, char** argv) {
     // In Android Q, the NNAPI delegate avoids delegation if the only device
     // is the reference CPU. However, for testing purposes, we still want
     // delegation coverage, so force use of this reference path.
-    const auto opt_name = "nnapi_accelerator_name";
-    std::string accelerator_name =
-        delegate_providers->ConstParams().Get<std::string>(opt_name);
-    delegate_providers->MutableParams()->Set<std::string>(
-        opt_name, accelerator_name.empty() ? "nnapi-reference"
-                                           : accelerator_name.c_str());
+    auto* params = delegate_providers->MutableParams();
+    if (!params->HasValueSet<std::string>("nnapi_accelerator_name")) {
+      params->Set<std::string>("nnapi_accelerator_name", "nnapi-reference");
+    }
   }
 }
 
