56942dddcc6a16f94c8f69451cba6482bf350f6a
A. Unique TensorFlower <gardener@tensorflow.org>
2020-08-18 00:22:36 -0700

    fixed dimension zero edge case. when one of the dimension is zero, where function should output empty output
    PiperOrigin-RevId: 327177867
    Change-Id: Ib2848d1d02605a162534e0290ca20a262317f231

--- tensorflow/lite/kernels/where_test.cc
+++ tensorflow/lite/kernels/where_test.cc
@@ -51,6 +51,30 @@ class IntegerWhereOpModel : public BaseWhereOpModel {
   std::vector<int64_t> GetOutput() { return ExtractVector<int64_t>(output_); }
 };
 
+template <typename T1>
+class ConstInputWhereOpModel : public SingleOpModel {
+ public:
+  ConstInputWhereOpModel(T1 constant_values, const TensorData& output) {
+    input_ = AddConstInput(GetTensorType<T1>(), {constant_values}, {});
+    output_ = AddOutput(output);
+    SetBuiltinOp(BuiltinOperator_WHERE, BuiltinOptions_WhereOptions,
+                 CreateWhereOptions(builder_).Union());
+    BuildInterpreter({{}});
+  }
+
+  int input() { return input_; }
+  std::vector<int64_t> GetOutput() { return ExtractVector<int64_t>(output_); }
+
+ protected:
+  int input_;
+  int output_;
+};
+
+TEST(WhereOpTest, ScalarValueFail) {
+  ConstInputWhereOpModel<bool> m(false, {TensorType_INT64, {}});
+  EXPECT_EQ(m.InvokeUnchecked(), kTfLiteError);
+}
+
 TEST(WhereOpTest, SelectFromVectorNoResult) {
   IntegerWhereOpModel m({TensorType_BOOL, {3}}, {TensorType_INT64, {}});
   m.PopulateTensor<bool>(m.input(), {false, false, false});
