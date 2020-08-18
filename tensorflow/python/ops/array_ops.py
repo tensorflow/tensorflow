80a51d5b8a65d87156d659ba6566f38afcd59ba6
ngc92 <7938269+ngc92@users.noreply.github.com>
2020-08-18 15:20:04 +0300

    python doctests

--- tensorflow/python/ops/array_ops.py
+++ tensorflow/python/ops/array_ops.py
@@ -2610,6 +2610,50 @@ def matrix_diag_part(
       input=input, k=k, padding_value=padding_value, align=align, name=name)
 
 
+@tf_export("linalg.tensor_diag_part", v1=["linalg.tensor_diag_part", "diag_part"])
+@dispatch.add_dispatch_support
+@deprecation.deprecated_endpoints("diag_part")
+@dispatch.add_dispatch_support
+def tensor_diag_part(
+    input,  # pylint:disable=redefined-builtin
+    name="tensor_diag_part"):
+  """Returns the diagonal part of the tensor.
+
+  This operation returns a tensor with the `diagonal` part
+  of the `input`. The `diagonal` part is computed as follows:
+
+  Assume `input` has dimensions `[D1,..., Dk, D1,..., Dk]`, then the output is a
+  tensor of rank `k` with dimensions `[D1,..., Dk]` where:
+
+  `diagonal[i1,..., ik] = input[i1, ..., ik, i1,..., ik]`.
+
+  For a rank 2 tensor, `diag_part` and `tensor_diag_part`
+  produce the same result. An example where they differ
+  is given below.
+
+  >>> x = [[[[1111,1112],[1121,1122]],
+  ...       [[1211,1212],[1221,1222]]],
+  ...      [[[2111, 2112], [2121, 2122]],
+  ...       [[2211, 2212], [2221, 2222]]]
+  ...      ]
+  >>> tf.linalg.tensor_diag_part(x)
+  <tf.Tensor: shape=(2, 2), dtype=int32, numpy=
+  array([[1111, 1212],
+         [2121, 2222]], dtype=int32)>
+  >>> tf.linalg.diag_part(x).shape
+  TensorShape([2, 2, 2])
+
+  Args:
+    input: A `Tensor` with rank `2k`.
+    name: A name for the operation (optional).
+
+  Returns:
+    A Tensor containing diagonals of `input`. Has the same type as `input`, and
+    rank `k`.
+  """
+  return gen_array_ops.diag_part(input=input, name=name)
+
+
 @tf_export("linalg.set_diag", v1=["linalg.set_diag", "matrix_set_diag"])
 @dispatch.add_dispatch_support
 @deprecation.deprecated_endpoints("matrix_set_diag")
