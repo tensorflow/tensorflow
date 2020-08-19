88379ce456b67a056c11d615453f086d8c006870
Eugene Brevdo <ebrevdo@google.com>
2020-08-19 16:32:23 -0700

    [TF.linalg LinearOperator] Add 'parameters' property to tf LinearOperator. (resubmission)
    This matches the behavior of TFP Kernels, Distributions, Bijectors, etc, and
    allows us to trace the constructor arguments of all objects used to create
    Distributions and Kernels.
    
    PiperOrigin-RevId: 327530603
    Change-Id: I9fe6502d4ec5f20d5c185a34e074d122776aeb2d

--- tensorflow/python/ops/linalg/linear_operator_kronecker.py
+++ tensorflow/python/ops/linalg/linear_operator_kronecker.py
@@ -167,6 +167,15 @@ class LinearOperatorKronecker(linear_operator.LinearOperator):
       TypeError:  If all operators do not have the same `dtype`.
       ValueError:  If `operators` is empty.
     """
+    parameters = dict(
+        operators=operators,
+        is_non_singular=is_non_singular,
+        is_self_adjoint=is_self_adjoint,
+        is_positive_definite=is_positive_definite,
+        is_square=is_square,
+        name=name
+    )
+
     # Validate operators.
     check_ops.assert_proper_iterable(operators)
     operators = list(operators)
@@ -226,6 +235,7 @@ class LinearOperatorKronecker(linear_operator.LinearOperator):
           is_self_adjoint=is_self_adjoint,
           is_positive_definite=is_positive_definite,
           is_square=is_square,
+          parameters=parameters,
           name=name)
     # TODO(b/143910018) Remove graph_parents in V3.
     self._set_graph_parents(graph_parents)
