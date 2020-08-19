c1f2f744bfe3fa1147df2ccd516eea853c2ac984
Tzu-Wei Sung <windqaq@gmail.com>
2020-08-19 08:35:16 -0700

    Update pywrap_gradient_exclusions.cc

--- tensorflow/python/eager/pywrap_gradient_exclusions.cc
+++ tensorflow/python/eager/pywrap_gradient_exclusions.cc
@@ -50,7 +50,7 @@ auto OpGradientInfoInit(const T &a) {
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 351> a = {{
+  static std::array<OpIndexInfo, 352> a = {{
       {"Acosh"},
       {"AllToAll", 1, {0}},
       {"ApproximateEqual"},
@@ -415,7 +415,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 467> a = {{
+  static std::array<OpIndexInfo, 468> a = {{
       {"Abs"},
       {"AccumulateNV2"},
       {"Acos"},
