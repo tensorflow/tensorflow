70a041db8aa09ff6a59219c75c01ded9bfe8e8dd
Yong Tang <yong.tang.github@outlook.com>
2020-08-19 20:02:32 +0000

    Expand captured exception to ValueError+TypeError, for internal test fix
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/util/nest_test.py
+++ tensorflow/python/util/nest_test.py
@@ -1219,11 +1219,11 @@ class NestTest(parameterized.TestCase, test.TestCase):
     )
 
   def testInvalidCheckTypes(self):
-    with self.assertRaises(ValueError):
+    with self.assertRaises((ValueError, TypeError)):
       nest.assert_same_structure(
           nest1=array_ops.zeros((1)), nest2=array_ops.ones((1, 1, 1)),
           check_types=array_ops.ones((2)))
-    with self.assertRaises(ValueError):
+    with self.assertRaises((ValueError, TypeError)):
       nest.assert_same_structure(
           nest1=array_ops.zeros((1)), nest2=array_ops.ones((1, 1, 1)),
           expand_composites=array_ops.ones((2)))
