06823a6f4e8149730f35457d5696b45cc1462dc1
A. Unique TensorFlower <gardener@tensorflow.org>
2020-08-21 12:02:27 -0700

    Bug fix: replacing `py::str` with `py::bytes`.
    Backward compatible change preparing for pybind11 update.
    
    Hidden (and luckily inconsequential) bugs discovered while testing
    with the current pybind11 github master branch, and current
    https://github.com/pybind/pybind11/pull/2409 applied locally.
    
    The code changed in this CL depends on a pybind11 mis-feature: Current `stable`
    `pybind11::str` can hold either `PyUnicodeObject` (as documented) or
    `PyBytesObject` (undocumented and probably very surprising), even under
    Python 3. pybind PR #2409 changes `pybind11::str` so that it can only hold
    `PyUnicodeObject`.
    
    PiperOrigin-RevId: 327849650
    Change-Id: I2a119479a6af8ab8ec5315a1b8565e96952b84c1

--- tensorflow/python/tfe_wrapper.cc
+++ tensorflow/python/tfe_wrapper.cc
@@ -558,7 +558,7 @@ PYBIND11_MODULE(_pywrap_tfe, m) {
               tensorflow::InputTFE_Context(ctx), policy);
         });
   m.def("TFE_ContextSetServerDef", [](py::handle& ctx, int keep_alive_secs,
-                                      py::str proto) {
+                                      py::bytes proto) {
     tensorflow::Safe_TF_StatusPtr status =
         tensorflow::make_safe(TF_NewStatus());
     tensorflow::Safe_TF_BufferPtr buf =
@@ -568,7 +568,7 @@ PYBIND11_MODULE(_pywrap_tfe, m) {
     tensorflow::MaybeRaiseRegisteredFromTFStatus(status.get());
   });
   m.def("TFE_ContextUpdateServerDef", [](py::handle& ctx, int keep_alive_secs,
-                                         py::str proto) {
+                                         py::bytes proto) {
     tensorflow::Safe_TF_StatusPtr status =
         tensorflow::make_safe(TF_NewStatus());
     tensorflow::Safe_TF_BufferPtr buf =
@@ -848,7 +848,7 @@ PYBIND11_MODULE(_pywrap_tfe, m) {
   m.def("TFE_NewContextOptions", &TFE_NewContextOptions,
         py::return_value_policy::reference);
   m.def("TFE_ContextOptionsSetConfig", [](TFE_ContextOptions* options,
-                                          py::str proto) {
+                                          py::bytes proto) {
     tensorflow::Safe_TF_StatusPtr status =
         tensorflow::make_safe(TF_NewStatus());
     tensorflow::Safe_TF_BufferPtr buf =
@@ -899,7 +899,7 @@ PYBIND11_MODULE(_pywrap_tfe, m) {
           return tensorflow::PyoOrThrow(
               TFE_Py_EncodeArg(o.ptr(), include_tensor_ranks_only));
         });
-  m.def("TFE_EnableCollectiveOps", [](const py::handle& ctx, py::str proto) {
+  m.def("TFE_EnableCollectiveOps", [](const py::handle& ctx, py::bytes proto) {
     tensorflow::Safe_TF_StatusPtr status =
         tensorflow::make_safe(TF_NewStatus());
     tensorflow::Safe_TF_BufferPtr buf =
