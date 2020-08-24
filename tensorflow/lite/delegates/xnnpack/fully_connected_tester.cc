b57f22382d97c61a644ba1c3a3d69f21d06504be
Chao Mei <chaomei@google.com>
2020-08-23 19:31:47 -0700

    Use BuiltinOpResolverWithoutDefaultDelegates instead of BuiltinOpResolver for unit tests of xnnpack delegate itself to prepare for enabling xnnpack delegate by default across all platforms in the next 2.4.0 release.
    PiperOrigin-RevId: 328068258
    Change-Id: I3459bc3e7f25d2925da65fba3e19ac2bad57fff1

--- tensorflow/lite/delegates/xnnpack/fully_connected_tester.cc
+++ tensorflow/lite/delegates/xnnpack/fully_connected_tester.cc
@@ -59,12 +59,16 @@ void FullyConnectedTester::Test(TfLiteDelegate* delegate) const {
 
   std::unique_ptr<Interpreter> delegate_interpreter;
   ASSERT_EQ(
-      InterpreterBuilder(model, ::tflite::ops::builtin::BuiltinOpResolver())(
+      InterpreterBuilder(
+          model,
+          ::tflite::ops::builtin::BuiltinOpResolverWithoutDefaultDelegates())(
           &delegate_interpreter),
       kTfLiteOk);
   std::unique_ptr<Interpreter> default_interpreter;
   ASSERT_EQ(
-      InterpreterBuilder(model, ::tflite::ops::builtin::BuiltinOpResolver())(
+      InterpreterBuilder(
+          model,
+          ::tflite::ops::builtin::BuiltinOpResolverWithoutDefaultDelegates())(
           &default_interpreter),
       kTfLiteOk);
 
