0634d08af130ded6d4ab3be80074c4762c66de94
Raman Sarokin <sorokin@google.com>
2020-08-25 16:56:47 -0700

    ConvConstants converted to generic GPUOperation.
    PiperOrigin-RevId: 328431894
    Change-Id: I464d830b55ab27cdd47761c2432f731e009fda15

--- tensorflow/lite/delegates/gpu/cl/kernels/conv_constants_test.cc
+++ tensorflow/lite/delegates/gpu/cl/kernels/conv_constants_test.cc
@@ -55,7 +55,7 @@ TEST_F(OpenCLOperationTest, ConvConstantsSimpleWeights) {
       op_def.src_tensors.push_back({data_type, storage, Layout::HWC});
       op_def.dst_tensors.push_back({data_type, storage, Layout::HWC});
       TensorFloat32 dst_tensor;
-      ConvConstants operation =
+      GPUOperation operation =
           CreateConvConstants(creation_context_.GetDeviceInfo(), op_def, attr);
       ASSERT_OK(ExecuteGPUOperation(src_tensor, creation_context_, &operation,
                                     BHWC(1, 2, 2, 1), &dst_tensor));
@@ -90,7 +90,7 @@ TEST_F(OpenCLOperationTest, ConvConstants) {
       op_def.src_tensors.push_back({data_type, storage, Layout::HWC});
       op_def.dst_tensors.push_back({data_type, storage, Layout::HWC});
       TensorFloat32 dst_tensor;
-      ConvConstants operation =
+      GPUOperation operation =
           CreateConvConstants(creation_context_.GetDeviceInfo(), op_def, attr);
       ASSERT_OK(ExecuteGPUOperation(src_tensor, creation_context_, &operation,
                                     BHWC(1, 2, 2, 2), &dst_tensor));
