6e1dd106530682da28e8b884939aaab7f7685fc2
bhack <bhack@users.noreply.github.com>
2020-08-29 13:20:33 +0200

    Fix typo

--- tensorflow/python/keras/mixed_precision/experimental/keras_test.py
+++ tensorflow/python/keras/mixed_precision/experimental/keras_test.py
@@ -1079,7 +1079,7 @@ class KerasModelTest(keras_parameterized.TestCase):
 
       loss_scale = loss_scale_module.DynamicLossScale(
           initial_loss_scale=1., increment_period=2., multiplier=2.)
-      opt = gradient_descent.Adam(1.)
+      opt = adam.Adam(1.)
       opt = loss_scale_optimizer.LossScaleOptimizer(opt, loss_scale)
       model.compile(
           optimizer=opt,
