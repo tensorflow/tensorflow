43641031eaf781fb12c8c18d1bc320bd9108de62
yuanbopeng <yuanbopeng@tencent.com>
2020-09-01 21:52:26 +0800

    Fix a bug that the file copied by TF from HDFS to local may be wrong, when HDFS file is being overwritten #42597

--- tensorflow/core/platform/hadoop/hadoop_file_system.cc
+++ tensorflow/core/platform/hadoop/hadoop_file_system.cc
@@ -228,7 +228,7 @@ class HDFSRandomAccessFile : public RandomAccessFile {
     Status s;
     char* dst = scratch;
     bool eof_retried = false;
-    const char* disable_eof_retried = getenv("DISABLE_HDFS_READ_EOF_RETRIED")
+    const char* disable_eof_retried = getenv("DISABLE_HDFS_READ_EOF_RETRIED");
     if (disable_eof_retried && disable_eof_retried[0] == '1') {
       // eof_retried = true, avoid calling hdfsOpenFile in Read, Fixes #42597
       eof_retried = true;
