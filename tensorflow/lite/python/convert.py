d08e1a80eb9e1cda95806449682fddf963c03a87
Meghna Natraj <mnatraj@google.com>
2020-09-02 16:35:43 -0700

    Reduce redundancy by replacing TFLite data types with TF data types
    PiperOrigin-RevId: 329812474
    Change-Id: Ia2f02aea1e8b33f800c3c2241979f43b3be2ca10

--- tensorflow/lite/python/convert.py
+++ tensorflow/lite/python/convert.py
@@ -35,6 +35,7 @@ from tensorflow.lite.python import wrap_toco
 from tensorflow.lite.toco import model_flags_pb2 as _model_flags_pb2
 from tensorflow.lite.toco import toco_flags_pb2 as _toco_flags_pb2
 from tensorflow.lite.toco import types_pb2 as _types_pb2
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.platform import resource_loader as _resource_loader
 from tensorflow.python.util import deprecation
@@ -290,7 +291,7 @@ Alternative, use virtualenv.""")
 
 def build_toco_convert_protos(input_tensors,
                               output_tensors,
-                              inference_type=lite_constants.FLOAT,
+                              inference_type=dtypes.float32,
                               inference_input_type=None,
                               input_format=lite_constants.TENSORFLOW_GRAPHDEF,
                               input_shapes=None,
