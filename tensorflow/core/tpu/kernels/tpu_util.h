1c518f3284102ed375ad1a1c55e1dda7f986209e
Henry Tan <henrytan@google.com>
2020-09-01 21:02:48 -0700

    Support remote distributed Compilation Cache for POD: - Setup remote cache proxy as part of `ConfigureDistributedTpuOp` - Construct `TpuPodState` as part of `ConfigureDistributedTpuOp`
    PiperOrigin-RevId: 329639206
    Change-Id: I4293f9394ee2cf928ef23b7b1ffb4d32e0f8c300

--- tensorflow/core/tpu/kernels/tpu_util.h
+++ tensorflow/core/tpu/kernels/tpu_util.h
@@ -15,9 +15,11 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_TPU_KERNELS_TPU_UTIL_H_
 #define TENSORFLOW_CORE_TPU_KERNELS_TPU_UTIL_H_
 
+#include <memory>
 #include <string>
 #include <vector>
 
+#include "grpcpp/server_builder.h"
 #include "absl/strings/str_cat.h"
 #include "tensorflow/cc/framework/ops.h"
 #include "tensorflow/compiler/tf2xla/xla_compiler.h"
@@ -55,10 +57,9 @@ Status DynamicShapesToTensorShapes(const OpInputList& dynamic_shapes,
 Status DynamicShapesToTensorShapes(const InputList& dynamic_shapes,
                                    std::vector<TensorShape>* shapes);
 
-// We only recycle ports which were given to us by the portserver. For ports
-// we obtained through local trial-and-error, there is no reason to expect the
-// port to remain available after it is unbound.
-void RecycleUnusedPort(int port);
+// Creates gRPC ServerBuilder.
+xla::StatusOr<std::unique_ptr<::grpc::ServerBuilder>> CreateServerBuilder(
+    int serving_port);
 }  // namespace tpu
 }  // namespace tensorflow
 
