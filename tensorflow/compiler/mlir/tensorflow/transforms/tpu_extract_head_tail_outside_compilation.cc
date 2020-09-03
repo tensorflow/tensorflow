bedf4eb16630f3a89795e9a51d5e605e039b4d79
Andy Ly <lyandy@google.com>
2020-09-02 21:46:11 -0700

    Removed unused imports in TPUExtractHeadTailOutsideCompilation pass (NFC).
    PiperOrigin-RevId: 329849796
    Change-Id: I7e38ac800d4b457db35548b7e07ca896bb8b65ed

--- tensorflow/compiler/mlir/tensorflow/transforms/tpu_extract_head_tail_outside_compilation.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/tpu_extract_head_tail_outside_compilation.cc
@@ -23,8 +23,6 @@ limitations under the License.
 #include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/ADT/iterator_range.h"
-#include "llvm/Support/FormatVariadic.h"
 #include "mlir/IR/Attributes.h"  // from @llvm-project
 #include "mlir/IR/Block.h"  // from @llvm-project
 #include "mlir/IR/Builders.h"  // from @llvm-project
