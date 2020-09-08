988fe13388c56ef43638392835a2004b18396d60
Alexander Belyaev <pifon@google.com>
2020-09-08 06:05:50 -0700

    [MLIR][KERNEL_GEN] Add a library to lower kernels with the host side.
    * Unified TF->Cubin and TF->Kernel_with_host side lowering in `kernel_creator.h|cc`
    * Added a pass that attaches GPU binary blob to GPUModuleOp
    * Refactored most of the code.
    * Added tf_to_kernel binary that emits obj file
    
    PiperOrigin-RevId: 330494488
    Change-Id: If01a2b2d2242f1f40fd102580d4923fc05754e84

--- tensorflow/compiler/mlir/tools/kernel_gen/transforms/tf_framework_legalize_to_llvm_pass.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/transforms/tf_framework_legalize_to_llvm_pass.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"  // from @llvm-project
 #include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVMPass.h"  // from @llvm-project
+#include "mlir/Dialect/GPU/GPUDialect.h"  // from @llvm-project
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"  // from @llvm-project
 #include "mlir/Dialect/StandardOps/IR/Ops.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
@@ -56,10 +57,11 @@ class TestTFFrameworkToLLVMPass
     // Set target.
     ConversionTarget target(getContext());
     target.addLegalDialect<LLVM::LLVMDialect>();
+    target.addLegalDialect<gpu::GPUDialect>();
     target.addIllegalDialect<tf_framework::TFFrameworkDialect>();
-    target.addLegalOp<ModuleOp, ModuleTerminatorOp>();
+    target.addIllegalOp<LLVM::DialectCastOp>();
 
-    if (failed(applyFullConversion(m, target, patterns))) {
+    if (failed(applyPartialConversion(m, target, patterns))) {
       signalPassFailure();
     }
   }
