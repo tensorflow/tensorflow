edb60d01ee17c60568359bbdcec9e283cb0e1d54
Vignesh Kothapalli <vikoth18@in.ibm.com>
2020-09-08 21:57:34 +0530

    added config based tests

--- tensorflow/python/data/experimental/service/server_lib_test.py
+++ tensorflow/python/data/experimental/service/server_lib_test.py
@@ -18,8 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tempfile
 from tensorflow.python.data.experimental.service import server_lib
-
 from tensorflow.python.platform import test
 
 
@@ -28,6 +28,32 @@ class ServerLibTest(test.TestCase):
   def testStartDispatcher(self):
     dispatcher = server_lib.DispatchServer(start=False)
     dispatcher.start()
+
+  def testStartDispatcherWithPortConfig(self):
+    config = server_lib.DispatcherConfig(port=5000)
+    dispatcher = server_lib.DispatchServer(config=config, start=False)
+    dispatcher.start()
+    assert dispatcher.target == "grpc://localhost:5000"
+
+  def testStartDispatcherWithWorkDirConfig(self):
+    temp_dir = tempfile.mkdtemp()
+    config = server_lib.DispatcherConfig(work_dir=temp_dir)
+    dispatcher = server_lib.DispatchServer(config=config, start=False)
+    dispatcher.start()
+
+  def testStartDispatcherWithFaultTolerantConfig(self):
+    temp_dir = tempfile.mkdtemp()
+    config = server_lib.DispatcherConfig(work_dir=temp_dir,
+                                         fault_tolerant_mode=True)
+    dispatcher = server_lib.DispatchServer(config=config, start=False)
+    dispatcher.start()
+
+  def testStartDispatcherWithWrongFaultTolerantConfig(self):
+    config = server_lib.DispatcherConfig(fault_tolerant_mode=True)
+    error = "Cannot enable fault tolerant mode without configuring a work_dir"
+    with self.assertRaisesRegex(ValueError, error):
+      dispatcher = server_lib.DispatchServer(config=config, start=False)
+      dispatcher.start()
 
   def testMultipleStartDispatcher(self):
     dispatcher = server_lib.DispatchServer(start=True)
@@ -38,6 +64,13 @@ class ServerLibTest(test.TestCase):
     worker = server_lib.WorkerServer(
         server_lib.WorkerConfig(dispatcher._address), start=False)
     worker.start()
+
+  def testStartWorkerWithPortConfig(self):
+    dispatcher = server_lib.DispatchServer()
+    worker = server_lib.WorkerServer(
+        server_lib.WorkerConfig(dispatcher._address, port=5005), start=False)
+    worker.start()
+    assert worker._address == "localhost:5005"
 
   def testMultipleStartWorker(self):
     dispatcher = server_lib.DispatchServer()
