1367b5c6e050176e1e4980a979e78ed679bc0bc2
Nick Kreeger <nick.kreeger@gmail.com>
2020-09-10 14:08:14 -0500

    Bump VariableTensorReset to ensure enough space is on disk for looping through tensors().

--- tensorflow/lite/micro/micro_interpreter_test.cc
+++ tensorflow/lite/micro/micro_interpreter_test.cc
@@ -270,7 +270,7 @@ TF_LITE_MICRO_TEST(TestVariableTensorReset) {
   tflite::AllOpsResolver op_resolver = tflite::testing::GetOpResolver();
 
   constexpr size_t allocator_buffer_size =
-      2096 /* optimal arena size at the time of writting. */ +
+      3072 /* optimal arena size at the time of writting. */ +
       16 /* alignment */ + 100 /* some headroom */;
   uint8_t allocator_buffer[allocator_buffer_size];
   tflite::MicroInterpreter interpreter(model, op_resolver, allocator_buffer,
