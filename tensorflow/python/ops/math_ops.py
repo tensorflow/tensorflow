e5e192107c4a008a8bba8928256d373e16629db3
A. Unique TensorFlower <gardener@tensorflow.org>
2020-09-14 11:05:38 -0700

    Replace call to gen_math_ops.add() with _add_dispatch() in reduce_logsumexp().
    PiperOrigin-RevId: 331584952
    Change-Id: I8b7bace40363f2a0d892fe7cfca27eb34c9f1041

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -3046,7 +3046,7 @@ def reduce_logsumexp(input_tensor, axis=None, keepdims=False, name=None):
             dims=reduce_dim))
     if not keepdims:
       my_max = array_ops.reshape(my_max, gen_array_ops.shape(result))
-    result = _add_dispatch(result, my_max, name=name)
+    result = gen_math_ops.add(result, my_max)
     return _may_reduce_to_scalar(keepdims, axis, result)
 
 
