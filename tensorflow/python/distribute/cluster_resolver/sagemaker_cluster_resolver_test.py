0c149558405aae28335236be068901f9ba86d26f
sboshin <sboshin@amazon.com>
2020-09-16 16:46:58 +0000

    Fixing documentation error

--- tensorflow/python/distribute/cluster_resolver/sagemaker_cluster_resolver_test.py
+++ tensorflow/python/distribute/cluster_resolver/sagemaker_cluster_resolver_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for TFCONFIGClusterResolver."""
+"""Tests for SageMakerClusterResolver."""
 
 from __future__ import absolute_import
 from __future__ import division
