1e4ddfc972010698f6ead9c072597f6c563e522f
A. Unique TensorFlower <gardener@tensorflow.org>
2020-09-16 16:48:14 -0700

    Internal changes only.
    PiperOrigin-RevId: 332117785
    Change-Id: I7742610a274a21fe5e11ce5f88e5f4ca1b93275e

--- tensorflow/python/modules_with_exports.py
+++ tensorflow/python/modules_with_exports.py
@@ -54,7 +54,6 @@ from tensorflow.python.ops import initializers_ns as initializers
 from tensorflow.python.util.tf_export import tf_export
 
 # _internal APIs
-from tensorflow.python.distribute.combinations import generate
 from tensorflow.python.framework.combinations import *
 from tensorflow.python.framework.test_combinations import *
 from tensorflow.python.util.tf_decorator import make_decorator
--- tensorflow/python/tools/api/generator/api_init_files.bzl
+++ tensorflow/python/tools/api/generator/api_init_files.bzl
@@ -8,8 +8,6 @@ TENSORFLOW_API_INIT_FILES = [
     "__internal__/decorator/__init__.py",
     "__internal__/test/__init__.py",
     "__internal__/test/combinations/__init__.py",
-    "__internal__/distribute/__init__.py",
-    "__internal__/distribute/combinations/__init__.py",
     "__operators__/__init__.py",
     "audio/__init__.py",
     "autograph/__init__.py",
