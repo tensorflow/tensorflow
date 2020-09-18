6e9da4b8c14ac894e3c270e2fa55c43084f6c7c0
Chenkai Kuang <chenkai@google.com>
2020-09-17 20:03:41 -0700

    Clear device_parsing_cache in context._reset_context().
    This fixes a bug where context.device("cpu:0") still resolves to a device str from an older context after resetting the context.
    
    PiperOrigin-RevId: 332369820
    Change-Id: I020588abe700c33c7750bc7ece6d1cc21628b314

--- tensorflow/python/eager/context.py
+++ tensorflow/python/eager/context.py
@@ -1831,11 +1831,13 @@ def _reset_context():
   Should only be used for testing.
   """
   global _context
+  global _device_parsing_cache
   with _context_lock:
     if _context is not None:
       _context._clear_caches()
       _context = None
   _create_context()
+  _device_parsing_cache = {}
   pywrap_tfe.TFE_ClearScalarCache()
 
 
