00302787b788c5ff04cb6f62aed5a74d936e86c0
Mihai Maruseac <mihaimaruseac@google.com>
2020-09-18 13:16:53 -0700

    [tflite] Make `GetOptionalInputTensor` the same as `GetInput`.
    With the previous change, there is no more need for two separate APIs. We would deprecate `GetOptionalInputTensor` in the future.
    
    PiperOrigin-RevId: 332513386
    Change-Id: Id7110271c25ebd6126ad8c82a493e37e0e0756b3

--- tensorflow/lite/kernels/kernel_util.cc
+++ tensorflow/lite/kernels/kernel_util.cc
@@ -75,12 +75,7 @@ TfLiteTensor* GetOutput(TfLiteContext* context, const TfLiteNode* node,
 
 const TfLiteTensor* GetOptionalInputTensor(const TfLiteContext* context,
                                            const TfLiteNode* node, int index) {
-  const bool use_tensor = index < node->inputs->size &&
-                          node->inputs->data[index] != kTfLiteOptionalTensor;
-  if (use_tensor) {
-    return GetMutableInput(context, node, index);
-  }
-  return nullptr;
+  return GetInput(context, node, index);
 }
 
 // Per-axis
