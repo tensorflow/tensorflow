c247a2d7229f23f7487fbf83a7fbc1f8cb10f192
Mihai Maruseac <mihaimaruseac@google.com>
2020-09-20 15:24:53 -0700

    Solve leftover from merge conflict

--- tensorflow/cc/saved_model/loader.cc
+++ tensorflow/cc/saved_model/loader.cc
@@ -20,7 +20,7 @@ limitations under the License.
 #include "tensorflow/cc/saved_model/constants.h"
 #include "tensorflow/cc/saved_model/reader.h"
 #include "tensorflow/core/framework/attr_value.pb.h"
-#include "tensorflow/core/framework/function.proto.h"
+#include "tensorflow/core/framework/function.pb.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/framework/tensor.pb.h"
 #include "tensorflow/core/lib/io/path.h"
