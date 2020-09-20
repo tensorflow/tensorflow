697f64ef322ea9800496fcb1d74d43a6acb9c803
Yair Ehrenwald <yair.ehrenwald@ceva-dsp.com>
2020-09-20 16:41:32 +0300

    fix cast error stm platform

--- tensorflow/lite/micro/kernels/shape_test.cc
+++ tensorflow/lite/micro/kernels/shape_test.cc
@@ -25,7 +25,7 @@ namespace testing {
 namespace {
 
 void ValidateShape(TfLiteTensor* tensors, const int tensor_count,
-                   int* output_data, const int* expected_output,
+                   int32_t* output_data, const int32_t* expected_output,
                    int output_dims_count) {
   int inputs_array_data[] = {1, 0};
   TfLiteIntArray* inputs_array = IntArrayFromInts(inputs_array_data);
@@ -45,8 +45,8 @@ void ValidateShape(TfLiteTensor* tensors, const int tensor_count,
 }
 
 void TestShape(const int* input_dims_data, const float* input_data,
-               const int* output_dims_data, const int* expected_output_data,
-               int* output_data) {
+               const int* output_dims_data, const int32_t* expected_output_data,
+               int32_t* output_data) {
   TfLiteIntArray* input_dims = IntArrayFromInts(input_dims_data);
   TfLiteIntArray* output_dims = IntArrayFromInts(output_dims_data);
   const int output_dims_count = ElementCount(*output_dims);
@@ -73,8 +73,8 @@ TF_LITE_MICRO_TEST(TestShape0) {
   int input_shape[] = {1, 5};
   float input_values[] = {1, 3, 1, 3, 5};
   int output_dims[] = {1, 1};  // this is actually input_shapes shape
-  int expected_output_data[] = {5};
-  int output_data[1];
+  int32_t expected_output_data[] = {5};
+  int32_t output_data[1];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
@@ -84,8 +84,8 @@ TF_LITE_MICRO_TEST(TestShape1) {
   int input_shape[] = {2, 4, 3};
   float input_values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   int output_dims[] = {2, 1, 1};
-  int expected_output_data[] = {4, 3};
-  int output_data[2];
+  int32_t expected_output_data[] = {4, 3};
+  int32_t output_data[2];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
@@ -95,8 +95,8 @@ TF_LITE_MICRO_TEST(TestShape2) {
   int input_shape[] = {2, 12, 1};
   float input_values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   int output_dims[] = {2, 1, 1};
-  int expected_output_data[] = {12, 1};
-  int output_data[2];
+  int32_t expected_output_data[] = {12, 1};
+  int32_t output_data[2];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
@@ -106,8 +106,8 @@ TF_LITE_MICRO_TEST(TestShape3) {
   int input_shape[] = {2, 2, 6};
   float input_values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   int output_dims[] = {2, 1, 1};
-  int expected_output_data[] = {2, 6};
-  int output_data[2];
+  int32_t expected_output_data[] = {2, 6};
+  int32_t output_data[2];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
@@ -117,8 +117,8 @@ TF_LITE_MICRO_TEST(TestShape4) {
   int input_shape[] = {2, 2, 2, 3};
   float input_values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   int output_dims[] = {3, 1, 1, 1};
-  int expected_output_data[] = {2, 2, 3};
-  int output_data[3];
+  int32_t expected_output_data[] = {2, 2, 3};
+  int32_t output_data[3];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
@@ -128,8 +128,8 @@ TF_LITE_MICRO_TEST(TestShape5) {
   int input_shape[] = {1, 1};
   float input_values[] = {1};
   int output_dims[] = {1, 1};
-  int expected_output_data[] = {1};
-  int output_data[1];
+  int32_t expected_output_data[] = {1};
+  int32_t output_data[1];
 
   tflite::testing::TestShape(input_shape, input_values, output_dims,
                              expected_output_data, output_data);
