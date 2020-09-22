4cf223069a94c78b208e6c829d5f938a0fae7d07
TensorFlow Release Automation <jenkins@tensorflow.org>
2020-09-21 18:55:42 -0700

    Update version numbers to 2.3.1

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -49,7 +49,7 @@ from setuptools.dist import Distribution
 # result for pip.
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
-_VERSION = '2.3.0'
+_VERSION = '2.3.1'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.7.0',
