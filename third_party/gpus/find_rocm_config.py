5ea061bd9101d522e619a8efd25da3d0fe7c51be
Deven Desai <deven.desai.amd@gmail.com>
2020-09-23 17:50:07 +0000

    Updating find_rocm_config.py to account for the new location of the rocblas version header file (in ROCm 3.8)

--- third_party/gpus/find_rocm_config.py
+++ third_party/gpus/find_rocm_config.py
@@ -138,7 +138,15 @@ def _find_miopen_config(rocm_install_path):
 def _find_rocblas_config(rocm_install_path):
 
   def rocblas_version_numbers(path):
-    version_file = os.path.join(path, "rocblas/include/rocblas-version.h")
+    possible_version_files = [
+      "rocblas/include/rocblas-version.h", # ROCm 3.7 and prior
+      "rocblas/include/internal/rocblas-version.h", # ROCm 3.8
+    ]
+    version_file = None
+    for f in possible_version_files:
+      version_file = os.path.join(path, f)
+      if  os.path.exists(version_file):
+        break
     if not os.path.exists(version_file):
       raise ConfigError(
           'rocblas version file "{}" not found'.format(version_file))
@@ -254,11 +262,11 @@ def find_rocm_config():
   result.update(_find_rocm_config(rocm_install_path))
   result.update(_find_hipruntime_config(rocm_install_path))
   result.update(_find_miopen_config(rocm_install_path))
-  # result.update(_find_rocblas_config(rocm_install_path))
-  # result.update(_find_rocrand_config(rocm_install_path))
-  # result.update(_find_rocfft_config(rocm_install_path))
-  # result.update(_find_roctracer_config(rocm_install_path))
-  # result.update(_find_hipsparse_config(rocm_install_path))
+  result.update(_find_rocblas_config(rocm_install_path))
+  result.update(_find_rocrand_config(rocm_install_path))
+  result.update(_find_rocfft_config(rocm_install_path))
+  result.update(_find_roctracer_config(rocm_install_path))
+  result.update(_find_hipsparse_config(rocm_install_path))
 
   return result
 
