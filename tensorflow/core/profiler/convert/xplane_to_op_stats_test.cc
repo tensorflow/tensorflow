8c30da064f56c636221e7e5ee3980807274693ad
A. Unique TensorFlower <gardener@tensorflow.org>
2020-09-23 12:42:34 -0700

    Use OpStats combiner in profiler_wrapper.cc
    PiperOrigin-RevId: 333350927
    Change-Id: Idb7bb344253f32326dbddf290e68f23a41827d6c

--- tensorflow/core/profiler/convert/xplane_to_op_stats_test.cc
+++ tensorflow/core/profiler/convert/xplane_to_op_stats_test.cc
@@ -16,6 +16,9 @@ limitations under the License.
 #include "tensorflow/core/profiler/convert/xplane_to_op_stats.h"
 
 #include "absl/strings/str_cat.h"
+#include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/platform/path.h"
+#include "tensorflow/core/platform/status.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/core/platform/types.h"
 #include "tensorflow/core/profiler/protobuf/diagnostics.pb.h"
@@ -173,6 +176,86 @@ TEST(ConvertXPlaneToOpStats, PropagateAndDedupErrors) {
 
   EXPECT_EQ(1, op_stats.diagnostics().errors_size());
   EXPECT_EQ(kError, op_stats.diagnostics().errors(/*index=*/0));
+}
+
+// Helper function to build a XSpace and store it to test directory.
+void BuildAndStoreXSpaceForTest(Env* test_env, const std::string& test_dir,
+                                const std::string& xspace_name) {
+  constexpr int64 kStepNum = 123;
+  constexpr int64 kStepId = 456;
+  // Create a host only XSpace for test.
+  XSpace xspace;
+  XPlaneBuilder host_plane_builder(GetOrCreateHostXPlane(&xspace));
+  host_plane_builder.ReserveLines(2);
+
+  auto main_thread = host_plane_builder.GetOrCreateLine(0);
+  CreateXEvent(&host_plane_builder, &main_thread, HostEventType::kTraceContext,
+               0, 100, {{StatType::kStepNum, kStepNum}});
+  CreateXEvent(&host_plane_builder, &main_thread, HostEventType::kFunctionRun,
+               10, 90, {{StatType::kStepId, kStepId}});
+
+  auto executor_thread = host_plane_builder.GetOrCreateLine(1);
+  CreateXEvent(&host_plane_builder, &executor_thread,
+               HostEventType::kExecutorStateProcess, 20, 80,
+               {{StatType::kStepId, kStepId}});
+  // Create a TensorFlow op that runs for 70 ps.
+  CreateXEvent(&host_plane_builder, &executor_thread, "aaa:bbb", 30, 70);
+  GroupTfEvents(&xspace, /*group_metadata_map=*/nullptr);
+
+  TF_CHECK_OK(
+      WriteBinaryProto(test_env, io::JoinPath(test_dir, xspace_name), xspace))
+      << "Failed to write binary XSpace to file: " << xspace_name;
+}
+
+TEST(ConvertXPlaneToOpStats, TestConvertMultiXSpacesToCombinedOpStats) {
+  // Initialize environment and directory for testing.
+  Env* test_env = Env::Default();
+  std::string test_dir = io::JoinPath(testing::TmpDir(), "test_dir");
+  TF_CHECK_OK(test_env->CreateDir(test_dir))
+      << "Failed to create test directory: " << test_dir;
+
+  const std::string xspace1 = "xspace1.pb";
+  const std::string xspace2 = "xspace2.pb";
+  BuildAndStoreXSpaceForTest(test_env, test_dir, xspace1);
+  BuildAndStoreXSpaceForTest(test_env, test_dir, xspace2);
+
+  std::vector<std::string> xspace_paths;
+  xspace_paths.push_back(io::JoinPath(test_dir, xspace1));
+  xspace_paths.push_back(io::JoinPath(test_dir, xspace2));
+  OpStatsOptions options;
+  options.generate_op_metrics_db = true;
+  options.generate_step_db = true;
+  OpStats combined_op_stats;
+
+  TF_CHECK_OK(ConvertMultiXSpacesToCombinedOpStats(xspace_paths, options,
+                                                   &combined_op_stats))
+      << "Failed to convert multi XSpace to OpStats";
+
+  // Result OpStats has 2 Host Ops, "IDLE" and "aaa:bbb".
+  ASSERT_EQ(combined_op_stats.host_op_metrics_db().metrics_db_size(), 2);
+  const auto& metric = combined_op_stats.host_op_metrics_db().metrics_db(1);
+  EXPECT_EQ(metric.name(), "aaa");
+  EXPECT_EQ(metric.category(), "bbb");
+  // Each host has the HostOp "aaa:bbb" running for 70 ps, so the combined
+  // OpStats has "aaa:bbb" running for 140 ps in total.
+  EXPECT_EQ(metric.self_time_ps(), 140);
+
+  // Result OpStats has 1 step, 2 cores.
+  ASSERT_EQ(combined_op_stats.step_db().step_sequence_size(), 1);
+  ASSERT_EQ(
+      combined_op_stats.step_db().step_sequence(0).step_info_per_core_size(),
+      2);
+  const auto& step_info_per_core =
+      combined_op_stats.step_db().step_sequence(0).step_info_per_core();
+  // global_core_id is computed using: 1000 * host_id + local_core_id.
+  EXPECT_TRUE(step_info_per_core.contains(1));
+  EXPECT_TRUE(step_info_per_core.contains(1001));
+
+  // Tear down environment and directory for testing.
+  int64 undeleted_files, undeleted_dirs;
+  TF_CHECK_OK(
+      test_env->DeleteRecursively(test_dir, &undeleted_files, &undeleted_dirs))
+      << "Failed to delete test directory: " << test_dir;
 }
 
 }  // namespace
