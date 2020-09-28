763697604ea95ad437084127161b936b2ffd5a6d
PlusPlusUltra <45232678+PlusPlusUltra@users.noreply.github.com>
2020-09-29 00:41:30 +0200

    More tests for experimental_get_tracing_count
    The function experimental_get_tracing_count was further tested.
    A method was defined in a class, and experimental_get_tracing_count
    was used to count the number of tracings of said method in two different
    instances of said class. As expected, the counts in the two instances
    were independent from each other.
    A minor fix was made in the previous test.

--- tensorflow/python/eager/def_function_test.py
+++ tensorflow/python/eager/def_function_test.py
@@ -940,7 +940,7 @@ class DefFunctionTest(test.TestCase, parameterized.TestCase):
     self.assertIn('Tracing is expensive', logs.output[0])
   
   def test_experimental_get_tracing_count(self):
-    @tf.function
+    @def_function.function
     def double(a):
       return a + a
     
@@ -949,6 +949,24 @@ class DefFunctionTest(test.TestCase, parameterized.TestCase):
     assertAllEqual(double.experimental_get_tracing_count(), 1)
     double(tf.constant("a"))
     assertAllEqual(double.experimental_get_tracing_count(), 2)
+
+    class TestClass():
+      @def_function.function
+      def testDouble(self, a):
+        return a + a
+    obj1 = TestClass()
+    obj1.testDouble(tf.constant(1))
+    obj1.testDouble(tf.constant(2))
+    obj1.testDouble(tf.constant(1.1))
+    assertAllEqual(obj1.testDouble.experimental_get_tracing_count(), 2)
+    obj2 = TestClass()
+    obj2.testDouble(tf.constant(1))
+    obj2.testDouble(tf.constant(1.1))
+    obj2.testDouble(tf.constant("a"))
+    assertAllEqual(obj2.testDouble.experimental_get_tracing_count(), 3)
+    assertAllEqual(obj1.testDouble.experimental_get_tracing_count(), 2)
+
+
 
 
 if __name__ == '__main__':
