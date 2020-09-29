bc3cbad8d8b8d3a8df73beb0686cca9dad62c7fa
A. Unique TensorFlower <gardener@tensorflow.org>
2020-09-29 08:01:46 -0700

    Internal change
    PiperOrigin-RevId: 334379282
    Change-Id: I2c293e1fcff614fb077ccd71ccc7ca74157f4a33

--- tensorflow/python/grappler/layout_optimizer_test.py
+++ tensorflow/python/grappler/layout_optimizer_test.py
@@ -193,9 +193,7 @@ def _get_cluster():
 
 def _is_transpose(node):
   return node.endswith('TransposeNHWCToNCHW-LayoutOptimizer') or node.endswith(
-      'TransposeNCHWToNHWC-LayoutOptimizer') or node.endswith(
-          'TransposeNDHWCToNCDHW-LayoutOptimizer') or node.endswith(
-              'TransposeNCDHWToNDHWC-LayoutOptimizer')
+      'TransposeNCHWToNHWC-LayoutOptimizer')
 
 
 def _is_permute(node):
@@ -1230,51 +1228,6 @@ class LayoutOptimizerTest(test.TestCase):
       self._assert_trans_ndhwc_to_ncdhw('Conv3D-0', nodes)
       self._assert_map_ndhwc_to_ncdhw('Mean-1', nodes)
       self._assert_trans_ncdhw_to_ndhwc('Mean-0-0', nodes)
-      self.assertAllClose(output_val_ref, output_val, atol=1e-3)
-
-  @test_util.deprecated_graph_mode_only
-  def testBinaryOpsFor5DTensors(self):
-    if test.is_gpu_available(cuda_only=True):
-      random_seed.set_random_seed(0)
-      x = random_ops.truncated_normal([1, 4, 2, 3, 3], seed=0)
-      w = random_ops.truncated_normal([2, 2, 2, 3, 3], seed=0)
-      mean = random_ops.truncated_normal([1, 1, 1, 1, 3], seed=0)
-      variance = random_ops.truncated_normal([1, 1, 1, 1, 3], seed=0)
-      gamma = random_ops.truncated_normal([1, 1, 1, 1, 3], seed=0)
-      beta = random_ops.truncated_normal([1, 1, 1, 1, 3], seed=0)
-      conv3d = gen_nn_ops.conv3d(x, w, [1, 1, 1, 1, 1], 'SAME')
-      y = nn.batch_normalization(
-          conv3d,
-          mean=mean,
-          variance=variance,
-          scale=gamma,
-          offset=beta,
-          variance_epsilon=0.001)
-      output = array_ops.identity(y)
-
-      with session.Session(config=_get_config(False)) as sess:
-        output_val_ref = sess.run(output)
-
-      with session.Session(config=_get_config()) as sess:
-        metadata = config_pb2.RunMetadata()
-        output_val = sess.run(output, run_metadata=metadata)
-
-      nodes = []
-      num_transposes = 0
-      for node in metadata.cost_graph.node:
-        if _is_transpose(node.name):
-          num_transposes += 1
-        nodes.append(node.name)
-
-      # The binary ops mul_1 and add_1 in batch norm need to transpose one of
-      # the two inputs to NCDHW. The other input has already been tranposed via
-      # Conv3D.
-      expected_num_transposes = 4
-      self.assertEqual(expected_num_transposes, num_transposes)
-      self._assert_trans_ndhwc_to_ncdhw('Conv3D-0', nodes)
-      self._assert_trans_ndhwc_to_ncdhw('batchnorm/mul_1-1', nodes)
-      self._assert_trans_ndhwc_to_ncdhw('batchnorm/add_1-1', nodes)
-      self._assert_trans_ncdhw_to_ndhwc('batchnorm/add_1-0-0', nodes)
       self.assertAllClose(output_val_ref, output_val, atol=1e-3)
 
   @test_util.deprecated_graph_mode_only
