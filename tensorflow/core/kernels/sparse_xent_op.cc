b0d877aa2b658ae08063f9f90c44550eedb2c911
Vishakha Agrawal <vishakha.agrawal@intel.com>
2020-10-01 12:08:48 -0700

    Another code formating

--- tensorflow/core/kernels/sparse_xent_op.cc
+++ tensorflow/core/kernels/sparse_xent_op.cc
@@ -18,11 +18,11 @@ limitations under the License.
 #define EIGEN_USE_THREADS
 
 #include "tensorflow/core/kernels/sparse_xent_op.h"
-#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/framework/tensor_types.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 
 namespace tensorflow {
 
