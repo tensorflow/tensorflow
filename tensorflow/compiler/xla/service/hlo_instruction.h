b678b55e332af661e5a3b58880399ea3b81679cd
Trent Lo <trentl@nvidia.com>
2020-10-02 14:52:36 -0700

    [XLA/GPU] clear operands for removed HLOs.
    This avoids possible accesses to the Null operands.

--- tensorflow/compiler/xla/service/hlo_instruction.h
+++ tensorflow/compiler/xla/service/hlo_instruction.h
@@ -1911,6 +1911,10 @@ class HloInstruction {
   // by factory methods.
   HloInstruction(HloOpcode opcode, const Shape& shape);
 
+  void RemoveAllOperands() {
+    operands_.clear();
+  }
+
   void RemoveOperandAt(int index) {
     operands_.erase(operands_.begin() + index);
   }
