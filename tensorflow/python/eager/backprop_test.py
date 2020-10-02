40ce446a7b75b257118e3dbfd8bdc5a792707600
Allen Lavoie <allenl@google.com>
2020-10-02 16:17:17 -0700

    Add a missing handle data copy in tf.function gradients
    PiperOrigin-RevId: 335120852
    Change-Id: I592a34fb2369d899eb069811494fa961d823e378

--- tensorflow/python/eager/backprop_test.py
+++ tensorflow/python/eager/backprop_test.py
@@ -1725,8 +1725,19 @@ class JacobianTest(test.TestCase):
 
   def test_foldl_partial_function(self):
     x = array_ops.zeros([3])
-    # TODO(allenl): Track down missing handle data when persistent=True and
-    # compare to experimental_use_pfor=False.
+    with backprop.GradientTape(persistent=True) as tape:
+      tape.watch(x)
+      result = def_function.function(
+          functools.partial(functional_ops.foldl_v2, lambda a, b: a + b))(
+              x)
+    self.assertAllClose([1., 1., 1.],
+                        tape.jacobian(result, x, experimental_use_pfor=True))
+    self.assertAllClose([1., 1., 1.],
+                        tape.jacobian(result, x, experimental_use_pfor=False))
+
+    # Non-persistent tapes take a different function gradient path, but also
+    # work with pfor=True.
+    x = array_ops.zeros([3])
     with backprop.GradientTape() as tape:
       tape.watch(x)
       result = def_function.function(
