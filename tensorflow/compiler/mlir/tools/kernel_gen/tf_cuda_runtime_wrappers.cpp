7ae3aed902725fbcd2258164778c0dd8eb6dd689
Andy Ly <lyandy@google.com>
2020-10-02 14:03:03 -0700

    Internal build changes.
    PiperOrigin-RevId: 335096321
    Change-Id: If49e4a5d8a0720b1b9c679aa83f1ed3b8dfb0e2e

--- tensorflow/compiler/mlir/tools/kernel_gen/tf_cuda_runtime_wrappers.cpp
+++ tensorflow/compiler/mlir/tools/kernel_gen/tf_cuda_runtime_wrappers.cpp
@@ -20,8 +20,8 @@ limitations under the License.
 #include <cassert>
 #include <numeric>
 
-#include "third_party/llvm/llvm-project/llvm/include/llvm/ADT/ArrayRef.h"
-#include "third_party/llvm/llvm-project/llvm/include/llvm/Support/raw_ostream.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/raw_ostream.h"
 #include "third_party/llvm/llvm-project/mlir/include/mlir/ExecutionEngine/CRunnerUtils.h"
 
 #if GOOGLE_CUDA
