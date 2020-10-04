f0784fcc9bea1d2b7342e57c28462564e1089972
juulie <julian.berteling@gmail.com>
2020-10-04 16:10:34 +0200

    Fixed inputs not being cast to tuple

--- tensorflow/python/keras/layers/recurrent.py
+++ tensorflow/python/keras/layers/recurrent.py
@@ -2959,7 +2959,7 @@ def _standardize_args(inputs, initial_state, constants, num_constants):
     if len(inputs) > 1:
       inputs = tuple(inputs)
     else:
-      inputs = inputs[0]
+      inputs = tuple(inputs[0])
 
   def to_list_or_none(x):
     if x is None or isinstance(x, list):
