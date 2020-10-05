a9740ce6bae089ade23095071807113b020a200f
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-10-05 20:09:40 +0530

    - updated according to review.

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -5539,8 +5539,8 @@ def generate_bounding_box_proposals(scores,
   Args:
     scores: A 4-D float `Tensor` of shape `[num_images, height, width, num_achors]` 
       containing scores of the boxes for given anchors, can be unsorted.
-    bbox_deltas: A 4-D float `Tensor` of shape `[num_images, height, width, 4 x num_anchors]`. 
-      encoding boxes with respec to each anchor. Coordinates are given in the form `[dy, dx, dh, dw]`.
+    bbox_deltas: A 4-D float `Tensor` of shape `[num_images, height, width, 4 x num_anchors]` 
+      encoding boxes with respect to each anchor. Coordinates are given in the form `[dy, dx, dh, dw]`.
     image_info: A 2-D float `Tensor` of shape `[num_images, 5]` containing image information Height, Width, Scale.
     anchors: A 2-D float `Tensor` of shape `[num_anchors, 4]` describing the anchor boxes. 
       Boxes are formatted in the form `[y1, x1, y2, x2]`.
