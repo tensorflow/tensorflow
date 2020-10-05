122655424ec4e5842ad8981a70e4b7fa13c6103d
Jose Baiocchi <jbaiocchi@google.com>
2020-10-05 14:15:15 -0700

    Move TF 2.x profiler files out of top-level profiler/internal
    profiler_interface,profiler_factory -> lib/
    parse_annotation -> utils/
    traceme_recorder,annotation_stack -> internal/cpu/
    
    PiperOrigin-RevId: 335496875
    Change-Id: I8f1483437ee4f3bbe37b3d3450a3e690cb6dfa90

--- tensorflow/core/profiler/utils/parse_annotation_test.cc
+++ tensorflow/core/profiler/utils/parse_annotation_test.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/core/profiler/internal/parse_annotation.h"
+#include "tensorflow/core/profiler/utils/parse_annotation.h"
 
 #include <vector>
 
