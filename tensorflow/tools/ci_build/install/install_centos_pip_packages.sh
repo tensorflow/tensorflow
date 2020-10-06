e191015d6f9c12f72f71d40d8c2d14f44f6a31b6
bhack <bhack@users.noreply.github.com>
2020-10-06 17:24:35 +0200

    Revert python2

--- tensorflow/tools/ci_build/install/install_centos_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_centos_pip_packages.sh
@@ -73,7 +73,7 @@ pip2 install py-cpuinfo
 pip3 install py-cpuinfo
 
 # pylint tests require the following:
-pip2 install pylint==2.6.0
+pip2 install pylint==1.6.4
 pip3 install pylint==2.6.0
 
 # pycodestyle tests require the following:
