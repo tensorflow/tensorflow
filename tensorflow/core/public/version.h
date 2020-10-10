66731f587f293a6189c296a7b447bdccf2819731
A. Unique TensorFlower <gardener@tensorflow.org>
2020-10-10 02:02:06 -0700

    Update GraphDef version to 550.
    PiperOrigin-RevId: 336435700
    Change-Id: I77d14835973c0434944e544b68f33e844b2764c3

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 549  // Updated: 2020/10/9
+#define TF_GRAPH_DEF_VERSION 550  // Updated: 2020/10/10
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
