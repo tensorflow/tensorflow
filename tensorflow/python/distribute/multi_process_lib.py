dac478e2bebb21142622bd6cb7add104d8585924
Bruce Fontaine <bfontain@google.com>
2020-10-19 11:32:38 -0700

    Add missing comment about TPU disable to multi_process_runner.
    PiperOrigin-RevId: 337897956
    Change-Id: Ica51a636eecbcc426c1c1c02253ccc1e6068867c

--- tensorflow/python/distribute/multi_process_lib.py
+++ tensorflow/python/distribute/multi_process_lib.py
@@ -32,6 +32,8 @@ def is_oss():
 
 
 def _is_enabled():
+  # Note that flags may not be parsed at this point and simply importing the
+  # flags module causes a variety of unusual errors.
   tpu_args = [arg for arg in sys.argv if arg.startswith('--tpu')]
   if is_oss() and tpu_args:
     return False
