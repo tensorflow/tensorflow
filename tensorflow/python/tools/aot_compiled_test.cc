9fea4acc267700f1956eebba4922e310f548d520
Eugene Brevdo <ebrevdo@google.com>
2020-10-19 22:14:16 -0700

    [TF] [saved_model_cli] Add unit test for single threaded AOT compiled large matmul.
    PiperOrigin-RevId: 337998263
    Change-Id: I0b58817043c190af183b67a5ae45a0f3607a5b80

--- tensorflow/python/tools/aot_compiled_test.cc
+++ tensorflow/python/tools/aot_compiled_test.cc
@@ -13,10 +13,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/python/tools/aot_compiled_vars_and_arithmetic.h"
 #include "tensorflow/python/tools/aot_compiled_vars_and_arithmetic_frozen.h"
+#include "tensorflow/python/tools/aot_compiled_x_matmul_y_large.h"
+#include "tensorflow/python/tools/aot_compiled_x_matmul_y_small.h"
 #include "tensorflow/python/tools/aot_compiled_x_plus_y.h"
 
 namespace tensorflow {
@@ -28,6 +31,63 @@ TEST(AOTCompiledSavedModelTest, XPlusY) {
   *model.arg_feed_y_data() = 4.0f;
   CHECK(model.Run());
   ASSERT_NEAR(model.result_fetch_output_0(), 7.0f, /*abs_error=*/1e-6f);
+}
+
+TEST(AOTCompiledSavedModelTest, XMatmulYLarge) {
+  XMatmulYLarge model;
+  // Calculation is: output_0 = x @ y.
+  EXPECT_EQ(model.arg0_size(), sizeof(float) * 3000 * 5000);
+  EXPECT_EQ(model.arg1_size(), sizeof(float) * 5000 * 4000);
+  EXPECT_EQ(model.result0_size(), sizeof(float) * 3000 * 4000);
+
+  Eigen::Tensor<float, 2, Eigen::RowMajor> arg0(3000, 5000);
+  Eigen::Tensor<float, 2, Eigen::RowMajor> arg1(5000, 4000);
+  arg0.setRandom();
+  arg1.setRandom();
+
+  // Set up dimensions for standard matmul.
+  const Eigen::array<Eigen::IndexPair<int>, 1> product_dims = {
+      Eigen::IndexPair<int>(1, 0)};
+  // Ground truth matmul.
+  const Eigen::Tensor<float, 2, Eigen::RowMajor> expected_output0 =
+      arg0.contract(arg1, product_dims);
+
+  model.set_arg_feed_x_data(arg0.data());
+  model.set_arg_feed_y_data(arg1.data());
+  CHECK(model.Run());
+  EXPECT_NEAR(model.result_fetch_output_0(0, 0), expected_output0(0, 0),
+              /*abs_error=*/1e-6f);
+  EXPECT_NEAR(model.result_fetch_output_0(2999, 3999),
+              expected_output0(2999, 3999),
+              /*abs_error=*/1e-6f);
+}
+
+TEST(AOTCompiledSavedModelTest, XMatmulYSmall) {
+  XMatmulYSmall model;
+  // Calculation is: output_0 = x @ y.
+  EXPECT_EQ(model.arg0_size(), sizeof(float) * 3 * 5);
+  EXPECT_EQ(model.arg1_size(), sizeof(float) * 5 * 4);
+  EXPECT_EQ(model.result0_size(), sizeof(float) * 3 * 4);
+
+  Eigen::Tensor<float, 2, Eigen::RowMajor> arg0(3, 5);
+  Eigen::Tensor<float, 2, Eigen::RowMajor> arg1(5, 4);
+  arg0.setRandom();
+  arg1.setRandom();
+
+  // Set up dimensions for standard matmul.
+  const Eigen::array<Eigen::IndexPair<int>, 1> product_dims = {
+      Eigen::IndexPair<int>(1, 0)};
+  // Ground truth matmul.
+  const Eigen::Tensor<float, 2, Eigen::RowMajor> expected_output0 =
+      arg0.contract(arg1, product_dims);
+
+  model.set_arg_feed_x_data(arg0.data());
+  model.set_arg_feed_y_data(arg1.data());
+  CHECK(model.Run());
+  EXPECT_NEAR(model.result_fetch_output_0(0, 0), expected_output0(0, 0),
+              /*abs_error=*/1e-6f);
+  EXPECT_NEAR(model.result_fetch_output_0(2, 3), expected_output0(2, 3),
+              /*abs_error=*/1e-6f);
 }
 
 TEST(AOTCompiledSavedModelTest, VarsAndArithmetic) {
