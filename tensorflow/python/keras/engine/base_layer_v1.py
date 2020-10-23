7ff387f88f3f5c898d0134586e0429f5a787a54e
Tomer Kaftan <kaftan@google.com>
2020-10-23 15:38:47 -0700

    Remove usage of internal InaccessibleTensorError from Keras.
    PiperOrigin-RevId: 338757838
    Change-Id: I8b4e69895cc999a518fa7038c1e5a1eec2fa3355

--- tensorflow/python/keras/engine/base_layer_v1.py
+++ tensorflow/python/keras/engine/base_layer_v1.py
@@ -951,9 +951,15 @@ class Layer(base_layer.Layer):
           if callable(u):
             try:
               u = u()
-            except errors.InaccessibleTensorError:
-              base_layer_utils.check_graph_consistency(
-                  method='add_update', force_raise=True)
+            except ValueError as e:
+              if 'InaccessibleTensorError' in type(e).__name__:
+                # For one specific case of error we try to raise
+                # a more meaningful error message about the graph if we can.
+                # This error is an internal TF symbol that is not
+                # publicly exposed, so we check the name directly rather
+                # than using a direct import.
+                base_layer_utils.check_graph_consistency(
+                    method='add_update', force_raise=True)
               raise  # check_graph_consistency may not always raise.
           base_layer_utils.check_graph_consistency(u, method='add_update')
           collected_updates.append(u)
