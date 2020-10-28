f9818f12c332e8a35dbc9042af55649be64f30c4
Jaesung Chung <jaesung@google.com>
2020-10-28 15:42:06 -0700

    Put the pass for converting TF control flow regions to functional before optimizing global tensors
    This transforms WhileRegion to a functional control flow since TFLite passes do not deal with WhileRegion as yet.
    
    PiperOrigin-RevId: 339552976
    Change-Id: I767f9308a686d98a8e5cd553019c1e7b5b602230

--- tensorflow/lite/python/lite_v2_test.py
+++ tensorflow/lite/python/lite_v2_test.py
@@ -888,6 +888,31 @@ class FromSavedModelTest(lite_v2_test_util.ModelTest):
 
     self.assertTrue(tflite_model)
 
+  @test_util.run_v2_only
+  def testNonStatefulConvLSTM2D(self):
+    """Test saved model with non stateful ConvLSTM2D keras layer."""
+    # Create keras model
+    model = tf.keras.Sequential([
+        tf.keras.layers.ConvLSTM2D(
+            32, (3, 3),
+            padding='same',
+            return_sequences=True,
+            stateful=False,
+            batch_input_shape=(1, 1, 10, 10, 1))
+    ])
+    model.compile()
+
+    # Export the keras model to saved model.
+    saved_model_dir = os.path.join(self.get_temp_dir(), 'conv_lstm_2d')
+    model.save(saved_model_dir, save_format='tf', include_optimizer=False)
+
+    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS
+    ]
+    tflite_model = converter.convert()
+    self.assertTrue(tflite_model)
+
 
 class FromKerasModelTest(lite_v2_test_util.ModelTest):
 
