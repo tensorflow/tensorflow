a445d6b7cf21c9c1c8730b74c29db2acd79ed3e3
fsx950223 <fsx950223@gmail.com>
2020-10-29 05:01:21 +0000

    add dict test case

--- tensorflow/python/eager/backprop_test.py
+++ tensorflow/python/eager/backprop_test.py
@@ -1600,10 +1600,22 @@ class BackpropTest(test.TestCase, parameterized.TestCase):
     def outer(x):
       return [x[0]+1, x[1]+1]
 
-    x = [variables.Variable([1.0, 2.0], name='x'),
-        variables.Variable(1.0, name='x')]
+    x = [variables.Variable([1.0, 2.0], name='a'),
+        variables.Variable(1.0, name='b')]
     with backprop.GradientTape():
       y = outer(x)
+      self.assertAllEqual(y[0], [2.0, 3.0])
+      self.assertAllEqual(y[1], 2.0)
+
+    @custom_gradient.recompute_grad
+    def outer_dict(x):
+      for key in x.keys():
+        x[key] = x[key] + 1
+      return x
+    x = {x[0].ref(): x[0], x[1].ref(): x[1]}
+    with backprop.GradientTape():
+      y = outer_dict(x)
+      y = list(y.values())
       self.assertAllEqual(y[0], [2.0, 3.0])
       self.assertAllEqual(y[1], 2.0)
 
