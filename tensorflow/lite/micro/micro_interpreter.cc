0c19080aa97ddfc48049be6cfe66cf9e1ed98e02
Advait Jain <advaitjain@users.noreply.github.com>
2020-10-29 16:18:11 -0700

    testing to see if CI results are visible.

--- tensorflow/lite/micro/micro_interpreter.cc
+++ tensorflow/lite/micro/micro_interpreter.cc
@@ -18,7 +18,7 @@ limitations under the License.
 #include <cstddef>
 #include <cstdint>
 
-#include "flatbuffers/flatbuffers.h"  // from @flatbuffers
+include "flatbuffers/flatbuffers.h"  // from @flatbuffers
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/core/api/error_reporter.h"
 #include "tensorflow/lite/core/api/tensor_utils.h"
