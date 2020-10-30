80004eb8ab959db1cea8701dd8022f0f0b774077
Renjie Liu <renjieliu@google.com>
2020-10-29 18:37:33 -0700

    Move split merged operand pass to the end of the pass manager.
    PiperOrigin-RevId: 339789063
    Change-Id: Ida829821979d69fc04b5f75e97ffefd5a7a31368

--- tensorflow/compiler/mlir/lite/tf_tfl_passes.cc
+++ tensorflow/compiler/mlir/lite/tf_tfl_passes.cc
@@ -211,6 +211,13 @@ void AddTFToTFLConversionPasses(const mlir::TFL::PassConfig& pass_config,
     pass_manager->addPass(mlir::createSymbolDCEPass());
     pass_manager->addNestedPass<mlir::FuncOp>(mlir::createCanonicalizerPass());
     pass_manager->addNestedPass<mlir::FuncOp>(mlir::createCSEPass());
+
+    // Run quantization after all the floating point model conversion is
+    // completed.
+    if (pass_config.quant_specs.RunPropagationAndRewriteQuantizationPasses()) {
+      AddQuantizationPasses(pass_config.quant_specs, pass_manager);
+    }
+
     // This pass should be always at the end of the floating point model
     // conversion. Some TFL ops like unidirectional
     // sequence lstm will have stateful operands and some optimization passes
@@ -219,12 +226,6 @@ void AddTFToTFLConversionPasses(const mlir::TFL::PassConfig& pass_config,
     // merged inputs until we have 1st class variable support or reuse
     // tf.variable to model this.
     pass_manager->addPass(mlir::TFL::CreateSplitMergedOperandsPass());
-
-    // Run quantization after all the floating point model conversion is
-    // completed.
-    if (pass_config.quant_specs.RunPropagationAndRewriteQuantizationPasses()) {
-      AddQuantizationPasses(pass_config.quant_specs, pass_manager);
-    }
   }
 }
 
