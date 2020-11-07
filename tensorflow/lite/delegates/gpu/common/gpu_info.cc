430dbcc5def72b26167559aa74f11c7f1ee73dcf
Raman Sarokin <sorokin@google.com>
2020-11-06 16:34:52 -0800

    GpuInfo extended, added Adreno info. Making it similar to OpenCL.
    PiperOrigin-RevId: 341140554
    Change-Id: I46b4476f28046fd4e16f1cc1d6d918032c860446

--- tensorflow/lite/delegates/gpu/common/gpu_info.cc
+++ tensorflow/lite/delegates/gpu/common/gpu_info.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "tensorflow/lite/delegates/gpu/common/gpu_info.h"
 
+#include <map>
 #include <string>
 
 #include "absl/strings/ascii.h"
@@ -23,79 +24,217 @@ namespace tflite {
 namespace gpu {
 namespace {
 
-GpuType GetGpuType(const std::string& renderer) {
+GpuVendor GetGpuVendor(const std::string& renderer) {
   if (renderer.find("mali") != renderer.npos) {
-    return GpuType::MALI;
+    return GpuVendor::kMali;
   }
   if (renderer.find("adreno") != renderer.npos) {
-    return GpuType::ADRENO;
+    return GpuVendor::kQualcomm;
   }
   if (renderer.find("powervr") != renderer.npos) {
-    return GpuType::POWERVR;
+    return GpuVendor::kPowerVR;
   }
   if (renderer.find("intel") != renderer.npos) {
-    return GpuType::INTEL;
+    return GpuVendor::kIntel;
   }
   if (renderer.find("nvidia") != renderer.npos) {
-    return GpuType::NVIDIA;
+    return GpuVendor::kNvidia;
   }
-  return GpuType::UNKNOWN;
+  return GpuVendor::kUnknown;
 }
 
-GpuModel GetGpuModel(const std::string& renderer) {
-  auto found_model = [&](std::string model) -> bool {
-    return renderer.find(model) != renderer.npos;
+AdrenoGpu GetAdrenoGpuVersion(const std::string& device_name) {
+  const std::map<std::string, AdrenoGpu> kMapping = {
+      // Adreno 6xx series
+      {"685", AdrenoGpu::kAdreno685},
+      {"680", AdrenoGpu::kAdreno680},
+      {"675", AdrenoGpu::kAdreno675},
+      {"650", AdrenoGpu::kAdreno650},
+      {"640", AdrenoGpu::kAdreno640},
+      {"630", AdrenoGpu::kAdreno630},
+      {"620", AdrenoGpu::kAdreno620},
+      {"616", AdrenoGpu::kAdreno618},
+      {"616", AdrenoGpu::kAdreno616},
+      {"615", AdrenoGpu::kAdreno615},
+      {"612", AdrenoGpu::kAdreno612},
+      {"610", AdrenoGpu::kAdreno610},
+      {"605", AdrenoGpu::kAdreno605},
+      // Adreno 5xx series
+      {"540", AdrenoGpu::kAdreno540},
+      {"530", AdrenoGpu::kAdreno530},
+      {"512", AdrenoGpu::kAdreno512},
+      {"510", AdrenoGpu::kAdreno510},
+      {"509", AdrenoGpu::kAdreno509},
+      {"508", AdrenoGpu::kAdreno508},
+      {"506", AdrenoGpu::kAdreno506},
+      {"505", AdrenoGpu::kAdreno505},
+      {"504", AdrenoGpu::kAdreno504},
+      // Adreno 4xx series
+      {"430", AdrenoGpu::kAdreno430},
+      {"420", AdrenoGpu::kAdreno420},
+      {"418", AdrenoGpu::kAdreno418},
+      {"405", AdrenoGpu::kAdreno405},
+      // Adreno 3xx series
+      {"330", AdrenoGpu::kAdreno330},
+      {"320", AdrenoGpu::kAdreno320},
+      {"308", AdrenoGpu::kAdreno308},
+      {"306", AdrenoGpu::kAdreno306},
+      {"305", AdrenoGpu::kAdreno305},
+      {"304", AdrenoGpu::kAdreno304},
+      // Adreno 2xx series
+      {"225", AdrenoGpu::kAdreno225},
+      {"220", AdrenoGpu::kAdreno220},
+      {"205", AdrenoGpu::kAdreno205},
+      {"203", AdrenoGpu::kAdreno203},
+      {"200", AdrenoGpu::kAdreno200},
+      // Adreno 1xx series
+      {"130", AdrenoGpu::kAdreno130},
+      {"120", AdrenoGpu::kAdreno120},
   };
-  // Adreno 6xx series
-  if (found_model("640")) return GpuModel::ADRENO640;
-  if (found_model("630")) return GpuModel::ADRENO630;
-  if (found_model("616")) return GpuModel::ADRENO616;
-  if (found_model("615")) return GpuModel::ADRENO615;
-  if (found_model("612")) return GpuModel::ADRENO612;
-  if (found_model("605")) return GpuModel::ADRENO605;
-  // Adreno 5xx series
-  if (found_model("540")) return GpuModel::ADRENO540;
-  if (found_model("530")) return GpuModel::ADRENO530;
-  if (found_model("512")) return GpuModel::ADRENO512;
-  if (found_model("510")) return GpuModel::ADRENO510;
-  if (found_model("509")) return GpuModel::ADRENO509;
-  if (found_model("508")) return GpuModel::ADRENO508;
-  if (found_model("506")) return GpuModel::ADRENO506;
-  if (found_model("505")) return GpuModel::ADRENO505;
-  if (found_model("504")) return GpuModel::ADRENO504;
-  // Adreno 4xx series
-  if (found_model("430")) return GpuModel::ADRENO430;
-  if (found_model("420")) return GpuModel::ADRENO420;
-  if (found_model("418")) return GpuModel::ADRENO418;
-  if (found_model("405")) return GpuModel::ADRENO405;
-  // Adreno 3xx series
-  if (found_model("330")) return GpuModel::ADRENO330;
-  if (found_model("320")) return GpuModel::ADRENO320;
-  if (found_model("308")) return GpuModel::ADRENO308;
-  if (found_model("306")) return GpuModel::ADRENO306;
-  if (found_model("305")) return GpuModel::ADRENO305;
-  if (found_model("304")) return GpuModel::ADRENO304;
-  // Adreno 2xx series
-  if (found_model("225")) return GpuModel::ADRENO225;
-  if (found_model("220")) return GpuModel::ADRENO220;
-  if (found_model("205")) return GpuModel::ADRENO205;
-  if (found_model("203")) return GpuModel::ADRENO203;
-  if (found_model("200")) return GpuModel::ADRENO200;
-  // Adreno 1xx series
-  if (found_model("130")) return GpuModel::ADRENO130;
-  return GpuModel::UNKNOWN;
+
+  for (const auto& v : kMapping) {
+    if (device_name.find(v.first) != std::string::npos) {
+      return v.second;
+    }
+  }
+  return AdrenoGpu::kUnknown;
 }
 
 }  // namespace
 
-void GetGpuModelAndType(const std::string& renderer, GpuModel* gpu_model,
-                        GpuType* gpu_type) {
-  std::string lowered = renderer;
-  absl::AsciiStrToLower(&lowered);
-  *gpu_type = GetGpuType(lowered);
-  *gpu_model =
-      *gpu_type == GpuType::ADRENO ? GetGpuModel(lowered) : GpuModel::UNKNOWN;
+AdrenoInfo::AdrenoInfo(const std::string& device_version)
+    : adreno_gpu(GetAdrenoGpuVersion(device_version)) {}
+
+bool AdrenoInfo::IsAdreno1xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno120 ||
+         adreno_gpu == AdrenoGpu::kAdreno130;
 }
+
+bool AdrenoInfo::IsAdreno2xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno200 ||
+         adreno_gpu == AdrenoGpu::kAdreno203 ||
+         adreno_gpu == AdrenoGpu::kAdreno205 ||
+         adreno_gpu == AdrenoGpu::kAdreno220 ||
+         adreno_gpu == AdrenoGpu::kAdreno225;
+}
+
+bool AdrenoInfo::IsAdreno3xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno304 ||
+         adreno_gpu == AdrenoGpu::kAdreno305 ||
+         adreno_gpu == AdrenoGpu::kAdreno306 ||
+         adreno_gpu == AdrenoGpu::kAdreno308 ||
+         adreno_gpu == AdrenoGpu::kAdreno320 ||
+         adreno_gpu == AdrenoGpu::kAdreno330;
+}
+
+bool AdrenoInfo::IsAdreno4xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno405 ||
+         adreno_gpu == AdrenoGpu::kAdreno418 ||
+         adreno_gpu == AdrenoGpu::kAdreno420 ||
+         adreno_gpu == AdrenoGpu::kAdreno430;
+}
+
+bool AdrenoInfo::IsAdreno5xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno504 ||
+         adreno_gpu == AdrenoGpu::kAdreno505 ||
+         adreno_gpu == AdrenoGpu::kAdreno506 ||
+         adreno_gpu == AdrenoGpu::kAdreno508 ||
+         adreno_gpu == AdrenoGpu::kAdreno509 ||
+         adreno_gpu == AdrenoGpu::kAdreno510 ||
+         adreno_gpu == AdrenoGpu::kAdreno512 ||
+         adreno_gpu == AdrenoGpu::kAdreno530 ||
+         adreno_gpu == AdrenoGpu::kAdreno540;
+}
+
+bool AdrenoInfo::IsAdreno6xx() const {
+  return adreno_gpu == AdrenoGpu::kAdreno605 ||
+         adreno_gpu == AdrenoGpu::kAdreno610 ||
+         adreno_gpu == AdrenoGpu::kAdreno612 ||
+         adreno_gpu == AdrenoGpu::kAdreno615 ||
+         adreno_gpu == AdrenoGpu::kAdreno616 ||
+         adreno_gpu == AdrenoGpu::kAdreno618 ||
+         adreno_gpu == AdrenoGpu::kAdreno620 ||
+         adreno_gpu == AdrenoGpu::kAdreno630 ||
+         adreno_gpu == AdrenoGpu::kAdreno640 ||
+         adreno_gpu == AdrenoGpu::kAdreno650 ||
+         adreno_gpu == AdrenoGpu::kAdreno675 ||
+         adreno_gpu == AdrenoGpu::kAdreno680 ||
+         adreno_gpu == AdrenoGpu::kAdreno685;
+}
+
+bool AdrenoInfo::IsAdreno6xxOrHigher() const { return IsAdreno6xx(); }
+
+int AdrenoInfo::GetMaximumWavesCount() const {
+  if (IsAdreno6xx()) {
+    if (adreno_gpu == AdrenoGpu::kAdreno640) {
+      return 30;
+    } else {
+      return 16;
+    }
+  } else {
+    // all other versions not supported
+    return 1;
+  }
+}
+
+int AdrenoInfo::GetRegisterMemorySizePerComputeUnit() const {
+  if (IsAdreno6xx()) {
+    if (adreno_gpu == AdrenoGpu::kAdreno640) {
+      return 128 * 144 * 16;
+    } else if (adreno_gpu == AdrenoGpu::kAdreno650) {
+      return 128 * 64 * 16;
+    } else {
+      return 128 * 96 * 16;
+    }
+  } else {
+    // all other versions not supported
+    return 1;
+  }
+}
+
+int AdrenoInfo::GetMaximumWavesCount(int register_footprint_per_tread,
+                                     bool full_wave) const {
+  const int register_usage_per_wave =
+      GetWaveSize(full_wave) * register_footprint_per_tread;
+  const int possible_waves_count =
+      GetRegisterMemorySizePerComputeUnit() / register_usage_per_wave;
+  return std::min(possible_waves_count, GetMaximumWavesCount());
+}
+
+int AdrenoInfo::GetWaveSize(bool full_wave) const {
+  if (IsAdreno6xx()) {
+    return full_wave ? 128 : 64;
+  } else if (IsAdreno5xx() || IsAdreno4xx()) {
+    return full_wave ? 64 : 32;
+  } else {
+    // all other versions not supported
+    return 1;
+  }
+}
+
+void GetGpuInfoFromDeviceDescription(const std::string& gpu_description,
+                                     GpuInfo* gpu_info) {
+  std::string lowered = gpu_description;
+  absl::AsciiStrToLower(&lowered);
+  gpu_info->vendor = GetGpuVendor(lowered);
+  if (gpu_info->IsAdreno()) {
+    gpu_info->adreno_info = AdrenoInfo(lowered);
+  }
+}
+
+bool GpuInfo::IsAdreno() const { return vendor == GpuVendor::kQualcomm; }
+
+bool GpuInfo::IsApple() const { return vendor == GpuVendor::kApple; }
+
+bool GpuInfo::IsMali() const { return vendor == GpuVendor::kMali; }
+
+bool GpuInfo::IsPowerVR() const { return vendor == GpuVendor::kPowerVR; }
+
+bool GpuInfo::IsNvidia() const { return vendor == GpuVendor::kNvidia; }
+
+bool GpuInfo::IsAMD() const { return vendor == GpuVendor::kAMD; }
+
+bool GpuInfo::IsIntel() const { return vendor == GpuVendor::kIntel; }
 
 }  // namespace gpu
 }  // namespace tflite
