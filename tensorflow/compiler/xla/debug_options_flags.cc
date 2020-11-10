86acd2c0aa0e172ce159899c1af661d010eaa944
Yunxing Dai <yunxing@google.com>
2020-11-10 10:21:12 -0800

    Enable xla detailed logging by default.
    PiperOrigin-RevId: 341646294
    Change-Id: Ia488f3d003746df9b69badc45d6ebdd9693163ee

--- tensorflow/compiler/xla/debug_options_flags.cc
+++ tensorflow/compiler/xla/debug_options_flags.cc
@@ -74,7 +74,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_cpu_enable_xprof_traceme(false);
   opts.set_xla_gpu_unsafe_fallback_to_driver_on_ptxas_not_found(false);
   opts.set_xla_multiheap_size_constraint_per_heap(-1);
-
+  opts.set_xla_detailed_logging(true);
   return opts;
 }
 
