3fd34b6ac3600e8a0eaf6f10ebd1db8c4113e343
Thomas O'Malley <omalleyt@google.com>
2020-11-13 14:36:54 -0800

    Support tf.Modules in keras.Sequential.
    Adds a private ModuleWrapper class that wraps the tf.Module that the user
    passes to keras.Sequential. keras.Sequential models created in this way support
    SavedModel saving, but do not support HDF5 saving format.
    
    PiperOrigin-RevId: 342340478
    Change-Id: I0f9d44b3d73fa00bed4f3c474750269b9ec10fb0

--- tensorflow/python/keras/engine/functional.py
+++ tensorflow/python/keras/engine/functional.py
@@ -1397,3 +1397,48 @@ def shape_with_no_batch_size(x):
   if shape:
     shape[0] = None
   return shape
+
+
+class ModuleWrapper(base_layer.Layer):
+  """Wrapper for `tf.Module`s to support the Functional and Sequential API."""
+
+  def __init__(self, module, method_name=None, **kwargs):
+    """Initializes the wrapper Layer for this module.
+
+    Arguments:
+      module: The `tf.Module` instance to be wrapped.
+      method_name: (Optional) str. The name of the method to use as the forward
+        pass of the module. If not set, defaults to '__call__' if defined, or
+        'call'.
+      **kwargs: Additional keywrod arguments. See `tf.keras.layers.Layer`.
+
+    Raises:
+      ValueError: If `method` is not defined on `module`.
+    """
+    super(ModuleWrapper, self).__init__(**kwargs)
+    if method_name is None:
+      if hasattr(module, '__call__'):
+        method_name = '__call__'
+      elif hasattr(module, 'call'):
+        method_name = 'call'
+    if method_name is None or not hasattr(module, method_name):
+      raise ValueError('{} is not defined on object {}'.format(
+          method_name, module))
+
+    self._module = module
+    self._method_name = method_name
+
+    # Check if module.__call__ has a `training` arg or accepts `**kwargs`.
+    method = getattr(module, method_name)
+    method_arg_spec = tf_inspect.getfullargspec(method)
+    self._expects_training_arg = ('training' in method_arg_spec.args or
+                                  method_arg_spec.varkw is not None)
+    self._expects_mask_arg = ('mask' in method_arg_spec.args or
+                              method_arg_spec.varkw is not None)
+
+  def call(self, *args, **kwargs):
+    if 'training' in kwargs and not self._expects_training_arg:
+      kwargs.pop('training')
+    if 'mask' in kwargs and not self._expects_mask_arg:
+      kwargs.pop('mask')
+    return getattr(self._module, self._method_name)(*args, **kwargs)
