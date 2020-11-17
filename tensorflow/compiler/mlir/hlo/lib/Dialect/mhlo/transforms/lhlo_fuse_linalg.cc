e0bc8a4710b672246181e1eeace10879fe62e224
Stephan Herhut <herhut@google.com>
2020-11-17 06:34:11 -0800

    Extend fusion root heuristic to also work in partially bufferized programs.
    We now follow data flow though tensor_cast, tensor_load and tensor_to_memref
    operations.
    
    PiperOrigin-RevId: 342851104
    Change-Id: Idabed7ef4fb8b1de843fe8c9c5ea4ae3505117d8

--- tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/transforms/lhlo_fuse_linalg.cc
+++ tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/transforms/lhlo_fuse_linalg.cc
@@ -91,6 +91,31 @@ class LhloFuseLinalgPass
         if (result_buffers.insert(alias).second) {
           worklist.push_back(alias);
         }
+        continue;
+      }
+
+      if (auto tensor_load = dyn_cast<TensorLoadOp>(definingOp)) {
+        auto alias = tensor_load.memref();
+        if (result_buffers.insert(alias).second) {
+          worklist.push_back(alias);
+        }
+        continue;
+      }
+
+      if (auto tensor_to_memref = dyn_cast<TensorToMemrefOp>(definingOp)) {
+        auto alias = tensor_to_memref.tensor();
+        if (result_buffers.insert(alias).second) {
+          worklist.push_back(alias);
+        }
+        continue;
+      }
+
+      if (auto tensor_cast = dyn_cast<TensorCastOp>(definingOp)) {
+        auto alias = tensor_cast.source();
+        if (result_buffers.insert(alias).second) {
+          worklist.push_back(alias);
+        }
+        continue;
       }
 
       if (auto regionInterface =
