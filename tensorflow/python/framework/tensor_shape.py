90490744b6983bb992cf263cf61df54b6d08bf81
Yanhua Sun <yanhuasun@google.com>
2020-11-18 11:51:35 -0800

    Improve Tensorshape.__eq__ documentation
    PiperOrigin-RevId: 343124825
    Change-Id: I9b835d5fb3008da8f91ae6ab4c5bfa2c90ba5d3d

--- tensorflow/python/framework/tensor_shape.py
+++ tensorflow/python/framework/tensor_shape.py
@@ -1201,7 +1201,50 @@ class TensorShape(object):
       ])
 
   def __eq__(self, other):
-    """Returns True if `self` is equivalent to `other`."""
+    """Returns True if `self` is equivalent to `other`.
+
+    It first tries to convert `other` to `TensorShape`. `TypeError` is thrown
+    when the conversion fails. Otherwise, it compares each element in the
+    TensorShape dimensions.
+
+    * Two *Fully known* shapes, return True iff each element is equal.
+    >>> t_a = tf.TensorShape([1,2])
+    >>> a = [1, 2]
+    >>> t_b = tf.TensorShape([1,2])
+    >>> t_c = tf.TensorShape([1,2,3])
+    >>> t_a.__eq__(a)
+    True
+    >>> t_a.__eq__(t_b)
+    True
+    >>> t_a.__eq__(t_c)
+    False
+
+    * Two *Partially-known* shapes, return False.
+    >>> p_a = tf.TensorShape([1,None])
+    >>> p_b = tf.TensorShape([2,None])
+    >>> p_a.__eq__(p_b)
+    False
+    >>> t_a.__eq__(p_a)
+    False
+
+    * Two *Unknown shape*, return True.
+    >>> unk_a = tf.TensorShape(None)
+    >>> unk_b = tf.TensorShape(None)
+    >>> unk_a.__eq__(unk_b)
+    True
+    >>> unk_a.__eq__(t_a)
+    False
+
+    Args:
+      other: A `TensorShape` or type that can be converted to `TensorShape`.
+
+    Returns:
+      True if the dimensions are all equal.
+
+    Raises:
+      TypeError if `other` can not be converted to `TensorShape`.
+    """
+
     try:
       other = as_shape(other)
     except TypeError:
