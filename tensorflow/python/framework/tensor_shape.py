de556cca3ab1cc8b776b486427b3b4fa02b8d9ab
Yanhua Sun <yanhuasun@google.com>
2020-11-18 14:16:12 -0800

    Improve tf.TensorShape.merge_with documentation
    PiperOrigin-RevId: 343154723
    Change-Id: Ide2f4397e700f33a31e902a2425751586cadd9ee

--- tensorflow/python/framework/tensor_shape.py
+++ tensorflow/python/framework/tensor_shape.py
@@ -925,8 +925,28 @@ class TensorShape(object):
   def merge_with(self, other):
     """Returns a `TensorShape` combining the information in `self` and `other`.
 
-    The dimensions in `self` and `other` are merged elementwise,
-    according to the rules defined for `Dimension.merge_with()`.
+    The dimensions in `self` and `other` are merged element-wise,
+    according to the rules below:
+
+    ```python
+    Dimension(n).merge_with(Dimension(None)) == Dimension(n)
+    Dimension(None).merge_with(Dimension(n)) == Dimension(n)
+    Dimension(None).merge_with(Dimension(None)) == Dimension(None)
+    # raises ValueError for n != m
+    Dimension(n).merge_with(Dimension(m))
+    ```
+    >> ts = tf.TensorShape([1,2])
+    >> ot1 = tf.TensorShape([1,2])
+    >> ts.merge_with(ot).as_list()
+    [1,2]
+
+    >> ot2 = tf.TensorShape([1,None])
+    >> ts.merge_with(ot2).as_list()
+    [1,2]
+
+    >> ot3 = tf.TensorShape([None, None])
+    >> ot3.merge_with(ot2).as_list()
+    [1, None]
 
     Args:
       other: Another `TensorShape`.
