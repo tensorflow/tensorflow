9d8598fe15f0b9a36a2a277fa609eccc5470fb10
A. Unique TensorFlower <gardener@tensorflow.org>
2020-11-19 02:08:54 -0800

    [MLIR][KernelGen] Make buffer reuse analysis consider aliases for buffer sizes
    When analyzing the underlying buffer sizes, consider aliases. An alias of a
    buffer naturally has the same size.
    
    PiperOrigin-RevId: 343248734
    Change-Id: I4ab2df0aafef757f00b898debc33b2282e0e9ddb

--- tensorflow/compiler/mlir/tools/kernel_gen/transforms/buffer_reuse_pass.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/transforms/buffer_reuse_pass.cc
@@ -55,12 +55,14 @@ namespace {
 /// A temporary buffer size analysis that is correct but may be incomplete.
 class BufferSizeAnalysis {
  public:
-  explicit BufferSizeAnalysis(FuncOp f) { build(f); }
+  BufferSizeAnalysis(FuncOp f, const BufferAliasAnalysis &aliases) {
+    build(f, aliases);
+  }
 
   bool is_same_size(Value a, Value b) { return ecs_.isEquivalent(a, b); }
 
  private:
-  void build(FuncOp &f) {
+  void build(FuncOp &f, const BufferAliasAnalysis &aliases) {
     auto buffers = find_buffer_values(f);
 
     // Memrefs with statically known same shape and same symbol-free affine maps
@@ -102,10 +104,16 @@ class BufferSizeAnalysis {
       }
     });
 
-    // Operand and result of `reshape_memref_cast` must be of same size.
-    f.walk([&](MemRefReshapeOp reshapeOp) {
-      ecs_.unionSets(reshapeOp.result(), reshapeOp.source());
-    });
+    // All aliases of a memref must be of the same underlying buffer size.
+    for (auto e : aliases) {
+      Value value = e.getFirst();
+      if (!value.getType().isa<BaseMemRefType>()) continue;
+      for (Value alias : e.getSecond()) {
+        assert(alias.getType().isa<BaseMemRefType>() &&
+               "Expected aliases of memref to be memrefs.");
+        ecs_.unionSets(value, alias);
+      }
+    }
   }
 
   bool affine_maps_symbol_free_and_equal(ArrayRef<AffineMap> as,
@@ -181,7 +189,7 @@ class BufferReuseAnalysis {
 
   void find_reuse_candiates(FuncOp &f, BufferAliasAnalysis &aliases) {
     Liveness liveness(f);
-    BufferSizeAnalysis size_equivalences(f);
+    BufferSizeAnalysis size_equivalences(f, aliases);
     f.walk([&](Block *block) {
       find_reuse_candiates(block, aliases, liveness.getLiveness(block),
                            size_equivalences, f.getArguments());
@@ -204,31 +212,32 @@ class BufferReuseAnalysis {
 
       // Find reuse candidates for the regarded allocation.
       SmallVector<int64_t, 2> local_reuse_candidates;
-      for (auto it : llvm::enumerate(arguments)) {
-        int64_t old_buffer_index = it.index();
-        Value old_buffer = it.value();
+      for (BlockArgument old_buffer : arguments) {
         if (!old_buffer.getType().isa<BaseMemRefType>()) continue;
 
-        // Will not reuse buffers of different size as they may be too small.
+        // Size criterion: Do not reuse buffers of different size as they may be
+        // too small.
         if (!size_equivalences.is_same_size(new_buffer, old_buffer)) continue;
 
-        // Only reuse buffers that are no longer used on first reuse, i.e. they
-        // are no longer alive.
+        // Livetime criterion: Only reuse buffers that are no longer used on
+        // first reuse, i.e. they are no longer alive.
         bool livetimes_compatible = true;
         for (Value old_buffer_alias : aliases.resolve(old_buffer)) {
           if (first_reuse == nullptr) {
             // If the first use is beyond the end of this block we look at the
             // block end. An argument buffer that is already reusable there is
-            // certainly reusable at any later actual use.
+            // certainly reusable at any later actual use. Otherwise, livetimes
+            // are incompatible.
             if (liveness->isLiveOut(old_buffer_alias)) {
               livetimes_compatible = false;
               break;
             }
           } else {
-            // A buffer is *not* reusable if
-            //   i)  its last use is after the point of reuse, or
-            //   ii) its last use is also its first reuse but the operation
-            //       does not allow for local reuse.
+            // A buffer is reusable if
+            //   i)  its last use is before the point of reuse, or
+            //   ii) its last use is also its first reuse and the operation
+            //       allows for local reuse.
+            // Otherwise, livetimes are incompatible.
             Operation *last_use =
                 liveness->getEndOperation(old_buffer_alias, &block->front());
             assert(last_use != nullptr && last_use->getBlock() == block &&
@@ -245,9 +254,11 @@ class BufferReuseAnalysis {
           }
         }
 
-        // All criteria are fulfilled 🙂.
-        if (livetimes_compatible)
+        if (livetimes_compatible) {
+          // All criteria are fulfilled 🙂.
+          int64_t old_buffer_index = old_buffer.getArgNumber();
           local_reuse_candidates.push_back(old_buffer_index);
+        }
       }
 
       reuse_candidates_[&op] = local_reuse_candidates;
