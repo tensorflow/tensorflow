22a7fd609f36346bc1077ff8e1544594e9fb540d
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2020-11-26 01:22:29 +0700

    Use gradient checker for testing BiasAddGrad

--- tensorflow/c/experimental/gradients/nn_grad_test.cc
+++ tensorflow/c/experimental/gradients/nn_grad_test.cc
@@ -14,35 +14,66 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/c/experimental/gradients/nn_grad.h"
 
-#include <memory>
-
-#include "absl/container/flat_hash_set.h"
-#include "absl/types/span.h"
-#include "tensorflow/c/eager/abstract_context.h"
-#include "tensorflow/c/eager/abstract_tensor_handle.h"
-#include "tensorflow/c/eager/c_api_experimental.h"
 #include "tensorflow/c/eager/c_api_test_util.h"
-#include "tensorflow/c/eager/c_api_unified_experimental.h"
-#include "tensorflow/c/eager/c_api_unified_experimental_internal.h"
-#include "tensorflow/c/eager/gradients.h"
-#include "tensorflow/c/eager/gradients_internal.h"
-#include "tensorflow/c/eager/gradients_util.h"
-#include "tensorflow/c/experimental/gradients/nn_grad_testutil.h"
+#include "tensorflow/c/experimental/gradients/grad_test_helper.h"
 #include "tensorflow/c/experimental/gradients/tape/tape_context.h"
-#include "tensorflow/c/experimental/ops/nn_ops.h"
-#include "tensorflow/c/tf_status_helper.h"
-#include "tensorflow/c/tf_tensor.h"
-#include "tensorflow/core/lib/llvm_rtti/llvm_rtti.h"
-#include "tensorflow/core/platform/errors.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
 namespace gradients {
 namespace internal {
 namespace {
-using std::vector;
+
 using tensorflow::TF_StatusPtr;
 using tracing::TracingOperation;
+
+Status BiasAddModel(AbstractContext* ctx,
+                    absl::Span<AbstractTensorHandle* const> inputs,
+                    absl::Span<AbstractTensorHandle*> outputs,
+                    const GradientRegistry& registry) {
+  auto tape = new Tape(/*persistent=*/false);
+  tape->Watch(ToId(inputs[0]));  // Watch x.
+  tape->Watch(ToId(inputs[1]));  // Watch y.
+  std::vector<AbstractTensorHandle*> bias_add_outputs(1);
+  AbstractContextPtr tape_ctx(new TapeContext(ctx, tape, registry));
+  TF_RETURN_IF_ERROR(ops::BiasAdd(tape_ctx.get(), inputs,
+                                  absl::MakeSpan(bias_add_outputs), "BiasAdd"));
+  outputs[0] = bias_add_outputs[0];
+
+  delete tape;
+  return Status::OK();
+}
+
+Status BiasAddGradModel(AbstractContext* ctx,
+                        absl::Span<AbstractTensorHandle* const> inputs,
+                        absl::Span<AbstractTensorHandle*> outputs,
+                        const GradientRegistry& registry) {
+  TapeVSpace vspace(ctx);
+  auto tape = new Tape(/*persistent=*/false);
+  tape->Watch(ToId(inputs[0]));  // Watch x.
+  tape->Watch(ToId(inputs[1]));  // Watch y.
+  std::vector<AbstractTensorHandle*> bias_add_outputs(1);
+  AbstractContextPtr tape_ctx(new TapeContext(ctx, tape, registry));
+  TF_RETURN_IF_ERROR(ops::BiasAdd(
+      tape_ctx.get(), inputs, absl::MakeSpan(bias_add_outputs), "BiasAddGrad"));
+  std::unordered_map<tensorflow::int64, TapeTensor>
+      source_tensors_that_are_targets;
+
+  std::vector<AbstractTensorHandle*> out_grads;
+  TF_RETURN_IF_ERROR(tape->ComputeGradient(
+      vspace, /*target_tensor_ids=*/{ToId(bias_add_outputs[0])},
+      /*source_tensor_ids=*/{ToId(inputs[0]), ToId(inputs[1])},
+      source_tensors_that_are_targets,
+      /*output_gradients=*/{}, &out_grads,
+      /*build_default_zeros_grads=*/false));
+  for (auto bias_add_output : bias_add_outputs) {
+    bias_add_output->Unref();
+  }
+  outputs[0] = out_grads[0];
+  outputs[1] = out_grads[1];
+  delete tape;
+  return Status::OK();
+}
 
 class CppGradients
     : public ::testing::TestWithParam<std::tuple<const char*, bool, bool>> {
@@ -60,12 +91,10 @@ Status RegisterGradients(GradientRegistry* registry) {
   return Status::OK();
 }
 
-TEST_P(CppGradients, TestBiasAddGrad) {
-  if (std::get<0>(GetParam()) == "mlir" && std::get<2>(GetParam()) == 0) {
+TEST_P(CppGradients, TestBiasAddGradChecker) {
+  if (std::get<0>(GetParam()) == "mlir" && !std::get<2>(GetParam())) {
     GTEST_SKIP() << "SetAttrString has not been implemented yet.\n";
   }
-  std::unique_ptr<TF_Status, decltype(&TF_DeleteStatus)> status(
-      TF_NewStatus(), TF_DeleteStatus);
   AbstractContextPtr ctx;
   {
     AbstractContext* ctx_raw = nullptr;
@@ -75,64 +104,24 @@ TEST_P(CppGradients, TestBiasAddGrad) {
     ctx.reset(ctx_raw);
   }
 
+  // A
   float A_vals[] = {1.0f, 2.0f, 3.0f, 4.0f};
   int64_t A_dims[] = {2, 2};
-  float Bias_vals[] = {2.0f, 3.0f};
-  int64_t Bias_dims[] = {2};
-
   AbstractTensorHandlePtr A =
       GetTensorHandleUtilFloat(ctx.get(), A_vals, A_dims, 2);
-  AbstractTensorHandlePtr B =
+  // Bias
+  float Bias_vals[] = {2.0f, 3.0f};
+  int64_t Bias_dims[] = {2};
+  AbstractTensorHandlePtr Bias =
       GetTensorHandleUtilFloat(ctx.get(), Bias_vals, Bias_dims, 1);
 
   GradientRegistry registry;
   Status s = RegisterGradients(&registry);
   ASSERT_EQ(errors::OK, s.code()) << s.error_message();
 
-  /* Pseudo-code:
-   *
-   * tape.watch(A)
-   * tape.watch(B)
-   * Y = BiasAdd(A, Bias)
-   * outputs = tape.gradient(Y, [A, Bias])
-   */
-  std::vector<AbstractTensorHandle*> outputs(2);
-  s = RunModel(BiasAddGradModel, ctx.get(), {A.get(), B.get()},
-               absl::MakeSpan(outputs),
-               /*use_function=*/!std::get<2>(GetParam()), registry);
-  ASSERT_EQ(errors::OK, s.code()) << s.error_message();
-
-  TF_Tensor* dA_tensor;
-  s = GetValue(outputs[0], &dA_tensor);
-  ASSERT_EQ(errors::OK, s.code()) << s.error_message();
-
-  float result_data_dA[4] = {0};
-  memcpy(&result_data_dA[0], TF_TensorData(dA_tensor),
-         TF_TensorByteSize(dA_tensor));
-
-  float expected_dA[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-  float tolerance = 1e-3;
-  for (int j = 0; j < 4; j++) {
-    ASSERT_NEAR(result_data_dA[j], expected_dA[j], tolerance);
-  }
-
-  TF_Tensor* dBias_tensor;
-  s = GetValue(outputs[1], &dBias_tensor);
-  ASSERT_EQ(errors::OK, s.code()) << s.error_message();
-
-  float result_data_dBias[2] = {0};
-  memcpy(&result_data_dBias[0], TF_TensorData(dBias_tensor),
-         TF_TensorByteSize(dBias_tensor));
-
-  float expected_dBias[2] = {2.0f, 2.0f};
-  for (int j = 0; j < 2; j++) {
-    ASSERT_NEAR(result_data_dBias[j], expected_dBias[j], tolerance);
-  }
-
-  outputs[0]->Unref();
-  outputs[1]->Unref();
-  TF_DeleteTensor(dA_tensor);
-  TF_DeleteTensor(dBias_tensor);
+  ASSERT_NO_FATAL_FAILURE(CompareWithGradientsCheckers(
+      BiasAddModel, BiasAddGradModel, ctx.get(), {A.get(), Bias.get()},
+      /*use_function=*/!std::get<2>(GetParam()), registry));
 }
 
 #ifdef PLATFORM_GOOGLE
