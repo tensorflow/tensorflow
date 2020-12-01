41565eb1e4838298c99630f31106e626b8c492c5
Ken Franko <kfranko@google.com>
2020-12-01 10:26:51 -0800

    Enable TPUDevicePropagation pass.
    PiperOrigin-RevId: 345048858
    Change-Id: Ie6048e834fc8034b32895f212928fa7e6ceb93b8

--- tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
@@ -51,6 +51,7 @@ void AddGraphExportLoweringPasses(OpPassManager &pm) {
   add_pass(TFDevice::CreateReplicateToIslandPass());
   add_pass(TFDevice::CreateParallelExecuteToIslandsPass());
   add_pass(TFDevice::CreateLaunchToDeviceAttributePass());
+  pm.addNestedPass<FuncOp>(CreateTPUDevicePropagationPass());
   pm.addPass(createSymbolDCEPass());
 }
 
