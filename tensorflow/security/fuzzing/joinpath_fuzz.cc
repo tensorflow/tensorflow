5e0f09438215f013da726c31147806b87893f91e
Amit Patankar <amitpatankar@google.com>
2020-12-01 17:18:47 -0800

    Create a fuzzer for `JoinPath`.
    PiperOrigin-RevId: 345132114
    Change-Id: Ief54aa3e761dbcf89100c42587ee468886f0d5da

--- tensorflow/security/fuzzing/joinpath_fuzz.cc
+++ tensorflow/security/fuzzing/joinpath_fuzz.cc
@@ -12,33 +12,32 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <fuzzer/FuzzedDataProvider.h>
 
 #include <cstdint>
 #include <cstdlib>
 
 #include "absl/strings/match.h"
 #include "tensorflow/core/platform/path.h"
-#include "tensorflow/core/platform/stringpiece.h"
 
-// This is a fuzzer for tensorflow::io::ParseURI.
+// This is a fuzzer for tensorflow::io::JoinPath.
 
 namespace {
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  std::string uri(reinterpret_cast<const char *>(data), size);
-  tensorflow::StringPiece scheme, host, path;
-  tensorflow::io::ParseURI(uri, &scheme, &host, &path);
+  FuzzedDataProvider fuzzed_data(data, size);
 
-  // If a path is invalid.
-  if (path == uri) {
-    assert(host == "");
-    assert(scheme == "");
-  } else {
-    assert(absl::StrContains(uri, host));
-    assert(absl::StrContains(uri, scheme));
-    assert(absl::StrContains(uri, path));
-    assert(absl::StrContains(uri, "://"));
-  }
+  // Choose random numbers here.
+  const int content_size = fuzzed_data.ConsumeIntegralInRange(10, 300);
+
+  std::string first = fuzzed_data.ConsumeRandomLengthString(content_size);
+  std::string second = fuzzed_data.ConsumeRemainingBytesAsString();
+
+  std::string path = tensorflow::io::JoinPath(first, second);
+
+  // Assert path contains strings
+  assert(absl::StrContains(path, first));
+  assert(absl::StrContains(path, second));
 
   return 0;
 }
