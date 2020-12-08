719ef26d3215ed850eef43ac5b488d32f80ea604
Mihai Maruseac <mihaimaruseac@google.com>
2020-12-07 20:31:31 -0800

    Prevent CHECK-fail in LSTM/GRU with zero-length input.
    PiperOrigin-RevId: 346239181
    Change-Id: I5f233dbc076aab7bb4e31ba24f5abd4eaf99ea4f

--- tensorflow/stream_executor/cuda/cuda_dnn.cc
+++ tensorflow/stream_executor/cuda/cuda_dnn.cc
@@ -1379,7 +1379,9 @@ class CudnnRnnSequenceTensorDescriptor
   static port::StatusOr<CudnnRnnSequenceTensorDescriptor> Create(
       GpuExecutor* parent, int max_seq_length, int batch_size, int data_size,
       cudnnDataType_t data_type) {
-    CHECK_GT(max_seq_length, 0);
+    if (max_seq_length <= 0) {
+      return port::Status(port::error::INVALID_ARGUMENT, "max_seq_length <= 0");
+    }
     int dims[] = {batch_size, data_size, 1};
     int strides[] = {dims[1] * dims[2], dims[2], 1};
     TensorDescriptor tensor_desc = CreateTensorDescriptor();
@@ -1400,7 +1402,9 @@ class CudnnRnnSequenceTensorDescriptor
       const absl::Span<const int>& seq_lengths, bool time_major,
       cudnnDataType_t data_type) {
 #if CUDNN_VERSION >= 7201
-    CHECK_GT(max_seq_length, 0);
+    if (max_seq_length <= 0) {
+      return port::Status(port::error::INVALID_ARGUMENT, "max_seq_length <= 0");
+    }
     int dims[] = {batch_size, data_size, 1};
     int strides[] = {dims[1] * dims[2], dims[2], 1};
     TensorDescriptor tensor_desc = CreateTensorDescriptor();
