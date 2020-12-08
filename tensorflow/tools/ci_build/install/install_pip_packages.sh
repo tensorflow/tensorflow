c0dbdfb718de6481b00d7ff17a0ff763cf7a7544
bhack <bhack@users.noreply.github.com>
2020-12-08 19:05:35 +0000

    Revert "Revert PR #45380: Try to switch to h5py 3.1.0"
    This reverts commit d36db2955c460d2abe185ced9cc5bdfabc67b631.

--- tensorflow/tools/ci_build/install/install_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_pip_packages.sh
@@ -134,7 +134,7 @@ pip3 install --upgrade termcolor
 pip2 install keras_preprocessing==1.1.0 --no-deps
 pip3 install keras_preprocessing==1.1.0 --no-deps
 pip2 install --upgrade h5py==2.8.0
-pip3 install --upgrade h5py==2.8.0
+pip3 install --upgrade h5py==3.1.0
 
 # Estimator
 pip2 install tf-estimator-nightly --no-deps
