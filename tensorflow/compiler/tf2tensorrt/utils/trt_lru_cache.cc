f5c929ca8c65ae42cb449481adb25b4d994b8b5e
Tamas Bela Feher <tfeher@nvidia.com>
2020-12-11 00:38:55 +0100

    Add TF-TRT optimization profiles for testing
    Additionally, profile handling improved in dynamic shape mode:
    if profile generation mode was not used, then we create a a single
    implicit batch like profile using the current input shape.

--- tensorflow/compiler/tf2tensorrt/utils/trt_lru_cache.cc
+++ tensorflow/compiler/tf2tensorrt/utils/trt_lru_cache.cc
@@ -120,7 +120,7 @@ EngineContext* TRTEngineCacheResource::GetEngineContext(
 }
 
 EngineContext* TRTEngineCacheResource::GetEngineContext(const int profile_id) {
-  if (profile_id >= profiles_.GetNumProfiles()) {
+  if (profiles_.NeedProfiles() && profile_id >= profiles_.GetNumProfiles()) {
     LOG(ERROR) << "Out of range: profile_id " << profile_id
                << " is larger than number of profiles "
                << profiles_.GetNumProfiles();
