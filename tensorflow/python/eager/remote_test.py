4867489c447466847703db0b0ece296506016c90
Yujing Zhang <yujingzhang@google.com>
2020-12-11 14:49:21 -0800

    Fix an issue of out of order execution. For a multi-device function, don't send a packed input to the function device until all underlying remote handles are ready on remote devices. Otherwise, on a remote worker, a remote component function execution request could be enqueued before a request for producing a function input.
    PiperOrigin-RevId: 347079883
    Change-Id: I7929f27539890ce6e55c09d1ef79847101187b28

--- tensorflow/python/eager/remote_test.py
+++ tensorflow/python/eager/remote_test.py
@@ -390,6 +390,30 @@ class MultiWorkersTest(test.TestCase, parameterized.TestCase):
 
     self.assertAllEqual(remote_function(constant_op.constant([1.0])), [3.0])
 
+  def testMultiDeviceFunctionExecutionOrderingWithPackedInput(self):
+    shape = [2]
+    with ops.device('/job:worker/replica:0/task:2/device:CPU:0'):
+      # Send 20 remote requests to simulate heavy load on worker:2.
+      unused_values = []
+      for _ in range(20):
+        unused_values.append(array_ops.zeros(shape))
+      func_input = array_ops.zeros(shape)
+
+    packed_input = ops.pack_eager_tensors([func_input])
+
+    @def_function.function
+    def func(packed_input):
+      # When worker:2 receives the component function request, packed_input
+      # should be ready on worker:2.
+      with ops.device('/job:worker/replica:0/task:2/device:CPU:0'):
+        ret = packed_input + constant_op.constant(1.0)
+      return ret + constant_op.constant(1.0)
+
+    # Run the function on a worker:1
+    with ops.device('/job:worker/replica:0/task:1/device:CPU:0'):
+      self.assertAllEqual(func(packed_input).numpy(),
+                          array_ops.ones(shape).numpy() * 2)
+
   def testMultiDeviceFunctionWithPackedVariable(self):
     with ops.device('/job:worker/replica:0/task:0/device:CPU:0'):
       var0 = resource_variable_ops.ResourceVariable(1.0)
