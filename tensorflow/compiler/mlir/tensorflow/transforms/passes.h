d90ce10b0cd9b2b3213193eb957364bd9e46944c
Dong Lin <donglin@google.com>
2020-12-16 15:20:47 -0800

    [TFRT:Compiler] Add the tf-cross-host-transfer compiler pass
    This pass inserts tf_device.send and tf_device.receive ops to make sure any argument of any op is on the same host of the op itself. It allows the later compiler pass not to worry about transferring values between hosts.
    
    PiperOrigin-RevId: 347908086
    Change-Id: Iaf2bec1919d36eb9a913e488862432bc113ea15b

--- tensorflow/compiler/mlir/tensorflow/transforms/passes.h
+++ tensorflow/compiler/mlir/tensorflow/transforms/passes.h
@@ -199,6 +199,10 @@ std::unique_ptr<OperationPass<FuncOp>> CreateInitTextFileToImportPass();
 // assignment of the result.
 std::unique_ptr<FunctionPass> CreateClusterTFOpsByHostPass();
 
+// Creates function pass to insert tf_device.send and tf_device.receive ops to
+// make sure any argument of any op is on the same host of the op itself.
+std::unique_ptr<FunctionPass> CreateCrossHostTransferPass();
+
 // Creates a pass that adds the device attribute to every tf.Const op based on
 // the device attribute of the operations that read its result. If the result of
 // a tf.Const op is read by operations placed on multiple devices, then the pass
