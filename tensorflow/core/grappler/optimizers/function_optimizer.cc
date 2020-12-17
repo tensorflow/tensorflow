e38610399a1293e5f8f1b1c290c355149bed1418
Ayush Dubey <ayushd@google.com>
2020-12-17 10:40:16 -0800

    Add CollectiveBcastSend/RecvV2 which takes input tensors rather than attributes.
    CollectiveBcastSend/Recv accepts the following inputs as attributes on the op:
    group_size, group_key, and instance_key.  Attributes imply these values are
    embedded in the NodeDef.
    
    The use case motivating this change is a compact representation for SPMD
    computation.  The goal is to change those inputs from the collective op which
    can be accepted during runtime to tensors rather than attributes.  This enables
    the graph builder to avoid early explosion of the SPMD program.
    
    This op is not exposed in the `tf.` namespace for now, and should be considered
    experimental.
    
    PiperOrigin-RevId: 348049802
    Change-Id: I15ffcb562fb75cc64e5970ac7795cb81ff187fd8

--- tensorflow/core/grappler/optimizers/function_optimizer.cc
+++ tensorflow/core/grappler/optimizers/function_optimizer.cc
@@ -830,7 +830,8 @@ const bool IsExemptFromSideEffectsExecutionValidation(const string& op) {
        // to run asynchronously to avoid deadlock.
        "CollectiveGather", "CollectiveGatherV2", "CollectiveReduce",
        "CollectiveReduceV2", "CollectiveBcastSend", "CollectiveBcastRecv",
-       "NcclAllReduce", "Send", "Recv",
+       "CollectiveBcastSendV2", "CollectiveBcastRecvV2", "NcclAllReduce",
+       "Send", "Recv",
 
        // Legacy random ops.
        // See details in tensorflow/python/framework/auto_control_deps.py.
