4a934b575a8fdb58b8016aae40e59a5ff5421079
Ryan Kuester <kuester@bdti.com>
2020-12-16 21:59:14 -0600

    Extract a function for parsing op SPACE_TO_BATCH_ND
    Extract the parsing out of a switch statement case to create a
    standalone function which can be called by the micro op resolver.

--- tensorflow/lite/core/api/flatbuffer_conversions.cc
+++ tensorflow/lite/core/api/flatbuffer_conversions.cc
@@ -374,6 +374,10 @@ TfLiteStatus ParseOpDataTfLite(const Operator* op, BuiltinOperator op_type,
       return ParseSoftmax(op, error_reporter, allocator, builtin_data);
     }
 
+    case BuiltinOperator_SPACE_TO_BATCH_ND: {
+      return ParseSpaceToBatchNd(op, error_reporter, allocator, builtin_data);
+    }
+
     case BuiltinOperator_SPLIT: {
       return ParseSplit(op, error_reporter, allocator, builtin_data);
     }
@@ -805,7 +809,6 @@ TfLiteStatus ParseOpDataTfLite(const Operator* op, BuiltinOperator op_type,
     case BuiltinOperator_SELECT:
     case BuiltinOperator_SELECT_V2:
     case BuiltinOperator_SLICE:
-    case BuiltinOperator_SPACE_TO_BATCH_ND:
     case BuiltinOperator_TILE:
     case BuiltinOperator_TOPK_V2:
     case BuiltinOperator_TRANSPOSE:
@@ -1634,6 +1637,14 @@ TfLiteStatus ParseSoftmax(const Operator* op, ErrorReporter* error_reporter,
   }
 
   *builtin_data = params.release();
+  return kTfLiteOk;
+}
+
+// We have this parse function instead of directly returning kTfLiteOk from the
+// switch-case in ParseOpData because this function is used as part of the
+// selective registration for the OpResolver implementation in micro.
+TfLiteStatus ParseSpaceToBatchNd(const Operator*, ErrorReporter*,
+                                 BuiltinDataAllocator*, void**) {
   return kTfLiteOk;
 }
 
