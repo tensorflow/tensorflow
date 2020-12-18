2637d4e98676229306ea343695c704d0d48d700a
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2020-12-18 21:55:59 +0000

    Unit test fixes

--- tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
+++ tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
@@ -1113,7 +1113,7 @@ Status IrEmitterUnnested::EmitConvolutionThunkFromMlir(MlirEmitterInput input) {
     descriptor.result_shape = apply_layout(TypeToShape(conv_result.getType()),
                                            op.backend_config().result_layout());
     descriptor.dnums = ConvertConvDimensionNumbers(op.dimension_numbers());
-    descriptor.scratch_size = 0; // TODO: figure out how to get the value
+    descriptor.scratch_size = input.extra_slice->shape.tuple_shapes(1).dimensions(0);
     mlir::DenseIntElementsAttr window_strides = op.window_strides().getValue();
     mlir::DenseIntElementsAttr padding = op.padding().getValue();
     mlir::DenseIntElementsAttr lhs_dilation = op.lhs_dilation().getValue();
