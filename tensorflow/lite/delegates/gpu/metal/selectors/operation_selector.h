60325944ed40840964b85802fd9ec3e349bc5433
Raman Sarokin <sorokin@google.com>
2020-12-21 20:56:32 -0800

    Added operation_selector for Metal backend. Making it similar to gpu/common/ structure.
    PiperOrigin-RevId: 348573563
    Change-Id: I993f124c4d3fac14c397495aaf131e6614c5317f

--- tensorflow/lite/delegates/gpu/metal/selectors/operation_selector.h
+++ tensorflow/lite/delegates/gpu/metal/selectors/operation_selector.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,29 +13,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_LITE_DELEGATES_GPU_METAL_KERNELS_CUSTOM_REGISTRY_H_
-#define TENSORFLOW_LITE_DELEGATES_GPU_METAL_KERNELS_CUSTOM_REGISTRY_H_
+#ifndef TENSORFLOW_LITE_DELEGATES_GPU_METAL_SELECTORS_OPERATION_SELECTOR_H_
+#define TENSORFLOW_LITE_DELEGATES_GPU_METAL_SELECTORS_OPERATION_SELECTOR_H_
 
-#include <vector>
+#include <memory>
 
 #include "tensorflow/lite/delegates/gpu/common/model.h"
-#include "tensorflow/lite/delegates/gpu/common/precision.h"
 #include "tensorflow/lite/delegates/gpu/common/status.h"
-#include "tensorflow/lite/delegates/gpu/metal/compute_task_descriptor.h"
+#include "tensorflow/lite/delegates/gpu/metal/selectors/subgraph.h"
 
 namespace tflite {
 namespace gpu {
 namespace metal {
 
-// Registers custom operations.
-absl::Status RegisterCustomOps(const GraphFloat32& graph, const Node* node,
-                               const std::vector<ValueId>& inputs,
-                               const std::vector<ValueId>& outputs,
-                               CalculationsPrecision precision,
-                               std::vector<ComputeTaskDescriptorPtr>* tasks);
-
+absl::Status GPUOperationFromNode(const GpuInfo& gpu_info,
+                                  const OperationDef& op_def,
+                                  const std::vector<Value*>& inputs,
+                                  const std::vector<Value*>& outputs,
+                                  const Node& node,
+                                  GPUOperationsSubgraph* gpu_subgraph);
 }  // namespace metal
 }  // namespace gpu
 }  // namespace tflite
 
-#endif  // TENSORFLOW_LITE_DELEGATES_GPU_METAL_KERNELS_CUSTOM_REGISTRY_H_
+#endif  // TENSORFLOW_LITE_DELEGATES_GPU_METAL_SELECTORS_OPERATION_SELECTOR_H_
