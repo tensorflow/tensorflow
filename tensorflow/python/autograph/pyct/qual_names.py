e31d31954d89d67a9623a2b9e3c7cacad3cb2efe
bhack <bhack@users.noreply.github.com>
2020-12-29 13:38:38 +0000

    Try some fixes to slice idx

--- tensorflow/python/autograph/pyct/qual_names.py
+++ tensorflow/python/autograph/pyct/qual_names.py
@@ -24,7 +24,6 @@ refers to scopes.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from sys import version_info
 
 import collections
 
