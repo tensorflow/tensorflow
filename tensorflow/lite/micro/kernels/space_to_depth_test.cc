0c0457302f4789beef0cd755ac4ab80b37f37459
Ryan Kuester <kuester@bdti.com>
2021-01-04 15:27:41 -0600

    micro: clang-format copy of SPACE_TO_DEPTH
    clang-format the copy of SPACE_TO_DEPTH added in the previous
    commit so that it passes CI checks, even though the files are not
    yet included in the build.

--- tensorflow/lite/micro/kernels/space_to_depth_test.cc
+++ tensorflow/lite/micro/kernels/space_to_depth_test.cc
@@ -12,12 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <gtest/gtest.h>
 #include <stdint.h>
 
 #include <initializer_list>
 #include <vector>
 
-#include <gtest/gtest.h>
 #include "flatbuffers/flatbuffers.h"  // from @flatbuffers
 #include "tensorflow/lite/kernels/test_util.h"
 #include "tensorflow/lite/schema/schema_generated.h"
