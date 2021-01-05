8fb788827ff397fddfb758f41e907f547f7e31bd
rsanthanam-amd <70280935+rsanthanam-amd@users.noreply.github.com>
2021-01-05 07:48:55 -0600

    Update relu_op_test.py

--- tensorflow/python/kernel_tests/relu_op_test.py
+++ tensorflow/python/kernel_tests/relu_op_test.py
@@ -505,7 +505,8 @@ class GeluTest(test.TestCase):
     if not test.is_gpu_available():
       self.skipTest("No GPU available")
     for t in [np.float16, np.float32, np.float64]:
-      self._testGelu(np.array([[-9, 7, -5, 3, -1], [1, -3, 5, -7, 9]]).astype(t))
+      self._testGelu(np.array([[-9, 7, -5, 3, -1],
+                               [1, -3, 5, -7, 9]]).astype(t))
 
   def testGradients(self):
     for t in [np.float16, np.float32, np.float64]:
