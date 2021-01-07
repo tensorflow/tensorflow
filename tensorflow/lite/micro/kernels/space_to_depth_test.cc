523f789ecdd129b1edc3525b9f36e9d4a1c278ec
Ryan Kuester <kuester@bdti.com>
2021-01-07 15:21:02 -0600

    micro: delete gmock/gtest includes in copy of SPACE_TO_DEPTH
    Delete the gmock and gtest includes in the currently inactive
    copy of op SPACE_TO_DEPTH. There is an annoying difference
    between internal and external versions of the code that causes
    clang-formatting errors in CI checks. When the code is imported
    internally, the gmock and gtest includes become:
    
        #include "/internal/path/to/gmock/gmock.h"
    
    and then clang-format wants them to be in the lower include block
    (i.e., next to "flatbuffers/flatbuffers.h").
    
    The solution for now is to delete gmock and gtest includes when
    making these operator copies.

--- tensorflow/lite/micro/kernels/space_to_depth_test.cc
+++ tensorflow/lite/micro/kernels/space_to_depth_test.cc
@@ -12,7 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <gtest/gtest.h>
 #include <stdint.h>
 
 #include <initializer_list>
