0a342ee36b2627c2ce5d3d1bb4103191dd8d7711
Ryan Kuester <kuester@bdti.com>
2021-01-07 15:21:02 -0600

    micro: delete gmock/gtest includes in copy of FILL
    Delete the gmock and gtest includes in the currently inactive
    copy of op FILL. There is an annoying difference between internal
    and external versions of the code that causes clang-formatting
    errors in CI checks (see issue #46297). When the code is imported
    internally, the gmock and gtest includes become:
    
        #include "/internal/path/to/gmock/gmock.h"
    
    and then clang-format wants them to be in the lower include block
    (i.e., next to "flatbuffers/flatbuffers.h").
    
    The solution for now is to delete gmock and gtest includes when
    making these operator copies.

--- tensorflow/lite/micro/kernels/fill_test.cc
+++ tensorflow/lite/micro/kernels/fill_test.cc
@@ -12,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 #include <stdint.h>
 
 #include <initializer_list>
