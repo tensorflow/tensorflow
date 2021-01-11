d31fab2d57453403900c296f08de6c2c65cbf282
Måns Nilsson <mans.nilsson@arm.com>
2021-01-11 09:44:24 +0100

    TFLM: Set new GCC version to default

--- tensorflow/lite/micro/tools/make/arm_gcc_download.sh
+++ tensorflow/lite/micro/tools/make/arm_gcc_download.sh
@@ -49,42 +49,25 @@ if [ -d ${DOWNLOADED_GCC_PATH} ]; then
   echo >&2 "${DOWNLOADED_GCC_PATH} already exists, skipping the download."
 else
 
-  if [[ ${OSTYPE} == "linux" ]]; then
+  UNAME_S=`uname -s`
+  if [ ${UNAME_S} == Linux ]; then
     GCC_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2"
     EXPECTED_MD5="8312c4c91799885f222f663fc81f9a31"
-  elif [[ ${HOST_OS} == "darwin" ]]; then
+  elif [ ${UNAME_S} == Darwin ]; then
     GCC_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2"
     EXPECTED_MD5="bc8ae26d7c429f30d583a605a4bcf9bc"
   else
-    GCC_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-win32.zip"
-    EXPECTED_MD5="a66be9828cf3c57d7d21178e07cd8904"
+    echo "OS type ${UNAME_S} not supported."
+    exit 1
   fi
 
-  FILE_EXTENSION="${GCC_URL##*.}"
   TEMPFILE=$(mktemp -d)/temp_file
-
   wget ${GCC_URL} -O ${TEMPFILE} >&2
   check_md5 ${TEMPFILE} ${EXPECTED_MD5}
 
-  mkdir "${DOWNLOADED_GCC_PATH}"
-  if [[ ${FILE_EXTENSION} == "bz2" ]]; then
-    tar -C "${DOWNLOADED_GCC_PATH}" --strip-components=1 -xjf ${TEMPFILE}
-  elif [[ ${FILE_EXTENSION} == "zip" ]]; then
-    TEMPDIR=$(mktemp -d)
-    unzip ${TEMPFILE} -d ${TEMPDIR} 2>&1 1>/dev/null
-    # If the zip file contains nested directories, extract the files from the
-    # inner directory.
-    if [ $(find $TEMPDIR/* -maxdepth 0 | wc -l) = 1 ] && [ -d $TEMPDIR/* ]; then
-      # unzip has no strip components, so unzip to a temp dir, and move the
-      # files we want from the tempdir to destination.
-      cp -R ${TEMPDIR}/*/* ${dir}/
-    else
-      cp -R ${TEMPDIR}/* ${dir}/
-    fi
-  else
-    echo "Error unsupported archive type. Failed to extract tool after download."
-    exit 1
-  fi
+  mkdir ${DOWNLOADED_GCC_PATH}
+  tar -C ${DOWNLOADED_GCC_PATH} --strip-components=1 -xjf ${TEMPFILE} >&2
+  echo >&2 "Unpacked to directory: ${DOWNLOADED_GCC_PATH}"
 fi
 
 echo "SUCCESS"
