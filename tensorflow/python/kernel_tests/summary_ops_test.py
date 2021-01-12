7ed592e8128d9e964a628ccdbfd5730f77920b2f
geetachavan1 <53313357+geetachavan1@users.noreply.github.com>
2021-01-12 11:37:49 -0800

    Update summary_ops_test.py

--- tensorflow/python/kernel_tests/summary_ops_test.py
+++ tensorflow/python/kernel_tests/summary_ops_test.py
@@ -1261,36 +1261,6 @@ class SummaryOpsTest(test_util.TensorFlowTestCase):
     def summary_op_fn():
       summary_ops.graph('hello')
 
-    with self.assertRaisesRegex(
-        ValueError,
-        r'\'graph_data\' is not tf.Graph or tf.compat.v1.GraphDef',
-    ):
-      self.exec_summary_op(summary_op_fn)
-
-  @test_util.run_v2_only
-  def testGraph_fromGraphMode(self):
-
-    @def_function.function
-    def f():
-      x = constant_op.constant(2)
-      y = constant_op.constant(3)
-      return x**y
-
-    @def_function.function
-    def g(graph):
-      summary_ops.graph(graph)
-
-    def summary_op_fn():
-      graph_def = f.get_concrete_function().graph.as_graph_def(add_shapes=True)
-      func_graph = constant_op.constant(graph_def.SerializeToString())
-      g(func_graph)
-
-    with self.assertRaisesRegex(
-        ValueError,
-        r'graph\(\) cannot be invoked inside a graph context.',
-    ):
-      self.exec_summary_op(summary_op_fn)
-
 
 def events_from_file(filepath):
   """Returns all events in a single event file.
