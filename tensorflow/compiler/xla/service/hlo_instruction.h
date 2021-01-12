6a1b5968b63a5ec94f1816ec855a996508f97959
Andy Ly <lyandy@google.com>
2021-01-12 12:22:08 -0800

    Add OpMetadata list to OpShardingProto/HloSharding.
    Extending OpShardingProto/HloSharding to have a repeated OpMetadata field will allow for tracking the source(s) of a sharding when being assigned to an instruction or propagated. OpMetadata is reused to allow for flexibility and reuse of tracking of instructions (e.g. op name, op type, file and line, etc.). This only adds the field and updates the printer and parser to support the addition of this field.
    
    For non tuple sharding, OpMetadata is added as an attribute to the sharding
    e.g.
    {replicated metadata={op_name="a"}}
    
    Multiple metadata can be associated to a sharding
    e.g.
    {replicated metadata={{op_name="e"}, {op_name="f"}}}
    
    Metadata on tuple sharding is unsupported in favor of metadata on tuple elements.
    
    PiperOrigin-RevId: 351423487
    Change-Id: I69a0a3c40e29e90ce085726daf757c7456153ab2

--- tensorflow/compiler/xla/service/hlo_instruction.h
+++ tensorflow/compiler/xla/service/hlo_instruction.h
@@ -2165,7 +2165,6 @@ StatusOr<HloInstruction::FusionKind> StringToFusionKind(
 string PaddingConfigToString(const PaddingConfig& padding);
 string FrontendAttributesToString(
     const FrontendAttributes& frontend_attributes);
-string OpMetadataToString(const OpMetadata& metadata);
 string RandomAlgorithmToString(const RandomAlgorithm& algorithm);
 string RandomDistributionToString(const RandomDistribution& distribution);
 string PrecisionToString(const PrecisionConfig::Precision& precision);
