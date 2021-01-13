4ef2cd7c1f4d887e769f6c8f19ba6b111d2068f1
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-01-13 20:06:51 +0700

    add `TestNegGrad` to `math_grad_test`

--- tensorflow/c/experimental/gradients/math_grad_test.cc
+++ tensorflow/c/experimental/gradients/math_grad_test.cc
@@ -115,6 +115,34 @@ Status SqrtGradModel(AbstractContext* ctx,
   return Status::OK();
 }
 
+Status NegModel(AbstractContext* ctx,
+                absl::Span<AbstractTensorHandle* const> inputs,
+                absl::Span<AbstractTensorHandle*> outputs) {
+  return ops::Neg(ctx, inputs, outputs, "Neg");
+}
+
+Status NegGradModel(AbstractContext* ctx,
+                    absl::Span<AbstractTensorHandle* const> inputs,
+                    absl::Span<AbstractTensorHandle*> outputs) {
+  GradientRegistry registry;
+  TF_RETURN_IF_ERROR(registry.Register("Neg", NegRegisterer));
+
+  Tape tape(/*persistent=*/false);
+  tape.Watch(inputs[0]);
+  std::vector<AbstractTensorHandle*> temp_outputs(1);
+  AbstractContextPtr tape_ctx(new TapeContext(ctx, &tape, registry));
+  TF_RETURN_IF_ERROR(ops::Neg(tape_ctx.get(), inputs,
+                              absl::MakeSpan(temp_outputs), "NegGrad"));
+
+  TF_RETURN_IF_ERROR(tape.ComputeGradient(ctx, /*targets=*/temp_outputs,
+                                          /*sources=*/inputs,
+                                          /*output_gradients=*/{}, outputs));
+  for (auto temp_output : temp_outputs) {
+    temp_output->Unref();
+  }
+  return Status::OK();
+}
+
 class CppGradients
     : public ::testing::TestWithParam<std::tuple<const char*, bool, bool>> {
  protected:
@@ -185,6 +213,19 @@ TEST_P(CppGradients, TestSqrtGrad) {
 
   ASSERT_NO_FATAL_FAILURE(CompareNumericalAndAutodiffGradients(
       SqrtModel, SqrtGradModel, ctx_.get(), {x.get()}, UseFunction()));
+}
+
+TEST_P(CppGradients, TestNegGrad) {
+  AbstractTensorHandlePtr x;
+  {
+    AbstractTensorHandle* x_raw = nullptr;
+    Status s = TestScalarTensorHandle(ctx_.get(), 2.0f, &x_raw);
+    ASSERT_EQ(errors::OK, s.code()) << s.error_message();
+    x.reset(x_raw);
+  }
+
+  ASSERT_NO_FATAL_FAILURE(CompareNumericalAndAutodiffGradients(
+      NegModel, NegGradModel, ctx_.get(), {x.get()}, UseFunction()));
 }
 
 #ifdef PLATFORM_GOOGLE
