2180118ff2601aaa437930e1b37e2d16d802de72
Mihai Maruseac <mihai.maruseac@gmail.com>
2021-01-22 10:09:26 -0800

    Apply suggestions from code review

--- tensorflow/c/experimental/filesystem/filesystem_interface.h
+++ tensorflow/c/experimental/filesystem/filesystem_interface.h
@@ -902,7 +902,7 @@ typedef struct TF_FilesystemOps {
   ///
   /// Plugins:
   ///   * Must set `status` to `TF_OK` on success. If there are no configurable
-  ///   keys, `num_keys` should be set to 0
+  ///     keys, `num_keys` should be set to 0
   ///   * Might use any other error value for `status` to signal other errors.
   ///
   /// DEFAULT IMPLEMENTATION: return `TF_OK` and `num_keys`=0.
