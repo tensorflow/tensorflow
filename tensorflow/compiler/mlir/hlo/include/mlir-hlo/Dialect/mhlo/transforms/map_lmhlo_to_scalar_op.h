5933c337d9e86e6725849cd3d6058253fb229412
Benjamin Kramer <kramerb@google.com>
2021-01-25 05:14:30 -0800

    [mlir][hlo] Make min/max always propagate NaNs
    This is the right behavior for TF and JAX and matches what TF does on GPU. It
    doesn't match TF on CPU, but that's really a TF bug.
    
    PiperOrigin-RevId: 353624935
    Change-Id: Ic54e718a45e412db031fd1b3772d135ac46ed8a1

--- tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/map_lmhlo_to_scalar_op.h
+++ tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/map_lmhlo_to_scalar_op.h
@@ -437,6 +437,20 @@ inline Value MapLhloOpToStdScalarOp<lmhlo::LogOp>(Location loc,
       loc, result_types, args, b);
 }
 
+inline Value LhloAlwaysPropagateNaN(Value v, ArrayRef<Value> args, Location loc,
+                                    OpBuilder* b) {
+  Type element_type = getElementTypeOrSelf(args.front().getType());
+  if (auto float_type = element_type.dyn_cast<FloatType>()) {
+    Value isnan =
+        b->create<mlir::CmpFOp>(loc, CmpFPredicate::UNO, args[0], args[1]);
+
+    auto nan_apfloat = APFloat::getQNaN(float_type.getFloatSemantics());
+    Value nan = b->create<mlir::ConstantFloatOp>(loc, nan_apfloat, float_type);
+    v = b->create<mlir::SelectOp>(loc, isnan, nan, v);
+  }
+  return v;
+}
+
 template <>
 inline Value MapLhloOpToStdScalarOp<lmhlo::LogisticOp>(
     Location loc, ArrayRef<Type> result_types, ArrayRef<Value> args,
@@ -464,10 +478,13 @@ inline Value MapLhloOpToStdScalarOp<lmhlo::MaxOp>(Location loc,
                                                   ArrayRef<Type> result_types,
                                                   ArrayRef<Value> args,
                                                   OpBuilder* b) {
-  return CompareSelectOpToStdScalarOp<
-      IntegerType, ScalarIOp<lmhlo::CompareOp>, CmpIPredicate, FloatType,
-      ScalarFOp<lmhlo::CompareOp>, CmpFPredicate>::map(loc, "GT", result_types,
-                                                       args, b);
+  return LhloAlwaysPropagateNaN(
+      CompareSelectOpToStdScalarOp<
+          IntegerType, ScalarIOp<lmhlo::CompareOp>, CmpIPredicate, FloatType,
+          ScalarFOp<lmhlo::CompareOp>, CmpFPredicate>::map(loc, "GT",
+                                                           result_types, args,
+                                                           b),
+      args, loc, b);
 }
 
 template <>
@@ -475,10 +492,13 @@ inline Value MapLhloOpToStdScalarOp<lmhlo::MinOp>(Location loc,
                                                   ArrayRef<Type> result_types,
                                                   ArrayRef<Value> args,
                                                   OpBuilder* b) {
-  return CompareSelectOpToStdScalarOp<
-      IntegerType, ScalarIOp<lmhlo::CompareOp>, CmpIPredicate, FloatType,
-      ScalarFOp<lmhlo::CompareOp>, CmpFPredicate>::map(loc, "LT", result_types,
-                                                       args, b);
+  return LhloAlwaysPropagateNaN(
+      CompareSelectOpToStdScalarOp<
+          IntegerType, ScalarIOp<lmhlo::CompareOp>, CmpIPredicate, FloatType,
+          ScalarFOp<lmhlo::CompareOp>, CmpFPredicate>::map(loc, "LT",
+                                                           result_types, args,
+                                                           b),
+      args, loc, b);
 }
 
 template <>
