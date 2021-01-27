5cac10d18289b10d08894935d2a1f6fc5410126f
George Karpenkov <cheshire@google.com>
2021-01-26 16:48:08 -0800

    Extend basic binary operations to more integral types
    PiperOrigin-RevId: 353980576
    Change-Id: Id108476c346b29136dc54b262e4a3eccac339753

--- tensorflow/core/kernels/cwise_op_pow.cc
+++ tensorflow/core/kernels/cwise_op_pow.cc
@@ -18,7 +18,7 @@ limitations under the License.
 namespace tensorflow {
 REGISTER6(BinaryOp, CPU, "Pow", functor::pow, float, Eigen::half, bfloat16,
           double, complex64, complex128);
-REGISTER2(BinaryOp, CPU, "Pow", functor::safe_pow, int32, int64);
+REGISTER4(BinaryOp, CPU, "Pow", functor::safe_pow, int8, int16, int32, int64);
 
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 #if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
