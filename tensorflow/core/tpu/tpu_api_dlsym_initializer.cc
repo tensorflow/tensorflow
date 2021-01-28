8bc0c84e45f1e172bfa81e9cc5fd4302a7d362e9
Skye Wanderman-Milne <skyewm@google.com>
2021-01-28 14:35:03 -0800

    [libtpu] Make copies of data returned by getenv.
    According to http://www.cplusplus.com/reference/cstdlib/getenv/:
    The pointer returned points to an internal memory block, whose content
    or validity may be altered by further calls to getenv (but not by
    other library functions).
    
    This appears to have been working without the defensive copies, but we
    should follow the spec just in case.
    
    This change also refactors TfTpu_Initialize to not use the argc/argv
    convention for simplicity (the implementation doesn't actually require
    a nullptr at the end of argv).
    
    PiperOrigin-RevId: 354396268
    Change-Id: Ie14f13d4a67e1012dc13af63df6a9a4fcb22feeb

--- tensorflow/core/tpu/tpu_api_dlsym_initializer.cc
+++ tensorflow/core/tpu/tpu_api_dlsym_initializer.cc
@@ -45,17 +45,18 @@ Status InitializeTpuLibrary(void* library_handle) {
   Status s = InitializeTpuStructFns(library_handle);
 
   // Retrieve arguments from environment if applicable
-  std::vector<const char*> argv_ptr = GetLibTpuInitArguments();
+  std::pair<std::vector<std::string>, std::vector<const char*> > args =
+      GetLibTpuInitArguments();
 
   // TPU platform registration must only be performed after the library is
   // loaded. We do not want to register a TPU platform in XLA without the
   // supporting library providing the necessary APIs.
   if (s.ok()) {
-    void (*initialize_fn)(bool init_library, int argc, const char** argv);
+    void (*initialize_fn)(bool init_library, int num_args, const char** args);
     initialize_fn = reinterpret_cast<decltype(initialize_fn)>(
         dlsym(library_handle, "TfTpu_Initialize"));
-    (*initialize_fn)(/*init_library=*/true, /*argc=*/argv_ptr.size() - 1,
-                     /*argv=*/argv_ptr.data());
+    (*initialize_fn)(/*init_library=*/true, args.second.size(),
+                     args.second.data());
 
     RegisterTpuPlatform();
   }
