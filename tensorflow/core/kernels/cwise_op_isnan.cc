23b10298680cc028098aed16d56567e2aac3e6d9
Stephan Herhut <herhut@google.com>
2021-02-02 01:10:10 -0800

    Enable generated versions of is_inf, is_nan and is_finite.
    PiperOrigin-RevId: 355115941
    Change-Id: I3fa22fac95945e651101dfb511abd0d2ba7b781e

--- tensorflow/core/kernels/cwise_op_isnan.cc
+++ tensorflow/core/kernels/cwise_op_isnan.cc
@@ -20,8 +20,7 @@ REGISTER4(UnaryOp, CPU, "IsNan", functor::isnan, float, Eigen::half, double,
           bfloat16);
 
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
-#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
-    !defined(MLIR_GENERATED_EXPERIMENTAL_GPU_KERNELS_ENABLED)
+#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED)
 REGISTER3(UnaryOp, GPU, "IsNan", functor::isnan, float, Eigen::half, double);
 #endif
 #endif
