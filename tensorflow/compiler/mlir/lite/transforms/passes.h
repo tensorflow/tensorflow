c52943fe64832c6f85b352f7fcbe9541c2ecd6ea
Karim Nosir <karimnosseir@google.com>
2021-02-02 20:30:47 -0800

    [lite] Add initialize variables pass in MLIR. Which initializes TF variables as TFL variables in session initialize function.
    PiperOrigin-RevId: 355311546
    Change-Id: I65eef7f54517a693fc3cee3c774580cf597b45a8

--- tensorflow/compiler/mlir/lite/transforms/passes.h
+++ tensorflow/compiler/mlir/lite/transforms/passes.h
@@ -107,6 +107,10 @@ std::unique_ptr<OperationPass<ModuleOp>> CreateLegalizeVariablesPass();
 // Creates a pass which removes any unused bounded input arguments to functions
 // which corresponds to GlobalTensor.
 std::unique_ptr<OperationPass<ModuleOp>> CreateRemoveArgsAndGlobalTensors();
+
+// Creates a pass which is responsible for initializing Tensorflow variables
+// as Tensorflow Lite variables.
+std::unique_ptr<OperationPass<ModuleOp>> CreateInitializeVariablesPass();
 }  // namespace TFL
 
 }  // namespace mlir
