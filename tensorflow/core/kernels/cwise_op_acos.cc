15c5c8362e90442340ff7a34f4bdaae267b5731c
Stephan Herhut <herhut@google.com>
2021-02-04 08:33:42 -0800

    Enable the generated version of acos and acosh.
    PiperOrigin-RevId: 355633110
    Change-Id: Ib08bd2d465000cb314a51718106886e79d187d6e

--- tensorflow/core/kernels/cwise_op_acos.cc
+++ tensorflow/core/kernels/cwise_op_acos.cc
@@ -19,8 +19,7 @@ namespace tensorflow {
 REGISTER2(UnaryOp, CPU, "Acos", functor::acos, float, double);
 
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
-#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
-    !defined(MLIR_GENERATED_EXPERIMENTAL_KERNELS_ENABLED)
+#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED)
 REGISTER2(UnaryOp, GPU, "Acos", functor::acos, float, double);
 #endif
 #endif
