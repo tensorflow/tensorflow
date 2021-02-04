57db297c71026f6c388484fbcfc391e426062831
David Rim <davidrim@google.com>
2021-02-03 22:28:09 -0800

    Quantize abs uses same input and output scale for compatibility with mlir.
    PiperOrigin-RevId: 355556020
    Change-Id: I29a2cbc5f3cfb45721907354287b086703a0314b

--- tensorflow/lite/tools/optimize/operator_property.cc
+++ tensorflow/lite/tools/optimize/operator_property.cc
@@ -66,6 +66,11 @@ OperatorProperty GetOperatorProperty(OpVariant op_variant) {
   OperatorProperty property;
   switch (op_code) {
     case BuiltinOperator_ABS:
+      property.inputs = {{0, {}}};
+      property.outputs = {{0, {}}};
+      property.version = 2;
+      property.restrict_same_input_output_scale = true;
+      break;
     case BuiltinOperator_RSQRT:
       property.inputs = {{0, {}}};
       property.outputs = {{0, {}}};
