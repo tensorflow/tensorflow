3094612f3c964e89f0e98dfd6031052eec505fea
A. Unique TensorFlower <gardener@tensorflow.org>
2021-02-04 08:59:59 -0800

    More official linear layout support.
    PiperOrigin-RevId: 355637931
    Change-Id: Ib493c4ca052a0e79052d2c770edd80b1044ed5a3

--- tensorflow/compiler/xla/service/layout_assignment.cc
+++ tensorflow/compiler/xla/service/layout_assignment.cc
@@ -1932,19 +1932,26 @@ Status LayoutAssignment::RunOnComputation(
 
   // Copy the root instruction's result if its layout does not match the result
   // layout constraint.
-  if (constraints.ResultLayout() != nullptr &&
-      !constraints.ResultLayout()->MatchesLayoutInShape(
-          computation->root_instruction()->shape(),
-          /*minor_to_major_only=*/true)) {
-    if (conditional_mismatch_.count(computation) > 0) {
-      *FindOrDie(computation_layouts_, computation).mutable_result_layout() =
-          FindOrDie(conditional_mismatch_, computation).result_layout();
+  if (constraints.ResultLayout() != nullptr) {
+    // Layout assignment at this point only does minor-to-major assignment so
+    // tiling info should be ignored here for comparison.
+    if (!constraints.ResultLayout()->MatchesLayoutInShape(
+            computation->root_instruction()->shape(),
+            /*minor_to_major_only=*/true)) {
+      if (conditional_mismatch_.count(computation) > 0) {
+        *FindOrDie(computation_layouts_, computation).mutable_result_layout() =
+            FindOrDie(conditional_mismatch_, computation).result_layout();
+      }
+      TF_ASSIGN_OR_RETURN(
+          HloInstruction * new_root,
+          CreateCopyWithNewLayout(constraints.ResultLayout()->shape(),
+                                  computation->root_instruction()));
+      computation->set_root_instruction(new_root);
+    } else {
+      // Use the specified shape including tiling info in layout.
+      *(computation->root_instruction()->mutable_shape()) =
+          constraints.ResultLayout()->shape();
     }
-    TF_ASSIGN_OR_RETURN(
-        HloInstruction * new_root,
-        CreateCopyWithNewLayout(constraints.ResultLayout()->shape(),
-                                computation->root_instruction()));
-    computation->set_root_instruction(new_root);
   }
   return Status::OK();
 }
