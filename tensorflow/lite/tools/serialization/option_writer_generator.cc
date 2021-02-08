ffe67626083d356629326fec99ad032c51a7d904
Thai Nguyen <thaink@google.com>
2021-02-07 17:47:41 -0800

    Add ComplexAbs Op to TensorFlow Lite
    Also promote Real and Image to builtin op.
    Converter support will be added in a follow-up cl.
    
    PiperOrigin-RevId: 356169843
    Change-Id: I766ca461a99038f592fe2008b2d4cf86c9496acc

--- tensorflow/lite/tools/serialization/option_writer_generator.cc
+++ tensorflow/lite/tools/serialization/option_writer_generator.cc
@@ -193,6 +193,9 @@ class OpOptionData {
     op_to_option_["RSQRT"] = "";
     op_to_option_["ELU"] = "";
     op_to_option_["REVERSE_SEQUENCE"] = "";
+    op_to_option_["REAL"] = "";
+    op_to_option_["IMAG"] = "";
+    op_to_option_["COMPLEX_ABS"] = "";
 
     // TODO(aselle): These are undesirable hacks. Consider changing C structs
     option_to_struct_["Pool2DOptions"] = "TfLitePoolParams";
