88728dd5fe13958515999450109c31a25d83018d
Eli Osherovich <eli.osherovich@gmail.com>
2021-02-08 14:33:47 +0200

    Minor cleanup.

--- tensorflow/core/kernels/image/image_projective_transform_test.cc
+++ tensorflow/core/kernels/image/image_projective_transform_test.cc
@@ -24,7 +24,6 @@ limitations under the License.
 #include "tensorflow/core/kernels/ops_testutil.h"
 #include "tensorflow/core/kernels/ops_util.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
-// #include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
