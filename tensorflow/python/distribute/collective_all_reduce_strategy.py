eb31d8660d9b246c2f15a5e29d1b21de3731b36f
Chenkai Kuang <chenkai@google.com>
2021-02-09 15:22:19 -0800

    Add all_reduce APIs that can be called in replica context to class `CrossDeviceOps` and `StrategyExtended`.
    For `StrategyExtended`, it is a private API that will be used by `ReplicaContext.all_reduce`.
    
    This is in preparation for deprecation of merge_call from user API.
    
    PiperOrigin-RevId: 356604626
    Change-Id: I2528b35b87db1b93907b17a246dbfbcfcb64ad33

--- tensorflow/python/distribute/collective_all_reduce_strategy.py
+++ tensorflow/python/distribute/collective_all_reduce_strategy.py
@@ -31,6 +31,7 @@ from tensorflow.python.distribute import cross_device_utils
 from tensorflow.python.distribute import device_util
 from tensorflow.python.distribute import distribute_lib
 from tensorflow.python.distribute import distribute_utils
+from tensorflow.python.distribute import distribution_strategy_context as ds_context
 from tensorflow.python.distribute import input_lib
 from tensorflow.python.distribute import mirrored_strategy
 from tensorflow.python.distribute import multi_worker_util
@@ -770,6 +771,30 @@ class CollectiveAllReduceExtended(mirrored_strategy.MirroredExtended):
         value,
         destinations=destinations,
         options=self._communication_options.merge(options))
+
+  def _replica_ctx_all_reduce(self, reduce_op, value, options=None):
+    """Implements `StrategyExtendedV2._replica_ctx_all_reduce`."""
+    # This implementation avoids using `merge_call` and just launches collective
+    # ops in one replica.
+    if options is None:
+      options = collective_util.Options()
+
+    if context.executing_eagerly():
+      # In eager mode, falls back to the default implemenation that uses
+      # `merge_call`. Replica functions are running sequentially in eager mode,
+      # and due to the blocking nature of collective ops, execution will hang if
+      # collective ops are to be launched sequentially.
+      return super()._replica_ctx_all_reduce(reduce_op, value, options)
+
+    replica_context = ds_context.get_replica_context()
+    assert replica_context, (
+        "`StrategyExtended._replica_ctx_all_reduce` must be called in a "
+        "replica context")
+    return self._cross_device_ops._all_reduce(  # pylint: disable=protected-access
+        reduce_op,
+        value,
+        replica_context._replica_id,  # pylint: disable=protected-access
+        options)
 
   def _check_health(self):
     while True:
