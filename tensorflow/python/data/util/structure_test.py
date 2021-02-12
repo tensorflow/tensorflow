2775fc9f582b8a40bced3de194a7434b3629568e
Vignesh Kothapalli <vikoth18@in.ibm.com>
2021-02-13 00:47:45 +0530

    switch to combinations from parameterized

--- tensorflow/python/data/util/structure_test.py
+++ tensorflow/python/data/util/structure_test.py
@@ -96,78 +96,109 @@ class StructureTest(test_base.DatasetTestBase, parameterized.TestCase):
       else:
         self.assertEqual(actual.as_list(), expected)
 
-  @combinations.generate(test_base.graph_only_combinations())
-  @parameterized.named_parameters(
-      ("Tensor", lambda: constant_op.constant(37.0), lambda: [
-          constant_op.constant(38.0),
-          array_ops.placeholder(dtypes.float32),
-          variables.Variable(100.0), 42.0,
-          np.array(42.0, dtype=np.float32)
-      ], lambda: [constant_op.constant([1.0, 2.0]),
-                  constant_op.constant(37)]),
-      ("TensorArray", lambda: tensor_array_ops.TensorArray(
-          dtype=dtypes.float32, element_shape=(3,), size=0), lambda: [
-              tensor_array_ops.TensorArray(
-                  dtype=dtypes.float32, element_shape=(3,), size=0),
-              tensor_array_ops.TensorArray(
-                  dtype=dtypes.float32, element_shape=(3,), size=10)
-          ], lambda: [
-              tensor_array_ops.TensorArray(
-                  dtype=dtypes.int32, element_shape=(3,), size=0),
-              tensor_array_ops.TensorArray(
-                  dtype=dtypes.float32, element_shape=(), size=0)
-          ]),
-      ("SparseTensor", lambda: sparse_tensor.SparseTensor(
-          indices=[[3, 4]], values=[-1], dense_shape=[4, 5]),
-       lambda: [
-           sparse_tensor.SparseTensor(
-               indices=[[1, 1], [3, 4]], values=[10, -1], dense_shape=[4, 5]),
-           sparse_tensor.SparseTensorValue(
-               indices=[[1, 1], [3, 4]], values=[10, -1], dense_shape=[4, 5]),
-           array_ops.sparse_placeholder(dtype=dtypes.int32),
-           array_ops.sparse_placeholder(dtype=dtypes.int32, shape=[None, None])
-       ], lambda: [
-           constant_op.constant(37, shape=[4, 5]),
-           sparse_tensor.SparseTensor(
-               indices=[[3, 4]], values=[-1], dense_shape=[5, 6]),
-           array_ops.sparse_placeholder(
-               dtype=dtypes.int32, shape=[None, None, None]),
-           sparse_tensor.SparseTensor(
-               indices=[[3, 4]], values=[-1.0], dense_shape=[4, 5])
-       ]),
-      ("RaggedTensor", lambda: ragged_factory_ops.constant([[1, 2], [], [3]]),
-       lambda: [
-           ragged_factory_ops.constant([[1, 2], [3, 4], []]),
-           ragged_factory_ops.constant([[1], [2, 3, 4], [5]]),
-       ], lambda: [
-           ragged_factory_ops.constant(1),
-           ragged_factory_ops.constant([1, 2]),
-           ragged_factory_ops.constant([[1], [2]]),
-           ragged_factory_ops.constant([["a", "b"]]),
-       ]),
-      ("Nested", lambda: {
-          "a": constant_op.constant(37.0),
-          "b": constant_op.constant([1, 2, 3])
-      }, lambda: [{
-          "a": constant_op.constant(15.0),
-          "b": constant_op.constant([4, 5, 6])
-      }], lambda: [{
-          "a": constant_op.constant(15.0),
-          "b": constant_op.constant([4, 5, 6, 7])
-      }, {
-          "a": constant_op.constant(15),
-          "b": constant_op.constant([4, 5, 6])
-      }, {
-          "a":
-              constant_op.constant(15),
-          "b":
-              sparse_tensor.SparseTensor(
-                  indices=[[0], [1], [2]], values=[4, 5, 6], dense_shape=[3])
-      }, (constant_op.constant(15.0), constant_op.constant([4, 5, 6]))]),
+  @combinations.generate(
+      combinations.times(
+          test_base.graph_only_combinations(),
+          combinations.combine(value_fns=[
+              combinations.NamedObject(
+                  "Tensor", [
+                      lambda: constant_op.constant(37.0),
+                      lambda: [
+                          constant_op.constant(38.0),
+                          array_ops.placeholder(dtypes.float32),
+                          variables.Variable(100.0), 42.0,
+                          np.array(42.0, dtype=np.float32)
+                      ],
+                      lambda: [constant_op.constant([1.0, 2.0]),
+                               constant_op.constant(37)]
+                  ]),
+              combinations.NamedObject(
+                  "TensorArray", [
+                      lambda: tensor_array_ops.TensorArray(
+                          dtype=dtypes.float32, element_shape=(3,), size=0),
+                      lambda: [
+                          tensor_array_ops.TensorArray(
+                              dtype=dtypes.float32, element_shape=(3,), size=0),
+                          tensor_array_ops.TensorArray(
+                              dtype=dtypes.float32, element_shape=(3,), size=10)
+                      ],
+                      lambda: [
+                          tensor_array_ops.TensorArray(
+                              dtype=dtypes.int32, element_shape=(3,), size=0),
+                          tensor_array_ops.TensorArray(
+                              dtype=dtypes.float32, element_shape=(), size=0)
+                      ]
+                  ]),
+              combinations.NamedObject(
+                  "SparseTensor", [
+                      lambda: sparse_tensor.SparseTensor(
+                          indices=[[3, 4]], values=[-1], dense_shape=[4, 5]),
+                      lambda: [
+                          sparse_tensor.SparseTensor(
+                              indices=[[1, 1], [3, 4]], values=[10, -1], dense_shape=[4, 5]),
+                          sparse_tensor.SparseTensorValue(
+                              indices=[[1, 1], [3, 4]], values=[10, -1], dense_shape=[4, 5]),
+                          array_ops.sparse_placeholder(dtype=dtypes.int32),
+                          array_ops.sparse_placeholder(
+                              dtype=dtypes.int32, shape=[None, None])
+                      ],
+                      lambda: [
+                          constant_op.constant(37, shape=[4, 5]),
+                          sparse_tensor.SparseTensor(
+                              indices=[[3, 4]], values=[-1], dense_shape=[5, 6]),
+                          array_ops.sparse_placeholder(
+                              dtype=dtypes.int32, shape=[None, None, None]),
+                          sparse_tensor.SparseTensor(
+                              indices=[[3, 4]], values=[-1.0], dense_shape=[4, 5])
+                      ]
+                  ]),
+              combinations.NamedObject(
+                  "RaggedTensor", [
+                      lambda: ragged_factory_ops.constant([[1, 2], [], [3]]),
+                      lambda: [
+                          ragged_factory_ops.constant([[1, 2], [3, 4], []]),
+                          ragged_factory_ops.constant([[1], [2, 3, 4], [5]]),
+                      ],
+                      lambda: [
+                          ragged_factory_ops.constant(1),
+                          ragged_factory_ops.constant([1, 2]),
+                          ragged_factory_ops.constant([[1], [2]]),
+                          ragged_factory_ops.constant([["a", "b"]]),
+                      ]
+                  ]),
+              combinations.NamedObject(
+                  "Nested", [
+                      lambda: {
+                          "a": constant_op.constant(37.0),
+                          "b": constant_op.constant([1, 2, 3])
+                      },
+                      lambda: [{
+                          "a": constant_op.constant(15.0),
+                          "b": constant_op.constant([4, 5, 6])
+                      }],
+                      lambda: [{
+                          "a": constant_op.constant(15.0),
+                          "b": constant_op.constant([4, 5, 6, 7])
+                      }, {
+                          "a": constant_op.constant(15),
+                          "b": constant_op.constant([4, 5, 6])
+                      }, {
+                          "a":
+                          constant_op.constant(15),
+                          "b":
+                          sparse_tensor.SparseTensor(
+                              indices=[[0], [1], [2]], values=[4, 5, 6], dense_shape=[3])
+                      }, (constant_op.constant(15.0), constant_op.constant([4, 5, 6]))
+                      ]
+                  ]),
+          ])
+      )
   )
-  def testIsCompatibleWithStructure(self, original_value_fn,
-                                    compatible_values_fn,
-                                    incompatible_values_fn):
+  def testIsCompatibleWithStructure(self, value_fns):
+    value_fns = value_fns._obj
+    original_value_fn = value_fns[0]
+    compatible_values_fn = value_fns[1]
+    incompatible_values_fn = value_fns[2]
     original_value = original_value_fn()
     compatible_values = compatible_values_fn()
     incompatible_values = incompatible_values_fn()
