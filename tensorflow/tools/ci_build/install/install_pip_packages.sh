945b9ae48fa06175a8b2ebb2303f6b9a44968d74
bhack <bhack@users.noreply.github.com>
2021-02-17 01:07:49 +0100

    Remove python2

--- tensorflow/tools/ci_build/install/install_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_pip_packages.sh
@@ -86,7 +86,6 @@ pip3 install lazy-object-proxy==1.4.3
 # pylint tests require the following version. pylint==1.6.4 hangs erratically,
 # thus using the updated version of 2.5.3 only for python3 as python2 is EOL
 # and this version is not available.
-pip2 install pylint==1.6.4
 pip3 install pylint==2.6.2
 
 # pycodestyle tests require the following:
