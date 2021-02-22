5d09b79848a96170d81224150d191aa67890e4d9
Vignesh Kothapalli <vikoth18@in.ibm.com>
2021-02-22 23:43:47 +0530

    docstring fix

--- tensorflow/python/data/kernel_tests/checkpoint_test_base.py
+++ tensorflow/python/data/kernel_tests/checkpoint_test_base.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Base test class for checkpointing of serializable datasets."""
+"""Base test class for checkpointing datasets."""
 
 from __future__ import absolute_import
 from __future__ import division
