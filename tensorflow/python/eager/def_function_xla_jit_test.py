b60aff4c1ded72946e73edb12b678aed3fd7cf65
Yujing Zhang <yujingzhang@google.com>
2021-02-22 15:34:37 -0800

    Disable testUpdateVariable when MLIR bridge is enabled
    PiperOrigin-RevId: 358918630
    Change-Id: I45fd0f6b8ff7f7371489648bd5963a74412e7fdc

--- tensorflow/python/eager/def_function_xla_jit_test.py
+++ tensorflow/python/eager/def_function_xla_jit_test.py
@@ -578,6 +578,7 @@ class DefFunctionTest(xla_test.XLATestCase):
 
       self.assertEqual(inner_retracings, 1)
 
+  @test_util.disable_mlir_bridge('b/180951174')
   def testUpdateVariable(self):
     with ops.device('device:{}:0'.format(self.device)):
 
