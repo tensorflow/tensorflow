ec3f158515788d9a86dc1bad479f6dfc3be2879a
Yuefeng Zhou <yuefengz@google.com>
2021-02-22 16:21:24 -0800

    Catch non-worker-preemption errors in the preemption handler.
    PiperOrigin-RevId: 358928837
    Change-Id: I8c6a248a89579ca4418ba73fae17fcb9f7acd5ea

--- tensorflow/python/distribute/coordinator/fault_tolerance_test.py
+++ tensorflow/python/distribute/coordinator/fault_tolerance_test.py
@@ -446,10 +446,26 @@ class BaseFaultToleranceTest(object):  # pylint: disable=missing-docstring
     self.assertGreaterEqual(model.iterations.numpy(), 10)
 
   def testPSFailureWhileRecoveryFromWokerFailure(self):
-    # Only by adding this empty test, can the problem of b/180348454 be
-    # reproduced.
-    # TODO(yuefengz): fill in this test.
-    pass
+    model = self._create_model_and_run_indefinitely()
+
+    time.sleep(1)
+    self.assertFalse(self.cluster_coord.done())
+
+    def kill(task):
+      self._cluster.kill_task(task, 0)
+      self.sleep(1)
+      self._cluster.start_task(task, 0)
+
+    kill_thread_1 = threading.Thread(target=kill, args=("worker",))
+    kill_thread_2 = threading.Thread(target=kill, args=("ps",))
+    kill_thread_1.start()
+    kill_thread_2.start()
+    kill_thread_1.join()
+    kill_thread_2.join()
+
+    with self.assertRaises(
+        (errors.UnavailableError, errors.InvalidArgumentError)):
+      model.join_training_functions()
 
   def testNumpyFetchedAfterWorkerFailure(self):
 
