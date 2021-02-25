42f96bfbd75ddce8cc5d7d27882ef4dea0efd817
Yair Ehrenwald <yair.ehrenwald@ceva-dsp.com>
2021-02-25 22:49:46 +0200

    clang format

--- tensorflow/lite/micro/kernels/ceva/fully_connected.cc
+++ tensorflow/lite/micro/kernels/ceva/fully_connected.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #include "tensorflow/lite/micro/kernels/fully_connected.h"
 #include "tensorflow/lite/kernels/internal/reference/fully_connected.h"
 
