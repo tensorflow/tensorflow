1b2635286bb757426125b7fbe28ecf47f268d734
bhack <bhack@users.noreply.github.com>
2021-02-27 14:34:53 +0100

    Trailing spaces

--- tensorflow/python/keras/mixed_precision/loss_scale_optimizer.py
+++ tensorflow/python/keras/mixed_precision/loss_scale_optimizer.py
@@ -890,7 +890,7 @@ class LossScaleOptimizer(_DelegatingTrackableMixin, optimizer_v2.OptimizerV2):
 
       # Delegate hyperparameter accesses to inner optimizer.
       if name == 'lr':
-        name = 'learning_rate'        
+        name = 'learning_rate'
       if name in self._optimizer._hyper:
         return self._optimizer._get_hyper(name)
       elif hasattr(self._optimizer,"_optimizer"):
