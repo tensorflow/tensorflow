e209e2fe1bee3997e0d1e966e773e5d596b001c9
Deven Desai <deven.desai.amd@gmail.com>
2021-03-02 18:34:27 +0000

    Re-enabling profiler related unit-tests / sub-tests, that were disabled on the ROCm platform

--- tensorflow/python/profiler/internal/run_metadata_test.py
+++ tensorflow/python/profiler/internal/run_metadata_test.py
@@ -136,10 +136,6 @@ class RunMetadataTest(test.TestCase):
 
     ret = _extract_node(run_meta, 'MatMul')
     self.assertEqual(len(ret['gpu:0']), 1)
-    if not test.is_built_with_rocm():
-      # skip this check for the ROCm platform
-      # stream level tracing is not yet supported on the ROCm platform
-      self.assertEqual(len(ret['gpu:0/stream:all']), 1, '%s' % run_meta)
 
   @test_util.run_deprecated_v1
   def testAllocationHistory(self):
@@ -251,9 +247,6 @@ class RunMetadataTest(test.TestCase):
       for node in ret['gpu:0']:
         total_cpu_execs += node.op_end_rel_micros
 
-      if not test.is_built_with_rocm():
-        # skip this check for the ROCm platform
-        # stream level tracing is not yet supported on the ROCm platform
         self.assertGreaterEqual(
             len(ret['gpu:0/stream:all']), 4, '%s' % run_meta)
 
