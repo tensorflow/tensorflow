c1edbc1be78c1451b7549a94ba061bbc6e03a0f3
Rick Chao <rchao@google.com>
2021-03-01 17:42:25 -0800

    PSv2: Fix the ClusterCoordinator-Strategy duo object leak at exit of the local scope. With this, the worker and preemption threads are closed properly at next GC after the local scope exits. Add steps in test to cover this for worker, ps, and coordinator preemption cases. The leak stemmed from worker threads holding local closure variable and is fixed by releasing such after it's being processed.
    PiperOrigin-RevId: 360316694
    Change-Id: I191056e6375a4028b42138bedd8b7e4ee48ce733

--- tensorflow/python/distribute/coordinator/cluster_coordinator.py
+++ tensorflow/python/distribute/coordinator/cluster_coordinator.py
@@ -825,13 +825,20 @@ class Worker(object):
     time.sleep(delay_secs)
 
   def _process_queue(self):
-    """Function running in a thread to process closure queues."""
+    """Function running in a worker thread to process closure queues."""
     self._maybe_delay()
     while self._should_worker_thread_run:
       closure = self._cluster._closure_queue.get()  # pylint: disable=protected-access
       if not self._should_worker_thread_run or closure is None:
         return
       self._process_closure(closure)
+      # To properly stop the worker and preemption threads, it is important that
+      # `ClusterCoordinator` object is not held onto so its `__del__` can be
+      # called. By removing the reference to the `closure` that has already been
+      # processed, we ensure that the `closure` object is released, while
+      # getting the next `closure` at above `self._cluster._closure_queue.get()`
+      # call.
+      del closure
 
   def _create_resource(self, function, args=None, kwargs=None):
     """Synchronously creates a per-worker resource represented by a `RemoteValue`.
