3a3a7631fad3971fe43196744cbfac5bc6252b7b
A. Unique TensorFlower <gardener@tensorflow.org>
2021-03-03 13:55:15 -0800

    Be assertive that we support unordered sparse tensors.
    PiperOrigin-RevId: 360750129
    Change-Id: If44b18ce8894a0608069dc09ed705aef0433942f

--- tensorflow/python/kernel_tests/sparse_tensor_dense_matmul_op_test.py
+++ tensorflow/python/kernel_tests/sparse_tensor_dense_matmul_op_test.py
@@ -163,6 +163,18 @@ class SparseTensorDenseMatMulTest(test.TestCase):
           sparse_ops.sparse_tensor_dense_matmul(
               sparse_t, dense_t, adjoint_a=True))
 
+  def testUnorderedIndicesForSparseTensorDenseMatmul(self):
+    indices = np.array([(2, 1), (0, 0)]).astype(np.int64)
+    values = np.array([10, 11]).astype(np.float32)
+    shape = [3, 2]
+    sparse_t = sparse_tensor.SparseTensor(indices, values, shape)
+
+    dense_t = np.array([[1] * 500, [2] * 500], dtype=np.float32)
+    expected_t = np.array([[11] * 500, [0] * 500, [20] * 500], dtype=np.float32)
+
+    self.assertAllClose(
+        expected_t, sparse_ops.sparse_tensor_dense_matmul(sparse_t, dense_t))
+
   @test_util.run_gpu_only
   def testInvalidIndicesForSparseTensorDenseMatmulOnGPU(self):
     indices = np.array([[1, 10]]).astype(np.int64)
