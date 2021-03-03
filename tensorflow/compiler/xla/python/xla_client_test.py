0e182ba66c18db83668c05d26ee0c84ca9e2dbff
A. Unique TensorFlower <gardener@tensorflow.org>
2021-03-02 16:44:59 -0800

    [XLA:Python] Expose buffer addresses in Python.
    PiperOrigin-RevId: 360544494
    Change-Id: I7af7353579db19688d5ce92d7e685a279ca55c91

--- tensorflow/compiler/xla/python/xla_client_test.py
+++ tensorflow/compiler/xla/python/xla_client_test.py
@@ -556,6 +556,19 @@ def TestFactory(xla_backend, cloud_tpu=False):
         arr = arr.to_py()
         self.assertEqual(dtype, type(arr[0]))
 
+    def testUnsafeBufferPointer(self):
+      if not isinstance(self.backend, xla_client.Client):
+        self.skipTest("TPU Driver doesn't support UnsafeBufferPointer().")
+      arg0 = np.array([])
+      arg1 = np.array([[0., 1., 2.]], np.float32)
+      arg2 = np.array([[3., 4., 5.]], bfloat16)
+      arg0_buffer = self.backend.buffer_from_pyval(arg0)
+      arg1_buffer = self.backend.buffer_from_pyval(arg1)
+      arg2_buffer = self.backend.buffer_from_pyval(arg2)
+      self.assertGreaterEqual(arg0_buffer.unsafe_buffer_pointer(), 0)
+      self.assertGreaterEqual(arg1_buffer.unsafe_buffer_pointer(), 0)
+      self.assertGreaterEqual(arg2_buffer.unsafe_buffer_pointer(), 0)
+
   tests.append(BufferTest)
 
   class SingleOpTest(ComputationTest):
