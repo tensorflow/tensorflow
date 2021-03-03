59b8b6dc462b02a2e727b1356cbcb54baeb334a1
Hiran Sarkar <hiransarkar.cse2019@nsec.ac.in>
2021-03-04 00:35:12 +0530

    Update losses.py

--- tensorflow/python/keras/losses.py
+++ tensorflow/python/keras/losses.py
@@ -1645,8 +1645,8 @@ def categorical_crossentropy(y_true,
     num_classes = math_ops.cast(array_ops.shape(y_true)[-1], y_pred.dtype)
     return y_true * (1.0 - label_smoothing) + (label_smoothing / num_classes)
 
-  y_true = smart_cond.smart_cond(
-      label_smoothing, _smooth_labels, lambda: y_true)
+  y_true = smart_cond.smart_cond(label_smoothing, _smooth_labels,
+                                 lambda: y_true)
   return K.categorical_crossentropy(y_true, y_pred, from_logits=from_logits, axis=axis)
 
 
