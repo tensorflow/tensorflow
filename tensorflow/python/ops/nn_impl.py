e947f1feba2677a716b7f670bee653959423b131
sangireddysiva <sangireddysiva@gmail.com>
2021-03-04 16:17:18 +0000

    Addition of TanhExp activation function - issue #45929

--- tensorflow/python/ops/nn_impl.py
+++ tensorflow/python/ops/nn_impl.py
@@ -550,6 +550,44 @@ def swish(features):
   return features * math_ops.sigmoid(features), grad
 
 
+@tf_export("nn.tanhexp")
+@dispatch.add_dispatch_support
+@custom_gradient.custom_gradient
+def tanhexp(features):
+  # pylint: disable=g-doc-args
+  """Computes the TanhExp activation function: `x * tanh(e^x)`.
+  The TanhExp activation function was introduced in "TanhExp: A Smooth Activation Function
+  with High Convergence Speed for Lightweight Neural Networks"
+  [Xinyu et al. 2020](https://arxiv.org/pdf/2003.09855.pdf) 
+  Args:
+    features: A `Tensor` representing preactivation values.
+    name: A name for the operation (optional).
+  Returns:
+    The activation value.
+  """
+  # pylint: enable=g-doc-args
+  if isinstance(features, int):
+    features = ops.convert_to_tensor(features, name="features", dtype ='double')
+  else:
+    features = ops.convert_to_tensor(features, name="features")
+
+  def grad(dy):
+    """Gradient for the Tanhexp activation function"""
+    # Naively, x * tf.nn.tanh(e^x) requires keeping both x and tanh(e^x)
+    # around for backprop, effectively doubling the tensor's memory consumption.
+    # We use a control dependency here so that tanh(e^x) is re-computed
+    # during backprop (the control dep prevents it being de-duped with the
+    # forward pass) and we can free the tanh(e^x) expression immediately
+    # after use during the forward pass.
+    with ops.control_dependencies([dy]):
+      tanhexp_features = math_ops.tanh(math_ops.exp(features))
+      activation_grad = (
+        tanhexp_features * (1.0 + features * (1.0 - tanhexp_features)))
+    return dy * activation_grad
+
+  return features * math_ops.tanh(math_ops.exp(features)) , grad
+
+
 # pylint: disable=redefined-builtin
 @tf_export("linalg.normalize")
 @dispatch.add_dispatch_support
