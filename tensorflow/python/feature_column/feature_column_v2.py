beb47d5291f56070393389ddb8bee9b0472888f4
Scott Zhu <scottzhu@google.com>
2021-03-05 13:48:42 -0800

    Expose feature_column_v2._StateManagerImpl into a tf.__internal__ API.
    PiperOrigin-RevId: 361217533
    Change-Id: I045d8b2ee8e80d0e0e246e2ec6cf9c88c371fd8f

--- tensorflow/python/feature_column/feature_column_v2.py
+++ tensorflow/python/feature_column/feature_column_v2.py
@@ -269,8 +269,15 @@ class StateManager(object):
     raise NotImplementedError('StateManager.get_resource')
 
 
+@tf_export('__internal__.feature_column.StateManager', v1=[])
 class _StateManagerImpl(StateManager):
-  """Manages the state of DenseFeatures and LinearLayer."""
+  """Manages the state of DenseFeatures and LinearLayer.
+
+  Some `FeatureColumn`s create variables or resources to assist their
+  computation. The `StateManager` is responsible for creating and storing these
+  objects since `FeatureColumn`s are supposed to be stateless configuration
+  only.
+  """
 
   def __init__(self, layer, trainable):
     """Creates an _StateManagerImpl object.
@@ -294,6 +301,21 @@ class _StateManagerImpl(StateManager):
                       trainable=True,
                       use_resource=True,
                       initializer=None):
+    """Creates a new variable.
+
+    Args:
+      feature_column: A `FeatureColumn` object this variable corresponds to.
+      name: variable name.
+      shape: variable shape.
+      dtype: The type of the variable. Defaults to `self.dtype` or `float32`.
+      trainable: Whether this variable is trainable or not.
+      use_resource: If true, we use resource variables. Otherwise we use
+        RefVariable.
+      initializer: initializer instance (callable).
+
+    Returns:
+      The created variable.
+    """
     if name in self._cols_to_vars_map[feature_column]:
       raise ValueError('Variable already exists.')
 
@@ -323,11 +345,29 @@ class _StateManagerImpl(StateManager):
     return var
 
   def get_variable(self, feature_column, name):
+    """Returns an existing variable.
+
+    Args:
+      feature_column: A `FeatureColumn` object this variable corresponds to.
+      name: variable name.
+    """
     if name in self._cols_to_vars_map[feature_column]:
       return self._cols_to_vars_map[feature_column][name]
     raise ValueError('Variable does not exist.')
 
   def add_resource(self, feature_column, resource_name, resource):
+    """Creates a new resource.
+
+    Resources can be things such as tables, variables, trackables, etc.
+
+    Args:
+      feature_column: A `FeatureColumn` object this resource corresponds to.
+      resource_name: Name of the resource.
+      resource: The resource.
+
+    Returns:
+      The created resource.
+    """
     self._cols_to_resources_map[feature_column][resource_name] = resource
     # pylint: disable=protected-access
     if self._layer is not None and isinstance(resource, trackable.Trackable):
@@ -339,9 +379,25 @@ class _StateManagerImpl(StateManager):
     # pylint: enable=protected-access
 
   def has_resource(self, feature_column, resource_name):
+    """Returns true iff a resource with same name exists.
+
+    Resources can be things such as tables, variables, trackables, etc.
+
+    Args:
+      feature_column: A `FeatureColumn` object this variable corresponds to.
+      resource_name: Name of the resource.
+    """
     return resource_name in self._cols_to_resources_map[feature_column]
 
   def get_resource(self, feature_column, resource_name):
+    """Returns an already created resource.
+
+    Resources can be things such as tables, variables, trackables, etc.
+
+    Args:
+      feature_column: A `FeatureColumn` object this variable corresponds to.
+      resource_name: Name of the resource.
+    """
     if (feature_column not in self._cols_to_resources_map or
         resource_name not in self._cols_to_resources_map[feature_column]):
       raise ValueError('Resource does not exist.')
