aef0659ad4e5ea21807a990d1372f1b0831b53e4
Sheng, Yang <yang.sheng@intel.com>
2021-03-05 13:28:23 +0800

    Graph utility C API

--- tensorflow/c/experimental/grappler/grappler.cc
+++ tensorflow/c/experimental/grappler/grappler.cc
@@ -193,3 +193,204 @@ tensorflow::Status InitGraphPlugin(TFInitGraphPluginFn init_fn) {
 
 }  // namespace grappler
 }  // namespace tensorflow
+
+const TF_GrapplerItem* TF_GetGrapplerItem(TF_Buffer* graph_buf,
+                                          TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  const auto it = GrapplerItemMap()->find(graph_buf);
+  if (it != GrapplerItemMap()->end()) {
+    return it->second;
+  } else {
+    status->status = tensorflow::errors::NotFound("GrapplerItem is not found");
+    return nullptr;
+  }
+}
+
+void TF_GetNodesToPreserveListSize(const TF_GrapplerItem* item, int* num_values,
+                                   size_t* storage_size, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::unordered_set<std::string> nodes =
+      reinterpret_cast<const tensorflow::grappler::GrapplerItem*>(item)
+          ->NodesToPreserve();
+  *num_values = nodes.size();
+  *storage_size = 0;
+  for (const std::string& str : nodes) {
+    *storage_size += str.size();
+  }
+}
+
+void TF_GetNodesToPreserveList(const TF_GrapplerItem* item, char** values,
+                               size_t* lengths, int num_values, void* storage,
+                               size_t storage_size, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::unordered_set<std::string> nodes =
+      reinterpret_cast<const tensorflow::grappler::GrapplerItem*>(item)
+          ->NodesToPreserve();
+  char* p = static_cast<char*>(storage);
+
+  int index = 0;
+  for (const std::string& s : nodes) {
+    if (index >= num_values) break;
+    values[index] = p;
+    lengths[index] = s.size();
+    if ((p + s.size()) > (static_cast<char*>(storage) + storage_size)) {
+      status->status = tensorflow::errors::InvalidArgument(
+          "Not enough storage to hold the requested list of nodes");
+      return;
+    }
+    memcpy(values[index], s.data(), s.size());
+    p += s.size();
+    index++;
+  }
+}
+
+void TF_GetFetchNodesListSize(const TF_GrapplerItem* item, int* num_values,
+                              size_t* storage_size, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::vector<std::string> nodes =
+      reinterpret_cast<const tensorflow::grappler::GrapplerItem*>(item)->fetch;
+  *num_values = nodes.size();
+  *storage_size = 0;
+  for (const std::string& str : nodes) {
+    *storage_size += str.size();
+  }
+}
+
+void TF_GetFetchNodesList(const TF_GrapplerItem* item, char** values,
+                          size_t* lengths, int num_values, void* storage,
+                          size_t storage_size, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::vector<std::string> nodes =
+      reinterpret_cast<const tensorflow::grappler::GrapplerItem*>(item)->fetch;
+
+  const int len = std::min(num_values, static_cast<int>(nodes.size()));
+  char* p = static_cast<char*>(storage);
+  for (int index = 0; index < len; ++index) {
+    const std::string& s = nodes[index];
+    values[index] = p;
+    lengths[index] = s.size();
+    if ((p + s.size()) > (static_cast<char*>(storage) + storage_size)) {
+      status->status = tensorflow::errors::InvalidArgument(
+          "Not enough storage to hold the requested list of nodes");
+      return;
+    }
+    memcpy(values[index], s.data(), s.size());
+    p += s.size();
+  }
+}
+
+TF_GraphProperties* TF_NewGraphProperties(const TF_GrapplerItem* item) {
+  return reinterpret_cast<TF_GraphProperties*>(
+      new tensorflow::grappler::GraphProperties(
+          *reinterpret_cast<const tensorflow::grappler::GrapplerItem*>(item)));
+}
+
+void TF_DeleteGraphProperties(TF_GraphProperties* g_prop) {
+  if (g_prop == nullptr) return;
+  delete reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop);
+}
+
+void TF_InferStatically(TF_GraphProperties* g_prop, TF_Bool assume_valid_feeds,
+                        TF_Bool aggressive_shape_inference,
+                        TF_Bool include_input_tensor_values,
+                        TF_Bool include_output_tensor_values,
+                        TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  tensorflow::Status s =
+      reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop)
+          ->InferStatically(assume_valid_feeds, aggressive_shape_inference,
+                            include_input_tensor_values,
+                            include_output_tensor_values);
+  if (!s.ok()) {
+    ::tensorflow::Set_TF_Status_from_Status(status, s);
+  }
+}
+
+void TF_GetInputPropertiesListSize(TF_GraphProperties* g_prop, const char* name,
+                                   int* num_values, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  *num_values = reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop)
+                    ->GetInputProperties(name)
+                    .size();
+}
+
+void TF_GetOutputPropertiesListSize(TF_GraphProperties* g_prop,
+                                    const char* name, int* num_values,
+                                    TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  *num_values = reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop)
+                    ->GetOutputProperties(name)
+                    .size();
+}
+
+void TF_GetInputPropertiesList(TF_GraphProperties* g_prop, const char* name,
+                               TF_Buffer** prop, int num_values,
+                               TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::vector<tensorflow::OpInfo::TensorProperties> in_prop =
+      reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop)
+          ->GetInputProperties(name);
+  const int len = std::min(num_values, static_cast<int>(in_prop.size()));
+  for (int i = 0; i < len; ++i) {
+    tensorflow::Status s = tensorflow::MessageToBuffer(in_prop[i], prop[i]);
+    if (!s.ok()) {
+      ::tensorflow::Set_TF_Status_from_Status(status, s);
+      return;
+    }
+  }
+}
+
+void TF_GetOutputPropertiesList(TF_GraphProperties* g_prop, const char* name,
+                                TF_Buffer** prop, int num_values,
+                                TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  std::vector<tensorflow::OpInfo::TensorProperties> out_prop =
+      reinterpret_cast<tensorflow::grappler::GraphProperties*>(g_prop)
+          ->GetOutputProperties(name);
+  const int len = std::min(num_values, static_cast<int>(out_prop.size()));
+  for (int i = 0; i < len; ++i) {
+    tensorflow::Status s = tensorflow::MessageToBuffer(out_prop[i], prop[i]);
+    if (!s.ok()) {
+      ::tensorflow::Set_TF_Status_from_Status(status, s);
+      return;
+    }
+  }
+}
+
+TF_FunctionLibraryDefinition* TF_NewFunctionLibraryDefinition(
+    TF_Buffer* graph_buf, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  tensorflow::GraphDef graph_def;
+  tensorflow::Status s = tensorflow::BufferToMessage(graph_buf, &graph_def);
+  if (!s.ok()) {
+    ::tensorflow::Set_TF_Status_from_Status(status, s);
+    return nullptr;
+  }
+  return reinterpret_cast<TF_FunctionLibraryDefinition*>(
+      new tensorflow::FunctionLibraryDefinition(
+          tensorflow::OpRegistry::Global(), graph_def.library()));
+}
+
+void TF_DeleteFunctionLibraryDefinition(TF_FunctionLibraryDefinition* f_lib) {
+  if (f_lib == nullptr) return;
+  delete reinterpret_cast<tensorflow::FunctionLibraryDefinition*>(f_lib);
+}
+
+void TF_LookUpOpDef(TF_FunctionLibraryDefinition* f_lib, const char* name,
+                    TF_Buffer* buf, TF_Status* status) {
+  TF_SetStatus(status, TF_OK, "");
+  const tensorflow::OpDef* op_def_ptr = nullptr;
+  tensorflow::Status s =
+      reinterpret_cast<tensorflow::FunctionLibraryDefinition*>(f_lib)
+          ->LookUpOpDef(name, &op_def_ptr);
+  if (!s.ok()) {
+    ::tensorflow::Set_TF_Status_from_Status(status, s);
+    return;
+  }
+
+  s = tensorflow::MessageToBuffer(*op_def_ptr, buf);
+  if (!s.ok()) {
+    ::tensorflow::Set_TF_Status_from_Status(status, s);
+    return;
+  }
+}
