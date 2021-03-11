62cf8047ac294353aca712c2fd05edc3ad00434b
Mihai Maruseac <mihaimaruseac@google.com>
2021-03-10 19:23:21 -0800

    Remove extra quotes which break the build
    PiperOrigin-RevId: 362196521
    Change-Id: I08f6c372884358bdf23c254b48edb199db859529

--- tensorflow/tools/ci_build/rel/macos/cpu_py39_pip.sh
+++ tensorflow/tools/ci_build/rel/macos/cpu_py39_pip.sh
@@ -42,7 +42,7 @@ yes "" | "$PYTHON_BIN_PATH" configure.py
 
 # Export optional variables for running pip.sh
 # Pass PYENV_VERSION since we're using pyenv. See b/182399580
-export TF_BUILD_FLAGS="--config=release_cpu_macos --action_env PYENV_VERSION='${PY_VERSION}'"
+export TF_BUILD_FLAGS="--config=release_cpu_macos --action_env PYENV_VERSION=${PY_VERSION}"
 export TF_TEST_FLAGS="--define=no_tensorflow_py_deps=true --test_lang_filters=py --test_output=errors --verbose_failures=true --keep_going --test_env=TF2_BEHAVIOR=1"
 export TF_TEST_TARGETS="//tensorflow/python/..."
 export TF_PIP_TESTS="test_pip_virtualenv_non_clean test_pip_virtualenv_clean"
