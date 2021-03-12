d8a39b626d69ef3e01650e245a3fc820d954196d
Hiran Sarkar <hiransarkar.cse2019@nsec.ac.in>
2021-03-12 15:24:04 +0530

    Update losses_test.py

--- tensorflow/python/keras/losses_test.py
+++ tensorflow/python/keras/losses_test.py
@@ -100,7 +100,7 @@ class KerasLossesTest(test.TestCase, parameterized.TestCase):
     np.testing.assert_allclose(
         backend.eval(output_from_logit_axis),
         backend.eval(output_from_softmax_axis),
-        atol=1e-5
+        atol=1e-5)
 
   @combinations.generate(combinations.combine(mode=['graph', 'eager']))
   def test_categorical_crossentropy_loss_with_unknown_rank_tensor(self):
