98dd2fbddbc5048984ccf36890109d7cadff497a
Zeno Gantner <zeno.gantner@gmail.com>
2021-03-14 20:35:12 +0100

    remove __future__ imports

--- tensorflow/python/keras/losses.py
+++ tensorflow/python/keras/losses.py
@@ -13,9 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Built-in loss functions."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import abc
 import functools
