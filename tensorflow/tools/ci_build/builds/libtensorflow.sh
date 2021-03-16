d6880998a7375749a8a0e0041fe183965ea8653d
Christian Sigg <csigg@google.com>
2021-03-15 23:59:53 -0700

    Use release configs to build libtensorflow.
    PiperOrigin-RevId: 363120295
    Change-Id: I1356622a6557995d5b7477e5cb59f3c948e69766

--- tensorflow/tools/ci_build/builds/libtensorflow.sh
+++ tensorflow/tools/ci_build/builds/libtensorflow.sh
@@ -51,21 +51,16 @@ function build_libtensorflow_tarball() {
   rm -rf ${DIR}
 
   TARBALL_SUFFIX="${1}"
-  BAZEL_OPTS="--config=opt --cxxopt=-D_GLIBCXX_USE_CXX11_ABI=0"
-  export CC_OPT_FLAGS="-mavx -msse4.2"
-  if [ "${TF_NEED_CUDA}" == "1" ]; then
-    BAZEL_OPTS="${BAZEL_OPTS} --config=cuda --crosstool_top=//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010-nvcc-cuda11.2:toolchain"
-    export TF_NEED_ROCM=0
-    export TF_CUDA_COMPUTE_CAPABILITIES="sm_35,sm_50,sm_60,sm_70,sm_75,compute_80"
+  if [ "$(uname)" == "Darwin" ]; then
+    BAZEL_OPTS="${BAZEL_OPTS} --config=release_macos_linux"
+  elif [ "${TF_NEED_CUDA}" == "1" ]; then
+    BAZEL_OPTS="${BAZEL_OPTS} --config=release_gpu_linux"
   else
-    if [ "$(uname)" != "Darwin" ]; then
-      BAZEL_OPTS="${BAZEL_OPTS} --crosstool_top=//third_party/toolchains/preconfig/ubuntu16.04/gcc7_manylinux2010:toolchain"
-    fi
+    BAZEL_OPTS="${BAZEL_OPTS} --config=release_cpu_linux"
   fi
   bazel clean --expunge
   export PYTHON_BIN_PATH=$(which python3.8)
   yes "" | "$PYTHON_BIN_PATH" configure.py
-
 
   # Remove this test call when
   # https://github.com/bazelbuild/bazel/issues/2352
