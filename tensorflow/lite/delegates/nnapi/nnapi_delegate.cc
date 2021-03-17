6ce48f04463a450a6674851dba1f23b91c1511e6
Lev Proleev <levp@google.com>
2021-03-17 09:50:35 -0700

    NNAPI delegate: add support for TransposeConv with bias
    PiperOrigin-RevId: 363441327
    Change-Id: I870ee0e98ab3902ae12055c2ad419c96aacb15ee

--- tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
@@ -2262,7 +2262,7 @@ bool NNAPIDelegateKernel::Validate(
       ExpectIsFloatOperator(context, node, &val_ctx);
     } break;
     case kTfLiteBuiltinTransposeConv: {
-      ExpectMaxOpVersion(version, 2, &val_ctx);
+      ExpectMaxOpVersion(version, 3, &val_ctx);
       ExpectMinAndroidSdkVersion(android_sdk_version, kMinSdkVersionForNNAPI12,
                                  &val_ctx);
       Expect((node->inputs->size > 1) &&
@@ -3100,60 +3100,70 @@ TfLiteStatus NNAPIDelegateKernel::Map(
           weight_tensor_id, hybrid_op,
           input_tensor_flags | NN_TENSOR_FLAG_FORCE_PER_CHANNEL);
 
-      // NNAPI requires a bias tensor, so we allocate a new tensor to fill
-      // it with zeroes. It is deleted with other tensors in the context
-      // during subgraph destructor call.
-      int bias_index = -1;
-      mapping_args.context->AddTensors(mapping_args.context, 1, &bias_index);
-      TfLiteTensor* bias_tensor = &mapping_args.context->tensors[bias_index];
-      const auto input_type =
+      const bool is_bias_present =
+          mapping_args.node->inputs->size == 4 &&
           mapping_args.context
-              ->tensors[mapping_args.node->inputs->data[/*kDataInputTensor*/ 2]]
-              .type;
-      if (input_type == kTfLiteFloat32) {
-        bias_tensor->type = kTfLiteFloat32;
+                  ->tensors[mapping_args.node->inputs->data[/*kBiasTensor*/ 3]]
+                  .type != -1;
+      if (is_bias_present) {
+        mapping_args.builder->AddTensorInput(
+            mapping_args.node->inputs->data[/*kBiasTensor*/ 3], hybrid_op);
       } else {
-        bias_tensor->type = kTfLiteInt32;
-      }
+        // NNAPI requires a bias tensor, so we allocate a new tensor to fill
+        // it with zeroes. It is deleted with other tensors in the context
+        // during subgraph destructor call.
+        int bias_index = -1;
+        mapping_args.context->AddTensors(mapping_args.context, 1, &bias_index);
+        TfLiteTensor* bias_tensor = &mapping_args.context->tensors[bias_index];
+        const auto input_type =
+            mapping_args.context
+                ->tensors[mapping_args.node->inputs
+                              ->data[/*kDataInputTensor*/ 2]]
+                .type;
+        if (input_type == kTfLiteFloat32) {
+          bias_tensor->type = kTfLiteFloat32;
+        } else {
+          bias_tensor->type = kTfLiteInt32;
+        }
 
-      // Create an array with a required bias shape and resize the bias
-      // tensor.
-      TfLiteIntArray* bias_shape = TfLiteIntArrayCreate(1);
-      const TfLiteTensor& output_shape =
-          mapping_args.context->tensors[mapping_args.node->inputs
-                                            ->data[/*kOutputShapeTensor*/ 0]];
-      const int output_depth = output_shape.data.i32[3];
-      bias_shape->data[0] = output_depth;
-      bias_tensor->allocation_type = kTfLiteDynamic;
-      mapping_args.context->ResizeTensor(mapping_args.context, bias_tensor,
-                                         bias_shape);
-
-      // Set tensor's values to zeroes and add it using AddVector*, so
-      // that the values are copied to NNAPI. We don't use the AddTensor
-      // function because it doesn't copy values and the tensor we just
-      // created is not in the node->inputs.
-      if (input_type == kTfLiteFloat32) {
-        memset(bias_tensor->data.f, 0, output_depth * sizeof(float));
-        mapping_args.builder->AddVectorFloat32Operand(bias_tensor->data.f,
-                                                      output_depth);
-      } else {
-        memset(bias_tensor->data.i32, 0, output_depth * sizeof(int));
-        const TfLiteTensor& input_tensor =
+        // Create an array with a required bias shape and resize the bias
+        // tensor.
+        TfLiteIntArray* bias_shape = TfLiteIntArrayCreate(1);
+        const TfLiteTensor& output_shape =
             mapping_args.context->tensors[mapping_args.node->inputs
-                                              ->data[/*kDataInputTensor*/ 2]];
-        const TfLiteTensor& filter_tensor =
-            mapping_args.context->tensors[mapping_args.node->inputs
-                                              ->data[/*kWeightsTensor*/ 1]];
-        // NNAPI requires bias scale to be a product of an input scale and
-        // a filter scale.
-        bias_tensor->params.scale =
-            input_tensor.params.scale * filter_tensor.params.scale;
-        mapping_args.builder->AddVectorInt32Operand(
-            bias_tensor->data.i32, output_depth,
-            input_tensor.params.scale * filter_tensor.params.scale,
-            /*zero_point=*/0);
-      }
+                                              ->data[/*kOutputShapeTensor*/ 0]];
+        const int output_depth = output_shape.data.i32[3];
+        bias_shape->data[0] = output_depth;
+        bias_tensor->allocation_type = kTfLiteDynamic;
+        mapping_args.context->ResizeTensor(mapping_args.context, bias_tensor,
+                                           bias_shape);
 
+        // Set tensor's values to zeroes and add it using AddVector*, so
+        // that the values are copied to NNAPI. We don't use the AddTensor
+        // function because it doesn't copy values and the tensor we just
+        // created is not in the node->inputs.
+        if (input_type == kTfLiteFloat32) {
+          memset(bias_tensor->data.f, 0, output_depth * sizeof(float));
+          mapping_args.builder->AddVectorFloat32Operand(bias_tensor->data.f,
+                                                        output_depth);
+        } else {
+          memset(bias_tensor->data.i32, 0, output_depth * sizeof(int));
+          const TfLiteTensor& input_tensor =
+              mapping_args.context->tensors[mapping_args.node->inputs
+                                                ->data[/*kDataInputTensor*/ 2]];
+          const TfLiteTensor& filter_tensor =
+              mapping_args.context->tensors[mapping_args.node->inputs
+                                                ->data[/*kWeightsTensor*/ 1]];
+          // NNAPI requires bias scale to be a product of an input scale and
+          // a filter scale.
+          bias_tensor->params.scale =
+              input_tensor.params.scale * filter_tensor.params.scale;
+          mapping_args.builder->AddVectorInt32Operand(
+              bias_tensor->data.i32, output_depth,
+              input_tensor.params.scale * filter_tensor.params.scale,
+              /*zero_point=*/0);
+        }
+      }
       mapping_args.builder->AddTensorInput(
           mapping_args.node->inputs->data[/*kOutputShapeTensor*/ 0], hybrid_op);
 
