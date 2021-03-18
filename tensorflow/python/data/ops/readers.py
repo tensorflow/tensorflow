4002c6c2bc4946a162236f3357ec54b4ab8b4e1e
Andrew Audibert <aaudibert@google.com>
2021-03-18 13:40:17 -0700

    Improve documentation for TFRecordDataset.
    This adds a longer description and includes the example used for TFRecordWriter (https://www.tensorflow.org/api_docs/python/tf/io/TFRecordWriter)
    
    PiperOrigin-RevId: 363734980
    Change-Id: I97e616bbcabae0e64b016d34bfbdba6996bd9ebc

--- tensorflow/python/data/ops/readers.py
+++ tensorflow/python/data/ops/readers.py
@@ -213,7 +213,48 @@ class TextLineDatasetV1(dataset_ops.DatasetV1Adapter):
 
 
 class _TFRecordDataset(dataset_ops.DatasetSource):
-  """A `Dataset` comprising records from one or more TFRecord files."""
+  """A `Dataset` comprising records from one or more TFRecord files.
+
+  This dataset loads TFRecords from file as bytes, exactly as they were written.
+  `TFRecordDataset` does not do any parsing or decoding on its own. Parsing and
+  decoding can be done by applying `Dataset.map` transformations after the
+  `TFRecordDataset`.
+
+  A minimal example is given below:
+
+  >>> import tempfile
+  >>> example_path = os.path.join(tempfile.gettempdir(), "example.tfrecords")
+  >>> np.random.seed(0)
+
+  >>> # Write the records to a file.
+  ... with tf.io.TFRecordWriter(example_path) as file_writer:
+  ...   for _ in range(4):
+  ...     x, y = np.random.random(), np.random.random()
+  ...
+  ...     record_bytes = tf.train.Example(features=tf.train.Features(feature={
+  ...         "x": tf.train.Feature(float_list=tf.train.FloatList(value=[x])),
+  ...         "y": tf.train.Feature(float_list=tf.train.FloatList(value=[y])),
+  ...     })).SerializeToString()
+  ...     file_writer.write(record_bytes)
+
+  >>> # Read the data back out.
+  >>> def decode_fn(record_bytes):
+  ...   return tf.io.parse_single_example(
+  ...       # Data
+  ...       record_bytes,
+  ...
+  ...       # Schema
+  ...       {"x": tf.io.FixedLenFeature([], dtype=tf.float32),
+  ...        "y": tf.io.FixedLenFeature([], dtype=tf.float32)}
+  ...   )
+
+  >>> for batch in tf.data.TFRecordDataset([example_path]).map(decode_fn):
+  ...   print("x = {x:.4f},  y = {y:.4f}".format(**batch))
+  x = 0.5488,  y = 0.7152
+  x = 0.6028,  y = 0.5449
+  x = 0.4237,  y = 0.6459
+  x = 0.4376,  y = 0.8918
+  """
 
   def __init__(self, filenames, compression_type=None, buffer_size=None):
     """Creates a `TFRecordDataset`.
