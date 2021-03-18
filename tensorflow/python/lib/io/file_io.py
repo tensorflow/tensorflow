2860aec5a925482e25d999e18cd41908423bfd3e
Mihai Maruseac <mihaimaruseac@google.com>
2021-03-18 16:12:41 -0700

    Add api documentation for `tf.io.gfile.exists`.
    PiperOrigin-RevId: 363769492
    Change-Id: I8e7107bf3e174951f47d4357e4251d40d9af14c9

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -233,26 +233,35 @@ class FileIO(object):
     return True
 
 
-@tf_export(v1=["gfile.Exists"])
-def file_exists(filename):
-  """Determines whether a path exists or not.
-
-  Args:
-    filename: string, a path
-
-  Returns:
-    True if the path exists, whether it's a file or a directory.
-    False if the path does not exist and there are no filesystem errors.
-
-  Raises:
-    errors.OpError: Propagates any errors reported by the FileSystem API.
-  """
-  return file_exists_v2(filename)
-
-
 @tf_export("io.gfile.exists")
 def file_exists_v2(path):
   """Determines whether a path exists or not.
+
+  >>> with open("/tmp/x", "w") as f:
+  ...   f.write("asdf")
+  ...
+  4
+  >>> tf.io.gfile.exists("/tmp/x")
+  True
+
+  You can also specify the URI scheme for selecting a different filesystem:
+
+  >>> # for a GCS filesystem path:
+  >>> # tf.io.gfile.exists("gs://bucket/file")
+  >>> # for a local filesystem:
+  >>> with open("/tmp/x", "w") as f:
+  ...   f.write("asdf")
+  ...
+  4
+  >>> tf.io.gfile.exists("file:///tmp/x")
+  True
+
+  This currently returns `True` for existing directories but don't rely on this
+  behavior, especially if you are using cloud filesystems (e.g., GCS, S3,
+  Hadoop):
+
+  >>> tf.io.gfile.exists("/tmp")
+  True
 
   Args:
     path: string, a path
@@ -269,6 +278,14 @@ def file_exists_v2(path):
   except errors.NotFoundError:
     return False
   return True
+
+
+@tf_export(v1=["gfile.Exists"])
+def file_exists(filename):
+  return file_exists_v2(filename)
+
+
+file_exists.__doc__ = file_exists_v2.__doc__
 
 
 @tf_export(v1=["gfile.Remove"])
