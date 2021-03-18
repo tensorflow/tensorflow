b6a25c7641ac37426b1dadd21b74f814c36f1738
A. Unique TensorFlower <gardener@tensorflow.org>
2021-03-17 17:57:48 -0700

    Internal change
    PiperOrigin-RevId: 363550120
    Change-Id: I5bb5909a0f30b4338e16b6eff10929579be6e35e

--- tensorflow/core/kernels/gpu_prim.h
+++ tensorflow/core/kernels/gpu_prim.h
@@ -14,8 +14,6 @@ limitations under the license, the license you must see.
 #ifndef TENSORFLOW_CORE_KERNELS_GPU_PRIM_H_
 #define TENSORFLOW_CORE_KERNELS_GPU_PRIM_H_
 
-#include "tensorflow/core/platform/bfloat16.h"
-
 #if GOOGLE_CUDA
 #include "cub/block/block_load.cuh"
 #include "cub/block/block_scan.cuh"
@@ -23,7 +21,6 @@ limitations under the license, the license you must see.
 #include "cub/device/device_histogram.cuh"
 #include "cub/device/device_radix_sort.cuh"
 #include "cub/device/device_reduce.cuh"
-#include "cub/device/device_scan.cuh"
 #include "cub/device/device_segmented_radix_sort.cuh"
 #include "cub/device/device_segmented_reduce.cuh"
 #include "cub/device/device_select.cuh"
@@ -34,47 +31,16 @@ limitations under the license, the license you must see.
 #include "third_party/gpus/cuda/include/cusparse.h"
 
 namespace gpuprim = ::cub;
-
-// Required for sorting Eigen::half and bfloat16.
-namespace cub {
-template <>
-__device__ __forceinline__ void ThreadStoreVolatilePtr<Eigen::half>(
-    Eigen::half *ptr, Eigen::half val, Int2Type<true> /*is_primitive*/) {
-  *reinterpret_cast<volatile uint16_t *>(ptr) =
-      Eigen::numext::bit_cast<uint16_t>(val);
-}
-
-template <>
-__device__ __forceinline__ Eigen::half ThreadLoadVolatilePointer<Eigen::half>(
-    Eigen::half *ptr, Int2Type<true> /*is_primitive*/) {
-  uint16_t result = *reinterpret_cast<volatile uint16_t *>(ptr);
-  return Eigen::numext::bit_cast<Eigen::half>(result);
-}
-
-template <>
-struct NumericTraits<Eigen::half>
-    : BaseTraits</*_CATEGORY=*/FLOATING_POINT, /*_PRIMITIVE=*/true,
-                 /*_NULL_TYPE=*/false, /*_UnsignedBits=*/uint16_t,
-                 /*T=*/Eigen::half> {};
-template <>
-struct NumericTraits<tensorflow::bfloat16>
-    : BaseTraits</*_CATEGORY=*/FLOATING_POINT, /*_PRIMITIVE=*/true,
-                 /*_NULL_TYPE=*/false, /*_UnsignedBits=*/uint16_t,
-                 /*T=*/tensorflow::bfloat16> {};
-}  // namespace cub
 #elif TENSORFLOW_USE_ROCM
 #include "rocm/include/hipcub/hipcub.hpp"
 namespace gpuprim = ::hipcub;
 
-// Required for sorting Eigen::half and bfloat16.
+// Required for sorting Eigen::half
 namespace rocprim {
 namespace detail {
 template <>
 struct radix_key_codec_base<Eigen::half>
     : radix_key_codec_floating<Eigen::half, uint16_t> {};
-template <>
-struct radix_key_codec_base<tensorflow::bfloat16>
-    : radix_key_codec_floating<tensorflow::bfloat16, uint16_t> {};
 };  // namespace detail
 };  // namespace rocprim
 
--- tensorflow/python/kernel_tests/unique_op_test.py
+++ tensorflow/python/kernel_tests/unique_op_test.py
@@ -152,24 +152,6 @@ class UniqueTest(test.TestCase):
     _, idx = gen_array_ops.unique_v2(x, axis=axis_t)
     self.assertEqual(idx.shape.as_list(), [None])
 
-  def testEmpty(self):
-    x = np.random.randint(2, size=0)
-    y, idx = array_ops.unique(x)
-    tf_y, tf_idx = self.evaluate([y, idx])
-
-    self.assertEqual(len(x), len(tf_idx))
-    self.assertEqual(len(tf_y), len(np.unique(x)))
-
-  def testOrderedByAppearance(self):
-    x = np.array([3, 5, 3, 4, 1, 4, 9, 8, 6, 3, 5, 7, 8, 8, 4, 6, 4, 2, 5, 6])
-    true_y = np.array([3, 5, 4, 1, 9, 8, 6, 7, 2])
-    true_idx = np.array(
-        [0, 1, 0, 2, 3, 2, 4, 5, 6, 0, 1, 7, 5, 5, 2, 6, 2, 8, 1, 6])
-    y, idx = array_ops.unique(x)
-    tf_y, tf_idx = self.evaluate([y, idx])
-    self.assertAllEqual(tf_y, true_y)
-    self.assertAllEqual(tf_idx, true_idx)
-
 
 class UniqueWithCountsTest(test.TestCase):
 
@@ -293,27 +275,6 @@ class UniqueWithCountsTest(test.TestCase):
         self.assertEqual(count, 1)
       else:
         self.assertEqual(count, np.sum(x == value))
-
-  def testEmpty(self):
-    x = np.random.randint(2, size=0)
-    y, idx, count = array_ops.unique_with_counts(x)
-    tf_y, tf_idx, tf_count = self.evaluate([y, idx, count])
-
-    self.assertEqual(tf_idx.shape, (0,))
-    self.assertEqual(tf_y.shape, (0,))
-    self.assertEqual(tf_count.shape, (0,))
-
-  def testOrderedByAppearance(self):
-    x = np.array([3, 5, 3, 4, 1, 4, 9, 8, 6, 3, 5, 7, 8, 8, 4, 6, 4, 2, 5, 6])
-    true_y = np.array([3, 5, 4, 1, 9, 8, 6, 7, 2])
-    true_idx = np.array(
-        [0, 1, 0, 2, 3, 2, 4, 5, 6, 0, 1, 7, 5, 5, 2, 6, 2, 8, 1, 6])
-    true_count = np.array([3, 3, 4, 1, 1, 3, 3, 1, 1])
-    y, idx, count = array_ops.unique_with_counts(x)
-    tf_y, tf_idx, tf_count = self.evaluate([y, idx, count])
-    self.assertAllEqual(tf_y, true_y)
-    self.assertAllEqual(tf_idx, true_idx)
-    self.assertAllEqual(tf_count, true_count)
 
 
 if __name__ == '__main__':
