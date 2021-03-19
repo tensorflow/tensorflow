5b5ae1e7b0fe051b08f96c92ae9379d00a37b2e8
Mihai Maruseac <mihaimaruseac@google.com>
2021-03-18 19:15:24 -0700

    Add api documentation for `tf.io.gfile.GFile.close()`.
    PiperOrigin-RevId: 363799320
    Change-Id: Ie42caf990a983caefd4774a805d3c227c6a7b760

--- tensorflow/python/lib/io/file_io.py
+++ tensorflow/python/lib/io/file_io.py
@@ -222,7 +222,21 @@ class FileIO(object):
       self._writable_file.flush()
 
   def close(self):
-    """Closes FileIO. Should be called for the WritableFile to be flushed."""
+    r"""Closes the file.
+
+    Should be called for the WritableFile to be flushed.
+
+    In general, if you use the context manager pattern, you don't need to call
+    this directly.
+
+    >>> with tf.io.gfile.GFile("/tmp/x", "w") as f:
+    ...   f.write("asdf\n")
+    ...   f.write("qwer\n")
+    >>> # implicit f.close() at the end of the block
+
+    For cloud filesystems, forgetting to call `close()` might result in data
+    loss as last write might not have been replicated.
+    """
     self._read_buf = None
     if self._writable_file:
       self._writable_file.close()
