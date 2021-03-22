67abdaed64fa2e5b834f5689f7e4629b83a4c592
Jun Xu <xjun@google.com>
2021-03-22 10:55:57 -0700

    Update docstring of tf.image.per_image_standardization.
    PiperOrigin-RevId: 364358269
    Change-Id: I957beb8af817b8a6ae39d41503382eeccb9ebd8c

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -1879,15 +1879,32 @@ def per_image_standardization(image):
     - `N` is the number of elements in `x`
     - `stddev` is the standard deviation of all values in `x`
 
+  Example Usage:
+
+  >>> image = tf.constant(np.arange(1, 13, dtype=np.int32), shape=[2, 2, 3])
+  >>> image # 3-D tensor
+  <tf.Tensor: shape=(2, 2, 3), dtype=int32, numpy=
+  array([[[ 1,  2,  3],
+          [ 4,  5,  6]],
+         [[ 7,  8,  9],
+          [10, 11, 12]]], dtype=int32)>
+  >>> new_image = tf.image.per_image_standardization(image)
+  >>> new_image # 3-D tensor with mean ~= 0 and variance ~= 1
+  <tf.Tensor: shape=(2, 2, 3), dtype=float32, numpy=
+  array([[[-1.593255  , -1.3035723 , -1.0138896 ],
+          [-0.7242068 , -0.4345241 , -0.14484136]],
+         [[ 0.14484136,  0.4345241 ,  0.7242068 ],
+          [ 1.0138896 ,  1.3035723 ,  1.593255  ]]], dtype=float32)>
+
   Args:
-    image: An n-D Tensor with at least 3 dimensions, the last 3 of which are the
-      dimensions of each image.
+    image: An n-D `Tensor` with at least 3 dimensions, the last 3 of which are
+      the dimensions of each image.
 
   Returns:
-    A `Tensor` with the same shape as `image`.
+    A `Tensor` with the same shape as `image` and its dtype is `float32`.
 
   Raises:
-    ValueError: if the shape of 'image' is incompatible with this function.
+    ValueError: The shape of `image` has fewer than 3 dimensions.
   """
   with ops.name_scope(None, 'per_image_standardization', [image]) as scope:
     image = ops.convert_to_tensor(image, name='image')
