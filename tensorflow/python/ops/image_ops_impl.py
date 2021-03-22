e8c51ba46d01f6ff52f70aeeba8e65e09bddecf6
Jun Xu <xjun@google.com>
2021-03-22 10:59:42 -0700

    Update the docstring of tf.image.crop_to_bounding_box.
    PiperOrigin-RevId: 364359246
    Change-Id: I6588b43f6b516188b87b8e05eadd7da900537fef

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -1105,33 +1105,50 @@ def pad_to_bounding_box(image, offset_height, offset_width, target_height,
 @dispatch.add_dispatch_support
 def crop_to_bounding_box(image, offset_height, offset_width, target_height,
                          target_width):
-  """Crops an image to a specified bounding box.
+  """Crops an `image` to a specified bounding box.
 
-  This op cuts a rectangular part out of `image`. The top-left corner of the
-  returned image is at `offset_height, offset_width` in `image`, and its
+  This op cuts a rectangular bounding box out of `image`. The top-left corner
+  of the bounding box is at `offset_height, offset_width` in `image`, and the
   lower-right corner is at
   `offset_height + target_height, offset_width + target_width`.
 
+  Example Usage:
+
+  >>> image = tf.constant(np.arange(1, 28, dtype=np.float32), shape=[3, 3, 3])
+  >>> image[:,:,0] # print the first channel of the 3-D tensor
+  <tf.Tensor: shape=(3, 3), dtype=float32, numpy=
+  array([[ 1.,  4.,  7.],
+         [10., 13., 16.],
+         [19., 22., 25.]], dtype=float32)>
+  >>> cropped_image = tf.image.crop_to_bounding_box(image, 0, 0, 2, 2)
+  >>> cropped_image[:,:,0] # print the first channel of the cropped 3-D tensor
+  <tf.Tensor: shape=(2, 2), dtype=float32, numpy=
+  array([[ 1.,  4.],
+         [10., 13.]], dtype=float32)>
+
   Args:
-    image: 4-D Tensor of shape `[batch, height, width, channels]` or 3-D Tensor
-      of shape `[height, width, channels]`.
-    offset_height: Vertical coordinate of the top-left corner of the result in
-      the input.
-    offset_width: Horizontal coordinate of the top-left corner of the result in
-      the input.
-    target_height: Height of the result.
-    target_width: Width of the result.
+    image: 4-D `Tensor` of shape `[batch, height, width, channels]` or 3-D
+      `Tensor` of shape `[height, width, channels]`.
+    offset_height: Vertical coordinate of the top-left corner of the bounding
+      box in `image`.
+    offset_width: Horizontal coordinate of the top-left corner of the bounding
+      box in `image`.
+    target_height: Height of the bounding box.
+    target_width: Width of the bounding box.
 
   Returns:
-    If `image` was 4-D, a 4-D float Tensor of shape
-    `[batch, target_height, target_width, channels]`
-    If `image` was 3-D, a 3-D float Tensor of shape
-    `[target_height, target_width, channels]`
+    If `image` was 4-D, a 4-D `Tensor` of shape
+    `[batch, target_height, target_width, channels]`.
+    If `image` was 3-D, a 3-D `Tensor` of shape
+    `[target_height, target_width, channels]`.
+    It has the same dtype with `image`.
 
   Raises:
-    ValueError: If the shape of `image` is incompatible with the `offset_*` or
-      `target_*` arguments, or either `offset_height` or `offset_width` is
-      negative, or either `target_height` or `target_width` is not positive.
+    ValueError: `image` is not a 3-D or 4-D `Tensor`.
+    ValueError: `offset_width < 0` or `offset_height < 0`.
+    ValueError: `target_width <= 0` or `target_width <= 0`.
+    ValueError: `width < offset_width + target_width` or
+      `height < offset_height + target_height`.
   """
   with ops.name_scope(None, 'crop_to_bounding_box', [image]):
     image = ops.convert_to_tensor(image, name='image')
