7c5ab109598621765f8491127b22f004b28efd66
Patrik Laurell <patrik.laurell@arm.com>
2021-03-23 17:43:24 +0100

    Change int32 to int32_t in resize_bilinear.h

--- tensorflow/lite/kernels/internal/reference/resize_bilinear.h
+++ tensorflow/lite/kernels/internal/reference/resize_bilinear.h
@@ -116,15 +116,16 @@ inline void ResizeBilinear(const tflite::ResizeBilinearParams& op_params,
 }
 
 inline void ComputeInterpolationValuesInteger(
-    const int32 value, const int32 scale_10, const bool half_pixel_centers,
-    int32 input_size, int32* scaled_value, int32* lower_bound,
-    int32* upper_bound) {
+    const int32_t value, const int32_t scale_10, const bool half_pixel_centers,
+    int32_t input_size, int32_t* scaled_value, int32_t* lower_bound,
+    int32_t* upper_bound) {
   if (half_pixel_centers) {
     *scaled_value = value * scale_10 + scale_10 / 2 - (1 << 9);
   } else {
     *scaled_value = value * scale_10;
   }
-  *lower_bound = std::max(*scaled_value / (1 << 10), 0);
+  constexpr int32_t zero = 0;
+  *lower_bound = std::max(*scaled_value / (1 << 10), zero);
   *upper_bound =
       std::min((*scaled_value + (1 << 10) - 1) / (1 << 10), input_size - 1);
 }
@@ -135,8 +136,8 @@ inline void ResizeBilinearInteger(
     const tflite::ResizeBilinearParams& op_params,
     const RuntimeShape& unextended_input_shape, const T* input_data,
     const RuntimeShape& unextended_output_size_shape,
-    const int32* output_size_data, const RuntimeShape& unextended_output_shape,
-    T* output_data) {
+    const int32_t* output_size_data,
+    const RuntimeShape& unextended_output_shape, T* output_data) {
   // If half_pixel_centers is True, align_corners must be False.
   TFLITE_DCHECK(!op_params.half_pixel_centers || !op_params.align_corners);
   TFLITE_DCHECK_LE(unextended_input_shape.DimensionsCount(), 4);
@@ -149,23 +150,23 @@ inline void ResizeBilinearInteger(
   const RuntimeShape output_shape =
       RuntimeShape::ExtendedShape(4, unextended_output_shape);
 
-  const int32 batches = MatchingDim(input_shape, 0, output_shape, 0);
-  const int32 input_height = input_shape.Dims(1);
-  const int32 input_width = input_shape.Dims(2);
-  const int32 depth = MatchingDim(input_shape, 3, output_shape, 3);
+  const int32_t batches = MatchingDim(input_shape, 0, output_shape, 0);
+  const int32_t input_height = input_shape.Dims(1);
+  const int32_t input_width = input_shape.Dims(2);
+  const int32_t depth = MatchingDim(input_shape, 3, output_shape, 3);
 
   TFLITE_DCHECK_EQ(output_size_shape.Dims(0), 1);
   TFLITE_DCHECK_EQ(output_size_shape.Dims(1), 1);
   TFLITE_DCHECK_EQ(output_size_shape.Dims(2), 1);
   TFLITE_DCHECK_EQ(output_size_shape.Dims(3), 2);
-  const int32 output_height =
+  const int32_t output_height =
       output_size_data[Offset(output_size_shape, 0, 0, 0, 0)];
-  const int32 output_width =
+  const int32_t output_width =
       output_size_data[Offset(output_size_shape, 0, 0, 0, 1)];
 
-  int32 height_scale_10 =
+  int32_t height_scale_10 =
       ((1 << 10) * input_height + output_height / 2) / output_height;
-  int32 width_scale_10 =
+  int32_t width_scale_10 =
       ((1 << 10) * input_width + output_width / 2) / output_width;
   if (op_params.align_corners && output_height > 1) {
     height_scale_10 =
@@ -179,12 +180,12 @@ inline void ResizeBilinearInteger(
 
   for (int b = 0; b < batches; ++b) {
     for (int y = 0; y < output_height; ++y) {
-      int32 input_y, y0, y1;
+      int32_t input_y, y0, y1;
       ComputeInterpolationValuesInteger(y, height_scale_10,
                                         op_params.half_pixel_centers,
                                         input_height, &input_y, &y0, &y1);
       for (int x = 0; x < output_width; ++x) {
-        int32 input_x, x0, x1;
+        int32_t input_x, x0, x1;
         ComputeInterpolationValuesInteger(x, width_scale_10,
                                           op_params.half_pixel_centers,
                                           input_width, &input_x, &x0, &x1);
