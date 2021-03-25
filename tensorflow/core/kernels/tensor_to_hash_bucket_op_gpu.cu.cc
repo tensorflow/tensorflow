3f613de2b684e91862643b3f39b61b59ac3aa7cc
Kaixi Hou <kaixih@nvidia.com>
2021-03-25 11:08:47 -0700

    Minor changes

--- tensorflow/core/kernels/tensor_to_hash_bucket_op_gpu.cu.cc
+++ tensorflow/core/kernels/tensor_to_hash_bucket_op_gpu.cu.cc
@@ -23,27 +23,28 @@ namespace tensorflow {
 
 namespace {
 
-constexpr int BUFFER_SIZE = 64;
+constexpr int kSharedMemBufferSizePerThread = 64;
 
 template<typename T>
-__device__ void FillDigits(T val, int num_digits, int* i, char *buf) {
-  eigen_assert(num_digits <= BUFFER_SIZE - (*i));
+__device__ __forceinline__ void FillDigits(T val, int num_digits, int* i,
+                                           char *buf) {
+  eigen_assert(num_digits <= kSharedMemBufferSizePerThread - (*i));
 
   int factor = (val < 0 ? -1: 1);
 
   int num_digits_a = num_digits;
-  while (num_digits > 0) {
+  do {
     int digit = static_cast<int>((val % 10) * factor);
     buf[(*i) + num_digits - 1] = digit + '0';
     val /= 10;
     num_digits--;
-  }
+  } while(val != 0);
 
   (*i) += num_digits_a;
 }
 
 template<typename T>
-__device__ int IntegerToString(T val, char *buf) {
+__device__ __forceinline__ int IntegerToString(T val, char *buf) {
   int num_digits = 0;
   T val_a = val;
   do {
@@ -67,9 +68,10 @@ __global__ void ComputeHashes(const T* __restrict__ vals, int vals_size,
   extern __shared__ char s[];
 
   GPU_1D_KERNEL_LOOP(tid, vals_size) {
-    int size = IntegerToString(vals[tid], s + threadIdx.x * BUFFER_SIZE);
-    uint64_t a_hash = ::util_gpu::Fingerprint64(s + threadIdx.x * BUFFER_SIZE,
-                                                size);
+    int size = IntegerToString(
+        vals[tid], s + threadIdx.x * kSharedMemBufferSizePerThread);
+    uint64_t a_hash = ::util_gpu::Fingerprint64(
+        s + threadIdx.x * kSharedMemBufferSizePerThread, size);
     int64 a_bucket = static_cast<int64>(a_hash % num_buckets);
     hashes[tid] = a_bucket;
   }
@@ -85,10 +87,11 @@ void LaunchTensorToHashBucket<Eigen::GpuDevice, T>::operator()(
          const int num_elems, int64* output) {
   const Eigen::GpuDevice& d = c->eigen_gpu_device();
   if (num_elems > 0) {
-    constexpr int32 kThreadInBlock = 128;
-    auto shared_memory_bytes = kThreadInBlock * BUFFER_SIZE * sizeof(char);
+    constexpr int32 kThreadsInBlock = 128;
+    auto shared_memory_bytes =
+        kThreadsInBlock * kSharedMemBufferSizePerThread * sizeof(char);
     GpuLaunchConfig config = GetGpuLaunchConfigFixedBlockSize(
-        num_elems, d, ComputeHashes<T>, shared_memory_bytes, kThreadInBlock);
+        num_elems, d, ComputeHashes<T>, shared_memory_bytes, kThreadsInBlock);
     OP_REQUIRES_OK(c, GpuLaunchKernel(
         ComputeHashes<T>, config.block_count, config.thread_per_block,
         shared_memory_bytes, d.stream(), input, num_elems, num_buckets,
