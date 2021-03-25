9325d4158d3002aeb0348b246e36e0d95280fd86
Ran Chen <crccw@google.com>
2021-03-24 17:31:58 -0700

    [retry]Add error message when MultiWorkerMirroredStrategy is copied in eager
    Estimator copies the strategy, but MultiWorkerMirroredStrategy cannot be copied
    once any collective runs. In eager collectives happen immediately after the
    creation of the strategy because of the check alive mechanism.
    
    PiperOrigin-RevId: 364927741
    Change-Id: I5a54afa6d8d47e61f5e0f0557554e3967839f21d

--- tensorflow/python/distribute/collective_all_reduce_strategy.py
+++ tensorflow/python/distribute/collective_all_reduce_strategy.py
@@ -508,8 +508,10 @@ class CollectiveAllReduceExtended(mirrored_strategy.MirroredExtended):
     self._rpc_layer = cluster_resolver.rpc_layer
     self._warn_nccl_no_gpu()
 
-    if self._enable_check_health:
+    if self._enable_check_health and context.executing_eagerly():
       self._start_check_health_thread()
+    else:
+      logging.info("Check health not enabled.")
 
     logging.info(
         "MultiWorkerMirroredStrategy with cluster_spec = %r, task_type = %r, "
@@ -841,9 +843,6 @@ class CollectiveAllReduceExtended(mirrored_strategy.MirroredExtended):
       time.sleep(self._check_health_interval)
 
   def _start_check_health_thread(self):
-    if not context.executing_eagerly():
-      logging.info("Check health is only supported in eager.")
-      return
     # Use a dummy all-reduce as a barrier to wait for all workers to be up,
     # otherwise the check health may fail immediately.
 
@@ -936,3 +935,20 @@ class CollectiveAllReduceExtended(mirrored_strategy.MirroredExtended):
   def _get_local_replica_id(self, replica_id_in_sync_group):
     return (replica_id_in_sync_group -
             self._id_in_cluster * len(self.worker_devices))
+
+  def __deepcopy__(self, memo):
+    # We check the check health thread instead of whether we are in eager mode
+    # to limit the backward incompatibility.
+    if hasattr(self, "_check_health_thread"):
+      raise ValueError(
+          "MultiWorkerMirroredStrategy cannot be deep copied in eager mode. "
+          "If you're using Estimator and see this error message, call "
+          "tf.compat.v1.disable_eager_execution() at the beginning of your "
+          "program")
+    # Otherwise, do a regular deepcopy.
+    cls = self.__class__
+    result = cls.__new__(cls)
+    memo[id(self)] = result
+    for k, v in self.__dict__.items():
+      setattr(result, k, copy.deepcopy(v, memo))
+    return result
