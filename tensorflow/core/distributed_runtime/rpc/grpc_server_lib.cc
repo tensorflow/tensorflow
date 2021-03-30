3f72e3946a0d31f30ffa93908eea6591c97bed9c
Haitang Hu <hthu@google.com>
2021-03-29 23:33:49 -0700

    Replace StaticDeviceMgr with DynamicDeviceMgr in worker_env.
    PiperOrigin-RevId: 365746672
    Change-Id: I3ea7c37acdfd071c22acf1880b2eebb4957d27d1

--- tensorflow/core/distributed_runtime/rpc/grpc_server_lib.cc
+++ tensorflow/core/distributed_runtime/rpc/grpc_server_lib.cc
@@ -190,7 +190,7 @@ Status GrpcServer::Init(const GrpcServerOptions& opts) {
     std::vector<std::unique_ptr<Device>> devices;
     TF_RETURN_IF_ERROR(
         DeviceFactory::AddDevices(sess_opts, name_prefix, &devices));
-    worker_env_.device_mgr = new StaticDeviceMgr(std::move(devices));
+    worker_env_.device_mgr = new DynamicDeviceMgr(std::move(devices));
     owned_device_manager_.reset(worker_env_.device_mgr);
   } else {
     worker_env_.device_mgr = opts.local_device_mgr;
