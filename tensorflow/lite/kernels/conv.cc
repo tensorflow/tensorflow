971887c5408768ff727081a8dcffb060496333c3
David Rim <davidrim@google.com>
2021-03-29 18:36:57 -0700

    ConvParams takes dilation factors from TfLiteConvParams
    PiperOrigin-RevId: 365711672
    Change-Id: I066f2475de10181772e203dc49c12c801e4732a3

--- tensorflow/lite/kernels/conv.cc
+++ tensorflow/lite/kernels/conv.cc
@@ -936,8 +936,8 @@ TfLiteStatus EvalHybridPerChannel(TfLiteContext* context, TfLiteNode* node,
   op_params.padding_values.height = data->padding.height;
   op_params.stride_width = params->stride_width;
   op_params.stride_height = params->stride_height;
-  op_params.dilation_width_factor = 1;
-  op_params.dilation_height_factor = 1;
+  op_params.dilation_width_factor = params->dilation_width_factor;
+  op_params.dilation_height_factor = params->dilation_height_factor;
   op_params.float_activation_min = output_activation_min;
   op_params.float_activation_max = output_activation_max;
   switch (effective_kernel_type) {
