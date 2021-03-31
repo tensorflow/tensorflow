0bb8e486a9713683c0f622a9448d2517dbd82124
Fergus Henderson <fergus@google.com>
2021-03-31 13:44:12 -0700

    Add SetNumThreads method to InterpreterBuilder, and deprecate the overload of operator() that takes a num_threads parameter.
    Rationale: 'num_threads' is just one of many possible parameters, no
    different than other builder parameters such as delegates (set via
    the AddDelegate method), the preserve_all_tensors boolean (set by the
    PreserveAllTensorsExperimental method), and others that we may add in
    future, and shouldn't be singled out and handled differently than the
    others by being a parameter to operator().
    PiperOrigin-RevId: 366107281
    Change-Id: I601d6860253a86808036a4344c8d0acab2608506

--- tensorflow/lite/model_test.cc
+++ tensorflow/lite/model_test.cc
@@ -300,6 +300,41 @@ TEST(BasicFlatBufferModel, TestWithNumThreads) {
                       reporter.error_messages());
 }
 
+TEST(BasicFlatBufferModel, TestSetNumThreads) {
+  TestErrorReporter reporter;
+  auto model = FlatBufferModel::BuildFromFile(
+      "tensorflow/lite/testdata/test_model.bin", &reporter);
+  ASSERT_TRUE(model);
+  std::unique_ptr<Interpreter> interpreter;
+  TrivialResolver resolver(&dummy_reg);
+  InterpreterBuilder builder(*model, resolver);
+
+  ASSERT_EQ(builder.SetNumThreads(42), kTfLiteOk);
+  interpreter.reset();
+  ASSERT_EQ(builder(&interpreter), kTfLiteOk);
+  ASSERT_NE(interpreter, nullptr);
+
+  ASSERT_EQ(builder.SetNumThreads(0), kTfLiteOk);
+  interpreter.reset();
+  ASSERT_EQ(builder(&interpreter), kTfLiteOk);
+  ASSERT_NE(interpreter, nullptr);
+
+  ASSERT_EQ(builder.SetNumThreads(-1), kTfLiteOk);
+  interpreter.reset();
+  ASSERT_EQ(builder(&interpreter), kTfLiteOk);
+  ASSERT_NE(interpreter, nullptr);
+
+  ASSERT_EQ(reporter.num_calls(), 0);
+  ASSERT_EQ(builder.SetNumThreads(-2), kTfLiteError);
+  interpreter.reset();
+  ASSERT_EQ(builder(&interpreter), kTfLiteOk);
+  ASSERT_NE(interpreter, nullptr);
+  ASSERT_EQ(reporter.num_calls(), 1);
+  ASSERT_PRED_FORMAT2(testing::IsSubstring,
+                      "num_threads should be >= 0 or just -1",
+                      reporter.error_messages());
+}
+
 // Test that loading a model with TensorFlow ops fails when the flex delegate is
 // not linked into the target.
 TEST(FlexModel, FailureWithoutFlexDelegate) {
