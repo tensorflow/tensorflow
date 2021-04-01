dd6240a0def102a7c49f733012cdddbc86cd4c8d
Chris Jones <cjfj@google.com>
2021-04-01 09:22:24 -0700

    [XLA:GPU] Add `AllGatherCombiner` pass.
    Reduce dispatch latency and allow NCCL to group all-gathers together.
    
    PiperOrigin-RevId: 366265440
    Change-Id: I7f846f58de86111aaf50db7bdfac72bc56fbfa1c

--- tensorflow/compiler/xla/service/all_gather_combiner.cc
+++ tensorflow/compiler/xla/service/all_gather_combiner.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/compiler/xla/service/all_reduce_combiner.h"
+#include "tensorflow/compiler/xla/service/all_gather_combiner.h"
 
 #include <algorithm>
 #include <list>
@@ -44,47 +44,44 @@ limitations under the License.
 namespace xla {
 namespace {
 
-// Combines the elements of to_combine into a single AllReduce op. All
-// entries in to_combine must be AllReduce ops with exactly one operand
-// and the same reduction operation.
-Status CombineAllReduces(absl::Span<HloInstruction* const> to_combine) {
+// Combines the elements of to_combine into a single AllGather op. All entries
+// in to_combine must be AllGather ops with exactly one operand and the same
+// all_gather_dimension.
+Status CombineAllGathers(absl::Span<HloInstruction* const> to_combine) {
   if (to_combine.size() < 2) {
     return Status::OK();
   }
-  VLOG(1) << "Combined " << to_combine.size() << " CRS ops";
+  VLOG(1) << "Combined " << to_combine.size() << " AllGather ops";
 
   HloComputation& computation = *to_combine.back()->parent();
-  HloComputation* reduction = to_combine[0]->to_apply();
-  const HloOpcode type = reduction->root_instruction()->opcode();
+  int64_t all_gather_dimension =
+      Cast<HloAllGatherInstruction>(to_combine.front())->all_gather_dimension();
 
-  // Create a single bigger AllReduce of the operands of the smaller
-  // AllReduces.
+  // Create a single bigger AllGather of the operands of the smaller AllGather.
   std::vector<HloInstruction*> operands;
-  std::vector<Shape> operand_shapes;
+  std::vector<Shape> output_shapes;
   VLOG(1) << "Combining set";
   for (HloInstruction* hlo : to_combine) {
     VLOG(1) << "Set element: " << hlo->ToString();
-    TF_RET_CHECK(hlo->opcode() == HloOpcode::kAllReduce);
+    TF_RET_CHECK(hlo->opcode() == HloOpcode::kAllGather);
     TF_RET_CHECK(hlo->operands().size() == 1);
-    TF_RET_CHECK(hlo->to_apply() == reduction ||
-                 (hlo->to_apply()->instruction_count() == 3 &&
-                  hlo->to_apply()->num_parameters() == 2 &&
-                  hlo->to_apply()->root_instruction()->opcode() == type));
+    TF_RET_CHECK(Cast<HloAllGatherInstruction>(hlo)->all_gather_dimension() ==
+                 all_gather_dimension);
     TF_RET_CHECK(hlo->shape().IsArray());
     for (HloInstruction* operand : hlo->operands()) {
       operands.push_back(operand);
-      operand_shapes.push_back(operand->shape());
+      output_shapes.push_back(hlo->shape());
     }
   }
 
   HloInstruction* combined;
-  // AllReduce ops with more than one operand produce a tuple.
+  // AllGather ops with more than one operand produce a tuple.
   TF_RET_CHECK(operands.size() >= 2);
-  combined = computation.AddInstruction(HloInstruction::CreateAllReduce(
-      ShapeUtil::MakeTupleShape(operand_shapes), operands, reduction,
+  combined = computation.AddInstruction(HloInstruction::CreateAllGather(
+      ShapeUtil::MakeTupleShape(output_shapes), operands, all_gather_dimension,
       to_combine.front()->replica_groups(),
       /*constrain_layout=*/false, to_combine.front()->channel_id(),
-      Cast<HloAllReduceInstruction>(to_combine.front())
+      Cast<HloAllGatherInstruction>(to_combine.front())
           ->use_global_device_ids()));
 
   // We have to propagate the sharding manually because Domain instructions are
@@ -94,9 +91,9 @@ Status CombineAllReduces(absl::Span<HloInstruction* const> to_combine) {
   }
   VLOG(1) << "Replacing with : " << combined->ToString();
 
-  // Replace all the smaller AllReduces with elements of the tuple output
-  // of the single bigger AllReduce.
-  for (int64 i = 0; i < to_combine.size(); ++i) {
+  // Replace all the smaller AllGathers with elements of the tuple output
+  // of the single bigger AllGather.
+  for (int64_t i = 0; i < to_combine.size(); ++i) {
     auto replace_with = HloInstruction::CreateGetTupleElement(
         to_combine[i]->shape(), combined, i);
     TF_RETURN_IF_ERROR(computation.ReplaceWithNewInstruction(
@@ -105,64 +102,60 @@ Status CombineAllReduces(absl::Span<HloInstruction* const> to_combine) {
   return Status::OK();
 }
 
-// The group key encapsulates all of the properties which must match for it
-// to be possible to combine the instructions.
-using GroupKey = std::tuple<HloOpcode, PrimitiveType, int64_t, bool, bool,
-                            std::vector<std::vector<int64_t>>>;
+// The group key encapsulates all of the properties which must match for it to
+// be possible to combine the instructions.
+using GroupKey =
+    std::tuple<int64_t, int64_t, bool, bool, std::vector<std::vector<int64_t>>>;
 
 // Returns a key that will be equal for instructions that might be combined, or
 // different if not.
 absl::optional<GroupKey> CombineKey(const HloInstruction* instruction,
                                     const HloDomainMap& domain_map) {
-  if (instruction->opcode() != HloOpcode::kAllReduce) {
+  if (instruction->opcode() != HloOpcode::kAllGather) {
     return absl::nullopt;
   }
 
-  if (instruction->to_apply()->instruction_count() != 3 ||
-      instruction->to_apply()->num_parameters() != 2) {
-    VLOG(1) << "Skipping due to non-trivial reduction function.";
-    return absl::nullopt;
-  }
-
-  const auto* ar = Cast<HloAllReduceInstruction>(instruction);
+  const auto* ag = Cast<HloAllGatherInstruction>(instruction);
 
   std::vector<std::vector<int64_t>> replica_groups;
-  replica_groups.reserve(ar->replica_groups().size());
-  for (const ReplicaGroup& replica_group : ar->replica_groups()) {
+  replica_groups.reserve(ag->replica_groups().size());
+  for (const ReplicaGroup& replica_group : ag->replica_groups()) {
     replica_groups.push_back(
         std::vector<int64_t>(replica_group.replica_ids().begin(),
                              replica_group.replica_ids().end()));
   }
 
-  const HloInstruction* to_apply_root = ar->to_apply()->root_instruction();
-  return GroupKey{to_apply_root->opcode(),
-                  to_apply_root->shape().element_type(),
-                  domain_map.GetDomainMetadataId(ar),
-                  ar->channel_id().has_value(),
-                  ar->use_global_device_ids(),
+  return GroupKey{ag->all_gather_dimension(),
+                  domain_map.GetDomainMetadataId(ag),
+                  ag->channel_id().has_value(), ag->use_global_device_ids(),
                   replica_groups};
 }
 
 }  // namespace
 
-AllReduceCombiner::AllReduceCombiner(int64 combine_threshold_in_bytes,
-                                     int64 combine_threshold_count)
+AllGatherCombiner::AllGatherCombiner(int64_t combine_threshold_in_bytes,
+                                     int64_t combine_threshold_count)
     : combine_threshold_in_bytes_(combine_threshold_in_bytes),
       combine_threshold_count_(combine_threshold_count) {}
 
-StatusOr<bool> AllReduceCombiner::Run(HloModule* module) {
-  VLOG(1) << "Running AllReduceCombiner with threshold of "
+StatusOr<bool> AllGatherCombiner::Run(HloModule* module) {
+  VLOG(1) << "Running AllGatherCombiner with threshold of "
           << combine_threshold_in_bytes_ << " bytes";
 
   if (combine_threshold_in_bytes_ <= 0 || combine_threshold_count_ <= 0) {
-    VLOG(1) << "Skip AllReduceCombiner because the threshold is zero";
+    VLOG(1) << "Skip AllGatherCombiner because the threshold is zero";
     return false;
   }
 
-  if (hlo_query::ContainsLayoutConstrainedAllReduce(*module)) {
-    VLOG(1) << "Skip AllReduceCombiner because the module contains all-reduce "
-               "with constrained layouts";
-    return false;
+  for (HloComputation* computation : module->computations()) {
+    for (HloInstruction* hlo : computation->instructions()) {
+      if (hlo->opcode() == HloOpcode::kAllGather &&
+          Cast<HloAllGatherInstruction>(hlo)->constrain_layout()) {
+        VLOG(1) << "Skip AllGatherCombiner because the module contains "
+                   "all-gather with constrained layouts";
+        return false;
+      }
+    }
   }
 
   bool changed = false;
@@ -176,7 +169,7 @@ StatusOr<bool> AllReduceCombiner::Run(HloModule* module) {
     TF_ASSIGN_OR_RETURN(
         bool computation_changed,
         CombineInstructionsByKey<GroupKey>(
-            computation, key_fn, &CombineAllReduces,
+            computation, key_fn, &CombineAllGathers,
             combine_threshold_in_bytes_, combine_threshold_count_));
     changed |= computation_changed;
   }
