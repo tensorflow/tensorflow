ff8941b11765bde81a46b9e41a2e10584f54a247
A. Unique TensorFlower <gardener@tensorflow.org>
2021-04-07 02:01:56 -0700

    Update GraphDef version to 729.
    PiperOrigin-RevId: 367176582
    Change-Id: I2e0ffc90263638fd2b86de97b4bbaecc4c9c72bf

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 728  // Updated: 2021/4/6
+#define TF_GRAPH_DEF_VERSION 729  // Updated: 2021/4/7
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
