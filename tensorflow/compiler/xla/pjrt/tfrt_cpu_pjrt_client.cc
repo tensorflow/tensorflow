ce4d7f147f1f81346cd7e498c3165cacfe0e131e
Qiao Zhang <zhangqiaorjc@google.com>
2021-04-13 11:22:29 -0700

    Add TfrtCpuClient.
    Introduce a new PJRT CPU backend based on TFRT (2).
    
    PiperOrigin-RevId: 368256862
    Change-Id: Iea691a87ac8dc8af6df3f56f9f4f7b636c28a04c

--- tensorflow/compiler/xla/pjrt/tfrt_cpu_pjrt_client.cc
+++ tensorflow/compiler/xla/pjrt/tfrt_cpu_pjrt_client.cc
@@ -15,9 +15,39 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/pjrt/tfrt_cpu_pjrt_client.h"
 
+#include <memory>
+
+#define EIGEN_USE_THREADS
+
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_map.h"
 #include "absl/strings/string_view.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/types/optional.h"
+#include "absl/types/span.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/compiler/xla/client/executable_build_options.h"
+#include "tensorflow/compiler/xla/client/xla_computation.h"
+#include "tensorflow/compiler/xla/layout.h"
+#include "tensorflow/compiler/xla/literal.h"
+#include "tensorflow/compiler/xla/pjrt/pjrt_client.h"
+#include "tensorflow/compiler/xla/pjrt/semaphore.h"
+#include "tensorflow/compiler/xla/pjrt/utils.h"
+#include "tensorflow/compiler/xla/pjrt/worker_thread.h"
+#include "tensorflow/compiler/xla/service/buffer_assignment.h"
+#include "tensorflow/compiler/xla/service/computation_placer.h"
+#include "tensorflow/compiler/xla/service/cpu/cpu_executable.h"
 #include "tensorflow/compiler/xla/service/cpu/cpu_xfeed.h"
-#include "tensorflow/core/platform/env.h"
+#include "tensorflow/compiler/xla/service/executable.h"
+#include "tensorflow/compiler/xla/service/hlo_cost_analysis.h"
+#include "tensorflow/compiler/xla/shape.h"
+#include "tensorflow/compiler/xla/statusor.h"
+#include "tensorflow/compiler/xla/xla_data.pb.h"
+#include "tfrt/host_context/async_value_ref.h"  // from @tf_runtime
+#include "tfrt/host_context/concurrent_work_queue.h"  // from @tf_runtime
+#include "tfrt/host_context/host_allocator.h"  // from @tf_runtime
+#include "tfrt/host_context/host_context.h"  // from @tf_runtime
+#include "tfrt/support/forward_decls.h"  // from @tf_runtime
 
 namespace xla {
 
@@ -42,6 +72,112 @@ Status TfrtCpuDevice::TransferToInfeed(const LiteralSlice& literal) {
 
 Status TfrtCpuDevice::TransferFromOutfeed(MutableBorrowingLiteral literal) {
   return TransferLiteralFromOutfeedOnCpu(local_hardware_id(), literal);
+}
+
+static int CpuDeviceCount() {
+  // By default we fix the number of devices to one.  However we do let the user
+  // override this behavior to help run tests on the host that run models in
+  // parallel across multiple devices, e.g. pmap.
+  return GetDebugOptionsFromFlags().xla_force_host_platform_device_count();
+}
+
+static StatusOr<std::vector<std::unique_ptr<TfrtCpuDevice>>> GetTfrtCpuDevices(
+    bool asynchronous) {
+  std::vector<std::unique_ptr<TfrtCpuDevice>> devices;
+  for (int i = 0; i < CpuDeviceCount(); ++i) {
+    auto device = std::make_unique<TfrtCpuDevice>(
+        /*id=*/i, asynchronous);
+    devices.push_back(std::move(device));
+  }
+  return std::move(devices);
+}
+
+StatusOr<std::unique_ptr<PjRtClient>> GetTfrtCpuClient(bool asynchronous) {
+  // TODO(zhangqiaorjc): Allow users set the number of threads.
+  // `num_blocking_threads=16` is picked arbitrarily for now.
+  // Need at least CpuDeviceCount threads to launch one collective.
+  int num_threads = std::max(DefaultThreadPoolSize(), CpuDeviceCount());
+  auto host_context = std::make_unique<tfrt::HostContext>(
+      [](const tfrt::DecodedDiagnostic& diag) {
+        LOG(ERROR) << "Encountered runtime error: " << diag.message << "\n";
+      },
+      tfrt::CreateMallocAllocator(),
+      tfrt::CreateMultiThreadedWorkQueue(
+          /*num_threads=*/num_threads,
+          /*num_blocking_threads=*/16));
+
+  TF_ASSIGN_OR_RETURN(std::vector<std::unique_ptr<TfrtCpuDevice>> devices,
+                      GetTfrtCpuDevices(asynchronous));
+
+  return std::unique_ptr<PjRtClient>(std::make_unique<TfrtCpuClient>(
+      /*process_index=*/0, std::move(devices), std::move(host_context)));
+}
+
+TfrtCpuClient::TfrtCpuClient(
+    int process_index, std::vector<std::unique_ptr<TfrtCpuDevice>> devices,
+    std::unique_ptr<tfrt::HostContext> host_ctx)
+    : process_index_(process_index),
+      owned_devices_(std::move(devices)),
+      host_ctx_(std::move(host_ctx)),
+      computation_placer_(std::make_unique<ComputationPlacer>()),
+      eigen_intraop_pool_(new tensorflow::thread::ThreadPool(
+          tensorflow::Env::Default(), "XLAEigen", DefaultThreadPoolSize())),
+      eigen_intraop_device_(
+          new Eigen::ThreadPoolDevice(eigen_intraop_pool_->AsEigenThreadPool(),
+                                      eigen_intraop_pool_->NumThreads())) {
+  for (const std::unique_ptr<TfrtCpuDevice>& device : owned_devices_) {
+    devices_.push_back(device.get());
+    CHECK(id_to_device_.insert({device->id(), device.get()}).second)
+        << "Duplicate device id: " << device->id();
+
+    device->SetClient(this);
+    if (device->IsAddressable()) {
+      int idx = device->local_hardware_id();
+      if (idx >= addressable_devices_.size()) {
+        addressable_devices_.resize(idx + 1);
+      }
+      CHECK(addressable_devices_[idx] == nullptr) << idx;
+      addressable_devices_[idx] = device.get();
+    }
+  }
+  for (int idx = 0; idx < addressable_devices_.size(); ++idx) {
+    CHECK(addressable_devices_[idx] != nullptr) << idx;
+  }
+  LOG(INFO) << "TfrtCpuClient created.";
+}
+
+StatusOr<PjRtDevice*> TfrtCpuClient::LookupDevice(int device_id) const {
+  auto it = id_to_device_.find(device_id);
+  if (it != id_to_device_.end()) {
+    return it->second;
+  }
+  return InvalidArgument("No matching device found for device_id %d",
+                         device_id);
+}
+
+StatusOr<PjRtDevice*> TfrtCpuClient::LookupAddressableDevice(
+    int local_hardware_id) const {
+  for (auto* device : addressable_devices_) {
+    if (local_hardware_id == device->local_hardware_id()) {
+      return device;
+    }
+  }
+  return InvalidArgument("No matching device found for local_hardware_id %d",
+                         local_hardware_id);
+}
+
+StatusOr<DeviceAssignment> TfrtCpuClient::GetDefaultDeviceAssignment(
+    int num_replicas, int num_partitions) const {
+  return computation_placer_->AssignDevices(num_replicas, num_partitions);
+}
+
+StatusOr<std::unique_ptr<HloCostAnalysis>> TfrtCpuClient::GetHloCostAnalysis() {
+  return absl::make_unique<HloCostAnalysis>(cpu::CpuExecutable::ShapeSizeBytes);
+}
+
+StatusOr<absl::optional<std::string>> TfrtCpuClient::ExecutableFingerprint(
+    const PjRtExecutable& executable) const {
+  return absl::optional<std::string>();
 }
 
 }  // namespace xla
--- tensorflow/compiler/xla/pjrt/tfrt_cpu_pjrt_client.h
+++ tensorflow/compiler/xla/pjrt/tfrt_cpu_pjrt_client.h
@@ -16,10 +16,23 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_XLA_PJRT_TFRT_CPU_PJRT_CLIENT_H_
 #define TENSORFLOW_COMPILER_XLA_PJRT_TFRT_CPU_PJRT_CLIENT_H_
 
+#include <memory>
+
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/compiler/xla/client/executable_build_options.h"
+#include "tensorflow/compiler/xla/client/xla_computation.h"
+#include "tensorflow/compiler/xla/layout.h"
+#include "tensorflow/compiler/xla/literal.h"
 #include "tensorflow/compiler/xla/pjrt/pjrt_client.h"
 #include "tensorflow/compiler/xla/pjrt/semaphore.h"
 #include "tensorflow/compiler/xla/pjrt/worker_thread.h"
+#include "tensorflow/compiler/xla/service/computation_placer.h"
+#include "tensorflow/compiler/xla/service/executable.h"
+#include "tensorflow/compiler/xla/service/hlo_cost_analysis.h"
 #include "tensorflow/compiler/xla/statusor.h"
+#include "tensorflow/compiler/xla/xla_data.pb.h"
+#include "tfrt/host_context/async_value_ref.h"  // from @tf_runtime
+#include "tfrt/host_context/host_context.h"  // from @tf_runtime
 
 namespace xla {
 
@@ -67,6 +80,125 @@ class TfrtCpuDevice : public PjRtDevice {
   // ahead of the device.
   Semaphore max_inflight_computations_semaphore_;
 };
+
+class TfrtCpuExecutable;
+
+class TfrtCpuClient : public PjRtClient {
+ public:
+  TfrtCpuClient(int process_index,
+                std::vector<std::unique_ptr<TfrtCpuDevice>> devices,
+                std::unique_ptr<tfrt::HostContext> host_ctx);
+
+  int process_index() const override { return process_index_; }
+
+  int device_count() const override { return devices_.size(); }
+
+  int addressable_device_count() const override {
+    return addressable_devices_.size();
+  }
+
+  absl::Span<PjRtDevice* const> devices() const override { return devices_; }
+
+  absl::Span<PjRtDevice* const> addressable_devices() const override {
+    return addressable_devices_;
+  }
+
+  StatusOr<PjRtDevice*> LookupDevice(int device_id) const override;
+
+  StatusOr<PjRtDevice*> LookupAddressableDevice(
+      int local_hardware_id) const override;
+
+  PjRtPlatformId platform_id() const override {
+    return tensorflow::Fingerprint64(kCpuName);
+  }
+
+  absl::string_view platform_name() const override { return kCpuName; }
+
+  absl::string_view platform_version() const override { return "<unknown>"; }
+
+  StatusOr<DeviceAssignment> GetDefaultDeviceAssignment(
+      int num_replicas, int num_partitions) const override;
+
+  StatusOr<std::unique_ptr<HloCostAnalysis>> GetHloCostAnalysis() override;
+
+  StatusOr<std::unique_ptr<PjRtExecutable>> Compile(
+      const XlaComputation& computation, CompileOptions options) override {
+    return Unimplemented("Compile");
+  }
+
+  StatusOr<absl::optional<std::string>> ExecutableFingerprint(
+      const PjRtExecutable& executable) const override;
+
+  StatusOr<std::unique_ptr<PjRtBuffer>> CreateUninitializedBuffer(
+      const Shape& shape, PjRtDevice* device) override {
+    return Unimplemented("CreateUninitializedBuffer");
+  }
+
+  StatusOr<std::unique_ptr<PjRtBuffer>> BufferFromHostBuffer(
+      const void* data, const Shape& shape,
+      HostBufferSemantics host_buffer_semantics,
+      std::function<void()> on_done_with_host_buffer,
+      PjRtDevice* device) override {
+    return Unimplemented("BufferFromHostBuffer");
+  }
+
+  StatusOr<std::unique_ptr<PjRtBuffer>> BufferFromHostLiteral(
+      const LiteralSlice& literal, PjRtDevice* device) override {
+    return Unimplemented("BufferFromHostLiteral");
+  }
+
+  void MakeCrossHostReceiveBuffers(
+      absl::Span<const Shape> shapes, PjRtDevice* device,
+      PjRtCrossHostRecvNotifier&& notifier) override {
+    LOG(FATAL) << "MakeCrossHostReceiveBuffers not implemented.";
+  }
+
+  StatusOr<std::unique_ptr<PjRtBuffer>> CreateViewOfDeviceBuffer(
+      void* device_ptr, const Shape& shape, PjRtDevice* device,
+      std::function<void()> on_delete_callback) override {
+    return Unimplemented("CreateViewOfDeviceBuffer not implemented.");
+  }
+
+  StatusOr<ChannelHandle> CreateChannelHandle() override {
+    return Unimplemented("CreateChannelHandle not implemented.");
+  }
+  StatusOr<ChannelHandle> CreateDeviceToHostChannelHandle() override {
+    return Unimplemented("CreateDeviceToHostChannelHandle not implemented.");
+  }
+  StatusOr<ChannelHandle> CreateHostToDeviceChannelHandle() override {
+    return Unimplemented("CreateHostToDeviceChannelHandle not implemented.");
+  }
+
+  Status Defragment(absl::Span<PjRtBuffer* const> buffers,
+                    absl::Span<PjRtExecutable* const> executables) override {
+    return Unimplemented("Defragment not implemented.");
+  }
+
+  tfrt::HostContext* GetHostContext() const { return host_ctx_.get(); }
+
+  Eigen::ThreadPoolDevice* eigen_intraop_device() const {
+    return eigen_intraop_device_.get();
+  }
+
+ private:
+  int process_index_;
+  // Includes all devices, including non-addressable devices.
+  std::vector<std::unique_ptr<TfrtCpuDevice>> owned_devices_;
+  // Pointers to `owned_devices_`.
+  std::vector<PjRtDevice*> devices_;
+  // Maps Device::id() to the corresponding Device. Includes all devices.
+  absl::flat_hash_map<int, TfrtCpuDevice*> id_to_device_;
+  // Addressable devices indexed by core_id.
+  std::vector<PjRtDevice*> addressable_devices_;
+  std::unique_ptr<tfrt::HostContext> host_ctx_;
+  std::unique_ptr<ComputationPlacer> computation_placer_;
+
+  // TODO(zhangqiaorjc): Use tfrt::compat::EigenHostContextThreadPool.
+  std::unique_ptr<tensorflow::thread::ThreadPool> eigen_intraop_pool_;
+  std::unique_ptr<Eigen::ThreadPoolDevice> eigen_intraop_device_;
+};
+
+StatusOr<std::unique_ptr<PjRtClient>> GetTfrtCpuClient(bool asynchronous);
 
 }  // namespace xla
 
