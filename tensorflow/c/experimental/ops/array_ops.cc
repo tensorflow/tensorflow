1f4ad17f423a8924a9c64b49f8e5cece21556832
Jeremy Meredith <jsmeredith@google.com>
2021-04-14 15:27:49 -0700

    Updating output arguments for experimental C++ ops to list each individually.
    The immediate benefit is that each output argument has a caller-visible name in the signature.  In turn, this makes it easier to use named variables in the calling code (see nn_grad_test.cc).
    
    Additionally, it removes the caller requirement on container types for non-list arguments.
    
    This simplifies some calling logic as well, using C arrays to give compiler length hints, allowing simpler notation in some cases (e.g. {initializer_list} instead of MakeSpan()), and avoids the (harder-to-read) subspan() calls.  This also avoids the heap for these temporary tensor handle pointer allocations.
    
    PiperOrigin-RevId: 368520605
    Change-Id: I4bc26af4264bafe0d1b453231edfcd6dcbb5ee8a

--- tensorflow/c/experimental/ops/array_ops.cc
+++ tensorflow/c/experimental/ops/array_ops.cc
@@ -31,13 +31,13 @@ namespace ops {
 //
 // Description:
 Status Identity(AbstractContext* ctx, AbstractTensorHandle* const input,
-                absl::Span<AbstractTensorHandle*> output, const char* name) {
+                AbstractTensorHandle** output, const char* name) {
   AbstractOperationPtr op_ptr(ctx->CreateOperation());
   TF_RETURN_IF_ERROR(op_ptr->Reset("Identity", /*raw_device_name=*/nullptr));
   TF_RETURN_IF_ERROR(MaybeSetOpName(op_ptr.get(), name));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(input));
   int num_retvals = 1;
-  return op_ptr->Execute(output, &num_retvals);
+  return op_ptr->Execute(absl::MakeSpan(output, 1), &num_retvals);
 }
 
 // Op: IdentityN()
@@ -76,13 +76,13 @@ Status IdentityN(AbstractContext* ctx,
 //
 // Description:
 Status ZerosLike(AbstractContext* ctx, AbstractTensorHandle* const x,
-                 absl::Span<AbstractTensorHandle*> y, const char* name) {
+                 AbstractTensorHandle** y, const char* name) {
   AbstractOperationPtr op_ptr(ctx->CreateOperation());
   TF_RETURN_IF_ERROR(op_ptr->Reset("ZerosLike", /*raw_device_name=*/nullptr));
   TF_RETURN_IF_ERROR(MaybeSetOpName(op_ptr.get(), name));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(x));
   int num_retvals = 1;
-  return op_ptr->Execute(y, &num_retvals);
+  return op_ptr->Execute(absl::MakeSpan(y, 1), &num_retvals);
 }
 
 // Op: Shape()
@@ -99,13 +99,13 @@ Status ZerosLike(AbstractContext* ctx, AbstractTensorHandle* const x,
 //   shape(t) ==> [2, 2, 3]
 //   ```
 Status Shape(AbstractContext* ctx, AbstractTensorHandle* const input,
-             absl::Span<AbstractTensorHandle*> output, const char* name) {
+             AbstractTensorHandle** output, const char* name) {
   AbstractOperationPtr op_ptr(ctx->CreateOperation());
   TF_RETURN_IF_ERROR(op_ptr->Reset("Shape", /*raw_device_name=*/nullptr));
   TF_RETURN_IF_ERROR(MaybeSetOpName(op_ptr.get(), name));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(input));
   int num_retvals = 1;
-  return op_ptr->Execute(output, &num_retvals);
+  return op_ptr->Execute(absl::MakeSpan(output, 1), &num_retvals);
 }
 
 // Op: ExpandDims()
@@ -144,14 +144,14 @@ Status Shape(AbstractContext* ctx, AbstractTensorHandle* const input,
 //   size 1.
 Status ExpandDims(AbstractContext* ctx, AbstractTensorHandle* const input,
                   AbstractTensorHandle* const dim,
-                  absl::Span<AbstractTensorHandle*> output, const char* name) {
+                  AbstractTensorHandle** output, const char* name) {
   AbstractOperationPtr op_ptr(ctx->CreateOperation());
   TF_RETURN_IF_ERROR(op_ptr->Reset("ExpandDims", /*raw_device_name=*/nullptr));
   TF_RETURN_IF_ERROR(MaybeSetOpName(op_ptr.get(), name));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(input));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(dim));
   int num_retvals = 1;
-  return op_ptr->Execute(output, &num_retvals);
+  return op_ptr->Execute(absl::MakeSpan(output, 1), &num_retvals);
 }
 
 // Op: OnesLike()
@@ -159,13 +159,13 @@ Status ExpandDims(AbstractContext* ctx, AbstractTensorHandle* const input,
 //
 // Description:
 Status OnesLike(AbstractContext* ctx, AbstractTensorHandle* const x,
-                absl::Span<AbstractTensorHandle*> y, const char* name) {
+                AbstractTensorHandle** y, const char* name) {
   AbstractOperationPtr op_ptr(ctx->CreateOperation());
   TF_RETURN_IF_ERROR(op_ptr->Reset("OnesLike", /*raw_device_name=*/nullptr));
   TF_RETURN_IF_ERROR(MaybeSetOpName(op_ptr.get(), name));
   TF_RETURN_IF_ERROR(op_ptr->AddInput(x));
   int num_retvals = 1;
-  return op_ptr->Execute(y, &num_retvals);
+  return op_ptr->Execute(absl::MakeSpan(y, 1), &num_retvals);
 }
 
 }  // namespace ops
