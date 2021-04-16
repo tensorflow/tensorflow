e8c447f90d3a1a53bf5bc14a019e1df7e089ed2b
Prakalp Srivastava <prakalps@google.com>
2021-04-16 11:23:46 -0700

    Do not make an exception for TPUReplicatedInput in graph pruning.
    TPUReplicatedInput ops can be safely pruned away if they have no use. It is anyways deleted by cluster formation pass later on if it has no use. This change just removes them early on in the bridge pipeline.
    
    PiperOrigin-RevId: 368885302
    Change-Id: I4976c74e21526e008fa851494151fe7ad577d691

--- tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/bridge.cc
@@ -74,7 +74,7 @@ void CreateTPUBridgePipeline(OpPassManager &pm) {
   // currently not the case (see b/177478741).
   const llvm::SmallVector<std::string, 4> ops_to_preserve = {
       "tf.TPUReplicateMetadata", "tf.TPUCompilationResult",
-      "tf.TPUReplicatedInput", "tf.TPUReplicatedOutput"};
+      "tf.TPUReplicatedOutput"};
   pm.addNestedPass<FuncOp>(
       tf_executor::CreateTFExecutorGraphPruningPass(ops_to_preserve));
   // It is assumed at this stage there are no V1 control flow ops as Graph
