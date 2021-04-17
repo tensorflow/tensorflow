0f32b446cf8cc140f8eaf02c1671e842ad126338
Alan Green <avg@google.com>
2021-04-18 09:19:30 +1000

    micro: Printed memory plan layout changes
    Reformats output of GreedyMemoryPlanner:PrintMemoryPlan() to make it
    simpler to interpret.
    
    Per-buffer info line improvements:
    
    (a) Reduces text quantity to make it easier to scan for information.
    (b) Moves size to near front line, to make it simpler for the eye to
        find this information.
    (c) Includes ordinal letter used in per-time display below to make it
        simpler to cross-reference.
    
    Per time improvements:
    
    (a) Includes tick number. This is useful in larger models for cross
        referencing to the per-buffer information and also for helping to
        determine the actual operation being executed at that time.
    (b) Includes total memory use of buffers, which helps to more clearly
        identify memory bottlenecks.

--- tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc
+++ tensorflow/lite/micro/memory_planner/greedy_memory_planner.cc
@@ -15,7 +15,24 @@ limitations under the License.
 
 #include "tensorflow/lite/micro/memory_planner/greedy_memory_planner.h"
 
+#include "tensorflow/lite/micro/micro_string.h"
+
 namespace tflite {
+
+namespace {
+
+char GetOrdinalCharacter(int i) {
+  if (i < 10) {
+    return '0' + i;
+  } else if (i < 36) {
+    return 'a' + (i - 10);
+  } else if (i < 62) {
+    return 'A' + (i - 36);
+  }
+  return '*';
+}
+
+}  // namespace
 
 // Simple stable in-place sort function. Not time-efficient for large arrays.
 // Would normally be in an anonymous namespace to keep it private, but we want
@@ -315,12 +332,11 @@ void GreedyMemoryPlanner::PrintMemoryPlan(ErrorReporter* error_reporter) {
   CalculateOffsetsIfNeeded();
 
   for (int i = 0; i < buffer_count_; ++i) {
+    char s[] = {GetOrdinalCharacter(i), 0};
     TF_LITE_REPORT_ERROR(
         error_reporter,
-        "Planner buffer ID: %d, calculated offset: %d, size required: %d, "
-        "first_time_created: %d, "
-        "last_time_used: %d",
-        i, buffer_offsets_[i], requirements_[i].size,
+        "%s (id=%d): size=%d, offset=%d, first_used=%d last_used=%d", s, i,
+        requirements_[i].size, buffer_offsets_[i],
         requirements_[i].first_time_used, requirements_[i].last_time_used);
   }
 
@@ -345,6 +361,7 @@ void GreedyMemoryPlanner::PrintMemoryPlan(ErrorReporter* error_reporter) {
     for (int c = 0; c < kLineWidth; ++c) {
       line[c] = '.';
     }
+    int memory_use = 0;
     for (int i = 0; i < buffer_count_; ++i) {
       BufferRequirements* requirements = &requirements_[i];
       if ((t < requirements->first_time_used) ||
@@ -356,28 +373,22 @@ void GreedyMemoryPlanner::PrintMemoryPlan(ErrorReporter* error_reporter) {
         continue;
       }
       const int size = requirements->size;
+      memory_use += size;
       const int line_start = (offset * kLineWidth) / max_size;
       const int line_end = ((offset + size) * kLineWidth) / max_size;
       for (int n = line_start; n < line_end; ++n) {
         if (line[n] == '.') {
-          char display;
-          if (i < 10) {
-            display = '0' + i;
-          } else if (i < 36) {
-            display = 'a' + (i - 10);
-          } else if (i < 62) {
-            display = 'A' + (i - 36);
-          } else {
-            display = '*';
-          }
-          line[n] = display;
+          line[n] = GetOrdinalCharacter(i);
         } else {
           line[n] = '!';
         }
       }
     }
     line[kLineWidth] = 0;
-    TF_LITE_REPORT_ERROR(error_reporter, "%s", (const char*)line);
+
+    char padding[] = {t < 100 ? ' ' : '\0', t < 10 ? ' ' : '\0', '\0'};
+    TF_LITE_REPORT_ERROR(error_reporter, "%s%d: %s (%dk)", padding, t,
+                         (const char*)line, (memory_use + 1023) / 1024);
   }
 }
 
