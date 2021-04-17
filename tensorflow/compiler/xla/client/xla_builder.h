3ab71a75adbd2443309ec777dad2ad193203958a
Mihai Maruseac <mihaimaruseac@google.com>
2021-04-17 09:34:53 -0700

    Allow value inference to analyze into embedded conditional computations.
    PiperOrigin-RevId: 369016974
    Change-Id: I8c37bedb8a2621b3a65c6270a01b585f7b33a2d6

--- tensorflow/compiler/xla/client/xla_builder.h
+++ tensorflow/compiler/xla/client/xla_builder.h
@@ -1003,15 +1003,6 @@ class XlaBuilder {
   // instruction is held.
   absl::flat_hash_map<int64, int64> handle_to_index_;
 
-  // Track imported instructions by their computation id and the position in
-  // their computation's instruction list.
-  struct ImportedInstruction {
-    int64 computation_id;
-    int64 instruction_index;
-  };
-
-  absl::flat_hash_map<int64, ImportedInstruction> handle_to_imported_index_;
-
   // The embedded computations used by this computation. Each computation was
   // the entry computation of some XlaComputation, the key is the unique id of
   // that XlaComputation.
@@ -1454,14 +1445,6 @@ class XlaBuilder {
       int64 handle) const {
     auto it = handle_to_index_.find(handle);
     if (it == handle_to_index_.end()) {
-      // Try look for the instruction in the imported instructions.
-      auto imported_it = handle_to_imported_index_.find(handle);
-      if (imported_it != handle_to_imported_index_.end()) {
-        ImportedInstruction imported = imported_it->second;
-        return const_cast<InstructionType>(
-            &embedded_.at(imported.computation_id)
-                 .instructions(imported.instruction_index));
-      }
       return InvalidArgument("No XlaOp with handle %d", handle);
     }
     return const_cast<InstructionType>(&instructions_.at(it->second));
