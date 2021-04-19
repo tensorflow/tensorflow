a0e5ec8cb940fbea193706f0c6609be6dad5a961
Meghna Natraj <mnatraj@google.com>
2021-04-19 16:52:26 -0700

    Update signature_defs tensor indices after quantization
    PiperOrigin-RevId: 369331463
    Change-Id: Ifce6785de59a4af6b9a6f38dd695b23671a8305f

--- tensorflow/lite/python/lite_v2_test.py
+++ tensorflow/lite/python/lite_v2_test.py
@@ -841,6 +841,21 @@ class FromSavedModelTest(lite_v2_test_util.ModelTest):
         saved_model.simple_save(sess, saved_model_dir, inputs, outputs)
     return saved_model_dir
 
+  def _createV2QATSavedModel(self, shape):
+    """Create a simple QAT SavedModel in TF 2."""
+    saved_model_dir = os.path.join(self.get_temp_dir(), 'saved_model')
+    input_name = 'input'
+    output_name = 'scores'
+
+    input_tensor = tf.keras.layers.Input((32, 32, 128), name=input_name)
+    x = tf.quantization.fake_quant_with_min_max_args(input_tensor, -3.0, 3.0)
+    x = tf.keras.layers.Conv2D(1, (3, 3))(x)
+    x = tf.quantization.fake_quant_with_min_max_args(x, -3.0, 3.0)
+    scores = tf.keras.layers.Reshape((-1,), name=output_name)(x)
+    model = tf.keras.Model(input_tensor, scores)
+    model.save(saved_model_dir)
+    return saved_model_dir, input_name, output_name
+
   @test_util.run_v2_only
   def testV1SimpleModel(self):
     """Test a SavedModel."""
@@ -1114,6 +1129,73 @@ class FromSavedModelTest(lite_v2_test_util.ModelTest):
     expected_value = root.f(input_data)
     actual_value = self._evaluateTFLiteModel(tflite_model, [input_data])
     self.assertEqual(expected_value.numpy(), actual_value)
+
+  @test_util.run_v2_only
+  def testSignatureDefsWithFullIntegerQuantization(self):
+    # SETUP
+    # 1. Define input shapes
+    tf_input_shape = (32, 32, 128)
+    tflite_input_shape = (1,) + tf_input_shape
+    # 2. Define model
+    tf_saved_model_dir, input_name, output_name = (
+        self._createV2QATSavedModel(tf_input_shape))
+
+    # MODEL 1: TFLite (float) model
+    # 1. Create TFLite model
+    converter = tf.lite.TFLiteConverter.from_saved_model(tf_saved_model_dir)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+    # 2. Initialize the Intepreter
+    interpreter = Interpreter(model_content=tflite_model)
+    input_details = interpreter.get_input_details()[0]
+    output_details = interpreter.get_output_details()[0]
+    interpreter.resize_tensor_input(input_details['index'], tflite_input_shape)
+    interpreter.allocate_tensors()
+    signature_list = interpreter._get_full_signature_list()['serving_default']
+    # 3. (Skip) Verify that signature def input/output tensors are in the model.
+    # 4. Evaluate the model
+    input_data = np.random.random(tflite_input_shape).astype(np.float32)
+    result = self._evaluateTFLiteModelUsingSignatureDef(
+        tflite_model, 'serving_default', {input_name: input_data})[output_name]
+
+    # MODEL 2: TFLite (full integer quantized) model
+    # 1. Create TFLite model
+    converter = tf.lite.TFLiteConverter.from_saved_model(tf_saved_model_dir)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.inference_input_type = tf.int8
+    converter.inference_output_type = tf.int8
+    tflite_model_quant = converter.convert()
+    # 2. Initialize the Intepreter
+    interpreter = Interpreter(model_content=tflite_model_quant)
+    input_details = interpreter.get_input_details()[0]
+    output_details = interpreter.get_output_details()[0]
+    interpreter.resize_tensor_input(input_details['index'], tflite_input_shape)
+    interpreter.allocate_tensors()
+    # 3. Verify that signature def input/output tensors are in the model.
+    all_indices = {item['index'] for item in interpreter.get_tensor_details()}
+    signature_list = interpreter._get_full_signature_list()['serving_default']
+    input_tensor_indices = set(signature_list['inputs'].values())
+    assert input_tensor_indices.issubset(all_indices)
+    output_tensor_indices = set(signature_list['outputs'].values())
+    assert output_tensor_indices.issubset(all_indices)
+
+    # 4. Evaluate the model
+    input_data = np.random.random(tflite_input_shape)
+    input_scale, input_zero_point = input_details['quantization']
+    if (input_scale, input_zero_point) != (0.0, 0):
+      input_data = input_data / input_scale + input_zero_point
+      input_data = input_data.astype(input_details['dtype'])
+    result_quant = self._evaluateTFLiteModelUsingSignatureDef(
+        tflite_model_quant, 'serving_default',
+        {input_name: input_data})[output_name]
+    output_scale, output_zero_point = output_details['quantization']
+    if (output_scale, output_zero_point) != (0.0, 0):
+      result_quant = result_quant.astype(np.float32)
+      result_quant = (result_quant - output_zero_point) * output_scale
+
+    # COMPARE: Validate that results from both models are approx. the same.
+    root_mean_squared = np.sqrt(np.mean((result-result_quant)**2))
+    assert root_mean_squared < 1.0
 
   @test_util.run_v2_only
   def testSignatureDefs(self):
