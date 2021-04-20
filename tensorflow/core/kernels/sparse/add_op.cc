b27d392fc0315a016d4b88b48578c801abc647d2
Deven Desai <deven.desai.amd@gmail.com>
2021-04-20 23:58:21 +0000

    Added necessary ops for CSR Add with real and complex types.

--- tensorflow/core/kernels/sparse/add_op.cc
+++ tensorflow/core/kernels/sparse/add_op.cc
@@ -253,8 +253,10 @@ class CSRAddOp : public OpKernel {
 
 REGISTER_GPU(float)
 REGISTER_GPU(double)
+#if GOOGLE_CUDA
 REGISTER_GPU(complex64)
 REGISTER_GPU(complex128)
+#endif
 
 #undef REGISTER_GPU
 
