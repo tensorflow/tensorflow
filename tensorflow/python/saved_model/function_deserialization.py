141ed39568fa287ae846f3f8f8a63f8775de6706
A. Unique TensorFlower <gardener@tensorflow.org>
2021-04-20 06:00:17 -0700

    Add existing function_def attributes to the created ConcreteFunction during load.
    PiperOrigin-RevId: 369422824
    Change-Id: I5e5d4bc80b4c33bbbdf0249883546bb4653b9e7e

--- tensorflow/python/saved_model/function_deserialization.py
+++ tensorflow/python/saved_model/function_deserialization.py
@@ -366,8 +366,7 @@ def load_function_def_library(library, load_shared_name_suffix=None):
     # signatures, respectively). ConcreteFunction that are part of a saved
     # function is set up later by recreate_function(); and bare ConcreteFunction
     # is set up by by setup_bare_concrete_function().
-    # However, we copy the FunctionDef attributes to the new ConcreteFunction.
-    func = function_lib.ConcreteFunction(func_graph, attrs=copy.attr)
+    func = function_lib.ConcreteFunction(func_graph)
     func.add_to_graph(graph)
 
     functions[fdef.signature.name] = func
