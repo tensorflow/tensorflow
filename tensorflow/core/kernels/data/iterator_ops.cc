7614c80dc9ad1798d83de17c80e1982c3cb9bdb6
Andrew Audibert <aaudibert@google.com>
2021-04-26 15:55:05 -0700

    [tf.data] Improve error message for iterator restoration failure.
    Previously, trying to restore into a modified dataset would raise an error such as "NotFoundError: Iterator::Model::TensorSlice". This would get caught in tracking/util.py and be incorrectly reraised as an error saying that the checkpoint couldn't be found.
    
    With this CL we provide a more accurate and actionable message:
    
    Failed to restore from checkpoint or SavedModel at <path>: Failed to restore dataset iterator from checkpoint: Iterator::Model::TensorSlice. Make sure the dataset definition has not changed between the process that saved the checkpoint and the process that is restoring it. [Op:DeserializeIterator]
    
    PiperOrigin-RevId: 370559855
    Change-Id: I56c55fd7cf098d52da03994179c5acc36a14fb3a

--- tensorflow/core/kernels/data/iterator_ops.cc
+++ tensorflow/core/kernels/data/iterator_ops.cc
@@ -1186,7 +1186,18 @@ void DeserializeIteratorOp::Compute(OpKernelContext* ctx) {
   OP_REQUIRES_OK(ctx, ctx->input("serialized", &serialized_t));
   IteratorVariantSerializer serializer;
   OP_REQUIRES_OK(ctx, serializer.InitFromTensor(serialized_t));
-  OP_REQUIRES_OK(ctx, iterator_resource->Restore(ctx, serializer.GetReader()));
+  Status s = iterator_resource->Restore(ctx, serializer.GetReader());
+  if (!s.ok()) {
+    OP_REQUIRES_OK(
+        ctx,
+        Status(s.code(),
+               absl::StrCat(
+                   "Failed to restore dataset iterator from checkpoint: ",
+                   s.error_message(),
+                   ". Make sure the dataset definition has not changed between "
+                   "the process that saved the checkpoint and the process that "
+                   "is restoring it.")));
+  }
 }
 
 namespace {
