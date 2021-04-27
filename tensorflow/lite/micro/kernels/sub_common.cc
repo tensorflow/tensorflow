859c703c992435da4880f47e76dbf742e00cc293
Yair Ehrenwald <yair.ehrenwald@ceva-dsp.com>
2021-04-27 22:52:50 +0300

    Added <algorithm> include

--- tensorflow/lite/micro/kernels/sub_common.cc
+++ tensorflow/lite/micro/kernels/sub_common.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <algorithm>
+
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/kernels/internal/common.h"
@@ -25,12 +27,10 @@ limitations under the License.
 #include "tensorflow/lite/kernels/op_macros.h"
 #include "tensorflow/lite/micro/kernels/kernel_util.h"
 #include "tensorflow/lite/micro/kernels/sub.h"
+#include <algorithm>
 
 namespace tflite {
-
-const int kSubInputTensor1 = 0;
-const int kSubInputTensor2 = 1;
-const int kSubOutputTensor = 0;
+namespace {
 
 TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteSubParams* params,
                              const TfLiteTensor* input1,
@@ -70,6 +70,11 @@ TfLiteStatus CalculateOpData(TfLiteContext* context, TfLiteSubParams* params,
 
   return kTfLiteOk;
 }
+}  // anonymous namespace
+
+const int kSubInputTensor1 = 0;
+const int kSubInputTensor2 = 1;
+const int kSubOutputTensor = 0;
 
 TfLiteStatus PrepareSub(TfLiteContext* context, TfLiteNode* node) {
   TFLITE_DCHECK(node->user_data != nullptr);
