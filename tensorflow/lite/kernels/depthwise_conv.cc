34c68a212b738dd7a938c759313f878d410f2eb5
Mihai Maruseac <mihaimaruseac@google.com>
2021-04-28 15:53:48 -0700

    Prevent divisions by 0

--- tensorflow/lite/kernels/depthwise_conv.cc
+++ tensorflow/lite/kernels/depthwise_conv.cc
@@ -175,8 +175,8 @@ TfLiteStatus ComputeDepthMultiplier(TfLiteContext* context,
                                     int16* depth_multiplier) {
   int num_filter_channels = SizeOfDimension(filter, 3);
   int num_input_channels = SizeOfDimension(input, 3);
+  TF_LITE_ENSURE(context, num_input_channels != 0);
   TF_LITE_ENSURE_EQ(context, num_filter_channels % num_input_channels, 0);
-
   *depth_multiplier = num_filter_channels / num_input_channels;
   return kTfLiteOk;
 }
