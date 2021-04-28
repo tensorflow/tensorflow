3ce0156711993acc95640a282cf4e8ce6b9f1d16
Rahul Joshi <jurahul@google.com>
2021-04-28 08:49:12 -0700

    [XLA:SPMD][NFC] Evaluate cache use condition just once.
    PiperOrigin-RevId: 370912222
    Change-Id: Iafb50ccfd15e62e32b986300d41c4b893f69d20e

--- tensorflow/compiler/xla/service/spmd/spmd_partitioner.cc
+++ tensorflow/compiler/xla/service/spmd/spmd_partitioner.cc
@@ -350,7 +350,9 @@ PartitionedHlo PartitionedHlo::Reshard(const HloSharding& target) {
   auto& cache = state_.reshard_cache->per_hlo_cache[hlo()].reshard_cache;
   const bool is_to_replicate =
       hlo_->shape().IsArray() && target.NumTiles() < sharding().NumTiles();
-  if (!is_to_replicate || state_.partitioner->options().cache_all_gather) {
+  const bool use_cache =
+      !is_to_replicate || state_.partitioner->options().cache_all_gather;
+  if (use_cache) {
     for (auto& entry : cache) {
       if (entry.first == target) {
         return entry.second;
@@ -360,7 +362,7 @@ PartitionedHlo PartitionedHlo::Reshard(const HloSharding& target) {
   auto resharded = ReshardNoCache(target);
   state_.reshard_cache->per_hlo_cache[resharded.hlo()]
       .reshard_cache.emplace_back(sharding(), *this);
-  if (!is_to_replicate || state_.partitioner->options().cache_all_gather) {
+  if (use_cache) {
     cache.emplace_back(target, std::move(resharded));
     return cache.back().second;
   }
