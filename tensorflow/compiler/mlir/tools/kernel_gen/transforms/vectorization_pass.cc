c8ac54c886fc638b24cf5d1d0e4e9fd75ea0a1c3
Tres Popp <tpopp@google.com>
2021-05-04 08:37:25 -0700

    [kernel_gen][NFC] Don't use patterns for Vectorization cleanup pass.
    Future work will be much easier not being constrained by the pattern application infrastructure. Additionally, there is not much loss to this change as it does not have a need to be iteratively applied with other patterns, so it is acceptable to keep it as a separate walk.
    
    PiperOrigin-RevId: 371921021
    Change-Id: I80e72e73e91ba0e61a87cb7498f919cfc1a846c4

--- tensorflow/compiler/mlir/tools/kernel_gen/transforms/vectorization_pass.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/transforms/vectorization_pass.cc
@@ -24,6 +24,8 @@ limitations under the License.
 #include "mlir/Dialect/Linalg/Transforms/Transforms.h"  // from @llvm-project
 #include "mlir/Dialect/MemRef/IR/MemRef.h"  // from @llvm-project
 #include "mlir/Dialect/SCF/SCF.h"  // from @llvm-project
+#include "mlir/Dialect/StandardOps/IR/Ops.h"  // from @llvm-project
+#include "mlir/IR/ImplicitLocOpBuilder.h"  // from @llvm-project
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/IR/lhlo_ops.h"
 #include "tensorflow/compiler/mlir/tools/kernel_gen/ir/tf_framework_ops.h"
@@ -54,109 +56,101 @@ namespace {
 //
 // This is possible because we can determine that the min will always be 2
 // except for the last iteration.
-class SplitSCFForOp : public OpRewritePattern<scf::ForOp> {
- public:
-  using OpRewritePattern<scf::ForOp>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(scf::ForOp scf_for,
-                                PatternRewriter &rewriter) const override {
-    // Match only when the lower bound is zero and the step is constant.
-    // TODO(TPOPP): Requiring constant steps and lower bound simplifies things
-    // but isn't necesarilly needed
-    auto lower_bound_op =
-        llvm::dyn_cast<ConstantOp>(scf_for.lowerBound().getDefiningOp());
-    if (!lower_bound_op) {
-      return failure();
-    }
-    auto lower_bound_value = lower_bound_op.getValue().dyn_cast<IntegerAttr>();
-    if (!lower_bound_value || lower_bound_value.getInt() != 0) {
-      return failure();
-    }
-
-    auto step_bound_op =
-        llvm::dyn_cast<ConstantOp>(scf_for.step().getDefiningOp());
-    if (!step_bound_op) {
-      return failure();
-    }
-    auto step_bound_value = step_bound_op.getValue().dyn_cast<IntegerAttr>();
-    if (!step_bound_value) {
-      return failure();
-    }
-
-    // This function will determine if the min_op is an operation that can be
-    // transformed after loop splitting. This relies on the function that the op
-    // represents relative to the induction variable in its loop and the
-    // bounds of the original for loop.
-    auto is_op_of_interest = [&](AffineMinOp min_op, Value iv) {
-      if (min_op.getDimOperands().size() + min_op.getSymbolOperands().size() !=
-          2)
-        return false;
-      bool min_by_step = false;
-      for (auto i : min_op.getAffineMap().getResults()) {
-        if (i == rewriter.getAffineConstantExpr(step_bound_value.getInt())) {
-          min_by_step = true;
-          continue;
-        }
-        if (i == rewriter.getAffineSymbolExpr(0) -
-                     rewriter.getAffineDimExpr(0) &&
-            min_op.getDimOperands().front() == iv &&
-            min_op.getSymbolOperands().front() == scf_for.upperBound())
-          continue;
-        if (i == rewriter.getAffineDimExpr(0) - rewriter.getAffineDimExpr(1) &&
-            min_op.getDimOperands().drop_front().front() == iv &&
-            min_op.getDimOperands().front() == scf_for.upperBound())
-          continue;
-        return false;
-      }
-      return min_by_step;
-    };
-
-    // Determine if the loop should be split based on the existence of
-    // AffineMinOps of an expected form.
-    llvm::SmallVector<AffineMinOp, 1> min_ops;
-    scf_for->walk([&](AffineMinOp min_op) {
-      if (is_op_of_interest(min_op, scf_for.getInductionVar()))
-        min_ops.push_back(min_op);
-    });
-    if (min_ops.empty()) {
-      return failure();
-    }
-
-    // Split the loop just before a possible last iteration.
-    rewriter.setInsertionPoint(scf_for);
-    Value split_point = rewriter.create<SubIOp>(
-        scf_for.getLoc(), scf_for.upperBound(),
-        rewriter.create<UnsignedRemIOp>(
-            scf_for.getLoc(),
-            rewriter.create<SubIOp>(scf_for.getLoc(), scf_for.upperBound(),
-                                    scf_for.lowerBound()),
-            scf_for.step()));
-
-    // New primary loop with relevant min ops replaced with their constant value
-    BlockAndValueMapping mapper;
-    auto new_loop = llvm::cast<scf::ForOp>(rewriter.clone(*scf_for, mapper));
-    new_loop.setUpperBound(split_point);
-
-    new_loop->walk([&](AffineMinOp min_op) {
-      if (is_op_of_interest(min_op, new_loop.getInductionVar()))
-        rewriter.replaceOp(min_op, scf_for.step());
-    });
-
-    // Tail loop
-    // TODO(TPOPP): Remove AffineMinOps and if/else statements because this only
-    // executes if the result is less than the step size. This is for simpler
-    // code rather than appreciable performance improvements with any large
-    // inputs.
-    BlockAndValueMapping tail_mapper;
-    tail_mapper.map(scf_for.initArgs(), new_loop.results());
-    auto tail_loop =
-        llvm::cast<scf::ForOp>(rewriter.clone(*scf_for, tail_mapper));
-    tail_loop.setLowerBound(split_point);
-    rewriter.replaceOp(scf_for, tail_loop.results());
-
-    return success();
+void SplitSCFForOp(scf::ForOp scf_for) {
+  // Match only when the lower bound is zero and the step is constant.
+  // TODO(TPOPP): Requiring constant steps and lower bound simplifies things
+  // but isn't necesarilly needed
+  auto lower_bound_op =
+      llvm::dyn_cast<ConstantOp>(scf_for.lowerBound().getDefiningOp());
+  if (!lower_bound_op) {
+    return;
   }
-};
+  auto lower_bound_value = lower_bound_op.getValue().dyn_cast<IntegerAttr>();
+  if (!lower_bound_value || lower_bound_value.getInt() != 0) {
+    return;
+  }
+
+  auto step_bound_op =
+      llvm::dyn_cast<ConstantOp>(scf_for.step().getDefiningOp());
+  if (!step_bound_op) {
+    return;
+  }
+  auto step_bound_value = step_bound_op.getValue().dyn_cast<IntegerAttr>();
+  if (!step_bound_value) {
+    return;
+  }
+
+  auto loc = scf_for.getLoc();
+  ImplicitLocOpBuilder b(loc, scf_for);
+
+  // This function will determine if the min_op is an operation that can be
+  // transformed after loop splitting. This relies on the function that the op
+  // represents relative to the induction variable in its loop and the
+  // bounds of the original for loop.
+  auto is_op_of_interest = [&](AffineMinOp min_op, Value iv) {
+    if (min_op.getDimOperands().size() + min_op.getSymbolOperands().size() != 2)
+      return false;
+    bool min_by_step = false;
+    for (auto i : min_op.getAffineMap().getResults()) {
+      if (i == b.getAffineConstantExpr(step_bound_value.getInt())) {
+        min_by_step = true;
+        continue;
+      }
+      if (i == b.getAffineSymbolExpr(0) - b.getAffineDimExpr(0) &&
+          min_op.getDimOperands().front() == iv &&
+          min_op.getSymbolOperands().front() == scf_for.upperBound())
+        continue;
+      if (i == b.getAffineDimExpr(0) - b.getAffineDimExpr(1) &&
+          min_op.getDimOperands().drop_front().front() == iv &&
+          min_op.getDimOperands().front() == scf_for.upperBound())
+        continue;
+      return false;
+    }
+    return min_by_step;
+  };
+
+  // Determine if the loop should be split based on the existence of
+  // AffineMinOps of an expected form.
+  llvm::SmallVector<AffineMinOp, 1> min_ops;
+  scf_for->walk([&](AffineMinOp min_op) {
+    if (is_op_of_interest(min_op, scf_for.getInductionVar()))
+      min_ops.push_back(min_op);
+  });
+  if (min_ops.empty()) {
+    return;
+  }
+
+  // Split the loop just before a possible last iteration.
+  b.setInsertionPoint(scf_for);
+  Value split_point = b.create<SubIOp>(
+      scf_for.upperBound(),
+      b.create<UnsignedRemIOp>(
+          b.create<SubIOp>(scf_for.upperBound(), scf_for.lowerBound()),
+          scf_for.step()));
+
+  // New primary loop with relevant min ops replaced with their constant value
+  BlockAndValueMapping mapper;
+  auto new_loop = llvm::cast<scf::ForOp>(b.clone(*scf_for, mapper));
+  new_loop.setUpperBound(split_point);
+
+  new_loop->walk([&](AffineMinOp min_op) {
+    if (is_op_of_interest(min_op, new_loop.getInductionVar()))
+      min_op->replaceAllUsesWith(llvm::makeArrayRef(scf_for.step()));
+  });
+
+  // Tail loop
+  // TODO(TPOPP): Remove AffineMinOps and if/else statements because this only
+  // executes if the result is less than the step size. This is for simpler
+  // code rather than appreciable performance improvements with any large
+  // inputs.
+  BlockAndValueMapping tail_mapper;
+  tail_mapper.map(scf_for.initArgs(), new_loop.results());
+  auto tail_loop = llvm::cast<scf::ForOp>(b.clone(*scf_for, tail_mapper));
+  tail_loop.setLowerBound(split_point);
+
+  scf_for->replaceAllUsesWith(tail_loop.results());
+  scf_for.erase();
+}
 
 struct VectorizationPass : public VectorizationPassBase<VectorizationPass> {
   void getDependentDialects(DialectRegistry &registry) const override {
@@ -200,9 +194,7 @@ struct VectorizationCleanupPass
   }
 
   void runOnFunction() override {
-    OwningRewritePatternList patterns(&getContext());
-    patterns.insert<SplitSCFForOp>(&getContext());
-    (void)applyPatternsAndFoldGreedily(getFunction(), std::move(patterns));
+    getFunction().walk([](scf::ForOp op) { SplitSCFForOp(op); });
   }
 };
 
