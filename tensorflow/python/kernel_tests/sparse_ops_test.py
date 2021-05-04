d94227d43aa125ad8b54115c03cece54f6a1977b
Sanjoy Das <sanjoy@google.com>
2021-05-04 13:41:28 -0700

    Add a test case for SparseFillEmptyRows
    This add a test for an untested configuration.
    
    PiperOrigin-RevId: 371986021
    Change-Id: Ibd18ef236fe8111d1ad36258b9ce0f53a6b7a2f2

--- tensorflow/python/kernel_tests/sparse_ops_test.py
+++ tensorflow/python/kernel_tests/sparse_ops_test.py
@@ -602,6 +602,24 @@ class SparseFillEmptyRowsTest(test_util.TensorFlowTestCase):
       self.assertAllEqual(output.dense_shape, [2, 5])
       self.assertAllEqual(empty_row_indicator_out, np.zeros(2).astype(np.bool))
 
+  def testUnordered(self):
+    with test_util.use_gpu():
+      sp_input = sparse_tensor.SparseTensor(
+          indices=np.array([[2, 3], [2, 2], [0, 1], [0, 3]]),
+          values=np.array([1, 3, 2, 4]),
+          dense_shape=np.array([3, 5]))
+      sp_output, empty_row_indicator = (
+          sparse_ops.sparse_fill_empty_rows(sp_input, -1))
+
+      output, empty_row_indicator_out = self.evaluate(
+          [sp_output, empty_row_indicator])
+
+      self.assertAllEqual(output.indices,
+                          [[0, 1], [0, 3], [1, 0], [2, 3], [2, 2]])
+      self.assertAllEqual(output.values, [2, 4, -1, 1, 3])
+      self.assertAllEqual(output.dense_shape, [3, 5])
+      self.assertAllEqual(empty_row_indicator_out, [False, True, False])
+
   def testEmptyIndicesTensor(self):
     with test_util.use_gpu():
       sp_input = sparse_tensor.SparseTensor(
