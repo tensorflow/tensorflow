f5d9e2c9d7a23ffb92e94032b1b39b82795c129a
bhack <bhack@users.noreply.github.com>
2021-05-07 01:14:54 +0000

    Fix space

--- tensorflow/python/ops/ragged/ragged_constant_value_op_test.py
+++ tensorflow/python/ops/ragged/ragged_constant_value_op_test.py
@@ -73,7 +73,7 @@ class RaggedConstantValueOpTest(test_util.TensorFlowTestCase,
                   np.array([]), [[5, 6], [7, 8], [9, 0]]],
           ragged_rank=1,
           expected_shape=(3, None, 2)),
-    dict(
+      dict(
           pylist=[np.array([3, np.array(4)]), [[1, 2]],
                   np.array([]), [[5, 6], [7, 8], [9, 0]]],
           ragged_rank=1,
