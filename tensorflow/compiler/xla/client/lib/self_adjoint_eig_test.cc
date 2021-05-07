1924a5085e9e86a96a6111639e546257b924a750
Xiao Yu <fishx@google.com>
2021-05-07 16:24:44 -0700

    Fix windows build
    PiperOrigin-RevId: 372649113
    Change-Id: I01dbb63bcde6c3843ad5d4bc5e0602d9826457f9

--- tensorflow/compiler/xla/client/lib/self_adjoint_eig_test.cc
+++ tensorflow/compiler/xla/client/lib/self_adjoint_eig_test.cc
@@ -286,19 +286,26 @@ XLA_TEST_P(RandomEighTest, Random) {
                              ErrorSpec(3e-3, 0));
 }
 
+#ifndef XLA_TEST_BACKEND_CPU
 INSTANTIATE_TEST_SUITE_P(
     RandomEighTestInstantiation, RandomEighTest,
     ::testing::Values(0, 1, 2, 3, 8, 16, 32, 77, 129, 203, 256, 257, 493, 511,
-                      512
-#ifndef XLA_TEST_BACKEND_CPU
+                      512,
                       // Large tests are slow on CPU.
-                      ,
-                      513, 1000
-#endif  // XLA_TEST_BACKEND_CPU
-                      ),
+                      513, 1000),
     [](const ::testing::TestParamInfo<EighTestCase>& info) {
       const int64 size = info.param;
       return absl::StrCat(size);
     });
+#else
+INSTANTIATE_TEST_SUITE_P(
+    RandomEighTestInstantiation, RandomEighTest,
+    ::testing::Values(0, 1, 2, 3, 8, 16, 32, 77, 129, 203, 256, 257, 493, 511,
+                      512),
+    [](const ::testing::TestParamInfo<EighTestCase>& info) {
+      const int64 size = info.param;
+      return absl::StrCat(size);
+    });
+#endif  // XLA_TEST_BACKEND_CPU
 
 }  // namespace xla
