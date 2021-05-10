3e6a36332feba1a0c4cb23bd86ab2ebe4bbe05d5
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-10 11:47:14 -0700

    Currently a shape mismatch for update_state will print "Shapes {y_pred.shape} and {y_true} are incompatible" which is confusingly the opposite order of the method parameters.
    PiperOrigin-RevId: 372978089
    Change-Id: Ibfeac1ac1f9efff31c0dafc44ef21ddf94b48929

--- tensorflow/python/keras/metrics.py
+++ tensorflow/python/keras/metrics.py
@@ -3380,7 +3380,7 @@ def accuracy(y_true, y_pred):
   [y_pred, y_true], _ = \
       metrics_utils.ragged_assert_compatible_and_get_flat_values(
           [y_pred, y_true])
-  y_pred.shape.assert_is_compatible_with(y_true.shape)
+  y_true.shape.assert_is_compatible_with(y_pred.shape)
   if y_true.dtype != y_pred.dtype:
     y_pred = math_ops.cast(y_pred, y_true.dtype)
   return math_ops.cast(math_ops.equal(y_true, y_pred), backend.floatx())
