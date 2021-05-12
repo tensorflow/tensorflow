5389a96f3fdbbe21b8427fb28d4cefc103956531
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-12 16:44:03 -0700

    Handle ResourceGather for embedding optimization.
    PiperOrigin-RevId: 373478787
    Change-Id: Id66de2d68e1b90d7b0c7b2497176d273a895db87

--- tensorflow/core/grappler/op_types.cc
+++ tensorflow/core/grappler/op_types.cc
@@ -297,7 +297,7 @@ bool IsFusedBatchNormGrad(const NodeDef& node) {
 
 bool IsGather(const NodeDef& node) {
   const auto& op = node.op();
-  return op == "Gather" || op == "GatherV2";
+  return op == "Gather" || op == "GatherV2" || op == "ResourceGather";
 }
 
 bool IsGreater(const NodeDef& node) { return node.op() == "Greater"; }
