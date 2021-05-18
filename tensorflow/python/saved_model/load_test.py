036891c8554c9d61ebd68eef46b188a156254d33
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-18 14:27:44 -0700

    Remove suffix begins with ':' from the directory name to work on Windows. Also move the test in deferred_init_vars_test into the existing load_test.
    PiperOrigin-RevId: 374506005
    Change-Id: I73da14e16fafd6c16eae1535b4da334b46348232

--- tensorflow/python/saved_model/load_test.py
+++ tensorflow/python/saved_model/load_test.py
@@ -29,6 +29,8 @@ import tempfile
 import weakref
 
 from absl.testing import parameterized
+import numpy as np
+
 from tensorflow.python.client import session as session_lib
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.eager import backprop
@@ -2307,6 +2309,111 @@ class SingleCycleTests(test.TestCase, parameterized.TestCase):
       gc.collect()
     if "Exception ignored in" in stderr.getvalue():
       raise Exception(stderr.getvalue())
+
+
+class DeferredInitModuleVariablesTest(test.TestCase):
+
+  def test_deferred_init_module_variables(self):
+    """Defer initialization of variables in a module to the load stage."""
+
+    class MyModule(module.Module):
+
+      def __init__(self, size):
+        super().__init__()
+        self.size = size
+        # variable initialized by a Tensor-compatible value
+        self.w1 = variables.Variable(
+            constant_op.constant(1., shape=[self.size]), trainable=False)
+        # variable initialized by a function
+        self.w2 = variables.Variable(
+            lambda: constant_op.constant(2., shape=[self.size]))
+        # variable instantiated lazily in call()
+        self.w3 = None
+
+      def call(self):
+        if self.w3 is None:
+          self.w3 = variables.Variable(
+              constant_op.constant(3., shape=[self.size]))
+        for w in (self.w1, self.w2, self.w3):
+          w.assign_add(constant_op.constant(1., shape=[self.size]))
+        return self.w1, self.w2, self.w3
+
+    def export_initializer(initial_value, export_dir):
+
+      class Initializer(module.Module):
+
+        @def_function.function(input_signature=[])
+        def call(self):
+          if callable(initial_value):
+            return initial_value()
+          return initial_value
+
+      save.save(Initializer(), export_dir)
+
+    def create_and_save_module(weight_size):
+
+      initial_values = {}  # For storing initial_value of created variables
+
+      def variable_creator(next_creator, **kwargs):
+        variable = next_creator(**kwargs)
+        variable_name = variable.name
+        if ":" in variable_name:
+          variable_name = variable_name[:variable_name.index(":")]
+        initial_values[variable_name] = kwargs["initial_value"]
+        return variable
+
+      export_dir = self.create_tempdir().full_path
+
+      with ops.Graph().as_default():
+        with variable_scope.variable_creator_scope(variable_creator):
+          exported = MyModule(weight_size)
+          exported.call = def_function.function(input_signature=[])(
+              exported.call)
+
+          module_dir = f"{export_dir}/module"
+          file_io.recursive_create_dir(module_dir)
+          save.save_and_return_nodes(
+              exported, module_dir, experimental_skip_checkpoint=True)
+
+      # Save the initializer of the created variables.
+      for variable_name, initial_value in initial_values.items():
+        export_initializer(initial_value,
+                           f"{export_dir}/variables/{variable_name}")
+
+      return export_dir
+
+    def load_and_run_module(export_dir, weight_size):
+
+      # pylint: disable=unused-argument
+      def layer_variable_creator(next_creator, **kwargs):
+        variable_dir = f"{export_dir}/variables/{kwargs['name']}"
+        initializer = load.load(variable_dir)
+        kwargs["initial_value"] = initializer.call
+        variable = resource_variable_ops.ResourceVariable(**kwargs)
+        return variable
+
+      with ops.Graph().as_default():
+        with variable_scope.variable_creator_scope(layer_variable_creator):
+          imported = load.load(
+              f"{export_dir}/module",
+              options=load_options.LoadOptions(
+                  experimental_skip_checkpoint=True))
+        outputs = imported.call()
+
+        with self.cached_session() as sess:
+          variables.global_variables_initializer().run()
+          # Check if variables work as expected across multiple iterations.
+          for i in range(3):
+            np_outputs = sess.run(outputs)
+            for j, np_output in enumerate(np_outputs):
+              self.assertAllClose(np_output, np.full(weight_size, i + j + 2))
+
+    # The size of the serialized content (both module and variables) stays
+    # small even with a large weight_size as the initial values are not stored
+    # in checkpoints.
+    weight_size = 1024
+    export_dir = create_and_save_module(weight_size)
+    load_and_run_module(export_dir, weight_size)
 
 
 if __name__ == "__main__":
