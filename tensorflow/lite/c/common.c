dae39275be6e1c9d7d5f4b2c8eb94d0ce475f604
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-18 16:23:21 -0700

    Add casts to lite/common.c
    These casts allow the code to be compiled in C or C++ mode.
    
    PiperOrigin-RevId: 374528951
    Change-Id: Ie2f57730b77766f426b1ab1f885ce2c88a7c7106

--- tensorflow/lite/c/common.c
+++ tensorflow/lite/c/common.c
@@ -183,9 +183,9 @@ void TfLiteTensorRealloc(size_t num_bytes, TfLiteTensor* tensor) {
   }
   // TODO(b/145340303): Tensor data should be aligned.
   if (!tensor->data.raw) {
-    tensor->data.raw = malloc(num_bytes);
+    tensor->data.raw = (char*)malloc(num_bytes);
   } else if (num_bytes > tensor->bytes) {
-    tensor->data.raw = realloc(tensor->data.raw, num_bytes);
+    tensor->data.raw = (char*)realloc(tensor->data.raw, num_bytes);
   }
   tensor->bytes = num_bytes;
 }
