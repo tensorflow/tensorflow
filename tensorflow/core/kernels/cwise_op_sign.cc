0e828561c48844f7e7e84753afd1e31eed66c642
George Necula <necula@google.com>
2021-05-20 08:20:36 -0700

    Add kernels for a few more integer types for tf.math.sign.
    In a previous cl/374365828 we added registrations for tf.math.sign for int8 and int16.
    
    PiperOrigin-RevId: 374874843
    Change-Id: Ia8b8f4c374c3e1ae2c99eb4e2529acdbde952e10

--- tensorflow/core/kernels/cwise_op_sign.cc
+++ tensorflow/core/kernels/cwise_op_sign.cc
@@ -16,8 +16,9 @@ limitations under the License.
 #include "tensorflow/core/kernels/cwise_ops_common.h"
 
 namespace tensorflow {
-REGISTER8(UnaryOp, CPU, "Sign", functor::sign, float, double, int32, int64,
-          complex64, Eigen::half, bfloat16, complex128);
+REGISTER6(UnaryOp, CPU, "Sign", functor::sign, float, double, Eigen::half,
+          bfloat16, complex64, complex128);
+REGISTER4(UnaryOp, CPU, "Sign", functor::sign, int8, int16, int32, int64);
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 #if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED)
 REGISTER6(UnaryOp, GPU, "Sign", functor::sign, float, Eigen::half, double,
