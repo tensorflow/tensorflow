01408520968796068b88f2b65e5ebc54a84beef2
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-26 16:36:30 -0700

    Make HardSwish layer convertable to TFLite HARD_SWISH node
    PiperOrigin-RevId: 376055180
    Change-Id: Icf30ca9b2647164f7b3a15eac29a5b5d6e125e01

--- tensorflow/python/keras/applications/mobilenet_v3.py
+++ tensorflow/python/keras/applications/mobilenet_v3.py
@@ -437,7 +437,7 @@ def hard_sigmoid(x):
 
 
 def hard_swish(x):
-  return layers.Multiply()([hard_sigmoid(x), x])
+  return layers.Multiply()([x, hard_sigmoid(x)])
 
 
 # This function is taken from the original tf repo.
