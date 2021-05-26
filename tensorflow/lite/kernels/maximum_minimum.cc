6c0e7d37ffd56ee8894cdf995c1b08563ff616de
Mihai Maruseac <mihai.maruseac@gmail.com>
2021-05-26 10:51:23 -0700

    Update tensorflow/lite/kernels/maximum_minimum.cc

--- tensorflow/lite/kernels/maximum_minimum.cc
+++ tensorflow/lite/kernels/maximum_minimum.cc
@@ -110,22 +110,22 @@ TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
 
   switch (op_context.output->type) {
     case kTfLiteFloat32:
-      TFLiteOperation<kernel_type, float, OpType>(context, node, op_context);
+      TFLiteOperation<float, OpType>(context, node, op_context);
       break;
     case kTfLiteUInt8:
-      TFLiteOperation<kernel_type, uint8_t, OpType>(context, node, op_context);
+      TFLiteOperation<uint8_t, OpType>(context, node, op_context);
       break;
     case kTfLiteInt8:
-      TFLiteOperation<kernel_type, int8_t, OpType>(context, node, op_context);
+      TFLiteOperation<int8_t, OpType>(context, node, op_context);
       break;
     case kTfLiteInt32:
-      TFLiteOperation<kernel_type, int32_t, OpType>(context, node, op_context);
+      TFLiteOperation<int32_t, OpType>(context, node, op_context);
       break;
     case kTfLiteInt64:
-      TFLiteOperation<kernel_type, int64_t, OpType>(context, node, op_context);
+      TFLiteOperation<int64_t, OpType>(context, node, op_context);
       break;
     case kTfLiteInt16:
-      TFLiteOperation<kernel_type, int16_t, OpType>(context, node, op_context);
+      TFLiteOperation<int16_t, OpType>(context, node, op_context);
       break;
     default:
       context->ReportError(context,
