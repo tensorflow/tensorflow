f16063b1611bab376d3e8ee4dfb46ee8ec375976
Ryan Nett <JNett96@gmail.com>
2021-05-27 14:46:29 -0700

    Formatting

--- tensorflow/c/c_api.h
+++ tensorflow/c/c_api.h
@@ -258,8 +258,8 @@ TF_CAPI_EXPORT extern void TF_GraphGetTensorShape(TF_Graph* graph,
 // TF_NewOperation, but without locking the graph.
 // Should prefer TF_NewOperation when possible.
 TF_CAPI_EXPORT extern TF_OperationDescription* TF_NewOperationLocked(TF_Graph* graph,
-                                                      const char* op_type,
-                                                      const char* oper_name);
+                                                                     const char* op_type,
+                                                                     const char* oper_name);
 
 // Operation will only be added to *graph when TF_FinishOperation() is
 // called (assuming TF_FinishOperation() does not return an error).
@@ -415,7 +415,7 @@ TF_CAPI_EXPORT extern void TF_SetAttrValueProto(TF_OperationDescription* desc,
 // TF_FinishOperation, but without locking the graph.
 // TF_FinishOperation should be preferred when possible.
 TF_CAPI_EXPORT extern TF_Operation* TF_FinishOperationLocked(TF_OperationDescription* desc,
-                                              TF_Status* status);
+                                                             TF_Status* status);
 
 // If this function succeeds:
 //   * *status is set to an OK value,
