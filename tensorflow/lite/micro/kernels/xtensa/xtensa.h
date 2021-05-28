ee1dd4ec6640b46bedcc4d05e2e1a5a1b6a279ef
amandal-cad <amandal@cadence.com>
2021-05-28 15:04:47 +0530

    Update xtensa HiFi5 library calls for kernels: AvgPool, MaxPool, Add, Mul, Relu, Relu6.

--- tensorflow/lite/micro/kernels/xtensa/xtensa.h
+++ tensorflow/lite/micro/kernels/xtensa/xtensa.h
@@ -1,3 +1,24 @@
+/******************************************************************************
+ * Copyright (C) 2021 Cadence Design Systems, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to use this Software with Cadence processor cores only and
+ * not with any other processors and platforms, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
 /* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,6 +42,46 @@ limitations under the License.
 #elif ((defined(FUSION_F1)) || (defined(HIFI5)))
 #include "include/nnlib/xa_nnlib_api.h"
 #include "include/nnlib/xa_nnlib_standards.h"
+
+#define CHECK_ERR_HIFI_NNLIB_KER(ret, err_msg) \
+  if (ret != 0) {                              \
+    TF_LITE_KERNEL_LOG(context, err_msg);      \
+    return kTfLiteError;                       \
+  }
+
+#ifndef XTENSA_NNLIB_MAX_SCRATCH_SIZE
+#define XTENSA_NNLIB_MAX_SCRATCH_SIZE (70 * 1024)
+#endif
+
+#define ALLOCATE_XTENSA_NNLIB_SCRATCH_MEM \
+  uint8_t xtensa_nnlib_scratch_buf[XTENSA_NNLIB_MAX_SCRATCH_SIZE];
+
+#define MIN(a, b) (a) < (b) ? (a) : (b);
+#define MAX(a, b) (a) > (b) ? (a) : (b);
+
+#define ACTIVATION_MIN_MAX(data_type, out, inp, min, max) \
+  {                                                       \
+    data_type temp = MAX(inp, min);                       \
+    out = MIN(temp, max);                                 \
+  }
+
+#define ACTIVATION_MIN_MAX_F32(out, inp, min, max) \
+  {                                                \
+    float temp = MAX(inp, min);                    \
+    out = MIN(temp, max);                          \
+  }
+
+#define ACTIVATION_MIN_MAX_ASYM8(out, inp, min, max) \
+  {                                                  \
+    int32_t temp = MAX((int32_t)inp, min);           \
+    out = (uint8_t)MIN(temp, max);                   \
+  }
+
+#define ACTIVATION_MIN_MAX_ASYM8S(out, inp, min, max) \
+  {                                                   \
+    int32_t temp = MAX((int32_t)inp, min);            \
+    out = (int8_t)MIN(temp, max);                     \
+  }
 
 #define ALIGNED_SIZE(x, bytes) (((x) + (bytes - 1)) & (~(bytes - 1)))
 #define ALIGN_PTR(x, bytes) ((((unsigned)(x)) + (bytes - 1)) & (~(bytes - 1)))
