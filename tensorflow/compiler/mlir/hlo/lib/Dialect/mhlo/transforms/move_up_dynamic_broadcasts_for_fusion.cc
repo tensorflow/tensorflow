7e71a1eb87302bca4d7aaede8668462df1438afe
A. Unique TensorFlower <gardener@tensorflow.org>
2021-05-31 13:49:21 -0700

    [MLIR][HLO] Eliminate duplicate broadcastable constraints
    PiperOrigin-RevId: 376718433
    Change-Id: Ibeab8ec3419c34d0e2e29ccb58cf5ce22cb5a0d4

--- tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/transforms/move_up_dynamic_broadcasts_for_fusion.cc
+++ tensorflow/compiler/mlir/hlo/lib/Dialect/mhlo/transforms/move_up_dynamic_broadcasts_for_fusion.cc
@@ -306,25 +306,58 @@ struct MergeAssumingOpsPattern : public OpRewritePattern<shape::AssumingOp> {
   }
 };
 
-struct EliminateDuplicateCstrBroadcastableOps
-    : public OpRewritePattern<shape::CstrBroadcastableOp> {
-  using OpRewritePattern<shape::CstrBroadcastableOp>::OpRewritePattern;
+// Eliminate casted extent tensors. Instead, produce the concrete extent tensor
+// type where possible.
+struct CanonicalizeCastedShapeOfOpPattern
+    : public OpRewritePattern<tensor::CastOp> {
+  using OpRewritePattern<tensor::CastOp>::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(shape::CstrBroadcastableOp op,
+  LogicalResult matchAndRewrite(tensor::CastOp op,
                                 PatternRewriter &rewriter) const override {
-    // Search for previous occurence of the same constraint.
-    Operation *it = op->getPrevNode();
-    while (it != nullptr) {
-      if (auto candidate = llvm::dyn_cast<shape::CstrBroadcastableOp>(it)) {
-        if (candidate.shapes() == op.shapes()) {
-          rewriter.replaceOp(op, candidate.result());
-          return success();
-        }
+    // Only merge tensor cast into `shape_of` ops.
+    auto shape_of_op = op.source().getDefiningOp<shape::ShapeOfOp>();
+    if (!shape_of_op) return failure();
+
+    // Desired type must be an extent tensor type.
+    auto result_ty = op.getType().dyn_cast<RankedTensorType>();
+    if (!result_ty || result_ty.getRank() != 1 ||
+        !result_ty.getElementType().isIndex())
+      return failure();
+
+    rewriter.replaceOpWithNewOp<shape::ShapeOfOp>(op, result_ty,
+                                                  shape_of_op.arg());
+    if (shape_of_op->getUses().empty()) rewriter.eraseOp(shape_of_op);
+    return success();
+  }
+};
+
+// TODO(frgossen): Remove this once it has landed upstream.
+struct CanonicalizeBroadcastPattern
+    : public OpRewritePattern<shape::BroadcastOp> {
+  using OpRewritePattern<shape::BroadcastOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(shape::BroadcastOp op,
+                                PatternRewriter &rewriter) const override {
+    // Only concretize dynamic extent tensor result types.
+    auto resultTy = op.getType().dyn_cast<RankedTensorType>();
+    if (!resultTy || !resultTy.isDynamicDim(0)) return failure();
+
+    // Infer resulting shape rank if possible.
+    int64_t maxRank = 0;
+    for (Value shape : op.shapes()) {
+      if (auto extentTensorTy = shape.getType().dyn_cast<RankedTensorType>()) {
+        // Cannot infer resulting shape rank if any operand is dynamically
+        // ranked.
+        if (extentTensorTy.isDynamicDim(0)) return failure();
+        maxRank = std::max(maxRank, extentTensorTy.getDimSize(0));
       }
-      it = it->getPrevNode();
     }
 
-    return failure();
+    auto newOp = rewriter.create<shape::BroadcastOp>(
+        op.getLoc(), RankedTensorType::get({maxRank}, rewriter.getIndexType()),
+        op.shapes());
+    rewriter.replaceOpWithNewOp<tensor::CastOp>(op, op.getType(), newOp);
+    return success();
   }
 };
 
@@ -399,7 +432,8 @@ void PopulateMoveUpDynamicBroadcastsForFusionPatterns(
     MLIRContext *context, OwningRewritePatternList *patterns) {
   // clang-format off
   patterns->insert<
-      EliminateDuplicateCstrBroadcastableOps,
+      CanonicalizeBroadcastPattern,
+      CanonicalizeCastedShapeOfOpPattern,
       InlineBroadcastedShapeOperandsPattern<shape::CstrBroadcastableOp>,
       MergeAssumingOpsPattern,
       MoveIntoAssumingOpPattern<shape::ShapeOfOp>,
@@ -409,7 +443,6 @@ void PopulateMoveUpDynamicBroadcastsForFusionPatterns(
       MoveUpBroadcastInDimOpPattern,
       ShapeReificationPattern>(context);
   // clang-format on
-  shape::BroadcastOp::getCanonicalizationPatterns(*patterns, context);
   tensor::CastOp::getCanonicalizationPatterns(*patterns, context);
 }
 
