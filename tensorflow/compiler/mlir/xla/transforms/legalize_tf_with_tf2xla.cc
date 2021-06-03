80346c60bbc52a6c33acf4f1e2a7f0f4b36118b6
Smit Hinsu <hinsu@google.com>
2021-06-03 13:10:08 -0700

    Emit remark instead of an error in xla-legalize-tf-with-tf2xla pass
    These are not errors on the IR and we do not fail the pass on these so remark is more suitable for this.
    
    PiperOrigin-RevId: 377356577
    Change-Id: If76ea6f2a44fba94145e963bceddcac81be7dd57

--- tensorflow/compiler/mlir/xla/transforms/legalize_tf_with_tf2xla.cc
+++ tensorflow/compiler/mlir/xla/transforms/legalize_tf_with_tf2xla.cc
@@ -506,7 +506,7 @@ LogicalResult Tf2XlaRewriter::PrepareParams() {
       device_->resource_manager(),
       tensorflow::XlaContext::kXlaContextResourceName, context_);
   if (!status.ok()) {
-    return emitError(op_->getLoc())
+    return emitRemark(op_->getLoc())
            << "failed to create XlaContext resource: " << status.ToString();
   }
   params_.step_container = step_container_.get();
@@ -654,8 +654,9 @@ LogicalResult Tf2XlaRewriter::LegalizeOp() {
         tensorflow::XlaExpression::CastExpressionFromTensor(*output);
     if (expr->kind() != tensorflow::XlaExpression::Kind::kXlaOp &&
         expr->kind() != tensorflow::XlaExpression::Kind::kConstant) {
-      return op_->emitError(
-          "expects XlaExpression of kind kXlaOp in compiled output");
+      return op_->emitRemark(
+          "expects XlaExpression of kind kXlaOp or kConstant in compiled "
+          "output");
     }
     mlir::Value value = hlo_builder_.GetValue(expr->AsXlaOp(&hlo_builder_));
     mlir::OpResult old_result = op_->getResult(i);
