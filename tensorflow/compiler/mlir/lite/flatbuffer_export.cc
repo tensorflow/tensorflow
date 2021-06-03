c1e5963a5fc487709aa137c2bb90659b64201873
Karim Nosir <karimnosseir@google.com>
2021-06-03 11:40:14 -0700

    [lite] Add TFL_VarHandleOp to tflite ops. Update converter_gen to export it as custom ops. Also, update Read/Assign variable ops to accept TFL_ResourceTensor
    PiperOrigin-RevId: 377336943
    Change-Id: Ic54738f75a318da598dcc0eb801592c05f6be18d

--- tensorflow/compiler/mlir/lite/flatbuffer_export.cc
+++ tensorflow/compiler/mlir/lite/flatbuffer_export.cc
@@ -562,6 +562,12 @@ class Translator {
       T op, const std::string& op_name, const std::vector<int32_t>& operands,
       const std::vector<int32_t>& results);
 
+  // Builds VarHandle op.
+  BufferOffset<tflite::Operator> BuildVarHandleOp(
+      mlir::TFL::VarHandleOp op, const std::string& op_name,
+      const std::vector<int32_t>& operands,
+      const std::vector<int32_t>& results);
+
   BufferOffset<tflite::Operator> BuildCustomOperator(
       Operation* inst, mlir::TFL::CustomOp op,
       const std::vector<int32_t>& operands,
@@ -974,6 +980,25 @@ BufferOffset<tflite::Operator> Translator::BuildNumericVerifyOperator(
       tflite::CustomOptionsFormat_FLEXBUFFERS);
 }
 
+BufferOffset<tflite::Operator> Translator::BuildVarHandleOp(
+    mlir::TFL::VarHandleOp op, const std::string& op_name,
+    const std::vector<int32_t>& operands, const std::vector<int32_t>& results) {
+  auto opcode_index = GetOpcodeIndex(op_name, tflite::BuiltinOperator_CUSTOM);
+  auto fbb = absl::make_unique<flexbuffers::Builder>();
+  fbb->Map([&]() {
+    fbb->String("shared_name", op.shared_name().str());
+    fbb->String("container", op.container().str());
+  });
+  fbb->Finish();
+  auto f = std::unique_ptr<flexbuffers::Builder>(fbb.release());
+  auto custom_option = f->GetBuffer();
+  return tflite::CreateOperator(
+      builder_, opcode_index, builder_.CreateVector(operands),
+      builder_.CreateVector(results), tflite::BuiltinOptions_NONE,
+      /*builtin_options=*/0, builder_.CreateVector<uint8_t>(custom_option),
+      tflite::CustomOptionsFormat_FLEXBUFFERS);
+}
+
 // Builds Assign/Read Variable ops.
 template <typename T>
 BufferOffset<tflite::Operator> Translator::BuildVariableOperator(
@@ -1128,7 +1153,7 @@ Optional<BufferOffset<tflite::Operator>> Translator::BuildOperator(
 
   // TODO(b/149099381): Remove this once the kernels are promoted as
   // builtin TFLite kernels.
-  // We export the Assign/Read variable ops as custom ops.
+  // We export the VarHandle, Assign, and Read variable ops as custom ops.
   if (auto read_op = llvm::dyn_cast<mlir::TFL::ReadVariableOp>(inst)) {
     return BuildVariableOperator<mlir::TFL::ReadVariableOp>(
         read_op, "ReadVariable", operands, results);
@@ -1136,6 +1161,9 @@ Optional<BufferOffset<tflite::Operator>> Translator::BuildOperator(
                  llvm::dyn_cast<mlir::TFL::AssignVariableOp>(inst)) {
     return BuildVariableOperator<mlir::TFL::AssignVariableOp>(
         assign_op, "AssignVariable", operands, results);
+  } else if (auto var_handle_op =
+                 llvm::dyn_cast<mlir::TFL::VarHandleOp>(inst)) {
+    return BuildVarHandleOp(var_handle_op, "VarHandle", operands, results);
   }
 
   // If TFLite built in op, create operator as a builtin op.
