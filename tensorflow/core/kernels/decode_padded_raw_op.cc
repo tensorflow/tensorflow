62dc7368fe6f2ab80c5f878634893298cd0653ad
Mihai Maruseac <mihai.maruseac@gmail.com>
2021-06-02 19:08:28 -0700

    Update tensorflow/core/kernels/decode_padded_raw_op.cc

--- tensorflow/core/kernels/decode_padded_raw_op.cc
+++ tensorflow/core/kernels/decode_padded_raw_op.cc
@@ -19,7 +19,6 @@ limitations under the License.
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/framework/op_requires.h"
 #include "tensorflow/core/framework/shape_inference.h"
 
 namespace tensorflow {
