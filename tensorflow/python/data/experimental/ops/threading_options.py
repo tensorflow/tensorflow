2a40dd7a4e72ec3ed257a488db0730f5315fe53e
Vignesh Kothapalli <vikoth18@in.ibm.com>
2021-06-08 23:36:10 +0530

    [tf.data] deprecate experimental ThreadingOptions

--- tensorflow/python/data/experimental/ops/threading_options.py
+++ tensorflow/python/data/experimental/ops/threading_options.py
@@ -19,11 +19,13 @@ from __future__ import print_function
 
 
 from tensorflow.core.framework import dataset_options_pb2
+from tensorflow.python.util import deprecation
 from tensorflow.python.data.util import options
 from tensorflow.python.util.tf_export import tf_export
 
 
-@tf_export("data.experimental.ThreadingOptions")
+@deprecation.deprecated("data.experimental.ThreadingOptions")
+@tf_export("data.experimental.ThreadingOptions", "data.ThreadingOptions")
 class ThreadingOptions(options.OptionsBase):
   """Represents options for dataset threading.
 
