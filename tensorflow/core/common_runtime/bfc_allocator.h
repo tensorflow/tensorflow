32f65f59b1f7d34f4390eb6ed57538a7deeea346
Antonio Sanchez <cantonios@google.com>
2021-06-10 14:18:36 -0700

    Reverting due to cuda_asan failure.
    The unowned `Chunk::op_name` char pointer is running out of scope, but in_use() is still returning true, resulting in use after free.
    
    PiperOrigin-RevId: 378733008
    Change-Id: I35ddabd0f2f7ff4890917931267a90fe5318a0ea

--- tensorflow/core/common_runtime/bfc_allocator.h
+++ tensorflow/core/common_runtime/bfc_allocator.h
@@ -196,24 +196,12 @@ class BFCAllocator : public Allocator {
 
     bool in_use() const { return allocation_id != -1; }
 
+#ifdef TENSORFLOW_MEM_DEBUG
     // optional debugging info
     const char* op_name = nullptr;
     uint64 step_id = 0;
-    uint64 action_count = 0;
-
-    // Get the op name used for memory debugging.
-    const char* GetDebugOpName() const {
-      // If chunk is not in use, although the op_name pointer is not nullptr,
-      // the corresponding OpKernel might have already been deallocated, and the
-      // op_name pointer might point to invalid memory. So in this case, return
-      // a special op name "UNUSED";
-      if (!in_use())
-        return "UNUSED";
-      else if (op_name)
-        return op_name;
-      else
-        return "UNKNOWN";
-    }
+    int64 action_count = 0;
+#endif
 
     string DebugString(BFCAllocator* a,
                        bool recurse) TF_NO_THREAD_SAFETY_ANALYSIS {
@@ -230,9 +218,11 @@ class BFCAllocator : public Allocator {
         Chunk* n = a->ChunkFromHandle(next);
         strings::StrAppend(&dbg, ", next: ", n->DebugString(a, false));
       }
-      strings::StrAppend(&dbg, ", for: ", GetDebugOpName(),
+#ifdef TENSORFLOW_MEM_DEBUG
+      strings::StrAppend(&dbg, ", for: ", op_name ? op_name : "UNKNOWN",
                          ", stepid: ", step_id,
                          ", last_action: ", action_count);
+#endif
       return dbg;
     }
   };
@@ -613,11 +603,11 @@ class BFCAllocator : public Allocator {
 
   // Stats.
   AllocatorStats stats_ TF_GUARDED_BY(lock_);
-  uint64 action_counter_ TF_GUARDED_BY(lock_);
-
-  // The circular buffer used to track memory operation history.
-  static constexpr uint64 kMemDebugHistorySize = 4096;
-  int64 size_history_[kMemDebugHistorySize];
+#ifdef TENSORFLOW_MEM_DEBUG
+  int64 action_counter_ = 0 TF_GUARDED_BY(lock_);
+#define MEM_DEBUG_SIZE_HISTORY_SIZE 4096
+  int64 size_history_[MEM_DEBUG_SIZE_HISTORY_SIZE];
+#endif
 
   friend class GPUBFCAllocatorPrivateMethodsTest;
   friend class GPUBFCAllocatorPrivateMethodsTest_SubAllocatorSpecific;
