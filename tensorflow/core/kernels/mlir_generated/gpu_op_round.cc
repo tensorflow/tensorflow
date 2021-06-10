bba4c4a5524522ac24df5bfe8a4c4843a084a990
A. Unique TensorFlower <gardener@tensorflow.org>
2021-06-10 13:41:20 -0700

    [MLIR][KernelGen] Add MLIR-generated `tf.Round` kernel
    PiperOrigin-RevId: 378725030
    Change-Id: I531306aa301fe1b6087fd8e53dcdb240c7a21429

--- tensorflow/core/kernels/mlir_generated/gpu_op_round.cc
+++ tensorflow/core/kernels/mlir_generated/gpu_op_round.cc
@@ -1,4 +1,4 @@
-/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,19 +13,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
-
-#include "tensorflow/core/kernels/cwise_ops_gpu_common.cu.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/kernels/mlir_generated/base_gpu_op.h"
 
 namespace tensorflow {
-namespace functor {
 
-#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
-    !defined(MLIR_GENERATED_EXPERIMENTAL_KERNELS_ENABLED)
-DEFINE_UNARY5(round, Eigen::half, float, double, int32, int64);
-#endif
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Round, DT_HALF);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Round, DT_FLOAT);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Round, DT_DOUBLE);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Round, DT_INT32);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Round, DT_INT64);
 
-}  // namespace functor
 }  // namespace tensorflow
-
-#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
