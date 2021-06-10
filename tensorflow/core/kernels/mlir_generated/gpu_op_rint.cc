be5b680688bfea7527dcdc8695a93c557bb0f37b
A. Unique TensorFlower <gardener@tensorflow.org>
2021-06-10 01:43:31 -0700

    [MLIR][KernelGen] Add `tf.Rint` kernel
    PiperOrigin-RevId: 378600537
    Change-Id: Ieb382fac3231d0607dfbe6f8ae5dd88ea98ecdd1

--- tensorflow/core/kernels/mlir_generated/gpu_op_rint.cc
+++ tensorflow/core/kernels/mlir_generated/gpu_op_rint.cc
@@ -1,4 +1,4 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,19 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
-
-#include "tensorflow/core/kernels/cwise_ops_gpu_common.cu.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/kernels/mlir_generated/base_gpu_op.h"
 
 namespace tensorflow {
-namespace functor {
 
-#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
-    !defined(MLIR_GENERATED_EXPERIMENTAL_KERNELS_ENABLED)
-DEFINE_UNARY2(rint, float, double);
-#endif
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Rint, DT_FLOAT);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Rint, DT_DOUBLE);
 
-}  // namespace functor
 }  // namespace tensorflow
-
-#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
