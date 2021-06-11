e269917bc32cb7a508dfa0c3776245e3b92bd88b
Jiri Simsa <jsimsa@google.com>
2021-06-11 15:36:03 -0700

    [tf.data] Minor cleanup.
    PiperOrigin-RevId: 378963238
    Change-Id: I49c8641b8d2b5b83c097d34659042b729dd67e7f

--- tensorflow/python/data/experimental/ops/optimization_options.py
+++ tensorflow/python/data/experimental/ops/optimization_options.py
@@ -23,9 +23,6 @@ from tensorflow.core.framework import dataset_options_pb2
 from tensorflow.python.data.util import options
 from tensorflow.python.util.tf_export import tf_export
 
-# Do not modify.
-_ENABLE_AUTOTUNE_BUFFERS_BY_DEFAULT = False
-
 
 class _AutotuneAlgorithm(enum.Enum):
   """Controls what algorithm is used in the autotune implementation."""
