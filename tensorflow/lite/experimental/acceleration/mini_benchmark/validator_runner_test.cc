691fbb00d376c335159e667f88d3252a9f0b10cd
Chao Mei <chaomei@google.com>
2021-06-18 21:05:46 -0700

    Clean up to make the code more readable.
    PiperOrigin-RevId: 380313457
    Change-Id: I4d5f7c9379879583e42afc0a3d533c4b4ba0c469

--- tensorflow/lite/experimental/acceleration/mini_benchmark/validator_runner_test.cc
+++ tensorflow/lite/experimental/acceleration/mini_benchmark/validator_runner_test.cc
@@ -44,30 +44,12 @@ namespace {
 
 class ValidatorRunnerTest : public ::testing::Test {
  protected:
-  std::string GetTestSrcDir() {
-    const char* from_env = getenv("TEST_SRCDIR");
-    if (from_env) {
-      return from_env;
-    }
-    return "/data/local/tmp";
-  }
-  std::string GetTestTmpDir() {
-    const char* from_env = getenv("TEST_TMPDIR");
-    if (from_env) {
-      return from_env;
-    }
-    return "/data/local/tmp";
-  }
   void* LoadEntryPointModule() {
 #ifndef _WIN32
-    std::string path = GetTestSrcDir() + "/libvalidator_runner_so_for_tests.so";
+    std::string path = ::testing::TempDir() +
+                       "/tensorflow/lite/experimental/acceleration/"
+                       "mini_benchmark/libvalidator_runner_so_for_tests.so";
     void* module = dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL | RTLD_NODELETE);
-    if (!module) {
-      path = GetTestSrcDir() +
-             "/tensorflow/lite/experimental/acceleration/"
-             "mini_benchmark/libvalidator_runner_so_for_tests.so";
-      module = dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL | RTLD_NODELETE);
-    }
     EXPECT_TRUE(module) << dlerror();
     return module;
 #else   // _WIN32
@@ -75,7 +57,7 @@ class ValidatorRunnerTest : public ::testing::Test {
 #endif  // !_WIN32
   }
 
-  void WriteFile(const std::string rootdir, const std::string& filename,
+  void WriteFile(const std::string& rootdir, const std::string& filename,
                  const unsigned char* data, size_t length) {
     std::string dir = rootdir +
                       "/tensorflow/lite/experimental/"
@@ -100,17 +82,17 @@ class ValidatorRunnerTest : public ::testing::Test {
       return;
     }
     ASSERT_NO_FATAL_FAILURE(
-        WriteFile(GetTestSrcDir(), "librunner_main.so",
+        WriteFile(::testing::TempDir(), "librunner_main.so",
                   g_tflite_acceleration_embedded_runner,
                   g_tflite_acceleration_embedded_runner_len));
     ASSERT_NO_FATAL_FAILURE(WriteFile(
-        GetTestSrcDir(), "libvalidator_runner_so_for_tests.so",
+        ::testing::TempDir(), "libvalidator_runner_so_for_tests.so",
         g_tflite_acceleration_embedded_validator_runner_so_for_tests,
         g_tflite_acceleration_embedded_validator_runner_so_for_tests_len));
     EXPECT_TRUE(LoadEntryPointModule());
 #endif
     ASSERT_NO_FATAL_FAILURE(WriteFile(
-        GetTestTmpDir(), "mobilenet_quant_with_validation.tflite",
+        ::testing::TempDir(), "mobilenet_quant_with_validation.tflite",
         g_tflite_acceleration_embedded_mobilenet_validation_model,
         g_tflite_acceleration_embedded_mobilenet_validation_model_len));
   }
@@ -125,34 +107,34 @@ class ValidatorRunnerTest : public ::testing::Test {
     }
 #endif  // __ANDROID__
 
-    std::unique_ptr<ValidatorRunner> validator, validator2;
+    std::unique_ptr<ValidatorRunner> validator1, validator2;
     std::string model_path =
-        GetTestTmpDir() +
+        ::testing::TempDir() +
         "/tensorflow/lite/experimental/acceleration/"
         "mini_benchmark/mobilenet_quant_with_validation.tflite";
 
-    std::string storage_path = GetTestTmpDir() + "/storage_path.fb";
+    std::string storage_path = ::testing::TempDir() + "/storage_path.fb";
     (void)unlink(storage_path.c_str());
     if (use_path) {
-      validator = std::make_unique<ValidatorRunner>(model_path, storage_path,
-                                                    GetTestTmpDir());
+      validator1 = std::make_unique<ValidatorRunner>(model_path, storage_path,
+                                                     ::testing::TempDir());
       validator2 = std::make_unique<ValidatorRunner>(model_path, storage_path,
-                                                     GetTestTmpDir());
+                                                     ::testing::TempDir());
     } else {
       int fd = open(model_path.c_str(), O_RDONLY);
       ASSERT_GE(fd, 0);
       struct stat stat_buf = {0};
       ASSERT_EQ(fstat(fd, &stat_buf), 0);
-      validator = std::make_unique<ValidatorRunner>(
-          fd, 0, stat_buf.st_size, storage_path, GetTestTmpDir());
+      validator1 = std::make_unique<ValidatorRunner>(
+          fd, 0, stat_buf.st_size, storage_path, ::testing::TempDir());
       validator2 = std::make_unique<ValidatorRunner>(
-          fd, 0, stat_buf.st_size, storage_path, GetTestTmpDir());
+          fd, 0, stat_buf.st_size, storage_path, ::testing::TempDir());
     }
-    ASSERT_EQ(validator->Init(), kMinibenchmarkSuccess);
+    ASSERT_EQ(validator1->Init(), kMinibenchmarkSuccess);
     ASSERT_EQ(validator2->Init(), kMinibenchmarkSuccess);
 
     std::vector<const BenchmarkEvent*> events =
-        validator->GetAndFlushEventsToLog();
+        validator1->GetAndFlushEventsToLog();
     ASSERT_TRUE(events.empty());
 
     std::vector<const TFLiteSettings*> settings;
@@ -169,18 +151,15 @@ class ValidatorRunnerTest : public ::testing::Test {
     }
     fbb_gpu.Finish(CreateTFLiteSettings(fbb_gpu, Delegate_GPU));
 #ifdef __ANDROID__
-    if (!android_info.is_emulator) {
-      // GPU doesn't run on emulators.
-      settings.push_back(
-          flatbuffers::GetRoot<TFLiteSettings>(fbb_gpu.GetBufferPointer()));
-    }
+    settings.push_back(
+        flatbuffers::GetRoot<TFLiteSettings>(fbb_gpu.GetBufferPointer()));
 #endif  // __ANDROID__
 
-    ASSERT_EQ(validator->TriggerMissingValidation(settings), settings.size());
+    ASSERT_EQ(validator1->TriggerMissingValidation(settings), settings.size());
 
     int event_count = 0;
     while (event_count < settings.size()) {
-      events = validator->GetAndFlushEventsToLog();
+      events = validator1->GetAndFlushEventsToLog();
       event_count += events.size();
       for (const BenchmarkEvent* event : events) {
         std::string delegate_name = "CPU";
@@ -205,10 +184,8 @@ class ValidatorRunnerTest : public ::testing::Test {
       sleep(1);
 #endif  // !_WIN32
     }
-#if !defined(__aarch64__)
-    // Out-of-process running doesn't work on 64-bit arm emulators.
+
     EXPECT_EQ(validator2->TriggerMissingValidation(settings), 0);
-#endif
   }
 };
 
