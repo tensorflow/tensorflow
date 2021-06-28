8c82372aa7a0f0dfb18fe8fed32be5ba773e04ec
Mihai Maruseac <mihaimaruseac@google.com>
2021-06-28 16:36:21 -0700

    Update tensorflow/tools/pip_package/setup.py

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -102,7 +102,7 @@ REQUIRED_PACKAGES = [
     # They are updated during the release process
     # When updating these, please also update the nightly versions below
     'tensorboard ~= 2.5',
-    'tf-estimator-nightly ~= 2.6.0.dev2021062501',
+    'tf-estimator-nightly == 2.6.0.dev2021062501',
     # TODO(scottzhu): OSS keras hasn't been formally released yet.
     # Use keras-nightly at the moment.
     'keras ~= 2.6.0rc0',
