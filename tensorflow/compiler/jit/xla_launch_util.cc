769a34d0197d3cb4f4adefb52b25c7b065b35373
Trent Lo <trentl@nvidia.com>
2021-06-29 16:28:54 -0700

    [XLA/GPU] Use arg_num to testing variable updates.

--- tensorflow/compiler/jit/xla_launch_util.cc
+++ tensorflow/compiler/jit/xla_launch_util.cc
@@ -262,7 +262,8 @@ XlaComputationLaunchContext::PopulateInputs(
         is_resource_variable &&
         absl::c_any_of(compilation_result->resource_updates,
                        [&](const XlaCompiler::ResourceUpdate& update) {
-                         return update.input_index == i && update.modified;
+                         return update.input_index == arg_num &&
+                                update.modified;
                        });
 
     const Tensor* t = is_resource_variable
