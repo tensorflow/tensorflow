a3c88115d56a5b262a267bbddfc29d7b369bb938
Terry Heo <terryheo@google.com>
2021-06-28 18:30:57 -0700

    analyzer: Support `model_path` and `model_content` as a model input
    ModelAnalyzer.analzer() comes to have named arguments `model_path`
    and `model_content` as lite.Interpreter() API.
    
    Now the following example works.
    example)
      converter = tf.lite.TFLiteConverter.from_concrete_functions(
          [func.get_concrete_function()])
      fb_model = converter.convert()
      txt = analyzer.ModelAnalyzer.analyze(model_content=fb_model, result_format='txt')
    
    PiperOrigin-RevId: 381988175
    Change-Id: I5c2fa3de792df5599a06ef2353755380de17e8d1

--- tensorflow/lite/python/analyzer_test.py
+++ tensorflow/lite/python/analyzer_test.py
@@ -14,6 +14,8 @@
 # ==============================================================================
 """Tests for analyzer package."""
 
+import tensorflow as tf
+
 from tensorflow.lite.python import analyzer
 from tensorflow.python.framework import test_util
 from tensorflow.python.platform import resource_loader
@@ -23,24 +25,24 @@ from tensorflow.python.platform import test
 class ConvertTest(test_util.TensorFlowTestCase):
 
   def testTxt(self):
-    model_path = resource_loader.get_path_to_datafile(
-        '../testdata/add.bin')
-    txt = analyzer.ModelAnalyzer.analyze(model_path, 'txt')
+    model_path = resource_loader.get_path_to_datafile('../testdata/add.bin')
+    txt = analyzer.ModelAnalyzer.analyze(
+        model_path=model_path, result_format='txt')
     self.assertIn('Subgraph#0(T#1) -> [T#2]', txt)
     self.assertIn('Op#0 ADD(T#1, T#1) -> [T#0]', txt)
     self.assertIn('Op#1 ADD(T#0, T#1) -> [T#2]', txt)
 
   def testHtml(self):
-    model_path = resource_loader.get_path_to_datafile(
-        '../testdata/add.bin')
-    html = analyzer.ModelAnalyzer.analyze(model_path, 'html')
+    model_path = resource_loader.get_path_to_datafile('../testdata/add.bin')
+    html = analyzer.ModelAnalyzer.analyze(
+        model_path=model_path, result_format='html')
     self.assertIn('<html>\n<head>', html)
     self.assertIn('ADD (0)', html)
 
   def testMlir(self):
-    model_path = resource_loader.get_path_to_datafile(
-        '../testdata/add.bin')
-    mlir = analyzer.ModelAnalyzer.analyze(model_path, 'mlir')
+    model_path = resource_loader.get_path_to_datafile('../testdata/add.bin')
+    mlir = analyzer.ModelAnalyzer.analyze(
+        model_path=model_path, result_format='mlir')
     self.assertIn(
         'func @main(%arg0: tensor<1x8x8x3xf32>) -> '
         'tensor<1x8x8x3xf32> attributes '
@@ -51,16 +53,68 @@ class ConvertTest(test_util.TensorFlowTestCase):
     self.assertIn(
         '%1 = tfl.add %0, %arg0 {fused_activation_function = "NONE"} : '
         'tensor<1x8x8x3xf32>', mlir)
-    self.assertIn(
-        'return %1 : tensor<1x8x8x3xf32>', mlir)
+    self.assertIn('return %1 : tensor<1x8x8x3xf32>', mlir)
 
   def testMlirHugeConst(self):
     model_path = resource_loader.get_path_to_datafile(
         '../testdata/conv_huge_im2col.bin')
-    mlir = analyzer.ModelAnalyzer.analyze(model_path, 'mlir')
+    mlir = analyzer.ModelAnalyzer.analyze(
+        model_path=model_path, result_format='mlir')
     self.assertIn(
         '%1 = "tfl.pseudo_const"() {value = opaque<"_", "0xDEADBEEF"> : '
         'tensor<3x3x3x8xf32>} : () -> tensor<3x3x3x8xf32>', mlir)
+
+  def testTxtWithFlatBufferModel(self):
+
+    @tf.function(
+        input_signature=[tf.TensorSpec(shape=[None], dtype=tf.float32)])
+    def func(x):
+      return x + tf.cos(x)
+
+    converter = tf.lite.TFLiteConverter.from_concrete_functions(
+        [func.get_concrete_function()])
+    fb_model = converter.convert()
+    txt = analyzer.ModelAnalyzer.analyze(
+        model_content=fb_model, result_format='txt')
+    self.assertIn('Subgraph#0 main(T#0) -> [T#2]', txt)
+    self.assertIn('Op#0 COS(T#0) -> [T#1]', txt)
+    self.assertIn('Op#1 ADD(T#0, T#1) -> [T#2]', txt)
+
+  def testMlirWithFlatBufferModel(self):
+
+    @tf.function(
+        input_signature=[tf.TensorSpec(shape=[None], dtype=tf.float32)])
+    def func(x):
+      return x + tf.cos(x)
+
+    converter = tf.lite.TFLiteConverter.from_concrete_functions(
+        [func.get_concrete_function()])
+    fb_model = converter.convert()
+    mlir = analyzer.ModelAnalyzer.analyze(
+        model_content=fb_model, result_format='mlir')
+    self.assertIn('func @main(%arg0: tensor<?xf32>) -> tensor<?xf32>', mlir)
+    self.assertIn('%0 = "tfl.cos"(%arg0) : (tensor<?xf32>) -> tensor<?xf32>',
+                  mlir)
+    self.assertIn(
+        '%1 = tfl.add %arg0, %0 {fused_activation_function = "NONE"} : '
+        'tensor<?xf32>', mlir)
+    self.assertIn('return %1 : tensor<?xf32', mlir)
+
+  def testHtmlWithFlatBufferModel(self):
+
+    @tf.function(
+        input_signature=[tf.TensorSpec(shape=[None], dtype=tf.float32)])
+    def func(x):
+      return x + tf.cos(x)
+
+    converter = tf.lite.TFLiteConverter.from_concrete_functions(
+        [func.get_concrete_function()])
+    fb_model = converter.convert()
+    html = analyzer.ModelAnalyzer.analyze(
+        model_content=fb_model, result_format='html')
+    self.assertIn('<html>\n<head>', html)
+    self.assertIn('COS (0)', html)
+    self.assertIn('ADD (1)', html)
 
 
 if __name__ == '__main__':
