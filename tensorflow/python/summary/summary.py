b2cd199c859b1c2fd5ca75f441acdd259340f07a
A. Unique TensorFlower <gardener@tensorflow.org>
2021-07-02 11:14:01 -0700

    Add TF2 migration block for tf.compat.v1.summary.FileWriter, tf.compat.v1.summary.merge_all, tf.compat.v1.summary.summary.merge.
    PiperOrigin-RevId: 382776974
    Change-Id: Ie6a37c93e1c79e3c583b568a2141fde94e6d7bef

--- tensorflow/python/summary/summary.py
+++ tensorflow/python/summary/summary.py
@@ -493,9 +493,45 @@ def merge(inputs, collections=None, name=None):
   Raises:
     RuntimeError: If called with eager mode enabled.
 
-  @compatibility(eager)
-  Not compatible with eager execution. To write TensorBoard
-  summaries under eager execution, use `tf.contrib.summary` instead.
+  @compatibility(TF2)
+  This API is not compatible with eager execution or `tf.function`. To migrate
+  to TF2, this API can be omitted entirely, because in TF2 individual summary
+  ops, like `tf.summary.scalar()`, write directly to the default summary writer
+  if one is active. Thus, it's not necessary to merge summaries or to manually
+  add the resulting merged summary output to the writer. See the usage example
+  shown below.
+
+  For a comprehensive `tf.summary` migration guide, please follow
+  [Migrating tf.summary usage to
+  TF 2.0](https://www.tensorflow.org/tensorboard/migrate#in_tf_1x).
+
+  #### TF1 & TF2 Usage Example
+
+  TF1:
+
+  ```python
+  dist = tf.compat.v1.placeholder(tf.float32, [100])
+  tf.compat.v1.summary.histogram(name="distribution", values=dist)
+  writer = tf.compat.v1.summary.FileWriter("/tmp/tf1_summary_example")
+  summaries = tf.compat.v1.summary.merge_all()
+
+  sess = tf.compat.v1.Session()
+  for step in range(100):
+    mean_moving_normal = np.random.normal(loc=step, scale=1, size=[100])
+    summ = sess.run(summaries, feed_dict={dist: mean_moving_normal})
+    writer.add_summary(summ, global_step=step)
+  ```
+
+  TF2:
+
+  ```python
+  writer = tf.summary.create_file_writer("/tmp/tf2_summary_example")
+  for step in range(100):
+    mean_moving_normal = np.random.normal(loc=step, scale=1, size=[100])
+    with writer.as_default(step=step):
+      tf.summary.histogram(name='distribution', data=mean_moving_normal)
+  ```
+
   @end_compatibility
   """
   # pylint: enable=line-too-long
@@ -519,7 +555,8 @@ def merge_all(key=_ops.GraphKeys.SUMMARIES, scope=None, name=None):
   Args:
     key: `GraphKey` used to collect the summaries.  Defaults to
       `GraphKeys.SUMMARIES`.
-    scope: Optional scope used to filter the summary ops, using `re.match`
+    scope: Optional scope used to filter the summary ops, using `re.match`.
+    name: A name for the operation (optional).
 
   Returns:
     If no summaries were collected, returns None.  Otherwise returns a scalar
@@ -529,9 +566,45 @@ def merge_all(key=_ops.GraphKeys.SUMMARIES, scope=None, name=None):
   Raises:
     RuntimeError: If called with eager execution enabled.
 
-  @compatibility(eager)
-  Not compatible with eager execution. To write TensorBoard
-  summaries under eager execution, use `tf.contrib.summary` instead.
+  @compatibility(TF2)
+  This API is not compatible with eager execution or `tf.function`. To migrate
+  to TF2, this API can be omitted entirely, because in TF2 individual summary
+  ops, like `tf.summary.scalar()`, write directly to the default summary writer
+  if one is active. Thus, it's not necessary to merge summaries or to manually
+  add the resulting merged summary output to the writer. See the usage example
+  shown below.
+
+  For a comprehensive `tf.summary` migration guide, please follow
+  [Migrating tf.summary usage to
+  TF 2.0](https://www.tensorflow.org/tensorboard/migrate#in_tf_1x).
+
+  #### TF1 & TF2 Usage Example
+
+  TF1:
+
+  ```python
+  dist = tf.compat.v1.placeholder(tf.float32, [100])
+  tf.compat.v1.summary.histogram(name="distribution", values=dist)
+  writer = tf.compat.v1.summary.FileWriter("/tmp/tf1_summary_example")
+  summaries = tf.compat.v1.summary.merge_all()
+
+  sess = tf.compat.v1.Session()
+  for step in range(100):
+    mean_moving_normal = np.random.normal(loc=step, scale=1, size=[100])
+    summ = sess.run(summaries, feed_dict={dist: mean_moving_normal})
+    writer.add_summary(summ, global_step=step)
+  ```
+
+  TF2:
+
+  ```python
+  writer = tf.summary.create_file_writer("/tmp/tf2_summary_example")
+  for step in range(100):
+    mean_moving_normal = np.random.normal(loc=step, scale=1, size=[100])
+    with writer.as_default(step=step):
+      tf.summary.histogram(name='distribution', data=mean_moving_normal)
+  ```
+
   @end_compatibility
   """
   if _context.executing_eagerly():
