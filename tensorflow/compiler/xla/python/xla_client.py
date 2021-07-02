2da17b4c1005bcb3bd10c4bc7a5ad4bf01da7d09
David Majnemer <majnemer@google.com>
2021-07-02 16:18:26 -0700

    Use XLA atan2 for complex atan
    PiperOrigin-RevId: 382831891
    Change-Id: Ic5298a190e72265b1c775178a9651013709764a0

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -50,7 +50,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 25
+_version = 26
 
 xla_platform_names = {
     'cpu': 'Host',
