d9d3653bb8b359989ff60caf893299bfda24382e
A. Unique TensorFlower <gardener@tensorflow.org>
2021-07-09 23:17:03 -0700

    Support eager mode in ExponentialMovingAverage.variables_to_restore
    Eager variables are unhashable, so we use Variable.ref() in the set.
    
    PiperOrigin-RevId: 383982206
    Change-Id: I32b2e680732a4c84efcc43fa23d74a46b4aaf045

--- tensorflow/python/training/moving_averages_test.py
+++ tensorflow/python/training/moving_averages_test.py
@@ -449,6 +449,24 @@ class ExponentialMovingAverageTest(test.TestCase):
     self.assertEqual(ema.average(v1).op.name, ema.average_name(v1))
     self.assertEqual(ema.average(tensor2).op.name, ema.average_name(tensor2))
 
+  def testSubsetAverageVariablesNamesEager(self):
+    v0 = variables.Variable(10.0, name="v0")
+    v1 = variables.Variable(30.0, name="v1")
+    # Add a non-trainable variable.
+    v2 = variables.Variable(20.0, name="v2", trainable=False)
+    ema = moving_averages.ExponentialMovingAverage(0.25, name="foo_avg")
+    self.assertEqual("v0/foo_avg", ema.average_name(v0))
+    self.assertEqual("v1/foo_avg", ema.average_name(v1))
+    vars_to_restore = ema.variables_to_restore([v0, v1, v2])
+    self.assertAllEqual(
+        sorted(vars_to_restore.keys()),
+        sorted([
+            ema.average_name(v0), ema.average_name(v1), ema.average_name(v2)
+        ]))
+    ema.apply([v0, v1])
+    self.assertEqual(ema.average(v0).name[:-len(":0")], ema.average_name(v0))
+    self.assertEqual(ema.average(v1).name[:-len(":0")], ema.average_name(v1))
+
   @test_util.deprecated_graph_mode_only
   def testAverageVariablesDeviceAssignment(self):
     with ops.device("/job:dev_v0"):
