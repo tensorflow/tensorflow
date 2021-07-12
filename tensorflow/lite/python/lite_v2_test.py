db978a52031dd8f2c1085da60a3163b824834725
Jaesung Chung <jaesung@google.com>
2021-07-12 14:50:56 -0700

    Add option to use converter backend as SavedModel MLIR converter in Concrete function Converter V2 when trackable obj is given.
    Users need to provide the trackable object when there are variables in the
    concrete function. Currently, from_concrete_functions converter API can not
    take trackable object yet so the followup change will address the API interface
    change.
    
    This change will enable new features only available in the V2 saved model
    converter including resource/variant support and signature feature in V2
    concrete function converter.
    
    PiperOrigin-RevId: 384322757
    Change-Id: Icc4a864d32fadc5cf982bb41724ae83ca77e3def

--- tensorflow/lite/python/lite_v2_test.py
+++ tensorflow/lite/python/lite_v2_test.py
@@ -351,6 +351,75 @@ class FromConcreteFunctionTest(lite_v2_test_util.ModelTest):
     # Ensure that the quantized weights tflite model is smaller.
     self.assertLess(len(quantized_tflite_model), len(float_tflite_model))
 
+  @test_util.run_v2_only
+  def testSignatureDefs(self):
+    """Test converting SignatureDef is correct and uses SignatureDef API."""
+    root = self._getMultiFunctionModel()
+    input_data = tf.constant(1., shape=[1])
+    add_func = root.add.get_concrete_function(input_data)
+
+    converter = lite.TFLiteConverterV2([add_func], trackable_obj=root)
+    converter.experimental_lower_to_saved_model = True
+    tflite_model = converter.convert()
+
+    # Check values from converted model.
+    expected_value = add_func(input_data)
+    interpreter = Interpreter(model_content=tflite_model)
+    signature_defs = interpreter.get_signature_list()
+    results = self._evaluateTFLiteModelUsingSignatureDef(
+        tflite_model, 'serving_default', {'x': input_data})
+    self.assertLen(list(results.keys()), 1)
+    self.assertStartsWith(list(results.keys())[0], 'output')
+    self.assertAllClose(
+        expected_value.numpy(),
+        results[signature_defs['serving_default']['outputs'][0]])
+
+    # Verify the SignatureDef structure returned is as expected.
+    self.assertEqual(len(signature_defs), 1)
+    self.assertEqual(list(signature_defs.keys()), ['serving_default'])
+    self.assertEqual(len(signature_defs.values()), 1)
+    self.assertEqual(
+        list(signature_defs['serving_default'].keys()), ['inputs', 'outputs'])
+    self.assertCountEqual(signature_defs['serving_default']['inputs'], ['x'])
+    self.assertLen(list(signature_defs['serving_default']['outputs']), 1)
+    self.assertStartsWith(
+        list(signature_defs['serving_default']['outputs'])[0], 'output')
+
+  @test_util.run_v2_only
+  def testNoSignatureDefsWhenTrackingObjIsNone(self):
+    """Test converting SignatureDef is correct and uses SignatureDef API."""
+    root = self._getSimpleVariableModel()
+    input_data = tf.constant(1., shape=[1])
+    concrete_func = root.f.get_concrete_function(input_data)
+
+    converter = lite.TFLiteConverterV2([concrete_func])
+    converter.experimental_lower_to_saved_model = True
+    tflite_model = converter.convert()
+
+    # Check values from converted model.
+    interpreter = Interpreter(model_content=tflite_model)
+    signature_defs = interpreter.get_signature_list()
+    # Verify that there is no SignatureDef structure found.
+    self.assertEqual(len(signature_defs), 0)
+
+  @test_util.run_v2_only
+  def testNoSignatureDefsWhenInvalidTrackingObjIsGiven(self):
+    """Test converting SignatureDef is correct and uses SignatureDef API."""
+    root = self._getSimpleVariableModel()
+    input_data = tf.constant(1., shape=[1])
+    concrete_func = root.f.get_concrete_function(input_data)
+
+    converter = lite.TFLiteConverterV2([concrete_func],
+                                       trackable_obj=tracking.AutoTrackable())
+    converter.experimental_lower_to_saved_model = True
+    tflite_model = converter.convert()
+
+    # Check values from converted model.
+    interpreter = Interpreter(model_content=tflite_model)
+    signature_defs = interpreter.get_signature_list()
+    # Verify that there is no SignatureDef structure found.
+    self.assertEqual(len(signature_defs), 0)
+
   def _getTrainingTimeQuantizedModel(self):
 
     class QLinear(tf.keras.layers.Layer):
