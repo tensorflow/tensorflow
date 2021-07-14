6d10ef2c2ecd30bc61e125da41b80c1c02ac4cbd
fsx950223 <fsx950223@gmail.com>
2021-07-14 01:24:17 +0000

    fix v1 test

--- tensorflow/python/ops/gradients_test.py
+++ tensorflow/python/ops/gradients_test.py
@@ -1028,7 +1028,10 @@ class GetDependentVariablesTest(test_util.TensorFlowTestCase):
     with context.graph_mode():
       init = constant_op.constant(100.0)
       var = variable_scope.variable(init, name='a/replica_1')
-      var._handle = array_ops.identity(var, name='a')
+      if isinstance(var, variables.RefVariable):
+        var._variable = array_ops.identity(var, name='a')
+      else:
+        var._handle = array_ops.identity(var, name='a')
       var2 = custom_gradient.get_variable_by_name('a')
       self.assertEqual(var2.name, var.name)
   
