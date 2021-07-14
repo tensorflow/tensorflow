ab9d17c5e4dc5882310a34d143de901b3dcbbb23
Xiao Yu <fishx@google.com>
2021-07-14 16:23:14 -0700

    Move outfeed template implementation to header file.
    PiperOrigin-RevId: 384809603
    Change-Id: I7d2ea6a15cf7d8a4ce815da91a7d89820bb3e994

--- tensorflow/core/tpu/kernels/outfeed_ops.cc
+++ tensorflow/core/tpu/kernels/outfeed_ops.cc
@@ -15,19 +15,6 @@ limitations under the License.
 
 #include "tensorflow/core/tpu/kernels/outfeed_ops.h"
 
-#include "tensorflow/compiler/jit/xla_device.h"
-#include "tensorflow/compiler/tf2xla/literal_util.h"
-#include "tensorflow/compiler/tf2xla/shape_util.h"
-#include "tensorflow/compiler/tf2xla/type_util.h"
-#include "tensorflow/core/framework/allocator.h"
-#include "tensorflow/core/framework/op.h"
-#include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/framework/shape_inference.h"
-#include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/tpu/kernels/transfer_ops.h"
-#include "tensorflow/core/tpu/tpu_defs.h"
-#include "tensorflow/stream_executor/multi_platform_manager.h"
-
 namespace tensorflow {
 namespace {
 template <class T>
@@ -59,80 +46,6 @@ class StreamExecutorOutfeedDequeueTupleOp : public TpuOutfeedDequeueTupleOp<T> {
 };
 
 }  // namespace
-
-template <class T>
-TpuOutfeedDequeueOp<T>::TpuOutfeedDequeueOp(
-    OpKernelConstruction* ctx,
-    std::unique_ptr<TpuTransferOpInterface> transfer_op)
-    : T(ctx, "outfeed_dequeue", 1, std::move(transfer_op)) {
-  OP_REQUIRES_OK(ctx, ctx->GetAttr("shape", &shape_));
-  OP_REQUIRES_OK(ctx, ctx->GetAttr("dtype", &dtype_));
-  OP_REQUIRES_OK(ctx, TensorShapeToXLAShape(dtype_, shape_, &xla_shape_));
-}
-
-template <class T>
-Status TpuOutfeedDequeueOp<T>::DoWork(OpKernelContext* ctx,
-                                      int device_ordinal) {
-  Tensor* output;
-  TF_RETURN_IF_ERROR(ctx->allocate_output(0, shape_, &output));
-
-  // Transfer from the outfeed interface of the device.
-  xla::MutableBorrowingLiteral literal;
-  TF_RETURN_IF_ERROR(
-      HostTensorToMutableBorrowingLiteral(xla_shape_, output, &literal));
-
-  VLOG(1) << "TransferLiteralFromOutfeed "
-          << xla::ShapeUtil::HumanStringWithLayout(xla_shape_);
-
-  TF_RETURN_IF_ERROR(
-      T::transfer_op_->TransferLiteralFromOutfeed(device_ordinal, literal));
-
-  VLOG(1) << "TransferLiteralFromOutfeed complete.";
-
-  return Status::OK();
-}
-
-// The OutfeedDequeueTuple op is used to retrieve multiple tensors from the
-// device outfeed queue.
-template <class T>
-TpuOutfeedDequeueTupleOp<T>::TpuOutfeedDequeueTupleOp(
-    OpKernelConstruction* ctx,
-    std::unique_ptr<TpuTransferOpInterface> transfer_op)
-    : T(ctx, "outfeed_dequeue", 1, std::move(transfer_op)) {
-  OP_REQUIRES_OK(ctx, ctx->GetAttr("shapes", &shapes_));
-  OP_REQUIRES_OK(ctx, ctx->GetAttr("dtypes", &dtypes_));
-  OP_REQUIRES(
-      ctx, shapes_.size() == dtypes_.size(),
-      errors::InvalidArgument("shapes and dtypes must be the same length."));
-  // The `dtypes` list is inferred from the supplied inputs, so it
-  // is always the correct length.
-  for (int i = 0; i < shapes_.size(); i++) {
-    xla::Shape xla_shape;
-    OP_REQUIRES_OK(ctx,
-                   TensorShapeToXLAShape(dtypes_[i], shapes_[i], &xla_shape));
-    xla_shapes_.push_back(xla_shape);
-  }
-  tuple_shape_ = xla::ShapeUtil::MakeTupleShape(xla_shapes_);
-}
-
-template <class T>
-Status TpuOutfeedDequeueTupleOp<T>::DoWork(OpKernelContext* ctx,
-                                           int device_ordinal) {
-  VLOG(1) << "TransferLiteralFromOutfeed "
-          << xla::ShapeUtil::HumanStringWithLayout(tuple_shape_);
-
-  for (int i = 0; i < shapes_.size(); ++i) {
-    Tensor* output;
-    TF_RETURN_IF_ERROR(ctx->allocate_output(i, shapes_[i], &output));
-
-    xla::MutableBorrowingLiteral literal;
-    TF_RETURN_IF_ERROR(
-        HostTensorToMutableBorrowingLiteral(xla_shapes_[i], output, &literal));
-    TF_RETURN_IF_ERROR(
-        T::transfer_op_->TransferLiteralFromOutfeed(device_ordinal, literal));
-  }
-  return Status::OK();
-}
 
 // These ops execute on either the TPU device or the CPU device. When
 // running on CPU they must specify a non-negative value for
