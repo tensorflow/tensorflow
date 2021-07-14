ed80649a9f8745bc0ba667c8045a1d70114a4741
Fergus Henderson <fergus@google.com>
2021-07-14 04:02:55 -0700

    Promote the TF Lite Java API "Delegate" class from experimental to stable.
    This is for consistency with the C API, where the TfLiteDelegate type and the TfLiteInterpreterOptionsAddDelegate function are part of the stable API.
    The Java API "Delegate" class is a thin wrapper around the C API's TfLiteOpaqueDelegate, which is an opaque alias for TfLiteDelegate.
    
    PiperOrigin-RevId: 384665700
    Change-Id: Ib92d02f1f9b4971f5080293baa4bde49461612d4

--- tensorflow/lite/java/src/main/java/org/tensorflow/lite/Delegate.java
+++ tensorflow/lite/java/src/main/java/org/tensorflow/lite/Delegate.java
@@ -18,8 +18,6 @@ package org.tensorflow.lite;
 /**
  * Wrapper for a native TensorFlow Lite Delegate.
  *
- * <p>WARNING: This is an experimental interface that is subject to change.
- *
  * <p>If a delegate implementation holds additional resources or memory that should be explicitly
  * freed, then best practice is to add a {@code close()} method to the implementation and have the
  * client call that explicitly when the delegate instance is no longer in use. While this approach
