5603ea14bbe50cb0adf317defb61a3cb59704978
Reed Wanderman-Milne <reedwm@google.com>
2021-07-14 20:49:08 -0700

    Fix crash in softmax-xent when some input dimensions are 1.
    Before, tf.nn.softmax_cross_entropy_with_logits would fail a CHECK if one input tensor had shape (1, 1) and the other did not.
    
    In particular, the call to ToIndexArray<2> here https://github.com/tensorflow/tensorflow/blob/1f3da84a89702d3b4f234ee83762d738caffe098/tensorflow/core/kernels/xent_op.cc#L99 would fail, since the call assumed the array had two dimensions. If both dimensions were 1, BCast would merge the two dimensions into a single dimension. Passing fewer_dims_optimization=false stops this optimization
    
    PiperOrigin-RevId: 384844496
    Change-Id: Ifb02dc74964132c3ed3f3bc98b0858dbe4e258b7

--- tensorflow/python/kernel_tests/xent_op_test.py
+++ tensorflow/python/kernel_tests/xent_op_test.py
@@ -195,6 +195,13 @@ class XentTest(test.TestCase):
         gen_nn_ops.softmax_cross_entropy_with_logits(
             [[0., 1.], [2., 3.]], [[0., 1., 0.], [1., 0., 0.]])
 
+    tf_f = constant_op.constant(np.array([[1.]]).astype(np.float32))
+    tf_l = constant_op.constant(np.array([[1.], [1.]]).astype(np.float32))
+    tf_loss, tf_gradient = gen_nn_ops.softmax_cross_entropy_with_logits(
+        tf_f, tf_l)
+    self.assertAllClose([0, 0], tf_loss)
+    self.assertAllCloseAccordingToType([[0], [0]], tf_gradient)
+
   @test_util.run_deprecated_v1
   def testNotMatrix(self):
     with self.cached_session():
