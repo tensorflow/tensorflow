3937bcbd091241618207f5b00c0b2c4630b60578
Robert Neale <rneale@google.com>
2021-07-15 08:35:36 -0700

    Adds the TF Kernel shim headers to the includes directory of the pip package.
    PiperOrigin-RevId: 384932716
    Change-Id: Idfd4ea13c3fd0432ca9b78a6a18e6d63fbbff34b

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -292,6 +292,7 @@ headers = (
     list(find_files('*.h', 'tensorflow/compiler')) +
     list(find_files('*.h.inc', 'tensorflow/compiler')) +
     list(find_files('*.h', 'tensorflow/core')) +
+    list(find_files('*.h', 'tensorflow/lite/kernels/shim')) +
     list(find_files('*.h', 'tensorflow/python')) +
     list(find_files('*.h', 'tensorflow/python/client')) +
     list(find_files('*.h', 'tensorflow/python/framework')) +
