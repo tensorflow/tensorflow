9d74e07817109faf234f8867edd0cc9ab409dbc0
Rahul Joshi <jurahul@google.com>
2021-07-19 14:16:22 -0700

    [XLA] Factor out CreateStartIndicesForCollectiveDecomposition into a separate lib. - Factor out CreateStartIndicesForCollectiveDecomposition into a separate lib and   use it in all gather decomposer. - Fix incorrect handling of CrossReplicaAndPartition mode in   CreateStartIndicesForCollectiveDecomposition. We need to map the replica_id to   replica_index and then flatten the (replica_index, partition_id) pair. - Also added a optimization when the replica_groups is a single singleton group. That   helps all_gather_decomposer to continue to generate the same code as it does today.
    PiperOrigin-RevId: 385638901
    Change-Id: I7cc1f488f3bfff9d8298b00f09a8f383a6626e24

--- tensorflow/compiler/xla/service/reduce_scatter_decomposer.cc
+++ tensorflow/compiler/xla/service/reduce_scatter_decomposer.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <limits>
 
 #include "tensorflow/compiler/xla/literal_util.h"
+#include "tensorflow/compiler/xla/service/collective_decomposer_utils.h"
 #include "tensorflow/compiler/xla/service/collective_ops_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_casting_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_computation.h"
@@ -32,102 +33,6 @@ limitations under the License.
 #include "tensorflow/compiler/xla/shape_util.h"
 
 namespace xla {
-
-namespace {
-
-// Create the start indices for decompositing the given collective.
-StatusOr<std::vector<HloInstruction *>>
-CreateStartIndicesForCollectiveDecomposition(
-    CollectiveOpGroupMode group_mode,
-    absl::Span<const ReplicaGroup> replica_groups, const Shape &shard_shape,
-    int64_t shard_dimension, HloComputation *computation) {
-  HloInstruction *zero = computation->AddInstruction(
-      HloInstruction::CreateConstant(LiteralUtil::Zero(U32)));
-  std::vector<HloInstruction *> start_indices(shard_shape.rank(), zero);
-  const Shape &scalar_shape = zero->shape();
-
-  HloInstruction *participant_id;
-  switch (group_mode) {
-    case CollectiveOpGroupMode::kCrossReplica:
-    case CollectiveOpGroupMode::kCrossReplicaAndPartition:
-      participant_id =
-          computation->AddInstruction(HloInstruction::CreateReplicaId());
-      break;
-    case CollectiveOpGroupMode::kCrossPartition:
-      participant_id =
-          computation->AddInstruction(HloInstruction::CreatePartitionId());
-      break;
-    case CollectiveOpGroupMode::kFlattenedID: {
-      const HloModuleConfig &config = computation->parent()->config();
-      HloInstruction *partition_count =
-          computation->AddInstruction(HloInstruction::CreateConstant(
-              LiteralUtil::CreateR0<uint32_t>(config.num_partitions())));
-      HloInstruction *mul =
-          computation->AddInstruction(HloInstruction::CreateBinary(
-              scalar_shape, HloOpcode::kMultiply,
-              computation->AddInstruction(HloInstruction::CreateReplicaId()),
-              partition_count));
-      participant_id = computation->AddInstruction(HloInstruction::CreateBinary(
-          scalar_shape, HloOpcode::kAdd, mul,
-          computation->AddInstruction(HloInstruction::CreatePartitionId())));
-      break;
-    }
-  }
-
-  auto is_trivial_group = [](absl::Span<const ReplicaGroup> replica_groups) {
-    if (replica_groups.empty()) {
-      return true;
-    }
-    if (replica_groups.size() == 1) {
-      for (int64_t index = 0; index < replica_groups[0].replica_ids_size();
-           ++index) {
-        if (index != replica_groups[0].replica_ids(index)) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  };
-
-  HloInstruction *index;
-  if (is_trivial_group(replica_groups)) {
-    index = participant_id;
-  } else {
-    size_t num_participants =
-        replica_groups.size() * replica_groups.front().replica_ids_size();
-    std::vector<uint32_t> index_values(num_participants,
-                                       std::numeric_limits<uint32_t>::max());
-    for (const ReplicaGroup &rg : replica_groups) {
-      for (uint64_t idx = 0; idx < rg.replica_ids_size(); ++idx) {
-        int64_t id = rg.replica_ids(idx);
-        TF_RET_CHECK(index_values[id] == std::numeric_limits<uint32_t>::max());
-        index_values[id] = idx;
-      }
-    }
-
-    // create a u32 constant table of index values and use dynamic-slice to
-    // index into it.
-    HloInstruction *table =
-        computation->AddInstruction(HloInstruction::CreateConstant(
-            LiteralUtil::CreateR1<uint32_t>(index_values)));
-    HloInstruction *ds =
-        computation->AddInstruction(HloInstruction::CreateDynamicSlice(
-            ShapeUtil::MakeShape(U32, {1}), table, {participant_id}, {1}));
-    index = computation->AddInstruction(
-        HloInstruction::CreateReshape(scalar_shape, ds));
-  }
-
-  // scale index by the shard size, which is the size of the shard_dimension.
-  HloInstruction *scale = computation->AddInstruction(
-      HloInstruction::CreateConstant(LiteralUtil::CreateR0<uint32_t>(
-          shard_shape.dimensions(shard_dimension))));
-  index = computation->AddInstruction(HloInstruction::CreateBinary(
-      scalar_shape, HloOpcode::kMultiply, index, scale));
-  start_indices[shard_dimension] = index;
-  return start_indices;
-}
-}  // namespace
 
 StatusOr<bool> ReduceScatterDecomposer::Run(HloModule *module) {
   bool changed = false;
--- tensorflow/compiler/xla/service/reduce_scatter_decomposer_test.cc
+++ tensorflow/compiler/xla/service/reduce_scatter_decomposer_test.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <utility>
 
 #include "tensorflow/compiler/xla/literal_util.h"
+#include "tensorflow/compiler/xla/service/collective_ops_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_matchers.h"
 #include "tensorflow/compiler/xla/service/hlo_module.h"
 #include "tensorflow/compiler/xla/service/hlo_opcode.h"
@@ -36,15 +37,11 @@ class ReduceScatterDecomposerTest : public HloTestBase {
     kTrivialGroups,
     kTableLookup,
   };
-  enum class ParticipantID {
-    kReplicaId,
-    kPartitionId,
-    kFlattenedId,
-  };
-  void RunPass(absl::string_view hlo_module, PassAction action,
-               ParticipantID id = ParticipantID::kReplicaId,
-               int64_t shard_size = 0, int64_t shard_dimension = 0) {
-    const int64_t replica_count = 2;
+  void RunPass(
+      absl::string_view hlo_module, PassAction action,
+      CollectiveOpGroupMode mode = CollectiveOpGroupMode::kCrossReplica,
+      int64_t shard_size = 0, int64_t shard_dimension = 0,
+      int64_t replica_count = 2) {
     const int64_t partition_count = 2;
     TF_ASSERT_OK_AND_ASSIGN(
         auto module, ParseAndReturnVerifiedModule(hlo_module, replica_count,
@@ -59,12 +56,14 @@ class ReduceScatterDecomposerTest : public HloTestBase {
 
     Literal multiplier = LiteralUtil::CreateR0<uint32_t>(shard_size);
     ::testing::Matcher<const ::xla::HloInstruction *> id_matcher = [&]() {
-      switch (id) {
-        case ParticipantID::kPartitionId:
+      switch (mode) {
+        case CollectiveOpGroupMode::kCrossPartition:
           return op::PartitionId();
-        case ParticipantID::kReplicaId:
+        case CollectiveOpGroupMode::kCrossReplica:
           return op::ReplicaId();
-        case ParticipantID::kFlattenedId: {
+        case CollectiveOpGroupMode::kCrossReplicaAndPartition:
+          return op::ReplicaId();
+        case CollectiveOpGroupMode::kFlattenedID: {
           return op::Add(
               op::Multiply(op::ReplicaId(),
                            op::Constant(LiteralUtil::CreateR0<uint32_t>(
@@ -80,7 +79,13 @@ class ReduceScatterDecomposerTest : public HloTestBase {
     if (action == PassAction::kTableLookup) {
       slice_index = op::Reshape(op::DynamicSlice(op::Constant(), id_matcher));
     }
-
+    if (mode == CollectiveOpGroupMode::kCrossReplicaAndPartition) {
+      slice_index = op::Add(
+          op::Multiply(
+              slice_index,
+              op::Constant(LiteralUtil::CreateR0<uint32_t>(partition_count))),
+          op::PartitionId());
+    }
     auto zero_matcher = op::Constant(LiteralUtil::Zero(U32));
 
     std::vector<::testing::Matcher<const ::xla::HloInstruction *>> ds_operands(
@@ -107,7 +112,8 @@ ENTRY main {
   ROOT rs = f32[4] reduce-scatter(p0), replica_groups={{0,1}}, dimensions={0}, to_apply=sum
 }
 )";
-  RunPass(hlo_string, PassAction::kTrivialGroups, ParticipantID::kReplicaId,
+  RunPass(hlo_string, PassAction::kTrivialGroups,
+          CollectiveOpGroupMode::kCrossReplica,
           /*shard_size=*/4);
 }
 
@@ -126,7 +132,8 @@ ENTRY main {
   ROOT rs = f32[4] reduce-scatter(p0), replica_groups={{1, 0}}, dimensions={0}, to_apply=sum
 }
 )";
-  RunPass(hlo_string, PassAction::kTableLookup, ParticipantID::kReplicaId,
+  RunPass(hlo_string, PassAction::kTableLookup,
+          CollectiveOpGroupMode::kCrossReplica,
           /*shard_size=*/4);
 }
 
@@ -147,8 +154,34 @@ ENTRY main {
   ROOT rs = f32[4, 2] reduce-scatter(p0), replica_groups={{0, 1}}, channel_id=1, dimensions={1}, to_apply=sum
 }
 )";
-  RunPass(hlo_string, PassAction::kTrivialGroups, ParticipantID::kReplicaId,
+  RunPass(hlo_string, PassAction::kTrivialGroups,
+          CollectiveOpGroupMode::kCrossReplicaAndPartition,
           /*shard_size=*/2, /*shard_dimension=*/1);
+}
+
+TEST_F(ReduceScatterDecomposerTest,
+       TrivialCrossReplicaAndPartition_SingleReplica) {
+  absl::string_view hlo_string = R"(
+HloModule m
+
+sum {
+  a = f32[] parameter(0)
+  b = f32[] parameter(1)
+  ROOT add.2 = f32[] add(a, b)
+}
+
+ENTRY main {
+  p0 = f32[4, 8] parameter(0)
+  // Tn this mode, the participants are the given replicas across all partitions.
+  // Here, we have 1 replicas and 2 partitions, so 2 total shards.
+  ROOT rs = f32[4, 4] reduce-scatter(p0), replica_groups={{0}}, channel_id=1, dimensions={1}, to_apply=sum
+}
+)";
+  // kCrossPartition here indicates that replica_index * num_partitions +
+  // partition_id will be simplified by the pass to just partition_id
+  RunPass(hlo_string, PassAction::kTrivialGroups,
+          CollectiveOpGroupMode::kCrossPartition,
+          /*shard_size=*/4, /*shard_dimension=*/1, /*replica_count=*/1);
 }
 
 TEST_F(ReduceScatterDecomposerTest, TableLookupFlattenedId) {
@@ -166,7 +199,8 @@ ENTRY main {
   ROOT rs = f32[4, 2] reduce-scatter(p0), replica_groups={{1,0, 3, 2}}, channel_id=1, dimensions={1}, to_apply=sum, use_global_device_ids=true
 }
 )";
-  RunPass(hlo_string, PassAction::kTableLookup, ParticipantID::kFlattenedId,
+  RunPass(hlo_string, PassAction::kTableLookup,
+          CollectiveOpGroupMode::kFlattenedID,
           /*shard_size=*/2, /*shard_dimension=*/1);
 }
 
