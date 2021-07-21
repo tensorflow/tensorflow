9beac1cf6425732b8e71ff5a642c664f613a041f
stevenireeves <reevessteven15@gmail.com>
2021-07-20 22:00:29 -0700

    Matrix Solve Op enabled for ROCm

--- tensorflow/core/kernels/linalg/matrix_solve_op.cc
+++ tensorflow/core/kernels/linalg/matrix_solve_op.cc
@@ -14,7 +14,7 @@ limitations under the License.
 ==============================================================================*/
 
 // See docs in ../ops/linalg_ops.cc.
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 #define EIGEN_USE_GPU
 #endif
 
@@ -31,10 +31,13 @@ limitations under the License.
 #include "tensorflow/core/platform/macros.h"
 #include "tensorflow/core/platform/types.h"
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/kernels/transpose_functor.h"
+#if GOOGLE_CUDA
 #include "tensorflow/core/util/cuda_solvers.h"
+#elif TENSORFLOW_USE_ROCM
+#include "tensorflow/core/util/rocm_solvers.h"
 #endif
 
 namespace tensorflow {
@@ -114,9 +117,10 @@ class MatrixSolveOp : public LinearAlgebraOp<Scalar> {
   TF_DISALLOW_COPY_AND_ASSIGN(MatrixSolveOp);
 };
 
-#if GOOGLE_CUDA
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 typedef Eigen::GpuDevice GPUDevice;
 
+#if GOOGLE_CUDA
 template <class Scalar>
 class MatrixSolveOpGpu : public AsyncOpKernel {
  public:
@@ -364,13 +368,263 @@ class MatrixSolveOpGpu : public AsyncOpKernel {
   bool adjoint_;
 };
 
+#elif TENSORFLOW_USE_ROCM
+template <class Scalar>
+class MatrixSolveOpGpu : public AsyncOpKernel {
+ public:
+  explicit MatrixSolveOpGpu(OpKernelConstruction* context)
+      : AsyncOpKernel(context) {
+    OP_REQUIRES_OK(context, context->GetAttr("adjoint", &adjoint_));
+  }
+
+  void ComputeAsync(OpKernelContext* context, DoneCallback done) final {
+    const Tensor& input = context->input(0);
+    const Tensor& rhs = context->input(1);
+    const int ndims = input.dims();
+    const int64 n = input.dim_size(ndims - 1);
+    const int64 nrhs = rhs.dim_size(ndims - 1);
+    // Validate inputs.
+    OP_REQUIRES_ASYNC(
+        context, ndims >= 2,
+        errors::InvalidArgument("Input must have rank >= 2, got ", ndims),
+        done);
+    OP_REQUIRES_ASYNC(context, rhs.dims() == ndims,
+                      errors::InvalidArgument(
+                          "Input and right-hand side must have same rank, got ",
+                          ndims, " != ", rhs.dims()),
+                      done);
+    OP_REQUIRES_ASYNC(
+        context, input.dim_size(ndims - 2) == n,
+        errors::InvalidArgument("Input matrices must be squares, got",
+                                input.dim_size(ndims - 2), " != ", n),
+        done);
+    OP_REQUIRES_ASYNC(context, rhs.dim_size(ndims - 2) == n,
+                      errors::InvalidArgument(
+                          "Input matrix and right-hand side must have the "
+                          "same number of rows, got",
+                          n, " != ", rhs.dim_size(ndims - 2)),
+                      done);
+    for (int dim = 0; dim < ndims - 2; dim++) {
+      OP_REQUIRES_ASYNC(
+          context, input.dim_size(dim) == rhs.dim_size(dim),
+          errors::InvalidArgument(
+              "All input tensors must have the same outer dimensions."),
+          done);
+    }
+
+    // Allocate output.
+    Tensor* output;
+    OP_REQUIRES_OK_ASYNC(
+        context,
+        context->forward_input_or_allocate_output({1}, 0, rhs.shape(), &output),
+        done);
+
+    // To be consistent with the MatrixInverse op, we define the solution for
+    // an empty set of equations as the empty matrix.
+    if (input.NumElements() == 0 || rhs.NumElements() == 0) {
+      done();
+      return;
+    }
+
+    std::unique_ptr<ROCmSolver> solver(new ROCmSolver(context));
+
+    // Make a copy of the input for the factorization step, or, if adjoint_ is
+    // false, try to reuse the input buffer if this op owns it exclusively.
+    Tensor input_copy;
+    const GPUDevice& device = context->eigen_device<GPUDevice>();
+    if (adjoint_) {
+      // For the adjoint case, it is simpler to always make a transposed copy up
+      // front.
+      OP_REQUIRES_OK_ASYNC(
+          context,
+          solver->allocate_scoped_tensor(DataTypeToEnum<Scalar>::value,
+                                         input.shape(), &input_copy),
+          done);
+      OP_REQUIRES_OK_ASYNC(context,
+                           DoMatrixTranspose(device, input, &input_copy), done);
+    } else {
+      OP_REQUIRES_OK_ASYNC(
+          context,
+          solver->forward_input_or_allocate_scoped_tensor(
+              {0}, DataTypeToEnum<Scalar>::value, input.shape(), &input_copy),
+          done);
+      if (!input.SharesBufferWith(input_copy)) {
+        device.memcpy(input_copy.flat<Scalar>().data(),
+                      input.flat<Scalar>().data(),
+                      input.NumElements() * sizeof(Scalar));
+      }
+    }
+    auto input_copy_reshaped = input_copy.template flat_inner_dims<Scalar, 3>();
+    const int64 batch_size = input_copy_reshaped.dimension(0);
+
+    // Allocate pivots on the device.
+    Tensor pivots;
+    OP_REQUIRES_OK_ASYNC(
+        context,
+        solver->allocate_scoped_tensor(DataTypeToEnum<int>::value,
+                                       TensorShape{batch_size, n}, &pivots),
+        done);
+    auto pivots_mat = pivots.template matrix<int>();
+    // 1. Compute the partially pivoted LU factorization(s) of the
+    // matrix/matrices.
+    std::vector<DeviceLapackInfo> dev_info;
+    auto input_copy_ptrs = solver->GetScratchSpace<uint8>(
+        sizeof(Scalar*) * batch_size, "input_copt_ptrs",
+        /* on_host */ true);
+    const int kMaxMatrixSizeToBatchSizeRatio = 128;
+    int *info = new int[batch_size]; 
+    const bool use_batched_solver =
+        n <= kMaxMatrixSizeToBatchSizeRatio * batch_size;
+    if (use_batched_solver) {
+      // For small matrices or large batch sizes, we use the batched interface
+      // from hipBLAS.
+      Scalar** input_copy_ptrs_base =
+          reinterpret_cast<Scalar**>(input_copy_ptrs.mutable_data());
+      for (int batch = 0; batch < batch_size; ++batch) {
+        input_copy_ptrs_base[batch] = &input_copy_reshaped(batch, 0, 0);
+      }
+      dev_info.push_back(
+          solver->GetDeviceLapackInfo(batch_size, "getrfBatched"));
+      OP_REQUIRES_OK_ASYNC(
+          context,
+          solver->getrf_batched<Scalar>(n, n, input_copy_ptrs_base, n, pivots_mat.data(),
+                                        n, &dev_info.back(), batch_size),
+          done);
+    } else {
+      // For small batch sizes or large matrices, we use the non-batched
+      // interface from ROCmSolver, which is much faster for large matrices.
+      dev_info.push_back(solver->GetDeviceLapackInfo(batch_size, "getrf"));
+      for (int batch = 0; batch < batch_size; ++batch) {
+        OP_REQUIRES_OK_ASYNC(
+            context,
+            solver->getrf(n, n, &input_copy_reshaped(batch, 0, 0), n,
+                          &pivots_mat(batch, 0), &dev_info.back()(batch)),
+            done);
+      }
+    }
+
+    // 2. Make a transposed copy of the right-hand sides. This is necessary
+    // because rocBLAS assumes column-major storage while TensorFlow TF uses
+    // row-major.
+    TensorShape transposed_rhs_shape(rhs.shape());
+    transposed_rhs_shape.RemoveLastDims(2);
+    transposed_rhs_shape.AddDim(nrhs);
+    transposed_rhs_shape.AddDim(n);
+    Tensor transposed_rhs;
+    OP_REQUIRES_OK_ASYNC(
+        context,
+        solver->allocate_scoped_tensor(DataTypeToEnum<Scalar>::value,
+                                       transposed_rhs_shape, &transposed_rhs),
+        done);
+    if (nrhs > 1) {
+      OP_REQUIRES_OK_ASYNC(
+          context, DoMatrixTranspose(device, rhs, &transposed_rhs), done);
+    } else {
+      device.memcpy(transposed_rhs.flat<Scalar>().data(),
+                    rhs.flat<Scalar>().data(),
+                    rhs.NumElements() * sizeof(Scalar));
+    }
+
+    // 3. Solve op(A) X = B (in column major form).
+    // We use a trick here: If adjoint_ is true, we converted A to column major
+    // form above. If adjoint is false then I leave A in row-major form and use
+    // trans_a = rocblas_operation_transpose to effectively transform it to column-major on the
+    // fly. (This means that we actually use the LU-factorization of A^T in that
+    // case, but that is equally good for solving AX=B). This way we save an
+    // explicit transpose in the more common case of adjoint_ == false.
+    auto input_copy_ptr_array = solver->GetScratchSpace<uint8>(
+        sizeof(Scalar*) * batch_size, "input_copy_ptr_array",
+        /* on_host */ true);
+    auto transposed_rhs_ptr_array = solver->GetScratchSpace<uint8>(
+        sizeof(Scalar*) * batch_size, "transposed_rhs_ptr_array",
+        /* on_host */ true);
+    auto transposed_rhs_reshaped =
+        transposed_rhs.template flat_inner_dims<Scalar, 3>();
+    if (use_batched_solver) {
+      Scalar** input_copy_ptrs_base =
+          reinterpret_cast<Scalar**>(input_copy_ptr_array.mutable_data());
+      Scalar** transposed_rhs_ptrs_base =
+          reinterpret_cast<Scalar**>(
+              transposed_rhs_ptr_array.mutable_data());
+      for (int batch = 0; batch < batch_size; ++batch) {
+        input_copy_ptrs_base[batch] = &input_copy_reshaped(batch, 0, 0);
+        transposed_rhs_ptrs_base[batch] = &transposed_rhs_reshaped(batch, 0, 0);
+      } 
+      int host_info = 0;
+      OP_REQUIRES_OK_ASYNC(
+          context,
+          solver->getrs_batched<Scalar>(adjoint_ ? rocblas_operation_conjugate_transpose
+                                         : rocblas_operation_transpose,
+                                        n, nrhs,
+                                        input_copy_ptrs_base, n, pivots_mat.data(),
+                                        n, transposed_rhs_ptrs_base, n, batch_size),
+          done);
+      OP_REQUIRES_ASYNC(
+          context, host_info == 0,
+          errors::InvalidArgument("The ", -host_info,
+                                  "'th argument to rocblas*getrs_batched had "
+                                  "an illegal value."),
+          done);
+    } else {
+      dev_info.push_back(solver->GetDeviceLapackInfo(batch_size, "getrs"));
+      for (int batch = 0; batch < batch_size; ++batch) {
+        OP_REQUIRES_OK_ASYNC(
+            context,
+            solver->getrs(adjoint_ ? rocblas_operation_conjugate_transpose
+                                         : rocblas_operation_transpose,
+                          n, nrhs,
+                          &input_copy_reshaped(batch, 0, 0), n,
+                          &pivots_mat(batch, 0),
+                          &transposed_rhs_reshaped(batch, 0, 0), n),
+            done);
+      }
+    }
+
+    // 4. Transpose X to get the final result in row-major form.
+    if (nrhs > 1) {
+      OP_REQUIRES_OK_ASYNC(
+          context, DoMatrixTranspose(device, transposed_rhs, output), done);
+    } else {
+      device.memcpy(output->flat<Scalar>().data(),
+                    transposed_rhs.flat<Scalar>().data(),
+                    transposed_rhs.NumElements() * sizeof(Scalar));
+    }
+    // Callback for checking info after kernels finish. Also capture the
+    // temporary Tensors/ScratchSpace so they don't get deallocated before the
+    // kernels run. TODO(rmlarsen): Use move capture once C++14 becomes
+    // available.
+    auto info_checker = [context, done, dev_info](
+                            const Status& status,
+                            const std::vector<HostLapackInfo>& host_infos) {
+      if (!status.ok() && errors::IsInvalidArgument(status) &&
+          !host_infos.empty()) {
+        for (int i = 0; i < host_infos[0].size(); ++i) {
+          // Match the CPU error message for singular matrices. Otherwise
+          // just print the original error message from the status below.
+          OP_REQUIRES_ASYNC(context, host_infos[0].data()[i] <= 0,
+                            errors::InvalidArgument(kErrMsg), done);
+        }
+      }
+      OP_REQUIRES_OK_ASYNC(context, status, done);
+      done();
+    };
+    ROCmSolver::CheckLapackInfoAndDeleteSolverAsync(std::move(solver), dev_info,
+                                                    std::move(info_checker));
+  }
+
+ private:
+  bool adjoint_;
+};
+#endif
+
+
 REGISTER_LINALG_OP_GPU("MatrixSolve", (MatrixSolveOpGpu<float>), float);
 REGISTER_LINALG_OP_GPU("MatrixSolve", (MatrixSolveOpGpu<double>), double);
 REGISTER_LINALG_OP_GPU("MatrixSolve", (MatrixSolveOpGpu<complex64>), complex64);
 REGISTER_LINALG_OP_GPU("MatrixSolve", (MatrixSolveOpGpu<complex128>),
                        complex128);
 
-#endif  // GOOGLE_CUDA
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
 REGISTER_LINALG_OP("MatrixSolve", (MatrixSolveOp<float>), float);
 REGISTER_LINALG_OP("MatrixSolve", (MatrixSolveOp<double>), double);
--- tensorflow/core/util/rocm_solvers.h
+++ tensorflow/core/util/rocm_solvers.h
@@ -18,7 +18,7 @@ limitations under the License.
 #define TENSORFLOW_CORE_KERNELS_LINALG_ROCM_SOLVERS_H_
 
 // This header declares the class ROCmSolver, which contains wrappers of linear
-// algebra solvers in the cuBlas and cuSolverDN libraries for use in TensorFlow
+// algebra solvers in the rocBlas and rocSolverDN libraries for use in TensorFlow
 // kernels.
 
 #if TENSORFLOW_USE_ROCM
@@ -34,7 +34,7 @@ limitations under the License.
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/stream_executor.h"
 #include "tensorflow/stream_executor/blas.h"
-
+#include "tensorflow/stream_executor/rocm/rocsolver_wrapper.h"
 namespace tensorflow {
 
 // Type traits to get ROCm complex types from std::complex<T>.
@@ -44,14 +44,14 @@ struct ROCmComplexT {
 };
 template <>
 struct ROCmComplexT<std::complex<float>> {
-  typedef hipComplex type;
+  typedef rocblas_float_complex type;
 };
 template <>
 struct ROCmComplexT<std::complex<double>> {
-  typedef hipDoubleComplex type;
+  typedef rocblas_double_complex type;
 };
 // Converts pointers of std::complex<> to pointers of
-// cuComplex/cuDoubleComplex. No type conversion for non-complex types.
+// ROCmComplex/ROCmDoubleComplex. No type conversion for non-complex types.
 template <typename T>
 inline const typename ROCmComplexT<T>::type* ROCmComplex(const T* p) {
   return reinterpret_cast<const typename ROCmComplexT<T>::type*>(p);
@@ -60,6 +60,16 @@ template <typename T>
 inline typename ROCmComplexT<T>::type* ROCmComplex(T* p) {
   return reinterpret_cast<typename ROCmComplexT<T>::type*>(p);
 }
+
+// Container of LAPACK info data (an array of int) generated on-device by
+// a rocSolver call. One or more such objects can be passed to
+// rocSolver::CopyLapackInfoToHostAsync() along with a callback to
+// check the LAPACK info data after the corresponding kernels
+// finish and LAPACK info has been copied from the device to the host.
+class DeviceLapackInfo;
+
+// Host-side copy of LAPACK info.
+class HostLapackInfo;
 
 template <typename Scalar>
 class ScratchSpace;
@@ -78,7 +88,61 @@ class ROCmSolver {
       gtl::ArraySlice<int> candidate_input_indices, DataType type,
       const TensorShape& shape, Tensor* input_alias_or_new_scoped_tensor);
 
+  static void CheckLapackInfoAndDeleteSolverAsync(
+    std::unique_ptr<ROCmSolver> solver, 
+    const std::vector<DeviceLapackInfo>& dev_lapack_info, 
+    std::function<void(const Status&, const std::vector<HostLapackInfo>&)>
+    info_checker_callback);  
+
+  static void CheckLapackInfoAndDeleteSolverAsync(
+    std::unique_ptr<ROCmSolver> solver,
+    const std::vector<DeviceLapackInfo>& dev_lapack_info,
+    AsyncOpKernel::DoneCallback done); 
+
   OpKernelContext* context() { return context_; }
+
+  template <typename Scalar>
+  ScratchSpace<Scalar> GetScratchSpace(const TensorShape& shape,
+                                       const std::string& debug_info,
+                                       bool on_host);
+  template <typename Scalar>
+  ScratchSpace<Scalar> GetScratchSpace(int64 size,
+                                       const std::string& debug_info,
+                                       bool on_host);
+  // Returns a DeviceLapackInfo that will live for the duration of the
+  // ROCmSolver object.
+  inline DeviceLapackInfo GetDeviceLapackInfo(int64 size,
+                                              const std::string& debug_info);
+
+
+  // ====================================================================
+  // Wrappers for ROCSolver start here
+  //
+  // The method names below
+  // map to those in ROCSolver, which follow the naming
+  // convention in LAPACK see
+
+  // LU factorization.
+  // Computes LU factorization with partial pivoting P * A = L * U.
+  template <typename Scalar>
+  Status getrf(int m, int n, Scalar* dev_A, int lda, int* dev_pivots, int* info);
+
+  // Uses LU factorization to solve A * X = B.
+  template <typename Scalar>
+  Status getrs(const rocblas_operation trans, int n, int nrhs, Scalar* A,
+               int lda, const int* dev_pivots, Scalar* B, int ldb);
+
+  template <typename Scalar>
+  Status
+  getrf_batched(int m, int n, Scalar** dev_A, int lda, int* dev_pivots,
+                rocblas_stride stride, DeviceLapackInfo* info, const int batch_count);
+
+  template <typename Scalar>
+  Status getrs_batched(const rocblas_operation trans, int n, int nrhs,
+                       Scalar** A, int lda, int* dev_pivots,
+                       rocblas_stride stride, Scalar** B, const int ldb,
+                       const int batch_count);
+
 
   template <typename Scalar>
   Status Trsm(rocblas_side side, rocblas_fill uplo, rocblas_operation trans,
@@ -153,6 +217,59 @@ class ScratchSpace {
   const bool on_host_;
   Tensor scratch_tensor_;
 };
+
+class HostLapackInfo : public ScratchSpace<int> {
+ public:
+  HostLapackInfo(OpKernelContext* context, int64 size,
+                 const std::string& debug_info)
+      : ScratchSpace<int>(context, size, debug_info, /* on_host */ true) {}
+};
+
+class DeviceLapackInfo : public ScratchSpace<int> {
+ public:
+  DeviceLapackInfo(OpKernelContext* context, int64 size,
+                   const std::string& debug_info)
+      : ScratchSpace<int>(context, size, debug_info, /* on_host */ false) {}
+
+  // Allocates a new scratch space on the host and launches a copy of the
+  // contents of *this to the new scratch space. Sets success to true if
+  // the copy kernel was launched successfully.
+  HostLapackInfo CopyToHost(bool* success) const {
+    CHECK(success != nullptr);
+    HostLapackInfo copy(context(), size(), debug_info());
+    auto stream = context()->op_device_context()->stream();
+    se::DeviceMemoryBase wrapped_src(
+        static_cast<void*>(const_cast<int*>(this->data())));
+    *success =
+        stream->ThenMemcpy(copy.mutable_data(), wrapped_src, this->bytes())
+            .ok();
+    return copy;
+  }
+};
+
+
+template <typename Scalar>
+ScratchSpace<Scalar> ROCmSolver::GetScratchSpace(const TensorShape& shape,
+                                                 const std::string& debug_info,
+                                                 bool on_host) {
+  ScratchSpace<Scalar> new_scratch_space(context_, shape, debug_info, on_host);
+  scratch_tensor_refs_.emplace_back(new_scratch_space.tensor());
+  return std::move(new_scratch_space);
+}
+
+template <typename Scalar>
+ScratchSpace<Scalar> ROCmSolver::GetScratchSpace(int64 size,
+                                                 const std::string& debug_info,
+                                                 bool on_host) {
+  return GetScratchSpace<Scalar>(TensorShape({size}), debug_info, on_host);
+}
+
+inline DeviceLapackInfo ROCmSolver::GetDeviceLapackInfo(
+    int64 size, const std::string& debug_info) {
+  DeviceLapackInfo new_dev_info(context_, size, debug_info);
+  scratch_tensor_refs_.emplace_back(new_dev_info.tensor());
+  return new_dev_info;
+}
 
 }  // namespace tensorflow
 
