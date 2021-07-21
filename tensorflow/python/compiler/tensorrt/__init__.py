fcf60ab34423cf1180d74c9d866d921cf38c719d
Jonathan Dekhtiar <jdekhtiar@nvidia.com>
2021-07-21 13:53:58 -0700

    [TF-TRT] Refactoring TF-TRT top level __init__.py

--- tensorflow/python/compiler/tensorrt/__init__.py
+++ tensorflow/python/compiler/tensorrt/__init__.py
@@ -15,5 +15,69 @@
 """Exposes the python wrapper for TensorRT graph transforms."""
 
 # pylint: disable=unused-import,line-too-long
+import os
+import sys
+from tensorflow.compiler.tf2tensorrt import _pywrap_py_utils
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
+from tensorflow.python.compiler.tensorrt import utils as trt_utils
+from tensorflow.python.platform import tf_logging as logging
 # pylint: enable=unused-import,line-too-long
+
+
+def _raise_trt_version_not_supported(version_type, trt_version):
+  assert version_type in ["linked", "loaded"], (
+    f"Incorrect value received for version_type: {version_type}. " \
+    f"Accepted: ['linked', 'loaded']"
+  )
+
+  logging.error(
+      f"The support for {version_type} version of TensorRT: "\
+      f"`{trt_utils.version_tuple_to_string(trt_version)}` has now been " \
+      f"removed. Please upgrade to TensorRT 7 or more recent.")
+
+  raise RuntimeError(f"Incompatible {version_type} TensorRT versions")
+
+
+if not _pywrap_py_utils.is_tensorrt_enabled():
+  logging.error(
+      "Tensorflow needs to be built with TensorRT support enabled to allow "
+      "TF-TRT to operate.")
+
+  raise RuntimeError("Tensorflow has not been built with TensorRT support.")
+
+
+# Do not execute the following on a machine that is not linux-based and during
+# bazel build execution.
+if "bazel" not in os.environ["_"] and sys.platform == "linux":
+  linked_version = _pywrap_py_utils.get_linked_tensorrt_version()
+  loaded_version = _pywrap_py_utils.get_loaded_tensorrt_version()
+
+  logging.info("Linked TensorRT version: %s", str(linked_version))
+  logging.info("Loaded TensorRT version: %s", str(loaded_version))
+
+  if not trt_utils.is_linked_tensorrt_version_greater_equal(7, 0, 0):
+    _raise_trt_version_not_supported("linked", linked_version)
+
+  if not trt_utils.is_loaded_tensorrt_version_greater_equal(7, 0, 0):
+    _raise_trt_version_not_supported("loaded", loaded_version)
+
+  if (loaded_version[0] != linked_version[0] or
+      not trt_utils.is_loaded_tensorrt_version_greater_equal(*linked_version)):
+    loaded_version_str = trt_utils.version_tuple_to_string(loaded_version)
+    linked_version_str = trt_utils.version_tuple_to_string(linked_version)
+    logging.error(
+        f"Loaded TensorRT `{loaded_version_str}` but linked against " \
+        f"TensorRT: `{linked_version_str}`. Not all requirements are met:\n" \
+        f"\t- It is required to use the same major version of TensorRT " \
+        f"during compilation and runtime.\n" \
+        f"\t- TensorRT does not support forward compatibility. The loaded " \
+        f"version has to be not later than the linked version."
+    )
+    raise RuntimeError("Incompatible TensorRT major version")
+
+  elif loaded_version != linked_version:
+    logging.info(
+        "Loaded TensorRT %s and linked TensorFlow against TensorRT %s. This is "
+        "supported because TensorRT minor/patch upgrades are backward "
+        "compatible.", trt_utils.version_tuple_to_string(loaded_version),
+        trt_utils.version_tuple_to_string(linked_version))
--- tensorflow/python/compiler/tensorrt/trt_convert.py
+++ tensorflow/python/compiler/tensorrt/trt_convert.py
@@ -62,10 +62,6 @@ gen_trt_ops = LazyLoader(
     "gen_trt_ops", globals(),
     "tensorflow.compiler.tf2tensorrt.ops.gen_trt_ops")
 
-_pywrap_py_utils = LazyLoader(
-    "_pywrap_py_utils", globals(),
-    "tensorflow.compiler.tf2tensorrt._pywrap_py_utils")
-
 # Register TRT ops in python, so that when users import this module they can
 # execute a TRT-converted graph without calling any of the methods in this
 # module.
@@ -200,66 +196,6 @@ def _check_conversion_params(conversion_params, is_v2=False):
     raise ValueError("minimum segment size should be positive or -1 "
                      "(to disable main graph conversion).")
 
-
-def _check_trt_version_compatibility():
-  """Check compatibility of TensorRT version.
-
-  Raises:
-    RuntimeError: if the TensorRT library version is incompatible.
-  """
-
-  if not _pywrap_py_utils.is_tensorrt_enabled():
-    logging.error(
-        "Tensorflow needs to be built with TensorRT support enabled to allow "
-        "TF-TRT to operate.")
-
-    raise RuntimeError("Tensorflow has not been built with TensorRT support.")
-
-  linked_version = _pywrap_py_utils.get_linked_tensorrt_version()
-  loaded_version = _pywrap_py_utils.get_loaded_tensorrt_version()
-
-  logging.info("Linked TensorRT version: %s", str(linked_version))
-  logging.info("Loaded TensorRT version: %s", str(loaded_version))
-
-  def raise_trt_version_deprecated(version_type, trt_version):
-    assert version_type in [
-        "linked", "loaded"
-    ], ("Incorrect value received for version_type: %s. Accepted: ['linked', "
-        "'loaded']") % version_type
-
-    logging.error(
-        "The {version_type} version of TensorRT: `{trt_version}` has now "
-        "been removed. Please upgrade to TensorRT 7 or more recent.".format(
-            version_type=version_type,
-            trt_version=trt_utils.version_tuple_to_string(trt_version)))
-
-    raise RuntimeError("Incompatible %s TensorRT versions" % version_type)
-
-  if not trt_utils.is_linked_tensorrt_version_greater_equal(7, 0, 0):
-    raise_trt_version_deprecated("linked", linked_version)
-
-  if not trt_utils.is_loaded_tensorrt_version_greater_equal(7, 0, 0):
-    raise_trt_version_deprecated("loaded", loaded_version)
-
-  if (loaded_version[0] != linked_version[0] or
-      not trt_utils.is_loaded_tensorrt_version_greater_equal(*linked_version)):
-    logging.error(
-        "Loaded TensorRT %s but linked TensorFlow against TensorRT %s. A few "
-        "requirements must be met:\n"
-        "\t-It is required to use the same major version of TensorRT during "
-        "compilation and runtime.\n"
-        "\t-TensorRT does not support forward compatibility. The loaded "
-        "version has to be equal or more recent than the linked version.",
-        trt_utils.version_tuple_to_string(loaded_version),
-        trt_utils.version_tuple_to_string(linked_version))
-    raise RuntimeError("Incompatible TensorRT major version")
-
-  elif loaded_version != linked_version:
-    logging.info(
-        "Loaded TensorRT %s and linked TensorFlow against TensorRT %s. This is "
-        "supported because TensorRT minor/patch upgrades are backward "
-        "compatible.", trt_utils.version_tuple_to_string(loaded_version),
-        trt_utils.version_tuple_to_string(linked_version))
 
 
 def _get_tensorrt_rewriter_config(conversion_params,
@@ -470,7 +406,6 @@ class TrtGraphConverter(object):
     if not input_graph_def and not input_saved_model_dir:
       raise ValueError("Must specify one of input_graph_def and "
                        "input_saved_model_dir")
-    _check_trt_version_compatibility()
 
     self._input_graph_def = input_graph_def
     self._nodes_denylist = nodes_denylist
@@ -1041,7 +976,6 @@ class TrtGraphConverterV2(object):
           use_calibration=use_calibration,
           allow_build_at_runtime=allow_build_at_runtime)
 
-    _check_trt_version_compatibility()
     _check_conversion_params(conversion_params, is_v2=True)
 
     self._conversion_params = conversion_params
