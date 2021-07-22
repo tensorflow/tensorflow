5c4815379dcb6231fec6ec09fd80c0eb984d96c0
Trent Lo <trentl@nvidia.com>
2021-07-21 18:05:20 -0700

    [XLA/GPU] Make PassThroughAlias work with ResVarIoAliasing.

--- tensorflow/compiler/xla/service/gpu/alias_passthrough_params.cc
+++ tensorflow/compiler/xla/service/gpu/alias_passthrough_params.cc
@@ -37,6 +37,15 @@ StatusOr<bool> AliasPassthroughParams::Run(HloModule* module) {
               << " in module " << module->name()
               << " is passed-through to root tuple element " << i << ": "
               << root->shape().ToString();
+
+      if (module->input_output_alias_config().OutputHasAlias({i}) ||
+          module->input_output_alias_config().ParameterHasAlias(
+              root->operand(i)->parameter_number(), /*param_index=*/{})) {
+        VLOG(2) << "Skip setting the above pass-through alias as an alias may"
+                << " have been set up by ResVarIoAliasing.";
+        continue;
+      }
+
       TF_RETURN_IF_ERROR(module->input_output_alias_config().SetUpAlias(
           /*output_index=*/{i},
           /*param_number=*/root->operand(i)->parameter_number(),
