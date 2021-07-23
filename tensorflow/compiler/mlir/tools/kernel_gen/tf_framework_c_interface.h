d63538dbe8084b09489f679f957456f94b4f5109
Tres Popp <tpopp@google.com>
2021-07-23 04:02:01 -0700

    [KernelGen][JIT] Rename `tfKernelGenLaunchKernel` to `_mlir_ciface_tf_launch_kernel`
    Rename `tfKernelGenLaunchKernel` to `_mlir_ciface_tf_launch_kernel` and make
    it a part of kernel generator's C interface.
    
    PiperOrigin-RevId: 386424429
    Change-Id: I13248205ea82344b7404782713f310b35e6b3439

--- tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.h
+++ tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.h
@@ -22,6 +22,11 @@ namespace mlir {
 namespace kernel_gen {
 namespace tf_framework {
 
+extern "C" MLIR_RUNNERUTILS_EXPORT void _mlir_ciface_tf_launch_kernel(
+    void* ctx, void* module_blob, char* kernel_name, intptr_t gridX,
+    intptr_t gridY, intptr_t gridZ, intptr_t blockX, intptr_t blockY,
+    intptr_t blockZ, void** params);
+
 extern "C" MLIR_RUNNERUTILS_EXPORT void* _mlir_ciface_tf_alloc(
     void* op_kernel_ctx, size_t num_elements, size_t element_size,
     int32_t output_index, int32_t num_candidates,
