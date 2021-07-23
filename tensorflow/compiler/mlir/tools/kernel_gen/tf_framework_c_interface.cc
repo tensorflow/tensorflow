600661e92f879664edb2d0cce97f0e7d9926c638
Tres Popp <tpopp@google.com>
2021-07-23 04:20:57 -0700

    [KernelGen][JIT] Implement JIT runtime functions
    Implement the JIT runtime function with a cache for already compiled modules.
    
    PiperOrigin-RevId: 386426657
    Change-Id: I319dd12110654b09a920b8df72888447b645105f

--- tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.cc
@@ -15,9 +15,17 @@ limitations under the License.
 
 #include "tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.h"
 
+#include "absl/container/flat_hash_map.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/TargetSelect.h"
+#include "mlir/ExecutionEngine/ExecutionEngine.h"  // from @llvm-project
+#include "mlir/ExecutionEngine/OptUtils.h"  // from @llvm-project
+#include "mlir/Parser.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/tools/kernel_gen/ir/tf_framework_ops.h"
+#include "tensorflow/compiler/mlir/tools/kernel_gen/kernel_creator.h"
 #include "tensorflow/core/framework/allocator.h"
 #include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/resource_mgr.h"
 
 namespace mlir {
 namespace kernel_gen {
@@ -94,17 +102,132 @@ static void ReportError(void* op_kernel_ctx, ErrorCode error_code,
                                const_cast<char*>(msg));
 }
 
+namespace {
+
+// A callback to register all externally defined symbols needed by the kernel.
+llvm::orc::SymbolMap TFFrameworkSymbolMap(llvm::orc::MangleAndInterner mangle) {
+  llvm::orc::SymbolMap symbol_map;
+  auto bind = [&](llvm::StringRef name, auto symbol_ptr) {
+    symbol_map[mangle(name)] = llvm::JITEvaluatedSymbol(
+        llvm::pointerToJITTargetAddress(symbol_ptr), llvm::JITSymbolFlags());
+  };
+
+  // Register all the symbols.
+  bind("_mlir_ciface_tf_alloc", &_mlir_ciface_tf_alloc);
+  bind("_mlir_ciface_tf_dealloc", &_mlir_ciface_tf_dealloc);
+  bind("_mlir_ciface_tf_launch_kernel", &_mlir_ciface_tf_launch_kernel);
+  bind("_mlir_ciface_tf_report_error", &_mlir_ciface_tf_report_error);
+
+  return symbol_map;
+}
+
+class JITCache : public tensorflow::ResourceBase {
+ public:
+  std::string DebugString() const override { return "JIT cache"; }
+
+  ExecutionEngine* LookupOrCompile(const char* code) {
+    // Check if we already have a compiled module in the cache.
+    std::string key(code);
+    {
+      tensorflow::mutex_lock lock(mu_);
+      if (execution_engine_by_key_.contains(key))
+        return execution_engine_by_key_[key].get();
+    }
+
+    // Otherwise, compile the module now.
+    // TODO(frgossen): Propagate these parameters through the JIT invocation.
+    // For now, use some default parameters.
+    llvm::SmallVector<std::string, 4> architectures = {"sm_60", "sm_70",
+                                                       "sm_75"};
+    llvm::SmallVector<int64_t, 1> tile_sizes = {1024};
+    llvm::SmallVector<int64_t, 1> unroll_factors = {4};
+    int64_t max_supported_rank = 5;
+    bool enable_ftz = false;
+    bool cpu_codegen = false;
+    bool jit_compile = false;
+
+    // Create the kernel.
+    mlir::MLIRContext context;
+    xla::StatusOr<mlir::OwningModuleRef> status_or_module =
+        tensorflow::kernel_gen::GenerateKernelForTfCode(
+            context, code, architectures, tile_sizes, unroll_factors,
+            max_supported_rank, /*embed_memref_prints=*/false,
+            /*print_ptx=*/false, enable_ftz, cpu_codegen, jit_compile);
+    if (!status_or_module.ok()) return nullptr;
+    mlir::OwningModuleRef module = std::move(status_or_module.ValueOrDie());
+
+    // Initialize LLVM targets.
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+
+    // Create execution engine with an inner optimization pipeline.
+    auto opt_pipeline = mlir::makeOptimizingTransformer(
+        /*optLevel=*/2, /*sizeLevel=*/0, /*targetMachine=*/nullptr);
+    llvm::Expected<std::unique_ptr<ExecutionEngine>> expected_engine =
+        mlir::ExecutionEngine::create(
+            module.get(), /*llvmModuleBuilder=*/nullptr, opt_pipeline);
+    if (!expected_engine) return nullptr;
+    std::unique_ptr<ExecutionEngine> engine = std::move(expected_engine.get());
+    engine->registerSymbols(TFFrameworkSymbolMap);
+
+    // Insert the compiled module into our cache and return a raw pointer.
+    {
+      tensorflow::mutex_lock lock(mu_);
+      execution_engine_by_key_[key] = std::move(engine);
+      return execution_engine_by_key_[key].get();
+    }
+  }
+
+ private:
+  tensorflow::mutex mu_;
+  absl::flat_hash_map<std::string, std::unique_ptr<ExecutionEngine>>
+      execution_engine_by_key_ TF_GUARDED_BY(mu_);
+};
+
+}  // namespace
+
 extern "C" void* _mlir_ciface_tf_jit_compile(void* op_kernel_ctx, char* code) {
-  ReportError(op_kernel_ctx, ErrorCode::UNIMPLEMENTED,
-              "JIT compilation is not yet implemented.");
-  return nullptr;
+  // Get the resource manager.
+  auto* ctx = static_cast<tensorflow::OpKernelContext*>(op_kernel_ctx);
+  tensorflow::ResourceMgr* rm = ctx->resource_manager();
+  if (!rm) {
+    ReportError(op_kernel_ctx, ErrorCode::UNKNOWN, "No resource manager.");
+    return nullptr;
+  }
+
+  // Get the JIT cache.
+  JITCache* jit_cache = nullptr;
+  const std::string kJITCacheName = "mlir-jit-cache";
+  auto status = rm->LookupOrCreate<JITCache>(
+      rm->default_container(), kJITCacheName, &jit_cache, [&](JITCache** dst) {
+        *dst = new JITCache;
+        return tensorflow::Status::OK();
+      });
+  tensorflow::core::ScopedUnref jit_cache_ref(jit_cache);
+  if (!status.ok()) {
+    ReportError(op_kernel_ctx, ErrorCode::UNKNOWN,
+                "Failed to find or create JIT cache.");
+    return nullptr;
+  }
+
+  // Lookup or compile the execution module.
+  ExecutionEngine* engine = jit_cache->LookupOrCompile(code);
+  if (engine == nullptr) {
+    ReportError(op_kernel_ctx, ErrorCode::UNKNOWN, "JIT compilation failed.");
+    return nullptr;
+  }
+  return engine;
 }
 
 extern "C" void _mlir_ciface_tf_jit_execute(void* op_kernel_ctx, void* callable,
                                             void* result, int64_t arg_rank,
                                             void* arg_descr) {
-  ReportError(op_kernel_ctx, ErrorCode::UNIMPLEMENTED,
-              "JIT execution is not yet implemented.");
+  ::UnrankedMemRefType<void> arg = {arg_rank, arg_descr};
+  llvm::Error invocation_result =
+      static_cast<ExecutionEngine*>(callable)->invoke("main", result,
+                                                      op_kernel_ctx, &arg);
+  if (invocation_result)
+    ReportError(op_kernel_ctx, ErrorCode::UNKNOWN, "JIT invocation failed.");
 }
 
 }  // namespace tf_framework
