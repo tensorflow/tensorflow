e82586197a14732830355b1f6aeafbd1424a1f2c
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-07-24 10:43:22 +0900

    .cmake update

--- tensorflow/lite/tools/cmake/modules/xnnpack.cmake
+++ tensorflow/lite/tools/cmake/modules/xnnpack.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   xnnpack
   GIT_REPOSITORY https://github.com/google/XNNPACK
   # Sync with tensorflow/workspace2.bzl
-  GIT_TAG 56b78a03e359ac04a3ba758596cd28b198a8000f
+  GIT_TAG 476eb84d6a8e6f8249d5584d30759c6fbdbf791d
   GIT_PROGRESS TRUE
   PREFIX "${CMAKE_BINARY_DIR}"
   SOURCE_DIR "${CMAKE_BINARY_DIR}/xnnpack"
