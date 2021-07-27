9dc1c6bdec46c2f2024f916939d53fd15c3990bc
Andrew Audibert <aaudibert@google.com>
2021-07-27 09:20:16 -0700

    [tf.data service] Document visitation guarantees for sharding modes.
    PiperOrigin-RevId: 387125353
    Change-Id: I1c4b73a06ba891b8389cb9d244fb3fb94dff2d8c

--- tensorflow/python/data/experimental/ops/data_service_ops.py
+++ tensorflow/python/data/experimental/ops/data_service_ops.py
@@ -58,19 +58,25 @@ class ShardingPolicy(enum.IntEnum):
   OFF: No sharding will be performed. Each worker produces the entire dataset
   without any sharding. With this mode, the best practice is to shuffle the
   dataset nondeterministically so that workers process the dataset in different
-  orders.
+  orders. If workers are restarted or join the cluster mid-job, they will begin
+  processing the dataset from the beginning.
 
   DYNAMIC: The input dataset is dynamically split among workers at runtime. Each
   worker gets the next split when it reads data from the dispatcher. Data is
   produced non-deterministically in this mode. Dynamic sharding works well with
   varying-sized tf.data service clusters, e.g., when you need to auto-scale your
-  workers.
+  workers. Dynamic sharding provides at-most once visitation guarantees. No
+  examples will be repeated, but some may be missed if a tf.data service worker
+  gets restarted while processing a file.
 
   The following are static sharding policies. The semantics are similar to
   `tf.data.experimental.AutoShardPolicy`. These policies require:
-  * The tf.data service cluster has a fixed size, and you need to specify the
-    workers in DispatcherConfig.
+  * The tf.data service cluster is configured with a fixed list of workers
+    in DispatcherConfig.
   * Each client only reads from the local tf.data service worker.
+
+  If a worker is restarted while performing static sharding, the worker will
+  begin processing its shard again from the beginning.
 
   FILE: Shards by input files (i.e. each worker will get a fixed set of files to
   process). When this option is selected, make sure that there is at least as
