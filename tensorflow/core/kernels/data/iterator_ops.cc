57da85f8870bc8dee1b77225b3e30ea3f314d304
Jose Baiocchi <jbaiocchi@google.com>
2021-07-27 18:52:13 -0700

    Use TraceMeEncode in IteratorGetNextAsOptionalOp
    PiperOrigin-RevId: 387246534
    Change-Id: Ibe13dc9aa42e1adba4f98d3c94582e9eb2eb0585

--- tensorflow/core/kernels/data/iterator_ops.cc
+++ tensorflow/core/kernels/data/iterator_ops.cc
@@ -943,9 +943,9 @@ Status IteratorGetNextOp::DoCompute(OpKernelContext* ctx) {
 Status IteratorGetNextAsOptionalOp::DoCompute(OpKernelContext* ctx) {
   profiler::TraceMe traceme(
       [&] {
-        return strings::StrCat(
-            "IteratorGetNextAsOptionalOp::DoCompute#id=", ctx->step_id(),
-            ",iter_num=", ctx->frame_iter().iter_id, "#");
+        return profiler::TraceMeEncode(
+            "IteratorGetNextAsOptionalOp::DoCompute",
+            {{"id", ctx->step_id()}, {"iter_num", ctx->frame_iter().iter_id}});
       },
       profiler::kInfo);
   tensorflow::ResourceTagger tag(kTFDataResourceTag,
