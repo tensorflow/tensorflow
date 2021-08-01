b52ed5b620ae66812d3fbff7432f487e08987708
Haoyu Zhang <haoyuzhang@google.com>
2021-08-01 14:55:17 -0700

    Enable standalone coordination service when connecting to cluster with certain protocols.
    PiperOrigin-RevId: 388112328
    Change-Id: I95ed5779c0da14f3721696399ad083f9bbc7fd97

--- tensorflow/python/eager/context.py
+++ tensorflow/python/eager/context.py
@@ -699,6 +699,10 @@ class Context(object):
           "Coordination service must be enabled at program startup time.")
     self._coordination_service = service_type
 
+  @property
+  def coordination_service(self):
+    return self._coordination_service
+
   def set_config_key_value(self, key, value):
     ensure_initialized()
     pywrap_tfe.TFE_InsertConfigKeyValue(self._context_handle, key, value)
