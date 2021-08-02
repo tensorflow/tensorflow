edb98309db7c7b532827d7b436cbd12922916cbb
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-02 15:42:38 -0700

    Add migratinon block for tf.compat.v1.div
    PiperOrigin-RevId: 388321814
    Change-Id: I22b45068866697973fe4e692f31fdfa178e8cdf4

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -1555,8 +1555,12 @@ def truediv(x, y, name=None):
 def div(x, y, name=None):
   """Divides x / y elementwise (using Python 2 division operator semantics).
 
-  NOTE: Prefer using the Tensor division operator or tf.divide which obey Python
-  3 division operator semantics.
+  @compatibility(TF2)
+  This function is deprecated in TF2. Prefer using the Tensor division operator,
+  `tf.divide`, or `tf.math.divide`, which obey the Python 3 division operator
+  semantics.
+  @end_compatibility
+
 
   This function divides `x` and `y`, forcing Python 2 semantics. That is, if `x`
   and `y` are both integers then the result will be an integer. This is in
