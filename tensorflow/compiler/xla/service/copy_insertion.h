d1ef81e3d4e01343be109a6a5aa8a6eaafc58204
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-03 09:38:11 -0700

    [XLA] Selectively turn on the more precise region-based analysis in copy insertion when the involved live range regions are smaller than a predefined limit, so that the compilation overhead of the new analysis is not overly steep.
    PiperOrigin-RevId: 388475953
    Change-Id: Iece947b5d920e1209ff2637a44f6e84bcda7a8b0

--- tensorflow/compiler/xla/service/copy_insertion.h
+++ tensorflow/compiler/xla/service/copy_insertion.h
@@ -46,6 +46,7 @@ namespace xla {
 class CopyInsertion : public HloModulePass {
  public:
   absl::string_view name() const override { return "copy-insertion"; }
+  static constexpr int64_t kUseRegionAnalysisLimit = 0;
 
   // backend specific function that decides whether an instruction
   // can share buffer with its operand.
@@ -54,7 +55,7 @@ class CopyInsertion : public HloModulePass {
   // buffer.
   explicit CopyInsertion(
       const HloDataflowAnalysis::CanShareBuffer& can_share_buffer = nullptr,
-      bool use_region_based_live_range_analysis = false)
+      int64_t use_region_based_live_range_analysis = kUseRegionAnalysisLimit)
       : can_share_buffer_(can_share_buffer),
         use_region_based_live_range_analysis_(
             use_region_based_live_range_analysis) {}
@@ -101,7 +102,7 @@ class CopyInsertion : public HloModulePass {
   // TODO(b/189898980): the region based live range analysis currently
   // does not enforce a strict ordering of the merged live ranges. This may
   // cause problems for parallel workloads (e.g., in SPMD).
-  bool use_region_based_live_range_analysis_;
+  int64_t use_region_based_live_range_analysis_;
 };
 
 }  // namespace xla
