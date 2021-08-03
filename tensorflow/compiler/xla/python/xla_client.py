5009f5c251b967c40791486e2447f14e3f5256d0
Roy Frostig <frostig@google.com>
2021-08-03 09:53:53 -0700

    [jax] set leaf and node counts when creating a tuple pytree definition
    PiperOrigin-RevId: 388479354
    Change-Id: Idc996aeb3b1a7419090a1511d79a217892ac53b4

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -50,7 +50,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 28
+_version = 29
 
 xla_platform_names = {
     'cpu': 'Host',
