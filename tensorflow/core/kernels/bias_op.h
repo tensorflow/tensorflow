f6e2f4285353c60244854a69ea57b2b435c12536
Sung Jin Hwang <sjhwang@google.com>
2021-08-05 01:32:00 -0700

    Removed unnecessary template instantiations in BiasAdd CPU op kernel.
    PiperOrigin-RevId: 388880939
    Change-Id: I6c6a2ad241d084a55234b8279b3aee00745e505e

--- tensorflow/core/kernels/bias_op.h
+++ tensorflow/core/kernels/bias_op.h
@@ -24,26 +24,36 @@ namespace tensorflow {
 namespace functor {
 
 // Functor used by BiasOp to do the computations.
-template <typename Device, typename T, int Dims>
+template <typename Device, typename T>
 struct Bias {
-  // Add "bias" to "input", broadcasting it on all dimensions but the last one.
-  void operator()(const Device& d, typename TTypes<T, Dims>::ConstTensor input,
+  // Add "bias" to "input", repeating "bias".
+  void operator()(const Device& d, typename TTypes<T>::ConstFlat input,
                   typename TTypes<T>::ConstVec bias,
-                  typename TTypes<T, Dims>::Tensor output) {
+                  typename TTypes<T>::Flat output) {
+    const Eigen::Index rest_size = input.size() / bias.dimension(0);
     if (input.size() >= INT_MAX) {
-      const Eigen::Index bias_size = bias.dimension(0);
-      const Eigen::Index rest_size = input.size() / bias_size;
-      Eigen::DSizes<Eigen::Index, 1> one_d(input.size());
       Eigen::DSizes<Eigen::Index, 1> bcast(rest_size);
-      output.reshape(one_d).device(d) =
-          input.reshape(one_d) + bias.broadcast(bcast);
+      output.device(d) = input + bias.broadcast(bcast);
     } else {
-      const int bias_size = bias.dimension(0);
-      const int rest_size = input.size() / bias_size;
-      Eigen::DSizes<int, 1> one_d(input.size());
       Eigen::DSizes<int, 1> bcast(rest_size);
-      To32Bit(output).reshape(one_d).device(d) =
-          To32Bit(input).reshape(one_d) + To32Bit(bias).broadcast(bcast);
+      To32Bit(output).device(d) =
+          To32Bit(input) + To32Bit(bias).broadcast(bcast);
+    }
+  }
+
+  // NCHW layout, repeating on the first dimension, broadcasting on the last
+  // dimension.
+  void operator()(const Device& d, typename TTypes<T>::ConstMatrix input,
+                  typename TTypes<T>::ConstMatrix bias1,  // shape [C, 1].
+                  typename TTypes<T>::Matrix output) {
+    const Eigen::Index rest_size = input.dimension(0) / bias1.dimension(0);
+    if (input.size() >= INT_MAX) {
+      Eigen::DSizes<Eigen::Index, 2> bcast(rest_size, input.dimension(1));
+      output.device(d) = input + bias1.broadcast(bcast);
+    } else {
+      Eigen::DSizes<int, 2> bcast(rest_size, input.dimension(1));
+      To32Bit(output).device(d) =
+          To32Bit(input) + To32Bit(bias1).broadcast(bcast);
     }
   }
 };
