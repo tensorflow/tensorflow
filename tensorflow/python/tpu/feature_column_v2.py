3c61a69d7a753a4ec825cfd970f77d93cf6dc647
Amy Skerry-Ryan <askerryryan@google.com>
2021-08-06 10:10:50 -0700

    Update __deepcopy__ on _TPUEmbeddingColumnV2 to preserve the state of previously dropped arguments
    PiperOrigin-RevId: 389201699
    Change-Id: I77a45a96b291a7a22addff3f521e8d738d52f33f

--- tensorflow/python/tpu/feature_column_v2.py
+++ tensorflow/python/tpu/feature_column_v2.py
@@ -445,7 +445,8 @@ class _TPUEmbeddingColumnV2(_TPUBaseEmbeddingColumn, fc_lib.EmbeddingColumn):
 
   def __getnewargs__(self):
     return (self._tpu_categorical_column, self.dimension, self.combiner,
-            self.initializer, self._max_sequence_length, self._learning_rate_fn)
+            self.initializer, self._max_sequence_length, self._learning_rate_fn,
+            self.use_safe_embedding_lookup, self._bypass_scope_validation)
 
   def __deepcopy__(self, memo):
     return _TPUEmbeddingColumnV2(
