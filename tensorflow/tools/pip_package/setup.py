fe32b8cb838a78401f36c11f7efe6da1fa808e0a
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-09 08:10:08 -0700

    Update tensorboard dependency to 2.6.x and and tb-nightly dependency to 2.7.x.
    PiperOrigin-RevId: 389635311
    Change-Id: I73e876a3f398463f600c4c61bfd27468a1d7a2dc

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -101,7 +101,7 @@ REQUIRED_PACKAGES = [
     # These need to be in sync with the existing TF version
     # They are updated during the release process
     # When updating these, please also update the nightly versions below
-    'tensorboard ~= 2.5',
+    'tensorboard ~= 2.6',
     'tensorflow_estimator ~= 2.5.0',
     # TODO(scottzhu): OSS keras hasn't been formally released yet.
     # Use keras-nightly at the moment.
@@ -117,7 +117,7 @@ REQUIRED_PACKAGES = [
 if 'tf_nightly' in project_name:
   for i, pkg in enumerate(REQUIRED_PACKAGES):
     if 'tensorboard' in pkg:
-      REQUIRED_PACKAGES[i] = 'tb-nightly ~= 2.6.0.a'
+      REQUIRED_PACKAGES[i] = 'tb-nightly ~= 2.7.0.a'
     elif 'tensorflow_estimator' in pkg:
       REQUIRED_PACKAGES[i] = 'tf-estimator-nightly ~= 2.7.0.dev'
     elif 'keras' in pkg and 'keras_preprocessing' not in pkg:
