589a562c31531384a22085b93e1d9d78e38906e9
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-12 10:00:51 -0700

    Add migration block for tf.compat.v1.fixed_size_partitioner
    PiperOrigin-RevId: 390397209
    Change-Id: I0e68060431b279ff3f0be7d6f93b239488058c34

--- tensorflow/python/ops/partitioned_variables.py
+++ tensorflow/python/ops/partitioned_variables.py
@@ -225,6 +225,42 @@ def min_max_variable_partitioner(max_partitions=1, axis=0,
 def fixed_size_partitioner(num_shards, axis=0):
   """Partitioner to specify a fixed number of shards along given axis.
 
+  @compatibility(TF2)
+  This API is deprecated in TF2. In TF2, partitioner is no longer part of
+  the variable declaration via `tf.Variable`.
+  [ParameterServer Training]
+  (https://www.tensorflow.org/tutorials/distribute/parameter_server_training)
+  handles partitioning of variables. The corresponding TF2 partitioner class of
+  `fixed_size_partitioner` is
+  `tf.distribute.experimental.partitioners.FixedShardsPartitioner`.
+
+  Check the [migration guide]
+  (https://www.tensorflow.org/guide/migrate#2_use_python_objects_to_track_variables_and_losses)
+  on the differences in treatment of variables and losses between TF1 and TF2.
+
+  Before:
+
+    ```
+    x = tf.compat.v1.get_variable(
+      "x", shape=(2,), partitioner=tf.compat.v1.fixed_size_partitioner(2)
+    )
+    ```
+  After:
+
+    ```
+    partitioner = (
+        tf.distribute.experimental.partitioners.FixedShardsPartitioner(
+            num_shards=2)
+    )
+    strategy = tf.distribute.experimental.ParameterServerStrategy(
+                   cluster_resolver=cluster_resolver,
+                   variable_partitioner=partitioner)
+
+    with strategy.scope():
+      x = tf.Variable([1.0, 2.0])
+    ```
+  @end_compatibility
+
   Args:
     num_shards: `int`, number of shards to partition variable.
     axis: `int`, axis to partition on.
