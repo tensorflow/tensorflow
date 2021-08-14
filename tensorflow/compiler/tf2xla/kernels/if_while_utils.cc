7fe1c8cfb08801eb0aaed0b8204ba78db530b8a7
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-14 15:38:58 -0700

    Internal change
    PiperOrigin-RevId: 390830370
    Change-Id: I1e52f74f6b272c9aa28b619e6b44b45e19ff232b

--- tensorflow/compiler/tf2xla/kernels/if_while_utils.cc
+++ tensorflow/compiler/tf2xla/kernels/if_while_utils.cc
@@ -38,21 +38,16 @@ absl::InlinedVector<int, 5> ConvertCompileTimeConstArgumentsToConst(
       VLOG(1) << "Trying to resolve constant " << i;
       // NOTE: We can not simply check that this is Kind::kConstant because
       // this could be the output of a MetadataOnly op e.g. Size.
-
-      // If we can infer the constant values of an inner computation's argument,
-      // replace them with constants. If that fails, we fallback to infer the
-      // bounds of the argument.
       StatusOr<absl::optional<Tensor>> maybe_constant =
           expression.ResolveConstant(ctx->compiler()->client());
-      StatusOr<absl::optional<Tensor>> bounds =
-          expression.ResolveConstant(ctx->compiler()->client(), false,
-                                     xla::ValueInferenceMode::kUpperBound);
-      if ((maybe_constant.ok() && maybe_constant->has_value()) ||
-          (bounds.ok() && bounds->has_value())) {
+      if (maybe_constant.ok() && maybe_constant.ValueOrDie().has_value()) {
         StatusOr<Tensor> values_are_dynamic =
             expression.ResolveDynamism(ctx->compiler()->client());
         bool all_values_are_static = false;
-        if (values_are_dynamic.ok()) {
+        if (!values_are_dynamic.ok()) {
+          // Conservatiely assume all values are dynamic.
+          all_values_are_static = true;
+        } else {
           xla::Literal literal =
               HostTensorToLiteral(values_are_dynamic.ValueOrDie()).ValueOrDie();
           all_values_are_static = literal.IsAll(0);
@@ -65,7 +60,8 @@ absl::InlinedVector<int, 5> ConvertCompileTimeConstArgumentsToConst(
           arg->shape = expression.GetShape().ValueOrDie();
           resolved_constant_idxs.push_back(i);
         } else {
-          arg->value_bound.emplace(std::move(bounds.ValueOrDie().value()));
+          arg->value_bound.emplace(
+              std::move(maybe_constant.ValueOrDie().value()));
           arg->value_dynamism.emplace(
               std::move(values_are_dynamic.ValueOrDie()));
         }
