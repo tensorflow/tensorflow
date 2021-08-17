0ce6e52efb7be17760e6f0e1855a5d0a9a7c2cae
Jean-Baptiste Lespiau <jblespiau@google.com>
2021-08-17 06:11:07 -0700

    Add post_hook support for pmap, to support debug_nans and debug_infs.
    It's the exact same code as for JIT. We just modify the Python function to accept ShardedDeviceArray in addition to DeviceArray objects. The test is updated accordingly.
    
    PiperOrigin-RevId: 391272270
    Change-Id: Iebd5fa71f01a602b0de7106589fb30fa6783de69

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -50,7 +50,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 35
+_version = 36
 
 xla_platform_names = {
     'cpu': 'Host',
