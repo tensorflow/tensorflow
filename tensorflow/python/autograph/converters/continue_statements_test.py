5e89d92bb0cc61105d8162aa2f8d634e13611f58
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-17 10:40:36 +0430

    Reformatting the indentation of continue_statements_test.py

--- tensorflow/python/autograph/converters/continue_statements_test.py
+++ tensorflow/python/autograph/converters/continue_statements_test.py
@@ -26,202 +26,202 @@ from tensorflow.python.platform import test
 
 class ContinueCanonicalizationTest(converter_testing.TestCase):
 
-  def assertTransformedEquivalent(self, f, *inputs):
-    tr = self.transform(f, continue_statements)
-    self.assertEqual(f(*inputs), tr(*inputs))
+    def assertTransformedEquivalent(self, f, *inputs):
+        tr = self.transform(f, continue_statements)
+        self.assertEqual(f(*inputs), tr(*inputs))
 
-  def test_basic(self):
+    def test_basic(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        if x % 2 == 0:
-          continue
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                if x % 2 == 0:
+                    continue
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_multiple_continues(self):
+    def test_multiple_continues(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        if x > 1:
-          continue
-        if x > 2:
-          continue
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                if x > 1:
+                    continue
+                if x > 2:
+                    continue
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_multiple_continues_in_nested_scope(self):
+    def test_multiple_continues_in_nested_scope(self):
 
-    def f(a):
-      v = []
-      for x in a:
-        x -= 1
-        if x > 100:
-          continue
-        try:
-          raise ValueError('intentional')
-        except ValueError:
-          continue
-        v.append(x)
-      return v
+        def f(a):
+            v = []
+            for x in a:
+                x -= 1
+                if x > 100:
+                    continue
+                try:
+                    raise ValueError('intentional')
+                except ValueError:
+                    continue
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, [])
-    self.assertTransformedEquivalent(f, [1])
-    self.assertTransformedEquivalent(f, [2])
-    self.assertTransformedEquivalent(f, [1, 2, 3])
+        self.assertTransformedEquivalent(f, [])
+        self.assertTransformedEquivalent(f, [1])
+        self.assertTransformedEquivalent(f, [2])
+        self.assertTransformedEquivalent(f, [1, 2, 3])
 
-  def test_for_loop(self):
+    def test_for_loop(self):
 
-    def f(a):
-      v = []
-      for x in a:
-        x -= 1
-        if x % 2 == 0:
-          continue
-        v.append(x)
-      return v
+        def f(a):
+            v = []
+            for x in a:
+                x -= 1
+                if x % 2 == 0:
+                    continue
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, [])
-    self.assertTransformedEquivalent(f, [1])
-    self.assertTransformedEquivalent(f, [2])
-    self.assertTransformedEquivalent(f, [1, 2, 3])
+        self.assertTransformedEquivalent(f, [])
+        self.assertTransformedEquivalent(f, [1])
+        self.assertTransformedEquivalent(f, [2])
+        self.assertTransformedEquivalent(f, [1, 2, 3])
 
-  def test_nested_with(self):
+    def test_nested_with(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        with ops.name_scope(''):
-          if x % 2 == 0:
-            continue
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                with ops.name_scope(''):
+                    if x % 2 == 0:
+                        continue
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_nested_multiple_withs(self):
+    def test_nested_multiple_withs(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        with ops.name_scope(''):
-          if x % 2 == 0:
-            continue
-        with ops.name_scope(''):
-          v.append(x)
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                with ops.name_scope(''):
+                    if x % 2 == 0:
+                        continue
+                with ops.name_scope(''):
+                    v.append(x)
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_nested_multiple_withs_and_statements(self):
+    def test_nested_multiple_withs_and_statements(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        with ops.name_scope(''):
-          if x % 2 == 0:
-            continue
-          v.append(x)
-        v.append(x)
-        with ops.name_scope(''):
-          v.append(x)
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                with ops.name_scope(''):
+                    if x % 2 == 0:
+                        continue
+                    v.append(x)
+                v.append(x)
+                with ops.name_scope(''):
+                    v.append(x)
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_nested_multiple_withs_and_nested_withs(self):
+    def test_nested_multiple_withs_and_nested_withs(self):
 
-    def f(x):
-      v = []
-      while x > 0:
-        x -= 1
-        with ops.name_scope(''):
-          if x % 2 == 0:
-            continue
-          with ops.name_scope(''):
-            v.append(x)
-        v.append(x)
-        with ops.name_scope(''):
-          v.append(x)
-        v.append(x)
-      return v
+        def f(x):
+            v = []
+            while x > 0:
+                x -= 1
+                with ops.name_scope(''):
+                    if x % 2 == 0:
+                        continue
+                    with ops.name_scope(''):
+                        v.append(x)
+                v.append(x)
+                with ops.name_scope(''):
+                    v.append(x)
+                v.append(x)
+            return v
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_nested(self):
+    def test_nested(self):
 
-    def f(x):
-      v = []
-      u = []
-      w = []
-      while x > 0:
-        x -= 1
-        if x % 2 == 0:
-          if x % 3 != 0:
-            u.append(x)
-          else:
-            w.append(x)
-            continue
-        v.append(x)
-      return v, u, w
+        def f(x):
+            v = []
+            u = []
+            w = []
+            while x > 0:
+                x -= 1
+                if x % 2 == 0:
+                    if x % 3 != 0:
+                        u.append(x)
+                    else:
+                        w.append(x)
+                        continue
+                v.append(x)
+            return v, u, w
 
-    self.assertTransformedEquivalent(f, 0)
-    self.assertTransformedEquivalent(f, 1)
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 4)
+        self.assertTransformedEquivalent(f, 0)
+        self.assertTransformedEquivalent(f, 1)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 4)
 
-  def test_multiple_guarded_continues_with_side_effects(self):
+    def test_multiple_guarded_continues_with_side_effects(self):
 
-    def f(x):
-      def track(u, x):
-        u.append(x)
-        return x
+        def f(x):
+            def track(u, x):
+                u.append(x)
+                return x
 
-      u = []
-      v = []
-      while x > 0:
-        x -= 1
-        if track(u, x) > 1:
-          continue
-        if track(u, x) > 2:
-          continue
-        v.append(x)
-      return u, v
+            u = []
+            v = []
+            while x > 0:
+                x -= 1
+                if track(u, x) > 1:
+                    continue
+                if track(u, x) > 2:
+                    continue
+                v.append(x)
+            return u, v
 
-    self.assertTransformedEquivalent(f, 3)
-    self.assertTransformedEquivalent(f, 2)
+        self.assertTransformedEquivalent(f, 3)
+        self.assertTransformedEquivalent(f, 2)
 
 
 if __name__ == '__main__':
-  test.main()
+    test.main()
