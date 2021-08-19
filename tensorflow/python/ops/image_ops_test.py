7d9c7679264c998afe6c9ef3873e5752e2b4454a
Duncan Riach <33532941+duncanriach@users.noreply.github.com>
2021-08-19 14:01:22 -0700

    PR #51140: [determinism] Add unimplemented exception to tf.image.adjust_contrast
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/51140
    
    This current PR adds and tests determinism-unimplemented exception-throwing for `tf.image.adjust_contrast`.
    
    This PR is related to [RFC: Enabling Determinism in TensorFlow](https://github.com/tensorflow/community/blob/master/rfcs/20210119-determinism.md). For status and history of GPU-determinism for this op, see [here](https://github.com/NVIDIA/framework-determinism/blob/master/tensorflow_status.md#adjust-contrast).
    
    CC @reedwm, @sanjoy, @nluehr
    Copybara import of the project:
    
    --
    e943d70a54aa19aae98d4f5fa26303e1138c4b10 by Duncan Riach <duncan@nvidia.com>:
    
    [determinism] Add unimplemented exception to tf.image.adjust_contrast
    
    --
    e6e5a1e690a357a3c894c5ec3dc633d52e26625a by Duncan Riach <duncan@nvidia.com>:
    
    [determinism] Address feedback (round one) on PR 51140
    
    --
    daaabb395444c2115c332bec50ed28f28492a4e6 by Duncan Riach <duncan@nvidia.com>:
    
    [determinism] Improve comment in test
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/51140 from duncanriach:adjust-contrast-d9m-unimplemented-exception daaabb395444c2115c332bec50ed28f28492a4e6
    PiperOrigin-RevId: 391838977
    Change-Id: I46bc40e72415ee09649733edf2afb7046e01f4b5

--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import colorsys
+import contextlib
 import functools
 import itertools
 import math
@@ -37,6 +38,7 @@ from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.eager import backprop
 from tensorflow.python.eager import context
 from tensorflow.python.eager import def_function
+from tensorflow.python.framework import config as tf_config
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
@@ -1591,6 +1593,43 @@ class AdjustContrastTest(test_util.TensorFlowTestCase):
                                 "contrast_factor must be scalar|"
                                 "Shape must be rank 0 but is rank 1"):
       image_ops.adjust_contrast(x_np, [2.0])
+
+  @test_util.run_all_in_graph_and_eager_modes
+  def testDeterminismUnimplementedExceptionThrowing(self):
+    """Test d9m-unimplemented exception-throwing when op-determinism is enabled.
+
+    This test depends upon other tests, tests which do not enable
+    op-determinism, to ensure that determinism-unimplemented exceptions are not
+    erroneously thrown when op-determinism is not enabled.
+    """
+    if test_util.is_xla_enabled():
+      self.skipTest('XLA implementation does not raise exception')
+    with self.session(), test_util.deterministic_ops():
+      input_shape = (1, 2, 2, 1)
+      on_gpu = len(tf_config.list_physical_devices("GPU"))
+      # AdjustContrast seems to now be inaccessible via the Python API.
+      # AdjustContrastv2 only supports float16 and float32 on GPU, and other
+      # types are converted to and from float32 at the Python level before
+      # AdjustContrastv2 is called.
+      dtypes_to_test = [
+          dtypes.uint8, dtypes.int8, dtypes.int16, dtypes.int32, dtypes.float32,
+          dtypes.float64
+      ]
+      if on_gpu:
+        dtypes_to_test.append(dtypes.float16)
+        ctx_mgr = self.assertRaisesRegex(
+            errors.UnimplementedError,
+            "A deterministic GPU implementation of AdjustContrastv2 is not" +
+            " currently available.")
+      else:
+        ctx_mgr = contextlib.suppress()
+      for dtype in dtypes_to_test:
+        input_images = array_ops.zeros(input_shape, dtype=dtype)
+        contrast_factor = 1.
+        with ctx_mgr:
+          output_images = image_ops.adjust_contrast(input_images,
+                                                    contrast_factor)
+          self.evaluate(output_images)
 
 
 class AdjustBrightnessTest(test_util.TensorFlowTestCase):
