3994a7305c8afc3c7342b9733a3e99fc3da900f3
Taehee Jeong <taeheej@google.com>
2021-08-21 11:05:28 -0700

    Add bias adjustments when the scale for the bias would cause overflow.
    In full-integer quantization, bias_scale is determined as input_scale * filter_scale. When the two dependent scales are too small, the INT32 quantized bias values will overflow and break the model. This change is to introduce a logic implemented in TOCO quantizer to MLIR's QuantizationDriver.
    
    Deleted temporary patch that used fixed threshold for this adjustment.
    
    PiperOrigin-RevId: 392189936
    Change-Id: I01489c587a554f13e76c16b9ef8fb393cfd8498d

--- tensorflow/lite/python/lite_v2_test.py
+++ tensorflow/lite/python/lite_v2_test.py
@@ -964,6 +964,37 @@ class FromConcreteFunctionTest(lite_v2_test_util.ModelTest):
     self.assertLen(quant_params['scales'], expected_num_params)
     self.assertLen(quant_params['zero_points'], expected_num_params)
 
+  @parameterized.named_parameters(('MlirQuantize', True),
+                                  ('TocoQuantize', False))
+  @test_util.run_v2_only
+  def testQuantizeBiasOverflow(self, enable_mlir_quantizer):
+    """Tests if the quantizer handles bias overflow by adjusting scales."""
+    input_data = np.array([[-1e-3, 1e-3]], dtype=np.float32)
+
+    def calibration_gen():
+      yield {'x': input_data}
+
+    root = self._getMatMulModelWithSmallWeights()
+    input_data = tf.constant([-1e-3, 1e-3], shape=(1, 2))
+    concrete_func = root.matmul.get_concrete_function(input_data)
+    converter = lite.TFLiteConverterV2.from_concrete_functions([concrete_func],
+                                                               root)
+    converter.optimizations = [lite.Optimize.DEFAULT]
+    converter.representative_dataset = calibration_gen
+    converter.experimental_new_quantizer = enable_mlir_quantizer
+    quantized_model = converter.convert()
+
+    interpreter = Interpreter(model_content=quantized_model)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_details = interpreter.get_output_details()
+    output = interpreter.get_tensor(output_details[0]['index'])
+    # the inputs and weights are far smaller than the biases, so the final
+    # result should be equal to the biases.
+    self.assertAllClose(root.bias, output.flatten())
+
   @test_util.run_v2_only
   def testOpVersion(self):
     @tf.function(
