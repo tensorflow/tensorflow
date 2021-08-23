7ab93b37ed9a972c5af27952977e9d435384131c
Edward Loper <edloper@google.com>
2021-08-23 09:10:46 -0700

    Type-based dispatch system for Python APIs.
    PiperOrigin-RevId: 392446267
    Change-Id: Id717849e2a47b4d2f79f3cbcb7903dd2f9465152

--- tensorflow/python/framework/python_op_gen.cc
+++ tensorflow/python/framework/python_op_gen.cc
@@ -173,7 +173,8 @@ class GenEagerPythonOp : public python_op_gen_internal::GenPythonOp {
   void AddEagerAttrs(const string& indentation);
   void AddEagerExecute(const string& indentation,
                        const string& num_outputs_expr);
-  void AddDispatch(const string& prefix);
+  void AddFallbackDispatch(const string& prefix);
+  void AddTypeBasedDispatch(const string& prefix);
 
   void AddRawOpExport(const string& parameters);
 
@@ -525,6 +526,10 @@ void GenEagerPythonOp::AddReturnTypeAnnotation(
 
 void GenEagerPythonOp::HandleGraphMode(
     const string& function_setup, const std::vector<string>& output_sizes) {
+  if (api_def_.visibility() == ApiDef::VISIBLE) {
+    strings::StrAppend(&result_, "  else:\n");
+    AddTypeBasedDispatch("    ");
+  }
   strings::StrAppend(&result_, "  # Add nodes to the TensorFlow graph.\n");
   strings::StrAppend(&result_, function_setup);
   if (api_def_.visibility() == ApiDef::VISIBLE) {
@@ -533,7 +538,7 @@ void GenEagerPythonOp::HandleGraphMode(
   strings::StrAppend(
       &result_, "  _, _, _op, _outputs = _op_def_library._apply_op_helper(\n");
   AddBodyNoReturn(strings::StrCat("        \"", op_def_.name(), "\", "));
-  AddDispatch("  ");
+  AddFallbackDispatch("  ");
 
   if (num_outs_ > 0) {
     strings::StrAppend(&result_, "  _result = _outputs[:]\n");
@@ -851,7 +856,8 @@ bool GenEagerPythonOp::AddEagerFastPathAndGraphCode(
     GenerateTypeVars(type_annotations);
   }
   if (api_def_.visibility() == ApiDef::VISIBLE) {
-    strings::StrAppend(&result_, "@_dispatch.add_dispatch_list\n");
+    strings::StrAppend(&result_, "@_dispatch.add_fallback_dispatch_list\n");
+    strings::StrAppend(&result_, "@_dispatch.add_type_based_api_dispatcher\n");
   }
 
   AddExport();
@@ -982,6 +988,7 @@ void GenEagerPythonOp::AddEagerFastPathExecute() {
   strings::StrAppend(&result_, "    ", "except _core._FallbackException:\n");
   strings::StrAppend(&result_, "      pass\n");
   strings::StrAppend(&result_, "    try:\n");
+  AddTypeBasedDispatch("      ");
   strings::StrAppend(
       &result_, "      ", "return ", function_name_, kEagerFallbackSuffix,
       "(\n",
@@ -991,7 +998,7 @@ void GenEagerPythonOp::AddEagerFastPathExecute() {
   strings::StrAppend(&result_, "    except _core._SymbolicException:\n");
   strings::StrAppend(&result_,
                      "      pass  # Add nodes to the TensorFlow graph.\n");
-  AddDispatch("    ");
+  AddFallbackDispatch("    ");
 }
 
 void GenEagerPythonOp::AddEagerInferredAttrs(const string& indentation) {
@@ -1129,20 +1136,36 @@ void GenEagerPythonOp::AddEagerExecute(const string& indentation,
                      WordWrap(return_prefix, return_args, kRightMargin), "\n");
 }
 
-void GenEagerPythonOp::AddDispatch(const string& prefix) {
+void GenEagerPythonOp::AddFallbackDispatch(const string& prefix) {
   if (api_def_.visibility() != ApiDef::VISIBLE) return;
 
   strings::StrAppend(&result_, prefix, "except (TypeError, ValueError):\n");
-  strings::StrAppend(&result_, prefix, "  result = _dispatch.dispatch(\n");
+  strings::StrAppend(&result_, prefix, "  _result = _dispatch.dispatch(\n");
   AddBodyNoReturn(strings::StrCat(prefix, "        ", function_name_,
                                   ", "
                                   "(), dict("));
   strings::StrAppend(&result_, prefix, "      )\n");
   strings::StrAppend(&result_, prefix,
-                     "  if result is not "
+                     "  if _result is not "
                      "_dispatch.OpDispatcher.NOT_SUPPORTED:\n");
-  strings::StrAppend(&result_, prefix, "    return result\n");
+  strings::StrAppend(&result_, prefix, "    return _result\n");
   strings::StrAppend(&result_, prefix, "  raise\n");
+}
+
+void GenEagerPythonOp::AddTypeBasedDispatch(const string& prefix) {
+  if (api_def_.visibility() != ApiDef::VISIBLE) return;
+  std::string args("(");
+  for (const auto& name : param_names_) {
+    strings::StrAppend(&args, name.GetRenameTo(), ", ");
+  }
+  strings::StrAppend(&args, "name,), None");
+
+  strings::StrAppend(
+      &result_, prefix, "_result = ", function_name_,
+      "._tf_type_based_dispatcher.Dispatch(\n",
+      WordWrap(strings::StrCat(prefix, "    "), args, kRightMargin), ")\n");
+  strings::StrAppend(&result_, prefix, "if _result is not NotImplemented:\n",
+                     prefix, "  return _result\n");
 }
 
 void GenEagerPythonOp::AddRawOpExport(const string& parameters) {
