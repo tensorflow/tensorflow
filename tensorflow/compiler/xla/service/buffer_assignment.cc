d244a1d7c64df780f001acfcd960a14d800f7957
Ce Zheng <zce@google.com>
2021-08-23 13:35:16 -0700

    [XLA] Avoid user hash map lookup when buffer value is not kCopy when reusing an allocation.
    PiperOrigin-RevId: 392508211
    Change-Id: Ia476ecd2f1f9cf727996099b2a338d316623edb3

--- tensorflow/compiler/xla/service/buffer_assignment.cc
+++ tensorflow/compiler/xla/service/buffer_assignment.cc
@@ -1052,16 +1052,17 @@ bool BufferAssigner::MaybeAssignBuffer(BufferAllocation* allocation,
         return false;
       }
 
-      for (const HloPosition& assigned_buffer_position :
-           assigned_buffer.positions()) {
-        // Copy instruction don't share a buffer with their input operand.
-        if (new_value->instruction()->IsUserOf(
-                assigned_buffer_position.instruction) &&
-            new_value->instruction()->opcode() == HloOpcode::kCopy) {
-          VLOG(4) << "Can't assign: assignee " << assigned_buffer
-                  << " is used at copy instruction "
-                  << new_value->ToShortString();
-          return false;
+      // Copy instruction don't share a buffer with their input operand.
+      if (new_value->instruction()->opcode() == HloOpcode::kCopy) {
+        for (const HloPosition& assigned_buffer_position :
+             assigned_buffer.positions()) {
+          if (new_value->instruction()->IsUserOf(
+                  assigned_buffer_position.instruction)) {
+            VLOG(4) << "Can't assign: assignee " << assigned_buffer
+                    << " is used at copy instruction "
+                    << new_value->ToShortString();
+            return false;
+          }
         }
       }
     }
