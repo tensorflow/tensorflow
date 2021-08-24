1fe2cab11d5c2487146d8085fbdd56f1a36ac28b
A. Unique TensorFlower <gardener@tensorflow.org>
2021-08-24 16:13:07 -0700

    Use BuildTensorShapeBase when parsing unverified TensorShapes during checkpoint loading.
    This avoids crashing when the TensorShape has negative dimensions.
    
    PiperOrigin-RevId: 392769882
    Change-Id: Id1f7ae7fcf8142193556af47abfda81b13d3cce4

--- tensorflow/core/util/tensor_slice_reader_test.cc
+++ tensorflow/core/util/tensor_slice_reader_test.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "tensorflow/core/framework/tensor_shape.pb.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/framework/versions.pb.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
@@ -408,6 +409,31 @@ TEST(TensorSliceReaderTest, UnsupportedTensorType) {
   EXPECT_TRUE(reader.HasTensor("test", nullptr, nullptr));
   std::unique_ptr<Tensor> tensor;
   EXPECT_FALSE(reader.GetTensor("test", &tensor).ok());
+}
+
+TEST(TensorSliceReaderTest, NegativeTensorShapeDimension) {
+  const string fname =
+      io::JoinPath(testing::TmpDir(), "negative_dim_checkpoint");
+  TensorSliceWriter writer(fname, CreateTableTensorSliceBuilder);
+  const int32 data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  TF_CHECK_OK(writer.Add("test", TensorShape({4, 5}),
+                         TensorSlice::ParseOrDie("0,2:-"), data));
+  TF_CHECK_OK(writer.Finish());
+
+  MutateSavedTensorSlices(fname, [](SavedTensorSlices sts) {
+    if (sts.has_meta()) {
+      for (auto& tensor : *sts.mutable_meta()->mutable_tensor()) {
+        for (auto& dim : *tensor.mutable_shape()->mutable_dim()) {
+          dim.set_size(-dim.size());
+        }
+      }
+    }
+    return sts.SerializeAsString();
+  });
+
+  TensorSliceReader reader(fname, OpenTableTensorSliceReader);
+  // The negative dimension should cause loading to fail.
+  EXPECT_FALSE(reader.status().ok());
 }
 
 void CachedTensorSliceReaderTesterHelper(
