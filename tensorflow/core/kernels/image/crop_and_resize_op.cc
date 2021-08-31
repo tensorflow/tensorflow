5aceeafd724e42090645d9eeb8bbc329d0774ed6
Yong Tang <yong.tang.github@outlook.com>
2021-08-31 16:23:54 -0700

    PR #51732: Fix crash of tf.image.crop_and_resize when input is large number
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/51732
    
    This PR is part of the effort in #46890 where
    tf.image.crop_and_resize will crash if shape consists of large number.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    Copybara import of the project:
    
    --
    c8d87055a56d8740d27ad8bdc74a7459ede6900e by Yong Tang <yong.tang.github@outlook.com>:
    
    Fix crash of tf.image.crop_and_resize when input is large number
    
    This PR is part of the effort in 46890 where
    tf.image.crop_and_resize will crash if shape consists of large number.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/51732 from yongtang:46890-tf.image.crop_and_resize c8d87055a56d8740d27ad8bdc74a7459ede6900e
    PiperOrigin-RevId: 394109830
    Change-Id: If049dad0844df9353722029ee95bc76819eda1f4

--- tensorflow/core/kernels/image/crop_and_resize_op.cc
+++ tensorflow/core/kernels/image/crop_and_resize_op.cc
@@ -169,14 +169,15 @@ class CropAndResizeOp : public AsyncOpKernel {
         context, crop_height > 0 && crop_width > 0,
         errors::InvalidArgument("crop dimensions must be positive"), done);
 
+    TensorShape shape;
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(num_boxes), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(crop_height), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(crop_width), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(depth), done);
     // Allocate output tensor.
     Tensor* output = nullptr;
-    OP_REQUIRES_OK_ASYNC(
-        context,
-        context->allocate_output(
-            0, TensorShape({num_boxes, crop_height, crop_width, depth}),
-            &output),
-        done);
+    OP_REQUIRES_OK_ASYNC(context, context->allocate_output(0, shape, &output),
+                         done);
 
     auto compute_callback = [this, context, output]() {
       const Tensor& image = context->input(0);
@@ -407,14 +408,25 @@ class CropAndResizeGradImageOp : public AsyncOpKernel {
         context, grads.dim_size(3) == depth,
         errors::InvalidArgument("image_size and grads are incompatible"), done);
 
+
+    if (std::is_same<Device, GPUDevice>::value) {
+      OP_REQUIRES_ASYNC(
+          context, !OpDeterminismRequired(),
+          errors::Unimplemented(
+              "Deterministic GPU implementation of CropAndResizeBackpropImage"
+              " not available."),
+          done);
+    }
+
+    TensorShape shape;
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(batch_size), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(image_height), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(image_width), done);
+    OP_REQUIRES_OK_ASYNC(context, shape.AddDimWithStatus(depth), done);
     // Allocate output tensor.
     Tensor* output = nullptr;
-    OP_REQUIRES_OK_ASYNC(
-        context,
-        context->allocate_output(
-            0, TensorShape({batch_size, image_height, image_width, depth}),
-            &output),
-        done);
+    OP_REQUIRES_OK_ASYNC(context, context->allocate_output(0, shape, &output),
+                         done);
 
     auto compute_callback = [this, context, output]() {
       const Tensor& grads = context->input(0);
