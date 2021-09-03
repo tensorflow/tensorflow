85459f1604c3ae25adfd060cdde10a8a2acec095
Annie Tallund <annie.tallund@arm.com>
2021-09-03 08:50:56 +0200

    Add #include for headers used
    Change-Id: Ife2aca76791cd8e673ab4aeb09ee82e526607cdb

--- tensorflow/lite/kernels/internal/reference/slice.h
+++ tensorflow/lite/kernels/internal/reference/slice.h
@@ -15,6 +15,9 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_KERNELS_INTERNAL_REFERENCE_SLICE_H_
 #define TENSORFLOW_LITE_KERNELS_INTERNAL_REFERENCE_SLICE_H_
 
+#include "tensorflow/lite/kernels/internal/tensor.h"
+#include "tensorflow/lite/kernels/internal/types.h"
+
 namespace tflite {
 
 namespace reference_ops {
