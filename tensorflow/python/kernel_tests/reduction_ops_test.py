11fd4adcd8aca1b8e37ddc61415daeece95832db
A. Unique TensorFlower <gardener@tensorflow.org>
2021-09-10 20:40:10 -0700

    Internal change
    PiperOrigin-RevId: 396058687
    Change-Id: I44e7a4aa5201672112ab35a82fd33c13556981db

--- tensorflow/python/kernel_tests/reduction_ops_test.py
+++ tensorflow/python/kernel_tests/reduction_ops_test.py
@@ -762,7 +762,6 @@ class MinReductionTest(test.TestCase):
         tf_v = self.evaluate(v)
       self.assertAllEqual(tf_v, 0)
 
-  @test_util.disable_xla("b/168718272")  # XLA handling of NaN is inconsistent
   def testSpecialValues(self):
     for dtype in [np.float32, np.float64]:
       for size in range(1, 4):
@@ -879,7 +878,6 @@ class MaxReductionTest(test.TestCase):
         tf_v = self.evaluate(v)
       self.assertAllEqual(tf_v, 0)
 
-  @test_util.disable_xla("b/168718272")  # XLA handling of NaN is inconsistent
   def testSpecialValues(self):
     for dtype in [np.float32, np.float64]:
       for size in range(1, 4):
