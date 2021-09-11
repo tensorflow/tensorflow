91b3f965a461857eec788f16ece7235ff8341bbe
Yunxing Dai <yunxing@google.com>
2021-09-10 17:27:33 -0700

    Unifies the implementation of UniqueV1 and UniqueV2
    UniqueV2's implementation can conver UniqueV1, it's also faster by replacing the O(n^2) while loop with a O(n) scan and a sort.
    
    PiperOrigin-RevId: 396039981
    Change-Id: Ie21e5f32d07363efbfd51a97dfabe5b5a74579f9

--- tensorflow/compiler/tf2xla/kernels/unique_op.cc
+++ tensorflow/compiler/tf2xla/kernels/unique_op.cc
@@ -44,231 +44,18 @@ limitations under the License.
 namespace tensorflow {
 namespace {
 
-class UniqueOp : public XlaOpKernel {
+class UniqueOpBase : public XlaOpKernel {
  public:
-  explicit UniqueOp(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {}
-
-  // We use a two level loop algorithm to calculate unique.
-  //
-  // i = 0
-  // output_size = 0
-  // output_indices = broadcast(0, {input_size})
-  // while (i < input_size) {
-  //   search_result_index = output_size
-  //   j = 0
-  //   while (j < output_size) {
-  //     if(input[j]==input[i]) {
-  //       search_result_index = j
-  //     }
-  //     ++j
-  //   }
-  //   input[search_result_index] = input[i]
-  //   output_indices[i] = search_result_index
-  //   if (search_result_index == output_size) {
-  //     // Not found
-  //     output_size ++;
-  //   }
-  //   i ++;
-  // }
-  //
-  // The algorithm is then functionalized into xla whiles.  Outer-scoped
-  // variables are captured as inputs and outputs to the while loop.
-  // Conditionals are rewritten into xla select for simplicity.
-  xla::XlaComputation BuildInnerLoopCond(XlaOpKernelContext* ctx,
-                                         xla::Shape inner_loop_shape) {
-    std::unique_ptr<xla::XlaBuilder> builder =
-        ctx->builder()->CreateSubBuilder("inner_loop_cond");
-    auto param = xla::Parameter(builder.get(), 0, inner_loop_shape, "param");
-    auto j = xla::GetTupleElement(param, 2);
-    auto output_element_size = xla::GetTupleElement(param, 3);
-    xla::Lt(j, output_element_size);
-    return builder->Build().ConsumeValueOrDie();
+  explicit UniqueOpBase(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {
+    DataType dtype;
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("out_idx", &dtype));
+    OP_REQUIRES_OK(ctx, DataTypeToPrimitiveType(dtype, &idx_type_));
   }
-
-  xla::XlaComputation BuildInnerLoopBody(XlaOpKernelContext* ctx,
-                                         xla::Shape inner_loop_shape,
-                                         xla::Shape single_element_shape) {
-    std::unique_ptr<xla::XlaBuilder> builder =
-        ctx->builder()->CreateSubBuilder("inner_loop_body");
-    auto param = xla::Parameter(builder.get(), 0, inner_loop_shape, "param");
-    auto input = xla::GetTupleElement(param, 0);
-    auto target = xla::GetTupleElement(param, 1);
-    auto j = xla::GetTupleElement(param, 2);
-    auto output_element_size = xla::GetTupleElement(param, 3);
-    auto output_index = xla::GetTupleElement(param, 4);
-    auto input_elem = xla::DynamicSlice(input, {j}, {1});
-    auto input_elem_scalar = xla::Reshape(single_element_shape, input_elem);
-    auto eq = xla::Eq(input_elem_scalar, target);
-    auto select = xla::Select(eq, j, output_index);
-    auto next_j = xla::Add(j, xla::One(builder.get(), xla::S32));
-    xla::Tuple(builder.get(),
-               {input, target, next_j, output_element_size, select});
-    return builder->Build().ConsumeValueOrDie();
-  }
-
-  xla::XlaComputation BuildOuterLoopCond(XlaOpKernelContext* ctx,
-                                         xla::Shape outer_loop_shape,
-                                         int64_t list_size) {
-    std::unique_ptr<xla::XlaBuilder> builder =
-        ctx->builder()->CreateSubBuilder("outer_loop_body");
-    auto param =
-        xla::Parameter(builder.get(), 0, outer_loop_shape, "outer_loop_param");
-    auto i = xla::GetTupleElement(param, 2);
-    auto bound = xla::ConstantR0<int32>(builder.get(), list_size);
-    xla::Lt(i, bound);
-    return builder->Build().ConsumeValueOrDie();
-  }
-
-  xla::XlaComputation BuildOuterLoopBody(
-      XlaOpKernelContext* ctx, xla::Shape outer_loop_shape,
-      xla::Shape single_element_shape, const xla::XlaComputation& inner_cond,
-      const xla::XlaComputation& inner_body) {
-    std::unique_ptr<xla::XlaBuilder> builder =
-        ctx->builder()->CreateSubBuilder("outer_loop_body");
-    auto param = xla::Parameter(builder.get(), 0, outer_loop_shape, "param");
-    auto input = xla::GetTupleElement(param, 0);
-    auto indices = xla::GetTupleElement(param, 1);
-    auto i = xla::GetTupleElement(param, 2);
-    auto output_element_size = xla::GetTupleElement(param, 3);
-    auto zero = xla::Zero(builder.get(), xla::S32);
-    auto target = xla::DynamicSlice(input, {i}, {1});
-    auto target_scalar = xla::Reshape(single_element_shape, target);
-    auto inner_loop_param = xla::Tuple(
-        builder.get(),
-        {input, target_scalar, zero, output_element_size, output_element_size});
-    auto inner_loop = xla::While(inner_cond, inner_body, inner_loop_param);
-    auto output_index = xla::GetTupleElement(inner_loop, 4);
-    auto one = xla::One(builder.get(), xla::S32);
-    auto update_output_element_size =
-        xla::Select(xla::Eq(output_index, output_element_size),
-                    xla::Add(output_element_size, one), output_element_size);
-    auto update_input = xla::DynamicUpdateSlice(input, target, {output_index});
-    auto update_indices =
-        xla::DynamicUpdateSlice(indices, xla::Reshape(output_index, {1}), {i});
-    xla::Tuple(builder.get(), {update_input, update_indices, xla::Add(i, one),
-                               update_output_element_size});
-    return builder->Build().ConsumeValueOrDie();
-  }
-
-  xla::XlaOp DataOutputFastPath(XlaOpKernelContext* ctx, xla::XlaOp input,
-                                const xla::Shape& input_shape) {
-    // Generate data output using a sub-quadratic sorting algorithm. If only the
-    // data output is used (meaning the indices output is ignored, which is
-    // common), DCE will only keep the fastpath.
-    auto iota_shape = input_shape;
-    iota_shape.set_element_type(xla::S32);
-    int64_t input_count = input_shape.dimensions(0);
-    xla::XlaOp iota = xla::Iota(ctx->builder(), iota_shape, 0);
-    std::vector<xla::XlaOp> to_sort = {input, iota};
-    std::vector<xla::PrimitiveType> types_to_sort = {input_shape.element_type(),
-                                                     xla::S32};
-    xla::XlaOp sorted = xla::Sort(
-        to_sort, xla::CreateScalarLtComputation(types_to_sort, ctx->builder()),
-        /*dimension=*/0,
-        /*is_stable=*/true);
-    xla::XlaOp sorted_data = xla::GetTupleElement(sorted, 0);
-    xla::XlaOp sorted_iota = xla::GetTupleElement(sorted, 1);
-    // Calculate unique_indices, unique_indices[i] is true when sorted_data[i]
-    // != sorted_data[i - 1]. unique_indices[0] is always true.
-    // We do this by shifting sorted_data by 1 position and then compare it with
-    // itself.
-
-    // A[0:n-1]
-    auto shifted = xla::SliceInDim(sorted_data, 0, input_count - 1,
-                                   /*stride=*/1, /*dimno=*/0);
-
-    auto zero =
-        xla::Zeros(ctx->builder(),
-                   xla::ShapeUtil::MakeShape(input_shape.element_type(), {1}));
-    // shifted = concat(0, A[0:n-1]),
-    shifted = xla::ConcatInDim(ctx->builder(), {zero, shifted}, 0);
-    xla::XlaOp unique_indices = xla::Ne(shifted, sorted_data);
-    xla::XlaOp true_r1 = xla::One(ctx->builder(), xla::PRED);
-    true_r1 = xla::Reshape(true_r1, {1});
-    // First element is always unique(true).
-    unique_indices = xla::DynamicUpdateSlice(
-        unique_indices, true_r1, {xla::Zero(ctx->builder(), xla::S32)});
-    unique_indices = xla::ConvertElementType(unique_indices, xla::S32);
-
-    // Do a reverse sort using iota as key, this moves `changed` to its original
-    // positions in input space.
-    std::vector<xla::XlaOp> to_sort_reverse = {sorted_iota, unique_indices};
-    std::vector<xla::PrimitiveType> types_to_sort_reverse = {xla::S32,
-                                                             xla::S32};
-    xla::XlaOp sort_reverse = xla::Sort(
-        to_sort_reverse,
-        xla::CreateScalarLtComputation(types_to_sort_reverse, ctx->builder()),
-        /*dimension=*/0,
-        /*is_stable=*/true);
-    // Now unique_indices are scattered back to original positions.
-    unique_indices = xla::GetTupleElement(sort_reverse, 1);
-    // Do a final sort to move unique items together.
-    std::vector<xla::XlaOp> to_sort_final = {unique_indices, input};
-    std::vector<xla::PrimitiveType> types_to_sort_final = {
-        xla::S32, input_shape.element_type()};
-    xla::XlaOp final_sort = xla::Sort(
-        to_sort_final,
-        xla::CreateScalarGtComputation(types_to_sort_final, ctx->builder()),
-        /*dimension=*/0,
-        /*is_stable=*/true);
-    xla::XlaOp output = xla::GetTupleElement(final_sort, 1);
-    xla::XlaOp unique_count = xla::ReduceAll(
-        unique_indices, xla::Zero(ctx->builder(), xla::S32),
-        xla::CreateScalarAddComputation(xla::S32, ctx->builder()));
-    xla::XlaOp output_dynamic = xla::SetDimensionSize(output, unique_count, 0);
-    return output_dynamic;
-  }
-
-  void Compile(XlaOpKernelContext* ctx) override {
-    xla::XlaOp input = ctx->Input(0);
-    StatusOr<xla::Shape> input_shape_or = ctx->builder()->GetShape(input);
-    OP_REQUIRES_OK(ctx, input_shape_or.status());
-    auto input_shape = input_shape_or.ValueOrDie();
-    ctx->SetOutput(0, DataOutputFastPath(ctx, input, input_shape));
-
-    // Slow path to generate indices.
-    xla::Shape single_index_shape = xla::ShapeUtil::MakeScalarShape(xla::S32);
-    xla::Shape single_element_shape =
-        xla::ShapeUtil::MakeScalarShape(input_shape.element_type());
-    OP_REQUIRES(ctx, input_shape.rank() == 1,
-                xla::InvalidArgument("Input to UniqueOp must be rank-1: %s",
-                                     input_shape.ToString()));
-    int64_t list_size = input_shape.dimensions()[0];
-    auto indices_shape =
-        xla::ShapeUtil::ChangeElementType(input_shape, xla::S32);
-    auto outer_loop_shape = xla::ShapeUtil::MakeTupleShape(
-        {input_shape, indices_shape, single_index_shape, single_index_shape});
-    auto inner_loop_shape = xla::ShapeUtil::MakeTupleShape(
-        {input_shape, single_element_shape, single_index_shape,
-         single_index_shape, single_index_shape});
-    xla::XlaComputation inner_loop_cond =
-        BuildInnerLoopCond(ctx, inner_loop_shape);
-    xla::XlaComputation inner_loop_body =
-        BuildInnerLoopBody(ctx, inner_loop_shape, single_element_shape);
-    xla::XlaComputation outer_loop_cond =
-        BuildOuterLoopCond(ctx, outer_loop_shape, list_size);
-    xla::XlaComputation outer_loop_body =
-        BuildOuterLoopBody(ctx, outer_loop_shape, single_element_shape,
-                           inner_loop_cond, inner_loop_body);
-    auto zero = xla::Zero(ctx->builder(), xla::S32);
-    auto init_indices = xla::Broadcast(zero, {list_size});
-    auto init = xla::Tuple(ctx->builder(), {input, init_indices, zero, zero});
-    auto outer_while = xla::While(outer_loop_cond, outer_loop_body, init);
-    auto output_indices = xla::GetTupleElement(outer_while, 1);
-    ctx->SetOutput(1, output_indices);
-  }
-};
-
-REGISTER_XLA_OP(Name("Unique").Device(DEVICE_TPU_XLA_JIT), UniqueOp);
-
-class UniqueV2Op : public XlaOpKernel {
- public:
-  explicit UniqueV2Op(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {}
 
   // Transpose a tensor by moving axis `from` into `to`.
   xla::XlaOp MoveAxis(xla::XlaOp a, int64_t from, int64_t to,
                       const xla::Shape& input_shape) {
-    std::vector<int64> permutation;
+    std::vector<int64_t> permutation;
     permutation.reserve(input_shape.rank());
     for (int64_t i = 0; i < input_shape.rank(); ++i) {
       permutation.push_back(i);
@@ -294,7 +81,7 @@ class UniqueV2Op : public XlaOpKernel {
   //
   // Requires mask[0] = 1, meaning that accum[i - 1] will never be accessed.
   //
-  // This is implemented an hlo while loop.
+  // This is implemented as an hlo while loop.
   xla::XlaOp RollingSelectR1(XlaOpKernelContext* ctx, xla::XlaOp data,
                              xla::XlaOp mask, int64_t size) {
     xla::XlaComputation cond, body;
@@ -354,22 +141,12 @@ class UniqueV2Op : public XlaOpKernel {
     return xla::GetTupleElement(xla::While(cond, body, init), 3);
   }
 
-  void Compile(XlaOpKernelContext* ctx) override {
+  void CompileWithAxis(XlaOpKernelContext* ctx, int64_t axis) {
     xla::XlaOp input = ctx->Input(0);
-    std::vector<int64_t> axis;
-    OP_REQUIRES_OK(ctx, ctx->ConstantInputAsIntVector(1, &axis));
-    OP_REQUIRES(
-        ctx, axis.size() <= 1,
-        xla::InvalidArgument("Only single axis unique op is supported"));
-    if (axis.empty()) {
-      axis_ = 0;
-    } else {
-      axis_ = axis.front();
-    }
     StatusOr<xla::Shape> input_shape_or = ctx->builder()->GetShape(input);
     OP_REQUIRES_OK(ctx, input_shape_or.status());
     auto input_shape = input_shape_or.ValueOrDie();
-    auto aux = MoveAxis(input, axis_, 0, input_shape);
+    auto aux = MoveAxis(input, axis, 0, input_shape);
     auto aux_shape = ctx->builder()->GetShape(aux).ValueOrDie();
     int64_t leading_size = aux_shape.dimensions(0);
     int64_t product = 1;
@@ -379,7 +156,7 @@ class UniqueV2Op : public XlaOpKernel {
     aux = xla::Reshape(aux, {leading_size, product});
     if (leading_size == 0) {
       auto result_data = xla::Reshape(aux, aux_shape.dimensions());
-      result_data = MoveAxis(result_data, 0, axis_, aux_shape);
+      result_data = MoveAxis(result_data, 0, axis, aux_shape);
       ctx->SetOutput(0, result_data);
       ctx->SetOutput(1, xla::Iota(ctx->builder(), xla::S32, leading_size));
       return;
@@ -452,8 +229,8 @@ class UniqueV2Op : public XlaOpKernel {
     auto mask_permute = xla::GetTupleElement(mask_sort, 1);
     permuted = xla::Gather(aux, mask_permute, gather_dim_numbers, {1, product});
     auto result_data = xla::Reshape(permuted, aux_shape.dimensions());
-    result_data = MoveAxis(result_data, 0, axis_, aux_shape);
-    result_data = xla::SetDimensionSize(result_data, dynamic_size, axis_);
+    result_data = MoveAxis(result_data, 0, axis, aux_shape);
+    result_data = xla::SetDimensionSize(result_data, dynamic_size, axis);
     ctx->SetOutput(0, result_data);
     auto imask = CumSumR1(ctx, mask, leading_size);
     imask = xla::Sub(imask, xla::One(ctx->builder(), xla::S32), {});
@@ -462,11 +239,48 @@ class UniqueV2Op : public XlaOpKernel {
                   xla::CreateScalarLtComputation({xla::S32, xla::S32},
                                                  ctx->builder())),
         1);
+    idx = xla::ConvertElementType(idx, idx_type_);
     ctx->SetOutput(1, idx);
   }
 
  private:
-  int64_t axis_;
+  xla::PrimitiveType idx_type_;
+};
+
+class UniqueOp : public UniqueOpBase {
+ public:
+  explicit UniqueOp(OpKernelConstruction* ctx) : UniqueOpBase(ctx) {}
+
+  void Compile(XlaOpKernelContext* ctx) override {
+    CompileWithAxis(ctx, /*axis=*/0);
+  }
+};
+
+// Only supports TPU for now as some tests (e.g.,
+// testMethodCompilationUnsupportedFunc) requires Unique to throw an
+// unimplemented error on XLA CPU & GPU.
+//
+// TODO(b/187993636): Enable this for CPU and GPU.
+REGISTER_XLA_OP(Name("Unique").Device(DEVICE_TPU_XLA_JIT), UniqueOp);
+
+class UniqueV2Op : public UniqueOpBase {
+ public:
+  explicit UniqueV2Op(OpKernelConstruction* ctx) : UniqueOpBase(ctx) {}
+
+  void Compile(XlaOpKernelContext* ctx) override {
+    std::vector<int64_t> axises;
+    OP_REQUIRES_OK(ctx, ctx->ConstantInputAsIntVector(1, &axises));
+    OP_REQUIRES(
+        ctx, axises.size() <= 1,
+        xla::InvalidArgument("Only single axis unique op is supported"));
+    int64_t axis;
+    if (axises.empty()) {
+      axis = 0;
+    } else {
+      axis = axises.front();
+    }
+    CompileWithAxis(ctx, /*axis=*/axis);
+  }
 };
 
 REGISTER_XLA_OP(Name("UniqueV2").CompileTimeConstantInput("axis"), UniqueV2Op);
