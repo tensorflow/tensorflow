e6880ca84ec66b82659f414870da9bc1d824609d
Tres Popp <tpopp@google.com>
2021-09-13 01:03:49 -0700

    [MLIR][KernelGen] Fix GPU module cache and make it a kernel resource
    This fixes a subtle bug in the GPU module caches, which assumed that no two GPU
    modules are loaded from the same memory location. When JIT-compiling kernels,
    h
    
    This is a rollforward of 2 reverted commits. The following commit has been squashed into this one:
    [MLIR][KernelGen] Share similar code between Cuda and Rocm error handling
    
    PiperOrigin-RevId: 396299583
    Change-Id: Ib2b00dca625ff5d4bbb1e3d4920bb2b49d54974b

--- tensorflow/compiler/mlir/tools/kernel_gen/tf_gpu_runtime_wrappers.h
+++ tensorflow/compiler/mlir/tools/kernel_gen/tf_gpu_runtime_wrappers.h
@@ -16,16 +16,50 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_MLIR_TOOLS_KERNEL_GEN_TF_GPU_RUNTIME_WRAPPERS_H_
 #define TENSORFLOW_COMPILER_MLIR_TOOLS_KERNEL_GEN_TF_GPU_RUNTIME_WRAPPERS_H_
 
+#include "absl/container/flat_hash_map.h"
 #include "mlir/ExecutionEngine/RunnerUtils.h"  // from @llvm-project
+#include "tensorflow/core/framework/resource_op_kernel.h"
+#include "tensorflow/core/platform/mutex.h"
 
-// Implements a C wrapper around the TensorFlow runtime and CUDA or ROCM that
-// allows launching a kernel on the current device and stream from a binary blob
-// for the module and function name.
+#if GOOGLE_CUDA
+#include "third_party/gpus/cuda/include/cuda.h"
+#endif
+#if TENSORFLOW_USE_ROCM
+#include "rocm/include/hip/hip_runtime.h"
+#endif
 
 namespace mlir {
 namespace kernel_gen {
 namespace tf_framework {
 
+class GPURuntimeCache : public tensorflow::ResourceBase {
+ public:
+#if GOOGLE_CUDA
+  using GPUModule = CUmodule;
+#endif
+#if TENSORFLOW_USE_ROCM
+  using GPUModule = hipModule_t;
+#endif
+
+  ~GPURuntimeCache() override;
+  static constexpr const char* kDefaultResourceName = "mlir-gpu-runtime-cache";
+  static tensorflow::Status Create(GPURuntimeCache** dst);
+  std::string DebugString() const override;
+
+  // Assumes that no two modules are loaded from the same memory location over
+  // the lifetime of this cache. This allows to use the pointer as a key. All
+  // modules are unloaded on destruction of this cache.
+  GPUModule LookupOrLoadModule(void* data);
+
+ private:
+  tensorflow::mutex mu_;
+  absl::flat_hash_map<void*, GPUModule> gpu_module_by_data_ptr_
+      TF_GUARDED_BY(mu_);
+};
+
+// Implements a C wrapper around the TensorFlow runtime and CUDA (or ROCm)
+// library that allows launching a kernel on the current device and stream from
+// a binary blob for the module and function name.
 extern "C" MLIR_RUNNERUTILS_EXPORT void _mlir_ciface_tf_launch_kernel(
     void* ctx, void* module_blob, char* kernel_name, intptr_t gridX,
     intptr_t gridY, intptr_t gridZ, intptr_t blockX, intptr_t blockY,
