c7ea4dd4e8340fe9dee6e2c0cb3526548c29cace
Advait Jain <advaitjain@google.com>
2021-09-15 14:50:00 -0700

    Fix downstream TFLM build.
    PiperOrigin-RevId: 396929090
    Change-Id: Ia56c52caf7eb3818caaf073c602953ceadf9633b

--- tensorflow/lite/kernels/kernel_util.h
+++ tensorflow/lite/kernels/kernel_util.h
@@ -271,10 +271,12 @@ void CalculateActivationRange(TfLiteFusedActivation activation,
 // Return true if the given tensors have the same shape.
 bool HaveSameShapes(const TfLiteTensor* input1, const TfLiteTensor* input2);
 
+#if !defined(TF_LITE_STATIC_MEMORY)
 // Gets the output shape from the input tensor.
 TfLiteStatus GetOutputShapeFromInput(TfLiteContext* context,
                                      const TfLiteTensor* input,
                                      TfLiteIntArray** output_shape);
+#endif  // !defined(TF_LITE_STATIC_MEMORY)
 
 // Calculates the output_shape that is necessary for element-wise operations
 // with broadcasting involving the two input tensors.
