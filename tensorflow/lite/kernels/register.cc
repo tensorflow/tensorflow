783d5bea7effed1c38141752eb8ad395eeff2359
Meghna Natraj <mnatraj@google.com>
2021-09-16 13:45:18 -0700

    Add RandomStandardNormal BuiltIn op support in TFLite (float32 only)
    PiperOrigin-RevId: 397159247
    Change-Id: I8a7260bac0c2eddea22ae0fb3cb1b835606e6702

--- tensorflow/lite/kernels/register.cc
+++ tensorflow/lite/kernels/register.cc
@@ -330,6 +330,8 @@ BuiltinOpResolver::BuiltinOpResolver() {
   AddBuiltin(BuiltinOperator_VAR_HANDLE, Register_VAR_HANDLE());
   AddBuiltin(BuiltinOperator_READ_VARIABLE, Register_READ_VARIABLE());
   AddBuiltin(BuiltinOperator_ASSIGN_VARIABLE, Register_ASSIGN_VARIABLE());
+  AddBuiltin(BuiltinOperator_RANDOM_STANDARD_NORMAL,
+             Register_RANDOM_STANDARD_NORMAL());
   AddCustom("NumericVerify", tflite::ops::custom::Register_NUMERIC_VERIFY());
   // TODO(andrewharp, ahentz): Move these somewhere more appropriate so that
   // custom ops aren't always included by default.
