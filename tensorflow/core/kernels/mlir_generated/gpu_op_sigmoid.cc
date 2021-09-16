fe6e25cc43d557c594f9931ba7ad6b7c5c22af4c
Adrian Kuegel <akuegel@google.com>
2021-09-16 00:03:30 -0700

    Add MLIR generated Sigmoid GPU kernel.
    It is not enabled by default yet.
    
    PiperOrigin-RevId: 397012147
    Change-Id: I268ec9e278369e14eb5d45bc320cd307b768ff0b

--- tensorflow/core/kernels/mlir_generated/gpu_op_sigmoid.cc
+++ tensorflow/core/kernels/mlir_generated/gpu_op_sigmoid.cc
@@ -1,4 +1,4 @@
-/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,19 +13,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
-
-#include "tensorflow/core/kernels/cwise_ops_gpu_common.cu.h"
-#include "tensorflow/core/kernels/cwise_ops_gpu_gradients.cu.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/kernels/mlir_generated/base_gpu_op.h"
 
 namespace tensorflow {
-namespace functor {
-#if !defined(MLIR_GENERATED_GPU_KERNELS_ENABLED) || \
-    !defined(MLIR_GENERATED_EXPERIMENTAL_KERNELS_ENABLED)
-DEFINE_UNARY3(sigmoid, Eigen::half, float, double);
-#endif
-DEFINE_SIMPLE_BINARY3(sigmoid_grad, Eigen::half, float, double);
-}  // namespace functor
-}  // namespace tensorflow
 
-#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Sigmoid, DT_HALF);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Sigmoid, DT_FLOAT);
+GENERATE_AND_REGISTER_UNARY_GPU_KERNEL(Sigmoid, DT_DOUBLE);
+
+}  // namespace tensorflow
