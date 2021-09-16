adb105c7e6bf5479e1a8486cc21fa90325d44a29
Jay Shi <xiaojies@google.com>
2021-09-16 13:45:33 -0700

    [tf.data] Improve error messages in convert.py.
    PiperOrigin-RevId: 397159308
    Change-Id: Ie876c38e9be751292f7c6ba5aae5c647556ea82d

--- tensorflow/python/data/util/convert.py
+++ tensorflow/python/data/util/convert.py
@@ -60,13 +60,12 @@ def partial_shape_to_tensor(shape_like):
     # machinery.
     ret = ops.convert_to_tensor(shape_like, preferred_dtype=dtypes.int64)
     if ret.shape.dims is not None and len(ret.shape.dims) != 1:
-      raise ValueError("The given shape %s must be a 1-D tensor of tf.int64 "
-                       "values, but the shape was %s."
-                       % (shape_like, ret.shape))
+      raise ValueError("The given shape {} must be a 1-D tensor of `tf.int64` "
+                       "values, but the shape was {}.".format(
+                           shape_like, ret.shape))
     if ret.dtype != dtypes.int64:
-      raise TypeError("The given shape %s must be a 1-D tensor of tf.int64 "
-                      "values, but the element type was %s."
-                      % (shape_like, ret.dtype.name))
+      raise TypeError("The given shape {} must be a 1-D tensor of `tf.int64` "
+                      "values, but the element type was {}.".format(
+                          shape_like, ret.dtype.name))
 
     return ret
-
