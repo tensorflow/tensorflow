d62bab8f1b88fd993d14f235164ef7b84c1bdfd0
Taehee Jeong <taeheej@google.com>
2021-09-16 20:24:39 -0700

    Change long long literals to use ll instead of LL, to improve readability
    PiperOrigin-RevId: 397229037
    Change-Id: I4277e13cd9ccd8393ac4ab931f3ca9c3187c9c9f

--- tensorflow/lite/kernels/activations.cc
+++ tensorflow/lite/kernels/activations.cc
@@ -390,7 +390,7 @@ TfLiteStatus TanhPrepare(TfLiteContext* context, TfLiteNode* node) {
 
       const double q =
           std::frexp(input_real_multiplier, &data->input_left_shift);
-      auto q_fixed = static_cast<int32_t>(TfLiteRound(q * (1ll << 15)));
+      auto q_fixed = static_cast<int32_t>(TfLiteRound(q * (1LL << 15)));
       data->input_multiplier = static_cast<int16_t>(q_fixed);
 
       int16_t input_range_radius =
@@ -495,7 +495,7 @@ TfLiteStatus SigmoidPrepare(TfLiteContext* context, TfLiteNode* node) {
 
       const double q =
           std::frexp(input_real_multiplier, &data->input_left_shift);
-      auto q_fixed = static_cast<int32_t>(TfLiteRound(q * (1ll << 15)));
+      auto q_fixed = static_cast<int32_t>(TfLiteRound(q * (1LL << 15)));
       data->input_multiplier = static_cast<int16_t>(q_fixed);
 
       int16_t input_range_radius =
