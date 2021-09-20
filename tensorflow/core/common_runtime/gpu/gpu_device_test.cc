ce8f9bb77a734c4d789f05f16093d48a3e4de70f
Reza Rahimi <reza.amd.2021@gmail.com>
2021-09-20 23:06:48 +0000

    skip pooling_ops_test (Eigen fp16 issue) and CudaMallocAsyncPreallocate for ROCm

--- tensorflow/core/common_runtime/gpu/gpu_device_test.cc
+++ tensorflow/core/common_runtime/gpu/gpu_device_test.cc
@@ -159,7 +159,7 @@ TEST_F(GPUDeviceTest, CudaMallocAsync) {
   EXPECT_EQ(status.code(), error::OK);
 }
 
-TEST_F(GPUDeviceTest, CudaMallocAsyncPreallocate) {
+TEST_F(GPUDeviceTest, DISABLED_ON_GPU_ROCM(CudaMallocAsyncPreallocate)) {
   SessionOptions opts = MakeSessionOptions("0", 0, 1, {}, {},
                                            /*use_cuda_malloc_async=*/true);
   setenv("TF_CUDA_MALLOC_ASYNC_SUPPORTED_PREALLOC", "2048", 1);
