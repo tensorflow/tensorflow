2a2ba00a840c239223c0da2e6ca619d73da9fe8e
Trent Lo <trentl@nvidia.com>
2021-09-22 16:50:14 -0700

    [XLA/GPU] add testUpdateVariableWithCompileTimeConstMemoryUsage()
    testing a signature of (compile-time const, args, res_var). The compile-time
    const will be optimized away so that the kernel signature will become
    (args, res_var).

--- tensorflow/python/eager/def_function_auto_jit_test.py
+++ tensorflow/python/eager/def_function_auto_jit_test.py
@@ -20,7 +20,9 @@ from __future__ import print_function
 from tensorflow.compiler.tests import xla_test
 from tensorflow.python.eager import context
 from tensorflow.python.eager import def_function
+from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import random_ops
 from tensorflow.python.ops import variables
 from tensorflow.python.platform import test
@@ -54,6 +56,35 @@ class DefFunctionAutoclusteringTest(xla_test.XLATestCase):
     self.assertIn('_XlaRun', [n.op for n in graphs[0].node])
     self.assertEqual(initial_usage, final_usage)
 
+  def testUpdateVariableWithCompileTimeConstMemoryUsage(self):
+    if 'tpu' in self.device.lower():
+      self.skipTest('Autoclustering does not run on TPU')
+
+    v = variables.Variable(random_ops.random_normal([128, 128]))
+
+    # test a signature of (compile-time const, args, res_var). The compile-time
+    # const will be optimized away so that the kernel signature will become
+    # (args, res_var).
+    @def_function.function(jit_compile=False)
+    def update_var(shape, a, b, c):
+      # create 4 ops here, which is the required minimum cluster size.
+      x = a * b
+      y = x + c
+      v.assign_add(array_ops.reshape(y, shape))
+
+    arg1 = random_ops.random_normal([1])
+    arg2 = random_ops.random_normal([1])
+    arg3 = random_ops.random_normal([1])
+
+    with context.collect_graphs(optimized=True) as graphs:
+      initial_usage = context.context().get_memory_info(
+          v.device)['current']
+      update_var(constant_op.constant([1, 1]), arg1, arg2, arg3)
+      final_usage = context.context().get_memory_info(
+          v.device)['current']
+
+    self.assertIn('_XlaRun', [n.op for n in graphs[0].node])
+    self.assertEqual(initial_usage, final_usage)
 
 if __name__ == '__main__':
   ops.enable_eager_execution()
