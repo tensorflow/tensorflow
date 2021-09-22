265d5ec9321c61f6904f12aeabeafebca6fafa49
A. Unique TensorFlower <gardener@tensorflow.org>
2021-09-22 11:37:38 -0700

    Internal change
    PiperOrigin-RevId: 398290269
    Change-Id: Ie570f26363319ad84adeff2401c6a270bab922e3

--- tensorflow/compiler/mlir/xla/transforms/xla_legalize_tf.cc
+++ tensorflow/compiler/mlir/xla/transforms/xla_legalize_tf.cc
@@ -163,6 +163,7 @@ const llvm::DenseSet<mlir::TypeID> &MlirPreferredOps() {
     TypeID::get<TF::TanhOp>(),
     TypeID::get<TF::TanhGradOp>(),
     TypeID::get<TF::XlaDotOp>(),
+    TypeID::get<TF::XlaDotV2Op>(),
     TypeID::get<TF::XlogyOp>(),
     TypeID::get<TF::ZetaOp>(),
 
