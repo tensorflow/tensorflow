fc5d6058e6e0dc097ce7e0b331bd48a33c45b383
Itai Zukerman <itaiz@google.com>
2021-09-27 08:36:10 -0700

    Internal change only.
    PiperOrigin-RevId: 399197401
    Change-Id: I594e48a585e95406fe3754c4cd339a3fd23eb5cf

--- tensorflow/c/experimental/saved_model/internal/testdata/gen_saved_models.py
+++ tensorflow/c/experimental/saved_model/internal/testdata/gen_saved_models.py
@@ -22,7 +22,6 @@ testing.
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 import os
--- tensorflow/python/distribute/integration_test/tpu_memory_test.py
+++ tensorflow/python/distribute/integration_test/tpu_memory_test.py
@@ -16,7 +16,6 @@
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 import gc
