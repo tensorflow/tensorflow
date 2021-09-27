813ef7df7cc3dcd795dfaf04112ed7f41ecc6db3
Robert Suderman <suderman@google.com>
2021-09-27 12:18:58 -0700

    Fix tfl.shape and tfl.pseudo_const lowering for dynamic shapes
    Dynamic shapes cannot be converted to a constant during the lowering process.
    Also, changed the rewriter to occur top down. This works better for shapes
    inference. Pseudo_const also requires a rewriter as they can be generated
    during canonicalization.
    
    PiperOrigin-RevId: 399251025
    Change-Id: I57ca1ce4fdfd8c56f4c6a90d4fb969904d3dd29d

--- tensorflow/compiler/mlir/tosa/transforms/legalize_tfl.cc
+++ tensorflow/compiler/mlir/tosa/transforms/legalize_tfl.cc
@@ -145,6 +145,7 @@ DECL_CONVERT_OP(Custom);
 DECL_CONVERT_OP(ReverseV2);
 DECL_CONVERT_OP(Quantize);
 DECL_CONVERT_OP(Dequantize);
+DECL_CONVERT_OP(Const);
 DECL_CONVERT_OP(QConst);
 DECL_CONVERT_OP(Gather);
 DECL_CONVERT_OP(GatherNd);
@@ -1474,7 +1475,8 @@ LogicalResult ConvertTFLShapeOp::matchAndRewrite(
 
   RankedTensorType input_type =
       tfl_shape_op.input().getType().dyn_cast<RankedTensorType>();
-  if (!input_type) return failure();
+  if (!input_type || !input_type.hasStaticShape())
+    return rewriter.notifyMatchFailure(op, "input shape not static");
 
   auto input_shape = input_type.getShape();
 
@@ -2670,20 +2672,48 @@ LogicalResult ConvertTFLDequantizeOp::matchAndRewrite(
   return failure();
 }
 
+LogicalResult ConvertTFLConstOp::matchAndRewrite(
+    Operation* op, PatternRewriter& rewriter) const {
+  auto tfl_const_op = cast<TFL::ConstOp>(op);
+
+  ShapedType output_type =
+      tfl_const_op.getResult().getType().dyn_cast<ShapedType>();
+  if (!output_type) return failure();
+
+  ElementsAttr elements = tfl_const_op.value();
+  Type element_type = elements.getType().getElementType();
+  if (output_type.getElementType().isa<quant::QuantizedType>()) {
+    output_type = RankedTensorType::get(output_type.getShape(), element_type);
+  }
+
+  // If the output shape is unranked we can extract the result shape from the
+  // attribute shape. This occurs as some TFLite folders create constants with
+  // unranked shapes.
+  if (!output_type.hasRank()) {
+    output_type = elements.getType().cast<ShapedType>().clone(element_type);
+  }
+
+  rewriter.replaceOpWithNewOp<tosa::ConstOp>(op, output_type, elements);
+
+  return success();
+}
+
 LogicalResult ConvertTFLQConstOp::matchAndRewrite(
     Operation* op, PatternRewriter& rewriter) const {
   auto tfl_qconst_op = cast<TFL::QConstOp>(op);
 
-  RankedTensorType output_type =
-      tfl_qconst_op.getResult().getType().dyn_cast<RankedTensorType>();
-
-  // Not a ranked tensor output
+  ShapedType output_type =
+      tfl_qconst_op.getResult().getType().dyn_cast<ShapedType>();
   if (!output_type) return failure();
 
   ElementsAttr elements = tfl_qconst_op.value();
-  Type element_type = elements.getType().getElementType();
-  if (output_type.getElementType().isa<quant::QuantizedType>()) {
-    output_type = RankedTensorType::get(output_type.getShape(), element_type);
+
+  // If the output shape is unranked we can extract the result shape from the
+  // attribute shape. This occurs as some TFLite folders create constants with
+  // unranked shapes.
+  if (!output_type.hasRank()) {
+    output_type = elements.getType().cast<ShapedType>().clone(
+        output_type.getElementType());
   }
 
   rewriter.replaceOpWithNewOp<tosa::ConstOp>(op, output_type, elements);
@@ -2710,6 +2740,12 @@ LogicalResult ConvertConstantOp::matchAndRewrite(
     e_type = rewriter.getIntegerType(48);
     attr = attr.cast<DenseIntOrFPElementsAttr>().mapValues(
         e_type, [](const APInt& x) -> APInt { return x.trunc(48); });
+  }
+
+  if (!output_type.hasRank()) {
+    if (auto attr_type = attr.getType().dyn_cast<ShapedType>()) {
+      output_type = attr_type.clone(e_type);
+    }
   }
 
   output_type = output_type.clone(e_type);
@@ -2905,6 +2941,7 @@ void LegalizeTFL::runOnFunction() {
   DEF_PATTERN_INSERT(TFLReverseV2);
   DEF_PATTERN_INSERT(TFLQuantize);
   DEF_PATTERN_INSERT(TFLDequantize);
+  DEF_PATTERN_INSERT(TFLConst);
   DEF_PATTERN_INSERT(TFLQConst);
   DEF_PATTERN_INSERT(Constant);
   DEF_PATTERN_INSERT(TFLGather);
@@ -2913,9 +2950,17 @@ void LegalizeTFL::runOnFunction() {
   DEF_PATTERN_INSERT(TFLFakeQuant);
   DEF_PATTERN_INSERT(TFLOneHot);
 
-  if (failed(applyPatternsAndFoldGreedily(func, std::move(patterns)))) {
+  GreedyRewriteConfig config;
+  config.useTopDownTraversal = true;
+  if (failed(applyPatternsAndFoldGreedily(func, std::move(patterns), config))) {
     signalPassFailure();
   }
+
+  func.walk([&](tosa::ConstOp op) {
+    auto ety = op.value().getType().getElementType();
+    auto new_ty = op.getType().cast<ShapedType>().clone(ety);
+    op.getResult().setType(new_ty);
+  });
 
   // Insert UnrealizedConversionCasts to guarantee ReturnOp agress with
   // the FuncOp type.
