2826ca2ae6842e8f7d2b41f3b8638eab229e0a74
A. Unique TensorFlower <gardener@tensorflow.org>
2021-09-28 02:01:36 -0700

    Update GraphDef version to 903.
    PiperOrigin-RevId: 399385579
    Change-Id: If689ce8f4c4adfc4e7dfd03852d9638f790943cc

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 902  // Updated: 2021/9/27
+#define TF_GRAPH_DEF_VERSION 903  // Updated: 2021/9/28
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
