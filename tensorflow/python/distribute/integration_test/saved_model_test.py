d4d97889b8bfcdb6198c5fb24e473a44594a0ad5
Ran Chen <crccw@google.com>
2021-09-29 16:18:28 -0700

    Prevent functions with the assertion node being saved
    We inserted an assertion node to prevent loaded distributed models being used
    outside of strategy.run(). The user can accidentally save the assertion node if
    they are saving ConcreteFunction. By making functions with the assertion node as
    unsaveable, the user gets the error at saving time instead of loading time.
    
    PiperOrigin-RevId: 399801333
    Change-Id: I3813cb19eb3ece0b3fe929ee0dd84b344be8f644

--- tensorflow/python/distribute/integration_test/saved_model_test.py
+++ tensorflow/python/distribute/integration_test/saved_model_test.py
@@ -295,6 +295,15 @@ class SaveAndLoadForServingTest(test.TestCase, parameterized.TestCase):
     with strategy.scope():
       m = Model(tf.saved_model.load(layer_export_dir))
       export_dir = self.get_temp_dir()
+      # Saving a ConcreteFunction should raise an error.
+      with self.assertRaisesRegex(
+          ValueError, "save a tf.function with input_signature instead"):
+        tf.saved_model.save(
+            m,
+            export_dir,
+            signatures={
+                "call": m.__call__.get_concrete_function(),
+            })
       tf.saved_model.save(m, export_dir)
 
     loaded = tf.saved_model.load(export_dir)
--- tensorflow/python/saved_model/load.py
+++ tensorflow/python/saved_model/load.py
@@ -64,10 +64,19 @@ def _unused_handle():
   error_message = ("Trying to access a placeholder that is not supposed to be "
                    "executed. This means you are executing a graph generated "
                    "from the cross-replica context in an in-replica context.")
+  save_error_message = (
+      "It seems that you are trying to save a "
+      "tf.types.experimental.ConcreteFunction that involves a distributed "
+      "model, and the model contains parts that are loaded form a SavedModel. "
+      "It's supported to save such tf.types.experimental.ConcreteFunction. Try"
+      " save a tf.function with input_signature instead, and file a bug there "
+      "are still issues.")
 
   assert_op = control_flow_ops.Assert(
-      array_ops.placeholder_with_default(False, shape=()),
-      [error_message])
+      array_ops.placeholder_with_default(False, shape=()), [error_message])
+  if (not context.executing_eagerly()
+     ) and ops.get_default_graph().building_function:
+    ops.get_default_graph().mark_as_unsaveable(save_error_message)
 
   with ops.control_dependencies([assert_op]):
     return array_ops.placeholder(dtype=dtypes.resource)
