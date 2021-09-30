92e84b48f6e2bed5be6088192216048fea63cba8
Edward Loper <edloper@google.com>
2021-09-30 16:05:58 -0700

    Fix out-of-bounds memory error in tf.ragged.cross shape inference when it is called with invalid inputs.
    PiperOrigin-RevId: 400049589
    Change-Id: Icd535f4c6b96b3c926befb70a0e44e6d2b004c0a

--- tensorflow/core/ops/ragged_array_ops.cc
+++ tensorflow/core/ops/ragged_array_ops.cc
@@ -64,6 +64,7 @@ REGISTER_OP("RaggedCross")
     .SetShapeFn([](shape_inference::InferenceContext* c) {
       std::vector<DataType> ragged_values_types;
       std::vector<DataType> ragged_splits_types;
+      std::vector<DataType> sparse_values_types;
       std::vector<DataType> dense_types;
 
       TF_RETURN_IF_ERROR(
@@ -71,15 +72,21 @@ REGISTER_OP("RaggedCross")
       TF_RETURN_IF_ERROR(
           c->GetAttr("ragged_splits_types", &ragged_splits_types));
       TF_RETURN_IF_ERROR(c->GetAttr("dense_types", &dense_types));
+      TF_RETURN_IF_ERROR(
+          c->GetAttr("sparse_values_types", &sparse_values_types));
 
       int num_ragged = ragged_values_types.size();
       if (num_ragged != ragged_splits_types.size()) {
         return errors::InvalidArgument(
-            "Parameters `values` and `row_splits` must be the same length");
+            "ragged values and splits must have the same length.");
       }
 
       int num_sparse;
       TF_RETURN_IF_ERROR(c->GetAttr("Nsparse", &num_sparse));
+      if (num_sparse != sparse_values_types.size()) {
+        return errors::InvalidArgument(
+            "sparse indices and values must have the same length");
+      }
 
       ShapeHandle out_values = c->UnknownShapeOfRank(1);
       ShapeHandle out_splits = c->UnknownShapeOfRank(1);
