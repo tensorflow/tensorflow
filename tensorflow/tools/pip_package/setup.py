f84ca5bf14f2ec0ca14a7b97cfe866b2cd79bf7d
Scott Zhu <scottzhu@google.com>
2021-10-06 10:59:00 -0700

    Increase the keras{-nightly} version for the 2.7 release.
    PiperOrigin-RevId: 401288349
    Change-Id: I386f02aa49813c7a69a1cce3839779e39c8c4232

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -99,7 +99,9 @@ REQUIRED_PACKAGES = [
     # When updating these, please also update the nightly versions below
     'tensorboard ~= 2.6',
     'tensorflow_estimator ~= 2.6',
-    'keras ~= 2.6',
+    # Keras release is not backward compatible with old tf release, and we have
+    # to make the version aligned between TF and Keras.
+    'keras >= 2.7, < 2.8',
     'tensorflow-io-gcs-filesystem >= 0.21.0',
 ]
 
@@ -116,7 +118,7 @@ if 'tf_nightly' in project_name:
     elif 'tensorflow_estimator' in pkg:
       REQUIRED_PACKAGES[i] = 'tf-estimator-nightly ~= 2.7.0.dev'
     elif 'keras' in pkg and 'keras_preprocessing' not in pkg:
-      REQUIRED_PACKAGES[i] = 'keras-nightly ~= 2.7.0.dev'
+      REQUIRED_PACKAGES[i] = 'keras-nightly ~= 2.8.0.dev'
 
 
 # grpcio does not build correctly on big-endian machines due to lack of
