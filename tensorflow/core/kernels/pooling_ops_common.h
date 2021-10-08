329795f305d9784b72f086bf9470fed050376ad9
A. Unique TensorFlower <gardener@tensorflow.org>
2021-10-07 20:53:56 -0700

    Add shape checks to MaxPool gradient kernels.
    Before, when the shapes were invalid, the kernels would nondeterministically segfault or output garbage values. Now an InvalidArgument error is raised.
    
    PiperOrigin-RevId: 401675689
    Change-Id: Ib27d18b533929eed8140a1f42128c52314ee16dc

--- tensorflow/core/kernels/pooling_ops_common.h
+++ tensorflow/core/kernels/pooling_ops_common.h
@@ -83,6 +83,11 @@ struct PoolParameters {
   TensorFormat data_format;
 };
 
+// Checks if the sizes of the paddings are less than the size of window.
+// This is required for MaxPool because it pads with -inf, so the pooling
+// window cannot fully cover the padded area.
+Status CheckPaddingSize(PoolParameters& params);
+
 // An implementation of MaxPooling (forward).
 // TODO (yongtang): Remove MaxPoolingOp and use MaxPoolingV2Op,
 //     QuantizedMaxPoolingOp depends on MaxPoolingOp so keep intact for now
