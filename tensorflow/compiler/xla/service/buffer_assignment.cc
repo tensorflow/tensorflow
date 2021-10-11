3a8e9c980e84c7005aed2c97f82408917a4a0ebb
Berkin Ilbeyi <berkin@google.com>
2021-10-11 09:52:19 -0700

    [XLA] Fix peak buffer accounting in buffer assignment.
    Buffers that alias with each other could have been included in peak buffers.
    Then, the sum of the peak buffers could have been greater than the size of the
    containing buffer, causing crashes in other parts of the compiler.
    
    PiperOrigin-RevId: 402324270
    Change-Id: I618f7159fe5f35472347220560bb2f47cda970db

--- tensorflow/compiler/xla/service/buffer_assignment.cc
+++ tensorflow/compiler/xla/service/buffer_assignment.cc
@@ -1519,17 +1519,44 @@ std::vector<const HloValue*> ComputePeakMemoryLogicalBuffers(
   }
   VLOG(1) << "Compute peak memory logical buffers";
 
+  // To properly account for shared buffers, we keep track of the number of
+  // instances of the same shared buffer are currently live, their canonical ids
+  // and the size we had returned when allocating the buffer so that we can
+  // return the -size when freeing the buffer.
+  absl::flat_hash_map<int64_t, int> num_outstanding_shared_buffers;
+  absl::flat_hash_map<int64_t, int64_t> shared_canonical_ids;
+  absl::flat_hash_map<int64_t, int64_t> allocated_sizes;
   // Returns how much the given event increases the total size of live
   // buffers. Can be negative.
-  auto memory_delta = [&id_to_value, &buffer_sizes](
-                          const HeapSimulatorTrace::Event& event) -> int64_t {
+  auto memory_delta = [&](const HeapSimulatorTrace::Event& event) -> int64_t {
     const HloValue* buffer = id_to_value.at(event.buffer_id());
     const int64_t buffer_size = buffer_sizes.at(buffer);
-    if (event.kind() == HeapSimulatorTrace::Event::ALLOC ||
-        event.kind() == HeapSimulatorTrace::Event::SHARE_WITH) {
+    if (event.kind() == HeapSimulatorTrace::Event::ALLOC) {
+      num_outstanding_shared_buffers[event.buffer_id()] = 1;
+      allocated_sizes[event.buffer_id()] = buffer_size;
       return buffer_size;
+    } else if (event.kind() == HeapSimulatorTrace::Event::SHARE_WITH) {
+      shared_canonical_ids[event.buffer_id()] = event.share_with_canonical_id();
+      if (++num_outstanding_shared_buffers[event.share_with_canonical_id()] ==
+          1) {
+        // This shared buffer is currently the only instance of the buffer with
+        // the canonical id. So we return the buffer size.
+        allocated_sizes[event.buffer_id()] = buffer_size;
+        return buffer_size;
+      }
+      // There are multiple instances of this buffer, so return 0.
+      allocated_sizes[event.buffer_id()] = 0;
+      return 0;
     } else if (event.kind() == HeapSimulatorTrace::Event::FREE) {
-      return -1 * buffer_size;
+      auto shared_canonical_id_it =
+          shared_canonical_ids.find(event.buffer_id());
+      // Decrement the outstanding instances of this buffer and return the
+      // -size.
+      int64_t buffer_id = (shared_canonical_id_it == shared_canonical_ids.end())
+                              ? event.buffer_id()
+                              : shared_canonical_id_it->second;
+      --num_outstanding_shared_buffers[buffer_id];
+      return -1 * allocated_sizes[event.buffer_id()];
     }
     LOG(FATAL) << "Unknown event kind: " << event.kind();
   };
@@ -1554,6 +1581,7 @@ std::vector<const HloValue*> ComputePeakMemoryLogicalBuffers(
   // maximal live set size.
   absl::flat_hash_set<const HloValue*> live_values;
   live_size = 0;
+  num_outstanding_shared_buffers.clear();
   for (const auto& event : heap_trace.events()) {
     if (!id_to_value.contains(event.buffer_id())) {
       // Skip as the buffer associated with this trace event is not placed into
@@ -1562,14 +1590,18 @@ std::vector<const HloValue*> ComputePeakMemoryLogicalBuffers(
       continue;
     }
     const HloValue* value = id_to_value.at(event.buffer_id());
-    if (event.kind() == HeapSimulatorTrace::Event::ALLOC ||
-        event.kind() == HeapSimulatorTrace::Event::SHARE_WITH) {
+    int64_t delta = memory_delta(event);
+    // To avoid including buffers that are aliases of each other to the peak
+    // buffers list, only add the buffers that memory_delta returns non-zero
+    // positive sizes. memory_delta returns 0 as the size for the buffer already
+    // has a live alias of itself.
+    if (delta > 0) {
       InsertOrDie(&live_values, value);
-    } else if (event.kind() == HeapSimulatorTrace::Event::FREE) {
+    } else if (delta < 0) {
       CHECK(ContainsKey(live_values, value));
       live_values.erase(value);
     }
-    live_size += memory_delta(event);
+    live_size += delta;
 
     if (live_size == max_live_size) {
       break;
