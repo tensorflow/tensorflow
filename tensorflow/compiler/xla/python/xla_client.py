be30c80eb20b76d2cd30db46bc8c5b1113da537d
Jake VanderPlas <vanderplas@google.com>
2021-10-14 12:24:49 -0700

    fix repr() of jit-compiled functions
    PiperOrigin-RevId: 403157400
    Change-Id: I1d7ec24aeacc2dfe555f39dde0a7d0faf6468791

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -46,7 +46,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 39
+_version = 40
 
 xla_platform_names = {
     'cpu': 'Host',
