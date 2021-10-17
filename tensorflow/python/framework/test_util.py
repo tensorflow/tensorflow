a794289f728df01be0a58a59fbe826dc9f4a52ce
Rohan Jain <rohanj@google.com>
2021-10-16 17:13:30 -0700

    Creating an assertDictEqual method for tensors.
    PiperOrigin-RevId: 403668352
    Change-Id: I5656e68590fa3985af8ff7f7982d2db919b5adc1

--- tensorflow/python/framework/test_util.py
+++ tensorflow/python/framework/test_util.py
@@ -3387,6 +3387,32 @@ class TensorFlowTestCase(googletest.TestCase):
         device1, device2,
         "Devices %s and %s are not equal. %s" % (device1, device2, msg))
 
+  @py_func_if_in_function
+  def assertDictEqual(self, a, b, msg=None):
+    """Assert that two given dictionary of tensors are the same.
+
+    Args:
+      a: Expected dictionary with numpy ndarray or anything else that can be
+        converted to one as values.
+      b: Actual dictionary with numpy ndarray or anything else that can be
+        converted to one as values.
+      msg: Optional message to report on failure.
+    """
+    # To keep backwards compatibility, we first try the base class
+    # assertDictEqual. If that fails we try the tensorflow one.
+    try:
+      super().assertDictEqual(a, b, msg)
+    except Exception:  # pylint: disable=broad-except
+      self.assertSameElements(a.keys(), b.keys())  # pylint: disable=g-assert-in-except
+      for k, v in a.items():
+        (a_k, b_k) = self.evaluate_if_both_tensors(v, b[k])
+        a_k = self._GetNdArray(a_k)
+        b_k = self._GetNdArray(b_k)
+        if np.issubdtype(a_k.dtype, np.floating):
+          self.assertAllClose(v, b[k], msg=k)
+        else:
+          self.assertAllEqual(v, b[k], msg=k)
+
   def _GetPyList(self, a):
     """Converts `a` to a nested python list."""
     if isinstance(a, ragged_tensor.RaggedTensor):
