4aa312f7172cbc35ba7bf16b81be1386b41309f6
Nick Felt <nickfelt@google.com>
2021-10-18 17:57:50 -0700

    Expose GetRegisteredFileSystemSchemes() Python API under tf.io.gfile
    The purpose of making this a Python API is to allow programmatic detection of what filesystem schemes are handled by TensorFlow's `tf.io.gfile` interface (including any registered by importing TensorFlow IO). This allows clients to determine which file URIs they can use `gfile` to resolve, and which ones they will need to resolve via other implementations.
    
    PiperOrigin-RevId: 404112847
    Change-Id: Ib6ee0faecc8d48620591819ceed5e3c39ac7f2e3

--- tensorflow/python/lib/io/file_io_test.py
+++ tensorflow/python/lib/io/file_io_test.py
@@ -694,6 +694,17 @@ class FileIoTest(test.TestCase, parameterized.TestCase):
   def testHasAtomicMove(self):
     self.assertTrue(file_io.has_atomic_move("/a/b/c"))
 
+  def testGetRegisteredSchemes(self):
+    expected = ["", "file", "ram"]
+    actual = file_io.get_registered_schemes()
+    # Be flexible about additional schemes that may sometimes be registered when
+    # this test is run, while still verifying each scheme appears just once.
+    maybe_expected = ["gs"]
+    for scheme in maybe_expected:
+      if scheme in actual:
+        expected.append("gs")
+    self.assertCountEqual(expected, actual)
+
 
 if __name__ == "__main__":
   test.main()
