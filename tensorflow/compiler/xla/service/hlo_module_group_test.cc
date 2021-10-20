994cfa99fb50117ccc2aff78be842cef7224f470
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-10-20 10:35:46 -0400

    [tensorflow/compiler/xla/service/hlo_module_group_test.cc] Use `const auto&` instead of `const auto`; remove underscore from name

--- tensorflow/compiler/xla/service/hlo_module_group_test.cc
+++ tensorflow/compiler/xla/service/hlo_module_group_test.cc
@@ -192,9 +192,9 @@ ENTRY entry {
     ASSERT_EQ(metadata->companion_sets().size(), 1);
 
     std::vector<int64_t> module_ids;
-    const auto _companion_sets = *metadata->companion_sets()[0];
-    module_ids.reserve(_companion_sets.size());
-    for (HloInstruction* companion : _companion_sets) {
+    const auto& companion_sets = *metadata->companion_sets()[0];
+    module_ids.reserve(companion_sets.size());
+    for (HloInstruction* companion : companion_sets) {
       module_ids.push_back(metadata->GetModuleId(companion->GetModule()));
     }
 
