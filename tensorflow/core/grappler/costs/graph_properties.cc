a1320ec1eac186da1d03f033109191f715b2b130
Roman Dzhabarov <rdzhabarov@google.com>
2021-10-19 19:12:38 -0700

    [NFC] Remove unused AnnotateOutputShapes method.
    PiperOrigin-RevId: 404420835
    Change-Id: I81bcd510db8373a0c79fce2cd9d772b2fe7cbaab

--- tensorflow/core/grappler/costs/graph_properties.cc
+++ tensorflow/core/grappler/costs/graph_properties.cc
@@ -2762,8 +2762,7 @@ Status GraphProperties::InferDynamically(Cluster* cluster) {
   return InferFromCostGraph(metadata.cost_graph());
 }
 
-Status GraphProperties::AnnotateOutputShapes(GraphDef* output_graph_def,
-                                             bool allow_symbolic_shapes) const {
+Status GraphProperties::AnnotateOutputShapes(GraphDef* output_graph_def) const {
   *output_graph_def = item_.graph;
   for (int i = 0; i < output_graph_def->node_size(); i++) {
     auto node = output_graph_def->mutable_node(i);
@@ -2772,9 +2771,7 @@ Status GraphProperties::AnnotateOutputShapes(GraphDef* output_graph_def,
     for (const auto& tensor_property : tensor_properties) {
       TensorShapeProto* proto = attr_output_shape.mutable_list()->add_shape();
       *proto = tensor_property.shape();
-      if (!allow_symbolic_shapes) {
-        NormalizeShapeForOutput(proto);
-      }
+      NormalizeShapeForOutput(proto);
     }
     (*node->mutable_attr())["_output_shapes"] = std::move(attr_output_shape);
   }
