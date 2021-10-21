ebf874cc243c498d47128d718889d8036670f284
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-10-21 19:39:14 -0400

    [tensorflow/compiler/mlir/tensorflow/utils/convert_tensor.cc] No longer `reserve` on `llvm::SmallVector<int64_t> original_dimensions`

--- tensorflow/compiler/mlir/tensorflow/utils/convert_tensor.cc
+++ tensorflow/compiler/mlir/tensorflow/utils/convert_tensor.cc
@@ -229,7 +229,6 @@ StatusOr<ElementsAttr> ConvertTensorProto(const TensorProto& input_tensor,
                         ConvertTensorProto(tensor_copy, builder));
 
     llvm::SmallVector<int64_t> original_dimensions;
-    original_dimensions.reserve(input_tensor_shape.num_elements());
     for (auto dim : input_tensor_shape) original_dimensions.push_back(dim.size);
     return ElementsAttr(mlir::SplatElementsAttr::get(
         single_attr.getType().clone(original_dimensions),
