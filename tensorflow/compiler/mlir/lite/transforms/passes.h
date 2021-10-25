83fa05101374909740db33f19fb7290a11a00cce
Songyi Han <syhan@google.com>
2021-10-25 01:52:19 -0700

    Improve long list of parameters in QuantizePass
    PiperOrigin-RevId: 405343554
    Change-Id: Ia44984a4e3b6beee429647b23dfd3b41aec995de

--- tensorflow/compiler/mlir/lite/transforms/passes.h
+++ tensorflow/compiler/mlir/lite/transforms/passes.h
@@ -54,7 +54,12 @@ std::unique_ptr<OperationPass<ModuleOp>> CreateLowerStaticTensorListPass(
     bool default_to_single_batch = false);
 
 // Creates an instance of the TensorFlow Lite dialect Quantize pass.
-// TODO(b/201596819): Improve long list of parameters in QuantizePass.
+std::unique_ptr<OperationPass<FuncOp>> CreateQuantizePass(
+    const QuantizationSpecs& quant_specs, const StringSet& ops_blocklist = {},
+    const StringSet& nodes_blocklist = {});
+
+// Overloading of CreateQuantizePass which takes only necessary flags to reduce
+// the binary size.
 std::unique_ptr<OperationPass<FuncOp>> CreateQuantizePass(
     bool verify_numeric = false, bool whole_model_verify = false,
     bool legacy_float_scale = false, const StringSet& ops_blocklist = {},
