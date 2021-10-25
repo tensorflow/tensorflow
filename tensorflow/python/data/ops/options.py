f91d17e74e4d31d64eaea5254df5058ea2f75593
Andrew Audibert <aaudibert@google.com>
2021-10-25 09:41:22 -0700

    [tf.data] Fix typo.
    PiperOrigin-RevId: 405422754
    Change-Id: I86b39073ab384e36047eaf9f8727b15082dcccca

--- tensorflow/python/data/ops/options.py
+++ tensorflow/python/data/ops/options.py
@@ -38,7 +38,7 @@ class AutotuneAlgorithm(enum.Enum):
   GRADIENT_DESCENT: In each optimization step, this algorithm updates the
   parameter values in the optimal direction.
 
-  MAX_PARALLELISM: Similar to HILL_CLIMB but uses a relaxed stoping condition,
+  MAX_PARALLELISM: Similar to HILL_CLIMB but uses a relaxed stopping condition,
   allowing the optimization to oversubscribe the CPU.
   """
   DEFAULT = 0
