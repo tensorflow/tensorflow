adc97f94c707c2b3f4b15854666b9eab70fd621a
Rahul Joshi <jurahul@google.com>
2021-10-26 09:37:38 -0700

    [XLA:GPU][SPMD] Expand simple scatter and gather prior to SPMD partitioning. - SPMD partitioner seems to be generating invalid HLO if such scatter/gather is seen. - Avoid the issue by expanding these simple scatter and gather
    PiperOrigin-RevId: 405667242
    Change-Id: I1d31235ca6920d0d56c68b07edf714363d4356fa

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
@@ -290,6 +290,10 @@ Status GpuCompiler::OptimizeHloModule(
 
       spmd_simplify.AddPass<SortSimplifier>();
       spmd_simplify.AddPass<TupleSimplifier>();
+      spmd_simplify.AddPass<ScatterExpander>(
+          ScatterExpander::kEliminateSimpleScatters);
+      spmd_simplify.AddPass<GatherExpander>(
+          GatherExpander::kEliminateSimpleGathers);
       spmd_simplify.AddPass<WhileLoopConstantSinking>();
       spmd_simplify.AddPass<WhileLoopSimplifier>();
 
