d2c0187130234b8ef7bad17a3b1fcdb4268130b0
Yuduo Wu <6426433+1duo@users.noreply.github.com>
2021-10-26 17:01:20 -0700

    Minor fix for doc string for avg_pool3d.

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -4631,7 +4631,7 @@ def avg_pool3d(input, ksize, strides, padding, data_format="NDHWC", name=None): 
   window in `value`.
 
   Args:
-    input: A 5-D `Tensor` of shape `[batch, height, width, channels]` and type
+    input: A 5-D `Tensor` of shape `[batch, depth, height, width, channels]` and type
       `float32`, `float64`, `qint8`, `quint8`, or `qint32`.
     ksize: An int or list of `ints` that has length `1`, `3` or `5`. The size of
       the window for each dimension of the input tensor.
