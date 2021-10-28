5a11f383263937c09d256b0df9e6852783712b0c
stevenireeves <reevessteven15@gmail.com>
2021-10-28 08:37:03 -0700

    Address Space Casting initial value addr to generic space for a fix for the llvm ir generated on AMD GPU.

--- tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
+++ tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.cc
@@ -4073,8 +4073,13 @@ void IrEmitterUnnested::EmitReductionOutputForRowReduction(
       llvm::Type* element_type =
           state.partial_result_address->getType()->getElementType();
 
-      llvm::Value* initial_value_addr = llvm_ir::EmitAllocaAtFunctionEntry(
-          element_type, "initial_value_addr", &b_);
+/* Insure initial value address is in generic, not scratch. */ 
+      llvm::Value* initial_value_addr = b_.CreateAddrSpaceCast(
+		       llvm_ir::EmitAllocaAtFunctionEntry(
+          		element_type, "initial_value_addr", &b_),
+		      llvm::PointerType::get(element_type,
+			      /*AddressSpace=*/0),
+		      "initial_value_addr");
       b_.CreateStore(state.initial_value, initial_value_addr);
 
       llvm::Value* warp_exists = b_.CreateICmpULT(
