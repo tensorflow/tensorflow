dd278c2f03b7458c1598aafdc8a9a1afd094f178
A. Unique TensorFlower <gardener@tensorflow.org>
2021-11-11 18:28:45 -0800

    Resubmit unchanged again: diffbase resubmitted again.
    PiperOrigin-RevId: 409293666
    Change-Id: Iae00d3a8ac08d1c4f22c0ac2477cd4a5f7da95d2

--- tensorflow/stream_executor/cuda/cuda_dnn.cc
+++ tensorflow/stream_executor/cuda/cuda_dnn.cc
@@ -4562,9 +4562,13 @@ port::Status CudnnSupport::GetConvolveRunners(
     bool use_cudnn_frontend, dnn::ConvolutionKind kind,
     dnn::DataType input_type, dnn::DataType output_type, Stream* stream,
     const dnn::BatchDescriptor& input_descriptor,
+    DeviceMemoryBase /*input_data*/,
     const dnn::FilterDescriptor& filter_descriptor,
+    DeviceMemoryBase /*filter_data*/,
     const dnn::BatchDescriptor& output_descriptor,
+    DeviceMemoryBase /*output_data*/,
     const dnn::ConvolutionDescriptor& convolution_descriptor,
+    ScratchAllocator* /*scratch_allocator*/,
     std::vector<std::unique_ptr<const dnn::ConvRunner>>* out_exec_plans) {
   if (!use_cudnn_frontend) {
     auto cuda_compute_capability = stream->GetCudaComputeCapability();
