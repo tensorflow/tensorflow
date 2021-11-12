d56a650fc9c0f08e6e52fd47d3fb4b0a81ab288d
Haoyu Zhang <haoyuzhang@google.com>
2021-11-12 10:29:10 -0800

    Support adding timeout to blocking operations in remote eager execution.
    PiperOrigin-RevId: 409445308
    Change-Id: I29aec8bbd6ef9a88c00c3ed516cb88276eb6a507

--- tensorflow/python/eager/remote_test.py
+++ tensorflow/python/eager/remote_test.py
@@ -182,6 +182,24 @@ class SingleWorkerTest(test.TestCase, parameterized.TestCase):
     with ops.device('/job:localhost/task:0'):
       self.assertAllEqual(func(constant_op.constant(1)), [2])
 
+  def testOperationTimeout(self):
+    context._reset_context()
+    context.context().operation_timeout_in_ms = 10
+    workers, _ = test_util.create_local_cluster(1, 0)
+    remote.connect_to_remote_host(workers[0].target)
+
+    q = data_flow_ops.FIFOQueue(1, dtypes.int32)
+
+    @def_function.function
+    def f():
+      return q.dequeue()
+
+    with self.assertRaises(errors.DeadlineExceededError):
+      with ops.device('/job:worker/replica:0/task:0'):
+        f()
+      # If streaming RPC is enabled, fetch remote errors before end of execution
+      context.async_wait()
+
 
 class RemoteAsyncTest(test.TestCase):
 
