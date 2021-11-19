cb36e189d7a6a2b60603778e8139a070d5fc1ab7
Mihai Maruseac <mihaimaruseac@google.com>
2021-11-19 09:44:58 -0800

    Ensure `l2` is scalar in `BoostedTreesCalculateBestFeatureSplitV2`
    PiperOrigin-RevId: 411082272
    Change-Id: I1fc631e51a46c11997d4b537fad99f200d2db585

--- tensorflow/core/kernels/boosted_trees/stats_ops.cc
+++ tensorflow/core/kernels/boosted_trees/stats_ops.cc
@@ -719,6 +719,10 @@ class BoostedTreesCalculateBestFeatureSplitV2 : public OpKernel {
     }
     const Tensor* l2_t;
     OP_REQUIRES_OK(context, context->input("l2", &l2_t));
+    OP_REQUIRES(
+        context, TensorShapeUtils::IsScalar(l2_t->shape()),
+        errors::InvalidArgument("l2 must be a scalar, got a tensor of shape ",
+                                l2_t->shape().DebugString()));
     const auto l2 = l2_t->scalar<float>()();
     DCHECK_GE(l2, 0);
     const Tensor* tree_complexity_t;
