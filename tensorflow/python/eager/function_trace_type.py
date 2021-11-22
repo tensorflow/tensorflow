90425b5390ea194ceb6802f0280c988cb3db4585
Faizan Muhammad <fmuham@google.com>
2021-11-22 09:18:06 -0800

    Remove GenericType Special Cases
    PiperOrigin-RevId: 411575943
    Change-Id: I2a3e141082e95e770d2d47687d6d93338dfb7a6e

--- tensorflow/python/eager/function_trace_type.py
+++ tensorflow/python/eager/function_trace_type.py
@@ -15,17 +15,10 @@
 """Utitiles for Cache Key generation based on Function Trace Type."""
 
 from typing import Dict, Hashable, Optional, Sequence, Tuple, Type
-import weakref
-
-import numpy as np
 
 from tensorflow.python import pywrap_tfe
 from tensorflow.python.eager import core
-from tensorflow.python.framework import composite_tensor
 from tensorflow.python.framework import errors
-from tensorflow.python.framework import ops
-from tensorflow.python.framework import tensor_spec
-from tensorflow.python.ops import resource_variable_ops
 from tensorflow.python.types import trace
 from tensorflow.python.util import _pywrap_utils
 
@@ -57,7 +50,7 @@ class GenericType(trace.TraceType):
 
   def __init__(self, obj):
     self._object = obj
-    self._object_hash = self._make_hash(obj)
+    self._object_hash = hash(obj)
 
   def is_subtype_of(self, other: trace.TraceType) -> bool:
     return self == other
@@ -82,69 +75,6 @@ class GenericType(trace.TraceType):
 
   def __repr__(self):
     return f"{self.__class__.__name__}(obj={self._object!r})"
-
-  # TODO(b/195985838): Cleanup once Tensor protocol is implemented.
-  def _make_hash(self, elem):
-    """Deals with special cases while hashing arbitrary Python objects."""
-    try:
-      return hash(elem)
-    except TypeError:
-      # TODO(slebedev): consider using nest.
-      if isinstance(elem, tuple):
-        return hash(tuple(map(self._make_hash, elem)))
-
-      # TFE_Py_EncodeArg weakrefs arguments it does not recognize, and we expect
-      # all recognized types to be hashable.
-      assert isinstance(elem, weakref.ReferenceType)
-      v = elem()
-
-      if resource_variable_ops.is_resource_variable(v):
-        # We special case variables here to use unique_id as the cache key. This
-        # ensures we have to retrace whenever a different variable is passed in.
-        # This is needed to support cases where the user may use the id of a
-        # variable in the function perhaps as a lookup in a dictionary.
-        #
-        # This choice leads to more retracing when we could have possibly used
-        # the shape and dtype instead. However, we expect the number of
-        # variables in a program to be bounded, and correspondingly the number
-        # of retraces.
-        #
-        # Note we also include the class name to avoid collisions with strings.
-        return hash((v.__class__, v._unique_id))  # pylint: disable=protected-access
-
-      if self._is_ndarray(v):
-        # Numpy arrays are not hashable, but when calling functions we treat
-        # them in the same way as tf.Tensors.
-        if not hasattr(v, "shape") or not hasattr(v, "dtype"):
-          # TODO(tomhennigan) De-dup with _as_ndarray in _convert_numpy_inputs.
-          v = self._as_ndarray(v)
-        return hash(tensor_spec.TensorSpec(v.shape, v.dtype))
-
-      raise ValueError(
-          "Arguments to a tf.function must be a nested structure of "
-          "Tensors, Variables, NumPy arrays, or hashable Python "
-          f"objects, got {type(v)}.")
-
-  def _as_ndarray(self, value):
-    """Converts value to an ndarray, assumes _is_ndarray(value)."""
-    # TODO(tomhennigan) Support __array_interface__ too (including for
-    # _convert_numpy_inputs).
-    return value.__array__()
-
-  def _is_ndarray(self, value):
-    """Tests whether the given value is an ndarray (and not a TF tensor/var)."""
-    # TODO(tomhennigan) Support __array_interface__ too.
-    return hasattr(value, "__array__") and not (
-        isinstance(value, ops.Tensor) or
-        isinstance(value, resource_variable_ops.BaseResourceVariable) or
-        hasattr(value, "_should_act_as_resource_variable")
-
-        # For legacy reasons we do not automatically promote Numpy strings.
-        or isinstance(value, np.str_)
-        # NumPy dtypes have __array__ as unbound methods.
-        or isinstance(value, type)
-        # CompositeTensors should be flattened instead.
-        or isinstance(value, composite_tensor.CompositeTensor))
 
 
 # TODO(b/182990542): Trigger function cache to remove associated concrete
