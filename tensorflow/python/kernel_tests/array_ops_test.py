ab44982f55d13d03d003b1bcbc7c77d3b27a8df1
Isha Arkatkar <ishark@google.com>
2021-11-24 13:13:21 -0800

    Fix Integer overflow error in Dequantize op shape function, by adding a bound check on axis.
    PiperOrigin-RevId: 412121389
    Change-Id: I3088dbad9e90f9998d406b618c16694388a9dfb4

--- tensorflow/python/kernel_tests/array_ops_test.py
+++ tensorflow/python/kernel_tests/array_ops_test.py
@@ -1703,6 +1703,21 @@ class QuantizeAndDequantizeTest(test_util.TensorFlowTestCase):
       output_grad = gradient_checker_v2.compute_gradient(f, [input_tensor])
       self.assertAllClose(output_grad[0], np.zeros([1, 4, 4]))
 
+  def testOutOfBoundAxis(self):
+    input_tensor = constant_op.constant([1., 1.])
+    input_min = [0]
+    input_max = [1]
+    q_input, _, _ = array_ops.quantize(input_tensor, 0, 1, dtypes.qint32)
+    error = (errors.InvalidArgumentError, ValueError)
+    with self.assertRaisesRegex(error,
+                                r".*Axis must be less than input dimension.*"):
+      self.evaluate(
+          gen_array_ops.dequantize(
+              input=q_input,
+              min_range=input_min,
+              max_range=input_max,
+              axis=2**31 - 1))
+
 
 @test_util.run_all_in_graph_and_eager_modes
 class SortedSearchTest(test_util.TensorFlowTestCase):
