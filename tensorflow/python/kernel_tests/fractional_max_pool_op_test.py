d6f5f52c082704768ae1e34a163f7c22ffa2d119
Isha Arkatkar <ishark@google.com>
2021-11-24 16:03:38 -0800

    Fix potential divide by zero error when executing FractionalMaxPool, when pooling ratio is higher than input size for a particular dimension.
    PiperOrigin-RevId: 412151722
    Change-Id: I06e57cbb8eca43816eff79eac264fa7aae8f7163

--- tensorflow/python/kernel_tests/fractional_max_pool_op_test.py
+++ tensorflow/python/kernel_tests/fractional_max_pool_op_test.py
@@ -24,6 +24,7 @@ import numpy as np
 
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import errors
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import gen_nn_ops
@@ -306,6 +307,22 @@ class FractionalMaxPoolTest(test.TestCase):
       expected = self._GetExpectedFractionalMaxPoolResult(
           input_b, row_seq, col_seq, overlapping)
       self.assertSequenceEqual(expected.shape, actual.shape)
+
+  def testDeterminismExceptionThrowing(self):
+    tensor_shape = (5, 20, 20, 3)
+    rand_mat = self._PRNG.random_sample(tensor_shape) * 1000 - 500
+    with test_util.deterministic_ops():
+      with self.assertRaisesRegex(
+          ValueError, "requires a non-zero seed to be passed in when "
+          "determinism is enabled"):
+        nn_ops.fractional_max_pool_v2(rand_mat, [1, 1.5, 1.5, 1])
+      nn_ops.fractional_max_pool_v2(rand_mat, [1, 1.5, 1.5, 1], seed=1)
+
+      with self.assertRaisesRegex(ValueError,
+                                  'requires "seed" and "seed2" to be non-zero'):
+        nn_ops.fractional_max_pool(rand_mat, [1, 1.5, 1.5, 1])
+      nn_ops.fractional_max_pool(
+          rand_mat, [1, 1.5, 1.5, 1], seed=1, seed2=1, deterministic=True)
 
 
 class FractionalMaxPoolGradTest(test.TestCase):
