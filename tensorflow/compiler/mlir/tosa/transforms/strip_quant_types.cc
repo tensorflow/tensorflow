fcb6182eda042c9420b0a7601eee1729a2cac720
Aaron DeBattista <aaron.debattista@arm.com>
2021-11-25 13:50:50 +0000

    Refactor the strip_quant_types.cc inorder to only strip quantization types from values that are produced by TOSA and consumed by TOSA ops

--- tensorflow/compiler/mlir/tosa/transforms/strip_quant_types.cc
+++ tensorflow/compiler/mlir/tosa/transforms/strip_quant_types.cc
@@ -1,27 +1,26 @@
-/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+// Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ==============================================================================*/
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+// This pass removes the quantized type from output Tensors attached to TOSA Ops
+//
+// We only strip away the quantization element type from types in between two
+// tosa ops and if all successors of that value is tosa.
+//
+// Everything else is left as is since we do not know who
+// else will consume that value
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
-// This pass converts a TFLite uint8 graph to the int8 domain, with adaptors at
-// input and output tensors. This is needed because TOSA precision is
-// implemented in the int8 domain. This pass does:
-// 1. match TFL::QConst with uint8, generate TFL::QConst with int8 with value
-// remapped.
-// 2. insert tosa.RESCALE uint8 -> int8 if block argument (placeholder of graph)
-// is uint8 typed.
-// 3. insert tosa.RESCALE int8 -> uint8 if original returned tensor is uint8
-// typed.
 
 #include <climits>
 #include <cstddef>
@@ -39,11 +38,15 @@ limitations under the License.
 #include "mlir/Pass/PassRegistry.h"  // from @llvm-project
 #include "mlir/Support/LogicalResult.h"  // from @llvm-project
 #include "mlir/Transforms/DialectConversion.h"  // from @llvm-project
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/Support/LLVM.h"
+
 #include "tensorflow/compiler/mlir/tosa/transforms/legalize_common.h"
 #include "tensorflow/compiler/mlir/tosa/transforms/legalize_utils.h"
 #include "tensorflow/compiler/mlir/tosa/transforms/passes.h"
 
-#define PASS_NAME "tosa-convert-tfl-uint8"
+#define PASS_NAME "tosa-strip-quantization"
 #define DEBUG_TYPE PASS_NAME
 
 namespace mlir {
@@ -52,69 +55,28 @@ namespace {
 #define GEN_PASS_CLASSES
 #include "tensorflow/compiler/mlir/tosa/transforms/passes.h.inc"
 
-class StripQuantTypes : public TosaStripQuantTypesPassBase<StripQuantTypes> {
- public:
-  explicit StripQuantTypes() {}
-  void runOnFunction() override;
-};
+bool isOpTosa(mlir::Operation *op)
+{
+  return (isa<tosa::TosaDialect>(op->getDialect()));
+}
 
-class QuantTypeConverter : public TypeConverter {
- public:
-  static Type convertType(Type type) {
-    if (auto qType = type.dyn_cast<quant::QuantizedType>()) {
-      if (qType.isSigned() || qType.getStorageTypeIntegralWidth() != 8) {
-        return IntegerType::get(type.getContext(),
-                                qType.getStorageTypeIntegralWidth());
-      }
+Type convertType(Type type)
+{
+  if (auto qType = type.dyn_cast<quant::QuantizedType>()) {
+    return IntegerType::get(type.getContext(),
+                            qType.getStorageTypeIntegralWidth(),
+                            qType.isSigned() ? IntegerType::SignednessSemantics::Signed:
+                                               IntegerType::SignednessSemantics::Unsigned);
+  }
+  return type;
+}
 
-      return IntegerType::get(type.getContext(),
-                              qType.getStorageTypeIntegralWidth(),
-                              IntegerType::SignednessSemantics::Unsigned);
-    }
-    return type;
-  }
-  static Type convertTensor(RankedTensorType type) {
-    auto newType = RankedTensorType::get(type.getShape(),
-                                         convertType(type.getElementType()));
-    return newType;
-  }
-  explicit QuantTypeConverter() {
-    addConversion([](Type type) { return convertType(type); });
-    addConversion(convertTensor);
-  }
-};
-
-// Handles the type conversion component of the TypeConversion. This updates
-// conversion patterns that used the original Quant types to be updated to
-// the non-quant variants.
-class GenericTypeConvert : public ConversionPattern {
- public:
-  GenericTypeConvert(MLIRContext* context, TypeConverter& converter)
-      : ConversionPattern(converter, MatchAnyOpTypeTag(), 0, context) {}
-  LogicalResult matchAndRewrite(
-      Operation* op, ArrayRef<Value> operands,
-      ConversionPatternRewriter& rewriter) const override {
-    llvm::SmallVector<Type, 4> newResults;
-    if (isa<FuncOp>(op)) {
-      return failure();
-    }
-
-    (void)getTypeConverter()->convertTypes(op->getResultTypes(), newResults);
-    OperationState state(op->getLoc(), op->getName().getStringRef(), operands,
-                         newResults, op->getAttrs(), op->getSuccessors());
-    for (Region& r : op->getRegions()) {
-      Region* newRegion = state.addRegion();
-      rewriter.inlineRegionBefore(r, *newRegion, newRegion->begin());
-      TypeConverter::SignatureConversion result(newRegion->getNumArguments());
-      (void)getTypeConverter()->convertSignatureArgs(
-          newRegion->getArgumentTypes(), result);
-      rewriter.applySignatureConversion(newRegion, result);
-    }
-    Operation* newOp = rewriter.createOperation(state);
-    rewriter.replaceOp(op, newOp->getResults());
-    return success();
-  }
-};
+Type convertTensor(RankedTensorType type)
+{
+  auto newType = RankedTensorType::get(type.getShape(),
+                                       convertType(type.getElementType()));
+  return newType;
+}
 
 static bool isIllegalType(Type type) {
   if (type.isa<quant::QuantizedType>()) return true;
@@ -124,40 +86,64 @@ static bool isIllegalType(Type type) {
   return false;
 }
 
+struct StripQuantizationTypes : public RewritePattern {
+  explicit StripQuantizationTypes(MLIRContext *context)
+      : RewritePattern(MatchAnyOpTypeTag(), 1, context) {}
+
+  LogicalResult matchAndRewrite(Operation *op,
+                                PatternRewriter &rewriter) const override
+  {
+    if (not(isOpTosa(op)))
+        return failure();
+
+    bool matched = false;
+
+    for (auto result_value: op->getResults())
+    {
+      /*
+         Checks that the Type of the Value being stripped is actually illegal
+      */
+      bool type_needs_stripping = isIllegalType(result_value.getType());
+
+      /*
+         Checking that all consumers of the value are TOSA Ops
+      */
+      bool all_successors_tosa = true;
+      for (auto consumer: result_value.getUsers())
+        all_successors_tosa &= (isOpTosa(consumer));
+
+      if (all_successors_tosa and type_needs_stripping)
+      {
+        auto new_type = convertTensor(result_value.getType().dyn_cast<mlir::RankedTensorType>());
+        result_value.setType(new_type);
+        matched = true;
+      }
+    }
+
+    if (matched)
+       return success();
+    return failure();
+  }
+};
+
+class StripQuantTypes : public TosaStripQuantTypesPassBase<StripQuantTypes> {
+ public:
+  explicit StripQuantTypes() {}
+  void runOnFunction() override;
+};
+
 void StripQuantTypes::runOnFunction() {
-  QuantTypeConverter converter;
-  ConversionTarget target(getContext());
-
-  target.addIllegalDialect<quant::QuantizationDialect>();
-  // Operations are legal if they don't contain any illegal type.
-  target.markUnknownOpDynamicallyLegal([](Operation* op) {
-    if (auto funcOp = dyn_cast<FuncOp>(op)) {
-      for (Type type : funcOp.getType().getInputs()) {
-        if (isIllegalType(type)) return false;
-      }
-      for (Type type : funcOp.getType().getResults()) {
-        if (isIllegalType(type)) return false;
-      }
-    }
-    for (Type type : op->getResultTypes()) {
-      if (type && isIllegalType(type)) return false;
-    }
-    for (Type type : op->getOperandTypes()) {
-      if (type && isIllegalType(type)) return false;
-    }
-    return true;
-  });
-
   auto* ctx = &getContext();
   auto func = getFunction();
 
-  RewritePatternSet patterns(&getContext());
-  patterns.insert<GenericTypeConvert>(ctx, converter);
-  populateFuncOpTypeConversionPattern(patterns, converter);
+  RewritePatternSet patterns(ctx);
 
-  if (failed(applyFullConversion(func, target, std::move(patterns)))) {
-    signalPassFailure();
-  }
+  patterns.insert<StripQuantizationTypes>(ctx);
+
+  GreedyRewriteConfig config;
+  config.useTopDownTraversal = true;
+
+  (void)applyPatternsAndFoldGreedily(func, std::move(patterns), config);
 }
 
 }  // anonymous namespace
