c1cc46fd17ceeaa4384d36e1cc4ce13ed3392e09
Isha Arkatkar <ishark@google.com>
2021-11-30 14:50:38 -0800

    Fix integer overflow leading to divide by zero error in Unravel index kernel when dimensions product exceeds max int value.
    PiperOrigin-RevId: 413250052
    Change-Id: I9450b6e8acecd2e881a64b882e2b7c70e8e9289a

--- tensorflow/python/kernel_tests/array_ops_test.py
+++ tensorflow/python/kernel_tests/array_ops_test.py
@@ -1580,6 +1580,20 @@ class UnravelIndexTest(test_util.TensorFlowTestCase):
           dims = constant_op.constant([3, 0], dtype=dtype)
           self.evaluate(array_ops.unravel_index(indices=indices, dims=dims))
 
+  def testUnravelIndexIntegerOverflow(self):
+    with self.cached_session():
+      for dtype in [dtypes.int32, dtypes.int64]:
+        with self.assertRaisesRegex(
+            errors.InvalidArgumentError,
+            r"Input dims product is causing integer overflow"):
+          indices = constant_op.constant(-0x100000, dtype=dtype)
+          if dtype == dtypes.int32:
+            value = 0x10000000
+          else:
+            value = 0x7FFFFFFFFFFFFFFF
+          dims = constant_op.constant([value, value], dtype=dtype)
+          self.evaluate(array_ops.unravel_index(indices=indices, dims=dims))
+
 
 class GuaranteeConstOpTest(test_util.TensorFlowTestCase):
 
