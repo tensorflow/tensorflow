f9be04dee0702f0b07e6fc8a32accd3645140ffd
Prakalp Srivastava <prakalps@google.com>
2021-11-30 15:24:02 -0800

    Add HLO -> MHLO importer for AllToAll HLO.
    AllToAll HLO instruction can either have pre-split operands (and return a tuple) or a single operand that all-to-all splits across `split_dimension` into the number of replicas in a group. Only the latter case (array all-to-all) is supported in importer right now and the former (tuple all-to-all) is not supported yet because mhlo::AllToAllOp does not allow tuple output or variadic operands.
    
    PiperOrigin-RevId: 413257417
    Change-Id: Ibfeed33903017274c433781a7860edc6e4f84443

--- tensorflow/compiler/mlir/xla/hlo_function_importer.cc
+++ tensorflow/compiler/mlir/xla/hlo_function_importer.cc
@@ -600,7 +600,7 @@ StatusOr<mlir::Operation*> HloFunctionImporter::ImportInstructionImpl(
       int num_branches = instruction->branch_count();
       auto op = func_builder->create<mlir::mhlo::CaseOp>(
           loc, rets, operands, attributes, num_branches);
-      for (auto index_and_computation :
+      for (const auto& index_and_computation :
            llvm::enumerate(instruction->branch_computations())) {
         auto index = index_and_computation.index();
         HloComputation* computation = index_and_computation.value();
@@ -644,6 +644,39 @@ StatusOr<mlir::Operation*> HloFunctionImporter::ImportInstructionImpl(
       TF_RETURN_IF_ERROR(ImportAsRegion(*all_reduce->to_apply(),
                                         &all_reduce_op.computation()));
       return all_reduce_op.getOperation();
+    }
+    case HloOpcode::kAllToAll: {
+      // TODO(b/207152612): all-to-all HLO can either have pre-split operands
+      // (and returns a tuple) or a single operand that is split across
+      // `split_dimension` into the number of replicas in a group. Only the
+      // latter case (array all-to-all) is supported in importer right now and
+      // the former (tuple all-to-all) is not supported yet.
+      auto all_to_all = Cast<HloAllToAllInstruction>(instruction);
+      if (all_to_all->shape().IsTuple())
+        return tensorflow::errors::Unimplemented(
+            "Importing tuple all-to-all HLO is not supported yet");
+
+      // Check invariants of array all-to-all. This is a sanity check and is
+      // verified by the HLO verifier.
+      if (!all_to_all->split_dimension().has_value() || operands.size() != 1 ||
+          all_to_all->replica_groups().empty())
+        return tensorflow::errors::InvalidArgument(
+            "Array all-to-all should have a split dimension, one operand and "
+            "non-empty replica groups");
+
+      auto replica_groups_attr =
+          ConvertReplicaGroups(all_to_all->replica_groups(), builder_)
+              .getValue()
+              .cast<DenseIntElementsAttr>();
+      uint64_t split_dim = all_to_all->split_dimension().value();
+      uint64_t concat_dim = split_dim;
+      uint64_t split_count = all_to_all->replica_groups()[0].replica_ids_size();
+
+      return func_builder
+          ->create<mlir::mhlo::AllToAllOp>(loc, result_type, operands[0],
+                                           split_dim, concat_dim, split_count,
+                                           replica_groups_attr)
+          .getOperation();
     }
     case HloOpcode::kReduce: {
       // Operands in the first half are reduction inputs and the remaining
@@ -1136,7 +1169,7 @@ mlir::NamedAttribute HloFunctionImporter::ConvertSourceTargetPairs(
     const std::vector<std::pair<int64_t, int64_t>>& source_target_pairs,
     mlir::Builder* builder) {
   std::vector<int64_t> attr(source_target_pairs.size() * 2);
-  for (auto p : llvm::enumerate(source_target_pairs)) {
+  for (const auto& p : llvm::enumerate(source_target_pairs)) {
     attr[2 * p.index()] = p.value().first;
     attr[2 * p.index() + 1] = p.value().second;
   }
