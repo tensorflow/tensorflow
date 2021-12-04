8a5b75dc4a43a454faff9ecb1012ff130c538fa8
Mihai Maruseac <mihaimaruseac@google.com>
2021-12-04 10:32:23 -0800

    Check tensor to be scalar before accessing it via `scalar<T>`
    Prevents `CHECK`-fail if access is wrong.
    
    PiperOrigin-RevId: 414159273
    Change-Id: I0f52ac8e23387f5c1388240dc64e953b859aae43

--- tensorflow/core/kernels/boosted_trees/training_ops.cc
+++ tensorflow/core/kernels/boosted_trees/training_ops.cc
@@ -509,6 +509,10 @@ class BoostedTreesCenterBiasOp : public OpKernel {
     // Get the regularization options.
     const Tensor* l1_t;
     OP_REQUIRES_OK(context, context->input("l1", &l1_t));
+    OP_REQUIRES(
+        context, TensorShapeUtils::IsScalar(l1_t->shape()),
+        errors::InvalidArgument("l1 must be a scalar, got a tensor of shape ",
+                                l1_t->shape().DebugString()));
     const auto l1 = l1_t->scalar<float>()();
     const Tensor* l2_t;
     OP_REQUIRES_OK(context, context->input("l2", &l2_t));
