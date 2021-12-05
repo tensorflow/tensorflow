9df179cf35a234c014e753cc5c6ff0b22f9c6176
Mark Harfouche <mark.harfouche@gmail.com>
2021-12-05 11:56:15 -0500

    Remove wheel dependency alltogether

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -88,7 +88,6 @@ REQUIRED_PACKAGES = [
     'six >= 1.12.0',
     'termcolor >= 1.1.0',
     'typing_extensions >= 3.6.6',
-    'wheel >= 0.32.0',
     'wrapt >= 1.11.0',
     # These packages need to be pinned exactly as newer versions are
     # incompatible with the rest of the ecosystem
