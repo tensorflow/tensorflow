f6615e3c31e90661bdf6cfe65c01959717364fc3
Jaesung Chung <jaesung@google.com>
2021-12-07 08:50:42 -0800

    Fix to accept saved models without inputs
    PiperOrigin-RevId: 414738064
    Change-Id: I9f5b9916be5899224ec6609ca26549a56a40ea6f

--- tensorflow/lite/python/lite_v2_test.py
+++ tensorflow/lite/python/lite_v2_test.py
@@ -136,6 +136,29 @@ class FromConcreteFunctionTest(lite_v2_test_util.ModelTest):
     self.assertEqual(expected_value.numpy(), actual_value)
 
   @test_util.run_v2_only
+  def testModelWithoutInputs(self):
+
+    def _get_random_number_gen():
+      root = tracking.AutoTrackable()
+
+      @tf.function(input_signature=[])
+      def func():
+        return tf.random.uniform(shape=[1], dtype=tf.float32)
+
+      root.f = func
+      to_save = root.f.get_concrete_function()
+      return (root, to_save)
+
+    # Model with no input
+    root, concrete_func = _get_random_number_gen()
+
+    # Convert model.
+    converter = lite.TFLiteConverterV2.from_concrete_functions([concrete_func],
+                                                               root)
+    tflite_model = converter.convert()
+    self.assertIsNotNone(tflite_model)
+
+  @test_util.run_v2_only
   def testMultiFunctionModel(self):
     """Convert a single model in a multi-functional model."""
     root = self._getMultiFunctionModel()
