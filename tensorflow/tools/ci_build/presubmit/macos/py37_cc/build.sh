e72aafcb4ec5d0950943438205c448acff5a02f4
Mihai Maruseac <mihaimaruseac@google.com>
2021-12-08 08:54:23 -0800

    Fix macos build broken by invalid call to analyze profile
    PiperOrigin-RevId: 415014701
    Change-Id: If657891aa5ff1a1304f4f87d7565aeb96c2d8c58

--- tensorflow/tools/ci_build/presubmit/macos/py37_cc/build.sh
+++ tensorflow/tools/ci_build/presubmit/macos/py37_cc/build.sh
@@ -50,7 +50,7 @@ function run_build () {
     -- //tensorflow/python/... # ${DEFAULT_BAZEL_TARGETS} -//tensorflow/java/... -//tensorflow/lite/java/...
 
   # Print build time statistics, including critical path.
-  "${BAZEL_WRAPPER_PATH}" analyze-profile "${KOKORO_ARTIFACTS_DIR}/profile.json"
+  bazel analyze-profile "${KOKORO_ARTIFACTS_DIR}/profile.json"
 
   # Copy log to output to be available to GitHub
   ls -la "$(bazel info output_base)/java.log"
