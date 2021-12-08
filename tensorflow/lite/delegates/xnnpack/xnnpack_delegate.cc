a6d352f031f272972da0e6271de07e8b40cac74a
Georgiy Manuilov <george.manuilov@gmail.com>
2021-12-08 02:12:58 -0800

    Support per-channel quantized INT8 weights unpacking in XNNPACK delegate
    PiperOrigin-RevId: 414942969
    Change-Id: I6cd0ed79c518246800d2bcfe00953b8e8372c594

--- tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
+++ tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
@@ -3874,7 +3874,7 @@ TfLiteIntArray* Delegate::PrepareOpsToDelegate(TfLiteContext* context) {
       if (is_supported_int8_tensor) {
         const auto* quant_params = static_cast<const TfLiteAffineQuantization*>(
             input_tensor.quantization.params);
-        if (quant_params == nullptr || quant_params->scale->size != 1) {
+        if (quant_params == nullptr) {
           is_supported_int8_tensor = false;
         }
       }
@@ -4070,14 +4070,23 @@ TfLiteIntArray* Delegate::PrepareOpsToDelegate(TfLiteContext* context) {
                     input_tensor.quantization.params);
             // Such conditions have been checked when preparing to unpack INT8
             // tensors.
-            TFLITE_DCHECK(quant_params != nullptr &&
-                          quant_params->scale->size == 1);
+            TFLITE_DCHECK(quant_params != nullptr);
 
-            DequantizeInt8(reinterpret_cast<const int8_t*>(packed_data),
-                           reinterpret_cast<float*>(unpacked_data),
-                           GetTensorShape(&input_tensor),
-                           input_tensor.params.zero_point,
-                           input_tensor.params.scale);
+            if (quant_params->scale->size == 1) {
+              // Per-tensor quantization
+              DequantizeInt8(reinterpret_cast<const int8_t*>(packed_data),
+                             reinterpret_cast<float*>(unpacked_data),
+                             GetTensorShape(&input_tensor),
+                             input_tensor.params.zero_point,
+                             input_tensor.params.scale);
+            } else {
+              // Per-channel quantization
+              PerChannelDequantizeInt8(
+                  reinterpret_cast<const int8_t*>(packed_data),
+                  reinterpret_cast<float*>(unpacked_data),
+                  GetTensorShape(&input_tensor), quant_params->zero_point->data,
+                  quant_params->scale->data, quant_params->quantized_dimension);
+            }
             break;
           }
           default:
