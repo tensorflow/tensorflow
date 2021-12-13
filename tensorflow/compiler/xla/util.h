f00d8bdbe097826a5ac8c88bce507e86c7430a2a
David Majnemer <majnemer@google.com>
2021-12-13 01:12:50 -0800

    [XLA] Rename Round{Up,Down}ToNearest -> Round{Up,Down}To
    PiperOrigin-RevId: 415977461
    Change-Id: I9135961f7d7545c733909ceeeb4be8c0aa4894e6

--- tensorflow/compiler/xla/util.h
+++ tensorflow/compiler/xla/util.h
@@ -423,17 +423,17 @@ T CeilOfRatio(T dividend, T divisor) {
 }
 
 // Rounds the value up to a multiple of the divisor by first calling CeilOfRatio
-// then multiplying by the divisor. For example: RoundUpToNearest(13, 8) => 16
+// then multiplying by the divisor. For example: RoundUpTo(13, 8) => 16
 template <typename T>
-T RoundUpToNearest(T value, T divisor) {
+T RoundUpTo(T value, T divisor) {
   return CeilOfRatio(value, divisor) * divisor;
 }
 
 // Rounds the value down to a multiple of the divisor by first calling
 // FloorOfRatio then multiplying by the divisor. For example:
-// RoundDownToNearest(13, 8) => 8
+// RoundDownTo(13, 8) => 8
 template <typename T>
-T RoundDownToNearest(T value, T divisor) {
+T RoundDownTo(T value, T divisor) {
   return FloorOfRatio(value, divisor) * divisor;
 }
 
