e896767551abe2876db91d2b9daeb03df2c0dc3d
TensorFlower Gardener <gardener@tensorflow.org>
2021-12-16 19:26:39 -0800

    Merge pull request #52707 from elfringham:init_ops_test_fix
    PiperOrigin-RevId: 416941851
    Change-Id: Iefa5a9b841b053b36f6b105cd82c9d32d5e47850

--- tensorflow/core/kernels/sequence_ops.cc
+++ tensorflow/core/kernels/sequence_ops.cc
@@ -71,13 +71,19 @@ class RangeOp : public OpKernel {
           errors::InvalidArgument(
               "Requires start >= limit when delta < 0: ", start, "/", limit));
     }
-    int64 size = 0;
-    if (std::is_integral<T>::value) {
-      size = static_cast<int64>(
-          (std::abs(limit - start) + std::abs(delta) - 1) / std::abs(delta));
-    } else {
-      size = static_cast<int64>(std::ceil(std::abs((limit - start) / delta)));
-    }
+    auto size_auto = (std::is_integral<T>::value
+                          ? (Eigen::numext::abs(limit - start) +
+                             Eigen::numext::abs(delta) - T(1)) /
+                                Eigen::numext::abs(delta)
+                          : Eigen::numext::ceil(
+                                Eigen::numext::abs((limit - start) / delta)));
+    OP_REQUIRES(
+        context, size_auto <= std::numeric_limits<int64>::max(),
+        errors::InvalidArgument("Requires ((limit - start) / delta) <= ",
+                                std::numeric_limits<int64>::max()));
+
+    int64 size = static_cast<int64>(size_auto);
+
     TensorShape shape;
     OP_REQUIRES_OK(context, shape.AddDimWithStatus(size));
     Tensor* out = nullptr;
