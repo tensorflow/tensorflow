b820cdf9bc3633fa5dc49ae5a6d8806abd427d1f
Mehdi Amini <aminim@google.com>
2021-12-28 01:56:26 -0800

    Apply clang-tidy fixes for performance-for-range-copy in ral_legalize_to_llvm.cc (NFC)
    PiperOrigin-RevId: 418586297
    Change-Id: I9f4d2b9c57e320f815af8c1fa0af419dc244c83f

--- tensorflow/compiler/mlir/hlo/lib/Dialect/disc-ral/transforms/ral_legalize_to_llvm.cc
+++ tensorflow/compiler/mlir/hlo/lib/Dialect/disc-ral/transforms/ral_legalize_to_llvm.cc
@@ -293,7 +293,7 @@ Value DispatchOpToLLVMPattern::rewriteInsOutsOfDispatchOp(
   Value array_ptr = rewriter.create<LLVM::AllocaOp>(
       loc, llvm_pointer_pointer_type, array_size, /*alignment=*/0);
 
-  for (auto en : llvm::enumerate(argument_types)) {
+  for (const auto& en : llvm::enumerate(argument_types)) {
     Value index = rewriter.create<LLVM::ConstantOp>(
         loc, llvm_int32_type, rewriter.getI32IntegerAttr(en.index()));
     Value field_ptr = rewriter.create<LLVM::GEPOp>(
@@ -424,7 +424,7 @@ Value ConvertLaunchFuncOpToRalCallPattern::generateParamsArray(
       loc, llvm_pointer_pointer_type, array_size, /*alignment=*/0);
   Value zero = builder.create<LLVM::ConstantOp>(loc, llvm_int32_type,
                                                 builder.getI32IntegerAttr(0));
-  for (auto en : llvm::enumerate(arguments)) {
+  for (const auto& en : llvm::enumerate(arguments)) {
     Value index = builder.create<LLVM::ConstantOp>(
         loc, llvm_int32_type, builder.getI32IntegerAttr(en.index()));
     Value field_ptr = builder.create<LLVM::GEPOp>(
