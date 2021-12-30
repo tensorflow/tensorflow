9864cba2187df41d9fb287898ad6c4a1a7b16123
Mihai Maruseac <mihaimaruseac@google.com>
2021-12-30 14:26:01 -0800

    Remove the `-j 4` argument.
    Even limiting the jobs, we still get time outs. So the issue must be somewhere else
    
    PiperOrigin-RevId: 419006793
    Change-Id: I54bde117bcca292a51fd7c22e2f320e4d777a6f3

--- tensorflow/tools/ci_build/rel/macos/cpu_py37_nonpip.sh
+++ tensorflow/tools/ci_build/rel/macos/cpu_py37_nonpip.sh
@@ -38,7 +38,7 @@ source tensorflow/tools/ci_build/build_scripts/DEFAULT_TEST_TARGETS.sh
 # Run tests
 # Pass PYENV_VERSION since we're using pyenv. See b/182399580
 # TODO(b/212470799): Figure out why this is extremely slow / hangs.
-bazel test -j 4\
+bazel test \
   --config=release_cpu_macos \
   --action_env PYENV_VERSION="${PY_VERSION}" \
   --build_tag_filters="${tag_filters}" \
