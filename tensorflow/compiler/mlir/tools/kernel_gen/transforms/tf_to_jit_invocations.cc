77b3b9f036d5369e07ad911ba8ce65b7a9d4572f
kushanam <kahmadian@nvidia.com>
2022-01-04 08:08:26 -0800

    moving indexer to jit invocation

--- tensorflow/compiler/mlir/tools/kernel_gen/transforms/tf_to_jit_invocations.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/transforms/tf_to_jit_invocations.cc
@@ -21,6 +21,8 @@ limitations under the License.
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
+#include "mlir/Dialect/SCF/SCF.h"  // from @llvm-project
+#include "mlir/Dialect/Shape/IR/Shape.h"  // from @llvm-project
 #include "mlir/Dialect/StandardOps/IR/Ops.h"  // from @llvm-project
 #include "mlir/IR/Block.h"  // from @llvm-project
 #include "mlir/IR/BlockAndValueMapping.h"  // from @llvm-project
@@ -45,7 +47,7 @@ namespace mlir {
 namespace kernel_gen {
 namespace transforms {
 namespace {
-
+using shape::ShapeOfOp;
 bool IsTFOperation(Operation *op) {
   return op != nullptr &&
          op->getDialect() ==
@@ -251,6 +253,126 @@ struct TFToJITInvocationPass
   }
 };
 
+struct TFT64BitIndexerPattern : public RewritePattern {
+  explicit TFT64BitIndexerPattern(MLIRContext *ctx, bool index_64bit)
+      : RewritePattern(MatchAnyOpTypeTag(), /*benefit=*/1, ctx) {index_64bit_ = index_64bit;}
+
+  LogicalResult matchAndRewrite(Operation *op,
+                                PatternRewriter &rewriter) const override {
+    if (!IsTFOperation(op) || op->getParentOfType<tf_framework::JITCompileOp>() ||
+          !llvm::isa<FuncOp>(op->getParentOp()))
+    {
+        return failure();
+    }
+    SmallVector<Operation *, 16> cluster;
+    llvm::SmallPtrSet<Value, 16> operand_set, result_set;
+    Operation *it = op;
+    while (IsTFOperation(it)) {
+      // Find results that escape the JIT compile region.
+      for (auto &use : it->getUses()) {
+        if (!llvm::is_contained(cluster, use.getOwner()))
+          result_set.insert(use.get());
+      }
+
+      // Update JIT region operands and results.
+      for (Value v : it->getResults()) operand_set.erase(v);
+      for (Value v : it->getOperands()) operand_set.insert(v);
+      cluster.push_back(it);
+      it = it->getPrevNode();
+    }
+
+    // Introduce order to the operands and results.
+    auto operands = llvm::to_vector<16>(operand_set);
+    auto results = llvm::to_vector<16>(result_set);
+    auto operand_types = llvm::to_vector<16>(
+        llvm::map_range(operands, [](Value v) { return v.getType(); }));
+    auto result_types = llvm::to_vector<16>(
+        llvm::map_range(results, [](Value v) { return v.getType(); }));
+
+    // Create the JIT compile op.
+    auto loc = op->getLoc();
+    Value shape_size_limit = rewriter.create<arith::ConstantIndexOp>(loc, 4294967296);
+    auto arg = op->getOperands().front(); 
+    auto shape = rewriter.create<shape::ShapeOfOp>(loc, arg);
+    auto num_elems = rewriter.create<shape::NumElementsOp>(loc, shape);
+    Value coniditon_check_main = rewriter.create<arith::CmpIOp>(
+                    loc, arith::CmpIPredicate::sgt, num_elems, shape_size_limit);
+
+    Value conditional_path =
+                rewriter.create<scf::IfOp>(
+                  loc, op->getResultTypes(), coniditon_check_main,
+                  [&](OpBuilder &b, Location l) {
+                    auto jit_compile_op = rewriter.create<tf_framework::JITCompileOp>(
+                      loc, rewriter.getType<tf_framework::JITCallableType>(), llvm::None);
+                      BlockAndValueMapping bvm;
+                      {
+                        OpBuilder::InsertionGuard guard(rewriter);
+                        Block *block =
+                            rewriter.createBlock(&jit_compile_op.body(), {}, operand_types);
+                        for (auto it : llvm::zip(operands, block->getArguments()))
+                          bvm.map(std::get<0>(it), std::get<1>(it));
+                        rewriter.setInsertionPointToStart(block);
+                        for (Operation *it : llvm::reverse(cluster)) rewriter.clone(*it, bvm);
+                        auto mapped_results = llvm::to_vector<16>(
+                            llvm::map_range(results, [&](Value v) { return bvm.lookup(v); }));
+                        rewriter.create<tf_framework::JITCompileYieldOp>(loc, TypeRange{},
+                                                                        mapped_results);
+                      }
+                    auto jit_execute_op = rewriter.create<tf_framework::JITExecuteOp>(
+                        loc, result_types, Value(), jit_compile_op.result(), operands);
+                          b.create<scf::YieldOp>(l, jit_execute_op.results());
+                     },
+                     [&](OpBuilder &b, Location l) {
+                       Operation *op_backup = rewriter.clone(*op);
+                       b.create<scf::YieldOp>(l, op_backup->getResult(0));
+                     })
+                    .getResult(0);
+
+  rewriter.replaceOp(op, conditional_path);
+  return success();
+  }
+  private:
+  bool index_64bit_;
+};
+
+#define GEN_PASS_CLASSES
+#include "tensorflow/compiler/mlir/tools/kernel_gen/transforms/kernel_gen_passes.h.inc"
+
+struct IndexSizerPass
+    : public IndexSizerPassBase<IndexSizerPass> {
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<mlir::kernel_gen::tf_framework::TFFrameworkDialect, 
+    scf::SCFDialect, shape::ShapeDialect>();
+  }
+  explicit IndexSizerPass(llvm::ArrayRef<std::string> architectures,
+                                 llvm::ArrayRef<int64_t> tile_sizes,
+                                 llvm::ArrayRef<int64_t> unroll_factors,
+                                 int64_t max_supported_rank, bool enable_ftz,
+                                 bool index_64bit, bool cpu_codegen) {
+    architectures_ = architectures;
+    tile_sizes_ = tile_sizes;
+    unroll_factors_ = unroll_factors;
+    max_supported_rank_ = max_supported_rank;
+    enable_ftz_ = enable_ftz;
+    index_64bit_ = index_64bit;
+    cpu_codegen_ = cpu_codegen;
+  }
+
+  void runOnFunction() override {
+    MLIRContext *ctx = &getContext();
+    RewritePatternSet patterns(ctx);
+    auto architecture_refs = llvm::to_vector<16>(llvm::map_range(
+        architectures_, [](std::string &arch) { return StringRef(arch); }));
+    Populate64BitIndexerPatterns(
+        ctx, &patterns, architecture_refs, tile_sizes_, unroll_factors_,
+        max_supported_rank_, enable_ftz_, index_64bit_, cpu_codegen_);
+    if (failed(
+            applyPatternsAndFoldGreedily(getFunction(), std::move(patterns)))) {
+      return signalPassFailure();
+    }
+  }
+};
+
 }  // namespace
 
 void PopulateTFToJITInvocationPatterns(MLIRContext *ctx,
@@ -274,6 +396,30 @@ std::unique_ptr<FunctionPass> CreateTFToJITInvocationPass(
     int64_t max_supported_rank, bool enable_ftz, bool index_64bit, 
     bool cpu_codegen) {
   return std::make_unique<TFToJITInvocationPass>(
+      architectures, tile_sizes, unroll_factors, max_supported_rank, enable_ftz,
+      index_64bit, cpu_codegen);
+}
+
+void Populate64BitIndexerPatterns(MLIRContext *ctx,
+                                       RewritePatternSet *patterns,
+                                       llvm::ArrayRef<StringRef> architectures,
+                                       llvm::ArrayRef<int64_t> tile_sizes,
+                                       llvm::ArrayRef<int64_t> unroll_factors,
+                                       int64_t max_supported_rank,
+                                       bool enable_ftz, bool index_64bit,
+                                       bool cpu_codegen) {
+  patterns->insert<TFT64BitIndexerPattern>(ctx, index_64bit);
+  patterns->insert<PackJITCompileOpPattern>(ctx, architectures, tile_sizes,
+                                            unroll_factors, max_supported_rank,
+                                            enable_ftz, 
+                                            index_64bit, cpu_codegen);
+}
+
+std::unique_ptr<FunctionPass> Create64BitIndexerInvocationPass(
+    llvm::ArrayRef<std::string> architectures,
+    llvm::ArrayRef<int64_t> tile_sizes, llvm::ArrayRef<int64_t> unroll_factors,
+    int64_t max_supported_rank, bool enable_ftz, bool index_64bit, bool cpu_codegen) {
+  return std::make_unique<IndexSizerPass>(
       architectures, tile_sizes, unroll_factors, max_supported_rank, enable_ftz,
       index_64bit, cpu_codegen);
 }
