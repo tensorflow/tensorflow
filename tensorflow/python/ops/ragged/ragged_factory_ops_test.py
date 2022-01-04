eec58775994212912bd63a06316461d96364b591
Dan Moldovan <mdan@google.com>
2022-01-04 10:53:45 -0800

    Add type inference functions to the more commonly used list ops: create and append.
    PiperOrigin-RevId: 419633646
    Change-Id: Ie778dbe1a32927f019a33410825d540b7e6c0e0d

--- tensorflow/python/ops/ragged/ragged_factory_ops_test.py
+++ tensorflow/python/ops/ragged/ragged_factory_ops_test.py
@@ -30,54 +30,72 @@ from tensorflow.python.ops.ragged import ragged_factory_ops
 from tensorflow.python.platform import test
 
 
-def _ragged_int():
-  return ragged_factory_ops.constant([[3, 1, 4, 1], [], [5, 9, 2], [6], [],
-                                      [3, 1, 4, 1], [3, 1], [2, 1, 4, 1]],
-                                     dtype=dtypes.int64)
+def ragged_int64():
+  return ragged_factory_ops.constant(
+      [
+          [3, 1, 4, 1],
+          [],
+          [5, 9, 2],
+          [6],
+          [],
+          [3, 1, 4, 1],
+          [3, 1],
+          [2, 1, 4, 1],
+      ],
+      dtype=dtypes.int64,
+  )
 
 
-def _ragged_str():
-  return ragged_factory_ops.constant([['3', '1', '4', '1'], [], ['5', '9', '2'],
-                                      ['6'], [], ['3', '1', '4', '1'],
-                                      ['3', '1'], ['2', '1', '4', '1']])
+def ragged_str():
+  return ragged_factory_ops.constant([
+      ['3', '1', '4', '1'],
+      [],
+      ['5', '9', '2'],
+      ['6'],
+      [],
+      ['3', '1', '4', '1'],
+      ['3', '1'],
+      ['2', '1', '4', '1'],
+  ])
 
 
 class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
                            parameterized.TestCase):
 
-  @parameterized.named_parameters(
-      dict(testcase_name='Int', factory_func=_ragged_int),
-      dict(testcase_name='String', factory_func=_ragged_str))
-  def testRaggedWithMapFn(self, factory_func):
+  @parameterized.parameters(
+      (ragged_int64,),
+      (ragged_str,),
+  )
+  def testRaggedWithMapFn(self, ragged_factory):
 
     @def_function.function
-    def create_using_map_fn(inputs):
+    def map_fn_producer(inputs):
       return map_fn.map_fn_v2(lambda x: x, inputs)
 
-    t = factory_func()  # a ragged tensor containing a particular type
-    if t.dtype == dtypes.string:
-      self.skipTest('b/197903812: fix ragged tensor of string')
-    result = self.evaluate(create_using_map_fn(t))
-    self.assertAllEqual(self.evaluate(t).values, result.values)
+    t = ragged_factory()
+    result = self.evaluate(map_fn_producer(t))
+    self.assertAllEqual(t.values, result.values)
 
-  @parameterized.named_parameters(
-      dict(testcase_name='Int', factory_func=_ragged_int),
-      dict(testcase_name='String', factory_func=_ragged_str))
-  def testRaggedWithDistributedDataset(self, factory_func):
+  @parameterized.parameters(
+      (ragged_int64,),
+      (ragged_str,),
+  )
+  def testRaggedWithDistributedDataset(self, ragged_factory):
 
     @def_function.function
-    def _create_from_distributed_dataset(t):
+    def distributed_dataset_producer(t):
       strategy = mirrored_strategy.MirroredStrategy(['GPU:0', 'GPU:1'])
       ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
       dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
       ds = iter(dist_dataset)
-      return strategy.experimental_local_results(next(ds))
+      return strategy.experimental_local_results(next(ds))[0]
 
-    t = factory_func()  # a ragged tensor containing a particular type
+    t = ragged_factory()
     if t.dtype == dtypes.string:
       self.skipTest('b/194439197: fix ragged tensor of string')
-    result = _create_from_distributed_dataset(t)
-    self.assertAllEqual(self.evaluate(t[0]), self.evaluate(result[0][0]))
+
+    result = distributed_dataset_producer(t)
+    self.assertAllEqual(self.evaluate(t[0]), self.evaluate(result[0]))
 
 
 if __name__ == '__main__':
