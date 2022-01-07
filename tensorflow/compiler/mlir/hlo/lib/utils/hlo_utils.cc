64c1080360e23a092b8f8241dab2a715db717599
Mehdi Amini <aminim@google.com>
2022-01-07 12:17:03 -0800

    Apply clang-tidy fixes for llvm-else-after-return in hlo_utils.cc (NFC)
    PiperOrigin-RevId: 420345038
    Change-Id: I46ec3082929d2a37d8454f77d2b393063ea2dc15

--- tensorflow/compiler/mlir/hlo/lib/utils/hlo_utils.cc
+++ tensorflow/compiler/mlir/hlo/lib/utils/hlo_utils.cc
@@ -64,7 +64,8 @@ DenseElementsAttr GetScalarOfType(Type ty, int64_t raw_value) {
   if (auto int_ty = ty.dyn_cast<IntegerType>()) {
     APInt value(int_ty.getWidth(), static_cast<int64_t>(raw_value), true);
     return DenseElementsAttr::get(scalar_ty, value);
-  } else if (auto complex_ty = ty.dyn_cast<ComplexType>()) {
+  }
+  if (auto complex_ty = ty.dyn_cast<ComplexType>()) {
     Type complex_element_ty = complex_ty.getElementType();
     if (complex_element_ty.isF32()) {
       return DenseElementsAttr::get(
