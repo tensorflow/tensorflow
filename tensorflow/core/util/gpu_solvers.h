a10b5002ed7c213498f11f533789322368f68450
stevenireeves <reevessteven15@gmail.com>
2022-01-11 12:09:10 -0800

    ROCm 4.3.1 compat for the self_adjoin_eig_op.

--- tensorflow/core/util/gpu_solvers.h
+++ tensorflow/core/util/gpu_solvers.h
@@ -319,13 +319,15 @@ class GpuSolver {
   template <typename Scalar>
   Status Ungqr(int m, int n, int k, Scalar* dev_a, int lda,
                const Scalar* dev_tau, int* dev_lapack_info);
-  
+
+#if TF_ROCM_VERSION >= 40500
   // Hermitian (Symmetric) Eigen decomposition.
   template <typename Scalar>
   Status Heevd(rocblas_evect jobz, rocblas_fill uplo, int n,
                Scalar* dev_A, int lda,
                typename Eigen::NumTraits<Scalar>::Real* dev_W,
                int* dev_lapack_info);
+#endif
 
 #else //GOOGLE_CUDA
   // ====================================================================
