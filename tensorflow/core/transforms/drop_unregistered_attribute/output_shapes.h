cd528863eff0d264a36500f655c32e8c84795b73
Jacques Pienaar <jpienaar@google.com>
2022-01-11 19:13:44 -0800

    Remove unregistered _output_shapes attribute
    Add pass to drop unregistered _output_shapes attribute.
    
    PiperOrigin-RevId: 421181082
    Change-Id: If70d984f8b5d89fdc743f67e7f46fb70928b82a2

--- tensorflow/core/transforms/drop_unregistered_attribute/output_shapes.h
+++ tensorflow/core/transforms/drop_unregistered_attribute/output_shapes.h
@@ -13,21 +13,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_CORE_TRANSFORMS_PASS_REGISTRATION_H_
-#define TENSORFLOW_CORE_TRANSFORMS_PASS_REGISTRATION_H_
+#ifndef TENSORFLOW_CORE_TRANSFORMS_DROP_UNREGISTERED_ATTRIBUTE_OUTPUT_SHAPES_H_
+#define TENSORFLOW_CORE_TRANSFORMS_DROP_UNREGISTERED_ATTRIBUTE_OUTPUT_SHAPES_H_
 
 #include <memory>
 
-#include "tensorflow/core/transforms/drop_unregistered_attribute/output_shapes.h"
-#include "tensorflow/core/transforms/graph_to_func/graph_to_func_pass.h"
-#include "tensorflow/core/transforms/toposort/toposort_pass.h"
+#include "mlir/Pass/Pass.h"  // from @llvm-project
 
 namespace mlir {
 namespace tfg {
-/// Generate the code for registering passes for command-line parsing.
-#define GEN_PASS_REGISTRATION
-#include "tensorflow/core/transforms/passes.h.inc"
+
+std::unique_ptr<mlir::Pass> CreateDropOutputShapesAttrPass();
+
 }  // namespace tfg
 }  // namespace mlir
 
-#endif  // TENSORFLOW_CORE_TRANSFORMS_PASS_REGISTRATION_H_
+#endif  // TENSORFLOW_CORE_TRANSFORMS_DROP_UNREGISTERED_ATTRIBUTE_OUTPUT_SHAPES_H_
