b6be1b4eb46addb890939989c3948b2600d4ebce
Ashiq Imran <ashiq.imran@intel.com>
2022-01-13 17:27:34 -0800

    bf16 support for complementary erf

--- tensorflow/core/kernels/cwise_op_erfc.cc
+++ tensorflow/core/kernels/cwise_op_erfc.cc
@@ -11,7 +11,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-==============================================================================*/
+======================================================================================*/
 
 #include "tensorflow/core/kernels/cwise_ops_common.h"
 
