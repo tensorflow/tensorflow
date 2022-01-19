e1e7c6571f4b058aeead4f2f9aa1ff560a83daa8
A. Unique TensorFlower <gardener@tensorflow.org>
2022-01-19 08:35:04 -0800

    Fixes a memory error in RaggedTensorToVariantGradientOp caused by passing empty Tensors to ConcatCPU(). Before this change, undefined behavior occurs when computing the gradient of a map_fn op defined over a RaggedTensor that contains one or more empty rows: Tensorflow may terminate with a SIG* (observed in debug compilation mode) or silently corrupt data (observed in release compilation mode).
    PiperOrigin-RevId: 422821715
    Change-Id: I06a41e09eab40540e591d24692d9f24192324c92

--- tensorflow/core/kernels/ragged_tensor_to_variant_op.cc
+++ tensorflow/core/kernels/ragged_tensor_to_variant_op.cc
@@ -222,8 +222,7 @@ class RaggedTensorToVariantGradientOp : public OpKernel {
         TensorShape zeros_shape = dense_values_shape;
         zeros_shape.set_dim(0, piece_size);
         Tensor zero(value_dtype, zeros_shape);
-        zero.flat<VALUE_TYPE>() =
-            zero.flat<VALUE_TYPE>().constant(VALUE_TYPE());
+        zero.flat<VALUE_TYPE>().setZero();
         values.push_back(zero);
       }
     }
@@ -238,6 +237,8 @@ class RaggedTensorToVariantGradientOp : public OpKernel {
       std::vector<std::unique_ptr<ConstPiece>> pieces;
       pieces.reserve(values.size());
       for (const Tensor& t : values) {
+        // ConcatCPU expects non-empty tensors.
+        if (t.NumElements() == 0) continue;
         pieces.emplace_back(
             new ConstPiece(t.shaped<VALUE_TYPE, 2>({1, t.NumElements()})));
       }
