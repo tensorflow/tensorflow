d503f08dd6621fd29568fe919e3d743d6afa04e5
Shilpa Krish <shilpakrish@google.com>
2022-01-20 12:59:47 -0800

    [tf.data] Implement random access for memory based caching.
    PiperOrigin-RevId: 423138516
    Change-Id: Ie4a004b5665b3d97462340d5756c870b91451e38

--- tensorflow/core/data/root_dataset.cc
+++ tensorflow/core/data/root_dataset.cc
@@ -48,7 +48,8 @@ inline int64_t value_or_default(int64_t x, int64_t y, int64_t z) {
 }  // namespace
 
 // static
-Status RootDataset::FromOptions(DatasetBase* input, DatasetBase** output) {
+Status RootDataset::FromOptions(const DatasetBase* input,
+                                DatasetBase** output) {
   const Options& options = input->options();
   Params params;
   if (ShouldConfigureMaxIntraOpParallelism(options)) {
@@ -308,7 +309,7 @@ Status RootDataset::AsGraphDefInternal(SerializationContext* ctx,
 }
 
 #if !defined(IS_MOBILE_PLATFORM)
-Status FinalizeDataset(OpKernelContext* ctx, DatasetBase* input,
+Status FinalizeDataset(OpKernelContext* ctx, const DatasetBase* input,
                        DatasetBase** output) {
   const Options& options = input->options();
   absl::flat_hash_set<tstring> optimizations_enabled;
@@ -355,7 +356,7 @@ Status FinalizeDataset(OpKernelContext* ctx, DatasetBase* input,
   return Status::OK();
 }
 #else   // !IS_MOBILE_PLATFORM
-Status FinalizeDataset(OpKernelContext* ctx, DatasetBase* input,
+Status FinalizeDataset(OpKernelContext* ctx, const DatasetBase* input,
                        DatasetBase** output) {
   return RootDataset::FromOptions(input, output);
 }
