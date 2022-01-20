b8776faba5a94fc6526da253595284cc2e807b88
Frederik Gossen <frgossen@google.com>
2022-01-20 09:05:30 -0800

    Extract common code for autofusion reduction benchmarks
    PiperOrigin-RevId: 423081732
    Change-Id: Iefe2518c4696d5538b729af03776521a63d770f5

--- tensorflow/compiler/mlir/tfrt/benchmarks/sum_4d_op_benchmark.cc
+++ tensorflow/compiler/mlir/tfrt/benchmarks/sum_4d_op_benchmark.cc
@@ -22,71 +22,13 @@ limitations under the License.
 namespace tensorflow {
 namespace {
 
-std::string SumND(llvm::ArrayRef<int32_t> input_shape,
-                  llvm::ArrayRef<bool> dynamic_dims,
-                  llvm::ArrayRef<int32_t> dims_to_reduce) {
-  return GetReductionIR("tf.Sum", input_shape, dynamic_dims, dims_to_reduce,
-                        "f32");
-}
-
-template <size_t N>
-TensorShape ReducedTensorShape(TensorShape input_shape,
-                               std::array<int32_t, N> dims_to_reduce) {
-  std::vector<int64_t> result_shape;
-  int j = 0;
-  for (int i = 0; i < input_shape.dims(); ++i) {
-    if (j < dims_to_reduce.size() && i == dims_to_reduce[j]) {
-      j++;
-      continue;
-    }
-    result_shape.push_back(input_shape.dim_size(i));
-  }
-  return TensorShape(result_shape);
-}
-
-template <int32_t INPUT_RANK, size_t N_DIMS_TO_REDUCE>
-auto EigenSumND(std::array<int32_t, N_DIMS_TO_REDUCE> dims_to_reduce) {
-  return [dims_to_reduce](llvm::ArrayRef<Tensor> inputs,
-                          llvm::Optional<Eigen::ThreadPoolDevice> device) {
-    Tensor output(DT_FLOAT,
-                  ReducedTensorShape(inputs[0].shape(), dims_to_reduce));
-    auto in = inputs[0].tensor<float, INPUT_RANK>();
-    auto out = output.tensor<float, INPUT_RANK - N_DIMS_TO_REDUCE>();
-    out.setZero();
-    if (device.hasValue()) {
-      out.device(*device) = in.sum(dims_to_reduce);
-    } else {
-      out = in.sum(dims_to_reduce);
-    }
-  };
-}
-
-llvm::SmallVector<InputTensorSpec> Inputs(llvm::ArrayRef<ssize_t> input_shape) {
-  return {InputTensorSpec(DT_FLOAT, input_shape)};
-}
-
-#define INTS(...) __VA_ARGS__
-#define BOOLS(...) __VA_ARGS__
-
-#define BM(KIND, ...) BM_##KIND(__VA_ARGS__)->Arg(0);
-
-#define BM_SUITE(NAME, INPUT_RANK, INPUT_SHAPE, DYNAMIC_DIMS,              \
-                 N_DIMS_TO_REDUCE, DIMS_TO_REDUCE)                         \
-  BM(JitrtV, NAME, SumND({INPUT_SHAPE}, {DYNAMIC_DIMS}, {DIMS_TO_REDUCE}), \
-     "main", Inputs({INPUT_SHAPE}));                                       \
-  BM(Eigen, NAME,                                                          \
-     (EigenSumND<INPUT_RANK>(                                              \
-         std::array<int32_t, N_DIMS_TO_REDUCE>{DIMS_TO_REDUCE})),          \
-     Inputs({INPUT_SHAPE}));                                               \
-  BM(Tfrt, NAME, SumND({INPUT_SHAPE}, {DYNAMIC_DIMS}, {DIMS_TO_REDUCE}),   \
-     "main", Inputs({INPUT_SHAPE}))
-
 /// All dimensions are statically shaped.
 
-#define BM_4D_REDUCE_STATIC_ALL(M, N, K, L)                                 \
-  BM_SUITE(Sum4DReduceStaticAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
-           BOOLS(kStaticDim, kStaticDim, kStaticDim, kStaticDim), 4,        \
-           INTS(0, 1, 2, 3))
+#define BM_4D_REDUCE_STATIC_ALL(M, N, K, L)                                  \
+  BM_SUITE_SUM_F32(Sum4DReduceStaticAll_##M##_##N##_##K##_##L, 4,            \
+                   INTS(M, N, K, L),                                         \
+                   BOOLS(kStaticDim, kStaticDim, kStaticDim, kStaticDim), 4, \
+                   INTS(0, 1, 2, 3))
 
 BM_4D_REDUCE_STATIC_ALL(2, 80, 2, 80);
 BM_4D_REDUCE_STATIC_ALL(8, 6, 8, 6);
@@ -95,7 +37,7 @@ BM_4D_REDUCE_STATIC_ALL(80, 60, 80, 60);
 BM_4D_REDUCE_STATIC_ALL(81, 61, 81, 61);
 
 #define BM_4D_COL_LIKE_REDUCE_STATIC_ALL(M, N, K, L)                          \
-  BM_SUITE(                                                                   \
+  BM_SUITE_SUM_F32(                                                           \
       Sum4DColLikeReduceStaticAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
       BOOLS(kStaticDim, kStaticDim, kStaticDim, kStaticDim), 2, INTS(0, 1))
 
@@ -106,7 +48,7 @@ BM_4D_COL_LIKE_REDUCE_STATIC_ALL(80, 60, 80, 60);
 BM_4D_COL_LIKE_REDUCE_STATIC_ALL(81, 61, 81, 61);
 
 #define BM_4D_ROW_LIKE_REDUCE_STATIC_ALL(M, N, K, L)                          \
-  BM_SUITE(                                                                   \
+  BM_SUITE_SUM_F32(                                                           \
       Sum4DRowLikeReduceStaticAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
       BOOLS(kStaticDim, kStaticDim, kStaticDim, kStaticDim), 2, INTS(2, 3))
 
@@ -119,11 +61,11 @@ BM_4D_ROW_LIKE_REDUCE_STATIC_ALL(81, 61, 81, 61);
 /// All parallel dimensions are statically shaped, all reduction dimensions are
 /// dynamically shaped.
 
-#define BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(M, N, K, L)            \
-  BM_SUITE(Sum4DColLikeReduceDynamicReductionDims_##M##_##N##_##K##_##L, 4, \
-           INTS(M, N, K, L),                                                \
-           BOOLS(kDynamicDim, kDynamicDim, kStaticDim, kStaticDim), 2,      \
-           INTS(0, 1))
+#define BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(M, N, K, L)       \
+  BM_SUITE_SUM_F32(                                                    \
+      Sum4DColLikeReduceDynamicReductionDims_##M##_##N##_##K##_##L, 4, \
+      INTS(M, N, K, L),                                                \
+      BOOLS(kDynamicDim, kDynamicDim, kStaticDim, kStaticDim), 2, INTS(0, 1))
 
 BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(2, 80, 2, 80);
 BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(8, 6, 8, 6);
@@ -131,11 +73,11 @@ BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(80, 1, 80, 1);
 BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(80, 60, 80, 60);
 BM_4D_COL_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(81, 61, 81, 61);
 
-#define BM_4D_ROW_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(M, N, K, L)            \
-  BM_SUITE(Sum4DRowLikeReduceDynamicReductionDims_##M##_##N##_##K##_##L, 4, \
-           INTS(M, N, K, L),                                                \
-           BOOLS(kStaticDim, kStaticDim, kDynamicDim, kDynamicDim), 2,      \
-           INTS(2, 3))
+#define BM_4D_ROW_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(M, N, K, L)       \
+  BM_SUITE_SUM_F32(                                                    \
+      Sum4DRowLikeReduceDynamicReductionDims_##M##_##N##_##K##_##L, 4, \
+      INTS(M, N, K, L),                                                \
+      BOOLS(kStaticDim, kStaticDim, kDynamicDim, kDynamicDim), 2, INTS(2, 3))
 
 BM_4D_ROW_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(2, 80, 2, 80);
 BM_4D_ROW_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(8, 6, 8, 6);
@@ -146,11 +88,11 @@ BM_4D_ROW_LIKE_REDUCE_DYNAMIC_REDUCTION_DIMS(81, 61, 81, 61);
 /// Exactly one of the parallel dimensions is dynamically shaped, all other
 /// dimensions are statically shaped.
 
-#define BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(M, N, K, L)           \
-  BM_SUITE(Sum4DColLikeReduceOneDynamicParallelDim_##M##_##N##_##K##_##L, 4, \
-           INTS(M, N, K, L),                                                 \
-           BOOLS(kStaticDim, kStaticDim, kStaticDim, kDynamicDim), 2,        \
-           INTS(0, 1))
+#define BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(M, N, K, L)      \
+  BM_SUITE_SUM_F32(                                                     \
+      Sum4DColLikeReduceOneDynamicParallelDim_##M##_##N##_##K##_##L, 4, \
+      INTS(M, N, K, L),                                                 \
+      BOOLS(kStaticDim, kStaticDim, kStaticDim, kDynamicDim), 2, INTS(0, 1))
 
 BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(2, 80, 2, 80);
 BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(8, 6, 8, 6);
@@ -158,11 +100,11 @@ BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(80, 1, 80, 1);
 BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(80, 60, 80, 60);
 BM_4D_COL_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(81, 61, 81, 61);
 
-#define BM_4D_ROW_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(M, N, K, L)           \
-  BM_SUITE(Sum4DRowLikeReduceOneDynamicParallelDim_##M##_##N##_##K##_##L, 4, \
-           INTS(M, N, K, L),                                                 \
-           BOOLS(kStaticDim, kDynamicDim, kStaticDim, kStaticDim), 2,        \
-           INTS(2, 3))
+#define BM_4D_ROW_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(M, N, K, L)      \
+  BM_SUITE_SUM_F32(                                                     \
+      Sum4DRowLikeReduceOneDynamicParallelDim_##M##_##N##_##K##_##L, 4, \
+      INTS(M, N, K, L),                                                 \
+      BOOLS(kStaticDim, kDynamicDim, kStaticDim, kStaticDim), 2, INTS(2, 3))
 
 BM_4D_ROW_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(2, 80, 2, 80);
 BM_4D_ROW_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(8, 6, 8, 6);
@@ -174,10 +116,11 @@ BM_4D_ROW_LIKE_REDUCE_ONE_DYNAMIC_PARALLEL_DIM(81, 61, 81, 61);
 /// parallel dimension is statically shaped, all reduction dimensions are
 /// dynamically shaped.
 
-#define BM_4D_REDUCE_DYNAMIC_ALL(M, N, K, L)                                 \
-  BM_SUITE(Sum4DReduceDynamicAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
-           BOOLS(kDynamicDim, kDynamicDim, kDynamicDim, kDynamicDim), 4,     \
-           INTS(0, 1, 2, 3))
+#define BM_4D_REDUCE_DYNAMIC_ALL(M, N, K, L)                                  \
+  BM_SUITE_SUM_F32(Sum4DReduceDynamicAll_##M##_##N##_##K##_##L, 4,            \
+                   INTS(M, N, K, L),                                          \
+                   BOOLS(kDynamicDim, kDynamicDim, kDynamicDim, kDynamicDim), \
+                   4, INTS(0, 1, 2, 3))
 
 BM_4D_REDUCE_DYNAMIC_ALL(2, 80, 2, 80);
 BM_4D_REDUCE_DYNAMIC_ALL(8, 6, 8, 6);
@@ -186,7 +129,7 @@ BM_4D_REDUCE_DYNAMIC_ALL(80, 60, 80, 60);
 BM_4D_REDUCE_DYNAMIC_ALL(81, 61, 81, 61);
 
 #define BM_4D_COL_LIKE_REDUCE_DYNAMIC_ALL(M, N, K, L)                          \
-  BM_SUITE(                                                                    \
+  BM_SUITE_SUM_F32(                                                            \
       Sum4DColLikeReduceDynamicAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
       BOOLS(kDynamicDim, kDynamicDim, kStaticDim, kDynamicDim), 2, INTS(0, 1))
 
@@ -197,7 +140,7 @@ BM_4D_COL_LIKE_REDUCE_DYNAMIC_ALL(80, 60, 80, 60);
 BM_4D_COL_LIKE_REDUCE_DYNAMIC_ALL(81, 61, 81, 61);
 
 #define BM_4D_ROW_LIKE_REDUCE_DYNAMIC_ALL(M, N, K, L)                          \
-  BM_SUITE(                                                                    \
+  BM_SUITE_SUM_F32(                                                            \
       Sum4DRowLikeReduceDynamicAll_##M##_##N##_##K##_##L, 4, INTS(M, N, K, L), \
       BOOLS(kStaticDim, kDynamicDim, kDynamicDim, kDynamicDim), 2, INTS(2, 3))
 
