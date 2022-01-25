f8051626a9f7df43d4df26a1d9f8d4b454bbac77
Weiyi Wang <weiyiw@google.com>
2022-01-25 10:50:32 -0800

    [tf.lite] Add quantization support to GELU op
    PiperOrigin-RevId: 424125347
    Change-Id: Ia3be062480f2154790840a26adc78bb7505ea453

--- tensorflow/lite/tools/versioning/op_version.cc
+++ tensorflow/lite/tools/versioning/op_version.cc
@@ -821,6 +821,12 @@ int GetBuiltinOperatorVersion(const OpSignature& op_sig) {
     case BuiltinOperator_WHERE:
       if (op_sig.inputs.at(0).type == kTfLiteBool) return 1;
       return 2;
+    case BuiltinOperator_GELU:
+      if (op_sig.inputs.at(0).type == kTfLiteInt8 ||
+          op_sig.inputs.at(0).type == kTfLiteUInt8) {
+        return 2;
+      }
+      return 1;
     default:
       return 1;
   }
