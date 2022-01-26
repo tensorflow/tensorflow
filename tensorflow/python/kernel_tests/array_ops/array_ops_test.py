ed16bffd83969bf5e48f717bdb970c18e78c5f96
A. Unique TensorFlower <gardener@tensorflow.org>
2022-01-26 11:42:12 -0800

    Better way to pick the dtype for strided slice indices.
    PiperOrigin-RevId: 424403010
    Change-Id: I5d9d63344c568179df68a41f9b7f011433138592

--- tensorflow/python/kernel_tests/array_ops/array_ops_test.py
+++ tensorflow/python/kernel_tests/array_ops/array_ops_test.py
@@ -844,25 +844,24 @@ class StridedSliceTest(test_util.TensorFlowTestCase):
     def _int16(i):
       return constant_op.constant(i, dtype=dtypes.int16)
 
-    def _int64(i):
-      return constant_op.constant(i, dtype=dtypes.int64)
-
     for tensor_type in STRIDED_SLICE_TYPES:
       with self.subTest(tensor_type=tensor_type, use_gpu=True):
         checker = StridedSliceChecker(
             self, StridedSliceChecker.REF_TENSOR, tensor_type=tensor_type)
 
-        _ = checker[_int16(1)]
+        with self.assertRaises(Exception):
+          _ = checker[::_int16(1), ::_int16(5), ::_int16(2)]
 
         with self.assertRaises(Exception):
-          _ = checker[_int16(1)::1, :, 1:_int64(3):2]
+          _ = checker[_int16(1)::1, :, :]
+
         with self.assertRaises(Exception):
           _ = checker[:, _int16(1):_int16(5):-1, :]
-        with self.assertRaises(Exception):
-          _ = checker[::_int64(1), _int64(1):10:_int16(3), ::_int64(2)]
 
-        _ = checker[::_int16(1), _int16(1)::_int16(5), ::2]
         _ = checker[_int16(1):_int16(5):_int16(2), 1:2, :]
+        _ = checker[_int16(1):_int16(5):_int16(2),
+                    _int16(0):_int16(5):_int16(1),
+                    _int16(0):_int16(4):_int16(2)]
 
 
 class StridedSliceShapeTest(test_util.TensorFlowTestCase):
