a3cd1d9c6f10bdae1fdca4dc0fbec11c5c61bba1
Patrice Vignola <vignola.patrice@gmail.com>
2022-01-27 01:48:06 -0800

    Move out of CUDA ifdef

--- tensorflow/core/kernels/sequence_ops.cc
+++ tensorflow/core/kernels/sequence_ops.cc
@@ -137,6 +137,8 @@ TF_CALL_float(REGISTER_GPU_KERNEL);
 TF_CALL_double(REGISTER_GPU_KERNEL);
 TF_CALL_int64(REGISTER_GPU_KERNEL);
 
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+
 // Special case to execute int32 on the host with host output.
 REGISTER_KERNEL_BUILDER(Name("Range")
                             .Device(DEVICE_DEFAULT)
@@ -146,8 +148,6 @@ REGISTER_KERNEL_BUILDER(Name("Range")
                             .HostMemory("output")
                             .TypeConstraint<int32_t>("Tidx"),
                         RangeOp<CPUDevice, int32_t>);
-
-#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
 #undef REGISTER_KERNEL
 #undef REGISTER_CPU_KERNEL
