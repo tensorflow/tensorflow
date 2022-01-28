13baf918202c5a9052aa9c4940be334c0cef912d
Mihai Maruseac <mihaimaruseac@google.com>
2022-01-28 10:56:47 -0800

    Remove TODOs where it is safe to do so.
    PiperOrigin-RevId: 424908780
    Change-Id: I800780dbe554862eb41d243226e8ebe940b90c2b

--- tensorflow/python/debug/examples/v1/debug_tflearn_iris.py
+++ tensorflow/python/debug/examples/v1/debug_tflearn_iris.py
@@ -59,7 +59,6 @@ def main(_):
   hooks = []
   if FLAGS.debug:
     if FLAGS.use_random_config_path:
-      # TODO(mihaimaruseac): Safe to ignore fd here?
       _, config_file_path = tempfile.mkstemp(".tfdbg_config")
     else:
       config_file_path = None
