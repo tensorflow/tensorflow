5ad4301046bf785abbc7ff9ff474df523781d4a6
Yating Jing <ytjing@google.com>
2022-02-07 08:38:24 -0800

    Invoke TensorBoard text v2 in `tf.compat.v1.summary.text`.
    PiperOrigin-RevId: 426926433
    Change-Id: I58b37be8c380627abba96a2df299eb19c552e378

--- tensorflow/python/summary/summary.py
+++ tensorflow/python/summary/summary.py
@@ -490,8 +490,19 @@ def text(name, tensor, collections=None):
     ValueError: If tensor has the wrong type.
 
   @compatibility(TF2)
-  This API is not compatible with eager execution or `tf.function`. To migrate
-  to TF2, please use `tf.summary.text` instead. Please check
+  For compatibility purposes, when invoked in TF2 where the outermost context is
+  eager mode, this API will check if there is a suitable TF2 summary writer
+  context available, and if so will forward this call to that writer instead. A
+  "suitable" writer context means that the writer is set as the default writer,
+  and there is an associated non-empty value for `step` (see
+  `tf.summary.SummaryWriter.as_default`, or alternatively
+  `tf.summary.experimental.set_step`). For the forwarded call, the arguments
+  here will be passed to the TF2 implementation of `tf.summary.text`, and the
+  return value will be an empty bytestring tensor, to avoid duplicate summary
+  writing. This forwarding is best-effort and not all arguments will be
+  preserved.
+
+  To migrate to TF2, please use `tf.summary.text` instead. Please check
   [Migrating tf.summary usage to
   TF 2.0](https://www.tensorflow.org/tensorboard/migrate#in_tf_1x) for concrete
   steps for migration.
@@ -516,6 +527,17 @@ def text(name, tensor, collections=None):
     raise ValueError('Expected tensor %s to have dtype string, got %s' %
                      (tensor.name, tensor.dtype))
 
+  # Special case: invoke v2 op for TF2 users who have a v2 writer.
+  if _should_invoke_v2_op():
+    # Defer the import to happen inside the symbol to prevent breakage due to
+    # missing dependency.
+    from tensorboard.summary.v2 import text as text_v2  # pylint: disable=g-import-not-at-top
+    text_v2(name=name, data=tensor, step=_get_step_for_v2())
+    # Return an empty Tensor, which will be acceptable as an input to the
+    # `tf.compat.v1.summary.merge()` API.
+    return _constant_op.constant(b'')
+
+  # Fall back to legacy v1 text implementation.
   summary_metadata = _SummaryMetadata(
       plugin_data=_SummaryMetadata.PluginData(plugin_name='text'))
   t_summary = tensor_summary(
