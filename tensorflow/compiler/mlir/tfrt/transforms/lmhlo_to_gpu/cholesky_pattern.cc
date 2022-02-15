ec50b08c01249a3c2201ac8767283b97a95ef1ea
Rohit Santhanam <rohit.santhanam@amd.com>
2022-02-15 00:05:00 +0000

    Fixes to enable the cholesky unit test for BEF thunk on ROCm.

--- tensorflow/compiler/mlir/tfrt/transforms/lmhlo_to_gpu/cholesky_pattern.cc
+++ tensorflow/compiler/mlir/tfrt/transforms/lmhlo_to_gpu/cholesky_pattern.cc
@@ -41,8 +41,7 @@ struct CholeskyRewritePattern
     Value context = rewriter.create<tfrt::gpu::StreamGetContextOp>(loc, stream);
     auto handle = rewriter.create<tfrt::gpu::SolverCreateOp>(loc, context);
 
-    cublasFillMode_t fill_mode =
-        op.is_lower() ? CUBLAS_FILL_MODE_LOWER : CUBLAS_FILL_MODE_UPPER;
+    auto fill_mode = op.is_lower() ? kBlasFillModeLower : kBlasFillModeUpper;
 
     mlir::Type element_type =
         op.input().getType().cast<mlir::MemRefType>().getElementType();
