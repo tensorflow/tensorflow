905b599bc5d63e30ec24288bb332003a69198047
Alan Liu <liualan@google.com>
2022-02-15 18:15:46 -0800

    Add internal API `image_ops.internal_pad_to_bounding_box`.
    PiperOrigin-RevId: 428923500
    Change-Id: I13d355778692b6be32344df81279fd9efa7c806a

--- tensorflow/python/ops/image_ops_impl.py
+++ tensorflow/python/ops/image_ops_impl.py
@@ -1045,6 +1045,53 @@ def pad_to_bounding_box(image, offset_height, offset_width, target_height,
       `target_*` arguments, or either `offset_height` or `offset_width` is
       negative.
   """
+  return pad_to_bounding_box_internal(
+      image,
+      offset_height,
+      offset_width,
+      target_height,
+      target_width,
+      check_dims=True)
+
+
+# TODO(b/190099338) Remove this internal method and remap call sites to call
+# image_ops.pad_to_bounding_box when asserts are no longer serialized. See also
+# b/204377079#comment6 for more context.
+def pad_to_bounding_box_internal(image, offset_height, offset_width,
+                                 target_height, target_width, check_dims):
+  """Pad `image` with zeros to the specified `height` and `width`.
+
+  Adds `offset_height` rows of zeros on top, `offset_width` columns of
+  zeros on the left, and then pads the image on the bottom and right
+  with zeros until it has dimensions `target_height`, `target_width`.
+
+  This op does nothing if `offset_*` is zero and the image already has size
+  `target_height` by `target_width`.
+
+  Args:
+    image: 4-D Tensor of shape `[batch, height, width, channels]` or 3-D Tensor
+      of shape `[height, width, channels]`.
+    offset_height: Number of rows of zeros to add on top.
+    offset_width: Number of columns of zeros to add on the left.
+    target_height: Height of output image.
+    target_width: Width of output image.
+    check_dims: If True, assert that dimensions are non-negative and in range.
+      In multi-GPU distributed settings, assertions can cause program slowdown.
+      Setting this parameter to `False` avoids this, resulting in faster speed
+      in some situations, with the tradeoff being that some error checking is
+      not happening.
+
+  Returns:
+    If `image` was 4-D, a 4-D float Tensor of shape
+    `[batch, target_height, target_width, channels]`
+    If `image` was 3-D, a 3-D float Tensor of shape
+    `[target_height, target_width, channels]`
+
+  Raises:
+    ValueError: If the shape of `image` is incompatible with the `offset_*` or
+      `target_*` arguments, or either `offset_height` or `offset_width` is
+      negative. Not raised if `check_dims` is `False`.
+  """
   with ops.name_scope(None, 'pad_to_bounding_box', [image]):
     image = ops.convert_to_tensor(image, name='image')
 
@@ -1062,22 +1109,23 @@ def pad_to_bounding_box(image, offset_height, offset_width, target_height,
           '\'image\' (shape %s) must have either 3 or 4 dimensions.' %
           image_shape)
 
-    assert_ops = _CheckAtLeast3DImage(image, require_static=False)
     batch, height, width, depth = _ImageDimensions(image, rank=4)
 
     after_padding_width = target_width - offset_width - width
 
     after_padding_height = target_height - offset_height - height
 
-    assert_ops += _assert(offset_height >= 0, ValueError,
-                          'offset_height must be >= 0')
-    assert_ops += _assert(offset_width >= 0, ValueError,
-                          'offset_width must be >= 0')
-    assert_ops += _assert(after_padding_width >= 0, ValueError,
-                          'width must be <= target - offset')
-    assert_ops += _assert(after_padding_height >= 0, ValueError,
-                          'height must be <= target - offset')
-    image = control_flow_ops.with_dependencies(assert_ops, image)
+    if check_dims:
+      assert_ops = _CheckAtLeast3DImage(image, require_static=False)
+      assert_ops += _assert(offset_height >= 0, ValueError,
+                            'offset_height must be >= 0')
+      assert_ops += _assert(offset_width >= 0, ValueError,
+                            'offset_width must be >= 0')
+      assert_ops += _assert(after_padding_width >= 0, ValueError,
+                            'width must be <= target - offset')
+      assert_ops += _assert(after_padding_height >= 0, ValueError,
+                            'height must be <= target - offset')
+      image = control_flow_ops.with_dependencies(assert_ops, image)
 
     # Do not pad on the depth dimensions.
     paddings = array_ops.reshape(
