19f7617b63e536b5917ea85a5431d016aa6a5bcb
Loren Maggiore <loreno@google.com>
2022-02-17 07:49:01 -0800

    Create `xla_gpu_nccl_termination_timeout_seconds` debug flag to terminate from WaitAndLogIfStuck in nccl_utils.
    PiperOrigin-RevId: 429308209

--- tensorflow/compiler/xla/debug_options_flags.cc
+++ tensorflow/compiler/xla/debug_options_flags.cc
@@ -82,6 +82,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_bef_executable(false);
   opts.set_xla_gpu_bef_thunk(false);
+  opts.set_xla_gpu_nccl_termination_timeout_seconds(-1);
 
   return opts;
 }
@@ -697,6 +698,12 @@ static void AllocateFlags() {
       bool_setter_for(&DebugOptions::set_xla_gpu_bef_thunk),
       flag_values->xla_gpu_bef_thunk(),
       "Whether to enable XLIR to compile thunks to TFRT BEF."));
+  flag_objects->push_back(tensorflow::Flag(
+      "xla_gpu_nccl_termination_timeout_seconds",
+      int64_setter_for(
+          &DebugOptions::set_xla_gpu_nccl_termination_timeout_seconds),
+      flag_values->xla_gpu_nccl_termination_timeout_seconds(),
+      "Timeout in seconds before terminating jobs stuck in NCCL Rendezvous."));
 
   ParseFlagsFromEnvAndDieIfUnknown("XLA_FLAGS", *flag_objects);
 }  // NOLINT(readability/fn_size)
