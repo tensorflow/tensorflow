cb951598346f984d881b4d9cd6345d55ad67f812
Katherine Wu <kathywu@google.com>
2022-02-24 12:59:31 -0800

    Fix bug with loading checkpoints with the `root` arg and attached dependencies.
    PiperOrigin-RevId: 430773240

--- tensorflow/python/training/tracking/util_test.py
+++ tensorflow/python/training/tracking/util_test.py
@@ -998,6 +998,37 @@ class CheckpointingTests(parameterized.TestCase, test.TestCase):
     new_root.v = variables_lib.Variable(1.0)
     self.assertEmpty(new_root._deferred_dependencies, 1)
 
+  def test_root_arg(self):
+    root = tracking.AutoTrackable()
+    root.v = variables_lib.Variable(1)
+    w = variables_lib.Variable(2)
+    y = variables_lib.Variable(3)
+    root_ckpt = trackable_utils.Checkpoint(root=root, w=w, y=y)
+
+    root2 = tracking.AutoTrackable()
+    root2.w = variables_lib.Variable(4)
+    v2 = variables_lib.Variable(5)
+    z = variables_lib.Variable(6)
+    root2_ckpt = trackable_utils.Checkpoint(root=root2,
+                                            v=v2,
+                                            z=z)
+
+    root_save_path = root_ckpt.save(os.path.join(self.get_temp_dir(),
+                                                 "root_ckpt"))
+    root2_save_path = root2_ckpt.save(os.path.join(self.get_temp_dir(),
+                                                   "root2_ckpt"))
+
+    root_ckpt.restore(root2_save_path)
+    root2_ckpt.restore(root_save_path)
+
+    self.assertEqual(root.v.numpy(), 5)
+    self.assertEqual(w.numpy(), 4)
+    self.assertEqual(y.numpy(), 3)
+
+    self.assertEqual(root2.w.numpy(), 2)
+    self.assertEqual(v2.numpy(), 1)
+    self.assertEqual(z.numpy(), 6)
+
 
 class TemplateTests(parameterized.TestCase, test.TestCase):
 
