7271faf713b9817da8f4c924df2f1dee74396872
A. Unique TensorFlower <gardener@tensorflow.org>
2022-03-02 09:34:35 -0800

    Add tf.random.experimental.index_shuffle().
    It maps an index in [0, maxval] to a new random index in [0, maxval]. This allows users to suffle a list {0, 1, ..., n} without materializing the full list in memory.
    
    The operation is stateless and does not use the global random seed. The current implementation only works on CPU.
    
    PiperOrigin-RevId: 431959476

--- tensorflow/python/eager/pywrap_gradient_exclusions.cc
+++ tensorflow/python/eager/pywrap_gradient_exclusions.cc
@@ -50,7 +50,7 @@ auto OpGradientInfoInit(const T &a) {
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 363> a = {{
+  static std::array<OpIndexInfo, 362> a = {{
       {"Acosh"},
       {"AllToAll", 1, {0}},
       {"ApproximateEqual"},
@@ -237,7 +237,6 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
       {"QueueSize"},
       {"RaggedRange"},
       {"RandomCrop"},
-      {"RandomIndexShuffle"},
       {"RandomStandardNormal"},
       {"RandomUniform"},
       {"Range"},
@@ -426,7 +425,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 481> a = {{
+  static std::array<OpIndexInfo, 480> a = {{
       {"Abs"},
       {"AccumulateNV2"},
       {"Acos"},
@@ -692,7 +691,6 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
       {"RaggedTensorToTensor"},
       {"RaggedTensorToVariant"},
       {"RandomCrop"},
-      {"RandomIndexShuffle"},
       {"RandomStandardNormal"},
       {"RandomUniform"},
       {"Range"},
