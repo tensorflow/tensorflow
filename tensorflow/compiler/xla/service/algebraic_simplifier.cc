b13f4a1c4d2b814778510505de428bf510ad168f
A. Unique TensorFlower <gardener@tensorflow.org>
2022-03-01 16:07:25 -0800

    Add simplification to replace Use(ReduceMin(Arg) with Use(Gte(ReduceArgMin(), 0)).
    PiperOrigin-RevId: 431798744

--- tensorflow/compiler/xla/service/algebraic_simplifier.cc
+++ tensorflow/compiler/xla/service/algebraic_simplifier.cc
@@ -175,6 +175,12 @@ static bool IsScalarConstantNegInf(const HloInstruction* hlo) {
                           LiteralUtil::MinValue(hlo->shape().element_type()));
 }
 
+static bool IsScalarConstantInf(const HloInstruction* hlo) {
+  return !primitive_util::IsComplexType(hlo->shape().element_type()) &&
+         IsScalarConstant(hlo,
+                          LiteralUtil::MaxValue(hlo->shape().element_type()));
+}
+
 bool IsNonNegative(const HloInstruction* hlo,
                    const AlgebraicSimplifierOptions& options) {
   // Utility only handles real types.
@@ -4655,6 +4661,59 @@ Status AlgebraicSimplifierVisitor::HandleDynamicUpdateSlice(
   return Status::OK();
 }
 
+static bool MatchArgMinMax(const HloInstruction* hlo, bool is_max) {
+  // Create matcher for shared sub-expression.
+  auto value_pred = m::OrAnyOrder(
+      m::Compare(m::Parameter(0), m::Parameter(2))
+          .WithComparisonDirection(is_max ? ComparisonDirection::kGt
+                                          : ComparisonDirection::kLt),
+      m::Compare(m::Parameter(0), m::Parameter(0))
+          .WithComparisonDirection(ComparisonDirection::kNe));
+
+  // Match on argmax reduction computation.
+  return Match(
+      hlo,
+      m::Tuple(
+          m::Select(value_pred, m::Parameter(0), m::Parameter(2)),
+          m::Select(
+              m::OrAnyOrder(
+                  value_pred,
+                  m::And(
+                      m::Compare(m::Parameter(0), m::Parameter(2))
+                          .WithComparisonDirection(ComparisonDirection::kEq),
+                      m::Compare(m::Parameter(1), m::Parameter(3))
+                          .WithComparisonDirection(ComparisonDirection::kLt))),
+              m::Parameter(1), m::Parameter(3))));
+}
+
+// Match on variadic reduce which computes and returns (min, arg_min).
+//
+//                   p0   p2    p1    p3
+//                  /|\ \/ |\    |\   /|
+//                 / | \/\ | \   | \ / |
+//                /  | /\ \|  |  |  /\ |
+//               Ne  Lt |  \  |  | |  ||
+//                 \ /  |  |\ |  | /  ||
+//                  Or /  /  Eq  Lt   ||
+//                  | /  /    \  /    //
+//                  | |  |     And   //
+//                  | |  |      |  //
+//                  select     select
+//                      \     /
+//                       tuple
+//
+static bool MatchArgMin(const HloInstruction* hlo) {
+  // Match on variadic Reduce ArgMin
+  if (hlo->opcode() != HloOpcode::kReduce || hlo->operand_count() != 4 ||
+      !hlo->shape().IsTuple() ||
+      hlo->operand(1)->opcode() != HloOpcode::kIota ||
+      !IsScalarConstantInf(hlo->operand(2)) ||
+      !IsScalarConstantZero(hlo->operand(3))) {
+    return false;
+  }
+  return MatchArgMinMax(hlo->to_apply()->root_instruction(), /*is_max=*/false);
+}
+
 // Match on variadic reduce which computes and returns (max, arg_max).
 //
 //                   p0   p2    p1    p3
@@ -4680,28 +4739,7 @@ static bool MatchArgMax(const HloInstruction* hlo) {
       !IsScalarConstantZero(hlo->operand(3))) {
     return false;
   }
-
-  // Create matcher for shared sub-expression.
-  auto value_pred =
-      m::OrAnyOrder(m::Compare(m::Parameter(0), m::Parameter(2))
-                        .WithComparisonDirection(ComparisonDirection::kGt),
-                    m::Compare(m::Parameter(0), m::Parameter(0))
-                        .WithComparisonDirection(ComparisonDirection::kNe));
-
-  // Match on argmax reduction computation.
-  return Match(
-      hlo->to_apply()->root_instruction(),
-      m::Tuple(
-          m::Select(value_pred, m::Parameter(0), m::Parameter(2)),
-          m::Select(
-              m::OrAnyOrder(
-                  value_pred,
-                  m::And(
-                      m::Compare(m::Parameter(0), m::Parameter(2))
-                          .WithComparisonDirection(ComparisonDirection::kEq),
-                      m::Compare(m::Parameter(1), m::Parameter(3))
-                          .WithComparisonDirection(ComparisonDirection::kLt))),
-              m::Parameter(1), m::Parameter(3))));
+  return MatchArgMinMax(hlo->to_apply()->root_instruction(), /*is_max=*/true);
 }
 
 Status AlgebraicSimplifierVisitor::HandleReduce(HloInstruction* hlo) {
@@ -5009,6 +5047,25 @@ Status AlgebraicSimplifierVisitor::HandleReduce(HloInstruction* hlo) {
     }
   }
 
+  // Replace Use(ReduceMin(Arg)) with Use(Gte(ReduceArgMin, 0)).
+  // Match on Reduce Min with init value Inf.
+  if (reduce->operand_count() == 2 && IsScalarConstantInf(init_value) &&
+      Match(reduce->to_apply()->root_instruction(),
+            m::MinimumAnyOrder(m::Parameter(0), m::Parameter(1)))) {
+    // Match on variadic Reduce ArgMin which is also fed by 'arg'.
+    auto arg_min_candidate =
+        absl::c_find_if(arg->users(), [&](const HloInstruction* user) {
+          return user != reduce && user->operand(0) == arg &&
+                 MatchArgMin(user) &&
+                 reduce->dimensions() == user->dimensions();
+        });
+    if (arg_min_candidate != arg->users().end()) {
+      // Replace 'reduce' uses with GTE(ArgMin, 0).
+      return ReplaceWithNewInstruction(
+          reduce, HloInstruction::CreateGetTupleElement(*arg_min_candidate,
+                                                        /*index=*/0));
+    }
+  }
   return Status::OK();
 }
 
