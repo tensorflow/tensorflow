1fba55bffc301fcd973f6b670a2516079cdf2ec4
Matt Callanan <mpcallanan@google.com>
2022-03-02 12:46:11 -0800

    Removing unused `ctx` argument.
    PiperOrigin-RevId: 432005805

--- tensorflow/core/framework/dataset.h
+++ tensorflow/core/framework/dataset.h
@@ -10,7 +10,7 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.
+limitations under the License. asdf
 ==============================================================================*/
 #ifndef TENSORFLOW_CORE_FRAMEWORK_DATASET_H_
 #define TENSORFLOW_CORE_FRAMEWORK_DATASET_H_
