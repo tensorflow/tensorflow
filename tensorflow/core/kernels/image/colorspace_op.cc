3cb011818cdc98eefb4e4c00d05626d5cf5fd6fa
Yong Tang <yong.tang.github@outlook.com>
2022-03-04 06:52:49 +0000

    Fix clang-format issue
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/core/kernels/image/colorspace_op.cc
+++ tensorflow/core/kernels/image/colorspace_op.cc
@@ -21,7 +21,6 @@ limitations under the License.
 #include <algorithm>
 #include <cmath>
 
-#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/tensor.h"
@@ -31,6 +30,7 @@ limitations under the License.
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/types.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 
 namespace tensorflow {
 
@@ -148,6 +148,5 @@ TF_CALL_double(DECLARE_GPU);
 TF_CALL_float(REGISTER_GPU);
 TF_CALL_double(REGISTER_GPU);
 #endif
-
 
 }  // namespace tensorflow
