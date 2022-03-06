7ee817313d920c6c09ee010432bd02b59cdf3bb5
A. Unique TensorFlower <gardener@tensorflow.org>
2022-03-05 22:47:48 -0800

    Add Gradient Op for Einsum
    PiperOrigin-RevId: 432729961

--- tensorflow/cc/gradients/math_grad.cc
+++ tensorflow/cc/gradients/math_grad.cc
@@ -15,12 +15,12 @@ limitations under the License.
 
 #include <cmath>
 
+#include "tensorflow/cc/framework/grad_op_registry.h"
+#include "tensorflow/cc/framework/gradients.h"
+#include "tensorflow/cc/gradients/grad_helper.h"
 #include "tensorflow/cc/ops/array_ops_internal.h"
 #include "tensorflow/cc/ops/math_ops_internal.h"
 #include "tensorflow/cc/ops/standard_ops.h"
-
-#include "tensorflow/cc/framework/grad_op_registry.h"
-#include "tensorflow/cc/framework/gradients.h"
 
 namespace tensorflow {
 namespace ops {
@@ -616,61 +616,6 @@ REGISTER_GRADIENT_OP("Conj", ConjGrad);
 // Integer division x / y, assuming x and y >=0, but treats x/0 = x
 Output SafeDivHelper(const Scope& scope, const Output& x, const Output& y) {
   return Div(scope, x, Maximum(scope, y, Const(scope, 1)));
-}
-
-// Helper function for reduction ops.
-//
-// input_shape: 1-D Tensor, the shape of the Tensor being reduced.
-// axes: 1-D Tensor, the reduction axes.
-//   Note that the reduction indices are in the range
-//   -rank(input_shape), rank(input_shape)
-// returns a 1-D Tensor, the output shape as if keep_dims were set to True.
-Output ReducedShapeHelper(const Scope& scope, const Output& input_shape,
-                          const Output& reduction_axes) {
-  auto zero = Const(scope, 0);
-  auto one = Const(scope, 1);
-
-  // Running example in comments
-  // input_shape = [2, 3, 5, 7]
-  // axes = [1, 2]
-  // The result (a shape after a reduction with keep_dims=True)
-  // [2, 1, 1, 7]
-  //
-  // We can treat each entry in axes as an index into input_shape that
-  // should be replaced by 1.
-  // We use DynamicStitch to do this.
-
-  // input_rank = 4
-  auto input_rank = Size(scope, input_shape);
-
-  // Normalize any negative indices in the reduction_axes to positive
-  // values.
-  auto axes = Mod(scope, Add(scope, reduction_axes, input_rank), input_rank);
-
-  // This [0..input_rank) range of integers is used in DynamicStitch to
-  // first copy input_shape to the result.
-  // input_rank_range = [0, 1, 2, 3]
-  auto input_rank_range = Range(scope, zero, input_rank, one);
-
-  // A 1-filled tensor with the same shape as axes. DynamicStitch will
-  // merge these 1s (using axes for indices) to the correct
-  // position in the result.
-  // axes_ones = [1, 1]
-  auto axes_ones = OnesLike(scope, axes);
-
-  // using DynamicStitch:
-  // indices = { input_rank_range, axes }
-  //         = { [0, 1, 2, 3], [1, 2] }
-  // data = { input_shape, axes_ones }
-  //      = { [2, 3, 5, 7], [1, 1] }
-  // The input_rank_range entry in indices first replicates the
-  // input_shape to the result.
-  // The axes entry in indices then moves a 1 to each of its entries,
-  // resulting in
-  // [2, 1, 1, 7]
-  std::vector<Output> indices = {input_rank_range, axes};
-  std::vector<Output> data = {input_shape, axes_ones};
-  return DynamicStitch(scope, indices, data);
 }
 
 // SumGradHelper returns the gradient for the Sum operator, and is used
