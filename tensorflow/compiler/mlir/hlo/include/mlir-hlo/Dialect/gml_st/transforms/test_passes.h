0557f9ef182290b28bb30076f9e4c52f67c6cc55
Mehdi Amini <aminim@google.com>
2022-03-12 17:09:09 -0800

    Apply clang-tidy fixes for llvm-header-guard in test_passes.h (NFC)
    PiperOrigin-RevId: 434206359

--- tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/gml_st/transforms/test_passes.h
+++ tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/gml_st/transforms/test_passes.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_COMPILER_MLIR_HLO_INCLUDE_MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H_
-#define TENSORFLOW_COMPILER_MLIR_HLO_INCLUDE_MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H_
+#ifndef MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H
+#define MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H
 
 #include "mlir/Pass/Pass.h"
 
@@ -31,4 +31,4 @@ std::unique_ptr<OperationPass<FuncOp>> createTestGmlStLoopTilingPass();
 }  // namespace gml_st
 }  // namespace mlir
 
-#endif  // TENSORFLOW_COMPILER_MLIR_HLO_INCLUDE_MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H_
+#endif  // MLIR_HLO_DIALECT_GML_ST_TRANSFORMS_TEST_PASSES_H
