85d64fdd40eafc488649b806a85a7a168dc16510
Mehdi Amini <aminim@google.com>
2022-03-12 17:22:39 -0800

    Apply clang-tidy fixes for llvm-else-after-return in map_mhlo_to_scalar_op.h (NFC)
    PiperOrigin-RevId: 434207416

--- tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/map_mhlo_to_scalar_op.h
+++ tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/map_mhlo_to_scalar_op.h
@@ -477,7 +477,8 @@ inline Value MapMhloOpToStdScalarOp<mhlo::ConvertOp>(
                                                targetType)) {
     return b->create<mlir::arith::SIToFPOp>(loc, result_types, args,
                                             mlir::None);
-  } else if (sourceType.isa<FloatType>() && targetType.isa<FloatType>()) {
+  }
+  if (sourceType.isa<FloatType>() && targetType.isa<FloatType>()) {
     FloatType src = sourceType.cast<FloatType>();
     FloatType res = targetType.cast<FloatType>();
     if (src.getWidth() > res.getWidth()) {
@@ -884,7 +885,8 @@ inline Value MapMhloOpToStdScalarOp<mhlo::SignOp>(Location loc,
         b->create<::mlir::arith::ShRSIOp>(loc, args[0], bitwidth_minus_one);
     Value or_op = b->create<::mlir::arith::OrIOp>(loc, ashr, one);
     return b->create<::mlir::arith::SelectOp>(loc, cmp, zero, or_op);
-  } else if (element_type.isa<ComplexType>()) {
+  }
+  if (element_type.isa<ComplexType>()) {
     return b->create<::mlir::complex::SignOp>(loc, element_type, args.front());
   }
   return nullptr;
