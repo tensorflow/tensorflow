2b4f2677b263a26f65428464040351b8db50d652
Ben Mares <services-git-throwaway1@tensorial.com>
2022-03-14 23:14:46 +0100

    Fix failing test

--- tensorflow/python/ops/nn_test.py
+++ tensorflow/python/ops/nn_test.py
@@ -156,7 +156,10 @@ class SoftmaxTest(test_lib.TestCase, parameterized.TestCase):
   @test_util.run_in_graph_and_eager_modes
   def testRagged(self):
     x_list = [[-500, -501, -502], [], [1729, 1729]]
-    y_list = [self._softmax(np.array([row])).tolist() for row in x_list]
+    x_row_matrices = [[row] for row in x_list]
+    y_row_matrices = [self._softmax(np.array(row_matrix)).tolist()
+                      for row_matrix in x_row_matrices]
+    y_list = [row_matrix[0] for row_matrix in y_row_matrices]
     expected = ragged_factory_ops.constant(y_list, dtype=dtypes.float32)
     x_tf = ragged_factory_ops.constant(x_list, dtype=dtypes.float32)
     y_tf = nn_ops.softmax_v2(x_tf)
