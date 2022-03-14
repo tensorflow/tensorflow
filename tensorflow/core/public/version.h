8157101cf0e9b99f67ae841d23087b961fc6d328
A. Unique TensorFlower <gardener@tensorflow.org>
2022-03-14 02:22:30 -0700

    Update GraphDef version to 1070.
    PiperOrigin-RevId: 434408219

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 1069  // Updated: 2022/3/13
+#define TF_GRAPH_DEF_VERSION 1070  // Updated: 2022/3/14
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
