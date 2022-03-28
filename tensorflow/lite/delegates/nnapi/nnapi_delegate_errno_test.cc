f3aebab2d3b547738ebb1010f41e5ae068345e6a
Yu-Cheng Ling <ycling@google.com>
2022-03-28 11:50:01 -0700

    Properly assert the SingleOpModel(test utility) invocation's success
    PiperOrigin-RevId: 437815667

--- tensorflow/lite/delegates/nnapi/nnapi_delegate_errno_test.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate_errno_test.cc
@@ -90,7 +90,7 @@ TEST_F(NnApiErrnoTest, IsZeroWhenNoErrorOccurs) {
                     {TensorType_FLOAT32, {}}, ActivationFunctionType_NONE);
   m.PopulateTensor<float>(m.input1(), {-2.0, 0.2, 0.7, 0.8});
   m.PopulateTensor<float>(m.input2(), {0.1, 0.2, 0.3, 0.5});
-  m.Invoke();
+  ASSERT_EQ(m.InvokeUnchecked(), kTfLiteOk);
 
   EXPECT_EQ(m.GetDelegate()->GetNnApiErrno(), 0);
 }
