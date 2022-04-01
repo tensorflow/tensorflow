d6efc3b8d44c6ac583b4ff529343749cdebbff06
A. Unique TensorFlower <gardener@tensorflow.org>
2022-04-01 13:47:37 -0700

    Remove deprecated overload of CopyRawToHost
    PiperOrigin-RevId: 438902197

--- tensorflow/compiler/xla/pjrt/pjrt_client.h
+++ tensorflow/compiler/xla/pjrt/pjrt_client.h
@@ -667,18 +667,6 @@ class PjRtBuffer {
   virtual PjRtFuture<Status> CopyRawToHost(void* dst, int64_t offset,
                                            int64_t transfer_size) = 0;
 
-  // Transfers a sub-range of the on-device representation of the buffer.
-  // offset+transfer_size must be less than GetOnDeviceSizeInBytes. on_ready
-  // is called if and only if CopyRawToHost returns OK. on_ready will be called
-  // with a non-OK status if the buffer asynchronously transitions to an error
-  // state.
-  ABSL_DEPRECATED("Use CopyRawToHost(...).OnReady() instead")
-  Status CopyRawToHost(void* dst, int64_t offset, int64_t transfer_size,
-                       std::function<void(Status)> on_ready) {
-    CopyRawToHost(dst, offset, transfer_size).OnReady(std::move(on_ready));
-    return Status::OK();
-  }
-
   // Drops the buffer's reference to its associated device memory, leaving the
   // buffer in an invalid state. The memory will be freed lazily when all async
   // operations using the buffer have completed, according to the allocation
