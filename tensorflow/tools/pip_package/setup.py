0f54936907ddb3b9e1e3f3f46bb00b6d92296c7a
pranve <88513897+pranve@users.noreply.github.com>
2022-04-04 16:52:58 -0700

    Update setup.py
    Updating the version for estimator lower bound for 2.9 release

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -106,7 +106,7 @@ REQUIRED_PACKAGES = [
     # version name.
     # These are all updated during the TF release process.
     standard_or_nightly('tensorboard >= 2.8, < 2.9', 'tb-nightly ~= 2.9.0.a'),
-    standard_or_nightly('tensorflow_estimator >= 2.9.0, < 2.10.0',
+    standard_or_nightly('tensorflow_estimator >= 2.9.0rc0, < 2.10.0',
                         'tf-estimator-nightly ~= 2.9.0.dev'),
     standard_or_nightly('keras >= 2.9.0, < 2.10.0',
                         'keras-nightly ~= 2.9.0.dev'),
