582248194008ec156023fd560ceece64da67dfbb
A. Unique TensorFlower <gardener@tensorflow.org>
2022-04-05 12:14:52 -0700

    Rollback of PR #55351 PR #55351: Integrating cuBLASLt into XLA
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/55351
    
    Adds support for the cuBLASLt library for GEMM operations to XLA. The library can be activated by setting the XLA flag `xla_gpu_enable_cublaslt=true`.
    
    Attn: @SandSnip3r
    Copybara import of the project:
    
    --
    2bbc43325f9fedd229527637ac85b1bc31c2a2aa by Philipp Hack <phack@nvidia.com>:
    
    Integrates cuBLASLt into XLA.
    
    PiperOrigin-RevId: 439644214

--- tensorflow/compiler/xla/debug_options_flags.cc
+++ tensorflow/compiler/xla/debug_options_flags.cc
@@ -68,8 +68,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_enable_cudnn_frontend(true);
 
-  opts.set_xla_gpu_enable_cublaslt(false);
-
   // Despite the name, fast min/max on GPUs does not seem to be any faster, and
   // adds very counter-intuitive "NaN-swallowing" behavior.
   opts.set_xla_gpu_enable_fast_min_max(false);
@@ -691,11 +689,6 @@ static void AllocateFlags() {
       bool_setter_for(&DebugOptions::set_xla_gpu_enable_cudnn_frontend),
       flag_values->xla_gpu_enable_cudnn_frontend(),
       "Use the cuDNN frontend API for convolutions when possible."));
-  flag_objects->push_back(tensorflow::Flag(
-      "xla_gpu_enable_cublaslt",
-      bool_setter_for(&DebugOptions::set_xla_gpu_enable_cublaslt),
-      flag_values->xla_gpu_enable_cublaslt(),
-      "Use cuBLASLt for GEMMs when possible."));
   flag_objects->push_back(tensorflow::Flag(
       "xla_dump_disable_metadata",
       bool_setter_for(&DebugOptions::set_xla_dump_disable_metadata),
