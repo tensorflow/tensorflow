553e8e8c9ae8fa60983b2c6043f5b2585c8c7633
Yang Chen <yangchen@google.com>
2022-04-06 10:19:39 -0700

    #tf-data-service Fix memory estimation for Tensors.
    `Tensor::TotalBytes()` does not include the size of the Variant, and
    underestimates the memory usage.
    
    PiperOrigin-RevId: 439874046

--- tensorflow/core/data/service/data_transfer_test.cc
+++ tensorflow/core/data/service/data_transfer_test.cc
@@ -16,6 +16,7 @@ limitations under the License.
 
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "tensorflow/core/framework/tensor.h"
@@ -70,12 +71,24 @@ TEST(DataTransferTest, EstimateMemoryUsageBytes) {
   GetElementResult empty;
   EXPECT_GT(empty.EstimatedMemoryUsageBytes(), 0);
 
-  Tensor tensor(DT_INT64, TensorShape({5, 5}));
+  Tensor tensor(DT_INT64, TensorShape({10, 10}));
   GetElementResult int64_result = MakeElementResult(tensor);
-  EXPECT_GT(int64_result.EstimatedMemoryUsageBytes(), 25 * sizeof(int64_t));
+  EXPECT_GT(int64_result.EstimatedMemoryUsageBytes(), 100 * sizeof(int64_t));
   EXPECT_GT(int64_result.EstimatedMemoryUsageBytes(),
             int64_result.components[0].AllocatedBytes());
   EXPECT_GE(int64_result.EstimatedMemoryUsageBytes(), sizeof(int64_result));
+}
+
+TEST(DataTransferTest, EstimateVarintMemoryUsageBytes) {
+  const size_t data_size = 1000;
+  CompressedElement compressed;
+  compressed.set_data(std::string(data_size, 'a'));
+
+  Tensor tensor(DT_VARIANT, TensorShape({}));
+  tensor.scalar<Variant>()() = std::move(compressed);
+
+  GetElementResult variant_result = MakeElementResult(tensor);
+  EXPECT_GT(variant_result.EstimatedMemoryUsageBytes(), data_size);
 }
 
 TEST(DataTransferTest, CopyGetElementResult) {
