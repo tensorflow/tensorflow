20d521419aab555d25b9f3df3a11032723365a69
Chia-hung Duan <chiahungduan@google.com>
2022-04-07 12:10:49 -0700

    [TFG] Add shape inference pass
    After importer, most of operations have unknown tensor type. This pass infer the tensor shape by using InferenceContext.
    
    PiperOrigin-RevId: 440168525

--- tensorflow/core/transforms/pass_registration.h
+++ tensorflow/core/transforms/pass_registration.h
@@ -26,6 +26,7 @@ limitations under the License.
 #include "tensorflow/core/transforms/functional_to_region/pass.h"
 #include "tensorflow/core/transforms/graph_to_func/graph_to_func_pass.h"
 #include "tensorflow/core/transforms/remapper/remapper_pass.h"
+#include "tensorflow/core/transforms/shape_inference/pass.h"
 #include "tensorflow/core/transforms/toposort/toposort_pass.h"
 
 namespace mlir {
