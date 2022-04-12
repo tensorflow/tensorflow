a7c8a73a9f6d3a13fad085d6cff79cf96cfd5b18
Loren Maggiore <loreno@google.com>
2022-04-12 03:21:21 -0700

    [xla::gpu] fuse bias addition for bf16 gemms.
    PiperOrigin-RevId: 441133520

--- tensorflow/compiler/xla/service/gpu/tests/gemm_rewrite_test.cc
+++ tensorflow/compiler/xla/service/gpu/tests/gemm_rewrite_test.cc
@@ -576,6 +576,30 @@ ENTRY int8gemm {
                       /*print_operand_shape=*/true);
   }
 }
+
+TEST_F(GemmRewriteTest, BF16GemmWithBias) {
+  const char* hlo_text = R"(
+HloModule BF16GemmWithBias
+
+ENTRY BF16GemmWithBias {
+  x = bf16[8,8]{1,0} parameter(0)
+  y = bf16[8,8]{1,0} parameter(1)
+  dot.5 = bf16[8,8]{1,0} dot(x, y), lhs_contracting_dims={1}, rhs_contracting_dims={0}
+  bias = bf16[8,8]{1,0} parameter(2)
+  ROOT add.6 = bf16[8,8]{1,0} add(dot.5, bias)
+}
+  )";
+
+  EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-3, 1e-3}));
+  MatchOptimizedHlo(hlo_text,
+                    R"(
+; CHECK-LABEL: ENTRY %BF16GemmWithBias (x: bf16[8,8], y: bf16[8,8], bias: bf16[8,8]) -> bf16[8,8] {
+; CHECK-NEXT:    [[INSTR_0:%[^ ]+]] = bf16[8,8]{1,0} parameter(0)
+; CHECK-NEXT:    [[INSTR_1:%[^ ]+]] = bf16[8,8]{1,0} parameter(1)
+; CHECK-NEXT:    [[INSTR_2:%[^ ]+]] = bf16[8,8]{1,0} parameter(2)
+; CHECK-NEXT:    ROOT [[INSTR_3:%[^ ]+]] = bf16[8,8]{1,0} custom-call([[INSTR_0]], [[INSTR_1]], [[INSTR_2]]), custom_call_target="__cublas$gemm", backend_config="{\"alpha_real\":1,\"alpha_imag\":0,\"beta\":1,\"dot_dimension_numbers\":{\"lhs_contracting_dimensions\":[\"1\"],\"rhs_contracting_dimensions\":[\"0\"],\"lhs_batch_dimensions\":[],\"rhs_batch_dimensions\":[]},\"batch_size\":\"1\",\"lhs_stride\":\"64\",\"rhs_stride\":\"64\",\"selected_algorithm\":\"{{-?[0-9]+}}\"}"
+      )");
+}
 }  // namespace
 }  // namespace gpu
 }  // namespace xla
