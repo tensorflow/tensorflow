5e9ad18a713ea5c94db652badd9298b96f2d1259
Justin Lebar <jlebar@google.com>
2022-04-14 14:07:16 -0700

    [XLA] Make kScatter nodes the same color as kReduce nodes in graph dumps.
    Scatter is a kind of reduce.
    
    Also scatter is expensive and should be "called out" as such.
    
    PiperOrigin-RevId: 441850542

--- tensorflow/compiler/xla/service/hlo_graph_dumper.cc
+++ tensorflow/compiler/xla/service/hlo_graph_dumper.cc
@@ -1092,8 +1092,6 @@ ColorScheme HloDotDumper::GetInstructionColor(const HloInstruction* instr) {
         return kWhite;
       }
       return kGreen;
-    case HloOpcode::kScatter:
-      // Do not de-emphasize Scatter, since it involves significant work.
     case HloOpcode::kCopy:
     case HloOpcode::kCopyStart:
     case HloOpcode::kCopyDone:
@@ -1119,6 +1117,7 @@ ColorScheme HloDotDumper::GetInstructionColor(const HloInstruction* instr) {
     case HloOpcode::kBatchNormTraining:
     case HloOpcode::kReduce:
     case HloOpcode::kReduceWindow:
+    case HloOpcode::kScatter:  // scatter is a kind of reduction
     case HloOpcode::kSelectAndScatter:
       return kPurple;
     case HloOpcode::kDomain:
