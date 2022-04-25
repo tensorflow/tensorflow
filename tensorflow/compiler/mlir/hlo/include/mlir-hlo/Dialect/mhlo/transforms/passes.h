233afff23652e46cd8ee81a70a9c61bbb167d5b3
Frederik Gossen <frgossen@google.com>
2022-04-25 09:28:19 -0700

    [MHLO] Add constraint fusion pass
    Add constraint fusion pass to simplify constraints and to merge all assuming
    regions of a function. This is in preparation of replacing the slow merge
    assuming regions pass.
    
    PiperOrigin-RevId: 444290314

--- tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/passes.h
+++ tensorflow/compiler/mlir/hlo/include/mlir-hlo/Dialect/mhlo/transforms/passes.h
@@ -95,6 +95,9 @@ std::unique_ptr<OperationPass<func::FuncOp>> createMergeAssumingOpsPass();
 // Iteratively reifies all shape computations in the function.
 std::unique_ptr<OperationPass<func::FuncOp>> CreateShapeReificationPass();
 
+// Fuse shape constraints and merge all assuming regions.
+std::unique_ptr<OperationPass<func::FuncOp>> CreateConstraintFusionPass();
+
 // Group reduction and parallel dimensions of reduction operations and realize
 // them through equivalent 1D or 2D reductions.
 std::unique_ptr<OperationPass<func::FuncOp>> createGroupReductionDimensionsPass(
