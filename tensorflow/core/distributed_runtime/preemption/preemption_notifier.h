6264e6c2d80ed41faa8ed9994c72ab2a2197ec83
A. Unique TensorFlower <gardener@tensorflow.org>
2022-04-28 11:33:36 -0700

    [Preemption Checkpoint] Initialize interfaces.
    PiperOrigin-RevId: 445205889

---
+++ tensorflow/core/distributed_runtime/preemption/preemption_notifier.h
@@ -0,0 +1,46 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_PREEMPTION_PREEMPTION_NOTIFIER_H_
+#define TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_PREEMPTION_PREEMPTION_NOTIFIER_H_
+
+#include <functional>
+#include <memory>
+
+#include "absl/time/time.h"
+
+namespace tensorflow {
+
+// Listens and propagates any task preemption notice.
+class PreemptionNotifier {
+ public:
+  virtual ~PreemptionNotifier() = default;
+
+  // Returns a death time when preemption/termination will occur once the
+  // listener receives the preemption notification. If no death time is
+  // specified, absl::Now() is returned.
+  virtual absl::Time WillBePreemptedAt() = 0;
+
+  // Registers a callback that takes the death time as input once the listener
+  // receives the preemption notification.
+  // If no death time is specified, absl::Now() is specified as input.
+  virtual void WillBePreemptedAtAsync(
+      std::function<void(absl::Time)> callback) = 0;
+};
+
+std::unique_ptr<PreemptionNotifier> CreatePreemptionNotifier();
+
+}  // namespace tensorflow
+
+#endif  // TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_PREEMPTION_PREEMPTION_NOTIFIER_H_
