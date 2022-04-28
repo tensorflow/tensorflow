9ed60fb1fb20e1b84db5766ba794d42bcce4ccfe
Alan Liu <liualan@google.com>
2022-04-28 13:08:57 -0700

    Fix tf.raw_ops.DeleteSessionTensor vulnerability with invalid `handle`.
    Check that `handle` input is actually a scalar before treating it as such.
    
    PiperOrigin-RevId: 445228994

--- tensorflow/core/kernels/session_ops.cc
+++ tensorflow/core/kernels/session_ops.cc
@@ -132,6 +132,8 @@ class DeleteSessionTensorOp : public OpKernel {
 
   void Compute(OpKernelContext* ctx) override {
     const Tensor& handle = ctx->input(0);
+    OP_REQUIRES(ctx, TensorShapeUtils::IsScalar(handle.shape()),
+                errors::InvalidArgument("`handle` must be scalar"));
     const string& name = handle.scalar<tstring>()();
     auto session_state = ctx->session_state();
     OP_REQUIRES(ctx, session_state != nullptr,
