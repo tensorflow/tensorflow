43be2eb15cf4937aa48520871628dda3d4ae9ff6
Antonio Sanchez <cantonios@google.com>
2022-04-28 08:58:14 -0700

    Update Eigen to commit:b02c384ef4e8eba7b8bdef16f9dc6f8f4d6a6b2b
    CHANGELOG
    =========
    b02c384ef - Add fused multiply functions for PowerPC - pmsub, pnmadd and pnmsub
    3de96caea - Fix HouseholderSequence.h
    f845a8bb1 - Fix cwise NaN propagation for scalar input.
    a4bb513b9 - Update HouseholderSequence.h
    fc1d88841 - Remove AVX512VL dependency in trsm
    07db964bd - Restrict new AVX512 trsm to AVX512VL, rename files for consistency.
    67eeba6e7 - Avoidable heap allocation in applyHouseholderToTheLeft
    3342fc7e4 - Allow all tests to pass with `EIGEN_TEST_NO_EXPLICIT_VECTORIZATION`
    efb08e0bb - Revert "Fix ambiguous DiagonalMatrix constructors."
    53eec53d2 - Fix Power GEMV order of operations in predux for MMA.
    a81bba962 - Fix ambiguous DiagonalMatrix constructors.
    f7b31f864 - Revert "Replace call to FixedDimensions() with a singleton instance of"
    f3ba220c5 - Remove EIGEN_EMPTY_STRUCT_CTOR
    5ed7a86ae - Fix MSVC+CUDA issues.
    734ed1efa - Fix ODR issues in lapacke_helpers.
    2c45a3846 - Fix some max size expressions.
    edc822666 - Fix navbar scroll with toc.
    df87d40e3 - constexpr reshape helper
    403fa3340 - Performance improvements in GEMM for Power
    e1df3636b - More constexpr helpers
    64909b82b - static const class members turned into constexpr
    2c0ef43b4 - Added Scaling function overload for vector rvalue reference
    ba2cb835a - Add back std::remove* aliases - third-party libraries rely on these.
    0c859cf35 - Consider inf/nan in scalar test_isApprox.
    1ddd3e29c - fixed order of arguments in blas syrk
    2c5644280 - Don't include .cpp in lapack.
    73b2c13bf - Disable f16c scalar conversions for MSVC.
    9bc9992dd - Eliminate trace unused warning.
    e22d58e81 - Add is_constant_evaluated, update alignment checks
    f0a91838a - Enable Aarch64 CI
    b9d2900e8 - added a missing typename and fixed a unused typedef warning
    0611f7fff - Add missing explicit reinterprets
    cd3c81c3b - Add a NNLS solver to unsupported - issue #655
    0699fa06f - Split general_matrix_vector_product interface for Power into two macros - one ColMajor and RowMajor.
    19a6a827c - Optimize visitor traversal in case of RowMajor.
    f2a3e03e9 - Fix usages of wrong namespace
    4451823fb - Fix ODR violation in trsm.
    9a14d91a9 - Fix AVX512 builds with MSVC.
    7b10795e3 - Change EIGEN_ALTIVEC_ENABLE_MMA_DYNAMIC_DISPATCH and EIGEN_ALTIVEC_DISABLE_MMA flags to be like TensorFlow's...
    3ca1228d4 - Work around MSVC compiler bug dropping `const`.
    40eb34bc5 - Fix RowMajorBit <-> RowMajor mixup.
    c06298346 - Completed a missing parenthesis in tutorial.
    9deaa1912 - Work around g++-10 docker issue for geo_orthomethods_4.
    e34db1239 - Fix missing pound
    591906477 - Fix up PowerPC MMA flags so it builds by default.
    518fc321c - AVX512 Optimizations for Triangular Solve
    01b5bc48c - Disable schur non-convergence test.
    421cbf086 - Replace Eigen type metaprogramming with corresponding std types and make use of alias templates
    514f90c9f - Remove workarounds for bad GCC-4 warnings
    9ad566148 - Revert "Fix up PowerPC MMA flags so it builds by default."
    65eeedf96 - Fix up PowerPC MMA flags so it builds by default.
    cb1e8228e - Convert bit calculation to constexpr, avoid casts.
    baf9a985e - Fix swap test for size 1 inputs.
    788240885 - Temporarily disable aarch64 CI.
    2a6be5492 - Fix construct_at compilation breakage on ROCm.
    a3b64625e - Remove ComputeCpp-specific code from SYCL Vptr
    9296bb4b9 - Fix edge-case in zeta for large inputs.
    cd2ba9d03 - Add construct_at, destroy_at wrappers. Use throughout.
    dfa517678 - make SparseSolverBase and IterativeSolverBase move constructable
    9883108f3 - Remove copy_bool workaround for gcc 4.3
    3a9d404d7 - Add support for Apple's Accelerate sparse matrix solvers
    
    PiperOrigin-RevId: 445164768

--- tensorflow/lite/tools/cmake/modules/eigen.cmake
+++ tensorflow/lite/tools/cmake/modules/eigen.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   eigen
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
   # Sync with tensorflow/third_party/eigen3/workspace.bzl
-  GIT_TAG 008ff3483a8c5604639e1c4d204eae30ad737af6
+  GIT_TAG b02c384ef4e8eba7b8bdef16f9dc6f8f4d6a6b2b
   # It's not currently (cmake 3.17) possible to shallow clone with a GIT TAG
   # as cmake attempts to git checkout the commit hash after the clone
   # which doesn't work as it's a shallow clone hence a different commit hash.
