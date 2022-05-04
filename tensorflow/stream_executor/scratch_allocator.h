42e72edf22b52eb81a196d65c9e56f25ed730ba4
Chris Jones <cjfj@google.com>
2022-05-04 05:03:57 -0700

    [XLA/STREAM_EXEC] Deduplicate scratch allocator code.
    PiperOrigin-RevId: 446426129

--- tensorflow/stream_executor/scratch_allocator.h
+++ tensorflow/stream_executor/scratch_allocator.h
@@ -19,6 +19,7 @@ limitations under the License.
 #include <memory>
 
 #include "tensorflow/stream_executor/device_memory.h"
+#include "tensorflow/stream_executor/device_memory_allocator.h"
 #include "tensorflow/stream_executor/lib/statusor.h"
 #include "tensorflow/stream_executor/platform/port.h"
 #include "tensorflow/stream_executor/temporary_device_memory.h"
@@ -35,7 +36,7 @@ class Stream;
 // optionally request scratch space to speed up the operation.
 class ScratchAllocator {
  public:
-  virtual ~ScratchAllocator();
+  virtual ~ScratchAllocator() {}
 
   // Returns a limit of memory this scratch allocator wants to produce, in
   // bytes. This information may be used to help select an algorithm.
@@ -60,9 +61,10 @@ class ScratchAllocator {
 // thread will request the scratch allocation).
 class OneTimeScratchAllocator : public ScratchAllocator {
  public:
-  explicit OneTimeScratchAllocator(Stream* stream);
-  ~OneTimeScratchAllocator() override;
-  int64_t GetMemoryLimitInBytes() override;
+  explicit OneTimeScratchAllocator(Stream* stream) : stream_(stream) {}
+
+  int64_t GetMemoryLimitInBytes() override { return -1; }
+
   port::StatusOr<DeviceMemory<uint8>> AllocateBytes(int64_t byte_size) override;
 
  private:
@@ -70,6 +72,28 @@ class OneTimeScratchAllocator : public ScratchAllocator {
   Stream* stream_;
 
   SE_DISALLOW_COPY_AND_ASSIGN(OneTimeScratchAllocator);
+};
+
+// Allocates a single memory allocation -- this memory is deallocated when the
+// scratch allocator is destroyed.
+//
+// Thread-compatible, but not thread-safe (use in scenarios where only one
+// thread will request the scratch allocation).
+class OwningScratchAllocator : public ScratchAllocator {
+ public:
+  OwningScratchAllocator(int device_ordinal, DeviceMemoryAllocator* allocator)
+      : device_ordinal_(device_ordinal), allocator_(allocator) {}
+
+  int64_t GetMemoryLimitInBytes() override { return -1; }
+
+  port::StatusOr<DeviceMemory<uint8>> AllocateBytes(int64_t byte_size) override;
+
+ private:
+  int device_ordinal_;
+  DeviceMemoryAllocator* allocator_;
+  OwningDeviceMemory buffer_;
+
+  SE_DISALLOW_COPY_AND_ASSIGN(OwningScratchAllocator);
 };
 
 }  // namespace stream_executor
