6a77bcfd699b0189271abf210022cb22505380f0
Ben Mares <services-git-throwaway1@tensorial.com>
2022-05-13 19:21:55 +0200

    Define additional tests

--- tensorflow/python/ops/ragged/ragged_math_ops_test.py
+++ tensorflow/python/ops/ragged/ragged_math_ops_test.py
@@ -36,14 +36,50 @@ class RaggedSoftmaxTest(test_lib.TestCase, parameterized.TestCase):
     return u / z
 
   @test_util.run_in_graph_and_eager_modes
-  def testRagged(self):
-    x_list = [[-500, -501, -502], [], [1729, 1729]]
+  def testOrdinaryValues(self):
+    eps = 1e-5
+    x_list = [np.log([0.5, 0.25, 0.25]), np.log([0.5, 0.5])]
     x_row_matrices = [[row] for row in x_list]
     y_row_matrices = [self._softmax(np.array(row_matrix)).tolist()
                       for row_matrix in x_row_matrices]
     y_list = [row_matrix[0] for row_matrix in y_row_matrices]
-    expected = ragged_factory_ops.constant(y_list, dtype=dtypes.float32)
+    y_expected_from_numpy = ragged_factory_ops.constant(
+        y_list, dtype=dtypes.float32
+    )
+    y_expected = ragged_factory_ops.constant(
+        [[0.5, 0.25, 0.25], [0.5, 0.5]], dtype=dtypes.float32
+    )
+    self.assertAllClose(y_expected_from_numpy, y_expected, eps)
     x_tf = ragged_factory_ops.constant(x_list, dtype=dtypes.float32)
     y_tf = nn_ops.softmax_v2(x_tf)
-    eps = 1e-3
-    self.assertAllClose(y_tf, expected, eps)
+    self.assertAllClose(y_tf, y_expected_from_numpy, eps)
+
+  @test_util.run_in_graph_and_eager_modes
+  def testLargeValues(self):
+    eps = 1e-5
+    x_list = [[-500, -501, -502], [1729, 1729]]
+    x_row_matrices = [[row] for row in x_list]
+    y_row_matrices = [self._softmax(np.array(row_matrix)).tolist()
+                      for row_matrix in x_row_matrices]
+    y_list = [row_matrix[0] for row_matrix in y_row_matrices]
+    y_expected_from_numpy = ragged_factory_ops.constant(
+        y_list, dtype=dtypes.float32
+    )
+    x_tf = ragged_factory_ops.constant(x_list, dtype=dtypes.float32)
+    y_tf = nn_ops.softmax_v2(x_tf)
+    self.assertAllClose(y_tf, y_expected_from_numpy, eps)
+
+  @test_util.run_in_graph_and_eager_modes
+  def testShortTensors(self):
+    eps = 1e-5
+    x_list = [[], [1]]
+    x_row_matrices = [[row] for row in x_list]
+    y_row_matrices = [self._softmax(np.array(row_matrix)).tolist()
+                      for row_matrix in x_row_matrices]
+    y_list = [row_matrix[0] for row_matrix in y_row_matrices]
+    y_expected_from_numpy = ragged_factory_ops.constant(
+        y_list, dtype=dtypes.float32
+    )
+    x_tf = ragged_factory_ops.constant(x_list, dtype=dtypes.float32)
+    y_tf = nn_ops.softmax_v2(x_tf)
+    self.assertAllClose(y_tf, y_expected_from_numpy, eps)
