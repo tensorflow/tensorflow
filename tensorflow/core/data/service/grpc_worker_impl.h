f6639a839b8e514644925d949d20331839747cdd
Yang Chen <yangchen@google.com>
2022-05-13 19:16:11 -0700

    #tf-data-service #codehealth Fix a missing include.
    missing #include <functional> for 'std::function'
    
    PiperOrigin-RevId: 448624452

--- tensorflow/core/data/service/grpc_worker_impl.h
+++ tensorflow/core/data/service/grpc_worker_impl.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_DATA_SERVICE_GRPC_WORKER_IMPL_H_
 #define TENSORFLOW_CORE_DATA_SERVICE_GRPC_WORKER_IMPL_H_
 
+#include <functional>
 #include <memory>
 #include <string>
 
