fb71bae584930b070d26036c37093da1e6bc633e
bhack <bhack@users.noreply.github.com>
2022-05-16 23:50:59 +0000

    Readd empty input and tensor

--- tensorflow/python/autograph/tests/loop_scoping_test.py
+++ tensorflow/python/autograph/tests/loop_scoping_test.py
@@ -130,8 +130,8 @@ class LoopScopingTest(reference_test_base.TestCase, parameterized.TestCase):
     self.assertFunctionMatchesEager(for_with_local_var, l)
 
   @parameterized.parameters(*itertools.product(
-      ([1], [1, 2]),
-      (list, list),
+      ([], [1], [1, 2]),
+      (list, _int_tensor),
   ))
   def test_for_with_lambda_var(self, l, type_):
     
