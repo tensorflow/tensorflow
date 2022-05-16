c5c1e1b8a332879dcc172df67b798e49efe1c93e
Skye Wanderman-Milne <skyewm@google.com>
2022-05-16 12:07:15 -0700

    Update xla_client._version and add missing version checks to JAX
    PiperOrigin-RevId: 449021408

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -43,7 +43,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 67
+_version = 68
 
 # Version number for MLIR:Python components.
 mlir_api_version = 18
