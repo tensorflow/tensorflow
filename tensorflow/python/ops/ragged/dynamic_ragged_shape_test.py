6dbe3e8ff3694ed8d1ac340a50b7903ee03639a6
A. Unique TensorFlower <gardener@tensorflow.org>
2022-05-17 10:45:32 -0700

    Modify __init__ to allow a hint for the static_inner_shape to be specified. Change row_partition variable so it won't conflict with row_partition package in the future.
    PiperOrigin-RevId: 449259774

--- tensorflow/python/ops/ragged/dynamic_ragged_shape_test.py
+++ tensorflow/python/ops/ragged/dynamic_ragged_shape_test.py
@@ -1044,6 +1044,23 @@ class DynamicRaggedShapeTest(test_util.TensorFlowTestCase,
     self.assertAllEqual(static_valid_rowids0, [0, 2, 4])
     self.assertAllEqual(static_valid_rowids1, [0, 1, 2])
 
+  def testConstructorWithStaticInnerShape(self):
+    row_partitions = [
+        RowPartition.from_value_rowids([0, 2, 4], nrows=5),
+        RowPartition.from_value_rowids([0, 1, 2], nrows=3)
+    ]
+    inner_shape = [3]
+    rts = DynamicRaggedShape(row_partitions, inner_shape, validate=True,
+                             static_inner_shape=[3])
+    static_inner_shape = tensor_util.constant_value(rts.inner_shape)
+    static_valid_rowids0 = tensor_util.constant_value(
+        rts.row_partitions[0].value_rowids())
+    static_valid_rowids1 = tensor_util.constant_value(
+        rts.row_partitions[1].value_rowids())
+    self.assertAllEqual(static_inner_shape, [3])
+    self.assertAllEqual(static_valid_rowids0, [0, 2, 4])
+    self.assertAllEqual(static_valid_rowids1, [0, 1, 2])
+
   def testZeros(self):
     shape_x = DynamicRaggedShape.from_lengths([3, (1, 3, 2), 4])
     foo = ragged_array_ops.zeros(shape_x)
