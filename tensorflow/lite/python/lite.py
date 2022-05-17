e22fb79c893b603882a071106c759c0939e52cbf
Terry Heo <terryheo@google.com>
2022-05-17 10:44:34 -0700

    lite: skip model_object creation if conversion metadata is not needed
    This change avoids unnecessary FB -> model_object -> FB conversion.
    
    PiperOrigin-RevId: 449259539

--- tensorflow/lite/python/lite.py
+++ tensorflow/lite/python/lite.py
@@ -911,13 +911,14 @@ class TFLiteConverterBase(object):
       self._increase_conversion_success_metric()
     self._set_conversion_latency_metric(round(elapsed_time_ms))
     self._tflite_metrics.export_metrics()
+    if self.exclude_conversion_metadata:
+      return result
     model_object = flatbuffer_utils.convert_bytearray_to_object(result)
-    if not self.exclude_conversion_metadata:
-      # Populates the conversion metadata.
-      # TODO(b/202090541): Collects sparsity block size information.
-      sparsity_modes = _get_sparsity_modes(model_object)
-      self._metadata.options.modelOptimizationModes.extend(sparsity_modes)
-      model_object = _populate_conversion_metadata(model_object, self._metadata)
+    # Populates the conversion metadata.
+    # TODO(b/202090541): Collects sparsity block size information.
+    sparsity_modes = _get_sparsity_modes(model_object)
+    self._metadata.options.modelOptimizationModes.extend(sparsity_modes)
+    model_object = _populate_conversion_metadata(model_object, self._metadata)
     return flatbuffer_utils.convert_object_to_bytearray(model_object)
 
 
