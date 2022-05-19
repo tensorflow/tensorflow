8bd91620e0a1f6b8272c829f84875ef8ca5fd4c6
Manu Seth <sethman@amazon.com>
2022-05-19 18:37:49 +0000

    update license

--- tensorflow/tools/ci_build/rel/ubuntu/cpu_arm64_pip.sh
+++ tensorflow/tools/ci_build/rel/ubuntu/cpu_arm64_pip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 set -e
 set -x
 
