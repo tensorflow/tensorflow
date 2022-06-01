9913aa11b9fc3942eb1a254760027f4041b969e9
Jean-Baptiste Lespiau <jblespiau@google.com>
2022-06-01 06:38:40 -0700

    Replace `tensorflow::Status::OK()` with tensorflow::OkStatus()`.
    PiperOrigin-RevId: 452283561

--- tensorflow/core/graph/control_flow.cc
+++ tensorflow/core/graph/control_flow.cc
@@ -79,7 +79,7 @@ Status ValidateControlFlowInfo(const Graph* graph,
       frame.loop_cond = node;
     }
   }
-  return Status::OK();
+  return OkStatus();
 }
 }  // namespace
 
@@ -180,7 +180,7 @@ Status BuildControlFlowInfo(const Graph* g, std::vector<ControlFlowInfo>* info,
     }
   }
   TF_RETURN_IF_ERROR(ValidateControlFlowInfo(g, *info));
-  return Status::OK();
+  return OkStatus();
 }
 
 }  // namespace tensorflow
