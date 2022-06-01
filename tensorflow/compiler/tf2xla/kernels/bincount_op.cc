9a75d11a026cd9b73bb2fa518cfd96778eaea019
bhack <bhack@users.noreply.github.com>
2022-06-01 01:42:00 +0000

    Add rank2 code

--- tensorflow/compiler/tf2xla/kernels/bincount_op.cc
+++ tensorflow/compiler/tf2xla/kernels/bincount_op.cc
@@ -36,15 +36,21 @@ class DenseBincountOp : public XlaOpKernel {
   // Dumb implementation for the simplest test case
     xla::XlaOp input = ctx->Input(0);
     int64_t output_size;
-    ctx->ConstantInputAsIntScalar("size",&output_size);
+    ctx->ConstantInputAsIntScalar("size", &output_size);
     StatusOr<xla::Shape> input_shape_or = ctx->builder()->GetShape(input);
     OP_REQUIRES_OK(ctx, input_shape_or.status());
     auto input_shape = input_shape_or.ValueOrDie();
-    int64_t size = input_shape.dimensions(0);
+    auto size = input_shape.dimensions(0);
+    auto dim = 1;
+    auto rank = input_shape.rank();
     auto counter_shape = xla::ShapeUtil::MakeShape(xla::S32, {});
-    const xla::Shape data_shape = xla::ShapeUtil::MakeShape(xla::S32, {size});
+    const xla::Shape data_shape = xla::ShapeUtil::MakeShape(xla::S32, {input_shape.dimensions()});
 
-    const xla::Shape output_shape = xla::ShapeUtil::MakeShape(xla::S32, {output_size});
+    xla::Shape output_shape = xla::ShapeUtil::MakeShape(xla::S32, {output_size});
+    if (rank == 2) {
+      output_shape = xla::ShapeUtil::MakeShape(xla::S32, {rank, output_size});
+      dim = input_shape.dimensions(1);
+    }
 
     auto loop_shape = xla::ShapeUtil::MakeTupleShape(
         {counter_shape, data_shape, output_shape});
@@ -56,31 +62,45 @@ class DenseBincountOp : public XlaOpKernel {
           ctx->builder()->CreateSubBuilder("condition");
       auto param = xla::Parameter(builder.get(), 0, loop_shape, "param");
       auto counter = xla::GetTupleElement(param, 0);
-      xla::Gt(xla::ConstantR0<int32_t>(builder.get(), size), counter);
+      xla::Gt(xla::ConstantR0<int32_t>(builder.get(), size*dim), counter);
       condition = builder->Build().ConsumeValueOrDie();
     }
-
+   
     // Create a computation for the body
     xla::XlaComputation body;
     {
       std::unique_ptr<xla::XlaBuilder> builder =
-          ctx->builder()->CreateSubBuilder("body");
+      ctx->builder()->CreateSubBuilder("body");
       auto param = Parameter(builder.get(), 0, loop_shape, "param");
       auto counter = xla::GetTupleElement(param, 0);
       auto data_stack = xla::GetTupleElement(param, 1);
       auto accum_stack = xla::GetTupleElement(param, 2);
+    if (rank == 1) {
       auto data = xla::DynamicSlice(data_stack, {counter}, {1});
       auto accum = xla::DynamicSlice(accum_stack, {data}, {1});
       accum = accum + xla::One(builder.get(), xla::S32);
       accum_stack = xla::DynamicUpdateSlice(
           accum_stack, xla::Reshape(accum, {1}), {data});
+    }
+    else {
+      auto dim_xla = xla::ConstantR0<int32_t>(builder.get(), dim);
+      auto idx_1 = xla::Div(counter, dim_xla);
+      auto idx_2 = counter % dim_xla;
+      auto data = xla::DynamicSlice(data_stack, {idx_1, idx_2}, {1, 1});
+      auto data_scalar = xla::Reshape(data, {0,1}, {});
+      auto accum = xla::DynamicSlice(accum_stack, {idx_1, data_scalar}, {1, 1});
+      accum = accum + xla::One(builder.get(), xla::S32);
+      accum_stack = xla::DynamicUpdateSlice(
+          accum_stack, xla::Reshape(accum, {1, 1}), {idx_1, data_scalar});
+    } 
       counter = counter + xla::One(builder.get(), xla::S32);
       xla::Tuple(builder.get(), {counter, data_stack, accum_stack});
       body = builder->Build().ConsumeValueOrDie();
     }
+
     // Create a While node with computations for the condition and the body.
     auto zero = xla::Zero(ctx->builder(), xla::S32);
-    auto zero_broadcast = xla::Broadcast(zero, {output_size});
+    auto zero_broadcast = xla::Broadcast(zero, {output_shape.dimensions()});
     auto init = xla::Tuple(ctx->builder(), {zero, input, zero_broadcast});
     auto result = xla::While(condition, body, init);
     auto output = xla::GetTupleElement(result,2);
