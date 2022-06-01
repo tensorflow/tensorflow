58bc956964d419b218aa9cd0e750aa3295cf3fb3
Jean-Baptiste Lespiau <jblespiau@google.com>
2022-06-01 06:00:14 -0700

    Remove the tensorflow::InvalidArgumentError and co symbols.
    It was initially planned to use these instead of tf::errors::InvalidArgument and co, but after discussion, we will simply keep using the tf::errors symbols, it will create less churn.
    
    PiperOrigin-RevId: 452277306

--- tensorflow/core/platform/status.h
+++ tensorflow/core/platform/status.h
@@ -192,24 +192,6 @@ class Status {
 // usage of `OkStatus()` when constructing such an OK status.
 Status OkStatus();
 
-// Convenience Status constructors, not having to specify the underlying Code.
-Status AbortedError(absl::string_view message);
-Status AlreadyExistsError(absl::string_view message);
-Status CancelledError(absl::string_view message);
-Status DataLossError(absl::string_view message);
-Status DeadlineExceededError(absl::string_view message);
-Status FailedPreconditionError(absl::string_view message);
-Status InternalError(absl::string_view message);
-Status InvalidArgumentError(absl::string_view message);
-Status NotFoundError(absl::string_view message);
-Status OutOfRangeError(absl::string_view message);
-Status PermissionDeniedError(absl::string_view message);
-Status ResourceExhaustedError(absl::string_view message);
-Status UnauthenticatedError(absl::string_view message);
-Status UnavailableError(absl::string_view message);
-Status UnimplementedError(absl::string_view message);
-Status UnknownError(absl::string_view message);
-
 
 // TODO(b/197552541) Move this namespace to errors.h.
 namespace errors {
