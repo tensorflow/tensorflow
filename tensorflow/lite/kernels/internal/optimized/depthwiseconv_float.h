79be9efa5190a4a9c828369ba358e6f9e59fbbe5
Jamie Cook <jimi.cook@gmail.com>
2022-06-09 17:54:52 +1000

    Use the help macro from absl to avoid gcc specific compiler macro (__PRETTY_FUNCTION__)

--- tensorflow/lite/kernels/internal/optimized/depthwiseconv_float.h
+++ tensorflow/lite/kernels/internal/optimized/depthwiseconv_float.h
@@ -768,7 +768,7 @@ void FloatDepthwiseConvAccumRow(int stride, int dilation_factor,
                                 const float* filter_data,
                                 int out_x_buffer_start, int out_x_buffer_end,
                                 int output_depth, float* acc_buffer) {
-  ruy::profiler::ScopeLabel label(__PRETTY_FUNCTION__);
+  ruy::profiler::ScopeLabel label(TFLITE_PRETTY_FUNCTION);
   // Consistency check parameters. This is important in particular to ensure
   // that we keep the number of template instantiations minimal, so we don't
   // increase binary size unnecessarily.
