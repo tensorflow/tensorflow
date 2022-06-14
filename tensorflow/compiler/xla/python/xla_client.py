a0877258f1607ffa5820aed65364ec358beca5d9
Andrew Fitzgibbon <awf@graphcore.ai>
2022-06-14 09:11:36 +0100

    Move test, increment version number

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -43,7 +43,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 74
+_version = 75
 
 # Version number for MLIR:Python components.
 mlir_api_version = 21
