5d189740ed607fbaf5b3f61009887b5ea9b89ca5
Weiyi Wang <weiyiw@google.com>
2022-06-21 11:13:59 -0700

    Provide a workaround for hexagon DepthwiseConv2d accuracy issues when weights of shape ?x5x5x?, stride width >= 2, depth > 32 and per channel quantized for accuracy concerns. See #54481 for details.
    The workaround splits DepthwiseConv2d into several 32-channel batches and concatenate those afterwards.
    
    PiperOrigin-RevId: 456305791

--- tensorflow/lite/delegates/hexagon/builders/transpose_conv_2d_builder.cc
+++ tensorflow/lite/delegates/hexagon/builders/transpose_conv_2d_builder.cc
@@ -148,9 +148,11 @@ TfLiteStatus TransposeConv2dOpBuilder::PopulateSubGraph(
     float bias_min = 0;
     float bias_max = 0;
     if (per_channel_quant_.channel_scales_node != nullptr) {
-      ProcessPerChannelQuantizedBias(
-          data_tensor, bias_tensor, inputs->data[3], context, &bias_min,
-          &bias_max, graph_builder_, &per_channel_quant_, &bias_const);
+      std::vector<int> preprocessed_bias_data;
+      ProcessPerChannelQuantizedBias(data_tensor, bias_tensor, inputs->data[3],
+                                     context, &bias_min, &bias_max,
+                                     graph_builder_, &per_channel_quant_,
+                                     &preprocessed_bias_data, &bias_const);
     } else {
       bias_const =
           graph_builder_->AddConstNodeWithData(inputs->data[3], bias_tensor);
