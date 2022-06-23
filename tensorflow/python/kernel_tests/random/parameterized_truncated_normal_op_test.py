54c01739253a08ec340e6774bec5d306698501ff
Antonio Sanchez <cantonios@google.com>
2022-06-22 22:30:03 -0700

    Fix tensor shape dtype bug in parameterized_truncated_normal.
    The original made the assumption that the dtype is int32 when it could
    also be int64 - leading to a crash due to mismatched type.
    
    PiperOrigin-RevId: 456685278

--- tensorflow/python/kernel_tests/random/parameterized_truncated_normal_op_test.py
+++ tensorflow/python/kernel_tests/random/parameterized_truncated_normal_op_test.py
@@ -303,6 +303,29 @@ class ParameterizedTruncatedNormalTest(test.TestCase):
       self.assertAllGreater(samples, 0.)
       self.assertAllGreater(samples_stateless, 0.)
 
+  def testShapeTypes(self):
+    for shape_dtype in [np.int32, np.int64]:
+      shape = np.array([1000], dtype=shape_dtype)
+      sample_op = random_ops.parameterized_truncated_normal(
+          shape=shape, means=0.0, stddevs=0.1, minvals=-1., maxvals=1.)
+      new_seed = random_ops.random_uniform([2],
+                                           seed=1234,
+                                           minval=0,
+                                           maxval=(2**31 - 1),
+                                           dtype=np.int32)
+      sample_op_stateless = stateless.stateless_parameterized_truncated_normal(
+          shape=shape,
+          seed=new_seed,
+          means=0.0,
+          stddevs=0.1,
+          minvals=-1.,
+          maxvals=1.)
+
+      samples = self.evaluate(sample_op)
+      stateless_samples = self.evaluate(sample_op_stateless)
+      self.assertAllEqual(samples.shape, shape)
+      self.assertAllEqual(stateless_samples.shape, shape)
+
   def testStatelessParameterizedTruncatedNormalHasGrads(self):
     mean = variables.Variable(0.01)
     stddev = variables.Variable(1.)
