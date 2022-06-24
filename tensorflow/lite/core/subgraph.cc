0730eba34abdb7d6e21ee666e43868438942d316
Yishuang Pang <ypang@google.com>
2022-06-24 15:22:54 -0700

    lite: enable setting minimum log severity level in the minimal logger.
    Log only when the severity is larger or equal to the minimum log severity.
    
    PiperOrigin-RevId: 457104650

--- tensorflow/lite/core/subgraph.cc
+++ tensorflow/lite/core/subgraph.cc
@@ -412,26 +412,15 @@ TfLiteStatus Subgraph::ReplaceNodeSubsetsWithDelegateKernels(
   PartitionGraphIntoIndependentNodeSubsets(&info, nodes_to_replace,
                                            &node_subsets);
 
-#ifdef __ANDROID__
   // On Android the log message below is used for diagnosing delegation success
-  // also in production builds. Delegation happens sufficiently rarely that the
-  // message isn't spammy.
+  // also in production builds. Use VERBOSE here so that the logging is turned
+  // off in production builds on other platforms.
   TFLITE_LOG_PROD(
-      tflite::TFLITE_LOG_INFO,
+      tflite::TFLITE_LOG_VERBOSE,
       "Replacing %d node(s) with delegate (%s) node, yielding %zu partitions.",
       nodes_to_replace->size,
       registration.custom_name ? registration.custom_name : "unknown",
       node_subsets.size());
-#else   // !__ANDROID__
-  // Server-side, delegation may happen so often as to make logging spammy + we
-  // don't have a clear need for the diagnostic in production builds.
-  TFLITE_LOG(
-      tflite::TFLITE_LOG_INFO,
-      "Replacing %d node(s) with delegate (%s) node, yielding %zu partitions.",
-      nodes_to_replace->size,
-      registration.custom_name ? registration.custom_name : "unknown",
-      node_subsets.size());
-#endif  // __ANDROID__
 
   execution_plan_.clear();
 
