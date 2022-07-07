e489a83d6a6b9865589623375b55c7c70ed3431d
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-07 13:46:50 -0700

    Define tf.shape for StructuredTensor.
    PiperOrigin-RevId: 459591874

--- tensorflow/python/ops/structured/structured_array_ops_test.py
+++ tensorflow/python/ops/structured/structured_array_ops_test.py
@@ -264,6 +264,22 @@ class StructuredArrayOpsTest(test_util.TensorFlowTestCase,
     actual2 = array_ops.size_v2(st, out_type=dtype)
     self.assertAllEqual(actual2, expected)
 
+  def test_shape_v2(self):
+    rt = ragged_tensor.RaggedTensor.from_row_lengths(["a", "b", "c"], [1, 2])
+    st = StructuredTensor.from_fields_and_rank({"r": rt}, rank=2)
+    actual = array_ops.shape_v2(st, out_type=dtypes.int64)
+    actual_static_lengths = actual.static_lengths()
+    self.assertAllEqual([2, (1, 2)], actual_static_lengths)
+
+  def test_shape(self):
+    rt = ragged_tensor.RaggedTensor.from_row_lengths(["a", "b", "c"], [1, 2])
+    st = StructuredTensor.from_fields_and_rank({"r": rt}, rank=2)
+    actual = array_ops.shape(st, out_type=dtypes.int64).static_lengths()
+    actual_v2 = array_ops.shape_v2(st, out_type=dtypes.int64).static_lengths()
+    expected = [2, (1, 2)]
+    self.assertAllEqual(expected, actual)
+    self.assertAllEqual(expected, actual_v2)
+
   @parameterized.named_parameters([
       dict(
           testcase_name="list_empty_2_1",
