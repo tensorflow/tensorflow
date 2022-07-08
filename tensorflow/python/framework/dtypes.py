a8669ad2d4d11cade59badbe63cd3e8f1254b52b
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-07 17:41:52 -0700

    Add clear error for dtypes that do not have defined limits
    PiperOrigin-RevId: 459641743

--- tensorflow/python/framework/dtypes.py
+++ tensorflow/python/framework/dtypes.py
@@ -147,11 +147,7 @@ class DType(_dtypes.DType):
         values. Returns
       min, max : tuple Lower and upper intensity limits.
     """
-    if self.as_numpy_dtype in dtype_range:
-      min, max = dtype_range[self.as_numpy_dtype]  # pylint: disable=redefined-builtin
-    else:
-      raise ValueError(str(self) + " does not have defined limits.")
-
+    min, max = dtype_range[self.as_numpy_dtype]  # pylint: disable=redefined-builtin
     if clip_negative:
       min = 0  # pylint: disable=redefined-builtin
     return min, max
