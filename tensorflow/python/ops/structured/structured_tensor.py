317653cb390136f78cb1929b0ef80a29130ae8dc
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-11 13:19:00 -0700

    Removing StructuredTensorSpec from structured_tensor_spec_test.py and structured_tensor_test.py.
    PiperOrigin-RevId: 460284859

--- tensorflow/python/ops/structured/structured_tensor.py
+++ tensorflow/python/ops/structured/structured_tensor.py
@@ -1119,6 +1119,34 @@ class StructuredTensor(extension_type.BatchableExtensionType):
     def __validate__(self):
       assert self._ragged_shape is not None
 
+    @classmethod
+    def _from_fields_and_rank(cls, fields, rank):
+      """Creates a spec of a StructuredTensor with fields and rank."""
+      shape = None
+      for (k, v) in fields.items():
+        field_shape_untruncated = _dynamic_ragged_shape_spec_from_spec(v)
+        if field_shape_untruncated is None:
+          raise ValueError(f'Cannot convert spec of {k}.')
+        untruncated_rank = field_shape_untruncated.rank
+        if (untruncated_rank is not None
+            and untruncated_rank < rank):
+          raise ValueError(
+              f'Rank of field {k} is {untruncated_rank}, '
+              f'but must be at least {rank}.')
+        field_shape = field_shape_untruncated._truncate(rank)  # pylint: disable=protected-access
+        if shape is None:
+          shape = field_shape
+        else:
+          shape = shape._merge_with(field_shape)
+      return StructuredTensor.Spec(_ragged_shape=shape, _fields=fields)
+
+    @classmethod
+    def _from_shape(
+        cls, shape: dynamic_ragged_shape.DynamicRaggedShape
+    ) -> 'StructuredTensor.Spec':
+      """Creates the spec of an empty StructuredTensor."""
+      return StructuredTensor.Spec(_ragged_shape=shape, _fields={})
+
     # For backwards compatibility
     @property
     def _shape(self) -> tensor_shape.TensorShape:
