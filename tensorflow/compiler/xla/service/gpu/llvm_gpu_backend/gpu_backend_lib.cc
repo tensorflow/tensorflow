37d02113ce5f33a64696499a8ff0ed90c3c63e7d
Jason Furmanek <Jason.Furmanek@amd.com>
2022-07-15 19:08:04 +0000

    Use ROCM_PATH instead of hard-coded path to locate LLVM lld

--- tensorflow/compiler/xla/service/gpu/llvm_gpu_backend/gpu_backend_lib.cc
+++ tensorflow/compiler/xla/service/gpu/llvm_gpu_backend/gpu_backend_lib.cc
@@ -734,9 +734,14 @@ StatusOr<std::vector<uint8_t>> EmitModuleToHsaco(
     ir_fs->flush();
   }
   // Locate lld.
-  // TODO(whchung@gmail.com): change to tensorflow::ROCmRoot() after
-  // ROCm-Device-Libs PR.
-  std::string lld_path = tensorflow::io::JoinPath("/opt/rocm", "llvm/bin");
+  std::string lld_path;
+  if (std::getenv("ROCM_PATH")) {
+       lld_path = tensorflow::io::JoinPath(std::getenv("ROCM_PATH"), "llvm/bin");
+  }
+  else {
+       lld_path = tensorflow::io::JoinPath("/opt/rocm", "llvm/bin");
+  }
+
   auto lld_program = llvm::sys::findProgramByName("ld.lld", {lld_path});
   if (!lld_program) {
     return xla::InternalError("unable to find ld.lld in %s: %s", lld_path,
