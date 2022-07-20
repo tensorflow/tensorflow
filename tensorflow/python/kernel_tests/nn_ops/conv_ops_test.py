27a65a43cf763897fecfa5cdb5cc653fc5dd0346
Antonio Sanchez <cantonios@google.com>
2022-07-20 13:32:38 -0700

    Fix GPU/CPU Conv2DBackpropInputOp check error.
    For empty `out_backprop` inputs (e.g. `[3, 1, 0, 1]`), the current
    CPU/GPU kernels fail (one with dnnl, the other with cudnn). Added
    a shortcut path to return a zero input.
    
    Note that the XLA test currently fails due to an incorrect result
    size bug (b/239598470), so it is currently disabled.
    
    PiperOrigin-RevId: 462217998

--- tensorflow/python/kernel_tests/nn_ops/conv_ops_test.py
+++ tensorflow/python/kernel_tests/nn_ops/conv_ops_test.py
@@ -1103,6 +1103,23 @@ class Conv2DTest(test.TestCase):
           use_gpu=use_gpu,
           err=1e-5)
 
+  @test_util.run_in_graph_and_eager_modes
+  @test_util.disable_xla("b/239598470")
+  def testConv2DBackpropInputDegenerateBackpropInput(self):
+    input_sizes = [3, 1, 1, 2]
+    expected_output = np.zeros(input_sizes).flatten()
+    for (data_format, use_gpu) in GetTestConfigs():
+      self._RunAndVerifyBackpropInput(
+          input_sizes=input_sizes,
+          filter_sizes=[1, 3, 2, 3],
+          output_sizes=[3, 1, 0, 3],
+          strides=[1, 2],
+          padding="VALID",
+          expected=expected_output,
+          data_format=data_format,
+          use_gpu=use_gpu,
+          err=1e-5)
+
   # Testing for backprops
   def _RunAndVerifyBackpropFilter(self,
                                   input_sizes,
