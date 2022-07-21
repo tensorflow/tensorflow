73ad1815ebcfeb7c051f9c2f7ab5024380ca8613
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-21 09:22:58 -0700

    Add IsScalar (rank == 0) check to input_min/max tensors for QuantizeDownAndShrinkRangeOp.
    PiperOrigin-RevId: 462401306

--- tensorflow/python/kernel_tests/quantization_ops/quantization_ops_test.py
+++ tensorflow/python/kernel_tests/quantization_ops/quantization_ops_test.py
@@ -261,5 +261,21 @@ class QuantizedRelu6OpTest(test_util.TensorFlowTestCase):
               out_type=dtypes.quint8))
 
 
+class QuantizeDownAndShrinkRangeOpTest(test_util.TensorFlowTestCase):
+
+  @test_util.run_in_graph_and_eager_modes
+  def test_invalid_inputs(self):
+    inputs = constant_op.constant(
+        np.int32(0), shape=[3, 3, 3, 3], dtype=dtypes.qint32)
+
+    with self.assertRaisesRegex((ValueError, errors.InvalidArgumentError),
+                                "must be rank 0"):
+      self.evaluate(
+          math_ops.quantize_down_and_shrink_range(input=inputs,
+                                                  input_min=[],
+                                                  input_max=4.0,
+                                                  out_type=dtypes.quint8))
+
+
 if __name__ == "__main__":
   googletest.main()
