1642b67714ef3321c5eb838991044eefe9954704
Raman Sarokin <sorokin@google.com>
2022-07-25 02:58:32 -0700

    TestExecutionEnvironment simplified. Removed virtual property for some methods by reusing existing functions. Simplified backend specific test execution environments.
    PiperOrigin-RevId: 463041635

--- tensorflow/lite/delegates/gpu/common/task/tensor_desc.h
+++ tensorflow/lite/delegates/gpu/common/task/tensor_desc.h
@@ -92,6 +92,10 @@ class TensorDescriptor : public GPUObjectDescriptor {
   void UploadData(const tflite::gpu::Tensor<BHWC, T>& src);
   template <DataType T>
   void DownloadData(tflite::gpu::Tensor<BHWC, T>* dst);
+  template <DataType T>
+  void UploadData(const tflite::gpu::Tensor<BHWDC, T>& src);
+  template <DataType T>
+  void DownloadData(tflite::gpu::Tensor<BHWDC, T>* dst);
 
   void UploadData(const tflite::gpu::Tensor<HWC, DataType::FLOAT32>& src);
   void UploadData(const tflite::gpu::Tensor<Linear, DataType::FLOAT32>& src);
@@ -283,6 +287,19 @@ void TensorDescriptor::UploadData(const tflite::gpu::Tensor<BHWC, T>& src) {
 template <DataType T>
 void TensorDescriptor::DownloadData(tflite::gpu::Tensor<BHWC, T>* dst) {
   dst->shape = BHWC(shape_.b, shape_.h, shape_.w, shape_.c);
+  dst->data.resize(dst->shape.DimensionsProduct(), 0.0f);
+  DownloadData(dst->data.data());
+}
+
+template <DataType T>
+void TensorDescriptor::UploadData(const tflite::gpu::Tensor<BHWDC, T>& src) {
+  shape_ = src.shape;
+  UploadData(src.data.data());
+}
+
+template <DataType T>
+void TensorDescriptor::DownloadData(tflite::gpu::Tensor<BHWDC, T>* dst) {
+  dst->shape = shape_;
   dst->data.resize(dst->shape.DimensionsProduct(), 0.0f);
   DownloadData(dst->data.data());
 }
