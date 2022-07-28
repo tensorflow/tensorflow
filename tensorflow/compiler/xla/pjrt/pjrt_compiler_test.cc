26709f7af92241800563d44342b5d008b774734e
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-28 11:24:03 -0700

    Define the PjRt cross-compilation APIs
    This change
    - Defines the interfaces.
    - Allows the registration of a compiler.
    - Provides an API to compile using the registered compiler.
    - Adds test that demonstrate the expected usage pattern.
    Nothing uses these APIs except the test yet.
    
    The return type from compilation here is 'PjRtLoadedExecutable' currently. This will eventually be changed to 'PjRtExecutable'.
    
    PiperOrigin-RevId: 463883494

---
+++ tensorflow/compiler/xla/pjrt/pjrt_compiler_test.cc
@@ -0,0 +1,76 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/compiler/xla/pjrt/pjrt_compiler.h"
+
+#include <memory>
+#include <utility>
+
+#include <gtest/gtest.h>
+#include "tensorflow/compiler/xla/client/xla_computation.h"
+#include "tensorflow/compiler/xla/pjrt/pjrt_client.h"
+
+namespace xla {
+
+namespace {
+
+TEST(PjRtCompilerTest, CompilerNotRegistered) {
+  class PjRtTestTopology : public PjRtDeviceTopology {
+   public:
+    PjRtPlatformId platform_id() const override { return 0; }
+    absl::string_view platform_name() const override {
+      return "not_registered";
+    }
+    absl::string_view platform_version() const override { return "test"; }
+  };
+  PjRtTestTopology topology;
+
+  CompileOptions options;
+  XlaComputation computation;
+  auto res = PjRtCompile(options, computation, topology);
+
+  EXPECT_TRUE(tensorflow::errors::IsNotFound(res.status()));
+}
+
+TEST(PjRtCompilerTest, CompilerRegistered) {
+  class PjRtTestTopology : public PjRtDeviceTopology {
+   public:
+    PjRtPlatformId platform_id() const override { return 0; }
+    absl::string_view platform_name() const override { return "registered"; }
+    absl::string_view platform_version() const override { return "test"; }
+  };
+  PjRtTestTopology topology;
+
+  class PjRtTestCompiler : public PjRtCompiler {
+   public:
+    StatusOr<std::unique_ptr<PjRtLoadedExecutable>> Compile(
+        CompileOptions options, const XlaComputation& computation,
+        const PjRtDeviceTopology& topology, PjRtClient* client) override {
+      return tensorflow::errors::Unimplemented("test compiler!");
+    }
+  };
+  std::unique_ptr<PjRtCompiler> compiler = std::make_unique<PjRtTestCompiler>();
+  PjRtRegisterCompiler(topology.platform_name(), std::move(compiler));
+
+  CompileOptions options;
+  XlaComputation computation;
+  auto res = PjRtCompile(options, computation, topology);
+
+  EXPECT_TRUE(tensorflow::errors::IsUnimplemented(res.status()));
+}
+
+}  // namespace
+
+}  // namespace xla
