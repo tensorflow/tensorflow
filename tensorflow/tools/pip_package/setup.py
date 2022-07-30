2804742cf6d1c0236be330bd0a4b300455bc4c04
Vinila Settem <vinilas@google.com>
2022-07-29 17:07:25 -0700

    Update estimator version in TF-core release branch to 2.11 release (Estimator & Keras)
    PiperOrigin-RevId: 464187154

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -113,8 +113,8 @@ REQUIRED_PACKAGES = [
     standard_or_nightly('tensorboard >= 2.9, < 2.10', 'tb-nightly ~= 2.10.0.a'),
     standard_or_nightly('tensorflow_estimator >= 2.9.0rc0, < 2.10',
                         'tf-estimator-nightly ~= 2.10.0.dev'),
-    standard_or_nightly('keras >= 2.9.0rc0, < 2.10',
-                        'keras-nightly ~= 2.10.0.dev'),
+    standard_or_nightly('keras >= 2.10.0rc0, < 2.11',
+                        'keras-nightly ~= 2.11.0.dev'),
 ]
 REQUIRED_PACKAGES = [ p for p in REQUIRED_PACKAGES if p is not None ]
 
