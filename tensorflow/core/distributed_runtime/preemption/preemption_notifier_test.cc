5621bb94aa7ab72a7fb4d1496235f8fe97ca730a
A. Unique TensorFlower <gardener@tensorflow.org>
2022-07-29 17:03:13 -0700

    Internal change: use internal library to register SIGTERM handler.
    PiperOrigin-RevId: 464186545

--- tensorflow/core/distributed_runtime/preemption/preemption_notifier_test.cc
+++ tensorflow/core/distributed_runtime/preemption/preemption_notifier_test.cc
@@ -24,33 +24,13 @@ limitations under the License.
 #include "absl/time/time.h"
 #include "tensorflow/core/platform/env.h"
 #include "tensorflow/core/platform/errors.h"
-#include "tensorflow/core/platform/platform.h"
 #include "tensorflow/core/platform/status.h"
 #include "tensorflow/core/platform/statusor.h"
 #include "tensorflow/core/platform/test.h"
-#if defined(PLATFORM_GOOGLE)
-#include "thread/executor.h"
-#include "thread/signal.h"
-#endif
 
 namespace tensorflow {
 namespace {
-
-class PreemptNotifierTest : public ::testing::Test {
- public:
-  PreemptNotifierTest() {
-#if defined(PLATFORM_GOOGLE)
-    // Override default test SIGTERM handler.
-    thread::signal::Token unused_token;
-
-    thread::signal::AddHandler(
-        SIGTERM, thread::Executor::DefaultExecutor(), []() {},
-        thread::signal::kOverrideDefault, &unused_token);
-#endif
-  }
-};
-
-TEST_F(PreemptNotifierTest, WillBePreemptedAt) {
+TEST(PreemptNotifierTest, WillBePreemptedAt) {
   auto env = Env::Default();
   std::unique_ptr<PreemptionNotifier> preempt_notifier =
       PreemptionNotifier::CreatePreemptionNotifier("sigterm", env);
@@ -71,8 +51,8 @@ TEST_F(PreemptNotifierTest, WillBePreemptedAt) {
   EXPECT_LT(time_diff, absl::Seconds(3));
 }
 
-TEST_F(PreemptNotifierTest,
-       WillBePreemptedAt_AlreadyPreempted_ReturnsImmediately) {
+TEST(PreemptNotifierTest,
+     WillBePreemptedAt_AlreadyPreempted_ReturnsImmediately) {
   auto env = Env::Default();
   std::unique_ptr<PreemptionNotifier> preempt_notifier =
       PreemptionNotifier::CreatePreemptionNotifier("sigterm", env);
@@ -97,7 +77,7 @@ TEST_F(PreemptNotifierTest,
   EXPECT_LT(time_diff, absl::Seconds(2));
 }
 
-TEST_F(PreemptNotifierTest, WillBePreemptedAtAsync_SameResultForAllCallbacks) {
+TEST(PreemptNotifierTest, WillBePreemptedAtAsync_SameResultForAllCallbacks) {
   auto env = Env::Default();
   std::unique_ptr<PreemptionNotifier> preempt_notifier =
       PreemptionNotifier::CreatePreemptionNotifier("sigterm", env);
@@ -128,7 +108,7 @@ TEST_F(PreemptNotifierTest, WillBePreemptedAtAsync_SameResultForAllCallbacks) {
   EXPECT_EQ(preempt_time.ValueOrDie(), preempt_time_2.ValueOrDie());
 }
 
-TEST_F(PreemptNotifierTest, Reset_TwoDifferentPreemptTimesRecorded) {
+TEST(PreemptNotifierTest, Reset_TwoDifferentPreemptTimesRecorded) {
   auto env = Env::Default();
   std::unique_ptr<PreemptionNotifier> preempt_notifier =
       PreemptionNotifier::CreatePreemptionNotifier("sigterm", env);
@@ -151,7 +131,7 @@ TEST_F(PreemptNotifierTest, Reset_TwoDifferentPreemptTimesRecorded) {
   EXPECT_NE(preempt_time, preempt_time_2);
 }
 
-TEST_F(PreemptNotifierTest, DestructorCancelsPendingCalls) {
+TEST(PreemptNotifierTest, DestructorCancelsPendingCalls) {
   auto env = Env::Default();
   std::unique_ptr<PreemptionNotifier> preempt_notifier =
       PreemptionNotifier::CreatePreemptionNotifier("sigterm", env);
