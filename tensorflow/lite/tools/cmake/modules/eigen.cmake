62e8194e806c1dbfeb8feac2e2e6f6c985780964
Antonio Sanchez <cantonios@google.com>
2022-08-02 22:56:36 -0700

    Update Eigen to commit:34780d8bd13d0af0cf17a22789ef286e8512594d
    CHANGELOG
    =========
    34780d8bd - Include immintrin.h header for enscripten.
    2cf4d18c9 - Disable AVX512 GEMM kernels by default.
    a678a3e05 - Fix aligned_realloc to call check_that_malloc_is_allowed() if ptr == 0
    4a5635940 - Add option to disable avx512 GEBP kernels
    1092574b2 - Fix wrong doxygen group usage
    e1165dbf9 - AutoDiff depends on Core, so include appropriate header.
    bb51d9f4f - Fix ODR violations.
    06a458a13 - Enable subtests which use device side malloc since this has been fixed in ROCm 5.2.
    84cf3ff18 - Add pload_partial, pstore_partial (and unaligned versions), pgather_partial, pscatter_partial, loadPacketPartial and storePacketPartial.
    c603275dc - Better performance for Power10 using more load and store vector pairs for GEMV
    
    PiperOrigin-RevId: 464973344

--- tensorflow/lite/tools/cmake/modules/eigen.cmake
+++ tensorflow/lite/tools/cmake/modules/eigen.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   eigen
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
   # Sync with tensorflow/third_party/eigen3/workspace.bzl
-  GIT_TAG 0e187141679fdb91da33249d18cb79a011c0e2ea
+  GIT_TAG 34780d8bd13d0af0cf17a22789ef286e8512594d
   # It's not currently (cmake 3.17) possible to shallow clone with a GIT TAG
   # as cmake attempts to git checkout the commit hash after the clone
   # which doesn't work as it's a shallow clone hence a different commit hash.
