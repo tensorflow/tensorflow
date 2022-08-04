452cc6bd4f442da960cf5d320e894fe573050299
Eugene Zhulenev <ezhulenev@google.com>
2022-08-04 12:34:52 -0700

    [xla:runtime] Move rt dialect tests from jitrt to xla/mlir
    PiperOrigin-RevId: 465378071

--- tensorflow/compiler/mlir/runlit.site.cfg.py
+++ tensorflow/compiler/mlir/runlit.site.cfg.py
@@ -54,6 +54,7 @@ mlir_tf_tools_dirs = [
     'tensorflow/compiler/aot',
     'tensorflow/compiler/xla/service/mlir_gpu',
     'tensorflow/compiler/xla/service/gpu/tests',
+    'tensorflow/compiler/xla/mlir/tools/runtime',
 ]
 config.mlir_tf_tools_dirs = [
     os.path.join(real_test_srcdir, os.environ['TEST_WORKSPACE'], s)
