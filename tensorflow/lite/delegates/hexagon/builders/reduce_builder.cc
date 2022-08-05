fcf015bdb51b8660e3a918657fca30794e104669
Sloane Simmons <sloane.simmons@toyotaconnected.com>
2022-08-05 16:26:04 -0500

    Attempted workaround for reduce_mean

--- tensorflow/lite/delegates/hexagon/builders/reduce_builder.cc
+++ tensorflow/lite/delegates/hexagon/builders/reduce_builder.cc
@@ -20,6 +20,7 @@ limitations under the License.
 
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/delegates/hexagon/hexagon_nn/hexagon_nn.h"
+#include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/util.h"
 
@@ -38,12 +39,65 @@ TfLiteStatus ReduceOpBuilder::PopulateSubGraph(const TfLiteIntArray* inputs,
   // Axes tensor should be constant.
   tensor_id = inputs->data[1];
   const auto& axes_tensor = context->tensors[tensor_id];
+
+
   if (axes_tensor.allocation_type == kTfLiteMmapRo) {
+
+    // TfLiteTensor* new_axes_tensor = new TfLiteTensor();
+    // TF_LITE_ENSURE_STATUS(TfLiteTensorCopy(&axes_tensor, new_axes_tensor));
+    // context->ReportError(context, "Axes tensor has type %d", axes_tensor.type);
+    // TF_LITE_ASSERT(axes_tensor.type == kTfLiteInt32);
+
+    auto axes_orig = GetTensorData<int32_t>(&axes_tensor);
+    auto axes_tensor_size = axes_tensor.dims->size;
+    // context->ReportError(context, "Axes tensor has size %d", axes_tensor_size);
+    // for (int i = 0; i < axes_tensor_size; i++){
+    //   context->ReportError(context, "Axes tensor at dim %d is %d", i, axes_orig[i]);
+    // }
+    auto num_axes = axes_tensor.dims->size;
+
+    // Increase the constant axis by the number of dimensions we're padding
+    int num_dimensions_padding = 4 - input_tensor.dims->size;
+    // context->ReportError(context, "Input tensor has %d dims", input_tensor.dims->size);
+    // context->ReportError(context, "Therefore will pad by %d (4 - num_dims)",
+    //                      num_dimensions_padding);
+
+    TfLiteTensor* new_tensor = new TfLiteTensor();
+    // context->ReportError(context, "initialized new tensor");
+
+    new_tensor->type = axes_tensor.type;
+    new_tensor->bytes = axes_tensor.bytes;
+    new_tensor->data = TfLitePtrUnion{};
+    new_tensor->dims = TfLiteIntArrayCopy(axes_tensor.dims);
+    new_tensor->buffer_handle = axes_tensor.buffer_handle;
+    new_tensor->data_is_stale = axes_tensor.data_is_stale;
+    new_tensor->delegate = axes_tensor.delegate;
+
+    new_tensor->data.data = (void *) new int32_t[axes_tensor_size];
+
+    auto axes = const_cast<int32_t*>(GetTensorData<int32_t>(new_tensor));
+
+    for (int i = 0; i < axes_tensor_size; i++) {
+      axes[i] = axes_orig[i];
+      // context->ReportError(context, "Padding axes at index %d of total %d", i, axes_tensor_size);
+      // context->ReportError(context, "Before padding: %d", axes[i]);
+      // We always left-pad by ones so these should be valid.
+      axes[i] += num_dimensions_padding;
+      // context->ReportError(context, "After padding: %d", axes[i]);
+    }
+
+    // context->tensors[tensor_id] = *new_tensor;
+
     // If the axes input is a constant, bake it into the Hexagon graph as a
     // Const node.
     auto* const_axes_node =
-        graph_builder_->AddConstNodeWithData(tensor_id, axes_tensor);
+        graph_builder_->AddConstNodeWithData(tensor_id, *new_tensor);
+
     AddInput(TensorID(const_axes_node->GetID(), 0));
+
+    // TfLiteTensorDataFree(&axes_tensor.data);
+    // TfLiteTensorFree(const_cast<TfLiteTensor*>(&axes_tensor));
+
   } else {
     TF_LITE_KERNEL_LOG(context, "Reduction op doesn't have constant axis");
     return kTfLiteError;
