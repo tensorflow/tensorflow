8fe38cec67729c1c19bc73efe324388fe3a178d2
Edward Schwartz <schwartzedward@google.com>
2022-08-05 14:13:31 -0700

    RaggedFactoryOpsTests on GPU
    1) Enable tests for gpu. testRaggedWithDistributedDatasetIter and testIntStringWithDistributedDataset now read all elements of the dataset and reach the end of the iterator.
    
    2) `testIntStringOpsWithDistributedDataset` updated to work for _xla_gpu.
    
    3) Parameterize tests and batch datasets with different values for drop_remainder to better test placement. When drop_remainder=True, the IteratorGetNext op is used and the type of the iterator's output is the type of the dataset elements. When drop_remainder=False the IteratorGetNextAsOptional op is used and the type of the iterator's output is an Optional that contains the type of the dataset elements. Optionals are encoded using variants.
    
    PiperOrigin-RevId: 465645254

--- tensorflow/python/ops/ragged/ragged_factory_ops_test.py
+++ tensorflow/python/ops/ragged/ragged_factory_ops_test.py
@@ -29,6 +29,7 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import test_util
 from tensorflow.python.ops import map_fn
+from tensorflow.python.ops.math_ops import reduce_sum
 from tensorflow.python.ops.ragged import ragged_factory_ops
 from tensorflow.python.ops.string_ops import string_to_hash_bucket
 from tensorflow.python.platform import test
@@ -79,9 +80,9 @@ def dense_str():
 class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
                            parameterized.TestCase):
 
-  @parameterized.parameters(
-      (ragged_int64,),
-      (ragged_str,),
+  @parameterized.named_parameters(
+      ('Int64', ragged_int64,),
+      ('Str', ragged_str,),
   )
   def testRaggedWithMapFn(self, ragged_factory):
 
@@ -93,31 +94,43 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
     result = self.evaluate(map_fn_producer(t))
     self.assertAllEqual(t.values, result.values)
 
-  @parameterized.parameters(
-      (ragged_int64,),
-      (ragged_str,),
+  # When drop_remainder=True, the IteratorGetNext op is used and the type
+  # of the iterator's output is the type of the dataset elements.
+  # When drop_remainder=False the IteratorGetNextAsOptional op is used and
+  # the type of the iterator's output is an Optional that contains
+  # the type of the dataset elements. Optionals are encoded using variants.
+  @parameterized.named_parameters(
+      ('Int64Drop', ragged_int64, True),
+      ('StrDrop', ragged_str, True),
+      ('Int64NoDrop', ragged_int64, False),
+      ('StrNoDrop', ragged_str, False),
   )
-  def testRaggedWithMultiDeviceIterator(self, ragged_factory):
+  def testRaggedWithMultiDeviceIterator(self, ragged_factory, drop_remainder):
 
     @def_function.function
     def dataset_producer(t):
-      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
+      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(
+          2, drop_remainder)
       it = multi_device_iterator_ops.MultiDeviceIterator(ragged_ds, ['GPU:0'])
       with ops.device_v2('GPU:0'):
         return it.get_next_as_optional()
 
     t = ragged_factory()
     if t.dtype == dtypes.string:
-      self.skipTest('b/194439197: fix ragged tensor of string')
+      self.skipTest('b/241136926: fix RaggedTensorFromVariant copy')
     result = dataset_producer(t)
     self.assertAllEqual(
         self.evaluate(t[0]), self.evaluate(result[0].get_value()[0]))
 
-  @parameterized.parameters(
-      (ragged_int64,),
-      (ragged_str,),
+  @parameterized.named_parameters(
+      ('Int65Drop', ragged_int64, True),
+      ('StrDrop', ragged_str, True),
+      ('Int65NoDrop', ragged_int64, False),
+      ('StrNoDrop', ragged_str, False),
   )
-  def testRaggedWithDistributedDataset(self, ragged_factory):
+  @test_util.run_gpu_only
+  def testRaggedWithDistributedDatasetIter(self, ragged_factory,
+                                           drop_remainder):
 
     @def_function.function
     def distributed_dataset_producer(t):
@@ -125,44 +138,148 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
       ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
       dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
       ds = iter(dist_dataset)
-      return strategy.experimental_local_results(next(ds))[0]
+      result0 = strategy.experimental_local_results(next(ds))
+      result1 = strategy.experimental_local_results(next(ds))
+      result2 = strategy.experimental_local_results(next(ds))
+      result3 = strategy.experimental_local_results(next(ds))
+      # Reach the end of the iterator
+      for ignore in ds:  # pylint: disable=unused-variable
+        pass
+      return result0, result1, result2, result3
 
     t = ragged_factory()
 
-    result = distributed_dataset_producer(t)
-    self.assertAllEqual(self.evaluate(t[0]), self.evaluate(result[0]))
+    result0, result1, result2, result3 = distributed_dataset_producer(t)
+    self.assertAllEqual(self.evaluate(t[0]), self.evaluate(result0[0][0]))
+    self.assertAllEqual(self.evaluate(t[1]), self.evaluate(result0[1][0]))
+    self.assertAllEqual(self.evaluate(t[2]), self.evaluate(result1[0][0]))
+    self.assertAllEqual(self.evaluate(t[3]), self.evaluate(result1[1][0]))
+    self.assertAllEqual(self.evaluate(t[4]), self.evaluate(result2[0][0]))
+    self.assertAllEqual(self.evaluate(t[5]), self.evaluate(result2[1][0]))
+    self.assertAllEqual(self.evaluate(t[6]), self.evaluate(result3[0][0]))
+    self.assertAllEqual(self.evaluate(t[7]), self.evaluate(result3[1][0]))
 
-  @parameterized.parameters(
-      (dense_str,),
-      (ragged_str,),
+  @parameterized.named_parameters(
+      ('Int65Drop', ragged_int64, True),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('StrDrop', ragged_str, True),
+      ('Int65NoDrop', ragged_int64, False),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('StrNoDrop', ragged_str, False),
   )
-  def testIntStringWithDistributedDataset(self, string_factory):
+  @test_util.run_v2_only
+  def testRaggedWithDistributedDatasetReplicaFn(self, ragged_factory,
+                                                drop_remainder):
 
-    @def_function.function
     def distributed_dataset_producer(t):
       strategy = mirrored_strategy.MirroredStrategy(['GPU:0', 'GPU:1'])
       ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
       dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
+
+      @def_function.function
+      def replica_fn(elem):
+        return elem
+
+      result = []
+      for x in dist_dataset:
+        result.append(strategy.run(replica_fn, args=(x,)))
+      return result
+
+    t = ragged_factory()
+    result = distributed_dataset_producer(t)
+
+    self.assertAllEqual(
+        self.evaluate(t[0]), self.evaluate(result[0].values[0][0]))
+    self.assertAllEqual(
+        self.evaluate(t[1]), self.evaluate(result[0].values[1][0]))
+    self.assertAllEqual(
+        self.evaluate(t[2]), self.evaluate(result[1].values[0][0]))
+    self.assertAllEqual(
+        self.evaluate(t[3]), self.evaluate(result[1].values[1][0]))
+    self.assertAllEqual(
+        self.evaluate(t[4]), self.evaluate(result[2].values[0][0]))
+    self.assertAllEqual(
+        self.evaluate(t[5]), self.evaluate(result[2].values[1][0]))
+    self.assertAllEqual(
+        self.evaluate(t[6]), self.evaluate(result[3].values[0][0]))
+    self.assertAllEqual(
+        self.evaluate(t[7]), self.evaluate(result[3].values[1][0]))
+
+  @parameterized.named_parameters(
+      ('DenseDrop', dense_str, True),
+      # ('RaggedDrop', ragged_str, True),  # TODO(b/241425197)
+      ('DenseNoDrop', dense_str, False),
+      ('RaggedNoDrop', ragged_str, False),
+  )
+  @test_util.run_gpu_only
+  def testIntStringWithDistributedDataset(self, string_factory, drop_remainder):
+
+    @def_function.function
+    def distributed_dataset_producer(t):
+      strategy = mirrored_strategy.MirroredStrategy(['GPU:0', 'GPU:1'])
+      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(
+          2, drop_remainder)
+      dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
       ds = iter(dist_dataset)
-      return strategy.experimental_local_results(next(ds))[0]
+      result0 = strategy.experimental_local_results(next(ds))
+      result1 = strategy.experimental_local_results(next(ds))
+      result2 = strategy.experimental_local_results(next(ds))
+      result3 = strategy.experimental_local_results(next(ds))
+      # Reach the end of the iterator
+      for ignore in ds:  # pylint: disable=unused-variable
+        pass
+      return result0, result1, result2, result3
 
     ds_dict = {'int': ragged_int64(), 'str': string_factory()}
-    result = distributed_dataset_producer(ds_dict)
+    result0, result1, result2, result3 = distributed_dataset_producer(ds_dict)
     self.assertAllEqual(
-        self.evaluate(ds_dict['int'][0]), self.evaluate(result['int'][0]))
+        self.evaluate(ds_dict['int'][0]), self.evaluate(result0[0]['int'][0]))
     self.assertAllEqual(
-        self.evaluate(ds_dict['str'][0]), self.evaluate(result['str'][0]))
+        self.evaluate(ds_dict['str'][0]), self.evaluate(result0[0]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][1]), self.evaluate(result0[1]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][1]), self.evaluate(result0[1]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][2]), self.evaluate(result1[0]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][2]), self.evaluate(result1[0]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][3]), self.evaluate(result1[1]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][3]), self.evaluate(result1[1]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][4]), self.evaluate(result2[0]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][4]), self.evaluate(result2[0]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][5]), self.evaluate(result2[1]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][5]), self.evaluate(result2[1]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][6]), self.evaluate(result3[0]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][6]), self.evaluate(result3[0]['str'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['int'][7]), self.evaluate(result3[1]['int'][0]))
+    self.assertAllEqual(
+        self.evaluate(ds_dict['str'][7]), self.evaluate(result3[1]['str'][0]))
 
-  @parameterized.parameters(
-      (dense_str,),
-      # (ragged_str,),  # TODO(b/194439197) fix ragged tensor of string
+  @parameterized.named_parameters(
+      ('DenseDrop', dense_str, True),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('RaggedDrop', ragged_str, True),
+      ('DenseNoDrop', dense_str, False),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('RaggedNoDrop', ragged_str, True),
   )
   @test_util.run_v2_only
-  def testOpsWithDistributedDataset(self, string_factory):
+  def testOpsWithDistributedDataset(self, string_factory, drop_remainder):
 
     def distributed_dataset_producer(t):
       strategy = mirrored_strategy.MirroredStrategy(['GPU:0', 'GPU:1'])
-      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
+      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(
+          2, drop_remainder)
       dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
 
       @def_function.function
@@ -196,12 +313,17 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
     self.assertAllEqual([[2000, 3000, 5000, 3000][:expect_length[7]]],
                         self.evaluate(result[3].values[1]))
 
-  @parameterized.parameters(
-      (dense_str,),
-      # (ragged_str,),  # TODO(b/194439197) fix ragged tensor of string
+  @parameterized.named_parameters(
+      ('DenseDrop', dense_str, True),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('RaggedDrop', ragged_str, True),
+      ('DenseNoDrop', dense_str, False),
+      # TODO(b/241136926) fix RaggedTensorFromVariant copy
+      # ('RaggedNoDrop', ragged_str, False),
   )
   @test_util.run_v2_only
-  def testIntStringOpsWithDistributedDataset(self, string_factory):
+  def testIntStringOpsWithDistributedDataset(self, string_factory,
+                                             drop_remainder):
 
     ri = ragged_int64()
     # To ease testing both dense and ragged strings, pass in the ragged sizes
@@ -211,7 +333,8 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
 
     def distributed_dataset_producer(t):
       strategy = mirrored_strategy.MirroredStrategy(['GPU:0', 'GPU:1'])
-      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(2)
+      ragged_ds = dataset_ops.Dataset.from_tensor_slices(t).batch(
+          2, drop_remainder)
       dist_dataset = strategy.experimental_distribute_dataset(ragged_ds)
 
       @def_function.function
@@ -221,7 +344,7 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
         # For dense string case, slice it to size of ragged int
         hashed_sliced = hashed[:, :elem['size'][0]]
         # Computation with both feature from string and numeric dataset output
-        return elem['int'] * 10 + hashed_sliced
+        return reduce_sum(hashed_sliced) + 100 * reduce_sum(elem['int'])
 
       result = []
       for x in dist_dataset:
@@ -229,14 +352,14 @@ class RaggedFactoryOpsTest(test_util.TensorFlowTestCase,
       return result
 
     result = distributed_dataset_producer(ds_dict)
-    self.assertAllEqual([[35, 13, 45, 13]], self.evaluate(result[0].values[0]))
-    self.assertAllEqual([[]], self.evaluate(result[0].values[1]))
-    self.assertAllEqual([[50, 93, 22]], self.evaluate(result[1].values[0]))
-    self.assertAllEqual([[62]], self.evaluate(result[1].values[1]))
-    self.assertAllEqual([[]], self.evaluate(result[2].values[0]))
-    self.assertAllEqual([[35, 13, 45, 13]], self.evaluate(result[2].values[1]))
-    self.assertAllEqual([[35, 13]], self.evaluate(result[3].values[0]))
-    self.assertAllEqual([[22, 13, 45, 13]], self.evaluate(result[3].values[1]))
+    self.assertAllEqual(916, self.evaluate(result[0].values[0]))
+    self.assertAllEqual(0, self.evaluate(result[0].values[1]))
+    self.assertAllEqual(1605, self.evaluate(result[1].values[0]))
+    self.assertAllEqual(602, self.evaluate(result[1].values[1]))
+    self.assertAllEqual(0, self.evaluate(result[2].values[0]))
+    self.assertAllEqual(916, self.evaluate(result[2].values[1]))
+    self.assertAllEqual(408, self.evaluate(result[3].values[0]))
+    self.assertAllEqual(813, self.evaluate(result[3].values[1]))
 
 
 if __name__ == '__main__':
