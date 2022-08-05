4db378f77ad253ba0ae9d6c410aae27d00c2563f
Matt Callanan <mpcallanan@google.com>
2022-08-05 13:04:46 -0700

    In tf.data service tests, pass in a `data_transfer_protocol` and `compression`.
    This supports testing of non-default data transfer protocols.
    
    PiperOrigin-RevId: 465630548

--- tensorflow/python/data/experimental/kernel_tests/service/test_base.py
+++ tensorflow/python/data/experimental/kernel_tests/service/test_base.py
@@ -244,6 +244,7 @@ class TestBase(test_base.DatasetTestBase):
                                consumer_index=None,
                                num_consumers=None,
                                max_outstanding_requests=None,
+                               data_transfer_protocol=None,
                                compression="AUTO",
                                cross_trainer_cache=None,
                                target_workers="AUTO"):
@@ -257,6 +258,7 @@ class TestBase(test_base.DatasetTestBase):
             num_consumers=num_consumers,
             max_outstanding_requests=max_outstanding_requests,
             task_refresh_interval_hint_ms=20,
+            data_transfer_protocol=data_transfer_protocol,
             compression=compression,
             cross_trainer_cache=cross_trainer_cache,
             target_workers=target_workers))
@@ -267,6 +269,7 @@ class TestBase(test_base.DatasetTestBase):
                                      processing_mode="parallel_epochs",
                                      job_name=None,
                                      max_outstanding_requests=None,
+                                     data_transfer_protocol=None,
                                      compression="AUTO",
                                      cross_trainer_cache=None,
                                      target_workers="AUTO"):
@@ -277,6 +280,7 @@ class TestBase(test_base.DatasetTestBase):
         processing_mode=processing_mode,
         job_name=job_name,
         max_outstanding_requests=max_outstanding_requests,
+        data_transfer_protocol=data_transfer_protocol,
         compression=compression,
         cross_trainer_cache=cross_trainer_cache,
         target_workers=target_workers)
