31270adc512ea0e3ec373b6977458d814c85df02
A. Unique TensorFlower <gardener@tensorflow.org>
2022-08-06 22:06:13 -0700

    [mhlo] Implement bounded dynamism for unary/binary ops.
    PiperOrigin-RevId: 465829155

--- tensorflow/compiler/xla/mlir_hlo/include/mlir-hlo/Dialect/mhlo/IR/hlo_ops_base.h
+++ tensorflow/compiler/xla/mlir_hlo/include/mlir-hlo/Dialect/mhlo/IR/hlo_ops_base.h
@@ -16,16 +16,9 @@ limitations under the License.
 #ifndef MLIR_HLO_DIALECT_MHLO_IR_HLO_OPS_BASE_H
 #define MLIR_HLO_DIALECT_MHLO_IR_HLO_OPS_BASE_H
 
-#include <algorithm>
-
 #include "llvm/ADT/Sequence.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
-
-// Include order below matters.
-#include "mlir-hlo/Dialect/mhlo/IR/hlo_ops_base_enums.h.inc"
-#define GET_ATTRDEF_CLASSES
-#include "mlir-hlo/Dialect/mhlo/IR/hlo_ops_base_attrs.h.inc"
 
 namespace mlir {
 namespace mhlo {
@@ -88,19 +81,16 @@ class CompatibleOperandsAndResultType
   }
 
   static LogicalResult inferReturnTypes(
-      MLIRContext *context, Optional<Location> location, ValueRange operands,
-      DictionaryAttr /*attributes*/, RegionRange /*regions*/,
-      SmallVectorImpl<Type> &inferredReturnTypes) {
+      MLIRContext * /*context*/, Optional<Location> location,
+      ValueRange operands, DictionaryAttr /*attributes*/,
+      RegionRange /*regions*/, SmallVectorImpl<Type> &inferredReturnTypes) {
     // TODO(b/231358795): Review the use of InferTypeOpInterface for ops that
     // support quantization or sparsity.
     if (operands.empty())
       return emitOptionalError(
           location,
           "Expected non-empty operands for [CompatibleOperandsAndResultType]");
-
-    if (failed(inferMostSpecificType(context, location, operands.getTypes(),
-                                     inferredReturnTypes)))
-      return failure();
+    inferredReturnTypes.push_back(operands[0].getType());
     return success();
   }
 
@@ -117,91 +107,6 @@ class CompatibleOperandsAndResultType
       return failure();
     auto inferredReturnType = inferredReturnTypes[0].cast<ShapedType>();
     inferredReturnShapes.push_back(inferredReturnType);
-    return success();
-  }
-
- private:
-  // Cases of infer return shape with bounds (lhs and rhs are commutative):
-  //       Dim of lhs     Dim of rhs      Infer
-  //  c0:  3              3               3
-  //  c1:  3              ?               3
-  //  c2:  3              ?, bound=4      3
-  //  c3:  3              ?, bound=2      Error out
-  //  c4:  ?              ?               ?
-  //  c5:  ?              ?, bound=3      ?, bound=3
-  //  c6:  ?, bound=3     ?, bound=3      ?, bound=3
-  //  c7:  ?, bound=3     ?, bound=4      ?, bound=3
-  // This method generalizes it to multiple inputs: 1) get the static input dims
-  // (if any) as infer dim, and 2) get min of input bounds as infer bound
-  static LogicalResult inferMostSpecificType(
-      MLIRContext *context, Optional<Location> location,
-      ValueTypeRange<ValueRange> inputTypes,
-      SmallVectorImpl<Type> &inferredReturnTypes) {
-    // TODO(zhouxin) remove this part and find a way to infer sparsity encoding.
-    if (inputTypes.size() == 1) {
-      inferredReturnTypes.push_back(inputTypes[0]);
-      return success();
-    }
-
-    SmallVector<RankedTensorType> rankedTypes;
-    for (auto inputType : inputTypes)
-      if (auto rankedType = inputType.dyn_cast<RankedTensorType>())
-        rankedTypes.push_back(rankedType);
-    if (rankedTypes.empty()) {
-      inferredReturnTypes.push_back(inputTypes[0]);
-      return success();
-    }
-
-    auto rank = rankedTypes[0].getRank();
-    SmallVector<int64_t> inferredDimSizes(rank, ShapedType::kDynamicSize);
-    SmallVector<int64_t> inferredBounds(rank, ShapedType::kDynamicSize);
-    for (auto rankedType : rankedTypes) {
-      SmallVector<int64_t> bounds;
-      if (auto encoding =
-              rankedType.getEncoding().dyn_cast_or_null<TypeExtensionsAttr>())
-        bounds = llvm::to_vector<4>(encoding.getBounds());
-
-      for (int dim = 0; dim < rank; ++dim) {
-        // Dimensions
-        auto dimSize = rankedType.getShape()[dim];
-        if (inferredDimSizes[dim] != ShapedType::kDynamicSize &&
-            dimSize != ShapedType::kDynamicSize &&
-            inferredDimSizes[dim] != dimSize)
-          return emitOptionalError(location, "Mismatch dimension size ",
-                                   inferredDimSizes[dim], " and ", dimSize,
-                                   " in dimension ", dim);
-        if (inferredDimSizes[dim] == ShapedType::kDynamicSize)
-          inferredDimSizes[dim] = dimSize;
-
-        // Bounds
-        if (!bounds.empty() && bounds[dim] != ShapedType::kDynamicSize) {
-          if (inferredBounds[dim] == ShapedType::kDynamicSize) {
-            inferredBounds[dim] = bounds[dim];
-          } else {
-            inferredBounds[dim] = std::min(inferredBounds[dim], bounds[dim]);
-          }
-        }
-        // Error out case that the inferred bound is smaller than inferred dim
-        if (inferredBounds[dim] != ShapedType::kDynamicSize &&
-            inferredBounds[dim] < inferredDimSizes[dim])
-          return emitOptionalError(location,
-                                   "bound must not be less than static "
-                                   "dimension size but has bound ",
-                                   inferredBounds[dim], " vs static size ",
-                                   inferredDimSizes[dim], " in dimension ",
-                                   dim);
-        if (inferredDimSizes[dim] != ShapedType::kDynamicSize)
-          inferredBounds[dim] = ShapedType::kDynamicSize;
-      }
-    }
-
-    Attribute encoding = nullptr;
-    if (llvm::any_of(inferredBounds,
-                     [](auto el) { return el != ShapedType::kDynamicSize; }))
-      encoding = TypeExtensionsAttr::get(context, inferredBounds);
-    inferredReturnTypes.push_back(RankedTensorType::get(
-        inferredDimSizes, rankedTypes[0].getElementType(), encoding));
-
     return success();
   }
 };
