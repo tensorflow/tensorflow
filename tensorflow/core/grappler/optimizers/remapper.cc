1705c90b9ea76bc24e9573ca6a132786b74cf15b
Penporn Koanantakool <38085909+penpornk@users.noreply.github.com>
2022-08-09 12:10:46 -0700

    Add #endif comment

--- tensorflow/core/grappler/optimizers/remapper.cc
+++ tensorflow/core/grappler/optimizers/remapper.cc
@@ -3593,7 +3593,7 @@ Status Remapper::Optimize(Cluster* cluster, const GrapplerItem& item,
                              &invalidated_nodes, &nodes_to_delete));
       continue;
     }
-#endif
+#endif  // !DNNL_AARCH64_USE_ACL
 
     // Remap FusedBatchNorm+<SideInput>+<Activation> into the _FusedBatchNormEx.
     FusedBatchNormEx fused_batch_norm_ex;
