0c47be31687ef33eac65f11dc1613b9ea4bd7c09
Bernhard Bauer <bauerb@google.com>
2022-08-22 06:07:57 -0700

    Remove dependency on GPU delegate from `GPUCompatibilityList`
    Extract the `TfLiteGpuDelegateOptionsV2` struct and the `TfLiteGpuDelegateOptionsV2Default` method into a separate target that `GPUCompatibilityList` can depend on instead of the full delegate.
    
    Also remove a few unnecessary dependencies from the delegate target.
    
    PiperOrigin-RevId: 469164634

---
+++ tensorflow/lite/delegates/gpu/delegate_options.cc
@@ -0,0 +1,32 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/lite/delegates/gpu/delegate_options.h"
+
+TfLiteGpuDelegateOptionsV2 TfLiteGpuDelegateOptionsV2Default() {
+  TfLiteGpuDelegateOptionsV2 options;
+  // set it to -1 to detect whether it was later adjusted.
+  options.is_precision_loss_allowed = -1;
+  options.inference_preference =
+      TFLITE_GPU_INFERENCE_PREFERENCE_FAST_SINGLE_ANSWER;
+  options.inference_priority1 = TFLITE_GPU_INFERENCE_PRIORITY_MAX_PRECISION;
+  options.inference_priority2 = TFLITE_GPU_INFERENCE_PRIORITY_AUTO;
+  options.inference_priority3 = TFLITE_GPU_INFERENCE_PRIORITY_AUTO;
+  options.experimental_flags = TFLITE_GPU_EXPERIMENTAL_FLAGS_ENABLE_QUANT;
+  options.max_delegated_partitions = 1;
+  options.model_token = nullptr;
+  options.serialization_dir = nullptr;
+  return options;
+}
