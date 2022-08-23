933f43f29931238df9583bc286c4a8eb8d2278ab
A. Unique TensorFlower <gardener@tensorflow.org>
2022-08-23 11:31:08 -0700

    [XLA] Introduce a pass to reassociate op(reduce-scatter(x), reduce-scatter(y)) to reduce-scatter(op(x,y)).
    PiperOrigin-RevId: 469512061

---
+++ tensorflow/compiler/xla/service/reduce_scatter_reassociate_test.cc
@@ -0,0 +1,98 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/compiler/xla/service/reduce_scatter_reassociate.h"
+
+#include "tensorflow/compiler/xla/service/hlo_matchers.h"
+#include "tensorflow/compiler/xla/service/hlo_module.h"
+#include "tensorflow/compiler/xla/service/hlo_opcode.h"
+#include "tensorflow/compiler/xla/tests/hlo_test_base.h"
+
+namespace xla {
+namespace {
+
+namespace m = xla::testing::opcode_matchers;
+
+class ReduceScatterReassociateTest : public HloTestBase {
+ public:
+  StatusOr<std::unique_ptr<HloModule>> RunPass(absl::string_view hlo_module,
+                                               bool expect_change) {
+    TF_ASSIGN_OR_RETURN(auto module, ParseAndReturnVerifiedModule(hlo_module));
+    auto changed = ReduceScatterReassociate().Run(module.get());
+    if (!changed.ok()) {
+      return changed.status();
+    }
+    EXPECT_EQ(changed.ValueOrDie(), expect_change);
+    return StatusOr<std::unique_ptr<HloModule>>(std::move(module));
+  }
+
+  size_t ReduceScatterCount(std::unique_ptr<HloModule>& module) {
+    return absl::c_count_if(module->entry_computation()->instructions(),
+                            [](const HloInstruction* inst) {
+                              return inst->opcode() ==
+                                     HloOpcode::kReduceScatter;
+                            });
+  }
+};
+
+TEST_F(ReduceScatterReassociateTest, Simple) {
+  absl::string_view hlo_string = R"(
+HloModule m
+
+sum {
+  a = f32[] parameter(0)
+  b = f32[] parameter(1)
+  ROOT add.2 = f32[] add(a, b)
+}
+
+ENTRY main {
+  p0 = f32[8] parameter(0)
+  p1 = f32[8] parameter(1)
+  rs0 = f32[4] reduce-scatter(p0), replica_groups={{0,1}}, dimensions={0}, to_apply=sum
+  rs1 = f32[4] reduce-scatter(p1), replica_groups={{0,1}}, dimensions={0}, to_apply=sum
+  ROOT add = f32[4] add(rs0, rs1)
+}
+)";
+  TF_ASSERT_OK_AND_ASSIGN(auto module,
+                          RunPass(hlo_string, /*expect_change=*/true));
+  EXPECT_THAT(module->entry_computation()->root_instruction(),
+              m::ReduceScatter(m::Add(m::Parameter(0), m::Parameter(1))));
+  EXPECT_EQ(ReduceScatterCount(module), 1);
+}
+
+TEST_F(ReduceScatterReassociateTest, SimpleConstrainedLayout) {
+  absl::string_view hlo_string = R"(
+HloModule m
+
+sum {
+  a = f32[] parameter(0)
+  b = f32[] parameter(1)
+  ROOT add.2 = f32[] add(a, b)
+}
+
+ENTRY main {
+  p0 = f32[8] parameter(0)
+  p1 = f32[8] parameter(1)
+  rs0 = f32[4] reduce-scatter(p0), replica_groups={{0,1}}, dimensions={0}, constrain_layout=true, to_apply=sum
+  rs1 = f32[4] reduce-scatter(p1), replica_groups={{0,1}}, dimensions={0}, constrain_layout=true, to_apply=sum
+  ROOT add = f32[4] add(rs0, rs1)
+}
+)";
+  TF_ASSERT_OK_AND_ASSIGN(auto module,
+                          RunPass(hlo_string, /*expect_change=*/false));
+}
+
+}  // namespace
+}  // namespace xla
