42b1ea503080c4d7067ca0c49ecffc755770f0c2
Jake <jakeh@graphcore.ai>
2022-08-25 11:35:08 +0100

    Fix typo.

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -117,7 +117,7 @@ def make_plugin_device_client():
     return _xla.get_plugin_device_client()
   except AttributeError as e:
     raise AttributeError(
-      'xla_extension has no attributes named get_plugin_device_client. `
+      'xla_extension has no attributes named get_plugin_device_client. '
       'Compile TensorFlow with '
       '//tensorflow/compiler/xla/python:enable_plugin_device set to true '
       '(defaults to false) to enable this.') from e
