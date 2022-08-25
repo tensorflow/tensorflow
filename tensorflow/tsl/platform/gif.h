cb3805ae2fb18f2311f963b13c0d4916fadbb5d1
Jake Harmon <jakeharmon@google.com>
2022-08-24 18:08:15 -0700

    Refactor core/platform/gif.h into TSL
    This is a header in core/platform with no dependencies in TF
    
    PiperOrigin-RevId: 469871927

--- tensorflow/tsl/platform/gif.h
+++ tensorflow/tsl/platform/gif.h
@@ -13,9 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_CORE_PLATFORM_GIF_H_
-#define TENSORFLOW_CORE_PLATFORM_GIF_H_
+#ifndef TENSORFLOW_TSL_PLATFORM_GIF_H_
+#define TENSORFLOW_TSL_PLATFORM_GIF_H_
 
-#include "tensorflow/tsl/platform/gif.h"
+#include "gif_lib.h"  // from @gif
 
-#endif  // TENSORFLOW_CORE_PLATFORM_GIF_H_
+#endif  // TENSORFLOW_TSL_PLATFORM_GIF_H_
