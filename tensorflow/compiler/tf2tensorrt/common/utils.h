edbb047ae33c9b82bbada20601e0116d1a48f921
andreii <andreii@nvidia.com>
2022-08-29 18:50:31 -0700

    Removing the names of nodes from parameter lists.

--- tensorflow/compiler/tf2tensorrt/common/utils.h
+++ tensorflow/compiler/tf2tensorrt/common/utils.h
@@ -41,25 +41,23 @@ std::tuple<int, int, int> GetLoadedTensorRTVersion();
 #include "tensorflow/core/platform/status.h"
 #include "third_party/tensorrt/NvInfer.h"
 
+#define ERROR_LOC   __FILE__, ":", __LINE__
+
 #define TFTRT_INTERNAL_ERROR_AT_NODE(node)                           \
-  do {                                                               \
-    return errors::Internal("TFTRT::", __FUNCTION__, ":", __LINE__,  \
-                            " failed to add TRT layer, at: ", node); \
-  } while (0)
+  return errors::Internal("TFTRT::", __FUNCTION__, "\n",  ERROR_LOC, \
+                          " failed to add TRT layer, at: ", node);
 
 #define TFTRT_RETURN_ERROR_IF_NULLPTR(ptr, node) \
-  do {                                           \
-    if (ptr == nullptr) {                        \
-      TFTRT_INTERNAL_ERROR_AT_NODE(node);        \
-    }                                            \
-  } while (0)
+  if (ptr == nullptr) {                          \
+    TFTRT_INTERNAL_ERROR_AT_NODE(node);          \
+  }
 
 // Use this macro within functions that return a Status or StatusOR<T> to check
 // boolean conditions. If the condition fails, it returns an
 // errors::Internal message with the file and line number.
-#define TRT_ENSURE(x)                                                        \
-  if (!(x)) {                                                                \
-    return errors::Internal(__FILE__, ":", __LINE__, " TRT_ENSURE failure"); \
+#define TRT_ENSURE(x)                                          \
+  if (!(x)) {                                                  \
+    return errors::Internal(ERROR_LOC, " TRT_ENSURE failure"); \
   }
 
 // Checks that a Status or StatusOr<T> object does not carry an error message.
@@ -68,7 +66,7 @@ std::tuple<int, int, int> GetLoadedTensorRTVersion();
 // pointer-containing StatusOr<T*>, use the below TRT_ENSURE_PTR_OK macro.
 #define TRT_ENSURE_OK(x)                                   \
   if (!x.ok()) {                                           \
-    return errors::Internal(__FILE__, ":", __LINE__,       \
+    return errors::Internal(ERROR_LOC,                     \
                             " TRT_ENSURE_OK failure:\n  ", \
                             x.status().ToString());        \
   }
@@ -80,7 +78,7 @@ std::tuple<int, int, int> GetLoadedTensorRTVersion();
 #define TRT_ENSURE_PTR_OK(x)                            \
   TRT_ENSURE_OK(x);                                     \
   if (*x == nullptr) {                                  \
-    return errors::Internal(__FILE__, ":", __LINE__,    \
+    return errors::Internal(ERROR_LOC,                  \
                             " pointer had null value"); \
   }
 
