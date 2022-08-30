56973fb11a0c00df2f559feab6a4b725514a7e51
A. Unique TensorFlower <gardener@tensorflow.org>
2022-08-30 13:47:52 -0700

    Fixed header guards to match style guide conventions.
    PiperOrigin-RevId: 471080363

--- tensorflow/core/transforms/graph_to_func/pass.h
+++ tensorflow/core/transforms/graph_to_func/pass.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_CORE_MLIR_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
-#define TENSORFLOW_CORE_MLIR_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
+#ifndef TENSORFLOW_CORE_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
+#define TENSORFLOW_CORE_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
 
 #include <memory>
 #include <string>
@@ -35,4 +35,4 @@ std::unique_ptr<Pass> CreateGraphToFuncPass(
 }  // namespace tfg
 }  // namespace mlir
 
-#endif  // TENSORFLOW_CORE_MLIR_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
+#endif  // TENSORFLOW_CORE_TRANSFORMS_GRAPH_TO_FUNC_PASS_H_
--- tensorflow/lite/delegates/gpu/common/tasks/mean_stddev_normalization.h
+++ tensorflow/lite/delegates/gpu/common/tasks/mean_stddev_normalization.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_LSTM_NORMALIZATION_H_
-#define TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_LSTM_NORMALIZATION_H_
+#ifndef TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_MEAN_STDDEV_NORMALIZATION_H_
+#define TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_MEAN_STDDEV_NORMALIZATION_H_
 
 #include <map>
 #include <set>
@@ -125,4 +125,4 @@ LayerNormalization CreateLayerNormalization(
 }  // namespace gpu
 }  // namespace tflite
 
-#endif  // TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_LSTM_NORMALIZATION_H_
+#endif  // TENSORFLOW_LITE_DELEGATES_GPU_COMMON_TASKS_MEAN_STDDEV_NORMALIZATION_H_
