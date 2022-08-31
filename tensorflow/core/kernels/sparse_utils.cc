0b09f5e66c237d3e9f6ce03ba31d0a2e14b3305f
Mihai Maruseac <mihai.maruseac@gmail.com>
2022-08-31 14:07:28 -0700

    Fix build error

--- tensorflow/core/kernels/sparse_utils.cc
+++ tensorflow/core/kernels/sparse_utils.cc
@@ -318,7 +318,6 @@ Status ValidateSparseTensor(const Tensor& indices, const Tensor& values,
       const std::vector<TypeIndex>& row_start_indices);                     \
   template std::vector<TypeIndex> ParseRowStartIndices<TypeIndex>(          \
       const tensorflow::Tensor& tensor,                                     \
-      const TypeIndex num_nonzero_entries_in_sparse_mat);
       const TypeIndex num_nonzero_entries_in_sparse_mat);                   \
   template Status ValidateSparseTensor<TypeIndex>(                          \
       const Tensor& indices, const Tensor& values, const Tensor& shape,     \
