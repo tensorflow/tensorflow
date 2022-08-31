e9c859885cd72f25d9548ebda93f02f2364cec6e
Sagun Bajra <sagunb@google.com>
2022-08-30 17:34:09 -0700

    Add support for tf.custom_gradients in the function transformation api.
    PiperOrigin-RevId: 471131422

--- tensorflow/core/function/transform/transform_test.py
+++ tensorflow/core/function/transform/transform_test.py
@@ -23,6 +23,7 @@ from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import tensor_spec
 from tensorflow.python.framework import test_util
+from tensorflow.python.ops import custom_gradient
 from tensorflow.python.ops import gradients_impl
 from tensorflow.python.ops import math_ops
 from tensorflow.python.platform import test
@@ -85,6 +86,56 @@ class TransformTest(test.TestCase, parameterized.TestCase):
 
     self.assertEqual(
         f_g(constant_op.constant(2.0), constant_op.constant(3.0)), 5.0)
+
+  @test_util.run_v2_only
+  def test_transform_with_custom_gradients(self):
+
+    @custom_gradient.custom_gradient
+    def add(x, y):
+      e = math_ops.add(x, y, name="x_plus_y")
+
+      # custom gradient that returns gradients of x * y instead of x + y
+      def grad(upstream):
+        dz_dx = y
+        dz_dy = x
+        return upstream * dz_dx, upstream * dz_dy
+
+      return e, grad
+
+    @def_function.function
+    def f(x, y):
+      return add(x, y)
+
+    one = constant_op.constant(1.0)
+    f = transform.transform_function(
+        f, inputs=[one, one], transform_fn=add_to_multiply)
+    self.assertEqual(f(one, one), 1.0)
+
+    @def_function.function
+    def f_g(x, y):
+      z = f(x, y)
+      dz_dx, dz_dy = gradients_impl.gradients(z, [x, y])
+      return math_ops.add(dz_dx, dz_dy)
+
+    self.assertEqual(
+        f_g(constant_op.constant(2.0), constant_op.constant(3.0)), 5.0)
+
+  @test_util.run_v2_only
+  def test_transform_with_nested_function(self):
+
+    @def_function.function
+    def f(x, z):
+
+      @def_function.function
+      def add():
+        return math_ops.add(x, z)
+      y = add()
+      return math_ops.add(x, y, name="x_plus_y")
+
+    one = constant_op.constant(1.0)
+    f = transform.transform_function(
+        f, inputs=[one, one], transform_fn=add_to_multiply)
+    self.assertEqual(f(one, one), 2.0)
 
 
 if __name__ == "__main__":
