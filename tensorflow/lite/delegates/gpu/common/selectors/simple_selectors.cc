6b43abe3affe204189d4a8fc1156b0b7cb265a43
Raman Sarokin <sorokin@google.com>
2022-08-31 03:59:24 -0700

    Improved Softmax selection.
    PiperOrigin-RevId: 471220667

--- tensorflow/lite/delegates/gpu/common/selectors/simple_selectors.cc
+++ tensorflow/lite/delegates/gpu/common/selectors/simple_selectors.cc
@@ -201,7 +201,7 @@ std::unique_ptr<GPUOperation> SelectReduce(const std::set<Axis>& axis_to_reduce,
 
 void SelectSoftmax(const BHWC& shape, const OperationDef& op_def,
                    std::unique_ptr<GPUOperation>* ptr) {
-  if (shape.w == 1 && shape.h == 1) {
+  if (shape.w * shape.h <= 16) {
     Softmax1x1 operation = CreateSoftmax1x1(op_def);
     *ptr = std::make_unique<Softmax1x1>(std::move(operation));
   } else {
