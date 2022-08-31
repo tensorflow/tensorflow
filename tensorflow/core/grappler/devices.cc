da9a9a098667e2f11bdea24540483293a2454a19
Jean-Baptiste Lespiau <jblespiau@google.com>
2022-08-31 09:34:03 -0700

    Use `value` instead of `ValueOrDie`.
    PiperOrigin-RevId: 471275777

--- tensorflow/core/grappler/devices.cc
+++ tensorflow/core/grappler/devices.cc
@@ -86,7 +86,7 @@ int64_t AvailableGPUMemory(int gpu_id) {
   // Look up the device, to see its attributes.
   se::Platform* gpu_platform = GPUMachineManager();
   CHECK_LT(gpu_id, gpu_platform->VisibleDeviceCount());
-  se::StreamExecutor* se = gpu_platform->ExecutorForDevice(gpu_id).ValueOrDie();
+  se::StreamExecutor* se = gpu_platform->ExecutorForDevice(gpu_id).value();
   int64_t total_memory, available_memory;
   CHECK(se->DeviceMemoryUsage(&available_memory, &total_memory));
 
