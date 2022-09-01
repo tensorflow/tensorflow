6c574581343c857f36e403b30eb5b2608414324e
Yaning Liang <yaning@google.com>
2022-09-01 08:46:23 -0700

    Implementation for checkpoint_view._descendants_with_paths().
    PiperOrigin-RevId: 471542349

--- tensorflow/python/checkpoint/checkpoint_view.py
+++ tensorflow/python/checkpoint/checkpoint_view.py
@@ -101,17 +101,30 @@ class CheckpointView(object):
   def descendants(self):
     """Returns a list of trackables by node_id attached to obj."""
 
-    all_nodes = []
+    return list(self._descendants_with_paths().keys())
+
+  def _descendants_with_paths(self):
+    """Returns a dict of descendants by node_id and paths to node.
+
+    The names returned by this private method are subject to change.
+    """
+
+    all_nodes_with_paths = {}
     to_visit = collections.deque([0])
-    all_nodes.append(0)
+    # node_id:0 will always be "root".
+    all_nodes_with_paths[0] = "root"
+    path = all_nodes_with_paths.get(0)
     while to_visit:
       node_id = to_visit.popleft()
       obj = self._object_graph_proto.nodes[node_id]
       for child in obj.children:
-        if child.node_id not in all_nodes:
-          all_nodes.append(child.node_id)
+        if child.node_id == 0 or child.node_id in all_nodes_with_paths.keys():
+          continue
+        path = all_nodes_with_paths.get(node_id)
+        if child.node_id not in all_nodes_with_paths.keys():
           to_visit.append(child.node_id)
-    return all_nodes
+        all_nodes_with_paths[child.node_id] = path + "." + child.local_name
+    return all_nodes_with_paths
 
   def match(self, obj):
     """Returns all matching trackables between CheckpointView and Trackable.
--- tensorflow/python/checkpoint/checkpoint_view_test.py
+++ tensorflow/python/checkpoint/checkpoint_view_test.py
@@ -47,6 +47,24 @@ class CheckpointViewTest(test.TestCase):
     self.assertEqual(0, all_nodes[0])
     self.assertEqual(1, all_nodes[1])
 
+  def test_all_nodes_with_paths(self):
+    root = autotrackable.AutoTrackable()
+    leaf1 = root.leaf1 = autotrackable.AutoTrackable()
+    leaf2 = root.leaf2 = autotrackable.AutoTrackable()
+    leaf1.leaf3 = autotrackable.AutoTrackable()
+    leaf1.leaf4 = autotrackable.AutoTrackable()
+    leaf2.leaf5 = autotrackable.AutoTrackable()
+    root_ckpt = trackable_utils.Checkpoint(root=root)
+    root_save_path = root_ckpt.save(
+        os.path.join(self.get_temp_dir(), "root_ckpt"))
+    all_nodes_with_paths = checkpoint_view.CheckpointView(
+        root_save_path)._descendants_with_paths()
+    self.assertEqual(
+        {
+            "root", "root.leaf1", "root.leaf2", "root.save_counter",
+            "root.leaf1.leaf3", "root.leaf1.leaf4", "root.leaf2.leaf5"
+        }, set(all_nodes_with_paths.values()))
+
   def test_match(self):
     root1 = autotrackable.AutoTrackable()
     leaf1 = root1.leaf1 = autotrackable.AutoTrackable()
