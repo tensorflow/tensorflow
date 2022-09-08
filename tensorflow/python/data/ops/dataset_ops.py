796ee14770c0f4ab75612d527ac2e5a86d39a72d
Matt Callanan <mpcallanan@google.com>
2022-09-08 15:01:37 -0700

    Graduate `tf.data.experimental.ignore_errors` to `tf.data.Dataset.ignore_errors`.
    PiperOrigin-RevId: 473093537

--- tensorflow/python/data/ops/dataset_ops.py
+++ tensorflow/python/data/ops/dataset_ops.py
@@ -2284,6 +2284,32 @@ name=None))
     """
     return FlatMapDataset(self, map_func, name=name)
 
+  def ignore_errors(self, log_warning=False, name=None):
+    """Drops elements that cause errors.
+
+    >>> dataset = tf.data.Dataset.from_tensor_slices([1., 2., 0., 4.])
+    >>> dataset = dataset.map(lambda x: tf.debugging.check_numerics(1. / x, ""))
+    >>> list(dataset.as_numpy_iterator())
+    Traceback (most recent call last):
+    ...
+    InvalidArgumentError: ... Tensor had Inf values
+    >>> dataset = dataset.ignore_errors()
+    >>> list(dataset.as_numpy_iterator())
+    [1.0, 0.5, 0.25]
+
+    Args:
+      log_warning: (Optional.) A bool indicating whether or not ignored errors
+        should be logged to stderr. Defaults to `False`.
+      name: (Optional.) A string indicating a name for the `tf.data` operation.
+
+    Returns:
+      A new `Dataset` with the transformation applied as described above.
+    """
+    # Loaded lazily due to a circular dependency (dataset_ops ->
+    # ignore_errors_op -> dataset_ops).
+    from tensorflow.python.data.ops import ignore_errors_op  # pylint: disable=g-import-not-at-top
+    return ignore_errors_op.ignore_errors(self, log_warning, name)
+
   def interleave(self,
                  map_func,
                  cycle_length=None,
