95602e4af81cc4c2c68ab738975f7e80751bff39
Mahmoud Abuzaina <mahmoud.abuzaina@intel.com>
2022-09-14 12:42:24 -0700

    Removed unnecessary header file inclusion

--- tensorflow/core/kernels/mkl/mkl_kernel_util.h
+++ tensorflow/core/kernels/mkl/mkl_kernel_util.h
@@ -26,7 +26,6 @@ limitations under the License.
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor_testutil.h"
 #include "tensorflow/core/framework/types.h"
-#include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/kernels/ops_testutil.h"
 #include "tensorflow/core/kernels/ops_util.h"
 #include "tensorflow/core/kernels/quantization_utils.h"
