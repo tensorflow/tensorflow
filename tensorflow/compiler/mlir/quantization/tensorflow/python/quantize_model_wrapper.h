f80cc1f6b96d38d9cedd187e8087cf07137cccf7
Dan Suh <dansuh@google.com>
2022-09-13 17:20:57 -0700

    Extend the use of pybind for connecting cpp quantizer API to python API.
    Instead of directly using python's C API (like `PyObject*`), use pybind for clarity and simplicity. We can also avoid multiple casting to c-style strings.
    
    * Throwing python error.
    * Returning python `bytes`.
    
    PiperOrigin-RevId: 474163888

--- tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model_wrapper.h
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model_wrapper.h
@@ -15,33 +15,28 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_MLIR_QUANTIZATION_TENSORFLOW_PYTHON_QUANTIZE_MODEL_WRAPPER_H_
 #define TENSORFLOW_COMPILER_MLIR_QUANTIZATION_TENSORFLOW_PYTHON_QUANTIZE_MODEL_WRAPPER_H_
 
-#include <pybind11/stl.h>
-
 #include <string>
 
 #include "absl/strings/string_view.h"
-#include "pybind11/pybind11.h"
-#include "pybind11/stl.h"
-#include "tensorflow/python/lib/core/pybind11_lib.h"
 
 namespace tensorflow {
 namespace quantization {
 
-PyObject* QuantizeQatModel(absl::string_view saved_model_path,
-                           absl::string_view exported_names_str,
-                           absl::string_view tags,
-                           absl::string_view quant_opts_serialized);
+std::string QuantizeQatModel(absl::string_view saved_model_path,
+                             absl::string_view exported_names_str,
+                             absl::string_view tags,
+                             absl::string_view quant_opts_serialized);
 
-PyObject* QuantizePtqDynamicRange(absl::string_view saved_model_path,
-                                  absl::string_view exported_names_str,
-                                  absl::string_view tags,
-                                  absl::string_view quant_opts_serialized);
+std::string QuantizePtqDynamicRange(absl::string_view saved_model_path,
+                                    absl::string_view exported_names_str,
+                                    absl::string_view tags,
+                                    absl::string_view quant_opts_serialized);
 
-PyObject* QuantizePtqModelPreCalibration(absl::string_view saved_model_path,
-                                         absl::string_view exported_names_str,
-                                         absl::string_view tags);
+std::string QuantizePtqModelPreCalibration(absl::string_view saved_model_path,
+                                           absl::string_view exported_names_str,
+                                           absl::string_view tags);
 
-PyObject* QuantizePtqModelPostCalibration(
+std::string QuantizePtqModelPostCalibration(
     absl::string_view saved_model_path, absl::string_view exported_names_str,
     absl::string_view tags, absl::string_view quant_opts_serialized);
 
