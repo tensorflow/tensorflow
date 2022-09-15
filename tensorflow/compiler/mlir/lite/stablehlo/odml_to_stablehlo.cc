e2cb87c8ec693f7b9875490dfc5001e193090a8c
Eric Yang <yijieyang@google.com>
2022-09-15 12:55:22 -0700

    Upgrade `odml_to_stablehlo` to output TFLite Flatbuffers
    odml_to_stablehlo /path/to/model -o model.tflite --export-type=tflite # export-type flag specifies whether we will export TFLite Flatbuffers or MLIR as output (currently support ["tflite", "mlir"] and default is "mlir")
    
    PiperOrigin-RevId: 474635197

--- tensorflow/compiler/mlir/lite/stablehlo/odml_to_stablehlo.cc
+++ tensorflow/compiler/mlir/lite/stablehlo/odml_to_stablehlo.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <algorithm>
+#include <iterator>
 #include <memory>
 #include <string>
 #include <unordered_set>
@@ -45,7 +47,9 @@ limitations under the License.
 #include "stablehlo/dialect/Register.h"  // from @stablehlo
 #include "tensorflow/cc/saved_model/loader.h"
 #include "tensorflow/compiler/mlir/init_mlir.h"
+#include "tensorflow/compiler/mlir/lite/flatbuffer_export.h"
 #include "tensorflow/compiler/mlir/lite/stablehlo/transforms/check_accepted_ops_pass.h"
+#include "tensorflow/compiler/mlir/lite/stablehlo/transforms/mhlo_tfl_pass.h"
 #include "tensorflow/compiler/mlir/lite/stablehlo/transforms/op_stat_pass.h"
 #include "tensorflow/compiler/mlir/lite/stablehlo/transforms/transforms.h"
 #include "tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h"
@@ -69,10 +73,13 @@ limitations under the License.
 // graphs, and lower them to StableHLO graphs for use by downstream tools.
 //
 // Input: TF Saved Model or TF MLIR files.
-// Output: StableHLO MLIR
+// Output: StableHLO MLIR or TFLite Flatbuffer.
 //
 // Usage:
-// odml_to_stablehlo /path/to/model -output=model.mlir
+// 1) convert the input model to StableHLO MLIR
+// odml_to_stablehlo /path/to/model -o=model.mlir
+// 2) convert the input model to TFLite Flatbuffer
+// odml_to_stablehlo /path/to/model -o=model.tflite --export-type=tflite
 //
 // TODO(pulkitb): Add more options.
 //  * Verbose - dump intermediate translations.
@@ -90,6 +97,10 @@ opt<std::string> input_model(llvm::cl::Positional,
 // NOLINTNEXTLINE
 opt<std::string> output_path("o", llvm::cl::desc("<output path>"),
                              llvm::cl::Required);
+
+// NOLINTNEXTLINE
+opt<std::string> export_type("export-type", llvm::cl::desc("<export type>"),
+                             llvm::cl::Optional, llvm::cl::init("mlir"));
 
 // NOLINTNEXTLINE
 opt<bool> verbose(
@@ -157,6 +168,26 @@ tensorflow::StatusOr<OwningOpRef<mlir::ModuleOp>> ImportSavedModelOrMLIR(
                           /*enable_variable_lifting=*/true, context, &bundle);
 }
 
+tensorflow::Status ConvertStableHLOToFlatbuffer(mlir::ModuleOp module,
+                                                std::string* flatbuffer_str) {
+  // Convert StableHLO MLIR to TFLite Custom Op MLIR
+  mlir::PassManager mhlo_tfl_pm(module->getContext());
+  mhlo_tfl_pm.addNestedPass<func::FuncOp>(TFL::mhlo::CreateMhloToTflPass());
+  if (failed(mhlo_tfl_pm.run(module))) {
+    return tensorflow::errors::Aborted("HLO to TFL passes failed.");
+  }
+
+  // Convert TFLite Custom Op MLIR to TFLite Flatbuffer
+  tflite::FlatbufferExportOptions options;
+  options.toco_flags.allow_custom_ops();
+  if (!tflite::MlirToFlatBufferTranslateFunction(module, options,
+                                                 flatbuffer_str)) {
+    return tensorflow::errors::Aborted("Unable to export flatbuffer");
+  }
+
+  return ::tensorflow::OkStatus();
+}
+
 tensorflow::Status ExportModule(mlir::ModuleOp module,
                                 const std::string& output_filename,
                                 bool elide_large_elements_attrs) {
@@ -167,6 +198,21 @@ tensorflow::Status ExportModule(mlir::ModuleOp module,
     return tensorflow::errors::Aborted("Unable to write to output path.");
   }
 
+  // Export TFLite Flatbuffer as output
+  if (export_type == "tflite") {
+    std::string flatbuffer_str;
+    auto status =
+        mlir::odml::ConvertStableHLOToFlatbuffer(module, &flatbuffer_str);
+    if (!status.ok()) {
+      return status;
+    }
+
+    output->os() << flatbuffer_str;
+    output->keep();
+    return ::tensorflow::OkStatus();
+  }
+
+  // Export StableHLO MLIR as output
   std::string result;
   llvm::raw_string_ostream os(result);
   OpPrintingFlags printing_flags;
@@ -178,6 +224,7 @@ tensorflow::Status ExportModule(mlir::ModuleOp module,
 
   output->os() << result;
   output->keep();
+
   return ::tensorflow::OkStatus();
 }
 
@@ -236,6 +283,13 @@ tensorflow::Status RunConverter(const PassPipelineCLParser& pass_pipeline) {
   SourceMgrDiagnosticHandler sourceMgrHandler(source_mgr, &context);
 
   auto verbose_dir = llvm::sys::path::parent_path(output_path).str();
+
+  const char* type_set[] = {"tflite", "mlir"};
+  bool found = std::find(std::begin(type_set), std::end(type_set),
+                         export_type) != std::end(type_set);
+  if (!found) {
+    return tensorflow::errors::Aborted("Export type is not supported.");
+  }
 
   TF_ASSIGN_OR_RETURN(
       auto module, ImportSavedModelOrMLIR(input_model, &context, &source_mgr));
