3782a8ad97b4e1eeb245f06eefefa0817f15da3f
A. Unique TensorFlower <gardener@tensorflow.org>
2022-09-15 11:02:51 -0700

    Deprecate `Status::OK`, prefer `OkStatus()`.
    PiperOrigin-RevId: 474606717

--- tensorflow/tsl/platform/status.h
+++ tensorflow/tsl/platform/status.h
@@ -24,7 +24,6 @@ limitations under the License.
 #include <unordered_map>
 #include <utility>
 
-#include "absl/base/attributes.h"
 #include "absl/status/status.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/string_view.h"
@@ -97,9 +96,6 @@ class Status {
 #endif  // SWIG
 
   // Prefer using OkStatus().
-  ABSL_DEPRECATED(
-      "Use `OkStatus()` (preferred) or `Status()` (which is backward "
-      "compatible with TF v2.9 and lower) instead.")
   static Status OK() { return Status(); }
 
   /// Returns true iff the status indicates success.
