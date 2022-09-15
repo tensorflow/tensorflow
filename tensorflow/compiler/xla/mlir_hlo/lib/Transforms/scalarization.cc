6f42ae7a4e59d73fd7d6b0cb961329e80f9b3604
Alexander Belyaev <pifon@google.com>
2022-09-15 12:49:15 -0700

    [GmlSt] Add scalarization of thlo.scatter.
    PiperOrigin-RevId: 474633831

--- tensorflow/compiler/xla/mlir_hlo/lib/Transforms/scalarization.cc
+++ tensorflow/compiler/xla/mlir_hlo/lib/Transforms/scalarization.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
+#include "mlir-hlo/Dialect/thlo/IR/thlo_ops.h"
 #include "mlir-hlo/Transforms/passes.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
@@ -30,14 +31,17 @@ namespace {
 #include "mlir-hlo/Transforms/passes.h.inc"
 
 using linalg::GenericOp;
+using tensor::ExtractOp;
+using tensor::FromElementsOp;
+using tensor::InsertOp;
 
 bool hasSingleElement(RankedTensorType type) {
   return type.hasStaticShape() && type.getNumElements() == 1;
 }
 
-class ScalarizeGenericOp : public OpRewritePattern<GenericOp> {
- public:
+struct ScalarizeGenericOp : public OpRewritePattern<GenericOp> {
   using OpRewritePattern::OpRewritePattern;
+
   LogicalResult matchAndRewrite(GenericOp genericOp,
                                 PatternRewriter &rewriter) const override {
     if (llvm::any_of(genericOp->getResultTypes(), [](Type resultType) {
@@ -63,7 +67,7 @@ class ScalarizeGenericOp : public OpRewritePattern<GenericOp> {
           tensorType.getRank(),
           rewriter.create<arith::ConstantIndexOp>(loc, 0));
       Value extractedElement =
-          rewriter.create<tensor::ExtractOp>(loc, operandValue, indices);
+          rewriter.create<ExtractOp>(loc, operandValue, indices);
       bvm.map(bbArg, extractedElement);
     }
 
@@ -77,8 +81,8 @@ class ScalarizeGenericOp : public OpRewritePattern<GenericOp> {
          llvm::zip(genericOp->getResultTypes(),
                    body->getTerminator()->getOperands())) {
       auto scalarValue = bvm.lookup(yieldOperand);
-      newResults.push_back(rewriter.create<tensor::FromElementsOp>(
-          loc, resultType, scalarValue));
+      newResults.push_back(
+          rewriter.create<FromElementsOp>(loc, resultType, scalarValue));
     }
     rewriter.replaceOp(genericOp, newResults);
 
@@ -86,17 +90,67 @@ class ScalarizeGenericOp : public OpRewritePattern<GenericOp> {
   }
 };
 
+struct ScalarizeScatterOp : public OpRewritePattern<thlo::ScatterOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(thlo::ScatterOp scatterOp,
+                                PatternRewriter &rewriter) const override {
+    Location loc = scatterOp.getLoc();
+    Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+
+    // Extract update.
+    Value updates = scatterOp.updates();
+    auto updatesType = updates.getType().dyn_cast<RankedTensorType>();
+    if (!updatesType || !hasSingleElement(updatesType)) return failure();
+    SmallVector<Value> updateIndices(updatesType.getRank(), zero);
+    Value updateValue = rewriter.create<ExtractOp>(loc, updates, updateIndices);
+
+    // Extract/compute index.
+    Value indices = scatterOp.indices();
+    auto indicesType = indices.getType().dyn_cast<RankedTensorType>();
+    SmallVector<Value> indicesIndices(indicesType.getRank(), zero);
+
+    Value init = scatterOp.init();
+    auto initType = init.getType().dyn_cast<RankedTensorType>();
+
+    SmallVector<Value> scatterIndices;
+    Type indexType = rewriter.getIndexType();
+    for (int64_t i = 0, e = initType.getRank(); i < e; ++i) {
+      indicesIndices.back() = rewriter.create<arith::ConstantIndexOp>(loc, i);
+      Value index = rewriter.create<ExtractOp>(loc, indices, indicesIndices);
+      if (index.getType() != indexType)
+        index = rewriter.create<arith::IndexCastOp>(loc, indexType, index);
+      scatterIndices.push_back(index);
+    }
+
+    // Extract the current value from the output tensor.
+    Value currentValue = rewriter.create<ExtractOp>(loc, init, scatterIndices);
+
+    // Combine update with the value in the output.
+    Block *body = scatterOp.getBody();
+    BlockAndValueMapping bvm;
+    bvm.map(body->getArgument(0), updateValue);
+    bvm.map(body->getArgument(1), currentValue);
+
+    for (Operation &op : body->without_terminator()) rewriter.clone(op, bvm);
+
+    // Wrap every scalar result into a tensor using `tensor.from_elements`.
+    auto combinedValue = bvm.lookup(body->getTerminator()->getOperand(0));
+    rewriter.replaceOpWithNewOp<InsertOp>(scatterOp, combinedValue, init,
+                                          scatterIndices);
+    return success();
+  }
+};
+
 struct ScalarizationPass
     : public impl::ScalarizationPassBase<ScalarizationPass> {
-  void getDependentDialects(DialectRegistry &registry) const override {}
-
   void runOnOperation() override {
     auto func = getOperation();
     auto *context = &getContext();
 
     RewritePatternSet patterns(context);
-    patterns.add<ScalarizeGenericOp>(context);
-    tensor::FromElementsOp::getCanonicalizationPatterns(patterns, context);
+    patterns.add<ScalarizeGenericOp, ScalarizeScatterOp>(context);
+    FromElementsOp::getCanonicalizationPatterns(patterns, context);
     if (failed(applyPatternsAndFoldGreedily(func, std::move(patterns))))
       signalPassFailure();
   }
