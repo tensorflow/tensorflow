cbae62dabb8feea4717265870bf070361cd8c486
A. Unique TensorFlower <gardener@tensorflow.org>
2022-09-15 20:34:04 -0700

    Update post-training quantization docs to be clearer.
    PiperOrigin-RevId: 474719867

--- tensorflow/lite/python/lite.py
+++ tensorflow/lite/python/lite.py
@@ -100,11 +100,11 @@ class Optimize(enum.Enum):
   """Enum defining the optimizations to apply when generating a tflite model.
 
   DEFAULT
-      Default optimization strategy that quantizes model weights. Enhanced
-      optimizations are gained by providing a representative dataset that
-      quantizes biases and activations as well.
-      Converter will do its best to reduce size and latency, while minimizing
-      the loss in accuracy.
+      The default optimization strategy that enables post-training quantization.
+      The type of post-training quantization that will be used is dependent on
+      the other converter options supplied. Refer to the
+      [documentation](/lite/performance/post_training_quantization) for further
+      information on the types available and how to use them.
 
   OPTIMIZE_FOR_SIZE
       Deprecated. Does the same as DEFAULT.
