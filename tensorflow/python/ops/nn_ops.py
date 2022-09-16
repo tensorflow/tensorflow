e3f317ce730f2cdef21cc013fa5111f3e5b8786d
A. Unique TensorFlower <gardener@tensorflow.org>
2022-09-16 13:07:08 -0700

    Internal change
    PiperOrigin-RevId: 474884055

--- tensorflow/python/ops/nn_ops.py
+++ tensorflow/python/ops/nn_ops.py
@@ -5728,8 +5728,7 @@ def _dropout(x, rate, noise_shape, uniform_sampler, dummy_rng_step, name,
     # than or equal to `rate`.
     random_tensor = uniform_sampler(shape=noise_shape, dtype=x_dtype)
     keep_mask = random_tensor >= rate
-    zero_tensor = constant_op.constant(0, dtype=x_dtype)
-    ret = array_ops.where_v2(keep_mask, ret, zero_tensor)
+    ret = gen_math_ops.mul(ret, gen_math_ops.cast(keep_mask, x_dtype))
     if not is_executing_eagerly:
       ret.set_shape(x.get_shape())
     return ret
