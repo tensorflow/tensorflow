4aa418ceb50056ea18432a9fa46d43e4ea37035e
Antonio Sanchez <cantonios@google.com>
2022-09-23 08:40:41 -0700

    Fix missing sparse matrix crash.
    Calling a sparse matrix op with no matrix currently causes a crash.  Here we check and
    return a non-ok status.
    
    PiperOrigin-RevId: 476379116

--- tensorflow/python/kernel_tests/linalg/sparse/csr_sparse_matrix_ops_test.py
+++ tensorflow/python/kernel_tests/linalg/sparse/csr_sparse_matrix_ops_test.py
@@ -1313,6 +1313,16 @@ class CSRSparseMatrixOpsTest(test.TestCase):
       self.assertLess(cholesky_with_amd_nnz_value,
                       cholesky_without_ordering_nnz_value)
 
+  @test_util.run_in_graph_and_eager_modes
+  def testNoMatrixNoCrash(self):
+    # Round-about way of creating an empty variant tensor that works in both
+    # graph and eager modes.
+    no_matrix = array_ops.reshape(dense_to_csr_sparse_matrix([[0.0]]), [1])[0:0]
+    with self.assertRaisesRegex(
+        (ValueError, errors.InvalidArgumentError),
+        "(Invalid input matrix)|(Shape must be rank 0)"):
+      sparse_csr_matrix_ops.sparse_matrix_nnz(no_matrix)
+
 
 class CSRSparseMatrixOpsBenchmark(test.Benchmark):
 
