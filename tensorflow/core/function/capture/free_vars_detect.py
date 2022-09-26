f2b56f5dbe55ecf744bf6e696d6afc529cb84558
A. Unique TensorFlower <gardener@tensorflow.org>
2022-09-26 13:48:39 -0700

    Implement free vars detection for patterns that tf.function invokes functions defined outside but contains free vars
    PiperOrigin-RevId: 476981881

--- tensorflow/core/function/capture/free_vars_detect.py
+++ tensorflow/core/function/capture/free_vars_detect.py
@@ -14,16 +14,19 @@
 # ==============================================================================
 """An independent module to detect free vars inside a function."""
 
+import collections
+import inspect
 import types
-from typing import List
 
 from tensorflow.python.autograph.pyct import anno
+from tensorflow.python.autograph.pyct import inspect_utils
 from tensorflow.python.autograph.pyct import naming
 from tensorflow.python.autograph.pyct import parser
 from tensorflow.python.autograph.pyct import qual_names
 from tensorflow.python.autograph.pyct import transformer
 from tensorflow.python.autograph.pyct.static_analysis import activity
-from tensorflow.python.util import tf_inspect
+
+FreeVar = collections.namedtuple("FreeVar", ["name", "is_function", "obj"])
 
 
 def _parse_and_analyze(func):
@@ -42,34 +45,68 @@ def _parse_and_analyze(func):
   return node
 
 
-def detect_function_free_vars(func: types.FunctionType) -> List[str]:
-  """Detect free vars in any Python function."""
-  assert isinstance(
-      func, types.FunctionType
-  ), f"The input should be of Python function type. Got type: {type(func)}."
-
-  node = _parse_and_analyze(func)
+def _search_callable_free_vars(fn):
+  """Search free vars from a callable object."""
+  node = _parse_and_analyze(fn)
   scope = anno.getanno(node, anno.Static.SCOPE)
   free_vars_all = list(scope.free_vars)
-  globals_dict = func.__globals__
+  namespace = inspect_utils.getnamespace(fn)
   filtered = []
   for var in free_vars_all:
     base = str(var.qn[0])
-    if base in globals_dict:
-      obj = globals_dict[base]
-      if tf_inspect.ismodule(obj):
+    obj = namespace[base]
+    if (inspect.ismodule(obj) or inspect.ismethod(obj) or inspect.isclass(obj)):
+      continue
+    elif inspect.isfunction(obj):
+      if obj.__module__ != fn.__module__:
         continue
-      if (tf_inspect.isclass(obj) or
-          tf_inspect.ismethod(obj) or
-          tf_inspect.isfunction(obj)):
-        if obj.__module__ != func.__module__:
-          continue
+      else:
+        while hasattr(fn, "__wrapped__"):
+          obj = obj.__wrapped__
+        filtered.append(FreeVar(str(var), True, obj))
+    else:
       # Only keep free vars without subscript for simplicity
       if not var.has_subscript():
-        filtered.append(str(var))
-    else:
-      if not var.has_subscript():
-        filtered.append(str(var))
+        filtered.append(FreeVar(str(var), False, None))
 
-  return sorted(filtered)
+  filtered = sorted(filtered, key=lambda x: x.name)
+  return filtered
 
+
+def _make_callable_signature(obj):
+  if inspect.isclass(obj) or inspect.isfunction(obj):
+    return obj.__name__
+  elif inspect.ismethod(obj):
+    return obj.__func__.__name__ + "." + obj.__name__
+  else:
+    raise TypeError(
+        f"Only class/function/methods are valid inputs, got {type(obj)}")
+
+
+def detect_function_free_vars(fn):
+  """Detect free vars in any Python function."""
+  assert isinstance(
+      fn, types.FunctionType
+  ), f"The input should be of Python function type. Got type: {type(fn)}."
+
+  while hasattr(fn, "__wrapped__"):
+    fn = fn.__wrapped__
+
+  queue = collections.deque([fn])
+  fn_map = dict()
+  # Perform BFS over functions to get free vars
+  while queue:
+    obj = queue.popleft()
+    signature = _make_callable_signature(obj)
+    if signature not in fn_map:
+      free_vars = _search_callable_free_vars(obj)
+      fn_map[signature] = free_vars
+      for var in free_vars:
+        # Only search callable free vars
+        if var.is_function:
+          obj = var.obj
+          if _make_callable_signature(obj) not in fn_map:
+            queue.append(obj)
+
+  # func_name -> namedtupe FreeVar
+  return fn_map
