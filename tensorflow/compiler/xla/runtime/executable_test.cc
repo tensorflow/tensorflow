8821ecfefdeaf7827292e3e4b9b8121aa6849d5e
Eugene Zhulenev <ezhulenev@google.com>
2022-09-27 14:26:40 -0700

    [xla:runtime] Add a testlib pipeline and a test for running simple executable
    PiperOrigin-RevId: 477275337

---
+++ tensorflow/compiler/xla/runtime/executable_test.cc
@@ -0,0 +1,114 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "absl/base/dynamic_annotations.h"
+#include "tensorflow/compiler/xla/mlir/transforms/runtime/tests/testlib_pipeline.h"
+#include "tensorflow/compiler/xla/runtime/arguments.h"
+#include "tensorflow/compiler/xla/runtime/async_runtime.h"
+#include "tensorflow/compiler/xla/runtime/jit_executable.h"
+#include "tensorflow/compiler/xla/runtime/results.h"
+#include "tensorflow/tsl/platform/test.h"
+
+namespace xla {
+namespace runtime {
+
+using absl::StatusOr;
+
+//===----------------------------------------------------------------------===//
+// A helper function that compiles the given `module` to an XLA runtime
+// executable and runs the module's `test` function with the given arguments.
+// Results are returned to the caller via the user-provided result converter.
+//===----------------------------------------------------------------------===//
+
+static absl::Status CompileAndExecute(std::string_view module,
+                                      ArgumentsRef args,
+                                      ResultConverter& results) {
+  JitExecutable::Options opts;
+  opts.specialization = JitExecutable::Specialization::kDisabled;
+  opts.compiler.register_dialects = RegisterXlaRuntimeTestlibDialects;
+  opts.compiler.create_compilation_pipeline = CreateXlaRuntimeTestlibPipeline;
+
+  StatusOr<JitExecutable> jit_executable =
+      JitExecutable::Instantiate(module, "test", opts);
+  if (!jit_executable.ok()) return jit_executable.status();
+
+  AsyncValuePtr<Executable> executable = jit_executable->DefaultExecutable();
+  if (executable.IsError())
+    return absl::InternalError(executable.GetError().message());
+
+  Executable::CallFrame call_frame;
+  auto initialized = executable->InitializeCallFrame(args, &call_frame);
+  if (!initialized.ok()) return initialized;
+
+  Executable::ExecuteOpts execute_opts;
+  execute_opts.async_task_runner =
+      reinterpret_cast<AsyncTaskRunner*>(0XDEADBEEF);
+
+  executable->Execute(call_frame, execute_opts);
+  if (call_frame.is_error) return absl::InternalError(call_frame.error);
+
+  return executable->ReturnResults(results, &call_frame);
+}
+
+//===----------------------------------------------------------------------===//
+
+static void AssertNoError(const absl::Status& status) {
+  assert(false && "Unexpected call to `ReturnError`");
+}
+
+struct ReturnI32 {
+  LogicalResult operator()(unsigned result_index, const Type* type,
+                           const Type* runtime_type, void* ret) const {
+    auto* scalar = llvm::dyn_cast<ScalarType>(type);
+    if (scalar && scalar->type() == PrimitiveType::S32) {
+      ABSL_ANNOTATE_MEMORY_IS_INITIALIZED(ret, sizeof(int32_t));
+      *ptr = *reinterpret_cast<int32_t*>(ret);
+      return success();
+    }
+    return failure();
+  }
+
+  int32_t* ptr = nullptr;
+};
+
+//===----------------------------------------------------------------------===//
+
+TEST(ExecutableTest, ReturnScalar) {
+  absl::string_view module = R"(
+    func.func @test() -> i32 {
+      %0 = arith.constant 42 : i32
+      return %0 : i32
+    }
+  )";
+
+  int32_t result = 0;
+  ResultConverterSet converter(AssertNoError, ReturnI32{&result});
+
+  ASSERT_TRUE(CompileAndExecute(module, {}, converter).ok());
+  EXPECT_EQ(result, 42);
+}
+
+}  // namespace runtime
+}  // namespace xla
