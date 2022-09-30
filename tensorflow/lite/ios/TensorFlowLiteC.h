d4a2104beb0d9aebf8fe2799430e7261387b08ef
Yishuang Pang <ypang@google.com>
2022-09-29 22:55:08 -0700

    Fix iOS nightly release. Exports the actual c api header instead of the shim header in the TensorflowLiteC framework
    PiperOrigin-RevId: 477905780

--- tensorflow/lite/ios/TensorFlowLiteC.h
+++ tensorflow/lite/ios/TensorFlowLiteC.h
@@ -21,7 +21,6 @@ limitations under the License.
 #include "tensorflow/lite/c/c_api_experimental.h"
 #include "tensorflow/lite/c/c_api_types.h"
 #include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/core/c/c_api.h"
 #include "tensorflow/lite/delegates/xnnpack/xnnpack_delegate.h"
 
 #endif  // TENSORFLOW_LITE_IOS_TENSORFLOWLITEC_H_
