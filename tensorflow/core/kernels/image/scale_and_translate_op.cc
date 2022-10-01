215850f0cc9855db9c3b66e97fd62518496e0908
Yong Tang <yong.tang.github@outlook.com>
2022-10-01 10:37:02 +0000

    Fix crash in tf.image.resize when antialias is True and size is large
    This PR tries to fix the issue raised in 57897 where tf.image.resize
    will crash when antialias is True and size is large.
    
    This PR fixes 57897.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/core/kernels/image/scale_and_translate_op.cc
+++ tensorflow/core/kernels/image/scale_and_translate_op.cc
@@ -323,10 +323,13 @@ class ScaleAndTranslateOp : public OpKernel {
     GetValues(context, 3, &row_translation, &col_translation);
 
     Tensor* output = nullptr;
+    TensorShape output_shape;
+    OP_REQUIRES_OK(context, output_shape.AddDimWithStatus(input.dim_size(0)));
+    OP_REQUIRES_OK(context, output_shape.AddDimWithStatus(output_height));
+    OP_REQUIRES_OK(context, output_shape.AddDimWithStatus(output_width));
+    OP_REQUIRES_OK(context, output_shape.AddDimWithStatus(input.dim_size(3)));
     OP_REQUIRES_OK(context, context->allocate_output(
-                                0,
-                                TensorShape({input.dim_size(0), output_height,
-                                             output_width, input.dim_size(3)}),
+                                0, output_shape,
                                 &output));
     if (!context->status().ok()) return;
 
