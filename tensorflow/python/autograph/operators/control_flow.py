dd0daa926fdd279ed2e81ebc77cca9607dbe3f00
mohantym <86464649+mohantym@users.noreply.github.com>
2022-10-02 08:24:51 +0530

    Fixed grammar at line 638
    Put "iteration index " instead of "iterate" , A verb can be used as noun.

--- tensorflow/python/autograph/operators/control_flow.py
+++ tensorflow/python/autograph/operators/control_flow.py
@@ -635,7 +635,7 @@ def _tf_range_for_stmt(
     nonlocal iterate
     # TODO(b/171479293): Drop the lint override.
     iterate, *loop_vars = aug_loop_vars  # pylint:disable=unused-variable
-    # The iteration index is not "output" by the for loop. If the iteration
+    # The iteration index is not "output" by the for loop. If the iteration index
     # is used outside the loop, it will appear in the loop vars separately.
     set_state(loop_vars)
 
