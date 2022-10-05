79b68c179c018c66405d4dc03410643e3e645508
Sachin Muradi <sachin.muradi@intel.com>
2022-10-05 15:29:27 -0700

    Add empty out tensor check

--- tensorflow/python/kernel_tests/nn_ops/pooling_ops_3d_test.py
+++ tensorflow/python/kernel_tests/nn_ops/pooling_ops_3d_test.py
@@ -245,6 +245,21 @@ class PoolingTest(test.TestCase):
     values = self.evaluate(max_pool_3d)
     self.assertEqual(values.shape, (0, 56, 56, 56, 64))
 
+  def testAvgPool3dEmptyOutTensor(self):
+    input_sizes = [30, 19, 4, 19, 17]
+    input_data = 1.0
+    input_tensor = constant_op.constant(
+        input_data, shape=input_sizes, name="input")
+    avg_pool_3d= nn_ops.avg_pool3d(
+        input_tensor,
+        ksize=(1, 13, 3, 20, 1),
+        strides=(1, 14, 4, 1, 1),
+        padding="VALID",
+        data_format="NDHWC",
+        name="avg_pool_3d")
+    values = self.evaluate(avg_pool_3d)
+    self.assertEqual(values.shape, (30, 1, 1, 0, 17))
+
   def _ConstructAndTestGradientForConfig(self,
                                          pool_func,
                                          input_sizes,
