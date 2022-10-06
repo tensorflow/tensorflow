ebe32cfbc3b0511849baf4018af2cb58615a9a1d
A. Unique TensorFlower <gardener@tensorflow.org>
2022-10-05 17:58:53 -0700

    Refactor MultiDeviceSaver.restore to have: 1. if slice_spec is empty, change restore_fn_inputs from dict {checkpoint_key: dict {slice_spec: Tensor}} to dict {checkpoint_key: Tensor}
    2. Restruct restore_fn_inputs[restore_fn] to have extracted_local_name instead of checkpoint_key itself.
    
    PiperOrigin-RevId: 479187460

--- tensorflow/python/trackable/trackable_utils.py
+++ tensorflow/python/trackable/trackable_utils.py
@@ -156,11 +156,7 @@ def extract_local_name(key, prefix=None):
   # "local name" refers to the the keys of `Trackable._serialize_to_tensors.`
   prefix = prefix or ""
   search_key = OBJECT_ATTRIBUTES_NAME + "/" + prefix
-  # If checkpoint is saved from TF1, return key as is.
-  try:
-    return key[key.index(search_key) + len(search_key):]
-  except ValueError:
-    return key
+  return key[key.index(search_key) + len(search_key):]
 
 
 def slot_variable_key(variable_path, optimizer_path, slot_name):
