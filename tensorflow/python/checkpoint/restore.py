7ebfd11e07b455a1c7eef686410f283fa36c6d5e
Yaning Liang <yaning@google.com>
2022-10-06 16:55:48 -0700

    Implementation of restore_nodes when Trackable.has_serialize_to_tensors().
    PiperOrigin-RevId: 479445862

--- tensorflow/python/checkpoint/restore.py
+++ tensorflow/python/checkpoint/restore.py
@@ -17,17 +17,20 @@
 import collections
 
 from tensorflow.python.checkpoint import checkpoint_view
+from tensorflow.python.checkpoint import functional_saver
 from tensorflow.python.checkpoint import saveable_compat
 from tensorflow.python.eager import context
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import gen_io_ops as io_ops
+from tensorflow.python.ops import io_ops
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.saved_model import registration
 from tensorflow.python.trackable import base
 from tensorflow.python.trackable import constants
 from tensorflow.python.trackable import python_state
 from tensorflow.python.trackable import trackable_utils
+from tensorflow.python.util import object_identity
 
 
 class CheckpointPosition(object):
@@ -520,21 +523,47 @@ def restore_nodes(save_path, nodes_to_restore):
           f"trackable: {trackable}."
       )
 
-    trackable_expects_ckpted_value = saveable_object_util.trackable_has_serialize_to_tensor(
-        trackable)
+  serialized_tensors = object_identity.ObjectIdentityDictionary()
+  for node_id, current_trackable in nodes_to_restore.items():
     ckpt_contains_serialized_tensors = ckpt_view._object_graph_proto.nodes[  # pylint: disable=protected-access
         node_id].attributes
+    node = ckpt_view._object_graph_proto.nodes[node_id]  # pylint: disable=protected-access
+
+    if not saveable_object_util.trackable_has_serialize_to_tensor(
+        current_trackable):
+      raise NotImplementedError("Will be implemented in a future change.")
+
+    serialized_tensors[
+        current_trackable] = current_trackable._serialize_to_tensors()  # pylint: disable=protected-access
+    trackable_expects_ckpted_value = bool(serialized_tensors[current_trackable])
 
     if trackable_expects_ckpted_value and not ckpt_contains_serialized_tensors:
       raise ValueError(
-          f"Trackable {trackable} expects checkpointed values but checkpoint "
-          f"does not contain serialized tensors for node_id: {node_id}.")
+          f"Trackable {current_trackable} expects checkpointed values but "
+          "checkpoint does not contain serialized tensors for node_id: "
+          f"{node_id}.")
 
     if not trackable_expects_ckpted_value and ckpt_contains_serialized_tensors:
       raise ValueError(
-          f"Trackable {trackable} does not expect checkpointed values but "
-          "checkpoint contains serialized tensors: "
+          f"Trackable {current_trackable} does not expect checkpointed "
+          "values but checkpoint contains serialized tensors: "
           f"{ckpt_contains_serialized_tensors} for node_id: {node_id}.")
+
+    serialized_tensors_renamed = object_identity.ObjectIdentityDictionary()
+    if len(node.attributes) != len(serialized_tensors[current_trackable]):
+      raise ValueError("Size for serialized_tensors for Trackable: "
+                       f"{len(serialized_tensors[current_trackable])} did not"
+                       " match size for serialized_tensors for checkpoint: "
+                       f"{len(node.attributes)}.")
+
+    serialized_tensors_renamed[current_trackable] = {}
+    for attribute in node.attributes:
+      name = attribute.name
+      checkpoint_key = attribute.checkpoint_key
+      serialized_tensors_renamed[current_trackable][
+          checkpoint_key] = serialized_tensors[current_trackable][name]
+    functional_saver.MultiDeviceSaver(serialized_tensors_renamed).restore(
+        save_path)
 
 
 def _queue_children_for_restoration(checkpoint_position, visit_queue):
--- tensorflow/python/checkpoint/restore_test.py
+++ tensorflow/python/checkpoint/restore_test.py
@@ -88,16 +88,96 @@ class ExtractSaveablenameTest(test.TestCase):
         "contain serialized tensors for node_id: 0."):
       restore.restore_nodes(root_save_path, {0: root2})
 
-    root = MyTrackable2()
+  def test_restore_nodes_has_serialize_to_tensor(self):
+
+    class MyTrackable(base.Trackable):
+
+      def __init__(self):
+        self.a = variables.Variable(5.0)
+
+      def _restore_from_tensors(self, restored_tensors):
+        self.a.assign(restored_tensors["a"])
+
+      def _serialize_to_tensors(self):
+        return {"a": self.a}
+
+    root = MyTrackable()
+    leaf = MyTrackable()
+    root._track_trackable(leaf, "leaf")
     root_ckpt = trackable_utils.Checkpoint(root=root)
     root_save_path = root_ckpt.save(
         os.path.join(self.get_temp_dir(), "root_ckpt"))
 
     root2 = MyTrackable()
+    leaf2 = MyTrackable()
+    root2._track_trackable(leaf2, "leaf")
+    root2.a.assign(3.0)
+
+    # Restore root
+    restore.restore_nodes(root_save_path, {0: root2})
+    self.assertEqual(root2.a.numpy(), 5.0)  # Restored from 3.0 to 5.0
+    self.assertEqual(leaf2.a.numpy(), 5.0)  # Unchanged
+
+    root3 = MyTrackable()
+    leaf3 = MyTrackable()
+    root3._track_trackable(leaf3, "leaf")
+    leaf3.a.assign(3.0)
+
+    # Restore leaf
+    restore.restore_nodes(root_save_path, {1: leaf3})
+    self.assertEqual(root3.a.numpy(), 5.0)  # Unchanged
+    self.assertEqual(leaf3.a.numpy(), 5.0)  # Restored from 3.0 to 5.0.
+
+  def test_restore_nodes_with_different_number_of_serialized_to_tensors(self):
+
+    class MyTrackableA(base.Trackable):
+
+      def __init__(self):
+        self.a = variables.Variable(5.0)
+
+      def _restore_from_tensors(self, restored_tensors):
+        self.a.assign(restored_tensors["a"])
+
+      def _serialize_to_tensors(self):
+        return {"a": self.a}
+
+    class MyTrackableAandB(base.Trackable):
+
+      def __init__(self):
+        self.a = variables.Variable(5.0)
+        self.b = variables.Variable(6.0)
+
+      def _restore_from_tensors(self, restored_tensors):
+        self.a.assign(restored_tensors["a"])
+        self.b.assign(restored_tensors["b"])
+
+      def _serialize_to_tensors(self):
+        return {"a": self.a, "b": self.b}
+
+    root = MyTrackableA()
+    root_ckpt = trackable_utils.Checkpoint(root=root)
+    root_save_path = root_ckpt.save(
+        os.path.join(self.get_temp_dir(), "root_ckpt"))
+
+    root2 = MyTrackableAandB()
+
     with self.assertRaisesRegex(
         ValueError,
-        "Trackable <.*?> does not expect checkpointed values but checkpoint "
-        "contains serialized tensors: .*?"):
+        "Size for serialized_tensors for Trackable: 2 did not match size for "
+        "serialized_tensors for checkpoint: 1."):
+      restore.restore_nodes(root_save_path, {0: root2})
+
+    root = MyTrackableAandB()
+    root_ckpt = trackable_utils.Checkpoint(root=root)
+    root_save_path = root_ckpt.save(
+        os.path.join(self.get_temp_dir(), "root_ckpt"))
+
+    root2 = MyTrackableA()
+
+    with self.assertRaisesRegex(
+        ValueError,
+        "Size for serialized_tensors for Trackable: 1 did not match size for "
+        "serialized_tensors for checkpoint: 2."):
       restore.restore_nodes(root_save_path, {0: root2})
 
 
