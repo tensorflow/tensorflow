5ee7151b6be1ff633e63c0dacf86c993f256d4de
Johannes Reifferscheid <jreiffers@google.com>
2022-10-13 05:53:42 -0700

    Add support for entry_computation result layout normalization.
    Currently, programs compiled through the MLIR pipeline that use non-standard
    layouts for results see transposed values.
    
    This normalization is only correct if none of the instructions inside the
    computation have non-standard layouts (which is the case for programs
    compiled through the MLIR pipeline, but not true in general).
    
    This fixes tensorflow/compiler/xla/tests:client_test_cpu with XLA_FLAGS=--xla_cpu_enable_mlir_lowering=true
    
    PiperOrigin-RevId: 480870452

--- tensorflow/compiler/xla/translate/hlo_to_mhlo/hlo_module_importer.cc
+++ tensorflow/compiler/xla/translate/hlo_to_mhlo/hlo_module_importer.cc
@@ -42,44 +42,103 @@ HloModuleImporter::HloModuleImporter(mlir::ModuleOp module,
 
 namespace {
 
-// Inserts reshapes and transposes for parameters that have non-default layouts.
+// Checks if the shape 1) has a non-default layout 2) without tiles.
+bool LayoutRequiresNormalization(const Shape& shape) {
+  if (shape.IsTuple()) {
+    return absl::c_any_of(shape.tuple_shapes(), LayoutRequiresNormalization);
+  }
+  if (!shape.has_layout()) return false;
+
+  // This code can't handle tiles.
+  if (!shape.layout().tiles().empty()) return false;
+
+  // For default shapes, there's nothing to do.
+  return shape.layout() != LayoutUtil::GetDefaultLayoutForShape(shape);
+}
+
+bool EntryComputationLayoutCanBeNormalized(const HloModule& module) {
+  // This code only supports normalization of entry_computation_layout, not of
+  // any other instructions. In the MLIR pipeline, no instructions will ever
+  // have layouts.
+  for (const auto* instruction : module.entry_computation()->instructions()) {
+    if (LayoutRequiresNormalization(instruction->shape())) return false;
+  }
+
+  return LayoutRequiresNormalization(
+             module.entry_computation_layout().result_layout().shape()) ||
+         absl::c_any_of(
+             module.entry_computation_layout().parameter_layouts(),
+             [](const auto& shape_layout) {
+               return LayoutRequiresNormalization(shape_layout.shape());
+             });
+}
+
+// For a tensor with a non-default layout, inserts a reshape and a transpose to
+// convert from the non-default layout to the default.
+HloInstruction* NormalizeTensor(HloInstruction* tensor, const Shape& shape,
+                                bool is_input) {
+  // Reshape the parameter into the shape that's actually passed.
+  int64_t rank = shape.rank();
+  std::vector<int64_t> permutation(rank);
+  for (int64_t dim = 0; dim < rank; ++dim) {
+    permutation[dim] = rank - 1ll - shape.layout().minor_to_major(dim);
+  }
+  auto inverse_permutation = InversePermutation(permutation);
+
+  auto physical_shape = ShapeUtil::MakeShapeWithDescendingLayout(
+      shape.element_type(),
+      Permute(shape.dimensions(),
+              is_input ? permutation : inverse_permutation));
+
+  auto* computation = tensor->parent();
+  if (is_input) {
+    auto* reshape = computation->AddInstruction(
+        HloInstruction::CreateReshape(physical_shape, tensor));
+    return computation->AddInstruction(
+        HloInstruction::CreateTranspose(shape, reshape, inverse_permutation));
+  }
+  auto* transpose = computation->AddInstruction(HloInstruction::CreateTranspose(
+      physical_shape, tensor, inverse_permutation));
+  return computation->AddInstruction(
+      HloInstruction::CreateReshape(shape, transpose));
+}
+
+HloInstruction* NormalizeValue(HloInstruction* value, const Shape& shape,
+                               bool is_input) {
+  if (!LayoutRequiresNormalization(shape)) return value;
+
+  if (!shape.IsTuple()) {
+    return NormalizeTensor(value, shape, is_input);
+  }
+
+  std::vector<HloInstruction*> elements;
+  for (int64_t i = 0; i < shape.tuple_shapes_size(); ++i) {
+    auto* element = value->parent()->AddInstruction(
+        HloInstruction::CreateGetTupleElement(value, i));
+    elements.push_back(
+        NormalizeValue(element, shape.tuple_shapes(i), is_input));
+  }
+  return value->parent()->AddInstruction(HloInstruction::CreateTuple(elements));
+}
+
+// Inserts reshapes and transposes for entry computation parameters and results
+// that have non-default layouts.
 Status NormalizeEntryComputationLayout(xla::HloModule* module) {
   auto* computation = module->entry_computation();
+  const auto& computation_layout = module->entry_computation_layout();
   for (int i = 0; i < computation->num_parameters(); ++i) {
     auto* param = computation->parameter_instruction(i);
-    const auto& shape =
-        module->entry_computation_layout().parameter_layout(i).shape();
-    if (!shape.has_layout()) continue;
-    const auto& layout = shape.layout();
-    // This code can't handle tiles. For default shapes, there's nothing to do.
-    if (!layout.tiles().empty() ||
-        layout == LayoutUtil::GetDefaultLayoutForShape(shape)) {
-      continue;
-    }
-
-    // Reshape the parameter into the shape that's actually passed.
-    int64_t rank = shape.rank();
-    std::vector<int64_t> permutation(rank);
-    for (int64_t dim = 0; dim < rank; ++dim) {
-      permutation[dim] = rank - 1ll - layout.minor_to_major(dim);
-    }
-
-    auto physical_shape = ShapeUtil::MakeShapeWithDescendingLayout(
-        shape.element_type(), Permute(shape.dimensions(), permutation));
-    auto* reshape = computation->AddInstruction(
-        HloInstruction::CreateReshape(physical_shape, param));
-
-    // Transpose the physical shape back to the logical shape.
-    auto* transpose =
-        computation->AddInstruction(HloInstruction::CreateTranspose(
-            shape, reshape, InversePermutation(permutation)));
-
-    std::vector<HloInstruction*> users_to_replace;
-    absl::c_remove_copy(param->users(), std::back_inserter(users_to_replace),
-                        reshape);
-
-    TF_RETURN_IF_ERROR(param->ReplaceUsesWith(users_to_replace, transpose));
+    const auto& shape = computation_layout.parameter_layout(i).shape();
+    std::vector<HloInstruction*> users = param->users();
+    auto* normalized = NormalizeValue(param, shape, /*is_input=*/true);
+    TF_RETURN_IF_ERROR(param->ReplaceUsesWith(users, normalized));
   }
+
+  const auto& result_shape = computation_layout.result_layout().shape();
+  auto* normalized_result = NormalizeValue(computation->root_instruction(),
+                                           result_shape, /*is_input=*/false);
+  computation->set_root_instruction(normalized_result);
+
   return ::tsl::OkStatus();
 }
 
@@ -88,15 +147,7 @@ Status NormalizeEntryComputationLayout(xla::HloModule* module) {
 Status HloModuleImporter::Import(const xla::HloModule& module) {
   std::unique_ptr<HloModule> module_copy;
   const HloModule* module_to_convert = &module;
-  if (absl::c_any_of(module.entry_computation_layout().parameter_layouts(),
-                     [&](const ShapeLayout& layout) {
-                       if (!layout.shape().has_layout()) return false;
-                       // This code can't handle tiles.
-                       return layout.layout().tiles().empty() &&
-                              layout.layout() !=
-                                  LayoutUtil::GetDefaultLayoutForShape(
-                                      layout.shape());
-                     })) {
+  if (EntryComputationLayoutCanBeNormalized(module)) {
     module_copy = module.Clone(/*suffix=*/"");
     TF_RETURN_IF_ERROR(NormalizeEntryComputationLayout(module_copy.get()));
     module_to_convert = module_copy.get();
@@ -111,12 +162,12 @@ Status HloModuleImporter::Import(const xla::HloModule& module) {
         &builder_, /*is_main*/ true);
 
   auto* module_entry_computation = module_to_convert->entry_computation();
-  for (const auto* computation : module_to_convert->computations())
+  for (const auto* computation : module_to_convert->computations()) {
     TF_RETURN_IF_ERROR(HloFunctionImporter::ImportAsFunc(
         *computation, module_, &function_map_, &builder_,
         /*is_main*/ computation == module_entry_computation));
-
-  return OkStatus();
+  }
+  return ::tsl::OkStatus();
 }
 
 Status HloModuleImporter::Import(const xla::HloModuleProto& module_proto) {
