9383e0cb3131703cc1a5547b088f4f8e7e45277d
Arooj Syed <47137999+AroojSyed@users.noreply.github.com>
2022-10-16 17:55:26 +0500

    Fixed tensorflow installation and grammatical mistake

--- tensorflow/python/tf2.py
+++ tensorflow/python/tf2.py
@@ -14,10 +14,11 @@
 # ==============================================================================
 """Tools to help with the TensorFlow 2.0 transition.
 
-This module is meant for TensorFlow internal implementation, not for users of
-the TensorFlow library. For that see tf.compat instead.
+This module is meant for TensorFlow internal implementation, not for users of the TensorFlow library. For that, see tf.compat instead.
 """
 
+"""conda install -c anaconda tensorflow-gpu==1.15.0 , pip install tensorflow
+"""
 from tensorflow.python.platform import _pywrap_tf2
 from tensorflow.python.util.tf_export import tf_export
 
