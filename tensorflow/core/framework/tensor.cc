c8d5d1cf684a21d64d050be608272baca39eb944
Mihai Maruseac <mihaimaruseac@google.com>
2022-10-18 19:03:15 -0700

    Resolve a sanitizer issue with invalid char -> bool conversion.
    When printing a tensor, we get it's data as a `const char*` array (since that's the underlying storage) and then we typecast it to the element type. However, conversions from `char` to `bool` are undefined if the `char` is not `0` or `1`, so sanitizers/fuzzers will crash.
    
    To fix, we're creating a mutable `char` array to convert the chars to 0/1, according to bool rules.
    
    Discovered via internal fuzzing.
    
    PiperOrigin-RevId: 482078850

--- tensorflow/core/framework/tensor.cc
+++ tensorflow/core/framework/tensor.cc
@@ -1183,12 +1183,10 @@ void PrintOneDimV2(int dim_index, const gtl::InlinedVector<int64, 4>& shape,
 }
 
 template <typename T>
-string SummarizeArray(int64_t limit, int64_t num_elts,
-                      const TensorShape& tensor_shape, const char* data,
-                      const bool print_v2) {
+string SummarizeArrayInternal(int64_t limit, int64_t num_elts,
+                              const TensorShape& tensor_shape, const T* array,
+                              const bool print_v2) {
   string ret;
-  const T* array = reinterpret_cast<const T*>(data);
-
   const gtl::InlinedVector<int64_t, 4> shape = tensor_shape.dim_sizes();
   if (shape.empty()) {
     for (int64_t i = 0; i < limit; ++i) {
@@ -1210,6 +1208,28 @@ string SummarizeArray(int64_t limit, int64_t num_elts,
   }
 
   return ret;
+}
+
+template <typename T>
+string SummarizeArray(int64_t limit, int64_t num_elts,
+                      const TensorShape& tensor_shape, const char* data,
+                      const bool print_v2) {
+  const T* array = reinterpret_cast<const T*>(data);
+  return SummarizeArrayInternal<T>(limit, num_elts, tensor_shape, array,
+                                   print_v2);
+}
+
+template <>
+string SummarizeArray<bool>(int64_t limit, int64_t num_elts,
+                            const TensorShape& tensor_shape, const char* data,
+                            const bool print_v2) {
+  // We first convert all chars to be 0/1 to not get InvalidEnumValue sanitizer
+  // error
+  char* mutable_data = const_cast<char*>(data);
+  for (int64_t i = 0; i < limit; ++i) mutable_data[i] = !!mutable_data[i];
+  bool* array = reinterpret_cast<bool*>(mutable_data);
+  return SummarizeArrayInternal<bool>(limit, num_elts, tensor_shape, array,
+                                      print_v2);
 }
 }  // namespace
 
