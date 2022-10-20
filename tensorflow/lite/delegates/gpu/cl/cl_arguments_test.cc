cdc496a5e8ecf0ef85d3b4ed3362bb425f835630
Mike Corrigan <corrigan@gmail.com>
2022-10-19 22:06:15 -0500

    Fix GPU builds for Android
    Fix GPU builds on Android (OpenCL and OpenGL).
    Tested with Android NDK r18b.
    
    Signed-off-by: Mike Corrigan <corrigan@gmail.com>

--- tensorflow/lite/delegates/gpu/cl/cl_arguments_test.cc
+++ tensorflow/lite/delegates/gpu/cl/cl_arguments_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include "tensorflow/lite/delegates/gpu/cl/buffer.h"
 #include "tensorflow/lite/delegates/gpu/cl/gpu_object.h"
 #include "tensorflow/lite/delegates/gpu/common/gpu_info.h"
+#include "tensorflow/lite/delegates/gpu/cl/cl_test.h"
 
 namespace tflite {
 namespace gpu {
@@ -45,7 +46,7 @@ __kernel void main_function($0) {
 
   CLArguments cl_args;
   GpuInfo gpu_info;
-  ASSERT_OK(cl_args.Init(gpu_info, {}, nullptr, &args, &sample_code));
+  ASSERT_OK(cl_args.Init(gpu_info, nullptr, &args, &sample_code));
   EXPECT_TRUE(absl::StrContains(sample_code, "value = weights_buffer[id];"));
   EXPECT_TRUE(
       absl::StrContains(sample_code, "__global float4* weights_buffer"));
@@ -67,7 +68,7 @@ TEST(CLArgumentsTest, TestNoSelector) {
 )";
   CLArguments cl_args;
   GpuInfo gpu_info;
-  EXPECT_FALSE(cl_args.Init(gpu_info, {}, nullptr, &args, &sample_code).ok());
+  EXPECT_FALSE(cl_args.Init(gpu_info, nullptr, &args, &sample_code).ok());
 }
 
 }  // namespace cl
