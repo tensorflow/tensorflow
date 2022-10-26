8e9ae0d770f98dcb83efbae245c51b8d3902a32d
Matt Kreileder <mattalexander@google.com>
2022-10-26 01:37:11 -0700

    `Interpreter` and `InterpreterBuilder`: Add opaque delegate overloads
    * Add an `AddDelegate` overload on the interpreter builder that accepts an
      opaque delegate.
    * Add overloads for `SetBufferHandle` and `GetBufferHandle` in the
      interpreter that accept an opaque delegate
    
    PiperOrigin-RevId: 483899021

--- tensorflow/lite/core/interpreter.h
+++ tensorflow/lite/core/interpreter.h
@@ -614,6 +614,9 @@ class Interpreter {
   TfLiteStatus SetBufferHandle(int tensor_index,
                                TfLiteBufferHandle buffer_handle,
                                TfLiteDelegate* delegate);
+  TfLiteStatus SetBufferHandle(int tensor_index,
+                               TfLiteBufferHandle buffer_handle,
+                               TfLiteOpaqueDelegateStruct* opaque_delegate);
 
   /// Get the delegate buffer handle, and the delegate which can process the
   /// buffer handle.
@@ -621,6 +624,9 @@ class Interpreter {
   TfLiteStatus GetBufferHandle(int tensor_index,
                                TfLiteBufferHandle* buffer_handle,
                                TfLiteDelegate** delegate);
+  TfLiteStatus GetBufferHandle(int tensor_index,
+                               TfLiteBufferHandle* buffer_handle,
+                               TfLiteOpaqueDelegateStruct** opaque_delegate);
 
   /// Sets the profiler to tracing execution. The caller retains ownership
   /// of the profiler and must ensure its validity.
