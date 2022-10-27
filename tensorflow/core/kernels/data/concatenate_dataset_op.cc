59df5b7c96bb30acd5b01afcfa88de3c6d0a64f9
bhack <bhack@users.noreply.github.com>
2022-10-27 15:14:57 +0200

    Protect also the else branch from assert/overflow

--- tensorflow/core/kernels/data/concatenate_dataset_op.cc
+++ tensorflow/core/kernels/data/concatenate_dataset_op.cc
@@ -251,7 +251,7 @@ class ConcatenateDatasetOp::Dataset : public DatasetBase {
         TF_RETURN_IF_ERROR(
             output_tensorshape->AddDimWithStatus(dims1[d]));
       else
-        output_tensorshape->AddDim(-1);
+        output_tensorshape->AddDimWithStatus(-1);
     }
     return OkStatus();
   }
