bc3d965df986d661629a0c083e522b7940b4685d
bhack <bhack@users.noreply.github.com>
2022-10-27 14:41:56 +0200

    Update tensorflow/core/kernels/data/concatenate_dataset_op.cc
    Co-authored-by: Mihai Maruseac <mihaimaruseac@google.com>

--- tensorflow/core/kernels/data/concatenate_dataset_op.cc
+++ tensorflow/core/kernels/data/concatenate_dataset_op.cc
@@ -251,7 +251,7 @@ class ConcatenateDatasetOp::Dataset : public DatasetBase {
         TF_RETURN_IF_ERROR(
             output_tensorshape->AddDimWithStatus(dims1[d]));
       else
-        (*output_tensorshape)->AddDim(-1);
+        output_tensorshape->AddDim(-1);
     }
     return OkStatus();
   }
