b7ba3ac31f0ddfb131258a7d078877996a8ac800
Eugene Zhulenev <ezhulenev@google.com>
2022-10-31 14:14:08 -0700

    [xla] NFC: Move xla-runtime-opt to xla/mlir/runtime folder
    PiperOrigin-RevId: 485145891

--- tensorflow/compiler/mlir/runlit.site.cfg.py
+++ tensorflow/compiler/mlir/runlit.site.cfg.py
@@ -54,6 +54,7 @@ mlir_tf_tools_dirs = [
     'tensorflow/compiler/aot',
     'tensorflow/compiler/xla/service/mlir_gpu',
     'tensorflow/compiler/xla/service/gpu/tests',
+    'tensorflow/compiler/xla/mlir/runtime',
     'tensorflow/compiler/xla/mlir/tools',
     'tensorflow/compiler/mlir/lite/stablehlo',
 ]
