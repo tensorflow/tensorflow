40f3c9f2c7590c08b3e8b2fcffa79147759021df
Felix Chern <fchern@google.com>
2022-11-03 10:58:43 -0700

    Correct db_half_norm as db_half_norm_sq. This change reflects the squared L2 distance should be squared.
    PiperOrigin-RevId: 485918550

--- tensorflow/compiler/tests/approx_topk_test.py
+++ tensorflow/compiler/tests/approx_topk_test.py
@@ -146,19 +146,20 @@ class ApproxTopkTest(test.TestCase, parameterized.TestCase):
   def test_l2ann(self, k, db_size, qy_size, feature_dim):
     qy = self._rng.random([qy_size, feature_dim], dtype=np.float32)
     db = self._rng.random([db_size, feature_dim], dtype=np.float32)
-    db_half_norm = np.linalg.norm(db, axis=1) / 2
+    db_half_norm_sq = np.linalg.norm(db, axis=1)**2 / 2
 
     @function(jit_compile=True)
-    def ann(qy, db, db_half_norm, k):
-      scores = db_half_norm - math_ops.matmul(qy, db, transpose_b=True)
+    def ann(qy, db, db_half_norm_sq, k):
+      scores = db_half_norm_sq - math_ops.matmul(qy, db, transpose_b=True)
       return nn_ops.approx_min_k(scores, k)
 
     with ops.device('/device:TPU:0'):
       qy_op = variables.Variable(qy)
       db_op = variables.Variable(db)
-      db_half_norm_op = variables.Variable(db_half_norm)
-      result = ann(qy_op, db_op, db_half_norm_op, k)[1]
-      scores = db_half_norm_op - math_ops.matmul(qy_op, db_op, transpose_b=True)
+      db_half_norm_sq_op = variables.Variable(db_half_norm_sq)
+      result = ann(qy_op, db_op, db_half_norm_sq_op, k)[1]
+      scores = db_half_norm_sq_op - math_ops.matmul(
+          qy_op, db_op, transpose_b=True)
 
     gt = np.argsort(scores.numpy())[:, :k]
     ann_recall = self.compute_recall(result.numpy(), gt)
