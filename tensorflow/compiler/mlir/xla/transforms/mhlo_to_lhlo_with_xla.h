726d66aa09b8b991b773d13c23849b24364c5ab2
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-07 10:52:44 -0800

    Replace all usages of //third_party/tensorflow/compiler/xla/service:hlo with //third_party/tensorflow/compiler/xla/hlo/ir:hlo. The former will be deleted soon.
    PiperOrigin-RevId: 486707673

--- tensorflow/compiler/mlir/xla/transforms/mhlo_to_lhlo_with_xla.h
+++ tensorflow/compiler/mlir/xla/transforms/mhlo_to_lhlo_with_xla.h
@@ -23,11 +23,11 @@ limitations under the License.
 #include "mlir/IR/Builders.h"  // from @llvm-project
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
+#include "tensorflow/compiler/xla/hlo/ir/hlo_instructions.h"
+#include "tensorflow/compiler/xla/hlo/ir/hlo_module.h"
 #include "tensorflow/compiler/xla/mlir_hlo/lhlo/IR/lhlo_ops.h"
 #include "tensorflow/compiler/xla/mlir_hlo/lhlo_gpu/IR/lhlo_gpu_ops.h"
 #include "tensorflow/compiler/xla/service/buffer_assignment.h"
-#include "tensorflow/compiler/xla/service/hlo_instructions.h"
-#include "tensorflow/compiler/xla/service/hlo_module.h"
 #include "tensorflow/compiler/xla/shape_util.h"
 #include "tensorflow/compiler/xla/statusor.h"
 #include "tensorflow/tsl/platform/status.h"
