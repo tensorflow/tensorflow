abee5320b66fd0a8b0af8b25352290d6b2a95a9e
Valerie Sarge <vsarge@nvidia.com>
2022-11-09 11:52:56 -0800

    Corrected code and comment formatting, simplified work element count, removed extraneous DtoH copy, removed early exit when validation fails on another work element (SparseIndicesToRaggedRowSplits)

--- tensorflow/core/kernels/sparse_indices_to_ragged_row_splits_op.cc
+++ tensorflow/core/kernels/sparse_indices_to_ragged_row_splits_op.cc
@@ -13,11 +13,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/core/framework/op.h"
-#include "tensorflow/core/framework/shape_inference.h"
-#include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/kernels/sparse_indices_to_ragged_row_splits_op.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/tensor_shape.h"
 
 namespace tensorflow {
 
@@ -27,101 +27,106 @@ namespace functor {
 
 template <typename IndexType>
 struct SparseIndicesToRaggedRowSplitsFunctor<CPUDevice, IndexType> {
-    Status operator()(
-            OpKernelContext* context,
-            int num_nonzero, // total number of nonzero values in tensor
-            bool validate_ragged_right, // enable validation of input tensor format
-            const IndexType* indices_flat_2d, // array of length 2*num_nonzero
-            const IndexType* dense_shape,
-            int32_t* invalid_flag // single bool, will be set to 1 if input tensor is invalid
-            ){
-        auto num_rows = dense_shape[0];
-        auto num_cols = dense_shape[1];
+  Status operator()(OpKernelContext* context, int num_nonzero,
+                    bool validate_ragged_right,
+                    const IndexType* indices_flat_2d,
+                    const IndexType* dense_shape, int32_t* invalid_flag) {
+    auto num_rows = dense_shape[0];
+    auto num_cols = dense_shape[1];
 
-        Tensor* output;
-        TF_RETURN_IF_ERROR(context, context->allocate_output("row_splits", TensorShape({num_rows + 1}), &output));
-        IndexType* row_splits = output->flat<IndexType>().data();
+    Tensor* output;
+    TF_RETURN_IF_ERROR(context,
+                       context->allocate_output(
+                           "row_splits", TensorShape({num_rows + 1}), &output));
+    IndexType* row_splits = output->flat<IndexType>().data();
 
-        *invalid_flag = 0;
+    *invalid_flag = 0;
 
-        int prev_row = -1;
-        int prev_col = -1;
-        int n = 0;
-        for (; n < num_nonzero; ++n) { // for each pair of value + indices in structure
-            int curr_row = indices_flat_2d[2*n];
-            if (validate_ragged_right) {
-                if (curr_row != prev_row) { // when row is changing
-                    // rows idx must increase monotonically and not exceed dense size
-                    // (to ensure indices are in order)
-                    if ((curr_row < prev_row) || (curr_row >= num_rows)) {
-                        *invalid_flag = 1;
-                        return Status::OK();
-                    }
-                    prev_col = -1;
-                }
-                // within a row, column values must always be one greater than the previous
-                // (to ensure that tensor is ragged-right and indices are in order)
-                int curr_col = indices_flat_2d[2*n+1];
-                if ((curr_col != prev_col + 1) || (curr_col >= num_cols)) {
-                    *invalid_flag = 1;
-                    return Status::OK();
-                } else {
-                    prev_col = curr_col;
-                }
-            }
-            // simply fill in row splits; loop used to fill all if a row is empty
-            for (int r = prev_row; r < curr_row; ++r) {
-                row_splits[r+1] = n;
-            }
-            prev_row = curr_row;
+    int prev_row = -1;
+    int prev_col = -1;
+    int n = 0;
+    for (; n < num_nonzero; ++n) {
+      int curr_row = indices_flat_2d[2 * n];
+      if (validate_ragged_right) {
+        if (curr_row != prev_row) {
+          // When the current indices represent the start of a new row,
+          // check that the row index increases monotonically and does not
+          // exceed the dense size.
+          // This ensures indices are in order by row.
+          if ((curr_row < prev_row) || (curr_row >= num_rows)) {
+            *invalid_flag = 1;
+            return Status::OK();
+          }
+          prev_col = -1;
         }
-        // fill final row split + any trailing empty rows
-        for (int r = prev_row; r < num_rows; ++r) {
-            row_splits[r+1] = n;
+        // Check that each column index within a row is one higher than the
+        // previous and that the first column index is 0 (prev_col + 1, where
+        // prev_col = -1). This ensures that the tensor is ragged-right and
+        // indices are in order.
+        int curr_col = indices_flat_2d[2 * n + 1];
+        if ((curr_col != prev_col + 1) || (curr_col >= num_cols)) {
+          *invalid_flag = 1;
+          return Status::OK();
+        } else {
+          prev_col = curr_col;
         }
-
-        return Status::OK();
+      }
+      // Fill in row_splits with the current index. A loop is used to fill all
+      // entries if there are empty rows between the previous and current index.
+      for (int r = prev_row; r < curr_row; ++r) {
+        row_splits[r + 1] = n;
+      }
+      prev_row = curr_row;
     }
+    // Fill in the final row split and those for any trailing empty rows.
+    for (int r = prev_row; r < num_rows; ++r) {
+      row_splits[r + 1] = n;
+    }
+
+    return Status::OK();
+  }
 };
 
-} // namespace functor
+}  // namespace functor
 
 template <typename Device, typename IndexType>
 class SparseIndicesToRaggedRowSplitsOp : public OpKernel {
-    private:
-        bool validate_ragged_right;
+ public:
+  explicit SparseIndicesToRaggedRowSplitsOp(OpKernelConstruction* context)
+      : OpKernel(context) {
+    OP_REQUIRES_OK(context, context->GetAttr("validate_ragged_right",
+                                             &validate_ragged_right_));
+  }
+  void Compute(OpKernelContext* context) override {
+    const Tensor& indices = context->input(0);
+    const Tensor& dense_shape = context->input(1);
 
-    public:
-        explicit SparseIndicesToRaggedRowSplitsOp(OpKernelConstruction* context) : OpKernel(context) {
-            OP_REQUIRES_OK(context, context->GetAttr("validate_ragged_right", &validate_ragged_right));
-        }
-        void Compute(OpKernelContext* context) override {
-            // inputs
-            const Tensor& indices = context->input(0);
-            const Tensor& dense_shape = context->input(1);
+    auto num_nonzero = indices.dim_size(0);
 
-            auto num_nonzero = indices.dim_size(0);
+    Tensor* output_invalid = nullptr;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output("invalid_flag", TensorShape({}),
+                                            &output_invalid));
 
-            Tensor* output_invalid = nullptr;
-            OP_REQUIRES_OK(context, context->allocate_output("invalid_flag", TensorShape({}), &output_invalid));
+    OP_REQUIRES_OK(
+        context,
+        functor::SparseIndicesToRaggedRowSplitsFunctor<Device, IndexType>()(
+            context, num_nonzero, validate_ragged_right_,
+            indices.flat<IndexType>().data(),
+            dense_shape.flat<IndexType>().data(),
+            output_invalid->flat<int32_t>().data()));
+  }
 
-            OP_REQUIRES_OK(context,
-                    functor::SparseIndicesToRaggedRowSplitsFunctor<Device, IndexType>()(
-                        context,
-                        //context->eigen_device<Device>(),
-                        num_nonzero,
-                        validate_ragged_right,
-                        indices.flat<IndexType>().data(),
-                        dense_shape.flat<IndexType>().data(),
-                        output_invalid->flat<int32_t>().data()
-            ));
-      }
+ private:
+  bool validate_ragged_right_;
 };
 
-#define REGISTER_CPU(IndexType) \
-  REGISTER_KERNEL_BUILDER( \
-      Name("SparseIndicesToRaggedRowSplits").Device(DEVICE_CPU).TypeConstraint<IndexType>("IndexType"), \
-      SparseIndicesToRaggedRowSplitsOp<CPUDevice, IndexType>);
+#define REGISTER_CPU(IndexType)                    \
+  REGISTER_KERNEL_BUILDER(                         \
+      Name("SparseIndicesToRaggedRowSplits")       \
+          .Device(DEVICE_CPU)                      \
+          .TypeConstraint<IndexType>("IndexType"), \
+      SparseIndicesToRaggedRowSplitsOp<CPUDevice, IndexType>)
 REGISTER_CPU(int32);
 REGISTER_CPU(int64);
 #undef REGISTER_CPU
@@ -130,14 +135,17 @@ REGISTER_CPU(int64);
 
 using GPUDevice = Eigen::GpuDevice;
 
-#define REGISTER_GPU(IndexType) \
-  REGISTER_KERNEL_BUILDER( \
-      Name("SparseIndicesToRaggedRowSplits").Device(DEVICE_GPU).TypeConstraint<IndexType>("IndexType"), \
-      SparseIndicesToRaggedRowSplitsOp<GPUDevice, IndexType>);
+#define REGISTER_GPU(IndexType)                    \
+  REGISTER_KERNEL_BUILDER(                         \
+      Name("SparseIndicesToRaggedRowSplits")       \
+          .Device(DEVICE_GPU)                      \
+          .HostMemory("dense_shape")               \
+          .TypeConstraint<IndexType>("IndexType"), \
+      SparseIndicesToRaggedRowSplitsOp<GPUDevice, IndexType>)
 REGISTER_GPU(int32);
 REGISTER_GPU(int64);
 #undef REGISTER_GPU
 
 #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
-} // namespace tensorflow
+}  // namespace tensorflow
--- tensorflow/core/kernels/sparse_indices_to_ragged_row_splits_op.cu.cc
+++ tensorflow/core/kernels/sparse_indices_to_ragged_row_splits_op.cu.cc
@@ -31,131 +31,114 @@ namespace {
 
 template <typename IndexType>
 __global__ void SparseIndicesToRaggedRowSplitsKernel(
-        const int num_nonzero, // total number of nonzero values in tensor
-        const bool validate_ragged_right, // enable validation of input tensor format
-        const IndexType* indices_flat_2d, // array of length 2*num_nonzero, sparse indices
-        const IndexType* dense_shape,
-        IndexType* row_splits, // array of length num_rows + 1, output row splits
-        int32_t* invalid_flag // single bool, will be set to 1 if input tensor is invalid
-        ) {
+    const int num_nonzero, const bool validate_ragged_right,
+    const IndexType* indices_flat_2d, const IndexType* dense_shape,
+    IndexType* row_splits, int32_t* invalid_flag) {
+  *invalid_flag = 0;  // Extremely simplistic way of zeroing out this value.
+  __syncthreads();
 
-    *invalid_flag = 0; // extremely simplistic way of zeroing out this value
-    __syncthreads();
+  auto num_rows = dense_shape[0];
+  auto num_cols = dense_shape[1];
 
-    auto num_rows = dense_shape[0];
-    auto num_cols = dense_shape[1];
+  int kernel_index = blockIdx.x * blockDim.x + threadIdx.x;
+  int kernel_count = blockDim.x * gridDim.x;
+  int kernel_dim = (kernel_count == 1)
+                       ? num_nonzero
+                       : ((num_nonzero / (kernel_count - 1)) + 1);
+  if (kernel_dim == 0) {
+    kernel_dim = 1;
+  }
+  int start_index = kernel_dim * kernel_index;
+  // In case of very unusual dimensions, return early if there is no work to do.
+  if (start_index >= num_nonzero) {
+    return;
+  }
+  int end_index = kernel_dim * (kernel_index + 1);
+  if (end_index > num_nonzero) {
+    end_index = num_nonzero;
+  }
+  IndexType prev_row =
+      (kernel_index == 0) ? -1 : indices_flat_2d[(start_index - 1) * 2];
+  IndexType prev_col = -1;
 
-    int kernel_index = blockIdx.x*blockDim.x + threadIdx.x;
-    int kernel_count = blockDim.x*gridDim.x;
-    int kernel_dim = (kernel_count == 1) ? num_nonzero : ((num_nonzero / (kernel_count - 1)) + 1);
-    if (kernel_dim == 0) {kernel_dim = 1;}
-    int start_index = kernel_dim*kernel_index;
-    if (start_index >= num_nonzero) { // in case of very weird dimensions
+  IndexType n = start_index;
+  if (validate_ragged_right && (n > 0)) {
+    // If starting in the middle of the row, set the previous column idx for
+    // comparison.
+    prev_col = indices_flat_2d[2 * n - 1];
+  }
+  for (; n < end_index; ++n) {
+    IndexType curr_row = indices_flat_2d[2 * n];
+    if (validate_ragged_right) {
+      // At the end of a row, check that the row index increased and is not
+      // outside of the tensor's recorded dimensions.
+      if (curr_row != prev_row) {
+        if ((curr_row < prev_row) || (curr_row >= num_rows)) {
+          GpuAtomicMax(invalid_flag, 1);
+          return;
+        }
+        prev_col = -1;
+      }
+      // Within each row, check that each column index is one greater than the
+      // last and that the first column index in the row is zero (checked
+      // against prev_col + 1 where prev_col = -1). This ensures the tensor is
+      // ragged-right and its indices are in order.
+      IndexType curr_col = indices_flat_2d[2 * n + 1];
+      if ((curr_col != prev_col + 1) || (curr_col >= num_cols)) {
+        GpuAtomicMax(invalid_flag, 1);
         return;
+      } else {
+        prev_col = curr_col;
+      }
     }
-    int end_index = kernel_dim*(kernel_index + 1);
-    if (end_index > num_nonzero) {end_index = num_nonzero;}
-    IndexType prev_row = (kernel_index == 0) ? -1 : indices_flat_2d[(start_index - 1)*2];
-    IndexType prev_col = -1;
-
-    IndexType n = start_index;
-    if (validate_ragged_right && (n > 0)) {
-        // if starting in the middle of the row, set the previous column idx for comparison
-        prev_col = indices_flat_2d[2*n-1];
+    // Fill in the row_splits with the current index; the loop will fill in any
+    // needed entries for empty rows between the previous and current index.
+    for (IndexType r = prev_row; r < curr_row; ++r) {
+      row_splits[r + 1] = n;
     }
-    for (; n < end_index; ++n) { // for each pair of value + indices
-        IndexType curr_row = indices_flat_2d[2*n];
-        if (validate_ragged_right) {
-            // if another kernel invocation found an issue with input tensor, quit
-            if (*invalid_flag == 1){
-                return;
-            }
-            // at the end of a row, check that row idx increased and is not outside size
-            // (to ensure indices are in order)
-            if (curr_row != prev_row) {
-                if ((curr_row < prev_row) || (curr_row >= num_rows)) {
-                    GpuAtomicMax(invalid_flag, 1);
-                    return;
-                }
-                prev_col = -1;
-            }
-            // within a row, check that column increased by one
-            // (to ensure tensor is ragged-right and indices are in order)
-            IndexType curr_col = indices_flat_2d[2*n+1];
-            if ((curr_col != prev_col + 1) || (curr_col >= num_cols)) {
-                GpuAtomicMax(invalid_flag, 1);
-                return;
-            } else {
-                prev_col = curr_col;
-            }
-        }
-        // fill in row splits; loop used to fill all if row is empty
-        for (IndexType r = prev_row; r < curr_row; ++r) {
-            row_splits[r+1] = n;
-        }
-        prev_row = curr_row;
+    prev_row = curr_row;
+  }
+  // At the end of the tensor, fill in the final row split and those for any
+  // trailing empty rows.
+  if ((start_index < num_nonzero) && (end_index == num_nonzero)) {
+    for (IndexType r = prev_row; r < num_rows; ++r) {
+      row_splits[r + 1] = n;
     }
-    // if at end of tensor, fill in final row split + splits for any training empty rows
-    if ((start_index < num_nonzero) && (end_index == num_nonzero)) {
-        for (IndexType r = prev_row; r < num_rows; ++r) {
-            row_splits[r+1] = n;
-        }
-    }
+  }
 }
 
-} // namespace
+}  // namespace
 
 template <typename IndexType>
 struct SparseIndicesToRaggedRowSplitsFunctor<GPUDevice, IndexType> {
-    Status operator()(
-            OpKernelContext* context,
-            //const GPUDevice& d,
-            int num_nonzero, // total number of nonzero values in tensor
-            bool validate_ragged_right,
-            const IndexType* indices_flat_2d, // array of length 2*num_nonzero
-            const IndexType* dense_shape,
-            int32_t* invalid_flag
-            ){
-        // copy number of rows to host in order to allocate row_splits tensor with correct size
-        ScratchSpace<IndexType> dense_row_count(context, 1, /*on_host=*/true);
-        cudaMemcpy(dense_row_count.mutable_data(), dense_shape, sizeof(*dense_row_count.data()), cudaMemcpyDeviceToHost);
-        IndexType num_rows = *dense_row_count.data();
-        Tensor* output;
-        TF_RETURN_IF_ERROR(context, context->allocate_output("row_splits", TensorShape({num_rows + 1}), &output));
-        IndexType* row_splits = output->flat<IndexType>().data();
+  Status operator()(OpKernelContext* context, int num_nonzero,
+                    bool validate_ragged_right,
+                    const IndexType* indices_flat_2d,
+                    const IndexType* dense_shape, int32_t* invalid_flag) {
+    auto num_rows = dense_shape[0];
+    Tensor* output;
+    TF_RETURN_IF_ERROR(context,
+                       context->allocate_output(
+                           "row_splits", TensorShape({num_rows + 1}), &output));
+    IndexType* row_splits = output->flat<IndexType>().data();
 
-        int splits = num_rows / 2;
-        if (splits > (num_nonzero / 10)) {
-            splits = (num_nonzero / 10);
-        }
-        if (splits < 1) {
-            splits = 1;
-        }
+    const GPUDevice& d = context->eigen_gpu_device();
+    GpuLaunchConfig config = GetGpuLaunchConfig(num_rows, d);
+    int block_count = config.block_count;
+    int thread_per_block = config.thread_per_block;
 
-        const GPUDevice& d = context->eigen_gpu_device();
-        GpuLaunchConfig config = GetGpuLaunchConfig(splits, d);
-        int block_count = config.block_count;
-        int thread_per_block = config.thread_per_block;
-
-        TF_CHECK_OK(GpuLaunchKernel(
-                    SparseIndicesToRaggedRowSplitsKernel<IndexType>,
-                    block_count, thread_per_block, 0, d.stream(),
-                    num_nonzero,
-                    validate_ragged_right,
-                    indices_flat_2d,
-                    dense_shape,
-                    row_splits,
-                    invalid_flag
-        ));
-        return Status::OK();
-    }
+    return GpuLaunchKernel(SparseIndicesToRaggedRowSplitsKernel<IndexType>,
+                           block_count, thread_per_block, 0, d.stream(),
+                           num_nonzero, validate_ragged_right, indices_flat_2d,
+                           dense_shape, row_splits, invalid_flag);
+  }
 };
 
 template struct SparseIndicesToRaggedRowSplitsFunctor<GPUDevice, int32>;
 template struct SparseIndicesToRaggedRowSplitsFunctor<GPUDevice, int64>;
 
-} // namespace functor
+}  // namespace functor
 
-} // namespace tensorflow
+}  // namespace tensorflow
 
 #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
