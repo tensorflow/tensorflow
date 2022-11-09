eec6d52fd5dce9ce849adea077d00ea35b492dff
Nick Felt <nickfelt@google.com>
2022-11-09 14:22:30 -0800

    Update tensorboard dependency for 2.11.0 release.
    PiperOrigin-RevId: 487346430

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -121,7 +121,7 @@ REQUIRED_PACKAGES = [
     # current release version. These also usually have "alpha" or "dev" in their
     # version name.
     # These are all updated during the TF release process.
-    standard_or_nightly('tensorboard >= 2.10, < 2.11',
+    standard_or_nightly('tensorboard >= 2.11, < 2.12',
                         'tb-nightly ~= 2.11.0.a'),
     standard_or_nightly('tensorflow_estimator >= 2.11.0rc0, < 2.12',
                         'tf-estimator-nightly ~= 2.12.0.dev'),
