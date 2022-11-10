22c9c2af657e9189c618dc25c896efc31d88e91f
Bruce Fontaine <bfontain@google.com>
2022-11-10 10:06:06 -0800

    Change how control dependencies are handled for TPUEmbedding enqueue ops and fix a couple of bugs as well.
    PiperOrigin-RevId: 487569518

--- tensorflow/core/grappler/optimizers/function_optimizer.cc
+++ tensorflow/core/grappler/optimizers/function_optimizer.cc
@@ -857,17 +857,16 @@ const bool IsExemptFromSideEffectsExecutionValidation(const string& op) {
        "EnqueueTPUEmbeddingSparseBatch", "EnqueueTPUEmbeddingIntegerBatch",
        "EnqueueTPUEmbeddingSparseTensorBatch",
        "EnqueueTPUEmbeddingRaggedTensorBatch",
-       "EnqueueTPUEmbeddingArbitraryTensorBatch"
+       "EnqueueTPUEmbeddingArbitraryTensorBatch",
+       "DynamicEnqueueTPUEmbeddingArbitraryTensorBatch",
 
        // SaveV2 and RestoreV2 should be allowed to operate in parallel on
        // multiple hosts.
-       "SaveV2",
-       "RestoreV2"
+       "SaveV2", "RestoreV2",
 
        // InfeedEnqueue are stateful but should not be serialized for the
        // input pipeline
-       "InfeedEnqueue",
-       "InfeedEnqueueTuple"});
+       "InfeedEnqueue", "InfeedEnqueueTuple"});
   // LINT.ThenChange(//tensorflow/python/framework/auto_control_deps.py)
   return exemption->contains(op);
 }
