fe26446816f8ac60b4537f7ea24cd2cd6f815e54
Rahul Joshi <jurahul@google.com>
2022-11-11 13:57:57 -0800

    [XLA] Add sharding verification to HLO verifier - Verify sharding on instructions in HLO verifier, with optional verification   of devices listed in the sharding (enabled under SPMD partitioning). - Change HLO protobuf and text parser to attempt to normalize tuple sharding   so that existing HLO text and protobuf can continue to pass verification - Fix multi-output fusion to clear sharding when mutating shape as the   existing sharding will no longer be valid.
    PiperOrigin-RevId: 487909736

--- tensorflow/compiler/xla/service/spmd/spmd_partitioner_test.cc
+++ tensorflow/compiler/xla/service/spmd/spmd_partitioner_test.cc
@@ -105,24 +105,6 @@ class SpmdPartitioningTest : public HloTestBase {
   }
 };
 
-TEST_F(SpmdPartitioningTest, InvalidSharding) {
-  absl::string_view hlo_string = R"(
-HloModule module
-
-ENTRY entry {
-  token0 = token[] after-all(), sharding={maximal device=0}
-  infeed = (f32[8,2]{1,0}, token[]) infeed(token0),
-    sharding={{devices=[2,1]0,1}, {maximal device=0}}
-  ROOT infeed.data = f32[8,2]{1,0} get-tuple-element(infeed), index=0,
-    sharding={maximal device=0}
-})";
-  auto module_status = PartitionComputation(hlo_string, /*num_devices=*/4);
-  EXPECT_FALSE(module_status.status().ok());
-  EXPECT_THAT(module_status.status().ToString(),
-              ::testing::HasSubstr(
-                  "only supports tile sharding that includes all partitions"));
-}
-
 TEST_F(SpmdPartitioningTest, SingleDeviceToReplicated) {
   absl::string_view hlo_string = R"(
 HloModule module
