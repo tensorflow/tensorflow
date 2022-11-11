8b7e03173d5656d5f8b69d35cb02586a8e1c9f48
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-11 01:02:36 -0800

    Update GraphDef version to 1312.
    PiperOrigin-RevId: 487754828

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 1311  // Updated: 2022/11/10
+#define TF_GRAPH_DEF_VERSION 1312  // Updated: 2022/11/11
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
