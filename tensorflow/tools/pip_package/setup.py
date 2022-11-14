f187e21827284ba8e1280232f5054206d4035aad
learning-to-play <66660475+learning-to-play@users.noreply.github.com>
2022-11-14 04:32:14 -0800

    Update setup.py

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -122,7 +122,7 @@ REQUIRED_PACKAGES = [
                         'tb-nightly ~= 2.11.0.a'),
     standard_or_nightly('tensorflow_estimator >= 2.11.0, < 2.12',
                         'tf-estimator-nightly ~= 2.12.0.dev'),
-    standard_or_nightly('keras >= 2.11.0rc1, < 2.12',
+    standard_or_nightly('keras >= 2.11.0, < 2.12',
                         'keras-nightly ~= 2.12.0.dev'),
 ]
 REQUIRED_PACKAGES = [p for p in REQUIRED_PACKAGES if p is not None]
