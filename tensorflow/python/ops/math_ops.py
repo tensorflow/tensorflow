e1906d1f9f73bbc79f70dac33e8266371d3a9eff
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-16 13:57:03 -0800

    Internal change
    PiperOrigin-RevId: 489030998

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -1019,10 +1019,9 @@ def cast(x, dtype, name=None):
 def saturate_cast(value, dtype, name=None):
   """Performs a safe saturating cast of `value` to `dtype`.
 
-  This function casts the input to `dtype` without overflow.  If
+  This function casts the input to `dtype` without applying any scaling.  If
   there is a danger that values would over or underflow in the cast, this op
-  applies the appropriate clamping before the cast.  See `tf.cast` for more
-  details.
+  applies the appropriate clamping before the cast.
 
   Args:
     value: A `Tensor`.
@@ -1037,38 +1036,14 @@ def saturate_cast(value, dtype, name=None):
   with ops.name_scope(name, "saturate_cast", [value]) as name:
     value = ops.convert_to_tensor(value, name="value")
     dtype = dtypes.as_dtype(dtype).base_dtype
-
-    in_dtype = value.dtype
-    if in_dtype.is_complex:
-      if dtype.is_complex:
-        # Clamp real and imag components separately, if required.
-        real_in_dtype = in_dtype.real_dtype
-        real_out_dtype = dtype.real_dtype
-        if real_in_dtype.min < real_out_dtype.min or real_in_dtype.max > real_out_dtype.max:
-          value = gen_math_ops._clip_by_value(
-              value,
-              ops.convert_to_tensor(
-                  np.complex(real_out_dtype.min, real_out_dtype.min),
-                  dtype=in_dtype),
-              ops.convert_to_tensor(
-                  np.complex(real_out_dtype.max, real_out_dtype.max),
-                  dtype=in_dtype),
-              name="clamp")
-        return cast(value, dtype, name=name)
-      else:
-        # Extract real component and fall through to clamp+cast.
-        value = real(value)
-        logging.warn("Casting complex to real discards imaginary part.")
-        in_dtype = in_dtype.real_dtype
-
-    # in_dtype is real, but out_dtype could be complex.
-    out_real_dtype = dtype.real_dtype
-    if in_dtype.min < out_real_dtype.min or in_dtype.max > out_real_dtype.max:
-      value = gen_math_ops._clip_by_value(
+    if value.dtype.min < dtype.min:
+      value = gen_math_ops.maximum(
           value,
-          ops.convert_to_tensor(out_real_dtype.min, dtype=in_dtype),
-          ops.convert_to_tensor(out_real_dtype.max, dtype=in_dtype),
-          name="clamp")
+          ops.convert_to_tensor(dtype.min, dtype=value.dtype, name="min"))
+    if value.dtype.max > dtype.max:
+      value = gen_math_ops.minimum(
+          value,
+          ops.convert_to_tensor(dtype.max, dtype=value.dtype, name="max"))
     return cast(value, dtype, name=name)
 
 
