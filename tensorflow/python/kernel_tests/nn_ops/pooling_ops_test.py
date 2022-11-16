ddaac2bdd099bec5d7923dea45276a7558217e5b
Zhufeng Pan <panzf@google.com>
2022-11-16 10:24:38 -0800

    Add inputs check for AvgPoolGrad
    PiperOrigin-RevId: 488975844

--- tensorflow/python/kernel_tests/nn_ops/pooling_ops_test.py
+++ tensorflow/python/kernel_tests/nn_ops/pooling_ops_test.py
@@ -2510,6 +2510,21 @@ class PoolingTest(test.TestCase, parameterized.TestCase):
             data_format="NHWC")
         self.evaluate(t)
 
+  def testAvgPoolGradInvalidStrideRaiseErrorProperly(self):
+    with self.assertRaises(errors_impl.InvalidArgumentError):
+      with self.cached_session():
+        orig_input_shape = [11, 9, 78, 9]
+        grad = constant_op.constant(
+            0.1, shape=[16, 16, 16, 16], dtype=dtypes.float64)
+        t = gen_nn_ops.AvgPoolGrad(
+            orig_input_shape=orig_input_shape,
+            grad=grad,
+            ksize=[1, 40, 128, 1],
+            strides=[1, 128, 128, 30],
+            padding="SAME",
+            data_format="NHWC")
+        self.evaluate(t)
+
 
 def GetMaxPoolFwdTest(input_size, filter_size, strides, padding):
 
