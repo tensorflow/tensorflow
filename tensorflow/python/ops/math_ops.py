fc0095d3ee7dd21b6b61978a8dd2f526c7455605
Antonio Sanchez <cantonios@google.com>
2022-11-16 11:14:25 -0800

    Fix dtype registrations for tan, asin, acos, atan.
    The original incorrectly allowed integer inputs for tan/atan/acos/asin.  We
    never had working kernels defined for these, either handwritten or in MLIR/XLA.
    Trying them in vanilla TF results in "No OpKernel was registered" failures.  For MLIR/XLA,
    we get `Unimplemented` errors. The original op registration for integer types
    was likely a typo.
    
    This does not actually break backward compatibility since it was never actually supported
    in any form.  We're simply removing the unused and unsupported integer inputs.
    
    Docs for valid input types are auto-generated for these ops from the op definition,
    so they will be updated on the website.
    
    Fixes #57465.
    
    PiperOrigin-RevId: 488989408

--- tensorflow/python/ops/math_ops.py
+++ tensorflow/python/ops/math_ops.py
@@ -5566,8 +5566,7 @@ def acos(x, name=None):
 
   Args:
     x: A `Tensor`. Must be one of the following types: `bfloat16`, `half`,
-      `float32`, `float64`, `uint8`, `int8`, `int16`, `int32`, `int64`,
-      `complex64`, `complex128`.
+      `float32`, `float64`, `complex64`, `complex128`.
     name: A name for the operation (optional).
 
   Returns:
