fb580655ca32713fc4e5dc753311f5a88b9a21ec
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-17 15:21:11 -0800

    Internal code refactor
    PiperOrigin-RevId: 489321922

--- tensorflow/python/distribute/values.py
+++ tensorflow/python/distribute/values.py
@@ -1019,6 +1019,40 @@ class DistributedVariable(DistributedDelegate, variables_lib.Variable,
           self._primary.handle]
     return obj_map, resource_map
 
+  def _export_to_saved_model_graph(self,
+                                   object_map=None,
+                                   tensor_map=None,
+                                   options=None,
+                                   **kwargs):
+    # Initialize for self._primary first, so that obj_map[self._primary] and
+    # resource_map[self._primary.handle] contain mapped values.
+    resource_list = self._primary._export_to_saved_model_graph(  # pylint:disable=protected-access
+        object_map=object_map,
+        tensor_map=tensor_map,
+        options=options,
+        **kwargs)
+    for v in [v for v in self._values if v != self._primary]:
+      if (options.experimental_variable_policy  # pylint:disable=protected-access
+          ._expand_distributed_variables()):
+        resource_list.extend(
+            v._export_to_saved_model_graph(  # pylint:disable=protected-access
+                object_map=object_map,
+                tensor_map=tensor_map,
+                options=options,
+                **kwargs))  # pylint:disable=protected-access
+      else:
+        object_map[v] = object_map[self._primary]
+        tensor_map[v.handle] = tensor_map[self._primary.handle]
+        resource_list.append(v.handle)
+    object_map[self] = object_map[self._primary]
+    tensor_map[self] = tensor_map[self._primary.handle]
+    resource_list.append(self)
+    if self._packed_var is not None:
+      tensor_map[self._packed_var.packed_handle] = tensor_map[
+          self._primary.handle]
+      resource_list.append(self._packed_var.packed_handle)
+    return resource_list
+
   def _write_object_proto(self, proto, options):
     """Update a SavedObject proto for the caller.
 
