e28e4ecea8e084dde5ec091cd1c2adbf7494c445
Chenguang Wang <chenguangwang@google.com>
2022-11-16 16:44:10 -0800

    [StableHLO] Drop redundant reshape around dot_general().
    This transform converts reshape(dot_general(reshape(%y), %z)) to
    dot_general(%y, %z) if possible.
    
    PiperOrigin-RevId: 489072943

--- tensorflow/compiler/mlir/lite/stablehlo/tests/optimize.mlir
+++ tensorflow/compiler/mlir/lite/stablehlo/tests/optimize.mlir
@@ -57,3 +57,45 @@ func.func @testDotToDotGeneralMatrixMatrix(%arg0: tensor<2x3072xf32>, %arg1: ten
 // CHECK-SAME: >} : (tensor<2x3072xf32>, tensor<3072x512xf32>) -> tensor<2x512xf32>
 // CHECK:      return %[[RES]] : tensor<2x512xf32>
 }
+
+// -----
+
+// CHECK-LABEL: testRemoveReshapeAroundDotGeneral
+func.func @testRemoveReshapeAroundDotGeneral(%arg0: tensor<3x72x1x2048xf32>, %arg1: tensor<3x2048x512xf32>) -> tensor<3x72x1x512xf32> {
+  %0 = "mhlo.reshape"(%arg0) : (tensor<3x72x1x2048xf32>) -> tensor<3x72x2048xf32>
+  %1 = "mhlo.dot_general"(%0, %arg1) {
+    dot_dimension_numbers = #mhlo.dot<
+        lhs_batching_dimensions = [0],
+        rhs_batching_dimensions = [0],
+        lhs_contracting_dimensions = [2],
+        rhs_contracting_dimensions = [1]
+    >} : (tensor<3x72x2048xf32>, tensor<3x2048x512xf32>) -> tensor<3x72x512xf32>
+  %2 = "mhlo.reshape"(%1) : (tensor<3x72x512xf32>) -> tensor<3x72x1x512xf32>
+  func.return %2 : tensor<3x72x1x512xf32>
+
+// CHECK:      %[[RES:.*]] = "mhlo.dot_general"(%arg0, %arg1) {
+// CHECK-SAME:   dot_dimension_numbers = #mhlo.dot<
+// CHECK-SAME:     lhs_batching_dimensions = [0],
+// CHECK-SAME:     rhs_batching_dimensions = [0],
+// CHECK-SAME:     lhs_contracting_dimensions = [3],
+// CHECK-SAME:     rhs_contracting_dimensions = [1]
+// CHECK-SAME: >} : (tensor<3x72x1x2048xf32>, tensor<3x2048x512xf32>) -> tensor<3x72x1x512xf32>
+// CHECK:      return %[[RES]] : tensor<3x72x1x512xf32>
+}
+
+// -----
+
+// CHECK-LABEL: testRemoveReshapeAroundDot
+func.func @testRemoveReshapeAroundDot(%arg0: tensor<1x1x512xf32>, %arg1: tensor<512x13x!quant.uniform<i8:f32, 0.00285>>) -> tensor<1x1x13xf32> {
+  %0 = "mhlo.reshape"(%arg0) : (tensor<1x1x512xf32>) -> tensor<1x512xf32>
+  %1 = "mhlo.dot"(%0, %arg1) : (tensor<1x512xf32>, tensor<512x13x!quant.uniform<i8:f32, 0.00285>>) -> tensor<1x13xf32>
+  %2 = "mhlo.reshape"(%1) : (tensor<1x13xf32>) -> tensor<1x1x13xf32>
+  func.return %2 : tensor<1x1x13xf32>
+
+// CHECK:      %[[RES:.*]] = "mhlo.dot_general"(%arg0, %arg1) {
+// CHECK-SAME:   dot_dimension_numbers = #mhlo.dot<
+// CHECK-SAME:     lhs_contracting_dimensions = [2],
+// CHECK-SAME:     rhs_contracting_dimensions = [0]
+// CHECK-SAME: >} : (tensor<1x1x512xf32>, tensor<512x13x!quant.uniform<i8:f32, 2.850000e-03>>) -> tensor<1x1x13xf32>
+// CHECK:      return %[[RES]] : tensor<1x1x13xf32>
+}
--- tensorflow/compiler/mlir/lite/stablehlo/transforms/optimize.cc
+++ tensorflow/compiler/mlir/lite/stablehlo/transforms/optimize.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
 #include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
@@ -58,6 +59,109 @@ LogicalResult ConvertDotToDotGeneral(mhlo::DotOp op,
   return success();
 }
 
+// Convert reshape(dot_general(reshape(%y), %z)) to
+// dot_general(%y, %z) if possible.
+LogicalResult RemoveReshapeAroundDotGeneral(mhlo::ReshapeOp reshape_after,
+                                            PatternRewriter &rewriter) {
+  auto dot = dyn_cast_or_null<mhlo::DotGeneralOp>(
+      reshape_after.getOperand().getDefiningOp());
+  if (!dot) return failure();
+
+  auto reshape_before =
+      dyn_cast_or_null<mhlo::ReshapeOp>(dot.getLhs().getDefiningOp());
+  if (!reshape_before) return failure();
+
+  if (!dot.getLhs().getType().hasStaticShape() ||
+      !dot.getRhs().getType().hasStaticShape() ||
+      !reshape_before.getOperand().getType().hasStaticShape() ||
+      !dot.getType().hasStaticShape() ||
+      !reshape_after.getType().hasStaticShape()) {
+    return rewriter.notifyMatchFailure(reshape_after,
+                                       "dynamic shapes not supported");
+  }
+
+  const auto range = [](int64_t begin, int n) {
+    SmallVector<int64_t> result;
+    result.reserve(n);
+    for (int i = 0; i < n; ++i) {
+      result.push_back(i + begin);
+    }
+    return result;
+  };
+
+  // We only support the mhlo.dot style input layouts, i.e.:
+  //   lhs: [batch, other dims, contract dims]
+  //   rhs: [batch, contract dims, other dims]
+  auto dim_nums = dot.getDotDimensionNumbers();
+  int batch_dims_count = dim_nums.getLhsBatchingDimensions().size();
+  int contracting_dims_count = dim_nums.getLhsContractingDimensions().size();
+  if (dim_nums.getLhsBatchingDimensions() !=
+          ArrayRef<int64_t>(range(0, batch_dims_count)) ||
+      dim_nums.getRhsBatchingDimensions() !=
+          ArrayRef<int64_t>(range(0, batch_dims_count)) ||
+      dim_nums.getLhsContractingDimensions() !=
+          ArrayRef<int64_t>(
+              range(dot.getLhs().getType().getRank() - contracting_dims_count,
+                    contracting_dims_count)) ||
+      dim_nums.getRhsContractingDimensions() !=
+          ArrayRef<int64_t>(range(batch_dims_count, contracting_dims_count))) {
+    return rewriter.notifyMatchFailure(reshape_after,
+                                       "unsupported dot_general layout");
+  }
+
+  // (B = batch dims, C = contracting dims, Y/Z = other dims)
+  //
+  // This pattern converts:
+  //   %before = "mhlo.reshape"(%lhs : BxY1xC) : BxY2xC
+  //   %dot = "mhlo.dot_general"(%before, %rhs : BxCxZ) : BxY2xZ
+  //   %after = "mhlo.reshape"(%dot) : BxY1xZ
+  // to:
+  //   %dot : "mhlo.dot_general"(%lhs : BxY1xC, %rhs : BxCxZ) : BxY1xZ
+
+  // Extract B, Y1, C from %lhs.
+  ArrayRef<int64_t> shape_lhs =
+      reshape_before.getOperand().getType().getShape();
+  ArrayRef<int64_t> shape_b = shape_lhs.take_front(batch_dims_count);
+  ArrayRef<int64_t> shape_c = shape_lhs.take_back(contracting_dims_count);
+  ArrayRef<int64_t> shape_y1 =
+      shape_lhs.drop_front(shape_b.size()).drop_back(shape_c.size());
+
+  // Check %before shape, and extract Y2 from it.
+  ArrayRef<int64_t> shape_before = reshape_before.getType().getShape();
+  if (shape_before.take_front(shape_b.size()) != shape_b ||
+      shape_before.take_back(shape_c.size()) != shape_c) {
+    return failure();
+  }
+  ArrayRef<int64_t> shape_y2 =
+      shape_before.drop_front(shape_b.size()).drop_back(shape_c.size());
+
+  // No need to check %dot; dot_general verifier ensures correct shapes.
+  // Extract Z from %dot.
+  ArrayRef<int64_t> shape_z =
+      dot.getType().getShape().drop_front(shape_b.size() + shape_y2.size());
+
+  // Check %after shape.
+  if (reshape_after.getType().getShape() !=
+      ArrayRef<int64_t>(llvm::to_vector(
+          llvm::concat<const int64_t>(shape_b, shape_y1, shape_z)))) {
+    return failure();
+  }
+
+  rewriter.replaceOpWithNewOp<mhlo::DotGeneralOp>(
+      reshape_after, reshape_after.getType(), reshape_before.getOperand(),
+      dot.getRhs(),
+      mhlo::DotDimensionNumbersAttr::get(
+          reshape_after.getContext(),
+          /*lhsBatchingDimensions=*/range(0, batch_dims_count),
+          /*rhsBatchingDimensions=*/range(0, batch_dims_count),
+          /*lhsContractingDimensions=*/
+          range(batch_dims_count + shape_y1.size(), contracting_dims_count),
+          /*rhsContractingDimensions=*/
+          range(batch_dims_count, contracting_dims_count)),
+      dot.getPrecisionConfigAttr());
+  return success();
+}
+
 class OptimizePass
     : public PassWrapper<OptimizePass, OperationPass<func::FuncOp>> {
  public:
@@ -69,6 +173,7 @@ class OptimizePass
   void runOnOperation() override {
     RewritePatternSet patterns(&getContext());
     patterns.add(ConvertDotToDotGeneral);
+    patterns.add(RemoveReshapeAroundDotGeneral);
     if (failed(applyPatternsAndFoldGreedily(getOperation(),
                                             std::move(patterns)))) {
       return signalPassFailure();
