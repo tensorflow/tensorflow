0f61e4cc74e670e81de0d73c69370ed60ce4fb6e
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-18 08:32:42 -0800

    Factor out `tf.data.Dataset.shuffle`.
    PiperOrigin-RevId: 489482436

--- tensorflow/python/data/ops/dataset_ops.py
+++ tensorflow/python/data/ops/dataset_ops.py
@@ -1429,8 +1429,13 @@ class DatasetV2(
     Returns:
       A new `Dataset` with the transformation applied as described above.
     """
-    return ShuffleDataset(
+    # Loaded lazily due to a circular dependency (dataset_ops ->
+    # shuffle_op -> dataset_ops).
+    # pylint: disable=g-import-not-at-top,protected-access
+    from tensorflow.python.data.ops import shuffle_op
+    return shuffle_op._shuffle(
         self, buffer_size, seed, reshuffle_each_iteration, name=name)
+    # pylint: enable=g-import-not-at-top,protected-access
 
   def cache(self, filename="", name=None):
     """Caches the elements in this dataset.
@@ -4784,46 +4789,6 @@ repeat_op = lazy_loader.LazyLoader(
     "repeat_op", globals(),
     "tensorflow.python.data.ops.repeat_op")
 RepeatDataset = repeat_op._RepeatDataset  # pylint: disable=protected-access
-
-
-class ShuffleDataset(UnaryUnchangedStructureDataset):
-  """A `Dataset` that randomly shuffles the elements of its input."""
-
-  def __init__(self,
-               input_dataset,
-               buffer_size,
-               seed=None,
-               reshuffle_each_iteration=None,
-               name=None):
-    """See `Dataset.shuffle()` for details."""
-    self._input_dataset = input_dataset
-    self._buffer_size = ops.convert_to_tensor(
-        buffer_size, dtype=dtypes.int64, name="buffer_size")
-    self._seed, self._seed2 = random_seed.get_seed(seed)
-    if reshuffle_each_iteration is None:
-      reshuffle_each_iteration = True
-    self._reshuffle_each_iteration = reshuffle_each_iteration
-    self._name = name
-
-    if (tf2.enabled() and
-        (context.executing_eagerly() or ops.inside_function())):
-      variant_tensor = gen_dataset_ops.shuffle_dataset_v3(
-          input_dataset._variant_tensor,  # pylint: disable=protected-access
-          buffer_size=self._buffer_size,
-          seed=self._seed,
-          seed2=self._seed2,
-          seed_generator=gen_dataset_ops.dummy_seed_generator(),
-          reshuffle_each_iteration=self._reshuffle_each_iteration,
-          **self._common_args)
-    else:
-      variant_tensor = gen_dataset_ops.shuffle_dataset(
-          input_dataset._variant_tensor,  # pylint: disable=protected-access
-          buffer_size=self._buffer_size,
-          seed=self._seed,
-          seed2=self._seed2,
-          reshuffle_each_iteration=self._reshuffle_each_iteration,
-          **self._common_args)
-    super(ShuffleDataset, self).__init__(input_dataset, variant_tensor)
 
 
 class InterleaveDataset(UnaryDataset):
