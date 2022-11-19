dbbf6c98ce43737e50cc7477c00bf1670141885c
Anlun Xu <anlunx@google.com>
2022-11-18 16:33:18 -0800

    [XLA:GPU] Add GpuTargetConfigProto so that we can pass in GpuTargetConfig to genrule
    PiperOrigin-RevId: 489587805

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.h
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.h
@@ -34,6 +34,7 @@ limitations under the License.
 #include "tensorflow/compiler/xla/service/llvm_compiler.h"
 #include "tensorflow/compiler/xla/statusor.h"
 #include "tensorflow/compiler/xla/stream_executor/device_description.h"
+#include "tensorflow/compiler/xla/stream_executor/device_description.pb.h"
 #include "tensorflow/compiler/xla/stream_executor/stream_executor.h"
 #include "tensorflow/compiler/xla/stream_executor/stream_executor_pimpl.h"
 #include "tensorflow/compiler/xla/types.h"
@@ -96,6 +97,24 @@ class GpuXlaRuntimeAotCompilationResult : public AotCompilationResult {
 };
 
 struct GpuTargetConfig {
+  GpuTargetConfig() = default;
+  explicit GpuTargetConfig(const stream_executor::GpuTargetConfigProto& proto)
+      : gpu_device_info(proto.gpu_device_info()),
+        cuda_compute_capability(proto.cuda_compute_capability()),
+        rocm_compute_capability(proto.rocm_compute_capability()),
+        platform_name(proto.platform_name()) {}
+
+  stream_executor::GpuTargetConfigProto ToProto() const {
+    stream_executor::GpuTargetConfigProto proto;
+    *proto.mutable_gpu_device_info() = gpu_device_info.ToProto();
+    *proto.mutable_cuda_compute_capability() =
+        cuda_compute_capability.ToProto();
+    *proto.mutable_rocm_compute_capability() =
+        rocm_compute_capability.ToProto();
+    proto.set_platform_name(platform_name);
+    return proto;
+  }
+
   GpuDeviceInfo gpu_device_info;
   // CUDA "CC" major value, -1 if not available.
   stream_executor::CudaComputeCapability cuda_compute_capability{-1, -1};
