e87e6cd81fc093065a12f8240dcfc8bf95dff34f
Thai Nguyen <thaink@google.com>
2022-11-28 23:23:43 -0800

    Make sure input/output node names are unique when creating the main function
    For manually created SavedModel, there is no guarantee that these names are unique. In such case, the signature prefix will be added to uniquify them.
    
    PiperOrigin-RevId: 491539727

--- tensorflow/compiler/mlir/quantization/tensorflow/python/save_model.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/save_model.py
@@ -137,37 +137,54 @@ def _create_empty_output_dir(output_directory: str) -> None:
 
 
 def _validate_signatures(signature_def_map: _SignatureDefMap,
-                         exported_graph: ops.Graph) -> None:
+                         exported_graph: ops.Graph) -> _SignatureDefMap:
   """Validates if the tensor names in signatures are consistent with the graph.
 
   This function checks if the input and output tensor names in the signatures
   exist if the graph. The output tensor names might change during conversion,
-  we try to fix that with `_restore_output_tensor_names`. However, if that
-  doesn't work the signatures can't be used with the converted graph.
+  we try to fix that with `_restore_output_tensor_names`. Besides, if there
+  are duplicated tensor names, they we will be prefixed with the signature name.
+  However, if that doesn't work the signatures can't be used with the converted
+  graph.
 
   Args:
     signature_def_map: the signatures to validate.
     exported_graph: The PTQ-exported GraphDef.
 
+  Returns:
+    The signatures with tensor names prefixed with signature name if necessary.
+
   Raises:
     ValueError: Iff the signatures are not consistent with the graph.
   """
-  for signature_def in signature_def_map.values():
+  for signature_key, signature_def in signature_def_map.items():
     for tensor_info in signature_def.inputs.values():
       try:
         exported_graph.get_tensor_by_name(tensor_info.name)
       except KeyError as exc:
-        raise ValueError(
-            'Cannot find the input tensor with name %s in the graph.' %
-            tensor_info.name) from exc
+        try:
+          prefixed_name = signature_key + '_' + tensor_info.name
+          exported_graph.get_tensor_by_name(prefixed_name)
+          tensor_info.name = prefixed_name
+        except KeyError:
+          raise ValueError(
+              'Cannot find the input tensor with name %s in the graph.' %
+              tensor_info.name) from exc
 
     for tensor_info in signature_def.outputs.values():
       try:
         exported_graph.get_tensor_by_name(tensor_info.name)
       except KeyError as exc:
-        raise ValueError(
-            'Cannot find the output tensor with name %s in the graph.' %
-            tensor_info.name) from exc
+        try:
+          prefixed_name = signature_key + '_' + tensor_info.name
+          exported_graph.get_tensor_by_name(prefixed_name)
+          tensor_info.name = prefixed_name
+        except KeyError:
+          raise ValueError(
+              'Cannot find the output tensor with name %s in the graph.' %
+              tensor_info.name) from exc
+
+  return signature_def_map
 
 
 def _find_op(graph: ops.Graph,
@@ -221,7 +238,8 @@ def save_model_v1(graph_def: graph_pb2.GraphDef,
   with session.Session(graph=ops.Graph()) as sess:
     importer.import_graph_def(graph_def, name='')
 
-    _validate_signatures(signature_def_map, ops.get_default_graph())
+    signature_def_map = _validate_signatures(signature_def_map,
+                                             ops.get_default_graph())
     v1_builder.add_meta_graph_and_variables(
         sess,
         tags,
