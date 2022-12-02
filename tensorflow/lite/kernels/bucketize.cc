badd21e0a71f6f6add9c6ae3855c4c79c7bca44d
Kun-Lu <kun.lu@ibm.com>
2022-12-02 12:16:51 -0500

    Fix the endianness issue in lite/kernels:bucketize_test on s390x
    Signed-off-by: Kun-Lu <kun.lu@ibm.com>

--- tensorflow/lite/kernels/bucketize.cc
+++ tensorflow/lite/kernels/bucketize.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/tensor.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
+#include "flatbuffers/flatbuffers.h"  // from @flatbuffers
 
 namespace tflite {
 namespace ops {
@@ -41,6 +42,12 @@ struct OpData {
 void* Init(TfLiteContext* context, const char* buffer, size_t length) {
   auto* op_data = new OpData();
   const auto* params = reinterpret_cast<const TfLiteBucketizeParams*>(buffer);
+
+  if(!FLATBUFFERS_LITTLEENDIAN){
+    int32_t *p = reinterpret_cast<int32_t*>(const_cast<float*>(params->boundaries));
+    for (size_t i = 0; i < params->num_boundaries; i++, p++)
+      *p = flatbuffers::EndianSwap(*p);
+  }
 
   op_data->boundaries = params->boundaries;
   op_data->num_boundaries = params->num_boundaries;
