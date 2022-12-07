ffecec16947521fb9fe30fc6398ebc64bd31f30d
Yosr MANAI <49559464+yo08@users.noreply.github.com>
2022-12-07 19:29:18 +0100

    Update label_image.py
    if we're using TensorFlow 2.0, we should do the following changes too:
    tf.read_file() -----becomes-----> tf.io.read_file()
    tf.gfile.GFile(label_file) -----becomes-----> tf.io.gfile.GFile(label_file)

--- tensorflow/examples/label_image/label_image.py
+++ tensorflow/examples/label_image/label_image.py
@@ -14,9 +14,10 @@
 # ==============================================================================
 
 import argparse
-
+import cv2
 import numpy as np
 import tensorflow as tf
+import PIL
 
 
 def load_graph(model_file):
@@ -38,7 +39,7 @@ def read_tensor_from_image_file(file_name,
                                 input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
+  file_reader = tf.read_file(file_name)
   if file_name.endswith(".png"):
     image_reader = tf.io.decode_png(file_reader, channels=3, name="png_reader")
   elif file_name.endswith(".gif"):
@@ -56,13 +57,23 @@ def read_tensor_from_image_file(file_name,
   return sess.run(normalized)
 
 
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
+ 
+
 def load_labels(label_file):
   proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
   return [l.rstrip() for l in proto_as_ascii_lines]
 
 
 if __name__ == "__main__":
-  file_name = "tensorflow/examples/label_image/data/grace_hopper.jpg"
+  
+  file_name = cv2.VideoCapture("VideoPath.jpg") # pass the corresponding video path as an argument
   model_file = \
     "tensorflow/examples/label_image/data/inception_v3_2016_08_28_frozen.pb"
   label_file = "tensorflow/examples/label_image/data/imagenet_slim_labels.txt"
@@ -105,25 +116,47 @@ if __name__ == "__main__":
     output_layer = args.output_layer
 
   graph = load_graph(model_file)
-  t = read_tensor_from_image_file(
-      file_name,
+  while (file_name.isOpened()):
+    (taken, frame)= file_name.read()
+    if frame is None:
+      break
+    cv2.imwrite("current.jpeg",frame)
+    t = read_tensor_from_image_file(
+      file_name="current.jpeg",
       input_height=input_height,
       input_width=input_width,
       input_mean=input_mean,
       input_std=input_std)
 
-  input_name = "import/" + input_layer
-  output_name = "import/" + output_layer
-  input_operation = graph.get_operation_by_name(input_name)
-  output_operation = graph.get_operation_by_name(output_name)
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
-  with tf.compat.v1.Session(graph=graph) as sess:
-    results = sess.run(output_operation.outputs[0], {
-        input_operation.outputs[0]: t
-    })
-  results = np.squeeze(results)
+    with tf.compat.v1.Session(graph=graph) as sess:
+      results = sess.run(output_operation.outputs[0], {
+          input_operation.outputs[0]: t
+      })
+    results = np.squeeze(results)
+    labels = load_labels(label_file)
+    
+    index = results.argmax()
+    value = str(results[index])
+    best_label = str(labels[index])
+    
+    np_im = np.array(tensor_to_image(t))
+    text = value + " "+ best_label
+    org = (6,280)
+    fontFace = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.6
+    thickness = 2
+  
+    if results[index] >= 0.80:
+      cv2.putText(np_im, text, org, fontFace,  fontScale, (0, 255,0), thickness)
+    else:
+      cv2.putText(np_im , text, org, fontFace, fontScale, (0, 0, 255), thickness)
+      
+    cv2.imshow(np_im)
 
-  top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(label_file)
-  for i in top_k:
-    print(labels[i], results[i])
+  file_name.release()
+  cv2.destroyAllWindows()
