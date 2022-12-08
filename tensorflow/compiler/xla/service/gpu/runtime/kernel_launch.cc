26dac69d4f9024dfaadba9454d27ede9bc04e1bc
Eugene Zhulenev <ezhulenev@google.com>
2022-12-08 15:41:36 -0800

    [xla:gpu] NFC: Use FunctionWrapper in kernel launch custom call implementation
    PiperOrigin-RevId: 494014511

--- tensorflow/compiler/xla/service/gpu/runtime/kernel_launch.cc
+++ tensorflow/compiler/xla/service/gpu/runtime/kernel_launch.cc
@@ -47,27 +47,13 @@ StreamExecutorKernels* GpuExecutableKernels::operator()(
 // Define the kernel launch custom call.
 //===----------------------------------------------------------------------===//
 
-namespace {
-struct KernelLaunch {
-  LLVM_ATTRIBUTE_ALWAYS_INLINE
-  absl::Status operator()(
-      const ServiceExecutableRunOptions* run_options, const std::string* ptx,
-      const std::vector<uint8_t>* cubin, se::DeviceMemoryBase* temp_buffer,
-      State<std::unique_ptr<se::KernelBase>> device_kernel, int32_t grid_size_x,
-      int32_t grid_size_y, int32_t grid_size_z, int32_t block_size_x,
-      int32_t block_size_y, int32_t block_size_z,
-      CustomCall::RemainingArgs args, std::string_view name) const;
-  static KernelLaunch Handler() { return KernelLaunch(); }
-};
-}  // namespace
-
-absl::Status KernelLaunch::operator()(
+static absl::Status LaunchImpl(
     const ServiceExecutableRunOptions* run_options, const std::string* ptx,
     const std::vector<uint8_t>* cubin, se::DeviceMemoryBase* temp_buffer,
     State<std::unique_ptr<se::KernelBase>> device_kernel, int32_t grid_size_x,
     int32_t grid_size_y, int32_t grid_size_z, int32_t block_size_x,
     int32_t block_size_y, int32_t block_size_z, CustomCall::RemainingArgs args,
-    std::string_view name) const {
+    std::string_view name) {
   se::Stream* stream = run_options->stream();
   se::StreamExecutor* executor = stream->parent();
 
@@ -119,7 +105,7 @@ absl::Status KernelLaunch::operator()(
 //===----------------------------------------------------------------------===//
 
 XLA_RUNTIME_DEFINE_CUSTOM_CALL(
-    Launch, KernelLaunch::Handler(), checks,
+    Launch, FunctionWrapper<LaunchImpl>(), checks,
     CustomCall::Bind("xla.gpu.func.launch")
         .UserData<const ServiceExecutableRunOptions*>()
         .UserData<const std::string*>()
