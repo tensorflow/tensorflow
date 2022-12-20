099f3f2fd5f5935be8e4dc1c6a662e229d6c306a
A. Unique TensorFlower <gardener@tensorflow.org>
2022-12-20 13:46:13 -0800

    DTensor tests using test_util conditionlessly enable v2 mode.
    DTensor is only intended to work with TF2, anyway.
    
    PiperOrigin-RevId: 496741682

--- tensorflow/dtensor/python/tests/test_util.py
+++ tensorflow/dtensor/python/tests/test_util.py
@@ -34,12 +34,16 @@ from tensorflow.dtensor.python.config import is_gpu_present  # pylint: disable=u
 from tensorflow.dtensor.python.config import is_tpu_present  # pylint: disable=unused-import
 from tensorflow.dtensor.python.config import preferred_device_type  # pylint: disable=unused-import
 from tensorflow.dtensor.python.tests.test_backend_name import DTensorTestUtilBackend
+from tensorflow.python.compat import v2_compat
 from tensorflow.python.eager import context
 from tensorflow.python.framework import config as tf_config
 from tensorflow.python.framework import device as tf_device
 from tensorflow.python.ops import resource_variable_ops
 from tensorflow.python.platform import test as tf_test
 # pylint: enable=g-direct-tensorflow-import
+
+# DTensor only runs with TF V2.
+v2_compat.enable_v2_behavior()
 
 DEFAULT_TOL = 1e-5
 
