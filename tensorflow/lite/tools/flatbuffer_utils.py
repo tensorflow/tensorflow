fc9cb35f3777aadb60a61639e0fd8ad462371913
A. Unique TensorFlower <gardener@tensorflow.org>
2022-12-20 15:32:27 -0800

    Update randomize weights script to skip over specified ops (RESIZE_BILINEAR for example shouldn't be randomized)
    PiperOrigin-RevId: 496768300

--- tensorflow/lite/tools/flatbuffer_utils.py
+++ tensorflow/lite/tools/flatbuffer_utils.py
@@ -172,6 +172,24 @@ def rename_custom_ops(model, map_custom_op_renames):
         op_code.customCode = map_custom_op_renames[op_code_str].encode('ascii')
 
 
+def opcode_to_name(model, op_code):
+  """Converts a TFLite op_code to the human readable name.
+
+  Args:
+    model: The input tflite model.
+    op_code: The op_code to resolve to a readable name.
+
+  Returns:
+    A string containing the human readable op name, or None if not resolvable.
+  """
+  op = model.operatorCodes[op_code]
+  code = max(op.builtinCode, op.deprecatedBuiltinCode)
+  for name, value in vars(schema_fb.BuiltinOperator).items():
+    if value == code:
+      return name
+  return None
+
+
 def xxd_output_to_bytes(input_cc_file):
   """Converts xxd output C++ source file to bytes (immutable).
 
