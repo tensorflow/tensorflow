98ab023eba61d1bed1e5603ad4b564236d765faf
Arian Arfaian <aarfaian@google.com>
2022-12-23 08:12:20 -0800

    Add a utility function to convert from TF's size values to TFLite.
    TF uses `std::numeric_limits<int64_t>::min()` to represent dynamic size. Since
    TFLite uses 32-bit types to represent sizes in most places this results in
    integer narrowing bugs.
    
    This change also cleans up call-sites where this is needed.
    
    PiperOrigin-RevId: 497377327

--- tensorflow/compiler/mlir/lite/ir/tfl_ops.cc
+++ tensorflow/compiler/mlir/lite/ir/tfl_ops.cc
@@ -56,6 +56,7 @@ limitations under the License.
 #include "mlir/Transforms/InliningUtils.h"  // from @llvm-project
 #include "mlir/Transforms/RegionUtils.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/lite/utils/arithmetic_count_util.h"
+#include "tensorflow/compiler/mlir/lite/utils/size_utils.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_op_interfaces.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops_a_m.h"
@@ -2000,10 +2001,7 @@ struct ReplacePackWithReshape : public RewritePattern {
     // This is to workaround the unnecessary cast i64 -> i32.
     SmallVector<int32_t, 4> new_shape_array;
     for (auto size : output_type.getShape()) {
-      // TODO(b/259719789): clean up dynamic shape check (e.g. into a
-      // discrete function) once bug is completely fixed.
-      new_shape_array.push_back(
-          mlir::ShapedType::isDynamic(size) ? -1 : static_cast<int32_t>(size));
+      new_shape_array.push_back(ConvertToTfliteSize(size));
     }
 
     auto new_shape = rewriter.create<TFL::ConstOp>(
@@ -2106,11 +2104,7 @@ TFL::ConstOp NarrowDownInt64InputValuesForOp(Operation* input_op,
   SmallVector<int32_t, 4> value_i32;
   value_i32.reserve(value_type.getRank());
   for (const auto& size : attr) {
-    // TODO(b/259719789): clean up dynamic shape check (e.g. into a
-    // discrete function) once bug is completely fixed.
-    value_i32.push_back(mlir::ShapedType::isDynamic(size.getSExtValue())
-                            ? -1
-                            : static_cast<int32_t>(size.getSExtValue()));
+    value_i32.push_back(ConvertToTfliteSize(size.getSExtValue()));
   }
   auto new_value_i32_attr =
       mlir::DenseIntElementsAttr::get(value_shape_type, value_i32);
