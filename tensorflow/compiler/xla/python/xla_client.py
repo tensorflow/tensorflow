f3d32d5642f3498672164e591582724f1ee430d8
George Necula <necula@google.com>
2022-12-27 04:47:48 -0800

    Add support for dynamic shapes to GPU threefry2x32 custom call.
    In presence of dynamic shapes the ThreeFry2x32Descriptor will contain the
    value n=-1, and the actual desired output length will be passed as
    an additional operand. If the shape is static then the length will be
    passed as part of the descriptor.
    
    PiperOrigin-RevId: 497945778

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -43,7 +43,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes.
-_version = 112
+_version = 113
 
 # Version number for MLIR:Python components.
 mlir_api_version = 41
