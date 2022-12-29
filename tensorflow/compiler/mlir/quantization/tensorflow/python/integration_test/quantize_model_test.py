f15ddff5da0f9986a953096df59785bb411a17e9
Thai Nguyen <thaink@google.com>
2022-12-28 18:03:57 -0800

    Add tests for Einsum in TF Quantizer
    PiperOrigin-RevId: 498280084

--- tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
@@ -897,6 +897,88 @@ class StaticRangeQuantizationTest(quantize_model_test_base.QuantizedModelTest):
     self.assertAllClose(new_outputs, got_outputs, atol=0.1202)
     self.assertAllClose(new_outputs, expected_outputs, atol=0.1023)
 
+  @parameterized.parameters(
+      ('abc,cde->abde', (2, 2, 64), (64, 3, 3), (3, 3), quant_opts_pb2.XLA),
+      ('abc,dce->adbe', (2, 2, 64), (3, 64, 3), (2, 3), quant_opts_pb2.XLA))
+  def test_einsum_ptq_model(self, equation: str, input_shape: Sequence[int],
+                            weight_shape: Sequence[int],
+                            bias_shape: Sequence[int],
+                            target_opset: quant_opts_pb2.OpSet):
+    input_saved_model_path = self.create_tempdir('input').full_path
+    model = self._create_einsum_model(
+        input_saved_model_path,
+        equation,
+        input_shape,
+        weight_shape,
+        bias_shape,
+        activation_fn=nn_ops.relu)
+
+    def data_gen() -> repr_dataset.RepresentativeDataset:
+      for _ in range(200):
+        yield {
+            'input_tensor':
+                ops.convert_to_tensor(
+                    np.random.uniform(low=0.0, high=1.0,
+                                      size=input_shape).astype('f4')),
+        }
+
+    tags = {tag_constants.SERVING}
+    output_directory = self.create_tempdir().full_path
+
+    quantization_options = quant_opts_pb2.QuantizationOptions(
+        quantization_method=quant_opts_pb2.QuantizationMethod(
+            experimental_method=_ExperimentalMethod.STATIC_RANGE))
+
+    converted_model = quantize_model.quantize(
+        input_saved_model_path, ['serving_default'],
+        tags,
+        output_directory,
+        quantization_options,
+        representative_dataset=data_gen())
+    self.assertIsNotNone(converted_model)
+    self.assertCountEqual(converted_model.signatures._signatures.keys(),
+                          {'serving_default'})
+
+    output_loader = saved_model_loader.SavedModelLoader(output_directory)
+    output_meta_graphdef = output_loader.get_meta_graph_def_from_tags(tags)
+    self.assertTrue(
+        self._contains_quantized_function_call(output_meta_graphdef))
+
+    input_data = ops.convert_to_tensor(
+        np.random.uniform(low=0.0, high=1.0, size=input_shape).astype('f4'))
+    expected_outputs = model.einsum(input_data)
+    got_outputs = converted_model.signatures['serving_default'](
+        input_tensor=ops.convert_to_tensor(input_data))
+    self.assertAllClose(expected_outputs, got_outputs, atol=0.0608)
+
+    # Check the converted model in the target opset.
+    quantization_options = quant_opts_pb2.QuantizationOptions(
+        quantization_method=quant_opts_pb2.QuantizationMethod(
+            experimental_method=_ExperimentalMethod.STATIC_RANGE),
+        op_set=target_opset)
+
+    output_directory = self.create_tempdir().full_path
+    converted_model = quantize_model.quantize(
+        input_saved_model_path, ['serving_default'],
+        tags,
+        output_directory,
+        quantization_options,
+        representative_dataset=data_gen())
+
+    self.assertIsNotNone(converted_model)
+    self.assertCountEqual(converted_model.signatures._signatures.keys(),
+                          {'serving_default'})
+    output_loader = saved_model_loader.SavedModelLoader(output_directory)
+    output_meta_graphdef = output_loader.get_meta_graph_def_from_tags(tags)
+    if target_opset == quant_opts_pb2.XLA:
+      self.assertTrue(self._contains_op(output_meta_graphdef, 'XlaDotV2'))
+
+    new_outputs = converted_model.signatures['serving_default'](
+        input_tensor=ops.convert_to_tensor(input_data))
+    # The difference between TF and target path is expected to be small.
+    self.assertAllClose(new_outputs, got_outputs, atol=0.0666)
+    self.assertAllClose(new_outputs, expected_outputs, atol=0.057)
+
   # Raises error because the constant unfreezing is not yet fully implemented.
   @test_util.deprecated_graph_mode_only
   def test_matmul_ptq_model_with_unfreeze_constants_raises_error(self):
--- tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test_base.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test_base.py
@@ -738,6 +738,75 @@ class QuantizedModelTest(test.TestCase, parameterized.TestCase):
                 shape=input_shape, dtype=dtypes.float32, name='input_tensor')))
     return model
 
+  def _create_einsum_model(
+      self,
+      saved_model_path: str,
+      equation: str,
+      input_shape: Sequence[int],
+      weight_shape: Sequence[int],
+      bias_shape: Optional[Sequence[int]] = None,
+      activation_fn: Optional[ops.Operation] = None) -> module.Module:
+
+    class EinsumModel(module.Module):
+      """A simple model with a single einsum.
+
+      Bias and activation function are optional.
+      """
+
+      def __init__(self,
+                   equation: str,
+                   weight_shape: Sequence[int],
+                   bias_shape: Optional[Sequence[int]] = None,
+                   activation_fn: Optional[ops.Operation] = None) -> None:
+        """Initializes a EinsumModel.
+
+        Args:
+          equation: a string describing the contraction.
+          weight_shape: Shape of the weight tensor.
+          bias_shape: Shape of the bias. This is not always 1D so Einsum ops
+            usually use Add op instead of BiasAdd.
+          activation_fn: The activation function to be used. No activation
+            function if None.
+        """
+        self.equation = equation
+        self.activation_fn = activation_fn
+        self.weight = np.random.uniform(low=-1.0, high=1.0, size=weight_shape)
+        self.bias = np.random.uniform(
+            low=-1.0, high=1.0,
+            size=bias_shape) if bias_shape is not None else None
+
+      @def_function.function
+      def einsum(self, input_tensor: core.Tensor) -> Mapping[str, core.Tensor]:
+        """Evaluates the Einstein summation convention.
+
+        Depending on self.has_bias and self.activation_fn, it may add a bias
+        term or go through the activaction function.
+
+        Args:
+          input_tensor: Input tensor to einsum with the weight.
+
+        Returns:
+          A map of: output key -> output result.
+        """
+        out = tensorflow.einsum(self.equation, input_tensor, self.weight)
+
+        if self.bias is not None:
+          out = out + self.bias
+
+        if self.activation_fn is not None:
+          out = self.activation_fn(out)
+
+        return {'output': out}
+
+    model = EinsumModel(equation, weight_shape, bias_shape, activation_fn)
+    saved_model_save.save(
+        model,
+        saved_model_path,
+        signatures=model.einsum.get_concrete_function(
+            tensor_spec.TensorSpec(
+                shape=input_shape, dtype=dtypes.float32, name='input_tensor')))
+    return model
+
   def _create_and_save_tf1_conv_model(
       self,
       saved_model_path: str,
