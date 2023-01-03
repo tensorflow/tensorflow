88e2cebb97baf457bf02b294adfc086c0e02bcad
Tom Hennigan <tomhennigan@google.com>
2023-01-03 09:14:57 -0800

    Add `{SerializeAs,ParseFrom}String` to `xe.CompileOptions`.
    Naming is aligned with Python protobuf API [0].
    
    [0] https://developers.google.com/protocol-buffers/docs/pythontutorial#parsing-and-serialization
    
    PiperOrigin-RevId: 499236638

--- tensorflow/compiler/xla/python/xla_client_test.py
+++ tensorflow/compiler/xla/python/xla_client_test.py
@@ -2528,6 +2528,37 @@ def TestFactory(xla_backend,
                                                       self.backend)
       self.assertTrue(np.all(actual == expected))
 
+    def testCompileOptionsSerialization(self):
+      options = xla_client.CompileOptions()
+      executable_build_options = options.executable_build_options
+      options.num_replicas = 3
+      options.num_partitions = 2
+      options.profile_version = 1337
+      options.compile_portable_executable = True
+      executable_build_options.num_replicas = 3
+      executable_build_options.num_partitions = 2
+      executable_build_options.debug_options.xla_cpu_enable_fast_math = True
+      executable_build_options.debug_options.xla_test_all_input_layouts = True
+
+      b = options.SerializeAsString()
+      restored = xla_client.CompileOptions.ParseFromString(b)
+
+      for name in ("num_replicas", "num_partitions", "profile_version",
+                   "compile_portable_executable"):
+        self.assertEqual(getattr(options, name), getattr(restored, name),
+                         msg=name)
+
+      for name in ("num_replicas", "num_partitions"):
+        self.assertEqual(getattr(options.executable_build_options, name),
+                         getattr(restored.executable_build_options, name),
+                         msg=name)
+
+      for name in ("xla_cpu_enable_fast_math", "xla_test_all_input_layouts"):
+        self.assertEqual(
+            getattr(options.executable_build_options.debug_options, name),
+            getattr(restored.executable_build_options.debug_options, name),
+            msg=name)
+
   tests.append(ClientTest)
 
   # TODO(b/182461453): Add TFRT and cloud TPU implementation of
