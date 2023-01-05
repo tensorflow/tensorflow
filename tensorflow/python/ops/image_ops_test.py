9cf7fc61f4744477d28195683c70d1b5d5374f79
Antonio Sanchez <cantonios@google.com>
2023-01-04 22:12:44 -0800

    Prevent integer overflow in combined_non_max_suppression.
    PiperOrigin-RevId: 499710532

--- tensorflow/python/ops/image_ops_test.py
+++ tensorflow/python/ops/image_ops_test.py
@@ -5155,6 +5155,47 @@ class CombinedNonMaxSuppressionTest(test_util.TensorFlowTestCase):
           max_output_size_per_class=max_output_size_per_class,
           max_total_size=max_total_size)
 
+  def testLargeMaxOutputSizePerClass(self):
+    # Ensure the max_output_size_per_class doesn't result in overflows.
+    boxes = [[[
+        [0, 0, 1, 1],
+        [0, 0.1, 1, 1.1],
+        [0, -0.1, 1, 0.9],
+        [0, 10, 1, 11],
+        [0, 10.1, 1, 11.1],
+        [0, 100, 1, 101],
+    ]]]
+    scores = [[[0.9, 0.75, 0.6, 0.95, 0.5, 0.3]]]
+    nmsed_boxes, nmsed_scores, nmsed_classes, valid_detections = (
+        image_ops.combined_non_max_suppression(
+            boxes=boxes,
+            scores=scores,
+            max_output_size_per_class=2**31 - 1,
+            max_total_size=8,
+            pad_per_class=True,
+            clip_boxes=False,
+        )
+    )
+
+    self.assertAllClose(
+        nmsed_boxes,
+        [[
+            [0, 10, 1, 11],
+            [0, 0, 1, 1],
+            [0, 0.1, 1.0, 1.1],
+            [0, -0.1, 1, 0.9],
+            [0, 10.1, 1, 11.1],
+            [0, 100, 1, 101],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]],
+    )
+    self.assertAllClose(nmsed_classes, [[3, 0, 1, 2, 4, 5, 0, 0]])
+    self.assertAllClose(
+        nmsed_scores, [[0.95, 0.9, 0.75, 0.6, 0.5, 0.3, 0.0, 0.0]]
+    )
+    self.assertAllClose(valid_detections, [6])
+
 
 class NonMaxSuppressionTest(test_util.TensorFlowTestCase):
 
