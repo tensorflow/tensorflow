6d11d0d6f2e99c2bfbfac0ba65c6319979742fd5
syedshahbaaz <syed.shahbaaz.ahmed@intel.com>
2023-01-11 23:27:00 -0800

    Updated tolerance values to default

--- tensorflow/python/kernel_tests/math_ops/aggregate_ops_test.py
+++ tensorflow/python/kernel_tests/math_ops/aggregate_ops_test.py
@@ -61,10 +61,7 @@ class AddNTest(test.TestCase):
           actual = self.evaluate(math_ops.add_n(data))
           expected = np.sum(np.vstack(
               [np.expand_dims(d, 0) for d in data]), axis=0)
-          self.assertAllCloseAccordingToType(
-              expected, actual, rtol=2e-6, atol=2e-6,
-              float_rtol=2e-6, float_atol=2e-6, half_rtol=5e-3, half_atol=5e-3,
-              bfloat16_rtol=2e-2, bfloat16_atol=2e-2)
+          self.assertAllCloseAccordingToType(expected, actual)
 
   @test_util.run_deprecated_v1
   def testUnknownShapes(self):
@@ -77,10 +74,7 @@ class AddNTest(test.TestCase):
           actual = sess.run(math_ops.add_n([data_ph] * count), {data_ph: data})
           expected = np.sum(np.vstack([np.expand_dims(data, 0)] * count),
                             axis=0)
-          self.assertAllCloseAccordingToType(
-              expected, actual, rtol=2e-6, atol=2e-6,
-              float_rtol=2e-6, float_atol=2e-6, half_rtol=5e-3, half_atol=5e-3,
-              bfloat16_rtol=2e-2, bfloat16_atol=2e-2)
+          self.assertAllCloseAccordingToType(expected, actual)
 
   @test_util.run_deprecated_v1
   def testVariant(self):
