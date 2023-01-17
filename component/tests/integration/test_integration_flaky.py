3552b620e48fc58441d87b01dbf36f567ddc0d40
Carson Gee <carson.gee@datarobot.com>
2023-01-16 22:39:09 -0700

    Unflakey Large Test Suite

--- component/tests/integration/test_integration_flaky.py
+++ component/tests/integration/test_integration_flaky.py
@@ -2,8 +2,11 @@ import random
 
 import pytest
 
+IS_FLAKY = False
 
-@pytest.mark.parametrize("input", range(0, 90000))
-def test_flaky(input):
-    assert input < 1000000
-    assert random.randint(0, 1)
+
+@pytest.mark.parametrize("param", range(0, 90_000))
+def test_flaky(param):
+    assert param < 100_000
+    if IS_FLAKY:
+        assert random.randint(0, 1_000)
