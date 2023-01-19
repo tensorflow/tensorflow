2e9fb85d1d909790e81872c9591e0a9957ece6cc
Justin Lebar <jlebar@google.com>
2023-01-19 01:20:19 -0800

    [SE] Remove references to stream_executor's status{,or}.h.
    We've removed uses of the types and functions declared in these headers.
    
    PiperOrigin-RevId: 503093517

--- tensorflow/compiler/mlir/tensorflow/utils/compile_mlir_util.h
+++ tensorflow/compiler/mlir/tensorflow/utils/compile_mlir_util.h
@@ -28,7 +28,6 @@ limitations under the License.
 #include "tensorflow/compiler/tf2xla/xla_argument.h"
 #include "tensorflow/compiler/tf2xla/xla_helpers.h"
 #include "tensorflow/compiler/xla/client/xla_computation.h"
-#include "tensorflow/compiler/xla/stream_executor/lib/statusor.h"
 #include "tensorflow/core/common_runtime/device.h"
 #include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/protobuf/graph_debug_info.pb.h"
