4f2935c9a63b48cafaeafacb20e1f1603487f4d8
Xinyi Wang <wxinyi@google.com>
2023-01-24 15:50:00 -0800

    Update exit code.
    PiperOrigin-RevId: 504396775

--- tensorflow/python/distribute/failure_handling/failure_handling.py
+++ tensorflow/python/distribute/failure_handling/failure_handling.py
@@ -213,7 +213,7 @@ class BorgTerminationConfig(TerminationConfig):
       grace_period=None,
       save_fn=None):
     self.termination_watcher_fn = termination_watcher_fn
-    default_exit_fn = lambda: sys.exit(0)
+    default_exit_fn = lambda: sys.exit(42)
     self.exit_fn = exit_fn or default_exit_fn
     self.grace_period = grace_period or 0
     self.save_fn = save_fn
