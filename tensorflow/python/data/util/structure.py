242d75633ec59e917313d57eaedb506294348c26
Jiri Simsa <jsimsa@google.com>
2023-01-26 11:29:40 -0800

    [tf.data] minor cleanup
    PiperOrigin-RevId: 504896917

--- tensorflow/python/data/util/structure.py
+++ tensorflow/python/data/util/structure.py
@@ -107,9 +107,10 @@ def normalize_element(element, element_signature=None):
         normalized_components.append(
             ops.convert_to_tensor(t, name="component_%d" % i))
       else:
-        if hasattr(spec, "_tf_data_normalize") and callable(
-            spec._tf_data_normalize):  # pylint: disable=protected-access
-          normalized_components.append(spec._tf_data_normalize(t))  # pylint: disable=protected-access
+        # To avoid a circular dependency between dataset_ops and structure,
+        # we check the class name instead of using `isinstance`.
+        if spec.__class__.__name__ == "DatasetSpec":
+          normalized_components.append(t)
         elif isinstance(spec, sparse_tensor.SparseTensorSpec):
           normalized_components.append(sparse_tensor.SparseTensor.from_value(t))
         elif isinstance(spec, ragged_tensor.RaggedTensorSpec):
