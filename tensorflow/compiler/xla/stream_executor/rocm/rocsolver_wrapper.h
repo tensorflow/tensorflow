48cd8ae95c2a1a7971c7e2b3b79d33fc1708363e
Chao Chen <cchen104@amd.com>
2023-02-01 15:48:51 +0000

    add rocsolvoler_warpper.h env.h

--- tensorflow/compiler/xla/stream_executor/rocm/rocsolver_wrapper.h
+++ tensorflow/compiler/xla/stream_executor/rocm/rocsolver_wrapper.h
@@ -26,7 +26,6 @@ limitations under the License.
 #else
 #include "rocm/include/rocsolver.h"
 #endif
-#include "tensorflow/compiler/xla/stream_executor/lib/env.h"
 #include "tensorflow/compiler/xla/stream_executor/platform/dso_loader.h"
 #include "tensorflow/compiler/xla/stream_executor/platform/port.h"
 #include "tensorflow/tsl/platform/env.h"
