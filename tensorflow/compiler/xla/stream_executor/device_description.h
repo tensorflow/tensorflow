c1b2218c9e26845e290b864bf0a2a4b16b523f1c
Wen Chen <Wen.Chen@amd.com>
2023-02-03 19:32:59 +0000

    Added gfx940 to the list of supported AMD GPUs.

--- tensorflow/compiler/xla/stream_executor/device_description.h
+++ tensorflow/compiler/xla/stream_executor/device_description.h
@@ -178,26 +178,27 @@ class RocmComputeCapability {
         "gfx906",  // MI50 / MI60
         "gfx908",  // MI100
         "gfx90a",  // MI200
+        "gfx940",  // MI300
         "gfx1030", // Navi21
         "gfx1100"  // Navi31
     };
   }
   std::set<std::string> gfx_versions_with_nhwc_layout_support() {
-    return {"gfx908", "gfx90a"};
+    return {"gfx908", "gfx90a", "gfx940"};
   }
   std::set<std::string> gfx_versions_with_fast_bf16_support() {
-    return {"gfx908", "gfx90a"};
+    return {"gfx908", "gfx90a", "gfx940"};
   }
   std::set<std::string> gfx_versions_with_fast_fp16_support() {
-    return {"gfx906", "gfx908", "gfx90a", "gfx1030", "gfx1100"};
+    return {"gfx906", "gfx908", "gfx90a", "gfx940", "gfx1030", "gfx1100"};
   }
   std::set<std::string> gfx_versions_with_mfma_instr_support() {
-    return {"gfx908", "gfx90a"};
+    return {"gfx908", "gfx90a", "gfx940"};
   }
   std::set<std::string> gfx_versions_with_fp16_atomics_support() {
     // TODO(rocm): Check. This should be the same as
     // gfx_versions_with_fast_fp16_support.
-    return {"gfx90a"};
+    return {"gfx90a", "gfx940"};
   }
 };
 
