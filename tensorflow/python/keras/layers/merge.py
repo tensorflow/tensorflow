a31310b9d86cf75b8b046eef17c8d744736190f0
joe <joesho112358@gmail.com>
2023-02-04 04:18:31 -0500

    converted else's to guard statements

--- tensorflow/python/keras/layers/merge.py
+++ tensorflow/python/keras/layers/merge.py
@@ -116,67 +116,67 @@ class _Merge(Layer):
   def call(self, inputs):
     if not isinstance(inputs, (list, tuple)):
       raise ValueError('A merge layer should be called on a list of inputs.')
-    if self._reshape_required:
-      reshaped_inputs = []
-      input_ndims = list(map(backend.ndim, inputs))
-      if None not in input_ndims:
-        # If ranks of all inputs are available,
-        # we simply expand each of them at axis=1
-        # until all of them have the same rank.
-        max_ndim = max(input_ndims)
-        for x in inputs:
-          x_ndim = backend.ndim(x)
-          for _ in range(max_ndim - x_ndim):
-            x = array_ops.expand_dims(x, axis=1)
-          reshaped_inputs.append(x)
-        return self._merge_function(reshaped_inputs)
-      else:
-        # Transpose all inputs so that batch size is the last dimension.
-        # (batch_size, dim1, dim2, ... ) -> (dim1, dim2, ... , batch_size)
-        transposed = False
-        for x in inputs:
-          x_ndim = backend.ndim(x)
-          if x_ndim is None:
-            x_shape = array_ops.shape(x)
-            batch_size = x_shape[0]
-            new_shape = backend.concatenate(
-                [x_shape[1:],
-                 array_ops.expand_dims(batch_size, axis=-1)])
-            x_transposed = array_ops.reshape(
-                x,
-                array_ops.stack(
-                    [batch_size, math_ops.reduce_prod(x_shape[1:])], axis=0))
-            x_transposed = array_ops.transpose(x_transposed, perm=(1, 0))
-            x_transposed = array_ops.reshape(x_transposed, new_shape)
-            reshaped_inputs.append(x_transposed)
-            transposed = True
-          elif x_ndim > 1:
-            dims = list(range(1, x_ndim)) + [0]
-            reshaped_inputs.append(array_ops.transpose(x, perm=dims))
-            transposed = True
-          else:
-            # We don't transpose inputs if they are 1D vectors or scalars.
-            reshaped_inputs.append(x)
-        y = self._merge_function(reshaped_inputs)
-        y_ndim = backend.ndim(y)
-        if transposed:
-          # If inputs have been transposed, we have to transpose the output too.
-          if y_ndim is None:
-            y_shape = array_ops.shape(y)
-            y_ndim = array_ops.shape(y_shape)[0]
-            batch_size = y_shape[y_ndim - 1]
-            new_shape = backend.concatenate([
-                array_ops.expand_dims(batch_size, axis=-1), y_shape[:y_ndim - 1]
-            ])
-            y = array_ops.reshape(y, (-1, batch_size))
-            y = array_ops.transpose(y, perm=(1, 0))
-            y = array_ops.reshape(y, new_shape)
-          elif y_ndim > 1:
-            dims = [y_ndim - 1] + list(range(y_ndim - 1))
-            y = array_ops.transpose(y, perm=dims)
-        return y
-    else:
+    if not self._reshape_required:
       return self._merge_function(inputs)
+
+    reshaped_inputs = []
+    input_ndims = list(map(backend.ndim, inputs))
+    if None not in input_ndims:
+      # If ranks of all inputs are available,
+      # we simply expand each of them at axis=1
+      # until all of them have the same rank.
+      max_ndim = max(input_ndims)
+      for x in inputs:
+        x_ndim = backend.ndim(x)
+        for _ in range(max_ndim - x_ndim):
+          x = array_ops.expand_dims(x, axis=1)
+        reshaped_inputs.append(x)
+      return self._merge_function(reshaped_inputs)
+    else:
+      # Transpose all inputs so that batch size is the last dimension.
+      # (batch_size, dim1, dim2, ... ) -> (dim1, dim2, ... , batch_size)
+      transposed = False
+      for x in inputs:
+        x_ndim = backend.ndim(x)
+        if x_ndim is None:
+          x_shape = array_ops.shape(x)
+          batch_size = x_shape[0]
+          new_shape = backend.concatenate(
+              [x_shape[1:],
+               array_ops.expand_dims(batch_size, axis=-1)])
+          x_transposed = array_ops.reshape(
+              x,
+              array_ops.stack(
+                  [batch_size, math_ops.reduce_prod(x_shape[1:])], axis=0))
+          x_transposed = array_ops.transpose(x_transposed, perm=(1, 0))
+          x_transposed = array_ops.reshape(x_transposed, new_shape)
+          reshaped_inputs.append(x_transposed)
+          transposed = True
+        elif x_ndim > 1:
+          dims = list(range(1, x_ndim)) + [0]
+          reshaped_inputs.append(array_ops.transpose(x, perm=dims))
+          transposed = True
+        else:
+          # We don't transpose inputs if they are 1D vectors or scalars.
+          reshaped_inputs.append(x)
+      y = self._merge_function(reshaped_inputs)
+      y_ndim = backend.ndim(y)
+      if transposed:
+        # If inputs have been transposed, we have to transpose the output too.
+        if y_ndim is None:
+          y_shape = array_ops.shape(y)
+          y_ndim = array_ops.shape(y_shape)[0]
+          batch_size = y_shape[y_ndim - 1]
+          new_shape = backend.concatenate([
+              array_ops.expand_dims(batch_size, axis=-1), y_shape[:y_ndim - 1]
+          ])
+          y = array_ops.reshape(y, (-1, batch_size))
+          y = array_ops.transpose(y, perm=(1, 0))
+          y = array_ops.reshape(y, new_shape)
+        elif y_ndim > 1:
+          dims = [y_ndim - 1] + list(range(y_ndim - 1))
+          y = array_ops.transpose(y, perm=dims)
+      return y
 
   @tf_utils.shape_type_conversion
   def compute_output_shape(self, input_shape):
@@ -697,8 +697,7 @@ class Dot(_Merge):
     if self.normalize:
       x1 = nn.l2_normalize(x1, axis=axes[0])
       x2 = nn.l2_normalize(x2, axis=axes[1])
-    output = backend.batch_dot(x1, x2, axes)
-    return output
+    return backend.batch_dot(x1, x2, axes)
 
   @tf_utils.shape_type_conversion
   def compute_output_shape(self, input_shape):
--- tensorflow/python/keras/layers/pooling.py
+++ tensorflow/python/keras/layers/pooling.py
@@ -971,17 +971,16 @@ class GlobalAveragePooling1D(GlobalPooling1D):
 
   def call(self, inputs, mask=None):
     steps_axis = 1 if self.data_format == 'channels_last' else 2
-    if mask is not None:
-      mask = math_ops.cast(mask, inputs[0].dtype)
-      mask = array_ops.expand_dims(
-          mask, 2 if self.data_format == 'channels_last' else 1)
-      inputs *= mask
-      return backend.sum(
-          inputs, axis=steps_axis,
-          keepdims=self.keepdims) / math_ops.reduce_sum(
-              mask, axis=steps_axis, keepdims=self.keepdims)
-    else:
+    if mask is None:
       return backend.mean(inputs, axis=steps_axis, keepdims=self.keepdims)
+    mask = math_ops.cast(mask, inputs[0].dtype)
+    mask = array_ops.expand_dims(
+        mask, 2 if self.data_format == 'channels_last' else 1)
+    inputs *= mask
+    return backend.sum(
+        inputs, axis=steps_axis,
+        keepdims=self.keepdims) / math_ops.reduce_sum(
+            mask, axis=steps_axis, keepdims=self.keepdims)
 
   def compute_mask(self, inputs, mask=None):
     return None
