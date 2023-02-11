d06d16ef29767129aeaaefd007481aeb759e5400
Ramesh Sampath <rameshsampath@google.com>
2023-02-10 19:20:49 -0800

    Update Keras dependency to >=2.12
    PiperOrigin-RevId: 508806646

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -127,7 +127,7 @@ REQUIRED_PACKAGES = [
                         'tb-nightly ~= 2.13.0.a'),
     standard_or_nightly('tensorflow_estimator >= 2.11.0rc0, < 2.12',
                         'tf-estimator-nightly ~= 2.13.0.dev'),
-    standard_or_nightly('keras >= 2.11.0rc1, < 2.12',
+    standard_or_nightly('keras >= 2.12.0rc0, < 2.13',
                         'keras-nightly ~= 2.13.0.dev'),
 ]
 REQUIRED_PACKAGES = [p for p in REQUIRED_PACKAGES if p is not None]
