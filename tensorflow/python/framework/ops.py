361c370c04da6949f309c6f57329359410007499
Tristen Allen <tristenallen@google.com>
2023-02-13 07:48:19 -0800

    Relocate core `convert_to_tensor` logic.
    Moves the primary code in `ops.convert_to_tensor`
    into `tensor_conversion_registry`. The `EagerTensor`
    special handling and various internal permutations
    of `convert_to_tensor` remain in `ops.py` for now.
    
    The public API for `convert_to_tensor` has not changed,
    nor has the behavior of the function or any of its
    wrappers.
    
    Tests for `convert_to_tensor` remain in `ops_test.py`
    due to the need to register conversion functions
    in order to test their functionality and the fact that
    the main call site is still `ops.convert_to_tensor`.
    
    If and when more logic is moved into `tensor_conversion_registry`,
    we may consider adding tests to the core function.
    
    If internal code calling `convert_to_tensor` does not
    or cannot pass `EagerTensor` values to
    `ops.convert_to_tensor`, it should use this function
    instead.
    
    PiperOrigin-RevId: 509225228

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -1579,8 +1579,6 @@ def convert_to_tensor(value,
                       ctx=None,
                       accepted_result_types=(Tensor,)):
   """Implementation of the public convert_to_tensor."""
-  # TODO(b/142518781): Fix all call-sites and remove redundant arg
-  preferred_dtype = preferred_dtype or dtype_hint
   if isinstance(value, EagerTensor):
     if ctx is None:
       ctx = context.context()
@@ -1594,73 +1592,10 @@ def convert_to_tensor(value,
                 name=name))
       return graph.capture(value, name=name)
 
-  if dtype is not None:
-    dtype = dtypes.as_dtype(dtype)
-  if isinstance(value, Tensor):
-    if dtype is not None and not dtype.is_compatible_with(value.dtype):
-      raise ValueError(
-          _add_error_prefix(
-              f"Tensor conversion requested dtype {dtype.name} "
-              f"for Tensor with dtype {value.dtype.name}: {value!r}",
-              name=name))
-    return value
-
-  if preferred_dtype is not None:
-    preferred_dtype = dtypes.as_dtype(preferred_dtype)
-
-  # See below for the reason why it's `type(value)` and not just `value`.
-  # https://docs.python.org/3.8/reference/datamodel.html#special-lookup
-  overload = getattr(type(value), "__tf_tensor__", None)
-  if overload is not None:
-    return overload(value, dtype, name)  #  pylint: disable=not-callable
-
-  for base_type, conversion_func in tensor_conversion_registry.get(type(value)):
-    # If dtype is None but preferred_dtype is not None, we try to
-    # cast to preferred_dtype first.
-    ret = None
-    if dtype is None and preferred_dtype is not None:
-      try:
-        ret = conversion_func(
-            value, dtype=preferred_dtype, name=name, as_ref=as_ref)
-      except (TypeError, ValueError):
-        # Could not coerce the conversion to use the preferred dtype.
-        pass
-      else:
-        if (ret is not NotImplemented and
-            ret.dtype.base_dtype != preferred_dtype.base_dtype):
-          raise RuntimeError(
-              _add_error_prefix(
-                  f"Conversion function {conversion_func!r} for type "
-                  f"{base_type} returned incompatible dtype: requested = "
-                  f"{preferred_dtype.base_dtype.name}, "
-                  f"actual = {ret.dtype.base_dtype.name}",
-                  name=name))
-
-    if ret is None:
-      ret = conversion_func(value, dtype=dtype, name=name, as_ref=as_ref)
-
-    if ret is NotImplemented:
-      continue
-
-    if not isinstance(ret, accepted_result_types):
-      raise RuntimeError(
-          _add_error_prefix(
-              f"Conversion function {conversion_func!r} for type "
-              f"{base_type} returned non-Tensor: {ret!r}",
-              name=name))
-    if dtype and not dtype.is_compatible_with(ret.dtype):
-      raise RuntimeError(
-          _add_error_prefix(
-              f"Conversion function {conversion_func} for type {base_type} "
-              f"returned incompatible dtype: requested = {dtype.name}, "
-              f"actual = {ret.dtype.name}",
-              name=name))
-    return ret
-  raise TypeError(
-      _add_error_prefix(
-          f"Cannot convert {value!r} with type {type(value)} to Tensor: "
-          f"no conversion function registered.",
-          name=name))
+  # TODO(b/142518781): Fix all call-sites and remove redundant arg
+  preferred_dtype = preferred_dtype or dtype_hint
+  return tensor_conversion_registry.convert(
+      value, dtype, name, as_ref, preferred_dtype, accepted_result_types)
 
 
 internal_convert_to_tensor = convert_to_tensor
