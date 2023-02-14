875ee391ad081daa9cd4129b6cee2b95e7341f1f
Fergus Henderson <fergus@google.com>
2023-02-14 09:27:24 -0800

    Add an ASSERT to the ReturnErrorWhenTimedOut test, to ensure that we fail the test cleanly rather than crashing with a SIGSEGV if the error field of the event is not set.
    PiperOrigin-RevId: 509552888

--- tensorflow/lite/experimental/acceleration/mini_benchmark/blocking_validator_runner_test.cc
+++ tensorflow/lite/experimental/acceleration/mini_benchmark/blocking_validator_runner_test.cc
@@ -187,6 +187,7 @@ TEST_F(BlockingValidatorRunnerTest, ReturnErrorWhenTimedOut) {
     const BenchmarkEvent* event =
         GetRoot<BenchmarkEvent>(result.GetBufferPointer());
     EXPECT_EQ(event->event_type(), BenchmarkEventType_ERROR);
+    ASSERT_NE(nullptr, event->error());
 #ifdef __ANDROID__
     EXPECT_EQ(event->error()->mini_benchmark_error_code(),
               kMinibenchmarkCommandTimedOut);
