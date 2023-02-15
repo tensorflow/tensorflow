e1405af5a4a4178279e1ca8a3e017a4b5d846856
Reid Wahl <nrwahl@protonmail.com>
2023-02-15 09:16:47 -0800

    Reorder includes to match style guide
    Found by clang-format
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- tensorflow/core/kernels/rnn/gru_ops.cc
+++ tensorflow/core/kernels/rnn/gru_ops.cc
@@ -16,8 +16,9 @@ limitations under the License.
 #define EIGEN_USE_THREADS
 
 #include "tensorflow/core/kernels/rnn/gru_ops.h"
-#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+
 #include "tensorflow/core/framework/op_kernel.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 
 namespace tensorflow {
 
