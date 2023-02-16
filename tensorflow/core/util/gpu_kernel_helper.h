764eae38919b45556373a320af1079ac54e70521
Antonio Sanchez <cantonios@google.com>
2023-02-15 16:14:29 -0800

    General fix for GPU kernels with empty inputs.
    We have received many crash reports of GPU kernels failing a check
    ```
    CHECK(work_element_count > 0);
    ```
    This occurs when the input to a GPU kernel is empty, so no work needs
    to actually be done.  Previously, we addressed this by adding special
    handling on an op-by-op basis.  This is a more general solution:
    if there is no work to do, simply return that we are done.
    
    PiperOrigin-RevId: 509960164

--- tensorflow/core/util/gpu_kernel_helper.h
+++ tensorflow/core/util/gpu_kernel_helper.h
@@ -104,21 +104,25 @@ Status GpuLaunchKernel(void (*function)(Ts...), dim3 grid_dim, dim3 block_dim,
                        Args... arguments) {
   static_assert(detail::NoneIsReference<Ts...>(),
                 "Kernels with reference arguments have undefined behaviour.");
+  if (grid_dim.x * grid_dim.y * grid_dim.z > 0 &&
+      block_dim.x * block_dim.y * block_dim.z > 0) {
 #if GOOGLE_CUDA
-  auto func_ptr = absl::bit_cast<const void*>(function);
-  // Cast arguments and forward them as an array of pointers.
-  auto args_tuple = std::tuple<Ts...>(arguments...);
-  auto arg_ptrs = detail::GetArrayOfElementPointers(&args_tuple);
-  auto result = cudaLaunchKernel(func_ptr, grid_dim, block_dim, arg_ptrs.data(),
-                                 shared_memory_size_bytes, stream);
-  if (result != cudaSuccess) {
-    return errors::Internal(cudaGetErrorString(result));
-  }
+    auto func_ptr = absl::bit_cast<const void*>(function);
+    // Cast arguments and forward them as an array of pointers.
+    auto args_tuple = std::tuple<Ts...>(arguments...);
+    auto arg_ptrs = detail::GetArrayOfElementPointers(&args_tuple);
+    auto result =
+        cudaLaunchKernel(func_ptr, grid_dim, block_dim, arg_ptrs.data(),
+                         shared_memory_size_bytes, stream);
+    if (result != cudaSuccess) {
+      return errors::Internal(cudaGetErrorString(result));
+    }
 #elif TENSORFLOW_USE_ROCM
-  hipLaunchKernelGGL(function, grid_dim, block_dim, shared_memory_size_bytes,
-                     stream, std::forward<Args>(arguments)...);
-  TF_RETURN_IF_CUDA_ERROR(hipGetLastError());
+    hipLaunchKernelGGL(function, grid_dim, block_dim, shared_memory_size_bytes,
+                       stream, std::forward<Args>(arguments)...);
+    TF_RETURN_IF_CUDA_ERROR(hipGetLastError());
 #endif
+  }
   return OkStatus();
 }
 
