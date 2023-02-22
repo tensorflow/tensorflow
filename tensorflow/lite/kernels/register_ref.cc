3eb716e0c8ffa139157d0eb2b4b6589fb22e5eea
A. Unique TensorFlower <gardener@tensorflow.org>
2023-02-22 12:02:39 -0800

    Adds a few extra Ops to the builtin reference registerer.
    PiperOrigin-RevId: 511559793

--- tensorflow/lite/kernels/register_ref.cc
+++ tensorflow/lite/kernels/register_ref.cc
@@ -177,6 +177,10 @@ TfLiteRegistration* Register_UNSORTED_SEGMENT_MIN();
 TfLiteRegistration* Register_UNSORTED_SEGMENT_SUM();
 TfLiteRegistration* Register_ATAN2();
 TfLiteRegistration* Register_SIGN();
+TfLiteRegistration* Register_CALL_ONCE();
+TfLiteRegistration* Register_VAR_HANDLE();
+TfLiteRegistration* Register_READ_VARIABLE();
+TfLiteRegistration* Register_ASSIGN_VARIABLE();
 
 namespace {
 
@@ -515,6 +519,11 @@ BuiltinRefOpResolver::BuiltinRefOpResolver() {
              Register_UNSORTED_SEGMENT_SUM());
   AddBuiltin(BuiltinOperator_ATAN2, Register_ATAN2());
   AddBuiltin(BuiltinOperator_SIGN, Register_SIGN());
+  AddBuiltin(BuiltinOperator_CALL_ONCE,
+             tflite::ops::builtin::Register_CALL_ONCE());
+  AddBuiltin(BuiltinOperator_VAR_HANDLE, Register_VAR_HANDLE());
+  AddBuiltin(BuiltinOperator_READ_VARIABLE, Register_READ_VARIABLE());
+  AddBuiltin(BuiltinOperator_ASSIGN_VARIABLE, Register_ASSIGN_VARIABLE());
   AddCustom("NumericVerify",
             tflite::ops::custom::Register_NUMERIC_VERIFY_REF());
   // TODO(andrewharp, ahentz): Move these somewhere more appropriate so that
