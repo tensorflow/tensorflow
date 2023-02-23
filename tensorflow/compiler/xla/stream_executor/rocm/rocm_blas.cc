9a5ab8c39d42e2df9b8e1a23094e68776e1afe2a
Chao Chen <cchen104@amd.com>
2023-02-23 13:26:38 +0000

    updated the latest rocm_blas.cc and rocm_helpers.cu.cc

--- tensorflow/compiler/xla/stream_executor/rocm/rocm_blas.cc
+++ tensorflow/compiler/xla/stream_executor/rocm/rocm_blas.cc
@@ -48,6 +48,9 @@ namespace gpu {
 
 PLUGIN_REGISTRY_DEFINE_PLUGIN_ID(kRocBlasPlugin);
 
+extern void rocm_Broadcast_fp32(void* stream, float* dst, int dst_stride,
+                                int batches, int src_batches, float* src, int size);
+
 template <class T>
 const typename RocBlasTypeConversionHelper<T>::mapped_type *complex_cast(
     const DeviceMemory<T> &a) {
@@ -550,53 +553,118 @@ bool ROCMBlas::GetBlasGemmAlgorithms(
   return true;
 }
 
+struct MemoryCopyOp {
+    char* src_ptr;
+    char* dst_ptr;
+    uint64_t size;
+    uint64_t count;
+    uint64_t dst_stride;
+    uint64_t src_count;
+};
+
+// Check whether two Memory Copy Ops can be fold together.
+// If it's true, fold it. Otherwise, return false.
+static bool MemCopyOpsFold(MemoryCopyOp& y, const MemoryCopyOp& x) {
+  bool misaligned = (x.size & 3)
+    || (reinterpret_cast<uint64_t>(x.dst_ptr) & 3)
+    || (reinterpret_cast<uint64_t>(x.src_ptr) & 3)
+    || (reinterpret_cast<uint64_t>(y.dst_ptr) & 3)
+    || (reinterpret_cast<uint64_t>(y.src_ptr) & 3);
+
+  int64_t dst_step = reinterpret_cast<int64_t>(x.dst_ptr) -
+                      reinterpret_cast<int64_t>(y.dst_ptr);
+
+  if (x.src_ptr == y.src_ptr && x.size == y.size
+      && (y.count == 1 || x.dst_ptr == y.dst_ptr + y.count * y.dst_stride)
+      && !misaligned && y.src_count == 1 && !(dst_step & 3)) {
+        if (y.count == 1){
+          y.dst_stride = dst_step;
+        }
+    y.count++;
+    return true;
+  }
+  else if (x.src_ptr == y.src_ptr + y.size &&
+           x.dst_ptr == y.dst_ptr + y.size &&
+           y.count == 1 && y.src_count == 1) {
+    y.size += x.size;
+    return true;
+  }
+  if (x.src_ptr == y.src_ptr + y.size * y.src_count &&
+      x.dst_ptr == y.dst_ptr + y.dst_stride * y.src_count * y.count &&
+      x.count == y.count && x.dst_stride == y.dst_stride){
+    y.src_count += x.src_count;
+    return true;
+  }
+  return false;
+}
+
+
 // This copies from source memory: raw_ptrs[i] to target memory:
 // device_memory_ptr at the interval of matrix_byte_size, or vice versa.
 // The below algorithm tries to minimize the number of memcpy by consolidating
-// neighboring memcpy into a single request
+// neighboring memcpy into a single request.
 template <typename MAPPED_T>
 tsl::Status ReorganizeMemory(Stream *stream,
                              DeviceMemory<MAPPED_T> *device_memory,
                              const std::vector<MAPPED_T *> &raw_ptrs,
                              int batch_count, uint64_t batch_stride,
                              bool gather) {
+
+  if (gather == false){
+    return tsl::Status(absl::StatusCode::kUnimplemented, "gather=false is unsupported");
+  }
+
   assert(batch_count > 0);
   char *device_memory_ptr = static_cast<char *>(device_memory->opaque());
   char *src_ptr = reinterpret_cast<char *>(raw_ptrs[0]);
   char *dst_ptr = device_memory_ptr;
   size_t matrix_byte_size = batch_stride * sizeof(MAPPED_T);
-  uint64_t cur_stride_size = matrix_byte_size;
+
+  std::vector<MemoryCopyOp> mem_copy_ops{MemoryCopyOp{src_ptr, dst_ptr, 
+                                                    matrix_byte_size, 1, 0, 1}};
 
   for (int i = 1; i < batch_count; ++i) {
-    if (reinterpret_cast<char *>(raw_ptrs[i]) == src_ptr + cur_stride_size) {
-      cur_stride_size += matrix_byte_size;
-    } else {
-      DeviceMemoryBase src_mem = DeviceMemoryBase(src_ptr, cur_stride_size);
-      DeviceMemoryBase target_mem = DeviceMemoryBase(dst_ptr, cur_stride_size);
-      bool a_status =
-          gather
-              ? stream->ThenMemcpy(&target_mem, src_mem, cur_stride_size).ok()
-              : stream->ThenMemcpy(&src_mem, target_mem, cur_stride_size).ok();
-      if (!a_status) {
+      src_ptr = reinterpret_cast<char*>(raw_ptrs[i]);
+      dst_ptr = device_memory_ptr + i * matrix_byte_size;
+
+      MemoryCopyOp x{src_ptr, dst_ptr, matrix_byte_size, 1, 0, 1};
+      while (mem_copy_ops.size() > 1 && 
+            MemCopyOpsFold(mem_copy_ops[mem_copy_ops.size() - 2], mem_copy_ops.back())){
+        mem_copy_ops.pop_back();
+      }
+      MemoryCopyOp& op = mem_copy_ops.back();
+      if (MemCopyOpsFold(op, x)) {
+        continue;
+      }
+      mem_copy_ops.push_back(x);
+  }
+
+  while (mem_copy_ops.size() > 1 && 
+        MemCopyOpsFold(mem_copy_ops[mem_copy_ops.size() - 2], mem_copy_ops.back())){          
+    mem_copy_ops.pop_back();
+  }
+
+  int i = 0;
+  for (auto& x : mem_copy_ops){
+    if (x.src_count > 1 || x.count > 1){
+      rocm_Broadcast_fp32(AsGpuStreamValue(stream),
+                          reinterpret_cast<float*>(x.dst_ptr),
+                          x.dst_stride >> 2, x.count, x.src_count,
+                          reinterpret_cast<float*>(x.src_ptr),
+                          x.size >> 2);
+    }
+    else{
+      DeviceMemoryBase src_mem = DeviceMemoryBase(x.src_ptr, x.size);
+      DeviceMemoryBase target_mem = DeviceMemoryBase(x.dst_ptr, x.size);
+      bool a_status = stream->ThenMemcpy(&target_mem, src_mem, x.size).ok();
+      if (!a_status){
         return tsl::Status(
             absl::StatusCode::kInternal,
             "failed to copy device memory in ROCMBlas::DoBlasGemmBatched");
-      }
-      src_ptr = reinterpret_cast<char *>(raw_ptrs[i]);
-      dst_ptr = device_memory_ptr + i * matrix_byte_size;
-      cur_stride_size = matrix_byte_size;
+        }
     }
+    i++;
   }
-
-  DeviceMemoryBase src_mem = DeviceMemoryBase(src_ptr, cur_stride_size);
-  DeviceMemoryBase target_mem = DeviceMemoryBase(dst_ptr, cur_stride_size);
-  bool a_status =
-      gather ? stream->ThenMemcpy(&target_mem, src_mem, cur_stride_size).ok()
-             : stream->ThenMemcpy(&src_mem, target_mem, cur_stride_size).ok();
-  if (!a_status)
-    return tsl::Status(
-        absl::StatusCode::kInternal,
-        "failed to copy device memory in ROCMBlas::DoBlasGemmBatched");
   return tsl::OkStatus();
 }
 
--- tensorflow/compiler/xla/stream_executor/rocm/rocm_helpers.cu.cc
+++ tensorflow/compiler/xla/stream_executor/rocm/rocm_helpers.cu.cc
@@ -19,11 +19,42 @@ limitations under the License.
 namespace stream_executor {
 namespace gpu {
 
+__global__ void rocm_Broadcast_fp32Kernel(float* dst, int dst_stride, int batches,
+                                         float* src, int size) {
+  dst += blockIdx.y * 4 * dst_stride + blockIdx.z * dst_stride * batches;
+  src += blockIdx.z * size;
+  float* dst2 = dst + dst_stride;
+  float* dst3 = dst + dst_stride*2;
+  float* dst4 = dst + dst_stride*3;
+  bool b2 = (blockIdx.y * 4 + 1 < batches);
+  bool b3 = (blockIdx.y * 4 + 2 < batches);
+  bool b4 = (blockIdx.y * 4 + 3 < batches);
+  for (int i=threadIdx.x + blockIdx.x * 256; i<size; i+=blockDim.x * gridDim.x){
+  	dst[i] = src[i];
+  	if (b2){
+  		dst2[i] = src[i];
+    }
+  	if (b3){
+  		dst3[i] = src[i];
+    }
+  	if (b4){
+  		dst4[i] = src[i];
+    }
+  }
+}
+
+void rocm_Broadcast_fp32(void* stream, float* dst, int dst_stride, int batches,
+                        int src_batches, float* src, int size) {
+  int x_blocks = (size + 255) / 256;
+  hipLaunchKernelGGL(rocm_Broadcast_fp32Kernel, dim3(x_blocks, (batches+3)/4, src_batches), 
+                     min(256, (int)size), 0, (hipStream_t)stream, dst, dst_stride, 
+                     batches, src, size);
+}
+
 // GPU kernel to populate an array of pointers:
 //
 //   [base + stride * i for i in range(n)].
 //
-
 __global__ void __xla_MakeBatchPointers(char* base, int stride, int n,
                                         void** ptrs_out) {
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
