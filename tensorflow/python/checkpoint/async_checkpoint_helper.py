007ef257793b6ea0464361d2fc6a5a5b57403ce5
A. Unique TensorFlower <gardener@tensorflow.org>
2023-02-23 11:43:59 -0800

    Initialize AsyncCheckpoint's underlying save_counter.
    PiperOrigin-RevId: 511844739

--- tensorflow/python/checkpoint/async_checkpoint_helper.py
+++ tensorflow/python/checkpoint/async_checkpoint_helper.py
@@ -226,6 +226,10 @@ class AsyncCheckpointHelper:
 
     # Initiate the underlying Checkpoint instance with the copied items.
     self._checkpoint = self._checkpointer_impl(**self._checkpoint_items)
+    # Initiate the underlying Checkpoint instance's save_counter.
+    save_counter = self._checkpoint.save_counter
+    logging.info("Initializing async checkpoint's save_counter: %d",
+                 save_counter)
 
     # Pass the object map of the copied variables to the underlying Checkpoint.
     self._checkpoint._saver._object_map = self._object_map  # pylint: disable=protected-access
@@ -387,6 +391,10 @@ class AsyncCheckpointHelper:
     Returns:
       The save counter variable.
     """
+    # TODO(sagunb): Improve the solution for initializing save_counter.
+    # If save_counter() is called before all the variables are created,
+    # self._ensure_initialized() would construct the object_map without some
+    # variables that need to be checkpointed, e.g., slot variables.
     self._ensure_initialized()
     return self._checkpoint.save_counter
 
