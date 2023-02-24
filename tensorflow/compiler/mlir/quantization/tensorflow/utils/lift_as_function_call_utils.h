ad620052e295cda59516a56c63e20dfdb845ef88
DongHyun Choi <dhchoi@google.com>
2023-02-23 18:26:19 -0800

    Einsum --> XlaDotV2 conversion condition.
    PiperOrigin-RevId: 511939306

--- tensorflow/compiler/mlir/quantization/tensorflow/utils/lift_as_function_call_utils.h
+++ tensorflow/compiler/mlir/quantization/tensorflow/utils/lift_as_function_call_utils.h
@@ -36,6 +36,9 @@ inline constexpr absl::string_view kNullAttributeValue = "N/A";
 // Checks if the op is inside a lifted function.
 bool IsInLiftedFunc(Operation *op);
 
+// Checks if the given einsum op is supported for XlaDotV2 quantization.
+bool IsEinsumSupportedByXlaDotV2(mlir::StringAttr equation_attr);
+
 // Creates a function to wrap the section between arguments and results.
 llvm::SmallVector<Value, 4> LiftAsFunctionCall(
     OpBuilder builder, Location location, StringRef func_name,
