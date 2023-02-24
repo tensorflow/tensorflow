8ab1059e469ca955a157fcbab55c0fb3586e5ee5
Anlun Xu <anlunx@google.com>
2023-02-24 11:01:43 -0800

    [xla:gpu] Pass NonAtomicallyUpgradeableRWLock to graph launch custom call
    The upgradeable lock is required by the GEMM custom call. So we should should pass the lock to the graph launch custom call, which forwards the lock to GEMM.
    
    PiperOrigin-RevId: 512113329

--- tensorflow/compiler/xla/service/gpu/runtime/graph_launch.cc
+++ tensorflow/compiler/xla/service/gpu/runtime/graph_launch.cc
@@ -25,6 +25,7 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/runtime/custom_call.h"
 #include "tensorflow/compiler/xla/runtime/executable.h"
+#include "tensorflow/compiler/xla/service/gpu/non_atomically_upgradeable_rw_lock.h"
 #include "tensorflow/compiler/xla/service/gpu/runtime/conv.h"
 #include "tensorflow/compiler/xla/service/gpu/runtime/gemm.h"
 #include "tensorflow/compiler/xla/service/gpu/runtime/kernel_launch.h"
@@ -182,6 +183,7 @@ static absl::Status LaunchGraph(
     StreamExecutorConvRunners::Snapshot* convs,
     StreamExecutorGraphInstances::Snapshot* instances,
     GemmConfigs::Snapshot* gemm_config, runtime::Executable* executable,
+    NonAtomicallyUpgradeableRWLock* gpu_lock,
     CustomCall::RemainingArgs fwd_args, CustomCall::FunctionOrdinal capture) {
 #if GOOGLE_CUDA
   VLOG(1) << "Launch Cuda Graph: capture=" << capture.ordinal;
@@ -196,7 +198,7 @@ static absl::Status LaunchGraph(
   auto user_data = [&] {
     return CustomCall::UserData(run_options, debug_options, ptx, cubin,
                                 temp_buffer, kernels, convs, executable,
-                                gemm_config);
+                                gemm_config, gpu_lock);
   };
 
   absl::StatusOr<GraphInstance*> instance = instances->GetOrCreate(
@@ -259,6 +261,7 @@ XLA_RUNTIME_DEFINE_CUSTOM_CALL(
         .UserData<StreamExecutorGraphInstances::Snapshot*>()
         .UserData<GemmConfigs::Snapshot*>()
         .UserData<Executable*>()
+        .UserData<NonAtomicallyUpgradeableRWLock*>()
         .RemainingArgs()
         .Attr<CustomCall::FunctionOrdinal>("capture"));
 
