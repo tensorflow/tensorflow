a5277b8fd640dfb3a4d3832eacd3e00e58a91250
Jason Furmanek <Jason.Furmanek@amd.com>
2023-02-27 20:38:18 +0000

    [ROCM] Fix xla_call_module_test

--- tensorflow/compiler/tests/xla_call_module_test.py
+++ tensorflow/compiler/tests/xla_call_module_test.py
@@ -29,6 +29,7 @@ from tensorflow.python.framework import errors
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.platform import googletest
+from tensorflow.python.platform import test
 
 
 def serialize(module_str: str) -> Tuple[str, int]:
@@ -63,7 +64,10 @@ class XlaCallModuleOpTest(xla_test.XLATestCase):
     if self.device in ['CPU', 'XLA_CPU']:
       return 'CPU'
     elif self.device in ['GPU', 'XLA_GPU']:
-      return 'CUDA'
+      if test.is_built_with_rocm():
+        return 'ROCM'
+      else:
+        return 'CUDA'
     elif self.device in ['TPU', 'XLA_TPU']:
       return 'TPU'
     else:
@@ -328,7 +332,7 @@ module @jit_f.0 {
   def test_platforms_basic(self):
     x = np.float32(0.)
 
-    #  returns x + 2. on CPU, x + 3. on GPU and x + 4. on TPU
+    #  returns x + 2. on CPU, x + 3. on GPU (CUDA or ROCM) and x + 4. on TPU
     module, version = serialize("""
 module @jit_f.0 {
   func.func public @main(%arg_platform_idx: tensor<i32>, %arg0: tensor<f32>) -> tensor<f32> {
@@ -348,7 +352,7 @@ module @jit_f.0 {
 }
 """)
 
-    platforms = ['CPU', 'CUDA', 'TPU']
+    platforms = ['CPU', 'CUDA', 'ROCM', 'TPU']
     def f(x):
       return xla.call_module([x], version=version,
                              module=module,
@@ -356,7 +360,10 @@ module @jit_f.0 {
                              Sout=[()],
                              platforms=platforms)
 
-    expected_value = x + dict(CPU=2., CUDA=3., TPU=4.)[self.testing_platform()]
+    expected_value = x + dict(CPU=2., 
+                              CUDA=3., 
+                              ROCM=3.,
+                              TPU=4.)[self.testing_platform()]
     self._assertOpOutputMatchesExpected(f, (x,), (expected_value,))
 
   def test_platforms_with_dim_vars(self):
@@ -434,7 +441,7 @@ module @jit_f.0 {
         'The current platform .* is not among the platforms'):
       self._assertOpOutputMatchesExpected(f, (x,), (x,))
 
-    platforms = ['CPU', 'CUDA']
+    platforms = ['CPU', 'CUDA', 'ROCM']
     if self.testing_platform() not in platforms:
       with self.assertRaisesRegex(
           errors.NotFoundError,
@@ -445,7 +452,6 @@ module @jit_f.0 {
 
     # The module cannot have i64 %arg_platform_idx
     module, version = serialize(module_str.replace('i32', 'i64'))
-    platforms = ['CPU', 'CUDA', 'TPU']
     with self.assertRaisesRegex(
         errors.InvalidArgumentError,
         'Module argument at index 0 should be a 0-dimensional '
