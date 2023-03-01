f1a3f964c324811e3f9adec765d5532ed5b00966
A. Unique TensorFlower <gardener@tensorflow.org>
2023-02-28 16:59:38 -0800

    Move //third_party/tensorflow/compiler/xla/service:hlo_reachability to //third_party/tensorflow/compiler/xla/hlo/ir:hlo_reachability and update all users.
    PiperOrigin-RevId: 513078682

--- tensorflow/compiler/xla/hlo/ir/hlo_reachability.h
+++ tensorflow/compiler/xla/hlo/ir/hlo_reachability.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_COMPILER_XLA_SERVICE_HLO_REACHABILITY_H_
-#define TENSORFLOW_COMPILER_XLA_SERVICE_HLO_REACHABILITY_H_
+#ifndef TENSORFLOW_COMPILER_XLA_HLO_IR_HLO_REACHABILITY_H_
+#define TENSORFLOW_COMPILER_XLA_HLO_IR_HLO_REACHABILITY_H_
 
 #include <memory>
 #include <utility>
@@ -208,4 +208,4 @@ class HloReachabilityMap {
 
 }  // namespace xla
 
-#endif  // TENSORFLOW_COMPILER_XLA_SERVICE_HLO_REACHABILITY_H_
+#endif  // TENSORFLOW_COMPILER_XLA_HLO_IR_HLO_REACHABILITY_H_
