f6f2b14213d1f525702d354f49e6fc6928f1a648
Tristen Allen <tristenallen@google.com>
2023-03-07 13:36:48 -0800

    Relocate `_DefaultStack` to a new module.
    Removes the `_DefaultStack` class from `ops.py`
    and renames it to `DefaultStack`. Additionally relocates
    the `default_session()` and `get_default_session()` methods
    to this new module.
    
    These methods are widely used outside of `ops` and in
    some cases are a module's only dependency on `ops`.
    It is also necessary to move these methods in preparation
    for further refactors of `ops.py`.
    
    To avoid immediate disruptive changes in the wider TensorFlow
    codebase, `get_default_session` is aliased in `ops.py`.
    
    PiperOrigin-RevId: 514825999

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -52,6 +52,7 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
 from tensorflow.python.framework import op_callbacks
 from tensorflow.python.framework import registry
+from tensorflow.python.framework import stack
 from tensorflow.python.framework import tensor_conversion_registry
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_util
@@ -5568,118 +5569,8 @@ def control_dependencies(control_inputs):
   else:
     return get_default_graph().control_dependencies(control_inputs)
 
-
-class _DefaultStack(threading.local):
-  """A thread-local stack of objects for providing implicit defaults."""
-
-  def __init__(self):
-    super(_DefaultStack, self).__init__()
-    self._enforce_nesting = True
-    self.stack = []
-
-  def get_default(self):
-    return self.stack[-1] if self.stack else None
-
-  def reset(self):
-    self.stack = []
-
-  def is_cleared(self):
-    return not self.stack
-
-  @property
-  def enforce_nesting(self):
-    return self._enforce_nesting
-
-  @enforce_nesting.setter
-  def enforce_nesting(self, value):
-    self._enforce_nesting = value
-
-  @tf_contextlib.contextmanager
-  def get_controller(self, default):
-    """A context manager for manipulating a default stack."""
-    self.stack.append(default)
-    try:
-      yield default
-    finally:
-      # stack may be empty if reset() was called
-      if self.stack:
-        if self._enforce_nesting:
-          if self.stack[-1] is not default:
-            raise AssertionError(
-                "Nesting violated for default stack of %s objects" %
-                type(default))
-          self.stack.pop()
-        else:
-          self.stack.remove(default)
-
-
-_default_session_stack = _DefaultStack()  # pylint: disable=protected-access
-
-
-def default_session(session):
-  """Python "with" handler for defining a default session.
-
-  This function provides a means of registering a session for handling
-  Tensor.eval() and Operation.run() calls. It is primarily intended for use
-  by session.Session, but can be used with any object that implements
-  the Session.run() interface.
-
-  Use with the "with" keyword to specify that Tensor.eval() and Operation.run()
-  invocations within the scope of a block should be executed by a particular
-  session.
-
-  The default session applies to the current thread only, so it is always
-  possible to inspect the call stack and determine the scope of a default
-  session. If you create a new thread, and wish to use the default session
-  in that thread, you must explicitly add a "with ops.default_session(sess):"
-  block in that thread's function.
-
-  Example:
-    The following code examples are equivalent:
-
-    # 1. Using the Session object directly:
-    sess = ...
-    c = tf.constant(5.0)
-    sess.run(c)
-
-    # 2. Using default_session():
-    sess = ...
-    with ops.default_session(sess):
-      c = tf.constant(5.0)
-      result = c.eval()
-
-    # 3. Overriding default_session():
-    sess = ...
-    with ops.default_session(sess):
-      c = tf.constant(5.0)
-      with ops.default_session(...):
-        c.eval(session=sess)
-
-  Args:
-    session: The session to be installed as the default session.
-
-  Returns:
-    A context manager for the default session.
-  """
-  return _default_session_stack.get_controller(session)
-
-
-@tf_export(v1=["get_default_session"])
-def get_default_session():
-  """Returns the default session for the current thread.
-
-  The returned `Session` will be the innermost session on which a
-  `Session` or `Session.as_default()` context has been entered.
-
-  NOTE: The default session is a property of the current thread. If you
-  create a new thread, and wish to use the default session in that
-  thread, you must explicitly add a `with sess.as_default():` in that
-  thread's function.
-
-  Returns:
-    The default `Session` being used in the current thread.
-  """
-  return _default_session_stack.get_default()
+# Forwarded from `stack.py` for backwards compatibility.
+get_default_session = stack.get_default_session
 
 
 def _eval_using_default_session(tensors, feed_dict, graph, session=None):
@@ -5703,7 +5594,7 @@ def _eval_using_default_session(tensors, feed_dict, graph, session=None):
       and it does not have "graph" as its graph.
   """
   if session is None:
-    session = get_default_session()
+    session = stack.get_default_session()
     if session is None:
       raise ValueError("Cannot evaluate tensor using `eval()`: No default "
                        "session is registered. Use `with "
@@ -5738,7 +5629,7 @@ def _run_using_default_session(operation, feed_dict, graph, session=None):
       and it does not have "graph" as its graph.
   """
   if session is None:
-    session = get_default_session()
+    session = stack.get_default_session()
     if session is None:
       raise ValueError("Cannot execute operation using `run()`: No default "
                        "session is registered. Use `with "
@@ -5757,7 +5648,7 @@ def _run_using_default_session(operation, feed_dict, graph, session=None):
   session.run(operation, feed_dict)
 
 
-class _DefaultGraphStack(_DefaultStack):  # pylint: disable=protected-access
+class _DefaultGraphStack(stack.DefaultStack):  # pylint: disable=protected-access
   """A thread-local stack of objects for providing an implicit default graph."""
 
   def __init__(self):
