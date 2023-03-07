9776a94ac5aa2db6c738412d8fc72dc881ff068e
Reed Wanderman-Milne <reedwm@google.com>
2023-03-07 13:04:14 -0800

    Fix converting FP8 to pred.
    The HLO->MHLO conversion turns "convert(x, pred)" into "x != 0", so this change adds support for FP8 comparisons in the emitter, despite the fact that normally FloatNormalization would turn such comparisons into FP16 comparisons.
    
    PiperOrigin-RevId: 514817034

--- tensorflow/compiler/tests/unary_ops_test.py
+++ tensorflow/compiler/tests/unary_ops_test.py
@@ -880,11 +880,8 @@ class UnaryOpsTest(xla_test.XLATestCase):
       # NaN on Mac
       self.skipTest("Casts to FP8 sometimes result in NaN on Mac")
     fp8_types = {dtypes.float8_e5m2, dtypes.float8_e4m3fn}
-    # TODO(b/259609697): Test casting to bool. Casting from float8 to bool is
-    # currently not supported since the cast is lowered to an Ne (not-equal) op,
-    # and FP8 is currently not supported with Ne.
     other_types = {
-        dtypes.float32, dtypes.float64, dtypes.complex64,
+        dtypes.bool, dtypes.float32, dtypes.float64, dtypes.complex64,
         dtypes.int32, dtypes.int64, dtypes.uint32, dtypes.uint64
     }
     for fp8_type in fp8_types:
