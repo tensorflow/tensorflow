34b1efd1af5d45bc08bcd79999068b915a071c1e
Pankaj Kanwar <pkanwar@google.com>
2023-03-09 20:07:55 -0800

    rollback to fix build
    PiperOrigin-RevId: 515512389

--- tensorflow/python/eager/core.py
+++ tensorflow/python/eager/core.py
@@ -16,7 +16,6 @@
 
 from tensorflow.python import pywrap_tfe
 from tensorflow.python.framework import errors
-from tensorflow.python.platform import tf_logging as logging
 
 # Trace of execution and memory usage.
 _active_trace = None
@@ -25,15 +24,10 @@ _active_trace = None
 def _status_to_exception(status):
   try:
     error_class = errors.exception_type_from_error_code(status.code)
-    e = error_class(None, None, status.message, status.payloads)
-    logging.error_log("%s: %s" % (e.__class__.__name__, e))
-    return e
+    return error_class(None, None, status.message, status.payloads)
   except KeyError:
-    e = errors.UnknownError(
-        None, None, status.message, status.code, status.payloads
-    )
-    logging.error_log("%s: %s" % (e.__class__.__name__, e))
-    return e
+    return errors.UnknownError(None, None, status.message, status.code,
+                               status.payloads)
 
 
 class _NotOkStatusException(Exception):
