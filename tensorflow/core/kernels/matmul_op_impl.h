429fc60471d64fd6a571c20b59a65157d71982c0
Jean-Baptiste Lespiau <jblespiau@google.com>
2023-03-13 10:34:12 -0700

    Replace usage of the tsl::Status constructor with a tsl::{error, errors}::Code.
    PiperOrigin-RevId: 516252666

--- tensorflow/core/kernels/matmul_op_impl.h
+++ tensorflow/core/kernels/matmul_op_impl.h
@@ -319,7 +319,7 @@ class BlasScratchAllocator : public se::ScratchAllocator {
 
     if (memory_limit_ > 0 && byte_size > memory_limit_) {
       return tsl::Status{
-          tsl::error::UNAVAILABLE,
+          absl::StatusCode::kUnavailable,
           absl::StrCat("Requested memory size (", byte_size,
                        ") exceeds the memory limit (", memory_limit_, ").")};
     }
@@ -329,7 +329,7 @@ class BlasScratchAllocator : public se::ScratchAllocator {
         DT_UINT8, TensorShape({byte_size}), &temporary_memory));
     if (!allocation_status.ok()) {
       return tsl::Status{
-          tsl::error::UNAVAILABLE,
+          absl::StatusCode::kUnavailable,
           absl::StrCat("Failed to allocate requested memory of (", byte_size,
                        ").")};
     }
