e610fcb716ceb1eec100c5e5ee5eebf6ab610743
Marissa Ikonomidis <marissaw@google.com>
2023-03-13 07:59:47 -0700

    Add support for rewriting a model's batching options during load
    This cl uses a preexisting grappler pass that can rewrite the batching
    op. It expands the rewrite support to allow rewriting batching options.
    Users can provide the grappler proto via the servo_tf_graph_rewrite_options
    flag.
    
    PiperOrigin-RevId: 516210724

--- tensorflow/core/grappler/optimizers/inference/batch_op_rewriter_test.cc
+++ tensorflow/core/grappler/optimizers/inference/batch_op_rewriter_test.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 #include "tensorflow/core/grappler/optimizers/inference/batch_op_rewriter.h"
 
+#include <vector>
+
 #include "google/protobuf/wrappers.pb.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/strings/escaping.h"
@@ -26,6 +28,7 @@ limitations under the License.
 #include "tensorflow/core/grappler/optimizers/graph_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/inference/batch_op_rewriter.pb.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
+#include "tensorflow/core/platform/errors.h"
 #include "tensorflow/core/protobuf/rewriter_config.pb.h"
 #include "tensorflow/tools/graph_transforms/transform_utils.h"
 
@@ -42,21 +45,28 @@ using ::tensorflow::grappler::GrapplerItem;
 using ::tensorflow::serving::BatchOpRewriteConfig;
 
 // Add batch op in both GraphDef.node and GraphDef.library.function.node_def.
-void AddBatchOp(
-    GraphDef* graph, int num_batch_threads,
-    const absl::flat_hash_map<string, int>& reserved_int_attrs = {}) {
+void AddBatchOp(GraphDef* graph, int num_batch_threads = 16,
+                const absl::flat_hash_map<string, int>& reserved_int_attrs = {},
+                int max_batch_size = 16, int batch_timeout_micros = 10000,
+                const std::vector<int32>& allowed_batch_sizes = {8, 16},
+                int max_enqueued_batches = 1000,
+                bool disable_large_batch_splitting = false) {
   auto set_batch_node_attribute = [&](const int32_t num_batch_threads,
                                       NodeDef* batch_op) {
     batch_op->set_name("cond/batch/BatchFunction");
     batch_op->set_op("BatchFunction");
     ::tensorflow::graph_transforms::SetNodeAttr("num_batch_threads",
                                                 num_batch_threads, batch_op);
-    ::tensorflow::graph_transforms::SetNodeAttr("max_batch_size", 16, batch_op);
-    ::tensorflow::graph_transforms::SetNodeAttr("batch_timeout_micros", 10000,
-                                                batch_op);
-    ::tensorflow::graph_transforms::SetNodeAttr(
-        "allowed_batch_sizes", std::vector<int32>{8, 16}, batch_op);
-    ::tensorflow::graph_transforms::SetNodeAttr("max_enqueued_batches", 1000,
+    ::tensorflow::graph_transforms::SetNodeAttr("max_batch_size",
+                                                max_batch_size, batch_op);
+    ::tensorflow::graph_transforms::SetNodeAttr("batch_timeout_micros",
+                                                batch_timeout_micros, batch_op);
+    ::tensorflow::graph_transforms::SetNodeAttr("allowed_batch_sizes",
+                                                allowed_batch_sizes, batch_op);
+    ::tensorflow::graph_transforms::SetNodeAttr("max_enqueued_batches",
+                                                max_enqueued_batches, batch_op);
+    ::tensorflow::graph_transforms::SetNodeAttr("enable_large_batch_splitting",
+                                                !disable_large_batch_splitting,
                                                 batch_op);
 
     if (!reserved_int_attrs.empty()) {
@@ -153,17 +163,21 @@ TEST_P(BatchOpRewriterTest, AdaptiveBatchScheduler) {
 
   // PARSE_TEXT_PROTO isn't available in TF OSS.
   config.set_enable_adaptive_shared_batching_thread_pool(GetParam());
-  (*config.mutable_model_scheduler_options())["model_with_override"]
-      .mutable_batches_to_average_over()
+  (*config.mutable_batch_options())["model_with_override"]
+      .mutable_adaptive_batch_scheduler_option()
+      ->mutable_batches_to_average_over()
       ->set_value(1000);
-  (*config.mutable_model_scheduler_options())["model_with_override"]
-      .mutable_initial_inflight_batches_limit()
+  (*config.mutable_batch_options())["model_with_override"]
+      .mutable_adaptive_batch_scheduler_option()
+      ->mutable_initial_inflight_batches_limit()
       ->set_value(16);
-  (*config.mutable_model_scheduler_options())["model_with_override"]
-      .mutable_min_inflight_batches_limit()
+  (*config.mutable_batch_options())["model_with_override"]
+      .mutable_adaptive_batch_scheduler_option()
+      ->mutable_min_inflight_batches_limit()
       ->set_value(8);
-  (*config.mutable_model_scheduler_options())["model_with_override"]
-      .mutable_max_inflight_batches_limit()
+  (*config.mutable_batch_options())["model_with_override"]
+      .mutable_adaptive_batch_scheduler_option()
+      ->mutable_max_inflight_batches_limit()
       ->set_value(32);
 
   RewriterConfig_CustomGraphOptimizer rewriter_config = MakeConfig(config);
@@ -195,6 +209,108 @@ TEST_P(BatchOpRewriterTest, AdaptiveBatchScheduler) {
              });
 
   EXPECT_EQ(optimized_graph.DebugString(), expected_graph.DebugString());
+}
+
+// Tests that using the deprecated model_scheduler_options returns an error.
+TEST_F(BatchOpRewriterTest, UpdateModelSchedulerOptions) {
+  BatchOpRewriteConfig config;
+
+  // PARSE_TEXT_PROTO isn't available in TF OSS.
+  config.set_enable_adaptive_shared_batching_thread_pool(true);
+  (*config.mutable_model_scheduler_options())["model_with_override"]
+      .mutable_batches_to_average_over()
+      ->set_value(1000);
+  (*config.mutable_model_scheduler_options())["model_with_override"]
+      .mutable_initial_inflight_batches_limit()
+      ->set_value(16);
+  (*config.mutable_model_scheduler_options())["model_with_override"]
+      .mutable_min_inflight_batches_limit()
+      ->set_value(8);
+  (*config.mutable_model_scheduler_options())["model_with_override"]
+      .mutable_max_inflight_batches_limit()
+      ->set_value(32);
+
+  RewriterConfig_CustomGraphOptimizer rewriter_config = MakeConfig(config);
+  ConfigProto config_proto;
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_version(
+      123);
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_name(
+      "model_with_override");
+  BatchOpRewriter optimizer;
+  TF_ASSERT_OK(optimizer.InitWithConfig(config_proto, &rewriter_config));
+  GraphDef optimized_graph;
+  GrapplerItem item;
+  AddBatchOp(&item.graph, 16);
+  ASSERT_FALSE(optimizer.Optimize(nullptr, item, &optimized_graph).ok());
+}
+
+// Tests that the updated batch options are present in the output GraphDef.
+TEST_F(BatchOpRewriterTest, UpdateBatchOptions) {
+  BatchOpRewriteConfig config;
+
+  // PARSE_TEXT_PROTO isn't available in TF OSS.
+  (*config.mutable_batch_options())["model_with_override"]
+      .set_num_batch_threads(2);
+  (*config.mutable_batch_options())["model_with_override"].set_max_batch_size(
+      128);
+  (*config.mutable_batch_options())["model_with_override"]
+      .set_batch_timeout_micros(5000);
+  const std::vector<int32> allowed_batch_sizes{4, 32};
+  (*config.mutable_batch_options())["model_with_override"]
+      .mutable_allowed_batch_sizes()
+      ->Add(allowed_batch_sizes.begin(), allowed_batch_sizes.end());
+  (*config.mutable_batch_options())["model_with_override"]
+      .set_max_enqueued_batches(500);
+  (*config.mutable_batch_options())["model_with_override"]
+      .set_disable_large_batch_splitting(true);
+
+  RewriterConfig_CustomGraphOptimizer rewriter_config = MakeConfig(config);
+  ConfigProto config_proto;
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_version(
+      123);
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_name(
+      "model_with_override");
+  BatchOpRewriter optimizer;
+  TF_ASSERT_OK(optimizer.InitWithConfig(config_proto, &rewriter_config));
+
+  GraphDef optimized_graph;
+  GrapplerItem item;
+  AddBatchOp(&item.graph);
+  TF_ASSERT_OK(optimizer.Optimize(nullptr, item, &optimized_graph));
+  // We can't use the testing::EqualsProto matcher because it is not available
+  // in OSS.
+  GraphDef expected_graph;
+  // For `model_with_override`, attribute `num_batch_threads` is 16 and not
+  // overridden to zero regardless of
+  // `enable_adaptive_shared_batching_thread_pool`, since `model_with_override`
+  // override its scheduler options in `model_scheduler_options`.
+  AddBatchOp(&expected_graph, 2 /* num_batch_threads */,
+             {} /* reserved_int_attrs */, 128 /* max_batch_size */,
+             5000 /* batch_timeout_micros */, allowed_batch_sizes,
+             500 /* max_enqueued_batches */,
+             true /* disable_large_batch_splitting */);
+
+  EXPECT_EQ(optimized_graph.DebugString(), expected_graph.DebugString());
+}
+
+TEST_F(BatchOpRewriterTest,
+       UpdateAdaptiveSharedBatchSchedulerAndNumBatchThreads) {
+  GrapplerItem item;
+  AddBatchOp(&item.graph, 16);
+  BatchOpRewriteConfig config;
+  config.set_enable_adaptive_shared_batching_thread_pool(true);
+  (*config.mutable_batch_options())["model_with_override"]
+      .set_num_batch_threads(2);
+  RewriterConfig_CustomGraphOptimizer rewriter_config = MakeConfig(config);
+  ConfigProto config_proto;
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_version(
+      123);
+  config_proto.mutable_experimental()->mutable_session_metadata()->set_name(
+      "model_with_override");
+  BatchOpRewriter optimizer;
+  TF_ASSERT_OK(optimizer.InitWithConfig(config_proto, &rewriter_config));
+  GraphDef optimized_graph;
+  ASSERT_FALSE(optimizer.Optimize(nullptr, item, &optimized_graph).ok());
 }
 
 }  // namespace
