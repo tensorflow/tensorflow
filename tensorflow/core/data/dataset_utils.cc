42f9caf82e84024ee438119afe1a0f6516d9bfbb
Matt Callanan <mpcallanan@google.com>
2023-03-15 16:38:48 -0700

    Ramp up "data_transfer" experiment to 3%.
    PiperOrigin-RevId: 516963218

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -966,7 +966,7 @@ REGISTER_DATASET_EXPERIMENT("stage_based_autotune",
 REGISTER_DATASET_EXPERIMENT("stage_based_autotune_v2",
                             RandomJobSamplePercentage<50>,
                             IndependentHostTasks);
-REGISTER_DATASET_EXPERIMENT("data_transfer", RandomJobSamplePercentage<1>,
+REGISTER_DATASET_EXPERIMENT("data_transfer", RandomJobSamplePercentage<3>,
                             IndependentHostTasks);
 }  // namespace
 }  // namespace data
