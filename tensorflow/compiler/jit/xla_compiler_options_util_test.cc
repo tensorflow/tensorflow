07fe373621ade9e6a82c225b4afa63f061847281
Swachhand Lokhande <swachhand@google.com>
2023-03-15 14:54:34 -0700

    Do a minor refactor of BuildXlaDeviceCompiler() and add unit tests.
    PiperOrigin-RevId: 516937505

--- tensorflow/compiler/jit/xla_compiler_options_util_test.cc
+++ tensorflow/compiler/jit/xla_compiler_options_util_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include <gtest/gtest.h>
 #include "tensorflow/compiler/jit/flags.h"
+#include "tensorflow/compiler/jit/test_util.h"
 #include "tensorflow/compiler/tf2xla/xla_compiler.h"
 #include "tensorflow/compiler/tf2xla/xla_helpers.h"
 #include "tensorflow/compiler/tf2xla/xla_op_registry.h"
@@ -77,48 +78,12 @@ class XlaCompilerOptionsTest : public ::testing::Test {
     tensorflow::GetXlaDeviceFlags()->tf_xla_enable_xla_devices = true;
   }
 
-  void AddDevicesAndSetUp(const std::vector<std::string>& device_names) {
-    SessionOptions options;
-    auto* device_count = options.config.mutable_device_count();
-    for (const auto& device_name : device_names) {
-      device_count->insert({device_name, 1});
-    }
-
-    std::vector<std::unique_ptr<Device>> devices;
-    TF_CHECK_OK(DeviceFactory::AddDevices(
-        options, "/job:localhost/replica:0/task:0", &devices));
-    device_mgr_ = std::make_unique<StaticDeviceMgr>(std::move(devices));
-
-    OptimizerOptions opts;
-    lib_def_ = std::make_unique<FunctionLibraryDefinition>(
-        OpRegistry::Global(), FunctionDefLibrary());
-    pflr_ = std::make_unique<ProcessFunctionLibraryRuntime>(
-        device_mgr_.get(), Env::Default(), /*config=*/nullptr,
-        TF_GRAPH_DEF_VERSION, lib_def_.get(), opts,
-        /*default_thread_pool=*/nullptr, /*cluster_flr=*/nullptr);
-    flr_ = pflr_->GetFLR("/job:localhost/replica:0/task:0/cpu:0");
-  }
-
-  Device* GetXlaGpuDevice() {
-    if (device_mgr_ == nullptr) {
-      return nullptr;
-    }
-
-    Device* device;
-    TF_CHECK_OK(device_mgr_->LookupDevice(
-        "/job:localhost/replica:0/task:0/device:XLA_GPU:0", &device));
-    return device;
-  }
-
-  FunctionLibraryRuntime* flr_;
-  std::unique_ptr<DeviceMgr> device_mgr_;
-  std::unique_ptr<FunctionLibraryDefinition> lib_def_;
-  std::unique_ptr<ProcessFunctionLibraryRuntime> pflr_;
+  DeviceSetup device_setup_;
 };
 
 TEST_F(XlaCompilerOptionsTest, PjRtOptionsXlaDevice) {
-  AddDevicesAndSetUp({DEVICE_XLA_GPU});
-  Device* device = GetXlaGpuDevice();
+  device_setup_.AddDevicesAndSetUp({DEVICE_XLA_GPU});
+  Device* device = device_setup_.GetDevice(DEVICE_XLA_GPU);
   DeviceType compilation_device_type = DeviceType(DEVICE_GPU_XLA_JIT);
 
   se::Platform::Id platform_id = nullptr;
@@ -127,8 +92,8 @@ TEST_F(XlaCompilerOptionsTest, PjRtOptionsXlaDevice) {
   XlaPlatformInfo platform_info(compilation_device_type, platform_id,
                                 xla_device_metadata.get(), custom_allocator);
 
-  XlaCompiler::Options options =
-      GenerateCompilerOptionsForPjRt(*flr_, device, platform_info);
+  XlaCompiler::Options options = GenerateCompilerOptionsForPjRt(
+      *device_setup_.flr(), device, platform_info);
 
   EXPECT_EQ(options.device_type, compilation_device_type);
   EXPECT_EQ(options.device_ordinal, 0);
@@ -149,12 +114,12 @@ TEST_F(XlaCompilerOptionsTest, PjRtOptionsXlaDevice) {
 }
 
 TEST_F(XlaCompilerOptionsTest, XlaOptions) {
-  AddDevicesAndSetUp({DEVICE_XLA_CPU});
-  Device* device = device_mgr_->HostCPU();
+  device_setup_.AddDevicesAndSetUp({DEVICE_XLA_GPU});
+  Device* device = device_setup_.GetDevice(DEVICE_XLA_GPU);
 
   xla::LocalClient* client = xla::ClientLibrary::LocalClientOrDie();
-  DeviceType device_type = DeviceType(DEVICE_XLA_CPU);
-  DeviceType compilation_device_type = DeviceType(DEVICE_CPU_XLA_JIT);
+  DeviceType device_type = DeviceType(DEVICE_XLA_GPU);
+  DeviceType compilation_device_type = DeviceType(DEVICE_GPU_XLA_JIT);
 
   auto xla_device_compiler = CreateXlaDeviceCompiler(
       XlaDeviceExecutablePersistor::Config(), compilation_device_type, client);
@@ -166,8 +131,9 @@ TEST_F(XlaCompilerOptionsTest, XlaOptions) {
   XlaPlatformInfo platform_info(device_type, platform_id,
                                 xla_device_metadata.get(), custom_allocator);
 
-  XlaCompiler::Options options = GenerateCompilerOptions(
-      *xla_device_compiler, *flr_, device, nullptr, platform_info, false);
+  XlaCompiler::Options options =
+      GenerateCompilerOptions(*xla_device_compiler, *device_setup_.flr(),
+                              device, nullptr, platform_info, false);
 
   EXPECT_EQ(options.device_type, compilation_device_type);
   EXPECT_NE(options.flib_def, nullptr);
@@ -187,8 +153,8 @@ TEST_F(XlaCompilerOptionsTest, XlaOptions) {
 }
 
 TEST_F(XlaCompilerOptionsTest, XlaOptionsHasRefVarsNoXlaDeviceMetadata) {
-  AddDevicesAndSetUp({DEVICE_CPU});
-  Device* device = device_mgr_->HostCPU();
+  device_setup_.AddDevicesAndSetUp({DEVICE_CPU});
+  Device* device = device_setup_.GetDevice(DEVICE_CPU);
 
   xla::LocalClient* client = xla::ClientLibrary::LocalClientOrDie();
   DeviceType device_type = DeviceType(DEVICE_CPU);
@@ -203,8 +169,9 @@ TEST_F(XlaCompilerOptionsTest, XlaOptionsHasRefVarsNoXlaDeviceMetadata) {
   XlaPlatformInfo platform_info(device_type, platform_id, nullptr,
                                 custom_allocator);
 
-  XlaCompiler::Options options = GenerateCompilerOptions(
-      *xla_device_compiler, *flr_, device, nullptr, platform_info, false);
+  XlaCompiler::Options options =
+      GenerateCompilerOptions(*xla_device_compiler, *device_setup_.flr(),
+                              device, nullptr, platform_info, false);
 
   EXPECT_EQ(options.device_type, compilation_device_type);
   EXPECT_NE(options.flib_def, nullptr);
@@ -227,7 +194,7 @@ TEST_F(XlaCompilerOptionsTest, XlaOptionsHasRefVarsNoXlaDeviceMetadata) {
 }
 
 TEST_F(XlaCompilerOptionsTest, TfRtTpuOptions) {
-  AddDevicesAndSetUp({DEVICE_TPU_NODE});
+  device_setup_.AddDevicesAndSetUp({DEVICE_TPU_NODE});
 
   // Just use the default local client for testing purposes.
   xla::LocalClient* client = xla::ClientLibrary::LocalClientOrDie();
@@ -237,8 +204,8 @@ TEST_F(XlaCompilerOptionsTest, TfRtTpuOptions) {
       XlaDeviceExecutablePersistor::Config(), compilation_device_type, client);
   core::ScopedUnref xla_device_compiler_ref(xla_device_compiler);
 
-  XlaCompiler::Options options =
-      GenerateCompilerOptionsForTfrtTpu(*xla_device_compiler, *flr_);
+  XlaCompiler::Options options = GenerateCompilerOptionsForTfrtTpu(
+      *xla_device_compiler, *device_setup_.flr());
 
   EXPECT_EQ(options.device_type, compilation_device_type);
   EXPECT_NE(options.flib_def, nullptr);
