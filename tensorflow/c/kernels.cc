tensorflow/tensorflow

commit 0b6b491d21d6a4eb5fbab1cca565bc1e94ca9543

Rename to underlying type rather than alias
Towards removing typedef. This changes from typedef to typedef'd type. This
doesn't cover all cases.

PiperOrigin-RevId: 390024245
Change-Id: I4c5c9dd86595311516a6f1584650e3cba979688a

tensorflow/c/kernels.cc
@@ -464,10 +464,10 @@ TF_Tensor* TF_AllocateOutput(TF_OpKernelContext* context, int index,
                              size_t len, TF_Status* status) {
   TF_SetStatus(status, TF_OK, "");
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
-  static_assert(sizeof(int64_t) == sizeof(tensorflow::int64),
+  static_assert(sizeof(int64_t) == sizeof(int64_t),
                 "64-bit int types should match in size");
-  tensorflow::gtl::ArraySlice<tensorflow::int64> dimarray(
-      reinterpret_cast<tensorflow::int64*>(dims), num_dims);
+  tensorflow::gtl::ArraySlice<int64_t> dimarray(
+      reinterpret_cast<int64_t*>(dims), num_dims);
   tensorflow::Tensor* tensor;
   tensorflow::Status s = cc_ctx->allocate_output(
       index, tensorflow::TensorShape(dimarray), &tensor);
@@ -490,12 +490,12 @@ TF_Tensor* TF_ForwardInputOrAllocateOutput(
   TF_SetStatus(status, TF_OK, "");
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
 
-  static_assert(sizeof(int64_t) == sizeof(tensorflow::int64),
+  static_assert(sizeof(int64_t) == sizeof(int64_t),
                 "64-bit int types should match in size");
   tensorflow::gtl::ArraySlice<int> input_indices_array(
       candidate_input_indices, num_candidate_input_indices);
-  tensorflow::gtl::ArraySlice<tensorflow::int64> output_dimarray(
-      reinterpret_cast<tensorflow::int64*>(output_dims), output_num_dims);
+  tensorflow::gtl::ArraySlice<int64_t> output_dimarray(
+      reinterpret_cast<int64_t*>(output_dims), output_num_dims);
   tensorflow::Tensor* output_tensor_pointer;
   tensorflow::Status s = cc_ctx->forward_input_or_allocate_output(
       input_indices_array, output_index,
@@ -519,10 +519,10 @@ TF_Tensor* TF_AllocateTemp(TF_OpKernelContext* context, TF_DataType dtype,
                            TF_Status* status) {
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
   TF_SetStatus(status, TF_OK, "");
-  static_assert(sizeof(int64_t) == sizeof(tensorflow::int64),
+  static_assert(sizeof(int64_t) == sizeof(int64_t),
                 "64-bit int types should match in size");
-  tensorflow::gtl::ArraySlice<tensorflow::int64> dimarray(
-      reinterpret_cast<tensorflow::int64*>(dims), num_dims);
+  tensorflow::gtl::ArraySlice<int64_t> dimarray(
+      reinterpret_cast<int64_t*>(dims), num_dims);
   if (attributes && !attributes->struct_size) {
     TF_SetStatus(
         status, TF_INVALID_ARGUMENT,

tensorflow/core/kernels/sparse/kernels.cc
@@ -29,7 +29,7 @@ namespace functor {
 
 Status SparseTensorToCSRSparseMatrixCPUFunctor::operator()(
     const int64_t batch_size, const int num_rows,
-    TTypes<int64>::ConstMatrix indices, TTypes<int32>::Vec batch_ptr,
+    TTypes<int64_t>::ConstMatrix indices, TTypes<int32>::Vec batch_ptr,
     TTypes<int32>::Vec csr_row_ptr, TTypes<int32>::Vec csr_col_ind) {
   // Validate inputs.
   if (batch_ptr.size() != batch_size + 1) {

commit b15e12ae5034de6fb387d4649be5b5d30f16ab41

Remove redundant asserts
This seems to have been introduced in 0b6b491d21d6 with the switch from `tensorflow::int64` to `int64_t`.

tensorflow/c/kernels.cc
@@ -791,8 +791,6 @@ TF_Tensor* TF_AllocateOutput(TF_OpKernelContext* context, int index,
                              int num_dims, size_t len, TF_Status* status) {
   TF_SetStatus(status, TF_OK, "");
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
-  static_assert(sizeof(int64_t) == sizeof(int64_t),
-                "64-bit int types should match in size");
   tensorflow::gtl::ArraySlice<const int64_t> dimarray(
       reinterpret_cast<const int64_t*>(dims), num_dims);
   tensorflow::Tensor* tensor;
@@ -818,8 +816,6 @@ TF_Tensor* TF_ForwardInputOrAllocateOutput(
   TF_SetStatus(status, TF_OK, "");
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
 
-  static_assert(sizeof(int64_t) == sizeof(int64_t),
-                "64-bit int types should match in size");
   tensorflow::gtl::ArraySlice<int> input_indices_array(
       candidate_input_indices, num_candidate_input_indices);
   tensorflow::gtl::ArraySlice<const int64_t> output_dimarray(
@@ -847,8 +843,6 @@ TF_Tensor* TF_AllocateTemp(TF_OpKernelContext* context, TF_DataType dtype,
                            TF_Status* status) {
   auto* cc_ctx = reinterpret_cast<::tensorflow::OpKernelContext*>(context);
   TF_SetStatus(status, TF_OK, "");
-  static_assert(sizeof(int64_t) == sizeof(int64_t),
-                "64-bit int types should match in size");
   tensorflow::gtl::ArraySlice<const int64_t> dimarray(
       reinterpret_cast<const int64_t*>(dims), num_dims);
   if (attributes && !attributes->struct_size) {