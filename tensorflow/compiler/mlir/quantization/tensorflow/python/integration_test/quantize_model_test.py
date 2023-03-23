db9dcb47aabd290fd8aa019ff5de03496c6ae713
Dan Suh <dansuh@google.com>
2023-03-22 23:54:07 -0700

    Copy assets after quantizing QAT models.
    PiperOrigin-RevId: 518778572

--- tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
@@ -1219,6 +1219,77 @@ class StaticRangeQuantizationTest(quantize_model_test_base.QuantizedModelTest):
 
       self.assertAllClose(lookup_val, [1.0, 2.0, 0.0])
 
+  # TODO(b/244276332): Allow table initialization in TF2 eager mode.
+  @test_util.deprecated_graph_mode_only
+  def test_qat_file_init_hash_table_lookup_model_tf1(self):
+    tags = {tag_constants.SERVING}
+    signature_def_key = signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY
+
+    # Create and save a simple model that involves a hash table.
+    inputs, outputs = self._create_and_save_file_init_hash_table_qat_model_tf1(
+        self._input_saved_model_path, tags, signature_def_key
+    )
+
+    # Make sure that the desired input key and output key is present.
+    self.assertIn('input_vocabs', inputs.keys())
+    self.assertIn('lookup', outputs.keys())
+
+    # Representative dataset is composed of a set of vocabs for table lookup.
+    repr_ds = [
+        {'input_vocabs': np.array([b'static', b'range', b'quantization'])}
+        for _ in range(4)
+    ]
+
+    quantization_options = quant_opts_pb2.QuantizationOptions(
+        quantization_method=quant_opts_pb2.QuantizationMethod(
+            experimental_method=_ExperimentalMethod.STATIC_RANGE
+        )
+    )
+    signature_def_keys = [signature_def_key]
+
+    quantize_model.quantize(
+        self._input_saved_model_path,
+        signature_def_keys,
+        tags,
+        self._output_saved_model_path,
+        quantization_options,
+        representative_dataset=repr_ds,
+    )
+
+    # Tests table lookup to make sure the table has been initialized
+    # successfully.
+    with session.Session(graph=ops.Graph()) as sess:
+      output_meta_graph_def = saved_model_loader.load(
+          sess, tags=tags, export_dir=self._output_saved_model_path
+      )
+
+      # The graph should contain a quantized function call (it contains a
+      # single f32 matmul node).
+      self.assertTrue(
+          self._contains_quantized_function_call(
+              output_meta_graph_def.graph_def
+          )
+      )
+      self.assertCountEqual(
+          output_meta_graph_def.signature_def.keys(), signature_def_keys
+      )
+
+      signature_def = output_meta_graph_def.signature_def[signature_def_key]
+      input_tensor_name = signature_def.inputs['input_vocabs'].name
+      input_tensor = sess.graph.get_tensor_by_name(input_tensor_name)
+      lookup_tensor_name = signature_def.outputs['lookup'].name
+      lookup_tensor = sess.graph.get_tensor_by_name(lookup_tensor_name)
+
+      lookup_val = sess.run(
+          lookup_tensor,
+          feed_dict={
+              input_tensor: np.array([b'dynamic', b'quantization', b'range'])
+          },
+      )
+
+      # "dynamic" is not in the table: -1 (default value)
+      self.assertAllClose(lookup_val, [-1.0, 2.0, 1.0])
+
   # Run this test only with the eager mode.
   @test_util.run_v2_only
   def test_ptq_model_with_variable(self):
--- tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model.py
@@ -514,6 +514,39 @@ def _run_graph_for_calibration(
   logging.info('Calibration step complete.')
 
 
+def _copy_assets(src_path: str, dst_path: str) -> None:
+  """Copies the assets directory of the saved model.
+
+  Clones the contents of the assets/ directory from the source saved model
+  directory to the destination saved model directory. Nothing will be copied if
+  there are no assets directory in the source directory.
+
+  Args:
+    src_path: Source saved model directory.
+    dst_path: Destination saved model directory. This directory must exist.
+  """
+  src_assets_path = file_io.join(src_path, _ASSETS_DIR)
+  if not file_io.file_exists_v2(src_assets_path):
+    # Do nothing if the source assets path does not exist.
+    return
+
+  dst_assets_path = file_io.join(dst_path, _ASSETS_DIR)
+  file_io.create_dir_v2(dst_assets_path)
+
+  for curr_dir, _, files in file_io.walk_v2(src_assets_path):
+    for asset_file_name in files:
+      src_asset_file = file_io.join(curr_dir, asset_file_name)
+
+      # Construct the destination assets file path.
+      curr_dst_dir = curr_dir.replace(src_assets_path, dst_assets_path)
+      dst_asset_file = file_io.join(curr_dst_dir, asset_file_name)
+
+      file_io.copy_v2(src_asset_file, dst_asset_file)
+      logging.info(
+          'Copied asset file: %s -> %s', src_asset_file, dst_asset_file
+      )
+
+
 def _run_static_range_qat(
     src_saved_model_path: str,
     dst_saved_model_path: str,
@@ -559,6 +592,8 @@ def _run_static_range_qat(
       asset_file_defs=exported_model.asset_file_defs,
   )
 
+  _copy_assets(src_saved_model_path, dst_saved_model_path)
+
 
 def _add_calibration_statistics(graph_def: graph_pb2.GraphDef) -> None:
   """Adds calibration statistics to the graph def.
@@ -591,39 +626,6 @@ def _add_calibration_statistics(graph_def: graph_pb2.GraphDef) -> None:
             node_id.decode('utf-8'),
             function_def.signature.name,
         )
-
-
-def _copy_assets(src_path: str, dst_path: str) -> None:
-  """Copies the assets directory of the saved model.
-
-  Clones the contents of the assets/ directory from the source saved model
-  directory to the destination saved model directory. Nothing will be copied if
-  there are no assets directory in the source directory.
-
-  Args:
-    src_path: Source saved model directory.
-    dst_path: Destination saved model directory. This directory must exist.
-  """
-  src_assets_path = file_io.join(src_path, _ASSETS_DIR)
-  if not file_io.file_exists_v2(src_assets_path):
-    # Do nothing if the source assets path does not exist.
-    return
-
-  dst_assets_path = file_io.join(dst_path, _ASSETS_DIR)
-  file_io.create_dir_v2(dst_assets_path)
-
-  for curr_dir, _, files in file_io.walk_v2(src_assets_path):
-    for asset_file_name in files:
-      src_asset_file = file_io.join(curr_dir, asset_file_name)
-
-      # Construct the destination assets file path.
-      curr_dst_dir = curr_dir.replace(src_assets_path, dst_assets_path)
-      dst_asset_file = file_io.join(curr_dst_dir, asset_file_name)
-
-      file_io.copy_v2(src_asset_file, dst_asset_file)
-      logging.info(
-          'Copied asset file: %s -> %s', src_asset_file, dst_asset_file
-      )
 
 
 def _get_saver_def_or_none(
