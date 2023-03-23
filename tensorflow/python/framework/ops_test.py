da7bd3c0693a6e0285bff0821effc6d78c113222
Russell Power <power@google.com>
2023-03-23 13:14:35 -0700

    Create custom Python objects for Graph, Operation, and Tensor.
    Migrate basic operations onto these types. The default Pybind wrapping approach
    is not compatible with subclassed objects, and given the reference cycles in
    the TF Graph/Tensor/Op usage, runs into memory management issues. Instead we
    follow the approach taken by JAX pjit and create a custom Python object, but
    continue to use the Pybind mechanisms to simplify the class creation and
    function calling mechanisms.
    
    PiperOrigin-RevId: 518944554

--- tensorflow/python/framework/ops_test.py
+++ tensorflow/python/framework/ops_test.py
@@ -697,13 +697,13 @@ class OperationTest(test_util.TensorFlowTestCase):
     float_t, label_str_t = op.values()
     self.assertEqual(dtypes.float32, float_t.dtype)
     self.assertEqual(op, float_t.op)
-    self.assertEqual(0, float_t._value_index)
+    self.assertEqual(0, float_t.value_index)
     self.assertEqual(0, len(float_t.consumers()))
     self.assertEqual("myop", float_t._as_node_def_input())
 
     self.assertEqual(dtypes.string, label_str_t.dtype)
     self.assertEqual(op, label_str_t.op)
-    self.assertEqual(1, label_str_t._value_index)
+    self.assertEqual(1, label_str_t.value_index)
     self.assertEqual(0, len(label_str_t.consumers()))
     self.assertEqual("myop:1", label_str_t._as_node_def_input())
 
