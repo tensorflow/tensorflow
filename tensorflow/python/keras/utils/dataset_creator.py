2d79d625696cbc429dcc1eba65ff2a06852c902a
amangupta201 <113130485+amangupta201@users.noreply.github.com>
2023-03-23 21:44:56 +0530

    Updated dataset_creator.py

--- tensorflow/python/keras/utils/dataset_creator.py
+++ tensorflow/python/keras/utils/dataset_creator.py
@@ -20,6 +20,7 @@ from tensorflow.python.types import data as data_types
 from tensorflow.python.util.tf_export import keras_export
 
 
+
 @keras_export('keras.utils.experimental.DatasetCreator', v1=[])
 class DatasetCreator(object):
   """Object that returns a `tf.data.Dataset` upon invoking.
@@ -79,6 +80,43 @@ class DatasetCreator(object):
       with distributed training. See `tf.distribute.InputOptions` for more
       information.
   """
+  class Builder(tfds.core.GeneratorBasedBuilder):
+  """DatasetBuilder for my_dataset dataset."""
+
+  VERSION = tfds.core.Version('1.0.0')
+  RELEASE_NOTES = {
+      '1.0.0': 'Initial release.',
+  }
+
+  def _info(self) -> tfds.core.DatasetInfo:
+    """Dataset metadata (homepage, citation,...)."""
+    return self.dataset_info_from_configs(
+        features=tfds.features.FeaturesDict({
+            'image': tfds.features.Image(shape=(256, 256, 3)),
+            'label': tfds.features.ClassLabel(
+                names=['no', 'yes'],
+                doc='Whether this is a picture of a cat'),
+        }),
+    )
+
+  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+    """Download the data and define splits."""
+    extracted_path = dl_manager.download_and_extract('http://data.org/data.zip')
+    # dl_manager returns pathlib-like objects with `path.read_text()`,
+    # `path.iterdir()`,...
+    return {
+        'train': self._generate_examples(path=extracted_path / 'train_images'),
+        'test': self._generate_examples(path=extracted_path / 'test_images'),
+    }
+
+  def _generate_examples(self, path) -> Iterator[Tuple[Key, Example]]:
+    """Generator of examples for each split."""
+    for img_path in path.glob('*.jpeg'):
+      # Yields (key, example)
+      yield img_path.name, {
+          'image': img_path,
+          'label': 'yes' if img_path.name.startswith('yes_') else 'no',
+      }
 
   def __init__(self, dataset_fn, input_options=None):
     if not callable(dataset_fn):
