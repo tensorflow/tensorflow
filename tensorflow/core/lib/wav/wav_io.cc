tensorflow/tensorflow

commit 50b4baf252cd464414840ccbc5b0d6d9c597b8d2

Fix signed integer overflow in wav_io.cc
PiperOrigin-RevId: 458264455

tensorflow/core/lib/wav/wav_io.cc
@@ -85,7 +85,7 @@ inline float Int16SampleToFloat(int16_t data) {
 
 // Handles moving the data index forward, validating the arguments, and avoiding
 // overflow or underflow.
-Status IncrementOffset(int old_offset, int increment, size_t max_size,
+Status IncrementOffset(int old_offset, int64_t increment, size_t max_size,
                        int* new_offset) {
   if (old_offset < 0) {
     return errors::InvalidArgument("Negative offsets are not allowed: ",

tensorflow/core/lib/wav/wav_io.h
@@ -72,7 +72,7 @@ Status DecodeLin16WaveAsFloatVector(const std::string& wav_string,
 
 // Handles moving the data index forward, validating the arguments, and avoiding
 // overflow or underflow.
-Status IncrementOffset(int old_offset, int increment, size_t max_size,
+Status IncrementOffset(int old_offset, int64_t increment, size_t max_size,
                        int* new_offset);
 
 // This function is only exposed in the header for testing purposes, as a

commit ba298d672c3f04444fb38a906d62f9a5f642121c

Fix endless loop in tensorflow::wav::DecodeLin16WaveAsFloatVector
This bug was originally fixed by #56455

Regression was introduced in 50b4baf

tensorflow/core/lib/wav/wav_io.cc
@@ -99,16 +99,16 @@ Status IncrementOffset(int old_offset, int64_t increment, size_t max_size,
     return errors::InvalidArgument("Initial offset is outside data range: ",
                                    old_offset);
   }
-  *new_offset = old_offset + increment;
-  if (*new_offset > max_size) {
+  int64_t sum = old_offset + increment;
+  if (sum > max_size) {
     return errors::InvalidArgument("Data too short when trying to read string");
   }
   // See above for the check that the input offset is positive. If it's negative
   // here then it means that there's been an overflow in the arithmetic.
-  if (*new_offset < 0) {
-    return errors::InvalidArgument("Offset too large, overflowed: ",
-                                   *new_offset);
+  if (sum < 0) {
+    return errors::InvalidArgument("Offset too large, overflowed: ", sum);
   }
+  *new_offset = sum;
   return OkStatus();
 }
 