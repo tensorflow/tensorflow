53594557d2a388f49b4e6f9b5605b704705bf947
Jason Furmanek <Jason.Furmanek@amd.com>
2023-03-23 03:19:53 +0000

    [ROCM] Fix amdgcn hlo tests

--- tensorflow/compiler/xla/service/gpu/tests/gpu_noalias_test.cc
+++ tensorflow/compiler/xla/service/gpu/tests/gpu_noalias_test.cc
@@ -52,9 +52,15 @@ TEST_F(GpuNoAliasTest, Concat) {
   // - After optimizations we have "concatenate(x, y, x)".
   // - We only pass the same parameters once, so the kernel will have these
   // parameters: (x, y, output), and all of them will be noalias.
+  auto expected_ir = is_built_with_rocm_ ? R"(
+CHECK: define amdgpu_kernel void @{{[a-zA-Z0-9_]+}}(ptr noalias align 16 dereferenceable(16) %arg0, ptr noalias align 16 dereferenceable(16) %arg1, ptr noalias align 128 dereferenceable(48) %arg2) #0
+  )"
+                                         : R"(
+CHECK: define void @{{[a-zA-Z0-9_]+}}(ptr noalias align 16 dereferenceable(16) %arg0, ptr noalias align 16 dereferenceable(16) %arg1, ptr noalias align 128 dereferenceable(48) %arg2)
+  )";
   CompileAndVerifyIr(
       std::move(hlo_module),
-      R"(CHECK: define void @{{[a-zA-Z0-9_]+}}(ptr noalias align 16 dereferenceable(16) %arg0, ptr noalias align 16 dereferenceable(16) %arg1, ptr noalias align 128 dereferenceable(48) %arg2))",
+      expected_ir,
       /*match_optimized_ir=*/false);
 }
 
