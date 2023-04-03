210389b86c0a1819c03ddd35f5fd4ee1274fa27e
Mason Chang <masonchang@google.com>
2023-04-03 13:54:38 -0700

    Clean up inconsistent usage of tf.RandomUniformOp in tf2xla fallback op list as we always use the MLIR one and never fallback
    PiperOrigin-RevId: 521553806

--- tensorflow/compiler/mlir/tf2xla/transforms/xla_legalize_tf.cc
+++ tensorflow/compiler/mlir/tf2xla/transforms/xla_legalize_tf.cc
@@ -699,10 +699,8 @@ const llvm::DenseSet<mlir::TypeID> &MlirPreferredOps() {
     // These ops have undetermined bugs, may not be legalizable with XlaOpKernel
     // legalization in TF2XLA fallback. By legalization with MLIR, we can fix
     // the bug. b/195583695 describes the motivation of this change.
-    // See b/216355804 how to reproduce the bug regarding tf.RandomUniform Op
     // See b/216353817 how to reproduce the bug regarding tf.StridedSlice Op
     // See b/245615401 how to reproduce the bug regarding tf.SliceOp
-    TypeID::get<TF::RandomUniformOp>(),
     TypeID::get<TF::StridedSliceOp>(),
     TypeID::get<TF::SliceOp>(),
 
