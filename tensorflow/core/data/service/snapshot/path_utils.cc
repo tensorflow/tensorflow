e09ffa95ee64f7313b35a73b3d9a1e8e0634d721
Matt Callanan <mpcallanan@google.com>
2023-04-04 15:23:19 -0700

    Add missing includes.
    PiperOrigin-RevId: 521880598

--- tensorflow/core/data/service/snapshot/path_utils.cc
+++ tensorflow/core/data/service/snapshot/path_utils.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/core/data/service/snapshot/path_utils.h"
 
+#include <cstdint>
 #include <string>
 #include <utility>
 #include <vector>
--- tensorflow/core/data/service/snapshot/path_utils.h
+++ tensorflow/core/data/service/snapshot/path_utils.h
@@ -15,6 +15,7 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_DATA_SERVICE_SNAPSHOT_PATH_UTILS_H_
 #define TENSORFLOW_CORE_DATA_SERVICE_SNAPSHOT_PATH_UTILS_H_
 
+#include <cstdint>
 #include <string>
 #include <utility>
 
