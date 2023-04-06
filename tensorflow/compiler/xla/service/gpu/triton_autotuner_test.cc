f822a288b6ac9ed63e300870b7c0bbd28056b5d8
Ilia Sergachev <sergachev@google.com>
2023-04-06 10:22:07 -0700

    [XLA:GPU] Enable reductions in triton GEMM autotuner.
    This reuses more common parts of the compilation pipeline (HLO->LLVM->PTX->CUBIN) and enables compilation and verification of split-K matmuls implemented as batch matmuls + reductions.
    
    PiperOrigin-RevId: 522369342

--- tensorflow/compiler/xla/service/gpu/triton_autotuner_test.cc
+++ tensorflow/compiler/xla/service/gpu/triton_autotuner_test.cc
@@ -19,9 +19,7 @@ limitations under the License.
 #include <utility>
 
 #include "absl/strings/string_view.h"
-#include "tensorflow/compiler/xla/hlo/ir/hlo_instruction.h"
 #include "tensorflow/compiler/xla/hlo/ir/hlo_module.h"
-#include "tensorflow/compiler/xla/hlo/ir/hlo_opcode.h"
 #include "tensorflow/compiler/xla/service/gpu/gemm_rewriter_triton.h"
 #include "tensorflow/compiler/xla/service/hlo_pass_pipeline.h"
 #include "tensorflow/compiler/xla/tests/hlo_test_base.h"
@@ -41,13 +39,6 @@ class TritonAutotunerTest : public HloTestBase {
     return debug_options;
   }
 
-  se::CudaComputeCapability GetCudaComputeCapability() {
-    return backend()
-        .default_stream_executor()
-        ->GetDeviceDescription()
-        .cuda_compute_capability();
-  }
-
   void CheckTritonAutotuning(absl::string_view hlo,
                              absl::string_view expected) {
     HloPassPipeline pipeline("gemm_rewrite");
@@ -62,15 +53,9 @@ class TritonAutotunerTest : public HloTestBase {
 
     RunAndFilecheckHloRewrite(
         hlo, std::move(pipeline), expected, [](const HloModule* m) {
-          VLOG(5) << m->ToString();
-          const HloInstruction* dot_fusion =
-              m->entry_computation()->root_instruction();
-          if (dot_fusion->opcode() == HloOpcode::kReduce) {
-            dot_fusion = dot_fusion->operand(0);
-          }
-          CHECK_EQ(dot_fusion->opcode(), HloOpcode::kFusion);
           CHECK_GT(
-              dot_fusion
+              m->entry_computation()
+                  ->root_instruction()
                   ->backend_config<tensorflow::AutotuneResult::TritonGemmKey>()
                   .value()
                   .block_m(),
@@ -124,42 +109,15 @@ ENTRY e {
   EXPECT_TRUE(RunAndCompare(hlo, ErrorSpec{1e-2, 1e-2}));
 }
 
-TEST_F(TritonAutotunerTest, SelectsSplitK) {
-  if (!GetCudaComputeCapability().IsAtLeast(
-          se::CudaComputeCapability::AMPERE)) {
-    GTEST_SKIP() << "No BF16 before Ampere.";
-  }
-  // Shapes with K >> M, N have to force split-K configurations.
-  const std::string kHloText = R"(
-HloModule t
-
-ENTRY e {
-  p0 = s8[7,4096] parameter(0)
-  p0c = bf16[7,4096] convert(p0)
-  p1 = bf16[4096,18] parameter(1)
-  ROOT dot.0 = bf16[7,18] dot(p0c, p1),
-    lhs_contracting_dims={1}, rhs_contracting_dims={0}
-})";
-
-  MatchOptimizedHlo(kHloText, R"(
-; CHECK: reduce
-; CHECK: fusion(%p0, %p1), kind=kCustom
-; CHECK: ROOT %fusion.1
-)");
-
-  EXPECT_TRUE(RunAndCompare(kHloText, ErrorSpec{1e-1, 1e-1}));
-}
-
 TEST_F(TritonAutotunerTest, KnownBestConfig) {
-  const std::string kHloText = R"(
+  const std::string hlo = R"(
 HloModule t
 
 ENTRY e {
-  p0 = f16[256,256] parameter(0)
-  p1 = s8[256,256] parameter(1)
-  c = f16[256,256] convert(p1)
-  ROOT _ = f16[256,256] dot(c, p0),
-    lhs_contracting_dims={1}, rhs_contracting_dims={1}
+  p0 = f16[16,12288]{1,0} parameter(0)
+  p1 = s8[2304,12288]{1,0} parameter(1)
+  c = f16[2304,12288]{1,0} convert(p1)
+  ROOT _ = f16[2304,16]{1,0} dot(c, p0), lhs_contracting_dims={1}, rhs_contracting_dims={1}
 })";
 
   // This is the fastest config amongst the currently probed ones
@@ -168,14 +126,14 @@ ENTRY e {
       GetTestPlatform()->ExecutorForDevice(0).value()->GetDeviceDescription();
   const std::string& name = device_description.name();
   if (name == "NVIDIA RTX A6000" || name == "Tesla V100-SXM2-16GB") {
-    CheckTritonAutotuning(kHloText, R"(
+    CheckTritonAutotuning(hlo, R"(
 // CHECK: backend_config="{\"block_m\":\"32\",\"block_n\":\"32\",\"block_k\":\"256\",\"split_k\":\"1\",\"num_stages\":\"1\",\"num_warps\":\"4\"}"
   )");
   } else {
     VLOG(1) << "Not tested on " << name;
   }
 
-  EXPECT_TRUE(RunAndCompare(kHloText, ErrorSpec{0.02, 0.01}));
+  EXPECT_TRUE(RunAndCompare(hlo, ErrorSpec{0.02, 0.01}));
 }
 
 class TritonAutotunerLevelTest : public HloTestBase,
@@ -212,6 +170,43 @@ ENTRY e {
 
 INSTANTIATE_TEST_SUITE_P(TritonAutotunerLevelSweep, TritonAutotunerLevelTest,
                          ::testing::Range(0, 5));
+
+using TritonAutotunerSplitKTest = TritonAutotunerLevelTest;
+
+TEST_P(TritonAutotunerSplitKTest, SplitK) {
+  // Shapes with K >> M, N have to force split-K configurations.
+  const std::string hlo_text = R"(
+HloModule t
+
+ENTRY e {
+  p0 = s8[7,4096] parameter(0)
+  p0c = f16[7,4096] convert(p0)
+  p1 = f16[4096,18] parameter(1)
+  ROOT dot.0 = f16[7,18] dot(p0c, p1),
+    lhs_contracting_dims={1}, rhs_contracting_dims={0}
+})";
+
+  TritonAutotuner::ClearAutotuneResults();
+  TritonAutotuner::ClearCompilationCache();
+
+  // According to should_check_correctness() in triton_autotuner.
+  if (GetParam() >= 4) {
+    MatchOptimizedHlo(hlo_text, R"(
+; CHECK: fusion(%p0, %p1), kind=kCustom
+; CHECK-NOT: reduce
+)");
+  } else {
+    MatchOptimizedHlo(hlo_text, R"(
+; CHECK: fusion(%p0, %p1), kind=kCustom
+; CHECK: ROOT %reduce
+)");
+  }
+
+  EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-1, 1e-1}));
+}
+
+INSTANTIATE_TEST_SUITE_P(TritonAutotunerLevel1Test, TritonAutotunerSplitKTest,
+                         ::testing::Values(1, 4));
 
 }  // namespace
 }  // namespace gpu
