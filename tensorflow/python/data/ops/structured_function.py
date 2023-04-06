b1719d32f877e03d953c0df49e2d21d1dc1d3576
Josh Parson <94084697+jsparson1@users.noreply.github.com>
2023-04-06 09:11:12 -0400

    Update structured_function.py

--- tensorflow/python/data/ops/structured_function.py
+++ tensorflow/python/data/ops/structured_function.py
@@ -247,7 +247,8 @@ class StructuredFunctionWrapper():
     else:
       defun_kwargs.update({"func_name": func_name})
       defun_kwargs.update({"_tf_data_function": True})
-      if debug_mode.DEBUG_MODE and str(dataset.element_spec)[0:16] != 'RaggedTensorSpec':
+      element_spec_name = str(dataset.element_spec)[0:16]
+      if debug_mode.DEBUG_MODE and element_spec_name != 'RaggedTensorSpec':
         fn_factory = trace_py_function(defun_kwargs)
       else:
         if def_function.functions_run_eagerly():
