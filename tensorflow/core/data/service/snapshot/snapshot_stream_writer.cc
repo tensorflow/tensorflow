f00f5da706107343fb94307eff7c76aba64aaf97
Matt Callanan <mpcallanan@google.com>
2023-04-11 16:42:36 -0700

    Remove distributed snapshot stream reassignment error handling.
    This is no longer needed with streams no longer being reassigned.
    
    PiperOrigin-RevId: 523540168

--- tensorflow/core/data/service/snapshot/snapshot_stream_writer.cc
+++ tensorflow/core/data/service/snapshot/snapshot_stream_writer.cc
@@ -87,13 +87,6 @@ void SnapshotStreamWriter::WriteSnapshotAndLog() TF_LOCKS_EXCLUDED(mu_) {
   LOG(INFO) << "Writing distributed tf.data snapshot stream: "
             << params_.DebugString();
   Status status = WriteSnapshot();
-  if (IsStreamAssignmentChanged(status)) {
-    LOG(INFO) << "Stopped writing distributed tf.data snapshot stream due to a "
-                 "transient error: "
-              << params_.DebugString()
-              << ". It will be retried. Status: " << status;
-    return;
-  }
   status = FinalizeStream(status);
   mutex_lock l(mu_);
   if (!status.ok()) {
