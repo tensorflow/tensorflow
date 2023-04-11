f6a3365ce156463e2004232f1892e3d2b34c559c
Marcello Maggioni <maggioni@google.com>
2023-04-11 09:06:47 -0700

    [XLA] Make all-reduce reassociate look through some formatting operations like pad/reshape/slice.
    PiperOrigin-RevId: 523417878

--- tensorflow/compiler/xla/service/all_reduce_reassociate.cc
+++ tensorflow/compiler/xla/service/all_reduce_reassociate.cc
@@ -15,13 +15,21 @@ limitations under the License.
 
 #include "tensorflow/compiler/xla/service/all_reduce_reassociate.h"
 
+#include <cstdint>
+#include <optional>
+
+#include "absl/container/flat_hash_set.h"
+#include "tensorflow/compiler/xla/hlo/ir/hlo_casting_utils.h"
 #include "tensorflow/compiler/xla/hlo/ir/hlo_computation.h"
 #include "tensorflow/compiler/xla/hlo/ir/hlo_instruction.h"
 #include "tensorflow/compiler/xla/hlo/ir/hlo_instructions.h"
+#include "tensorflow/compiler/xla/hlo/ir/hlo_opcode.h"
+#include "tensorflow/compiler/xla/literal.h"
 #include "tensorflow/compiler/xla/service/all_reduce_key.h"
 #include "tensorflow/compiler/xla/service/collective_ops_utils.h"
 #include "tensorflow/compiler/xla/service/hlo_domain_map.h"
 #include "tensorflow/compiler/xla/service/hlo_query.h"
+#include "tensorflow/compiler/xla/statusor.h"
 #include "tensorflow/tsl/platform/errors.h"
 
 namespace xla {
@@ -38,6 +46,49 @@ bool AreCompatible(const HloAllReduceInstruction *ar0,
   std::optional<AllReduceKey> key1 = GetAllReduceKey(ar1);
   auto kind0 = MatchReductionComputation(ar0->to_apply());
   return key0 && key1 && kind0 && *key0 == *key1 && kind0 == op_kind;
+}
+
+// Look-through some formatting operations that might be in front of the
+// all-reduces we want to reassociate. Making sure the chain only has 1 user
+// throughout.
+HloAllReduceInstruction* LookThroughForAllReduce(
+    HloInstruction* instr, const Literal& reduction_identity) {
+  while (instr->opcode() != HloOpcode::kAllReduce) {
+    if (instr->user_count() != 1) {
+      return nullptr;
+    }
+    if (instr->opcode() != HloOpcode::kReshape &&
+        instr->opcode() != HloOpcode::kPad &&
+        instr->opcode() != HloOpcode::kSlice) {
+      return nullptr;
+    }
+    if (instr->opcode() == HloOpcode::kPad) {
+      if (!instr->operand(1)->IsConstant()) {
+        return nullptr;
+      }
+      if (instr->operand(1)->literal() != reduction_identity) {
+        return nullptr;
+      }
+    }
+    instr = instr->mutable_operand(0);
+  }
+  if (instr->user_count() != 1) {
+    return nullptr;
+  }
+  return Cast<HloAllReduceInstruction>(instr);
+}
+
+// Because we can look through pads its possible that reassociating the
+// all-reduce makes us reduce over more than the sum of the two unpadded
+// individual all-reduces. Check that's not the case.
+bool ReassociateAllReduceIsProfitable(HloInstruction* ar0, HloInstruction* ar1,
+                                      HloInstruction* reassociated_inst) {
+  int64_t pre_reassociated_size = ShapeUtil::ElementsIn(ar0->shape());
+  if (ar0 != ar1) {
+    pre_reassociated_size += ShapeUtil::ElementsIn(ar1->shape());
+  }
+  return pre_reassociated_size >=
+         ShapeUtil::ElementsIn(reassociated_inst->shape());
 }
 
 }  // namespace
@@ -57,42 +108,63 @@ StatusOr<bool> AllReduceReassociate::Run(
   bool changed = false;
   for (auto computation : module->computations(execution_threads)) {
     for (HloInstruction *inst : computation->MakeInstructionPostOrder()) {
+      // Check if the instruction we want to reassociate with will match any
+      // valid all-reduce reduction function. Save the ReductionKind object for
+      // later.
       std::optional<ReductionKind> kind = MatchReductionInstruction(inst);
-      if (!kind || inst->operand(0)->opcode() != HloOpcode::kAllReduce ||
-          inst->operand(1)->opcode() != HloOpcode::kAllReduce ||
-          !inst->shape().IsArray()) {
+      if (!kind) {
         continue;
       }
-
-      auto *ar0 = Cast<HloAllReduceInstruction>(inst->mutable_operand(0));
-      auto *ar1 = Cast<HloAllReduceInstruction>(inst->mutable_operand(1));
+      std::optional<Literal> reduction_identity =
+          GetReductionIdentity(*kind, inst->shape().element_type());
+      // Unsupported reduction type.
+      if (!reduction_identity) {
+        continue;
+      }
+      // Find LHS all-reduce.
+      HloAllReduceInstruction* ar0 = LookThroughForAllReduce(
+          inst->mutable_operand(0), *reduction_identity);
+      if (ar0 == nullptr) {
+        continue;
+      }
+      // Find RHS all-reduce.
+      HloAllReduceInstruction* ar1 = LookThroughForAllReduce(
+          inst->mutable_operand(1), *reduction_identity);
+      if (ar1 == nullptr) {
+        continue;
+      }
+      if (!inst->shape().IsArray()) {
+        continue;
+      }
+      // Because we look through pads it might not be profitable to actually
+      // reassociate if reassociating makes us all-reduce more values.
+      if (!ReassociateAllReduceIsProfitable(ar0, ar1, inst)) {
+        continue;
+      }
       if (!AreCompatible(ar0, ar1, *kind)) {
         VLOG(2) << "All-Reduce operations are not compatible, skipping";
         continue;
       }
-
-      if (ar0->user_count() != 1 || ar1->user_count() != 1) {
-        VLOG(2) << "All-Reduce operations have > 1 users";
-        continue;
-      }
+      VLOG(2) << "Reassociated:";
+      VLOG(2) << "\tAR0: " << ar0->opcode();
+      VLOG(2) << "\tAR1: " << ar1->opcode();
 
       // Found pattern op(ar(x), ar(y)). Transform it into ar(op(x,y)).
-      HloInstruction *new_op = computation->AddInstruction(
-          inst->CloneWithNewOperands(inst->shape(), {ar0->mutable_operand(0),
-                                                     ar1->mutable_operand(0)}));
-      HloInstruction *new_ar = computation->AddInstruction(
-          ar0->CloneWithNewOperands(inst->shape(), {new_op}));
+      TF_RETURN_IF_ERROR(ar0->ReplaceAllUsesWith(ar0->mutable_operand(0)));
+      TF_RETURN_IF_ERROR(ar1->ReplaceAllUsesWith(ar1->mutable_operand(0)));
+      auto op_users = inst->users();
+      HloInstruction* new_ar = computation->AddInstruction(
+          ar0->CloneWithNewOperands(inst->shape(), {inst}));
 
       // Do not reuse channel_id from the existing instruction.
       if (new_ar->channel_id()) {
         new_ar->set_channel_id(next_channel_id++);
       }
 
-      TF_RETURN_IF_ERROR(inst->ReplaceAllUsesWith(new_ar));
+      TF_RETURN_IF_ERROR(inst->ReplaceUsesWith(op_users, new_ar));
       // Note that RemoveInstructionAndUnusedOperands may not remove the 2
       // all-reduce operands of `inst` if they are not safe to remove otherwise,
       // so manually these instructions.
-      TF_RETURN_IF_ERROR(computation->RemoveInstruction(inst));
       TF_RETURN_IF_ERROR(computation->RemoveInstruction(ar0));
       if (ar0 != ar1) {
         TF_RETURN_IF_ERROR(computation->RemoveInstruction(ar1));
