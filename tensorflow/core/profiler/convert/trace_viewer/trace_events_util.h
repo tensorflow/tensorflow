14c1414cd967df089fb6e4b39d41650259ac789e
Jie Sun <jiesun@google.com>
2023-04-18 13:25:28 -0700

    add nway_merge to trace_event_util for OSS support.
    PiperOrigin-RevId: 525235150

--- tensorflow/core/profiler/convert/trace_viewer/trace_events_util.h
+++ tensorflow/core/profiler/convert/trace_viewer/trace_events_util.h
@@ -72,6 +72,69 @@ inline bool IsCompleteFlow(const TraceEventFlow& flow) {
          flow.back()->flow_entry_type() == TraceEvent::FLOW_END;
 }
 
+// Nway-merge implementation.
+
+// Reorders the elements of the range [first, last) to restore the heap
+// condition (i.e. `std::is_heap(first, last, comp)`) following a change
+// in the value of `*first`.
+//
+// REQUIRES: `first < last`, and [first, last) would be a valid heap if `*first`
+// had a suitable value.
+template <typename RandIt, typename Compare>
+void push_down_root(RandIt first, RandIt last, Compare comp) {
+  size_t size = last - first;
+  size_t hole = 0;  // root.
+  auto value = std::move(*first);
+  while (true) {
+    size_t l_child = 2 * hole + 1;
+    size_t r_child = l_child + 1;
+    size_t max_child = l_child;
+    if (r_child < size && comp(first[l_child], first[r_child])) {
+      max_child = r_child;
+    }
+    if (max_child >= size) break;
+    if (!comp(value, first[max_child])) break;
+    first[hole] = std::move(first[max_child]);
+    hole = max_child;
+  }
+  first[hole] = std::move(value);
+}
+
+template <typename ContainerContainer, typename Out, typename Cmp>
+Out nway_merge(const ContainerContainer& containers, Out out, Cmp cmp) {
+  using std::begin;
+  using std::end;
+  using In = decltype(begin(*begin(containers)));  // The input iterator type.
+  using Range = std::pair<In, In>;
+  std::vector<Range> sources;
+  for (const auto& container : containers) {
+    Range r(begin(container), end(container));
+    if (r.first != r.second) {
+      sources.push_back(r);
+    }
+  }
+  if (sources.empty()) return out;
+  // Take a comparator for T and produce an inverse comparator
+  // for std::pair<In<T>, In<T>>, inverted so as to produce a min-heap.
+  auto heap_cmp = [&](const Range& a, const Range& b) {
+    // Compares b < a instead of a < b.
+    return cmp(*b.first, *a.first);
+  };
+  std::make_heap(sources.begin(), sources.end(), heap_cmp);
+  while (true) {
+    Range& r = sources.front();
+    *out = *r.first;
+    ++r.first;
+    ++out;
+    if (r.first == r.second) {
+      if (sources.size() == 1) return out;
+      r = std::move(sources.back());
+      sources.pop_back();
+    }
+    push_down_root(sources.begin(), sources.end(), heap_cmp);
+  }
+}
+
 }  // namespace profiler
 }  // namespace tensorflow
 
