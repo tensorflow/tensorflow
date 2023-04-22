7ddf59d89ff13fd6c9086d23c71156a4d9d29ba5
Wilsin Gosti <wilsin@google.com>
2023-04-21 17:45:21 -0700

    #tf-data Ramp down both `autotune_buffer_optimization` and `stage_based_autotune_v2` experiments to fix a bug.
    PiperOrigin-RevId: 526180096

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -950,7 +950,7 @@ namespace {
 REGISTER_DATASET_EXPERIMENT("allow_small_function_optimizations",
                             RandomJobSamplePercentage<0>, AllTasks);
 REGISTER_DATASET_EXPERIMENT("autotune_buffer_optimization",
-                            RandomJobSamplePercentage<50>, AllTasks);
+                            RandomJobSamplePercentage<0>, AllTasks);
 REGISTER_DATASET_EXPERIMENT(kFilterParallelizationOpt,
                             RandomJobSamplePercentage<0>, AllTasks);
 REGISTER_DATASET_EXPERIMENT("min_outer_interleave_parallelism",
@@ -962,8 +962,7 @@ REGISTER_DATASET_EXPERIMENT("serialize_input_cycle_length",
 REGISTER_DATASET_EXPERIMENT("stage_based_autotune",
                             RandomJobSamplePercentage<0>, IndependentHostTasks);
 REGISTER_DATASET_EXPERIMENT("stage_based_autotune_v2",
-                            RandomJobSamplePercentage<25>,
-                            IndependentHostTasks);
+                            RandomJobSamplePercentage<0>, IndependentHostTasks);
 REGISTER_DATASET_EXPERIMENT("data_transfer", RandomJobSamplePercentage<1>,
                             IndependentHostTasks);
 }  // namespace
