c85e07d7fc06c6f91c326afe732e38aad8e1735a
Ramesh Sampath <rameshsampath@google.com>
2023-04-26 09:40:02 -0700

    Add graph analysis for MLIR CPU/GPU bridge
    This skips MLIR CPU/GPU phase 1 pipeline if the input graph has reshape ops with non-constant output shape, which is not currently supported in the MLIR bridge.
    
    PiperOrigin-RevId: 527291167

--- tensorflow/compiler/tf2xla/mlir_bridge_pass.cc
+++ tensorflow/compiler/tf2xla/mlir_bridge_pass.cc
@@ -22,7 +22,6 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_structs.h"
 #include "tensorflow/compiler/mlir/tensorflow/transforms/bridge.h"
 #include "tensorflow/compiler/mlir/tensorflow/utils/device_util.h"
-#include "tensorflow/compiler/mlir/tf2xla/graph_analysis.h"
 #include "tensorflow/compiler/tf2xla/tf2xla_defs.h"
 #include "tensorflow/core/common_runtime/device_set.h"
 #include "tensorflow/core/framework/metrics.h"
@@ -295,18 +294,6 @@ Status MlirBridgePass::Run(const std::string& function_name,
     VLOG(1) << "Running MLIR TPU Bridge";
     mlir_bridge_gauge_v2->GetCell()->Set(true);
     return mlir::TFTPU::TPUBridge(module, fallback_enabled, function_name);
-  }
-
-  if (GraphHasFeaturesUnsupportedByMlirGenericBridge(graph,
-                                                     &function_library)) {
-    VLOG(1) << "Skipping MLIR CPU/GPU Bridge, disabled because graph has "
-               "unsupported features.";
-    metrics::UpdateTfMlirBridgeFirstPhaseCounter(
-        /*device type*/ "cpu/gpu",
-        /*bridge version*/ "tfxla",
-        /*fallback_enabled*/ false,
-        /*result*/ "invalid_graph");
-    return OkStatus();
   }
   VLOG(1) << "Running MLIR CPU/GPU Bridge";
   return mlir::TF::RunTFXLABridge(module, function_name);
