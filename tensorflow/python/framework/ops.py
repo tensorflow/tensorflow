b4c517a381cbac8b5229c522ba6a7b2410fa3302
Tristen Allen <tristenallen@google.com>
2023-04-27 11:28:51 -0700

    Remove eager execution check from `Tensor.__hash__()`.
    This will have the effect of disabling `Tensor.__hash__()`
    in eager execution mode regardless of whether or not
    numpy-style equality is enabled.
    
    The ability to hash (generalized) eager tensors was
    disabled in a previous change - this change extends
    that to the base Tensor class in eager execution mode.
    
    PiperOrigin-RevId: 527635690

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -808,8 +808,7 @@ class Tensor(
 
   def __hash__(self):
     g = getattr(self, "graph", None)
-    if (Tensor._USE_EQUALITY and executing_eagerly_outside_functions() and
-        (g is None or g.building_function)):
+    if (Tensor._USE_EQUALITY and (g is None or g.building_function)):
       raise TypeError("Tensor is unhashable. "
                       "Instead, use tensor.ref() as the key.")
     else:
