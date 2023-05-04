e5e565ff1eb64f103d9d93634d5bcb48ff68bb81
Scott Zhu <scottzhu@google.com>
2023-05-04 09:29:04 -0700

    Restructure the DTensor based strategy.
    1. Move the StrategyExtended to its own module since it will be shared by all the strategies (TPU strategy in future).
    2. Move most of the util methods to dtensor_util.py
    
    PiperOrigin-RevId: 529431253

--- tensorflow/python/distribute/experimental/multi_worker_mirrored_strategy.py
+++ tensorflow/python/distribute/experimental/multi_worker_mirrored_strategy.py
@@ -25,8 +25,8 @@ from tensorflow.dtensor.python import mesh_util
 from tensorflow.python.distribute import distribute_lib
 from tensorflow.python.distribute import multi_worker_util
 from tensorflow.python.distribute.cluster_resolver import tfconfig_cluster_resolver
+from tensorflow.python.distribute.experimental import dtensor_strategy_extended
 from tensorflow.python.distribute.experimental import dtensor_util
-from tensorflow.python.distribute.experimental import mirrored_strategy
 
 
 class MultiWorkerMirroredStrategy(distribute_lib.Strategy):
@@ -61,7 +61,7 @@ class MultiWorkerMirroredStrategy(distribute_lib.Strategy):
           cluster_resolver)
       _config_dtensor_env_var(dtensor_env_var)
       mesh = _build_distributed_mesh(dtensor_util.DEFAULT_BATCH_MESH_DIM_NAME)
-    extended = mirrored_strategy.MirroredExtended(
+    extended = dtensor_strategy_extended.DTensorStrategyExtended(
         container_strategy=self, mesh=mesh)
     super().__init__(extended)
     self._mesh = mesh
