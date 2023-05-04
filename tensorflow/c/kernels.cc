47eaa828a1dd4bf50ec4203ef4bbb348b3ef0dd0
dingyuqing05 <yuqing.ding@intel.com>
2023-05-04 09:01:40 +0000

    Add nullptr check

--- tensorflow/c/kernels.cc
+++ tensorflow/c/kernels.cc
@@ -357,6 +357,10 @@ void TF_GetInput(TF_OpKernelContext* ctx, int i, TF_Tensor** tensor,
     return;
   }
   const ::tensorflow::Tensor& cc_tensor(cc_ctx->input(i));
+  if ((&cc_tensor) == nullptr) {
+    *tensor = nullptr;
+    return;
+  }
   TF_Tensor* result =
       ::tensorflow::TF_TensorFromTensor(cc_tensor, &status->status);
   if (TF_GetCode(status) == TF_OK) {
