bf79252333c23a92239e7eb8b764b1e95c7bfdef
Bhavani Subramanian <bhavani1.subramanian@intel.com>
2023-05-09 17:18:20 -0700

    [oneDNN v3.x]: Fixed a bug in reorder primitive cache

--- tensorflow/core/util/mkl_util.h
+++ tensorflow/core/util/mkl_util.h
@@ -2208,11 +2208,13 @@ class MklReorderPrimitiveFactory : public MklPrimitiveFactory<T> {
     key_creator.AddAsKey(from_inner_idxs);
     key_creator.AddAsKey(static_cast<int>(from_desc.get_data_type()));
     key_creator.AddAsKey(from_dims);
+    key_creator.AddAsKey(from_strides);
     key_creator.AddAsKey(static_cast<int>(to_inner_nblks));
     key_creator.AddAsKey(to_inner_blks);
     key_creator.AddAsKey(to_inner_idxs);
     key_creator.AddAsKey(static_cast<int>(to_desc.get_data_type()));
     key_creator.AddAsKey(to_dims);
+    key_creator.AddAsKey(to_strides);
 #endif  // !ENABLE_ONEDNN_V3
     return key_creator.GetKey();
   }
