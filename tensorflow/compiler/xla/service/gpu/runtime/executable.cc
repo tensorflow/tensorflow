cc7a62d3b2a8242873b6f3648f17a0dca89e757a
Eugene Zhulenev <ezhulenev@google.com>
2023-05-10 15:10:08 -0700

    [xla:runtime] Do not depend on weak symbols in FFI module
    Fix for: https://github.com/google/jax/issues/14466
    
    PiperOrigin-RevId: 531016451

--- tensorflow/compiler/xla/service/gpu/runtime/executable.cc
+++ tensorflow/compiler/xla/service/gpu/runtime/executable.cc
@@ -51,24 +51,6 @@ limitations under the License.
 #endif  // #if GOOGLE_CUDA
 
 namespace xla {
-
-#if GOOGLE_CUDA
-namespace runtime {
-namespace ffi {
-
-// Override weak symbol defined in the `xla/runtime/ffi.cc` with a strong one
-// that provides implementation for the XLA:GPU backend.
-XLA_FFI_Stream* GetXlaFfiStream(const CustomCall::UserData* user_data,
-                                const DiagnosticEngine* diagnostic) {
-  auto run_opts = user_data->getIfExists<const ServiceExecutableRunOptions>();
-  auto stream = se::gpu::AsGpuStreamValue(run_opts->stream());
-  return reinterpret_cast<XLA_FFI_Stream*>(stream);
-}
-
-}  // namespace ffi
-}  // namespace runtime
-#endif  // GOOGLE_CUDA
-
 namespace gpu {
 
 using ::xla::runtime::CustomCallAttrEncodingSet;
@@ -79,11 +61,28 @@ using ::xla::runtime::success;
 using ::xla::runtime::Tagged;
 using ::xla::runtime::TypeIDNameRegistry;
 
+using ::xla::runtime::CustomCall;
+using ::xla::runtime::DiagnosticEngine;
 using ::xla::runtime::ExportModules;
 using ::xla::runtime::ffi::ExportFfiModules;
 using ::xla::runtime::ffi::FfiStateVector;
+using ::xla::runtime::ffi::RegisterXlaFfiStreamProvider;
+
+#if GOOGLE_CUDA
+static XLA_FFI_Stream* GetXlaFfiGpuStream(const CustomCall::UserData* user_data,
+                                          const DiagnosticEngine* diagnostic) {
+  auto run_opts = user_data->getIfExists<const ServiceExecutableRunOptions>();
+  if (!run_opts) return nullptr;
+  auto stream = se::gpu::AsGpuStreamValue(run_opts->stream());
+  return reinterpret_cast<XLA_FFI_Stream*>(stream);
+}
+#endif  // GOOGLE_CUDA
 
 void RegisterXlaGpuRuntimeCustomCalls(DirectCustomCallRegistry& registry) {
+#if GOOGLE_CUDA
+  RegisterXlaFfiStreamProvider(GetXlaFfiGpuStream);
+#endif  // GOOGLE_CUDA
+
   RegisterKernelLaunchCustomCalls(registry);
   RegisterTracingCustomCalls(registry);
   RegisterFftCustomCalls(registry);
