c493abb830af988a6b0b011bd89f2b9e207a7e1c
Feng Wang <wffw@google.com>
2023-05-10 17:40:29 -0700

    Fix GetHostname in repository.cc
    PiperOrigin-RevId: 531051540

---
+++ tensorflow/core/profiler/convert/repository_test.cc
@@ -0,0 +1,42 @@
+/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/profiler/convert/repository.h"
+
+#include <optional>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "tensorflow/tsl/profiler/protobuf/xplane.pb.h"
+
+namespace tensorflow {
+namespace profiler {
+namespace {
+
+using ::testing::Eq;
+
+TEST(Repository, GetHostName) {
+  auto session_snapshot_or =
+      SessionSnapshot::Create({"log/plugins/profile/hostname0.xplane.pb",
+                               "log/plugins/profile/hostname1.xplane.pb"},
+                              /*xspaces=*/std::nullopt);
+  TF_CHECK_OK(session_snapshot_or.status());
+  EXPECT_THAT(session_snapshot_or.value().GetHostname(0), Eq("hostname0"));
+  EXPECT_THAT(session_snapshot_or.value().GetHostname(1), Eq("hostname1"));
+}
+
+}  // namespace
+}  // namespace profiler
+}  // namespace tensorflow
