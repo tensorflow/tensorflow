15f85c0d2ba99118711e0095d16107cfec807cc3
Emilio Cota <ecg@google.com>
2023-05-15 20:01:25 -0700

    [xla:tf2xla] BufferInfo: embed result parameter info
    Some buffers are both input and output parameters.
    In order to embed this information without increasing the size
    of the encoded BufferInfo, we lower the length from i64 to i32
    for the parameter numbers.
    
    PiperOrigin-RevId: 532303133

--- tensorflow/compiler/aot/codegen_test.cc
+++ tensorflow/compiler/aot/codegen_test.cc
@@ -215,18 +215,22 @@ TEST(CodegenTest, Golden) {
   CompileResult compile_result;
   compile_result.aot.reset(new xla::cpu::CpuAotCompilationResult(
       {},
-      {BufferInfo::MakeTempBuffer(1),
-       BufferInfo::MakeEntryParameter(/*size=*/8, /*param_number=*/0),
+      {BufferInfo::MakeTempBuffer(3 * 8),
+       BufferInfo::MakeEntryParameter(/*size=*/8, /*entry_param_number=*/0),
        BufferInfo::MakeTempBuffer(1),
-       BufferInfo::MakeEntryParameter(/*size=*/96, /*param_number=*/1),
+       BufferInfo::MakeEntryParameter(/*size=*/96, /*entry_param_number=*/1),
        BufferInfo::MakeTempBuffer(1),
-       BufferInfo::MakeEntryParameter(/*size=*/96, /*param_number=*/2),
+       BufferInfo::MakeEntryParameter(/*size=*/96, /*entry_param_number=*/2),
        BufferInfo::MakeTempBuffer(1),
-       BufferInfo::MakeEntryParameter(/*size=*/96, /*param_number=*/3),
-       BufferInfo::MakeTempBuffer(1),
-       BufferInfo::MakeEntryParameter(/*size=*/96, /*param_number=*/4),
-       BufferInfo::MakeTempBuffer(1), BufferInfo::MakeTempBuffer(120)},
-      11, {}));
+       BufferInfo::MakeEntryParameter(/*size=*/96, /*entry_param_number=*/3),
+       BufferInfo::MakeResultParameter(/*size=*/5 * 6 * 4,
+                                       /*result_param_number=*/0),
+       BufferInfo::MakeEntryParameter(/*size=*/96, /*entry_param_number=*/4),
+       BufferInfo::MakeResultParameter(/*size=*/1 * 4,
+                                       /*result_param_number=*/1),
+       BufferInfo::MakeResultParameter(/*size=*/5 * 4,
+                                       /*result_param_number=*/2)},
+      0, {}));
   compile_result.program_shape =
       xla::ShapeUtil::MakeProgramShape(
           {
