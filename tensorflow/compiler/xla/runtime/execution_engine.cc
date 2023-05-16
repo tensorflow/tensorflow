589e80656fb5e5c3a8a83eeb895cf9c8d51e8d4f
Emilio Cota <ecg@google.com>
2023-05-15 23:00:10 -0700

    [xla:runtime] execution_engine: introduce ExportWithXlaRuntimeAbi
    This will soon be needed by XLA:CPU-Next's AOT flow.
    
    PiperOrigin-RevId: 532339403

--- tensorflow/compiler/xla/runtime/execution_engine.cc
+++ tensorflow/compiler/xla/runtime/execution_engine.cc
@@ -99,17 +99,15 @@ static std::string GetExportedName(std::string_view name) {
   return StrFormat("__xla__%s", name);
 }
 
-// Converts exported function to an interface function that wraps all the
-// arguments of the original function into an i8** pointer to provide a function
-// with trivial ABI.
-static absl::Status SetUpExportedFunction(llvm::Module &module,
-                                          std::string_view function_name) {
+absl::Status ExportWithXlaRuntimeAbi(llvm::Module &module,
+                                     std::string_view original_name,
+                                     std::string_view exported_name) {
   llvm::IRBuilder<> builder(module.getContext());
 
   // Check that we have a function with a valid type.
-  llvm::Function *func = module.getFunction(function_name);
+  llvm::Function *func = module.getFunction(original_name);
   if (!func)
-    return InternalError("exported function not found: %s", function_name);
+    return InternalError("exported function not found: %s", original_name);
   if (!func->getReturnType()->isVoidTy())
     return InternalError("exported function must return void");
 
@@ -118,8 +116,8 @@ static absl::Status SetUpExportedFunction(llvm::Module &module,
       builder.getVoidTy(), builder.getInt8PtrTy()->getPointerTo(),
       /*isVarArg=*/false);
 
-  llvm::FunctionCallee xla_runtime_func = module.getOrInsertFunction(
-      GetExportedName(func->getName()), xla_runtime_type);
+  llvm::FunctionCallee xla_runtime_func =
+      module.getOrInsertFunction(exported_name, xla_runtime_type);
 
   llvm::Function *callee = cast<llvm::Function>(xla_runtime_func.getCallee());
   llvm::Value *packed_args = callee->arg_begin();
@@ -245,10 +243,13 @@ ExecutionEngine::CreateFromModule(std::unique_ptr<llvm::LLVMContext> ctx,
 
   // Set up exported functions interface functions in the LLVM module.
   for (std::string_view name : exported) {
-    if (auto status = SetUpExportedFunction(*module, name); !status.ok())
+    if (auto status =
+            ExportWithXlaRuntimeAbi(*module, name, GetExportedName(name));
+        !status.ok()) {
       return InternalError(
           "failed to set up exported function %s interface: %s", name,
           status.message());
+    }
   }
 
   // Run an optimization pipeline over the LLVM module (alway run with default
