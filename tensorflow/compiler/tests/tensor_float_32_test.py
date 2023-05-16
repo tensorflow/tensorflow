0cc6fa08ecb157fd34463279aaba81e7d16b471d
Wen Chen <Wen.Chen@amd.com>
2023-05-16 18:31:48 +0000

    Use fp32 compute result as the reference for tf32 test in ROCm as MIOpen does not support fp64.

--- tensorflow/compiler/tests/tensor_float_32_test.py
+++ tensorflow/compiler/tests/tensor_float_32_test.py
@@ -22,6 +22,7 @@ from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.platform import googletest
+from tensorflow.python.platform import sysconfig
 
 
 class TensorFloat32ConvTest(xla_test.XLATestCase):
@@ -40,8 +41,13 @@ class TensorFloat32ConvTest(xla_test.XLATestCase):
 
       # Test the output is sufficiently precise by comparing with FP64 results
       out = compiled_fn(*inputs)
-      f64_out = compiled_fn(*[math_ops.cast(x, 'float64') for x in inputs])
-      self.assertAllClose(out, f64_out, rtol=1e-5, atol=1e-5)
+      sys_details = sysconfig.get_build_info()
+      if sys_details["is_rocm_build"]: # MIOpen does not support fp64 data type
+          f32_out = compiled_fn(*[math_ops.cast(x, 'float32') for x in inputs])
+          self.assertAllClose(out, f32_out, rtol=1e-5, atol=1e-5)
+      else:
+          f64_out = compiled_fn(*[math_ops.cast(x, 'float64') for x in inputs])
+          self.assertAllClose(out, f64_out, rtol=1e-5, atol=1e-5)
 
       # Test with TF32 enabled. Recompile fn because enabling TF32 does not
       # reset function cache.
