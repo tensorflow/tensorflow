dbb09377a191d39678e82a4050b73f12c4e0f663
Rahul Joshi <jurahul@google.com>
2023-05-19 16:02:56 -0700

    [NFC] Fix some spellings.
    PiperOrigin-RevId: 533573691

--- tensorflow/compiler/xla/mlir/backends/gpu/transforms/lmhlo_to_gpu_runtime.cc
+++ tensorflow/compiler/xla/mlir/backends/gpu/transforms/lmhlo_to_gpu_runtime.cc
@@ -501,12 +501,12 @@ using mlir::lmhlo_gpu::ReduceScatterStartOp;
 // TODO(ezhulenev): Once XLA runtime custom calls support returning values, we
 // should explicitly return event id from the `Start` custom call, and pass it
 // to the `Done` custom call. Longer term this should become an `!async.token`
-// and rely on XLA runtime asynchonous execution.
+// and rely on XLA runtime asynchronous execution.
 class CollectiveUidGenerator {
  public:
   CollectiveUidGenerator() : cnt_(0) {}
 
-  // Assings a unique event id to the pair of start and done operations.
+  // Assigns a unique event id to the pair of start and done operations.
   int32_t AssignUid(Operation* start, Operation* done) {
     int32_t id = next();
     uids_[start] = id;
@@ -821,7 +821,7 @@ class CollectiveOpLowering : public OpRewritePattern<CollectiveOp> {
     if (!is_async) {
       erase_done_op();
     } else {
-      // For asynchonous start operation we need to produce a fake token, that
+      // For asynchronous start operation we need to produce a fake token, that
       // will be later removed, because corresponding `done` operation doesn't
       // have a token argument. We rely on the `unrealized_conversion_cast`
       // operation to create a fake token from the `i8` constant, and on the
