e015b4da6eff754dc51a1dfca2b333cd2aff987a
Suxin Guo <suxinguo@google.com>
2023-05-22 19:43:00 -0700

    Add prepare pattern to rewrite TF_Select to TF_SelectV2 in the case of the inputs not all being static and equal.
    TF_Select does not support broadcasting whereas TF_SelectV2 does. Broadcasting might be needed at runtime if stapes are not fully known ahead of time so we need TF_SelectV2. See tensorflow/ir/tf_ops.td for more op details.
    
    PiperOrigin-RevId: 534260994

--- tensorflow/compiler/mlir/lite/utils/utils.h
+++ tensorflow/compiler/mlir/lite/utils/utils.h
@@ -16,8 +16,6 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_MLIR_LITE_UTILS_UTILS_H_
 #define TENSORFLOW_COMPILER_MLIR_LITE_UTILS_UTILS_H_
 
-#include <numeric>
-
 #include "llvm/ADT/ArrayRef.h"
 #include "mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
 #include "mlir/IR/Operation.h"  // from @llvm-project
@@ -30,31 +28,26 @@ using mlir::Operation;
 using mlir::ShapedType;
 using mlir::Value;
 
-// Returns true if each Operand at given indices have the same static shape.
-inline bool OpHasSameStaticShapes(Operation* op,
-                                  llvm::ArrayRef<int> operand_idxs) {
-  if (op->getNumOperands() == 0 || operand_idxs.empty()) return true;
-  const int first_opr_idx = operand_idxs[0];
-  ArrayRef<int64_t> shape =
-      op->getOperand(first_opr_idx).getType().dyn_cast<ShapedType>().getShape();
-  for (int opr_idx : operand_idxs) {
-    Value operand = op->getOperand(opr_idx);
-    auto shaped_type = operand.getType().dyn_cast<ShapedType>();
+// Returns true if all tensor value in `values` has static shape and same shape.
+inline bool OpHasSameStaticShapes(Operation* op) {
+  auto values = op->getOperands();
+  int operand_num = 0;
+  ArrayRef<int64_t> shape;
+  for (Value value : values) {
+    auto shaped_type = value.getType().dyn_cast<ShapedType>();
     if (!shaped_type || !shaped_type.hasStaticShape()) {
       return false;
     }
-    if (shape != shaped_type.getShape()) {
-      return false;
+    if (operand_num == 0) {
+      shape = shaped_type.getShape();
+    } else {
+      if (shape != shaped_type.getShape()) {
+        return false;
+      }
     }
+    ++operand_num;
   }
   return true;
-}
-
-// Returns true if each Operand has the same static shape.
-inline bool OpHasSameStaticShapes(Operation* op) {
-  llvm::OwningArrayRef<int> operand_idxs(op->getNumOperands());
-  std::iota(operand_idxs.begin(), operand_idxs.end(), 0);
-  return OpHasSameStaticShapes(op, operand_idxs);
 }
 }  // namespace TFL
 }  // namespace mlir
