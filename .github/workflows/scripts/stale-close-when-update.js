0daff340aa3c9167e1ddbaf6a5182fb092f7f76d
“shmishra99” <shivammishr@google.com>
2023-05-23 18:03:29 +0530

    Fix close stale issue when updated by user in stale-management

--- .github/workflows/scripts/stale-close-when-update.js
+++ .github/workflows/scripts/stale-close-when-update.js
@@ -21,13 +21,13 @@ module.exports = async ({ github, context }) => {
         let nodeType = issueList[i].node_id
         let issueLabels = issueList[i].labels
         let stringLabel = JSON.stringify(issueLabels)
-        console.log("issue list",issueList[i])
+        console.log("issue list", issueList[i])
         let closeAfterStale = 7
-        if(nodeType.startsWith('PR'))
-           closeAfterStale = 14
-        else if(stringLabel.indexOf('stat:contribution welcome') !=-1 || stringLabel.indexOf('stat:good first issue') !=-1 )
-           closeAfterStale = 365
-       
+        if (nodeType.startsWith('PR'))
+            closeAfterStale = 14
+        else if (stringLabel.indexOf('stat:contribution welcome') != -1 || stringLabel.indexOf('stat:good first issue') != -1)
+            closeAfterStale = 365
+
         //fetch all the events inside the issue.
         let resp = await github.rest.issues.listEventsForTimeline({
             owner: context.repo.owner,
@@ -38,9 +38,9 @@ module.exports = async ({ github, context }) => {
         let events = resp.data;
         let closeIssue = false
         for (let i = 0; i < events.length; i++) {
-            let event_details = events[i];  
+            let event_details = events[i];
             // Check if issue is marked as stale.    
-            if (event_details.event == 'labeled' && event_details.label && event_details.label.name == "stale") {  
+            if (event_details.event == 'labeled' && event_details.label && event_details.label.name == "stale") {
                 let currentDate = new Date();
                 let labeledDate = new Date(event_details.created_at)
                 let timeInDays = (currentDate - labeledDate) / 86400000
@@ -48,28 +48,28 @@ module.exports = async ({ github, context }) => {
                 if (timeInDays > closeAfterStale)
                     closeIssue = true
             }
-            if (event_details.event == 'unlabeled' && event_details.label && event_details.label.name == "stale"){
+            if (event_details.event == 'unlabeled' && event_details.label && event_details.label.name == "stale") {
                 console.log(`Stale is unlabel for issue ${number}.`)
-                    closeIssue = false
+                closeIssue = false
             }
-           
+
         }
-        if(closeIssue){
+        if (closeIssue) {
             console.log(`Closing the issue ${number} more then 7 days old with stale label.`)
             let comment = "This issue was closed because it has been inactive for 7 days since being marked as stale. Please reopen if you'd like to work on this further."
-            if(closeAfterStale == 14)
-               comment = "This PR was closed because it has been inactive for 14 days since being marked as stale. Please reopen if you'd like to work on this further."
-            else if(closeAfterStale == 365)
-              comment = "This issue was closed because it has been inactive for 1 year"
+            if (closeAfterStale == 14)
+                comment = "This PR was closed because it has been inactive for 14 days since being marked as stale. Please reopen if you'd like to work on this further."
+            else if (closeAfterStale == 365)
+                comment = "This issue was closed because it has been inactive for 1 year"
 
             await github.rest.issues.update({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
                 issue_number: number,
-                state:"closed"
-              });
-              
-              await github.rest.issues.createComment({
+                state: "closed"
+            });
+
+            await github.rest.issues.createComment({
                 issue_number: number,
                 owner: context.repo.owner,
                 repo: context.repo.repo,
