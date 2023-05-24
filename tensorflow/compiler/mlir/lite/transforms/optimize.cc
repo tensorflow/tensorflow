be44c4b773f27ab6a9121967274d06299c5bac51
David Rim <davidrim@google.com>
2023-05-24 15:19:38 -0700

    Allow dynamic shape for reshape-equal to one-hot transform if the unknown dimension is the first dimension and remaining dimensions are 1.
    PiperOrigin-RevId: 534999601

--- tensorflow/compiler/mlir/lite/transforms/optimize.cc
+++ tensorflow/compiler/mlir/lite/transforms/optimize.cc
@@ -480,8 +480,12 @@ Value ReshapeValueDroppingLastDim(OpBuilder &builder, Value value) {
   // so we could cast safely here.
   auto type = value.getType().cast<ShapedType>();
   SmallVector<int> new_shape;
-  for (int64_t dim : type.getShape().drop_back()) {
-    new_shape.push_back(dim);
+  if (type.hasStaticShape()) {
+    for (int64_t dim : type.getShape().drop_back()) {
+      new_shape.push_back(dim);
+    }
+  } else {
+    new_shape.push_back(-1);
   }
   return builder.create<ReshapeOp>(
       value.getLoc(), value,
