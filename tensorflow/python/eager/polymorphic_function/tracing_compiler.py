6b984884a59c49669b6acd085f1c6e2fdf343258
Fiona Lang <flang@google.com>
2023-05-26 16:41:05 -0700

    Turn lazy load in tracing_compiler into a regular import.
    PiperOrigin-RevId: 535743373

--- tensorflow/python/eager/polymorphic_function/tracing_compiler.py
+++ tensorflow/python/eager/polymorphic_function/tracing_compiler.py
@@ -22,6 +22,7 @@ from tensorflow.core.function import trace_type
 from tensorflow.core.function.capture import capture_container
 from tensorflow.core.function.polymorphism import function_cache
 from tensorflow.core.function.polymorphism import function_type as function_type_lib
+from tensorflow.python.autograph.core import ag_ctx
 from tensorflow.python.eager import monitoring
 from tensorflow.python.eager.polymorphic_function import attributes as attributes_lib
 from tensorflow.python.eager.polymorphic_function import concrete_function as concrete_function_lib
@@ -33,17 +34,7 @@ from tensorflow.python.framework import ops
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.profiler import trace
 from tensorflow.python.util import compat
-from tensorflow.python.util import lazy_loader
 
-
-# Loaded lazily due to a circular dependency (roughly
-# tf.function->autograph->->dataset->tf.function).
-# TODO(b/133251390): Use a regular import.
-ag_ctx = lazy_loader.LazyLoader(
-    "ag_ctx",
-    globals(),
-    "tensorflow.python.autograph.core.ag_ctx",
-)
 
 _graph_building_time_counter = monitoring.Counter(
     "/tensorflow/core/tf_function/graph_building_time_usecs",
