4fd358de2f2615546f9e43abb5cfaa055bf9db6f
Thai Nguyen <thaink@google.com>
2023-06-01 17:38:39 -0700

    Preserving `assets.extra` directory from the input saved model
    This directory usually contains the widely used `tf_serving_warmup_requests`.
    
    PiperOrigin-RevId: 537174104

--- tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/integration_test/quantize_model_test.py
@@ -2733,6 +2733,53 @@ class StaticRangeQuantizationTest(quantize_model_test_base.QuantizedModelTest):
     output_graphdef = output_loader.get_meta_graph_def_from_tags(tags).graph_def
     self.assertTrue(self._contains_quantized_function_call(output_graphdef))
 
+  def test_model_ptq_preserving_assets_extra(self):
+    self._create_matmul_model(
+        input_shape=(1, 1024),
+        weight_shape=(1024, 3),
+        saved_model_path=self._input_saved_model_path,
+    )
+    asset_filename = 'assets.extra/tf_serving_warmup_requests'
+    file_io.create_dir_v2(
+        os.path.join(self._input_saved_model_path, 'assets.extra')
+    )
+    file_io.write_string_to_file(
+        filename=os.path.join(self._input_saved_model_path, asset_filename),
+        file_content='Test content',
+    )
+
+    quantization_options = quant_opts_pb2.QuantizationOptions(
+        quantization_method=quant_opts_pb2.QuantizationMethod(
+            experimental_method=_ExperimentalMethod.STATIC_RANGE
+        ),
+        op_set=quant_opts_pb2.TF,
+    )
+    tags = {tag_constants.SERVING}
+
+    # Use plain python lists as representative samples.
+    representative_dataset = [
+        {
+            'input_tensor': [[i * 0.1 for i in range(1024)]],
+        }
+        for _ in range(4)
+    ]
+
+    converted_model = quantize_model.quantize(
+        self._input_saved_model_path,
+        ['serving_default'],
+        tags,
+        self._output_saved_model_path,
+        quantization_options=quantization_options,
+        representative_dataset=representative_dataset,
+    )
+    self.assertIsNotNone(converted_model)
+    # Check if the assets.extra file exists in the output model.
+    self.assertTrue(
+        file_io.file_exists_v2(
+            os.path.join(self._output_saved_model_path, asset_filename)
+        )
+    )
+
   # tf.data.Dataset is as an Iterable (thus can be used as representative
   # dataset) only in TF2 (eager mode).
   @test_util.run_v2_only
