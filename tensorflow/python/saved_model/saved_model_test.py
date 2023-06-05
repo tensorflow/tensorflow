a2a1a9ea5ef9f75c0759968b2ba54bd2c121cece
Adam Cogdell <adamcogdell@google.com>
2023-06-05 14:46:28 -0700

    Internal change only.
    PiperOrigin-RevId: 537986580

--- tensorflow/python/saved_model/saved_model_test.py
+++ tensorflow/python/saved_model/saved_model_test.py
@@ -199,7 +199,7 @@ class SavedModelTest(SavedModelTestBase):
       f.write("invalid content")
     with self.session(graph=ops.Graph()) as sess:
       with self.assertRaisesRegex(
-          IOError, "Cannot parse file.*%s" % constants.SAVED_MODEL_FILENAME_PB):
+          IOError, "Cannot parse file.*Requested.*bytes but only read 0"):
         loader.load(sess, ["foo"], export_dir)
 
     # Cleanup the directory and start again.
