9af8528173225b00c527677524daa3d89f99419a
Hossein <hosseinb@google.com>
2023-06-06 10:42:45 -0700

    Create msys2.ps1

---
+++ tensorflow/tools/tf_windows_build_dockerfiles/msys2.ps1
@@ -0,0 +1,5 @@
+(New-Object Net.WebClient).DownloadFile('https://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20220603.tar.xz', 'msys2.tar.xz'); `
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList 'x msys2.tar.xz -oC:\tmp\msys2.tar' -Wait; `
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList 'x C:\tmp\msys2.tar -oC:\tools' -Wait; `
+$env:PATH = [Environment]::GetEnvironmentVariable('PATH', 'Machine') + ';C:\tools\msys64;C:\tools\msys64\usr\bin\'; `
+[Environment]::SetEnvironmentVariable('PATH', $env:PATH, 'Machine');
