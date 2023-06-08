86b058aed88b2edd8f6e8c31fd4315411d7647de
Hossein <hosseinb@google.com>
2023-06-08 17:20:51 +0000

    Delete visualstudio.bat

--- tensorflow/tools/tf_windows_build_dockerfiles/visualstudio.bat
+++
@@ -1,1 +0,0 @@
-C:\tmp\vs_community.exe --quiet --wait --norestart --nocache --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.14.29.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.20348
