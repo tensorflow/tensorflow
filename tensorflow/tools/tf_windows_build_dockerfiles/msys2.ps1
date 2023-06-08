dfa890251a05c03755244f6f4b94cfc9a254ba57
Hossein <hosseinb@google.com>
2023-06-08 14:23:05 -0700

    Update msys2.ps1
    Adding header comment

--- tensorflow/tools/tf_windows_build_dockerfiles/msys2.ps1
+++ tensorflow/tools/tf_windows_build_dockerfiles/msys2.ps1
@@ -1,3 +1,4 @@
+# These lines consist of sub-commands that are executed using a single RUN command in the Dockerfile to install msys2, and add some extra tools.
 (New-Object Net.WebClient).DownloadFile('https://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20220603.tar.xz', 'msys2.tar.xz'); `
 Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList 'x msys2.tar.xz -oC:\tmp\msys2.tar' -Wait; `
 Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList 'x C:\tmp\msys2.tar -oC:\tools' -Wait; `
