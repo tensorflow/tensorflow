e8cecf5baa16842299c0692b39d8777f82257361
Varghese, Jojimon <jojimon.varghese@intel.com>
2023-06-08 16:02:03 -0700

    Changes to add bfloat16 support for image rotation

--- tensorflow/core/kernels/image/image_ops.cc
+++ tensorflow/core/kernels/image/image_ops.cc
@@ -39,6 +39,7 @@ template struct FillProjectiveTransform<CPUDevice, int64_t>;
 template struct FillProjectiveTransform<CPUDevice, Eigen::half>;
 template struct FillProjectiveTransform<CPUDevice, float>;
 template struct FillProjectiveTransform<CPUDevice, double>;
+template struct FillProjectiveTransform<CPUDevice, bfloat16>;
 
 }  // end namespace functor
 
@@ -163,6 +164,7 @@ TF_CALL_int64(REGISTER);
 TF_CALL_half(REGISTER);
 TF_CALL_float(REGISTER);
 TF_CALL_double(REGISTER);
+TF_CALL_bfloat16(REGISTER);
 
 #undef REGISTER
 
@@ -186,6 +188,7 @@ TF_CALL_int64(REGISTER);
 TF_CALL_half(REGISTER);
 TF_CALL_float(REGISTER);
 TF_CALL_double(REGISTER);
+TF_CALL_bfloat16(REGISTER);
 
 #undef REGISTER
 
