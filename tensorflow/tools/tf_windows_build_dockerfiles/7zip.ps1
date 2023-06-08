842527805b22f01c347d824a2edf5e80ec63dc50
Hossein <hosseinb@google.com>
2023-06-08 14:16:38 -0700

    Update 7zip.ps1
    Adding header comment

--- tensorflow/tools/tf_windows_build_dockerfiles/7zip.ps1
+++ tensorflow/tools/tf_windows_build_dockerfiles/7zip.ps1
@@ -1,3 +1,4 @@
+# These lines consist of sub-commands that are executed using a single RUN command in the Dockerfile to install 7-Zip.
 (New-Object Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z2201-x64.msi", "7z.msi"); `
 Start-Process -FilePath msiexec.exe -ArgumentList "/i 7z.msi /qn /norestart /log C:\tmp\7z_install_log.txt" -Wait; `
 Remove-Item .\7z.msi; `
