b1ed4c6b2db92ba7096c4573bab0e0ac3a73df6c
Hossein <hosseinb@google.com>
2023-06-08 14:28:56 -0700

    Update vs.bat
    Adding header comment

--- tensorflow/tools/tf_windows_build_dockerfiles/vs.bat
+++ tensorflow/tools/tf_windows_build_dockerfiles/vs.bat
@@ -1,1 +1,2 @@
+rem Install Visual Studio 2019 Build Tools.
 C:\tmp\vs_community.exe --quiet --wait --norestart --nocache --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.14.29.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.20348
