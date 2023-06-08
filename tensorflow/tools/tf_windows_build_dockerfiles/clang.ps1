f01e6041d8273ba4d8d59632781a1f99f838ea6b
Hossein <hosseinb@google.com>
2023-06-08 14:19:06 -0700

    Update clang.ps1
    Adding header comment.

--- tensorflow/tools/tf_windows_build_dockerfiles/clang.ps1
+++ tensorflow/tools/tf_windows_build_dockerfiles/clang.ps1
@@ -1,3 +1,4 @@
+# These lines consist of sub-commands that are executed using a single RUN command in the Dockerfile to install Clang.
 (New-Object Net.WebClient).DownloadFile("https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/LLVM-16.0.4-win64.exe", "LLVM.exe"); `
 Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "x LLVM.exe -oC:\tools\llvm" -Wait; `
 $env:PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine") + ";C:\tools\llvm\bin"; `
