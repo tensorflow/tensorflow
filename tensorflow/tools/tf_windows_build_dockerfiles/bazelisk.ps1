6b1fdfc20dd51000c7401b0875915a2c152a1e83
Hossein <hosseinb@google.com>
2023-06-08 14:18:01 -0700

    Update bazelisk.ps1
    Adding header comment

--- tensorflow/tools/tf_windows_build_dockerfiles/bazelisk.ps1
+++ tensorflow/tools/tf_windows_build_dockerfiles/bazelisk.ps1
@@ -1,3 +1,4 @@
+# These lines consist of sub-commands that are executed using a single RUN command in the Dockerfile to install Bazelisk. 
 (New-Object Net.WebClient).DownloadFile( `
      "https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-windows-amd64.exe", `
      "C:\tools\bazel\bazel.exe"); `
