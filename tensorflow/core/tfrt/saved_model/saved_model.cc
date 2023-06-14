04e07712f54ca9f7f244f2e5d9926c3a7996ceb1
Juanli Shen <juanlishen@google.com>
2023-06-13 18:27:17 -0700

    Suppress errors upon MLIR uploading failures
    PiperOrigin-RevId: 540132398

--- tensorflow/core/tfrt/saved_model/saved_model.cc
+++ tensorflow/core/tfrt/saved_model/saved_model.cc
@@ -627,8 +627,7 @@ SavedModelImpl::LoadSavedModel(Options options,
           options.graph_execution_options.compile_options.use_bridge_for_gpu));
   // TODO(b/278143179): Upload module w/o control flow.
   SymbolUids symbol_uids;
-  ASSIGN_OR_RETURN_IN_IMPORT(symbol_uids.tf_symbol_uid,
-                             MaybeUploadMlirToXsymbol(mlir_module.get()));
+  symbol_uids.tf_symbol_uid = MaybeUploadMlirToXsymbol(mlir_module.get());
 
   const auto import_duration = absl::Now() - import_start_time;
   saved_model_import_time_seconds->GetCell(std::string(saved_model_dir))
@@ -659,8 +658,7 @@ SavedModelImpl::LoadSavedModel(Options options,
         options.graph_execution_options.compile_options, mlir_module.get(),
         &bef, fallback_state.get()));
   }
-  ASSIGN_OR_RETURN_IN_COMPILE(symbol_uids.tfrt_symbol_uid,
-                              MaybeUploadMlirToXsymbol(mlir_module.get()));
+  symbol_uids.tfrt_symbol_uid = MaybeUploadMlirToXsymbol(mlir_module.get());
   const auto compile_duration = absl::Now() - compile_start_time;
   saved_model_compile_time_seconds->GetCell(std::string(saved_model_dir))
       ->Set(absl::ToInt64Seconds(compile_duration));
@@ -1054,8 +1052,7 @@ SavedModelImpl::LoadJoinedSignature(const JoinedSignature& joined_signature) {
                                   joined_signature.target_nodes));
   // TODO(b/278143179): Upload module w/o control flow.
   SymbolUids symbol_uids;
-  ASSIGN_OR_RETURN_IN_IMPORT(symbol_uids.tf_symbol_uid,
-                             MaybeUploadMlirToXsymbol(module.get()));
+  symbol_uids.tf_symbol_uid = MaybeUploadMlirToXsymbol(module.get());
 
   // Step 2: Compile the MLIR module from TF dialect to TFRT dialect (in BEF).
   auto loading_result = std::make_unique<LoadingResult>();
@@ -1067,8 +1064,7 @@ SavedModelImpl::LoadJoinedSignature(const JoinedSignature& joined_signature) {
   RETURN_IF_ERROR_IN_COMPILE(tensorflow::ConvertTfMlirToBef(
       options_.graph_execution_options.compile_options, module.get(),
       &loading_result->bef, fallback_state_.get()));
-  ASSIGN_OR_RETURN_IN_COMPILE(symbol_uids.tfrt_symbol_uid,
-                              MaybeUploadMlirToXsymbol(module.get()));
+  symbol_uids.tfrt_symbol_uid = MaybeUploadMlirToXsymbol(module.get());
   loading_result->symbol_uids = std::move(symbol_uids);
 
   // Step 3: Initialize runtime states using special BEF functions.
