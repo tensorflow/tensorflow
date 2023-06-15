59c65828f37dbfcda829238e0eed690ec666bff4
Anton S <anton@unity3d.com>
2023-06-15 13:42:10 +0300

    fix mistype in the mixed_bfloat16 policy type

--- tensorflow/python/keras/mixed_precision/policy.py
+++ tensorflow/python/keras/mixed_precision/policy.py
@@ -192,7 +192,7 @@ class Policy(object):
       raise TypeError("'name' must be a string, but got: %s" % (name,))
     self._name = name
     self._compute_dtype, self._variable_dtype = self._parse_name(name)
-    if name in ('mixed_float16', 'mixed_bloat16'):
+    if name in ('mixed_float16', 'mixed_bfloat16'):
       device_compatibility_check.log_device_compatibility_check(name)
 
   def _parse_name(self, name):
