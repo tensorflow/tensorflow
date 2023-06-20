e281388458904ee55d205b3888a373de7638805d
Yang Chen <yangchen@google.com>
2023-06-20 10:23:45 -0700

    #tf-data-service distributed_save_ft_test reuses snapshot path utils from C++.
    PiperOrigin-RevId: 541970923

--- tensorflow/python/data/experimental/kernel_tests/service/distributed_save_ft_test.py
+++ tensorflow/python/data/experimental/kernel_tests/service/distributed_save_ft_test.py
@@ -23,6 +23,7 @@ from absl.testing import parameterized
 
 from tensorflow.python.data.experimental.kernel_tests.service import test_base as data_service_test_base
 from tensorflow.python.data.experimental.ops import distributed_save_op
+from tensorflow.python.data.experimental.service import _pywrap_snapshot_utils
 from tensorflow.python.data.kernel_tests import test_base
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.data.ops import test_mode
@@ -76,11 +77,13 @@ def get_stream_assignments(
 
 
 def snapshot_is_done(path):
-  return os.path.exists(os.path.join(path, "DONE"))
+  return os.path.exists(
+      _pywrap_snapshot_utils.TF_DATA_SnapshotDoneFilePath(path))
 
 
 def snapshot_has_error(path):
-  return os.path.exists(os.path.join(path, "ERROR"))
+  return os.path.exists(
+      _pywrap_snapshot_utils.TF_DATA_SnapshotErrorFilePath(path))
 
 
 def snapshots_are_done(paths):
@@ -266,6 +269,7 @@ class SnapshotFtTest(data_service_test_base.TestBase, parameterized.TestCase):
 
   @combinations.generate(test_base.default_test_combinations())
   def testWorkersDontExceedMaxStreamAssignments(self):
+    # TODO(b/250921378): Fix flaky test.
     num_workers = 3
     num_snapshots = 12
     cluster = data_service_test_base.TestCluster(num_workers=num_workers)
