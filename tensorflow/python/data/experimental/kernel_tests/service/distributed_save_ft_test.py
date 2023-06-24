4802a5081094f6406b9c947e4beb756eac74dc2f
Yang Chen <yangchen@google.com>
2023-06-23 18:46:44 -0700

    #tf-data-service Implement a TODO to verify element counts in ft tests.
    Also fix a bug that when we commit a chunk, the old chunk is not deleted.
    Without this fix, when we renamed `chunk_0_0_x` from uncommitted chunks
    to committed chunks, we did not delete previous `chunk_0_0_y` chunks,
    resulting in duplicate elements.
    
    PiperOrigin-RevId: 543009546

--- tensorflow/python/data/experimental/kernel_tests/service/distributed_save_ft_test.py
+++ tensorflow/python/data/experimental/kernel_tests/service/distributed_save_ft_test.py
@@ -315,6 +315,24 @@ class SnapshotFtTest(data_service_test_base.TestBase, parameterized.TestCase):
                            [worker_max_concurrent_snapshots] * num_workers)
 
   @combinations.generate(test_base.default_test_combinations())
+  def testDatasetRecoversAndCompletes(self):
+    cluster = data_service_test_base.TestCluster(num_workers=3)
+    ds = dataset_ops.Dataset.range(1000)
+    self.evaluate(distributed_save_op.distributed_save(
+        ds, self._path, cluster.dispatcher_address(), compression=None))
+
+    # Blocks until all workers have streams.
+    get_stream_assignments(cluster, 3, [self._path])
+    cluster.stop_worker(0)
+    cluster.restart_dispatcher()
+    cluster.restart_worker(0)
+    self._wait_for_snapshot()
+    self.assertTrue(self._snapshot_is_done())
+
+    dataset = dataset_ops.Dataset.load(self._path)
+    self.assertDatasetProduces(dataset, range(1000), assert_items_equal=True)
+
+  @combinations.generate(test_base.default_test_combinations())
   def testLargeMultiSourceSnapshotRecoversAndCompletes(self):
     n = 5
     cluster, _ = self.setup(num_workers=n, ds_size=1000, num_sources=3)
