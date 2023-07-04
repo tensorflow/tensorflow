aa4b697bc2c923dbd6b33c9a6826ee8476ed4de0
Milos Puzovic <Milos.Puzovic@arm.com>
2023-07-04 13:58:07 +0100

    Set weights to any for matmul and ip oneDNN primitives

--- tensorflow/core/kernels/mkl/mkl_matmul_ops_common.h
+++ tensorflow/core/kernels/mkl/mkl_matmul_ops_common.h
@@ -260,7 +260,11 @@ class MklDnnMatMulFwdPrimitive : public MklPrimitive {
 
     context_.weight_md.reset(new memory::desc({matmul_fwd_params.weight_dims},
                                               MklDnnType<Tweight>(),
+#ifdef DNNL_AARCH64_USE_ACL
+                                              memory::format_tag::any));
+#else
                                               matmul_fwd_params.weight_format));
+#endif
 
     context_.dst_md.reset(new memory::desc({matmul_fwd_params.dst_dims},
                                            MklDnnType<Toutput>(),
@@ -818,8 +822,11 @@ class MklMatMulPrimitive : public MklPrimitive {
                                          params.a_strides));
 
     context_.b_md.reset(new memory::desc({params.b_dims}, MklDnnType<Trhs>(),
+#ifdef DNNL_AARCH64_USE_ACL
+                                         memory::format_tag::any));
+#else
                                          params.b_strides));
-
+#endif
     context_.c_md.reset(new memory::desc({params.c_dims}, MklDnnType<Toutput>(),
                                          params.c_strides));
 
@@ -873,8 +880,13 @@ class MklMatMulPrimitive : public MklPrimitive {
     // Create memory primitive based on dummy data.
     context_.a_mem.reset(
         new dnnl::memory(*context_.a_md, cpu_engine_, DummyData));
+#ifndef DNNL_AARCH64_USE_ACL
     context_.b_mem.reset(
         new dnnl::memory(*context_.b_md, cpu_engine_, DummyData));
+#else
+    context_.b_mem.reset(new dnnl::memory(
+        context_.prim_desc.get()->weights_desc(), cpu_engine_, DummyData));
+#endif
     context_.c_mem.reset(
         new dnnl::memory(*context_.c_md, cpu_engine_, DummyData));
     auto scratchpad_md = context_.prim_desc->scratchpad_desc();
