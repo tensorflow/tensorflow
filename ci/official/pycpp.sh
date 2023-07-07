5a85a6859d9780dadf027b6d2190caef32df2e9a
Austin Anderson <angerson@google.com>
2023-07-06 18:37:42 -0700

    Cleanup

--- ci/official/pycpp.sh
+++ ci/official/pycpp.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source "${BASH_SOURCE%/*}/utilities/setup.sh"
 
-# TODO(b/284172313) Revert this difference between presubmits and continuous. RBE serverside behavior is causing flakes,
-# so we're temporarily allowing flaky tests again for presubmits.
 tfrun bazel "${TFCI_BAZEL_BAZELRC_ARGS[@]}" test "${TFCI_BAZEL_CACHE_ARGS[@]}" --config=rbe --config=pycpp --config=build_event_export
 
 tfrun bazel analyze-profile build/profile.json.gz
--- ci/official/utilities/setup.sh
+++ ci/official/utilities/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# Common setup for all TF scripts.
+#
+# Make as FEW changes to this file as possible. It should not contain utility
+# functions (except for tfrun); use dedicated scripts instead and reference them
+# specifically. Use your best judgment to keep the scripts in this directory
+# lean and easy to follow. When in doubt, remember that for CI scripts, "keep it
+# simple" is MUCH more important than "don't repeat yourself."
+
 # -e: abort script if one command fails
 # -u: error if undefined variable used
+# -x: log all commands
 # -o pipefail: entire command fails if pipe fails. watch out for yes | ...
 # -o history: record shell history
 # -o allexport: export all functions and variables to be available to subscripts
@@ -16,6 +25,11 @@ cd "$TFCI_GIT_DIR" && mkdir -p build
 
 # Setup tfrun, a helper function for executing steps that can either be run
 # locally or run under Docker. docker.sh, below, redefines it as "docker exec".
+# Important: "tfrun foo | bar" is "( tfrun foo ) | bar", not tfrun (foo | bar).
+# Therefore, "tfrun" commands cannot include pipes -- which is probably for the
+# better. If a pipe is necessary for something, it is probably complex. Write a
+# well-documented script under utilities/ to encapsulate the functionality
+# instead.
 tfrun() { "$@"; }
 
 # For Google-internal jobs, run copybara, which will overwrite the source tree.
