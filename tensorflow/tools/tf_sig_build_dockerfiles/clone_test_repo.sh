cae0ac40c84b8ed3a1be334405e3b6874debad1a
jayfurmanek <Jason.Furmanek@amd.com>
2023-07-11 17:31:33 -0500

    Add rocm5.7 support for TF wheel and docker files (#2140) (#2156)
    Signed-off-by: Wang, Yanyao <yanyao.wang@amd.com>
    Co-authored-by: Yanyao Wang <wangyanyao@msn.com>
    Co-authored-by: Wang, Yanyao <yanyao.wang@amd.com>

---
+++ tensorflow/tools/tf_sig_build_dockerfiles/clone_test_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+_DL_FLD=${TF_TESTING_FL:-'/'}
+cd "${_DL_FLD}" || exit 2
+
+
+clone_benchmark() {
+    local benchmarks_repo='https://github.com/tensorflow/benchmarks'
+    local tf_ver
+    tf_ver=$(grep -oP "_VERSION = '\K[0-9]+\.[0-9]+" \
+        "${_DL_FLD}/tensorflow/tensorflow/tools/pip_package/setup.py")
+    if [ -z "${tf_ver}" ]; then
+        echo "TF version cannot be found..."
+        exit 1
+    else
+        echo "tf_ver=${tf_ver}"
+    fi
+    # For TF version higher than 2.12, use the new AMD benchmarks repo
+    if (( ${tf_ver//./} > 212 ));then
+        benchmarks_repo='https://github.com/ROCmSoftwarePlatform/benchmarks'
+    fi
+    git clone ${benchmarks_repo}
+}
+
+git clone https://github.com/tensorflow/models.git
+git clone https://github.com/tensorflow/examples.git
+git clone https://github.com/tensorflow/autograph.git
+clone_benchmark
