511236b54cbc8ba567c15d5b54b719cc6fe7ce0a
Jason Furmanek <Jason.Furmanek@amd.com>
2023-07-14 23:53:34 +0000

    fix autobuilds

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
@@ -39,7 +39,8 @@ ROCM_DEB_REPO=${ROCM_DEB_REPO_HOME}${ROCM_VERS}/
 if [ ! -f "/${CUSTOM_INSTALL}" ]; then
 # Add rocm repository
 chmod 1777 /tmp
-DEBIAN_FRONTEND=noninteractive apt-get --allow-unauthenticated update && apt install -y wget software-properties-common
+DEBIAN_FRONTEND=noninteractive apt-get --allow-unauthenticated update 
+DEBIAN_FRONTEND=noninteractive apt install -y wget software-properties-common
 DEBIAN_FRONTEND=noninteractive apt-get clean all
 wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -;
 if [[ $ROCM_DEB_REPO == https://repo.radeon.com/rocm/*  ]] ; then \
