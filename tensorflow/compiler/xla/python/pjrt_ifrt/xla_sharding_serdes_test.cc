4d7ba9ddeb22daefabd4423f2eb201becd602a9d
weihanmines <wei.han3@amd.com>
2023-07-18 00:23:10 +0000

    attemp to fix xla_sharding_serdes_test failure

--- tensorflow/compiler/xla/python/pjrt_ifrt/xla_sharding_serdes_test.cc
+++ tensorflow/compiler/xla/python/pjrt_ifrt/xla_sharding_serdes_test.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/functional/bind_front.h"
+#include "absl/types/span.h"
 #include "tensorflow/compiler/xla/hlo/ir/hlo_sharding.h"
 #include "tensorflow/compiler/xla/python/ifrt/sharding_serdes.h"
 #include "tensorflow/compiler/xla/python/ifrt/sharding_test_util.h"
@@ -34,7 +35,7 @@ class XlaShardingSerDesTest : public test_util::ShardingTest {};
 
 TEST_P(XlaShardingSerDesTest, HloShardingRoundTrip) {
   auto device_list = GetDevices({0, 1});
-  auto xla_hlo_sharding = xla::HloSharding::Tile(xla::TileAssignment({2, 1}));
+  auto xla_hlo_sharding = xla::HloSharding::Tile(xla::TileAssignment(absl::Span<const int64_t>({2, 1})));
   auto sharding = HloSharding::Create(device_list,
                                       /*xla_hlo_sharding=*/xla_hlo_sharding);
 
