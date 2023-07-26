cf4afb6b6ff75ed69bd02cc9f7cdbe47bfa2bfa8
Jie Sun <jiesun@google.com>
2023-07-26 12:01:06 -0700

    special allocations' aggregated metrics need to consider memory color.
    PiperOrigin-RevId: 551275563

--- tensorflow/core/profiler/convert/hlo_proto_to_memory_visualization_utils.cc
+++ tensorflow/core/profiler/convert/hlo_proto_to_memory_visualization_utils.cc
@@ -466,13 +466,14 @@ void Convert(const BufferAllocationProto& proto,
 }
 
 void NoteSpecialAllocations(const HloProtoBufferWrapper& wrapper,
-                            int64_t small_buffer_size,
+                            int64_t memory_color, int64_t small_buffer_size,
                             PreprocessResult* result) {
   int64_t entry_parameters_bytes = 0;
   int64_t non_reusable_bytes = 0;
   int64_t maybe_live_out_bytes = 0;
-  for (const BufferAllocationProto& buffer_allocation :
-       wrapper.GetHloProto().buffer_assignment().buffer_allocations()) {
+  for (const auto* buffer_allocation_struct :
+       wrapper.GetBufferAllocations(memory_color)) {
+    const auto& buffer_allocation = buffer_allocation_struct->proto();
     if (buffer_allocation.is_entry_computation_parameter()) {
       entry_parameters_bytes += buffer_allocation.size();
     }
@@ -1020,7 +1021,8 @@ void GeneratePreprocessResult(const HloProtoBufferWrapper& wrapper,
                        logical_buffer->span->second);
   }
 
-  NoteSpecialAllocations(wrapper, peak_snapshot.small_buffer_size, result);
+  NoteSpecialAllocations(wrapper, memory_color, peak_snapshot.small_buffer_size,
+                         result);
 
   ConvertAllocationTimeline(wrapper, simulator_stats, memory_color, result);
 }
