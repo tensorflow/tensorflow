f433fe3ce95a2e7dbb1b579479060fa1f89e8ecf
Matt Callanan <mpcallanan@google.com>
2023-07-27 15:54:37 -0700

    #tf-data Ramp up `"file_locality_v2"` experiment to 50%.
    PiperOrigin-RevId: 551664452

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -978,7 +978,7 @@ REGISTER_DATASET_EXPERIMENT("data_transfer", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("file_locality", RandomJobSamplePercentage<0>,
                             IndependentHostTasks);
-REGISTER_DATASET_EXPERIMENT("file_locality_v2", RandomJobSamplePercentage<10>,
+REGISTER_DATASET_EXPERIMENT("file_locality_v2", RandomJobSamplePercentage<50>,
                             IndependentHostTasks);
 }  // namespace
 }  // namespace data
