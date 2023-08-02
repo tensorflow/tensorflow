52f15341522e853d8f2b2089c4723e261107e988
Peter Hawkins <phawkins@google.com>
2023-08-02 11:28:30 -0700

    [JAX] Don't include ShardingSpecs or out_indices in the data passed to the C++ pmap() fast path.
    The pmap() fast path doesn't even look the ShardingSpec or the out_indices since the jax.Sharding rework.
    
    PiperOrigin-RevId: 553206145

--- tensorflow/compiler/xla/python/xla_client.py
+++ tensorflow/compiler/xla/python/xla_client.py
@@ -44,7 +44,7 @@ profiler = _xla.profiler
 
 # Just an internal arbitrary increasing number to help with backward-compatible
 # changes. In JAX, reference this via jax._src.lib.xla_extension_version.
-_version = 175
+_version = 176
 
 # Version number for MLIR:Python components.
 mlir_api_version = 54
