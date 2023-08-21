f23e87cee05d6dc38bfe2767c1914185a51390a2
Jieying Luo <jieying@google.com>
2023-08-21 14:23:29 -0700

    [PJRT C API] Remove LoadTpuLibraryAndInitializeTpuStructFns in xla.cc.
    TPU profiler C API initialization has been moved to tpu_tracer.cc (https://github.com/openxla/xla/commit/9f70d6df0f4d323406ea572bee0e44a9ac2c109f). Currently TPU embedding C APIs initialization is done in tensorflow code (jax_tpu_embedding triggers TF code).
    
    The following tests pass with this change:
    - JAX_ENABLE_TPU_XDIST=true python3 -m pytest -n=4 --tb=short --maxfail=20 -m "not multiaccelerator"
    - python3 -m pytest --tb=short --maxfail=20 -m "multiaccelerator" tests
    
    PiperOrigin-RevId: 558899841

--- tensorflow/compiler/xla/python/xla.cc
+++ tensorflow/compiler/xla/python/xla.cc
@@ -60,7 +60,6 @@ limitations under the License.
 #include "tensorflow/compiler/xla/python/pjrt_ifrt/pjrt_client.h"
 #ifdef XLA_PYTHON_ENABLE_TPU
 #include "tensorflow/compiler/xla/pjrt/tpu_client.h"
-#include "tensorflow/compiler/xla/stream_executor/tpu/tpu_initializer_framework_helper.h"  // NOLINT(unused-includes): required for tensorflow::tpu::LoadTpuLibraryAndInitializeTpuStructFns
 #endif  // XLA_PYTHON_ENABLE_TPU
 #include "tensorflow/compiler/xla/pjrt/pjrt_api.h"
 #include "tensorflow/compiler/xla/python/custom_call_sharding.h"
@@ -623,15 +622,6 @@ PYBIND11_MODULE(xla_extension, m) {
          std::shared_ptr<DistributedRuntimeClient> distributed_client)
           -> std::shared_ptr<PyClient> {
         py::gil_scoped_release gil_release;
-#ifdef XLA_PYTHON_ENABLE_TPU
-#if !defined(PLATFORM_GOOGLE)
-        if (absl::AsciiStrToLower(platform_name) == "tpu") {
-          // TODO(b/261484192): handle device specific initialization.
-          xla::ThrowIfError(
-              tensorflow::tpu::LoadTpuLibraryAndInitializeTpuStructFns());
-        }
-#endif  // PLATFORM_GOOGLE
-#endif  // XLA_PYTHON_ENABLE_TPU
         PjRtClient::KeyValueGetCallback kv_get = nullptr;
         PjRtClient::KeyValuePutCallback kv_put = nullptr;
         if (distributed_client != nullptr) {
