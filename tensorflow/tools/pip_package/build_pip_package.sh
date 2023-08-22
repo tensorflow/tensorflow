6803962efdca5f14b5f62da12a4acfa9ac83e70f
mraunak <83710963+mraunak@users.noreply.github.com>
2023-08-21 19:17:27 -0700

    Indentation of comments

--- tensorflow/tools/pip_package/build_pip_package.sh
+++ tensorflow/tools/pip_package/build_pip_package.sh
@@ -168,13 +168,12 @@ function prepare_src() {
     cp -L \
       bazel-bin/tensorflow/tools/pip_package/simple_console_for_window_unzip/runfiles/org_tensorflow/LICENSE \
       "${TMPDIR}"
-
-	# Change the format of file path (TMPDIR-->TMPDIR_rsync) which is input to the rsync from
-	# Windows-compatible to Linux-compatible to resolve the error below 
-	# error: ssh: Could not resolve hostname c: No such host is known. 
-	
-	TMPDIR_rsync=`cygpath $TMPDIR`
-
+      
+    # Change the format of file path (TMPDIR-->TMPDIR_rsync) which is input to the rsync from
+    # Windows-compatible to Linux-compatible to resolve the error below 
+    # error: ssh: Could not resolve hostname c: No such host is known. 
+    
+    TMPDIR_rsync=`cygpath $TMPDIR`  
     rsync -a \
       bazel-bin/tensorflow/tools/pip_package/build_pip_package.exe.runfiles/org_tensorflow/tensorflow \
       "${TMPDIR_rsync}"
