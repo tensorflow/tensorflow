b161ab8e91a22b855943f2c540f24495c1265992
David Silverstone <silverstone@google.com>
2023-08-25 16:42:09 -0700

    Runtime fallback: Clean up C++ headers, includes and build_targets
    Context: Will remove `tf_tensor` impl from libtpu, which requires some cleanup of headers and separation of interface and implementation
    PiperOrigin-RevId: 560226430

--- tensorflow/core/runtime_fallback/util/tensor_util.h
+++ tensorflow/core/runtime_fallback/util/tensor_util.h
@@ -16,19 +16,18 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_RUNTIME_FALLBACK_UTIL_TENSOR_UTIL_H_
 #define TENSORFLOW_CORE_RUNTIME_FALLBACK_UTIL_TENSOR_UTIL_H_
 
+#include <cstdint>
 #include <memory>
 
 #include "tensorflow/c/tf_tensor.h"
-#include "tensorflow/c/tf_tensor_internal.h"
 #include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/platform/status.h"
+#include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/runtime_fallback/util/type_util.h"
-#include "tfrt/core_runtime/tensor_handle.h"  // from @tf_runtime
+#include "tfrt/dtype/dtype.h"  // from @tf_runtime
 #include "tfrt/host_context/host_buffer.h"  // from @tf_runtime
-#include "tfrt/host_context/host_context.h"  // from @tf_runtime
 #include "tfrt/support/forward_decls.h"  // from @tf_runtime
-#include "tfrt/tensor/dense_host_tensor.h"  // from @tf_runtime
 #include "tfrt/tensor/string_host_tensor.h"  // from @tf_runtime
+#include "tfrt/tensor/tensor_metadata.h"  // from @tf_runtime
 #include "tfrt/tensor/tensor_shape.h"  // from @tf_runtime
 
 namespace tensorflow {
