306a58ad46058e995babf691524d54ab4cafb347
Yuriy Chernyshov <thegeorg@yandex-team.com>
2023-08-29 13:36:51 +0300

    Fix typo

--- tensorflow/lite/delegates/gpu/metal/benchmarking/main.mm
+++ tensorflow/lite/delegates/gpu/metal/benchmarking/main.mm
@@ -7,7 +7,7 @@ You may obtain a copy of the License at
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the Licensgoe is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
