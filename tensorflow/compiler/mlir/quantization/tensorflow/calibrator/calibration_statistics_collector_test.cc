a3f564ce5bec36c41bc26177133b78e9124177d1
A. Unique TensorFlower <gardener@tensorflow.org>
2023-08-31 04:23:10 -0700

    Split CalibrationStatisticsCollector to small collectors.
    PiperOrigin-RevId: 561614946

--- tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector_test.cc
+++ tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector_test.cc
@@ -12,27 +12,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector.h"
-
 #include <optional>
 #include <vector>
 
 #include <gtest/gtest.h>
 #include "tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics.pb.h"
-#include "tensorflow/compiler/mlir/quantization/tensorflow/quantization_options.pb.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector_average_min_max.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector_base.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/calibrator/calibration_statistics_collector_min_max.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
 namespace calibrator {
 namespace {
 
-using tensorflow::quantization::CalibrationOptions;
-
 TEST(CalibrationStatisticsCollectorTest, SimpleMinMax) {
-  CalibrationStatisticsCollector calibration_statistics_collector;
-  CalibrationOptions calib_opts;
-  calib_opts.set_calibration_method(
-      CalibrationOptions::CALIBRATION_METHOD_MIN_MAX);
+  auto collector = CalibrationStatisticsCollectorMinMax();
 
   std::vector<std::vector<float>> collect_vec;
 
@@ -41,12 +36,10 @@ TEST(CalibrationStatisticsCollectorTest, SimpleMinMax) {
   collect_vec.push_back({-5.0f, 2.0f, 3.0f, 4.0f, 5.0f});
 
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
-
-  std::optional<CalibrationStatistics> statistics =
-      calibration_statistics_collector.GetStatistics();
+  std::optional<CalibrationStatistics> statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   EXPECT_EQ(statistics.value().min_max_statistics().global_min(), -5.0f);
@@ -54,10 +47,7 @@ TEST(CalibrationStatisticsCollectorTest, SimpleMinMax) {
 }
 
 TEST(CalibrationStatisticsCollectorTest, SimpleAverageMinMax) {
-  CalibrationStatisticsCollector calibration_statistics_collector;
-  CalibrationOptions calib_opts;
-  calib_opts.set_calibration_method(
-      CalibrationOptions::CALIBRATION_METHOD_AVERAGE_MIN_MAX);
+  auto collector = CalibrationStatisticsCollectorAverageMinMax();
 
   std::vector<std::vector<float>> collect_vec;
 
@@ -68,11 +58,10 @@ TEST(CalibrationStatisticsCollectorTest, SimpleAverageMinMax) {
       {-5.0f, 2.0f, 3.0f, 4.0f, 5.0f});  // min=-5.0f, max=5.0f
 
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
-  std::optional<CalibrationStatistics> statistics =
-      calibration_statistics_collector.GetStatistics();
+  std::optional<CalibrationStatistics> statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   // 1.0f + 1.0f - 5.0f
@@ -84,10 +73,7 @@ TEST(CalibrationStatisticsCollectorTest, SimpleAverageMinMax) {
 }
 
 TEST(CalibrationStatisticsCollectorTest, ClearDataAndGetResultsMinMax) {
-  CalibrationStatisticsCollector calibration_statistics_collector;
-  CalibrationOptions calib_opts;
-  calib_opts.set_calibration_method(
-      CalibrationOptions::CALIBRATION_METHOD_MIN_MAX);
+  auto collector = CalibrationStatisticsCollectorMinMax();
 
   std::vector<std::vector<float>> collect_vec;
 
@@ -96,29 +82,27 @@ TEST(CalibrationStatisticsCollectorTest, ClearDataAndGetResultsMinMax) {
   collect_vec.push_back({-5.0f, 2.0f, 3.0f, 4.0f, 5.0f});
 
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
 
-  std::optional<CalibrationStatistics> statistics =
-      calibration_statistics_collector.GetStatistics();
+  std::optional<CalibrationStatistics> statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   EXPECT_EQ(statistics.value().min_max_statistics().global_min(), -5.0f);
   EXPECT_EQ(statistics.value().min_max_statistics().global_max(), 10.0f);
 
-  calibration_statistics_collector.ClearData();
-  collect_vec.pop_back();  // pop last element
-
-  statistics = calibration_statistics_collector.GetStatistics();
+  collector.ClearData();
+  statistics = collector.GetStatistics();
   EXPECT_FALSE(statistics.has_value());
 
+  collect_vec.pop_back();  // pop last element
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
 
-  statistics = calibration_statistics_collector.GetStatistics();
+  statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   EXPECT_EQ(statistics.value().min_max_statistics().global_min(), 1.0f);
@@ -126,10 +110,7 @@ TEST(CalibrationStatisticsCollectorTest, ClearDataAndGetResultsMinMax) {
 }
 
 TEST(CalibrationStatisticsCollectorTest, ClearDataAndGetResultsAverageMinMax) {
-  CalibrationStatisticsCollector calibration_statistics_collector;
-  CalibrationOptions calib_opts;
-  calib_opts.set_calibration_method(
-      CalibrationOptions::CALIBRATION_METHOD_AVERAGE_MIN_MAX);
+  auto collector = CalibrationStatisticsCollectorAverageMinMax();
 
   std::vector<std::vector<float>> collect_vec;
 
@@ -138,30 +119,28 @@ TEST(CalibrationStatisticsCollectorTest, ClearDataAndGetResultsAverageMinMax) {
   collect_vec.push_back({-5.0f, 2.0f, 3.0f, 4.0f, 5.0f});
 
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
 
-  std::optional<CalibrationStatistics> statistics =
-      calibration_statistics_collector.GetStatistics();
+  std::optional<CalibrationStatistics> statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   EXPECT_EQ(statistics.value().average_min_max_statistics().min_sum(), -3.0f);
   EXPECT_EQ(statistics.value().average_min_max_statistics().max_sum(), 30.0f);
   EXPECT_EQ(statistics.value().average_min_max_statistics().num_samples(), 3);
 
-  calibration_statistics_collector.ClearData();
-  collect_vec.pop_back();  // pop last element
-
-  statistics = calibration_statistics_collector.GetStatistics();
+  collector.ClearData();
+  statistics = collector.GetStatistics();
   EXPECT_FALSE(statistics.has_value());
 
+  collect_vec.pop_back();  // pop last element
   for (auto data_vec : collect_vec) {
-    calibration_statistics_collector.Collect(
-        /*data_vec=*/data_vec, /*calib_opts=*/calib_opts);
+    collector.CalibrationStatisticsCollectorBase::Collect(
+        /*data_vec=*/data_vec);
   }
 
-  statistics = calibration_statistics_collector.GetStatistics();
+  statistics = collector.GetStatistics();
 
   EXPECT_TRUE(statistics.has_value());
   EXPECT_EQ(statistics.value().average_min_max_statistics().min_sum(), 2.0f);
