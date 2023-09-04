2b304c12dd9f9854294e16eeb8be0ee6ba1eb1cd
Surya <116063290+SuryanarayanaY@users.noreply.github.com>
2023-09-04 10:07:52 +0530

    Add CPU_kernel with float16 for adjust_contrast_op.cc
    The API tf.image.adjust_contrast supports float16 and float32 for the argument images . But with CPU runtime and float16 dtype the API raising exception stating there is no kernel available.
    
    Hence adding a kernel for CPU with float16.

--- tensorflow/core/kernels/image/adjust_contrast_op.cc
+++ tensorflow/core/kernels/image/adjust_contrast_op.cc
@@ -387,9 +387,14 @@ class AdjustContrastOpv2<CPUDevice, float> : public AdjustContrastOpV2Base {
   }
 };
 
-REGISTER_KERNEL_BUILDER(
-    Name("AdjustContrastv2").Device(DEVICE_CPU).TypeConstraint<float>("T"),
-    AdjustContrastOpv2<CPUDevice, float>);
+#define REGISTER_KERNEL(T)                                                 \
+  REGISTER_KERNEL_BUILDER(                                                 \
+      Name("AdjustContrastv2").Device(DEVICE_CPU).TypeConstraint<T>("T"),  \
+      AdjustContrastOpv2<CPUDevice, T>);
+
+REGISTER_KERNEL(float);
+REGISTER_KERNEL(Eigen::half);
+#undef REGISTER_KERNEL
 
 #if (defined(GOOGLE_CUDA) && GOOGLE_CUDA) || \
     (defined(TENSORFLOW_USE_ROCM) && TENSORFLOW_USE_ROCM)
