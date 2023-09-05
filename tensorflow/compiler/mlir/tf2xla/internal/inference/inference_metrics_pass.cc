dd1d02228720957171d5da79e6de7da9b693e9fd
Mason Chang <masonchang@google.com>
2023-09-05 14:20:11 -0700

    Add a streamz to record how often a MLIR Module has a TPUPartitionedCallOp. This will be used and inserted into the V1CompatBridge to verify that TPUPartitionedCallOp == Inference Graph.
    PiperOrigin-RevId: 562889821

--- tensorflow/compiler/mlir/tf2xla/internal/inference/inference_metrics_pass.cc
+++ tensorflow/compiler/mlir/tf2xla/internal/inference/inference_metrics_pass.cc
@@ -14,13 +14,24 @@ limitations under the License.
 ==============================================================================*/
 
 #include <memory>
+#include <string>
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
+#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
+#include "mlir/IR/Operation.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
+#include "tensorflow/core/lib/monitoring/counter.h"
 
 namespace mlir {
 namespace tf2xla {
 namespace internal {
+
+auto* has_tpu_partitioned_call_streamz =
+    tensorflow::monitoring::Counter<1>::New(
+        "/tensorflow/core/tf2xla/internal/inference/tpu_partitioned_call",
+        "Whether the model has TPUPartitionedCallOp.",
+        "has_tpu_partitioned_call");
 
 namespace {
 
@@ -33,11 +44,26 @@ class InferenceMetricsPass
   void runOnOperation() override;
 };
 
-void InferenceMetricsPass::runOnOperation() {}
+void InferenceMetricsPass::runOnOperation() {
+  bool has_tpu_partitioned_call = false;
+  ModuleOp module = getOperation();
+
+  for (auto func_op : module.getOps<func::FuncOp>()) {
+    func_op->walk(
+        [&](TF::TPUPartitionedCallOp op) { has_tpu_partitioned_call = true; });
+
+    if (has_tpu_partitioned_call) break;
+  }
+
+  std::string has_tpu_partitioned_call_str =
+      has_tpu_partitioned_call ? "true" : "false";
+  has_tpu_partitioned_call_streamz->GetCell(has_tpu_partitioned_call_str)
+      ->IncrementBy(1);
+}
 
 }  // namespace
 
-std::unique_ptr<mlir::OperationPass<func::FuncOp>>
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
 CreateInferenceMetricsPass() {
   return std::make_unique<InferenceMetricsPass>();
 }
