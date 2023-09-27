446e8e2cc39b73ecc8268dc5eaa019f68619c829
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2023-09-27 13:14:43 +0000

    fixes according to review + one missing file

--- tensorflow/python/kernel_tests/nn_ops/conv_ops_test.py
+++ tensorflow/python/kernel_tests/nn_ops/conv_ops_test.py
@@ -2543,7 +2543,8 @@ class Conv2DTest(parameterized.TestCase, test.TestCase):
           padding="VALID",
           test_input=True,
           data_format=data_format,
-          use_gpu=use_gpu)
+          use_gpu=use_gpu,
+          max_err=0.005)
 
   @test_util.deprecated_graph_mode_only
   def testFilterGradientValidPaddingStrideThree(self):
@@ -2597,7 +2598,8 @@ class Conv2DTest(parameterized.TestCase, test.TestCase):
           padding="SAME",
           test_input=False,
           data_format=data_format,
-          use_gpu=use_gpu)
+          use_gpu=use_gpu,
+          max_err=0.005)
 
   @test_util.deprecated_graph_mode_only
   def testInputGradientSamePaddingStrideTwo(self):
@@ -2651,7 +2653,8 @@ class Conv2DTest(parameterized.TestCase, test.TestCase):
           padding="SAME",
           test_input=True,
           data_format=data_format,
-          use_gpu=use_gpu)
+          use_gpu=use_gpu,
+          max_err=0.005)
 
   @test_util.deprecated_graph_mode_only
   def testFilterGradientSamePaddingStrideThree(self):
@@ -2906,7 +2909,8 @@ class Conv2DTest(parameterized.TestCase, test.TestCase):
           padding=[[0, 0], [4, 3], [2, 1], [0, 0]],
           test_input=False,
           data_format=data_format,
-          use_gpu=use_gpu)
+          use_gpu=use_gpu,
+          max_err=0.005)
 
   @test_util.deprecated_graph_mode_only
   def testInputGradient0_0_0_5PaddingStride1x2(self):
@@ -2924,7 +2928,8 @@ class Conv2DTest(parameterized.TestCase, test.TestCase):
           padding=[[0, 0], [0, 0], [0, 5], [0, 0]],
           test_input=True,
           data_format=data_format,
-          use_gpu=use_gpu)
+          use_gpu=use_gpu,
+          max_err=0.005)
 
   @test_util.deprecated_graph_mode_only
   def testFilterGradient0_0_0_5PaddingStride1x2(self):
