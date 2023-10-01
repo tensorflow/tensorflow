9746df1c5256726dbac8cab79f885909c193e467
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-01 10:10:34 +0200

    Update trt_convert_test.py with minor efficiency improvement

--- tensorflow/python/compiler/tensorrt/trt_convert_test.py
+++ tensorflow/python/compiler/tensorrt/trt_convert_test.py
@@ -92,10 +92,7 @@ class TrtConvertTest(test_util.TensorFlowTestCase, parameterized.TestCase):
   def _GetGraph(cls, inp1, inp2, var):
     """Get the graph for testing."""
     # The graph computes: inp1^2 + inp1*var + inp1 + inp2 + var
-    add = inp1 + var
-    mul = inp1 * add
-    add = mul + add
-    add = add + inp2
+    add = (inp1 + 1) * (inp1 + var) + inp1
     out = array_ops.identity(add, name="output")
     return out
 
