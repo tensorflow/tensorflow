6dfbe1a5e0ae7d4863543d6b83a1f528fe130726
Christian Sigg <csigg@google.com>
2023-10-03 11:46:35 -0700

    [XLA:GPU] Remove cuda_executor.h, rocm_gpu_executor.h and targets.
    They don't provide anything useful.
    
    PiperOrigin-RevId: 570452290

--- third_party/xla/xla/stream_executor/cuda/cuda_executor.cc
+++ third_party/xla/xla/stream_executor/cuda/cuda_executor.cc
@@ -13,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "xla/stream_executor/cuda/cuda_executor.h"
-
 #include <cstdint>
 #include <memory>
 #include <utility>
