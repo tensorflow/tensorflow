9219692e7399fcb6ff7dbfa0b05af17b97228a3c
Matt Callanan <mpcallanan@google.com>
2023-10-05 14:04:32 -0700

    #tf-data Turn up `"file_locality"` (v1) experiment to 10% job level.
    PiperOrigin-RevId: 571124258

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -984,8 +984,8 @@ REGISTER_DATASET_EXPERIMENT("stage_based_autotune_v2",
                             RandomJobSamplePercentage<0>, IndependentHostTasks);
 REGISTER_DATASET_EXPERIMENT("data_transfer", RandomJobSamplePercentage<0>,
                             AllTasks);
-REGISTER_DATASET_EXPERIMENT("file_locality", RandomJobSamplePercentage<0>,
-                            IndependentHostTasks);
+REGISTER_DATASET_EXPERIMENT("file_locality", RandomJobSamplePercentage<10>,
+                            AllTasks);
 REGISTER_DATASET_EXPERIMENT("file_locality_v2", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("no_compression", RandomJobSamplePercentage<50>,
