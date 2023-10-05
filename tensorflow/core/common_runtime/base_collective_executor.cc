5a90e2753802c09b0ac22c94ad078d5ac64b844d
Derek Murray <mrry@google.com>
2023-10-05 16:47:59 -0700

    Add fields to the CollectiveExecutor trace metadata.
    PiperOrigin-RevId: 571168796

--- tensorflow/core/common_runtime/base_collective_executor.cc
+++ tensorflow/core/common_runtime/base_collective_executor.cc
@@ -342,8 +342,11 @@ void BaseCollectiveExecutor::ExecuteAsync(OpKernelContext* ctx,
                                           ctx->op_kernel().type_string_view());
           return profiler::TraceMeEncode(
               std::move(op),
-              {{"id", ctx->step_id()},
+              {{"step_id", ctx->step_id()},
+               {"iter_id", ctx->frame_iter().iter_id},
+               {"frame_id", ctx->frame_iter().frame_id},
                {"instance_key", col_ctx->col_params->instance.instance_key},
+               {"group_key", col_ctx->col_params->group.group_key},
                {"collective", col_ctx->col_params->instance.type}});
         },
         context_id);
