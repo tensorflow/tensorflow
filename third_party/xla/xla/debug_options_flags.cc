bdb14431fb8623df8301db9a5b70e933d487f748
Anlun Xu <anlunx@google.com>
2023-10-11 13:02:07 -0700

    [xla:gpu] Revert the default gpu_graph_level back to 1
    PiperOrigin-RevId: 572662254

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -93,8 +93,9 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   // flag.
   opts.set_xla_gpu_enable_cublaslt(false);
 
-  // TODO(b/258036887): Enable gpu_graph_level=3.
-  opts.set_xla_gpu_graph_level(2);
+  // TODO(b/258036887): Create separate flags for enabling cuBLAS, cuDNN, and
+  // NCCL in GPU graphs.
+  opts.set_xla_gpu_graph_level(1);
   opts.set_xla_gpu_graph_num_runs_to_instantiate(-1);
   opts.set_xla_gpu_enable_persistent_temp_buffers(false);
   opts.set_xla_gpu_graph_min_graph_size(5);
