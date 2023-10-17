5f7f05a80aac9b01325a78ec3fcff0dbedb1cc23
A. Unique TensorFlower <gardener@tensorflow.org>
2023-10-17 05:04:40 -0700

    Update Python packages' requirements for Python 3.12.
    PiperOrigin-RevId: 574111451

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -87,10 +87,12 @@ REQUIRED_PACKAGES = [
     'flatbuffers >= 23.5.26',
     'gast >=0.2.1,!=0.5.0,!=0.5.1,!=0.5.2',
     'google_pasta >= 0.1.1',
-    'h5py >= 2.9.0',
+    'h5py >= 3.10.0',
     'libclang >= 13.0.0',
-    'ml_dtypes ~= 0.2.0',
-    'numpy >= 1.23.5, < 2.0.0',
+    'ml_dtypes ~= 0.3.1',
+    # TODO(b/304751256): Adjust the numpy pin to a single version, when ready
+    'numpy >= 1.23.5, < 2.0.0 ; python_version <= "3.11"',
+    'numpy >= 1.26.0, < 2.0.0 ; python_version >= "3.12"',
     'opt_einsum >= 2.3.2',
     'packaging',
     # pylint:disable=line-too-long
@@ -102,7 +104,8 @@ REQUIRED_PACKAGES = [
     'termcolor >= 1.1.0',
     'typing_extensions >= 3.6.6',
     'wrapt >= 1.11.0, < 1.15',
-    'tensorflow-io-gcs-filesystem >= 0.23.1',
+    # TODO(b/305196096): Remove the <3.12 condition once the pkg is updated
+    'tensorflow-io-gcs-filesystem >= 0.23.1 ; python_version < "3.12"',
     # grpcio does not build correctly on big-endian machines due to lack of
     # BoringSSL support.
     # See https://github.com/tensorflow/tensorflow/issues/17882.
