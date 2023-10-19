e544ac5837d62e7c8bcdaaad8abba1c5d37e6dc1
Anlun Xu <anlunx@google.com>
2023-10-19 15:28:30 -0700

    [xla:gpu] Add support for building KernelArguments from HLO #6224
    - KernelArgument will not contain the MLIR value if emitting from HLO.
    - Properly build CompileModuleResults.
    - CholeskyTest passes after this change
    
    PiperOrigin-RevId: 575020293

--- third_party/xla/xla/service/gpu/kernel_thunk.h
+++ third_party/xla/xla/service/gpu/kernel_thunk.h
@@ -54,7 +54,8 @@ class KernelThunk : public Thunk {
   // `mlir::memref::ViewOp`).
   KernelThunk(mlir::Operation* op, std::string kernel_name,
               absl::Span<const KernelArgument> kernel_arguments,
-              LaunchDimensions launch_dimensions, int64_t shmem_bytes);
+              LaunchDimensions launch_dimensions, int64_t shmem_bytes,
+              bool emit_ir_from_hlo = false);
   KernelThunk(const KernelThunk&) = delete;
   KernelThunk& operator=(const KernelThunk&) = delete;
   ~KernelThunk() override = default;
