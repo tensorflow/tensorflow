7a037597e2367044f64067bedde615ceeac1029f
Yu Feng <feyu@google.com>
2023-10-26 10:23:06 -0700

    Use feature path to index feature offsets.
    This change drops the requirement that feature names must be unique.
    
    Previously non-unique feature names cause silent (there is a warning for it)
    wrong lookups when table stacking is enabled.
    
    PiperOrigin-RevId: 576914369

--- tensorflow/python/tpu/tpu_embedding_v3.py
+++ tensorflow/python/tpu/tpu_embedding_v3.py
@@ -629,11 +629,11 @@ class TPUEmbeddingV2(tpu_embedding_base.TPUEmbeddingBase):
     self._table_to_sample_count = {
         table_name: 0 for table_name in self._stacked_table_to_tables
     }
-    for _, feature in self._flat_features:
+    for feature_path, feature in self._flat_features:
       stacked_table_name = self._table_to_stacked_table_offset[
           feature.table.name
       ][0]
-      self._feature_to_sample_offset[feature.name] = (
+      self._feature_to_sample_offset[feature_path] = (
           self._table_to_sample_count[stacked_table_name]
       )
       self._table_to_sample_count[stacked_table_name] += functools.reduce(
@@ -1033,13 +1033,13 @@ class TPUEmbeddingV2(tpu_embedding_base.TPUEmbeddingBase):
     table_to_list_of_coos = {
         table_name: ([], [], []) for table_name in stacked_table_to_tables
     }
-    for inp, weight, (_, feature) in zip(
+    for inp, weight, (feature_path, feature) in zip(
         flat_inputs, flat_weights, flat_features
     ):
       table_name, col_offset, col_shift = table_to_stacked_table_offset[
           feature.table.name
       ]
-      row_offset = feature_to_sample_offset[feature.name]
+      row_offset = feature_to_sample_offset[feature_path]
       # Consider making this into one op per table rather than per feature?
       row_ids, col_ids, gains = TPUEmbeddingV2._convert_input_feature_to_coo(
           inp,
@@ -1716,14 +1716,14 @@ class TPUEmbeddingV2(tpu_embedding_base.TPUEmbeddingBase):
         )
         for table_name in stacked_table_to_tables
     }
-    for inp, weight, (_, feature) in zip(
+    for inp, weight, (feature_path, feature) in zip(
         flat_inputs, flat_weights, flat_features
     ):
       table_name, col_offset, col_shift = table_to_stacked_table_offset[
           feature.table.name
       ]
       stacked_table_sample_count = stacked_table_to_sample_count[table_name]
-      row_offset = feature_to_sample_offset[feature.name]
+      row_offset = feature_to_sample_offset[feature_path]
       # Consider making this into one op per table rather than per feature?
       row_ids_list, col_ids_list, gains_list, sample_count = (
           TPUEmbeddingV2._experimental_convert_input_feature_to_list_of_coo_tensors(
