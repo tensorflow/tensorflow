f992d4457513f1d5828d3196442478903dd7cc39
Eugene Zhulenev <ezhulenev@google.com>
2023-10-26 09:38:12 -0700

    [stream_executor:gpu] Use stream executor allocated workspace for all cuBLAS operations
    This is a work around a cuBLAS + CUDA graphs bug where cuBLAS adds an allocation node that triggers memset node update error
    
    PiperOrigin-RevId: 576899709

--- third_party/xla/xla/stream_executor/cuda/cuda_blas.h
+++ third_party/xla/xla/stream_executor/cuda/cuda_blas.h
@@ -25,6 +25,7 @@ limitations under the License.
 #include "third_party/gpus/cuda/include/cublas_v2.h"
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/cuda/cuda_blas_lt.h"
+#include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/platform/port.h"
 #include "xla/stream_executor/plugin_registry.h"
 
@@ -115,6 +116,20 @@ class CUDABlas : public blas::BlasSupport {
   // GpuExecutor which instantiated this CUDABlas.
   // Immutable post-initialization.
   gpu::GpuExecutor *parent_;
+
+  // TODO(b/307832648): We use a per-device workspace for all cuBLAS operations.
+  // In general this is unsafe, however we rely on the fact that XLA uses a
+  // single compute stream, and today it's guaranteed that all cuBLAS operations
+  // (as any other kernels launched on Gpu device) will have a well defined
+  // total order, either because they run on the main stream or have explicit
+  // events that synchronize them with a main stream.
+  //
+  // This is not true inside CUDA graphs with non-linear DAG, however today this
+  // is not enabled by default. This is a short term solution, and should be
+  // replaced with an explicit workspace allocation at HLO level.
+  //
+  // See: https://docs.nvidia.com/cuda/cublas/#cublassetworkspace
+  DeviceMemoryBase workspace_;  // owned
 
   // cuBLAS library handle on the device.
   cublasHandle_t blas_ ABSL_GUARDED_BY(mu_);
