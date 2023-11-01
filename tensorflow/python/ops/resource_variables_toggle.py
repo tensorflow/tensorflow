c9e4cca05921caf756d46ebae92f195b1d10b5d8
Fiona Lang <flang@google.com>
2023-11-01 11:45:54 -0700

    Fix outdated monitoring description.
    PiperOrigin-RevId: 578589659

--- tensorflow/python/ops/resource_variables_toggle.py
+++ tensorflow/python/ops/resource_variables_toggle.py
@@ -23,7 +23,7 @@ from tensorflow.python.util.tf_export import tf_export
 
 _api_usage_gauge = monitoring.BoolGauge(
     "/tensorflow/api/resource_variables",
-    "Whether variable_scope.enable_resource_variables() is called.")
+    "Whether resource_variables_toggle.enable_resource_variables() is called.")
 
 _DEFAULT_USE_RESOURCE = tf2.enabled()
 
