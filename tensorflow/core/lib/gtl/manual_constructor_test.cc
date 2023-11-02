ce7b77f3a930966fd04d50e0ade859f735e1b2d1
Raunak <mayank.kumar.raunak@intel.com>
2023-11-01 17:36:44 -0700

    included msvc run

--- tensorflow/core/lib/gtl/manual_constructor_test.cc
+++ tensorflow/core/lib/gtl/manual_constructor_test.cc
@@ -92,9 +92,7 @@ TEST(ManualConstructorTest, Alignment) {
 
   EXPECT_EQ(reinterpret_cast<char*>(test2.b.get()) - &test2.a,
             reinterpret_cast<char*>(&control2.b) - &control2.a);
-#ifdef __x86_64__
   EXPECT_EQ(reinterpret_cast<intptr_t>(test2.b.get()) % alignof(long double), 0);
-#endif
 }
 
 TEST(ManualConstructorTest, DefaultInitialize) {
