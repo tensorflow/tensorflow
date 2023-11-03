3c1eab32569fe1eb858e6cbfa47c2adcd9a191fa
Frederik Gossen <frgossen@google.com>
2023-11-03 07:35:02 -0700

    Add regression test for large reduce-scatter ops to be combined.
    PiperOrigin-RevId: 579178032

--- third_party/xla/xla/service/reduce_scatter_combiner_test.cc
+++ third_party/xla/xla/service/reduce_scatter_combiner_test.cc
@@ -15,8 +15,12 @@ limitations under the License.
 
 #include "xla/service/reduce_scatter_combiner.h"
 
+#include <cstddef>
 #include <utility>
 
+#include "absl/algorithm/container.h"
+#include "absl/log/log.h"
+#include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/hlo/utils/hlo_matchers.h"
@@ -35,19 +39,31 @@ class ReduceScatterCombinerTest : public HloTestBase {
       int64_t byte_threshold = kMaxByteCount,
       int64_t count_threshold = kMaxCombineCount, bool combine_by_dim = true) {
     TF_ASSIGN_OR_RETURN(auto module, ParseAndReturnVerifiedModule(hlo_module));
+
+    VLOG(1) << "Before running ReduceScatterCombiner: "
+            << ReduceScatterCount(module.get()) << " reduce-scatter ops";
+
     auto changed =
         ReduceScatterCombiner(byte_threshold, count_threshold, combine_by_dim)
             .Run(module.get());
     if (!changed.ok()) {
       return changed.status();
     }
+
+    VLOG(1) << "After running ReduceScatterCombiner: "
+            << ReduceScatterCount(module.get()) << " reduce-scatter ops";
+
     EXPECT_EQ(changed.value(), expect_change);
     return StatusOr<std::unique_ptr<HloModule>>(std::move(module));
   }
 
-  size_t ReduceScatterCount(std::unique_ptr<HloModule>& module) {
-    return absl::c_count_if(module->entry_computation()->instructions(),
-                            HloPredicateIsOp<HloOpcode::kReduceScatter>);
+  size_t ReduceScatterCount(HloModule *module) {
+    int64_t sum = 0;
+    for (auto comp : module->computations()) {
+      sum += absl::c_count_if(comp->instructions(),
+                              HloPredicateIsOp<HloOpcode::kReduceScatter>);
+    }
+    return sum;
   }
 };
 
@@ -73,7 +89,7 @@ ENTRY main {
 )";
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           RunPass(hlo_string, /*expect_change=*/true));
-  EXPECT_EQ(ReduceScatterCount(module), 1);
+  EXPECT_EQ(ReduceScatterCount(module.get()), 1);
 }
 
 TEST_F(ReduceScatterCombinerTest, SimpleMultipleGroups) {
@@ -103,7 +119,7 @@ ENTRY main {
 )";
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           RunPass(hlo_string, /*expect_change=*/true));
-  EXPECT_EQ(ReduceScatterCount(module), 2);
+  EXPECT_EQ(ReduceScatterCount(module.get()), 2);
 }
 
 TEST_F(ReduceScatterCombinerTest, DifferentDimensions) {
@@ -134,7 +150,7 @@ ENTRY main {
   TF_ASSERT_OK_AND_ASSIGN(
       auto module, RunPass(hlo_string, /*expect_change=*/true, kMaxByteCount,
                            kMaxCombineCount, /*combine_by_dim=*/false));
-  EXPECT_EQ(ReduceScatterCount(module), 1);
+  EXPECT_EQ(ReduceScatterCount(module.get()), 1);
 }
 
 // Test that dependent reduce-scatter do not get combined.
@@ -219,6 +235,42 @@ ENTRY entry{
 )";
   TF_ASSERT_OK_AND_ASSIGN(auto module,
                           RunPass(hlo_string, /*expect_change=*/false));
+}
+
+TEST_F(ReduceScatterCombinerTest, HighThreshold) {
+  absl::string_view hlo_string = R"(
+HloModule m
+
+sum_reduce {
+  lhs = bf16[] parameter(0)
+  rhs = bf16[] parameter(1)
+  ROOT add = bf16[] add(lhs, rhs)
+}
+
+ENTRY main {
+  param.0 = bf16[1024,32768]{1,0} parameter(0)
+  param.1 = bf16[4096,8192]{1,0} parameter(1)
+  param.2 = bf16[3,128,64,1024]{2,1,0,3}parameter(2)
+  param.3 = bf16[1024,128,64]{2,1,0} parameter(3)
+  reduce-scatter.19 = bf16[1024,32768]{1,0} reduce-scatter(param.0),
+      channel_id=132, replica_groups={{0}}, dimensions={0}, to_apply=sum_reduce
+  reduce-scatter.21 = bf16[4096,8192]{1,0} reduce-scatter(param.1),
+      channel_id=134, replica_groups={{0}}, dimensions={0}, to_apply=sum_reduce
+  reduce-scatter.23 = bf16[3,128,64,1024]{2,1,0,3} reduce-scatter(param.2),
+      channel_id=136, replica_groups={{0}}, dimensions={3}, to_apply=sum_reduce
+  reduce-scatter.25 = bf16[1024,128,64]{2,1,0} reduce-scatter(param.3),
+      channel_id=138, replica_groups={{0}}, dimensions={0}, to_apply=sum_reduce
+  ROOT tuple = tuple(reduce-scatter.19, reduce-scatter.21, reduce-scatter.23,
+      reduce-scatter.25)
+})";
+
+  int64_t combined_bytes = 67108864 + 67108864 + 50331648 + 16777216;
+  TF_ASSERT_OK_AND_ASSIGN(
+      auto module,
+      RunPass(hlo_string, /*expect_change=*/true,
+              /*byte_threshold=*/combined_bytes,
+              /*count_threshold=*/kMaxCombineCount, /*combine_by_dim=*/false));
+  EXPECT_EQ(ReduceScatterCount(module.get()), 1);
 }
 
 }  // namespace
