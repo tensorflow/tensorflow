4b8611e38656de7fdd101a058a76ec65d2f8297a
David Dunleavy <ddunleavy@google.com>
2023-11-05 09:08:44 -0800

    Update jax build script in preparation for JAX GPU job
    PiperOrigin-RevId: 579643732

--- third_party/xla/.kokoro/jax/build.sh
+++ third_party/xla/.kokoro/jax/build.sh
@@ -23,13 +23,25 @@ set -euox pipefail -o history
 
 source "${KOKORO_GFILE_DIR}/utils.sh"
 
+function is_linux_gpu_job() {
+  [[ "$KOKORO_JOB_NAME" =~ tensorflow/xla/jax/.*gpu.* ]]
+}
+
 clone_main_jax() {
   git clone https://github.com/google/jax.git
 }
 
-build_and_test_on_rbe_cpu() {
+prelude() {
   export JAX_ENABLE_X64=0
 
+  if is_linux_gpu_job ; then
+    export JAX_CUDA_VERSION=12
+    export JAX_CUDNN_VERSION=8.9
+
+    nvidia-smi
+  fi
+
+  setup_env_vars_py312
   cd "${KOKORO_ARTIFACTS_DIR}"
 
   use_local_or_install_python
@@ -41,6 +53,9 @@ build_and_test_on_rbe_cpu() {
   chmod +x "${KOKORO_GFILE_DIR}/bazel_wrapper.py"
   cd jax
 
+}
+
+build_and_test_on_rbe_cpu() {
   # Run the tests.
    "${KOKORO_GFILE_DIR}/bazel_wrapper.py" \
       test \
@@ -56,14 +71,38 @@ build_and_test_on_rbe_cpu() {
       --test_output=errors
 }
 
+build_and_test_on_rbe_gpu() {
+  # Runs non-multiaccelerator tests with one GPU apiece.
+  # It appears --run_under needs an absolute path.
+  "${KOKORO_GFILE_DIR}/bazel_wrapper.py" \
+    test \
+    --verbose_failures=true \
+    //tests:gpu_tests //tests:backend_independent_tests \
+    --override_repository=xla="${KOKORO_ARTIFACTS_DIR}"/github/xla \
+    --config=avx_posix \
+    --config=mkl_open_source_only \
+    --config="$CUDA_RBE_CONFIG_NAME" \
+    --test_env=XLA_PYTHON_CLIENT_ALLOCATOR=platform \
+    --test_output=errors \
+    --test_env=JAX_SKIP_SLOW_TESTS=1 \
+    --test_env=TF_CPP_MIN_LOG_LEVEL=0 \
+    --test_env=JAX_EXCLUDE_TEST_TARGETS=PmapTest.testSizeOverflow \
+    --test_tag_filters=-multiaccelerator
+}
+
 # Generate a templated results file to make output accessible to everyone
 "$KOKORO_ARTIFACTS_DIR"/github/xla/.kokoro/generate_index_html.sh "$KOKORO_ARTIFACTS_DIR"/index.html
 
-# TODO(ddunleavy): move below to different script
 NOCUDA_RBE_CONFIG_NAME="rbe_cpu_linux_py312"
+CUDA_RBE_CONFIG_NAME="rbe_linux_cuda12.2_nvcc_py3.12"
 
-setup_env_vars_py312
-build_and_test_on_rbe_cpu
+prelude
+
+if is_linux_gpu_job ; then
+  build_and_test_on_rbe_gpu
+else
+  build_and_test_on_rbe_cpu
+fi
 
 echo "bazel-testlogs (test results) location:"
 find "$KOKORO_ARTIFACTS_DIR" \
