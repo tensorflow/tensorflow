8ae9227ed04a8448a3a30e6e8db4916982282d80
Fergus Henderson <fergus@google.com>
2023-11-08 18:31:10 -0800

    Fix misleading variable name in SetBufferHandleToInput test: s/kInputTensorIndex/kOutputTensorIndex/
    This was probably a cut-and-paste error from SetBufferHandleToOutput below,
    which uses the same variable name for the tensor at index 3.
    In the graph constructed by TestDelegation (which TestDelegate inherits from),
    which is used by both of these tests, the tensors at index 0 and 1 are inputs
    and tensors at index 3 and 4 are outputs.
    
    PiperOrigin-RevId: 580730411

--- tensorflow/lite/delegates/delegate_test.cc
+++ tensorflow/lite/delegates/delegate_test.cc
@@ -178,14 +178,14 @@ TEST_F(TestDelegate, SetBufferHandleToInput) {
   TfLiteDelegate* delegate = delegate_->get_tf_lite_delegate();
   interpreter_->ModifyGraphWithDelegate(delegate);
 
-  constexpr int kOutputTensorIndex = 0;
-  TfLiteTensor* tensor = interpreter_->tensor(kOutputTensorIndex);
+  constexpr int kInputTensorIndex = 0;
+  TfLiteTensor* tensor = interpreter_->tensor(kInputTensorIndex);
   ASSERT_EQ(tensor->delegate, nullptr);
   ASSERT_EQ(tensor->buffer_handle, kTfLiteNullBufferHandle);
 
   TfLiteBufferHandle handle = AllocateBufferHandle();
   TfLiteStatus status =
-      interpreter_->SetBufferHandle(kOutputTensorIndex, handle, delegate);
+      interpreter_->SetBufferHandle(kInputTensorIndex, handle, delegate);
   ASSERT_EQ(status, kTfLiteOk);
   EXPECT_EQ(tensor->delegate, delegate);
   EXPECT_EQ(tensor->buffer_handle, handle);
