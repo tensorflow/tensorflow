592f3172d762a3d4dc104406aa301bed387d3357
Dan Suh <dansuh@google.com>
2023-11-09 21:57:02 -0800

    Move `create_feed_dict_from_input_data` to `representative_dataset.py`.
    Since `create_feed_dict_from_input_data` is directly related to representative dataset usages, moving it and making it a public api helps other dependencies to use the functionality with better guarantees.
    
    PiperOrigin-RevId: 581121654

--- tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model.py
+++ tensorflow/compiler/mlir/quantization/tensorflow/python/quantize_model.py
@@ -16,10 +16,9 @@
 
 import collections.abc
 import tempfile
-from typing import Callable, Collection, Dict, Mapping, Optional, Sequence
+from typing import Callable, Collection, Mapping, Optional, Sequence
 
 from absl import logging
-import numpy as np
 
 from tensorflow.compiler.mlir.quantization.tensorflow import exported_model_pb2
 from tensorflow.compiler.mlir.quantization.tensorflow import quantization_options_pb2 as quant_opts_pb2
@@ -239,43 +238,6 @@ def _convert_values_to_tf_tensors(
   return tensor_mapping
 
 
-def _create_feed_dict_from_input_data(
-    input_data: repr_dataset.RepresentativeSample,
-    signature_def: meta_graph_pb2.SignatureDef,
-) -> Dict[str, np.ndarray]:
-  """Constructs a feed_dict from input data.
-
-  Note: This function should only be used in graph mode.
-
-  This is a helper function that converts an 'input key -> input value' mapping
-  to a feed dict. A feed dict is an 'input tensor name -> input value' mapping
-  and can be directly passed to the `feed_dict` argument of `sess.run()`.
-
-  Args:
-    input_data: Input key -> input value mapping. The input keys should match
-      the input keys of `signature_def`.
-    signature_def: A SignatureDef representing the function that `input_data` is
-      an input to.
-
-  Returns:
-    Feed dict, which is intended to be used as input for `sess.run`. It is
-    essentially a mapping: input tensor name -> input value. Note that the input
-    value in the feed dict is not a `Tensor`.
-  """
-  feed_dict = {}
-  for input_key, input_value in input_data.items():
-    input_tensor_name = signature_def.inputs[input_key].name
-
-    value = input_value
-    if isinstance(input_value, core.Tensor):
-      # Take the data out of the tensor.
-      value = input_value.eval()
-
-    feed_dict[input_tensor_name] = value
-
-  return feed_dict
-
-
 # TODO(b/249918070): Implement a progress bar.
 def _log_sample_num_for_calibration(
     representative_dataset: repr_dataset.RepresentativeDataset,
@@ -358,7 +320,9 @@ def _run_function_for_calibration_graph_mode(
   ):
     # Create a mapping from input tensor name to the input tensor value.
     # ex) "Placeholder:0" -> [0, 1, 2]
-    feed_dict = _create_feed_dict_from_input_data(sample, signature_def)
+    feed_dict = repr_dataset.create_feed_dict_from_input_data(
+        sample, signature_def
+    )
     sess.run(output_tensor_names, feed_dict=feed_dict)
 
 
