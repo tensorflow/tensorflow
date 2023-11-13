b71841568ef65ad78c6251fc893c1050a16d1da6
Samuel Agyakwa <sagyakwa@google.com>
2023-11-13 11:56:49 -0800

    [PJRT C API] Support passing platform_name as an option in PJRT GPU plugin.
    PiperOrigin-RevId: 582038998

--- third_party/xla/xla/pjrt/c/pjrt_c_api_gpu_test.cc
+++ third_party/xla/xla/pjrt/c/pjrt_c_api_gpu_test.cc
@@ -333,6 +333,83 @@ TEST(PjrtCApiGpuAllocatorTest, InvalidAllocatorOptionsParsing) {
   api->PJRT_Error_Destroy(&error_destroy_args);
 }
 
+TEST(PjrtCApiPlatformNameTest, AvailablePlatformName) {
+  auto api = GetPjrtApi();
+  std::string expected_platform_name_for_cuda = "cuda";
+  std::string expected_platform_name_for_rocm = "rocm";
+  absl::flat_hash_map<std::string, xla::PjRtValueType> options = {
+      {"platform_name", static_cast<std::string>("gpu")},
+      {"allocator", static_cast<std::string>("default")},
+      {"visible_devices", xla::PjRtValueType(std::vector<int64_t>{0, 1})},
+  };
+  TF_ASSERT_OK_AND_ASSIGN(
+      std::vector<PJRT_NamedValue> c_options,
+      ::pjrt::ConvertToPjRtNamedValueList(options, /*api_minor_version=*/30));
+  PJRT_Client_Create_Args create_arg;
+  create_arg.struct_size = PJRT_Client_Create_Args_STRUCT_SIZE;
+  create_arg.priv = nullptr;
+  create_arg.client = nullptr;
+  create_arg.create_options = c_options.data();
+  create_arg.num_options = c_options.size();
+  PJRT_Error* error = api->PJRT_Client_Create(&create_arg);
+  EXPECT_EQ(error, nullptr) << error->status.message();
+
+  PJRT_Client_PlatformName_Args platform_name_args;
+  platform_name_args.struct_size = PJRT_Client_PlatformName_Args_STRUCT_SIZE;
+  platform_name_args.priv = nullptr;
+  platform_name_args.client = create_arg.client;
+
+  PJRT_Error* platform_name_error =
+      api->PJRT_Client_PlatformName(&platform_name_args);
+  EXPECT_EQ(platform_name_error, nullptr);
+#if TENSORFLOW_USE_ROCM
+  EXPECT_EQ(platform_name_args.platform_name, expected_platform_name_for_rocm);
+#else
+  EXPECT_EQ(platform_name_args.platform_name, expected_platform_name_for_cuda);
+#endif
+
+  PJRT_Client_Destroy_Args destroy_args;
+  destroy_args.struct_size = PJRT_Client_Destroy_Args_STRUCT_SIZE;
+  destroy_args.priv = nullptr;
+  destroy_args.client = create_arg.client;
+
+  PJRT_Error* destroy_error = api->PJRT_Client_Destroy(&destroy_args);
+  CHECK_EQ(destroy_error, nullptr);
+}
+
+TEST(PjrtCApiPlatformNameTest, UnavailablePlatformName) {
+  auto api = GetPjrtApi();
+  absl::flat_hash_map<std::string, xla::PjRtValueType> options = {
+      {"platform_name", static_cast<std::string>("invalid_platform_name")},
+      {"allocator", static_cast<std::string>("default")},
+      {"visible_devices", xla::PjRtValueType(std::vector<int64_t>{0, 1})},
+  };
+  TF_ASSERT_OK_AND_ASSIGN(
+      std::vector<PJRT_NamedValue> c_options,
+      ::pjrt::ConvertToPjRtNamedValueList(options, /*api_minor_version=*/30));
+  PJRT_Client_Create_Args create_arg;
+  create_arg.struct_size = PJRT_Client_Create_Args_STRUCT_SIZE;
+  create_arg.priv = nullptr;
+  create_arg.client = nullptr;
+  create_arg.create_options = c_options.data();
+  create_arg.num_options = c_options.size();
+  PJRT_Error* error = api->PJRT_Client_Create(&create_arg);
+  EXPECT_NE(error, nullptr);
+  EXPECT_THAT(error->status,
+              ::tsl::testing::StatusIs(
+                  absl::StatusCode::kNotFound,
+                  testing::StartsWith("Could not find registered platform with "
+                                      "name: \"invalid_platform_name\". "
+                                      "Available platform names are:")));
+
+  PJRT_Error_Destroy_Args error_destroy_args;
+  error_destroy_args.struct_size = PJRT_Error_Destroy_Args_STRUCT_SIZE;
+  error_destroy_args.priv = nullptr;
+  error_destroy_args.error = error;
+
+  api->PJRT_Error_Destroy(&error_destroy_args);
+}
+
 void TestCustomCall() {}
 
 TEST(PjrtCApiGpuPrivTest, CustomCall) {
