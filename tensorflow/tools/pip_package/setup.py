f2d5de028fbc67b8f692d64324da20e22f683f2c
A. Unique TensorFlower <gardener@tensorflow.org>
2023-11-14 11:00:25 -0800

    Add the Python 3.12 classifier to setup.py.
    PiperOrigin-RevId: 582377585

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -437,6 +437,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
