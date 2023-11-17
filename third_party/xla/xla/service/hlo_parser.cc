87681cbb46a32243ec7feca93adf63ce20344d42
TJ Xu <tjx@nvidia.com>
2023-11-17 01:45:54 -0800

    PR #7025: [NVIDIA XLA] Add 2 new attributes to hlo instructions to specify the operation queue to run current instruction.
    Imported from GitHub PR https://github.com/openxla/xla/pull/7025
    
     In CUDA term, operation queue id will be the cuda stream ID. This is to support upcoming cases such as collective matmul where we may need to run compute kernels on multiple streams.
    operation_queue_id means the queue(stream) to run this instruction.
    wait_on_operation_queues means the queues(streams) to wait for input data, this signals the backend to insert EventWait primitives.
    Copybara import of the project:
    
    --
    9059ecfe88ca18c4c23abd5af6bf5ceaf650249e by TJ <tjx@nvidia.com>:
    
    Add 2 new attributes to hlo instructions to specify the operation queue
    to run current instruction. In CUDA term, this will be the cuda stream
    ID.
    
    Merging this change closes #7025
    
    PiperOrigin-RevId: 583307710

--- third_party/xla/xla/service/hlo_parser.cc
+++ third_party/xla/xla/service/hlo_parser.cc
@@ -1322,6 +1322,14 @@ bool HloParserImpl::ParseInstructionRhs(HloComputation::Builder* builder,
   attrs["backend_config"] = {/*required=*/false, AttrTy::kStringOrJsonDict,
                              &backend_config};
 
+  optional<int64_t> operation_queue_id;
+  attrs["operation_queue_id"] = {/*required=*/false, AttrTy::kInt64,
+                                 &operation_queue_id};
+
+  optional<std::vector<int64_t>> wait_on_operation_queues;
+  attrs["wait_on_operation_queues"] = {
+      /*required=*/false, AttrTy::kBracedInt64List, &wait_on_operation_queues};
+
   std::optional<Shape> maybe_shape;
   if (parse_shape) {
     maybe_shape = shape;
@@ -1393,6 +1401,13 @@ bool HloParserImpl::ParseInstructionRhs(HloComputation::Builder* builder,
   if (statistics_viz) {
     instruction->set_statistics_viz(*statistics_viz);
   }
+  if (operation_queue_id) {
+    instruction->set_operation_queue_id(*operation_queue_id);
+  }
+  if (wait_on_operation_queues) {
+    instruction->set_wait_on_operation_queues(*wait_on_operation_queues);
+  }
+
   return AddInstruction(name, instruction, name_loc);
 }
 
