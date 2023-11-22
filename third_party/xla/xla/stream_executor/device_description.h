acb0a073d504486c570e363b4ef8c28ccdaed12a
Chao <cchen104@amd.com>
2023-11-22 03:42:36 -0800

    PR #7197: [ROCm] add navi31 on xla
    Imported from GitHub PR https://github.com/openxla/xla/pull/7197
    
    add navi31 support on XLA
    
    @xla-rotation Thanks!
    Copybara import of the project:
    
    --
    7ba0eaef68e19bd2ba52c61c9b45365d34383c24 by Chao Chen <cchen104@amd.com>:
    
    add navi31 on xla
    
    Merging this change closes #7197
    
    PiperOrigin-RevId: 584578379

--- third_party/xla/xla/stream_executor/device_description.h
+++ third_party/xla/xla/stream_executor/device_description.h
@@ -200,11 +200,12 @@ class RocmComputeCapability {
   std::string gcn_arch_name_ = "gfx000";  // default to invalid arch.
 
   static constexpr absl::string_view kSupportedGfxVersions[]{
-      "gfx900",  // MI25
-      "gfx906",  // MI50 / MI60
-      "gfx908",  // MI100
-      "gfx90a",  // MI200
-      "gfx1030"  // Navi21
+      "gfx900",   // MI25
+      "gfx906",   // MI50 / MI60
+      "gfx908",   // MI100
+      "gfx90a",   // MI200
+      "gfx1030",  // Navi21
+      "gfx1100"   // Navi31
   };
 };
 
