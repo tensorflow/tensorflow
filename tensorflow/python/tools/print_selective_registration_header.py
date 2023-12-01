595f5ea875648fcc274f874cbd1a5a67ee860c7d
Grant Jensen <grantjensen@google.com>
2023-11-30 16:37:56 -0800

    [tflite] Fix tflite selective_build_script.
    This file prints out a header file which is piped to ops_to_register.h. The problem lies in that when we import selective_registration_header_lib it prints "Using TensorFlow backend" which also gets piped to ops_to_register, but does not belong.
    
    PiperOrigin-RevId: 586824804

--- tensorflow/python/tools/print_selective_registration_header.py
+++ tensorflow/python/tools/print_selective_registration_header.py
@@ -32,10 +32,17 @@ When compiling for Android:
 """
 
 import argparse
+import contextlib
 import sys
 
 from absl import app
-from tensorflow.python.tools import selective_registration_header_lib
+
+# Import statement prints "Using TensorFlow backend" which gets piped to
+# ops_to_register.h. Avoid this printing import statement to /dev/null
+with open('/dev/null', 'w') as f, contextlib.redirect_stdout(f):
+  # pylint: disable=g-import-not-at-top
+  from tensorflow.python.tools import selective_registration_header_lib
+  # pylint: enable
 
 FLAGS = None
 
