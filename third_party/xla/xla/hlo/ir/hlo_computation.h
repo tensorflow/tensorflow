2326e80b5bcded31cdab020507c78aff34beefe8
A. Unique TensorFlower <gardener@tensorflow.org>
2023-12-01 16:17:57 -0800

    [XLA] Share the same underlying DFS stack across instructions.
    PiperOrigin-RevId: 587153954

--- third_party/xla/xla/hlo/ir/hlo_computation.h
+++ third_party/xla/xla/hlo/ir/hlo_computation.h
@@ -820,12 +820,14 @@ class HloComputation {
   void ComputeInstructionPostOrder(
       HloInstruction* root, const ChannelDependencies& channel_dependencies,
       absl::flat_hash_map<HloInstruction*, VisitState>& visited,
-      std::vector<HloInstruction*>& post_order) const;
+      std::vector<HloInstruction*>& post_order,
+      std::vector<HloInstruction*>* dfs_stack_scratch) const;
 
   void ForEachInstructionPostOrderImpl(
       absl::FunctionRef<void(HloInstruction*)> func, HloInstruction* root,
       const ChannelDependencies& channel_dependencies,
-      absl::flat_hash_map<HloInstruction*, VisitState>& visited) const;
+      absl::flat_hash_map<HloInstruction*, VisitState>& visited,
+      std::vector<HloInstruction*>* dfs_stack_scratch) const;
 
   Status RemoveUnusedParametersImpl(bool allow_non_fusion);
 
