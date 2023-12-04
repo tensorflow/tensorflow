d0717110e29703a22343f6b8ffe1446f2e7ea773
Mihai Maruseac <mihai.maruseac@gmail.com>
2023-12-04 07:15:18 -0800

    Shorten line to make pylint happy

--- configure.py
+++ configure.py
@@ -195,7 +195,7 @@ def setup_python(environ_cp):
   try:
     if not os.path.isfile(python_bin_path):
       if os.access(python_bin_path, os.X_OK):
-        raise FileNotFoundError(f'The python path {python_bin_path} is not valid.')
+        raise FileNotFoundError(f'Python path {python_bin_path} is invalid.')
     python_version = get_python_major_version(python_bin_path)
     if python_version != '3':
       raise ValueError(f'Python found at {python_bin_path} is not version 3. Please update.')
