7e85121a698d1cded8fd777437a93f1f0185cfc8
Parker Schuh <parkers@google.com>
2023-12-04 17:48:20 -0800

    Add BufferDonor support to cpu_compiler.cc.
    PiperOrigin-RevId: 587893584

--- third_party/xla/xla/service/cpu/cpu_compiler.cc
+++ third_party/xla/xla/service/cpu/cpu_compiler.cc
@@ -189,6 +189,7 @@ limitations under the License.
 #include "xla/service/map_inliner.h"
 #include "xla/service/operand_upcaster.h"
 #include "xla/service/optimization_barrier_expander.h"
+#include "xla/service/optimize_input_output_buffer_alias.h"
 #include "xla/service/qr_expander.h"
 #include "xla/service/reduce_decomposer.h"
 #include "xla/service/reshape_decomposer.h"
@@ -939,6 +940,7 @@ Status CpuCompiler::RunHloPassesAfterLayoutAssn(
   pipeline.AddPass<HloDCE>();
   pipeline.AddPass<CopyInsertion>();
   pipeline.AddPass<HloDCE>();
+  pipeline.AddPass<OptimizeInputOutputBufferAlias>(true);
   return pipeline.Run(module).status();
 }
 
