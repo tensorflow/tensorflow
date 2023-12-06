7c73a1856b4387c6ea5f08b0c1c7e37f46be618f
Deqiang Chen <deqiangc@google.com>
2023-12-06 09:14:42 -0800

    Add a utility functions to support host buffer sharding.
    PiperOrigin-RevId: 588448114

--- third_party/xla/xla/python/pjrt_ifrt/tfrt_cpu_client_test_lib.cc
+++ third_party/xla/xla/python/pjrt_ifrt/tfrt_cpu_client_test_lib.cc
@@ -27,7 +27,7 @@ namespace {
 const bool kUnused = (test_util::RegisterClientFactory(
                           []() -> StatusOr<std::shared_ptr<Client>> {
                             CpuClientOptions options;
-                            options.cpu_device_count = 2;
+                            options.cpu_device_count = 4;
                             TF_ASSIGN_OR_RETURN(auto pjrt_client,
                                                 xla::GetTfrtCpuClient(options));
                             return std::shared_ptr<Client>(
