4ead88ce888671f7666135a6bb35694a5fe42728
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2023-12-07 04:03:07 +0000

    Update dockerfiles to include clang-17

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
@@ -52,7 +52,7 @@ if [[ $ROCM_DEB_REPO == https://repo.radeon.com/rocm/*  ]] ; then \
 else
     bash "/${CUSTOM_INSTALL}"
 fi
-echo "deb [arch=amd64 trusted=yes] http://apt.llvm.org/focal/ llvm-toolchain-focal-16 main" > /etc/apt/sources.list.d/llvm.list
+echo "deb [arch=amd64 trusted=yes] http://apt.llvm.org/focal/ llvm-toolchain-focal-17 main" > /etc/apt/sources.list.d/llvm.list
 
 GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx900 gfx906 gfx908 gfx90a gfx940 gfx941 gfx942 gfx1030 gfx1100"}
 
