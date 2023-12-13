059b23d0dfd34e0d6cdf1f4a65dbc8ed1dfdf54a
Fiona Lang <flang@google.com>
2023-12-13 10:15:15 -0800

    Ensure tf.Tensor slicing operator override is always registered.
    PiperOrigin-RevId: 590638453

--- tensorflow/python/modules_with_exports.py
+++ tensorflow/python/modules_with_exports.py
@@ -121,6 +121,7 @@ from tensorflow.python.ops import rnn
 from tensorflow.python.ops import rnn_cell
 from tensorflow.python.ops import sets
 from tensorflow.python.ops import stateful_random_ops
+from tensorflow.python.ops import tensor_getitem_override
 from tensorflow.python.ops import while_v2
 from tensorflow.python.ops.linalg import linalg
 from tensorflow.python.ops.linalg.sparse import sparse
