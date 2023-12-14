913e4e956ba0a58a1a5422a628022930e48fb98a
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2023-12-14 06:27:25 -0800

    PR #7714: [ROCm] Gate CanTritonHandleGEMM for rocm
    Imported from GitHub PR https://github.com/openxla/xla/pull/7714
    
    Copybara import of the project:
    
    --
    e13213d65e06cf150b45ea67ef62e94f32f29e2e by Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>:
    
    [ROCm] Gate CanTritonHandleGEMM for rocm
    
    Merging this change closes #7714
    
    PiperOrigin-RevId: 590919335

--- third_party/xla/xla/service/gpu/gemm_rewriter_triton.cc
+++ third_party/xla/xla/service/gpu/gemm_rewriter_triton.cc
@@ -677,21 +677,19 @@ FusionDecision CanTritonHandleGEMM(
     return "Non-default precision.";
   }
 
+  auto cuda_compute_capability =
+      std::get_if<se::CudaComputeCapability>(&gpu_version);
+
+  if (!cuda_compute_capability) return "Non CUDA device.";
+
   auto supported_output_type = [&](const PrimitiveType t) {
     switch (t) {
       case F16:
       case F32:
         return true;
       case BF16:
-        return std::visit(
-            Overload{[](const se::CudaComputeCapability& cc) {
-                       return cc.IsAtLeast(
-                           stream_executor::CudaComputeCapability::AMPERE);
-                     },
-                     [](const se::RocmComputeCapability&) {
-                       return true;  // TODO check rocm support!
-                     }},
-            gpu_version);
+        return cuda_compute_capability->IsAtLeast(
+            stream_executor::CudaComputeCapability::AMPERE);
       default:
         return false;
     }
