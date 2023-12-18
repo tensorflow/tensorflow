572d02005f99e994138be108974159007049588e
AbhisekOmkar <67184718+AbhisekOmkar@users.noreply.github.com>
2023-12-18 11:09:30 +0530

    Made Chenges to Fix indentation

--- tensorflow/python/debug/wrappers/disk_usage_test.py
+++ tensorflow/python/debug/wrappers/disk_usage_test.py
@@ -1,18 +1,3 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Debugger Wrapper Session Consisting of a Local Curses-based CLI."""
 import os
 import tempfile
 import tensorflow as tf
@@ -31,8 +16,7 @@ class DumpingDebugWrapperVariableTypesTest(test_util.TensorFlowTestCase):
 
     @classmethod
     def setUpClass(cls):
-        # For efficient testing, set the disk usage bytes limit to a small
-        # number (10).
+        # For efficient testing, set the disk usage bytes limit to a small number (10).
         os.environ["TFDBG_DISK_BYTES_LIMIT"] = "50"
 
     def setUp(self):
