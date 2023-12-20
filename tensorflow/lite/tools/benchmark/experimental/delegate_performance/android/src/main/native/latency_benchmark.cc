de92054e43f01478d2680c4ad34199d2fb15ca0d
Yoshio Soma <yoshio.soma@goinc.jp>
2023-12-20 21:14:49 +0900

    Fix condition.

--- tensorflow/lite/tools/benchmark/experimental/delegate_performance/android/src/main/native/latency_benchmark.cc
+++ tensorflow/lite/tools/benchmark/experimental/delegate_performance/android/src/main/native/latency_benchmark.cc
@@ -157,8 +157,8 @@ std::vector<std::string> ParseArgumentsFromTfLiteSettings(
             absl::StrFormat("--num_threads=%d",
                             tflite_settings.xnnpack_settings()->num_threads()));
       }
-      if (tflite_settings.xnnpack_settings() &&
-          tflite_settings.xnnpack_settings()->flags() | TFLITE_XNNPACK_DELEGATE_FLAG_FORCE_FP16) {
+      if (tflite_settings.xnnpack_settings()->flags() ==
+              XNNPackFlags_TFLITE_XNNPACK_DELEGATE_FLAG_FORCE_FP16) {
             args.push_back("--xnnpack_force_fp16=true");
       }
       return args;
