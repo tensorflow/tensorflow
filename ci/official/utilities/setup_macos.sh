7db05f8a3fdb3ba78ac9c9297e72d2fdd573d0c1
Nitin Srinivasan <srnitin@google.com>
2023-12-20 16:34:35 -0800

    Add experimental cross-compile build for macOS
    This adds experimental nightly and continuous CI build that cross-compile Tensorflow for macOS on remote Linux x86 VMs.
    
    PiperOrigin-RevId: 592688193

--- ci/official/utilities/setup_macos.sh
+++ ci/official/utilities/setup_macos.sh
@@ -99,3 +99,19 @@ if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]]; then
   gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 fi
 set -x
+
+# When cross-compiling with RBE, we need to copy the macOS sysroot to be
+# inside the TensorFlow root directory. We then define them as a filegroup
+# target inside "tensorflow/tools/toolchains/cross_compile/cc" so that Bazel
+# can register it as an input to compile/link actions and send it to the remote
+# VMs when needed.
+# TODO(b/316932689): Avoid copying and replace with a local repository rule.
+if [[ "$TFCI_MACOS_CROSS_COMPILE_ENABLE" == 1 ]]; then
+  mkdir -p "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/usr"
+  mkdir -p "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/System/Library/Frameworks/"
+  cp -r "${TFCI_MACOS_CROSS_COMPILE_SDK_SOURCE}/usr/include" "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/usr/include"
+  cp -r "${TFCI_MACOS_CROSS_COMPILE_SDK_SOURCE}/usr/lib" "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/usr/lib"
+  cp -r "${TFCI_MACOS_CROSS_COMPILE_SDK_SOURCE}/System/Library/Frameworks/CoreFoundation.framework" "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/System/Library/Frameworks/CoreFoundation.framework"
+  cp -r "${TFCI_MACOS_CROSS_COMPILE_SDK_SOURCE}/System/Library/Frameworks/Security.framework" "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/System/Library/Frameworks/Security.framework"
+  cp -r "${TFCI_MACOS_CROSS_COMPILE_SDK_SOURCE}/System/Library/Frameworks/SystemConfiguration.framework" "${TFCI_MACOS_CROSS_COMPILE_SDK_DEST}/System/Library/Frameworks/SystemConfiguration.framework"
+fi
