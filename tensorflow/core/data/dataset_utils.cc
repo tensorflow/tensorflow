8c21374c5b5325e1eed33f43ece49b12c4cb5b58
Matt Callanan <mpcallanan@google.com>
2024-01-09 15:53:15 -0800

    #tf-data-service Graduate `no_compression` experiment.
    PiperOrigin-RevId: 597067927

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -1001,7 +1001,7 @@ REGISTER_DATASET_EXPERIMENT("file_locality", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("file_locality_v2", RandomJobSamplePercentage<0>,
                             AllTasks);
-REGISTER_DATASET_EXPERIMENT("no_compression", RandomJobSamplePercentage<50>,
+REGISTER_DATASET_EXPERIMENT("no_compression", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("inject_io_prefetch", RandomJobSamplePercentage<0>,
                             AllTasks);
