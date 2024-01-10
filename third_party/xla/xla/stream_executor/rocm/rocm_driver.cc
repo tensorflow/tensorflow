104c922e9b981e3dc828581f80a0659a15141238
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2024-01-10 06:10:47 -0800

    PR #8251: [ROCm] Fix hipGraph Memcpy nodes on ROCM5.7
    Imported from GitHub PR https://github.com/openxla/xla/pull/8251
    
    Copybara import of the project:
    
    --
    152a1fa53fd5cc844ec0673981306807e6187ba0 by Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>:
    
    [ROCm] Fix hipGraph Memcpy nodes on ROCM5.7
    
    Merging this change closes #8251
    
    PiperOrigin-RevId: 597229031

--- third_party/xla/xla/stream_executor/rocm/rocm_driver.cc
+++ third_party/xla/xla/stream_executor/rocm/rocm_driver.cc
@@ -922,10 +922,10 @@ GpuDriver::GraphGetMemAllocNodeParams(GpuGraphNodeHandle node) {
   hipMemcpy3DParms params{
       .srcArray = {},
       .srcPos = {},
-      .srcPtr = {.ptr = gpu_src},
+      .srcPtr = {.ptr = gpu_src, .pitch = size, .xsize = size, .ysize = 1},
       .dstArray = {},
       .dstPos = {},
-      .dstPtr = {.ptr = gpu_dst},
+      .dstPtr = {.ptr = gpu_dst, .pitch = size, .xsize = size, .ysize = 1},
       .extent = hipExtent{.width = size, .height = 1, .depth = 1},
       .kind = hipMemcpyDeviceToDevice};
 
@@ -947,10 +947,10 @@ GpuDriver::GraphGetMemAllocNodeParams(GpuGraphNodeHandle node) {
   hipMemcpy3DParms params{
       .srcArray = {},
       .srcPos = {},
-      .srcPtr = {.ptr = gpu_src},
+      .srcPtr = {.ptr = gpu_src, .pitch = size, .xsize = size, .ysize = 1},
       .dstArray = {},
       .dstPos = {},
-      .dstPtr = {.ptr = gpu_dst},
+      .dstPtr = {.ptr = gpu_dst, .pitch = size, .xsize = size, .ysize = 1},
       .extent = hipExtent{.width = size, .height = 1, .depth = 1},
       .kind = hipMemcpyDeviceToDevice};
 
