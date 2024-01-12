e9552f222b3237aafa4773e3f96a8cefb3ebaf95
Matt Callanan <mpcallanan@google.com>
2024-01-11 22:15:25 -0800

    #tf-data-service Turn up `no_compression_v2` experiment to 50%.
    PiperOrigin-RevId: 597742104

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -1003,7 +1003,7 @@ REGISTER_DATASET_EXPERIMENT("file_locality_v2", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("no_compression", RandomJobSamplePercentage<0>,
                             AllTasks);
-REGISTER_DATASET_EXPERIMENT("no_compression_v2", RandomJobSamplePercentage<0>,
+REGISTER_DATASET_EXPERIMENT("no_compression_v2", RandomJobSamplePercentage<50>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("inject_io_prefetch", RandomJobSamplePercentage<0>,
                             AllTasks);
