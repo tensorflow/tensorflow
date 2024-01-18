5f4e54d963e22aff5ec55219a229e17bc8886c21
David Dunleavy <ddunleavy@google.com>
2024-01-18 15:39:59 -0800

    Change header to attribute copyright to the OpenXLA authors
    PiperOrigin-RevId: 599646087

--- third_party/xla/xla/service/gpu/fusions/custom.cc
+++ third_party/xla/xla/service/gpu/fusions/custom.cc
@@ -1,4 +1,4 @@
-/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- third_party/xla/xla/service/hlo_value_semantics_analysis.cc
+++ third_party/xla/xla/service/hlo_value_semantics_analysis.cc
@@ -1,4 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The OpenXLA Authors.
 
 
 Licensed under the Apache License, Version 2.0 (the "License");
