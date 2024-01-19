5172b8ff1c762119b07046c5463cc5c60ef8a40c
Victor Stone <victorstone@google.com>
2024-01-19 14:32:05 -0800

    Replace manual creation of copystart/copydone with HloComputation::CreateAsyncInstructions.
    PiperOrigin-RevId: 599939285

--- third_party/xla/xla/service/host_memory_transfer_asyncifier.cc
+++ third_party/xla/xla/service/host_memory_transfer_asyncifier.cc
@@ -176,23 +176,10 @@ class HostMemoryTransferAsyncifierVisitor : public DfsHloVisitorWithDefault {
         << "Copy \"" << copy->name()
         << "\" is between device and host memory space. Converting to async.";
     const Shape context_shape = ShapeUtil::MakeScalarShape(U32);
-    {
-      // TODO(b/319466176): CreateAsyncInstructions does not work for `copy`.
-      // Once it does, replace this block with that.
-      Shape instruction_shape_source = copy->operand(0)->shape();
-      Shape instruction_shape_destination = copy->shape();
-      HloInstruction* copy_start_to_device =
-          copy->parent()->AddInstruction(HloInstruction::CreateCopyStart(
-              ShapeUtil::MakeTupleShape({instruction_shape_destination,
-                                         instruction_shape_source,
-                                         context_shape}),
-              copy->mutable_operand(0)));
-      HloInstruction* copy_done_to_device =
-          copy->parent()->AddInstruction(HloInstruction::CreateUnary(
-              instruction_shape_destination, HloOpcode::kCopyDone,
-              copy_start_to_device));
-      TF_RETURN_IF_ERROR(copy->ReplaceAllUsesWith(copy_done_to_device));
-    }
+    TF_ASSIGN_OR_RETURN(
+        HloInstruction * async_done,
+        copy->parent()->CreateAsyncInstructions(copy, {context_shape}));
+    (void)async_done;
     MarkAsChanged();
     return OkStatus();
   }
