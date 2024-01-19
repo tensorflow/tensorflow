tensorflow/tensorflow

commit 1493218625e3f2a2e239930171373c0429b6abe3

PR #8545: The CHECK macros can give warnings or errors on redefinition
Imported from GitHub PR https://github.com/openxla/xla/pull/8545

These macros can be defined in more than one external project leading to warnings or errors as there is no guard against redefinition in abseil. So ensure that the abseil defines come first to prevent such issues.
Copybara import of the project:

--
369b5310425365e7c104d943ecb299774fe17485 by Andrew Goodbody <andrew.goodbody@linaro.org>:

The CHECK macros can give warnings or errors on redefinition

These macros can be defined in more than one external project
leading to warnings or errors as there is no guard against
redefinition in abseil. So ensure that the abseil defines come
first to prevent such issues.

Merging this change closes #8545

PiperOrigin-RevId: 599812713

third_party/xla/xla/hlo/ir/BUILD
@@ -87,6 +87,7 @@ cc_library(
         "@com_google_absl//absl/container:flat_hash_set",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/functional:function_ref",
+        "@com_google_absl//absl/log",
         "@com_google_absl//absl/log:check",
         "@com_google_absl//absl/memory",
         "@com_google_absl//absl/strings",

third_party/xla/xla/hlo/ir/hlo_computation.h
@@ -26,6 +26,8 @@ limitations under the License.
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/functional/function_ref.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
@@ -101,7 +103,7 @@ class HloComputation {
         std::unique_ptr<HloInstruction> parameter) {
       if (!parameter_numbers_.insert(parameter->parameter_number()).second) {
         return Internal("Duplicate parameter number %d",
-                             parameter->parameter_number());
+                        parameter->parameter_number());
       }
       return AddInstruction(std::move(parameter));
     }

commit 29f7ef8b251dc7dd2071bd3c16793839591fa115

Rollback of PR #8545
Rollback of 1493218625e3f2a2e239930171373c0429b6abe3, broke windows TF continuous build

PiperOrigin-RevId: 599867986

third_party/xla/xla/hlo/ir/BUILD
@@ -87,7 +87,6 @@ cc_library(
         "@com_google_absl//absl/container:flat_hash_set",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/functional:function_ref",
-        "@com_google_absl//absl/log",
         "@com_google_absl//absl/log:check",
         "@com_google_absl//absl/memory",
         "@com_google_absl//absl/strings",

third_party/xla/xla/hlo/ir/hlo_computation.h
@@ -26,8 +26,6 @@ limitations under the License.
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/functional/function_ref.h"
-#include "absl/log/check.h"
-#include "absl/log/log.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
@@ -103,7 +101,7 @@ class HloComputation {
         std::unique_ptr<HloInstruction> parameter) {
       if (!parameter_numbers_.insert(parameter->parameter_number()).second) {
         return Internal("Duplicate parameter number %d",
-                        parameter->parameter_number());
+                             parameter->parameter_number());
       }
       return AddInstruction(std::move(parameter));
     }