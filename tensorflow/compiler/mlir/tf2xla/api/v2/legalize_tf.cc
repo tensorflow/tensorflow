6321495c3f3492758617231bea45c25b652e4b28
A. Unique TensorFlower <gardener@tensorflow.org>
2024-01-22 15:01:51 -0800

    Add logging of graph before bridge phase 2
    PiperOrigin-RevId: 600581010

--- tensorflow/compiler/mlir/tf2xla/api/v2/legalize_tf.cc
+++ tensorflow/compiler/mlir/tf2xla/api/v2/legalize_tf.cc
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/types/variant.h"
 #include "tensorflow/compiler/mlir/tensorflow/utils/dump_mlir_util.h"
 #include "tensorflow/compiler/mlir/tf2xla/api/v1/compile_mlir_util.h"
@@ -34,8 +35,10 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/xla.pb.h"
 #include "tensorflow/core/framework/metrics.h"
+#include "tensorflow/core/graph/graph.h"
 #include "tensorflow/core/platform/status.h"
 #include "tensorflow/core/tpu/kernels/tpu_compile_op_support.h"
+#include "tensorflow/core/util/dump_graph.h"
 #include "tsl/platform/error_logging.h"
 #include "tsl/platform/errors.h"
 #include "tsl/platform/statusor.h"
@@ -62,6 +65,32 @@ bool ShouldFallbackToGraphCompiler(
 
   return std::get<0>(computation).rollout_state ==
          ConfigProto::Experimental::MLIR_BRIDGE_ROLLOUT_DISABLED;
+}
+
+void DumpComputationInput(
+    const std::variant<tpu::MlirToHloArgs, tpu::FunctionToHloArgs>
+        computation) {
+  if (!VLOG_IS_ON(2)) {
+    return;
+  }
+  switch (computation.index()) {
+    case 0:
+      VLOG(2) << "LegalizeMlirToHlo with MLIR computation input: "
+              << std::get<0>(computation).mlir_module;
+      break;
+    case 1: {
+      auto input = std::get<1>(computation);
+      Graph g(input.flib_def);
+      VLOG(2) << "LegalizeMlirToHlo with FLIB computation input: "
+              << DumpGraphToFile(
+                     absl::StrCat("legalize_mlir_hlo_computation_input_",
+                                  input.function->name()),
+                     g, std::get<1>(computation).flib_def);
+    } break;
+    default:
+      VLOG(2) << "LegalizeMlirToHlo computation input: unknown";
+      break;
+  }
 }
 
 Status DumpHloCompilationResult(std::string_view name,
@@ -101,6 +130,8 @@ tsl::StatusOr<tensorflow::XlaCompilationResult> LegalizeMlirToHlo(
     std::vector<std::vector<xla::Shape>>* per_core_arg_shapes,
     xla::CompileOnlyClient* client) {
   auto compilation_result = std::make_unique<XlaCompilationResult>();
+
+  DumpComputationInput(computation);
 
   // If there are no MLIR args, compile the given function in the library.
   if (ShouldFallbackToGraphCompiler(computation)) {
