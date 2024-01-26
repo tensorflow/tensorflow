6b4a246f9c5c8c82ef111440f79f263e99dc188a
Adrian Kuegel <akuegel@google.com>
2024-01-26 03:52:10 -0800

    Revert: Clean up wrong callers of FindNonTrivialHero
    Still causes problems
    
    PiperOrigin-RevId: 601722210

--- third_party/xla/xla/service/gpu/instruction_fusion.cc
+++ third_party/xla/xla/service/gpu/instruction_fusion.cc
@@ -130,7 +130,7 @@ FusionDecision GpuInstructionFusion::ShouldFuse(HloInstruction* consumer,
 
 HloInstruction::FusionKind GpuInstructionFusion::ChooseKind(
     const HloInstruction* producer, const HloInstruction* consumer) {
-  return ChooseFusionKind(*producer, *consumer);
+  return ChooseFusionKind(*consumer);
 }
 
 HloInstruction* GpuInstructionFusion::FuseInstruction(
