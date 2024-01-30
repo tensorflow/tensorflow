0cb0c4d2c4d2e16bdb80b4f2eeb051f874cb6992
Jason Furmanek <Jason.Furmanek@amd.com>
2024-01-30 14:57:39 +0000

    Include WAR for numpy issue in sgpu script

--- tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
@@ -63,6 +63,7 @@ if [ -f /usertools/rocm.bazelrc ]; then
              --config=sigbuild_local_cache \
              --config=rocm \
              --config=pycpp \
+	     --action_env=OPENBLAS_CORETYPE=Haswell \
              --action_env=TF_PYTHON_VERSION=$PYTHON_VERSION \
              --test_env=TF_TESTS_PER_GPU=$TF_TESTS_PER_GPU \
              --test_env=TF_GPU_COUNT=$TF_GPU_COUNT
@@ -83,6 +84,7 @@ else
 	      --test_env=TF_TESTS_PER_GPU=$TF_TESTS_PER_GPU \
 	      --test_env=HSA_TOOLS_LIB=libroctracer64.so \
 	      --test_env=TF_PYTHON_VERSION=$PYTHON_VERSION \
+	      --action_env=OPENBLAS_CORETYPE=Haswell \
 	      --test_timeout 920,2400,7200,9600 \
 	      --build_tests_only \
 	      --test_output=errors \
