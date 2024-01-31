238630e74c9c1b6d078914dd7f4b044993eb010c
Tamás Danyluk <tdanyluk@google.com>
2024-01-31 04:39:38 -0800

    [XLA:GPU] Don't use new GPU runtime at all on CUDA 11 to avoid hangs
    We're not totally sure this is the issue. (Temporarily) disable the runtime to check.
    
    PiperOrigin-RevId: 603010415

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -37,6 +37,10 @@ limitations under the License.
 #include "tsl/platform/protobuf.h"  // IWYU pragma: keep
 #include "tsl/util/command_line_flags.h"
 
+#if GOOGLE_CUDA
+#include "third_party/gpus/cuda/include/cuda.h"
+#endif
+
 namespace xla {
 
 DebugOptions DefaultDebugOptionsIgnoringFlags() {
@@ -98,9 +102,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   // flag.
   opts.set_xla_gpu_enable_cublaslt(false);
 
-#if CUDA_VERSION >= 12000
-  // The new GPU runtime causes occasional hangs on cuda 11, possibly due to the
-  // use of cuda graphs. Disable using cuda graphs.
   opts.add_xla_gpu_enable_command_buffer(DebugOptions::FUSION);
   opts.add_xla_gpu_enable_command_buffer(DebugOptions::CUBLAS);
   opts.add_xla_gpu_enable_command_buffer(DebugOptions::CUSTOM_CALL);
@@ -108,7 +109,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_graph_min_graph_size(5);
   opts.set_xla_gpu_graph_enable_concurrent_region(false);
   opts.set_xla_gpu_graph_eviction_timeout_seconds(60);
-#endif
 
   // Despite the name, fast min/max on GPUs does not seem to be any faster, and
   // adds very counter-intuitive "NaN-swallowing" behavior.
@@ -139,7 +139,13 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_detailed_logging(true);
   opts.set_xla_enable_dumping(true);
 
+#if (!GOOGLE_CUDA) || CUDA_VERSION >= 12000
   opts.set_xla_gpu_enable_xla_runtime_executable(false);
+#else
+  // The new GPU runtime causes occasional hangs on cuda 11, possibly due to the
+  // use of cuda graphs. Keep using the old runtime for now.
+  opts.set_xla_gpu_enable_xla_runtime_executable(true);
+#endif
   opts.set_xla_gpu_enable_custom_fusions(false);
   opts.set_xla_gpu_nccl_termination_timeout_seconds(-1);
   opts.set_xla_gpu_enable_shared_constants(true);
