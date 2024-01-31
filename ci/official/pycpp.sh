4f5b948a7a30403942d24d23dd2a09356cd55cc0
A. Unique TensorFlower <gardener@tensorflow.org>
2024-01-30 16:01:29 -0800

    Addressing TF query analysis errors
    PiperOrigin-RevId: 602866660

--- ci/official/pycpp.sh
+++ ci/official/pycpp.sh
@@ -15,6 +15,8 @@
 # ==============================================================================
 source "${BASH_SOURCE%/*}/utilities/setup.sh"
 
+tfrun bazel query --config=analysis_checker
+
 tfrun bazel test $TFCI_BAZEL_COMMON_ARGS --profile "$TFCI_OUTPUT_DIR/profile.json.gz" --config="${TFCI_BAZEL_TARGET_SELECTING_CONFIG_PREFIX}_pycpp_test"
 
 # Note: the profile can be viewed by visiting chrome://tracing in a Chrome browser.
