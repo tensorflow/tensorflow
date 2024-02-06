d9cec81c14e1553d7f8b048c23de75ccd7d34c0e
mdfaijul <md.faijul.amin@intel.com>
2024-02-05 21:27:11 -0800

    Fix Windows compilation issue.

--- tensorflow/core/kernels/mkl/mkl_quantize_op.cc
+++ tensorflow/core/kernels/mkl/mkl_quantize_op.cc
@@ -496,7 +496,7 @@ class MklQuantizeV2Op : public OpKernel {
       // Estimating scales for quantization.
       const int num_bits = sizeof(T) * 8;
       const float max_abs = std::max(std::abs(min_range), std::abs(max_range));
-      const bool is_signed = std::is_signed<T>::value;
+      const bool is_signed = std::is_same<T, qint8>();
       float target_range;
       if (is_signed) {
         max_range = max_abs;
