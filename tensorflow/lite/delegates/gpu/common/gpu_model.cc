7020b9d3acc5fff0504075ea51b47e0831a6bb9f
Grant Jensen <grantjensen@google.com>
2024-02-13 18:35:05 -0800

    Add Apple A17 Pro Gpu Info
    PiperOrigin-RevId: 606813718

--- tensorflow/lite/delegates/gpu/common/gpu_model.cc
+++ tensorflow/lite/delegates/gpu/common/gpu_model.cc
@@ -248,9 +248,7 @@ absl::Status ReserveGraphTensors(const CreateGpuModelInfo& create_info,
           tensor_desc.UpdateToSupportedStorageType(gpu_info, shape));
       if (gpu_info.IsApiMetal() &&
           storage_type == TensorStorageType::TEXTURE_2D) {
-        const bool a7_gen_gpu =
-            gpu_info.IsApple() && gpu_info.apple_info.IsA7GenerationGpu();
-        if (!a7_gen_gpu) {
+        if (!(gpu_info.IsApple() && gpu_info.apple_info.IsFamilyApple1())) {
           tensor_desc.SetUseBufferForWriteOnlyTexture2d(true);
         }
       }
