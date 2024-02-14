2caa942f286aa33e9445cc2cf968ff03a678ad06
Yue Sheng <yueshengys@google.com>
2024-02-14 09:58:19 -0800

    Let StreamExecutorGpuDevice::GetAllocatorStats() return unimplemented error when we are using the platform allocator through XLA_PYTHON_CLIENT_ALLOCATOR=platform, such that device.memory_stats() returns None instead of throwing an error.
    PiperOrigin-RevId: 607020637

--- third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.cc
+++ third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.cc
@@ -946,8 +946,9 @@ absl::StatusOr<tsl::AllocatorStats> StreamExecutorGpuDevice::GetAllocatorStats()
   auto* allocator_adapter = dynamic_cast<se::MultiDeviceAdapter*>(
       tensorflow::down_cast<PjRtStreamExecutorClient*>(client())->allocator());
   if (!allocator_adapter) {
-    return FailedPrecondition(
-        "GetAllocatorStats() only works with MultiDeviceAdapter allocator");
+    return Unimplemented(
+        "GetAllocatorStats() is only implemented with MultiDeviceAdapter "
+        "allocator");
   }
 
   TF_ASSIGN_OR_RETURN(auto allocator, allocator_adapter->GetAllocator(
