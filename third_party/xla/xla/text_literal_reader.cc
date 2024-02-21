be99dc188b72a6031f3971af863daf6a0d28597d
A. Unique TensorFlower <gardener@tensorflow.org>
2024-02-20 22:39:47 -0800

    Automated Code Change
    PiperOrigin-RevId: 608863698

--- third_party/xla/xla/text_literal_reader.cc
+++ third_party/xla/xla/text_literal_reader.cc
@@ -39,7 +39,7 @@ limitations under the License.
 
 namespace xla {
 
-StatusOr<Literal> TextLiteralReader::ReadPath(absl::string_view path) {
+absl::StatusOr<Literal> TextLiteralReader::ReadPath(absl::string_view path) {
   CHECK(!absl::EndsWith(path, ".gz"))
       << "TextLiteralReader no longer supports reading .gz files";
   std::unique_ptr<tsl::RandomAccessFile> file;
@@ -55,7 +55,7 @@ StatusOr<Literal> TextLiteralReader::ReadPath(absl::string_view path) {
 TextLiteralReader::TextLiteralReader(tsl::RandomAccessFile* file)
     : file_(file) {}
 
-StatusOr<Literal> TextLiteralReader::ReadAllLines() {
+absl::StatusOr<Literal> TextLiteralReader::ReadAllLines() {
   tsl::io::RandomAccessInputStream stream(file_.get());
   tsl::io::BufferedInputStream buf(&stream, 65536);
   std::string shape_string;
