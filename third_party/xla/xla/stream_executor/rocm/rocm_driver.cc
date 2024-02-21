bcac10f2ad4c5ae601a19869d894ab7d0d2680de
ekuznetsov139 <nameless@fastmail.fm>
2024-02-21 05:55:28 -0800

    PR #9664: [ROCm] Build fix
    Imported from GitHub PR https://github.com/openxla/xla/pull/9664
    
    This fixes the ROCm build after it was broken by https://github.com/openxla/xla/commit/bc5d23c2d5f199dd0c22a9cabc5e9864c5c69848.
    Copybara import of the project:
    
    --
    abcbe1ff5951168715228a47d49e224c1b5c0068 by Eugene Kuznetsov <eugene.kuznetsov@amd.com>:
    
    Build fix
    
    Merging this change closes #9664
    
    PiperOrigin-RevId: 608963883

--- third_party/xla/xla/stream_executor/rocm/rocm_driver.cc
+++ third_party/xla/xla/stream_executor/rocm/rocm_driver.cc
@@ -895,7 +895,7 @@ static hipMemAllocationType ToHipAllocationType(
 
 /*static*/ absl::Status GpuDriver::GraphAddMemFreeNode(
     GpuGraphNodeHandle* node, GpuGraphHandle graph,
-    absl::Span<GpuGraphNodeHandle> deps, GpuDevicePtr gpu_dst) {
+    absl::Span<const GpuGraphNodeHandle> deps, GpuDevicePtr gpu_dst) {
   RETURN_IF_ROCM_ERROR(wrap::hipGraphAddMemFreeNode(node, graph, deps.data(),
                                                     deps.size(), gpu_dst),
                        "Failed to add memory free node to a HIP graph");
@@ -904,7 +904,7 @@ static hipMemAllocationType ToHipAllocationType(
 
 /*static*/ absl::Status GpuDriver::GraphAddMemAllocNode(
     GpuGraphNodeHandle* node, GpuGraphHandle graph,
-    absl::Span<GpuGraphNodeHandle> deps, MemAccessFlags access_flags,
+    absl::Span<const GpuGraphNodeHandle> deps, MemAccessFlags access_flags,
     MemLocationType location_type, int device_id,
     MemAllocationType allocation_type, uint64_t size, GpuDevicePtr* d_ptr,
     uint64_t max_pool_size) {
@@ -952,7 +952,7 @@ GpuDriver::GraphGetMemAllocNodeParams(GpuGraphNodeHandle node) {
 
 /* static */ absl::Status GpuDriver::GraphAddMemcpyD2DNode(
     GpuContext* context, GpuGraphNodeHandle* node, GpuGraphHandle graph,
-    absl::Span<GpuGraphNodeHandle> deps, GpuDevicePtr gpu_dst,
+    absl::Span<const GpuGraphNodeHandle> deps, GpuDevicePtr gpu_dst,
     GpuDevicePtr gpu_src, uint64_t size) {
   VLOG(2) << "Add memcpy d2d node to a graph " << graph
           << "; dst: " << reinterpret_cast<void*>(gpu_dst)
@@ -1035,7 +1035,7 @@ struct BitPatternToValue {
 
 /* static */ absl::Status GpuDriver::GraphAddMemsetNode(
     GpuContext* context, GpuGraphNodeHandle* node, GpuGraphHandle graph,
-    absl::Span<GpuGraphNodeHandle> deps, GpuDevicePtr dst,
+    absl::Span<const GpuGraphNodeHandle> deps, GpuDevicePtr dst,
     std::variant<uint8_t, uint16_t, uint32_t> bit_pattern,
     uint64_t num_elements) {
   VLOG(2) << "Add memset node to a graph " << graph
