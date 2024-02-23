7088987f0d45e4b41b4ee87c3c2d2d47707ce5bf
A. Unique TensorFlower <gardener@tensorflow.org>
2024-02-22 21:59:55 -0800

    Automated Code Change
    PiperOrigin-RevId: 609610028

--- third_party/xla/xla/service/convert_mover.h
+++ third_party/xla/xla/service/convert_mover.h
@@ -39,7 +39,7 @@ class ConvertMover : public HloModulePass {
   ConvertMover() = default;
 
   absl::string_view name() const override { return "convert-mover"; }
-  StatusOr<bool> Run(
+  absl::StatusOr<bool> Run(
       HloModule* module,
       const absl::flat_hash_set<absl::string_view>& execution_threads) override;
 };
