acfad21a317e46bcad9dee2c5c56ae88e0f24532
RJ Ascani <rjascani@google.com>
2024-02-27 15:10:31 -0800

    Add is_finite op
    This change implements the stablehlo is_finite op and serves as an example for future op implementations. Each op will have a Create, Prepare, and Evaluate function that will allow users to separate allocation, initialization and inference stages.
    
    PiperOrigin-RevId: 610888654

---
+++ tensorflow/lite/experimental/shlo/ops/is_finite_test.cc
@@ -0,0 +1,82 @@
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/lite/experimental/shlo/ops/is_finite.h"
+
+#include <cmath>
+#include <cstddef>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/types/span.h"
+#include "tensorflow/lite/experimental/shlo/data_type.h"
+#include "tensorflow/lite/experimental/shlo/shape.h"
+#include "tensorflow/lite/experimental/shlo/tensor.h"
+#include "tensorflow/lite/experimental/shlo/tensor_matcher.h"
+#include "tensorflow/lite/experimental/shlo/tensor_with_data.h"
+
+namespace shlo_ref {
+namespace {
+
+using ::shlo_ref::testing::TensorEq;
+
+struct Params {
+  TensorWithData operand;
+  TensorWithData expected;
+};
+
+class IsFiniteTest : public ::testing::TestWithParam<Params> {};
+
+TEST_P(IsFiniteTest, IsFinite) {
+  const auto& params = GetParam();
+
+  IsFiniteOp op = Create(IsFiniteOp::Attributes{});
+
+  EXPECT_OK(Prepare(op, params.operand.tensor(),
+                    Tensor{.type = params.expected.tensor().type}));
+
+  std::vector<std::byte> result_data(op.result.SizeInBytes());
+  op.result.data = result_data.data();
+
+  EXPECT_OK(Evaluate(op));
+  EXPECT_THAT(op.result, TensorEq(params.expected.tensor()));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Unquantized, IsFiniteTest,
+    ::testing::Values(
+        Params{TensorWithData::Create<DataType::kBF16>(
+                   Shape{{7}}, {+NAN, -NAN, -INFINITY, +INFINITY, -1, 0, 1}),
+               TensorWithData::Create<DataType::kI1>(
+                   Shape{{7}}, {false, false, false, false, true, true, true})},
+        Params{TensorWithData::Create<DataType::kF16>(
+                   Shape{{7}}, {+NAN, -NAN, -INFINITY, +INFINITY, -1, 0, 1}),
+               TensorWithData::Create<DataType::kI1>(
+                   Shape{{7}}, {false, false, false, false, true, true, true})},
+        Params{
+            TensorWithData::Create<DataType::kF32>(
+                Shape{{7}}, {+NAN, -NAN, -INFINITY, +INFINITY, -1, 0, 1}),
+            TensorWithData::Create<DataType::kI1>(
+                Shape{{7}}, {false, false, false, false, true, true, true})}));
+
+INSTANTIATE_TEST_SUITE_P(
+    Quantized, IsFiniteTest,
+    ::testing::Values(Params{
+        .operand = TensorWithData::Create<DataType::kSI32, DataType::kF32>(
+            Shape{{7}}, {0, -1, 0, 1, 1, 0, 1}, 0.1, 0),
+        .expected = TensorWithData::Create<DataType::kI1>(
+            Shape{{7}}, {true, true, true, true, true, true, true})}));
+}  // namespace
+}  // namespace shlo_ref
