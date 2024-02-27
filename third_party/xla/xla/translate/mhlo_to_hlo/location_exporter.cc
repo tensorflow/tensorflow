a52f6c8a2c9b73478dcf7cfea88ed24948463404
Zixuan Jiang <zixuanjiang@google.com>
2024-02-27 11:46:37 -0800

    Support `FusedLoc` when processing the source file/line in `CreateOpMetadataFromLocation`.
    `CreateOpMetadataFromLocation` generates `xla::OpMetadata` based on the `mlir::Location`. Before this cl, it cannot process the fused location to obtain the source file/line. This cl fixes this issue.
    
    The `HLO -> MLIR MHLO` conversion generates fused location with source file/line. The `MLIR MHLO -> HLO` needs to handle the fused location to make these two conversions invertible to each other.
    
    PiperOrigin-RevId: 610824763

--- third_party/xla/xla/translate/mhlo_to_hlo/location_exporter.cc
+++ third_party/xla/xla/translate/mhlo_to_hlo/location_exporter.cc
@@ -17,8 +17,17 @@ limitations under the License.
 
 #include <string>
 
+#include "absl/log/log.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringRef.h"
 #include "mlir/IR/Builders.h"  // from @llvm-project
+#include "mlir/IR/Location.h"  // from @llvm-project
+#include "mlir/IR/Operation.h"  // from @llvm-project
+#include "mlir/IR/Visitors.h"  // from @llvm-project
+#include "mlir/Support/LLVM.h"  // from @llvm-project
+#include "xla/translate/mhlo_to_hlo/stack_frame_index_builder.h"
 
 namespace mlir {
 namespace mhlo {
@@ -39,21 +48,15 @@ static std::string GetNameFromLocImpl(Location loc) {
       if (!name.ends_with(":")) {
         loc_names.push_back(name);
       }
-      continue;
     } else if (auto call_loc = curr_loc.dyn_cast<CallSiteLoc>()) {
       // Use location of the Callee to generate the name.
       locs.push_back(call_loc.getCallee());
-      continue;
     } else if (auto fused_loc = curr_loc.dyn_cast<FusedLoc>()) {
       // Push all locations in FusedLoc in reverse order, so locations are
       // visited based on order in FusedLoc.
       auto reversed_fused_locs = llvm::reverse(fused_loc.getLocations());
       locs.append(reversed_fused_locs.begin(), reversed_fused_locs.end());
-      continue;
     }
-
-    // Location is not a supported, so an empty StringRef is added.
-    loc_names.push_back(llvm::StringRef());
   }
 
   return llvm::join(loc_names.begin(), loc_names.end(), ";");
@@ -75,23 +78,29 @@ static std::string GetOpTypeFromLoc(Location loc) {
         op_type = op_type.substr(0, op_type.size() - 1);
         loc_op_types.push_back(op_type);
       }
-      continue;
     } else if (auto call_loc = curr_loc.dyn_cast<CallSiteLoc>()) {
       // Use location of the Callee to generate the name.
       locs.push_back(call_loc.getCallee());
-      continue;
     } else if (auto fused_loc = curr_loc.dyn_cast<FusedLoc>()) {
       // The first location is reserved for op_type.
       if (!fused_loc.getLocations().empty())
         locs.push_back(fused_loc.getLocations()[0]);
-      continue;
     }
-
-    // Location is not a supported, so an empty StringRef is added.
-    loc_op_types.push_back(llvm::StringRef());
   }
 
   return llvm::join(loc_op_types.begin(), loc_op_types.end(), ";");
+}
+
+static void SetSourceFileAndLine(Location loc, xla::OpMetadata& metadata) {
+  if (auto file_line_col_loc = loc.dyn_cast<mlir::FileLineColLoc>()) {
+    metadata.set_source_file(file_line_col_loc.getFilename().str());
+    metadata.set_source_line(file_line_col_loc.getLine());
+  } else if (auto fused_loc = loc.dyn_cast<FusedLoc>()) {
+    for (Location it : fused_loc.getLocations()) {
+      SetSourceFileAndLine(it, metadata);
+    }
+  }
+  LOG(INFO) << "loc is not FileLineColLoc or FusedLoc";
 }
 
 xla::OpMetadata CreateOpMetadataFromLocation(
@@ -105,7 +114,7 @@ xla::OpMetadata CreateOpMetadataFromLocation(
   std::string op_type = GetOpTypeFromLoc(loc);
   metadata.set_op_type(op_type);
 
-  if (auto name_loc = dyn_cast<mlir::NameLoc>(op->getLoc())) {
+  if (auto name_loc = loc.dyn_cast<mlir::NameLoc>()) {
     loc = name_loc.getChildLoc();
     if (isa<mlir::UnknownLoc>(loc)) return metadata;
 
@@ -118,11 +127,7 @@ xla::OpMetadata CreateOpMetadataFromLocation(
     }
   }
 
-  if (auto file_line_col_loc = loc.dyn_cast<mlir::FileLineColLoc>()) {
-    metadata.set_source_file(file_line_col_loc.getFilename().str());
-    metadata.set_source_line(file_line_col_loc.getLine());
-  }
-
+  SetSourceFileAndLine(loc, metadata);
   return metadata;
 }
 
