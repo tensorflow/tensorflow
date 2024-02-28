3237cfbb9f30a0df74da0999a87b915914f24f4c
Fergus Henderson <fergus@google.com>
2024-02-28 04:31:13 -0800

    Add new API function TfLiteOpaqueNodeSetTemporaries, which sets the `temporaries` field of TfLiteOpaqueNode. This is for custom ops that preallocate scratch space.
    See the documentation in item 2 of "Best Practices" in
    third_party/tensorflow/lite/g3doc/guide/ops_custom.md for usage of the
    `temporaries` field with the non-stable custom ops API.  This new API
    function is needed to do the same thing for the stable custom ops API.
    
    PiperOrigin-RevId: 611059749

--- tensorflow/lite/core/c/c_api_opaque_test.cc
+++ tensorflow/lite/core/c/c_api_opaque_test.cc
@@ -15,9 +15,13 @@ limitations under the License.
 
 #include "tensorflow/lite/core/c/c_api_opaque.h"
 
+#include <stddef.h>
+
+#include <cstring>
 #include <memory>
 
 #include <gtest/gtest.h>
+#include "tensorflow/lite/builtin_ops.h"
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/core/c/c_api.h"
 
@@ -452,6 +456,158 @@ TEST(TestTfLiteOpaqueDelegate, GetData_NoDataSetViaOpaqueDelegateBuilder) {
   // delegate builder was not set.
   EXPECT_EQ(nullptr, TfLiteOpaqueDelegateGetData(opaque_delegate));
   TfLiteOpaqueDelegateDelete(opaque_delegate);
+}
+
+// For testing TfLiteOpaqueNodeSetTemporaries, we define a custom op which uses
+// TfLiteOpaqueNodeSetTemporaries in its Prepare method.
+namespace my_custom_op {
+
+struct MyOpData {
+  int temp_tensor_index;
+};
+
+// Allocates MyOpData.
+void* Init(TfLiteOpaqueContext* context, const char* buffer, size_t length) {
+  auto* op_data = new MyOpData{};
+  return op_data;
+}
+
+// Deallocates MyOpData.
+void Free(TfLiteOpaqueContext* context, void* buffer) {
+  delete reinterpret_cast<MyOpData*>(buffer);
+}
+
+// Allocates a temp tensor and stores it using TfLiteOpaqueTensorSetTemporaries.
+// Also does some tests of TfLiteOpaqueTensorTemporaries
+// and TfLiteOpaqueTensorSetTemporaries.
+TfLiteStatus Prepare(TfLiteOpaqueContext* context, TfLiteOpaqueNode* node) {
+  auto* op_data =
+      reinterpret_cast<MyOpData*>(TfLiteOpaqueNodeGetUserData(node));
+
+  // Test #1: calling TfLiteOpaqueNodeSetTemporaries with a negative number
+  // should result in a kTfLiteError status.
+  const int num_temporaries = 1;
+  int temporary_tensor_indices[num_temporaries];
+  TfLiteStatus status =
+      TfLiteOpaqueNodeSetTemporaries(node, temporary_tensor_indices,
+                                     /*num_temporaries=*/-1);
+  TF_LITE_OPAQUE_ENSURE(context, status == kTfLiteError);
+
+  // Test #2: calling TfLiteOpaqueNodeSetTemporaries with zero
+  // should result in a kTfLiteOk status.
+  status = TfLiteOpaqueNodeSetTemporaries(node, temporary_tensor_indices,
+                                          /*num_temporaries=*/0);
+  TF_LITE_OPAQUE_ENSURE(context, status == kTfLiteOk);
+
+  // Test #3: calling TfLiteOpaqueNodeSetTemporaries with a positive number.
+  // For this test, the setup occurs here in Prepare, but the test assertion
+  // is done in Invoke below.
+
+  // Allocate a temp tensor index.
+  TfLiteOpaqueTensorBuilder* builder = TfLiteOpaqueTensorBuilderCreate();
+  TfLiteOpaqueTensorBuilderSetType(builder, kTfLiteFloat32);
+  TfLiteOpaqueTensorBuilderSetAllocationType(builder, kTfLiteArenaRw);
+  TfLiteOpaqueContextAddTensor(context, builder, &temporary_tensor_indices[0]);
+  TfLiteOpaqueTensorBuilderDelete(builder);
+
+  // Store the temp tensor index in the node temporaries and also in MyOpData
+  // (so that we can verify the node's temporaries' contents later).
+  status = TfLiteOpaqueNodeSetTemporaries(node, temporary_tensor_indices,
+                                          num_temporaries);
+  TF_LITE_OPAQUE_ENSURE(context, status == kTfLiteOk);
+  op_data->temp_tensor_index = temporary_tensor_indices[0];
+
+  // Allocate the temp tensor data.
+  TfLiteOpaqueTensor* temp_tensor =
+      TfLiteOpaqueContextGetOpaqueTensor(context, op_data->temp_tensor_index);
+  TfLiteIntArray* temp_size = TfLiteIntArrayCreate(1);
+  temp_size->data[0] = 1;
+  return TfLiteOpaqueContextResizeTensor(context, temp_tensor, temp_size);
+}
+
+// Copies input tensor to output tensor via a previously allocated temp tensor.
+// Also does some tests of TfLiteOpaqueTensorTemporaries /
+// TfLiteOpaqueTensorSetTemporaries.
+TfLiteStatus Invoke(TfLiteOpaqueContext* context, TfLiteOpaqueNode* node) {
+  auto* op_data =
+      reinterpret_cast<MyOpData*>(TfLiteOpaqueNodeGetUserData(node));
+  // Test that TfLiteOpaqueNodeTemporaries() returns the expected result.
+  const int* temporary_tensor_indices;
+  int num_temporaries;
+  TfLiteOpaqueNodeTemporaries(node, &temporary_tensor_indices,
+                              &num_temporaries);
+  TF_LITE_OPAQUE_ENSURE(context, num_temporaries == 1);
+  TF_LITE_OPAQUE_ENSURE(
+      context, temporary_tensor_indices[0] == op_data->temp_tensor_index);
+
+  // Get the temp tensor previously allocated by Prepare().
+  TfLiteOpaqueTensor* temp_tensor =
+      TfLiteOpaqueContextGetOpaqueTensor(context, op_data->temp_tensor_index);
+  TF_LITE_OPAQUE_ENSURE(context,
+                        TfLiteOpaqueTensorType(temp_tensor) == kTfLiteFloat32);
+  TF_LITE_OPAQUE_ENSURE(context, TfLiteOpaqueTensorGetAllocationType(
+                                     temp_tensor) == kTfLiteArenaRw);
+  size_t temp_bytes = TfLiteOpaqueTensorByteSize(temp_tensor);
+  void* temp_data = TfLiteOpaqueTensorData(temp_tensor);
+  TF_LITE_OPAQUE_ENSURE(context, temp_bytes != 0);
+  TF_LITE_OPAQUE_ENSURE(context, temp_data != nullptr);
+
+  // Copy input tensor to temp tensor.
+  EXPECT_EQ(1, TfLiteOpaqueNodeNumberOfInputs(node));
+  const TfLiteOpaqueTensor* input = TfLiteOpaqueNodeGetInput(context, node, 0);
+  size_t input_bytes = TfLiteOpaqueTensorByteSize(input);
+  void* input_data = TfLiteOpaqueTensorData(input);
+  EXPECT_EQ(input_bytes, temp_bytes);
+  std::memcpy(temp_data, input_data, input_bytes);
+
+  // Copy temp tensor to output.
+  EXPECT_EQ(1, TfLiteOpaqueNodeNumberOfOutputs(node));
+  TfLiteOpaqueTensor* output = TfLiteOpaqueNodeGetOutput(context, node, 0);
+  size_t output_bytes = TfLiteOpaqueTensorByteSize(output);
+  void* output_data = TfLiteOpaqueTensorData(output);
+  EXPECT_EQ(output_bytes, temp_bytes);
+  std::memcpy(output_data, temp_data, output_bytes);
+
+  return kTfLiteOk;
+}
+
+}  // namespace my_custom_op
+
+TEST(TestTfLiteOpaqueNode, CustomOpWithSetAndGetTemporaries) {
+  TfLiteModel* model = TfLiteModelCreateFromFile(
+      "tensorflow/lite/testdata/custom_sinh.bin");
+  ASSERT_NE(model, nullptr);
+
+  TfLiteRegistrationExternal* reg =
+      TfLiteRegistrationExternalCreate(kTfLiteBuiltinCustom, "Sinh", 1);
+  TfLiteRegistrationExternalSetPrepare(reg, my_custom_op::Prepare);
+  TfLiteRegistrationExternalSetInit(reg, my_custom_op::Init);
+  TfLiteRegistrationExternalSetFree(reg, my_custom_op::Free);
+  TfLiteRegistrationExternalSetInvoke(reg, my_custom_op::Invoke);
+
+  TfLiteInterpreterOptions* options = TfLiteInterpreterOptionsCreate();
+  TfLiteInterpreterOptionsAddRegistrationExternal(options, reg);
+
+  TfLiteInterpreter* interpreter = TfLiteInterpreterCreate(model, options);
+
+  TfLiteInterpreterOptionsDelete(options);
+
+  ASSERT_EQ(TfLiteInterpreterAllocateTensors(interpreter), kTfLiteOk);
+  TfLiteTensor* input_tensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
+  const float input_value = 42.0f;
+  TfLiteTensorCopyFromBuffer(input_tensor, &input_value, sizeof(float));
+
+  EXPECT_EQ(TfLiteInterpreterInvoke(interpreter), kTfLiteOk);
+
+  const TfLiteTensor* output_tensor =
+      TfLiteInterpreterGetOutputTensor(interpreter, 0);
+  float output_value;
+  TfLiteTensorCopyToBuffer(output_tensor, &output_value, sizeof(float));
+  EXPECT_EQ(output_value, input_value);
+
+  TfLiteInterpreterDelete(interpreter);
+  TfLiteRegistrationExternalDelete(reg);
+  TfLiteModelDelete(model);
 }
 
 }  // namespace
