5c13782c497d6cccf98157876b110570461c2210
Meekail Zain <zainmeekail@gmail.com>
2024-03-05 01:52:13 -0800

    PR #9670: Adds compute capability property to CUDA/ROCm device arrays
    Imported from GitHub PR https://github.com/openxla/xla/pull/9670
    
    Closes https://github.com/google/jax/issues/14222
    
    New device property `compute_capability` is introduced for CUDA/ROCm device arrays, encoding CUDA compute capability and ROCm gfx version respectively.
    Copybara import of the project:
    
    --
    a206374aa09d5f4911becc9089d27241161830f6 by Meekail Zain <zainmeekail@gmail.com>:
    
    Formatting
    
    Merging this change closes #9670
    
    PiperOrigin-RevId: 612752295

--- third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.h
+++ third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.h
@@ -145,7 +145,8 @@ class StreamExecutorGpuDevice : public PjRtStreamExecutorDevice {
   StreamExecutorGpuDevice(int id,
                           std::unique_ptr<LocalDeviceState> local_device_state,
                           std::string device_kind, std::string device_vendor,
-                          int node_id, int slice_index = 0);
+                          std::string compute_capability, int node_id,
+                          int slice_index = 0);
 
   int slice_index() const;
 
@@ -233,6 +234,8 @@ class StreamExecutorGpuClient : public xla::PjRtStreamExecutorClient {
 std::vector<std::unique_ptr<PjRtStreamExecutorDevice>> BuildLocalDevices(
     std::map<int, std::unique_ptr<LocalDeviceState>> local_device_states,
     int node_id);
+
+std::string MakeComputeCapabilityString(const se::DeviceDescription* desc);
 
 struct GpuClientOptions {
   GpuAllocatorConfig allocator_config;
