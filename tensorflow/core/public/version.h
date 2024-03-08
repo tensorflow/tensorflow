b00f5e95531194a17b412331357e1e8dd674aca0
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2024-03-08 13:15:15 -0800

    PR #10082: [ROCm] Enable GpuCommandBuffer::Trace
    Imported from GitHub PR https://github.com/openxla/xla/pull/10082
    
    Copybara import of the project:
    
    --
    e7c8de8fcdadd7bf9b02e4e79afe941afc35d190 by Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>:
    
    [ROCm] Enable GpuCommandBuffer::Trace
    
    --
    b66e16f53a9a6d6a44f51439a5b39e2670effaf7 by Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>:
    
    [ROCm] Allow capturing operations to command buffer that require tracing
    
    Merging this change closes #10082
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/10082 from ROCm:cmmand_trace b66e16f53a9a6d6a44f51439a5b39e2670effaf7
    PiperOrigin-RevId: 614025602

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 1798  // Updated: 2024/3/11
+#define TF_GRAPH_DEF_VERSION 1799  // Updated: 2024/3/12
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
