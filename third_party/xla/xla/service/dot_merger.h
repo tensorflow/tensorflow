d0b3a4ccdec21bc1affe65fabe46ba3ace4a6129
Sergey Kozub <sergeykozub@google.com>
2024-03-11 07:20:45 -0700

    Add sparse dot op support in dot_merger pass
    PiperOrigin-RevId: 614655252

--- third_party/xla/xla/service/dot_merger.h
+++ third_party/xla/xla/service/dot_merger.h
@@ -16,9 +16,12 @@ limitations under the License.
 #ifndef XLA_SERVICE_DOT_MERGER_H_
 #define XLA_SERVICE_DOT_MERGER_H_
 
-#include <functional>
-#include <utility>
+#include <cstdint>
 
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "xla/hlo/ir/hlo_module.h"
 #include "xla/service/hlo_pass_interface.h"
 
 namespace xla {
