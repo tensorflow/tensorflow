03abb6f35303b99efa38877a2b0a69f9cadff5ba
mraunak <83710963+mraunak@users.noreply.github.com>
2024-03-14 10:12:05 -0700

    Update build_tf_windows.sh

--- tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
+++ tensorflow/tools/ci_build/windows/cpu/pip/build_tf_windows.sh
@@ -142,12 +142,20 @@ bazel build ${EXTRA_BUILD_FLAGS}  \
   --output_filter=^$ \
   tensorflow/lite:framework tensorflow/lite/examples/minimal:minimal || exit $?
 
-bazel build \
+if [[ "$TF_NIGHTLY" == 1 ]]; then
+  bazel build \
   --experimental_cc_shared_library \
   --config=release_cpu_windows ${EXTRA_BUILD_FLAGS} \
   --repo_env=WHEEL_NAME=tf_nightly \
   --output_filter=^$ \
   tensorflow/tools/pip_package:wheel || exit $?
+else 
+  bazel build \
+  --experimental_cc_shared_library \
+  --config=release_cpu_windows ${EXTRA_BUILD_FLAGS} \
+  --output_filter=^$ \
+  tensorflow/tools/pip_package:wheel || exit $?
+  
 
 if [[ "$SKIP_TEST" == 1 ]]; then
   exit 0
