23406e08151c02be65cc979bee57803a8d2498b6
Henning Becker <hebecker@google.com>
2024-03-15 04:17:27 -0700

    Rollback of GpuTimer: improve kernel execution time measurement accuracy
    This breaks the gemm algorithm picker test on V100
    
    PiperOrigin-RevId: 616083054

--- third_party/xla/xla/service/gpu/gemm_algorithm_picker.cc
+++ third_party/xla/xla/service/gpu/gemm_algorithm_picker.cc
@@ -25,6 +25,7 @@ limitations under the License.
 #include <vector>
 
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
@@ -240,6 +241,15 @@ class GemmAutotuner {
 
     auto tuned_func = [&](const se::blas::AlgorithmType& algorithm)
         -> absl::StatusOr<se::blas::ProfileResult> {
+      // Do a warm-up run first, without a profile result. This avoids a timeout
+      // and error message if lazy module loading is enabled by ensuring that
+      // lazy loading happens outside the GpuTimer. RunGemm swallows error codes
+      // when profile_result is passed, as it is in the measurement below, but
+      // not otherwise. It is, therefore, consistent to ignore the error code
+      // here.
+      static_cast<void>(RunGemm(gemm_config, lhs_buffer_, rhs_buffer_,
+                                output_buffer_, workspace_buffer,
+                                deterministic_ops_, stream_, algorithm));
       se::blas::ProfileResult profile_result;
       // We expect GemmWithAlgorithm to fail sometimes -- in fact, it will fail
       // for all algorithms if we're targeting < sm_50. But because we pass a
@@ -411,15 +421,28 @@ absl::StatusOr<bool> RunOnInstruction(HloInstruction* gemm,
                  config.GetGpuComputeCapability());
 
   if (update_algorithm) {
+    int64_t new_algorithm{};
     if (algorithm.has_gemm()) {
-      backend_config.set_selected_algorithm(algorithm.gemm().algorithm());
+      new_algorithm = algorithm.gemm().algorithm();
     } else {
       // NOTE: runtime autotuning is no longer available => set to default
-      backend_config.set_selected_algorithm(se::blas::kDefaultAlgorithm);
+      new_algorithm = se::blas::kDefaultAlgorithm;
     }
+
+    if (new_algorithm == old_algorithm &&
+        backend_config.has_selected_algorithm()) {
+      // We don't need to update the backend config if
+      // the algorithm hasn't changed unless previously
+      // the algorithm wasn't set explicitly.
+      return false;
+    }
+
+    backend_config.set_selected_algorithm(new_algorithm);
+    TF_RETURN_IF_ERROR(gemm->set_backend_config(gpu_config));
+    return true;  // We changed `gemm`
   }
-  TF_RETURN_IF_ERROR(gemm->set_backend_config(gpu_config));
-  return old_algorithm != backend_config.selected_algorithm();
+
+  return false;  // No change to `gemm`
 }
 
 absl::StatusOr<bool> RunOnComputation(HloComputation* computation,
