2ffc09fb6f89f2f28e1c63e01340be12142dd29b
Wilsin Gosti <wilsin@google.com>
2024-03-15 16:36:03 -0700

    #tf-data Ramp up `map_fusion` experiment back to 50% job-level.
    PiperOrigin-RevId: 616276271

--- tensorflow/core/data/dataset_utils.cc
+++ tensorflow/core/data/dataset_utils.cc
@@ -1024,8 +1024,8 @@ REGISTER_DATASET_EXPERIMENT("inject_io_prefetch", RandomJobSamplePercentage<0>,
                             AllTasks);
 REGISTER_DATASET_EXPERIMENT("reduce_array_record_dataset_memory_usage",
                             RandomJobSamplePercentage<50>, AllTasks);
-REGISTER_DATASET_EXPERIMENT("map_fusion", RandomJobSamplePercentage<5>,
-                            IndependentHostTasks);
+REGISTER_DATASET_EXPERIMENT("map_fusion", RandomJobSamplePercentage<50>,
+                            AllTasks);
 }  // namespace
 }  // namespace data
 }  // namespace tensorflow
