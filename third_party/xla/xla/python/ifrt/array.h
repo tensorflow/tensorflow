a8228226e05b1eeb13ecf12318abcc6a99415308
Skye Wanderman-Milne <skyewm@google.com>
2024-03-19 12:53:35 -0700

    [IFRT] Introduce ifrt::Array::layout()
    PiperOrigin-RevId: 617265234

--- third_party/xla/xla/python/ifrt/array.h
+++ third_party/xla/xla/python/ifrt/array.h
@@ -23,6 +23,7 @@ limitations under the License.
 #include <vector>
 
 #include "llvm/Support/ExtensibleRTTI.h"
+#include "xla/pjrt/pjrt_layout.h"
 #include "xla/python/ifrt/dtype.h"
 #include "xla/python/ifrt/future.h"
 #include "xla/python/ifrt/shape.h"
@@ -69,6 +70,10 @@ class Array : public llvm::RTTIExtends<Array, Value> {
   virtual const Shape& shape() const = 0;
   virtual const Sharding& sharding() const = 0;
   virtual std::shared_ptr<const Sharding> shared_ptr_sharding() const = 0;
+  // The device memory layout for each shard of the Array. All shards are
+  // assumed to have the same layout. Cannot be nullptr; implementations should
+  // return UNIMPLEMENTED instead.
+  virtual absl::StatusOr<std::unique_ptr<PjRtLayout>> layout() const = 0;
 
   // Breaks an array up into per-device arrays. This is the elimination
   // counterpart of `Client::AssembleArrayFromSingleDeviceArrays()`.
