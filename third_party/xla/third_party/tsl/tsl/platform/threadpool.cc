71080b9e204b83d5370f14c31892749bc5300b3e
A. Unique TensorFlower <gardener@tensorflow.org>
2024-03-22 16:36:55 -0700

    No need to include all of Eigen Tensor to get the threadpool after we moved it to Core.
    PiperOrigin-RevId: 618318519

--- third_party/xla/third_party/tsl/tsl/platform/threadpool.cc
+++ third_party/xla/third_party/tsl/tsl/platform/threadpool.cc
@@ -18,7 +18,7 @@ limitations under the License.
 #define EIGEN_USE_THREADS
 
 #include "absl/types/optional.h"
-#include "unsupported/Eigen/CXX11/Tensor"  // from @eigen_archive
+#include "Eigen/ThreadPool"  // from @eigen_archive
 #include "tsl/platform/blocking_counter.h"
 #include "tsl/platform/context.h"
 #include "tsl/platform/denormal.h"
