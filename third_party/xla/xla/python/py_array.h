fddf11459dcf486ee2e4ef0428b9bc3da29f2aab
A. Unique TensorFlower <gardener@tensorflow.org>
2024-03-22 03:45:15 -0700

    Expose `.layout` on jax.Array. Also add checks in the AOT path to make sure that the input `Array`'s layout matches the layout given to `jax.jit`.
    PiperOrigin-RevId: 618127324

--- third_party/xla/xla/python/py_array.h
+++ third_party/xla/xla/python/py_array.h
@@ -176,10 +176,6 @@ class PyArray : public nanobind::object {
 
   const nanobind::object& sharding() const { return GetStorage().sharding; }
 
-  StatusOr<std::unique_ptr<PjRtLayout>> layout() {
-    return ifrt_array()->layout();
-  }
-
   bool committed() const { return GetStorage().committed; }
 
   const nanobind::object& npy_value() const { return GetStorage().npy_value; }
