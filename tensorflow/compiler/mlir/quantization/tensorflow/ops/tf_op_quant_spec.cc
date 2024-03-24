8024347e2ee1d583afc7ec06b33e80c5714ef5ec
A. Unique TensorFlower <gardener@tensorflow.org>
2024-03-24 00:37:45 -0700

    Automated Code Change
    PiperOrigin-RevId: 618568619

--- tensorflow/compiler/mlir/quantization/tensorflow/ops/tf_op_quant_spec.cc
+++ tensorflow/compiler/mlir/quantization/tensorflow/ops/tf_op_quant_spec.cc
@@ -20,10 +20,12 @@ limitations under the License.
 
 #include "absl/container/flat_hash_set.h"
 #include "llvm/Support/Casting.h"
+#include "mlir/IR/BuiltinAttributes.h"  // from @llvm-project
 #include "mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
 #include "mlir/IR/Operation.h"  // from @llvm-project
 #include "mlir/IR/Value.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/common/quantization_lib/quantization_utils.h"
 #include "tensorflow/compiler/mlir/quantization/tensorflow/quantization_options.pb.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
 
