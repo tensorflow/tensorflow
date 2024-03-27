b9b8b38e1ceb4acf977a03d40caf5c865b902880
Benjamin Kramer <kramerb@google.com>
2024-03-27 05:52:42 -0700

    Run buildifier on all files where it sorts loads differently
    PiperOrigin-RevId: 619503669

--- third_party/xla/xla/service/gpu/fusions/scatter_mlir.cc
+++ third_party/xla/xla/service/gpu/fusions/scatter_mlir.cc
@@ -105,7 +105,7 @@ std::optional<IndexingMap> MlirScatterFusion::ComputeThreadIdToInputIndexing(
   }
   // Compute thread id mapping based on the first update operand.
   Shape scatter_update_shape = scatter->scatter_updates().front()->shape();
-  IndexingMap scatter_update_map = GetDefaultThreadIdToOutputIndexingMap(
+  IndexingMap scatter_update_map = GetDefaultThreadIdIndexingMap(
       launch_dimensions(), config_.unroll_factor, scatter_update_shape, ctx);
 
   // For scatter indices we project indexing for scatter updates and take the
