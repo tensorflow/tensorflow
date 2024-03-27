google/tsl

Internal cleanup of BUILD/.bzl files
PiperOrigin-RevId: 619514702

third_party/mkl_dnn/mkldnn_v1.BUILD
@@ -1,7 +1,7 @@
-load("@tsl//tsl:tsl.bzl", "tf_openmp_copts")
-load("@tsl//third_party/mkl_dnn:build_defs.bzl", "if_mkldnn_openmp")
-load("@xla//xla/tsl/mkl:build_defs.bzl", "if_mkl", "if_mkl_ml")
 load("@bazel_skylib//rules:expand_template.bzl", "expand_template")
+load("@tsl//third_party/mkl_dnn:build_defs.bzl", "if_mkldnn_openmp")
+load("@tsl//tsl:tsl.bzl", "tf_openmp_copts")
+load("@xla//xla/tsl/mkl:build_defs.bzl", "if_mkl", "if_mkl_ml")
 
 exports_files(["LICENSE"])
 

tensorflow/tensorflow

third_party/mkl_dnn/mkldnn_v1.BUILD
@@ -1,7 +1,7 @@
-load("@local_tsl//tsl:tsl.bzl", "tf_openmp_copts")
-load("@org_tensorflow//third_party/mkl_dnn:build_defs.bzl", "if_mkldnn_openmp")
-load("@local_xla//xla/tsl/mkl:build_defs.bzl", "if_mkl", "if_mkl_ml")
 load("@bazel_skylib//rules:expand_template.bzl", "expand_template")
+load("@local_tsl//tsl:tsl.bzl", "tf_openmp_copts")
+load("@local_xla//xla/tsl/mkl:build_defs.bzl", "if_mkl", "if_mkl_ml")
+load("@org_tensorflow//third_party/mkl_dnn:build_defs.bzl", "if_mkldnn_openmp")
 
 exports_files(["LICENSE"])
 