48cc5bba3e1c6b32488020840e27c46ea3131ed8
Chris Minge <chrisminge@google.com>
2024-03-27 16:42:53 -0700

    Remove check that the first thread pool is the default, as it is no longer needed.
    PiperOrigin-RevId: 619704113

--- tensorflow/core/tfrt/tfrt_session/tfrt_session.cc
+++ tensorflow/core/tfrt/tfrt_session/tfrt_session.cc
@@ -604,15 +604,6 @@ class TfrtSessionFactory::ThreadPoolManager {
         auto pool_index = it.index();
         auto num_threads = pool_options.num_threads();
 
-        // For the current use cases the first thread pool is always the default
-        // thread pool. We add this check here to verify the assumption. We can
-        // remove this check once the code stablizes, since it is semantically
-        // meaningful to use non-default thread pool as the first thread pool.
-        if (pool_index == 0 && num_threads != 0) {
-          return errors::InvalidArgument(
-              "The first thread pool must have num_threads = 0");
-        }
-
         if (num_threads != 0) {
           TF_ASSIGN_OR_RETURN(
               auto* thread_pool,
