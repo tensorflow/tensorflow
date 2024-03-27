7edecb917185eae8f3ed6a1f7b5ccc63df1bdfdb
TJ Xu <tjx@nvidia.com>
2024-03-27 11:26:36 -0700

    PR #10858: [NVIDIA GPU] Enforce first collective permute of collective matmul to always run at the top of loop.
    Imported from GitHub PR https://github.com/openxla/xla/pull/10858
    
    This is to port collective matmul related changes from this pr(https://github.com/openxla/xla/pull/10316).
    We'd need the first collective permute of a collective matmul loop always run at the beginning of the loop.
    We set the force_earliest_schedule flag  for that instruction to achieve so.
    Force this schedule order would give about additional 3% speedup on top of existing collective matmul for GPT-3 530B model.
    Copybara import of the project:
    
    --
    eb822e1493c0b0b51438f4b7153df6f9dfaa9276 by TJ Xu <tjx@nvidia.com>:
    
    Enforce first collective permute of collective matmul to always run at
    the top of loop.
    
    Merging this change closes #10858
    
    PiperOrigin-RevId: 619595918

--- third_party/xla/xla/service/gpu/stream_attribute_async_wrapper.cc
+++ third_party/xla/xla/service/gpu/stream_attribute_async_wrapper.cc
@@ -42,6 +42,12 @@ static absl::StatusOr<bool> AsynchronizeInstruction(HloInstruction* instr) {
       computation->CreateAsyncInstructions(
           instr, {}, StreamAttributeAsyncWrapper::kParallelExecutionThread,
           /*replace=*/true));
+  TF_ASSIGN_OR_RETURN(GpuBackendConfig gpu_config,
+                      done->backend_config<GpuBackendConfig>());
+  // Set the false delay of done op to be false so it can be scheduled
+  // far apart from start.
+  gpu_config.set_force_earliest_schedule(false);
+  TF_RETURN_IF_ERROR(done->set_backend_config(gpu_config));
   VLOG(5) << "Created async instruction: " << done->ToString();
   return true;
 }
