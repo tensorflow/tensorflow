c9d1cda451c7822c0052b6a44f1c11fb3fb0ae9e
Xiao Yu <fishx@google.com>
2024-03-28 13:49:52 -0700

    Fix an internal breakage.
    Reverts 573fc841781f593f40ac75e8f8de204c1162f8f9
    
    PiperOrigin-RevId: 620046516

--- tensorflow/c/experimental/ops/gen/cpp/renderers/include_renderer.cc
+++ tensorflow/c/experimental/ops/gen/cpp/renderers/include_renderer.cc
@@ -14,10 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/c/experimental/ops/gen/cpp/renderers/include_renderer.h"
 
-#include "tensorflow/c/experimental/ops/gen/cpp/renderers/renderer.h"
-#include "tensorflow/c/experimental/ops/gen/cpp/renderers/renderer_context.h"
-#include "tensorflow/core/platform/path.h"
-#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/lib/io/path.h"
 
 namespace tensorflow {
 namespace generator {
