cb84303d406aa4a51cee41c3c3d71e63e38e26eb
A. Unique TensorFlower <gardener@tensorflow.org>
2024-04-15 01:21:00 -0700

    Automated Code Change
    PiperOrigin-RevId: 624869111

--- tensorflow/compiler/aot/compile.cc
+++ tensorflow/compiler/aot/compile.cc
@@ -100,7 +100,7 @@ Status CompileXla(xla::CompileOnlyClient* client,
   compile_result->entry_point = aot_opts.entry_point_name();
   compile_result->pointer_size =
       xla::CompileOnlyClient::PointerSizeForTriple(aot_opts.triple());
-  return OkStatus();
+  return absl::OkStatus();
 }
 
 }  // namespace
@@ -259,7 +259,7 @@ Status Main(const MainFlags& flags) {
       nodes.insert(fetch.id().node_name());
     }
     std::cout << absl::StrJoin(nodes, ",");
-    return OkStatus();
+    return absl::OkStatus();
   }
 
   // Read and initialize the graph.
@@ -313,7 +313,7 @@ Status Main(const MainFlags& flags) {
   TF_RETURN_IF_ERROR(GenerateHeader(codegen_opts, config, compile_result,
                                     metadata_result, &header));
   TF_RETURN_IF_ERROR(WriteStringToFile(env, flags.out_header, header));
-  return OkStatus();
+  return absl::OkStatus();
 }
 
 }  // namespace tfcompile
