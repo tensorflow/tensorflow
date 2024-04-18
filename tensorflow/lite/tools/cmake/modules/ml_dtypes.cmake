5fd8c4f1e3487ae07d73c4fc72a104906ae7c071
Quentin Khan <qkhan@google.com>
2024-04-18 04:55:46 -0700

    Sync `ml_dtype` version with the one used in XLA.
    PiperOrigin-RevId: 625992809

--- tensorflow/lite/tools/cmake/modules/ml_dtypes.cmake
+++ tensorflow/lite/tools/cmake/modules/ml_dtypes.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   ml_dtypes
   GIT_REPOSITORY https://github.com/jax-ml/ml_dtypes
   # Sync with tensorflow/third_party/py/ml_dtypes/workspace.bzl
-  GIT_TAG 780b6d0ee01ffbfac45f7ec5418bc08f2b166483
+  GIT_TAG 15b400f4dbad93e725e77e7b8171e4bfebfac874
   # It's not currently possible to shallow clone with a GIT TAG
   # as cmake attempts to git checkout the commit hash after the clone
   # which doesn't work as it's a shallow clone hence a different commit hash.
