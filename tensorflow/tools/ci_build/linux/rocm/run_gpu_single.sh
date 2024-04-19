d410efb9a6734bb6bec861e9ec843f13aba96410
Zachary Streeter <Zachary.Streeter@amd.com>
2024-04-19 18:03:58 +0000

    modified gpu.bazelrc to skip test

--- tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
@@ -71,12 +71,7 @@ if [ -f /usertools/rocm.bazelrc ]; then
              --action_env=TF_PYTHON_VERSION=$PYTHON_VERSION \
              --action_env=TF_ENABLE_ONEDNN_OPTS=0 \
              --test_env=TF_TESTS_PER_GPU=$TF_TESTS_PER_GPU \
-             --test_env=TF_GPU_COUNT=$TF_GPU_COUNT \
-	           -- \
-	           //tensorflow/... \
-             -//tensorflow/dtensor/python/tests:multi_client_test_2gpus \
-             -//tensorflow/dtensor/python/tests:multi_client_test_nccl_2gpus \
-             -//tensorflow/python/distribute/experimental:multi_worker_mirrored_strategy_test_2gpus
+             --test_env=TF_GPU_COUNT=$TF_GPU_COUNT
 else
 	# Legacy style: run configure then build
 	yes "" | $PYTHON_BIN_PATH configure.py
