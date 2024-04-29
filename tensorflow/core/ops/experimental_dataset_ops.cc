6560af9e819c7a6536a02089f137bbd5999ef118
A. Unique TensorFlower <gardener@tensorflow.org>
2024-04-29 04:20:48 -0700

    Automated Code Change
    PiperOrigin-RevId: 629025132

--- tensorflow/core/ops/experimental_dataset_ops.cc
+++ tensorflow/core/ops/experimental_dataset_ops.cc
@@ -12,9 +12,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include "absl/status/status.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/full_type.pb.h"
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/platform/errors.h"
+#include "tsl/platform/errors.h"
 
 namespace tensorflow {
 
