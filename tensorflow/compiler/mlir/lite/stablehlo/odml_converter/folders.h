0ad69af1752c8c226bc6b7c53cb8f2846937f314
Luke Boyer <lukeboyer@google.com>
2024-05-01 15:08:44 -0700

    Add folding for shlo div op and start a pass that canonicalizes and folds shlo within the odml converter.
    PiperOrigin-RevId: 629851095

--- tensorflow/compiler/mlir/lite/stablehlo/odml_converter/folders.h
+++ tensorflow/compiler/mlir/lite/stablehlo/odml_converter/folders.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,22 +12,15 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
-#ifndef TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_PASSES_H_
-#define TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_PASSES_H_
-
-#include <memory>
-
-#include "mlir/IR/BuiltinOps.h"  // from @llvm-project
-#include "mlir/Pass/Pass.h"  // from @llvm-project
+#ifndef TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_FOLDERS_H_
+#define TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_FOLDERS_H_
 
 namespace mlir::odml {
 
-std::unique_ptr<OperationPass<ModuleOp>> CreateSHLOSimplifyPass();
-
-#define GEN_PASS_REGISTRATION
-#include "tensorflow/compiler/mlir/lite/stablehlo/odml_converter/passes.h.inc"
+// Populates the pattern set with all folding patterns. These patterns
+// are intended to have precedence over any other patterns added to the set.
+void PopulateFolderPatterns(RewritePatternSet &patternSet);
 
 }  // namespace mlir::odml
 
-#endif  // TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_PASSES_H_
+#endif  // TENSORFLOW_COMPILER_MLIR_LITE_STABLEHLO_ODML_CONVERTER_FOLDERS_H_
