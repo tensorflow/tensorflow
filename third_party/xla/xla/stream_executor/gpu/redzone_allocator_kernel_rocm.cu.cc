1075195acce90deaa3aa04fc03eea8f821fc61f6
Henning Becker <hebecker@google.com>
2024-05-02 09:50:20 -0700

    Separate backend specific code in redzone_allocator
    - Moves `LoadKernelOrGetPtr` from `asm_compiler.h` into `redzone_allocator_kernel_cuda.cc` since it's the only user of this function.
    - Separate CUDA / ROCm code into separate files: `redzone_allocator_kernel_{cuda|rocm}.cc`.
    - Removes all preprocessor branches from `redzone_allocator.cc`.
    - Removes unnecessary build macros from `redzone_allocator_test` target definition.
    - Reenables ASAN for the `redzone_allocator_test`.
    
    PiperOrigin-RevId: 630093741

--- third_party/xla/xla/stream_executor/gpu/redzone_allocator_kernel_rocm.cu.cc
+++ third_party/xla/xla/stream_executor/gpu/redzone_allocator_kernel_rocm.cu.cc
@@ -1,4 +1,4 @@
-/* Copyright 2023 The OpenXLA Authors.
+/* Copyright 2024 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,14 +13,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "xla/stream_executor/gpu/redzone_allocator.h"
+#include <cstdint>
 
-
-namespace stream_executor {
+#include "absl/status/statusor.h"
+#include "xla/stream_executor/device_memory.h"
+#include "xla/stream_executor/gpu/redzone_allocator_kernel.h"
+#include "xla/stream_executor/kernel.h"
+#include "xla/stream_executor/stream_executor.h"
+#include "xla/stream_executor/stream_executor_pimpl.h"
+#include "tsl/platform/statusor.h"
 
 namespace {
-#if TENSORFLOW_USE_ROCM
-
 __global__ void redzone_checker_kernel(uint8_t* input_buffer,
                                        uint8_t redzone_pattern,
                                        uint64_t buffer_length,
@@ -29,16 +32,20 @@ __global__ void redzone_checker_kernel(uint8_t* input_buffer,
   if (idx >= buffer_length) return;
   if (input_buffer[idx] != redzone_pattern) atomicAdd(out_mismatched_ptr, 1);
 }
-
-#endif
 }  // namespace
 
-void* RedzoneAllocator::kernel_symbol() const {
-#if TENSORFLOW_USE_ROCM
-  return reinterpret_cast<void*>(&redzone_checker_kernel);
-#else
-  return nullptr;
-#endif
+namespace stream_executor {
+
+absl::StatusOr<const RedzoneAllocator::ComparisonKernel*> GetComparisonKernel(
+    StreamExecutor* executor, GpuAsmOpts /*gpu_asm_opts*/) {
+  static auto kernel =
+      TypedKernel<DeviceMemory<uint8>, uint8, uint64_t,
+                  DeviceMemory<uint64_t>>::Create(executor, "redzone_checker",
+                                                  reinterpret_cast<void*>(
+                                                      redzone_checker_kernel));
+
+  if (!kernel.ok()) return kernel.status();
+  return &kernel.value();
 }
 
 }  // namespace stream_executor
