0a559b427aba95e2e41bf018a5b18405151ac608
Kyle Lucke <klucke@google.com>
2024-05-03 10:11:17 -0700

    Use StreamExecutorInterface::GetPlatform rather than deprecated StreamExecutor::platform method.
    PiperOrigin-RevId: 630423698

--- tensorflow/core/tpu/tpu_execute.cc
+++ tensorflow/core/tpu/tpu_execute.cc
@@ -166,7 +166,7 @@ absl::Status UpdateDynamicInputs(
   TF_RET_CHECK(runtime_inputs->size() == compile_time_shapes.size());
   TF_ASSIGN_OR_RETURN(
       auto transfer_manager,
-      xla::TransferManager::GetForPlatform(stream->parent()->platform()));
+      xla::TransferManager::GetForPlatform(stream->parent()->GetPlatform()));
   for (int64_t i = 0; i < compile_time_shapes.size(); i++) {
     // TODO(yunxing): Iterating over thousands of elements can be slow. One way
     // to optimize for fast path without dynamic shapes is add a field in
