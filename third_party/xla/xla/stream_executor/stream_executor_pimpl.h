098085f3aa9047f5cc9af5c64848f43a9da74263
Kyle Lucke <klucke@google.com>
2024-05-03 14:11:15 -0700

    Remove StreamExecutor::platform in favor of StreamExecutorInterface::GetPlatform.
    PiperOrigin-RevId: 630494314

--- third_party/xla/xla/stream_executor/stream_executor_pimpl.h
+++ third_party/xla/xla/stream_executor/stream_executor_pimpl.h
@@ -70,11 +70,6 @@ class StreamExecutor : public StreamExecutorInterface {
 
   const Platform* GetPlatform() const override { return platform_; }
 
-  // Returns a reference to the platform that created this executor.
-  // TODO(b/301020144) Delete this once all callers are migrated to GetPlatform.
-  ABSL_DEPRECATED("Use GetPlatform instead.")
-  const Platform* platform() const { return platform_; }
-
   // Synchronously allocates an array on the device of type T with element_count
   // elements.
   template <typename T>
