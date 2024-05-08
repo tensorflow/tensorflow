fa7f1efc7beedacd41b5faeec8c8e866140a411c
Dan Suh <dansuh@google.com>
2024-05-07 18:56:46 -0700

    Fix variable naming pattern for quantization utility files.
    Along with other readability fixes:
    
    * Remove obsolete TODOs
    * Raname obscure names
    * Include missing header files.
    
    PiperOrigin-RevId: 631619242

--- tensorflow/compiler/mlir/quantization/common/ir/UniformSupport.cc
+++ tensorflow/compiler/mlir/quantization/common/ir/UniformSupport.cc
@@ -15,55 +15,66 @@ limitations under the License.
 
 #include "tensorflow/compiler/mlir/quantization/common/ir/UniformSupport.h"
 
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <iterator>
 #include <numeric>
 
+#include "mlir/Dialect/Quant/QuantTypes.h"  // from @llvm-project
+#include "mlir/IR/Attributes.h"  // from @llvm-project
+#include "mlir/IR/BuiltinAttributeInterfaces.h"  // from @llvm-project
+#include "mlir/IR/BuiltinAttributes.h"  // from @llvm-project
+#include "mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
 #include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
+#include "mlir/IR/Types.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
 
-using namespace mlir;
-using namespace mlir::quantfork;
+namespace mlir::quantfork {
 
-static bool isQuantizablePrimitiveType(Type inputType) {
-  return mlir::isa<FloatType>(inputType);
+static bool isQuantizablePrimitiveType(Type input_type) {
+  return isa<FloatType>(input_type);
 }
 
 ExpressedToQuantizedConverter ExpressedToQuantizedConverter::forInputType(
-    Type inputType) {
-  if (mlir::isa<TensorType, VectorType>(inputType)) {
-    Type elementType = mlir::cast<ShapedType>(inputType).getElementType();
-    if (!isQuantizablePrimitiveType(elementType))
-      return ExpressedToQuantizedConverter{inputType, nullptr};
-    return ExpressedToQuantizedConverter{inputType, elementType};
+    Type input_type) {
+  if (isa<TensorType, VectorType>(input_type)) {
+    Type element_type = cast<ShapedType>(input_type).getElementType();
+    if (!isQuantizablePrimitiveType(element_type))
+      return ExpressedToQuantizedConverter{input_type, nullptr};
+    return ExpressedToQuantizedConverter{input_type, element_type};
   }
   // Supported primitive type (which just is the expressed type).
-  if (isQuantizablePrimitiveType(inputType))
-    return ExpressedToQuantizedConverter{inputType, inputType};
+  if (isQuantizablePrimitiveType(input_type))
+    return ExpressedToQuantizedConverter{input_type, input_type};
   // Unsupported.
-  return ExpressedToQuantizedConverter{inputType, nullptr};
+  return ExpressedToQuantizedConverter{input_type, nullptr};
 }
 
 Type ExpressedToQuantizedConverter::convert(
-    quant::QuantizedType elementalType) const {
-  assert(expressedType && "convert() on unsupported conversion");
-  if (auto tensorType = mlir::dyn_cast<RankedTensorType>(inputType))
-    return RankedTensorType::get(tensorType.getShape(), elementalType);
-  if (auto tensorType = mlir::dyn_cast<UnrankedTensorType>(inputType))
-    return UnrankedTensorType::get(elementalType);
-  if (auto vectorType = mlir::dyn_cast<VectorType>(inputType))
-    return VectorType::get(vectorType.getShape(), elementalType);
+    quant::QuantizedType elemental_type) const {
+  assert(expressed_type && "convert() on unsupported conversion");
+  if (auto tensor_type = dyn_cast<RankedTensorType>(input_type))
+    return RankedTensorType::get(tensor_type.getShape(), elemental_type);
+  if (auto tensor_type = dyn_cast<UnrankedTensorType>(input_type))
+    return UnrankedTensorType::get(elemental_type);
+  if (auto vector_type = dyn_cast<VectorType>(input_type))
+    return VectorType::get(vector_type.getShape(), elemental_type);
 
   // If the expressed types match, just use the new elemental type.
-  if (elementalType.getExpressedType() == expressedType) return elementalType;
+  if (elemental_type.getExpressedType() == expressed_type) {
+    return elemental_type;
+  }
   // Unsupported.
   return nullptr;
 }
 
 ElementsAttr UniformQuantizedPerAxisValueConverter::convert(
-    Attribute realValue) {
-  if (auto attr = mlir::dyn_cast<DenseFPElementsAttr>(realValue)) {
+    Attribute real_value) {
+  if (auto attr = dyn_cast<DenseFPElementsAttr>(real_value)) {
     return convert(attr);
   }
-  // TODO: handles sparse elements attribute
   return nullptr;
 }
 
@@ -72,26 +83,30 @@ DenseElementsAttr UniformQuantizedPerAxisValueConverter::convert(
   // Creates the converter for each chunk. Normally the size of the
   // quantization dim is 3, so we can cache all the converters.
   ShapedType type = attr.getType();
-  size_t dimSize = type.getDimSize(quantizationDim);
-  if (dimSize != scales.size()) {
+  std::size_t dim_size = type.getDimSize(quantization_dim_);
+  if (dim_size != scales_.size()) {
     return {};
   }
   SmallVector<UniformQuantizedValueConverter, 4> converters;
-  converters.reserve(dimSize);
-  for (int i = 0, e = dimSize; i != e; ++i) {
+  converters.reserve(dim_size);
+  for (int i = 0, e = dim_size; i != e; ++i) {
     converters.push_back(getPerChunkConverter(i));
   }
 
   // Scan the elements of the dense elements attributes and quantize them by
   // using the right quantization parameters.
-  int64_t flattenIndex = 0;
+  int64_t flatten_index = 0;
   auto shape = type.getShape();
-  int64_t chunkSize =
-      std::accumulate(std::next(shape.begin(), quantizationDim + 1),
+  int64_t chunk_size =
+      std::accumulate(std::next(shape.begin(), quantization_dim_ + 1),
                       shape.end(), 1, std::multiplies<int64_t>());
-  Type newElementType = IntegerType::get(attr.getContext(), storageBitWidth);
-  return attr.mapValues(newElementType, [&](const APFloat &old) {
-    int chunkIndex = (flattenIndex++) / chunkSize;
-    return converters[chunkIndex % dimSize].quantizeFloatToInt(old);
+  Type new_element_type =
+      IntegerType::get(attr.getContext(), storage_bit_width_);
+  return attr.mapValues(new_element_type, [&](const APFloat &old) {
+    int chunk_index = flatten_index / chunk_size;
+    flatten_index++;
+    return converters[chunk_index % dim_size].quantizeFloatToInt(old);
   });
 }
+
+}  // namespace mlir::quantfork
