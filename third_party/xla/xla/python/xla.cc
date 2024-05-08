cfd031a0e870ae694c0899597fb65ec1ebc68677
Anshuman Goswami <anshumang@google.com>
2024-05-07 18:47:58 -0700

    Add `WaitAtBarrier` with subset of nodes to `DistributedRuntimeCoordinationServiceClient` and plumb that up to the JAX API layer.
    PiperOrigin-RevId: 631617749

--- third_party/xla/xla/python/xla.cc
+++ third_party/xla/xla/python/xla.cc
@@ -643,12 +643,14 @@ NB_MODULE(xla_extension, m_nb) {
       .def(
           "wait_at_barrier",
           [](DistributedRuntimeClient& client, std::string barrier_id,
-             int64_t timeout_in_ms) {
+             int64_t timeout_in_ms,
+             std::optional<absl::Span<const int32_t>> process_ids) {
             nb::gil_scoped_release gil_release;
             xla::ThrowIfError(client.WaitAtBarrier(
-                barrier_id, absl::Milliseconds(timeout_in_ms)));
+                barrier_id, absl::Milliseconds(timeout_in_ms), process_ids));
           },
-          nb::arg("barrier_id"), nb::arg("timeout_in_ms"))
+          nb::arg("barrier_id"), nb::arg("timeout_in_ms"),
+          nb::arg("process_ids") = std::nullopt)
       // The key must be a string, but the value can either be a Python string
       // or bytes object.
       // With Python string values, use `key_value_set()` and
