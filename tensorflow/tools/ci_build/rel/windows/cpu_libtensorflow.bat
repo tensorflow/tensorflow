1dad7f3c6af95763e620d9234a600359c10f018f
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-09 07:50:09 -0700

    Switch libtensorflow (Windows) to use Docker + Clang.
    PiperOrigin-RevId: 632148067

--- tensorflow/tools/ci_build/rel/windows/cpu_libtensorflow.bat
+++ tensorflow/tools/ci_build/rel/windows/cpu_libtensorflow.bat
@@ -1,4 +1,4 @@
-:: Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+:: Copyright 2024 The TensorFlow Authors. All Rights Reserved.
 ::
 :: Licensed under the Apache License, Version 2.0 (the "License");
 :: you may not use this file except in compliance with the License.
@@ -13,10 +13,42 @@
 :: limitations under the License.
 :: =============================================================================
 
-CALL tensorflow\tools\ci_build\release\common_win.bat
+SET TF_DIR=%cd%\github\tensorflow
+SET TF_DOCKER_DIR=C:\src\tensorflow
 
-call tensorflow\tools\ci_build\windows\cpu\bazel\run_libtensorflow.bat || exit /b 1
+set TF_DOCKER_IMAGE="gcr.io/tensorflow-testing/tf-win2019-rbe@sha256:1082ef4299a72e44a84388f192ecefc81ec9091c146f507bc36070c089c0edcc"
 
-copy lib_package %TF_ARTIFACTS_DIR%\lib_package
+docker pull %TF_DOCKER_IMAGE% || exit /b 1
+@echo *****Finished docker image pull: %date% %time%
 
-CALL gsutil cp windows_cpu_libtensorflow_binaries.tar.gz gs://libtensorflow-nightly/prod/tensorflow/release/windows/latest/cpu
+@REM Adjust some variables and such.
+for /f %%i in ('dirname %KOKORO_BAZEL_AUTH_CREDENTIAL%') do set KEYDIR=%%i
+for /f %%i in ('basename %KOKORO_BAZEL_AUTH_CREDENTIAL%') do set KEYFILENAME=%%i
+SET GUESTKEYNAME=C:\keys\%KEYFILENAME%
+
+docker run ^
+    --name tf ^
+    -itd ^
+    --rm ^
+    --env TF_PYTHON_VERSION=%TF_PYTHON_VERSION% ^
+    -v %TF_DIR%:%TF_DOCKER_DIR% ^
+    -v T:\tmp:C:\tmp ^
+    -v %KEYDIR%:C:\keys ^
+    -w %TF_DOCKER_DIR% ^
+    -e GOOGLE_APPLICATION_CREDENTIALS=%GUESTKEYNAME% ^
+    --dns 8.8.8.8 ^
+    --dns 8.8.4.4 ^
+    %TF_DOCKER_IMAGE% ^
+    bash || exit /b 1
+
+docker exec tf ^
+    bash tensorflow/tools/ci_build/windows/libtensorflow_cpu.sh || exit /b 1
+
+set LIB_PACKAGE_ARTIFACT_DIR=%KOKORO_ARTIFACTS_DIR%\lib_package
+mkdir %LIB_PACKAGE_ARTIFACT_DIR%
+
+docker cp tf:/c/src/tensorflow/windows_cpu_libtensorflow_binaries.tar.gz %LIB_PACKAGE_ARTIFACT_DIR%
+
+dir %LIB_PACKAGE_ARTIFACT_DIR%
+
+CALL gsutil cp %KOKORO_ARTIFACTS_DIR%\windows_cpu_libtensorflow_binaries.tar.gz gs://libtensorflow-nightly/prod/tensorflow/release/windows/latest/cpu
