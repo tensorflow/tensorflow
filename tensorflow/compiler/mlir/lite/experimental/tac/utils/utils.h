173a8ef5000f3ee100ea4ef66b2c67db4ee788db
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-12 02:00:03 -0700

    Automated Code Change
    PiperOrigin-RevId: 632928240

--- tensorflow/compiler/mlir/lite/experimental/tac/utils/utils.h
+++ tensorflow/compiler/mlir/lite/experimental/tac/utils/utils.h
@@ -23,6 +23,7 @@ limitations under the License.
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/IR/MLIRContext.h"  // from @llvm-project
+#include "mlir/IR/OwningOpRef.h"  // from @llvm-project
 #include "mlir/Parser/Parser.h"  // from @llvm-project
 
 namespace mlir {
