7fd8e285c203dde472a2671a7cda8ba55bcb49d6
Kuy Mainwaring <kuym@google.com>
2024-05-13 16:12:02 -0700

    [XLA:GPU] Clang-tidy fixes for xla/service/gpu/fusions/mlir/mlir_fusion_emitter.cc
    PiperOrigin-RevId: 633362802

--- third_party/xla/xla/service/gpu/fusions/mlir/mlir_fusion_emitter.h
+++ third_party/xla/xla/service/gpu/fusions/mlir/mlir_fusion_emitter.h
@@ -18,6 +18,7 @@ limitations under the License.
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/status.h"
