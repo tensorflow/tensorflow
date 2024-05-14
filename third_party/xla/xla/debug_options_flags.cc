a1750554dc91ccf3c1585dbc90bcb1b2856855b7
Ilia Sergachev <isergachev@nvidia.com>
2024-05-14 13:33:54 -0700

    PR #12465: [GPU] Fix handling of xla_gpu_require_complete_aot_autotune_results flag.
    Imported from GitHub PR https://github.com/openxla/xla/pull/12465
    
    Copybara import of the project:
    
    --
    4a050dce154b924b98134294f3f15ffce35bdcbb by Ilia Sergachev <isergachev@nvidia.com>:
    
    [GPU] Fix handling of xla_gpu_require_complete_aot_autotune_results flag.
    
    Merging this change closes #12465
    
    PiperOrigin-RevId: 633690035

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -249,6 +249,8 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_reduce_window_rewrite_base_length(32);
 
+  opts.set_xla_gpu_require_complete_aot_autotune_results(false);
+
   return opts;
 }
 
@@ -1427,7 +1429,7 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       "xla_gpu_require_complete_aot_autotune_results",
       bool_setter_for(
           &DebugOptions::set_xla_gpu_require_complete_aot_autotune_results),
-      debug_options->xla_gpu_multi_streamed_windowed_einsum(),
+      debug_options->xla_gpu_require_complete_aot_autotune_results(),
       "Whether to require complete AOT autotuning results."));
   flag_list->push_back(tsl::Flag(
       "xla_gpu_auto_spmd_partitioning_memory_budget_gb",
