42aeac11da3458b463937dc382882ac459d8edea
Harsha H S <hsharsha@users.noreply.github.com>
2024-05-15 01:26:40 -0700

    PR #12463: [ROCm] Fix build break in `xla/service/gpu/runtime/convolution_thunk.cc`
    Imported from GitHub PR https://github.com/openxla/xla/pull/12463
    
    Fix build break introduced in db8f9c0cf2bf08b171aec2cbe3da2fcb074a6207
    Copybara import of the project:
    
    --
    42d5839ffb47362a7f6789ba98ef50b31189dccc by Harsha HS <harsha.havanurshamsundara@amd.com>:
    
    [ROCm] Fix build break in `xla/service/gpu/runtime/convolution_thunk.cc`
    
    --
    fd95e342a4e8de6c8348df3576510584a0020150 by Harsha HS <harsha.havanurshamsundara@amd.com>:
    
    Gaurd header using TENSORFLOW_USE_ROCM
    
    Merging this change closes #12463
    
    PiperOrigin-RevId: 633851634

--- third_party/xla/xla/service/gpu/runtime/convolution_thunk.cc
+++ third_party/xla/xla/service/gpu/runtime/convolution_thunk.cc
@@ -27,6 +27,9 @@ limitations under the License.
 #include "absl/synchronization/mutex.h"
 #include "absl/types/span.h"
 #include "xla/service/buffer_assignment.h"
+#if TENSORFLOW_USE_ROCM
+#include "xla/service/gpu/stream_executor_util.h"
+#endif  // TENSORFLOW_USE_ROCM
 #include "xla/service/gpu/gpu_conv_runner.h"
 #include "xla/service/gpu/runtime/thunk.h"
 #include "xla/stream_executor/device_memory.h"
