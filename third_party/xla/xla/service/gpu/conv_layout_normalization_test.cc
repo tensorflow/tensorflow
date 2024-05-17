a8b7969a726dcbb5c3fd628a6b0d89827ab454f8
Henning Becker <hebecker@google.com>
2024-05-17 11:50:03 -0700

    Remove duplicate test macros from TSL
    `tsl/platform/test.h` used to define a macro `DISABLED_ON_GPU_ROCM` which also exists in
    `xla/tests/test_macros.h`. This led to build failures if both headers were included.
    
    Since `test_macros.h` contains a huge set of similar macros let's remove the one
    from `platform/test.h` and use `test_macros.h` everywhere.
    
    I'm also fixing a CUDA include issue along the way.
    
    PiperOrigin-RevId: 634844088

--- third_party/xla/xla/service/gpu/conv_layout_normalization_test.cc
+++ third_party/xla/xla/service/gpu/conv_layout_normalization_test.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #include "xla/error_spec.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/tests/hlo_test_base.h"
+#include "xla/tests/test_macros.h"
 #include "tsl/platform/test.h"
 
 namespace xla {
