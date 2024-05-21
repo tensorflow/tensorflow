f30bdc86c168313211c03d44dbf2288c3afd3e19
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-21 02:35:06 -0700

    Automated Code Change
    PiperOrigin-RevId: 635733471

--- tensorflow/c/experimental/ops/gen/common/path_config.cc
+++ tensorflow/c/experimental/ops/gen/common/path_config.cc
@@ -16,7 +16,9 @@ limitations under the License.
 
 #include <iostream>
 
+#include "absl/strings/str_join.h"
 #include "tensorflow/core/lib/strings/str_util.h"
+#include "tensorflow/core/platform/types.h"
 
 namespace tensorflow {
 namespace generator {
