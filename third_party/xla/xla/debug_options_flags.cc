8bc3d1e3ae43bc99208ca448f6d9e4154648fcf9
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-22 05:15:05 -0700

    Automated Code Change
    PiperOrigin-RevId: 636127791

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -125,6 +125,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_reduce_scatter_combine_threshold_bytes(kDefaultThreshold);
   opts.set_xla_gpu_enable_all_gather_combine_by_dim(true);
   opts.set_xla_gpu_enable_reduce_scatter_combine_by_dim(true);
+  opts.set_xla_gpu_all_reduce_contiguous(true);
 
   opts.set_xla_gpu_enable_reassociation_for_converted_ar(true);
 
