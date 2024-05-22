1611584f25591cba365a7295ec4a1c4788f4578f
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-22 03:52:52 -0700

    Automated Code Change
    PiperOrigin-RevId: 636109295

--- tensorflow/lite/graph_info_test.cc
+++ tensorflow/lite/graph_info_test.cc
@@ -25,7 +25,6 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "tensorflow/lite/core/c/common.h"
-#include "tensorflow/lite/testing/util.h"
 
 namespace tflite {
 namespace {
