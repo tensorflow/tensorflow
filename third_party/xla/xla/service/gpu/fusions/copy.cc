a5969894823fc7d3d2dd15778f22f48bb62b244a
Oleg Shyshkov <shyshkov@google.com>
2024-05-22 04:46:38 -0700

    [XLA:GPU] Move buffer assignment logic to MemcpyFusion::Emit.
    GetFusionEmitter should be responsible only for matching the correct emitter. If buffer assignment fails, there is no way to fix that and we propagate status to the caller anyway, so it's better to do it in MemcpyFusion::Emit.
    
    PiperOrigin-RevId: 636121302

--- third_party/xla/xla/service/gpu/fusions/copy.cc
+++ third_party/xla/xla/service/gpu/fusions/copy.cc
@@ -15,13 +15,22 @@ limitations under the License.
 #include "xla/service/gpu/fusions/copy.h"
 
 #include <memory>
+#include <vector>
 
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
+#include "xla/service/buffer_assignment.h"
 #include "xla/service/gpu/fusions/fusion_emitter.h"
+#include "xla/service/gpu/hlo_traversal.h"
 #include "xla/service/gpu/ir_emitter_context.h"
 #include "xla/service/gpu/runtime/copy_thunk.h"
 #include "xla/service/gpu/runtime/thunk.h"
+#include "xla/shape.h"
+#include "xla/shape_util.h"
+#include "tsl/platform/errors.h"
+#include "tsl/platform/statusor.h"
 
 namespace xla {
 namespace gpu {
@@ -29,14 +38,36 @@ namespace gpu {
 absl::StatusOr<FusionEmissionResult> MemcpyFusion::Emit(
     IrEmitterContext& ir_emitter_context,
     const HloFusionInstruction& fusion) const {
+  std::vector<BufferAllocation::Slice> src_buffers;
+  for (const HloInstructionAdaptor& root_adaptor : analysis_.fusion_roots()) {
+    const HloInstruction* root = &root_adaptor.instruction();
+    const HloInstruction* src_instr =
+        fusion.operand(root->operand(0)->parameter_number());
+    TF_ASSIGN_OR_RETURN(BufferAllocation::Slice slice,
+                        buffer_assignment_->GetUniqueSlice(src_instr, {}));
+    src_buffers.push_back(slice);
+  }
+
+  std::vector<BufferAllocation::Slice> dst_buffers;
+  TF_RETURN_IF_ERROR(ShapeUtil::ForEachSubshapeWithStatus(
+      fusion.shape(), [&](const Shape& subshape, const ShapeIndex& index) {
+        if (!subshape.IsArray()) {
+          return absl::OkStatus();
+        }
+        TF_ASSIGN_OR_RETURN(BufferAllocation::Slice slice,
+                            buffer_assignment_->GetUniqueSlice(&fusion, index));
+        dst_buffers.push_back(slice);
+        return absl::OkStatus();
+      }));
+
   FusionEmissionResult result;
-  for (int i = 0; i < src_buffers_.size(); ++i) {
-    if (src_buffers_[i] != dst_buffers_[i]) {
+  for (int i = 0; i < src_buffers.size(); ++i) {
+    if (src_buffers[i] != dst_buffers[i]) {
       result.thunks.emplace_back(std::make_unique<DeviceToDeviceCopyThunk>(
           Thunk::ThunkInfo::WithProfileAnnotation(&fusion),
-          /*source_buffer=*/src_buffers_[i],
-          /*destination_buffer=*/dst_buffers_[i],
-          /*mem_size=*/src_buffers_[i].size()));
+          /*source_buffer=*/src_buffers[i],
+          /*destination_buffer=*/dst_buffers[i],
+          /*mem_size=*/src_buffers[i].size()));
     }
   }
   return result;
