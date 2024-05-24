306833457d32dd27a198ec8f6839b4966f941d4d
Kuy Mainwaring <kuym@google.com>
2024-05-24 10:42:30 -0700

    [XLA:GPU] Clang-tidy cleanup for xla/service/gpu/cudnn_workspace_rewriter.h
    PiperOrigin-RevId: 636966632

--- third_party/xla/xla/service/gpu/cudnn_workspace_rewriter.h
+++ third_party/xla/xla/service/gpu/cudnn_workspace_rewriter.h
@@ -16,9 +16,12 @@ limitations under the License.
 #ifndef XLA_SERVICE_GPU_CUDNN_WORKSPACE_REWRITER_H_
 #define XLA_SERVICE_GPU_CUDNN_WORKSPACE_REWRITER_H_
 
+#include <string>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
-#include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/service/hlo_pass_interface.h"
 #include "xla/stream_executor/dnn.h"
