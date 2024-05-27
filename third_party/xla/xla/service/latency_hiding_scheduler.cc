d7dbc60827a7b36cddfa25f16def42449adc9c61
mmakevic-amd <Milica.Makevic@amd.com>
2024-05-27 04:53:13 -0700

    PR #13089: Fix reduce_large_row_to_scalar.hlo.test
    Imported from GitHub PR https://github.com/openxla/xla/pull/13089
    
    Copybara import of the project:
    
    --
    ae97058c01ca57107a2566a6f190d51f5ad4ca0e by mmakevic <Milica.Makevic@amd.com>:
    
    Fix reduce_large_row_to_scalar.hlo.test
    
    Merging this change closes #13089
    
    Reverts changelist 637594837
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/13089 from ROCm:ci_reduce_large_row_to_scalar ae97058c01ca57107a2566a6f190d51f5ad4ca0e
    PiperOrigin-RevId: 637605119

--- third_party/xla/xla/service/latency_hiding_scheduler.cc
+++ third_party/xla/xla/service/latency_hiding_scheduler.cc
@@ -104,6 +104,14 @@ bool LatencyEstimator::IsAsyncPair(const HloGraphNode& from,
          from_op.inner == target_op.inner;
 }
 
+bool LatencyEstimator::IsP2pPair(const HloGraphNode& from,
+                                 const HloGraphNode& target) const {
+  return (from.GetInstr().opcode() == HloOpcode::kSend &&
+          target.GetInstr().opcode() == HloOpcode::kSendDone) ||
+         (from.GetInstr().opcode() == HloOpcode::kRecv &&
+          target.GetInstr().opcode() == HloOpcode::kRecvDone);
+}
+
 LatencyEstimator::TimeCost ApproximateLatencyEstimator::GetLatencyBetween(
     const HloGraphNode& from, const HloGraphNode& target) const {
   if (IsAsyncPair(from, target)) {
