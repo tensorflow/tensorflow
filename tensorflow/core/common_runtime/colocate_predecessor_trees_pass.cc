e8a8c26c68fcaf792807743db17865e6f93bbf74
A. Unique TensorFlower <gardener@tensorflow.org>
2024-05-31 10:35:55 -0700

    Reimplementation of ICI weight distribution.
    1. Less Fill op generated in XlaBroadcast pass in MLIR.
    
    2. Propagate colocation attr for a specified pattern in a graphdef optimization post MLIR bridge but before placer.
    
    PiperOrigin-RevId: 639079950

--- tensorflow/core/common_runtime/colocate_predecessor_trees_pass.cc
+++ tensorflow/core/common_runtime/colocate_predecessor_trees_pass.cc
@@ -45,15 +45,10 @@ namespace {
 
 constexpr absl::string_view kClassAttr = "_class";
 
-// Check if the node is a valid tree node. Noticed this node must not be the
-// root of the tree. We find root of the tree in other place.
-// For a valid tree node, it must
-// 1. not a arg node
-// 2. not have device attr (neither assigned device nor requested device)
-// 3. not have colocation attr
-// 4. must register for CPU
-// 5. only have one output node
-bool IsValidTreeNode(const Node& node, bool in_node_mode) {
+bool IsValidFillOp(const Node& node) {
+  if (node.type_string() != "Fill") {
+    return false;
+  }
   if (node.IsArg()) {
     return false;
   }
@@ -69,25 +64,13 @@ bool IsValidTreeNode(const Node& node, bool in_node_mode) {
   if (!KernelDefAvailable(DeviceType(DEVICE_CPU), node.def())) {
     return false;
   }
-
-  int num_parents_to_tree_nodes = 0;
-  auto parent_nodes = in_node_mode ? node.out_nodes() : node.in_nodes();
-  for (auto parent_node : parent_nodes) {
-    if (in_node_mode && (parent_node->IsExit() || parent_node->IsSink()))
-      continue;
-    if (!in_node_mode && parent_node->IsSource()) continue;
-    num_parents_to_tree_nodes++;
-  }
-  if (num_parents_to_tree_nodes != 1) return false;
   return true;
 }
 
-// Check if the node is potential root node. For a valid root node, it must
-// 1. have requested device attr
-// 2. not a arg node
-// 3. must register for CPU has device type must be CPU
-// 4. the output node can only be exit or sink node
-bool IsPotentialRootNode(const Node& node) {
+bool IsValidIdentityNode(const Node& node) {
+  if (node.type_string() != "Identity") {
+    return false;
+  }
   if (node.requested_device().empty()) {
     return false;
   }
@@ -106,52 +89,44 @@ bool IsPotentialRootNode(const Node& node) {
   return true;
 }
 
-// Find all tree nodes for the root node. Otherwise, return false.
-std::optional<absl::flat_hash_set<Node*>> FindTreeNodes(Node* potential_root) {
-  absl::flat_hash_set<Node*> tree_nodes;
-  tree_nodes.insert(potential_root);
-
-  auto seek_tree_nodes = [&](bool in_node_mode) {
-    std::queue<Node*> pending_nodes;
-    auto nodes_to_potential_nodes =
-        in_node_mode ? potential_root->in_nodes() : potential_root->out_nodes();
-    for (Node* node : nodes_to_potential_nodes) {
-      if (in_node_mode && node->IsSource()) continue;
-      if (!in_node_mode && (node->IsSink() || node->IsExit())) continue;
-      pending_nodes.push(node);
+std::optional<std::string> GetColocateStringName(const Node& node) {
+  std::string device = "";
+  std::string colocation_prefix = "loc:@";
+  std::string colocation_name;
+  for (auto node : node.out_nodes()) {
+    if (!IsValidIdentityNode(*node)) return std::nullopt;
+    if (device == "") {
+      device = node->requested_device();
+      colocation_name = absl::StrCat(colocation_prefix, node->name());
+    } else if (device != node->requested_device()) {
+      return std::nullopt;
     }
-    while (!pending_nodes.empty()) {
-      Node* node = pending_nodes.front();
-      pending_nodes.pop();
-      if (tree_nodes.find(node) != tree_nodes.end()) {
-        return false;
-      }
-      if (!IsValidTreeNode(*node, in_node_mode)) {
-        return false;
-      }
-      tree_nodes.insert(node);
-      auto nodes_to_potential_node =
-          in_node_mode ? node->in_nodes() : node->out_nodes();
-      for (Node* node : nodes_to_potential_node) {
-        if (in_node_mode && node->IsSource()) continue;
-        if (!in_node_mode && (node->IsSink() || node->IsExit())) continue;
-        pending_nodes.push(node);
-      }
+  }
+  return colocation_name;
+}
+
+bool AreAllInNodesQualifiedConst(const Node& node) {
+  for (auto in_node : node.in_nodes()) {
+    if (in_node->type_string() != "Const") {
+      return false;
     }
-    return true;
-  };
-
-  if (!seek_tree_nodes(/*in_node_mode=*/true)) {
-    return std::nullopt;
+    if (in_node->IsArg()) {
+      return false;
+    }
+    if (in_node->has_assigned_device_name()) {
+      return false;
+    }
+    if (!in_node->requested_device().empty()) {
+      return false;
+    }
+    if (HasNodeAttr(in_node->def(), kClassAttr)) {
+      return false;
+    }
+    if (!KernelDefAvailable(DeviceType(DEVICE_CPU), in_node->def())) {
+      return false;
+    }
   }
-
-  // size of tree node must larger than one which means the tree contains at
-  // least one non root node.
-  if (tree_nodes.size() == 1) {
-    return std::nullopt;
-  }
-
-  return tree_nodes;
+  return true;
 }
 
 // Propagate colocation info from root node to each tree nodes.
@@ -185,18 +160,21 @@ Status ColocatePredecessorTreesPass::Run(
                                options.flib_def);
   }
 
-  absl::flat_hash_map<Node*, absl::flat_hash_set<Node*>> tree_nodes_map;
   for (Node* node : graph->nodes()) {
-    if (IsPotentialRootNode(*node)) {
-      std::optional<absl::flat_hash_set<Node*>> nodes = FindTreeNodes(node);
-      if (nodes.has_value()) {
-        tree_nodes_map[node] = *std::move(nodes);
+    if (!IsValidFillOp(*node)) {
+      continue;
+    }
+    auto colocation_name = GetColocateStringName(*node);
+    if (!colocation_name.has_value()) continue;
+    if (AreAllInNodesQualifiedConst(*node)) {
+      node->AddAttr(std::string(kClassAttr), {*colocation_name});
+      for (auto in_node : node->in_nodes()) {
+        in_node->AddAttr(std::string(kClassAttr), {*colocation_name});
+      }
+      for (auto out_node : node->out_nodes()) {
+        out_node->AddAttr(std::string(kClassAttr), {*colocation_name});
       }
     }
-  }
-
-  for (auto& [root_node, tree_nodes] : tree_nodes_map) {
-    PropagateColocationInfo(root_node, tree_nodes);
   }
 
   if (VLOG_IS_ON(1)) {
