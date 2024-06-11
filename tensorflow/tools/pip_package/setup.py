8fde1f4381aeee29f18c00c2ffcf5cfe5c15c229
Michael Hudgins <michaelhudgins@google.com>
2024-06-11 11:36:14 -0700

    Update TensorFlow ml_dtypes dependency to >= 0.3.1 < 0.5.0
    PiperOrigin-RevId: 642337465

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -83,7 +83,7 @@ REQUIRED_PACKAGES = [
     'google_pasta >= 0.1.1',
     'h5py >= 3.10.0',
     'libclang >= 13.0.0',
-    'ml_dtypes ~= 0.3.1',
+    'ml_dtypes >= 0.3.1, < 0.5.0',
     # TODO(b/304751256): Adjust the numpy pin to a single version, when ready
     'numpy >= 1.23.5, < 2.0.0 ; python_version <= "3.11"',
     'numpy >= 1.26.0, < 2.0.0 ; python_version >= "3.12"',
