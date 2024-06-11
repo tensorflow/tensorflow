48eb446369e6b2e3c5605590c1e95d270805ef62
Clive Verghese <cliveverghese@google.com>
2024-06-11 13:14:25 -0700

    Move XplaneEventMutator and XplaneEventMutatorFactory to a separate file.
    This would simplify the preprocess_xplane.h and would allow for each mutator to be in it's own header.
    
    PiperOrigin-RevId: 642368231

---
+++ third_party/xla/third_party/tsl/tsl/profiler/utils/xplane_mutators.h
@@ -0,0 +1,63 @@
+/* Copyright 2024 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TENSORFLOW_TSL_PROFILER_UTILS_XPLANE_MUTATORS_H_
+#define TENSORFLOW_TSL_PROFILER_UTILS_XPLANE_MUTATORS_H_
+
+#include <memory>
+#include <vector>
+
+#include "tsl/profiler/utils/xplane_builder.h"
+
+namespace tsl {
+namespace profiler {
+
+/*
+ * Subclass of this interface will perform different mutatation to the event.
+ * Checking eligibilities of event mutation is not responsible of this class.
+ */
+class XplaneEventMutator {
+ public:
+  virtual ~XplaneEventMutator() = default;
+
+  // Mutate event by event specified by the event_metadata.
+  virtual void Mutate(XEventBuilder& builder) = 0;
+  // Mutate line by line if event_metadata() return nullptr.
+  virtual void MutateEventsInLine(XLineBuilder& line) = 0;
+
+  const XEventMetadata* event_metadata() const { return event_metadata_; }
+
+ protected:
+  explicit XplaneEventMutator(XEventMetadata* event_metadata)
+      : event_metadata_(event_metadata) {}
+
+  XEventMetadata* event_metadata_;
+};
+
+class XplaneEventMutatorFactory {
+ public:
+  virtual ~XplaneEventMutatorFactory() = default;
+
+  virtual std::vector<std::unique_ptr<XplaneEventMutator>> CreateMutators(
+      XPlaneBuilder& xplane) const = 0;
+
+ protected:
+  XplaneEventMutatorFactory() = default;
+};
+
+}  // namespace profiler
+}  // namespace tsl
+
+#endif  // TENSORFLOW_TSL_PROFILER_UTILS_XPLANE_MUTATORS_H_
