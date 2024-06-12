611e003aa44ad5c1297e5085269f9c91f012bfc9
Raviteja Gorijala <gorijala@google.com>
2024-06-12 11:22:29 -0700

    Use tensorboard nightly 2.18.0a
    PiperOrigin-RevId: 642677285

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -114,7 +114,7 @@ REQUIRED_PACKAGES = [
     # dependencies on the release branch is updated to the stable releases (RC
     # or final). For example, 'keras-nightly ~= 2.14.0.dev' will be replaced by
     # 'keras >= 2.14.0rc0, < 2.15' on the release branch after the branch cut.
-    'tb-nightly ~= 2.17.0.a',
+    'tb-nightly ~= 2.18.0.a',
     'keras-nightly >= 3.2.0.dev',
 ]
 REQUIRED_PACKAGES = [p for p in REQUIRED_PACKAGES if p is not None]
