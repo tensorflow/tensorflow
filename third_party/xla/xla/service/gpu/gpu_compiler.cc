84276966b916f7991cfa2e37fb7a23969dcbfea9
Bixia Zheng <bixia@google.com>
2024-06-15 01:40:58 -0700

    [xla:gpu] Move the code that add SPMD pipeline to a utility function.
    Also add a test for the GPU SPMD pipeline.
    
    PiperOrigin-RevId: 643567454

--- third_party/xla/xla/service/gpu/gpu_compiler.cc
+++ third_party/xla/xla/service/gpu/gpu_compiler.cc
@@ -65,8 +65,8 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_module_group.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/hlo/ir/hlo_schedule.h"
-#include "xla/hlo/transforms/hlo_constant_splitter.h"
 #include "xla/maybe_owning.h"
+#include "xla/service/algebraic_simplifier.h"
 #include "xla/service/all_gather_broadcast_reorder.h"
 #include "xla/service/all_gather_combiner.h"
 #include "xla/service/all_reduce_combiner.h"
@@ -141,6 +141,7 @@ limitations under the License.
 #include "xla/service/gpu/gpu_reduce_scatter_creator.h"
 #include "xla/service/gpu/gpu_sanitize_constant_names.h"
 #include "xla/service/gpu/gpu_scatter_expander.h"
+#include "xla/service/gpu/gpu_spmd_pipeline.h"
 #include "xla/service/gpu/gpu_windowed_einsum_handler.h"
 #include "xla/service/gpu/hlo_fusion_stats.h"
 #include "xla/service/gpu/ir_emission_utils.h"
@@ -205,14 +206,11 @@ limitations under the License.
 #include "xla/service/rng_expander.h"
 #include "xla/service/scatter_expander.h"
 #include "xla/service/scatter_simplifier.h"
-#include "xla/service/sharding_propagation.h"
 #include "xla/service/sharding_remover.h"
 #include "xla/service/simplify_fp_conversions.h"
 #include "xla/service/slice_sinker.h"
 #include "xla/service/slow_operation_alarm.h"
 #include "xla/service/sort_simplifier.h"
-#include "xla/service/spmd/collective_permute_motion.h"
-#include "xla/service/spmd/stateful_rng_spmd_partitioner.h"
 #include "xla/service/stable_sort_expander.h"
 #include "xla/service/stochastic_convert_decomposer.h"
 #include "xla/service/sub_byte_normalization.h"
@@ -547,15 +545,14 @@ absl::Status RunPreSPMDPartitionerPasses(HloModule* hlo_module) {
 absl::Status RunSPMDPasses(
     HloModule* hlo_module, const Compiler::TargetConfig& gpu_target_config,
     const AlgebraicSimplifierOptions& layout_insensitive_algsimp_opts) {
-  const int64_t num_partitions = hlo_module->config().num_partitions();
   bool auto_sharding = hlo_module->config().use_auto_spmd_partitioning();
-
 #ifndef PLATFORM_GOOGLE
   if (auto_sharding) {
     LOG(ERROR) << "GPU autosharding is not yet available in open source.";
   }
 #endif
 
+  const int64_t num_partitions = hlo_module->config().num_partitions();
   if (num_partitions > 1) {
     if (!hlo_module->config().use_spmd_partitioning()) {
       return InvalidArgument(
@@ -563,83 +560,46 @@ absl::Status RunSPMDPasses(
           num_partitions);
     }
     HloPassPipeline spmd_pipeline("spmd-partitioner");
-    HloPassPipeline& spmd_simplify =
-        spmd_pipeline.AddPass<HloPassFix<HloPassPipeline>>("spmd-simplify");
-
-    se::GpuComputeCapability gpu_version =
-        gpu_target_config.device_description.gpu_compute_capability();
-    spmd_simplify.AddPass<GpuAlgebraicSimplifier>(
-        layout_insensitive_algsimp_opts, gpu_version);
-    spmd_simplify.AddPass<SortSimplifier>();
-    spmd_simplify.AddPass<TupleSimplifier>();
-    spmd_simplify.AddPass<ScatterExpander>(
-        ScatterExpander::kEliminateSimpleScatters);
-    spmd_simplify.AddPass<GatherExpander>(
-        GatherExpander::kEliminateSimpleGathers);
-    spmd_simplify.AddPass<WhileLoopConstantSinking>();
-    spmd_simplify.AddPass<WhileLoopSimplifier>();
-
-    ReshapeMoverOptions reshape_mover_options;
-    reshape_mover_options.reshape_of_1d_broadcast_is_cheap = true;
-    spmd_simplify.AddPass<ReshapeMover>(reshape_mover_options);
-    // Run AlgebraicSimplifier directly before HloConstantFolding, because we
-    // need to simplify DynamicSlice(Broadcast) away. Constant folding of
-    // DynamicSlice can be quite costly, as the whole operand will be evaluated.
-    // We run AlgebraicSimplifier as HloPassFix to make sure all simplifications
-    // have been done before running HloConstantFolding. This is necessary
-    // because simplifications create new instructions which may not be visited
-    // in the same iteration of AlgebraicSimplifier.
-    spmd_simplify.AddPass<HloPassFix<GpuAlgebraicSimplifier>>(
-        layout_insensitive_algsimp_opts, gpu_version);
-    spmd_simplify.AddPass<HloConstantFolding>();
-    spmd_simplify.AddPass<ConditionalSimplifier>();
-
-    spmd_pipeline.AddPass<HloConstantSplitter>();
-    spmd_simplify.AddPass<HloDCE>();
-
+    AddSPMDPasses(
+        hlo_module, layout_insensitive_algsimp_opts,
+        gpu_target_config.device_description.gpu_compute_capability(),
+        spmd_pipeline,
 #ifdef PLATFORM_GOOGLE
-    if (auto_sharding) {
-      AutoShardingOption option;
-      option.enable = true;
-      if (!hlo_module->config().auto_spmd_partitioning_mesh_shape().empty()) {
-        option.device_mesh_shape =
-            hlo_module->config().auto_spmd_partitioning_mesh_shape();
-      } else {
-        // Use a simple mesh shape if not specified.
-        option.device_mesh_shape = {
-            gpu_target_config.device_description.core_count(), 1};
-      }
-      if (!hlo_module->config().auto_spmd_partitioning_mesh_ids().empty()) {
-        option.device_mesh_ids =
-            hlo_module->config().auto_spmd_partitioning_mesh_ids();
-      }
-      option.memory_budget_per_device =
-          hlo_module->config()
-              .debug_options()
-              .xla_gpu_auto_spmd_partitioning_memory_budget_gb() *
-          1024 * 1024 * 1024;
-      option.memory_budget_ratio =
-          hlo_module->config()
-              .debug_options()
-              .xla_gpu_auto_spmd_partitioning_memory_budget_ratio();
-      spmd_pipeline.AddPass<AutoSharding>(option);
-    }
+        [&](HloPassPipeline& pipeline) {
+          if (auto_sharding) {
+            AutoShardingOption option;
+            option.enable = true;
+            if (!hlo_module->config()
+                     .auto_spmd_partitioning_mesh_shape()
+                     .empty()) {
+              option.device_mesh_shape =
+                  hlo_module->config().auto_spmd_partitioning_mesh_shape();
+            } else {
+              // Use a simple mesh shape if not specified.
+              option.device_mesh_shape = {
+                  gpu_target_config.device_description.core_count(), 1};
+            }
+            if (!hlo_module->config()
+                     .auto_spmd_partitioning_mesh_ids()
+                     .empty()) {
+              option.device_mesh_ids =
+                  hlo_module->config().auto_spmd_partitioning_mesh_ids();
+            }
+            option.memory_budget_per_device =
+                hlo_module->config()
+                    .debug_options()
+                    .xla_gpu_auto_spmd_partitioning_memory_budget_gb() *
+                1024 * 1024 * 1024;
+            option.memory_budget_ratio =
+                hlo_module->config()
+                    .debug_options()
+                    .xla_gpu_auto_spmd_partitioning_memory_budget_ratio();
+            spmd_pipeline.AddPass<AutoSharding>(option);
+          }
+        });
+#else
+        std::nullopt);
 #endif  // PLATFORM_GOOGLE
-
-    spmd_pipeline.AddPass<ShardingPropagation>(
-        /*is_spmd=*/true, /*propagate_metadata=*/false,
-        hlo_module->config().allow_spmd_sharding_propagation_to_output());
-    spmd_pipeline.AddPass<spmd::StatefulRngSpmdPartitioner>(
-        num_partitions, hlo_module->config().replica_count(),
-        hlo_module->config()
-            .debug_options()
-            .xla_gpu_threshold_for_windowed_einsum_mib(),
-        hlo_module->config()
-            .debug_options()
-            .xla_gpu_multi_streamed_windowed_einsum(),
-        /*skip_checking_windowed_einsum_users=*/true,
-        /*disable_ag_rewrite_for_multiple_consumers=*/true);
-    spmd_pipeline.AddPass<CollectivePermuteMotion>();
     return spmd_pipeline.Run(hlo_module).status();
   } else {
     HloPassPipeline sharding_removal_pipeline("sharding-removal");
