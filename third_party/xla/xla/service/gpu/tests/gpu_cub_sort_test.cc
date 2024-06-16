d750c0c219865780242945c0d05c010bbe8cccd5
pemeliya <141146080+pemeliya@users.noreply.github.com>
2024-06-16 07:27:56 -0700

    PR #13722: [ROCM] rocBLAS: default algorithm fallback
    Imported from GitHub PR https://github.com/openxla/xla/pull/13722
    
    Some number types (like complex64) are generally supported by rocBLAS but the library does not provide any solutions for autotuning. In that case, we shall fallback to use the default solution (kDefaultAlgorithm).
    
    Besides, I have also added workspace buffer provided by XLA runtime which previously was ignored by rocBLAS.
    
    @xla-rotation: would you please have a look ?
    Copybara import of the project:
    
    --
    57645a2bfb357b9d00f6b85ae3b0e77a4b00fb61 by Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>:
    
    use fallback default algoritm if no solutions are provided by the library
    
    --
    be8327f8e2e11f0eaf2383171340f69a23759260 by Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>:
    
    adding space
    
    Merging this change closes #13722
    
    Reverts changelist 578813627
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/13722 from ROCm:ci_rocm_blas_get_solutions_fix be8327f8e2e11f0eaf2383171340f69a23759260
    PiperOrigin-RevId: 643772477

--- third_party/xla/xla/service/gpu/tests/gpu_cub_sort_test.cc
+++ third_party/xla/xla/service/gpu/tests/gpu_cub_sort_test.cc
@@ -46,7 +46,13 @@ bool HloWasRewrittenToUseCubSort(const HloModule& module) {
 
 class CubSortKeysTest : public HloTestBase,
                         public ::testing::WithParamInterface<
-                            std::tuple<PrimitiveType, bool, int>> {};
+                            std::tuple<PrimitiveType, bool, int>> {
+  DebugOptions GetDebugOptionsForTest() override {
+    auto options = HloTestBase::GetDebugOptionsForTest();
+    options.set_xla_gpu_enable_cub_radix_sort(true);
+    return options;
+  }
+};
 
 TEST_P(CubSortKeysTest, CompareToReference) {
   int batch_size = std::get<2>(GetParam());
@@ -96,7 +102,13 @@ INSTANTIATE_TEST_SUITE_P(
 class CubSortPairsTest
     : public HloTestBase,
       public ::testing::WithParamInterface<
-          std::tuple<PrimitiveType, PrimitiveType, bool, int>> {};
+          std::tuple<PrimitiveType, PrimitiveType, bool, int>> {
+  DebugOptions GetDebugOptionsForTest() override {
+    auto options = HloTestBase::GetDebugOptionsForTest();
+    options.set_xla_gpu_enable_cub_radix_sort(true);
+    return options;
+  }
+};
 
 TEST_P(CubSortPairsTest, CompareToReference) {
   int batch_size = std::get<3>(GetParam());
