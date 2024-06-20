6da5c0d9b2e891519aad55a37186c5376796225c
A. Unique TensorFlower <gardener@tensorflow.org>
2024-06-20 13:59:47 -0700

    Reverts 49f4d9052259ae562ad0b0b84b5ba759494e6f83
    PiperOrigin-RevId: 645147471

--- third_party/xla/xla/pjrt/pjrt_stream_executor_client.cc
+++ third_party/xla/xla/pjrt/pjrt_stream_executor_client.cc
@@ -2284,6 +2284,8 @@ PjRtStreamExecutorLoadedExecutable::PjRtStreamExecutorLoadedExecutable(
       parameter_shapes.push_back(transfer_manager->HostShapeToDeviceShape(
           computation_layout.parameter_shape(i)));
     }
+    fingerprint_ = absl::StrCat(
+        fingerprint_, executable->executable()->module().GetFingerprint128());
     executables_.emplace_back(std::move(executable));
     on_device_executable_parameter_shapes_.push_back(
         std::move(parameter_shapes));
@@ -3237,22 +3239,6 @@ PjRtStreamExecutorLoadedExecutable::GetHloModules() const {
 absl::StatusOr<std::vector<std::vector<absl::string_view>>>
 PjRtStreamExecutorLoadedExecutable::GetOutputMemoryKinds() const {
   return Unimplemented("GetOutputMemoryKinds is not supported.");
-}
-
-absl::StatusOr<std::string>
-PjRtStreamExecutorLoadedExecutable::FingerprintExecutable() const {
-  if (executables_.size() != 1) {
-    return absl::InternalError(
-        "Fingerprinting multiple executables within one "
-        "PjRtStreamExecutorLoadedExecutable is not supported.");
-  }
-
-  Executable* executable = executables_[0]->executable();
-  if (executable->has_module()) {
-    return executable->module().GetFingerprint128();
-  } else {
-    return absl::InternalError("Executable does not have HLO modules.");
-  }
 }
 
 absl::StatusOr<PjRtStreamExecutorClient::ExecutableExtras>
