304d7bd6c16dfdf96806d19c714350e76da010ec
Henning Becker <hebecker@google.com>
2024-06-20 00:36:12 -0700

    Make gpu_timer_kernel target compatible with older versions of NVCC
    NVCC can't handle Abseil logging macro in CUDA C++ files - probably due to
    some template magic. So I'm putting the host code that uses Abseil logging
    into a separate file so that NVCC won't need to parse that.
    
    PiperOrigin-RevId: 644922832

--- third_party/xla/xla/stream_executor/gpu/gpu_timer_kernel_cuda.cu.cc
+++ third_party/xla/xla/stream_executor/gpu/gpu_timer_kernel_cuda.cu.cc
@@ -13,13 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <cstddef>
+#include "xla/stream_executor/gpu/gpu_timer_kernel_cuda.h"
 
-#include "xla/stream_executor/gpu/gpu_driver.h"
-#include "xla/stream_executor/gpu/gpu_executor.h"
 #include "xla/stream_executor/gpu/gpu_semaphore.h"
-#include "xla/stream_executor/gpu/gpu_timer_kernel.h"
-#include "xla/stream_executor/typed_kernel_factory.h"
 
 namespace stream_executor::gpu {
 namespace {
@@ -49,45 +45,6 @@ __global__ void DelayKernel(volatile GpuSemaphoreState* semaphore,
   }
 }
 }  // namespace
-
-absl::StatusOr<GpuSemaphore> LaunchDelayKernel(Stream* stream) {
-  StreamExecutor* executor = stream->parent();
-
-  // Allocate a semaphore value that will be used to signal to the delay
-  // kernel that it may exit.
-  TF_ASSIGN_OR_RETURN(auto semaphore, GpuSemaphore::Create(executor));
-  *semaphore = GpuSemaphoreState::kHold;
-  // In principle the kernel could be loaded lazily and shared across
-  // multiple GpuTimer objects.
-  TF_ASSIGN_OR_RETURN(
-      auto kernel,
-      (TypedKernelFactory<DeviceMemory<GpuSemaphoreState>,
-                          GpuSemaphoreState>::Create(executor, "DelayKernel",
-                                                     reinterpret_cast<void*>(
-                                                         DelayKernel))));
-  // Launch a delay kernel into this stream, which will spin until
-  // GetElapsedDuration() is called, the timer is destroyed, or the timeout
-  // in the kernel is reached.
-  TF_RETURN_IF_ERROR(stream->ThenLaunch(ThreadDim(1, 1, 1), BlockDim(1, 1, 1),
-                                        kernel, semaphore.device(),
-                                        GpuSemaphoreState::kRelease));
-
-  return semaphore;
-}
-
-absl::StatusOr<bool> DelayKernelIsSupported(GpuStream* stream) {
-  // Check the assumption that this device supports unified addressing,
-  // otherwise skip the delay kernel
-  TF_ASSIGN_OR_RETURN(int status, GpuDriver::GetDeviceAttribute(
-                                      CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING,
-                                      stream->parent()->device()));
-  if (!status) {
-    LOG(WARNING) << "Skipping the delay kernel because the device does not "
-                    "support unified addressing";
-  }
-
-  return static_cast<bool>(status);
-}
 
 namespace delay_kernel {
 void* kernel() { return reinterpret_cast<void*>(DelayKernel); }
