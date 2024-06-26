d42bb4dabd3fa209152da42580e4d1696cbc7bd0
A. Unique TensorFlower <gardener@tensorflow.org>
2024-06-25 17:42:03 -0700

    Automated Code Change
    PiperOrigin-RevId: 646667395

--- tensorflow/lite/delegates/gpu/common/selectors/simple_selectors.cc
+++ tensorflow/lite/delegates/gpu/common/selectors/simple_selectors.cc
@@ -19,8 +19,12 @@ limitations under the License.
 #include <set>
 #include <utility>
 
-#include "absl/memory/memory.h"
+#include "tensorflow/lite/delegates/gpu/common/data_type.h"
+#include "tensorflow/lite/delegates/gpu/common/gpu_info.h"
+#include "tensorflow/lite/delegates/gpu/common/operations.h"
+#include "tensorflow/lite/delegates/gpu/common/shape.h"
 #include "tensorflow/lite/delegates/gpu/common/status.h"
+#include "tensorflow/lite/delegates/gpu/common/task/gpu_operation.h"
 #include "tensorflow/lite/delegates/gpu/common/tasks/add.h"
 #include "tensorflow/lite/delegates/gpu/common/tasks/cast.h"
 #include "tensorflow/lite/delegates/gpu/common/tasks/concat_xy.h"
@@ -50,6 +54,7 @@ limitations under the License.
 #include "tensorflow/lite/delegates/gpu/common/tasks/tile.h"
 #include "tensorflow/lite/delegates/gpu/common/tasks/transpose.h"
 #include "tensorflow/lite/delegates/gpu/common/tasks/winograd.h"
+#include "tensorflow/lite/delegates/gpu/common/tensor.h"
 
 namespace tflite {
 namespace gpu {
