4d538ab029efd995d3674d6efaafcbb65e5d211f
Johannes Reifferscheid <jreiffers@google.com>
2024-06-26 02:17:49 -0700

    Fix excessive type conversions for pred reductions.
    The problem is shown in simplify_arith.mlir. The fix is to
    push truncs/exts over bitwise/shuffle ops, allowing them
    to fold.
    
    This speeds up the predicate row reduction microbenchmarks
    (pred[1, 65536, 64] -> pred[65536]) by ~10% on A100. MLIR
    emitter performance is now slightly faster than vanilla at
    15.52us vs 15.94us.
    
    PiperOrigin-RevId: 646786315

--- third_party/xla/xla/service/gpu/fusions/mlir/simplify_arith.cc
+++ third_party/xla/xla/service/gpu/fusions/mlir/simplify_arith.cc
@@ -23,8 +23,10 @@ limitations under the License.
 
 #include "mlir/Dialect/Arith/IR/Arith.h"  // from @llvm-project
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
+#include "mlir/Dialect/GPU/IR/GPUDialect.h"  // from @llvm-project
 #include "mlir/Dialect/Utils/StaticValueUtils.h"  // from @llvm-project
 #include "mlir/IR/PatternMatch.h"  // from @llvm-project
+#include "mlir/IR/Value.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
 #include "mlir/Support/LogicalResult.h"  // from @llvm-project
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"  // from @llvm-project
@@ -125,6 +127,101 @@ struct RewriteMinSi : mlir::OpRewritePattern<mlir::arith::MinSIOp> {
   }
 };
 
+// Finds the defining `trunc` op if the value is the result of a trunc or a
+// {trunc, ext} chain.
+mlir::arith::TruncIOp FindDefiningTrunc(mlir::Value value) {
+  if (auto ext = value.getDefiningOp<mlir::arith::ExtUIOp>()) {
+    return FindDefiningTrunc(ext.getOperand());
+  }
+
+  auto defining_op = value.getDefiningOp<mlir::arith::TruncIOp>();
+  if (defining_op) {
+    auto first_trunc = FindDefiningTrunc(defining_op.getOperand());
+    if (first_trunc && first_trunc.getType().getIntOrFloatBitWidth() <=
+                           defining_op.getType().getIntOrFloatBitWidth()) {
+      return first_trunc;
+    }
+  }
+  return defining_op;
+}
+
+// Rewrites trunc-bitwise to bitwise-trunc.
+//
+// For pred reductions, we generate code like this:
+//
+//   %1 = arith.trunci %0 : i32 to i1
+//   %2 = arith.ori %1, %x
+//   %3 = arith.extui %2 : i1 to i32
+//   %4 = gpu.shuffle %3
+//
+// By swapping the trunc with the or, we get a trunc-ext-shuffle sequence, which
+// can be rewritten to shuffle-trunc-ext. If there is another copy of the
+// pattern afterwards, we can push the truncs/exts further down.
+template <typename Op>
+struct RewriteTruncBitExt : mlir::OpRewritePattern<Op> {
+  using mlir::OpRewritePattern<Op>::OpRewritePattern;
+
+  mlir::LogicalResult matchAndRewrite(
+      Op op, mlir::PatternRewriter& rewriter) const override {
+    mlir::Value lhs = op.getLhs();
+    mlir::Value rhs = op.getRhs();
+
+    mlir::arith::TruncIOp trunci_lhs = FindDefiningTrunc(lhs);
+    mlir::arith::TruncIOp trunci_rhs = FindDefiningTrunc(rhs);
+    if (!trunci_lhs && !trunci_rhs) {
+      return rewriter.notifyMatchFailure(op, "no truncation");
+    }
+
+    auto narrow_type = trunci_lhs ? trunci_lhs.getType() : trunci_rhs.getType();
+    auto wide_type =
+        (trunci_lhs ? trunci_lhs : trunci_rhs).getOperand().getType();
+    if (trunci_rhs && (trunci_rhs.getOperand().getType() != wide_type ||
+                       trunci_rhs.getType() != narrow_type)) {
+      return rewriter.notifyMatchFailure(op, "mismatched truncation types");
+    }
+
+    mlir::Value new_lhs = trunci_lhs ? trunci_lhs.getOperand()
+                                     : rewriter.create<mlir::arith::ExtUIOp>(
+                                           op.getLoc(), wide_type, lhs);
+    mlir::Value new_rhs = trunci_rhs ? trunci_rhs.getOperand()
+                                     : rewriter.create<mlir::arith::ExtUIOp>(
+                                           op.getLoc(), wide_type, rhs);
+    mlir::Value new_op = rewriter.create<Op>(op.getLoc(), new_lhs, new_rhs);
+    rewriter.replaceOpWithNewOp<mlir::arith::TruncIOp>(op, op.getType(),
+                                                       new_op);
+
+    return mlir::success();
+  }
+};
+
+// Rewrites trunc-ext-shuffle to shuffle-trunc-ext. This pattern is designed to
+// work together with RewriteTruncBitExt to optimize pred reductions.
+struct RewriteTruncExtShuffle
+    : public mlir::OpRewritePattern<mlir::gpu::ShuffleOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult matchAndRewrite(
+      mlir::gpu::ShuffleOp op, mlir::PatternRewriter& rewriter) const override {
+    auto ext = op.getOperand(0).getDefiningOp<mlir::arith::ExtUIOp>();
+    if (!ext) {
+      return rewriter.notifyMatchFailure(op, "no ext");
+    }
+    auto trunc = ext.getOperand().getDefiningOp<mlir::arith::TruncIOp>();
+    if (!trunc || trunc.getOperand().getType() != ext.getType()) {
+      return rewriter.notifyMatchFailure(op, "no trunc or type mismatch");
+    }
+    rewriter.setInsertionPointAfter(op);
+    auto new_trunc = rewriter.create<mlir::arith::TruncIOp>(
+        op.getLoc(), trunc.getType(), op.getResult(0));
+    auto new_ext = rewriter.create<mlir::arith::ExtUIOp>(
+        op.getLoc(), ext.getType(), new_trunc.getResult());
+    rewriter.modifyOpInPlace(op,
+                             [&]() { op->setOperand(0, trunc.getOperand()); });
+    rewriter.replaceAllUsesExcept(op.getResult(0), new_ext, new_trunc);
+    return mlir::success();
+  }
+};
+
 void AnnotateRanges(mlir::func::FuncOp func) {
   func->walk([](mlir::Operation* op) {
     if (op->getNumResults() != 1) {
@@ -176,6 +273,10 @@ class SimplifyArithPass
     mlir::RewritePatternSet patterns(&getContext());
     AnnotateRanges(getOperation());
     patterns.add<RewriteCmpI, RewriteMaxSi, RewriteMinSi>(&getContext());
+    patterns
+        .add<RewriteTruncBitExt<mlir::arith::OrIOp>,
+             RewriteTruncBitExt<mlir::arith::AndIOp>, RewriteTruncExtShuffle>(
+            &getContext());
     if (mlir::failed(mlir::applyPatternsAndFoldGreedily(getOperation(),
                                                         std::move(patterns)))) {
       signalPassFailure();
