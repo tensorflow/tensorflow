9fd81779af40e029abd134ea290e5dfa11823a91
Vamsi Manchala <vamsimanchala@google.com>
2024-06-27 17:11:56 -0700

    Remove deprecated debug utility code from tf_tfl_translate and tf_tfl_flatbuffer_helpers.
    PiperOrigin-RevId: 647493323

--- tensorflow/compiler/mlir/lite/python/tf_tfl_flatbuffer_helpers.cc
+++ tensorflow/compiler/mlir/lite/python/tf_tfl_flatbuffer_helpers.cc
@@ -24,16 +24,11 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/str_cat.h"
-#include "llvm/Support/ToolOutputFile.h"
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/IR/OwningOpRef.h"  // from @llvm-project
 #include "mlir/IR/Visitors.h"  // from @llvm-project
-#include "mlir/Pass/PassManager.h"  // from @llvm-project
 #include "mlir/Support/FileUtilities.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
-#include "mlir/Support/LogicalResult.h"  // from @llvm-project
-#include "mlir/Transforms/ViewOpGraph.h"  // from @llvm-project
 #include "tensorflow/cc/saved_model/loader.h"
 #include "tensorflow/compiler/mlir/lite/common/tfl_pass_config.h"
 #include "tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h"
@@ -52,7 +47,6 @@ limitations under the License.
 #include "tensorflow/lite/toco/toco_flags.pb.h"
 #include "tensorflow/lite/toco/types.pb.h"
 #include "tensorflow/lite/tools/optimize/reduced_precision_support.h"
-#include "tsl/platform/errors.h"
 #include "tsl/platform/protobuf.h"  // IWYU pragma: keep
 #include "tsl/platform/statusor.h"
 
@@ -328,23 +322,6 @@ absl::Status PopulateQuantizationSpecs(
   return absl::OkStatus();
 }
 
-// Dumps the op graph of the `module` to `filename` in DOT format.
-absl::Status DumpOpGraphToFile(mlir::ModuleOp module,
-                               const std::string& filename) {
-  std::string error_message;
-  auto output = mlir::openOutputFile(filename, &error_message);
-  if (!error_message.empty()) {
-    return errors::InvalidArgument("Failed to open file in ", filename);
-  }
-  mlir::PassManager pm(module.getContext());
-  pm.addPass(mlir::createPrintOpGraphPass(output->os()));
-  if (failed(pm.run(module))) {
-    return errors::Unknown("Failed to dump Op Graph from MLIR module.");
-  }
-  output->keep();
-  return absl::OkStatus();
-}
-
 absl::Status ConvertMLIRToTFLiteFlatBuffer(
     const toco::ModelFlags& model_flags, toco::TocoFlags& toco_flags,
     mlir::OwningOpRef<mlir::ModuleOp> module,
@@ -352,12 +329,6 @@ absl::Status ConvertMLIRToTFLiteFlatBuffer(
     const std::unordered_set<std::string>& saved_model_tags,
     std::string* result, std::unique_ptr<SavedModelBundle> saved_model_bundle,
     const PyFunctionLibrary* quantization_py_function_lib) {
-  if (toco_flags.has_dump_graphviz_dir()) {
-    TF_RETURN_IF_ERROR(DumpOpGraphToFile(
-        module.get(),
-        // rename once we enable the new converter feature flag.
-        absl::StrCat(toco_flags.dump_graphviz_dir(), "/toco_AT_IMPORT.dot")));
-  }
 
   mlir::TFL::PassConfig pass_config_copy = pass_config;
   pass_config_copy.outline_tf_while = true;
@@ -378,12 +349,6 @@ absl::Status ConvertMLIRToTFLiteFlatBuffer(
       saved_model_tags, model_flags.saved_model_dir(),
       std::move(saved_model_bundle), result, /*serialize_stablehlo_ops=*/false,
       quantization_py_function_lib);
-  if (toco_flags.has_dump_graphviz_dir()) {
-    TF_RETURN_IF_ERROR(DumpOpGraphToFile(
-        // rename once we enable the new converter feature flag.
-        module.get(), absl::StrCat(toco_flags.dump_graphviz_dir(),
-                                   "/toco_AFTER_TRANSFORMATIONS.dot")));
-  }
 
   return status;
 }
--- tensorflow/compiler/mlir/lite/tf_tfl_translate.cc
+++ tensorflow/compiler/mlir/lite/tf_tfl_translate.cc
@@ -13,10 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <functional>
-#include <iostream>
 #include <memory>
-#include <optional>
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -29,7 +26,6 @@ limitations under the License.
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/FormatVariadic.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/raw_ostream.h"
@@ -39,7 +35,6 @@ limitations under the License.
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/IR/Diagnostics.h"  // from @llvm-project
 #include "mlir/IR/DialectRegistry.h"  // from @llvm-project
-#include "mlir/IR/Location.h"  // from @llvm-project
 #include "mlir/IR/MLIRContext.h"  // from @llvm-project
 #include "mlir/Parser/Parser.h"  // from @llvm-project
 #include "mlir/Pass/PassManager.h"  // from @llvm-project
@@ -49,7 +44,6 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/init_mlir.h"
 #include "tensorflow/compiler/mlir/lite/common/tfl_pass_config.h"
 #include "tensorflow/compiler/mlir/lite/flatbuffer_export_flags.h"
-#include "tensorflow/compiler/mlir/lite/schema/schema_generated.h"
 #include "tensorflow/compiler/mlir/lite/tf_tfl_translate_cl.h"
 #include "tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h"
 #include "tensorflow/compiler/mlir/lite/transforms/passes.h"
@@ -60,20 +54,9 @@ limitations under the License.
 #include "xla/translate/hlo_to_mhlo/translate.h"
 #include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/platform/errors.h"
-#include "tensorflow/core/public/session.h"
-#include "tensorflow/lite/model_builder.h"
 
 using mlir::MLIRContext;
 using mlir::ModuleOp;
-using mlir::func::FuncOp;
-
-// Debugging flag to print function mapping in the flatbuffer.
-// NOLINTNEXTLINE
-static llvm::cl::opt<bool> print_function_result_mapping(
-    "print-function-result-mapping",
-    llvm::cl::desc(
-        "Print the mapping of function result to flatbuffer output buffer"),
-    llvm::cl::init(false));
 
 // NOLINTNEXTLINE
 static llvm::cl::opt<std::string> weight_quantization(
@@ -83,56 +66,6 @@ static llvm::cl::opt<std::string> weight_quantization(
     llvm::cl::init("NONE"));
 
 enum TranslationStatus { kTrSuccess, kTrFailure };
-
-static int PrintFunctionResultMapping(const std::string &result,
-                                      ModuleOp module) {
-  // Build model from the resultant string to extract the return values from
-  // their source of truth.
-  auto model =
-      tflite::FlatBufferModel::BuildFromBuffer(result.data(), result.size());
-  if (!model) return kTrFailure;
-
-  // Get an unknown location for where we don't have a terminator to get the
-  // location of the return value from.
-  auto unknown_loc = mlir::UnknownLoc::get(module.getContext());
-
-  auto print_buffer = [&](const tflite::SubGraph &subgraph, int id, int buffer,
-                          std::function<mlir::Location(int)> loc) {
-    const auto &output_tensor = (*subgraph.tensors())[buffer];
-    std::cout << "\tname: '"
-              << (output_tensor->name() ? output_tensor->name()->str()
-                                        : "<<unnamed>>")
-              << "' buffer: " << buffer;
-    if (loc) std::cout << llvm::formatv(" {0}", loc(id)).str();
-    std::cout << '\n';
-  };
-
-  // For every subgraph print out the name (if available), each result's output
-  // buffer number and location of the return value (if available).
-  for (auto *subgraph : *(*model)->subgraphs()) {
-    std::string subgraph_name =
-        subgraph->name() ? subgraph->name()->str() : "<<unnamed subgraph>>";
-
-    std::cout << '\'' << subgraph_name << "' inputs:\n";
-    int i = 0;
-    for (auto input : *subgraph->inputs())
-      print_buffer(*subgraph, i++, input, nullptr);
-
-    std::cout << '\'' << subgraph_name << "' outputs:\n";
-    mlir::Operation *terminator = nullptr;
-    if (subgraph->name()) {
-      if (auto fn = module.lookupSymbol<FuncOp>(subgraph->name()->str()))
-        terminator = fn.back().getTerminator();
-    }
-    i = 0;
-    for (auto output : *subgraph->outputs()) {
-      print_buffer(*subgraph, i, output, [&](int i) {
-        return terminator ? terminator->getOperand(i).getLoc() : unknown_loc;
-      });
-    }
-  }
-  return kTrSuccess;
-}
 
 int main(int argc, char **argv) {
   // TODO(jpienaar): Revise the command line option parsing here.
@@ -335,8 +268,5 @@ int main(int argc, char **argv) {
   output->os() << result;
   output->keep();
 
-  // Print out debugging info related to function mapping.
-  if (print_function_result_mapping)
-    return PrintFunctionResultMapping(result, module.value().get());
   return kTrSuccess;
 }
