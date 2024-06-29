1cbb8d8d76e4af8beff67f14de8ae9561339a0b1
lingzhi98 <103185827+lingzhi98@users.noreply.github.com>
2024-06-28 21:04:23 -0700

    PR #14177: Fix missing entries of device description proto
    Imported from GitHub PR https://github.com/openxla/xla/pull/14177
    
    Add registers_per_core_limit and registers_per_block_limit which were missed before into device description proto.
    Copybara import of the project:
    
    --
    17a52f950ec9958a9bdce58b6e1570822bde68f7 by Zhou, Lingzhi <lingzhi.zhou@intel.com>:
    
    fix missing entries of device description proto
    
    --
    a881d59e3431b911176c326d38e7077090fc0a0f by Zhou, Lingzhi <lingzhi.zhou@intel.com>:
    
    use new tag number
    
    Merging this change closes #14177
    
    PiperOrigin-RevId: 647875048

--- third_party/xla/xla/stream_executor/device_description.cc
+++ third_party/xla/xla/stream_executor/device_description.cc
@@ -78,6 +78,8 @@ DeviceDescription::DeviceDescription(const GpuDeviceInfoProto &proto) {
   l2_cache_size_ = proto.l2_cache_size();
   clock_rate_ghz_ = proto.clock_rate_ghz();
   device_memory_size_ = proto.device_memory_size();
+  registers_per_core_limit_ = proto.registers_per_core_limit();
+  registers_per_block_limit_ = proto.registers_per_block_limit();
 }
 
 GpuDeviceInfoProto DeviceDescription::ToGpuProto() const {
@@ -104,6 +106,8 @@ GpuDeviceInfoProto DeviceDescription::ToGpuProto() const {
   proto.set_l2_cache_size(l2_cache_size_);
   proto.set_clock_rate_ghz(clock_rate_ghz_);
   proto.set_device_memory_size(device_memory_size_);
+  proto.set_registers_per_core_limit(registers_per_core_limit_);
+  proto.set_registers_per_block_limit(registers_per_block_limit_);
   return proto;
 }
 
