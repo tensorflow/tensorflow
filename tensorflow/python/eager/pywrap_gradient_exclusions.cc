647fad16e09c95e4316aa09c867894180d2cdca3
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-03 10:19:01 -0700

    [TF] Register `ResizeBilinearGrad` op as not differentiable
    PiperOrigin-RevId: 649118126

--- tensorflow/python/eager/pywrap_gradient_exclusions.cc
+++ tensorflow/python/eager/pywrap_gradient_exclusions.cc
@@ -50,7 +50,7 @@ auto OpGradientInfoInit(const T &a) {
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 366> a = {{
+  static std::array<OpIndexInfo, 367> a = {{
       {"Acosh"},
       {"AllToAll", 1, {0}},
       {"ApproximateEqual"},
@@ -267,6 +267,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
       {"Reshape", 1, {1}},
       {"ResizeBicubic", 1, {1}},
       {"ResizeBilinear", 1, {1}},
+      {"ResizeBilinearGrad"},
       {"ResizeNearestNeighbor", 1, {1}},
       {"Reverse", 1, {0}},
       {"ReverseSequence", 1, {0}},
@@ -429,7 +430,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
     const tensorflow::string &op_name) {
-  static std::array<OpIndexInfo, 485> a = {{
+  static std::array<OpIndexInfo, 486> a = {{
       {"Abs"},
       {"AccumulateNV2"},
       {"Acos"},
@@ -728,6 +729,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
       {"Reshape"},
       {"ResizeBicubic"},
       {"ResizeBilinear"},
+      {"ResizeBilinearGrad"},
       {"ResizeNearestNeighbor"},
       {"ResourceGather"},
       {"ResourceGatherNd"},
