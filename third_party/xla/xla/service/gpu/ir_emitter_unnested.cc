66edefcb91845bc7c82f4240aba9ab7c7db11d06
terryysun <tesun@nvidia.com>
2024-07-08 07:14:46 -0700

    PR #14344: [NVIDIA GPU] Annotate syntactic sugar op name in nsys profile
    Imported from GitHub PR https://github.com/openxla/xla/pull/14344
    
    **Problem:** Currently in HLO dumping the syntactic sugar for async ops are turned on by default, while in nsys profile the op names are the actual op names, which is causing inefficiency when trying to correspond them.
    
    **Solution:** Annotate syntactic sugar op name in nsys profile.
    Copybara import of the project:
    
    --
    18c4c6a04471b61b95ae4939066fc45ca5cba952 by Terry Sun <tesun@nvidia.com>:
    
    override wrapped op name with wrapper
    
    --
    3be330507f14e9ef4c264e5b3a7cc58dd2954d01 by Terry Sun <tesun@nvidia.com>:
    
    better doc string
    
    Merging this change closes #14344
    
    PiperOrigin-RevId: 650239638

--- third_party/xla/xla/service/gpu/ir_emitter_unnested.cc
+++ third_party/xla/xla/service/gpu/ir_emitter_unnested.cc
@@ -2322,9 +2322,11 @@ absl::Status IrEmitterUnnested::EmitNcclThunk(
   }
 
   if (should_use_nccl_thunk) {
+    auto thunk_info = Thunk::ThunkInfo::WithProfileAnnotation(inst);
+    // The wrapper name is used when an op is wrapped by syntactic sugar.
+    thunk_info.profile_annotation = async_start->name();
     auto thunk = std::make_unique<NcclThunkType>(
-        Thunk::ThunkInfo::WithProfileAnnotation(inst), NcclApi::Default(), inst,
-        /*buffers=*/std::move(buffers));
+        thunk_info, NcclApi::Default(), inst, /*buffers=*/std::move(buffers));
     GetCollectivesAsyncEvents().insert({async_start, thunk->async_events()});
     AddThunkToThunkSequence(std::move(thunk));
     return absl::OkStatus();
