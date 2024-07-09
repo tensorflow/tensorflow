7f67760d2ef69718700817a3f2d110144f241778
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-09 04:22:02 -0700

    Automated Code Change
    PiperOrigin-RevId: 650576399

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -31,6 +31,7 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
+#include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/str_join.h"
