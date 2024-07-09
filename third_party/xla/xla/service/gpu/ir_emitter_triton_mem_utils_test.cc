b6509a72ece3a0a9c3814e66251ae464b8487555
Oleg Shyshkov <shyshkov@google.com>
2024-07-09 12:18:27 -0700

    [XLA:GPU] Use proper tiled hlo instruction in ir_emitter_triton_mem_utils_test.
    PiperOrigin-RevId: 650717188

--- third_party/xla/xla/service/gpu/ir_emitter_triton_mem_utils_test.cc
+++ third_party/xla/xla/service/gpu/ir_emitter_triton_mem_utils_test.cc
@@ -16,11 +16,16 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 #include <optional>
+#include <string>
 #include <utility>
+#include <variant>
 #include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/log/check.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/substitute.h"
 #include "absl/types/span.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"  // from @llvm-project
 #include "mlir/Dialect/LLVMIR/NVVMDialect.h"  // from @llvm-project
@@ -37,12 +42,14 @@ limitations under the License.
 #include "mlir/IR/ValueRange.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
 #include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/service/gpu/hlo_traversal.h"
 #include "xla/service/gpu/ir_emitter_triton.h"
-#include "xla/service/gpu/model/indexing_map.h"
+#include "xla/service/gpu/model/symbolic_tile_analysis.h"
+#include "xla/service/gpu/model/tiled_hlo_computation.h"
 #include "xla/service/gpu/model/tiled_hlo_instruction.h"
 #include "xla/service/llvm_ir/llvm_util.h"
-#include "xla/shape_util.h"
 #include "xla/tests/hlo_test_base.h"
+#include "xla/tests/verified_hlo_module.h"
 #include "tsl/lib/core/status_test_util.h"
 #include "tsl/platform/logging.h"  // IWYU pragma: keep
 #include "triton/Dialect/Triton/IR/Dialect.h"
@@ -62,38 +69,60 @@ class TritonMakeTensorPtrTest : public HloTestBase {
  public:
   void SetUp() override { LoadMlirDialectsForTriton(mlir_context_); }
 
+  std::pair<std::unique_ptr<VerifiedHloModule>, TiledHloComputation>
+  CreateAndTileParameterHloInstruction(
+      std::vector<int64_t> shape_sizes, const std::vector<int64_t>& tile_sizes,
+      const std::vector<int64_t>& tile_strides);
+
+  std::pair<mlir::OwningOpRef<mlir::ModuleOp>, MakeTensorPtrOpAndBoundaryChecks>
+  CreateTestTensorPtr(const std::vector<int64_t>& tile_sizes,
+                      const std::vector<int64_t>& tile_strides);
+
  protected:
   MLIRContext mlir_context_;
 };
 
-// This is not a proper affine map, just something that enables the
-// creation of the index.
-//
-// TODO(b/332649307): Test with a proper affine map once the code starts
-// passing proper offsets to MakeTensorPtr.
-IndexingMap CreateAffineMap(const std::vector<int64_t>& tile_sizes,
-                            MLIRContext& ctx) {
-  mlir::AffineExpr d0 = mlir::getAffineDimExpr(0, &ctx);
-  std::vector<mlir::AffineExpr> dims(tile_sizes.size(), d0);
-  return IndexingMap::FromTensorSizes(mlir::AffineMap::get(1, 0, dims, &ctx),
-                                      /*dim_upper_bounds=*/{8},
-                                      /*symbol_upper_bounds=*/{});
-}
-
 // Returns a Parameter HLO instruction with a parameter number 0.
-std::pair<std::unique_ptr<HloInstruction>, std::unique_ptr<TiledHloInstruction>>
-CreateAndTileParameterHloInstruction(std::vector<int64_t> shape_sizes,
-                                     const std::vector<int64_t>& tile_sizes,
-                                     const std::vector<int64_t>& tile_strides,
-                                     MLIRContext& ctx) {
-  std::unique_ptr<HloInstruction> hlo = HloInstruction::CreateParameter(
-      /*parameter_number=*/0,
-      ShapeUtil::MakeShape(PrimitiveType::F32, shape_sizes), "p0");
+std::pair<std::unique_ptr<VerifiedHloModule>, TiledHloComputation>
+TritonMakeTensorPtrTest::CreateAndTileParameterHloInstruction(
+    std::vector<int64_t> shape_sizes, const std::vector<int64_t>& tile_sizes,
+    const std::vector<int64_t>& tile_strides) {
+  const std::string hlo_text = R"(
+  HloModule test_module
 
-  auto tiled_hlo = TiledHloInstruction::Create(
-      hlo.get(), tile_sizes, tile_strides, CreateAffineMap(tile_sizes, ctx));
-  TF_EXPECT_OK(tiled_hlo);
-  return std::make_pair(std::move(hlo), std::move(tiled_hlo.value()));
+  fusion {
+    p0 = f32[$0] parameter(0)
+    ROOT log = f32[$0] log(p0)
+  }
+
+  ENTRY %main{
+    p0.1 = f32[$0] parameter(0)
+    ROOT fusion = f32[$0] fusion(p0.1), kind=kLoop, calls=%fusion
+  })";
+  auto verified_hlo_module_or = ParseAndReturnVerifiedModule(
+      absl::Substitute(hlo_text, absl::StrJoin(shape_sizes, ",")));
+  CHECK_OK(verified_hlo_module_or);
+
+  std::unique_ptr<VerifiedHloModule> verified_hlo_module =
+      std::move(verified_hlo_module_or).value();
+
+  auto fusion_adaptor = HloFusionAdaptor::ForInstruction(
+      verified_hlo_module->entry_computation()->root_instruction());
+
+  SymbolicTileAnalysisOrError symbolic_tile_analysis_or =
+      SymbolicTileAnalysis::AnalyzeFusion(*fusion_adaptor, &mlir_context_);
+  CHECK(
+      std::holds_alternative<SymbolicTileAnalysis>(symbolic_tile_analysis_or));
+
+  SymbolicTileAnalysis symbolic_tile_analysis =
+      std::get<SymbolicTileAnalysis>(std::move(symbolic_tile_analysis_or));
+
+  auto tiled_hlo_computation_or =
+      symbolic_tile_analysis.ComputeTiledHloInstructions(
+          tile_sizes, /*constraints_are_known_satisfied=*/true);
+  TF_EXPECT_OK(tiled_hlo_computation_or.status());
+  return std::make_pair(std::move(verified_hlo_module),
+                        *std::move(tiled_hlo_computation_or));
 }
 
 mlir::triton::FuncOp CreateTritonFunction(
@@ -111,19 +140,26 @@ mlir::triton::FuncOp CreateTritonFunction(
 }
 
 std::pair<mlir::OwningOpRef<mlir::ModuleOp>, MakeTensorPtrOpAndBoundaryChecks>
-CreateTestTensorPtr(const std::vector<int64_t>& tile_sizes,
-                    const std::vector<int64_t>& tile_strides,
-                    MLIRContext& ctx) {
+TritonMakeTensorPtrTest::CreateTestTensorPtr(
+    const std::vector<int64_t>& tile_sizes,
+    const std::vector<int64_t>& tile_strides) {
   std::vector<int64_t> shape_sizes;
   for (int64_t tile_size : tile_sizes) {
-    constexpr int64_t kShapeToTileRatio = 5;
-    shape_sizes.push_back(tile_size * kShapeToTileRatio);
+    // The test is parametrised by the tile sizes. We set the hlo shape in the
+    // way that there are 5 tiles for each dimension.
+    constexpr int64_t kTilesPerDim = 5;
+    shape_sizes.push_back(tile_size * kTilesPerDim);
   }
 
-  auto [hlo, tiled_hlo] = CreateAndTileParameterHloInstruction(
-      shape_sizes, tile_sizes, tile_strides, ctx);
+  auto [hlo_module, tiled_hlo_computation] =
+      CreateAndTileParameterHloInstruction(shape_sizes, tile_sizes,
+                                           tile_strides);
 
-  OpBuilder builder(&ctx);
+  const TiledHloInstruction* tiled_hlo =
+      tiled_hlo_computation.GetRoot()->operand(0);
+  const HloInstruction* hlo = tiled_hlo->hlo();
+
+  OpBuilder builder(&mlir_context_);
   auto loc = mlir::NameLoc::get(builder.getStringAttr(hlo->name()));
   mlir::OwningOpRef<mlir::ModuleOp> triton_module =
       llvm_ir::CreateMlirModuleOp(loc);
@@ -157,7 +193,7 @@ mlir::ArrayRef<int64_t> TensorShape(const mt::MakeTensorPtrOp& op) {
 
 TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
   {
-    auto [module, ptr] = CreateTestTensorPtr({3, 4}, {1, 1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({3, 4}, {1, 1});
     EXPECT_THAT(ConstOpValuesToInt(ptr.op.getShape()), ElementsAre(3, 4));
     EXPECT_THAT(TensorShape(ptr.op), ElementsAre(4, 4));
     EXPECT_THAT(ptr.boundary_checks, ElementsAre(0));
@@ -166,7 +202,7 @@ TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
     EXPECT_THAT(ptr.op.getOrder(), ElementsAre(1, 0));
   }
   {
-    auto [module, ptr] = CreateTestTensorPtr({4, 4}, {1, 1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({4, 4}, {1, 1});
     EXPECT_THAT(ConstOpValuesToInt(ptr.op.getShape()), ElementsAre(4, 4));
     EXPECT_THAT(TensorShape(ptr.op), ElementsAre(4, 4));
     EXPECT_TRUE(ptr.boundary_checks.empty());
@@ -175,7 +211,7 @@ TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
     EXPECT_THAT(ptr.op.getOrder(), ElementsAre(1, 0));
   }
   {
-    auto [module, ptr] = CreateTestTensorPtr({1}, {1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({1}, {1});
     EXPECT_TRUE(ConstOpValuesToInt(ptr.op.getShape()).empty());
     EXPECT_TRUE(TensorShape(ptr.op).empty());
     EXPECT_TRUE(ptr.boundary_checks.empty());
@@ -184,8 +220,7 @@ TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
     EXPECT_TRUE(ptr.op.getOrder().empty());
   }
   {
-    auto [module, ptr] =
-        CreateTestTensorPtr({1, 1, 1}, {1, 1, 1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({1, 1, 1}, {1, 1, 1});
     EXPECT_TRUE(ConstOpValuesToInt(ptr.op.getShape()).empty());
     EXPECT_TRUE(TensorShape(ptr.op).empty());
     EXPECT_TRUE(ptr.boundary_checks.empty());
@@ -194,8 +229,7 @@ TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
     EXPECT_TRUE(ptr.op.getOrder().empty());
   }
   {
-    auto [module, ptr] =
-        CreateTestTensorPtr({1, 3, 4}, {1, 1, 1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({1, 3, 4}, {1, 1, 1});
     EXPECT_THAT(ConstOpValuesToInt(ptr.op.getShape()), ElementsAre(3, 4));
     EXPECT_THAT(TensorShape(ptr.op), ElementsAre(4, 4));
     EXPECT_THAT(ptr.boundary_checks, ElementsAre(0));
@@ -207,8 +241,7 @@ TEST_F(TritonMakeTensorPtrTest, BlockProperties) {
     // TODO(b/332649307): Clarify whether the 1 at index 3 should indeed be
     // skipped. Maybe this depends on the shape? E.g. if the shape is also 1,
     // then it's fine to skip, otherwise not.
-    auto [module, ptr] =
-        CreateTestTensorPtr({1, 3, 4, 1, 6}, {1, 1, 1, 1, 1}, mlir_context_);
+    auto [module, ptr] = CreateTestTensorPtr({1, 3, 4, 1, 6}, {1, 1, 1, 1, 1});
     EXPECT_THAT(ConstOpValuesToInt(ptr.op.getShape()), ElementsAre(3, 4, 6));
     EXPECT_THAT(TensorShape(ptr.op), ElementsAre(4, 4, 8));
     EXPECT_THAT(ptr.boundary_checks, ElementsAre(0, 2));
