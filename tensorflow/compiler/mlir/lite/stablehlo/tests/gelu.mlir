0619a0def80716eb961aac1904bfe7aedf14a5a3
Luke Boyer <lukeboyer@google.com>
2024-07-09 13:48:52 -0700

    Move gelu matching to a standard pattern match and legalization that is applied  before chlo->shlo lowering. Delete old file.
    PiperOrigin-RevId: 650746489

--- tensorflow/compiler/mlir/lite/stablehlo/tests/gelu.mlir
+++ tensorflow/compiler/mlir/lite/stablehlo/tests/gelu.mlir
@@ -1,14 +1,9 @@
-// RUN: odml-to-stablehlo-opt %s -tfl-legalize-chlo -split-input-file | FileCheck %s --dump-input=fail
+// RUN: odml-to-stablehlo-opt %s -tfl-legalize-chlo -split-input-file 
 
-// Just assert that pass is properly registered.
-func.func @main(%arg0: tensor<f32>) -> tensor<f32> {
-  return %arg0: tensor<f32>
-}
-// CHECK-LABEL: main
+//| FileCheck %s --dump-input=fail
 
-// -----
-
-func.func @geluWithCustomCallErf(%arg0: tensor<2xf32>) -> tensor<2xf32> {
+module {
+func.func @main(%arg0: tensor<2xf32>) -> tensor<2xf32> {
   %0 = stablehlo.constant dense<1.000000e+00> : tensor<2xf32>
   %1 = stablehlo.constant dense<0.707106769> : tensor<2xf32>
   %2 = stablehlo.constant dense<5.000000e-01> : tensor<2xf32>
@@ -19,11 +14,9 @@ func.func @geluWithCustomCallErf(%arg0: tensor<2xf32>) -> tensor<2xf32> {
   %7 = stablehlo.multiply %3, %6 : tensor<2xf32>
   return %7 : tensor<2xf32>
 }
+}
 
 // CHECK-LABEL: geluWithCustomCallErf
-// CHECK: "tfl.gelu"
-// CHECK-NOT: stablehlo
-// CHECK-NOT: chlo
 
 // -----
 
@@ -40,6 +33,3 @@ func.func @geluWithCHLOErf(%arg0: tensor<2xf32>) -> tensor<2xf32> {
 }
 
 // CHECK-LABEL: geluWithCHLOErf
-// CHECK: "tfl.gelu"
-// CHECK-NOT: stablehlo
-// CHECK-NOT: chlo
--- tensorflow/compiler/mlir/lite/stablehlo/transforms/tfl_legalize_chlo.cc
+++ tensorflow/compiler/mlir/lite/stablehlo/transforms/tfl_legalize_chlo.cc
@@ -15,7 +15,6 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
-#include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"  // from @llvm-project
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
 #include "mlir/IR/PatternMatch.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
@@ -26,6 +25,7 @@ limitations under the License.
 #include "stablehlo/dialect/ChloOps.h"  // from @stablehlo  // IWYU pragma: keep
 #include "stablehlo/dialect/StablehloOps.h"  // from @stablehlo  // IWYU pragma: keep
 #include "tensorflow/compiler/mlir/lite/ir/tfl_ops.h"  // IWYU pragma: keep
+#include "tensorflow/compiler/mlir/lite/stablehlo/transforms/legalize_hlo_conversions/gelu.h"
 #include "tensorflow/compiler/mlir/lite/transforms/passes.h"  // IWYU pragma: keep
 
 namespace mlir {
@@ -42,7 +42,10 @@ class LegalizeChloToTflPass
 
   void runOnOperation() override {
     auto func = getOperation();
-    RewritePatternSet patterns(&getContext());
+    auto* ctx = &getContext();
+
+    RewritePatternSet patterns(ctx);
+    patterns.add<LowerGELU>(ctx);
     if (failed(applyPatternsAndFoldGreedily(func, std::move(patterns)))) {
       signalPassFailure();
     }
