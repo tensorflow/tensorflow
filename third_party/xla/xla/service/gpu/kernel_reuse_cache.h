504acf4313f6f1fc088da45579205691efcf0569
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-09 18:39:07 -0700

    [NCCL] Upgrade nvtx dependency
    PiperOrigin-RevId: 650821253

--- third_party/xla/xla/service/gpu/kernel_reuse_cache.h
+++ third_party/xla/xla/service/gpu/kernel_reuse_cache.h
@@ -22,6 +22,8 @@ limitations under the License.
 #include <utility>
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
