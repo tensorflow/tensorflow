4d14ff6a1f39573a65903d5de105381769136ada
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-09 18:52:05 -0700

    Automated Code Change
    PiperOrigin-RevId: 650823347

--- tensorflow/lite/kernels/internal/tensor_utils.cc
+++ tensorflow/lite/kernels/internal/tensor_utils.cc
@@ -14,10 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/kernels/internal/tensor_utils.h"
 
-#include "tensorflow/lite/kernels/internal/optimized/neon_check.h"
-
 #if defined(__SSSE3__) && !defined(TF_LITE_STATIC_MEMORY)
-#include "tensorflow/lite/kernels/internal/optimized/sse_tensor_utils.h"
 #elif defined(USE_NEON) && !defined(TF_LITE_STATIC_MEMORY)
 #include "tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.h"
 #else
