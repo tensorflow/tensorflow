fdfa858eaff645645affc33d031385ef48941787
Ezekiel Calubaquib <ecalubaquib@google.com>
2024-07-10 09:53:26 -0700

    moving TF/lite/python/metrics/converter_error_data.proto to TF/mlir/lite/python/metrics
    PiperOrigin-RevId: 651056793

--- tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.cc
+++ tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.cc
@@ -65,6 +65,7 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/lite/experimental/remat/metadata_util.h"
 #include "tensorflow/compiler/mlir/lite/flatbuffer_export.h"
 #include "tensorflow/compiler/mlir/lite/ir/tfl_ops.h"
+#include "tensorflow/compiler/mlir/lite/metrics/converter_error_data.pb.h"
 #include "tensorflow/compiler/mlir/lite/metrics/error_collector.h"
 #include "tensorflow/compiler/mlir/lite/metrics/error_collector_inst.h"
 #include "tensorflow/compiler/mlir/lite/quantization/stablehlo/quantization.h"
@@ -100,7 +101,6 @@ limitations under the License.
 #include "tensorflow/core/ir/types/dialect.h"
 #include "tensorflow/core/protobuf/meta_graph.pb.h"
 #include "tensorflow/core/public/session.h"
-#include "tensorflow/lite/python/metrics/converter_error_data.pb.h"
 #include "tensorflow/lite/tools/optimize/quantize_weights.h"
 #include "tsl/platform/protobuf.h"  // IWYU pragma: keep
 #include "tsl/platform/statusor.h"
