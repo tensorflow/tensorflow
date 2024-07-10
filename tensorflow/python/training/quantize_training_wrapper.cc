54342e37b8e5ca43ea5fc740c8820cfd550e5238
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-09 19:16:51 -0700

    Automated Code Change
    PiperOrigin-RevId: 650827528

--- tensorflow/python/training/quantize_training_wrapper.cc
+++ tensorflow/python/training/quantize_training_wrapper.cc
@@ -17,6 +17,8 @@ limitations under the License.
 
 #include "pybind11/pybind11.h"  // from @pybind11
 #include "tensorflow/core/common_runtime/quantize_training.h"
+#include "tensorflow/core/platform/errors.h"
+#include "tensorflow/core/platform/types.h"
 #include "tensorflow/python/lib/core/pybind11_lib.h"
 #include "tensorflow/python/lib/core/pybind11_status.h"
 
