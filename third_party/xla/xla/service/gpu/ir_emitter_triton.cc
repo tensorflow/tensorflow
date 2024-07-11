5479bd7ebcbf3ae866c929d6b545cefa0c9415a0
Oleg Shyshkov <shyshkov@google.com>
2024-07-11 03:25:32 -0700

    [XLA:GPU] Replace block_id_to_tile_offsets_indexing with N-d tile_offsets_indexing map.
    Currently we compute an indexing map from 1-d block_id to N-d tile offset for each TiledHloInstruction. We use that indexing map to deduplicate identical tiles. To get the map we compute delinearization of block_id in SymbolicTileAnalysis.
    
    Composition and simplification of `block_id_to_tile_offsets_indexing` is actually very computationally intensive, because it the expression has a lot of mods and floordivs from delinearization. This is not necessary for out purposes.
    
    After this change, `TiledHloComputation` will have N-d to M-d map from N-d tile indexing into M-d tile offsets of the instruction. This way expressions in the map
    are much smaller and easier to simplify (see changes in symbolic_tile_analysis_test).
    
    This change has an additional benefit that we don't enforce 1-d launch grid at the early stage.
    
    PiperOrigin-RevId: 651344451

--- third_party/xla/xla/service/gpu/ir_emitter_triton.cc
+++ third_party/xla/xla/service/gpu/ir_emitter_triton.cc
@@ -865,13 +865,14 @@ absl::StatusOr<Value> EmitTiledHloInstruction(
     ImplicitLocOpBuilder& b, absl::string_view libdevice_path,
     const se::DeviceDescription& device_info,
     const HloFusionInstruction* fusion, const TiledHloInstruction& tiled_hlo,
-    mlir::triton::FuncOp fn, Value pid,
+    mlir::triton::FuncOp fn, ValueRange tile_multi_index,
     absl::flat_hash_map<const TiledHloInstruction*, Value>& values) {
   const HloInstruction* hlo = tiled_hlo.hlo();
 
   if (fusion->IsUserOf(tiled_hlo.hlo())) {
     auto make_tensor = ir_emitter_triton_internal::CreateMakeTensorPtrOp(
-        b, pid, tiled_hlo, fn.getArgument(fusion->operand_index(hlo)));
+        b, tile_multi_index, tiled_hlo,
+        fn.getArgument(fusion->operand_index(hlo)));
 
     return EmitParameterLoad(b, make_tensor.op, make_tensor.boundary_checks);
   }
@@ -922,13 +923,14 @@ absl::StatusOr<Value> EmitTiledScope(
     const se::DeviceDescription& device_info,
     const HloFusionInstruction* fusion,
     const TiledHloComputation& tiled_computation, mlir::triton::FuncOp fn,
-    Value pid) {
+    ValueRange tile_multi_index) {
   absl::flat_hash_map<const TiledHloInstruction*, Value> values;
   for (const TiledHloInstruction* tiled_hlo :
        tiled_computation.instructions()) {
-    TF_ASSIGN_OR_RETURN(Value result, EmitTiledHloInstruction(
-                                          b, libdevice_path, device_info,
-                                          fusion, *tiled_hlo, fn, pid, values));
+    TF_ASSIGN_OR_RETURN(
+        Value result,
+        EmitTiledHloInstruction(b, libdevice_path, device_info, fusion,
+                                *tiled_hlo, fn, tile_multi_index, values));
     TF_RET_CHECK(values.insert({tiled_hlo, result}).second)
         << tiled_hlo->hlo()->ToString();
     VLOG(8) << "Emitted "
@@ -2454,8 +2456,9 @@ absl::Status EmitMatMul(mlir::OpBuilder builder,
   return absl::OkStatus();
 }
 
-// Computes the base pointer offset for the given pid and shape.
-Value ComputeBasePtrOffset(ImplicitLocOpBuilder b, Value pid,
+// Computes the base pointer offset for the given tile multi-index and hlo shape
+// taking into account the physical layout of the hlo buffer.
+Value ComputeBasePtrOffset(ImplicitLocOpBuilder b, ValueRange tile_multi_index,
                            const TiledHloInstruction& tiled_hlo) {
   const Shape& shape = tiled_hlo.hlo()->shape();
   Shape linear_shape = ShapeUtil::MakeShape(shape.element_type(),
@@ -2464,21 +2467,43 @@ Value ComputeBasePtrOffset(ImplicitLocOpBuilder b, Value pid,
   // Bitcast map gives an indexing map from linear index to the parameter shape
   // index respecting physical layout of the memory.
   auto bitcast_map = GetBitcastMap(shape, linear_shape, b.getContext());
-  auto compose_indexing_maps = ComposeIndexingMaps(
-      tiled_hlo.block_id_to_tile_offsets_indexing(), bitcast_map);
+  auto compose_indexing_maps =
+      ComposeIndexingMaps(tiled_hlo.tile_offsets_indexing(), bitcast_map);
   compose_indexing_maps.Simplify();
 
   return b.create<ma::IndexCastUIOp>(
       b.getI64Type(), mlir_converter::ApplyIndexing(compose_indexing_maps,
-                                                    /*dims=*/pid,
+                                                    /*dims=*/tile_multi_index,
                                                     /*symbols=*/{}, b)[0]);
 }
 
 namespace ir_emitter_triton_internal {
 
+SmallVector<Value, 3> ComputeDelinearizedTileIndex(
+    ImplicitLocOpBuilder& b, const TiledHloComputation& tiled_hlo_computation) {
+  Value pid = b.create<ma::IndexCastUIOp>(
+      b.getIndexType(), b.create<mt::GetProgramIdOp>(mt::ProgramIDDim::X));
+
+  // Delinearize the block id.
+  mlir::AffineExpr program_id = mlir::getAffineDimExpr(0, b.getContext());
+  auto tile_exprs =
+      DelinearizeIndex(tiled_hlo_computation.num_output_tiles_per_dim(),
+                       program_id, b.getContext());
+
+  IndexingMap program_id_to_root_tile_offset = IndexingMap::FromTensorSizes(
+      mlir::AffineMap::get(/*dimCount=*/1, /*symbolCount=*/0, tile_exprs,
+                           b.getContext()),
+      /*dim_upper_bounds=*/{tiled_hlo_computation.num_output_tiles()},
+      /*symbol_upper_bounds=*/{});
+
+  return mlir_converter::ApplyIndexing(program_id_to_root_tile_offset,
+                                       /*dims=*/pid,
+                                       /*symbols=*/{}, b);
+}
+
 MakeTensorPtrOpAndBoundaryChecks CreateMakeTensorPtrOp(
-    ImplicitLocOpBuilder& b, Value pid, const TiledHloInstruction& tiled_hlo,
-    Value argument_block) {
+    ImplicitLocOpBuilder& b, ValueRange tile_multi_index,
+    const TiledHloInstruction& tiled_hlo, Value argument_block) {
   llvm::SmallVector<Value> sizes;
   llvm::SmallVector<Value> strides;
   llvm::SmallVector<Value> offsets;
@@ -2538,7 +2563,7 @@ MakeTensorPtrOpAndBoundaryChecks CreateMakeTensorPtrOp(
 
   // Manually compute pointer offset to avoid materialized fully parallel
   // dimensions in the tile. Current codegen tried to avoid size-1 dims.
-  Value ptr_offset = ComputeBasePtrOffset(b, pid, tiled_hlo);
+  Value ptr_offset = ComputeBasePtrOffset(b, tile_multi_index, tiled_hlo);
   auto tile_ptr = AddPtr(b, argument_block, ptr_offset);
 
   return MakeTensorPtrOpAndBoundaryChecks{b.create<mt::MakeTensorPtrOp>(
@@ -2575,19 +2600,23 @@ absl::Status EmitGeneric(mlir::OpBuilder builder,
   auto loc = mlir::NameLoc::get(builder.getStringAttr(root->name()));
   ImplicitLocOpBuilder b(loc, builder);
 
-  Value pid = b.create<ma::IndexCastUIOp>(
-      b.getIndexType(), b.create<mt::GetProgramIdOp>(mt::ProgramIDDim::X));
-
   TF_ASSIGN_OR_RETURN(TiledHloComputation tiled_hlo_computation,
                       symbolic_tile_analysis.ComputeTiledHloInstructions(
                           block_level_parameters.output_tile_sizes));
-  TF_ASSIGN_OR_RETURN(Value result,
-                      EmitTiledScope(b, libdevice_path, device_info, fusion,
-                                     tiled_hlo_computation, fn, pid));
+
+  SmallVector<Value, 3> tile_multi_index =
+      ir_emitter_triton_internal::ComputeDelinearizedTileIndex(
+          b, tiled_hlo_computation);
+
+  TF_ASSIGN_OR_RETURN(
+      Value result,
+      EmitTiledScope(b, libdevice_path, device_info, fusion,
+                     tiled_hlo_computation, fn, tile_multi_index));
 
   const auto& tiled_hlo = *tiled_hlo_computation.GetRoot();
   auto make_tensor = ir_emitter_triton_internal::CreateMakeTensorPtrOp(
-      b, pid, tiled_hlo, fn.getArgument(computation->num_parameters()));
+      b, tile_multi_index, tiled_hlo,
+      fn.getArgument(computation->num_parameters()));
   b.create<mt::StoreOp>(make_tensor.op, result, make_tensor.boundary_checks,
                         mt::CacheModifier::NONE, mt::EvictionPolicy::NORMAL);
 
