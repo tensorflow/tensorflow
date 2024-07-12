7404c105a45396bc01deb0d6a87e173fd0ee1f37
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-12 00:36:39 -0700

    Automated Code Change
    PiperOrigin-RevId: 651676705

--- tensorflow/lite/delegates/coreml/builders/op_builder.cc
+++ tensorflow/lite/delegates/coreml/builders/op_builder.cc
@@ -18,10 +18,12 @@ limitations under the License.
 #include <memory>
 #include <string>
 
+#include "mlmodel/format/Model.pb.h"
+#include "mlmodel/format/NeuralNetwork.pb.h"
 #include "tensorflow/lite/builtin_ops.h"
-#include "tensorflow/lite/core/c/builtin_op_data.h"
+#include "tensorflow/lite/c/c_api_types.h"
+#include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/delegates/coreml/builders/op_factory.h"
-#include "tensorflow/lite/kernels/kernel_util.h"
 
 namespace tflite {
 namespace delegates {
