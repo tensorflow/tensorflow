3e88d8724043f93c4f59e8305f4f291df2fc7e5f
Laura Pak <lpak@google.com>
2024-07-15 14:02:15 -0700

    Move third_party/tensorflow/lite/tools/optimize/quantize_weights to MLIR.
    PiperOrigin-RevId: 652592337

--- third_party/xla/xla/python/pjit.cc
+++ third_party/xla/xla/python/pjit.cc
@@ -375,6 +375,8 @@ PrepareIfrtInputs(const xla::PyLoadedExecutable& executable,
                   std::vector<nb::object>& keep_alive_objects) {
   const auto& addressable_devices =
       executable.ifrt_loaded_executable()->addressable_devices();
+  const auto& num_global_devices =
+      executable.ifrt_loaded_executable()->num_devices();
   int num_args = flat_dynamic_args.size();
 
   std::vector<tsl::RCReference<xla::ifrt::Array>> num_args_arrays;
@@ -467,7 +469,7 @@ PrepareIfrtInputs(const xla::PyLoadedExecutable& executable,
       continue;
     }
 
-    if (py_array.num_shards() != addressable_devices.size()) {
+    if (py_array.num_shards() != num_global_devices) {
       CHECK(in_device_local_layout.is_none());
       CallShardArgFallback(arg.ptr(), in_shardings[dce_index],
                            in_device_local_layout, shard_arg_fallback,
