14339e25ab47fe62664faae6a809679616cb82b4
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-16 13:47:11 -0700

    Adds test to ensure that the `Delete()` methods of all `ifrt::Array` implementations satisfy the idempotent property.
    PiperOrigin-RevId: 652963772

--- third_party/xla/xla/python/ifrt/array_impl_test_lib.cc
+++ third_party/xla/xla/python/ifrt/array_impl_test_lib.cc
@@ -718,6 +718,31 @@ TEST(ArrayImplTest, Delete) {
   TF_EXPECT_OK(array->Delete().Await());
 }
 
+TEST(ArrayImplTest, DeleteIsIdempotent) {
+  TF_ASSERT_OK_AND_ASSIGN(auto client, test_util::GetClient());
+
+  DType dtype(DType::kF32);
+  Shape shape({2, 3});
+  std::vector<float> data(6);
+  std::iota(data.begin(), data.end(), 0);
+  Device* device = client->addressable_devices().at(0);
+  std::shared_ptr<const Sharding> sharding =
+      SingleDeviceSharding::Create(device, MemoryKind());
+  auto semantics = Client::HostBufferSemantics::kImmutableOnlyDuringCall;
+
+  TF_ASSERT_OK_AND_ASSIGN(
+      auto array, client->MakeArrayFromHostBuffer(
+                      data.data(), dtype, shape,
+                      /*byte_strides=*/std::nullopt, sharding, semantics,
+                      /*on_done_with_host_buffer=*/{}));
+
+  auto future_1 = array->Delete();
+  auto future_2 = array->Delete();
+
+  TF_EXPECT_OK(future_1.Await());
+  TF_EXPECT_OK(future_2.Await());
+}
+
 TEST(ArrayImplTest, IsDeleted) {
   TF_ASSERT_OK_AND_ASSIGN(auto client, test_util::GetClient());
 
