b9274b40678a8413f5e7eb75bc17216f22edc73a
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-17 22:10:24 -0700

    Automated Code Change
    PiperOrigin-RevId: 653477264

--- third_party/xla/xla/service/llvm_compiler.h
+++ third_party/xla/xla/service/llvm_compiler.h
@@ -16,8 +16,13 @@ limitations under the License.
 #ifndef XLA_SERVICE_LLVM_COMPILER_H_
 #define XLA_SERVICE_LLVM_COMPILER_H_
 
+#include "absl/log/check.h"
+#include "absl/status/statusor.h"
 #include "llvm/IR/Module.h"
+#include "xla/executable_run_options.h"
+#include "xla/hlo/ir/hlo_module_group.h"
 #include "xla/service/compiler.h"
+#include "xla/service/executable.h"
 
 namespace xla {
 
