31bb2f690440c6be2b968f79f8aa1bb24b49637c
Mihai Maruseac <mihaimaruseac@google.com>
2024-07-21 07:07:02 -0700

    Fix wrong spelling fix

--- tensorflow/lite/python/lite.py
+++ tensorflow/lite/python/lite.py
@@ -1908,7 +1908,7 @@ class TFLiteFrozenGraphConverterV2(TFLiteConverterBaseV2):
     """Converts a TensorFlow GraphDef based on instance variables.
 
     Returns:
-      The converted data is in serialized format.
+      The converted data in serialized format.
 
     Raises:
       ValueError:
