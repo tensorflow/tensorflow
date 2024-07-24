bd2be7f9c4fdc65453340f6b7c66ae785d0bb2fe
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-23 20:41:44 -0700

    Automated Code Change
    PiperOrigin-RevId: 655409504

--- tensorflow/lite/tools/versioning/gpu_compatibility.cc
+++ tensorflow/lite/tools/versioning/gpu_compatibility.cc
@@ -21,10 +21,11 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
-#include "tensorflow/lite/builtin_op_data.h"
 #include "tensorflow/lite/builtin_ops.h"
 #include "tensorflow/lite/c/c_api_types.h"
+#include "tensorflow/lite/core/c/builtin_op_data.h"
 #include "tensorflow/lite/core/c/common.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/tools/versioning/op_signature.h"
 
 namespace tflite {
