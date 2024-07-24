05691c2af966ef4d08a4efbbf7442357e68aad65
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-23 17:47:49 -0700

    [XLA:MSA] Implement an auxiliary function (SimulateAsyncCopyDone) to simulate the overhead of processing copy-done instruction.
    This CL implements a function to simulate the overhead of copy-done instructions. Specifically, there are two directions which share the bandwidth: default-read and default-write. Two directions will share the bandwidth equally. For example, when we process a default-read request, if there are also outstanding default-write process, we can only use half of the full bandwidth to process requests in each direction.
    
    PiperOrigin-RevId: 655365388

--- third_party/xla/xla/service/memory_space_assignment/simulator.h
+++ third_party/xla/xla/service/memory_space_assignment/simulator.h
@@ -16,6 +16,10 @@ limitations under the License.
 #ifndef XLA_SERVICE_MEMORY_SPACE_ASSIGNMENT_SIMULATOR_H_
 #define XLA_SERVICE_MEMORY_SPACE_ASSIGNMENT_SIMULATOR_H_
 
+#include <cstdint>
+#include <list>
+
+#include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/utils/hlo_live_range.h"
 #include "xla/service/memory_space_assignment/allocation.h"
 #include "xla/service/memory_space_assignment/cost_analysis.h"
@@ -23,12 +27,49 @@ limitations under the License.
 namespace xla {
 namespace memory_space_assignment {
 
+enum class MemoryTransferDirection {
+  kUnsupported,
+  kDefaultToAlternate,
+  kAlternateToDefault,
+};
+
+// REQUIRES:
+// * async_copy must be an async copy-start instruction.
+MemoryTransferDirection GetAsyncCopyDirection(const HloInstruction* async_copy,
+                                              int64_t alternate_memory_space);
+
+// This struct is used to track the outstanding async copy instructions and
+// the remaining bytes required to be accessed.
+struct OutstandingAsyncCopy {
+  const HloInstruction* copy_start_inst;
+  float remaining_bytes_to_transfer;
+  bool operator==(const OutstandingAsyncCopy& other) const {
+    return copy_start_inst == other.copy_start_inst &&
+           remaining_bytes_to_transfer == other.remaining_bytes_to_transfer;
+  }
+};
+
 // A wrapper class around runtime simulator.
 class RuntimeSimulator {
  public:
-  explicit RuntimeSimulator(CostAnalysis* cost_analysis)
-      : cost_analysis_(cost_analysis) {}
-  virtual ~RuntimeSimulator() = default;
+  explicit RuntimeSimulator(CostAnalysis* cost_analysis,
+                            int64_t alternate_memory_space)
+      : cost_analysis_(cost_analysis),
+        alternate_memory_space_(alternate_memory_space) {}
+
+  // This constructor is used to inject the outstanding async copy queues for
+  // testing purpose.
+  explicit RuntimeSimulator(
+      CostAnalysis* cost_analysis, int64_t alternate_memory_space,
+      const std::list<OutstandingAsyncCopy>& outstanding_read_default_queue,
+      const std::list<OutstandingAsyncCopy>& outstanding_write_default_queue)
+      : cost_analysis_(cost_analysis),
+        alternate_memory_space_(alternate_memory_space),
+        outstanding_read_default_queue_(outstanding_read_default_queue),
+        outstanding_write_default_queue_(outstanding_write_default_queue) {}
+
+  ~RuntimeSimulator() = default;
+
   // This function is used to predict the effectiveness of the memory space
   // assignment solution. Specifically, it returns the estimated execution time
   // (in seconds) of the HLO module for the given memory space assignment (i.e.,
@@ -36,9 +77,39 @@ class RuntimeSimulator {
   float ComputeEstimatedElapsedTime(const HloLiveRange& hlo_live_range,
                                     const AllocationSequence& allocations);
 
+  // This is an auxiliary function for simulating the execution
+  // time for executing a copy-done instruction. It returns the
+  // elapsed time (in seconds) for executing the copy-done instruction.
+  //
+  // This function also updates the passed in queues as we complete async copies
+  // during the simulation.
+  //
+  // We simulate the shared bandwidth for default-alternate memory access.
+  // For example, if the copy-done instruction is a default-write memory
+  // process, and there are outstanding default-read memory processes in the
+  // outstanding_read_default_queue, then we use half of the bandwidth to
+  // process both requests in parallel. Otherwise, we use the full bandwidth to
+  // process the default-write request.
+  float SimulateAsyncCopyDone(const HloInstruction* copy_done_instruction);
+
+  const std::list<OutstandingAsyncCopy>& GetOutstandingReadDefaultQueue() const;
+
+  const std::list<OutstandingAsyncCopy>& GetOutstandingWriteDefaultQueue()
+      const;
+
  private:
   const CostAnalysis* cost_analysis_;
   CostAnalysis::Cache cost_analysis_cache_;
+  // Members used for memory model simulation
+  int64_t alternate_memory_space_;
+  std::list<OutstandingAsyncCopy> outstanding_read_default_queue_;
+  std::list<OutstandingAsyncCopy> outstanding_write_default_queue_;
+  // This function updates the queue by updating the front request with the
+  // processed bytes. If the request is completed (no remaining bytes to
+  // process), the function returns the instruction and pop it from the queue.
+  // Otherwise, it returns nullptr.
+  const HloInstruction* RemoveBytesFromQueueIfNotEmpty(
+      std::list<OutstandingAsyncCopy>& async_copy_queue, float processed_bytes);
 };
 }  // namespace memory_space_assignment
 }  // namespace xla
