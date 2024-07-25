677607d9ba59454c0ded0a8d0c3836824f2a5958
Adam Banaś <adambanas@google.com>
2024-07-25 12:38:40 -0700

    [XLA:CPU] Fix LLVM compiler test for thunks runtime.
    PiperOrigin-RevId: 656047728

--- third_party/xla/xla/tests/llvm_compiler_test.cc
+++ third_party/xla/xla/tests/llvm_compiler_test.cc
@@ -41,10 +41,12 @@ namespace {
 using LLVMCompilerTest = HloTestBase;
 
 const char* const kHloText = R"(
-HloModule Constant
+HloModule Add
 
 ENTRY main  {
-  ROOT constant = f32[] constant(42.0)
+  constant.0 = f32[] constant(42.0)
+  constant.1 = f32[] constant(43.0)
+  ROOT add.0 = f32[] add(constant.0, constant.1)
 }
 )";
 
@@ -61,8 +63,12 @@ TEST_F(LLVMCompilerTest, HooksTest) {
     return absl::OkStatus();
   };
 
-  // Create HLO module, and run the compiler.
+  // Create HLO module. Note this module needs to consist of at least one
+  // instruction that is compiled using LLVM (e.g. for CPU thunks runtime it is
+  // 'add' instruction), otherwise the hooks are never called.
   auto hlo_module = ParseAndReturnVerifiedModule(kHloText).value();
+
+  // Create and run the compiler.
   LLVMCompiler* compiler =
       tensorflow::down_cast<xla::LLVMCompiler*>(backend().compiler());
   compiler->SetPreOptimizationHook(pre_opt_hook);
