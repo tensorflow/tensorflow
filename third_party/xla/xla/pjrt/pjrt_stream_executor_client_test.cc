479e7ef06e972b0771ac1e98278e9788457c0551
Kyle Lucke <klucke@google.com>
2024-07-31 16:20:04 -0700

    Use Platform::ExecutorForDevice rather than GetExecutor, as device ordinal is all that's being searched for.
    PiperOrigin-RevId: 658179612

--- third_party/xla/xla/pjrt/pjrt_stream_executor_client_test.cc
+++ third_party/xla/xla/pjrt/pjrt_stream_executor_client_test.cc
@@ -46,10 +46,8 @@ absl::StatusOr<std::unique_ptr<PjRtStreamExecutorClient>> GetClient() {
   LocalClient* local_client = xla::ClientLibrary::LocalClientOrDie();
   TF_ASSIGN_OR_RETURN(se::Platform * platform,
                       PlatformUtil::GetPlatform("Host"));
-  se::StreamExecutorConfig config;
-  config.ordinal = 0;
   TF_ASSIGN_OR_RETURN(se::StreamExecutor * executor,
-                      platform->GetExecutor(config));
+                      platform->ExecutorForDevice(0));
   auto device_state = std::make_unique<LocalDeviceState>(
       executor, local_client, LocalDeviceState::kSynchronous,
       /*max_inflight_computations=*/32,
