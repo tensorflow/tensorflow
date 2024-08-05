2246e59a88f739899cdf54990082fcba38472ba1
Adam Banaś <adambanas@google.com>
2024-08-05 01:58:48 -0700

    Fix loading inputs from file in `run_hlo_module_main.cc`
    Starting iteration loop indexing from 0 (instead of 1) is more natural. It solves the issue with loading literals from file, which wasn't working for the last iteration (so in case of one iteration, it wasn't working at all).
    
    PiperOrigin-RevId: 659474095

--- third_party/xla/xla/tools/run_hlo_module_main.cc
+++ third_party/xla/xla/tools/run_hlo_module_main.cc
@@ -252,9 +252,9 @@ int main(int argc, char** argv) {
                                 &input_literals_proto);
     }
 
-    for (int i = 1; i <= iteration_count; ++i) {
+    for (int i = 0; i < iteration_count; ++i) {
       if (iteration_count != 1) {
-        std::cerr << "\n=== Iteration " << i << "\n";
+        std::cerr << "\n=== Iteration " << i + 1 << "\n";
       }
       xla::RunHloModuleIterationLiterals* iteration_literals_proto = nullptr;
       if (!opts.output_literals_file.empty() ||
@@ -276,7 +276,7 @@ int main(int argc, char** argv) {
           opts, iteration_literals_proto,
           /*reference_module_modifier_hook=*/{},
           [&](xla::HloModuleConfig* config) {
-            config->set_seed(different_random_seeds ? i : 42);
+            config->set_seed(different_random_seeds ? i + 1 : 42);
           });
 
       if (result.ok()) {
