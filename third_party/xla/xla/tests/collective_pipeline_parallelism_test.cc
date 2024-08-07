165752e47096430113efade5f0b8f2e1e367cb02
Frederik Gossen <frgossen@google.com>
2024-08-07 15:03:48 -0700

    [XLA:GPU] Share common HLO computations between all of the pipeline tests
    PiperOrigin-RevId: 660552175

--- third_party/xla/xla/tests/collective_pipeline_parallelism_test.cc
+++ third_party/xla/xla/tests/collective_pipeline_parallelism_test.cc
@@ -117,146 +117,6 @@ XLA_TEST_F(CollectivePipelineParallelismTest,
   LiteralTestUtil::ExpectR2Equal<float>({{0, 0}, {1, 1}}, results[3]);
 }
 
-// Naive implementation of pipeline parallelism:
-//   - 4 devices
-//   - 4 microbatches
-//   - no circular repeat
-//   - no disabled collectives
-//   - no collective pipelining
-//
-// Every stage of the pipeline is a single linear layer.
-XLA_TEST_F(CollectivePipelineParallelismTest, NaiveDFSMicrobatch4Replica4) {
-  const absl::string_view kModuleStr = R"(
-  HloModule test
-
-  get_circ_buffer_index {
-    offset = u32[] parameter(0)
-    index = u32[] parameter(1)
-    size = u32[] parameter(2)
-    t0 = u32[] add(offset, index)
-    t1 = u32[] divide(t0, size)
-    t2 = u32[] multiply(t1, size)
-    ROOT t4 = u32[] subtract(t0, t2)
-  }
-
-  is_input_replica {
-    replica_id = u32[] replica-id()
-    c0 = u32[] constant(0)
-    ROOT predicate = pred[] compare(replica_id, c0), direction=EQ
-  }
-
-  is_output_replica {
-    replica_id = u32[] replica-id()
-    c1 = u32[] constant(1)
-    ROOT predicate = pred[] compare(replica_id, c1), direction=EQ
-  }
-
-  while_condition {
-    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) parameter(0)
-    i = u32[] get-tuple-element(tuple), index=4
-    n = u32[] constant(7)
-    ROOT predicate = pred[] compare(i, n), direction=LT
-  }
-
-  while_body {
-    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) parameter(0)
-    weights = f32[16,16] get-tuple-element(tuple), index=0
-    input = f32[4,16] get-tuple-element(tuple), index=1
-    output = f32[4,16] get-tuple-element(tuple), index=2
-    tmp = f32[16] get-tuple-element(tuple), index=3
-    i = u32[] get-tuple-element(tuple), index=4
-
-    c1 = u32[] constant(1)
-    c0 = u32[] constant(0)
-    c4 = u32[] constant(4)
-
-    input_idx = u32[] call(c0, i, c4), to_apply=get_circ_buffer_index
-    input_slice = f32[1,16] dynamic-slice(input, input_idx, c0),
-        dynamic_slice_sizes={1,16}
-    input_slice_ = f32[16] reshape(input_slice)
-
-    prev_stage_slice = f32[16] collective-permute(tmp),
-        source_target_pairs={{0,1}, {1,2}, {2,3}, {3,0}}
-
-    read_input = pred[] call(), to_apply=is_input_replica
-    compute_in = f32[16] select(read_input, input_slice_, prev_stage_slice)
-
-    compute_out = f32[16] dot(weights, compute_in), lhs_contracting_dims={1},
-        rhs_contracting_dims={0}
-
-    output_index = u32[] call(c1, i, c4), to_apply=get_circ_buffer_index
-    output_slice = f32[1,16] reshape(compute_out)
-    output_ = f32[4,16] dynamic-update-slice(output, output_slice, output_index,
-        c0)
-
-    i_ = add(i, c1)
-
-    ROOT tuple1 = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) tuple(
-        weights, input, output_, compute_out, i_)
-  }
-
-  ENTRY main {
-    weights = f32[16,16] parameter(0)
-    input = f32[4,16] parameter(1)
-
-    cf0 = f32[] constant(0)
-    output = f32[4,16] broadcast(cf0), dimensions={}
-    tmp = f32[16] broadcast(cf0), dimensions={}
-    c0 = u32[] constant(0)
-
-    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) tuple(weights,
-        input, output, tmp, c0)
-    tuple_ = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) while(tuple),
-        condition=while_condition, body=while_body
-
-    ROOT output_ = f32[4,16] get-tuple-element(tuple_), index=2
-  }
-  )";
-
-  const int64_t kNumReplicas = 4;
-  SKIP_TEST_IF_NUM_DEVICES_LESS_THAN(kNumReplicas)
-
-  HloModuleConfig config =
-      GetModuleConfigForTest(/*replica_count=*/kNumReplicas);
-  TF_ASSERT_OK_AND_ASSIGN(auto module,
-                          ParseAndReturnVerifiedModule(kModuleStr, config));
-
-  // This pipeline consists of 4 layers, each of which is a single linear layer.
-  // We assign the weights to the replicas such that the layers scale the input
-  // data by 1.0, 2.0, 3.0 and 4.0. The combined effect is to scale the input
-  // data by 24.0.
-  const int64_t kInputSize = 16;
-  Literal weights_r0 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 1.0);
-  Literal weights_r1 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 2.0);
-  Literal weights_r2 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 3.0);
-  Literal weights_r3 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 4.0);
-
-  // Only the first replica holds the input to the pipeline in this naive
-  // implementation. The remaining replicas get zero/dummy input.
-  const int64_t kMicrobatches = 4;
-  Literal real_input =
-      LiteralUtil::CreateFingerprintMatixR2<float>(kMicrobatches, kInputSize);
-  Literal fake_input =
-      LiteralUtil::CreateFull<float>({kMicrobatches, kInputSize}, 0.0);
-
-  std::vector<std::vector<Literal *>> args = {{&weights_r0, &real_input},
-                                              {&weights_r1, &fake_input},
-                                              {&weights_r2, &fake_input},
-                                              {&weights_r3, &fake_input}};
-  TF_ASSERT_OK_AND_ASSIGN(
-      std::vector<Literal> results,
-      ExecuteReplicated(std::move(module), args, kNumReplicas,
-                        /*run_hlo_passes=*/true));
-
-  // Check pipeline output for last replica.
-  // The combined effect of the pipeline is to scale the input data by 24.0.
-  const float kExpectedFactor = 1.0 * 2.0 * 3.0 * 4.0;
-  Literal expected_output = LiteralUtil::CreateFingerprintMatixR2<float>(
-      kMicrobatches, kInputSize, kExpectedFactor);
-  EXPECT_TRUE(LiteralTestUtil::NearOrEqual(expected_output, results[3],
-                                           ErrorSpec{1e-5, 1e-5}));
-}
-
 std::string GetModuleStrWithCommonComputations(
     const std::string name, const std::string more_computations) {
   static constexpr char kCommonComputationsStr[] = R"(
@@ -342,6 +202,123 @@ std::string GetModuleStrWithCommonComputations(
   )";
   return "HloModule " + name + "\n" + kCommonComputationsStr + "\n" +
          more_computations;
+}
+
+// Naive implementation of pipeline parallelism:
+//   - 4 devices
+//   - 4 microbatches
+//   - no circular repeat
+//   - no disabled collectives
+//   - no collective pipelining
+//
+// Every stage of the pipeline is a single linear layer.
+XLA_TEST_F(CollectivePipelineParallelismTest, NaiveDFSMicrobatch4Replica4) {
+  constexpr char kMoreComputationsStr[] = R"(
+  while_condition {
+    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) parameter(0)
+    i = u32[] get-tuple-element(tuple), index=4
+    n = u32[] constant(7)
+    ROOT predicate = pred[] compare(i, n), direction=LT
+  }
+
+  while_body {
+    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) parameter(0)
+    weights = f32[16,16] get-tuple-element(tuple), index=0
+    input = f32[4,16] get-tuple-element(tuple), index=1
+    output = f32[4,16] get-tuple-element(tuple), index=2
+    prev_iteration_compute_res = f32[16] get-tuple-element(tuple), index=3
+    i = u32[] get-tuple-element(tuple), index=4
+
+    c0 = u32[] constant(0)
+    c1 = u32[] constant(1)
+    c4 = u32[] constant(4)
+
+    // Read from buffers.
+    input_slice = f32[16] call(input, c0, i), to_apply=read_buffer_mb4
+
+    // Shift data to the next stage in the pipeline.
+    prev_stage_slice = f32[16] collective-permute(prev_iteration_compute_res),
+        source_target_pairs={{0,1}, {1,2}, {2,3}, {3,0}}
+
+    // Select compute argument from previous stage or from input and perform
+    // compute.
+    read_input = pred[] call(), to_apply=is_input_replica
+    compute_arg = f32[16] select(read_input, input_slice, prev_stage_slice)
+    compute_res = f32[16] dot(weights, compute_arg), lhs_contracting_dims={1},
+        rhs_contracting_dims={0}
+
+    // Update buffers.
+    output_ = call(output, compute_res, c1, i), to_apply=update_buffer_mb4
+
+    i_ = add(i, c1)
+
+    ROOT tuple1 = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) tuple(
+        weights, input, output_, compute_res, i_)
+  }
+
+  ENTRY main {
+    weights = f32[16,16] parameter(0)
+    input = f32[4,16] parameter(1)
+
+    cf0 = f32[] constant(0)
+    output = f32[4,16] broadcast(cf0), dimensions={}
+    prev_iteration_compute_res = f32[16] broadcast(cf0), dimensions={}
+    c0 = u32[] constant(0)
+
+    tuple = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) tuple(weights,
+        input, output, prev_iteration_compute_res, c0)
+    tuple_ = (f32[16,16], f32[4,16], f32[4,16], f32[16], u32[]) while(tuple),
+        condition=while_condition, body=while_body
+
+    ROOT output_ = f32[4,16] get-tuple-element(tuple_), index=2
+  }
+  )";
+
+  const int64_t kNumReplicas = 4;
+  SKIP_TEST_IF_NUM_DEVICES_LESS_THAN(kNumReplicas)
+
+  HloModuleConfig config =
+      GetModuleConfigForTest(/*replica_count=*/kNumReplicas);
+  TF_ASSERT_OK_AND_ASSIGN(
+      auto module,
+      ParseAndReturnVerifiedModule(GetModuleStrWithCommonComputations(
+                                       /*name=*/"test", kMoreComputationsStr),
+                                   config));
+
+  // This pipeline consists of 4 layers, each of which is a single linear layer.
+  // We assign the weights to the replicas such that the layers scale the input
+  // data by 1.0, 2.0, 3.0 and 4.0. The combined effect is to scale the input
+  // data by 24.0.
+  const int64_t kInputSize = 16;
+  Literal weights_r0 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 1.0);
+  Literal weights_r1 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 2.0);
+  Literal weights_r2 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 3.0);
+  Literal weights_r3 = LiteralUtil::MakeScalarMatrixR2<float>(kInputSize, 4.0);
+
+  // Only the first replica holds the input to the pipeline in this naive
+  // implementation. The remaining replicas get zero/dummy input.
+  const int64_t kMicrobatches = 4;
+  Literal real_input =
+      LiteralUtil::CreateFingerprintMatixR2<float>(kMicrobatches, kInputSize);
+  Literal fake_input =
+      LiteralUtil::CreateFull<float>({kMicrobatches, kInputSize}, 0.0);
+
+  std::vector<std::vector<Literal *>> args = {{&weights_r0, &real_input},
+                                              {&weights_r1, &fake_input},
+                                              {&weights_r2, &fake_input},
+                                              {&weights_r3, &fake_input}};
+  TF_ASSERT_OK_AND_ASSIGN(
+      std::vector<Literal> results,
+      ExecuteReplicated(std::move(module), args, kNumReplicas,
+                        /*run_hlo_passes=*/true));
+
+  // Check pipeline output for last replica.
+  // The combined effect of the pipeline is to scale the input data by 24.0.
+  const float kExpectedFactor = 1.0 * 2.0 * 3.0 * 4.0;
+  Literal expected_output = LiteralUtil::CreateFingerprintMatixR2<float>(
+      kMicrobatches, kInputSize, kExpectedFactor);
+  EXPECT_TRUE(LiteralTestUtil::NearOrEqual(expected_output, results[3],
+                                           ErrorSpec{1e-5, 1e-5}));
 }
 
 // Naive implementation of pipeline parallelism:
