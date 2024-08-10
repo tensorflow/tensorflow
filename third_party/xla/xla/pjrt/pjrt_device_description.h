7a010c730937faf55c497897db9a233dec5adfeb
Parker Schuh <parkers@google.com>
2024-08-09 18:47:55 -0700

    Basic version of adding memory spaces to CompileOnlyClient.
    PiperOrigin-RevId: 661480736

--- third_party/xla/xla/pjrt/pjrt_device_description.h
+++ third_party/xla/xla/pjrt/pjrt_device_description.h
@@ -20,11 +20,34 @@ limitations under the License.
 #include <string_view>
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "xla/pjrt/pjrt_common.h"
 
 namespace xla {
 
 using PjRtDeviceAttribute = PjRtValueType;
+
+class PjRtMemorySpaceDescription {
+ public:
+  PjRtMemorySpaceDescription(absl::string_view kind, int kind_id)
+      : kind_(kind), kind_id_(kind_id) {}
+
+  // A platform-dependent string that uniquely identifies the kind of the
+  // memory space.
+  absl::string_view kind() const { return kind_; }
+
+  // An ID uniquely identifies the kind of the memory space among those attached
+  // to the same `PjRtClient`. The IDs assigned to a kind is implementation
+  // specific.
+  int kind_id() const { return kind_id_; }
+
+ private:
+  absl::string_view kind_;
+  int kind_id_;
+};
 
 class PjRtDeviceDescription {
  public:
@@ -60,6 +83,19 @@ class PjRtDeviceDescription {
   // reference will remain valid for the lifetime of the PjRtDevice.
   virtual const absl::flat_hash_map<std::string, PjRtDeviceAttribute>&
   Attributes() const = 0;
+
+  // Returns all memory spaces attached to this device.
+  // The memory spaces are in no particular order.
+  virtual absl::Span<const PjRtMemorySpaceDescription* const> memory_spaces()
+      const {
+    return {};
+  }
+
+  // Returns the default memory space attached to this device.
+  virtual absl::StatusOr<const PjRtMemorySpaceDescription*>
+  default_memory_space() const {
+    return absl::UnimplementedError("default_memory_space Not implemented.");
+  }
 };
 
 }  // namespace xla
