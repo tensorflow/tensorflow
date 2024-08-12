541d95260cf9e3f6a3b6f046a387b11aeeea4ab3
A. Unique TensorFlower <gardener@tensorflow.org>
2024-08-12 08:50:54 -0700

    Set batch timeout and number of batch threads in the batch stats registry.
    PiperOrigin-RevId: 662102488

--- tensorflow/core/runtime_fallback/runtime/fallback_batch_kernel.h
+++ tensorflow/core/runtime_fallback/runtime/fallback_batch_kernel.h
@@ -29,6 +29,7 @@ limitations under the License.
 #include "tensorflow/core/kernels/batching_util/adaptive_shared_batch_scheduler.h"
 #include "tensorflow/core/kernels/batching_util/batch_resource_base.h"
 #include "tensorflow/core/kernels/batching_util/batch_scheduler.h"
+#include "tensorflow/core/kernels/batching_util/batch_stats.h"
 #include "tensorflow/core/kernels/batching_util/warmup.h"
 #include "tensorflow/core/lib/core/refcount.h"
 #include "tensorflow/core/lib/core/status.h"
@@ -224,6 +225,13 @@ void BatchFunctionFallbackKernel<BatchResourceType>::ComputeAsync(
           low_priority_max_enqueued_batches_;
       batch_resource_options.low_priority_allowed_batch_sizes =
           low_priority_allowed_batch_sizes_;
+
+      serving::ModelBatchStats& model_batch_stats =
+          serving::GlobalBatchStatsRegistry().model(
+              /* model_name= */ std::string(GetModelName(c)),
+              /* op_name= */ c->op_kernel().name());
+      model_batch_stats.SetBatchTimeoutMicros(batch_timeout_micros_);
+      model_batch_stats.SetNumBatchThreads(num_batch_threads_);
 
       std::unique_ptr<BatchResourceType> new_resource;
       auto status = BatchResourceType::Create(
