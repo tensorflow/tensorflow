8c501ffe7f7c814432b19a5788319edf82f1dcf7
A. Unique TensorFlower <gardener@tensorflow.org>
2024-08-14 04:24:06 -0700

    Automated Code Change
    PiperOrigin-RevId: 662867583

--- tensorflow/lite/core/api/flatbuffer_conversions.cc
+++ tensorflow/lite/core/api/flatbuffer_conversions.cc
@@ -20,9 +20,8 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 
-#include "flatbuffers/flatbuffers.h"  // from @flatbuffers
 #include "flatbuffers/vector.h"  // from @flatbuffers
-#include "tensorflow/lite/core/api/error_reporter.h"
+#include "tensorflow/compiler/mlir/lite/core/api/error_reporter.h"
 #include "tensorflow/lite/core/c/builtin_op_data.h"
 #include "tensorflow/lite/core/c/common.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
