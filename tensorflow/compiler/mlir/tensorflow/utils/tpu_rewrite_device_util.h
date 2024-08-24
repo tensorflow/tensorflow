6b8eacc75d92c3f17c67c4ff9eec5c06a31df17b
A. Unique TensorFlower <gardener@tensorflow.org>
2024-08-23 18:24:32 -0700

    Automated Code Change
    PiperOrigin-RevId: 666986071

--- tensorflow/compiler/mlir/tensorflow/utils/tpu_rewrite_device_util.h
+++ tensorflow/compiler/mlir/tensorflow/utils/tpu_rewrite_device_util.h
@@ -20,12 +20,15 @@ limitations under the License.
 #include <string>
 #include <utility>
 
+#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "mlir/IR/Attributes.h"  // from @llvm-project
+#include "mlir/IR/BuiltinAttributes.h"  // from @llvm-project
 #include "mlir/IR/Operation.h"  // from @llvm-project
+#include "mlir/IR/Types.h"  // from @llvm-project
 #include "mlir/Support/LogicalResult.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_device.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_structs.h"
