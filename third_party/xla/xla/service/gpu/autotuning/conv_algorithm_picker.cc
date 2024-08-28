614104555a559753ddd0f3d9beaffc3d3cdc6fc3
Philipp Hack <phack@nvidia.com>
2024-08-28 06:53:50 -0700

    PR #14111: Tuple Outputs in Convolution Algorithm Picker
    Imported from GitHub PR https://github.com/openxla/xla/pull/14111
    
    TestConvAmaxF8 and TestConvReluAmaxF8 in cudnn_fused_conv_rewriter_test.cc return tuples. The algorithm picker currently creates a BufferComparator for the output shape of the convolution even though that class doesn't support tuples. The current change to conv_algorithm_picker.cc resolves this by separately creating a BufferComparator object for each of the elements of the output tuple.
    
    Copybara import of the project:
    
    --
    f9c98f1e210df12a00bf050fd8a59bb66913f12e by Philipp Hack <phack@nvidia.com>:
    
    Adds support for tuple outputs in the convolution algorithm picker.
    
    --
    d7225e74e8f4966f963a3c9a73bad220281c88ab by Philipp Hack <phack@nvidia.com>:
    
    Adds support for tuple outputs in the convolution algorithm picker.
    
    Merging this change closes #14111
    
    PiperOrigin-RevId: 668431722

--- third_party/xla/xla/service/gpu/autotuning/conv_algorithm_picker.cc
+++ third_party/xla/xla/service/gpu/autotuning/conv_algorithm_picker.cc
@@ -97,6 +97,16 @@ using se::DeviceMemoryBase;
 using se::dnn::AlgorithmDesc;
 using std::optional;
 
+// Returns the shape of the element with index tuple_idx from shape when shape
+// is a tuple shape. Returns shape when shape is not a tuple shape.
+Shape MaybeTupleElementShape(Shape shape, int64_t tuple_idx) {
+  if (shape.IsTuple()) {
+    return shape.tuple_shapes(tuple_idx);
+  } else {
+    return shape;
+  }
+}
+
 class ScratchAllocator : public se::ScratchAllocator {
  public:
   ScratchAllocator(int device_ordinal,
@@ -735,9 +745,15 @@ absl::StatusOr<AutotuneResult> GpuConvAlgorithmPicker::AutotuneOneConvRunner(
 
     const DebugOptions& debug_options =
         runtime_arguments.hlo_module_config.debug_options();
-    BufferComparator comparator(runtime_arguments.rz_buffers.output_shape(),
-                                debug_options.xla_gpu_autotune_gemm_rtol());
     for (int i = 0; i < result_buffers.size(); ++i) {
+      // If there is one output, the output shape describes the shape of an
+      // array. If there are multiple outputs, the output shape is a tuple, in
+      // which case get the shape of the i-th element.
+      Shape output_shape = MaybeTupleElementShape(
+          runtime_arguments.rz_buffers.output_shape(), i);
+      XLA_SCOPED_LOGGING_TIMER_LEVEL("BufferComparator::CompareEqual", 2);
+      BufferComparator comparator(output_shape,
+                                  debug_options.xla_gpu_autotune_gemm_rtol());
       absl::StatusOr<bool> compare_result = comparator.CompareEqual(
           stream, (*reference_result)->buffers[i], result_buffers[i]);
       if (!compare_result.ok()) {
