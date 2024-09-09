3c6ad4555a493bdb3efbb2a07118b1fb180ebf95
A. Unique TensorFlower <gardener@tensorflow.org>
2024-09-09 14:01:11 -0700

    [XLA] Templatize minus one LiteralUtil method.
    PiperOrigin-RevId: 672665343

--- third_party/xla/xla/literal_util.cc
+++ third_party/xla/xla/literal_util.cc
@@ -116,6 +116,11 @@ struct ZeroProvider {
 };
 
 template <PrimitiveType kType>
+struct MinusOneProvider {
+  NativeT<kType> operator()() const { return static_cast<NativeT<kType>>(-1); }
+};
+
+template <PrimitiveType kType>
 struct OneProvider {
   NativeT<kType> operator()() const { return static_cast<NativeT<kType>>(1); }
 };
@@ -289,6 +294,10 @@ void SetScalarAtIndexImpl(MutableLiteralBase& literal,
 
 /* static */ Literal LiteralUtil::One(PrimitiveType primitive_type) {
   return CreateScalar<OneProvider>(primitive_type);
+}
+
+/* static */ Literal LiteralUtil::MinusOne(PrimitiveType primitive_type) {
+  return CreateScalar<MinusOneProvider>(primitive_type);
 }
 
 /* static */ Literal LiteralUtil::MinValue(PrimitiveType primitive_type) {
