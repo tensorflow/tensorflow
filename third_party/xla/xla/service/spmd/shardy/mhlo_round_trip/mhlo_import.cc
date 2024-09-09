b29f0425d750e380b8e191600c239af614017a78
A. Unique TensorFlower <gardener@tensorflow.org>
2024-09-09 04:15:16 -0700

    Support mlir::ShapedType during import
    PiperOrigin-RevId: 672494680

--- third_party/xla/xla/service/spmd/shardy/mhlo_round_trip/mhlo_import.cc
+++ third_party/xla/xla/service/spmd/shardy/mhlo_round_trip/mhlo_import.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypeInterfaces.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Diagnostics.h"
 #include "mlir/IR/MLIRContext.h"
@@ -78,7 +79,7 @@ using ::mlir::OpBuilder;
 using ::mlir::OperationPass;
 using ::mlir::Pass;
 using ::mlir::PassWrapper;
-using ::mlir::RankedTensorType;
+using ::mlir::ShapedType;
 using ::mlir::SmallVector;
 using ::mlir::StringAttr;
 using ::mlir::StringRef;
@@ -510,7 +511,7 @@ LogicalResult importShardings(
           argNum, kShardingAttr,
           convertToNewSharding(parseShardingFromString(oldSharding), globalMesh,
                                deviceIdToMaximalMeshName,
-                               mlir::cast<RankedTensorType>(argType).getRank(),
+                               mlir::cast<ShapedType>(argType).getRank(),
                                shouldOpenDims(allowPropagationToArgs, argNum)));
       funcOp.removeArgAttr(argNum, kXlaShardingAttr);
     }
@@ -524,7 +525,7 @@ LogicalResult importShardings(
           convertToNewSharding(
               parseShardingFromString(oldSharding), globalMesh,
               deviceIdToMaximalMeshName,
-              mlir::cast<RankedTensorType>(resType).getRank(),
+              mlir::cast<ShapedType>(resType).getRank(),
               shouldOpenDims(allowPropagationToResults, resNum)));
       funcOp.removeResultAttr(
           resNum, StringAttr::get(funcOp.getContext(), kXlaShardingAttr));
@@ -544,7 +545,7 @@ LogicalResult importShardings(
            llvm::zip_equal(flatHloSharding, op->getResultTypes())) {
         newShardings.push_back(convertToNewSharding(
             resHloSharding, globalMesh, deviceIdToMaximalMeshName,
-            mlir::cast<RankedTensorType>(resType).getRank(),
+            mlir::cast<ShapedType>(resType).getRank(),
             /*openDims=*/false));
       }
       op->setAttr(kShardingAttr, TensorShardingPerValueAttr::get(
