f671dfff845cae38c563dac48c3224f7b06759ef
Sergey Kozub <skozub@nvidia.com>
2024-09-10 10:46:21 -0700

    PR #17021: [NV] Use FP8 conversion intrinsics, when available
    Imported from GitHub PR https://github.com/openxla/xla/pull/17021
    
    The previous #16734 was rolled back. This PR addresses some comments from it.
    
    PTX "cvt" instruction supports converting to/from FP8 types. The NV hardware supports E4M3FN and E5M2 types. This PR updates the MLIR emitter to use this instruction instead of emitting a long sequence of operations (this matters in compute-bound FP8 kernels).
    
    The NVVM intrinsic allows converting two FP8 values with a single instruction, but as the emitter is elementwise, only one of the inputs is used. This is wasteful, but still much faster than emitting the sequence of instructions.
    
    Before ptx 7.8 (cuda 11.8), the instruction is not supported. Starting with ptx 8.1 (cuda 12.1), the instruction is supported for sm89+. Between those versions, the instruction is supported for sm90+, thas is, if trying to compile on Ada (sm89) with cuda version < 12.1, the ptxas will complain..
    
    Reference:
    https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#data-movement-and-conversion-instructions-cvt (see "PTX ISA Notes" and "Target ISA Notes").
    Copybara import of the project:
    
    --
    c7d86e0e1ee5a7e1db6c0828009c992639b48af0 by Sergey Kozub <skozub@nvidia.com>:
    
    [NV] Use FP8 conversion intrinsics, when available
    
    The previous https://github.com/openxla/xla/pull/16734 was rolled back. This PR addresses some comments from #16734.
    
    PTX "cvt" instruction supports converting to/from FP8 types. The NV hardware supports E4M3FN and E5M2 types.
    This PR updates the MLIR emitter to use this instruction instead of emitting a long sequence of operations (this matters in compute-bound FP8 kernels).
    
    The NVVM intrinsic allows converting two FP8 values with a single instruction, but as the emitter is elementwise, only one of the inputs is used. This is wasteful, but still much faster than emitting the sequence of instructions.
    
    Before ptx 7.8 (cuda 11.8), the instruction is not supported. Starting with ptx 8.1 (cuda 12.1), the instruction is supported for sm89+. Between those versions, the instruction is supported for sm90+, thas is, if trying to compile on Ada (sm89) with cuda version < 12.1, the ptxas will complain..
    
    Reference:
    https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#data-movement-and-conversion-instructions-cvt (see "PTX ISA Notes" and "Target ISA Notes").
    
    Merging this change closes #17021
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/17021 from openxla:skozub/f8-cvt-intrinsics-v2 c7d86e0e1ee5a7e1db6c0828009c992639b48af0
    PiperOrigin-RevId: 673022327

--- tensorflow/compiler/mlir/tfrt/analysis/cost_analysis.h
+++ tensorflow/compiler/mlir/tfrt/analysis/cost_analysis.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef TENSORFLOW_COMPILER_MLIR_TFRT_ANALYSIS_COST_ANALYSIS_H_
 #define TENSORFLOW_COMPILER_MLIR_TFRT_ANALYSIS_COST_ANALYSIS_H_
 
+#include <cstdint>
 #include <functional>
 
 #include "absl/strings/string_view.h"
@@ -58,6 +59,7 @@ class CostAnalysis {
   void EvaluateCost(mlir::Operation* op);
 
   int64_t max_arg_size_ = 1;
+  int64_t cost_threshold_ = -1;
   llvm::DenseMap<mlir::Operation*, int64_t> cost_map_;
   const tfrt_stub::CostRecorder* cost_recorder_;
 };
