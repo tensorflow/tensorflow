83428b7797583137121fc58d495fec7bd77c970e
Samuel Agyakwa <sagyakwa@google.com>
2024-09-10 10:12:30 -0700

    Store device manager within TFRTSession instead of graph_executor, so the device manager is created when the TFRTSession is initialized, instead of when TFRTSession is created.
    PiperOrigin-RevId: 673009525

--- tensorflow/core/tfrt/fallback/fallback_state.cc
+++ tensorflow/core/tfrt/fallback/fallback_state.cc
@@ -17,21 +17,32 @@ limitations under the License.
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <new>
 #include <utility>
+#include <variant>
 #include <vector>
 
+#include "absl/base/nullability.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "absl/strings/strip.h"
 #include "tensorflow/core/common_runtime/device_mgr.h"
+#include "tensorflow/core/common_runtime/graph_execution_state.h"
 #include "tensorflow/core/common_runtime/rendezvous_mgr.h"
 #include "tensorflow/core/framework/device_attributes.pb.h"
 #include "tensorflow/core/framework/device_factory.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/rendezvous.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/graph/types.h"
 #include "tensorflow/core/platform/strcat.h"
 #include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/public/session_options.h"
 #include "tensorflow/core/public/version.h"
 #include "tensorflow/core/tpu/virtual_device.h"
+#include "tsl/platform/errors.h"
+#include "tsl/platform/refcount.h"
 
 namespace tensorflow {
 namespace tfrt_stub {
@@ -99,29 +110,48 @@ FallbackState::CreateWithMockGpuDevice(
                                          fdef_lib);
 }
 
+absl::StatusOr<std::unique_ptr<FallbackState>>
+FallbackState::CreateWithDeviceMgr(
+    const SessionOptions &session_options,
+    const tensorflow::FunctionDefLibrary &fdef_lib,
+    absl::Nonnull<DynamicDeviceMgr *> device_mgr) {
+  return std::make_unique<FallbackState>(session_options, device_mgr, fdef_lib);
+}
+
 FallbackState::FallbackState(const SessionOptions &session_options,
-                             std::vector<std::unique_ptr<Device>> devices,
+                             std::variant<std::vector<std::unique_ptr<Device>>,
+                                          absl::Nonnull<DynamicDeviceMgr *>>
+                                 device_mgr,
                              const tensorflow::FunctionDefLibrary &fdef_lib)
     : session_options_(session_options),
-      device_manager_(std::move(devices)),
+      device_manager_(
+          std::holds_alternative<std::vector<std::unique_ptr<Device>>>(
+              device_mgr)
+              ? std::move(
+                    std::get<std::vector<std::unique_ptr<Device>>>(device_mgr))
+              : std::vector<std::unique_ptr<Device>>()),
+      device_manager_ptr_(
+          std::holds_alternative<absl::Nonnull<DynamicDeviceMgr *>>(device_mgr)
+              ? std::get<absl::Nonnull<DynamicDeviceMgr *>>(device_mgr)
+              : &device_manager_),
       func_lib_def_(OpRegistry::Global(), fdef_lib),
-      pflr_(&device_manager_, session_options.env, &session_options.config,
+      pflr_(device_manager_ptr_, session_options.env, &session_options.config,
             TF_GRAPH_DEF_VERSION, &func_lib_def_,
             session_options.config.graph_options().optimizer_options(),
             /*thread_pool=*/nullptr, /*parent=*/nullptr,
             /*session_metadata=*/nullptr,
-            Rendezvous::Factory{[](const int64, const DeviceMgr *device_mgr,
+            Rendezvous::Factory{[](const int64_t, const DeviceMgr *device_mgr,
                                    tsl::core::RefCountPtr<Rendezvous> *r) {
               *r = tsl::core::RefCountPtr<Rendezvous>(
                   new IntraProcessRendezvous(device_mgr));
               return absl::OkStatus();
             }}) {
-  for (auto *d : device_manager_.ListDevices()) {
+  for (auto *d : device_manager_ptr_->ListDevices()) {
     device_set_.AddDevice(d);
   }
 
   // client_device is the device for feed and fetch tensors.
-  device_set_.set_client_device(device_manager_.HostCPU());
+  device_set_.set_client_device(device_manager().HostCPU());
 }
 
 absl::StatusOr<std::unique_ptr<GraphExecutionState>>
@@ -143,7 +173,7 @@ FallbackState::CreateGraphExecutionState(GraphDef graph_def,
   return execution_state;
 }
 
-Status FallbackState::AddFunctionDef(const FunctionDef &func_def) {
+absl::Status FallbackState::AddFunctionDef(const FunctionDef &func_def) {
   return func_lib_def_.AddFunctionDef(func_def);
 }
 
