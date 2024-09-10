38bc10275f104de57e99716ed1b67dc522d53caf
Heiner <heiner@x.ai>
2024-09-10 02:13:54 -0700

    PR #15317: Add rendezvous timeouts as new XLA flags.
    Imported from GitHub PR https://github.com/openxla/xla/pull/15317
    
    We found the need to increase the warn-stuck timeout at @xai-org.
    Copybara import of the project:
    
    --
    261f88f6a2e5cc5f8bb5cb13c15cbaee5b837883 by Heiner <heiner@x.ai>:
    
    Add rendezvous timeouts as new XLA flags.
    
    --
    45579f82843beb1e041fd7afa049459a051926c1 by Heiner <heiner@x.ai>:
    
    Add xla.proto entry, clang-format.
    
    --
    220e85c9107af7164c2d43ac01dce9535f37902d by Heiner <heiner@x.ai>:
    
    bool -> int32.
    
    --
    83a1ddd997b9ab195c0c74ba804aeeb6dbff17d4 by Heiner <heiner@x.ai>:
    
    Add missing arg.
    
    Merging this change closes #15317
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/15317 from heiner:heiner/rendezvous-timeout a5b464d7b77639313ba5e138e754ed370686f36a
    PiperOrigin-RevId: 672866150

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -287,6 +287,9 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_cudnn_gemm_max_plans(5);
 
   opts.set_xla_gpu_enable_triton_gemm_int4(false);
+
+  opts.set_xla_gpu_executable_warn_stuck_timeout(10);
+  opts.set_xla_gpu_executable_terminate_timeout(30);
   return opts;
 }
 
@@ -1887,12 +1890,14 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       "Experimental: Enable command buffers while a profiling active. "
       "By default, enabling profiling switches from command buffers to "
       "op-by-op mode."));
+
   flag_list->push_back(tsl::Flag(
       "xla_gpu_cudnn_gemm_max_plans",
       int32_setter_for(&DebugOptions::set_xla_gpu_cudnn_gemm_max_plans),
       debug_options->xla_gpu_cudnn_gemm_max_plans(),
       "Limit for the number of kernel configurations (plans) to use during "
       "autotuning of cuDNN GEMM fusions."));
+
   flag_list->push_back(tsl::Flag(
       "xla_gpu_enable_triton_gemm_int4",
       bool_setter_for(&DebugOptions::set_xla_gpu_enable_triton_gemm_int4),
@@ -1911,6 +1916,18 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       "Option to emit a target-specific marker to indicate the start of "
       "a training. The location of the marker (if any) is determined "
       "by the option value of type DebugOptions::StepMarkerLocation."));
+
+  flag_list->push_back(
+      tsl::Flag("xla_gpu_executable_warn_stuck_timeout",
+                int32_setter_for(
+                    &DebugOptions::set_xla_gpu_executable_warn_stuck_timeout),
+                debug_options->xla_gpu_executable_warn_stuck_timeout(),
+                "Set timeout for RendezvousSingle stuck warning"));
+  flag_list->push_back(tsl::Flag(
+      "xla_gpu_executable_terminate_timeout",
+      int32_setter_for(&DebugOptions::set_xla_gpu_executable_terminate_timeout),
+      debug_options->xla_gpu_executable_terminate_timeout(),
+      "Set timeout for RendezvousSingle termination"));
 }  // NOLINT(readability/fn_size)
 
 // Allocates flag_values and flag_objects; this function must not be called more
