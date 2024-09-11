1da0958d5f0696ddb5bb4d86a4ed56e77abc5f4a
Terry Heo <terryheo@google.com>
2024-09-11 11:20:00 -0700

    Promote testing::tflite::SimpleConstTensor
    This is a struct that simplifies the creation and management of constant
    `TfLiteTensor` objects, automatically deallocating the memory (including
    dims) at destruction time.
    
    Example:
      float data[] = {2.71828f, 3.14159f};
      SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
    PiperOrigin-RevId: 673466280

--- tensorflow/lite/testing/matchers_test.cc
+++ tensorflow/lite/testing/matchers_test.cc
@@ -15,13 +15,10 @@ limitations under the License.
 
 #include "tensorflow/lite/testing/matchers.h"
 
-#include <cstring>
-#include <iostream>
 #include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include "absl/base/casts.h"
 #include "absl/types/span.h"
 #include "tensorflow/lite/core/c/c_api_types.h"
 #include "tensorflow/lite/core/c/common.h"
@@ -29,101 +26,98 @@ limitations under the License.
 namespace tflite {
 namespace {
 
-// A wrapper of TfLiteTensor that frees dims at destruction.
-struct Tensor : public TfLiteTensor {
-  template <typename T>
-  Tensor(TfLiteType dtype, const std::vector<int>& shape, absl::Span<T> buf) {
-    type = dtype;
-    dims = TfLiteIntArrayCreate(shape.size());
-    std::memcpy(dims->data, shape.data(), shape.size() * sizeof(int));
-    data = {.data = buf.data()};
-    bytes = buf.size() * sizeof(T);
-  }
-  ~Tensor() { TfLiteIntArrayFree(dims); }
-};
-
-// Delegate pretty print to PrintTo(TfLiteTensor&).
-void PrintTo(const Tensor& tensor, std::ostream* os) {  // NOLINT
-  PrintTo(absl::implicit_cast<const TfLiteTensor&>(tensor), os);
-}
-
 using ::testing::tflite::Approximately;
 using ::testing::tflite::EqualsTensor;
+using ::testing::tflite::SimpleConstTensor;
 
 TEST(TensorMatcherTest, ExactlyEqualsSelf) {
   float data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
   EXPECT_THAT(a, EqualsTensor(a));
 }
 
 TEST(TensorMatcherTest, ExactlyEqualsSame) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.71828f, 3.14159f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, EqualsTensor(b));
 }
 
 TEST(TensorMatcherTest, DoesNotExactlyEqualDifferentType) {
   float data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
-  Tensor b(TfLiteType::kTfLiteInt32, {1, 2}, absl::MakeSpan(data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
+  SimpleConstTensor b(TfLiteType::kTfLiteInt32, {1, 2}, absl::MakeSpan(data));
   EXPECT_THAT(a, Not(EqualsTensor(b)));
 }
 
 TEST(TensorMatcherTest, DoesNotExactlyEqualDifferentDims) {
   float data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
-  Tensor b(TfLiteType::kTfLiteFloat32, {2, 1}, absl::MakeSpan(data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {2, 1}, absl::MakeSpan(data));
   EXPECT_THAT(a, Not(EqualsTensor(b)));
 }
 
 TEST(TensorMatcherTest, DoesNotExactlyEqualDifferentData) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {3.14159f, 2.71828f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, Not(EqualsTensor(b)));
 }
 
 TEST(TensorMatcherTest, ApproximatelyEqualsDefaultMargin) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.718277f, 3.141593f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, Approximately(EqualsTensor(b)));
 }
 
 TEST(TensorMatcherTest, ApproximatelyEqualsWithLooseMargin) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.72f, 3.14f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, Approximately(EqualsTensor(b), /*margin=*/0.01));
 }
 
 TEST(TensorMatcherTest, DoesNotApproximatelyEqualWithTightMargin) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.72f, 3.14f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, Not(Approximately(EqualsTensor(b), /*margin=*/0.001)));
 }
 
 TEST(TensorMatcherTest, ApproximatelyEqualsWithLooseFraction) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.72f, 3.14f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(
       a, Approximately(EqualsTensor(b), /*margin=*/0.0, /*fraction=*/0.999));
 }
 
 TEST(TensorMatcherTest, DoesNotApproximatelyEqualWithTightFraction) {
   float a_data[] = {2.71828f, 3.14159f};
-  Tensor a(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(a_data));
+  SimpleConstTensor a(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(a_data));
   float b_data[] = {2.72f, 3.14f};
-  Tensor b(TfLiteType::kTfLiteFloat32, {1, 2}, absl::MakeSpan(b_data));
+  SimpleConstTensor b(TfLiteType::kTfLiteFloat32, {1, 2},
+                      absl::MakeSpan(b_data));
   EXPECT_THAT(a, Not(Approximately(EqualsTensor(b), /*margin=*/0.0,
                                    /*fraction=*/0.0001)));
 }
