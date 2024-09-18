2a1e63bdc3733b00dcd6f7692d2b8a66fb87d7a3
Alan Kelly <alankelly@google.com>
2024-09-18 06:51:05 -0700

    Update XNNPack version
    PiperOrigin-RevId: 675972497

--- tensorflow/lite/tools/cmake/modules/xnnpack.cmake
+++ tensorflow/lite/tools/cmake/modules/xnnpack.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   xnnpack
   GIT_REPOSITORY https://github.com/google/XNNPACK
   # Sync with tensorflow/workspace2.bzl
-  GIT_TAG 9ddeb74f9f6866174d61888947e4aa9ffe963b1b
+  GIT_TAG 6b83f69d4938da4dc9ad63c00bd13e9695659a51
   GIT_PROGRESS TRUE
   PREFIX "${CMAKE_BINARY_DIR}"
   SOURCE_DIR "${CMAKE_BINARY_DIR}/xnnpack"
