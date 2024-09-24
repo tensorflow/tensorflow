12236c653b6678288a2bd511c710fa913eed9117
A. Unique TensorFlower <gardener@tensorflow.org>
2024-09-24 00:42:19 -0700

    Automated Code Change
    PiperOrigin-RevId: 678116708

--- third_party/xla/xla/python/dlpack.cc
+++ third_party/xla/xla/python/dlpack.cc
@@ -54,6 +54,7 @@ limitations under the License.
 #include "xla/shape_util.h"
 #include "xla/status_macros.h"
 #include "xla/util.h"
+#include "xla/xla_data.pb.h"
 #include "tsl/platform/errors.h"
 #include "tsl/platform/logging.h"
 #include "tsl/platform/statusor.h"
