84bbb81cb98159bdeb3895628b6e063e2bf38f9d
Hyeontaek Lim <hyeontaek@google.com>
2024-09-24 22:59:59 -0700

    [IFRT] Add DeviceList::AddressableDeviceList()
    This change adds `DeviceList::AddressableDeviceList()`, which returns a
    `DeviceList` that contains only addressable devices from the original
    `DeviceList`. It returns itself if the original `DeviceList` contains only
    addressable devices.
    
    `BasicDeviceList` gets a tsan-friendly implementation of
    `AddressableDeviceList()` that lazily computes it.
    
    It also removes internal state sharing between `BasicDeviceList` objects, as
    `BasicDeviceList` is no longer copyable since `DeviceList` is wrapped with
    `tsl::RCReference<>`.
    
    PiperOrigin-RevId: 678543054

--- third_party/xla/xla/python/ifrt/device_list.cc
+++ third_party/xla/xla/python/ifrt/device_list.cc
@@ -17,16 +17,17 @@ limitations under the License.
 
 #include <atomic>
 #include <cstdint>
-#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "absl/base/call_once.h"
 #include "absl/base/optimization.h"
 #include "absl/hash/hash.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
+#include "absl/types/span.h"
 #include "xla/python/ifrt/device.h"
 #include "xla/python/ifrt/device.pb.h"
 #include "xla/tsl/concurrency/ref_count.h"
@@ -64,12 +65,32 @@ tsl::RCReference<DeviceList> BasicDeviceList::Create(Devices devices) {
   return tsl::MakeRef<BasicDeviceList>(std::move(devices));
 }
 
-BasicDeviceList::BasicDeviceList(Devices devices) : hash_(kUnsetHash) {
-  if (devices.size() <= kInlineDeviceSize) {
-    state_ = State{std::move(devices)};
-  } else {
-    state_ = std::make_shared<State>(State{std::move(devices)});
-  }
+BasicDeviceList::BasicDeviceList(Devices devices)
+    : devices_(std::move(devices)), hash_(kUnsetHash) {}
+
+DeviceList* BasicDeviceList::AddressableDeviceList() const {
+  absl::call_once(addressable_device_list_cache_.once_flag, [this] {
+    Devices addressable_devices;
+    for (Device* device : devices_) {
+      if (device->IsAddressable()) {
+        addressable_devices.push_back(device);
+      }
+    }
+    const bool already_fully_addressable =
+        addressable_devices.size() == devices_.size();
+    if (already_fully_addressable) {
+      // `device_list_holder` is intentionally unset. We skip storing a
+      // reference-counted copy in the holder to avoid creating a self cycle.
+      addressable_device_list_cache_.device_list =
+          const_cast<BasicDeviceList*>(this);
+    } else {
+      addressable_device_list_cache_.device_list_holder =
+          BasicDeviceList::Create(std::move(addressable_devices));
+      addressable_device_list_cache_.device_list =
+          addressable_device_list_cache_.device_list_holder.get();
+    }
+  });
+  return addressable_device_list_cache_.device_list;
 }
 
 uint64_t BasicDeviceList::hash() const {
@@ -86,7 +107,7 @@ uint64_t BasicDeviceList::hash() const {
 
 std::string BasicDeviceList::ToString() const {
   return absl::StrCat("BasicDeviceList([",
-                      absl::StrJoin(state().devices, ",",
+                      absl::StrJoin(devices_, ",",
                                     [](std::string* out, Device* device) {
                                       absl::StrAppend(out,
                                                       device->DebugString());
