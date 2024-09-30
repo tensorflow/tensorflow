efba8bc51e23e2c9b184edb72149bebc3291cf13
A. Unique TensorFlower <gardener@tensorflow.org>
2024-09-30 01:52:16 -0700

    Automated Code Change
    PiperOrigin-RevId: 680473564

--- tensorflow/core/common_runtime/pluggable_device/pluggable_device_context.cc
+++ tensorflow/core/common_runtime/pluggable_device/pluggable_device_context.cc
@@ -17,12 +17,14 @@ limitations under the License.
 
 #include <functional>
 
-#include "tensorflow/core/common_runtime/device.h"
+#include "absl/status/status.h"
 #include "tensorflow/core/common_runtime/device/device_event_mgr.h"
 #include "tensorflow/core/common_runtime/pluggable_device/pluggable_device_util.h"
+#include "tensorflow/core/framework/device.h"
+#include "tensorflow/core/framework/device_base.h"
 #include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/framework/types.h"
-#include "tensorflow/core/platform/stream_executor.h"
+#include "tensorflow/core/platform/status.h"
+#include "tensorflow/core/platform/stringpiece.h"
 
 namespace tensorflow {
 
