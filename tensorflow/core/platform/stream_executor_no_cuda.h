e42fe1b05991528536bcf10c7226b97ed16f8a00
Kyle Lucke <klucke@google.com>
2024-10-02 17:35:49 -0700

    Remove unneeded stream_executor/platform/dso_loader.h.
    PiperOrigin-RevId: 681654649

--- tensorflow/core/platform/stream_executor_no_cuda.h
+++ tensorflow/core/platform/stream_executor_no_cuda.h
@@ -22,7 +22,6 @@ limitations under the License.
 #include "xla/stream_executor/event.h"
 #include "xla/stream_executor/host/host_platform_id.h"
 #include "xla/stream_executor/platform.h"
-#include "xla/stream_executor/platform/dso_loader.h"
 #include "xla/stream_executor/platform_manager.h"
 #include "xla/stream_executor/rocm/rocm_platform_id.h"
 #include "xla/stream_executor/scratch_allocator.h"
