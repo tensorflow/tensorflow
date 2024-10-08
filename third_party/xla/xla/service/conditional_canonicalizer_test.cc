82d31eb88be3626038e7f4d9854f90dd54d5dccd
Sandeep Dasgupta <sdasgup@google.com>
2024-10-08 16:51:52 -0700

    [HLO Componentization] Remove unused deps of xla/tests:test_utils on passes
    PiperOrigin-RevId: 683810190

--- third_party/xla/xla/service/conditional_canonicalizer_test.cc
+++ third_party/xla/xla/service/conditional_canonicalizer_test.cc
@@ -24,7 +24,6 @@ limitations under the License.
 #include "xla/shape_util.h"
 #include "xla/tests/hlo_test_base.h"
 #include "xla/tests/literal_test_util.h"
-#include "xla/tests/test_utils.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/types.h"
 #include "xla/util.h"
--- third_party/xla/xla/service/hlo_dce_test.cc
+++ third_party/xla/xla/service/hlo_dce_test.cc
@@ -35,7 +35,6 @@ limitations under the License.
 #include "xla/shape_util.h"
 #include "xla/tests/hlo_test_base.h"
 #include "xla/tests/literal_test_util.h"
-#include "xla/tests/test_utils.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/types.h"
 #include "xla/xla_data.pb.h"
