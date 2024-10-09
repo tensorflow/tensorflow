7174bde2052fb086d57ffacbaca06c42d8d2e2bc
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-08 19:04:57 -0700

    Integrate LLVM at llvm/llvm-project@23309d7d9553
    Updates LLVM usage to match
    [23309d7d9553](https://github.com/llvm/llvm-project/commit/23309d7d9553)
    
    PiperOrigin-RevId: 683842931

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 2011  // Updated: 2024/10/10
+#define TF_GRAPH_DEF_VERSION 2012  // Updated: 2024/10/11
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
