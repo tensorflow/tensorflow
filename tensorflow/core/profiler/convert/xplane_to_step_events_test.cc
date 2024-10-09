6d3775d6b8460189b68365cd380ff6568494d997
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-08 19:07:29 -0700

    Automated Code Change
    PiperOrigin-RevId: 683843736

--- tensorflow/core/profiler/convert/xplane_to_step_events_test.cc
+++ tensorflow/core/profiler/convert/xplane_to_step_events_test.cc
@@ -20,12 +20,12 @@ limitations under the License.
 #include "absl/container/flat_hash_map.h"
 #include "xla/tsl/profiler/utils/group_events.h"
 #include "tensorflow/core/platform/test.h"
-#include "tensorflow/core/platform/types.h"
 #include "tensorflow/core/profiler/protobuf/xplane.pb.h"
 #include "tensorflow/core/profiler/utils/event_span.h"
 #include "tensorflow/core/profiler/utils/xplane_builder.h"
 #include "tensorflow/core/profiler/utils/xplane_schema.h"
 #include "tensorflow/core/profiler/utils/xplane_test_utils.h"
+#include "tsl/profiler/protobuf/xplane.pb.h"
 
 namespace tensorflow {
 namespace profiler {
