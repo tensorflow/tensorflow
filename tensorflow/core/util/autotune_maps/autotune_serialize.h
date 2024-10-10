325db49ab64b3a64041d508178148ecbb8111c63
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-09 20:13:05 -0700

    Automated Code Change
    PiperOrigin-RevId: 684273864

--- tensorflow/core/util/autotune_maps/autotune_serialize.h
+++ tensorflow/core/util/autotune_maps/autotune_serialize.h
@@ -34,11 +34,11 @@ namespace tensorflow {
 // TODO(b/189530096) Support autotune maps for more ops.
 // Loads autotune maps from string output by SerializeAutotuneMaps and uses
 // them to update the runtime autotune maps.
-Status LoadSerializedAutotuneMaps(absl::string_view s);
+absl::Status LoadSerializedAutotuneMaps(absl::string_view s);
 
 // Serializes all the autotune maps into a string that can be decoded by
 // LoadSerializedAutotuneMaps.
-Status SerializeAutotuneMaps(std::string* output);
+absl::Status SerializeAutotuneMaps(std::string* output);
 
 // Resets all autotune maps. For test use only.
 void ResetAutotuneMaps();
