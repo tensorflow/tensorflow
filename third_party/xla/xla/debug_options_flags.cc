2df917530503ab915756711ea1c5957f63755fa0
Philipp Hack <phack@nvidia.com>
2024-10-10 16:22:15 -0700

    PR #17950: Remove Post Layout Assignment Collective Pipeliner
    Imported from GitHub PR https://github.com/openxla/xla/pull/17950
    
    Removes the functionality to optionally run the collective pipeliner pass post layout assignment. This option was introduced in #12866 to preserve FP8 quantization and dequantization patterns and was obsoleted by the reordering of the collective optimization passes in #17453.
    Copybara import of the project:
    
    --
    e9e9c1af754b0391d72277de27681bed6c5fa37a by Philipp Hack <phack@nvidia.com>:
    
    Removes the option to run the collective pipeliner post layout assignment.
    
    --
    c5489ac962af27ac3b13e3ebc96bc6b7a68cc63e by Philipp Hack <phack@nvidia.com>:
    
    Removes the option to run the collective pipeliner post layout assignment.
    
    Merging this change closes #17950
    
    PiperOrigin-RevId: 684614799

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -175,8 +175,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_enable_pipelined_reduce_scatter(true);
   opts.set_xla_gpu_enable_pipelined_p2p(false);
 
-  opts.set_xla_gpu_run_post_layout_collective_pipeliner(false);
-
   opts.set_xla_gpu_collective_permute_decomposer_threshold(
       std::numeric_limits<int64_t>::max());
 
@@ -1559,12 +1557,6 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       bool_setter_for(&DebugOptions::set_xla_gpu_enable_pipelined_p2p),
       debug_options->xla_gpu_enable_pipelined_p2p(),
       "Enable pipelinling of P2P instructions."));
-  flag_list->push_back(tsl::Flag(
-      "xla_gpu_run_post_layout_collective_pipeliner",
-      bool_setter_for(
-          &DebugOptions::set_xla_gpu_run_post_layout_collective_pipeliner),
-      debug_options->xla_gpu_run_post_layout_collective_pipeliner(),
-      "Move collective pipeliner after the post-layout optimization."));
   flag_list->push_back(tsl::Flag(
       "xla_gpu_collective_permute_decomposer_threshold",
       int64_setter_for(
