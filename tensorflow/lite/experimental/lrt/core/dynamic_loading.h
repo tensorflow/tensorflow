f57c8da6e2d51dcde45be6da49c3ab3be529b7fd
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-09 20:03:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 684270800

--- tensorflow/lite/experimental/lrt/core/dynamic_loading.h
+++ tensorflow/lite/experimental/lrt/core/dynamic_loading.h
@@ -18,6 +18,9 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 
+#include <iostream>
+#include <ostream>
+
 #include "absl/strings/string_view.h"
 #include "tensorflow/lite/experimental/lrt/c/lite_rt_common.h"
 #include "tensorflow/lite/experimental/lrt/core/logging.h"
@@ -28,7 +31,7 @@ namespace lrt {
 LrtStatus OpenLib(absl::string_view so_path, void** lib_handle);
 
 // Dumps loading details of given lib handle.
-void DumpLibInfo(void* lib_handle);
+void DumpLibInfo(void* lib_handle, std::ostream& out = std::cerr);
 
 // Resolves a named symbol from given lib handle of type Sym.
 template <class Sym>
