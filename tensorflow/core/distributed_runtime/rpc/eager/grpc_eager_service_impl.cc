56754650bd7b5c02aa770b79f6709b388d869397
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-09 19:32:50 -0700

    Automated Code Change
    PiperOrigin-RevId: 684259752

--- tensorflow/core/distributed_runtime/rpc/eager/grpc_eager_service_impl.cc
+++ tensorflow/core/distributed_runtime/rpc/eager/grpc_eager_service_impl.cc
@@ -42,7 +42,7 @@ GrpcEagerServiceImpl::GrpcEagerServiceImpl(
   cq_ = server_builder->AddCompletionQueue();
 }
 
-Status GrpcEagerServiceImpl::CreateMasterContext(
+absl::Status GrpcEagerServiceImpl::CreateMasterContext(
     const tensorflow::uint64 context_id, EagerContext* context) {
   return local_impl_.CreateMasterContext(context_id, context);
 }
