ffda817dbda97b9ab80ed8744d1b9a2a2db0132e
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-09 17:01:36 -0700

    Add envvar **MLIR_BRIDGE_LOG_ENABLE_ONLY_TOP_LEVEL_PASSES**, which only logs passes that are only applied towards top-level operations (i.e., operations with no parent), unless explicitly disabled. This is to reduce the amount of MLIR-based TF2XLA bridge dumps during training jobs.
    PiperOrigin-RevId: 684220556

--- tensorflow/compiler/mlir/tensorflow/transforms/host_runtime/lower_cluster_to_runtime_ops_test.cc
+++ tensorflow/compiler/mlir/tensorflow/transforms/host_runtime/lower_cluster_to_runtime_ops_test.cc
@@ -74,7 +74,8 @@ class LowerClusterToRuntimeOpsTest : public ::testing::Test {
     env_ = Env::Default();
     test_group_name_ = "TestGroup";
     test_dir_ = testing::TmpDir();
-    setenv("TF_DUMP_GRAPH_PREFIX", test_dir_.c_str(), /*overwrite=*/1);
+    setenv(/*name=*/"TF_DUMP_GRAPH_PREFIX", /*value=*/test_dir_.c_str(),
+           /*overwrite=*/1);
   }
 
   absl::Status CreateMlirModule(std::string mlir_module_filename) {
@@ -179,8 +180,9 @@ TEST_F(LowerClusterToRuntimeOpsTest, DumpsPipelinePasses) {
   std::vector<std::string> files;
   TF_ASSERT_OK(env_->GetChildren(test_dir_, &files));
   EXPECT_THAT(files, ::testing::IsEmpty());
-  setenv("TF_DUMP_GRAPH_NAME_FILTER", "*", /*overwrite=*/1);
-  setenv("TF_DUMP_GRAPH_GROUPS", "main,runtime_lowering", /*overwrite=*/1);
+  setenv(/*name=*/"TF_DUMP_GRAPH_NAME_FILTER", /*value=*/"*", /*overwrite=*/1);
+  setenv(/*name=*/"TF_DUMP_GRAPH_GROUPS", /*value=*/"main,runtime_lowering",
+         /*overwrite=*/1);
   DEBUG_DATA_DUMPER()->LoadEnvvars();
 
   TF_ASSERT_OK(CreateMlirModule("basic_cluster.mlir"));
