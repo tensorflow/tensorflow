c22d2cbd0886be2a9d46468e45b5845d9088bb87
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-14 13:46:25 -0700

    Automated Code Change.
    PiperOrigin-RevId: 685822113

--- third_party/xla/xla/service/gpu/nvptx_compiler_test.cc
+++ third_party/xla/xla/service/gpu/nvptx_compiler_test.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "xla/hlo/analysis/hlo_ordering.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/hlo/utils/hlo_query.h"
@@ -30,7 +31,6 @@ limitations under the License.
 #include "xla/service/gpu/gpu_constants.h"
 #include "xla/service/gpu/gpu_hlo_schedule.h"
 #include "xla/service/gpu/gpu_latency_hiding_scheduler.h"
-#include "xla/service/hlo_ordering.h"
 #include "xla/service/logical_buffer.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/tests/hlo_test_base.h"
