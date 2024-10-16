ca2e8c5842993536da3138f7d077e48c490716bf
Eugene Zhulenev <ezhulenev@google.com>
2024-10-16 12:09:01 -0700

    [hlo] Do not overwrite derived instruction backend config
    Derived instruction might set its own backend config, and it's not safe to overwrite it with an original one.
    
    Fix for: https://github.com/openxla/xla/issues/18214
    
    PiperOrigin-RevId: 686594662

--- third_party/xla/xla/hlo/ir/hlo_instruction.cc
+++ third_party/xla/xla/hlo/ir/hlo_instruction.cc
@@ -2216,9 +2216,11 @@ void HloInstruction::SetupDerivedInstruction(
     derived_instruction->mutable_rare()->frontend_attributes.Clear();
     derived_instruction->mutable_rare()->statistics_viz.Clear();
   }
-  // If the derived instruction has the same opcode as current,
-  // then the backend config is also applicable.
-  if (opcode() == derived_instruction->opcode() && has_backend_config()) {
+  // If the derived instruction has the same opcode as current, then the backend
+  // config is also applicable (only if derived instruction doesn't have its own
+  // backend config which might be different from the original one).
+  if (opcode() == derived_instruction->opcode() && has_backend_config() &&
+      !derived_instruction->has_backend_config()) {
     derived_instruction->CopyBackendConfigFrom(this);
   }
 }
