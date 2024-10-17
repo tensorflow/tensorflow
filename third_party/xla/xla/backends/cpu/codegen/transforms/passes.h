8cd003da83987584924fb798598038c709fb80f1
Eugene Zhulenev <ezhulenev@google.com>
2024-10-17 08:19:52 -0700

    [xla:cpu] Add basic lowering from xla_cpu to LLVM
    PiperOrigin-RevId: 686921419

---
+++ third_party/xla/xla/backends/cpu/codegen/transforms/passes.h
@@ -0,0 +1,35 @@
+/* Copyright 2024 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_BACKENDS_CPU_CODEGEN_TRANSFORMS_PASSES_H_
+#define XLA_BACKENDS_CPU_CODEGEN_TRANSFORMS_PASSES_H_
+
+#include <memory>
+
+#include "mlir/Pass/Pass.h"
+
+namespace xla::cpu {
+
+#define GEN_PASS_DECL
+#include "xla/backends/cpu/codegen/transforms/passes.h.inc"
+
+std::unique_ptr<mlir::Pass> CreateLowerTrivialPass();
+
+#define GEN_PASS_REGISTRATION
+#include "xla/backends/cpu/codegen/transforms/passes.h.inc"
+
+}  // namespace xla::cpu
+
+#endif  // XLA_BACKENDS_CPU_CODEGEN_TRANSFORMS_PASSES_H_
