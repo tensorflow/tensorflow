d1486efb223208c0f11c2f1c889d33916f9f34eb
mmakevic-amd <Milica.Makevic@amd.com>
2024-10-17 02:39:05 -0700

    PR #18387: [ROCm] Disable failing subtests for command_buffer_scheduling_test
    Imported from GitHub PR https://github.com/openxla/xla/pull/18387
    
    Disabling `Conditional` and `While` subtests in `command_buffer_scheduling_test`  since ROCm does not support conditional nodes in the hip graph
    Copybara import of the project:
    
    --
    ba4396eee2410a1cc0846ee6eac0c401ba984afe by Milica Makevic <Milica.Makevic@amd.com>:
    
    Disable Conditional and While subtests for ROCm
    
    Merging this change closes #18387
    
    PiperOrigin-RevId: 686834433

--- third_party/xla/xla/service/gpu/transforms/command_buffer_scheduling_test.cc
+++ third_party/xla/xla/service/gpu/transforms/command_buffer_scheduling_test.cc
@@ -54,6 +54,13 @@ class CommandBufferSchedulingTest : public HloTestBase {
     debug_options.set_xla_gpu_graph_min_graph_size(2);
     return debug_options;
   }
+
+  const se::GpuComputeCapability& GetGpuComputeCapability() {
+    return backend()
+        .default_stream_executor()
+        ->GetDeviceDescription()
+        .gpu_compute_capability();
+  }
 };
 
 using CommandBuffer = CommandBufferScheduling::CommandBuffer;
@@ -809,6 +816,10 @@ TEST_F(CommandBufferSchedulingTest, WhileNotCommand) {
 }
 
 TEST_F(CommandBufferSchedulingTest, While) {
+  const auto& gpu_desc = GetGpuComputeCapability();
+  if (std::holds_alternative<se::RocmComputeCapability>(gpu_desc)) {
+    GTEST_SKIP() << "Not supported for ROCm!";
+  }
   const char* hlo = R"(
     HloModule TestModule, is_scheduled=true
 
@@ -870,6 +881,10 @@ TEST_F(CommandBufferSchedulingTest, While) {
 }
 
 TEST_F(CommandBufferSchedulingTest, Conditional) {
+  const auto& gpu_desc = GetGpuComputeCapability();
+  if (std::holds_alternative<se::RocmComputeCapability>(gpu_desc)) {
+    GTEST_SKIP() << "Not supported for ROCm!";
+  }
   const char* hlo = R"(
     HloModule TestModule, is_scheduled=true
 
