d31b5a1bb5eb2e7f52db9f85d7f5a1b78a9d08d5
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-20 20:15:32 -0700

    Automated Code Change
    PiperOrigin-RevId: 687970842

--- tensorflow/cc/saved_model/loader_util.cc
+++ tensorflow/cc/saved_model/loader_util.cc
@@ -28,8 +28,9 @@ namespace internal {
 // A SavedModel may store the name of the initialization op to run in the
 // in the SignatureDef (v2) or a collection (v1). If an init_op collection
 // exists, then the collection must contain exactly one op.
-Status GetInitOp(const string& export_dir, const MetaGraphDef& meta_graph_def,
-                 string* init_op_name) {
+absl::Status GetInitOp(const string& export_dir,
+                       const MetaGraphDef& meta_graph_def,
+                       string* init_op_name) {
   const auto& sig_def_map = meta_graph_def.signature_def();
   const auto& init_op_sig_it =
       meta_graph_def.signature_def().find(kSavedModelInitOpSignatureKey);
@@ -65,8 +66,8 @@ Status GetInitOp(const string& export_dir, const MetaGraphDef& meta_graph_def,
   return absl::OkStatus();
 }
 
-Status GetAssetFileDefs(const MetaGraphDef& meta_graph_def,
-                        std::vector<AssetFileDef>* asset_file_defs) {
+absl::Status GetAssetFileDefs(const MetaGraphDef& meta_graph_def,
+                              std::vector<AssetFileDef>* asset_file_defs) {
   // With SavedModel v2, we write asset file def into metagraph instead of
   // collection, so read from metagraph first.
   if (meta_graph_def.asset_file_def_size() > 0) {
