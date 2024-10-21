5ec263fa2ab9b9547f1725d78d4909c3e9a25cc9
akhilgoe <114951738+akhilgoe@users.noreply.github.com>
2024-10-21 02:20:26 -0700

    PR #18527: [XLA:CPU][oneDNN] Refactor population of oneDNN post-ops
    Imported from GitHub PR https://github.com/openxla/xla/pull/18527
    
    This PR refactors the code that populates post-ops for oneDNN primitive and moves it to onednn_util so that it can be re-used by other primitive kinds in the future.
    Copybara import of the project:
    
    --
    69e22d1bbc5ff8791d914f5bc69e2cb86ffdeaeb by Akhil Goel <akhil.goel@intel.com>:
    
    Refactor post-op population
    
    Merging this change closes #18527
    
    PiperOrigin-RevId: 688049332

--- third_party/xla/xla/service/cpu/onednn_util.h
+++ third_party/xla/xla/service/cpu/onednn_util.h
@@ -52,6 +52,11 @@ inline bool IsSupportedType(xla::PrimitiveType dtype) {
   return false;
 }
 
+struct FusedOperandsRef {
+  const std::vector<void*>& bufs;
+  std::vector<std::pair<int, dnnl::memory>>& postop_args;
+};
+
 std::unique_ptr<tsl::OneDnnThreadPool> CreateOneDnnThreadPool(
     const Eigen::ThreadPoolDevice* threadpool_device);
 
@@ -72,6 +77,13 @@ struct PrimitiveTrait;
 template <BackendConfigOneofCase config>
 typename PrimitiveTrait<config>::pointer_type GetKernelConfig(
     absl::StatusOr<BackendConfig>*);
+
+dnnl::post_ops PopulateOneDnnPostOps(
+    const dnnl::engine& cpu_engine,
+    const std::vector<dnnl::memory::desc>& fused_mds,
+    const OneDnnFusionConfig* fusion_config, const int output_ndims,
+    FusedOperandsRef* fused_operands_ref = nullptr,
+    dnnl::memory::desc* bias_md = nullptr);
 
 }  // namespace cpu
 }  // namespace xla
