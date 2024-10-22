9230f62006bc32700a7795658a1e9cda3250e102
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-22 01:51:57 -0700

    Automated Code Change
    PiperOrigin-RevId: 688452513

--- third_party/xla/xla/pjrt/c/pjrt_c_api_helpers.h
+++ third_party/xla/xla/pjrt/c/pjrt_c_api_helpers.h
@@ -26,6 +26,7 @@ limitations under the License.
 #include "absl/base/attributes.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "xla/layout.h"
