b68aaec36dacb8244d1499a713f041d395902865
Ilia Sergachev <isergachev@nvidia.com>
2024-10-22 06:15:16 -0700

    PR #18562: Compute cost tool: add an option to print costs of each instruction.
    Imported from GitHub PR https://github.com/openxla/xla/pull/18562
    
    Copybara import of the project:
    
    --
    314f9646442329c3cf99b5b339e81866d37d241c by Ilia Sergachev <isergachev@nvidia.com>:
    
    Compute cost tool: add an option to print costs of each instruction.
    
    --
    4e5ea3ba30209e76d3ffd09a3d70dcc526200552 by Ilia Sergachev <isergachev@nvidia.com>:
    
    address review comments
    
    Merging this change closes #18562
    
    PiperOrigin-RevId: 688522322

--- third_party/xla/xla/tools/compute_cost.cc
+++ third_party/xla/xla/tools/compute_cost.cc
@@ -22,13 +22,16 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
 #include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 #include "xla/debug_options_flags.h"
+#include "xla/hlo/ir/hlo_computation.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/gpu/model/gpu_hlo_cost_analysis.h"
 #include "xla/service/hlo_cost_analysis.h"
-#include "xla/shape.h"
-#include "xla/shape_util.h"
 #include "xla/tools/hlo_module_loader.h"
 #include "xla/tsl/util/command_line_flags.h"
 #include "tsl/platform/init_main.h"
@@ -43,18 +46,50 @@ The input file can be obtained from XProf graph viewer by clicking
 
 Usage:
 
-  bazel run compute_cost -- --input=path/to/hlo_module --format=[hlo|pb|pbtxt] [--gpu]
+  bazel run compute_cost -- --input=path/to/hlo_module --format=[hlo|pb|pbtxt] [--gpu] [--all]
 )";
 }  // namespace
+
+namespace xla {
+void print_costs_of_all_instructions(const HloModule& module,
+                                     const HloCostAnalysis& analysis) {
+  absl::flat_hash_map<std::string, std::string> fingerprint_to_name;
+  std::cout << "HLO name, deduplicated name, bytes accessed, flops\n";
+  for (const HloComputation* computation : module.computations()) {
+    for (const HloInstruction* hlo : computation->instructions()) {
+      if (hlo->opcode() == HloOpcode::kParameter ||
+          hlo->opcode() == HloOpcode::kConstant ||
+          hlo->opcode() == HloOpcode::kTuple ||
+          hlo->opcode() == HloOpcode::kGetTupleElement ||
+          hlo->opcode() == HloOpcode::kBitcast) {
+        // These instructions always have zero costs.
+        continue;
+      }
+      absl::string_view deduplicated_name = hlo->metadata().deduplicated_name();
+      if (deduplicated_name.empty()) {
+        deduplicated_name = hlo->name();
+      }
+      std::cout << hlo->name() << ", " << deduplicated_name << ", "
+                << analysis.bytes_accessed(*hlo) << ", "
+                << analysis.flop_count(*hlo) << "\n";
+    }
+  }
+}
+}  // namespace xla
 
 int main(int argc, char** argv) {
   std::string input, format;
   bool gpu = false;
+  bool all = false;
   std::vector<tsl::Flag> flag_list = {
       tsl::Flag("input", &input, "input file"),
       tsl::Flag("format", &format, "hlo|pb|pbtxt"),
       tsl::Flag("gpu", &gpu,
-                "Use GPU flavor of cost analysis instead of the generic one")};
+                "Use GPU flavor of cost analysis instead of the generic one"),
+      tsl::Flag(
+          "all", &all,
+          "Also print costs and deduplicated name of each instruction, not "
+          "just the total costs for the module")};
   xla::AppendDebugOptionsFlags(&flag_list);
   const std::string kUsageString =
       absl::StrCat(kUsage, "\n\n", tsl::Flags::Usage(argv[0], flag_list));
@@ -72,14 +107,19 @@ int main(int argc, char** argv) {
     analysis = std::make_unique<xla::HloCostAnalysis>();
   }
 
-  TF_CHECK_OK(xla::LoadModuleFromFile(input, format, {})
-                  .value()
-                  ->entry_computation()
-                  ->root_instruction()
-                  ->Accept(&*analysis));
+  std::unique_ptr<xla::HloModule> module =
+      *xla::LoadModuleFromFile(input, format, {});
+
+  TF_CHECK_OK(
+      module->entry_computation()->root_instruction()->Accept(&*analysis));
+
+  if (all) {
+    print_costs_of_all_instructions(*module, *analysis);
+  }
 
   std::cout << std::setw(5) << std::setprecision(4)
-            << analysis->flop_count() / (1e9) << " GFLOPS. "
+            << "Total: " << analysis->flop_count() / (1e9) << " GFLOPS. "
             << analysis->bytes_accessed() / (1e6) << " MB." << std::endl;
+
   return 0;
 }
