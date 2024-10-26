29fd7423fbafe7a17b4de16f3dd79760c1623a82
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-25 20:40:31 -0700

    Automated Code Change
    PiperOrigin-RevId: 690003843

--- tensorflow/c/experimental/saved_model/internal/saved_model_api.cc
+++ tensorflow/c/experimental/saved_model/internal/saved_model_api.cc
@@ -98,7 +98,7 @@ TF_ConcreteFunction* TF_GetSavedModelConcreteFunction(TF_SavedModel* model,
                                                       const char* function_path,
                                                       TF_Status* status) {
   tensorflow::ConcreteFunction* result = nullptr;
-  tensorflow::Status get_function_status =
+  absl::Status get_function_status =
       tensorflow::unwrap(model)->GetFunction(function_path, &result);
   status->status.Update(get_function_status);
   if (!get_function_status.ok()) {
@@ -112,7 +112,7 @@ TF_GetSavedModelSignatureDefFunction(TF_SavedModel* model,
                                      const char* signature_def_key,
                                      TF_Status* status) {
   tensorflow::SignatureDefFunction* result = nullptr;
-  tensorflow::Status get_function_status =
+  absl::Status get_function_status =
       tensorflow::unwrap(model)->GetSignatureDefFunction(signature_def_key,
                                                          &result);
   status->status.Update(get_function_status);
