df47700e336d3b5e13100cd6ca2d5df386e55450
Peter Hawkins <phawkins@google.com>
2024-10-28 09:28:05 -0700

    [XLA] Speed up the HLO DCE pass.
    As in HloModule::RemoveUnusedComputations, we can simply perform a DFS over the computations both to find used computations and to trigger RunOnComputation on any used computations. We then remove all unreachable computations.
    
    This improves over the existing code because we avoid repeatedly calling HloModule::MakeComputationPostOrder(), each call to which requires traversing every instruction in every computation. Instead, we can perform a single traversal of the computations.
    
    PiperOrigin-RevId: 690640585

--- third_party/xla/xla/hlo/transforms/simplifiers/hlo_dce.h
+++ third_party/xla/xla/hlo/transforms/simplifiers/hlo_dce.h
@@ -16,9 +16,7 @@ limitations under the License.
 #ifndef XLA_HLO_TRANSFORMS_SIMPLIFIERS_HLO_DCE_H_
 #define XLA_HLO_TRANSFORMS_SIMPLIFIERS_HLO_DCE_H_
 
-#include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_computation.h"
@@ -58,18 +56,6 @@ class HloDCE : public HloModulePass {
       const absl::flat_hash_set<absl::string_view>& execution_threads) override;
 
  private:
-  // Finds all computations that are not called by any instruction and removes
-  // them from the module. Returns whether any dead code was removed.
-  absl::StatusOr<bool> RecursivelyRemoveDeadComputations(HloModule* module);
-
-  // Given a dead computation, decrements the ref count of all its called
-  // computations and checks if any of the subcomputations become dead after the
-  // removal. Returns whether all dead computations were successfully removed
-  // from the module.
-  absl::Status RecursivelyRemoveDeadComputation(
-      HloModule* module, HloComputation* computation,
-      absl::flat_hash_map<HloComputation*, int>& live_call_counts);
-
   bool remove_cross_partition_collective_ops_;
 };
 
