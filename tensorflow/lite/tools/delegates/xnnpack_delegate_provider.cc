3a89f661c56c19fee682e341e5472c4597af2686
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-29 00:18:28 -0700

    Automated Code Change
    PiperOrigin-RevId: 690902103

--- tensorflow/lite/tools/delegates/xnnpack_delegate_provider.cc
+++ tensorflow/lite/tools/delegates/xnnpack_delegate_provider.cc
@@ -12,8 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <cstdint>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "tensorflow/lite/tools/delegates/delegate_provider.h"
 #include "tensorflow/lite/tools/evaluation/utils.h"
