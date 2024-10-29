c2c72daad7c49c87da7a81390b6a63daca1317de
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-28 23:48:52 -0700

    Automated Code Change
    PiperOrigin-RevId: 690894698

--- tensorflow/lite/experimental/resource/initialization_status.h
+++ tensorflow/lite/experimental/resource/initialization_status.h
@@ -15,6 +15,8 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_EXPERIMENTAL_RESOURCE_INITIALIZATION_STATUS_H_
 #define TENSORFLOW_LITE_EXPERIMENTAL_RESOURCE_INITIALIZATION_STATUS_H_
 
+#include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <unordered_map>
 
