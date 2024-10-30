9f661a5f215d10e79896cdfddc9486852b814106
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-30 03:16:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 691351485

--- tensorflow/python/framework/experimental/tape.cc
+++ tensorflow/python/framework/experimental/tape.cc
@@ -27,7 +27,7 @@ namespace py = pybind11;
 namespace tensorflow {
 namespace gradients {
 
-Status RegisterGradients(GradientRegistry* registry) {
+absl::Status RegisterGradients(GradientRegistry* registry) {
   // TODO(srbs): Rename ops::Add and AddRegisterer to AddV2.
   TF_RETURN_IF_ERROR(registry->Register("AddV2", AddRegisterer));
   TF_RETURN_IF_ERROR(registry->Register("Exp", ExpRegisterer));
@@ -54,9 +54,9 @@ PYBIND11_MODULE(_tape, m) {
               std::vector<AbstractTensorHandle*> source_tensors,
               std::vector<AbstractTensorHandle*> output_gradients) {
              std::vector<AbstractTensorHandle*> results(source_tensors.size());
-             Status s = self->ComputeGradient(ctx, target_tensors,
-                                              source_tensors, output_gradients,
-                                              absl::MakeSpan(results));
+             absl::Status s = self->ComputeGradient(
+                 ctx, target_tensors, source_tensors, output_gradients,
+                 absl::MakeSpan(results));
              MaybeRaiseRegisteredFromStatus(s);
              return results;
            });
