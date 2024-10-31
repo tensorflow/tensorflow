cf0a82a12cdb2b3a611d442ed528ef7203659f63
SMIT PATEL <147633120+smit23patel@users.noreply.github.com>
2024-10-31 09:48:10 +0530

    Update c_test.c
    1. Added error handling for the creation of TF_Status to ensure it is not NULL.
    
    2. Initialized input to NULL before using it in TF_GetInput.
    
    3.Added error handling for TF_NewWritableFile to check if status is NULL.
    
    4.Ensured proper cleanup of allocated resources in case of errors
    
    5.Added a check for malloc failure for full_path.
    
    6.Ensured that TF_DeleteWritableFileHandle is called only if h is not NULL to avoid dereferencing a NULL pointer.

--- tensorflow/c/c_test.c
+++ tensorflow/c/c_test.c
@@ -1,93 +1,116 @@
-/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+  /* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ==============================================================================*/
 
-#include <limits.h>
-#include <memory.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
+  #include <limits.h>
+  #include <memory.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <time.h>
+  #include <unistd.h>
 
-#ifdef _WIN32
-#include <process.h>
-#endif
+  #ifdef _WIN32
+  #include <process.h>
+  #endif
 
-#include "tensorflow/c/c_api.h"
-#include "tensorflow/c/c_api_experimental.h"
-#include "tensorflow/c/env.h"
-#include "tensorflow/c/kernels.h"
+  #include "tensorflow/c/c_api.h"
+  #include "tensorflow/c/c_api_experimental.h"
+  #include "tensorflow/c/env.h"
+  #include "tensorflow/c/kernels.h"
 
-// A create function. This will never actually get called in this test, it's
-// just nice to know that it compiles.
-void* create(TF_OpKernelConstruction* ctx) {
-  TF_DataType type;
-  TF_Status* s = TF_NewStatus();
-  TF_OpKernelConstruction_GetAttrType(ctx, "foobar", &type, s);
-  TF_DeleteStatus(s);
-  return NULL;
-}
-
-// A compute function. This will never actually get called in this test, it's
-// just nice to know that it compiles.
-void compute(void* kernel, TF_OpKernelContext* ctx) {
-  TF_Tensor* input;
-  TF_Status* s = TF_NewStatus();
-  TF_GetInput(ctx, 0, &input, s);
-  TF_DeleteTensor(input);
-  TF_DeleteStatus(s);
-}
-
-// Exercises tensorflow's C API.
-int main(int argc, char** argv) {
-  TF_InitMain(argv[0], &argc, &argv);
-
-  struct TF_StringStream* s = TF_GetLocalTempDirectories();
-  const char* path;
-
-  if (!TF_StringStreamNext(s, &path)) {
-    fprintf(stderr, "TF_GetLocalTempDirectories returned no results\n");
-    return 1;
+  // A create function. This will never actually get called in this test, it's
+  // just nice to know that it compiles.
+  void* create(TF_OpKernelConstruction* ctx) {
+    TF_DataType type;
+    TF_Status* s = TF_NewStatus();
+    if (s == NULL) {
+      fprintf(stderr, "Failed to create TF_Status\n");
+      return NULL; // Early return on error
+    }
+    TF_OpKernelConstruction_GetAttrType(ctx, "foobar", &type, s);
+    TF_DeleteStatus(s);
+    return NULL;
   }
 
-  char file_name[100];
-  time_t t = time(NULL);
-  snprintf(file_name, sizeof(file_name), "test-%d-%ld.txt", getpid(), t);
-
-  size_t length = 2 + strlen(path) + strlen(file_name);
-  char* full_path = malloc(length);
-  snprintf(full_path, length, "%s/%s", path, file_name);
-
-  TF_WritableFileHandle* h;
-  TF_Status* status = TF_NewStatus();
-  TF_NewWritableFile(full_path, &h, status);
-  if (TF_GetCode(status) != TF_OK) {
-    fprintf(stderr, "TF_NewWritableFile failed: %s\n", TF_Message(status));
-    return 1;
+  // A compute function. This will never actually get called in this test, it's
+  // just nice to know that it compiles.
+  void compute(void* kernel, TF_OpKernelContext* ctx) {
+    TF_Tensor* input = NULL;
+    TF_Status* s = TF_NewStatus();
+    TF_GetInput(ctx, 0, &input, s);
+    if (TF_GetCode(s) != TF_OK) {
+      fprintf(stderr, "TF_GetInput failed: %s\n", TF_Message(s));
+      TF_DeleteStatus(s);
+      return; // Early return on error
+    }
+    TF_DeleteTensor(input);
+    TF_DeleteStatus(s);
   }
-  fprintf(stderr, "wrote %s\n", full_path);
-  free(full_path);
-  TF_CloseWritableFile(h, status);
-  if (TF_GetCode(status) != TF_OK) {
-    fprintf(stderr, "TF_CloseWritableFile failed: %s\n", TF_Message(status));
+
+  // Exercises tensorflow's C API.
+  int main(int argc, char** argv) {
+    TF_InitMain(argv[0], &argc, &argv);
+
+    struct TF_StringStream* s = TF_GetLocalTempDirectories();
+    const char* path;
+
+    if (!TF_StringStreamNext(s, &path)) {
+      fprintf(stderr, "TF_GetLocalTempDirectories returned no results\n");
+      return 1;
+    }
+
+    char file_name[100];
+    time_t t = time(NULL);
+    snprintf(file_name, sizeof(file_name), "test-%d-%ld.txt", getpid(), t);
+
+    size_t length = 2 + strlen(path) + strlen(file_name);
+    char* full_path = malloc(length);
+    if (full_path == NULL) {
+      fprintf(stderr, "Failed to allocate memory for full_path\n");
+      free(full_path);
+      return 1;
+    }
+    snprintf(full_path, length, "%s/%s", path, file_name);
+
+    TF_WritableFileHandle* h;
+    TF_Status* status = TF_NewStatus();
+    if (status == NULL) {
+      fprintf(stderr, "Failed to create TF_Status\n");
+      free(full_path);
+      return 1; // Early return on error
+    }
+    TF_NewWritableFile(full_path, &h, status);
+    if (TF_GetCode(status) != TF_OK) {
+      fprintf(stderr, "TF_NewWritableFile failed: %s\n", TF_Message(status));
+      free(full_path);
+      return 1;
+    }
+    fprintf(stderr, "wrote %s\n", full_path);
+    free(full_path);
+    TF_CloseWritableFile(h, status);
+    if (TF_GetCode(status) != TF_OK) {
+      fprintf(stderr, "TF_CloseWritableFile failed: %s\n", TF_Message(status));
+    }
+    if (h != NULL) {
+      TF_DeleteWritableFileHandle(h);
+    }
+    TF_StringStreamDone(s);
+
+    TF_KernelBuilder* b =
+        TF_NewKernelBuilder("SomeOp", "SomeDevice", &create, &compute, NULL);
+    TF_RegisterKernelBuilder("someKernel", b, status);
+
+    TF_DeleteStatus(status);
+    return 0;
   }
-  TF_StringStreamDone(s);
-
-  TF_KernelBuilder* b =
-      TF_NewKernelBuilder("SomeOp", "SomeDevice", &create, &compute, NULL);
-  TF_RegisterKernelBuilder("someKernel", b, status);
-
-  TF_DeleteStatus(status);
-  return 0;
-}
