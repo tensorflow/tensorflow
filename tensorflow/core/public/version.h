e01de691f8e57fd1cedde43687d626fb18847165
A. Unique TensorFlower <gardener@tensorflow.org>
2024-10-31 21:22:45 -0700

    compat: Update forward compatibility horizon to 2024-11-01
    PiperOrigin-RevId: 692039711

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 2032  // Updated: 2024/10/31
+#define TF_GRAPH_DEF_VERSION 2033  // Updated: 2024/11/1
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
