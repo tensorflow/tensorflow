3d075f598968d148d422a97000bcb568ba2065f1
Ionel Gog <icgog@google.com>
2024-11-01 14:13:30 -0700

    [IFRT] Add pass to detect atom program type and invoke preprocessing pipelines.
    The change also adds a pass that verifies that each atom program is using devices of the expected platform (e.g., all devices of an atom program are of the same platform).
    
    PiperOrigin-RevId: 692287779

--- third_party/xla/xla/python/ifrt/ir/transforms/passes.h
+++ third_party/xla/xla/python/ifrt/ir/transforms/passes.h
@@ -113,6 +113,14 @@ CreateIfrtCompileAndPropagateShardingsPass(
         compile_options,
     std::shared_ptr<AtomExecutableMap> atom_executable_map);
 
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
+CreateIfrtPrecompileAtomProgramPreprocessingPass(
+    IfrtPrecompileAtomProgramPreprocessingPassOptions options = {});
+
+std::unique_ptr<mlir::OperationPass<mlir::ModuleOp>>
+CreateIfrtVerifyDeviceTypeConsistencyPass(
+    IfrtVerifyDeviceTypeConsistencyPassOptions options = {});
+
 // Generated definitions. This should be placed after all Pass creations.
 #define GEN_PASS_REGISTRATION
 #include "xla/python/ifrt/ir/transforms/passes.h.inc"  // IWYU pragma: export
