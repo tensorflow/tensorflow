68c3f59eacd8a3ac3e613c1a139f721cfcfa0816
A. Unique TensorFlower <gardener@tensorflow.org>
2024-11-01 21:00:28 -0700

    Automated Code Change
    PiperOrigin-RevId: 692380386

--- tensorflow/compiler/mlir/lite/stablehlo/transforms/fold_broadcast_pass.cc
+++ tensorflow/compiler/mlir/lite/stablehlo/transforms/fold_broadcast_pass.cc
@@ -13,10 +13,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
 #include <functional>
 #include <memory>
 #include <utility>
-#include <vector>
 
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/APInt.h"
