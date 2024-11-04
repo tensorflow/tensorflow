d31d341316098a20fb478fc1c3835eacdc547cbf
Kyle Lucke <klucke@google.com>
2024-11-04 15:01:18 -0800

    Eliminate or narrow stream_executor dependencies rather than use the broad "everything" target.
    PiperOrigin-RevId: 693102925

--- tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.cc
+++ tensorflow/compiler/mlir/tools/kernel_gen/tf_framework_c_interface.cc
@@ -45,7 +45,6 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/tools/kernel_gen/ir/tf_framework_ops.h"
 #include "tensorflow/compiler/mlir/tools/kernel_gen/kernel_creator.h"
 #include "tensorflow/compiler/mlir/tools/kernel_gen/tf_jit_cache.h"
-#include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/tsl/framework/allocator.h"
 #include "tensorflow/core/framework/allocator.h"
