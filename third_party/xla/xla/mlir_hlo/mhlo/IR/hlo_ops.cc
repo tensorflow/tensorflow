836efba624df3e609bfdfd9eeed4e8c79db598a1
Matthias Kramm <kramm@google.com>
2024-11-05 14:23:10 -0800

    Don't fold SetDimensionSize to a constant.
    PiperOrigin-RevId: 693476660

--- third_party/xla/xla/mlir_hlo/mhlo/IR/hlo_ops.cc
+++ third_party/xla/xla/mlir_hlo/mhlo/IR/hlo_ops.cc
@@ -4005,12 +4005,16 @@ LogicalResult SelectOp::reifyReturnTypeShapes(
 
 OpFoldResult SetDimensionSizeOp::fold(FoldAdaptor adaptor) {
   auto operands = adaptor.getOperands();
-  DenseElementsAttr input = dyn_cast_or_null<DenseElementsAttr>(operands[0]);
-  if (input) return input;
 
+  // Even if all operands are constants, we can't fold SetDimensionSize to a
+  // constant, since mhlo.constant doesn't support dynamic dimensions. We can,
+  // however, replace the op with its operand, in the case where the (constant)
+  // bound of a dimension is the same as the full extent of said dimension.
   DenseElementsAttr size = dyn_cast_or_null<DenseElementsAttr>(operands[1]);
   if (!size || !size.isSplat()) return {};
 
+  // TODO(b/377537099): This is the result type, which is always dynamic in the
+  // dimension we're looking at. So the code below doesn't do anything.
   auto ty = dyn_cast<RankedTensorType>(getType());
   if (!ty) return {};
 
