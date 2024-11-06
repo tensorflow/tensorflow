9f93672bc74a5eb8deb5cfd47518a727cebe3ff0
Farzin Houshmand <farzinh@google.com>
2024-11-05 18:22:40 -0800

    Rollback of PR #18765 Breaking internal tests
    Reverts 605a64af71d669281c2212ffd508272625170b38
    
    PiperOrigin-RevId: 693543028

--- third_party/xla/xla/hlo/analysis/while_loop_analysis.cc
+++ third_party/xla/xla/hlo/analysis/while_loop_analysis.cc
@@ -18,24 +18,21 @@ limitations under the License.
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
-#include <memory>
-#include <optional>
 
-#include "absl/algorithm/container.h"
 #include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "xla/comparison_util.h"
 #include "xla/hlo/evaluator/hlo_evaluator.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/hlo/ir/hlo_reachability.h"
-#include "xla/hlo/utils/hlo_query.h"
 #include "xla/literal.h"
 #include "xla/literal_util.h"
 #include "xla/service/pattern_matcher.h"
 #include "xla/shape_util.h"
-#include "xla/tools/hlo_extractor.h"
 
 namespace xla {
 
@@ -43,92 +40,66 @@ using std::nullopt;
 using std::optional;
 namespace m = match;
 
-// Finds and returns the non-constant operand in instr, if there is only one
-// such operand.
+// Finds and returns the non-constant operand in instr.
 //
-// Returns nullptr if instr doesn't have exactly one unique non-constant
-// operand.
+// CHECK-fails if instr doesn't have exactly one unique non-constant operand.
 static const HloInstruction* NonConstantOperand(const HloInstruction* instr) {
   const HloInstruction* result = nullptr;
   for (const HloInstruction* operand : instr->operands()) {
     if (!operand->IsConstant()) {
-      if (result != nullptr && result != operand) {
-        return nullptr;
+      if (result != nullptr) {
+        CHECK_EQ(result, operand);
       }
       result = operand;
     }
   }
+  CHECK_NE(result, nullptr);
   return result;
 }
 
-// If `out` is a function of a single value in the tuple `in` and has no other
-// dependence, i.e. if `out=f(gte(in))`, then this function will return the
-// unique get-tuple-element index for the dependence.
-//
-// For example, in the following HLO, this function will return `1`:
-//   in = (s32[], s32[], s32[]) tuple(a,b,c)
-//   gte.1 = get-tuple-element(in), index=1
-//   out = fusion(gte.1), ...
-std::optional<int64_t> GetUniqueGTEDependenceIndex(const HloInstruction* out,
-                                                   const HloInstruction* in) {
-  if (out->parent() != in->parent() || !in->shape().IsTuple()) {
-    return std::nullopt;
+// If all of instr's operands are either constants or have the form
+//   get-tuple-element(gte_operand, N)
+// for the same value N, returns N.  Otherwise, returns nullopt.
+static optional<int64_t> GetGTEOperandIndex(const HloInstruction* instr,
+                                            const HloInstruction* gte_operand) {
+  VLOG(2) << "GetGTEOperandIndex(" << instr->ToString()
+          << ", GTE Operand: " << gte_operand->ToString() << ")";
+
+  // All operands of `instr` must be either constants or of the form
+  //   get-tuple-element(gte_operand, tuple_idx)
+  // for the same value tuple_idx. We also support the case where GTE feeds a
+  // copy that is then used.
+  optional<int64_t> tuple_idx;
+  for (const HloInstruction* operand : instr->operands()) {
+    if (Match(operand, m::Constant())) {
+      continue;
+    }
+    auto possibly_gte_operand = operand;
+
+    if (operand->opcode() == HloOpcode::kCopy) {
+      possibly_gte_operand = operand->operand(0);
+    }
+
+    if (possibly_gte_operand->opcode() != HloOpcode::kGetTupleElement) {
+      return nullopt;
+    }
+
+    if (!Match(possibly_gte_operand,
+               m::GetTupleElement(m::Op().Is(gte_operand)))) {
+      return nullopt;
+    }
+
+    int64_t operand_tuple_idx = possibly_gte_operand->tuple_index();
+    // This is the first GTE we are seeing. Set tuple_idx.
+    if (!tuple_idx.has_value()) {
+      tuple_idx = operand_tuple_idx;
+    } else {
+      if (operand_tuple_idx != tuple_idx) {
+        return nullopt;
+      }
+    }
   }
-
-  // Extracts the instruction `out` as a function of the instruction `in`.
-  // HloModule extracted
-  // ENTRY main {
-  //   in = parameter(0)
-  //   //... some calculations
-  //   ROOT out = ...
-  // }
-  std::unique_ptr<HloModule> extracted = ExtractModule(
-      /*instruction=*/out, /*height=*/-1, /*extract_selector=*/
-      [in](const HloInstruction* inst) -> bool { return inst != in; },
-      /*replace_type_selector=*/
-      [](const HloInstruction* inst) -> ReplaceType {
-        return ReplaceType::kReplaceParam;
-      });
-  HloComputation* entry = extracted->entry_computation();
-
-  // Check that the extracted module takes nothing but `in` as input. If `out`
-  // does not depend on in, the extracted module will have some other shape for
-  // input.
-  if (entry->num_parameters() != 1 ||
-      entry->parameter_instruction(0)->shape() != in->shape()) {
-    return std::nullopt;
-  }
-  HloInstruction* param = entry->parameter_instruction(0);
-
-  // If there are no users for the input `in`, it would mean that `out` does not
-  // depend on a get-tuple-element of `in`.
-  if (param->user_count() == 0) return nullopt;
-
-  // If any of the users of the input `in` is not a get-tuple-element
-  // instruction, then that would mean that the output does not depend uniquely
-  // on a get-tuple-element of on `in`, instead it depends on some other
-  // calculations on `in`.
-  if (absl::c_any_of(param->users(), [](const HloInstruction* inst) -> bool {
-        return inst->opcode() != HloOpcode::kGetTupleElement;
-      })) {
-    return std::nullopt;
-  }
-
-  // We extract the candidate index from the first user. At this point we
-  // already know that the all the users are get-tuple-elements and that there
-  // is atleast one user.
-  int64_t candidate_index = param->users()[0]->tuple_index();
-
-  // We check that all the users of the input instruction `in` (which we already
-  // know to be get-tuple-element instructions) have the same tuple index.
-  if (absl::c_any_of(param->users(),
-                     [candidate_index](const HloInstruction* inst) -> bool {
-                       return inst->tuple_index() != candidate_index;
-                     })) {
-    return std::nullopt;
-  }
-
-  return candidate_index;
+  return tuple_idx;
 }
 
 // The below function identifies a subset of all possible auxiliary
@@ -304,7 +275,7 @@ optional<int64_t> GetLoopInductionVarTupleIdx(const HloInstruction* while_op) {
   auto* while_cond_root = while_cond->root_instruction();
   auto* while_cond_param = while_cond->parameter_instruction(0);
   optional<int64_t> indvar_tuple_idx =
-      GetUniqueGTEDependenceIndex(while_cond_root, while_cond_param);
+      GetGTEOperandIndex(while_cond_root, while_cond_param);
   if (!indvar_tuple_idx) {
     VLOG(2) << "Induction variable not found in loop condition: "
             << while_cond->root_instruction()->ToString();
@@ -330,7 +301,7 @@ optional<int64_t> GetLoopInductionVarTupleIdx(const HloInstruction* while_op) {
   while_body_inc = while_body_root->operand(*indvar_tuple_idx);
   auto* while_body_param = while_body->parameter_instruction(0);
   optional<int64_t> while_body_indvar_tuple_idx =
-      GetUniqueGTEDependenceIndex(while_body_inc, while_body_param);
+      GetGTEOperandIndex(while_body_inc, while_body_param);
   if (!while_body_indvar_tuple_idx) {
     VLOG(2)
         << "Induction variable not found in while body increment instruction: "
@@ -402,12 +373,6 @@ optional<int64_t> MatchTrivialLoopTripCount(const HloInstruction* while_op,
   auto* while_body_indvar_update =
       while_body->root_instruction()->mutable_operand(indvar_tuple_idx);
   auto* while_body_indvar = NonConstantOperand(while_body_indvar_update);
-  if (while_body_indvar == nullptr ||
-      while_body_indvar !=
-          hlo_query::GetUniqueGteInstruction(
-              while_body->parameter_instruction(0), indvar_tuple_idx)) {
-    return std::nullopt;
-  }
   HloInstruction* trip_count_increase_step_instr = nullptr;
   int64_t trip_count_step = 0;
   if (!Match(while_body_indvar_update,
@@ -452,12 +417,6 @@ optional<int64_t> MatchTrivialLoopTripCount(const HloInstruction* while_op,
   auto* while_cond = while_op->while_condition();
   auto* while_cond_root = while_cond->root_instruction();
   auto* while_cond_indvar = NonConstantOperand(while_cond_root);
-  if (while_cond_indvar == nullptr ||
-      while_cond_indvar !=
-          hlo_query::GetUniqueGteInstruction(
-              while_cond->parameter_instruction(0), indvar_tuple_idx)) {
-    return std::nullopt;
-  }
   HloInstruction* while_cond_bound = nullptr;
   if (!Match(while_cond_root,
              m::Op().WithBinaryOperandsAnyOrder(
@@ -571,22 +530,10 @@ optional<int64_t> ComputeWhileLoopTripCount(const HloInstruction* while_op,
   auto* while_body_indvar_update =
       while_body->root_instruction()->operand(*indvar_tuple_idx);
   auto* while_body_indvar = NonConstantOperand(while_body_indvar_update);
-  if (while_body_indvar == nullptr ||
-      while_body_indvar !=
-          hlo_query::GetUniqueGteInstruction(
-              while_body->parameter_instruction(0), *indvar_tuple_idx)) {
-    return std::nullopt;
-  }
 
   auto* while_cond = while_op->while_condition();
   auto* while_cond_root = while_cond->root_instruction();
   auto* while_cond_indvar = NonConstantOperand(while_cond_root);
-  if (while_cond_indvar == nullptr ||
-      while_cond_indvar !=
-          hlo_query::GetUniqueGteInstruction(
-              while_cond->parameter_instruction(0), *indvar_tuple_idx)) {
-    return std::nullopt;
-  }
 
   for (int64_t trip_count = 0; trip_count != max_brute_force_iters + 1;
        ++trip_count) {
