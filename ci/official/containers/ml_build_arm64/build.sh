31a7e5c672d078db8d2bfacf541437ceb2eef559
Quoc Truong <quoct@google.com>
2024-11-12 17:11:50 -0800

    Fix the build script since we removed the TF and JAX tags and will only have one image for both TF and JAX.
    PiperOrigin-RevId: 695922929

--- ci/official/containers/ml_build_arm64/build.sh
+++ ci/official/containers/ml_build_arm64/build.sh
@@ -16,8 +16,7 @@
 
 # Builds the following Docker images for Linux ARM64. See the accompanying
 # Dockerfile for more details:
-# - us-central1-docker.pkg.dev/tensorflow-sigs/build-arm64:jax-latest-multi-python
-# - us-central1-docker.pkg.dev/tensorflow-sigs/build-arm64:tf-latest-multi-python
+# - us-central1-docker.pkg.dev/tensorflow-sigs/ml-build-arm64:latest-multi-python
 
 set -exo pipefail
 
@@ -30,14 +29,9 @@ cd "$(dirname "$0")"
 
 if is_continuous_or_release || [[ -z "$KOKORO_BUILD_ID" ]]; then
   # A continuous job is the only one to publish to latest
-  TAG="latest-multi-python"
+  TAG="latest"
 else
-  # If it is a change, grab a good tag for iterative builds
-  if [[ -z "${KOKORO_GITHUB_PULL_REQUEST_NUMBER}" ]]; then
-    TAG=$(head -n 1 "$KOKORO_PIPER_DIR/presubmit_request.txt" | cut -d" " -f2)
-  else
-    TAG="pr-${KOKORO_GITHUB_PULL_REQUEST_NUMBER}"
-  fi
+  TAG=$(head -n 1 "$KOKORO_PIPER_DIR/presubmit_request.txt" | grep -oP "^presubmit_changelist: +\K(\d+)")
 fi
 
 AR_IMAGE_PATH="us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/ml-build-arm64"
@@ -45,32 +39,29 @@ AR_IMAGE_PATH="us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/ml-build-ar
 # Build for both JAX and TF usage.  We do these in one place because they share
 # almost all of the same cache layers
 export DOCKER_BUILDKIT=1
-for target in jax tf; do
-  AR_IMAGE="$AR_IMAGE_PATH:$target-$TAG"
-  docker pull "$AR_IMAGE" || true
-  # Due to some flakiness of resources pulled in the build, allow the docker
-  # command to reattempt build a few times in the case of failure (b/302558736)
-  set +e
-  for i in $(seq 1 5)
-  do
-    docker build \
-    --build-arg REQUIREMENTS_FILE=jax.requirements.txt \
-    --target=$target \
-    --cache-from "$AR_IMAGE" \
-    -t "$AR_IMAGE" . && break
-  done
-  final=$?
-  if [ $final -ne 0 ]; then
-    exit $final
-  fi
-  set -e
-
-  INFRA_PUBLIC_TAG=infrastructure-public-image-$(docker images "$AR_IMAGE" --quiet)
-  AR_IMAGE_INFRA_PUBLIC="$AR_IMAGE_PATH:$INFRA_PUBLIC_TAG"
-  docker image tag "$AR_IMAGE" "$AR_IMAGE_INFRA_PUBLIC"
-
-  if [[ -n "$KOKORO_BUILD_ID" ]]; then
-    gcloud auth configure-docker us-central1-docker.pkg.dev
-    docker push "$AR_IMAGE_PATH" --all-tags
-  fi
+AR_IMAGE="$AR_IMAGE_PATH:$TAG"
+docker pull "$AR_IMAGE" || true
+# Due to some flakiness of resources pulled in the build, allow the docker
+# command to reattempt build a few times in the case of failure (b/302558736)
+set +e
+for i in $(seq 1 5)
+do
+  docker build \
+  --build-arg REQUIREMENTS_FILE=jax.requirements.txt \
+  --cache-from "$AR_IMAGE" \
+  -t "$AR_IMAGE" . && break
 done
+final=$?
+if [ $final -ne 0 ]; then
+  exit $final
+fi
+set -e
+
+INFRA_PUBLIC_TAG=infrastructure-public-image-$(docker images "$AR_IMAGE" --quiet)
+AR_IMAGE_INFRA_PUBLIC="$AR_IMAGE_PATH:$INFRA_PUBLIC_TAG"
+docker image tag "$AR_IMAGE" "$AR_IMAGE_INFRA_PUBLIC"
+
+if [[ -n "$KOKORO_BUILD_ID" ]]; then
+  gcloud auth configure-docker us-central1-docker.pkg.dev
+  docker push "$AR_IMAGE_PATH" --all-tags
+fi
