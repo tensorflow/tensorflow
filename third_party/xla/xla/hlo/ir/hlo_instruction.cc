238f6fea9e9076668ec30df1eaa15e158af1601c
A. Unique TensorFlower <gardener@tensorflow.org>
2024-11-13 14:21:11 -0800

    Update clone with new operands to handle ragged all-to-all.
    PiperOrigin-RevId: 696269330

--- third_party/xla/xla/hlo/ir/hlo_instruction.cc
+++ third_party/xla/xla/hlo/ir/hlo_instruction.cc
@@ -2560,6 +2560,7 @@ std::unique_ptr<HloInstruction> HloInstruction::CloneWithNewOperands(
     case HloOpcode::kReduceScatter:
     case HloOpcode::kAllReduceStart:
     case HloOpcode::kAllToAll:
+    case HloOpcode::kRaggedAllToAll:
     case HloOpcode::kCollectiveBroadcast:
     case HloOpcode::kCollectivePermute:
     case HloOpcode::kCollectivePermuteStart:
