ed3e40f2a2bac873463b40268e2b39e1b552b11c
Milica Makevic <Milica.Makevic@amd.com>
2024-11-14 13:23:27 +0000

    Fix block dim limit in gpu_kernel_tiling_test

--- third_party/xla/xla/service/gpu/tests/gpu_kernel_tiling_test.cc
+++ third_party/xla/xla/service/gpu/tests/gpu_kernel_tiling_test.cc
@@ -402,7 +402,7 @@ TEST_F(GpuKernelTilingTest, ReductionInputTooLarge) {
   EXPECT_THAT(status.message(),
               ::testing::ContainsRegex(
                   "Kernel '.*' launch needs more blocks [(]4294967296, 1[)] "
-                  "than allowed by hardware [(]2147483647, 65535[)]"));
+                  "than allowed by hardware [(]2147483647, 65536[)]"));
 }
 
 }  // namespace
