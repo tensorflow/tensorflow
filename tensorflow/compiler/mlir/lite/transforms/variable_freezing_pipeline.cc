569a09491d86b855ccad91696bb2fb1160a34cda
Vamsi Manchala <vamsimanchala@google.com>
2024-11-15 12:00:24 -0800

    Add boilerplate code to help define, create and register pipelines in TFLite Converter
    PiperOrigin-RevId: 696959021

--- tensorflow/compiler/mlir/lite/transforms/variable_freezing_pipeline.cc
+++ tensorflow/compiler/mlir/lite/transforms/variable_freezing_pipeline.cc
@@ -13,27 +13,24 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/compiler/mlir/lite/transforms/converter_pass_options_setter.h"
+#include "tensorflow/compiler/mlir/lite/transforms/variable_freezing_pipeline.h"
 
-#include "tensorflow/compiler/mlir/lite/transforms/optimize_pass_options.h"
-#include "tensorflow/compiler/mlir/lite/transforms/pass_options.h"
+#include "tensorflow/compiler/mlir/lite/transforms/pass_registry_utils.h"
+#include "tensorflow/compiler/mlir/lite/transforms/unfreeze_global_constants.h"
 #include "tensorflow/compiler/mlir/lite/transforms/variable_freezing_pipeline_options.h"
+#include "tensorflow/compiler/mlir/tensorflow/transforms/tf_saved_model_passes.h"
 
 namespace mlir {
 namespace TFL {
 
-void ConverterPassOptionsSetter::SetOptions(
-    OptimizePassOptions& options) const {
-  options.enable_canonicalization = true;
-  options.disable_fuse_mul_and_fc = converter_flags_.disable_fuse_mul_and_fc();
+void VariableFreezingPipeline::AddPasses() {
+  AddPass(mlir::tf_saved_model::CreateOptimizeGlobalTensorsPass(),
+          [](const VariableFreezingPipelineOptions& options) { return true; });
+  AddPass(mlir::tf_saved_model::CreateFreezeGlobalTensorsPass(true),
+          [](const VariableFreezingPipelineOptions& options) { return true; });
+  AddPass(mlir::TFL::Create<mlir::TFL::UnfreezeMutableGlobalTensorsPass>(),
+          [](const VariableFreezingPipelineOptions& options) { return true; });
 }
-
-void ConverterPassOptionsSetter::SetOptions(
-    VariableFreezingPipelineOptions& options) const {
-  options.enable_tflite_variables = pass_config_.enable_tflite_variables;
-}
-
-void ConverterPassOptionsSetter::SetOptions(EmptyPassOptions& options) const {}
 
 }  // namespace TFL
 }  // namespace mlir
