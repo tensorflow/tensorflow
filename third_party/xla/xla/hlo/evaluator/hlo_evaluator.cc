9f33981c820fc233f151805e83b92a739239d71b
Bill Varcho <varcho@google.com>
2024-11-19 03:01:47 -0800

    [Upkeep][XLA-Code-Health] Resolve 2 instances of the following issue: Todo (resolved)
    PiperOrigin-RevId: 697943839

--- third_party/xla/xla/hlo/evaluator/hlo_evaluator.cc
+++ third_party/xla/xla/hlo/evaluator/hlo_evaluator.cc
@@ -3804,7 +3804,6 @@ absl::StatusOr<Literal> StochasticConvertOp(const Literal& operand_literal,
                                      primitive_type_constant>(
               operand_literal, random_literal, result_shape);
         }
-        // TODO(b/232442915): Enable converting big floats to small floats.
         return Unimplemented(
             "Stochastically converting from type %s to type %s is not "
             "implemented.",
@@ -3826,7 +3825,7 @@ absl::StatusOr<Literal> StochasticConvertOp(const Literal& operand_literal,
               primitive_util::UnsignedIntegralTypeForBitWidth(
                   primitive_util::BitWidth(primitive_type_constant))>(
               operand_literal, random_literal, result_shape);
-        }  // TODO(b/232442915): Enable converting big floats to small floats.
+        }
         return Unimplemented(
             "Stochastically converting from type %s to type %s is not "
             "implemented.",
