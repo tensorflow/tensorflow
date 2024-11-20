605e172b30b466c4842c5c9548083f60c151ee61
Farzin Houshmand <farzinh@google.com>
2024-11-20 15:59:48 -0800

    Adding step to constant_value and add support for multiplication while recursively calculating the range of an expression.
    PiperOrigin-RevId: 698551804

--- third_party/xla/xla/service/value_range.cc
+++ third_party/xla/xla/service/value_range.cc
@@ -15,10 +15,19 @@ limitations under the License.
 
 #include "xla/service/value_range.h"
 
+#include <cstdint>
 #include <optional>
 #include <string>
 
+#include "absl/container/flat_hash_map.h"
+#include "absl/log/log.h"
+#include "absl/strings/str_cat.h"
+#include "xla/comparison_util.h"
+#include "xla/hlo/analysis/hlo_alias_analysis.h"
 #include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/primitive_util.h"
+#include "xla/service/constant_value.h"
 
 namespace xla {
 
@@ -40,15 +49,34 @@ std::string Range::ToString() const {
   if (IsEmpty()) {
     return std::string("Empty");
   }
-  return absl::StrCat("min: ", min_.ToString(), " max: ", max_.ToString());
+  return absl::StrCat(
+      "min: ", min_.ToString(), " max: ", max_.ToString(),
+      " step: ", IsStepKnown() ? step_.value().ToString() : "Unknown");
+}
+
+std::optional<ConstantValue> FindStepForBinaryOp(const Range& lhs,
+                                                 const Range& rhs) {
+  if (!lhs.IsStepKnown() || !rhs.IsStepKnown()) {
+    return std::nullopt;
+  }
+  if (lhs.IsSingleValue()) {
+    return rhs.step();
+  }
+  if (rhs.IsSingleValue()) {
+    return lhs.step();
+  }
+  if (lhs.step().eq(rhs.step())) {
+    return lhs.step();
+  }
+  return std::nullopt;
 }
 
 // Identify the value ranges of a scalar HLO with a integer type. It returns
 // a range of values that the instruction can have.
 Range RecursivelyIdentifyRange(
     const HloInstruction* instr,
-    const absl::flat_hash_map<const HloInstruction*, Range>&
-        predefined_ranges) {
+    const absl::flat_hash_map<const HloInstruction*, Range>& predefined_ranges,
+    const HloAliasAnalysis* alias_analysis) {
   // Non scalar or non-integer HLO. Abort.
   if ((!instr->shape().IsInteger() && instr->shape().element_type() != PRED) ||
       instr->shape().dimensions_size() != 0) {
@@ -60,14 +88,26 @@ Range RecursivelyIdentifyRange(
     VLOG(5) << "Found range! " << it->second.max().GetSignedValue() << " "
             << it->second.min().GetSignedValue();
     return it->second;
+  } else if (alias_analysis != nullptr) {
+    auto value_set =
+        alias_analysis->dataflow_analysis().GetFlattenedValueSet(instr);
+    for (const auto& value : value_set.TakeValues()) {
+      auto it = predefined_ranges.find(value->defining_instruction());
+      if (it != predefined_ranges.end()) {
+        VLOG(5) << "Found range in defining instruction! "
+                << it->second.max().GetSignedValue() << " "
+                << it->second.min().GetSignedValue();
+        return it->second;
+      }
+    }
   }
   switch (instr->opcode()) {
     case HloOpcode::kCompare: {
       VLOG(5) << "Handling Compare";
-      Range lhs =
-          RecursivelyIdentifyRange(instr->operand(0), predefined_ranges);
-      Range rhs =
-          RecursivelyIdentifyRange(instr->operand(1), predefined_ranges);
+      Range lhs = RecursivelyIdentifyRange(instr->operand(0), predefined_ranges,
+                                           alias_analysis);
+      Range rhs = RecursivelyIdentifyRange(instr->operand(1), predefined_ranges,
+                                           alias_analysis);
       VLOG(5) << "Returned Rhs: " << rhs.ToString()
               << " Lhs: " << lhs.ToString();
       // Only kLt supported right now.
@@ -105,11 +145,13 @@ Range RecursivelyIdentifyRange(
         const int64_t value = *instr->literal().GetFirstInteger();
         return Range{ConstantValue::GetSigned(value, bitwidth),
                      ConstantValue::GetSigned(value, bitwidth),
+                     ConstantValue::GetOne(/*bitwidth=*/1, /*is_signed=*/false),
                      /*is_linear=*/true};
       }
       const uint64_t value = *instr->literal().GetFirstInteger();
       return Range{ConstantValue::GetUnsigned(value, bitwidth),
                    ConstantValue::GetUnsigned(value, bitwidth),
+                   ConstantValue::GetOne(/*bitwidth=*/1, /*is_signed=*/false),
                    /*is_linear=*/true};
     }
     case HloOpcode::kAdd: {
@@ -117,10 +159,10 @@ Range RecursivelyIdentifyRange(
         return Range{};
       }
       VLOG(5) << "Handling Add";
-      Range lhs =
-          RecursivelyIdentifyRange(instr->operand(0), predefined_ranges);
-      Range rhs =
-          RecursivelyIdentifyRange(instr->operand(1), predefined_ranges);
+      Range lhs = RecursivelyIdentifyRange(instr->operand(0), predefined_ranges,
+                                           alias_analysis);
+      Range rhs = RecursivelyIdentifyRange(instr->operand(1), predefined_ranges,
+                                           alias_analysis);
       VLOG(5) << "Returned Rhs: " << rhs.ToString()
               << " Lhs: " << lhs.ToString();
       if (lhs.IsEmpty() || rhs.IsEmpty()) {
@@ -132,31 +174,61 @@ Range RecursivelyIdentifyRange(
         VLOG(5) << "Add wrapped";
         return Range{};
       }
-      return Range{min, max, lhs.IsLinear() && rhs.IsLinear()};
+      return Range{min, max, FindStepForBinaryOp(lhs, rhs),
+                   lhs.IsLinear() && rhs.IsLinear()};
+    }
+    case HloOpcode::kMultiply: {
+      if (!instr->shape().IsInteger()) {
+        return Range{};
+      }
+      VLOG(5) << "Handling Multiply";
+      Range lhs = RecursivelyIdentifyRange(instr->operand(0), predefined_ranges,
+                                           alias_analysis);
+      Range rhs = RecursivelyIdentifyRange(instr->operand(1), predefined_ranges,
+                                           alias_analysis);
+      VLOG(5) << "Returned Rhs: " << rhs.ToString()
+              << " Lhs: " << lhs.ToString();
+      if (lhs.IsEmpty() || rhs.IsEmpty()) {
+        return Range{};
+      }
+      // We only handle multiplication of a single value with a range.
+      if (!lhs.IsSingleValue() && !rhs.IsSingleValue()) {
+        return Range{};
+      }
+      ConstantValue single_value = lhs.IsSingleValue() ? lhs.min() : rhs.min();
+      ConstantValue min = lhs.IsSingleValue() ? rhs.min().mul(single_value)
+                                              : lhs.min().mul(single_value);
+      ConstantValue max = lhs.IsSingleValue() ? rhs.max().mul(single_value)
+                                              : lhs.max().mul(single_value);
+      return Range{min, max, FindStepForBinaryOp(lhs, rhs),
+                   lhs.IsLinear() && rhs.IsLinear()};
     }
     case HloOpcode::kSelect: {
       VLOG(5) << "Handling Select";
       const HloInstruction* cmp = instr->operand(0);
-      Range cmp_range = RecursivelyIdentifyRange(cmp, predefined_ranges);
+      Range cmp_range =
+          RecursivelyIdentifyRange(cmp, predefined_ranges, alias_analysis);
       // Support only when the select has a constant value as condition.
       if (cmp_range.IsEmpty() || !cmp_range.IsSingleValue()) {
         VLOG(5) << "Select failed";
         return Range{};
       }
       if (cmp_range.GetSingleSignedValue() == 0) {
-        return RecursivelyIdentifyRange(instr->operand(2), predefined_ranges);
+        return RecursivelyIdentifyRange(instr->operand(2), predefined_ranges,
+                                        alias_analysis);
       }
-      return RecursivelyIdentifyRange(instr->operand(1), predefined_ranges);
+      return RecursivelyIdentifyRange(instr->operand(1), predefined_ranges,
+                                      alias_analysis);
     }
     case HloOpcode::kSubtract: {
       if (!instr->shape().IsInteger()) {
         return Range{};
       }
       VLOG(5) << "Handling Subtract";
-      Range lhs =
-          RecursivelyIdentifyRange(instr->operand(0), predefined_ranges);
-      Range rhs =
-          RecursivelyIdentifyRange(instr->operand(1), predefined_ranges);
+      Range lhs = RecursivelyIdentifyRange(instr->operand(0), predefined_ranges,
+                                           alias_analysis);
+      Range rhs = RecursivelyIdentifyRange(instr->operand(1), predefined_ranges,
+                                           alias_analysis);
       VLOG(5) << "Returned Rhs: " << rhs.ToString()
               << " Lhs: " << lhs.ToString();
       if (lhs.IsEmpty() || rhs.IsEmpty()) {
@@ -168,7 +240,8 @@ Range RecursivelyIdentifyRange(
         VLOG(5) << "Subtract wrapped";
         return Range{};
       }
-      return Range{min, max, lhs.IsLinear() && rhs.IsLinear()};
+      return Range{min, max, FindStepForBinaryOp(lhs, rhs),
+                   lhs.IsLinear() && rhs.IsLinear()};
     }
     default:
       break;
