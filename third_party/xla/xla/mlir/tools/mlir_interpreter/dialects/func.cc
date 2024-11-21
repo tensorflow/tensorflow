b02f2fb780b7d0dce203d3963feb238303757fd6
mraunak <83710963+mraunak@users.noreply.github.com>
2024-11-21 06:18:07 -0800

    PR #18407: Fix xla-mlir failures on Windows
    Imported from GitHub PR https://github.com/openxla/xla/pull/18407
    
    This PR aims to enable the XLA/mlir/tool test cases on the Windows Platform.
    
    Error:
    //xla/mlir/tools/mlir_bisect/... tests were failing on the Windows platform with the errors shown below:
    
    Errors
    Error 1.Error with llvm::seq
    no matching function for call to 'seq'
    for (auto i : llvm::seq(0ul, sizeof...(T))) {
    Solution: change to llvm::seq(0, sizeof...(T))
    By explicitly specifying the type (unsigned long) in llvm::seq, the compiler now clearly understands the type of the sequence.
    
    Error 2. Missing dlfcn.h:
    Location: xla/mlir/tools/mlir_interpreter/dialects/func.cc
    fatal error: 'dlfcn.h' file not found
    Solution: include 'windows.h' for Windows platform
    Error 3.
    Use of Undeclared Identifiers sym and RTLD_DEFAULT:
    Location: xla/mlir/tools/mlir_interpreter/dialects/func.cc
    use of undeclared identifier 'sym'
    sym = dlsym(RTLD_DEFAULT, callee.getSymName().str().c_str());
    ^
    use of undeclared identifier 'RTLD_DEFAULT'
    Solution:
    On Windows, the approach to obtaining a symbol's address differs from Unix-based systems.
    GetModuleHandle function retrieves a handle to the specified module (DLL) that is loaded in the address space of the calling process. This handle is necessary to access the module's symbols.
    GetProcAddress function locates the address of an exported function or variable by name.
    Copybara import of the project:
    
    --
    1a428996c7991df8e093393e7989fbcf251dc0f4 by Raunak <mayank.kumar.raunak@intel.com>:
    
    fix xla-mlir failures on windows
    
    --
    15009666c4ee861218bb798c6fe0d2493fa8e060 by Raunak <mayank.kumar.raunak@intel.com>:
    
    resolve comments
    
    --
    2483001d510582179d74b94571f9fd6beb943aaa by Raunak <mayank.kumar.raunak@intel.com>:
    
    Keep the original file
    
    --
    4c7fe5e4debed0ff39eb87f64f60f99ce6ee0a74 by Raunak <mayank.kumar.raunak@intel.com>:
    
    fix the formatting issue
    
    --
    270898a2b0bca97a7de30435ce6a53b5980ca73e by mraunak <83710963+mraunak@users.noreply.github.com>:
    
    Update symbol_finder_windows.cc
    --
    6b63a306ee4ef69f9849822418426d5f705e73ff by mraunak <83710963+mraunak@users.noreply.github.com>:
    
    Update symbol_finder_linux.cc
    --
    f0996fcc1c67e43bbb7b7829adddf0c7d8f5c738 by mraunak <83710963+mraunak@users.noreply.github.com>:
    
    Update symbol_finder.h
    --
    0c0c9bba3dac548e663aab5e2e3af6fb96c77fde by Raunak <mayank.kumar.raunak@intel.com>:
    
    Fix the build file
    
    --
    6d7f269262dcc8a85579c62db51e38dc534d6564 by Raunak <mayank.kumar.raunak@intel.com>:
    
    Resolve the comments
    
    --
    ef598af149e9ad96dc1fa27be763a7ffd219011c by Raunak <mayank.kumar.raunak@intel.com>:
    
    Resolve the comments
    
    --
    7131b8d24ad353044b622614c36c342d90101d37 by Raunak <mayank.kumar.raunak@intel.com>:
    
    added :find_symbol to dependency
    
    --
    64a6e9e45d6deef4201c3cd8da64e99b9d40ca78 by mraunak <83710963+mraunak@users.noreply.github.com>:
    
    Update BUILD
    --
    d47a8b27c89e5df01ea94a237080fd2ac3ad8e85 by mraunak <83710963+mraunak@users.noreply.github.com>:
    
    Fix clang format
    --
    1a24df16d3de5f007065b69a67965158e821ffe3 by Raunak <mayank.kumar.raunak@intel.com>:
    
    resolve the comments
    
    --
    12f69fc2d188f8bc368bc5e29b53a80d15b6dbac by Raunak <mayank.kumar.raunak@intel.com>:
    
    adding namespace and header style consistent
    
    --
    ec9b5051471a36f7881ed21215f60ec893f18e7d by Raunak <mayank.kumar.raunak@intel.com>:
    
    Fix the build file
    
    Merging this change closes #18407
    
    PiperOrigin-RevId: 698754912

--- third_party/xla/xla/mlir/tools/mlir_interpreter/dialects/func.cc
+++ third_party/xla/xla/mlir/tools/mlir_interpreter/dialects/func.cc
@@ -13,16 +13,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <dlfcn.h>
-
 #include <tuple>
 #include <type_traits>
 
+#include "absl/status/statusor.h"
 #include "llvm/Support/Casting.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/IR/Types.h"
 #include "mlir/Support/LLVM.h"
+#include "xla/mlir/tools/mlir_interpreter/dialects/symbol_finder.h"
 #include "xla/mlir/tools/mlir_interpreter/dialects/util.h"
 #include "xla/mlir/tools/mlir_interpreter/framework/interpreter.h"
 #include "xla/mlir/tools/mlir_interpreter/framework/interpreter_value.h"
@@ -96,11 +96,14 @@ llvm::SmallVector<InterpreterValue> Call(MutableArrayRef<InterpreterValue> args,
     return Interpret(state, callee.getRegion(), args);
   }
 
-  void* sym = dlsym(RTLD_DEFAULT, callee.getSymName().str().c_str());
-  if (sym == nullptr) {
-    state.AddFailure("callee not found");
+  // Use the new FindSymbolInProcess function
+  absl::StatusOr<void*> symbol_status =
+      FindSymbolInProcess(callee.getSymName().str());
+  if (!symbol_status.ok()) {
+    state.AddFailure(symbol_status.status().message());
     return {};
   }
+  void* sym = *symbol_status;  // Retrieve the symbol pointer
 
   InterpreterValue result;
   if (TryCall<float, float>(sym, callee, args, result) ||
@@ -109,7 +112,6 @@ llvm::SmallVector<InterpreterValue> Call(MutableArrayRef<InterpreterValue> args,
       TryCall<double, double, double>(sym, callee, args, result)) {
     return {result};
   }
-
   state.AddFailure("unsupported call target");
   return {};
 }
