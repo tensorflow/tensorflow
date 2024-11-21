f34d1d513b6b3b1c51d77adb7eece7e5b8271689
Frederik Gossen <frgossen@google.com>
2024-11-20 16:32:57 -0800

    [Cleanup] Use HloPredicateIs(Not)Op in dot_normalizer.cc
    PiperOrigin-RevId: 698561890

--- third_party/xla/xla/service/gpu/transforms/cudnn_fused_conv_rewriter.cc
+++ third_party/xla/xla/service/gpu/transforms/cudnn_fused_conv_rewriter.cc
@@ -67,7 +67,7 @@ namespace {
 namespace m = match;
 
 bool IsConvCustomCall(const HloInstruction* instr) {
-  return instr->opcode() == HloOpcode::kCustomCall &&
+  return HloPredicateIsOp<HloOpcode::kCustomCall>(instr) &&
          (instr->custom_call_target() == kCudnnConvForwardCallTarget ||
           instr->custom_call_target() ==
               kCudnnConvBiasActivationForwardCallTarget);
@@ -161,7 +161,7 @@ bool IsLosslesslyConvertibleTo(const HloInstruction* instr,
                                                instr->shape().element_type());
   }
 
-  if (instr->opcode() == HloOpcode::kConstant) {
+  if (HloPredicateIsOp<HloOpcode::kConstant>(instr)) {
     if (!instr->shape().IsArray()) {
       return false;
     }
@@ -183,9 +183,8 @@ bool IsLosslesslyConvertibleTo(const HloInstruction* instr,
     return instr->literal() == *converted2;
   }
 
-  if (instr->opcode() == HloOpcode::kBroadcast ||
-      instr->opcode() == HloOpcode::kReshape ||
-      instr->opcode() == HloOpcode::kTranspose) {
+  if (HloPredicateIsOp<HloOpcode::kBroadcast, HloOpcode::kReshape,
+                       HloOpcode::kTranspose>(instr)) {
     return IsLosslesslyConvertibleTo(instr->operand(0), dst_ty);
   }
 
@@ -476,7 +475,7 @@ bool AppliesMaxReduce(HloInstruction* op) {
          ShapeUtil::IsScalar(op->operand(1)->shape()) &&
          op->operand(1)->IsConstant() &&
          op->operand(1)->literal().GetAsDouble({}) <= 0. &&
-         reduce_comp_root->opcode() == HloOpcode::kMaximum &&
+         HloPredicateIsOp<HloOpcode::kMaximum>(reduce_comp_root) &&
          reduce_comp_root->operand(0)->opcode() == HloOpcode::kParameter &&
          reduce_comp_root->operand(1)->opcode() == HloOpcode::kParameter;
 }
@@ -728,10 +727,11 @@ absl::StatusOr<bool> F8GraphConv(HloComputation* comp,
           CaptureConvGraph(
               instr, convolution, wide_input, wide_filter, input_scale,
               filter_scale,
-              input_scale_op ? input_scale_op->opcode() == HloOpcode::kMultiply
-                             : false,
+              input_scale_op
+                  ? HloPredicateIsOp<HloOpcode::kMultiply>(input_scale_op)
+                  : false,
               filter_scale_op
-                  ? filter_scale_op->opcode() == HloOpcode::kMultiply
+                  ? HloPredicateIsOp<HloOpcode::kMultiply>(filter_scale_op)
                   : false));
       TF_ASSIGN_OR_RETURN(auto gpu_config,
                           convolution->backend_config<GpuBackendConfig>());
@@ -833,15 +833,16 @@ absl::StatusOr<bool> FuseBiasOrSideInput(HloComputation* comp) {
     PrimitiveType bias_ty =
         primitive_util::IsFloatingPointType(conv_ty) ? conv_ty : F32;
     bool addend_may_be_rank1_bias =
-        addend->opcode() == HloOpcode::kBroadcast &&
+        HloPredicateIsOp<HloOpcode::kBroadcast>(addend) &&
         addend->dimensions().size() == 1 &&
         addend->dimensions(0) ==
             conv->convolution_dimension_numbers().output_feature_dimension() &&
         IsLosslesslyConvertibleTo(addend, bias_ty);
 
-    bool addend_may_be_rank0_bias = addend->opcode() == HloOpcode::kBroadcast &&
-                                    addend->dimensions().empty() &&
-                                    IsLosslesslyConvertibleTo(addend, bias_ty);
+    bool addend_may_be_rank0_bias =
+        HloPredicateIsOp<HloOpcode::kBroadcast>(addend) &&
+        addend->dimensions().empty() &&
+        IsLosslesslyConvertibleTo(addend, bias_ty);
 
     absl::InlinedVector<HloInstruction*, 4> new_operands(
         conv->operands().begin(), conv->operands().end());
@@ -916,8 +917,8 @@ absl::StatusOr<bool> FuseSideInputAlpha(HloComputation* comp) {
     // alpha is f32 except for f64 convs, where it's f64.  See
     // https://docs.nvidia.com/deeplearning/cudnn/api/index.html#cudnnConvolutionBiasActivationForward
     HloInstruction* before_reshape = side_input;
-    while (before_reshape->opcode() == HloOpcode::kReshape ||
-           before_reshape->opcode() == HloOpcode::kTranspose) {
+    while (HloPredicateIsOp<HloOpcode::kReshape, HloOpcode::kTranspose>(
+        before_reshape)) {
       before_reshape = before_reshape->mutable_operand(0);
     }
 
@@ -970,8 +971,8 @@ absl::StatusOr<bool> FuseSideInputAlpha(HloComputation* comp) {
           if (instr == before_reshape) {
             return base;
           }
-          CHECK(instr->opcode() == HloOpcode::kReshape ||
-                instr->opcode() == HloOpcode::kTranspose)
+          CHECK(HloPredicateIsOp<HloOpcode::kReshape, HloOpcode::kTranspose>(
+              instr))
               << "Must be reshape or transpose: " << instr->ToString();
           return comp->AddInstruction(instr->CloneWithNewOperands(
               instr->shape(), {clone(instr->operand(0))}));
