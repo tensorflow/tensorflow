65e4fb1885a102b8e687db3127c3bccdca570c6b
Matthias Kramm <kramm@google.com>
2024-11-21 12:03:57 -0800

    Make the implementation of GetXlaPjrtTpuClient more similar to how Jax uses PJRT.
    PiperOrigin-RevId: 698867902

--- third_party/xla/xla/pjrt/plugin/xla_tpu/xla_tpu_pjrt_client.cc
+++ third_party/xla/xla/pjrt/plugin/xla_tpu/xla_tpu_pjrt_client.cc
@@ -17,36 +17,15 @@ limitations under the License.
 
 #include <memory>
 
-#include "absl/log/check.h"
-#include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/string_view.h"
-#include "xla/pjrt/c/pjrt_c_api.h"
-#include "xla/pjrt/c/pjrt_c_api_tpu.h"
 #include "xla/pjrt/pjrt_c_api_client.h"
-#include "xla/pjrt/pjrt_client.h"
-#include "tsl/platform/statusor.h"
-
-namespace xla {
 
 const char kTpuPjrtName[] = "tpu";
 
+namespace xla {
+
 absl::StatusOr<std::unique_ptr<PjRtClient>> GetXlaPjrtTpuClient() {
-  const PJRT_Api* tpu_c_api = GetPjrtApi();
-  if (!tpu_c_api) {
-    return absl::InternalError("Failed to get PjrtApi");
-  }
-
-  TF_ASSIGN_OR_RETURN(std::unique_ptr<PjRtClient> tpu_client,
-                      xla::WrapClientAroundCApi(tpu_c_api));
-
-  if (tpu_client->platform_name() != kTpuPjrtName) {
-    return absl::InternalError(
-        absl::StrCat("Expected TPU client, got ", tpu_client->platform_name()));
-  }
-
-  return tpu_client;
+  return GetCApiClient(kTpuPjrtName);
 }
 
 }  // namespace xla
