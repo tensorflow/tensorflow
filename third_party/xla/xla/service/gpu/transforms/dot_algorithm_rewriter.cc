d70ee79b567aaf2ac530f01ed5ebd1f8f3dd924f
Ilya Tikhonovskiy <loislo@google.com>
2024-11-22 06:39:13 -0800

    [XLA:GPU] Fix a bug in dot_algorithm_rewriter.
    The low_f32 should be rounded to bf16 instead of truncation.
    
    PiperOrigin-RevId: 699154452

--- third_party/xla/xla/service/gpu/transforms/dot_algorithm_rewriter.cc
+++ third_party/xla/xla/service/gpu/transforms/dot_algorithm_rewriter.cc
@@ -80,9 +80,9 @@ std::tuple<HloInstruction*, HloInstruction*, HloInstruction*> Split3xToBF16(
   HloInstruction* high_f32_t = Truncate(f32_param);
   HloInstruction* mid_f32 = Sub(f32_param, high_f32_t);
   HloInstruction* mid_f32_t = Truncate(mid_f32);
-  HloInstruction* low_f32_t = Truncate(Sub(mid_f32, mid_f32_t));
+  HloInstruction* low_f32 = Sub(mid_f32, mid_f32_t);
   return std::make_tuple(RoundToBF16(high_f32_t), RoundToBF16(mid_f32_t),
-                         RoundToBF16(low_f32_t));
+                         RoundToBF16(low_f32));
 }
 
 // If lhs is 1.0, we will have lhs_high = 1.0 and lhs_low = 0.0.
