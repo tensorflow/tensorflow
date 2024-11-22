68fddf70082eac1485c14a9b48d319096bcd286a
gaikwadrahul8 <115997457+gaikwadrahul8@users.noreply.github.com>
2024-11-22 21:04:36 +0530

    Update graph_info.h to avoid warning: multi-line comment [-Wcomment]

--- tensorflow/lite/graph_info.h
+++ tensorflow/lite/graph_info.h
@@ -124,7 +124,7 @@ using ControlEdges = std::vector<ControlEdge>;
 // (Example: with `greedily`, `control_edges.empty()`, and `nodes_to_partition
 // == {2, 3}`, the graph
 //
-//                    /------------\
+//                    /------------\ // Adjusted to avoid warnings
 //                    |            v
 // 0 --> 1 --> 2* --> 3*     4 --> 5
 //       |                   ^
@@ -136,7 +136,7 @@ using ControlEdges = std::vector<ControlEdge>;
 // With an additional control dependency `control_edges == {{3, 4}}` (notated
 // '==>'), execution of node 4 requires prior execution of node 3:
 //
-//                    /------------\
+//                    /------------\ // Adjusted to avoid warnings
 //                    |            v
 // 0 --> 1 --> 2* --> 3* ==> 4 --> 5
 //       |                   ^
