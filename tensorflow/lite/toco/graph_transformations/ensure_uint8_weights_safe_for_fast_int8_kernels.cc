a0cf87f86bb0c113ce00f808f7bd1c06b91c9296
A. Unique TensorFlower <gardener@tensorflow.org>
2024-11-24 19:53:48 -0800

    Automated Code Change
    PiperOrigin-RevId: 699819853

--- tensorflow/lite/toco/graph_transformations/ensure_uint8_weights_safe_for_fast_int8_kernels.cc
+++ tensorflow/lite/toco/graph_transformations/ensure_uint8_weights_safe_for_fast_int8_kernels.cc
@@ -12,9 +12,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <cstddef>
+#include <iostream>
 #include <memory>
+#include <ostream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #include "absl/status/status.h"
--- tensorflow/lite/toco/graph_transformations/remove_tensorflow_assert.cc
+++ tensorflow/lite/toco/graph_transformations/remove_tensorflow_assert.cc
@@ -12,8 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <cstddef>
 #include <memory>
-#include <string>
 #include <vector>
 
 #include "absl/status/status.h"
