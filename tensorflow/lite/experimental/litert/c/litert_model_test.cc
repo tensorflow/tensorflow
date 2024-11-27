b96e7c3f91242ff5964f5211b737d4ef36ab56c8
A. Unique TensorFlower <gardener@tensorflow.org>
2024-11-27 00:58:23 -0800

    Automated Code Change
    PiperOrigin-RevId: 700600251

--- tensorflow/lite/experimental/litert/c/litert_model_test.cc
+++ tensorflow/lite/experimental/litert/c/litert_model_test.cc
@@ -21,6 +21,7 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "tensorflow/lite/experimental/litert/c/litert_common.h"
 #include "tensorflow/lite/experimental/litert/c/litert_op_code.h"
