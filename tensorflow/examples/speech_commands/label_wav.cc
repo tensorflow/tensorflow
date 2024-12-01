788579ed1d13e4164968c9565dbf8f38bd332123
A. Unique TensorFlower <gardener@tensorflow.org>
2024-11-30 18:08:57 -0800

    Automated Code Change
    PiperOrigin-RevId: 701585430

--- tensorflow/examples/speech_commands/label_wav.cc
+++ tensorflow/examples/speech_commands/label_wav.cc
@@ -13,7 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <algorithm>
+#include <cstdint>
 #include <fstream>
+#include <memory>
+#include <string>
+#include <utility>
 #include <vector>
 
 #include "absl/status/status.h"
