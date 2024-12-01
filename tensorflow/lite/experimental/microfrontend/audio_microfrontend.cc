25c52932df0ffa17bcdc1512c9e819c9c7b39e0b
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-01 09:01:57 -0800

    Automated Code Change
    PiperOrigin-RevId: 701712886

--- tensorflow/lite/experimental/microfrontend/audio_microfrontend.cc
+++ tensorflow/lite/experimental/microfrontend/audio_microfrontend.cc
@@ -12,6 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
 #include "flatbuffers/flexbuffers.h"  // from @flatbuffers
 #include "tensorflow/lite/c/c_api_types.h"
 #include "tensorflow/lite/c/common.h"
