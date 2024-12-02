e464273d623629eee078ccc2ffafdc1f5202d578
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-02 10:12:03 -0800

    Add static identifier name to LatencyHidingScheduler
    PiperOrigin-RevId: 702007060

--- third_party/xla/xla/service/latency_hiding_scheduler.h
+++ third_party/xla/xla/service/latency_hiding_scheduler.h
@@ -1107,7 +1107,8 @@ class LatencyHidingScheduler : public HloModulePass {
         async_tracker_(std::move(async_tracker)),
         scheduler_core_(std::move(scheduler_core)),
         shape_size_bytes_(shape_size_bytes) {}
-  absl::string_view name() const override { return "latency-hiding-scheduler"; }
+  constexpr static absl::string_view kName = "latency-hiding-scheduler";
+  absl::string_view name() const override { return kName; }
 
   // Returns some printable statistics about the latency hiding for
   // operations that can run in parallel to help evaluating the performance of
