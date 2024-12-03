118f41980fc3144ee8647106419e181df9fc6a20
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-03 12:34:05 -0800

    Check whether profile info is empty to determine if the module is using profiles.
    PiperOrigin-RevId: 702432829

--- third_party/xla/xla/hlo/ir/hlo_module.h
+++ third_party/xla/xla/hlo/ir/hlo_module.h
@@ -649,9 +649,7 @@ class HloModule {
   }
 
   bool has_module_autofdo_profiles() const {
-    return !autofdo_profile_keys_.empty() &&
-           // TODO(b/368119768): Remove this check once we rollout Flagnet.
-           !autofdo_profile_keys_.contains(HloModuleProto::INVALID);
+    return !profile_info_list_.empty();
   }
 
   void set_relative_speedup(double relative_speedup) {
