2d0cec4b889497e108b8a9139676fcbd4a2b7890
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-04 00:29:10 -0800

    Automated Code Change
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/20028 from openxla:cleanup b4af0bc14794b381a8c21dfe9e3d5b5f6e7ae6f7
    PiperOrigin-RevId: 702614874

--- third_party/xla/xla/backends/cpu/testlib/kernel_runner_test.cc
+++ third_party/xla/xla/backends/cpu/testlib/kernel_runner_test.cc
@@ -27,6 +27,7 @@ limitations under the License.
 #include "xla/shape.h"
 #include "xla/shape_util.h"
 #include "xla/stream_executor/launch_dim.h"
+#include "xla/xla_data.pb.h"
 #include "tsl/platform/statusor.h"
 #include "tsl/platform/test.h"
 
