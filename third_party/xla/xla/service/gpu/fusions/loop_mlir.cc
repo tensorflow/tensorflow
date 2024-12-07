4af022ae2d6a03a2f9b7a9becac87b5d38fb50bc
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-06 21:28:40 -0800

    Automated Code Change
    PiperOrigin-RevId: 703711927

--- third_party/xla/xla/service/gpu/fusions/loop_mlir.cc
+++ third_party/xla/xla/service/gpu/fusions/loop_mlir.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "xla/service/gpu/fusions/loop_mlir.h"
 
+#include <cstdint>
 #include <iterator>
 #include <optional>
 #include <vector>
