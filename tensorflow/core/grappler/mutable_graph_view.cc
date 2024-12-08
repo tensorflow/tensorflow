ee3cc5a6f1c80cc49c150b5cd98eed0c433f58d2
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-08 07:56:45 -0800

    Automated Code Change
    PiperOrigin-RevId: 704012396

--- tensorflow/core/grappler/mutable_graph_view.cc
+++ tensorflow/core/grappler/mutable_graph_view.cc
@@ -16,14 +16,23 @@ limitations under the License.
 #include "tensorflow/core/grappler/mutable_graph_view.h"
 
 #include <algorithm>
+#include <functional>
 #include <utility>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "absl/strings/substitute.h"
+#include "absl/types/span.h"
+#include "third_party/protobuf/message_lite.h"
+#include "third_party/protobuf/text_format.h"
 #include "tensorflow/core/framework/function.h"
+#include "tensorflow/core/framework/function.pb.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/graph/graph.h"
