442bc397e50b7c4bfabe4f53114e7f40dc047755
Henning Becker <hebecker@google.com>
2024-12-09 02:29:48 -0800

    Reverts a27025f94345d873bc9e4718b4afc45651ea2db2
    PiperOrigin-RevId: 704202653

--- third_party/xla/xla/service/gpu/nvptx_compiler.h
+++ third_party/xla/xla/service/gpu/nvptx_compiler.h
@@ -17,16 +17,11 @@ limitations under the License.
 #define XLA_SERVICE_GPU_NVPTX_COMPILER_H_
 
 #include <cstdint>
-#include <string>
-#include <utility>
+#include <memory>
 #include <vector>
 
-#include "absl/base/thread_annotations.h"
-#include "absl/container/node_hash_map.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/string_view.h"
-#include "absl/synchronization/mutex.h"
 #include "llvm/IR/Module.h"
 #include "xla/autotune_results.pb.h"
 #include "xla/hlo/analysis/hlo_dataflow_analysis.h"
@@ -37,7 +32,7 @@ limitations under the License.
 #include "xla/service/gpu/gpu_compiler.h"
 #include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/service/hlo_module_config.h"
-#include "xla/stream_executor/cuda/ptx_linking_method.h"
+#include "xla/stream_executor/cuda/compilation_provider.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/stream_executor/dnn.h"
 #include "xla/stream_executor/semantic_version.h"
@@ -53,7 +48,12 @@ void WarnIfBadDriverJITVersion();
 // NVPTXCompiler generates efficient GPU executables for NVPTX target.
 class NVPTXCompiler : public GpuCompiler {
  public:
-  NVPTXCompiler();
+  // DebugOptions are used to determine which CompilationProvider to use.
+  explicit NVPTXCompiler(const DebugOptions& debug_options);
+
+  // The same as above, but uses the default DebugOptions determined by
+  // flags.
+  explicit NVPTXCompiler();
 
   absl::Status OptimizeHloConvolutionCanonicalization(
       HloModule* hlo_module, se::GpuComputeCapability gpu_version,
@@ -104,87 +104,8 @@ class NVPTXCompiler : public GpuCompiler {
       std::vector<std::vector<uint8_t>> modules,
       const DebugOptions& debug_options) override;
 
-  absl::StatusOr<stream_executor::PtxLinkingMethod> ChooseLinkingMethod(
-      const DebugOptions& debug_options,
-      const stream_executor::DeviceDescription& device_description);
-
-  // Tries to compile the given ptx string to cubin.  Returns a vector with the
-  // compiled cubin if compilation succeeded.
-  absl::StatusOr<std::vector<uint8_t>> CompileGpuAsmOrGetCachedResult(
-      const std::string& ptx, se::CudaComputeCapability cc,
-      const HloModuleConfig& hlo_module_config, absl::string_view module_name,
-      bool relocatable, const CompileOptions& options);
-
-  struct CompilationCacheFlags {
-    template <typename H>
-    friend H AbslHashValue(H h, const CompilationCacheFlags& flags) {
-      return H::combine(std::move(h),
-                        flags.filter_kernels_spilling_registers_on_autotuning);
-    }
-
-    friend bool operator==(const CompilationCacheFlags& a,
-                           const CompilationCacheFlags& b) {
-      return a.filter_kernels_spilling_registers_on_autotuning ==
-             b.filter_kernels_spilling_registers_on_autotuning;
-    }
-
-    bool filter_kernels_spilling_registers_on_autotuning;
-  };
-
-  // The compilation_cache_ map is a cache from {ptx string, cc_major, cc_minor}
-  // -> cubin so we don't recompile the same ptx twice.  This is important for
-  // some interactive workflows.  (We also cache at the HLO level, but sometimes
-  // we can't realize that two modules are the same until we lower to ptx.)
-  //
-  // Compilation of distinct PTX happens in parallel. If more than one thread
-  // attempts to compile the same PTX, the fist thread to obtain
-  // cache_value_->mutex_ performs the compilation. The rest wait() on
-  // cache_value_->compilation_done_cv_ until the compilation is done.
-  //
-  // If compiling the ptx fails, we return an empty cubin, cross our fingers,
-  // and leave compilation up to the driver.
-  struct CompilationCacheKey {
-    CompilationCacheKey(std::string ptx, int cc_major, int cc_minor,
-                        bool relocatable, CompilationCacheFlags flags)
-        : ptx(std::move(ptx)),
-          cc_major(cc_major),
-          cc_minor(cc_minor),
-          relocatable(relocatable),
-          flags(std::move(flags)) {}
-
-    template <typename H>
-    friend H AbslHashValue(H h, const CompilationCacheKey& key) {
-      return H::combine(std::move(h), key.ptx, key.cc_major, key.cc_minor,
-                        key.relocatable, key.flags);
-    }
-
-    friend bool operator==(const CompilationCacheKey& a,
-                           const CompilationCacheKey& b) {
-      return a.cc_major == b.cc_major && a.cc_minor == b.cc_minor &&
-             a.ptx == b.ptx && a.relocatable == b.relocatable &&
-             a.flags == b.flags;
-    }
-
-    std::string ptx;
-    int cc_major;
-    int cc_minor;
-    bool relocatable;
-    CompilationCacheFlags flags;
-  };
-
-  struct CompilationCacheValue {
-    bool compilation_done = false;
-    absl::StatusOr<std::vector<uint8_t>> maybe_cubin;
-    // mutex and condition variable to serialize compilation completing.
-    absl::Mutex mutex;
-    absl::CondVar compilation_done_cv;
-  };
-
-  // Don't even think about switching this to flat_hash_map; iterator stability
-  // is critical here.
-  absl::Mutex mutex_;
-  absl::node_hash_map<CompilationCacheKey, CompilationCacheValue>
-      compilation_cache_ ABSL_GUARDED_BY(mutex_);
+  absl::StatusOr<std::unique_ptr<se::cuda::CompilationProvider>>
+      compilation_provider_;
 
   NVPTXCompiler(const NVPTXCompiler&) = delete;
   NVPTXCompiler& operator=(const NVPTXCompiler&) = delete;
