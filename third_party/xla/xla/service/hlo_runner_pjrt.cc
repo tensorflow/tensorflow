698657e26534dd99defe012f1258d1594c36ad41
Niklas Vangerow <nikv@google.com>
2024-12-10 13:47:52 -0800

    Respect DeviceAssignment in HloRunnerPjRt.
    `DeviceAssignment` maps a (replica, computation) tuple to a physical device
    index. We must respect this mapping. Prior to this patch we mapped replicas
    directly onto devices with the same index.
    
    PiperOrigin-RevId: 704833654

--- third_party/xla/xla/service/hlo_runner_pjrt.cc
+++ third_party/xla/xla/service/hlo_runner_pjrt.cc
@@ -459,13 +459,16 @@ absl::StatusOr<std::vector<Literal>> HloRunnerPjRt::ExecuteReplicatedImpl(
     std::function<const Literal*(int64_t, int64_t)> argument_provider,
     const ReplicatedExecuteOptions& options,
     DeviceAssignment* device_assignment) {
+  const int64_t num_computations = device_assignment->computation_count();
   absl::Span<PjRtDevice* const> devices = pjrt_client_->devices();
 
   std::vector<std::vector<std::unique_ptr<PjRtBuffer>>> argument_buffer_slices;
   argument_buffer_slices.reserve(pjrt_client_->addressable_device_count());
 
   for (int64_t i = 0; i < options.num_replicas; ++i) {
-    PjRtDevice* device_ptr = devices[i];
+    const int64_t device_index =
+        (*device_assignment)(i / num_computations, i % num_computations);
+    PjRtDevice* device_ptr = devices[device_index];
 
     // Transfer literals to device.
     const int64_t argument_count = argument_count_provider(i);
