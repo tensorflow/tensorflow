67d8f1616566242fd54f17c6d33c8933bf00df39
Milica Makevic <Milica.Makevic@amd.com>
2024-12-11 13:46:25 +0000

    Use new implementation for buffer comparison (https://github.com/tensorflow/tensorflow/commit/04e1d35c344b36d1391718219fdaff237a56f9ff)

--- third_party/xla/xla/service/gpu/buffer_comparator.cu.cc
+++ third_party/xla/xla/service/gpu/buffer_comparator.cu.cc
@@ -51,96 +51,15 @@ __device__ __inline__ auto Canonicalize(double elem) {
   return elem;
 }
 
-#if TENSORFLOW_USE_ROCM && TF_ROCM_VERSION >= 60200
-
-__global__ void xla_fp8_e4m3fnuz_comparison(__hip_fp8_storage_t* buffer_a,
-                                            __hip_fp8_storage_t* buffer_b,
-                                            float rel_error_threshold,
-                                            uint64_t buffer_length,
-                                            int* mismatch_count) {
-#if defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__) 
-// NOTE: according to amd_hip_fp8.h, GFX1200 and GFX1201 support ocp __hip_fp8_e4m3 
-// but not __hip_fp8_e4m3_fnuz
+template <typename T>
+__global__ void xla_fp_comparison(T* buffer_a, T* buffer_b,
+                                  float rel_error_threshold,
+                                  uint64_t buffer_length, int* mismatch_count) {
 
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  if (idx >= buffer_length) return;
-  __hip_fp8_e4m3_fnuz elem_a_fp8, elem_b_fp8;
-  elem_a_fp8.__x = buffer_a[idx];
-  elem_b_fp8.__x = buffer_b[idx];
-  float elem_a = static_cast<float>(elem_a_fp8);
-  float elem_b = static_cast<float>(elem_b_fp8);
-  elem_a = Canonicalize(elem_a);
-  elem_b = Canonicalize(elem_b);
-  if (isnan(elem_a) && isnan(elem_b)) return;
-
-  float rel_error = abs(elem_a - elem_b) / (max(abs(elem_a), abs(elem_b)) + 1);
-
-  if (rel_error > rel_error_threshold || isnan(rel_error))
-    atomicAdd(mismatch_count, 1);
-#else
-  // on unsupported architectures, this should not / cannot be used!
-  atomicAdd(mismatch_count, 1); 
-#endif
-}
-
-__global__ void xla_fp8_e5m2fnuz_comparison(__hip_fp8_storage_t* buffer_a,
-                                            __hip_fp8_storage_t* buffer_b,
-                                            float rel_error_threshold,
-                                            uint64_t buffer_length,
-                                            int* mismatch_count) {
-#if defined(__gfx940__) || defined(__gfx941__) || defined(__gfx942__) 
-  int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  if (idx >= buffer_length) return;
-  __hip_fp8_e5m2_fnuz elem_a_fp8, elem_b_fp8;
-  elem_a_fp8.__x = buffer_a[idx];
-  elem_b_fp8.__x = buffer_b[idx];
-  float elem_a = static_cast<float>(elem_a_fp8);
-  float elem_b = static_cast<float>(elem_b_fp8);
-  elem_a = Canonicalize(elem_a);
-  elem_b = Canonicalize(elem_b);
-  if (isnan(elem_a) && isnan(elem_b)) return;
-
-  float rel_error = abs(elem_a - elem_b) / (max(abs(elem_a), abs(elem_b)) + 1);
-
-  if (rel_error > rel_error_threshold || isnan(rel_error))
-    atomicAdd(mismatch_count, 1);
-#else
-  // on unsupported architectures, this should not / cannot be used!
-  atomicAdd(mismatch_count, 1); 
-#endif
-}
-
-#endif  // TENSORFLOW_USE_ROCM && TF_ROCM_VERSION >= 60200
-
-__global__ void xla_fp16_comparison(__half* buffer_a, __half* buffer_b,
-                                    float rel_error_threshold,
-                                    uint64_t buffer_length,
-                                    int* mismatch_count) {
-  int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  if (idx >= buffer_length) return;
-  float elem_a = __half2float(buffer_a[idx]);
-  float elem_b = __half2float(buffer_b[idx]);
-  elem_a = Canonicalize(elem_a);
-  elem_b = Canonicalize(elem_b);
-  if (isnan(elem_a) && isnan(elem_b)) return;
-
-  float rel_error = abs(elem_a - elem_b) / (max(abs(elem_a), abs(elem_b)) + 1);
-
-  if (rel_error > rel_error_threshold || isnan(rel_error))
-    atomicAdd(mismatch_count, 1);
-}
-
-__global__ void xla_fp32_comparison(float* buffer_a, float* buffer_b,
-                                    float rel_error_threshold,
-                                    uint64_t buffer_length,
-                                    int* mismatch_count) {
-  int idx = threadIdx.x + blockIdx.x * blockDim.x;
-  if (idx >= buffer_length) return;
-  float elem_a = buffer_a[idx];
-  float elem_b = buffer_b[idx];
-  if (isnan(elem_a) && isnan(elem_b)) return;
-  if (isinf(elem_a) && isinf(elem_b) && signbit(elem_a) == signbit(elem_b)) return;
-
+  if (idx >= buffer_length) {
+    return;
+  }
   auto elem_a = Canonicalize(buffer_a[idx]);
   auto elem_b = Canonicalize(buffer_b[idx]);
 
