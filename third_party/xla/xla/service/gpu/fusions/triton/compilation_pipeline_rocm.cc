c22f471b1f71c3d9d40e1a1a11ad41fa1d519855
Kyle Lucke <klucke@google.com>
2024-12-11 17:05:02 -0800

    Split ROCm-specific backend calls into their own targets.
    PiperOrigin-RevId: 705290149

--- third_party/xla/xla/service/gpu/fusions/triton/compilation_pipeline_rocm.cc
+++ third_party/xla/xla/service/gpu/fusions/triton/compilation_pipeline_rocm.cc
@@ -25,7 +25,7 @@ limitations under the License.
 #include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
-#include "xla/service/gpu/llvm_gpu_backend/gpu_backend_lib.h"
+#include "xla/service/gpu/llvm_gpu_backend/amdgpu_backend.h"
 #include "xla/service/gpu/matmul_utils.h"
 #include "xla/service/hlo_module_config.h"
 #include "xla/stream_executor/device_description.h"
