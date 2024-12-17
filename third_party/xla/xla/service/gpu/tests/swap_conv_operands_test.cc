e70107dab67f830d98df1b4067bc4c9a22244409
Benjamin Chetioui <bchetioui@google.com>
2024-12-17 04:12:50 -0800

    [XLA:GPU] Force cuDNN convolutions to be assigned a `NHWC` layout from Hopper.
    This is the best way to use convolutions on tensor cores, according to
    [NVIDIA's documentation](https://docs.nvidia.com/deeplearning/performance/dl-performance-convolutional/index.html#tensor-layout).
    
    Extending that to pre-Hopper GPUs is left for a future change. We also filter
    out a few cases that are simply not supported by cuDNN.
    
    PiperOrigin-RevId: 707037402

--- third_party/xla/xla/service/gpu/tests/swap_conv_operands_test.cc
+++ third_party/xla/xla/service/gpu/tests/swap_conv_operands_test.cc
@@ -13,8 +13,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <memory>
+#include <variant>
+
+#include "absl/status/statusor.h"
 #include "xla/error_spec.h"
 #include "xla/service/gpu/tests/gpu_codegen_test.h"
+#include "xla/stream_executor/device_description.h"
+#include "tsl/platform/statusor.h"
 #include "tsl/platform/test.h"
 
 // TODO(b/210165681): The tests in this file are fragile to HLO op names.
@@ -24,11 +30,20 @@ namespace gpu {
 
 namespace {
 
-class SwapConvOperandsTest : public GpuCodegenTest {};
+class SwapConvOperandsTest : public GpuCodegenTest {
+ public:
+  absl::StatusOr<se::GpuComputeCapability> GpuComputeCapability() {
+    TF_ASSIGN_OR_RETURN(
+        std::unique_ptr<se::DeviceDescription> device_description,
+        GetTestPlatform()->DescriptionForDevice(0));
+
+    return device_description->gpu_compute_capability();
+  }
+};
 
 // Here, we swap the operands of a convolution to avoid the performance penalty
 // associated with convolutions with large padding. This tests that the operands
-// are swapped in this case, and that the emitted convolution is sucessfully
+// are swapped in this case, and that the emitted convolution is successfully
 // lowered to a cuDNN custom-call.
 TEST_F(SwapConvOperandsTest, LargePadding) {
   const char* hlo_text = R"(
@@ -42,10 +57,22 @@ ENTRY swap_conv {
 }
 )";
 
-  MatchOptimizedHloWithShapes(hlo_text,
-                              R"(
+  TF_ASSERT_OK_AND_ASSIGN(se::GpuComputeCapability gpu_compute_capability,
+                          GpuComputeCapability());
+
+  if (std::get_if<se::CudaComputeCapability>(&gpu_compute_capability)
+          ->IsAtLeastHopper()) {
+    MatchOptimizedHloWithShapes(hlo_text,
+                                R"(
+// CHECK: [[cudnn_conv_1_0:%[^ ]+]] = (f32[1,32,32,128]{3,2,1,0}, u8[{{.*}}]{0}) custom-call(f32[1,30,30,512]{3,2,1,0} {{[^ ]+}}, f32[128,3,3,512]{3,2,1,0} {{[^ ]+}}), window={size=3x3 pad=2_2x2_2 rhs_reversal=1x1}, dim_labels=b01f_o01i->b01f, custom_call_target="__cudnn$convForward"
+    )");
+  } else {
+    MatchOptimizedHloWithShapes(hlo_text,
+                                R"(
 // CHECK: [[cudnn_conv_1_0:%[^ ]+]] = (f32[1,128,32,32]{3,2,1,0}, u8[{{.*}}]{0}) custom-call(f32[1,512,30,30]{3,2,1,0} [[fusion_1_1:%[^ ]+]], f32[128,512,3,3]{3,2,1,0} [[transpose_1_2:%[^ ]+]]), window={size=3x3 pad=2_2x2_2 rhs_reversal=1x1}, dim_labels=bf01_oi01->bf01, custom_call_target="__cudnn$convForward"
-      )");
+    )");
+  }
+
   EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-3, 1e-3}));
 }
 
@@ -62,10 +89,21 @@ ENTRY swap_conv {
 }
 )";
 
-  MatchOptimizedHloWithShapes(hlo_text,
-                              R"(
+  TF_ASSERT_OK_AND_ASSIGN(se::GpuComputeCapability gpu_compute_capability,
+                          GpuComputeCapability());
+
+  if (std::get_if<se::CudaComputeCapability>(&gpu_compute_capability)
+          ->IsAtLeastHopper()) {
+    MatchOptimizedHloWithShapes(hlo_text,
+                                R"(
+// CHECK: [[cudnn_conv_1_0:%[^ ]+]] = (f32[1,32,32,128]{3,2,1,0}, u8[{{[0-9]*}}]{0}) custom-call(f32[1,30,30,512]{3,2,1,0} {{[^ ]+}}, f32[128,3,3,512]{3,2,1,0} {{[^ ]+}}), window={size=3x3 pad=2_2x2_2 rhs_reversal=1x1}, dim_labels=b01f_o01i->b01f, custom_call_target="__cudnn$convForward"
+    )");
+  } else {
+    MatchOptimizedHloWithShapes(hlo_text,
+                                R"(
 // CHECK: [[cudnn_conv_1_0:%[^ ]+]] = (f32[1,128,32,32]{3,2,1,0}, u8[{{[0-9]*}}]{0}) custom-call(f32[1,512,30,30]{3,2,1,0} [[fusion_1_1:%[^ ]+]], f32[128,512,3,3]{3,2,1,0} [[transpose_1_2:%[^ ]+]]), window={size=3x3 pad=2_2x2_2 rhs_reversal=1x1}, dim_labels=bf01_oi01->bf01, custom_call_target="__cudnn$convForward"
-      )");
+    )");
+  }
   EXPECT_TRUE(RunAndCompare(hlo_text, ErrorSpec{1e-3, 1e-3}));
 }
 
