87f0c007f3cc4dde6db2563d0322a12f3bd6be04
Eugene Zhulenev <ezhulenev@google.com>
2024-12-18 20:43:03 -0800

    [xla:cpu] Add support for running with default pthreadpool
    PiperOrigin-RevId: 707764382

--- third_party/xla/xla/backends/cpu/runtime/xnnpack/xnn_threadpool.h
+++ third_party/xla/xla/backends/cpu/runtime/xnnpack/xnn_threadpool.h
@@ -24,7 +24,12 @@ namespace xla::cpu {
 // Returns true if the custom pthreadpool is enabled.
 bool IsCustomPthreadpoolEnabled();
 
-// Creates a `pthreadpool` that uses the given `runner` to execute work.
+// Returns the default per-process pthreadpool. If custom `pthreadpool` is
+// enabled it will return nullptr.
+pthreadpool_t DefaultPthreadpool();
+
+// Creates a `pthreadpool` that uses the given `runner` to execute work. If
+// custom `pthreadpool` is disabled it will kill the process.
 pthreadpool_t CreatePthreadpool(xla::cpu::ParallelLoopRunner* runner);
 
 // Returns the parallel loop runner associated with the given `pthreadpool`. If
