fbe8b07c5a8255c89f5352acb2876b09fe5790ce
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-19 03:57:52 -0800

    [XLA:TPU] Rollback Disable some optimization passes based on effort flag
    Reverts 16b5c0451fb851b68f90f398ee0fe11421793f75
    
    PiperOrigin-RevId: 707859058

--- third_party/xla/xla/service/all_reduce_simplifier.h
+++ third_party/xla/xla/service/all_reduce_simplifier.h
@@ -30,8 +30,7 @@ namespace xla {
 // replaced by a multiply with the replica count.
 class AllReduceSimplifier : public HloModulePass {
  public:
-  static constexpr absl::string_view kName = "all-reduce-simplifier";
-  absl::string_view name() const override { return kName; }
+  absl::string_view name() const override { return "all-reduce-simp"; }
 
   // Run all-reduce simplification on the given computation. Returns whether the
   // computation was changed.
