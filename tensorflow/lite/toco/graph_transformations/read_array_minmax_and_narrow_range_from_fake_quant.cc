9b4a4e3aae8c091530022c9d86ff6e6022d1fba8
A. Unique TensorFlower <gardener@tensorflow.org>
2024-12-22 21:14:00 -0800

    Automated Code Change
    PiperOrigin-RevId: 708941107

--- tensorflow/lite/toco/graph_transformations/read_array_minmax_and_narrow_range_from_fake_quant.cc
+++ tensorflow/lite/toco/graph_transformations/read_array_minmax_and_narrow_range_from_fake_quant.cc
@@ -12,10 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <algorithm>
+#include <cstddef>
 #include <memory>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #include "absl/status/status.h"
