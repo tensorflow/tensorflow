c86e0dcc05a72a3eca6c9c03f55629997e8221fc
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-01 17:50:02 -0800

    Automated Code Change
    PiperOrigin-RevId: 711273338

--- tensorflow/lite/toco/graph_transformations/resolve_space_to_batch_nd_attributes.cc
+++ tensorflow/lite/toco/graph_transformations/resolve_space_to_batch_nd_attributes.cc
@@ -26,9 +26,9 @@ limitations under the License.
 
 namespace toco {
 
-::tensorflow::Status ResolveSpaceToBatchNDAttributes::Run(Model* model,
-                                                          std::size_t op_index,
-                                                          bool* modified) {
+absl::Status ResolveSpaceToBatchNDAttributes::Run(Model* model,
+                                                  std::size_t op_index,
+                                                  bool* modified) {
   *modified = false;
   const auto op_it = model->operators.begin() + op_index;
   if (op_it->get()->type != OperatorType::kSpaceToBatchND)
