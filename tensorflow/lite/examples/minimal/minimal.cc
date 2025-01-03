f43fcc76ac77d44d3d53b7062fd9cffa3bf427f8
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-03 01:24:39 -0800

    Automated Code Change
    PiperOrigin-RevId: 711670493

--- tensorflow/lite/examples/minimal/minimal.cc
+++ tensorflow/lite/examples/minimal/minimal.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include <cstdio>
+#include <cstdlib>
+#include <memory>
 
 #include "tensorflow/lite/core/interpreter_builder.h"
 #include "tensorflow/lite/interpreter.h"
