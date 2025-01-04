964f8c93e6340471b397466cf804844498884aae
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-03 17:09:49 -0800

    Automated Code Change
    PiperOrigin-RevId: 711892017

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -108,7 +108,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 2096  // Updated: 2025/1/3
+#define TF_GRAPH_DEF_VERSION 2097  // Updated: 2025/1/4
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
