09ce373aca762c677b00051ef197f439191c449a
Toli Yevtushenko <anatoliyy@google.com>
2025-01-08 18:53:49 -0800

    Refactor collective_permute decomposer. Extract general purpose collective permute related methods to a cp_utils.
    PiperOrigin-RevId: 713493391

--- third_party/xla/xla/service/collective_permute_decomposer.cc
+++ third_party/xla/xla/service/collective_permute_decomposer.cc
@@ -15,7 +15,6 @@ limitations under the License.
 
 #include "xla/service/collective_permute_decomposer.h"
 
-#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <optional>
@@ -23,13 +22,10 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
-#include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_computation.h"
@@ -37,13 +33,12 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/collective_ops_utils.h"
+#include "xla/service/collective_permute_utils.h"
 #include "xla/service/gpu/backend_configs.pb.h"
-#include "xla/service/graphcycles/graphcycles.h"
 #include "xla/shape.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/xla_data.pb.h"
-#include "tsl/platform/errors.h"
 #include "tsl/platform/statusor.h"
 
 namespace xla {
@@ -51,36 +46,6 @@ namespace {
 
 using SourceTargetPair = std::pair<int64_t, int64_t>;
 using SourceTargetPairs = std::vector<SourceTargetPair>;
-
-// Returns true if the (source, target) relationship has a cycle.
-bool HasCycles(const SourceTargetPairs& pairs) {
-  // Build a direct graph to check for cycles in (source, target) relationship.
-  GraphCycles graph;
-
-  // Map replica numbers to graph node ids.
-  absl::flat_hash_map<int64_t, int32_t> replica_to_node_id;
-  auto get_node_id = [&](int64_t replica) {
-    auto it_and_inserted = replica_to_node_id.emplace(replica, -1);
-    auto it = it_and_inserted.first;
-    auto inserted = it_and_inserted.second;
-    if (inserted) {
-      // First time to see the replica, create a node for it.
-      it->second = graph.NewNode();
-    }
-    return it->second;
-  };
-
-  for (auto pair : pairs) {
-    int source = get_node_id(pair.first);
-    int target = get_node_id(pair.second);
-    VLOG(3) << "See source " << source << " -> target " << target;
-    if (!graph.InsertEdge(source, target)) {
-      VLOG(3) << "Detected cycles";
-      return true;
-    }
-  }
-  return false;
-}
 
 // Returns true if the CollectivePermute instruction should be transformed
 // to Send/Recv. We currently limit the transformation to CollectivePermute
@@ -99,7 +64,7 @@ bool ShouldDecompose(const HloCollectivePermuteInstruction& collective_permute,
   if (ShapeUtil::ByteSizeOf(result_shape) < threshold_in_bytes) {
     return false;
   }
-  return !HasCycles(collective_permute.source_target_pairs());
+  return !cp_utils::HasCycles(collective_permute.source_target_pairs());
 }
 
 // Returns true for a pipelineable collective-permute. As a simple heuristic,
@@ -119,11 +84,12 @@ struct CpWithDecomposedOps {
   SourceTargetPairs source_target_pairs;
 };
 
-// Decomposes a collective-permute and adds frontend attributes to record
-// pipeline decision. The present of the frontend attribute means that the
-// collective-permute will be pipelined and the value of the attribute
-// represents the runtime stream to execute the instruction. Without the
-// frontend attribute, the collective-permute will not be pipelined.
+// Decomposes a collective-permute into send, send-done, recv, recv-done.
+// Adds frontend attributes to record pipeline decision. The present of the
+// frontend attribute means that the collective-permute will be pipelined and
+// the value of the attribute represents the runtime stream to execute the
+// instruction. Without the frontend attribute, the collective-permute will not
+// be pipelined.
 absl::StatusOr<CpWithDecomposedOps> DecomposeCollectivePermute(
     HloCollectivePermuteInstruction* cp, HloComputation* computation,
     const std::string& pipeline_decision) {
@@ -136,33 +102,21 @@ absl::StatusOr<CpWithDecomposedOps> DecomposeCollectivePermute(
 
   const xla::FrontendAttributes& old_attributes = cp->frontend_attributes();
   xla::FrontendAttributes attributes;
-  std::string source_target_pairs_string =
-      "{" +
-      absl::StrJoin(cp->source_target_pairs(), ",",
-                    absl::PairFormatter(
-                        [](std::string* out, int64_t value) {
-                          absl::StrAppend(out, "{", value);
-                        },
-                        ",",
-                        [](std::string* out, int64_t value) {
-                          absl::StrAppend(out, value, "}");
-                        })) +
-      "}";
   attributes.mutable_map()->insert(old_attributes.map().begin(),
                                    old_attributes.map().end());
   (*attributes.mutable_map())[kSendRecvSourceTargetPairsAttr] =
-      source_target_pairs_string;
+      cp_utils::SourceTargetPairsString(*cp);
 
   HloInstruction* after_all =
       computation->AddInstruction(HloInstruction::CreateToken());
   HloInstruction* recv = computation->AddInstruction(HloInstruction::CreateRecv(
       data_shape, after_all, channel_id, /*is_host_transfer=*/false));
-  recv->add_frontend_attributes(attributes);
+  recv->set_frontend_attributes(attributes);
   recv->set_metadata(metadata);
 
   HloInstruction* send = computation->AddInstruction(HloInstruction::CreateSend(
       data, after_all, channel_id, /*is_host_transfer=*/false));
-  send->add_frontend_attributes(attributes);
+  send->set_frontend_attributes(attributes);
   send->set_metadata(metadata);
 
   HloInstruction* recv_done =
@@ -172,13 +126,12 @@ absl::StatusOr<CpWithDecomposedOps> DecomposeCollectivePermute(
       computation->AddInstruction(HloInstruction::CreateSendDone(
           send, channel_id, /*is_host_transfer=*/false));
 
-  // We will add control dependence to represent how we want to order Send/Recv
-  // and other collective operations. Here we only add the necessary control
-  // dependence to avoid optimization that can cause problems, in particular,
-  // to prevent fusion from fusing the computation of Send-data with the
-  // computation that requires the Recv-result.
-  TF_RETURN_IF_ERROR(send->AddControlDependencyTo(recv_done));
+  // Control dependencies are require to assure order of the instructions.
+  // To avoid deadlocks as the program runs on multiple devices, we need to
+  // assure that we initiate receival before initiating sending and that receive
+  // done is executed after send is initiated.
   TF_RETURN_IF_ERROR(recv->AddControlDependencyTo(send));
+  TF_RETURN_IF_ERROR(send->AddControlDependencyTo(recv_done));
 
   HloInstruction* recv_data = computation->AddInstruction(
       HloInstruction::CreateGetTupleElement(recv_done, 0));
@@ -199,46 +152,6 @@ absl::StatusOr<CpWithDecomposedOps> DecomposeCollectivePermute(
   return decomposed_cp;
 }
 
-// Returns true if the (source, target) pairs form a forward cycle with all
-// participants in the cycle, such as {{0,1},{1,2},{2,3},{3,0}}. We assume that
-// the (source, target) pairs are ordered via increasing source IDs, as they are
-// currently generated by SPMD partitioning.
-//
-bool IsForwardCycle(const SourceTargetPair& backedge,
-                    const SourceTargetPairs& others) {
-  int64_t num_pairs = others.size() + 1;
-  if (backedge.first != num_pairs - 1 || backedge.second != 0) {
-    return false;
-  }
-  for (int64_t i = 0; i < num_pairs - 1; ++i) {
-    const SourceTargetPair& pair = others[i];
-    if (pair.first != i || pair.second != i + 1) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Returns true if the (source, target) pairs form a backward cycle with all
-// participants in the cycle, such as {{0,3},{1,0},{2,1},{3,2}}. We assume that
-// the (source, target) pairs are ordered via increasing source IDs, as they are
-// currently generated by SPMD partitioning.
-//
-bool IsBackwardCycle(const SourceTargetPair& backedge,
-                     const SourceTargetPairs& others) {
-  int64_t num_pairs = others.size() + 1;
-  if (backedge.first != 0 || backedge.second != num_pairs - 1) {
-    return false;
-  }
-  for (int64_t i = 0; i < num_pairs - 1; ++i) {
-    const SourceTargetPair& pair = others[i];
-    if (pair.first != i + 1 || pair.second != i) {
-      return false;
-    }
-  }
-  return true;
-}
-
 // Checks whether the two collective-permutes for a forward cycle or a backward
 // cycle for pipelining. If the two collective-permutes form a cycle, returns
 // a pair of the collective-permutes with the one for the backward edge of the
@@ -250,15 +163,15 @@ CheckCyclePatterns(HloCollectivePermuteInstruction* cp0,
   const SourceTargetPairs& cp0_pairs = cp0->source_target_pairs();
   const SourceTargetPairs& cp1_pairs = cp1->source_target_pairs();
   if (cp0_pairs.size() == 1) {
-    if (IsForwardCycle(cp0_pairs.front(), cp1_pairs) ||
-        IsBackwardCycle(cp0_pairs.front(), cp1_pairs)) {
+    if (cp_utils::IsForwardCycle(cp0_pairs.front(), cp1_pairs) ||
+        cp_utils::IsBackwardCycle(cp0_pairs.front(), cp1_pairs)) {
       // cp0 represents the backedge for the cycle.
       return std::make_pair(cp0, cp1);
     }
   }
   if (cp1_pairs.size() == 1) {
-    if (IsForwardCycle(cp1_pairs.front(), cp0_pairs) ||
-        IsBackwardCycle(cp1_pairs.front(), cp0_pairs)) {
+    if (cp_utils::IsForwardCycle(cp1_pairs.front(), cp0_pairs) ||
+        cp_utils::IsBackwardCycle(cp1_pairs.front(), cp0_pairs)) {
       // cp1 represents the forward edge for the cycle.
       return std::make_pair(cp1, cp0);
     }
