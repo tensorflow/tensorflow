f975999eddad24c21f835688c3df24afcd16b99c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-09 23:45:07 -0800

    Automated Code Change
    PiperOrigin-RevId: 713933671

--- tensorflow/compiler/mlir/tfr/passes/decompose.cc
+++ tensorflow/compiler/mlir/tfr/passes/decompose.cc
@@ -19,13 +19,10 @@ limitations under the License.
 #include <iterator>
 #include <limits>
 #include <memory>
-#include <numeric>
 #include <optional>
 #include <string>
 #include <utility>
 
-#include "absl/memory/memory.h"
-#include "absl/strings/string_view.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
