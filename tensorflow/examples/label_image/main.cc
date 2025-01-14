80e5c84d18b70d0b31ee478ccd6661bea7913b72
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-13 23:15:17 -0800

    Automated Code Change
    PiperOrigin-RevId: 715249101

--- tensorflow/examples/label_image/main.cc
+++ tensorflow/examples/label_image/main.cc
@@ -118,7 +118,7 @@ static Status ReadEntireFile(tensorflow::Env* env, const string& filename,
   std::unique_ptr<tensorflow::RandomAccessFile> file;
   TF_RETURN_IF_ERROR(env->NewRandomAccessFile(filename, &file));
 
-  tensorflow::StringPiece data;
+  absl::string_view data;
   TF_RETURN_IF_ERROR(file->Read(0, file_size, &data, &(contents)[0]));
   if (data.size() != file_size) {
     return tensorflow::errors::DataLoss("Truncated read of '", filename,
