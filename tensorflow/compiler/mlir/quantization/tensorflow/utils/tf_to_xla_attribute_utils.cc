448854c072a0a1c0fbe05f732b3a6a57896c69ca
Laura Pak <lpak@google.com>
2025-01-15 14:59:20 -0800

    Remove //third_party/tensorflow/compiler/mlir/lite/core/c:tflite_common from //third_party/tensorflow/compiler/mlir/quantization/tensorflow/utils:tf_to_xla_attribute_utils
    PiperOrigin-RevId: 715958780

--- tensorflow/compiler/mlir/quantization/tensorflow/utils/tf_to_xla_attribute_utils.cc
+++ tensorflow/compiler/mlir/quantization/tensorflow/utils/tf_to_xla_attribute_utils.cc
@@ -21,7 +21,6 @@ limitations under the License.
 #include "absl/strings/str_format.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "mlir/Support/LLVM.h"  // from @llvm-project
-#include "tensorflow/compiler/mlir/lite/core/c/builtin_op_data.h"
 #include "tensorflow/compiler/mlir/lite/kernels/padding.h"
 #include "tensorflow/compiler/mlir/quantization/common/attrs_and_constraints.h"
 #include "tensorflow/compiler/mlir/quantization/tensorflow/cc/constant_fold.h"
