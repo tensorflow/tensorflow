d046cb00aeac8e87324cc3fc15d9ecf55782815e
Mikhail Goncharov <goncharov@google.com>
2025-01-15 04:57:11 -0800

    [XLA:GPU] IWYU in hlo_pass_fix.h NFC
    PiperOrigin-RevId: 715751621

--- third_party/xla/xla/hlo/pass/hlo_pass_fix.h
+++ third_party/xla/xla/hlo/pass/hlo_pass_fix.h
@@ -16,16 +16,19 @@ limitations under the License.
 #ifndef XLA_HLO_PASS_HLO_PASS_FIX_H_
 #define XLA_HLO_PASS_HLO_PASS_FIX_H_
 
-#include <algorithm>
+#include <cstdint>
 #include <type_traits>
 
+#include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_module_group.h"
 #include "xla/hlo/pass/hlo_pass_interface.h"
-#include "xla/status_macros.h"
-#include "xla/types.h"
+#include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace xla {
 
