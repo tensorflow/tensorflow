f82a59839a13153160d20e05d7528288e4a62a7a
weilhuan-quic <quic_weilhuan@quicinc.com>
2025-01-15 14:55:13 +0800

    Implement TensorPool to manage TensorWrappers of a graph

---
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/core/tensor_pool.cc
@@ -0,0 +1,102 @@
+//==============================================================================
+//
+//  Copyright (c) Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+#include "tensorflow/lite/experimental/litert/vendors/qualcomm/core/tensor_pool.h"
+
+namespace qnn {
+
+TensorPool::TensorPool() = default;
+
+TensorPool::TensorPool(std::function<void(TensorWrapper&)> tensor_callback)
+    : tensor_callback_{tensor_callback}, tensor_wrappers_{} {}
+
+TensorWrapper& TensorPool::CreateInputTensor(
+    Qnn_DataType_t data_type, const QuantizeParamsWrapperVariant& quant_params,
+    const std::vector<std::uint32_t>& dimentions) {
+  const auto id = tensor_wrappers_.size();
+  auto& back = tensor_wrappers_.emplace_back(
+      id, QNN_TENSOR_TYPE_APP_WRITE, data_type, quant_params, dimentions);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+TensorWrapper& TensorPool::CreateOutpuTensor(
+    Qnn_DataType_t data_type, const QuantizeParamsWrapperVariant& quant_params,
+    const std::vector<std::uint32_t>& dimentions) {
+  const auto id = tensor_wrappers_.size();
+  auto& back = tensor_wrappers_.emplace_back(
+      id, QNN_TENSOR_TYPE_APP_READ, data_type, quant_params, dimentions);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+TensorWrapper& TensorPool::CreateNativeTensor(
+    Qnn_DataType_t data_type, const QuantizeParamsWrapperVariant& quant_params,
+    const std::vector<std::uint32_t>& dimentions) {
+  const auto id = tensor_wrappers_.size();
+  auto& back = tensor_wrappers_.emplace_back(
+      id, QNN_TENSOR_TYPE_NATIVE, data_type, quant_params, dimentions);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+TensorWrapper& TensorPool::CreateStaticTensor(
+    Qnn_DataType_t data_type, const QuantizeParamsWrapperVariant& quant_params,
+    const std::vector<std::uint32_t>& dimentions, std::uint32_t bytes,
+    const void* data) {
+  const auto id = tensor_wrappers_.size();
+  auto& back =
+      tensor_wrappers_.emplace_back(id, QNN_TENSOR_TYPE_STATIC, data_type,
+                                    quant_params, dimentions, bytes, data);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+TensorWrapper& TensorPool::CloneNativeTensorFrom(const TensorWrapper& src) {
+  const auto id = tensor_wrappers_.size();
+  auto& back = tensor_wrappers_.emplace_back(
+      id, QNN_TENSOR_TYPE_NATIVE, src.GetDataType(), src.quantize_params_,
+      src.dimentions_);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+TensorWrapper& TensorPool::CloneNativeTensorFrom(
+    const TensorWrapper& src, const std::vector<std::uint32_t>& dimentions) {
+  const auto id = tensor_wrappers_.size();
+  auto& back = tensor_wrappers_.emplace_back(id, QNN_TENSOR_TYPE_NATIVE,
+                                             src.GetDataType(),
+                                             src.quantize_params_, dimentions);
+
+  if (tensor_callback_) {
+    tensor_callback_(back);
+  }
+
+  return back;
+}
+
+}  // namespace qnn
