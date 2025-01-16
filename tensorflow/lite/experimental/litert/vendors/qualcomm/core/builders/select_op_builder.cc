813ad3a0e01ce083dfb6b99c42f9ed7a1dc9c232
weilhuan-quic <quic_weilhuan@quicinc.com>
2025-01-16 17:16:59 +0800

    Add op builders

---
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/core/builders/select_op_builder.cc
@@ -0,0 +1,26 @@
+//==============================================================================
+//
+//  Copyright (c) Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+#include "tensorflow/lite/experimental/litert/vendors/qualcomm/core/builders/select_op_builder.h"
+
+namespace qnn {
+
+std::vector<OpWrapper> BuildSelectOp(TensorPool& tensor_pool,
+                                     const std::vector<TensorWrapperRef>& inputs,
+                                     const std::vector<TensorWrapperRef>& outputs) {
+  std::vector<OpWrapper> res;
+
+  auto& select_op = CreateOpWrapper(res, QNN_OP_ELEMENT_WISE_SELECT);
+  for (const auto& input : inputs) {
+    select_op.AddInputTensor(input);
+  }
+  select_op.AddOutputTensor(outputs[0]);
+
+  return res;
+}
+
+}  // namespace qnn
---
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/core/builders/softmax_op_builder.cc
@@ -0,0 +1,25 @@
+//==============================================================================
+//
+//  Copyright (c) Qualcomm Technologies, Inc.
+//  All Rights Reserved.
+//  Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+#include "tensorflow/lite/experimental/litert/vendors/qualcomm/core/builders/softmax_op_builder.h"
+
+namespace qnn {
+
+std::vector<OpWrapper> BuildSoftmaxOp(
+    TensorPool& tensor_pool, const std::vector<TensorWrapperRef>& inputs,
+    const std::vector<TensorWrapperRef>& outputs, const float beta) {
+  std::vector<OpWrapper> res;
+
+  auto& softmax_op = CreateOpWrapper(res, QNN_OP_SOFTMAX);
+  softmax_op.AddInputTensor(inputs[0]);
+  softmax_op.AddOutputTensor(outputs[0]);
+  softmax_op.AddScalarParam<float>(QNN_OP_SOFTMAX_PARAM_BETA, beta);
+
+  return res;
+}
+
+}  // namespace qnn
