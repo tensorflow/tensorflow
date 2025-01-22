8a77aec26b41eeb62e47c5472b05425748aee83c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-21 23:40:04 -0800

    Automated Code Change
    PiperOrigin-RevId: 718240575

--- tensorflow/core/tfrt/utils/debug/node_io_dump_rewriter.h
+++ tensorflow/core/tfrt/utils/debug/node_io_dump_rewriter.h
@@ -18,6 +18,7 @@ limitations under the License.
 #include <string>
 
 #include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "tensorflow/core/graph/graph.h"
 #include "tensorflow/core/platform/status.h"
