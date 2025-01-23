26984e3013ada0d0770736c883a67a0e48e47207
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-22 21:13:40 -0800

    Automated Code Change
    PiperOrigin-RevId: 718691640

--- tensorflow/core/tfrt/kernels/ifrt_program_ops_test.cc
+++ tensorflow/core/tfrt/kernels/ifrt_program_ops_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
