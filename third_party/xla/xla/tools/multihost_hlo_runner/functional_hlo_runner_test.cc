a0636d1bcd58d2c7a79cf9acd3a66e1a243aba16
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-26 05:38:53 -0800

    Automated Code Change
    PiperOrigin-RevId: 719861077

--- third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner_test.cc
+++ third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/log/check.h"
 #include "absl/status/status.h"
