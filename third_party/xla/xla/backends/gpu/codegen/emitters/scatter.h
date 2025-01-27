7b5de9bc19e4bc1a0e8f58457295467d3c9f730e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-27 02:34:36 -0800

    Automated Code Change
    PiperOrigin-RevId: 720088433

--- third_party/xla/xla/backends/gpu/codegen/emitters/scatter.h
+++ third_party/xla/xla/backends/gpu/codegen/emitters/scatter.h
@@ -38,6 +38,7 @@ limitations under the License.
 #include "xla/shape.h"
 #include "xla/stream_executor/device_description.h"
 #include "xla/util.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
