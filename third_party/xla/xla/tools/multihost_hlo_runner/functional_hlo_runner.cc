079cf0bb603aeb70cc301df5902f76433a984de1
Alexander Lyashuk <crem@google.com>
2025-01-29 09:24:17 -0800

    [XLA:GPU] Allow AUTO layout in the multihost runner.
    PiperOrigin-RevId: 721004911

--- third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner.cc
+++ third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner.cc
@@ -618,7 +618,8 @@ FunctionalHloRunner::ReadModuleFromHloTextFile(absl::string_view hlo_file) {
   std::string hlo_string;
   TF_RETURN_IF_ERROR(tsl::ReadFileToString(tsl::Env::Default(),
                                            std::string(hlo_file), &hlo_string));
-  return ParseAndReturnUnverifiedModule(hlo_string, {}, HloParserOptions());
+  return ParseAndReturnUnverifiedModule(
+      hlo_string, {}, HloParserOptions().set_keep_module_auto_layouts(true));
 }
 
 absl::StatusOr<std::unique_ptr<HloModule>>
