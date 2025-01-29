bc188e915f1fdfada9b66ab8e024e84e875421b5
A. Unique TensorFlower <gardener@tensorflow.org>
2025-01-28 17:26:00 -0800

    Automated Code Change
    PiperOrigin-RevId: 720762739

--- tensorflow/lite/tools/versioning/op_version.h
+++ tensorflow/lite/tools/versioning/op_version.h
@@ -15,6 +15,7 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_TOOLS_VERSIONING_OP_VERSION_H_
 #define TENSORFLOW_LITE_TOOLS_VERSIONING_OP_VERSION_H_
 
+#include <cstdint>
 #include <vector>
 
 #include "tensorflow/compiler/mlir/lite/schema/mutable/schema_generated.h"
