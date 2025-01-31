f45c97c2956c507dce18329b909c62a986126d36
Alex <alexandros.theodoridis@amd.com>
2025-01-31 08:41:02 -0800

    PR #22102: [ROCm] Add support for the collective memory allocator type
    Imported from GitHub PR https://github.com/openxla/xla/pull/22102
    
    This pr introduces support for the collective memory allocator in rocm. Fixing the failing test due to the missing support.
    Copybara import of the project:
    
    --
    68cb406def43250e7d3005452434ec938ca09e9b by Alexandros Theodoridis <atheodor@amd.com>:
    
    Add support for the collective memory allocator type
    
    --
    35ad29769dd13b3f8430ed0e315e94d468af34f6 by Alexandros Theodoridis <atheodor@amd.com>:
    
    Add test
    
    Merging this change closes #22102
    
    PiperOrigin-RevId: 721786038

--- third_party/xla/xla/stream_executor/rocm/rocm_executor_test.cc
+++ third_party/xla/xla/stream_executor/rocm/rocm_executor_test.cc
@@ -111,14 +111,27 @@ TEST(RocmExecutorTest, CreateHostMemoryAllocatorWorks) {
   allocation.reset();
 }
 
+TEST(RocmExecutorTest, CreateCollectiveMemoryAllocatorWorks) {
+  TF_ASSERT_OK_AND_ASSIGN(Platform * platform,
+                          PlatformManager::PlatformWithName("ROCM"));
+  TF_ASSERT_OK_AND_ASSIGN(StreamExecutor * executor,
+                          platform->ExecutorForDevice(0));
+  TF_ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<MemoryAllocator> allocator,
+      executor->CreateMemoryAllocator(MemoryType::kCollective));
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<MemoryAllocation> allocation,
+                          allocator->Allocate(1024));
+  EXPECT_NE(allocation->opaque(), nullptr);
+  EXPECT_EQ(allocation->size(), 1024);
+  allocation.reset();
+}
+
 TEST(RocmExecutorTest, CreateUnsupportedMemoryAllocatorsFail) {
   TF_ASSERT_OK_AND_ASSIGN(Platform * platform,
                           PlatformManager::PlatformWithName("ROCM"));
   TF_ASSERT_OK_AND_ASSIGN(StreamExecutor * executor,
                           platform->ExecutorForDevice(0));
   EXPECT_THAT(executor->CreateMemoryAllocator(MemoryType::kDevice),
-              Not(IsOk()));
-  EXPECT_THAT(executor->CreateMemoryAllocator(MemoryType::kCollective),
               Not(IsOk()));
 }
 
