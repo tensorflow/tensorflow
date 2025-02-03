acb39a15b4af1826d93eff9a85a32f9cb6e7cac9
Mihai Maruseac <mihai.maruseac@gmail.com>
2025-02-03 06:38:46 -0800

    Update tensorflow/core/kernels/ragged_tensor_to_tensor_op.cc

--- tensorflow/core/kernels/ragged_tensor_to_tensor_op.cc
+++ tensorflow/core/kernels/ragged_tensor_to_tensor_op.cc
@@ -417,20 +417,20 @@ class RaggedTensorToTensorBaseOp : public OpKernel {
                    context->allocate_output(0, output_shape, &output_tensor));
     const INDEX_TYPE full_size = multiplier[0] * output_size[0];
     if (full_size > 0) {
-        vector<INDEX_TYPE> output_index, new_output_index;
-        int nvals = context->input(kValueInputIndex).shape().dim_size(0);
-        output_index.reserve(nvals);
-        new_output_index.reserve(nvals);
+      vector<INDEX_TYPE> output_index, new_output_index;
+      int nvals = context->input(kValueInputIndex).shape().dim_size(0);
+      output_index.reserve(nvals);
+      new_output_index.reserve(nvals);
 
-        CalculateFirstParentOutputIndex(first_dimension, multiplier[0],
-                                        output_size[0], &output_index);
-        for (int i = 1; i <= ragged_rank_; ++i) {
-            OP_REQUIRES_OK(context, CalculateOutputIndex(
-                                        context, i - 1, output_index, multiplier[i],
-                                        output_size[i], &new_output_index));
-            output_index.swap(new_output_index);
-            new_output_index.clear();
-        }
+      CalculateFirstParentOutputIndex(first_dimension, multiplier[0],
+                                      output_size[0], &output_index);
+      for (int i = 1; i <= ragged_rank_; ++i) {
+        OP_REQUIRES_OK(context, CalculateOutputIndex(
+                                    context, i - 1, output_index, multiplier[i],
+                                    output_size[i], &new_output_index));
+        output_index.swap(new_output_index);
+        new_output_index.clear();
+      }
 
         SetOutput(context, ragged_rank_, output_index, output_tensor);
     }
