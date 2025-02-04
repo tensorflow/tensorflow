0c5f184a355371d3c35e186efb826ec02ac4be15
A. Unique TensorFlower <gardener@tensorflow.org>
2025-02-03 17:30:59 -0800

    Automated Code Change
    Reverts changelist 723246423
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/85476 from jiunkaiy:dev/weilhuan/wrapper 06e061657d780fe341be8404e27697b762c00805
    PiperOrigin-RevId: 722860572

--- third_party/xla/xla/hlo/testlib/hlo_hardware_independent_test_base.h
+++ third_party/xla/xla/hlo/testlib/hlo_hardware_independent_test_base.h
@@ -43,9 +43,9 @@ limitations under the License.
 #include "xla/service/hlo_verifier.h"
 #include "xla/shape_layout.h"
 #include "xla/shape_util.h"
+#include "xla/tsl/platform/test.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
-#include "tsl/platform/test.h"
 
 namespace xla {
 
