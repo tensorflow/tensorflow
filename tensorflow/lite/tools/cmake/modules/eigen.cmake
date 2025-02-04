b4bbc6349bf08b3a9d4b08b7b8ca9b798b9725ae
Antonio Sanchez <cantonios@google.com>
2025-02-04 12:13:52 -0800

    Update Eigen to commit:b73bb766a57f872b4fe376c4818cd9b17dc1a761
    PiperOrigin-RevId: 723181903

--- tensorflow/lite/tools/cmake/modules/eigen.cmake
+++ tensorflow/lite/tools/cmake/modules/eigen.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   eigen
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
   # Sync with tensorflow/third_party/eigen3/workspace.bzl
-  GIT_TAG 33d0937c6bdf5ec999939fb17f2a553183d14a74
+  GIT_TAG b73bb766a57f872b4fe376c4818cd9b17dc1a761
   # It's not currently (cmake 3.17) possible to shallow clone with a GIT TAG
   # as cmake attempts to git checkout the commit hash after the clone
   # which doesn't work as it's a shallow clone hence a different commit hash.
