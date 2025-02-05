58a8e903a836c8a0af4c75d21b39112010b0e5bf
Alex <alexandros.theodoridis@amd.com>
2025-02-05 03:52:34 -0800

    PR #22334: [ROCm] Fix flaky gpu compiler test when building with rocm
    Imported from GitHub PR https://github.com/openxla/xla/pull/22334
    
    This change fixes the flaky gpu compiler test used to run on rocm CI pipeline gate.
    Triton pipeline was wrongly using the TritonGPUAccelerateMatmul pass which supports cuda only.
    In rocm there is a different pass which is now used in the rocm pipeline.
    
    https://github.com/triton-lang/triton/blob/main/third_party/amd/lib/TritonAMDGPUTransforms/AccelerateAMDMatmul.cpp
    Copybara import of the project:
    
    --
    c5f600f03aa87d155bb624bedb0584e635af190e by Alexandros Theodoridis <atheodor@amd.com>:
    
    Fix flaky gpu compiler test when building with rocm
    
    Merging this change closes #22334
    
    Reverts changelist 723246423
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/22334 from ROCm:rocm_fix_flaky_gpu_compiler_test c5f600f03aa87d155bb624bedb0584e635af190e
    PiperOrigin-RevId: 723447950

--- tensorflow/compiler/mlir/lite/transforms/lower_quant_annotations_helper.h
+++ tensorflow/compiler/mlir/lite/transforms/lower_quant_annotations_helper.h
@@ -31,7 +31,8 @@ namespace mlir::TFL {
 LogicalResult FillCompositeParams(stablehlo::CompositeOp op,
                                   SmallVector<double, 4>& scales,
                                   SmallVector<int64_t, 4>& zero_points,
-                                  int& num_bits, bool& is_signed);
+                                  int& num_bits, bool& is_signed,
+                                  bool& is_narrow_range);
 
 LogicalResult GetStorageParams(unsigned num_bits, bool narrow_range,
                                bool is_signed, MLIRContext* ctx,
