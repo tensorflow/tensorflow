3806dd2d2f8446ff12379e8441ff72b19fae3d01
A. Unique TensorFlower <gardener@tensorflow.org>
2025-02-05 17:51:54 -0800

    Automated Code Change
    PiperOrigin-RevId: 723719304

--- third_party/xla/xla/hlo/transforms/simplifiers/dynamic_dimension_simplifier_test.cc
+++ third_party/xla/xla/hlo/transforms/simplifiers/dynamic_dimension_simplifier_test.cc
@@ -17,6 +17,7 @@ limitations under the License.
 
 #include <memory>
 
+#include <gtest/gtest.h>
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
