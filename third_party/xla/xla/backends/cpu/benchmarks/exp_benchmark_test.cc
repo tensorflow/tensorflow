4cb23fba43a210970768e94fe60244bc579ffcda
Eugene Zhulenev <ezhulenev@google.com>
2025-02-06 21:26:19 -0800

    [xla:cpu] Use f16 to f32 upcast for polynomial math approximations
    For consistency with Eigen (Tensorflow) use f16 to f32 casting to compute approximations.
    
    PiperOrigin-RevId: 724194469

--- third_party/xla/xla/backends/cpu/benchmarks/exp_benchmark_test.cc
+++ third_party/xla/xla/backends/cpu/benchmarks/exp_benchmark_test.cc
@@ -1,4 +1,4 @@
-/* Copyright 2024 The OpenXLA Authors.
+/* Copyright 2025 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -30,15 +30,15 @@ limitations under the License.
 
 namespace xla::cpu {
 
-static void BM_TanhF32(benchmark::State& state) {
+static void BM_ExpF32(benchmark::State& state) {
   int64_t d0 = state.range(0);
 
   absl::string_view hlo = R"(
-    HloModule tanh_f32_$d0
+    HloModule exp_f32_$d0
 
     ENTRY e {
       input = f32[$d0] parameter(0)
-      ROOT output = tanh(input)
+      ROOT output = exponential(input)
     }
   )";
 
@@ -51,15 +51,15 @@ static void BM_TanhF32(benchmark::State& state) {
   CHECK_OK(RunHloBenchmark(state, hlo, args, {{"$d0", absl::StrCat(d0)}}));
 }
 
-static void BM_TanhF16(benchmark::State& state) {
+static void BM_ExpF16(benchmark::State& state) {
   int64_t d0 = state.range(0);
 
   absl::string_view hlo = R"(
-    HloModule tanh_f16_$d0
+    HloModule exp_f16_$d0
 
     ENTRY e {
       input = f16[$d0] parameter(0)
-      ROOT output = tanh(input)
+      ROOT output = exponential(input)
     }
   )";
 
@@ -72,38 +72,16 @@ static void BM_TanhF16(benchmark::State& state) {
   CHECK_OK(RunHloBenchmark(state, hlo, args, {{"$d0", absl::StrCat(d0)}}));
 }
 
-static void BM_TanhF64(benchmark::State& state) {
-  int64_t d0 = state.range(0);
-
-  absl::string_view hlo = R"(
-    HloModule tanh_f64_$d0
-
-    ENTRY e {
-      input = f64[$d0] parameter(0)
-      ROOT output = tanh(input)
-    }
-  )";
-
-  std::minstd_rand0 engine;
-
-  auto input_shape = ShapeUtil::MakeShape(F64, {d0});
-  auto p0 =
-      *LiteralUtil::CreateRandomLiteral<F64>(input_shape, &engine, 1.0f, 0.1f);
-  std::vector<const Literal*> args = {&p0};
-  CHECK_OK(RunHloBenchmark(state, hlo, args, {{"$d0", absl::StrCat(d0)}}));
-}
-
-#define REGISTER_TANH_BENCHMARK(NAME) \
-  BENCHMARK(NAME)                     \
-      ->MeasureProcessCPUTime()       \
-      ->Arg(128)                      \
-      ->Arg(256)                      \
-      ->Arg(512)                      \
-      ->Arg(1024)                     \
+#define REGISTER_EXP_BENCHMARK(NAME) \
+  BENCHMARK(NAME)                    \
+      ->MeasureProcessCPUTime()      \
+      ->Arg(128)                     \
+      ->Arg(256)                     \
+      ->Arg(512)                     \
+      ->Arg(1024)                    \
       ->Arg(4096);
 
-REGISTER_TANH_BENCHMARK(BM_TanhF32);
-REGISTER_TANH_BENCHMARK(BM_TanhF16);
-REGISTER_TANH_BENCHMARK(BM_TanhF64);
+REGISTER_EXP_BENCHMARK(BM_ExpF32);
+REGISTER_EXP_BENCHMARK(BM_ExpF16);
 
 }  // namespace xla::cpu
