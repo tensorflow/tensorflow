090d0ce21a33d96ebc86b2faa85da556ecc58a5c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-02-10 17:57:00 -0800

    Automated Code Change
    PiperOrigin-RevId: 725415142

--- tensorflow/core/grappler/optimizers/model_pruner.cc
+++ tensorflow/core/grappler/optimizers/model_pruner.cc
@@ -15,12 +15,18 @@ limitations under the License.
 
 #include "tensorflow/core/grappler/optimizers/model_pruner.h"
 
+#include <algorithm>
+#include <memory>
 #include <unordered_set>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "tensorflow/core/framework/attr_value.pb.h"
 #include "tensorflow/core/framework/function.pb.h"
+#include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/framework/node_def_builder.h"
 #include "tensorflow/core/framework/types.h"
