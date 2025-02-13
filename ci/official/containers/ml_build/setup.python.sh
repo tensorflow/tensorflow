4d034e905efed280386b5c211db369202f1b95fd
Nitin Srinivasan <srnitin@google.com>
2025-02-12 23:03:08 -0800

    Install uv in CI Dockerfiles
    uv is much faster than pip for installing Python packages.
    
    PiperOrigin-RevId: 726336762

--- ci/official/containers/ml_build/setup.python.sh
+++ ci/official/containers/ml_build/setup.python.sh
@@ -68,7 +68,14 @@ wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 --tries=5 
 /usr/bin/$VERSION -m pip install --no-cache-dir --upgrade pip
 /usr/bin/$VERSION -m pip install -U setuptools
 
-# Disable the cache dir to save image space, and install packages
-if [[ ${VERSION} != "python3.13-nogil" ]]; then
-  /usr/bin/$VERSION -m pip install --no-cache-dir -r $REQUIREMENTS -U
+
+# For Python 3.13t, do not install twine as it does not have pre-built wheels
+# for this Python version and building it from source fails. We only need twine
+# to be present on the system Python which in this case is 3.12.
+if [[ ${VERSION} == "python3.13-nogil" ]]; then
+  grep -v "twine" $REQUIREMENTS > requirements_without_twine.txt
+  REQUIREMENTS=requirements_without_twine.txt
 fi
+
+# Disable the cache dir to save image space, and install packages
+/usr/bin/$VERSION -m pip install --no-cache-dir -r $REQUIREMENTS -U
