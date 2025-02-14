35fb4a99534dc85e47eeedf760ab691ddda31219
Ryan M. Lefever <lefever@google.com>
2025-02-14 14:47:22 -0800

    1. Replaced the BaseCosts API with the OpCostManager API in MSA's CostAnalysis. 2. MSA's CostAnalysis directly exposes OperandBytesAccess() and OutputBytesAccess() methods, rather than through base_costs(), as it previously did.
    PiperOrigin-RevId: 727070950

--- third_party/xla/xla/service/hlo_cost_analysis.cc
+++ third_party/xla/xla/service/hlo_cost_analysis.cc
@@ -103,7 +103,7 @@ absl::Status HloCostAnalysis::Postprocess(const HloInstruction* hlo) {
   auto [it_ignored, inserted] =
       hlo_properties_.emplace(hlo, std::move(current_properties_));
   current_properties_ = Properties();
-  TF_RET_CHECK(inserted);
+  TF_RET_CHECK(inserted) << hlo->name() << " already exists in hlo_properties_";
 
   return absl::OkStatus();
 }
