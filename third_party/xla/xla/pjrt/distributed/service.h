9e210ae9371a771d522a5af5659bbeaa8e2b1ce3
Michael Whittaker <mwhittaker@google.com>
2025-02-18 16:41:29 -0800

    Simplify heartbeat options for PjRt distributed runtime.
    PiperOrigin-RevId: 728410906

--- third_party/xla/xla/pjrt/distributed/service.h
+++ third_party/xla/xla/pjrt/distributed/service.h
@@ -45,13 +45,9 @@ class CoordinationServiceImpl {
 
     tsl::Env* env = tsl::Env::Default();
 
-    // Interval at which the service should check for missed heartbeat RPCs
-    // from the clients.
-    absl::Duration heartbeat_interval = absl::Seconds(10);
-
-    // Number of heartbeats that a client may miss in a row before the
-    // coordinator concludes that a client has vanished.
-    int max_missing_heartbeats = 10;
+    // The duration after which the service concludes a client has vanished if
+    // it hasn't received any heartbeats from the client.
+    absl::Duration heartbeat_timeout = absl::Seconds(100);
 
     // How long should we wait for all clients to call Connect() before
     // giving up?
