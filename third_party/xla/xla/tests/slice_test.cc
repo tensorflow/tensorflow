93c8fe583f63862f3de6695835ec5de2351f76f5
A. Unique TensorFlower <gardener@tensorflow.org>
2025-02-20 21:42:53 -0800

    Automated Code Change
    PiperOrigin-RevId: 729373475

--- third_party/xla/xla/tests/slice_test.cc
+++ third_party/xla/xla/tests/slice_test.cc
@@ -15,9 +15,14 @@ limitations under the License.
 
 // Tests that slice operations can be performed.
 
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
 #include <numeric>
-#include <vector>
+#include <string>
 
+#include <gtest/gtest.h>
 #include "absl/container/inlined_vector.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
