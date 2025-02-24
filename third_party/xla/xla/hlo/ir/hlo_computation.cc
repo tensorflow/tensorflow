386f7e6895f07908d99d106d7599b074900b8873
Shraiysh <svaishay@nvidia.com>
2025-02-24 11:16:39 -0800

    PR #22970: Fix bug in post order traversal of computation instructions
    Imported from GitHub PR https://github.com/openxla/xla/pull/22970
    
    While creating post order traversal, an instruction may have a user outside the computation. This is the case when we are constructing new instructions to store in replacements for cloning the computation later. This user should be ignored. Added test for the same.
    
    Because of this, functions like `ToString()`, and
    `GetUniqueGteInstruction()` encounter errors. They rely on post-order traversal to have all the instructions.
    Copybara import of the project:
    
    --
    326469b7cab50e90616094dffe36758afef815e1 by Shraiysh Vaishay <svaishay@nvidia.com>:
    
    Fix bug in post order traversal of computation instructions
    
    While creating post order traversal, an instruction may have a user
    outside the computation. This is the case when we are constructing
    new instructions to store in replacements for cloning the computation
    later. This user should be ignored. Added test for the same.
    
    Because of this, functions like `ToString()`, and
    `GetUniqueGteInstruction()` encounter errors. They rely on post-order
    traversal to have all the instructions.
    
    Merging this change closes #22970
    
    PiperOrigin-RevId: 730525630

--- third_party/xla/xla/hlo/ir/hlo_computation.cc
+++ third_party/xla/xla/hlo/ir/hlo_computation.cc
@@ -743,7 +743,12 @@ std::vector<HloInstruction*> HloComputation::MakeInstructionPostOrder(
   dfs_stack_scratch.reserve(instruction_count());
 
   for (const auto& instruction : instructions()) {
-    if (instruction->users().empty()) {
+    // We don't consider users outside any computation as real users. This can
+    // happen when creating new instructions for replacement when cloning
+    // computations.
+    if (absl::c_all_of(instruction->users(), [](const HloInstruction* user) {
+          return user->parent() == nullptr;
+        })) {
       ComputeInstructionPostOrder(instruction, channel_dependencies, visited,
                                   post_order, &dfs_stack_scratch);
     }
@@ -823,7 +828,12 @@ void HloComputation::ForEachInstructionPostOrder(
   dfs_stack_scratch.reserve(instruction_count());
   auto channel_dependencies = ComputeChannelDependencies();
   for (const auto& instruction : instructions()) {
-    if (instruction->users().empty()) {
+    // We don't consider users outside any computation as real users. This can
+    // happen when creating new instructions for replacement when cloning
+    // computations.
+    if (absl::c_all_of(instruction->users(), [](const HloInstruction* user) {
+          return user->parent() == nullptr;
+        })) {
       ForEachInstructionPostOrderImpl(func, instruction, channel_dependencies,
                                       visited, &dfs_stack_scratch);
     }
