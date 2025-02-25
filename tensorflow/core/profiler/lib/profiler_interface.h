07b4cc5d92253941c473bb9ba9646a8e2cd534ca
Yin Zhang <yinzz@google.com>
2025-02-24 22:08:43 -0800

    Enable hiding ACF parameters node that are implementation details in the hlo graph.
    PiperOrigin-RevId: 730725629

--- tensorflow/core/profiler/lib/profiler_interface.h
+++ tensorflow/core/profiler/lib/profiler_interface.h
@@ -16,14 +16,8 @@ limitations under the License.
 #define TENSORFLOW_CORE_PROFILER_LIB_PROFILER_INTERFACE_H_
 
 #include "absl/base/macros.h"
-#include "tensorflow/core/platform/status.h"
 #include "tsl/profiler/lib/profiler_interface.h"
 #include "tsl/profiler/protobuf/xplane.pb.h"
-
-// TODO: b/323943471 - This macro should eventually be provided by Abseil.
-#ifndef ABSL_DEPRECATE_AND_INLINE
-#define ABSL_DEPRECATE_AND_INLINE()
-#endif
 
 namespace tensorflow {
 namespace profiler {
