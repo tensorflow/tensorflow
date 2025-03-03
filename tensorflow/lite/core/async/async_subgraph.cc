1f9676d7ba2f9e85f87b40e83cef17ce4ef8a713
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-02 20:07:30 -0800

    Automated Code Change
    PiperOrigin-RevId: 732763335

--- tensorflow/lite/core/async/async_subgraph.cc
+++ tensorflow/lite/core/async/async_subgraph.cc
@@ -14,6 +14,8 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/core/async/async_subgraph.h"
 
+#include <atomic>
+#include <cstddef>
 #include <vector>
 
 #include "tensorflow/lite/core/async/async_kernel_internal.h"
