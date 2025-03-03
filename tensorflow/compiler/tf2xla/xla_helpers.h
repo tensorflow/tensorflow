95e690fdf3202d3bce393b5f6aa594afd6f971ad
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-02 20:07:35 -0800

    Automated Code Change
    PiperOrigin-RevId: 732763358

--- tensorflow/compiler/tf2xla/xla_helpers.h
+++ tensorflow/compiler/tf2xla/xla_helpers.h
@@ -20,6 +20,10 @@ limitations under the License.
 
 #include <string>
 
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "absl/types/span.h"
 #include "tensorflow/compiler/tf2xla/host_compute_metadata.pb.h"
