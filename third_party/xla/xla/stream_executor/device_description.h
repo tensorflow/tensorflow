dd91ea94f23dfdeb50c9ab351bed264fdfe139c2
mmakevic-amd <Milica.Makevic@amd.com>
2025-03-03 18:11:53 -0800

    PR #23237: [ROCm] Remove gfx940 and gfx941
    Imported from GitHub PR https://github.com/openxla/xla/pull/23237
    
    https://github.com/llvm/llvm-project/commit/8615f9aaffd4337a33ea979f010c4d6410ba6125
    
    This PR removes all occurrences of gfx940/gfx941
    Copybara import of the project:
    
    --
    4bab9ba12920c95bf35d050da6fc162fc519e994 by Milica Makevic <Milica.Makevic@amd.com>:
    
    Remove gfx940 and gfx941
    
    Merging this change closes #23237
    
    PiperOrigin-RevId: 733129259

--- third_party/xla/xla/stream_executor/device_description.h
+++ third_party/xla/xla/stream_executor/device_description.h
@@ -71,20 +71,15 @@ class RocmComputeCapability {
 
   bool gfx9_mi200() const { return gfx_version() == "gfx90a"; }
 
-  bool gfx9_mi300() const {
-    static constexpr absl::string_view kList[] = {"gfx940", "gfx941", "gfx942"};
-    return absl::c_count(kList, gfx_version()) != 0;
-  }
+  bool gfx9_mi300() const { return gfx_version() == "gfx942"; }
 
   bool gfx9_mi100_or_later() const {
-    static constexpr absl::string_view kList[] = {"gfx908", "gfx90a", "gfx940",
-                                                  "gfx941", "gfx942"};
+    static constexpr absl::string_view kList[] = {"gfx908", "gfx90a", "gfx942"};
     return absl::c_count(kList, gfx_version()) != 0;
   }
 
   bool gfx9_mi200_or_later() const {
-    static constexpr absl::string_view kList[] = {"gfx90a", "gfx940", "gfx941",
-                                                  "gfx942"};
+    static constexpr absl::string_view kList[] = {"gfx90a", "gfx942"};
     return absl::c_count(kList, gfx_version()) != 0;
   }
 
@@ -147,13 +142,13 @@ class RocmComputeCapability {
   std::string gcn_arch_name_ = "gfx000";  // default to invalid arch.
 
   static constexpr absl::string_view kSupportedGfxVersions[]{
-      "gfx900",                        // MI25
-      "gfx906",                        // MI50 / MI60
-      "gfx908",                        // MI100
-      "gfx90a",                        // MI200
-      "gfx940",  "gfx941",  "gfx942",  // MI300
-      "gfx1030",                       // RX68xx / RX69xx
-      "gfx1100",                       // RX7900
+      "gfx900",   // MI25
+      "gfx906",   // MI50 / MI60
+      "gfx908",   // MI100
+      "gfx90a",   // MI200
+      "gfx942",   // MI300
+      "gfx1030",  // RX68xx / RX69xx
+      "gfx1100",  // RX7900
       "gfx1200", "gfx1201",
   };
 };
