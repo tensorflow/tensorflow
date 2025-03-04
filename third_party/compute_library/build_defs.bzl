openxla/xla

Move TSL's `third_party` to XLA, update users
Mostly updating `WORKSPACE` and related files from `@tsl//third_party` -> `@xla//third_party`. Please tag me if this change breaks you.

PiperOrigin-RevId: 731391656

third_party/compute_library/build_defs.bzl
@@ -1,6 +1,6 @@
 def if_enable_acl(if_true, if_false = []):
     return select({
-        "@tsl//third_party/compute_library:build_with_acl": if_true,
+        "@xla//third_party/compute_library:build_with_acl": if_true,
         "//conditions:default": if_false,
     })
 
@@ -15,6 +15,6 @@ def acl_deps():
       inclusion in the deps attribute of rules.
     """
     return select({
-        "@tsl//third_party/compute_library:build_with_acl": ["@compute_library//:arm_compute"],
+        "@xla//third_party/compute_library:build_with_acl": ["@compute_library//:arm_compute"],
         "//conditions:default": [],
     })

third_party/implib_so/get_symbols.py
@@ -1,7 +1,7 @@
 """Given a .so file, lists symbols that should be included in a stub.
 
 Example usage:
-$ bazel run -c opt @tsl//third_party/implib_so:get_symbols
+$ bazel run -c opt @xla//third_party/implib_so:get_symbols
 /usr/local/cuda/lib64/libcudart.so > third_party/tsl/tsl/cuda/cudart.symbols
 """
 

third_party/llvm_openmp/cmake_vars.bzl
@@ -46,7 +46,7 @@ def expand_cmake_vars(name, src, dst, cmake_vars):
       cmake_vars: a string containing the CMake variables, as generated by
         cmake_var_string.
     """
-    expand_cmake_vars_tool = "@tsl//third_party/llvm_openmp:expand_cmake_vars"
+    expand_cmake_vars_tool = "@xla//third_party/llvm_openmp:expand_cmake_vars"
     native.genrule(
         name = name,
         srcs = [src],

xla/tsl/platform/default/build_config.bzl
@@ -2,7 +2,7 @@
 
 load("@com_github_grpc_grpc//bazel:generate_cc.bzl", "generate_cc")
 load("@com_google_protobuf//:protobuf.bzl", "proto_gen")
-load("@tsl//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
+load("@xla//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
 load(
     "@xla//xla/tsl:tsl.bzl",
     "clean_dep",
@@ -120,7 +120,7 @@ def pyx_library(
         native.cc_binary(
             name = shared_object_name,
             srcs = [stem + ".cpp"],
-            deps = cc_deps + ["@tsl//third_party/python_runtime:headers"],
+            deps = cc_deps + ["@xla//third_party/python_runtime:headers"],
             linkshared = 1,
             testonly = testonly,
             copts = copts,

xla/tsl/platform/default/build_config_root.bzl
@@ -5,7 +5,7 @@ be separate to avoid cyclic references.
 """
 
 load("@local_config_remote_execution//:remote_execution.bzl", "gpu_test_tags")
-load("@tsl//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
+load("@xla//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
 
 # RBE settings for tests that require a GPU. This is used in exec_properties of rules
 # that need GPU access.

xla/tsl/tsl.bzl
@@ -33,7 +33,7 @@ load(
     "if_tensorrt",
 )
 load(
-    "@tsl//third_party/py/rules_pywrap:pywrap.bzl",
+    "@xla//third_party/py/rules_pywrap:pywrap.bzl",
     "use_pywrap_rules",
 )
 

tensorflow/tensorflow

third_party/compute_library/build_defs.bzl
@@ -1,6 +1,6 @@
 def if_enable_acl(if_true, if_false = []):
     return select({
-        "@local_tsl//third_party/compute_library:build_with_acl": if_true,
+        "@local_xla//third_party/compute_library:build_with_acl": if_true,
         "//conditions:default": if_false,
     })
 
@@ -15,6 +15,6 @@ def acl_deps():
       inclusion in the deps attribute of rules.
     """
     return select({
-        "@local_tsl//third_party/compute_library:build_with_acl": ["@compute_library//:arm_compute"],
+        "@local_xla//third_party/compute_library:build_with_acl": ["@compute_library//:arm_compute"],
         "//conditions:default": [],
     })

third_party/implib_so/get_symbols.py
@@ -1,7 +1,7 @@
 """Given a .so file, lists symbols that should be included in a stub.
 
 Example usage:
-$ bazel run -c opt @local_tsl//third_party/implib_so:get_symbols
+$ bazel run -c opt @local_xla//third_party/implib_so:get_symbols
 /usr/local/cuda/lib64/libcudart.so > third_party/tsl/tsl/cuda/cudart.symbols
 """
 

third_party/llvm_openmp/cmake_vars.bzl
@@ -46,7 +46,7 @@ def expand_cmake_vars(name, src, dst, cmake_vars):
       cmake_vars: a string containing the CMake variables, as generated by
         cmake_var_string.
     """
-    expand_cmake_vars_tool = "@local_tsl//third_party/llvm_openmp:expand_cmake_vars"
+    expand_cmake_vars_tool = "@local_xla//third_party/llvm_openmp:expand_cmake_vars"
     native.genrule(
         name = name,
         srcs = [src],

third_party/xla/xla/tsl/platform/default/build_config.bzl
@@ -2,7 +2,7 @@
 
 load("@com_github_grpc_grpc//bazel:generate_cc.bzl", "generate_cc")
 load("@com_google_protobuf//:protobuf.bzl", "proto_gen")
-load("@local_tsl//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
+load("@local_xla//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
 load(
     "@local_xla//xla/tsl:tsl.bzl",
     "clean_dep",
@@ -120,7 +120,7 @@ def pyx_library(
         native.cc_binary(
             name = shared_object_name,
             srcs = [stem + ".cpp"],
-            deps = cc_deps + ["@local_tsl//third_party/python_runtime:headers"],
+            deps = cc_deps + ["@local_xla//third_party/python_runtime:headers"],
             linkshared = 1,
             testonly = testonly,
             copts = copts,

third_party/xla/xla/tsl/platform/default/build_config_root.bzl
@@ -5,7 +5,7 @@ be separate to avoid cyclic references.
 """
 
 load("@local_config_remote_execution//:remote_execution.bzl", "gpu_test_tags")
-load("@local_tsl//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
+load("@local_xla//third_party/py/rules_pywrap:pywrap.bzl", "use_pywrap_rules")
 
 # RBE settings for tests that require a GPU. This is used in exec_properties of rules
 # that need GPU access.

third_party/xla/xla/tsl/tsl.bzl
@@ -33,7 +33,7 @@ load(
     "if_tensorrt",
 )
 load(
-    "@local_tsl//third_party/py/rules_pywrap:pywrap.bzl",
+    "@local_xla//third_party/py/rules_pywrap:pywrap.bzl",
     "use_pywrap_rules",
 )
 