d27d9da7ff6df88c5854b37167e181fffed4a226
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-04 08:36:40 -0800

    Add support for AHWB-backed GL Buffers in LiteRT runtime.
    PiperOrigin-RevId: 733341648

--- tensorflow/lite/experimental/litert/runtime/gl_buffer.cc
+++ tensorflow/lite/experimental/litert/runtime/gl_buffer.cc
@@ -32,10 +32,100 @@
 #include "tensorflow/lite/delegates/gpu/gl/gl_buffer.h"
 #include "tensorflow/lite/experimental/litert/c/litert_tensor_buffer.h"
 #include "tensorflow/lite/experimental/litert/cc/litert_expected.h"
+#include "tensorflow/lite/experimental/litert/cc/litert_macros.h"
 #include "tensorflow/lite/experimental/litert/runtime/gl_buffer.h"
 
 namespace litert {
 namespace internal {
+
+#if LITERT_HAS_AHWB_SUPPORT
+
+PFNGLBUFFERSTORAGEEXTERNALEXTPROC glBufferStorageExternalEXT;
+PFNEGLGETNATIVECLIENTBUFFERANDROIDPROC eglGetNativeClientBufferANDROID;
+PFNEGLDUPNATIVEFENCEFDANDROIDPROC eglDupNativeFenceFDANDROID;
+PFNEGLCREATESYNCKHRPROC eglCreateSyncKHR;
+PFNEGLWAITSYNCKHRPROC eglWaitSyncKHR;
+PFNEGLCLIENTWAITSYNCKHRPROC eglClientWaitSyncKHR;
+PFNEGLDESTROYSYNCKHRPROC eglDestroySyncKHR;
+
+bool IsAhwbToGlInteropSupported() {
+  static const bool extensions_allowed = [] {
+    eglGetNativeClientBufferANDROID =
+        reinterpret_cast<PFNEGLGETNATIVECLIENTBUFFERANDROIDPROC>(
+            eglGetProcAddress("eglGetNativeClientBufferANDROID"));
+    glBufferStorageExternalEXT =
+        reinterpret_cast<PFNGLBUFFERSTORAGEEXTERNALEXTPROC>(
+            eglGetProcAddress("glBufferStorageExternalEXT"));
+    eglDupNativeFenceFDANDROID =
+        reinterpret_cast<PFNEGLDUPNATIVEFENCEFDANDROIDPROC>(
+            eglGetProcAddress("eglDupNativeFenceFDANDROID"));
+    eglCreateSyncKHR = reinterpret_cast<PFNEGLCREATESYNCKHRPROC>(
+        eglGetProcAddress("eglCreateSyncKHR"));
+    eglWaitSyncKHR = reinterpret_cast<PFNEGLWAITSYNCKHRPROC>(
+        eglGetProcAddress("eglWaitSyncKHR"));
+    eglClientWaitSyncKHR = reinterpret_cast<PFNEGLCLIENTWAITSYNCKHRPROC>(
+        eglGetProcAddress("eglClientWaitSyncKHR"));
+    eglDestroySyncKHR = reinterpret_cast<PFNEGLDESTROYSYNCKHRPROC>(
+        eglGetProcAddress("eglDestroySyncKHR"));
+    return eglClientWaitSyncKHR && eglWaitSyncKHR &&
+           eglGetNativeClientBufferANDROID && glBufferStorageExternalEXT &&
+           eglCreateSyncKHR && eglDupNativeFenceFDANDROID && eglDestroySyncKHR;
+  }();
+  return extensions_allowed;
+}
+
+Expected<GlBuffer> GlBuffer::AllocFromAhwbBuffer(AhwbBuffer& ahwb_buffer) {
+  LITERT_RETURN_IF_ERROR(
+      IsAhwbToGlInteropSupported(),
+      Unexpected(kLiteRtStatusErrorRuntimeFailure,
+                 "AHardwareBuffer to GL interop is not supported"));
+  LITERT_RETURN_IF_ERROR(
+      ahwb_buffer.ahwb != nullptr,
+      Unexpected(kLiteRtStatusErrorRuntimeFailure, "AHardwareBuffer is null"));
+
+  // Create GL buffer id.
+  GLuint gl_id;
+  glGenBuffers(1, &gl_id);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, gl_id);
+
+  // Create EGLClientBuffer from AHardwareBuffer.
+  EGLClientBuffer native_buffer =
+      eglGetNativeClientBufferANDROID(ahwb_buffer.ahwb);
+  LITERT_RETURN_IF_ERROR(
+      native_buffer != nullptr,
+      Unexpected(kLiteRtStatusErrorRuntimeFailure,
+                 "Failed to create EGLClientBuffer from AHardwareBuffer"));
+
+  LITERT_ASSIGN_OR_RETURN(
+      size_t size_bytes,
+      litert::internal::AhwbBuffer::GetSize(ahwb_buffer.ahwb));
+  LITERT_RETURN_IF_ERROR(size_bytes != 0,
+                         Unexpected(kLiteRtStatusErrorRuntimeFailure,
+                                    "AHardwareBuffer size is 0"));
+
+  // Create OpenGl buffer object backed by the AHardwareBuffer.
+  glBufferStorageExternalEXT(
+      GL_SHADER_STORAGE_BUFFER, 0, size_bytes, native_buffer,
+      GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_COHERENT_BIT_EXT |
+          GL_MAP_PERSISTENT_BIT_EXT);
+  // Check for OpenGL errors.
+  absl::Status status = tflite::gpu::gl::GetOpenGlErrors();
+  if (!status.ok()) {
+    return Unexpected(kLiteRtStatusErrorRuntimeFailure,
+                      absl::StrCat("glBufferStorageExternalEXT: Failed to "
+                                   "create GL buffer from AHardwareBuffer: ",
+                                   status.message()));
+  }
+  // Unbind the buffer.
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+
+  // Create GL buffer object.
+  tflite::gpu::gl::GlBuffer tflite_gl_buffer(GL_SHADER_STORAGE_BUFFER, /*id=*/0,
+                                             size_bytes, /*offset=*/0,
+                                             /*has_ownership=*/false);
+  return GlBuffer(std::move(tflite_gl_buffer), ahwb_buffer.ahwb);
+}
+#endif  // LITERT_HAS_AHWB_SUPPORT
 
 Expected<GlBuffer> GlBuffer::Alloc(size_t bytes_size) {
   tflite::gpu::gl::GlBuffer tflite_gl_buffer;
