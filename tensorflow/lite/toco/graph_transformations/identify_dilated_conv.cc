5d4be8091ae27096b90fd308d9883ccf228bcd0c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-05 23:33:37 -0800

    Automated Code Change
    PiperOrigin-RevId: 734008038

--- tensorflow/lite/toco/graph_transformations/identify_dilated_conv.cc
+++ tensorflow/lite/toco/graph_transformations/identify_dilated_conv.cc
@@ -12,9 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <cstddef>
 #include <string>
 #include <vector>
 
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/status.h"
--- tensorflow/lite/toco/graph_transformations/resolve_reorder_axes.cc
+++ tensorflow/lite/toco/graph_transformations/resolve_reorder_axes.cc
@@ -12,13 +12,15 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <algorithm>
+#include <cstddef>
 #include <memory>
 #include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
 
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/status.h"
