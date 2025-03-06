3c33432178b14a318e62dc41da6970f4aefcdffc
Terry Heo <terryheo@google.com>
2025-03-06 14:43:50 -0800

    dispatch_api: Fix nullptr reference
    Assigning nullptr to std::string is not recommended.
    
    PiperOrigin-RevId: 734294409

--- tensorflow/lite/experimental/litert/vendors/qualcomm/qnn_manager.cc
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/qnn_manager.cc
@@ -267,8 +267,8 @@ LiteRtStatus QnnManager::Init(absl::Span<const QnnBackend_Config_t*> configs,
                               std::optional<QnnHtpDevice_Arch_t> soc_model) {
   // Users can set ADSP_LIBRARY_PATH, if it is not set, we will set it to the
   // shared library directory.
-  std::string adsp_library_path = getenv("ADSP_LIBRARY_PATH");
-  if (adsp_library_path.empty() && shared_library_dir.has_value()) {
+  const char* adsp_library_path = getenv("ADSP_LIBRARY_PATH");
+  if (adsp_library_path == nullptr && shared_library_dir.has_value()) {
     setenv("ADSP_LIBRARY_PATH", shared_library_dir->data(), /*overwrite=*/1);
   }
 
