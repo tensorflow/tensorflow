3ce14f75e5ce2add937e5c710aca3ffa05b168c3
Amine Kaddour <kaddoura@oregonstate.edu>
2025-03-12 13:09:38 -0500

    added mock.patch.object to better handle static analysis of imports

--- tensorflow/python/client/device_lib_test.py
+++ tensorflow/python/client/device_lib_test.py
@@ -15,7 +15,8 @@
 """Tests for the SWIG-wrapped device lib."""
 
 from tensorflow.core.protobuf import config_pb2
-from tensorflow.python.client import device_lib
+from tensorflow.python.client import device_lib, _pywrap_device_lib
+from tensorflow.python.client.device_lib import device_attributes_pb2
 from tensorflow.python.framework import test_util
 from tensorflow.python.platform import googletest
 from tensorflow.python.platform import test
@@ -38,21 +39,23 @@ class DeviceLibTest(test_util.TensorFlowTestCase):
       self.assertGreater(len(devices), 1)
       self.assertIn("GPU", [d.device_type for d in devices])
 
-    @mock.patch('tensorflow.python.client._pywrap_device_lib.list_local_devices')
-    def testListLocalDevicesException(self, mock_list_local_devices):
-      mock_list_local_devices.side_effect = RuntimeError('Device list failure')
-      with self.assertRaisesRegex(RuntimeError) as e:
-        device_lib.list_local_devices()
-      self.assertIn('Device list failure', str(e.exception))
+  @mock.patch.object(_pywrap_device_lib, 'list_local_devices')
+  def testListLocalDevicesException(self, mock_list_local_devices):
+    mock_list_local_devices.side_effect = RuntimeError('Device list failure')
+    with self.assertRaisesRegex(RuntimeError) as e:
+      device_lib.list_local_devices()
+    self.assertIn('Device list failure', str(e.exception))
 
-    @mock.patch('tensorflow.python.client.device_lib.device_attributes_pb2.DeviceAttributes.ParseFromString')
-    @mock.patch('tensorflow.python.client._pywrap_device_lib.list_local_devices')
-    def testDeviceAttributesParsingException(self, mock_list_local_devices, mock_parse):
-      mock_list_local_devices.return_value = [b"invalid_proto_string"]
-      mock_parse.side_effect = ValueError("parsing failure")
-      with self.assertRaises(ValueError) as e:
-        device_lib.list_local_devices()
-      self.assertIn('Failed to parse devices', str(e.exception))
+  @mock.patch.object(device_attributes_pb2.DeviceAttributes, 'ParseFromString')
+  @mock.patch.object(_pywrap_device_lib, 'list_local_devices')
+  def testDeviceAttributesParsingException(
+    self, mock_list_local_devices, mock_parse
+  ):
+    mock_list_local_devices.return_value = [b"invalid_proto_string"]
+    mock_parse.side_effect = ValueError("parsing failure")
+    with self.assertRaises(ValueError) as e:
+      device_lib.list_local_devices()
+    self.assertIn('Failed to parse devices', str(e.exception))
 
 if __name__ == "__main__":
   googletest.main()
