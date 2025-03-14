b91833367b250f4a1062742c1dadd3973bff6d86
Bixia Zheng <bixia@google.com>
2025-03-14 10:17:47 -0700

    [xla:copy_insertion] Fix a problem in handling Send feeding into a while-loop.
    Previously, we only make a copy of the operand for rotated Send/SendDone inside
    a while-loop. We now also make a copy of the operand for the Send feeding into
    a while-loop so that the buffer for the operand does not have to hold different
    values with live range interference.
    
    PiperOrigin-RevId: 736904572

--- third_party/xla/xla/service/copy_insertion.cc
+++ third_party/xla/xla/service/copy_insertion.cc
@@ -2097,20 +2097,35 @@ absl::Status CopyInsertion::AddCopiesForAsyncSendRecv(
     return absl::OkStatus();
   }
 
-  // For send/recv outside of the while loop, live times are disjoint. No copies
-  // needed.
-  HloComputation* while_body = start_op->parent();
-  if (!while_body->IsWhileBodyComputation()) return absl::OkStatus();
+  HloComputation* parent = start_op->parent();
+  // If a Send is feeded into a pipelined while-loop, we need to make a copy
+  // of the Send operand and use it in the Send.
+  if (start_op->opcode() == HloOpcode::kSend &&
+      unique_user->opcode() == HloOpcode::kTuple &&
+      unique_user->users().size() == 1 &&
+      unique_user->users().front()->opcode() == HloOpcode::kWhile) {
+    HloInstruction* operand = start_op->mutable_operand(0);
+    HloInstruction* copied_operand =
+        parent->AddInstruction(HloInstruction::CreateUnary(
+            operand->shape(), HloOpcode::kCopy, operand));
+    TF_RETURN_IF_ERROR(operand->ReplaceUseWith(start_op, copied_operand));
+    return absl::OkStatus();
+  }
+
+  // For other cases that send/recv are outside of the while loop, live times
+  // are disjoint. No copies are needed.
+  if (!parent->IsWhileBodyComputation()) {
+    return absl::OkStatus();
+  }
 
   // Handle send case.
-  HloInstruction* done_op =
-      FindAsyncSendRecvDoneInWhileBody(while_body, start_op);
+  HloInstruction* done_op = FindAsyncSendRecvDoneInWhileBody(parent, start_op);
   // TODO(b/369589022): Disambiguate sync and async use of send/recv.
   if (done_op == nullptr) return absl::OkStatus();
   if (start_op->opcode() == HloOpcode::kSend) {
     HloInstruction* operand = start_op->mutable_operand(0);
     HloInstruction* copied_operand =
-        while_body->AddInstruction(HloInstruction::CreateUnary(
+        parent->AddInstruction(HloInstruction::CreateUnary(
             operand->shape(), HloOpcode::kCopy, operand));
     TF_RETURN_IF_ERROR(operand->ReplaceUseWith(start_op, copied_operand));
     TF_RETURN_IF_ERROR(done_op->AddControlDependencyTo(copied_operand));
@@ -2122,7 +2137,7 @@ absl::Status CopyInsertion::AddCopiesForAsyncSendRecv(
   PtrVec<HloInstruction*> done_op_users = done_op->users();
   ShapeTree<HloInstruction*> copies_added(done_op->shape());
   TF_ASSIGN_OR_RETURN(HloInstruction * done_op_copy,
-                      while_body->DeepCopyInstruction(
+                      parent->DeepCopyInstruction(
                           done_op, /*indices_to_copy=*/nullptr, &copies_added));
   for (auto [shape_index, instr] : copies_added) {
     if (instr != nullptr)
