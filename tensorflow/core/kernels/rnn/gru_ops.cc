3066e63f6656cdaca04fcfa67f2e1a618f5482fd
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-16 21:13:16 -0700

    Automated Code Change
    PiperOrigin-RevId: 737473372

--- tensorflow/core/kernels/rnn/gru_ops.cc
+++ tensorflow/core/kernels/rnn/gru_ops.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
 #define EIGEN_USE_THREADS
 
 #include "tensorflow/core/kernels/rnn/gru_ops.h"
