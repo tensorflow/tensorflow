819247756d279c8d689a5088f7cef42ff3b494de
Terry Sun <tesun@nvidia.com>
2025-03-18 13:24:27 -0700

    PR #23320: [NVIDIA GPU] Add utility functions for multi-host fast-interconnect domain
    Imported from GitHub PR https://github.com/openxla/xla/pull/23320
    
    On Blackwell and onwards, multiple hosts may be connected by NVLink thus creating a fast-interconnect domain beyond a single node. This PR leverages NVML APIs to detect the actual fast-interconnect domain. Followup PRs will update the logic in PjRt client on top of these utility functions.
    Copybara import of the project:
    
    --
    ee084714876336243fa3ba52a857d2d41124b730 by Terry Sun <tesun@nvidia.com>:
    
    pipe nvml to detect nvl
    
    --
    36e1f71dfabb75dbd0853355a7be8c0d6d103903 by Terry Sun <tesun@nvidia.com>:
    
    convert clusterUUID
    
    --
    917eb219a1bdebfc7a12f5849545615c83092c73 by Terry Sun <tesun@nvidia.com>:
    
    bake boot id into key
    
    --
    cbd909ab66ccf47f6e140632645fbb4d6c61c7c3 by Terry Sun <tesun@nvidia.com>:
    
    use global id and add getter
    
    --
    e487c4718eefe72e77708ffaf2c8cbbf6c979e8e by Terry Sun <tesun@nvidia.com>:
    
    use device ordinal and add doc string
    
    --
    2c761da2c04ea39d6cc79314f0dc14efef6d0380 by Terry Sun <tesun@nvidia.com>:
    
    merge fabric info into device proto
    
    --
    ed34dfbba07fbc5ffaaa914478381184a90f9cbb by Terry Sun <tesun@nvidia.com>:
    
    error handling and cleanup
    
    --
    c62f08691b7e3a59effc0f721948b30baea5b8a8 by Terry Sun <tesun@nvidia.com>:
    
    polish doc string
    
    --
    ab9d41e3142a0792859b9d37b99fe3ddfd9bbba8 by Terry Sun <tesun@nvidia.com>:
    
    conditional compile
    
    --
    29d857815a55d7df7911625cfeb35047bfbb07cb by Terry Sun <tesun@nvidia.com>:
    
    more conditional compile
    
    --
    524b569f1937db51babdaac04b50e9499da81a95 by Terry Sun <tesun@nvidia.com>:
    
    fix field designator order
    
    --
    7a0fb0f10be555c32403af4d226e687c650603d2 by Terry Sun <tesun@nvidia.com>:
    
    str buffer size
    
    --
    91e28b675b2f25e2b5d6c73732c8dae193b0dff3 by Terry Sun <tesun@nvidia.com>:
    
    compute capability condition
    
    --
    48fafb9dc4f44c451ca64bdb8ca15a1678cae398 by Terry Sun <tesun@nvidia.com>:
    
    protobuf backward compatibility
    
    --
    90f07569cf64d4d7d957cf4efe16fd2c9142eb4c by Terry Sun <tesun@nvidia.com>:
    
    guard cluster uuid size
    
    Merging this change closes #23320
    
    PiperOrigin-RevId: 738121931

--- third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.cc
+++ third_party/xla/xla/pjrt/gpu/se_gpu_pjrt_client.cc
@@ -119,6 +119,7 @@ limitations under the License.
 #if GOOGLE_CUDA
 #include "third_party/gpus/cuda/include/cuda.h"
 #include "third_party/gpus/cuda/include/cuda_runtime_api.h"
+#include "xla/service/gpu/model/gpu_collective_performance_model.h"
 #include "xla/stream_executor/gpu/gpu_cudamallocasync_allocator.h"
 #elif TENSORFLOW_USE_ROCM
 #include "rocm/rocm_config.h"
@@ -1159,9 +1160,17 @@ absl::StatusOr<DeviceTopologyPair> BuildDistributedDevices(
     device_proto->set_local_device_ordinal(ordinal_and_device.first);
     device_proto->set_name(desc->name());
     device_proto->set_vendor(desc->device_vendor());
-    device_proto->set_compute_capability(
-        MakeComputeCapabilityString(desc.get()));
+    auto compute_capability = MakeComputeCapabilityString(desc.get());
+    device_proto->set_compute_capability(compute_capability);
     device_proto->set_core_count(desc->core_count());
+#if defined(GOOGLE_CUDA) && CUDA_VERSION >= 12040
+    if (std::stoi(compute_capability) >= 9) {
+      auto fabric_info = GetDeviceFabricInfo(ordinal_and_device.first);
+      if (fabric_info.ok()) {
+        device_proto->set_fabric_uuid(*fabric_info);
+      }
+    }
+#endif  // defined(GOOGLE_CUDA) && CUDA_VERSION >= 12040
   }
 
   GlobalTopologyProto global_topology;
@@ -1413,6 +1422,57 @@ std::vector<std::unique_ptr<PjRtStreamExecutorDevice>> BuildLocalDevices(
     devices.push_back(std::move(device));
   }
   return devices;
+}
+
+absl::StatusOr<std::string> GetDeviceFabricInfo(const int device_ordinal) {
+#if defined(GOOGLE_CUDA) && CUDA_VERSION >= 12040
+  if (!gpu::GpuPerformanceWithCollectiveModel::InitNvml()) {
+    return absl::InternalError("Failed to initialize NVML library.");
+  }
+
+  // NVML library is not a part of the CUDA toolkit, so there might be a
+  // situation when user is using CUDA 12.4 an higher, but the host NVML
+  // version doen't have the required functions.
+  if (xla_nvmlDeviceGetHandleByPciBusId_v2 == nullptr ||
+      xla_nvmlDeviceGetGpuFabricInfoV == nullptr) {
+    return absl::InternalError("NVML library doesn't have required functions.");
+  }
+
+  char pciBusId[] = "00000000:00:00.0";
+  cudaDeviceGetPCIBusId(pciBusId, sizeof(pciBusId), device_ordinal);
+  nvmlDevice_t device;
+  auto get_bus_id_status =
+      xla_nvmlDeviceGetHandleByPciBusId_v2(pciBusId, &device);
+  CHECK_EQ(get_bus_id_status, NVML_SUCCESS);
+
+  nvmlGpuFabricInfoV_t fabricInfo = {
+      .version = nvmlGpuFabricInfo_v2,
+      .state = NVML_GPU_FABRIC_STATE_NOT_SUPPORTED};
+  auto get_fabric_info_status =
+      xla_nvmlDeviceGetGpuFabricInfoV(device, &fabricInfo);
+  CHECK_EQ(get_fabric_info_status, NVML_SUCCESS);
+
+  if (fabricInfo.state == NVML_GPU_FABRIC_STATE_NOT_SUPPORTED) {
+    VLOG(2) << "NVL is not supported";
+    return absl::InternalError("NVL is not supported");
+  }
+
+  CHECK_EQ(sizeof(fabricInfo.clusterUuid), 16);
+  std::string uuid_str = absl::StrFormat(
+      "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+      fabricInfo.clusterUuid[0], fabricInfo.clusterUuid[1],
+      fabricInfo.clusterUuid[2], fabricInfo.clusterUuid[3],
+      fabricInfo.clusterUuid[4], fabricInfo.clusterUuid[5],
+      fabricInfo.clusterUuid[6], fabricInfo.clusterUuid[7],
+      fabricInfo.clusterUuid[8], fabricInfo.clusterUuid[9],
+      fabricInfo.clusterUuid[10], fabricInfo.clusterUuid[11],
+      fabricInfo.clusterUuid[12], fabricInfo.clusterUuid[13],
+      fabricInfo.clusterUuid[14], fabricInfo.clusterUuid[15]);
+  return absl::StrCat(uuid_str, "/", std::to_string(fabricInfo.cliqueId));
+#else   // defined(GOOGLE_CUDA) && CUDA_VERSION >= 12040
+  VLOG(2) << "NVL is not supported";
+  return absl::InternalError("NVL is not supported");
+#endif  // defined(GOOGLE_CUDA) && CUDA_VERSION >= 12040
 }
 
 #if defined(GOOGLE_CUDA) || defined(TENSORFLOW_USE_ROCM)
