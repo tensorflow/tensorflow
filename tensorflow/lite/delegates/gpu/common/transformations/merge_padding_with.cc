c237a62ee19e68bfbabafb916836f9ddbbc78edf
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-21 00:52:49 -0700

    Automated Code Change
    PiperOrigin-RevId: 739085053

--- tensorflow/lite/delegates/gpu/common/transformations/merge_padding_with.cc
+++ tensorflow/lite/delegates/gpu/common/transformations/merge_padding_with.cc
@@ -17,12 +17,12 @@ limitations under the License.
 
 #include <memory>
 #include <string>
+#include <variant>
 #include <vector>
 
 #include "absl/memory/memory.h"
 #include "absl/strings/str_cat.h"
 #include "absl/types/any.h"
-#include "absl/types/variant.h"
 #include "tensorflow/lite/delegates/gpu/common/data_type.h"
 #include "tensorflow/lite/delegates/gpu/common/model.h"
 #include "tensorflow/lite/delegates/gpu/common/model_transformer.h"
