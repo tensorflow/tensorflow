8be8d5a4dd4525e5a9758102884c2083be3a9b72
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-25 19:19:38 -0700

    Automated Code Change
    PiperOrigin-RevId: 740573653

--- tensorflow/tools/graph_transforms/transform_graph.h
+++ tensorflow/tools/graph_transforms/transform_graph.h
@@ -18,6 +18,7 @@ limitations under the License.
 
 #include <vector>
 
+#include "absl/status/status.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/tools/graph_transforms/transform_utils.h"
