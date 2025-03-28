4cda95f0b53b33dcfee109e110b56f7fa994786e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-03-28 15:29:45 -0700

    Remove deprecated `Shape::rank()` now that it's no longer used.
    PiperOrigin-RevId: 741664867

--- third_party/xla/xla/shape.h
+++ third_party/xla/xla/shape.h
@@ -89,11 +89,6 @@ class Shape {
   // without layout. e.g. "F32[42,12] {0, 1}" or "F32[64]".
   std::string ToString(bool print_layout = false) const;
 
-  // Returns the number of dimensions of the given array shape. Returns 0 for
-  // non-array shapes.
-  ABSL_DEPRECATE_AND_INLINE()
-  inline int64_t rank() const { return dimensions().size(); }
-
   // Returns whether the shape is of the specified type (array, tuple, etc).
   bool IsArray() const { return primitive_util::IsArrayType(element_type()); }
   bool IsTuple() const { return element_type() == TUPLE; }
