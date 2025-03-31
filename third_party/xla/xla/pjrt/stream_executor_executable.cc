59f2d850d14539a80060544a81eedcd3ed3b0067
Changhui Lin <changhuilin@google.com>
2025-03-31 13:02:28 -0700

    Implement `Compile()` and `DeserializeExecutable()` that return an unloaded executable for PJRT stream executor.
    Also refactor `LoadSerializedExecutable()` and `Load()` accordingly.
    
    PiperOrigin-RevId: 742367054

--- third_party/xla/xla/pjrt/stream_executor_executable.cc
+++ third_party/xla/xla/pjrt/stream_executor_executable.cc
@@ -18,11 +18,15 @@ limitations under the License.
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "xla/pjrt/host_memory_spaces.h"
 #include "xla/pjrt/stream_executor_executable.pb.h"
 #include "xla/service/compiler.h"
+#include "xla/shape.h"
+#include "xla/util.h"
 #include "tsl/platform/statusor.h"
 
 namespace xla {
@@ -50,4 +54,57 @@ absl::StatusOr<std::string> StreamExecutorExecutable::SerializeExecutable()
                       compile_options_.ToProto());
   return proto.SerializeAsString();
 }
+
+namespace {
+
+absl::StatusOr<absl::string_view> MemoryKindFromSimpleShape(
+    const Shape& shape, absl::string_view default_memory_kind) {
+  if (!shape.has_layout()) {
+    return default_memory_kind;
+  }
+  switch (shape.layout().memory_space()) {
+    case Layout::kHostMemorySpace:
+      return PinnedHostMemorySpace::kKind;
+    case Layout::kGenericFastMemorySpace:
+    case Layout::kDefaultMemorySpace:
+      return default_memory_kind;
+    default:
+      return InvalidArgument("Unexpected memory space %d in output layout",
+                             shape.layout().memory_space());
+  }
+}
+
+absl::StatusOr<std::vector<absl::string_view>> MemoryKindsFromShape(
+    const Shape& shape, absl::string_view default_memory_kind) {
+  if (!shape.IsTuple()) {
+    TF_ASSIGN_OR_RETURN(absl::string_view memory_kind,
+                        MemoryKindFromSimpleShape(shape, default_memory_kind));
+    return {{memory_kind}};
+  }
+  std::vector<absl::string_view> result;
+  result.reserve(shape.tuple_shapes_size());
+  for (const auto& element_shape : shape.tuple_shapes()) {
+    TF_ASSIGN_OR_RETURN(
+        absl::string_view element_memory_kind,
+        MemoryKindFromSimpleShape(element_shape, default_memory_kind));
+    result.push_back(element_memory_kind);
+  }
+  return result;
+}
+
+}  // namespace
+
+absl::StatusOr<std::vector<std::vector<absl::string_view>>>
+StreamExecutorExecutable::GetOutputMemoryKinds() const {
+  TF_ASSIGN_OR_RETURN(auto shapes, GetOutputShapes());
+  std::vector<std::vector<absl::string_view>> out;
+  out.reserve(shapes.size());
+  for (const auto& shape : shapes) {
+    TF_ASSIGN_OR_RETURN(std::vector<absl::string_view> memory_kind,
+                        MemoryKindsFromShape(shape, default_memory_kind_));
+    out.push_back(memory_kind);
+  }
+  return out;
+}
+
 }  // namespace xla
