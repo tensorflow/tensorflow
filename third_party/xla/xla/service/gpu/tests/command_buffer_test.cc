44919f1b593683fcbed963cd0a8ab783e92def2a
Eugene Zhulenev <ezhulenev@google.com>
2025-03-31 00:33:29 -0700

    [xla:gpu] Add an HLO test for testing conditional (case) command
    PiperOrigin-RevId: 742165114

--- third_party/xla/xla/service/gpu/tests/command_buffer_test.cc
+++ third_party/xla/xla/service/gpu/tests/command_buffer_test.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
 #include <utility>
 
 #include <gtest/gtest.h>
@@ -67,6 +68,132 @@ TEST_F(CommandBufferTest, Fusions) {
 
   Literal result = ExecuteNoHloPasses(std::move(module), {&argument});
   EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+}
+
+TEST_F(CommandBufferTest, TrueFalseConditional) {
+  constexpr absl::string_view hlo_text = R"(
+  HloModule m, is_scheduled=true
+
+  double {
+    p0 = f32[2,2] parameter(0)
+    ROOT add = f32[2,2] add(p0, p0)
+  }
+
+  square {
+    p0 = f32[2,2] parameter(0)
+    ROOT add = f32[2,2] multiply(p0, p0)
+  }
+
+  double_computation {
+    p0 = f32[2,2] parameter(0)
+    ROOT double = f32[2,2] fusion(p0), kind=kLoop, calls=double
+  }
+
+  square_computation {
+    p0 = f32[2,2] parameter(0)
+    ROOT square = f32[2,2] fusion(p0), kind=kLoop, calls=square
+  }
+
+  command_buffer {
+    p0 = pred[] parameter(0)
+    p1 = f32[2,2] parameter(1)
+    ROOT conditional = f32[2,2] conditional(p0, p1, p1),
+                                true_computation=double_computation,
+                                false_computation=square_computation
+  }
+
+  ENTRY main {
+    p0 = pred[] parameter(0)
+    p1 = f32[2,2] parameter(1)
+    ROOT call = f32[2,2] call(p0, p1), to_apply=command_buffer
+  })";
+
+  Literal p1 = LiteralUtil::CreateR2<float>({{1.0, 2.0}, {3.0, 4.0}});
+
+  {  // Execute `true` branch.
+    TF_ASSERT_OK_AND_ASSIGN(auto m, ParseAndReturnVerifiedModule(hlo_text));
+
+    Literal pred = LiteralUtil::CreateR0<bool>(true);
+    Literal expected = LiteralUtil::CreateR2<float>({{2.0, 4.0}, {6.0, 8.0}});
+    Literal result = ExecuteNoHloPasses(std::move(m), {&pred, &p1});
+    EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+  }
+
+  {  // Execute `false` branch.
+    TF_ASSERT_OK_AND_ASSIGN(auto m, ParseAndReturnVerifiedModule(hlo_text));
+
+    Literal pred = LiteralUtil::CreateR0<bool>(false);
+    Literal expected = LiteralUtil::CreateR2<float>({{1.0, 4.0}, {9.0, 16.0}});
+    Literal result = ExecuteNoHloPasses(std::move(m), {&pred, &p1});
+    EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+  }
+}
+
+TEST_F(CommandBufferTest, IndexConditional) {
+  constexpr absl::string_view hlo_text = R"(
+  HloModule m, is_scheduled=true
+
+  double {
+    p0 = f32[2,2] parameter(0)
+    ROOT add = f32[2,2] add(p0, p0)
+  }
+
+  square {
+    p0 = f32[2,2] parameter(0)
+    ROOT add = f32[2,2] multiply(p0, p0)
+  }
+
+  double_computation {
+    p0 = f32[2,2] parameter(0)
+    ROOT double = f32[2,2] fusion(p0), kind=kLoop, calls=double
+  }
+
+  square_computation {
+    p0 = f32[2,2] parameter(0)
+    ROOT square = f32[2,2] fusion(p0), kind=kLoop, calls=square
+  }
+
+  command_buffer {
+    p0 = s32[] parameter(0)
+    p1 = f32[2,2] parameter(1)
+    ROOT conditional = f32[2,2] conditional(p0, p1, p1),
+      branch_computations={double_computation, square_computation}
+  }
+
+  ENTRY main {
+    p0 = s32[] parameter(0)
+    p1 = f32[2,2] parameter(1)
+    ROOT call = f32[2,2] call(p0, p1), to_apply=command_buffer
+  })";
+
+  Literal p1 = LiteralUtil::CreateR2<float>({{1.0, 2.0}, {3.0, 4.0}});
+
+  {  // Execute `0` branch.
+    TF_ASSERT_OK_AND_ASSIGN(auto m, ParseAndReturnVerifiedModule(hlo_text));
+
+    Literal index = LiteralUtil::CreateR0<int32_t>(0);
+    Literal expected = LiteralUtil::CreateR2<float>({{2.0, 4.0}, {6.0, 8.0}});
+    Literal result = ExecuteNoHloPasses(std::move(m), {&index, &p1});
+    EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+  }
+
+  {  // Execute `1` branch.
+    TF_ASSERT_OK_AND_ASSIGN(auto m, ParseAndReturnVerifiedModule(hlo_text));
+
+    Literal index = LiteralUtil::CreateR0<int32_t>(1);
+    Literal expected = LiteralUtil::CreateR2<float>({{1.0, 4.0}, {9.0, 16.0}});
+    Literal result = ExecuteNoHloPasses(std::move(m), {&index, &p1});
+    EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+  }
+
+  {  // Execute `1024` branch (our of bound index executes N-1 branch).
+    TF_ASSERT_OK_AND_ASSIGN(auto m, ParseAndReturnVerifiedModule(hlo_text));
+
+    Literal index = LiteralUtil::CreateR0<int32_t>(1024);
+    Literal expected = LiteralUtil::CreateR2<float>({{1.0, 4.0}, {9.0, 16.0}});
+    Literal result = ExecuteNoHloPasses(std::move(m), {&index, &p1});
+    EXPECT_TRUE(LiteralTestUtil::Equal(expected, result));
+  }
 }
 
 }  // namespace
