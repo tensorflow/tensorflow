00a6963b5ea10d9c336a72d02e525279f46c4c95
Benjamin Chetioui <bchetioui@google.com>
2025-04-01 07:31:47 -0700

    [XLA:GPU][NFC] Remove dead `algorithm_util::IsAmpere` util.
    PiperOrigin-RevId: 742690841

--- third_party/xla/xla/service/algorithm_util.cc
+++ third_party/xla/xla/service/algorithm_util.cc
@@ -148,13 +148,6 @@ bool HasFastAccum(PrecisionConfig::Algorithm algorithm) {
   return algorithm == PrecisionConfig::ALG_DOT_ANY_F8_ANY_F8_F32_FAST_ACCUM;
 }
 
-bool IsAmpere(stream_executor::GpuComputeCapability gpu_compute_capability) {
-  return std::holds_alternative<se::CudaComputeCapability>(
-             gpu_compute_capability) &&
-         std::get<se::CudaComputeCapability>(gpu_compute_capability).major ==
-             stream_executor::CudaComputeCapability::kAmpere;
-}
-
 // It's clear that those libraries could support more, but we only list the ones
 // which we explicitly test for now.
 bool IsSupportedByCublasOrCublasLt(
