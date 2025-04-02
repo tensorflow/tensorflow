04a31546300362cd5b198486ed3584a5f7f00bfa
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-01 20:20:06 -0700

    Automated Code Change
    PiperOrigin-RevId: 742943430

--- third_party/xla/xla/pjrt/pjrt_future_test.cc
+++ third_party/xla/xla/pjrt/pjrt_future_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include <gtest/gtest.h>
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "tsl/platform/test.h"
