a85e7ee8fb563385526798cb90284c971d443459
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-01 20:20:07 -0700

    Automated Code Change
    PiperOrigin-RevId: 742943432

--- third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_client.h
+++ third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_client.h
@@ -73,6 +73,7 @@ limitations under the License.
 #include "xla/tsl/framework/allocator.h"
 #include "xla/tsl/platform/threadpool.h"
 #include "xla/util.h"
+#include "xla/xla.pb.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/fingerprint.h"
 
