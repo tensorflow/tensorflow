ba268154571bffb32028cc9f1185241210455bd3
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-04 17:24:33 -0700

    Temporarily remove input pipeline analyzer.
    PiperOrigin-RevId: 744116422

--- tensorflow/core/profiler/convert/xplane_to_tool_names.cc
+++ tensorflow/core/profiler/convert/xplane_to_tool_names.cc
@@ -39,7 +39,9 @@ absl::StatusOr<std::string> GetAvailableToolNames(
     tools.reserve(11);
     tools.push_back(is_cloud_vertex_ai ? "trace_viewer" : "trace_viewer@");
     tools.push_back("overview_page");
-    tools.push_back("input_pipeline_analyzer");
+    // TODO(jonahweaver): Re-enable input_pipeline_analyzer when it is ready.
+    // b/407096031
+    // tools.push_back("input_pipeline_analyzer");
     tools.push_back("framework_op_stats");
     tools.push_back("memory_profile");
     tools.push_back("pod_viewer");
