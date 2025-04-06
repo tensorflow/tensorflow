88d978ee5ae2f6642883eea22615b8039dc6f29a
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-05 19:39:12 -0700

    Automated Code Change
    PiperOrigin-RevId: 744351136

--- third_party/xla/xla/hlo/transforms/simplifiers/bfloat16_conversion_folding_test.cc
+++ third_party/xla/xla/hlo/transforms/simplifiers/bfloat16_conversion_folding_test.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <cstdint>
 #include <optional>
 
+#include <gtest/gtest.h>
 #include "absl/status/statusor.h"
 #include "xla/hlo/ir/collective_device_list.h"
 #include "xla/hlo/ir/hlo_computation.h"
