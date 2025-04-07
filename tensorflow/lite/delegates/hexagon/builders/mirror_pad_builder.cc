b43757eb62315ae9d95ff82b1046777dd0296fee
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-06 23:06:47 -0700

    Automated Code Change
    PiperOrigin-RevId: 744596327

--- tensorflow/lite/delegates/hexagon/builders/mirror_pad_builder.cc
+++ tensorflow/lite/delegates/hexagon/builders/mirror_pad_builder.cc
@@ -16,7 +16,7 @@ limitations under the License.
 
 #include <stdint.h>
 
-#include <limits>
+#include <vector>
 
 #include "tensorflow/lite/core/c/builtin_op_data.h"
 #include "tensorflow/lite/core/c/common.h"
