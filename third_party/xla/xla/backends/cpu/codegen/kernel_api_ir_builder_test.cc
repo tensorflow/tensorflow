2d858f5a378d2634a18978fa831df62d5a41146a
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-06 20:30:48 -0700

    Automated Code Change
    PiperOrigin-RevId: 744570197

--- third_party/xla/xla/backends/cpu/codegen/kernel_api_ir_builder_test.cc
+++ third_party/xla/xla/backends/cpu/codegen/kernel_api_ir_builder_test.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "llvm/IR/Constants.h"
