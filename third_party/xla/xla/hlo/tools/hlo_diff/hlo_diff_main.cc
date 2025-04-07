646deb57e9b42af4dfd6f335afa3adf692c87762
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-07 09:06:32 -0700

    Remove Diff Eval logging as eval runs are not calculated.
    PiperOrigin-RevId: 744742094

--- third_party/xla/xla/hlo/tools/hlo_diff/hlo_diff_main.cc
+++ third_party/xla/xla/hlo/tools/hlo_diff/hlo_diff_main.cc
@@ -29,7 +29,6 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/parser/hlo_parser.h"
-#include "xla/hlo/tools/hlo_diff/hlo_diff_eval.h"
 #include "xla/hlo/tools/hlo_diff/hlo_diff_result.h"
 #include "xla/hlo/tools/hlo_diff/hlo_diff_summary.h"
 #include "xla/hlo/tools/hlo_diff/hlo_gumgraph_diff.h"
@@ -160,7 +159,6 @@ absl::Status RunGumgraphDiff(HloModule& first_module, HloModule& second_module,
   const DiffResult& diff = *hlo_gumgraph_diff.diff_result;
   const DiffSummary& diff_summary = *hlo_gumgraph_diff.diff_summary;
   LogDiffResult(diff);
-  LogDiffEval(*hlo_gumgraph_diff.diff_eval);
   std::ostringstream text;
   RenderTextSummary(diff, text);
   std::cout << text.str() << '\n';
