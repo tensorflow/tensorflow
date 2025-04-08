afa14754739afcf5c3319ebf012992f85115d22f
Adrian Kuegel <akuegel@google.com>
2025-04-07 23:22:35 -0700

    Reverts c2032e84aec1063e48007e528b53793c5e368b1c
    PiperOrigin-RevId: 744995845

--- third_party/xla/xla/hlo/analysis/hlo_reachability.h
+++ third_party/xla/xla/hlo/analysis/hlo_reachability.h
@@ -55,8 +55,7 @@ class HloReachabilityMap {
   // dependencies (operands) and control dependencies are considered for
   // reachability. Trivially an instruction is reachable from itself.
   static std::unique_ptr<HloReachabilityMap> Build(
-      const HloComputation* computation,
-      const std::vector<HloInstruction*>& po_instructions = {});
+      const HloComputation* computation);
 
   // Similar to the above Build operation except that it tries to identify
   // paths between instructions that do not contain control instructions
--- third_party/xla/xla/hlo/transforms/collectives/collectives_schedule_linearizer.cc
+++ third_party/xla/xla/hlo/transforms/collectives/collectives_schedule_linearizer.cc
@@ -43,15 +43,14 @@ absl::StatusOr<bool> CollectivesScheduleLinearizer::Run(
        module->MakeNonfusionComputations(execution_threads)) {
     std::unique_ptr<HloReachabilityMap> reachability;
     HloInstruction* prev_done = nullptr;
-    auto post_order = computation->MakeInstructionPostOrder();
-    for (HloInstruction* inst : post_order) {
+    for (HloInstruction* inst : computation->MakeInstructionPostOrder()) {
       auto* next = DynCast<HloCollectiveInstruction>(inst);
       if (!next) {
         continue;
       }
       // Build reachability map on demand if we actually see collectives.
       if (!reachability) {
-        reachability = HloReachabilityMap::Build(computation, post_order);
+        reachability = HloReachabilityMap::Build(computation);
       }
       // Derive the 'start' and 'done' peers of this instruction. For non-async
       // variants of collectives, they are the same as this instruction. For
