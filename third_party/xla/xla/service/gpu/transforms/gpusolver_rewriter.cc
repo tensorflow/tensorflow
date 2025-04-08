649a47160e01543607fad5fe0b487292050f6453
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-08 11:12:08 -0700

    Fix clang-tidy in gpusolver_rewriter and associated build rule.
    PiperOrigin-RevId: 745218065

--- third_party/xla/xla/service/gpu/transforms/gpusolver_rewriter.cc
+++ third_party/xla/xla/service/gpu/transforms/gpusolver_rewriter.cc
@@ -17,11 +17,16 @@ limitations under the License.
 
 #include <cstdint>
 #include <functional>
+#include <memory>
 #include <utility>
 #include <vector>
 
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/functional/any_invocable.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/comparison_util.h"
 #include "xla/hlo/ir/hlo_computation.h"
@@ -35,11 +40,10 @@ limitations under the License.
 #include "xla/shape_util.h"
 #include "xla/stream_executor/blas.h"
 #include "xla/stream_executor/gpu_solver_context.h"
+#include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
-#include "tsl/platform/errors.h"
-#include "tsl/platform/logging.h"
-#include "tsl/platform/statusor.h"
 
 namespace xla {
 namespace gpu {
