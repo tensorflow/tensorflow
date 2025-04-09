f9541fea975bcb5110e3b555c8f74f0917c54a2b
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-08 19:18:43 -0700

    Automated Code Change
    PiperOrigin-RevId: 745380730

--- tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h
+++ tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h
@@ -21,6 +21,7 @@ limitations under the License.
 #include <unordered_set>
 #include <vector>
 
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
