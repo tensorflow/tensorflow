92ea23d5c602aa6a929b67d1f97612b5d44b97c3
Kevin Gleason <gleasonk@google.com>
2025-04-09 12:22:37 -0700

    Fix direct HLO->StableHLO conversion for programs with bounded dynamism
    PiperOrigin-RevId: 745687946

--- third_party/xla/xla/hlo/translate/stablehlo.cc
+++ third_party/xla/xla/hlo/translate/stablehlo.cc
@@ -18,9 +18,13 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
+#include "mhlo/transforms/passes.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/LogicalResult.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/Extensions/AllExtensions.h"
@@ -28,9 +32,11 @@ limitations under the License.
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/UB/IR/UBOps.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Location.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/OwningOpRef.h"
+#include "mlir/IR/Types.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
 #include "stablehlo/dialect/Register.h"
@@ -40,6 +46,7 @@ limitations under the License.
 #include "xla/hlo/translate/mhlo_to_hlo/mlir_hlo_to_hlo.h"
 #include "xla/hlo/translate/mhlo_to_hlo/module_attributes_exporter.h"
 #include "xla/mlir/utils/error_util.h"
+#include "xla/mlir_hlo/mhlo/IR/hlo_ops.h"
 #include "xla/mlir_hlo/mhlo/IR/register.h"
 #include "xla/mlir_hlo/mhlo/transforms/passes.h"
 #include "xla/mlir_hlo/stablehlo_ext/transforms/passes.h"
@@ -47,15 +54,47 @@ limitations under the License.
 #include "xla/service/llvm_ir/llvm_util.h"
 #include "xla/tsl/platform/errors.h"
 
+#define DEBUG_TYPE "xla-translate"
+
 namespace xla {
 
 namespace {
+
+bool isBoundedDynamic(mlir::Type type) {
+  LLVM_DEBUG(llvm::dbgs() << "isBoundedDynamic: " << type << "\n");
+  if (!llvm::isa<mlir::RankedTensorType>(type)) {
+    return false;
+  }
+  auto encoding = llvm::cast<mlir::RankedTensorType>(type).getEncoding();
+  return encoding && llvm::isa<mlir::mhlo::TypeExtensionsAttr>(encoding);
+}
+
+bool hasBoundedDynamism(mlir::ModuleOp module) {
+  bool has_bounded_dynamism = false;
+  module->walk([&](mlir::Operation* op) {
+    auto results = op->getResultTypes();
+    has_bounded_dynamism |= llvm::any_of(results, isBoundedDynamic);
+    if (has_bounded_dynamism) {
+      return mlir::WalkResult::interrupt();
+    }
+    return mlir::WalkResult::advance();
+  });
+  return has_bounded_dynamism;
+}
+
 absl::Status MhloToStablehlo(mlir::ModuleOp module) {
+  LLVM_DEBUG(llvm::dbgs() << "MHLO to StableHLO\n");
   auto context = module.getContext();
   mlir::PassManager pm(context);
   mlir::BaseScopedDiagnosticHandler diag_handler(context);
   mlir::mhlo::HloLegalizeToStablehloPassOptions options;
   options.allow_xla_features_ = true;
+  bool has_bounded_dynamism = hasBoundedDynamism(module);
+  if (has_bounded_dynamism) {
+    // Need to converge program to MHLO before StableHLO in the presence of
+    // bounded dynamism.
+    pm.addPass(mlir::mhlo::createStablehloLegalizeToHloPass());
+  }
   pm.addPass(mlir::mhlo::createHloLegalizeToStablehloPass(options));
   if (failed(pm.run(module))) {
     return diag_handler.ConsumeStatus();
@@ -151,7 +190,7 @@ absl::StatusOr<mlir::OwningOpRef<mlir::ModuleOp>> ConvertHloToStablehlo(
   TF_RETURN_IF_ERROR(HloModuleImporter(mlir_module.get(),
                                        /*import_all_computation=*/true,
                                        /*flatten_computation_args_result=*/true,
-                                       /*emit_stablehlo=*/true)
+                                       /*emit_stablehlo=*/false)
                          .Import(*hlo_module));
   TF_RETURN_IF_ERROR(MhloToStablehlo(mlir_module.get()));
   return std::move(mlir_module);
@@ -164,7 +203,7 @@ absl::StatusOr<mlir::OwningOpRef<mlir::ModuleOp>> ConvertHloToStablehlo(
   TF_RETURN_IF_ERROR(HloModuleImporter(mlir_module.get(),
                                        /*import_all_computation=*/true,
                                        /*flatten_computation_args_result=*/true,
-                                       /*emit_stablehlo=*/true)
+                                       /*emit_stablehlo=*/false)
                          .Import(*hlo_module_proto));
   TF_RETURN_IF_ERROR(MhloToStablehlo(mlir_module.get()));
   return std::move(mlir_module);
