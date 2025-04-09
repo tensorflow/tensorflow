2df2b8059717e39fdf19b49908a083bf13b46d48
Goran Flegar <gflegar@google.com>
2025-04-09 02:57:04 -0700

    Add contracting split (split-K) selection logic to dynamic search space
    First step towards the full search space. We figure out the contracting split
    based on the problem size, and a few other properties of the search space (still hardcoded for now).
    
    We also have an option for forcing a specific split, which is helpful for both
    disabling autotuning that parameter, and will facilitate support for analytically setting it in the future.
    
    PiperOrigin-RevId: 745505400

--- third_party/xla/xla/service/gpu/autotuning/dot_search_space.cc
+++ third_party/xla/xla/service/gpu/autotuning/dot_search_space.cc
@@ -15,35 +15,153 @@ limitations under the License.
 
 #include "xla/service/gpu/autotuning/dot_search_space.h"
 
+#include <algorithm>
+#include <cstdint>
+#include <optional>
 #include <string>
 #include <vector>
 
+#include "absl/log/log.h"
 #include "absl/strings/str_format.h"
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/service/gpu/matmul_utils.h"
+#include "xla/shape.h"
+#include "xla/shape_util.h"
 #include "xla/stream_executor/device_description.h"
+#include "xla/tsl/lib/core/bits.h"
+#include "xla/util.h"
+#include "tsl/platform/protobuf.h"
 
 namespace xla::gpu {
+namespace {
+
+// Returns the size (in number of elements) of the subshape of `shape` defined
+// by `dimensions`.
+int64_t GetSizeInDimensions(
+    const Shape& shape,
+    const tsl::protobuf::RepeatedField<int64_t>& dimensions) {
+  int64_t size = 1;
+  for (int64_t dim : dimensions) {
+    size *= shape.dimensions(dim);
+  }
+  return size;
+}
+
+// Finds the next power of two larger than or equal to x.
+//
+// Unlike tsl::NextPowerOfTwo, doesn't crash for 0.
+int64_t NextPowerOfTwo(int64_t x) {
+  if (x == 0) {
+    return 1;
+  }
+  return tsl::NextPowerOfTwoS64(x);
+}
+
+}  // namespace
 
 TritonDotFusionSearchSpace::TritonDotFusionSearchSpace(
     const se::DeviceDescription& device_description,
     const HloDotInstruction* dot)
     :  // Set up basic information about the hardware and the problem.
-      device_description_(device_description) {
-  // TODO: b/404470821 - Do something based on `dot`.
-}
+      device_description_(device_description),
+      contracting_size_(GetSizeInDimensions(
+          dot->operand(0)->shape(),
+          dot->dot_dimension_numbers().lhs_contracting_dimensions())),
+      batch_size_(GetSizeInDimensions(
+          dot->operand(0)->shape(),
+          dot->dot_dimension_numbers().lhs_batch_dimensions())),
+      lhs_parallel_size_(ShapeUtil::ElementsIn(dot->operand(0)->shape()) /
+                         (contracting_size_ * batch_size_)),
+      rhs_parallel_size_(ShapeUtil::ElementsIn(dot->operand(1)->shape()) /
+                         (contracting_size_ * batch_size_)),
+      // TODO: b/404470821 - Compute these from the problem properties instead
+      // of hardcoding.
+      desired_total_warps_(2160),
+      max_out_tile_{64, 128},
+      min_warps_per_cta_(4),
+      min_contracting_tile_size_(16),
+      max_contracting_split_(GetMaxContractingSplit(max_out_tile_)) {}
 
-std::vector<TritonGemmConfig> TritonDotFusionSearchSpace::GenerateConfigs() {
+std::vector<TritonGemmConfig> TritonDotFusionSearchSpace::GenerateConfigs(
+    std::optional<int64_t> force_contracting_split) {
+  std::vector<TritonGemmConfig> configs;
+  if (force_contracting_split.has_value()) {
+    TritonGemmConfig config;
+    config.split_k = force_contracting_split.value();
+    configs.push_back(config);
+  } else {
+    configs = GenerateContractingSplitFactors();
+  }
   // TODO: b/404470821 - Implement this properly rather than hardcoding the
-  // config.
-  return {TritonGemmConfig(
-      /*block_m=*/64, /*block_n=*/128, /*block_k=*/64,
-      /*split_k=*/1, /*num_stages=*/3, /*num_warps=*/4,
-      /*num_ctas=*/1)};
+  // config parameters.
+  for (auto& config : configs) {
+    config.block_m = 64;
+    config.block_n = 128;
+    config.block_k = 64;
+    config.num_stages = 3;
+    config.num_warps = 4;
+    config.num_ctas = 1;
+  }
+  return configs;
 }
 
 std::string TritonDotFusionSearchSpace::Serialize() {
-  return absl::StrFormat("TODO: b/404470821 - Implement this.");
+  return absl::StrFormat(
+      "problem_size_BxMxNxK: %dx%dx%dx%d "
+      "tile_range_SxMxNxK: [1-%d]x[1-%d]x[1-%d]x[%d-?] "
+      "desired_total_warps: %d warps_per_block: [%d-?]",
+      batch_size_, lhs_parallel_size_, rhs_parallel_size_, contracting_size_,
+      max_contracting_split_, max_out_tile_.lhs_dim, max_out_tile_.rhs_dim,
+      min_contracting_tile_size_, desired_total_warps_, min_warps_per_cta_);
+}
+
+int64_t TritonDotFusionSearchSpace::GetNumResultTiles(
+    OutputTile output_tile) const {
+  return batch_size_ *
+         CeilOfRatio<int64_t>(lhs_parallel_size_, output_tile.lhs_dim) *
+         CeilOfRatio<int64_t>(rhs_parallel_size_, output_tile.rhs_dim);
+}
+
+int TritonDotFusionSearchSpace::GetMaxContractingSplit(
+    OutputTile output_tile) const {
+  const int64_t desired_num_blocks = desired_total_warps_ / min_warps_per_cta_;
+  VLOG(5) << "Computing split_k: Considering output tile "
+          << output_tile.lhs_dim << "x" << output_tile.rhs_dim;
+  VLOG(5) << "Computing split_k: Want up to " << desired_num_blocks
+          << " blocks to occupy all cores.";
+
+  const int64_t min_result_tiles = GetNumResultTiles(output_tile);
+  VLOG(5) << "Computing split_k: Without split_k have " << min_result_tiles
+          << " tiles.";
+
+  const int64_t split_for_occupancy =
+      NextPowerOfTwo(CeilOfRatio(desired_num_blocks, min_result_tiles));
+  VLOG(5) << "Computing split_k: Want split_k of up to " << split_for_occupancy
+          << " for sufficient occupancy.";
+
+  const int64_t split_for_contracting_size =
+      NextPowerOfTwo(contracting_size_ / min_contracting_tile_size_);
+  VLOG(5) << "Computing split_k: Can't have split_k more than "
+          << split_for_contracting_size
+          << " to have sufficiently large contracting dimension.";
+
+  const int64_t split =
+      std::min(split_for_occupancy, split_for_contracting_size);
+  VLOG(5) << "Computing split_k: max_split_k = " << split;
+  return split;
+}
+
+std::vector<TritonGemmConfig>
+TritonDotFusionSearchSpace::GenerateContractingSplitFactors() {
+  std::vector<TritonGemmConfig> configs;
+  TritonGemmConfig config;
+  for (int split = 1; split <= max_contracting_split_; split *= 2) {
+    config.split_k = split;
+    VLOG(10) << "Generating contracting split factors: config = "
+             << config.ToString();
+    configs.push_back(config);
+  }
+  return configs;
 }
 
 }  // namespace xla::gpu
--- third_party/xla/xla/service/gpu/autotuning/dot_search_space_test.cc
+++ third_party/xla/xla/service/gpu/autotuning/dot_search_space_test.cc
@@ -19,6 +19,8 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/status/statusor.h"
+#include "absl/strings/str_format.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/testlib/hlo_hardware_independent_test_base.h"
@@ -31,40 +33,113 @@ limitations under the License.
 namespace xla::gpu {
 namespace {
 
+using ::testing::Eq;
 using ::testing::Field;
 using ::testing::Ge;
+using ::testing::IsEmpty;
+using ::testing::Le;
+
+template <typename MatcherType>
+auto BlockMIs(MatcherType matcher) {
+  return Field("block_m", &TritonGemmConfig::block_m, matcher);
+}
+template <typename MatcherType>
+auto BlockNIs(MatcherType matcher) {
+  return Field("block_n", &TritonGemmConfig::block_n, matcher);
+}
+template <typename MatcherType>
+auto BlockKIs(MatcherType matcher) {
+  return Field("block_k", &TritonGemmConfig::block_k, matcher);
+}
+template <typename MatcherType>
+auto SplitKIs(MatcherType matcher) {
+  return Field("split_k", &TritonGemmConfig::split_k, matcher);
+}
+template <typename MatcherType>
+auto NumStagesIs(MatcherType matcher) {
+  return Field("num_stages", &TritonGemmConfig::num_stages, matcher);
+}
+template <typename MatcherType>
+auto NumWarpsIs(MatcherType matcher) {
+  return Field("num_warps", &TritonGemmConfig::num_warps, matcher);
+}
+template <typename MatcherType>
+auto NumCtasIs(MatcherType matcher) {
+  return Field("num_ctas", &TritonGemmConfig::num_ctas, matcher);
+}
 
 auto IsValidConfig() {
-  return AllOf(Field("block_m", &TritonGemmConfig::block_m, Ge(1)),
-               Field("block_n", &TritonGemmConfig::block_n, Ge(1)),
-               Field("block_k", &TritonGemmConfig::block_k, Ge(1)),
-               Field("split_k", &TritonGemmConfig::split_k, Ge(1)),
-               Field("num_stages", &TritonGemmConfig::num_stages, Ge(1)),
-               Field("num_warps", &TritonGemmConfig::num_warps, Ge(1)),
-               Field("num_ctas", &TritonGemmConfig::num_ctas, Ge(1)));
+  return AllOf(BlockMIs(Ge(1)), BlockNIs(Ge(1)), BlockKIs(Ge(1)),
+               SplitKIs(Ge(1)), NumStagesIs(Ge(1)), NumWarpsIs(Ge(1)),
+               NumCtasIs(Ge(1)));
 };
 
 class DotSearchSpaceTest : public HloHardwareIndependentTestBase {
  protected:
   se::DeviceDescription device_description_{
       se::DeviceDescription(se::GpuDeviceInfoProto::default_instance())};
+
+  absl::StatusOr<std::unique_ptr<VerifiedHloModule>> GetDefaultDotModule(
+      int lhs_parallel_dim = 1024, int rhs_parallel_dim = 1024,
+      int contracting_dim = 1024) {
+    constexpr const char* kModuleTextFormat = R"(
+ENTRY e {
+  p0 = f16[%d,%d] parameter(0)
+  p1 = f16[%d,%d] parameter(1)
+  ROOT r = f16[%d,%d] dot(p0, p1),
+    lhs_contracting_dims={1}, rhs_contracting_dims={0}
+})";
+    return ParseAndReturnVerifiedModule(absl::StrFormat(
+        kModuleTextFormat, lhs_parallel_dim, contracting_dim, contracting_dim,
+        rhs_parallel_dim, lhs_parallel_dim, rhs_parallel_dim));
+  }
+
+  HloDotInstruction* GetDot(VerifiedHloModule* module) {
+    return Cast<HloDotInstruction>(
+        module->entry_computation()->root_instruction());
+  }
 };
 
 TEST_F(DotSearchSpaceTest, ReturnsValidConfigList) {
-  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<VerifiedHloModule> module,
-                          ParseAndReturnVerifiedModule(R"(
-ENTRY e {
-  p0 = f32[1024,1024] parameter(0)
-  p1 = f32[1024,1024] parameter(1)
-  ROOT r = f32[1024,1024] dot(p0, p1),
-    lhs_contracting_dims={1}, rhs_contracting_dims={0}
-})"));
-  TritonDotFusionSearchSpace search_space(
-      device_description_,
-      Cast<HloDotInstruction>(module->entry_computation()->root_instruction()));
+  TF_ASSERT_OK_AND_ASSIGN(auto module, GetDefaultDotModule());
+  TritonDotFusionSearchSpace search_space(device_description_,
+                                          GetDot(module.get()));
 
   EXPECT_THAT(search_space.GenerateConfigs(),
-              AllOf(Not(::testing::IsEmpty()), Each(IsValidConfig())));
+              AllOf(Not(IsEmpty()), Each(IsValidConfig())));
+}
+
+TEST_F(DotSearchSpaceTest, HonorsForcedContractingSplit) {
+  TF_ASSERT_OK_AND_ASSIGN(auto module, GetDefaultDotModule());
+  TritonDotFusionSearchSpace search_space(device_description_,
+                                          GetDot(module.get()));
+
+  EXPECT_THAT(
+      search_space.GenerateConfigs(/*force_contracting_split=*/2),
+      AllOf(Not(IsEmpty()), Each(IsValidConfig()), Each(SplitKIs(Eq(2)))));
+}
+
+TEST_F(DotSearchSpaceTest, ConsidersContractingSplitForSmallOutputSize) {
+  TF_ASSERT_OK_AND_ASSIGN(auto module,
+                          GetDefaultDotModule(/*lhs_parallel_dim=*/16,
+                                              /*rhs_parallel_dim=*/16,
+                                              /*contracting_dim=*/1024));
+  TritonDotFusionSearchSpace search_space(device_description_,
+                                          GetDot(module.get()));
+
+  EXPECT_THAT(search_space.GenerateConfigs(), Contains(SplitKIs(Ge(2))));
+}
+
+TEST_F(DotSearchSpaceTest, LimitsContractingSplitForSmallerContractingSize) {
+  TF_ASSERT_OK_AND_ASSIGN(auto module,
+                          GetDefaultDotModule(/*lhs_parallel_dim=*/16,
+                                              /*rhs_parallel_dim=*/16,
+                                              /*contracting_dim=*/32));
+  TritonDotFusionSearchSpace search_space(device_description_,
+                                          GetDot(module.get()));
+
+  EXPECT_THAT(search_space.GenerateConfigs(),
+              AllOf(Not(IsEmpty()), Each(SplitKIs(Le(2)))));
 }
 
 }  // namespace
