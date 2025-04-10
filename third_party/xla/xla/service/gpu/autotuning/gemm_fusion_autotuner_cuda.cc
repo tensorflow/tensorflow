97c65bec293151cbe5bb9ab1d828af5a879f9a35
Alexander Lyashuk <crem@google.com>
2025-04-10 06:06:51 -0700

    [XLA:GPU] Update default autotuner configs
    PiperOrigin-RevId: 745998088

--- third_party/xla/xla/service/gpu/autotuning/gemm_fusion_autotuner_cuda.cc
+++ third_party/xla/xla/service/gpu/autotuning/gemm_fusion_autotuner_cuda.cc
@@ -84,33 +84,24 @@ std::vector<TritonGemmConfig> GemmFusionAutotunerImpl::GetDefaultTritonConfigs()
   using Config = TritonGemmConfig;
 
   std::vector<Config> configs = {
-      Config(32, 32, 256, 1, 1, 4),   Config(64, 32, 32, 16, 1, 4),
-      Config(32, 64, 64, 4, 1, 4),    Config(128, 128, 64, 4, 1, 4),
-      Config(16, 16, 256, 1, 1, 4),   Config(16, 128, 32, 16, 1, 4),
-      Config(16, 64, 128, 1, 1, 4),   Config(16, 128, 32, 8, 1, 4),
-      Config(16, 16, 512, 1, 1, 4),   Config(32, 16, 512, 1, 1, 4),
-      Config(64, 32, 64, 1, 2, 8),    Config(128, 256, 32, 1, 3, 8),
-      Config(256, 128, 32, 1, 3, 8),  Config(256, 64, 32, 1, 4, 4),
-      Config(64, 256, 32, 1, 4, 4),   Config(128, 64, 32, 1, 4, 4),
-      Config(64, 128, 32, 1, 4, 4),   Config(256, 128, 128, 1, 3, 8),
-      Config(256, 64, 128, 1, 4, 4),  Config(64, 256, 128, 1, 4, 4),
-      Config(128, 128, 128, 1, 4, 4), Config(128, 64, 64, 1, 4, 4),
-      Config(64, 128, 64, 1, 4, 4),   Config(128, 32, 64, 1, 4, 4),
-      Config(64, 32, 64, 1, 4, 4),    Config(32, 128, 32, 1, 4, 4),
-      Config(128, 128, 32, 1, 4, 4),  Config(16, 16, 256, 1, 3, 4),
-      Config(128, 128, 64, 2, 1, 8),  Config(64, 64, 64, 1, 2, 4),
-      Config(16, 64, 256, 8, 1, 4),   Config(256, 256, 128, 1, 3, 8)};
-  auto cu_compute_capability =
-      std::get<se::CudaComputeCapability>(GetComputeCapability());
-  if (cu_compute_capability.IsAtLeastHopper()) {
-    absl::c_copy(
-        std::vector<Config>{
-            Config(16, 32, 32, 8, 1, 2),
-            Config(16, 64, 128, 8, 1, 4),
-            Config(16, 64, 128, 16, 3, 4),
-        },
-        std::back_inserter(configs));
-  }
+      Config(16, 16, 64, 1, 4, 2),    Config(16, 16, 128, 1, 4, 4),
+      Config(16, 16, 128, 128, 4, 2), Config(16, 16, 128, 16, 1, 2),
+      Config(16, 256, 16, 1, 1, 2),   Config(32, 32, 128, 16, 1, 4),
+      Config(32, 256, 32, 1, 3, 4),   Config(32, 256, 32, 16, 3, 8),
+      Config(64, 16, 32, 1, 4, 2),    Config(64, 16, 32, 16, 4, 2),
+      Config(64, 16, 64, 1, 1, 4),    Config(64, 16, 64, 4, 3, 2),
+      Config(64, 16, 64, 16, 4, 4),   Config(64, 16, 128, 1, 4, 2),
+      Config(64, 16, 128, 16, 4, 4),  Config(64, 32, 32, 1, 4, 4),
+      Config(64, 32, 64, 16, 3, 4),   Config(64, 32, 128, 1, 3, 2),
+      Config(64, 32, 128, 128, 2, 4), Config(64, 64, 32, 1, 4, 4),
+      Config(64, 64, 64, 1, 4, 4),    Config(64, 64, 64, 4, 4, 4),
+      Config(64, 64, 128, 16, 3, 4),  Config(64, 64, 256, 16, 4, 8),
+      Config(64, 128, 16, 1, 4, 2),   Config(64, 128, 64, 1, 3, 4),
+      Config(64, 128, 128, 8, 1, 4),  Config(64, 256, 32, 1, 4, 4),
+      Config(128, 16, 32, 8, 4, 2),   Config(128, 16, 64, 16, 3, 2),
+      Config(128, 16, 64, 16, 1, 4),  Config(128, 32, 32, 8, 4, 2),
+      Config(128, 128, 32, 8, 4, 8),  Config(128, 256, 32, 1, 4, 8),
+      Config(128, 256, 64, 1, 4, 8)};
   return configs;
 }
 
