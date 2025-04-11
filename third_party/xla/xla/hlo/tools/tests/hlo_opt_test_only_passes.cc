a451a31b2986b9214a763d32f22ec24a57288f3e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-10 20:12:25 -0700

    Automated Code Change
    PiperOrigin-RevId: 746274671

--- third_party/xla/xla/hlo/tools/tests/hlo_opt_test_only_passes.cc
+++ third_party/xla/xla/hlo/tools/tests/hlo_opt_test_only_passes.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include <tuple>
 #include <vector>
 
+#include "absl/container/flat_hash_set.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
