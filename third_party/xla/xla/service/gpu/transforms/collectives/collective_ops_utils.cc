71e335411dfc3703f97db0c7465904958246e2fc
Greg Olechwierowicz <olechwierowicz@google.com>
2025-04-11 08:16:42 -0700

    [XLA:GPU] Productionize communication type detection.
    In XLA:GPU we always assume that iota is the optimal rank assignment.
    We compute generally whether participants are within a single node or not and whether full rail alignment is reached or not.
    
    PiperOrigin-RevId: 746467442

--- third_party/xla/xla/service/gpu/transforms/collectives/collective_ops_utils.cc
+++ third_party/xla/xla/service/gpu/transforms/collectives/collective_ops_utils.cc
@@ -15,10 +15,18 @@ limitations under the License.
 
 #include "xla/service/gpu/transforms/collectives/collective_ops_utils.h"
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
 #include <variant>
+#include <vector>
 
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "xla/hlo/ir/collective_device_list.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
@@ -26,9 +34,77 @@ limitations under the License.
 #include "xla/service/hlo_module_config.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.h"
+#include "xla/tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
+
+namespace {
+
+struct CommunicationMetadata {
+  absl::flat_hash_map<int64_t, size_t> node_to_participant_count;
+};
+
+bool SameParticipantCounts(const absl::flat_hash_map<int64_t, size_t>& lhs,
+                           const absl::flat_hash_map<int64_t, size_t>& rhs) {
+  std::vector<size_t> lhs_counts, rhs_counts;
+  lhs_counts.reserve(lhs.size());
+  for (const auto& [_, v] : lhs) {
+    lhs_counts.push_back(v);
+  }
+
+  rhs_counts.reserve(rhs.size());
+  for (const auto& [_, v] : rhs) {
+    rhs_counts.push_back(v);
+  }
+  std::sort(lhs_counts.begin(), lhs_counts.end());
+  std::sort(rhs_counts.begin(), rhs_counts.end());
+  return lhs_counts == rhs_counts;
+}
+
+absl::StatusOr<CommunicationMetadata> CommunicationContext(
+    const CollectiveDeviceList& device_list, int num_devices_per_host) {
+  absl::flat_hash_map<int64_t, size_t> node_to_participant_count;
+  for (const ReplicaGroup& replica_group : device_list.replica_groups()) {
+    absl::flat_hash_map<int64_t, size_t> buffer;
+    for (int64_t rank : replica_group.replica_ids()) {
+      int64_t node_id = rank / num_devices_per_host;
+      buffer[node_id]++;
+    }
+    if (!node_to_participant_count.empty() &&
+        !SameParticipantCounts(buffer, node_to_participant_count)) {
+      return absl::FailedPreconditionError(absl::StrCat(
+          "Non homogenous replica group: ", device_list.ToString()));
+    }
+    if (node_to_participant_count.empty()) {
+      node_to_participant_count = buffer;
+    }
+  }
+
+  return CommunicationMetadata{node_to_participant_count};
+}
+
+bool IsSingleHost(const CommunicationMetadata& pattern) {
+  return pattern.node_to_participant_count.size() == 1;
+}
+
+bool IsRailAligned(const CommunicationMetadata& pattern,
+                   int num_devices_per_host) {
+  return absl::c_all_of(pattern.node_to_participant_count,
+                        [num_devices_per_host](const auto& elem) {
+                          const auto& [node_id, participant_count] = elem;
+                          return participant_count == num_devices_per_host;
+                        });
+}
+
+bool IsNonRailAligned(const CommunicationMetadata& pattern,
+                      int num_devices_per_host) {
+  return !IsSingleHost(pattern) &&
+         !IsRailAligned(pattern, num_devices_per_host);
+}
+
+}  // namespace
 
 bool IsGPUSyncCollective(const HloInstruction& instr) {
   auto backend_config = instr.backend_config<GpuBackendConfig>();
@@ -56,22 +132,22 @@ absl::StatusOr<GPUCommunicationType> CommunicationType(
 
   // We assume no topology was provided to the compiler and no
   // `CUDA_VISIBLE_DEVICES` env var has been set.
+  // For now we only support H100 and assume 8GPUs per host.
   int num_devices_per_host = 8;
 
-  if (!iota.has_value()) {
-    return absl::FailedPreconditionError(
-        "Only iota device assignment is supported.");
-  }
-  if (iota->num_replica_groups() == 1) {
-    return GPUCommunicationType::RAIL_ALIGNED;
-  }
-  if (iota->num_replica_groups() == num_devices_per_host &&
-      iota->transpose_perm().size() == 2 && iota->transpose_perm()[0] == 1) {
-    return GPUCommunicationType::NON_RAIL_ALIGNED;
-  }
-  if (iota->num_devices_per_group() == num_devices_per_host) {
+  TF_ASSIGN_OR_RETURN(
+      CommunicationMetadata comm,
+      CommunicationContext(instr.device_list(), num_devices_per_host));
+  if (IsSingleHost(comm)) {
     return GPUCommunicationType::SINGLE_HOST;
   }
+  if (IsRailAligned(comm, num_devices_per_host)) {
+    return GPUCommunicationType::RAIL_ALIGNED;
+  }
+  if (IsNonRailAligned(comm, num_devices_per_host)) {
+    return GPUCommunicationType::NON_RAIL_ALIGNED;
+  }
+
   return GPUCommunicationType::UNDEFINED;
 }
 
--- third_party/xla/xla/service/gpu/transforms/collectives/collective_ops_utils_test.cc
+++ third_party/xla/xla/service/gpu/transforms/collectives/collective_ops_utils_test.cc
@@ -15,14 +15,25 @@ limitations under the License.
 
 #include "xla/service/gpu/transforms/collectives/collective_ops_utils.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "xla/hlo/ir/hlo_casting_utils.h"
+#include "xla/hlo/ir/hlo_instructions.h"
+#include "xla/hlo/parser/hlo_parser.h"
+#include "xla/service/gpu/gpu_device_info_for_tests.h"
 #include "xla/service/hlo_module_config.h"
 #include "xla/stream_executor/cuda/cuda_compute_capability.h"
 #include "xla/stream_executor/device_description.h"
+#include "xla/tsl/platform/statusor.h"
+#include "tsl/platform/status_matchers.h"
+#include "tsl/platform/test.h"
 
-namespace xla {
-namespace gpu {
+namespace xla::gpu {
 namespace {
+
+using ::testing::Test;
+using ::tsl::testing::IsOkAndHolds;
 
 bool IsMultiHostTopology(se::CudaComputeCapability compute_capability,
                          int num_partitions, int replica_count) {
@@ -63,6 +74,149 @@ TEST(IsMultiHostTopologyTest, MultiHosts) {
                                   /*num_partitions=*/1, /*replica_count=*/16));
 }
 
+class CommunicationTypeTest : public Test {
+ protected:
+  se::DeviceDescription& device_info() { return device_info_; }
+
+ private:
+  se::DeviceDescription device_info_ = TestGpuDeviceInfo::RTXA6000DeviceInfo(
+      stream_executor::CudaComputeCapability(9, 0));
+};
+
+TEST_F(CommunicationTypeTest, DetectsSingleHost8Devices) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=8
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[1024] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups=[1,8]<=[8]
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::SINGLE_HOST));
+}
+
+TEST_F(CommunicationTypeTest, DetectsSingleHost4Devices) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=8
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[512] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups=[1,4]<=[4]
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::SINGLE_HOST));
+}
+
+TEST_F(CommunicationTypeTest, DetectsSingleHost16Devices) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=16
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[512] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups=[2,8]<=[16]
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::SINGLE_HOST));
+}
+
+TEST_F(CommunicationTypeTest, DetectRailAlignedAllDevices) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=16
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[2048] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups=[1,16]<=[16]
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::RAIL_ALIGNED));
+}
+
+TEST_F(CommunicationTypeTest, DetectRailAlignedHalfMesh) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=32
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[512] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups={
+          {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},
+          {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}
+        }
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::RAIL_ALIGNED));
+}
+
+TEST_F(CommunicationTypeTest, DetectNonRailAligned) {
+  absl::string_view kHlo = R"(
+    HloModule m, num_partitions=16
+
+    ENTRY e {
+      p = f32[128] parameter(0)
+      ROOT _ = f32[512] all-gather(p),
+        dimensions={0},
+        use_global_device_ids=true,
+        channel_id=1,
+        replica_groups={{0,8},{1,9},{2,10},{3,11},{4,12},{5,13},{6,14},{7,15}}
+    }
+  )";
+
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(kHlo));
+
+  HloCollectiveInstruction* instr = Cast<HloCollectiveInstruction>(
+      module->entry_computation()->root_instruction());
+  EXPECT_THAT(CommunicationType(*instr, device_info().gpu_compute_capability()),
+              IsOkAndHolds(GPUCommunicationType::NON_RAIL_ALIGNED));
+}
+
 }  // namespace
-}  // namespace gpu
-}  // namespace xla
+}  // namespace xla::gpu
