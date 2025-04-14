f75f7fed6bd3bf4ea9a6ddbe205e82b0ece147c4
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-14 00:59:31 -0700

    Automated Code Change
    PiperOrigin-RevId: 747293330

--- third_party/xla/xla/service/conditional_to_select_test.cc
+++ third_party/xla/xla/service/conditional_to_select_test.cc
@@ -16,7 +16,7 @@ limitations under the License.
 #include "xla/service/conditional_to_select.h"
 
 #include <memory>
-#include <utility>
+#include <string>
 
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
