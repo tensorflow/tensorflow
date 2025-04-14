4971bcaac7fd08c29410864d0cc5db00abafa273
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-14 05:00:39 -0700

    Automated Code Change
    PiperOrigin-RevId: 747365742

--- third_party/xla/xla/pjrt/cpu/abstract_tfrt_cpu_buffer.cc
+++ third_party/xla/xla/pjrt/cpu/abstract_tfrt_cpu_buffer.cc
@@ -430,7 +430,7 @@ AbstractTfrtCpuBuffer::CopyToDeviceAcrossClients(PjRtDevice* dst_device) {
   // Avoid use-after-free on `literal` due to unsequenced move and use.
   Literal* literal_pointer = literal.get();
   absl::InlinedVector<int64_t, 4> byte_strides(
-      literal->shape().dimensions_size());
+      literal->shape().dimensions().size());
   TF_RETURN_IF_ERROR(
       ShapeUtil::ByteStrides(literal->shape(), absl::MakeSpan(byte_strides)));
   TF_ASSIGN_OR_RETURN(PjRtMemorySpace * dst_memory_space,
