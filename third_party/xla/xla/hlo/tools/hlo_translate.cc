bbd1aaf1833dd5cb41816503158afb29ad4bb832
Kevin Gleason <gleasonk@google.com>
2025-04-24 14:50:20 -0700

    Add more descriptive errors in hlo-translate.
    PiperOrigin-RevId: 751146947

--- third_party/xla/xla/hlo/tools/hlo_translate.cc
+++ third_party/xla/xla/hlo/tools/hlo_translate.cc
@@ -19,6 +19,7 @@ limitations under the License.
 
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/LogicalResult.h"
@@ -100,7 +101,10 @@ absl::StatusOr<mlir::OwningOpRef<mlir::ModuleOp>> GetModuleFromHLOText(
     absl::string_view content, mlir::MLIRContext* context, bool emit_mhlo) {
   auto hlo_text = xla::ParseAndReturnUnverifiedModule(
       content, {}, xla::HloParserOptions().set_keep_module_auto_layouts(true));
-  if (!hlo_text.ok()) return absl::InvalidArgumentError(kLoadHloError);
+  if (!hlo_text.ok()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat(kLoadHloError, hlo_text.status().message()));
+  }
 
   auto hlo_module = std::move(hlo_text.value());
 
@@ -159,7 +163,7 @@ mlir::OwningOpRef<mlir::ModuleOp> GetModuleFromHloInput(
   // Try HLO Text
   auto module_from_text = GetModuleFromHLOText(content, context, emit_mhlo);
   if (module_from_text.ok()) return std::move(module_from_text).value();
-  if (module_from_text.status().message() != kLoadHloError) {
+  if (module_from_text.status().message().rfind(kLoadHloError, 0) != 0) {
     emitError() << "Failed to convert HLO to MLIR: "
                 << module_from_text.status().message();
     return nullptr;
@@ -169,14 +173,15 @@ mlir::OwningOpRef<mlir::ModuleOp> GetModuleFromHloInput(
   auto module_from_proto =
       GetModuleFromHLOProto(std::string(content), context, emit_mhlo);
   if (module_from_proto.ok()) return std::move(module_from_proto).value();
-  if (module_from_proto.status().message() != kLoadHloError) {
+  if (module_from_text.status().message().rfind(kLoadHloError, 0) != 0) {
     emitError() << "Failed to convert HLO to MLIR: "
                 << module_from_proto.status().message();
     return nullptr;
   }
 
   // Failed to parse
-  emitError() << "Failed to parse input as HLO text or proto.";
+  emitError() << "Failed to parse input as HLO text or proto.\n"
+              << module_from_text.status().message();
   return nullptr;
 }
 
