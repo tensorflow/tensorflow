3b2b4cfa715ab41fa39898273fcb6b02861b1570
A. Unique TensorFlower <gardener@tensorflow.org>
2025-04-25 16:39:14 -0700

    reset dynamic size to 0 when creating dynamic shaped fake arguments.
    PiperOrigin-RevId: 751601296

--- third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner.cc
+++ third_party/xla/xla/tools/multihost_hlo_runner/functional_hlo_runner.cc
@@ -115,7 +115,10 @@ absl::StatusOr<Literal> MakeFakeLiteralWithSameValue(const Shape& shape,
                 static_cast<NativeT>(type == PRED ? (value % 2) == 0 : value));
             for (int i = 0; i < shape.dimensions_size(); i++) {
               if (shape.is_dynamic_dimension(i)) {
-                literal.SetDynamicSize(i, shape.dimensions(i));
+                // TODO(b/378917570): We might need to set the dynamic size to
+                // the actual bound i.e., shape.dimensions(i) when HybridSim
+                // supports SparseCore.
+                literal.SetDynamicSize(i, 0);
               }
             }
             return literal;
