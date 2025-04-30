c0f102c739b833fcc3782da32799f5e3b5759cc7
Henning Becker <hebecker@google.com>
2025-04-30 07:26:58 -0700

    Remove usage of AsGpuStreamValue from CudaCommandBuffer class
    This inline the two calls to AsGpuStreamValue in cuda_command_buffer.cc.
    
    The change also contains the usual drive-by fixes (clang-tidy, includes).
    
    PiperOrigin-RevId: 753158876

--- third_party/xla/xla/stream_executor/cuda/cuda_command_buffer.cc
+++ third_party/xla/xla/stream_executor/cuda/cuda_command_buffer.cc
@@ -19,13 +19,11 @@ limitations under the License.
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
-#include <iterator>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
-#include "absl/algorithm/container.h"
 #include "absl/base/casts.h"
 #include "absl/functional/any_invocable.h"
 #include "absl/log/check.h"
@@ -45,7 +43,6 @@ limitations under the License.
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/dnn.h"
 #include "xla/stream_executor/gpu/gpu_command_buffer.h"
-#include "xla/stream_executor/gpu/gpu_stream.h"
 #include "xla/stream_executor/gpu/scoped_update_mode.h"
 #include "xla/stream_executor/kernel.h"
 #include "xla/stream_executor/launch_dim.h"
@@ -548,7 +545,8 @@ absl::Status CudaCommandBuffer::Trace(
   VLOG(5) << "Trace into GPU command buffer graph " << graph_
           << " on a stream: " << stream;
 
-  CUstream stream_handle = AsGpuStreamValue(stream);
+  CUstream stream_handle =
+      absl::bit_cast<CUstream>(stream->platform_specific_handle().stream);
 
   // Switch stream into the capture mode.
   uint64_t start_nanos = tsl::Env::Default()->NowNanos();
@@ -571,9 +569,10 @@ absl::Status CudaCommandBuffer::Trace(
   DCHECK(captured_graph == graph_) << "Stream capture should update graph_";
   uint64_t end_nanos = tsl::Env::Default()->NowNanos();
 
-  if (!traced.ok())
+  if (!traced.ok()) {
     return absl::InternalError(
         absl::StrCat("Failed to capture gpu graph: ", traced.message()));
+  }
 
   VLOG(5) << "Traced into the GPU command buffer graph " << graph_ << " (took "
           << (end_nanos - start_nanos) / 1000 << " μs)";
@@ -585,8 +584,10 @@ absl::Status CudaCommandBuffer::Trace(
 absl::Status CudaCommandBuffer::LaunchGraph(Stream* stream) {
   VLOG(3) << "Launch command buffer executable graph " << exec_
           << " on a stream: " << stream;
-  return cuda::ToStatus(cuGraphLaunch(exec_, AsGpuStreamValue(stream)),
-                        "Failed to launch CUDA graph");
+  return cuda::ToStatus(
+      cuGraphLaunch(exec_, absl::bit_cast<CUstream>(
+                               stream->platform_specific_handle().stream)),
+      "Failed to launch CUDA graph");
 }
 
 absl::StatusOr<size_t> CudaCommandBuffer::GetNodeCount() const {
