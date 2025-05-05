cb21a2950f362d176dff84c0b89008c589f7ca73
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-05 15:40:57 -0700

    Updates IOPDDL conversion to shift edge costs by a constant amount if negative values are present.
    PiperOrigin-RevId: 755099760

--- third_party/xla/xla/hlo/experimental/auto_sharding/auto_sharding_iopddl.cc
+++ third_party/xla/xla/hlo/experimental/auto_sharding/auto_sharding_iopddl.cc
@@ -84,16 +84,26 @@ iopddl::Problem ConvertToProblem(const AutoShardingSolverRequest& request) {
   // The first kind of edges come from request.edges
   for (int64_t edge_idx = 0; edge_idx < request.edges_size(); ++edge_idx) {
     const auto& edge = request.edges(edge_idx);
-    CHECK_LT(edge.first(), request.s_len_size());
-    CHECK_LT(edge.second(), request.s_len_size());
+    NodeIdx u = edge.first(), v = edge.second();
+    CHECK_LT(u, request.s_len_size());
+    CHECK_LT(v, request.s_len_size());
     CHECK_LT(edge_idx, request.resharding_costs_size());
-    problem.edges.push_back({{edge.first(), edge.second()}});
-    for (int64_t i = 0; i < request.s_len(edge.first()); ++i) {
-      for (int64_t j = 0; j < request.s_len(edge.second()); ++j) {
-        const int64_t k = i * request.s_len(edge.second()) + j;
+    problem.edges.push_back({{u, v}});
+    double min_cost = 0.0;
+    for (int64_t i = 0; i < request.s_len(u); ++i) {
+      for (int64_t j = 0; j < request.s_len(v); ++j) {
+        const int64_t k = i * request.s_len(v) + j;
+        CHECK_LT(k, request.resharding_costs(edge_idx).costs_size());
+        min_cost =
+            std::min(min_cost, request.resharding_costs(edge_idx).costs(k));
+      }
+    }
+    for (int64_t i = 0; i < request.s_len(u); ++i) {
+      for (int64_t j = 0; j < request.s_len(v); ++j) {
+        const int64_t k = i * request.s_len(v) + j;
         CHECK_LT(k, request.resharding_costs(edge_idx).costs_size());
         const iopddl::Cost cost =
-            ConvertCost(request.resharding_costs(edge_idx).costs(k));
+            ConvertCost(request.resharding_costs(edge_idx).costs(k) - min_cost);
         problem.edges.back().strategies.push_back({cost});
       }
     }
