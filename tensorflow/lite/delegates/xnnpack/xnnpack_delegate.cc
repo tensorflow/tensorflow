71691c769e76655e5b2b869f258572d1e8febe64
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-05 11:28:10 -0700

    Prepare VarHandle ops too
    In order for VarHandle to work if the variable is used outside of the ops delegated by XNNPACK, we need to prepare the output.
    
    PiperOrigin-RevId: 755010505

--- tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
+++ tensorflow/lite/delegates/xnnpack/xnnpack_delegate.cc
@@ -729,20 +729,30 @@ class Delegate {
       var_handles_;
 };
 
-// This is basically an implementation of invoke for VarHandle that also returns
-// the resource_id. We can't use that implementation because:
-// - It writes its output to the output tensor, which may not exist if that
-// tensor is fully delegated (along with this VarHandle op).
-// - There's a circular dependency if we try to depend on "builtin_op_kernels".
+// Prepare/invoke for VarHandle that also returns the resource_id. We can't use
+// the tensorflow/lite/kernels/var_handle.cc implementation because there's a
+// circular dependency if we try to depend on "builtin_op_kernels".
+TfLiteStatus PrepareVarHandle(TfLiteContext* context, const TfLiteNode* node) {
+  TfLiteTensor* output;
+  TF_LITE_ENSURE_OK(context, GetOutputSafe(context, node, 0, &output));
+
+  output->allocation_type = kTfLiteArenaRwPersistent;
+  const int kBytesRequired = sizeof(int32_t);
+  TfLiteTensorRealloc(kBytesRequired, output);
+  output->bytes = kBytesRequired;
+
+  return kTfLiteOk;
+}
+
 TfLiteStatus InvokeVarHandle(TfLiteContext* context,
                              const TfLiteNode* var_handle, int& resource_id) {
   // This is struct VarParams { int resource_id; };
-  const int* op_data = static_cast<const int*>(var_handle->user_data);
+  const int32_t* op_data = static_cast<const int32_t*>(var_handle->user_data);
   TF_LITE_ENSURE(context, op_data != nullptr);
   resource_id = *op_data;
 
   TfLiteTensor& output = context->tensors[var_handle->outputs->data[0]];
-  if (int* output_data = GetTensorData<int>(&output)) {
+  if (int32_t* output_data = GetTensorData<int32_t>(&output)) {
     // If we delegate the VarHandle op, but the result is an output of
     // the delegated subgraph, we need to implement the op.
     *output_data = resource_id;
@@ -1203,6 +1213,18 @@ class Subgraph {
         }
       }
     }
+
+    // Prepare any VarHandle ops we delegated.
+    for (std::pair<const int, void*>& io_info : externals_) {
+      const auto& resource_it = resources_.find(io_info.first);
+      if (resource_it != resources_.end()) {
+        const TfLiteNode* var_handle = resource_it->second.GetVarHandle();
+        if (var_handle) {
+          TF_LITE_ENSURE_STATUS(PrepareVarHandle(context, var_handle));
+        }
+      }
+    }
+
     return kTfLiteOk;
   }
 
@@ -6424,8 +6446,7 @@ class Subgraph {
   static TfLiteStatus VisitVarHandleNode(xnn_subgraph_t subgraph,
                                          Delegate& delegate,
                                          TfLiteContext* logging_context,
-                                         int node_index,
-                                         const TfLiteNode* node) {
+                                         int node_index, TfLiteNode* node) {
     if (!delegate.support_variable_ops()) {
       return kTfLiteError;
     }
