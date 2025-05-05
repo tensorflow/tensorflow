76ee7a6450a8884f2ca47f6e165c028caeb33025
Frederik Gossen <frgossen@google.com>
2025-05-05 16:58:06 -0700

    [XLA:GPU] Move all_reduce_decomposer to collectives folder
    PiperOrigin-RevId: 755123520

--- third_party/xla/xla/service/gpu/transforms/collectives/all_reduce_decomposer.h
+++ third_party/xla/xla/service/gpu/transforms/collectives/all_reduce_decomposer.h
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef XLA_SERVICE_GPU_TRANSFORMS_ALL_REDUCE_DECOMPOSER_H_
-#define XLA_SERVICE_GPU_TRANSFORMS_ALL_REDUCE_DECOMPOSER_H_
+#ifndef XLA_SERVICE_GPU_TRANSFORMS_COLLECTIVES_ALL_REDUCE_DECOMPOSER_H_
+#define XLA_SERVICE_GPU_TRANSFORMS_COLLECTIVES_ALL_REDUCE_DECOMPOSER_H_
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/statusor.h"
@@ -38,4 +38,4 @@ class AllReduceDecomposer : public HloModulePass {
 }  // namespace gpu
 }  // namespace xla
 
-#endif  // XLA_SERVICE_GPU_TRANSFORMS_ALL_REDUCE_DECOMPOSER_H_
+#endif  // XLA_SERVICE_GPU_TRANSFORMS_COLLECTIVES_ALL_REDUCE_DECOMPOSER_H_
