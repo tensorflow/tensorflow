229cbc47ac8bb132587892bca8faf4ae79edae06
Dirk Hornung <dirkh@google.com>
2025-05-09 09:35:12 -0700

    [XLA:GPU] Add cuBLASLt autotuner backend. The backend returns configs for cuBLASLt custom calls.
    Note: The WrapInModule and RunHloPasses are unimplemented and will be implemented once we know how to handle un-fusions.
    PiperOrigin-RevId: 756799152

---
+++ third_party/xla/xla/backends/autotuner/backends/gpu/cublaslt_test.cc
@@ -0,0 +1,152 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/autotuner/backends/gpu/cublaslt.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/statusor.h"
+#include "xla/backends/autotuner/codegen_backend.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/testlib/hlo_hardware_independent_test_base.h"
+#include "xla/service/compiler.h"
+#include "xla/service/gpu/gpu_device_info_for_tests.h"
+#include "xla/service/gpu/nvptx_compiler.h"
+#include "xla/service/platform_util.h"
+#include "xla/stream_executor/blas.h"
+#include "xla/stream_executor/device_description.pb.h"
+#include "xla/stream_executor/stream_executor.h"
+#include "xla/tsl/platform/status_matchers.h"
+#include "xla/tsl/platform/statusor.h"
+#include "xla/xla.pb.h"
+
+namespace xla {
+namespace gpu {
+
+using CublasLtBackendConfig = AutotuneResult::GemmKey;
+using ::tsl::testing::IsOk;
+using ::tsl::testing::StatusIs;
+
+const char kCublasLtCustomCallHlo[] = R"(
+HloModule module
+
+ENTRY main {
+  p0 = f32[100,100] parameter(0)
+  p1 = f32[100,100] parameter(1)
+  %custom-call.1 = (f32[100,100]{1,0}, s8[4194304]{0}) custom-call(p0, p1),
+    custom_call_target="__cublas$lt$matmul",
+    backend_config={
+      "gemm_backend_config":{
+        "selected_algorithm":"0",
+        "alpha_real":1,
+        "beta":0,
+        "dot_dimension_numbers":{
+          "lhs_contracting_dimensions":["1"],
+          "rhs_contracting_dimensions":["0"],
+          "lhs_batch_dimensions":[],
+          "rhs_batch_dimensions":[]
+        },
+        "alpha_imag":0,
+        "precision_config":{
+          "operand_precision":["DEFAULT","DEFAULT"],
+          "algorithm":"ALG_UNSET"
+        },
+        "epilogue":"DEFAULT",
+        "lhs_stride":"10000",
+        "rhs_stride":"10000",
+        "grad_x":false,
+        "grad_y":false,
+        "damax_output":false
+      }
+    }
+  ROOT %get-tuple-element = f32[100,100]{1,0} get-tuple-element(%custom-call.1), index=0
+})";
+
+class CublasLtBackendTest : public HloHardwareIndependentTestBase {
+ protected:
+  DebugOptions debug_options_;
+  NVPTXCompiler compiler_;
+  Compiler::TargetConfig target_config_;
+  CublasLtBackend backend_;
+
+  CublasLtBackendTest()
+      : target_config_([]() {
+          se::GpuTargetConfigProto target_config_proto;
+          *target_config_proto.mutable_gpu_device_info() =
+              TestGpuDeviceInfo().CudaOrRocmDeviceInfo().ToGpuProto();
+          return Compiler::TargetConfig(target_config_proto);
+        }()),
+        backend_(&target_config_, &debug_options_, &compiler_) {}
+
+  CublasLtBackendConfig ExpectedDefaultAlgorithm() {
+    auto config = AutotuneResult::GemmKey();
+    config.set_algorithm(se::blas::kDefaultAlgorithm);
+    return config;
+  }
+};
+
+TEST_F(CublasLtBackendTest, CanCreateCublasBackend) {
+  ASSERT_NE(nullptr, &backend_);
+}
+
+TEST_F(CublasLtBackendTest, GetSupportedConfigs) {
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          ParseAndReturnVerifiedModule(kCublasLtCustomCallHlo));
+
+  se::StreamExecutor* stream_executor =
+      PlatformUtil::GetDefaultPlatform().value()->ExecutorForDevice(0).value();
+  const HloInstruction* gemm_instr =
+      module->entry_computation()->root_instruction()->operand(0);
+  absl::StatusOr<std::vector<std::unique_ptr<BackendConfig>>> configs =
+      backend_.GetSupportedConfigs(*gemm_instr, stream_executor);
+  EXPECT_THAT(configs, IsOk());
+  EXPECT_GT(configs.value().size(), 0);
+}
+
+TEST_F(CublasLtBackendTest, GetDefaultConfig) {
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          ParseAndReturnVerifiedModule(kCublasLtCustomCallHlo));
+
+  absl::StatusOr<std::unique_ptr<BackendConfig>> config =
+      backend_.GetDefaultConfig(
+          (*module->entry_computation()->root_instruction()->operand(0)));
+  EXPECT_THAT(config, IsOk());
+}
+
+TEST_F(CublasLtBackendTest, GetDefaultConfigFailsWithoutACublasLtCustomCall) {
+  std::string hlo = R"(
+    HloModule module
+
+    ENTRY main {
+      p0 = f32[1024,1024]{1,0} parameter(0)
+      p1 = f32[1024,1024]{1,0} parameter(1)
+      ROOT dot = f32[1024,1024]{1,0} dot(p0, p1),
+          lhs_contracting_dims={1}, rhs_contracting_dims={0}
+    })";
+
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          ParseAndReturnVerifiedModule(hlo));
+  absl::StatusOr<std::unique_ptr<BackendConfig>> config =
+      backend_.GetDefaultConfig(
+          (*module->entry_computation()->root_instruction()));
+  EXPECT_THAT(config, StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+}  // namespace gpu
+}  // namespace xla
