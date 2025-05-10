173723fc7fb651e6dd6ddae94e0b520999946c95
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-09 19:51:48 -0700

    Provide flexibility to derived classes for specific backends w.r.t how the initial fusion worklist is formed for a current computation.
    PiperOrigin-RevId: 756997596

--- third_party/xla/xla/service/multi_output_fusion.h
+++ third_party/xla/xla/service/multi_output_fusion.h
@@ -16,14 +16,18 @@ limitations under the License.
 #ifndef XLA_SERVICE_MULTI_OUTPUT_FUSION_H_
 #define XLA_SERVICE_MULTI_OUTPUT_FUSION_H_
 
+#include <cstdint>
+#include <memory>
 #include <optional>
 #include <queue>
+#include <utility>
 #include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/analysis/hlo_reachability.h"
+#include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/pass/hlo_pass_interface.h"
 
@@ -84,6 +88,13 @@ class MultiOutputFusion : public HloModulePass {
   virtual bool LegalToFuse(HloInstruction* instr1, HloInstruction* instr2);
 
   // Test if it's legal to fuse instr1 and instr2 into one fusion instruction
+  // using preliminary constraints such as whether two instructions are dead or
+  // reachable from each other or are not fusible kinds of instructions such as
+  // GTEs.
+  bool LegalToFusePreliminaryConstraints(HloInstruction* instr1,
+                                         HloInstruction* instr2);
+
+  // Test if it's legal to fuse instr1 and instr2 into one fusion instruction
   // using main constraints.
   bool LegalToFuseMainConstraints(HloInstruction* instr1,
                                   HloInstruction* instr2);
@@ -100,6 +111,23 @@ class MultiOutputFusion : public HloModulePass {
 
   // Returns the computation for the pass.
   HloComputation* computation() const { return computation_; }
+
+  // An internal data structure for each instruction in current computation.
+  // When an instruction is removed, member 'hlo' is set to nullptr.
+  struct FusionCandidate {
+    HloInstruction* hlo;
+    std::list<std::pair<HloInstruction*, int64_t>> fusibles;
+    explicit FusionCandidate(HloInstruction* hlo) : hlo(hlo) {}
+  };
+
+  const std::vector<FusionCandidate>& current_computation_candidates() const {
+    return candidates_;
+  }
+
+  void AddFusibleCandidate(HloInstruction* instr);
+
+  void AddToWorkList(HloInstruction* instr1, HloInstruction* instr2,
+                     int64_t profit);
 
   // Update the reachability map after fusing instr1 and instr2.
   void UpdateReachability(
@@ -132,15 +160,11 @@ class MultiOutputFusion : public HloModulePass {
     return reachability_->IsConnected(instr1, instr2);
   }
 
- private:
-  // An internal data structure for each instruction in current computation.
-  // When an instruction is removed, member 'hlo' is set to nullptr.
-  struct FusionCandidate {
-    HloInstruction* hlo;
-    std::list<std::pair<HloInstruction*, int64_t>> fusibles;
-    explicit FusionCandidate(HloInstruction* hlo) : hlo(hlo) {}
-  };
+  // Creates an initial worklist of fusible instruction pairs for the current
+  // computation.
+  virtual void CreateFusionWorkListForCurrentComputation();
 
+ private:
   // The pair of candidates to be fused and the profit score.
   struct ToBeFused {
     HloInstruction* instr1;
