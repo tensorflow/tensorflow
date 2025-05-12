f478dcf1bfebeace20585f1c674b911cbc02d67b
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-11 23:50:20 -0700

    Automated Code Change
    PiperOrigin-RevId: 757617038

--- third_party/xla/xla/pjrt/interpreter/interpreter_client_test.cc
+++ third_party/xla/xla/pjrt/interpreter/interpreter_client_test.cc
@@ -32,6 +32,7 @@ limitations under the License.
 #include "xla/tests/literal_test_util.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace {
