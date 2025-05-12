67c30641712010c2dd5d4af416c69f767ce7feee
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-12 02:00:09 -0700

    Automated Code Change
    PiperOrigin-RevId: 757655763

--- third_party/xla/xla/backends/cpu/runtime/all_to_all_thunk.cc
+++ third_party/xla/xla/backends/cpu/runtime/all_to_all_thunk.cc
@@ -20,7 +20,6 @@ limitations under the License.
 
 #include "absl/container/inlined_vector.h"
 #include "absl/memory/memory.h"
-#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
 #include "xla/backends/cpu/collectives/cpu_collectives.h"
