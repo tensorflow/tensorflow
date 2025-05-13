fb2300af29635348067e7416b5f026ee3a1ea548
Tori Baker <vwbaker@google.com>
2025-05-13 09:09:53 -0700

    [xla:gpu] add extract_tma_info pass
    There is information such as the swizzling mode that is necessary to create the correct tma descriptors, which is only available mid-triton pipeline. This pass is the same point that triton extracts the tma metadata from the pipeline here: https://github.com/triton-lang/triton/blob/main/third_party/nvidia/backend/compiler.py#L285
    
    Rather than running/pausing the pipeline though, I added a pass that copies the information to our already existent "tma_descriptor" attribute. All the information is then extracted at the same time lower in the pipeline as the attributes live through LLVM IR.
    
    PiperOrigin-RevId: 758246048

---
+++ third_party/xla/xla/backends/gpu/codegen/triton/transforms/extract_tma_info_pass.cc
@@ -0,0 +1,84 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <memory>
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Diagnostics.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/LLVM.h"
+#include "xla/backends/gpu/codegen/triton/ir/triton_xla_ops.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/Triton/IR/Types.h"
+#include "triton/Dialect/TritonNvidiaGPU/Transforms/TMAUtilities.h"
+
+namespace mlir::triton::xla {
+namespace {
+
+#define GEN_PASS_DEF_EXTRACTTMAINFOPASS
+#include "xla/backends/gpu/codegen/triton/transforms/passes.h.inc"
+
+struct ExtractTmaInfoPass
+    : public impl::ExtractTmaInfoPassBase<ExtractTmaInfoPass> {
+  void runOnOperation() override {
+    mlir::ModuleOp mod = getOperation();
+    mod.walk([&](mlir::triton::FuncOp func_op) -> void {
+      for (auto arg : func_op.getArguments()) {
+        if (!isa<TensorDescType>(arg.getType())) {
+          continue;
+        }
+
+        auto tma_descriptor_attr = func_op.getArgAttrOfType<TmaDescriptorAttr>(
+            arg.getArgNumber(), "tt.tma_descriptor");
+        if (!tma_descriptor_attr) {
+          emitError(arg.getLoc(),
+                    "Argument of type tt.tensordesc must have attribute "
+                    "tt.tma_descriptor");
+          signalPassFailure();
+          return;
+        }
+
+        auto swizzle_mode = nvidia_gpu::getTMASwizzleMode(
+            /*op=*/nullptr, mlir::cast<TensorDescType>(arg.getType()));
+        if (!swizzle_mode.has_value()) {
+          emitError(
+              arg.getLoc(),
+              "Unable to determine swizzle mode from tt.tensordesc's layout");
+          signalPassFailure();
+          return;
+        }
+
+        IRRewriter rewriter(&getContext());
+        func_op.setArgAttr(arg.getArgNumber(), "tt.tma_descriptor",
+                           rewriter.getAttr<TmaDescriptorAttr>(
+                               tma_descriptor_attr.getGlobalShape(),
+                               tma_descriptor_attr.getBlockShape(),
+                               tma_descriptor_attr.getLayout(),
+                               tma_descriptor_attr.getElementByteSize(),
+                               swizzle_mode.value()));
+      }
+    });
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<mlir::Pass> CreateExtractTmaInfoPass() {
+  return std::make_unique<ExtractTmaInfoPass>();
+}
+
+}  // namespace mlir::triton::xla
