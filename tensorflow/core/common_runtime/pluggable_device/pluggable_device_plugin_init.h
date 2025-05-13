12e50aa4ad72e772b0c836ef8f2f7a57756188f1
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-12 22:57:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 758059747

--- tensorflow/core/common_runtime/pluggable_device/pluggable_device_plugin_init.h
+++ tensorflow/core/common_runtime/pluggable_device/pluggable_device_plugin_init.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_COMMON_RUNTIME_PLUGGABLE_DEVICE_PLUGGABLE_DEVICE_PLUGIN_INIT_H_
 #define TENSORFLOW_CORE_COMMON_RUNTIME_PLUGGABLE_DEVICE_PLUGGABLE_DEVICE_PLUGIN_INIT_H_
 
+#include "absl/status/status.h"
 #include "tensorflow/core/platform/status.h"
 
 // Forward declarations to avoid dependency.
