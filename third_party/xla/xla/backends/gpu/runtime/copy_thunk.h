5112ab3377f232a08312613d49a9081908d5c4c7
Alex Pivovarov <upwind@google.com>
2025-05-13 05:23:57 -0700

    Add proto serialization for GPU ConditionalThunk
    PiperOrigin-RevId: 758172901

--- third_party/xla/xla/backends/gpu/runtime/copy_thunk.h
+++ third_party/xla/xla/backends/gpu/runtime/copy_thunk.h
@@ -23,6 +23,7 @@ limitations under the License.
 
 #include "absl/base/thread_annotations.h"
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/inlined_vector.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/synchronization/mutex.h"
