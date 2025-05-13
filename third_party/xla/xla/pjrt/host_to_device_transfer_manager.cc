c6dbbef82fe97b588de451637e4c98bcb852832e
Parker Schuh <parkers@google.com>
2025-05-12 17:56:08 -0700

    Move CreateViewOfDeviceBuffer and BufferFromHostBuffer into CommonPjRtClient based on raw buffers. Aliasing in BufferFromHostBuffer is now just based on CreateViewOfDeviceBuffer.
    PiperOrigin-RevId: 757984517

--- third_party/xla/xla/pjrt/host_to_device_transfer_manager.cc
+++ third_party/xla/xla/pjrt/host_to_device_transfer_manager.cc
@@ -149,7 +149,8 @@ class CommonAsyncHostToDeviceTransferManager
                                     allocation_event));
       TF_ASSIGN_OR_RETURN(auto buffer,
                           client->DefineBuffer(device_shape, raw_buffer,
-                                               {std::move(definition_event)}));
+                                               {std::move(definition_event)},
+                                               /*raw_buffer_is_mutable=*/true));
       device_shapes.push_back(std::move(device_shape));
       buffers.push_back(std::move(buffer));
       undispatched_buffer_refs.push_back(raw_buffer);
