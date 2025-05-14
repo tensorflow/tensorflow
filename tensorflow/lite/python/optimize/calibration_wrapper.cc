9a1b0be4eff4933619a5cac07cdf970f48281802
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-14 00:38:31 -0700

    Automated Code Change
    PiperOrigin-RevId: 758553971

--- tensorflow/lite/python/optimize/calibration_wrapper.cc
+++ tensorflow/lite/python/optimize/calibration_wrapper.cc
@@ -50,7 +50,6 @@ limitations under the License.
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
-#include "absl/types/optional.h"
 #include "tensorflow/compiler/mlir/lite/offset_buffer.h"
 #include "tensorflow/lite/core/c/common.h"
 #include "tensorflow/lite/core/interpreter.h"
