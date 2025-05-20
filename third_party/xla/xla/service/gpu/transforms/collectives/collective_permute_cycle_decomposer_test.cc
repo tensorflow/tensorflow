352bfed7bb8493b096d1d0f16395f3b8ba1418db
Frederik Gossen <frgossen@google.com>
2025-05-20 14:18:46 -0700

    [XLA:GPU] Move collective_permute_cycle_decomposer
    PiperOrigin-RevId: 761228176

--- third_party/xla/xla/service/gpu/transforms/collectives/collective_permute_cycle_decomposer_test.cc
+++ third_party/xla/xla/service/gpu/transforms/collectives/collective_permute_cycle_decomposer_test.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "xla/service/gpu/transforms/collective_permute_cycle_decomposer.h"
+#include "xla/service/gpu/transforms/collectives/collective_permute_cycle_decomposer.h"
 
 #include <cstdint>
 #include <memory>
