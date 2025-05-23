f878b479796e3e92e6b18d8eeb2e66b1b8604b4e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-22 17:23:15 -0700

    ReadyIfScheduled() is used as a heuristic and thus called many times, move it to being updated only when the graph is initialized/changed and fetch from storage in the hot path. Tidy up 'Does(Occupy/Free)Resources' by computing during initialization, getting rid of std::optional / lazy init.
    PiperOrigin-RevId: 762189923

--- third_party/xla/xla/backends/gpu/runtime/all_reduce_test.cc
+++ third_party/xla/xla/backends/gpu/runtime/all_reduce_test.cc
@@ -17,25 +17,27 @@ limitations under the License.
 
 #include <algorithm>
 #include <cstdint>
-#include <functional>
 #include <memory>
-#include <numeric>
-#include <string>
+#include <utility>
 #include <vector>
 
 #include <gtest/gtest.h>
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/types/span.h"
+#include "xla/array.h"
 #include "xla/core/collectives/rank_id.h"
 #include "xla/primitive_util.h"
 #include "xla/service/gpu/launch_dimensions.h"
+#include "xla/status_macros.h"
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/device_memory_handle.h"
 #include "xla/stream_executor/gpu/gpu_init.h"
 #include "xla/stream_executor/platform.h"
 #include "xla/stream_executor/platform_manager.h"
 #include "xla/stream_executor/stream.h"
-#include "xla/tsl/lib/core/status_test_util.h"
+#include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/platform/test.h"
 #include "xla/types.h"
 #include "xla/xla_data.pb.h"
@@ -49,112 +51,157 @@ se::StreamExecutor* GetGpuExecutor(int64_t device_ordinal) {
   return platform->ExecutorForDevice(device_ordinal).value();
 }
 
-template <typename T>
-class AllReduceKernelTest : public ::testing::Test {};
-
-using AllReduceKernelTestTypes = ::testing::Types<float, bfloat16>;
-
-class AllReduceKernelTestNameGenerator {
+class AllReduceKernelTest : public ::testing::Test {
  public:
   template <typename T>
-  static std::string GetName(int) {
-    if constexpr (std::is_same_v<T, float>) {
-      return "f32";
+  static absl::StatusOr<std::vector<Array<T>>> RunKernel(
+      const std::vector<se::StreamExecutor*>& executors,
+      const std::vector<Array<T>>& input_data) {
+    constexpr LaunchDimensions kLaunchDimensions{
+        /*block_x_count=*/8,
+        /*thread_x_count_per_block=*/512};
+
+    int64_t num_ranks = input_data.size();
+    int64_t num_elements = input_data[0].num_elements();
+
+    TF_RETURN_IF_ERROR(executors[0]->EnablePeerAccessTo(executors[1]));
+    TF_RETURN_IF_ERROR(executors[1]->EnablePeerAccessTo(executors[0]));
+
+    std::vector<std::unique_ptr<se::Stream>> streams;
+    std::vector<se::DeviceMemoryHandle> local_input_buffers;
+    std::vector<se::DeviceMemoryHandle> data_buffers;
+    std::vector<se::DeviceMemoryHandle> signal_flags_buffers;
+    std::vector<se::DeviceMemoryBase> remote_input_buffers_span;
+    std::vector<se::DeviceMemoryBase> signal_flags_buffers_span;
+
+    for (int i = 0; i < num_ranks; ++i) {
+      auto* executor = executors[i];
+      streams.push_back(executor->CreateStream().value());
+
+      local_input_buffers.emplace_back(
+          executor, executor->AllocateArray<T>(num_elements));
+      TF_RET_CHECK(!local_input_buffers[i].memory().is_null());
+
+      data_buffers.emplace_back(executor,
+                                executor->AllocateArray<T>(num_elements));
+      TF_RET_CHECK(!data_buffers[i].memory().is_null());
+
+      signal_flags_buffers.emplace_back(
+          executor, executor->AllocateArray<uint32_t>(
+                        num_ranks * kLaunchDimensions.num_blocks()));
+      TF_RET_CHECK(!signal_flags_buffers[i].memory().is_null());
+
+      TF_RETURN_IF_ERROR(executor->SynchronousMemZero(
+          signal_flags_buffers[i].memory_ptr(),
+          signal_flags_buffers[i].memory().size()));
+
+      TF_RETURN_IF_ERROR(streams[i]->Memcpy(local_input_buffers[i].memory_ptr(),
+                                            input_data[i].data(),
+                                            num_elements * sizeof(T)));
+
+      remote_input_buffers_span.push_back(data_buffers[i].memory());
+      signal_flags_buffers_span.push_back(signal_flags_buffers[i].memory());
     }
-    if constexpr (std::is_same_v<T, bfloat16>) {
-      return "bf16";
+
+    for (int i = 0; i < num_ranks; ++i) {
+      TF_RETURN_IF_ERROR(streams[i]->BlockHostUntilDone());
     }
+
+    for (int i = 0; i < num_ranks; ++i) {
+      auto active_context = executors[i]->Activate();
+      TF_RETURN_IF_ERROR(RunAllReduceKernel(
+          streams[i].get(), kLaunchDimensions,
+          primitive_util::NativeToPrimitiveType<T>(), remote_input_buffers_span,
+          // Memory is aliased for both input and output (similar to what nccl
+          // would do).
+          /*local_input_buffer=*/local_input_buffers[i].memory(),
+          /*output_buffer=*/local_input_buffers[i].memory(),
+          /*rank=*/RankId(i), /*num_ranks=*/num_ranks,
+          /*num_elements=*/num_elements,
+          /*signal_flags_buffers=*/signal_flags_buffers_span));
+    }
+
+    for (int i = 0; i < num_ranks; ++i) {
+      TF_RETURN_IF_ERROR(streams[i]->BlockHostUntilDone());
+    }
+
+    std::vector<Array<T>> results;
+
+    for (int i = 0; i < num_ranks; ++i) {
+      Array<T> output_results({num_elements});
+      TF_RETURN_IF_ERROR(streams[i]->Memcpy(output_results.data(),
+                                            local_input_buffers[i].memory(),
+                                            num_elements * sizeof(T)));
+
+      results.push_back(std::move(output_results));
+    }
+
+    return results;
   }
 };
 
-TYPED_TEST_SUITE(AllReduceKernelTest, AllReduceKernelTestTypes,
-                 AllReduceKernelTestNameGenerator);
-
-TYPED_TEST(AllReduceKernelTest, SimpleKernelTest) {
-  using T = TypeParam;
-
+TEST_F(AllReduceKernelTest, KernelTestAddF32) {
   constexpr int64_t kNumRanks = 2;
   constexpr int64_t kNumElements = 128000;
 
-  LaunchDimensions launch_dimensions(/*block_x_count=*/8,
-                                     /*thread_x_count_per_block=*/512);
-
-  std::vector<se::StreamExecutor*> executors;
-  std::vector<std::unique_ptr<se::Stream>> streams;
-  std::vector<se::DeviceMemoryHandle> local_input_buffers;
-  std::vector<se::DeviceMemoryHandle> data_buffers;
-  std::vector<se::DeviceMemoryHandle> signal_flags_buffers;
-  std::vector<T> output_data(kNumElements);
-
-  std::vector<se::DeviceMemoryBase> remote_input_buffers_span;
-  std::vector<se::DeviceMemoryBase> signal_flags_buffers_span;
-
-  for (int i = 0; i < kNumRanks; ++i) {
-    auto* executor = GetGpuExecutor(i);
-    executors.push_back(executor);
-    streams.push_back(executor->CreateStream().value());
-
-    local_input_buffers.emplace_back(executor,
-                                     executor->AllocateArray<T>(kNumElements));
-    ASSERT_TRUE(!local_input_buffers[i].memory().is_null());
-
-    data_buffers.emplace_back(executor,
-                              executor->AllocateArray<T>(kNumElements));
-    ASSERT_TRUE(!data_buffers[i].memory().is_null());
-
-    signal_flags_buffers.emplace_back(
-        executor, executor->AllocateArray<uint32_t>(
-                      kNumRanks * launch_dimensions.num_blocks()));
-    ASSERT_TRUE(!signal_flags_buffers[i].memory().is_null());
-
-    std::vector<T> input_data(kNumElements);
-    std::iota(input_data.begin(), input_data.end(), static_cast<T>(0));
-    std::transform(input_data.begin(), input_data.end(), output_data.begin(),
-                   output_data.begin(), std::plus<T>());
-
-    TF_ASSERT_OK(streams[i]->Memcpy(local_input_buffers[i].memory_ptr(),
-                                    input_data.data(),
-                                    kNumElements * sizeof(T)));
-
-    remote_input_buffers_span.push_back(data_buffers[i].memory());
-    signal_flags_buffers_span.push_back(signal_flags_buffers[i].memory());
-  }
-
-  for (int i = 0; i < kNumRanks; ++i) {
-    TF_ASSERT_OK(streams[i]->BlockHostUntilDone());
-  }
+  std::vector<se::StreamExecutor*> executors = {GetGpuExecutor(0),
+                                                GetGpuExecutor(1)};
 
   if (!executors[0]->CanEnablePeerAccessTo(executors[1])) {
     GTEST_SKIP() << "Test requires direct peer memory access between devices.";
   }
 
-  TF_ASSERT_OK(executors[0]->EnablePeerAccessTo(executors[1]));
-  TF_ASSERT_OK(executors[1]->EnablePeerAccessTo(executors[0]));
+  Array<float> expected_output({kNumElements});
+  std::vector<Array<float>> inputs;
 
   for (int i = 0; i < kNumRanks; ++i) {
-    auto active_context = executors[i]->Activate();
-    TF_ASSERT_OK(RunAllReduceKernel(
-        streams[i].get(), launch_dimensions,
-        primitive_util::NativeToPrimitiveType<T>(), remote_input_buffers_span,
-        // Memory is aliased for both input and output (similar to what nccl
-        // would do).
-        /*local_input_buffer=*/local_input_buffers[i].memory(),
-        /*output_buffer=*/local_input_buffers[i].memory(),
-        /*rank=*/RankId(i), /*num_ranks=*/kNumRanks, kNumElements,
-        signal_flags_buffers_span));
+    Array<float> input_data({kNumElements});
+    input_data.FillRandom(0.0f, 10.0f, /*seed=*/i);
+
+    expected_output.Each([&](absl::Span<const int64_t> indices, float* val) {
+      *val += input_data(indices);
+    });
+
+    inputs.push_back(std::move(input_data));
   }
 
+  TF_ASSERT_OK_AND_ASSIGN(auto results, RunKernel<float>(executors, inputs));
+
   for (int i = 0; i < kNumRanks; ++i) {
-    TF_ASSERT_OK(streams[i]->BlockHostUntilDone());
+    EXPECT_EQ(results[i], expected_output);
+  }
+}
+
+TEST_F(AllReduceKernelTest, KernelTestAddBF16) {
+  constexpr int64_t kNumRanks = 2;
+  constexpr int64_t kNumElements = 128000;
+
+  std::vector<se::StreamExecutor*> executors = {GetGpuExecutor(0),
+                                                GetGpuExecutor(1)};
+
+  if (!executors[0]->CanEnablePeerAccessTo(executors[1])) {
+    GTEST_SKIP() << "Test requires direct peer memory access between devices.";
   }
 
-  for (int i = 0; i < kNumRanks; ++i) {
-    std::vector<T> output_results(kNumElements);
-    TF_ASSERT_OK(streams[i]->Memcpy(output_results.data(),
-                                    local_input_buffers[i].memory(),
-                                    kNumElements * sizeof(T)));
+  Array<bfloat16> expected_output({kNumElements});
+  std::vector<Array<bfloat16>> inputs;
 
-    EXPECT_EQ(output_results, output_data);
+  for (int i = 0; i < kNumRanks; ++i) {
+    Array<bfloat16> input_data({kNumElements});
+    input_data.FillRandom(static_cast<bfloat16>(0.0f),
+                          static_cast<bfloat16>(10.0f), /*seed=*/i);
+
+    expected_output.Each([&](absl::Span<const int64_t> indices, bfloat16* val) {
+      *val += input_data(indices);
+    });
+
+    inputs.push_back(std::move(input_data));
+  }
+
+  TF_ASSERT_OK_AND_ASSIGN(auto results, RunKernel<bfloat16>(executors, inputs));
+
+  for (int i = 0; i < kNumRanks; ++i) {
+    EXPECT_EQ(results[i], expected_output);
   }
 }
 
