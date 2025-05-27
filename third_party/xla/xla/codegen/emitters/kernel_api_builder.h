247ff548abd1d0e567586964205ad82b0cba7aa7
Will Froom <willfroom@google.com>
2025-05-27 05:12:53 -0700

    [XLA:GPU] Move entry function codegen to shared directory
    PiperOrigin-RevId: 763742793

---
+++ third_party/xla/xla/codegen/emitters/kernel_api_builder.h
@@ -0,0 +1,41 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_CODEGEN_EMITTERS_KERNEL_API_BUILDER_H_
+#define XLA_CODEGEN_EMITTERS_KERNEL_API_BUILDER_H_
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "xla/codegen/emitters/kernel_arguments.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/service/buffer_assignment.h"
+
+namespace xla::emitters {
+
+// Emits and return the kernel entry function into the provided module it for
+// the given HLO instruction.
+// The function will be given the provided name and the arguments will be
+// annotated with the buffer indices.
+absl::StatusOr<mlir::func::FuncOp> EmitKernelApi(
+    mlir::ModuleOp module, const HloInstruction& hlo_instruction,
+    const BufferAssignment* buffer_assignment,
+    const KernelArguments::BufferAlignment& buffer_alignment,
+    absl::string_view entry_function_name);
+
+}  // namespace xla::emitters
+
+#endif  // XLA_CODEGEN_EMITTERS_KERNEL_API_BUILDER_H_
