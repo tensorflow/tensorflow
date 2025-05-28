ab26e48646bc153932947e70fc1081110c20662f
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-27 22:01:40 -0700

    Added int8 support for DIV operation
    PiperOrigin-RevId: 764089142

--- tensorflow/lite/kernels/div_test.cc
+++ tensorflow/lite/kernels/div_test.cc
@@ -260,6 +260,10 @@ TEST(QuantizedDivOpTest, QuantizedNoActivationUInt8) {
   QuantizedNoActivation<TensorType_UINT8, uint8_t>();
 }
 
+TEST(QuantizedDivOpTest, QuantizedNoActivationInt8) {
+  QuantizedNoActivation<TensorType_INT8, int8_t>();
+}
+
 template <TensorType tensor_type, typename integer_dtype>
 void QuantizedActivationRELU_N1_TO_1() {
   const float kQuantizedTolerance = GetTolerance(-1.0, 1.0);
@@ -286,6 +290,10 @@ void QuantizedActivationRELU_N1_TO_1() {
 
 TEST(QuantizedDivOpTest, QuantizedActivationRELU_N1_TO_1UInt8) {
   QuantizedActivationRELU_N1_TO_1<TensorType_UINT8, uint8_t>();
+}
+
+TEST(QuantizedDivOpTest, QuantizedActivationRELU_N1_TO_1Int8) {
+  QuantizedActivationRELU_N1_TO_1<TensorType_INT8, int8_t>();
 }
 
 template <TensorType tensor_type, typename integer_dtype>
@@ -315,6 +323,10 @@ TEST(QuantizedDivOpTest, QuantizedVariousInputShapesUInt8) {
   QuantizedVariousInputShapes<TensorType_UINT8, uint8_t>();
 }
 
+TEST(QuantizedDivOpTest, QuantizedVariousInputShapesInt8) {
+  QuantizedVariousInputShapes<TensorType_INT8, int8_t>();
+}
+
 template <TensorType tensor_type, typename integer_dtype>
 void QuantizedWithBroadcast() {
   const float kQuantizedTolerance = GetTolerance(-3.0, 3.0);
@@ -338,6 +350,10 @@ void QuantizedWithBroadcast() {
 
 TEST(QuantizedDivOpTest, QuantizedWithBroadcastUInt8) {
   QuantizedWithBroadcast<TensorType_UINT8, uint8_t>();
+}
+
+TEST(QuantizedDivOpTest, QuantizedWithBroadcastInt8) {
+  QuantizedWithBroadcast<TensorType_INT8, int8_t>();
 }
 
 }  // namespace
