CodeLinaro-mirror/yocto-mirrors_github_tensorflow_tensorflow

Remove the need to downcast kernel definition from emitters by templating on concrete source type.
PiperOrigin-RevId: 762446133

third_party/xla/xla/codegen/kernel_emitter.h
@@ -20,41 +20,34 @@ limitations under the License.
 
 #include "absl/status/statusor.h"
 #include "xla/codegen/kernel_definition.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace xla {
 
-// TODO(ezhulenev): Do we need virtual KernelEmitterContext in API?
+class KernelEmitterBase {
+ public:
+  virtual ~KernelEmitterBase() = default;
+
+  virtual absl::StatusOr<std::unique_ptr<KernelDefinitionBase>>
+  EmitBaseKernelDefinition() = 0;
+};
 
 // KernelEmitter is an API that emits kernel definition from a given input
 // (i.e. it emits kernels compiled from HLO fusions).
-class KernelEmitter {
+template <typename KernelDefinitionT>
+class KernelEmitter : public KernelEmitterBase {
  public:
   virtual ~KernelEmitter() = default;
 
-  virtual absl::StatusOr<KernelDefinition> EmitKernelDefinition() = 0;
-};
-
-// A base class for backend-specific kernel emitters.
-//
-// Example: XLA:GPU backend kernel emitter.
-//
-//   class xla::gpu::GpuPlatform;
-//
-//   class xla::gpu::HloFusionEmitter :
-//     public KernelEmitter<GpuPlatform, const HloFusionInstruction*>;
-//
-template <typename Platform, typename Operation>
-class KernelEmitterBase {
- public:
-  KernelEmitterBase(std::shared_ptr<Platform> platform, Operation operation)
-      : platform_(std::move(platform)), operation_(std::move(operation)) {}
-
-  const Operation& operation() const { return operation_; }
-  const Platform& platform() const { return *platform_; }
+  virtual absl::StatusOr<KernelDefinitionT> EmitKernelDefinition() = 0;
 
  private:
-  std::shared_ptr<Platform> platform_;
-  Operation operation_;
+  absl::StatusOr<std::unique_ptr<KernelDefinitionBase>>
+  EmitBaseKernelDefinition() final {
+    TF_ASSIGN_OR_RETURN(KernelDefinitionT kernel_definition,
+                        EmitKernelDefinition());
+    return std::make_unique<KernelDefinitionT>(std::move(kernel_definition));
+  }
 };
 
 }  // namespace xla

tensorflow/tensorflow

third_party/xla/xla/codegen/kernel_emitter.h
@@ -20,41 +20,34 @@ limitations under the License.
 
 #include "absl/status/statusor.h"
 #include "xla/codegen/kernel_definition.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace xla {
 
-// TODO(ezhulenev): Do we need virtual KernelEmitterContext in API?
+class KernelEmitterBase {
+ public:
+  virtual ~KernelEmitterBase() = default;
+
+  virtual absl::StatusOr<std::unique_ptr<KernelDefinitionBase>>
+  EmitBaseKernelDefinition() = 0;
+};
 
 // KernelEmitter is an API that emits kernel definition from a given input
 // (i.e. it emits kernels compiled from HLO fusions).
-class KernelEmitter {
+template <typename KernelDefinitionType>
+class KernelEmitter : public KernelEmitterBase {
  public:
   virtual ~KernelEmitter() = default;
 
-  virtual absl::StatusOr<KernelDefinition> EmitKernelDefinition() = 0;
-};
-
-// A base class for backend-specific kernel emitters.
-//
-// Example: XLA:GPU backend kernel emitter.
-//
-//   class xla::gpu::GpuPlatform;
-//
-//   class xla::gpu::HloFusionEmitter :
-//     public KernelEmitter<GpuPlatform, const HloFusionInstruction*>;
-//
-template <typename Platform, typename Operation>
-class KernelEmitterBase {
- public:
-  KernelEmitterBase(std::shared_ptr<Platform> platform, Operation operation)
-      : platform_(std::move(platform)), operation_(std::move(operation)) {}
-
-  const Operation& operation() const { return operation_; }
-  const Platform& platform() const { return *platform_; }
+  virtual absl::StatusOr<KernelDefinitionType> EmitKernelDefinition() = 0;
 
  private:
-  std::shared_ptr<Platform> platform_;
-  Operation operation_;
+  absl::StatusOr<std::unique_ptr<KernelDefinitionBase>>
+  EmitBaseKernelDefinition() final {
+    TF_ASSIGN_OR_RETURN(KernelDefinitionType kernel_definition,
+                        EmitKernelDefinition());
+    return std::make_unique<KernelDefinitionType>(std::move(kernel_definition));
+  }
 };
 
 }  // namespace xla