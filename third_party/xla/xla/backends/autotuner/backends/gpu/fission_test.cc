e885bbd686e9d414b7b453326cc214af9f1bd55a
A. Unique TensorFlower <gardener@tensorflow.org>
2025-05-27 19:58:21 -0700

    Automated Code Change
    PiperOrigin-RevId: 764053235

--- third_party/xla/xla/backends/autotuner/backends/gpu/fission_test.cc
+++ third_party/xla/xla/backends/autotuner/backends/gpu/fission_test.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/tsl/platform/status_matchers.h"
 #include "xla/tsl/platform/statusor.h"
+#include "xla/xla.pb.h"
 
 namespace xla {
 namespace gpu {
