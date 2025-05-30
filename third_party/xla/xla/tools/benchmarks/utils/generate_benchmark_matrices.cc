bf044cf112750528d47309bb049a553eb277f30d
Quoc Truong <quoct@google.com>
2025-05-30 12:03:52 -0700

    Change the container in generate_benchmark_matrices to use the new one us-docker.pkg.dev/ml-ss-artifacts-published/ml-public-container.
    The older container is `us-central1-docker.pkg.dev` is no longer maintained.
    
    PiperOrigin-RevId: 765283088

--- third_party/xla/xla/tools/benchmarks/utils/generate_benchmark_matrices.cc
+++ third_party/xla/xla/tools/benchmarks/utils/generate_benchmark_matrices.cc
@@ -111,19 +111,19 @@ GetHardwareToContainerImage() {
   static const auto* kHardwareToContainerImage =
       new absl::flat_hash_map<std::string, std::string>{
           {"CPU_X86",
-           "us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/"
+           "us-docker.pkg.dev/ml-oss-artifacts-published/ml-public-container/"
            "ml-build:latest"},
           {"CPU_ARM64",
-           "us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/"
+           "us-docker.pkg.dev/ml-oss-artifacts-published/ml-public-container/"
            "ml-build-arm64:latest"},
           {"GPU_L4",
-           "us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/"
+           "us-docker.pkg.dev/ml-oss-artifacts-published/ml-public-container/"
            "ml-build-cuda12.8-cudnn9.8:latest"},
           {"GPU_B200",
-           "us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/"
+           "us-docker.pkg.dev/ml-oss-artifacts-published/ml-public-container/"
            "ml-build-cuda12.8-cudnn9.8:latest"},
           {"GPU_L4_1H_4D",
-           "us-central1-docker.pkg.dev/tensorflow-sigs/tensorflow/"
+           "us-docker.pkg.dev/ml-oss-artifacts-published/ml-public-container/"
            "ml-build-cuda12.8-cudnn9.8:latest"},
       };
   return *kHardwareToContainerImage;
