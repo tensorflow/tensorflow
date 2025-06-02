ed43b383bdb58e52330739c8bec8d4ca9e29f3dc
A. Unique TensorFlower <gardener@tensorflow.org>
2025-06-01 20:23:55 -0700

    Automated Code Change
    PiperOrigin-RevId: 766005072

--- tensorflow/compiler/mlir/quantization/tensorflow/utils/tf_tf_to_xla_attribute_utils.cc
+++ tensorflow/compiler/mlir/quantization/tensorflow/utils/tf_tf_to_xla_attribute_utils.cc
@@ -13,9 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <algorithm>
-#include <numeric>
-#include <string>
+#include <cstdint>
 
 #include "absl/algorithm/container.h"
 #include "absl/strings/str_format.h"
