c6c49948886115827223206d61ef13e8005cb132
Rachel Han <hanrach@google.com>
2025-06-02 14:38:20 -0700

    Take serialized PJRT executable as an input for hlo runner.
    PiperOrigin-RevId: 766347717

--- third_party/xla/xla/tools/multihost_hlo_runner/hlo_input_output_format.h
+++ third_party/xla/xla/tools/multihost_hlo_runner/hlo_input_output_format.h
@@ -40,6 +40,7 @@ enum class InputFormat {
                                     // format. Can be dumped by TensorFlow by
                                     // setting the flag xla_dump_hlo_snapshots
                                     // in conjunction with xla_dump_as_text.
+  kSerializedPjRtExecutable,        // Serialized PJRT executable.
 };
 
 bool AbslParseFlag(absl::string_view text, InputFormat* input_format,
