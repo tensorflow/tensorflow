84378a76414fcf340b750bc6b44f9e850e339264
A. Unique TensorFlower <gardener@tensorflow.org>
2025-06-04 19:15:54 -0700

    Automated Code Change
    PiperOrigin-RevId: 767380743

--- third_party/xla/xla/backends/cpu/codegen/emitters/cpu_scatter_emitter.cc
+++ third_party/xla/xla/backends/cpu/codegen/emitters/cpu_scatter_emitter.cc
@@ -28,7 +28,6 @@ limitations under the License.
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
-#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "llvm/ADT/STLExtras.h"
