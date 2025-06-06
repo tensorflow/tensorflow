5079c04622254cf044f236f7ad0f1c2083e8b785
A. Unique TensorFlower <gardener@tensorflow.org>
2025-06-05 20:46:49 -0700

    Automated Code Change
    PiperOrigin-RevId: 767886012

--- tensorflow/lite/experimental/acceleration/compatibility/devicedb_test.cc
+++ tensorflow/lite/experimental/acceleration/compatibility/devicedb_test.cc
@@ -14,7 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/experimental/acceleration/compatibility/devicedb.h"
 
-#include <memory>
+#include <map>
 #include <string>
 
 #include <gtest/gtest.h>
