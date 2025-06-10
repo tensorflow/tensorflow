0e44c8cc8854a6a7ec62667a1792a77d4557f359
Daniel Sosa <sosagarcia@google.com>
2025-06-10 09:56:42 -0700

    cleanup for fake_quant_utils
    PiperOrigin-RevId: 769676506

--- tensorflow/compiler/mlir/quantization/tensorflow/utils/fake_quant_utils.h
+++ tensorflow/compiler/mlir/quantization/tensorflow/utils/fake_quant_utils.h
@@ -20,12 +20,15 @@ limitations under the License.
 #define TENSORFLOW_COMPILER_MLIR_QUANTIZATION_TENSORFLOW_UTILS_FAKE_QUANT_UTILS_H_
 
 #include "mlir/IR/Attributes.h"  // from @llvm-project
-#include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
+#include "mlir/IR/Builders.h"  // from @llvm-project
+#include "mlir/IR/BuiltinAttributes.h"  // from @llvm-project
+#include "mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
 #include "mlir/IR/MLIRContext.h"  // from @llvm-project
+#include "mlir/IR/Matchers.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
 #include "mlir/Support/LogicalResult.h"  // from @llvm-project
-#include "tensorflow/compiler/mlir/lite/quantization/ir/QuantOps.h"
-#include "tensorflow/compiler/mlir/quantization/common/quantization_lib/quantization_utils.h"
+#include "tensorflow/compiler/mlir/quantization/common/ir/QuantOps.h"
+#include "tensorflow/compiler/mlir/quantization/common/tf_quantization_lib/tf_quantization_utils.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops_a_m.h"
 
 namespace mlir {
@@ -127,7 +130,7 @@ class ConvertFakeQuantOpToQuantOps {
     IntegerAttr num_bits = rewriter.getI64IntegerAttr(tf_op.getNumBits());
     BoolAttr narrow_range = rewriter.getBoolAttr(tf_op.getNarrowRange());
     Type res_type = tf_op.getType();
-    TypeAttr qtype = quant::GetQuantizedTypeAttr(
+    TypeAttr qtype = tf_quant::GetQuantizedTypeAttr(
         rewriter, input_type, min_value, max_value, quant_dim, num_bits,
         narrow_range, /*is_signed=*/true, /*legacy_float_scale=*/false,
         use_fake_quant_num_bits_);
@@ -138,9 +141,9 @@ class ConvertFakeQuantOpToQuantOps {
     // Finally, use the quantization parameter to create the quantize and
     // dequantize ops, and insert them between the tf.FakeQuantWithMinMaxVarsOp
     // and its users.
-    auto quantize = rewriter.create<quantfork::QuantizeCastOp>(
+    auto quantize = rewriter.create<mlir::quant::ir::QuantizeCastOp>(
         tf_op.getLoc(), qtype.getValue(), input);
-    auto dequantize = rewriter.create<quantfork::DequantizeCastOp>(
+    auto dequantize = rewriter.create<mlir::quant::ir::DequantizeCastOp>(
         tf_op.getLoc(), res_type, quantize.getResult());
     tf_op.getOutputs().replaceAllUsesWith(dequantize);
 
