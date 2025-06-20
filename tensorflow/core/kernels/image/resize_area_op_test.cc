598aa82805fdef609d797df6c1aafef8b7ae1aed
A. Unique TensorFlower <gardener@tensorflow.org>
2025-06-19 20:18:14 -0700

    Automated Code Change
    PiperOrigin-RevId: 773509833

--- tensorflow/core/kernels/image/resize_area_op_test.cc
+++ tensorflow/core/kernels/image/resize_area_op_test.cc
@@ -13,12 +13,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <algorithm>
 #include <cmath>
+#include <cstdint>
+#include <memory>
 
+#include "absl/log/check.h"
 #include "tensorflow/core/common_runtime/kernel_benchmark_testlib.h"
 #include "tensorflow/core/framework/fake_input.h"
 #include "tensorflow/core/framework/node_def_builder.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/kernels/ops_testutil.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
 #include "tensorflow/core/platform/test.h"
