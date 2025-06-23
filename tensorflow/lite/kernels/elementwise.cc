47eb8dac257eb0bc6b850c8711ca48bb3cb8a063
A. Unique TensorFlower <gardener@tensorflow.org>
2025-06-23 12:14:56 -0700

    Integrate LLVM at llvm/llvm-project@bae48ac3c0e6
    Updates LLVM usage to match
    [bae48ac3c0e6](https://github.com/llvm/llvm-project/commit/bae48ac3c0e6)
    
    PiperOrigin-RevId: 774878157

--- tensorflow/lite/kernels/elementwise.cc
+++ tensorflow/lite/kernels/elementwise.cc
@@ -299,7 +299,8 @@ TfLiteStatus AbsEval(TfLiteContext* context, TfLiteNode* node) {
   const TfLiteType type = input->type;
   switch (type) {
     case kTfLiteFloat32:
-      return EvalImpl<float>(context, node, std::abs<float>, type);
+      return EvalImpl<float>(
+          context, node, [](float f) { return std::abs(f); }, type);
     case kTfLiteInt8:
       return AbsEvalQuantized<int8_t>(context, node, type);
     case kTfLiteInt16:
@@ -307,7 +308,8 @@ TfLiteStatus AbsEval(TfLiteContext* context, TfLiteNode* node) {
                  ? AbsInt16EvalImpl(context, node, type)
                  : AbsEvalQuantized<int16_t>(context, node, type);
     case kTfLiteInt32:
-      return EvalImpl<int32_t>(context, node, std::abs<int32_t>, type);
+      return EvalImpl<int32_t>(
+          context, node, [](int32_t i) { return std::abs(i); }, type);
     default:
       TF_LITE_KERNEL_LOG(context, "Current data type %s is not supported.",
                          TfLiteTypeGetName(type));
