7c0ac5c5ce6bc5c83db2e779c50baeefa27234a3
shadowemperor1510 <adityaaditya@iitgn.ac.in>
2025-06-24 18:23:40 +0530

    Log errors to stderr for review

--- tensorflow/python/platform/tf_logging.py
+++ tensorflow/python/platform/tf_logging.py
@@ -32,6 +32,10 @@ import threading
 
 from tensorflow.python.util.tf_export import tf_export
 
+import sys
+import logging
+logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
+
 # Don't use this directly. Use get_logger() instead.
 _logger = None
 _logger_lock = threading.Lock()
