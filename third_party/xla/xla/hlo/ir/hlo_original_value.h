21e89cfe70a2a416b773778e6ddd88f0388e4c24
Jian Cai <jiancai@google.com>
2025-07-02 10:30:22 -0700

    Reproduce an OSS test failure
    PiperOrigin-RevId: 778557752

--- third_party/xla/xla/hlo/ir/hlo_original_value.h
+++ third_party/xla/xla/hlo/ir/hlo_original_value.h
@@ -84,14 +84,11 @@ struct OriginalValuePointer {
     // Compares nodes.
     for (auto& leaf : lhs.original_value->leaves()) {
       xla::ShapeIndex index = leaf.first;
-      std::optional<xla::OriginalArray> lhs_original_array = leaf.second;
-      std::optional<xla::OriginalArray> rhs_original_array =
+      std::optional<const xla::OriginalArray> lhs_original_array = leaf.second;
+      std::optional<const xla::OriginalArray> rhs_original_array =
           rhs.original_value->element(index);
       if (!lhs_original_array.has_value() || !rhs_original_array.has_value() ||
-          (lhs_original_array->instruction_name !=
-           rhs_original_array->instruction_name) ||
-          (lhs_original_array->shape_index !=
-           rhs_original_array->shape_index)) {
+          *lhs_original_array != *rhs_original_array) {
         return false;
       }
     }
