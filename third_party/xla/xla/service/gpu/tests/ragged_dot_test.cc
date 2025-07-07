6991f6e761db28f3b7b6fe98cfe0142ac8021ab1
Tori Baker <vwbaker@google.com>
2025-07-07 00:45:50 -0700

    Add Ragged Dot Extra LHS dim test.
    new_dim_index is where we expand the matrix. At the index of the size of the group_sizes matrix - 1 makes sense because the last dim of group_sizes is always the one that represents the group and the ones before that are either extra dims that also appear in the matrices that get expanded (such as other non-contracting lhs dims if in NonContracting mode, contracting lhs/rhs dims is in contracting mode, or batch dims).
    
    PiperOrigin-RevId: 779986546

--- third_party/xla/xla/service/gpu/tests/ragged_dot_test.cc
+++ third_party/xla/xla/service/gpu/tests/ragged_dot_test.cc
@@ -110,6 +110,33 @@ ENTRY main {
                             ErrorSpec{0, 0}));
 }
 
+TEST_F(RaggedDotTest, NonContractingWithExtraLhsDim) {
+  const char* hlo_text = R"(
+HloModule m
+
+ENTRY main {
+  p0 = f32[2,6,4]{2,1,0} parameter(0)
+  p1 = f32[3,4,5]{2,1,0} parameter(1)
+  p2 = s64[2,3]{1,0} parameter(2)
+  ROOT ragged-dot = f32[2,6,5]{2,1,0} ragged-dot(p0, p1, p2),
+                      lhs_contracting_dims={2}, rhs_contracting_dims={1},
+                      lhs_ragged_dims={1}, rhs_group_dims={0}
+}
+)";
+  TF_ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(hlo_text));
+  TF_ASSERT_OK_AND_ASSIGN(
+      auto fake_arguments,
+      MakeFakeArguments(module.get(), /*pseudo_random=*/true,
+                        /*use_large_range=*/false,
+                        /*treat_gte_as_data_formatting=*/false,
+                        /*max_bits_of_precision=*/10));
+  // Set group sizes to reasonable numbers for ragged_dim_size=6.
+  fake_arguments[2] = LiteralUtil::CreateR2<int64_t>({{1, 2, 3}, {3, 2, 1}});
+  EXPECT_TRUE(RunAndCompare(std::move(module),
+                            LiteralUtil::MakePointers(fake_arguments),
+                            ErrorSpec{0, 0}));
+}
+
 }  // namespace
 }  // namespace gpu
 }  // namespace xla
