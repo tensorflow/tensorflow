5d3b3b7e78b43f460214f3e238e17a29581354f6
Junwhan Ahn <junwhan@google.com>
2025-07-09 11:28:20 -0700

    Remove the deprecated version of `Array::DisassembleIntoSingleDeviceArrays`
    PiperOrigin-RevId: 781138001

--- third_party/xla/xla/python/ifrt/array.h
+++ third_party/xla/xla/python/ifrt/array.h
@@ -22,8 +22,6 @@ limitations under the License.
 #include <vector>
 
 #include "absl/base/attributes.h"
-#include "absl/base/macros.h"
-#include "absl/base/nullability.h"
 #include "absl/status/statusor.h"
 #include "absl/types/span.h"
 #include "llvm/Support/ExtensibleRTTI.h"
@@ -93,15 +91,6 @@ class Array : public llvm::RTTIExtends<Array, Value> {
   DisassembleIntoSingleDeviceArrays(
       ArrayCopySemantics array_copy_semantics,
       SingleDeviceShardSemantics single_device_shard_semantics) = 0;
-
-  // TODO(hyeontaek): Replace this API with the version that takes
-  // `SingleDeviceShardSemantics`.
-  ABSL_DEPRECATE_AND_INLINE()
-  absl::StatusOr<std::vector<ArrayRef>> DisassembleIntoSingleDeviceArrays(
-      ArrayCopySemantics semantics) {
-    return DisassembleIntoSingleDeviceArrays(
-        semantics, SingleDeviceShardSemantics::kAddressableShards);
-  }
 
   // Returns a shard of an Array which is fully replicated. This is an
   // optimization so that instead of disassembling into all the shards when
