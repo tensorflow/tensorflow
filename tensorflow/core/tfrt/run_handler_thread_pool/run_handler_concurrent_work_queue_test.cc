8a026ad1ac9c97eb95a2382dfbd79edc54b68c0a
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-10 12:39:50 -0700

    Return DeadlineExceededError when getting handler from the pool times out.
    PiperOrigin-RevId: 781641050

--- tensorflow/core/tfrt/run_handler_thread_pool/run_handler_concurrent_work_queue_test.cc
+++ tensorflow/core/tfrt/run_handler_thread_pool/run_handler_concurrent_work_queue_test.cc
@@ -19,6 +19,7 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/status/status.h"
 #include "absl/strings/match.h"
 #include "xla/tsl/protobuf/error_codes.pb.h"
 #include "tensorflow/core/platform/errors.h"
@@ -184,7 +185,7 @@ TEST_F(RunHandlerThreadWorkQueueTest, NoHandlerReturnsError) {
   EXPECT_THAT(
       queue->InitializeRequest(/*request_id=*/100),
       tensorflow::testing::StatusIs(
-          tensorflow::error::INTERNAL,
+          absl::StatusCode::kDeadlineExceeded,
           "Could not obtain RunHandler for request after waiting for 1 ms."));
 }
 
