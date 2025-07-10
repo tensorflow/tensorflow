2e245e0e6b2fc5f40268d2af8dc985b12b8fa213
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-09 22:38:12 -0700

    Automated Code Change
    PiperOrigin-RevId: 781370948

--- third_party/xla/xla/pjrt/tracked_device_buffer.h
+++ third_party/xla/xla/pjrt/tracked_device_buffer.h
@@ -28,6 +28,7 @@ limitations under the License.
 #include "absl/container/inlined_vector.h"
 #include "absl/functional/any_invocable.h"
 #include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/synchronization/mutex.h"
 #include "absl/types/span.h"
