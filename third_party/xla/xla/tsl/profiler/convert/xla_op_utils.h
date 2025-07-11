430f05efccd0613179b26a6d70547e0ec5329dca
Bryan Massoth <bmassoth@google.com>
2025-07-11 14:04:23 -0700

    Add HloInstruction extractor for custom-call kernel metadata.
    PiperOrigin-RevId: 782102451

--- third_party/xla/xla/tsl/profiler/convert/xla_op_utils.h
+++ third_party/xla/xla/tsl/profiler/convert/xla_op_utils.h
@@ -15,11 +15,19 @@ limitations under the License.
 #ifndef XLA_TSL_PROFILER_CONVERT_XLA_OP_UTILS_H_
 #define XLA_TSL_PROFILER_CONVERT_XLA_OP_UTILS_H_
 
+#include <cstdint>
+#include <memory>
 #include <string>
 
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "nlohmann/json.hpp"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_module.h"
+#include "xla/hlo/parser/hlo_parser.h"
 #include "xla/tsl/platform/macros.h"
 #include "tsl/profiler/lib/traceme_encode.h"
 
@@ -184,6 +192,49 @@ struct OpSourceInfo {
   std::string GetSourceStack() const { return stack_frame; }
 };
 
+// Extracts the xprof kernel metadata from the given HLO expression.
+// Returns an empty string if the HLO expression does not contain xprof kernel
+// metadata.
+inline std::string ExtractXprofKernelMetadata(
+    absl::string_view hlo_expression) {
+  if (!absl::StrContains(hlo_expression, "xprof_metadata")) {
+    return "";
+  }
+  xla::HloParserOptions options;
+  options.set_fill_missing_layouts(false);
+  absl::StatusOr<std::unique_ptr<xla::HloModule>> hlo_module =
+      xla::ParseAndReturnUnverifiedModule(hlo_expression,
+                                          /*config=*/{}, options);
+  if (!hlo_module.ok()) {
+    LOG_EVERY_N_SEC(ERROR, 1)
+        << "Failed to parse HLO module: " << hlo_module.status();
+    return "";
+  }
+  const xla::HloInstruction* instruction =
+      hlo_module->get()->entry_computation()->root_instruction();
+  const xla::FrontendAttributes& frontend_attributes =
+      instruction->frontend_attributes();
+  if (frontend_attributes.map().contains("kernel_metadata")) {
+    nlohmann::json kernel_metadata =
+        nlohmann::json::parse(frontend_attributes.map().at("kernel_metadata"),
+                              /*cb=*/nullptr, /*allow_exceptions=*/false,
+                              /*ignore_comments=*/true);
+    if (!kernel_metadata.is_discarded() &&
+        kernel_metadata.contains("xprof_metadata")) {
+      nlohmann::json xprof_metadata = kernel_metadata["xprof_metadata"];
+      if (xprof_metadata.is_string()) {
+        xprof_metadata =
+            nlohmann::json::parse(xprof_metadata.get<std::string>(),
+                                  /*cb=*/nullptr, /*allow_exceptions=*/false,
+                                  /*ignore_comments=*/true);
+      }
+      if (!xprof_metadata.is_discarded()) {
+        return xprof_metadata.dump();
+      }
+    }
+  }
+  return "";
+}
 }  // namespace profiler
 }  // namespace tsl
 
--- third_party/xla/xla/tsl/profiler/convert/xla_op_utils_test.cc
+++ third_party/xla/xla/tsl/profiler/convert/xla_op_utils_test.cc
@@ -15,12 +15,45 @@ limitations under the License.
 
 #include "xla/tsl/profiler/convert/xla_op_utils.h"
 
+#include <string>
+
 #include <gtest/gtest.h>
 #include "xla/tsl/platform/test.h"
 
 namespace tsl {
 namespace profiler {
 namespace {
+
+TEST(XlaOpUtilsTest, ExtractXprofKernelMetadataTest) {
+  std::string hlo_expression = R"(
+  %blah.1 = bf16[4096,2048]{1,0:T(8,128)(2,1)}
+    custom-call(s32[]{:T(128)} %bitcast), custom_call_target="tpu_custom_call",
+    frontend_attributes={
+      kernel_metadata={"xprof_metadata": "{\"whatever\": {\"I\": \"want\"}}"}}
+)";
+  std::string expected = R"({"whatever":{"I":"want"}})";
+  EXPECT_EQ(ExtractXprofKernelMetadata(hlo_expression), expected);
+}
+
+TEST(XlaOpUtilsTest, ExtractXprofKernelMetadataTest_InvalidHlo) {
+  std::string hlo_expression = R"(
+  %blah.1 = bf16[4096,2048]{1,0:T(8,128)(2,1)}
+    custom-call(s32[]{:T(128)} %bitcast), custom_call_target="tpu_custom_call",
+    frontend_attributes={
+      kernel_metadata={"xprof_metadata": "{broken_json\"}}"}}
+)";
+  EXPECT_EQ(ExtractXprofKernelMetadata(hlo_expression), "");
+}
+
+TEST(XlaOpUtilsTest, ExtractXprofKernelMetadataTest_NoXprofMetadata) {
+  std::string hlo_expression = R"(
+  %blah.1 = bf16[4096,2048]{1,0:T(8,128)(2,1)}
+    custom-call(s32[]{:T(128)} %bitcast), custom_call_target="tpu_custom_call",
+    frontend_attributes={
+      kernel_metadata={}"}}
+)";
+  EXPECT_EQ(ExtractXprofKernelMetadata(hlo_expression), "");
+}
 
 TEST(XlaOpUtilsTest, HloModuleNameWithProgramId) {
   EXPECT_EQ("module(123)", HloModuleNameWithProgramId("module", 123));
