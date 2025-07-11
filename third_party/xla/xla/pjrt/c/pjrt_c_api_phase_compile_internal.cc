71b19d1848d1b413479083e2b450554207a2a134
Sandeep Dasgupta <sdasgup@google.com>
2025-07-11 16:24:40 -0700

    [Phase Compilation] Part-4: Add C++ layers to test and interact with C PJRT API.
    PiperOrigin-RevId: 782147317

--- third_party/xla/xla/pjrt/c/pjrt_c_api_phase_compile_internal.cc
+++ third_party/xla/xla/pjrt/c/pjrt_c_api_phase_compile_internal.cc
@@ -15,12 +15,13 @@ limitations under the License.
 
 #include "xla/pjrt/c/pjrt_c_api_phase_compile_internal.h"
 
-#include <cassert>
 #include <cstddef>
 #include <cstring>
 #include <string>
+#include <utility>
 #include <vector>
 
+#include "absl/cleanup/cleanup.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
@@ -30,9 +31,24 @@ limitations under the License.
 #include "xla/pjrt/c/pjrt_c_api_phase_compile_extension.h"
 #include "xla/pjrt/c/pjrt_c_api_wrapper_impl.h"
 #include "xla/pjrt/pjrt_compiler.h"
+#include "xla/pjrt/pjrt_executable.h"
 #include "xla/pjrt/proto/pjrt_partial_program.pb.h"
 
 namespace pjrt {
+
+namespace {
+
+absl::StatusOr<xla::CompileOptions> ParseCompileOptions(
+    absl::string_view options_str) {
+  xla::CompileOptionsProto options_proto;
+  if (!options_proto.ParseFromArray(options_str.data(), options_str.size())) {
+    return absl::InvalidArgumentError(
+        "PJRT_Client_Compile: failed to deserialize CompileOptionsProto");
+  }
+  return xla::CompileOptions::FromProto(options_proto);
+}
+
+}  // namespace
 
 std::vector<std::string> ConvertCharBuffersToCppStrings(
     const char** char_buffers, const size_t* char_buffer_sizes,
@@ -70,27 +86,117 @@ const char** ConvertCppStringsToCharBuffers(
   return char_buffers;
 }
 
+absl::StatusOr<std::vector<xla::PjRtPartialProgramProto>>
+ConvertCharBuffersToPjRtPartialProgramProtos(const char** char_buffers,
+                                             const size_t* char_buffer_sizes,
+                                             size_t num_programs) {
+  if (char_buffers == nullptr || char_buffer_sizes == nullptr) {
+    return std::vector<xla::PjRtPartialProgramProto>();
+  }
+
+  std::vector<xla::PjRtPartialProgramProto> partial_programs;
+  partial_programs.reserve(num_programs);
+  for (size_t i = 0; i < num_programs; ++i) {
+    xla::PjRtPartialProgramProto partial_program;
+    bool success =
+        partial_program.ParseFromArray(char_buffers[i], char_buffer_sizes[i]);
+    if (!success) {
+      return absl::InvalidArgumentError(
+          "Failed to deserialize PjRtPartialProgramProto");
+    }
+    partial_programs.push_back(partial_program);
+  }
+
+  return partial_programs;
+}
+
+absl::StatusOr<const char**> ConvertPjRtPartialProgramProtosToCharBuffers(
+    const std::vector<xla::PjRtPartialProgramProto>& partial_programs,
+    const size_t*& char_buffer_sizes) {
+  const char** char_buffers = new const char*[partial_programs.size()]();
+  size_t* buffer_sizes = new size_t[partial_programs.size()];
+  absl::Cleanup cleanup_buffers = [char_buffers, buffer_sizes,
+                                   partial_programs_size =
+                                       partial_programs.size()] {
+    for (size_t i = 0; i < partial_programs_size; ++i) {
+      delete[] char_buffers[i];
+    }
+    delete[] char_buffers;
+    delete[] buffer_sizes;
+  };
+
+  for (size_t i = 0; i < partial_programs.size(); ++i) {
+    const xla::PjRtPartialProgramProto& partial_program = partial_programs[i];
+    size_t buffer_size = partial_program.ByteSizeLong();
+    char* buffer = new char[buffer_size];
+    bool success = partial_program.SerializeToArray(
+        buffer, partial_program.ByteSizeLong());
+    if (!success) {
+      return absl::InvalidArgumentError(
+          "Failed to serialize PjRtPartialProgramProto");
+    }
+    char_buffers[i] = buffer;
+    buffer_sizes[i] = buffer_size;
+  }
+  char_buffer_sizes = buffer_sizes;
+
+  std::move(cleanup_buffers).Cancel();
+  return char_buffers;
+}
+
+static PJRT_Error* PJRT_PhaseCompile_Run_Phase(
+    PJRT_PhaseCompile_Run_Phase_Args* args) {
+  PJRT_RETURN_IF_ERROR(ActualStructSizeIsGreaterOrEqual(
+      "PJRT_PhaseCompile_Run_Phase_Args",
+      PJRT_PhaseCompile_Run_Phase_Args_STRUCT_SIZE, args->struct_size));
+
+  std::vector<std::string> phases_to_run = ConvertCharBuffersToCppStrings(
+      args->phases_to_run, args->phases_to_run_sizes, args->num_phases_to_run);
+
+  PJRT_ASSIGN_OR_RETURN(
+      std::vector<xla::PjRtPartialProgramProto> programs_in_protos,
+      ConvertCharBuffersToPjRtPartialProgramProtos(args->input_programs,
+                                                   args->input_programs_sizes,
+                                                   args->num_input_programs));
+
+  PJRT_ASSIGN_OR_RETURN(
+      xla::CompileOptions options,
+      ParseCompileOptions(absl::string_view(args->compile_options,
+                                            args->compile_options_size)));
+
+  if (args->phase_compiler == nullptr) {
+    return new PJRT_Error{absl::InternalError(
+        "PJRT_PhaseCompile_Run_Phase: phase compiler is null")};
+  }
+  PJRT_ASSIGN_OR_RETURN(std::vector<xla::PjRtPartialProgramProto> programs_out,
+                        args->phase_compiler->compiler->RunPhases(
+                            options, programs_in_protos,
+                            *args->topology->topology, phases_to_run));
+
+  PJRT_ASSIGN_OR_RETURN(args->output_programs,
+                        ConvertPjRtPartialProgramProtosToCharBuffers(
+                            programs_out, args->output_programs_sizes));
+  args->num_output_programs = programs_out.size();
+
+  return nullptr;
+}
+
 static PJRT_Error* PJRT_PhaseCompile_Get_Phase_Names(
     PJRT_PhaseCompile_Get_PhaseNames_Args* args) {
   PJRT_RETURN_IF_ERROR(ActualStructSizeIsGreaterOrEqual(
       "PJRT_PhaseCompile_Get_Phase_Names_Args",
       PJRT_PhaseCompile_Get_PhaseNames_Args_STRUCT_SIZE, args->struct_size));
 
-  // Get the phase names from the compiler.
   if (args->phase_compiler == nullptr) {
     return new PJRT_Error{absl::InternalError(
         "PJRT_PhaseCompile_Get_Phase_Names: phase compiler is null")};
   }
-  PJRT_ASSIGN_OR_RETURN(absl::StatusOr<std::vector<std::string>> phase_names,
+  PJRT_ASSIGN_OR_RETURN(std::vector<std::string> phase_names,
                         args->phase_compiler->compiler->GetPhaseNames());
-  if (!phase_names.ok()) {
-    return new PJRT_Error{phase_names.status()};
-  }
 
-  // Copy the phase names to the output buffer.
   args->phase_names =
-      ConvertCppStringsToCharBuffers(*phase_names, args->phase_names_sizes);
-  args->num_phase_names = phase_names->size();
+      ConvertCppStringsToCharBuffers(phase_names, args->phase_names_sizes);
+  args->num_phase_names = phase_names.size();
 
   return nullptr;
 }
@@ -122,8 +228,9 @@ PJRT_PhaseCompile_Extension CreatePhaseCompileExtension(
       },
       /*phase_compile_get_compiler=*/get_compiler,
       /*phase_compile_destroy_compiler=*/destroy_compiler,
-      /*phase_compile_run_phase=*/nullptr,
-      /*phase_compile_get_phase_names=*/PJRT_PhaseCompile_Get_Phase_Names,
+      /*phase_compile_run_phase=*/PJRT_PhaseCompile_Run_Phase,
+      /*phase_compile_get_phase_names=*/
+      PJRT_PhaseCompile_Get_Phase_Names,
       /*phase_compile_c_buffers_destroy=*/PJRT_PhaseCompile_C_Buffers_Destroy,
   };
 }
--- third_party/xla/xla/pjrt/pjrt_phase_compile_extension_test.cc
+++ third_party/xla/xla/pjrt/pjrt_phase_compile_extension_test.cc
@@ -13,34 +13,146 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdlib>
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/log/check.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/OwningOpRef.h"
+#include "mlir/Support/LLVM.h"
+#include "stablehlo/reference/Api.h"
 #include "xla/pjrt/c/pjrt_c_api.h"
+#include "xla/pjrt/c/pjrt_c_api_cpu_internal.h"
+#include "xla/pjrt/c/pjrt_c_api_helpers.h"
 #include "xla/pjrt/c/pjrt_c_api_phase_compile_extension.h"
 #include "xla/pjrt/c/pjrt_c_api_phase_compile_internal.h"
+#include "xla/pjrt/pjrt_api.h"
+#include "xla/pjrt/pjrt_c_api_client.h"
+#include "xla/pjrt/pjrt_compiler.h"
+#include "xla/pjrt/pjrt_executable.h"
+#include "xla/pjrt/pjrt_phase_compile.h"
 #include "xla/pjrt/pjrt_phase_compile_sample_plugin.h"
+#include "xla/pjrt/proto/pjrt_partial_program.pb.h"
+#include "xla/tsl/lib/core/status_test_util.h"
+#include "xla/tsl/platform/status_matchers.h"
 
 namespace pjrt {
 namespace {
 
-// Test that the phase compile extension has all the callbacks deliberately set
+using ::testing::ElementsAre;
+using ::testing::HasSubstr;
+using ::tsl::testing::IsOkAndHolds;
+using ::tsl::testing::StatusIs;
+
+constexpr absl::string_view kStablehloModuleStr = R"(
+  module {
+    func.func @main(%arg0: tensor<4xi32>) -> tensor<4xi32> {
+      %0 = stablehlo.constant dense<0> : tensor<4xi32>
+      %1 = stablehlo.add %arg0, %0 : tensor<4xi32>
+      func.return %1 : tensor<4xi32>
+    }
+  }
+  )";
+
+constexpr absl::string_view kPhaseName = "stablehlo_to_optimized_stablehlo";
+
+std::vector<xla::PjRtPartialProgramProto> PrepareInputPartialPrograms(
+    const std::string& next_phase, size_t program_format) {
+  std::string program_code{kStablehloModuleStr};
+
+  mlir::MLIRContext context;
+  mlir::FailureOr<mlir::OwningOpRef<mlir::ModuleOp>> stablehlo_module =
+      mlir::stablehlo::parseStablehloModule(program_code, context);
+  CHECK(mlir::succeeded(stablehlo_module));
+
+  auto bytecode_status =
+      pjrt::phase_compile_sample_plugin::StablehloTypeSerialization::Serialize(
+          *stablehlo_module);
+  CHECK_OK(bytecode_status);
+
+  xla::PjRtPartialProgramProto partial_program;
+  partial_program.set_program(*bytecode_status);
+  partial_program.set_program_format(program_format);
+  partial_program.set_generating_phase("n/a");
+  partial_program.add_next_phases({next_phase});
+  partial_program.set_version("1.0");
+
+  return {partial_program};
+}
+
+class PhaseCompileExtensionTest : public ::testing::Test {
+ protected:
+  std::unique_ptr<pjrt::CApiPjrtPhaseCompiler> phase_compile_extension_wrapper_;
+  const xla::PjRtTopologyDescription* topology_description_;
+  std::unique_ptr<xla::PjRtClient> client_;
+
+  PhaseCompileExtensionTest() {
+    // For the CPU plugin, a few callbacks for the phase compile extension are
+    // set to null. So we need to set them with the ones from the sample plugin
+    // for testing.
+
+    // During the first test case, the registration won't have occurred yet, so
+    // we expect a failure and need to register explicitly.
+    auto api = PjrtApi("cpu");
+    if (!api.ok()) {
+      CHECK_OK(pjrt::SetPjrtApi("cpu", cpu_plugin::GetCpuPjrtApi()));
+    }
+    api = PjrtApi("cpu");
+    CHECK_OK(api);
+    auto phase_compile_extension =
+        pjrt::FindExtension<PJRT_PhaseCompile_Extension>(
+            *api, PJRT_Extension_Type::PJRT_Extension_Type_PhaseCompile);
+    PJRT_PhaseCompile_Extension sample_plugin_phase_compile_extension =
+        pjrt::phase_compile_sample_plugin::CreateSamplePhaseCompileExtension();
+    phase_compile_extension->phase_compile_get_compiler =
+        sample_plugin_phase_compile_extension.phase_compile_get_compiler;
+    phase_compile_extension->phase_compile_destroy_compiler =
+        sample_plugin_phase_compile_extension.phase_compile_destroy_compiler;
+
+    // Get the phase compile extension wrapper.
+    absl::StatusOr<std::unique_ptr<pjrt::CApiPjrtPhaseCompiler>>
+        phase_compile_extension_wrapper_or_status =
+            pjrt::GetCApiPjrtPhaseCompiler(*api);
+    CHECK_OK(phase_compile_extension_wrapper_or_status);
+    phase_compile_extension_wrapper_ =
+        std::move(*phase_compile_extension_wrapper_or_status);
+
+    // Create a topology description.
+    auto client_or_status = xla::GetCApiClient("cpu");
+    CHECK_OK(client_or_status);
+    client_ = std::move(*client_or_status);
+    auto topology_or_status = client_->GetTopologyDescription();
+    CHECK_OK(topology_or_status);
+    topology_description_ = *topology_or_status;
+  }
+};
+
+// Test that the phase compile extension has none of the callbacks set
 // to null.
-TEST(PhaseCompileExtensionTest, TestExtensionRegistration) {
+TEST(BasicPhaseCompileExtensionTest, TestExtensionRegistration) {
   // Create a phase compile extension.
   PJRT_PhaseCompile_Extension phase_compile_extension =
       pjrt::phase_compile_sample_plugin::CreateSamplePhaseCompileExtension();
 
   EXPECT_NE(phase_compile_extension.phase_compile_get_compiler, nullptr);
   EXPECT_NE(phase_compile_extension.phase_compile_destroy_compiler, nullptr);
+  EXPECT_NE(phase_compile_extension.phase_compile_run_phases, nullptr);
   EXPECT_NE(phase_compile_extension.phase_compile_get_phase_names, nullptr);
-  EXPECT_EQ(phase_compile_extension.phase_compile_run_phases, nullptr);
+  EXPECT_NE(phase_compile_extension.phase_compile_c_buffers_destroy, nullptr);
 }
 
 // Test the correct usage of the (1) GetCompiler, (2) DestroyCompiler, and (3)
 // CBuffersDestroy APIs.
-TEST(PhaseCompileExtensionTest,
+TEST(BasicPhaseCompileExtensionTest,
      TestPhaseCompileExtensionForGetCompilerDestroyCompilerAndCBuffersDestroy) {
   // Create a phase compile extension.
   PJRT_PhaseCompile_Extension phase_compile_extension =
@@ -63,7 +175,6 @@ TEST(PhaseCompileExtensionTest,
   error = phase_compile_extension.phase_compile_get_phase_names(
       &get_phase_names_args);
   ASSERT_EQ(error, nullptr);
-
   // Convert the C-style phase names to C++ strings.
   std::vector<std::string> converted_strings =
       pjrt::ConvertCharBuffersToCppStrings(
@@ -92,6 +203,77 @@ TEST(PhaseCompileExtensionTest,
   destroy_compiler_args.phase_compiler = get_compiler_args.phase_compiler;
   phase_compile_extension.phase_compile_destroy_compiler(
       &destroy_compiler_args);
+}
+
+// Test the correct usage of the RunPhases API.
+TEST_F(PhaseCompileExtensionTest, RunPhases) {
+  // Prepare the input programs.
+  auto partial_programs_in = PrepareInputPartialPrograms(
+      /*next_phase=*/std::string(kPhaseName),
+      /*program_format=*/0);  // 0 expresses StableHLO bytecode per the
+                              // sample plugin used in this test.
+
+  // Run the partial compile phase.
+  std::vector<std::string> phases_to_run = {std::string(kPhaseName)};
+  auto partial_programs_out = phase_compile_extension_wrapper_->RunPhases(
+      xla::CompileOptions(), partial_programs_in, *topology_description_,
+      phases_to_run);
+  TF_ASSERT_OK(partial_programs_out);
+
+  // Verify that the output programs are deserializable.
+  for (auto& partial_program : *partial_programs_out) {
+    mlir::MLIRContext context;
+    auto deserialized_module =
+        phase_compile_sample_plugin::StablehloTypeSerialization::Deserialize(
+            partial_program.program(), context);
+    TF_EXPECT_OK(deserialized_module);
+  }
+}
+
+// Plugin-agnostic validation: Test the RunPhases API with an empty phases to
+// run.
+TEST_F(PhaseCompileExtensionTest,
+       PluginAgnosticValidationWithEmptyPhasesToRun) {
+  // Prepare the input programs.
+  auto partial_programs_in =
+      PrepareInputPartialPrograms(/*next_phase=*/std::string(kPhaseName),
+                                  /*program_format=*/0);
+
+  // Run the partial compile phase.
+  std::vector<std::string> phases_to_run = {};
+  auto partial_programs_out = phase_compile_extension_wrapper_->RunPhases(
+      xla::CompileOptions(), partial_programs_in, *topology_description_,
+      phases_to_run);
+  EXPECT_THAT(partial_programs_out,
+              StatusIs(absl::StatusCode::kInvalidArgument,
+                       HasSubstr("Phases to run cannot be empty")));
+}
+
+// Plugin-agnostic validation: Test the RunPhases API with an incompatible
+// phase: one that is not expected to be run on the input programs.
+TEST_F(PhaseCompileExtensionTest,
+       PluginAgnosticValidationWithIncompatiblePhase) {
+  // Prepare the input programs.
+  auto partial_programs_in =
+      PrepareInputPartialPrograms(/*next_phase=*/std::string(kPhaseName),
+                                  /*program_format=*/0);
+
+  // Run the partial compile phase.
+  std::vector<std::string> phases_to_run = {"IllegalPhaseName"};
+  auto partial_programs_out = phase_compile_extension_wrapper_->RunPhases(
+      xla::CompileOptions(), partial_programs_in, *topology_description_,
+      phases_to_run);
+  EXPECT_THAT(
+      partial_programs_out,
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("Input partial programs cannot be compiled by a phase "
+                         "with name")));
+}
+
+// Test the correct usage of the GetPhaseNames API.
+TEST_F(PhaseCompileExtensionTest, TestPhaseCompileExtensionForGetPhaseNames) {
+  auto phase_names_status = phase_compile_extension_wrapper_->GetPhaseNames();
+  EXPECT_THAT(phase_names_status, IsOkAndHolds(ElementsAre(kPhaseName)));
 }
 
 }  // namespace
