87f47206ad36c8151507040203d98d067a7a7af8
Qingwei Zhang <qzhang.canada@gmail.com>
2025-07-12 15:54:16 -0400

    [Cleanup] Remove non-English comment to maintain consistency with TensorFlow style guide

--- tensorflow/dtensor/cc/small_constant_optimization_test.cc
+++ tensorflow/dtensor/cc/small_constant_optimization_test.cc
@@ -1,6 +1,6 @@
 #include "tensorflow/dtensor/cc/small_constant_optimization.h"
 
-#include "tensorflow/core/framework/node_def.pb.h"  // 只用 proto
+#include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/dtensor/cc/tensor_layout.h"
 
@@ -9,16 +9,13 @@ namespace dtensor {
 namespace {
 
 TEST(SmallConstantOptimization, NullTensorReturnsNullopt) {
-  // Arrange
   Layout layout = Layout::Empty();
   TF_Status* tf_status = TF_NewStatus();
 
-  // Act
   std::optional<NodeDef> result = ExtractSmallTensorValue(
       /*context=*/nullptr,
       /*tensor=*/nullptr, layout, tf_status);
 
-  // Assert
   EXPECT_FALSE(result.has_value());
   EXPECT_EQ(TF_GetCode(tf_status), TF_INVALID_ARGUMENT);
 
