7c4b534257df364b1334e7f4cb80d83f61d2c9ec
Harshith_N <harshithn31@gmail.com>
2025-07-14 20:42:04 +0200

    revert

--- tensorflow/lite/delegates/utils/mw_delegates/fully_connected_delegate/fully_connected_drivers/fully_connected_bram_driver.cc
+++ tensorflow/lite/delegates/utils/mw_delegates/fully_connected_delegate/fully_connected_drivers/fully_connected_bram_driver.cc
@@ -204,7 +204,7 @@ int FpgaBramDriver::read_output_from_bram(float* output, const int size) {
         return -1;
     }
     //TODO: REMOVE THIS LINE
-    output = bram_address["input_bram"];
+    output = read_from_bram("intput_bram");
     ////////////////////////////////////
     float* bram_output = read_from_bram("output_bram");
     std::cout << "Entering If : going to memcpy" << std::endl;
