89aa527dad3a4e4a8fc34b76400e09fdf2d76d88
Benjamin Kramer <kramerb@google.com>
2025-07-15 05:45:16 -0700

    Remove workarounds for missing ABSL_DEPRECATE_AND_INLINE
    TF OSS absl is now new enough to provide it unconditionally.
    
    PiperOrigin-RevId: 783297380

--- tensorflow/core/platform/stringpiece.h
+++ tensorflow/core/platform/stringpiece.h
@@ -29,11 +29,6 @@ limitations under the License.
 #include "absl/base/macros.h"
 #include "tsl/platform/stringpiece.h"  // IWYU pragma: export
 
-// TODO: b/323943471 - This macro should eventually be provided by Abseil.
-#ifndef ABSL_DEPRECATE_AND_INLINE
-#define ABSL_DEPRECATE_AND_INLINE()
-#endif
-
 namespace tensorflow {
 
 using StringPiece ABSL_DEPRECATE_AND_INLINE() = absl::string_view;
