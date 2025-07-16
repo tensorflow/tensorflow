6f308439dd6e59f8126952176c4d7cff07e834d4
Will Froom <willfroom@google.com>
2025-07-16 07:55:30 -0700

    [XLA:GPU]  Move IsIntermediate & FindHero to shared ir_emission_utils.
    PiperOrigin-RevId: 783757129

--- third_party/xla/xla/codegen/ir_emission_utils.h
+++ third_party/xla/xla/codegen/ir_emission_utils.h
@@ -1,3 +1,7 @@
+#include <optional>
+
+#include "absl/functional/any_invocable.h"
+#include "xla/hlo/utils/hlo_traversal.h"
 /* Copyright 2025 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -63,6 +67,15 @@ struct TransposeDescription {
                GetBitwidth(other.instr->shape().element_type());
   }
 };
+
+// Checks if the instruction is elementwise.
+bool IsIntermediate(const HloInstruction* instr, int allowed_operand_count = 1);
+
+// Find the first gero that statises the given predicate.
+std::optional<HloInstructionAdaptor> FindHero(
+    const HloInstructionAdaptor& root,
+    absl::AnyInvocable<bool(const HloInstruction&)> predicate);
+
 }  // namespace xla
 
 #endif  // XLA_CODEGEN_IR_EMISSION_UTILS_H_
