236ca9962f2c5427c885799d5249bef7d85dfce9
Hyeontaek Lim <hyeontaek@google.com>
2025-07-17 15:15:05 -0700

    [IFRT] Add `UserContextScope`
    `xla::ifrt::UserContextScope` provides tracking of the currently active
    `xla::ifrt::UserContext` on the current thread. It gives a mechanism for IFRT
    APIs to take a user-provided context and associate it with IFRT runtime objects
    (`Array`, `LoadedExecutable`, etc.).
    
    We begin the changes by first using this thread-local scoping mechanism, as
    this allows more incremental steps. A helper function
    `xla::ifrt::GetUserContext()` provides a way to get the current
    `xla::ifrt::UserContext` in a uniform way across IFRT implementations.
    
    The long-term plan remains to be making the propagation of context objects
    explicit by making IFRT APIs to take a `user_context` argument. This
    thread-local scoping mechanism can be transferred from the IFRT API to IFRT
    users or runtimes in case they still prefer using this mechanism.
    
    PiperOrigin-RevId: 784333019

--- third_party/xla/xla/python/ifrt/client_impl_util.h
+++ third_party/xla/xla/python/ifrt/client_impl_util.h
@@ -28,6 +28,18 @@ limitations under the License.
 namespace xla {
 namespace ifrt {
 
+// Convenient helper to choose the current `UserContextRef` in a uniform way.
+//
+// * If `per_call_user_context != nullptr`, `per_call_user_context` is returned.
+// * If `UserContextScope::current() != nullptr`, `UserContextScope::current()`
+//   is returned.
+// * Otherwise, `client->CreateUserContext()` is returned.
+//
+// TODO(hyeontaek): Remove this helper and use `UserContextScope::current()`
+// when there is no plural way of getting the current `UserContextRef`.
+UserContextRef GetUserContext(
+    Client* client, UserContextRef per_call_user_context = UserContextRef());
+
 // Portable adapter for `MakeArraysFromHostBufferShards`. It breaks downs
 // requests into `MakeArrayFromHostBuffer` calls followed by
 // `AssembleArrayFromSingleDeviceArrays`.
