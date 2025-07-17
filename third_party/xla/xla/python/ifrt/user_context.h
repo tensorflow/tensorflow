c4e5b80b690d82c5253a8cc917e3ba05e87fac49
Hyeontaek Lim <hyeontaek@google.com>
2025-07-17 15:15:05 -0700

    [IFRT] Add `UserContextScope`
    `xla::ifrt::UserContextScope` provides tracking of the currently active
    `xla::ifrt::UserContext` on the current thread. It gives a mechanism for IFRT
    APIs to take a user-provided context and associate it with IFRT runtime objects
    (`Array`, `LoadedExecutable`, etc.).
    
    We begin the changes by first using this thread-local scoping mechanism, as
    this allows more incremental steps. A helper function
    `xla::ifrt::GetUserContext()` provides a way to get the current
    `xla::ifrt::UserContext` in a uniform way across IFRT implementations.
    
    The long-term plan remains to be making the propagation of context objects
    explicit by making IFRT APIs to take a `user_context` argument. This
    thread-local scoping mechanism can be transferred from the IFRT API to IFRT
    users or runtimes in case they still prefer using this mechanism.
    
    PiperOrigin-RevId: 784333019

--- third_party/xla/xla/python/ifrt/user_context.h
+++ third_party/xla/xla/python/ifrt/user_context.h
@@ -33,24 +33,71 @@ namespace ifrt {
 class UserContext : public tsl::ReferenceCounted<UserContext>,
                     public llvm::RTTIExtends<UserContext, llvm::RTTIRoot> {
  public:
-  static tsl::RCReference<UserContext> Default();
-
   ~UserContext() override = default;
 
-  // Returns a fingerprint of the UserContext. The returned fingerprint is must
+  // Returns a fingerprint of the UserContext. The returned fingerprint must
   // be non-zero, as the special value of zero is reserved for the IFRT
   // implementations for their internal default UserContext.  IFRT
   // implementations may use internally. IFRT implementations
   // may also use this as a key for holding the UserContexts in a container, and
   // so this should be efficient enough to called multiple times.
+  //
+  // TODO(hyeontaek): Remove this method once we migrate the UserContext to
+  // a unique id scheme, where the id can be chosen without fingerprinting the
+  // content of the UserContext.
   virtual uint64_t Fingerprint() const = 0;
 
   // Returns a human readable string. Meant for debugging, logging, and for
   // putting together statusz-like pages.
+  //
+  // Caution: A call to this method is often expensive, and may accompany a
+  // certain precondition. For instance, this method might internally acquire a
+  // mutex lock that is visible to the external world (e.g., Python GIL), and
+  // the caller must ensure that doing so would not cause a deadlock (e.g., no
+  // one who is holding such a mutex lock never blocks on a call to this
+  // method).
   virtual std::string DebugString() const = 0;
 
   // For llvm::RTTI
   static char ID;  // NOLINT
+};
+
+using UserContextRef = tsl::RCReference<UserContext>;
+
+// Tracks the active `UserContext` within the scope. It holds a pointer to the
+// `UserContext` instance and uses a thread-local variable to make it
+// discoverable through a static method.
+class UserContextScope {
+ public:
+  // Sets up the current thread's `UserContextRef` to the given `context`.
+  // `context` must be valid throughout the lifetime of the scope.
+  explicit UserContextScope(UserContextRef context);
+
+  // Restores the current thread's `UserContextRef` to the state before this
+  // scope was created.
+  ~UserContextScope();
+
+  // Not copyable or moveable. The current scope's UserContextScope will be
+  // referenced by a thread-local raw pointer.
+  UserContextScope(const UserContextScope&) = delete;
+  UserContextScope(UserContextScope&&) = delete;
+  UserContextScope& operator=(const UserContextScope&) = delete;
+  UserContextScope& operator=(UserContextScope&&) = delete;
+
+  // Returns the active `UserContextRef`. The returned reference is stable only
+  // during the lifetime of the current scope. If the `UserContextRef` should be
+  // used outside the current scope, it must be copied as a new
+  // `UserContextRef`.
+  //
+  // Returns `nullptr` if there is no `UserContextRef` in the scope.
+  static const UserContextRef& current();
+
+ private:
+  // The outer scope's `UserContext`. When this scope is destroyed, the current
+  // scope's `UserContext` will be restored to it.
+  const UserContextRef* const outer_context_;  // Not owned.
+  // The current scope's `UserContext`.
+  const UserContextRef context_;
 };
 
 }  // namespace ifrt
