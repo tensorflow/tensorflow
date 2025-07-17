bea0a4fe8077cb0da0e067bdc74a9e89798d71b3
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-17 12:24:04 -0700

    Internal change only
    PiperOrigin-RevId: 784271831

--- tensorflow/python/saved_model/builder_impl.py
+++ tensorflow/python/saved_model/builder_impl.py
@@ -37,7 +37,7 @@ from tensorflow.python.training import saver as tf_saver
 from tensorflow.python.util import compat
 from tensorflow.python.util.deprecation import deprecated_args
 from tensorflow.python.util.tf_export import tf_export
-# Placeholder for protosplitter import.  # copybara:comment
+# copybara:uncomment # Placeholder for protosplitter import.
 # API label for SavedModel metrics.
 _SAVE_BUILDER_LABEL = "save_v1_builder"
 
