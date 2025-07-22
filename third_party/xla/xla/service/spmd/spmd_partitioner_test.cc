1d2d17c095de22391cfc01690a14519c596fc183
Zixuan Jiang <zixuanjiang@google.com>
2025-07-22 12:06:52 -0700

    Add reshard cache when the sharding of reshape is tile maximal (replicated or maximal).
    In the test case, the added reshard cache can avoid the reshard (dynamic-slice) on reshape.
    
    PiperOrigin-RevId: 785947225

--- third_party/xla/xla/service/spmd/spmd_partitioner_test.cc
+++ third_party/xla/xla/service/spmd/spmd_partitioner_test.cc
@@ -4087,8 +4087,6 @@ ENTRY %reshape {
               op::Tuple(reshape_1, abs));
 }
 
-// TODO(b/397731516). We can add a reshard cache for the reshape such that we
-// can avoid the reshard (dynamic-slice) on reshape.
 TEST_P(SpmdPartitioningTest, ReshapeWithSpecialCache2) {
   absl::string_view hlo_string = R"(
 HloModule module
@@ -4107,12 +4105,11 @@ ENTRY %reshape {
   auto param_replicated = AllOf(
       op::Shape("bf16[8,8]"), op::AllReduce(op::DynamicUpdateSlice(
                                   op::Broadcast(op::Constant()), param, _, _)));
-  auto reshape = AllOf(op::Shape("bf16[64]"), op::Reshape(param_replicated));
-  auto reshape_resharded =
-      AllOf(op::Shape("bf16[16]"), op::DynamicSlice(reshape, _));
-  auto abs = AllOf(op::Shape("bf16[16]"), op::Abs(reshape_resharded));
+  auto reshape_1 = AllOf(op::Shape("bf16[64]"), op::Reshape(param_replicated));
+  auto reshape_2 = AllOf(op::Shape("bf16[16]"), op::Reshape(param));
+  auto abs = AllOf(op::Shape("bf16[16]"), op::Abs(reshape_2));
   EXPECT_THAT(module->entry_computation()->root_instruction(),
-              op::Tuple(reshape, abs));
+              op::Tuple(reshape_1, abs));
 }
 
 TEST_P(SpmdPartitioningTest, PartialReplicateShardableReshape) {
