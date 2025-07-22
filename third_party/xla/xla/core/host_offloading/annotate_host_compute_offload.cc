51f55eb59ceae3a334ee62265494c1629b524f87
Karlo Basioli <basioli@google.com>
2025-07-22 09:56:50 -0700

    [XLA][host offloading] Open source annotate host compute offload.
    PiperOrigin-RevId: 785893444

---
+++ third_party/xla/xla/core/host_offloading/annotate_host_compute_offload.cc
@@ -0,0 +1,81 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/core/host_offloading/annotate_host_compute_offload.h"
+
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "xla/hlo/ir/hlo_computation.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_module_group.h"
+#include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/service/host_offload_utils.h"
+#include "xla/side_effect_util.h"
+#include "xla/xla_data.pb.h"
+#include "tsl/platform/statusor.h"
+
+namespace xla {
+namespace {
+void SetHostComputeFrontendAttribute(HloInstruction& host_instruction) {
+  FrontendAttributes frontend_attributes =
+      host_instruction.frontend_attributes();
+  frontend_attributes.mutable_map()->insert(
+      {kXlaComputeTypeAttr, kXlaComputeTypeHost});
+  host_instruction.set_frontend_attributes(frontend_attributes);
+}
+
+void AnnotateComputationHostOffload(HloComputation& computation) {
+  for (HloInstruction* host_instruction : computation.instructions()) {
+    if (host_instruction->opcode() == HloOpcode::kParameter) {
+      continue;
+    }
+    SetHostComputeFrontendAttribute(*host_instruction);
+    for (HloComputation* called_computation :
+         host_instruction->called_computations()) {
+      AnnotateComputationHostOffload(*called_computation);
+    }
+  }
+}
+}  // namespace
+
+absl::StatusOr<bool> AnnotateHostComputeOffload::RunOnModuleGroup(
+    HloModuleGroup* module_group,
+    const absl::flat_hash_set<absl::string_view>& execution_threads) {
+  bool modified = false;
+  for (HloModule* module : module_group->modules()) {
+    TF_ASSIGN_OR_RETURN(bool m, Run(module, execution_threads));
+    modified |= m;
+  }
+  return modified;
+}
+
+absl::StatusOr<bool> AnnotateHostComputeOffload::Run(
+    HloModule* module,
+    const absl::flat_hash_set<absl::string_view>& execution_threads) {
+  bool modified = false;
+  for (HloComputation* computation : module->MakeNonfusionComputations()) {
+    for (HloInstruction* instruction : computation->instructions()) {
+      if (instruction->opcode() == HloOpcode::kCall &&
+          host_offload_utils::ComputeTypeIsHost(instruction)) {
+        AnnotateComputationHostOffload(*instruction->to_apply());
+        modified = true;
+      }
+    }
+  }
+  return modified;
+}
+
+}  // namespace xla
