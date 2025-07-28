openxla/xla

#sdy Mark `xla.sdy.LocalToGlobalShape` custom call as side effecting so it isn't removed if unused.
We also change `zero_sized_hlo_elimination` to eliminate `xla.sdy.LocalToGlobalShape` if it zero sized, so it's replaced with constants. This means `sdy-round-trip-shard-map-import` can see a manual computation call op with no uses, which we handle by erasing the op and called function.

PiperOrigin-RevId: 783829783

xla/service/spmd/shardy/sdy_round_trip/shard_map_import.cc
@@ -81,6 +81,26 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       return mlir::failure();
     }
 
+    auto shmapBodyFunc = symbolTable.lookup<FuncOp>(callOp.getCallee());
+    if (shmapBodyFunc.empty()) {
+      return callOp->emitOpError(
+          "expected a unique FuncOp per "
+          "@xla.sdy.manual_computation_body call. Were "
+          "functions maybe somehow shared/de-duped between "
+          "two ManualComputations?");
+    }
+
+    // If the callOp has no uses, but has at least one result, then it means
+    // all its results have a dimension of size 0 (i.e. 0 num-elements), and
+    // therefore they were replaced with constants of the same shape. In which
+    // case, we can safely erase the callOp and the manual computation body
+    // function.
+    if (callOp.use_empty() && !callOp->getResults().empty()) {
+      rewriter.eraseOp(callOp);
+      rewriter.eraseOp(shmapBodyFunc);
+      return mlir::success();
+    }
+
     // NOTE: if the original `ManualComputationOp` had no operands (results),
     // then a @GlobalToLocalShape (@LocalToGlobalShape) custom call won't be
     // present. So we have to take the operands/results of the newly created
@@ -114,10 +134,8 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       // dimension of size 0, in which case, the corresponding result of
       // `@xla.sdy.manual_computation_body` call would be replaced with a
       // constant. Therefore, we check the first use rather than first result.
-      if (!callOp->use_empty()) {
-        localToGlobalShape =
-            mlir::dyn_cast<CustomCallOp>(*callOp->user_begin());
-      }
+      CHECK(!callOp->use_empty());
+      localToGlobalShape = mlir::dyn_cast<CustomCallOp>(*callOp->user_begin());
       if (!localToGlobalShape) {
         return callOp->emitOpError("expected the first use of ")
                << callOp.getCalleeAttr() << " to be by a "
@@ -126,15 +144,6 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       CHECK(localToGlobalShape.getCallTargetName() ==
             kLocalToGlobalShapeCallTargetName);
       resultTypes = localToGlobalShape->getResultTypes();
-    }
-
-    auto shmapBodyFunc = symbolTable.lookup<FuncOp>(callOp.getCallee());
-    if (shmapBodyFunc.empty()) {
-      return callOp->emitOpError(
-          "expected a unique FuncOp per "
-          "@xla.sdy.manual_computation_body call. Were "
-          "functions maybe somehow shared/de-duped between "
-          "two ManualComputations?");
     }
 
     MLIRContext* context = rewriter.getContext();

xla/service/spmd/shardy/test/sdy_round_trip_shard_map_import.mlir
@@ -181,8 +181,8 @@ func.func @manual_computation_no_inputs_no_outputs() {
   return
 }
 
-// CHECK-LABEL: func @manual_computation_zero_dim_inputs_outputs
-func.func @manual_computation_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %arg1: tensor<16x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>) {
+// CHECK-LABEL: func @manual_computation_some_zero_dim_inputs_outputs
+func.func @manual_computation_some_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %arg1: tensor<16x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>) {
   // CHECK-NOT: call @xla.sdy.manual_computation_body
   // CHECK:               %[[CONST_0_32:.*]] = stablehlo.constant dense<0.000000e+00> : tensor<0x32xf32>
   // CHECK:               %[[MAN_COMP:.*]]:2 = sdy.manual_computation(%arg0, %arg1)
@@ -200,6 +200,18 @@ func.func @manual_computation_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %
   %1:2 = call @xla.sdy.manual_computation_body_9(%c1, %0#1) : (tensor<0x8xf32>, tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<8x32xf32>)
   %2:2 = stablehlo.custom_call @xla.sdy.LocalToGlobalShape(%c2, %1#1) {mhlo.frontend_attributes = {xla.sdy.manual_axes = "#sdy<manual_axes{\22b\22}>", xla.sdy.out_shardings = "#sdy.sharding_per_value<[<@mesh_0, [{}, {}], replicated={\22b\22}>, <@mesh_0, [{\22b\22}, {}]>]>"}} : (tensor<0x32xf32>, tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>)
   return %c2, %2#1 : tensor<0x32xf32>, tensor<16x32xf32>
+}
+
+// CHECK-LABEL: func @manual_computation_all_outputs_zero_dim
+func.func @manual_computation_all_outputs_zero_dim(%arg0: tensor<0x16xf32>, %arg1: tensor<16x0xf32>) -> tensor<0x0xf32> {
+  // CHECK:       %[[CONST_0_0:.*]] = stablehlo.constant dense<0.000000e+00> : tensor<0x0xf32>
+  // CHECK-NOT: call @xla.sdy.manual_computation_body
+  // CHECK-NEXT:  return %[[CONST_0_0]]
+  %c1 = stablehlo.constant dense<0.000000e+00> : tensor<0x8xf32>
+  %c2 = stablehlo.constant dense<0.000000e+00> : tensor<8x0xf32>
+  %c3 = stablehlo.constant dense<0.000000e+00> : tensor<0x0xf32>
+  %0:2 = call @xla.sdy.manual_computation_body_10(%c1, %c2) : (tensor<0x8xf32>, tensor<8x0xf32>) -> (tensor<0x0xf32>, tensor<8x0xf32>)
+  return %c3 : tensor<0x0xf32>
 }
 
 // CHECK-LABEL: func @stray_unused_manual_computation_custom_calls
@@ -279,4 +291,10 @@ func.func @xla.sdy.manual_computation_body_8() {
 func.func @xla.sdy.manual_computation_body_9(%arg0: tensor<0x8xf32>, %arg1: tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<8x32xf32>) {
   %0 = stablehlo.dot %arg0, %arg1 : (tensor<0x8xf32>, tensor<8x32xf32>) -> tensor<0x32xf32>
   return %0, %arg1 : tensor<0x32xf32>, tensor<8x32xf32>
+}
+
+// CHECK-NOT: func @xla.sdy.manual_computation_body_10(
+func.func @xla.sdy.manual_computation_body_10(%arg0: tensor<0x8xf32>, %arg1: tensor<8x0xf32>) -> (tensor<0x0xf32>, tensor<8x0xf32>) {
+  %0 = stablehlo.dot %arg0, %arg1 : (tensor<0x8xf32>, tensor<8x0xf32>) -> tensor<0x0xf32>
+  return %0, %arg1 : tensor<0x0xf32>, tensor<8x0xf32>
 }

tensorflow/tensorflow

third_party/xla/xla/service/spmd/shardy/sdy_round_trip/shard_map_import.cc
@@ -81,6 +81,26 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       return mlir::failure();
     }
 
+    auto shmapBodyFunc = symbolTable.lookup<FuncOp>(callOp.getCallee());
+    if (shmapBodyFunc.empty()) {
+      return callOp->emitOpError(
+          "expected a unique FuncOp per "
+          "@local_xla.sdy.manual_computation_body call. Were "
+          "functions maybe somehow shared/de-duped between "
+          "two ManualComputations?");
+    }
+
+    // If the callOp has no uses, but has at least one result, then it means
+    // all its results have a dimension of size 0 (i.e. 0 num-elements), and
+    // therefore they were replaced with constants of the same shape. In which
+    // case, we can safely erase the callOp and the manual computation body
+    // function.
+    if (callOp.use_empty() && !callOp->getResults().empty()) {
+      rewriter.eraseOp(callOp);
+      rewriter.eraseOp(shmapBodyFunc);
+      return mlir::success();
+    }
+
     // NOTE: if the original `ManualComputationOp` had no operands (results),
     // then a @GlobalToLocalShape (@LocalToGlobalShape) custom call won't be
     // present. So we have to take the operands/results of the newly created
@@ -114,10 +134,8 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       // dimension of size 0, in which case, the corresponding result of
       // `@local_xla.sdy.manual_computation_body` call would be replaced with a
       // constant. Therefore, we check the first use rather than first result.
-      if (!callOp->use_empty()) {
-        localToGlobalShape =
-            mlir::dyn_cast<CustomCallOp>(*callOp->user_begin());
-      }
+      CHECK(!callOp->use_empty());
+      localToGlobalShape = mlir::dyn_cast<CustomCallOp>(*callOp->user_begin());
       if (!localToGlobalShape) {
         return callOp->emitOpError("expected the first use of ")
                << callOp.getCalleeAttr() << " to be by a "
@@ -126,15 +144,6 @@ class ManualComputationPattern : public OpConversionPattern<CallOp> {
       CHECK(localToGlobalShape.getCallTargetName() ==
             kLocalToGlobalShapeCallTargetName);
       resultTypes = localToGlobalShape->getResultTypes();
-    }
-
-    auto shmapBodyFunc = symbolTable.lookup<FuncOp>(callOp.getCallee());
-    if (shmapBodyFunc.empty()) {
-      return callOp->emitOpError(
-          "expected a unique FuncOp per "
-          "@local_xla.sdy.manual_computation_body call. Were "
-          "functions maybe somehow shared/de-duped between "
-          "two ManualComputations?");
     }
 
     MLIRContext* context = rewriter.getContext();

third_party/xla/xla/service/spmd/shardy/test/sdy_round_trip_shard_map_import.mlir
@@ -181,8 +181,8 @@ func.func @manual_computation_no_inputs_no_outputs() {
   return
 }
 
-// CHECK-LABEL: func @manual_computation_zero_dim_inputs_outputs
-func.func @manual_computation_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %arg1: tensor<16x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>) {
+// CHECK-LABEL: func @manual_computation_some_zero_dim_inputs_outputs
+func.func @manual_computation_some_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %arg1: tensor<16x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>) {
   // CHECK-NOT: call @local_xla.sdy.manual_computation_body
   // CHECK:               %[[CONST_0_32:.*]] = stablehlo.constant dense<0.000000e+00> : tensor<0x32xf32>
   // CHECK:               %[[MAN_COMP:.*]]:2 = sdy.manual_computation(%arg0, %arg1)
@@ -200,6 +200,18 @@ func.func @manual_computation_zero_dim_inputs_outputs(%arg0: tensor<0x16xf32>, %
   %1:2 = call @local_xla.sdy.manual_computation_body_9(%c1, %0#1) : (tensor<0x8xf32>, tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<8x32xf32>)
   %2:2 = stablehlo.custom_call @local_xla.sdy.LocalToGlobalShape(%c2, %1#1) {mhlo.frontend_attributes = {xla.sdy.manual_axes = "#sdy<manual_axes{\22b\22}>", xla.sdy.out_shardings = "#sdy.sharding_per_value<[<@mesh_0, [{}, {}], replicated={\22b\22}>, <@mesh_0, [{\22b\22}, {}]>]>"}} : (tensor<0x32xf32>, tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<16x32xf32>)
   return %c2, %2#1 : tensor<0x32xf32>, tensor<16x32xf32>
+}
+
+// CHECK-LABEL: func @manual_computation_all_outputs_zero_dim
+func.func @manual_computation_all_outputs_zero_dim(%arg0: tensor<0x16xf32>, %arg1: tensor<16x0xf32>) -> tensor<0x0xf32> {
+  // CHECK:       %[[CONST_0_0:.*]] = stablehlo.constant dense<0.000000e+00> : tensor<0x0xf32>
+  // CHECK-NOT: call @local_xla.sdy.manual_computation_body
+  // CHECK-NEXT:  return %[[CONST_0_0]]
+  %c1 = stablehlo.constant dense<0.000000e+00> : tensor<0x8xf32>
+  %c2 = stablehlo.constant dense<0.000000e+00> : tensor<8x0xf32>
+  %c3 = stablehlo.constant dense<0.000000e+00> : tensor<0x0xf32>
+  %0:2 = call @local_xla.sdy.manual_computation_body_10(%c1, %c2) : (tensor<0x8xf32>, tensor<8x0xf32>) -> (tensor<0x0xf32>, tensor<8x0xf32>)
+  return %c3 : tensor<0x0xf32>
 }
 
 // CHECK-LABEL: func @stray_unused_manual_computation_custom_calls
@@ -279,4 +291,10 @@ func.func @local_xla.sdy.manual_computation_body_8() {
 func.func @local_xla.sdy.manual_computation_body_9(%arg0: tensor<0x8xf32>, %arg1: tensor<8x32xf32>) -> (tensor<0x32xf32>, tensor<8x32xf32>) {
   %0 = stablehlo.dot %arg0, %arg1 : (tensor<0x8xf32>, tensor<8x32xf32>) -> tensor<0x32xf32>
   return %0, %arg1 : tensor<0x32xf32>, tensor<8x32xf32>
+}
+
+// CHECK-NOT: func @local_xla.sdy.manual_computation_body_10(
+func.func @local_xla.sdy.manual_computation_body_10(%arg0: tensor<0x8xf32>, %arg1: tensor<8x0xf32>) -> (tensor<0x0xf32>, tensor<8x0xf32>) {
+  %0 = stablehlo.dot %arg0, %arg1 : (tensor<0x8xf32>, tensor<8x0xf32>) -> tensor<0x0xf32>
+  return %0, %arg1 : tensor<0x0xf32>, tensor<8x0xf32>
 }