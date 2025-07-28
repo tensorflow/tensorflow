5239d2a5d5bbdb19dee636805eed12678edd3b81
kleaSCM <kleascm@gmail.com>
2025-07-28 07:45:44 +0000

    Fix Apache license headers in Go session config files
    Replace custom author blocks with proper Apache License 2.0 headers
    to match TensorFlow's standard license format.

--- tensorflow/go/session_config_test.go
+++ tensorflow/go/session_config_test.go
@@ -1,8 +1,17 @@
 /*
-Author: KleaSCM
-Email: KleaSCM@gmail.com
-File: session_config_test.go
-Description: Tests for the type-safe SessionConfig and GPUOptions APIs in TensorFlow Go bindings. Ensures correct configuration, serialization, and backward compatibility with the legacy []byte config.
+Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package tensorflow
