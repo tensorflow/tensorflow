3bf3ac322ee02f8295c5cd6e257772bbb0540f6c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-27 17:54:26 -0700

    Automated Code Change
    PiperOrigin-RevId: 787800573

--- tensorflow/core/framework/node_properties_test.cc
+++ tensorflow/core/framework/node_properties_test.cc
@@ -15,8 +15,14 @@ limitations under the License.
 
 #include "tensorflow/core/framework/node_properties.h"
 
+#include <memory>
+
+#include "absl/status/status.h"
+#include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_def.pb.h"
 #include "tensorflow/core/framework/op_def_builder.h"
+#include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
