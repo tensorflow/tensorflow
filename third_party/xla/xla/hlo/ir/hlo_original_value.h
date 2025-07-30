010bf763fe095c2ad55f3f5c62482dec60d5c568
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-29 20:48:36 -0700

    Automated Code Change
    PiperOrigin-RevId: 788730702

--- third_party/xla/xla/hlo/ir/hlo_original_value.h
+++ third_party/xla/xla/hlo/ir/hlo_original_value.h
@@ -20,6 +20,7 @@ limitations under the License.
 #include <string>
 #include <utility>
 
+#include "absl/strings/string_view.h"
 #include "xla/shape_tree.h"
 #include "xla/shape_util.h"
 #include "xla/xla_data.pb.h"
