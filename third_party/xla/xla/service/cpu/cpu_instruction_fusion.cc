8752f6dc28dcbd5ae8a5485a25ce062562e9f679
Will Froom <willfroom@google.com>
2025-07-30 08:28:26 -0700

    [XLA:CPU] Don't fuse non-scalar constants.
    PiperOrigin-RevId: 788911526

--- third_party/xla/xla/service/cpu/cpu_instruction_fusion.cc
+++ third_party/xla/xla/service/cpu/cpu_instruction_fusion.cc
@@ -163,8 +163,9 @@ FusionDecision CpuInstructionFusion::ShouldFuse(HloInstruction* consumer,
   // number of arguments.
   static constexpr int64_t kMaxConcatenateArguments = 8;
 
-  if (IsLargeConstant(producer)) {
-    return FusionDecision::Forbid("Don't fuse large constants.");
+  if (HloPredicateIsOp<HloOpcode::kConstant>(producer) &&
+      !ShapeUtil::IsEffectiveScalar(producer->shape())) {
+    return FusionDecision::Forbid("Don't fuse non-scalar constants.");
   }
 
   if (CanBeOutputFused(producer, consumer)) {
@@ -335,11 +336,5 @@ HloInstruction* CpuInstructionFusion::FuseInstruction(
   return new_producer;
 }
 
-bool CpuInstructionFusion::IsLargeConstant(
-    const HloInstruction* constant) const {
-  return constant->IsConstant() &&
-         Cast<HloConstantInstruction>(constant)->literal().size_bytes() >
-             GetLargeConstantThresholdBytes();
-}
 }  // namespace cpu
 }  // namespace xla
