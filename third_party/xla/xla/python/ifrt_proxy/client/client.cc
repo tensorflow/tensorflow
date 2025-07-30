422486872f3f9ef4c2a2c6f32fb327c50277bc24
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-30 09:57:36 -0700

    Add NOLINT to suppress redundant string conversion warnings
    PiperOrigin-RevId: 788940259

--- third_party/xla/xla/python/ifrt_proxy/client/client.cc
+++ third_party/xla/xla/python/ifrt_proxy/client/client.cc
@@ -330,6 +330,8 @@ absl::StatusOr<std::vector<xla::ifrt::ArrayRef>> Client::CopyArrays(
   }
   if (memory_kind.has_value()) {
     // Use an empty string to indicate the default memory kind.
+    // OSS requires explicit string conversion
+    // NOLINTNEXTLINE(*-redundant-string-conversions)
     req->set_memory_kind(std::string(memory_kind->memory_kind().value_or("")));
   }
   req->set_copy_semantics(ToArrayCopySemanticsProto(semantics));
@@ -470,6 +472,8 @@ Client::GetDefaultPjRtLayout(xla::ifrt::DType dtype,
     req->add_dims(dim);
   }
   req->set_device_id(device->Id().value());
+  // OSS requires explicit string conversion
+  // NOLINTNEXTLINE(*-redundant-string-conversions)
   req->set_memory_kind(std::string(memory_kind.memory_kind().value_or("")));
 
   auto future = rpc_helper_->GetDefaultLayout(std::move(req));
