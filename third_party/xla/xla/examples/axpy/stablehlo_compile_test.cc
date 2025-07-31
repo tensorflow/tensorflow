5443ec5236a3ac71d54ced9d877b16837e79a81e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-07-31 05:47:54 -0700

    Automated Code Change
    PiperOrigin-RevId: 789302220

--- third_party/xla/xla/examples/axpy/stablehlo_compile_test.cc
+++ third_party/xla/xla/examples/axpy/stablehlo_compile_test.cc
@@ -104,15 +104,15 @@ TEST_F(StableHloAxpyTest, UsePjrtCppWrapper) {
 
 TEST_F(StableHloAxpyTest, RegisterAPIAndRetrieve) {
   const char* device = "MyFancyCPUDevice";
-  EXPECT_THAT(GetCApiClient(device), ::testing::Not(::tsl::testing::IsOk()));
-  EXPECT_THAT(pjrt::PjrtApi(device), ::testing::Not(::tsl::testing::IsOk()));
+  EXPECT_THAT(GetCApiClient(device), ::testing::Not(absl_testing::IsOk()));
+  EXPECT_THAT(pjrt::PjrtApi(device), ::testing::Not(absl_testing::IsOk()));
 
   const PJRT_Api* c_api = GetPjrtApi();
   EXPECT_THAT(c_api, NotNull());
   TF_EXPECT_OK(pjrt::SetPjrtApi(device, c_api));
 
-  EXPECT_THAT(GetCApiClient(device), ::tsl::testing::IsOk());
-  EXPECT_THAT(pjrt::PjrtApi(device), ::tsl::testing::IsOk());
+  EXPECT_THAT(GetCApiClient(device), absl_testing::IsOk());
+  EXPECT_THAT(pjrt::PjrtApi(device), absl_testing::IsOk());
 }
 
 TEST_F(StableHloAxpyTest, CompileCPUTestProgram) {
