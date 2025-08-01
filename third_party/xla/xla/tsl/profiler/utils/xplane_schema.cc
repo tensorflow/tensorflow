529daea385e36a1a9e6a81a0ebfed542def5c476
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-01 14:36:36 -0700

    Chain GpuOnDeviceEventTrace with gpu_tracer.
    PiperOrigin-RevId: 789899761

--- third_party/xla/xla/tsl/profiler/utils/xplane_schema.cc
+++ third_party/xla/xla/tsl/profiler/utils/xplane_schema.cc
@@ -37,6 +37,8 @@ const char kSparseCorePlaneRegex[] = {
 // TODO(b/195582092): change it to /device:custom once all literals are
 // migrated.
 const absl::string_view kCustomPlanePrefix = "/device:CUSTOM:";
+const absl::string_view kCustomGpuOnDeviceTracePlanePrefix =
+    "/device:CUSTOM:MOSAIC:";  // /device:CUSTOM:MOSAIC:INSTANCE_ID
 
 const absl::string_view kScopeRangeIdTreePlaneName =
     "/host:__ScopeRangeCallStack__";
