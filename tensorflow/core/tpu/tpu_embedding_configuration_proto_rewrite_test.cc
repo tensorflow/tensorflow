38657db7e441b669093c1e432eda01aaec9ba9b1
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-03 17:58:05 -0700

    Automated Code Change
    PiperOrigin-RevId: 790523044

--- tensorflow/core/tpu/tpu_embedding_configuration_proto_rewrite_test.cc
+++ tensorflow/core/tpu/tpu_embedding_configuration_proto_rewrite_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/strings/string_view.h"
 #include "xla/tsl/lib/core/status_test_util.h"
 #include "xla/tsl/platform/test.h"
