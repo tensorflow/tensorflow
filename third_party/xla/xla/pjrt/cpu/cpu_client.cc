27f7e2d47b832eb7d43d45ab9d7937ffecf2ebfc
Karlo Basioli <basioli@google.com>
2025-08-04 03:20:34 -0700

    [XLA:CPU] Support dumping snapshots with unoptimized HLO.
    PiperOrigin-RevId: 790672294

--- third_party/xla/xla/pjrt/cpu/cpu_client.cc
+++ third_party/xla/xla/pjrt/cpu/cpu_client.cc
@@ -526,7 +526,7 @@ PjRtCpuClient::LoadSerializedExecutable(absl::string_view serialized,
       compile_options.parameter_is_tupled_arguments, std::move(input_options),
       std::move(executable), std::move(result_buffer_indices),
       std::move(addressable_device_logical_ids), std::move(addressable_devices),
-      this);
+      this, nullptr);
   TF_RETURN_IF_ERROR(cpu_executable->SetUpDonation(
       compile_options.parameter_is_tupled_arguments));
 
@@ -843,12 +843,26 @@ PjRtCpuClient::CompileInternal(
       FindResultBufferAllocationIndex(cpu_executable_ptr->buffer_assignment(),
                                       cpu_executable->module()));
 
+  std::unique_ptr<HloModule> unoptimized_hlo_module = nullptr;
+
+  const bool xla_cpu_dump_unoptimized_hlo_snapshots =
+      options.executable_build_options.has_debug_options() &&
+      options.executable_build_options.debug_options()
+          .xla_cpu_dump_unoptimized_hlo_snapshots();
+
+  if (xla_cpu_dump_unoptimized_hlo_snapshots) {
+    TF_ASSIGN_OR_RETURN(
+        unoptimized_hlo_module,
+        HloModule::CreateFromProto(computation.proto(),
+                                   cpu_executable->module().config()));
+  }
+
   auto executable = std::make_unique<PjRtCpuExecutable>(
       num_replicas, num_partitions, std::move(device_assignment),
       options.parameter_is_tupled_arguments, std::move(input_options),
       std::move(cpu_executable), std::move(result_buffer_indices),
       std::move(addressable_device_logical_ids), std::move(addressable_devices),
-      this);
+      this, std::move(unoptimized_hlo_module));
   TF_RETURN_IF_ERROR(
       executable->SetUpDonation(options.parameter_is_tupled_arguments));
 
@@ -1016,7 +1030,8 @@ PjRtCpuExecutable::PjRtCpuExecutable(
     std::unique_ptr<Executable> cpu_executable,
     absl::InlinedVector<BufferAllocation::Index, 4> result_buffer_indices,
     std::vector<LogicalDeviceIds> addressable_device_logical_ids,
-    std::vector<PjRtDevice*> addressable_devices, PjRtCpuClient* client)
+    std::vector<PjRtDevice*> addressable_devices, PjRtCpuClient* client,
+    std::unique_ptr<HloModule> unoptimized_hlo_module)
     : client_(client),
       num_replicas_(num_replicas),
       num_partitions_(num_partitions),
@@ -1027,7 +1042,8 @@ PjRtCpuExecutable::PjRtCpuExecutable(
       result_buffer_indices_(std::move(result_buffer_indices)),
       addressable_device_logical_ids_(
           std::move(addressable_device_logical_ids)),
-      addressable_devices_(std::move(addressable_devices)) {
+      addressable_devices_(std::move(addressable_devices)),
+      unoptimized_hlo_module_(std::move(unoptimized_hlo_module)) {
   auto hlo_cost_analysis =
       std::make_unique<HloCostAnalysis>(cpu::CpuExecutable::ShapeSizeBytes);
   CHECK_OK(cpu_executable_->module().entry_computation()->Accept(
@@ -1787,7 +1803,8 @@ absl::StatusOr<PjRtLoadedExecutable::Result> PjRtCpuExecutable::ExecuteHelper(
 static void MaybeDumpHloSnapshot(
     const HloModule& module, RunId run_id,
     const std::vector<PjRtBuffer*>& arguments,
-    const std::vector<std::unique_ptr<PjRtBuffer>>& results) {
+    const std::vector<std::unique_ptr<PjRtBuffer>>& results,
+    absl::string_view file_name_prefix = "") {
   if (!DumpingEnabledForHloModule(module)) {
     return;
   }
@@ -1814,8 +1831,10 @@ static void MaybeDumpHloSnapshot(
         LiteralUtil::MakeTupleOwned(std::move(result_literals)).ToProto();
   }
 
-  DumpToFileInDir(module, "", absl::StrCat("snapshot.", run_id.ToInt(), ".pb"),
-                  hlo_snapshot.SerializeAsString());
+  DumpToFileInDir(
+      module, "",
+      absl::StrCat(file_name_prefix, "snapshot.", run_id.ToInt(), ".pb"),
+      hlo_snapshot.SerializeAsString());
 }
 
 absl::StatusOr<std::vector<std::vector<std::unique_ptr<PjRtBuffer>>>>
@@ -1867,6 +1886,10 @@ PjRtCpuExecutable::Execute(
     // Dump once before running, in case there's a crash.
     MaybeDumpHloSnapshot(cpu_executable_->module(), run_id, argument_handles[0],
                          {});
+    if (unoptimized_hlo_module_ != nullptr) {
+      MaybeDumpHloSnapshot(*unoptimized_hlo_module_, run_id,
+                           argument_handles[0], {}, "unoptimized_hlo_");
+    }
     auto statusor = ExecuteHelper(
         argument_handles[0], replica, partition, run_id, options,
         /*last_collective_launch_event=*/{}, returned_futures.has_value());
@@ -1882,6 +1905,11 @@ PjRtCpuExecutable::Execute(
 
     MaybeDumpHloSnapshot(cpu_executable_->module(), run_id, argument_handles[0],
                          wrapped_results[0]);
+    if (unoptimized_hlo_module_ != nullptr) {
+      MaybeDumpHloSnapshot(*unoptimized_hlo_module_, run_id,
+                           argument_handles[0], wrapped_results[0],
+                           "unoptimized_hlo_");
+    }
   } else {
     // Gang schedule collectives to ensure that collectives with the same RunId
     // are run at the same time. We conservatively run only one collective at a
