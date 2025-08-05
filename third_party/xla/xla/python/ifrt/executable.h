b6b46b51e4c8b990149d0c72b0552f88fba625e1
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-05 12:58:46 -0700

    Add program call_location info to HAL log on crash
    PiperOrigin-RevId: 791334435

--- third_party/xla/xla/python/ifrt/executable.h
+++ third_party/xla/xla/python/ifrt/executable.h
@@ -135,6 +135,9 @@ struct ExecuteOptions {
   // are responsible for ensuring version compatibility.
   std::optional<AttributeMap> custom_options;
 
+  // Call location of the compiled program.
+  std::optional<std::string> call_location;
+
   absl::StatusOr<ExecuteOptionsProto> ToProto(
       SerDesVersion version = SerDesDefaultVersionAccessor::Get()) const;
 
