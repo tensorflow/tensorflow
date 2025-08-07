b9445e82e47ebf6c3eb192ef712c2306a31e591e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-07 00:08:02 -0700

    Automated Code Change
    PiperOrigin-RevId: 792021594

--- third_party/xla/xla/python/transfer/pjrt_transfer_server.cc
+++ third_party/xla/xla/python/transfer/pjrt_transfer_server.cc
@@ -56,6 +56,7 @@ limitations under the License.
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/util.h"
+#include "xla/xla_data.pb.h"
 #include "tsl/platform/casts.h"
 
 namespace xla {
