e5ce848daccc64119fb6816af1a859be7c3d25d3
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-07 02:06:59 -0700

    Replace lite/tools/versioning:versioning with compiler/mlir/lite/tools/versioning:versioning
    PiperOrigin-RevId: 792058587

--- tensorflow/compiler/mlir/lite/tools/versioning/runtime_version_test.cc
+++ tensorflow/compiler/mlir/lite/tools/versioning/runtime_version_test.cc
@@ -1,4 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2025 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,13 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/lite/tools/versioning/runtime_version.h"
+#include "tensorflow/compiler/mlir/lite/tools/versioning/runtime_version.h"
 
 #include <string>
 
 #include <gtest/gtest.h>
-#include "tensorflow/lite/core/kernels/register.h"
-#include "tensorflow/lite/schema/schema_generated.h"
+
 namespace tflite {
 
 TEST(OpVersionTest, CompareRuntimeVersion) {
@@ -31,26 +30,6 @@ TEST(OpVersionTest, CompareRuntimeVersion) {
   EXPECT_FALSE(CompareRuntimeVersion("2.1.0", "1.2.0"));
   EXPECT_TRUE(CompareRuntimeVersion("", "1.13"));
   EXPECT_FALSE(CompareRuntimeVersion("", ""));
-}
-
-// This test will fail if an op version is added to a builtin op, but not
-// registered to runtime version.
-TEST(OpVersionTest, OpversionMissing) {
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-
-  for (int id = BuiltinOperator_MIN; id <= BuiltinOperator_MAX; ++id) {
-    for (int version = 1;; ++version) {
-      auto op_code = static_cast<tflite::BuiltinOperator>(id);
-      if (resolver.FindOp(op_code, version) == nullptr) break;
-      // Throw error if the version is not registered in runtime version.
-      std::string runtime_version =
-          FindMinimumRuntimeVersionForOp(op_code, version);
-      EXPECT_NE(runtime_version, "")
-          << "Please add the version " << version << " of "
-          << tflite::EnumNamesBuiltinOperator()[op_code]
-          << " to runtime_version.cc";
-    }
-  }
 }
 
 }  // namespace tflite
