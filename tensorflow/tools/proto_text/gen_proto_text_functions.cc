d0a89916fb34f1e6dbf8a900c10b88b3747cc90f
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-10 19:11:58 -0700

    Automated Code Change
    PiperOrigin-RevId: 793435905

--- tensorflow/tools/proto_text/gen_proto_text_functions.cc
+++ tensorflow/tools/proto_text/gen_proto_text_functions.cc
@@ -15,9 +15,11 @@ limitations under the License.
 
 #include <stdio.h>
 
-#include <set>
+#include <cstring>
 
+#include "absl/log/log.h"
 #include "absl/strings/string_view.h"
+#include "third_party/protobuf/descriptor.h"
 #include "xla/tsl/platform/protobuf_compiler.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/protobuf.h"
