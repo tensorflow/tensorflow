ba8b6db1d9fd0c9eb4825767c6aea22824ef8c3e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-11 18:21:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 793874679

--- third_party/xla/xla/backends/autotuner/autotuner_test.cc
+++ third_party/xla/xla/backends/autotuner/autotuner_test.cc
@@ -20,9 +20,11 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "google/protobuf/any.pb.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "absl/time/time.h"
