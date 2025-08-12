9564ec9f0b1f3bd13897b0276d230c41b380ee98
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-11 22:03:16 -0700

    Automated Code Change
    PiperOrigin-RevId: 793930380

--- third_party/xla/xla/tools/benchmarks/utils/generate_benchmark_matrices_test.cc
+++ third_party/xla/xla/tools/benchmarks/utils/generate_benchmark_matrices_test.cc
@@ -22,6 +22,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "json/json.h"
