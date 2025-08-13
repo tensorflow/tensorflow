f67f13d39f88362b046113d64a44cedad4bd0962
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-12 21:21:11 -0700

    Automated Code Change
    PiperOrigin-RevId: 794386108

--- third_party/xla/xla/service/memory_space_assignment/memory_space_assignment_test.cc
+++ third_party/xla/xla/service/memory_space_assignment/memory_space_assignment_test.cc
@@ -39,6 +39,7 @@ limitations under the License.
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
