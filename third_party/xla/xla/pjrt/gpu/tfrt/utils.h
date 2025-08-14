b2cd00d38e565dc80b0e789f1215f5799b48956e
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-14 14:52:29 -0700

    Move all util functions from tfrt_gpu_client into a dedicated util file.
    PiperOrigin-RevId: 795209927

---
+++ third_party/xla/xla/pjrt/gpu/tfrt/utils.h
@@ -0,0 +1,178 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_PJRT_GPU_TFRT_UTILS_H_
+#define XLA_PJRT_GPU_TFRT_UTILS_H_
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/functional/any_invocable.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
+#include "unsupported/Eigen/CXX11/Tensor"
+#include "xla/client/local_client.h"
+#include "xla/executable_run_options.h"
+#include "xla/layout.h"
+#include "xla/maybe_owning.h"
+#include "xla/pjrt/distributed/key_value_store_interface.h"
+#include "xla/pjrt/gpu/gpu_topology.h"
+#include "xla/pjrt/gpu/se_gpu_topology_description.h"
+#include "xla/pjrt/gpu/tfrt/gpu_event.h"
+#include "xla/pjrt/gpu/tfrt/tfrt_gpu_buffer.h"
+#include "xla/pjrt/gpu/tfrt/tfrt_gpu_device.h"
+#include "xla/pjrt/gpu/tfrt/tracked_gpu_device_buffer.h"
+#include "xla/pjrt/pjrt_client.h"
+#include "xla/pjrt/pjrt_common.h"
+#include "xla/pjrt/pjrt_compiler.h"
+#include "xla/pjrt/pjrt_executable.h"
+#include "xla/pjrt/pjrt_future.h"
+#include "xla/pjrt/plugin/xla_gpu/xla_gpu_client_options.h"
+#include "xla/service/computation_placer.h"
+#include "xla/service/gpu/gpu_executable_run_options.h"
+#include "xla/service/hlo.pb.h"
+#include "xla/shape.h"
+#include "xla/stream_executor/device_memory_allocator.h"
+#include "xla/stream_executor/platform.h"
+#include "xla/stream_executor/stream.h"
+#include "xla/stream_executor/stream_executor.h"
+#include "xla/tsl/concurrency/async_value_ref.h"
+#include "xla/tsl/framework/allocator.h"
+#include "xla/tsl/platform/threadpool.h"
+#include "xla/xla.pb.h"
+#include "xla/xla_data.pb.h"
+
+namespace xla {
+
+PjRtFuture<>::Promise CreatePromiseForEvent(
+    tsl::AsyncValueRef<xla::GpuEvent> event);
+
+absl::StatusOr<Shape> GetDestinationDeviceShape(const Shape& host_shape,
+                                                TfrtGpuDevice* device,
+                                                TfrtGpuClient* client,
+                                                PjRtMemorySpace* memory_space);
+
+absl::StatusOr<std::unique_ptr<TfrtGpuBuffer>> AllocateTfrtGpuDestinationBuffer(
+    const Shape& on_host_shape, tsl::AsyncValueRef<GpuEvent> definition_event,
+    TfrtGpuDevice* device, TfrtGpuClient* client, PjRtMemorySpace* memory_space,
+    int64_t pack_size = 0);
+
+void EnqueueWork(tsl::thread::ThreadPool* pool,
+                 absl::AnyInvocable<void() &&> callee);
+
+std::string MakeComputeCapabilityString(
+    const stream_executor::DeviceDescription* desc);
+
+bool IsAllZeros(const DeviceAssignment& assignment);
+
+std::vector<tsl::RCReference<tsl::AsyncValue>> CopyAsyncValues(
+    absl::Span<const tsl::RCReference<tsl::AsyncValue>> events);
+
+// Checks that the input buffers passed in by the user have the correct size
+// on device for the compiled program.
+absl::Status CheckBufferCompatibilities(
+    absl::Span<int64_t const> input_buffer_sizes_in_bytes,
+    absl::Span<TrackedGpuDeviceBuffer* const> input_buffers);
+
+template <typename MemorySpaceKind>
+bool IsMemorySpaceKind(const PjRtMemorySpace* memory_space) {
+  return memory_space->kind_id() == MemorySpaceKind::kKindId;
+}
+
+std::optional<stream_executor::GpuTargetConfigProto> GetTargetConfigForDevices(
+    absl::Span<PjRtDevice* const> devices);
+
+absl::flat_hash_map<std::string, PjRtDeviceAttribute> GetAttrsForDevices(
+    std::optional<stream_executor::GpuTargetConfigProto> target_config);
+
+template <typename T>
+const T* FindCallback(int channel_id, absl::Span<const T> callbacks) {
+  // TODO(ezhulenev): Can we use binary search here assuming that callbacks
+  // are sorted by channel id? Are they always sorted?
+  auto it = absl::c_find_if(callbacks, [&](const T& callback) {
+    return callback.channel_id == channel_id;
+  });
+  return it == callbacks.end() ? nullptr : &*it;
+}
+
+// Converts PjRt SendCallbacks to an XLA StreamExecutor send function.
+SendDeviceMemoryFunction ConvertSendCallbacksToSendFunction(
+    int replica, const ExecuteOptions& options,
+    tsl::thread::ThreadPool* thread_pool);
+
+RecvDeviceMemoryFunction ConvertRecvCallbacksToRecvFunction(
+    int replica, const ExecuteOptions& options);
+
+std::vector<PjRtMemorySpace*> GetMemorySpacePointers(
+    const std::vector<std::unique_ptr<PjRtMemorySpace>>& memory_spaces);
+
+std::vector<PjRtDevice*> InitializeDevices(
+    TfrtGpuClient* client,
+    const std::vector<std::unique_ptr<TfrtGpuDevice>>& owned_devices);
+
+absl::flat_hash_map<PjRtGlobalDeviceId, TfrtGpuDevice*> GetIdToDeviceMap(
+    absl::Span<const std::unique_ptr<TfrtGpuDevice>> devices);
+
+std::vector<PjRtDevice*> GetAddressableDevicePointers(
+    absl::Span<const std::unique_ptr<TfrtGpuDevice>> devices);
+
+StreamExecutorGpuTopologyDescription GetTopology(
+    absl::string_view platform_name,
+    std::shared_ptr<const GpuTopology> gpu_topology,
+    absl::Span<PjRtDevice* const> devices);
+
+std::vector<std::unique_ptr<PjRtMemorySpace>> InitializeMemorySpaces(
+    int global_device_count, absl::Span<PjRtDevice* const> addressable_devices);
+
+absl::StatusOr<std::unique_ptr<tsl::Allocator>> CreateAllocatorForDevice(
+    se::StreamExecutor* executor, const GpuAllocatorConfig& allocator_config);
+
+absl::StatusOr<MaybeOwning<se::DeviceMemoryAllocator>> CreateDeviceAllocator(
+    LocalClient* xla_client, const GpuAllocatorConfig& allocator_config,
+    const std::vector<std::unique_ptr<TfrtGpuDevice>>& devices);
+
+using DeviceTopologyPair =
+    std::pair<std::vector<std::unique_ptr<TfrtGpuDevice>>, GpuTopologyProto>;
+
+absl::StatusOr<DeviceTopologyPair> BuildDistributedDevices(
+    absl::string_view platform_name, LocalClient* xla_client, int node_id,
+    int num_nodes, gpu::GpuExecutableRunOptions* gpu_executable_run_options,
+    std::shared_ptr<KeyValueStoreInterface> kv_store, bool enable_mock_nccl,
+    std::optional<absl::string_view> mock_gpu_topology,
+    std::optional<int> partition_index,
+    absl::Duration get_local_topology_timeout,
+    absl::Duration get_global_topology_timeout);
+
+absl::StatusOr<std::vector<absl::string_view>> MemoryKindsFromShape(
+    const Shape& shape, absl::string_view default_memory_kind);
+
+// Enqueue to a thread pool when all `values` are ready.
+void EnqueueWorkWhenReady(
+    tsl::thread::ThreadPool* pool,
+    absl::Span<const tsl::RCReference<tsl::AsyncValue>> values,
+    absl::AnyInvocable<void()> callee);
+
+}  // namespace xla
+
+#endif  // XLA_PJRT_GPU_TFRT_UTILS_H_
