4087311e9599388c98981c375a5fc92287de3699
A. Unique TensorFlower <gardener@tensorflow.org>
2025-08-19 16:59:23 -0700

    Propagate `tensorflow::Context` in IFRT load sequence.
    PiperOrigin-RevId: 797074655

--- tensorflow/core/tfrt/ifrt/ifrt_loaded_variable_utils.cc
+++ tensorflow/core/tfrt/ifrt/ifrt_loaded_variable_utils.cc
@@ -37,6 +37,7 @@ limitations under the License.
 #include "tensorflow/core/framework/resource_handle.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/platform/context.h"
 #include "tensorflow/core/tfrt/ifrt/ifrt_loaded_variable_registry.h"
 #include "tensorflow/core/tfrt/ifrt/ifrt_restore_tensor_registry.h"
 #include "tensorflow/core/tfrt/ifrt/sharding_utils.h"
@@ -124,11 +125,13 @@ absl::Status AsyncLoadRestoredTensorAsIfrtLoadedVariable(
             {.array = loaded_variable_future});
       }));
 
+  tensorflow::Context bg_context(tensorflow::ContextKind::kThread);
   restored_tensor_future.OnReady(
       [ifrt_client = std::move(ifrt_client), &thread_pool = thread_pool,
        checkpoint_loader_queue = checkpoint_loader_queue,
        sharding_config = sharding_config,
-       loaded_variable_promise = std::move(loaded_variable_promise)](
+       loaded_variable_promise = std::move(loaded_variable_promise),
+       bg_context = std::move(bg_context)](
           absl::StatusOr<tensorflow::Tensor> restored_tensor) mutable {
         if (!restored_tensor.ok()) {
           loaded_variable_promise.Set(restored_tensor.status());
@@ -140,8 +143,9 @@ absl::Status AsyncLoadRestoredTensorAsIfrtLoadedVariable(
             [ifrt_client = ifrt_client, &thread_pool = thread_pool,
              sharding_config = std::move(sharding_config),
              restored_tensor = std::move(*restored_tensor),
-             loaded_variable_promise =
-                 std::move(loaded_variable_promise)]() mutable {
+             loaded_variable_promise = std::move(loaded_variable_promise),
+             bg_context = std::move(bg_context)]() mutable {
+              tensorflow::WithContext wc(bg_context);
               absl::StatusOr<xla::ifrt::ArrayRef> variable_array =
                   LoadIfrtVariable(ifrt_client, thread_pool, restored_tensor,
                                    sharding_config);
