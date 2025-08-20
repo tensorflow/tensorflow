df28153934ec3ff6b4a1caae82defbe7566797fc
Krzysztof Witkowski <krzysztof.witkowski@grinn-global.com>
2025-08-20 08:17:27 -0700

    PR #93850: Stable delegate python api
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/93850
    
    This PR introduces support for loading TensorFlow Lite Stable Delegates from JSON configuration files via the Python `Interpreter` API.
    
    ### Summary of changes:
    - Added `experimental_stable_delegates` argument to the `Interpreter` constructor in `interpreter.py`, allowing users to pass a list of paths to JSON delegate configuration files.
    - Extended the C++ backend (`interpreter_wrapper.cc`) with `LoadStableDelegateCPP`, which:
      - Parses TFLiteSettings from JSON.
      - Loads the specified delegate from a shared library file.
      - Constructs the delegate using its settings.
    - Exposed `LoadStableDelegate` through pybind11 in `interpreter_wrapper_pybind11.cc`.
    - Updated Bazel build files to include necessary targets for delegate loading and JSON parsing.
    
    Copybara import of the project:
    
    --
    796f29663d726500ece8afa7ea73ef208f59a6cf by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    feat: add stable delegate python api
    
    --
    23436bd6b95ca2b11cc6b56853c2ebadefadb2d8 by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    fix: return delegate pointer with PyLong_FromVoidPtr
    
    --
    aea85a9ba3840e761f76fa4eaf3a345c63c6627a by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    fix: add error handling for missing fields in settings file
    
    --
    370aa902a3504e163862f3a1a3f10f91a081d479 by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    docs: add to experimental_stable_delegates description JSON example and its field explanation
    
    --
    8057fce043fb011b46a8803d7d8917e8b6968979 by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    fix: reorder dependency order in interpreter_wrapper BUILD file
    
    --
    19a387401e64a063530a3d3337f20bfec815453e by krzy3chu <krzysztof.witkowski.16@gmail.com>:
    
    fix: add flatbuffers dependency to interpreter_wrapper_lib
    
    --
    8fc9d1fa7cf4fd62810d59c9bd036706b0949a33 by Krzysztof Witkowski <krzysztof.witkowski@grinn-global.com>:
    
    fix: add compatible_with attribute to dependent rules
    
    --
    d78358343db5e3693f7f1549744af6b9d05e0a4b by Krzysztof Witkowski <krzysztof.witkowski@grinn-global.com>:
    
    fix: add compatible_with attribute to delegate_loader dependencies
    
    --
    c22f315ae09093b9c13564c1315a1f94529e486c by Krzysztof Witkowski <krzysztof.witkowski@grinn-global.com>:
    
    fix: resolve conflict with master branch
    
    Merging this change closes #93850
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/93850 from grinn-global:feature/stable-delegate-python-api f0e085012b49f01db324687796c54adcca022b73
    PiperOrigin-RevId: 797322274

--- tensorflow/lite/python/interpreter.py
+++ tensorflow/lite/python/interpreter.py
@@ -409,6 +409,7 @@ class Interpreter:
       experimental_preserve_all_tensors=False,
       experimental_disable_delegate_clustering=False,
       experimental_default_delegate_latest_features=False,
+      experimental_stable_delegates=None,
   ):
     """Constructor.
 
@@ -449,6 +450,26 @@ class Interpreter:
         model. Default is False.
       experimental_default_delegate_latest_features: If true, default delegates
         may enable all flag protected features. Default is False;
+      experimental_stable_delegates: List of file paths to stable delegate
+        configuration files in JSON format. Each file describes a stable
+        delegate, including the shared library to load and its initialization
+        parameters. The stable delegate should conform to the interface defined
+        in tensorflow/lite/delegates/utils/experimental/stable_delegate/
+        stable_delegate_interface.h. Example delegate configuration JSON file:
+          {
+            "stable_delegate_loader_settings": {
+              "delegate_path": "path_to_stable_delegate.so"
+            },
+            "xnnpack_settings": {
+              "num_threads": 5
+            }
+          }
+        Note that the "stable_delegate_loader_settings.delegate_path" field is
+        required and must point to the shared library of the stable delegate.
+        Additional fields in the JSON (like "xnnpack_settings") are optional and
+        may be used to pass delegate-specific settings. For a list of supported
+        fields and their meanings, refer to:
+        tensorflow/lite/acceleration/configuration/configuration.proto
 
     Raises:
       ValueError: If the interpreter was unable to create.
@@ -535,6 +556,12 @@ class Interpreter:
       for delegate in self._delegates:
         self._interpreter.ModifyGraphWithDelegate(
             delegate._get_native_delegate_pointer())  # pylint: disable=protected-access
+
+    if experimental_stable_delegates:
+      for delegate in experimental_stable_delegates:
+        self._interpreter.ModifyGraphWithDelegate(
+            self._interpreter.LoadStableDelegate(delegate))
+
     self._signature_defs = self.get_signature_list()
 
     self._metrics = metrics.TFLiteMetrics()
