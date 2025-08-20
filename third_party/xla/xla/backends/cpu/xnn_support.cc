d1ae2d07a3f0a5a753a84f504e9f86c82d875f38
Eugene Zhulenev <ezhulenev@google.com>
2025-08-19 18:32:55 -0700

    [xla:cpu:xnn] Delete thread pool heuristic for XNN fusions
    PiperOrigin-RevId: 797101864

--- third_party/xla/xla/backends/cpu/xnn_support.cc
+++ third_party/xla/xla/backends/cpu/xnn_support.cc
@@ -49,51 +49,6 @@ limitations under the License.
 
 namespace xla::cpu {
 
-// Thresholds for when to use thread pool for XNNPACK fusions for different
-// HLOs. These numbers picked up randomly and need benchmarks to tune.
-static constexpr int64_t kDotThreshold = 10 * 1000;
-static constexpr int64_t kDefaultThreshold = 100 * 1000;
-
-static int64_t MaxElementsCount(const Shape& shape) {
-  int64_t ret = 0;
-  ShapeUtil::ForEachSubshape(
-      shape, [&](const Shape& shape, const ShapeIndex& index) {
-        ret = std::max(ret, ShapeUtil::ElementsIn(shape));
-      });
-  return ret;
-}
-
-// We rely on a very simple heuristic to determine if thread pool is beneficial
-// for XNNPACK fusions. We assume that if the HLO produces a large result (or
-// has large operands), thread pool will be beneficial for running operation in
-// parallel. For small operations, thread pool overheads are higher than the
-// actual computation.
-static int64_t MaxElementsCount(const HloInstruction* hlo,
-                                bool include_operands = true) {
-  int64_t ret = MaxElementsCount(hlo->shape());
-  if (include_operands) {
-    for (auto* operand : hlo->operands()) {
-      ret = std::max(ret, MaxElementsCount(operand->shape()));
-    }
-  }
-  return ret;
-}
-
-bool XnnShouldUseThreadPool(const HloInstruction* hlo) {
-  switch (hlo->opcode()) {
-    case HloOpcode::kDot:
-      return MaxElementsCount(hlo) > kDotThreshold;
-    default:
-      return MaxElementsCount(hlo) > kDefaultThreshold;
-  }
-}
-
-bool XnnShouldUseThreadPool(const HloComputation* computation) {
-  return absl::c_any_of(
-      computation->instructions(),
-      [](const HloInstruction* hlo) { return XnnShouldUseThreadPool(hlo); });
-}
-
 bool AreDtypesSupported(const Shape& lhs_shape, const Shape& rhs_shape,
                         const Shape& out_shape,
                         const TargetMachineFeatures* cpu_features) {
