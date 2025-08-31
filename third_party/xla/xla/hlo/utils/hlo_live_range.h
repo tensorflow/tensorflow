5122693f685f1d3f68c78f1a91ed8f6fddaaf5df
Vlad Sytchenko <vsytch@google.com>
2025-08-31 14:36:52 -0700

    [XLA] Don't extend async lifetimes
    PiperOrigin-RevId: 801577651

--- third_party/xla/xla/hlo/utils/hlo_live_range.h
+++ third_party/xla/xla/hlo/utils/hlo_live_range.h
@@ -111,12 +111,8 @@ class HloLiveRange {
   // recurse into each called computations in module_scoped_analysis mode. As it
   // walks it also tracks down the ordinal number of each instruction in the
   // schedule and store it in the `instruction_schedule` and
-  // 'flattened_instruction_sequence`. async_context contains the asynchronous
-  // computation that this computation is in, if any. When this value is
-  // non-null, it means that this computation is called by an async op or
-  // another op in an asynchronous context.
-  void FlattenSchedule(const HloComputation& computation,
-                       const HloComputation* async_context = nullptr);
+  // 'flattened_instruction_sequence`.
+  void FlattenSchedule(const HloComputation& computation);
 
   // Returns the last position of a value.
   TimeBound GetLastPosition(const HloValue& value,
@@ -219,8 +215,6 @@ class HloLiveRange {
   absl::flat_hash_map<const HloInstruction*, LogicalTime> instruction_schedule_;
   absl::flat_hash_map<const HloComputation*, TimeBound> computation_span_times_;
   absl::flat_hash_map<const HloValue*, TimeBound> buffer_live_ranges_;
-  absl::flat_hash_map<const HloComputation*, const HloComputation*>
-      computations_in_async_context_;
 };
 
 }  // namespace xla
