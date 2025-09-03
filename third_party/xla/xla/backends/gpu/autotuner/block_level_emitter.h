aea60a9ec5f747001cf3a95697b09fa5caf68493
Tori Baker <vwbaker@google.com>
2025-09-03 07:27:31 -0700

    Modify BlockLevelEmitter to be run before any triton config is found & use the CostModel to generate a default config
    This will allow us to autotune between the "best" BlockLevelEmitter config and other backends without destroying compilation time. This will happen before any triton config is ever created so it should check if it's feasible to go through triton at all & then attempt to create a config for it. If an error arises or no config is found, this should be handled gracefully allowing the autotuner to simply not use it as a backend.
    
    PiperOrigin-RevId: 802545126

--- third_party/xla/xla/backends/gpu/autotuner/block_level_emitter.h
+++ third_party/xla/xla/backends/gpu/autotuner/block_level_emitter.h
@@ -17,6 +17,7 @@ limitations under the License.
 #define XLA_BACKENDS_GPU_AUTOTUNER_BLOCK_LEVEL_EMITTER_H_
 
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "absl/base/nullability.h"
@@ -26,6 +27,8 @@ limitations under the License.
 #include "xla/backends/gpu/autotuner/gpu_codegen_backend.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/service/compiler.h"
+#include "xla/service/gpu/backend_configs.pb.h"
+#include "xla/service/hlo_cost_analysis.h"
 #include "xla/stream_executor/stream_executor.h"
 #include "xla/xla.pb.h"
 
@@ -42,10 +45,13 @@ class BlockLevelEmitterBackend : public GpuCodegenBackend {
   explicit BlockLevelEmitterBackend(
       stream_executor::StreamExecutor* absl_nonnull stream_executor,
       const DebugOptions* absl_nonnull debug_options,
-      Compiler* absl_nonnull compiler, bool use_default_config = false)
+      Compiler* absl_nonnull compiler,
+      HloCostAnalysis::ShapeSizeFunction shape_size_fn,
+      bool use_default_config = false)
       : GpuCodegenBackend("BlockLevelEmitter", stream_executor, debug_options,
                           compiler),
-        use_default_config_(use_default_config) {}
+        use_default_config_(use_default_config),
+        shape_size_fn_(std::move(shape_size_fn)) {}
 
   // Returns all supported block-level tiling configurations for the given
   // instruction.
@@ -64,12 +70,17 @@ class BlockLevelEmitterBackend : public GpuCodegenBackend {
   bool IsSupported(const HloInstruction& instr);
 
  private:
+  absl::StatusOr<BlockLevelFusionConfig> GetCostModelConfig(
+      const HloInstruction& instr) const;
   // If true, the backend will return a single default configuration in
   // GetSupportedConfigs instead of generating all supported configurations.
   // This is useful to autotune between different backends without increasing
   // compile time by too much. It will use the default config, likely already
   // assigned by the cost model.
   bool use_default_config_;
+  // A function which returns the size in bytes of the top-level buffer of a
+  // shape.
+  HloCostAnalysis::ShapeSizeFunction shape_size_fn_;
 };
 
 }  // namespace gpu
