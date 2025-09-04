bb4e7f5f1a6e390944ac15afee3a6c6c98461da9
Dirk Hornung <dirkh@google.com>
2025-09-04 05:09:39 -0700

    [Autotuner]Noop: Reuse the primary stream from the allocator instead of creating a new stream when profiling autotuning results. WeRunning cuBLAS on multiple streams concurrently is currently not supported.
    PiperOrigin-RevId: 802978813

--- third_party/xla/xla/backends/gpu/autotuner/gpu_profiler.cc
+++ third_party/xla/xla/backends/gpu/autotuner/gpu_profiler.cc
@@ -100,14 +100,17 @@ std::unique_ptr<GpuProfiler> GpuProfiler::Create(
     active_allocator = owned_allocator.get();
   }
 
-  auto stream = stream_executor->CreateStream();
+  // TODO(b/442997461): Create a new stream using
+  // `stream_executor->CreateStream()` instead of reusing the allocator stream
+  // once we can handle cuBLAS using multiple streams.
+  auto stream = active_allocator->GetStream(stream_executor->device_ordinal());
   if (!stream.ok()) {
     LOG(ERROR) << "Failed to create stream: " << stream.status();
     return nullptr;
   }
   return absl::WrapUnique(new GpuProfiler(stream_executor, active_allocator,
                                           std::move(owned_allocator),
-                                          std::move(stream.value()), options));
+                                          stream.value(), options));
 }
 
 absl::StatusOr<std::unique_ptr<InputBuffers>> GpuProfiler::CreateInputBuffers(
@@ -119,7 +122,7 @@ absl::StatusOr<std::unique_ptr<InputBuffers>> GpuProfiler::CreateInputBuffers(
           RedzoneBuffers::BuffersToCreate::kAllInputs,
           options_.should_init_buffers,
           /*should_check_correctness=*/true, options_.redzone_padding_bytes,
-          allocator_, stream_.get()));
+          allocator_, stream_));
   auto gpu_buffers = std::make_unique<GpuInputBuffers>();
   gpu_buffers->redzone_buffers = std::move(buffers);
   return gpu_buffers;
@@ -175,7 +178,7 @@ absl::StatusOr<ExecutionOutput> GpuProfiler::Execute(
 
   ExecutableRunOptions run_options;
   run_options.set_device_ordinal(stream_executor_->device_ordinal());
-  run_options.set_stream(stream_.get());
+  run_options.set_stream(stream_);
   run_options.set_allocator(allocator_);
   run_options.set_gpu_executable_run_options(&gpu_opts);
   run_options.set_execution_profile(profile);
@@ -205,10 +208,9 @@ absl::Status GpuProfiler::CheckOutputBuffer(ScopedShapedBuffer& output,
                                             float rtol) {
   BufferComparator comparator(output.on_device_shape(), rtol);
 
-  TF_ASSIGN_OR_RETURN(
-      bool outputs_match,
-      comparator.CompareEqual(stream_.get(), output.root_buffer(),
-                              reference.root_buffer()));
+  TF_ASSIGN_OR_RETURN(bool outputs_match,
+                      comparator.CompareEqual(stream_, output.root_buffer(),
+                                              reference.root_buffer()));
   if (outputs_match) {
     return absl::OkStatus();
   }
