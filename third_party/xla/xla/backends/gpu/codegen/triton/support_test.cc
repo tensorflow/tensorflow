55565ccfd9b02e42a9c2e8608de24365b6be0728
Adrian Kuegel <akuegel@google.com>
2025-09-04 07:03:35 -0700

    [XLA:GPU] Simplify triton support test for bitcast that changes element type.
    PiperOrigin-RevId: 803009739

--- third_party/xla/xla/backends/gpu/codegen/triton/support_test.cc
+++ third_party/xla/xla/backends/gpu/codegen/triton/support_test.cc
@@ -2557,7 +2557,6 @@ TEST_P(BitcastConvertTest, BitcastConvertDisguisedAsBitcast) {
 
   const int bit_width_in = primitive_util::BitWidth(data_type_in);
   const int bit_width_out = primitive_util::BitWidth(data_type_out);
-  ExpectedFailMode fail_mode = ExpectedFailMode::kFail;
   std::vector<int64_t> output_tile_sizes = {1, 32};
   std::string hlo_text;
   const std::string data_type_in_str =
@@ -2581,7 +2580,6 @@ ENTRY triton_computation {
   ROOT bc = $1[33, $2] bitcast(parameter)
 })",
         data_type_in_str, data_type_out_str, bit_width_in / bit_width_out);
-    fail_mode = ExpectedFailMode::kFailOrCrash;
   } else {  // bit_width_in < bit_width_out
     hlo_text = absl::Substitute(
         R"(
@@ -2591,14 +2589,13 @@ ENTRY triton_computation {
 })",
         data_type_in_str, bit_width_out / bit_width_in, data_type_out_str);
     output_tile_sizes = {1};
-    fail_mode = ExpectedFailMode::kFailOrCrash;
   }
 
   TF_ASSERT_OK_AND_ASSIGN(TestedInstruction ti,
                           ParseTemplateAndGetInstruction(hlo_text, data_type_in,
                                                          HloOpcode::kBitcast));
 
-  RunSupportTest(std::move(ti), output_tile_sizes, cc, fail_mode);
+  RunSupportTest(std::move(ti), output_tile_sizes, cc);
 }
 
 INSTANTIATE_TEST_SUITE_P(
