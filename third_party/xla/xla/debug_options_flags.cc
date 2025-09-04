e386ac4e788791dacc2d9ac04d991ca825bf37c5
Adrian Kuegel <akuegel@google.com>
2025-09-04 03:46:34 -0700

    [XLA:GPU] Disable deterministic scatter expander.
    There is a bug reported that seems to be caused by this pass:
    
    https://github.com/jax-ml/jax/issues/27796
    
    PiperOrigin-RevId: 802954287

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -432,7 +432,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_enable_fast_math(false);
   opts.set_xla_gpu_experimental_parallel_collective_overlap_limit(1);
   opts.set_xla_pjrt_allow_auto_layout_in_hlo(false);
-  opts.set_xla_gpu_enable_scatter_determinism_expander(true);
+  opts.set_xla_gpu_enable_scatter_determinism_expander(false);
   opts.set_xla_gpu_unsupported_enable_ragged_all_to_all_decomposer(false);
   opts.set_xla_gpu_unsupported_use_all_reduce_one_shot_kernel(false);
   opts.set_xla_gpu_unsupported_use_ragged_all_to_all_one_shot_kernel(true);
