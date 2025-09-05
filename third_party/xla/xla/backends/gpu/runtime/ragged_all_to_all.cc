4e62a95c121a920c02eca5f23fb0f5cd513dcfb2
Oleg Shyshkov <shyshkov@google.com>
2025-09-05 08:46:57 -0700

    [XLA:GPU] Add missing self-include to `ragged_all_to_all.cc`.
    PiperOrigin-RevId: 803478413

--- third_party/xla/xla/backends/gpu/runtime/ragged_all_to_all.cc
+++ third_party/xla/xla/backends/gpu/runtime/ragged_all_to_all.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include "xla/backends/gpu/runtime/ragged_all_to_all.h"
+
 #include <algorithm>
 #include <array>
 #include <cstddef>
