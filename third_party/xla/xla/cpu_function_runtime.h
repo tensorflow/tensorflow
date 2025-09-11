120a9152c5b3e1827b552aaacf5420dce565fa48
Eugene Zhulenev <ezhulenev@google.com>
2025-09-11 14:58:01 -0700

    [xla] Remove unused XLA_ALIGN macro
    PiperOrigin-RevId: 805997708

--- third_party/xla/xla/cpu_function_runtime.h
+++ third_party/xla/xla/cpu_function_runtime.h
@@ -185,17 +185,6 @@ inline constexpr size_t Align() { return xla::cpu::Align(); }
 ABSL_DEPRECATE_AND_INLINE()
 inline constexpr size_t MinAlign() { return xla::cpu::MinAlign(); }
 
-// When declaring variables that will be passed to an XLA instance as input via
-// set_arg_data(), be it a regular input or a resource variable in the graph,
-// the C++ variables must be aligned.
-//
-// Example usage:
-//   XLA_ALIGN std::array<float, 4> arg_x;
-//   XLA_ALIGN float arg_y;
-//   xla_instance.set_arg_data(0, arg_x.date());
-//   xla_instance.set_arg_data(0, &arg_y);
-#define XLA_ALIGN alignas(xla::cpu_function_runtime::Align())
-
 // AlignedBufferBytes returns the sum of the size of each buffer in
 // `buffer_infos`, skipping constants, on-stack buffers and, if
 // allocate_entry_params is false, entry parameters.  There are `n` entries in
