8f23fe17bcce1f4696c278d295b636b28d86c3db
Eugene Zhulenev <ezhulenev@google.com>
2025-09-11 10:43:07 -0700

    [xla] Migrate to PjRtFuture<>::MakePromise() API
    Some of the Schedule() APIs require copyable std::function parameter, in such cases wrap move-only promise into std::shared_ptr
    
    PiperOrigin-RevId: 805900653

--- third_party/xla/xla/python/ifrt_proxy/server/host_buffer_test.cc
+++ third_party/xla/xla/python/ifrt_proxy/server/host_buffer_test.cc
@@ -17,6 +17,7 @@
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <utility>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -57,15 +58,15 @@ TEST(HostBufferStoreTest, WriteAfterReadStarted) {
   HostBufferStore store;
   const uint64_t kHandle = 1;
 
-  auto lookup_promise =
-      Future<std::shared_ptr<const std::string>>::CreatePromise();
-  Future<std::shared_ptr<const std::string>> lookup_fut(lookup_promise);
+  auto [lookup_promise, lookup_fut] =
+      Future<std::shared_ptr<const std::string>>::MakePromise();
 
   absl::Notification closure_started;
-  tsl::Env::Default()->SchedClosure([&]() {
-    closure_started.Notify();
-    lookup_promise.Set(store.Lookup(kHandle, /*timeout=*/absl::Seconds(10)));
-  });
+  tsl::Env::Default()->SchedClosure(
+      [&, promise = std::move(lookup_promise)]() mutable {
+        closure_started.Notify();
+        promise.Set(store.Lookup(kHandle, /*timeout=*/absl::Seconds(10)));
+      });
 
   closure_started.WaitForNotification();
   absl::SleepFor(absl::Seconds(1));
@@ -79,15 +80,14 @@ TEST(HostBufferStoreTest, ShutdownAfterReadStarted) {
   HostBufferStore store;
   const uint64_t kHandle = 1;
 
-  auto lookup_promise =
-      Future<std::shared_ptr<const std::string>>::CreatePromise();
-  Future<std::shared_ptr<const std::string>> lookup_fut(lookup_promise);
+  auto [lookup_promise, lookup_fut] =
+      Future<std::shared_ptr<const std::string>>::MakePromise();
 
   absl::Notification closure_started;
-  tsl::Env::Default()->SchedClosure([&]() {
+  tsl::Env::Default()->SchedClosure([&, promise = std::move(
+                                            lookup_promise)]() mutable {
     closure_started.Notify();
-    lookup_promise.Set(
-        store.Lookup(kHandle, /*timeout=*/absl::InfiniteDuration()));
+    promise.Set(store.Lookup(kHandle, /*timeout=*/absl::InfiniteDuration()));
   });
 
   closure_started.WaitForNotification();
