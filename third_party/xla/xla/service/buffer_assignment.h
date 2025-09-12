fd44220969cca8cd856d19ee87aa6813199b9819
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-12 11:54:39 -0700

    Add `BufferAllocation::Slice::Contains` to check whether one slice is a subslice of another.
    PiperOrigin-RevId: 806372585

--- third_party/xla/xla/service/buffer_assignment.h
+++ third_party/xla/xla/service/buffer_assignment.h
@@ -222,6 +222,13 @@ class BufferAllocation {
              end > other.offset_;
     }
 
+    bool Contains(const Slice& other) const {
+      const int64_t end = offset_ + size_;
+      const int64_t other_end = other.offset_ + other.size_;
+      return index() == other.index() && offset_ <= other.offset_ &&
+             other_end <= end;
+    }
+
     template <typename H>
     friend H AbslHashValue(H h, const Slice& s) {
       return H::combine(std::move(h), s.index(), s.offset(), s.size());
