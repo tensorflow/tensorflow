openxla/xla

PR #29646: [aarch64 cpu] add oneDNN Graph API to the thunk build
Imported from GitHub PR https://github.com/openxla/xla/pull/29646

Updated the aarch64 oneDNN builds to include Graph API. And added aarch64 to the supported list of platforms in `if_graph_api()`
Copybara import of the project:

--
2a7809e857665cfea1fda866f890dc3d49e67c54 by Sunita Nadampalli <nadampal@amazon.com>:

[aarch64 cpu] add oneDNN Graph API to thunk build

Merging this change closes #29646

COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/29646 from snadampal:enable_onednn_backend_for_aarch64 2a7809e857665cfea1fda866f890dc3d49e67c54
PiperOrigin-RevId: 794120707

xla/tsl/mkl/build_defs.bzl
@@ -167,12 +167,12 @@ def if_mkldnn_aarch64_acl_openmp(if_true, if_false = []):
         "//conditions:default": if_false,
     })
 
-# Temporarily disable Graph API on aarch64 until we change the aarch64 BUILD
-# file to support Graph API.
+# Enable Graph API on linux x86_64 and aarch64 platform with mkl builds
 def if_graph_api(if_true, if_false = []):
     """Returns `if_true` if Graph API is used with oneDNN."""
     return select({
         "@xla//xla/tsl:linux_x86_64": if_true,
+        "@xla//xla/tsl/mkl:build_with_mkl_aarch64": if_true,
         "//conditions:default": if_false,
     })
 

tensorflow/tensorflow

third_party/xla/xla/tsl/mkl/build_defs.bzl
@@ -167,12 +167,12 @@ def if_mkldnn_aarch64_acl_openmp(if_true, if_false = []):
         "//conditions:default": if_false,
     })
 
-# Temporarily disable Graph API on aarch64 until we change the aarch64 BUILD
-# file to support Graph API.
+# Enable Graph API on linux x86_64 and aarch64 platform with mkl builds
 def if_graph_api(if_true, if_false = []):
     """Returns `if_true` if Graph API is used with oneDNN."""
     return select({
         "@local_xla//xla/tsl:linux_x86_64": if_true,
+        "@local_xla//xla/tsl/mkl:build_with_mkl_aarch64": if_true,
         "//conditions:default": if_false,
     })
 