openxla/xla

Mark the callop as un-inlinable in `sdy_round_trip/shard_map_export`.
We will simplify the `CallInliner` by avoiding special handling for Shardy related manual computations. The `CallInliner` has the general interface to decide if a callop is inlinable.

PiperOrigin-RevId: 793703206

xla/service/spmd/shardy/test/sdy_round_trip_shard_map_import.mlir
@@ -177,7 +177,7 @@ func.func @manual_computation_no_inputs_no_outputs() {
   // CHECK-NEXT:   sdy.return
   // CHECK-NEXT: } : () -> ()
   // CHECK-NEXT: return
-  call @xla.sdy.manual_computation_body_8() : () -> ()
+  call @xla.sdy.manual_computation_body_8() {mhlo.frontend_attributes = {inlineable = "false"}} : () -> ()
   return
 }
 

tensorflow/tensorflow

third_party/xla/xla/service/spmd/shardy/test/sdy_round_trip_shard_map_import.mlir
@@ -177,7 +177,7 @@ func.func @manual_computation_no_inputs_no_outputs() {
   // CHECK-NEXT:   sdy.return
   // CHECK-NEXT: } : () -> ()
   // CHECK-NEXT: return
-  call @local_xla.sdy.manual_computation_body_8() : () -> ()
+  call @local_xla.sdy.manual_computation_body_8() {mhlo.frontend_attributes = {inlineable = "false"}} : () -> ()
   return
 }
 