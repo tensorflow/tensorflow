b9d10c8a6ed4ea3817983b843a8e12a7ef0c4a5c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-17 08:47:41 -0700

    [IFRT IR] Support TPU7X in `GetDeviceMemoryInBytes()`.
    PiperOrigin-RevId: 808171878

--- third_party/xla/xla/python/ifrt/ir/utils.cc
+++ third_party/xla/xla/python/ifrt/ir/utils.cc
@@ -48,6 +48,7 @@ absl::StatusOr<int64_t> GetDeviceMemoryInBytes(absl::string_view device_kind) {
   if (device_kind == "TPU v6 lite") {
     return 32LL * kGB;
   }
+
   if (device_kind == "NVIDIA H100 80GB HBM3") {
     return 80LL * kGB;
   }
