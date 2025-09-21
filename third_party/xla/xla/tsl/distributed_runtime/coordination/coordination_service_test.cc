47c3e3b13caa2515072c9bdef1f8b866e972f398
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-21 06:00:54 -0700

    Automated Code Change
    PiperOrigin-RevId: 809678355

--- third_party/xla/xla/tsl/distributed_runtime/coordination/coordination_service_test.cc
+++ third_party/xla/xla/tsl/distributed_runtime/coordination/coordination_service_test.cc
@@ -95,7 +95,7 @@ class TestCoordinationClient : public CoordinationClient {
   TestCoordinationClient() = default;
 
   absl::Status GetStatus() {
-    absl::MutexLock l(&mu_);
+    absl::MutexLock l(mu_);
     return status_;
   }
 
@@ -109,7 +109,7 @@ class TestCoordinationClient : public CoordinationClient {
                               const ReportErrorToTaskRequest* request,
                               ReportErrorToTaskResponse* response,
                               StatusCallback done) override {
-    absl::MutexLock l(&mu_);
+    absl::MutexLock l(mu_);
     status_ = absl::Status(static_cast<absl::StatusCode>(request->error_code()),
                            request->error_message());
     done(absl::OkStatus());
