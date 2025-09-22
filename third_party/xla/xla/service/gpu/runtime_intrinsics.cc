2499c21893683fde9d60f3f3f7d9f51a97548296
Benjamin Chetioui <bchetioui@google.com>
2025-09-22 05:20:26 -0700

    [XLA:GPU] Add a new runtime intrinsic custom call that allows logging the content of any number of arrays.
    It allows specifying a format for logging, following the convention of
    `absl::Substitute` (i.e. argument 0 corresponds to string `$0`, argument 1 to
    string `$1`, and so on).
    
    PiperOrigin-RevId: 809971519

--- third_party/xla/xla/service/gpu/runtime_intrinsics.cc
+++ third_party/xla/xla/service/gpu/runtime_intrinsics.cc
@@ -16,22 +16,33 @@ limitations under the License.
 #include "xla/service/gpu/runtime_intrinsics.h"
 
 #include <cstdint>
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/strings/ascii.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_replace.h"
 #include "absl/strings/string_view.h"
+#include "absl/strings/substitute.h"
 #include "xla/ffi/ffi.h"
 #include "xla/ffi/ffi_api.h"
+#include "xla/layout_util.h"
+#include "xla/literal.h"
 #include "xla/service/collective_ops_utils.h"
 #include "xla/service/custom_call_status.h"
 #include "xla/service/custom_call_target_registry.h"
 #include "xla/service/platform_util.h"
+#include "xla/shape.h"
 #include "xla/shape_util.h"
+#include "xla/stream_executor/memory_allocation.h"
 #include "xla/stream_executor/stream.h"
 #include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
 
@@ -70,7 +81,68 @@ void NopReturnTokenCustomCall(void* stream_handle, void** buffers,
   VLOG(1) << "NopReturnTokenCustomCall called.";
 }
 
+absl::Status DebugPrintCustomCall(se::Stream* stream, ffi::RemainingArgs args,
+                                  absl::string_view format,
+                                  ffi::Result<ffi::Buffer<xla::TOKEN>> res) {
+  if (!stream) {
+    return Internal("Stream is nullptr.");
+  }
+
+  std::vector<ffi::AnyBuffer> args_buffers;
+  args_buffers.reserve(args.size());
+  for (int i = 0; i < args.size(); ++i) {
+    absl::StatusOr<ffi::AnyBuffer> arg = args.get<ffi::AnyBuffer>(i);
+    if (!arg.ok()) {
+      return arg.status();
+    }
+    args_buffers.push_back(*arg);
+  }
+
+  std::string formatted(format);
+
+  // Iterate in reverse order to match the longest string to substitute first.
+  for (int i = args_buffers.size() - 1; i >= 0; --i) {
+    std::string to_substitute = absl::StrCat("$", i);
+    if (!absl::StrContains(formatted, to_substitute)) {
+      return absl::FailedPreconditionError(absl::Substitute(
+          "Missing formatter for argument $0 in debug print custom call", i));
+    }
+    const ffi::AnyBuffer& arg = args_buffers[i];
+    int64_t size_bytes = arg.size_bytes();
+    TF_ASSIGN_OR_RETURN(std::unique_ptr<se::MemoryAllocation> host_buffer,
+                        stream->parent()->HostMemoryAllocate(size_bytes));
+    TF_RETURN_IF_ERROR(
+        stream->Memcpy(host_buffer->opaque(), arg.device_memory(), size_bytes));
+    TF_RETURN_IF_ERROR(stream->BlockHostUntilDone());
+
+    Shape shape = ShapeUtil::MakeShape(arg.element_type(), arg.dimensions());
+    LayoutUtil::SetToDefaultLayout(&shape);
+    MutableBorrowingLiteral literal(static_cast<char*>(host_buffer->opaque()),
+                                    shape);
+    formatted =
+        absl::StrReplaceAll(formatted, {{to_substitute, literal.ToString()}});
+  }
+
+  LOG(INFO) << formatted;
+
+  return absl::OkStatus();
+}
+
 }  // namespace
+
+// This custom call copies its arguments to the host and pretty-prints them as
+// an info log. It takes in a "format" attribute to help identify the arguments
+// in the log. "Format" follows the convention of `absl::Substitute`, i.e.,
+// positional arguments are specified by `$0`, `$1`, etc.
+XLA_FFI_DEFINE_HANDLER(kXlaGpuDebugPrintCustomCall, DebugPrintCustomCall,
+                       ffi::Ffi::Bind()
+                           .Ctx<ffi::Stream>()
+                           .RemainingArgs()
+                           .Attr<absl::string_view>("format")
+                           .Ret<xla::ffi::Buffer<xla::TOKEN>>());
+
+XLA_FFI_REGISTER_HANDLER(ffi::GetXlaFfiApi(), kXlaGpuDebugPrintCustomCallTag,
+                         GetGpuPlatformName(), kXlaGpuDebugPrintCustomCall);
 
 XLA_FFI_DEFINE_HANDLER(kXlaGpuAssertCustomCall, AssertionCustomCall,
                        ffi::Ffi::Bind()
--- third_party/xla/xla/service/gpu/runtime_intrinsics_test.cc
+++ third_party/xla/xla/service/gpu/runtime_intrinsics_test.cc
@@ -16,17 +16,24 @@ limitations under the License.
 #include <memory>
 #include <utility>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/base/log_severity.h"
+#include "absl/log/scoped_mock_log.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/tests/hlo_test_base.h"
-#include "tsl/platform/statusor.h"
+#include "xla/tsl/platform/statusor.h"
+#include "xla/tsl/platform/test.h"
 
 namespace xla {
 namespace gpu {
 namespace {
 
 using RuntimeIntrinsicsTest = HloTestBase;
+
+using ::testing::EndsWith;
+using ::testing::HasSubstr;
 
 TEST_F(RuntimeIntrinsicsTest, NopReturnTokenWorks) {
   constexpr absl::string_view kHloText = R"(
@@ -80,6 +87,57 @@ ENTRY e {
   EXPECT_EQ(module->entry_computation()->instruction_count(), 2);
   // Can run.
   EXPECT_FALSE(Run(std::move(module), /*run_hlo_passes=*/false));
+}
+
+TEST_F(RuntimeIntrinsicsTest, DebugPrintCustomCallFailsWhenFormatIsMissing) {
+  constexpr absl::string_view kHloText = R"(
+HloModule m
+
+ENTRY e {
+  constant = f32[2]{0} constant({1, 2})
+  ROOT print_token = token[] custom-call(constant),
+    backend_config="{format = \"test format\"}",
+    custom_call_target="__xla_gpu_debug_print",
+    custom_call_has_side_effect=true,
+    api_version=API_VERSION_TYPED_FFI
+})";
+
+  ::testing::AssertionResult result = Run(kHloText, /*run_hlo_passes=*/false);
+  EXPECT_FALSE(result);
+  EXPECT_THAT(result.message(), HasSubstr("Missing formatter for argument 0"));
+}
+
+TEST_F(RuntimeIntrinsicsTest, DebugPrintCustomCallWithCorrectLogsAsInfo) {
+  constexpr absl::string_view kHloText = R"(
+HloModule m
+
+ENTRY e {
+  constant = f32[2]{0} constant({1, 2})
+  constant2 = f16[3]{0} constant({3, 4, 5})
+  ROOT print_token = token[] custom-call(constant, constant2),
+    backend_config="{format = \"test format $0 $1\"}",
+    custom_call_target="__xla_gpu_debug_print",
+    custom_call_has_side_effect=true,
+    api_version=API_VERSION_TYPED_FFI
+})";
+
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<HloModule> module,
+                          GetOptimizedModule(kHloText));
+
+  // The parameters of the custom call are not removed.
+  EXPECT_EQ(module->entry_computation()->instruction_count(), 3);
+  absl::ScopedMockLog mock_log(absl::MockLogDefault::kIgnoreUnexpected);
+  EXPECT_CALL(mock_log,
+              Log(absl::LogSeverity::kInfo, EndsWith("runtime_intrinsics.cc"),
+                  HasSubstr("test format f32[2] {1, 2} f16[3] {3, 4, 5}")));
+  // Run the program once before starting capturing the locks. This works around
+  // a deadlock caused by ScopedMockLog.
+  std::unique_ptr<HloModule> module2 = module->Clone();
+  EXPECT_TRUE(Run(std::move(module2), /*run_hlo_passes=*/false));
+  mock_log.StartCapturingLogs();
+  // Runs successfully and logs the expected info.
+  EXPECT_TRUE(Run(std::move(module), /*run_hlo_passes=*/false));
+  mock_log.StopCapturingLogs();
 }
 
 }  // namespace
