3f965eabcecf6311908f9bbecbbe36555354f197
Kamran Mughal <93716295+KamranBaig1122@users.noreply.github.com>
2025-09-23 21:50:39 +0500

    Added License header in separable_conv2d_patch_test.py

--- tensorflow/python/kernel_tests/nn_ops/separable_conv2d_patch_test.py
+++ tensorflow/python/kernel_tests/nn_ops/separable_conv2d_patch_test.py
@@ -1,3 +1,17 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """Unit tests for tf.nn.separable_conv2d operation."""
 
 import numpy as np
@@ -76,3 +90,4 @@ class SeparableConv2DTest(test.TestCase, parameterized.TestCase):
 
 if __name__ == "__main__":
     test.main()
+
