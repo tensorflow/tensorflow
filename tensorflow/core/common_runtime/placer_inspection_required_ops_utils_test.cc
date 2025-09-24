9a348638d48778aae0705880a2a2846e16999f46
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-23 19:00:48 -0700

    Automated Code Change
    PiperOrigin-RevId: 810664608

--- tensorflow/core/common_runtime/placer_inspection_required_ops_utils_test.cc
+++ tensorflow/core/common_runtime/placer_inspection_required_ops_utils_test.cc
@@ -16,13 +16,14 @@ limitations under the License.
 #include "tensorflow/core/common_runtime/placer_inspection_required_ops_utils.h"
 
 #include <map>
+#include <unordered_map>
 
-#include "absl/memory/memory.h"
-#include "absl/strings/str_join.h"
 #include "tensorflow/core/common_runtime/graph_constructor.h"
+#include "tensorflow/core/framework/function.pb.h"
 #include "tensorflow/core/framework/function_testlib.h"
 #include "tensorflow/core/framework/node_def_builder.h"
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/types.pb.h"
 #include "tensorflow/core/graph/graph.h"
 #include "tensorflow/core/graph/graph_def_builder.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
