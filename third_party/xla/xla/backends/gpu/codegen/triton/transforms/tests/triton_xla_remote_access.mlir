ef9c73a29e776be6e724d34637d231d13738bf49
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-25 03:13:16 -0700

    [XLA:GPU]: Add triton_xla.get_peer_ptr operation.
    This operation allows to calculate an address on a peer device.
    
    PiperOrigin-RevId: 811271849

--- third_party/xla/xla/backends/gpu/codegen/triton/transforms/tests/triton_xla_remote_access.mlir
+++ third_party/xla/xla/backends/gpu/codegen/triton/transforms/tests/triton_xla_remote_access.mlir
@@ -10,3 +10,44 @@ tt.func @get_rank(
   %rank = triton_xla.get_rank %metadata : !tt.ptr<i64> -> i64
   tt.return %rank : i64
 }
+
+tt.func @get_peer_ptr(
+  %arg0: !tt.ptr<i64>, %peer_id: i64, %metadata: !tt.ptr<i64>
+) -> !tt.ptr<i64> {
+  // CHECK-NOT: triton_xla.get_peer_ptr
+  // Offset to local_buffer_root_ptrs.
+  // CHECK: %c72_i64 = arith.constant 72 : i64
+
+  // Byte size of a pointer.
+  // CHECK-NEXT: %c8_i64 = arith.constant 8 : i64
+
+  // Load metadata->rank
+  // CHECK-NEXT: %0 = tt.load %arg2 : !tt.ptr<i64>
+
+  // Calculate offset to current base pointer.
+  // CHECK-NEXT: %1 = arith.muli %0, %c8_i64 : i64
+
+  // Load metadata->local_buffer_root_ptrs[metadata->rank].
+  // CHECK-NEXT: %2 = arith.addi %1, %c72_i64 : i64
+  // CHECK-NEXT: %3 = tt.addptr %arg2, %2 : !tt.ptr<i64>, i64
+  // CHECK-NEXT: %4 = tt.load %3 : !tt.ptr<i64>
+
+  // Calculate offset to address.
+  // CHECK-NEXT: %5 = tt.ptr_to_int %arg0 : !tt.ptr<i64> -> i64
+  // CHECK-NEXT: %6 = arith.subi %5, %4 : i64
+
+  // Calculate offset to peer base pointer.
+  // CHECK-NEXT: %7 = arith.muli %arg1, %c8_i64 : i64
+  // CHECK-NEXT: %8 = arith.addi %7, %c72_i64 : i64
+
+  // Load metadata->local_buffer_root_ptrs[peer_id].
+  // CHECK-NEXT: %9 = tt.addptr %arg2, %8 : !tt.ptr<i64>, i64
+  // CHECK-NEXT: %10 = tt.load %9 : !tt.ptr<i64>
+
+  // Load metadata->local_buffer_root_ptrs[peer_id] + offset.
+  // CHECK-NEXT: %11 = arith.addi %10, %6 : i64
+  // CHECK-NEXT: %12 = tt.int_to_ptr %11 : i64 -> !tt.ptr<i64>
+  // CHECK-NEXT: tt.return %12 : !tt.ptr<i64>
+  %peer_ptr = triton_xla.get_peer_ptr %arg0, %peer_id, %metadata : (!tt.ptr<i64>, i64, !tt.ptr<i64>) -> !tt.ptr<i64>
+  tt.return %peer_ptr : !tt.ptr<i64>
+}
--- third_party/xla/xla/backends/gpu/codegen/triton/transforms/triton_xla_lower_remote_access_pass.cc
+++ third_party/xla/xla/backends/gpu/codegen/triton/transforms/triton_xla_lower_remote_access_pass.cc
@@ -13,10 +13,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <utility>
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/Types.h"
 #include "mlir/IR/Value.h"
@@ -25,6 +29,7 @@ limitations under the License.
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "xla/backends/gpu/codegen/triton/ir/triton_xla_ops.h"
+#include "xla/stream_executor/gpu/collective_kernel_metadata.h"
 #include "triton/Dialect/Triton/IR/Dialect.h"
 #include "triton/Dialect/Triton/IR/Types.h"
 
@@ -34,35 +39,110 @@ namespace mlir::triton::xla {
 #include "xla/backends/gpu/codegen/triton/transforms/passes.h.inc"
 
 namespace {
-LogicalResult LowerGetRankOp(GetRankOp get_rank, PatternRewriter& rewriter) {
-  mlir::OpBuilder::InsertionGuard guard(rewriter);
-  rewriter.setInsertionPoint(get_rank);
 
+LogicalResult LowerGetRankOp(GetRankOp get_rank, PatternRewriter& rewriter) {
   mlir::Value metadata = get_rank.getMetadata();
-  auto metadata_type = dyn_cast<mlir::triton::PointerType>(metadata.getType());
+  auto metadata_type = dyn_cast<PointerType>(metadata.getType());
   if (!metadata_type) {
     return rewriter.notifyMatchFailure(get_rank, "Metadata is not a pointer");
   }
 
-  mlir::Type expectedResultType = metadata_type.getPointeeType();
-  if (get_rank->getResult(0).getType() != expectedResultType) {
+  Type expected_result_type = metadata_type.getPointeeType();
+  if (get_rank->getResult(0).getType() != expected_result_type) {
     return rewriter.notifyMatchFailure(
         get_rank, "Call result type must match the pointer's element type");
   }
 
   // The rank id is stored as a first element under the metadata pointer.
-  // The structure of the metadata is defined in
-  // `xla::gpu::CollectiveKernelMetadata`.
-  mlir::Value loadOp = rewriter.create<mlir::triton::LoadOp>(
-      get_rank.getLoc(), expectedResultType, metadata,
+  Value loadOp = rewriter.create<LoadOp>(
+      get_rank.getLoc(), expected_result_type, metadata,
       /*mask=*/nullptr, /*other=*/nullptr, /*boundaryCheck=*/nullptr,
       /*padding=*/nullptr,
-      mlir::triton::CacheModifierAttr::get(get_rank.getContext(),
-                                           mlir::triton::CacheModifier::NONE),
-      mlir::triton::EvictionPolicyAttr::get(
-          get_rank.getContext(), mlir::triton::EvictionPolicy::NORMAL),
+      CacheModifierAttr::get(get_rank.getContext(), CacheModifier::NONE),
+      EvictionPolicyAttr::get(get_rank.getContext(), EvictionPolicy::NORMAL),
       /*isVolatile=*/rewriter.getBoolAttr(false));
   rewriter.replaceOp(get_rank, loadOp);
+  return success();
+}
+
+// The peer address should be computed as follows:
+//
+// offset = address - metadata->buffer_root_ptrs[metadata->rank].
+// peer_address = metadata->buffer_root_ptrs[peer_id] + offset.
+LogicalResult LowerGetPeerPtrOp(GetPeerPtrOp get_peer_ptr,
+                                PatternRewriter& rewriter) {
+  Value metadata = get_peer_ptr.getMetadata();
+  auto metadata_type = dyn_cast<PointerType>(metadata.getType());
+  if (!metadata_type) {
+    return rewriter.notifyMatchFailure(get_peer_ptr,
+                                       "Metadata is not a pointer");
+  }
+
+  ImplicitLocOpBuilder builder(get_peer_ptr.getLoc(), rewriter);
+  Value address = get_peer_ptr.getAddress();
+  Value peer_id = get_peer_ptr.getPeerId();
+  MLIRContext* ctx = rewriter.getContext();
+
+  // Pointer type.
+  Type type_i64 = rewriter.getI64Type();
+  Type result_type = get_peer_ptr.getResult().getType();
+
+  // Size of the pointer in bytes.
+  Value pointer_size_bytes_const =
+      builder.create<arith::ConstantIntOp>(type_i64, sizeof(int64_t));
+
+  // 1. Load metadata->rank.
+  Value current_rank_load_op = builder.create<GetRankOp>(metadata);
+
+  // 2. Load metadata->local_buffer_root_ptrs[metadata->rank].
+  Value local_buffers_ptrs_offset = builder.create<arith::ConstantIntOp>(
+      type_i64, offsetof(CollectiveKernelMetadata, local_buffer_root_ptrs));
+
+  Value rank_offset =
+      builder.create<arith::ExtUIOp>(type_i64, current_rank_load_op);
+  Value current_rank_offset_bytes =
+      builder.create<arith::MulIOp>(rank_offset, pointer_size_bytes_const);
+  Value current_ptr_offset_bytes = builder.create<arith::AddIOp>(
+      local_buffers_ptrs_offset, current_rank_offset_bytes);
+
+  Value current_range_address = builder.create<AddPtrOp>(
+      metadata.getType(), metadata, current_ptr_offset_bytes);
+
+  Value current_range_address_value = builder.create<LoadOp>(
+      type_i64, current_range_address,
+      /*mask=*/nullptr, /*other=*/nullptr, /*boundaryCheck=*/nullptr,
+      /*padding=*/nullptr, CacheModifierAttr::get(ctx, CacheModifier::NONE),
+      EvictionPolicyAttr::get(ctx, EvictionPolicy::NORMAL),
+      /*isVolatile=*/rewriter.getBoolAttr(false));
+
+  // 3. Calculate offset =
+  //      address - metadata->local_buffer_root_ptrs[metadata->rank].
+  Value current_range_address_int =
+      builder.create<PtrToIntOp>(type_i64, address);
+  Value offsetInt = builder.create<arith::SubIOp>(current_range_address_int,
+                                                  current_range_address_value);
+
+  // 4. Load metadata->local_buffer_root_ptrs[peer_id].
+  Value peer_index = builder.create<arith::ExtUIOp>(type_i64, peer_id);
+  Value peer_index_offset_bytes =
+      builder.create<arith::MulIOp>(peer_index, pointer_size_bytes_const);
+  Value peer_range_offset_bytes = builder.create<arith::AddIOp>(
+      local_buffers_ptrs_offset, peer_index_offset_bytes);
+  Value peer_range_address = builder.create<AddPtrOp>(
+      metadata.getType(), metadata, peer_range_offset_bytes);
+
+  Value peer_range_address_value = builder.create<LoadOp>(
+      type_i64, peer_range_address,
+      /*mask=*/nullptr, /*other=*/nullptr, /*boundaryCheck=*/nullptr,
+      /*padding=*/nullptr, CacheModifierAttr::get(ctx, CacheModifier::NONE),
+      EvictionPolicyAttr::get(ctx, EvictionPolicy::NORMAL),
+      /*isVolatile=*/rewriter.getBoolAttr(false));
+
+  // 5. Calculate the result address: peerBasePtr + offset.
+  Value result_int =
+      builder.create<arith::AddIOp>(peer_range_address_value, offsetInt);
+  Value result_address = builder.create<IntToPtrOp>(result_type, result_int);
+  rewriter.replaceOp(get_peer_ptr, result_address);
   return success();
 }
 
@@ -76,6 +156,7 @@ class TritonXLALowerRemoteAccessPass
   void runOnOperation() override {
     RewritePatternSet patterns(&getContext());
     patterns.add(LowerGetRankOp);
+    patterns.add(LowerGetPeerPtrOp);
     if (failed(applyPatternsGreedily(getOperation(), std::move(patterns)))) {
       return signalPassFailure();
     }
