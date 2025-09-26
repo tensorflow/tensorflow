bd0c58359a462f27e38d146955eacf94bd5a5566
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-25 22:20:26 -0700

    add a Get method to attribute_map
    allowing for easier value access
    
    PiperOrigin-RevId: 811657204

--- third_party/xla/xla/python/ifrt/attribute_map.h
+++ third_party/xla/xla/python/ifrt/attribute_map.h
@@ -19,12 +19,17 @@ limitations under the License.
 #include <cstddef>
 #include <cstdint>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <variant>
 #include <vector>
 
 #include "absl/container/flat_hash_map.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "xla/python/ifrt/attribute_map.pb.h"
 #include "xla/python/ifrt/serdes_default_version_accessor.h"
 #include "xla/python/ifrt/serdes_version.h"
@@ -86,6 +91,25 @@ class AttributeMap {
 
   const Map& map() const { return map_; }
 
+  template <typename T>
+  absl::StatusOr<T> Get(const std::string& key) const {
+    if constexpr (std::is_same_v<T, std::string> ||
+                  std::is_same_v<T, absl::string_view>) {
+      return Get<T, StringValue>(key);
+    } else if constexpr (std::is_same_v<T, bool>) {
+      return Get<T, BoolValue>(key);
+    } else if constexpr (std::is_same_v<T, int64_t>) {
+      return Get<T, Int64Value>(key);
+    } else if constexpr (std::is_same_v<T, std::vector<int64_t>> ||
+                         std::is_same_v<T, absl::Span<const int64_t>>) {
+      return Get<T, Int64ListValue>(key);
+    } else if constexpr (std::is_same_v<T, float>) {
+      return Get<T, FloatValue>(key);
+    } else {
+      static_assert(false, "Unsupported type for AttributeMap::Get");
+    }
+  }
+
   // Deserializes `AttributeMapProto` into `AttributeMap`.
   static absl::StatusOr<AttributeMap> FromProto(const AttributeMapProto& proto);
 
@@ -102,6 +126,20 @@ class AttributeMap {
   }
 
  private:
+  template <typename T, typename V>
+  absl::StatusOr<T> Get(const std::string& key) const {
+    auto it = map_.find(key);
+    if (it == map_.end()) {
+      return absl::NotFoundError(absl::StrCat("Key not found: ", key));
+    }
+    const V* value = std::get_if<V>(&it->second);
+    if (value == nullptr) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("Value type mismatch for key: ", key));
+    }
+    return value->value;
+  }
+
   Map map_;
 };
 
