fc0465fd9f2caf35c6791e967799d99a3bd0477c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-25 18:13:25 -0700

    Automated Code Change
    PiperOrigin-RevId: 811577557

--- tensorflow/core/lib/db/sqlite.cc
+++ tensorflow/core/lib/db/sqlite.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/platform/status.h"
 #include "tensorflow/core/platform/strcat.h"
