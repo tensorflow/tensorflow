eaa6b35a60dea4a50e259e28a3ae322e17ea95ba
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-26 21:18:14 -0700

    Avoid copying BufferAllocations in BufferAssignment::CombineTempAllocations
    PiperOrigin-RevId: 812074858

--- third_party/xla/xla/service/buffer_assignment.cc
+++ third_party/xla/xla/service/buffer_assignment.cc
@@ -688,10 +688,6 @@ absl::Status BufferAssignment::CombineTempAllocations(
     const absl::flat_hash_set<BufferValue::Color>& private_stack_colors,
     std::optional<BufferValue::Color> temp_buffer_color) {
   VLOG(1) << "CombineTempAllocations()";
-  // Stores the combined allocations.
-  std::deque<BufferAllocation> combined_allocations;
-  // Holds the pointer to a combined allocation of each color, if any.
-  flat_hash_map<BufferValue::Color, BufferAllocation*> combined_allocation_map;
 
   // Move all temp allocations into a single run at the end of the allocations
   // vector.
@@ -700,91 +696,97 @@ absl::Status BufferAssignment::CombineTempAllocations(
                      [](const BufferAllocation& allocation) {
                        return !allocation.IsPreallocatedTempBuffer();
                      });
+  // Stores the combined allocations.
+  std::vector<BufferAllocation> combined_allocations;
+  // Reserve the max possible size we'd need to ensure pointer stability.
+  combined_allocations.reserve(allocations_.end() - first_temp_it);
+  // Holds the pointer to a combined allocation of each color, if any.
+  flat_hash_map<BufferValue::Color, BufferAllocation*> combined_allocation_map;
 
   // Walk over the run of temp allocations, collecting the allocations belonging
   // to the same color.
-  if (first_temp_it != allocations_.end()) {
-    for (auto it = first_temp_it; it != allocations_.end(); ++it) {
-      BufferAllocation& temp_allocation = *it;
-      BufferValue::Color color = temp_allocation.color();
-      auto combined_it = combined_allocation_map.find(color);
-      if (combined_it == combined_allocation_map.end()) {
-        // We have found the first temp allocation of this color. Collect
-        // the other temp allocations of the same color into it subject to the
-        // size constraint.
-        VLOG(1) << "Combined temp allocation for color " << color
-                << " is: " << temp_allocation;
-        combined_allocations.push_back(temp_allocation);
-        combined_allocation_map.emplace(color, &combined_allocations.back());
-        continue;
-      }
-      if (combined_it->second->size() + it->size() >=
-          multiheap_size_constraint_per_heap_) {
-        // We cannot put more into the current combined_it. So, appoint a new
-        // combined_it.
-        VLOG(1) << "Due to size constraint, reset temp allocation for color "
-                << color << " to: " << temp_allocation;
-        combined_allocations.push_back(temp_allocation);
-        combined_allocation_map.emplace(color, &combined_allocations.back());
-        continue;
-      }
+  for (auto it = first_temp_it; it != allocations_.end(); ++it) {
+    BufferAllocation& temp_allocation = *it;
+    BufferValue::Color color = temp_allocation.color();
+    auto combined_it = combined_allocation_map.find(color);
+    if (combined_it == combined_allocation_map.end()) {
+      // We have found the first temp allocation of this color. Collect
+      // the other temp allocations of the same color into it subject to the
+      // size constraint.
+      VLOG(1) << "Combined temp allocation for color " << color
+              << " is: " << temp_allocation;
+      combined_allocations.push_back(std::move(temp_allocation));
+      combined_allocation_map.emplace(color, &combined_allocations.back());
+      continue;
+    }
+    if (combined_it->second->size() + temp_allocation.size() >=
+        multiheap_size_constraint_per_heap_) {
+      // We cannot put more into the current combined_it. So, appoint a new
+      // combined_it.
+      VLOG(1) << "Due to size constraint, reset temp allocation for color "
+              << color << " to: " << temp_allocation;
+      combined_allocations.push_back(std::move(temp_allocation));
+      combined_allocation_map.emplace(color, &combined_allocations.back());
+      continue;
+    }
 
-      BufferAllocation* combined_allocation = combined_it->second;
-      VLOG(1) << "Combined allocation absorbing temp allocation: "
-              << temp_allocation;
+    // If we got here, temp_allocation is still valid, since all the paths
+    // that `std::move` it continue.
+    BufferAllocation* combined_allocation = combined_it->second;
+    VLOG(1) << "Combined allocation absorbing temp allocation: "
+            << temp_allocation;
 
-      // Each temp allocation is placed end-to-end, accounting for alignment.
-      // The offset of each buffer in the combined allocation is computed from
-      // the base offset of the allocation. For private stack color, we assume
-      // each allocation object corresponds to one of the independent executions
-      // of the private stack computations, so it is safe to reuse offsets in
-      // that case.
-      int64_t alignment = color_alignment_(color);
-      int64_t base;
-      bool is_private_stack = private_stack_colors.contains(color);
-      if (is_private_stack) {
-        base = 0;
-        combined_allocation->set_size(std::max(base, temp_allocation.size()));
-      } else {
-        base = RoundUpTo(combined_allocation->size(), alignment);
-        combined_allocation->set_size(base + temp_allocation.size());
-      }
-      for (const auto& buffer_offset_size : temp_allocation.assigned_buffers_) {
-        const HloValue* value = buffer_offset_size.first;
-        const int64_t offset = buffer_offset_size.second.offset;
-        const int64_t size = buffer_offset_size.second.size;
-        TF_RETURN_IF_ERROR(
-            combined_allocation->AddAssignment(*value, base + offset, size));
-      }
-      if (!temp_allocation.HeapTraces().empty()) {
-        CHECK_EQ(temp_allocation.HeapTraces().size(), 1);
-        combined_allocation->AddHeapTrace(temp_allocation.HeapTraces().front());
-      }
+    // Each temp allocation is placed end-to-end, accounting for alignment.
+    // The offset of each buffer in the combined allocation is computed from
+    // the base offset of the allocation. For private stack color, we assume
+    // each allocation object corresponds to one of the independent executions
+    // of the private stack computations, so it is safe to reuse offsets in
+    // that case.
+    int64_t alignment = color_alignment_(color);
+    int64_t base;
+    bool is_private_stack = private_stack_colors.contains(color);
+    if (is_private_stack) {
+      base = 0;
+      combined_allocation->set_size(std::max(base, temp_allocation.size()));
+    } else {
+      base = RoundUpTo(combined_allocation->size(), alignment);
+      combined_allocation->set_size(base + temp_allocation.size());
+    }
+    for (const auto& buffer_offset_size : temp_allocation.assigned_buffers_) {
+      const HloValue* value = buffer_offset_size.first;
+      const int64_t offset = buffer_offset_size.second.offset;
+      const int64_t size = buffer_offset_size.second.size;
+      TF_RETURN_IF_ERROR(
+          combined_allocation->AddAssignment(*value, base + offset, size));
+    }
+    if (!temp_allocation.HeapTraces().empty()) {
+      CHECK_EQ(temp_allocation.HeapTraces().size(), 1);
+      combined_allocation->AddHeapTrace(temp_allocation.HeapTraces().front());
+    }
 
-      if (is_private_stack) {
-        if (temp_allocation.size() == combined_allocation->size()) {
-          combined_allocation->peak_buffers_ = temp_allocation.peak_buffers_;
-        }
-      } else {
-        combined_allocation->peak_buffers_.insert(
-            combined_allocation->peak_buffers_.end(),
-            temp_allocation.peak_buffers_.begin(),
-            temp_allocation.peak_buffers_.end());
+    if (is_private_stack) {
+      if (temp_allocation.size() == combined_allocation->size()) {
+        combined_allocation->peak_buffers_ = temp_allocation.peak_buffers_;
       }
+    } else {
+      combined_allocation->peak_buffers_.insert(
+          combined_allocation->peak_buffers_.end(),
+          temp_allocation.peak_buffers_.begin(),
+          temp_allocation.peak_buffers_.end());
+    }
 
-      if (temp_buffer_color.has_value()) {
-        if (combined_allocation->color() == 0) {
-          combined_allocation->set_color(temp_buffer_color.value());
-        }
+    if (temp_buffer_color.has_value()) {
+      if (combined_allocation->color() == 0) {
+        combined_allocation->set_color(temp_buffer_color.value());
       }
     }
-    // Replace all existing temporary allocations with the new combined
-    // allocations.
-    allocations_.erase(first_temp_it, allocations_.end());
-    for (BufferAllocation& combined : combined_allocations) {
-      temp_allocation_total_size_ += combined.size();
-      allocations_.push_back(std::move(combined));
-    }
+  }
+  // Replace all existing temporary allocations with the new combined
+  // allocations.
+  allocations_.erase(first_temp_it, allocations_.end());
+  for (BufferAllocation& combined : combined_allocations) {
+    temp_allocation_total_size_ += combined.size();
+    allocations_.push_back(std::move(combined));
   }
 
   // Update allocation indices to their new positions.
