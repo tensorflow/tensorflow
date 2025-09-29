334cd41a6ce4782fa3308df03b0db8201e1d9706
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-29 01:09:12 -0700

    Automated Code Change
    PiperOrigin-RevId: 812670180

--- third_party/xla/xla/service/gpu/split_k_gemm_rewriter_test.cc
+++ third_party/xla/xla/service/gpu/split_k_gemm_rewriter_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "absl/status/status_matchers.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "xla/autotuning.pb.h"
