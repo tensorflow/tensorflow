a81600d78b0715da8961422277c933bece730bee
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-30 18:46:55 -0700

    Automated Code Change
    PiperOrigin-RevId: 813523011

--- tensorflow/c/kernels/summary_op.cc
+++ tensorflow/c/kernels/summary_op.cc
@@ -14,9 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
 #include <sstream>
 #include <string>
 
+#include "absl/log/check.h"
 #include "unsupported/Eigen/CXX11/Tensor"  // from @eigen_archive
 #include "tensorflow/c/kernels.h"
 #include "tensorflow/c/kernels/tensor_shape_utils.h"
