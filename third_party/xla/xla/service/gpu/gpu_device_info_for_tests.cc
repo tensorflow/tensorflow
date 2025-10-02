ae7eb0dc24162fc0cd297d76eef805fa00870105
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-02 14:13:18 -0700

    [XLA:GPU] WIP: Add B200 device info for tests
    PiperOrigin-RevId: 814381430

--- third_party/xla/xla/service/gpu/gpu_device_info_for_tests.cc
+++ third_party/xla/xla/service/gpu/gpu_device_info_for_tests.cc
@@ -73,6 +73,32 @@ stream_executor::DeviceDescription TestGpuDeviceInfo::RTXH100SXMDeviceInfo(
   return b;
 }
 
+stream_executor::DeviceDescription TestGpuDeviceInfo::RTXB200SXMDeviceInfo(
+    stream_executor::GpuComputeCapability cc) {
+  stream_executor::DeviceDescription b;
+  b.set_gpu_compute_capability(cc);
+  b.set_threads_per_block_limit(1024);
+  b.set_threads_per_warp(32);
+  b.set_shared_memory_per_block(128 * 1024);
+  b.set_shared_memory_per_block_optin(227 * 1024);
+  b.set_shared_memory_per_core(228 * 1024);
+  b.set_threads_per_core_limit(2048);
+  b.set_core_count(132);
+  b.set_fpus_per_core(128);
+  b.set_block_dim_limit_x(2'147'483'647);
+  b.set_block_dim_limit_y(65535);
+  b.set_block_dim_limit_z(65535);
+  b.set_memory_bandwidth(8'796'093'022'208);
+  b.set_l2_cache_size(126 * 1024 * 1024);
+  b.set_clock_rate_ghz(1.837);
+  b.set_device_memory_size(193'273'528'320);
+  b.set_registers_per_core_limit(65536);
+  b.set_registers_per_block_limit(65536);
+  b.set_runtime_version(stream_executor::SemanticVersion{12, 4, 0});
+  b.set_driver_version(stream_executor::SemanticVersion{12, 4, 0});
+  return b;
+}
+
 stream_executor::DeviceDescription TestGpuDeviceInfo::AMDMI210DeviceInfo() {
   stream_executor::DeviceDescription b;
   b.set_gpu_compute_capability(
