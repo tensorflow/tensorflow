08efc8ef05cd5b7d56cdb11c474ea78e2e840441
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-02 04:30:51 -0700

    Automated Code Change
    PiperOrigin-RevId: 814175383

--- third_party/xla/xla/ffi/ffi_test.cc
+++ third_party/xla/xla/ffi/ffi_test.cc
@@ -29,6 +29,7 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "absl/log/check.h"
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/strings/match.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
