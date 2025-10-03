82c187192ad49db170b919f9951863b2779cbf5c
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-02 23:32:01 -0700

    Automated Code Change
    PiperOrigin-RevId: 814555104

--- third_party/xla/xla/tests/exhaustive/exhaustive_op_test_base.cc
+++ third_party/xla/xla/tests/exhaustive/exhaustive_op_test_base.cc
@@ -52,6 +52,7 @@ limitations under the License.
 #include "xla/tsl/platform/test.h"
 #include "xla/tsl/util/command_line_flags.h"
 #include "xla/types.h"
+#include "xla/xla.pb.h"
 #include "xla/xla_data.pb.h"
 #include "tsl/platform/path.h"
 
