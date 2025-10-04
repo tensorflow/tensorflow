3e1a1136a8d2294cbef4d25dc415e6695cb799c7
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-03 18:08:21 -0700

    Automated Code Change
    PiperOrigin-RevId: 814900411

--- third_party/xla/xla/service/graphcycles/graphcycles_test.cc
+++ third_party/xla/xla/service/graphcycles/graphcycles_test.cc
@@ -25,7 +25,10 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include <gtest/gtest.h>
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/random/random.h"
 #include "tsl/platform/logging.h"
 #include "tsl/platform/test.h"
