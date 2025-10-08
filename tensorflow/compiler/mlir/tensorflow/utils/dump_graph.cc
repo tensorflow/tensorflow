99e6e88db03db67b75e5a5e074139e9ce8b538e1
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-07 17:40:09 -0700

    Automated Code Change
    PiperOrigin-RevId: 816451774

--- tensorflow/compiler/mlir/tensorflow/utils/dump_graph.cc
+++ tensorflow/compiler/mlir/tensorflow/utils/dump_graph.cc
@@ -17,9 +17,11 @@ limitations under the License.
 
 #include <cstdint>
 #include <cstring>
-#include <string>
 #include <utility>
 
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
 #include "llvm/ADT/StringMap.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
@@ -28,6 +30,7 @@ limitations under the License.
 #include "mlir/IR/Operation.h"  // from @llvm-project
 #include "mlir/IR/Verifier.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/tensorflow/utils/error_util.h"
+#include "tensorflow/core/framework/graph_debug_info.pb.h"
 #include "tensorflow/core/ir/importexport/graphdef_import.h"
 #include "tensorflow/core/platform/env.h"
 #include "tensorflow/core/platform/logging.h"
