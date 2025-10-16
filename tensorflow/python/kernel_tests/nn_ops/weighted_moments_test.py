05e862df35e5e74616d2c5fb996037b6d553404e
Nikhil <jainnicky2k@gmail.com>
2025-10-16 13:04:43 -0400

    Address review feedback: fix imports, indentation, line lengths, and remove gitignore change

--- tensorflow/python/kernel_tests/nn_ops/weighted_moments_test.py
+++ tensorflow/python/kernel_tests/nn_ops/weighted_moments_test.py
@@ -1,19 +1,21 @@
-import tensorflow as tf
-from tensorflow.python.framework import test_util
+from tensorflow.python.framework import constant_op
+from tensorflow.python.ops import nn_ops
 from tensorflow.python.platform import test
+
 
 class WeightedMomentsTest(test.TestCase):
 
   def test_tensor_axes_keepdims_false_shape_mismatch(self):
-    x = tf.constant([[1., 2., 3., 4., 5.],
-                     [6., 7., 8., 9., 10.]])
-    weights = tf.constant([[1.], [1.]])
-    axes = tf.constant([0], dtype=tf.int32)
-
-    mean, var = tf.nn.weighted_moments(x, axes=axes, frequency_weights=weights, keepdims=False)
-
+    x = constant_op.constant([[1., 2., 3., 4., 5.],
+                              [6., 7., 8., 9., 10.]])
+    weights = constant_op.constant([[1.], [1.]])
+    axes = constant_op.constant([0], dtype=constant_op.dtypes.int32)
+    mean, var = nn_ops.weighted_moments(
+        x, axes=axes, frequency_weights=weights, keepdims=False)
     self.assertEqual(mean.shape.as_list(), [5])
     self.assertEqual(var.shape.as_list(), [5])
 
+
 if __name__ == "__main__":
-    test.main()
+  test.main()
+
