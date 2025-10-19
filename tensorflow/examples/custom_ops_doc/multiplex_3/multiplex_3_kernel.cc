2b4c6fee5ba0927f557fe9f2dba0e50bb838109a
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-19 06:37:43 -0700

    Automated Code Change
    PiperOrigin-RevId: 821311426

--- tensorflow/examples/custom_ops_doc/multiplex_3/multiplex_3_kernel.cc
+++ tensorflow/examples/custom_ops_doc/multiplex_3/multiplex_3_kernel.cc
@@ -209,7 +209,7 @@ class MultiplexSparseOp : public OpKernel {
                              indices_tensor.shape().DebugString(),
                              " values: ", values_tensor.shape().DebugString());
     }
-    return OkStatus();
+    return absl::OkStatus();
   }
 };
 
