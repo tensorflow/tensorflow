0c0947cea658b39f07f37156640ad9986f8cdfa2
Ilya Tikhonovskiy <loislo@google.com>
2025-10-24 05:29:10 -0700

    [XLA:GPU] Initialize PrecisionConfig for ScaledDot in composite rewriter.
    Explicitly set the operand precisions to `PrecisionConfig::DEFAULT` when creating a `ScaledDot` instruction from a composite call.
    
    PiperOrigin-RevId: 823488638

--- third_party/xla/xla/service/gpu/transforms/composite_rewriter.cc
+++ third_party/xla/xla/service/gpu/transforms/composite_rewriter.cc
@@ -120,11 +120,13 @@ absl::StatusOr<bool> CompositeRewriter::RewriteComputation(
     TF_ASSIGN_OR_RETURN(
         DotDimensionNumbers dot_dimension_numbers,
         ParseDimensionNumbers(frontend_attrs.at("composite.attributes")));
+    PrecisionConfig precision{};
+    precision.mutable_operand_precision()->Resize(2, PrecisionConfig::DEFAULT);
     auto* scaled_dot =
         computation->AddInstruction(HloInstruction::CreateScaledDot(
             call->shape(), call->mutable_operand(0), call->mutable_operand(1),
             call->mutable_operand(2), call->mutable_operand(3),
-            dot_dimension_numbers, PrecisionConfig{}));
+            dot_dimension_numbers, precision));
     TF_RETURN_IF_ERROR(call->ReplaceAllUsesWith(scaled_dot));
     TF_RETURN_IF_ERROR(computation->RemoveInstruction(call));
     changed = true;
