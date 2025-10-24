7a62801cdb027980289a461d6d56a436da8abf06
Zixuan Jiang <zixuanjiang@google.com>
2025-10-24 13:45:38 -0700

    Remove temporary Shardy error messages.
    cl/784549550 was submitted 100 days ago. Now that it is stable, we remove the error messages.
    
    Reverts 0b9012dc05ee31be0b713801e6254f874127d1aa
    
    PiperOrigin-RevId: 823651646

--- third_party/xla/xla/service/spmd/shardy/round_trip_common/import_sdy_custom_calls.cc
+++ third_party/xla/xla/service/spmd/shardy/round_trip_common/import_sdy_custom_calls.cc
@@ -75,14 +75,7 @@ mlir::LogicalResult rewriteShardingCustomCall(
   }
   TensorShardingAttr sharding = mlir::sdy::getSharding(op->getResult(0));
   if (!sharding) {
-    op.emitError()
-        << "expected CustomCallOp with a sharding attribute. "
-        // TODO: b/432201708 - Remove this once Shardy is stable in JAX.
-        << "If you are a JAX user hitting this, please file a bug against the "
-        << "OpenXLA Shardy team. This shouldn't happen to you. One of the "
-        << "possible bugs is the model has been lowered targeting GSPMD, but "
-        << "Shardy was then enabled in XLA.";
-
+    op.emitError() << "expected CustomCallOp with a sharding attribute";
     return mlir::failure();
   }
 
