9ec8d8ece3ad9e7f8379406d263330a5beb65d5b
Alexander Grund <alexander.grund@tu-dresden.de>
2025-10-27 04:04:23 -0700

    PR #31886: Fix libdevice search
    Imported from GitHub PR https://github.com/openxla/xla/pull/31886
    
    📝 Summary of Changes
    This enhances the search for the CUDA libdevice path:
    - Fix an invalid empty path added when `TF_CUDA_TOOLKIT_PATH` which may be empty
    - Fix invalid paths based on runtime folders: `runfiles_dir.substr(0, runfiles_ind + runfiles_suffix.length())` is not meaningful when `runfiles_ind` isn't valid, i.e. `std::string::npos`
    - Add `$CUDA_HOME` to the search paths. This is also used in TensorFlow already
    
    🎯 Justification
    Without this the libdevice file won't be found if CUDA isn't installed in a standard location or e.g. an updated version is available in a different location.
    This is the case for e.g. HPC systems where multiple CUDA versions are available side-by-side.
    
    🚀 Kind of Contribution
    🐛 Bug Fix, ♻️ Cleanup
    
    Fixes #28590
    
    🧪 Unit Tests:
    
    Simple test that when `CandidateCudaRoots` returns anything it contains `$CUDA_HOME`
    
    Copybara import of the project:
    
    --
    01788b896900717ee916377a71d5c14963e0176d by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Fix libdevice search when outside test environment
    
    When there is no `runfiles_suffix` the `rfind` returns
    `std::string::npos` which should be handled to not add meaningless paths.
    
    --
    900715a846102bacdfc7688f14713cbe6101506d by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Use `$CUDA_HOME` when searching for libdevice.
    
    With a CUDA installed to a non-default location XLA/TF fails with:
    > gpu_backend_lib.cc:579] Can't find libdevice directory ${CUDA_DIR}/nvvm/libdevice. This may result in compilation or runtime failures, if the program we try to run uses routines from libdevice.
    > Searched for CUDA in the following directories:
    >   ./cuda_sdk_lib
    >   /builddir/TensorFlow/TensorFlow-2.x_mnist-test.py.runfiles/cuda_nvcc
    >   /buildi/cuda_nvcc
    >
    >   /usr/local/cuda
    >   /software/TensorFlow/lib/python3.12/site-packages/tensorflow/python/platform/../../../nvidia/cuda_nvcc
    >   /software/TensorFlow/lib/python3.12/site-packages/tensorflow/python/platform/../../../../nvidia/cuda_nvcc
    >   /software/TensorFlow/lib/python3.12/site-packages/tensorflow/python/platform/../../cuda
    
    Consider $CUDA_HOME as an additional location after the runfiles dirs (used for tests)
    
    --
    905d0596d199598036032f0f84b4487e9afd2bef by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Don't add empty TF_CUDA_TOOLKIT_PATH to libdevice search
    
    At least in some environments that define is the empty string which
    doesn't make sense to add to the search paths.
    Add a check for that.
    
    --
    23eb59bfabd570caabf0b9ec3515233f46a4fae7 by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Add test for $CUDA_HOME in CandidateCudaRoots
    
    --
    a8c215bc222b4ba8581f2f44549613ebd59b9cbb by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Add braces to loops/conditions
    
    --
    39efc67f8b1d44e131f993c8040b7eb69ff52f0c by Alexander Grund <alexander.grund@tu-dresden.de>:
    
    Use kIsOpenSource in skip condition
    
    Merging this change closes #31886
    
    PiperOrigin-RevId: 824450284

--- third_party/xla/xla/tsl/platform/default/cuda_root_path.cc
+++ third_party/xla/xla/tsl/platform/default/cuda_root_path.cc
@@ -56,14 +56,23 @@ std::vector<std::string> CandidateCudaRoots() {
   // The CUDA candidate root for python targets.
   std::string runfiles_dir = tsl::Env::Default()->GetRunfilesDir();
   std::size_t runfiles_ind = runfiles_dir.rfind(runfiles_suffix);
-  for (const std::string& cuda_dir_name : cuda_dir_names) {
-    std::string cuda_dir = io::JoinPath(
-        runfiles_dir.substr(0, runfiles_ind + runfiles_suffix.length()),
-        cuda_dir_name);
-    roots.push_back(cuda_dir);
+  if (runfiles_ind != std::string::npos) {
+    for (const std::string& cuda_dir_name : cuda_dir_names) {
+      std::string cuda_dir = io::JoinPath(
+          runfiles_dir.substr(0, runfiles_ind + runfiles_suffix.length()),
+          cuda_dir_name);
+      roots.push_back(cuda_dir);
+    }
   }
 
-  roots.push_back(TF_CUDA_TOOLKIT_PATH);
+  const char* cuda_home = getenv("CUDA_HOME");
+  if (cuda_home) {
+    roots.emplace_back(cuda_home);
+  }
+  std::string cuda_toolkit_path = TF_CUDA_TOOLKIT_PATH;
+  if (!cuda_toolkit_path.empty()) {
+    roots.push_back(std::move(cuda_toolkit_path));
+  }
   roots.emplace_back(std::string("/usr/local/cuda"));
   roots.emplace_back(std::string("/opt/cuda"));
 
@@ -101,12 +110,15 @@ std::vector<std::string> CandidateCudaRoots() {
     // $CONDA_PREFIX/lib/python3.12/site-packages/pkg_name, so if we want
     // to add $CONDA_PREFIX to the candidate roots dirs we need to add
     // ../../../..
-    for (auto path : {"../../../..", "../../../../.."})
+    for (auto path : {"../../../..", "../../../../.."}) {
       roots.emplace_back(io::JoinPath(dir, path));
+    }
   }
 #endif  // defined(PLATFORM_POSIX) && !defined(__APPLE__)
 
-  for (auto root : roots) VLOG(3) << "CUDA root = " << root;
+  for (auto root : roots) {
+    VLOG(3) << "CUDA root = " << root;
+  }
   return roots;
 #else   // !defined(PLATFORM_GOOGLE)
   return {};
