af521505f3f5291bc512739a1bb9703f9bae5869
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-26 21:51:24 -0700

    Check prepack info before reusing the GPU memory in shared memory manager
    PiperOrigin-RevId: 824347863

--- tensorflow/lite/core/interpreter_builder.cc
+++ tensorflow/lite/core/interpreter_builder.cc
@@ -725,7 +725,8 @@ TfLiteStatus InterpreterBuilder::ParseTensors(
       if (subgraph->SetTensorParametersReadOnly(
               i, type, get_name(tensor), dims, quantization, buffer_ptr,
               buffer_size, allocation_, sparsity,
-              /*buffer_identifier=*/tensor->buffer()) != kTfLiteOk) {
+              /*buffer_identifier=*/tensor->buffer(),
+              /*external_buffer_id=*/tensor->external_buffer()) != kTfLiteOk) {
         TF_LITE_REPORT_ERROR(error_reporter_,
                              "Tensor %d is invalidly specified in schema.\n",
                              i);
