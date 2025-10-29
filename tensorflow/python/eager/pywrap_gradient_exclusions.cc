08c86870c6362e95ca22030b710be3eface6f5df
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-29 03:00:56 -0700

    Automated Code Change
    PiperOrigin-RevId: 825458925

--- tensorflow/python/eager/pywrap_gradient_exclusions.cc
+++ tensorflow/python/eager/pywrap_gradient_exclusions.cc
@@ -49,7 +49,7 @@ auto OpGradientInfoInit(const T &a) {
 }  // namespace
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
-    const tensorflow::string &op_name) {
+    const std::string& op_name) {
   static std::array<OpIndexInfo, 367> a = {{
       {"Acosh"},
       {"AllToAll", 1, {0}},
@@ -429,7 +429,7 @@ absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedInputIndices(
 }
 
 absl::optional<tensorflow::gtl::FlatSet<int>> OpGradientUnusedOutputIndices(
-    const tensorflow::string &op_name) {
+    const std::string& op_name) {
   static std::array<OpIndexInfo, 486> a = {{
       {"Abs"},
       {"AccumulateNV2"},
