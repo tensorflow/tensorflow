d7d9bf76fbd2bc47ad44fea3d34c6bd7566941e6
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-29 02:35:15 -0700

    Automated Code Change
    PiperOrigin-RevId: 825450841

--- tensorflow/core/kernels/linalg/matrix_solve_op.cc
+++ tensorflow/core/kernels/linalg/matrix_solve_op.cc
@@ -66,8 +66,9 @@ class MatrixSolveOp : public LinearAlgebraOp<Scalar> {
     double rows = static_cast<double>(input_matrix_shapes[0].dim_size(0));
     double num_rhss = static_cast<double>(input_matrix_shapes[1].dim_size(1));
     double cost = rows * rows * (rows + num_rhss);
-    return cost >= static_cast<double>(kint64max) ? kint64max
-                                                  : static_cast<int64_t>(cost);
+    return cost >= static_cast<double>(std::numeric_limits<int64_t>::max())
+               ? std::numeric_limits<int64_t>::max()
+               : static_cast<int64_t>(cost);
   }
 
   bool EnableInputForwarding() const final { return false; }
