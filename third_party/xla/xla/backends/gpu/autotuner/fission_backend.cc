d912510fb4db247f219bba63956102a9fc9d870f
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-03 10:08:39 -0800

    [Autotuner] Add proxy FissionBackend for GPU autotuner.
    - It breaks up the current combined fission backend. Autotuner users can register Fission wrapping specific codegen backends as per the need.
    - This structure seemlessly fit with the autotuner, and hence is easy to discard when we don't want to do fissions.
    - The Backend creation is a bit messy, but we can create a factory method to hide the details.
    
    PiperOrigin-RevId: 827547707

---
+++ third_party/xla/xla/backends/gpu/autotuner/fission_backend.cc
@@ -0,0 +1,170 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "xla/backends/gpu/autotuner/fission_backend.h"
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "xla/backends/autotuner/codegen_backend.h"
+#include "xla/hlo/ir/hlo_casting_utils.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_instructions.h"
+#include "xla/hlo/ir/hlo_opcode.h"
+#include "xla/hlo/pass/hlo_pass_pipeline.h"
+#include "xla/service/compiler.h"
+#include "xla/service/gpu/transforms/priority_fusion.h"
+#include "xla/service/hlo_cost_analysis.h"
+#include "xla/tools/hlo_decomposer.h"
+#include "xla/tsl/platform/errors.h"
+#include "xla/tsl/platform/statusor.h"
+
+namespace xla {
+
+namespace gpu {
+
+namespace {
+
+// Replaces the fusion instruction with the instructions from the fissioned
+// computation.
+absl::Status InlineFissionedComputation(HloInstruction* fusion_instr,
+                                        HloComputation* fissioned_computation) {
+  absl::flat_hash_map<const HloInstruction*, HloInstruction*>
+      cloned_instructions;
+  HloComputation* parent_computation = fusion_instr->parent();
+
+  for (HloInstruction* instruction_to_clone :
+       fissioned_computation->MakeInstructionPostOrder()) {
+    if (instruction_to_clone->opcode() == HloOpcode::kParameter) {
+      cloned_instructions[instruction_to_clone] = fusion_instr->mutable_operand(
+          instruction_to_clone->parameter_number());
+      continue;
+    }
+
+    std::vector<HloInstruction*> new_operands;
+    for (const HloInstruction* operand : instruction_to_clone->operands()) {
+      new_operands.push_back(cloned_instructions.at(operand));
+    }
+    HloInstruction* new_instruction = parent_computation->AddInstruction(
+        instruction_to_clone->CloneWithNewOperands(
+            instruction_to_clone->shape(), new_operands));
+    cloned_instructions[instruction_to_clone] = new_instruction;
+  }
+  HloInstruction* new_root =
+      cloned_instructions.at(fissioned_computation->root_instruction());
+  return parent_computation->ReplaceInstruction(fusion_instr, new_root);
+}
+
+}  // namespace
+
+absl::StatusOr<std::vector<std::unique_ptr<BackendConfig>>>
+FissionBackend::GetSupportedConfigs(const HloInstruction& instr) {
+  if (!IsSupported(instr)) {
+    return std::vector<std::unique_ptr<BackendConfig>>();
+  }
+  TF_ASSIGN_OR_RETURN(std::unique_ptr<HloModule> hlo_module,
+                      GetFissionedAndRewrittenModule(instr));
+  TF_ASSIGN_OR_RETURN(HloInstruction * supported_instr,
+                      FindFirstSupportedInstruction(hlo_module.get()));
+  return codegen_backend_->GetSupportedConfigs(*supported_instr);
+
+  return std::vector<std::unique_ptr<BackendConfig>>();
+}
+
+absl::StatusOr<std::unique_ptr<BackendConfig>> FissionBackend::GetDefaultConfig(
+    const HloInstruction& instr) {
+  if (!IsSupported(instr)) {
+    return absl::InvalidArgumentError("Not a fusion instruction.");
+  }
+  TF_ASSIGN_OR_RETURN(std::unique_ptr<HloModule> hlo_module,
+                      GetFissionedAndRewrittenModule(instr));
+  TF_ASSIGN_OR_RETURN(HloInstruction * supported_instr,
+                      FindFirstSupportedInstruction(hlo_module.get()));
+  return codegen_backend_->GetDefaultConfig(*supported_instr);
+
+  return absl::InvalidArgumentError("No supported configs found.");
+}
+
+absl::StatusOr<std::unique_ptr<HloModule>> FissionBackend::RunHloPasses(
+    std::unique_ptr<HloModule> hlo_module,
+    const Compiler::CompileOptions& options) {
+  TF_ASSIGN_OR_RETURN(
+      std::unique_ptr<HloModule> module,
+      codegen_backend_->RunHloPasses(std::move(hlo_module), options));
+
+  // Run priority fusion to fuse the fissioned HLOs.
+  HloCostAnalysis::Options priority_fusion_options;
+  priority_fusion_options.count_multiple_input_accesses = true;
+  // TODO: b/407494653 - Get rid of PriorityFusion.
+  PriorityFusion priority_fusion(
+      /*thread_pool=*/nullptr, target_config().device_description,
+      priority_fusion_options, symbolic_expr_context_);
+  TF_RETURN_IF_ERROR(priority_fusion.Run(module.get()).status());
+  return module;
+}
+
+absl::Status FissionBackend::ApplyConfig(HloInstruction& instr,
+                                         const BackendConfig& config) {
+  TF_ASSIGN_OR_RETURN(std::unique_ptr<HloModule> hlo_module,
+                      GetFissionedAndRewrittenModule(instr));
+  TF_ASSIGN_OR_RETURN(HloInstruction * supported_instr,
+                      FindFirstSupportedInstruction(hlo_module.get()));
+  TF_RETURN_IF_ERROR(codegen_backend_->ApplyConfig(*supported_instr, config));
+  return InlineFissionedComputation(&instr, hlo_module->entry_computation());
+}
+
+bool FissionBackend::IsSupported(const HloInstruction& instr) {
+  return instr.opcode() == HloOpcode::kFusion;
+}
+
+absl::StatusOr<std::unique_ptr<HloModule>>
+FissionBackend::GetFissionedAndRewrittenModule(
+    const HloInstruction& fusion_instr) {
+  const auto* fusion = Cast<HloFusionInstruction>(&fusion_instr);
+  std::unique_ptr<HloModule> hlo_module =
+      ExtractComputationIntoNewModule(*fusion->called_computation());
+  TF_RETURN_IF_ERROR(rewriter_pipeline_->Run(hlo_module.get()).status());
+  return hlo_module;
+}
+
+absl::StatusOr<HloInstruction*> FissionBackend::FindFirstSupportedInstruction(
+    const HloModule* module) {
+  std::vector<HloInstruction*> supported_instructions;
+  for (HloComputation* computation : module->computations()) {
+    for (HloInstruction* instruction : computation->instructions()) {
+      if (codegen_backend_->IsSupported(*instruction)) {
+        supported_instructions.push_back(instruction);
+      }
+    }
+  }
+  if (supported_instructions.empty()) {
+    return absl::InvalidArgumentError("No supported instructions found.");
+  }
+  if (supported_instructions.size() > 1) {
+    LOG(WARNING) << "Backend " << name()
+                 << " found multiple supported instructions found. Using the "
+                    "first one.";
+  }
+  return supported_instructions[0];
+}
+
+}  // namespace gpu
+
+}  // namespace xla
---
+++ third_party/xla/xla/backends/gpu/autotuner/fission_backend.h
@@ -0,0 +1,88 @@
+/* Copyright 2025 The OpenXLA Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef XLA_BACKENDS_GPU_AUTOTUNER_FISSION_BACKEND_H_
+#define XLA_BACKENDS_GPU_AUTOTUNER_FISSION_BACKEND_H_
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "xla/backends/autotuner/codegen_backend.h"
+#include "xla/backends/gpu/autotuner/gpu_codegen_backend.h"
+#include "xla/hlo/analysis/symbolic_expr.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/pass/hlo_pass_pipeline.h"
+#include "xla/service/compiler.h"
+#include "xla/stream_executor/stream_executor.h"
+
+namespace xla::gpu {
+
+// A proxy backend that wraps an actual codegen backend. The `rewriter_pipeline`
+// is used to transform unfused instructions to retarget them for the underlying
+// codegen backend.
+// For the get/apply config operations, the proxy backend only operates on the
+// *first* supported instruction by the underlying backend, found in the unfused
+// and transmormed HLO.
+// The assumption is that there is only one operation of interest in the fusion
+// (e.g., a 'dot' in a gemm fusion).
+class FissionBackend : public GpuCodegenBackend {
+ public:
+  FissionBackend(const DebugOptions* debug_options, Compiler* compiler,
+                 const Compiler::TargetConfig* target_config,
+                 std::unique_ptr<GpuCodegenBackend> backend,
+                 std::unique_ptr<HloPassPipeline> rewriter_pipeline,
+                 SymbolicExprContext* symbolic_expr_context,
+                 stream_executor::StreamExecutor* stream_executor = nullptr)
+      : GpuCodegenBackend(absl::StrCat(backend->name(), "_fission"),
+                          debug_options, compiler, target_config,
+                          stream_executor),
+        rewriter_pipeline_(std::move(rewriter_pipeline)),
+        codegen_backend_(std::move(backend)),
+        symbolic_expr_context_(symbolic_expr_context) {}
+  ~FissionBackend() override = default;
+
+  absl::StatusOr<std::vector<std::unique_ptr<BackendConfig>>>
+  GetSupportedConfigs(const HloInstruction& instr) override;
+
+  absl::StatusOr<std::unique_ptr<BackendConfig>> GetDefaultConfig(
+      const HloInstruction& instr) override;
+
+  absl::StatusOr<std::unique_ptr<HloModule>> RunHloPasses(
+      std::unique_ptr<HloModule> hlo_module,
+      const Compiler::CompileOptions& options) override;
+
+  absl::Status ApplyConfig(HloInstruction& instr,
+                           const BackendConfig& config) override;
+
+  bool IsSupported(const HloInstruction& instr) override;
+
+ private:
+  absl::StatusOr<std::unique_ptr<HloModule>> GetFissionedAndRewrittenModule(
+      const HloInstruction& fusion_instr);
+  absl::StatusOr<HloInstruction*> FindFirstSupportedInstruction(
+      const HloModule* module);
+
+  std::unique_ptr<HloPassPipeline> rewriter_pipeline_;
+  std::unique_ptr<GpuCodegenBackend> codegen_backend_;
+  SymbolicExprContext* symbolic_expr_context_;
+};
+
+}  // namespace xla::gpu
+
+#endif  // XLA_BACKENDS_GPU_AUTOTUNER_FISSION_BACKEND_H_
