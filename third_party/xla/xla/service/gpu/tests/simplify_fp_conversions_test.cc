b3d10ba7f1217c9dd79f1d5942413f8b7085f2f0
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-04 19:15:52 -0800

    Automated Code Change
    PiperOrigin-RevId: 828239090

--- third_party/xla/xla/service/gpu/tests/simplify_fp_conversions_test.cc
+++ third_party/xla/xla/service/gpu/tests/simplify_fp_conversions_test.cc
@@ -13,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <variant>
-
 #include <gtest/gtest.h>
 #include "absl/strings/string_view.h"
 #include "xla/tests/hlo_test_base.h"
