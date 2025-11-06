ff9c53bc3a5ba2d946861fde5fe5a0a5778d4d13
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-05 20:54:42 -0800

    Automated Code Change
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/33106 from dimvar:cudnn-cuda-graph b2f357e89343eaf5325df9b88b68acd04f6fdf87
    PiperOrigin-RevId: 828762717

--- third_party/xla/xla/stream_executor/engine_options.h
+++ third_party/xla/xla/stream_executor/engine_options.h
@@ -13,25 +13,34 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ------------------------------------------------------------------------------*/
 
-#ifndef XLA_STREAM_EXECUTOR_NUMERIC_OPTIONS_H_
-#define XLA_STREAM_EXECUTOR_NUMERIC_OPTIONS_H_
+#ifndef XLA_STREAM_EXECUTOR_ENGINE_OPTIONS_H_
+#define XLA_STREAM_EXECUTOR_ENGINE_OPTIONS_H_
 
 namespace stream_executor {
 
-// Options that specify the numeric behavior of operations like matrix
-// multiplications and convolutions
-struct NumericOptions {
-  NumericOptions(bool require_determinism, bool allow_tf32)
-      : require_determinism(require_determinism), allow_tf32(allow_tf32) {}
+// Options (used when calling math libraries) that specify the behavior of
+// operations like matrix multiplications and convolutions.
+struct EngineOptions {
+  EngineOptions(bool require_determinism, bool allow_tf32,
+                bool require_command_buffer)
+      : require_determinism(require_determinism),
+        allow_tf32(allow_tf32),
+        require_command_buffer(require_command_buffer) {}
 
-  NumericOptions() : require_determinism(false), allow_tf32(true) {}
+  EngineOptions()
+      : require_determinism(false),
+        allow_tf32(true),
+        require_command_buffer(false) {}
 
   // If true, the op must be deterministic
   bool require_determinism;
   // If true, float32 inputs can be rounded to TensorFloat-32 precision
   bool allow_tf32;
+  // If true, the execution plan selected must support command buffer
+  // construction.
+  bool require_command_buffer;
 };
 
 }  // namespace stream_executor
 
-#endif  // XLA_STREAM_EXECUTOR_NUMERIC_OPTIONS_H_
+#endif  // XLA_STREAM_EXECUTOR_ENGINE_OPTIONS_H_
