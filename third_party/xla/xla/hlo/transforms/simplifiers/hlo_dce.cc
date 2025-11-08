c66648c3bb8d1c3cada83c02d2310abc35171436
Sharad Vikram <sharadmv@google.com>
2025-11-07 17:48:43 -0800

    Allow DCE side-effecting custom calls
    PiperOrigin-RevId: 829639173

--- third_party/xla/xla/hlo/transforms/simplifiers/hlo_dce.cc
+++ third_party/xla/xla/hlo/transforms/simplifiers/hlo_dce.cc
@@ -51,6 +51,9 @@ namespace xla {
 
 namespace {
 
+const absl::string_view kDceSideEffectFrontendAttribute =
+    "xla_allow_dce_side_effecting_op";
+
 // Checks if the instruction is a removable while given
 // remove_cross_partition_collective_ops
 bool IsRemovableWhile(const HloInstruction* instruction,
@@ -208,7 +211,12 @@ bool CanRemoveInstruction(
                             !maybe_collective_op->constrain_layout();
     bool allow_while =
         IsRemovableWhile(instruction, remove_cross_partition_collective_ops);
-    if (!allow_collective && !allow_while) {
+    bool allow_custom_call = instruction->IsCustomCall("tpu_custom_call") &&
+                             instruction->frontend_attributes().map().contains(
+                                 kDceSideEffectFrontendAttribute) &&
+                             instruction->frontend_attributes().map().at(
+                                 kDceSideEffectFrontendAttribute) == "true";
+    if (!allow_collective && !allow_while && !allow_custom_call) {
       return false;
     }
   }
