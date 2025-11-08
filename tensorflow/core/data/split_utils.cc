f1f8e96f59d415a62575220789ee9a3b516440ce
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-07 22:56:35 -0800

    Automated Code Change
    PiperOrigin-RevId: 829714998

--- tensorflow/core/data/split_utils.cc
+++ tensorflow/core/data/split_utils.cc
@@ -82,7 +82,7 @@ absl::Status IndexSplitProvider::Restore(
 int64_t IndexSplitProvider::Cardinality() const {
   // RandomDataset uses kint64max to simulate infinite splits.
   // See RandomDatasetOp::Dataset::MakeSplitProviders.
-  if (n_ == tsl::kint64max) {
+  if (n_ == std::numeric_limits<int64_t>::max()) {
     return kInfiniteCardinality;
   }
   return n_;
