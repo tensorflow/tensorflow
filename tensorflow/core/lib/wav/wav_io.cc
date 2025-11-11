9dc23986bc9db733bd87cb6fa9f434ed11e21124
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-10 17:54:04 -0800

    Automated Code Change
    PiperOrigin-RevId: 830669765

--- tensorflow/core/lib/wav/wav_io.cc
+++ tensorflow/core/lib/wav/wav_io.cc
@@ -21,8 +21,13 @@ limitations under the License.
 #include <string.h>
 
 #include <algorithm>
+#include <cstdint>
+#include <limits>
+#include <string>
+#include <vector>
 
 #include "absl/base/casts.h"
+#include "absl/status/status.h"
 #include "tensorflow/core/lib/core/coding.h"
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/platform/byte_order.h"
