f314034eb0c2fedc77a179f3c5fa83b5e8515cf9
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-13 20:28:02 -0800

    Automated Code Change
    PiperOrigin-RevId: 832126423

--- third_party/xla/xla/python/ifrt/ir/ifrt_ir_program_serdes_test.cc
+++ third_party/xla/xla/python/ifrt/ir/ifrt_ir_program_serdes_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
 #include "absl/strings/string_view.h"
 #include "llvm/Support/raw_ostream.h"
 #include "mlir/IR/BuiltinOps.h"
