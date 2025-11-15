a56c67183a91aa0c687cf0b78bca6eed046c7745
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-14 23:33:09 -0800

    Automated Code Change
    PiperOrigin-RevId: 832604639

--- third_party/xla/xla/codegen/intrinsic/type.cc
+++ third_party/xla/xla/codegen/intrinsic/type.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "xla/primitive_util.h"
 #include "xla/service/llvm_ir/llvm_util.h"
 #include "xla/util.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla::codegen::intrinsics {
 
