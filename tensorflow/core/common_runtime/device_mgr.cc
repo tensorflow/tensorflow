990f6266a3931da9218b770050d71a7c441b7d0f
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-17 18:41:40 -0800

    Automated Code Change
    PiperOrigin-RevId: 833583643

--- tensorflow/core/common_runtime/device_mgr.cc
+++ tensorflow/core/common_runtime/device_mgr.cc
@@ -15,9 +15,6 @@ limitations under the License.
 
 #include "tensorflow/core/common_runtime/device_mgr.h"
 
-#include <memory>
-#include <vector>
-
 #include "tensorflow/core/common_runtime/local_device.h"
 #include "tensorflow/core/framework/device_attributes.pb.h"
 #include "tensorflow/core/lib/core/errors.h"
