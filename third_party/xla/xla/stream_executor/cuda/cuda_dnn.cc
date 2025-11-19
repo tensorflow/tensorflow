98a39e096618e58608a6c773a26c1e84dd66e738
Joshua Lang <joshualang@google.com>
2025-11-19 11:39:54 -0800

    Qualify usage of cudnnGetLastErrorString to versions that support it.
    PiperOrigin-RevId: 834379808

--- third_party/xla/xla/stream_executor/cuda/cuda_dnn.cc
+++ third_party/xla/xla/stream_executor/cuda/cuda_dnn.cc
@@ -5432,11 +5432,13 @@ absl::Status CreateOpRunners(
                     .setEngineConfig(filtered_configs[i], op_graph->getTag())
                     .build();
     if (plan.get_status() != CUDNN_STATUS_SUCCESS) {
+#if CUDNN_VERSION >= 90000
       std::string message(65535, '\0');
       cudnnGetLastErrorString(message.data(), message.size());
       VLOG(4) << "Failed building ExecutionPlan: found error: "
               << cudnnGetErrorString(plan.get_status())
               << " with message: " << message;
+#endif
       continue;
     }
 
