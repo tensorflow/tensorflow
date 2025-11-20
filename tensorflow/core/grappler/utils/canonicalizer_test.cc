e4b7dfbb190b0ad6c18daf0a2540e6c84bc0beef
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-19 18:02:36 -0800

    Automated Code Change
    PiperOrigin-RevId: 834518762

--- tensorflow/core/grappler/utils/canonicalizer_test.cc
+++ tensorflow/core/grappler/utils/canonicalizer_test.cc
@@ -15,6 +15,8 @@ limitations under the License.
 
 #include "tensorflow/core/grappler/utils/canonicalizer.h"
 
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
--- tensorflow/core/grappler/utils/colocation_test.cc
+++ tensorflow/core/grappler/utils/colocation_test.cc
@@ -15,7 +15,10 @@ limitations under the License.
 
 #include "tensorflow/core/grappler/utils/colocation.h"
 
+#include "absl/status/status.h"
 #include "tensorflow/core/framework/function_testlib.h"
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/framework/node_def_builder.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
 #include "tensorflow/core/platform/test.h"
