81d4f107e7baf779ca90960f15d572077304ba40
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-20 18:28:23 -0800

    Automated Code Change
    PiperOrigin-RevId: 834994692

--- tensorflow/compiler/aot/codegen_test.cc
+++ tensorflow/compiler/aot/codegen_test.cc
@@ -19,7 +19,7 @@ limitations under the License.
 #include <string>
 #include <vector>
 
-#include "absl/memory/memory.h"
+#include "absl/status/status.h"
 #include "absl/strings/match.h"
 #include "absl/strings/string_view.h"
 #include "llvm/Support/TargetSelect.h"
