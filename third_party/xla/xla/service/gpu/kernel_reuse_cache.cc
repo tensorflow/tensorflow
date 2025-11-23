4e4965d1b0862bf1167ae72efc5630da92e4a1e8
A. Unique TensorFlower <gardener@tensorflow.org>
2025-11-22 17:40:56 -0800

    Automated Code Change
    PiperOrigin-RevId: 835715504

--- third_party/xla/xla/service/gpu/kernel_reuse_cache.cc
+++ third_party/xla/xla/service/gpu/kernel_reuse_cache.cc
@@ -30,6 +30,7 @@ limitations under the License.
 #include "xla/codegen/emitters/kernel_arguments.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/service/gpu/kernel_reuse_cache.pb.h"
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/status_macros.h"
 #include "xla/stream_executor/launch_dim.h"
