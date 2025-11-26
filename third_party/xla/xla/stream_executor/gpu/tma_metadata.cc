d1d2d9890a4a65e9f4a2f1b626bfcae545d33418
Mohammed Anany <manany@google.com>
2025-11-26 08:58:16 -0800

    [XLA:GPU/TMA] Disable TMA on B200 due to a timeout failure.
    PiperOrigin-RevId: 837146719

--- third_party/xla/xla/stream_executor/gpu/tma_metadata.cc
+++ third_party/xla/xla/stream_executor/gpu/tma_metadata.cc
@@ -460,11 +460,12 @@ absl::StatusOr<TmaMetadata> TmaMetadata::FromProto(
   return metadata;
 }
 
+// TODO(b/463912789): Re-enable TMA for Blackwell once the bug is fixed.
 bool IsTmaAvailableForDevice(
     const stream_executor::DeviceDescription& device_info) {
   if (auto* cuda_cc =
           device_info.gpu_compute_capability().cuda_compute_capability()) {
-    return cuda_cc->IsAtLeastHopper();
+    return cuda_cc->IsHopper();
   }
   return false;
 }
