3f9cd141c4a4a1faf8c892d73f2634548f7b31ef
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-01 01:34:52 -0800

    Automated Code Change
    PiperOrigin-RevId: 838658706

--- third_party/xla/xla/hlo/tools/hlo_diff/hlo_gumgraph_diff.cc
+++ third_party/xla/xla/hlo/tools/hlo_diff/hlo_gumgraph_diff.cc
@@ -23,8 +23,6 @@
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/strings/string_view.h"
-#include "absl/types/span.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/tools/hlo_diff/graph/hlo_gumgraph.h"
