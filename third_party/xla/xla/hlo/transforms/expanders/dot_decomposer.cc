5fb03342f2e1b7ffdd6b47daddf20062ab62f68c
Thomas Joerg <tjoerg@google.com>
2025-12-01 05:52:50 -0800

    [XLA:GPU] Pre-factor DotDecomposer: Extract repetitive code for handling LHS and RHS operands during dot canonicalization.
    Also use a consistent naming scheme using the prefixes `original` and `canonical` to refer to the existing dot and the canonical replacement being built.
    
    PiperOrigin-RevId: 838735497

--- third_party/xla/xla/hlo/transforms/expanders/dot_decomposer.cc
+++ third_party/xla/xla/hlo/transforms/expanders/dot_decomposer.cc
@@ -15,7 +15,6 @@ limitations under the License.
 
 #include "xla/hlo/transforms/expanders/dot_decomposer.h"
 
-#include <algorithm>
 #include <cstdint>
 #include <memory>
 #include <utility>
@@ -33,18 +32,120 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_instructions.h"
 #include "xla/hlo/ir/hlo_opcode.h"
-#include "xla/layout_util.h"
-#include "xla/service/shape_inference.h"
 #include "xla/shape.h"
 #include "xla/shape_util.h"
+#include "xla/tsl/platform/errors.h"
+#include "xla/util.h"
 #include "xla/xla_data.pb.h"
-#include "tsl/platform/errors.h"
-#include "tsl/platform/logging.h"
-#include "tsl/platform/statusor.h"
 
 namespace xla {
 
 namespace {
+
+bool IsCanonical(Shape operand_shape,
+                 absl::Span<const int64_t> contracting_dimensions,
+                 absl::Span<const int64_t> batch_dimensions) {
+  // A dot is not canonical if there is more than one contracting dimension.
+  if (contracting_dimensions.size() != 1) {
+    return false;
+  }
+  // A dot is not canonical if it has more than one non-contracting
+  // dimension.
+  if (batch_dimensions.size() + 2 < operand_shape.dimensions().size()) {
+    return false;
+  }
+  if (batch_dimensions.empty() && contracting_dimensions.empty()) {
+    return false;
+  }
+  // Check that batch dims, if present, are canonical.
+  std::vector<int64_t> canonical_batch_dims(batch_dimensions.size());
+  absl::c_iota(canonical_batch_dims, 0);
+  if (!absl::c_equal(batch_dimensions, canonical_batch_dims)) {
+    return false;
+  }
+  return true;
+}
+
+HloInstruction* CanonicalizeOperand(
+    HloInstruction* operand, absl::Span<const int64_t> original_batch_dims,
+    absl::Span<const int64_t> original_contracting_dims,
+    absl::Span<const int64_t> canonical_batch_dims,
+    const std::vector<bool>& canonical_batch_dynamic_dims,
+    tsl::protobuf::RepeatedField<int64_t>* canonical_contracting_dims,
+    bool contracting_dim_as_most_minor) {
+  const Shape& operand_shape = operand->shape();
+  const int64_t rank = operand_shape.dimensions().size();
+  const int64_t num_non_contracting_dims =
+      rank - original_batch_dims.size() - original_contracting_dims.size();
+  std::vector<int64_t> non_contracting_dims;
+  non_contracting_dims.reserve(num_non_contracting_dims);
+  int64_t contracting_size = 1;
+  bool contracting_dynamic = false;
+  int64_t non_contracting_size = 1;
+  bool non_contracting_dynamic = false;
+  for (int64_t i = 0; i < rank; ++i) {
+    if (absl::c_linear_search(original_contracting_dims, i)) {
+      contracting_size *= operand_shape.dimensions(i);
+      contracting_dynamic |= operand_shape.is_dynamic_dimension(i);
+    } else if (!absl::c_linear_search(original_batch_dims, i)) {
+      non_contracting_dims.push_back(i);
+      non_contracting_size *= operand_shape.dimensions(i);
+      non_contracting_dynamic |= operand_shape.is_dynamic_dimension(i);
+    }
+  }
+  std::vector<int64_t> permutation;
+  permutation.reserve(rank);
+  permutation.insert(permutation.end(), original_batch_dims.begin(),
+                     original_batch_dims.end());
+  if (contracting_dim_as_most_minor) {
+    permutation.insert(permutation.end(), non_contracting_dims.begin(),
+                       non_contracting_dims.end());
+    permutation.insert(permutation.end(), original_contracting_dims.begin(),
+                       original_contracting_dims.end());
+  } else {
+    permutation.insert(permutation.end(), original_contracting_dims.begin(),
+                       original_contracting_dims.end());
+    permutation.insert(permutation.end(), non_contracting_dims.begin(),
+                       non_contracting_dims.end());
+  }
+  HloComputation* computation = operand->parent();
+  HloInstruction* transpose_op = computation->AddInstruction(
+      HloInstruction::CreateTranspose(
+          ShapeUtil::PermuteDimensionsIgnoringLayout(permutation,
+                                                     operand_shape),
+          operand, permutation),
+      &operand->metadata());
+
+  std::vector<int64_t> reshape_dims(canonical_batch_dims.begin(),
+                                    canonical_batch_dims.end());
+  std::vector<bool> reshape_dynamic_dims(canonical_batch_dynamic_dims.begin(),
+                                         canonical_batch_dynamic_dims.end());
+  if (contracting_dim_as_most_minor) {
+    canonical_contracting_dims->Add(canonical_batch_dims.size() +
+                                    (non_contracting_size != 1 ? 1 : 0));
+    if (non_contracting_size != 1) {
+      reshape_dims.push_back(non_contracting_size);
+      reshape_dynamic_dims.push_back(non_contracting_dynamic);
+    }
+    reshape_dims.push_back(contracting_size);
+    reshape_dynamic_dims.push_back(contracting_dynamic);
+  } else {
+    canonical_contracting_dims->Add(canonical_batch_dims.size());
+    reshape_dims.push_back(contracting_size);
+    reshape_dynamic_dims.push_back(contracting_dynamic);
+    if (non_contracting_size != 1) {
+      reshape_dims.push_back(non_contracting_size);
+      reshape_dynamic_dims.push_back(non_contracting_dynamic);
+    }
+  }
+  // Reshape the contracting and non-contracting dimensions together.
+  return computation->AddInstruction(
+      HloInstruction::CreateReshape(
+          ShapeUtil::MakeShape(operand_shape.element_type(), reshape_dims,
+                               reshape_dynamic_dims),
+          transpose_op),
+      &transpose_op->metadata());
+}
 
 // Convert a dot into a canonical form;
 // * Non-contracting dimensions are reshaped together,
@@ -56,158 +157,84 @@ absl::Status CanonicalizeDot(HloDotInstruction* original_dot) {
   auto computation = original_dot->parent();
   const auto& original_dnums = original_dot->dot_dimension_numbers();
   const int64_t num_batch_dims = original_dnums.lhs_batch_dimensions_size();
-  const int64_t num_contracting_dims =
-      original_dnums.lhs_contracting_dimensions_size();
+  std::vector<int64_t> canonical_batch_dims;
+  canonical_batch_dims.reserve(num_batch_dims);
+  std::vector<bool> canonical_batch_dynamic_dims;
+  canonical_batch_dynamic_dims.reserve(num_batch_dims);
 
-  const auto& lhs_shape = original_dot->operand(0)->shape();
+  HloInstruction* lhs_operand = original_dot->mutable_operand(0);
+  HloInstruction* rhs_operand = original_dot->mutable_operand(1);
+  const auto& lhs_shape = lhs_operand->shape();
   const int64_t lhs_rank = lhs_shape.dimensions().size();
-  const int64_t num_lhs_non_contracting_dims =
-      lhs_rank - num_batch_dims - num_contracting_dims;
-
-  std::vector<int64_t> lhs_non_contracting_dims;
-  lhs_non_contracting_dims.reserve(num_lhs_non_contracting_dims);
-  int64_t lhs_contracting_size = 1;
-  bool lhs_contracting_dynamic = false;
-  int64_t lhs_non_contracting_size = 1;
-  bool lhs_non_contracting_dynamic = false;
-  std::vector<int64_t> batch_dim_sizes;
-  batch_dim_sizes.reserve(num_batch_dims);
-  std::vector<bool> batch_dynamic_dims;
-  batch_dynamic_dims.reserve(num_batch_dims);
   for (int64_t i = 0; i < lhs_rank; ++i) {
-    if (absl::c_linear_search(original_dnums.lhs_contracting_dimensions(), i)) {
-      lhs_contracting_size *= lhs_shape.dimensions(i);
-      lhs_contracting_dynamic |= lhs_shape.is_dynamic_dimension(i);
-    } else if (absl::c_linear_search(original_dnums.lhs_batch_dimensions(),
-                                     i)) {
-      batch_dim_sizes.push_back(lhs_shape.dimensions(i));
-      batch_dynamic_dims.push_back(lhs_shape.is_dynamic_dimension(i));
-    } else {
-      lhs_non_contracting_dims.push_back(i);
-      lhs_non_contracting_size *= lhs_shape.dimensions(i);
-      lhs_non_contracting_dynamic |= lhs_shape.is_dynamic_dimension(i);
+    if (absl::c_linear_search(original_dnums.lhs_batch_dimensions(), i)) {
+      canonical_batch_dims.push_back(lhs_shape.dimensions(i));
+      canonical_batch_dynamic_dims.push_back(lhs_shape.is_dynamic_dimension(i));
     }
   }
+  DotDimensionNumbers canonical_dnums;
+  std::vector<int64_t> canonical_dot_dims = canonical_batch_dims;
+  std::vector<bool> canonical_dot_dynamic_dims = canonical_batch_dynamic_dims;
+  for (int64_t i = 0; i < num_batch_dims; ++i) {
+    canonical_dnums.add_lhs_batch_dimensions(i);
+    canonical_dnums.add_rhs_batch_dimensions(i);
+  }
+
   // The canonical form of the lhs is
   // [BatchDims, NonContractingDimsProduct, ContractingsDimsProduct]
   // However, [ContractingDim, NonContractingDim] is considered canonical too.
   // If NonContractingDimsProduct is 1, it is omitted.
-  std::vector<int64_t> lhs_transpose;
-  lhs_transpose.reserve(lhs_rank);
-  lhs_transpose.insert(lhs_transpose.end(),
-                       original_dnums.lhs_batch_dimensions().begin(),
-                       original_dnums.lhs_batch_dimensions().end());
-  lhs_transpose.insert(lhs_transpose.end(), lhs_non_contracting_dims.begin(),
-                       lhs_non_contracting_dims.end());
-  lhs_transpose.insert(lhs_transpose.end(),
-                       original_dnums.lhs_contracting_dimensions().begin(),
-                       original_dnums.lhs_contracting_dimensions().end());
-  HloInstruction* lhs_operand = original_dot->mutable_operand(0);
-  HloInstruction* transposed_lhs = computation->AddInstruction(
-      HloInstruction::CreateTranspose(
-          ShapeUtil::PermuteDimensionsIgnoringLayout(lhs_transpose, lhs_shape),
-          lhs_operand, lhs_transpose),
-      &lhs_operand->metadata());
 
-  std::vector<int64_t> lhs_reshape_dims = batch_dim_sizes;
-  std::vector<bool> lhs_reshape_dynamic_dims = batch_dynamic_dims;
-  if (lhs_non_contracting_size != 1) {
-    lhs_reshape_dims.push_back(lhs_non_contracting_size);
-    lhs_reshape_dynamic_dims.push_back(lhs_non_contracting_dynamic);
-  }
-  lhs_reshape_dims.push_back(lhs_contracting_size);
-  lhs_reshape_dynamic_dims.push_back(lhs_contracting_dynamic);
-  // Reshape the contracting and non-contracting dimensions together.
-  HloInstruction* reshaped_lhs = computation->AddInstruction(
-      HloInstruction::CreateReshape(
-          ShapeUtil::MakeShape(lhs_shape.element_type(), lhs_reshape_dims,
-                               lhs_reshape_dynamic_dims),
-          transposed_lhs),
-      &transposed_lhs->metadata());
-
-  const auto& rhs_shape = original_dot->operand(1)->shape();
-  const int64_t rhs_rank = rhs_shape.dimensions().size();
-  const int64_t num_rhs_non_contracting_dims =
-      rhs_rank - num_batch_dims - num_contracting_dims;
-  std::vector<int64_t> rhs_non_contracting_dims;
-  rhs_non_contracting_dims.reserve(num_rhs_non_contracting_dims);
-  int64_t rhs_non_contracting_size = 1;
-  bool rhs_non_contracting_dynamic = false;
-  int64_t rhs_contracting_size = 1;
-  bool rhs_contracting_dynamic = false;
-  for (int64_t i = 0; i < rhs_rank; ++i) {
-    if (absl::c_linear_search(original_dnums.rhs_contracting_dimensions(), i)) {
-      rhs_contracting_size *= rhs_shape.dimensions(i);
-      rhs_contracting_dynamic |= rhs_shape.is_dynamic_dimension(i);
-    } else if (!absl::c_linear_search(original_dnums.rhs_batch_dimensions(),
-                                      i)) {
-      rhs_non_contracting_dims.push_back(i);
-      rhs_non_contracting_size *= rhs_shape.dimensions(i);
-      rhs_non_contracting_dynamic |= rhs_shape.is_dynamic_dimension(i);
-    }
+  HloInstruction* reshaped_lhs =
+      CanonicalizeOperand(lhs_operand, original_dnums.lhs_batch_dimensions(),
+                          original_dnums.lhs_contracting_dimensions(),
+                          canonical_batch_dims, canonical_batch_dynamic_dims,
+                          canonical_dnums.mutable_lhs_contracting_dimensions(),
+                          /*contracting_dim_as_most_minor=*/true);
+  const auto& canonical_lhs_shape = reshaped_lhs->shape();
+  const auto& canonical_lhs_non_contracting_dims =
+      GetNonContractingDims(canonical_lhs_shape.dimensions().size(),
+                            canonical_dnums.lhs_batch_dimensions(),
+                            canonical_dnums.lhs_contracting_dimensions());
+  // At this point of canonicalization, there are 0 or 1 non-contracting dims.
+  if (canonical_lhs_non_contracting_dims.size() == 1) {
+    int64_t lhs_non_contracting_dim = canonical_lhs_non_contracting_dims.at(0);
+    canonical_dot_dims.push_back(
+        canonical_lhs_shape.dimensions(lhs_non_contracting_dim));
+    canonical_dot_dynamic_dims.push_back(
+        canonical_lhs_shape.is_dynamic_dimension(lhs_non_contracting_dim));
   }
 
   // The canonical form of the rhs is
   // [BatchDims, ContractingsDimsProduct, NonContractingDimsProduct]
   // However, [NonContractingDim, ContractingDim] is considered canonical too.
   // If NonContractingDimsProduct is 1, it is omitted.
-  std::vector<int64_t> rhs_transpose;
-  rhs_transpose.reserve(rhs_rank);
-  rhs_transpose.insert(rhs_transpose.end(),
-                       original_dnums.rhs_batch_dimensions().begin(),
-                       original_dnums.rhs_batch_dimensions().end());
-  rhs_transpose.insert(rhs_transpose.end(),
-                       original_dnums.rhs_contracting_dimensions().begin(),
-                       original_dnums.rhs_contracting_dimensions().end());
-  rhs_transpose.insert(rhs_transpose.end(), rhs_non_contracting_dims.begin(),
-                       rhs_non_contracting_dims.end());
-  HloInstruction* rhs_operand = original_dot->mutable_operand(1);
-  HloInstruction* transposed_rhs = computation->AddInstruction(
-      HloInstruction::CreateTranspose(
-          ShapeUtil::PermuteDimensionsIgnoringLayout(rhs_transpose, rhs_shape),
-          rhs_operand, rhs_transpose),
-      &rhs_operand->metadata());
+  HloInstruction* reshaped_rhs =
+      CanonicalizeOperand(rhs_operand, original_dnums.rhs_batch_dimensions(),
+                          original_dnums.rhs_contracting_dimensions(),
+                          canonical_batch_dims, canonical_batch_dynamic_dims,
+                          canonical_dnums.mutable_rhs_contracting_dimensions(),
+                          /*contracting_dim_as_most_minor=*/false);
 
-  std::vector<int64_t> rhs_reshape_dims = batch_dim_sizes;
-  rhs_reshape_dims.push_back(rhs_contracting_size);
-  std::vector<bool> rhs_reshape_dynamic_dims = batch_dynamic_dims;
-  rhs_reshape_dynamic_dims.push_back(rhs_contracting_dynamic);
-  if (rhs_non_contracting_size != 1) {
-    rhs_reshape_dims.push_back(rhs_non_contracting_size);
-    rhs_reshape_dynamic_dims.push_back(rhs_non_contracting_dynamic);
+  const auto& canonical_rhs_shape = reshaped_rhs->shape();
+  const auto& canonical_rhs_non_contracting_dims =
+      GetNonContractingDims(canonical_rhs_shape.dimensions().size(),
+                            canonical_dnums.rhs_batch_dimensions(),
+                            canonical_dnums.rhs_contracting_dimensions());
+  // At this point of canonicalization, there are 0 or 1 non-contracting dims.
+  if (canonical_rhs_non_contracting_dims.size() == 1) {
+    int64_t rhs_non_contracting_dim = canonical_rhs_non_contracting_dims.at(0);
+    canonical_dot_dims.push_back(
+        canonical_rhs_shape.dimensions(rhs_non_contracting_dim));
+    canonical_dot_dynamic_dims.push_back(
+        canonical_rhs_shape.is_dynamic_dimension(rhs_non_contracting_dim));
   }
-  // Reshape the contracting and non-contracting dimensions together.
-  HloInstruction* reshaped_rhs = computation->AddInstruction(
-      HloInstruction::CreateReshape(
-          ShapeUtil::MakeShape(rhs_shape.element_type(), rhs_reshape_dims,
-                               rhs_reshape_dynamic_dims),
-          transposed_rhs),
-      &transposed_rhs->metadata());
-
-  std::vector<int64_t> dot_dims = batch_dim_sizes;
-  std::vector<bool> dot_dynamic_dims = batch_dynamic_dims;
-  if (lhs_non_contracting_size != 1) {
-    dot_dims.push_back(lhs_non_contracting_size);
-    dot_dynamic_dims.push_back(lhs_non_contracting_dynamic);
-  }
-  if (rhs_non_contracting_size != 1) {
-    dot_dims.push_back(rhs_non_contracting_size);
-    dot_dynamic_dims.push_back(rhs_non_contracting_dynamic);
-  }
-
-  DotDimensionNumbers dot_dnums;
-  for (int64_t i = 0; i < num_batch_dims; ++i) {
-    dot_dnums.add_lhs_batch_dimensions(i);
-    dot_dnums.add_rhs_batch_dimensions(i);
-  }
-  dot_dnums.add_lhs_contracting_dimensions(
-      num_batch_dims + (lhs_non_contracting_size != 1 ? 1 : 0));
-  dot_dnums.add_rhs_contracting_dimensions(num_batch_dims);
 
   HloInstruction* dot = computation->AddInstruction(HloInstruction::CreateDot(
-      ShapeUtil::MakeShape(original_dot->shape().element_type(), dot_dims,
-                           dot_dynamic_dims),
-      reshaped_lhs, reshaped_rhs, dot_dnums, original_dot->precision_config()));
+      ShapeUtil::MakeShape(original_dot->shape().element_type(),
+                           canonical_dot_dims, canonical_dot_dynamic_dims),
+      reshaped_lhs, reshaped_rhs, canonical_dnums,
+      original_dot->precision_config()));
   original_dot->SetupDerivedInstruction(dot);
 
   std::unique_ptr<HloInstruction> replacement =
@@ -234,31 +261,12 @@ absl::StatusOr<bool> DotDecomposer::RunImpl(
         continue;
       }
       const DotDimensionNumbers& dnums = instruction->dot_dimension_numbers();
-      // A dot it not canonical if there is more than one contracting dimension.
-      if (dnums.lhs_contracting_dimensions_size() != 1) {
-        non_canonical_dots.push_back(instruction);
-        continue;
-      }
-      // A dot is not canonical if it has more than one non-contracting
-      // dimension.
-      if (dnums.lhs_batch_dimensions_size() + 2 <
-              instruction->operand(0)->shape().dimensions().size() ||
-          dnums.rhs_batch_dimensions_size() + 2 <
-              instruction->operand(1)->shape().dimensions().size()) {
-        non_canonical_dots.push_back(instruction);
-        continue;
-      }
-      if (dnums.lhs_batch_dimensions().empty() &&
-          dnums.lhs_contracting_dimensions().empty()) {
-        non_canonical_dots.push_back(instruction);
-        continue;
-      }
-      // Check that batch dims, if present, are canonical.
-      std::vector<int64_t> canonical_batch_dims(
-          dnums.lhs_batch_dimensions_size());
-      absl::c_iota(canonical_batch_dims, 0);
-      if (!absl::c_equal(dnums.lhs_batch_dimensions(), canonical_batch_dims) ||
-          !absl::c_equal(dnums.rhs_batch_dimensions(), canonical_batch_dims)) {
+      if (!IsCanonical(instruction->operand(0)->shape(),
+                       dnums.lhs_contracting_dimensions(),
+                       dnums.lhs_batch_dimensions()) ||
+          !IsCanonical(instruction->operand(1)->shape(),
+                       dnums.rhs_contracting_dimensions(),
+                       dnums.rhs_batch_dimensions())) {
         non_canonical_dots.push_back(instruction);
       }
     }
