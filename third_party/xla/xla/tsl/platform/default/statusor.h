fe61b1339bb225f4eb0f96a2c4e26668e3601070
Ilya Tikhonovskiy <loislo@google.com>
2025-12-02 07:20:43 -0800

    #experiment
    Replace TF_ASSIGN_OR_RETURN and TF_RETURN_IF_ERROR with standard macros.
    
    This change updates the status handling macros from TensorFlow-specific versions to the more standard ASSIGN_OR_RETURN and RETURN_IF_ERROR.
    
    PiperOrigin-RevId: 839257485

--- third_party/xla/xla/tsl/platform/default/statusor.h
+++ third_party/xla/xla/tsl/platform/default/statusor.h
@@ -30,4 +30,29 @@ limitations under the License.
   }                                                    \
   lhs = std::move(statusor).value()
 
+#ifndef ASSIGN_OR_RETURN
+#define ASSIGN_OR_RETURN(lhs, rexpr) \
+  TF_ASSIGN_OR_RETURN_IMPL(          \
+      TF_STATUS_MACROS_CONCAT_NAME(_status_or_value, __COUNTER__), lhs, rexpr)
+
+#define ASSIGN_OR_RETURN_IMPL(statusor, lhs, rexpr) \
+  auto statusor = (rexpr);                          \
+  if (TF_PREDICT_FALSE(!statusor.ok())) {           \
+    return statusor.status();                       \
+  }                                                 \
+  lhs = std::move(statusor).value()
+#endif  // ASSIGN_OR_RETURN
+
+#ifndef RETURN_IF_ERROR
+// For propagating errors when calling a function.
+#define RETURN_IF_ERROR(...)               \
+  do {                                     \
+    absl::Status _status = (__VA_ARGS__);  \
+    if (TF_PREDICT_FALSE(!_status.ok())) { \
+      MAYBE_ADD_SOURCE_LOCATION(_status)   \
+      return _status;                      \
+    }                                      \
+  } while (0)
+#endif  // RETURN_IF_ERROR
+
 #endif  // XLA_TSL_PLATFORM_DEFAULT_STATUSOR_H_
