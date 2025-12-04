42deefc178fbd1c8024eea0d548cf170d257d0a6
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-03 19:15:15 -0800

    Automated Code Change
    PiperOrigin-RevId: 840013267

--- third_party/xla/xla/hlo/transforms/simplifiers/hlo_rematerialization.h
+++ third_party/xla/xla/hlo/transforms/simplifiers/hlo_rematerialization.h
@@ -24,6 +24,7 @@
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/functional/any_invocable.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
