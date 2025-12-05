efe051207c0b4f25f8e0a8ab084b8e71fd915055
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-04 19:40:27 -0800

    Automated Code Change
    PiperOrigin-RevId: 840505215

--- tensorflow/python/util/kernel_registry.cc
+++ tensorflow/python/util/kernel_registry.cc
@@ -14,6 +14,8 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/python/util/kernel_registry.h"
 
+#include <string>
+
 #include "absl/log/log.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/framework/node_def_util.h"
