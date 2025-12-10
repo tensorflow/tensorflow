99f1675518575b0bdbc7a1f3fbc96c5601a0a3e2
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-09 22:36:52 -0800

    Automated Code Change
    PiperOrigin-RevId: 842547550

--- tensorflow/tools/graph_transforms/inline_partitionedcall_test.cc
+++ tensorflow/tools/graph_transforms/inline_partitionedcall_test.cc
@@ -14,8 +14,9 @@ limitations under the License.
 ==============================================================================*/
 
 #include <algorithm>
-#include <string>
 
+#include "absl/status/status.h"
+#include "third_party/protobuf/text_format.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
