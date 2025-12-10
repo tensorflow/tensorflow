7d78fdae98ed04c2b629d7331d63b5fd16ebdf03
Karlo Basioli <basioli@google.com>
2025-12-10 06:13:07 -0800

    [XLA:GPU][codegen] Add AllReduce checks to triton support checks
    PiperOrigin-RevId: 842693983

--- third_party/xla/xla/hlo/analysis/indexing_analysis.cc
+++ third_party/xla/xla/hlo/analysis/indexing_analysis.cc
@@ -1729,7 +1729,8 @@ HloInstructionIndexing ComputeOutputToInputIndexing(const HloInstruction* instr,
       // b/65689298.
       instr->opcode() == HloOpcode::kMap ||
       // For a single device, all-reduce is an elementwise op.
-      instr->opcode() == HloOpcode::kAllReduceStart) {
+      instr->opcode() == HloOpcode::kAllReduceStart ||
+      instr->opcode() == HloOpcode::kAllReduceDone) {
     return ComputeOutputToInputCwiseOpIndexing(instr, mlir_context);
   }
   if (instr->opcode() == HloOpcode::kBitcast) {
@@ -1815,7 +1816,8 @@ HloInstructionIndexing ComputeInputToOutputIndexing(const HloInstruction* instr,
       // b/65689298.
       instr->opcode() == HloOpcode::kMap ||
       // For a single device, all-reduce has 1:1 output to input mapping.
-      instr->opcode() == HloOpcode::kAllReduceStart) {
+      instr->opcode() == HloOpcode::kAllReduceStart ||
+      instr->opcode() == HloOpcode::kAllReduceDone) {
     return ComputeInputToOutputCwiseOpIndexing(instr, mlir_context);
   }
   if (instr->opcode() == HloOpcode::kBitcast) {
