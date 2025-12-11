316717f4cfea35af30621a18680be1c918399455
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-10 18:38:17 -0800

    Automated Code Change
    PiperOrigin-RevId: 842978216

--- tensorflow/core/grappler/graph_topology_view.h
+++ tensorflow/core/grappler/graph_topology_view.h
@@ -18,9 +18,12 @@ limitations under the License.
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/inlined_vector.h"
+#include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "absl/types/span.h"
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/node_def.pb.h"
 #include "tensorflow/core/graph/tensor_id.h"
 #include "tensorflow/core/grappler/graph_view.h"
 
