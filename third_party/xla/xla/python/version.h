ad8dc5068d2cd02bca662d1c7ce8f82228fc2b02
Luke Baumann <lukebaumann@google.com>
2025-12-15 11:57:30 -0800

    Expose profiler advanced configuration as a Python dict.
    In profiler.cc, the advanced_configuration property of tensorflow::ProfileOptions is now exposed as a Python dictionary. The getter converts the proto map to a nb::dict, handling different value types (bool, int64, string).
    
    Example error:
    ```
    ProfileOptions().advanced_configuration
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    TypeError: Unable to convert function return value to a Python type! The signature was
        (self) -> proto2::Map<std::__u::basic_string<char, std::__u::char_traits<char>, std::__u::allocator<char>>, tensorflow::ProfileOptions_AdvancedConfigValue>
    ```
    PiperOrigin-RevId: 844865140

--- third_party/xla/xla/python/version.h
+++ third_party/xla/xla/python/version.h
@@ -18,6 +18,6 @@ limitations under the License.
 
 // An increasing version number to protect jax code against breaking changes.
 // In JAX, reference this via jax._src.lib.ifrt_version.
-#define JAX_IFRT_VERSION_NUMBER 40  // Shardy sharded -> unreduced
+#define JAX_IFRT_VERSION_NUMBER 41  // Python getter for advanced_configuration
 
 #endif  // XLA_PYTHON_VERSION_H_
