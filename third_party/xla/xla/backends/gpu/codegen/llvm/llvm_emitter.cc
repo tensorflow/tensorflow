82d621c1b916e13de42c0cf7966cc32063eb3662
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-15 20:00:03 -0800

    Automated Code Change
    PiperOrigin-RevId: 845038593

--- third_party/xla/xla/backends/gpu/codegen/llvm/llvm_emitter.cc
+++ third_party/xla/xla/backends/gpu/codegen/llvm/llvm_emitter.cc
@@ -91,6 +91,7 @@ limitations under the License.
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/util.h"
+#include "xla/xla_data.pb.h"
 #include "tsl/platform/fingerprint.h"
 
 namespace xla::gpu {
