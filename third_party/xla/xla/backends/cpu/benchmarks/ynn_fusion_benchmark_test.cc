d7a2f4a21ea36000f33fa0485d91bf287a70f0f3
Dillon Sharlet <dsharlet@google.com>
2025-12-15 18:00:56 -0800

    Change xnn_fusion_benchmark_test to ynn_fusion_benchmark_test
    I also incorrectly removed the build rule for this in a previous change.
    
    PiperOrigin-RevId: 844995933

--- third_party/xla/xla/backends/cpu/benchmarks/ynn_fusion_benchmark_test.cc
+++ third_party/xla/xla/backends/cpu/benchmarks/ynn_fusion_benchmark_test.cc
@@ -37,7 +37,7 @@ namespace xla::cpu {
 static absl::Status RunFusionBenchmark(benchmark::State& state,
                                        HloBenchmarkOptions options,
                                        absl::string_view hlo,
-                                       bool is_xnn_fusion = false) {
+                                       bool is_ynn_fusion = false) {
   int64_t d0 = state.range(0);  // Tensor size.
   int64_t n = state.range(1);   // Number of add-multiply iterations.
 
@@ -58,7 +58,7 @@ static absl::Status RunFusionBenchmark(benchmark::State& state,
       ShapeUtil::MakeShape(F32, {d0, d0}), &engine, 1.0f, 0.1f);
   std::vector<const Literal*> args = {&p0, &p1};
 
-  if (is_xnn_fusion) {
+  if (is_ynn_fusion) {
     options.disable_parallel_task_assigner = true;
     options.aot_options = nullptr;
   }
@@ -88,13 +88,13 @@ static void BM_EltwiseF32(benchmark::State& state,
   CHECK_OK(RunFusionBenchmark(state, std::move(options), hlo));
 }
 
-static void BM_XnnEltwiseF32(benchmark::State& state,
+static void BM_YnnEltwiseF32(benchmark::State& state,
                              HloBenchmarkOptions options) {
   // Perform `n+1` iterations of `add` and `multiply`, then end with `subtract`.
   absl::string_view hlo = R"(
     HloModule eltwise_f32_$n
 
-    xnn_fusion {
+    ynn_fusion {
       p0 = f32[$d0,$d0] parameter(0)
       p1 = f32[$d0,$d0] parameter(1)
       add0 = f32[$d0,$d0] add(p0, p1)
@@ -107,12 +107,12 @@ static void BM_XnnEltwiseF32(benchmark::State& state,
       p0 = f32[$d0,$d0] parameter(0)
       p1 = f32[$d0,$d0] parameter(1)
       ROOT %result = f32[$d0,$d0] fusion(%p0, %p1), kind=kCustom,
-        calls=xnn_fusion,
-        backend_config={"fusion_config": {kind: "__xnn_fusion"}}
+        calls=ynn_fusion,
+        backend_config={"fusion_config": {kind: "__ynn_fusion"}}
     }
   )";
   CHECK_OK(RunFusionBenchmark(state, std::move(options), hlo,
-                              /*is_xnn_fusion=*/true));
+                              /*is_ynn_fusion=*/true));
 }
 
 static void BM_DotAndEltwiseF32(benchmark::State& state,
@@ -136,14 +136,14 @@ static void BM_DotAndEltwiseF32(benchmark::State& state,
   CHECK_OK(RunFusionBenchmark(state, std::move(options), hlo));
 }
 
-static void BM_XnnDotAndEltwiseF32(benchmark::State& state,
+static void BM_YnnDotAndEltwiseF32(benchmark::State& state,
                                    HloBenchmarkOptions options) {
   // Perform `dot` followed by `n+1` iterations of `add` and `multiply`, then
   // end with `subtract`.
   absl::string_view hlo = R"(
     HloModule dot_and_eltwise_f32_$n
 
-    xnn_fusion {
+    ynn_fusion {
       p0 = f32[$d0,$d0] parameter(0)
       p1 = f32[$d0,$d0] parameter(1)
       dot0 = f32[$d0,$d0] dot(p0, p1), lhs_contracting_dims={1},
@@ -158,12 +158,12 @@ static void BM_XnnDotAndEltwiseF32(benchmark::State& state,
       p0 = f32[$d0,$d0] parameter(0)
       p1 = f32[$d0,$d0] parameter(1)
       ROOT %result = f32[$d0,$d0] fusion(%p0, %p1), kind=kCustom,
-        calls=xnn_fusion,
-        backend_config={"fusion_config": {kind: "__xnn_fusion"}}
+        calls=ynn_fusion,
+        backend_config={"fusion_config": {kind: "__ynn_fusion"}}
     }
   )";
   CHECK_OK(RunFusionBenchmark(state, std::move(options), hlo,
-                              /*is_xnn_fusion=*/true));
+                              /*is_ynn_fusion=*/true));
 }
 
 #define BENCHMARK_FUSION(name)  \
@@ -175,8 +175,8 @@ static void BM_XnnDotAndEltwiseF32(benchmark::State& state,
       ->Args({1024, 32})
 
 BENCHMARK_FUSION(BM_EltwiseF32);
-BENCHMARK_FUSION(BM_XnnEltwiseF32);
+BENCHMARK_FUSION(BM_YnnEltwiseF32);
 BENCHMARK_FUSION(BM_DotAndEltwiseF32);
-BENCHMARK_FUSION(BM_XnnDotAndEltwiseF32);
+BENCHMARK_FUSION(BM_YnnDotAndEltwiseF32);
 
 }  // namespace xla::cpu
