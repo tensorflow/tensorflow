aa1c37b22c9967dcb74eb914d73f9adaf22fba18
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-19 02:11:35 -0800

    Automated Code Change
    PiperOrigin-RevId: 846637593

--- third_party/xla/xla/backends/gpu/codegen/triton/transforms/int4_passes.cc
+++ third_party/xla/xla/backends/gpu/codegen/triton/transforms/int4_passes.cc
@@ -14,7 +14,6 @@ limitations under the License.
 ==============================================================================*/
 #include <algorithm>
 #include <cstdint>
-#include <functional>
 #include <iterator>
 #include <memory>
 #include <optional>
