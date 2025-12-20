4baf6a3d215f91e4a58c3fa0cb1fd890248bebbb
Yash Katariya <yashkatariya@google.com>
2025-12-19 18:06:03 -0800

    Add replicated -> unreduced test coverage
    PiperOrigin-RevId: 846930629

--- third_party/xla/xla/python/version.h
+++ third_party/xla/xla/python/version.h
@@ -18,7 +18,6 @@ limitations under the License.
 
 // An increasing version number to protect jax code against breaking changes.
 // In JAX, reference this via jax._src.lib.ifrt_version.
-#define JAX_IFRT_VERSION_NUMBER \
-  45  // Refresh custom layouts when copying an array across clients.
+#define JAX_IFRT_VERSION_NUMBER 46  // Shardy replicated -> unreduced
 
 #endif  // XLA_PYTHON_VERSION_H_
