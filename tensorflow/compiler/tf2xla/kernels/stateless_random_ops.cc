50359737b5919db157264b92f8f7245be4c6d6e9
A. Unique TensorFlower <gardener@tensorflow.org>
2025-12-26 05:18:07 -0800

    Automated Code Change
    PiperOrigin-RevId: 849144574

--- tensorflow/compiler/tf2xla/kernels/stateless_random_ops.cc
+++ tensorflow/compiler/tf2xla/kernels/stateless_random_ops.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstdint>
+#include <string>
 #include <tuple>
 
 #include "absl/status/status.h"
