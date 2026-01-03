4aee3669420e7873db071befc356fddb48b1d22e
Junwhan Ahn <junwhan@google.com>
2026-01-02 21:10:53 -0800

    Switch Async PjRt GPU to use `tsl::Env::SchedClosure` to enqueue async work
    This CL addresses the following two issues due to the use of `tsl::thread::ThreadPool` by switching to an unbounded thread pool:
    
    1. `tsl::thread::ThreadPool::Schedule()` runs the callable inline if the queue is full. This pattern is known to be prone to deadlocks, e.g., calling `Schedule` while holding a mutex that the callable needs.
    2. The bounded thread pool requires more careful reasoning about deadlock avoidance.
    
    PiperOrigin-RevId: 851540336

--- third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_buffer.cc
+++ third_party/xla/xla/pjrt/gpu/tfrt/tfrt_gpu_buffer.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include <cstdlib>
 #include <cstring>
 #include <memory>
+#include <new>
 #include <utility>
 
 #include "absl/algorithm/container.h"
@@ -68,6 +69,7 @@ limitations under the License.
 #include "xla/tsl/platform/statusor.h"
 #include "xla/util.h"
 #include "xla/xla_data.pb.h"
+#include "tsl/platform/casts.h"
 #include "tsl/platform/mem.h"
 #include "tsl/profiler/lib/connected_traceme.h"
 #include "tsl/profiler/lib/traceme.h"
@@ -153,12 +155,12 @@ absl::StatusOr<Shape> TfrtGpuBuffer::logical_on_device_shape() {
   };
 
   absl::StatusOr<Shape> shape_or;
-  EnqueueWorkWhenReady(client_->blocking_thread_pool(),
-                       {device_buffer->definition_event().CopyRCRef()},
-                       [get_shape = std::move(get_shape), &shape_or,
-                        usage_event_holder = std::move(ready_on_exit)]() {
-                         shape_or = get_shape();
-                       });
+  client_->async_work_runner()->ScheduleWhenReady(
+      {device_buffer->definition_event().CopyRCRef()},
+      [get_shape = std::move(get_shape), &shape_or,
+       usage_event_holder = std::move(ready_on_exit)]() {
+        shape_or = get_shape();
+      });
 
   tsl::BlockUntilReady(usage_event);
   return shape_or;
@@ -534,9 +536,8 @@ Future<> TfrtGpuBuffer::ToLiteralHelper(Future<MutableLiteralBase*> literal) {
           promise.Set(absl::OkStatus());
         });
   };
-  EnqueueWorkWhenReady(client_->blocking_thread_pool(),
-                       {device_buffer->definition_event().CopyRCRef()},
-                       std::move(d2h_copy));
+  client_->async_work_runner()->ScheduleWhenReady(
+      {device_buffer->definition_event().CopyRCRef()}, std::move(d2h_copy));
 
   return FutureHelpers::WithProfiling(
       std::move(future),
@@ -658,8 +659,7 @@ Future<> TfrtGpuBuffer::CopyRawToHostFuture(Future<void*> dst_future,
           LOG(ERROR) << "dst resolved to an error: " << dst_or.status();
           return;
         }
-        EnqueueWorkWhenReady(
-            client->blocking_thread_pool(),
+        client->async_work_runner()->ScheduleWhenReady(
             {device_buffer->definition_event().CopyRCRef()},
             [dst = std::move(dst_or.value()), promise = std::move(promise),
              d2h_copy = std::move(d2h_copy)]() mutable {
@@ -848,9 +848,8 @@ absl::StatusOr<std::unique_ptr<PjRtBuffer>> TfrtGpuBuffer::CopyToMemorySpace(
         }
       };
 
-  EnqueueWorkWhenReady(client_->blocking_thread_pool(),
-                       {src_device_buffer->ready_event().CopyRCRef()},
-                       std::move(transfer_d2d));
+  client_->async_work_runner()->ScheduleWhenReady(
+      {src_device_buffer->ready_event().CopyRCRef()}, std::move(transfer_d2d));
   return output_buffer;
 }
 
