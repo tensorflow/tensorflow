c590fbddbe7ccce19390a542900d069abb58c80c
A. Unique TensorFlower <gardener@tensorflow.org>
2026-01-05 01:39:50 -0800

    Automated Code Change
    PiperOrigin-RevId: 852189926

--- third_party/xla/xla/backends/cpu/codegen/emitters/cpu_fusion_emitter_test.cc
+++ third_party/xla/xla/backends/cpu/codegen/emitters/cpu_fusion_emitter_test.cc
@@ -14,7 +14,6 @@ limitations under the License.
 ==============================================================================*/
 
 #include <memory>
-#include <string>
 #include <utility>
 
 #include <gtest/gtest.h>
