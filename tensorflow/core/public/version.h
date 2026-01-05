3593546f809558264ce87fb76bf90c5f4bde6346
A. Unique TensorFlower <gardener@tensorflow.org>
2026-01-05 03:34:19 -0800

    Automated Code Change
    PiperOrigin-RevId: 852224593

--- tensorflow/core/public/version.h
+++ tensorflow/core/public/version.h
@@ -93,7 +93,7 @@ limitations under the License.
 
 #define TF_GRAPH_DEF_VERSION_MIN_PRODUCER 0
 #define TF_GRAPH_DEF_VERSION_MIN_CONSUMER 0
-#define TF_GRAPH_DEF_VERSION 2463  // Updated: 2026/1/5
+#define TF_GRAPH_DEF_VERSION 2464  // Updated: 2026/1/6
 
 // Checkpoint compatibility versions (the versions field in SavedSliceMeta).
 //
--- third_party/xla/xla/pjrt/c_api_client/pjrt_c_api_phase_compiler.cc
+++ third_party/xla/xla/pjrt/c_api_client/pjrt_c_api_phase_compiler.cc
@@ -32,6 +32,8 @@ limitations under the License.
 #include "xla/pjrt/partial_program_utils.h"
 #include "xla/pjrt/pjrt_compiler.h"
 #include "xla/pjrt/pjrt_executable.h"
+#include "xla/pjrt/proto/compile_options.pb.h"
+#include "xla/pjrt/proto/pjrt_partial_program.pb.h"
 #include "xla/pjrt/string_utils.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/statusor.h"
