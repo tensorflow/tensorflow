4bce255fe366602eb733569218631696e64cc4f5
Niklas Vangerow <nikv@google.com>
2026-01-06 13:54:54 -0800

    Tag PjRt migration candidates explicitly. (BUILD changes)
    This change adds a new tag "pjrt_migration_candidate" to all test targets that
    depend on HloTestBase, ClientLibraryTestBase, and HloRunnerTpuSystem.
    
    This change also adds a new `use_legacy_runtime` kwarg to `xla_test`, which acts
    as a replacement for "test_migrated_to_hlo_runner_pjrt".  During a brief
    transition phase, we will leave all "test_migrated_to_hlo_runner_pjrt" tags in
    place so that we can identify any tests that have the `use_legacy_runtime` set
    to an incorrect value.
    
    "pjrt_migration_candidate" and "test_migrated_to_hlo_runner_pjrt" are mutually
    exclusive.
    
    "pjrt_migration_candidate" should not appear on any tests using the new runtime.
    
    Unlike "test_migrated_to_hlo_runner_pjrt", which primarily tags `xla_test`
    targets, "pjrt_migration_candidate" intends to tag all outstanding migration
    candidates to obtain an accurate picture of migration progress. If a test cannot
    or should not be migrated, it can be excluded from any analysis just by removing
    the tag.
    
    PiperOrigin-RevId: 852923628

--- tensorflow/compiler/mlir/tfr/integration/tfr_decompose_ctx.h
+++ tensorflow/compiler/mlir/tfr/integration/tfr_decompose_ctx.h
@@ -17,6 +17,7 @@ limitations under the License.
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/IR/MLIRContext.h"  // from @llvm-project
