f23ac21e1b4807dec63ae2c2b20a81632d03aed1
Dillon Sharlet <dsharlet@google.com>
2026-01-06 20:49:03 -0800

    Add float16 support to SELECT ops
    PiperOrigin-RevId: 853053904

--- tensorflow/lite/kernels/select.cc
+++ tensorflow/lite/kernels/select.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/tensor.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
+#include "tensorflow/lite/types/half.h"
 
 namespace tflite {
 namespace ops {
@@ -155,6 +156,9 @@ TfLiteStatus SelectEval(TfLiteContext* context, TfLiteNode* node) {
       break;                                                                 \
     case kTfLiteFloat32:                                                     \
       TF_LITE_SELECT(float, op);                                             \
+      break;                                                                 \
+    case kTfLiteFloat16:                                                     \
+      TF_LITE_SELECT(half, op);                                              \
       break;                                                                 \
     case kTfLiteUInt8:                                                       \
       TF_LITE_SELECT(uint8_t, op);                                           \
