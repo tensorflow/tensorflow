bbfa1cbbb6b2bace0eb1a023403b51bc8eb087d9
Allan Renucci <allanrenucci@google.com>
2026-01-08 00:52:47 -0800

    Disable heuristic collective combining by default.
    PiperOrigin-RevId: 853607024

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -459,7 +459,8 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_experimental_pack_dot_operands_along_k_dimension(true);
   opts.set_xla_unsupported_crash_on_hlo_pass_fix_max_iterations(false);
   opts.set_xla_hlo_pass_fix_detect_cycles(false);
-  opts.set_xla_gpu_experimental_enable_heuristic_collective_combining(true);
+  // TODO(b/449025971): Set to true once the issue is fixed.
+  opts.set_xla_gpu_experimental_enable_heuristic_collective_combining(false);
   opts.set_xla_unsupported_crash_on_hlo_pass_silent_hlo_change(false);
   opts.set_xla_disable_automatic_host_compute_offload(false);
   opts.set_xla_allow_h2h_copy_when_automatic_host_compute_offload_disabled(
