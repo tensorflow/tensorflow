3614e11d59e4727197c6bf1c12e3cb95994fca8d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-01-12 05:14:11 -0800

    Automated Code Change
    PiperOrigin-RevId: 855181796

--- third_party/xla/xla/service/gpu/transforms/nest_gemm_fusion.cc
+++ third_party/xla/xla/service/gpu/transforms/nest_gemm_fusion.cc
@@ -18,19 +18,16 @@ limitations under the License.
 #include <cstdint>
 #include <memory>
 #include <utility>
-#include <variant>
 #include <vector>
 
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/container/inlined_vector.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "llvm/ADT/SmallVector.h"
