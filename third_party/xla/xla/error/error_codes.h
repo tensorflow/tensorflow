45d49d1e28fb53dccee189d3a97bc5c7782027e4
Buddh Prakash <buddhap@google.com>
2026-01-13 00:28:43 -0800

    Introduce the E300X error codes for SparseCore Compilation failures.
    PiperOrigin-RevId: 855570373

--- third_party/xla/xla/error/error_codes.h
+++ third_party/xla/xla/error/error_codes.h
@@ -84,6 +84,10 @@ namespace xla::error {
     absl::StatusCode::kResourceExhausted)                                     \
   X("E0102", RuntimeProgramInputMismatch, absl::StatusCode::kInvalidArgument) \
   X("E0103", RuntimeUnexpectedCoreHalt, absl::StatusCode::kInternal)          \
+  X("E3000", CompileTimeSparseCoreAllocationFailure,                          \
+    absl::StatusCode::kResourceExhausted)                                     \
+  X("E3001", CompileTimeSparseCoreInvalidReplicaCount,                        \
+    absl::StatusCode::kInvalidArgument)                                       \
                                                                               \
   /* go/keep-sorted end */
 
@@ -143,7 +147,7 @@ inline std::string GetErrorUrl(ErrorCode code) {
 // "EXXXX: ErrorName:\n<Original Message>\nSee <URL> for more details."
 inline std::string FormatMessageWithCode(absl::string_view message,
                                          ErrorCode code) {
-  return absl::StrCat(GetErrorCodeAndName(code), ":\n", message, "\nSee ",
+  return absl::StrCat(GetErrorCodeAndName(code), ": ", message, "\nSee ",
                       GetErrorUrl(code), " for more details.");
 }
 
