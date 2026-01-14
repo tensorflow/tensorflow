0d58c6d77be42937d4431bdc7fabc2b03da9d3c1
Buddh Prakash <buddhap@google.com>
2026-01-14 08:48:44 -0800

    Introduce error codes for Mosaic CompileTime failures.
    PiperOrigin-RevId: 856232653

--- third_party/xla/xla/error/error_codes.h
+++ third_party/xla/xla/error/error_codes.h
@@ -88,6 +88,12 @@ namespace xla::error {
   X("E1001", CompileTimeScopedVmemOom, absl::StatusCode::kResourceExhausted)  \
   X("E1200", CompileTimeHostOffloadOutputLocationMismatch,                    \
     absl::StatusCode::kInvalidArgument)                                       \
+  X("E2001", CompileTimeMosaicUnsupportedRhsType,                             \
+    absl::StatusCode::kUnimplemented)                                         \
+  X("E2002", CompileTimeMosaicMisalignedBlockAndTiling,                       \
+    absl::StatusCode::kInvalidArgument)                                       \
+  X("E2003", CompileTimeMosaicUnprovenMemoryAccessAlignment,                  \
+    absl::StatusCode::kInvalidArgument)                                       \
   X("E3000", CompileTimeSparseCoreAllocationFailure,                          \
     absl::StatusCode::kResourceExhausted)                                     \
   X("E3001", CompileTimeSparseCoreInvalidReplicaCount,                        \
