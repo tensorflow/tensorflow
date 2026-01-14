1ff922748cc6d4dc8ba5d934d35e61aea6742da7
Buddh Prakash <buddhap@google.com>
2026-01-13 16:38:46 -0800

    Introduce and instrument the E1200 error code for Compile Time Host Offload Output location mismatch errors.
    PiperOrigin-RevId: 855934942

--- third_party/xla/xla/error/error_codes.h
+++ third_party/xla/xla/error/error_codes.h
@@ -86,6 +86,8 @@ namespace xla::error {
   X("E0103", RuntimeUnexpectedCoreHalt, absl::StatusCode::kInternal)          \
   X("E1000", CompileTimeHbmOom, absl::StatusCode::kResourceExhausted)         \
   X("E1001", CompileTimeScopedVmemOom, absl::StatusCode::kResourceExhausted)  \
+  X("E1200", CompileTimeHostOffloadOutputLocationMismatch,                    \
+    absl::StatusCode::kInvalidArgument)                                       \
   X("E3000", CompileTimeSparseCoreAllocationFailure,                          \
     absl::StatusCode::kResourceExhausted)                                     \
   X("E3001", CompileTimeSparseCoreInvalidReplicaCount,                        \
