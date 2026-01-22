d2cb651d92f405d9cf09390238f9b016ff4b760e
Mehrdad Khani <mehrdadk@google.com>
2026-01-21 17:32:25 -0800

    Cleanup any dead computations introduced in MSA.
    PiperOrigin-RevId: 859332212

--- third_party/xla/xla/service/memory_space_assignment/memory_space_assignment.cc
+++ third_party/xla/xla/service/memory_space_assignment/memory_space_assignment.cc
@@ -700,6 +700,54 @@ void MemorySpaceAssignment::RemoveScopedMemoryAssignments(
   }
 }
 
+namespace {
+
+// Removes the given computation, and adds computations to the worklist that
+// will be orphaned as a result. Removed instructions are added to
+// removed_instructions.
+absl::Status ProcessDeadComputation(
+    HloModule* module, HloComputation* computation,
+    absl::flat_hash_set<const HloInstruction*>& removed_instructions,
+    std::vector<HloComputation*>& worklist) {
+  for (HloInstruction* instruction : computation->instructions()) {
+    removed_instructions.insert(instruction);
+    for (HloComputation* called_computation :
+         instruction->called_computations()) {
+      // If computation is cloned, the called computation might still have other
+      // callers, so not necessarily dead.
+      if (called_computation->caller_computations().size() == 1) {
+        worklist.push_back(called_computation);
+      }
+    }
+  }
+  VLOG(2) << "Removing dead computation: " << computation->name();
+  TF_RETURN_IF_ERROR(module->RemoveEmbeddedComputation(computation));
+  return absl::OkStatus();
+}
+
+// Removes dead fusion computations from the module and populates
+// removed_instructions with the instructions that were removed.
+absl::Status CleanupDeadFusionComputations(
+    HloModule* module,
+    absl::flat_hash_set<const HloInstruction*>& removed_instructions) {
+  std::vector<HloComputation*> worklist;
+  for (HloComputation* computation : module->computations()) {
+    if (!computation->IsEntryComputation() &&
+        computation->caller_computations().empty()) {
+      worklist.push_back(computation);
+    }
+  }
+  while (!worklist.empty()) {
+    HloComputation* computation = worklist.back();
+    worklist.pop_back();
+    TF_RETURN_IF_ERROR(ProcessDeadComputation(module, computation,
+                                              removed_instructions, worklist));
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace
+
 absl::Status MemorySpaceAssignment::SimplifyGraph() {
   VLOG(1) << "Simplifying graph...";
 
@@ -807,6 +855,9 @@ absl::Status MemorySpaceAssignment::SimplifyGraph() {
       }
     }
   }
+
+  TF_RETURN_IF_ERROR(
+      CleanupDeadFusionComputations(module_, removed_instructions));
 
   RemoveAlternateMemoryAssignments(removed_instructions);
   RemoveScopedMemoryAssignments(removed_instructions);
