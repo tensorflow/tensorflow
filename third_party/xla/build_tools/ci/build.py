a5ce7d8b2713359b802ed4c04f9657be6700ca6f
Yicheng Luo <yichengluo@google.com>
2026-01-22 17:08:03 -0800

    Don't hack XLA into using `@local_tsl` or `@local_xla` attempt 2.
    This was merged as https://github.com/tensorflow/tensorflow/commit/47aee3cc5efef8453232b2081fe9b8880d43e2fe but was then reintroduced in https://github.com/tensorflow/tensorflow/commit/190e895b01588ad4fe7bb1cf2e79dc8722c7a151.
    
    PiperOrigin-RevId: 859836308

--- third_party/xla/build_tools/ci/build.py
+++ third_party/xla/build_tools/ci/build.py
@@ -829,7 +829,7 @@ Build(
         color="yes",
     ),
     override_repository=dict(
-        local_xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
+        xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
 )
@@ -850,7 +850,7 @@ Build(
     build_tag_filters=tensorflow_gpu_tag_filters,
     test_tag_filters=tensorflow_gpu_tag_filters,
     override_repository=dict(
-        local_xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
+        xla=f"{_GITHUB_WORKSPACE}/openxla/xla",
     ),
     options=dict(
         verbose_failures=True,
@@ -860,24 +860,7 @@ Build(
         color="yes",
     ),
     repo_env={"USE_PYWRAP_RULES": "True"},
-    extra_setup_commands=(
-        # This is pretty devious - but we have to do some adhoc extra Copybara
-        # work here to get XLA into the shape TF expects. b/407638223
-        # pyformat:disable
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@xla/@local_xla/g", "{}", "+",
-        ],
-        [
-            "find",
-            f"{_GITHUB_WORKSPACE}/openxla/xla",
-            "-type", "f",
-            "-exec", "sed", "-i", "s/@tsl/@local_tsl/g", "{}", "+",
-        ],
-        ["nvidia-smi"],
-    ),
+    extra_setup_commands=(["nvidia-smi"],),
 )
 
 
