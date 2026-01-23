3f77050e6b8adc21e511add9c0ba77777b22ea5f
Dillon Sharlet <dsharlet@google.com>
2026-01-23 09:14:04 -0800

    Fix `ChangeOpDataType` to not attempt to change the result type
    Currently, `ChangeOpDataType` only checks if the operands match the `from_type`, and then tries to convert both the operands *and* the result to `to_type`, but the result type may have been different and may not require conversion, or a different conversion.
    
    PiperOrigin-RevId: 860134376

--- third_party/xla/xla/service/change_op_data_type.cc
+++ third_party/xla/xla/service/change_op_data_type.cc
@@ -17,7 +17,18 @@ limitations under the License.
 
 #include <optional>
 
+#include "absl/container/flat_hash_set.h"
+#include "absl/container/inlined_vector.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
+#include "xla/hlo/ir/hlo_computation.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_module.h"
+#include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/hlo_creation_utils.h"
+#include "xla/shape.h"
+#include "xla/tsl/platform/errors.h"
 
 #ifdef XLA_ONEDNN
 #include "xla/service/cpu/onednn_contraction_rewriter.h"
@@ -86,8 +97,10 @@ absl::StatusOr<bool> ChangeOpDataType::RunImpl(
 
       HloInstruction* new_instr =
           comp->AddInstruction(cloner(instr, new_shape, new_operands));
-      TF_RETURN_IF_ERROR(comp->ReplaceInstruction(
-          instr, MakeConvertToHlo(new_instr, from_type)));
+      if (new_instr->shape().element_type() != instr->shape().element_type()) {
+        new_instr = MakeConvertToHlo(new_instr, instr->shape().element_type());
+      }
+      TF_RETURN_IF_ERROR(comp->ReplaceInstruction(instr, new_instr));
       changed = true;
     }
   }
