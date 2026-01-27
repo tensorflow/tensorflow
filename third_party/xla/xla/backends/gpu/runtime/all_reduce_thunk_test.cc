dfe7c83394d6dac77855e524e8d0b80fd4fd3f32
A. Unique TensorFlower <gardener@tensorflow.org>
2026-01-27 06:32:25 -0800

    Automated Code Change
    PiperOrigin-RevId: 861691927

--- third_party/xla/xla/backends/gpu/runtime/all_reduce_thunk_test.cc
+++ third_party/xla/xla/backends/gpu/runtime/all_reduce_thunk_test.cc
@@ -100,5 +100,43 @@ TEST(CollectiveThunkTest, SyncCollective) {
   ASSERT_EQ(thunk->async_events(), nullptr);
 }
 
+TEST(ReduceScatterStartThunkTest, ProtoRoundTrip) {
+  ThunkProto proto = tsl::proto_testing::ParseTextProtoOrDie<ThunkProto>(
+      R"pb(
+        thunk_info {
+          profile_annotation: "partition_id_profile_annotation"
+          execution_stream_id: 2
+        }
+        reduce_scatter_start_thunk {
+          async_events_unique_id: 3
+          collective_config {}
+          reduction_kind: 1
+        }
+      )pb");
+
+  Thunk::ThunkInfo thunk_info;
+  thunk_info.profile_annotation = proto.thunk_info().profile_annotation();
+  thunk_info.execution_stream_id = xla::gpu::ExecutionStreamId{
+      static_cast<xla::gpu::ExecutionStreamId::ValueType>(
+          proto.thunk_info().execution_stream_id())};
+
+  CollectiveThunk::AsyncEventsMap async_events_map;
+  std::vector<BufferAllocation> buffer_allocations = {
+      BufferAllocation(/*index=*/0, /*size=*/4, /*color=*/0)};
+
+  ASSERT_OK_AND_ASSIGN(std::unique_ptr<ReduceScatterStartThunk> thunk,
+                       ReduceScatterStartThunk::FromProto(
+                           thunk_info, proto.reduce_scatter_start_thunk(),
+                           buffer_allocations, async_events_map));
+  ASSERT_NE(thunk->async_events(), nullptr);
+
+  ASSERT_OK_AND_ASSIGN(ThunkProto round_trip_proto, thunk->ToProto());
+
+  // Ids are unique and expected to differ.
+  proto.mutable_reduce_scatter_start_thunk()->set_async_events_unique_id(
+      round_trip_proto.reduce_scatter_start_thunk().async_events_unique_id());
+  EXPECT_THAT(round_trip_proto, EqualsProto(proto));
+}
+
 }  // namespace
 }  // namespace xla::gpu
