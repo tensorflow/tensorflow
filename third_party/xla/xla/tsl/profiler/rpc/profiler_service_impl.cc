267ddf6d9add9690c254b96bf259f2de2f399754
Sannidhya Chauhan <sannidhya@google.com>
2026-01-28 09:22:30 -0800

    Add RPC to stop continuous profiling.
    PiperOrigin-RevId: 862268209

--- third_party/xla/xla/tsl/profiler/rpc/profiler_service_impl.cc
+++ third_party/xla/xla/tsl/profiler/rpc/profiler_service_impl.cc
@@ -52,6 +52,8 @@ using tensorflow::MonitorRequest;
 using tensorflow::MonitorResponse;
 using tensorflow::ProfileRequest;
 using tensorflow::ProfileResponse;
+using tensorflow::StopContinuousProfilingRequest;
+using tensorflow::StopContinuousProfilingResponse;
 using tensorflow::TerminateRequest;
 using tensorflow::TerminateResponse;
 
@@ -156,6 +158,24 @@ class ProfilerServiceImpl : public tensorflow::grpc::ProfilerService::Service {
     tensorflow::ProfileRequest request = *req;
     request.set_emit_xspace(true);
     continuous_profiling_session_ = {request, std::move(profiler)};
+    return ::grpc::Status::OK;
+  }
+
+  ::grpc::Status StopContinuousProfiling(
+      ::grpc::ServerContext* ctx, const StopContinuousProfilingRequest* req,
+      StopContinuousProfilingResponse* response) override {
+    std::optional<ContinuousSession> session_to_destroy;
+    {
+      absl::MutexLock lock(&mutex_);
+      if (!continuous_profiling_session_.has_value()) {
+        return ::grpc::Status(::grpc::StatusCode::NOT_FOUND,
+                              "No continuous profiling session found.");
+      }
+      // Move session to a local variable so that it is destroyed after mutex
+      // is released, avoiding potentially expensive destruction of
+      // ProfilerSession under lock.
+      session_to_destroy.swap(continuous_profiling_session_);
+    }
     return ::grpc::Status::OK;
   }
 
