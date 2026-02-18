05074c433a238c6072841fe3b671b373d9c22aa1
Jayden G <user.josephms@gmail.com>
2026-02-18 21:28:36 +0530

    Fix pylint issues in sparse_concat_overflow_test

--- tensorflow/python/kernel_tests/sparse_ops/sparse_concat_overflow_test.py
+++ tensorflow/python/kernel_tests/sparse_ops/sparse_concat_overflow_test.py
@@ -1,28 +1,48 @@
-import tensorflow as tf
+# Copyright 2024 The TensorFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for SparseConcat overflow handling."""
+
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
+from tensorflow.python.framework import sparse_tensor
 from tensorflow.python.platform import test
 
 
 class SparseConcatOverflowTest(test.TestCase):
 
-    def test_sparse_concat_shape_overflow(self):
-        indices1 = tf.constant([[0, 0, 0]], dtype=tf.int64)
-        values1 = tf.constant([1], dtype=tf.int32)
-        shape1 = tf.constant([5, 2, 2147483647], dtype=tf.int64)
+  def testSparseConcatLargeShapeOverflow(self):
+    indices1 = constant_op.constant(
+        [[0, 0, 0], [1, 1, 0], [2, 0, 1]], dtype=dtypes.int64)
+    values1 = constant_op.constant(["tensor", "flow", "test"])
+    shape1 = constant_op.constant([5, 2, 2147483647], dtype=dtypes.int64)
 
-        indices2 = tf.constant([[0, 1, 0]], dtype=tf.int64)
-        values2 = tf.constant([2], dtype=tf.int32)
-        shape2 = tf.constant([5, 1879048192, 536870912], dtype=tf.int64)
+    indices2 = constant_op.constant(
+        [[0, 1, 0], [1, 0, 0], [2, 1, 1], [3, 0, 1]], dtype=dtypes.int64)
+    values2 = constant_op.constant(["a", "b", "c", "d"])
+    shape2 = constant_op.constant(
+        [5, 1879048192, 536870912], dtype=dtypes.int64)
 
-        with self.assertRaises(errors.InvalidArgumentError):
-            tf.raw_ops.SparseConcat(
-                indices=[indices1, indices2],
-                values=[values1, values2],
-                shapes=[shape1, shape2],
-                concat_dim=1,
-            )
+    sp1 = sparse_tensor.SparseTensor(indices1, values1, shape1)
+    sp2 = sparse_tensor.SparseTensor(indices2, values2, shape2)
+
+    with self.assertRaises(errors.InvalidArgumentError):
+      sparse_tensor.sparse_concat(
+          axis=1,
+          sp_inputs=[sp1, sp2])
 
 
 if __name__ == "__main__":
-    test.main()
-
+  test.main()
