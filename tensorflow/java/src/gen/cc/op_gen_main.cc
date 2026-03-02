ef61acf9cf094cbd1fa8fa2e48ae257fb50b177d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-01 19:49:37 -0800

    Automated Code Change
    PiperOrigin-RevId: 877160540

--- tensorflow/java/src/gen/cc/op_gen_main.cc
+++ tensorflow/java/src/gen/cc/op_gen_main.cc
@@ -13,6 +13,7 @@
  limitations under the License.
 ==============================================================================*/
 
+#include <string>
 #include <vector>
 
 #include "absl/log/check.h"
