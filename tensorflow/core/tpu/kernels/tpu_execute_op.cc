c28b96f83b1cc5e5d6a3873dc6520ab2cad53ef2
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-04 06:22:00 -0800

    Automated Code Change
    PiperOrigin-RevId: 878456667

--- tensorflow/core/tpu/kernels/tpu_execute_op.cc
+++ tensorflow/core/tpu/kernels/tpu_execute_op.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "absl/base/casts.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
