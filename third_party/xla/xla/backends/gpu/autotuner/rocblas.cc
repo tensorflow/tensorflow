183ed27c5246c6fadfaebd7185ed4eb1711e9009
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-08 19:47:35 -0700

    Automated Code Change
    PiperOrigin-RevId: 880606948

--- third_party/xla/xla/backends/gpu/autotuner/rocblas.cc
+++ third_party/xla/xla/backends/gpu/autotuner/rocblas.cc
@@ -38,6 +38,7 @@ limitations under the License.
 #include "xla/stream_executor/stream_executor_memory_allocator.h"
 #include "xla/tsl/platform/errors.h"
 #include "xla/tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace gpu {
