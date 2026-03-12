5607932d180403eebab1aacecaa7450629330ca4
Dirk Hornung <dirkh@google.com>
2026-03-12 16:22:49 -0700

    [CublasLt] Disallow sharing the workspace buffer of a CublasLt gemm with the buffer of a fused add operand.
    PiperOrigin-RevId: 882835512

--- third_party/xla/xla/service/gpu/nvptx_alias_info.cc
+++ third_party/xla/xla/service/gpu/nvptx_alias_info.cc
@@ -25,7 +25,6 @@ limitations under the License.
 #include "xla/service/gpu/alias_info.h"
 #include "xla/service/gpu/backend_configs.pb.h"
 #include "xla/service/gpu/cublas_cudnn.h"
-#include "xla/service/gpu/ir_emission_utils.h"
 #include "xla/shape_util.h"
 
 namespace xla::gpu {
@@ -48,12 +47,22 @@ std::optional<bool> NVPTXAliasInfo::MayAlias(
               user->operand(user_index[0]) == operand);
     case HloOpcode::kCustomCall:
       // The matrix bias operand can be overwritten in-place.
+      // See https://docs.nvidia.com/cuda/cublas/#cublasltmatmul.
+      // Note that we disallow aliasing the matmul workspace buffer
+      // with the bias buffer.
       if (user->custom_call_target() == kCublasLtMatmulCallTarget) {
         GemmBackendConfig config =
             std::move(user->backend_config<GpuBackendConfig>())
                 ->gemm_backend_config();
+        // For cublasLt matmul, operand(2) is the matrix 'C' in D = alpha * (A *
+        // B) + beta * C. We only allow aliasing if beta is non-zero (so 'C' is
+        // actually used), the operand is 'C', and it is used only once by this
+        // instruction. Finally, we check that we are looking at the
+        // first output of the custom call, which is the result incontrary to
+        // the second output which is the workspace buffer.
         return (config.beta() != 0.) && operand == user->operand(2) &&
-               absl::c_count(user->operands(), operand) == 1;
+               absl::c_count(user->operands(), operand) == 1 &&
+               user_index.size() == 1 && user_index[0] == 0;
       }
       return false;
     default:
