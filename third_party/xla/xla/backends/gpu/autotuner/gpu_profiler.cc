049b5276407d329cd4ca2a50d3466059fc32d60b
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-22 00:56:12 -0700

    Automated Code Change
    PiperOrigin-RevId: 887567753

--- third_party/xla/xla/backends/gpu/autotuner/gpu_profiler.cc
+++ third_party/xla/xla/backends/gpu/autotuner/gpu_profiler.cc
@@ -132,7 +132,7 @@ absl::StatusOr<std::unique_ptr<InputBuffers>> GpuProfiler::CreateInputBuffers(
 absl::StatusOr<ProfileResult> GpuProfiler::Profile(
     Executable* executable, const InputBuffers& buffers) {
   const GpuInputBuffers& gpu_buffers =
-      tsl::down_cast<const GpuInputBuffers&>(buffers);
+      absl::down_cast<const GpuInputBuffers&>(buffers);
   const RedzoneBuffers& rz_buffers = gpu_buffers.redzone_buffers;
   ProfileResult result;
   result.scratch_bytes = GetScratchBytes(executable);
@@ -193,7 +193,7 @@ absl::Status GpuProfiler::CheckInputBuffers(InputBuffers& buffers) {
     return absl::OkStatus();
   }
   const GpuInputBuffers& gpu_buffers =
-      tsl::down_cast<const GpuInputBuffers&>(buffers);
+      absl::down_cast<const GpuInputBuffers&>(buffers);
   const RedzoneBuffers& rz_buffers = gpu_buffers.redzone_buffers;
   TF_ASSIGN_OR_RETURN(se::RedzoneAllocator::RedzoneCheckStatus rz_check_status,
                       rz_buffers.RedzoneAllocator().CheckRedzones());
