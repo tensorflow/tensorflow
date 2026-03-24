d14b30b00478c03b8aa2e0420864006dc348ae28
jmestwa-coder <jmestwa@gmail.com>
2026-03-24 17:38:33 +0530

    Validate tf.name_scope names with spaces in eager and graph modes

--- tensorflow/python/framework/ops.py
+++ tensorflow/python/framework/ops.py
@@ -5797,6 +5797,21 @@ class name_scope_v2(contextlib.AbstractContextManager[str]):
     """
     ctx = context.context()
     if ctx.executing_eagerly():
+      # Validate name in eager mode to match Graph.name_scope behavior.
+      if self._name:
+        if ctx.scope_name:
+          if not _VALID_SCOPE_NAME_REGEX.match(self._name):
+            raise ValueError(
+                "'%s' is not a valid scope name. A scope name has to match "
+                "the following pattern: %s" %
+                (self._name, _VALID_SCOPE_NAME_REGEX.pattern))
+        else:
+          if not _VALID_OP_NAME_REGEX.match(self._name):
+            raise ValueError(
+                "'%s' is not a valid root scope name. A root scope name has "
+                "to match the following pattern: %s" %
+                (self._name, _VALID_OP_NAME_REGEX.pattern))
+
       # Names are not auto-incremented in eager mode.
       # A trailing slash breaks out of nested name scopes, indicating a
       # fully specified scope name, for compatibility with Graph.name_scope.
