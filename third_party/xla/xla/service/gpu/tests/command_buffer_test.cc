eac76ea72f929016106867ee0d76b1672c23c58b
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-24 20:56:22 -0700

    Automated Code Change
    PiperOrigin-RevId: 889000908

--- third_party/xla/xla/service/gpu/tests/command_buffer_test.cc
+++ third_party/xla/xla/service/gpu/tests/command_buffer_test.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <array>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <optional>
