813e684ff9e944843428464dc58b57db3fd73c5a
A. Unique TensorFlower <gardener@tensorflow.org>
2026-03-27 20:17:35 -0700

    Automated Code Change
    PiperOrigin-RevId: 890759836

--- third_party/xla/xla/pjrt/c_api_client/pjrt_c_api_client.h
+++ third_party/xla/xla/pjrt/c_api_client/pjrt_c_api_client.h
@@ -59,6 +59,7 @@ limitations under the License.
 #include "xla/pjrt/pjrt_device_dimensions.h"
 #include "xla/pjrt/pjrt_executable.h"
 #include "xla/pjrt/pjrt_layout.h"
+#include "xla/pjrt/proto/pjrt_abi_version.pb.h"
 #include "xla/pjrt/proto/topology_description.pb.h"
 #include "xla/pjrt/scoped_async_tracking_event.h"
 #include "xla/service/computation_placer.h"
