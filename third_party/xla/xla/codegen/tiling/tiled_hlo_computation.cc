ddf24d7e2d705a408f6d79dcec0e009ba2667573
Mikhail Goncharov <goncharov@google.com>
2026-03-28 00:09:14 -0700

    [XLA:GPU] fix printing of tiled computation with regions
    - added test for ToString to make sure we don't break it randomly
    - use spaces instead of tabs (they are annoying)
    - less \n
    - print all region sizes on one line
    
    PiperOrigin-RevId: 890822847

--- third_party/xla/xla/codegen/tiling/tiled_hlo_computation.cc
+++ third_party/xla/xla/codegen/tiling/tiled_hlo_computation.cc
@@ -15,13 +15,17 @@ limitations under the License.
 
 #include "xla/codegen/tiling/tiled_hlo_computation.h"
 
+#include <cstdint>
+#include <memory>
 #include <sstream>
 #include <string>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/inlined_vector.h"
+#include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
+#include "absl/types/span.h"
 #include "xla/codegen/tiling/tiled_hlo_instruction.h"
 #include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/service/name_uniquer.h"
@@ -29,27 +33,50 @@ limitations under the License.
 
 namespace xla {
 
-std::string TiledHloComputation::ToString() const {
+namespace {
+std::string InstructionsToString(
+    absl::Span<const std::unique_ptr<TiledHloInstruction>> instructions,
+    int64_t indent, NameUniquer& name_uniquer,
+    absl::flat_hash_map<const TiledHloInstruction*, std::string>& tile_names) {
+  std::string indentation(indent, ' ');
   std::stringstream ss;
-  NameUniquer name_uniquer("_");
-  absl::flat_hash_map<const TiledHloInstruction*, std::string> tile_names;
-
-  for (const auto* tiled_hlo : instructions()) {
+  for (const std::unique_ptr<xla::TiledHloInstruction>& tiled_hlo :
+       instructions) {
+    absl::InlinedVector<std::string, 4> regions;
+    for (const auto& region : tiled_hlo->regions()) {
+      regions.push_back(
+          InstructionsToString(region, indent + 4, name_uniquer, tile_names));
+    }
     std::string tile_name = name_uniquer.GetUniqueName(
         absl::StrCat(tiled_hlo->hlo()->name(), ".tile_0"));
-    tile_names[tiled_hlo] = tile_name;
-
+    tile_names[tiled_hlo.get()] = tile_name;
     absl::InlinedVector<std::string, 4> operand_names;
     for (const auto& operand : tiled_hlo->operands()) {
-      operand_names.push_back(tile_names.at(operand));
+      if (tile_names.contains(operand)) {
+        operand_names.push_back(tile_names.at(operand));
+      } else {
+        LOG(ERROR) << "Operand " << operand->ToString()
+                   << " not found in tile_names";
+        operand_names.push_back(
+            absl::StrCat("<error ", operand->hlo()->name(), ">"));
+      }
     }
-
-    ss << tile_name << " = " << HloOpcodeString(tiled_hlo->hlo()->opcode())
-       << "(" << absl::StrJoin(operand_names, ", ") << ")\n";
-
-    ss << tiled_hlo->ToString() << "\n";
+    ss << indentation << tile_name << " = "
+       << HloOpcodeString(tiled_hlo->hlo()->opcode()) << "("
+       << absl::StrJoin(operand_names, ", ") << ")\n";
+    ss << tiled_hlo->ToString(indent + 2) << "\n";
+    for (int i = 0; i < regions.size(); ++i) {
+      ss << indentation << "  region." << i << " {\n" << regions[i] << "  }";
+    }
   }
   return ss.str();
+}
+}  // namespace
+
+std::string TiledHloComputation::ToString() const {
+  NameUniquer name_uniquer("_");
+  absl::flat_hash_map<const TiledHloInstruction*, std::string> tile_names;
+  return InstructionsToString(instructions_, 0, name_uniquer, tile_names);
 }
 
 }  // namespace xla
