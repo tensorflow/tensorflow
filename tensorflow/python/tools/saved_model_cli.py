e2b1e42ee4aa74a3289ff3192ac94e624b0cd029
instantraaamen <masa.shiramizu@gmail.com>
2026-04-09 10:59:57 +0900

    Disable pickle deserialization in saved_model_cli input loading
    np.load is called with allow_pickle=True for --inputs, which was added
    in 2019 to keep things working after NumPy 1.16.3 changed the default
    to False (CVE-2019-6446). Normal tensor inputs (float, int, string) do
    not need pickle. Object arrays are not valid SavedModel inputs anyway.
    
    The same file already uses ast.literal_eval for --input_exprs to avoid
    code execution (CVE-2021-41228), so this brings --inputs in line with
    that approach.

--- tensorflow/python/tools/saved_model_cli.py
+++ tensorflow/python/tools/saved_model_cli.py
@@ -959,7 +959,7 @@ def load_inputs_from_input_arg_string(inputs_str, input_exprs_str,
   input_examples = preprocess_input_examples_arg_string(input_examples_str)
 
   for input_tensor_key, (filename, variable_name) in inputs.items():
-    data = np.load(file_io.FileIO(filename, mode='rb'), allow_pickle=True)  # pylint: disable=unexpected-keyword-arg
+    data = np.load(file_io.FileIO(filename, mode='rb'), allow_pickle=False)  # pylint: disable=unexpected-keyword-arg
 
     # When a variable_name key is specified for the input file
     if variable_name:
