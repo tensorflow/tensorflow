6000405709524ce9059b5ae1b17e40b01c1aad22
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-14 21:57:59 -0700

    Automated Code Change
    PiperOrigin-RevId: 915785954

--- third_party/xla/xla/backends/gpu/transforms/collectives/all_gather_optimizer.cc
+++ third_party/xla/xla/backends/gpu/transforms/collectives/all_gather_optimizer.cc
@@ -19,6 +19,8 @@ limitations under the License.
 #include <utility>
 
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_casting_utils.h"
 #include "xla/hlo/ir/hlo_instruction.h"
