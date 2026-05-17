c2ff611cbc0efc7b04230fd217fd8214612cc8fc
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-16 22:25:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 916654763

--- third_party/xla/xla/hlo/transforms/hlo_module_splitter_test.cc
+++ third_party/xla/xla/hlo/transforms/hlo_module_splitter_test.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include <vector>
 
 #include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
 #include "xla/tsl/platform/status_macros.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
