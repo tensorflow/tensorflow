7ffc62d0b85b55a5b8fcc7279f5842ab1f3da91b
Dirk Hornung <dirkh@google.com>
2026-05-20 04:20:40 -0700

    Deprecate enable_cublaslt
    This change deprecates the `xla_gpu_enable_cublaslt` flag.
    
    PiperOrigin-RevId: 918362155

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -1875,7 +1875,7 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       tsl::Flag("xla_gpu_enable_cublaslt",
                 bool_setter_for(&DebugOptions::set_xla_gpu_enable_cublaslt),
                 debug_options->xla_gpu_enable_cublaslt(),
-                "Use cuBLASLt for GEMMs when possible."));
+                "[Deprecated] Use cuBLASLt for GEMMs when possible."));
   flag_list->push_back(tsl::Flag(
       "xla_gpu_enable_command_buffer",
       SetterForRepeatedEnum<DebugOptions::CommandBufferCmdType>(
