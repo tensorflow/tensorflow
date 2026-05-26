fb2d2fd67914749044055c24d323a4a46453d9ec
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-26 07:11:58 -0700

    Automated Code Change
    PiperOrigin-RevId: 921448476

--- tensorflow/dtensor/mlir/layout_propagation_v2.cc
+++ tensorflow/dtensor/mlir/layout_propagation_v2.cc
@@ -25,6 +25,7 @@ limitations under the License.
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
