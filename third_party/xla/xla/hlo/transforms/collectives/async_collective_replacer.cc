75bc31f6eba549d014505b1b9a5f6f916fb9ce5a
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-26 00:47:37 -0700

    Automated Code Change
    PiperOrigin-RevId: 921284908

--- third_party/xla/xla/hlo/transforms/collectives/async_collective_replacer.cc
+++ third_party/xla/xla/hlo/transforms/collectives/async_collective_replacer.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/strings/string_view.h"
 #include "xla/tsl/platform/status_macros.h"
 #include "xla/hlo/ir/hlo_computation.h"
