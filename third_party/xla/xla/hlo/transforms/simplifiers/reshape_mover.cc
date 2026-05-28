e058b5d9ed315a73c28c92e87495f71368eee1c3
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-27 20:03:27 -0700

    Automated Code Change
    PiperOrigin-RevId: 922492568

--- third_party/xla/xla/hlo/transforms/simplifiers/reshape_mover.cc
+++ third_party/xla/xla/hlo/transforms/simplifiers/reshape_mover.cc
@@ -26,6 +26,7 @@ limitations under the License.
 #include "absl/container/inlined_vector.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
