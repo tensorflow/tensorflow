02f4e51818050b7111a24706e1f0df014ec6efd8
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-30 00:30:30 -0700

    Automated Code Change
    PiperOrigin-RevId: 923800481

--- third_party/xla/xla/hlo/parser/hlo_parser.cc
+++ third_party/xla/xla/hlo/parser/hlo_parser.cc
@@ -2592,7 +2592,7 @@ HloInstruction* HloParserImpl::CreateInstruction(  // NOLINT
         *precision_config.mutable_operand_precision() = {
             operand_precision->begin(), operand_precision->end()};
       } else {
-        precision_config.mutable_operand_precision()->Resize(
+        precision_config.mutable_operand_precision()->resize(
             operands.size(), PrecisionConfig::DEFAULT);
       }
       SparsityConfig sparsity_config =
@@ -3414,7 +3414,7 @@ HloInstruction* HloParserImpl::CreateInstruction(  // NOLINT
         *precision_config.mutable_operand_precision() = {
             operand_precision->begin(), operand_precision->end()};
       } else {
-        precision_config.mutable_operand_precision()->Resize(
+        precision_config.mutable_operand_precision()->resize(
             operands.size(), PrecisionConfig::DEFAULT);
       }
       *custom_call_instr->mutable_precision_config() = precision_config;
@@ -3469,7 +3469,7 @@ HloInstruction* HloParserImpl::CreateInstruction(  // NOLINT
         *precision_config.mutable_operand_precision() = {
             operand_precision->begin(), operand_precision->end()};
       } else {
-        precision_config.mutable_operand_precision()->Resize(
+        precision_config.mutable_operand_precision()->resize(
             operands.size(), PrecisionConfig::DEFAULT);
       }
       if (algorithm) {
@@ -3555,7 +3555,7 @@ HloInstruction* HloParserImpl::CreateInstruction(  // NOLINT
             operand_precision->begin(), operand_precision->end()};
       } else {
         // Only the lhs and rhs operands have precision.
-        precision_config.mutable_operand_precision()->Resize(
+        precision_config.mutable_operand_precision()->resize(
             HloRaggedDotInstruction::kOperands - 1, PrecisionConfig::DEFAULT);
       }
       if (!maybe_infer_shape([&] {
@@ -3626,7 +3626,7 @@ HloInstruction* HloParserImpl::CreateInstruction(  // NOLINT
         *precision_config.mutable_operand_precision() = {
             operand_precision->begin(), operand_precision->end()};
       } else {
-        precision_config.mutable_operand_precision()->Resize(
+        precision_config.mutable_operand_precision()->resize(
             2, PrecisionConfig::DEFAULT);
       }
       if (algorithm) {
