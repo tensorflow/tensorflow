648c2c580bd158eae73a26bc3a2db6e7008cbe1a
A. Unique TensorFlower <gardener@tensorflow.org>
2026-05-31 19:40:33 -0700

    Automated Code Change
    PiperOrigin-RevId: 924420461

--- third_party/xla/xla/hlo/tools/hlo_diff/matchers/bipartite_matching_test.cc
+++ third_party/xla/xla/hlo/tools/hlo_diff/matchers/bipartite_matching_test.cc
@@ -33,6 +33,7 @@ limitations under the License.
 #include "xla/service/hlo_module_config.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/platform/statusor.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla::hlo_diff {
 namespace {
