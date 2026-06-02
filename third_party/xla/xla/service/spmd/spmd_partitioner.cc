6351f73b029c42629595df1eaaade58fc3e3cd77
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-01 20:34:42 -0700

    Automated Code Change
    PiperOrigin-RevId: 925073594

--- third_party/xla/xla/service/spmd/spmd_partitioner.cc
+++ third_party/xla/xla/service/spmd/spmd_partitioner.cc
@@ -17,6 +17,7 @@ limitations under the License.
 
 #include <algorithm>
 #include <array>
+#include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <iterator>
@@ -35,6 +36,7 @@ limitations under the License.
 #include "absl/container/inlined_vector.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
