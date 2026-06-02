21407994a5f1d5f0eb052b352bd8fa626198cc58
Robert David <lrdx@google.com>
2026-06-02 15:05:02 -0700

    Fix overflow in 32 bit ARM NEON kernel for 4-bit fully connected op, and add overflow test.
    The 32 bit assembly used int16 for accumulation. Change it to use int32, similarly to what the 64 bit non-sdot code path was doing.
    
    PiperOrigin-RevId: 925599217

--- tensorflow/lite/kernels/internal/optimized/4bit/neon_fully_connected_arm32.cc
+++ tensorflow/lite/kernels/internal/optimized/4bit/neon_fully_connected_arm32.cc
@@ -64,11 +64,11 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
           R"asm(
           vmov.i8 q14, #15
           vld1.8 {q4}, [%[lhs_ptr]]!
-          vmov.i16 q0, #0
-          vmov.i16 q1, #0
+          vmov.i32 q0, #0
+          vmov.i32 q1, #0
           vld1.8 {q5}, [%[lhs_ptr]]!
-          vmov.i16 q2, #0
-          vmov.i16 q3, #0
+          vmov.i32 q2, #0
+          vmov.i32 q3, #0
           vld1.8 {q6}, [%[lhs_ptr]]!
           vand q8, q4, q14
           vand q9, q5, q14
@@ -85,24 +85,28 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
           subs r3, r3, #1
           bls 1f /* skip loop */
             0: /* loop start */
-            vmlal.s8 q0, d8, d24
-            vmlal.s8 q1, d10, d24
-            vmlal.s8 q0, d9, d25
-            vmlal.s8 q1, d11, d25
+            vmull.s8 q15, d8, d24
+            vmlal.s8 q15, d9, d25
+            vmlal.s8 q15, d16, d26
+            vmlal.s8 q15, d17, d27
+            vpadal.s16 q0, q15
+            vmull.s8 q15, d10, d24
+            vmlal.s8 q15, d11, d25
+            vmlal.s8 q15, d18, d26
+            vmlal.s8 q15, d19, d27
+            vpadal.s16 q1, q15
             vld1.8 {q4, q5}, [%[lhs_ptr]]!
-            vmlal.s8 q2, d12, d24
-            vmlal.s8 q3, d14, d24
-            vmlal.s8 q2, d13, d25
-            vmlal.s8 q3, d15, d25
+            vmull.s8 q15, d12, d24
+            vmlal.s8 q15, d13, d25
+            vmlal.s8 q15, d20, d26
+            vmlal.s8 q15, d21, d27
+            vpadal.s16 q2, q15
+            vmull.s8 q15, d14, d24
+            vmlal.s8 q15, d15, d25
+            vmlal.s8 q15, d22, d26
+            vmlal.s8 q15, d23, d27
+            vpadal.s16 q3, q15
             vld1.8 {q6, q7}, [%[lhs_ptr]]!
-            vmlal.s8 q0, d16, d26
-            vmlal.s8 q1, d18, d26
-            vmlal.s8 q2, d20, d26
-            vmlal.s8 q3, d22, d26
-            vmlal.s8 q0, d17, d27
-            vmlal.s8 q1, d19, d27
-            vmlal.s8 q2, d21, d27
-            vmlal.s8 q3, d23, d27
             vld1.8 {d24, d25, d26, d27}, [%[rhs_ptr]]!
             vand q8, q4, q14
             vand q9, q5, q14
@@ -115,41 +119,42 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
             subs r3, r3, #1
             bhi 0b /* loop branch */
           1: /* loop end */
-          vmlal.s8 q0, d8, d24
-          vmlal.s8 q1, d10, d24
-          vmlal.s8 q2, d12, d24
-          vmlal.s8 q3, d14, d24
-          vmlal.s8 q0, d9, d25
-          vmlal.s8 q1, d11, d25
-          vmlal.s8 q2, d13, d25
-          vmlal.s8 q3, d15, d25
-          vmlal.s8 q0, d16, d26
-          vmlal.s8 q1, d18, d26
-          vmlal.s8 q2, d20, d26
-          vmlal.s8 q3, d22, d26
-          vmlal.s8 q0, d17, d27
-          vmlal.s8 q1, d19, d27
-          vmlal.s8 q2, d21, d27
-          vmlal.s8 q3, d23, d27
-          vpaddl.s16 q4, q0
-          vpaddl.s16 q5, q1
-          vpaddl.s16 q6, q2
-          vpaddl.s16 q7, q3
-          vpadd.i32 d0, d8, d9
-          vpadd.i32 d1, d10, d11
-          vpadd.i32 d2, d12, d13
-          vpadd.i32 d3, d14, d15
+          vmull.s8 q15, d8, d24
+          vmlal.s8 q15, d9, d25
+          vmlal.s8 q15, d16, d26
+          vmlal.s8 q15, d17, d27
+          vpadal.s16 q0, q15
+          vmull.s8 q15, d10, d24
+          vmlal.s8 q15, d11, d25
+          vmlal.s8 q15, d18, d26
+          vmlal.s8 q15, d19, d27
+          vpadal.s16 q1, q15
+          vmull.s8 q15, d12, d24
+          vmlal.s8 q15, d13, d25
+          vmlal.s8 q15, d20, d26
+          vmlal.s8 q15, d21, d27
+          vpadal.s16 q2, q15
+          vmull.s8 q15, d14, d24
+          vmlal.s8 q15, d15, d25
+          vmlal.s8 q15, d22, d26
+          vmlal.s8 q15, d23, d27
+          vpadal.s16 q3, q15
+          vpadd.i32 d0, d0, d1
+          vpadd.i32 d1, d2, d3
+          vpadd.i32 d2, d4, d5
+          vpadd.i32 d3, d6, d7
           vpadd.i32 d4, d0, d1
           vpadd.i32 d5, d2, d3
           vst1.32 {d4, d5}, [%[dst]]!
           )asm"
           : [lhs_ptr] "+r"(lhs_ptr), [rhs_ptr] "+r"(rhs_ptr), [dst] "+r"(dst)
           : [run_depth] "r"(run_depth)
-          : "cc", "memory", "r3", "d0", "d1", "d2", "d3", "d4", "d5", "d8",
-            "d9", "d10", "d11", "d12", "d13", "d14", "d15", "d16", "d17", "d18",
-            "d19", "d20", "d21", "d22", "d23", "d24", "d25", "d26", "d27", "q0",
-            "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
-            "q14");
+          : "cc", "memory", "r3", "d0", "d1", "d2", "d3", "d4", "d5", "d6",
+            "d7", "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15", "d16",
+            "d17", "d18", "d19", "d20", "d21", "d22", "d23", "d24", "d25",
+            "d26", "d27", "d28", "d29", "d30", "d31", "q0", "q1", "q2", "q3",
+            "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13",
+            "q14", "q15");
     }
   }
 }
