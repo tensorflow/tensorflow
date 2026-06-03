8f365f4fc537bb807ae036106d201a5ef7e0fb62
Benjamin Chetioui <bchetioui@google.com>
2026-06-03 15:54:16 -0700

    [XLA:GPU] Disable lowering ragged dot to cuDNN.
    This breaks JAX tests on cuDNN 9.22 on both H100 and B200: https://github.com/jax-ml/jax/actions/runs/26268451560/job/77317768109
    
    PiperOrigin-RevId: 926320524

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -499,7 +499,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_experimental_ragged_all_to_all_zero_copy(true);
   opts.set_xla_gpu_experimental_use_ragged_dot_grouped_gemm(true);
   opts.set_xla_gpu_native_emitter_tune_unroll_factor_for_loops(false);
-  opts.set_xla_gpu_experimental_use_ragged_dot_fusion(true);
+  opts.set_xla_gpu_experimental_use_ragged_dot_fusion(false);
 
   opts.set_xla_cpu_collective_call_warn_stuck_seconds(20);
   opts.set_xla_cpu_collective_call_terminate_timeout_seconds(40);
