07d30d2dec48b3334db47a8798ff5d971f28d7fe
Om Thakkar <om.thakkar@intel.com>
2026-06-03 00:56:37 -0700

    PR #41989: [XLA:CPU][oneDNN] Refactor TSL oneDNN threadpool for async oneDNN support
    Imported from GitHub PR https://github.com/openxla/xla/pull/41989
    
    Part 1 of 3 to replace #38198.
    
    This PR refactors the TSL `onednn_threadpool` integration in XLA:CPU to support asynchronous oneDNN execution (to be enabled later).
    Copybara import of the project:
    
    --
    0bcec7d0598a92132ad8f9aa86e6754c3d58533e by Om Thakkar <om.thakkar@intel.com>:
    
    refactor tsl onednn_threadpool to work with async oneDNN
    
    --
    309a2070628f8064e3ead2180156c770821aa520 by Om Thakkar <om.thakkar@intel.com>:
    
    add comments
    
    --
    952437bc5d6e3db4bbd8578c665054ed780c3517 by Om Thakkar <om.thakkar@intel.com>:
    
    update comments
    
    Merging this change closes #41989
    
    PiperOrigin-RevId: 925844550

--- third_party/xla/xla/tsl/util/onednn_threadpool.h
+++ third_party/xla/xla/tsl/util/onednn_threadpool.h
@@ -94,13 +94,19 @@ class OneDnnThreadPool : public threadpool_iface {
   bool get_in_parallel() const override {
     return (eigen_interface_->CurrentThreadId() != -1) ? true : false;
   }
-  uint64_t get_flags() const override { return ASYNCHRONOUS; }
 #ifdef ENABLE_ONEDNN_ASYNC
+  // Return 0 for synchronous execution; caller handles synchronization.
+  uint64_t get_flags() const override { return 0; }
   // wait() method for synchronous execution is basically a no-op.
   // But we need to implement it to satisfy the interface.
   // This is the requirement of the new experimental async runtime support
   // in oneDNN.
   virtual void wait() override {}
+#else
+  // Return ASYNCHRONOUS to work with current onednn version.
+  // TODO(intel-tf): remove this ifdefing block after making oneDNN v3.11 (async
+  // support) default
+  uint64_t get_flags() const override { return ASYNCHRONOUS; }
 #endif  // ENABLE_ONEDNN_ASYNC
   void parallel_for(int n, const std::function<void(int, int)>& fn) override {
     // Should never happen (handled by DNNL)
@@ -125,6 +131,23 @@ class OneDnnThreadPool : public threadpool_iface {
     const int njobs_to_schedule = use_caller_thread ? njobs - 1 : njobs;
 
     if (use_caller_thread) {
+#ifdef ENABLE_ONEDNN_ASYNC
+      // Add barriers for synchronization when ENABLE_ONEDNN_ASYNC is defined.
+      // TODO(intel-tf): remove this ifdefing block after making oneDNN v3.11
+      // (async support) default
+      absl::BlockingCounter counter(njobs_to_schedule + 1);
+      for (int i = 0; i < njobs_to_schedule; i++) {
+        eigen_interface_->ScheduleWithHint(
+            [balance, i, n, njobs, fn, &counter]() {
+              run_jobs(balance, i, n, njobs, fn);
+              counter.DecrementCount();
+            },
+            i, i + 1);
+      }
+      run_jobs(balance, njobs_to_schedule, n, njobs, fn);
+      counter.DecrementCount();
+      counter.Wait();
+#else
       for (int i = 0; i < njobs_to_schedule; i++) {
         eigen_interface_->ScheduleWithHint(
             [balance, i, n, njobs, fn]() {
@@ -133,6 +156,7 @@ class OneDnnThreadPool : public threadpool_iface {
             i, i + 1);
       }
       run_jobs(balance, njobs_to_schedule, n, njobs, fn);
+#endif  // ENABLE_ONEDNN_ASYNC
     } else {
       absl::BlockingCounter counter(njobs);
       std::function<void(int, int)> handle_range = [=, &handle_range, &counter](
