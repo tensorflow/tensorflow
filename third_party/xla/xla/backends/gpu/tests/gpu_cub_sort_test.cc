b5e21830259b2fd23cf85cfdc28d40c27ef5f4e7
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-03 07:16:26 -0700

    Automated Code Change
    PiperOrigin-RevId: 926008939

--- third_party/xla/xla/backends/gpu/tests/gpu_cub_sort_test.cc
+++ third_party/xla/xla/backends/gpu/tests/gpu_cub_sort_test.cc
@@ -37,6 +37,7 @@ limitations under the License.
 #include "xla/shape_util.h"
 #include "xla/tests/hlo_pjrt_interpreter_reference_mixin.h"
 #include "xla/tests/hlo_pjrt_test_base.h"
+#include "xla/xla.pb.h"
 #include "xla/xla_data.pb.h"
 
 namespace xla {
