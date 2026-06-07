4f8bc2ea01371a173137ea6af5eef6de770c3e48
abolshov <artem.e.bolshov@gmail.com>
2026-06-06 19:25:37 -0500

    change argument of sizeof to int64_t

--- tensorflow/core/kernels/sparse_cross_op.cc
+++ tensorflow/core/kernels/sparse_cross_op.cc
@@ -236,7 +236,7 @@ int64_t KeyedDenseTensorColumn<int64_t>::Feature(int64_t batch, int64_t n,
     return StrongKeyedHash(
         key_,
         {reinterpret_cast<const char*>(&tensor_.matrix<int64_t>()(batch, n)),
-         sizeof(tensor_.dtype())});
+         sizeof(int64_t)});
   }
   if (DT_STRING == tensor_.dtype())
     return Fingerprint64(tensor_.matrix<tstring>()(batch, n));
