bddce6bac90f9fbf51e65b31035a4ff2ab172aae
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-08 02:40:53 -0700

    Automated Code Change
    PiperOrigin-RevId: 928438278

--- third_party/xla/xla/stream_executor/device_description.cc
+++ third_party/xla/xla/stream_executor/device_description.cc
@@ -34,6 +34,7 @@ limitations under the License.
 #include "xla/tsl/lib/math/math_util.h"
 #include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/util/sorted_range.h"
+#include "xla/xla_data.pb.h"
 
 namespace stream_executor {
 
