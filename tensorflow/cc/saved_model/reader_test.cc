23fec843713cdfdb71e877a3d37781148d3658fe
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-07 19:45:24 -0700

    Automated Code Change
    PiperOrigin-RevId: 928283731

--- tensorflow/cc/saved_model/reader_test.cc
+++ tensorflow/cc/saved_model/reader_test.cc
@@ -15,16 +15,24 @@ limitations under the License.
 
 #include "tensorflow/cc/saved_model/reader.h"
 
+#include <algorithm>
+#include <memory>
+
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/status.h"
+#include "absl/strings/match.h"
 #include "tensorflow/cc/saved_model/constants.h"
 #include "tensorflow/cc/saved_model/metrics.h"
 #include "tensorflow/cc/saved_model/tag_constants.h"
+#include "tensorflow/core/framework/graph_debug_info.pb.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/lib/core/status_test_util.h"
 #include "tensorflow/core/lib/io/path.h"
 #include "tensorflow/core/lib/strings/str_util.h"
 #include "tensorflow/core/platform/path.h"
 #include "tensorflow/core/platform/resource_loader.h"
+#include "tensorflow/core/protobuf/meta_graph.pb.h"
 
 namespace tensorflow {
 namespace {
