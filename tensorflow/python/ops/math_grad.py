2f202d2fdafba4adc0b053d3e58ede2799352a0f
Rachit-Mangawa <145089194+Rachit-Mangawa@users.noreply.github.com>
2026-06-11 22:18:03 +0530

    Fix incorrect bessel_i1 gradient at x=0

--- tensorflow/python/ops/math_grad.py
+++ tensorflow/python/ops/math_grad.py
@@ -1019,13 +1019,13 @@ def _BesselI1Grad(op: ops.Operation, grad):
   x = op.inputs[0]
   y = op.outputs[0]
   with ops.control_dependencies([grad]):
-    # For x = 0, the correct gradient is 1.0.
+    # For x = 0, the correct gradient is 0.5.
     # However, the main branch gives NaN because of the division by x, so
     # we impute the gradient manually.
     # An alternative solution is to express the gradient via bessel_i0 and
     # bessel_i2, but the latter is not yet implemented in Eigen.
     dy_dx = array_ops.where_v2(
-        math_ops.equal(x, 0.), math_ops.cast(1., x.dtype),
+        math_ops.equal(x, 0.), math_ops.cast(0.5, x.dtype),
         special_math_ops.bessel_i0(x) - math_ops.div(y, x))
     return grad * dy_dx
 
--- tensorflow/python/ops/special_math_ops_test.py
+++ tensorflow/python/ops/special_math_ops_test.py
@@ -590,6 +590,16 @@ class BesselTest(test.TestCase, parameterized.TestCase):
         special_math_ops.bessel_i1e, inputs)
     self.assertLess(gradient_checker_v2.max_error(analytical, numerical), 1e-4)
 
+  def test_besseli_gradient_at_zero(self):
+    inputs = [np.array([0.], dtype=np.float32)]
+    analytical, numerical = gradient_checker_v2.compute_gradient(
+        special_math_ops.bessel_i1, inputs)
+    self.assertLess(gradient_checker_v2.max_error(analytical, numerical), 1e-4)
+
+    analytical, numerical = gradient_checker_v2.compute_gradient(
+        special_math_ops.bessel_i1e, inputs)
+    self.assertLess(gradient_checker_v2.max_error(analytical, numerical), 1e-4)
+
   def test_besselj_gradient(self):
     inputs = [np.random.uniform(-50., 50., size=int(1e2))]
     analytical, numerical = gradient_checker_v2.compute_gradient(
