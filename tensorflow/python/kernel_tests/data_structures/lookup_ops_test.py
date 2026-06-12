494f3920a9bcf714045f3d3b0a782b00fb063129
wilmerdooley <wilmerdooley1@gmail.com>
2026-06-12 16:17:35 +0000

    fix(lookup_ops): include dtypes in TextFileInitializer shared_name
    Signed-off-by: wilmerdooley <wilmerdooley1@gmail.com>

--- tensorflow/python/kernel_tests/data_structures/lookup_ops_test.py
+++ tensorflow/python/kernel_tests/data_structures/lookup_ops_test.py
@@ -888,6 +888,81 @@ class InitializeTableFromFileOpTest(BaseLookupTableTest):
       self.assertAllEqual([0, 1, -1], out2)
       self.assertAllEqual([0, 1, -1], out3)
 
+  def testInitializeSameFileWithDifferentDtypes(self, is_anonymous):
+    if is_anonymous and not tf2.enabled():
+      self.skipTest(SKIP_ANONYMOUS_IN_TF1_REASON)
+    vocabulary_file = self._createVocabFile(
+        "one_column_dtypes.txt", ("1", "2", "3"))
+
+    with self.cached_session():
+      default_value = -1
+      init1 = lookup_ops.TextFileInitializer(
+          vocabulary_file, dtypes.string, lookup_ops.TextFileIndex.WHOLE_LINE,
+          dtypes.int64, lookup_ops.TextFileIndex.LINE_NUMBER)
+      self.assertIn("one_column_dtypes.txt_-2_-1_string_int64",
+                    init1._shared_name)
+      table1 = self.getHashTable()(
+          init1, default_value, experimental_is_anonymous=is_anonymous)
+      init2 = lookup_ops.TextFileInitializer(
+          vocabulary_file, dtypes.int64, lookup_ops.TextFileIndex.WHOLE_LINE,
+          dtypes.int64, lookup_ops.TextFileIndex.LINE_NUMBER)
+      self.assertIn("one_column_dtypes.txt_-2_-1_int64_int64",
+                    init2._shared_name)
+      self.assertNotEqual(init1._shared_name, init2._shared_name)
+      table2 = self.getHashTable()(
+          init2, default_value, experimental_is_anonymous=is_anonymous)
+
+      self.evaluate(lookup_ops.tables_initializer())
+
+      output1 = table1.lookup(
+          constant_op.constant(["1", "2", "4"], dtype=dtypes.string))
+      output2 = table2.lookup(
+          constant_op.constant([1, 2, 4], dtype=dtypes.int64))
+
+      out1, out2 = self.evaluate([output1, output2])
+      self.assertAllEqual([0, 1, -1], out1)
+      self.assertAllEqual([0, 1, -1], out2)
+
+  def testTextFileInitializerSharedNameCombinations(self, is_anonymous):
+    del is_anonymous
+    filename = "test_vocab.txt"
+    key_dtype = dtypes.string
+    key_index = lookup_ops.TextFileIndex.WHOLE_LINE
+    value_dtype = dtypes.int64
+    value_index = lookup_ops.TextFileIndex.LINE_NUMBER
+
+    for vocab_size in (None, 10):
+      for offset in (0, 5):
+        init = lookup_ops.TextFileInitializer(
+            filename,
+            key_dtype,
+            key_index,
+            value_dtype,
+            value_index,
+            vocab_size=vocab_size,
+            value_index_offset=offset)
+
+        if vocab_size:
+          if offset:
+            expected = "hash_table_%s_%d_%s_%s_%s_%s_%s" % (
+                filename, vocab_size, key_index, value_index, offset,
+                key_dtype.name, value_dtype.name)
+          else:
+            expected = "hash_table_%s_%d_%s_%s_%s_%s" % (
+                filename, vocab_size, key_index, value_index, key_dtype.name,
+                value_dtype.name)
+        else:
+          if offset:
+            expected = "hash_table_%s_%s_%s_%s_%s_%s" % (
+                filename, key_index, value_index, offset, key_dtype.name,
+                value_dtype.name)
+          else:
+            expected = "hash_table_%s_%s_%s_%s_%s" % (
+                filename, key_index, value_index, key_dtype.name,
+                value_dtype.name)
+
+        self.assertEqual(expected, init._shared_name)
+
   def testInitializeTableWithNoFilename(self, is_anonymous):
     with self.cached_session():
       default_value = -1
