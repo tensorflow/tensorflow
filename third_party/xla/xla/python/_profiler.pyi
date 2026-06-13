fbourgey/jax

[XLA:CPU] Implement jax.experimental.profiler.measure for XLA:CPU
This change implements a programmatic, low-overhead CPU profiling API (`jax.experimental.profiler.measure`) in JAX for XLA:CPU.
It allows measuring the execution time of JAX functions on CPU, capturing the duration of individual XLA thunks while excluding compilation warmup.

Key features:
- Programmatic profiling using C++ ProfilerSession.
- Support for total aggregated duration or detailed thunk-by-thunk breakdown.
- Support for profiling over multiple iterations with sequential blocking and thunk name consistency checks.
- Robust error handling for concurrent profiling sessions (collisions) and warnings for empty traces (e.g., eager execution).

PiperOrigin-RevId: 926632336

jax/_src/lib/_profiler.pyi
@@ -16,6 +16,7 @@ class ProfilerSession:
   def stop(self) -> bytes: ...
   def stop_and_export(self, tensorboard_dir: str) -> None: ...
   def export(self, xspace: bytes, tensorboard_dir: str) -> None:...
+  def stop_and_get_profile_data(self) -> Any: ...
 
 class ProfileOptions:
   include_dataset_ops: bool

tensorflow/tensorflow

third_party/xla/xla/python/_profiler.pyi
@@ -33,7 +33,7 @@ class ProfilerSession:
   def __init__(self, arg: ProfileOptions, /) -> None: ...
   def stop_and_export(self, arg: str, /) -> None: ...
   def stop(self) -> bytes: ...
-  def stop_and_get_profile_data() -> ProfileData: ...
+  def stop_and_get_profile_data(self) -> ProfileData: ...
   def export(self, arg0: bytes, arg1: str, /) -> None: ...
 
 class ProfileOptions: