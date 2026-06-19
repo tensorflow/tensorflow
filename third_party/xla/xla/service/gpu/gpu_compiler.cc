564c800df5e68e48a0ddc781d57fc79e2a700778
Levon Ter-Grigoryan <patrios@google.com>
2026-06-19 08:32:36 -0700

    [XLA:GPU] Remove unused parameters from collective backend assigner.
    PiperOrigin-RevId: 934942803

--- third_party/xla/xla/service/gpu/gpu_compiler.cc
+++ third_party/xla/xla/service/gpu/gpu_compiler.cc
@@ -1381,9 +1381,7 @@ void AddCollectiveCombinerPasses(
 
   // Assign collective backends after combining, so that combined collectives
   // get the correct backend config.
-  pipeline.AddPass<CollectiveBackendAssigner>(
-      device_description.gpu_compute_capability(),
-      num_visible_devices_per_process, options.slice_size);
+  pipeline.AddPass<CollectiveBackendAssigner>();
 
   // Annotate AllReduce ops with the Triton kernel strategy (one-shot /
   // two-shot) that will be used at runtime. This annotation is consumed by
