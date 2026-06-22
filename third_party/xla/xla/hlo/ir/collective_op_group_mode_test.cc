2b09825c0c2792b26aeb64417e55bd991a13bc25
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-21 19:03:14 -0700

    Automated Code Change
    PiperOrigin-RevId: 935783102

--- third_party/xla/xla/hlo/ir/collective_op_group_mode_test.cc
+++ third_party/xla/xla/hlo/ir/collective_op_group_mode_test.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "absl/status/statusor.h"
 #include "xla/tsl/lib/core/status_test_util.h"
+#include "xla/xla_data.pb.h"
 
 namespace xla {
 namespace {
