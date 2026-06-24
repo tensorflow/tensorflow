e620f46ecc49e6910de31ae10b77ae59224e9422
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-23 18:58:55 -0700

    Automated Code Change
    PiperOrigin-RevId: 937023620

--- tensorflow/compiler/mlir/quantization/tensorflow/passes/prepare_quantize.cc
+++ tensorflow/compiler/mlir/quantization/tensorflow/passes/prepare_quantize.cc
@@ -42,8 +42,10 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/quantization/common/quantization_lib/quantization_driver.h"
 #include "tensorflow/compiler/mlir/quantization/common/quantization_lib/quantization_utils.h"
 #include "tensorflow/compiler/mlir/quantization/tensorflow/ops/tf_op_quant_spec.h"
+#include "tensorflow/compiler/mlir/quantization/tensorflow/quantization_options.pb.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_dialect.h"
 #include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"  // IWYU pragma: keep
+#include "tensorflow/core/framework/types.pb.h"
 
 //===----------------------------------------------------------------------===//
 // The prepare-quantize Pass.
