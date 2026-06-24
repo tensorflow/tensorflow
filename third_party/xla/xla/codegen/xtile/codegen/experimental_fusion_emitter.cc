5b06996b64b11c6044f2a32b7019138f6d5ae0a4
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-23 22:55:37 -0700

    Automated Code Change
    PiperOrigin-RevId: 937109770

--- third_party/xla/xla/codegen/xtile/codegen/experimental_fusion_emitter.cc
+++ third_party/xla/xla/codegen/xtile/codegen/experimental_fusion_emitter.cc
@@ -25,6 +25,7 @@ limitations under the License.
 #include "absl/log/check.h"
 #include "absl/log/log.h"
 #include "absl/status/status.h"
+#include "absl/status/status_macros.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
