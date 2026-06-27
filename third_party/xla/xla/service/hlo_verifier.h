992f5f20457e8b51d00b465bd836da6b279f6bd2
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-26 21:22:45 -0700

    Combined folded changes from revisions: 467224b7-f9bc11b5
    PiperOrigin-RevId: 938913156

--- third_party/xla/xla/service/hlo_verifier.h
+++ third_party/xla/xla/service/hlo_verifier.h
@@ -345,10 +345,20 @@ class ShapeVerifier : public DfsHloVisitor {
                                         const HloComputation* computation,
                                         int64_t parameter_number);
 
+  // Checks that the shape of the output of the given async instruction
+  absl::Status CheckAsyncOpOutputShape(const HloInstruction* async_op);
+  // Checks that the shape of the given async op's operands.
+  absl::Status CheckAsyncOpOperands(const HloInstruction* async_op);
+  absl::Status CheckAsyncStartOperands(const HloInstruction* async_start);
+  absl::Status CheckAsyncUpdateOperands(const HloInstruction* async_update);
+  absl::Status CheckAsyncDoneOperands(const HloInstruction* async_done);
+
   // Checks that the shape of async op operands and results match the called
   // computation parameters and root.
   absl::Status CheckAsyncOpComputationShapes(const HloInstruction* async_op,
                                              const Shape& async_shape);
+
+  absl::Status CheckAsyncOpAliasConfig(const HloInstruction* async_op);
 
   // Returns true if the shapes of the two operands have the same element type,
   // and the result shape either has the same element type as the operand shapes
