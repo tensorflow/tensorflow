75c8f8242a3230e925b19691393add89f5aaa010
Jayden Jeswin Raj <user.josephms@gmail.com>
2026-06-29 13:55:15 +0530

    Add regression test for SparseConcat shape overflow

--- tensorflow/python/kernel_tests/sparse_ops/sparse_concat_op_test.py
+++ tensorflow/python/kernel_tests/sparse_ops/sparse_concat_op_test.py
@@ -413,5 +413,25 @@ class SparseConcatTest(test.TestCase):
         self.evaluate(concat_op)
 
 
+  def testSparseConcatLargeShapeOverflow(self):
+    indices1 = constant_op.constant(
+        [[0, 0, 0], [1, 1, 0], [2, 0, 1]], dtype=dtypes.int64)
+    values1 = constant_op.constant(["tensor", "flow", "test"])
+    shape1 = constant_op.constant([5, 2, 2147483647], dtype=dtypes.int64)
+
+    indices2 = constant_op.constant(
+        [[0, 1, 0], [1, 0, 0], [2, 1, 1], [3, 0, 1]], dtype=dtypes.int64)
+    values2 = constant_op.constant(["a", "b", "c", "d"])
+    shape2 = constant_op.constant(
+        [5, 1879048192, 2147483647], dtype=dtypes.int64)
+
+    sp1 = sparse_tensor.SparseTensor(indices1, values1, shape1)
+    sp2 = sparse_tensor.SparseTensor(indices2, values2, shape2)
+
+    with self.assertRaisesOpError(
+        "Encountered overflow from large input shape."
+    ):
+      self.evaluate(sparse_ops.sparse_concat(axis=1, sp_inputs=[sp1, sp2]))
+
 if __name__ == "__main__":
   test.main()
