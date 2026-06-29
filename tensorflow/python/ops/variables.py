74227e852b702225c25b3140c7d2f164eb004580
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2026-06-29 22:18:01 +0300

    Fix indentation for experimental_enable_variable_lifting

--- tensorflow/python/ops/variables.py
+++ tensorflow/python/ops/variables.py
@@ -1374,7 +1374,7 @@ class Variable(trackable.Trackable, metaclass=VariableMetaclass):
         aggregation=aggregation,
         shape=shape,
         experimental_enable_variable_lifting=(
-          experimental_enable_variable_lifting),
+            experimental_enable_variable_lifting),
         **kwargs
     )
 
