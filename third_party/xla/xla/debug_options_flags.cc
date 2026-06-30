317bfee7eb2294ec44b332ef056239fff15fad51
Ilia Sergachev <isergachev@nvidia.com>
2026-06-30 04:10:11 -0700

    PR #45001: [GPU] Enable PDL launch instruction insertion by default.
    Imported from GitHub PR https://github.com/openxla/xla/pull/45001
    
    See https://github.com/openxla/xla/pull/43894 for details.
    
    Copybara import of the project:
    
    --
    bf2848135cb04cda87c815eaa89a648db7b7d445 by Ilia Sergachev <isergachev@nvidia.com>:
    
    [GPU] Enable PDL launch instruction insertion by default.
    
    See https://github.com/openxla/xla/pull/43894 for details.
    
    Merging this change closes #45001
    
    PiperOrigin-RevId: 940372990

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -521,7 +521,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_print_compilation_stats(false);
 
   opts.set_xla_gpu_enable_pdl(true);
-  opts.set_xla_gpu_enable_pdl_launch(false);
+  opts.set_xla_gpu_enable_pdl_launch(true);
   opts.set_xla_gpu_command_buffer_update_mode(DebugOptions::ALWAYS_UPDATE);
 
   opts.set_xla_gpu_experimental_aot_compiled_thunks(true);
