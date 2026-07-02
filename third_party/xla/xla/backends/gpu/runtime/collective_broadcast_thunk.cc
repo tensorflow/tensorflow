323c00003d4805bf79659f6f7532d7bc005b0a97
TJ Xu <tjx@nvidia.com>
2026-07-02 09:08:45 -0700

    PR #44235: [NVIDIA GPU] Thunk change for dynamic root collective bcast
    Imported from GitHub PR https://github.com/openxla/xla/pull/44235
    
    Follow-up pr of [this](https://github.com/openxla/xla/pull/44145).
    This adds a metadata to the collectivfe broadcast thunk to host the cpu buffers for root ranks.
    Ranks will be copied to host for each invocation.
    I will push the e2e test after the above pr is merged so it doesn't fail all the CIs.
    Copybara import of the project:
    
    --
    61b8c331c670136cdd9c52eb78f2cda25a074196 by TJ Xu <tjx@nvidia.com>:
    
    Thunk change for dynamic root collective bcast
    
    --
    a57cefdf76e9768abe59fe2054ee0ffadadcba0c by TJ Xu <tjx@nvidia.com>:
    
    address comments
    
    Merging this change closes #44235
    
    PiperOrigin-RevId: 941727545

--- third_party/xla/xla/backends/gpu/runtime/collective_broadcast_thunk.cc
+++ third_party/xla/xla/backends/gpu/runtime/collective_broadcast_thunk.cc
@@ -55,10 +55,11 @@ CollectiveBroadcastThunk::CollectiveBroadcastThunk(ThunkInfo thunk_info,
 
 CollectiveBroadcastThunk::CollectiveBroadcastThunk(
     ThunkInfo thunk_info, const HloCollectiveBroadcastInstruction* instr,
-    std::vector<Buffer> buffers, bool p2p_memcpy_enabled)
+    std::vector<Buffer> buffers, bool p2p_memcpy_enabled, bool has_dynamic_root)
     : CollectiveThunk(Thunk::kCollectiveBroadcast, thunk_info,
                       std::move(buffers)),
-      config_(GetCollectiveConfig(instr, std::nullopt)) {}
+      config_(GetCollectiveConfig(instr, std::nullopt)),
+      has_dynamic_root_(has_dynamic_root) {}
 
 /*static*/ absl::Status CollectiveBroadcastThunk::CheckImplementable(
     const HloInstruction* instr, int64_t replica_count,
@@ -69,6 +70,25 @@ CollectiveBroadcastThunk::CollectiveBroadcastThunk(
 /*static*/ CollectiveOpGroupMode CollectiveBroadcastThunk::GetGroupMode(
     const HloCollectiveBroadcastInstruction* inst) {
   return GetCollectiveConfig(inst, std::nullopt).group_mode;
+}
+
+absl::Status CollectiveBroadcastThunk::Initialize(
+    const InitializeParams& params) {
+  se::StreamExecutor* executor = params.executor;
+  CollectiveBroadcastMetadata* cb_metadata = nullptr;
+  {
+    absl::MutexLock lock(&mutex_);
+    cb_metadata = per_executor_cb_metadata_[executor].get();
+  }
+  if (has_dynamic_root_ && cb_metadata->bcast_roots == nullptr) {
+    // Last operand is the dynamic root buffer which contains actual root ranks
+    cb_metadata->num_roots = buffers().size() - 1;
+    ASSIGN_OR_RETURN(
+        std::unique_ptr<se::MemoryAllocation> alloc,
+        executor->HostMemoryAllocate(cb_metadata->num_roots * sizeof(int32_t)));
+    cb_metadata->bcast_roots = std::move(alloc);
+  }
+  return absl::OkStatus();
 }
 
 absl::StatusOr<std::unique_ptr<CollectiveBroadcastThunk>>
@@ -113,21 +133,52 @@ absl::Status CollectiveBroadcastThunk::RunCollective(
   ASSIGN_OR_RETURN(std::vector<DeviceBufferPair> device_buffers,
                    ConvertToDeviceBuffers(params.buffer_allocations, buffers(),
                                           config_.operand_element_type));
-  return ::xla::gpu::RunCollectiveBroadcast(device_buffers, stream, comm);
+  CollectiveBroadcastMetadata* cb_metadata = nullptr;
+  {
+    absl::MutexLock lock(&mutex_);
+    cb_metadata = per_executor_cb_metadata_[stream.parent()].get();
+  }
+
+  return ::xla::gpu::RunCollectiveBroadcast(device_buffers, stream, comm,
+                                            cb_metadata);
 }
 
 absl::Status RunCollectiveBroadcast(std::vector<DeviceBufferPair>& buffers,
-                                    se::Stream& stream, Communicator& comm) {
+                                    se::Stream& stream, Communicator& comm,
+                                    CollectiveBroadcastMetadata* cb_metadata,
+                                    bool has_dynamic_root) {
+  if (has_dynamic_root && cb_metadata) {
+    DeviceBufferPair& roots_device_buffer = buffers.back();
+    CHECK(cb_metadata->bcast_roots != nullptr);
+    RETURN_IF_ERROR(stream.Memcpy(cb_metadata->bcast_roots->address().opaque(),
+                                  roots_device_buffer.source_buffer,
+                                  roots_device_buffer.source_buffer.size()));
+    // Wait for the copies to complete.
+    if (absl::Status blocked = stream.BlockHostUntilDone(); !blocked.ok()) {
+      return absl::InternalError(
+          absl::StrFormat("Failed to copy dynamic roots on stream %p: %s",
+                          &stream, blocked.message()));
+    }
+  }
   auto* gpu_comm = absl::down_cast<GpuCommunicator*>(&comm);
   Future<> future = gpu_comm->GroupExecute([&]() -> absl::Status {
-    for (auto buffer : buffers) {
+    RankId root = RankId(0);
+    for (int64_t i = 0; i < buffers.size(); ++i) {
+      const DeviceBufferPair& buffer = buffers[i];
+      if (has_dynamic_root && cb_metadata) {
+        // If dynamic root is enabled, the actual root rank is read from the
+        // last buffer and can be different for each broadcast.
+        int32_t* roots_ptr = reinterpret_cast<int32_t*>(
+            cb_metadata->bcast_roots->address().opaque());
+        root = RankId(roots_ptr[i]);
+      }
       se::DeviceAddressBase src_addr = buffer.source_buffer;
       se::DeviceAddressBase dest_addr = buffer.destination_buffer;
       RETURN_IF_ERROR(gpu_comm->LaunchBroadcast(
           // Always use rank 0 since we always broadcast from the first id
           // in replica_groups
-          src_addr, dest_addr, buffer.element_type, buffer.element_count,
-          RankId(0), GpuCollectives::On(stream)));
+          src_addr, dest_addr, buffer.element_type, buffer.element_count, root,
+          GpuCollectives::On(stream)));
     }
     return absl::OkStatus();
   });
