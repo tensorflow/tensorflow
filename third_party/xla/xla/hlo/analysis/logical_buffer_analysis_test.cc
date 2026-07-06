1b401c1e534744d77e7f737b7d1f8a7b912fcce7
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-06 14:16:30 -0700

    Add AsyncUpdate and AsyncDone handling to LogicalBufferAnalysis
    PiperOrigin-RevId: 943491724

--- third_party/xla/xla/hlo/analysis/logical_buffer_analysis_test.cc
+++ third_party/xla/xla/hlo/analysis/logical_buffer_analysis_test.cc
@@ -23,9 +23,11 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/parser/hlo_parser.h"
 #include "xla/hlo/testlib/hlo_hardware_independent_test_base.h"
 #include "xla/hlo/testlib/test.h"
 #include "xla/service/logical_buffer.h"
+#include "xla/shape.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/platform/status_matchers.h"
 
@@ -259,23 +261,27 @@ TEST_F(LogicalBufferAnalysisTest, CustomCallFusionAsync) {
     fusion = f32[2,3] fusion(p0), kind=kLoop, calls=fused_computation
     async-start = ((f32[2,3]), f32[2,3], u32[]) custom-call-start(p0),
                                                 custom_call_target="bar"
-    ROOT async-done = f32[2,3] custom-call-done(async-start)
+    async-update = ((f32[2,3]), f32[2,3], u32[]) async-update(async-start)
+    ROOT async-done = f32[2,3] custom-call-done(async-update)
   }
   )";
   ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(hlo_str));
   ASSERT_OK_AND_ASSIGN(analysis_, LogicalBufferAnalysis::Run(module.get()));
 
+  HloInstruction* p0_fused = FindInstruction(module.get(), "p0.fused");
+  HloInstruction* copy_fused = FindInstruction(module.get(), "copy.fused");
   HloInstruction* p0 = FindInstruction(module.get(), "p0");
   HloInstruction* ccall = FindInstruction(module.get(), "ccall");
   HloInstruction* fusion = FindInstruction(module.get(), "fusion");
   HloInstruction* async_start = FindInstruction(module.get(), "async-start");
+  HloInstruction* async_update = FindInstruction(module.get(), "async-update");
   HloInstruction* async_done = FindInstruction(module.get(), "async-done");
+  HloComputation* async_computation = async_start->async_wrapped_computation();
+  HloInstruction* async_wrapper_root = async_computation->root_instruction();
+  ASSERT_EQ(async_wrapper_root->operand_count(), 1);
+  HloInstruction* async_wrapper_param = async_wrapper_root->mutable_operand(0);
 
-  HloInstruction* p0_fused =
-      fusion->fused_instructions_computation()->parameter_instruction(0);
-  HloInstruction* copy_fused = fusion->fused_expression_root();
-
-  EXPECT_EQ(analysis_->num_logical_buffers(), 12);
+  EXPECT_EQ(analysis_->num_logical_buffers(), 13);
 
   VerifyBufferDefinedAt(p0, {});
   VerifyBufferDefinedAt(ccall, {});
@@ -287,7 +293,20 @@ TEST_F(LogicalBufferAnalysisTest, CustomCallFusionAsync) {
   VerifyBufferDefinedAt(async_start, {0});
   VerifyBufferDefinedAt(async_start, {1});
   VerifyBufferDefinedAt(async_start, {2});
+  // {0, 0} is implicitly aliased to input parameter p0.
   VerifyNoBufferDefinedAt(async_start, {0, 0});
+
+  VerifyBufferDefinedAt(async_update, {});
+  VerifyNoBufferDefinedAt(async_update, {0});
+  VerifyNoBufferDefinedAt(async_update, {1});
+  VerifyNoBufferDefinedAt(async_update, {2});
+
+  // There are two more buffers defined in the async computation,
+  // which is created by the custom-call-start and custom-call-done
+  // in async wrapped computation.
+  VerifyBufferDefinedAt(async_wrapper_param, {});
+  VerifyBufferDefinedAt(async_wrapper_root, {});
+
   VerifyBufferDefinedAt(async_done, {});
 }
 
@@ -439,6 +458,179 @@ TEST_F(LogicalBufferAnalysisTest, InvalidGetBufferIdBehavior) {
   // GetBuffer with invalid ID of 100 on a module containing 2 buffers.
   EXPECT_THAT(analysis_->GetBuffer(100),
               StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST_F(LogicalBufferAnalysisTest, SimpleAsyncChain) {
+  absl::string_view hlo_str = R"(
+  HloModule module
+
+  async_computation {
+    p0 = f32[4] parameter(0)
+    ROOT ccall = f32[4] custom-call(p0), custom_call_target="bar"
+  }
+
+  ENTRY entry {
+    p0 = f32[4] parameter(0)
+    start = ((f32[4]), f32[4], s32[]) async-start(p0), calls=async_computation,
+                                       async_execution_thread="sparsecore"
+    update = ((f32[4]), f32[4], s32[]) async-update(start)
+    ROOT done = f32[4] async-done(update)
+  }
+  )";
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnVerifiedModule(hlo_str));
+  ASSERT_OK_AND_ASSIGN(analysis_, LogicalBufferAnalysis::Run(module.get()));
+
+  HloInstruction* p0 = FindInstruction(module.get(), "p0");
+  HloInstruction* start = FindInstruction(module.get(), "start");
+  HloInstruction* update = FindInstruction(module.get(), "update");
+
+  VerifyBufferDefinedAt(p0, {});
+
+  // Verify start buffers:
+  // - {} (top-level tuple) -> defined
+  // - {0} (sub-tuple) -> defined
+  // - {0, 0} -> implicitly aliased (to input parameter p0) -> not defined
+  // - {1} (output) -> defined
+  // - {2} (state) -> defined
+  VerifyBufferDefinedAt(start, {});
+  VerifyBufferDefinedAt(start, {0});
+  VerifyBufferDefinedAt(start, {1});
+  VerifyBufferDefinedAt(start, {2});
+  VerifyNoBufferDefinedAt(start, {0, 0});
+
+  // Verify update buffers:
+  // - {} (top-level tuple) -> defined (always defined for new async-update)
+  // - all other subshapes are compatible with start -> not defined
+  VerifyBufferDefinedAt(update, {});
+  VerifyNoBufferDefinedAt(update, {0});
+  VerifyNoBufferDefinedAt(update, {0, 0});
+  VerifyNoBufferDefinedAt(update, {1});
+  VerifyNoBufferDefinedAt(update, {2});
+}
+
+TEST_F(LogicalBufferAnalysisTest, AsyncUpdateWithChangedShapes) {
+  absl::string_view hlo_str = R"(
+  HloModule module
+
+  async_computation {
+    p0 = f32[4] parameter(0)
+    ROOT ccall = f32[4] custom-call(p0), custom_call_target="bar"
+  }
+
+  ENTRY entry {
+    p0 = f32[4] parameter(0)
+    start = ((f32[4]), f32[4], s32[]) async-start(p0), calls=async_computation,
+                                       async_execution_thread="sparsecore"
+    update = ((f32[4]), f32[4], s32[]) async-update(start)
+    ROOT done = f32[4] async-done(update)
+  }
+  )";
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(hlo_str));
+
+  HloInstruction* update = FindInstruction(module.get(), "update");
+
+  // Mutate the shape of update to ((f32[8]), f32[8], s32[])
+  // This makes it different from start's shape.
+  Shape new_shape = ShapeUtil::MakeTupleShape(
+      {ShapeUtil::MakeTupleShape({ShapeUtil::MakeShape(F32, {8})}),
+       ShapeUtil::MakeShape(F32, {8}), ShapeUtil::MakeShape(S32, {})});
+  *update->mutable_shape() = new_shape;
+
+  ASSERT_OK_AND_ASSIGN(analysis_, LogicalBufferAnalysis::Run(module.get()));
+
+  // {0, 0} is an input operand (starts with 0, size >= 2).
+  // It is NOT compatible with start's {0, 0} (f32[4] vs f32[8]).
+  // It should NOT define a buffer because it's an input.
+  VerifyNoBufferDefinedAt(update, {0, 0});
+
+  // {1} is an output.
+  // It is NOT compatible with start's {1} (f32[4] vs f32[8]).
+  // It SHOULD define a buffer because it's a new output.
+  VerifyBufferDefinedAt(update, {1});
+}
+
+TEST_F(LogicalBufferAnalysisTest,
+       AsyncUpdateWithExplicitAliasAndChangedShapes) {
+  absl::string_view hlo_str = R"(
+  HloModule module
+
+  async_computation {
+    p0 = f32[4] parameter(0)
+    ROOT ccall = f32[4] custom-call(p0), custom_call_target="bar"
+  }
+
+  ENTRY entry {
+    p0 = f32[4] parameter(0)
+    start = ((f32[4]), f32[4], s32[]) async-start(p0), calls=async_computation,
+                                       async_execution_thread="sparsecore",
+                                       output_to_operand_aliasing={ {1}: (0, {}) }
+    update = ((f32[4]), f32[4], s32[]) async-update(start)
+    ROOT done = f32[4] async-done(update)
+  }
+  )";
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(hlo_str));
+
+  HloInstruction* update = FindInstruction(module.get(), "update");
+
+  // Mutate the shape of update to ((f32[4]), f32[8], s32[])
+  // This changes the shape of the aliased output {1} from f32[4] to f32[8].
+  Shape new_shape = ShapeUtil::MakeTupleShape(
+      {ShapeUtil::MakeTupleShape({ShapeUtil::MakeShape(F32, {4})}),
+       ShapeUtil::MakeShape(F32, {8}), ShapeUtil::MakeShape(S32, {})});
+  *update->mutable_shape() = new_shape;
+
+  ASSERT_OK_AND_ASSIGN(analysis_, LogicalBufferAnalysis::Run(module.get()));
+
+  // {1} is explicitly aliased.
+  // It is NOT compatible with start's {1} (f32[4] vs f32[8]).
+  // It should NOT define a buffer because it's aliased.
+  VerifyNoBufferDefinedAt(update, {1});
+}
+TEST_F(LogicalBufferAnalysisTest, AsyncAliasedEmptyTupleBecomesValued) {
+  absl::string_view hlo_str = R"(
+  HloModule module
+
+  async_computation {
+    p0 = (f32[4]) parameter(0)
+    ROOT ccall = () custom-call(p0), custom_call_target="bar"
+  }
+
+  ENTRY entry {
+    p0 = (f32[4]) parameter(0)
+    start = (((f32[4])), (), s32[]) async-start(p0), calls=async_computation,
+                                       async_execution_thread="sparsecore",
+                                       output_to_operand_aliasing={ {1}: (0, {}) }
+    update = (((f32[4])), (), s32[]) async-update(start)
+    ROOT done = f32[4] async-done(update)
+  }
+  )";
+  ASSERT_OK_AND_ASSIGN(auto module, ParseAndReturnUnverifiedModule(hlo_str));
+
+  HloInstruction* start = FindInstruction(module.get(), "start");
+  HloInstruction* update = FindInstruction(module.get(), "update");
+
+  // Mutate update shape to (((f32[4])), (f32[4]), s32[])
+  // This simulates late binding where the empty tuple becomes a non-empty
+  // tuple.
+  Shape f32_4 = ShapeUtil::MakeShape(F32, {4});
+  Shape new_shape = ShapeUtil::MakeTupleShape(
+      {ShapeUtil::MakeTupleShape({ShapeUtil::MakeTupleShape({f32_4})}),
+       ShapeUtil::MakeTupleShape({f32_4}), ShapeUtil::MakeShape(S32, {})});
+  *update->mutable_shape() = new_shape;
+
+  ASSERT_OK_AND_ASSIGN(analysis_, LogicalBufferAnalysis::Run(module.get()));
+
+  // For start:
+  // {1} is () and is aliased. It SHOULD get a buffer because it is empty.
+  VerifyBufferDefinedAt(start, {1});
+
+  // For update:
+  // {1} is (f32[4]) (non-empty tuple) and is aliased. It SHOULD NOT get a
+  // buffer.
+  VerifyNoBufferDefinedAt(update, {1});
+
+  // {1, 0} is f32[4] and is NOT aliased. It SHOULD get a buffer.
+  VerifyBufferDefinedAt(update, {1, 0});
 }
 
 }  // namespace
