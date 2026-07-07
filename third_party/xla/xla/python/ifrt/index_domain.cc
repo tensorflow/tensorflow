f689cd60ebffa0a7f1e5924edcedd78991370d4d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-06 23:02:40 -0700

    Automated Code Change
    PiperOrigin-RevId: 943687800

--- third_party/xla/xla/python/ifrt/index_domain.cc
+++ third_party/xla/xla/python/ifrt/index_domain.cc
@@ -23,6 +23,7 @@ limitations under the License.
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "xla/tsl/platform/status_macros.h"
+#include "xla/python/ifrt/index_domain.pb.h"
 #include "xla/python/ifrt/serdes_version.h"
 
 namespace xla {
