bece95d7523cddfb4933eda6ee4c87a9b3f94cb5
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-06 23:31:46 -0700

    Automated Code Change
    PiperOrigin-RevId: 943699279

--- third_party/xla/xla/service/gpu/gpu_executable_buffer_allocator.cc
+++ third_party/xla/xla/service/gpu/gpu_executable_buffer_allocator.cc
@@ -29,7 +29,6 @@ limitations under the License.
 #include "absl/functional/function_ref.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
-#include "absl/log/vlog_is_on.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
