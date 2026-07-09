eb9e081fb96f2c61013ab758384a1527832d46b2
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-08 19:10:38 -0700

    Automated Code Change
    PiperOrigin-RevId: 944831100

--- third_party/xla/xla/stream_executor/gpu/gpu_command_buffer.cc
+++ third_party/xla/xla/stream_executor/gpu/gpu_command_buffer.cc
@@ -24,9 +24,11 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "absl/base/casts.h"
 #include "absl/functional/any_invocable.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/log/vlog_is_on.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
