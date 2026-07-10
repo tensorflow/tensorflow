01a786bb5d8cafb7746e149e044349234c88c3dd
guozhong.zhuang <guozhong.zhuang@intel.com>
2026-07-10 15:28:18 -0700

    [oneDNN] Fix AvgPooling3DGrad op core dump issue

--- tensorflow/core/kernels/mkl/mkl_avgpooling_op.cc
+++ tensorflow/core/kernels/mkl/mkl_avgpooling_op.cc
@@ -224,6 +224,10 @@ class MklAvgPoolingGradOp : public MklPoolingBackwardOpBase<T> {
           errors::InvalidArgument("ksize must have 4 or 5 elements, but got ",
                                   this->ksize_.size()));
 
+      OP_REQUIRES(context, this->ksize_.size() == grad_tensor.dims(),
+                  errors::InvalidArgument("input and grad tensors must have "
+                                          "same dimension"));
+
       bool is_pool2d = (this->ksize_.size() == 4);
       int expected_rank = is_pool2d ? 4 : 5;
 
