99748f505f1925336b3606344a905b301cc49f9a
Marcin Radomski <dextero@google.com>
2026-07-21 00:22:57 -0700

    [XLA:GPU] Add xla_gpu_experimental_enable_same_shape_multi_output_fusion flag
    And its corresponding field in DebugOptions proto. The flag defaults to false and does nothing for now.
    
    PiperOrigin-RevId: 951298781

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -400,6 +400,7 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_enable_triton_gemm(true);
   opts.set_xla_gpu_unsupported_enable_triton_multi_output_fusion(false);
+  opts.set_xla_gpu_experimental_enable_same_shape_multi_output_fusion(false);
   opts.set_xla_gpu_enable_cudnn_int8x32_convolution_reordering(true);
   opts.set_xla_gpu_triton_gemm_any(true);
   opts.set_xla_gpu_experimental_gemm_fusion_v2(false);
@@ -478,7 +479,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_experimental_autotune_cache_mode(
       DebugOptions::AUTOTUNE_CACHE_MODE_UPDATE);
-
 
   opts.set_xla_gpu_autotune_gemm_rtol(0.1f);
 
@@ -2517,6 +2517,15 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
               set_xla_gpu_unsupported_enable_triton_multi_output_fusion),
       debug_options->xla_gpu_unsupported_enable_triton_multi_output_fusion(),
       "Enable Triton multi-output fusions."));
+  flag_list->push_back(tsl::Flag(
+      "xla_gpu_experimental_enable_same_shape_multi_output_fusion",
+      bool_setter_for(
+          &DebugOptions::
+              set_xla_gpu_experimental_enable_same_shape_multi_output_fusion),
+      debug_options
+          ->xla_gpu_experimental_enable_same_shape_multi_output_fusion(),
+      "Enable experimental support for multi-output block-level emitter "
+      "fusions where all roots share the same shape."));
   flag_list->push_back(tsl::Flag(
       "xla_gpu_verify_triton_fusion_numerics",
       bool_setter_for(&DebugOptions::set_xla_gpu_verify_triton_fusion_numerics),
