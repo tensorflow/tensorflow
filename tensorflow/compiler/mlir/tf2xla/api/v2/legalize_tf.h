98f10d780ad7e44e0c9fbed6140b315bfabfecaf
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-20 18:46:59 -0700

    Automated Code Change
    PiperOrigin-RevId: 951172998

--- tensorflow/compiler/mlir/tf2xla/api/v2/legalize_tf.h
+++ tensorflow/compiler/mlir/tf2xla/api/v2/legalize_tf.h
@@ -30,6 +30,7 @@ limitations under the License.
 #include "xla/pjrt/proto/compile_options.pb.h"
 #include "xla/tsl/platform/statusor.h"
 #include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/protobuf/tpu/compile_metadata.pb.h"
 #include "tensorflow/core/tpu/kernels/tpu_compile_op_support.h"
 
 namespace tensorflow {
