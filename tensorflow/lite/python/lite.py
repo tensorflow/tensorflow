fcdd1cc05ba9e0175b1fb25264ea36a235bd029b
Sai Ramineni <csramineni@gmail.com>
2026-07-27 02:34:34 -0400

    Fix TFLite Python lint presubmit

--- tensorflow/lite/python/lite.py
+++ tensorflow/lite/python/lite.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# pylint: disable=line-too-long
 """TensorFlow Lite tooling helper functionality."""
 
 import enum
