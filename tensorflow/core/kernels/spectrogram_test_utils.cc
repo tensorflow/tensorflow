91bb8ca59c4f3aeef3ced37054b1b090e6a1ca23
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-31 03:19:58 -0700

    Automated Code Change
    PiperOrigin-RevId: 957033239

--- tensorflow/core/kernels/spectrogram_test_utils.cc
+++ tensorflow/core/kernels/spectrogram_test_utils.cc
@@ -144,7 +144,7 @@ void ReadCSVFileToComplexVectorOrDie(
         if (j->find_first_of("ij") != std::string::npos) {
           strings::safe_strtod(*j, &imaginary_part);
         } else {
-          strings::safe_strtod(*j, &real_part);
+          absl::SimpleAtod(*j, &real_part);
         }
       }
       data_line.push_back(std::complex<double>(real_part, imaginary_part));
