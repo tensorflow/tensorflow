c8294cdbccf9e61282e3b8335e1fbddec30f0309
A. Unique TensorFlower <gardener@tensorflow.org>
2026-07-31 18:29:20 -0700

    Automated Code Change
    PiperOrigin-RevId: 957429141

--- tensorflow/tools/graph_transforms/inline_partitionedcall.cc
+++ tensorflow/tools/graph_transforms/inline_partitionedcall.cc
@@ -14,11 +14,15 @@ limitations under the License.
 ==============================================================================*/
 
 #include <string>
+#include <vector>
 
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
+#include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
 #include "tensorflow/core/framework/attr_value.pb.h"
 #include "tensorflow/core/framework/function.pb.h"
 #include "tensorflow/core/framework/graph.pb.h"
