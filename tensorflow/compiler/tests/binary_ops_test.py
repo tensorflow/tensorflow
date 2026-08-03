9c0abb6ffc886f4eeef952601bbdb18e0057c833
Cyrax321 <cyrax8590@gmail.com>
2026-08-03 19:37:47 +0530

    Normalize negative perm indices in XLA Transpose kernel

--- tensorflow/compiler/tests/binary_ops_test.py
+++ tensorflow/compiler/tests/binary_ops_test.py
@@ -1527,6 +1527,16 @@ class BinaryOpsTest(xla_test.XLATestCase):
           np.array([[1, 2], [3, 4]], dtype=dtype),
           np.array([1, 0], dtype=np.int32),
           expected=np.array([[1, 3], [2, 4]], dtype=dtype))
+      self._testBinary(
+          array_ops.transpose,
+          np.array([[1, 2], [3, 4]], dtype=dtype),
+          np.array([-1, 0], dtype=np.int32),
+          expected=np.array([[1, 3], [2, 4]], dtype=dtype))
+      self._testBinary(
+          array_ops.transpose,
+          np.zeros(shape=[1, 0, 4], dtype=dtype),
+          np.array([-2, -1, -3], dtype=np.int32),
+          expected=np.zeros(shape=[0, 4, 1], dtype=dtype))
 
   def testConjugateTranspose(self):
     for dtype in self.complex_types:
@@ -1544,6 +1554,11 @@ class BinaryOpsTest(xla_test.XLATestCase):
           array_ops.conjugate_transpose,
           np.array([[1 - 1j, 2 + 2j], [3 - 3j, 4 + 4j]], dtype=dtype),
           np.array([1, 0], dtype=np.int32),
+          expected=np.array([[1 + 1j, 3 + 3j], [2 - 2j, 4 - 4j]], dtype=dtype))
+      self._testBinary(
+          array_ops.conjugate_transpose,
+          np.array([[1 - 1j, 2 + 2j], [3 - 3j, 4 + 4j]], dtype=dtype),
+          np.array([-1, 0], dtype=np.int32),
           expected=np.array([[1 + 1j, 3 + 3j], [2 - 2j, 4 - 4j]], dtype=dtype))
 
   def testCross(self):
