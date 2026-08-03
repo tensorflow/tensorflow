f578c7d2fff7ad227623b08c7743ff8ce261d6e9
Emilio Cota <ecg@google.com>
2026-08-03 06:48:32 -0700

    [xla:cpu] remove obsolete msan annotations / flags
    The annotations are now obsolete; XLA:CPU has now full MSan support.
    The flag passed to tfcompile via --xla_backend_extra_options is also
    removed, since it has been superseded by tfcompile's --sanitize_memory
    flag.
    
    PiperOrigin-RevId: 958346386

--- third_party/xla/xla/backends/cpu/runtime/onednn/onednn_op_thunk.cc
+++ third_party/xla/xla/backends/cpu/runtime/onednn/onednn_op_thunk.cc
@@ -23,7 +23,6 @@ limitations under the License.
 #include <vector>
 
 #include "absl/base/call_once.h"
-#include "absl/base/dynamic_annotations.h"
 #include "absl/log/check.h"
 #include "absl/memory/memory.h"
 #include "absl/status/status.h"
@@ -185,7 +184,6 @@ tsl::AsyncValueRef<OneDnnOpThunk::ExecuteEvent> OneDnnOpThunk::Execute(
                      params.buffer_allocations->GetDeviceAddress(
                          op_buffers_.arguments_buffers[i]));
 
-    ABSL_ANNOTATE_MEMORY_IS_INITIALIZED(arg.opaque(), arg.size());
     VLOG(3) << absl::StreamFormat(
         "  arg: %s (%p)", op_buffers_.arguments_shapes[i].ToString(true),
         arg.opaque());
@@ -203,7 +201,6 @@ tsl::AsyncValueRef<OneDnnOpThunk::ExecuteEvent> OneDnnOpThunk::Execute(
                      params.buffer_allocations->GetDeviceAddress(
                          op_buffers_.results_buffers[i]));
 
-    ABSL_ANNOTATE_MEMORY_IS_INITIALIZED(res.opaque(), res.size());
     VLOG(3) << absl::StreamFormat("  res: %s (%p)",
                                   op_buffers_.results_shapes[i].ToString(true),
                                   res.opaque());
