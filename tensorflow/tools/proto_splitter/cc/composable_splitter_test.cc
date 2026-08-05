0701b5df6682c4da2b8d72c1f542753948507af3
A. Unique TensorFlower <gardener@tensorflow.org>
2026-08-04 18:57:24 -0700

    Automated Code Change
    PiperOrigin-RevId: 959351768

--- tensorflow/tools/proto_splitter/cc/composable_splitter_test.cc
+++ tensorflow/tools/proto_splitter/cc/composable_splitter_test.cc
@@ -23,8 +23,8 @@ limitations under the License.
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
 #include "absl/strings/cord.h"
-#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "third_party/protobuf/message_lite.h"
 #include "riegeli/base/maker.h"  // from @riegeli
 #include "riegeli/bytes/cord_reader.h"  // from @riegeli
 #include "riegeli/bytes/fd_reader.h"  // from @riegeli
