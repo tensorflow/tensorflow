86be87709160de2db2bc3d7baa20d90d393c8ae8
Som Tripathi <somtri@iastate.edu>
2026-08-10 13:19:24 -0500

    Drop unused test_lib import from binary_ops_test
    The import has been unused since it was added. PyLint runs on changed
    files, so it fails any PR that touches this one.

--- tensorflow/compiler/tests/binary_ops_test.py
+++ tensorflow/compiler/tests/binary_ops_test.py
@@ -31,7 +31,6 @@ from tensorflow.python.ops import gradients_impl
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.platform import googletest
-from tensorflow.python.platform import test as test_lib
 
 
 class BinaryOpsTest(xla_test.XLATestCase):
