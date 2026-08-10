e7542ac378c2b7ce949605ff1228718b0c3f4a6d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-08-10 04:58:45 -0700

    [XLA:MemoryScheduling] Remove TuplePointsToAnalysis from memory schedulers.
    PiperOrigin-RevId: 962083072

--- third_party/xla/xla/hlo/transforms/simplifiers/hlo_memory_scheduler.h
+++ third_party/xla/xla/hlo/transforms/simplifiers/hlo_memory_scheduler.h
@@ -29,7 +29,6 @@ limitations under the License.
 #include "absl/strings/string_view.h"
 #include "xla/hlo/analysis/alias_info.h"
 #include "xla/hlo/analysis/hlo_alias_analysis.h"
-#include "xla/hlo/analysis/tuple_points_to_analysis.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/hlo/ir/hlo_schedule.h"
@@ -39,19 +38,16 @@ limitations under the License.
 namespace xla {
 
 // A module scheduler computes an execution sequence for the HLO instructions in
-// 'module' given a points-to analysis result that describes buffer aliasing.
+// 'module' given an alias analysis result that describes buffer aliasing.
 // peak_memory (may be nullptr) is set to the peak memory of the resulting
 // schedule according to the HeapSimulator.
-//
-// TODO(yunxing): Cleanup usage of TuplePointsToAnalysis.
 class ModuleSchedulerAlgorithm {
  public:
   explicit ModuleSchedulerAlgorithm(const AliasInfo* alias_info)
       : alias_info_(alias_info) {}
   virtual ~ModuleSchedulerAlgorithm() = default;
   virtual absl::StatusOr<HloSchedule> Run(
-      HloModule* module, const TuplePointsToAnalysis& points_to_analysis,
-      const HloAliasAnalysis& alias_analysis,
+      HloModule* module, const HloAliasAnalysis& alias_analysis,
       const absl::flat_hash_set<absl::string_view>& execution_threads,
       int64_t* peak_memory) const = 0;
 
@@ -75,11 +71,9 @@ class ComputationSchedulerAlgorithm : public ModuleSchedulerAlgorithm {
  public:
   virtual absl::StatusOr<HloInstructionSequence> Run(
       HloComputation* computation,
-      const TuplePointsToAnalysis& points_to_analysis,
       const HloAliasAnalysis& alias_analysis) const = 0;
   absl::StatusOr<HloSchedule> Run(
-      HloModule* module, const TuplePointsToAnalysis& points_to_analysis,
-      const HloAliasAnalysis& alias_analysis,
+      HloModule* module, const HloAliasAnalysis& alias_analysis,
       const absl::flat_hash_set<absl::string_view>& execution_threads,
       int64_t* peak_memory) const override;
 
@@ -124,7 +118,6 @@ class ListMemoryScheduler : public ComputationSchedulerAlgorithm {
   using ModuleSchedulerAlgorithm::Run;
   absl::StatusOr<HloInstructionSequence> Run(
       HloComputation* computation,
-      const TuplePointsToAnalysis& points_to_analysis,
       const HloAliasAnalysis& alias_analysis) const override;
 };
 
@@ -145,7 +138,6 @@ class DFSMemoryScheduler : public ComputationSchedulerAlgorithm {
   using ModuleSchedulerAlgorithm::Run;
   absl::StatusOr<HloInstructionSequence> Run(
       HloComputation* computation,
-      const TuplePointsToAnalysis& points_to_analysis,
       const HloAliasAnalysis& alias_analysis) const override;
 };
 
@@ -173,7 +165,6 @@ class BFScheduler : public ComputationSchedulerAlgorithm {
                                       std::move(postprocessor)) {}
   absl::StatusOr<HloInstructionSequence> Run(
       HloComputation* computation,
-      const TuplePointsToAnalysis& points_to_analysis,
       const HloAliasAnalysis& alias_analysis) const override;
 };
 
@@ -194,7 +185,6 @@ class PostOrderScheduler : public ComputationSchedulerAlgorithm {
   using ModuleSchedulerAlgorithm::Run;
   absl::StatusOr<HloInstructionSequence> Run(
       HloComputation* computation,
-      const TuplePointsToAnalysis& points_to_analysis,
       const HloAliasAnalysis& alias_analysis) const override;
 };
 
@@ -224,8 +214,7 @@ class DefaultMemoryScheduler : public ModuleSchedulerAlgorithm {
         dfs_scheduler_(alias_info, size_function_, postprocessor),
         post_order_scheduler_(alias_info, size_function_, postprocessor) {}
   absl::StatusOr<HloSchedule> Run(
-      HloModule* module, const TuplePointsToAnalysis& points_to_analysis,
-      const HloAliasAnalysis& alias_analysis,
+      HloModule* module, const HloAliasAnalysis& alias_analysis,
       const absl::flat_hash_set<absl::string_view>& execution_threads,
       int64_t* peak_memory) const override;
 
