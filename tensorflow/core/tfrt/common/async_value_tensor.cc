c2a3d862d26d4d6b7098c6001cfbe36eac38b4be
Ricardo Torres <torresguzman@google.com>
2026-08-11 14:22:43 -0700

    Fix type confusion vulnerability in AsyncValueTensor::FromTensor by validating root buffer and pointer alignment.
    PiperOrigin-RevId: 962998902

--- tensorflow/core/tfrt/common/async_value_tensor.cc
+++ tensorflow/core/tfrt/common/async_value_tensor.cc
@@ -36,7 +36,8 @@ constexpr uintptr_t kTag = 0x1ULL;
     const Tensor* tensor) {
   if (tensor == nullptr) return nullptr;
   const TensorBuffer* buf = DMAHelper::buffer(tensor);
-  if (buf == nullptr || !buf->AllocatesOpaqueHandle()) {
+  if (buf == nullptr || const_cast<TensorBuffer*>(buf)->root_buffer() != buf ||
+      !buf->AllocatesOpaqueHandle()) {
     return nullptr;
   }
   AsyncValueTensor* av_tensor = FromOpaquePointer(buf->data());
@@ -62,7 +63,11 @@ void AsyncValueTensor::SetBuffer(std::shared_ptr<xla::PjRtBuffer> buffer) {
 /*static*/ AsyncValueTensor* AsyncValueTensor::FromOpaquePointer(void* ptr) {
   uintptr_t value = reinterpret_cast<uintptr_t>(ptr);
   if (value & kTag) {
-    return reinterpret_cast<AsyncValueTensor*>(value & ~kTag);
+    uintptr_t untagged = value & ~kTag;
+    if (untagged % alignof(AsyncValueTensor) != 0) {
+      return nullptr;
+    }
+    return reinterpret_cast<AsyncValueTensor*>(untagged);
   } else {
     return nullptr;
   }
