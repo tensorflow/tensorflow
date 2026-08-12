322e993b5783f7e3692b7d394e821375d9e8f980
David Dunleavy <ddunleavy@google.com>
2026-08-11 17:15:33 -0700

    Fix `dlpack` include paths in XLA
    PiperOrigin-RevId: 963092145

--- third_party/xla/xla/python/hlo.cc
+++ third_party/xla/xla/python/hlo.cc
@@ -30,7 +30,7 @@ limitations under the License.
 #include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
-#include "include/dlpack/dlpack.h"
+#include "dlpack/dlpack.h"
 #include "mlir/Support/LLVM.h"
 #include "nanobind/nanobind.h"
 #include "nanobind/ndarray.h"
