9a6d61d3bcf0e9bfae1d0a782ff445cb0baf9919
Oleg Shyshkov <shyshkov@google.com>
2026-08-11 20:06:46 -0700

    [XLA:GPU] Remove xla_gpu_experimental_ragged_all_to_all_use_barrier flag.
    PiperOrigin-RevId: 963160547

--- third_party/xla/xla/debug_options_flags.cc
+++ third_party/xla/xla/debug_options_flags.cc
@@ -551,7 +551,6 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
   opts.set_xla_gpu_experimental_scaled_dot_with_triton(true);
   opts.set_xla_early_exit_with_layouts(false);
   opts.set_xla_gpu_experimental_all_fusions_with_triton(false);
-  opts.set_xla_gpu_experimental_ragged_all_to_all_use_barrier(true);
   opts.set_xla_gpu_experimental_ragged_all_to_all_use_barrier_with_nccl(true);
   opts.set_xla_gpu_ragged_all_to_all_mode(
       DebugOptions::COLLECTIVES_PRIVATE_MEMORY);
@@ -3448,14 +3447,6 @@ void MakeDebugOptionsFlags(std::vector<tsl::Flag>* flag_list,
       "that checks for unstable reductions in HLO computations after "
       "optimizations. Acceptable values are: 'none', 'log', and "
       "'crash'. 'none' is the default."));
-  flag_list->push_back(tsl::Flag(
-      "xla_gpu_experimental_ragged_all_to_all_use_barrier",
-      bool_setter_for(
-          &DebugOptions::
-              set_xla_gpu_experimental_ragged_all_to_all_use_barrier),
-      debug_options->xla_gpu_experimental_ragged_all_to_all_use_barrier(),
-      "If true, use the MultiGpuBarrierKernel in one-shot RaggedAllToAll "
-      "thunk."));
   flag_list->push_back(tsl::Flag(
       "xla_gpu_experimental_ragged_all_to_all_use_barrier_with_nccl",
       bool_setter_for(
