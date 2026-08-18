3228483d4ae8ba8eb47d0eb5338a81cbd8defd20
Gerardo Carranza <gcarranza@google.com>
2026-08-18 11:28:45 -0700

    [LiteRT] Downcast constant int64 padding amounts to int32 for TFL_PadV2Op.
    Reverts 8537a319505e185323ecb0b761941faab81ecb98
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/47226 from skye-ant:mtls 0888c1ded59f94daee3660d6000554516f93b23d
    PiperOrigin-RevId: 966707823

--- tensorflow/compiler/mlir/lite/ir/tfl_ops.cc
+++ tensorflow/compiler/mlir/lite/ir/tfl_ops.cc
@@ -5912,16 +5912,17 @@ OpFoldResult PadOp::fold(FoldAdaptor) {
   return {};
 }
 
-// When padding amounts are constants, cast them to i32. XNN can only
-// consume i32 pad amounts in some cases.
-struct CastConstPadAmounts : public OpRewritePattern<PadOp> {
-  using OpRewritePattern<PadOp>::OpRewritePattern;
+// When padding amounts are constants, cast them to i32. XNN and GPU delegates
+// can only consume i32 pad amounts in some cases.
+template <typename OpType>
+struct CastConstPadAmounts : public OpRewritePattern<OpType> {
+  using OpRewritePattern<OpType>::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(PadOp op,
+  LogicalResult matchAndRewrite(OpType op,
                                 PatternRewriter& rewriter) const override {
     auto padding_amount_op = op.getPadding().getDefiningOp();
     if (!padding_amount_op ||
-        !padding_amount_op->hasTrait<OpTrait::ConstantLike>()) {
+        !padding_amount_op->template hasTrait<OpTrait::ConstantLike>()) {
       return failure();
     }
 
@@ -5942,7 +5943,7 @@ struct CastConstPadAmounts : public OpRewritePattern<PadOp> {
 
 void PadOp::getCanonicalizationPatterns(RewritePatternSet& results,
                                         MLIRContext* context) {
-  results.add<CastConstPadAmounts>(context);
+  results.add<CastConstPadAmounts<PadOp>>(context);
 }
 
 //===----------------------------------------------------------------------===//
@@ -5954,6 +5955,11 @@ OpFoldResult PadV2Op::fold(FoldAdaptor) {
     return getInput();
 
   return {};
+}
+
+void PadV2Op::getCanonicalizationPatterns(RewritePatternSet& results,
+                                          MLIRContext* context) {
+  results.add<CastConstPadAmounts<PadV2Op>>(context);
 }
 
 //===----------------------------------------------------------------------===//
