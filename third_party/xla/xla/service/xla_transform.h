394ec9cf32ab162aa083310376bc656ef30f9440
A. Unique TensorFlower <gardener@tensorflow.org>
2026-08-22 06:07:20 -0700

    Automated Code Change
    PiperOrigin-RevId: 968990037

--- third_party/xla/xla/service/xla_transform.h
+++ third_party/xla/xla/service/xla_transform.h
@@ -19,6 +19,8 @@ limitations under the License.
 #include <memory>
 #include <string>
 
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "xla/hlo/ir/hlo_module.h"
