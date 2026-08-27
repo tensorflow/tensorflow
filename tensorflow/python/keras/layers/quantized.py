ba1b7f7ad0940722483e62ecc9f390f2851855cf
AshiteshSingh <raghuvesh1285@gmail.com>
2026-08-27 19:02:58 +0530

    Fix pylint indentation to 2 spaces and long lines

--- tensorflow/python/keras/layers/quantized.py
+++ tensorflow/python/keras/layers/quantized.py
@@ -16,7 +16,6 @@
 
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import tensor_shape
-from tensorflow.python.framework import ops
 from tensorflow.python.keras import activations
 from tensorflow.python.keras import constraints
 from tensorflow.python.keras import initializers
@@ -25,123 +24,131 @@ from tensorflow.python.keras.engine.base_layer import Layer
 from tensorflow.python.keras.engine.input_spec import InputSpec
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import nn
 from tensorflow.python.ops import nn_ops
 
 
 class QuantizedDense(Layer):
-    """A densely-connected layer with weight quantization.
+  """A densely-connected layer with weight quantization.
 
-    This layer acts like a standard Dense layer but simulates
-    4-bit or 8-bit weight quantization using fake quantization nodes.
-    """
+  This layer acts like a standard Dense layer but simulates
+  4-bit or 8-bit weight quantization using fake quantization nodes.
+  """
 
-    def __init__(self,
-                 units,
-                 bits=8,
-                 activation=None,
-                 use_bias=True,
-                 kernel_initializer="glorot_uniform",
-                 bias_initializer="zeros",
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 bias_constraint=None,
-                 **kwargs):
-        super(QuantizedDense, self).__init__(
-            activity_regularizer=activity_regularizer, **kwargs)
-        self.units = int(units)
-        self.bits = int(bits)
-        if self.bits not in [4, 8]:
-            raise ValueError("Only 4-bit and 8-bit quantization are supported.")
-        self.activation = activations.get(activation)
-        self.use_bias = use_bias
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
-        self.input_spec = InputSpec(min_ndim=2)
+  def __init__(self,
+               units,
+               bits=8,
+               activation=None,
+               use_bias=True,
+               kernel_initializer="glorot_uniform",
+               bias_initializer="zeros",
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               **kwargs):
+    super(QuantizedDense, self).__init__(
+        activity_regularizer=activity_regularizer, **kwargs)
+    self.units = int(units)
+    self.bits = int(bits)
+    if self.bits not in [4, 8]:
+      raise ValueError("Only 4-bit and 8-bit quantization are supported.")
+    self.activation = activations.get(activation)
+    self.use_bias = use_bias
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+    self.input_spec = InputSpec(min_ndim=2)
 
-    def build(self, input_shape):
-        input_shape = tensor_shape.TensorShape(input_shape)
-        last_dim = tensor_shape.dimension_value(input_shape[-1])
-        if last_dim is None:
-            raise ValueError("The last dimension of the inputs to `QuantizedDense` "
-                             "should be defined. Found `None`.")
-        self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
-        self.kernel = self.add_weight(
-            "kernel",
-            shape=[last_dim, self.units],
-            initializer=self.kernel_initializer,
-            regularizer=self.kernel_regularizer,
-            constraint=self.kernel_constraint,
-            dtype=self.dtype,
-            trainable=True)
-        if self.use_bias:
-            self.bias = self.add_weight(
-                "bias",
-                shape=[self.units,],
-                initializer=self.bias_initializer,
-                regularizer=self.bias_regularizer,
-                constraint=self.bias_constraint,
-                dtype=self.dtype,
-                trainable=True)
-        else:
-            self.bias = None
-        self.built = True
+  def build(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape)
+    last_dim = tensor_shape.dimension_value(input_shape[-1])
+    if last_dim is None:
+      raise ValueError(
+          "The last dimension of the inputs to `QuantizedDense` "
+          "should be defined. Found `None`.")
+    self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
+    self.kernel = self.add_weight(
+        "kernel",
+        shape=[last_dim, self.units],
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        dtype=self.dtype,
+        trainable=True)
+    if self.use_bias:
+      self.bias = self.add_weight(
+          "bias",
+          shape=[self.units,],
+          initializer=self.bias_initializer,
+          regularizer=self.bias_regularizer,
+          constraint=self.bias_constraint,
+          dtype=self.dtype,
+          trainable=True)
+    else:
+      self.bias = None
+    self.built = True
 
-    def call(self, inputs):
-        kernel = math_ops.cast(self.kernel, dtypes.float32)
-        min_val = math_ops.reduce_min(kernel)
-        max_val = math_ops.reduce_max(kernel)
-        max_val = math_ops.maximum(max_val, min_val + 1e-5)
+  def call(self, inputs):
+    kernel = math_ops.cast(self.kernel, dtypes.float32)
+    min_val = math_ops.reduce_min(kernel)
+    max_val = math_ops.reduce_max(kernel)
+    max_val = math_ops.maximum(max_val, min_val + 1e-5)
 
-        quantized_kernel = array_ops.fake_quant_with_min_max_vars(
-            kernel,
-            min_val,
-            max_val,
-            num_bits=self.bits,
-            narrow_range=True)
-        quantized_kernel = math_ops.cast(quantized_kernel, self.dtype)
+    quantized_kernel = array_ops.fake_quant_with_min_max_vars(
+        kernel,
+        min_val,
+        max_val,
+        num_bits=self.bits,
+        narrow_range=True)
+    quantized_kernel = math_ops.cast(quantized_kernel, self.dtype)
 
-        rank = inputs.shape.rank
-        if rank is not None and rank <= 2:
-            outputs = math_ops.matmul(a=inputs, b=quantized_kernel)
-        else:
-            outputs = math_ops.tensordot(inputs, quantized_kernel, [[rank - 1 if rank else -1], [0]])
+    rank = inputs.shape.rank
+    if rank is not None and rank <= 2:
+      outputs = math_ops.matmul(a=inputs, b=quantized_kernel)
+    else:
+      axes = [[rank - 1 if rank else -1], [0]]
+      outputs = math_ops.tensordot(inputs, quantized_kernel, axes)
 
-        if self.use_bias:
-            outputs = nn_ops.bias_add(outputs, self.bias)
+    if self.use_bias:
+      outputs = nn_ops.bias_add(outputs, self.bias)
 
-        if self.activation is not None:
-            outputs = self.activation(outputs)
-        return outputs
+    if self.activation is not None:
+      outputs = self.activation(outputs)
+    return outputs
 
-    def compute_output_shape(self, input_shape):
-        input_shape = tensor_shape.TensorShape(input_shape)
-        input_shape = input_shape.with_rank_at_least(2)
-        if tensor_shape.dimension_value(input_shape[-1]) is None:
-            raise ValueError(
-                "The innermost dimension of input_shape must be defined, but saw: %s"
-                % input_shape)
-        return input_shape[:-1].concatenate(self.units)
+  def compute_output_shape(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape)
+    input_shape = input_shape.with_rank_at_least(2)
+    if tensor_shape.dimension_value(input_shape[-1]) is None:
+      raise ValueError(
+          "The innermost dimension of input_shape must be defined, "
+          "but saw: %s" % input_shape)
+    return input_shape[:-1].concatenate(self.units)
 
-    def get_config(self):
-        config = super(QuantizedDense, self).get_config()
-        config.update({
-            "units": self.units,
-            "bits": self.bits,
-            "activation": activations.serialize(self.activation),
-            "use_bias": self.use_bias,
-            "kernel_initializer": initializers.serialize(self.kernel_initializer),
-            "bias_initializer": initializers.serialize(self.bias_initializer),
-            "kernel_regularizer": regularizers.serialize(self.kernel_regularizer),
-            "bias_regularizer": regularizers.serialize(self.bias_regularizer),
-            "activity_regularizer": regularizers.serialize(self.activity_regularizer),
-            "kernel_constraint": constraints.serialize(self.kernel_constraint),
-            "bias_constraint": constraints.serialize(self.bias_constraint)
-        })
-        return config
+  def get_config(self):
+    config = super(QuantizedDense, self).get_config()
+    config.update({
+        "units": self.units,
+        "bits": self.bits,
+        "activation": activations.serialize(self.activation),
+        "use_bias": self.use_bias,
+        "kernel_initializer": initializers.serialize(
+            self.kernel_initializer),
+        "bias_initializer": initializers.serialize(
+            self.bias_initializer),
+        "kernel_regularizer": regularizers.serialize(
+            self.kernel_regularizer),
+        "bias_regularizer": regularizers.serialize(
+            self.bias_regularizer),
+        "activity_regularizer": regularizers.serialize(
+            self.activity_regularizer),
+        "kernel_constraint": constraints.serialize(
+            self.kernel_constraint),
+        "bias_constraint": constraints.serialize(
+            self.bias_constraint)
+    })
+    return config
--- tensorflow/python/keras/layers/quantized_test.py
+++ tensorflow/python/keras/layers/quantized_test.py
@@ -23,36 +23,36 @@ from tensorflow.python.platform import test
 
 class QuantizedDenseTest(test.TestCase):
 
-    def setUp(self):
-        super(QuantizedDenseTest, self).setUp()
-        tf.random.set_seed(0)
-        np.random.seed(0)
+  def setUp(self):
+    super(QuantizedDenseTest, self).setUp()
+    tf.random.set_seed(0)
+    np.random.seed(0)
 
-    def test_quantized_dense_basic(self):
-        inputs = tf.random.uniform((32, 128))
+  def test_quantized_dense_basic(self):
+    inputs = tf.random.uniform((32, 128))
 
-        # Test 8-bit quantization
-        layer_8bit = QuantizedDense(64, bits=8)
-        out_8bit = layer_8bit(inputs)
+    # Test 8-bit quantization
+    layer_8bit = QuantizedDense(64, bits=8)
+    out_8bit = layer_8bit(inputs)
 
-        self.assertEqual(out_8bit.shape, (32, 64))
-        self.assertEqual(layer_8bit.kernel.shape, (128, 64))
-        self.assertEqual(layer_8bit.bias.shape, (64,))
+    self.assertEqual(out_8bit.shape, (32, 64))
+    self.assertEqual(layer_8bit.kernel.shape, (128, 64))
+    self.assertEqual(layer_8bit.bias.shape, (64,))
 
-    def test_quantized_dense_4bit(self):
-        inputs = tf.random.uniform((16, 32))
+  def test_quantized_dense_4bit(self):
+    inputs = tf.random.uniform((16, 32))
 
-        # Test 4-bit quantization
-        layer_4bit = QuantizedDense(16, bits=4, use_bias=False)
-        out_4bit = layer_4bit(inputs)
+    # Test 4-bit quantization
+    layer_4bit = QuantizedDense(16, bits=4, use_bias=False)
+    out_4bit = layer_4bit(inputs)
 
-        self.assertEqual(out_4bit.shape, (16, 16))
-        self.assertIsNone(layer_4bit.bias)
+    self.assertEqual(out_4bit.shape, (16, 16))
+    self.assertIsNone(layer_4bit.bias)
 
-    def test_invalid_bits(self):
-        with self.assertRaisesRegex(ValueError,
-                                    "Only 4-bit and 8-bit quantization"):
-            QuantizedDense(32, bits=16)
+  def test_invalid_bits(self):
+    with self.assertRaisesRegex(ValueError,
+                                "Only 4-bit and 8-bit quantization"):
+      QuantizedDense(32, bits=16)
 
 if __name__ == "__main__":
-    test.main()
+  test.main()
