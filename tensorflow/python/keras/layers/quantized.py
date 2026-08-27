1595ef53b6244ce65ce86d4e7f91ea059fc9e972
AshiteshSingh <raghuvesh1285@gmail.com>
2026-08-27 18:49:12 +0530

    Address review comments: fix architecture, tests, and formatting

--- tensorflow/python/keras/layers/quantized.py
+++ tensorflow/python/keras/layers/quantized.py
@@ -1,28 +1,48 @@
-import tensorflow as tf
-from tensorflow.python.keras.engine.base_layer import Layer
+# Copyright 2026 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Quantized Dense layer."""
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import ops
+from tensorflow.python.keras import activations
+from tensorflow.python.keras import constraints
 from tensorflow.python.keras import initializers
 from tensorflow.python.keras import regularizers
-from tensorflow.python.keras import constraints
-from tensorflow.python.keras import activations
-from tensorflow.python.framework import dtypes
-from tensorflow.python.ops import math_ops
+from tensorflow.python.keras.engine.base_layer import Layer
 from tensorflow.python.keras.engine.input_spec import InputSpec
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
+from tensorflow.python.ops import nn_ops
+
 
 class QuantizedDense(Layer):
     """A densely-connected layer with weight quantization.
-    
+
     This layer acts like a standard Dense layer but simulates
     4-bit or 8-bit weight quantization using fake quantization nodes.
     """
-    
+
     def __init__(self,
                  units,
                  bits=8,
                  activation=None,
                  use_bias=True,
-                 kernel_initializer='glorot_uniform',
-                 bias_initializer='zeros',
+                 kernel_initializer="glorot_uniform",
+                 bias_initializer="zeros",
                  kernel_regularizer=None,
                  bias_regularizer=None,
                  activity_regularizer=None,
@@ -34,7 +54,7 @@ class QuantizedDense(Layer):
         self.units = int(units)
         self.bits = int(bits)
         if self.bits not in [4, 8]:
-            raise ValueError('Only 4-bit and 8-bit quantization are supported.')
+            raise ValueError("Only 4-bit and 8-bit quantization are supported.")
         self.activation = activations.get(activation)
         self.use_bias = use_bias
         self.kernel_initializer = initializers.get(kernel_initializer)
@@ -46,14 +66,14 @@ class QuantizedDense(Layer):
         self.input_spec = InputSpec(min_ndim=2)
 
     def build(self, input_shape):
-        input_shape = tf.TensorShape(input_shape)
-        last_dim = tf.compat.dimension_value(input_shape[-1])
+        input_shape = tensor_shape.TensorShape(input_shape)
+        last_dim = tensor_shape.dimension_value(input_shape[-1])
         if last_dim is None:
-            raise ValueError('The last dimension of the inputs to `QuantizedDense` '
-                             'should be defined. Found `None`.')
+            raise ValueError("The last dimension of the inputs to `QuantizedDense` "
+                             "should be defined. Found `None`.")
         self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
         self.kernel = self.add_weight(
-            'kernel',
+            "kernel",
             shape=[last_dim, self.units],
             initializer=self.kernel_initializer,
             regularizer=self.kernel_regularizer,
@@ -62,7 +82,7 @@ class QuantizedDense(Layer):
             trainable=True)
         if self.use_bias:
             self.bias = self.add_weight(
-                'bias',
+                "bias",
                 shape=[self.units,],
                 initializer=self.bias_initializer,
                 regularizer=self.bias_regularizer,
@@ -74,61 +94,54 @@ class QuantizedDense(Layer):
         self.built = True
 
     def call(self, inputs):
-        # Apply Fake Quantization to weights
-        # fake_quant_with_min_max_vars requires float32 inputs.
-        kernel = tf.cast(self.kernel, dtypes.float32)
-        min_val = tf.math.reduce_min(kernel)
-        max_val = tf.math.reduce_max(kernel)
-        # Ensure min_val and max_val are not equal to avoid division by zero or NaN gradients
-        max_val = tf.math.maximum(max_val, min_val + 1e-5)
-        
-        quant_bits = self.bits
-        
-        quantized_kernel = tf.quantization.fake_quant_with_min_max_vars(
-            kernel, 
-            min_val, 
-            max_val, 
-            num_bits=quant_bits,
+        kernel = math_ops.cast(self.kernel, dtypes.float32)
+        min_val = math_ops.reduce_min(kernel)
+        max_val = math_ops.reduce_max(kernel)
+        max_val = math_ops.maximum(max_val, min_val + 1e-5)
+
+        quantized_kernel = array_ops.fake_quant_with_min_max_vars(
+            kernel,
+            min_val,
+            max_val,
+            num_bits=self.bits,
             narrow_range=True)
-        quantized_kernel = tf.cast(quantized_kernel, self.dtype)
-            
+        quantized_kernel = math_ops.cast(quantized_kernel, self.dtype)
+
         rank = inputs.shape.rank
-        if rank == 2 or rank is None:
-            if isinstance(inputs, tf.SparseTensor):
-                raise NotImplementedError("Sparse inputs not supported.")
-            outputs = tf.matmul(a=inputs, b=quantized_kernel)
+        if rank is not None and rank <= 2:
+            outputs = math_ops.matmul(a=inputs, b=quantized_kernel)
         else:
-            outputs = tf.tensordot(inputs, quantized_kernel, [[rank - 1], [0]])
-            
+            outputs = math_ops.tensordot(inputs, quantized_kernel, [[rank - 1 if rank else -1], [0]])
+
         if self.use_bias:
-            outputs = tf.nn.bias_add(outputs, self.bias)
-            
+            outputs = nn_ops.bias_add(outputs, self.bias)
+
         if self.activation is not None:
             outputs = self.activation(outputs)
         return outputs
-        
+
     def compute_output_shape(self, input_shape):
-        input_shape = tf.TensorShape(input_shape)
+        input_shape = tensor_shape.TensorShape(input_shape)
         input_shape = input_shape.with_rank_at_least(2)
-        if tf.compat.dimension_value(input_shape[-1]) is None:
+        if tensor_shape.dimension_value(input_shape[-1]) is None:
             raise ValueError(
-                'The innermost dimension of input_shape must be defined, but saw: %s'
+                "The innermost dimension of input_shape must be defined, but saw: %s"
                 % input_shape)
         return input_shape[:-1].concatenate(self.units)
-        
+
     def get_config(self):
         config = super(QuantizedDense, self).get_config()
         config.update({
-            'units': self.units,
-            'bits': self.bits,
-            'activation': activations.serialize(self.activation),
-            'use_bias': self.use_bias,
-            'kernel_initializer': initializers.serialize(self.kernel_initializer),
-            'bias_initializer': initializers.serialize(self.bias_initializer),
-            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
-            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
-            'activity_regularizer': regularizers.serialize(self.activity_regularizer),
-            'kernel_constraint': constraints.serialize(self.kernel_constraint),
-            'bias_constraint': constraints.serialize(self.bias_constraint)
+            "units": self.units,
+            "bits": self.bits,
+            "activation": activations.serialize(self.activation),
+            "use_bias": self.use_bias,
+            "kernel_initializer": initializers.serialize(self.kernel_initializer),
+            "bias_initializer": initializers.serialize(self.bias_initializer),
+            "kernel_regularizer": regularizers.serialize(self.kernel_regularizer),
+            "bias_regularizer": regularizers.serialize(self.bias_regularizer),
+            "activity_regularizer": regularizers.serialize(self.activity_regularizer),
+            "kernel_constraint": constraints.serialize(self.kernel_constraint),
+            "bias_constraint": constraints.serialize(self.bias_constraint)
         })
         return config
--- tensorflow/python/keras/layers/quantized_test.py
+++ tensorflow/python/keras/layers/quantized_test.py
@@ -1,7 +1,25 @@
+# Copyright 2026 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for quantized Dense layer."""
+
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.platform import test
+
 from tensorflow.python.keras.layers.quantized import QuantizedDense
+from tensorflow.python.platform import test
+
 
 class QuantizedDenseTest(test.TestCase):
 
@@ -12,28 +30,29 @@ class QuantizedDenseTest(test.TestCase):
 
     def test_quantized_dense_basic(self):
         inputs = tf.random.uniform((32, 128))
-        
+
         # Test 8-bit quantization
         layer_8bit = QuantizedDense(64, bits=8)
         out_8bit = layer_8bit(inputs)
-        
+
         self.assertEqual(out_8bit.shape, (32, 64))
         self.assertEqual(layer_8bit.kernel.shape, (128, 64))
         self.assertEqual(layer_8bit.bias.shape, (64,))
 
     def test_quantized_dense_4bit(self):
         inputs = tf.random.uniform((16, 32))
-        
+
         # Test 4-bit quantization
         layer_4bit = QuantizedDense(16, bits=4, use_bias=False)
         out_4bit = layer_4bit(inputs)
-        
+
         self.assertEqual(out_4bit.shape, (16, 16))
         self.assertIsNone(layer_4bit.bias)
-        
+
     def test_invalid_bits(self):
-        with self.assertRaisesRegex(ValueError, 'Only 4-bit and 8-bit quantization'):
+        with self.assertRaisesRegex(ValueError,
+                                    "Only 4-bit and 8-bit quantization"):
             QuantizedDense(32, bits=16)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test.main()
