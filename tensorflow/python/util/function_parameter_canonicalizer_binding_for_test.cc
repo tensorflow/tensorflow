a84243dc93c8ad193549b4f03413676a8ce28ba5
Mina Ibrahim <minaibrahim190@gmail.com>
2026-08-30 16:32:05 +0300

    Fix canonicalizer binding after master rebase

--- tensorflow/python/util/function_parameter_canonicalizer_binding_for_test.cc
+++ tensorflow/python/util/function_parameter_canonicalizer_binding_for_test.cc
@@ -57,7 +57,7 @@ PYBIND11_MODULE(_function_parameter_canonicalizer_binding_for_test, m) {
       }))
       .def("canonicalize", [](FunctionParameterCanonicalizerWrapper& self,
                               py::args args, py::kwargs kwargs) {
-        std::vector<PyObject*> result_raw(
+        std::vector<tensorflow::Safe_PyObjectPtr> result_raw(
             self.function_parameter_canonicalizer_.GetArgSize());
 
         bool is_suceeded = self.function_parameter_canonicalizer_.Canonicalize(
@@ -69,7 +69,9 @@ PYBIND11_MODULE(_function_parameter_canonicalizer_binding_for_test, m) {
         }
 
         py::list result_list;
-        for (PyObject* obj : result_raw) result_list.append(obj);
+        for (const auto& obj : result_raw) {
+          result_list.append(py::handle(obj.get()));
+        }
         return result_list;
       });
 }
