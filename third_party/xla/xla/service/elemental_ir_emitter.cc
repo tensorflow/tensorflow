21f5b885053d4d2f93e2ac57cdc4851dd0bbc141
Ryo Suzuki <suzukiryo@google.com>
2026-09-01 02:11:01 -0700

    [XLA]: Fix -1 ^ x for negative exponents in elemental_ir_emitter
    Fix elemental_ir_emitter so -1 ^ x returns -1 if x is odd and 1 if x is even.
    
    PiperOrigin-RevId: 974364039

--- third_party/xla/xla/service/elemental_ir_emitter.cc
+++ third_party/xla/xla/service/elemental_ir_emitter.cc
@@ -2440,31 +2440,44 @@ llvm::Value* ElementalIrEmitter::EmitIntegerRemainder(llvm::Value* lhs,
       Select(has_int_min_overflow, GetZero(lhs->getType()), safe_rem));
 }
 
-llvm::Value* ElementalIrEmitter::EmitIntegerPow(llvm::Value* base,
-                                                llvm::Value* exponent,
+llvm::Value* ElementalIrEmitter::EmitIntegerPow(llvm::Value* lhs,
+                                                llvm::Value* rhs,
                                                 bool is_signed) {
   // Exponentiation by squaring:
   // https://en.wikipedia.org/wiki/Exponentiation_by_squaring;
   int bits = 6;  // Everything else would overflow for any exponent > 1, as 2^64
                  // is the larget possible exponent for a 64-bit integer, and
                  // that's 1 << 6.
-  llvm::Value* accumulator = llvm::ConstantInt::get(base->getType(), 1);
-  llvm::Value* one = llvm::ConstantInt::get(exponent->getType(), 1);
-  llvm::Value* zero = llvm::ConstantInt::get(exponent->getType(), 0);
+  llvm::Value* base = lhs;
+  llvm::Value* exponent = rhs;
+  llvm::Value* exp_one = llvm::ConstantInt::get(exponent->getType(), 1);
+  llvm::Value* exp_zero = llvm::ConstantInt::get(exponent->getType(), 0);
+  llvm::Value* base_one = llvm::ConstantInt::get(base->getType(), 1);
+  llvm::Value* base_zero = llvm::ConstantInt::get(base->getType(), 0);
+  llvm::Value* base_neg_one = llvm::ConstantInt::get(base->getType(), -1, true);
+  llvm::Value* accumulator = base_one;
   llvm::Value* original_base = base;
   llvm::Value* original_exponent = exponent;
 
   // Unroll the loop at compile time.
   for (int i = 0; i < bits; i++) {
-    accumulator =
-        b_->CreateSelect(b_->CreateICmpEQ(b_->CreateAnd(exponent, one), one),
-                         b_->CreateMul(accumulator, base), accumulator);
+    accumulator = b_->CreateSelect(
+        b_->CreateICmpEQ(b_->CreateAnd(exponent, exp_one), exp_one),
+        b_->CreateMul(accumulator, base), accumulator);
     base = b_->CreateMul(base, base);
     exponent = b_->CreateLShr(exponent, 1);
   }
+
+  llvm::Value* neg_one_base_result = b_->CreateSelect(
+      b_->CreateICmpEQ(b_->CreateAnd(original_exponent, exp_one), exp_one),
+      base_neg_one, base_one);
+  llvm::Value* neg_exp_res =
+      b_->CreateSelect(b_->CreateICmpEQ(original_base, base_neg_one),
+                       neg_one_base_result, base_zero);
   return b_->CreateSelect(
-      b_->CreateICmpSGE(original_exponent, zero), accumulator,
-      b_->CreateSelect(b_->CreateICmpEQ(original_base, one), one, zero));
+      b_->CreateICmpSGE(original_exponent, exp_zero), accumulator,
+      b_->CreateSelect(b_->CreateICmpEQ(original_base, base_one), base_one,
+                       neg_exp_res));
 }
 
 llvm::Value* ElementalIrEmitter::EmitIntegerMulhi(llvm::Value* lhs,
