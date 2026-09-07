da3820d00b5e130d948bbb918dca0ab34019a7a3
Vlad Sytchenko <vsytch@google.com>
2026-09-07 14:21:45 -0700

    [XLA] Support extracting async chains in HloExtractor
    PiperOrigin-RevId: 977577408

--- third_party/xla/xla/tools/hlo_extractor.cc
+++ third_party/xla/xla/tools/hlo_extractor.cc
@@ -104,8 +104,12 @@ class ExtractionVisitor : public ConstDfsHloVisitorWithDefault {
     // Replace the following two types of instructions with parameters/constants
     // (1) the instructions at the boundary with (2) the instructions that are
     // not selected by the hlo_selector.
-    if ((boundary_ != nullptr && boundary_->contains(hlo) > 0) ||
-        (extract_selector_ != nullptr && !extract_selector_(hlo))) {
+    const bool in_root_async_chain =
+        hlo->IsAsynchronous() && root_instruction_->IsAsynchronous() &&
+        hlo->async_chain_start() == root_instruction_->async_chain_start();
+    if (!in_root_async_chain &&
+        ((boundary_ != nullptr && boundary_->contains(hlo) > 0) ||
+         (extract_selector_ != nullptr && !extract_selector_(hlo)))) {
       if (replace_type_selector_ != nullptr) {
         switch (replace_type_selector_(hlo)) {
           case ReplaceType::kReplaceConst:
@@ -357,8 +361,11 @@ void ComputeBoundary(const HloInstruction* root, int64_t limit,
       if (visited.count(operand)) {
         continue;
       }
+      const bool in_same_async_chain =
+          hlo->IsAsynchronous() && operand->IsAsynchronous() &&
+          hlo->async_chain_start() == operand->async_chain_start();
       worklist.push_back(operand);
-      visited.emplace(operand, hops + 1);
+      visited.emplace(operand, in_same_async_chain ? hops : hops + 1);
     }
   }
 }
@@ -401,17 +408,23 @@ std::unique_ptr<HloModule> ExtractModule(
   QCHECK(height == -1 || !cross_computation)
       << "Boundary cannnot be calculated across the computations.";
 
+  const HloInstruction* root = instruction;
+  if (instruction->IsAsynchronous() &&
+      instruction->async_chain_done() != nullptr) {
+    root = instruction->async_chain_done();
+  }
+
   absl::flat_hash_set<const HloInstruction*> boundary;
   if (height != -1) {
-    ComputeBoundary(instruction, height, &boundary);
+    ComputeBoundary(root, height, &boundary);
   }
-  ExtractionVisitor visitor(instruction, &boundary, extract_selector,
+  ExtractionVisitor visitor(root, &boundary, extract_selector,
                             replace_type_selector, inherit_module_config,
                             inherit_schedule);
 
-  CHECK_OK(instruction->Accept(&visitor, /*call_finish_visit=*/true,
-                               /*ignore_control_predecessors=*/false,
-                               /*cross_computation=*/cross_computation));
+  CHECK_OK(root->Accept(&visitor, /*call_finish_visit=*/true,
+                        /*ignore_control_predecessors=*/false,
+                        /*cross_computation=*/cross_computation));
 
   // Inline called computations and fusions if the flag
   // `inline_calls_and_fusions` is true.
