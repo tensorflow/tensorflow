2f5d583576a7307dacd10cc040914fafddd6e53c
AshiteshSingh <raghuvesh1285@gmail.com>
2026-09-08 14:53:48 +0530

    fix(distribute): move auto_strategy imports to module top level
    Resolve g-import-not-at-top lint error by placing remote and tpu_strategy_util imports at the module top level.

--- tensorflow/python/distribute/auto_strategy.py
+++ tensorflow/python/distribute/auto_strategy.py
@@ -24,7 +24,9 @@ from tensorflow.python.distribute import tpu_strategy
 from tensorflow.python.distribute.cluster_resolver import tpu_cluster_resolver
 from tensorflow.python.distribute.experimental import (
     multi_worker_mirrored_strategy)
+from tensorflow.python.eager import remote
 from tensorflow.python.framework import config
+from tensorflow.python.tpu import tpu_strategy_util
 from tensorflow.python.util.tf_export import tf_export
 
 
@@ -63,17 +65,11 @@ def AutoStrategy() -> distribute_lib.StrategyBase:
   # Check for TPUs
   try:
     resolver = tpu_cluster_resolver.TPUClusterResolver()
-    # pylint: disable=g-import-not-at-top
-    from tensorflow.python.tpu import tpu_strategy_util
-    # pylint: enable=g-import-not-at-top
 
     # Must connect to the cluster before initializing the system
     if hasattr(config, "experimental_connect_to_cluster"):
       config.experimental_connect_to_cluster(resolver)
     else:
-      # pylint: disable=g-import-not-at-top
-      from tensorflow.python.eager import remote
-      # pylint: enable=g-import-not-at-top
       remote.connect_to_cluster(resolver)
 
     if not tpu_strategy_util.get_initialized_tpu_systems():
