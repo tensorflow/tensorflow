d39bf88666275ef59072acf708fc3b6a62d33125
Sai Ramineni <csramineni@gmail.com>
2026-09-08 20:44:29 -0400

    Include Abseil string concatenation header

--- tensorflow/core/kernels/random_index_shuffle_ops.cc
+++ tensorflow/core/kernels/random_index_shuffle_ops.cc
@@ -18,6 +18,7 @@ limitations under the License.
 #include <cstdint>
 
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "tensorflow/core/framework/bounds_check.h"
