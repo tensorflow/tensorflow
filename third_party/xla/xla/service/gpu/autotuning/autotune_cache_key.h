fa5edd456bc0961af3e135c5478ddd7b7ebc3639
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-09 07:59:37 -0700

    Integrate Triton up to 2074a1b8a5cd283ce584f8ebca825c4f0b215fc1
    [List of integrated commits](https://github.com/triton-lang/triton/compare/a77e7c793abc0d0c923a9afb275058e2fe57a198...2074a1b8a5cd283ce584f8ebca825c4f0b215fc1)
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/127295 from MaddipatlaChetan24:patch-12 cecbb3ce4c71873f7f83d819e6ae4bdbe152895d
    PiperOrigin-RevId: 978548887

--- third_party/xla/xla/service/gpu/autotuning/autotune_cache_key.h
+++ third_party/xla/xla/service/gpu/autotuning/autotune_cache_key.h
@@ -34,7 +34,7 @@ class AutotuneCacheKey {
   // changes that may affect the autotuning results.
   // To prevent accidental merges of concurrent increments, update the comment
   // to explain why the version is bumped.
-  static constexpr int kCurrentVersion = 52;  // Triton integration 1.38
+  static constexpr int kCurrentVersion = 53;  // Triton integration 1.39
 
   AutotuneCacheKey(const se::DeviceDescription& device_description,
                    const HloInstruction& instruction,
