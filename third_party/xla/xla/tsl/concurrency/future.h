4f38a22ad81c4d44dc25b5404d5a97118bfa4fe3
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-13 18:29:20 -0700

    Automated Code Change
    PiperOrigin-RevId: 980855748

--- third_party/xla/xla/tsl/concurrency/future.h
+++ third_party/xla/xla/tsl/concurrency/future.h
@@ -670,14 +670,16 @@ class Future : public internal::FutureBase<absl::StatusOr<T>> {
   // Constructs an immediately available future with the given value.
   template <
       int&... ExplicitParameterBarrier, typename U,
-      std::enable_if_t<std::is_convertible_v<U, absl::StatusOr<T>>>* = nullptr>
+      std::enable_if_t<!std::is_same_v<absl::remove_cvref_t<U>, Future<T>> &&
+                       std::is_convertible_v<U, absl::StatusOr<T>>>* = nullptr>
   Future(U&& value)  // NOLINT
       : Base(std::forward<U>(value)) {}
 
   // Constructs and immediately available future from the given value.
   template <
       int&... ExplicitParameterBarrier, typename U,
-      std::enable_if_t<std::is_constructible_v<T, U> &&
+      std::enable_if_t<!std::is_same_v<absl::remove_cvref_t<U>, Future<T>> &&
+                       std::is_constructible_v<T, U> &&
                        !std::is_convertible_v<U, absl::StatusOr<T>>>* = nullptr>
   explicit Future(U&& value) : Base(std::forward<U>(value)) {}
 
