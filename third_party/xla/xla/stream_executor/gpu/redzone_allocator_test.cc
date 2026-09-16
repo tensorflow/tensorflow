062ac9253e36bea19cb01945ed517da4a62a637f
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-16 01:19:32 -0700

    Automated Code Change
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/48815 from afzpatel:hipcc-path ad32fadbad5ec4cf55f874ccbd111889a8c01aea
    PiperOrigin-RevId: 982345650

--- third_party/xla/xla/stream_executor/gpu/redzone_allocator_test.cc
+++ third_party/xla/xla/stream_executor/gpu/redzone_allocator_test.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include <vector>
 
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
