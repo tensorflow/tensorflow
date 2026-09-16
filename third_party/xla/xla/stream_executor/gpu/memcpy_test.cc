1eca80ed3cceb495aae2941948b5fa57880628e6
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-16 04:50:20 -0700

    Automated Code Change
    PiperOrigin-RevId: 982440933

--- third_party/xla/xla/stream_executor/gpu/memcpy_test.cc
+++ third_party/xla/xla/stream_executor/gpu/memcpy_test.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include "absl/status/status_matchers.h"
 #include "xla/service/platform_util.h"
 #include "xla/stream_executor/device_address.h"
