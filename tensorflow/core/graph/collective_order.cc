54f24dbb5ce228eab664c8e3196bc8f3b555fdf6
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-15 19:25:50 -0700

    Automated Code Change
    PiperOrigin-RevId: 982192578

--- tensorflow/core/graph/collective_order.cc
+++ tensorflow/core/graph/collective_order.cc
@@ -24,6 +24,7 @@ limitations under the License.
 #include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "tensorflow/core/graph/algorithm.h"
--- tensorflow/core/graph/graph_def_builder.h
+++ tensorflow/core/graph/graph_def_builder.h
@@ -19,6 +19,9 @@ limitations under the License.
 #include <string>
 #include <vector>
 
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "tensorflow/core/framework/function.pb.h"
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/op.h"
