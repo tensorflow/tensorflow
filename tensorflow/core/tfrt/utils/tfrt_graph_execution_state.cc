d6764b001a9dcc4420e089a8122023f58847325d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-18 09:34:55 -0700

    Don't import a null sharding for replicated placeholder shardings.
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/47777 from ankitxvx:fix-ragged-all-to-all-46982 bc5ec206d7881d118da64513c4cfd03d23705543
    PiperOrigin-RevId: 983908110

--- tensorflow/core/tfrt/utils/tfrt_graph_execution_state.cc
+++ tensorflow/core/tfrt/utils/tfrt_graph_execution_state.cc
@@ -32,6 +32,7 @@ limitations under the License.
 #include "tensorflow/core/common_runtime/function_body.h"
 #include "tensorflow/core/common_runtime/function_def_utils.h"
 #include "tensorflow/core/common_runtime/graph_constructor.h"
+#include "tensorflow/core/common_runtime/graph_execution_state.h"
 #include "tensorflow/core/framework/attr_value.pb.h"
 #include "tensorflow/core/framework/function.h"
 #include "tensorflow/core/framework/function.pb.h"
@@ -190,8 +191,13 @@ tensorflow::GraphDef CreateGraphDefFromGraphAndFlibDef(
     const tensorflow::Graph& graph,
     const tensorflow::FunctionLibraryDefinition& flib_def) {
   tensorflow::GraphDef graph_def;
-  graph.ToGraphDef(&graph_def);
-  *graph_def.mutable_library() = flib_def.ToProto();
+  graph.ToGraphDef(&graph_def, /*include_flib_def=*/false);
+  if (tensorflow::ProtoMemoryOptimizationsEnabled()) {
+    *graph_def.mutable_library() =
+        flib_def.ReachableDefinitions(graph).ToProto();
+  } else {
+    *graph_def.mutable_library() = flib_def.ToProto();
+  }
   return graph_def;
 }
 
@@ -693,6 +699,16 @@ TfrtGraphExecutionState::OptimizeGraph(
         &optimized_flib));
   }
 
+  if (!options_.run_placer_grappler_on_functions &&
+      tensorflow::ProtoMemoryOptimizationsEnabled()) {
+    // Fast path: Directly merge the optimized FunctionLibraryDefinition in C++
+    // without serializing to a FunctionDefLibrary proto, avoiding expensive
+    // heap allocations and copy overhead during model initialization.
+    TF_RETURN_IF_ERROR(optimized_graph->mutable_flib_def()->AddLibrary(
+        std::move(*optimized_flib)));
+    return optimized_graph;
+  }
+
   FunctionDefLibrary optimized_flib_proto = optimized_flib->ToProto();
   if (options_.run_placer_grappler_on_functions) {
     TF_RETURN_IF_ERROR(OptimizeFunctions(optimized_flib_proto, *optimized_flib,
@@ -704,7 +720,8 @@ TfrtGraphExecutionState::OptimizeGraph(
     optimized_graph->mutable_flib_def()->Clear();
   }
 
-  TF_RETURN_IF_ERROR(optimized_graph->AddFunctionLibrary(optimized_flib_proto));
+  TF_RETURN_IF_ERROR(
+      optimized_graph->AddFunctionLibrary(std::move(optimized_flib_proto)));
 
   return optimized_graph;
 }
