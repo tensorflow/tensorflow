ae613ffc894bba007a50b083305c33f4287b6682
SundaramGupta <powerofaisinstudy@gmail.com>
2026-09-18 19:52:57 +0530

    Restore ops import in nn_grad.py

--- tensorflow/python/ops/nn_grad.py
+++ tensorflow/python/ops/nn_grad.py
@@ -25,6 +25,7 @@ from tensorflow.python.ops import array_ops_stack
 from tensorflow.python.ops import gen_nn_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_ops
+from tensorflow.python.framework import ops
 
 
 
