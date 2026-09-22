e0dcf0231432211ab1b95c26442bdc2216ff77d0
Blake Hechtman <blakehechtman@google.com>
2026-09-22 07:05:44 -0700

    Support T(*, 128) dynamic TPU tiling in SparseCore collectives and custom kernels
    Update SparseCore collective lowering, compute offloading, custom kernel, and
    grouped concatenate emitters to handle T(*, 128)(N, 1) dynamic tiling.
    
    Reverts 3b6d61bd25b9684406becae4912c451331b93174
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/127744 from Phoebus-Liu:fix/issue-59796 17cdade8046370ec422c81947e002cfad7c1b859
    PiperOrigin-RevId: 985972012

--- tensorflow/core/tfrt/common/pjrt_util_test.cc
+++ tensorflow/core/tfrt/common/pjrt_util_test.cc
@@ -22,12 +22,10 @@ limitations under the License.
 #include "xla/pjrt/plugin/xla_cpu/cpu_client_options.h"
 #include "xla/pjrt/plugin/xla_cpu/xla_cpu_pjrt_client.h"
 #include "xla/tsl/lib/core/status_test_util.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/protobuf/error_codes.pb.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/tfrt/common/pjrt_state.h"
-#include "tsl/platform/status_matchers.h"
-#include "tsl/platform/statusor.h"
-#include "tsl/platform/test.h"
 
 namespace tensorflow {
 namespace {
@@ -62,6 +60,27 @@ TEST(PjRtGpuClientCreationInfoTest, SetAndGet) {
                           GetPjRtGpuClientCreationInfo());
 
   EXPECT_THAT(retrieved_info->allowed_devices, ElementsAre(123));
+}
+
+TEST(PjRtUtilTest, TestOnlyResetPjRtClientInTFGlobalResourceManager) {
+  xla::CpuClientOptions options;
+  options.asynchronous = true;
+  options.cpu_device_count = 1;
+  TF_ASSERT_OK_AND_ASSIGN(auto pjrt_client, xla::GetXlaPjrtCpuClient(options));
+  TF_ASSERT_OK(SetPjRtClientInTFGlobalResourceManager(DEVICE_CPU,
+                                                      std::move(pjrt_client)));
+  TF_ASSERT_OK(TestOnlyResetPjRtClientInTFGlobalResourceManager(DEVICE_CPU));
+  EXPECT_THAT(GetPjRtClient(DEVICE_CPU),
+              absl_testing::StatusIs(error::NOT_FOUND));
+
+  auto info = std::make_unique<PjRtGpuClientCreationInfo>();
+  info->allowed_devices.insert(123);
+  TF_ASSERT_OK(
+      SetPjRtGpuClientCreationInfoInTFGlobalResourceManager(std::move(info)));
+  TF_ASSERT_OK(TestOnlyResetPjRtClientInTFGlobalResourceManager(DEVICE_GPU));
+  TF_ASSERT_OK_AND_ASSIGN(PjRtGpuClientCreationInfo * retrieved_info,
+                          GetPjRtGpuClientCreationInfo());
+  EXPECT_EQ(retrieved_info, nullptr);
 }
 
 }  // namespace
