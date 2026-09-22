b08f0ed4e9b581f51372a0a5b8b8cdd292a9ba65
A. Unique TensorFlower <gardener@tensorflow.org>
2026-09-21 18:19:16 -0700

    Automated Code Change
    Reverts 0f8856f9b72fe860abfccaf8707659ee8ef36774
    
    PiperOrigin-RevId: 985633899

--- tensorflow/compiler/tf2xla/kernels/tensor_list_ops.cc
+++ tensorflow/compiler/tf2xla/kernels/tensor_list_ops.cc
@@ -19,6 +19,7 @@ limitations under the License.
 #include <utility>
 #include <vector>
 
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "tensorflow/compiler/tf2xla/kernels/gather_op_helpers.h"
